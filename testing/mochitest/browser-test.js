0f16e2569d03b1244a94de5e41a7e354b3551f58
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:44:21 +0000

    Slightly better handling of exceptions, especially parse errors.  Bug 388248, r=gavin
    UltraBlame original commit: 81a8d4dfc0c39da19eecdeb27f8fc5bf59d289f7

--- testing/mochitest/browser-test.js
+++ testing/mochitest/browser-test.js
@@ -63,14 +63,14 @@ Tester.prototype = {
 
     var scriptLoader = Cc["@mozilla.org/moz/jssubscript-loader;1"].
                        getService(Ci.mozIJSSubScriptLoader);
-    scriptLoader.loadSubScript(this.currentTest.path, this.currentTest.scope);
-
-    
-    var exception = null;
     try {
+      scriptLoader.loadSubScript(this.currentTest.path, this.currentTest.scope);
+
+      
       this.currentTest.scope.test();
     } catch (ex) {
       this.currentTest.tests.push(new testResult(false, "Exception thrown", ex, false));
+      this.currentTest.scope.done = true;
     }
 
     
