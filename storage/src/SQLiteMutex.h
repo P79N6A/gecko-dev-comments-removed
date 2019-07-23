97a30e39f0a295203ce33e9101452f1d1c250d29
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:27:28 +0000

    Bug 494828 - Stop using our own mutexes and use SQLite's where possible. Part 1: Create helper objects to make using sqlite3_mutex safer and easier. r=cjones
    UltraBlame original commit: 50281086dddd341782ab704431e5b9ce947e1f2c

---
+++ storage/src/SQLiteMutex.h
@@ -0,0 +1,207 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_storage_SQLiteMutex_h_
+#define mozilla_storage_SQLiteMutex_h_
+
+#include "mozilla/BlockingResourceBase.h"
+#include "sqlite3.h"
+
+namespace mozilla {
+namespace storage {
+
+
+
+
+
+
+
+
+class SQLiteMutex : private BlockingResourceBase
+{
+public:
+  
+
+
+
+
+
+  SQLiteMutex(const char *aName)
+  : BlockingResourceBase(aName, eMutex)
+  , mMutex(NULL)
+  {
+  }
+
+  
+
+
+
+
+
+
+
+  void initWithMutex(sqlite3_mutex *aMutex)
+  {
+    NS_ASSERTION(aMutex, "You must pass in a valid mutex!");
+    NS_ASSERTION(!mMutex, "A mutex has already been set for this!");
+    mMutex = aMutex;
+  }
+
+#ifndef DEBUG
+  
+
+
+  void lock()
+  {
+    sqlite3_mutex_enter(mMutex);
+  }
+
+  
+
+
+  void unlock()
+  {
+    sqlite3_mutex_leave(mMutex);
+  }
+
+  
+
+
+  void assertCurrentThreadOwns()
+  {
+  }
+
+  
+
+
+  void assertNotCurrentThreadOwns()
+  {
+  }
+
+#else
+  void lock()
+  {
+    NS_ASSERTION(mMutex, "No mutex associated with this wrapper!");
+
+    
+    
+    CallStack callContext = CallStack();
+
+    CheckAcquire(callContext);
+    sqlite3_mutex_enter(mMutex);
+    Acquire(callContext); 
+  }
+
+  void unlock()
+  {
+    NS_ASSERTION(mMutex, "No mutex associated with this wrapper!");
+
+    
+    
+    Release(); 
+    sqlite3_mutex_leave(mMutex);
+  }
+
+  void assertCurrentThreadOwns()
+  {
+    NS_ASSERTION(mMutex, "No mutex associated with this wrapper!");
+    NS_ASSERTION(sqlite3_mutex_held(mMutex),
+                 "Mutex is not held, but we expect it to be!");
+  }
+
+  void assertNotCurrentThreadOwns()
+  {
+    NS_ASSERTION(mMutex, "No mutex associated with this wrapper!");
+    NS_ASSERTION(sqlite3_mutex_notheld(mMutex),
+                 "Mutex is held, but we expect it to not be!");
+  }
+#endif 
+
+private:
+  sqlite3_mutex *mMutex;
+};
+
+
+
+
+
+class NS_STACK_CLASS SQLiteMutexAutoLock
+{
+public:
+  SQLiteMutexAutoLock(SQLiteMutex &aMutex)
+  : mMutex(aMutex)
+  {
+    mMutex.lock();
+  }
+
+  ~SQLiteMutexAutoLock()
+  {
+    mMutex.unlock();
+  }
+
+private:
+  SQLiteMutex &mMutex;
+};
+
+
+
+
+
+class NS_STACK_CLASS SQLiteMutexAutoUnlock
+{
+public:
+  SQLiteMutexAutoUnlock(SQLiteMutex &aMutex)
+  : mMutex(aMutex)
+  {
+    mMutex.unlock();
+  }
+
+  ~SQLiteMutexAutoUnlock()
+  {
+    mMutex.lock();
+  }
+
+private:
+  SQLiteMutex &mMutex;
+};
+
+} 
+} 
+
+#endif 
---
+++ storage/test/test_mutex.cpp
@@ -0,0 +1,117 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "storage_test_harness.h"
+
+#include "SQLiteMutex.h"
+
+using namespace mozilla::storage;
+
+
+
+
+
+void
+test_AutoLock()
+{
+  int lockTypes[] = {
+    SQLITE_MUTEX_FAST,
+    SQLITE_MUTEX_RECURSIVE,
+  };
+  for (size_t i = 0; i < NS_ARRAY_LENGTH(lockTypes); i++) {
+    
+    
+    SQLiteMutex mutex("TestMutex");
+    sqlite3_mutex *inner = sqlite3_mutex_alloc(lockTypes[i]);
+    do_check_true(inner);
+    mutex.initWithMutex(inner);
+
+    
+    mutex.assertNotCurrentThreadOwns();
+    {
+      SQLiteMutexAutoLock lockedScope(mutex);
+      mutex.assertCurrentThreadOwns();
+    }
+    mutex.assertNotCurrentThreadOwns();
+
+    
+    sqlite3_mutex_free(inner);
+  }
+}
+
+void
+test_AutoUnlock()
+{
+  int lockTypes[] = {
+    SQLITE_MUTEX_FAST,
+    SQLITE_MUTEX_RECURSIVE,
+  };
+  for (size_t i = 0; i < NS_ARRAY_LENGTH(lockTypes); i++) {
+    
+    
+    SQLiteMutex mutex("TestMutex");
+    sqlite3_mutex *inner = sqlite3_mutex_alloc(lockTypes[i]);
+    do_check_true(inner);
+    mutex.initWithMutex(inner);
+
+    
+    {
+      SQLiteMutexAutoLock lockedScope(mutex);
+
+      {
+        SQLiteMutexAutoUnlock unlockedScope(mutex);
+        mutex.assertNotCurrentThreadOwns();
+      }
+      mutex.assertCurrentThreadOwns();
+    }
+
+    
+    sqlite3_mutex_free(inner);
+  }
+}
+
+void (*gTests[])(void) = {
+  test_AutoLock,
+  test_AutoUnlock,
+};
+
+const char *file = __FILE__;
+#define TEST_NAME "SQLiteMutex"
+#define TEST_FILE file
+#include "storage_test_harness_tail.h"
