6bcaf9291fdc705da034bd415f2fcd4b8c5ae581
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:29:55 +0000

    bug 371802 - JavaScript Test by Brendan Eich.
    UltraBlame original commit: 1f93b9bea4ba73e5fe0c77a4b53e22671a6bc336

---
+++ js/tests/js1_8_1/decompilation/regress-371802.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-371802.js';
+
+var BUGNUMBER = 371802;
+var summary = 'Do not assert with group assignment';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  var f = (function (a,b,n){for(var [i,j]=[a,b];i<n;[i,j]=[a,b])print(i)});
+  expect = 'function (a,b,n){for(var [i,j]=[a,b];i<n;[i,j]=[a,b]){print(i);}}';
+  actual = f + '';
+
+  compareSource(expect, actual, summary);
+
+  exitFunc ('test');
+}
