d2244a83c06a83bd41ac1fefa022381fb57b8ec9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:12:46 +0000

    Bug 461199 (Part 10) - Ensure that Link::ResetLinkState is always called when an element is bound or unbound from the DOM tree. We would not be notified about base URI changes during this time, so we need to make sure we are not registered and have no cached state. r=bz r=jwatt
    UltraBlame original commit: f947b766de6f6173b0fe89689a821d76a1aef290

--- content/html/content/src/nsHTMLAreaElement.cpp
+++ content/html/content/src/nsHTMLAreaElement.cpp
@@ -204,6 +204,8 @@ nsHTMLAreaElement::BindToTree(nsIDocument* aDocument, nsIContent* aParent,
                               nsIContent* aBindingParent,
                               PRBool aCompileEventHandlers)
 {
+  Link::ResetLinkState();
+
   nsresult rv = nsGenericHTMLElement::BindToTree(aDocument, aParent,
                                                  aBindingParent,
                                                  aCompileEventHandlers);
@@ -219,11 +221,12 @@ nsHTMLAreaElement::BindToTree(nsIDocument* aDocument, nsIContent* aParent,
 void
 nsHTMLAreaElement::UnbindFromTree(PRBool aDeep, PRBool aNullParent)
 {
+  
+  
+  Link::ResetLinkState();
+
   if (IsInDoc()) {
     RegUnRegAccessKey(PR_FALSE);
-    
-    
-    Link::ResetLinkState();
   }
 
   nsGenericHTMLElement::UnbindFromTree(aDeep, aNullParent);
