916f2a704dbf451e46b3be1ffcd1c2f459c81409
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:32:43 +0000

    bug 470914 - add a __LOCATION__ field to the global object for files loaded on the commandline in xpcshell. r=timeless
    UltraBlame original commit: 6d33746ff263b52bf79d0b761e96020d9ce80c6c

---
+++ tools/test-harness/xpcshell-simple/example/unit/test_location.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function run_test() {
+  do_check_eq(__LOCATION__.leafName, "test_location.js");
+  
+  do_import_script("tools/test-harness/xpcshell-simple/example/location_load.js");
+}
