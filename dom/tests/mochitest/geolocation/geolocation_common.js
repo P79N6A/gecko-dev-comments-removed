19364ddce1a1a0736a4c79cabffc2595df77956d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:47:21 +0000

    Bug 445180.  More geolocation mochitests.  Patch by harthur@cmu.edu, r=dougt@meer.net
    UltraBlame original commit: 7b0636303d388bf2c12c21fd3c77b711067833d0

--- dom/tests/mochitest/geolocation/geolocation_common.js
+++ dom/tests/mochitest/geolocation/geolocation_common.js
@@ -1,16 +1,127 @@
+
+const ACCEPT = 0;
+const ACCEPT_FUZZ = 1;
+const DECLINE = 2;
+
+
+
+
+
+var TEST_PROVIDER = 0; 
+
+
+var DELAYED_PROMPT = 0;
+
+var prompt_delay = timeout * 2;
+
+
+var old_prompt;
+
+
+var prompted = 0;
+
+
+var promptOption;
+
+
+var num_pos_changes = 3;
+
+ 
+var timer_interval = 500;
+var slack = 500;
+
+
+var timeout = num_pos_changes * timer_interval + slack;
+
 function check_geolocation(location) {
 
   ok(location, "Check to see if this location is non-null");
 
-  ok(location.latitude, "Check to see if there is a latitude");
-  ok(location.longitude, "Check to see if there is a longitude");
-  ok(location.altitude, "Check to see if there is a altitude");
-  ok(location.accuracy, "Check to see if there is a accuracy");
-  ok(location.altitudeAccuracy, "Check to see if there is a alt accuracy");
+  ok("latitude" in location, "Check to see if there is a latitude");
+  ok("longitude" in location, "Check to see if there is a longitude");
+  ok("altitude" in location, "Check to see if there is a altitude");
+  ok("accuracy" in location, "Check to see if there is a accuracy");
+  ok("altitudeAccuracy" in location, "Check to see if there is a alt accuracy");
+  ok("heading" in location, "Check to see if there is a heading");
+  ok("velocity" in location, "Check to see if there is a velocity");
+  ok("timestamp" in location, "Check to see if there is a timestamp");
 
-  ok(location.heading, "Check to see if there is a heading");
-  ok(location.velocity, "Check to see if there is a velocity");
+}
 
-  ok(location.timestamp, "Check to see if there is a timestamp");
 
+function check_fuzzed_geolocation(location) {
+  check_geolocation(location);
+}
+
+function check_no_geolocation(location) {
+   ok(!location, "Check to see if this location is null");
+}
+
+function checkFlags(flags, value, isExact) {
+  for(var i = 0; i < flags.length; i++)
+    ok(isExact ? flags[i] == value : flags[i] >= value, "ensure callbacks called " + value + " times");
+}
+
+function success_callback(position) {
+  if(prompted == 0)
+    ok(0, "Should not call success callback before prompt accepted");
+  if(position == null)
+    ok(1, "No geolocation available");
+  else {
+    switch(promptOption) {
+      case ACCEPT:
+        check_geolocation(position);
+        break;
+      case ACCEPT_FUZZ:
+        check_fuzzed_geolocation(position);
+        break;
+      case DECLINE:
+        check_no_geolocation(position);
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+function geolocation_prompt(request) {
+  netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+	prompted = 1;
+  switch(promptOption) {
+    case ACCEPT:
+      request.allow();
+      break;
+    case ACCEPT_FUZZ:
+      request.allowButFuzz();
+      break;
+    case DECLINE:
+      request.cancel();
+      break;
+    default:
+      break;
+  }
+  return 1;
+}
+
+function delayed_prompt(request) {
+  setTimeout(geolocation_prompt, prompt_delay, request);
+}
+
+function attachPrompt() {
+  netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+  var geolocationService = Components.classes["@mozilla.org/geolocation/service;1"]
+                           .getService(Components.interfaces.nsIGeolocationService);
+  old_prompt = geolocationService.prompt;
+
+	if(DELAYED_PROMPT)
+    geolocationService.prompt = delayed_prompt;
+  else
+    geolocationService.prompt = geolocation_prompt;
+}
+
+function removePrompt() {
+  netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
+  var geolocationService = Components.classes["@mozilla.org/geolocation/service;1"]
+                           .getService(Components.interfaces.nsIGeolocationService);
+  geolocationService.prompt = old_prompt;
 }
