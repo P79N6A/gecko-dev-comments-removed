2cfae2c506c8e4aba0ccf34e6fbc25236e12234e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:02:32 +0000

    Bug 416416 - "change extensions.update.url to not live behind https://addons.mozilla.org/" [p=reed r=Mossop a1.9=beltzner a=blocking-firefox3+]
    UltraBlame original commit: 7bfd48ef29328c25fbd9e40376e162d23ddc0e71

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -154,7 +154,7 @@ pref("app.update.incompatible.mode", 0);
 
 
 pref("extensions.update.enabled", true);
-pref("extensions.update.url", "chrome://mozapps/locale/extensions/extensions.properties");
+pref("extensions.update.url", "https://versioncheck.addons.mozilla.org/update/VersionCheck.php?reqVersion=%REQ_VERSION%&id=%ITEM_ID%&version=%ITEM_VERSION%&maxAppVersion=%ITEM_MAXAPPVERSION%&status=%ITEM_STATUS%&appID=%APP_ID%&appVersion=%APP_VERSION%&appOS=%APP_OS%&appABI=%APP_ABI%&locale=%APP_LOCALE%");
 pref("extensions.update.interval", 86400);  
                                             
 
