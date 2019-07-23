0763bb83192bd8d842df9b49284b690bce79d1fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:35:22 +0000

    Backed out changeset 97907892496f (Bug 462106 - Clear the data copied to clipboard inside the private browsing mode after leaving it; r,sr=roc a=blocking-firefox3.1+) to fix orange.
    UltraBlame original commit: 173e57cbce97ad7ed4710aacf6486fba023ddbc7

--- widget/src/gtk2/nsClipboard.cpp
+++ widget/src/gtk2/nsClipboard.cpp
@@ -164,9 +164,6 @@ nsClipboard::SetData(nsITransferable *aTransferable,
         return NS_OK;
     }
 
-    nsresult rv = mPrivacyHandler.PrepareDataForClipboard(aTransferable);
-    NS_ENSURE_SUCCESS(rv, rv);
-
     
     EmptyClipboard(aWhichClipboard);
 
@@ -190,6 +187,7 @@ nsClipboard::SetData(nsITransferable *aTransferable,
     gtk_selection_clear_targets(mWidget, selectionAtom);
 
     
+    nsresult rv;
     nsCOMPtr<nsISupportsArray> flavors;
 
     rv = aTransferable->FlavorsTransferableCanExport(getter_AddRefs(flavors));
