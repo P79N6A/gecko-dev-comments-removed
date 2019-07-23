e0bd65a1f3cf4e30960155b5271b52dee9a87b25
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:33:06 +0000

    Backout bug 506125 due to talos failures.
    UltraBlame original commit: 809e7d5f5b7816009324dc072307c5d94a2927b7

--- js/src/jsscope.cpp
+++ js/src/jsscope.cpp
@@ -172,6 +172,7 @@ JSScope::createTable(JSContext *cx, bool report)
             JS_ReportOutOfMemory(cx);
         return false;
     }
+    cx->updateMallocCounter(JS_BIT(sizeLog2) * sizeof(JSScopeProperty *));
 
     hashShift = JS_DHASH_BITS - sizeLog2;
     for (sprop = lastProp; sprop; sprop = sprop->parent) {
@@ -409,6 +410,9 @@ JSScope::changeTable(JSContext *cx, int change)
     removedCount = 0;
     oldtable = table;
     table = newtable;
+
+    
+    cx->runtime->gcMallocBytes += nbytes;
 
     
     for (oldspp = oldtable; oldsize != 0; oldspp++) {
