5031dfac097705b219a27e1be66b5956e77c78e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:26:06 +0000

    Bug 446212 - Random failure on Linux mozilla-central qm-centos5-03 / browser_bug410900.js. r=gavin
    UltraBlame original commit: f91119d7f946211c942e9a3c85e1ccd5b016f2ee

--- browser/components/preferences/applications.js
+++ browser/components/preferences/applications.js
@@ -982,6 +982,10 @@ var gApplicationsPane = {
       self._rebuildVisibleTypes();
       self._sortVisibleTypes();
       self._rebuildView();
+
+      
+      Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService).
+      notifyObservers(window, "app-handler-pane-loaded", null);
     }
     setTimeout(_delayedPaneLoad, 0, this);
   },
