0a4601b52fac88a88bb057615964c8c6f9ee61c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:54:53 +0000

    JavaScript Test - add missing test for bug 396969, by Brendan Eich, not part of the build
    UltraBlame original commit: fe1a42aacc0be5ed8d62773754eea6a6d0a5d690

---
+++ js/tests/ecma_3/Expressions/11.10-03.js
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = '11.10-03.js';
+
+var BUGNUMBER = 396969;
+var summary = '11.10 - | should evaluate operands in order';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  expect = 'o.valueOf, p.valueOf';
+  var actualval;
+  var expectval = 42;
+
+  var o = {
+    valueOf: (function (){ actual += 'o.valueOf'; return this.value}), 
+    value:42
+  };
+
+  var p = {
+    valueOf: (function (){ actual += ', p.valueOf'; return this.value}), 
+    value:2
+  };
+
+  actualval = (o | p);
+
+  reportCompare(expectval, actualval, summary + ': value');
+  reportCompare(expect, actual, summary + ': order');
+
+  exitFunc ('test');
+}
