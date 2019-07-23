6fd57361f32e882ae99802f13e448e3ea90364c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:22:15 +0000

    Test only - Bug 497578 - Fallback upgrade ends in endless loop of downloading partial upgrades when Private Browsing auto-start is enabled. r=dtownsend
    UltraBlame original commit: 07fb0b9396fa1105ab9cea2fe52f3217789fe057

---
+++ toolkit/mozapps/update/test/unit/test_bug497578.js
@@ -0,0 +1,156 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PRIVATEBROWSING_CONTRACT_ID = "@mozilla.org/privatebrowsing;1";
+
+function run_test() {
+  
+  if (!(PRIVATEBROWSING_CONTRACT_ID in AUS_Cc))
+    return;
+
+  do_test_pending();
+  dump("Testing: Bug 497578 - begin download of a complete update after a " +
+       "failure to apply a partial update\n");
+
+  removeUpdateDirsAndFiles();
+
+  var registrar = Components.manager.QueryInterface(AUS_Ci.nsIComponentRegistrar);
+  registrar.registerFactory(Components.ID("{1dfeb90a-2193-45d5-9cb8-864928b2af55}"),
+                            "Fake Window Watcher",
+                            "@mozilla.org/embedcomp/window-watcher;1",
+                             WindowWatcherFactory);
+
+  var pb = getPrefBranch();
+  
+  
+  pb.setBoolPref("app.update.enabled", true);
+  pb.setBoolPref("browser.privatebrowsing.autostart", true);
+
+  var defaults = pb.QueryInterface(AUS_Ci.nsIPrefService).getDefaultBranch(null);
+  defaults.setCharPref("app.update.channel", "bogus_channel");
+
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1.0", "2.0");
+  setDefaultPrefs();
+  do_timeout(0, "run_test_pt1()");
+}
+
+function end_test() {
+  do_test_finished();
+  
+  
+  
+  gAUS.observe(null, "xpcom-shutdown", "");
+  cleanUp();
+}
+
+function run_test_pt1() {
+  writeUpdatesToXMLFile(getLocalUpdatesXMLString(""), false);
+  var url = URL_HOST + DIR_DATA + "/partial.mar";
+  var patches = getLocalPatchString("partial", url, null, null, null, null,
+                                    STATE_FAILED) +
+                getLocalPatchString(null, null, null, null, null, null,
+                                    STATE_NONE);
+  var updates = getLocalUpdateString(patches, false, null, "1.0", null, "1.0",
+                                     null, null, null,
+                                     URL_HOST + DIR_DATA + "/partial.mar");
+  writeUpdatesToXMLFile(getLocalUpdatesXMLString(updates), true);
+  writeStatusFile(STATE_FAILED);
+
+  
+  
+  
+  gAUS = AUS_Cc["@mozilla.org/updates/update-service;1"].
+         getService(AUS_Ci.nsIApplicationUpdateService).
+         QueryInterface(AUS_Ci.nsIObserver);
+  startUpdateManager();
+
+  var privBrowsing = AUS_Cc[PRIVATEBROWSING_CONTRACT_ID].
+                     getService(AUS_Ci.nsIPrivateBrowsingService).
+                     QueryInterface(AUS_Ci.nsIObserver);
+
+  gAUS.observe(null, "profile-after-change", "");
+  privBrowsing.observe(null, "profile-after-change", "");
+  dump("Testing: private mode should be entered automatically\n");
+  do_check_true(privBrowsing.privateBrowsingEnabled);
+
+  dump("Testing: private browsing is auto-started\n");
+  do_check_true(privBrowsing.autoStarted);
+
+  
+  do_timeout(0, "run_test_pt2()");
+}
+function run_test_pt2() {
+  gAUS.observe(null, "final-ui-startup", "");
+
+  dump("Testing: update count should equal 1\n");
+  do_check_eq(gUpdateManager.updateCount, 1);
+  dump("Testing: activeUpdate should not equal null\n");
+  do_check_neq(gUpdateManager.activeUpdate, null);
+  dump("Testing: activeUpdate.state should not equal null\n");
+  do_check_neq(gUpdateManager.activeUpdate.state, null);
+  dump("Testing: activeUpdate.state should equal STATE_DOWNLOADING\n");
+  do_check_eq(gUpdateManager.activeUpdate.state, STATE_DOWNLOADING);
+  end_test();
+}
+
+
+var WindowWatcher = {
+  openWindow: function(parent, url, name, features, args) {
+  },
+
+  QueryInterface: function(iid) {
+    if (iid.equals(AUS_Ci.nsIWindowWatcher)
+     || iid.equals(AUS_Ci.nsISupports))
+      return this;
+
+    throw AUS_Cr.NS_ERROR_NO_INTERFACE;
+  }
+}
+
+var WindowWatcherFactory = {
+  createInstance: function createInstance(outer, iid) {
+    if (outer != null)
+      throw AUS_Cr.NS_ERROR_NO_AGGREGATION;
+    return WindowWatcher.QueryInterface(iid);
+  }
+};
