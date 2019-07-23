ea4f051159644429caa94bc05eb2d7eeffee5d91
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:43:27 +0000

    bug 383585, improve tooling for dummy localizations, r=sayrer
    UltraBlame original commit: 294f0e16d2a91cca97a73659eed4abe91eb91431

---
+++ tools/l10n/l10n.py
@@ -0,0 +1,147 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import logging
+from optparse import OptionParser
+import os
+import os.path
+import re
+from subprocess import Popen, PIPE
+from shutil import copy2
+import sys
+
+def walk(base):
+  for root, dirs, files in os.walk(base):
+    try:
+      dirs.remove('CVS')
+    except ValueError:
+      pass
+    yield (os.path.normpath(root[len(base)+1:]), files)
+
+def createLocalization(source, dest, apps = ['browser'], exceptions = {}):
+  '''
+  Creates a new localization.
+
+  @type  source: string
+  @param source: path to the mozilla sources to use
+
+  @type  dest: string
+  @param dest: path to the localization to create or update
+
+  @type  apps: array of strings
+  @param apps: the applications for which to create or update the
+               localization
+
+  @type  exceptions: mapping
+  @param exceptions: stuff to ignore
+  '''
+
+  assert os.path.isdir(source), "source directory is not a directory"
+  clientmk = os.path.join(source,'client.mk')
+  assert os.path.isfile(clientmk), "client.mk missing"
+
+  if not os.path.isdir(dest):
+    os.makedirs(dest)
+
+  assert os.path.isdir(dest), "target should be a directory"
+
+  
+  dirs = set()
+  cmd = ['make', '-f', clientmk] + \
+        ['echo-variable-LOCALES_' + app for app in apps]
+  p = Popen(cmd, stdout = PIPE)
+  for ln in p.stdout.readlines():
+    dirs.update(ln.strip().split())
+  dirs = sorted(list(dirs))
+
+  for d in dirs:
+    assert os.path.isdir(os.path.join(source, d)), \
+           "expecting source directory %s" % d
+
+  for d in dirs:
+    logging.debug('processing %s' % d)
+    if d in exceptions and exceptions[d] == 'all':
+      continue
+
+    basepath = os.path.join(source, d, 'locales', 'en-US')
+    
+    ign_mod = {}
+    if d in exceptions:
+      ign_mod = exceptions[d]
+      logging.debug('using exceptions: %s' % str(ign_mod))
+
+    l10nbase = os.path.join(dest, d)
+    if not os.path.isdir(l10nbase):
+      os.makedirs(l10nbase)
+    
+    for root, files in walk(basepath):
+      ignore = None
+      if root in ign_mod:
+        if ign_mod[root] == '.':
+          continue
+        ignore = re.compile(ign_mod[root])
+      l10npath = os.path.join(l10nbase, root)
+      if not os.path.isdir(l10npath):
+        os.mkdir(l10npath)
+
+      for f in files:
+        if ignore and ignore.search(f):
+          
+          continue
+        copy2(os.path.join(basepath, root, f), l10npath)
+
+if __name__ == '__main__':
+  p = OptionParser()
+  p.add_option('--source', default = '.',
+               help='Mozilla sources')
+  p.add_option('--dest', default = '../l10n',
+               help='Localization target directory')
+  p.add_option('-v', '--verbose', action="store_true", default=False,
+               help='report debugging information')
+  (opts, args) = p.parse_args()
+  if opts.verbose:
+    logging.basicConfig(level=logging.DEBUG)
+  assert len(args) == 1, "language code expected"
+  
+  
+  exceptions = {'browser':
+                {'searchplugins': '\\.xml$'},
+                'extensions/spellcheck': 'all'}
+  createLocalization(opts.source, os.path.join(opts.dest, args[0]),
+                     exceptions=exceptions)
