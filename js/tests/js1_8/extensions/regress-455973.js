9e428ca1657820b6ccbaeab203b4743cb5dc68ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:20:43 +0000

    bug 455973 - JavaScript Test by Jesse Ruderman.
    UltraBlame original commit: 620a51565ddb0e5d60a5089eb9e18515898010fa

---
+++ js/tests/js1_8/extensions/regress-455973.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-455973.js';
+
+var BUGNUMBER = 455973;
+var summary = 'Do not assert: !cx->throwing';
+var actual = '';
+var expect = '';
+
+printBugNumber(BUGNUMBER);
+printStatus (summary);
+ 
+jit(true);
+
+try
+{
+  for (let i = 0; i < 5; ++i) void (this["y" + i] = "");
+  this.__defineGetter__("z", function () { throw 2; });
+  for (let j = 0; j < 2; ++j) { [1 for each (q in this) if ('')]; } 
+}
+catch(ex)
+{
+}
+
+jit(false);
+
+reportCompare(expect, actual, summary);
