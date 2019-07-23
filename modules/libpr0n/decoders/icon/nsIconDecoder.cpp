7296eb6a17da1ecc4be4ac68f6bcdc53b5de9752
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:43:32 +0000

    Bug 361299 - read image directly from input stream in icon decoder. Patch by Alfred Kayser <alfredkayser@nl.ibm.com>. r=tor, sr=biesi.
    UltraBlame original commit: 6d16ffa718cd34b72cd88ad78ae8f26a6133ae99

--- modules/libpr0n/decoders/icon/nsIconDecoder.cpp
+++ modules/libpr0n/decoders/icon/nsIconDecoder.cpp
@@ -47,6 +47,9 @@
 #include "nsRect.h"
 #include "nsComponentManagerUtils.h"
 
+#include "nsIImage.h"
+#include "nsIInterfaceRequestorUtils.h"
+
 NS_IMPL_THREADSAFE_ADDREF(nsIconDecoder)
 NS_IMPL_THREADSAFE_RELEASE(nsIconDecoder)
 
@@ -98,59 +101,48 @@ NS_IMETHODIMP nsIconDecoder::Flush()
 
 NS_IMETHODIMP nsIconDecoder::WriteFrom(nsIInputStream *inStr, PRUint32 count, PRUint32 *_retval)
 {
-  nsresult rv;
-
-  PRUint8 * const buf = (PRUint8 *)PR_Malloc(count);
-  if (!buf) return NS_ERROR_OUT_OF_MEMORY; 
- 
   
   PRUint32 readLen;
-  rv = inStr->Read((char*)buf, count, &readLen);
-  NS_ENSURE_SUCCESS(rv, rv);
-  NS_ENSURE_TRUE(readLen >= 2, NS_ERROR_UNEXPECTED); 
+  PRUint8 header[2];
+  nsresult rv = inStr->Read((char*)header, 2, &readLen);
+  NS_ENSURE_TRUE(readLen == 2, NS_ERROR_UNEXPECTED); 
+  count -= 2;
 
-  PRUint8 * const buf_end = buf + readLen;
-  PRUint8 *data = buf;
-
-  
-
-  mObserver->OnStartDecode(nsnull);
-  
-  PRInt32 w = *(data++);
-  PRInt32 h = *(data++);
+  PRInt32 w = header[0];
+  PRInt32 h = header[1];
   NS_ENSURE_TRUE(w > 0 && h > 0, NS_ERROR_UNEXPECTED);
 
+  if (mObserver)
+    mObserver->OnStartDecode(nsnull);
   mImage->Init(w, h, mObserver);
   if (mObserver)
     mObserver->OnStartContainer(nsnull, mImage);
 
-  gfx_format format = gfxIFormats::BGRA; 
-  rv = mFrame->Init(0, 0, w, h, format, 24);
+  rv = mFrame->Init(0, 0, w, h, gfxIFormats::BGRA, 24);
   if (NS_FAILED(rv))
     return rv;
 
   mImage->AppendFrame(mFrame);
   if (mObserver)
     mObserver->OnStartFrame(nsnull, mFrame);
+
+  PRUint32 imageLen;
+  PRUint8 *imageData;
+  mFrame->GetImageData(&imageData, &imageLen);
+
   
-  PRUint32 bpr, abpr;
-  PRInt32 width, height;
-  mFrame->GetImageBytesPerRow(&bpr);
-  mFrame->GetAlphaBytesPerRow(&abpr);
-  mFrame->GetWidth(&width);
-  mFrame->GetHeight(&height);
+  NS_ENSURE_TRUE(count >= imageLen, NS_ERROR_UNEXPECTED);
 
-  PRInt32 rownum;
-  NS_ENSURE_TRUE(buf_end - data >= PRInt32(bpr) * height,
-                 NS_ERROR_UNEXPECTED);
+  
+  rv = inStr->Read((char*)imageData, imageLen, &readLen);
+  NS_ENSURE_SUCCESS(rv, rv);
+  NS_ENSURE_TRUE(readLen == imageLen, NS_ERROR_UNEXPECTED);
 
-  for (rownum = 0; rownum < height; ++rownum, data += bpr)
-    mFrame->SetImageData(data, bpr, rownum * bpr);
-
-  nsIntRect r(0, 0, width, height);
+  
+  nsIntRect r(0, 0, w, h);
+  nsCOMPtr<nsIImage> img(do_GetInterface(mFrame));
+  img->ImageUpdated(nsnull, nsImageUpdateFlags_kBitsChanged, &r);
   mObserver->OnDataAvailable(nsnull, mFrame, &r);
-
-  PR_Free(buf);
 
   return NS_OK;
 }
