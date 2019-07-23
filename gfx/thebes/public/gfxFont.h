f832157d9be9bdb943510f1a2f99032c37b7beb3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:23:55 +0000

    b=403513 bad kerning in print output if hinting is set to medium or full r=karlt sr=vlad
    UltraBlame original commit: d136d73e3ba18412c81a762bd5d8f17a9ac8d5a8

--- gfx/thebes/public/gfxFont.h
+++ gfx/thebes/public/gfxFont.h
@@ -78,7 +78,8 @@ struct THEBES_API gfxFontStyle {
     gfxFontStyle(PRUint8 aStyle, PRUint16 aWeight, gfxFloat aSize,
                  const nsACString& aLangGroup,
                  float aSizeAdjust, PRPackedBool aSystemFont,
-                 PRPackedBool aFamilyNameQuirks);
+                 PRPackedBool aFamilyNameQuirks,
+                 PRPackedBool aPrinterFont);
     gfxFontStyle(const gfxFontStyle& aStyle);
 
     
@@ -88,6 +89,9 @@ struct THEBES_API gfxFontStyle {
     
     
     PRPackedBool systemFont : 1;
+
+    
+    PRPackedBool printerFont : 1;
 
     
     
@@ -133,6 +137,7 @@ struct THEBES_API gfxFontStyle {
         return (size == other.size) &&
             (style == other.style) &&
             (systemFont == other.systemFont) &&
+            (printerFont == other.printerFont) &&
             (familyNameQuirks == other.familyNameQuirks) &&
             (weight == other.weight) &&
             (langGroup.Equals(other.langGroup)) &&
