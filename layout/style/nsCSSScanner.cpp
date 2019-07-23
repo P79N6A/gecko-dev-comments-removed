0af39f18d75095d946ef4e78a54a315fa0d22f23
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:32:12 +0000

    Avoid recursion after parsing CSS comments.  (Bug 473914)  r+sr=bzbarsky
    UltraBlame original commit: fefdf894b7ecec6b3c38058ef5adf49a632a4f1f

--- layout/style/nsCSSScanner.cpp
+++ layout/style/nsCSSScanner.cpp
@@ -707,130 +707,135 @@ nsCSSScanner::EatNewline()
 PRBool
 nsCSSScanner::Next(nsCSSToken& aToken)
 {
-  PRInt32 ch = Read();
-  if (ch < 0) {
-    return PR_FALSE;
-  }
-
-  
-  if (StartsIdent(ch, Peek()))
-    return ParseIdent(ch, aToken);
-
-  
-  if (ch == '@') {
-    PRInt32 nextChar = Read();
-    if (nextChar >= 0) {
-      PRInt32 followingChar = Peek();
-      Pushback(nextChar);
-      if (StartsIdent(nextChar, followingChar))
-        return ParseAtKeyword(ch, aToken);
+  for (;;) { 
+    PRInt32 ch = Read();
+    if (ch < 0) {
+      return PR_FALSE;
     }
-  }
 
-  
-  if ((ch == '.') || (ch == '+') || (ch == '-')) {
-    PRInt32 nextChar = Peek();
-    if (IsDigit(nextChar)) {
+    
+    if (StartsIdent(ch, Peek()))
+      return ParseIdent(ch, aToken);
+
+    
+    if (ch == '@') {
+      PRInt32 nextChar = Read();
+      if (nextChar >= 0) {
+        PRInt32 followingChar = Peek();
+        Pushback(nextChar);
+        if (StartsIdent(nextChar, followingChar))
+          return ParseAtKeyword(ch, aToken);
+      }
+    }
+
+    
+    if ((ch == '.') || (ch == '+') || (ch == '-')) {
+      PRInt32 nextChar = Peek();
+      if (IsDigit(nextChar)) {
+        return ParseNumber(ch, aToken);
+      }
+      else if (('.' == nextChar) && ('.' != ch)) {
+        nextChar = Read();
+        PRInt32 followingChar = Peek();
+        Pushback(nextChar);
+        if (IsDigit(followingChar))
+          return ParseNumber(ch, aToken);
+      }
+    }
+    if (IsDigit(ch)) {
       return ParseNumber(ch, aToken);
     }
-    else if (('.' == nextChar) && ('.' != ch)) {
-      nextChar = Read();
-      PRInt32 followingChar = Peek();
-      Pushback(nextChar);
-      if (IsDigit(followingChar))
-        return ParseNumber(ch, aToken);
+
+    
+    if (ch == '#') {
+      return ParseRef(ch, aToken);
     }
-  }
-  if (IsDigit(ch)) {
-    return ParseNumber(ch, aToken);
-  }
 
-  
-  if (ch == '#') {
-    return ParseRef(ch, aToken);
-  }
+    
+    if ((ch == '"') || (ch == '\'')) {
+      return ParseString(ch, aToken);
+    }
 
-  
-  if ((ch == '"') || (ch == '\'')) {
-    return ParseString(ch, aToken);
-  }
-
-  
-  if (IsWhitespace(ch)) {
-    aToken.mType = eCSSToken_WhiteSpace;
-    aToken.mIdent.Assign(PRUnichar(ch));
-    (void) EatWhiteSpace();
-    return PR_TRUE;
-  }
-  if (ch == '/') {
-    PRInt32 nextChar = Peek();
-    if (nextChar == '*') {
-      (void) Read();
-#if 0
-      
-      
-      
-      
-      aToken.mIdent.SetCapacity(2);
+    
+    if (IsWhitespace(ch)) {
+      aToken.mType = eCSSToken_WhiteSpace;
       aToken.mIdent.Assign(PRUnichar(ch));
-      aToken.mIdent.Append(PRUnichar(nextChar));
-      return ParseCComment(aToken);
-#endif
-      return SkipCComment() && Next(aToken);
+      (void) EatWhiteSpace();
+      return PR_TRUE;
     }
-  }
-  if (ch == '<') {  
-    if (LookAhead('!')) {
-      if (LookAhead('-')) {
+    if (ch == '/') {
+      PRInt32 nextChar = Peek();
+      if (nextChar == '*') {
+        (void) Read();
+#if 0
+        
+        
+        
+        
+        aToken.mIdent.SetCapacity(2);
+        aToken.mIdent.Assign(PRUnichar(ch));
+        aToken.mIdent.Append(PRUnichar(nextChar));
+        return ParseCComment(aToken);
+#endif
+        if (!SkipCComment()) {
+          return PR_FALSE;
+        }
+        continue; 
+      }
+    }
+    if (ch == '<') {  
+      if (LookAhead('!')) {
         if (LookAhead('-')) {
+          if (LookAhead('-')) {
+            aToken.mType = eCSSToken_HTMLComment;
+            aToken.mIdent.AssignLiteral("<!--");
+            return PR_TRUE;
+          }
+          Pushback('-');
+        }
+        Pushback('!');
+      }
+    }
+    if (ch == '-') {  
+      if (LookAhead('-')) {
+        if (LookAhead('>')) {
           aToken.mType = eCSSToken_HTMLComment;
-          aToken.mIdent.AssignLiteral("<!--");
+          aToken.mIdent.AssignLiteral("-->");
           return PR_TRUE;
         }
         Pushback('-');
       }
-      Pushback('!');
     }
-  }
-  if (ch == '-') {  
-    if (LookAhead('-')) {
-      if (LookAhead('>')) {
-        aToken.mType = eCSSToken_HTMLComment;
-        aToken.mIdent.AssignLiteral("-->");
-        return PR_TRUE;
-      }
-      Pushback('-');
-    }
-  }
 
-  
-  if (( ch == '|' ) || ( ch == '~' ) || ( ch == '^' ) ||
-      ( ch == '$' ) || ( ch == '*' )) {
-    PRInt32 nextChar = Read();
-    if ( nextChar == '=' ) {
-      if (ch == '~') {
-        aToken.mType = eCSSToken_Includes;
+    
+    if (( ch == '|' ) || ( ch == '~' ) || ( ch == '^' ) ||
+        ( ch == '$' ) || ( ch == '*' )) {
+      PRInt32 nextChar = Read();
+      if ( nextChar == '=' ) {
+        if (ch == '~') {
+          aToken.mType = eCSSToken_Includes;
+        }
+        else if (ch == '|') {
+          aToken.mType = eCSSToken_Dashmatch;
+        }
+        else if (ch == '^') {
+          aToken.mType = eCSSToken_Beginsmatch;
+        }
+        else if (ch == '$') {
+          aToken.mType = eCSSToken_Endsmatch;
+        }
+        else if (ch == '*') {
+          aToken.mType = eCSSToken_Containsmatch;
+        }
+        return PR_TRUE;
+      } else if (nextChar >= 0) {
+        Pushback(nextChar);
       }
-      else if (ch == '|') {
-        aToken.mType = eCSSToken_Dashmatch;
-      }
-      else if (ch == '^') {
-        aToken.mType = eCSSToken_Beginsmatch;
-      }
-      else if (ch == '$') {
-        aToken.mType = eCSSToken_Endsmatch;
-      }
-      else if (ch == '*') {
-        aToken.mType = eCSSToken_Containsmatch;
-      }
-      return PR_TRUE;
-    } else if (nextChar >= 0) {
-      Pushback(nextChar);
     }
+    aToken.mType = eCSSToken_Symbol;
+    aToken.mSymbol = ch;
+    return PR_TRUE;
   }
-  aToken.mType = eCSSToken_Symbol;
-  aToken.mSymbol = ch;
-  return PR_TRUE;
 }
 
 PRBool
