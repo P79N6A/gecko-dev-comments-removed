0684608a71bc1ac03f79a36f7a4debafb18d25d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:08:44 +0000

    bug 487515 - PRMJ_Now needs better granularity for panning in fennec r=crowder, robarnold
    UltraBlame original commit: 89e3d635456f4e618505f6d775acfd1f23f74d03

--- js/src/prmjtime.h
+++ js/src/prmjtime.h
@@ -78,7 +78,7 @@ extern JSInt64
 PRMJ_Now(void);
 
 
-#if defined(JS_THREADSAFE) && defined(HAVE_GETSYSTEMTIMEASFILETIME)
+#if defined(JS_THREADSAFE) && (defined(HAVE_GETSYSTEMTIMEASFILETIME) || defined(HAVE_SYSTEMTIMETOFILETIME))
 extern void
 PRMJ_NowShutdown(void);
 #else
