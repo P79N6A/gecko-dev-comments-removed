cd64197b3579c0968cd5c160317583574b7efcf2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:02:42 +0000

    Backed out bug 514188 - fix nsProfileLock to use SA_SIGINFO style signal handler, so it can chain to Breakpad's signal handler properly to try to fix Linux Breakpad bustage. Backed out changeset 4354c4d85277
    UltraBlame original commit: a3dea6c607dd264ee226c3a3df16427b0d1ae8c1

--- toolkit/crashreporter/test/nsTestCrasher.cpp
+++ toolkit/crashreporter/test/nsTestCrasher.cpp
@@ -2,7 +2,6 @@
 #include "nsIComponentManager.h"
 #include "nsIGenericFactory.h"
 #include "nsITestCrasher.h"
-#include "nsXULAppAPI.h"
 
 class nsTestCrasher : public nsITestCrasher
 {
@@ -26,13 +25,6 @@ NS_IMETHODIMP nsTestCrasher::Crash()
   *foo = 0;
   
   return NS_OK;
-}
-
-
-NS_IMETHODIMP nsTestCrasher::LockDir(nsILocalFile *directory,
-                                     nsISupports **_retval NS_OUTPARAM)
-{
-  return XRE_LockProfileDirectory(directory, _retval);
 }
 
 
