c592c0c99c95cbd49da9cdafecccd7a883360322
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:00:12 +0000

    Bug 395038 - Add "Not Now" button to save password notification bar. r=gavin, ui-r=beltzner, a1.9=schrep
    UltraBlame original commit: c669c43bc8a70b3fe3dc4e0aa7b1d2b3b6ca0856

--- toolkit/components/passwordmgr/src/nsLoginManagerPrompter.js
+++ toolkit/components/passwordmgr/src/nsLoginManagerPrompter.js
@@ -338,6 +338,10 @@ LoginManagerPrompter.prototype = {
               this._getLocalizedString("notifyBarRememberButtonText");
         var rememberButtonAccessKey =
               this._getLocalizedString("notifyBarRememberButtonAccessKey");
+        var notNowButtonText =
+              this._getLocalizedString("notifyBarNotNowButtonText");
+        var notNowButtonAccessKey =
+              this._getLocalizedString("notifyBarNotNowButtonAccessKey");
 
         var brandShortName =
               this._brandBundle.GetStringFromName("brandShortName");
@@ -369,9 +373,15 @@ LoginManagerPrompter.prototype = {
                 callback: function(aNotificationBar, aButton) {
                     pwmgr.setLoginSavingEnabled(aLogin.hostname, false);
                 }
-            }
+            },
 
             
+            {
+                label:     notNowButtonText,
+                accessKey: notNowButtonAccessKey,
+                popup:     null,
+                callback:  function() {  } 
+            }
         ];
 
 
