c31ba6c5073aa8f8180e4c25d5696d86adb2e023
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:29:51 +0000

    Bug 443878 - "XPConnect doesn't always suspend requests when switching contexts". r+sr=jst.
    UltraBlame original commit: be03f86900e170ace665e56b1cb56c8bd8070c75

--- js/src/xpconnect/src/xpcwrappednativeinfo.cpp
+++ js/src/xpconnect/src/xpcwrappednativeinfo.cpp
@@ -165,14 +165,6 @@ XPCNativeMember::Resolve(XPCCallContext& ccx, XPCNativeInterface* iface)
 
     
 
-    
-    
-    
-
-    JSContext* cx = ccx.GetSafeJSContext();
-    if(!cx)
-        return JS_FALSE;
-
     intN argc;
     intN flags;
     JSNative callback;
@@ -201,10 +193,31 @@ XPCNativeMember::Resolve(XPCCallContext& ccx, XPCNativeInterface* iface)
         callback = XPC_WN_GetterSetter;
     }
 
-    JSAutoRequest ar(cx);
+    
+    
+    
+
+    JSContext* cx = ccx.GetSafeJSContext();
+    if(!cx)
+        return JS_FALSE;
+
+    const char *memberName = iface->GetMemberName(ccx, this);
+
+    jsrefcount suspendDepth = 0;
+    if(cx != ccx) {
+        
+        suspendDepth = JS_SuspendRequest(ccx);
+        JS_BeginRequest(cx);
+    }
 
     JSFunction *fun = JS_NewFunction(cx, callback, argc, flags, nsnull,
-                                     iface->GetMemberName(ccx, this));
+                                     memberName);
+
+    if(suspendDepth) {
+        JS_EndRequest(cx);
+        JS_ResumeRequest(ccx, suspendDepth);
+    }
+
     if(!fun)
         return JS_FALSE;
 
