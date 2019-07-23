2dd4db77460775f0e250796c443ac148244670e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:22:43 +0000

    Bug 551660. Create FramePropertyTable. r=mats,sr=dbaron
    UltraBlame original commit: 1f811ed2621977c367c6261509d77a491bf254cd

---
+++ layout/base/FramePropertyTable.cpp
@@ -0,0 +1,260 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "FramePropertyTable.h"
+#include "prlog.h"
+
+namespace mozilla {
+
+void
+FramePropertyTable::Set(nsIFrame* aFrame, const FramePropertyDescriptor* aProperty,
+                        void* aValue)
+{
+  NS_ASSERTION(aFrame, "Null frame?");
+  NS_ASSERTION(aProperty, "Null property?");
+
+  if (mLastFrame != aFrame || !mLastEntry) {
+    mLastFrame = aFrame;
+    mLastEntry = mEntries.PutEntry(aFrame);
+  }
+  Entry* entry = mLastEntry;
+
+  if (!entry->mProp.IsArray()) {
+    if (!entry->mProp.mProperty) {
+      
+      entry->mProp.mProperty = aProperty;
+      entry->mProp.mValue = aValue;
+      return;
+    }
+    if (entry->mProp.mProperty == aProperty) {
+      
+      entry->mProp.DestroyValue();
+      entry->mProp.mValue = aValue;
+      return;
+    }
+
+    
+    PropertyValue current = entry->mProp;
+    entry->mProp.mProperty = nsnull;
+    PR_STATIC_ASSERT(sizeof(nsTArray<PropertyValue>) <= sizeof(entry->mProp.mValue));
+    new (&entry->mProp.mValue) nsTArray<PropertyValue>(4);
+    entry->mProp.ToArray()->AppendElement(current);
+  }
+
+  nsTArray<PropertyValue>* array = entry->mProp.ToArray();
+  nsTArray<PropertyValue>::index_type index =
+    array->IndexOf(aProperty, 0, PropertyComparator());
+  if (index != nsTArray<PropertyValue>::NoIndex) {
+    PropertyValue* pv = &array->ElementAt(index);
+    pv->DestroyValue();
+    pv->mValue = aValue;
+    return;
+  }
+
+  array->AppendElement(PropertyValue(aProperty, aValue));
+}
+
+void*
+FramePropertyTable::Get(const nsIFrame* aFrame,
+                        const FramePropertyDescriptor* aProperty,
+                        PRBool* aFoundResult)
+{
+  NS_ASSERTION(aFrame, "Null frame?");
+  NS_ASSERTION(aProperty, "Null property?");
+
+  if (aFoundResult) {
+    *aFoundResult = PR_FALSE;
+  }
+
+  if (mLastFrame != aFrame) {
+    mLastFrame = const_cast<nsIFrame*>(aFrame);
+    mLastEntry = mEntries.GetEntry(mLastFrame);
+  }
+  Entry* entry = mLastEntry;
+  if (!entry)
+    return nsnull;
+
+  if (entry->mProp.mProperty == aProperty) {
+    if (aFoundResult) {
+      *aFoundResult = PR_TRUE;
+    }
+    return entry->mProp.mValue;
+  }
+  if (!entry->mProp.IsArray()) {
+    
+    return nsnull;
+  }
+
+  nsTArray<PropertyValue>* array = entry->mProp.ToArray();
+  nsTArray<PropertyValue>::index_type index =
+    array->IndexOf(aProperty, 0, PropertyComparator());
+  if (index == nsTArray<PropertyValue>::NoIndex)
+    return nsnull;
+
+  if (aFoundResult) {
+    *aFoundResult = PR_TRUE;
+  }
+
+  return array->ElementAt(index).mValue;
+}
+
+void*
+FramePropertyTable::Remove(nsIFrame* aFrame, const FramePropertyDescriptor* aProperty,
+                           PRBool* aFoundResult)
+{
+  NS_ASSERTION(aFrame, "Null frame?");
+  NS_ASSERTION(aProperty, "Null property?");
+
+  if (aFoundResult) {
+    *aFoundResult = PR_FALSE;
+  }
+
+  if (mLastFrame != aFrame) {
+    mLastFrame = aFrame;
+    mLastEntry = mEntries.GetEntry(aFrame);
+  }
+  Entry* entry = mLastEntry;
+  if (!entry)
+    return nsnull;
+
+  if (entry->mProp.mProperty == aProperty) {
+    
+    void* value = entry->mProp.mValue;
+    mEntries.RawRemoveEntry(entry);
+    mLastEntry = nsnull;
+    if (aFoundResult) {
+      *aFoundResult = PR_TRUE;
+    }
+    return value;
+  }
+  if (!entry->mProp.IsArray()) {
+    
+    return nsnull;
+  }
+
+  nsTArray<PropertyValue>* array = entry->mProp.ToArray();
+  nsTArray<PropertyValue>::index_type index =
+    array->IndexOf(aProperty, 0, PropertyComparator());
+  if (index == nsTArray<PropertyValue>::NoIndex) {
+    
+    return nsnull;
+  }
+
+  if (aFoundResult) {
+    *aFoundResult = PR_TRUE;
+  }
+
+  void* result = array->ElementAt(index).mValue;
+
+  PRUint32 last = array->Length() - 1;
+  array->ElementAt(index) = array->ElementAt(last);
+  array->RemoveElementAt(last);
+
+  if (last == 1) {
+    PropertyValue pv = array->ElementAt(0);
+    array->nsTArray<PropertyValue>::~nsTArray<PropertyValue>();
+    entry->mProp = pv;
+  }
+  
+  return result;
+}
+
+void
+FramePropertyTable::Delete(nsIFrame* aFrame, const FramePropertyDescriptor* aProperty)
+{
+  NS_ASSERTION(aFrame, "Null frame?");
+  NS_ASSERTION(aProperty, "Null property?");
+
+  PRBool found;
+  void* v = Remove(aFrame, aProperty, &found);
+  if (found && aProperty->mDestructor) {
+    aProperty->mDestructor(v);
+  }
+}
+
+ void
+FramePropertyTable::DeleteAllForEntry(Entry* aEntry)
+{
+  if (!aEntry->mProp.IsArray()) {
+    aEntry->mProp.DestroyValue();
+    return;
+  }
+
+  nsTArray<PropertyValue>* array = aEntry->mProp.ToArray();
+  for (PRUint32 i = 0; i < array->Length(); ++i) {
+    array->ElementAt(i).DestroyValue();
+  }
+  array->nsTArray<PropertyValue>::~nsTArray<PropertyValue>();
+}
+
+void
+FramePropertyTable::DeleteAllFor(nsIFrame* aFrame)
+{
+  NS_ASSERTION(aFrame, "Null frame?");
+
+  Entry* entry = mEntries.GetEntry(aFrame);
+  if (!entry)
+    return;
+
+  if (mLastFrame == aFrame) {
+    
+    
+    mLastFrame = nsnull;
+    mLastEntry = nsnull;
+  }
+
+  DeleteAllForEntry(entry);
+  mEntries.RawRemoveEntry(entry);
+}
+
+ PLDHashOperator
+FramePropertyTable::DeleteEnumerator(Entry* aEntry, void* aArg)
+{
+  DeleteAllForEntry(aEntry);
+  return PL_DHASH_REMOVE;
+}
+
+void
+FramePropertyTable::DeleteAll()
+{
+  mLastFrame = nsnull;
+  mLastEntry = nsnull;
+
+  mEntries.EnumerateEntries(DeleteEnumerator, nsnull);
+}
+
+}
---
+++ layout/base/FramePropertyTable.h
@@ -0,0 +1,244 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef FRAMEPROPERTYTABLE_H_
+#define FRAMEPROPERTYTABLE_H_
+
+#include "nsTHashtable.h"
+#include "nsHashKeys.h"
+
+class nsIFrame;
+
+namespace mozilla {
+
+struct FramePropertyDescriptor;
+
+typedef void (*FramePropertyDestructor)(void* aPropertyValue);
+
+
+
+
+
+
+
+
+
+
+
+
+struct FramePropertyDescriptor {
+  
+
+
+  FramePropertyDestructor mDestructor;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+class FramePropertyTable {
+public:
+  FramePropertyTable() : mLastFrame(nsnull), mLastEntry(nsnull)
+  {
+    mEntries.Init();
+  }
+  ~FramePropertyTable()
+  {
+    DeleteAll();
+  }
+
+  
+
+
+
+
+
+  void Set(nsIFrame* aFrame, const FramePropertyDescriptor* aProperty,
+           void* aValue);
+  
+
+
+
+
+
+
+
+
+
+  void* Get(const nsIFrame* aFrame, const FramePropertyDescriptor* aProperty,
+            PRBool* aFoundResult = nsnull);
+  
+
+
+
+
+
+
+
+
+
+
+  void* Remove(nsIFrame* aFrame, const FramePropertyDescriptor* aProperty,
+               PRBool* aFoundResult = nsnull);
+  
+
+
+
+
+
+  void Delete(nsIFrame* aFrame, const FramePropertyDescriptor* aProperty);
+  
+
+
+
+  void DeleteAllFor(nsIFrame* aFrame);
+  
+
+
+  void DeleteAll();
+
+protected:
+  
+
+
+
+  struct PropertyValue {
+    PropertyValue() : mProperty(nsnull), mValue(nsnull) {}
+    PropertyValue(const FramePropertyDescriptor* aProperty, void* aValue)
+      : mProperty(aProperty), mValue(aValue) {}
+
+    PRBool IsArray() { return !mProperty && mValue; }
+    nsTArray<PropertyValue>* ToArray()
+    {
+      NS_ASSERTION(IsArray(), "Must be array");
+      return reinterpret_cast<nsTArray<PropertyValue>*>(&mValue);
+    }
+
+    void DestroyValue() {
+      if (mProperty->mDestructor) {
+        mProperty->mDestructor(mValue);
+      }
+    }
+
+    const FramePropertyDescriptor* mProperty;
+    void* mValue;
+  };
+
+  
+
+
+
+  class PropertyComparator {
+  public:
+    PRBool Equals(const PropertyValue& a, const PropertyValue& b) const {
+      return a.mProperty == b.mProperty;
+    }
+    PRBool Equals(const FramePropertyDescriptor* a, const PropertyValue& b) const {
+      return a == b.mProperty;
+    }
+    PRBool Equals(const PropertyValue& a, const FramePropertyDescriptor* b) const {
+      return a.mProperty == b;
+    }
+  };
+
+  
+
+
+
+  class Entry : public nsPtrHashKey<nsIFrame>
+  {
+  public:
+    Entry(KeyTypePointer aKey) : nsPtrHashKey<nsIFrame>(aKey) {}
+    Entry(const Entry &toCopy) :
+      nsPtrHashKey<nsIFrame>(toCopy), mProp(toCopy.mProp) {}
+
+    PropertyValue mProp;
+  };
+
+  static void DeleteAllForEntry(Entry* aEntry);
+  static PLDHashOperator DeleteEnumerator(Entry* aEntry, void* aArg);
+
+  nsTHashtable<Entry> mEntries;
+  nsIFrame* mLastFrame;
+  Entry* mLastEntry;
+};
+
+
+
+
+class FrameProperties {
+public:
+  FrameProperties(FramePropertyTable* aTable, nsIFrame* aFrame)
+    : mTable(aTable), mFrame(aFrame) {}
+  FrameProperties(FramePropertyTable* aTable, const nsIFrame* aFrame)
+    : mTable(aTable), mFrame(const_cast<nsIFrame*>(aFrame)) {}
+
+  void Set(const FramePropertyDescriptor* aProperty, void* aValue) const
+  {
+    mTable->Set(mFrame, aProperty, aValue);
+  }
+  void* Get(const FramePropertyDescriptor* aProperty,
+            PRBool* aFoundResult = nsnull) const
+  {
+    return mTable->Get(mFrame, aProperty, aFoundResult);
+  }
+  void* Remove(const FramePropertyDescriptor* aProperty,
+               PRBool* aFoundResult = nsnull) const
+  {
+    return mTable->Remove(mFrame, aProperty, aFoundResult);
+  }
+  void Delete(const FramePropertyDescriptor* aProperty)
+  {
+    mTable->Delete(mFrame, aProperty);
+  }
+
+private:
+  FramePropertyTable* mTable;
+  nsIFrame* mFrame;
+};
+
+}
+
+#endif 
