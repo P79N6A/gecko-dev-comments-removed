81094f63506acedc354dad7f2a33037ea1fe0ad3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:48:59 +0000

    bug 501034 - add a Win32 helper app to crash a process, and make automation.py use it when the app hangs. r=Waldo
    UltraBlame original commit: 3b932018ce6a0ee85d7e6b7ff9db23b558469967

---
+++ build/win32/crashinject.cpp
@@ -0,0 +1,128 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+
+int main(int argc, char** argv)
+{
+  if (argc != 2) {
+    fprintf(stderr, "Usage: crashinject <PID>\n");
+    return 1;
+  }
+  
+  int pid = atoi(argv[1]);
+  if (pid <= 0) {
+    fprintf(stderr, "Usage: crashinject <PID>\n");
+    return 1;
+  }
+
+  
+  wchar_t filename[_MAX_PATH];
+  if (GetModuleFileNameW(NULL, filename, sizeof(filename) / sizeof(wchar_t)) == 0)
+    return 1;
+
+  wchar_t* slash = wcsrchr(filename, L'\\');
+  if (slash == NULL)
+    return 1;
+
+  slash++;
+  wcscpy(slash, L"crashinjectdll.dll");
+
+  
+  HANDLE targetProc = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_CREATE_THREAD,
+                                  FALSE,
+                                  pid);
+  if (targetProc == NULL) {
+    fprintf(stderr, "Error %d opening target process\n", GetLastError());
+    return 1;
+  }
+
+  
+
+
+
+
+
+
+
+
+
+  HMODULE hKernel32 = GetModuleHandleW(L"Kernel32");
+  
+  void*   pLibRemote = VirtualAllocEx(targetProc, NULL, sizeof(filename),
+                                      MEM_COMMIT, PAGE_READWRITE);
+  if (pLibRemote == NULL) {
+    fprintf(stderr, "Error %d in VirtualAllocEx\n", GetLastError());
+    CloseHandle(targetProc);
+    return 1;
+  }
+
+  if (!WriteProcessMemory(targetProc, pLibRemote, (void*)filename,
+                          sizeof(filename), NULL)) {
+    fprintf(stderr, "Error %d in WriteProcessMemory\n", GetLastError());
+    VirtualFreeEx(targetProc, pLibRemote, sizeof(filename), MEM_RELEASE);
+    CloseHandle(targetProc);
+    return 1;
+  }
+  
+  HANDLE hThread = CreateRemoteThread(
+                     targetProc, NULL, 0,
+                     (LPTHREAD_START_ROUTINE)GetProcAddress(hKernel32,
+                                                            "LoadLibraryW"),
+                     pLibRemote, 0, NULL);
+  if (hThread == NULL) {
+    fprintf(stderr, "Error %d in CreateRemoteThread\n", GetLastError());
+    VirtualFreeEx(targetProc, pLibRemote, sizeof(filename), MEM_RELEASE);
+    CloseHandle(targetProc);
+    return 1;
+  }
+  WaitForSingleObject(hThread, INFINITE);
+  
+  CloseHandle(hThread);
+  VirtualFreeEx(targetProc, pLibRemote, sizeof(filename), MEM_RELEASE);
+  CloseHandle(targetProc);
+  
+  return 0;
+}
---
+++ build/win32/crashinjectdll/crashinjectdll.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <Windows.h>
+
+
+DWORD tid = -1;
+
+DWORD WINAPI CrashingThread(
+  LPVOID lpParameter
+)
+{
+  
+  volatile int* x = (int *)0x0;
+  *x = 1;
+  return 0;
+}
+
+BOOL WINAPI DllMain(
+  HANDLE hinstDLL, 
+  DWORD dwReason, 
+  LPVOID lpvReserved
+)
+{
+  if (tid == -1)
+    
+    
+    CreateThread( 
+                 NULL,                   
+                 0,                      
+                 CrashingThread  ,       
+                 NULL,                   
+                 0,                      
+                 &tid);                  
+  return TRUE;
+}
