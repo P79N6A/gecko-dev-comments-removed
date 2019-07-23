02aef8f0e75c02cab49a7ee0c5d2d9fa440fb344
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:51:50 +0000

    Part 1 of fix for bug 379718 (using trace API for reference counts) and bug 386912 (cycle collector faults after tracing "JS object but unknown to the JS GC"). r=igor/jst, sr=jst, a=blocking1.9+/M9 (for bug 386912).
    UltraBlame original commit: a363325649d920a3203129c75443120e6263f48e

--- js/src/xpconnect/src/nsXPConnect.cpp
+++ js/src/xpconnect/src/nsXPConnect.cpp
@@ -697,13 +697,28 @@ static uint8 GCTypeToTraceKindMap[GCX_NTYPES] = {
     JSTRACE_OBJECT,     
     JSTRACE_STRING,     
     JSTRACE_DOUBLE,     
-    JSTRACE_STRING,     
     JSTRACE_FUNCTION,   
     JSTRACE_NAMESPACE,  
     JSTRACE_QNAME,      
-    JSTRACE_XML         
-    
+    JSTRACE_XML,        
+    (uint8)-1,         
+    JSTRACE_STRING,     
+    JSTRACE_STRING,     
+    JSTRACE_STRING,     
+    JSTRACE_STRING,     
+    JSTRACE_STRING,     
+    JSTRACE_STRING,     
+    JSTRACE_STRING,     
+    JSTRACE_STRING,     
 };
+
+
+uint8
+nsXPConnect::GetTraceKind(void *thing)
+{
+    uint8 type = *js_GetGCThingFlags(thing) & GCF_TYPEMASK;
+    return GCTypeToTraceKindMap[type];
+}
 
 NS_IMETHODIMP
 nsXPConnect::Traverse(void *p, nsCycleCollectionTraversalCallback &cb)
@@ -716,7 +731,7 @@ nsXPConnect::Traverse(void *p, nsCycleCollectionTraversalCallback &cb)
     PRUint32 refcount = mObjRefcounts->Get(p);
     NS_ASSERTION(refcount > 0, "JS object but unknown to the JS GC?");
 
-    uint8 ty = *js_GetGCThingFlags(p) & GCF_TYPEMASK;
+    uint8 ty = GetTraceKind(p);
     if(ty != GCX_OBJECT && ty != GCX_NAMESPACE && ty != GCX_QNAME &&
        ty != GCX_XML)
         return NS_OK;
@@ -2095,6 +2110,18 @@ nsXPConnect::OnDispatchedEvent(nsIThreadInternal* aThread)
 {
     NS_NOTREACHED("Why tell us?");
     return NS_ERROR_UNEXPECTED;
+}
+
+NS_IMETHODIMP
+nsXPConnect::AddJSHolder(void* aHolder, nsScriptObjectTracer* aTracer)
+{
+    return mRuntime->AddJSHolder(aHolder, aTracer);
+}
+
+NS_IMETHODIMP
+nsXPConnect::RemoveJSHolder(void* aHolder)
+{
+    return mRuntime->RemoveJSHolder(aHolder);
 }
 
 #ifdef DEBUG
