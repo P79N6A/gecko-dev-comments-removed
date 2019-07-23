2415cd1ab2bfb4707348821fb70fc69674accd86
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:27:19 +0000

    Bug 553359. Cache whether the PresContext is chrome or not. r=bzbarsky
    UltraBlame original commit: ae2093359899b9db68d3343fc9420cd770746098

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -2415,6 +2415,12 @@ nsDocShell::SetItemType(PRInt32 aItemType)
     
     mAllowAuth = mItemType == typeContent; 
 
+    nsRefPtr<nsPresContext> presContext = nsnull;
+    GetPresContext(getter_AddRefs(presContext));
+    if (presContext) {
+        presContext->UpdateIsChromeCache();
+    }
+
     return NS_OK;
 }
 
