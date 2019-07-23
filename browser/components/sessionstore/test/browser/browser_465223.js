34effccc342bf26de3bd24d21bbbd731e2e3942c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:54:16 +0000

    Count open browser windows for bug 528776
    UltraBlame original commit: f2c89a3da712791925a62778cac82b0d0bf0280e

--- browser/components/sessionstore/test/browser/browser_465223.js
+++ browser/components/sessionstore/test/browser/browser_465223.js
@@ -34,8 +34,21 @@
 
 
 
+function browserWindowsCount() {
+  let count = 0;
+  let e = Cc["@mozilla.org/appshell/window-mediator;1"]
+            .getService(Ci.nsIWindowMediator)
+            .getEnumerator("navigator:browser");
+  while (e.hasMoreElements()) {
+    if (!e.getNext().closed)
+      ++count;
+  }
+  return count;
+}
+
 function test() {
   
+  is(browserWindowsCount(), 1, "Only one browser window should be open initially");
   
   
   let ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
@@ -49,7 +62,6 @@ function test() {
   
   let newWin = openDialog(location, "_blank", "chrome,all,dialog=no");
   newWin.addEventListener("load", function(aEvent) {
-    this.removeEventListener("load", arguments.callee, false);
     ss.setWindowValue(newWin, uniqueKey1, uniqueValue1);
     
     let newState = { windows: [{ tabs:[{ entries: [] }], extData: {} }] };
@@ -75,6 +87,7 @@ function test() {
     
     
     newWin.close();
+    is(browserWindowsCount(), 1, "Only one browser window should be open eventually");
     finish();
   }, false);
 }
--- browser/components/sessionstore/test/browser/browser_480148.js
+++ browser/components/sessionstore/test/browser/browser_480148.js
@@ -34,8 +34,24 @@
 
 
 
+function browserWindowsCount() {
+  let count = 0;
+  let e = Cc["@mozilla.org/appshell/window-mediator;1"]
+            .getService(Ci.nsIWindowMediator)
+            .getEnumerator("navigator:browser");
+  while (e.hasMoreElements()) {
+    if (!e.getNext().closed)
+      ++count;
+  }
+  return count;
+}
+
 function test() {
   
+  is(browserWindowsCount(), 1, "Only one browser window should be open initially");
+
+  let ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
+  waitForExplicitFinish();
 
   
   function buildTestState(num, selected) {
@@ -64,10 +80,6 @@ function test() {
     }
     return expected;
   }
-
-  
-  let ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
-  waitForExplicitFinish();
 
   
   let numTests = 4;
@@ -111,6 +123,7 @@ function test() {
         if (++completedTests == numTests) {
           this.window.removeEventListener("load", this, false);
           this.window.removeEventListener("SSTabRestoring", this, false);
+          is(browserWindowsCount(), 1, "Only one browser window should be open eventually");
           finish();
         }
       },
