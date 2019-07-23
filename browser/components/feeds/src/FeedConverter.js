69f204368fa170fcb5399ac1b6055ce34bb137dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:56:15 +0000

    Bug 355473 - Make FeedProtocolHandler.newChannel create a new real URI instead of modifying the caller's URI, and hack around bug 408599 by refusing in newURI to create feed URIs that will cause newChannel to create a channel with a spectacularly bad URI, r=sayrer, sr=bz, a=blocker
    UltraBlame original commit: 5ec559d43ebfe3eaee5659e8759025b559f9eeb8

--- browser/components/feeds/src/FeedConverter.js
+++ browser/components/feeds/src/FeedConverter.js
@@ -464,7 +464,6 @@ var FeedResultService = {
 
 
 
-
 function FeedProtocolHandler(scheme) {
   this._scheme = scheme;
   var ios = 
@@ -491,6 +490,23 @@ FeedProtocolHandler.prototype = {
   },
   
   newURI: function FPH_newURI(spec, originalCharset, baseURI) {
+    
+    
+    
+    
+    
+    
+
+    const feedSlashes = "feed:
+    const feedHttpSlashes = "feed:http:
+    const feedHttpsSlashes = "feed:https:
+    const NS_ERROR_MALFORMED_URI = 0x804B000A;
+
+    if (spec.substr(0, feedSlashes.length) != feedSlashes &&
+        spec.substr(0, feedHttpSlashes.length) != feedHttpSlashes &&
+        spec.substr(0, feedHttpsSlashes.length) != feedHttpsSlashes)
+      throw NS_ERROR_MALFORMED_URI;
+
     var uri = 
         Cc["@mozilla.org/network/standard-url;1"].
         createInstance(Ci.nsIStandardURL);
@@ -499,26 +515,28 @@ FeedProtocolHandler.prototype = {
     return uri;
   },
   
-  newChannel: function FPH_newChannel(uri) {
+  newChannel: function FPH_newChannel(aUri) {
     var ios = 
         Cc["@mozilla.org/network/io-service;1"].
         getService(Ci.nsIIOService);
     
     
+    var feedSpec = aUri.spec;
     const httpsChunk = "feed:
     const httpChunk = "feed:
-    if (uri.spec.substr(0, httpsChunk.length) == httpsChunk)
-      uri.spec = "https:
-    else if (uri.spec.substr(0, httpChunk.length) == httpChunk)
-      uri.spec = "http:
+    if (feedSpec.substr(0, httpsChunk.length) == httpsChunk)
+      feedSpec = "https:
+    else if (feedSpec.substr(0, httpChunk.length) == httpChunk)
+      feedSpec = "http:
     else
-      uri.scheme = "http";
+      feedSpec = feedSpec.replace(/^feed/, "http");
 
+    var uri = ios.newURI(feedSpec, aUri.originCharset, null);
     var channel =
       ios.newChannelFromURI(uri, null).QueryInterface(Ci.nsIHttpChannel);
     
     channel.setRequestHeader("X-Moz-Is-Feed", "1", false);
-    channel.originalURI = uri;
+    channel.originalURI = aUri;
     return channel;
   },
   
---
+++ browser/components/feeds/test/unit/test_355473.js
@@ -0,0 +1,49 @@
+const NS_ERROR_MALFORMED_URI = 0x804B000A;
+
+function run_test() {
+  var feedFeedURI = ios.newURI("feed://example.com/feed.xml", null, null);
+  var httpFeedURI = ios.newURI("feed:http://example.com/feed.xml", null, null);
+  var httpURI = ios.newURI("http://example.com/feed.xml", null, null);
+
+  var httpsFeedURI =
+    ios.newURI("feed:https://example.com/feed.xml", null, null);
+  var httpsURI = ios.newURI("https://example.com/feed.xml", null, null);
+
+  var feedChannel = ios.newChannelFromURI(feedFeedURI, null);
+  var httpChannel = ios.newChannelFromURI(httpFeedURI, null);
+  var httpsChannel = ios.newChannelFromURI(httpsFeedURI, null);
+
+  
+  do_check_true(feedFeedURI.equals(feedChannel.originalURI));
+  do_check_true(httpFeedURI.equals(httpChannel.originalURI));
+  do_check_true(httpsFeedURI.equals(httpsChannel.originalURI));
+
+  
+  do_check_true(httpURI.equals(feedChannel.URI));
+  do_check_true(httpURI.equals(httpChannel.URI));
+  do_check_true(httpsURI.equals(httpsChannel.URI));
+
+  
+  
+  try {
+    var dataFeedURI = ios.newURI("feed:data:text/xml,<rss/>", null, null);
+    do_throw("Not reached");
+  } catch (e if (e instanceof Ci.nsIException &&
+                 e.result == NS_ERROR_MALFORMED_URI)) {
+    
+  }
+  try {
+    var ftpFeedURI = ios.newURI("feed:ftp://example.com/feed.xml", null, null);
+    do_throw("Not reached");
+  } catch (e if (e instanceof Ci.nsIException &&
+                 e.result == NS_ERROR_MALFORMED_URI)) {
+    
+  }
+  try {
+    var fileFeedURI = ios.newURI("feed:file:///var/feed.xml", null, null);
+    do_throw("Not reached");
+  } catch (e if (e instanceof Ci.nsIException &&
+                 e.result == NS_ERROR_MALFORMED_URI)) {
+    
+  }
+}
