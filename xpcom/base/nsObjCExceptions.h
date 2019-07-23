d309e0754971c278032ef9c4af8301ecb9ddd196
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:13:01 +0000

    Bug 430718 - "Better objc exception logging with stack traces in debug builds". r=josh, sr=roc. a=DEBUG-only change.
    UltraBlame original commit: d9eef5ca9ae5d02f52257bad5d72a766fd9fc174

--- xpcom/base/nsObjCExceptions.h
+++ xpcom/base/nsObjCExceptions.h
@@ -40,8 +40,12 @@
 #ifndef nsObjCExceptions_h_
 #define nsObjCExceptions_h_
 
-#import <Foundation/NSException.h>
-#import <Foundation/NSObjCRuntime.h>
+#import <Foundation/Foundation.h>
+
+#ifdef DEBUG
+#import <ExceptionHandling/NSExceptionHandler.h>
+#endif
+
 #include <unistd.h>
 #include <signal.h>
 #include "nsError.h"
@@ -49,9 +53,80 @@
 
 
 
-static void nsObjCExceptionLog(NSException *e)
+static void nsObjCExceptionLog(NSException* aException)
 {
-  NSLog(@"%@: %@", [e name], [e reason]);
+  NSLog(@"Mozilla has caught an Obj-C exception [%@: %@]",
+        [aException name], [aException reason]);
+
+#ifdef DEBUG
+  @try {
+    
+    
+    NSArray *stackTrace = nil;
+    if ([aException respondsToSelector:@selector(callStackReturnAddresses)]) {
+      NSArray* addresses = (NSArray*)
+        [aException performSelector:@selector(callStackReturnAddresses)];
+      if ([addresses count])
+        stackTrace = addresses;
+    }
+
+    
+    
+    if (!stackTrace)
+      stackTrace = [[aException userInfo] objectForKey:NSStackTraceKey];
+
+    if (stackTrace) {
+      
+      
+      NSMutableArray *args =
+        [NSMutableArray arrayWithCapacity:[stackTrace count] + 3];
+
+      [args addObject:@"-p"];
+      int pid = [[NSProcessInfo processInfo] processIdentifier];
+      [args addObject:[NSString stringWithFormat:@"%d", pid]];
+
+      [args addObject:@"-printHeader"];
+
+      unsigned int stackCount = [stackTrace count];
+      unsigned int stackIndex = 0;
+      for (; stackIndex < stackCount; stackIndex++)
+        [args addObject:[[stackTrace objectAtIndex:stackIndex] stringValue]];
+
+      NSPipe *outPipe = [NSPipe pipe];
+
+      NSTask *task = [[NSTask alloc] init];
+      [task setLaunchPath:@"/usr/bin/atos"];
+      [task setArguments:args];
+      [task setStandardOutput:outPipe];
+      [task setStandardError:outPipe];
+
+      NSLog(@"Generating stack trace for Obj-C exception...");
+
+      
+      
+      [task launch];
+
+      [task waitUntilExit];
+      [task release];
+
+      NSData *outData =
+        [[outPipe fileHandleForReading] readDataToEndOfFile];
+      NSString *outString =
+        [[NSString alloc] initWithData:outData encoding:NSUTF8StringEncoding];
+
+      NSLog(@"Stack trace:\n%@", outString);
+
+      [outString release];
+    }
+    else {
+      NSLog(@"<No stack information available for Obj-C exception>");
+    }
+  }
+  @catch (NSException *exn) {
+    NSLog(@"Failed to generate stack trace for Obj-C exception [%@: %@]",
+          [exn name], [exn reason]);
+  }
+#endif
 }
 
 static void nsObjCExceptionAbort()
@@ -146,4 +221,4 @@ NS_OBJC_TRY(_e, )
                                                 }                               \
                                                 return _rv;
 
-#endif 
+#endif
