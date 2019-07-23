2c67f37d41ca376d8698ac8f5116701fd96fbaf6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:15 +0000

    JavaScript Test - Regression test for bug 355736, by Jesse Ruderman
    UltraBlame original commit: 188207ea1b62cd39882aba27fb674eb23495cdec

---
+++ js/tests/js1_5/extensions/regress-355736.js
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 355736;
+var summary = 'Decompilation of "[reserved]" has extra quotes';
+var actual = '';
+var expect = '';
+var f;
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  f = function() { [super] = q; };
+  expect = 'function() { [super] = q; }';
+  actual = f + '';
+  compareSource(expect, actual, summary + ': 1');
+
+  f = function() { return { get super() { } } };
+  expect = 'function() { return { get super() { } }; }';
+  actual = f + '';
+  compareSource(expect, actual, summary + ': 2');
+
+  f = function() { [goto] = a };
+  expect = 'function() { [goto] = a; }';
+  actual = f + '';
+  compareSource(expect, actual, summary + ': 3');
+
+  exitFunc ('test');
+}
