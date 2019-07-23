645aaf8fa16d59adb9f7ff002254a582ca03dd4a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:14:37 +0000

    Backed out changeset cf005233ebad (text-shadow style system changes) due to mochitest crash on Windows (which I can reproduce on Windows).
    UltraBlame original commit: b8d287406eb6b707a88760a0277556e3aa053ce0

--- layout/style/nsStyleStruct.cpp
+++ layout/style/nsStyleStruct.cpp
@@ -42,7 +42,6 @@
 
 
 
-
 #include "nsStyleStruct.h"
 #include "nsStyleConsts.h"
 #include "nsThemeConstants.h"
@@ -1545,23 +1544,7 @@ nsChangeHint nsStyleTextReset::MaxDifference()
 
 
 
-
-nsrefcnt
-nsTextShadowArray::Release()
-{
-  mRefCnt--;
-  if (mRefCnt == 0) {
-    delete this;
-    return 0;
-  }
-  return mRefCnt;
-}
-
-
-
-
-
-nsStyleText::nsStyleText(void)
+nsStyleText::nsStyleText(void) 
 { 
   mTextAlign = NS_STYLE_TEXT_ALIGN_DEFAULT;
   mTextTransform = NS_STYLE_TEXT_TRANSFORM_NONE;
@@ -1571,47 +1554,26 @@ nsStyleText::nsStyleText(void)
   mLineHeight.SetNormalValue();
   mTextIndent.SetCoordValue(0);
   mWordSpacing.SetNormalValue();
-
-  mShadowArray = nsnull;
 }
 
-nsStyleText::nsStyleText(const nsStyleText& aSource)
-  : mTextAlign(aSource.mTextAlign),
-    mTextTransform(aSource.mTextTransform),
-    mWhiteSpace(aSource.mWhiteSpace),
-    mLetterSpacing(aSource.mLetterSpacing),
-    mLineHeight(aSource.mLineHeight),
-    mTextIndent(aSource.mTextIndent),
-    mWordSpacing(aSource.mWordSpacing),
-    mShadowArray(aSource.mShadowArray)
-{ }
+nsStyleText::nsStyleText(const nsStyleText& aSource) 
+{ 
+  memcpy((nsStyleText*)this, &aSource, sizeof(nsStyleText));
+}
 
 nsStyleText::~nsStyleText(void) { }
 
 nsChangeHint nsStyleText::CalcDifference(const nsStyleText& aOther) const
 {
-  if ((mTextAlign != aOther.mTextAlign) ||
-      (mTextTransform != aOther.mTextTransform) ||
-      (mWhiteSpace != aOther.mWhiteSpace) ||
-      (mLetterSpacing != aOther.mLetterSpacing) ||
-      (mLineHeight != aOther.mLineHeight) ||
-      (mTextIndent != aOther.mTextIndent) ||
-      (mWordSpacing != aOther.mWordSpacing))
-    return NS_STYLE_HINT_REFLOW;
-
-  if ((!mShadowArray && !aOther.mShadowArray) ||
-      mShadowArray == aOther.mShadowArray)
+  if ((mTextAlign == aOther.mTextAlign) &&
+      (mTextTransform == aOther.mTextTransform) &&
+      (mWhiteSpace == aOther.mWhiteSpace) &&
+      (mLetterSpacing == aOther.mLetterSpacing) &&
+      (mLineHeight == aOther.mLineHeight) &&
+      (mTextIndent == aOther.mTextIndent) &&
+      (mWordSpacing == aOther.mWordSpacing))
     return NS_STYLE_HINT_NONE;
-
-  if (!mShadowArray || !aOther.mShadowArray ||
-      (mShadowArray->Length() != aOther.mShadowArray->Length()))
-    return NS_STYLE_HINT_REFLOW;
-
-  for (PRUint32 i = 0; i < mShadowArray->Length(); ++i) {
-    if (*mShadowArray->ShadowAt(i) != *aOther.mShadowArray->ShadowAt(i))
-      return NS_STYLE_HINT_REFLOW;
-  }
-  return NS_STYLE_HINT_NONE;
+  return NS_STYLE_HINT_REFLOW;
 }
 
 #ifdef DEBUG
