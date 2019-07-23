f438630eb8a20158b40f4110ef07ce0ca08b48cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:54:20 +0000

    Fixing bug 406040. Paving the way for the new NPRuntime enabled Java plugin, making Firefox able to find the new plugin when installed alongside the old plugin. Patch by kenneth.russell@sun.com, r=robert.bugzilla@gmail.com, sr=jst@mozilla.org
    UltraBlame original commit: d5651717051b8896bfd1c90906fda08d287259e6

--- modules/plugin/base/src/nsPluginDirServiceProvider.cpp
+++ modules/plugin/base/src/nsPluginDirServiceProvider.cpp
@@ -194,6 +194,37 @@ CompareVersion(verBlock vbVersionOld, verBlock vbVersionNew)
 
 
 
+static PRBool
+TryToUseNPRuntimeJavaPlugIn(const char* javaVersion)
+{
+  HKEY javaKey = NULL;
+  char keyName[_MAX_PATH];
+  keyName[0] = 0;
+  PL_strcat(keyName, "Software\\JavaSoft\\Java Plug-in\\");
+  PL_strcat(keyName, javaVersion);
+  DWORD val;
+  DWORD valSize = sizeof(DWORD);
+    
+  if (ERROR_SUCCESS != ::RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+                                      keyName, 0, KEY_READ, &javaKey)) {
+    return FALSE;
+  }
+
+  
+  if (ERROR_SUCCESS != ::RegQueryValueEx(javaKey, "UseNewJavaPlugin",
+                                         NULL, NULL,
+                                         (LPBYTE) &val,
+                                         &valSize)) {
+    val = 0;
+  }
+
+  ::RegCloseKey(javaKey);
+  return (val == 0) ? PR_FALSE : PR_TRUE;
+}
+
+
+
+
 
 
 nsPluginDirServiceProvider::nsPluginDirServiceProvider()
@@ -300,9 +331,12 @@ nsPluginDirServiceProvider::GetFile(const char *prop, PRBool *persistant,
     char curKey[_MAX_PATH] = "Software\\JavaSoft\\Java Runtime Environment";
     char path[_MAX_PATH];
     
-    char newestPath[_MAX_PATH + 4];
+    
+#define JAVA_PATH_SIZE _MAX_PATH + 15
+    char newestPath[JAVA_PATH_SIZE];
     const char mozPath[_MAX_PATH] = "Software\\mozilla.org\\Mozilla";
     char browserJavaVersion[_MAX_PATH];
+    PRBool tryNPRuntimeJavaPlugIn = PR_FALSE;
 
     newestPath[0] = 0;
     LONG result = ::RegOpenKeyEx(HKEY_LOCAL_MACHINE, curKey, 0, KEY_READ,
@@ -348,6 +382,7 @@ nsPluginDirServiceProvider::GetFile(const char *prop, PRBool *persistant,
             if (CompareVersion(curVer, minVer) >= 0) {
               if (!strncmp(browserJavaVersion, curKey, _MAX_PATH)) {
                 PL_strcpy(newestPath, path);
+                tryNPRuntimeJavaPlugIn = TryToUseNPRuntimeJavaPlugIn(curKey);
                 ::RegCloseKey(keyloc);
                 break;
               }
@@ -355,6 +390,7 @@ nsPluginDirServiceProvider::GetFile(const char *prop, PRBool *persistant,
               if (CompareVersion(curVer, maxVer) >= 0) {
                 PL_strcpy(newestPath, path);
                 CopyVersion(&maxVer, &curVer);
+                tryNPRuntimeJavaPlugIn = TryToUseNPRuntimeJavaPlugIn(curKey);
               }
             }
           }
@@ -382,6 +418,35 @@ nsPluginDirServiceProvider::GetFile(const char *prop, PRBool *persistant,
       }
 
       PL_strcat(newestPath,"\\bin");
+
+      
+      
+      
+      
+      
+      
+      if (tryNPRuntimeJavaPlugIn) {
+        
+        char tmpPath[JAVA_PATH_SIZE];
+        PL_strcpy(tmpPath, newestPath);
+        PL_strcat(tmpPath, "\\new_plugin");
+        nsCOMPtr<nsILocalFile> tmpFile;
+        if (NS_SUCCEEDED(NS_NewNativeLocalFile(nsDependentCString(tmpPath),
+                                               PR_TRUE,
+                                               getter_AddRefs(tmpFile))) &&
+            tmpFile) {
+          PRBool exists = PR_FALSE;
+          PRBool isDir = PR_FALSE;
+          if (NS_SUCCEEDED(tmpFile->Exists(&exists)) && exists &&
+              NS_SUCCEEDED(tmpFile->IsDirectory(&isDir)) && isDir) {
+            
+            
+            
+            PL_strcpy(newestPath, tmpPath);
+          }
+        }
+      }
+
       rv = NS_NewNativeLocalFile(nsDependentCString(newestPath), PR_TRUE,
                                  getter_AddRefs(localFile));
     }
