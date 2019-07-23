c0fc55ec0378d3e546a4836af10245e255af5340
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:28:21 +0000

    Bug 504221 part 2.  Switch ReparentFrameViewList to nsFrameList.  r+sr=roc
    UltraBlame original commit: ae178a6cd27678851b2b2808174933513e36fc27

--- layout/generic/nsHTMLContainerFrame.h
+++ layout/generic/nsHTMLContainerFrame.h
@@ -96,10 +96,10 @@ public:
                                     nsIFrame*       aOldParentFrame,
                                     nsIFrame*       aNewParentFrame);
 
-  static nsresult ReparentFrameViewList(nsPresContext* aPresContext,
-                                        nsIFrame*       aChildFrameList,
-                                        nsIFrame*       aOldParentFrame,
-                                        nsIFrame*       aNewParentFrame);
+  static nsresult ReparentFrameViewList(nsPresContext*     aPresContext,
+                                        const nsFrameList& aChildFrameList,
+                                        nsIFrame*          aOldParentFrame,
+                                        nsIFrame*          aNewParentFrame);
 
   
 
