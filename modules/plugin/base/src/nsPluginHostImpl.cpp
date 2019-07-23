6c3831de0eb748e384c811b28d334c97a1b33b30
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:10:12 +0000

    Bug 424226 Immediate Fx crash when starting venkman [@ nsPluginHostImpl::TrySetUpPluginInstance] r=jst sr=jsr a=shaver
    UltraBlame original commit: 190cbddca7f4562c291f4c213cd0695e466bb91c

--- modules/plugin/base/src/nsPluginHostImpl.cpp
+++ modules/plugin/base/src/nsPluginHostImpl.cpp
@@ -3964,7 +3964,7 @@ nsPluginHostImpl::TrySetUpPluginInstance(const char *aMimeType,
   
   
   nsPluginTag* pluginTag = FindPluginForType(aMimeType, PR_TRUE);
-  if (aURL && !pluginTag) {
+  if (!pluginTag) {
     nsCOMPtr<nsIURL> url = do_QueryInterface(aURL);
     if (!url) return NS_ERROR_FAILURE;
 
