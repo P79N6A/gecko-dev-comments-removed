6d3b4567fd9dad25060bc0b67c9d800a8cf5bb33
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:49:30 +0000

    Backing out bug 389188 due to potential private bytes increase on talos
    UltraBlame original commit: cce81ee429824cd1c7f6a26905818b25c626f52d

--- modules/libpr0n/src/imgRequestProxy.cpp
+++ modules/libpr0n/src/imgRequestProxy.cpp
@@ -343,15 +343,6 @@ NS_IMETHODIMP imgRequestProxy::Clone(imgIDecoderObserver* aObserver,
 }
 
 
-NS_IMETHODIMP imgRequestProxy::GetImagePrincipal(nsIPrincipal **aPrincipal)
-{
-  if (!mOwner)
-    return NS_ERROR_FAILURE;
-
-  return mOwner->GetPrincipal(aPrincipal);
-}
-
-
 
 NS_IMETHODIMP imgRequestProxy::GetPriority(PRInt32 *priority)
 {
