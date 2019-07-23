1b14541429b517f65e77f30d12439a269250e72d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:40:01 +0000

    Relanding bug 378987. Don't try to collect windows, documents or elements of actively viewed pages. r=bz/jst sr=jst
    UltraBlame original commit: 900a0a4c8df59d5baa174ec943a20460c22ceb63

--- content/base/public/nsINode.h
+++ content/base/public/nsINode.h
@@ -83,10 +83,15 @@ class nsNodeSupportsWeakRefTearoff;
 #define NODE_MAY_HAVE_FRAME          0x00000020U
 
 
-#define NODE_SCRIPT_TYPE_OFFSET                6
 
 
-#define NODE_TYPE_SPECIFIC_BITS_OFFSET       0x0a
+#define NODE_HAS_FAKED_INDOC         0x00000040U
+
+
+#define NODE_SCRIPT_TYPE_OFFSET                7
+
+
+#define NODE_TYPE_SPECIFIC_BITS_OFFSET       0x0b
 
 
 
