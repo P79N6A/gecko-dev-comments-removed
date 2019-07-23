b7874c74eb88e97f7a889913315b15712d6540e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:03:28 +0000

    Bug 459446 - Trace JSOP_POPV. r=brendan.
    UltraBlame original commit: eaf0d0a2b29229f5480f2b3596a1bdf3eca577f9

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -4201,8 +4201,17 @@ TraceRecorder::record_JSOP_PUSH()
 bool
 TraceRecorder::record_JSOP_POPV()
 {
+    jsval& rval = stackval(-1);
+    LIns *rval_ins = get(&rval);
+    if (!box_jsval(rval, rval_ins))
+        return false;
+
     
-    return false;
+    
+    
+    LIns *fp_ins = lir->insLoad(LIR_ldp, cx_ins, offsetof(JSContext, fp));
+    lir->insStorei(rval_ins, fp_ins, offsetof(JSStackFrame, rval));
+    return true;
 }
 
 bool TraceRecorder::record_JSOP_ENTERWITH()
