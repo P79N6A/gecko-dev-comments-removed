a099133305bdcc6046aadab6cfd2d57767fed521
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:14:32 +0000

    Bug 489415. Handle media streams with data from different principals in the media cache. r=bzbarsky,doublec
    UltraBlame original commit: 1721b571672d940497809bceb3f8ba253833bde6

--- content/media/video/public/nsMediaStream.h
+++ content/media/video/public/nsMediaStream.h
@@ -44,6 +44,8 @@
 #include "nsIPrincipal.h"
 #include "nsIURI.h"
 #include "nsIStreamListener.h"
+#include "nsIChannelEventSink.h"
+#include "nsIInterfaceRequestor.h"
 #include "prlock.h"
 #include "nsMediaCache.h"
 #include "nsTimeStamp.h"
@@ -150,8 +152,6 @@ public:
 
   
   
-  already_AddRefed<nsIPrincipal> GetCurrentPrincipal();
-  
   nsMediaDecoder* Decoder() { return mDecoder; }
   
   
@@ -161,6 +161,8 @@ public:
   virtual void Suspend() = 0;
   
   virtual void Resume() = 0;
+  
+  virtual already_AddRefed<nsIPrincipal> GetCurrentPrincipal() = 0;
 
   
   
@@ -317,6 +319,7 @@ public:
   virtual nsresult Close();
   virtual void     Suspend();
   virtual void     Resume();
+  virtual already_AddRefed<nsIPrincipal> GetCurrentPrincipal();
   
   PRBool IsClosed() const { return mCacheStream.IsClosed(); }
 
@@ -337,13 +340,18 @@ public:
   virtual PRBool  IsSuspendedByCache();
 
 protected:
-  class Listener : public nsIStreamListener {
+  class Listener : public nsIStreamListener,
+                   public nsIInterfaceRequestor,
+                   public nsIChannelEventSink
+  {
   public:
     Listener(nsMediaChannelStream* aStream) : mStream(aStream) {}
 
     NS_DECL_ISUPPORTS
     NS_DECL_NSIREQUESTOBSERVER
     NS_DECL_NSISTREAMLISTENER
+    NS_DECL_NSICHANNELEVENTSINK
+    NS_DECL_NSIINTERFACEREQUESTOR
 
     void Revoke() { mStream = nsnull; }
 
@@ -358,10 +366,12 @@ protected:
   nsresult OnDataAvailable(nsIRequest* aRequest,
                            nsIInputStream* aStream,
                            PRUint32 aCount);
+  nsresult OnChannelRedirect(nsIChannel* aOld, nsIChannel* aNew, PRUint32 aFlags);
 
   
   
   nsresult OpenChannel(nsIStreamListener** aStreamListener, PRInt64 aOffset);
+  void SetupChannelHeaders();
   
   void CloseChannel();
 
@@ -373,9 +383,9 @@ protected:
                                       PRUint32 *aWriteCount);
 
   
+  PRInt64            mLastSeekOffset;
   nsRefPtr<Listener> mListener;
   PRUint32           mSuspendCount;
-  PRPackedBool       mSeeking;
 
   
   nsMediaCacheStream mCacheStream;
