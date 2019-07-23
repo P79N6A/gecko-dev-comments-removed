f184b3cef8d822b81239f1c88b62e15c05e9f01a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:55:54 +0000

    fix for bug #408790:  URIs that we exclude from history (like about:blank, data:, etc) will appear visited if we check immediately after visiting them. r=dietrich, a=blocking-firefox-3+
    UltraBlame original commit: bb5d519047004444ca0e5fc1d46e55fa0b9008e2

--- toolkit/components/places/tests/unit/test_isvisited.js
+++ toolkit/components/places/tests/unit/test_isvisited.js
@@ -79,9 +79,17 @@ function run_test() {
   
   
   
-  
-  
-  
-  
-  
+  var urlsToIgnore = ["about:config", 
+    "data:,Hello%2C%20World!",
+    "imap://cyrus.andrew.cmu.edu/archive.imap",
+    "news://news.mozilla.org/mozilla.dev.apps.firefox",
+    "moz-anno:favicon:http://www.mozilla.org/2005/made-up-favicon/84-1321",
+    "chrome://browser/content/browser.xul",
+    "view-source:http://www.google.com/"];
+
+  for each (var currentURL in urlsToIgnore) {
+    var cantAddUri = uri(currentURL);
+    add_uri_to_history(cantAddUri);
+    do_check_false(gh.isVisited(cantAddUri));
+  }
 }
