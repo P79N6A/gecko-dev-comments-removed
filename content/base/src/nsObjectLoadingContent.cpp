69f53aa5c0d827c7a857c8297bc5a1c0be211c0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:10:34 +0000

    bug 489988 -  Java Applets reload when print is called, r+sr=roc
    UltraBlame original commit: b90d16b200883b7af4f4abdb307556c2098cc91a

--- content/base/src/nsObjectLoadingContent.cpp
+++ content/base/src/nsObjectLoadingContent.cpp
@@ -134,14 +134,14 @@ nsAsyncInstantiateEvent::Run()
   
   nsIObjectFrame* frame = mContent->
     GetExistingFrame(nsObjectLoadingContent::eFlushContent);
-#ifdef DEBUG
-  if (frame && mFrame.IsAlive()) {
-    nsIFrame* objectFrame = do_QueryFrame(frame);
-    NS_ASSERTION(objectFrame == mFrame.GetFrame(), "Wrong frame!");
+
+  nsIFrame* objectFrame = nsnull;
+  if (frame) {
+    objectFrame = do_QueryFrame(frame);
   }
-#endif
-  if (frame &&
-      mFrame.IsAlive() &&
+
+  if (objectFrame &&
+      mFrame.GetFrame() == objectFrame &&
       mContent->mURI == mURI &&
       mContent->mContentType.Equals(mContentType)) {
     if (LOG_ENABLED()) {
