0e20276dfd272109d452b736239f97f53cd4ce33
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:10:52 +0000

    Testcase for bug 425768
    UltraBlame original commit: cd29a3a5468e4e6e0b0a165e99de846ff606e770

---
+++ modules/libjar/zipwriter/test/unit/test_bug425768.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const DIRNAME = "test/";
+const time = Date.now();
+
+function run_test()
+{
+  
+  var source = do_get_file("modules/libjar/zipwriter/test/unit/data/test.zip");
+  source.copyTo(tmpFile.parent, tmpFile.leafName);
+
+  
+  zipW.open(tmpFile, PR_RDWR | PR_APPEND);
+  zipW.addEntryDirectory(DIRNAME, time * PR_USEC_PER_MSEC, false);
+  do_check_true(zipW.hasEntry(DIRNAME));
+  zipW.close();
+
+  var zipR = new ZipReader(tmpFile);
+  do_check_true(zipR.hasEntry(DIRNAME));
+  zipR.close();
+
+  
+  
+  var extra = ZIP_FILE_HEADER_SIZE + ZIP_CDS_HEADER_SIZE + (DIRNAME.length * 2);
+  do_check_eq(source.fileSize + extra, tmpFile.fileSize);
+}
