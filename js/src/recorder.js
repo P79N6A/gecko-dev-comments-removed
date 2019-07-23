6a9cd4254992ca7cde32af166bc143cecf7d27c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:34:46 +0000

    Expand tabs.
    UltraBlame original commit: d809377ccb1aa9a8506665a0096c87d998ae6301

--- js/src/recorder.js
+++ js/src/recorder.js
@@ -40,25 +40,25 @@
 ({
     start: function(pc) {
         print("Recording at @" + pc);
-		return true;
+        return true;
     },
     stop: function(pc) {
         print("Recording ended at @" + pc);
     },
-	
-	track: function(from, to) {
-		print("Mapped value @" + from + " to @" + to);
-		return true;
-	},
-	
-	setSP: function(sp) {
-		print("SP = @" + sp);
-		return true;
-	},
-	
-	constant: function(v, c) {
-		print("constant " + c + " -> @" + v); 
-		return true;
-	}	
+    
+    track: function(from, to) {
+        print("Mapped value @" + from + " to @" + to);
+        return true;
+    },
+    
+    setSP: function(sp) {
+        print("SP = @" + sp);
+        return true;
+    },
+    
+    constant: function(v, c) {
+        print("constant " + c + " -> @" + v); 
+        return true;
+    }   
 });
 
