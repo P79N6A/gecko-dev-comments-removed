6c5832a66d956ef709712665d33eaf230085057b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:37:09 +0000

    Bug 513233 - Implement accelerometer support for Windows ThinkPad. r=dougt
    UltraBlame original commit: 82c8c0993190aae8112614c734d89a3e52e7f482

--- widget/src/windows/nsAccelerometerWin.cpp
+++ widget/src/windows/nsAccelerometerWin.cpp
@@ -34,10 +34,12 @@
 
 
 
-#include "nsAccelerometerCE.h"
+
+#include "nsAccelerometerWin.h"
 #include "nsIServiceManager.h"
 #include "windows.h"
 
+#ifdef WINCE_WINDOWS_MOBILE
 
 
 
@@ -262,6 +264,92 @@ SMISensor::GetValues(double *x, double *y, double *z)
   *z = vector.z;
 }
 
+#endif 
+
+#if !defined(WINCE) && !defined(WINCE_WINDOWS_MOBILE) 
+
+
+
+
+
+typedef struct {
+  int status; 
+  unsigned short x; 
+  unsigned short y; 
+  unsigned short xx; 
+  unsigned short yy; 
+  char temp; 
+  unsigned short x0; 
+  unsigned short y0; 
+} ThinkPadAccelerometerData;
+
+typedef void (__stdcall *ShockproofGetAccelerometerData)(ThinkPadAccelerometerData*);
+
+ShockproofGetAccelerometerData gShockproofGetAccelerometerData = nsnull;
+
+class ThinkPadSensor : public Sensor
+{
+public:
+  ThinkPadSensor();
+  ~ThinkPadSensor();
+  PRBool Startup();
+  void Shutdown();
+  void GetValues(double *x, double *y, double *z);
+private:
+  HMODULE mLibrary;
+};
+
+ThinkPadSensor::ThinkPadSensor()
+{
+}
+
+ThinkPadSensor::~ThinkPadSensor()
+{
+}
+
+PRBool
+ThinkPadSensor::Startup()
+{
+  mLibrary = LoadLibrary("sensor.dll");
+  if (!mLibrary)
+    return PR_FALSE;
+
+  gShockproofGetAccelerometerData = (ShockproofGetAccelerometerData)
+    GetProcAddress(mLibrary, "ShockproofGetAccelerometerData");
+  if (!gShockproofGetAccelerometerData) {
+    FreeLibrary(mLibrary);
+    mLibrary = nsnull;
+    return PR_FALSE;
+  }
+  return PR_TRUE;
+}
+
+void
+ThinkPadSensor::Shutdown()
+{
+  NS_ASSERTION(mLibrary, "Shutdown called when mLibrary is null?");
+  FreeLibrary(mLibrary);
+  mLibrary = nsnull;
+  gShockproofGetAccelerometerData = nsnull;
+}
+
+void
+ThinkPadSensor::GetValues(double *x, double *y, double *z)
+{
+  ThinkPadAccelerometerData accelData;
+
+  gShockproofGetAccelerometerData(&accelData);
+
+  
+  
+  
+  *x = ((double)(accelData.y - 526)) / 144;
+  *y = ((double)(accelData.x - 528)) / 144;
+  *z = 1.0;
+}
+
+#endif
+
 nsAccelerometerWin::nsAccelerometerWin(){}
 nsAccelerometerWin::~nsAccelerometerWin(){}
 
@@ -280,9 +368,12 @@ nsAccelerometerWin::UpdateHandler(nsITimer *aTimer, void *aClosure)
 
 void nsAccelerometerWin::Startup()
 {
-  NS_ASSERTION(!mLibrary, "mLibrary should be null.  Startup called twice?");
+  NS_ASSERTION(!mSensor, "mSensor should be null.  Startup called twice?");
 
   PRBool started = PR_FALSE;
+
+#ifdef WINCE_WINDOWS_MOBILE
+
   mSensor = new SMISensor();
   if (mSensor)
     started = mSensor->Startup();
@@ -292,6 +383,16 @@ void nsAccelerometerWin::Startup()
     if (mSensor)
       started = mSensor->Startup();
   }
+
+#endif
+
+#if !defined(WINCE) && !defined(WINCE_WINDOWS_MOBILE) 
+
+  mSensor = new ThinkPadSensor();
+  if (mSensor)
+    started = mSensor->Startup();
+
+#endif
   
   if (!started)
     return;
