8d681804317bf09192873f79c4772ad012a17729
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:35:20 +0000

    Bug 4658080 -  use FUN_OBJECT(callerFrame->fun) instead of potentially cloned callerFrame->callee; p=bcrowder,r=brendan
    UltraBlame original commit: e81a7ff740bdbdfda1e0599a442d24fed38d54a9

--- js/src/jsparse.cpp
+++ js/src/jsparse.cpp
@@ -549,7 +549,7 @@ js_CompileScript(JSContext *cx, JSObject *scopeChain, JSStackFrame *callerFrame,
 
 
 
-        JSParsedObjectBox *pob = js_NewParsedObjectBox(cx, &pc, callerFrame->callee);
+        JSParsedObjectBox *pob = js_NewParsedObjectBox(cx, &pc, FUN_OBJECT(callerFrame->fun));
         pob->emitLink = cg.objectList.lastPob;
         cg.objectList.lastPob = pob;
         cg.objectList.length++;
