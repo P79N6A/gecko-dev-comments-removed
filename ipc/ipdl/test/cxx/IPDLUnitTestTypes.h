3701cf96a921a1d417631add478470d51871108c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:00:08 +0000

    bug 525172: from IPDL specs generate .cpp files with method definitions instead of everything in .h files. r=bsmedberg
    UltraBlame original commit: a006c48594102ef7eba67b126ed0fa44a3aa1c11

---
+++ ipc/ipdl/test/cxx/IPDLUnitTestTypes.h
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla__ipdltest_IPDLUnitTestTypes_h
+#define mozilla__ipdltest_IPDLUnitTestTypes_h
+
+
+namespace mozilla {
+namespace _ipdltest {
+
+struct DirtyRect
+{
+    int x; int y; int w; int h;
+};
+
+}
+}
+
+namespace IPC {
+template<>
+struct ParamTraits<mozilla::_ipdltest::DirtyRect>
+{
+    typedef mozilla::_ipdltest::DirtyRect paramType;
+    static void Write(Message* aMsg, const paramType& aParam) {
+        WriteParam(aMsg, aParam.x);
+        WriteParam(aMsg, aParam.y);
+        WriteParam(aMsg, aParam.w);
+        WriteParam(aMsg, aParam.h);
+    }
+    static bool Read(const Message* aMsg, void** aIter, paramType* aResult)
+    {
+        return (ReadParam(aMsg, aIter, &aResult->x) &&
+                ReadParam(aMsg, aIter, &aResult->y) &&
+                ReadParam(aMsg, aIter, &aResult->w) &&
+                ReadParam(aMsg, aIter, &aResult->h));
+    }
+};
+}
+
+
+#endif 
