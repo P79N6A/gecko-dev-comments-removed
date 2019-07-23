5e86bce07142c3f1f9bf0147d619c05d15e9feba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:36:42 +0000

    Bug 475078: Fix hang during "save-as-complete" by using nsTArray::Contains instead of nsTArray::IndexOf to check for array-membership. r+sr=roc a=beltzner
    UltraBlame original commit: ad84355b19bc59bb715bf392a0ac5fdd47144858

--- embedding/components/webbrowserpersist/src/nsWebBrowserPersist.cpp
+++ embedding/components/webbrowserpersist/src/nsWebBrowserPersist.cpp
@@ -1995,7 +1995,7 @@ nsWebBrowserPersist::CalculateUniqueFilename(nsIURI *aURI)
     
     
 
-    if (base.IsEmpty() || mFilenameList.Length() > 0)
+    if (base.IsEmpty() || !mFilenameList.IsEmpty())
     {
         nsCAutoString tmpPath;
         nsCAutoString tmpBase;
@@ -2031,7 +2031,7 @@ nsWebBrowserPersist::CalculateUniqueFilename(nsIURI *aURI)
             tmpPath.Append(ext);
 
             
-            if (mFilenameList.IndexOf(tmpPath) < 0)
+            if (!mFilenameList.Contains(tmpPath))
             {
                 if (!base.Equals(tmpBase))
                 {
