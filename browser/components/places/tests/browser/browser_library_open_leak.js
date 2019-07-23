115d215cc6ddacfeb4dbcac20128606a9fa37261
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:37:21 +0000

    Bug 474831 - Add a browser chrome test to check Places Library does not leak when opened and closed, r=sdwilsh
    UltraBlame original commit: d7b0a9fa09dd63db9b991f4c576cc35eba030da5

---
+++ browser/components/places/tests/browser/browser_library_open_leak.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let ww = Cc["@mozilla.org/embedcomp/window-watcher;1"].
+         getService(Ci.nsIWindowWatcher);
+
+let windowObserver = {
+  observe: function(aSubject, aTopic, aData) {
+    if (aTopic === "domwindowopened") {
+      let win = aSubject.QueryInterface(Ci.nsIDOMWindow);
+      win.addEventListener("load", function onLoad(event) {
+        win.removeEventListener("load", onLoad, false);
+        executeSoon(function () {
+          win.close();
+          finish();
+        });
+      }, false);
+    }
+  }
+};
+
+function test() {
+  waitForExplicitFinish();
+  ww.registerNotification(windowObserver);
+  ww.openWindow(null,
+                "chrome://browser/content/places/places.xul",
+                "",
+                "chrome,toolbar=yes,dialog=no,resizable",
+                null);
+}
