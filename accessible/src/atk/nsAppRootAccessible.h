334b091958a8f5ee34e6532a0e06270b1c2587b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:25 +0000

    Bug 381302 - move application accessible to cross platform code, r=aaronlev, ginn.chen, sr=neil
    UltraBlame original commit: 61de7e21eeb57a0361c4645e7075539f0345f96d

--- accessible/src/atk/nsAppRootAccessible.h
+++ accessible/src/atk/nsAppRootAccessible.h
@@ -42,59 +42,27 @@
 #ifndef __NS_APP_ROOT_ACCESSIBLE_H__
 #define __NS_APP_ROOT_ACCESSIBLE_H__
 
-#include "nsIMutableArray.h"
-#include "nsIAccessibleDocument.h"
-#include "nsAccessibilityService.h"
-#include "nsAccessibleWrap.h"
-#include "nsRootAccessibleWrap.h"
+#include "nsApplicationAccessible.h"
 
-#define MAI_TYPE_APP_ROOT (MAI_TYPE_ATK_OBJECT)
-
-
-
-
-
-
-
-
-
-class nsAppRootAccessible;
-class nsAppRootAccessible: public nsAccessibleWrap
+class nsApplicationAccessibleWrap: public nsApplicationAccessible
 {
 public:
-    virtual ~nsAppRootAccessible();
-
-    static nsAppRootAccessible *Create();
     static void Unload();
+    static void PreCreate();
 
 public:
-    nsAppRootAccessible();
+    nsApplicationAccessibleWrap();
+    virtual ~nsApplicationAccessibleWrap();
 
     
     NS_IMETHOD Init();
 
     
-    NS_IMETHOD GetName(nsAString & aName);
-    NS_IMETHOD GetDescription(nsAString & aDescription);
-    NS_IMETHOD GetRole(PRUint32 *aRole);
-    NS_IMETHOD GetFinalRole(PRUint32 *aFinalRole);
-    NS_IMETHOD GetState(PRUint32 *aState, PRUint32 *aExtraState);
-    NS_IMETHOD GetParent(nsIAccessible * *aParent);
-    NS_IMETHOD GetNextSibling(nsIAccessible * *aNextSibling);
-    NS_IMETHOD GetPreviousSibling(nsIAccessible **aPreviousSibling);
-    NS_IMETHOD GetChildAt(PRInt32 aChildNum, nsIAccessible **aChild);
-
-    
     NS_IMETHOD GetNativeInterface(void **aOutAccessible);
 
-    nsresult AddRootAccessible(nsIAccessible *aRootAccWrap);
-    nsresult RemoveRootAccessible(nsIAccessible *aRootAccWrap);
-
-protected:
-    virtual void CacheChildren();
-
-private:
-    nsCOMPtr<nsIMutableArray> mChildren;
+    
+    virtual nsresult AddRootAccessible(nsIAccessible *aRootAccWrap);
+    virtual nsresult RemoveRootAccessible(nsIAccessible *aRootAccWrap);
 };
 
 #endif   
