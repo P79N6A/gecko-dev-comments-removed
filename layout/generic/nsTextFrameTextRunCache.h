8b5a9ddc1cfa54b5498efcdb875397660db8997a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:54 +0000

    Bug 380209. Hooks for a textframe-specific textrun cache startup/shutdown. r+sr=bzbarsky
    UltraBlame original commit: 3d714d9cd9d46e1cb0b135d41bf42adc6f02f2e0

---
+++ layout/generic/nsTextFrameTextRunCache.h
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef NSTEXTFRAMETEXTRUNCACHE_H_
+#define NSTEXTFRAMETEXTRUNCACHE_H_
+
+#include "nscore.h"
+
+
+
+
+
+class nsTextFrameTextRunCache {
+public:
+  static nsresult Init();
+  static void Shutdown();
+};
+
+#endif
