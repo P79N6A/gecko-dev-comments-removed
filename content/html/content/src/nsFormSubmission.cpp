1e8f4a00f68b560990bcc14a8cce89056931a439
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:51:42 +0000

    Backing out fix for bug 116346 to fix regression bug 392982. a=dsicore@mozilla.com
    UltraBlame original commit: be10e495414796a2af7f6901f1845d0ce35e7ae8

--- content/html/content/src/nsFormSubmission.cpp
+++ content/html/content/src/nsFormSubmission.cpp
@@ -764,16 +764,13 @@ nsFSMultipartFormData::AddNameValuePair(nsIDOMHTMLElement* aSource,
   
   
   
-  
-  
-  
   mPostDataChunk += NS_LITERAL_CSTRING("--") + mBoundary
                  + NS_LITERAL_CSTRING(CRLF)
-                 + NS_LITERAL_CSTRING("Content-Disposition: form-data; name=\"")
-                 + nameStr + NS_LITERAL_CSTRING("\"" CRLF)
                  + NS_LITERAL_CSTRING("Content-Type: text/plain; charset=")
                  + mCharset
-                 + NS_LITERAL_CSTRING(CRLF CRLF)
+                 + NS_LITERAL_CSTRING(CRLF)
+                 + NS_LITERAL_CSTRING("Content-Disposition: form-data; name=\"")
+                 + nameStr + NS_LITERAL_CSTRING("\"" CRLF CRLF)
                  + valueStr + NS_LITERAL_CSTRING(CRLF);
 
   return NS_OK;
@@ -800,20 +797,9 @@ nsFSMultipartFormData::AddNameFilePair(nsIDOMHTMLElement* aSource,
                  + NS_LITERAL_CSTRING(CRLF);
   if (!mBackwardsCompatibleSubmit) {
     
-    
-    
-    
-    
-    
-    
-    
-    
     mPostDataChunk +=
           NS_LITERAL_CSTRING("Content-Transfer-Encoding: binary" CRLF);
   }
-  
-  
-  
   
   
   
