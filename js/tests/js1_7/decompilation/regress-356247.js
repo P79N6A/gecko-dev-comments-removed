806d7bab735e4e3c6978557687fd7e8e3c153e99
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:24 +0000

    JavaScript Test - regression test for bug 356247, by Jesse Ruderman
    UltraBlame original commit: 6e63513a56e0da21362e5c899b8763f5fd11ce87

---
+++ js/tests/js1_7/decompilation/regress-356247.js
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-356247.js';
+
+
+var BUGNUMBER = 356247;
+var summary = 'Decompilation of let {} = [1] in a loop';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  var f = function() { for(let x in []) let {} = [1]; };
+  expect = 'function() { for(let x in []) let [] = [1]; }';
+  actual = f + '';
+  compareSource(expect, actual, summary);
+
+  var g = eval(f + '');
+  actual = g + '';
+  compareSource(expect, actual, summary);
+
+  f = function() { while(0) let {} = [1]; };
+  expect = 'function() { while(0) let [] = [1]; }';
+  actual = f + '';
+  compareSource(expect, actual, summary);
+
+  g = eval(f + '');
+  actual = g + '';
+  compareSource(expect, actual, summary);
+
+
+  exitFunc ('test');
+}
