4faa99bed59df5996de75994512d269d3e7b530f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:58:23 +0000

    bug 531135 - rewrite build-list.pl in Python to fix race conditions with locking. r=bsmedberg
    UltraBlame original commit: 83b704c74e5f0004b7fd629a4fed31d421110bf0

---
+++ config/buildlist.py
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''A generic script to add entries to a file 
+if the entry does not already exist.
+
+Usage: buildlist.py <filename> <entry> [<entry> ...]
+'''
+
+import sys
+import os
+from utils import lockFile
+
+def addEntriesToListFile(listFile, entries):
+  """Given a file |listFile| containing one entry per line,
+  add each entry in |entries| to the file, unless it is already
+  present."""
+  lock = lockFile(listFile + ".lck")
+  try:
+    if os.path.exists(listFile):
+      f = open(listFile)
+      existing = set([x.strip() for x in f.readlines()])
+      f.close()
+    else:
+      existing = set()
+    f = open(listFile, 'a')
+    for e in entries:
+      if e not in existing:
+        f.write("%s\n" % e)
+        existing.add(e)
+    f.close()
+  finally:
+    lock = None
+
+if __name__ == '__main__':
+  if len(sys.argv) < 3:
+    print >>sys.stderr, "Usage: buildlist.py <list file> <entry> [<entry> ...]"
+    sys.exit(1)
+  addEntriesToListFile(sys.argv[1], sys.argv[2:])
