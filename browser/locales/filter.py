586767cb8b83900c799c11664b853784c34f6b02
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:49:04 +0000

    bug 395396, move exceptions from compare-locales to app/locales, r=robcee for compare-locales, r=bsmedberg for exceptions, l10n testing only
    UltraBlame original commit: 50100721f4bbc4375c37ed5a17636431cb3942ac

---
+++ browser/locales/filter.py
@@ -0,0 +1,28 @@
+def test(mod, path, entity = None):
+  import re
+  
+  if mod not in ("netwerk", "dom", "toolkit", "security/manager",
+                 "browser", "extensions/reporter", "extensions/spellcheck",
+                 "other-licenses/branding/firefox"):
+    return False
+  if mod != "browser" and mod != "extensions/spellcheck":
+    
+    return True
+  if not entity:
+    if mod == "extensions/spellcheck":
+      return False
+    
+    return not (re.match(r"searchplugins\/.+\.xml", path) or
+                re.match(r"chrome\/help\/images\/[A-Za-z-_]+\.png", path))
+  if mod == "extensions/spellcheck":
+    
+    return True
+  if path == "defines.inc":
+    return entity != "MOZ_LANGPACK_CONTRIBUTORS"
+
+  if path != "chrome/browser-region/region.properties":
+    
+    return True
+  
+  return not (re.match(r"browser\.search\.order.[1-9]", entity) or
+              re.match(r"browser\.contentHandlers\.types.[0-5]", entity))
