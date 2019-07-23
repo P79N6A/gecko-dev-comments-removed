bb2f28a7ef1eb3e2b16185197ff3fd4b16367876
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:03:33 +0000

    Re-enable sync for random oranges that could have been fixed by bug 536435.
    UltraBlame original commit: 59db8924376af36b01174b1aa0540ac5db21e465

--- browser/components/places/tests/unit/head_bookmarks.js
+++ browser/components/places/tests/unit/head_bookmarks.js
@@ -333,7 +333,6 @@ function flush_main_thread_events()
 
 
 let randomFailingSyncTests = [
-  "test_browserGlue_smartBookmarks.js",
 ];
 let currentTestFilename = do_get_file(_TEST_FILE[0], true).leafName;
 if (randomFailingSyncTests.indexOf(currentTestFilename) != -1) {
