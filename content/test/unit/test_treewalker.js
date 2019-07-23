d78c3a3bf367c67c90509c3f8432c1916dfa9207
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:00:10 +0000

    Bug 378969 - ###!!! ASSERTION: ReplaceElementAt(negative index): aIndex >= 0, file nsVoidArray.cpp, line 491, r=jwalden+bmo, sr=peterv, a=mtschrep
    UltraBlame original commit: 738602c570e02199b0c2c4afb4ebed3885adcc5e

---
+++ content/test/unit/test_treewalker.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function run_test()
+{
+  test_treeWalker_currentNode();
+}
+
+
+
+function test_treeWalker_currentNode()
+{
+  var XHTMLDocString = '<html xmlns="http://www.w3.org/1999/xhtml">';
+  XHTMLDocString += '<body><input/>input</body></html>';
+
+  var doc = ParseXML(XHTMLDocString);
+
+  var body = doc.getElementsByTagName("body")[0];
+  var filter = I.nsIDOMNodeFilter.SHOW_ELEMENT | I.nsIDOMNodeFilter.SHOW_TEXT;
+  var walker = doc.createTreeWalker(body, filter, null, false);
+  walker.currentNode = body.firstChild;
+  walker.nextNode();
+}
+
