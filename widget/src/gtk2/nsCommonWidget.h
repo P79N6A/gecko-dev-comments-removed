4300de4626fbb60928e14ea3194e3329396718e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:23:24 +0000

    Clamp window width to twice the X screen width (ditto for height). b=409006 r+sr=roc
    UltraBlame original commit: af0281bfafdbf6f863c153d7502ad6f01d431540

--- widget/src/gtk2/nsCommonWidget.h
+++ widget/src/gtk2/nsCommonWidget.h
@@ -102,6 +102,8 @@ public:
 
     virtual void NativeShow  (PRBool  aAction) = 0;
 
+    virtual nsSize GetSafeWindowSize(nsSize aSize) = 0;
+
     
     NS_IMETHOD         Show             (PRBool aState);
     NS_IMETHOD         Resize           (PRInt32 aWidth,
