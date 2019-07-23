a88e63b13ecc35dc53a9568aa18279b9b9b9bb09
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:27:29 +0000

    Bug 466407 - History menu often shows duplicated items, r=dietrich
    UltraBlame original commit: 80118343c779e812c63918f3128e8eb311d881e8

---
+++ toolkit/components/places/tests/sync/test_database_sync_with_specialHistoryQueries.js
@@ -0,0 +1,107 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var hs = Cc["@mozilla.org/browser/nav-history-service;1"].
+         getService(Ci.nsINavHistoryService);
+var bs = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
+         getService(Ci.nsINavBookmarksService);
+var prefs = Cc["@mozilla.org/preferences-service;1"].
+            getService(Ci.nsIPrefService).
+            getBranch("places.");
+var os = Cc["@mozilla.org/observer-service;1"].
+         getService(Ci.nsIObserverService);
+
+const TEST_URI = "http://test.com/";
+
+const kSyncPrefName = "syncDBTableIntervalInSecs";
+const SYNC_INTERVAL = 600;
+const kSyncFinished = "places-sync-finished";
+
+var observer = {
+  observe: function(aSubject, aTopic, aData) {
+    if (aTopic == kSyncFinished) {
+      
+      hs.addVisit(uri(TEST_URI), Date.now() * 1000, null,
+                  hs.TRANSITION_TYPED, false, 0);
+
+      
+      var options = hs.getNewQueryOptions();
+      options.maxResults = 10;
+      options.resultType = options.RESULTS_AS_URI;
+      options.sortingMode = options.SORT_BY_DATE_DESCENDING;
+      var query = hs.getNewQuery();
+      var result = hs.executeQuery(query, options);
+      var root = result.root;
+      root.containerOpen = true;
+      do_check_eq(root.childCount, 1);
+      root.containerOpen = false;
+
+      
+      var options = hs.getNewQueryOptions();
+      options.maxResults = 10;
+      options.resultType = options.RESULTS_AS_URI;
+      options.sortingMode = options.SORT_BY_VISITCOUNT_DESCENDING;
+      var query = hs.getNewQuery();
+      var result = hs.executeQuery(query, options);
+      var root = result.root;
+      root.containerOpen = true;
+      do_check_eq(root.childCount, 1);
+      root.containerOpen = false;
+
+      os.removeObserver(this, kSyncFinished);
+      finish_test();
+    }
+  }
+}
+os.addObserver(observer, kSyncFinished, false);
+
+function run_test()
+{
+  
+  prefs.setIntPref(kSyncPrefName, SYNC_INTERVAL);
+
+  
+  visitId = hs.addVisit(uri(TEST_URI), Date.now() * 1000, null,
+                        hs.TRANSITION_TYPED, false, 0);
+
+  
+  bs.insertBookmark(bs.toolbarFolder, uri(TEST_URI), bs.DEFAULT_INDEX, "visited");
+
+  do_test_pending();
+}
