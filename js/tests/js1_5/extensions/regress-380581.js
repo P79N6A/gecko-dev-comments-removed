47776ee5c03fa1b4fe8336670beca14e3b6c552c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:16 +0000

    JavaScript Test - regression test for bug 380581, by Jesse Ruderman
    UltraBlame original commit: 5bbd901d769dce8fb35eb86adb241f264b9116ae

---
+++ js/tests/js1_5/extensions/regress-380581.js
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-380581.js';
+
+
+var BUGNUMBER = 380581;
+var summary = 'Incorrect uneval with setter in object literal';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  expect = '({ set x () {}})';
+  actual = uneval({x setter: eval("function () { }") });
+  compareSource(expect, actual, summary);
+  
+  expect = 'function() { }';
+  actual = uneval(eval("function() { }"));
+  compareSource(expect, actual, summary);
+    
+  expect = '(function() { })';
+  actual = uneval(eval("(function() { })"));
+  compareSource(expect, actual, summary);
+
+  exitFunc ('test');
+}
