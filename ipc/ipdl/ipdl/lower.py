3257f1c5a4fe994860c994d9254515875ad7128e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:30:18 +0000

    Bug 559953: Add a |const nsTArray<Subactor*>& ManagedFoo();| accessor. r=bent
    UltraBlame original commit: e6fcd7bd5f81a69940a5e5c5927c16b0b72dd2da

--- ipc/ipdl/ipdl/lower.py
+++ ipc/ipdl/ipdl/lower.py
@@ -2801,6 +2801,7 @@ class _GenerateProtocolActorCode(ipdl.ast.Visitor):
                 self.cls.addstmts([ managermeth, Whitespace.NL ])
 
         
+        
         for managed in ptype.manages:
             arrvar = ExprVar('aArr')
             meth = MethodDefn(MethodDecl(
@@ -2810,7 +2811,15 @@ class _GenerateProtocolActorCode(ipdl.ast.Visitor):
                 const=1))
             meth.addstmt(StmtExpr(ExprAssn(
                 arrvar, p.managedVar(managed, self.side))))
-            self.cls.addstmts([ meth, Whitespace.NL ])
+
+            refmeth = MethodDefn(MethodDecl(
+                p.managedMethod(managed, self.side).name,
+                params=[ ],
+                ret=p.managedVarType(managed, self.side, const=1, ref=1),
+                const=1))
+            refmeth.addstmt(StmtReturn(p.managedVar(managed, self.side)))
+            
+            self.cls.addstmts([ meth, refmeth, Whitespace.NL ])
 
         
 
