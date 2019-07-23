0af3071d0e697789b7df7c79bfb953280e42c598
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:30:46 +0000

    Bug 552736: Initial support for plugins in the API. r=robstrong
    UltraBlame original commit: d44f759a97b42f78e7d358f8ab1724a61c226c93

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_plugins.js
@@ -0,0 +1,97 @@
+
+
+
+
+
+var gID = null;
+
+function run_test() {
+  do_test_pending();
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+  startupManager(1);
+  AddonManager.addAddonListener(AddonListener);
+  AddonManager.addInstallListener(InstallListener);
+
+  run_test_1();
+}
+
+
+function run_test_1() {
+  AddonManager.getAddonsByTypes("plugin", function(addons) {
+    do_check_true(addons.length > 0);
+
+    addons.forEach(function(p) {
+      if (p.name == "Test Plug-in")
+        gID = p.id;
+    });
+
+    do_check_neq(gID, null);
+
+    AddonManager.getAddon(gID, function(p) {
+      do_check_neq(p, null)
+      do_check_false(p.userDisabled);
+      do_check_false(p.appDisabled);
+      do_check_true(p.isActive);
+      do_check_eq(p.name, "Test Plug-in");
+
+      run_test_2(p);
+    });
+  });
+}
+
+
+function run_test_2(p) {
+  let test = {};
+  test[gID] = [
+    ["onDisabling", false],
+    "onDisabled"
+  ];
+  prepare_test(test);
+
+  p.userDisabled = true;
+
+  ensure_test_completed();
+
+  do_check_true(p.userDisabled);
+  do_check_false(p.appDisabled);
+  do_check_false(p.isActive);
+
+  AddonManager.getAddon(gID, function(p) {
+    do_check_neq(p, null)
+    do_check_true(p.userDisabled);
+    do_check_false(p.appDisabled);
+    do_check_false(p.isActive);
+    do_check_eq(p.name, "Test Plug-in");
+
+    run_test_3(p);
+  });
+}
+
+
+function run_test_3(p) {
+  let test = {};
+  test[gID] = [
+    ["onEnabling", false],
+    "onEnabled"
+  ];
+  prepare_test(test);
+
+  p.userDisabled = false;
+
+  ensure_test_completed();
+
+  do_check_false(p.userDisabled);
+  do_check_false(p.appDisabled);
+  do_check_true(p.isActive);
+
+  AddonManager.getAddon(gID, function(p) {
+    do_check_neq(p, null)
+    do_check_false(p.userDisabled);
+    do_check_false(p.appDisabled);
+    do_check_true(p.isActive);
+    do_check_eq(p.name, "Test Plug-in");
+
+    do_test_finished();
+  });
+}
