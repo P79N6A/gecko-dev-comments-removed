1dd39542c21ad26f29dfd0ecf4aaf2673b228476
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:12:35 +0000

    b=427122, padding on select elements ignored, r=dbaron,a=beltzner
    UltraBlame original commit: 82f1d9efa6bc0de2cc9d9bcca6342563825248d6

--- widget/src/windows/nsNativeThemeWin.cpp
+++ widget/src/windows/nsNativeThemeWin.cpp
@@ -461,6 +461,15 @@ nsNativeThemeWin::GetTheme(PRUint8 aWidgetType)
   if (!mThemeDLL)
     return NULL;
 
+  if (!mIsVistaOrLater) {
+    
+    
+    
+    
+    if (aWidgetType == NS_THEME_DROPDOWN)
+      aWidgetType = NS_THEME_TEXTFIELD;
+  }
+
   switch (aWidgetType) {
     case NS_THEME_BUTTON:
     case NS_THEME_RADIO:
@@ -632,6 +641,12 @@ nsresult
 nsNativeThemeWin::GetThemePartAndState(nsIFrame* aFrame, PRUint8 aWidgetType, 
                                        PRInt32& aPart, PRInt32& aState)
 {
+  if (!mIsVistaOrLater) {
+    
+    if (aWidgetType == NS_THEME_DROPDOWN)
+      aWidgetType = NS_THEME_TEXTFIELD;
+  }
+
   switch (aWidgetType) {
     case NS_THEME_BUTTON: {
       aPart = BP_BUTTON;
@@ -1527,6 +1542,15 @@ nsNativeThemeWin::GetWidgetPadding(nsIDeviceContext* aContext,
   }
 
   if (mIsVistaOrLater) {
+    if (aWidgetType == NS_THEME_TEXTFIELD ||
+        aWidgetType == NS_THEME_TEXTFIELD_MULTILINE ||
+        aWidgetType == NS_THEME_DROPDOWN)
+    {
+      
+      if (aFrame->PresContext()->HasAuthorSpecifiedRules(aFrame, NS_AUTHOR_SPECIFIED_PADDING))
+        return PR_FALSE;
+    }
+
     
 
 
@@ -1537,13 +1561,7 @@ nsNativeThemeWin::GetWidgetPadding(nsIDeviceContext* aContext,
       aResult->top = aResult->bottom = 1;
       aResult->left = aResult->right = 1;
       return PR_TRUE;
-    }
-  }
-
-  
-  
-  if (IsHTMLContent(aFrame)) {
-    if (aWidgetType == NS_THEME_DROPDOWN) {
+    } else if (IsHTMLContent(aFrame) && aWidgetType == NS_THEME_DROPDOWN) {
       
 
 
