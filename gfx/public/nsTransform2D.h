c409a49a7a8acd104aaa568329e9edb10266224b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:03:30 +0000

    Bug 410114 - "Simplify nsTransform2D" [p=alfredkayser@gmail.com (Alfred Kayser) r+sr=roc a1.9=damons]
    UltraBlame original commit: 438f42c6c355477dd1196179f948f7c069f71a62

--- gfx/public/nsTransform2D.h
+++ gfx/public/nsTransform2D.h
@@ -35,15 +35,12 @@
 
 
 
+
 #ifndef nsTransform2D_h___
 #define nsTransform2D_h___
 
 #include "gfxCore.h"
 #include "nsCoord.h"
-
-#define MG_2DIDENTITY     0
-#define MG_2DTRANSLATION  1
-#define MG_2DSCALE        2
 
 class NS_GFX nsTransform2D
 {
@@ -61,11 +58,15 @@ private:
 
 
   float     m00, m11, m20, m21;
-  PRUint16  type;
 
 public:
-  nsTransform2D(void)                         { SetToIdentity(); }
-  nsTransform2D(nsTransform2D *aTransform2D)  { SetMatrix(aTransform2D); }
+  nsTransform2D(void)                         { m20 = m21 = 0.0f; m00 = m11 = 1.0f; }
+  nsTransform2D(nsTransform2D *aTransform2D)  {
+    m00 = aTransform2D->m00;
+    m11 = aTransform2D->m11;
+    m20 = aTransform2D->m20;
+    m21 = aTransform2D->m21;
+  }
 
   ~nsTransform2D(void)                        { }
 
@@ -76,53 +77,14 @@ public:
 
 
 
-
-
-  PRUint16 GetType(void) const                { return type; }
-
- 
-
-
-
-
-
-
-
-  void SetToIdentity(void)                    { m20 = m21 = 0.0f; m00 = m11 = 1.0f; type = MG_2DIDENTITY; }
-
- 
-
-
-
-
-
-
-
-
-  void SetToScale(float sx, float sy)        { m00 = sx; m11 = sy; m20 = m21 = 0.0f; type = MG_2DSCALE; }
+  void SetToTranslate(float tx, float ty)    { m00 = m11 = 1.0f; m20 = tx; m21 = ty; }
   
-
  
 
 
 
 
 
-
-
-
-  void SetToTranslate(float tx, float ty)    { m00 = m11 = 1.0f; m20 = tx; m21 = ty; type = MG_2DTRANSLATION; }
-  
-
- 
-
-
-
-
-
-
-
-  void GetTranslation(float *ptX, float *ptY) const { *ptX = m20; *ptY = m21; }
   void GetTranslationCoord(nscoord *ptX, nscoord *ptY) const { *ptX = NSToCoordRound(m20); *ptY = NSToCoordRound(m21); }
 
  
@@ -131,86 +93,6 @@ public:
 
 
 
-
-
-  void SetTranslation(float tX, float tY) {
-    m20 = tX;
-    m21 = tY;
-    type |= MG_2DTRANSLATION;
-  }
-
- 
-
-
-
-
-
-
-
-  float GetXTranslation(void)  const          { return m20; }
-  nscoord GetXTranslationCoord(void) const    { return NSToCoordRound(m20); }
-
- 
-
-
-
-
-
-
-
-  float GetYTranslation(void) const         { return m21; }
-  nscoord GetYTranslationCoord(void) const  { return NSToCoordRound(m21); }
-
- 
-
-
-
-
-
-
-
-  void SetMatrix(nsTransform2D *aTransform2D);
-
- 
-
-
-
-
-
-
-
-  void Concatenate(nsTransform2D *newxform);
-
- 
-
-
-
-
-
-
-
-  void PreConcatenate(nsTransform2D *newxform);
-
- 
-
-
-
-
-
-
-
-  void TransformNoXLate(float *ptX, float *ptY) const;
-  void TransformNoXLateCoord(nscoord *ptX, nscoord *ptY) const;
-
- 
-
-
-
-
-
-
-
-  void Transform(float *ptX, float *ptY) const;
   void TransformCoord(nscoord *ptX, nscoord *ptY) const;
 
  
@@ -219,22 +101,7 @@ public:
 
 
 
-
-
-  void Transform(float *aX, float *aY, float *aWidth, float *aHeight) const;
   void TransformCoord(nscoord *aX, nscoord *aY, nscoord *aWidth, nscoord *aHeight) const;
-  void TransformNoXLateCoord(nscoord *aX, nscoord *aY, nscoord *aWidth, nscoord *aHeight) const;
-
-  
-
-
-
-
-
-
-
-  void ScaleXCoords(const nscoord* aSrc, PRUint32 aNumCoords, PRIntn* aDst) const;
-  void ScaleYCoords(const nscoord* aSrc, PRUint32 aNumCoords, PRIntn* aDst) const;
 
  
 
@@ -243,20 +110,7 @@ public:
 
 
 
-
-
-  void AddTranslation(float ptX, float ptY);
-
- 
-
-
-
-
-
-
-
-
-  void AddScale(float ptX, float ptY);
+  void AddScale(float ptX, float ptY) { m00 *= ptX; m11 *= ptY; }
 };
 
 #endif
