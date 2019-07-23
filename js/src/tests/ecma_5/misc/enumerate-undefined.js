fd556d0ff265ec24be8c2f9b05ed662e37a5dff4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:22:29 +0000

    Bug 547087 - JS_EnumerateStandardClasses uses non-configurable, not non-configurable/non-writable, as attributes for global.undefined.  r=jorendorff
    UltraBlame original commit: 090b914be39ba5972267d489db3aa5adf0ee5544

---
+++ js/src/tests/ecma_5/misc/enumerate-undefined.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+var gTestfile = 'enumerate-undefined.js';
+
+var BUGNUMBER = 547087;
+var summary = 'JS_EnumerateStandardClasses uses wrong attributes for undefined';
+
+print(BUGNUMBER + ": " + summary);
+
+
+
+
+
+for (var p in this);
+
+assertEq(Object.getOwnPropertyDescriptor(this, "undefined").writable, false);
+
+
+
+reportCompare(true, true);
+
+print("All tests passed!");
