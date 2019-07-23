25634dc6c90b552355a806a4f0edee1c974702ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:33:31 +0000

    bug 508501 - bustage fix for Windows CE
    UltraBlame original commit: e0cc032cb8525746e5a1cee07a6ffc7f21cbc5cc

--- widget/src/windows/nsWindowCE.cpp
+++ widget/src/windows/nsWindowCE.cpp
@@ -354,11 +354,12 @@ DWORD nsWindow::WindowStyle()
 
 NS_IMETHODIMP nsWindow::SetSizeMode(PRInt32 aMode)
 {
+#if defined(WINCE_HAVE_SOFTKB)
   if (aMode == 0 && mSizeMode == 3  && nsWindowCE::sSIPInTransition) {
       
     return NS_OK;
-    
   }
+#endif
   nsresult rv;
 
   
