07dbbf02a4d66f18768c2c67df7dde59b0e9f394
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:26:11 +0000

    Bug 426806 - "Sort by Name" is slow in Places. r=dietrich.
    UltraBlame original commit: 04081367489217d21ab86a3d569fa7aea49a73cd

--- browser/components/places/src/nsPlacesTransactionsService.js
+++ browser/components/places/src/nsPlacesTransactionsService.js
@@ -898,13 +898,24 @@ placesSortFolderByNameTransactions.prototype = {
     }
 
     
-    for (var i = 0; i < count; ++i)
-      PlacesUtils.bookmarks.setItemIndex(newOrder[i].itemId, i);
+    var callback = {
+      runBatched: function(aUserData) {
+        for (var i = 0; i < newOrder.length; ++i) {
+          PlacesUtils.bookmarks.setItemIndex(newOrder[i].itemId, i);
+        }
+      }
+    };
+    PlacesUtils.bookmarks.runInBatchMode(callback, newOrder);
   },
 
   undoTransaction: function PSSFBN_undoTransaction() {
-    for (item in this._oldOrder)
-      PlacesUtils.bookmarks.setItemIndex(item, this._oldOrder[item]);
+    var callback = {
+      runBatched: function(aUserData) {
+        for (item in oldOrder)
+          PlacesUtils.bookmarks.setItemIndex(item, oldOrder[item]);
+      }
+    };
+    PlacesUtils.bookmarks.runInBatchMode(callback, this._oldOrder);
   }
 };
 
