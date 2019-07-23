2cff4c8c37aac8a979fd604218efea472807dbe6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:24:32 +0000

    Bug 458849 - transition download visits saved when "Keep my history..." is unchecked, r=sdwilsh
    UltraBlame original commit: 32a3759db50fc58a991d1c3ba2fca748143b7306

--- toolkit/components/places/tests/unit/test_download_history.js
+++ toolkit/components/places/tests/unit/test_download_history.js
@@ -38,12 +38,28 @@
 
 
 
-try {
-  var histsvc = Cc["@mozilla.org/browser/nav-history-service;1"].
-                getService(Ci.nsINavHistoryService);
-} catch(ex) {
-  do_throw("Could not get history service\n");
-} 
+
+var histsvc = Cc["@mozilla.org/browser/nav-history-service;1"].
+              getService(Ci.nsINavHistoryService);
+let bh = histsvc.QueryInterface(Ci.nsIBrowserHistory);
+var os = Cc["@mozilla.org/observer-service;1"].
+         getService(Ci.nsIObserverService);
+var prefs = Cc["@mozilla.org/preferences-service;1"].
+            getService(Ci.nsIPrefBranch);
+var dh = Cc["@mozilla.org/browser/download-history;1"].
+         getService(Ci.nsIDownloadHistory);
+
+do_check_true(dh instanceof Ci.nsINavHistoryService);
+var pb = Cc["@mozilla.org/privatebrowsing;1"].
+         getService(Ci.nsIPrivateBrowsingService);
+
+
+const NS_LINK_VISITED_EVENT_TOPIC = "link-visited";
+const DISABLE_HISTORY_PREF = "browser.history_expire_days";
+const PB_KEEP_SESSION_PREF = "browser.privatebrowsing.keep_current_session";
+
+var testURI = uri("http://google.com/");
+var referrerURI = uri("http://yahoo.com");
 
 
 
@@ -52,7 +68,8 @@ try {
 
 
 
-function uri_in_db(aURI) {
+
+function uri_in_db(aURI, aExpected) {
   var options = histsvc.getNewQueryOptions();
   options.maxResults = 1;
   options.resultType = options.RESULTS_AS_URI;
@@ -62,46 +79,78 @@ function uri_in_db(aURI) {
   var result = histsvc.executeQuery(query, options);
   var root = result.root;
   root.containerOpen = true;
-  return (root.childCount == 1);
+  var cc = root.childCount;
+  root.containerOpen = false;
+  var checker = aExpected ? do_check_true : do_check_false;
+  checker(cc == 1);
 }
+
+function test_dh() {
+  dh.addDownload(testURI, referrerURI, Date.now() * 1000);
+
+  do_check_true(observer.topicReceived);
+  uri_in_db(testURI, true);
+  uri_in_db(referrerURI, true);
+}
+
+function test_dh_privateBrowsing() {
+  prefs.setBoolPref(PB_KEEP_SESSION_PREF, true);
+  pb.privateBrowsingEnabled = true;
+
+  dh.addDownload(testURI, referrerURI, Date.now() * 1000);
+
+  do_check_false(observer.topicReceived);
+  uri_in_db(testURI, false);
+  uri_in_db(referrerURI, false);
+
+  
+  pb.privateBrowsingEnabled = false;
+}
+
+function test_dh_disabledHistory() {
+  
+  prefs.setIntPref(DISABLE_HISTORY_PREF, 0);
+
+  dh.addDownload(testURI, referrerURI, Date.now() * 1000);
+
+  do_check_false(observer.topicReceived);
+  uri_in_db(testURI, false);
+  uri_in_db(referrerURI, false);
+
+  
+  prefs.setIntPref(DISABLE_HISTORY_PREF, 180);
+}
+
+var tests = [
+  test_dh,
+  test_dh_privateBrowsing,
+  test_dh_disabledHistory,
+];
+
+var observer = {
+  topicReceived: false,
+  observe: function tlvo_observe(aSubject, aTopic, aData)
+  {
+    if (NS_LINK_VISITED_EVENT_TOPIC == aTopic) {
+      this.topicReceived = true;
+    }
+  }
+};
+os.addObserver(observer, NS_LINK_VISITED_EVENT_TOPIC, false);
 
 
 function run_test() {
-  
-  
-  var dh = Cc["@mozilla.org/browser/download-history;1"].
-           getService(Ci.nsIDownloadHistory);
-  do_check_true(dh instanceof Ci.nsINavHistoryService);
+  while (tests.length) {
+    
+    uri_in_db(testURI, false);
+    uri_in_db(referrerURI, false);
 
-  
-  const NS_LINK_VISITED_EVENT_TOPIC = "link-visited";
-  var ios = Cc["@mozilla.org/network/io-service;1"].
-            getService(Ci.nsIIOService);
-  var testURI = ios.newURI("http://google.com/", null, null);
+    (tests.shift())();
 
-  do_check_false(uri_in_db(testURI));
+    
+    bh.removeAllPages();
+    observer.topicReceived = false;
+  }
 
-  var topicReceived = false;
-  var obs = {
-    observe: function tlvo_observe(aSubject, aTopic, aData)
-    {
-      if (NS_LINK_VISITED_EVENT_TOPIC == aTopic) {
-        do_check_true(uri_in_db(testURI));
-        do_check_eq(testURI, aSubject);
-        topicReceived = true;
-      }
-    }
-  };
-
-  var os = Cc["@mozilla.org/observer-service;1"].
-           getService(Ci.nsIObserverService);
-  os.addObserver(obs, NS_LINK_VISITED_EVENT_TOPIC, false);
-
-  var referrerURI = ios.newURI("http://yahoo.com", null, null);
-  do_check_false(uri_in_db(referrerURI));
-  do_check_false(uri_in_db(testURI));
-  dh.addDownload(testURI, referrerURI, Date.now() * 1000);
-  do_check_true(topicReceived);
-  do_check_true(uri_in_db(testURI));
-  do_check_true(uri_in_db(referrerURI));
+  os.removeObserver(observer, NS_LINK_VISITED_EVENT_TOPIC);
 }
