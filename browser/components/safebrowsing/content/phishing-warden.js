bf46408858329993cb26028724454aeb42f1fff1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:03:29 +0000

    Bug 400324 - Remove old safebrowsing 'speach bubble' UI code. r=tony, a=damons
    UltraBlame original commit: 37786a1901c81b5d99a539831e7105bfcfe1ce73

--- browser/components/safebrowsing/content/phishing-warden.js
+++ browser/components/safebrowsing/content/phishing-warden.js
@@ -66,14 +66,6 @@ const kPhishWardenEnabledPref = "browser.safebrowsing.enabled";
 
 
 
-const kTestUrls = {
-  "http:
-  "http:
-  "http:
-}
-
-
-
 
 
 
@@ -84,8 +76,6 @@ function PROT_PhishingWarden() {
   PROT_ListWarden.call(this);
 
   this.debugZone = "phishwarden";
-  this.testing_ = false;
-  this.browserViews_ = [];
 
   
   this.prefs_ = new G_Preferences();
@@ -105,9 +95,6 @@ function PROT_PhishingWarden() {
 }
 
 PROT_PhishingWarden.inherits(PROT_ListWarden);
-
-
-
 
 PROT_PhishingWarden.prototype.QueryInterface = function(iid) {
   if (iid.equals(Ci.nsISupports) || 
@@ -136,9 +123,6 @@ PROT_PhishingWarden.prototype.shutdown = function() {
 
  
 PROT_PhishingWarden.prototype.maybeToggleUpdateChecking = function() {
-  if (this.testing_)
-    return;
-
   var phishWardenEnabled = this.prefs_.getPref(kPhishWardenEnabledPref, null);
 
   G_Debug(this, "Maybe toggling update checking. " +
