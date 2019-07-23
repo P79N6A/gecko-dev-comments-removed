284081a37a290d671dd1db15803ed3c41be25f4a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:46:51 +0000

    Experimental changes to randomly failing sessionstore browser tests (will backout if noting changes)
    UltraBlame original commit: af0ade2a9a1d4c105242453a12a164f37e6329cb

--- browser/components/sessionstore/test/browser/browser_459906.js
+++ browser/components/sessionstore/test/browser/browser_459906.js
@@ -56,35 +56,37 @@ function test() {
     
     frameCount = 0;
     let tab2 = gBrowser.duplicateTab(tab);
-    tab2.linkedBrowser.addEventListener("load", function(aEvent) {
-      
-      if (frameCount++ < 2)
-        return;
-      this.removeEventListener("load", arguments.callee, true);
-
-      let maxWait = Date.now() + 1000;
-      executeSoon(function() {
-        let iframes = tab2.linkedBrowser.contentWindow.frames;
-        if (iframes[1].document.body.innerHTML != uniqueValue && Date.now() < maxWait) {
-          executeSoon(arguments.callee);
+    executeSoon(function() {
+      tab2.linkedBrowser.addEventListener("load", function(aEvent) {
+        
+        if (frameCount++ < 2)
           return;
-        }
-        is(iframes[1].document.body.innerHTML, uniqueValue,
-           "rich textarea's content correctly duplicated");
-        
-        let innerDomain = null;
-        try {
-          innerDomain = iframes[0].document.domain;
-        }
-        catch (ex) {  }
-        is(innerDomain, "localhost", "XSS exploit prevented!");
-        
-        
-        gBrowser.removeTab(tab2);
-        gBrowser.removeTab(tab);
-        
-        finish();
-      });
-    }, true);
+        this.removeEventListener("load", arguments.callee, true);
+
+        let maxWait = Date.now() + 1000;
+        executeSoon(function() {
+          let iframes = tab2.linkedBrowser.contentWindow.frames;
+          if (iframes[1].document.body.innerHTML != uniqueValue && Date.now() < maxWait) {
+            executeSoon(arguments.callee);
+            return;
+          }
+          is(iframes[1].document.body.innerHTML, uniqueValue,
+             "rich textarea's content correctly duplicated");
+          
+          let innerDomain = null;
+          try {
+            innerDomain = iframes[0].document.domain;
+          }
+          catch (ex) {  }
+          is(innerDomain, "localhost", "XSS exploit prevented!");
+          
+          
+          gBrowser.removeTab(tab2);
+          gBrowser.removeTab(tab);
+          
+          finish();
+        });
+      }, true);
+    });
   }, true);
 }
