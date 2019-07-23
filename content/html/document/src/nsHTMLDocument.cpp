ae698c2e9839bb5846b767fdf7dba0ab553af97b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:53:42 +0000

    Make getElementById in HTML always return the first node in document order. Bug 403868, r+sr=sicking
    UltraBlame original commit: 6832b08c2c981033bb91b9a8816df5905759ef9a

--- content/html/document/src/nsHTMLDocument.cpp
+++ content/html/document/src/nsHTMLDocument.cpp
@@ -270,14 +270,35 @@ IdAndNameMapEntry::AddIdContent(nsIContent* aContent)
   }
 
   
-  
-  PRInt32 index = mIdContentList.IndexOf(aContent);
-  if (index != -1) {
-    
-    return PR_TRUE;
+  if (mIdContentList.Count() == 0) {
+    return mIdContentList.AppendElement(aContent);
   }
+
+  
+  
+  
+  PRInt32 start = 0;
+  PRInt32 end = mIdContentList.Count();
+  do {
+    NS_ASSERTION(start < end, "Bogus start/end");
     
-  return mIdContentList.AppendElement(aContent);
+    PRInt32 cur = (start + end) / 2;
+    NS_ASSERTION(cur >= start && cur < end, "What happened here?");
+
+    nsIContent* curContent = static_cast<nsIContent*>(mIdContentList[cur]);
+    if (curContent == aContent) {
+      
+      return PR_TRUE;
+    }
+
+    if (nsContentUtils::PositionIsBefore(aContent, curContent)) {
+      end = cur;
+    } else {
+      start = cur + 1;
+    }
+  } while (start != end);
+  
+  return mIdContentList.InsertElementAt(aContent, start);
 }
 
 
