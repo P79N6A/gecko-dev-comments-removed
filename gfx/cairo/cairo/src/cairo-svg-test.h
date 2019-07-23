c020c8fdf4e72471cd07d7e543d867943d8f61c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:45:04 +0000

    cairo backout
    UltraBlame original commit: c9c429e54cede46a44dc4e86a3cef7ac11aef635

---
+++ gfx/cairo/cairo/src/cairo-svg-test.h
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef CAIRO_SVG_TEST_H
+#define CAIRO_SVG_TEST_H
+
+#include <cairo.h>
+
+#if CAIRO_HAS_SVG_SURFACE
+
+#include <cairo-svg.h>
+
+CAIRO_BEGIN_DECLS
+
+cairo_public void
+_cairo_svg_test_force_fallbacks (void);
+
+CAIRO_END_DECLS
+
+#endif 
+#endif 
---
+++ gfx/cairo/cairo/src/cairo-xlib-test.h
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef CAIRO_XLIB_TEST_H
+#define CAIRO_XLIB_TEST_H
+
+#include <cairo.h>
+
+#if CAIRO_HAS_XLIB_SURFACE
+
+#include <cairo-xlib.h>
+
+CAIRO_BEGIN_DECLS
+
+cairo_public void
+_cairo_xlib_test_disable_render (void);
+
+CAIRO_END_DECLS
+
+#endif 
+#endif 
