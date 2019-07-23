e9314e3d6147256bd3a6bc5e3d08102cd0a9b638
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:40:18 +0000

    Bug 510856. Create tests for which areas of the page are repainted or blitted by scrolling. r=dbaron
    UltraBlame original commit: 2b23795830c8e42ee414b55c4079169b133b209f

---
+++ layout/base/tests/region_lib.js
@@ -0,0 +1,106 @@
+
+
+
+
+
+
+
+function Region(rects) {
+  this._rects = rects || [];
+}
+
+(function() {
+function dumpRect(r) {
+  return "{" + r.join(",") + "}";
+}
+
+function generateSpan(coords) {
+  coords.sort(function(a,b) { return a - b; });
+  var result = [coords[0]];
+  for (var i = 1; i < coords.length; ++i) {
+    if (coords[i] != coords[i - 1]) {
+      result.push(coords[i]);
+    }
+  }
+  return result;
+}
+
+function rectContainsRect(r1, r2) {
+  return r1[0] <= r2[0] && r1[2] >= r2[2] &&
+         r1[1] <= r2[1] && r1[3] >= r2[3];
+}
+
+function rectIntersectsRect(r1, r2) {
+  return Math.max(r1[0], r2[0]) < Math.min(r1[2], r2[2]) &&
+         Math.max(r1[1], r2[1]) < Math.min(r1[3], r2[3]);
+}
+
+function subtractRect(r1, r2, rlist) {
+  var spanX = generateSpan([r1[0], r1[2], r2[0], r2[2]]);
+  var spanY = generateSpan([r1[1], r1[3], r2[1], r2[3]]);
+  for (var i = 1; i < spanX.length; ++i) {
+    for (var j = 1; j < spanY.length; ++j) {
+      var subrect = [spanX[i - 1], spanY[j - 1], spanX[i], spanY[j]];
+      if (rectContainsRect(r1, subrect) && !rectContainsRect(r2, subrect)) {
+        rlist.push(subrect);
+      }
+    }
+  }
+}
+
+
+Region.prototype.toString = function Region_toString() {
+  var s = [];
+  for (var i = 0; i < this._rects.length; ++i) {
+    var r = this._rects[i];
+    s.push(dumpRect(r));
+  }
+  return "Region(" + s.join(",") + ")";
+};
+
+
+Region.prototype.containsRect = function Region_containsRect(r) {
+  var rectList = [r];
+  for (var i = 0; i < this._rects.length; ++i) {
+    var newList = [];
+    for (var j = 0; j < rectList.length; ++j) {
+      subtractRect(rectList[j], this._rects[i], newList);
+    }
+    if (newList.length == 0)
+      return true;
+    rectList = newList;
+  }
+  return false;
+};
+
+Region.prototype.isEmpty = function Region_isEmpty() {
+  for (var i = 0; i < this._rects.length; ++i) {
+    var r = this._rects[i];
+    if (r[0] < r[2] && r[1] < r[3])
+      return false;
+  }
+  return true;
+};
+
+Region.prototype.intersectsRect = function Region_intersectsRect(r) {
+  for (var i = 0; i < this._rects.length; ++i) {
+    if (rectIntersectsRect(this._rects[i], r))
+      return true;
+  }
+  return false;
+};
+
+
+Region.prototype.containsRegion = function Region_containsRegion(rgn) {
+  for (var i = 0; i < rgn._rects.length; ++i) {
+    if (!this.containsRect(rgn._rects[i]))
+      return false;
+  }
+  return true;
+};
+
+
+Region.prototype.equalsRegion = function Region_equalsRegion(rgn) {
+  return this.containsRegion(rgn) && rgn.containsRegion(this);
+};
+})();
