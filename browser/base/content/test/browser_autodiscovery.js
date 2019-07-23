696730021de4c2daab845cb7924584e175cef596
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:54:25 +0000

    Bug 457634: browser_autocomplete.js test doesn't clean up after itself. r=gavin
    UltraBlame original commit: e82e0b06ea423c11559b11fb0be1e02bf9038356

--- browser/base/content/test/browser_autodiscovery.js
+++ browser/base/content/test/browser_autodiscovery.js
@@ -122,6 +122,10 @@ function runMultipleEnginesTestAndFinalize() {
   is(browser.engines[0].uri, "http://first.mozilla.com/search.xml", "first engine wins");
 
   gTestPage.close();
+
+  
+  DOMLinkHandler.handleEvent = gBrowserHandler;
+
   finish();
 }
 
