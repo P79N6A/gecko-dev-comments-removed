12da3f99fe8868534401b036e2e975fe70220a3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:55:49 +0000

    backing out to investigate Ts increase
    UltraBlame original commit: 3badc6dbb80d709e590ace237466f60dc5cb47f1

--- xpcom/glue/nsTObserverArray.h
+++ xpcom/glue/nsTObserverArray.h
@@ -35,35 +35,65 @@
 
 
 
-
 #ifndef nsTObserverArray_h___
 #define nsTObserverArray_h___
 
-#include "nsTArray.h"
+#include "nsVoidArray.h"
 
 class NS_COM_GLUE nsTObserverArray_base {
-  protected:
-    typedef PRUint32 index_type;
-    typedef PRUint32 size_type;
-    typedef PRInt32  diff_type;
+  public:
+    class Iterator_base;
+    friend class Iterator_base;
 
     class Iterator_base {
       protected:
         friend class nsTObserverArray_base;
 
-        Iterator_base(index_type aPosition, Iterator_base* aNext)
+        Iterator_base(PRInt32 aPosition, const nsTObserverArray_base& aArray)
           : mPosition(aPosition),
-            mNext(aNext) {
+            mNext(aArray.mIterators),
+            mArray(aArray) {
+          aArray.mIterators = this;
+        }
+
+        ~Iterator_base() {
+          NS_ASSERTION(mArray.mIterators == this,
+                       "Iterators must currently be destroyed in opposite order "
+                       "from the construction order. It is suggested that you "
+                       "simply put them on the stack");
+          mArray.mIterators = mNext;
         }
 
         
         
-        index_type mPosition;
+        void* GetSafeElementAt(PRInt32 aIndex) {
+          return mArray.mObservers.SafeElementAt(aIndex);
+        }
+        void* FastElementAt(PRInt32 aIndex) {
+          return mArray.mObservers.FastElementAt(aIndex);
+        }
+
+        
+        
+        
+        
+        PRInt32 mPosition;
 
         
         Iterator_base* mNext;
+
+        
+        const nsTObserverArray_base& mArray;
     };
 
+    
+
+
+
+
+    void Clear();
+
+  protected:
     nsTObserverArray_base()
       : mIterators(nsnull) {
     }
@@ -75,14 +105,10 @@ class NS_COM_GLUE nsTObserverArray_base {
 
 
 
-    void AdjustIterators(index_type aModPos, diff_type aAdjustment);
-
-    
-
-
-    void ClearIterators();
+    void AdjustIterators(PRInt32 aModPos, PRInt32 aAdjustment);
 
     mutable Iterator_base* mIterators;
+    nsVoidArray mObservers;
 };
 
 
@@ -92,284 +118,160 @@ class NS_COM_GLUE nsTObserverArray_base {
 
 
 
-
-
-
-template<class T, PRUint32 N>
-class nsAutoTObserverArray : protected nsTObserverArray_base {
+template<class T>
+class nsTObserverArray : public nsTObserverArray_base {
   public:
-    typedef T           elem_type;
-    typedef nsTArray<T> array_type;
 
-    nsAutoTObserverArray() {
+    PRUint32 Count() const {
+      return mObservers.Count();
     }
 
     
-    
-    
 
-    
-    size_type Length() const {
-      return mArray.Length();
+
+
+    PRBool PrependObserver(T* aObserver) {
+      NS_PRECONDITION(!Contains(aObserver),
+                      "Don't prepend if the observer is already in the list");
+
+      PRBool res = mObservers.InsertElementAt(aObserver, 0);
+      if (res) {
+        AdjustIterators(0, 1);
+      }
+      return res;
     }
 
     
-    PRBool IsEmpty() const {
-      return mArray.IsEmpty();
+
+
+
+
+
+    PRBool AppendObserverUnlessExists(T* aObserver) {
+      return Contains(aObserver) || mObservers.AppendElement(aObserver);
     }
 
     
-    
-    
-    
-    elem_type& ElementAt(index_type i) {
-      return mArray.ElementAt(i);
+
+
+
+
+    PRBool AppendObserver(T* aObserver) {
+      return mObservers.AppendElement(aObserver);
     }
 
     
-    const elem_type& ElementAt(index_type i) const {
-      return mArray.ElementAt(i);
-    }
 
-    
-    
-    
-    
-    
-    elem_type& SafeElementAt(index_type i, elem_type& def) {
-      return mArray.SafeElementAt(i, def);
-    }
 
-    
-    const elem_type& SafeElementAt(index_type i, const elem_type& def) const {
-      return mArray.SafeElementAt(i, def);
-    }
 
-    
-    
-    
 
-    
-    
-    
-    
-    
-    template<class Item>
-    PRBool Contains(const Item& item) const {
-      return IndexOf(item) != array_type::NoIndex;
-    }
-
-    
-    
-    
-    
-    
-    
-    template<class Item>
-    index_type IndexOf(const Item& item, index_type start = 0) const {
-      return mArray.IndexOf(item, start);
-    }
-
-    
-    
-    
-
-    
-    
-    
-    
-    template<class Item>
-    PRBool PrependElementUnlessExists(const Item& item) {
-      return Contains(item) || mArray.InsertElementAt(0, item) != nsnull;
-    }
-
-    
-    
-    
-    template<class Item>
-    elem_type* AppendElement(const Item& item) {
-      return mArray.AppendElement(item);
-    }
-
-    
-    
-    elem_type* AppendElement() {
-      return mArray.AppendElement();
-    }
-
-    
-    
-    
-    
-    template<class Item>
-    PRBool AppendElementUnlessExists(const Item& item) {
-      return Contains(item) || AppendElement(item) != nsnull;
-    }
-
-    
-    
-    void RemoveElementAt(index_type index) {
-      NS_ASSERTION(index < mArray.Length(), "invalid index");
-      mArray.RemoveElementAt(index);
-      AdjustIterators(index, -1);
-    }
-
-    
-    
-    
-    
-    
-    template<class Item>
-    PRBool RemoveElement(const Item& item) {
-      index_type index = mArray.IndexOf(item, 0);
-      if (index == array_type::NoIndex)
+    PRBool RemoveObserver(T* aObserver) {
+      PRInt32 index = mObservers.IndexOf(aObserver);
+      if (index < 0) {
         return PR_FALSE;
+      }
 
-      mArray.RemoveElementAt(index);
+      mObservers.RemoveElementAt(index);
       AdjustIterators(index, -1);
+
       return PR_TRUE;
     }
 
     
-    
-    
-    void Clear() {
-      mArray.Clear();
-      ClearIterators();
+
+
+
+    void RemoveObserverAt(PRUint32 aIndex) {
+      if (aIndex < (PRUint32)mObservers.Count()) {
+        mObservers.RemoveElementAt(aIndex);
+        AdjustIterators(aIndex, -1);
+      }
+    }
+
+    PRBool Contains(T* aObserver) const {
+      return mObservers.IndexOf(aObserver) >= 0;
+    }
+
+    PRBool IsEmpty() const {
+      return mObservers.Count() == 0;
+    }
+
+    T* SafeObserverAt(PRInt32 aIndex) const {
+      return static_cast<T*>(mObservers.SafeElementAt(aIndex));
+    }
+
+    T* FastObserverAt(PRInt32 aIndex) const {
+      return static_cast<T*>(mObservers.FastElementAt(aIndex));
     }
 
     
-    
-    
 
-    
-    class Iterator : public Iterator_base {
-      protected:
-        friend class nsAutoTObserverArray;
-        typedef nsAutoTObserverArray<T, N> array_type;
 
-        Iterator(index_type aPosition, const array_type& aArray)
-          : Iterator_base(aPosition, aArray.mIterators),
-            mArray(const_cast<array_type&>(aArray)) {
-          aArray.mIterators = this;
-        }
-
-        ~Iterator() {
-          NS_ASSERTION(mArray.mIterators == this,
-                       "Iterators must currently be destroyed in opposite order "
-                       "from the construction order. It is suggested that you "
-                       "simply put them on the stack");
-          mArray.mIterators = mNext;
-        }
-
-        
-        array_type& mArray;
-    };
 
     
     
     
-    
-    
-    
-    
-    class ForwardIterator : protected Iterator {
+    class ForwardIterator : public nsTObserverArray_base::Iterator_base {
       public:
-        typedef nsAutoTObserverArray<T, N> array_type;
-        typedef Iterator                   base_type;
-
-        ForwardIterator(const array_type& aArray)
-          : Iterator(0, aArray) {
+        ForwardIterator(const nsTObserverArray<T>& aArray)
+          : Iterator_base(0, aArray) {
+        }
+        ForwardIterator(const nsTObserverArray<T>& aArray, PRInt32 aPos)
+          : Iterator_base(aPos, aArray) {
         }
 
-        ForwardIterator(const array_type& aArray, index_type aPos)
-          : Iterator(aPos, aArray) {
-        }
-
-        PRBool operator <(const ForwardIterator& aOther) const {
-          NS_ASSERTION(&this->mArray == &aOther.mArray,
+        PRBool operator <(const ForwardIterator& aOther) {
+          NS_ASSERTION(&mArray == &aOther.mArray,
                        "not iterating the same array");
-          return base_type::mPosition < aOther.mPosition;
+          return mPosition < aOther.mPosition;
         }
 
         
-        
-        
-        PRBool HasMore() const {
-          return base_type::mPosition < base_type::mArray.Length();
-        }
 
-        
-        
-        
-        elem_type& GetNext() {
-          NS_ASSERTION(HasMore(), "iterating beyond end of array");
-          return base_type::mArray.ElementAt(base_type::mPosition++);
+
+
+
+
+        T* GetNext() {
+          return static_cast<T*>(GetSafeElementAt(mPosition++));
         }
     };
 
     
     
-    class EndLimitedIterator : protected ForwardIterator {
+    class EndLimitedIterator : private ForwardIterator {
       public:
-        typedef nsAutoTObserverArray<T, N> array_type;
-        typedef Iterator                   base_type;
+        typedef typename nsTObserverArray<T>::ForwardIterator base_type;
 
-        EndLimitedIterator(const array_type& aArray)
+        EndLimitedIterator(const nsTObserverArray<T>& aArray)
           : ForwardIterator(aArray),
-            mEnd(aArray, aArray.Length()) {
+            mEnd(aArray, aArray.Count()) {
         }
 
         
-        
-        
-        PRBool HasMore() const {
-          return *this < mEnd;
-        }
 
-        
-        
-        
-        elem_type& GetNext() {
-          NS_ASSERTION(HasMore(), "iterating beyond end of array");
-          return base_type::mArray.ElementAt(base_type::mPosition++);
+
+
+
+
+        T* GetNext() {
+          return (*this < mEnd) ?
+                 static_cast<T*>(FastElementAt(base_type::mPosition++)) :
+                 nsnull;
         }
 
       private:
         ForwardIterator mEnd;
     };
-
-  protected:
-    nsAutoTArray<T, N> mArray;
 };
-
-template<class T>
-class nsTObserverArray : public nsAutoTObserverArray<T, 0> {
-  public:
-    typedef nsAutoTObserverArray<T, 0>       base_type;
-    typedef nsTObserverArray_base::size_type size_type;
-
-    
-    
-    
-
-    nsTObserverArray() {}
-
-    
-    explicit nsTObserverArray(size_type capacity) {
-      base_type::mArray.SetCapacity(capacity);
-    }
-};
-
 
 
 
 #define NS_OBSERVER_ARRAY_NOTIFY_OBSERVERS(array_, obstype_, func_, params_) \
   PR_BEGIN_MACRO                                                             \
-    nsTObserverArray<obstype_ *>::ForwardIterator iter_(array_);             \
+    nsTObserverArray<obstype_>::ForwardIterator iter_(array_);               \
     nsCOMPtr<obstype_> obs_;                                                 \
-    while (iter_.HasMore()) {                                                 \
-      obs_ = iter_.GetNext();                                                \
+    while ((obs_ = iter_.GetNext())) {                                       \
       obs_ -> func_ params_ ;                                                \
     }                                                                        \
   PR_END_MACRO
