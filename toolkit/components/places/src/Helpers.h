3067c51295d45566c5410aa06b4eec950390acc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:07:35 +0000

    Bug 539076 - Part3: Consolidate lazy statements boilerplate in macros, r=dietrich
    UltraBlame original commit: 6c91fcc70f18eb157882655ff4189f27c0e10345

--- toolkit/components/places/src/Helpers.h
+++ toolkit/components/places/src/Helpers.h
@@ -66,6 +66,32 @@ public:
   NS_IMETHOD HandleResult(mozIStorageResultSet *); \
   NS_IMETHOD HandleCompletion(PRUint16);
 
+
+
+
+
+#define RETURN_IF_STMT(_stmt, _sql)                                            \
+  PR_BEGIN_MACRO                                                               \
+  if (address_of(_stmt) == address_of(aStmt)) {                                \
+    if (!_stmt) {                                                              \
+      nsresult rv = mDBConn->CreateStatement(_sql, getter_AddRefs(_stmt));     \
+      NS_ENSURE_TRUE(NS_SUCCEEDED(rv) && _stmt, nsnull);                       \
+    }                                                                          \
+    return _stmt.get();                                                        \
+  }                                                                            \
+  PR_END_MACRO
+
+
+
+
+#define DECLARE_AND_ASSIGN_LAZY_STMT(_localStmt, _globalStmt)                  \
+  mozIStorageStatement* _localStmt = GetStatement(_globalStmt);                \
+  NS_ENSURE_STATE(_localStmt)
+
+#define DECLARE_AND_ASSIGN_SCOPED_LAZY_STMT(_localStmt, _globalStmt)           \
+  DECLARE_AND_ASSIGN_LAZY_STMT(_localStmt, _globalStmt);                       \
+  mozStorageStatementScoper scoper(_localStmt)
+
 } 
 } 
 
