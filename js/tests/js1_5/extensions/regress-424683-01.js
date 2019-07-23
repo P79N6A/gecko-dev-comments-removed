20109c0da4a44291bd14f3bbf47a1e5766efd704
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:11:27 +0000

    JavaScript Tests - regression test for bug 424683, by Brian Crowder
    UltraBlame original commit: 280c3831f45bf10b9e942b617030c7409bc87b57

---
+++ js/tests/js1_5/extensions/regress-424683-01.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-424683.js';
+
+var BUGNUMBER = 424683;
+var summary = 'Throw too much recursion instead of script stack space quota';
+var actual = 'No Error';
+var expect = 'InternalError: too much recursion';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  function f() { f(); }
+
+  try
+  {
+    f();
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
