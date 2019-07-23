42875495b99e23cb9518cf38a799f42323d6383a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:25:20 +0000

    Bug 444821 - Re-enable test_storage_statement_executeAsync.js This fixes the test to run properly on slower running systems (like the tinderbox machines). r=vlad
    UltraBlame original commit: 6db0b5d30af105d5f32c8770b7e74b138aa27df3

--- storage/test/unit/test_storage_statement_executeAsync.js
+++ storage/test/unit/test_storage_statement_executeAsync.js
@@ -85,6 +85,10 @@ function test_create_table()
       );
       stmt.finalize();
 
+      
+      for (var i = 0; i < tests.length; i++)
+        tests[i]();
+
       do_test_finished();
     }
   });
@@ -446,7 +450,6 @@ function test_double_execute()
 
 var tests =
 [
-  test_create_table,
   test_add_data,
   test_get_data,
   test_tuple_out_of_bounds,
@@ -459,11 +462,9 @@ var tests =
 
 function run_test()
 {
-  
-  
-  return;
   cleanup();
 
-  for (var i = 0; i < tests.length; i++)
-    tests[i]();
+  
+  
+  test_create_table();
 }
