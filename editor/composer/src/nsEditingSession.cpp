d5b294b637e058249b54c32fe4dd80250e38ccc6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:53:03 +0000

    Bug 207531 Allow editor to edit xhtml files (as html) r=glazou sr=roc a=mconnor
    UltraBlame original commit: e284ed16663d5b761d98dbc4cda935751bc6dcc0

--- editor/composer/src/nsEditingSession.cpp
+++ editor/composer/src/nsEditingSession.cpp
@@ -353,7 +353,8 @@ nsEditingSession::SetupEditorOnWindow(nsIDOMWindow *aWindow)
         mEditorType.AssignLiteral("text");
         mimeCType = "text/plain";
       }
-      else if (!mimeCType.EqualsLiteral("text/html"))
+      else if (!mimeCType.EqualsLiteral("text/html") &&
+               !mimeCType.EqualsLiteral("application/xhtml+xml"))
       {
         
         mEditorStatus = eEditorErrorCantEditMimeType;
