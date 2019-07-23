5178619fdeb7df36e3e0cdbd9af34e2eccbd4c68
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:28:57 +0000

    System for testing plugins, take 4. b=386676 r=smichaud sr=roc
    UltraBlame original commit: 7b1292782cd39c59fbe1e14986eda53a16ca7f9e

---
+++ modules/plugin/test/testplugin/nptest_utils.h
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nptest_utils_h_
+#define nptest_utils_h_
+
+#include "nptest.h"
+
+NPUTF8* createCStringFromNPVariant(const NPVariant* variant);
+
+NPIdentifier variantToIdentifier(NPVariant variant);
+NPIdentifier stringVariantToIdentifier(NPVariant variant);
+NPIdentifier int32VariantToIdentifier(NPVariant variant);
+NPIdentifier doubleVariantToIdentifier(NPVariant variant);
+
+#endif 
