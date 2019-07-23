366d898e48d278cb821da8cb79b5cdf087a34555
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:03:40 +0000

    Use inverse upcase instead of downcase per ECMA-262 for case-insensitive matching, r=dmandelin
    UltraBlame original commit: 5c102146cd95420afe9f0e55e454c67e64ed5f79

---
+++ js/src/tests/js1_5/Regress/regress-440926.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-440926.js';
+
+var BUGNUMBER = 440926;
+var summary = 'Correctly match regexps with special "i" characters';
+var actual = '';
+var expect = 'iI#,iI#;iI#,iI#';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  actual += 'iI\u0130'.replace(/[\u0130]/gi, '#');
+  actual += ',' + 'iI\u0130'.replace(/\u0130/gi, '#');
+
+  jit(true);
+  actual += ';' + 'iI\u0130'.replace(/[\u0130]/gi, '#');
+  actual += ',' + 'iI\u0130'.replace(/\u0130/gi, '#');
+  jit(false);
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
