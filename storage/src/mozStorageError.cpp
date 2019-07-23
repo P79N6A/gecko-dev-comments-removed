d530f9d75476bc8f2db1edc8caa86c7344aea49c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:12:33 +0000

    Bug 489603 - Update mozStorageError.* to follow style guidelines Consistent spacing, consistent naming, and namespaces - OH MY! r=asuth
    UltraBlame original commit: c1cb148be7bb2a1df30d902457737cf38d02aca7

--- storage/src/mozStorageError.cpp
+++ storage/src/mozStorageError.cpp
@@ -39,34 +39,44 @@
 
 #include "mozStorageError.h"
 
+namespace mozilla {
+namespace storage {
 
 
 
 
-
-
-
-NS_IMPL_THREADSAFE_ISUPPORTS1(mozStorageError, mozIStorageError)
-
-mozStorageError::mozStorageError(int aResult, const char *aMessage) :
-    mResult(aResult)
-  , mMessage(aMessage)
+Error::Error(int aResult,
+             const char *aMessage)
+: mResult(aResult)
+, mMessage(aMessage)
 {
 }
 
 
 
 
+
+NS_IMPL_THREADSAFE_ISUPPORTS1(
+  Error,
+  mozIStorageError
+)
+
+
+
+
 NS_IMETHODIMP
-mozStorageError::GetResult(PRInt32 *_result)
+Error::GetResult(PRInt32 *_result)
 {
   *_result = mResult;
   return NS_OK;
 }
 
 NS_IMETHODIMP
-mozStorageError::GetMessage(nsACString &_message)
+Error::GetMessage(nsACString &_message)
 {
   _message = mMessage;
   return NS_OK;
 }
+
+} 
+} 
