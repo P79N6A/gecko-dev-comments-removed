51a672981f2ac51cc14c6fcd0ec26f4eeaf11437
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:51:15 +0000

    Bug 393758. Detect when we're about to create two flows with the same element in the same textrun (which violates our invariants), and prevent it by starting a new textrun for the second flow. r=smontagu
    UltraBlame original commit: ec39f2451eb257043d66053bf786b7f77e4f5069

--- layout/generic/nsTextFrameThebes.cpp
+++ layout/generic/nsTextFrameThebes.cpp
@@ -1106,6 +1106,17 @@ BuildTextRunsScanner::ContinueTextRunAcrossFrames(nsTextFrame* aFrame1, nsTextFr
   if (textStyle1->WhiteSpaceIsSignificant() && HasTerminalNewline(aFrame1))
     return PR_FALSE;
 
+  if (aFrame1->GetContent() == aFrame2->GetContent() &&
+      aFrame1->GetNextInFlow() != aFrame2) {
+    
+    
+    
+    
+    
+    
+    return PR_FALSE;
+  }
+
   nsStyleContext* sc2 = aFrame2->GetStyleContext();
   if (sc1 == sc2)
     return PR_TRUE;
@@ -1772,8 +1783,6 @@ nsTextFrame::EnsureTextRun(gfxContext* aReferenceContext, nsIFrame* aLineContain
     for (i = startAt; 0 <= i && i < userData->mMappedFlowCount; i += direction) {
       TextRunMappedFlow* flow = &userData->mMappedFlows[i];
       if (flow->mStartFrame->GetContent() == mContent) {
-        
-        
         
         
         userData->mLastFlowIndex = i;
