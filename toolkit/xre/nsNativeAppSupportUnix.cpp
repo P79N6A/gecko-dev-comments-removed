2e162054b5d4feed11901dc7aa0a27e6edec3441
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:44:23 +0000

    Bug 93789, Support Xsmp, r=benjamin
    UltraBlame original commit: e5f2fbcd206b0ef899bc9fdc4a78c71bcefa624f

---
+++ toolkit/xre/nsNativeAppSupportUnix.cpp
@@ -0,0 +1,194 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsNativeAppSupportBase.h"
+#include "nsCOMPtr.h"
+#include "nsXPCOM.h"
+#include "nsISupportsPrimitives.h"
+#include "nsIObserverService.h"
+#include "nsIAppStartup.h"
+#include "nsServiceManagerUtils.h"
+#include "prlink.h"
+
+#include <glib.h>
+#include <glib-object.h>
+
+typedef struct _GnomeProgram GnomeProgram;
+typedef struct _GnomeModuleInfo GnomeModuleInfo;
+typedef struct _GnomeClient GnomeClient;
+
+typedef enum {
+  GNOME_SAVE_GLOBAL,
+  GNOME_SAVE_LOCAL,
+  GNOME_SAVE_BOTH
+} GnomeSaveStyle;
+
+typedef enum {
+  GNOME_INTERACT_NONE,
+  GNOME_INTERACT_ERRORS,
+  GNOME_INTERACT_ANY
+} GnomeInteractStyle;
+
+typedef enum {
+  GNOME_DIALOG_ERROR,
+  GNOME_DIALOG_NORMAL
+} GnomeDialogType;
+
+typedef GnomeProgram * (*_gnome_program_init_fn)(const char *, const char *,
+                                                 const GnomeModuleInfo *, int,
+                                                 char **, const char *, ...);
+typedef const GnomeModuleInfo * (*_libgnomeui_module_info_get_fn)();
+typedef GnomeClient * (*_gnome_master_client_fn)(void);
+typedef void (*GnomeInteractFunction)(GnomeClient *, gint, GnomeDialogType,
+                                      gpointer);
+typedef void (*_gnome_client_request_interaction_fn)(GnomeClient *,
+                                                     GnomeDialogType,
+                                                     GnomeInteractFunction,
+                                                     gpointer);
+typedef void (*_gnome_interaction_key_return_fn)(gint, gboolean);
+
+static _gnome_client_request_interaction_fn gnome_client_request_interaction;
+static _gnome_interaction_key_return_fn gnome_interaction_key_return;
+
+void interact_cb(GnomeClient *client, gint key,
+                 GnomeDialogType type, gpointer data)
+{
+  nsCOMPtr<nsIObserverService> obsServ =
+    do_GetService("@mozilla.org/observer-service;1");
+  nsCOMPtr<nsISupportsPRBool> cancelQuit =
+    do_CreateInstance(NS_SUPPORTS_PRBOOL_CONTRACTID);
+
+  cancelQuit->SetData(PR_FALSE);
+
+  obsServ->NotifyObservers(cancelQuit, "quit-application-requested", nsnull);
+
+  PRBool abortQuit;
+  cancelQuit->GetData(&abortQuit);
+
+  gnome_interaction_key_return(key, abortQuit);
+}
+
+gboolean save_yourself_cb(GnomeClient *client, gint phase,
+                          GnomeSaveStyle style, gboolean shutdown,
+                          GnomeInteractStyle interact, gboolean fast,
+                          gpointer user_data)
+{
+  if (interact == GNOME_INTERACT_ANY)
+    gnome_client_request_interaction(client, GNOME_DIALOG_NORMAL,
+                                     interact_cb, nsnull);
+  return TRUE;
+}
+
+void die_cb(GnomeClient *client, gpointer user_data)
+{
+  nsCOMPtr<nsIObserverService> obsServ =
+    do_GetService("@mozilla.org/observer-service;1");
+  nsCOMPtr<nsIAppStartup> appService =
+    do_GetService("@mozilla.org/toolkit/app-startup;1");
+
+  if (obsServ)
+    obsServ->NotifyObservers(nsnull, "quit-application-granted", nsnull);
+
+  if (appService)
+    appService->Quit(nsIAppStartup::eForceQuit);
+}
+
+class nsNativeAppSupportUnix : public nsNativeAppSupportBase
+{
+public:
+  NS_IMETHOD Start(PRBool* aRetVal);
+};
+
+NS_IMETHODIMP
+nsNativeAppSupportUnix::Start(PRBool *aRetVal)
+{
+  *aRetVal = PR_TRUE;
+
+  PRLibrary *gnomeuiLib = PR_LoadLibrary("libgnomeui-2.so.0");
+  if (!gnomeuiLib)
+    return NS_OK;
+
+  PRLibrary *gnomeLib = PR_LoadLibrary("libgnome-2.so.0");
+  if (!gnomeLib) {
+    PR_UnloadLibrary(gnomeuiLib);
+    return NS_OK;
+  }
+
+  _gnome_program_init_fn gnome_program_init =
+    (_gnome_program_init_fn)PR_FindFunctionSymbol(gnomeLib, "gnome_program_init");
+  _libgnomeui_module_info_get_fn libgnomeui_module_info_get = (_libgnomeui_module_info_get_fn)PR_FindFunctionSymbol(gnomeuiLib, "libgnomeui_module_info_get");
+  if (!gnome_program_init || !libgnomeui_module_info_get) {
+    PR_UnloadLibrary(gnomeuiLib);
+    PR_UnloadLibrary(gnomeLib);
+    return NS_OK;
+  }
+
+  char *argv[2] = { "gecko", "--disable-crash-dialog" };
+  gnome_program_init("Gecko", "1.0", libgnomeui_module_info_get(), 2, argv, NULL);
+
+  
+  
+  
+
+  gnome_client_request_interaction = (_gnome_client_request_interaction_fn)
+    PR_FindFunctionSymbol(gnomeuiLib, "gnome_client_request_interaction");
+  gnome_interaction_key_return = (_gnome_interaction_key_return_fn)
+    PR_FindFunctionSymbol(gnomeuiLib, "gnome_interaction_key_return");
+
+  _gnome_master_client_fn gnome_master_client = (_gnome_master_client_fn)
+    PR_FindFunctionSymbol(gnomeuiLib, "gnome_master_client");
+
+  GnomeClient *client = gnome_master_client();
+  g_signal_connect(client, "save-yourself", G_CALLBACK(save_yourself_cb), NULL);
+  g_signal_connect(client, "die", G_CALLBACK(die_cb), NULL);
+
+  return NS_OK;
+}
+
+nsresult
+NS_CreateNativeAppSupport(nsINativeAppSupport **aResult)
+{
+  nsNativeAppSupportBase* native = new nsNativeAppSupportUnix();
+  if (!native)
+    return NS_ERROR_OUT_OF_MEMORY;
+
+  *aResult = native;
+  NS_ADDREF(*aResult);
+
+  return NS_OK;
+}
