9b07769f42a3d3cf13549972c3d5d9fef301ffbe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:54:26 +0000

    Bug 382917 - "Display script filename and line number in Stop Script dialog" [p=manish@flock.com (Manish Singh) r+sr=mrbkap a1.9=jst]
    UltraBlame original commit: 3c8b980d028ff2dfd32d27ce5bc93eaaacc5e67d

--- dom/src/base/nsJSEnvironment.cpp
+++ dom/src/base/nsJSEnvironment.cpp
@@ -938,6 +938,35 @@ nsJSContext::DOMBranchCallback(JSContext *cx, JSScript *script)
     return JS_TRUE;
   }
 
+  
+  if (script) {
+    const char *filename = ::JS_GetScriptFilename(cx, script);
+    if (filename) {
+      nsXPIDLString scriptLocation;
+      NS_ConvertUTF8toUTF16 filenameUTF16(filename);
+      const PRUnichar *formatParams[] = { filenameUTF16.get() };
+      rv = bundle->FormatStringFromName(NS_LITERAL_STRING("KillScriptLocation").get(),
+                                        formatParams, 1,
+                                        getter_Copies(scriptLocation));
+
+      if (NS_SUCCEEDED(rv) && scriptLocation) {
+        msg.AppendLiteral("\n\n");
+        msg.Append(scriptLocation);
+
+        JSStackFrame *fp, *iterator = nsnull;
+        fp = ::JS_FrameIterator(cx, &iterator);
+        if (fp) {
+          jsbytecode *pc = ::JS_GetFramePC(cx, fp);
+          if (pc) {
+            PRUint32 lineno = ::JS_PCToLineNumber(cx, script, pc);
+            msg.Append(':');
+            msg.AppendInt(lineno);
+          }
+        }
+      }
+    }
+  }
+
   PRInt32 buttonPressed = 1; 
   PRBool neverShowDlgChk = PR_FALSE;
   PRUint32 buttonFlags = (nsIPrompt::BUTTON_TITLE_IS_STRING *
