5da780248dbaac3a2d84874ddcb64e4c068e62a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:57 +0000

    Bug 373253. Initial addition of test framework and tests for pwmgr. Patch by Justin Dolske <dolske@mozilla.com>. r=sayrer, sr=mconnor
    UltraBlame original commit: d8c055007b8e36d1232ed23fdfe085260fd3c8eb

---
+++ toolkit/components/passwordmgr/test/pwmgr_common.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+function $_(name) {
+  var elements = document.getElementsByName(name);
+
+  if (!elements || elements.length < 1) { return null; }
+  if (elements.length > 2) {
+    logWarning("found multiple elements with name="+name);
+  }
+
+  return elements[0];
+}
