b892ad4acebcaeed8a919b4f072559d23454270f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:24:51 +0000

    Remove bogus assertion.  (Bug 470063)  r+sr=roc
    UltraBlame original commit: b25c43b4a4199565421c97f10eeba7b8943704b0

--- layout/xul/base/src/nsPopupSetFrame.cpp
+++ layout/xul/base/src/nsPopupSetFrame.cpp
@@ -77,7 +77,6 @@ nsPopupSetFrame::Init(nsIContent*      aContent,
   
   
   nsIRootBox *rootBox = nsIRootBox::GetRootBox(PresContext()->GetPresShell());
-  NS_ASSERTION(rootBox, "must have root box");
   if (rootBox) {
     rootBox->SetPopupSetFrame(this);
   }
@@ -148,7 +147,6 @@ nsPopupSetFrame::Destroy()
   
   
   nsIRootBox *rootBox = nsIRootBox::GetRootBox(PresContext()->GetPresShell());
-  NS_ASSERTION(rootBox, "must have root box");
   if (rootBox) {
     rootBox->SetPopupSetFrame(nsnull);
   }
