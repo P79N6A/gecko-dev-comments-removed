760bcdfc9e67c66ea04738500643c575cf11e9d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:24:49 +0000

    Bug 502799 add new nsISound method for the event sounds r=roc+neil, sr=roc
    UltraBlame original commit: 91bde0e380a2b1ed0825a2b7673ca263c9a48c40

--- widget/src/os2/nsSound.cpp
+++ widget/src/os2/nsSound.cpp
@@ -488,6 +488,7 @@ NS_IMETHODIMP nsSound::PlaySystemSound(const nsAString &aSoundAlias)
   }
 
   if (NS_IsMozAliasSound(aSoundAlias)) {
+    NS_WARNING("nsISound::playSystemSound is called with \"_moz_\" events, they are obsolete, use nsISound::playEventSound instead");
     
     if (aSoundAlias.Equals(NS_SYSSOUND_MAIL_BEEP))
       Beep();
@@ -510,4 +511,15 @@ NS_IMETHODIMP nsSound::PlaySystemSound(const nsAString &aSoundAlias)
   rc = DosCloseEventSem(arg.hev);
 
   return NS_OK;
+}
+
+NS_IMETHODIMP nsSound::PlayEventSound(PRUint32 aEventId)
+{
+  
+  if (!sMMPMInstalled) {
+    return Beep();
+  }
+
+  
+  return aEventId == EVENT_NEW_MAIL_RECIEVED ? Beep() : NS_OK;
 }
