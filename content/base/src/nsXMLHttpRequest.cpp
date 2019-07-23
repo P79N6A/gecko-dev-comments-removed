ec3956f65f8627bfd645b7db4db9b925cebd4541
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:09 +0000

    Bug 371123 - XMLHttpRequest should trigger content policies p=Wladimir Palant <trev@gtchat.de>r=jst, sr=sicking
    UltraBlame original commit: 85051915636ede0c9f8b19fe8a1054ec3a41b2d9

--- content/base/src/nsXMLHttpRequest.cpp
+++ content/base/src/nsXMLHttpRequest.cpp
@@ -79,6 +79,9 @@
 #include "nsDOMClassInfo.h"
 #include "nsIScriptableUConv.h"
 #include "nsCycleCollectionParticipant.h"
+#include "nsIContentPolicy.h"
+#include "nsContentPolicyUtils.h"
+#include "nsContentErrors.h"
 
 #define LOAD_STR "load"
 #define ERROR_STR "error"
@@ -1035,6 +1038,23 @@ nsXMLHttpRequest::OpenRequest(const nsACString& method,
 
   rv = NS_NewURI(getter_AddRefs(uri), url, nsnull, GetBaseURI());
   if (NS_FAILED(rv)) return rv;
+
+  
+  
+  nsCOMPtr<nsIDocument> doc = GetDocumentFromScriptContext(mScriptContext);
+  PRInt16 shouldLoad = nsIContentPolicy::ACCEPT;
+  rv = NS_CheckContentLoadPolicy(nsIContentPolicy::TYPE_OTHER,
+                                 uri,
+                                 (doc ? doc->GetDocumentURI() : nsnull),
+                                 doc,
+                                 EmptyCString(), 
+                                 nsnull,         
+                                 &shouldLoad);
+  if (NS_FAILED(rv)) return rv;
+  if (NS_CP_REJECTED(shouldLoad)) {
+    
+    return NS_ERROR_CONTENT_BLOCKED;
+  }
 
   if (!user.IsEmpty()) {
     nsCAutoString userpass;
