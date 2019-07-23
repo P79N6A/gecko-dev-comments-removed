40a1b1fc14064ab8211d38acdffc2dc6d951c31a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:31 +0000

    JavaScript Test - regression test for bug 350387, by Nanto Vi (TOYAMA Nao)
    UltraBlame original commit: 4326b267c79e86c76f7e95993bd391e560045dc3

---
+++ js/tests/js1_7/regress/regress-350387.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 350387;
+var summary = 'Var declaration and let with same name';
+var actual = '';
+var expect = '';
+
+expect = undefined + '';
+actual = '';
+let (x = 2) 
+{ 
+var x; 
+} 
+actual = x + '';
+reportCompare(expect, actual, summary);
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+
+  expect = undefined + '';
+  actual = '';
+  (function () { let (x = 2) { var x; } actual = x + ''; })();  
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
