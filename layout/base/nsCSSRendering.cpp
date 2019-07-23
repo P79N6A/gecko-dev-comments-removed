f2faec8a57f029e7f625a6cac632fa75f9d11bdf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:08:07 +0000

    This should be a bit-check, not a boolean check.  No bug, r+sr=dbaron
    UltraBlame original commit: a55ed9d7328acc2a2c1c3feeed276c98b0c9993d

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -216,7 +216,7 @@ protected:
   nsIFrame* GetPrevContinuation(nsIFrame* aFrame)
   {
     nsIFrame* prevCont = aFrame->GetPrevContinuation();
-    if (!prevCont && (aFrame->GetStateBits() && NS_FRAME_IS_SPECIAL)) {
+    if (!prevCont && (aFrame->GetStateBits() & NS_FRAME_IS_SPECIAL)) {
       nsIFrame* block =
         static_cast<nsIFrame*>
                    (aFrame->GetProperty(nsGkAtoms::IBSplitSpecialPrevSibling));
@@ -235,7 +235,7 @@ protected:
   nsIFrame* GetNextContinuation(nsIFrame* aFrame)
   {
     nsIFrame* nextCont = aFrame->GetNextContinuation();
-    if (!nextCont && (aFrame->GetStateBits() && NS_FRAME_IS_SPECIAL)) {
+    if (!nextCont && (aFrame->GetStateBits() & NS_FRAME_IS_SPECIAL)) {
       
       aFrame = aFrame->GetFirstContinuation();
       nsIFrame* block =
