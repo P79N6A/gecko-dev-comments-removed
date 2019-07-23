c6b6f9bfadb978581a893fdd898473e376721b07
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:09:53 +0000

    Bug 533876 - In JSScope::{add,put}Property, disregard the SPROP_IN_DICTIONARY bit of the flags. r=brendan.
    UltraBlame original commit: 3036013432ce47875bce99e7c1ab54cdc1532ce4

---
+++ js/src/tests/js1_8_5/regress/regress-533876.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+gTestfile = 'regress-533876';
+
+var x = [0];
+eval();
+
+x.__proto__ = this;  
+try {
+    DIE;
+} catch(e) {
+}
+
+delete eval;  
+gc();
+var f = eval("function () { return /x/; }");
+x.watch('x', f);  
