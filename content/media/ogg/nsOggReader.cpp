8faed7350736f4618632b04c364805ad601d9b23
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:30:57 +0000

    Bug 562585 - AllFrameTimesIncrease defined but not used. r=doublec
    UltraBlame original commit: 8eb00d668f61a388881d246348724632a3292232

--- content/media/ogg/nsOggReader.cpp
+++ content/media/ogg/nsOggReader.cpp
@@ -324,6 +324,7 @@ TheoraVersion(th_info* info,
   return (th_ver >= ver) ? 1 : 0;
 }
 
+#ifdef DEBUG
 
 
 static PRBool
@@ -341,6 +342,7 @@ AllFrameTimesIncrease(nsTArray<VideoData*>& aFrames)
   }
   return PR_TRUE;
 }
+#endif
 
 static void Clear(nsTArray<VideoData*>& aFrames) {
   for (PRUint32 i = 0; i < aFrames.Length(); ++i) {
