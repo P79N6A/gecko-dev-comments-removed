d937e53d006fc1939d7a4de4a41436a4dd8fdb08
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:37:04 +0000

    Separate previously-copied font constants into their own file and have them in one place rather than three.  (Bug 3512)  r=vlad
    UltraBlame original commit: cbcf14ce64cc104d051c30d700cf80f190725b5f

---
+++ gfx/thebes/public/gfxFontConstants.h
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef GFX_FONT_CONSTANTS_H
+#define GFX_FONT_CONSTANTS_H
+
+
+
+
+
+
+#define NS_FONT_STYLE_NORMAL            0
+#define NS_FONT_STYLE_ITALIC            1
+#define NS_FONT_STYLE_OBLIQUE           2
+
+#define NS_FONT_WEIGHT_NORMAL           400
+#define NS_FONT_WEIGHT_BOLD             700
+#define NS_FONT_WEIGHT_BOLDER           1
+#define NS_FONT_WEIGHT_LIGHTER          -1
+
+#endif
