1c83e403cce47a0b62e2f44ae41410e1dcc128d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:03:46 +0000

    remove unused files, rename mac prompt service file to .mm. prep for exceptions handling. b=419389 r/sr=roc
    UltraBlame original commit: dc0558e397dd313918a0b3519b09adfac267f06c

--- embedding/components/printingui/src/mac/nsPrintingPromptServiceX.mm
+++ embedding/components/printingui/src/mac/nsPrintingPromptServiceX.mm
@@ -1,41 +1,41 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 1998
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Rod Spears <rods@netscape.com>
+ *   Conrad Carlen <ccarlen@netscape.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 #include "nsPrintingPromptService.h"
 
@@ -57,18 +57,18 @@
 
 #include "nsPDECommon.h"
 
-
+// Printing Progress Includes
 #include "nsPrintProgress.h"
 #include "nsPrintProgressParams.h"
 #include "nsIWebProgressListener.h"
 
-
+// OS includes
 #include <PMApplication.h>
 #include <CFPlugIn.h>
 
-
-
-
+//-----------------------------------------------------------------------------
+// Static Helpers
+//-----------------------------------------------------------------------------
 
 static nsresult LoadPDEPlugIn()
 {
@@ -139,7 +139,7 @@ GetDictionaryStringValue(CFDictionaryRef aDictionary, CFStringRef aKey, nsAStrin
     return PR_FALSE;
 }
 
-
+// returns success or failure (not the read value)
 static PRBool
 GetDictionaryBooleanValue(CFDictionaryRef aDictionary, CFStringRef aKey, PRBool& aResult)
 {
@@ -170,9 +170,9 @@ SetDictionaryBooleanvalue(CFMutableDictionaryRef aDictionary, CFStringRef aKey, 
     CFDictionaryAddValue(aDictionary, aKey, aValue ? kCFBooleanTrue : kCFBooleanFalse);
 }
 
-
-
-
+//*****************************************************************************
+// nsPrintingPromptService
+//*****************************************************************************   
 
 NS_IMPL_ISUPPORTS2(nsPrintingPromptService, nsIPrintingPromptService, nsIWebProgressListener)
 
@@ -190,9 +190,9 @@ nsresult nsPrintingPromptService::Init()
     return NS_OK;
 }
 
-
-
-
+//*****************************************************************************
+// nsPrintingPromptService::nsIPrintingPromptService
+//*****************************************************************************   
 
 NS_IMETHODIMP 
 nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrint *webBrowserPrint, nsIPrintSettings *printSettings)
@@ -223,8 +223,8 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
     if (status != noErr)
         return NS_ERROR_FAILURE;
         
-    
-    
+    // Reset the print settings to their defaults each time. We don't want to remember
+    // the last printed page range or whatever. This is expected Mac behavior.
     status = ::PMSessionDefaultPrintSettings(printSession, nativePrintSettings);
     if (status != noErr)
         return NS_ERROR_FAILURE;
@@ -234,7 +234,7 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
     rv = ::LoadPDEPlugIn();
     NS_ASSERTION(NS_SUCCEEDED(rv), "LoadPDEPlugIn() failed");
     
-    
+    // Set the print job title
     PRUnichar** docTitles;
     PRUint32 titleCount;
     rv = webBrowserPrint->EnumerateDocumentNames(&titleCount, &docTitles);
@@ -252,7 +252,7 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
       titleCount = 0;
     }
 
-    
+    // Create a dictionary and store our settings into it
     CFMutableDictionaryRef dictToPDE = CFDictionaryCreateMutable(kCFAllocatorDefault, 0,
                                     (const CFDictionaryKeyCallBacks *)&kCFTypeDictionaryKeyCallBacks,
                                     (const CFDictionaryValueCallBacks *)&kCFTypeDictionaryValueCallBacks);
@@ -278,7 +278,7 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
             CFDictionaryAddValue(dictToPDE, kPDEKeyHaveFrameSelected, kCFBooleanFalse);
         }
 
-        
+        // get the boolean values
         printSettings->GetShrinkToFit(&isOn);
         SetDictionaryBooleanvalue(dictToPDE, kPDEKeyShrinkToFit, isOn);
 
@@ -288,7 +288,7 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
         printSettings->GetPrintBGImages(&isOn);
         SetDictionaryBooleanvalue(dictToPDE, kPDEKeyPrintBGImages, isOn);
 
-        
+        // read headers
         nsXPIDLString tempString;
         printSettings->GetHeaderStrRight(getter_Copies(tempString));
         SetDictionaryStringValue(dictToPDE, kPDEKeyHeaderRight, tempString);
@@ -299,7 +299,7 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
         printSettings->GetHeaderStrLeft(getter_Copies(tempString));
         SetDictionaryStringValue(dictToPDE, kPDEKeyHeaderLeft, tempString);
 
-        
+        // read footers
         printSettings->GetFooterStrRight(getter_Copies(tempString));
         SetDictionaryStringValue(dictToPDE, kPDEKeyFooterRight, tempString);
 
@@ -326,7 +326,7 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
         
         CFDictionaryRef dictFromPDE = ExtractCustomSettingsDict(nativePrintSettings);
         if (dictFromPDE) {
-            
+            //CFShow(dictFromPDE);
             
             PRBool printSelectionOnly;
             if (GetDictionaryBooleanValue(dictFromPDE, kPDEKeyPrintSelection, printSelectionOnly)) {
@@ -363,7 +363,7 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
 
             nsAutoString stringFromDict;
             
-            
+            // top headers
             if (GetDictionaryStringValue(dictFromPDE, kPDEKeyHeaderLeft, stringFromDict))
                 printSettings->SetHeaderStrLeft(stringFromDict.get());
 
@@ -373,7 +373,7 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
             if (GetDictionaryStringValue(dictFromPDE, kPDEKeyHeaderRight, stringFromDict))
                 printSettings->SetHeaderStrRight(stringFromDict.get());
 
-            
+            // bottom footers
             if (GetDictionaryStringValue(dictFromPDE, kPDEKeyFooterLeft, stringFromDict))
                 printSettings->SetFooterStrLeft(stringFromDict.get());
 
@@ -416,9 +416,9 @@ nsPrintingPromptService::ShowPrintDialog(nsIDOMWindow *parent, nsIWebBrowserPrin
 
 NS_IMETHODIMP 
 nsPrintingPromptService::ShowProgress(nsIDOMWindow*            parent, 
-                                      nsIWebBrowserPrint*      webBrowserPrint,    
-                                      nsIPrintSettings*        printSettings,      
-                                      nsIObserver*             openDialogObserver, 
+                                      nsIWebBrowserPrint*      webBrowserPrint,    // ok to be null
+                                      nsIPrintSettings*        printSettings,      // ok to be null
+                                      nsIObserver*             openDialogObserver, // ok to be null
                                       PRBool                   isForPrinting,
                                       nsIWebProgressListener** webProgressListener,
                                       nsIPrintProgressParams** printProgressParams,
@@ -474,9 +474,9 @@ nsPrintingPromptService::ShowPrinterProperties(nsIDOMWindow *parent, const PRUni
 }
 
 
-
-
-
+//*****************************************************************************
+// nsPrintingPromptService::nsIWebProgressListener
+//*****************************************************************************   
 
 NS_IMETHODIMP 
 nsPrintingPromptService::OnStateChange(nsIWebProgress *aWebProgress, nsIRequest *aRequest, PRUint32 aStateFlags, nsresult aStatus)
@@ -491,7 +491,7 @@ nsPrintingPromptService::OnStateChange(nsIWebProgress *aWebProgress, nsIRequest 
     return NS_OK;
 }
 
-
+/* void onProgressChange (in nsIWebProgress aWebProgress, in nsIRequest aRequest, in long aCurSelfProgress, in long aMaxSelfProgress, in long aCurTotalProgress, in long aMaxTotalProgress); */
 NS_IMETHODIMP 
 nsPrintingPromptService::OnProgressChange(nsIWebProgress *aWebProgress, nsIRequest *aRequest, PRInt32 aCurSelfProgress, PRInt32 aMaxSelfProgress, PRInt32 aCurTotalProgress, PRInt32 aMaxTotalProgress)
 {
@@ -501,7 +501,7 @@ nsPrintingPromptService::OnProgressChange(nsIWebProgress *aWebProgress, nsIReque
     return NS_OK;
 }
 
-
+/* void onLocationChange (in nsIWebProgress aWebProgress, in nsIRequest aRequest, in nsIURI location); */
 NS_IMETHODIMP 
 nsPrintingPromptService::OnLocationChange(nsIWebProgress *aWebProgress, nsIRequest *aRequest, nsIURI *location)
 {
@@ -511,7 +511,7 @@ nsPrintingPromptService::OnLocationChange(nsIWebProgress *aWebProgress, nsIReque
     return NS_OK;
 }
 
-
+/* void onStatusChange (in nsIWebProgress aWebProgress, in nsIRequest aRequest, in nsresult aStatus, in wstring aMessage); */
 NS_IMETHODIMP 
 nsPrintingPromptService::OnStatusChange(nsIWebProgress *aWebProgress, nsIRequest *aRequest, nsresult aStatus, const PRUnichar *aMessage)
 {
@@ -521,7 +521,7 @@ nsPrintingPromptService::OnStatusChange(nsIWebProgress *aWebProgress, nsIRequest
     return NS_OK;
 }
 
-
+/* void onSecurityChange (in nsIWebProgress aWebProgress, in nsIRequest aRequest, in unsigned long state); */
 NS_IMETHODIMP 
 nsPrintingPromptService::OnSecurityChange(nsIWebProgress *aWebProgress, nsIRequest *aRequest, PRUint32 state)
 {
