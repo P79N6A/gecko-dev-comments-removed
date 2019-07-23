4ed02b57dbd95b0da953c14cf3eeca47d90494a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:47:48 +0000

    Bug 333673 -  document.implementation != document.implementation, r=peterv
    UltraBlame original commit: ba0ca9a1c14c9a16fe9ac4ba30880a2240148652

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -1785,6 +1785,7 @@ NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN(nsDocument)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE_NSCOMPTR(mLayoutHistoryState)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE_NSCOMPTR(mOnloadBlocker)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE_NSCOMPTR(mFirstBaseNodeWithHref)
+  NS_IMPL_CYCLE_COLLECTION_TRAVERSE_NSCOMPTR(mDOMImplementation)
 
   
   
@@ -1837,6 +1838,7 @@ NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(nsDocument)
   NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mCachedRootContent)
   NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mDisplayDocument)
   NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mFirstBaseNodeWithHref)
+  NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mDOMImplementation)
 
   NS_IMPL_CYCLE_COLLECTION_UNLINK_USERDATA
 
@@ -4095,22 +4097,22 @@ nsDocument::GetDoctype(nsIDOMDocumentType** aDoctype)
 NS_IMETHODIMP
 nsDocument::GetImplementation(nsIDOMDOMImplementation** aImplementation)
 {
-  
-  
-  nsCOMPtr<nsIURI> uri;
-  NS_NewURI(getter_AddRefs(uri), "about:blank");
-  NS_ENSURE_TRUE(uri, NS_ERROR_OUT_OF_MEMORY);
-  PRBool hasHadScriptObject = PR_TRUE;
-  nsIScriptGlobalObject* scriptObject =
-    GetScriptHandlingObject(hasHadScriptObject);
-  NS_ENSURE_STATE(scriptObject || !hasHadScriptObject);
-  *aImplementation = new nsDOMImplementation(scriptObject, uri, uri,
-                                             NodePrincipal());
-  if (!*aImplementation) {
-    return NS_ERROR_OUT_OF_MEMORY;
+  if (!mDOMImplementation) {
+    nsCOMPtr<nsIURI> uri;
+    NS_NewURI(getter_AddRefs(uri), "about:blank");
+    NS_ENSURE_TRUE(uri, NS_ERROR_OUT_OF_MEMORY);
+    PRBool hasHadScriptObject = PR_TRUE;
+    nsIScriptGlobalObject* scriptObject =
+      GetScriptHandlingObject(hasHadScriptObject);
+    NS_ENSURE_STATE(scriptObject || !hasHadScriptObject);
+    mDOMImplementation = new nsDOMImplementation(scriptObject, uri, uri,
+                                                 NodePrincipal());
+    if (!mDOMImplementation) {
+      return NS_ERROR_OUT_OF_MEMORY;
+    }
   }
 
-  NS_ADDREF(*aImplementation);
+  NS_ADDREF(*aImplementation = mDOMImplementation);
 
   return NS_OK;
 }
