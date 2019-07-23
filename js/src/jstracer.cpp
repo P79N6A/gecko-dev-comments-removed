5ddb42e3ce140303e4c38e5fe16687e97542fa82
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:45:56 +0000

    Track new rval from JSOP_RETURN when constructing inline frames (same as JSOP_STOP).
    UltraBlame original commit: da0c32f2b9a2e87a33dc99dfb813b4c6d7b84bfc

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -2345,7 +2345,7 @@ js_FinishJIT(JSTraceMonitor *tm)
            stat.recorderStarted, stat.recorderAborted,
            stat.traceCompleted, stat.returnToDifferentLoopHeader, stat.treesTrashed,
            stat.slotPromoted, stat.unstableLoopVariable);
-    printf("monitor: triggered(%llu), exits(%llu), type mismatch(%llu), "
+    printf("monitor: triggered(%llu), exits (%llu), type mismatch(%llu), "
            "global mismatch(%llu)\n", stat.traceTriggered, stat.sideExitIntoInterpreter,
            stat.typeMapMismatchAtEntry, stat.globalShapeMismatchAtEntry);
 #endif
@@ -2538,9 +2538,7 @@ TraceRecorder::ifop()
         jsdouble d = asNumber(v);
         jsdpun u;
         u.d = 0;
-        guard(d == 0 || JSDOUBLE_IS_NaN(d),
-              lir->ins2(LIR_feq, get(&v), lir->insImmq(u.u64)),
-              BRANCH_EXIT);
+        guard((d == 0 || JSDOUBLE_IS_NaN(d)), lir->ins2(LIR_feq, get(&v), lir->insImmq(u.u64)), BRANCH_EXIT);
     } else if (JSVAL_IS_STRING(v)) {
         guard(JSSTRING_LENGTH(JSVAL_TO_STRING(v)) == 0,
               lir->ins_eq0(lir->ins2(LIR_piand,
@@ -3295,11 +3293,14 @@ bool
 TraceRecorder::record_JSOP_RETURN()
 {
     jsval& rval = stackval(-1);
+    JSStackFrame *fp = cx->fp;
     if (cx->fp->flags & JSFRAME_CONSTRUCTING) {
-        
+        JS_ASSERT(OBJECT_TO_JSVAL(fp->thisp) == fp->argv[-1]);
+        rval_ins = get(&fp->argv[-1]);
+    } else {
+        rval_ins = get(&rval);
     }
     debug_only_v(printf("returning from %s\n", js_AtomToPrintableString(cx, cx->fp->fun->atom)););
-    rval_ins = get(&rval);
     clearFrameSlotsFromCache();
     return true;
 }
@@ -3820,7 +3821,7 @@ TraceRecorder::record_JSOP_TYPEOF()
 bool
 TraceRecorder::record_JSOP_VOID()
 {
-    stack(-1, lir->insImm(JSVAL_TO_BOOLEAN(JSVAL_VOID)));
+    stack(0, lir->insImm(JSVAL_TO_BOOLEAN(JSVAL_VOID)));
     return true;
 }
 
@@ -3934,7 +3935,7 @@ TraceRecorder::record_JSOP_SETPROP()
     JSObject* obj = JSVAL_TO_OBJECT(l);
 
     if (obj->map->ops->setProperty != js_SetProperty)
-        ABORT_TRACE("non-native JSObjectOps::setProperty");
+        ABORT_TRACE("non-native setProperty");
 
     LIns* obj_ins = get(&l);
 
@@ -5034,82 +5035,7 @@ TraceRecorder::record_JSOP_THROW()
 bool
 TraceRecorder::record_JSOP_IN()
 {
-    jsval& rval = stackval(-1);
-    if (JSVAL_IS_PRIMITIVE(rval))
-        ABORT_TRACE("JSOP_IN on non-object right operand");
-
-    jsval& lval = stackval(-2);
-    if (!JSVAL_IS_PRIMITIVE(lval))
-        ABORT_TRACE("JSOP_IN on E4X QName left operand");
-
-    jsid id;
-    if (JSVAL_IS_INT(lval)) {
-        id = INT_JSVAL_TO_JSID(lval);
-    } else {
-        if (!js_ValueToStringId(cx, lval, &id))
-            ABORT_TRACE("OOM under js_ValueToStringId in JSOP_IN");
-        lval = ID_TO_VALUE(id);
-    }
-
-    
-    
-    
-    JSObject* obj = JSVAL_TO_OBJECT(rval);
-    LIns* obj_ins = get(&rval);
-
-    bool cond;
-    LIns* x;
-    do {
-        if (guardDenseArray(obj, obj_ins)) {
-            if (JSVAL_IS_INT(lval)) {
-                jsint idx = JSVAL_TO_INT(lval);
-                LIns* idx_ins = f2i(get(&lval));
-                LIns* dslots_ins = lir->insLoad(LIR_ldp, obj_ins, offsetof(JSObject, dslots));
-                if (!guardDenseArrayIndex(obj, idx, obj_ins, dslots_ins, idx_ins))
-                    ABORT_TRACE("dense array index out of bounds");
-
-                cond = obj->dslots[idx] != JSVAL_HOLE;
-                x = lir->ins_eq0(lir->ins2(LIR_eq,
-                                           lir->insLoad(LIR_ldp, dslots_ins, idx * sizeof(jsval)),
-                                           INS_CONST(JSVAL_HOLE)));
-                break;
-            }
-
-            
-            obj = STOBJ_GET_PROTO(obj);
-            obj_ins = stobj_get_fslot(obj_ins, JSSLOT_PROTO);
-        } else {
-            if (JSVAL_IS_INT(id))
-                ABORT_TRACE("INT in OBJ where OBJ is not a dense array");
-        }
-
-        JSObject* obj2;
-        JSProperty* prop;
-        if (!OBJ_LOOKUP_PROPERTY(cx, obj, id, &obj2, &prop))
-            ABORT_TRACE("OBJ_LOOKUP_PROPERTY failed in JSOP_IN");
-
-        cond = prop != NULL;
-        if (prop)
-            OBJ_DROP_PROPERTY(cx, obj2, prop);
-
-        LIns* args[] = { get(&lval), obj_ins, cx_ins };
-        x = lir->insCall(F_HasNamedProperty, args);
-        guard(false, lir->ins2i(LIR_eq, x, 2), OOM_EXIT);
-        x = lir->ins2i(LIR_eq, x, 1);
-    } while (0);
-
-    
-
-    if (cx->fp->regs->pc[1] == JSOP_IFEQ || cx->fp->regs->pc[1] == JSOP_IFNE)
-        guard(cond, x, BRANCH_EXIT);
-
-    
-
-
-
-
-    set(&lval, x);
-    return true;
+    return false;
 }
 
 bool
