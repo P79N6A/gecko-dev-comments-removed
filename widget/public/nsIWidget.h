555ee215368ff244af5e2a04e1411ad15b02d964
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:44:19 +0000

    Bug 387594 - Kill old gfx code in nsIWidget. Patch by Jonathan Steele <xfsunoles@gmail.com>. r+sr=roc.
    UltraBlame original commit: 0f5b3289a6dda1b7a4d2a7ddebe80f382bd8e991

--- widget/public/nsIWidget.h
+++ widget/public/nsIWidget.h
@@ -63,10 +63,7 @@ class   nsIRollupListener;
 class   nsGUIEvent;
 struct  nsColorMap;
 class   imgIContainer;
-
-#ifdef MOZ_CAIRO_GFX
 class   gfxASurface;
-#endif
 
 
 
@@ -1030,12 +1027,10 @@ class nsIWidget : public nsISupports {
 
     NS_IMETHOD GetLastInputEventTime(PRUint32& aTime) = 0;
 
-#ifdef MOZ_CAIRO_GFX
     
 
 
     virtual gfxASurface *GetThebesSurface() = 0;
-#endif
 
     
 
