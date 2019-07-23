ca29d3bfdac87e38d41d914f305e8089e615dda3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:42:35 +0000

    Bug 513952 Refactor nsTSMManager r=josh
    UltraBlame original commit: 7d4dcb4ed7ee229e80fd6770c0bba22705c78431

---
+++ widget/src/cocoa/nsCocoaTextInputHandler.h
@@ -0,0 +1,326 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsCocoaTextInputHandler_h_
+#define nsCocoaTextInputHandler_h_
+
+#include "nsCocoaUtils.h"
+
+#ifdef NS_LEOPARD_AND_LATER
+
+#import <Carbon/Carbon.h>
+#import <Cocoa/Cocoa.h>
+#include "mozView.h"
+#include "nsString.h"
+#include "nsCOMPtr.h"
+#include "nsITimer.h"
+#include "npapi.h"
+
+struct PRLogModuleInfo;
+class nsChildView;
+
+
+
+
+
+
+
+
+
+class nsTISInputSource
+{
+public:
+  nsTISInputSource()
+  {
+    mInputSourceList = nsnull;
+    Clear();
+  }
+
+  nsTISInputSource(const char* aID)
+  {
+    mInputSourceList = nsnull;
+    InitByInputSourceID(aID);
+  }
+
+  nsTISInputSource(SInt32 aLayoutID)
+  {
+    mInputSourceList = nsnull;
+    InitByLayoutID(aLayoutID);
+  }
+
+  nsTISInputSource(TISInputSourceRef aInputSource)
+  {
+    mInputSourceList = nsnull;
+    InitByTISInputSourceRef(aInputSource);
+  }
+
+  ~nsTISInputSource() { Clear(); }
+
+  void InitByInputSourceID(const char* aID);
+  void InitByInputSourceID(const nsAFlatString &aID);
+  void InitByInputSourceID(const CFStringRef aID);
+  void InitByLayoutID(SInt32 aLayoutID);
+  void InitByCurrentInputSource();
+  void InitByCurrentKeyboardLayout();
+  void InitByCurrentASCIICapableInputSource();
+  void InitByCurrentASCIICapableKeyboardLayout();
+  void InitByTISInputSourceRef(TISInputSourceRef aInputSource);
+  void InitByLanguage(CFStringRef aLanguage);
+
+  const UCKeyboardLayout* GetUCKeyboardLayout();
+
+  PRBool IsOpenedIMEMode();
+  PRBool IsIMEMode();
+
+  PRBool IsASCIICapable()
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetBoolProperty(kTISPropertyInputSourceIsASCIICapable);
+  }
+
+  PRBool IsEnabled()
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetBoolProperty(kTISPropertyInputSourceIsEnabled);
+  }
+
+  PRBool GetLanguageList(CFArrayRef &aLanguageList);
+  PRBool GetPrimaryLanguage(CFStringRef &aPrimaryLanguage);
+  PRBool GetPrimaryLanguage(nsAString &aPrimaryLanguage);
+
+  PRBool GetLocalizedName(CFStringRef &aName)
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetStringProperty(kTISPropertyLocalizedName, aName);
+  }
+
+  PRBool GetLocalizedName(nsAString &aName)
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetStringProperty(kTISPropertyLocalizedName, aName);
+  }
+
+  PRBool GetInputSourceID(CFStringRef &aID)
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetStringProperty(kTISPropertyInputSourceID, aID);
+  }
+
+  PRBool GetInputSourceID(nsAString &aID)
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetStringProperty(kTISPropertyInputSourceID, aID);
+  }
+
+  PRBool GetBundleID(CFStringRef &aBundleID)
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetStringProperty(kTISPropertyBundleID, aBundleID);
+  }
+
+  PRBool GetBundleID(nsAString &aBundleID)
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetStringProperty(kTISPropertyBundleID, aBundleID);
+  }
+
+  PRBool GetInputSourceType(CFStringRef &aType)
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetStringProperty(kTISPropertyInputSourceType, aType);
+  }
+
+  PRBool GetInputSourceType(nsAString &aType)
+  {
+    NS_ENSURE_TRUE(mInputSource, PR_FALSE);
+    return GetStringProperty(kTISPropertyInputSourceType, aType);
+  }
+
+  void Select();
+
+protected:
+  void Clear();
+
+  PRBool GetBoolProperty(const CFStringRef aKey);
+  PRBool GetStringProperty(const CFStringRef aKey, CFStringRef &aStr);
+  PRBool GetStringProperty(const CFStringRef aKey, nsAString &aStr);
+
+  TISInputSourceRef mInputSource;
+  CFArrayRef mInputSourceList;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class nsCocoaIMEHandler
+{
+public:
+  nsCocoaIMEHandler();
+  virtual ~nsCocoaIMEHandler();
+
+  virtual void Init(nsChildView* aOwner);
+
+  virtual void OnFocusChangeInGecko(PRBool aFocus);
+  virtual void OnDestroyView(NSView<mozView> *aDestroyingView);
+
+  void OnStartIMEComposition(NSView<mozView> *aView);
+  void OnUpdateIMEComposition(NSString* aIMECompositionString);
+  void OnEndIMEComposition();
+
+  PRBool IsIMEComposing() { return mIsIMEComposing; }
+  PRBool IsIMEOpened();
+  PRBool IsIMEEnabled() { return mIsIMEEnabled; }
+  PRBool IsASCIICapableOnly() { return mIsASCIICapableOnly; }
+  PRBool IgnoreIMECommit() { return mIgnoreIMECommit; }
+
+  PRBool IgnoreIMEComposition()
+  {
+    
+    
+    return (mPendingMethods & kDiscardIMEComposition) &&
+           (mIsInFocusProcessing || !IsFocused());
+  }
+
+  void CommitIMEComposition();
+  void CancelIMEComposition();
+
+  void EnableIME(PRBool aEnableIME);
+  void SetIMEOpenState(PRBool aOpen);
+  void SetASCIICapableOnly(PRBool aASCIICapableOnly);
+
+  static CFArrayRef CreateAllIMEModeList();
+  static void DebugPrintAllIMEModes(PRLogModuleInfo* aLogModuleInfo);
+
+protected:
+  
+  
+  nsChildView* mOwnerWidget;
+
+  
+  
+  NSView<mozView>* mView;
+
+  
+  
+  
+  nsCOMPtr<nsITimer> mTimer;
+  enum {
+    kResetIMEWindowLevel     = 1,
+    kDiscardIMEComposition   = 2,
+    kSyncASCIICapableOnly    = 4
+  };
+  PRUint32 mPendingMethods;
+
+  PRBool IsFocused();
+  void ResetTimer();
+
+  virtual void ExecutePendingMethods();
+
+private:
+  
+  NSString* mIMECompositionString;
+
+  PRPackedBool mIsIMEComposing;
+  PRPackedBool mIsIMEEnabled;
+  PRPackedBool mIsASCIICapableOnly;
+  PRPackedBool mIgnoreIMECommit;
+  
+  
+  
+  
+  PRPackedBool mIsInFocusProcessing;
+
+  void KillIMEComposition();
+  void SendCommittedText(NSString *aString);
+  void OpenSystemPreferredLanguageIME();
+
+  
+  void ResetIMEWindowLevel();
+  void DiscardIMEComposition();
+  void SyncASCIICapableOnly();
+
+  static PRBool sStaticMembersInitialized;
+  static CFStringRef sLatestIMEOpenedModeInputSourceID;
+  static void InitStaticMembers();
+  static void OnCurrentTextInputSourceChange(CFNotificationCenterRef aCenter,
+                                             void* aObserver,
+                                             CFStringRef aName,
+                                             const void* aObject,
+                                             CFDictionaryRef aUserInfo);
+
+  static void FlushPendingMethods(nsITimer* aTimer, void* aClosure);
+
+  
+  
+  
+  
+  
+  static nsCocoaIMEHandler* sFocusedIMEHandler;
+};
+
+
+
+
+class nsCocoaTextInputHandler : public nsCocoaIMEHandler
+{
+public:
+  static CFArrayRef CreateAllKeyboardLayoutList();
+  static void DebugPrintAllKeyboardLayouts(PRLogModuleInfo* aLogModuleInfo);
+
+  nsCocoaTextInputHandler();
+  virtual ~nsCocoaTextInputHandler();
+};
+
+#endif 
+
+#endif 
