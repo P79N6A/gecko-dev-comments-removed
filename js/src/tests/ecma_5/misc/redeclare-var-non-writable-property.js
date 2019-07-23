74139d55cb0e6fdac7259e5c930419a9ce576b17
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:07:37 +0000

    Bug 539488 - |var| statements for existing, read-only/permanent properties should not be errors.  r=jorendorff
    UltraBlame original commit: 7e68181ddedbc2b2fc6d11f3aeab1e57e55d6f90

---
+++ js/src/tests/ecma_5/misc/redeclare-var-non-writable-property.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+var gTestfile = 'redeclare-var-non-writable-property.js';
+
+var BUGNUMBER = 539488;
+var summary =
+  '|var| statements for existing, read-only/permanent properties should not ' +
+  'be errors';
+
+print(BUGNUMBER + ": " + summary);
+
+
+
+
+
+var undefined;
+
+
+
+reportCompare(true, true);
+
+print("All tests passed!");
