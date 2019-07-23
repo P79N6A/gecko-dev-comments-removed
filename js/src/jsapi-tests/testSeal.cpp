2d26161f3f129e896690d421e6978eb2308de14a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:02:44 +0000

    Bug 535703 - JS_SealObject crash with deep=true. r=mrbkap.
    UltraBlame original commit: 968dc5d58a7f934bed7f08015387617aba6a1a36

---
+++ js/src/jsapi-tests/testSeal.cpp
@@ -0,0 +1,10 @@
+#include "tests.h"
+
+BEGIN_TEST(testSeal_bug535703)
+{
+    JSObject *obj = JS_NewObject(cx, NULL, NULL, NULL);
+    CHECK(obj);
+    JS_SealObject(cx, obj, JS_TRUE);  
+    return true;
+}
+END_TEST(testSeal_bug535703)
