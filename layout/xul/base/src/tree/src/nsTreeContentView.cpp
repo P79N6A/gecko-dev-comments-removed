5df915c7540e4c3e34a232ee8be6a612cce9ac2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:53 +0000

    Bug 377035 Allow <tree> to be anonymous parent of <treechildren> r=Enn sr=roc
    UltraBlame original commit: df0fc8a849e19ea9dbafbd551e90a8d18af27477

--- layout/xul/base/src/tree/src/nsTreeContentView.cpp
+++ layout/xul/base/src/tree/src/nsTreeContentView.cpp
@@ -556,9 +556,9 @@ nsTreeContentView::SetTree(nsITreeBoxObject* aTree)
     nsCOMPtr<nsIDOMElement> bodyElement;
     mBoxObject->GetTreeBody(getter_AddRefs(bodyElement));
     if (bodyElement) {
-      nsCOMPtr<nsIContent> bodyContent = do_QueryInterface(bodyElement);
+      mBody = do_QueryInterface(bodyElement);
       PRInt32 index = 0;
-      Serialize(bodyContent, -1, &index, mRows);
+      Serialize(mBody, -1, &index, mRows);
     }
   }
 
@@ -819,17 +819,15 @@ nsTreeContentView::AttributeChanged(nsIDocument *aDocument,
   }
 
   
-  nsCOMPtr<nsIContent> parent = aContent;
-  nsINodeInfo *ni = nsnull;
-  do {
-    parent = parent->GetParent();
-    if (parent)
-      ni = parent->NodeInfo();
-  } while (parent && !ni->Equals(nsGkAtoms::tree, kNameSpaceID_XUL));
+  
 
-  if (parent != mRoot) {
-    
-    return;
+  for (nsIContent* element = aContent; element != mBody; element = element->GetParent()) {
+    if (!element)
+      return; 
+    nsIAtom *parentTag = element->Tag();
+    if ((element->IsNodeOfType(nsINode::eXUL) && parentTag == nsGkAtoms::tree) ||
+        (element->IsNodeOfType(nsINode::eHTML) && parentTag == nsGkAtoms::select))
+      return; 
   }
 
   
@@ -989,7 +987,7 @@ nsTreeContentView::ContentInserted(nsIDocument *aDocument,
   
   
 
-  for (nsIContent* element = aContainer; element != mRoot; element = element->GetParent()) {
+  for (nsIContent* element = aContainer; element != mBody; element = element->GetParent()) {
     if (!element)
       return; 
     nsIAtom *parentTag = element->Tag();
@@ -1072,11 +1070,11 @@ nsTreeContentView::ContentRemoved(nsIDocument *aDocument,
   
   
 
-  for (nsIContent* element = aContainer; element != mRoot; element = element->GetParent()) {
+  for (nsIContent* element = aContainer; element != mBody; element = element->GetParent()) {
     if (!element)
       return; 
     nsIAtom *parentTag = element->Tag();
-    if ((element->IsNodeOfType(nsINode::eXUL) && parentTag == nsGkAtoms::tree) || 
+    if ((element->IsNodeOfType(nsINode::eXUL) && parentTag == nsGkAtoms::tree) ||
         (element->IsNodeOfType(nsINode::eHTML) && parentTag == nsGkAtoms::select))
       return; 
   }
@@ -1439,6 +1437,7 @@ nsTreeContentView::ClearRows()
     Row::Destroy(mAllocator, (Row*)mRows[i]);
   mRows.Clear();
   mRoot = nsnull;
+  mBody = nsnull;
   
   if (mDocument) {
     mDocument->RemoveObserver(this);
