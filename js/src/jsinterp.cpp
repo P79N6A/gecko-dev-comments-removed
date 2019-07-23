feebb9e3b17f8e566f654ace1d9191e93a9d7332
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:48:30 +0000

    Back out patch for bug 454039.
    UltraBlame original commit: 3bee5d8ea9ac07704f4fd34cd4b511911e6588a7

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -2510,7 +2510,7 @@ js_Interpret(JSContext *cx)
 
 # ifdef JS_TRACER
 #  define CHECK_RECORDER()  JS_BEGIN_MACRO                                    \
-                                JS_ASSERT(!TRACE_RECORDER(cx) ^               \
+                                JS_ASSERT(!JS_TRACE_MONITOR(cx).recorder ^    \
                                           (jumpTable == recordingJumpTable)); \
                             JS_END_MACRO
 # else
@@ -2569,11 +2569,11 @@ js_Interpret(JSContext *cx)
 #ifdef JS_TRACER
     
     TraceRecorder *tr = NULL;
-    if (JS_ON_TRACE(cx)) {
-        tr = TRACE_RECORDER(cx);
-        SET_TRACE_RECORDER(cx, NULL);
+    if (JS_TRACE_MONITOR(cx).onTrace) {
+        tr = JS_TRACE_MONITOR(cx).recorder;
+        JS_TRACE_MONITOR(cx).recorder = NULL;
     }
-#endif
+#endif    
 
     
     JS_CHECK_RECURSION(cx, return JS_FALSE);
@@ -2695,13 +2695,13 @@ js_Interpret(JSContext *cx)
 # define LOAD_INTERRUPT_HANDLER(cx)                                           \
     ((void) (jumpTable = (cx)->debugHooks->interruptHandler                   \
                          ? interruptJumpTable                                 \
-                         : TRACE_RECORDER(cx)                                 \
+                         : JS_TRACE_MONITOR(cx).recorder                      \
                          ? recordingJumpTable                                 \
                          : normalJumpTable))
 # define ENABLE_TRACER(flag)                                                  \
     JS_BEGIN_MACRO                                                            \
         bool flag_ = (flag);                                                  \
-        JS_ASSERT(flag_ == !!TRACE_RECORDER(cx));                             \
+        JS_ASSERT(flag_ == !!JS_TRACE_MONITOR(cx).recorder);                  \
         jumpTable = flag_ ? recordingJumpTable : normalJumpTable;             \
     JS_END_MACRO
 #else 
@@ -2715,11 +2715,12 @@ js_Interpret(JSContext *cx)
 #ifdef JS_TRACER
 # define LOAD_INTERRUPT_HANDLER(cx)                                           \
     ((void) (switchMask = ((cx)->debugHooks->interruptHandler ||              \
-                           TRACE_RECORDER(cx)) ? 0 : 255))
+                           JS_TRACE_MONITOR(cx).recorder)                     \
+                          ? 0 : 255))
 # define ENABLE_TRACER(flag)                                                  \
     JS_BEGIN_MACRO                                                            \
         bool flag_ = (flag);                                                  \
-        JS_ASSERT(flag_ == !!TRACE_RECORDER(cx));                             \
+        JS_ASSERT(flag_ == !!JS_TRACE_MONITOR(cx).recorder);                  \
         switchMask = flag_ ? 0 : 255;                                         \
     JS_END_MACRO
 #else 
@@ -3021,7 +3022,7 @@ js_Interpret(JSContext *cx)
                 inlineCallCount--;
                 if (JS_LIKELY(ok)) {
 #ifdef JS_TRACER
-                    if (TRACE_RECORDER(cx))
+                    if (JS_TRACE_MONITOR(cx).recorder)
                         RECORD(LeaveFrame);
 #endif
                     JS_ASSERT(js_CodeSpec[*regs.pc].length == JSOP_CALL_LENGTH);
@@ -3261,6 +3262,7 @@ js_Interpret(JSContext *cx)
                 JS_ASSERT(op == JSOP_FORNAME);
 
                 
+
 
 
 
@@ -4437,7 +4439,8 @@ js_Interpret(JSContext *cx)
 
 
 
-                    entry = &cache->table[PROPERTY_CACHE_HASH_PC(regs.pc, kshape)];
+                    entry = &cache->table[PROPERTY_CACHE_HASH_PC(regs.pc,
+                                                                 kshape)];
                     PCMETER(cache->tests++);
                     PCMETER(cache->settests++);
                     if (entry->kpc == regs.pc && entry->kshape == kshape) {
@@ -4450,8 +4453,6 @@ js_Interpret(JSContext *cx)
                             sprop = PCVAL_TO_SPROP(entry->vword);
                             JS_ASSERT(!(sprop->attrs & JSPROP_READONLY));
                             JS_ASSERT(!SCOPE_IS_SEALED(OBJ_SCOPE(obj)));
-
-                            TRACE_2(SetPropHit, kshape, sprop);
 
                             if (scope->object == obj) {
                                 
@@ -4907,7 +4908,7 @@ js_Interpret(JSContext *cx)
                     cx->fp = fp = &newifp->frame;
 
 #ifdef JS_TRACER
-                    if (TRACE_RECORDER(cx))
+                    if (JS_TRACE_MONITOR(cx).recorder)
                         RECORD(EnterFrame);
 #endif
 
@@ -6120,8 +6121,6 @@ js_Interpret(JSContext *cx)
                     if (sprop->parent != scope->lastProp)
                         goto do_initprop_miss;
 
-                    TRACE_2(SetPropHit, kshape, sprop);
-
                     
 
 
@@ -7027,7 +7026,7 @@ js_Interpret(JSContext *cx)
     JS_ASSERT(inlineCallCount == 0);
     JS_ASSERT(fp->regs == &regs);
 #ifdef JS_TRACER
-    if (TRACE_RECORDER(cx))
+    if (JS_TRACE_MONITOR(cx).recorder)
         js_AbortRecording(cx, regs.pc, "recording out of js_Interpret");
 #endif
     if (JS_UNLIKELY(fp->flags & JSFRAME_YIELDING)) {
@@ -7052,12 +7051,12 @@ js_Interpret(JSContext *cx)
         js_SetVersion(cx, originalVersion);
     --cx->interpLevel;
 
-#ifdef JS_TRACER
+#ifdef JS_TRACER    
     if (tr) {
-        SET_TRACE_RECORDER(cx, tr);
+        JS_TRACE_MONITOR(cx).recorder = tr;
         tr->deepAbort();
     }
-#endif
+#endif    
     return ok;
 
   atom_not_defined:
