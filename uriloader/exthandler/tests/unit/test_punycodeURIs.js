583fc2cf3febe3fd0272bc300623618135d0eb42
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:28:29 +0000

    Bug 456606 - test_punycodeURIs.js failure on Mac OS X 10.4 (currently worked around); (Av2) Remove a useless createInstance(), Whitespace cleanup, Remove MacOSX 10.4 workaround (on m-c). r=cbiesinger.
    UltraBlame original commit: 95310a8ac7651275faaa25fe32836a65c4e7778a

--- uriloader/exthandler/tests/unit/test_punycodeURIs.js
+++ uriloader/exthandler/tests/unit/test_punycodeURIs.js
@@ -46,15 +46,13 @@ var gCheckExistsAttempts = 0;
 
 function checkFile() {
   
-  var tempFile = Components.classes["@mozilla.org/file/local;1"].
-    createInstance(Components.interfaces.nsILocalFile);
-  tempFile = do_get_cwd();
+  var tempFile = do_get_cwd();
   tempFile.append(kOutputFile);
 
   if (!tempFile.exists()) {
     if (gCheckExistsAttempts >= kMaxCheckExistAttempts) {
       do_throw("Expected File " + tempFile.path + " does not exist after " +
-	       kMaxCheckExistAttempts + " seconds");
+                 kMaxCheckExistAttempts + " seconds");
     }
     else {
       
@@ -63,12 +61,14 @@ function checkFile() {
   }
 
   
-  var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].
-    createInstance(Components.interfaces.nsIFileInputStream);
-  var sstream = Components.classes["@mozilla.org/scriptableinputstream;1"].
-    createInstance(Components.interfaces.nsIScriptableInputStream);
+  var fstream =
+    Components.classes["@mozilla.org/network/file-input-stream;1"]
+              .createInstance(Components.interfaces.nsIFileInputStream);
+  var sstream =
+    Components.classes["@mozilla.org/scriptableinputstream;1"]
+              .createInstance(Components.interfaces.nsIScriptableInputStream);
   fstream.init(tempFile, -1, 0, 0);
-  sstream.init(fstream); 
+  sstream.init(fstream);
 
   
   
@@ -93,24 +93,14 @@ function checkFile() {
 
 function run_test() {
   
-  const httpHandler =
-    Components.classes["@mozilla.org/network/protocol;1?name=http"]
-              .getService(Components.interfaces.nsIHttpProtocolHandler);
-  const oscpu = httpHandler.oscpu;
-  if (oscpu.match(/Mac OS X 10.4/)) {
-    dump("This test is not run on Mac OS 10.4 because it fails for unknown " +
-         "reasons. See bug 447999.\n");
-    return;
-  }
+  var ioService =
+    Components.classes["@mozilla.org/network/io-service;1"]
+              .getService(Components.interfaces.nsIIOService);
 
   
-  var ioService = Components.classes["@mozilla.org/network/io-service;1"].
-    getService(Components.interfaces.nsIIOService);
-
-  
-  var localHandler = 
-    Components.classes["@mozilla.org/uriloader/local-handler-app;1"].
-    createInstance(Components.interfaces.nsILocalHandlerApp);
+  var localHandler =
+    Components.classes["@mozilla.org/uriloader/local-handler-app;1"]
+              .createInstance(Components.interfaces.nsILocalHandlerApp);
   localHandler.name = "Test Local Handler App";
 
   
@@ -129,8 +119,9 @@ function run_test() {
   outFile.append(kOutputFile);
 
   
-  var envSvc = Components.classes["@mozilla.org/process/environment;1"].
-    getService(Components.interfaces.nsIEnvironment);
+  var envSvc =
+    Components.classes["@mozilla.org/process/environment;1"]
+              .getService(Components.interfaces.nsIEnvironment);
 
   
   
