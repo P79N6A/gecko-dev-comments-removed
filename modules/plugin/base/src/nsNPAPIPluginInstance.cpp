579a18b2be2428efd18df87e4019c61c9fe01a07
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:35:34 +0000

    Backed out changeset abd5a41c0b70
    UltraBlame original commit: 8921ce0e628239566649a7067fd369c2ce43490a

--- modules/plugin/base/src/nsNPAPIPluginInstance.cpp
+++ modules/plugin/base/src/nsNPAPIPluginInstance.cpp
@@ -841,6 +841,8 @@ NS_IMETHODIMP nsNPAPIPluginInstance::Stop(void)
   }
 
   if (!mStarted) {
+    
+    mPeer = nsnull;
     return NS_OK;
   }
 
@@ -859,6 +861,8 @@ NS_IMETHODIMP nsNPAPIPluginInstance::Stop(void)
   OnPluginDestroy(&fNPP);
 
   if (fCallbacks->destroy == NULL) {
+    
+    mPeer = nsnull;
     return NS_ERROR_FAILURE;
   }
 
@@ -885,6 +889,9 @@ NS_IMETHODIMP nsNPAPIPluginInstance::Stop(void)
   ("NPP Destroy called: this=%p, npp=%p, return=%d\n", this, &fNPP, error));
 
   nsJSNPRuntime::OnPluginDestroy(&fNPP);
+
+  
+  mPeer = nsnull;
 
   if (error != NPERR_NO_ERROR)
     return NS_ERROR_FAILURE;
--- modules/plugin/base/src/nsPluginHostImpl.cpp
+++ modules/plugin/base/src/nsPluginHostImpl.cpp
@@ -5909,16 +5909,6 @@ nsPluginHostImpl::StopPluginInstance(nsIPluginInstance* aInstance)
       }
     }
   }
-
-  nsCOMPtr<nsIPluginInstancePeer> peer;
-  aInstance->GetPeer(getter_AddRefs(peer));
-
-  if (peer) {
-    
-    
-    ((nsPluginInstancePeerImpl*)peer.get())->SetOwner(nsnull);
-  }
-
   return NS_OK;
 }
 
