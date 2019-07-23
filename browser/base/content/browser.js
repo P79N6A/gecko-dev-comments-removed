c1cd124804fe132bf63181fbb8c36e5ec4d0dafd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:08:15 +0000

    Bug 436304 - Implement All Tabs panel with previews and search. r=connor
    UltraBlame original commit: bf2f8ad8f1ce19a0c40007da760ec267089061c3

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1260,10 +1260,7 @@ function delayedStartup(isLoadingBlank, mustLoadSidebar) {
   gBrowser.addEventListener("command", BrowserOnCommand, false);
 
   tabPreviews.init();
-  if ((!gPrefService.prefHasUserValue("browser.ctrlTab.disallowForScreenReaders") ||
-       !gPrefService.getBoolPref("browser.ctrlTab.disallowForScreenReaders")) &&
-       gPrefService.getBoolPref("browser.ctrlTab.mostRecentlyUsed"))
-    ctrlTab.init();
+  ctrlTab.init();
 
   
   
