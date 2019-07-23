9deb0ea27cb3f71021d73797cf7144281f0b1d4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:46:32 +0000

    Bug 370353: Dynamically setting "visibility: collapse" to a <col> no longer works. Patch by Daniel Holbert <dholbert@mozilla.com>. r=bernd sr=dbaron a1.9=dbaron
    UltraBlame original commit: 523647a0ccd86cd01b90214bef92a24e6ba40d3d

--- layout/tables/nsTableFrame.cpp
+++ layout/tables/nsTableFrame.cpp
@@ -1845,6 +1845,7 @@ NS_METHOD nsTableFrame::Reflow(nsPresContext*          aPresContext,
 
   PRBool haveDesiredHeight = PR_FALSE;
   PRBool reflowedChildren  = PR_FALSE;
+  SetHaveReflowedColGroups(PR_FALSE);
 
   if (aReflowState.ComputedHeight() != NS_UNCONSTRAINEDSIZE ||
       
@@ -3061,6 +3062,7 @@ nsTableFrame::ReflowColGroups(nsIRenderingContext *aRenderingContext)
     nsPresContext *presContext = PresContext();
     for (nsIFrame* kidFrame = mColGroups.FirstChild(); kidFrame;
          kidFrame = kidFrame->GetNextSibling()) {
+      if (NS_SUBTREE_DIRTY(kidFrame)) {
       
       nsHTMLReflowState kidReflowState(presContext, kidFrame,
                                        aRenderingContext, nsSize(0,0));
@@ -3068,6 +3070,7 @@ nsTableFrame::ReflowColGroups(nsIRenderingContext *aRenderingContext)
       ReflowChild(kidFrame, presContext, kidMet, kidReflowState, 0, 0, 0,
                   cgStatus);
       FinishReflowChild(kidFrame, presContext, nsnull, kidMet, 0, 0, 0);
+    }
     }
     SetHaveReflowedColGroups(PR_TRUE);
   }
