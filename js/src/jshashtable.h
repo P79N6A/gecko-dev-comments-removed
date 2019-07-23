9efe6cd97aeb58fc81003f603274577e9ab6d5c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:15:41 +0000

    Attempt to prevent MSVC ICE during PGO
    UltraBlame original commit: ce654228dabec7cb521765c50bc388502a7a72ac

--- js/src/jshashtable.h
+++ js/src/jshashtable.h
@@ -278,6 +278,13 @@ class HashTable : AllocPolicy
 #endif
     {}
 
+
+
+
+
+#ifdef _MSC_VER
+# pragma optimize("", off)
+#endif
     bool init(uint32 capacity)
     {
         if (capacity < sMinSize)
@@ -296,6 +303,9 @@ class HashTable : AllocPolicy
         METER(memset(&stats, 0, sizeof(stats)));
         return true;
     }
+#ifdef _MSC_VER
+# pragma optimize("", on)
+#endif
 
     ~HashTable()
     {
