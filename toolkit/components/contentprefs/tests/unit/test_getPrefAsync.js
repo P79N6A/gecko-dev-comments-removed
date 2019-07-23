0759353c062b85f94594d73bb8086a2cb68e8953
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:11:41 +0000

    Bug 539907 - Have getPref use asynchronous statements when called with an optional callback. r=myk, sr=robstrong
    UltraBlame original commit: 57c91cab646bb91ae4558e58f32f9535d7b36457

---
+++ toolkit/components/contentprefs/tests/unit/test_getPrefAsync.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var cps = Cc["@mozilla.org/content-pref/service;1"].
+          getService(Ci.nsIContentPrefService);
+var uri = ContentPrefTest.getURI("http://www.example.com/");
+
+function run_test() {
+  do_test_pending();
+
+  cps.setPref(uri, "asynctest", "pie");
+  do_check_eq(cps.getPref(uri, "asynctest"), "pie");
+
+  cps.getPref(uri, "asynctest", function(aValue) {
+    do_check_eq(aValue, "pie");
+    testCallbackObj();
+  });
+}
+
+function testCallbackObj() {
+  cps.getPref(uri, "asynctest", {
+    onResult: function(aValue) {
+      do_check_eq(aValue, "pie");
+      cps.removePref(uri, "asynctest");
+      testNoResult();
+    }
+  });
+}
+
+function testNoResult() {
+  cps.getPref(uri, "asynctest", function(aValue) {
+    do_check_eq(aValue, undefined);
+    do_test_finished();
+  });
+}
