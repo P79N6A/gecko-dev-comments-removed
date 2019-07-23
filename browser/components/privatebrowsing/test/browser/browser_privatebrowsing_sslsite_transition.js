cd23cde9e65141b537204e0c931d081c820cab36
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:03:30 +0000

    Bug 463256 - SSL sites sometimes don't load after re-entering regular browsing mode (from Private Browsing); r=mconnor,sdwilsh
    UltraBlame original commit: 3f5521e1c5a3187bf0b0cc7db2c5de95c7e21f53

---
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_sslsite_transition.js
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  let prefBranch = Cc["@mozilla.org/preferences-service;1"].
+                   getService(Ci.nsIPrefBranch);
+  prefBranch.setBoolPref("browser.privatebrowsing.keep_current_session", true);
+  let pb = Cc["@mozilla.org/privatebrowsing;1"].
+           getService(Ci.nsIPrivateBrowsingService);
+
+  const kTestURL = "https://example.com/";
+
+  
+  let tab = gBrowser.addTab();
+  gBrowser.selectedTab = tab;
+  let browser = gBrowser.getBrowserForTab(tab);
+  browser.addEventListener("load", function() {
+    browser.removeEventListener("load", arguments.callee, true);
+
+    pb.privateBrowsingEnabled = true;
+    pb.privateBrowsingEnabled = false;
+
+    
+    browser.addEventListener("load", function() {
+      browser.removeEventListener("load", arguments.callee, true);
+
+      is(browser.contentWindow.location, kTestURL,
+        "The original SSL page should be loaded at this stage");
+
+      gBrowser.removeTab(tab);
+      prefBranch.clearUserPref("browser.privatebrowsing.keep_current_session");
+      finish();
+    }, true);
+
+    executeSoon(function(){
+      browser.contentWindow.location = kTestURL;
+    });
+  }, true);
+  browser.contentWindow.location = kTestURL;
+
