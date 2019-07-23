6db56f34754f62a7bbee94106fbdc67b812f288c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:54 +0000

    Bug 380197: fixing regression from bug 380197. r=brendan
    UltraBlame original commit: 2f9fcd67197875692d2c52eb1b8ee93d3773cad3

--- js/src/jsopcode.c
+++ js/src/jsopcode.c
@@ -1719,6 +1719,12 @@ Decompile(SprintStack *ss, jsbytecode *pc, intN nb)
         if (nb < 0 && -(nb + 1) == (intN)ss->top - cs->nuses + cs->ndefs)
             return pc;
 
+        
+
+
+
+        token = CodeToken[op];
+
         if (pc + oplen == jp->dvgfence) {
             JSStackFrame *fp;
             uint32 format, mode, type;
@@ -1818,7 +1824,6 @@ Decompile(SprintStack *ss, jsbytecode *pc, intN nb)
             jp->dvgfence = NULL;
         }
 
-        token = CodeToken[op];
         if (token) {
             switch (cs->nuses) {
               case 2:
