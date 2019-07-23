fe3b8089ef70b8620673a5cf11a533b92a3ecc3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:12:33 +0000

    Bug 489624 - Update mozStorageRow.* to follow style guidelines Consistent spacing, consistent naming, and namespaces - OH MY! r=asuth
    UltraBlame original commit: c9b6ca7c431f15f2da557647b02c86530790f4bb

--- storage/src/mozStoragePrivateHelpers.h
+++ storage/src/mozStoragePrivateHelpers.h
@@ -41,14 +41,25 @@
 #ifndef _mozStoragePrivateHelpers_h_
 #define _mozStoragePrivateHelpers_h_
 
+
+
+
+
 #include "mozStorage.h"
 
-
-
-
+struct sqlite3_stmt;
 
 namespace mozilla {
 namespace storage {
+
+
+
+
+#define ENSURE_INDEX_VALUE(aIndex, aCount) \
+  NS_ENSURE_TRUE(aIndex < aCount, NS_ERROR_INVALID_ARG)
+
+
+
 
 
 
