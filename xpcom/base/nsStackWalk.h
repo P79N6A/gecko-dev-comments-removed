cc37de7c7692f834ddd27f10eac2e3371f9963f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:46:01 +0000

    Split the address->symbol mapping from the rest of the stack walking code.  b=374829  r=bsmedberg  a=bzbarsky
    UltraBlame original commit: 07dcdf4c2fc595eb77bd2e4478925bcb94ba67c2

--- xpcom/base/nsStackWalk.h
+++ xpcom/base/nsStackWalk.h
@@ -47,7 +47,7 @@
 PR_BEGIN_EXTERN_C
 
 typedef void
-(* PR_CALLBACK NS_WalkStackCallback)(char *aFrame, void *aClosure);
+(* PR_CALLBACK NS_WalkStackCallback)(void *aPC, void *aClosure);
 
 
 
@@ -68,6 +68,60 @@ typedef void
 XPCOM_API(nsresult)
 NS_StackWalk(NS_WalkStackCallback aCallback, PRUint32 aSkipFrames,
              void *aClosure);
+
+typedef struct {
+    
+
+
+
+
+    char library[256];
+    unsigned long loffset;
+    
+
+
+
+
+    char filename[256];
+    unsigned long lineno;
+    
+
+
+
+    char function[256];
+    unsigned long foffset;
+} nsCodeAddressDetails;
+
+
+
+
+
+
+
+
+XPCOM_API(nsresult)
+NS_DescribeCodeAddress(void *aPC, nsCodeAddressDetails *aDetails);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+XPCOM_API(nsresult)
+NS_FormatCodeAddressDetails(void *aPC, const nsCodeAddressDetails *aDetails,
+                            char *aBuffer, PRUint32 aBufferSize);
 
 PR_END_EXTERN_C
 
