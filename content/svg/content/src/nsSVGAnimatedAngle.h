403281e827d92d5724ec8379ed4eb05e6cc09f87
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:50:26 +0000

    Backout of 397749 - suspicion of talos regression.
    UltraBlame original commit: c0ef978d8e12b4d8b10a4c75d03fccd1af3c5513

---
+++ content/svg/content/src/nsSVGAnimatedAngle.h
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef __NS_SVGANIMATEDANGLE_H__
+#define __NS_SVGANIMATEDANGLE_H__
+
+#include "nsIDOMSVGAnimatedAngle.h"
+#include "nsIDOMSVGAngle.h"
+
+nsresult NS_NewSVGAnimatedAngle(nsIDOMSVGAnimatedAngle** result,
+                                nsIDOMSVGAngle* baseVal);
+
+#endif 
