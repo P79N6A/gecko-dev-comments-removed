63d130c876b8f097f7abda3d5db4d44832bbe450
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:24:53 +0000

    Restore lost rt->protoHazardShape check (503860, r=jorendorff).
    UltraBlame original commit: 49e51ab798cf133f187155c0bbf6da00cc148fd9

---
+++ js/src/tests/js1_5/Regress/regress-452189.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+var gTestfile = 'regress-452189.js';
+
+var BUGNUMBER = 452189;
+var summary = "Don't shadow a readonly or setter proto-property";
+var expect = "PASS";
+var actual = "FAIL";
+
+function c() {
+    this.x = 3;
+}
+
+
+new c;
+Object.prototype.__defineSetter__('x', function(){ actual = expect; })
+new c;
+
+reportCompare(expect, actual, summary);
---
+++ js/src/tests/js1_5/Regress/regress-503860.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+var gTestfile = 'regress-503860.js';
+
+var BUGNUMBER = 503860;
+var summary = "Don't shadow a readonly or setter proto-property";
+var expect = "PASS";
+var actual = "FAIL";
+var a = {y: 1};
+
+function B(){}
+B.prototype.__defineSetter__('x', function setx(val) { actual = expect; });
+var b = new B;
+b.y = 1;
+
+var arr = [a, b];       
+for each (var obj in arr)
+    obj.x = 2;          
+
+reportCompare(expect, actual, summary);
