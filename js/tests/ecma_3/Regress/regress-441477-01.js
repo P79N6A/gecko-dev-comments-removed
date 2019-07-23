c35069718565e61226a1dc4c535571bfc828b041
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:27:58 +0000

    JavaScript Tests - regression test for bug 441477, by Jason Orendorff
    UltraBlame original commit: aa0cb953b8f70cd0bb67035c5ef53bb88143fc85

---
+++ js/tests/ecma_3/Regress/regress-441477-01.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-441477-01.js';
+
+var BUGNUMBER = 441477-01;
+var summary = '';
+var actual = 'No Exception';
+var expect = 'No Exception';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  try
+  {
+    for (i = 0; i < 5;) 
+    { 
+      if (i > 5) 
+        throw "bad"; 
+      i++; 
+      continue; 
+    }
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
