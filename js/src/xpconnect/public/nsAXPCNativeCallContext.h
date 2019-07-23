70c9b93158cb3bfe3d99d4c00445ddba431bd96f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:57:05 +0000

    Bug 411327 - nsIXPCNativeCallContext should not inherit from nsISupports, r=mrbkap, a=schrep
    UltraBlame original commit: 39073ec5ae4cd20115aca87d25446802908bdfc5

---
+++ js/src/xpconnect/public/nsAXPCNativeCallContext.h
@@ -0,0 +1,89 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsAXPCNativeCallContext_h__
+#define nsAXPCNativeCallContext_h__
+
+class nsIXPConnectWrappedNative;
+
+
+
+
+
+
+class nsAXPCNativeCallContext
+{
+public:
+    NS_IMETHOD GetCallee(nsISupports **aResult) = 0;
+    NS_IMETHOD GetCalleeMethodIndex(PRUint16 *aResult) = 0;
+    NS_IMETHOD GetCalleeWrapper(nsIXPConnectWrappedNative **aResult) = 0;
+    NS_IMETHOD GetJSContext(JSContext **aResult) = 0;
+    NS_IMETHOD GetArgc(PRUint32 *aResult) = 0;
+    NS_IMETHOD GetArgvPtr(jsval **aResult) = 0;
+
+    
+
+
+    NS_IMETHOD GetRetValPtr(jsval **aResult) = 0;
+
+    
+
+
+
+
+    NS_IMETHOD GetExceptionWasThrown(PRBool *aResult) = 0;
+    NS_IMETHOD SetExceptionWasThrown(PRBool aValue) = 0;
+
+    
+
+
+
+
+    NS_IMETHOD GetReturnValueWasSet(PRBool *aResult) = 0;
+    NS_IMETHOD SetReturnValueWasSet(PRBool aValue) = 0;
+
+    
+
+    NS_IMETHOD GetCalleeInterface(nsIInterfaceInfo **aResult) = 0;
+    NS_IMETHOD GetCalleeClassInfo(nsIClassInfo **aResult) = 0;
+};
+
+#endif
