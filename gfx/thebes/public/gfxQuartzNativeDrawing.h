06d77d6aaeb5dfbb973cfe7297ead7a2358e3b90
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:01:40 +0000

    Bug 395983 - "Duplicated Flash menus using r60(+) of Flash player" (do native drawing instead) [p=kinetik@flim.org (Matthew Gregan) r=vlad sr=roc a=blocking1.9+]
    UltraBlame original commit: 486dec2b5808c495096edc6bc1eb576aa95526ae

---
+++ gfx/thebes/public/gfxQuartzNativeDrawing.h
@@ -0,0 +1,85 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef _GFXQUARTZNATIVEDRAWING_H_
+#define _GFXQUARTZNATIVEDRAWING_H_
+
+#include "gfxContext.h"
+#include "gfxQuartzSurface.h"
+
+class THEBES_API gfxQuartzNativeDrawing {
+public:
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    gfxQuartzNativeDrawing(gfxContext *ctx,
+                           const gfxRect& nativeRect);
+
+    
+
+
+    CGContextRef BeginNativeDrawing();
+
+    
+    void EndNativeDrawing();
+
+private:
+    
+    gfxQuartzNativeDrawing(const gfxQuartzNativeDrawing&);
+    const gfxQuartzNativeDrawing& operator=(const gfxQuartzNativeDrawing&);
+
+    nsRefPtr<gfxContext> mContext;
+    gfxRect mNativeRect; 
+
+    
+    nsRefPtr<gfxQuartzSurface> mQuartzSurface;
+    CGContextRef mCGContext;
+};
+
+#endif
---
+++ gfx/thebes/src/gfxQuartzNativeDrawing.cpp
@@ -0,0 +1,131 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsMathUtils.h"
+
+#include "gfxQuartzNativeDrawing.h"
+#include "gfxQuartzSurface.h"
+
+
+enum {
+    kPrivateCGCompositeSourceOver = 2
+};
+
+
+extern "C" {
+    CG_EXTERN void CGContextSetCompositeOperation(CGContextRef, int);
+}
+
+gfxQuartzNativeDrawing::gfxQuartzNativeDrawing(gfxContext* ctx,
+                                               const gfxRect& nativeRect)
+    : mContext(ctx), mNativeRect(nativeRect)
+{
+}
+
+CGContextRef
+gfxQuartzNativeDrawing::BeginNativeDrawing()
+{
+    NS_ASSERTION(!mQuartzSurface, "BeginNativeDrawing called when drawing already in progress");
+
+    gfxPoint deviceOffset;
+    nsRefPtr<gfxASurface> surf = mContext->CurrentSurface(&deviceOffset.x, &deviceOffset.y);
+    if (!surf || surf->CairoStatus())
+        return nsnull;
+
+    
+    
+    
+    
+    if (surf->GetType() == gfxASurface::SurfaceTypeQuartz &&
+        (surf->GetContentType() == gfxASurface::CONTENT_COLOR ||
+         (surf->GetContentType() == gfxASurface::CONTENT_COLOR_ALPHA))) {
+        mQuartzSurface = static_cast<gfxQuartzSurface*>(static_cast<gfxASurface*>(surf.get()));
+    } else {
+        
+        
+        
+        NS_WARNING("unhandled surface type");
+        return nsnull;
+    }
+
+    
+    mContext->UpdateSurfaceClip();
+
+    
+    mCGContext = mQuartzSurface->GetCGContext();
+    if (!mCGContext)
+        return nsnull;
+
+    gfxMatrix m = mContext->CurrentMatrix();
+    CGContextSaveGState(mCGContext);
+    CGContextTranslateCTM(mCGContext, deviceOffset.x, deviceOffset.y);
+
+    
+    
+    
+
+    gfxFloat x0 = m.x0;
+    gfxFloat y0 = m.y0;
+
+    
+    
+    
+    if (!m.HasNonTranslationOrFlip()) {
+        x0 = floor(x0 + 0.5);
+        y0 = floor(y0 + 0.5);
+    }
+
+    CGContextConcatCTM(mCGContext, CGAffineTransformMake(m.xx, m.yx,
+                                                         m.xy, m.yy,
+                                                         x0, y0));
+
+    
+    CGContextSetCompositeOperation(mCGContext, kPrivateCGCompositeSourceOver);
+
+    return mCGContext;
+}
+
+void
+gfxQuartzNativeDrawing::EndNativeDrawing()
+{
+    NS_ASSERTION(mQuartzSurface, "EndNativeDrawing called without BeginNativeDrawing");
+
+    
+    CGContextRestoreGState(mCGContext);
+    mQuartzSurface->MarkDirty();
+    mQuartzSurface = nsnull;
+}
