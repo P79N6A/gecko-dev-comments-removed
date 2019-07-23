ba543c500f5f0e28e973d2f098698dabf2d40fa5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:25:54 +0000

    Bug 555246 - Wrong answer for 'this' in function code when entry frame was produced by a call across globals. r=brendan.
    UltraBlame original commit: e4364736e170e88788578cf963d563d137d2d54f

---
+++ js/src/tests/js1_8_5/regress/regress-555246-0.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+var cx = evalcx("");
+evalcx("function f() { return this; }", cx);
+var f = cx.f;
+assertEq(f(), cx);
+reportCompare(0, 0, "");
---
+++ js/src/tests/js1_8_5/regress/regress-555246-1.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+var cx = evalcx("");
+evalcx("function f() { return this; }", cx);
+f = cx.f;
+assertEq(f(), cx);
+reportCompare(0, 0, "");
