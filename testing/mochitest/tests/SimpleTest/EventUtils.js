60a16ce3e00c32e292b0b2bc116285d9ec0efe2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:11 +0000

    Fixing bug 372098. HTML links with target="" should use the base target. r=Olli.Pettay@gmail.com, sr=jonas@sicking.cc
    UltraBlame original commit: 59882c5c8b3a87d29e5888d406b9ad525f02dcf3

--- testing/mochitest/tests/SimpleTest/EventUtils.js
+++ testing/mochitest/tests/SimpleTest/EventUtils.js
@@ -15,6 +15,51 @@
 
 
 
+function sendMouseEvent(aEvent, aTarget) {
+  if (['click', 'mousedown', 'mouseup', 'mouseover', 'mouseout'].indexOf(aEvent.type) == -1) {
+    throw new Error("sendMouseEvent doesn't know about event type '"+aEvent.type+"'");
+  }
+
+  
+  netscape.security.PrivilegeManager.enablePrivilege('UniversalBrowserWrite');
+
+  var event = document.createEvent('MouseEvent');
+
+  var typeArg          = aEvent.type;
+  var canBubbleArg     = true;
+  var cancelableArg    = true;
+  var viewArg          = window;
+  var detailArg        = aEvent.detail        || (aEvent.type == 'click'     ||
+                                                  aEvent.type == 'mousedown' ||
+                                                  aEvent.type == 'mouseup' ? 1 : 0);
+  var screenXArg       = aEvent.screenX       || 0;
+  var screenYArg       = aEvent.screenY       || 0;
+  var clientXArg       = aEvent.clientX       || 0;
+  var clientYArg       = aEvent.clientY       || 0;
+  var ctrlKeyArg       = aEvent.ctrlKey       || false;
+  var altKeyArg        = aEvent.altKey        || false;
+  var shiftKeyArg      = aEvent.shiftKey      || false;
+  var metaKeyArg       = aEvent.metaKey       || false;
+  var buttonArg        = aEvent.button        || 0;
+  var relatedTargetArg = aEvent.relatedTarget || null;
+
+  event.initMouseEvent(typeArg, canBubbleArg, cancelableArg, viewArg, detailArg,
+                       screenXArg, screenYArg, clientXArg, clientYArg,
+                       ctrlKeyArg, altKeyArg, shiftKeyArg, metaKeyArg,
+                       buttonArg, relatedTargetArg);
+
+  document.getElementById(aTarget).dispatchEvent(event);
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
