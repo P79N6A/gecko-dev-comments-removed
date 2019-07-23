5c7e7c5dabb7c7ffd0f5fd4d37b500d3f1f7178c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:13:24 +0000

    bug 431642 - Put back state change event when doc starts loading (e. g. when pressing ENTER on a link) Patch by Alexander Surkov (surkov.alexander@gmail.com) r=me r=aaronlev a1.9=beltzner
    UltraBlame original commit: 2d5f2a94846110970d5e46cb56c0e58c75ce5efe

--- accessible/src/base/nsDocAccessible.cpp
+++ accessible/src/base/nsDocAccessible.cpp
@@ -854,8 +854,18 @@ NS_IMETHODIMP nsDocAccessible::FireDocLoadEvents(PRUint32 aEventType)
       }
     }
   }
+
   if (sameTypeRoot == treeItem) {
     
+    if (!isFinished) {
+      
+      
+      nsCOMPtr<nsIAccessibleStateChangeEvent> accEvent =
+        new nsAccStateChangeEvent(this, nsIAccessibleStates::STATE_BUSY,
+                                  PR_FALSE, PR_TRUE);
+      FireAccessibleEvent(accEvent);
+    }
+
     nsAccUtils::FireAccEvent(aEventType, this);
   }
   return NS_OK;
