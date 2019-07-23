2c2b512a4d81673ea7fa608e357184245f660da7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:46:33 +0000

    JavaScript Tests - regression test for bug 392308, by Norris Boyd, not part of the build
    UltraBlame original commit: 0c92e8f396327a805c513468e8c0dcf432656476

---
+++ js/tests/js1_7/extensions/regress-392308.js
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress--392308.js';
+
+var BUGNUMBER = 392308;
+var summary = 'StopIteration should be catchable';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  function testStop() {
+    function yielder() {
+      actual += 'before, ';
+      yield;
+      actual += 'after, ';
+    }
+
+    expect = 'before, after, iteration terminated normally';
+
+    try {
+      var gen = yielder();
+      result = gen.next();
+      gen.send(result);
+    } catch (x if x instanceof StopIteration) {
+      actual += 'iteration terminated normally';
+    } catch (x2) {
+      actual += 'unexpected throw: ' + x2;
+    }
+  }
+  testStop();
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
