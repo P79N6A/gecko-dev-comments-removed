bd91c4d78d8b01cfeb03bee831e8fba941232b0e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:09:55 +0000

    Fix build breakage --with-static-checking. No bug#.
    UltraBlame original commit: 2ddabafdf4a0790466c76094b9f8ae2f3c9e46f0

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -1781,7 +1781,7 @@ TraceRecorder::snapshot(ExitType exitType)
         JS_ASSERT(*pc == JSOP_CALL || *pc == JSOP_APPLY || *pc == JSOP_NEXTITER);
         pc += cs.length;
         regs->pc = pc;
-        MUST_FLOW_THROUGH(restore_pc);
+        MUST_FLOW_THROUGH("restore_pc");
     }
 
     
