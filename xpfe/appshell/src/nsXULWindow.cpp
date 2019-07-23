181f0900db265597db8d51f4594dd183f7d38697
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:26:46 +0000

    Bug 465448. Perform sanity check to avoid resizing a window smaller than the content area we're trying to achieve. r+sr=bzbarsky
    UltraBlame original commit: 4e1a2c3bc40d997bcf6203ae0d51bc38b9f374ae

--- xpfe/appshell/src/nsXULWindow.cpp
+++ xpfe/appshell/src/nsXULWindow.cpp
@@ -1716,7 +1716,13 @@ NS_IMETHODIMP nsXULWindow::SizeShellTo(nsIDocShellTreeItem* aShellItem,
     PRInt32 winCY = 0;
 
     GetSize(&winCX, &winCY);
-    SetSize(winCX + widthDelta, winCY + heightDelta, PR_TRUE);
+    
+    
+    
+    
+    winCX = PR_MAX(winCX + widthDelta, aCX);
+    winCY = PR_MAX(winCY + heightDelta, aCY);
+    SetSize(winCX, winCY, PR_TRUE);
   }
 
   return NS_OK;
