0788a389dd6c63cf7a318252fef8eda2508fd5d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:49:50 +0000

    bug 524346 - using jsval, not jsdouble *, for nan and +-oo. r=brendan
    UltraBlame original commit: f2aa13fe207767f3dad8c2497dd2004e26965310

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -536,9 +536,9 @@ struct JSRuntime {
     JSSetSlotRequest    *setSlotRequests;
 
     
-    jsdouble            *jsNaN;
-    jsdouble            *jsNegativeInfinity;
-    jsdouble            *jsPositiveInfinity;
+    jsval               NaNValue;
+    jsval               negativeInfinityValue;
+    jsval               positiveInfinityValue;
 
 #ifdef JS_THREADSAFE
     JSLock              *deflatedStringCacheLock;
