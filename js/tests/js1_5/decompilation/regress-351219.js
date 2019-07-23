ffca33cd2ac6672ca89d413665000d33f8c8f541
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:28 +0000

    JavaScript Test -regression test for bug 351219, by Jesse Ruderman
    UltraBlame original commit: 7142abe7fdaede0bf767c47cb4d64312bd17d428

---
+++ js/tests/js1_5/decompilation/regress-351219.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 351219;
+var summary = 'Decompilation of immutable infinity, NaN';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  var f = function () { return 2e308 };
+  expect = 'function () { return 1 / 0; }';
+  actual = f + '';
+  compareSource(expect, actual, summary + ' decompile Infinity as 1/0');
+
+  f = function () { var NaN = 1 % 0; return NaN };
+  expect = 'function () { var NaN = 0 / 0; return NaN; }';
+  actual = f + '';
+  compareSource(expect, actual, summary + ': decompile NaN as 0/0');
+
+  exitFunc ('test');
+}
