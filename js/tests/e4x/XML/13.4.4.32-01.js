3ee475e8d7539ee26c5e8a75a94cea6c73e91bc8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:52 +0000

    JavaScript Test - regression test for bug 291927, by Michael Lipp, David P. Caldwell
    UltraBlame original commit: f69d350dff3671a0223bd25a69e1831eda2f594b

---
+++ js/tests/e4x/XML/13.4.4.32-01.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+START("13.4.4.32-1 - XML replace() by index, text to string");
+
+BUG(291927);
+
+var root = <root>text</root>;
+
+TEST_XML(1, "text", root.child(0));
+
+root.replace(0, "new text");
+
+TEST_XML(2, "new text", root.child(0));
+TEST(3, <root>new text</root>, root);
+
+END();
