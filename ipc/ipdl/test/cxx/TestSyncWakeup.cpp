993af431d8cbdcd3adab2f0998b89e2c1d079a03
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:07:39 +0000

    Test for bug 538239.
    UltraBlame original commit: 908c2e08e36436a151dcc381159f13edc8f302f2

---
+++ ipc/ipdl/test/cxx/TestSyncWakeup.cpp
@@ -0,0 +1,121 @@
+#include "TestSyncWakeup.h"
+
+#include "IPDLUnitTests.h"      
+
+namespace mozilla {
+namespace _ipdltest {
+
+
+
+
+TestSyncWakeupParent::TestSyncWakeupParent()
+{
+    MOZ_COUNT_CTOR(TestSyncWakeupParent);
+}
+
+TestSyncWakeupParent::~TestSyncWakeupParent()
+{
+    MOZ_COUNT_DTOR(TestSyncWakeupParent);
+}
+
+void
+TestSyncWakeupParent::Main()
+{
+    if (!SendStart())
+        fail("sending Start()");
+}
+
+bool
+TestSyncWakeupParent::AnswerStackFrame()
+{
+    if (!CallStackFrame())
+        fail("calling StackFrame()");
+    return true;
+}
+
+bool
+TestSyncWakeupParent::RecvSync1()
+{
+    if (!SendNote1())
+        fail("sending Note1()");
+
+    
+    
+    
+    sleep(5);
+
+    return true;
+}
+
+bool
+TestSyncWakeupParent::RecvSync2()
+{
+    if (!SendNote2())
+        fail("sending Note2()");
+
+    
+    sleep(5);
+
+    return true;
+}
+
+
+
+
+TestSyncWakeupChild::TestSyncWakeupChild() : mDone(false)
+{
+    MOZ_COUNT_CTOR(TestSyncWakeupChild);
+}
+
+TestSyncWakeupChild::~TestSyncWakeupChild()
+{
+    MOZ_COUNT_DTOR(TestSyncWakeupChild);
+}
+
+bool
+TestSyncWakeupChild::RecvStart()
+{
+    
+    
+    if (!SendSync1())
+        fail("sending Sync()");
+
+    
+    
+    return true;
+}
+
+bool
+TestSyncWakeupChild::RecvNote1()
+{
+    
+    
+    if (!CallStackFrame())
+        fail("calling StackFrame()");
+
+    if (!mDone)
+        fail("should have received Note2()!");
+
+    Close();
+
+    return true;
+}
+
+bool
+TestSyncWakeupChild::AnswerStackFrame()
+{
+    if (!SendSync2())
+        fail("sending Sync()");
+
+    return true;
+}
+
+bool
+TestSyncWakeupChild::RecvNote2()
+{
+    mDone = true;
+    return true;
+}
+
+} 
+} 
