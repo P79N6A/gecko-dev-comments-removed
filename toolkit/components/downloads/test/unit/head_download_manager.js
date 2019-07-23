2907e21d1e40cdde95398d12c0f7fbead58de35a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:02 +0000

    Bug 381646 - Re-enable tests for download manager. r=sayrer
    UltraBlame original commit: 50b257daaa33b4921ec12abc0757a20ad5a059a3

---
+++ toolkit/components/downloads/test/unit/head_download_manager.js
@@ -0,0 +1,81 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+do_import_script("netwerk/test/httpserver/httpd.js");
+
+function createURI(aObj)
+{
+  var ios = Cc["@mozilla.org/network/io-service;1"].
+            getService(Ci.nsIIOService);
+  return (aObj instanceof Ci.nsIFile) ? ios.newFileURI(aObj) :
+                                        ios.newURI(aObj, null, null);
+}
+
+var dirSvc = Cc["@mozilla.org/file/directory_service;1"].
+             getService(Ci.nsIProperties);
+var profileDir = null;
+try {
+  profileDir = dirSvc.get("ProfD", Ci.nsIFile);
+} catch (e) { }
+if (!profileDir) {
+  
+  
+  var provider = {
+    getFile: function(prop, persistent) {
+      persistent.value = true;
+      if (prop == "ProfD") {
+        return dirSvc.get("CurProcD", Ci.nsILocalFile);
+      } else if (prop == "DLoads") {
+        var file = dirSvc.get("CurProcD", Ci.nsILocalFile);
+        file.append("downloads.rdf");
+        return file;
+      }
+      throw Cr.NS_ERROR_FAILURE;
+    },
+    QueryInterface: function(iid) {
+      if (iid.equals(Ci.nsIDirectoryProvider) ||
+          iid.equals(Ci.nsISupports)) {
+        return this;
+      }
+      throw Cr.NS_ERROR_NO_INTERFACE;
+    }
+  };
+  dirSvc.QueryInterface(Ci.nsIDirectoryService).registerProvider(provider);
+}
+
