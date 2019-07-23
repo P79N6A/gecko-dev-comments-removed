3c609e43bfe5a53dd986374014ee548d8caf73ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:54:55 +0000

    JavaScript Tests - regression test for bug 353116, by Jesse Ruderman, Rich Dougherty, not part of the build
    UltraBlame original commit: e3147ca033a4eeb8bde70ab46609853847db20e1

---
+++ js/tests/js1_8/extensions/regress-353116.js
@@ -0,0 +1,112 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-353116.js';
+
+var BUGNUMBER = 353116;
+var summary = 'Improve errors messages for null, undefined properties';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  expect = 'TypeError: undefined has no properties';
+  actual = 'No Error';
+
+  try
+  {
+    undefined.y;
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+  reportCompare(expect, actual, summary);
+
+  expect = 'TypeError: null has no properties';
+  actual = 'No Error';
+
+  try
+  {
+    null.y;
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+  reportCompare(expect, actual, summary);
+
+  expect = 'TypeError: x is undefined';
+  actual = 'No Error';
+
+  try
+  {
+    x = undefined; 
+    x.y;
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+  reportCompare(expect, actual, summary);
+
+  expect = 'TypeError: x is null';
+  actual = 'No Error';
+
+  try
+  {
+    x = null; 
+    x.y;
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
