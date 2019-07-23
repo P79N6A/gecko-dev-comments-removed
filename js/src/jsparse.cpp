76f37ff555ecb12311f99a0315279ef5b39ac1e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:32:23 +0000

    Fix thinko in patch for 560234, caught by jorendorff, all praise to him.
    UltraBlame original commit: 90112fdfaa8f2095c93bfd538300076b188ba733

--- js/src/jsparse.cpp
+++ js/src/jsparse.cpp
@@ -2249,8 +2249,7 @@ Parser::setFunctionKinds(JSFunctionBox *funbox, uint32& tcflags)
                     FUN_METER(onlyfreevar);
                     FUN_SET_KIND(fun, JSFUN_NULL_CLOSURE);
                 } else if (!mutation &&
-                           !(~funbox->tcflags
-                             & (TCF_FUN_IS_GENERATOR | TCF_FUN_ENTRAINS_SCOPES))) {
+                           !(funbox->tcflags & (TCF_FUN_IS_GENERATOR | TCF_FUN_ENTRAINS_SCOPES))) {
                     
 
 
