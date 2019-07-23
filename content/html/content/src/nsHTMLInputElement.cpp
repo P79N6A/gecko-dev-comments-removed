35c9620f74e64bbb0304cb8851c936f0c2eec6a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:55:54 +0000

    Changing the alt or value attribute of an image input should change the text shown when the image is broken.   Bug 408782, r+sr=dbaron, a=schrep
    UltraBlame original commit: 69e1edc92593da5d80e6b3480d8f03d1ad05e66b

--- content/html/content/src/nsHTMLInputElement.cpp
+++ content/html/content/src/nsHTMLInputElement.cpp
@@ -2096,6 +2096,12 @@ nsHTMLInputElement::GetAttributeChangeHint(const nsIAtom* aAttribute,
     nsGenericHTMLFormElement::GetAttributeChangeHint(aAttribute, aModType);
   if (aAttribute == nsGkAtoms::type) {
     NS_UpdateHint(retval, NS_STYLE_HINT_FRAMECHANGE);
+  } else if (mType == NS_FORM_INPUT_IMAGE &&
+             (aAttribute == nsGkAtoms::alt ||
+              aAttribute == nsGkAtoms::value)) {
+    
+    
+    NS_UpdateHint(retval, NS_STYLE_HINT_FRAMECHANGE);
   } else if (aAttribute == nsGkAtoms::value) {
     NS_UpdateHint(retval, NS_STYLE_HINT_REFLOW);
   } else if (aAttribute == nsGkAtoms::size &&
