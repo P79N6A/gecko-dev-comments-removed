28d4e55174f347ccaa8bd2095eaefda0062a18a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:32:42 +0000

    Bug 448741 - "nsISessionStore uses private methods and is not extensible" [r=dietrich]
    UltraBlame original commit: 181796ac1c8a11a85d69ce0f396a894e08d0a29e

--- browser/components/sessionstore/src/nsSessionStartup.js
+++ browser/components/sessionstore/src/nsSessionStartup.js
@@ -108,7 +108,7 @@ SessionStartup.prototype = {
     var resumeFromCrash = this._prefBranch.getBoolPref("sessionstore.resume_from_crash");
     if ((resumeFromCrash || this._doResumeSession()) && this._sessionFile.exists()) {
       
-      this._iniString = this._readFile(this._sessionFile);
+      this._iniString = this._readStateFile(this._sessionFile);
       if (this._iniString) {
         try {
           
@@ -321,6 +321,25 @@ SessionStartup.prototype = {
   },
 
 
+
+  
+
+
+
+
+
+
+  _readStateFile: function sss_readStateFile(aFile) {
+    var stateString = Cc["@mozilla.org/supports-string;1"].
+                        createInstance(Ci.nsISupportsString);
+    stateString.data = this._readFile(aFile) || "";
+    
+    var observerService = Cc["@mozilla.org/observer-service;1"].
+                          getService(Ci.nsIObserverService);
+    observerService.notifyObservers(stateString, "sessionstore-state-read", "");
+    
+    return stateString.data;
+  },
 
   
 
---
+++ browser/components/sessionstore/test/browser/browser_448741.js
@@ -0,0 +1,96 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  
+  
+  let ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
+  let os = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
+  let tabbrowser = getBrowser();
+  waitForExplicitFinish();
+  
+  let uniqueName = "bug 448741";
+  let uniqueValue = "as good as unique: " + Date.now();
+  
+  
+  var tab = tabbrowser.addTab();
+  ss.setTabValue(tab, uniqueName, uniqueValue);
+  let valueWasCleaned = false;
+  
+  
+  let cleaningObserver = {
+    observe: function(aSubject, aTopic, aData) {
+      ok(aTopic == "sessionstore-state-write", "observed correct topic?");
+      ok(aSubject instanceof Ci.nsISupportsString, "subject is a string?");
+      ok(aSubject.data.indexOf(uniqueValue) > -1, "data contains our value?");
+      
+      
+      let state = eval(aSubject.data);
+      for each (let winData in state.windows) {
+        for each (let tabData in winData.tabs) {
+          if (tabData.extData && uniqueName in tabData.extData &&
+              tabData.extData[uniqueName] == uniqueValue) {
+            delete tabData.extData[uniqueName];
+            valueWasCleaned = true;
+          }
+        }
+      }
+      
+      ok(valueWasCleaned, "found and removed the specific tab value");
+      aSubject.data = uneval(state);
+      os.removeObserver(this, aTopic, false);
+    }
+  };
+  
+  
+  let checkingObserver = {
+    observe: function(aSubject, aTopic, aData) {
+      ok(valueWasCleaned && aSubject instanceof Ci.nsISupportsString,
+         "ready to check the cleaned state?");
+      ok(aSubject.data.indexOf(uniqueValue) == -1, "data no longer contains our value?");
+      
+      
+      tabbrowser.removeTab(tab);
+      os.removeObserver(this, aTopic, false);
+      finish();
+    }
+  };
+  
+  
+  os.addObserver(checkingObserver, "sessionstore-state-write", false);
+  os.addObserver(cleaningObserver, "sessionstore-state-write", false);
+}
