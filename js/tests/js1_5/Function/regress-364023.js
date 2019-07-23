352d13f2f3d8a467db0d3a46cb940ee0f9883fc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:45:49 +0000

    JavaScript Tests - regression test for bug 364023, by shutdown, not part of the build
    UltraBlame original commit: 15cf9a3efeb724128588deab9730ae5c323dcd45

---
+++ js/tests/js1_5/Function/regress-364023.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-364023.js';
+
+var BUGNUMBER = 364023;
+var summary = 'Do not crash in JS_GetPrivate';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  function exploit() {
+    var code = "";
+    for(var i = 0; i < 0x10000; i++) {
+      if(i == 125) {
+        code += "void 0x10000050505050;\n";
+      } else {
+        code += "void " + (0x10000000000000 + i) + ";\n";
+      }
+    }
+    code += "function foo() {}\n";
+    eval(code);
+  }
+  exploit();
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
