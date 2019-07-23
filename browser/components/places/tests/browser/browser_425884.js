a6a66ca7f342b956e5887b0a25fbe130f5e87a81
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:11:07 +0000

    Bug 425884 - Unable to copy bookmarks more than one folder deep (r=mano)
    UltraBlame original commit: 54ee7fd12f281744187503d86b51366b2bc7da78

---
+++ browser/components/places/tests/browser/browser_425884.js
@@ -0,0 +1,122 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  ok(PlacesUtils, "checking PlacesUtils, running in chrome context?");
+  ok(PlacesUIUtils, "checking PlacesUIUtils, running in chrome context?");
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var toolbarId = PlacesUtils.toolbarFolderId;
+  var toolbarNode = PlacesUtils.getFolderContents(toolbarId).root;
+  is(toolbarNode.childCount, 0, "confirm toolbar is empty");
+
+  
+  var folderAId = PlacesUtils.bookmarks.createFolder(toolbarId, "A", -1);
+  populate(folderAId);
+  var folderANode = PlacesUtils.getFolderContents(folderAId).root;
+  validate(folderANode);
+  is(toolbarNode.childCount, 1, "create test folder");
+
+  
+  var serializedNode = PlacesUtils.wrapNode(folderANode, PlacesUtils.TYPE_X_MOZ_PLACE_CONTAINER);
+  var rawNode = PlacesUtils.unwrapNodes(serializedNode, PlacesUtils.TYPE_X_MOZ_PLACE_CONTAINER).shift();
+  
+  ok(rawNode.type, "confirm json node");
+  var transaction = PlacesUIUtils.makeTransaction(rawNode,
+                                                  PlacesUtils.TYPE_X_MOZ_PLACE_CONTAINER,
+                                                  toolbarId,
+                                                  -1,
+                                                  true);
+  ok(transaction, "create transaction");
+  PlacesUIUtils.ptm.doTransaction(transaction);
+  
+  is(toolbarNode.childCount, 2, "create test folder via copy");
+
+  
+  var folderBNode = toolbarNode.getChild(1);
+  validate(folderBNode);
+
+  
+  PlacesUIUtils.ptm.undoTransaction();
+  is(toolbarNode.childCount, 1, "confirm undo removed the copied folder");
+
+  
+  PlacesUIUtils.ptm.redoTransaction();
+  is(toolbarNode.childCount, 2, "confirm redo re-copied the folder");
+  folderBNode = toolbarNode.getChild(1);
+  validate(folderBNode);
+
+  
+  PlacesUIUtils.ptm.undoTransaction();
+  PlacesUtils.bookmarks.removeItem(folderAId);
+}
+
+function populate(aFolderId) {
+  var folderId = PlacesUtils.bookmarks.createFolder(aFolderId, "test folder", -1);
+  PlacesUtils.bookmarks.insertBookmark(folderId, PlacesUtils._uri("http://foo"), -1, "test bookmark");
+  PlacesUtils.bookmarks.insertSeparator(folderId, -1);
+}
+
+function validate(aNode) {
+  asContainer(aNode);
+  aNode.containerOpen = true;
+  is(aNode.childCount, 1, "confirm child count match");
+  var folderNode = aNode.getChild(0);
+  is(folderNode.title, "test folder", "confirm folder title");
+  asContainer(folderNode);
+  folderNode.containerOpen = true;
+  is(folderNode.childCount, 2, "confirm child count match");
+  var bookmarkNode = folderNode.getChild(0);
+  var separatorNode = folderNode.getChild(1);
+  folderNode.containerOpen = false;
+  aNode.containerOpen = false;
+}
