a8d850ce125cff76e9093479bea8e97ed13ef0d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:58:13 +0000

    Bug 456342 - also restore non-standard input field types (such as type="search")
    UltraBlame original commit: ac1e09aa72ab8ecae21b30567ef4c9a3daef64f1

---
+++ browser/components/sessionstore/test/browser/browser_456342.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  
+  waitForExplicitFinish();
+  
+  
+  let privacy_level = gPrefService.getIntPref("browser.sessionstore.privacy_level");
+  gPrefService.setIntPref("browser.sessionstore.privacy_level", 0);
+  
+  let testURL = "chrome://mochikit/content/browser/" +
+    "browser/components/sessionstore/test/browser/browser_456342_sample.xhtml";
+  let tab = gBrowser.addTab(testURL);
+  tab.linkedBrowser.addEventListener("load", function(aEvent) {
+    gBrowser.removeTab(tab);
+    
+    let ss = Cc["@mozilla.org/browser/sessionstore;1"]
+               .getService(Ci.nsISessionStore);
+    let undoItems = eval("(" + ss.getClosedTabData(window) + ")");
+    let savedFormData = undoItems[0].state.entries[0].formdata;
+    
+    let countGood = 0, countBad = 0;
+    for each (let value in savedFormData) {
+      if (value == "save me")
+        countGood++;
+      else
+        countBad++;
+    }
+    
+    is(countGood, 4, "Saved text for non-standard input fields");
+    is(countBad,  0, "Didn't save text for ignored field types");
+    
+    
+    gPrefService.setIntPref("browser.sessionstore.privacy_level", privacy_level);
+    finish();
+  }, true);
+}
