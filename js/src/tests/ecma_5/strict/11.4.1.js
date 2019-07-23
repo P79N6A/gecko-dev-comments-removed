281c6ccae469f7cd056a8abf2ccd99e096e20506
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:56:02 +0000

    Bug 514572: Forbid deletion of variables, arguments and functions in ES5 strict mode. r=jorendorff
    UltraBlame original commit: b23078c3e55afba4d277f197a0d8a24bff6bcef0

---
+++ js/src/tests/ecma_5/strict/11.4.1.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+assertEq(testLenientAndStrict('delete x;',
+                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError)),
+         true);
+
+
+
+
+
+assertEq(testLenientAndStrict('delete (x);',
+                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError)),
+         true);
+
+
+assertEq(testLenientAndStrict('delete x.y;',
+                              parsesSuccessfully,
+                              parsesSuccessfully),
+         true);
+
+
+assertEq(testLenientAndStrict('function f() { delete x; }',
+                              parsesSuccessfully,
+                              parseRaisesException(SyntaxError)),
+         true);
+
+
+assertEq(testLenientAndStrict('function f() { "use strict"; delete x; }',
+                              parseRaisesException(SyntaxError),
+                              parseRaisesException(SyntaxError)),
+         true);
