a26240036b2289612de765660ec5847a62b21bbe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:23:31 +0000

    Bug 439340: set cachemax on OS/2, too, so that the desktop stays responsive, r=gavin
    UltraBlame original commit: 87e90c513b243f2f0b617ddabc4d6329301a18d7

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -239,7 +239,9 @@ pref("browser.download.manager.showAlertOnComplete", true);
 pref("browser.download.manager.showAlertInterval", 2000);
 pref("browser.download.manager.retention", 2);
 pref("browser.download.manager.showWhenStarting", true);
+pref("browser.download.manager.useWindow", true);
 pref("browser.download.manager.closeWhenDone", false);
+pref("browser.download.manager.openDelay", 0);
 pref("browser.download.manager.focusWhenStarting", false);
 pref("browser.download.manager.flashCount", 2);
 pref("browser.download.manager.addToRecentDocs", true);
@@ -633,8 +635,15 @@ pref("urlclassifier.gethashtables", "goog-phish-shavar,goog-malware-shavar");
 
 pref("urlclassifier.confirm-age", 2700);
 
-
 #ifdef MOZ_WIDGET_GTK2
+#define RESTRICT_CACHEMAX
+#endif
+#ifdef XP_OS2
+#define RESTRICT_CACHEMAX
+#endif
+
+
+#ifdef RESTRICT_CACHEMAX
 pref("urlclassifier.updatecachemax", 104857600);
 #else
 pref("urlclassifier.updatecachemax", -1);
