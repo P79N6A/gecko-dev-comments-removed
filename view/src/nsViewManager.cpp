d68f92d566f4788ec68e705bae765584f1bc3052
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:40:29 +0000

    Remove unused variable/parameter in view code.  b=371392  r+sr=roc
    UltraBlame original commit: f778d3406405beace82ab4c4d84b55a22d63af2b

--- view/src/nsViewManager.cpp
+++ view/src/nsViewManager.cpp
@@ -459,7 +459,6 @@ void nsViewManager::Refresh(nsView *aView, nsIRenderingContext *aContext,
   SetPainting(PR_TRUE);
 
   nsCOMPtr<nsIRenderingContext> localcx;
-  nsIDrawingSurface*    ds = nsnull;
 
   if (nsnull == aContext)
     {
@@ -491,7 +490,7 @@ void nsViewManager::Refresh(nsView *aView, nsIRenderingContext *aContext,
   AddCoveringWidgetsToOpaqueRegion(opaqueRegion, mContext, aView);
   damageRegion.Sub(damageRegion, opaqueRegion);
 
-  RenderViews(aView, *localcx, damageRegion, ds);
+  RenderViews(aView, *localcx, damageRegion);
 
   ctx->Restore();
 
@@ -591,13 +590,8 @@ void nsViewManager::AddCoveringWidgetsToOpaqueRegion(nsRegion &aRgn, nsIDeviceCo
   }
 }
 
-
-
-
-
-
 void nsViewManager::RenderViews(nsView *aView, nsIRenderingContext& aRC,
-                                const nsRegion& aRegion, nsIDrawingSurface* aRCSurface)
+                                const nsRegion& aRegion)
 {
   if (mObserver) {
     nsView* displayRoot = GetDisplayRootFor(aView);
