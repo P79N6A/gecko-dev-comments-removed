04f74cdfee1067271fef5a55db72cfb455fa383e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:08:54 +0000

    Backed out changeset 70854fa518da -- toolkit/ files were changed by accident
    UltraBlame original commit: 5df38aea4ff072418faf0c80756cffb97a8b2ae4

---
+++ toolkit/crashreporter/google-breakpad/src/common/dwarf/types.h
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef _COMMON_DWARF_TYPES_H__
+#define _COMMON_DWARF_TYPES_H__
+
+typedef signed char         int8;
+typedef short               int16;
+typedef int                 int32;
+typedef long long           int64;
+
+typedef unsigned char      uint8;
+typedef unsigned short     uint16;
+typedef unsigned int       uint32;
+typedef unsigned long long uint64;
+
+#ifdef __PTRDIFF_TYPE__
+typedef          __PTRDIFF_TYPE__ intptr;
+typedef unsigned __PTRDIFF_TYPE__ uintptr;
+#else
+#error "Can't find pointer-sized integral types."
+#endif
+
+#endif 
