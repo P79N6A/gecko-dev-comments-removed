a00ca63d864190105c2f24cc1258488b0008efcb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:22:11 +0000

    Bug 554692 - Integrate Maemo Geolocation provider. r=roxama
    UltraBlame original commit: 11d951c99c0fc424a84c750f2ce7a2b9b40bbacf

---
+++ dom/src/geolocation/MaemoLocationProvider.h
@@ -0,0 +1,121 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsCOMPtr.h"
+#include "nsAutoPtr.h"
+#include "nsCOMArray.h"
+#include "nsTArray.h"
+#include "nsITimer.h"
+#include "nsIObserver.h"
+#include "nsIURI.h"
+
+#include "nsWeakPtr.h"
+#include "nsCycleCollectionParticipant.h"
+
+#include "nsIDOMGeoGeolocation.h"
+#include "nsIDOMGeoPosition.h"
+#include "nsIDOMGeoPositionError.h"
+#include "nsIDOMGeoPositionCallback.h"
+#include "nsIDOMGeoPositionErrorCallback.h"
+#include "nsIDOMGeoPositionOptions.h"
+#include "nsIDOMNavigatorGeolocation.h"
+#include "nsIDOMGeoPositionCoords.h"
+
+#include "nsPIDOMWindow.h"
+
+#include "nsIGeolocationProvider.h"
+
+
+extern "C"
+{
+#include <location/location-gps-device.h>
+#include <location/location-gpsd-control.h>
+#include <location/location-distance-utils.h>
+#include <location/location-misc.h>
+}
+
+class MaemoLocationProvider : public nsIGeolocationProvider,
+                              public nsITimerCallback
+
+{
+ public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIGEOLOCATIONPROVIDER
+  NS_DECL_NSITIMERCALLBACK
+
+  MaemoLocationProvider();
+
+  void Update(nsIDOMGeoPosition* aPosition);
+
+ private:
+  ~MaemoLocationProvider();
+
+  nsresult StartControl();
+  nsresult StartDevice();
+  nsresult LocationUpdate(LocationGPSDevice* aDev);
+
+  static void DeviceDisconnected(LocationGPSDevice* device, void* self);
+  static void ControlStopped(LocationGPSDControl* device, void* self);
+  static void ControlError(LocationGPSDControl* control, void* self);
+  static void LocationChanged(LocationGPSDevice* device, void* self);
+
+  gulong mLocationChanged;
+  gulong mControlError;
+  gulong mDeviceDisconnected;
+  gulong mControlStopped;
+
+  nsCOMPtr<nsIGeolocationUpdate> mCallback;
+  PRPackedBool mHasSeenLocation;
+  PRPackedBool mHasGPS;
+
+  nsCOMPtr<nsITimer> mUpdateTimer;
+  LocationGPSDControl* mGPSControl;
+  LocationGPSDevice* mGPSDevice;
+
+  PRBool mIgnoreMinorChanges;
+
+  double mPrevLat;
+  double mPrevLong;
+
+  LocationGPSDControlInterval mInterval;
+  PRBool mIgnoreBigHErr;
+  PRInt32 mMaxHErr;
+  PRBool mIgnoreBigVErr;
+  PRInt32 mMaxVErr;
+
+};
