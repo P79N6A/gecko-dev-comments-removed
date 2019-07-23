c3602ea08ac68d707f822ea34d3a8dad117412aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:40:29 +0000

    don't put a dropmarker in comboboxes if nsITheme says not to. b=370282 r/sr=roc
    UltraBlame original commit: 61b38b0adcd3484efd11498c2b1541cdfe57c3c8

--- widget/src/windows/nsNativeThemeWin.cpp
+++ widget/src/windows/nsNativeThemeWin.cpp
@@ -1289,6 +1289,12 @@ nsNativeThemeWin::ThemeDrawsFocusForWidget(nsPresContext* aPresContext, nsIFrame
   return PR_FALSE;
 }
 
+PRBool
+nsNativeThemeWin::ThemeNeedsComboboxDropmarker()
+{
+  return PR_TRUE;
+}
+
 
 
 PRBool 
