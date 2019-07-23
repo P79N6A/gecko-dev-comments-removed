ac30da54b1fbafc6aa3ecf5cd37825dec35493dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:42:23 +0000

    Bug 517078. New test for NPN_NewStream and NPN_Write.
    UltraBlame original commit: d1328e8b7e4d7adc5a0d9fb43a1bb8fb0b0d3d6c

--- modules/plugin/test/mochitest/pluginstream.js
+++ modules/plugin/test/mochitest/pluginstream.js
@@ -3,20 +3,29 @@
   function frameLoaded() {
     var testframe = document.getElementById('testframe');
     var embed = document.getElementsByTagName('embed')[0];
-    var content = testframe.contentDocument.body.innerHTML;
-    if (!content.length)
-      return;
+    try {
+      var content = testframe.contentDocument.body.innerHTML;
+      if (!content.length)
+        return;
 
-    var filename = embed.getAttribute("src") ||
-        embed.getAttribute("geturl") ||
-        embed.getAttribute("geturlnotify");
-    
-    var req = new XMLHttpRequest();
-    req.open('GET', filename, false);
-    req.overrideMimeType('text/plain; charset=x-user-defined');
-    req.send(null);
-    is(req.status, 200, "bad XMLHttpRequest status");
-    is(content, req.responseText.replace(/\r\n/g, "\n"),
-       "content doesn't match");
+      var filename = embed.getAttribute("src") ||
+          embed.getAttribute("geturl") ||
+          embed.getAttribute("geturlnotify");
+      
+      var req = new XMLHttpRequest();
+      req.open('GET', filename, false);
+      req.overrideMimeType('text/plain; charset=x-user-defined');
+      req.send(null);
+      is(req.status, 200, "bad XMLHttpRequest status");
+      is(content, req.responseText.replace(/\r\n/g, "\n"),
+         "content doesn't match");
+    }
+    catch (e) {
+      
+      
+      
+      ok(e.message.indexOf("file:") > -1, "exception " + e.message + " thrown");
+    }
+    is(embed.getError(), "pass", "plugin reported error");
     SimpleTest.finish();
   }
