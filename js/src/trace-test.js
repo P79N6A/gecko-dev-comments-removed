7f4e6f5c1ba4dfb7efe10aff42c8ff282a8c0814
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:22:22 +0000

    Bug 461180: Fix result comparison function for trace tests. r=mrbkap
    This allows the math tests (next patch) to be pickier about NaN and
    negative zero.  With the earlier patch, it introduces no regressions.
    
    UltraBlame original commit: 71e52b2b4289774c085faabfdd7904541c1312d5

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -63,9 +63,41 @@ function map_test(t, cases)
   }
 }
 
+
+
+
+
+
+
+
+function close_enough(expected, actual)
+{
+  if (typeof expected != typeof actual)
+    return false;
+  if (typeof expected != 'number')
+    return actual == expected;
+
+  
+  
+  if (actual != actual)
+    return expected != expected
+  if (expected != expected)
+    return false;
+
+  
+  if (actual != expected)
+    return Math.abs(actual - expected) <= 1E-10;
+
+  
+  if (actual == 0)
+    return (1 / actual > 0) == (1 / expected > 0);
+
+  return true;
+}
+
 function check(desc, actual, expected, oldJITstats, expectedJITstats)
 {
-  if (expected == actual) {
+  if (close_enough(expected, actual)) {
     var pass = true;
     jitstatHandler(function(prop) {
                      if (expectedJITstats && prop in expectedJITstats &&
