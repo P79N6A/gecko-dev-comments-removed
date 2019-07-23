6143f89ff82e79edc6aabb900f5afe73974fd5d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:54:19 +0000

    Bug 477657 - saner default handling for _closedTabs and sizemode. r=dietrich
    UltraBlame original commit: 841e2745a834f067d0f3ab5ea401c334a078b108

---
+++ browser/components/sessionstore/test/browser/browser_477657.js
@@ -0,0 +1,95 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  
+  let ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
+  waitForExplicitFinish();
+  
+  let newWin = openDialog(location, "_blank", "chrome,all,dialog=no");
+  newWin.addEventListener("load", function(aEvent) {
+    let newState = { windows: [{
+      tabs: [{ entries: [] }],
+      _closedTabs: [{
+        state: { entries: [{ url: "about:" }]},
+        title: "About:"
+      }],
+      sizemode: "maximized"
+    }] };
+    
+    let uniqueKey = "bug 477657";
+    let uniqueValue = "unik" + Date.now();
+  
+    ss.setWindowValue(newWin, uniqueKey, uniqueValue);
+    is(ss.getWindowValue(newWin, uniqueKey), uniqueValue,
+       "window value was set before the window was overwritten");
+    ss.setWindowState(newWin, JSON.stringify(newState), true);
+    
+    
+    setTimeout(function() {
+      is(ss.getWindowValue(newWin, uniqueKey), "",
+         "window value was implicitly cleared");
+      
+      is(newWin.windowState, newWin.STATE_MAXIMIZED,
+         "the window was maximized");
+      
+      is(JSON.parse(ss.getClosedTabData(newWin)).length, 1,
+         "the closed tab was added before the window was overwritten");
+      delete newState.windows[0]._closedTabs;
+      delete newState.windows[0].sizemode;
+      ss.setWindowState(newWin, JSON.stringify(newState), true);
+      
+      setTimeout(function() {
+        is(JSON.parse(ss.getClosedTabData(newWin)).length, 0,
+           "closed tabs were implicitly cleared");
+        
+        is(newWin.windowState, newWin.STATE_MAXIMIZED,
+           "the window remains maximized");
+        newState.windows[0].sizemode = "normal";
+        ss.setWindowState(newWin, JSON.stringify(newState), true);
+        
+        setTimeout(function() {
+          isnot(newWin.windowState, newWin.STATE_MAXIMIZED,
+                "the window was explicitly unmaximized");
+          
+          newWin.close();
+          finish();
+        }, 0);
+      }, 0);
+    }, 0);
+  }, false);
+}
