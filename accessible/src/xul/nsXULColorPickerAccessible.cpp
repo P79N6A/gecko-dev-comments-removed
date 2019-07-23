17dd02d92c46a088a5ddbd7765ff54637d029d88
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:17:05 +0000

    Bug 416872 - Simplify GetState() impl's for defunct objects, r=david.bolter, sr=neil, a=beltzner
    UltraBlame original commit: 8dc5392b0c1de8502e06c4b8cee32ea875ed82d1

--- accessible/src/xul/nsXULColorPickerAccessible.cpp
+++ accessible/src/xul/nsXULColorPickerAccessible.cpp
@@ -71,9 +71,7 @@ nsXULColorPickerTileAccessible::GetStateInternal(PRUint32 *aState,
 {
   
   nsresult rv = nsFormControlAccessible::GetStateInternal(aState, aExtraState);
-  NS_ENSURE_SUCCESS(rv, rv);
-  if (!mDOMNode)
-    return NS_OK;
+  NS_ENSURE_A11Y_SUCCESS(rv, rv);
 
   *aState |= nsIAccessibleStates::STATE_FOCUSABLE;
 
@@ -130,9 +128,7 @@ nsXULColorPickerAccessible::GetStateInternal(PRUint32 *aState,
 {
   
   nsresult rv = nsFormControlAccessible::GetStateInternal(aState, aExtraState);
-  NS_ENSURE_SUCCESS(rv, rv);
-  if (!mDOMNode)
-    return NS_OK;
+  NS_ENSURE_A11Y_SUCCESS(rv, rv);
 
   *aState |= nsIAccessibleStates::STATE_FOCUSABLE |
              nsIAccessibleStates::STATE_HASPOPUP;
