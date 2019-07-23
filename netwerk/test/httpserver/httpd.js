08db8976bfca7c269024da4b4cdec2aae8374cf7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:07:21 +0000

    Extensive debugging instrumentation for bug 536603
    UltraBlame original commit: 153390d0f83b68e7e29d9d0bf79c8f17716aeeff

--- netwerk/test/httpserver/httpd.js
+++ netwerk/test/httpserver/httpd.js
@@ -57,6 +57,9 @@ const PR_UINT32_MAX = Math.pow(2, 32) - 1;
 
 var DEBUG = false; 
 
+
+var DEBUG_TIMESTAMP = false; 
+
 var gGlobalObject = this;
 
 
@@ -75,7 +78,7 @@ function NS_ASSERT(cond, msg)
 
     var stack = new Error().stack.split(/\n/);
     dumpn(stack.map(function(val) { return "###!!!   " + val; }).join("\n"));
-    
+
     throw Cr.NS_ERROR_ABORT;
   }
 }
@@ -166,10 +169,26 @@ const SJS_TYPE = "sjs";
 
 
 
+var first_stamp = null;
 function dumpn(str)
 {
-  if (DEBUG)
-    dump(str + "\n");
+  if (DEBUG) {
+    var prefix = "|HTTPD|";
+    if (DEBUG_TIMESTAMP) {
+      if (first_stamp === null) {
+        first_stamp = new Date();
+      }
+      var elapsed = (new Date() - first_stamp)/1000; 
+      var min = Math.floor(elapsed/60);
+      var sec = elapsed - 60*min;
+
+      if (sec < 10)
+        prefix += min + ":0" + sec.toFixed(3) + " ";
+      else
+        prefix += min + ":" + sec.toFixed(3) + " ";
+    }
+    dump(prefix + str + "\n");
+  }
 }
 
 
--- testing/mochitest/server.js
+++ testing/mochitest/server.js
@@ -198,6 +198,7 @@ function createMochitestServer(serverBasePath)
 
   server.registerDirectory("/", serverBasePath);
   server.registerPathHandler("/server/shutdown", serverShutdown);
+  server.registerPathHandler("/server/debug", serverDebug);
   server.registerContentType("sjs", "sjs"); 
   server.registerContentType("jar", "application/x-jar");
   server.registerContentType("ogg", "application/ogg");
@@ -295,6 +296,36 @@ function serverShutdown(metadata, response)
 
   dumpn("Server shutting down now...");
   server.stop(serverStopped);
+}
+
+
+function serverDebug(metadata, response)
+{
+  var mode;
+  if (metadata.queryString.length != 1) {
+    throw HTTP_400;
+  } else if (metadata.queryString[0] == '0') {
+    
+    dumpn("Server debug logs disabled.");
+    DEBUG = false;
+    DEBUG_TIMESTAMP = false;
+    mode = "disabled";
+  } else if (metadata.queryString[0] == '1') {
+    DEBUG = true;
+    DEBUG_TIMESTAMP = false;
+    mode = "enabled";
+  } else if (metadata.queryString[0] == '2') {
+    DEBUG = true;
+    DEBUG_TIMESTAMP = true;
+    mode = "enabled, with timestamps";
+  } else {
+    throw HTTP_400;
+  }
+  response.setStatusLine("1.1", 200, "OK");
+  response.setHeader("Content-type", "text/plain", false);
+  var body = "Server debug logs " + mode + ".";
+  response.bodyOutputStream.write(body, body.length);
+  dumpn(body);
 }
 
 
