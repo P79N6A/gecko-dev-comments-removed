b5fed25576eb9bf2329b8f47253e35e651ac8abd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:46:09 +0000

    bug 469044 - JavaScript Test by Jesse Ruderman.
    UltraBlame original commit: c359260c50098f7b1ee143f69eb894506b7bf46d

---
+++ js/tests/js1_5/Regress/regress-469044.js
@@ -0,0 +1,104 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-469044.js';
+
+var BUGNUMBER = 469044;
+var summary = 'type unstable globals';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  expect = '---000---000';
+  actual = '';
+
+  for (var i = 0; i < 2; ++i) {
+    for (var e = 0; e < 2; ++e) {
+    }
+    var c = void 0;
+    print(actual += "---");
+    for (var a = 0; a < 3; ++a) {
+      c <<= c;
+      print(actual += "" + c);
+    }
+  }
+  reportCompare(expect, actual, summary + ': 1');
+
+  expect = '00000000';
+  actual = '';
+
+  print("");
+  for (var i = 0; i < 2; ++i) {
+    for (var e = 0; e < 2; ++e) {
+    }
+    var c = void 0;
+    for (var a = 0; a < 3; ++a) {
+      c <<= c;
+      print(actual += "" + c);
+    }
+    print(actual += c);
+  }
+  reportCompare(expect, actual, summary + ': 2');
+
+  actual = '';
+  print("");
+
+  for (var i = 0; i < 2; ++i) {
+    for (var e = 0; e < 2; ++e) {
+    }
+    var c = void 0;
+    for (var a = 0; a < 3; ++a) {
+      c <<= c;
+      Math;
+      print(actual += "" + c);
+    }
+    print(actual += c);
+  }  
+  reportCompare(expect, actual, summary + ': 3');
+
+  exitFunc ('test');
+}
