e9fadae0b4f0fe08eb14fafc673e6401f3f842a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:58:07 +0000

    Bug 413789 - Remove "#" href from "Toggle passed tests" and "Toggle failed tests".  This unbreaks tests which strictly rely on exact URL matches against the test window, in the case where you run the test, click a Toggle link, and then reload the page (which has a # tacked onto the URL before this patch).  r+a=sayrer
    UltraBlame original commit: c3e9af5abb1c16e28e8a4bf2542d65816d9b91b8

--- testing/mochitest/tests/SimpleTest/SimpleTest.js
+++ testing/mochitest/tests/SimpleTest/SimpleTest.js
@@ -150,9 +150,11 @@ SimpleTest.toggle = function(el) {
 
 
 
-SimpleTest.toggleByClass = function (cls) {
+SimpleTest.toggleByClass = function (cls, evt) {
     var elems = getElementsByTagAndClassName('div', cls);
     MochiKit.Base.map(SimpleTest.toggle, elems);
+    if (evt)
+        evt.preventDefault();
 };
 
 
