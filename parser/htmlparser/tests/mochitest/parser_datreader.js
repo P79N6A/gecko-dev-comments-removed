ae5c4d9fb926c1e437f0a21cb3aacd6c76bffb96
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:32:43 +0000

    Bug 509672 - Make HTML5 parser not fire mutation events when adding attributes to root or body elements. r=jonas
    UltraBlame original commit: 58f91861b1f15eda8ebc700f38dda791ea1741aa

--- parser/htmlparser/tests/mochitest/parser_datreader.js
+++ parser/htmlparser/tests/mochitest/parser_datreader.js
@@ -58,6 +58,137 @@ function trimString(s) {
   return(s.replace(/^\s+/,'').replace(/\s+$/,''));
 }
 
+function getLastLine(str) {
+  var str_array = str.split("\n");
+  let last_line = str_array[str_array.length - 1];
+  return last_line;
+}
+
+
+
+
+
+
+
+
+
+
+var gDumpMode;
+function dumpTree(buf, obj, indent) {
+  var buffer = buf;
+  if (typeof(obj) == "object" && (obj instanceof Array)) {
+    for each (var item in obj) {
+      [buffer, indent] = dumpTree(buffer, item, indent);
+    }
+    gDumpMode = -1;
+  }
+  else {
+    
+    switch(obj) {
+      case "ParseError":
+        
+        break;
+      case "Character":
+        gDumpMode = Node.TEXT_NODE;
+        break;
+      case "StartTag":
+        gDumpMode = Node.ELEMENT_NODE;
+        break;
+      case "EndTag":
+        indent = indent.substring(2);
+        break;
+      case "Comment":
+        gDumpMode = Node.COMMENT_NODE;
+        break;
+      case "DOCTYPE":
+        gDumpMode = Node.DOCUMENT_TYPE_NODE;
+        break;
+      default:
+        switch(gDumpMode) {
+          case Node.DOCUMENT_TYPE_NODE:
+            buffer += "<!DOCTYPE " + obj + ">\n<html>\n  <head>\n  <body>";
+            indent += "    "
+            gDumpMode = -1;
+            break;
+          case Node.COMMENT_NODE:
+            if (buffer.length > 1) {
+              buffer += "\n";
+            }
+            buffer += indent + "<!-- " + obj + " -->";
+            gDumpMode = -1;
+            break;
+          case Node.ATTRIBUTE_NODE:
+            is(typeof(obj), "object", "obj not an object!");
+            indent += "  ";
+            for (var key in obj) {
+              buffer += "\n" + indent + key + "=\"" + obj[key] + "\"";
+            }
+            gDumpMode = -1;
+            break;
+          case Node.TEXT_NODE:
+            if (buffer.indexOf("<head>") == -1) {
+              buffer += "\n<html>\n  <head>\n  <body>";
+              indent += "    ";
+            }
+            
+            
+            
+            let last_line = trimString(getLastLine(buffer));
+            if (last_line[0] == "\"" && 
+              last_line[last_line.length - 1] == "\"") {
+              buffer = buffer.substring(0, buffer.length - 1);    
+            }
+            else {
+              buffer += "\n" + indent + "\"";  
+            }
+            buffer += obj + "\"";
+            break;
+          case Node.ELEMENT_NODE:
+            buffer += "\n" + indent + "<" + obj + ">";
+            gDumpMode = Node.ATTRIBUTE_NODE;
+            break;
+          default:
+            
+            break;
+        }
+        break;
+    }
+  }
+  return [buffer, indent];
+}
+
+
+
+
+
+
+
+function parseJsonTestcase(testcase) {
+  gDumpMode = -1;
+  
+  
+  
+  if (testcase["input"].toLowerCase().indexOf("<!doc") == 0) {
+    var test_output = dumpTree(
+      "", 
+      testcase["output"],
+      "");
+  } else {
+    var test_output = dumpTree(
+      "<!DOCTYPE html>\n<html>\n  <head>\n  <body>", 
+      testcase["output"],
+      "    ");
+  }
+  
+  
+  if (test_output[0].indexOf("<head>") == -1) {
+    test_output[0] += "\n<html>\n  <head>\n  <body>";
+  }
+  return [testcase["input"], test_output[0], "",
+    testcase["description"],
+    JSON.stringify(testcase["output"])];
+}
+
 
 
 
@@ -66,6 +197,7 @@ function trimString(s) {
 
 
 function parseTestcase(testcase) {
+  var documentFragmentTest = false;
   var lines = testcase.split("\n");
   if (lines[0] != "#data") {
     log(lines);
@@ -74,10 +206,15 @@ function parseTestcase(testcase) {
   var input = [];
   var output = [];
   var errors = [];
+  var description = undefined;
+  var expectedTokenizerOutput = undefined;
   var currentList = input;
   for each (var line in lines) {
-    if (line && !(startsWith(line, "#error") ||
+    
+    if ((line || currentList == input) && !(startsWith(line, "#errors") ||
 		  startsWith(line, "#document") ||
+		  startsWith(line, "#description") ||
+		  startsWith(line, "#expected") || 
 		  startsWith(line, "#data"))) {
       if (currentList == output && startsWith(line, "|")) {
       	currentList.push(line.substring(2));
@@ -88,10 +225,20 @@ function parseTestcase(testcase) {
       currentList = errors;
     } else if (line == "#document") {
       currentList = output;
+    } else if (line == "#document-fragment") {
+      documentFragmentTest = true;
     }
   }  
   
-  return [input.join("\n"), output.join("\n"), errors];
+  
+  
+  
+  if (documentFragmentTest) {
+    output = [];
+  }
+  
+  return [input.join("\n"), output.join("\n"), errors, description,
+    expectedTokenizerOutput];
 }
 
 
@@ -173,12 +320,23 @@ function isAttributeLine(line) {
 
 
 
+
 function test_parser(testlist) {
-  for each (var testgroup in testlist) {
-    var tests = testgroup.split("#data\n");
-    tests = ["#data\n" + test for each(test in tests) if (test)];
-    for each (var test in tests) {
-      yield parseTestcase(test);
+  var index = 1;
+  if (gTokenizerMode) {
+    for each (var test in testlist) {
+      var tmpArray = [index];
+      yield tmpArray.concat(parseJsonTestcase(test));
+      index++;
+    }
+  }
+  else {
+    for each (var testgroup in testlist) {
+      var tests = testgroup.split("#data\n");
+      tests = ["#data\n" + test for each(test in tests) if (test)];
+      for each (var test in tests) {
+        yield parseTestcase(test);
+      }
     }
   }
 }
@@ -197,29 +355,61 @@ function docToTestOutput(doc) {
 function addLevels(walker, buf, indent) {
   if(walker.firstChild()) {
     do {
-      buf += indent;
       switch (walker.currentNode.nodeType) {
         case Node.ELEMENT_NODE:
-          buf += "<" + walker.currentNode.tagName.toLowerCase() + ">";
+          buf += indent + "<";
+          
+          
+          if (walker.currentNode.namespaceURI.toLowerCase().
+          indexOf("math") != -1) {
+            buf += "math " + walker.currentNode.tagName.toLowerCase() + ">\n";
+          }
+          else if (walker.currentNode.namespaceURI.toLowerCase().
+          indexOf("svg") != -1) {
+            buf += "svg " + walker.currentNode.tagName + ">\n";
+          }
+          else {
+            buf += walker.currentNode.tagName.toLowerCase() + ">\n";
+          }
           if (walker.currentNode.hasAttributes()) {
             var attrs = walker.currentNode.attributes;
             for (var i=0; i < attrs.length; ++i) {
-              buf += "\n" + indent + "  " + attrs[i].name + 
-                     "=\"" + attrs[i].value +"\"";
+              
+              
+              var attrname = attrs[i].name;
+              if (attrname != "-moz-math-font-style") {
+                buf += indent + "  " + attrname + 
+                       "=\"" + attrs[i].value +"\"\n";
+              }
             }
           }
           break;
         case Node.DOCUMENT_TYPE_NODE:
-          buf += "<!DOCTYPE " + walker.currentNode.name + ">";
+          if (!gJSCompatibilityMode) {
+            buf += indent + "<!DOCTYPE " + walker.currentNode.name + ">\n";
+          }
           break;
         case Node.COMMENT_NODE:
-          buf += "<!-- " + walker.currentNode.nodeValue + " -->";
+          if (!gJSCompatibilityMode) {
+            buf += indent + "<!-- " + walker.currentNode.nodeValue + " -->\n";
+          }
           break;
         case Node.TEXT_NODE:
-          buf += "\"" + walker.currentNode.nodeValue + "\"";
+          
+          
+          
+          let last_line = getLastLine(
+            buf.substring(0, buf.length - 1));
+          if (last_line[indent.length] == "\"" && 
+            last_line[last_line.length - 1] == "\"") {
+            buf = buf.substring(0, buf.length - 2);    
+          }
+          else {
+            buf += indent + "\"";
+          }
+          buf += walker.currentNode.nodeValue + "\"\n";
           break;
       }
-      buf += "\n";
       buf = addLevels(walker, buf, indent + "  ");
     } while(walker.nextSibling());
     walker.parentNode();
--- parser/htmlparser/tests/mochitest/parser_web_testrunner.js
+++ parser/htmlparser/tests/mochitest/parser_web_testrunner.js
@@ -48,18 +48,70 @@
 
 
 
-function writeErrorSummary(input, expected, got, isTodo) {
+function writeErrorSummary(input, expected, got, isTodo, description,
+  expectedTokenizerOutput) {
   if (!isTodo) {
     appendChildNodes($("display"), H2("Unexpected Failure:"));
   }
   appendChildNodes(
     $("display"), BR(),
-    SPAN("Matched: "), "" + (expected == got),
-    P("Input: " + input),
-    PRE("Expected:\n|" + expected +"|", "\n-\n",
-        "Output:\n|" + got + "|\n\n"),
+    SPAN("Matched: "), "" + (expected == got)
+  );
+  if (typeof(description) != "undefined") {
+    appendChildNodes(
+      $("display"), P("Description: " + description)
+    );
+  }
+  appendChildNodes(
+    $("display"), 
+    PRE("Input: " + input)
+  );
+  if (typeof(expectedTokenizerOutput) != "undefined") {
+    appendChildNodes(
+      $("display"), P("Expected raw tokenizer output: " +
+        expectedTokenizerOutput)
+    );
+  }
+  let expectedTitle = "Expected:";
+  let outputTitle = "Output:";
+  if (gJSCompatibilityMode) {
+    outputTitle = "Gecko parser output:";
+    expectedTitle = "JavaScript parser output:";
+  }
+  appendChildNodes(
+    $("display"),
+    PRE(expectedTitle + "\n|" + expected +"|", "\n-\n",
+        outputTitle + "\n|" + got + "|\n\n"),
     HR()
   );
+}
+
+function checkTests(input, expected, errors, description, 
+  expectedTokenizerOutput, testDocument) {
+  
+  
+  if (expected.length > 1) {    
+    var domAsString = docToTestOutput(testDocument);
+    
+    if (expected == domAsString) {
+      is(domAsString, expected, "HTML5 expected success. " + new Date());
+    } else {
+      var reorderedDOM = reorderToMatchExpected(domAsString, expected);
+      if (!gJSCompatibilityMode && html5Exceptions[input]) {
+        todo(reorderedDOM == expected, "HTML5 expected failure. " + new Date());
+        writeErrorSummary(input, expected, reorderedDOM, true, description,
+          expectedTokenizerOutput);
+      } else {
+        if (reorderedDOM != expected) {
+          is(reorderedDOM, expected, "HTML5 unexpected failure. " + input + " " + new Date());
+          writeErrorSummary(input, expected, reorderedDOM, false,
+            description, expectedTokenizerOutput);
+        } else {
+          is(reorderedDOM, expected, "HTML5 expected success. " + new Date());
+        }
+      }
+    }
+  }    
 }
 
 
@@ -67,39 +119,55 @@ function writeErrorSummary(input, expected, got, isTodo) {
 
 
 
-function makeTestChecker(input, expected, errors) {
+function makeTestChecker(input, expected, errors, description, 
+  expectedTokenizerOutput) {
   return function (e) {
-    var domAsString = docToTestOutput(e.target.contentDocument);
-    
-    if (expected == domAsString) {
-      is(domAsString, expected, "HTML5 expected success. " + new Date());
-    } else {
-      var reorderedDOM = reorderToMatchExpected(domAsString, expected);
-      if (html5Exceptions[input]) {
-        todo(reorderedDOM == expected, "HTML5 expected failure. " + new Date());
-        writeErrorSummary(input, expected, reorderedDOM, true);
-      } else {
-        if (reorderedDOM != expected) {
-          is(reorderedDOM, expected, "HTML5 unexpected failure. " + input + " " + new Date());
-          writeErrorSummary(input, expected, reorderedDOM, false);
-        } else {
-          is(reorderedDOM, expected, "HTML5 expected success. " + new Date());
-        }
-      }
+    if (!gJSCompatibilityMode) {
+      checkTests(input, expected, errors, description, 
+        expectedTokenizerOutput, e.target.contentDocument);
+      nextTest(e.target);
     }
-    nextTest(e.target);
-  } 
+    else {
+      window.parseHtmlDocument(input, $("jsframe").contentDocument,
+        function() {
+          expected = docToTestOutput($("jsframe").contentDocument);
+          checkTests(input, expected, errors, description, 
+            expectedTokenizerOutput, e.target.contentDocument);
+          nextTest(e.target);        
+        }, null);      
+    }
+  }
 }
 
 var testcases;
 function nextTest(testframe) {
   var test = 0;
   try {
-    var [input, output, errors] = testcases.next();
-    dataURL = "data:text/html;base64," + btoa(input);
-    testframe.onload = makeTestChecker(input, output, errors);
+    if (gTokenizerMode) {
+      
+      
+      
+      var [index, input, output, errors, description, expectedTokenizerOutput]
+        = testcases.next();
+      dataURL = "tokenizer_file_server.sjs?" + index + 
+        "&" + gTokenizerTestFile;
+    } else {
+      var [input, output, errors, description, expectedTokenizerOutput] = 
+        testcases.next();
+      dataURL = "data:text/html;base64," + btoa(input);
+    }
+    testframe.onload = makeTestChecker(input, output, errors, description,
+      expectedTokenizerOutput);
     testframe.src = dataURL;
   } catch (err if err instanceof StopIteration) {
+    
+    if (typeof(gOriginalHtml5Pref) == "boolean") {
+      netscape.security.PrivilegeManager
+                  .enablePrivilege("UniversalXPConnect");
+      var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                  .getService(Components.interfaces.nsIPrefBranch);
+      prefs.setBoolPref("html5.enable", gOriginalHtml5Pref);
+    }
     SimpleTest.finish();
   }
 }
@@ -121,14 +189,36 @@ function frameLoaded(e) {
 
 
 function makeIFrames() {
-  for each (var filename in parserDatFiles) {
-    var datFrame = document.createElement("iframe");
-    datFrame.onload = frameLoaded;
-    datFrame.src = filename;
-    $("display").appendChild(datFrame);
+  
+  
+  gJSCompatibilityMode = typeof(window.parseHtmlDocument) != "undefined";
+
+  if (gTokenizerMode) {
+    
+    appendChildNodes($("display"), BR(), "Results: ", HR());
+    testcases = test_parser(tokenizerTests["tests"]);    
+    nextTest($("testframe"));
   }
-  appendChildNodes($("display"), BR(), "Results: ", HR());
+  else {
+    for each (var filename in parserDatFiles) {
+      var datFrame = document.createElement("iframe");
+      datFrame.onload = frameLoaded;
+      datFrame.src = filename;
+      $("display").appendChild(datFrame);
+    }
+    appendChildNodes($("display"), BR(), "Results: ", HR());
+  }
 }
+
+
+
+if (typeof(gTokenizerMode) == "undefined") {
+  gTokenizerMode = false;
+}
+
+
+
+var gJSCompatibilityMode;
 
 addLoadEvent(makeIFrames);
 SimpleTest.waitForExplicitFinish();
