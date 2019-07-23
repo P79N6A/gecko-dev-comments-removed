e35d3f48c937dafab9e53b74571021cb54bd8e4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:58:14 +0000

    Bug 481703 - Use python to create pre release suffix. r=bsmedberg
    UltraBlame original commit: ae69c2f236cdcab7377a9c5d17451911e48d3072

---
+++ config/printprereleasesuffix.py
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import sys
+import re
+
+def get_prerelease_suffix(version):
+  """ Returns the prerelease suffix from the version string argument """
+
+  def mfunc(m):
+    return " %s %s %s" % (m.group('prefix'),
+                         {'a': 'Alpha', 'b': 'Beta'}[m.group('c')],
+                         m.group('suffix'))
+  result, c = re.subn(r'^(?P<prefix>(\d+\.)*\d+)(?P<c>[ab])(?P<suffix>\d+)$',
+                      mfunc, version)
+  if c != 1:
+    return ''
+  return result
+
+if len(sys.argv) == 2:
+  print get_prerelease_suffix(sys.argv[1])
