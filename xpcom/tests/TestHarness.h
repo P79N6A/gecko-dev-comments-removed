3ccfa4a4935d6648f29773c00ebf70a59a8aa122
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:10:33 +0000

    bug 486793: add programmatic control of profiler to xpcom unit tests. r=bsmedberg
    UltraBlame original commit: 7be0087c83ca876fbeeea6c12c9df89310c0f334

--- xpcom/tests/TestHarness.h
+++ xpcom/tests/TestHarness.h
@@ -85,6 +85,150 @@ void passed(const char* test)
 }
 
 
+
+
+static const char* gCurrentProfile;
+static PRBool gProfilerTriedInit = PR_FALSE;
+static PRBool gProfilerInited = PR_FALSE;
+
+
+
+
+
+static PRBool _PlatformInitProfiler();
+static PRBool _PlatformStartProfile(const char* profileName);
+static PRBool _PlatformStopProfile(const char* profileName);
+static PRBool _PlatformDeinitProfiler();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static PRBool
+StartProfiling(const char* profileName)
+{
+    if (!gProfilerTriedInit) {
+        gProfilerTriedInit = PR_TRUE;
+        gProfilerInited = _PlatformInitProfiler();
+    }
+    if (!gProfilerInited)
+        return PR_FALSE;
+
+    NS_ASSERTION(profileName, "need a name for this profile");
+    NS_PRECONDITION(!gCurrentProfile, "started a new profile before stopping another");
+
+    PRBool rv = _PlatformStartProfile(profileName);
+    gCurrentProfile = profileName;
+    return rv;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+static PRBool
+StopProfiling()
+{
+    NS_ASSERTION(gProfilerTriedInit, "tried to stop profile before starting one");
+    if (!gProfilerInited)
+        return PR_FALSE;
+
+    NS_PRECONDITION(gCurrentProfile, "tried to stop profile before starting one");
+    
+    const char* profileName = gCurrentProfile;
+    gCurrentProfile = 0;
+    return _PlatformStopProfile(profileName);
+}
+
+
+
+#if defined(MOZ_SHARK)
+#include <CHUD/CHUD.h>
+
+static PRBool
+_PlatformInitProfiler()
+{
+    if (chudSuccess != chudInitialize())
+        return PR_FALSE;
+    if (chudSuccess != chudAcquireRemoteAccess()) {
+        NS_WARNING("Couldn't connect to Shark.  Is it running and in Programmatic mode (Shift-Cmd-R)?");
+        return PR_FALSE;
+    }
+   return PR_TRUE;
+}
+
+static PRBool
+_PlatformStartProfile(const char* profileName)
+{
+    return (chudSuccess == chudStartRemotePerfMonitor(profileName)) ?
+        PR_TRUE : PR_FALSE;
+}
+
+static PRBool
+_PlatformStopProfile(const char* profileName)
+{
+    return (chudSuccess == chudStopRemotePerfMonitor()) ?
+        PR_TRUE : PR_FALSE;
+}
+
+static PRBool
+_PlatformDeinitProfiler()
+{
+    return (chudIsRemoteAccessAcquired() 
+            && chudSuccess == chudReleaseRemoteAccess()) ?
+        PR_TRUE : PR_FALSE;
+}
+
+
+
+#else 
+
+static PRBool
+_PlatformInitProfiler()
+{
+    NS_WARNING("Profiling is not available/configured for your platform.");
+    return PR_FALSE;
+}
+static PRBool
+_PlatformStartProfile(const char* profileName)
+{
+    NS_WARNING("Profiling is not available/configured for your platform.");
+    return PR_FALSE;
+}
+static PRBool
+_PlatformStopProfile(const char* profileName)
+{
+    NS_WARNING("Profiling is not available/configured for your platform.");
+    return PR_FALSE;
+}
+static PRBool
+_PlatformDeinitProfiler()
+{
+    NS_WARNING("Profiling is not available/configured for your platform.");
+    return PR_FALSE;
+}
+
+#endif
+
+
+
 class ScopedXPCOM
 {
   public:
@@ -104,6 +248,10 @@ class ScopedXPCOM
 
     ~ScopedXPCOM()
     {
+      if (gProfilerInited)
+        if (!_PlatformDeinitProfiler())
+          NS_WARNING("Problem shutting down profiler");
+
       if (mServMgr)
       {
         NS_RELEASE(mServMgr);
