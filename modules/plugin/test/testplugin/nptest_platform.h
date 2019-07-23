64ca39a0a1b330038e1cace931e3bf65864a53a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:01:31 +0000

    Bug 484923. Test plugin should support 'window' mode on GTK2/X. r=josh,karl
    UltraBlame original commit: adbc4043e8bd3b7c994c4ca4ea8a20008a16bc8e

--- modules/plugin/test/testplugin/nptest_platform.h
+++ modules/plugin/test/testplugin/nptest_platform.h
@@ -36,8 +36,39 @@
 
 #include "nptest.h"
 
+
+
+
+bool    pluginSupportsWindowMode();
+
+
+
+
+
+
+bool    pluginSupportsWindowlessMode();
+
+
+
+
+
 NPError pluginInstanceInit(InstanceData* instanceData);
+
+
+
+
+void    pluginInstanceShutdown(InstanceData* instanceData);
+
+
+
+
+
+void    pluginWidgetInit(InstanceData* instanceData, void* oldWindow);
+
+
+
+
+
 int16_t pluginHandleEvent(InstanceData* instanceData, void* event);
-void    pluginDraw(InstanceData* instanceData);
 
 #endif 
