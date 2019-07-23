e5b3738db4c6fb099ffa24522811750823b69f6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:13:28 +0000

    Bug 431729: make it clearer that in Clear Private Data, 'Browsing History' deletes 'Download History' too, patch by Shawn Wilsher <sdwilsh@forerunnerdesigns.com>, r=me, a=vlad
    UltraBlame original commit: 38959c8e17bb40ae3704cbef164c41ea79540774

---
+++ browser/base/content/test/browser_sanitize-download-history.js
@@ -0,0 +1,182 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test()
+{
+  
+  
+
+  function test_checkedAndDisabledAtStart(aWin)
+  {
+    let doc = aWin.document;
+    let downloads = doc.getElementById("downloads-checkbox");
+    let history = doc.getElementById("history-checkbox");
+
+    ok(history.checked, "history checkbox is checked");
+    ok(downloads.disabled, "downloads checkbox is disabled");
+    ok(downloads.checked, "downloads checkbox is checked");
+  }
+
+  function test_checkedAndDisabledOnHistoryToggle(aWin)
+  {
+    let doc = aWin.document;
+    let downloads = doc.getElementById("downloads-checkbox");
+    let history = doc.getElementById("history-checkbox");
+
+    EventUtils.synthesizeMouse(history, 0, 0, {}, aWin);
+    ok(!history.checked, "history checkbox is not checked");
+    ok(downloads.disabled, "downloads checkbox is disabled");
+    ok(downloads.checked, "downloads checkbox is checked");
+  }
+
+  function test_checkedAfterAddingDownload(aWin)
+  {
+    let doc = aWin.document;
+    let downloads = doc.getElementById("downloads-checkbox");
+    let history = doc.getElementById("history-checkbox");
+
+    
+    let ios = Cc["@mozilla.org/network/io-service;1"].
+              getService(Ci.nsIIOService);
+    let file = Cc["@mozilla.org/file/directory_service;1"].
+               getService(Ci.nsIProperties).get("TmpD", Ci.nsIFile);
+    file.append("satitize-dm-test.file");
+    file.createUnique(Ci.nsIFile.NORMAL_FILE_TYPE, 0666);
+    let testPath = ios.newFileURI(file).spec;
+    let data = {
+      name: "381603.patch",
+      source: "https://bugzilla.mozilla.org/attachment.cgi?id=266520",
+      target: testPath,
+      startTime: 1180493839859230,
+      endTime: 1180493839859239,
+      state: Ci.nsIDownloadManager.DOWNLOAD_FINISHED,
+      currBytes: 0, maxBytes: -1, preferredAction: 0, autoResume: 0
+    };
+    let db = Cc["@mozilla.org/download-manager;1"].
+             getService(Ci.nsIDownloadManager).DBConnection;
+    let rawStmt = db.createStatement(
+      "INSERT INTO moz_downloads (name, source, target, startTime, endTime, " +
+        "state, currBytes, maxBytes, preferredAction, autoResume) " +
+      "VALUES (:name, :source, :target, :startTime, :endTime, :state, " +
+        ":currBytes, :maxBytes, :preferredAction, :autoResume)");
+    let stmt = Cc["@mozilla.org/storage/statement-wrapper;1"].
+               createInstance(Ci.mozIStorageStatementWrapper);
+    stmt.initialize(rawStmt);
+    try {
+      for (let prop in data)
+        stmt.params[prop] = data[prop];
+      stmt.execute();
+    }
+    finally {
+      stmt.statement.finalize();
+    }
+
+    
+    EventUtils.synthesizeMouse(history, 0, 0, {}, aWin);
+    EventUtils.synthesizeMouse(history, 0, 0, {}, aWin);
+
+    ok(!history.checked, "history checkbox is not checked");
+    ok(!downloads.disabled, "downloads checkbox is not disabled");
+    ok(downloads.checked, "downloads checkbox is checked");
+  }
+
+  function test_checkedAndDisabledWithHistoryChecked(aWin)
+  {
+    let doc = aWin.document;
+    let downloads = doc.getElementById("downloads-checkbox");
+    let history = doc.getElementById("history-checkbox");
+
+    EventUtils.synthesizeMouse(history, 0, 0, {}, aWin);
+    ok(history.checked, "history checkbox is checked");
+    ok(downloads.disabled, "downloads checkbox is disabled");
+    ok(downloads.checked, "downloads checkbox is checked");
+  }
+
+  let tests = [
+    test_checkedAndDisabledAtStart,
+    test_checkedAndDisabledOnHistoryToggle,
+    test_checkedAfterAddingDownload,
+    test_checkedAndDisabledWithHistoryChecked,
+  ];
+
+  
+  
+
+  let dm = Cc["@mozilla.org/download-manager;1"].
+           getService(Ci.nsIDownloadManager);
+  let db = dm.DBConnection;
+
+  
+  db.executeSimpleSQL("DELETE FROM moz_downloads");
+
+  
+  let ww = Cc["@mozilla.org/embedcomp/window-watcher;1"].
+           getService(Ci.nsIWindowWatcher);
+  let win = ww.getWindowByName("Sanatize", null);
+  if (win && (win instanceof Ci.nsIDOMWindowInternal)) win.close();
+
+  
+  ww.registerNotification({
+    observe: function(aSubject, aTopic, aData) {
+      ww.unregisterNotification(this);
+      aSubject.QueryInterface(Ci.nsIDOMEventTarget).
+      addEventListener("DOMContentLoaded", doTest, false);
+    }
+  });
+
+  
+  let doTest = function() setTimeout(function() {
+    let win = ww.getWindowByName("Sanitize", null)
+                .QueryInterface(Ci.nsIDOMWindowInternal);
+
+    for (let i = 0; i < tests.length; i++)
+      tests[i](win);
+
+    win.close();
+    finish();
+  }, 0);
+ 
+  
+  ww.openWindow(window,
+                "chrome://browser/content/sanitize.xul",
+                "Sanitize",
+                "chrome,titlebar,centerscreen",
+                null);
+
+  waitForExplicitFinish();
+}
