00e8db2057fe523bd190c0440c68053ad9775698
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:35:33 +0000

    Backed out changeset 745089a5d1c5
    UltraBlame original commit: 44bce227ed5d3dcfd9bec3faefef49da244e33c0

--- js/src/jstracerinlines.h
+++ js/src/jstracerinlines.h
@@ -249,12 +249,12 @@ call_ValueToECMAInt32(JSContext* cx, jsval& v, jsint& i)
 }
 
 static inline bool
-call_NewIntValue(JSContext* cx, jsint& i, jsval& v)
+call_NewIntInRootedValue(JSContext* cx, jsint& i, jsval& v)
 {
     
 
     recorder(cx)->copy(&i, &v);
-    return interp_call_NewIntValue(cx, i, v);
+    return interp_call_NewIntInRootedValue(cx, i, v);
 }    
 
 static inline void
