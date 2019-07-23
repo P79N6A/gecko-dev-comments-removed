e45abc0dc5b6c00825205cad601c75e114d6ff83
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:32:31 +0000

    Bug 485834 ObserverService shouldn't recurse infinitely for stupid consumers r=dougt
    UltraBlame original commit: 9b612b23ae160a1aaf888876e8c7964e29e4c9b5

--- xpcom/ds/nsObserverService.cpp
+++ xpcom/ds/nsObserverService.cpp
@@ -147,6 +147,9 @@ nsObserverService::RemoveObserver(nsIObserver* anObserver, const char* aTopic)
     if (!observerList)
         return NS_ERROR_FAILURE;
 
+    
+
+    nsCOMPtr<nsIObserver> kungFuDeathGrip(anObserver);
     return observerList->RemoveObserver(anObserver);
 }
 
