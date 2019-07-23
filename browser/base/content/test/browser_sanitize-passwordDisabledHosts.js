b55fc598a50c806fa94fab90d0d5adb985007d20
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:28:13 +0000

    use gPrefService in tests
    UltraBlame original commit: 5bda0fcb85e88e011f050f2a0008337d6a1850b5

--- browser/base/content/test/browser_sanitize-passwordDisabledHosts.js
+++ browser/base/content/test/browser_sanitize-passwordDisabledHosts.js
@@ -1,13 +1,12 @@
 
 
 
-Cc["@mozilla.org/moz/jssubscript-loader;1"].getService(Components.interfaces.mozIJSSubScriptLoader)
+Cc["@mozilla.org/moz/jssubscript-loader;1"].getService(Ci.mozIJSSubScriptLoader)
                                            .loadSubScript("chrome://browser/content/sanitize.js");
 
 function test() {
 
-  var pwmgr = Components.classes["@mozilla.org/login-manager;1"]
-             .getService(Components.interfaces.nsILoginManager);
+  var pwmgr = Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager);
 
   
   pwmgr.setLoginSavingEnabled("http://example.com", false);
@@ -20,9 +19,7 @@ function test() {
   let s = new Sanitizer();
   s.ignoreTimespan = false;
   s.prefDomain = "privacy.cpd.";
-  var itemPrefs = Cc["@mozilla.org/preferences-service;1"]
-                  .getService(Components.interfaces.nsIPrefService)
-                  .getBranch(s.prefDomain);
+  var itemPrefs = gPrefService.getBranch(s.prefDomain);
   itemPrefs.setBoolPref("history", false);
   itemPrefs.setBoolPref("downloads", false);
   itemPrefs.setBoolPref("cache", false);
