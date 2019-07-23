eed67dc8f045a22c8fc0991c46664c63ba3e409b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:56 +0000

    JavaScript Tests - regression test for bug 375183, by Igor Bukanov
    UltraBlame original commit: e9cebde40a548971e20c0ff1a028b698556fa39d

---
+++ js/tests/js1_5/extensions/regress-375183.js
@@ -0,0 +1,95 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-375183.js';
+
+var BUGNUMBER = 375184;
+var summary = '__noSuchMethod__ should not allocate beyond fp->script->depth';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  var obj = { get __noSuchMethod__() {
+      print("Executed");
+      return new Object();
+    }};
+
+  try
+  {
+    obj.x();
+  }
+  catch(ex)
+  {
+  }
+
+  reportCompare(expect, actual, summary + ':1');
+
+  obj = { __noSuchMethod__: {} };
+  try
+  {
+    obj.x();
+  }
+  catch(ex)
+  {
+  }
+
+  reportCompare(expect, actual, summary + ':2');
+
+  obj = { }
+  obj.__noSuchMethod__ = {};
+  try
+  {
+    obj.x();
+  }
+  catch(ex)
+  {
+  }
+
+  reportCompare(expect, actual, summary + ':3');
+
+  exitFunc ('test');
+}
