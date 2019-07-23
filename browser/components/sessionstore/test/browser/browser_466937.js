064bde21e5ff963c2db18427737e883d4a5cde23
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:23:15 +0000

    Bug 466937 - [r=dietrich]
    UltraBlame original commit: 8d0d5017c101dc06e1930a48c5118f85e3fb4796

---
+++ browser/components/sessionstore/test/browser/browser_466937.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  
+  waitForExplicitFinish();
+  
+  let testURL = "http://localhost:8888/browser/" +
+    "browser/components/sessionstore/test/browser/browser_466937_sample.html";
+  let testPath = "/home/user/regular.file";
+  
+  let tab = gBrowser.addTab(testURL);
+  tab.linkedBrowser.addEventListener("load", function(aEvent) {
+    let doc = tab.linkedBrowser.contentDocument;
+    doc.getElementById("reverse_thief").value = "/home/user/secret2";
+    doc.getElementById("bystander").value = testPath;
+    
+    let tab2 = gBrowser.duplicateTab(tab);
+    tab2.linkedBrowser.addEventListener("load", function(aEvent) {
+      doc = tab2.linkedBrowser.contentDocument;
+      is(doc.getElementById("thief").value, "",
+         "file path wasn't set to text field value");
+      is(doc.getElementById("reverse_thief").value, "",
+         "text field value wasn't set to full file path");
+      is(doc.getElementById("bystander").value, testPath,
+         "normal case: file path was correctly preserved");
+      
+      
+      gBrowser.removeTab(tab2);
+      gBrowser.removeTab(tab);
+      
+      finish();
+    }, true);
+  }, true);
+}
