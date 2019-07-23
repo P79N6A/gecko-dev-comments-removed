b56b9051f7ef850c532a99297e8eb1225ede52da
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:01:31 +0000

    Bug 461192: Rename fd_copysign to js_copysign. r=jorendorff
    Since we're no longer using fdlibm, it doesn't make sense to use
    fd_copysign as the name of SpiderMonkey's appropriately chosen version
    of copysign.  js_copysign seems more appropriate.
    
    UltraBlame original commit: 644b31f76962168aaf637d16c77d6305b40559cb

--- js/src/jslibmath.h
+++ js/src/jslibmath.h
@@ -50,19 +50,19 @@
 
 
 #if __GNUC__ >= 4 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
-#define fd_copysign __builtin_copysign
+#define js_copysign __builtin_copysign
 #elif defined WINCE
-#define fd_copysign _copysign
+#define js_copysign _copysign
 #elif defined _WIN32
 #if _MSC_VER < 1400
 
-#define fd_copysign js_copysign
+#define js_copysign js_copysign
 extern double js_copysign(double, double);
 #else
-#define fd_copysign _copysign
+#define js_copysign _copysign
 #endif
 #else
-#define fd_copysign copysign
+#define js_copysign copysign
 #endif
 
 #endif 
