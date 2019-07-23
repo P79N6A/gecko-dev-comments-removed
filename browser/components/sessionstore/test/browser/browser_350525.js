ef466dd38688d2781fe865ebf95d9acdb2c99e43
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:43:32 +0000

    Bug 451366: Convert chrome tests to browser tests. r=dietrich
    UltraBlame original commit: 7a2ba6daa301f476436639ae292ee745211d91df

---
+++ browser/components/sessionstore/test/browser/browser_350525.js
@@ -0,0 +1,100 @@
+function test() {
+  
+  
+  function test(aLambda) {
+    try {
+      return aLambda() || true;
+    }
+    catch (ex) { }
+    return false;
+  }
+  
+  
+  let tabbrowser = getBrowser();
+  waitForExplicitFinish();
+  
+  
+  let ssComponent = test(function() Cc["@mozilla.org/browser/sessionstore;1"]);
+  ok(ssComponent, "reference the sessionstore component");
+  
+  
+  let ss = test(function() ssComponent.getService(Ci.nsISessionStore));
+  ok(ss, "reference the sessionstore service");
+  
+  
+  
+  
+  let key = "Unique name: " + Date.now();
+  let value = "Unique value: " + Math.random();
+  
+  
+  ok(test(function() ss.setWindowValue(window, key, value)), "set a window value");
+  
+  
+  is(ss.getWindowValue(window, key), value, "stored window value matches original");
+  
+  
+  ok(test(function() ss.deleteWindowValue(window, key)), "delete the window value");
+  
+  
+  is(ss.getWindowValue(window, key), "", "window value was deleted");
+  
+  
+  
+  
+  key = "Unique name: " + Math.random();
+  value = "Unique value: " + Date.now();
+  let tab = tabbrowser.addTab();
+  
+  
+  ok(test(function() ss.setTabValue(tab, key, value)), "store a tab value");
+  
+  
+  is(ss.getTabValue(tab, key), value, "stored tab value match original");
+  
+  
+  ok(test(function() ss.deleteTabValue(tab, key)), "delete the tab value");
+  
+  is(ss.getTabValue(tab, key), "", "tab value was deleted");
+  
+  
+  tabbrowser.removeTab(tab);
+  
+  
+  
+  
+  
+  
+  let count = ss.getClosedTabCount(window);
+  let max_tabs_undo = gPrefService.getIntPref("browser.sessionstore.max_tabs_undo");
+  ok(0 <= count && count <= max_tabs_undo,
+     "getClosedTabCount returns zero or at most max_tabs_undo");
+  
+  
+  let testURL = "about:";
+  tab = tabbrowser.addTab(testURL);
+  tab.linkedBrowser.addEventListener("load", function(aEvent) {
+    
+    gPrefService.setIntPref("browser.sessionstore.max_tabs_undo", max_tabs_undo + 1);
+    
+    
+    tabbrowser.removeTab(tab);
+    
+    
+    var newcount = ss.getClosedTabCount(window);
+    ok(newcount > count, "after closing a tab, getClosedTabCount has been incremented");
+    
+    
+    ok(test(function() ss.undoCloseTab(window, 0)), "undoCloseTab doesn't throw")
+    tab = tabbrowser.selectedTab;
+    
+    tab.linkedBrowser.addEventListener("load", function(aEvent) {
+      is(this.currentURI.spec, testURL, "correct tab was reopened");
+      
+      
+      gPrefService.setIntPref("browser.sessionstore.max_tabs_undo", max_tabs_undo);
+      tabbrowser.removeTab(tab);
+      finish();
+    }, true);
+  }, true);
+}
---
+++ browser/components/sessionstore/test/browser/browser_393716.js
@@ -0,0 +1,78 @@
+function test() {
+  
+  
+  
+  try {
+    var ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
+  }
+  catch (ex) { }
+  ok(ss, "SessionStore service is available");
+  let tabbrowser = getBrowser();
+  waitForExplicitFinish();
+  
+  
+  
+  
+  let key = "Unique key: " + Date.now();
+  let value = "Unique value: " + Math.random();
+  let testURL = "about:config";
+  
+  
+  let tab = tabbrowser.addTab(testURL);
+  ss.setTabValue(tab, key, value);
+  tab.linkedBrowser.addEventListener("load", function(aEvent) {
+    
+    let state = ss.getTabState(tab);
+    ok(state, "get the tab's state");
+    
+    
+    state = eval("(" + state + ")");
+    ok(state instanceof Object && state.entries instanceof Array && state.entries.length > 0,
+       "state object seems valid");
+    ok(state.entries.length == 1 && state.entries[0].url == testURL,
+       "Got the expected state object (test URL)");
+    ok(state.extData && state.extData[key] == value,
+       "Got the expected state object (test manually set tab value)");
+    
+    
+    tabbrowser.removeTab(tab);
+  }, true);
+  
+  
+  
+  
+  let key2 = "key2";
+  let value2 = "Value " + Math.random();
+  let value3 = "Another value: " + Date.now();
+  let state = { entries: [{ url: testURL }], extData: { key2: value2 } };
+  
+  
+  let tab2 = tabbrowser.addTab();
+  
+  ss.setTabState(tab2, state.toSource());
+  tab2.linkedBrowser.addEventListener("load", function(aEvent) {
+    
+    ok(ss.getTabValue(tab2, key2) == value2 && this.currentURI.spec == testURL,
+       "the tab's state was correctly restored");
+    
+    
+    let textbox = this.contentDocument.getElementById("textbox");
+    textbox.wrappedJSObject.value = value3;
+    
+    
+    let duplicateTab = ss.duplicateTab(window, tab2);
+    tabbrowser.removeTab(tab2);
+    
+    duplicateTab.linkedBrowser.addEventListener("load", function(aEvent) {
+      
+      ok(ss.getTabValue(duplicateTab, key2) == value2 && this.currentURI.spec == testURL,
+         "correctly duplicated the tab's state");
+      let textbox = this.contentDocument.getElementById("textbox");
+      is(textbox.wrappedJSObject.value, value3, "also duplicated text data");
+      
+      
+      tabbrowser.removeTab(duplicateTab);
+      finish();
+    }, true);
+  }, true);
+}
