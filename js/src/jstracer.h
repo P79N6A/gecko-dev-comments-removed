3af69164de51ce17e396e253224048e2557a3d83
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:34:19 +0000

    Merge.
    UltraBlame original commit: 783ab3f4f488fb54bf50ccb6a7ac424c4217f84f

--- js/src/jstracer.h
+++ js/src/jstracer.h
@@ -55,6 +55,7 @@
 struct JSTraceMonitor {
     jsval*      loopTable;
     uint32      loopTableSize;
+    JSObject*   recorder;
 };
 
 #define TRACE_THRESHOLD 10
@@ -62,5 +63,18 @@ struct JSTraceMonitor {
 uint32 js_AllocateLoopTableSlots(JSContext* cx, uint32 nloops);
 void   js_FreeLoopTableSlots(JSContext* cx, uint32 base, uint32 nloops);
 bool   js_GrowLoopTable(JSContext* cx, uint32 slot);
+jsval  js_CallRecorder(JSContext* cx, const char* fn, uintN argc, jsval* argv);
+
+
+
+
+
+
+static inline jsval
+native_pointer_to_jsval(void* p)
+{
+    JS_ASSERT(INT_FITS_IN_JSVAL((int)p));
+    return INT_TO_JSVAL((int)p);
+}
 
 #endif 
