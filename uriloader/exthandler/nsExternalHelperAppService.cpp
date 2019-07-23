a0ec8d65d8515ee43e8b06659d88c0db69cb4105
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:48:42 +0000

    [Bug 280419] When a temporary file is created from the download manager it should be readonly. Also applies to attachments in mail. r=cbiesinger, sr=dmose, a=bsmedberg
    UltraBlame original commit: 0376d89f39a260a30791a949bf8bb0f43053cd62

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -1979,6 +1979,9 @@ nsresult nsExternalAppHandler::OpenWithApplication()
   
   if (mStopRequestIssued)
   {
+    
+    mFinalFileDestination->SetPermissions(0400);
+
     rv = mMimeInfo->LaunchWithFile(mFinalFileDestination);        
     if (NS_FAILED(rv))
     {
