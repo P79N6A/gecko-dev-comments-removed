6047c529705329df9671d46980c32becf776c1db
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:51:56 +0000

    Changes for SH4 and ARM processors. These were made by Roger Maclean, reviewed by me. r=rmaclean@qnx.com This change is for the QNX (Photon) platform only. It will not affect the runtime or build of other Mozilla platforms since QNX source is not part of Tinderbox builds. None of the changes will affect non-QNX builds in any way. You can contact me at: mfeil@qnx.com
    UltraBlame original commit: b8d9d76edbebc7bcc6e4de902ddda8f823c410b2

---
+++ xpcom/reflect/xptcall/src/md/unix/xptcinvoke_nto_shle.cpp
@@ -0,0 +1,150 @@
+#include "xptcprivate.h"
+
+extern "C" {
+
+const int c_int_register_params = 3;
+const int c_float_register_params = 8;
+
+static void
+copy_to_stack(PRUint32 paramCount, nsXPTCVariant* s, int size, PRUint32* data)
+{
+	int intCount = 0;
+	int floatCount = 0;
+	PRUint32 *intRegParams = data + (size / sizeof(PRUint32)) - c_int_register_params;
+	float  *floatRegParams = (float *)intRegParams - c_float_register_params;
+
+    for ( PRUint32 i = 0; i < paramCount; ++i, ++s )
+    {
+		nsXPTType type = s->IsPtrData() ? nsXPTType::T_I32 : s->type;
+
+		switch ( type ) {
+        case nsXPTType::T_I64:
+        case nsXPTType::T_U64:
+			
+			if ( (c_int_register_params - intCount) >= 2 ) {
+				*((PRInt64 *) intRegParams) = s->val.i64;
+				intRegParams += 2;
+				intCount += 2;
+			}
+			else {
+				*((PRInt64*) data) = s->val.i64;
+				data += 2;
+			}
+			break;
+        case nsXPTType::T_FLOAT:
+			
+			if ( floatCount < c_float_register_params ) {
+				*floatRegParams = s->val.f;
+				++floatCount;
+				++floatRegParams;
+			}
+			else {
+				*((float*) data) = s->val.f;
+				++data;
+			}			
+			break;
+        case nsXPTType::T_DOUBLE:
+			
+			if ( (c_float_register_params - floatCount) >= 2  ) {
+				if ( (floatCount & 1) != 0 ) {
+					++floatCount;
+					++floatRegParams;					
+				}
+				*(double *)floatRegParams = s->val.d;
+				floatCount += 2;
+				floatRegParams += 2;
+			}
+			else {
+				*((double *) data) = s->val.d;
+				data += 2;
+			}			
+			break;
+		default:		
+			PRInt32 value = (PRInt32) (s->IsPtrData() ?  s->ptr : s->val.p);
+			
+			if ( intCount < c_int_register_params ) {
+				*intRegParams = value;
+				++intRegParams;
+				++intCount;
+			}
+			else {
+				*data = value;
+				++data;
+			}
+			break;
+        }
+    }	
+}
+
+XPTC_PUBLIC_API(nsresult)
+XPTC_InvokeByIndex(nsISupports* that, PRUint32 methodIndex,
+                   PRUint32 paramCount, nsXPTCVariant* params)
+{
+#ifdef __GNUC__            
+	int result;
+	void (*fn_copy) (PRUint32 paramCount, nsXPTCVariant* s, int size, PRUint32* d) = copy_to_stack;
+
+	
+
+
+
+
+
+
+ __asm__ __volatile__(
+    
+
+
+
+
+
+	"mov.l	@(8,r14), r4 \n\t"	
+	"mov	r4, r6 \n\t"
+	"mov	#3, r1 \n\t"
+	"shld	r1, r6 \n\t"
+	"add	#36, r6 \n\t"		
+	"sub	r6, r15 \n\t"
+	"mov	r15, r7 \n\t"
+	"mov.l	@(20,r14), r1 \n\t"	
+	"jsr	@r1 \n\t"			
+	"mov.l	@(12,r14), r5 \n\t"	
+
+	
+
+
+
+	
+	"mov	r14, r1 \n\t"
+	"add	#-44, r1 \n\t"
+	"fmov.s	@r1+, fr5 \n\t"
+	"fmov.s	@r1+, fr4 \n\t"
+	"fmov.s	@r1+, fr7 \n\t"
+	"fmov.s	@r1+, fr6 \n\t"
+	"fmov.s	@r1+, fr9 \n\t"
+	"fmov.s	@r1+, fr8 \n\t"
+	"fmov.s	@r1+, fr11 \n\t"
+	"fmov.s	@r1+, fr10 \n\t"
+	"mov.l	@r1+, r5 \n\t"
+	"mov.l	@r1+, r6 \n\t"
+	"mov.l	@r1+, r7 \n\t"	
+	"mov.l	@r14, r1 \n\t"		
+	"mov.l	@r1, r1 \n\t"		
+	"mov.l	@(4,r14), r4 \n\t"	
+	"shll2	r4	\n\t"
+	"add	r4, r1 \n\t"
+	"mov.l	@r1, r1 \n\t"		
+	"jsr	@r1 \n\t"
+	"mov.l	@r14, r4 \n\t"		
+	"mov.l	r0, @(16,r14) \n\r"	
+    : : : "r1", "r4", "r6", "r7", "pr", "dr4", "dr6", "dr8"
+   );
+    
+  return result;
+
+#else
+#error "can't find a compiler to use"
+#endif 
+
+}
+
+}
---
+++ xpcom/reflect/xptcall/src/md/unix/xptcstubs_nto_shle.cpp
@@ -0,0 +1,140 @@
+#include "xptcprivate.h"
+
+const int c_int_register_params = 3;
+const int c_float_register_params = 8;
+
+
+
+
+
+
+
+
+
+
+
+extern "C" nsresult
+PrepareAndDispatch(nsXPTCStubBase* self, int methodIndex, PRUint32* data, 
+				   PRUint32 *intRegParams, float *floatRegParams)
+{
+#define PARAM_BUFFER_COUNT     16
+
+	nsresult result = NS_ERROR_FAILURE;
+	int intCount = 0;
+	int floatCount = 0;
+    nsXPTCMiniVariant paramBuffer[PARAM_BUFFER_COUNT];
+    nsXPTCMiniVariant* dispatchParams = NULL;
+    nsIInterfaceInfo* iface_info = NULL;
+    const nsXPTMethodInfo* info;
+    PRUint8 paramCount;
+	PRUint8 i;
+
+	NS_ASSERTION(self,"no self");
+
+	self->GetInterfaceInfo(&iface_info);
+	NS_ASSERTION(iface_info,"no interface info");
+
+	iface_info->GetMethodInfo(PRUint16(methodIndex), &info);
+	NS_ASSERTION(info,"no interface info");
+
+	paramCount = info->GetParamCount();
+
+	
+	if(paramCount > PARAM_BUFFER_COUNT)
+		dispatchParams = new nsXPTCMiniVariant[paramCount];
+	else
+		dispatchParams = paramBuffer;
+	NS_ASSERTION(dispatchParams,"no place for params");
+
+	for ( i = 0; i < paramCount; ++i ) {
+		const nsXPTParamInfo& param = info->GetParam(i);
+		nsXPTCMiniVariant* dp = &dispatchParams[i];
+		nsXPTType type = param.IsOut() ? nsXPTType::T_I32 : param.GetType();
+
+		switch ( type ) {
+		case nsXPTType::T_I64:
+		case nsXPTType::T_U64:
+			
+			if ( (c_int_register_params - intCount) >= 2 ) {
+				dp->val.i64 = *((PRInt64 *) intRegParams);
+				intRegParams += 2;
+				intCount += 2;
+			}
+			else {
+				dp->val.i64 = *((PRInt64*) data);
+				data += 2;
+			}
+			break;
+        case nsXPTType::T_FLOAT:
+			
+			if ( floatCount < c_float_register_params ) {
+				dp->val.f = *floatRegParams;
+				++floatCount;
+				++floatRegParams;
+			}
+			else {
+				dp->val.f = *((float*) data);
+				++data;
+			}			
+			break;
+        case nsXPTType::T_DOUBLE:
+			
+			if ( (c_float_register_params - floatCount) >= 2  ) {
+				if ( floatCount & 1 != 0 ) {
+					++floatCount;
+					++floatRegParams;
+				}
+				dp->val.d = *(double *)floatRegParams;
+				floatCount += 2;
+				floatRegParams += 2;
+			}
+			else {
+				dp->val.d = *((double *) data);
+				data += 2;
+			}			
+			break;
+		default:		
+			
+			if ( intCount < c_int_register_params ) {
+				dp->val.i32 = *intRegParams;
+				++intRegParams;
+				++intCount;
+			}
+			else {
+				dp->val.i32 = *data;
+				++data;
+			}
+			break;
+        }
+	}
+
+	result = self->CallMethod((PRUint16)methodIndex, info, dispatchParams);
+
+	NS_RELEASE(iface_info);
+
+	if(dispatchParams != paramBuffer)
+		delete [] dispatchParams;
+
+	return result;
+}
+
+#ifdef __GNUC__			
+
+
+
+
+
+#define STUB_ENTRY(n)
+
+#define SENTINEL_ENTRY(n) \
+nsresult nsXPTCStubBase::Sentinel##n() \
+{ \
+	NS_ASSERTION(0,"nsXPTCStubBase::Sentinel called"); \
+	return NS_ERROR_NOT_IMPLEMENTED; \
+}
+
+#else
+#error "can't find a compiler to use"
+#endif 
+
+#include "xptcstubsdef.inc"
