488a38b20e5b778ff053a613d7015a621113f1bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:05:07 +0000

    Bug 384370 ? use JSON as the on disk, lossless format for our bookmark backup - TESTING (r=mconnor)
    UltraBlame original commit: ce87f83a217e4db30224c363026b5009539ecd42

--- browser/components/places/content/sidebarUtils.js
+++ browser/components/places/content/sidebarUtils.js
@@ -67,13 +67,13 @@ var SidebarUtils = {
       
       
       tbo.view.selection.select(row.value);
-      PlacesUtils.openNodeWithEvent(aTree.selectedNode, aEvent);
+      PlacesUIUtils.openNodeWithEvent(aTree.selectedNode, aEvent);
     }
   },
 
   handleTreeKeyPress: function SU_handleTreeKeyPress(aEvent) {
     if (aEvent.keyCode == KeyEvent.DOM_VK_RETURN)
-      PlacesUtils.openNodeWithEvent(aEvent.target.selectedNode, aEvent);
+      PlacesUIUtils.openNodeWithEvent(aEvent.target.selectedNode, aEvent);
   },
 
   
