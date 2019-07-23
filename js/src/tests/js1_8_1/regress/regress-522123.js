dc121b443da5b5f18189946c718aa91aebe12b5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:49:24 +0000

    Bug 522123: Test case for indirect eval scope chain bug. r=mrbkap
    UltraBlame original commit: 8499ce94e7a1369d3c500ea033585aff2f5731af

---
+++ js/src/tests/js1_8_1/regress/regress-522123.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-522123.js';
+
+var BUGNUMBER = 522123;
+var summary = 'Indirect eval confuses scope chain';
+var actual = '';
+var expect = '';
+
+
+
+var x=1;
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  expect = 1;
+
+  evil=eval;
+  let (x=2) {
+    actual = evil("x");
+  };
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
