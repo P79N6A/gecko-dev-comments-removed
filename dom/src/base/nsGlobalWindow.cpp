13435108a1fa01d9fb572d057ff1d2fd24f04bc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:06:36 +0000

    Backed out changeset ba895ab8cbe7 to fix orange
    UltraBlame original commit: 0200b956a23ae4cee42746d326c634b28d60e7dc

--- dom/src/base/nsGlobalWindow.cpp
+++ dom/src/base/nsGlobalWindow.cpp
@@ -1002,6 +1002,13 @@ NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN(nsGlobalWindow)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE_NSCOMPTR(mDoc)
 
   
+  {
+    if (tmp->mDoc) {
+      cb.NoteXPCOMChild(tmp->mDoc->GetReference(tmp));
+    }
+  }
+
+  
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE_NSCOMPTR(mChromeEventHandler)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE_NSCOMPTR(mDocument)
 
@@ -1032,6 +1039,12 @@ NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(nsGlobalWindow)
   NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mSessionStorage)
   NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mApplicationCache)
   NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mDocumentPrincipal)
+
+  
+  if (tmp->mDoc) {
+    tmp->mDoc->RemoveReference(tmp);
+    NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mDoc)
+  }
 
   
   NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mChromeEventHandler)
