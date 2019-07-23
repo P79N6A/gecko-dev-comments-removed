452372b9f865725ed18baf2366426aaf888a8726
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:35:57 +0000

    Removed assert that compares nativeFrameSlots to nativeFrameOffset since nativeFrameOffset is undefined in case sp is at sp+depth, so the assert sometimes randomly bites. Also fixed typo in cmp(). trace-test.js compiles now.
    UltraBlame original commit: f48df61f8c432f3e75308cd92f7a5ba1444c6d2f

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -229,7 +229,7 @@ public:
                     return insImm(1);
                 }
             }
-        } else if (LIR_feq <= v && v <= LIR_fge) {
+        } else if (v >= LIR_feq && v <= LIR_fge) {
             if (isPromoteInt(s0) && isPromoteInt(s1)) {
                 
                 v = LOpcode(v + (LIR_eq - LIR_feq));
@@ -529,10 +529,11 @@ TraceRecorder::isGlobal(jsval* p) const
     JSObject* varobj = global->varobj;
 
     
-    if (size_t(p - varobj->fslots) < JS_INITIAL_NSLOTS)
+    if (p >= varobj->fslots && p < varobj->fslots + JS_INITIAL_NSLOTS)
         return true;
     return varobj->dslots &&
-           size_t(p - varobj->dslots) < size_t(varobj->dslots[-1] - JS_INITIAL_NSLOTS);
+           p >= varobj->dslots &&
+           p < varobj->dslots + STOBJ_NSLOTS(varobj) - JS_INITIAL_NSLOTS;
 }
 
 
@@ -545,10 +546,8 @@ TraceRecorder::nativeFrameSlots(JSStackFrame* fp, JSFrameRegs& regs) const
         slots += 1 + (regs.sp - fp->spbase);
         if (fp->down)
             slots += fp->argc + fp->nvars;
-        if (fp == entryFrame) {
-            JS_ASSERT(nativeFrameOffset(&regs.sp[0])/sizeof(double) == slots);
+        if (fp == entryFrame) 
             return slots;
-        }
         fp = fp->down;
     }
     JS_NOT_REACHED("nativeFrameSlots");
@@ -567,8 +566,9 @@ TraceRecorder::nativeFrameOffset(jsval* p) const
     );
     
 
-    JS_ASSERT(size_t(p - currentFrame->regs->sp) < currentFrame->script->depth);
-    offset += size_t(p - currentFrame->regs->sp) * sizeof(double);
+    JS_ASSERT((p >= currentFrame->regs->sp) && (p < currentFrame->spbase +
+            currentFrame->script->depth));
+    offset += ((jsval*)p - currentFrame->regs->sp) * sizeof(double);
     return offset;
 }
 
@@ -1220,7 +1220,7 @@ TraceRecorder::cmp(LOpcode op, bool negate)
             cond = asNumber(l) >= asNumber(r);
             break;
           default:
-            JS_ASSERT(cond == LIR_feq);
+            JS_ASSERT(op == LIR_feq);
             cond = asNumber(l) == asNumber(r);
             break;
         }
@@ -1998,7 +1998,7 @@ bool TraceRecorder::JSOP_SETNAME()
 {
     jsval& l = stackval(-1);
     jsval& r = stackval(-2);
-
+    
     if (JSVAL_IS_PRIMITIVE(l))
         return false;
 
@@ -2017,7 +2017,7 @@ bool TraceRecorder::JSOP_SETNAME()
 
     if (!test_property_cache(obj, obj_ins, obj2, entry))
         return false;
-
+    
     
 
 
@@ -2027,12 +2027,12 @@ bool TraceRecorder::JSOP_SETNAME()
 
     if (!PCVAL_IS_SLOT(entry->vword))
         return false;
-
+    
     jsval slotval = cx->fp->vars[PCVAL_TO_SLOT(entry->vword)];
     if (JSVAL_IS_NULL(slotval))
         return false;
     uint32 slot = JSVAL_TO_INT(slotval);
-
+        
     LIns* dslots_ins;
     stobj_set_slot(obj_ins, slot, dslots_ins, get(&r));
     return true;
