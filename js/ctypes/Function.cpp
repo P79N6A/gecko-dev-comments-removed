26bf9499004466f3f6e2a77a39454b182c3b8520
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:43:18 +0000

    Fix jsctypes unsigned __int64 conversion on MSVC. b=519235, r=jorendorff
    UltraBlame original commit: 1d1373e850f04f232e297f877097fc4fe4581a0f

--- js/ctypes/Function.cpp
+++ js/ctypes/Function.cpp
@@ -52,6 +52,26 @@ namespace ctypes {
 
 
 template<class IntegerType>
+static IntegerType
+Convert(jsdouble d)
+{
+  return IntegerType(d);
+}
+
+#ifdef _MSC_VER
+
+
+template<>
+static PRUint64
+Convert<PRUint64>(jsdouble d)
+{
+  return d > 0x7fffffffffffffffui64 ?
+         PRUint64(d - 0x8000000000000000ui64) + 0x8000000000000000ui64 :
+         PRUint64(d);
+}
+#endif
+
+template<class IntegerType>
 static bool
 jsvalToIntStrict(jsval aValue, IntegerType *aResult)
 {
@@ -65,7 +85,7 @@ jsvalToIntStrict(jsval aValue, IntegerType *aResult)
   }
   if (JSVAL_IS_DOUBLE(aValue)) {
     jsdouble d = *JSVAL_TO_DOUBLE(aValue);
-    *aResult = IntegerType(d);
+    *aResult = Convert<IntegerType>(d);
 
     
     
