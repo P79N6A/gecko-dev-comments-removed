015c2ce4ef275bd032a6e6c858620919c9ce3fe2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:17:05 +0000

    Bug 463819: Blocklisted add-ons with a compatibility update say they are incompatible rather than blocklisted. r=rstrong, a=blocking1.9.1
    UltraBlame original commit: 070e364189c8a622170920c3cc95ceeb8442d4ea

---
+++ toolkit/mozapps/extensions/test/unit/test_bug463819.js
@@ -0,0 +1,262 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const INSTALLERROR_SUCCESS               = 0;
+const INSTALLERROR_INCOMPATIBLE_VERSION  = -3;
+const INSTALLERROR_BLOCKLISTED           = -6;
+const INSTALLERROR_SOFTBLOCKED           = -10;
+
+
+gPrefs.setBoolPref("extensions.checkUpdateSecurity", false);
+
+
+do_import_script("netwerk/test/httpserver/httpd.js");
+var testserver;
+
+
+var promptService = {
+  alert: function(aParent, aDialogTitle, aText) {
+  },
+  
+  alertCheck: function(aParent, aDialogTitle, aText, aCheckMsg, aCheckState) {
+  },
+  
+  confirm: function(aParent, aDialogTitle, aText) {
+  },
+  
+  confirmCheck: function(aParent, aDialogTitle, aText, aCheckMsg, aCheckState) {
+  },
+  
+  confirmEx: function(aParent, aDialogTitle, aText, aButtonFlags, aButton0Title, aButton1Title, aButton2Title, aCheckMsg, aCheckState) {
+  },
+  
+  prompt: function(aParent, aDialogTitle, aText, aValue, aCheckMsg, aCheckState) {
+  },
+  
+  promptUsernameAndPassword: function(aParent, aDialogTitle, aText, aUsername, aPassword, aCheckMsg, aCheckState) {
+  },
+
+  promptPassword: function(aParent, aDialogTitle, aText, aPassword, aCheckMsg, aCheckState) {
+  },
+  
+  select: function(aParent, aDialogTitle, aText, aCount, aSelectList, aOutSelection) {
+  },
+  
+  QueryInterface: function(iid) {
+    if (iid.equals(Components.interfaces.nsIPromptService)
+     || iid.equals(Components.interfaces.nsISupports))
+      return this;
+  
+    throw Components.results.NS_ERROR_NO_INTERFACE;
+  }
+};
+
+
+
+var WindowWatcher = {
+  openWindow: function(parent, url, name, features, arguments) {
+  },
+
+  QueryInterface: function(iid) {
+    if (iid.equals(Ci.nsIWindowWatcher)
+     || iid.equals(Ci.nsISupports))
+      return this;
+
+    throw Cr.NS_ERROR_NO_INTERFACE;
+  }
+}
+
+var WindowWatcherFactory = {
+  createInstance: function createInstance(outer, iid) {
+    if (outer != null)
+      throw Components.results.NS_ERROR_NO_AGGREGATION;
+    return WindowWatcher.QueryInterface(iid);
+  }
+};
+
+var PromptServiceFactory = {
+  createInstance: function (outer, iid) {
+    if (outer != null)
+      throw Components.results.NS_ERROR_NO_AGGREGATION;
+    return promptService.QueryInterface(iid);
+  }
+};
+var registrar = Components.manager.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+registrar.registerFactory(Components.ID("{6cc9c9fe-bc0b-432b-a410-253ef8bcc699}"),
+                          "PromptService",
+                          "@mozilla.org/embedcomp/prompt-service;1", PromptServiceFactory);
+registrar.registerFactory(Components.ID("{1dfeb90a-2193-45d5-9cb8-864928b2af55}"),
+                          "Fake Window Watcher",
+                          "@mozilla.org/embedcomp/window-watcher;1", WindowWatcherFactory);
+
+var gNextTest, gLastStatus;
+
+
+var installListener = {
+  onDownloadStarted: function(aAddon) {
+  },
+
+  onDownloadEnded: function(aAddon) {
+  },
+
+  onInstallStarted: function(aAddon) {
+  },
+
+  onCompatibilityCheckStarted: function(aAddon) {
+  },
+
+  onCompatibilityCheckEnded: function(aAddon, aStatus) {
+  },
+
+  onInstallEnded: function(aAddon, aStatus) {
+    gLastStatus = aStatus;
+  },
+
+  onInstallsCompleted: function() {
+    gNextTest();
+  },
+
+  onDownloadProgress: function onProgress(aAddon, aValue, aMaxValue) {
+  }
+}
+
+function run_test() {
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "2", "1.9");
+
+  
+  var blocklist = do_get_file("toolkit/mozapps/extensions/test/unit/data/test_bug463819.xml");
+  blocklist.copyTo(gProfD, "blocklist.xml");
+
+  
+  testserver = new nsHttpServer();
+  testserver.registerDirectory("/", do_get_file("toolkit/mozapps/extensions/test/unit/data"));
+  testserver.start(4444);
+
+  startupEM();
+  gEM.addInstallListener(installListener);
+
+  
+  dump("Installing add-on 1\n");
+  gNextTest = test_addon_1;
+  gEM.installItemFromFile(do_get_addon("test_bug463819_1"), NS_INSTALL_LOCATION_APPPROFILE);
+  dump("Installing add-on 4\n");
+  gNextTest = test_addon_4;
+  gEM.installItemFromFile(do_get_addon("test_bug463819_4"), NS_INSTALL_LOCATION_APPPROFILE);
+  dump("Installing add-on 7\n");
+  gNextTest = test_addon_7;
+  gEM.installItemFromFile(do_get_addon("test_bug463819_7"), NS_INSTALL_LOCATION_APPPROFILE);
+
+  do_test_pending();
+
+  
+  dump("Installing add-on 2\n");
+  gNextTest = test_addon_2;
+  gEM.installItemFromFile(do_get_addon("test_bug463819_2"), NS_INSTALL_LOCATION_APPPROFILE);
+}
+
+
+function test_addon_1() {
+  do_check_eq(gLastStatus, INSTALLERROR_SUCCESS);
+}
+
+
+function test_addon_4() {
+  do_check_eq(gLastStatus, INSTALLERROR_SOFTBLOCKED);
+}
+
+
+function test_addon_7() {
+  do_check_eq(gLastStatus, INSTALLERROR_BLOCKLISTED);
+}
+
+
+function test_addon_2() {
+  do_check_eq(gLastStatus, INSTALLERROR_SUCCESS);
+
+  dump("Installing add-on 3\n");
+  gNextTest = test_addon_3;
+  gEM.installItemFromFile(do_get_addon("test_bug463819_3"), NS_INSTALL_LOCATION_APPPROFILE);
+}
+
+
+function test_addon_3() {
+  do_check_eq(gLastStatus, INSTALLERROR_INCOMPATIBLE_VERSION);
+
+  dump("Installing add-on 5\n");
+  gNextTest = test_addon_5;
+  gEM.installItemFromFile(do_get_addon("test_bug463819_5"), NS_INSTALL_LOCATION_APPPROFILE);
+}
+
+
+function test_addon_5() {
+  do_check_eq(gLastStatus, INSTALLERROR_SOFTBLOCKED);
+
+  dump("Installing add-on 6\n");
+  gNextTest = test_addon_6;
+  gEM.installItemFromFile(do_get_addon("test_bug463819_6"), NS_INSTALL_LOCATION_APPPROFILE);
+}
+
+
+function test_addon_6() {
+  do_check_eq(gLastStatus, INSTALLERROR_INCOMPATIBLE_VERSION);
+
+  dump("Installing add-on 8\n");
+  gNextTest = test_addon_8;
+  gEM.installItemFromFile(do_get_addon("test_bug463819_8"), NS_INSTALL_LOCATION_APPPROFILE);
+}
+
+
+function test_addon_8() {
+  do_check_eq(gLastStatus, INSTALLERROR_BLOCKLISTED);
+
+  dump("Installing add-on 9\n");
+  gNextTest = test_addon_9;
+  gEM.installItemFromFile(do_get_addon("test_bug463819_9"), NS_INSTALL_LOCATION_APPPROFILE);
+}
+
+
+function test_addon_9() {
+  do_check_eq(gLastStatus, INSTALLERROR_INCOMPATIBLE_VERSION);
+
+  finish_test();
+}
+
+function finish_test() {
+  testserver.stop();
+  do_test_finished();
+}
