bb238d40565e9ec5419f62d5a603a365ce7cf5cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:30:24 +0000

    Add implementation of nsWindow object
    UltraBlame original commit: 3aa9e0175031056251068b8e9b20976ff1fcece9

--- widget/src/qt/nsWidgetFactory.cpp
+++ widget/src/qt/nsWidgetFactory.cpp
@@ -45,9 +45,8 @@
 #include "nsCOMPtr.h"
 #include "nsWidgetsCID.h"
 #include "nsAppShell.h"
-
-
-
+#include "nsWindow.h"
+#include "nsToolkit.h"
 
 
 
@@ -75,10 +74,10 @@
 
 
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsAppShell)
-
-
-
-
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsWindow)
+NS_GENERIC_FACTORY_CONSTRUCTOR(ChildWindow)
+NS_GENERIC_FACTORY_CONSTRUCTOR(PopupWindow)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsToolkit)
 
 
 
@@ -101,7 +100,15 @@ static const nsModuleComponentInfo components[] =
     { "Qt AppShell",
       NS_APPSHELL_CID,
       "@mozilla.org/widget/appshell/qt;1",
-      nsAppShellConstructor }
+      nsAppShellConstructor },
+    { "Qt Child nsWindow",
+      NS_CHILD_CID,
+      "@mozilla.org/widgets/child_window/qt;1",
+      ChildWindowConstructor },
+    { "Qt Popup nsWindow",
+      NS_POPUP_CID,
+      "@mozilla.org/widgets/popup_window/qt;1",
+      PopupWindowConstructor },
 
 
 
@@ -152,18 +159,10 @@ static const nsModuleComponentInfo components[] =
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+    { "Qt nsWindow",
+      NS_WINDOW_CID,
+      "@mozilla.org/widgets/window/qt;1",
+      nsWindowConstructor }
 };
 
 NS_IMPL_NSGETMODULE(nsWidgetQtModule,components)
