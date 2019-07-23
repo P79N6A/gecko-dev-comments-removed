4c75363a48989d0ebef1d19cbaaaf15a7e4d7f50
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:27:32 +0000

    Bug 523936 - temporarily disable test_browserGlue_distribution.js (r=sdwilsh)
    UltraBlame original commit: d287e7a7b4496d67a123d9b9e2d9ea6fa658ff9b

--- browser/components/places/tests/unit/test_browserGlue_distribution.js
+++ browser/components/places/tests/unit/test_browserGlue_distribution.js
@@ -50,6 +50,9 @@ const TOPIC_FINAL_UI_STARTUP = "final-ui-startup";
 const TOPIC_CUSTOMIZATION_COMPLETE = "distribution-customization-complete";
 
 function run_test() {
+  
+  return;
+
   do_test_pending();
 
   
@@ -142,6 +145,8 @@ function continue_test() {
 }
 
 do_register_cleanup(function() {
+  
+  return;
   
   
   let iniFile = Services.dirsvc.get("XCurProcD", Ci.nsIFile);
