85eefa89ffac98df1abd91c7aa7b57b2603b3bc3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:48:31 +0000

    OS/2 build bustage as a result of 32649 - once files are moved, this should build
    UltraBlame original commit: 1936a5d6aaf369d3abe5e7c23912b0f4cce3c01b

---
+++ widget/src/os2/nsPrintOS2.h
@@ -0,0 +1,99 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsPrintOS2_h___
+#define nsPrintOS2_h___
+
+#define INCL_PM
+#define INCL_DOS
+#define INCL_DOSERRORS
+#define INCL_SPLDOSPRINT
+#define INCL_DEV
+#define INCL_DEVDJP
+#define INCL_GRE_DEVICE
+#include <os2.h>
+#include <pmddim.h>
+
+
+
+
+
+BOOL PrnInitialize(HMODULE hmodResources);
+BOOL PrnTerminate(void);
+
+
+class PRTQUEUE;
+
+#define MAX_PRINT_QUEUES  (128)
+
+class PRINTDLG
+{
+public:
+   PRINTDLG();
+  ~PRINTDLG();
+   void      RefreshPrintQueue();
+   ULONG     GetNumPrinters();
+   void      GetPrinter(ULONG printerNdx, char** printerName);
+   PRTQUEUE* SetPrinterQueue(ULONG printerNdx);
+   LONG      GetPrintDriverSize(ULONG printerNdx);
+   PDRIVDATA GetPrintDriver(ULONG printerNdx);
+   HDC       GetDCHandle(ULONG printerNdx);
+   char*     GetDriverType(ULONG printerNdx);
+   BOOL      ShowProperties(ULONG printerNdx);
+
+private:
+  ULONG      mQueueCount;
+  PRTQUEUE*  mPQBuf[MAX_PRINT_QUEUES];
+};
+
+
+
+BOOL PrnClosePrinter( PRTQUEUE *pPrintQueue);
+
+
+HDC PrnOpenDC( PRTQUEUE *pPrintQueue, PSZ pszApplicationName, int copies, int destination, char *file);
+
+
+BOOL PrnQueryHardcopyCaps( HDC hdc, PHCINFO pHCInfo);
+
+
+BOOL PrnAbortJob( HDC hdc);
+
+
+
+#endif
