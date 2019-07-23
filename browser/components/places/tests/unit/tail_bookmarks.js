a2d6d9cc726351d23b1b46860e49588af7823244
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:27:29 +0000

    Bug 471788 - 4 TUnit tests leak now, r=dietrich
    UltraBlame original commit: b98a20b1e91e84df7637ea4ddfc83089aed67a01

--- browser/components/places/tests/unit/tail_bookmarks.js
+++ browser/components/places/tests/unit/tail_bookmarks.js
@@ -36,3 +36,10 @@
 
 
 
+
+
+
+var os = Cc['@mozilla.org/observer-service;1'].
+         getService(Ci.nsIObserverService);
+os.notifyObservers(null, "quit-application-granted", null);
+os.notifyObservers(null, "quit-application", null);
