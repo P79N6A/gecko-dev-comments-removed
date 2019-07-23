83b4d0183d26513bed77955e6c029d7de2ac6266
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:45:28 +0000

    Add new skin work (thanks to Zach Linder), fix tab bar, and add "test n of n" to litmus tab.
    UltraBlame original commit: f42b2de2e1189a89d288a3ab27b1f1b57886180a

--- testing/extensions/community/chrome/content/litmus.js
+++ testing/extensions/community/chrome/content/litmus.js
@@ -109,7 +109,6 @@
         if (litmus.lastTestRunSummary == "") return;
         
         
-        
         $("qa-testrun-label").value = litmus.lastTestRunSummary;
         $("qa-testgroup-label").value = litmus.lastTestGroupSummary;
         litmus.lastSubgroupObject = litmus.preDialogueSubgroupObject;
@@ -205,7 +204,12 @@
     selectCurrentTestCase : function() {
         
         
-        litmus.getTestcase($("testlist").selectedItem.value, litmus.populateTestcase);
+        litmus.getTestcase($("testlist").selectedItem.value, function(testcase) {
+        	litmus.populateTestcase(testcase);
+        	$('qa-testcase-progress').value = 
+        		qaMain.bundle.getFormattedString('qa.extension.litmus.progress',
+        			[$("testlist").selectedIndex+1, $("testlist").getRowCount()]);
+        });
     },
 	populatePreviewBox : function(testcases) {
         
@@ -224,7 +228,7 @@
             checkbox.setAttribute("type", "checkbox");
             checkbox.setAttribute("disabled", "true");
             var name = document.createElement("listcell");
-            name.setAttribute("label", "#" + testcases[i].testcase_id + " -- " + testcases[i].summary);
+            name.setAttribute("label", (i+1) + " -- " + testcases[i].summary);
             name.setAttribute("crop", "end");
             name.setAttribute("maxwidth", "175");
             row.appendChild(checkbox);
@@ -252,12 +256,14 @@
 	populateFields : function(subgroup) {
         litmus.lastSubgroupObject = subgroup;
 		litmus.populatePreviewBox(subgroup.testcases);
+		$('qa-subgroup-label').value = subgroup.name;
         $("testlist").selectedIndex = 0;
         litmus.selectCurrentTestCase();
 	},
     statePopulateFields : function(subgroup) {  
         litmus.lastSubgroupObject = subgroup;
         litmus.populatePreviewBox(subgroup.testcases);
+        $('qa-subgroup-label').value = subgroup.name;
         
         $("testlist").selectedIndex = qaPref.getPref(qaPref.prefBase + ".currentTestcase.testcaseIndex", "int");
         litmus.selectCurrentTestCase();
