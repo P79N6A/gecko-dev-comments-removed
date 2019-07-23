a43eb87eae99d878418b8b8025b8ca1670cdbfe8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:25:00 +0000

    Bug 502211. Create test_playback and test_playback_errors for easy regression testing. r=doublec
    UltraBlame original commit: fee9fe62baee9061e796f7ca1fb7443d2955ea9e

--- content/media/test/manifest.js
+++ content/media/test/manifest.js
@@ -1,6 +1,51 @@
 
+
+
+
+
 var gSmallTests = [
   { name:"r11025_s16_c1.wav", type:"audio/x-wav", duration:1.0 },
   { name:"320x240.ogv", type:"video/ogg", width:320, height:240 },
   { name:"bogus.duh", type:"bogus/duh" }
 ];
+
+
+
+
+
+var gPlayTests = [
+  
+  { name:"r11025_u8_c1_trunc.wav", type:"audio/x-wav", duration:1.8 },
+  
+  { name:"r11025_s16_c1_trailing.wav", type:"audio/x-wav", duration:1.0 },
+  
+  { name:"r16000_u8_c1_list.wav", type:"audio/x-wav", duration:4.2 },
+  
+  { name:"bug461281.ogg", type:"application/ogg" },
+  
+  { name:"bug482461.ogv", type:"video/ogg", duration:4.24 },
+  { name:"bogus.duh", type:"bogus/duh" }
+];
+
+
+
+
+
+var gErrorTests = [
+  { name:"bogus.wav", type:"audio/x-wav" },
+  { name:"bogus.ogv", type:"video/ogg" },
+  { name:"bogus.duh", type:"bogus/duh" }
+];
+
+function checkMetadata(msg, e, test) {
+  if (test.width) {
+    is(e.videoWidth, test.width, msg + " video width");
+  }
+  if (test.height) {
+    is(e.videoHeight, test.height, msg + " video height");
+  }
+  if (test.duration) {
+    ok(Math.abs(e.duration - test.duration) < 0.1,
+       msg + " duration should be around " + test.duration);
+  }
+}
