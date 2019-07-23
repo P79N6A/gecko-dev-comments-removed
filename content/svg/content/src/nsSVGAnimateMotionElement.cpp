71ee16b32de849c5af1f287ad094482c94eb9f1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:30:38 +0000

    Bug 436418, patch B: SVG/SMIL animateMotion - update license text, and fix compilation issues. r=roc
    UltraBlame original commit: 48cd33adb97a9b0a6b451110b242d5d2e1ccb39e

--- content/svg/content/src/nsSVGAnimateMotionElement.cpp
+++ content/svg/content/src/nsSVGAnimateMotionElement.cpp
@@ -37,6 +37,7 @@
 
 
 
+
 #include "nsSVGAnimationElement.h"
 #include "nsIDOMSVGAnimateMotionElement.h"
 #include "nsSVGEnum.h"
@@ -85,12 +86,14 @@ NS_IMPL_NS_NEW_SVG_ELEMENT(AnimateMotion)
 NS_IMPL_ADDREF_INHERITED(nsSVGAnimateMotionElement,nsSVGAnimateMotionElementBase)
 NS_IMPL_RELEASE_INHERITED(nsSVGAnimateMotionElement,nsSVGAnimateMotionElementBase)
 
+DOMCI_DATA(SVGAnimateMotionElement, nsSVGAnimateMotionElement)
+
 NS_INTERFACE_TABLE_HEAD(nsSVGAnimateMotionElement)
   NS_NODE_INTERFACE_TABLE5(nsSVGAnimateMotionElement, nsIDOMNode,
                            nsIDOMElement, nsIDOMSVGElement,
                            nsIDOMSVGAnimationElement,
                            nsIDOMSVGAnimateMotionElement)
-  NS_INTERFACE_MAP_ENTRY_CONTENT_CLASSINFO(SVGAnimateElement)
+  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(SVGAnimateMotionElement)
 NS_INTERFACE_MAP_END_INHERITING(nsSVGAnimateMotionElementBase)
 
 
