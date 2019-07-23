6ef6cd7deb3823e4db1f37c1fef5464728a45e29
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:12:26 +0000

    Bug 422978: disable pipelining for SSL again, since it broke some internet banking websites, patch by Honza Bambas <honzab@allpeers.com>, r=biesi, a=beltzner
    UltraBlame original commit: db0163a56b905f03def05bd3bb43a03d052a0ba9

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -610,7 +610,7 @@ pref("network.http.redirection-limit", 20);
 pref("network.http.accept-encoding" ,"gzip,deflate");
 
 pref("network.http.pipelining"      , false);
-pref("network.http.pipelining.ssl"  , true); 
+pref("network.http.pipelining.ssl"  , false); 
 pref("network.http.proxy.pipelining", false);
 
 
