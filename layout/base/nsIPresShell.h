37a8ae9768b8a1191d00e386985f20211e40dda8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:52:25 +0000

    Bug 401361 - Draw correctly when drawWindow is called on a window using fullZoom [p=roc r+sr=vlad a=blocking1.9+]
    UltraBlame original commit: fd373e6a5ca57fb22af3dc3032f07b0a056d5b35

--- layout/base/nsIPresShell.h
+++ layout/base/nsIPresShell.h
@@ -704,6 +704,8 @@ public:
 
 
 
+
+
   NS_IMETHOD RenderDocument(const nsRect& aRect, PRBool aUntrusted,
                             PRBool aIgnoreViewportScrolling,
                             nscolor aBackgroundColor,
