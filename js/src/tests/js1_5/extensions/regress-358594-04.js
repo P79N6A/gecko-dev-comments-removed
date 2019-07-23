60b8c704ca9085b547b3ba1c6601df857118dea6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:27:54 +0000

    Bug 517580 - Unconditionally build with support for getters and setters, remove support for the deprecated { x getter: function() {}} and x getter= function() {} getter syntaxes (mutatis mutandis for setter) now that ES5 provides Object.getOwnPropertyDescriptor and Object.defineProperty and { get x() { } } syntax, and provide __{define,lookup}{G,S}etter__ only if a preprocessor variable is defined (unconditionally provided for the moment).  r=luke
    UltraBlame original commit: e47d2506e0ad6e314fe8014d33f784c339cdcf99

--- js/src/tests/js1_5/extensions/regress-358594-04.js
+++ js/src/tests/js1_5/extensions/regress-358594-04.js
@@ -49,6 +49,6 @@ printStatus (summary);
 
 f = function () { };
 f.__proto__ = this; 
-this.m setter = f; 
+Object.defineProperty(this, "m", { set: f, enumerable: true, configurable: true });
 uneval(this);
 reportCompare(expect, actual, summary);
