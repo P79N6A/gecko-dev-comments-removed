3e91368a7f4e9b454d787c2089c2a1d8d7b2d59e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:06:31 +0000

    Bug 487539.  Don't use an <area>'s primary frame as a primary frame hint.  r+sr=roc
    UltraBlame original commit: 683b00c3d1d1394ec6bb978a8baaf53dec3ce7d2

--- layout/base/nsFrameManager.cpp
+++ layout/base/nsFrameManager.cpp
@@ -386,7 +386,10 @@ nsFrameManager::GetPrimaryFrameFor(nsIContent* aContent,
           entry = static_cast<PrimaryFrameMapEntry*>
                              (PL_DHashTableOperate(&mPrimaryFrameMap, prevSibling,
                                                PL_DHASH_LOOKUP));
-          if (PL_DHASH_ENTRY_IS_BUSY(entry))
+          
+          
+          if (PL_DHASH_ENTRY_IS_BUSY(entry) && entry->frame &&
+              entry->frame->GetContent() == prevSibling)
             hint.mPrimaryFrameForPrevSibling = entry->frame;
         }
       }
