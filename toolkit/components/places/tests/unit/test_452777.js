78229546f1db1aef6268919a7dae8732587b6011
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:46:30 +0000

    Bug 452777 - regression from lastInsertRowID: Undo folder deletion mix up views r=Mak77
    UltraBlame original commit: b3201f4a8641fcf49845907af9aa83bf628f31f9

---
+++ toolkit/components/places/tests/unit/test_452777.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bs = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
+         getService(Ci.nsINavBookmarksService);
+
+function run_test()
+{
+  const TITLE = "test folder";
+
+  
+  
+  
+  let id = bs.createFolder(bs.placesRoot, TITLE, -1);
+  bs.createFolder(bs.placesRoot, "test folder 2", -1);
+  let transaction = bs.getRemoveFolderTransaction(id);
+  transaction.doTransaction();
+
+  
+  bs.addObserver({
+    onItemAdded: function(aItemId, aFolder, aIndex)
+    {
+      do_check_eq(aItemId, id);
+      do_check_eq(bs.getItemTitle(aItemId), TITLE);
+    }
+  }, false);
+  transaction.undoTransaction();
+}
