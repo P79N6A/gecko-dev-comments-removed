d2f9d231e9543bd11cf2833ab80c145d691be56a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:26:41 +0000

    Bug 443877 - "Need a way to point timers at a different event target". r=bsmedberg.
    UltraBlame original commit: c7ca7c9ca7ba46de89f35cd81bd25c6dc0370aa2

---
+++ xpcom/tests/TestTimers.cpp
@@ -0,0 +1,182 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "TestHarness.h"
+
+#include "nsIThread.h"
+#include "nsITimer.h"
+
+#include "nsAutoLock.h"
+#include "nsCOMPtr.h"
+#include "nsComponentManagerUtils.h"
+#include "nsServiceManagerUtils.h"
+#include "nsThreadUtils.h"
+#include "prinrval.h"
+#include "prmon.h"
+
+typedef nsresult(*TestFuncPtr)();
+
+class AutoTestThread
+{
+public:
+  AutoTestThread() {
+    nsCOMPtr<nsIThread> newThread;
+    nsresult rv = NS_NewThread(getter_AddRefs(newThread));
+    NS_ENSURE_SUCCESS(rv,);
+
+    newThread.swap(mThread);
+  }
+
+  ~AutoTestThread() {
+    mThread->Shutdown();
+  }
+
+  operator nsDerivedSafe<nsIThread>*() const {
+    return mThread;
+  }
+
+  nsDerivedSafe<nsIThread>* operator->() const {
+    return mThread;
+  }
+
+private:
+  nsCOMPtr<nsIThread> mThread;
+};
+
+class AutoCreateAndDestroyMonitor
+{
+public:
+  AutoCreateAndDestroyMonitor() {
+    mMonitor = nsAutoMonitor::NewMonitor("TestTimers::AutoMon");
+    NS_ASSERTION(mMonitor, "Out of memory!");
+  }
+
+  ~AutoCreateAndDestroyMonitor() {
+    if (mMonitor) {
+      nsAutoMonitor::DestroyMonitor(mMonitor);
+    }
+  }
+
+  operator PRMonitor*() {
+    return mMonitor;
+  }
+
+private:
+  PRMonitor* mMonitor;
+};
+
+class TimerCallback : public nsITimerCallback
+{
+public:
+  NS_DECL_ISUPPORTS
+
+  TimerCallback(nsIThread** aThreadPtr, PRMonitor* aMonitor)
+  : mThreadPtr(aThreadPtr), mMonitor(aMonitor) { }
+
+  NS_IMETHOD Notify(nsITimer* aTimer) {
+    nsCOMPtr<nsIThread> current(do_GetCurrentThread());
+
+    nsAutoMonitor mon(mMonitor);
+
+    NS_ASSERTION(!*mThreadPtr, "Timer called back more than once!");
+    *mThreadPtr = current;
+
+    mon.Notify();
+
+    return NS_OK;
+  }
+private:
+  nsIThread** mThreadPtr;
+  PRMonitor* mMonitor;
+};
+
+NS_IMPL_THREADSAFE_ISUPPORTS1(TimerCallback, nsITimerCallback)
+
+nsresult
+TestTargetedTimers()
+{
+  AutoCreateAndDestroyMonitor newMon;
+  NS_ENSURE_TRUE(newMon, NS_ERROR_OUT_OF_MEMORY);
+
+  AutoTestThread testThread;
+  NS_ENSURE_TRUE(testThread, NS_ERROR_OUT_OF_MEMORY);
+
+  nsresult rv;
+  nsCOMPtr<nsITimer> timer = do_CreateInstance(NS_TIMER_CONTRACTID, &rv);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsIEventTarget* target = static_cast<nsIEventTarget*>(testThread);
+
+  rv = timer->SetTarget(target);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsIThread* notifiedThread = nsnull;
+
+  nsCOMPtr<nsITimerCallback> callback =
+    new TimerCallback(&notifiedThread, newMon);
+  NS_ENSURE_TRUE(callback, NS_ERROR_OUT_OF_MEMORY);
+
+  rv = timer->InitWithCallback(callback, PR_MillisecondsToInterval(2000),
+                               nsITimer::TYPE_ONE_SHOT);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsAutoMonitor mon(newMon);
+  while (!notifiedThread) {
+    mon.Wait();
+  }
+  NS_ENSURE_TRUE(notifiedThread == testThread, NS_ERROR_FAILURE);
+
+  return NS_OK;
+}
+
+int main(int argc, char** argv)
+{
+  ScopedXPCOM xpcom("TestTimers");
+  NS_ENSURE_FALSE(xpcom.failed(), 1);
+
+  static TestFuncPtr testsToRun[] = {
+    TestTargetedTimers
+  };
+  static PRUint32 testCount = sizeof(testsToRun) / sizeof(testsToRun[0]);
+
+  for (PRUint32 i = 0; i < testCount; i++) {
+    nsresult rv = testsToRun[i]();
+    NS_ENSURE_SUCCESS(rv, 1);
+  }
+
+  return 0;
+}
--- xpcom/threads/nsTimerImpl.h
+++ xpcom/threads/nsTimerImpl.h
@@ -44,7 +44,7 @@
 
 
 #include "nsITimer.h"
-#include "nsIThread.h"
+#include "nsIEventTarget.h"
 #include "nsIObserver.h"
 
 #include "nsCOMPtr.h"
@@ -121,7 +121,7 @@ private:
       NS_RELEASE(mCallback.o);
   }
 
-  nsCOMPtr<nsIThread>   mCallingThread;
+  nsCOMPtr<nsIEventTarget> mEventTarget;
 
   void *                mClosure;
 
