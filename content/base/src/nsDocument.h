a2e4a06cec3ab81bbb6b3f75eb271780f44889ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:32:39 +0000

    Part 10 of fix for bug 560273 (Stop using DOM tearoffs from quickstubs) - use nsNodeSelectorTearoff for nsDocument. r=jst.
    UltraBlame original commit: 7b8a7ae82c41f17d1bd46a4047879d29cc8fc0f1

--- content/base/src/nsDocument.h
+++ content/base/src/nsDocument.h
@@ -494,7 +494,6 @@ class nsDocument : public nsIDocument,
                    public nsIDOMNSEventTarget,
                    public nsIScriptObjectPrincipal,
                    public nsIRadioGroupContainer,
-                   public nsIDOMNodeSelector,
                    public nsIApplicationCacheContainer,
                    public nsStubMutationObserver
 {
@@ -811,9 +810,6 @@ public:
 
   
   NS_DECL_NSIDOMNSEVENTTARGET
-
-  
-  NS_DECL_NSIDOMNODESELECTOR
 
   
   NS_DECL_NSIMUTATIONOBSERVER_CONTENTAPPENDED
