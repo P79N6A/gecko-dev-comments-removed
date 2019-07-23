cfe5a4e2a669c22f36d768c192acca376922f5d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:36:32 +0000

    bug 474801 - Checking for MaybeGC conditions when allocating GC things in JS shell. r=igor
    UltraBlame original commit: 799649d4e416f2e6320d33874d130a6ac3b26a44

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -270,6 +270,7 @@ struct JSRuntime {
     uint32              gcLevel;
     uint32              gcNumber;
     JSTracer            *gcMarkingTracer;
+    uint32              gcTriggerFactor;
 
     
 
