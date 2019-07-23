4fd83b3a65d8860141617f40fb40f677281b9f71
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:53:54 +0000

    Bug 339445 - Session store should save/restore sessionStorage data (from DOMStorage); tests, r=mano
    UltraBlame original commit: 948b5a201326d047da70d87368f2aa0f725dfed6

---
+++ browser/components/sessionstore/test/browser/browser_339445.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  
+  waitForExplicitFinish();
+  
+  let testURL = "http://localhost:8888/browser/" +
+    "browser/components/sessionstore/test/browser/browser_339445_sample.html"
+  
+  let tab = gBrowser.addTab(testURL);
+  tab.linkedBrowser.addEventListener("load", function(aEvent) {
+    let doc = tab.linkedBrowser.contentDocument;
+    is(doc.getElementById("storageTestItem").textContent, "PENDING",
+       "sessionStorage value has been set");
+    
+    let tab2 = gBrowser.duplicateTab(tab);
+    tab2.linkedBrowser.addEventListener("load", function(aEvent) {
+      let doc2 = tab2.linkedBrowser.contentDocument;
+      is(doc2.getElementById("storageTestItem").textContent, "SUCCESS",
+         "sessionStorage value has been duplicated");
+      
+      
+      gBrowser.removeTab(tab2);
+      gBrowser.removeTab(tab);
+      
+      finish();
+    }, true);
+  }, true);
+}
