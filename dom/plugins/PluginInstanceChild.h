52a19834f1a33a22c76cbb83685bd3b93810f1ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:16:27 +0000

    Bug 544074 - Crashes at mozilla::plugins::PluginModuleChild::UnregisterActorForNPObject - the hash of scriptable objects is mutating while we enumerate the actors being destroyed after a plugin instance is destroyed. Save the list of actors off into a separate non-mutating table, r=bent
    UltraBlame original commit: bbe683272abf5470f99ab3a951b062680f9cd96e

--- dom/plugins/PluginInstanceChild.h
+++ dom/plugins/PluginInstanceChild.h
@@ -52,6 +52,7 @@
 #include "ChildAsyncCall.h"
 #include "ChildTimer.h"
 #include "nsRect.h"
+#include "nsTHashtable.h"
 
 namespace mozilla {
 namespace plugins {
@@ -181,6 +182,8 @@ public:
     void UnscheduleTimer(uint32_t id);
 
 private:
+    friend class PluginModuleChild;
+
     
     enum PluginQuirks {
         QUIRK_SILVERLIGHT_WINLESS_INPUT_TRANSLATION = 1, 
@@ -252,6 +255,13 @@ private:
     friend class ChildAsyncCall;
     nsTArray<ChildAsyncCall*> mPendingAsyncCalls;
     nsTArray<nsAutoPtr<ChildTimer> > mTimers;
+
+    
+
+
+
+
+    nsAutoPtr< nsTHashtable<DeletingObjectEntry> > mDeletingHash;
 
 #if defined(OS_WIN)
 private:
