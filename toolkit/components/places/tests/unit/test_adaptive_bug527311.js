b3b26586bbc5d7b72afb79ded395f546b336779c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:57:52 +0000

    Bug 527311 - Addressbar suggests adaptive results even if they have no visits and we want "history" behavior, r=sdwilsh
    UltraBlame original commit: e00cdc84b9dd05e9ccd5f70e8983da46aa2a1a1c

---
+++ toolkit/components/places/tests/unit/test_adaptive_bug527311.js
@@ -0,0 +1,161 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const TEST_URL = "http://adapt.mozilla.org/";
+const SEARCH_STRING = "adapt";
+
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+let hs = Cc["@mozilla.org/browser/nav-history-service;1"].
+         getService(Ci.nsINavHistoryService);
+let bs = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
+         getService(Ci.nsINavBookmarksService);
+let os = Cc["@mozilla.org/observer-service;1"].
+         getService(Ci.nsIObserverService);
+let ps = Cc["@mozilla.org/preferences-service;1"].
+         getService(Ci.nsIPrefBranch);
+
+const PLACES_AUTOCOMPLETE_FEEDBACK_UPDATED_TOPIC =
+  "places-autocomplete-feedback-updated";
+
+function AutoCompleteInput(aSearches) {
+  this.searches = aSearches;
+}
+AutoCompleteInput.prototype = {
+  constructor: AutoCompleteInput,
+  searches: null,
+  minResultsForPopup: 0,
+  timeout: 10,
+  searchParam: "",
+  textValue: "",
+  disableAutoComplete: false,
+  completeDefaultIndex: false,
+
+  get searchCount() {
+    return this.searches.length;
+  },
+
+  getSearchAt: function ACI_getSearchAt(aIndex) {
+    return this.searches[aIndex];
+  },
+
+  onSearchComplete: function ACI_onSearchComplete() {},
+
+  popupOpen: false,
+
+  popup: {
+    setSelectedIndex: function() {},
+    invalidate: function() {},
+
+    QueryInterface: function(iid) {
+      if (iid.equals(Ci.nsISupports) ||
+          iid.equals(Ci.nsIAutoCompletePopup))
+        return this;
+
+      throw Components.results.NS_ERROR_NO_INTERFACE;
+    }
+  },
+
+  onSearchBegin: function() {},
+
+  QueryInterface: function(iid) {
+    if (iid.equals(Ci.nsISupports) ||
+        iid.equals(Ci.nsIAutoCompleteInput))
+      return this;
+
+    throw Components.results.NS_ERROR_NO_INTERFACE;
+  }
+}
+
+
+function check_results() {
+  let controller = Cc["@mozilla.org/autocomplete/controller;1"].
+                   getService(Ci.nsIAutoCompleteController);
+  let input = new AutoCompleteInput(["history"]);
+  controller.input = input;
+
+  input.onSearchComplete = function() {
+    do_check_eq(controller.searchStatus,
+                Ci.nsIAutoCompleteController.STATUS_COMPLETE_NO_MATCH);
+    do_check_eq(controller.matchCount, 0);
+
+    remove_all_bookmarks();
+    do_test_finished();
+ };
+
+  controller.startSearch(SEARCH_STRING);
+}
+
+
+function addAdaptiveFeedback(aUrl, aSearch, aCallback) {
+  let observer = {
+    observe: function(aSubject, aTopic, aData) {
+      os.removeObserver(observer, PLACES_AUTOCOMPLETE_FEEDBACK_UPDATED_TOPIC);
+      do_timeout(0, aCallback);
+    }
+  };
+  os.addObserver(observer, PLACES_AUTOCOMPLETE_FEEDBACK_UPDATED_TOPIC, false);
+
+  let thing = {
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteInput,
+                                           Ci.nsIAutoCompletePopup,
+                                           Ci.nsIAutoCompleteController]),
+    get popup() { return thing; },
+    get controller() { return thing; },
+    popupOpen: true,
+    selectedIndex: 0,
+    getValueAt: function() aUrl,
+    searchString: aSearch
+  };
+
+  os.notifyObservers(thing, "autocomplete-will-enter-text", null);
+}
+
+
+function run_test() {
+  do_test_pending();
+
+  
+  bs.insertBookmark(bs.unfiledBookmarksFolder, uri(TEST_URL),                   
+                    bs.DEFAULT_INDEX, "test_book");
+  
+  ps.setIntPref("browser.urlbar.default.behavior",
+                Ci.mozIPlacesAutoComplete.BEHAVIOR_HISTORY);
+  
+  addAdaptiveFeedback(TEST_URL, SEARCH_STRING, check_results);
+}
