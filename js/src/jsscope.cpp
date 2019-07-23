3c499a1b41bc087ce1b84f995ff9198362cca07c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:33:06 +0000

    Memory-pressure based GC scheduler (506125, r=igor).
    UltraBlame original commit: 70b3b23c948a43033e94c3407c32a28351628423

--- js/src/jsscope.cpp
+++ js/src/jsscope.cpp
@@ -172,7 +172,6 @@ JSScope::createTable(JSContext *cx, bool report)
             JS_ReportOutOfMemory(cx);
         return false;
     }
-    cx->updateMallocCounter(JS_BIT(sizeLog2) * sizeof(JSScopeProperty *));
 
     hashShift = JS_DHASH_BITS - sizeLog2;
     for (sprop = lastProp; sprop; sprop = sprop->parent) {
@@ -410,9 +409,6 @@ JSScope::changeTable(JSContext *cx, int change)
     removedCount = 0;
     oldtable = table;
     table = newtable;
-
-    
-    cx->runtime->gcMallocBytes += nbytes;
 
     
     for (oldspp = oldtable; oldsize != 0; oldspp++) {
