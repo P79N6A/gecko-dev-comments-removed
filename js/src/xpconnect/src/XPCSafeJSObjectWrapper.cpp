c4d01e1154a7bfb21ee01dade428c87972a25055
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:59:06 +0000

    SJOW's equality hook returns true too much. bug 413902, r+sr=jst a=damons
    UltraBlame original commit: b2e6aa3448fc4ce9a15b8c43904a78bbae65dcf1

--- js/src/xpconnect/src/XPCSafeJSObjectWrapper.cpp
+++ js/src/xpconnect/src/XPCSafeJSObjectWrapper.cpp
@@ -956,9 +956,21 @@ XPC_SJOW_Equality(JSContext *cx, JSObject *obj, jsval v, JSBool *bp)
     JSObject *other = JSVAL_TO_OBJECT(v);
     JSObject *otherUnsafe = GetUnsafeObject(other);
 
-    *bp = (obj == other || unsafeObj == other ||
-           (unsafeObj && unsafeObj == otherUnsafe) ||
-           XPC_GetIdentityObject(cx, obj) == XPC_GetIdentityObject(cx, other));
+    
+    
+    
+    
+    
+    
+    if (obj == other || unsafeObj == other ||
+        (unsafeObj && unsafeObj == otherUnsafe)) {
+      *bp = JS_TRUE;
+    } else {
+      nsISupports *objIdentity = XPC_GetIdentityObject(cx, obj);
+      nsISupports *otherIdentity = XPC_GetIdentityObject(cx, other);
+
+      *bp = objIdentity && objIdentity == otherIdentity;
+    }
   }
 
   return JS_TRUE;
