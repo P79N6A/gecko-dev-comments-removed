4a1b02bdb3c4630d1264477270d33933f3266a4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:33 +0000

    Bug 382682, Add makefile target to launch interactive xpcshell for debugging. Patch by Alex Vincent
    UltraBlame original commit: 15efbdafa0a758e6ae6c406c5358f955fe0c263f

--- tools/test-harness/xpcshell-simple/tail.js
+++ tools/test-harness/xpcshell-simple/tail.js
@@ -35,18 +35,19 @@
 
 
 
+function _execute_test() {
+  try {
+    do_test_pending();
+    run_test();
+    do_test_finished();
+    _do_main();
+  } catch (e) {
+    _fail = true;
+    dump(e + "\n");
+  }
 
-try {
-  do_test_pending();
-  run_test();
-  do_test_finished();
-  _do_main();
-} catch (e) {
-  _fail = true;
-  dump(e + "\n");
+  if (_fail)
+    dump("*** FAIL ***\n");
+  else
+    dump("*** PASS ***\n");
 }
-
-if (_fail)
-  dump("*** FAIL ***\n");
-else
-  dump("*** PASS ***\n");
