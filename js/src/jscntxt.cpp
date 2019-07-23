0d9532fc73270ecffc177eda696830f8587110d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:28:42 +0000

    bug 557140 - avoid abort() on OOM with ill-lopping traced code. r=jorendorff
    UltraBlame original commit: 2425ea22570c7d2c9884540451e667816972646e

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -1844,11 +1844,27 @@ js_InvokeOperationCallback(JSContext *cx)
 
 
 
-    if (cx->runtime->gcIsNeeded)
+    JSRuntime *rt = cx->runtime;
+    if (rt->gcIsNeeded) {
         js_GC(cx, GC_NORMAL);
+
+        
+
+
+
+        bool delayedOutOfMemory;
+        JS_LOCK_GC(rt);
+        delayedOutOfMemory = (rt->gcBytes > rt->gcMaxBytes);
+        JS_UNLOCK_GC(rt);
+        if (delayedOutOfMemory) {
+            js_ReportOutOfMemory(cx);
+            return false;
+        }
+    }
 #ifdef JS_THREADSAFE
-    else
+    else {
         JS_YieldRequest(cx);
+    }
 #endif
 
     JSOperationCallback cb = cx->operationCallback;
