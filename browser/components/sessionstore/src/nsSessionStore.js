b4da787e020b9d9125bbcadb1a4538f2deb9465d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:44:18 +0000

    Bug 387995, don't _repeatedly_ try to use Breakpad if it wasn't built, Patch by Simon Bünzli, r=gavin
    UltraBlame original commit: 1dac5c39836f8684d97ebf0ba177b58a4e9e6dc9

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -355,8 +355,6 @@ SessionStoreService.prototype = {
         this.onTabLoad(aEvent.currentTarget.ownerDocument.defaultView, aEvent.currentTarget, aEvent);
         break;
       case "input":
-        this.onTabInput(aEvent.currentTarget.ownerDocument.defaultView, aEvent.currentTarget, aEvent);
-        break;
       case "DOMAutoComplete":
         this.onTabInput(aEvent.currentTarget.ownerDocument.defaultView, aEvent.currentTarget, aEvent);
         break;
@@ -1883,15 +1881,17 @@ SessionStoreService.prototype = {
 
 
   _updateCrashReportURL: function sss_updateCrashReportURL(aWindow) {
-     var currentUrl = aWindow.getBrowser().currentURI.spec;
-     try {
-       var cr = Cc["@mozilla.org/xre/app-info;1"].
-                getService(Ci.nsICrashReporter);
-       cr.annotateCrashReport("URL", currentUrl);
-     }
-     catch (ex) {
-       
-     }
+    if (!Ci.nsICrashReporter) {
+      
+      this._updateCrashReportURL = function(aWindow) {};
+      return;
+    }
+    try {
+      var currentUrl = aWindow.getBrowser().currentURI.spec;
+      var cr = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsICrashReporter);
+      cr.annotateCrashReport("URL", currentUrl);
+    }
+    catch (ex) { debug(ex); }
   },
 
   
