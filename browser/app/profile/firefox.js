cadb6a77822189c4de4d107bf8fa3696e6552f54
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:54:02 +0000

    Bug 522571: Link from themes manager to personas gallery. r=robstrong
    UltraBlame original commit: a6db334c3c849cde5a76b20cc5a798171b2279fc

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -165,9 +165,7 @@ pref("extensions.update.url", "https://versioncheck.addons.mozilla.org/update/Ve
 pref("extensions.update.interval", 86400);  
                                             
 
-pref("extensions.getMoreExtensionsURL", "https://%LOCALE%.add-ons.mozilla.com/%LOCALE%/%APP%/%VERSION%/extensions/");
-pref("extensions.getMoreThemesURL", "https://%LOCALE%.add-ons.mozilla.com/%LOCALE%/%APP%/%VERSION%/themes/");
-pref("extensions.getMorePluginsURL", "https://%LOCALE%.add-ons.mozilla.com/%LOCALE%/%APP%/%VERSION%/plugins/");
+pref("extensions.getMoreThemesURL", "https://addons.mozilla.org/%LOCALE%/%APP%/getpersonas");
 pref("extensions.dss.enabled", false);          
 pref("extensions.dss.switchPending", false);    
                                                 
