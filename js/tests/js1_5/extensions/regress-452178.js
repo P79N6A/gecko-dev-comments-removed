e10d38f01cc7b1a204410b6a33824108221e1c4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:49:55 +0000

    JavaScript Tests - regression test for bug 452178, by Jesse Ruderman
    UltraBlame original commit: 40bc810be6f27386b544506a94eb4835a24cf078

---
+++ js/tests/js1_5/extensions/regress-452178.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-452178.js';
+
+var BUGNUMBER = 452178;
+var summary = 'Do not assert with JIT: !(sprop->attrs & JSPROP_SHARED)';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  jit(true);
+
+  q getter= function(){}; for (var j = 0; j < 4; ++j) q = 1;
+
+  jit(false);
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
