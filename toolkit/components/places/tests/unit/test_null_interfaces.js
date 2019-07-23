e8a5c1c1841ffc27b886077260b0375d64ed12cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:13:11 +0000

    Bug 489872 - [@ nsNavHistory::IsVisited] doesn't check null aURI. r=sdwilsh For public entry points of nsNavHistory, add ENSURE_ARG for in-pointers and ENSURE_ARG_POINTER for out-pointers; NS_ASSERTION for private APIs. Test automatically finds functions from nav history service and calls them with null arguments.
    UltraBlame original commit: 6c48080dba78e9aa444aa70212e0806cf51743dc

---
+++ toolkit/components/places/tests/unit/test_null_interfaces.js
@@ -0,0 +1,126 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let _ = function(some, debug, text, to) print(Array.slice(arguments).join(" "));
+
+_("Make an array of services to test, each specifying a class id, interface,",
+  "and an array of function names that don't throw when passed nulls");
+let testServices = [
+  ["browser/nav-history-service;1", "nsINavHistoryService",
+    ["queryStringToQueries", "removePagesByTimeframe", "removePagesFromHost"]],
+];
+_(testServices.join("\n"));
+
+function run_test()
+{
+  testServices.forEach(function([cid, iface, nothrow]) {
+    _("Running test with", cid, iface, nothrow);
+    let s = Cc["@mozilla.org/" + cid].getService(Ci[iface]);
+
+    let okName = function(name) {
+      _("Checking if function is okay to test:", name);
+      let func = s[name];
+
+      let mesg = "";
+      if (typeof func != "function")
+        mesg = "Not a function!";
+      else if (func.length == 0)
+        mesg = "No args needed!";
+      else if (name == "QueryInterface")
+        mesg = "Ignore QI!";
+
+      if (mesg == "")
+        return true;
+
+      _(mesg, "Skipping:", name);
+      return false;
+    }
+
+    _("Generating an array of functions to test service:", s);
+    [i for (i in s) if (okName(i))].sort().forEach(function(n) {
+      _();
+      _("Testing history function with null args:", n);
+
+      let func = s[n];
+      let num = func.length;
+      _("Generating array of nulls for #args:", num);
+      let args = [];
+      for (let i = num; --i >= 0; )
+        args.push(null);
+
+      let tryAgain = true;
+      while (tryAgain == true) {
+        try {
+          _("Calling with args:", JSON.stringify(args));
+          func.apply(s, args);
+  
+          _("The function didn't throw! Is it one of the nothrow?", nothrow);
+          do_check_neq(nothrow.indexOf(n), -1);
+
+          _("Must have been an expected nothrow, so no need to try again");
+          tryAgain = false;
+        }
+        catch(ex if ex.name.match(/NS_ERROR_ILLEGAL_VALUE/)) {
+          _("Caught an expected exception:", ex.name);
+
+          _("Moving on to the next test..");
+          tryAgain = false;
+        }
+        catch(ex if ex.name.match(/NS_ERROR_XPC_NEED_OUT_OBJECT/)) {
+          let pos = Number(ex.message.match(/object arg (\d+)/)[1]);
+          _("Function call expects an out object at", pos);
+          args[pos] = {};
+        }
+        catch(ex) {
+          _("Caught some unexpected exception.. dumping");
+          _([[i, ex[i]] for (i in ex)].join("\n"));
+          do_check_true(false);
+        }
+      }
+    });
+  });
+}
