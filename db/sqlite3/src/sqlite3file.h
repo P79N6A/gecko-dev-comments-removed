82c3d4c6e53e8eae4e65591b5a5d362cab10aa31
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:44:23 +0000

    Backout of Bug 306907
    UltraBlame original commit: e27f063885b034417a669d603c6461ec8188b649

--- db/sqlite3/src/sqlite3file.h
+++ db/sqlite3/src/sqlite3file.h
@@ -200,11 +200,7 @@ struct sqlite3OsVtbl {
   
 
 
-#ifdef XP_WIN
-  extern __declspec(dllimport) struct sqlite3OsVtbl sqlite3Os;
-#else
   extern struct sqlite3OsVtbl sqlite3Os;
-#endif 
 #endif 
 
 } 
