a02ec3b7d5634e83651f12df157975d6b4c06629
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:48:17 +0000

    Bug 479381 - js_FinalizeStringRT dies with multi-threaded app. r=igor.
    UltraBlame original commit: 03fc512bd2a2680a462b6cbeacb444bbbb250f9e

--- js/src/jsstr.cpp
+++ js/src/jsstr.cpp
@@ -48,6 +48,7 @@
 
 
 
+#include "jsstddef.h"
 #include <stdlib.h>
 #include <string.h>
 #include "jstypes.h"
@@ -2746,6 +2747,10 @@ js_GetUnitStringForChar(JSContext *cx, jschar c)
         JS_LOCK_GC(rt);
         if (!rt->unitStrings[c])
             rt->unitStrings[c] = str;
+#ifdef DEBUG
+        else
+            JSFLATSTR_INIT(str, NULL, 0);  
+#endif
         JS_UNLOCK_GC(rt);
     }
     return rt->unitStrings[c];
