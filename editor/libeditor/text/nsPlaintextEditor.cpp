891faeaf8770ebe8009d76fe5cb2cae70e1b4502
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:01:39 +0000

    Back out patch from Theppitak Karoonboonyanan in bug 157546 due to test failures.
    UltraBlame original commit: 09fd4969c7d67bdb8b6801c117e41264b01fa664

--- editor/libeditor/text/nsPlaintextEditor.cpp
+++ editor/libeditor/text/nsPlaintextEditor.cpp
@@ -678,7 +678,6 @@ NS_IMETHODIMP nsPlaintextEditor::DeleteSelection(nsIEditor::EDirection aAction)
   
   
   if (aAction == eNextWord || aAction == ePreviousWord
-      || (aAction == eNext && bCollapsed)
       || aAction == eToBeginningOfLine || aAction == eToEndOfLine)
   {
     nsCOMPtr<nsISelectionController> selCont (do_QueryReferent(mSelConWeak));
@@ -695,10 +694,6 @@ NS_IMETHODIMP nsPlaintextEditor::DeleteSelection(nsIEditor::EDirection aAction)
           break;
         case ePreviousWord:
           result = selCont->WordExtendForDelete(PR_FALSE);
-          aAction = eNone;
-          break;
-        case eNext:
-          result = selCont->CharacterExtendForDelete();
           aAction = eNone;
           break;
         case eToBeginningOfLine:
