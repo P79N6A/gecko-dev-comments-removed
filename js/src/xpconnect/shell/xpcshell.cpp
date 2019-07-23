237d0bb27f9ff12253dd57ca9a857d324251802a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:43:07 +0000

    Bug 461841: xpcshell: default to non-interactive when isatty is missing. r=roc
    UltraBlame original commit: 22446b1a041cc9b3831747e6a0d030c97cff8ad1

--- js/src/xpconnect/shell/xpcshell.cpp
+++ js/src/xpconnect/shell/xpcshell.cpp
@@ -831,8 +831,11 @@ ProcessFile(JSContext *cx, JSObject *obj, const char *filename, FILE *file,
     if (forceTTY) {
         file = stdin;
     }
+    else
 #ifdef HAVE_ISATTY
-    else if (!isatty(fileno(file))) {
+    if (!isatty(fileno(file)))
+#endif
+    {
         
 
 
@@ -863,7 +866,6 @@ ProcessFile(JSContext *cx, JSObject *obj, const char *filename, FILE *file,
 
         return;
     }
-#endif
 
     
     lineno = 1;
