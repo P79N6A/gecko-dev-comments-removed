e1b5b880b6873d7b269969ba5f3b2fd97811d3fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:15:45 +0000

    Fix two subtle bugs to-do with partial flat closures (545573, r=jorendorff).
    UltraBlame original commit: c1a0484b8a233a3010b55dbf1d04ffc143c4ace8

---
+++ js/src/tests/js1_8/extensions/expclo.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+var gTestfile = 'expclo.js';
+var summary = "Flat expression closure source coordinate fencepost test";
+
+function f(a) {
+    if (a) {
+        let b = 42;
+        let c = function () a+b;
+        ++b;
+        return c;
+    }
+    return null;
+}
+
+var expect = 44;
+var actual = f(1)();
+
+reportCompare(expect, actual, summary);
---
+++ js/src/tests/js1_8/extensions/expclo2.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+var gTestfile = 'expclo.js';
+var summary = "Partial flat expression closure upvar order test";
+
+function f(a) {
+    if (a) {
+        let b = 42;
+        let c = function () b+a;
+        ++b;
+        return c;
+    }
+    return null;
+}
+
+var expect = 44;
+var actual = f(1)();
+
+reportCompare(expect, actual, summary);
