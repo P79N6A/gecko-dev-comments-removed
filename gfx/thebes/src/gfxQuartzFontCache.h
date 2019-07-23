905b204e499efcd380753cbdc3b03689c901577a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:28:31 +0000

    b=455243, weird behaviour with fonts with bad faces; r=jdaggett
    UltraBlame original commit: da0a0796090fad74500b59ffab6e8de148e5df07

--- gfx/thebes/src/gfxQuartzFontCache.h
+++ gfx/thebes/src/gfxQuartzFontCache.h
@@ -65,16 +65,18 @@ struct FontSearch {
 
 class MacOSFamilyEntry;
 class gfxQuartzFontCache;
+class FontEntryStandardFaceComparator;
 
 
 class MacOSFontEntry : public gfxFontEntry
 {
 public:
     friend class gfxQuartzFontCache;
+    friend class FontEntryStandardFaceComparator;
 
     
     MacOSFontEntry(const nsAString& aPostscriptName, PRInt32 aAppleWeight, PRUint32 aTraits, 
-                    MacOSFamilyEntry *aFamily);
+                   MacOSFamilyEntry *aFamily, PRBool aIsStandardFace = PR_FALSE);
 
     const nsString& FamilyName();
 
@@ -94,6 +96,7 @@ protected:
 
     ATSUFontID mATSUFontID;
     PRPackedBool mATSUIDInitialized;
+    PRPackedBool mStandardFace;
 };
 
 
@@ -150,6 +153,9 @@ public:
         for (i = 0; i < numFonts; i++)
             mAvailableFonts[i]->mIsBadUnderlineFont = aIsBadUnderlineFont;
     }
+
+    
+    void SortAvailableFonts();
 
 protected:
     
