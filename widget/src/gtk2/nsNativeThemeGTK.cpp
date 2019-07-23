3624bbf4a72f31ba53b5dd106958793ac073a073
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:51:42 +0000

    Bug 400470 - "GTK radio buttons make region too large" [p=ventnor.bugzilla@yahoo.com.au (Michael Ventnor) r+sr=roc aM9=beltzner]
    UltraBlame original commit: 27d92cd4552225a7c5d2109d02a6b6be5f4e67cf

--- widget/src/gtk2/nsNativeThemeGTK.cpp
+++ widget/src/gtk2/nsNativeThemeGTK.cpp
@@ -817,7 +817,7 @@ nsNativeThemeGTK::GetMinimumWidgetSize(nsIRenderingContext* aContext,
       }
 
       
-      aResult->width = indicator_size + 3 * indicator_spacing;
+      aResult->width = indicator_size + 2 * indicator_spacing;
       aResult->height = indicator_size + 2 * indicator_spacing;
       *aIsOverridable = PR_FALSE;
     }
