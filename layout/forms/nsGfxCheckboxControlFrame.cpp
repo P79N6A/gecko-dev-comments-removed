7fbc07d996b75869f82a95c13f7c81b3c97f6969
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:03:29 +0000

    Bug 475535 - don't go through PaintBackgroundWithSC to draw non-native radio buttons and checkboxes; remove special UA pseudo-elements for styling these. r+sr=roc
    UltraBlame original commit: f3d729b22f94d01cc90db5a9e8e80af6640bca11

--- layout/forms/nsGfxCheckboxControlFrame.cpp
+++ layout/forms/nsGfxCheckboxControlFrame.cpp
@@ -49,9 +49,14 @@
 #include "nsIDOMNSHTMLInputElement.h"
 
 static void
-PaintCheckMark(nsIRenderingContext& aRenderingContext,
-               const nsRect& aRect)
+PaintCheckMark(nsIFrame* aFrame,
+               nsIRenderingContext* aCtx,
+               const nsRect& aDirtyRect,
+               nsPoint aPt)
 {
+  nsRect rect(aPt, aFrame->GetSize());
+  rect.Deflate(aFrame->GetUsedBorderAndPadding());
+
   
   const PRInt32 checkPolygonX[] = { -3, -1,  3,  3, -1, -3 };
   const PRInt32 checkPolygonY[] = { -1,  1, -3, -1,  3,  1 };
@@ -60,9 +65,9 @@ PaintCheckMark(nsIRenderingContext& aRenderingContext,
                                     
 
   
-  nscoord paintScale = PR_MIN(aRect.width, aRect.height) / checkSize;
-  nsPoint paintCenter(aRect.x + aRect.width  / 2,
-                      aRect.y + aRect.height / 2);
+  nscoord paintScale = PR_MIN(rect.width, rect.height) / checkSize;
+  nsPoint paintCenter(rect.x + rect.width  / 2,
+                      rect.y + rect.height / 2);
 
   nsPoint paintPolygon[checkNumPoints];
   
@@ -72,24 +77,30 @@ PaintCheckMark(nsIRenderingContext& aRenderingContext,
                                       checkPolygonY[polyIndex] * paintScale);
   }
 
-  aRenderingContext.FillPolygon(paintPolygon, checkNumPoints);
+  aCtx->SetColor(aFrame->GetStyleColor()->mColor);
+  aCtx->FillPolygon(paintPolygon, checkNumPoints);
 }
 
 static void
-PaintIndeterminateMark(nsIRenderingContext& aRenderingContext,
-                       const nsRect& aRect)
+PaintIndeterminateMark(nsIFrame* aFrame,
+                       nsIRenderingContext* aCtx,
+                       const nsRect& aDirtyRect,
+                       nsPoint aPt)
 {
-  
-  nsRect fillRect = aRect;
-  fillRect.height /= 4;
-  fillRect.y += (aRect.height - fillRect.height) / 2;
+  nsRect rect(aPt, aFrame->GetSize());
+  rect.Deflate(aFrame->GetUsedBorderAndPadding());
 
-  aRenderingContext.FillRect(fillRect);
+  rect.y += (rect.height - rect.height/4) / 2;
+  rect.height /= 4;
+
+  aCtx->SetColor(aFrame->GetStyleColor()->mColor);
+  aCtx->FillRect(rect);
 }
 
 
 nsIFrame*
-NS_NewGfxCheckboxControlFrame(nsIPresShell* aPresShell, nsStyleContext* aContext)
+NS_NewGfxCheckboxControlFrame(nsIPresShell* aPresShell,
+                              nsStyleContext* aContext)
 {
   return new (aPresShell) nsGfxCheckboxControlFrame(aContext);
 }
@@ -111,62 +122,21 @@ NS_QUERYFRAME_HEAD(nsGfxCheckboxControlFrame)
   NS_QUERYFRAME_ENTRY(nsICheckboxControlFrame)
 NS_QUERYFRAME_TAIL_INHERITING(nsFormControlFrame)
 
-NS_IMETHODIMP
-nsGfxCheckboxControlFrame::Init(nsIContent* aContent,
-                                nsIFrame* aParent,
-                                nsIFrame* aPrevInFlow)
-{
-  nsresult rv = nsFormControlFrame::Init(aContent, aParent, aPrevInFlow);
-  if (NS_SUCCEEDED(rv)) {
-    mCheckButtonFaceStyle =
-      PresContext()->PresShell()->StyleSet()->
-        ResolvePseudoStyleFor(aContent, nsCSSAnonBoxes::check,
-                              GetStyleContext());
-  }
-
-  return rv;
-}
-
 #ifdef ACCESSIBILITY
-NS_IMETHODIMP nsGfxCheckboxControlFrame::GetAccessible(nsIAccessible** aAccessible)
+NS_IMETHODIMP
+nsGfxCheckboxControlFrame::GetAccessible(nsIAccessible** aAccessible)
 {
-  nsCOMPtr<nsIAccessibilityService> accService = do_GetService("@mozilla.org/accessibilityService;1");
+  nsCOMPtr<nsIAccessibilityService> accService
+    = do_GetService("@mozilla.org/accessibilityService;1");
 
   if (accService) {
-    return accService->CreateHTMLCheckboxAccessible(static_cast<nsIFrame*>(this), aAccessible);
+    return accService->CreateHTMLCheckboxAccessible(
+      static_cast<nsIFrame*>(this), aAccessible);
   }
 
   return NS_ERROR_FAILURE;
 }
 #endif
-
-
-nsStyleContext*
-nsGfxCheckboxControlFrame::GetAdditionalStyleContext(PRInt32 aIndex) const
-{
-  switch (aIndex) {
-  case NS_GFX_CHECKBOX_CONTROL_FRAME_FACE_CONTEXT_INDEX:
-    return mCheckButtonFaceStyle;
-    break;
-  default:
-    return nsnull;
-  }
-}
-
-
-
-
-void
-nsGfxCheckboxControlFrame::SetAdditionalStyleContext(PRInt32 aIndex, 
-                                                     nsStyleContext* aStyleContext)
-{
-  switch (aIndex) {
-  case NS_GFX_CHECKBOX_CONTROL_FRAME_FACE_CONTEXT_INDEX:
-    mCheckButtonFaceStyle = aStyleContext;
-    break;
-  }
-}
-
 
 
 NS_IMETHODIMP
@@ -177,64 +147,14 @@ nsGfxCheckboxControlFrame::OnChecked(nsPresContext* aPresContext,
   return NS_OK;
 }
 
-static void PaintCheckMarkFromStyle(nsIFrame* aFrame,
-     nsIRenderingContext* aCtx, const nsRect& aDirtyRect, nsPoint aPt) {
-  static_cast<nsGfxCheckboxControlFrame*>(aFrame)
-    ->PaintCheckBoxFromStyle(*aCtx, aPt, aDirtyRect);
-}
-
-class nsDisplayCheckMark : public nsDisplayItem {
-public:
-  nsDisplayCheckMark(nsGfxCheckboxControlFrame* aFrame)
-    : nsDisplayItem(aFrame) {
-    MOZ_COUNT_CTOR(nsDisplayCheckMark);
-  }
-#ifdef NS_BUILD_REFCNT_LOGGING
-  virtual ~nsDisplayCheckMark() {
-    MOZ_COUNT_DTOR(nsDisplayCheckMark);
-  }
-#endif
-
-  virtual void Paint(nsDisplayListBuilder* aBuilder, nsIRenderingContext* aCtx,
-     const nsRect& aDirtyRect);
-  NS_DISPLAY_DECL_NAME("CheckMark")
-};
-
-void
-nsDisplayCheckMark::Paint(nsDisplayListBuilder* aBuilder,
-     nsIRenderingContext* aCtx, const nsRect& aDirtyRect) {
-  static_cast<nsGfxCheckboxControlFrame*>(mFrame)->
-    PaintCheckBox(*aCtx, aBuilder->ToReferenceFrame(mFrame), aDirtyRect);
-}
-
-
-
-void
-nsGfxCheckboxControlFrame::PaintCheckBox(nsIRenderingContext& aRenderingContext,
-                                         nsPoint aPt,
-                                         const nsRect& aDirtyRect)
-{
-  
-  
-  nsRect checkRect(aPt, mRect.Size());
-  checkRect.Deflate(GetUsedBorderAndPadding());
-
-  const nsStyleColor* color = GetStyleColor();
-  aRenderingContext.SetColor(color->mColor);
-
-  if (IsIndeterminate())
-    PaintIndeterminateMark(aRenderingContext, checkRect);
-  else
-    PaintCheckMark(aRenderingContext, checkRect);
-}
-
 
 NS_IMETHODIMP
 nsGfxCheckboxControlFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
                                             const nsRect&           aDirtyRect,
                                             const nsDisplayListSet& aLists)
 {
-  nsresult rv = nsFormControlFrame::BuildDisplayList(aBuilder, aDirtyRect, aLists);
+  nsresult rv = nsFormControlFrame::BuildDisplayList(aBuilder, aDirtyRect,
+                                                     aLists);
   NS_ENSURE_SUCCESS(rv, rv);
   
   
@@ -244,45 +164,11 @@ nsGfxCheckboxControlFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
   if (IsThemed())
     return NS_OK; 
 
-  
-  if (mCheckButtonFaceStyle) {
-    
-    
-    
-    
-    
-    const nsStyleBackground* myBackground = mCheckButtonFaceStyle->GetStyleBackground();
-    if (!myBackground->IsTransparent())
-      return aLists.Content()->AppendNewToTop(new (aBuilder)
-          nsDisplayGeneric(this, PaintCheckMarkFromStyle, "CheckMarkFromStyle"));
-  }
-
-  return aLists.Content()->AppendNewToTop(new (aBuilder) nsDisplayCheckMark(this));
-}
-
-void
-nsGfxCheckboxControlFrame::PaintCheckBoxFromStyle(
-    nsIRenderingContext& aRenderingContext, nsPoint aPt, const nsRect& aDirtyRect) {
-  const nsStylePosition* myPosition = mCheckButtonFaceStyle->GetStylePosition();
-  const nsStyleBorder* myBorder = mCheckButtonFaceStyle->GetStyleBorder();
-  const nsStyleBackground* myBackground = mCheckButtonFaceStyle->GetStyleBackground();
-
-  NS_ASSERTION(myPosition->mWidth.GetUnit() == eStyleUnit_Coord &&
-               myPosition->mHeight.GetUnit() == eStyleUnit_Coord,
-               "styles for :-moz-checkbox are incorrect or author-accessible");
-  nscoord width = myPosition->mWidth.GetCoordValue();
-  nscoord height = myPosition->mHeight.GetCoordValue();
-  
-  nscoord x = (mRect.width - width) / 2;
-  nscoord y = (mRect.height - height) / 2;
-  nsRect rect(aPt.x + x, aPt.y + y, width, height);
-
-  nsCSSRendering::PaintBackgroundWithSC(PresContext(), aRenderingContext,
-                                        this, aDirtyRect, rect, *myBackground,
-                                        *myBorder, PR_FALSE);
-  nsCSSRendering::PaintBorder(PresContext(), aRenderingContext, this,
-                              aDirtyRect, rect, *myBorder,
-                              mCheckButtonFaceStyle);
+  return aLists.Content()->AppendNewToTop(new (aBuilder)
+    nsDisplayGeneric(this,
+                     IsIndeterminate()
+                     ? PaintIndeterminateMark : PaintCheckMark,
+                     "CheckedCheckbox"));
 }
 
 
--- layout/forms/nsGfxRadioControlFrame.cpp
+++ layout/forms/nsGfxRadioControlFrame.cpp
@@ -66,29 +66,16 @@ NS_QUERYFRAME_HEAD(nsGfxRadioControlFrame)
   NS_QUERYFRAME_ENTRY(nsIRadioControlFrame)
 NS_QUERYFRAME_TAIL_INHERITING(nsFormControlFrame)
 
-NS_IMETHODIMP
-nsGfxRadioControlFrame::Init(nsIContent* aContent,
-                             nsIFrame* aParent,
-                             nsIFrame* aPrevInFlow)
-{
-  nsresult rv = nsFormControlFrame::Init(aContent, aParent, aPrevInFlow);
-  if (NS_SUCCEEDED(rv)) {
-    mRadioButtonFaceStyle =
-      PresContext()->PresShell()->StyleSet()->
-        ResolvePseudoStyleFor(aContent, nsCSSAnonBoxes::radio,
-                              GetStyleContext());
-  }
-
-  return rv;
-}
-
 #ifdef ACCESSIBILITY
-NS_IMETHODIMP nsGfxRadioControlFrame::GetAccessible(nsIAccessible** aAccessible)
+NS_IMETHODIMP
+nsGfxRadioControlFrame::GetAccessible(nsIAccessible** aAccessible)
 {
-  nsCOMPtr<nsIAccessibilityService> accService = do_GetService("@mozilla.org/accessibilityService;1");
+  nsCOMPtr<nsIAccessibilityService> accService
+    = do_GetService("@mozilla.org/accessibilityService;1");
 
   if (accService) {
-    return accService->CreateHTMLRadioButtonAccessible(static_cast<nsIFrame*>(this), aAccessible);
+    return accService->CreateHTMLRadioButtonAccessible(
+      static_cast<nsIFrame*>(this), aAccessible);
   }
 
   return NS_ERROR_FAILURE;
@@ -96,98 +83,31 @@ NS_IMETHODIMP nsGfxRadioControlFrame::GetAccessible(nsIAccessible** aAccessible)
 #endif
 
 
-nsStyleContext*
-nsGfxRadioControlFrame::GetAdditionalStyleContext(PRInt32 aIndex) const
+
+static void
+PaintCheckedRadioButton(nsIFrame* aFrame,
+                        nsIRenderingContext* aCtx,
+                        const nsRect& aDirtyRect,
+                        nsPoint aPt)
 {
-  switch (aIndex) {
-  case NS_GFX_RADIO_CONTROL_FRAME_FACE_CONTEXT_INDEX:
-    return mRadioButtonFaceStyle;
-    break;
-  default:
-    return nsnull;
-  }
+  
+  
+  nsRect rect(aPt, aFrame->GetSize());
+  rect.Deflate(aFrame->GetUsedBorderAndPadding());
+  rect.Deflate(nsPresContext::CSSPixelsToAppUnits(2),
+               nsPresContext::CSSPixelsToAppUnits(2));
+
+  aCtx->SetColor(aFrame->GetStyleColor()->mColor);
+  aCtx->FillEllipse(rect);
 }
-
-
-void
-nsGfxRadioControlFrame::SetAdditionalStyleContext(PRInt32 aIndex, 
-                                                  nsStyleContext* aStyleContext)
-{
-  switch (aIndex) {
-  case NS_GFX_RADIO_CONTROL_FRAME_FACE_CONTEXT_INDEX:
-    mRadioButtonFaceStyle = aStyleContext;
-    break;
-  }
-}
-
-
-void
-nsGfxRadioControlFrame::PaintRadioButtonFromStyle(
-    nsIRenderingContext& aRenderingContext, nsPoint aPt, const nsRect& aDirtyRect)
-{
-  const nsStyleBorder* myBorder = mRadioButtonFaceStyle->GetStyleBorder();
-  
-  const nsStyleBackground* myColor = mRadioButtonFaceStyle->GetStyleBackground();
-  const nsStyleColor* color = mRadioButtonFaceStyle->GetStyleColor();
-  const nsStylePosition* myPosition = mRadioButtonFaceStyle->GetStylePosition();
-
-  NS_ASSERTION(myPosition->mWidth.GetUnit() == eStyleUnit_Coord &&
-               myPosition->mHeight.GetUnit() == eStyleUnit_Coord,
-               "styles for :-moz-radio are incorrect or author-accessible");
-  nscoord width = myPosition->mWidth.GetCoordValue();
-  nscoord height = myPosition->mHeight.GetCoordValue();
-  
-  nscoord x = (mRect.width - width) / 2;
-  nscoord y = (mRect.height - height) / 2;
-  nsRect rect = nsRect(x, y, width, height) + aPt;
-
-  
-  
-  
-  
-  
-  
-  nsStyleBackground tmpColor     = *myColor;
-  tmpColor.mBackgroundColor = color->mColor;
-  nsPresContext* pc = PresContext();
-  nsCSSRendering::PaintBackgroundWithSC(pc, aRenderingContext,
-                                        this, aDirtyRect, rect,
-                                        tmpColor, *myBorder, PR_FALSE);
-  nsCSSRendering::PaintBorder(pc, aRenderingContext, this,
-                              aDirtyRect, rect, *myBorder, mRadioButtonFaceStyle, 0);
-}
-
-class nsDisplayRadioButtonFromStyle : public nsDisplayItem {
-public:
-  nsDisplayRadioButtonFromStyle(nsGfxRadioControlFrame* aFrame)
-    : nsDisplayItem(aFrame) {
-    MOZ_COUNT_CTOR(nsDisplayRadioButtonFromStyle);
-  }
-#ifdef NS_BUILD_REFCNT_LOGGING
-  virtual ~nsDisplayRadioButtonFromStyle() {
-    MOZ_COUNT_DTOR(nsDisplayRadioButtonFromStyle);
-  }
-#endif
-  
-  virtual void Paint(nsDisplayListBuilder* aBuilder, nsIRenderingContext* aCtx,
-     const nsRect& aDirtyRect);
-  NS_DISPLAY_DECL_NAME("RadioButton")
-};
-
-void
-nsDisplayRadioButtonFromStyle::Paint(nsDisplayListBuilder* aBuilder,
-     nsIRenderingContext* aCtx, const nsRect& aDirtyRect) {
-  static_cast<nsGfxRadioControlFrame*>(mFrame)->
-    PaintRadioButtonFromStyle(*aCtx, aBuilder->ToReferenceFrame(mFrame), aDirtyRect);
-}
-
 
 NS_IMETHODIMP
 nsGfxRadioControlFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
                                          const nsRect&           aDirtyRect,
                                          const nsDisplayListSet& aLists)
 {
-  nsresult rv = nsFormControlFrame::BuildDisplayList(aBuilder, aDirtyRect, aLists);
+  nsresult rv = nsFormControlFrame::BuildDisplayList(aBuilder, aDirtyRect,
+                                                     aLists);
   NS_ENSURE_SUCCESS(rv, rv);
   
   if (!IsVisibleForPainting(aBuilder))
@@ -196,19 +116,14 @@ nsGfxRadioControlFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
   if (IsThemed())
     return NS_OK; 
 
-  if (!mRadioButtonFaceStyle)
-    return NS_OK;
-  
   PRBool checked = PR_TRUE;
   GetCurrentCheckState(&checked); 
   if (!checked)
     return NS_OK;
     
   return aLists.Content()->AppendNewToTop(new (aBuilder)
-      nsDisplayRadioButtonFromStyle(this));
+    nsDisplayGeneric(this, PaintCheckedRadioButton, "CheckedRadioButton"));
 }
-
-
 
 NS_IMETHODIMP
 nsGfxRadioControlFrame::OnChecked(nsPresContext* aPresContext,
@@ -217,4 +132,3 @@ nsGfxRadioControlFrame::OnChecked(nsPresContext* aPresContext,
   InvalidateOverflowRect();
   return NS_OK;
 }
-
