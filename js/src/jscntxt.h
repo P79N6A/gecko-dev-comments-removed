f06156a6bca603ce6a3c1f55a33b16fd4d319719
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:22:45 +0000

    Bug 440473 - Crash [@ Decompile][@ js_GetSrcNoteOffset] with firebug/jQuery, r=igor
    UltraBlame original commit: 4aeb5932fc5ac4deab932926fb50fa22f58681f3

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -63,8 +63,10 @@ JS_BEGIN_EXTERN_C
 
 
 
+
+
 typedef struct JSGSNCache {
-    JSScript        *script;
+    jsbytecode      *code;
     JSDHashTable    table;
 #ifdef JS_GSNMETER
     uint32          hits;
@@ -79,7 +81,7 @@ typedef struct JSGSNCache {
 
 #define GSN_CACHE_CLEAR(cache)                                                \
     JS_BEGIN_MACRO                                                            \
-        (cache)->script = NULL;                                               \
+        (cache)->code = NULL;                                                 \
         if ((cache)->table.ops) {                                             \
             JS_DHashTableFinish(&(cache)->table);                             \
             (cache)->table.ops = NULL;                                        \
