cb2c5549c56ac092a044e0dc9212a1de5b735d10
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:31:36 +0000

    Bug 508229 Clean up whitespace in nsBinHexDecoder r=biesi
    UltraBlame original commit: 795199c7fdaa2202aab04e57ef74554e74cad82f

--- netwerk/streamconv/converters/nsBinHexDecoder.h
+++ netwerk/streamconv/converters/nsBinHexDecoder.h
@@ -60,35 +60,35 @@
 #include "nsCOMPtr.h"
 #include "nsString.h"
 
-#define NS_BINHEXDECODER_CID                        \
+#define NS_BINHEXDECODER_CID                         \
 { /* 301DEA42-6850-4cda-8945-81F7DBC2186B */         \
-    0x301dea42, 0x6850, 0x4cda,                      \
-    { 0x89, 0x45, 0x81, 0xf7, 0xdb, 0xc2, 0x18, 0x6b } \
+  0x301dea42, 0x6850, 0x4cda,                        \
+  { 0x89, 0x45, 0x81, 0xf7, 0xdb, 0xc2, 0x18, 0x6b } \
 }
 
 typedef struct _binhex_header
 {
-	PRUint32	type, creator;
-	PRUint16  flags;
-	PRInt32	dlen, rlen;
+  PRUint32 type, creator;
+  PRUint16 flags;
+  PRInt32 dlen, rlen;
 } binhex_header;
 
-typedef union 
+typedef union
 {
-	unsigned char c[4];
-	PRUint32  	  val;
+  unsigned char c[4];
+  PRUint32      val;
 } longbuf;
 
-#define BINHEX_STATE_START  	0
-#define BINHEX_STATE_FNAME  	1
-#define BINHEX_STATE_HEADER 	2
-#define BINHEX_STATE_HCRC   	3
-#define BINHEX_STATE_DFORK  	4
-#define BINHEX_STATE_DCRC   	5
-#define BINHEX_STATE_RFORK  	6
-#define BINHEX_STATE_RCRC   	7
-#define BINHEX_STATE_FINISH   	8
-#define BINHEX_STATE_DONE  		9
+#define BINHEX_STATE_START    0
+#define BINHEX_STATE_FNAME    1
+#define BINHEX_STATE_HEADER   2
+#define BINHEX_STATE_HCRC     3
+#define BINHEX_STATE_DFORK    4
+#define BINHEX_STATE_DCRC     5
+#define BINHEX_STATE_RFORK    6
+#define BINHEX_STATE_RCRC     7
+#define BINHEX_STATE_FINISH   8
+#define BINHEX_STATE_DONE     9
 
 
 class nsBinHexDecoder : public nsIStreamConverter
@@ -121,19 +121,19 @@ protected:
 
   
   nsCOMPtr<nsIOutputStream>     mOutputStream;     
-  nsCOMPtr<nsIInputStream>	    mInputStream;
+  nsCOMPtr<nsIInputStream>      mInputStream;
 
-  PRInt16   mState;			
-  PRUint16  mCRC; 
-	PRUint16  mFileCRC;		
-  longbuf   mOctetBuf;		
-	PRInt16 	mOctetin;		
-	PRInt16 	mDonePos;		
-	PRInt16 	mInCRC;			
+  PRInt16   mState;      
+  PRUint16  mCRC;        
+  PRUint16  mFileCRC;    
+  longbuf   mOctetBuf;   
+  PRInt16   mOctetin;    
+  PRInt16   mDonePos;    
+  PRInt16   mInCRC;      
 
   
   binhex_header mHeader;
-  char 	mName[64];		
+  char      mName[64];   
 
   
   
@@ -141,13 +141,13 @@ protected:
   char * mOutgoingBuffer; 
   PRUint32 mPosInDataBuffer;
 
-	unsigned char mRlebuf;	
+  unsigned char mRlebuf;  
 
-	PRInt32 mCount;			  
-  PRInt16 mMarker;			
-	
-	PRInt32	mPosInbuff;		   
-	PRInt32 mPosOutputBuff; 
+  PRInt32 mCount;         
+  PRInt16 mMarker;        
+
+  PRInt32 mPosInbuff;     
+  PRInt32 mPosOutputBuff; 
 };
 
 #endif 
