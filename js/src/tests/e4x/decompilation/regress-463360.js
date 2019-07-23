4070b9abe18708a475083ee0fe65b1fed6b782fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:29:29 +0000

    Bug 463360 - Uneval then eval E4X with { gives error.  r=jwalden
    UltraBlame original commit: 350ed77bf3999cf9c14a43530f807e76eab8b05c

---
+++ js/src/tests/e4x/decompilation/regress-463360.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+gTestfile = 'template.js';
+
+var summary = 'Uneval+eval of XML containing string with {';
+var BUGNUMBER = 463360;
+var actual = '';
+var expect = '';
+
+printBugNumber(BUGNUMBER);
+START(summary);
+
+actual = eval(uneval(XML("<a>{</a>")));
+expect = XML("<a>{</a>");
+
+compareSource(expect, actual, inSection(1) + summary);
+
+END();
