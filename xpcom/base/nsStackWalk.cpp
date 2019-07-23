bb56bf87e3cbe2e34bb3d6b56072124a30734421
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:46:20 +0000

    Enable Linux stack walking code on Mac OS X.  b=336517  r+a=bsmedberg
    UltraBlame original commit: 5c27a0fafb0f6720de04309a79b80a0b0d81796c

--- xpcom/base/nsStackWalk.cpp
+++ xpcom/base/nsStackWalk.cpp
@@ -1032,7 +1032,7 @@ NS_FormatCodeAddressDetails(void *aPC, const nsCodeAddressDetails *aDetails,
 
 
 
-#elif (defined(linux) && defined(__GNUC__) && (defined(__i386) || defined(PPC) || defined(__x86_64__))) || (defined(__sun) && (defined(__sparc) || defined(sparc) || defined(__i386) || defined(i386)))
+#elif (defined(linux) && defined(__GNUC__) && (defined(__i386) || defined(PPC) || defined(__x86_64__))) || (defined(__sun) && (defined(__sparc) || defined(sparc) || defined(__i386) || defined(i386))) || (defined(XP_MACOSX) && (defined(__ppc__) || defined(__i386)))
 
 #include <stdlib.h>
 #include <string.h>
@@ -1049,7 +1049,7 @@ NS_FormatCodeAddressDetails(void *aPC, const nsCodeAddressDetails *aDetails,
 #define __USE_GNU
 #endif
 
-#ifdef HAVE_LIBDL
+#if defined(HAVE_LIBDL) || defined(XP_MACOSX)
 #include <dlfcn.h>
 #endif
 
@@ -1081,7 +1081,7 @@ void DemangleSymbol(const char * aSymbol,
 }
 
 
-#if defined(linux) && defined(__GNUC__) && (defined(__i386) || defined(PPC) || defined(__x86_64__)) 
+#if (defined(linux) && defined(__GNUC__) && (defined(__i386) || defined(PPC) || defined(__x86_64__))) || (defined(XP_MACOSX) && (defined(__i386) || defined(__ppc__))) 
 
 
 EXPORT_XPCOM_API(nsresult)
@@ -1105,7 +1105,11 @@ NS_StackWalk(NS_WalkStackCallback aCallback, PRUint32 aSkipFrames,
 
   int skip = aSkipFrames;
   for ( ; (void**)*bp > bp; bp = (void**)*bp) {
+#if defined(__ppc__) && defined(XP_MACOSX) 
+    void *pc = *(bp+2);
+#else
     void *pc = *(bp+1);
+#endif
     if (--skip < 0) {
       (*aCallback)(pc, aClosure);
     }
