1a8a9ebc5b28dfafe3e7d8943b0db679747b30eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:57:03 +0000

    Bug 412068 - Number.prototype.toLocaleString incorrectly accesses the memory where its first argument should be, even if no first argument is actually given.  Tag-team r=igor, r+a=brendan By the way, this checkin occurred on an orange tree.  Just like every other patch in the last day.  Yay checkin policy.
    UltraBlame original commit: 773bfaeaa3416240e1521aaaca3fdb3d9916995d

---
+++ js/tests/ecma_3/Number/15.7.4.3-01.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = '15.7.4.3-01.js';
+
+var BUGNUMBER = "412068";
+var summary = "num.toLocaleString incorrectly accesses its first argument " +
+              "even when no first argument has been given";
+var actual, expect;
+
+printBugNumber(BUGNUMBER);
+printStatus(summary);
+
+
+
+
+
+var failed = false;
+
+try
+{
+  if ("3" !== 3..toLocaleString())
+    throw '"3" should equal 3..toLocaleString()";
+  if ("9" !== 9..toLocaleString(8))
+    throw 'Number.prototype.toLocaleString should ignore its first argument';
+}
+catch (e)
+{
+  failed = e;
+}
+
+expect = false;
+actual = failed;
+
+reportCompare(expect, actual, summary);
