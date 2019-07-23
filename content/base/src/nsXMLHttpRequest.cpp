e0f12b95496bebfebd5044dba01752cbb4fd9502
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:38:12 +0000

    Bug 313646 -  synchronous XMLHttpRequest does not fire readystatechange event, r=justin.lebar,sicking, sr=jst
    UltraBlame original commit: 36a582d5c645a8c69590cc837bfeb089f7c8cb6b

--- content/base/src/nsXMLHttpRequest.cpp
+++ content/base/src/nsXMLHttpRequest.cpp
@@ -2611,12 +2611,6 @@ nsXMLHttpRequest::Send(nsIVariant *aBody)
   }
 
   
-  
-  
-  
-  ChangeState(XML_HTTP_REQUEST_SENT);
-
-  
   if (!(mState & XML_HTTP_REQUEST_ASYNC)) {
     mState |= XML_HTTP_REQUEST_SYNCLOOPING;
 
@@ -2638,6 +2632,9 @@ nsXMLHttpRequest::Send(nsIVariant *aBody)
       }
     }
 
+    ChangeState(XML_HTTP_REQUEST_SENT);
+    
+    
     nsIThread *thread = NS_GetCurrentThread();
     while (mState & XML_HTTP_REQUEST_SYNCLOOPING) {
       if (!NS_ProcessNextEvent(thread)) {
@@ -2654,6 +2651,11 @@ nsXMLHttpRequest::Send(nsIVariant *aBody)
       NS_DispatchToCurrentThread(resumeTimeoutRunnable);
     }
   } else {
+    
+    
+    
+    
+    ChangeState(XML_HTTP_REQUEST_SENT);
     if (!mUploadComplete &&
         HasListenersFor(NS_LITERAL_STRING(UPLOADPROGRESS_STR)) ||
         (mUpload && mUpload->HasListenersFor(NS_LITERAL_STRING(PROGRESS_STR)))) {
@@ -2971,9 +2973,11 @@ nsXMLHttpRequest::ChangeState(PRUint32 aState, PRBool aBroadcast)
     mProgressNotifier->Cancel();
   }
 
-  if ((mState & XML_HTTP_REQUEST_ASYNC) &&
-      (aState & XML_HTTP_REQUEST_LOADSTATES) && 
-      aBroadcast) {
+  if ((aState & XML_HTTP_REQUEST_LOADSTATES) && 
+      aBroadcast &&
+      (mState & XML_HTTP_REQUEST_ASYNC ||
+       aState & XML_HTTP_REQUEST_OPENED ||
+       aState & XML_HTTP_REQUEST_COMPLETED)) {
     nsCOMPtr<nsIDOMEvent> event;
     rv = CreateReadystatechangeEvent(getter_AddRefs(event));
     NS_ENSURE_SUCCESS(rv, rv);
