09d0a592ba54e3b8273b382fbada5ea9cc3f8c67
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:43:11 +0000

    Bug 466586 - Only expire cache elements that don't have any proxies/observers.  r=stuart sr=vlad a=blocking1.9.1+
    The image cache is now composed of two things: a heap of tracked items that
    can be expired and evicted, and a superset of that which hashes from URI to
    cache element. Cache elements start out in the second set, and are moved to
    the first (and start to be tracked for expiry) once they have no observers.
    
    UltraBlame original commit: a6b38a1f66241a06b91b0d7d974c02adcc4344d7

--- modules/libpr0n/src/imgLoader.h
+++ modules/libpr0n/src/imgLoader.h
@@ -61,11 +61,12 @@ class imgCacheEntry
 {
 public:
   imgCacheEntry(imgRequest *request, PRBool mustValidateIfExpired = PR_FALSE);
+  ~imgCacheEntry();
 
   nsrefcnt AddRef()
   {
     NS_PRECONDITION(PRInt32(mRefCnt) >= 0, "illegal refcnt");
-    NS_ASSERT_OWNINGTHREAD(imgCacheEntry);
+    NS_ABORT_IF_FALSE(_mOwningThread.GetThread() == PR_GetCurrentThread(), "imgCacheEntry addref isn't thread-safe!");
     ++mRefCnt;
     NS_LOG_ADDREF(this, mRefCnt, "imgCacheEntry", sizeof(*this));
     return mRefCnt;
@@ -74,7 +75,7 @@ public:
   nsrefcnt Release()
   {
     NS_PRECONDITION(0 != mRefCnt, "dup release");
-    NS_ASSERT_OWNINGTHREAD(imgCacheEntry);
+    NS_ABORT_IF_FALSE(_mOwningThread.GetThread() == PR_GetCurrentThread(), "imgCacheEntry release isn't thread-safe!");
     --mRefCnt;
     NS_LOG_RELEASE(this, mRefCnt, "imgCacheEntry");
     if (mRefCnt == 0) {
@@ -143,6 +144,11 @@ public:
     return &mExpirationState;
   }
 
+  PRBool HasNoProxies() const
+  {
+    return mHasNoProxies;
+  }
+
 private: 
   friend class imgLoader;
   friend class imgCacheQueue;
@@ -152,18 +158,24 @@ private:
   {
     mEvicted = evict;
   }
+  void SetHasNoProxies(PRBool hasNoProxies);
+
+  
+  imgCacheEntry(const imgCacheEntry &);
 
 private: 
   nsAutoRefCnt mRefCnt;
   NS_DECL_OWNINGTHREAD
 
   nsRefPtr<imgRequest> mRequest;
+  nsCOMPtr<nsIURI> mKeyURI;
   PRUint32 mDataSize;
   PRInt32 mTouchedTime;
   PRInt32 mExpiryTime;
   nsExpirationState mExpirationState;
-  PRBool mMustValidateIfExpired;
-  PRBool mEvicted;
+  PRPackedBool mMustValidateIfExpired : 1;
+  PRPackedBool mEvicted : 1;
+  PRPackedBool mHasNoProxies : 1;
 };
 
 #include <vector>
@@ -249,6 +261,20 @@ public:
   }
 
   static void VerifyCacheSizes();
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  static PRBool SetHasNoProxies(nsIURI *key, imgCacheEntry *entry);
+  static PRBool SetHasProxies(nsIURI *key);
 
 private: 
 
