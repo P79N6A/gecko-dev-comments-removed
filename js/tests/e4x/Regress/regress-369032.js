112c539149fb495b9e5297d285de7681faf87bda
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:48 +0000

    JavaScript Test - regression test for bug 369032, by Jesse Ruderman
    UltraBlame original commit: e074f47cacfb2b4d871774a59319d5b583c4d76d

---
+++ js/tests/e4x/Regress/regress-369032.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gTestfile = 'regress-369032.js';
+
+var summary = 'Assertion: kid2->parent == xml || !kid2->parent';
+var BUGNUMBER = 369032;
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+printBugNumber(BUGNUMBER);
+START(summary);
+
+var y = <y/>;
+var b = y.b;
+b.a = 3;
+var x = <x/>.appendChild(b);
+y.b = 5;
+
+expect = '<y>\n  <b>5</b>\n</y>';
+actual = y.toXMLString();
+TEST(1, expect, actual);
+
+expect = '<x>\n  <b>\n    <a>3</a>\n  </b>\n</x>';
+actual = x.toXMLString();
+TEST(2, expect, actual);
+
+END();
