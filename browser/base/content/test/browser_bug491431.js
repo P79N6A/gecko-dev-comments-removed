3aba080438db2c55c71a3f39d9b8f094626695d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:14:33 +0000

    Bug 491431 - No way to distinguish tab detach vs tab close, r=gavin
    UltraBlame original commit: 49174f70a4986299a632bb21bc0df7e83a9f8141

---
+++ browser/base/content/test/browser_bug491431.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+let testPage = "data:text/plain,test bug 491431 Page";
+
+function test() {
+  waitForExplicitFinish();
+
+  let newWin, tabA, tabB;
+
+  
+  tabA = gBrowser.addTab(testPage);
+  gBrowser.addEventListener("TabClose", function(aEvent) {
+    gBrowser.removeEventListener("TabClose", arguments.callee, true);
+    ok(!aEvent.detail, "This was a normal tab close");
+
+    
+    tabB = gBrowser.addTab(testPage);
+    gBrowser.addEventListener("TabClose", function(aEvent) {
+      gBrowser.removeEventListener("TabClose", arguments.callee, true);
+      executeSoon(function() {
+        ok(aEvent.detail, "This was a tab closed by moving");
+
+        
+        newWin.close();
+        executeSoon(finish);
+      });
+    }, true);
+    newWin = gBrowser.replaceTabWithWindow(tabB);
+  }, true);
+  gBrowser.removeTab(tabA);
+}
+
