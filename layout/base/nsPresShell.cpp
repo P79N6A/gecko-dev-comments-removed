860581ed5cb46037a94de849f47eaaa457872299
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:05:55 +0000

    Bug 370436 - "Context menu from keyboard for spell checker selects the wrong line" [r=roc]
    UltraBlame original commit: e3dd7678ff3f47409d7d28333104eecc68f9e619

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -6776,6 +6776,10 @@ PresShell::PrepareToUseCaretPosition(nsIWidget* aEventWidget, nsIntPoint& aTarge
   aTargetPt.x = presContext->AppUnitsToDevPixels(viewDelta.x + caretCoords.x + caretCoords.width);
   aTargetPt.y = presContext->AppUnitsToDevPixels(viewDelta.y + caretCoords.y + caretCoords.height);
 
+  
+  
+  aTargetPt.y -= 1;
+
   return PR_TRUE;
 }
 
