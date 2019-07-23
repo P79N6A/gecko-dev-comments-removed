4dd8f47fe538be39997f131104cc2bb6f312d1b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:51:19 +0000

    Nanojit merge death throes, no bug, r=dvander.
    UltraBlame original commit: a658f130ad09ca1050268557f401289ca8b3fb35

--- js/src/nanojit/nanojit.h
+++ js/src/nanojit/nanojit.h
@@ -72,7 +72,7 @@
 
 
 #if !defined(ARM_VFP)
-    #if defined(AVMPLUS_ARM)
+    #ifdef AVMPLUS_ARM
         #if defined(NJ_ARM_VFP)
             #define ARM_VFP      1
         #else
@@ -81,7 +81,7 @@
     #else
         
         
-        #define ARM_VFP          1
+        #define ARM_VFP 1
     #endif
 #endif
 
