701d1bbf32cc432c546d0e44a8a12f98d02eccb4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:38:29 +0000

    Trace String.fromCharCode. Handle failure signals of < 0 or NULL from traceable natives.
    UltraBlame original commit: 0793c5db30463c567e6ae7e5fae7b2a9675b38b2

--- js/src/jsbuiltins.cpp
+++ js/src/jsbuiltins.cpp
@@ -187,11 +187,38 @@ builtin_String_p_substring_1(JSContext *cx, JSString *str, jsint begin)
 }
 
 JSString* FASTCALL
-builtin_ConcatStrings(JSContext *cx, JSString* left, JSString* right)
+builtin_ConcatStrings(JSContext* cx, JSString* left, JSString* right)
 {
     
-    
     return js_ConcatStrings(cx, left, right);
+}
+
+JSString* FASTCALL
+builtin_String_getelem(JSContext* cx, JSString* str, jsint i)
+{
+    if ((size_t)i >= JSSTRING_LENGTH(str))
+        return NULL;
+    
+    str = js_GetUnitString(cx, str, (size_t)i);
+    return str;
+}
+
+JSString* FASTCALL
+builtin_String_fromCharCode(JSContext* cx, jsint i)
+{
+    jschar c = (jschar)i;
+    
+    
+    
+    return js_NewStringCopyN(cx, &c, 1);
+}
+
+jsint FASTCALL
+builtin_String_p_charCodeAt(JSString* str, jsint i)
+{
+    if (i < 0 || (jsint)JSSTRING_LENGTH(str) <= i)
+        return -1;
+    return JSSTRING_CHARS(str)[i];
 }
 
 #define LO ARGSIZE_LO
