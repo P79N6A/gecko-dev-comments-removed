509b9a6b5c3eef30ddbdbd45079cb3e499c08e94
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:55 +0000

    Add unit test for bug 378839 and remove obsolete test programs
    UltraBlame original commit: 5bdbf35f58d9118399437cbeaac0634da84a0223

---
+++ intl/strres/tests/unit/test_bug378839.js
@@ -0,0 +1,61 @@
+
+
+
+const name_file = "file";
+const value_file = "File";
+
+const name_loyal = "loyal";
+const value_loyal = "\u5fe0\u5fc3"; 
+
+const name_trout = "trout";
+const value_trout = "\u9cdf\u9b5a"; 
+
+const name_edit = "edit";
+const value_edit = "Edit"; 
+
+const name_view = "view";
+const value_view = "View"; 
+
+const name_go = "go";
+const value_go = " Go"; 
+
+const name_message = "message";
+const value_message = "Message "; 
+
+const name_hello = "hello";
+const var_hello = "World";
+const value_hello = "Hello World"; 
+
+
+function run_test() {
+    var StringBundle = 
+	Components.classes["@mozilla.org/intl/stringbundle;1"]
+	 .getService(Components.interfaces.nsIStringBundleService);
+
+    var bundle = StringBundle.createBundle("resource://gre/res/strres.properties");
+
+    var bundle_file = bundle.GetStringFromName(name_file);
+    do_check_eq(bundle_file, value_file);
+
+    var bundle_loyal = bundle.GetStringFromName(name_loyal);
+    do_check_eq(bundle_loyal, value_loyal);
+
+    var bundle_trout = bundle.GetStringFromName(name_trout);
+    do_check_eq(bundle_trout, value_trout);
+
+    var bundle_edit = bundle.GetStringFromName(name_edit);
+    do_check_eq(bundle_edit, value_edit);
+
+    var bundle_view = bundle.GetStringFromName(name_view);
+    do_check_eq(bundle_view, value_view);
+
+    var bundle_go = bundle.GetStringFromName(name_go);
+    do_check_eq(bundle_go, value_go);
+
+    var bundle_message = bundle.GetStringFromName(name_message);
+    do_check_eq(bundle_message, value_message);
+
+    var bundle_hello = bundle.formatStringFromName(name_hello, [var_hello], 1);
+    do_check_eq(bundle_hello, value_hello);
+}
+    
