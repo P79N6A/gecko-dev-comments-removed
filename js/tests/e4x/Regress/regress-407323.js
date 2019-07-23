624b55965a01e9e85bb473bf92086af2304f916c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:55:12 +0000

    JavaScript Tests - regression tests for bug 407323, not part of the build
    UltraBlame original commit: 86e254efab51f80fa9e083907f27e82f5d97dc34

---
+++ js/tests/e4x/Regress/regress-407323.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gTestfile = 'regress-407323.js';
+
+var summary = 'XML, XMLList, QName are mutable, Namespace is not.';
+var BUGNUMBER = 407323;
+var actual = '';
+var expect = '';
+
+printBugNumber(BUGNUMBER);
+START(summary);
+
+var obj           = {};
+var saveQName     = QName;
+var saveXML       = XML;
+var saveXMLList   = XMLList;
+var saveNamespace = Namespace;
+
+QName = obj;
+TEST(1, obj, QName);
+
+XML = obj;
+TEST(2, obj, XML);
+
+XMLList = obj;
+TEST(3, obj, XMLList);
+
+Namespace = {};
+TEST(4, saveNamespace, Namespace);
+
+END();
---
+++ js/tests/js1_5/Regress/regress-407323.js
@@ -0,0 +1,75 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-407323.js';
+
+var BUGNUMBER = 407323;
+var summary = 'XML, XMLList, QName are mutable, Namespace is not.';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  var obj           = {};
+  var saveQName     = QName;
+  var saveXML       = XML;
+  var saveXMLList   = XMLList;
+  var saveNamespace = Namespace;
+
+  QName = obj;
+  reportCompare(obj, QName, summary + ': QName');
+
+  XML = obj;
+  reportCompare(obj, XML, summary + ': XML');
+
+  XMLList = obj;
+  reportCompare(obj, XMLList, summary + ': XMLList');
+
+  Namespace = {};
+  reportCompare(saveNamespace, Namespace, summary);
+
+  exitFunc ('test');
+}
