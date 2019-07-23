573c0e3862d01bce0d8fd462f41261f1ed1821a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:00:04 +0000

    Followup to bug 525090 - log new processes to a file based on the environment, instead of unconditionally to stdout, r=cjones
    UltraBlame original commit: ee79c580c4823cdf1f5609c706865c79e96658ba

--- ipc/chromium/src/base/debug_util.h
+++ ipc/chromium/src/base/debug_util.h
@@ -67,4 +67,22 @@ class DebugUtil {
 #endif  
 };
 
+namespace mozilla {
+
+class EnvironmentLog
+{
+public:
+  EnvironmentLog(const char* varname);
+  ~EnvironmentLog();
+
+  void print(const char* format, ...);
+
+private:
+  FILE* fd_;
+
+  DISALLOW_EVIL_CONSTRUCTORS(EnvironmentLog);
+};
+
+} 
+
 #endif  
