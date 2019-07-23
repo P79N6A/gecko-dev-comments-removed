14a4960a219c39423b9f7b43187732dc8524ab5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:45:06 +0000

    bug 378581 - merge platform implementations of crash reporter client.  Patch by Dave Camp <dcamp@mozilla.com>, r=me
    UltraBlame original commit: e7346ae77d75d070312c65e143ce581cb09904a4

--- toolkit/crashreporter/client/crashreporter_osx.h
+++ toolkit/crashreporter/client/crashreporter_osx.h
@@ -41,34 +41,48 @@
 
 #include <Cocoa/Cocoa.h>
 #include "HTTPMultipartUpload.h"
+#include "crashreporter.h"
 
 @interface CrashReporterUI : NSObject
 {
-    
-    IBOutlet NSView *enableView;
-
-    IBOutlet NSTextField *descriptionLabel;
-    IBOutlet NSButton *disableReportingButton;
-    IBOutlet NSButton *dontSendButton;
-    IBOutlet NSButton *sendButton;
+    IBOutlet NSWindow* window;
 
     
-    IBOutlet NSView *uploadingView;
+    IBOutlet NSView* enableView;
 
-    IBOutlet NSTextField *progressLabel;
-    IBOutlet NSProgressIndicator *progressBar;
-    IBOutlet NSButton *closeButton;
+    IBOutlet NSTextField* descriptionLabel;
+    IBOutlet NSButton* disableReportingButton;
+    IBOutlet NSButton* dontSendButton;
+    IBOutlet NSButton* sendButton;
+
+    
+    IBOutlet NSView* uploadingView;
+
+    IBOutlet NSTextField* progressLabel;
+    IBOutlet NSProgressIndicator* progressBar;
+    IBOutlet NSButton* closeButton;
+
+    
+    IBOutlet NSView* errorView;
+    IBOutlet NSTextField* errorLabel;
+    IBOutlet NSButton* errorCloseButton;
 
     HTTPMultipartUpload *mPost;
 }
+
+- (void)showDefaultUI;
+- (void)showCrashUI:(const std::string&)dumpfile
+    queryParameters:(const StringTable&)queryParameters
+            sendURL:(const std::string&)sendURL;
+- (void)showErrorUI:(const std::string&)dumpfile;
 
 - (IBAction)closeClicked:(id)sender;
 - (IBAction)sendClicked:(id)sender;
 
 - (void)setView:(NSWindow *)w newView: (NSView *)v animate: (BOOL) animate;
-- (void)setupPost;
+- (bool)setupPost;
 - (void)uploadThread:(id)post;
-- (void)uploadComplete:(id)error;
+- (void)uploadComplete:(id)data;
 
 @end
 
