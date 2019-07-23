d36346faa59a48539af722c0cb5c397a6d2f24e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:50:40 +0000

    Bug 510082. Silverlight depends on NPN_InvalidateRect working even when the plugin is in windowed mode (although it should really just call Win32 APIs to invalidate its window), so make NPN_InvalidateRect work for windowed plugins. r=josh
    UltraBlame original commit: 650ad2ba0092c75f36a98c034938dc7f9d622cb0

--- modules/plugin/test/testplugin/nptest_gtk2.cpp
+++ modules/plugin/test/testplugin/nptest_gtk2.cpp
@@ -155,6 +155,8 @@ pluginDrawWindow(InstanceData* instanceData, GdkDrawable* gdkWindow)
   int width = window.width;
   int height = window.height;
 
+  ++instanceData->paintCount;
+
   if (instanceData->scriptableObject->drawMode == DM_SOLID_COLOR) {
     
     pluginDrawSolid(instanceData, gdkWindow, x, y, width, height);
