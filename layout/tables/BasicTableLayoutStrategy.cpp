6649c48d2ed4c6476c75733360b3417183ab3f6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:12:59 +0000

    Bug 428521: Allow DistributeWidthToColumns to complete BTLS_PREF_WIDTH runs in situations with guess_min < aWidth <= guess_pref.  (Reverts optimization from 368504 in which I'd thought that a run with aWidth <= guess_pref would be skippable) r+sr=dbaron a=beltzner
    UltraBlame original commit: b02d19d9d434d5f00c14d372066a2ef096af5254

--- layout/tables/BasicTableLayoutStrategy.cpp
+++ layout/tables/BasicTableLayoutStrategy.cpp
@@ -774,8 +774,7 @@ BasicTableLayoutStrategy::DistributeWidthToColumns(nscoord aWidth,
         float f;
     } basis; 
     if (aWidth < guess_pref) {
-        if ((aWidthType == BTLS_MIN_WIDTH  && aWidth <= guess_min) ||
-            (aWidthType == BTLS_PREF_WIDTH && aWidth <= guess_pref)) {
+        if (aWidthType != BTLS_FINAL_WIDTH && aWidth <= guess_min) {
             
             return;
         }
