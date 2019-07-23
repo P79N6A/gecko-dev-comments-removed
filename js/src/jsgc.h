5e4c647df75332af7dc3784d24541dca39441d33
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:58:57 +0000

    Backed out changeset e117c22cc1d1 - the landed patch for bug 437325 has a shutdown leak.
    UltraBlame original commit: 124a3d626d1e059fc0c13ff7549df8e2a69e6b34

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -340,9 +340,6 @@ extern const JSGCFreeListSet js_GCEmptyFreeListSet;
 extern void
 js_RevokeGCLocalFreeLists(JSContext *cx);
 
-extern void
-js_DestroyScriptsToGC(JSContext *cx, JSThreadData *data);
-
 struct JSWeakRoots {
     
     void            *newborn[GCX_NTYPES];
