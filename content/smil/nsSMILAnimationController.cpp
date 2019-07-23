24f7f03f779dd75a0a0507fdb4ef219b97d87874
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:12:08 +0000

    Remove incorrect usage of nsresult to catch return-val of an EnumerateEntries() call in nsSMILAnimationController. (trivial fix, no bug #)
    UltraBlame original commit: 46663814d7644f25067c4fa9c30fd6ba3fc70de8

--- content/smil/nsSMILAnimationController.cpp
+++ content/smil/nsSMILAnimationController.cpp
@@ -335,11 +335,8 @@ nsSMILAnimationController::DoSample(PRBool aSkipUnchangedContainers)
 
   SampleAnimationParams saParams = { &activeContainers,
                                      currentCompositorTable };
-  nsresult rv = mAnimationElementTable.EnumerateEntries(SampleAnimation,
-                                                        &saParams);
-  if (NS_FAILED(rv)) {
-    NS_WARNING("SampleAnimationParams failed");
-  }
+  mAnimationElementTable.EnumerateEntries(SampleAnimation,
+                                          &saParams);
   activeContainers.Clear();
 
   
