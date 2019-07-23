f827ba4cc17be3e7d182e3ce04d22f465105eb0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:40:24 +0000

    Bug 477759 - stop using fp:fast as a build option on windows mobile. r=blassey
    UltraBlame original commit: 541fc9b1a7444a520bbf4513d2f95bc2164a71d8

--- build/wince/tools/arm-wince-gcc.c
+++ build/wince/tools/arm-wince-gcc.c
@@ -34,7 +34,6 @@ main(int argc, char **argv)
   args[i++] = "/Zc:wchar_t-";          
   args[i++] = "/GS-";                  
   args[i++] = "/GR-";                  
-  args[i++] = "/fp:fast";
 
   startOfArgvs = i;
 
