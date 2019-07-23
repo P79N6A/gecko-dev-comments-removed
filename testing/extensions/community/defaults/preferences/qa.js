084bfa26f1062784e3f9ab6ae119d7173c6fafed
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:48:36 +0000

    Bug 395466 Checking in patch by Ben_H for QA Extension
    UltraBlame original commit: 79e8c861a0dd70389b8ea77092fbff989b24cd2f

--- testing/extensions/community/defaults/preferences/qa.js
+++ testing/extensions/community/defaults/preferences/qa.js
@@ -1,5 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 pref("qa.extension.litmus.url", "http://litmus.mozilla.org/");
 pref("qa.extension.hermes.url", "http://litmus.mozilla.org/hermes/1/");
+pref("qa.extension.bugzilla.url", "http:/bugzilla.mozilla.org/");
 pref("qa.extension.isFirstTime", true);
 pref("qa.extension.minNotificationInterval", 5400000); 
 pref("qa.extension.lastNotificationTime", 0);
