434a36ef8be7c3c05990ee3e5b749b7cefb87879
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:11:38 +0000

    Fixing bug 489251. Crash on shutdown after touching window.java. r+sr=mrbkap@gmail.com
    UltraBlame original commit: 869fd52a1854048f74dcd65550e0fc5ed2994486

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -73,6 +73,7 @@
 #include "nsPluginArray.h"
 #include "nsIPluginHost.h"
 #include "nsPIPluginHost.h"
+#include "nsIPluginInstancePeer2.h"
 #include "nsGeolocation.h"
 #include "nsContentCID.h"
 #include "nsLayoutStatics.h"
@@ -432,6 +433,15 @@ nsDummyJavaPluginOwner::Destroy()
   if (mInstance) {
     mInstance->Stop();
     mInstance->Destroy();
+
+    nsCOMPtr<nsIPluginInstancePeer> peer;
+    mInstance->GetPeer(getter_AddRefs(peer));
+
+    nsCOMPtr<nsIPluginInstancePeer2> peer2(do_QueryInterface(peer));
+
+    
+    if (peer2)
+      peer2->InvalidateOwner();
 
     mInstance = nsnull;
   }
@@ -5793,6 +5803,14 @@ nsGlobalWindow::InitJavaProperties()
   }
 
   host->InstantiateDummyJavaPlugin(mDummyJavaPluginOwner);
+
+  
+  
+  
+  
+  if (!mDummyJavaPluginOwner) {
+    return;
+  }
 
   nsCOMPtr<nsIPluginInstance> dummyPlugin;
   mDummyJavaPluginOwner->GetInstance(*getter_AddRefs(dummyPlugin));
