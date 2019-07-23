e391abffa17069b653a04a9473ea8c44df9553a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:08:09 +0000

    Bug 462389 - 'XPCVariant used in nsXPCException::SetThrownJSVal can cause cycle collection on non-main threads'. r=bent, sr=jst.
    UltraBlame original commit: dc1aff36a411534b33fd8ba9922eb3149fe53cbb

---
+++ js/src/xpconnect/public/nsAutoJSValHolder.h
@@ -0,0 +1,164 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef __NSAUTOJSVALHOLDER_H__
+#define __NSAUTOJSVALHOLDER_H__
+
+#include "jsapi.h"
+
+
+
+
+
+
+class nsAutoJSValHolder
+{
+public:
+
+  nsAutoJSValHolder()
+    : mRt(NULL)
+    , mVal(JSVAL_NULL)
+    , mGCThing(NULL)
+    , mHeld(JS_FALSE)
+  {
+    
+  }
+
+  
+
+
+  virtual ~nsAutoJSValHolder() {
+    Release();
+  }
+
+  
+
+
+  JSBool Hold(JSContext* aCx) {
+    return Hold(JS_GetRuntime(aCx));
+  }
+
+  
+
+
+
+  JSBool Hold(JSRuntime* aRt) {
+    if (!mHeld) {
+      if (JS_AddNamedRootRT(aRt, &mGCThing, "nsAutoJSValHolder")) {
+        mRt = aRt;
+        mHeld = JS_TRUE;
+      } else {
+        Release(); 
+      }
+    }
+    return mHeld;
+  }
+
+  
+
+
+
+  jsval Release() {
+    NS_ASSERTION(!mHeld || mRt, "Bad!");
+
+    jsval oldval = mVal;
+
+    if (mHeld) {
+      JS_RemoveRootRT(mRt, &mGCThing); 
+      mHeld = JS_FALSE;
+    }
+
+    mVal = JSVAL_NULL;
+    mGCThing = NULL;
+    mRt = NULL;
+
+    return oldval;
+  }
+
+  
+
+
+  JSBool IsHeld() {
+    return mHeld;
+  }
+
+  
+
+
+  operator JSObject*() const {
+    return JSVAL_IS_OBJECT(mVal)
+         ? JSVAL_TO_OBJECT(mVal)
+         : JSVAL_NULL;
+  }
+
+  
+
+
+  operator jsval() const { return mVal; }
+
+  nsAutoJSValHolder &operator=(JSObject* aOther) {
+#ifdef DEBUG
+    if (aOther) {
+      NS_ASSERTION(mHeld, "Not rooted!");
+    }
+#endif
+    return *this = OBJECT_TO_JSVAL(aOther);
+  }
+
+  nsAutoJSValHolder &operator=(jsval aOther) {
+#ifdef DEBUG
+    if (aOther) {
+      NS_ASSERTION(mHeld, "Not rooted!");
+    }
+#endif
+    mVal = aOther;
+    mGCThing = JSVAL_IS_GCTHING(aOther)
+             ? JSVAL_TO_GCTHING(aOther)
+             : NULL;
+    return *this;
+  }
+
+private:
+  JSRuntime* mRt;
+  jsval mVal;
+  void* mGCThing;
+  JSBool mHeld;
+};
+
+#endif 
