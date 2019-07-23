87fcab7df7ffca9f3d3c597a5b542b1180adf1f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:39 +0000

    Change the URI argument to Components.utils.import to be a resource: URI.  Bug380970, patch by Alex Vincent <ajvincent@gmail.com>, r=sayrer, sr=bsmedberg
    UltraBlame original commit: cfcdd8d9684ad409e3b5e782007c8fecc41f7968

--- js/src/xpconnect/loader/mozJSComponentLoader.cpp
+++ js/src/xpconnect/loader/mozJSComponentLoader.cpp
@@ -71,6 +71,7 @@
 #ifndef XPCONNECT_STANDALONE
 #include "nsIScriptSecurityManager.h"
 #include "nsIURI.h"
+#include "nsIFileURL.h"
 #include "nsNetUtil.h"
 #endif
 #include "jsxdrapi.h"
@@ -1389,17 +1390,26 @@ mozJSComponentLoader::ImportInto(const nsACString & aLocation,
         NS_ENSURE_SUCCESS(rv, rv);
     }
     
-    
-    
-    
-    nsCOMPtr<nsIComponentManager> cm;
-    rv = NS_GetComponentManager(getter_AddRefs(cm));
+    nsCOMPtr<nsIIOService> ioService = do_GetIOService(&rv);
     NS_ENSURE_SUCCESS(rv, rv);
-    nsCOMPtr<nsIComponentManagerObsolete> mgr = do_QueryInterface(cm, &rv);
+
+    nsCAutoString scheme;
+    rv = ioService->ExtractScheme(aLocation, scheme);
+    if (NS_FAILED(rv) ||
+        !scheme.EqualsLiteral("resource")) {
+      *_retval = nsnull;
+      return NS_ERROR_INVALID_ARG;
+    }
+
+    
+    nsCOMPtr<nsIURI> resURI;
+    rv = ioService->NewURI(aLocation, nsnull, nsnull, getter_AddRefs(resURI));
+    nsCOMPtr<nsIFileURL> fileURL = do_QueryInterface(resURI, &rv);
     NS_ENSURE_SUCCESS(rv, rv);
+
+    
     nsCOMPtr<nsIFile> file;
-    rv = mgr->SpecForRegistryLocation(PromiseFlatCString(aLocation).get(),
-                                      getter_AddRefs(file));
+    rv = fileURL->GetFile(getter_AddRefs(file));
     NS_ENSURE_SUCCESS(rv, rv);
     nsCOMPtr<nsILocalFile> componentFile = do_QueryInterface(file, &rv);
     NS_ENSURE_SUCCESS(rv, rv);
