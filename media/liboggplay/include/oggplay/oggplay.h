646bc034a1aa0cb28161cff59633db0e7e68ec21
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:36:11 +0000

    Bug 512398 - fix liboggplay update (rev f4087c5ac148) which accidentally removed liboggplay's os2_semaphore.*. r=doublec
    UltraBlame original commit: fb6d235b9efb55e597d61f44e28b4eed39f2c245

--- media/liboggplay/include/oggplay/oggplay.h
+++ media/liboggplay/include/oggplay/oggplay.h
@@ -37,6 +37,9 @@
 
 
 
+
+
+ 
 #ifndef __OGGPLAY_H__
 #define __OGGPLAY_H__
 
@@ -46,16 +49,38 @@ extern "C" {
 
 #include <oggplay/oggplay_enums.h>
 #include <oggplay/oggplay_reader.h>
+ 
+
+
 
 typedef struct _OggPlay OggPlay;
+
+
+
+
+
 typedef struct _OggPlayCallbackInfo OggPlayCallbackInfo;
-typedef int (OggPlayDataCallback)(OggPlay *player, int num_records,
-                OggPlayCallbackInfo **records, void *user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+typedef int (OggPlayDataCallback) (OggPlay *player, int num_records, 
+                                   OggPlayCallbackInfo **records, void *user);
 
 #include <oggplay/oggplay_query.h>
 #include <oggplay/oggplay_callback_info.h>
 #include <oggplay/oggplay_tools.h>
 #include <oggplay/oggplay_seek.h>
+
 
 
 
@@ -80,15 +105,43 @@ oggplay_open_with_reader(OggPlayReader *reader);
 OggPlay *
 oggplay_new_with_reader(OggPlayReader *reader);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 OggPlayErrorCode
 oggplay_initialise(OggPlay *me, int block);
 
-OggPlayErrorCode
-oggplay_set_source(OggPlay *OS, char *source);
+
+
+
+
+
+
+
+
+
+
 
 OggPlayErrorCode
 oggplay_set_data_callback(OggPlay *me, OggPlayDataCallback callback, 
-                            void *user);
+                          void *user);
+
 
 OggPlayErrorCode
 oggplay_set_callback_num_frames(OggPlay *me, int stream, int frames);
@@ -99,17 +152,80 @@ oggplay_set_callback_period(OggPlay *me, int stream, int milliseconds);
 OggPlayErrorCode
 oggplay_set_offset(OggPlay *me, int track, ogg_int64_t offset);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 OggPlayErrorCode
 oggplay_get_video_y_size(OggPlay *me, int track, int *y_width, int *y_height);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 OggPlayErrorCode
 oggplay_get_video_uv_size(OggPlay *me, int track, int *uv_width, int *uv_height);
 
+
+
+
+
+
+
+
+
+
+
+
+
 OggPlayErrorCode
 oggplay_get_audio_channels(OggPlay *me, int track, int *channels);
 
+
+
+
+
+
+
+
+
+
+
+
+
 OggPlayErrorCode
 oggplay_get_audio_samplerate(OggPlay *me, int track, int *samplerate); 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 OggPlayErrorCode
 oggplay_get_video_fps(OggPlay *me, int track, int* fps_denom, int* fps_num);
@@ -118,7 +234,7 @@ OggPlayErrorCode
 oggplay_get_video_aspect_ratio(OggPlay *me, int track, int* aspect_denom, int* aspect_num);
 
 OggPlayErrorCode
-oggplay_convert_video_to_rgb(OggPlay *me, int track, int convert);
+oggplay_convert_video_to_rgb(OggPlay *me, int track, int convert, int swap_rgb);
 
 OggPlayErrorCode
 oggplay_get_kate_category(OggPlay *me, int track, const char** category);
@@ -127,7 +243,7 @@ OggPlayErrorCode
 oggplay_get_kate_language(OggPlay *me, int track, const char** language);
 
 OggPlayErrorCode
-oggplay_set_kate_tiger_rendering(OggPlay *me, int track, int use_tiger);
+oggplay_set_kate_tiger_rendering(OggPlay *me, int track, int use_tiger, int swap_rgb, int default_width, int default_height);
 
 OggPlayErrorCode
 oggplay_overlay_kate_track_on_video(OggPlay *me, int kate_track, int video_track);
@@ -135,14 +251,71 @@ oggplay_overlay_kate_track_on_video(OggPlay *me, int kate_track, int video_track
 OggPlayErrorCode
 oggplay_start_decoding(OggPlay *me);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 OggPlayErrorCode
 oggplay_step_decoding(OggPlay *me);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 OggPlayErrorCode
 oggplay_use_buffer(OggPlay *player, int size);
 
+
+
+
+
+
+
+
 OggPlayCallbackInfo **
 oggplay_buffer_retrieve_next(OggPlay *player);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 OggPlayErrorCode
 oggplay_buffer_release(OggPlay *player, OggPlayCallbackInfo **track_info);
@@ -162,6 +335,13 @@ oggplay_close(OggPlay *player);
 
 int
 oggplay_get_available(OggPlay *player);
+
+
+
+
+
+
+
 
 ogg_int64_t
 oggplay_get_duration(OggPlay * player);
--- media/liboggplay/include/oggplay/oggplay_seek.h
+++ media/liboggplay/include/oggplay/oggplay_seek.h
@@ -36,8 +36,19 @@
 
 
 
+
 #ifndef __OGGPLAY_SEEK_H__
 #define __OGGPLAY_SEEK_H__
+
+
+
+
+
+
+
+
+
+
 
 OggPlayErrorCode
 oggplay_seek(OggPlay *me, ogg_int64_t milliseconds);
