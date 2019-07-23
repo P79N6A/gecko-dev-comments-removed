57924887496d429c905a290acd525c13e4b21ae6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:31:40 +0000

    Bug 428465 - nsTArray should refuse to compile with T=nsAutoString (or work correctly).  r=bsmedberg
    UltraBlame original commit: 9d3f9621b2f2754fddb1136f3ca0e0c9d6e36864

--- xpcom/string/public/nsTString.h
+++ xpcom/string/public/nsTString.h
@@ -540,6 +540,34 @@ class NS_STACK_CLASS nsTAutoString_CharT : public nsTFixedString_CharT
 
 
   
+  
+  
+  
+  
+  template<class E> class nsTArrayElementTraits;
+  template<>
+  class nsTArrayElementTraits<nsTAutoString_CharT> {
+    public:
+      template<class A> struct Dont_Instantiate_nsTArray_of;
+      template<class A> struct Instead_Use_nsTArray_of;;
+
+      static Dont_Instantiate_nsTArray_of<nsTAutoString_CharT> *
+      Construct(Instead_Use_nsTArray_of<nsTString_CharT> *e) {
+        return 0;
+      }
+      template<class A>
+      static Dont_Instantiate_nsTArray_of<nsTAutoString_CharT> *
+      Construct(Instead_Use_nsTArray_of<nsTString_CharT> *e,
+                const A &arg) {
+        return 0;
+      }
+      static Dont_Instantiate_nsTArray_of<nsTAutoString_CharT> *
+      Destruct(Instead_Use_nsTArray_of<nsTString_CharT> *e) {
+        return 0;
+      }
+  };
+
+  
 
 
 
