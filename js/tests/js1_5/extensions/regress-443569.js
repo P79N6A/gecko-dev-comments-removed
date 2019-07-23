ce982ec6c73bc5d30ec3a87168dea3ce7e5225cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:27:58 +0000

    JavaScript Tests - regression test for bug 443569, by Jesse Ruderman
    UltraBlame original commit: d634d3d4388e3114a96b61b13018ee1bbd8b2792

---
+++ js/tests/js1_5/extensions/regress-443569.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-443569-01.js';
+
+var BUGNUMBER = 443569;
+var summary = 'Do not assert: OBJ_IS_NATIVE(obj)';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+printBugNumber(BUGNUMBER);
+printStatus (summary);
+
+if (typeof window != 'undefined')
+{
+  
+  gDelayTestDriverEnd = true;
+
+  window.addEventListener("load", boom, false);
+}
+else
+{
+  reportCompare(expect, actual, summary);
+}
+
+function boom()
+{
+  var r = RegExp.prototype;
+  r["-1"] = 0;
+  Array.prototype.__proto__ = r;
+  []["-1"];
+
+  reportCompare(expect, actual, summary);
+
+  gDelayTestDriverEnd = false;
+  jsTestDriverEnd();
+}
+
+
