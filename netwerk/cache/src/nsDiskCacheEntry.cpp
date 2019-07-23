3b67f5fd6571f52e00172bcbf957bd9420419073
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:56:26 +0000

    relanding fix for bug 262116 that got accidentally backed out by previous landing.
    UltraBlame original commit: e5b11400fcaecc5be1d1ea8e166767d78be91ee1

--- netwerk/cache/src/nsDiskCacheEntry.cpp
+++ netwerk/cache/src/nsDiskCacheEntry.cpp
@@ -46,6 +46,8 @@
 
 #include "nsCache.h"
 
+#include "nsISerializable.h"
+#include "nsSerializationHelper.h"
 
 
 
@@ -80,7 +82,15 @@ nsDiskCacheEntry::CreateCacheEntry(nsCacheDevice *  device)
         delete entry;
         return nsnull;
     }
+
     
+    const char* info = entry->GetMetaDataElement("security-info");
+    if (info) {
+        nsCOMPtr<nsISupports> infoObj;
+        NS_DeserializeObject(nsDependentCString(info), getter_AddRefs(infoObj));
+        entry->SetSecurityInfo(infoObj);
+    }
+
     return entry;                      
 }
 
