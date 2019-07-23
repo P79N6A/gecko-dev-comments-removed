953472089f510641daf26a7a49144e12373e7aa4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:28 +0000

    JavaScript Test - regression test for bug 352360, by Jesse Ruderman
    UltraBlame original commit: e926d0074b307539fd579310bccbe51724c16638

---
+++ js/tests/js1_5/decompilation/regress-352360.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 352360;
+var summary = 'Decompilation of negative 0';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  var f = function() { return -0 };
+  expect = 'function() { return -0; }';
+  actual = f + '';
+  compareSource(expect, actual, summary);
+
+  expect = -Infinity;
+  actual = 8 / f();
+  reportCompare(expect, actual, summary + ': 8 / f()');
+
+  expect = -Infinity;
+  actual = 8 / eval('' + f)();
+  reportCompare(expect, actual, summary + ': 8 / eval("" + f)()');
+
+  exitFunc ('test');
+}
