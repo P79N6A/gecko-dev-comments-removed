72b8374445ee80455a575768083866e2837c1403
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:12:12 +0000

    Bug 427559 - Gmail keyboard commands don't work after switching tabs without reclicking the page. r=gavin, r=Mardak, b-ff3=beltzner, a1.9=mconnor
    UltraBlame original commit: e423b453cf77556fb0d02952b54253d0abba56e8

---
+++ browser/base/content/test/browser_bug427559.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let testPage = 'data:text/html,<body><button onblur="this.parentNode.removeChild(this);"><script>document.body.firstChild.focus();</script></body>';
+
+function test() {
+  waitForExplicitFinish();
+
+  
+  let testTab = gBrowser.addTab();
+  gBrowser.selectedTab = testTab;
+  let testBrowser = gBrowser.getBrowserForTab(testTab);
+
+  testBrowser.addEventListener("load", function() {
+    
+    
+    
+    let emptyTab = gBrowser.addTab();
+    gBrowser.selectedTab = emptyTab;
+    gBrowser.removeCurrentTab();
+    gBrowser.selectedTab = testTab;
+
+    
+    is(document.commandDispatcher.focusedWindow, window.content,
+       "content window is focused");
+    gBrowser.removeCurrentTab();
+    finish();
+  }, true);
+
+  
+  testBrowser.contentWindow.location = testPage;
+}
