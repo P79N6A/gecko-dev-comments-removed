d8a45243a9f676b7a16780f01de8346b028bd1c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:01:17 +0000

    Bug 461625 - Hide the UI for saving permission manager entries in Private Browsing mode (test); r=mconnor
    UltraBlame original commit: eb9dc9dc24fa478237ed9c3f24a8a018389ee208

---
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_popupblocker.js
@@ -0,0 +1,97 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  let pb = Cc["@mozilla.org/privatebrowsing;1"].
+           getService(Ci.nsIPrivateBrowsingService);
+
+  let oldPopupPolicy = gPrefService.getBoolPref("dom.disable_open_during_load");
+  gPrefService.setBoolPref("dom.disable_open_during_load", true);
+
+  const TEST_URI = "http://localhost:8888/browser/browser/components/privatebrowsing/test/browser/popup.html";
+
+  waitForExplicitFinish();
+
+  function testPopupBlockerMenuItem(expectedDisabled, callback) {
+    gBrowser.addEventListener("DOMUpdatePageReport", function() {
+      gBrowser.removeEventListener("DOMUpdatePageReport", arguments.callee, false);
+      executeSoon(function() {
+        let pageReportButton = document.getElementById("page-report-button");
+        let notification = gBrowser.getNotificationBox().getNotificationWithValue("popup-blocked");
+
+        ok(!pageReportButton.hidden, "The page report button should not be hidden");
+        ok(notification, "The notification box should be displayed");
+
+        function checkMenuItem(callback) {
+          document.addEventListener("popupshown", function(event) {
+            document.removeEventListener("popupshown", arguments.callee, false);
+
+            if (expectedDisabled)
+              is(document.getElementById("blockedPopupAllowSite").getAttribute("disabled"), "true",
+                 "The allow popups menu item should be disabled");
+
+            event.originalTarget.hidePopup();
+            callback();
+          }, false);
+        }
+
+        checkMenuItem(function() {
+          checkMenuItem(function() {
+            gBrowser.removeTab(tab);
+            callback();
+          });
+          notification.querySelector("button").doCommand();
+        });
+        EventUtils.synthesizeMouse(document.getElementById("page-report-button"), 1, 1, {});
+      });
+    }, false);
+
+    let tab = gBrowser.addTab(TEST_URI);
+    gBrowser.selectedTab = tab;
+  }
+
+  pb.privateBrowsingEnabled = true;
+  testPopupBlockerMenuItem(true, function() {
+    pb.privateBrowsingEnabled = false;
+    gPrefService.setBoolPref("dom.disable_open_during_load", oldPopupPolicy);
+    finish();
+  });
+}
