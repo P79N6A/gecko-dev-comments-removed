5654c342ea4569683309aeae0d54c1f8330c73a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:59:06 +0000

    Bug 408113: allocate stackPools less often, r=brendan, a=blocking1.9 (schrep)
    UltraBlame original commit: baba618bf0985a08d89a67af91c25be2530e5c3d

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -1101,8 +1101,14 @@ extern JS_PUBLIC_API(JSBool)
 JS_IsAboutToBeFinalized(JSContext *cx, void *thing);
 
 typedef enum JSGCParamKey {
-    JSGC_MAX_BYTES        = 0,  
-    JSGC_MAX_MALLOC_BYTES = 1   
+    
+    JSGC_MAX_BYTES          = 0,
+
+    
+    JSGC_MAX_MALLOC_BYTES   = 1,
+
+    
+    JSGC_STACKPOOL_LIFESPAN = 2
 } JSGCParamKey;
 
 extern JS_PUBLIC_API(void)
