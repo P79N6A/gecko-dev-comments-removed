b45317643d8f3c9deff5368926072d1db6b1fe1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:48:56 +0000

    Bug 395667 - New style nsSVGInteger. r=tor,sr=roc,a=roc
    UltraBlame original commit: 1f572df93bca364c0437a4d9efbe5657a0daa3c7

---
+++ content/svg/content/src/nsSVGInteger.cpp
@@ -0,0 +1,95 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsSVGInteger.h"
+#include "nsTextFormatter.h"
+#include "prdtoa.h"
+
+NS_IMPL_ADDREF(nsSVGInteger::DOMAnimatedInteger)
+NS_IMPL_RELEASE(nsSVGInteger::DOMAnimatedInteger)
+
+NS_INTERFACE_MAP_BEGIN(nsSVGInteger::DOMAnimatedInteger)
+  NS_INTERFACE_MAP_ENTRY(nsIDOMSVGAnimatedInteger)
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+  NS_INTERFACE_MAP_ENTRY_CONTENT_CLASSINFO(SVGAnimatedInteger)
+NS_INTERFACE_MAP_END
+
+
+
+nsresult
+nsSVGInteger::SetBaseValueString(const nsAString &aValueAsString,
+                                 nsSVGElement *aSVGElement,
+                                 PRBool aDoSetAttr)
+{
+  nsAutoString s;
+  s.Assign(aValueAsString);
+  PRInt32 err;
+  PRInt32 val = s.ToInteger(&err);
+  nsresult rv = static_cast<nsresult>(err);
+  NS_ENSURE_SUCCESS(rv, rv);
+  mBaseVal = mAnimVal = val;
+  return NS_OK;
+}
+
+void
+nsSVGInteger::GetBaseValueString(nsAString & aValueAsString)
+{
+  nsAutoString s;
+  s.AppendInt(mBaseVal);
+  aValueAsString.Assign(s);
+}
+
+void
+nsSVGInteger::SetBaseValue(int aValue,
+                           nsSVGElement *aSVGElement,
+                           PRBool aDoSetAttr)
+{
+  mAnimVal = mBaseVal = aValue;
+  aSVGElement->DidChangeInteger(mAttrEnum, aDoSetAttr);
+}
+
+nsresult
+nsSVGInteger::ToDOMAnimatedInteger(nsIDOMSVGAnimatedInteger **aResult,
+                                   nsSVGElement *aSVGElement)
+{
+  *aResult = new DOMAnimatedInteger(this, aSVGElement);
+  if (!*aResult)
+    return NS_ERROR_OUT_OF_MEMORY;
+
+  NS_ADDREF(*aResult);
+  return NS_OK;
+}
+
---
+++ content/svg/content/src/nsSVGInteger.h
@@ -0,0 +1,93 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef __NS_SVGINTEGER_H__
+#define __NS_SVGINTEGER_H__
+
+#include "nsIDOMSVGAnimatedInteger.h"
+#include "nsSVGElement.h"
+#include "nsDOMError.h"
+
+class nsSVGInteger
+{
+
+public:
+  void Init(PRUint8 aAttrEnum = 0xff, PRInt32 aValue = 0) {
+    mAnimVal = mBaseVal = aValue;
+    mAttrEnum = aAttrEnum;
+  }
+
+  nsresult SetBaseValueString(const nsAString& aValue,
+                              nsSVGElement *aSVGElement,
+                              PRBool aDoSetAttr);
+  void GetBaseValueString(nsAString& aValue);
+
+  void SetBaseValue(PRInt32 aValue, nsSVGElement *aSVGElement, PRBool aDoSetAttr);
+  PRInt32 GetBaseValue() const
+    { return mBaseVal; }
+  PRInt32 GetAnimValue() const
+    { return mAnimVal; }
+
+  nsresult ToDOMAnimatedInteger(nsIDOMSVGAnimatedInteger **aResult,
+                                nsSVGElement* aSVGElement);
+
+private:
+
+  PRInt32 mAnimVal;
+  PRInt32 mBaseVal;
+  PRUint8 mAttrEnum; 
+
+  struct DOMAnimatedInteger : public nsIDOMSVGAnimatedInteger
+  {
+    NS_DECL_ISUPPORTS
+
+    DOMAnimatedInteger(nsSVGInteger* aVal, nsSVGElement *aSVGElement)
+      : mVal(aVal), mSVGElement(aSVGElement) {}
+
+    nsSVGInteger* mVal; 
+    nsRefPtr<nsSVGElement> mSVGElement;
+
+    NS_IMETHOD GetBaseVal(PRInt32* aResult)
+      { *aResult = mVal->GetBaseValue(); return NS_OK; }
+    NS_IMETHOD SetBaseVal(PRInt32 aValue)
+      { mVal->SetBaseValue(aValue, mSVGElement, PR_TRUE); return NS_OK; }
+    NS_IMETHOD GetAnimVal(PRInt32* aResult)
+      { *aResult = mVal->GetAnimValue(); return NS_OK; }
+
+  };
+
+};
+#endif 
