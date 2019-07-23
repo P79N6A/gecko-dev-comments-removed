54ae46d7bf411244377081f22230e594421b922d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:05:21 +0000

    Bug 487126: fix OS/2 and Qt test plugins build break, r=joshmoz
    UltraBlame original commit: 9d83ce1fb4b3d06bd10afef5125e8c098d84ab1d

--- modules/plugin/test/testplugin/nptest_os2.cpp
+++ modules/plugin/test/testplugin/nptest_os2.cpp
@@ -57,6 +57,12 @@ pluginInstanceShutdown(InstanceData* instanceData)
 }
 
 void
+pluginDoSetWindow(InstanceData* instanceData, NPWindow* newWindow)
+{
+  instanceData->window = *newWindow;
+}
+
+void
 pluginWidgetInit(InstanceData* instanceData, void* oldWindow)
 {
 }
@@ -65,4 +71,23 @@ int16_t
 pluginHandleEvent(InstanceData* instanceData, void* event)
 {
   return 0;
+}
+
+int32_t pluginGetEdge(InstanceData* instanceData, RectEdge edge)
+{
+  
+  return NPTEST_INT32_ERROR;
+}
+
+int32_t pluginGetClipRegionRectCount(InstanceData* instanceData)
+{
+  
+  return NPTEST_INT32_ERROR;
+}
+
+int32_t pluginGetClipRegionRectEdge(InstanceData* instanceData, 
+    int32_t rectIndex, RectEdge edge)
+{
+  
+  return NPTEST_INT32_ERROR;
 }
