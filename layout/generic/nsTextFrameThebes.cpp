30fe523d20d37cafe3b945181b3dd78ecb15000f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:14:32 +0000

    Bug 491960. Only reflow due to selection if we did or will have overflowing selection decorations. r=dbaron
    UltraBlame original commit: 282cbda019202d4e2baa30cbbe79614639be8633

--- layout/generic/nsTextFrameThebes.cpp
+++ layout/generic/nsTextFrameThebes.cpp
@@ -5032,11 +5032,12 @@ nsTextFrame::SetSelected(nsPresContext* aPresContext,
     
     
     
-    PRBool didHaveSelectionUnderline =
-             !!(mState & TEXT_SELECTION_UNDERLINE_OVERFLOWED);
+    PRBool didHaveOverflowingSelection =
+      (mState & TEXT_SELECTION_UNDERLINE_OVERFLOWED) != 0;
     nsRect r(nsPoint(0, 0), GetSize());
-    if (didHaveSelectionUnderline != aSelected ||
-        (aSelected && CombineSelectionUnderlineRect(PresContext(), r))) {
+    PRBool willHaveOverflowingSelection =
+      aSelected && CombineSelectionUnderlineRect(PresContext(), r);
+    if (didHaveOverflowingSelection || willHaveOverflowingSelection) {
       PresContext()->PresShell()->FrameNeedsReflow(this,
                                                    nsIPresShell::eStyleChange,
                                                    NS_FRAME_IS_DIRTY);
