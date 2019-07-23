57703e9b921a879f4e30e1ca579cd8fd1d23101b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:23:57 +0000

    Bug 443764 – nsHTMLSelectListAccessible::GetState -- |if| condition is always true, r=aaronlev
    UltraBlame original commit: b0476bcee6e9029c5e4dbb16d7498792d8f45da7

--- accessible/src/html/nsHTMLSelectAccessible.cpp
+++ accessible/src/html/nsHTMLSelectAccessible.cpp
@@ -335,7 +335,7 @@ nsHTMLSelectListAccessible::GetState(PRUint32 *aState, PRUint32 *aExtraState)
 
   nsCOMPtr<nsIDOMHTMLSelectElement> select (do_QueryInterface(mDOMNode));
   if (select) {
-    if (*aState | nsIAccessibleStates::STATE_FOCUSED) {
+    if (*aState & nsIAccessibleStates::STATE_FOCUSED) {
       
       
       nsCOMPtr<nsIDOMNode> focusedOption;
