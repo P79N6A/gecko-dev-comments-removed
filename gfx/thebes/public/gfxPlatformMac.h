ad0f44b472716ed71bc1b990c6367daa7cd9b982
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:59:27 +0000

    Add Core Text backend, will eventually replace ATSUI. Turned off for 32-bit Mac OS X builds right now due to Mac OS X 10.4 not supporting Core Text. b=389074 r=jdaggett
    UltraBlame original commit: e413694940b3108fa9018388ca9ec574f4d36130

--- gfx/thebes/public/gfxPlatformMac.h
+++ gfx/thebes/public/gfxPlatformMac.h
@@ -45,9 +45,12 @@
 #define MAC_OS_X_VERSION_10_4_HEX 0x00001040
 #define MAC_OS_X_VERSION_10_5_HEX 0x00001050
 
+class gfxTextRun;
+
 class THEBES_API gfxPlatformMac : public gfxPlatform {
 public:
     gfxPlatformMac();
+    virtual ~gfxPlatformMac();
 
     static gfxPlatformMac *GetPlatform() {
         return (gfxPlatformMac*) gfxPlatform::GetPlatform();
@@ -93,7 +96,13 @@ public:
 
     
     PRUint32 GetAntiAliasingThreshold() { return mFontAntiAliasingThreshold; }
+
     
+    static void SetupClusterBoundaries(gfxTextRun *aTextRun, const PRUnichar *aString);
+
+    
+    static eFontPrefLang GetFontPrefLangFor(PRUint8 aUnicodeRange);
+
 private:
     void AppendCJKPrefLangs(eFontPrefLang aPrefLangs[], PRUint32 &aLen, 
                             eFontPrefLang aCharLang, eFontPrefLang aPageLang);
@@ -106,6 +115,12 @@ private:
     nsTArray<PRUint32> mCJKPrefLangs;
     PRInt32 mOSXVersion;
     PRUint32 mFontAntiAliasingThreshold;
+
+#ifndef __LP64__
+    
+    
+    PRBool mUseCoreText;
+#endif
 };
 
 #endif 
