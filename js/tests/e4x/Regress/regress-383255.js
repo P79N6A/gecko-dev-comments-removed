d808e3899d332209b05b3f4bc9c093109fbf1f99
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:48 +0000

    JavaScript Test - regression test for bug 383255, by Jesse Ruderman
    UltraBlame original commit: 8dfd754dcde5df00be417114703989c3f57bae4c

---
+++ js/tests/e4x/Regress/regress-383255.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gTestfile = 'regress-383255.js';
+
+var summary = 'Assertion: JS_UPTRDIFF(fp->sp, fp->spbase) <= depthdiff';
+var BUGNUMBER = 383255;
+var actual = '';
+var expect = '';
+
+printBugNumber(BUGNUMBER);
+START(summary);
+
+try
+{
+    <x/>[0]++;
+}
+catch(ex)
+{
+    print(ex);
+}
+
+TEST(1, expect, actual)
+
+END();
