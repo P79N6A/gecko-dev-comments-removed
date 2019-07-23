bc3acb07c9080ff2a6be17840cfb2de007ce7940
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:36 +0000

    JavaScript Test - regression test for bug 376564, by Jesse Ruderman
    UltraBlame original commit: afd06e5833adec499e730b17b978e1cb2b775340

---
+++ js/tests/js1_5/decompilation/regress-376564.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 376564;
+var summary = 'Decompilation of new (eval())';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  var f = function() { new (eval()) };
+  expect = 'function() { new (eval()); }';
+  actual = f + '';
+  compareSource(expect, actual, summary);
+
+  f = function() { new (g()) };
+  expect = 'function() { new (g()); }';
+  actual = f + '';
+  compareSource(expect, actual, summary);
+
+  exitFunc ('test');
+}
