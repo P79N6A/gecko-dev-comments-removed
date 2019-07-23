263d2e397a8d363de8a341d9e76f4abbd6dedba2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:39 +0000

    JavaScript Test - regression test for bug 366123, by shutdown
    UltraBlame original commit: a2d2549d8fca1995f010f8173fbb031eba3377bb

---
+++ js/tests/e4x/Expressions/regress-366123.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 366123;
+var summary = 'Compiling long XML filtering predicate';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+printBugNumber (bug);
+printStatus (summary);
+
+function exploit() {
+  var code = "foo = <x/>.(", obj = {};
+  for(var i = 0; i < 0x10000; i++) {
+    code += "0, ";
+  }
+  code += "0);";
+  Function(code);
+}
+try
+{
+    exploit();
+}
+catch(ex)
+{
+}
+
+TEST(1, expect, actual);
+
+END();
