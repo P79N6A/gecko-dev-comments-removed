c779af5e73932756f98f505b5b9fa2e361cd72c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:01:25 +0000

    Bug 424359. Fix OutputBodyOnly to not output a stray </html> and fix newline stuff for doctypes, comments, and the entire XML serializer. Now with even better passing tests.  Third time is the charm! r=smaug, sr=bzbarsky
    UltraBlame original commit: 037eb901b8b28cc985017ca9bb1c001174ef24d5

---
+++ content/base/test/unit/head_utilities.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const gBasePath = "content/base/test/unit/";
+const nsIDocumentEncoder = Components.interfaces.nsIDocumentEncoder;
+const replacementChar = Components.interfaces.nsIConverterInputStream.DEFAULT_REPLACEMENT_CHARACTER;
+
+function loadContentFile(aFile, aCharset) {
+    
+    if(aCharset == undefined)
+        aCharset = 'UTF-8';
+
+    var file = do_get_file(gBasePath+aFile);
+    var ios = Components.classes['@mozilla.org/network/io-service;1']
+            .getService(Components.interfaces.nsIIOService);
+    var chann = ios.newChannelFromURI ( ios.newFileURI (file) );
+    chann.contentCharset = aCharset;
+
+    
+
+
+
+
+
+    var inputStream = Components.classes["@mozilla.org/intl/converter-input-stream;1"]
+                       .createInstance(Components.interfaces.nsIConverterInputStream);
+    inputStream.init(chann.open(), aCharset, 1024, replacementChar);
+    var str = {}, content = '';
+    while (inputStream.readString(4096, str) != 0) {
+        content += str.value;
+    }
+    return content;
+}
