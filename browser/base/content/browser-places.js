2eff1e7d24ff2b6ccbaf06c75d3ef04fc09d3d7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:47:28 +0000

    Bug 392820 (Bookmarks 'star' panel should capture keyboard shortcuts) - * block accel+w/accel+shift+w within the panel. * close the panel on enter/esc. * focus the name-field/picker and select its contents when opening the panel. * focus the content-area when the panel is closed. r=dietrich.
    UltraBlame original commit: bce13b03a82388f4ce7d0d0026ca21ef7370ccf7

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -54,11 +54,47 @@ var PlacesCommandHook = {
   },
 
   
-  handleEvent: function PCH_handleEvent(aEvent) {
-    if (aEvent.originalTarget != this.panel)
-      return;
+  _blockedCommands: ["cmd_close", "cmd_closeWindow"],
 
-    gEditItemOverlay.uninitPanel(true);
+  _blockCommands: function PCH__blockCommands() {
+    for each(var key in this._blockedCommands) {
+      var elt = document.getElementById(key);
+      if (elt.getAttribute("disabled") == "true")
+        elt.setAttribute("wasDisabled", "true");
+      else {
+        elt.setAttribute("wasDisabled", "false");
+        elt.setAttribute("disabled", "true");
+      }
+    }
+  },
+
+  _restoreCommandsState: function PCH__restoreCommandsState() {
+    for each(var key in this._blockedCommands) {
+      var elt = document.getElementById(key);
+      if (elt.getAttribute("wasDisabled") != "true")
+        elt.removeAttribute("disabled");
+      elt.removeAttribute("wasDisabled");
+    }
+  },
+
+  
+  handleEvent: function PCH_handleEvent(aEvent) {
+    switch (aEvent.type) {
+      case "popuphiding":
+        if (aEvent.originalTarget == this.panel) {
+          gEditItemOverlay.uninitPanel(true);
+          this._restoreCommandsState();
+        }
+        break;
+      case "keypress":
+        if (aEvent.keyCode == KeyEvent.DOM_VK_ESCAPE ||
+            aEvent.keyCode == KeyEvent.DOM_VK_RETURN) {
+          
+          window.content.focus();
+          this.panel.hidePopup();
+        }
+        break;
+    }
   },
 
   _overlayLoaded: false,
@@ -98,10 +134,17 @@ var PlacesCommandHook = {
 
   _doShowEditBookmarkPanel:
   function PCH__doShowEditBookmarkPanel(aItemId, aAnchorElement, aPosition) {
+    this.panel.addEventListener("keypress", this, true);
+    this._blockCommands(); 
     this.panel.openPopup(aAnchorElement, aPosition, -1, -1);
 
     gEditItemOverlay.initPanel(aItemId,
                                { hiddenRows: ["description", "location"] });
+    setTimeout(function() {
+                 var namePicker = document.getElementById("editBMPanel_namePicker");
+                 namePicker.focus();
+                 namePicker.editor.selectAll();
+              }, 0);
   },
 
   
