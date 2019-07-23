e02d6de838a1733f9aac3bb955ea426bcb374881
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:27:21 +0000

    Bug 443433 - [refactor] move places table creation statements to one file This refactors how tables are created in places.  There is no change in logic. r=dietrich
    UltraBlame original commit: 0d0a155411ddc4a4f9fdddeaf1151e373b59b2bd

---
+++ toolkit/components/places/src/nsPlacesTables.h
@@ -0,0 +1,152 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef __nsPlacesTables_h__
+#define __nsPlacesTables_h__
+
+#define CREATE_MOZ_PLACES NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_places ( " \
+    "  id INTEGER PRIMARY KEY" \
+    ", url LONGVARCHAR" \
+    ", title LONGVARCHAR" \
+    ", rev_host LONGVARCHAR" \
+    ", visit_count INTEGER DEFAULT 0" \
+    ", hidden INTEGER DEFAULT 0 NOT NULL" \
+    ", typed INTEGER DEFAULT 0 NOT NULL" \
+    ", favicon_id INTEGER" \
+    ", frecency INTEGER DEFAULT -1 NOT NULL" \
+  ")" \
+)
+
+#define CREATE_MOZ_HISTORYVISITS NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_historyvisits (" \
+    "  id INTEGER PRIMARY KEY" \
+    ", from_visit INTEGER" \
+    ", place_id INTEGER" \
+    ", visit_date INTEGER" \
+    ", visit_type INTEGER" \
+    ", session INTEGER" \
+  ")" \
+)
+
+#define CREATE_MOZ_INPUTHISTORY NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_inputhistory (" \
+    "  place_id INTEGER NOT NULL" \
+    ", input LONGVARCHAR NOT NULL" \
+    ", use_count INTEGER" \
+    ", PRIMARY KEY (place_id, input)" \
+  ")" \
+)
+
+#define CREATE_MOZ_ANNOS NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_annos (" \
+    "  id INTEGER PRIMARY KEY" \
+    ", place_id INTEGER NOT NULL" \
+    ", anno_attribute_id INTEGER" \
+    ", mime_type VARCHAR(32) DEFAULT NULL" \
+    ", content LONGVARCHAR" \
+    ", flags INTEGER DEFAULT 0" \
+    ", expiration INTEGER DEFAULT 0" \
+    ", type INTEGER DEFAULT 0" \
+    ", dateAdded INTEGER DEFAULT 0" \
+    ", lastModified INTEGER DEFAULT 0" \
+  ")" \
+)
+
+#define CREATE_MOZ_ANNO_ATTRIBUTES NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_anno_attributes (" \
+    "  id INTEGER PRIMARY KEY" \
+    ", name VARCHAR(32) UNIQUE NOT NULL" \
+  ")" \
+)
+
+#define CREATE_MOZ_ITEMS_ANNOS NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_items_annos (" \
+    "  id INTEGER PRIMARY KEY" \
+    ", item_id INTEGER NOT NULL" \
+    ", anno_attribute_id INTEGER" \
+    ", mime_type VARCHAR(32) DEFAULT NULL" \
+    ", content LONGVARCHAR" \
+    ", flags INTEGER DEFAULT 0" \
+    ", expiration INTEGER DEFAULT 0" \
+    ", type INTEGER DEFAULT 0" \
+    ", dateAdded INTEGER DEFAULT 0" \
+    ", lastModified INTEGER DEFAULT 0" \
+  ")" \
+)
+
+#define CREATE_MOZ_FAVICONS NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_favicons (" \
+    "  id INTEGER PRIMARY KEY" \
+    ", url LONGVARCHAR UNIQUE" \
+    ", data BLOB" \
+    ", mime_type VARCHAR(32)" \
+    ", expiration LONG" \
+  ")" \
+)
+
+#define CREATE_MOZ_BOOKMARKS NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_bookmarks (" \
+    "  id INTEGER PRIMARY KEY" \
+    ", type INTEGER" \
+    ", fk INTEGER DEFAULT NULL" /* place_id */ \
+    ", parent INTEGER" \
+    ", position INTEGER" \
+    ", title LONGVARCHAR" \
+    ", keyword_id INTEGER" \
+    ", folder_type TEXT" \
+    ", dateAdded INTEGER" \
+    ", lastModified INTEGER" \
+  ")" \
+)
+
+#define CREATE_MOZ_BOOKMARKS_ROOTS NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_bookmarks_roots (" \
+    "  root_name VARCHAR(16) UNIQUE" \
+    ", folder_id INTEGER" \
+  ")" \
+)
+
+#define CREATE_MOZ_KEYWORDS NS_LITERAL_CSTRING( \
+  "CREATE TABLE moz_keywords (" \
+    "  id INTEGER PRIMARY KEY AUTOINCREMENT" \
+    ", keyword TEXT UNIQUE" \
+  ")" \
+)
+
+#endif 
