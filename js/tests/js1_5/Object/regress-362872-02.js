9588d8838f883f64748bb6739a3c0fe4930b8ccf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:45:49 +0000

    JavaScript Tests - regression tests for bug 362872, by shutdown, Blake Kaplan, not part of the build
    UltraBlame original commit: 62ce3647c9c140d67589e524c7f90dad562ecd1d

---
+++ js/tests/js1_5/Object/regress-362872-02.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-362872-02.js';
+
+var BUGNUMBER = 362872;
+var summary = 'script should not drop watchpoint that is in use';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+printBugNumber(BUGNUMBER);
+printStatus (summary);
+ 
+this.watch('x', function f() {
+             print("before");
+             x = 3;
+             print("after");
+           });
+x = 3;
+
+reportCompare(expect, actual, summary);
