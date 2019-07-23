7dbe3e576cffe01356abce92fcd3d5b1e109c536
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:56:58 +0000

    Bug 384339 - The check-interactive code executes head, tail scripts before the user runs; (Cv1) Move _execute_test() definition to head.js from tail.js; r=jwalden+bmo
    UltraBlame original commit: 1752f796d0aa497715818280698323c7545786c5

--- testing/xpcshell/head.js
+++ testing/xpcshell/head.js
@@ -43,6 +43,7 @@
 
 
 
+
 var _quit = false;
 var _fail = false;
 var _tests_pending = 0;
@@ -91,6 +92,25 @@ function _do_quit() {
 
   _quit = true;
 }
+
+function _execute_test() {
+  try {
+    do_test_pending();
+    run_test();
+    do_test_finished();
+    _do_main();
+  } catch (e) {
+    _fail = true;
+    dump(e + "\n");
+  }
+
+  if (_fail)
+    dump("*** FAIL ***\n");
+  else
+    dump("*** PASS ***\n");
+}
+
+
 
 
 
--- testing/xpcshell/tail.js
+++ testing/xpcshell/tail.js
@@ -35,19 +35,11 @@
 
 
 
-function _execute_test() {
-  try {
-    do_test_pending();
-    run_test();
-    do_test_finished();
-    _do_main();
-  } catch (e) {
-    _fail = true;
-    dump(e + "\n");
-  }
 
-  if (_fail)
-    dump("*** FAIL ***\n");
-  else
-    dump("*** PASS ***\n");
-}
+
+
+
+
+
+
+
