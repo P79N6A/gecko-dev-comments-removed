c9f10b650fbf600436b01f478608f18e6cad2c30
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:52:43 +0000

    Bug 328412 - Find in empty document triggers "ASSERTION: No first node!"; r=bzbarsky sr=jst
    UltraBlame original commit: d431b9fb79fccbb812f23033001fc933630a03b5

--- embedding/components/find/src/nsFind.cpp
+++ embedding/components/find/src/nsFind.cpp
@@ -301,6 +301,9 @@ nsFindContentIterator::Reset()
   range->SetStart(mStartNode, mStartOffset);
   range->SetEnd(mEndNode, mEndOffset);
   mOuterIterator->Init(range);
+  
+  if (mOuterIterator->IsDone())
+    return;
 
   if (!mFindBackward) {
     if (mStartOuterNode != mStartNode) {
