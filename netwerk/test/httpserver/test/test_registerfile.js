76cdc9bfb92000d4a15a1fbe03dcdde5b7a679b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:48:37 +0000

    Bug 395559 - Fix two typos in httpd.js which prevented the registerFile API from working correctly, and add a dumpn so that errors in handlers can be detected more easily by reading console dumps.  r=sayrer, NPOTB, test-only, etc.
    UltraBlame original commit: aac1069b4bdf55eb27df039c367c9948cb19168a

---
+++ netwerk/test/httpserver/test/test_registerfile.js
@@ -0,0 +1,82 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const BASE = "http://localhost:4444";
+
+function isException(e, code)
+{
+  if (e !== code && e.result !== code)
+    do_throw("unexpected error: " + e);
+}
+
+var file = do_get_file("netwerk/test/httpserver/test/test_registerfile.js");
+
+function onStart(ch, cx)
+{
+  do_check_eq(ch.responseStatus, 200);
+}
+
+function onStop(ch, cx, status, data)
+{
+  
+  do_check_eq(data.length, file.fileSize);
+}
+
+var test = new Test(BASE + "/foo", null, onStart, onStop);
+
+function run_test()
+{
+  var srv = createServer();
+
+  try
+  {
+    srv.registerFile("/foo", do_get_file("netwerk/test/httpserver/"));
+    throw "registerFile succeeded!";
+  }
+  catch (e)
+  {
+    isException(e, Cr.NS_ERROR_INVALID_ARG);
+  }
+
+  srv.registerFile("/foo", file);
+  srv.start(4444);
+
+  runHttpTests([test], function() { srv.stop(); });
+}
