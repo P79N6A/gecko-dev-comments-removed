a7e8b7631ab6bb322752c28ee11933e362f1b8cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:12:11 +0000

    Bug 393707: Landing 389634 broke building without XUL, patch by romaxa <romaxa@gmail.com>, r=smaug, a=beltzner
    UltraBlame original commit: de6f7f2397e82d4707451e0c99894b802160ddac

---
+++ embedding/components/windowwatcher/src/nsAutoWindowStateHelper.h
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef __nsAutoWindowStateHelper_h
+#define __nsAutoWindowStateHelper_h
+
+#include "nsCOMPtr.h"
+
+
+
+
+
+
+class nsIDOMWindow;
+
+class nsAutoWindowStateHelper
+{
+public:
+  nsAutoWindowStateHelper(nsIDOMWindow *aWindow);
+  ~nsAutoWindowStateHelper();
+
+  PRBool DefaultEnabled()
+  {
+    return mDefaultEnabled;
+  }
+
+protected:
+  PRBool DispatchCustomEvent(const char *aEventName);
+
+  nsIDOMWindow *mWindow;
+  PRBool mDefaultEnabled;
+};
+
+
+#endif
