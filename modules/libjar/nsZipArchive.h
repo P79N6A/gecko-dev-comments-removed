23eceacee682609c405279adf24a7133919d7f7d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:36:18 +0000

    Backing out 510844 to fix unittest failure on CLOSED TREE.
    UltraBlame original commit: ac90123f11e9a9a6dedf1721f5b3e40424d4d865

--- modules/libjar/nsZipArchive.h
+++ modules/libjar/nsZipArchive.h
@@ -47,15 +47,18 @@
 #define ZIPFIND_MAGIC 0x5A495046L   /* "ZIPF" */
 #define ZIP_TABSIZE   256
 
-#define ZIP_BUFLEN    (4*1024)      /* Used as output buffer when deflating items to a file */
+#define ZIP_BUFLEN    (4 * 1024 - 1)
 
 #define PL_ARENA_CONST_ALIGN_MASK 7
 #include "plarena.h"
+#define ZIP_Seek(fd,p,m) (PR_Seek((fd),((PROffset32)p),(m))==((PROffset32)p))
 
 #include "zlib.h"
 #include "nsAutoPtr.h"
 
 class nsZipFind;
+class nsZipReadState;
+class nsZipItemMetadata;
 
 struct PRFileDesc;
 
@@ -85,6 +88,7 @@ struct nsZipItem
   nsZipItem*  next;
 
   PRUint32    headerOffset;
+  PRUint32    dataOffset;
   PRUint32    size;             
   PRUint32    realsize;         
   PRUint32    crc32;
@@ -96,18 +100,21 @@ struct nsZipItem
   PRUint16     date;
   PRUint16     mode;
   PRUint8      compression;
-  bool         isDirectory;
-  bool         isSynthetic;     
+  PRPackedBool hasDataOffset : 1;
+  PRPackedBool isDirectory : 1; 
+  PRPackedBool isSynthetic : 1;  
+
+
 
 #if defined(XP_UNIX) || defined(XP_BEOS)
-  bool         isSymlink;
+  PRPackedBool isSymlink : 1;
 #endif
 
-  char         name[1];         
+  char        name[1]; 
 };
 
 class nsZipHandle;
-
+class nsSeekableZipHandle;
 
 
 
@@ -186,14 +193,7 @@ public:
   
 
 
-  nsZipHandle* GetFD();
-
-  
-
-
-
-
-  PRUint8* GetData(nsZipItem* aItem);
+  nsZipHandle* GetFD(nsZipItem* aItem);
 
 private:
   
@@ -202,7 +202,14 @@ private:
   PLArenaPool   mArena;
 
   
-  bool          mBuiltSynthetics;
+
+
+
+
+  bool MaybeReadItem(nsZipItem* aItem);
+
+  
+  PRPackedBool  mBuiltSynthetics;
 
   
   nsRefPtr<nsZipHandle> mFd;
@@ -215,29 +222,92 @@ private:
   nsresult          BuildFileList();
   nsresult          BuildSynthetics();
 
-  nsresult  CopyItemToDisk(nsZipItem* item, PRFileDesc* outFD);
-  nsresult  InflateItem(nsZipItem* item, PRFileDesc* outFD);
+  nsresult  CopyItemToDisk(PRUint32 size, PRUint32 crc, nsSeekableZipHandle &fd, PRFileDesc* outFD);
+  nsresult  InflateItem(const nsZipItem* aItem, nsSeekableZipHandle &fd, PRFileDesc* outFD);
 };
 
 class nsZipHandle {
 friend class nsZipArchive;
+friend class nsSeekableZipHandle;
 public:
   static nsresult Init(PRFileDesc *fd, nsZipHandle **ret NS_OUTPARAM);
+
+  
+
+
+
+
+  PRInt32 Read(PRUint32 aPosition, void *aBuffer, PRUint32 aCount);
 
   NS_METHOD_(nsrefcnt) AddRef(void);
   NS_METHOD_(nsrefcnt) Release(void);
 
 protected:
-  PRFileDesc * mFd;       
-  PRUint8 *    mFileData; 
-  PRUint32     mLen;      
+  PRFileDesc *mFd; 
+  PRUint8 *mFileData; 
+  PRUint32 mLen; 
 
 private:
   nsZipHandle();
   ~nsZipHandle();
 
-  PRFileMap *  mMap;      
-  nsrefcnt     mRefCnt;   
+  PRFileMap *mMap; 
+  nsrefcnt mRefCnt; 
+};
+
+
+
+
+class nsSeekableZipHandle {
+  
+public:
+  nsSeekableZipHandle()
+    : mOffset(0)
+    , mRemaining(0)
+  {
+  }
+
+  
+
+
+
+  bool Open(nsZipHandle *aHandle, PRUint32 aOffset, PRUint32 aLength) {
+    NS_ABORT_IF_FALSE (aHandle, "Argument must not be NULL");
+    if (aOffset > aHandle->mLen)
+      return false;
+    mFd = aHandle;
+    mOffset = aOffset;
+    mRemaining = aLength;
+    return true;
+  }
+
+  
+  void Close()
+  {
+    mFd = NULL;
+  }
+
+  
+
+
+
+  PRInt32 Read(void *aBuffer, PRUint32 aCount)
+  {
+    if (!mFd.get())
+      return -1;
+    aCount = PR_MIN(mRemaining, aCount);
+    PRInt32 ret = mFd->Read(mOffset, aBuffer, aCount);
+    if (ret > 0) {
+      mOffset += ret;
+      mRemaining -= ret;
+    }
+    return ret;
+  }
+
+private:
+  nsRefPtr<nsZipHandle> mFd; 
+  PRUint32 mOffset; 
+  PRUint32 mRemaining; 
 };
 
 
@@ -249,6 +319,7 @@ private:
 class nsZipFind
 {
 public:
+
   nsZipFind(nsZipArchive* aZip, char* aPattern, PRBool regExp);
   ~nsZipFind();
 
