77d528de49838301052fed4819c3a7178f23b8a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:52:14 +0000

    Fixed breaks in switch statements causing premature end-of-traces (bug 456345, r=gal,brendan).
    UltraBlame original commit: 71b97156373a871d752c96d66320ed2277f2b61e

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -1583,6 +1583,32 @@ function testMatchStringObject() {
 testMatchStringObject.expected = null;
 test(testMatchStringObject);
 
+function innerSwitch(k)
+{
+    var m = 0;
+
+    switch (k)
+    {
+    case 0:
+        m = 1;
+        break;
+    }
+
+    return m;
+}
+function testInnerSwitchBreak()
+{
+    var r = new Array(5);
+    for (var i = 0; i < 5; i++)
+    {
+        r[i] = innerSwitch(0);
+    }
+
+    return r.join(",");
+}
+testInnerSwitchBreak.expected = "1,1,1,1,1";
+test(testInnerSwitchBreak);
+
 
 print("\npassed:", passes.length && passes.join(","));
 print("\nFAILED:", fails.length && fails.join(","));
