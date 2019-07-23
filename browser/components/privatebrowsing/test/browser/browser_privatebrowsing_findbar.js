f71c06f6ef4334b18cd1452006c476efedc5ddbf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:11:07 +0000

    Bug 463692 - Clear the findbar text when leaving the private browsing mode; r,a1.9.1b2=mconnor
    UltraBlame original commit: d8295ec827fa1cd400a2406f9763183014773cf2

---
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_findbar.js
@@ -0,0 +1,75 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  let prefBranch = Cc["@mozilla.org/preferences-service;1"].
+                   getService(Ci.nsIPrefBranch);
+  prefBranch.setBoolPref("browser.privatebrowsing.keep_current_session", true);
+  let pb = Cc["@mozilla.org/privatebrowsing;1"].
+           getService(Ci.nsIPrivateBrowsingService);
+
+  
+  const kTestSearchString = "privatebrowsing";
+  let findBox = gFindBar.getElement("findbar-textbox");
+  gFindBar.startFind(gFindBar.FIND_NORMAL);
+  for (let i = 0; i < kTestSearchString.length; ++ i)
+    EventUtils.synthesizeKey(kTestSearchString[i], {});
+
+  
+  pb.privateBrowsingEnabled = true;
+
+  is(findBox.value, kTestSearchString,
+    "entering the private browsing mode should not clear the findbar");
+  ok(findBox.editor.transactionManager.numberOfUndoItems > 0,
+    "entering the private browsing mode should not reset the undo list of the findbar control");
+
+  
+  pb.privateBrowsingEnabled = false;
+
+  is(findBox.value, "",
+    "leaving the private browsing mode should clear the findbar");
+  is(findBox.editor.transactionManager.numberOfUndoItems, 0,
+    "leaving the private browsing mode should reset the undo list of the findbar control");
+
+  
+  prefBranch.clearUserPref("browser.privatebrowsing.keep_current_session");
+  gFindBar.close();
+}
