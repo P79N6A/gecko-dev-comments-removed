ec5d0a604d8142d643be3558dc1b02ab9f5d96c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:49 +0000

    JavaScript Test - regression test for bug 352732, by Jesse Ruderman
    UltraBlame original commit: 86db62aa783401baaa9c801f333a3913117fad93

---
+++ js/tests/js1_7/decompilation/regress-352732.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 352732;
+var summary = 'Decompiling "if (x) L: let x;"';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  var f = (function() { if (x) L: let x; });
+  expect = 'function() { if (x) { L: let x; } }';
+  actual = f + '';
+  compareSource(expect, actual, summary);
+
+  f = (function() { if (x) L: let x; else y; });
+  expect = 'function() { if (x) { L: let x; } else { y;} }';
+  actual = f + '';
+  compareSource(expect, actual, summary);
+
+  exitFunc ('test');
+}
