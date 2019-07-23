6955bbcd1654d6330ca4ffb37dba4dee40fa57b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:55:12 +0000

    JavaScript Tests - regression tests for bug 407957, not part of the build
    UltraBlame original commit: d39f15986b72ed9ec7f84b46db7a0e3e9559f4c9

---
+++ js/tests/js1_5/Regress/regress-407957.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-407957.js';
+
+var BUGNUMBER = 407323;
+var summary = 'Iterator is mutable.';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  var obj           = {};
+  var saveIterator  = Iterator;
+
+  Iterator = obj;
+  reportCompare(obj, Iterator, summary);
+
+  exitFunc ('test');
+}
