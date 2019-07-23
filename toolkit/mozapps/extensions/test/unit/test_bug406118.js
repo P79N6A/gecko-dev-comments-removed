8f76c92a579ede82c12e253f48ed7a5219ea4a16
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:04:48 +0000

    Bug 406118: Extension blocklist is broken. r=bsmedberg, a1.9b4=beltzner
    UltraBlame original commit: f43f7b88ba52f0590f10f37afd6929a13ef4bcac

---
+++ toolkit/mozapps/extensions/test/unit/test_bug406118.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function run_test() {
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9");
+
+  
+  var source = do_get_file("toolkit/mozapps/extensions/test/unit/data/test_bug393285.xml");
+  source.copyTo(gProfD, "blocklist.xml");
+
+  var blocklist = Components.classes["@mozilla.org/extensions/blocklist;1"]
+                            .getService(Components.interfaces.nsIBlocklistService);
+  
+  
+  do_check_false(blocklist.isAddonBlocklisted("test_bug393285_1@tests.mozilla.org", "1", null, null));
+  do_check_true(blocklist.isAddonBlocklisted("test_bug393285_2@tests.mozilla.org", "1", null, null));
+  do_check_true(blocklist.isAddonBlocklisted("test_bug393285_3@tests.mozilla.org", "1", null, null));
+  do_check_true(blocklist.isAddonBlocklisted("test_bug393285_4@tests.mozilla.org", "1", null, null));
+
+}
