d22fab8ad9d8fb9bc186dd4b48400949ee917bb2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:38 +0000

    Fix for bug 376514 - stop text accessible using for nsHTMLListBulletAccessible
    UltraBlame original commit: fdb8325080f123c84c5b1410302cf0573d355ecd

--- accessible/src/html/nsHTMLTextAccessible.cpp
+++ accessible/src/html/nsHTMLTextAccessible.cpp
@@ -311,44 +311,95 @@ void nsHTMLLIAccessible::CacheChildren()
 }
 
 
-nsHTMLListBulletAccessible::nsHTMLListBulletAccessible(nsIDOMNode* aDomNode, 
-  nsIWeakReference* aShell, nsIFrame *aFrame, const nsAString& aBulletText): 
-  nsHTMLTextAccessible(aDomNode, aShell, aFrame), mWeakParent(nsnull), mBulletText(aBulletText)
+
+nsHTMLListBulletAccessible::
+  nsHTMLListBulletAccessible(nsIDOMNode* aDomNode, nsIWeakReference* aShell,
+                             nsIFrame *aFrame, const nsAString& aBulletText) :
+    nsLeafAccessible(aDomNode, aShell), mFrame(aFrame), mWeakParent(nsnull),
+    mBulletText(aBulletText)
 {
   mBulletText += ' '; 
 }
 
-NS_IMETHODIMP nsHTMLListBulletAccessible::GetUniqueID(void **aUniqueID)
+NS_IMETHODIMP
+nsHTMLListBulletAccessible::GetUniqueID(void **aUniqueID)
 {
   
   *aUniqueID = NS_STATIC_CAST(void*, this);
   return NS_OK;
 }
 
-
-NS_IMETHODIMP nsHTMLListBulletAccessible::Shutdown()
+NS_IMETHODIMP
+nsHTMLListBulletAccessible::Shutdown()
 {
   mBulletText.Truncate();
   mWeakParent = nsnull;
-  return nsHTMLTextAccessible::Shutdown();
+  mFrame = nsnull;
+
+  return nsLeafAccessible::Shutdown();
 }
 
-NS_IMETHODIMP nsHTMLListBulletAccessible::GetName(nsAString &aName)
+nsIFrame*
+nsHTMLListBulletAccessible::GetFrame()
+{
+  if (!mWeakShell)
+    return nsnull;
+
+  if (!mFrame)
+    mFrame = nsLeafAccessible::GetFrame();
+  return mFrame;
+}
+
+NS_IMETHODIMP
+nsHTMLListBulletAccessible::GetName(nsAString &aName)
 {
   aName = mBulletText;
   return NS_OK;
 }
 
 NS_IMETHODIMP
+nsHTMLListBulletAccessible::GetRole(PRUint32 *aRole)
+{
+  *aRole = nsIAccessibleRole::ROLE_STATICTEXT;
+  return NS_OK;
+}
+
+NS_IMETHODIMP
 nsHTMLListBulletAccessible::GetState(PRUint32 *aState, PRUint32 *aExtraState)
 {
-  nsresult rv = nsHTMLTextAccessible::GetState(aState, aExtraState);
+  nsresult rv = nsLeafAccessible::GetState(aState, aExtraState);
   NS_ENSURE_SUCCESS(rv, rv);
 
   *aState &= ~nsIAccessibleStates::STATE_FOCUSABLE;
   *aState |= nsIAccessibleStates::STATE_READONLY;
   return NS_OK;
 }
+
+NS_IMETHODIMP
+nsHTMLListBulletAccessible::SetParent(nsIAccessible *aParentAccessible)
+{
+  mParent = nsnull;
+  mWeakParent = aParentAccessible;
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLListBulletAccessible::GetParent(nsIAccessible **aParentAccessible)
+{
+  NS_IF_ADDREF(*aParentAccessible = mWeakParent);
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsHTMLListBulletAccessible::FireToolkitEvent(PRUint32 aEvent,
+                                             nsIAccessible *aTarget,
+                                             void *aData)
+{
+  if (aEvent == nsIAccessibleEvent::EVENT_HIDE)
+    mFrame = nsnull;  
+  return nsLeafAccessible::FireToolkitEvent(aEvent, aTarget, aData);
+}
+
 
 
 
--- accessible/src/html/nsHTMLTextAccessible.h
+++ accessible/src/html/nsHTMLTextAccessible.h
@@ -100,20 +100,34 @@ public:
   NS_IMETHOD GetChildCount(PRInt32 *aAccChildCount);
 };
 
-class nsHTMLListBulletAccessible : public nsHTMLTextAccessible
+class nsHTMLListBulletAccessible : public nsLeafAccessible
 {
 public:
   nsHTMLListBulletAccessible(nsIDOMNode *aDOMNode, nsIWeakReference* aShell,
                              nsIFrame *aFrame, const nsAString& aBulletText);
+
+  
   NS_IMETHOD GetUniqueID(void **aUniqueID);
+
+  
   NS_IMETHOD Shutdown();
+  NS_IMETHOD_(nsIFrame *) GetFrame(void);
+
+  
   NS_IMETHOD GetName(nsAString& aName);
-  NS_IMETHOD GetRole(PRUint32 *aRole) { *aRole = nsIAccessibleRole::ROLE_STATICTEXT; return NS_OK; }
+  NS_IMETHOD GetRole(PRUint32 *aRole);
   NS_IMETHOD GetState(PRUint32 *aState, PRUint32 *aExtraState);
+
   
   
-  NS_IMETHOD SetParent(nsIAccessible *aParentAccessible) { mParent = nsnull; mWeakParent = aParentAccessible; return NS_OK; }
-  NS_IMETHOD GetParent(nsIAccessible **aParentAccessible) { NS_IF_ADDREF(*aParentAccessible = mWeakParent); return NS_OK; }
+  
+  NS_IMETHOD SetParent(nsIAccessible *aParentAccessible);
+  NS_IMETHOD GetParent(nsIAccessible **aParentAccessible);
+
+  
+  NS_IMETHOD FireToolkitEvent(PRUint32 aEvent, nsIAccessible *aTarget,
+                              void *aData);
+
 protected:
   
   
@@ -121,8 +135,8 @@ protected:
   
   
   
-  
   nsIAccessible *mWeakParent;
+  nsIFrame *mFrame;
   nsString mBulletText;
 };
 
