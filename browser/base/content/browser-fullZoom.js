4a3642eacc9cdefe074f81a7e50378a9708a50e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:37:45 +0000

    fix nit for bug 513912
    UltraBlame original commit: 8afb652052039e3475d40b01594b61987cbf0e68

--- browser/base/content/browser-fullZoom.js
+++ browser/base/content/browser-fullZoom.js
@@ -333,11 +333,10 @@ var FullZoom = {
 
 
   _applyPrefToSetting: function FullZoom__applyPrefToSetting(aValue, aBrowser) {
-    var browser = aBrowser || gBrowser.selectedBrowser;
-
     if (!this.siteSpecific && !this._inPrivateBrowsing)
       return;
 
+    var browser = aBrowser || gBrowser.selectedBrowser;
     try {
       if (gInPrintPreviewMode ||
           browser.contentDocument instanceof Ci.nsIImageDocument ||
