7248f7f93f73fe6925dd3ea0ff78239fa2145544
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:18:31 +0000

    Make @ns::attr force HEAVYWEIGHT to avoid having to analyze ns as a lexical name (hack for 496316, r=igor).
    UltraBlame original commit: fb21a3a1218078d3335f7e7ac34fda68b02c40fa

--- js/src/jsparse.cpp
+++ js/src/jsparse.cpp
@@ -7018,8 +7018,11 @@ QualifiedIdentifier(JSContext *cx, JSTokenStream *ts, JSTreeContext *tc)
     pn = PropertySelector(cx, ts, tc);
     if (!pn)
         return NULL;
-    if (js_MatchToken(cx, ts, TOK_DBLCOLON))
+    if (js_MatchToken(cx, ts, TOK_DBLCOLON)) {
+        
+        tc->flags |= TCF_FUN_HEAVYWEIGHT;
         pn = QualifiedSuffix(cx, ts, pn, tc);
+    }
     return pn;
 }
 
