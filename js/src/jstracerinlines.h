58d8850b1bd26e40660112c0a9a24e8bd595098f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:34:59 +0000

    Add license block-comment, expand tabs.
    UltraBlame original commit: a6818f87ee5b0fb8c5de2f06a2fe88f038386307

--- js/src/jstracerinlines.h
+++ js/src/jstracerinlines.h
@@ -210,7 +210,6 @@ static inline bool
 guard_int_fits_in_jsval(JSContext* cx, JSFrameRegs& regs, jsint& i)
 {
     bool ok = interp_guard_int_fits_in_jsval(cx, regs, i);
-    
     return ok;
 }
 
@@ -225,7 +224,6 @@ static inline bool
 guard_uint_fits_in_jsval(JSContext* cx, JSFrameRegs& regs, uint32& u)
 {
     bool ok = interp_guard_uint_fits_in_jsval(cx, regs, u);
-    
     return ok;
 }
 
