6fa01540b27c5919cc78c3a17696233867f81dc5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:55:29 +0000

    Bug 521889 - browser_461743.js is randomly timing out, r=zeniko
    UltraBlame original commit: 7385868057d79e69d4618e3285c3f42643a0e2d2

--- browser/components/sessionstore/test/browser/browser_461743.js
+++ browser/components/sessionstore/test/browser/browser_461743.js
@@ -36,37 +36,36 @@
 
 function test() {
   
-  
+
   waitForExplicitFinish();
-  
+
   let testURL = "http://localhost:8888/browser/" +
     "browser/components/sessionstore/test/browser/browser_461743_sample.html";
-  
+
+  let frameCount = 0;
   let tab = gBrowser.addTab(testURL);
-  info("New tab added");
   tab.linkedBrowser.addEventListener("load", function(aEvent) {
-    info("New tab loaded");
+    
+    if (frameCount++ < 2)
+      return;
     tab.linkedBrowser.removeEventListener("load", arguments.callee, true);
-    executeSoon(function() {
-      let tab2 = gBrowser.duplicateTab(tab);
-      info("Duplicated tab");
-      tab2.linkedBrowser.addEventListener("461743", function(aEvent) {
-        tab2.linkedBrowser.removeEventListener("461743", arguments.callee, true);
-        is(aEvent.data, "done", "XSS injection was attempted");
+    let tab2 = gBrowser.duplicateTab(tab);
+    tab2.linkedBrowser.addEventListener("461743", function(aEvent) {
+      tab2.linkedBrowser.removeEventListener("461743", arguments.callee, true);
+      is(aEvent.data, "done", "XSS injection was attempted");
+
+      executeSoon(function() {
+        let iframes = tab2.linkedBrowser.contentWindow.frames;
+        let innerHTML = iframes[1].document.body.innerHTML;
+        isnot(innerHTML, Components.utils.reportError.toString(),
+              "chrome access denied!");
+
         
-        executeSoon(function() {
-          let iframes = tab2.linkedBrowser.contentWindow.frames;
-          let innerHTML = iframes[1].document.body.innerHTML;
-          isnot(innerHTML, Components.utils.reportError.toString(),
-                "chrome access denied!");
-          
-          
-          gBrowser.removeTab(tab2);
-          gBrowser.removeTab(tab);
-          
-          finish();
-        });
-      }, true, true);
-    });
+        gBrowser.removeTab(tab2);
+        gBrowser.removeTab(tab);
+
+        finish();
+      });
+    }, true, true);
   }, true);
 }
