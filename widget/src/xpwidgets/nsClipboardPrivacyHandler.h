cd1e8db589e7cb55ec98a2318317a6f926e1207c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:37:17 +0000

    Bug 462106 - Clear the data copied to clipboard inside the private browsing mode after leaving it (2nd try); r,sr=roc
    UltraBlame original commit: 9c777aa29014700fc6954e7e5dc1a289c7193c65

---
+++ widget/src/xpwidgets/nsClipboardPrivacyHandler.h
@@ -0,0 +1,81 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsClipboardPrivacyHandler_h__
+#define nsClipboardPrivacyHandler_h__
+
+#include "nsIObserver.h"
+#include "nsIPrivateBrowsingService.h"
+#include "nsWeakReference.h"
+#include "nsCOMPtr.h"
+
+class nsITransferable;
+
+
+
+
+
+
+
+
+class nsClipboardPrivacyHandler : public nsIObserver,
+                                  public nsSupportsWeakReference
+{
+
+public:
+
+  
+  NS_DECL_ISUPPORTS
+
+  
+  NS_DECL_NSIOBSERVER
+
+  nsresult Init();
+  nsresult PrepareDataForClipboard(nsITransferable * aTransferable);
+
+private:
+
+  PRBool InPrivateBrowsing();
+
+  nsCOMPtr<nsIPrivateBrowsingService> mPBService;
+
+};
+
+nsresult NS_NewClipboardPrivacyHandler(nsClipboardPrivacyHandler ** aHandler);
+
+#endif 
