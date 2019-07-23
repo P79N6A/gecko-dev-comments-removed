b12441e106061d0a4747f56007821a8993964d87
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:34:47 +0000

    Bug 508051 part 2 - Avoid imacros for JSOP_INSTANCEOF. Re-landing per comment 8. r=gal.
    UltraBlame original commit: d2f6a9e585c151ddcfd68a7f3aeed3a259cdc412

---
+++ js/src/trace-test/tests/basic/testDeepBailFromHasInstance.js
@@ -0,0 +1,12 @@
+var arr = [StopIteration, StopIteration, StopIteration, StopIteration, {}];
+var obj = {};
+var x;
+var result = 'no error';
+try {
+    for (var i = 0; i < arr.length; i++)
+        x = (obj instanceof arr[i]);  
+} catch (exc) {
+    result = exc.constructor.name;
+}
+assertEq(result, 'TypeError');
+
