05a193142b23eaf15ffadc45ac3fbbad18a4a125
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:34:51 +0000

    bug 473040 - JavaScript Test by Gary Kwong.
    UltraBlame original commit: 46099e1c0fa5dec191f8e0cdb750d421eb164cfb

---
+++ js/tests/js1_8/extensions/regress-473040.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-473040.js';
+
+var BUGNUMBER = 473040;
+var summary = 'Do not assert: tm->reservedDoublePoolPtr > tm->reservedDoublePool';
+var actual = '';
+var expect = '';
+
+printBugNumber(BUGNUMBER);
+printStatus (summary);
+ 
+jit(true);
+
+__proto__.functional getter= (new Function("gc()"));
+for each (let x in [new Boolean(true), new Boolean(true), -0, new
+                    Boolean(true), -0]) { undefined; }
+
+jit(false);
+
+reportCompare(expect, actual, summary);
