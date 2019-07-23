eefa6ab4df262712c137bed780370e91378e5cc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:48 +0000

    Recalc quotes and counters before reflow.  Bug 377216, r+sr=dbaron
    UltraBlame original commit: 80cf8457c23a0bfbae096f3180607553cfc7a13e

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -10180,22 +10180,28 @@ nsCSSFrameConstructor::EndUpdate()
     
     
 
-    if (mQuotesDirty) {
-      mQuotesDirty = PR_FALSE;
-      mQuoteList.RecalcAll();
-    }
-
-    if (mCountersDirty) {
-      mCountersDirty = PR_FALSE;
-      mCounterManager.RecalcAll();
-    }
-
-    NS_ASSERTION(!mQuotesDirty, "Quotes updates will be lost");
-    NS_ASSERTION(!mCountersDirty, "Counter updates will be lost");
+    RecalcQuotesAndCounters();
     NS_ASSERTION(mUpdateCount == 1, "Odd update count");
   }
 
   --mUpdateCount;
+}
+
+void
+nsCSSFrameConstructor::RecalcQuotesAndCounters()
+{
+  if (mQuotesDirty) {
+    mQuotesDirty = PR_FALSE;
+    mQuoteList.RecalcAll();
+  }
+
+  if (mCountersDirty) {
+    mCountersDirty = PR_FALSE;
+    mCounterManager.RecalcAll();
+  }
+
+  NS_ASSERTION(!mQuotesDirty, "Quotes updates will be lost");
+  NS_ASSERTION(!mCountersDirty, "Counter updates will be lost");  
 }
 
 void
