e8715c798ca7066e7e3d4f259d30fcb16eb2e5e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:35:33 +0000

    Bug 451958. Add more CheckLoadURIs (part 4). r+sr=roc
    UltraBlame original commit: b0f7dea54e79302a554e480c5f0d575f4fa2e1d0

--- content/media/video/src/nsMediaStream.cpp
+++ content/media/video/src/nsMediaStream.cpp
@@ -518,8 +518,14 @@ nsresult nsHttpStreamStrategy::OpenInternal(nsIStreamListener **aStreamListener,
                                               &rv);
       NS_ENSURE_TRUE(listener, NS_ERROR_OUT_OF_MEMORY);
       NS_ENSURE_SUCCESS(rv, rv);
-    }
+    } else {
+      rv = nsContentUtils::GetSecurityManager()->
+             CheckLoadURIWithPrincipal(element->NodePrincipal(),
+                                       mURI,
+                                       nsIScriptSecurityManager::STANDARD);
+      NS_ENSURE_SUCCESS(rv, rv);
 
+    }
     
     
     
