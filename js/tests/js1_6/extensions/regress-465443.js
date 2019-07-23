0228ff720f0b2a98299fc0c1f0e94f47104be16e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:35:21 +0000

    bug 465433 - JavaScript Test by Jesse Ruderman.
    UltraBlame original commit: 708dad34da8fc73f3ccd9308c7b00f7965e3327d

---
+++ js/tests/js1_6/extensions/regress-465443.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-465443.js';
+
+var BUGNUMBER = 465433;
+var summary = '';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  expect = 'SyntaxError: invalid for/in left-hand side';
+
+  jit(true);
+
+  try
+  {
+    eval('(function () { const b = 16; var out = []; for each (b in [true, "", true, "", true, ""]) out.push(b >> 1) })();');
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+
+  jit(false);
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
