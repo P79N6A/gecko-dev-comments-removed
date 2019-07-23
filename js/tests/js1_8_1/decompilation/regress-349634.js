54f8d7e4f5977e2bd89125151a35101545f5de69
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:34:50 +0000

    bug 457618 - JavaScript Tests - update tests due bug 346749 (let declarations at function level should be treated as var).
    UltraBlame original commit: be6a4555fb39aa0d5df1fc5209fe432f0eec7370

---
+++ js/tests/js1_8_1/decompilation/regress-349634.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-349634.js';
+
+var BUGNUMBER = 349634;
+var summary = 'decompilation of {} and let';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  var f;
+
+  f = function () { let a = 3; { let a = 4; } }
+  actual = f + '';
+  expect = 'function () {\n    var a = 3;\n    {\n        let a = 4;\n    }\n}';
+  compareSource(expect, actual, summary);
+
+  exitFunc ('test');
+}
---
+++ js/tests/js1_8_1/extensions/regress-353214-02.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-353214-02.js';
+
+var BUGNUMBER = 353214;
+var summary = 'bug 353214';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  var f = function ([x]) { let x; }
+  expect = 'function ([x]) { var x; }';
+  actual = f + '';
+
+  compareSource(expect, actual, summary);
+
+  exitFunc ('test');
+}
