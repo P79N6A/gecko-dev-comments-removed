cd250ca75b80152522886f870d7b7d68e20976ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:58:52 +0000

    bug 480244 - JavaScript Test by Graydon Hoare.
    UltraBlame original commit: 753d620da90d3676a9a1205cde06161f2225076c

---
+++ js/tests/js1_5/Regress/regress-480244.js
@@ -0,0 +1,86 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-480244.js';
+
+var BUGNUMBER = 480244;
+var summary = 'Do not assert: isInt32(*p)';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  jit(true);
+
+  function outer() {
+    var v = 10.234;
+    for (var i = 0; i < 0xff; ++i) {
+      inner(v);
+    }
+  }
+
+  var g = 0;
+  var h = 0;
+
+  function inner() {
+    var v = 10;
+    for (var k = 0; k < 0xff; ++k) {
+      g++;
+      if (g & 0xff == 0xff)
+        h++;
+    }
+    return h;
+  }
+
+  outer();
+  print("g=" + g + " h=" + h);
+
+  jit(false);
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
