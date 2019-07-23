81064cf10bf5b44e190c8824180c6497e84b54e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:56:33 +0000

    XPCOMUtilify nsURLFormatter and add tests. r=dietrich, a=schrep
    UltraBlame original commit: 690a453b87686307a21458c9966dd75ce88a76f0

---
+++ toolkit/components/urlformatter/tests/unit/test_urlformatter.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function run_test() {
+  var formatter = Cc["@mozilla.org/toolkit/URLFormatterService;1"].
+                  getService(Ci.nsIURLFormatter);
+  var locale = Cc["@mozilla.org/chrome/chrome-registry;1"].
+               getService(Ci.nsIXULChromeRegistry).
+               getSelectedLocale('global');
+  var prefs = Cc['@mozilla.org/preferences-service;1'].
+              getService(Ci.nsIPrefBranch);
+
+  var upperUrlRaw = "http://%LOCALE%.%VENDOR%.foo/?name=%NAME%&id=%ID%&version=%VERSION%&platversion=%PLATFORMVERSION%&abid=%APPBUILDID%&pbid=%PLATFORMBUILDID%&app=%APP%";
+  var lowerUrlRaw = "http://%locale%.%vendor%.foo/?name=%name%&id=%id%&version=%version%&platversion=%platformversion%&abid=%appbuildid%&pbid=%platformbuildid%&app=%app%";
+  
+  var ulUrlRef = "http://" + locale + ".Mozilla.foo/?name=Url Formatter Test&id=urlformattertest@test.mozilla.org&version=1&platversion=2.0&abid=2007122405&pbid=2007122406&app=urlformatter test";
+  var multiUrl = "http://%VENDOR%.%VENDOR%.%NAME%.%VENDOR%.%NAME%";
+  var multiUrlRef = "http://Mozilla.Mozilla.Url Formatter Test.Mozilla.Url Formatter Test";
+
+  var pref = "xpcshell.urlformatter.test";
+  var str = Cc["@mozilla.org/supports-string;1"].
+            createInstance(Ci.nsISupportsString);
+  str.data = upperUrlRaw;
+  prefs.setComplexValue(pref, Ci.nsISupportsString, str);
+
+  do_check_eq(formatter.formatURL(upperUrlRaw), ulUrlRef);
+  do_check_eq(formatter.formatURLPref(pref), ulUrlRef);
+  
+  do_check_neq(formatter.formatURL(lowerUrlRaw), ulUrlRef);
+  do_check_eq(formatter.formatURL(multiUrl), multiUrlRef);
+}
