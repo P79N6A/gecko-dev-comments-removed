f46bae33143d17b5f1ea0bcd745166da538ac357
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:37 +0000

    JavaScript Test - regression test for bug 376052, by George Kangas
    UltraBlame original commit: 07cb5412aa47325633ff54c0e85685fa7f37ada2

---
+++ js/tests/js1_5/extensions/regress-376052.js
@@ -0,0 +1,102 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 376052;
+var summary = 'javascript.options.anonfunfix to allow function (){} expressions';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+
+  if (typeof window != 'undefined')
+  {
+    print('Test skipped. anonfunfix not configurable in browser.');
+    reportCompare(expect, actual, summary);
+  }
+  else
+  {
+    expect = 'No Error';
+    try
+    {
+      eval('function () {1;}');
+      actual = 'No Error';
+    }
+    catch(ex)
+    {
+      actual = ex + '';
+    }
+    reportCompare(expect, actual, summary + ': 1');
+
+    options('anonfunfix');
+
+    expect = 'No Error';
+    try
+    {
+      eval('(function () {1;})');
+      actual = 'No Error';
+    }
+    catch(ex)
+    {
+      actual = ex + '';
+    }
+    reportCompare(expect, actual, summary + ': 2');
+
+    expect = 'SyntaxError: syntax error';
+    try
+    {
+      eval('function () {1;}');
+      actual = 'No Error';
+    }
+    catch(ex)
+    {
+      actual = ex + '';
+    }
+    reportCompare(expect, actual, summary + ': 3');
+
+  }
+
+  exitFunc ('test');
+}
