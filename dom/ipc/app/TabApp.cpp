9b317a1720ddf629fdb125bf3fb3bf3f725f7cc3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:58:28 +0000

    Minor fixups... not sure why we needed unistd.h here, but Windows doesn't have it.
    UltraBlame original commit: e49b2b3c9a8b362da8aba0fbb9841b4a908cdd02

--- dom/ipc/app/TabApp.cpp
+++ dom/ipc/app/TabApp.cpp
@@ -40,11 +40,6 @@
 #include "nsXPCOM.h"
 #include "nsXULAppAPI.h"
 
-
-
-#include <unistd.h>
-
-
 #ifdef XP_WIN
 #include <windows.h>
 
@@ -64,4 +59,6 @@ main(int argc, char* argv[])
     ScopedLogging log;
     nsresult rv = XRE_InitChildProcess(argc, argv, "TabThread");
     NS_ENSURE_SUCCESS(rv, 1);
+
+    return 0;
 }
