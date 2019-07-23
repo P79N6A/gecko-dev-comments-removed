ebf3758b68f3355adbc592e9e21821493a15c738
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:52:46 +0000

    Bug 366527 - build 1-1 relation between tab and tabpanel for accessible object, r=ginn.chen, a=dsicore
    UltraBlame original commit: f1d72fa0784f3fc9cf40c407d8c0aeb3e55f1ed6

--- accessible/src/base/nsAccessibilityService.cpp
+++ accessible/src/base/nsAccessibilityService.cpp
@@ -1925,7 +1925,20 @@ nsAccessibilityService::GetAccessibleForDeckChildren(nsIDOMNode *aNode, nsIAcces
                 frame->GetType() == nsAccessibilityAtoms::scrollFrame)) { 
     parentFrame = frame->GetParent();
     if (parentFrame && parentFrame->GetType() == nsAccessibilityAtoms::deckFrame) {
-      *aAccessible = new nsEnumRoleAccessible(aNode, weakShell, nsIAccessibleRole::ROLE_PROPERTYPAGE);
+      
+      
+      nsCOMPtr<nsIContent> parentContent = parentFrame->GetContent();
+      if (parentContent->NodeInfo()->Equals(nsAccessibilityAtoms::tabpanels,
+                                            kNameSpaceID_XUL)) {
+        *aAccessible = new nsXULTabpanelAccessible(aNode, weakShell);
+      } else {
+        *aAccessible =
+          new nsEnumRoleAccessible(aNode, weakShell,
+                                   nsIAccessibleRole::ROLE_PROPERTYPAGE);
+      }
+
+      NS_ENSURE_TRUE(*aAccessible, NS_ERROR_OUT_OF_MEMORY);
+
       NS_ADDREF(*aAccessible);
     }
   }
--- accessible/src/xul/nsXULTabAccessible.h
+++ accessible/src/xul/nsXULTabAccessible.h
@@ -44,17 +44,25 @@
 #include "nsXULSelectAccessible.h"
 
 
+
+
 class nsXULTabAccessible : public nsLeafAccessible
 {
 public:
   enum { eAction_Switch = 0 };
 
   nsXULTabAccessible(nsIDOMNode* aNode, nsIWeakReference* aShell);
+
+  
   NS_IMETHOD GetRole(PRUint32 *_retval); 
   NS_IMETHOD GetState(PRUint32 *aState, PRUint32 *aExtraState);
   NS_IMETHOD GetNumActions(PRUint8 *_retval);
   NS_IMETHOD GetActionName(PRUint8 aIndex, nsAString& aName);
   NS_IMETHOD DoAction(PRUint8 index);
+  NS_IMETHOD GetAccessibleRelated(PRUint32 aRelationType,
+                                  nsIAccessible **aRelatedAccessible);
+
+  
   virtual nsresult GetAttributesInternal(nsIPersistentProperties *aAttributes);
 };
 
@@ -73,6 +81,8 @@ public:
 };
 
 
+
+
 class nsXULTabsAccessible : public nsXULSelectableAccessible
 {
 public:
@@ -84,4 +94,22 @@ public:
   NS_IMETHOD GetName(nsAString& _retval);
 };
 
+
+
+
+
+
+
+class nsXULTabpanelAccessible : public nsAccessibleWrap
+{
+public:
+  nsXULTabpanelAccessible(nsIDOMNode *aNode, nsIWeakReference *aShell);
+
+  
+  NS_IMETHOD GetRole(PRUint32 *aRole);
+  NS_IMETHOD GetAccessibleRelated(PRUint32 aRelationType,
+                                  nsIAccessible **aRelatedAccessible);
+};
+
 #endif
+
