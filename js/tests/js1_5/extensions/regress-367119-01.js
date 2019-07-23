a80724e4ce818e2e2f3fdd3de2eeb6475cbb47b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:39 +0000

    JavaScript Test - regression test for bug 367119, by moz_bu_r_a4
    UltraBlame original commit: 3811b3f6df7b0fd6558dff9776f987300f6ac9f4

---
+++ js/tests/js1_5/extensions/regress-367119-01.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 367119;
+var summary = 'memory corruption in script_exec';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  if (typeof Script == 'undefined')
+  {
+    print('Test skipped. Script or toSource not defined');
+  }
+  else
+  {
+    var s = new Script("");
+    var o = {
+      valueOf : function() {
+        s.compile("");
+        Array(11).join(Array(11).join(Array(101).join("aaaaa")));
+        return {};
+      }
+    };
+    s.exec(o);
+  }
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
---
+++ js/tests/js1_5/extensions/regress-367119-02.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 367119;
+var summary = 'memory corruption in script_exec';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  if (typeof Script == 'undefined')
+  {
+    print('Test skipped. Script or toSource not defined');
+  }
+  else
+  {
+    var s = new Script("");
+    var o = {
+      valueOf : function() {
+        s.compile("");
+        print(1);
+        return {};
+      }
+    };
+    s.exec(o);
+  }
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
