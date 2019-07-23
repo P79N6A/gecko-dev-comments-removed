9deabbdcc7934546643a5d6b0e4d0e80fbce642e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:54:20 +0000

    Bug 405241 - "Don't register nsDownloadHistory if it's not needed" [p=comrade693+bmo@gmail.com (Shawn Wilsher [sdwilsh]) r=biesi sr=bzbarsky a1.9=damons]
    UltraBlame original commit: 7526fd0d10291e2b6e9bff279942039857bed513

--- docshell/build/nsDocShellModule.cpp
+++ docshell/build/nsDocShellModule.cpp
@@ -250,7 +250,8 @@ static const nsModuleComponentInfo gDocShellModuleInfo[] = {
     
     
     { "nsDownloadHistory", NS_DOWNLOADHISTORY_CID,
-      NS_DOWNLOADHISTORY_CONTRACTID, nsDownloadHistoryConstructor }
+      nsnull, nsDownloadHistoryConstructor,
+      nsDownloadHistory::RegisterSelf }
 };
 
 
