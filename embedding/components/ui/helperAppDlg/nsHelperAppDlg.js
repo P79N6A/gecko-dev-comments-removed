73b23650d2049a7b6366da348c23c15eb13f95f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:45:00 +0000

    Fix SeaMonkey regression from bug 384374 landing (no bug); r+sr=bz
    UltraBlame original commit: dc97671a75cc30154358ce0300e21b5536169df6

--- embedding/components/ui/helperAppDlg/nsHelperAppDlg.js
+++ embedding/components/ui/helperAppDlg/nsHelperAppDlg.js
@@ -641,7 +641,8 @@ nsHelperAppDialog.prototype = {
         
         if ( this.dialogElement( "openUsing" ).selected ) {
             var helperApp = this.helperAppChoice();
-            if ( !helperApp || !helperApp.executable || !helperApp.exists() ) {
+            if ( !helperApp || !helperApp.executable ||
+                 !helperApp.executable.exists() ) {
                 
                 var msg = this.replaceInsert( this.getString( "badApp" ), 1, this.dialogElement( "appPath" ).value );
                 var svc = Components.classes[ "@mozilla.org/embedcomp/prompt-service;1" ]
