5910b6cd9b0d36ff08d07ae2b339ca8ffe62c983
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:58:27 +0000

    Bug 412127: Canceling extension installation leaks nsXPITriggerInfo and nsXPITriggerItem. r=dveditz, sr=sicking
    UltraBlame original commit: 7cb5ee9040ed14cab4da8a63089e240f20898876

--- xpinstall/src/nsXPInstallManager.cpp
+++ xpinstall/src/nsXPInstallManager.cpp
@@ -362,13 +362,7 @@ nsXPInstallManager::InitManagerInternal()
     if ( cbstatus != 0 )
     {
         
-        for (PRUint32 i = 0; i < mTriggers->Size(); i++)
-        {
-            mTriggers->SendStatus( mTriggers->Get(i)->mURL.get(), cbstatus );
-        }
-
-        
-        NS_RELEASE_THIS();
+        Shutdown( cbstatus );
     }
 
     return rv;
@@ -869,7 +863,7 @@ PRBool nsXPInstallManager::VerifyHash(nsXPITriggerItem* aItem)
 }
 
 
-void nsXPInstallManager::Shutdown()
+void nsXPInstallManager::Shutdown(PRInt32 status)
 {
     if (mDlg)
     {
@@ -890,8 +884,7 @@ void nsXPInstallManager::Shutdown()
             item = (nsXPITriggerItem*)mTriggers->Get(mNextItem++);
             if ( item && !item->mURL.IsEmpty() )
             {
-                mTriggers->SendStatus( item->mURL.get(),
-                                       nsInstall::USER_CANCELLED );
+                mTriggers->SendStatus( item->mURL.get(), status );
             }
         }
 
