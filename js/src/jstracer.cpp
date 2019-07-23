361d03d09a42978f408cbc4a9fbd34c0dbc3b16f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:35:54 +0000

    Add dummy vpname/vpnum parameters when not compiling in DEBUG mode.
    UltraBlame original commit: eeec9337de5e49c86806fd8959d887681cccdb71

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -311,11 +311,9 @@ public:
 
 
 #ifdef DEBUG
-#define DEF_VPNAME          char* vpname; unsigned vpnum;
 #define SET_VPNAME(name)    do { vpname = name; vpnum = 0; } while(0)
 #define INC_VPNUM()         do { ++vpnum; } while(0)
 #else
-#define DEF_VPNAME          ((void)0)
 #define SET_VPNAME(name)    ((void)0)
 #define INC_VPNUM()         ((void)0)
 #endif
@@ -326,7 +324,7 @@ public:
 
 #define FORALL_SLOTS_IN_PENDING_FRAMES(entryFrame, currentFrame, code)        \
     JS_BEGIN_MACRO                                                            \
-        DEF_VPNAME;                                                           \
+        char* vpname = ""; unsigned vpnum = 0;                                \
         /* find us the global frame */                                        \
         JSStackFrame* global = entryFrame;                                    \
         while (global->down)                                                  \
