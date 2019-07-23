5291d8db438bf2863a22ad07b30b333f4b558ebd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:42:18 +0000

    Bug 512173 Removing some unused references to the message-icon class r=dao
    UltraBlame original commit: 29e8f7716f6da58aacb1e99e33a34cd97d70a348

--- toolkit/content/commonDialog.js
+++ toolkit/content/commonDialog.js
@@ -166,9 +166,8 @@ function commonDialogOnLoad()
   
   var iconElement = document.getElementById("info.icon");
   var iconClass = gCommonDialogParam.GetString(2);
-  if (!iconClass)
-    iconClass = "message-icon";
-  iconElement.setAttribute("class", iconElement.getAttribute("class") + " " + iconClass);
+  if (iconClass)
+    iconElement.classList.add(iconClass);
 
   switch (nButtons) {
     case 4:
