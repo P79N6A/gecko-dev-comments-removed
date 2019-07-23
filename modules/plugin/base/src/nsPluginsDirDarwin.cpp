81cb8f4db46ff6bf65607b322206328be28ec40b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:23:08 +0000

    Bug 438394 – land workaround patch for unkillable hangs when loading VerifiedDownloadPlugin.plugin on trunk. r=josh
    UltraBlame original commit: fb635b59dea3f6afb705cc66f771fb87e65530de

--- modules/plugin/base/src/nsPluginsDirDarwin.cpp
+++ modules/plugin/base/src/nsPluginsDirDarwin.cpp
@@ -241,6 +241,16 @@ nsresult nsPluginFile::LoadPlugin(PRLibrary* &outLibrary)
         return NS_ERROR_NULL_POINTER;
 
     nsCAutoString temp;
+    mPlugin->GetNativeLeafName(temp);
+    
+
+
+
+    if (!strcmp(temp.get(), "VerifiedDownloadPlugin.plugin")) {
+        NS_WARNING("Preventing load of VerifiedDownloadPlugin.plugin (see bug 436575)");
+        return NS_ERROR_FAILURE;
+    }
+
     mPlugin->GetNativePath(temp);
     path = temp.get();
 
