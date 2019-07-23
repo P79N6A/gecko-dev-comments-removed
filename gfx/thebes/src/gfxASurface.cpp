d73be8dee11dcd108cae234152fdf689176eeb25
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:29 +0000

    more backouts
    UltraBlame original commit: 443766494fd63286b463d81f100d97f2881891d0

--- gfx/thebes/src/gfxASurface.cpp
+++ gfx/thebes/src/gfxASurface.cpp
@@ -1,40 +1,40 @@
-/* -*- Mode: C++; tab-width: 20; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla Foundation code.
- *
- * The Initial Developer of the Original Code is Mozilla Foundation.
- * Portions created by the Initial Developer are Copyright (C) 2006
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *   Stuart Parmenter <stuart@mozilla.com>
- *   Vladimir Vukicevic <vladimir@pobox.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #include "gfxASurface.h"
 
@@ -50,7 +50,7 @@
 #include "gfxXlibSurface.h"
 #endif
 
-#ifdef CAIRO_HAS_QUARTZ_SURFACE
+#ifdef CAIRO_HAS_NQUARTZ_SURFACE
 #include "gfxQuartzSurface.h"
 #endif
 
@@ -58,15 +58,15 @@
 
 static cairo_user_data_key_t gfxasurface_pointer_key;
 
-// Surfaces use refcounting that's tied to the cairo surface refcnt, to avoid
-// refcount mismatch issues.
+
+
 nsrefcnt
 gfxASurface::AddRef(void)
 {
     NS_PRECONDITION(mSurface != nsnull, "gfxASurface::AddRef without mSurface");
 
     if (mHasFloatingRef) {
-        // eat the floating ref
+        
         mHasFloatingRef = PR_FALSE;
     } else {
         cairo_surface_reference(mSurface);
@@ -80,13 +80,13 @@ gfxASurface::Release(void)
 {
     NS_PRECONDITION(!mHasFloatingRef, "gfxASurface::Release while floating ref still outstanding!");
     NS_PRECONDITION(mSurface != nsnull, "gfxASurface::Release without mSurface");
-    // Note that there is a destructor set on user data for mSurface,
-    // which will delete this gfxASurface wrapper when the surface's refcount goes
-    // out of scope.
+    
+    
+    
     nsrefcnt refcnt = (nsrefcnt) cairo_surface_get_reference_count(mSurface);
     cairo_surface_destroy(mSurface);
 
-    // |this| may not be valid any more, don't use it!
+    
 
     return --refcnt;
 }
@@ -94,7 +94,7 @@ gfxASurface::Release(void)
 void
 gfxASurface::SurfaceDestroyFunc(void *data) {
     gfxASurface *surf = (gfxASurface*) data;
-    // fprintf (stderr, "Deleting wrapper for %p (wrapper: %p)\n", surf->mSurface, data);
+    
     delete surf;
 }
 
@@ -115,15 +115,15 @@ gfxASurface::Wrap (cairo_surface_t *csurf)
 {
     gfxASurface *result;
 
-    /* Do we already have a wrapper for this surface? */
+    
     result = GetSurfaceWrapper(csurf);
     if (result) {
-        // fprintf(stderr, "Existing wrapper for %p -> %p\n", csurf, result);
+        
         NS_ADDREF(result);
         return result;
     }
 
-    /* No wrapper; figure out the surface type and create it */
+    
     cairo_surface_type_t stype = cairo_surface_get_type(csurf);
 
     if (stype == CAIRO_SURFACE_TYPE_IMAGE) {
@@ -139,8 +139,8 @@ gfxASurface::Wrap (cairo_surface_t *csurf)
         result = new gfxXlibSurface(csurf);
     }
 #endif
-#ifdef CAIRO_HAS_QUARTZ_SURFACE
-    else if (stype == CAIRO_SURFACE_TYPE_QUARTZ) {
+#ifdef CAIRO_HAS_NQUARTZ_SURFACE
+    else if (stype == CAIRO_SURFACE_TYPE_NQUARTZ) {
         result = new gfxQuartzSurface(csurf);
     }
 #endif
@@ -148,7 +148,7 @@ gfxASurface::Wrap (cairo_surface_t *csurf)
         result = new gfxUnknownSurface(csurf);
     }
 
-    // fprintf(stderr, "New wrapper for %p -> %p\n", csurf, result);
+    
 
     NS_ADDREF(result);
     return result;
