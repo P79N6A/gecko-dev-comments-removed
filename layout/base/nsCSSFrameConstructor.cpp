171e31a8f407459fbca3aa096239a224861216b9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:45:32 +0000

    Remove bogus assert.  Bug 350128, r+sr=roc, a=dbaron
    UltraBlame original commit: d0971bca5f7953038778ea64e0f160c9bfc2950e

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -10772,8 +10772,6 @@ nsCSSFrameConstructor::FindFrameWithContent(nsFrameManager*  aFrameManager,
               kidFrame = parentFrame->GetFirstChild(listName);
               
               
-              NS_ASSERTION(!kidFrame || parentFrame->GetContent() == aParentContent,
-                           "next-in-flow has different content");
             }
           }
 #ifdef NOISY_FINDFRAME
