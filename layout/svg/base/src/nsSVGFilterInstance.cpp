5d99b74c93082c4158b21e2076c072a8790dc570
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:23:01 +0000

    Bug 448243. When nsSVGFilterInstance needs to create a dummy input image, create one of the right size. r=jwatt,sr=mats
    UltraBlame original commit: 2c8e43e57337dff25a1181bc6b97db8fe0b1d5ef

--- layout/svg/base/src/nsSVGFilterInstance.cpp
+++ layout/svg/base/src/nsSVGFilterInstance.cpp
@@ -479,8 +479,7 @@ nsSVGFilterInstance::Render(gfxASurface** aOutput)
       if (!input->mImage.mImage) {
         
         
-        input->mImage.mImage =
-          new gfxImageSurface(gfxIntSize(1, 1), gfxASurface::ImageFormatARGB32);
+        input->mImage.mImage = CreateImage();
         if (!input->mImage.mImage)
           return NS_ERROR_OUT_OF_MEMORY;
       }
