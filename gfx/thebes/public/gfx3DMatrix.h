e00296a65e9418215cd1ba3eaec4572149700cae
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:20:22 +0000

    Bug 538266. Part 1: create gfx3DMatrix. r=jrmuizel
    UltraBlame original commit: 86d55856479f43f0a85dc3da20371ab4838f5cec

---
+++ gfx/thebes/public/gfx3DMatrix.h
@@ -0,0 +1,193 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef GFX_3DMATRIX_H
+#define GFX_3DMATRIX_H
+
+#include <gfxTypes.h>
+#include <gfxMatrix.h>
+#include <math.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class THEBES_API gfx3DMatrix
+{
+public:
+  
+
+
+  inline gfx3DMatrix(void);
+
+  
+
+
+  inline gfx3DMatrix operator*(const gfx3DMatrix &aMatrix);
+
+  
+
+
+
+
+  static inline gfx3DMatrix From2D(const gfxMatrix &aMatrix);
+
+  
+
+
+
+
+  PRBool Is2D(gfxMatrix* aMatrix = nsnull) const;
+
+  
+
+
+
+  inline PRBool IsIdentity() const;
+
+  
+
+
+
+
+
+
+  static inline gfx3DMatrix Translation(float aX, float aY, float aZ);
+
+  
+
+
+
+
+  static inline gfx3DMatrix Scale(float aFactor);
+
+  
+  float _11, _12, _13, _14;
+  float _21, _22, _23, _24;
+  float _31, _32, _33, _34;
+  float _41, _42, _43, _44;
+};
+
+inline
+gfx3DMatrix::gfx3DMatrix(void)
+{
+  _11 = _22 = _33 = _44 = 1.0f;
+  _12 = _13 = _14 = 0.0f;
+  _21 = _23 = _24 = 0.0f;
+  _31 = _32 = _34 = 0.0f;
+  _41 = _42 = _43 = 0.0f;
+}
+
+inline gfx3DMatrix
+gfx3DMatrix::operator*(const gfx3DMatrix &aMatrix)
+{
+  gfx3DMatrix matrix;
+
+  matrix._11 = _11 * aMatrix._11 + _12 * aMatrix._21 + _13 * aMatrix._31 + _14 * aMatrix._41;
+  matrix._21 = _21 * aMatrix._11 + _22 * aMatrix._21 + _23 * aMatrix._31 + _24 * aMatrix._41;
+  matrix._31 = _31 * aMatrix._11 + _32 * aMatrix._21 + _33 * aMatrix._31 + _34 * aMatrix._41;
+  matrix._41 = _41 * aMatrix._11 + _42 * aMatrix._21 + _43 * aMatrix._31 + _44 * aMatrix._41;
+  matrix._12 = _11 * aMatrix._12 + _12 * aMatrix._22 + _13 * aMatrix._32 + _14 * aMatrix._42;
+  matrix._22 = _21 * aMatrix._12 + _22 * aMatrix._22 + _23 * aMatrix._32 + _24 * aMatrix._42;
+  matrix._32 = _31 * aMatrix._12 + _32 * aMatrix._22 + _33 * aMatrix._32 + _34 * aMatrix._42;
+  matrix._42 = _41 * aMatrix._12 + _42 * aMatrix._22 + _43 * aMatrix._32 + _44 * aMatrix._42;
+  matrix._13 = _11 * aMatrix._13 + _12 * aMatrix._23 + _13 * aMatrix._33 + _14 * aMatrix._43;
+  matrix._23 = _21 * aMatrix._13 + _22 * aMatrix._23 + _23 * aMatrix._33 + _24 * aMatrix._43;
+  matrix._33 = _31 * aMatrix._13 + _32 * aMatrix._23 + _33 * aMatrix._33 + _34 * aMatrix._43;
+  matrix._43 = _41 * aMatrix._13 + _42 * aMatrix._23 + _43 * aMatrix._33 + _44 * aMatrix._43;
+  matrix._14 = _11 * aMatrix._14 + _12 * aMatrix._24 + _13 * aMatrix._34 + _14 * aMatrix._44;
+  matrix._24 = _21 * aMatrix._14 + _22 * aMatrix._24 + _23 * aMatrix._34 + _24 * aMatrix._44;
+  matrix._34 = _31 * aMatrix._14 + _32 * aMatrix._24 + _33 * aMatrix._34 + _34 * aMatrix._44;
+  matrix._44 = _41 * aMatrix._14 + _42 * aMatrix._24 + _43 * aMatrix._34 + _44 * aMatrix._44;
+
+  return matrix;
+}
+
+inline gfx3DMatrix
+gfx3DMatrix::From2D(const gfxMatrix &aMatrix)
+{
+  gfx3DMatrix matrix;
+  matrix._11 = (float)aMatrix.xx;
+  matrix._12 = (float)aMatrix.yx;
+  matrix._21 = (float)aMatrix.xy;
+  matrix._22 = (float)aMatrix.yy;
+  matrix._41 = (float)aMatrix.x0;
+  matrix._42 = (float)aMatrix.y0;
+  return matrix;
+}
+
+inline PRBool
+gfx3DMatrix::IsIdentity() const
+{
+  return _11 == 1.0f && _12 == 0.0f && _13 == 0.0f && _14 == 0.0f &&
+         _21 == 0.0f && _22 == 1.0f && _23 == 0.0f && _24 == 0.0f &&
+         _31 == 0.0f && _32 == 0.0f && _33 == 1.0f && _34 == 0.0f &&
+         _41 == 0.0f && _42 == 0.0f && _43 == 0.0f && _44 == 1.0f;
+}
+
+inline gfx3DMatrix
+gfx3DMatrix::Translation(float aX, float aY, float aZ)
+{
+  gfx3DMatrix matrix;
+
+  matrix._41 = aX;
+  matrix._42 = aY;
+  matrix._43 = aZ;
+  return matrix;
+}
+
+inline gfx3DMatrix
+gfx3DMatrix::Scale(float aFactor)
+{
+  gfx3DMatrix matrix;
+
+  matrix._11 = matrix._22 = matrix._33 = aFactor;
+  return matrix;
+}
+
+#endif 
