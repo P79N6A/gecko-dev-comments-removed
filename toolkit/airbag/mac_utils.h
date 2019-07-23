5d3237280999d4d36aa5edd46b54411373b51234
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:26 +0000

    bug 382541 - Mozilla Crash Reporter supresses writing of Mac OS X crash log - short term workaround.  r=mento
    UltraBlame original commit: f4b9ea4ac22aed5e945ef454e4ab3c7613f85b8f

---
+++ toolkit/airbag/mac_utils.h
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef toolkit_breakpad_mac_utils_h__
+#define toolkit_breakpad_mac_utils_h__
+
+
+
+
+
+bool PassToOSCrashReporter();
+
+#endif 
