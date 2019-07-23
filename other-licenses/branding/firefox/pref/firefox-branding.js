a56e6115654f9ae9e1d70b330cc423f0c736c949
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:57:36 +0000

    Bug 454300 - "Remove locale-specific subdomains from client-generated URLs" [r=gavin]
    UltraBlame original commit: 5f761972d7455f3eea9c59f235c3c24a7c7e4604

--- other-licenses/branding/firefox/pref/firefox-branding.js
+++ other-licenses/branding/firefox/pref/firefox-branding.js
@@ -1,5 +1,5 @@
-pref("startup.homepage_override_url","http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/%VERSION%/whatsnew/");
-pref("startup.homepage_welcome_url","http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/%VERSION%/firstrun/");
+pref("startup.homepage_override_url","http://www.mozilla.com/%LOCALE%/%APP%/%VERSION%/whatsnew/");
+pref("startup.homepage_welcome_url","http://www.mozilla.com/%LOCALE%/%APP%/%VERSION%/firstrun/");
 
 
 pref("app.update.interval", 86400);
@@ -11,10 +11,10 @@ pref("app.update.download.backgroundInterval", 600);
 pref("app.update.url.manual", "http://www.firefox.com");
 
 
-pref("app.update.url.details", "http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/releases/");
+pref("app.update.url.details", "http://www.mozilla.com/%LOCALE%/%APP%/releases/");
 
 
-pref("app.releaseNotesURL", "http://%LOCALE%.www.mozilla.com/%LOCALE%/%APP%/%VERSION%/releasenotes/");
+pref("app.releaseNotesURL", "http://www.mozilla.com/%LOCALE%/%APP%/%VERSION%/releasenotes/");
 
 pref("browser.search.param.yahoo-fr", "moz35");
 pref("browser.search.param.yahoo-fr-cjkt", "moz35");
