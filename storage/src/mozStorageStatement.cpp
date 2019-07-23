8d1d4503a59498921b5208bc87704b8a2ece54e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:44:17 +0000

    Bug 452836 - Optimize calls to sqlite3_prepare_v2 This makes sqlite not copy the sql string when preparing it, saving us a string copy every time. r=bholley
    UltraBlame original commit: 6c46bdf4a7d7b910f3b96acd8ca284ae32f0f646

--- storage/src/mozStorageStatement.cpp
+++ storage/src/mozStorageStatement.cpp
@@ -114,8 +114,11 @@ mozStorageStatement::Initialize(mozStorageConnection *aDBConnection,
     NS_ENSURE_TRUE(db != nsnull, NS_ERROR_NULL_POINTER);
 
     int srv;
-    srv = sqlite3_prepare_v2(db, nsPromiseFlatCString(aSQLStatement).get(),
-                             aSQLStatement.Length(), &mDBStatement, NULL);
+    
+    
+    
+    srv = sqlite3_prepare_v2(db, PromiseFlatCString(aSQLStatement).get(),
+                             aSQLStatement.Length() + 1, &mDBStatement, NULL);
     if (srv != SQLITE_OK) {
 #ifdef PR_LOGGING
         PR_LOG(gStorageLog, PR_LOG_ERROR, ("Sqlite statement prepare error: %d '%s'", srv, sqlite3_errmsg(db)));
