6fea89ae9639ea2ca9460cdd1514920c3298a3ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:23:58 +0000

    Change plugin unloading behavior to never unload plugins by default. Prior to this patch plugins were unloaded whenever their last instance was destroyed. The old behavior meant that going from one page with a Flash instance to another page with a Flash instance would shut down the entire Flash plugin instead of just destroying one instance and starting another. The new behavior dramatically cuts down on plugin loading/unloading activity. Set "plugins.unloadASAP" to "true" to enable the old behavior of unloading plugins as soon as their last instance is destroyed. b=500925 r=bz sr=jst
    UltraBlame original commit: 61a70b55686d70ad3a84365322b02adefc11f42c

--- modules/plugin/base/src/nsPluginHostImpl.cpp
+++ modules/plugin/base/src/nsPluginHostImpl.cpp
@@ -418,6 +418,7 @@ PRBool nsPluginInstanceTagList::remove(nsPluginInstanceTag * plugin)
   for (nsPluginInstanceTag * p = mFirst; p != nsnull; p = p->mNext) {
     if (p == plugin) {
       PRBool lastInstance = IsLastInstance(p);
+      nsPluginTag *pluginTag = p->mPluginTag;
 
       if (p == mFirst)
         mFirst = p->mNext;
@@ -427,23 +428,18 @@ PRBool nsPluginInstanceTagList::remove(nsPluginInstanceTag * plugin)
       if (prev && !prev->mNext)
         mLast = prev;
 
-      
-      
-      
-      if (lastInstance) {
-        
-        nsPluginTag *pluginTag = p->mPluginTag;
+      delete p;
 
-        delete p; 
+      if (lastInstance && pluginTag) {
+        nsresult rv;
+        nsCOMPtr<nsIPrefBranch> pref(do_GetService(NS_PREFSERVICE_CONTRACTID, &rv));
+        NS_ENSURE_SUCCESS(rv, rv);
 
-        if (pluginTag)
+        PRBool unloadPluginsASAP = PR_FALSE;
+        rv = pref->GetBoolPref("plugins.unloadASAP", &unloadPluginsASAP);
+        if (NS_SUCCEEDED(rv) && unloadPluginsASAP)
           pluginTag->TryUnloadPlugin();
-        else
-          NS_ASSERTION(pluginTag, "pluginTag was not set, plugin not shutdown");
-
       }
-      else
-        delete p;
 
       mCount--;
       return PR_TRUE;
@@ -801,7 +797,7 @@ nsPluginTag::nsPluginTag(const char* aName,
 
 nsPluginTag::~nsPluginTag()
 {
-  TryUnloadPlugin(PR_TRUE);
+  TryUnloadPlugin();
 
   
   
@@ -1015,7 +1011,7 @@ nsresult PostPluginUnloadEvent(PRLibrary* aLibrary)
   return NS_ERROR_FAILURE;
 }
 
-void nsPluginTag::TryUnloadPlugin(PRBool aForceShutdown)
+void nsPluginTag::TryUnloadPlugin()
 {
   if (mEntryPoint) {
     mEntryPoint->Shutdown();
@@ -1026,9 +1022,10 @@ void nsPluginTag::TryUnloadPlugin(PRBool aForceShutdown)
   
   if (mLibrary && mCanUnloadLibrary) {
     
-    if (!mXPConnected)
+    if (!mXPConnected) {
       
       PostPluginUnloadEvent(mLibrary);
+    }
     else {
       
       if (mPluginHost)
