8b3cb11963a6c29041f0b3febe97f7218c833cf1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:28:10 +0000

    Bug 443871 - "Protect against null objects in a title's scope/map". r=brendan.
    UltraBlame original commit: 5f878cdd1d5e1233789faa80fe377ccaeb9f7641

--- js/src/jslock.cpp
+++ js/src/jslock.cpp
@@ -405,18 +405,20 @@ js_FinishSharingTitle(JSContext *cx, JSTitle *title)
     scope = (JSScope *)map;
 
     obj = scope->object;
-    nslots = scope->map.freeslot;
-    for (i = 0; i != nslots; ++i) {
-        v = STOBJ_GET_SLOT(obj, i);
-        if (JSVAL_IS_STRING(v) &&
-            !js_MakeStringImmutable(cx, JSVAL_TO_STRING(v))) {
-            
+    if (obj) {
+        nslots = scope->map.freeslot;
+        for (i = 0; i != nslots; ++i) {
+            v = STOBJ_GET_SLOT(obj, i);
+            if (JSVAL_IS_STRING(v) &&
+                !js_MakeStringImmutable(cx, JSVAL_TO_STRING(v))) {
+                
 
 
 
 
 
-            STOBJ_SET_SLOT(obj, i, JSVAL_VOID);
+                STOBJ_SET_SLOT(obj, i, JSVAL_VOID);
+            }
         }
     }
 
