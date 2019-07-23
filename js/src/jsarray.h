fe5ca2ba820e978e969365efd7989d960d6cd612
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:41:43 +0000

    b=451242, add fast-paths for dense array to uint/int/double buffer conversion; r=bent/mrbkap
    UltraBlame original commit: 109ca6e80874e97a4dfa4accb6a514f1956d0e14

--- js/src/jsarray.h
+++ js/src/jsarray.h
@@ -136,6 +136,61 @@ extern JSBool
 js_array_join_sub(JSContext *cx, JSObject *obj, enum ArrayToStringOp op,
                   JSString *sep, jsval *rval);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+JS_EXTERN_API(JSBool)
+js_ArrayToJSUint8Buffer(JSContext *cx, JSObject *obj, jsuint offset, jsuint count,
+                        JSUint8 *dest);
+
+JS_EXTERN_API(JSBool)
+js_ArrayToJSUint16Buffer(JSContext *cx, JSObject *obj, jsuint offset, jsuint count,
+                         JSUint16 *dest);
+
+JS_EXTERN_API(JSBool)
+js_ArrayToJSUint32Buffer(JSContext *cx, JSObject *obj, jsuint offset, jsuint count,
+                         JSUint32 *dest);
+
+JS_EXTERN_API(JSBool)
+js_ArrayToJSInt8Buffer(JSContext *cx, JSObject *obj, jsuint offset, jsuint count,
+                       JSInt8 *dest);
+
+JS_EXTERN_API(JSBool)
+js_ArrayToJSInt16Buffer(JSContext *cx, JSObject *obj, jsuint offset, jsuint count,
+                        JSInt16 *dest);
+
+JS_EXTERN_API(JSBool)
+js_ArrayToJSInt32Buffer(JSContext *cx, JSObject *obj, jsuint offset, jsuint count,
+                        JSInt32 *dest);
+
+JS_EXTERN_API(JSBool)
+js_ArrayToJSDoubleBuffer(JSContext *cx, JSObject *obj, jsuint offset, jsuint count,
+                         jsdouble *dest);
+
 JS_END_EXTERN_C
 
 #endif 
