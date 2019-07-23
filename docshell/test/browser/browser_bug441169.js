6241126097fcecf37025642cf1e5fbf0df44f8ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:25:06 +0000

    Bug 441169 - [r=bzbarsky sr=dveditz]
    UltraBlame original commit: ed55fa85231dcff9c668ba6ce0834658ffdc7cd3

---
+++ docshell/test/browser/browser_bug441169.js
@@ -0,0 +1,26 @@
+
+var newBrowser
+
+
+
+
+var chromeURL = "about:neterror?e=nssBadCert&u=https%3A//test.kuix.de/&c=UTF-8&d=This%20sentence%20should%20not%20be%20parsed%20to%20include%20a%20%3Cspan%20id=%22test_span%22%3Enamed%3C/span%3E%20span%20tag.%0A%0AThe%20certificate%20is%20only%20valid%20for%20%3Ca%20id=%22cert_domain_link%22%20title=%22kuix.de%22%3Ekuix.de%3C/a%3E%0A%0A(Error%20code%3A%20ssl_error_bad_cert_domain)";
+
+function test() {
+  waitForExplicitFinish();
+  
+  var newTab = gBrowser.addTab();
+  gBrowser.selectedTab = newTab;
+  newBrowser = gBrowser.getBrowserForTab(newTab);
+  
+  window.addEventListener("DOMContentLoaded", checkPage, false);
+  newBrowser.contentWindow.location = chromeURL;
+}
+
+function checkPage() {
+  
+  is(newBrowser.contentDocument.getElementById("test_span"), null, "Error message should not be parsed as HTML, and hence shouldn't include the 'test_span' element.");
+  
+  gBrowser.removeCurrentTab();
+  finish();
+}
