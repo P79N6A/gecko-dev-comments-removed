d02d9817192da84919612fb4c65cb2dd2849d2f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:00:26 +0000

    Bug 534407 -  Crash [@ nsPrintEngine::DoPrint(nsPrintObject*)] while printing, r=roc
    UltraBlame original commit: 1c71987688fe9b330b4b7e9a35bece2b852ef690

--- layout/printing/nsPrintEngine.cpp
+++ layout/printing/nsPrintEngine.cpp
@@ -1878,8 +1878,10 @@ nsPrintEngine::ReflowPrintObject(nsPrintObject * aPO)
     }
     
     
-    if (!frame)
+    if (!frame) {
+      aPO->mDontPrint = PR_TRUE;
       return NS_OK;
+    }
 
     adjSize = frame->GetContentRect().Size();
     documentIsTopLevel = PR_FALSE;
