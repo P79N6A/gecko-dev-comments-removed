d6791b3a3a11930d29a0639f91a7b1e137de03d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:37:28 +0000

    Add fields to nsCSSPropList.h for style struct offset and animation type.  (Bug 504652)  r=bzbarsky
    UltraBlame original commit: d1b659f7db49191f59723ddb69354cd49dae29cb

--- layout/style/nsCSSProps.h
+++ layout/style/nsCSSProps.h
@@ -65,6 +65,13 @@
 
 #define CSS_PROPERTY_IGNORED_WHEN_COLORS_DISABLED (1<<4)
 
+
+
+
+enum nsStyleAnimType {
+  eStyleAnimType_None 
+};
+
 class nsCSSProps {
 public:
   static void AddRefTable(void);
@@ -109,6 +116,9 @@ public:
   static const nsCSSType       kTypeTable[eCSSProperty_COUNT_no_shorthands];
   static const nsStyleStructID kSIDTable[eCSSProperty_COUNT_no_shorthands];
   static const PRInt32* const  kKeywordTableTable[eCSSProperty_COUNT_no_shorthands];
+  static const nsStyleAnimType kAnimTypeTable[eCSSProperty_COUNT_no_shorthands];
+  static const ptrdiff_t
+    kStyleStructOffsetTable[eCSSProperty_COUNT_no_shorthands];
 
 private:
   static const PRUint32        kFlagsTable[eCSSProperty_COUNT];
