d0941dd2bfa27c9ff03d41aa48a9a6626256b2a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:13:41 +0000

    Bug 451958. Implement content-policy and CheckLoadURI checks for video sources. r+sr=roc,a=beltzner
    UltraBlame original commit: 73fb44504b62b597e35c826cd8f5fb10e4771d53

--- content/media/video/src/nsMediaDecoder.cpp
+++ content/media/video/src/nsMediaDecoder.cpp
@@ -51,6 +51,7 @@
 #include "gfxImageSurface.h"
 #include "nsPresContext.h"
 #include "nsMediaDecoder.h"
+#include "nsDOMError.h"
 
 #ifdef PR_LOGGING
 
@@ -235,3 +236,7 @@ void nsMediaDecoder::ElementUnavailable()
   mElement = nsnull;
 }
 
+nsHTMLMediaElement* nsMediaDecoder::GetMediaElement()
+{
+  return mElement;
+}
