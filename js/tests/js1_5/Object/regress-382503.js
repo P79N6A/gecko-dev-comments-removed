66d94ecad204ce0ac72be117b41ab429db8db899
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:49:12 +0000

    JavaScript Test - regression test for bug 382503, by Jesse Ruderman, not part of the build
    UltraBlame original commit: 4c6e01d8f92026ff2bdde4fa39f8fc8c1a04165e

---
+++ js/tests/js1_5/Object/regress-382503.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-383503.js';
+
+var BUGNUMBER = 382503;
+var summary = 'Do not assert: with prototype=regexp';
+var actual = '';
+var expect = '';
+
+printBugNumber(BUGNUMBER);
+printStatus (summary);
+ 
+function f(x)
+{
+  prototype = /a/;
+
+  if (x) {
+    return /b/;
+    return /c/;
+  } else {
+    return /d/;
+  }
+}
+
+void f(false);
+
+reportCompare(expect, actual, summary);
