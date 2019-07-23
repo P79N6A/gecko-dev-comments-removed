8c2dd9cc8f9a1729f7d8a7e616fdd3980463cfe2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:37:18 +0000

    Bug 475548. Don't let the view manager's DefaultBackgroundColor be translucent. r+sr=roc
    UltraBlame original commit: 02dd72d71d837f0a4079c8ec5d6bed137e805d1f

--- view/src/nsViewManager.cpp
+++ view/src/nsViewManager.cpp
@@ -533,26 +533,38 @@ void nsViewManager::Refresh(nsView *aView, nsIRenderingContext *aContext,
 }
 
 
-void nsViewManager::DefaultRefresh(nsView* aView, nsIRenderingContext *aContext, const nsRect* aRect)
+void nsViewManager::DefaultRefresh(nsView* aView,
+                                   nsIRenderingContext *aContext,
+                                   const nsRect* aRect)
 {
   NS_PRECONDITION(aView, "Must have a view to work with!");
+
+  
   nsIWidget* widget = aView->GetNearestWidget(nsnull);
-  if (! widget)
+  if (!widget || widget->GetTransparencyMode() != eTransparencyOpaque)
     return;
 
   nsCOMPtr<nsIRenderingContext> context = aContext;
-  if (! aContext)
+  if (!context)
     context = CreateRenderingContext(*aView);
 
-  if (! context)
-    return;
-
-  nscolor bgcolor = mDefaultBackgroundColor;
-
-  if (NS_GET_A(mDefaultBackgroundColor) == 0) {
-    NS_WARNING("nsViewManager: Asked to paint a default background, but no default background color is set!");
+  
+  
+  if (!context) {
+    NS_WARNING("nsViewManager: No rendering context for DefaultRefresh");
     return;
   }
+
+  nscolor bgcolor = mDefaultBackgroundColor;
+  
+  
+  if (bgcolor == NS_RGBA(0,0,0,0)) {
+    NS_WARNING("nsViewManager: DefaultRefresh called with no background set");
+    bgcolor = NS_RGB(255,255,255);
+  }
+
+  NS_ASSERTION(NS_GET_A(bgcolor) == 255,
+               "nsViewManager: non-opaque background color snuck in");
 
   context->SetColor(bgcolor);
   context->FillRect(*aRect);
@@ -2221,6 +2233,7 @@ nsViewManager::ProcessInvalidateEvent()
 NS_IMETHODIMP
 nsViewManager::SetDefaultBackgroundColor(nscolor aColor)
 {
+  NS_ASSERTION(NS_GET_A(aColor) == 255, "default background must be opaque");
   mDefaultBackgroundColor = aColor;
   return NS_OK;
 }
