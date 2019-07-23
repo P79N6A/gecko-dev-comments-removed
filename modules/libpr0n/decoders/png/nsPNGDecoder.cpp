db17fd0bb760f6a0635f30fc44c5668d59654d4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:11:16 +0000

    Bug 411852 - Glitch in an aPNG image rendering and tab thumbnail. r=stuart, a1.9=beltzner
    UltraBlame original commit: 0bea627017604c3d9a6746d13d12678c8e7f08c9

--- modules/libpr0n/decoders/png/nsPNGDecoder.cpp
+++ modules/libpr0n/decoders/png/nsPNGDecoder.cpp
@@ -185,7 +185,7 @@ void nsPNGDecoder::EndImageFrame()
     
     PRInt32 width, height;
     mFrame->GetWidth(&width);
-    mFrame->GetWidth(&height);
+    mFrame->GetHeight(&height);
 
     nsIntRect r(0, 0, width, height);
     nsCOMPtr<nsIImage> img(do_GetInterface(mFrame));
