48d4370c784e6e8b52c54b61a43818564aeecdbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:29 +0000

    Bug 378695: SetupClusterBoundaries in gfxPangoFonts.cpp misinterprets attrs from pango_break, patch by Karl Tomlinson <mozbugz@karlt.net>, r=roc
    UltraBlame original commit: 0b2f3aaadcb1333d5605e8713210915bb77b0e9b

--- gfx/thebes/src/gfxPangoFonts.cpp
+++ gfx/thebes/src/gfxPangoFonts.cpp
@@ -950,19 +950,23 @@ SetupClusterBoundaries(gfxTextRun* aTextRun, const gchar *aUTF8, PRUint32 aUTF8L
         return;
     }
 
+    
+    
+    
+    
     nsAutoTArray<PangoLogAttr,2000> buffer;
     if (!buffer.AppendElements(aUTF8Length + 1))
         return;
 
-    
-    
-    pango_break(aUTF8, aUTF8Length, aAnalysis, buffer.Elements(), aUTF8Length + 1);
+    pango_break(aUTF8, aUTF8Length, aAnalysis,
+                buffer.Elements(), buffer.Length());
 
     const gchar *p = aUTF8;
     const gchar *end = aUTF8 + aUTF8Length;
+    const PangoLogAttr *attr = buffer.Elements();
     gfxTextRun::CompressedGlyph g;
     while (p < end) {
-        if (!buffer[p - aUTF8].is_cursor_position) {
+        if (!attr->is_cursor_position) {
             aTextRun->SetCharacterGlyph(aUTF16Offset, g.SetClusterContinuation());
         }
         ++aUTF16Offset;
@@ -974,6 +978,7 @@ SetupClusterBoundaries(gfxTextRun* aTextRun, const gchar *aUTF8, PRUint32 aUTF8L
         }
         
         p = g_utf8_next_char(p);
+        ++attr;
     }
 }
 
