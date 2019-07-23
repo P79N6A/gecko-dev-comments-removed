e3f82e080bc31b42ca98eaaed014ef986c25ecb1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:40:26 +0000

    Revert part of bug 377521 to fix bug 380825 (and add comment).  r+sr=bzbarsky
    UltraBlame original commit: f5fbc835f7ab317c2cbc46397c38c4734161b841

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -2116,7 +2116,9 @@ nsRuleNode::SetFont(nsPresContext* aPresContext, nsStyleContext* aContext,
     zoom = PR_FALSE;
   }
   else if (eCSSUnit_Initial == aFontData.mSize.GetUnit()) {
-    aFont->mSize = defaultVariableFont->size;
+    
+    
+    aFont->mSize = aDefaultFont.size;
     zoom = PR_TRUE;
   }
 
