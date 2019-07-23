bd68473c77b6526a4d4c367f2e14a0ebd62280d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:15:40 +0000

    Bug 515812 - Double hashing template (r=jorendorff)
    UltraBlame original commit: 784ceadd60e5847e11b6eebddcf1128f60232e7e

--- js/src/jsarray.cpp
+++ js/src/jsarray.cpp
@@ -1467,20 +1467,6 @@ array_toSource(JSContext *cx, uintN argc, jsval *vp)
 }
 #endif
 
-static JSHashNumber
-js_hash_array(const void *key)
-{
-    return (JSHashNumber)JS_PTR_TO_UINT32(key) >> JSVAL_TAGBITS;
-}
-
-bool
-js_InitContextBusyArrayTable(JSContext *cx)
-{
-    cx->busyArrayTable = JS_NewHashTable(4, js_hash_array, JS_CompareValues,
-                                         JS_CompareValues, NULL, NULL);
-    return cx->busyArrayTable != NULL;
-}
-
 static JSBool
 array_toString_sub(JSContext *cx, JSObject *obj, JSBool locale,
                    JSString *sepstr, jsval *rval)
@@ -1491,22 +1477,16 @@ array_toString_sub(JSContext *cx, JSObject *obj, JSBool locale,
 
 
 
-    JSHashTable *table = cx->busyArrayTable;
-
-    
-
-
-
-    JSHashNumber hash = js_hash_array(obj);
-    JSHashEntry **hep = JS_HashTableRawLookup(table, hash, obj);
-    JSHashEntry *he = *hep;
-    if (!he) {
+    typedef js::HashSet<JSObject *> ObjSet;
+    ObjSet::AddPtr hashp = cx->busyArrays.lookupForAdd(obj);
+    uint32 genBefore;
+    if (!hashp) {
         
-        he = JS_HashTableRawAdd(table, hep, hash, obj, NULL);
-        if (!he) {
+        if (!cx->busyArrays.add(hashp, obj)) {
             JS_ReportOutOfMemory(cx);
             return false;
         }
+        genBefore = cx->busyArrays.generation();
     } else {
         
         *rval = ATOM_KEY(cx->runtime->atomState.emptyAtom);
@@ -1580,11 +1560,10 @@ array_toString_sub(JSContext *cx, JSObject *obj, JSBool locale,
     ok = true;
 
   out:
-    
-
-
-
-    JS_HashTableRemove(table, obj);
+    if (genBefore == cx->busyArrays.generation())
+        cx->busyArrays.remove(hashp);
+    else
+        cx->busyArrays.remove(obj);
     return ok;
 }
 
