ece7b4ab21f7167549f2d9e01b4b35f0d5fb47a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:05:44 +0000

    Bug 522126, part 2: Add a POSIX clock (CLOCK_MONOTONIC) implementation of TimeStamp/TimeDuration. r=roc
    UltraBlame original commit: a2e7574e43ee9f596aa28158b30154b3bf5cc0db

---
+++ xpcom/ds/TimeStamp_posix.cpp
@@ -0,0 +1,185 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <time.h>
+
+#include "mozilla/TimeStamp.h"
+
+
+static PRUint64 sResolution;
+static PRUint64 sResolutionSigDigs;
+
+static const PRUint16 kNsPerUs   =       1000;
+static const PRUint64 kNsPerMs   =    1000000;
+static const PRUint64 kNsPerSec  = 1000000000; 
+static const double kNsPerSecd   = 1000000000.0;
+
+
+static PRUint64
+ClockTimeNs()
+{
+  struct timespec ts;
+  
+  
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+
+  
+  
+  
+  
+  
+  
+  PRUint64 baseNs = PRUint64(ts.tv_sec) * kNsPerSec;
+
+  return baseNs + PRUint64(ts.tv_nsec);
+}
+
+static PRUint64
+ClockResolutionNs()
+{
+  
+  
+  
+  
+  
+  
+
+  PRUint64 start = ClockTimeNs();
+  PRUint64 end = ClockTimeNs();
+  PRUint64 minres = (end - start);
+
+  
+  
+  
+  for (int i = 0; i < 9; ++i) {
+    start = ClockTimeNs();
+    end = ClockTimeNs();
+
+    PRUint64 candidate = (start - end);
+    if (candidate < minres)
+      minres = candidate;
+  }
+
+  if (0 == minres) {
+    NS_WARNING("the clock resolution is *not* 1ns, something's wrong");
+    minres = 1;                 
+  }
+  if (minres / kNsPerMs)
+    NS_WARNING("the clock resolution is *not* >=1ms, something's wrong");
+
+  return minres;
+}
+
+
+namespace mozilla {
+
+double
+TimeDuration::ToSeconds() const
+{
+  return double(mValue) / kNsPerSecd;
+}
+
+double
+TimeDuration::ToSecondsSigDigits() const
+{
+  
+  PRInt64 valueSigDigs = sResolution * (mValue / sResolution);
+  
+  valueSigDigs = sResolutionSigDigs * (valueSigDigs / sResolutionSigDigs);
+  return double(valueSigDigs) / kNsPerSecd;
+}
+
+TimeDuration
+TimeDuration::FromSeconds(PRInt32 aSeconds)
+{
+  return TimeDuration::FromTicks((PRInt64(aSeconds) * PRInt64(kNsPerSec)));
+}
+
+TimeDuration
+TimeDuration::FromMilliseconds(PRInt32 aMilliseconds)
+{
+  return TimeDuration::FromTicks(PRInt64(aMilliseconds) * PRInt64(kNsPerMs));
+}
+
+TimeDuration
+TimeDuration::Resolution()
+{
+  return TimeDuration::FromTicks(sResolution);
+}
+
+
+nsresult
+TimeStamp::Startup()
+{
+  struct timespec dummy;
+  if (0 != clock_gettime(CLOCK_MONOTONIC, &dummy))
+      NS_RUNTIMEABORT("CLOCK_MONOTONIC is absent!");
+
+  sResolution = ClockResolutionNs();
+
+  
+  
+  for (sResolutionSigDigs = 1;
+       !(sResolutionSigDigs == sResolution
+         || 10*sResolutionSigDigs > sResolution);
+       sResolutionSigDigs *= 10);
+
+  return NS_OK;
+}
+
+void
+TimeStamp::Shutdown()
+{
+}
+
+TimeStamp
+TimeStamp::Now()
+{
+  return TimeStamp(ClockTimeNs());
+}
+
+}
