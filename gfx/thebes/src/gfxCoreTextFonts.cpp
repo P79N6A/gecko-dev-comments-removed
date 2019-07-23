0557969a9dbbf6199e7034b5099550597b452645
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:08:21 +0000

    Bug 541277: correct boundary handling in character/glyph clump processing. r=roc
    UltraBlame original commit: 263b6541c83fead708070cf98df0ee49bf4c79c4

--- gfx/thebes/src/gfxCoreTextFonts.cpp
+++ gfx/thebes/src/gfxCoreTextFonts.cpp
@@ -449,9 +449,9 @@ gfxCoreTextFont::InitTextRun(gfxTextRun *aTextRun,
 
     
     
-    const UniChar beginLTR[]    = { 0x202d };
-    const UniChar beginRTL[]    = { 0x202e };
-    const UniChar endBidiWrap[] = { 0x202c };
+    const UniChar beginLTR[]    = { 0x202d, 0x20 };
+    const UniChar beginRTL[]    = { 0x202e, 0x20 };
+    const UniChar endBidiWrap[] = { 0x20, 0x202c };
 
     PRUint32 startOffset;
     CFStringRef stringObj;
@@ -638,8 +638,6 @@ gfxCoreTextFont::SetGlyphsFromRun(gfxTextRun *aTextRun,
         charToGlyph[offset] = NO_GLYPH;
     for (PRInt32 g = 0; g < numGlyphs; ++g) {
         PRInt32 loc = glyphToChar[g] - stringRange.location;
-        if (loc == 0 && !isLTR)
-            ++loc; 
         if (loc >= 0 && loc < stringRange.length) {
             charToGlyph[loc] = g;
         }
