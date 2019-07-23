e194d20c4df6af0e141c85fac67f2421f5f3f849
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:58:29 +0000

    Bug 237592 - Bookmarks/RSS items should always show tooltip when hovering with mouse, r=gavin
    UltraBlame original commit: b2248e8b2a74a652ed0ced3d2b5fdac9f2becdff

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -749,29 +749,23 @@ var BookmarksEventHandler = {
   },
 
   fillInBTTooltip: function(aTipElement) {
-    
-    if (aTipElement.localName != "toolbarbutton")
+    if (!aTipElement.node)
       return false;
 
     
     if (!PlacesUtils.nodeIsURI(aTipElement.node))
       return false;
 
+    var title = aTipElement.node.title;
     var url = aTipElement.node.uri;
-    if (!url) 
-      return false;
+
+    var tooltipTitle = document.getElementById("btTitleText");
+    tooltipTitle.hidden = !title || (title == url);
+    if (!tooltipTitle.hidden)
+      tooltipTitle.textContent = title;
 
     var tooltipUrl = document.getElementById("btUrlText");
     tooltipUrl.value = url;
-
-    var title = aTipElement.label;
-    var tooltipTitle = document.getElementById("btTitleText");
-    if (title && title != url) {
-      tooltipTitle.hidden = false;
-      tooltipTitle.textContent = title;
-    }
-    else
-      tooltipTitle.hidden = true;
 
     
     return true;
