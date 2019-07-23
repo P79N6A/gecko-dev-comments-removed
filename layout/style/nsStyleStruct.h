3fae2f5e4f07b5f8e35616c4ab57b917256c95f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:10:12 +0000

    Bug 417178 - "Google reader does not show subscribed topics in ff3b3 at certain zoom levels" [p=roc@ocallahan.org (Robert O'Callahan [roc]) r+sr=dbaron a1.9b5=mconnor]
    UltraBlame original commit: 5ae69c9e8fe810eb74c669fee9f22ba9d91906a6

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -308,8 +308,9 @@ struct nsBorderColors {
 
 
 
+
 #define NS_ROUND_BORDER_TO_PIXELS(l,tpp) \
-  ((l) == 0) ? 0 : PR_MAX((tpp), ((l) + ((tpp) / 2)) / (tpp) * (tpp))
+  ((l) == 0) ? 0 : PR_MAX((tpp), (l) / (tpp) * (tpp))
 
 
 
