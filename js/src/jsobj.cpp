46e4e861999c0c8f0b20fe4a2a61e31b2efea0b5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:49:08 +0000

    Bug 520511 - Add some tvrs. r=brendan
    UltraBlame original commit: 2581d8d91679fb288d5067651348e5c17e1951a6

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -1253,6 +1253,7 @@ obj_eval(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 #if JS_HAS_EVAL_THIS_SCOPE
     JSObject *callerScopeChain = NULL, *callerVarObj = NULL;
     JSBool setCallerScopeChain = JS_FALSE, setCallerVarObj = JS_FALSE;
+    JSTempValueRooter scopetvr, varobjtvr;
 #endif
 
     fp = js_GetTopStackFrame(cx);
@@ -1357,12 +1358,14 @@ obj_eval(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 
             
             setCallerScopeChain = JS_TRUE;
+            JS_PUSH_TEMP_ROOT_OBJECT(cx, callerScopeChain, &scopetvr);
 
             callerVarObj = caller->varobj;
             if (obj != callerVarObj) {
                 
                 caller->varobj = fp->varobj = obj;
                 setCallerVarObj = JS_TRUE;
+                JS_PUSH_TEMP_ROOT_OBJECT(cx, callerVarObj, &varobjtvr);
             }
         }
 #endif
@@ -1516,10 +1519,14 @@ obj_eval(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 out:
 #if JS_HAS_EVAL_THIS_SCOPE
     
-    if (setCallerScopeChain)
-        caller->scopeChain = callerScopeChain;
-    if (setCallerVarObj)
+    if (setCallerVarObj) {
         caller->varobj = callerVarObj;
+        JS_POP_TEMP_ROOT(cx, &varobjtvr);
+    }
+    if (setCallerScopeChain) {
+        caller->scopeChain = callerScopeChain;
+        JS_POP_TEMP_ROOT(cx, &scopetvr);
+    }
 #endif
     return ok;
 }
