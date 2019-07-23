bc9d883f0592cc0579acedec1f224e609ec715cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:54:46 +0000

    add unit test for permission manager.
    UltraBlame original commit: 59e169b2d2476b42751ccfce34fbc9dc4c92e3f2

---
+++ netwerk/test/unit/test_permmgr.js
@@ -0,0 +1,101 @@
+
+
+var hosts = [
+  
+  ["mozilla.org", "cookie", 1],
+  ["mozilla.org", "image", 2],
+  ["mozilla.org", "popup", 3],
+  ["mozilla.com", "cookie", 1],
+  ["www.mozilla.com", "cookie", 2],
+  ["dev.mozilla.com", "cookie", 3]
+];
+
+var results = [
+  
+  
+  ["localhost", "cookie", 0, 0],
+  ["spreadfirefox.com", "cookie", 0, 0],
+  
+  ["mozilla.org", "cookie", 1, 1],
+  ["mozilla.org", "image", 2, 2],
+  ["mozilla.org", "popup", 3, 3],
+  
+  ["www.mozilla.org", "cookie", 1, 0],
+  ["www.dev.mozilla.org", "cookie", 1, 0],
+  
+  ["mozilla.com", "cookie", 1, 1],
+  ["www.mozilla.com", "cookie", 2, 2],
+  ["dev.mozilla.com", "cookie", 3, 3],
+  ["www.dev.mozilla.com", "cookie", 3, 0]
+];
+
+function run_test() {
+  var pm = Components.classes["@mozilla.org/permissionmanager;1"]
+                     .getService(Components.interfaces.nsIPermissionManager);
+
+  var ioService = Components.classes["@mozilla.org/network/io-service;1"]
+                            .getService(Components.interfaces.nsIIOService);
+
+  
+  if (!pm)
+    return;
+
+  
+  for (var i = 0; i < hosts.length; ++i) {
+    var uri = ioService.newURI("http://" + hosts[i][0], null, null);
+    pm.add(uri, hosts[i][1], hosts[i][2]);
+  }
+
+  
+  for (var i = 0; i < results.length; ++i) {
+    var uri = ioService.newURI("http://" + results[i][0], null, null);
+    do_check_eq(pm.testPermission(uri, results[i][1]), results[i][2]);
+    do_check_eq(pm.testExactPermission(uri, results[i][1]), results[i][3]);
+  }
+
+  
+  var j = 0;
+  var perms = new Array();
+  var enumerator = pm.enumerator;
+  while (enumerator.hasMoreElements()) {
+    perms[j] = enumerator.getNext().QueryInterface(Components.interfaces.nsIPermission);
+    ++j;
+  }
+  do_check_eq(perms.length, hosts.length);
+
+  
+  for (var j = 0; j < perms.length; ++j) {
+    pm.remove(perms[j].host, perms[j].type);
+  }
+  
+  
+  for (var i = 0; i < hosts.length; ++i) {
+    for (var j = 0; j < perms.length; ++j) {
+      if (hosts[i][0] == perms[j].host &&
+          hosts[i][1] == perms[j].type &&
+          hosts[i][2] == perms[j].capability) {
+        perms.splice(j, 1);
+        break;
+      }
+    }
+  }
+  do_check_eq(perms.length, 0);
+
+  
+  do_check_eq(pm.enumerator.hasMoreElements(), false);
+
+  
+  var utf8 = "b\u00FCcher.dolske.org"; 
+  var aceref = "xn--bcher-kva.dolske.org";
+  var uri = ioService.newURI("http://" + utf8, null, null);
+  pm.add(uri, "utf8", 1);
+  var enumerator = pm.enumerator;
+  do_check_eq(enumerator.hasMoreElements(), true);
+  var ace = enumerator.getNext().QueryInterface(Components.interfaces.nsIPermission);
+  do_check_eq(ace.host, aceref);
+  do_check_eq(enumerator.hasMoreElements(), false);
+
+  
+  pm.removeAll();
+  do_check_eq(pm.enumerator.hasMoreElements(), false);
+}
