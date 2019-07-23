a932664d7f8cd032b3e6d461d5b5f74bb2c6ded8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:36:43 +0000

    dead stuff in nsDeviceContextSpecGTK b=459306 r=karlt
    UltraBlame original commit: d809cc77d823d73f5eaa9f4a419e0cceddaf52d6

--- widget/src/gtk2/nsDeviceContextSpecG.h
+++ widget/src/gtk2/nsDeviceContextSpecG.h
@@ -70,57 +70,22 @@ public:
   NS_IMETHOD GetSurfaceForPrinter(gfxASurface **surface);
 
   NS_IMETHOD Init(nsIWidget *aWidget, nsIPrintSettings* aPS, PRBool aIsPrintPreview);
-  NS_IMETHOD ClosePrintManager(); 
   NS_IMETHOD BeginDocument(PRUnichar * aTitle, PRUnichar * aPrintToFileName, PRInt32 aStartPage, PRInt32 aEndPage);
   NS_IMETHOD EndDocument();
   NS_IMETHOD BeginPage() { return NS_OK; }
   NS_IMETHOD EndPage() { return NS_OK; }
 
-  NS_IMETHOD GetToPrinter(PRBool &aToPrinter); 
-  NS_IMETHOD GetIsPrintPreview(PRBool &aIsPPreview);
-  NS_IMETHOD GetPrinterName ( const char **aPrinter );
-  NS_IMETHOD GetCopies ( int &aCopies );
-  NS_IMETHOD GetFirstPageFirst(PRBool &aFpf);     
-  NS_IMETHOD GetGrayscale(PRBool &aGrayscale);   
-  NS_IMETHOD GetTopMargin(float &value); 
-  NS_IMETHOD GetBottomMargin(float &value); 
-  NS_IMETHOD GetLeftMargin(float &value); 
-  NS_IMETHOD GetRightMargin(float &value); 
-  NS_IMETHOD GetCommand(const char **aCommand);   
   NS_IMETHOD GetPath (const char **aPath);    
-  NS_IMETHOD GetLandscape (PRBool &aLandscape);
-  NS_IMETHOD GetUserCancelled(PRBool &aCancel);      
   NS_IMETHOD GetPrintMethod(PrintMethod &aMethod);
   static nsresult GetPrintMethod(const char *aPrinter, PrintMethod &aMethod);
-  NS_IMETHOD GetPaperName(const char **aPaperName);
-  NS_IMETHOD GetPlexName(const char **aPlexName);
-  NS_IMETHOD GetResolutionName(const char **aResolutionName);
-  NS_IMETHOD GetColorspace(const char **aColorspace);
-  NS_IMETHOD GetDownloadFonts(PRBool &aDownloadFonts);   
   virtual ~nsDeviceContextSpecGTK();
   
 protected:
   nsCOMPtr<nsIPrintSettings> mPrintSettings;
   PRPackedBool mToPrinter : 1;      
   PRPackedBool mIsPPreview : 1;     
-  PRPackedBool mFpf : 1;            
-  PRPackedBool mGrayscale : 1;      
-  PRPackedBool mDownloadFonts : 1;  
-  PRPackedBool mCancel : 1;         
-  int    mOrientation;        
-  char   mCommand[PATH_MAX];  
   char   mPath[PATH_MAX];     
   char   mPrinter[256];       
-  char   mPaperName[256];     
-  char   mPlexName[256];      
-  char   mResolutionName[256];
-  char   mColorspace[256];    
-  int    mCopies;             
-  float  mLeft;               
-  float  mRight;              
-  float  mTop;                
-  float  mBottom;             
-
   GtkPrintJob*      mPrintJob;
   GtkPrinter*       mGtkPrinter;
   GtkPrintSettings* mGtkPrintSettings;
