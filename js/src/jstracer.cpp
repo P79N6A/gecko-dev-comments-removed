32e293b462f7e5b9213788a4fb1714a9d3f9b552
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:39:13 +0000

    Fixed a hack that created temporarily ill-typed LIR, i2f(i2f) (bug 503427, r=gal).
    UltraBlame original commit: e8a6a3216b05f5801deb59b7c4faea4c87356576

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -2973,21 +2973,21 @@ TraceRecorder::lazilyImportGlobalSlot(unsigned slot)
 
 
 LIns*
-TraceRecorder::writeBack(LIns* i, LIns* base, ptrdiff_t offset)
+TraceRecorder::writeBack(LIns* i, LIns* base, ptrdiff_t offset, bool demote)
 {
     
 
 
 
 
-    if (isPromoteInt(i))
+    if (demote && isPromoteInt(i))
         i = ::demote(lir, i);
     return lir->insStorei(i, base, offset);
 }
 
 
 JS_REQUIRES_STACK void
-TraceRecorder::set(jsval* p, LIns* i, bool initializing)
+TraceRecorder::set(jsval* p, LIns* i, bool initializing, bool demote)
 {
     JS_ASSERT(i != NULL);
     JS_ASSERT(initializing || known(p));
@@ -3003,9 +3003,9 @@ TraceRecorder::set(jsval* p, LIns* i, bool initializing)
     LIns* x = nativeFrameTracker.get(p);
     if (!x) {
         if (isGlobal(p))
-            x = writeBack(i, lirbuf->state, nativeGlobalOffset(p));
+            x = writeBack(i, lirbuf->state, nativeGlobalOffset(p), demote);
         else
-            x = writeBack(i, lirbuf->sp, -treeInfo->nativeStackBase + nativeStackOffset(p));
+            x = writeBack(i, lirbuf->sp, -treeInfo->nativeStackBase + nativeStackOffset(p), demote);
         nativeFrameTracker.set(p, x);
     } else {
 #define ASSERT_VALID_CACHE_HIT(base, offset)                                  \
@@ -3016,7 +3016,7 @@ TraceRecorder::set(jsval* p, LIns* i, bool initializing)
 
         JS_ASSERT(x->isop(LIR_sti) || x->isop(LIR_stqi));
         ASSERT_VALID_CACHE_HIT(x->oprnd2(), x->disp());
-        writeBack(i, x->oprnd2(), x->disp());
+        writeBack(i, x->oprnd2(), x->disp(), demote);
     }
 #undef ASSERT_VALID_CACHE_HIT
 }
@@ -3751,7 +3751,10 @@ class SlotMap : public SlotVisitorBase
                 mRecorder.set(info.v, mRecorder.f2i(mRecorder.get(info.v)));
             } else if (info.lastCheck == TypeCheck_Demote) {
                 JS_ASSERT(isNumber(*info.v));
-                mRecorder.set(info.v, mRecorder.lir->ins1(LIR_i2f, mRecorder.get(info.v)));
+                JS_ASSERT(mRecorder.get(info.v)->isQuad());
+
+                
+                mRecorder.set(info.v, mRecorder.get(info.v), false, false);
             }
         }
     }
