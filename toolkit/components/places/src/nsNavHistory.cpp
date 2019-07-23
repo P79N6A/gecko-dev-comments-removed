fcce16c91ab437523e4c76b6962ee68d55b83470
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:26:44 +0000

    Bug 464486 - lots of bookmark corruption reported via user support (r=marco, sdwilsh)
    UltraBlame original commit: 7c47afbd5675039d7a541ad272e54f0fabe81c0d

--- toolkit/components/places/src/nsNavHistory.cpp
+++ toolkit/components/places/src/nsNavHistory.cpp
@@ -670,6 +670,12 @@ nsNavHistory::InitDB()
   NS_ENSURE_SUCCESS(rv, rv);
 #endif
 
+  
+  
+  rv = mDBConn->ExecuteSimpleSQL(NS_LITERAL_CSTRING(
+      "PRAGMA synchronous = FULL"));
+  NS_ENSURE_SUCCESS(rv, rv);
+
   mozStorageTransaction transaction(mDBConn, PR_FALSE);
 
   
