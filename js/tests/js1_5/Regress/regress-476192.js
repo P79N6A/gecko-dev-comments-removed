d5b47ae42b9c4ddbcd5e10983b71b2d4223202ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:55:52 +0000

    bug 476192 - JavaScript Test by Carsten Book, Jesse Ruderman.
    UltraBlame original commit: 488f2529b4474ce32933bc44eac8262fe50d829f

---
+++ js/tests/js1_5/Regress/regress-476192.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-476192.js';
+
+var BUGNUMBER = 476192;
+var summary = 'TM: Do not assert: Assertion failure: JSVAL_TAG(v) == JSVAL_STRING';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  jit(true);
+
+  var global;
+
+  (function(){
+    var ad = {present: ""};
+    var params = ['present', 'a', 'present', 'a', 'present', 'a', 'present'];
+    for (var j = 0; j < params.length; j++) {
+      global = ad[params[j]];
+    }
+  })();
+
+  jit(false);
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
