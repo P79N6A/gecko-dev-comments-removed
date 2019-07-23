2beb69865676a326ed6b131ede09cd9d523ab7eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:08:01 +0000

    Bug 540858 part I: separate the refcount logging of NPObject (which works for both plugin and proxy NPObject) from ctor/dtor logging of ChildNPObject (proxy only), r=bent
    UltraBlame original commit: d4d28cc35858af6c0007b61a057d628b12d3b8be

--- dom/plugins/PluginScriptableObjectChild.h
+++ dom/plugins/PluginScriptableObjectChild.h
@@ -52,7 +52,15 @@ class PluginScriptableObjectChild;
 struct ChildNPObject : NPObject
 {
   ChildNPObject()
-    : NPObject(), parent(NULL), invalidated(false) { }
+    : NPObject(), parent(NULL), invalidated(false)
+  {
+    MOZ_COUNT_CTOR(ChildNPObject);
+  }
+
+  ~ChildNPObject()
+  {
+    MOZ_COUNT_DTOR(ChildNPObject);
+  }
 
   
   
