314de71ab3e134658a516607fd17ce0d82a71c35
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:12:10 +0000

    JavaScript Test - regression test for bug 425360, by Andrew Schultz
    UltraBlame original commit: cdc5762c562e99779b264943a4a1ebc35afd2c96

---
+++ js/tests/js1_5/Regress/regress-425360.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-425360.js';
+
+var BUGNUMBER = 425369;
+var summary = 'Do not assert: !cx->throwing';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+function finishtest()
+{
+  gDelayTestDriverEnd = false;
+  reportCompare(expect, actual, summary);
+  jsTestDriverEnd();
+}
+
+function throwBlah()
+{
+  throw 'blah';
+}
+
+printBugNumber(BUGNUMBER);
+printStatus (summary);
+ 
+if (typeof window == 'undefined')
+{
+  expect = actual = 'Not tested. Requires browser.';
+  reportCompare(expect, actual, summary);
+}
+else
+{
+  gDelayTestDriverEnd = true;
+  window.onerror = null;
+  setTimeout('finishtest()', 1000);
+  window.onload = (function () { setInterval('throwBlah()', 0); });
+  setInterval('foo(', 0);
+}
+
+
