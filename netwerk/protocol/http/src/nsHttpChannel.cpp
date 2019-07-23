b5de9ad0e43353b89237b3933bda1124a193b0b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:34:17 +0000

    Backed out changeset 3a829715fd39 (Bug 482935) on suspicion of causing mochitest-plain leaks.
    UltraBlame original commit: 36d3597620c7b8392cfb99cc056dcbd2caea2de4

--- netwerk/protocol/http/src/nsHttpChannel.cpp
+++ netwerk/protocol/http/src/nsHttpChannel.cpp
@@ -5125,11 +5125,6 @@ nsHttpChannel::OnStopRequest(nsIRequest *request, nsISupports *ctxt, nsresult st
         this, request, status));
 
     
-    PRBool contentComplete = PR_FALSE;
-    if (NS_SUCCEEDED(status))
-        contentComplete = PR_TRUE;
-
-    
     if (mCanceled || NS_FAILED(mStatus))
         status = mStatus;
 
@@ -5219,7 +5214,7 @@ nsHttpChannel::OnStopRequest(nsIRequest *request, nsISupports *ctxt, nsresult st
     }
 
     if (mCacheEntry)
-        CloseCacheEntry(!contentComplete);
+        CloseCacheEntry(PR_TRUE);
 
     if (mOfflineCacheEntry)
         CloseOfflineCacheEntry();
