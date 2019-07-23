52c91a38906dfe39421a7223f7cae0858df78fea
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:39:31 +0000

    Fix a leak valgrind finds in jsapi-tests/testXDR.cpp. No bug, too trivial for a review, no_r=me.
    UltraBlame original commit: 465737c189fc7a6a6634321a8c54f88e49a756cf

--- js/src/jsapi-tests/testXDR.cpp
+++ js/src/jsapi-tests/testXDR.cpp
@@ -35,6 +35,9 @@ BEGIN_TEST(testXDR_bug506491)
     JS_XDRMemSetData(r, frozen, nbytes);
     CHECK(JS_XDRScript(r, &script));
     JS_XDRDestroy(r);  
+    scrobj = JS_NewScriptObject(cx, script);
+    CHECK(scrobj);
+    v = OBJECT_TO_JSVAL(scrobj);
 
     
     jsvalRoot v2(cx);
