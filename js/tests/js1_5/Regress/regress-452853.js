b10f70ce78301e264af7f40cc54598962a9049fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:49:35 +0000

    JavaScript Tests - regression test for bug 452853, by Jesse Ruderman
    UltraBlame original commit: 05120f3c57786bf637c3821b037770b48dc4132d

---
+++ js/tests/js1_5/Regress/regress-452853.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-452853.js';
+
+var BUGNUMBER = 452853;
+var summary = 'Do not crash in simple loop with array';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  for (var j=0; j<4; ++j) { var a = ["", ""]; a[0] * a[1]; }
+ 
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
