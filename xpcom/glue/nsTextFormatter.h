f3bbc47778379a620b02ff846bd3b1acb773b8ce
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:49:09 +0000

    Bug 384979 --> move nsTextFormatter to the glue code so it can be used with frozen linkage. Patch by prasad <prasad@medhas.org> sr=bsmedberg a=bsmedberg
    UltraBlame original commit: f374f4c3f2cdc32089ebe5980a53292536b33e5c

---
+++ xpcom/glue/nsTextFormatter.h
@@ -0,0 +1,108 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsTextFormatter_h___
+#define nsTextFormatter_h___
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "prtypes.h"
+#include "prio.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include "nscore.h"
+#include "nsStringGlue.h"
+
+
+class NS_COM_GLUE nsTextFormatter {
+
+  public:
+
+    
+
+
+
+
+    static PRUint32 snprintf(PRUnichar *out, PRUint32 outlen, const PRUnichar *fmt, ...);
+
+    
+
+
+
+    static PRUnichar* smprintf(const PRUnichar *fmt, ...);
+
+    static PRUint32 ssprintf(nsAString& out, const PRUnichar* fmt, ...);
+
+    
+
+
+    static PRUint32 vsnprintf(PRUnichar *out, PRUint32 outlen, const PRUnichar *fmt, va_list ap);
+    static PRUnichar* vsmprintf(const PRUnichar *fmt, va_list ap);
+    static PRUint32 vssprintf(nsAString& out, const PRUnichar *fmt, va_list ap);
+
+    
+
+
+
+
+    static void smprintf_free(PRUnichar *mem);
+
+};
+
+#endif 
