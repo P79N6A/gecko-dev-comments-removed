2767ad5b1f11496e8009610b126325e4ac34b94a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:30:38 +0000

    Bug 436418, patch A: SVG/SMIL animateMotion - add some foundational code. r=roc
    UltraBlame original commit: 06f995a12cbb067273ac85da8977a2003cd52d92

---
+++ content/svg/content/src/nsSVGAnimateMotionElement.cpp
@@ -0,0 +1,116 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsSVGAnimationElement.h"
+#include "nsIDOMSVGAnimateMotionElement.h"
+#include "nsSVGEnum.h"
+#include "nsSMILAnimationFunction.h"
+
+#include "nsCOMArray.h"
+#include "nsIDOMSVGPathSeg.h"
+#include "nsSVGPathSeg.h"
+#include "nsSVGPathDataParser.h"
+
+
+typedef nsSVGAnimationElement nsSVGAnimateMotionElementBase;
+
+class nsSVGAnimateMotionElement : public nsSVGAnimateMotionElementBase,
+                                  public nsIDOMSVGAnimateMotionElement
+{
+protected:
+  friend nsresult NS_NewSVGAnimateMotionElement(nsIContent **aResult,
+                                                   nsINodeInfo *aNodeInfo);
+  nsSVGAnimateMotionElement(nsINodeInfo* aNodeInfo);
+
+  nsSMILAnimationFunction mAnimationFunction;
+
+public:
+  
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIDOMSVGANIMATEMOTIONELEMENT
+
+  NS_FORWARD_NSIDOMNODE(nsSVGAnimateMotionElementBase::)
+  NS_FORWARD_NSIDOMELEMENT(nsSVGAnimateMotionElementBase::)
+  NS_FORWARD_NSIDOMSVGELEMENT(nsSVGAnimateMotionElementBase::)
+  NS_FORWARD_NSIDOMSVGANIMATIONELEMENT(nsSVGAnimateMotionElementBase::)
+
+  
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+
+  
+  virtual nsSMILAnimationFunction& AnimationFunction();
+};
+
+NS_IMPL_NS_NEW_SVG_ELEMENT(AnimateMotion)
+
+
+
+
+NS_IMPL_ADDREF_INHERITED(nsSVGAnimateMotionElement,nsSVGAnimateMotionElementBase)
+NS_IMPL_RELEASE_INHERITED(nsSVGAnimateMotionElement,nsSVGAnimateMotionElementBase)
+
+NS_INTERFACE_TABLE_HEAD(nsSVGAnimateMotionElement)
+  NS_NODE_INTERFACE_TABLE5(nsSVGAnimateMotionElement, nsIDOMNode,
+                           nsIDOMElement, nsIDOMSVGElement,
+                           nsIDOMSVGAnimationElement,
+                           nsIDOMSVGAnimateMotionElement)
+  NS_INTERFACE_MAP_ENTRY_CONTENT_CLASSINFO(SVGAnimateElement)
+NS_INTERFACE_MAP_END_INHERITING(nsSVGAnimateMotionElementBase)
+
+
+
+
+nsSVGAnimateMotionElement::nsSVGAnimateMotionElement(nsINodeInfo *aNodeInfo)
+  : nsSVGAnimateMotionElementBase(aNodeInfo)
+{
+}
+
+
+
+
+NS_IMPL_ELEMENT_CLONE_WITH_INIT(nsSVGAnimateMotionElement)
+
+
+
+
+nsSMILAnimationFunction&
+nsSVGAnimateMotionElement::AnimationFunction()
+{
+  return mAnimationFunction;
+}
