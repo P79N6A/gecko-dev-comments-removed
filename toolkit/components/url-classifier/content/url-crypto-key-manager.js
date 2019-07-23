48bf18ed234867452dc65794a2ed2a551e587db2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:27 +0000

    Bug 382816 - remove /toolkit/components/url-classifier/content/moz/filesystem.js and /toolkit/components/url-classifier/content/application.jspatch by Simon Bünzli, r=me
    UltraBlame original commit: 62d813985016a6ec711688480c678f96b8e6870f

--- toolkit/components/url-classifier/content/url-crypto-key-manager.js
+++ toolkit/components/url-classifier/content/url-crypto-key-manager.js
@@ -274,12 +274,22 @@ PROT_UrlCryptoKeyManager.prototype.serializeKey_ = function() {
   
   try {  
 
-    var appDir = new PROT_ApplicationDirectory();
-    if (!appDir.exists())
-      appDir.create();
-    var keyfile = appDir.getAppDirFileInterface();
+    var keyfile = Cc["@mozilla.org/file/directory_service;1"]
+                 .getService(Ci.nsIProperties)
+                 .get("ProfD", Ci.nsILocalFile); 
     keyfile.append(this.keyFilename_);
-    G_FileWriter.writeAll(keyfile, (new G_Protocol4Parser).serialize(map));
+    var data = (new G_Protocol4Parser()).serialize(map);
+
+    try {
+      var stream = Cc["@mozilla.org/network/file-output-stream;1"]
+                   .createInstance(Ci.nsIFileOutputStream);
+      stream.init(keyfile,
+                  0x02 | 0x08 | 0x20 ,
+                  -1 , 0 );
+      stream.write(data, data.length);
+    } finally {
+      stream.close();
+    }
     return true;
 
   } catch(e) {
@@ -323,11 +333,24 @@ PROT_UrlCryptoKeyManager.prototype.maybeLoadOldKey = function() {
   
   var oldKey = null;
   try {  
-    var appDir = new PROT_ApplicationDirectory();
-    var keyfile = appDir.getAppDirFileInterface();
+    var keyfile = Cc["@mozilla.org/file/directory_service;1"]
+                 .getService(Ci.nsIProperties)
+                 .get("ProfD", Ci.nsILocalFile); 
     keyfile.append(this.keyFilename_);
-    if (keyfile.exists())
-      oldKey = G_FileReader.readAll(keyfile);
+    if (keyfile.exists()) {
+      try {
+        var fis = Cc["@mozilla.org/network/file-input-stream;1"]
+                  .createInstance(Ci.nsIFileInputStream);
+        fis.init(keyfile, 0x01 , 0444, 0);
+        var stream = Cc["@mozilla.org/scriptableinputstream;1"]
+                     .createInstance(Ci.nsIScriptableInputStream);
+        stream.init(fis);
+        oldKey = stream.read(stream.available());
+      } finally {
+        if (stream)
+          stream.close();
+      }
+    }
   } catch(e) {
     G_Debug(this, "Caught " + e + " trying to read keyfile");
     return;
@@ -365,8 +388,9 @@ function TEST_PROT_UrlCryptoKeyManager() {
 
     
     function removeTestFile(f) {
-      var appDir = new PROT_ApplicationDirectory();
-      var file = appDir.getAppDirFileInterface();
+      var file = Cc["@mozilla.org/file/directory_service;1"]
+                 .getService(Ci.nsIProperties)
+                 .get("ProfD", Ci.nsILocalFile); 
       file.append(f);
       if (file.exists())
         file.remove(false );
