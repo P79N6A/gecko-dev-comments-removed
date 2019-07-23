86708850111509660c725e66f39c53c8bd1e55ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:25:54 +0000

    Bug 538324 - Move ctypes into js/src. Part 6: Add ctypes to JSAPI. r=sayrer
    UltraBlame original commit: 6cda3ed74baaace07cdc2341b1335e90de96f9ac

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -823,6 +823,15 @@ JS_GetScopeChain(JSContext *cx);
 extern JS_PUBLIC_API(JSObject *)
 JS_GetGlobalForObject(JSContext *cx, JSObject *obj);
 
+#ifdef JS_HAS_CTYPES
+
+
+
+
+extern JS_PUBLIC_API(JSBool)
+JS_InitCTypesClass(JSContext *cx, JSObject *global);
+#endif
+
 
 
 
