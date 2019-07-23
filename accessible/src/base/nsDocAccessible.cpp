6d36cfc6dab4fa4daa349fe81405187ebed9aa76
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:48:19 +0000

    bustage fix bug 390414
    UltraBlame original commit: d0ad1f109c5f8d13a40bb41071c4bd7bef19f575

--- accessible/src/base/nsDocAccessible.cpp
+++ accessible/src/base/nsDocAccessible.cpp
@@ -1244,8 +1244,8 @@ nsDocAccessible::FireTextChangeEventForText(nsIContent *aContent,
     return;
 
   PRInt32 length = aIsInserted ?
-    aInfo->mReplaceLength; 
-    aInfo->mChangeEnd - start : 
+    aInfo->mReplaceLength: 
+    aInfo->mChangeEnd - start; 
 
   if (length > 0) {
     nsCOMPtr<nsIPresShell> shell(do_QueryReferent(mWeakShell));
