82d92d593672aac0cf48688d178809d0e4a0ff3e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:51:48 +0000

    Bug 398969 - "SessionStore: [Exception... "Component returned failure code: 0xc1f30001 (NS_ERROR_NOT_INITIALIZED) [nsICrashReporter.annotateCrashReport]"" [p=f.qu@queze.net (Florian Quèze) r=gavin aM9=beltzner]
    UltraBlame original commit: 4be6d4277fb3f37e2be20943cead118e5ac5b0b0

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -1979,7 +1979,11 @@ SessionStoreService.prototype = {
       var cr = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsICrashReporter);
       cr.annotateCrashReport("URL", currentUrl);
     }
-    catch (ex) { debug(ex); }
+    catch (ex) {
+      
+      if (ex.result != Components.results.NS_ERROR_NOT_INITIALIZED)
+        debug(ex);
+    }
   },
 
   
