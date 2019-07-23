b1d807f126ec852bdece65b4aee7c53e47a03753
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:11:46 +0000

    Bug 231754, pressing up/down should move caret on Mac instead of open autocomplete popup, r=gavin,a=beltzner
    UltraBlame original commit: a4748a71dcced9a8c8620bdec5531b45796b527a

--- toolkit/components/autocomplete/src/nsAutoCompleteController.cpp
+++ toolkit/components/autocomplete/src/nsAutoCompleteController.cpp
@@ -454,15 +454,38 @@ nsAutoCompleteController::HandleKeyNavigation(PRUint32 aKey, PRBool *_retval)
         }
       }
     } else {
+#ifdef XP_MACOSX
       
-      PRUint32 resultCount;
-      mResults->Count(&resultCount);
-      if (resultCount) {
-        if (mRowCount) {
-          OpenPopup();
-        }
-      } else
-        StartSearchTimer();
+      
+      
+      
+      PRInt32 start, end;
+      if (aKey == nsIDOMKeyEvent::DOM_VK_UP) {
+        input->GetSelectionStart(&start);
+        input->GetSelectionEnd(&end);
+        if (start > 0 || start != end)
+          *_retval = PR_FALSE;
+      }
+      else if (aKey == nsIDOMKeyEvent::DOM_VK_DOWN) {
+        nsAutoString text;
+        input->GetTextValue(text);
+        input->GetSelectionStart(&start);
+        input->GetSelectionEnd(&end);
+        if (start != end || end < (PRInt32)text.Length())
+          *_retval = PR_FALSE;
+      }
+#endif
+      if (*_retval) {
+        
+        PRUint32 resultCount;
+        mResults->Count(&resultCount);
+        if (resultCount) {
+          if (mRowCount) {
+            OpenPopup();
+          }
+        } else
+          StartSearchTimer();
+      }
     }    
   } else if (   aKey == nsIDOMKeyEvent::DOM_VK_LEFT 
              || aKey == nsIDOMKeyEvent::DOM_VK_RIGHT 
