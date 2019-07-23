cb39374efcfe57bb4414755d19498a0ca5c87c0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:49:26 +0000

    Added debug-break to JS_Assert for gcc x64 (bug 524143, r=gal).
    UltraBlame original commit: 0646a572b8326334c5be8f540dd437d91e75ba5c

--- js/src/jsutil.cpp
+++ js/src/jsutil.cpp
@@ -49,8 +49,6 @@
 
 #ifdef WIN32
 #    include <windows.h>
-#else
-#    include <signal.h>
 #endif
 
 
@@ -65,10 +63,10 @@ JS_PUBLIC_API(void) JS_Assert(const char *s, const char *file, JSIntn ln)
 #if defined(WIN32)
     DebugBreak();
     exit(3);
-#else
-    
-    raise(SIGABRT);
+#elif defined(XP_OS2) || (defined(__GNUC__) && (defined(__i386) || defined(__x86_64__)))
+    asm("int $3");
 #endif
+    abort();
 }
 
 #ifdef JS_BASIC_STATS
