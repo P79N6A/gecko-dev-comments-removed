75402b51ecf5abc32105a6d5b9e32040abc3b8c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:53:41 +0000

    b=527122; fix for dll blocklist sporadic tinderbox crashes; r=shaver
    UltraBlame original commit: 16a85357099076a6ab8600a1b8dd41ec5a7e1deb

--- toolkit/xre/nsWindowsDllBlocklist.cpp
+++ toolkit/xre/nsWindowsDllBlocklist.cpp
@@ -54,6 +54,15 @@
 #define STATUS_DLL_NOT_FOUND ((DWORD)0xC0000135L)
 #endif
 
+
+#undef DEBUG_very_verbose
+
+
+
+
+
+PR_STATIC_ASSERT(sizeof(PULONG) == sizeof(ULONG));
+
 typedef NTSTATUS (NTAPI *LdrLoadDll_func) (PWCHAR filePath, PULONG flags, PUNICODE_STRING moduleFileName, PHANDLE handle);
 
 static LdrLoadDll_func stub_LdrLoadDll = 0;
@@ -65,47 +74,47 @@ patched_LdrLoadDll (PWCHAR filePath, PULONG flags, PUNICODE_STRING moduleFileNam
 #define DLLNAME_MAX 128
   char dllName[DLLNAME_MAX+1];
 
-  
-  
-  
-  
-  
-  
-  
-  
-  int len = moduleFileName->Length;
-  wchar_t *fn_buf = moduleFileName->Buffer;
-
-  int count = 0;
-  while (count < len && fn_buf[count] != 0)
-    count++;
-
-  len = count;
+  int len = moduleFileName->Length / 2;
+  wchar_t *fname = moduleFileName->Buffer;
 
   
-  nsAutoArrayPtr<wchar_t> fname = new wchar_t[len+1];
-  wcsncpy(fname, moduleFileName->Buffer, len);
-  fname[len] = 0; 
+  
+  
+  
+  if (moduleFileName->MaximumLength < moduleFileName->Length+2 ||
+      fname[len] != 0)
+  {
+#ifdef DEBUG
+    printf_stderr("LdrLoadDll: non-null terminated string found!\n");
+#endif
+    goto continue_loading;
+  }
 
   wchar_t *dll_part = wcsrchr(fname, L'\\');
   if (dll_part) {
     dll_part = dll_part + 1;
-    len = (fname+len) - dll_part;
+    len -= dll_part - fname;
   } else {
     dll_part = fname;
   }
 
+#ifdef DEBUG_very_verbose
+  printf_stderr("LdrLoadDll: dll_part '%S' %d\n", dll_part, len);
+#endif
+
   
   
   
-  if (len > DLLNAME_MAX)
+  if (len > DLLNAME_MAX) {
+#ifdef DEBUG
+    printf_stderr("LdrLoadDll: len too long! %d\n", len);
+#endif
     goto continue_loading;
+  }
 
   
   for (int i = 0; i < len; i++) {
     wchar_t c = dll_part[i];
-    if (c >= 'A' && c <= 'Z')
-      c += 'a' - 'A';
 
     if (c > 0x7f) {
       
@@ -113,10 +122,18 @@ patched_LdrLoadDll (PWCHAR filePath, PULONG flags, PUNICODE_STRING moduleFileNam
       goto continue_loading;
     }
 
+    
+    if (c >= 'A' && c <= 'Z')
+      c += 'a' - 'A';
+
     dllName[i] = (char) c;
   }
 
   dllName[len] = 0;
+
+#ifdef DEBUG_very_verbose
+  printf_stderr("LdrLoadDll: dll name '%s'\n", dllName);
+#endif
 
   
   DllBlockInfo *info = &sWindowsDllBlocklist[0];
@@ -128,17 +145,26 @@ patched_LdrLoadDll (PWCHAR filePath, PULONG flags, PUNICODE_STRING moduleFileNam
   }
 
   if (info->name) {
-    BOOL load_ok = FALSE;
+    bool load_ok = false;
+
+#ifdef DEBUG_very_verbose
+    printf_stderr("LdrLoadDll: info->name: '%s'\n", info->name);
+#endif
 
     if (info->maxVersion != ALL_VERSIONS) {
       
       DWORD pathlen = SearchPathW(filePath, fname, L".dll", 0, NULL, NULL);
       if (pathlen == 0) {
         
+        printf_stderr("LdrLoadDll: Blocking load of '%s' (SearchPathW didn't find it?)\n", dllName);
         return STATUS_DLL_NOT_FOUND;
       }
 
-      nsAutoArrayPtr<wchar_t> full_fname = new wchar_t[pathlen+1];
+      wchar_t *full_fname = (wchar_t*) malloc(sizeof(wchar_t)*(pathlen+1));
+      if (!full_fname) {
+        
+        return STATUS_DLL_NOT_FOUND;
+      }
 
       
       SearchPathW(filePath, fname, L".dll", pathlen+1, full_fname, NULL);
@@ -163,16 +189,24 @@ patched_LdrLoadDll (PWCHAR filePath, PULONG flags, PUNICODE_STRING moduleFileNam
           
           
           if (fVersion > info->maxVersion)
-            goto continue_loading;
+            load_ok = true;
         }
       }
+
+      free(full_fname);
     }
 
-    PR_LogPrint("LdrLoadDll: Blocking load of '%s'", dllName);
-    return STATUS_DLL_NOT_FOUND;
+    if (!load_ok) {
+      printf_stderr("LdrLoadDll: Blocking load of '%s' -- see http://www.mozilla.com/en-US/blocklist/\n", dllName);
+      return STATUS_DLL_NOT_FOUND;
+    }
   }
 
 continue_loading:
+#ifdef DEBUG_very_verbose
+  printf_stderr("LdrLoadDll: continuing load... ('%S')\n", moduleFileName->Buffer);
+#endif
+
   return stub_LdrLoadDll(filePath, flags, moduleFileName, handle);
 }
 
@@ -185,10 +219,8 @@ SetupDllBlocklist()
 
   bool ok = NtDllIntercept.AddHook("LdrLoadDll", patched_LdrLoadDll, (void**) &stub_LdrLoadDll);
 
+#ifdef DEBUG
   if (!ok)
-    PR_LogPrint ("LdrLoadDll hook failed, no dll blocklisting active");
+    printf_stderr ("LdrLoadDll hook failed, no dll blocklisting active\n");
+#endif
 }
-
-
-
-
