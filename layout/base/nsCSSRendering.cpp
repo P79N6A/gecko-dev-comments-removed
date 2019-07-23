d3a7462ce09d710f3b274fc714d0630872ab9d66
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:26:46 +0000

    Bug 445741. Round rects consistently to avoid a gap between an element and its shadow. r+sr=roc
    UltraBlame original commit: 7c9c52594a4b91c66031fa57a44cdb85a0d4d467

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -1077,6 +1077,7 @@ nsCSSRendering::PaintBoxShadow(nsPresContext* aPresContext,
                     twipsPerPixel, &borderRadii);
 
   gfxRect frameGfxRect = RectToGfxRect(frameRect, twipsPerPixel);
+  frameGfxRect.Round();
   gfxRect dirtyGfxRect = RectToGfxRect(aDirtyRect, twipsPerPixel);
 
   for (PRUint32 i = styleBorder->mBoxShadow->Length(); i > 0; --i) {
@@ -1087,7 +1088,7 @@ nsCSSRendering::PaintBoxShadow(nsPresContext* aPresContext,
 
     gfxRect shadowRectPlusBlur = shadowRect;
     shadowRect.ScaleInverse(twipsPerPixel);
-    shadowRect.RoundOut();
+    shadowRect.Round();
 
     
     
