098afc809a8fd0afbe1076b4dcb571476097429c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:05:22 +0000

    switching windows narrow char system calls to wide char b=418703 r=bsmedberg a=beltzner
    UltraBlame original commit: 1aa0ca075ff2c3efcbe75bd5dba792aa235dfe4a

--- modules/oji/src/scd.cpp
+++ modules/oji/src/scd.cpp
@@ -100,7 +100,7 @@ nsSymantecDebugManager::SetDebugAgentPassword(PRInt32 pwd)
 
         
     }
-    sem = OpenSemaphore(SEMAPHORE_MODIFY_STATE, FALSE, "Netscape-Symantec Debugger");
+    sem = OpenSemaphoreW(SEMAPHORE_MODIFY_STATE, FALSE, L"Netscape-Symantec Debugger");
     if (sem) {
         ReleaseSemaphore(sem, 1, NULL);
         CloseHandle(sem);
