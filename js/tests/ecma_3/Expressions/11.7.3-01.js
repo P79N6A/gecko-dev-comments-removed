e532ee6de9b2b588693df194f19107f7cdba5b0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:49:20 +0000

    JavaScript Tests - regression tests for bug 396969, by Brendan Eich, not part of the build
    UltraBlame original commit: 2bdf31bab871f20edad2b17551a43cafca139a37

---
+++ js/tests/ecma_3/Expressions/11.7.3-01.js
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = '11.7.1-01.js';
+
+var BUGNUMBER = 396969;
+var summary = '11.7.1 - >>> should evaluate operands in order';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  expect = 'o.valueOf, p.valueOf';
+  var actualval;
+  var expectval = 10;
+
+  var o = {
+    valueOf: (function (){ actual += 'o.valueOf'; return this.value}), 
+    value:42
+  };
+
+  var p = {
+    valueOf: (function (){ actual += ', p.valueOf'; return this.value}), 
+    value:2
+  };
+
+  actualval = (o >>> p);
+
+  reportCompare(expectval, actualval, summary + ': value');
+  reportCompare(expect, actual, summary + ': order');
+
+  exitFunc ('test');
+}
