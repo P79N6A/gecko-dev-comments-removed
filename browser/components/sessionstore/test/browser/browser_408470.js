13ecfa2501efcce3aa6e15ac428ea8a5104b117c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:56:03 +0000

    Bug 408470 - "Page Style (e.g. "No Style") does not persist across sessions" [r=dietrich]
    UltraBlame original commit: 4a01683e50bbe5d77c289f9db23d1baa064d0f8e

---
+++ browser/components/sessionstore/test/browser/browser_408470.js
@@ -0,0 +1,85 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  
+  waitForExplicitFinish();
+  
+  let pendingCount = 1;
+  let testUrl = "chrome://mochikit/content/browser/" +
+    "browser/components/sessionstore/test/browser/browser_408470_sample.html";
+  let tab = gBrowser.addTab(testUrl);
+  
+  tab.linkedBrowser.addEventListener("load", function(aEvent) {
+    
+    Array.forEach(tab.linkedBrowser.contentDocument.styleSheets, function(aSS, aIx) {
+      pendingCount++;
+      let ssTitle = aSS.title;
+      stylesheetSwitchAll(tab.linkedBrowser.contentWindow, ssTitle);
+      
+      let newTab = gBrowser.duplicateTab(tab);
+      newTab.linkedBrowser.addEventListener("load", function(aEvent) {
+        let states = Array.map(newTab.linkedBrowser.contentDocument.styleSheets,
+                               function(aSS) !aSS.disabled);
+        let correct = states.indexOf(true) == aIx && states.indexOf(true, aIx + 1) == -1;
+        
+        if (/^fail_/.test(ssTitle))
+          ok(!correct, "didn't restore stylesheet " + ssTitle);
+        else
+          ok(correct, "restored stylesheet " + ssTitle);
+        
+        gBrowser.removeTab(newTab);
+        if (--pendingCount == 0)
+          finish();
+      }, true);
+    });
+    
+    
+    tab.linkedBrowser.markupDocumentViewer.authorStyleDisabled = true;
+    let newTab = gBrowser.duplicateTab(tab);
+    newTab.linkedBrowser.addEventListener("load", function(aEvent) {
+      is(newTab.linkedBrowser.markupDocumentViewer.authorStyleDisabled, true,
+         "disabled all stylesheets");
+      
+      gBrowser.removeTab(newTab);
+      if (--pendingCount == 0)
+        finish();
+    }, true);
+    
+    gBrowser.removeTab(tab);
+  }, true);
+}
