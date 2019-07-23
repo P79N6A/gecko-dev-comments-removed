f500e553b58a92cdabd342177ce5fbd00237b741
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:30:23 +0000

    Fixed nsGNOMERegistry building in non-gtk configuration
    UltraBlame original commit: 1221f32ce4e8e6aa59513d9b2a02f5b418f36e66

--- uriloader/exthandler/unix/nsGNOMERegistry.cpp
+++ uriloader/exthandler/unix/nsGNOMERegistry.cpp
@@ -47,8 +47,10 @@
 #include "nsIGConfService.h"
 #include "nsIGnomeVFSService.h"
 
+#ifdef MOZ_WIDGET_GTK2
 #include <glib.h>
 #include <glib-object.h>
+#endif
 
  PRBool
 nsGNOMERegistry::HandlerExists(const char *aProtocolScheme)
