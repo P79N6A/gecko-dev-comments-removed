0eb291c5233b07af0350c814b5dc78db144a496a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:49:02 +0000

    Bug 396651. Fix JS Request assert in nsXPCComponents_Utils::LookupMethod. r/sr/a=brendan
    UltraBlame original commit: 9e63c794e07a86dd5a8bf0e2259778cb79235cfd

--- js/src/xpconnect/src/xpccomponents.cpp
+++ js/src/xpconnect/src/xpccomponents.cpp
@@ -2761,6 +2761,8 @@ nsXPCComponents_Utils::LookupMethod()
     if(NS_FAILED(rv) || !cx)
         return NS_ERROR_FAILURE;
 
+    JSAutoRequest ar(cx);
+
     
     jsval *retval = nsnull;
     rv = cc->GetRetValPtr(&retval);
