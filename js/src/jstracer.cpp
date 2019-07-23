b580925a76525c4faf58c8adb95f338d234ae258
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:40:41 +0000

    Merge. Nothing to see here. Move along.
    UltraBlame original commit: c4a4d3b58841c608a166665744d8dd75f4dc88c1

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -4129,6 +4129,7 @@ TraceRecorder::forInLoop(LIns*& id_ins)
 
     
     
+    
     int flag = 0;
     id_ins = NULL;
 
@@ -4152,6 +4153,11 @@ TraceRecorder::forInLoop(LIns*& id_ins)
 
     JSNativeEnumerator* ne;
 
+    
+    
+    
+    
+    flag = 1;
     ne = (JSNativeEnumerator*) (stateval & ~jsval(3));
     if (ne->cursor == 0)
         goto done;
@@ -4168,8 +4174,6 @@ TraceRecorder::forInLoop(LIns*& id_ins)
                             lir->ins2i(LIR_lsh, cursor_ins, (sizeof(jsid) == 4) ? 2 : 3));
 
 
-    
-    flag = 1;
     id_ins = lir->insLoadi(id_addr_ins, 0);
 done:
     stack(0, lir->insImm(flag));
