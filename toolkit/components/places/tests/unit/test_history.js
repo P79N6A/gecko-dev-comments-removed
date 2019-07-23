a175a3aaea952e435d1888b44339dfc143a4334c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:37 +0000

    Bug 376786 - Annotation-based queries are broken. r=dietrich.
    UltraBlame original commit: 6d717d1c232619ab549c113ceff709ec6345b8d9

--- toolkit/components/places/tests/unit/test_history.js
+++ toolkit/components/places/tests/unit/test_history.js
@@ -134,6 +134,24 @@ function run_test() {
   result = histsvc.executeQuery(query, options);
   result.root.containerOpen = true;
   do_check_eq(result.root.childCount, 2);
+  
+  
+  var annos = Cc["@mozilla.org/browser/annotation-service;1"].
+              getService(Ci.nsIAnnotationService);
+  annos.setAnnotationInt32(uri("http://mozilla.com/"), "testAnno", 0, 0,
+                           Ci.nsIAnnotationService.EXPIRE_NEVER);
+  query.annotation = "testAnno";
+  result = histsvc.executeQuery(query, options);
+  result.root.containerOpen = true;
+  do_check_eq(result.root.childCount, 1);
+  do_check_eq(result.root.getChild(0).uri, "http://mozilla.com/");
+
+  
+  query.annotationIsNot = true;
+  result = histsvc.executeQuery(query, options);
+  result.root.containerOpen = true;
+  do_check_eq(result.root.childCount, 1);
+  do_check_eq(result.root.getChild(0).uri, "http://google.com/");
 
   
   do_check_true(!histsvc.historyDisabled);
