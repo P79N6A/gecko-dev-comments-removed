3f088bcb51e120fd826c522dacdff94cbb5b5680
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:57:53 +0000

    Bug 528829 -  If mouse button is held down during page transition, Firefox stops responding to mouse clicks, r=roc
    UltraBlame original commit: 6bd1b6ea49c4c34b6785cba1b56244801456a30c

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -1028,6 +1028,18 @@ protected:
   
   void RestoreRootScrollPosition();
 
+  void MaybeReleaseCapturingContent()
+  {
+    nsCOMPtr<nsFrameSelection> frameSelection = FrameSelection();
+    if (frameSelection) {
+      frameSelection->SetMouseDownState(PR_FALSE);
+    }
+    if (gCaptureInfo.mContent &&
+        gCaptureInfo.mContent->GetOwnerDoc() == mDocument) {
+      SetCapturingContent(nsnull, 0);
+    }
+  }
+
   nsCOMPtr<nsICSSStyleSheet> mPrefStyleSheet; 
                                               
 #ifdef DEBUG
@@ -1831,6 +1843,8 @@ PresShell::Destroy()
 
   if (mHaveShutDown)
     return NS_OK;
+
+  MaybeReleaseCapturingContent();
 
   mContentToScrollTo = nsnull;
 
@@ -7075,6 +7089,8 @@ FreezeSubDocument(nsIDocument *aDocument, void *aData)
 void
 PresShell::Freeze()
 {
+  MaybeReleaseCapturingContent();
+
   mDocument->EnumerateFreezableElements(FreezeElement, this);
 
   if (mCaret)
