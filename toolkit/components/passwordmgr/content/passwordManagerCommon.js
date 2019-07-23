d0ed34537bbfe87dd82461861a30f673b014b47b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:40:18 +0000

    Bug 324063: "passwords never saved" is too hard to find, so move it into it's own window, r=mconnor, ui-r=beltzner
    UltraBlame original commit: 59b0b7de2f5233f1d739195d84b6f768bea2af9b

---
+++ toolkit/components/passwordmgr/content/passwordManagerCommon.js
@@ -0,0 +1,289 @@
+# -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+#
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http:
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Mozilla Communicator client code, released
+# March 31, 1998.
+#
+# The Initial Developer of the Original Code is
+# Netscape Communications Corporation.
+# Portions created by the Initial Developer are Copyright (C) 1998
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Ben "Count XULula" Goodger
+#   Brian Ryner <bryner@brianryner.com>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+
+
+var kObserverService;
+var gSelectUserInUse = false;
+
+
+var passwordmanager     = null;
+
+
+var signons             = [];
+var rejects             = [];
+var deletedSignons      = [];
+var deletedRejects      = [];
+
+var signonsTree;
+var rejectsTree;
+
+var showingPasswords = false;
+
+function Startup() {
+  
+  passwordmanager = Components.classes["@mozilla.org/passwordmanager;1"].getService(Components.interfaces.nsIPasswordManager);
+
+  
+  kObserverService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
+  kObserverService.addObserver(signonReloadDisplay, "signonChanged", false);
+
+  
+  kObserverService.addObserver(signonReloadDisplay, "signonSelectUser", false);
+
+  signonsTree = document.getElementById("signonsTree");
+  rejectsTree = document.getElementById("rejectsTree");
+}
+
+function Shutdown() {
+  kObserverService.removeObserver(signonReloadDisplay, "signonChanged");
+  kObserverService.removeObserver(signonReloadDisplay, "signonSelectUser");
+}
+
+var signonReloadDisplay = {
+  observe: function(subject, topic, state) {
+    if (topic == "signonChanged") {
+      if (state == "signons") {
+        signons.length = 0;
+        if (lastSignonSortColumn == "host") {
+          lastSignonSortAscending = !lastSignonSortAscending; 
+        }
+        LoadSignons();
+      } else if (state == "rejects") {
+        rejects.length = 0;
+        if (lastRejectSortColumn == "host") {
+          lastRejectSortAscending = !lastRejectSortAscending; 
+        }
+        LoadRejects();
+      }
+    } else if (topic == "signonSelectUser") {
+      if (state == "suspend") {
+        gSelectUserInUse = true;
+        document.getElementById("removeSignon").disabled = true;
+        document.getElementById("removeAllSignons").disabled = true;
+        document.getElementById("togglePasswords").disabled = true;
+      } else if (state == "resume") {
+        gSelectUserInUse = false;
+        var selections = GetTreeSelections(signonsTree);
+        if (selections.length > 0) {
+          document.getElementById("removeSignon").disabled = false;
+        }
+        if (signons.length > 0) {
+          document.getElementById("removeAllSignons").disabled = false;
+          document.getElementById("togglePasswords").disabled = false;
+        }
+      } else if (state == "inUse") {
+        gSelectUserInUse = true;
+      }
+    }
+  }
+}
+
+
+
+function DeleteAllFromTree(tree, view, table, deletedTable, removeButton, removeAllButton) {
+
+  
+  for (var i=0; i<table.length; i++) {
+    deletedTable[deletedTable.length] = table[i];
+  }
+  table.length = 0;
+
+  
+  view.selection.select(-1); 
+
+  
+  view.rowCount = 0;
+
+  var box = tree.treeBoxObject;
+  box.rowCountChanged(0, -deletedTable.length);
+  box.invalidate();
+
+
+  
+  document.getElementById(removeButton).setAttribute("disabled", "true")
+  document.getElementById(removeAllButton).setAttribute("disabled","true");
+}
+
+function DeleteSelectedItemFromTree
+    (tree, view, table, deletedTable, removeButton, removeAllButton) {
+
+  var box = tree.treeBoxObject;
+
+  
+  
+
+  var selection = box.view.selection;
+  var oldSelectStart = table.length;
+  box.beginUpdateBatch();
+
+  var selCount = selection.getRangeCount();
+  var min = new Object();
+  var max = new Object();
+
+  for (var s = 0; s < selCount; ++s) {
+    selection.getRangeAt(s, min, max);
+    var minVal = min.value;
+    var maxVal = max.value;
+
+    oldSelectStart = minVal < oldSelectStart ? minVal : oldSelectStart;
+
+    var rowCount = maxVal - minVal + 1;
+    view.rowCount -= rowCount;
+    box.rowCountChanged(minVal, -rowCount);
+
+    for (var i = minVal; i <= maxVal; ++i) {
+      deletedTable[deletedTable.length] = table[i];
+      table[i] = null;
+    }
+  }
+
+  
+  for (var j = 0; j < table.length; ++j) {
+    if (!table[j]) {
+      var k = j;
+      while (k < table.length && !table[k])
+        k++;
+
+      table.splice(j, k-j);
+    }
+  }
+
+  box.endUpdateBatch();
+
+  
+  var removeBtn = document.getElementById(removeButton);
+  var removeAllBtn = document.getElementById(removeAllButton);
+
+  if (table.length) {
+    removeBtn.removeAttribute("disabled");
+    removeAllBtn.removeAttribute("disabled");
+
+    selection.select(oldSelectStart < table.length ? oldSelectStart : table.length - 1);
+  } else {
+    removeBtn.setAttribute("disabled", "true");
+    removeAllBtn.setAttribute("disabled", "true");
+  }
+}
+
+function GetTreeSelections(tree) {
+  var selections = [];
+  var select = tree.view.selection;
+  if (select) {
+    var count = select.getRangeCount();
+    var min = new Object();
+    var max = new Object();
+    for (var i=0; i<count; i++) {
+      select.getRangeAt(i, min, max);
+      for (var k=min.value; k<=max.value; k++) {
+        if (k != -1) {
+          selections[selections.length] = k;
+        }
+      }
+    }
+  }
+  return selections;
+}
+
+function SortTree(tree, view, table, column, lastSortColumn, lastSortAscending, updateSelection) {
+
+  
+  var selections = GetTreeSelections(tree);
+  var selectedNumber = selections.length ? table[selections[0]].number : -1;
+
+  
+  var ascending = (column == lastSortColumn) ? !lastSortAscending : true;
+
+  
+  var compareFunc;
+  if (ascending) {
+    compareFunc = function compare(first, second) {
+      return CompareLowerCase(first[column], second[column]);
+    }
+  } else {
+    compareFunc = function compare(first, second) {
+      return CompareLowerCase(second[column], first[column]);
+    }
+  }
+  table.sort(compareFunc);
+
+  
+  var selectedRow = -1;
+  if (selectedNumber>=0 && updateSelection) {
+    for (var s=0; s<table.length; s++) {
+      if (table[s].number == selectedNumber) {
+        
+        
+        tree.view.selection.select(-1);
+        tree.view.selection.select(s);
+        selectedRow = s;
+        break;
+      }
+    }
+  }
+
+  
+  tree.treeBoxObject.invalidate();
+  if (selectedRow >= 0) {
+    tree.treeBoxObject.ensureRowIsVisible(selectedRow)
+  }
+
+  return ascending;
+}
+
+
+
+
+function CompareLowerCase(first, second) {
+
+  var firstLower  = first.toLowerCase();
+  var secondLower = second.toLowerCase();
+
+  if (firstLower < secondLower) {
+    return -1;
+  }
+
+  if (firstLower > secondLower) {
+    return 1;
+  }
+
+  return 0;
+}
