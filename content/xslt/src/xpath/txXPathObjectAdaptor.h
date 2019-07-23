195f794b918e91b65275f1f4f9120921cf3f903b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:06:17 +0000

    Fix for bug 436864 (XPath needs to support an 'object' type as a parameter and return type). r/sr=sicking.
    UltraBlame original commit: 5ea477cf4e7bb08bab763b3439cd3c00d5b14511

---
+++ content/xslt/src/xpath/txXPathObjectAdaptor.h
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef txXPathObjectAdaptor_h__
+#define txXPathObjectAdaptor_h__
+
+#include "txExprResult.h"
+#include "txINodeSet.h"
+#include "txIXPathObject.h"
+
+
+
+
+
+
+class txXPathObjectAdaptor : public txIXPathObject
+{
+public:
+    txXPathObjectAdaptor(txAExprResult *aValue) : mValue(aValue)
+    {
+        NS_ASSERTION(aValue,
+                     "Don't create a txXPathObjectAdaptor if you don't have a "
+                     "txAExprResult");
+    }
+
+    NS_DECL_ISUPPORTS
+
+    NS_IMETHODIMP_(txAExprResult*) GetResult()
+    {
+        return mValue;
+    }
+
+protected:
+    txXPathObjectAdaptor() : mValue(nsnull)
+    {
+    }
+
+    nsRefPtr<txAExprResult> mValue;
+};
+
+#endif 
