6e5eca65f046df1c5292411d7064697506001085
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:50:07 +0000

    Bug 424101 - Inputs with type image don't give a context menu to save the image; r=gavin ui-r=beltzner
    UltraBlame original commit: 8b9317b5ab2e326f4499335d140142e692c43963

--- browser/base/content/nsContextMenu.js
+++ browser/base/content/nsContextMenu.js
@@ -1246,7 +1246,8 @@ nsContextMenu.prototype = {
   
   isTargetAFormControl: function(aNode) {
     if (aNode instanceof HTMLInputElement)
-      return (aNode.type != "text" && aNode.type != "password");
+      return (aNode.type != "text" && aNode.type != "password" &&
+              aNode.type != "image");
 
     return (aNode instanceof HTMLButtonElement) ||
            (aNode instanceof HTMLSelectElement) ||
