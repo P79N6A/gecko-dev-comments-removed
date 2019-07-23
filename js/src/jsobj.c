ee50564f02f09bb5aac94ab751e34a2053b53a0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:55:07 +0000

    Fixing bug 408143. Make JSObjects share their prototypes scope (map) even if their ops differ, as long as their newObjectMap hooks are the same. r+a=brendan@mozilla.org
    UltraBlame original commit: b20b7b63b5eb290ce6e6fca2146bb32a8187a59a

--- js/src/jsobj.c
+++ js/src/jsobj.c
@@ -2585,8 +2585,9 @@ js_NewObject(JSContext *cx, JSClass *clasp, JSObject *proto, JSObject *parent)
 
 
 
+
     if (proto &&
-        (map = proto->map)->ops == ops &&
+        (map = proto->map)->ops->newObjectMap == ops->newObjectMap &&
         ((protoclasp = OBJ_GET_CLASS(cx, proto)) == clasp ||
          (!((protoclasp->flags ^ clasp->flags) &
             (JSCLASS_HAS_PRIVATE |
