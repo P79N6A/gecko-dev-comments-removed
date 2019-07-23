0d92905d63df64e4ac7a9a59ebbd3ee405392138
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:21:33 +0000

    Bug 465215 - tab values can get synchronized. r=dietrich
    UltraBlame original commit: 5c0da891a0902e7db281150c088758a03c8f455a

---
+++ browser/components/sessionstore/test/browser/browser_465215.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  
+  let ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
+  waitForExplicitFinish();
+  
+  let uniqueName = "bug 465215";
+  let uniqueValue1 = "as good as unique: " + Date.now();
+  let uniqueValue2 = "as good as unique: " + Math.random();
+  
+  
+  let tab1 = gBrowser.addTab();
+  tab1.linkedBrowser.addEventListener("load", function() {
+    tab1.linkedBrowser.removeEventListener("load", arguments.callee, true);
+    ss.setTabValue(tab1, uniqueName, uniqueValue1);
+    
+    
+    let tab2 = ss.duplicateTab(window, tab1);
+    is(ss.getTabValue(tab2, uniqueName), uniqueValue1, "tab value was duplicated");
+    
+    ss.setTabValue(tab2, uniqueName, uniqueValue2);
+    isnot(ss.getTabValue(tab1, uniqueName), uniqueValue2, "tab values aren't sync'd");
+    
+    
+    ss.setTabState(tab1, JSON.stringify({ entries: [] }));
+    tab1.linkedBrowser.addEventListener("load", function() {
+      tab1.linkedBrowser.removeEventListener("load", arguments.callee, true);
+      is(ss.getTabValue(tab1, uniqueName), "", "tab value was cleared");
+      
+      
+      gBrowser.removeTab(tab2);
+      gBrowser.removeTab(tab1);
+      finish();
+    }, true);
+  }, true);
+}
