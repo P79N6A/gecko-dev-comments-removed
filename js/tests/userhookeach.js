e787d8c5dd4142da35fb6472a0d2dbd150f89e65
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:49:58 +0000

    JavaScript Tests - sync Sisyphus support, bug 398005, not part of the build
    UltraBlame original commit: c82bc2d6d3d03687f1efafc322c07b0785ff2409

--- js/tests/userhookeach.js
+++ js/tests/userhookeach.js
@@ -40,38 +40,43 @@
 
 
 
-
 var gCheckInterval = 1000;
-
-
+var gCurrentTestId;
+var gReport;
 
 function userOnStart()
 {
+  dlog('userOnStart');
   registerDialogCloser();
 }
 
 function userOnBeforePage()
 {
   dlog('userOnBeforePage');
+  gCurrentTestId = /test=(.*);language/.exec(gSpider.mCurrentUrl.mUrl)[1];
+  cdump('JavaScriptTest: Begin Test ' + gCurrentTestId);
+  cdump('JavaScriptTest: Begin Execution ' + gCurrentTestId);
 }
-
-var gReport;
 
 function userOnAfterPage()
 {
   dlog('userOnAfterPage');
+  cdump('JavaScriptTest: End Execution ' + gCurrentTestId);
   checkTestCompleted();
 }
 
 function userOnStop()
 {
   
+  cdump('JavaScriptTest: End Test ' + gCurrentTestId);
   closeDialog();
   unregisterDialogCloser();
 }
 
 function checkTestCompleted()
 {
+  dlog('checkTestCompleted()');
+
   var win = gSpider.mDocument.defaultView;
   if (win.wrappedJSObject)
   {
@@ -79,27 +84,28 @@ function checkTestCompleted()
   }
   if (win.gPageCompleted)
   {
-    unregisterDialogCloser();
+    dlog('Page Completed');
+
+    cdump('JavaScriptTest: Begin Summary ' + gCurrentTestId);
+
     gPageCompleted = true;
 
     var gTestcases = win.gTestcases;
     if (typeof gTestcases == 'undefined')
     {
-      return;
+      cdump('JavaScriptTest: ' + gCurrentTestId +
+            ' gTestcases array not defined. Possible test failure.');
     }
-
-    for (var i = 0; i < gTestcases.length; i++)
+    else if (gTestcases.length == 0)
     {
-      var testcase = gTestcases[i];
-      cdump('test: '    + testcase.path + ' ' +
-            'bug: '         + testcase.bugnumber + ' ' +
-            'result: ' + (testcase.passed ? 'PASSED':'FAILED') + ' ' +
-            'type: browser ' +
-            'description: ' + testcase.description + ' ' +
-            'expected: '    + testcase.expect + ' ' +
-            'actual: '      + testcase.actual + ' ' +
-            'reason: '      + testcase.reason);
+      cdump('JavaScriptTest: ' + gCurrentTestId +
+            ' gTestcases array is empty. Tests not run.');
     }
+    else
+    {
+    }
+    cdump('JavaScriptTest: End Summary ' + gCurrentTestId);
+
     
     
     collectGarbage();
