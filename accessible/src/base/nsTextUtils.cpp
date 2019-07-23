c089d50583990ad50e7c43fa57dd6c00db7bc7d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:25:35 +0000

    Bustage fix for bug 345759
    UltraBlame original commit: 0948307f3591e4ebee088af37c8330c20d571b84

---
+++ accessible/src/base/nsTextUtils.cpp
@@ -0,0 +1,180 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsTextUtils.h"
+
+#include "nsAccessNode.h"
+
+
+
+
+PRBool
+nsLangTextAttr::equal(nsIDOMElement *aElm)
+{
+  nsCOMPtr<nsIContent> content(do_QueryInterface(aElm));
+  if (!content)
+    return PR_FALSE;
+
+  nsAutoString lang;
+  nsAccUtils::GetLanguageFor(content, mRootContent, lang);
+
+  return lang == mLang;
+}
+
+
+
+
+
+
+
+struct nsCSSTextAttrMapItem
+{
+  const char* mCSSName;
+  const char* mCSSValue;
+  const char* mAttrName;
+  const char* mAttrValue;
+};
+
+
+
+
+
+const char* const kAnyValue = nsnull;
+const char* const kCopyName = nsnull;
+const char* const kCopyValue = nsnull;
+
+static nsCSSTextAttrMapItem gCSSTextAttrsMap[] = {
+  
+  { "background-color",  kAnyValue,       kCopyName,                  kCopyValue },
+  { "color",             kAnyValue,       kCopyName,                  kCopyValue },
+  { "font-family",       kAnyValue,       kCopyName,                  kCopyValue },
+  { "font-size",         kAnyValue,       kCopyName,                  kCopyValue },
+  { "font-style",        kAnyValue,       kCopyName,                  kCopyValue },
+  { "font-weight",       kAnyValue,       kCopyName,                  kCopyValue },
+  { "text-decoration",   "line-through",  "text-line-through-style",  "solid" },
+  { "text-decoration",   "underline",     "text-underline-style",     "solid" },
+  { "text-align",        kAnyValue,       kCopyName,                  kCopyValue },
+  { "text-indent",       kAnyValue,       kCopyName,                  kCopyValue }
+};
+
+nsCSSTextAttr::nsCSSTextAttr(PRBool aIncludeDefAttrValue, nsIDOMElement *aElm,
+                             nsIDOMElement *aRootElm) :
+  mIndex(-1), mIncludeDefAttrValue(aIncludeDefAttrValue)
+{
+  nsAccessNode::GetComputedStyleDeclaration(EmptyString(), aElm,
+                                            getter_AddRefs(mStyleDecl));
+
+  if (!mIncludeDefAttrValue)
+    nsAccessNode::GetComputedStyleDeclaration(EmptyString(), aRootElm,
+                                              getter_AddRefs(mDefStyleDecl));
+}
+
+PRBool
+nsCSSTextAttr::equal(nsIDOMElement *aElm)
+{
+  if (!aElm || !mStyleDecl)
+    return PR_FALSE;
+
+  nsCOMPtr<nsIDOMCSSStyleDeclaration> currStyleDecl;
+  nsAccessNode::GetComputedStyleDeclaration(EmptyString(), aElm,
+                                            getter_AddRefs(currStyleDecl));
+  if (!currStyleDecl)
+    return PR_FALSE;
+
+  NS_ConvertASCIItoUTF16 cssName(gCSSTextAttrsMap[mIndex].mCSSName);
+
+  nsAutoString currValue;
+  nsresult rv = currStyleDecl->GetPropertyValue(cssName, currValue);
+  if (NS_FAILED(rv))
+    return PR_FALSE;
+
+  nsAutoString value;
+  rv = mStyleDecl->GetPropertyValue(cssName, value);
+  return NS_SUCCEEDED(rv) && value == currValue;
+}
+
+PRBool
+nsCSSTextAttr::iterate()
+{
+  return ++mIndex < static_cast<PRInt32>(NS_ARRAY_LENGTH(gCSSTextAttrsMap));
+}
+
+PRBool
+nsCSSTextAttr::get(nsACString& aName, nsAString& aValue)
+{
+  if (!mStyleDecl)
+    return PR_FALSE;
+
+  NS_ConvertASCIItoUTF16 cssName(gCSSTextAttrsMap[mIndex].mCSSName);
+  nsresult rv = mStyleDecl->GetPropertyValue(cssName, aValue);
+  if (NS_FAILED(rv))
+    return PR_FALSE;
+
+  
+  
+  
+  if (!mIncludeDefAttrValue) {
+    if (!mDefStyleDecl)
+      return PR_FALSE;
+
+    nsAutoString defValue;
+    mDefStyleDecl->GetPropertyValue(cssName, defValue);
+    if (defValue == aValue)
+      return PR_FALSE;
+  }
+
+  
+  
+  const char *cssValue = gCSSTextAttrsMap[mIndex].mCSSValue;
+  if (cssValue != kAnyValue && !aValue.EqualsASCII(cssValue))
+    return PR_FALSE;
+
+  
+  if (gCSSTextAttrsMap[mIndex].mAttrName != kCopyName)
+    aName = gCSSTextAttrsMap[mIndex].mAttrName;
+  else
+    aName = gCSSTextAttrsMap[mIndex].mCSSName;
+
+  
+  const char *attrValue = gCSSTextAttrsMap[mIndex].mAttrValue;
+  if (attrValue != kCopyValue)
+    AppendASCIItoUTF16(attrValue, aValue);
+
+  return PR_TRUE;
+}
+
---
+++ accessible/src/base/nsTextUtils.h
@@ -0,0 +1,112 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsTextUtils_h_
+#define nsTextUtils_h_
+
+#include "nsIDOMElement.h"
+#include "nsIDOMCSSStyleDeclaration.h"
+
+#include "nsIContent.h"
+
+#include "nsCOMPtr.h"
+#include "nsString.h"
+
+
+
+
+class nsTextAttr
+{
+public:
+  
+
+
+
+  virtual PRBool equal(nsIDOMElement *aElm) = 0;
+};
+
+
+
+
+
+class nsLangTextAttr : public nsTextAttr
+{
+public:
+  nsLangTextAttr(nsAString& aLang, nsIContent *aRootContent) :
+    mLang(aLang), mRootContent(aRootContent) { }
+
+  virtual PRBool equal(nsIDOMElement *aElm);
+
+private:
+  nsString mLang;
+  nsCOMPtr<nsIContent> mRootContent;
+};
+
+
+
+
+
+class nsCSSTextAttr : public nsTextAttr
+{
+public:
+  nsCSSTextAttr(PRBool aIncludeDefAttrValue, nsIDOMElement *aElm,
+                nsIDOMElement *aRootElm);
+
+  
+  virtual PRBool equal(nsIDOMElement *aElm);
+
+  
+  
+
+
+  PRBool iterate();
+
+  
+
+
+  PRBool get(nsACString& aName, nsAString& aValue);
+
+private:
+  PRInt32 mIndex;
+  PRBool mIncludeDefAttrValue;
+
+  nsCOMPtr<nsIDOMCSSStyleDeclaration> mStyleDecl;
+  nsCOMPtr<nsIDOMCSSStyleDeclaration> mDefStyleDecl;
+};
+
+#endif
