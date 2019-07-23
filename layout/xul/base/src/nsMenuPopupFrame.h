451980b5ae1f7c2cefb401dc33fa9a1eb0e7965a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:48:40 +0000

    Bug 394887, lazily create popup content and widgets, r+sr=bz, a=roc
    UltraBlame original commit: 4cb943c78a05b6d761dc63d6d344d26ec00977d6

--- layout/xul/base/src/nsMenuPopupFrame.h
+++ layout/xul/base/src/nsMenuPopupFrame.h
@@ -183,22 +183,7 @@ public:
   NS_IMETHOD SetInitialChildList(nsIAtom*        aListName,
                                  nsIFrame*       aChildList);
 
-  virtual PRBool IsLeaf() const
-  {
-    if (!mGeneratedChildren && mPopupType == ePopupTypeMenu) {
-      
-      
-      
-      
-      
-      nsIContent* parentContent = mContent->GetParent();
-      if (parentContent &&
-          !parentContent->HasAttr(kNameSpaceID_None, nsGkAtoms::sizetopopup))
-        return PR_TRUE;
-    }
-
-    return PR_FALSE;
-  }
+  virtual PRBool IsLeaf() const;
 
   
   
