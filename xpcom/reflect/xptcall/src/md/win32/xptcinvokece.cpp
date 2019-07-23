923f432c36cb87bb0d24d2891ad86a58b609c91c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:55:49 +0000

    bug 530393 - improve xptcinvoke for Windows Mobile and Windows CE on ARM r=vlad
    UltraBlame original commit: b2106fb015d3ab203508079e67e6a11baf404837

--- xpcom/reflect/xptcall/src/md/win32/xptcinvokece.cpp
+++ xpcom/reflect/xptcall/src/md/win32/xptcinvokece.cpp
@@ -39,107 +39,68 @@
 
 #include "xptcprivate.h"
 
+extern "C" PRUint32 *
+writeArgs(PRUint32 *d,
+          PRUint32  paramCount,
+          nsXPTCVariant *params)
+{
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  d--;
+  for (nsXPTCVariant * s = &params[paramCount-1]; paramCount != 0; paramCount--, s--, d--) {
+    
+    
+    if(s->IsPtrData())
+        {
+            *((void**)d) = s->ptr;
+            continue;
+        }
+        switch(s->type)
+        {
+        case nsXPTType::T_I8     :  *((PRInt32*)   d) = s->val.i8;     break;
+        case nsXPTType::T_I16    :  *((PRInt32*)   d) = s->val.i16;    break;
+        case nsXPTType::T_I32    :  *((PRInt32*)   d) = s->val.i32;    break;
+        case nsXPTType::T_I64    :  *((PRInt64*) --d) = s->val.i64;    break;
+        case nsXPTType::T_U8     :  *((PRUint32*)  d) = s->val.u8;     break;
+        case nsXPTType::T_U16    :  *((PRUint32*)  d) = s->val.u16;    break;
+        case nsXPTType::T_U32    :  *((PRUint32*)  d) = s->val.u32;    break;
+        case nsXPTType::T_U64    :  *((PRUint64*)--d) = s->val.u64;    break;
+        case nsXPTType::T_FLOAT  :  *((float*)     d) = s->val.f;      break;
+        case nsXPTType::T_DOUBLE :  *((double*)  --d) = s->val.d;      break;
+        case nsXPTType::T_BOOL   :  *((PRInt32*)   d) = s->val.b;      break;
+        case nsXPTType::T_CHAR   :  *((PRInt32*)   d) = s->val.c;      break;
+        case nsXPTType::T_WCHAR  :  *((PRInt32*)   d) = s->val.wc;     break;
+        default:
+            
+            *((void**)d) = s->val.p;
+            break;
+        }
+
+  }
+  return ++d;
+}
+
 extern "C" nsresult
-asmXPTC_InvokeByIndex( nsISupports* that,
-                       PRUint32 methodIndex,
-  					   PRUint32 paramCount,
-					   nsXPTCVariant* params,
-					   PRUint32 pfn_CopyToStack,
-					   PRUint32 pfn_CountWords);  
-
-
-
-
-
-extern "C" static void
-invoke_copy_to_stack(PRUint32* d, PRUint32 paramCount, nsXPTCVariant* s)
-{
-  for(; paramCount > 0; paramCount--, d++, s++)
-  {
-    if(s->IsPtrData())
-    {
-      *((void**)d) = s->ptr;
-      continue;
-    }
-    
-    
-
-    switch(s->type)
-    {
-      case nsXPTType::T_I8     : *((PRInt32*) d) = s->val.i8;          break;
-      case nsXPTType::T_I16    : *((PRInt32*) d) = s->val.i16;         break;
-      case nsXPTType::T_I32    : *((PRInt32*) d) = s->val.i32;         break;
-      case nsXPTType::T_I64    : *((PRInt64*) d) = s->val.i64; d++;    break;
-      case nsXPTType::T_U8     : *((PRUint32*)d) = s->val.u8;          break;
-      case nsXPTType::T_U16    : *((PRUint32*)d) = s->val.u16;         break;
-      case nsXPTType::T_U32    : *((PRUint32*)d) = s->val.u32;         break;
-      case nsXPTType::T_U64    : *((PRUint64*)d) = s->val.u64; d++;    break;
-      case nsXPTType::T_FLOAT  : *((float*)   d) = s->val.f;           break;
-      case nsXPTType::T_DOUBLE : *((double*)  d) = s->val.d;   d++;    break;
-      case nsXPTType::T_BOOL   : *((PRInt32*) d) = s->val.b;           break;
-      case nsXPTType::T_CHAR   : *((PRInt32*) d) = s->val.c;           break;
-      case nsXPTType::T_WCHAR  : *((PRInt32*) d) = s->val.wc;          break;
-      default:
-        
-        *((void**)d) = s->val.p;
-        break;
-    }
-  }
-}
-
-
-extern "C" static uint32
-invoke_count_words(PRUint32 paramCount, nsXPTCVariant* s)
-{
-  uint32 nCnt = 0;
-  
-  for(; paramCount > 0; paramCount--, s++)
-  {
-    if(s->IsPtrData())
-    {
-      nCnt++;
-      continue;
-    }
-    switch(s->type)
-    {
-      case nsXPTType::T_I64    : 
-      case nsXPTType::T_U64    : 
-      case nsXPTType::T_DOUBLE : 
-        nCnt += 2;
-        break;
-        
-      case nsXPTType::T_I8     : 
-      case nsXPTType::T_I16    : 
-      case nsXPTType::T_I32    : 
-      case nsXPTType::T_U8     : 
-      case nsXPTType::T_U16    : 
-      case nsXPTType::T_U32    : 
-      case nsXPTType::T_FLOAT  : 
-      case nsXPTType::T_BOOL   : 
-      case nsXPTType::T_CHAR   : 
-      case nsXPTType::T_WCHAR  : 
-      default:
-        nCnt++;
-        break;
-    }
-  }
-  
-  
-  
-  return nCnt;
-}
-
+asmXPTC_InvokeByIndex(nsISupports* that,
+		      PRUint32 methodIndex,
+		      PRUint32 paramCount,
+		      nsXPTCVariant* params);  
 
 extern "C" NS_EXPORT  nsresult NS_FROZENCALL
 NS_InvokeByIndex_P(nsISupports* that, PRUint32 methodIndex,
-                 PRUint32 paramCount, nsXPTCVariant* params)
+		   PRUint32 paramCount, nsXPTCVariant* params)
 {
-  return asmXPTC_InvokeByIndex(that,
-                               methodIndex,
-                               paramCount,
-                               params,
-                               (PRUint32) &invoke_copy_to_stack,
-                               (PRUint32) &invoke_count_words);
+  return asmXPTC_InvokeByIndex(that, methodIndex, paramCount, params);
 }
 
 
