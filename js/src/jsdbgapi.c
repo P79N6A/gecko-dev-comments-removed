e10ce16c8734ffcb19313250d47ef4ff6eec4f3b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:49:27 +0000

    Bug 396758: the system flag is moved from GC flags to JSObject itself. r=brendan
    UltraBlame original commit: 925e8a3d1bb92df66feae6c295d615f5140fa535

--- js/src/jsdbgapi.c
+++ js/src/jsdbgapi.c
@@ -1643,16 +1643,21 @@ JS_FlagScriptFilenamePrefix(JSRuntime *rt, const char *prefix, uint32 flags)
 JS_PUBLIC_API(JSBool)
 JS_IsSystemObject(JSContext *cx, JSObject *obj)
 {
-    return (*js_GetGCThingFlags(obj) & GCF_SYSTEM) != 0;
+    return STOBJ_IS_SYSTEM(obj);
 }
 
 JS_PUBLIC_API(void)
 JS_FlagSystemObject(JSContext *cx, JSObject *obj)
 {
-    uint8 *flagp;
+    
 
-    flagp = js_GetGCThingFlags(obj);
-    *flagp |= GCF_SYSTEM;
+
+
+
+
+
+
+    STOBJ_SET_SYSTEM(obj);
 }
 
 
