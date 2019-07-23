c2839c2ffcc593c89215db892e048e816d2b005d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:11:33 +0000

    Bug 427831 - CHECK FAILED: file.txt == CVS/ -- no more CVS! r=waldo
    UltraBlame original commit: 08f156f5782d800706a10a8adeb944bc129e58d1

--- netwerk/test/httpserver/test/test_default_index_handler.js
+++ netwerk/test/httpserver/test/test_default_index_handler.js
@@ -95,8 +95,7 @@ function hiddenDataCheck(bytes, uri, path)
   var top = ios.newURI(uri, null, null);
 
   
-  var dirEntries = [{name: "CVS", isDirectory: true}, 
-                    {name: "file.txt", isDirectory: false},
+  var dirEntries = [{name: "file.txt", isDirectory: false},
                     {name: "SHOULD_SEE_THIS.txt^", isDirectory: false}];
 
   for (var i = 0; i < items.length; i++)
