ca271152c6c85fdd3bb9a06382d5193a83fa6ff9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:30:28 +0000

    FilePicker implementation
    UltraBlame original commit: 9a537750df3610def70a9c93ad68c8ad70b97750

--- widget/src/qt/nsWidgetFactory.cpp
+++ widget/src/qt/nsWidgetFactory.cpp
@@ -59,6 +59,7 @@
 
 
 
+#include "nsFilePicker.h"
 #include "nsClipboard.h"
 #include "nsClipboardHelper.h"
 #include "nsIdleServiceQt.h"
@@ -66,7 +67,6 @@
 #include "nsSound.h"
 #include "nsBidiKeyboard.h"
 #include "nsNativeThemeQt.h"
-
 
 
 
@@ -101,7 +101,7 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(nsIdleServiceQt)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsSound)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsNativeThemeQt)
 
-
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsFilePicker)
 
 
 static const nsModuleComponentInfo components[] =
@@ -169,21 +169,18 @@ static const nsModuleComponentInfo components[] =
     { "Native Theme Renderer",
       NS_THEMERENDERER_CID,
       "@mozilla.org/chrome/chrome-native-theme;1",
-      nsNativeThemeQtConstructor }
-
-
-
-
-
+      nsNativeThemeQtConstructor },
+    { "Qt File Picker",
+      NS_FILEPICKER_CID,
+      "@mozilla.org/filepicker;1",
+      nsFilePickerConstructor }
 
 };
 
 PR_STATIC_CALLBACK(void)
 nsWidgetQtModuleDtor(nsIModule *aSelf)
 {
-
     nsSound::Shutdown();
-
     nsAppShellShutdown(aSelf);
 }
 
