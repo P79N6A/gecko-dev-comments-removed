b64c633f78582497c57b9d921a1f161bc1f0ade1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:29:56 +0000

    bug 428366 - JavaScript Test by Blake Kaplan.
    UltraBlame original commit: 1305b9b2633b2d82a755312c77dbe2bf9638170e

---
+++ js/tests/js1_5/Regress/regress-428366.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-428366.js';
+
+var BUGNUMBER = 428366;
+var summary = 'Do not assert deleting eval 16 times';
+var actual = '';
+var expect = '';
+
+printBugNumber(BUGNUMBER);
+printStatus (summary);
+
+this.__proto__.x = eval;
+for (i = 0; i < 16; ++i) delete eval;
+(function w() { x = 1; })();
+ 
+reportCompare(expect, actual, summary);
