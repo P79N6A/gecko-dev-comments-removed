bbd658524da6ee5e309104472699009557979be4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:50:52 +0000

    Bug 369428 - nsExternalAppHandler::SetUpTempFile uses a poor source of randomness, resulting in predictable filenames.  r=kaie, sr=rrelyea, a=blocking1.9
    UltraBlame original commit: 609965a99cdece97904d1eeceb7a5ed64f32c589

---
+++ security/manager/ssl/src/nsRandomGenerator.cpp
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsRandomGenerator.h"
+#include "pk11pub.h"
+
+
+
+
+NS_IMPL_ISUPPORTS1(nsRandomGenerator, nsIRandomGenerator)
+
+
+
+
+
+
+NS_IMETHODIMP
+nsRandomGenerator::GenerateRandomBytes(PRUint32 aLength,
+                                       PRUint8 **aBuffer)
+{
+  NS_ENSURE_ARG_POINTER(aBuffer);
+
+  PRUint8 *buf = reinterpret_cast<PRUint8 *>(NS_Alloc(aLength));
+  if (!buf)
+    return NS_ERROR_OUT_OF_MEMORY;
+
+  SECStatus srv = PK11_GenerateRandom(buf, aLength);
+  if (SECSuccess != srv) {
+    NS_Free(buf);
+    return NS_ERROR_FAILURE;
+  }
+
+  *aBuffer = buf;
+
+  return NS_OK;
+}
