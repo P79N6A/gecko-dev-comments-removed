dfb71344c56673e4f0986d8cca9709c3f21cbcd5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:04:48 +0000

    Bug 418766: Use width, not height, as a quick proxy for whether a line is empty. r+sr=roc, a=beltzner
    UltraBlame original commit: b44dd3d3ee5b4e0bf85d309f80ed6c47f8aa5114

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -1923,7 +1923,7 @@ nsBlockFrame::ReflowDirtyLines(nsBlockReflowState& aState)
       
       if (line.next() != end_lines()) {
         PRBool maybeWasEmpty = oldY == line.next()->mBounds.y;
-        PRBool isEmpty = line->mBounds.height == 0 && line->CachedIsEmpty();
+        PRBool isEmpty = line->mBounds.width == 0 && line->CachedIsEmpty();
         if (maybeReflowingForFirstTime  ||
             (isEmpty || maybeWasEmpty) ) {
           line.next()->MarkPreviousMarginDirty();
