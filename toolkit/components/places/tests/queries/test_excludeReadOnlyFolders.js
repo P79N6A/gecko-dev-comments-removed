e7ccde596d50068501fcd0a5e5e1e3beff258386
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:34:27 +0000

    Bug 430862 - create a test for excludeReadOnlyFolders query option, original test by ctalbert, r=dietrich
    UltraBlame original commit: 5f1882e23b39b794d4dece09b4155a17c25e81f6

---
+++ toolkit/components/places/tests/queries/test_excludeReadOnlyFolders.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var testData = [
+  
+  { isInQuery: true, isFolder: true, title: "Folder 1",
+    parentFolder: bmsvc.toolbarFolder },
+
+  
+  { isInQuery: false, isFolder: true, title: "Folder 2 RO",
+    parentFolder: bmsvc.toolbarFolder, readOnly: true }
+];
+
+function run_test() {
+  populateDB(testData);
+
+  var query = histsvc.getNewQuery();
+  query.setFolders([bmsvc.toolbarFolder], 1);
+
+  
+  var options = histsvc.getNewQueryOptions();
+  options.excludeQueries = true;
+  options.excludeReadOnlyFolders = true;
+
+  
+  var result = histsvc.executeQuery(query, options);
+  var root = result.root;
+  displayResultSet(root);
+  
+  do_check_eq(1, root.childCount);
+  do_check_eq("Folder 1", root.getChild(0).title);
+
+  root.containerOpen = false;
+}
