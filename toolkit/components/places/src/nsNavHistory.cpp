b211bca0dd40bfccec61522bbb9ad44ba5c6dd2c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:20:49 +0000

    Bug 494713 - Logic error in nsNavHistory::CalculateFrecencyInternal due to lack of braces around if statement, r=sdwilsh
    UltraBlame original commit: de82397b1ea73ad3c45e6d10cfd124d735afddae

--- toolkit/components/places/src/nsNavHistory.cpp
+++ toolkit/components/places/src/nsNavHistory.cpp
@@ -7535,8 +7535,7 @@ nsNavHistory::CalculateFrecencyInternal(PRInt64 aPlaceId,
           break;
         default:
           
-          if (visitType)
-            NS_WARNING("new transition but no weight for frecency");
+          NS_WARN_IF_FALSE(!visitType, "new transition but no weight for frecency");
           bonus = mDefaultVisitBonus;
           break;
       }
