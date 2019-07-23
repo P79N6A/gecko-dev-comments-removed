c9d5b00f14904dddb754c6f095ca9504300811a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:10:23 +0000

    JavaScript Tests - regression test for bug 424954
    UltraBlame original commit: b4470c7ea852a272e377a7fd88a1d5218f06fdbc

---
+++ js/tests/js1_5/Array/regress-424954.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-424954.js';
+
+var BUGNUMBER = 424954;
+var summary = 'Do not crash with [].concat(null)';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  [].concat(null);
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
