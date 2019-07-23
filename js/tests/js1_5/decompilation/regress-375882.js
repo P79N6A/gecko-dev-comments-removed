fc190a02a61fab8080c1c2b3847dca4ba1a6b68a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:28 +0000

    JavaScript Test - regression test for bug 375882, by Jesse Ruderman
    UltraBlame original commit: 19865a6e4e27a0822ac553f327965a741d9b6a7a

---
+++ js/tests/js1_5/decompilation/regress-375882.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 375882;
+var summary = 'Decompilation of switch with case 0/0';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  var f = function() 
+    { 
+      switch(a) 
+      { 
+        case 0/0: a; 
+        case 1/0: b; 
+        case -1/0: c; 
+        case -0:
+        d; 
+      } 
+    };
+
+  expect = 'function ( ) { switch ( a ) { case 0 / 0 : a ; case 1 / 0 : b ; ' +
+    'case 1 / - 0 : c ; case - 0 : d ; default : ; } } ';
+
+  actual = f + '';
+
+  compareSource(expect, actual, summary);
+
+  exitFunc ('test');
+}
