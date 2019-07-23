9810e3c225dff861b1f557ee61e842ae83789fec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:10:59 +0000

    Bug 423060. Feed preview page broken when a default web reader is selected. r=gavin.
    UltraBlame original commit: 39b86814056d11533687de971826d26cc280abf5

--- toolkit/components/feeds/src/FeedProcessor.js
+++ toolkit/components/feeds/src/FeedProcessor.js
@@ -1421,9 +1421,9 @@ FeedProcessor.prototype = {
   
   
   onStartRequest: function FP_onStartRequest(request, context) {
+    
     var channel = request.QueryInterface(Ci.nsIChannel);
-    if (channel)
-      channel.contentType = "application/xml";
+    channel.contentType = "application/vnd.mozilla.maybe.feed";
     this._reader.onStartRequest(request, context);
   },
 
