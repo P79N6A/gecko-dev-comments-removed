193cf212f8ec88e450e56a5ffddd4c002cec0e1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:01:44 +0000

    Landing fix for bug 418377. Don't run code on invalid contextx. Patch by mrbkap@gmail.com, r+sr=jst@mozilla.org
    UltraBlame original commit: 98a7938432d9f65301fa35436f6656888eb389f7

--- js/src/xpconnect/src/xpcwrappedjsclass.cpp
+++ js/src/xpconnect/src/xpcwrappedjsclass.cpp
@@ -513,6 +513,8 @@ GetContextFromObject(JSObject *obj)
 
     
     XPCCallContext ccx(NATIVE_CALLER);
+    if(!ccx.IsValid())
+        return nsnull;
     XPCWrappedNativeScope* scope =
         XPCWrappedNativeScope::FindInJSObjectScope(ccx, obj);
     XPCContext *xpcc = scope->GetContext();
