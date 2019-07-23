e3831acf99d686d89a9a1b24714166fd8e076c9d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:37:33 +0000

    Fix bustage from bug 509557.
    UltraBlame original commit: 7653df541fbe64cfcb8bacce59ed8dee56b359b4

--- js/src/xpconnect/src/XPCChromeObjectWrapper.cpp
+++ js/src/xpconnect/src/XPCChromeObjectWrapper.cpp
@@ -260,7 +260,7 @@ XPC_COW_RewrapForChrome(JSContext *cx, JSObject *wrapperObj, jsval *vp)
       !nsXPCWrappedJSClass::IsWrappedJS(wn->Native())) {
     
     
-    return XPCNativeWrapperCtor(cx, obj, 1, vp, vp);
+    return XPCNativeWrapperCtor(cx, nsnull, 1, vp, vp);
   }
 
   return XPC_SJOW_Construct(cx, obj, 1, vp, vp);
