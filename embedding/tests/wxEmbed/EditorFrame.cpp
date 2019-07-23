27fc233347d4007297fe98535b3c4bc67f3ce186
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:43:19 +0000

    Bustage fix, bug 386255 (Trunk XULRunner compile error in nsIEditingSession::MakeWindowEditable). Pending r=sicking.
    UltraBlame original commit: 93137e1bb094303e9c5f5ae95d6742672ed8eaf0

--- embedding/tests/wxEmbed/EditorFrame.cpp
+++ embedding/tests/wxEmbed/EditorFrame.cpp
@@ -84,7 +84,8 @@ void EditorFrame::MakeEditable()
     nsCOMPtr<nsIEditingSession> editingSession = do_GetInterface(mWebBrowser);
     if (!editingSession)
         return;
-    editingSession->MakeWindowEditable(domWindow, NULL, PR_TRUE, PR_FALSE);
+    editingSession->MakeWindowEditable(domWindow, NULL, PR_TRUE, PR_TRUE,
+                                       PR_FALSE);
 }
 
 nsresult EditorFrame::DoCommand(const char *aCommand, nsICommandParams *aCommandParams)
