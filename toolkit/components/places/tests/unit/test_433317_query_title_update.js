9212bdaa0665b80bb13d01ad92b895066eb3fcb8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:54:25 +0000

    pushing test for bug 433317, forgot to check-in initially
    UltraBlame original commit: c57ea0aac0d0ff2a6c2263f4f5a6e9fb6e146888

---
+++ toolkit/components/places/tests/unit/test_433317_query_title_update.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function run_test() {
+  try {
+  var histsvc = Cc["@mozilla.org/browser/nav-history-service;1"].
+                getService(Ci.nsINavHistoryService);
+  var bmsvc = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
+              getService(Ci.nsINavBookmarksService);
+  } catch(ex) {
+    do_throw("Unable to initialize Places services");
+  }
+
+  
+  var queryId = bmsvc.insertBookmark(bmsvc.toolbarFolder, uri("place:"),
+                                     0 , "test query");
+
+  
+  var options = histsvc.getNewQueryOptions();
+  var query = histsvc.getNewQuery();
+  query.setFolders([bmsvc.toolbarFolder], 1);
+  var result = histsvc.executeQuery(query, options);
+  var root = result.root;
+  root.containerOpen = true;
+  var queryNode = root.getChild(0);
+  do_check_eq(queryNode.title, "test query");
+
+  
+  bmsvc.setItemTitle(queryId, "foo");
+
+  
+  do_check_eq(queryNode.title, "foo");
+}
