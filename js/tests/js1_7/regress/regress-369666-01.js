e2caedf42ee2c93a67bbde12164ee718360c421d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:56 +0000

    JavaScript Test - regression test for bug 369666, by Brendan Eich
    UltraBlame original commit: da8686ff4e53e58f90dc5ae445d4c51e215a8436

---
+++ js/tests/js1_7/regress/regress-369666-01.js
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-369666-01.js';
+
+var BUGNUMBER = 369666;
+var summary = 'inner function declaration in let-induced outer ' +
+  'function body gets wrong scope.';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  function foo() {
+    let x = 42
+
+      function bar() {
+      return x;
+    }
+
+    return bar;
+  }
+
+  print(foo()());
+
+  baz = false;
+
+  function foo2() {
+    let x = 42
+
+      function bar() {
+      return x;
+    }
+
+    try {
+      if (baz)
+        return bar;
+    } finally {
+      print('finally', x);
+    }
+    return bar;
+  }
+
+  print(foo2()());
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
