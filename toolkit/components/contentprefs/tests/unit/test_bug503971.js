6d35908b3248b23d29d36f1c27a369db72b8dc4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:34:38 +0000

    Bug 503971 - nsIContentPrefService methods should throw when passed a null setting name; r=myk sr=mconnor
    UltraBlame original commit: 5e61c13575e78bdea54c73661bbd8264e4d83cd2

---
+++ toolkit/components/contentprefs/tests/unit/test_bug503971.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function run_test() {
+  var cps = Cc["@mozilla.org/content-pref/service;1"].
+            getService(Ci.nsIContentPrefService);
+
+  var uri = ContentPrefTest.getURI("http://www.example.com/");
+  
+  do_check_thrown(function () { cps.setPref(uri, null, 8); });
+  do_check_thrown(function () { cps.hasPref(uri, null); });
+  do_check_thrown(function () { cps.getPref(uri, null); });
+  do_check_thrown(function () { cps.removePref(uri, null); });
+  do_check_thrown(function () { cps.getPrefsByName(null); });
+  do_check_thrown(function () { cps.removePrefsByName(null); });
+
+  do_check_thrown(function () { cps.setPref(uri, "", 21); });
+  do_check_thrown(function () { cps.hasPref(uri, ""); });
+  do_check_thrown(function () { cps.getPref(uri, ""); });
+  do_check_thrown(function () { cps.removePref(uri, ""); });
+  do_check_thrown(function () { cps.getPrefsByName(""); });
+  do_check_thrown(function () { cps.removePrefsByName(""); });
+}
+
+function do_check_thrown (aCallback) {
+  var exThrown = false;
+  try {
+    aCallback();
+    do_throw("NS_ERROR_ILLEGAL_VALUE should have been thrown here");
+  } catch (e) {
+    do_check_eq(e.result, Cr.NS_ERROR_ILLEGAL_VALUE);
+    exThrown = true;
+  }
+  do_check_true(exThrown);
+}
