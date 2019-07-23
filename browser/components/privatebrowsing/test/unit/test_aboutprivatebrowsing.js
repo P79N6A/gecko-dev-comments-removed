5d75ea8bada4bb084cfb3d05d5f9dcf3ab6e3181
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:26:30 +0000

    Bug 463400 - Make about:privatebrowsing useful outside the private browsing mode; r=gavin ui-r=beltzner
    UltraBlame original commit: f035bd74e1ca813592c982cd8892dbd05eaa9322

---
+++ browser/components/privatebrowsing/test/unit/test_aboutprivatebrowsing.js
@@ -0,0 +1,88 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function is_about_privatebrowsing_available() {
+  try {
+    var ios = Cc["@mozilla.org/network/io-service;1"].
+              getService(Ci.nsIIOService);
+    var channel = ios.newChannel("about:privatebrowsing", null, null);
+    var input = channel.open();
+    var sinput = Cc["@mozilla.org/scriptableinputstream;1"].
+                 createInstance(Ci.nsIScriptableInputStream);
+    sinput.init(input);
+    while (true)
+      if (!sinput.read(1024).length)
+        break;
+    sinput.close();
+    input.close();
+    return true;
+  } catch (ex if ("result" in ex && ex.result == Cr.NS_ERROR_MALFORMED_URI)) { 
+  }
+
+  return false;
+}
+
+function run_test() {
+  
+  var pb = Cc["@mozilla.org/privatebrowsing;1"].
+           getService(Ci.nsIPrivateBrowsingService);
+  var prefBranch = Cc["@mozilla.org/preferences-service;1"].
+                   getService(Ci.nsIPrefBranch);
+  prefBranch.setBoolPref("browser.privatebrowsing.keep_current_session", true);
+
+  try {
+    
+    do_check_true(is_about_privatebrowsing_available());
+
+    
+    pb.privateBrowsingEnabled = true;
+
+    
+    do_check_true(is_about_privatebrowsing_available());
+
+    
+    pb.privateBrowsingEnabled = false;
+
+    
+    do_check_true(is_about_privatebrowsing_available());
+  } finally {
+    prefBranch.clearUserPref("browser.privatebrowsing.keep_current_session");
+  }
+}
