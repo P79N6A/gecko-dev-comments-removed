a9c7bd5126b21f850159b979ffa09bbe2ab9e254
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:03:58 +0000

    Encapsulate AR structure (r=nnethercote,bug=473769)
    UltraBlame original commit: f3f879abe16b2f6b5d6cd7b39417f5337fd14f85

--- js/src/nanojit/Assembler.h
+++ js/src/nanojit/Assembler.h
@@ -100,12 +100,51 @@ namespace nanojit
     
     
     
-    struct AR
+    class AR
     {
-        LIns*           entry[ NJ_MAX_STACK_ENTRY ];    
-        uint32_t        tos;                            
-        uint32_t        lowwatermark;                   
+    private:
+        uint32_t        _highWaterMark;                 
+        LIns*           _entries[ NJ_MAX_STACK_ENTRY ]; 
+
+
+        bool isEmptyRange(uint32_t start, uint32_t nStackSlots) const;
+        static uint32_t nStackSlotsFor(LIns* ins);
+        
+    public:
+        
+        uint32_t stackSlotsNeeded() const;
+
+        void clear();
+        void freeEntryAt(uint32_t i);
+        uint32_t reserveEntry(LIns* ins); 
+        
+        #ifdef _DEBUG
+        bool isValidEntry(uint32_t idx, LIns* ins) const; 
+        void checkForResourceConsistency(const RegAlloc& regs) const;
+        void checkForResourceLeaks() const;
+        #endif
+        
+        class Iter
+        {
+        private:
+            const AR& _ar;
+            uint32_t _i;
+        public:
+            inline Iter(const AR& ar) : _ar(ar), _i(1) { }
+            bool next(LIns*& ins, uint32_t& nStackSlots, int32_t& offset);             
+        };
     };
+
+    inline  uint32_t AR::nStackSlotsFor(LIns* ins)
+    {
+        return ins->isop(LIR_alloc) ? (ins->size()>>2) : (ins->isQuad() ? 2 : 1);
+    }
+
+    inline uint32_t AR::stackSlotsNeeded() const 
+    { 
+        
+        return _highWaterMark+1; 
+    }
 
 	#ifndef AVMPLUS_ALIGN16
 		#ifdef AVMPLUS_WIN32
@@ -257,8 +296,8 @@ namespace nanojit
             NIns*       genPrologue();
             NIns*       genEpilogue();
 
-            uint32_t    arReserve(LIns* l);
-            void        arFree(uint32_t idx);
+            uint32_t    arReserve(LIns* ins);
+            void        arFreeIfInUse(LIns* ins);
             void        arReset();
 
             Register    registerAlloc(LIns* ins, RegisterMask allow);
--- js/src/nanojit/NativePPC.cpp
+++ js/src/nanojit/NativePPC.cpp
@@ -97,7 +97,7 @@ namespace nanojit
         
 
         
-        uint32_t stackNeeded = max_param_size + linkage_size + _activation.tos * 4;
+        uint32_t stackNeeded = max_param_size + linkage_size + _activation.stackSlotsNeeded() * 4;
         uint32_t aligned = alignUp(stackNeeded, NJ_ALIGN_STACK);
 
         UNLESS_PEDANTIC( if (isS16(aligned)) {
