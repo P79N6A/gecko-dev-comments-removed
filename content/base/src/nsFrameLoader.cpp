199e92d3ce41de8a088fc613ce627c9faf24be40
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:56:58 +0000

    Bug 482659.  Give about:blank documents the base URI of the document that did the load.  r=dcamp, sr=jst
    UltraBlame original commit: 69aa67fe33144eac6314a2a99b9ac6bd7bd60eb3

--- content/base/src/nsFrameLoader.cpp
+++ content/base/src/nsFrameLoader.cpp
@@ -159,7 +159,7 @@ nsFrameLoader::LoadFrame()
   
   if (rv == NS_ERROR_MALFORMED_URI) {
     rv = NS_NewURI(getter_AddRefs(uri), NS_LITERAL_STRING("about:blank"),
-                   charset);
+                   charset, base_uri);
   }
 
   NS_ENSURE_SUCCESS(rv, rv);
--- netwerk/protocol/about/src/nsAboutProtocolHandler.h
+++ netwerk/protocol/about/src/nsAboutProtocolHandler.h
@@ -39,6 +39,7 @@
 #define nsAboutProtocolHandler_h___
 
 #include "nsIProtocolHandler.h"
+#include "nsSimpleNestedURI.h"
 
 class nsCString;
 class nsIAboutModule;
@@ -71,5 +72,37 @@ private:
     ~nsSafeAboutProtocolHandler() {}
 };
 
+
+
+class nsNestedAboutURI : public nsSimpleNestedURI {
+public:
+    nsNestedAboutURI(nsIURI* aInnerURI, nsIURI* aBaseURI)
+        : nsSimpleNestedURI(aInnerURI)
+        , mBaseURI(aBaseURI)
+    {}
+
+    
+    nsNestedAboutURI() : nsSimpleNestedURI() {}
+
+    virtual ~nsNestedAboutURI() {}
+
+    
+    NS_IMETHOD QueryInterface(REFNSIID aIID, void** aInstancePtr);
+
+    
+    
+    
+    virtual nsSimpleURI* StartClone();
+    NS_IMETHOD Read(nsIObjectInputStream* aStream);
+    NS_IMETHOD Write(nsIObjectOutputStream* aStream);
+    NS_IMETHOD GetClassIDNoAlloc(nsCID *aClassIDNoAlloc);
+
+    nsIURI* GetBaseURI() const {
+        return mBaseURI;
+    }
+
+protected:
+    nsCOMPtr<nsIURI> mBaseURI;
+};
 
 #endif 
