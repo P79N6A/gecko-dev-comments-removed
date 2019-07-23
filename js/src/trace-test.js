2f54b9298edb4a74097f2253a7d6ba63caee288b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:51:05 +0000

    Properly handle side exits in scripted constructors (originally mrbkap, r=brendan, 453462).
    UltraBlame original commit: 3090e1560262ed47cb332fa302ea58153e6f07e0

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -1348,6 +1348,44 @@ function testPrimitiveConstructorPrototype() {
 testPrimitiveConstructorPrototype.expected = "ok";
 test(testPrimitiveConstructorPrototype);
 
+function testSideExitInConstructor() {
+    var FCKConfig = {};
+    FCKConfig.CoreStyles =
+	{
+	    'Bold': { },
+	    'Italic': { },
+	    'FontFace': { },
+	    'Size' :
+	    {
+		Overrides: [ ]
+	    },
+
+	    'Color' :
+	    {
+		Element: '',
+		Styles: {  },
+		Overrides: [  ]
+	    },
+	    'BackColor': {
+		Element : '',
+		Styles : { 'background-color' : '' }
+	    },
+	    
+	};
+    var FCKStyle = function(A) {
+	A.Element;
+    };
+    
+    var pass = true;
+    for (var s in FCKConfig.CoreStyles) {
+	var x = new FCKStyle(FCKConfig.CoreStyles[s]);
+	if (!x) pass = false;
+    }
+    return pass;
+}
+testSideExitInConstructor.expected = true;
+test(testSideExitInConstructor);
+
 
 print("\npassed:", passes.length && passes.join(","));
 print("\nFAILED:", fails.length && fails.join(","));
