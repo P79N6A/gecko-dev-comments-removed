5f5aff1f2ec4e424fc9696e463a99fe87a784d87
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:48:21 +0000

    Backing out previous change, didn't notice the orange, sorry:(
    UltraBlame original commit: c3bb0f904ecaa11cb4a4f39a6183bb9c948da6a2

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -1976,9 +1976,6 @@ nsresult nsExternalAppHandler::OpenWithApplication()
   
   if (mStopRequestIssued)
   {
-    
-    mFinalFileDestination->SetPermissions(0400);
-
     rv = mMimeInfo->LaunchWithFile(mFinalFileDestination);        
     if (NS_FAILED(rv))
     {
