7b0f105104b5b4f6b20b4fcfb96ba896c1a6de4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:52:34 +0000

    Fixing bustage from landing of bug 397510.
    UltraBlame original commit: 1e88eea26359b6d4da9614bdfb647b86b90894d1

--- layout/generic/nsTextFrameThebes.cpp
+++ layout/generic/nsTextFrameThebes.cpp
@@ -602,6 +602,9 @@ public:
     mMaxTextLength = 0;
     mDoubleByteText = PR_FALSE;
   }
+  void ResetLineBreaker() {
+    mLineBreaker.Reset();
+  }
   void AccumulateRunInfo(nsTextFrame* aFrame);
   void BuildTextRunForFrames(void* aTextBuffer);
   void AssignTextRun(gfxTextRun* aTextRun);
@@ -963,7 +966,7 @@ BuildTextRuns(gfxContext* aContext, nsTextFrame* aForFrame,
         
         
         
-        mLineBreaker.Reset();
+        scanner.ResetLineBreaker();
         return;
       }
     }
