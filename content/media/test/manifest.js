e857d55f4813878c45a3b14fc085ba78a15a3e5c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:25:00 +0000

    Bug 502211. Make test_media_selection.html and test_load.html backend-independent. r=doublec
    UltraBlame original commit: 985f04d6dd4eff98477ab9a0b02d66553b6b5a7f

---
+++ content/media/test/manifest.js
@@ -0,0 +1,6 @@
+
+var gSmallTests = [
+  { name:"r11025_s16_c1.wav", type:"audio/x-wav", duration:1.0 },
+  { name:"320x240.ogv", type:"video/ogg", width:320, height:240 },
+  { name:"bogus.duh", type:"bogus/duh" }
+];
