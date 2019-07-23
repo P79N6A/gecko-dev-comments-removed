4d02597ec4cb978a406091868d9c52711f94c59a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:30:14 +0000

    Whitespace-only: Fix parameter indentation in nsTableCellFame.cpp
    UltraBlame original commit: f38ff6f08594a3a2f80257f345a2c000c673839f

--- layout/tables/nsTableCellFrame.cpp
+++ layout/tables/nsTableCellFrame.cpp
@@ -672,7 +672,7 @@ nsTableCellFrame::HasVerticalAlignBaseline()
 PRBool 
 nsTableCellFrame::CellHasVisibleContent(nscoord       height,
                                         nsTableFrame* tableFrame,
-                                        nsIFrame* kidFrame)
+                                        nsIFrame*     kidFrame)
 {
   
   if (height > 0)
@@ -835,7 +835,7 @@ CalcUnpaginagedHeight(nsPresContext*       aPresContext,
   return computedHeight;
 }
 
-NS_METHOD nsTableCellFrame::Reflow(nsPresContext*          aPresContext,
+NS_METHOD nsTableCellFrame::Reflow(nsPresContext*           aPresContext,
                                    nsHTMLReflowMetrics&     aDesiredSize,
                                    const nsHTMLReflowState& aReflowState,
                                    nsReflowStatus&          aStatus)
