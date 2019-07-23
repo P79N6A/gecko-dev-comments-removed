f1a3252431bb1c8f59c3cb1fdf0b514183f3b4d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:11:06 +0000

    Test for Bug 425563 - Clicked links not colored as visited, or visited color forgotten after hard refresh (for mak77@supereva.it, r=dietrich)
    UltraBlame original commit: 45f02283949892b66634d821e461785a431903a8

---
+++ toolkit/components/places/tests/unit/test_425563.js
@@ -0,0 +1,107 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+try {
+  var histsvc = Cc["@mozilla.org/browser/nav-history-service;1"].
+                getService(Ci.nsINavHistoryService);
+  var bhist = histsvc.QueryInterface(Ci.nsIBrowserHistory);
+} catch(ex) {
+  do_throw("Could not get history services\n");
+}
+
+
+function add_visit(aURI, aType) {
+  var placeID = histsvc.addVisit(uri(aURI),
+                                 Date.now() * 1000,
+                                 null, 
+                                 aType,
+                                 false, 
+                                 0);
+  do_check_true(placeID > 0);
+  return placeID;
+}
+
+
+function run_test() {
+  var count_visited_URIs = ["http://www.test-link.com/",
+                            "http://www.test-typed.com/",
+                            "http://www.test-bookmark.com/",
+                            "http://www.test-redirect-permanent.com/",
+                            "http://www.test-redirect-temporary.com/"];
+
+  var notcount_visited_URIs = ["http://www.test-embed.com/",
+                               "http://www.test-download.com/"];
+
+  
+  add_visit("http://www.test-link.com/", histsvc.TRANSITION_LINK);
+  add_visit("http://www.test-typed.com/", histsvc.TRANSITION_TYPED);
+  add_visit("http://www.test-bookmark.com/", histsvc.TRANSITION_BOOKMARK);
+  add_visit("http://www.test-embed.com/", histsvc.TRANSITION_EMBED);
+  add_visit("http://www.test-redirect-permanent.com/", histsvc.TRANSITION_REDIRECT_PERMANENT);
+  add_visit("http://www.test-redirect-temporary.com/", histsvc.TRANSITION_REDIRECT_TEMPORARY);
+  add_visit("http://www.test-download.com/", histsvc.TRANSITION_DOWNLOAD);
+
+  
+  for each(var visited_uri in count_visited_URIs)
+    do_check_eq(bhist.isVisited(uri(visited_uri)), true);
+  for each(var visited_uri in notcount_visited_URIs)
+    do_check_eq(bhist.isVisited(uri(visited_uri)), true);
+
+  
+  
+  var options = histsvc.getNewQueryOptions();
+  options.sortingMode = options.SORT_BY_VISITCOUNT_DESCENDING;
+  options.resultType = options.RESULTS_AS_VISIT;
+  options.includeHidden = true;
+  var query = histsvc.getNewQuery();
+  query.minVisits = 1;
+  var result = histsvc.executeQuery(query, options);
+  var root = result.root;
+
+  root.containerOpen = true;
+  var cc = root.childCount;
+  do_check_eq(cc, count_visited_URIs.length);
+
+  for (var i = 0; i < cc; i++) {
+    var node = root.getChild(i);
+    do_check_neq(count_visited_URIs.indexOf(node.uri), -1);
+  }
+  root.containerOpen = false;
+}
