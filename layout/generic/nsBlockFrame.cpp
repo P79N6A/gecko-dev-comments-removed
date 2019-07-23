8497126fab1725f2494b768c216ef5942f55e5d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:17:06 +0000

    Bug 462968. Sanitize block height. r+sr=dbaron
    UltraBlame original commit: 6d6a178f9132fbb6aa17b0ad9012b28760bf6b12

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -1425,6 +1425,9 @@ nsBlockFrame::ComputeFinalSize(const nsHTMLReflowState& aReflowState,
     NS_FRAME_SET_OVERFLOW_INCOMPLETE(aState.mReflowStatus);
   }
 
+  
+  aMetrics.height = PR_MAX(0, aMetrics.height);
+
 #ifdef DEBUG_blocks
   if (CRAZY_WIDTH(aMetrics.width) || CRAZY_HEIGHT(aMetrics.height)) {
     ListTag(stdout);
