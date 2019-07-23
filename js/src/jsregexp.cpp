7d607610088f0c459e8a4e4565928688dbc59b09
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:23:58 +0000

    Bug 500621: make regexp lirbuf setup work the same as tracing lirbuf to avoid mem leak, r=dvander
    UltraBlame original commit: 277600eefe7b529bf5d2c682db4d9e0fd7508b50

--- js/src/jsregexp.cpp
+++ js/src/jsregexp.cpp
@@ -2402,12 +2402,6 @@ class RegExpNativeCompiler {
         
 #ifdef NJ_VERBOSE
         debug_only_stmt(
-        fragment->lirbuf->names = new (&gc) LirNameMap(&gc, fragmento->labels);
-        )
-#endif
-        
-#ifdef NJ_VERBOSE
-        debug_only_stmt(
             if (js_LogController.lcbits & LC_TMRegexp) {
                 lir = new (&gc) VerboseWriter(&gc, lir, lirbuf->names,
                                               &js_LogController);
