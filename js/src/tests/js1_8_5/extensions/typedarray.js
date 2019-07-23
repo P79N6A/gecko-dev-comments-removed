3c345e03d2299d1a908a9385c88db89ae2e56ef8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:17:29 +0000

    Backed out changeset b4268cb7bf48
    UltraBlame original commit: e37e71d297f4429e81482c871b08a8b010c5d175

--- js/src/tests/js1_8_5/extensions/typedarray.js
+++ js/src/tests/js1_8_5/extensions/typedarray.js
@@ -194,21 +194,6 @@ function test()
     check(function() a[2] == 0);
     check(function() a[3] == 0);
 
-    
-    var x = Array(3);
-    x[0] = "hello";
-    x[1] = { };
-
-    a = new Uint8Array(x);
-    check(function() a[0] == 0);
-    check(function() a[1] == 0);
-    check(function() a[2] == 0);
-
-    a = new Float32Array(x);
-    check(function() a[0] == NaN, TODO); 
-    check(function() a[1] == NaN);
-    check(function() a[2] == NaN);
-
     print ("done");
 
     reportCompare(0, TestFailCount, "typed array tests");
