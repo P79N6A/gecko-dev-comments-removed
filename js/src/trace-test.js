901f327224aa7308a5701bdf46d458e45b6de710
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:09:35 +0000

    Added multitrees test to trace-tests.js for bug 454315.
    UltraBlame original commit: 10ea4db73d63d12480170a51b453dbb03964dc54

--- js/src/trace-test.js
+++ js/src/trace-test.js
@@ -1779,6 +1779,20 @@ function testDestructuring() {
 testDestructuring.expected = (HOTLOOP + 1) * 3;
 test(testDestructuring);
 
+function loopWithUndefined1(t, val) {
+    var a = new Array(6);
+    for (var i = 0; i < 6; i++)
+        a[i] = (t > val);
+    return a;
+}
+loopWithUndefined1(5.0, 2);     
+
+function testLoopWithUndefined1() {
+    return loopWithUndefined1(5.0).join(",");  
+};
+testLoopWithUndefined1.expected = "false,false,false,false,false,false";
+test(testLoopWithUndefined1);
+
 
 
 
