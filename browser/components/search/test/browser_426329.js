8a5a566f0e6fbe0a16ccf164004bb1e1c6cafcdb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:28:24 +0000

    Bug 554400, convert search.xml to use new dropped link handler, r=gavin
    UltraBlame original commit: d9c5998f3e4149cc54b515d65b2ae7a6cce6e3df

--- browser/components/search/test/browser_426329.js
+++ browser/components/search/test/browser_426329.js
@@ -125,12 +125,44 @@ function test() {
       isnot(event.originalTarget, gBrowser.contentDocument,
             "Shift+MiddleClick loaded results in background tab");
 
-      testRightClick();
+      testDropText();
+     });
+   }
+ 
+  
+  function stopPopup(event) { event.preventDefault(); }
+
+  function testDropText() {
+    init();
+    searchBar.addEventListener("popupshowing", stopPopup, true);
+    
+    
+    EventUtils.synthesizeDrop(searchBar.searchButton, [[ {type: "text/plain", data: "Some Text" } ]], "copy", window);
+    doOnloadOnce(function(event) {
+      is(searchBar.value, "Some Text", "drop text/plain on searchbar");
+      testDropInternalText();
     });
   }
 
+  function testDropInternalText() {
+    init();
+    EventUtils.synthesizeDrop(searchBar.searchButton, [[ {type: "text/x-moz-text-internal", data: "More Text" } ]], "copy", window);
+    doOnloadOnce(function(event) {
+      is(searchBar.value, "More Text", "drop text/x-moz-text-internal on searchbar");
+      testDropLink();
+    });
+  }
+
+  function testDropLink() {
+    init();
+    EventUtils.synthesizeDrop(searchBar.searchButton, [[ {type: "text/uri-list", data: "http://www.mozilla.org" } ]], "copy", window);
+    is(searchBar.value, "More Text", "drop text/uri-list on searchbar");
+    SimpleTest.executeSoon(testRightClick);
+  }
+  
   function testRightClick() {
     init();
+    searchBar.removeEventListener("popupshowing", stopPopup, true);
     content.location.href = "about:blank";
     simulateClick({ button: 2 }, searchButton);
     setTimeout(function() {
