2da771ac3913ac319fcfa9a67e10b10d9a0d8c7c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:30:24 +0000

    Adde modules which not requires implementation
    UltraBlame original commit: a802faf6bce93eb96b2ae3d479528c36f57fbb56

--- widget/src/qt/nsWidgetFactory.cpp
+++ widget/src/qt/nsWidgetFactory.cpp
@@ -47,7 +47,8 @@
 #include "nsAppShell.h"
 #include "nsWindow.h"
 #include "nsToolkit.h"
-
+#include "nsHTMLFormatConverter.h"
+#include "nsTransferable.h"
 
 
 
@@ -78,6 +79,8 @@ NS_GENERIC_FACTORY_CONSTRUCTOR(nsWindow)
 NS_GENERIC_FACTORY_CONSTRUCTOR(ChildWindow)
 NS_GENERIC_FACTORY_CONSTRUCTOR(PopupWindow)
 NS_GENERIC_FACTORY_CONSTRUCTOR(nsToolkit)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsHTMLFormatConverter)
+NS_GENERIC_FACTORY_CONSTRUCTOR(nsTransferable)
 
 
 
@@ -109,18 +112,18 @@ static const nsModuleComponentInfo components[] =
       NS_POPUP_CID,
       "@mozilla.org/widgets/popup_window/qt;1",
       PopupWindowConstructor },
-
-
-
-
-
-
-
-
-
-
-
-
+    { "HTML Format Converter",
+      NS_HTMLFORMATCONVERTER_CID,
+      "@mozilla.org/widget/htmlformatconverter/qt;1",
+      nsHTMLFormatConverterConstructor },
+    { "Qt Toolkit",
+      NS_TOOLKIT_CID,
+      "@mozilla.org/widget/toolkit/qt;1",
+      nsToolkitConstructor },
+    { "Transferrable",
+      NS_TRANSFERABLE_CID,
+      "@mozilla.org/widget/transferable;1",
+      nsTransferableConstructor },
 
 
 
