602dd562463f06e8edb2090d7e6781bee6f707df
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:00:26 +0000

    Remove additional chunk of code that should have been removed in bug 238072.  (Bug 460029)  r+sr=bzbarsky
    UltraBlame original commit: 30df17ed7d7f35f6cfa30cf4f0c077de93873196

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -3170,13 +3170,6 @@ nsRuleNode::ComputeDisplayData(void* aStartStruct,
 {
   COMPUTE_START_RESET(Display, (), display, parentDisplay,
                       Display, displayData)
-  nsIAtom* pseudoTag = aContext->GetPseudoType();
-  PRBool generatedContent = (pseudoTag == nsCSSPseudoElements::before || 
-                             pseudoTag == nsCSSPseudoElements::after);
-  NS_ASSERTION(!generatedContent || parentContext,
-               "Must have parent context for generated content");
-  if (parentDisplay == display && generatedContent)
-    parentDisplay = parentContext->GetStyleDisplay();
 
   
   SetFactor(displayData.mOpacity, display->mOpacity, inherited,
@@ -3374,7 +3367,7 @@ nsRuleNode::ComputeDisplayData(void* aStartStruct,
     
     
 
-    if (nsCSSPseudoElements::firstLetter == pseudoTag) {
+    if (nsCSSPseudoElements::firstLetter == aContext->GetPseudoType()) {
       
       
       display->mDisplay = NS_STYLE_DISPLAY_INLINE;
