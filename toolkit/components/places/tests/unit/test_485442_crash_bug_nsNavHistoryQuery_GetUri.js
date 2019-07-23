af1b9fbb113c957fc533f2a969e1b120bbe2e027
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:08:47 +0000

    Bug 485442 - crash bug calling nsINavHistoryQuery.uri, r=mak,sdwilsh
    UltraBlame original commit: 49ba14fac398e555a39756d0c9e8b6ca1550dfa1

---
+++ toolkit/components/places/tests/unit/test_485442_crash_bug_nsNavHistoryQuery_GetUri.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var hs = Cc["@mozilla.org/browser/nav-history-service;1"].
+         getService(Ci.nsINavHistoryService);
+
+function run_test() {
+  var query = hs.getNewQuery();
+  var options = hs.getNewQueryOptions();
+  options.resultType = options.RESULT_TYPE_QUERY;
+  var result = hs.executeQuery(query, options);
+  result.root.containerOpen = true;
+  var rootNode = result.root;
+  rootNode.QueryInterface(Ci.nsINavHistoryQueryResultNode);
+  var outObj = {};
+  var queries = rootNode.getQueries(outObj);
+  do_check_eq(queries[0].uri, null); 
+  rootNode.containerOpen = false;
+}
