1e570c32b1d67a1669488816c27d357f49d81867
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:35:21 +0000

    bug 469927 - JavaScript Test by Jesse Ruderman.
    UltraBlame original commit: e281e5f0266ed7d0b29fffdf3c1a76ef2ae80ea6

---
+++ js/tests/js1_8_1/trace/regress-469927.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-469927.js';
+
+var BUGNUMBER = 469927;
+var summary = 'TM: jit should not slow down short loop with let';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  function letitbe() {
+    var start = new Date();
+    for (let i = 0; i < 500000; ++i) { 
+      for (let j = 0; j < 4; ++j) { } 
+    }
+    var stop = new Date();
+    return stop - start;
+  }
+
+  jit(false);
+  var timenonjit = letitbe();
+  jit(true);
+  var timejit = letitbe();
+  jit(false);
+
+  print('time: nonjit = ' + timenonjit + ', jit = ' + timejit);
+
+  expect = true;
+  actual = timejit < timenonjit;
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
