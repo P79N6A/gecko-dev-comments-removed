56892ff712e680a5c293b039fd03b0cbe1f21103
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:03:36 +0000

    Implement events part of CSS transitions: add support for the transitionend DOM event.  (Bug 531585)  r=smaug
    UltraBlame original commit: 3647e6e07a59419de0ba513e14e312363007c49f

---
+++ content/events/src/nsDOMTransitionEvent.h
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsDOMTransitionEvent_h_
+#define nsDOMTransitionEvent_h_
+
+#include "nsDOMEvent.h"
+#include "nsIDOMTransitionEvent.h"
+#include "nsString.h"
+
+class nsTransitionEvent;
+
+class nsDOMTransitionEvent : public nsDOMEvent,
+                             public nsIDOMTransitionEvent
+{
+public:
+  nsDOMTransitionEvent(nsPresContext *aPresContext,
+                       nsTransitionEvent *aEvent);
+  ~nsDOMTransitionEvent();
+
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_FORWARD_TO_NSDOMEVENT
+  NS_DECL_NSIDOMTRANSITIONEVENT
+
+private:
+  nsTransitionEvent* TransitionEvent() {
+    NS_ABORT_IF_FALSE(mEvent->eventStructType == NS_TRANSITION_EVENT,
+                      "unexpected struct type");
+    return static_cast<nsTransitionEvent*>(mEvent);
+  }
+};
+
+#endif 
