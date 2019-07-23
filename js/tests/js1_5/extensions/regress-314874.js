4e48b49fe879ac9560b0985dee8f6e83b5afd3bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:38:31 +0000

    JavaScript Test - regression test for bug 314874, by HE Shi-Jun
    UltraBlame original commit: 8815aab3c25f9f617e59290e1ba58a236ef122ab

---
+++ js/tests/js1_5/extensions/regress-314874.js
@@ -0,0 +1,71 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 314874;
+var summary = 'Function.call/apply with non-primitive argument';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  var thisArg = { valueOf: function() { return {a: 'a', b: 'b'}; } };
+
+  var f = function () { return (this); };
+
+  expect  = f.call(thisArg);
+
+  thisArg.f = f;
+
+  actual = thisArg.f();
+
+  delete thisArg.f;
+
+  expect = expect.toSource();
+  actual = actual.toSource();
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
