e9d5b244a8a075aba7af0dd2b388cedb0bc699e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:54:51 +0000

    Bug 426932: update online/offline state on osx.  p=dcamp/smichaud, r=smichaud/josh, sr=roc
    UltraBlame original commit: 3ea2c46a98145cccf8125d1f78f5cb059219e9cb

---
+++ netwerk/system/mac/nsNetworkLinkService.h
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef NSNETWORKLINKSERVICEMAC_H_
+#define NSNETWORKLINKSERVICEMAC_H_
+
+#include "nsINetworkLinkService.h"
+#include "nsIObserver.h"
+
+#include <SystemConfiguration/SCNetworkReachability.h>
+
+class nsNetworkLinkService : public nsINetworkLinkService,
+                             public nsIObserver
+{
+public:
+    NS_DECL_ISUPPORTS
+    NS_DECL_NSINETWORKLINKSERVICE
+    NS_DECL_NSIOBSERVER
+
+    nsNetworkLinkService();
+    virtual ~nsNetworkLinkService();
+
+    nsresult Init();
+    nsresult Shutdown();
+
+private:
+    PRPackedBool mLinkUp;
+    PRPackedBool mStatusKnown;
+
+    SCNetworkReachabilityRef mReachability;
+    CFRunLoopRef mCFRunLoop;
+
+    void UpdateReachability();
+    void SendEvent();
+    static void ReachabilityChanged(SCNetworkReachabilityRef target,
+                                    SCNetworkConnectionFlags flags,
+                                    void *info);
+};
+
+#endif 
