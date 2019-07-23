9371f8e2ab25a9814037164717e8a8af9b042fe4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:44:15 +0000

    Bug 387358. Don't go into an infinite loop searching for end-of-first-letter-cluster; also translate from DOM to textrun offsets. r=smontagu
    UltraBlame original commit: 4cf5fd22ef67d03cbe2884db250f3d47957db2fd

--- layout/generic/nsTextFrameThebes.cpp
+++ layout/generic/nsTextFrameThebes.cpp
@@ -4864,7 +4864,8 @@ nsTextFrame::GetOffsets(PRInt32 &start, PRInt32 &end) const
 static PRBool
 FindFirstLetterRange(const nsTextFragment* aFrag,
                      gfxTextRun* aTextRun,
-                     PRInt32 aOffset, PRInt32* aLength)
+                     PRInt32 aOffset, const gfxSkipCharsIterator& aIter,
+                     PRInt32* aLength)
 {
   
   PRInt32 i;
@@ -4879,11 +4880,15 @@ FindFirstLetterRange(const nsTextFragment* aFrag,
     return PR_FALSE;
 
   
-  while (i + 1 < length) {
-    if (aTextRun->IsClusterStart(aOffset + i + 1))
+  gfxSkipCharsIterator iter(aIter);
+  PRInt32 nextClusterStart;
+  for (nextClusterStart = i + 1; nextClusterStart < length; ++nextClusterStart) {
+    iter.SetOriginalOffset(nextClusterStart);
+    if (iter.IsOriginalCharSkipped() ||
+        aTextRun->IsClusterStart(iter.GetSkippedOffset()))
       break;
   }
-  *aLength = i + 1;
+  *aLength = nextClusterStart;
   return PR_TRUE;
 }
 
@@ -5303,7 +5308,7 @@ nsTextFrame::Reflow(nsPresContext*           aPresContext,
   PRBool completedFirstLetter = PR_FALSE;
   if (lineLayout.GetFirstLetterStyleOK()) {
     AddStateBits(TEXT_FIRST_LETTER);
-    completedFirstLetter = FindFirstLetterRange(frag, mTextRun, offset, &length);
+    completedFirstLetter = FindFirstLetterRange(frag, mTextRun, offset, iter, &length);
   }
 
   
