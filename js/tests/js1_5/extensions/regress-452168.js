861ecc295d651a4888b1edff6505d1fcc7f9625d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:49:56 +0000

    JavaScript Tests - regression test for bug 452168, by Jesse Ruderman
    UltraBlame original commit: a89c6b449c61bd648ed2306cc0850ee9209ac69f

---
+++ js/tests/js1_5/extensions/regress-452168.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-452168.js';
+
+var BUGNUMBER = 452168;
+var summary = 'Do not crash with gczeal 2, JIT: @ avmplus::List or @ nanojit::LirBuffer::validate';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  jit(true);
+  gczeal(2);
+
+  var a, b; gczeal(2); (function() { for (var p in this) { } })();
+
+  gczeal(0);
+  jit(false);
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
