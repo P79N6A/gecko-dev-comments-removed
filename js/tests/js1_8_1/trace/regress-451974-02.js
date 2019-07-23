c0ea04a3b79a8f4381cfd498ef994b658f9727d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:36:45 +0000

    bug 451974 - JavaScript Tests by Andreas Gal.
    UltraBlame original commit: 72511e23379018a9446ace4d4e28ceb5f72d5afd

---
+++ js/tests/js1_8_1/trace/regress-451974-02.js
@@ -0,0 +1,98 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-451974-02.js';
+
+var BUGNUMBER = 451974;
+var summary = 'TM: loops with anon functions should not be slower with jit enabled';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  var chars = '0123456789abcdef';
+  var size = 1000;
+  var mult = 100;
+  var densearray = [];
+  var lsize = size;
+
+  while (lsize--) 
+  { 
+    densearray.push(chars); 
+  }
+
+  function loop()
+  {
+    var start = new Date();
+
+    for (var a = 0; a < mult; a++) 
+    {
+      var f = (function(x){}); 
+      for (var i = 0, len = densearray.length; i < len; i++) 
+      { 
+        f(densearray[i]); 
+      }
+    }
+
+    var stop = new Date();
+    return stop - start;
+  }
+
+  jit(false);
+  var timenonjit = loop();
+  jit(true);
+  var timejit = loop();
+  jit(false);
+
+  print('time: nonjit = ' + timenonjit + ', jit = ' + timejit);
+
+  expect = true;
+  actual = timejit < timenonjit;
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
