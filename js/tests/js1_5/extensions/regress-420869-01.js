ee4f8e98a6b57c3e8244c512d9e6973e35fb3501
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:11:27 +0000

    JavaScript Tests - regression test for bug 420869, by Igor Bukanov
    UltraBlame original commit: 1a2b70310188a90b4c85ba87c54e91db5a015e88

---
+++ js/tests/js1_5/extensions/regress-420869-01.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-420869-01.js';
+
+var BUGNUMBER = 424683;
+var summary = 'Throw too much recursion instead of script stack space quota';
+var actual = 'No Error';
+var expect = 'InternalError: too much recursion';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  function f(i) {
+    if (i == 0) 
+      return 1; 
+    return i*f(i-1);
+  }
+
+  try
+  {
+    f();
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
