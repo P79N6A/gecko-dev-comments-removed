dcbd5c8eef4633fd4e09a06e29414cb7b324dc3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:21:58 +0000

    Bug 465510 - random failures on test_database_sync_after_addVisit.js, r=sdwilsh
    UltraBlame original commit: b6aec13be588f062ca6accbf68aef52c8cd896a6

--- toolkit/components/places/tests/sync/test_database_sync_after_addVisit.js
+++ toolkit/components/places/tests/sync/test_database_sync_after_addVisit.js
@@ -45,11 +45,43 @@ var bs = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
 var prefs = Cc["@mozilla.org/preferences-service;1"].
             getService(Ci.nsIPrefService).
             getBranch("places.");
+var os = Cc["@mozilla.org/observer-service;1"].
+         getService(Ci.nsIObserverService);
 
 const TEST_URI = "http://test.com/";
 
 const kSyncPrefName = "syncDBTableIntervalInSecs";
 const SYNC_INTERVAL = 1;
+const kSyncFinished = "places-sync-finished";
+
+var observer = {
+  visitId: -1,
+  observe: function(aSubject, aTopic, aData) {
+    if (aTopic == kSyncFinished && this.visitId != -1) {
+      
+      
+      do_check_eq(this.visitId, visitId);
+      
+      os.removeObserver(this, kSyncFinished);
+      
+      new_test_visit_uri_event(this.visitId, TEST_URI, true, true);
+    }
+  }
+}
+os.addObserver(observer, kSyncFinished, false);
+
+
+var historyObserver = {
+  onVisit: function(aURI, aVisitId, aTime, aSessionId, aReferringId,
+                    aTransitionType, aAdded) {
+    observer.visitId = aVisitId;
+    hs.removeObserver(this, false);
+  }
+}
+hs.addObserver(historyObserver, false);
+
+
+var visitId = -1;
 
 function run_test()
 {
@@ -57,17 +89,8 @@ function run_test()
   prefs.setIntPref(kSyncPrefName, SYNC_INTERVAL);
 
   
-  let id = hs.addVisit(uri(TEST_URI), Date.now() * 1000, null,
-                       hs.TRANSITION_TYPED, false, 0);
+  visitId = hs.addVisit(uri(TEST_URI), Date.now() * 1000, null,
+                        hs.TRANSITION_TYPED, false, 0);
 
-  
-  
-  let timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
-  timer.initWithCallback({
-    notify: function(aTimer)
-    {
-      new_test_visit_uri_event(id, TEST_URI, true, true);
-    }
-  }, (SYNC_INTERVAL * 1000) * 2, Ci.nsITimer.TYPE_ONE_SHOT);
   do_test_pending();
 }
