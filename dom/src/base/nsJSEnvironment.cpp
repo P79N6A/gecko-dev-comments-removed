9cf4819c7498c4d9bccf2d67ecc8b843a32944fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:58:09 +0000

    Bug 440595.  Report the right line number in the onerror handler even if not reporting the whole error.  r=jonas, sr=jst
    UltraBlame original commit: 3762e6244e6398f7dd5dd222badac7512cbb1815

--- dom/src/base/nsJSEnvironment.cpp
+++ dom/src/base/nsJSEnvironment.cpp
@@ -487,18 +487,12 @@ NS_ScriptErrorReporter(JSContext *cx,
               nsCOMPtr<nsIURI> errorURI;
               NS_NewURI(getter_AddRefs(errorURI), report->filename);
 
-              nsCOMPtr<nsIURI> codebase;
-              p->GetURI(getter_AddRefs(codebase));
-
-              if (errorURI && codebase) {
+              if (errorURI) {
                 
                 
                 
                 
-                sameOrigin =
-                  NS_SUCCEEDED(sSecurityManager->
-                               CheckSameOriginURI(errorURI, codebase,
-                                                  PR_FALSE));
+                sameOrigin = NS_SUCCEEDED(p->CheckMayLoad(errorURI, PR_FALSE));
               }
             }
 
