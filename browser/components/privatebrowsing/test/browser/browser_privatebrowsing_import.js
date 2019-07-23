794f23bd2d583f7fd05efc5ec78f9fc8cca96120
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:46:07 +0000

    Bug 464736 - Cannot import History and Cookies from Safari while in Private Browsing mode; r=gavin
    UltraBlame original commit: 44e28804fd03b0a144c6f672bffeb392691a9958

---
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_import.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  let prefBranch = Cc["@mozilla.org/preferences-service;1"].
+                   getService(Ci.nsIPrefBranch);
+  prefBranch.setBoolPref("browser.privatebrowsing.keep_current_session", true);
+  let pb = Cc["@mozilla.org/privatebrowsing;1"].
+           getService(Ci.nsIPrivateBrowsingService);
+
+  let importMenuItem = document.getElementById("menu_import");
+
+  
+  ok(!importMenuItem.hasAttribute("disabled"),
+    "File->Import menu item should not be disabled outside of the private browsing mode");
+
+  
+  pb.privateBrowsingEnabled = true;
+
+  ok(importMenuItem.hasAttribute("disabled"),
+    "File->Import menu item should be disabled inside of the private browsing mode");
+
+  
+  pb.privateBrowsingEnabled = false;
+
+  ok(!importMenuItem.hasAttribute("disabled"),
+    "File->Import menu item should not be disabled after leaving the private browsing mode");
+
+  
+  prefBranch.clearUserPref("browser.privatebrowsing.keep_current_session");
+}
