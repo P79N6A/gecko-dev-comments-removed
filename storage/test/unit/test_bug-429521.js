d5106ad4bb2b053d16efa759f27584ce729b15c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:27:20 +0000

    Avoid returning false without setting an exception. bug 429521, r=sdwilsh
    UltraBlame original commit: f01bfd3595951b5e9e4a607ab56a71621e3e3cf0

---
+++ storage/test/unit/test_bug-429521.js
@@ -0,0 +1,92 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function createStatementWrapper(aSQL) 
+{
+    var stmt = getOpenedDatabase().createStatement(aSQL);
+    var wrapper = Components.Constructor("@mozilla.org/storage/statement-wrapper;1", Ci.mozIStorageStatementWrapper)();
+    wrapper.initialize(stmt);
+    return wrapper;
+}
+
+function setup() 
+{
+    getOpenedDatabase().createTable("t1", "x TEXT");
+
+    var stmt = getOpenedDatabase().createStatement("INSERT INTO t1 (x) VALUES ('/mozilla.org/20070129_1/Europe/Berlin')");
+    stmt.execute();
+    stmt.finalize();
+}
+
+function test_bug429521() 
+{
+    var wrapper = createStatementWrapper(
+        "SELECT DISTINCT(zone) FROM ("+
+            "SELECT x AS zone FROM t1 WHERE x LIKE '/mozilla.org%'" +
+        ");");
+
+    print("*** test_bug429521: started");
+
+    try {
+        while (wrapper.step()) {
+            print("*** test_bug429521: step() Read wrapper.row.zone");
+
+            
+            
+            var tzId = wrapper.row.zone;
+
+            print("*** test_bug429521: step() Read wrapper.row.zone finished");
+        }
+    } catch (e) {
+        print("*** test_bug429521: " + e);
+    }
+
+    print("*** test_bug429521: finished");
+
+    wrapper.statement.finalize();
+}
+
+function run_test()
+{
+  setup();
+
+  test_bug429521();
+    
+  cleanup();
+}
