bf975ce6db663952af2bcf4ba59b5fb8486fa8a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:00:49 +0000

    Bug 412610 - "MAXPATHLEN too small for glibc's realpath()" (define MAXPATHLEN as PATH_MAX when PATH_MAX is available) [r=bsmedberg]
    UltraBlame original commit: adf33dbb59d04461121d236451dc8b5973b96fab

--- xpcom/build/nsXPCOMPrivate.h
+++ xpcom/build/nsXPCOMPrivate.h
@@ -231,6 +231,7 @@ NS_GetFrozenFunctions(XPCOMFunctions *entryPoints, const char* libraryPath);
 #define XUL_DLL   "libxul"MOZ_DLL_SUFFIX
 
 #else 
+#include <limits.h> 
 
 #define XPCOM_DLL "libxpcom"MOZ_DLL_SUFFIX
 
