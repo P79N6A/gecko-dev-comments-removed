f8b9d8e65c313b20828f1e9fc1a69ce1182be934
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:40:20 +0000

    Bug 508860 - Crash [@ nsCanvasRenderingContext2D::InitializeWithSurface] r=vladimir
    UltraBlame original commit: bf607bae5e3896b2702c9f0be3c3aaa77089c638

--- content/canvas/src/nsCanvasRenderingContext2D.cpp
+++ content/canvas/src/nsCanvasRenderingContext2D.cpp
@@ -902,7 +902,7 @@ nsCanvasRenderingContext2D::InitializeWithSurface(nsIDocShell *docShell, gfxASur
     mHeight = height;
 
     mSurface = surface;
-    mThebes = new gfxContext(mSurface);
+    mThebes = surface ? new gfxContext(mSurface) : nsnull;
 
     
     if (mSurface == nsnull || mSurface->CairoStatus() != 0 ||
