be09524a29ff4fada206f5d21fd9763083e92ffa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:41:51 +0000

    Bug 475522 -  walk subtree once for all text attributes, r=marcoz, davidb, sr=neil
    UltraBlame original commit: 4767c92771e658f69cc9d908b2fd147f9b0d37bc

--- accessible/tests/mochitest/attributes.js
+++ accessible/tests/mochitest/attributes.js
@@ -70,8 +70,11 @@ function testGroupAttrs(aAccOrElmOrID, aPosInSet, aSetSize, aLevel)
 
 
 
-function testTextAttrs(aID, aOffset, aAttrs, aStartOffset, aEndOffset,
-                       aSkipUnexpectedAttrs)
+
+
+
+function testTextAttrs(aID, aOffset, aAttrs, aDefAttrs,
+                       aStartOffset, aEndOffset, aSkipUnexpectedAttrs)
 {
   var accessible = getAccessible(aID, [nsIAccessibleText]);
   if (!accessible)
@@ -79,17 +82,13 @@ function testTextAttrs(aID, aOffset, aAttrs, aStartOffset, aEndOffset,
 
   var startOffset = { value: -1 };
   var endOffset = { value: -1 };
-  var attrs = null;
-  try {
-    attrs = accessible.getTextAttributes(false, aOffset,
-                                         startOffset, endOffset);
-  } catch (e) {
-  }
 
-  if (!attrs) {
-    ok(false, "Can't get text attributes for " + aID);
+  
+  var attrs = getTextAttributes(aID, accessible, false, aOffset,
+                                startOffset, endOffset);
+
+  if (!attrs)
     return;
-  }
 
   var errorMsg = " for " + aID + " at offset " + aOffset;
 
@@ -97,6 +96,24 @@ function testTextAttrs(aID, aOffset, aAttrs, aStartOffset, aEndOffset,
   is(endOffset.value, aEndOffset, "Wrong end offset" + errorMsg);
 
   compareAttrs(errorMsg, attrs, aAttrs, aSkipUnexpectedAttrs);
+
+  
+  var expectedAttrs = {};
+  for (var name in aAttrs)
+    expectedAttrs[name] = aAttrs[name];
+
+  for (var name in aDefAttrs) {
+    if (!(name in expectedAttrs))
+      expectedAttrs[name] = aDefAttrs[name];
+  }
+
+  attrs = getTextAttributes(aID, accessible, true, aOffset,
+                            startOffset, endOffset);
+  
+  if (!attrs)
+    return;
+
+  compareAttrs(errorMsg, attrs, expectedAttrs, aSkipUnexpectedAttrs);
 }
 
 
@@ -132,6 +149,25 @@ function testDefaultTextAttrs(aID, aDefAttrs, aSkipUnexpectedAttrs)
 
 
 
+
+function getTextAttributes(aID, aAccessible, aIncludeDefAttrs, aOffset,
+                           aStartOffset, aEndOffset)
+{
+  
+  
+  var attrs = null;
+  try {
+    attrs = aAccessible.getTextAttributes(aIncludeDefAttrs, aOffset,
+                                          aStartOffset, aEndOffset);
+  } catch (e) {
+  }
+
+  if (attrs)
+    return attrs;
+
+  ok(false, "Can't get text attributes for " + aID);
+  return null;
+}
 
 function compareAttrs(aErrorMsg, aAttrs, aExpectedAttrs, aSkipUnexpectedAttrs)
 {
