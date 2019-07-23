9f39bf69495776eb2da7824ec305aeda1385744f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:55:35 +0000

    JavaScript Tests - handle 'race' in some Date tests, not part of the build
    UltraBlame original commit: 6342d0dcba240f9c92ad713dc53e19c97adec004

--- js/tests/ecma/Date/15.9.2.2-3.js
+++ js/tests/ecma/Date/15.9.2.2-3.js
@@ -61,9 +61,26 @@ writeHeaderToLog(SECTION+" "+TITLE );
 
 
 
-new TestCase( SECTION, "Date(1899,11,31,23,59,59)",     (new Date()).toString(),    Date(1899,11,31,23,59,59));
-new TestCase( SECTION, "Date(1900,0,1,0,0,0)",          (new Date()).toString(),    Date(1900,0,1,0,0,0) );
-new TestCase( SECTION, "Date(1900,0,1,0,0,1)",          (new Date()).toString(),    Date(1900,0,1,0,0,1) );
-new TestCase( SECTION, "Date(1899,11,31,16,0,0,0)",     (new Date()).toString(),    Date(1899,11,31,16,0,0,0));
+
+var d1;
+var d2;
+
+
+
+d1 = new Date();
+d2 = Date.parse(Date(1899,11,31,23,59,59));
+new TestCase( SECTION, "Date(1899,11,31,23,59,59)", true, d2 - d1 <= 1000);
+
+d1 = new Date();
+d2 = Date.parse(Date(1900,0,1,0,0,0));
+new TestCase( SECTION, "Date(1900,0,1,0,0,0)", true, d2 - d1 <= 1000);
+
+d1 = new Date();
+d2 = Date.parse(Date(1900,0,1,0,0,1) );
+new TestCase( SECTION, "Date(1900,0,1,0,0,1)", true, d2 - d1 <= 1000);
+
+d1 = new Date();
+d2 = Date.parse(Date(1899,11,31,16,0,0,0));
+new TestCase( SECTION, "Date(1899,11,31,16,0,0,0)", true, d2 - d1 <= 1000);
 
 test();
--- js/tests/ecma/Date/15.9.2.2-5.js
+++ js/tests/ecma/Date/15.9.2.2-5.js
@@ -60,9 +60,27 @@ var TITLE = "The Date Constructor Called as a Function";
 writeHeaderToLog(SECTION+" "+TITLE );
 
 
-new TestCase( SECTION, "Date(2004,11,31,23,59,59)",     (new Date()).toString(),    Date(2004,11,31,23,59,59));
-new TestCase( SECTION, "Date(2005,0,1,0,0,0)",          (new Date()).toString(),    Date(2005,0,1,0,0,0) );
-new TestCase( SECTION, "Date(2005,0,1,0,0,1)",          (new Date()).toString(),    Date(2005,0,1,0,0,1) );
-new TestCase( SECTION, "Date(2004,11,31,16,0,0,0)",     (new Date()).toString(),    Date(2004,11,31,16,0,0,0));
+
+
+var d1;
+var d2;
+
+
+
+d1 = new Date();
+d2 = Date.parse(Date(2004,11,31,23,59,59));
+new TestCase( SECTION, "Date(2004,11,31,23,59,59)", true, d2 - d1 <= 1000);
+
+d1 = new Date();
+d2 = Date.parse(Date(2005,0,1,0,0,0) );
+new TestCase( SECTION, "Date(2005,0,1,0,0,0)", true, d2 - d1 <= 1000);
+
+d1 = new Date();
+d2 = Date.parse(Date(2005,0,1,0,0,1) );
+new TestCase( SECTION, "Date(2005,0,1,0,0,1)", true, d2 - d1 <= 1000);
+
+d1 = new Date();
+d2 = Date.parse(Date(2004,11,31,16,0,0,0));
+new TestCase( SECTION, "Date(2004,11,31,16,0,0,0)", true, d2 - d1 <= 1000);
 
 test();
