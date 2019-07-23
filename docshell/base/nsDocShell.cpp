1401bb4e3c05c56cfc7d4adaa75cf83ee3de6308
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:59:53 +0000

    Bug 94514 - POST result page should not appear in global history or history autocomplete results.  r=bz, sr=bz
    UltraBlame original commit: b697005df2bee746c9279334bbdfdf78b5d93089

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -8612,6 +8612,16 @@ nsDocShell::AddToGlobalHistory(nsIURI * aURI, PRBool aRedirect,
     if (mItemType != typeContent || !mGlobalHistory)
         return NS_OK;
 
+    
+    
+    nsCOMPtr<nsIHttpChannel> hchan(do_QueryInterface(aChannel));
+    if (hchan) {
+        nsCAutoString type;
+        nsresult rv = hchan->GetRequestMethod(type);
+        if (NS_SUCCEEDED(rv) && type.EqualsLiteral("POST"))
+            return NS_OK;
+    }
+
     PRBool visited;
     nsresult rv = mGlobalHistory->IsVisited(aURI, &visited);
     if (NS_FAILED(rv))
