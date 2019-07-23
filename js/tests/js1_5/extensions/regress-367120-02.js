c4f60a69b0f63321e9b09cdec67e3564436c922d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:39 +0000

    JavaScript Test - regression test for bug 367120, by moz_bu_r_a4
    UltraBlame original commit: 02ac9ca3d4f9a84aa63c8ef64775e74cd1307a94

---
+++ js/tests/js1_5/extensions/regress-367120-02.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 367120;
+var summary = 'memory corruption in script_toString';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  if (typeof Script == 'undefined')
+  {
+    print('Test skipped. Script not defined.');
+  }
+  else
+  {
+    var s = new Script("");
+    var o = {
+      valueOf : function() {
+        s.compile("");
+        Array(11).join(Array(11).join(Array(101).join("aaaaa")));
+        return;
+      }
+    };
+    s.toString(o);
+  }
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
