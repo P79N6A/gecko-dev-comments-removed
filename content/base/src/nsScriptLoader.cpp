2987d195d7adab554293c94a663ba57934e728de
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:49:31 +0000

    Fix bug 397605.  r+sr+a=jst
    UltraBlame original commit: 4c065d544cb22b060c8ef141cd2cfbb485ea3eb7

--- content/base/src/nsScriptLoader.cpp
+++ content/base/src/nsScriptLoader.cpp
@@ -852,13 +852,13 @@ nsScriptLoader::PrepareLoadedRequest(nsScriptLoadRequest* aRequest,
     
     
     if (channel) {
-      nsCOMPtr<nsISupports> owner;
-      channel->GetOwner(getter_AddRefs(owner));
-      nsCOMPtr<nsIPrincipal> principal = do_QueryInterface(owner);
-
-      if (principal) {
+      nsCOMPtr<nsIPrincipal> channelPrincipal;
+      nsContentUtils::GetSecurityManager()->
+        GetChannelPrincipal(channel, getter_AddRefs(channelPrincipal));
+      if (channelPrincipal) {
         nsCOMPtr<nsIPrincipal> newPrincipal =
-          MaybeDowngradeToCodebase(mDocument->NodePrincipal(), principal);
+          MaybeDowngradeToCodebase(mDocument->NodePrincipal(),
+                                   channelPrincipal);
 
         mDocument->SetPrincipal(newPrincipal);
       }
