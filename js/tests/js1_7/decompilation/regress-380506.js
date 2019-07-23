d5b1850e7f86a41efa43d0275ffc380d969bdf03
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:16 +0000

    JavaScript Test - regression test for bug 380506, by Jesse Ruderman
    UltraBlame original commit: 9ec2693ba2246ee9187ae48219540c70c0a1feb8

---
+++ js/tests/js1_7/decompilation/regress-380506.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-380506.js';
+
+
+var BUGNUMBER = 380506;
+var summary = 'Decompilation of nested-for and for-if comprehensions';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  var f = function (){return [i*i for(i in [0]) if (i%2)]};
+  expect = 'function (){return [i*i for(i in [0]) if (i%2)];}';
+  actual = f + '';
+  compareSource(expect, actual, summary);
+
+  f = function (){return [i*j for(i in [0]) for (j in [1])]};
+  expect = 'function (){return [i*j for(i in [0]) for (j in [1])];}';
+  actual = f + '';
+  compareSource(expect, actual, summary);
+
+  exitFunc ('test');
+}
