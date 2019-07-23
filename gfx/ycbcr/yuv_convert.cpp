98b9b4a6bbd6302ce83770026aeebce5178e371e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:29:04 +0000

    Bustage fix for non-libxul windows builds from bug 551277. Wrap the return value for the implementation in NS_GFX_ as well. a=bustage-fix,tree quiet
    UltraBlame original commit: fe937d72a9cea4ca9d1cf85230989dc109d6bc36

--- gfx/ycbcr/yuv_convert.cpp
+++ gfx/ycbcr/yuv_convert.cpp
@@ -27,18 +27,18 @@ namespace mozilla {
 namespace gfx {
 
 
-void ConvertYCbCrToRGB32(const uint8* y_buf,
-                         const uint8* u_buf,
-                         const uint8* v_buf,
-                         uint8* rgb_buf,
-                         int pic_x,
-                         int pic_y,
-                         int pic_width,
-                         int pic_height,
-                         int y_pitch,
-                         int uv_pitch,
-                         int rgb_pitch,
-                         YUVType yuv_type) {
+NS_GFX_(void) ConvertYCbCrToRGB32(const uint8* y_buf,
+                                  const uint8* u_buf,
+                                  const uint8* v_buf,
+                                  uint8* rgb_buf,
+                                  int pic_x,
+                                  int pic_y,
+                                  int pic_width,
+                                  int pic_height,
+                                  int y_pitch,
+                                  int uv_pitch,
+                                  int rgb_pitch,
+                                  YUVType yuv_type) {
   unsigned int y_shift = yuv_type == YV12 ? 1 : 0;
   unsigned int x_shift = yuv_type == YV24 ? 0 : 1;
   
