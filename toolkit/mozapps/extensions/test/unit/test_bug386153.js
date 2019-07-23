066819511f488cb5a944db4bb2d1842fd007479c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:16:14 +0000

    Bug 386153: XPI files dropped in the extensions folder always appear unsigned. r=robstrong
    UltraBlame original commit: 37d2e8a7899f6c6c763b66580a6fdd99201d00e6

---
+++ toolkit/mozapps/extensions/test/unit/test_bug386153.js
@@ -0,0 +1,131 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+const URI_XPINSTALL_CONFIRM_DIALOG = "chrome://mozapps/content/xpinstall/xpinstallConfirm.xul";
+
+
+function findXPI(dpb, name) {
+  for (var i = 0; i < 3; i++) {
+    if (dpb.GetString(i * 4 + 1).substr(-(name.length + 1)) == "/" + name)
+      return i * 4;
+  }
+  do_throw(name + " wasn't in the list");
+}
+
+
+var WindowWatcher = {
+  openWindow: function(parent, url, name, features, arguments) {
+    do_check_eq(url, URI_XPINSTALL_CONFIRM_DIALOG);
+    var dpb = arguments.QueryInterface(Ci.nsISupportsInterfacePointer)
+                       .data.QueryInterface(Ci.nsIDialogParamBlock);
+    do_check_eq(dpb.GetInt(1), 12);
+
+    
+    var unsigned = findXPI(dpb, "unsigned.xpi");
+    var signed = findXPI(dpb, "signed.xpi");
+    var untrusted = findXPI(dpb, "signed-untrusted.xpi");
+
+    
+    do_check_eq(dpb.GetString(unsigned), "XPI Test");
+    do_check_eq(dpb.GetString(unsigned + 3), "");
+
+    do_check_eq(dpb.GetString(signed), "Signed XPI Test");
+    do_check_eq(dpb.GetString(signed + 3), "Mozilla Testing");
+
+    
+    do_check_eq(dpb.GetString(untrusted), "Signed XPI Test - Untrusted");
+    do_check_eq(dpb.GetString(untrusted + 3), "");
+
+    
+    dpb.SetInt(0, 0);
+  },
+
+  QueryInterface: function(iid) {
+    if (iid.equals(Ci.nsIWindowWatcher)
+     || iid.equals(Ci.nsISupports))
+      return this;
+
+    throw Cr.NS_ERROR_NO_INTERFACE;
+  }
+}
+
+var WindowWatcherFactory = {
+  createInstance: function createInstance(outer, iid) {
+    if (outer != null)
+      throw Components.results.NS_ERROR_NO_AGGREGATION;
+    return WindowWatcher.QueryInterface(iid);
+  }
+};
+
+var registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
+registrar.registerFactory(Components.ID("{1dfeb90a-2193-45d5-9cb8-864928b2af55}"),
+                          "Fake Window Watcher",
+                          "@mozilla.org/embedcomp/window-watcher;1", WindowWatcherFactory);
+
+function run_test()
+{
+  
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1");
+
+  
+  do_get_file("data/cert8.db").copyTo(gProfD, null);
+  do_get_file("data/key3.db").copyTo(gProfD, null);
+  do_get_file("data/secmod.db").copyTo(gProfD, null);
+
+  
+  var il = gProfD.clone();
+  il.append("extensions");
+  if (!il.exists())
+    il.create(Ci.nsIFile.DIRECTORY_TYPE, 0755);
+  do_get_file("data/unsigned.xpi").copyTo(il, null);
+  do_get_file("data/signed.xpi").copyTo(il, null);
+  do_get_file("data/signed-untrusted.xpi").copyTo(il, null);
+  do_get_file("data/signed-tampered.xpi").copyTo(il, null);
+
+  
+  startupEM();
+  do_check_neq(gEM.getItemForID("signed-xpi@tests.mozilla.org"), null);
+  do_check_neq(gEM.getItemForID("unsigned-xpi@tests.mozilla.org"), null);
+  do_check_neq(gEM.getItemForID("untrusted-xpi@tests.mozilla.org"), null);
+  do_check_eq(gEM.getItemForID("tampered-xpi@tests.mozilla.org"), null);
+
+  shutdownEM();
+}
