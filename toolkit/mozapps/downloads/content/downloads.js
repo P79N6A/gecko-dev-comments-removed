f4174e2c14286485db888094b4cb1288e451872f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:46:38 +0000

    fix for bug #392386:  searching in the download manager can fail to show all the matches if we have items with bad native paths. r=mano
    UltraBlame original commit: 92db60a3e21130cd1ced591f591bc2d7d8aeb559

--- toolkit/mozapps/downloads/content/downloads.js
+++ toolkit/mozapps/downloads/content/downloads.js
@@ -96,16 +96,19 @@ function createDownloadItem(aID, aFile, aTarget, aURI, aState,
   dl.setAttribute("status", aStatus);
   dl.setAttribute("progress", aProgress);
   dl.setAttribute("startTime", aStartTime);
-
-  var ioSvc = Cc["@mozilla.org/network/io-service;1"].
-              getService(Ci.nsIIOService);
-  var file = ioSvc.newURI(aFile, null, null).QueryInterface(Ci.nsIFileURL).file;
-  dl.setAttribute("path", file.nativePath || file.path);
-
   if (aReferrer)
     dl.setAttribute("referrer", aReferrer);
 
-  return dl;
+  try {
+    var file = getLocalFileFromNativePathOrUrl(aFile);
+    dl.setAttribute("path", file.nativePath || file.path);
+    return dl;
+  }
+  catch (ex) {
+    
+    
+  }
+  return null;
 }
 
 function getDownload(aID)
@@ -678,7 +681,8 @@ function buildDownloadList(aStmt, aRef)
                                 state, "", percentComplete,
                                 Math.round(aStmt.getInt64(5) / 1000),
                                 aStmt.getString(6));
-    gDownloadsView.insertBefore(dl, aRef.nextSibling);
+    if (dl)
+      gDownloadsView.insertBefore(dl, aRef.nextSibling);
   }
 }
 
@@ -815,10 +819,13 @@ function onSearchboxFocus() {
 
 
 
+
+
+
+
 function getLocalFileFromNativePathOrUrl(aPathOrUrl)
 {
   if (aPathOrUrl.substring(0,7) == "file://") {
-
     
     let ioSvc = Cc["@mozilla.org/network/io-service;1"].
                 getService(Ci.nsIIOService);
@@ -827,9 +834,7 @@ function getLocalFileFromNativePathOrUrl(aPathOrUrl)
     const fileUrl = ioSvc.newURI(aPathOrUrl, null, null).
                     QueryInterface(Ci.nsIFileURL);
     return fileUrl.file.clone().QueryInterface(Ci.nsILocalFile);
-
   } else {
-
     
     var f = new nsLocalFile(aPathOrUrl);
 
