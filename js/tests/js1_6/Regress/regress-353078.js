d2a817eaceb663d2e01306dda0b8e8685589e38d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:39:39 +0000

    JavaScript Tests - regression test for bug 353078, by Jesse Ruderman
    UltraBlame original commit: eec4a46db0e6a30cc16b69ac331b33d9e0a50156

---
+++ js/tests/js1_6/Regress/regress-353078.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bug = 353078;
+var summary = 'Do not assert with bogus toString, map, split';
+var actual = 'No Crash';
+var expect = 'No Crash';
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber (bug);
+  printStatus (summary);
+  
+  expect = 'TypeError: can\'t convert global to string';
+  try
+  {
+    this.toString = function() { return {}; }; p = [11].map('foo'.split);
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
