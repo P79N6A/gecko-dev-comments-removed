ba9e55c661681405e4cae3ab07f4cf27da7a0920
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 14:46:10 +0000

    bug 477581 - JavaScript Test by Jason Orendorff.
    UltraBlame original commit: e9b89d4d27798e70e3e14582d14ee04780b608ae

---
+++ js/tests/js1_8/regress/regress-477581.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-477581.js';
+
+var BUGNUMBER = 477581;
+var summary = 'Do not assert: !JSVAL_IS_PRIMITIVE(regs.sp[-2])';
+var actual = '';
+var expect = '';
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  jit(true);
+ 
+  function g() { yield 2; }
+  var iterables = [[1], [], [], [], g()];
+  for (let i = 0; i < iterables.length; i++)
+    for each (let j in iterables[i])
+               ;
+
+  jit(false);
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
