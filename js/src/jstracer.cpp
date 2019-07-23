ca104a082104f1e2775122373adb6f53b5ffe86d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:27:40 +0000

    Fix typeChar array (used only in debug output) to match the TraceType enum values. No bug#. r=dvander over IRC.
    UltraBlame original commit: 54d16bda64559a1284ac97af58461f81b1282613

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -198,7 +198,7 @@ JS_STATIC_ASSERT(sizeof(TraceType) == 1);
 JS_STATIC_ASSERT(offsetof(TraceNativeStorage, stack_global_buf) % 16 == 0);
 
 
-static const char typeChar[] = "OIDXSNBF";
+static const char typeChar[] = "OIDXSNBUF";
 static const char tagChar[]  = "OIDISIBI";
 
 
