f8fa986d507c1d05235d85790886e60927438aa4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:51:55 +0000

    JavaScript Tests - regression test for bug 383682, by Blake Kaplan, not part of the build
    UltraBlame original commit: 848976f98b1fea473464010a8ea2f49895fd1e9e

---
+++ js/tests/js1_5/Regress/regress-383682.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-383682.js';
+
+var BUGNUMBER = 383682;
+var summary = 'eval is too dynamic';
+var actual = '';
+var expect = '';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  function f(s) {
+    return this.eval(s);
+  }
+
+  expect = 'PASS';
+  f("function g() { return('PASS'); }");
+  actual = g();
+
+  reportCompare(expect, actual, summary);
+
+  exitFunc ('test');
+}
