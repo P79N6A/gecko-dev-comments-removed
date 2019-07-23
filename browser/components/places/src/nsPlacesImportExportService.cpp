924c7c03b8bc326f0342543720afc4cf91783e95
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:04:51 +0000

    Backing out fix for bug 419654 due to centos orange
    UltraBlame original commit: d3cb395288a184adad76db7747dbcd794076b1e7

--- browser/components/places/src/nsPlacesImportExportService.cpp
+++ browser/components/places/src/nsPlacesImportExportService.cpp
@@ -329,8 +329,6 @@ nsPlacesImportExportService::~nsPlacesImportExportService()
 class BookmarkContentSink : public nsIHTMLContentSink
 {
 public:
-  BookmarkContentSink();
-
   nsresult Init(PRBool aAllowRootChanges,
                 nsINavBookmarksService* bookmarkService,
                 PRInt64 aFolder,
@@ -437,10 +435,6 @@ protected:
   }
 #endif
 };
-
-BookmarkContentSink::BookmarkContentSink() : mFrames(16)
-{
-}
 
 
 
@@ -1228,11 +1222,9 @@ BookmarkContentSink::NewFrame()
     
   }
 
-  frame.mPreviousId = ourID;
-
   if (!mFrames.AppendElement(BookmarkImportFrame(ourID)))
     return NS_ERROR_OUT_OF_MEMORY;
-
+  frame.mPreviousId = ourID;
   return NS_OK;
 }
 
