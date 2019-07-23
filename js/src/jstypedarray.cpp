37839b839d5427ee047fb8495edf8e8067f0ce1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:17:28 +0000

    b=543682; don't let js array holes escape via typed arrays; r=jorendorff
    UltraBlame original commit: b4268cb7bf4823f1f4b7e50f20899d04e1342db2

--- js/src/jstypedarray.cpp
+++ js/src/jstypedarray.cpp
@@ -920,7 +920,7 @@ class TypedArrayTemplate
     {
         NativeType *dest = static_cast<NativeType*>(data);
 
-        if (ar->isDenseArray()) {
+        if (ar->isDenseArray() && js_DenseArrayCapacity(ar) >= len) {
             JS_ASSERT(ar->fslots[JSSLOT_ARRAY_LENGTH] == (jsval)len);
 
             jsval *src = ar->dslots;
@@ -931,6 +931,12 @@ class TypedArrayTemplate
                     *dest++ = NativeType(JSVAL_TO_INT(v));
                 } else if (JSVAL_IS_DOUBLE(v)) {
                     *dest++ = NativeType(*JSVAL_TO_DOUBLE(v));
+                } else if (v == JSVAL_HOLE) {
+                    
+                    
+                    
+                    
+                    *dest++ = NativeType(int32(0));
                 } else {
                     jsdouble dval;
                     if (!JS_ValueToNumber(cx, v, &dval))
@@ -950,6 +956,9 @@ class TypedArrayTemplate
                     *dest++ = NativeType(JSVAL_TO_INT(v));
                 } else if (JSVAL_IS_DOUBLE(v)) {
                     *dest++ = NativeType(*JSVAL_TO_DOUBLE(v));
+                } else if (v == JSVAL_VOID) {
+                    
+                    *dest++ = NativeType(int32(0));
                 } else {
                     jsdouble dval;
                     if (!JS_ValueToNumber(cx, v, &dval))
