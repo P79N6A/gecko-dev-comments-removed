d8abad6e7aed03195643653bf6a06da554c5b90f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:44:11 +0000

    Bug 387685: clean up Firefox tooltip code now that bug 279703 has landed (back out most of the patch for bug 218223), r=Enn (original SeaMonkey patch by Neil Rashbrook in bug 386390)
    UltraBlame original commit: 3267f4cf2543a3dd0b50b3293fec00c8ed09d2e0

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2608,12 +2608,11 @@ function FillInHTMLTooltip(tipElement)
     tipElement = tipElement.parentNode;
   }
 
-  var tipNode = document.getElementById("aHTMLTooltip").firstChild;
-  var label = tipNode.firstChild;
-
+  var tipNode = document.getElementById("aHTMLTooltip");
+  tipNode.style.direction = direction;
+  
   for each (var t in [titleText, XLinkTitleText]) {
     if (t && /\S/.test(t)) {
-      tipNode.style.direction = direction;
 
       
       
@@ -2622,9 +2621,8 @@ function FillInHTMLTooltip(tipElement)
       
       t = t.replace(/[\r\t]/g, ' ');
       t = t.replace(/\n/g, '');
-      
-      label.textContent = t;
-      
+
+      tipNode.setAttribute("label", t);
       retVal = true;
     }
   }
