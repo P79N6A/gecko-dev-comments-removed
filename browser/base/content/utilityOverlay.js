eb164c06491ece4037b13d432c45dc96fa584c84
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:42:32 +0000

    Bug 383746 - Page Info RSS section should use code in browser.js to enumerate page feeds. Patch by Ehsan Akhgari <ehsan.akhgari@gmail.com>. r=mano.
    UltraBlame original commit: 9cbe781d3b7651519dcff15a74b2d50adec15a67

--- browser/base/content/utilityOverlay.js
+++ browser/base/content/utilityOverlay.js
@@ -5,7 +5,7 @@
 # The contents of this file are subject to the Mozilla Public License Version
 # 1.1 (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
+# http:
 #
 # Software distributed under the License is distributed on an "AS IS" basis,
 # WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
@@ -21,6 +21,7 @@
 #
 # Contributor(s):
 #   Alec Flett <alecf@netscape.com>
+#   Ehsan Akhgari <ehsan.akhgari@gmail.com>
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -36,10 +37,10 @@
 #
 # ***** END LICENSE BLOCK *****
 
-/**
- * Communicator Shared Utility Library
- * for shared application glue for the Communicator suite of applications
- **/
+
+
+
+
 
 var goPrefWindow = 0;
 var gBidiUI = false;
@@ -89,17 +90,17 @@ function getBoolPref ( prefname, def )
   }
 }
 
-// Change focus for this browser window to |aElement|, without focusing the
-// window itself.
+
+
 function focusElement(aElement) {
-  // This is a redo of the fix for jag bug 91884
+  
   var ww = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
                      .getService(Components.interfaces.nsIWindowWatcher);
   if (window == ww.activeWindow)
     aElement.focus();
   else {
-    // set the element in command dispatcher so focus will restore properly
-    // when the window does become active
+    
+    
     var cmdDispatcher = document.commandDispatcher;
     if (aElement instanceof Window) {
       cmdDispatcher.focusedWindow = aElement;
@@ -112,7 +113,7 @@ function focusElement(aElement) {
   }
 }
 
-// openUILink handles clicks on UI elements that cause URLs to load.
+
 function openUILink( url, e, ignoreButton, ignoreAlt, allowKeywordFixup, postData, referrerUrl )
 {
   var where = whereToOpenLink(e, ignoreButton, ignoreAlt);
@@ -120,30 +121,30 @@ function openUILink( url, e, ignoreButton, ignoreAlt, allowKeywordFixup, postDat
 }
 
 
-/* whereToOpenLink() looks at an event to decide where to open a link.
- *
- * The event may be a mouse event (click, double-click, middle-click) or keypress event (enter).
- *
- * On Windows, the modifiers are:
- * Ctrl        new tab, selected
- * Shift       new window
- * Ctrl+Shift  new tab, in background
- * Alt         save
- *
- * You can swap Ctrl and Ctrl+shift by toggling the hidden pref
- * browser.tabs.loadBookmarksInBackground (not browser.tabs.loadInBackground, which
- * is for content area links).
- *
- * Middle-clicking is the same as Ctrl+clicking (it opens a new tab) and it is
- * subject to the shift modifier and pref in the same way.
- *
- * Exceptions: 
- * - Alt is ignored for menu items selected using the keyboard so you don't accidentally save stuff.  
- *    (Currently, the Alt isn't sent here at all for menu items, but that will change in bug 126189.)
- * - Alt is hard to use in context menus, because pressing Alt closes the menu.
- * - Alt can't be used on the bookmarks toolbar because Alt is used for "treat this as something draggable".
- * - The button is ignored for the middle-click-paste-URL feature, since it's always a middle-click.
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function whereToOpenLink( e, ignoreButton, ignoreAlt )
 {
   if (!e)
@@ -154,11 +155,11 @@ function whereToOpenLink( e, ignoreButton, ignoreAlt )
   var meta =  e.metaKey;
   var alt  =  e.altKey && !ignoreAlt;
 
-  // ignoreButton allows "middle-click paste" to use function without always opening in a new window.
+  
   var middle = !ignoreButton && e.button == 1;
   var middleUsesTabs = getBoolPref("browser.tabs.opentabfor.middleclick", true);
 
-  // Don't do anything special with right-mouse clicks.  They're probably clicks on context menu items.
+  
 
 #ifdef XP_MACOSX
   if (meta || (middle && middleUsesTabs)) {
@@ -181,19 +182,19 @@ function whereToOpenLink( e, ignoreButton, ignoreAlt )
   }
 }
 
-/* openUILinkIn opens a URL in a place specified by the parameter |where|.
- *
- * |where| can be:
- *  "current"     current tab            (if there aren't any browser windows, then in a new window instead)
- *  "tab"         new tab                (if there aren't any browser windows, then in a new window instead)
- *  "tabshifted"  same as "tab" but in background if default is to select new tabs, and vice versa
- *  "window"      new window
- *  "save"        save to disk (with no filename hint!)
- *
- * allowThirdPartyFixup controls whether third party services such as Google's
- * I Feel Lucky are allowed to interpret this URL. This parameter may be
- * undefined, which is treated as false.
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
 function openUILinkIn( url, where, allowThirdPartyFixup, postData, referrerUrl )
 {
   if (!where || !url)
@@ -220,7 +221,7 @@ function openUILinkIn( url, where, allowThirdPartyFixup, postData, referrerUrl )
     break;
   case "tabshifted":
     loadInBackground = !loadInBackground;
-    // fall through
+    
   case "tab":
     var browser = w.getBrowser();
     browser.loadOneTab(url, referrerUrl, null, postData, loadInBackground,
@@ -228,37 +229,37 @@ function openUILinkIn( url, where, allowThirdPartyFixup, postData, referrerUrl )
     break;
   }
 
-  // Call focusElement(w.content) instead of w.content.focus() to make sure
-  // that we don't raise the old window, since the URI we just loaded may have
-  // resulted in a new frontmost window (e.g. "javascript:window.open('');").
+  
+  
+  
   focusElement(w.content);
 }
 
-// Used as an onclick handler for UI elements with link-like behavior.
-// e.g. onclick="checkForMiddleClick(this, event);"
+
+
 function checkForMiddleClick(node, event)
 {
-  // We should be using the disabled property here instead of the attribute,
-  // but some elements that this function is used with don't support it (e.g.
-  // menuitem).
+  
+  
+  
   if (node.getAttribute("disabled") == "true")
-    return; // Do nothing
+    return; 
 
   if (event.button == 1) {
-    /* Execute the node's oncommand.
-     *
-     * XXX: we should use node.oncommand(event) once bug 246720 is fixed.
-     */
+    
+
+
+
     var fn = new Function("event", node.getAttribute("oncommand"));
     fn.call(node, event);
 
-    // If the middle-click was on part of a menu, close the menu.
-    // (Menus close automatically with left-click but not with middle-click.)
+    
+    
     closeMenus(event.target);
   }
 }
 
-// Closes all popups that are ancestors of the node.
+
 function closeMenus(node)
 {
   if ("tagName" in node) {
@@ -270,47 +271,47 @@ function closeMenus(node)
   }
 }
 
-// Gather all descendent text under given document node.
+
 function gatherTextUnder ( root ) 
 {
   var text = "";
   var node = root.firstChild;
   var depth = 1;
   while ( node && depth > 0 ) {
-    // See if this node is text.
+    
     if ( node.nodeType == Node.TEXT_NODE ) {
-      // Add this text to our collection.
+      
       text += " " + node.data;
     } else if ( node instanceof HTMLImageElement) {
-      // If it has an alt= attribute, use that.
+      
       var altText = node.getAttribute( "alt" );
       if ( altText && altText != "" ) {
         text = altText;
         break;
       }
     }
-    // Find next node to test.
-    // First, see if this node has children.
+    
+    
     if ( node.hasChildNodes() ) {
-      // Go to first child.
+      
       node = node.firstChild;
       depth++;
     } else {
-      // No children, try next sibling.
+      
       if ( node.nextSibling ) {
         node = node.nextSibling;
       } else {
-        // Last resort is our next oldest uncle/aunt.
+        
         node = node.parentNode.nextSibling;
         depth--;
       }
     }
   }
-  // Strip leading whitespace.
+  
   text = text.replace( /^\s+/, "" );
-  // Strip trailing whitespace.
+  
   text = text.replace( /\s+$/, "" );
-  // Compress remaining whitespace.
+  
   text = text.replace( /\s+/g, " " );
   return text;
 }
@@ -343,7 +344,7 @@ function isBidiEnabled() {
     }
   } catch (e) {}
 
-  // check the overriding pref
+  
   if (!rv)
     rv = getBoolPref("bidi.browser.ui");
 
@@ -359,8 +360,8 @@ function openAboutDialog()
   if (win)
     win.focus();
   else {
-    // XXXmano: define minimizable=no although it does nothing on OS X
-    // (see Bug 287162); remove this comment once Bug 287162 is fixed...
+    
+    
     window.open("chrome://browser/content/aboutDialog.xul", "About",
                 "chrome, resizable=no, minimizable=no");
   }
@@ -389,13 +390,13 @@ function openPreferences(paneID)
                "Preferences", features, paneID);
 }
 
-/**
- * Opens the release notes page for this version of the application.
- * @param   event
- *          The DOM Event that caused this function to be called, used to
- *          determine where the release notes page should be displayed based
- *          on modifiers (e.g. Ctrl = new tab)
- */
+
+
+
+
+
+
+
 function openReleaseNotes(event)
 {
   var formatter = Components.classes["@mozilla.org/toolkit/URLFormatterService;1"]
@@ -405,9 +406,9 @@ function openReleaseNotes(event)
   openUILink(relnotesURL, event, false, true);
 }
   
-/**
- * Opens the update manager and checks for updates to the application.
- */
+
+
+
 function checkForUpdates()
 {
   var um = 
@@ -417,9 +418,9 @@ function checkForUpdates()
       Components.classes["@mozilla.org/updates/update-prompt;1"].
       createInstance(Components.interfaces.nsIUpdatePrompt);
 
-  // If there's an update ready to be applied, show the "Update Downloaded"
-  // UI instead and let the user know they have to restart the browser for
-  // the changes to be applied. 
+  
+  
+  
   if (um.activeUpdate && um.activeUpdate.state == "pending")
     prompter.showUpdateDownloaded(um.activeUpdate);
   else
@@ -428,8 +429,8 @@ function checkForUpdates()
 
 function buildHelpMenu()
 {
-  // Enable/disable the "Report Web Forgery" menu item.  safebrowsing object
-  // may not exist in OSX
+  
+  
   if (typeof safebrowsing != "undefined")
     safebrowsing.setReportPhishingMenu();
 
@@ -440,8 +441,8 @@ function buildHelpMenu()
       Components.classes["@mozilla.org/updates/update-manager;1"].
       getService(Components.interfaces.nsIUpdateManager);
 
-  // Disable the UI if the update enabled pref has been locked by the 
-  // administrator or if we cannot update for some other reason
+  
+  
   var checkForUpdates = document.getElementById("checkForUpdates");
   var canUpdate = updates.canUpdate;
   checkForUpdates.setAttribute("disabled", !canUpdate);
@@ -451,27 +452,27 @@ function buildHelpMenu()
   var strings = document.getElementById("bundle_browser");
   var activeUpdate = um.activeUpdate;
   
-  // If there's an active update, substitute its name into the label
-  // we show for this item, otherwise display a generic label.
+  
+  
   function getStringWithUpdateName(key) {
     if (activeUpdate && activeUpdate.name)
       return strings.getFormattedString(key, [activeUpdate.name]);
     return strings.getString(key + "Fallback");
   }
   
-  // By default, show "Check for Updates..."
+  
   var key = "default";
   if (activeUpdate) {
     switch (activeUpdate.state) {
     case "downloading":
-      // If we're downloading an update at present, show the text:
-      // "Downloading Firefox x.x..." otherwise we're paused, and show
-      // "Resume Downloading Firefox x.x..."
+      
+      
+      
       key = updates.isDownloading ? "downloading" : "resume";
       break;
     case "pending":
-      // If we're waiting for the user to restart, show: "Apply Downloaded
-      // Updates Now..."
+      
+      
       key = "pending";
       break;
     }
@@ -485,9 +486,9 @@ function buildHelpMenu()
 
 function isElementVisible(aElement)
 {
-  // * When an element is hidden, the width and height of its boxObject
-  //   are set to 0
-  // * css-visibility (unlike css-display) is inherited.
+  
+  
+  
   var bo = aElement.boxObject;
   return (bo.height != 0 && bo.width != 0 &&
           document.defaultView
@@ -505,26 +506,26 @@ function getBrowserFromContentWindow(aContentWindow)
 }
 
 
-/**
- * openNewTabWith: opens a new tab with the given URL.
- *
- * @param aURL
- *        The URL to open (as a string).
- * @param aDocument
- *        The document from which the URL came, or null. This is used to set the
- *        referrer header and to do a security check of whether the document is
- *        allowed to reference the URL. If null, there will be no referrer
- *        header and no security check.
- * @param aPostData
- *        Form POST data, or null.
- * @param aEvent
- *        The triggering event (for the purpose of determining whether to open
- *        in the background), or null.
- * @param aAllowThirdPartyFixup
- *        If true, then we allow the URL text to be sent to third party services
- *        (e.g., Google's I Feel Lucky) for interpretation. This parameter may
- *        be undefined in which case it is treated as false.
- */ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 function openNewTabWith(aURL, aDocument, aPostData, aEvent,
                         aAllowThirdPartyFixup)
 {
@@ -535,7 +536,7 @@ function openNewTabWith(aURL, aDocument, aPostData, aEvent,
                           .getService(Components.interfaces.nsIPrefService);
   prefSvc = prefSvc.getBranch(null);
 
-  // should we open it in a new tab?
+  
   var loadInBackground = true;
   try {
     loadInBackground = prefSvc.getBoolPref("browser.tabs.loadInBackground");
@@ -546,14 +547,14 @@ function openNewTabWith(aURL, aDocument, aPostData, aEvent,
   if (aEvent && aEvent.shiftKey)
     loadInBackground = !loadInBackground;
 
-  // As in openNewWindowWith(), we want to pass the charset of the
-  // current document over to a new tab. 
+  
+  
   var wintype = document.documentElement.getAttribute("windowtype");
   var originCharset;
   if (wintype == "navigator:browser")
     originCharset = window.content.document.characterSet;
 
-  // open link in new tab
+  
   var referrerURI = aDocument ? aDocument.documentURIObject : null;
   var browser = top.document.getElementById("content");
   browser.loadOneTab(aURL, referrerURI, originCharset, aPostData,
@@ -565,10 +566,10 @@ function openNewWindowWith(aURL, aDocument, aPostData, aAllowThirdPartyFixup)
   if (aDocument)
     urlSecurityCheck(aURL, aDocument.nodePrincipal);
 
-  // if and only if the current window is a browser window and it has a
-  // document with a character set, then extract the current charset menu
-  // setting from the current document and use it to initialize the new browser
-  // window...
+  
+  
+  
+  
   var charsetArg = null;
   var wintype = document.documentElement.getAttribute("windowtype");
   if (wintype == "navigator:browser")
@@ -578,4 +579,72 @@ function openNewWindowWith(aURL, aDocument, aPostData, aAllowThirdPartyFixup)
   window.openDialog(getBrowserURL(), "_blank", "chrome,all,dialog=no",
                     aURL, charsetArg, referrerURI, aPostData,
                     aAllowThirdPartyFixup);
+}
+
+
+
+
+
+
+
+
+
+
+
+ 
+function recognizeFeedFromLink(aLink, aPrincipal)
+{
+  if (!aLink || !aPrincipal)
+    return null;
+
+  var erel = aLink.rel;
+  var etype = aLink.type;
+  var etitle = aLink.title;
+  const rssTitleRegex = /(^|\s)rss($|\s)/i;
+  var rels = {};
+
+  if (erel) {
+    for each (var relValue in erel.split(/\s+/))
+      rels[relValue] = true;
+  }
+  var isFeed = rels.feed;
+
+  if (!isFeed && (!rels.alternate || rels.stylesheet || !etype))
+    return null;
+
+  if (!isFeed) {
+    
+    etype = etype.replace(/^\s+/, "");
+    etype = etype.replace(/\s+$/, "");
+    etype = etype.replace(/\s*;.*/, "");
+    etype = etype.toLowerCase();
+    isFeed = (etype == "application/rss+xml" ||
+              etype == "application/atom+xml");
+    if (!isFeed) {
+      
+      isFeed = ((etype == "text/xml" || etype == "application/xml" ||
+                 etype == "application/rdf+xml") && rssTitleRegex.test(etitle));
+    }
+  }
+
+  if (isFeed) {
+    try { 
+      urlSecurityCheck(aLink.href,
+                       aPrincipal,
+                       Components.interfaces.nsIScriptSecurityManager.DISALLOW_INHERIT_PRINCIPAL);
+    }
+    catch (ex) {
+      dump(ex.message);
+      return null; 
+    }
+
+    
+    return {
+        href: aLink.href,
+        type: etype,
+        title: aLink.title
+      };
+  }
+
+  return null;
 }
