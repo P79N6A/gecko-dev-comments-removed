7a7c6622dda74d41a2ecc41e19963728bba5badc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 16:28:08 +0000

    Bug 548437: Test SysV shmem.
    UltraBlame original commit: 41ceb429c51dbdb0d6c2542b3e26a09a17c66520

---
+++ ipc/ipdl/test/cxx/TestSysVShmem.cpp
@@ -0,0 +1,75 @@
+#include "TestSysVShmem.h"
+
+#include "IPDLUnitTests.h"      
+
+
+namespace mozilla {
+namespace _ipdltest {
+
+
+
+
+void
+TestSysVShmemParent::Main()
+{
+    Shmem mem;
+    size_t size = 12345;
+    if (!AllocShmem(size, SharedMemory::TYPE_SYSV, &mem))
+        fail("can't alloc shmem");
+
+    if (mem.Size<char>() != size)
+        fail("shmem is wrong size: expected %lu, got %lu",
+             size, mem.Size<char>());
+
+    char* ptr = mem.get<char>();
+    memcpy(ptr, "Hello!", sizeof("Hello!"));
+    if (!SendGive(mem, size))
+        fail("can't send Give()");
+
+    
+    
+
+    
+    
+}
+
+
+bool
+TestSysVShmemParent::RecvTake(Shmem& mem, const size_t& expectedSize)
+{
+    if (mem.Size<char>() != expectedSize)
+        fail("expected shmem size %lu, but it has size %lu",
+             expectedSize, mem.Size<char>());
+
+    if (strcmp(mem.get<char>(), "And yourself!"))
+        fail("expected message was not written");
+
+    Close();
+
+    return true;
+}
+
+
+
+
+bool
+TestSysVShmemChild::RecvGive(Shmem& mem, const size_t& expectedSize)
+{
+    if (mem.Size<char>() != expectedSize)
+        fail("expected shmem size %lu, but it has size %lu",
+             expectedSize, mem.Size<char>());
+
+    if (strcmp(mem.get<char>(), "Hello!"))
+        fail("expected message was not written");
+
+    memcpy(mem.get<char>(), "And yourself!", sizeof("And yourself!"));
+
+    if (!SendTake(mem, expectedSize))
+        fail("can't send Take()");
+
+    return true;
+}
+
+
+} 
+} 
