9fe33eebd176d24d66db7fa8d58ecf09a7eb9c4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:46:39 +0000

    bug 392748 (talos should use PyYAML instead of Syck to read config file) r=rhelmer/anodelman a=myk
    UltraBlame original commit: 958e0bb437971ba91b41679eecba1dca85cef858

--- testing/performance/talos/ts.py
+++ testing/performance/talos/ts.py
@@ -137,7 +137,7 @@ def RunStartupTests(profile_configs, num_runs):
     profile_dir = ffprofile.CreateTempProfileDir(config[5],
                                                  config[0],
                                                  config[1])
-    utils.debug("temp profile dir created")
+    utils.debug("temp profile dir " + profile_dir + " created")
     
     
     
