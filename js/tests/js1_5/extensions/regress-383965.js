4b7b6bf573043d75aee92880fe636d5ef4a08722
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:41:50 +0000

    JavaScript Test - regression test for bug 383965, by Jesse Ruderman
    UltraBlame original commit: 741c19d9394d06141c7ae171b67dbcdef40b42c6

---
+++ js/tests/js1_5/extensions/regress-383965.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var gTestfile = 'regress-383965.js';
+
+var BUGNUMBER = 383965;
+var summary = 'getter function with toSource';
+var actual = '';
+var expect = '';
+
+printBugNumber(BUGNUMBER);
+printStatus (summary);
+ 
+expect = /({get aaa :{}})|({aaa:{prototype:{}}})/;
+
+getter function aaa(){};
+var obj = {};
+var gett = this.__lookupGetter__("aaa");
+gett.__proto__ = obj;
+obj.__defineGetter__("aaa", gett);
+actual = obj.toSource();
+
+reportMatch(expect, actual, summary);
