d07e3f55a15946b31d16366e23007ae96dec493e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 15:11:04 +0000

    Remove setFileTypeAndCreatorFromMIMEType and setFileTypeAndCreatorFromExtension from nsILocalFileMac. b=491099 sr=roc
    UltraBlame original commit: d51bc20e79bf6190d63f5eadac1f4e7ea2285bad

--- embedding/components/webbrowserpersist/src/nsWebBrowserPersist.cpp
+++ embedding/components/webbrowserpersist/src/nsWebBrowserPersist.cpp
@@ -42,9 +42,6 @@
 
 #include "nsIFileStreams.h"       
 
-#ifdef XP_MAC
-#include "nsILocalFileMac.h"
-#endif
 #ifdef XP_OS2
 #include "nsILocalFileOS2.h"
 #endif
@@ -2246,15 +2243,6 @@ nsWebBrowserPersist::CalculateAndAppendFileExt(nsIURI *aURI, nsIChannel *aChanne
             }
 
         }
-
-#ifdef  XP_MAC
-        
-        nsCOMPtr<nsILocalFileMac> macFile(do_QueryInterface(localFile));
-        if (macFile)
-        {
-            macFile->SetFileTypeAndCreatorFromMIMEType(contentType.get());
-        }
-#endif            
     }
 
     return NS_OK;
