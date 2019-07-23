38523d92245c822f551a76ae73872d86f927b615
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 13:59:01 +0000

    Bug 459762 - 'Workers: Support synchronous XHR'. r+sr=sicking.
    UltraBlame original commit: e38f0e77d4375c5a3f35d8278ae42d53ae6d20d2

--- dom/src/threads/nsDOMWorkerXHRProxy.h
+++ dom/src/threads/nsDOMWorkerXHRProxy.h
@@ -57,15 +57,18 @@ class nsIThread;
 class nsIVariant;
 class nsIXMLHttpRequest;
 class nsDOMWorkerXHREvent;
+class nsDOMWorkerXHRFinishSyncXHRRunnable;
 class nsDOMWorkerXHRWrappedListener;
 class nsXMLHttpRequest;
 
-class nsDOMWorkerXHRProxy : public nsRunnable,
+class nsDOMWorkerXHRProxy : public nsIRunnable,
                             public nsIDOMEventListener,
                             public nsIRequestObserver
 {
   friend class nsDOMWorkerXHRAttachUploadListenersRunnable;
   friend class nsDOMWorkerXHREvent;
+  friend class nsDOMWorkerXHRFinishSyncXHRRunnable;
+  friend class nsDOMWorkerXHRLastProgressOrLoadEvent;
   friend class nsDOMWorkerXHR;
   friend class nsDOMWorkerXHRUpload;
 
@@ -80,7 +83,7 @@ class nsDOMWorkerXHRProxy : public nsRunnable,
 public:
   typedef nsAutoTArray<nsCOMPtr<nsIRunnable>, 5> SyncEventQueue;
 
-  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_ISUPPORTS
   NS_DECL_NSIDOMEVENTLISTENER
   NS_DECL_NSIRUNNABLE
   NS_DECL_NSIREQUESTOBSERVER
@@ -92,13 +95,13 @@ public:
 
   nsIXMLHttpRequest* GetXMLHttpRequest();
 
-  nsresult Abort();
-
   nsresult OpenRequest(const nsACString& aMethod,
                        const nsACString& aUrl,
                        PRBool aAsync,
                        const nsAString& aUser,
                        const nsAString& aPassword);
+
+  nsresult Abort();
 
   SyncEventQueue* SetSyncEventQueue(SyncEventQueue* aQueue);
 
@@ -112,6 +115,7 @@ protected:
 
   nsresult Destroy();
 
+  void AddRemoveXHRListeners(PRBool aAdd);
   void FlipOwnership();
 
   nsresult AddEventListener(PRUint32 aType,
@@ -151,6 +155,14 @@ protected:
   nsresult OverrideMimeType(const nsACString& aMimetype);
   nsresult GetMultipart(PRBool* aMultipart);
   nsresult SetMultipart(PRBool aMultipart);
+  nsresult GetWithCredentials(PRBool* aWithCredentials);
+  nsresult SetWithCredentials(PRBool aWithCredentials);
+
+  nsresult RunSyncEventLoop();
+
+  
+  
+  PRBool HasListenersForType(PRUint32 aType, nsIDOMEvent* aEvent = nsnull);
 
   
   nsDOMWorkerXHR* mWorkerXHR;
@@ -165,6 +177,7 @@ protected:
   nsCOMPtr<nsIThread> mMainThread;
 
   nsRefPtr<nsDOMWorkerXHREvent> mLastXHREvent;
+  nsRefPtr<nsDOMWorkerXHREvent> mLastProgressOrLoadEvent;
 
   nsTArray<ListenerArray> mXHRListeners;
   nsTArray<WrappedListener> mXHROnXListeners;
@@ -177,10 +190,18 @@ protected:
   PRInt32 mChannelID;
 
   
+  nsCOMPtr<nsIThread> mSyncXHRThread;
+
+  
+  nsRefPtr<nsDOMWorkerXHRFinishSyncXHRRunnable> mSyncFinishedRunnable;
+
+  
   PRPackedBool mOwnedByXHR;
 
   PRPackedBool mWantUploadListeners;
   PRPackedBool mCanceled;
+
+  PRPackedBool mSyncRequest;
 };
 
 #endif 
