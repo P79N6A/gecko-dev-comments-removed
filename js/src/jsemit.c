016ca51644d43a6a04a7117c050e988ef9911f68
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-23 12:42:07 +0000

    Fix recent regression in CheckSideEffects for paren-expr case (384680, r=mrbkap).
    UltraBlame original commit: 79dfe5baee71d281f49467653af55ccb866c0b1b

--- js/src/jsemit.c
+++ js/src/jsemit.c
@@ -2228,15 +2228,12 @@ CheckSideEffects(JSContext *cx, JSTreeContext *tc, JSParseNode *pn,
         break;
 
       case PN_UNARY:
-        if (pn->pn_type == TOK_INC || pn->pn_type == TOK_DEC ||
-            pn->pn_type == TOK_THROW ||
-#if JS_HAS_GENERATORS
-            pn->pn_type == TOK_YIELD ||
-#endif
-            pn->pn_type == TOK_DEFSHARP) {
-            
-            *answer = JS_TRUE;
-        } else if (pn->pn_type == TOK_DELETE) {
+        switch (pn->pn_type) {
+          case TOK_RP:
+            ok = CheckSideEffects(cx, tc, pn->pn_kid, answer);
+            break;
+
+          case TOK_DELETE:
             pn2 = pn->pn_kid;
             switch (pn2->pn_type) {
               case TOK_NAME:
@@ -2255,12 +2252,17 @@ CheckSideEffects(JSContext *cx, JSTreeContext *tc, JSParseNode *pn,
                 ok = CheckSideEffects(cx, tc, pn2, answer);
                 break;
             }
-        } else {
+            break;
+
+          default:
             
 
 
 
+
+
             *answer = JS_TRUE;
+            break;
         }
         break;
 
