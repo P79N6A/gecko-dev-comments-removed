6c094398b58b48f33750e47726c10e588441cc03
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:00:57 +0000

    Bug 620335 - TM: "Assertion failure: !argsobj.getPrivate()" leaving the trace JIT in strict mode code when an arguments object has been created.  r=luke
    UltraBlame original commit: 37ebdb3e7617a84ccd2f83517514eb79f812bc31

---
+++ js/src/jit-test/tests/arguments/strict-args-flushstack.js
@@ -0,0 +1,27 @@
+
+
+
+
+var args;
+
+function test()
+{
+  "use strict";
+  eval("args = arguments;");
+  var a = [];
+  for (var i = 0; i < RUNLOOP; i++)
+    a.push(arguments);
+  return a;
+}
+
+var a = test();
+
+assertEq(Array.isArray(a), true);
+assertEq(a.length, RUNLOOP);
+
+var count = 0;
+a.forEach(function(v, i) { count++; assertEq(v, args); });
+assertEq(count, RUNLOOP);
+
+assertEq(Object.prototype.toString.call(args), "[object Arguments]");
+assertEq(args.length, 0);
