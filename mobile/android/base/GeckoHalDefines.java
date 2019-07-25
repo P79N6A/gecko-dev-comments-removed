3652fa390a442ba347e91e10270bdff5d648fd66
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:55:59 +0000

    Bug 734324 - implement device motion - rotation rate and acceleration. r=jdm
    UltraBlame original commit: fdb8a781820a40e2861c18a647b4706830063237

---
+++ mobile/android/base/GeckoHalDefines.java
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package org.mozilla.gecko;
+
+public class GeckoHalDefines
+{
+    
+
+
+    public static final int SENSOR_ORIENTATION = 0;
+    public static final int SENSOR_ACCELERATION = 1;
+    public static final int SENSOR_PROXIMITY = 2;
+    public static final int SENSOR_LINEAR_ACCELERATION = 3;
+    public static final int SENSOR_GYROSCOPE = 4;
+};
