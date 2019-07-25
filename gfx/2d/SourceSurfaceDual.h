151725a5010c0f3bc96dfe9523c968b65af35bee
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:01:38 +0000

    Bug 740815 - Part 1: Add DrawTargetDual to Azure for Component Alpha drawing. r=jrmuizel
    UltraBlame original commit: 3dd886d95c55d36646d5a284fa399e3bc8290649

---
+++ gfx/2d/SourceSurfaceDual.h
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+#ifndef MOZILLA_GFX_SOURCESURFACEDUAL_H_
+#define MOZILLA_GFX_SOURCESURFACEDUAL_H_
+     
+#include "2D.h"
+     
+namespace mozilla {
+namespace gfx {
+
+class DualSurface;
+class DualPattern;
+
+class SourceSurfaceDual : public SourceSurface
+{
+public:
+  SourceSurfaceDual(DrawTarget *aDTA, DrawTarget *aDTB)
+    : mA(aDTA->Snapshot())
+    , mB(aDTB->Snapshot())
+  { }
+
+  virtual SurfaceType GetType() const { return SURFACE_DUAL_DT; }
+  virtual IntSize GetSize() const { return mA->GetSize(); }
+  virtual SurfaceFormat GetFormat() const { return mA->GetFormat(); }
+
+  
+  virtual TemporaryRef<DataSourceSurface> GetDataSurface() { return NULL; }
+private:
+  friend class DualSurface;
+  friend class DualPattern;
+
+  RefPtr<SourceSurface> mA;
+  RefPtr<SourceSurface> mB;
+};
+
+}
+}
+
+#endif 
