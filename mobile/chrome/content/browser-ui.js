dcfff7898983d83268fb0f1110aadfad05fa5210
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:04:58 +0000

    Bug 639754 - Remove MOZ_IPC checks (mobile bustage fix). r=mfinkle
    UltraBlame original commit: f3b02ea8cbf16139b27fe7b5528bf2afa8d947ae

--- mobile/chrome/content/browser-ui.js
+++ mobile/chrome/content/browser-ui.js
@@ -508,11 +508,9 @@ var BrowserUI = {
       DownloadsView.init();
       ConsoleView.init();
 
-#ifdef MOZ_IPC
       
       Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime)
           .ensureContentProcess();
-#endif
 
 #ifdef MOZ_SERVICES_SYNC
       
