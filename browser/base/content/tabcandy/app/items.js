26ce1129fa0e630047329ce96b80572fe90852bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:20:21 +0000

    + Group and TabItem now inherit from Item (currently the pushAway routine and isAnItem property) + Group._children is now an array of Items (rather than DOM elements) + Groups and tabs now push other items away when they're moved, and groups also do so when resized + The Groups object now keeps an array of groups. This is more robust than counting DOM elements, for instance when a group is fading away (you want to act like it doesn't exist, even though its DOM element is still on the screen) + Documented the Item interface in items.js + lots of miscellaneous refinements to the code
    UltraBlame original commit: 0248276eb0e921fb29a233ef4af6d99213ad74a8

--- browser/base/content/tabcandy/app/items.js
+++ browser/base/content/tabcandy/app/items.js
@@ -1,5 +1,95 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.Item = function() {
+  this.isAnItem = true;
+};
+
+window.Item.prototype = { 
+  
+  pushAway: function() {
+    var buffer = 10;
+    
+    var items = Items.getTopLevelItems();
+    $.each(items, function(index, item) {
+      var data = {};
+      data.bounds = item.getBounds();
+      data.startBounds = new Rect(data.bounds);
+      data.generation = Infinity;
+      item.pushAwayData = data;
+    });
+    
+    var itemsToPush = [this];
+    this.pushAwayData.generation = 0;
+
+    var pushOne = function(baseItem) {
+      var baseData = baseItem.pushAwayData;
+      var bb = new Rect(baseData.bounds);
+      bb.inset(-buffer, -buffer);
+      var bbc = bb.center();
+    
+      $.each(items, function(index, item) {
+        if(item == baseItem)
+          return;
+          
+        var data = item.pushAwayData;
+        if(data.generation <= baseData.generation)
+          return;
+          
+        var bounds = data.bounds;
+        var box = new Rect(bounds);
+        box.inset(-buffer, -buffer);
+        if(box.intersects(bb)) {
+          var offset = new Point();
+          var center = box.center(); 
+          if(Math.abs(center.x - bbc.x) < Math.abs(center.y - bbc.y)) {
+            if(center.y > bbc.y)
+              offset.y = bb.bottom - box.top; 
+            else
+              offset.y = bb.top - box.bottom;
+          } else {
+            if(center.x > bbc.x)
+              offset.x = bb.right - box.left; 
+            else
+              offset.x = bb.left - box.right;
+          }
+          
+          bounds.offset(offset); 
+          data.generation = baseData.generation + 1;
+          itemsToPush.push(item);
+        }
+      });
+    };   
+    
+    while(itemsToPush.length)
+      pushOne(itemsToPush.shift());         
+
+    $.each(items, function(index, item) {
+      var data = item.pushAwayData;
+      if(!data.bounds.equals(data.startBounds))
+        item.setPosition(data.bounds.left, data.bounds.top);
+    });
+  }
+};  
+
+
 window.Items = {
+  
+  item: function(el) {
+    return $(el).data('item');
+  },
+  
   
   getTopLevelItems: function() {
     var items = [];
