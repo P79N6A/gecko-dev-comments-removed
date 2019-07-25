d11ca4c23678244194dd15d95c6576f4eea69249
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:48:09 +0000

    Back out 8fbb8ff7a712 (bug 627692) on suspicion of causing orange
    UltraBlame original commit: 42bd70d77a863cbb6de76316d4dda13214db3d45

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -6807,7 +6807,6 @@ LeaveTree(TraceMonitor *tm, TracerState& state, VMSideExit* lr)
 
 
 
-
         if (!(bs & BUILTIN_ERROR)) {
             
 
@@ -7324,7 +7323,6 @@ TraceRecorder::monitorRecording(JSOp op)
     if (pendingGuardCondition) {
         LIns* cond = pendingGuardCondition;
         bool expected = true;
-
         
         ensureCond(&cond, &expected);
         guard(expected, cond, STATUS_EXIT);
@@ -8007,21 +8005,6 @@ DeepBail(JSContext *cx)
     state->builtinStatus |= BUILTIN_BAILED;
 
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -14880,10 +14863,6 @@ TraceRecorder::record_JSOP_MOREITER()
 
     cond_ins = is_boxed_true(AllocSlotsAddress(vp_ins));
     stack(0, cond_ins);
-
-    
-    
-    stack(-1, iterobj_ins);
 
     return ARECORD_CONTINUE;
 }
