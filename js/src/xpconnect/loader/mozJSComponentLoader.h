ade3dbf1690bb58a91566247364f0727fec6ee6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:29:21 +0000

    Bug 592930 - TM: set right compartment for mozJSComponentLoader::ModuleEntry::Clear r=mrbkap
    UltraBlame original commit: 70bc9f81ccf5cebc101763823bb269fcfaa6f48d

--- js/src/xpconnect/loader/mozJSComponentLoader.h
+++ js/src/xpconnect/loader/mozJSComponentLoader.h
@@ -39,12 +39,11 @@
 
 #include "plhash.h"
 #include "jsapi.h"
-#include "nsIModuleLoader.h"
+#include "mozilla/ModuleLoader.h"
 #include "nsIJSRuntimeService.h"
 #include "nsIJSContextStack.h"
 #include "nsISupports.h"
 #include "nsIXPConnect.h"
-#include "nsIModule.h"
 #include "nsIFile.h"
 #include "nsAutoPtr.h"
 #include "nsIFastLoadService.h"
@@ -58,6 +57,13 @@
 #ifndef XPCONNECT_STANDALONE
 #include "nsIPrincipal.h"
 #endif
+#ifdef MOZ_ENABLE_LIBXUL
+#include "mozilla/scache/StartupCache.h"
+
+using namespace mozilla::scache;
+#endif
+
+#include "xpcIJSGetFactory.h"
 
 
 
@@ -65,7 +71,6 @@
   {0x6bd13476, 0x1dd2, 0x11b2, \
     { 0xbb, 0xef, 0xf0, 0xcc, 0xb5, 0xfa, 0x64, 0xb6 }}
 #define MOZJSCOMPONENTLOADER_CONTRACTID "@mozilla.org/moz/jsloader;1"
-#define MOZJSCOMPONENTLOADER_TYPE_NAME "text/javascript"
 
 
 class nsXPCFastLoadIO : public nsIFastLoadFileIO
@@ -89,19 +94,23 @@ class nsXPCFastLoadIO : public nsIFastLoadFileIO
 };
 
 
-class mozJSComponentLoader : public nsIModuleLoader,
+class mozJSComponentLoader : public mozilla::ModuleLoader,
                              public xpcIJSModuleLoader,
                              public nsIObserver
 {
     friend class JSCLContextHelper;
  public:
     NS_DECL_ISUPPORTS
-    NS_DECL_NSIMODULELOADER
     NS_DECL_XPCIJSMODULELOADER
     NS_DECL_NSIOBSERVER
 
     mozJSComponentLoader();
     virtual ~mozJSComponentLoader();
+
+    
+    const mozilla::Module* LoadModule(nsILocalFile* aFile);
+    const mozilla::Module* LoadModuleFromJAR(nsILocalFile* aJARFile,
+                                             const nsACString& aPath);
 
  protected:
     static mozJSComponentLoader* sSelf;
@@ -109,65 +118,90 @@ class mozJSComponentLoader : public nsIModuleLoader,
     nsresult ReallyInit();
     void UnloadModules();
 
-    nsresult GlobalForLocation(nsILocalFile *aComponent,
+    nsresult FileKey(nsILocalFile* aFile, nsAString &aResult);
+    nsresult JarKey(nsILocalFile* aFile,
+                    const nsACString& aComponentPath,
+                    nsAString &aResult);
+
+    const mozilla::Module* LoadModuleImpl(nsILocalFile* aSourceFile,
+                                          nsAString &aKey,
+                                          nsIURI* aComponentURI);
+
+    nsresult GlobalForLocation(nsILocalFile* aComponentFile,
+                               nsIURI *aComponent,
                                JSObject **aGlobal,
                                char **location,
                                jsval *exception);
 
-    nsresult StartFastLoad(nsIFastLoadService *flSvc);
-    nsresult ReadScript(nsIFastLoadService *flSvc, const char *nativePath,
-                        nsIURI *uri, JSContext *cx, JSScript **script);
-    nsresult WriteScript(nsIFastLoadService *flSvc, JSScript *script,
-                         nsIFile *component, const char *nativePath,
-                         nsIURI *uri, JSContext *cx);
-    static void CloseFastLoad(nsITimer *timer, void *closure);
-    void CloseFastLoad();
+#ifdef MOZ_ENABLE_LIBXUL
+    nsresult ReadScript(StartupCache *cache, nsIURI *uri, 
+                        JSContext *cx, JSScript **script);
+    nsresult WriteScript(StartupCache *cache, JSScript *script,
+                         nsIFile *component, nsIURI *uri, JSContext *cx);
+#endif
 
     nsCOMPtr<nsIComponentManager> mCompMgr;
     nsCOMPtr<nsIJSRuntimeService> mRuntimeService;
     nsCOMPtr<nsIThreadJSContextStack> mContextStack;
-    nsCOMPtr<nsIFile> mFastLoadFile;
-    nsRefPtr<nsXPCFastLoadIO> mFastLoadIO;
-    nsCOMPtr<nsIObjectInputStream> mFastLoadInput;
-    nsCOMPtr<nsIObjectOutputStream> mFastLoadOutput;
-    nsCOMPtr<nsITimer> mFastLoadTimer;
 #ifndef XPCONNECT_STANDALONE
     nsCOMPtr<nsIPrincipal> mSystemPrincipal;
 #endif
     JSRuntime *mRuntime;
     JSContext *mContext;
 
-    class ModuleEntry
+    class ModuleEntry : public mozilla::Module
     {
     public:
-        ModuleEntry() {
+        ModuleEntry() : mozilla::Module() {
+            mVersion = mozilla::Module::kVersion;
+            mCIDs = NULL;
+            mContractIDs = NULL;
+            mCategoryEntries = NULL;
+            getFactoryProc = GetFactory;
+            loadProc = NULL;
+            unloadProc = NULL;
+
             global = nsnull;
             location = nsnull;
         }
 
         ~ModuleEntry() {
-            module = nsnull;
+            Clear();
+        }
+
+        void Clear() {
+            getfactoryobj = NULL;
 
             if (global) {
                 JSAutoRequest ar(sSelf->mContext);
+                JSAutoEnterCompartment ac(sSelf->mContext, global);
                 JS_ClearScope(sSelf->mContext, global);
                 JS_RemoveObjectRoot(sSelf->mContext, &global);
             }
 
             if (location)
                 NS_Free(location);
+
+            global = NULL;
+            location = NULL;
         }
 
-        nsCOMPtr<nsIModule>  module;
+        static already_AddRefed<nsIFactory> GetFactory(const mozilla::Module& module,
+                                                       const mozilla::Module::CIDEntry& entry);
+
+        nsCOMPtr<xpcIJSGetFactory> getfactoryobj;
         JSObject            *global;
         char                *location;
     };
 
     friend class ModuleEntry;
 
-    nsClassHashtable<nsHashableHashKey, ModuleEntry> mModules;
-    nsClassHashtable<nsHashableHashKey, ModuleEntry> mImports;
-    nsDataHashtable<nsHashableHashKey, ModuleEntry*> mInProgressImports;
+    
+    static PLDHashOperator ClearModules(const nsAString& key, ModuleEntry*& entry, void* cx);
+    nsDataHashtable<nsStringHashKey, ModuleEntry*> mModules;
+
+    nsClassHashtable<nsStringHashKey, ModuleEntry> mImports;
+    nsDataHashtable<nsStringHashKey, ModuleEntry*> mInProgressImports;
 
     PRBool mInitialized;
 };
