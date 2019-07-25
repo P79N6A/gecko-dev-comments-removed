d7b76f0c61080a7e1f3a2b80028004dafa535286
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:04:46 +0000

    Bug 634666 - Use DEFER_OPEN in m-b [r=mfinkle]
    UltraBlame original commit: e5f360ab151b91277608bd5b29825580e0c1d970

--- mobile/components/SessionStore.js
+++ mobile/components/SessionStore.js
@@ -497,7 +497,7 @@ SessionStore.prototype = {
 
     
     let ostream = Cc["@mozilla.org/network/safe-file-output-stream;1"].createInstance(Ci.nsIFileOutputStream);
-    ostream.init(aFile, 0x02 | 0x08 | 0x20, 0600, 0);
+    ostream.init(aFile, 0x02 | 0x08 | 0x20, 0600, ostream.DEFER_OPEN);
 
     
     let converter = Cc["@mozilla.org/intl/scriptableunicodeconverter"].createInstance(Ci.nsIScriptableUnicodeConverter);
