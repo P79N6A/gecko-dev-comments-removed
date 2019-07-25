cf002db9a50c35d543c8fb7f5775f62f05243044
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:28:31 +0000

    Bug 668151 - Bug 515492 (VS2010 jemalloc support) doesn't work on Win64. r=khuey
    UltraBlame original commit: d67a8f7bfc0fca74dafcd25584edb19d570193b4

--- memory/jemalloc/fixcrt.py
+++ memory/jemalloc/fixcrt.py
@@ -40,5 +40,8 @@ from __future__ import with_statement
 with open('crtdll.obj', 'rb') as infile:
   data = infile.read()
   with open('crtdll_fixed.obj', 'wb') as outfile:
+    
     data = data.replace('__imp__free', '__imp__frex')
+    
+    data = data.replace('__imp_free', '__imp_frex')
     outfile.write(data)
