f0cb75e629ca430f6854e60a748ea3a65a699063
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:39:52 +0000

    [INFER] Unwind getProto() changes outside of JS, fix test failures, bug 619271.
    UltraBlame original commit: 0a97e6a40d5cc6fe5ba4592996068d43966d80df

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -1642,14 +1642,14 @@ static const JSClass *sObjectClass = nsnull;
 
 
 static void
-FindObjectClass(JSContext *cx, JSObject* aGlobalObject)
+FindObjectClass(JSObject* aGlobalObject)
 {
   NS_ASSERTION(!sObjectClass,
                "Double set of sObjectClass");
   JSObject *obj, *proto = aGlobalObject;
   do {
     obj = proto;
-    proto = JS_GetPrototype(cx, obj);
+    proto = obj->getProto();
   } while (proto);
 
   sObjectClass = obj->getJSClass();
@@ -4661,7 +4661,7 @@ nsDOMClassInfo::PostCreatePrototype(JSContext * cx, JSObject * proto)
   
   
   if (!sObjectClass) {
-    FindObjectClass(cx, proto);
+    FindObjectClass(proto);
     NS_ASSERTION(sObjectClass && !strcmp(sObjectClass->name, "Object"),
                  "Incorrect object class!");
   }
@@ -6956,7 +6956,7 @@ nsWindowSH::NewResolve(nsIXPConnectWrappedNative *wrapper, JSContext *cx,
     wrapper->GetJSObject(&realObj);
 
     if (obj == realObj) {
-      JSObject *proto = JS_GetPrototype(cx, obj);
+      JSObject *proto = obj->getProto();
       if (proto) {
         JSObject *pobj = NULL;
         jsval val;
@@ -8652,7 +8652,7 @@ nsHTMLDocumentSH::DocumentAllGetProperty(JSContext *cx, JSObject *obj,
   }
 
   while (obj->getJSClass() != &sHTMLDocumentAllClass) {
-    obj = JS_GetPrototype(cx, obj);
+    obj = obj->getProto();
 
     if (!obj) {
       NS_ERROR("The JS engine lies!");
