ed1a3d8a9b1003dad5e9a652a1c62d42f78926f3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:24:14 +0000

    Do the necessary reflow when our font inflation data changes.  (Bug 759755)  r=roc
    UltraBlame original commit: 30e441f7ad9d7b73271fcfd147eaac74a255c5cb

--- layout/generic/nsHTMLReflowState.cpp
+++ layout/generic/nsHTMLReflowState.cpp
@@ -305,12 +305,6 @@ nsHTMLReflowState::Init(nsPresContext* aPresContext,
                    "have unconstrained width; this should only result from "
                    "very large sizes, not attempts at intrinsic width "
                    "calculation");
-
-  if (frame->GetStateBits() & NS_FRAME_FONT_INFLATION_FLOW_ROOT) {
-    
-    
-    nsFontInflationData::UpdateFontInflationDataWidthFor(*this);
-  }
 }
 
 void nsHTMLReflowState::InitCBReflowState()
@@ -364,6 +358,20 @@ IsQuirkContainingBlockHeight(const nsHTMLReflowState* rs, nsIAtom* aFrameType)
 void
 nsHTMLReflowState::InitResizeFlags(nsPresContext* aPresContext, nsIAtom* aFrameType)
 {
+  if (frame->GetStateBits() & NS_FRAME_FONT_INFLATION_FLOW_ROOT) {
+    
+    
+    bool dirty = nsFontInflationData::UpdateFontInflationDataWidthFor(*this);
+    if (dirty) {
+      
+      
+      
+      
+      
+      frame->AddStateBits(NS_FRAME_IS_DIRTY);
+    }
+  }
+
   mFlags.mHResize = !(frame->GetStateBits() & NS_FRAME_IS_DIRTY) &&
                     frame->GetSize().width !=
                       mComputedWidth + mComputedBorderPadding.LeftRight();
