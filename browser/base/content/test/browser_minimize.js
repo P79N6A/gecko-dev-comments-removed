6d2f43ed09fd106033e23eae87abaf299a015f11
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:30:13 +0000

    Bug 648045.  Mark the currently selected tab in a minimized window as inactive.  r=gavin,smaug
    UltraBlame original commit: 5decde435e4a768f3a9cc785556ecb245ebdc927

---
+++ browser/base/content/test/browser_minimize.js
@@ -0,0 +1,33 @@
+
+
+function waitForActive() {
+    if (!gBrowser.docShell.isActive) {
+        executeSoon(waitForActive);
+        return;
+    }
+    is(gBrowser.docShell.isActive, true, "Docshell should be active again");
+    finish();
+}
+
+function waitForInactive() {
+    if (gBrowser.docShell.isActive) {
+        executeSoon(waitForInactive);
+        return;
+    }
+    is(gBrowser.docShell.isActive, false, "Docshell should be inactive");
+    window.restore();
+    waitForActive();
+}
+
+function test() {
+    waitForExplicitFinish();
+    is(gBrowser.docShell.isActive, true, "Docshell should be active");
+    window.minimize();
+    
+    
+    
+    
+    
+    
+    waitForInactive();
+}
