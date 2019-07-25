7d31362e6640b511d390a7406a7129b5364f9457
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:50:54 +0000

    Bug 587910 - SVG SMIL accessKey timing, r=dholbert,smaug; sr=roc; a=roc
    UltraBlame original commit: a249d862417717083024524585fa7c1846a4fc0e

--- layout/reftests/svg/smil/event/event-util.js
+++ layout/reftests/svg/smil/event/event-util.js
@@ -1,21 +1,37 @@
 
 
-function delayedSnapshot(seekTimeInSeconds) {
+function delayedSnapshot(seekTimeInSeconds)
+{
   
   window.setTimeout(finish, 10, seekTimeInSeconds);
 }
 
-function finish(seekTimeInSeconds) {
+function finish(seekTimeInSeconds)
+{
   document.documentElement.pauseAnimations();
   if (seekTimeInSeconds)
     document.documentElement.setCurrentTime(seekTimeInSeconds);
   document.documentElement.removeAttribute("class");
 }
 
-function click(targetId) {
+function click(targetId)
+{
   var evt = document.createEvent("MouseEvents");
   evt.initMouseEvent("click", true, true, window,
     0, 0, 0, 0, 0, false, false, false, false, 0, null);
   var target = document.getElementById(targetId);
   target.dispatchEvent(evt);
+}
+
+function keypress(charCode)
+{
+  var evt = document.createEvent("KeyboardEvent");
+  evt.initKeyEvent("keypress", true, true, window,
+                   false, 
+                   false, 
+                   false, 
+                   false, 
+                   0,     
+                   charCode);
+  document.documentElement.dispatchEvent(evt);
 }
