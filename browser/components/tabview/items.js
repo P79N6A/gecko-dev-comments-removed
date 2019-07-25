49d5e57735f8a6a9303892e45e9698fb760872ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:32:36 +0000

    Bug 772463 - Unprefix CSS3 transforms in browser/. r=dao
    UltraBlame original commit: 0507d42be36a1d9dd9d682d21df110e2284c6348

--- browser/components/tabview/items.js
+++ browser/components/tabview/items.js
@@ -254,7 +254,7 @@ Item.prototype = {
   
   setRotation: function Item_setRotation(degrees) {
     var value = degrees ? "rotate(%deg)".replace(/%/, degrees) : null;
-    iQ(this.container).css({"-moz-transform": value});
+    iQ(this.container).css({"transform": value});
   },
 
   
