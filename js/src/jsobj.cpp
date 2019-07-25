bfb3df768f18c42fc6e2eb43259eb7d3fd461c57
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:39:18 +0000

    Preserve types when swapping objects, bug 774953. r=dvander
    UltraBlame original commit: d29b79c37f86cea741af162866002b26f72aada1

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -2897,6 +2897,14 @@ JSObject::ReserveForTradeGuts(JSContext *cx, JSObject *a, JSObject *b,
 
 
 
+    
+
+
+
+    RootedObject na(cx, a), aProto(cx, a->getProto()), nb(cx, b), bProto(cx, b->getProto());
+    if (!SetProto(cx, na, bProto, false) || !SetProto(cx, nb, aProto, false))
+        return false;
+
     if (a->sizeOfThis() == b->sizeOfThis())
         return true;
 
@@ -3111,6 +3119,14 @@ JSObject::TradeGuts(JSContext *cx, JSObject *a, JSObject *b, TradeGutsReserved &
         a->lastProperty()->listp = &a->shape_;
     if (b->inDictionaryMode())
         b->lastProperty()->listp = &b->shape_;
+
+    
+
+
+
+    TypeObject *tmp = a->type_;
+    a->type_ = b->type_;
+    b->type_ = tmp;
 }
 
 
@@ -3736,7 +3752,6 @@ bool
 SetProto(JSContext *cx, HandleObject obj, HandleObject proto, bool checkForCycles)
 {
     JS_ASSERT_IF(!checkForCycles, obj != proto);
-    JS_ASSERT(obj->isExtensible());
 
 #if JS_HAS_XML_SUPPORT
     if (proto && proto->isXML()) {
