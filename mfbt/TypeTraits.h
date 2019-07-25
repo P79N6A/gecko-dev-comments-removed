8dc4c44c91d29cc7f558bbfeff44430aa04da605
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:26:14 +0000

    Fix a couple style nits missed during review.  Followup to bug 616262, r=sparky
    UltraBlame original commit: a112cf3d055d8930f8bdb7f40b8537cc26bab787

--- mfbt/TypeTraits.h
+++ mfbt/TypeTraits.h
@@ -2,10 +2,10 @@
 
 
 
+
+
 #ifndef mozilla_TypeTraits_h_
 #define mozilla_TypeTraits_h_
-
-
 
 namespace mozilla {
 
@@ -21,14 +21,14 @@ namespace mozilla {
 
 
 
-template <class Base, class Derived>
+template<class Base, class Derived>
 class IsBaseOf
 {
   private:
-    static char Test(Base *);
-    static int Test(...);
+    static char test(Base* b);
+    static int test(...);
   public:
-    static const bool value = (sizeof(Test(static_cast<Derived *>(0))) == sizeof(char));
+    static const bool value = (sizeof(test(static_cast<Derived*>(0))) == sizeof(char));
 };
 
 
@@ -37,13 +37,13 @@ class IsBaseOf
 
 
 
-template <bool condition, class A, class B>
+template<bool condition, class A, class B>
 struct Conditional
 {
     typedef A Type;
 };
 
-template <class A, class B>
+template<class A, class B>
 struct Conditional<false, A, B>
 {
     typedef B Type;
