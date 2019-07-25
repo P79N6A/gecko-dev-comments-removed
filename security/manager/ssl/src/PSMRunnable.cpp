44de7493d5e722cd2085dabd5dfd5938defa247f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:57:32 +0000

    Bug 675221: Remove XPCOM Proxies: Implement Runnable that can be waited on synchronously, r=bsmedberg
    UltraBlame original commit: 871bfac06cf13645c2180498a94a310e694a5840

---
+++ security/manager/ssl/src/PSMRunnable.cpp
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+#include "PSMRunnable.h"
+ 
+namespace mozilla { namespace psm {
+
+SyncRunnableBase::SyncRunnableBase()
+  : monitor("SyncRunnableBase::monitor")
+{
+}
+
+nsresult
+SyncRunnableBase::DispatchToMainThreadAndWait()
+{
+  NS_ASSERTION(!NS_IsMainThread(),
+               "DispatchToMainThreadAndWait called on the main thread.");
+
+  mozilla::MonitorAutoLock lock(monitor);
+  nsresult rv = NS_DispatchToMainThread(this);
+  if (NS_SUCCEEDED(rv)) {
+    lock.Wait();
+  }
+  return rv;
+}
+
+NS_IMETHODIMP
+SyncRunnableBase::Run()
+{
+  RunOnTargetThread();
+  mozilla::MonitorAutoLock(monitor).Notify();
+  return NS_OK;
+}
+
+} } 
---
+++ security/manager/ssl/src/PSMRunnable.h
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef PSMRunnable_h
+#define PSMRunnable_h
+
+#include "mozilla/Monitor.h"
+#include "nsThreadUtils.h"
+
+namespace mozilla { namespace psm {
+
+
+
+
+class SyncRunnableBase : public nsRunnable
+{
+public:
+  NS_DECL_NSIRUNNABLE
+  nsresult DispatchToMainThreadAndWait();
+protected:
+  SyncRunnableBase();
+  virtual void RunOnTargetThread() = 0;
+private:
+  mozilla::Monitor monitor;
+};
+
+} } 
+
+#endif
