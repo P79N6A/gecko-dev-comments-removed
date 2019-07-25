ceabeacd05402c8bf76cf1ab816d19db88cf0407
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:40:57 +0000

    Bug 613722 - Do not make <embed hidden> display none. r=jst a=blocking-betaN
    UltraBlame original commit: ef449201d114a690a7502e2c96fe8ad778747ea9

--- content/html/content/src/nsGenericHTMLElement.h
+++ content/html/content/src/nsGenericHTMLElement.h
@@ -331,6 +331,15 @@ public:
 
   static void MapCommonAttributesInto(const nsMappedAttributes* aAttributes, 
                                       nsRuleData* aRuleData);
+
+  
+
+
+
+
+  static void MapCommonAttributesExceptHiddenInto(const nsMappedAttributes* aAttributes,
+                                                  nsRuleData* aRuleData);
+
   static const MappedAttributeEntry sCommonAttributeMap[];
   static const MappedAttributeEntry sImageMarginSizeAttributeMap[];
   static const MappedAttributeEntry sImageBorderAttributeMap[];
