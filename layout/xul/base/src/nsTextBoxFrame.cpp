c77e4373e82fa0afc016ce4e9ca92d37dad85ab1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:09:46 +0000

    Fix a state bit that *might* be overlapping.  (Bug 570837)  r=roc
    UltraBlame original commit: 04eaa2543795ec5b763afbccc01c83cb28475df5

--- layout/xul/base/src/nsTextBoxFrame.cpp
+++ layout/xul/base/src/nsTextBoxFrame.cpp
@@ -71,6 +71,7 @@
 #include "nsDisplayList.h"
 #include "nsCSSRendering.h"
 #include "nsIReflowCallback.h"
+#include "nsBoxFrame.h"
 
 #ifdef IBMBIDI
 #include "nsBidiUtils.h"
@@ -83,7 +84,9 @@
 #define CROP_START  "start"
 #define CROP_END    "end"
 
-#define NS_STATE_NEED_LAYOUT NS_FRAME_STATE_BIT(24)
+
+
+#define NS_STATE_NEED_LAYOUT NS_STATE_BOX_CHILD_RESERVED
 
 class nsAccessKeyInfo
 {
