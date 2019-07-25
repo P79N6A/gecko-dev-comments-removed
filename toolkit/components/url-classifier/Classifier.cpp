29fd1b94a9c3d9c9711355115c65c5d4b7888a2a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:49:56 +0000

    Bug 725597 - SafeBrowsing fails to update persistent PrefixSet on Windows. r=dcamp
    UltraBlame original commit: e9291f227d6304d0aea43a3dcb6a96ed97fdffb7

--- toolkit/components/url-classifier/Classifier.cpp
+++ toolkit/components/url-classifier/Classifier.cpp
@@ -587,7 +587,8 @@ Classifier::ApplyTableUpdates(nsTArray<TableUpdate*>* aUpdates,
 #if defined(DEBUG) && defined(PR_LOGGING)
   prefixSet->Dump();
 #endif
-  prefixSet->WriteFile();
+  rv = prefixSet->WriteFile();
+  NS_ENSURE_SUCCESS(rv, rv);
 
   
   rv = store->FinishUpdate();
