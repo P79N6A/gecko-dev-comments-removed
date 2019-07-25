80ed914b90d9e59e1db512fef3a75401d03c96a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:19:02 +0000

    bug 607946 - Allow specifying a post-upload command for uploadsymbols. r=catlee a=bsmedberg
    UltraBlame original commit: ed88ab5f4dbf9ca12a74c32d84463d83c6c20bc7

--- js/src/jit-test/tests/basic/testProxyConstructors.js
+++
@@ -1,9 +0,0 @@
-
-assertEq(new (Proxy.createFunction({}, function(){}, function(){})), undefined);
-
-x = Proxy.createFunction((function () {}), Uint16Array, wrap)
-new(wrap(x))
-
-
-var x = Proxy.createFunction({}, function (q) { return q; });
-new x(x);
--- js/src/jit-test/tests/jaeger/bug563000/trap-force-return-1.js
+++
@@ -1,7 +0,0 @@
-setDebug(true);
-function main() {
-  return "failure";
-}
-
-trap(main, 3, "'success'");
-assertEq(main(), "success");
