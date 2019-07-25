badbd169f62e36d8e2178f675f15c9306ca74843
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:04:39 +0000

    Bug 568995 - Update tests to use waitForClipboard [r=dao, r=dolske]
    UltraBlame original commit: 19fda49851ee4c500e42218d023111fd824cf45c

--- browser/base/content/test/browser_bug321000.js
+++ browser/base/content/test/browser_bug321000.js
@@ -63,62 +63,21 @@ var gTests = [
 function test() {
   waitForExplicitFinish();
 
-  
-  info("About to put a string in clipboard");
-  Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper)
-                                             .copyString(kTestString);
+  let cbHelper = Cc["@mozilla.org/widget/clipboardhelper;1"].
+                 getService(Ci.nsIClipboardHelper);
 
   
   
-  setTimeout(poll_clipboard, 100);
-}
-
-var runCount = 0;
-function poll_clipboard() {
   
-  if (++runCount > 50) {
-    
-    ok(false, "Timed out while polling clipboard for pasted data");
-    
-    finish_test();
-    return;
-  }
-
-  info("Polling clipboard cycle " + runCount);
-  var clip = Cc["@mozilla.org/widget/clipboard;1"].
-             getService(Ci.nsIClipboard);
-  var trans = Cc["@mozilla.org/widget/transferable;1"].
-              createInstance(Ci.nsITransferable);
-  trans.addDataFlavor("text/unicode");
-  var str = new Object();
-  try {
-    
-    clip.getData(trans, clip.kGlobalClipboard);
-    trans.getTransferData("text/unicode", str, {});
-    str = str.value.QueryInterface(Ci.nsISupportsString);
-  }
-  catch(ex) {}
-
-  if (kTestString == str) {
-    next_test();
-  }
-  else
-    setTimeout(poll_clipboard, 100);
+  waitForClipboard(kTestString, function() { cbHelper.copyString(kTestString); },
+                   next_test, finish);
 }
 
 function next_test() {
-  if (gTests.length) {
-    var currentTest = gTests.shift();
-    test_paste(currentTest);
-  }
-  else {
-    
-    
-    
-    Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper)
-                                               .copyString("");
+  if (gTests.length)
+    test_paste(gTests.shift());
+  else
     finish();
-  }
 }
 
 function test_paste(aCurrentTest) {
