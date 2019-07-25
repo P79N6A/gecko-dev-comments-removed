d4e2a4d1af0ebc4eac4d52c075e904e5da6194c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:39:44 +0000

    [INFER] Fix overly aggressive assert, bug 609899.
    UltraBlame original commit: 9b8ceaa548c2a0e8df8be656bc914f676ec0555e

--- js/src/jsanalyze.cpp
+++ js/src/jsanalyze.cpp
@@ -935,7 +935,7 @@ LifetimeScript::analyze(JSContext *cx, analyze::Script *analysis, JSScript *scri
                     targetOffset = targetOffset + GetJumpOffset(target, target);
                 } else {
                     
-                    JS_ASSERT(nop == JSOP_TRACE);
+                    JS_ASSERT(nop == JSOP_TRACE || nop == JSOP_NOTRACE);
                     codeArray[targetOffset].loopBackedge = offset;
                     break;
                 }
