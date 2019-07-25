54b8490c39bb3dcf387af503a2658e7d55408b1a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:02:41 +0000

    Bug 724739, Bug 724740 - Part 2: Queue folder deletions, processing them after other records; reparent children during bookmark folder deletion. r=nalexander
    UltraBlame original commit: 1c2a06f36ba3868f7ce9cd1dcf3b21007e425a2e

---
+++ mobile/android/base/sync/repositories/android/BookmarksDeletionManager.java
@@ -0,0 +1,213 @@
+
+
+
+
+package org.mozilla.gecko.sync.repositories.android;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.mozilla.gecko.db.BrowserContract;
+import org.mozilla.gecko.sync.Logger;
+import org.mozilla.gecko.sync.repositories.NullCursorException;
+import org.mozilla.gecko.sync.repositories.domain.BookmarkRecord;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class BookmarksDeletionManager {
+  private static final String LOG_TAG = "BookmarkDelete";
+
+  private final AndroidBrowserBookmarksDataAccessor dataAccessor;
+  private final int flushThreshold;
+
+  private final HashSet<String> folders    = new HashSet<String>();
+  private final HashSet<String> nonFolders = new HashSet<String>();
+  private int nonFolderCount = 0;
+
+  
+  private HashSet<String> nonFolderParents = new HashSet<String>();
+  private HashSet<String> folderParents    = new HashSet<String>();
+
+  
+
+
+
+
+
+
+
+
+
+
+  public BookmarksDeletionManager(AndroidBrowserBookmarksDataAccessor dataAccessor, int flushThreshold) {
+    this.dataAccessor = dataAccessor;
+    this.flushThreshold = flushThreshold;
+  }
+
+  public void deleteRecord(BookmarkRecord r, boolean isFolder) {
+    if (r.guid == null) {
+      Logger.warn(LOG_TAG, "Cannot queue deletion of record with no GUID.");
+      return;
+    }
+    Logger.debug(LOG_TAG, "Queuing deletion of " + r.guid);
+
+    if (isFolder) {
+      folders.add(r.guid);
+      if (!folders.contains(r.parentID)) {
+        
+        folderParents.add(r.parentID);
+      }
+
+      nonFolderParents.remove(r.guid);
+      folderParents.remove(r.guid);
+      return;
+    }
+
+    if (!folders.contains(r.parentID)) {
+      
+      nonFolderParents.add(r.parentID);
+    }
+
+    if (nonFolders.add(r.guid)) {
+      if (++nonFolderCount >= flushThreshold) {
+        deleteNonFolders();
+      }
+    }
+  }
+
+  
+
+
+  public void incrementalFlush() {
+    
+    deleteNonFolders();
+  }
+
+  
+
+
+
+
+
+
+
+
+  public Set<String> flushAll(long orphanDestination, long now) throws NullCursorException {
+    Logger.debug(LOG_TAG, "Doing complete flush of deleted items. Moving orphans to " + orphanDestination);
+    deleteNonFolders();
+
+    
+    
+    nonFolderParents.removeAll(folders);
+
+    Logger.debug(LOG_TAG, "Bumping modified times for " + nonFolderParents.size() +
+                          " parents of deleted non-folders.");
+    dataAccessor.bumpModifiedByGUID(nonFolderParents, now);
+
+    if (folders.size() > 0) {
+      final String[] folderGUIDs = folders.toArray(new String[folders.size()]);
+      final String[] folderIDs = getIDs(folderGUIDs);   
+      int moved = dataAccessor.moveChildren(folderIDs, orphanDestination);
+      if (moved > 0) {
+        dataAccessor.bumpModified(orphanDestination, now);
+      }
+
+      
+      
+      final String folderWhere = RepoUtils.computeSQLInClause(folders.size(), BrowserContract.Bookmarks.GUID);
+      dataAccessor.delete(folderWhere, folderGUIDs);
+
+      folderParents.removeAll(folders);
+      Logger.debug(LOG_TAG, "Bumping modified times for " + folderParents.size() +
+                            " parents of deleted folders.");
+      dataAccessor.bumpModifiedByGUID(folderParents, now);
+
+      
+      folders.clear();
+    }
+
+    HashSet<String> ret = nonFolderParents;
+    ret.addAll(folderParents);
+
+    nonFolderParents = new HashSet<String>();
+    folderParents    = new HashSet<String>();
+    return ret;
+  }
+
+  private String[] getIDs(String[] guids) throws NullCursorException {
+    
+    String[] ids = new String[guids.length];
+    Map<String, Long> guidsToIDs = dataAccessor.idsForGUIDs(guids);
+    for (int i = 0; i < guids.length; ++i) {
+      String guid = guids[i];
+      Long id =  guidsToIDs.get(guid);
+      if (id == null) {
+        throw new IllegalArgumentException("Can't get ID for unknown record " + guid);
+      }
+      ids[i] = id.toString();
+    }
+    return ids;
+  }
+
+  
+
+
+  private void deleteNonFolders() {
+    if (nonFolderCount == 0) {
+      Logger.debug(LOG_TAG, "No non-folders to delete.");
+      return;
+    }
+
+    Logger.debug(LOG_TAG, "Applying deletion of " + nonFolderCount + " non-folders.");
+    final String[] nonFolderGUIDs = nonFolders.toArray(new String[nonFolderCount]);
+    final String nonFolderWhere = RepoUtils.computeSQLInClause(nonFolderCount, BrowserContract.Bookmarks.GUID);
+    dataAccessor.delete(nonFolderWhere, nonFolderGUIDs);
+
+    
+    
+    nonFolders.clear();
+    nonFolderCount = 0;
+  }
+
+  
+
+
+  public void clear() {
+    nonFolders.clear();
+    nonFolderCount = 0;
+    folders.clear();
+    nonFolderParents.clear();
+    folderParents.clear();
+  }
+}
