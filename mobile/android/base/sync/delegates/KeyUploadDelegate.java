1fbef473fc2e59a7d95c4108d8fabe2fd1a117fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:08:19 +0000

    Bug 709321 - Generate and upload keys. r=rnewman, a=blocking-fennec
    UltraBlame original commit: 4c42dd1bf025ef3a29c1450346bcd15ab9130074

--- mobile/android/base/sync/delegates/KeyUploadDelegate.java
+++ mobile/android/base/sync/delegates/KeyUploadDelegate.java
@@ -2,42 +2,20 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 package org.mozilla.gecko.sync.delegates;
 
 public interface KeyUploadDelegate {
+  
+
+
+
+
+
+
+
+
+
+
   void onKeysUploaded();
   void onKeyUploadFailed(Exception e);
 }
