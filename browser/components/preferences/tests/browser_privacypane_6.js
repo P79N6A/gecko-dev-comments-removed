9808e1820f7091efa26e7657f741370cb418b758
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:08:40 +0000

    Bug 535490 - Intermittent timeout: browser_privacypane_6.js; r=gavin
    UltraBlame original commit: 802221c97140bb1997e658ba894fa6684457e6e3

--- browser/components/preferences/tests/browser_privacypane_6.js
+++ browser/components/preferences/tests/browser_privacypane_6.js
@@ -44,11 +44,7 @@ function test() {
     test_privatebrowsing_toggle,
     enter_private_browsing, 
     test_privatebrowsing_toggle,
-    test_privatebrowsing_ui,
-    enter_private_browsing, 
-    test_privatebrowsing_ui,
 
     
-    reset_preferences
   ]);
 }
--- browser/components/preferences/tests/browser_privacypane_8.js
+++ browser/components/preferences/tests/browser_privacypane_8.js
@@ -34,16 +34,26 @@
 
 
 
-
 function test() {
   let loader = Cc["@mozilla.org/moz/jssubscript-loader;1"].
                getService(Ci.mozIJSSubScriptLoader);
   loader.loadSubScript("chrome://mochikit/content/browser/browser/components/preferences/tests/privacypane_tests.js", this);
 
   run_test_subset([
-    test_privatebrowsing_ui,
-    enter_private_browsing, 
-    test_privatebrowsing_ui,
+    
+    test_historymode_retention("remember", undefined),
+
+    
+    test_custom_retention("acceptCookies", "remember"),
+
+    
+    test_historymode_retention("dontremember", "custom"),
+
+    
+    test_historymode_retention("remember", "custom"),
+
+    
+    test_historymode_retention("remember", "remember"),
 
     
     reset_preferences
