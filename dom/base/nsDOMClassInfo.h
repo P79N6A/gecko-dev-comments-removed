c6ba94bb6ddec2a029f8c9732db5ea0d9142d296
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:59:57 +0000

    Bug 728029 - Preserve added JS properties on the JS object returned by webgl.getExtension - r=peterv
    UltraBlame original commit: 46a8181a7f158920b60b54d5b7bb3d3d00dc31f9

--- dom/base/nsDOMClassInfo.h
+++ dom/base/nsDOMClassInfo.h
@@ -497,6 +497,32 @@ public:
 
 
 
+class WebGLExtensionSH : public nsDOMGenericSH
+{
+protected:
+  WebGLExtensionSH(nsDOMClassInfoData* aData) : nsDOMGenericSH(aData)
+  {
+  }
+
+  virtual ~WebGLExtensionSH()
+  {
+  }
+
+public:
+  NS_IMETHOD PreCreate(nsISupports *nativeObj, JSContext *cx,
+                       JSObject *globalObj, JSObject **parentObj);
+  NS_IMETHOD AddProperty(nsIXPConnectWrappedNative *wrapper, JSContext *cx,
+                         JSObject *obj, jsid id, jsval *vp, bool *_retval);
+  virtual void PreserveWrapper(nsISupports *aNative);
+
+  static nsIClassInfo *doCreate(nsDOMClassInfoData* aData)
+  {
+    return new WebGLExtensionSH(aData);
+  }
+};
+
+
+
 
 
 class nsNodeSH : public nsDOMGenericSH
