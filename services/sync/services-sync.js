dc2c4ec7b1d36eb6f4700e70b2c9cb91887f28b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:37:03 +0000

    Bug 678588 - Drop singleDeviceInterval to 1 hour. r=mconnor
    UltraBlame original commit: a68442b5ff5c5f9b12c4c95d77b5467e92a36c5e

--- services/sync/services-sync.js
+++ services/sync/services-sync.js
@@ -9,7 +9,7 @@ pref("services.sync.syncKeyHelpURL", "https://services.mozilla.com/help/synckey"
 pref("services.sync.lastversion", "firstrun");
 pref("services.sync.sendVersionInfo", true);
 
-pref("services.sync.scheduler.singleDeviceInterval", 86400); 
+pref("services.sync.scheduler.singleDeviceInterval", 3600);  
 pref("services.sync.scheduler.idleInterval",         3600);  
 pref("services.sync.scheduler.activeInterval",       300);   
 pref("services.sync.scheduler.immediateInterval",    60);    
