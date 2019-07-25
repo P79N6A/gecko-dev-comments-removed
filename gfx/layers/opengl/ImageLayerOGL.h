1891535aab772ce8785f84d9dc13a48749337881
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:34:11 +0000

    Bug 687267 - Initial support for Flash on Honeycomb r=bgirard,vlad,jgilbert,blassey
    UltraBlame original commit: f07aca82e80e5b772f6e0def69c2ca5d08677497

--- gfx/layers/opengl/ImageLayerOGL.h
+++ gfx/layers/opengl/ImageLayerOGL.h
@@ -180,6 +180,13 @@ private:
 
 
   nsRefPtr<TextureImage> mTexImage;
+
+  
+  gl::SharedTextureHandle mSharedHandle;
+  gl::TextureImage::TextureShareType mShareType;
+  bool mInverted;
+  GLuint mTexture;
+  
   GLTexture mYUVTexture[3];
   gfxIntSize mSize;
   gfxIntSize mCbCrSize;
