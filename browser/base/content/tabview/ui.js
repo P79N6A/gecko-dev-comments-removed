89e3f03dc559ff51382538ad8446378a897ab434
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:48:08 +0000

    Bug 681599 - Ctrl+T while in search mode should close search layer; r=ttaubert
    UltraBlame original commit: bace24f47138c5767fc97968b7d54061a57b27de

--- browser/base/content/tabview/ui.js
+++ browser/base/content/tabview/ui.js
@@ -713,6 +713,7 @@ let UI = {
         if (data == "enter" || data == "exit") {
           hideSearch();
           self._privateBrowsing.transitionMode = data;
+          self.storageBusy();
         }
       } else if (topic == "private-browsing-transition-complete") {
         
@@ -721,6 +722,7 @@ let UI = {
           self.showTabView(false);
 
         self._privateBrowsing.transitionMode = "";
+        self.storageReady();
       }
     }
 
@@ -867,12 +869,8 @@ let UI = {
     this._currentTab = tab;
 
     if (this.isTabViewVisible()) {
-      
-      
-      
-      
-      if (!this.restoredClosedTab && !this._privateBrowsing.transitionMode &&
-          this._lastOpenedTab == tab && tab._tabViewTabItem) {
+      if (!this.restoredClosedTab && this._lastOpenedTab == tab && 
+        tab._tabViewTabItem) {
         tab._tabViewTabItem.zoomIn(true);
         this._lastOpenedTab = null;
         return;
@@ -1132,26 +1130,18 @@ let UI = {
       function getClosestTabBy(norm) {
         if (!self.getActiveTab())
           return null;
-
-        let activeTab = self.getActiveTab();
-        let activeTabGroup = activeTab.parent;
-        let myCenter = activeTab.bounds.center();
-        let match;
-
-        TabItems.getItems().forEach(function (item) {
-          if (!item.parent.hidden &&
-              (!activeTabGroup.expanded || activeTabGroup.id == item.parent.id)) {
-            let itemCenter = item.bounds.center();
-
-            if (norm(itemCenter, myCenter)) {
-              let itemDist = myCenter.distance(itemCenter);
-              if (!match || match[0] > itemDist)
-                match = [itemDist, item];
-            }
-          }
-        });
-
-        return match && match[1];
+        let centers =
+          [[item.bounds.center(), item]
+             for each(item in TabItems.getItems()) if (!item.parent || !item.parent.hidden)];
+        let myCenter = self.getActiveTab().bounds.center();
+        let matches = centers
+          .filter(function(item){return norm(item[0], myCenter)})
+          .sort(function(a,b){
+            return myCenter.distance(a[0]) - myCenter.distance(b[0]);
+          });
+        if (matches.length > 0)
+          return matches[0][1];
+        return null;
       }
 
       let preventDefault = true;
@@ -1512,7 +1502,7 @@ let UI = {
         matches[0].zoomIn();
         zoomedIn = true;
       }
-      hideSearch(null);
+      hideSearch();
     }
 
     if (!zoomedIn) {
@@ -1623,15 +1613,11 @@ let UI = {
   getFavIconUrlForTab: function UI_getFavIconUrlForTab(tab) {
     let url;
 
-    if (tab.image) {
-      
-      if (/^https?:/.test(tab.image))
-        url = gFavIconService.getFaviconLinkForIcon(gWindow.makeURI(tab.image)).spec;
-      else
-        url = tab.image;
-    } else {
+    
+    if (tab.image && !(/^https?:/.test(tab.image)))
+      url = tab.image;
+    else
       url = gFavIconService.getFaviconImageForPage(tab.linkedBrowser.currentURI).spec;
-    }
 
     return url;
   },
