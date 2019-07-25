b6757de5c0637aca6e7039896261baeeed6ea672
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 12:08:26 +0000

    bug 637487 - Unable to install extensions when Fennec moved to SD Card r=dougt a=blocking-fennec
    UltraBlame original commit: 378eec685e74ab810aff9bb3f910625256b774dc

--- xpcom/io/nsLocalFileUnix.cpp
+++ xpcom/io/nsLocalFileUnix.cpp
@@ -119,6 +119,7 @@ static nsresult MacErrorMapper(OSErr inErr);
 #ifdef ANDROID
 #include "AndroidBridge.h"
 #include "nsIMIMEService.h"
+#include <linux/magic.h>
 #endif
 
 #include "nsNativeCharsetUtils.h"
@@ -1103,9 +1104,20 @@ nsLocalFile::SetPermissions(PRUint32 aPermissions)
 
 
 
-    if (chmod(mPath.get(), aPermissions) < 0)
-        return NSRESULT_FOR_ERRNO();
-    return NS_OK;
+    if (chmod(mPath.get(), aPermissions) >= 0)
+        return NS_OK;
+#if defined(ANDROID) && defined(STATFS)
+    
+    
+    struct STATFS sfs;
+    if (STATFS(mPath.get(), &sfs) < 0)
+         return NSRESULT_FOR_ERRNO();
+
+    
+    if (sfs.f_type == MSDOS_SUPER_MAGIC )
+        return NS_OK;
+#endif
+    return NSRESULT_FOR_ERRNO();
 }
 
 NS_IMETHODIMP
