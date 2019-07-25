3dfc66821995fd4667da514b53ca184f83ee2048
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:08:19 +0000

    Bug 751766 - Remove unused function MOZ_Crash. r=glandium
    UltraBlame original commit: 3f21124ebc63137dd8de23752091b45334137ea7

--- mfbt/Assertions.cpp
+++ mfbt/Assertions.cpp
@@ -38,70 +38,17 @@
 
 
 
-#include "mozilla/Attributes.h"
-#include "mozilla/Types.h"
-
-#include <cstdio>
-#include <cstdlib>
-#ifndef WIN32
-#include <signal.h>
-#endif
-
-#ifdef ANDROID
-#include <android/log.h>
-#endif
+#include "mozilla/Assertions.h"
 
 
 
 extern "C" {
 
 MOZ_EXPORT_API(void)
-MOZ_Crash()
-{
-  
-
-
-
-
-
-
-#if defined(WIN32)
-  
-
-
-
-  *((volatile int *) NULL) = 123;
-  exit(3);
-#elif defined(ANDROID)
-  
-
-
-
-  *((volatile int *) NULL) = 123;
-  abort();
-#elif defined(__APPLE__)
-  
-
-
-
-  *((volatile int *) NULL) = 123;  
-  raise(SIGABRT);  
-#else
-  raise(SIGABRT);  
-#endif
-}
-
-MOZ_EXPORT_API(void)
 MOZ_Assert(const char* s, const char* file, int ln)
 {
-#ifdef ANDROID
-  __android_log_print(ANDROID_LOG_FATAL, "MOZ_Assert",
-                      "Assertion failure: %s, at %s:%d\n", s, file, ln);
-#else
-  fprintf(stderr, "Assertion failure: %s, at %s:%d\n", s, file, ln);
-  fflush(stderr);
-#endif
-  MOZ_Crash();
+  MOZ_OutputAssertMessage(s, file, ln);
+  MOZ_CRASH();
 }
 
 }
