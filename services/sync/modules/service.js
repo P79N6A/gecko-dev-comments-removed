63c3589ff0e68ca021abd1876a41ac38c1e9fc63
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:59:14 +0000

    Bug 688574 - Fix indentation in in sync's services.js (r=philikon)
    UltraBlame original commit: 9c903b53932e62f748179306bfeb6e6a0fe680d2

--- services/sync/modules/service.js
+++ services/sync/modules/service.js
@@ -759,10 +759,9 @@ WeaveSvc.prototype = {
             
             
             if (this._remoteSetup()) {
-
               
-            Status.login = LOGIN_SUCCEEDED;
-            return true;
+              Status.login = LOGIN_SUCCEEDED;
+              return true;
             }
 
             this._log.warn("Remote setup failed.");
@@ -917,7 +916,6 @@ WeaveSvc.prototype = {
 
   startOver: function() {
     Svc.Obs.notify("weave:engine:stop-tracking");
-    Status.resetSync();
 
     
     
@@ -946,6 +944,7 @@ WeaveSvc.prototype = {
     this.resetClient();
     CollectionKeys.clear();
     Status.resetBackoff();
+    Status.resetSync();
 
     
     this._ignorePrefObserver = true;
@@ -955,6 +954,7 @@ WeaveSvc.prototype = {
     Svc.Prefs.set("lastversion", WEAVE_VERSION);
     
     this.password = "";
+    this.passphrase = "";
     Services.logins.findLogins({}, PWDMGR_HOST, "", "").map(function(login) {
       Services.logins.removeLogin(login);
     });
