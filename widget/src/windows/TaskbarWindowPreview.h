c57b7485e44527b4e3a9348e2d40354ea8841e55
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:55:50 +0000

    Bug 515907 - Support taskbar icon overlays in Windows 7. Original patch by Tim Miller <mille449@gmail.com>. r=jimm, sr=roc
    UltraBlame original commit: 9cd3dc884d1c296656e5b07192e7c3143a5250c7

--- widget/src/windows/TaskbarWindowPreview.h
+++ widget/src/windows/TaskbarWindowPreview.h
@@ -46,6 +46,7 @@
 
 #include "nsITaskbarWindowPreview.h"
 #include "nsITaskbarProgress.h"
+#include "nsITaskbarOverlayIconController.h"
 #include "TaskbarPreview.h"
 #include <nsWeakReference.h>
 
@@ -56,6 +57,7 @@ class TaskbarPreviewButton;
 class TaskbarWindowPreview : public TaskbarPreview,
                              public nsITaskbarWindowPreview,
                              public nsITaskbarProgress,
+                             public nsITaskbarOverlayIconController,
                              public nsSupportsWeakReference
 {
 public:
@@ -65,6 +67,7 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSITASKBARWINDOWPREVIEW
   NS_DECL_NSITASKBARPROGRESS
+  NS_DECL_NSITASKBAROVERLAYICONCONTROLLER
   NS_FORWARD_NSITASKBARPREVIEW(TaskbarPreview::)
 
   virtual LRESULT WndProc(UINT nMsg, WPARAM wParam, LPARAM lParam);
@@ -90,6 +93,7 @@ private:
 
   
   nsresult UpdateTaskbarProgress();
+  nsresult UpdateOverlayIcon();
 
   
   TBPFLAG                 mState;
@@ -97,10 +101,14 @@ private:
   ULONGLONG               mMaxValue;
 
   
-  static PRBool TaskbarProgressWindowHook(void *aContext,
-                                          HWND hWnd, UINT nMsg,
-                                          WPARAM wParam, LPARAM lParam,
-                                          LRESULT *aResult);
+  HICON                   mOverlayIcon;
+  nsString                mIconDescription;
+
+  
+  static PRBool TaskbarWindowHook(void *aContext,
+                                  HWND hWnd, UINT nMsg,
+                                  WPARAM wParam, LPARAM lParam,
+                                  LRESULT *aResult);
 
   friend class TaskbarPreviewButton;
 };
