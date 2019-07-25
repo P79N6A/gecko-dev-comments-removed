e0681c27e7b4614bb66eef883612367c06716fd7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 13:59:44 +0000

    Weaken overzealous assert in JSStackFrame::resetInvokeCallFrame (r=warnings-as-errors)
    UltraBlame original commit: a7b3c44aea656bf4d064cd373bf36c44d6c3657f

--- js/src/jsinterpinlines.h
+++ js/src/jsinterpinlines.h
@@ -111,7 +111,18 @@ JSStackFrame::resetInvokeCallFrame()
                            JSFRAME_HAS_SCOPECHAIN |
                            JSFRAME_HAS_ANNOTATION |
                            JSFRAME_HAS_HOOK_DATA |
+                           JSFRAME_HAS_CALL_OBJ |
+                           JSFRAME_HAS_ARGS_OBJ |
                            JSFRAME_FINISHED_IN_INTERPRETER)));
+
+    
+
+
+
+
+    JS_ASSERT_IF(flags_ & JSFRAME_HAS_CALL_OBJ, callObj().getPrivate() == NULL);
+    JS_ASSERT_IF(flags_ & JSFRAME_HAS_ARGS_OBJ, argsObj().getPrivate() == NULL);
+
     flags_ &= JSFRAME_FUNCTION |
               JSFRAME_OVERFLOW_ARGS |
               JSFRAME_HAS_PREVPC |
