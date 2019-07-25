2f95260f23a7170d76d19b78c9e1333db4c1aab7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:30:29 +0000

    Bug 771048: fix typo in nsAutoRef::swap, r=khuey
    UltraBlame original commit: 84623518d42861f0787d0e7ee654405e40b2add5

---
+++ xpcom/tests/TestAutoRef.cpp
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+#include "nsAutoRef.h"
+#include "TestHarness.h"
+
+#define TEST(aCondition, aMsg) \
+  if (!(aCondition)) { fail("TestAutoRef: "#aMsg); exit(1); }
+
+struct TestObjectA {
+public:
+  TestObjectA() : mRefCnt(0) {
+  }
+
+  ~TestObjectA() {
+    TEST(mRefCnt == 0, "mRefCnt in destructor");
+  }
+
+public:
+  int mRefCnt;
+};
+
+template <>
+class nsAutoRefTraits<TestObjectA> : public nsPointerRefTraits<TestObjectA>
+{
+public:
+  static int mTotalRefsCnt;
+
+  static void Release(TestObjectA *ptr) {
+    ptr->mRefCnt--;
+    if (ptr->mRefCnt == 0) {
+      delete ptr;
+    }
+  }
+
+  static void AddRef(TestObjectA *ptr) {
+    ptr->mRefCnt++;
+  }
+};
+
+int nsAutoRefTraits<TestObjectA>::mTotalRefsCnt = 0;
+
+int main()
+{
+  {
+    nsCountedRef<TestObjectA> a(new TestObjectA());
+    TEST(a->mRefCnt == 1, "nsCountedRef instantiation with valid RawRef");
+
+    nsCountedRef<TestObjectA> b;
+    TEST(b.get() == nsnull, "nsCountedRef instantiation with invalid RawRef");
+
+    a.swap(b);
+    TEST(b->mRefCnt, "nsAutoRef::swap() t1");
+    TEST(a.get() == nsnull, "nsAutoRef::swap() t2");
+  }
+
+  TEST(true, "All tests pass");
+  return 0;
+}
