e3262cf2d63f9342e4a01464f78c7590f1f7c29e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:58:31 +0000

    Bug 603373 - Remove unused library argument of NS_TRY_SAFE_CALL_* macros r=joshmoz a=benjamin
    UltraBlame original commit: 2f23afd0b705cad1dde3ca2519940ebcde6be28a

--- modules/plugin/base/src/nsPluginHost.cpp
+++ modules/plugin/base/src/nsPluginHost.cpp
@@ -333,7 +333,7 @@ NS_IMETHODIMP nsPluginUnloadEvent::Run()
 {
   if (mLibrary) {
     
-    NS_TRY_SAFE_CALL_VOID(PR_UnloadLibrary(mLibrary), nsnull, nsnull);
+    NS_TRY_SAFE_CALL_VOID(PR_UnloadLibrary(mLibrary), nsnull);
   } else {
     NS_WARNING("missing library from nsPluginUnloadEvent");
   }
@@ -348,7 +348,7 @@ nsresult nsPluginHost::PostPluginUnloadEvent(PRLibrary* aLibrary)
     return NS_OK;
 
   
-  NS_TRY_SAFE_CALL_VOID(PR_UnloadLibrary(aLibrary), nsnull, nsnull);
+  NS_TRY_SAFE_CALL_VOID(PR_UnloadLibrary(aLibrary), nsnull);
 
   return NS_ERROR_FAILURE;
 }
