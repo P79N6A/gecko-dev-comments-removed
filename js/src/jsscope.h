821736872b91cbec9feabc3ae079c01828d0a1d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:27:27 +0000

    Bug 709885 - Simplify read barriers for shapes and types (r=bhackett)
    UltraBlame original commit: 6d443ec0dda17a11bf2330d078f55b45fc399249

--- js/src/jsscope.h
+++ js/src/jsscope.h
@@ -493,7 +493,7 @@ struct BaseShapeEntry
     static inline HashNumber hash(const BaseShape *base);
     static inline bool match(UnownedBaseShape *key, const BaseShape *lookup);
 };
-typedef HashSet<UnownedBaseShape *, BaseShapeEntry, SystemAllocPolicy> BaseShapeSet;
+typedef HashSet<ReadBarriered<UnownedBaseShape>, BaseShapeEntry, SystemAllocPolicy> BaseShapeSet;
 
 struct Shape : public js::gc::Cell
 {
@@ -975,7 +975,7 @@ struct InitialShapeEntry
 
 
 
-    js::Shape *shape;
+    ReadBarriered<Shape> shape;
 
     
 
