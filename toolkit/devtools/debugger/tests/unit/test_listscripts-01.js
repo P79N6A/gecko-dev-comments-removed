12617dca64b9480d7c9deac4eb2a8b0a4e41bd57
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:41:42 +0000

    Fix test hang on Win opt (Bug 783393). r=past
    UltraBlame original commit: 73622d728d4cef8a8550c866f1d814683151b284

--- toolkit/devtools/debugger/tests/unit/test_listscripts-01.js
+++ toolkit/devtools/debugger/tests/unit/test_listscripts-01.js
@@ -28,13 +28,18 @@ function test_simple_listscripts()
   gThreadClient.addOneTimeListener("paused", function (aEvent, aPacket) {
     var path = getFilePath('test_listscripts-01.js');
     gThreadClient.getScripts(function (aResponse) {
-        
-        do_check_eq(aResponse.scripts[0].url, path);
-        do_check_eq(aResponse.scripts[0].startLine, 41);
-        do_check_eq(aResponse.scripts[0].lineCount, 4);
-        gThreadClient.resume(function () {
-          finishClient(gClient);
-        });
+      let script = aResponse.scripts
+        .filter(function (s) {
+          return s.url.match(/test_listscripts-01.js$/);
+        })[0];
+      
+      do_check_true(!!script);
+      do_check_eq(script.url, path);
+      do_check_eq(script.startLine, 46);
+      do_check_eq(script.lineCount, 4);
+      gThreadClient.resume(function () {
+        finishClient(gClient);
+      });
     });
   });
 
