f940c247ec0962ea844569fcc09b4a0627ff71f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:08:05 +0000

    Bug 429592 - Add a monitor thread for process hangs and crash by default if a chrome process doesn't end up back in the event loop for more than 30 seconds. By default this affects non-debug builds only. r=cjones/bent
    UltraBlame original commit: e297dc3d0401621daf50c00778b86ba5af39a43f

---
+++ xpcom/threads/HangMonitor.h
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_HangMonitor_h
+#define mozilla_HangMonitor_h
+
+namespace mozilla { namespace HangMonitor {
+
+
+
+
+void Startup();
+
+
+
+
+void Shutdown();
+
+
+
+
+
+void NotifyActivity();
+
+
+
+
+
+void Suspend();
+
+} } 
+
+#endif 
