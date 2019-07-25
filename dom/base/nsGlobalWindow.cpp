3030512d57185f9875732e5b44b791dfd62c90cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:24:20 +0000

    Bug 757182 - Handle window.close in <iframe mozbrowser>. r=smaug
    UltraBlame original commit: 0d138bc9b28ee02fb26a98741a4c6da42f60980f

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -6361,7 +6361,12 @@ nsGlobalWindow::Close()
 {
   FORWARD_TO_OUTER(Close, (), NS_ERROR_NOT_INITIALIZED);
 
-  if (IsFrame() || !mDocShell || IsInModalState()) {
+  bool isMozBrowser = false;
+  if (mDocShell) {
+    mDocShell->GetIsBrowserFrame(&isMozBrowser);
+  }
+
+  if ((!isMozBrowser && IsFrame()) || !mDocShell || IsInModalState()) {
     
     
     
