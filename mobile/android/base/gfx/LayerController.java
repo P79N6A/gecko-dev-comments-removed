53ad21ae272ab184c7089162683e83ae84c59b8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:01:49 +0000

    Bug 741400 - Guard against NullPointerException, and also move some code so it shouldn't happen. r=mfinkle
    UltraBlame original commit: d96543e0f7113f3cec65a7c1bfc771651fac8eda

--- mobile/android/base/gfx/LayerController.java
+++ mobile/android/base/gfx/LayerController.java
@@ -343,6 +343,10 @@ public class LayerController implements Tabs.OnTabsChangedListener {
 
 
     public PointF convertViewPointToLayerPoint(PointF viewPoint) {
+        if (mLayerClient == null) {
+            return null;
+        }
+
         ImmutableViewportMetrics viewportMetrics = mViewportMetrics;
         PointF origin = viewportMetrics.getOrigin();
         float zoom = viewportMetrics.zoomFactor;
