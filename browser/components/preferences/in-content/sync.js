726f6ea4e74cbb36c3f6e86ee5006739aac6be66
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:09:20 +0000

    Bug 735091 - Sync pane for in-content prefrences. r=jaws,bmcbride
    UltraBlame original commit: 29de131322c5793c7f3a639653d3deef7d88d647

---
+++ browser/components/preferences/in-content/sync.js
@@ -0,0 +1,155 @@
+
+
+
+
+Components.utils.import("resource://services-sync/service.js");
+
+const PAGE_NO_ACCOUNT = 0;
+const PAGE_HAS_ACCOUNT = 1;
+const PAGE_NEEDS_UPDATE = 2;
+
+let gSyncPane = {
+  _stringBundle: null,
+  prefArray: ["engine.bookmarks", "engine.passwords", "engine.prefs",
+              "engine.tabs", "engine.history"],
+
+  get page() {
+    return document.getElementById("weavePrefsDeck").selectedIndex;
+  },
+
+  set page(val) {
+    document.getElementById("weavePrefsDeck").selectedIndex = val;
+  },
+
+  get _usingCustomServer() {
+    return Weave.Svc.Prefs.isSet("serverURL");
+  },
+
+  needsUpdate: function () {
+    this.page = PAGE_NEEDS_UPDATE;
+    let label = document.getElementById("loginError");
+    label.value = Weave.Utils.getErrorString(Weave.Status.login);
+    label.className = "error";
+  },
+
+  init: function () {
+    let topics = ["weave:service:login:error",
+                  "weave:service:login:finish",
+                  "weave:service:start-over",
+                  "weave:service:setup-complete",
+                  "weave:service:logout:finish"];
+
+    
+    
+    
+    topics.forEach(function (topic) {
+      Weave.Svc.Obs.add(topic, this.updateWeavePrefs, this);
+    }, this);
+    window.addEventListener("unload", function() {
+      topics.forEach(function (topic) {
+        Weave.Svc.Obs.remove(topic, this.updateWeavePrefs, this);
+      }, gSyncPane);
+    }, false);
+
+    this._stringBundle =
+      Services.strings.createBundle("chrome://browser/locale/preferences/preferences.properties");
+    this.updateWeavePrefs();
+  },
+
+  updateWeavePrefs: function () {
+    if (Weave.Status.service == Weave.CLIENT_NOT_CONFIGURED ||
+        Weave.Svc.Prefs.get("firstSync", "") == "notReady") {
+      this.page = PAGE_NO_ACCOUNT;
+    } else if (Weave.Status.login == Weave.LOGIN_FAILED_INVALID_PASSPHRASE ||
+               Weave.Status.login == Weave.LOGIN_FAILED_LOGIN_REJECTED) {
+      this.needsUpdate();
+    } else {
+      this.page = PAGE_HAS_ACCOUNT;
+      document.getElementById("accountName").value = Weave.Service.account;
+      document.getElementById("syncComputerName").value = Weave.Clients.localName;
+      document.getElementById("tosPP").hidden = this._usingCustomServer;
+    }
+  },
+
+  startOver: function (showDialog) {
+    if (showDialog) {
+      let flags = Services.prompt.BUTTON_POS_0 * Services.prompt.BUTTON_TITLE_IS_STRING +
+                  Services.prompt.BUTTON_POS_1 * Services.prompt.BUTTON_TITLE_CANCEL + 
+                  Services.prompt.BUTTON_POS_1_DEFAULT;
+      let buttonChoice =
+        Services.prompt.confirmEx(window,
+                                  this._stringBundle.GetStringFromName("syncUnlink.title"),
+                                  this._stringBundle.GetStringFromName("syncUnlink.label"),
+                                  flags,
+                                  this._stringBundle.GetStringFromName("syncUnlinkConfirm.label"),
+                                  null, null, null, {});
+
+      
+      if (buttonChoice == 1)
+        return;
+    }
+
+    Weave.Service.startOver();
+    this.updateWeavePrefs();
+  },
+
+  updatePass: function () {
+    if (Weave.Status.login == Weave.LOGIN_FAILED_LOGIN_REJECTED)
+      gSyncUtils.changePassword();
+    else
+      gSyncUtils.updatePassphrase();
+  },
+
+  resetPass: function () {
+    if (Weave.Status.login == Weave.LOGIN_FAILED_LOGIN_REJECTED)
+      gSyncUtils.resetPassword();
+    else
+      gSyncUtils.resetPassphrase();
+  },
+
+  
+
+
+
+
+
+
+
+
+  openSetup: function (wizardType) {
+    var win = Services.wm.getMostRecentWindow("Weave:AccountSetup");
+    if (win)
+      win.focus();
+    else {
+      window.openDialog("chrome://browser/content/sync/setup.xul",
+                        "weaveSetup", "centerscreen,chrome,resizable=no",
+                        wizardType);
+    }
+  },
+
+  openQuotaDialog: function () {
+    let win = Services.wm.getMostRecentWindow("Sync:ViewQuota");
+    if (win)
+      win.focus();
+    else 
+      window.openDialog("chrome://browser/content/sync/quota.xul", "",
+                        "centerscreen,chrome,dialog,modal");
+  },
+
+  openAddDevice: function () {
+    if (!Weave.Utils.ensureMPUnlocked())
+      return;
+    
+    let win = Services.wm.getMostRecentWindow("Sync:AddDevice");
+    if (win)
+      win.focus();
+    else 
+      window.openDialog("chrome://browser/content/sync/addDevice.xul",
+                        "syncAddDevice", "centerscreen,chrome,resizable=no");
+  },
+
+  resetSync: function () {
+    this.openSetup("reset");
+  }
+}
+
