6ed48b1c9f14bc2f84cac8296f9b00517557140c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:59:49 +0000

    Bug 739137: Try harder to ensure that b2g shuts down when we ask it to. r=fabrice
    UltraBlame original commit: 6e4ae5caddf49cc86c116a452a52af8c5c7778b4

--- b2g/components/UpdatePrompt.js
+++ b2g/components/UpdatePrompt.js
@@ -24,6 +24,8 @@ UpdatePrompt.prototype = {
   classID: Components.ID("{88b3eb21-d072-4e3b-886d-f89d8c49fe59}"),
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIUpdatePrompt]),
 
+  _selfDestructTimer: null,
+
   
 
   
@@ -39,6 +41,10 @@ UpdatePrompt.prototype = {
 
     log("Update downloaded, restarting to apply it");
 
+    
+    
+    this._setSelfDestructTimer(5000);
+
     let appStartup = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup);
     
     
@@ -49,6 +55,24 @@ UpdatePrompt.prototype = {
                     | appStartup.eRestart
 #endif
       );
+  },
+
+  _setSelfDestructTimer: function UP__setSelfDestructTimer(timeoutMs) {
+#ifdef ANDROID
+    Cu.import("resource://gre/modules/ctypes.jsm");
+    let libc = ctypes.open("libc.so");
+    let _exit = libc.declare("_exit",  ctypes.default_abi,
+                             ctypes.void_t, 
+                             ctypes.int);   
+    this.notify = function UP_notify(_) {
+      log("Self-destruct timer fired; didn't cleanly shut down.  BOOM");
+      _exit(0);
+    }
+
+    let timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+    timer.initWithCallback(this, timeoutMs, timer.TYPE_ONE_SHOT);
+    this._selfDestructTimer = timer;
+#endif
   },
 
   showUpdateInstalled: function UP_showUpdateInstalled() { },
