92a29b11271fb9aa48dbb1bffbf74bbebc61085f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:56:19 +0000

    Bug 581654 - Fix places in browser-places.js that use for each...in to loop over arrays. r=mak
    UltraBlame original commit: 379c41d8de91af4537ddb95b436bc3945be5b548

--- browser/base/content/browser-places.js
+++ browser/base/content/browser-places.js
@@ -61,29 +61,32 @@ var StarUI = {
   },
 
   
-  _blockedCommands: ["cmd_close", "cmd_closeWindow"],
+  get _blockedCommands() {
+    delete this._blockedCommands;
+    return this._blockedCommands =
+      ["cmd_close", "cmd_closeWindow"].map(function (id) this._element(id), this);
+  },
+
   _blockCommands: function SU__blockCommands() {
-    for each(var key in this._blockedCommands) {
-      var elt = this._element(key);
+    this._blockedCommands.forEach(function (elt) {
       
       if (elt.hasAttribute("wasDisabled"))
-        continue;
-      if (elt.getAttribute("disabled") == "true")
+        return;
+      if (elt.getAttribute("disabled") == "true") {
         elt.setAttribute("wasDisabled", "true");
-      else {
+      } else {
         elt.setAttribute("wasDisabled", "false");
         elt.setAttribute("disabled", "true");
       }
-    }
+    });
   },
 
   _restoreCommandsState: function SU__restoreCommandsState() {
-    for each(var key in this._blockedCommands) {
-      var elt = this._element(key);
+    this._blockedCommands.forEach(function (elt) {
       if (elt.getAttribute("wasDisabled") != "true")
         elt.removeAttribute("disabled");
       elt.removeAttribute("wasDisabled");
-    }
+    });
   },
 
   
