87c13c52713017ebea73597742c84eb4e4429f1e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:54:53 +0000

    Bug 602759 part 8 - Move nsSVGSMILTransform to SVGTransform; r=jwatt
    UltraBlame original commit: 04d244bc338f88010ef3e62fab0eed230b124d50

--- content/svg/content/src/SVGTransform.h
+++ content/svg/content/src/SVGTransform.h
@@ -112,6 +112,92 @@ protected:
   PRUint16 mType;
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class SVGTransformSMILData
+{
+public:
+  
+  
+  static const PRUint32 NUM_SIMPLE_PARAMS = 3;
+
+  
+  
+  static const PRUint32 NUM_STORED_PARAMS = 6;
+
+  explicit SVGTransformSMILData(PRUint16 aType)
+  : mTransformType(aType)
+  {
+    NS_ABORT_IF_FALSE(aType >= nsIDOMSVGTransform::SVG_TRANSFORM_MATRIX &&
+                      aType <= nsIDOMSVGTransform::SVG_TRANSFORM_SKEWY,
+                      "Unexpected transform type");
+    for (PRUint32 i = 0; i < NUM_STORED_PARAMS; ++i) {
+      mParams[i] = 0.f;
+    }
+  }
+
+  SVGTransformSMILData(PRUint16 aType, float (&aParams)[NUM_SIMPLE_PARAMS])
+  : mTransformType(aType)
+  {
+    NS_ABORT_IF_FALSE(aType >= nsIDOMSVGTransform::SVG_TRANSFORM_TRANSLATE &&
+                      aType <= nsIDOMSVGTransform::SVG_TRANSFORM_SKEWY,
+                      "Expected 'simple' transform type");
+    for (PRUint32 i = 0; i < NUM_SIMPLE_PARAMS; ++i) {
+      mParams[i] = aParams[i];
+    }
+    for (PRUint32 i = NUM_SIMPLE_PARAMS; i < NUM_STORED_PARAMS; ++i) {
+      mParams[i] = 0.f;
+    }
+  }
+
+  
+  SVGTransformSMILData(const SVGTransform& aTransform);
+  SVGTransform ToSVGTransform() const;
+
+  PRBool operator==(const SVGTransformSMILData& aOther) const
+  {
+    if (mTransformType != aOther.mTransformType)
+      return PR_FALSE;
+
+    for (PRUint32 i = 0; i < NUM_STORED_PARAMS; ++i) {
+      if (mParams[i] != aOther.mParams[i]) {
+        return PR_FALSE;
+      }
+    }
+
+    return PR_TRUE;
+  }
+
+  PRBool operator!=(const SVGTransformSMILData& aOther) const
+  {
+    return !(*this == aOther);
+  }
+
+  PRUint16 mTransformType;
+  float    mParams[NUM_STORED_PARAMS];
+};
+
 } 
 
 #endif 
