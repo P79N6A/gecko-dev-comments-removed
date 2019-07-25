f2b1c01c896c74b43d99784957140ee221e537cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:53:05 +0000

    Bug 725881 - Add a deleted form history table. r=dolske
    UltraBlame original commit: 289cd639ff3418020d6adb18153a1b0b4c9de552

---
+++ toolkit/components/satchel/test/unit/test_db_update_v4.js
@@ -0,0 +1,42 @@
+
+
+
+
+var testnum = 0;
+var fh;
+
+function run_test()
+{
+  try {
+
+  
+  var testfile = do_get_file("formhistory_v3.sqlite");
+  var profileDir = dirSvc.get("ProfD", Ci.nsIFile);
+
+  
+  var destFile = profileDir.clone();
+  destFile.append("formhistory.sqlite");
+  if (destFile.exists())
+    destFile.remove(false);
+
+  testfile.copyTo(profileDir, "formhistory.sqlite");
+  do_check_eq(3, getDBVersion(testfile));
+
+  fh = Cc["@mozilla.org/satchel/form-history;1"].
+       getService(Ci.nsIFormHistory2);
+
+
+  
+  testnum++;
+
+  
+  do_check_true(fh.DBConnection.tableExists("moz_deleted_formhistory"));
+  
+  do_check_eq(CURRENT_SCHEMA, fh.DBConnection.schemaVersion);
+  
+  do_check_true(fh.entryExists("name-A", "value-A"));
+
+  } catch (e) {
+    throw "FAILED in test #" + testnum + " -- " + e;
+  }
+}
