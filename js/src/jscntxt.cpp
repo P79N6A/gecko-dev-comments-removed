88b56432dcd3393bbe69967ebf1a1fcc2248d792
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:51:16 +0000

    Bug 602393: Fix idiotic mistake causing JITs to be blacklisted on all android devices. r=dvander a=blocking-fennec-b1+
    UltraBlame original commit: 2f281ff6d5e48dd9d105295dbde5047bb9fccf7f

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -438,7 +438,7 @@ void
 FrameRegsIter::incSlow(JSStackFrame *fp, JSStackFrame *prev)
 {
     JS_ASSERT(prev);
-    JS_ASSERT(curpc == prev->savedpc_);
+    JS_ASSERT(curpc == curfp->pc(cx, fp));
     JS_ASSERT(fp == curseg->getInitialFrame());
 
     
@@ -2039,16 +2039,10 @@ void
 JSContext::pushSegmentAndFrame(js::StackSegment *newseg, JSFrameRegs &newregs)
 {
     JS_ASSERT(regs != &newregs);
-    if (hasActiveSegment()) {
-        JS_ASSERT(regs->fp->savedpc_ == JSStackFrame::sInvalidpc);
-        regs->fp->savedpc_ = regs->pc;
+    if (hasActiveSegment())
         currentSegment->suspend(regs);
-    }
     newseg->setPreviousInContext(currentSegment);
     currentSegment = newseg;
-#ifdef DEBUG
-    newregs.fp->savedpc_ = JSStackFrame::sInvalidpc;
-#endif
     setCurrentRegs(&newregs);
     newseg->joinContext(this, newregs.fp);
 }
@@ -2058,7 +2052,6 @@ JSContext::popSegmentAndFrame()
 {
     JS_ASSERT(currentSegment->maybeContext() == this);
     JS_ASSERT(currentSegment->getInitialFrame() == regs->fp);
-    JS_ASSERT(regs->fp->savedpc_ == JSStackFrame::sInvalidpc);
     currentSegment->leaveContext();
     currentSegment = currentSegment->getPreviousInContext();
     if (currentSegment) {
@@ -2067,9 +2060,6 @@ JSContext::popSegmentAndFrame()
         } else {
             setCurrentRegs(currentSegment->getSuspendedRegs());
             currentSegment->resume();
-#ifdef DEBUG
-            regs->fp->savedpc_ = JSStackFrame::sInvalidpc;
-#endif
         }
     } else {
         JS_ASSERT(regs->fp->prev() == NULL);
@@ -2082,8 +2072,6 @@ JSContext::saveActiveSegment()
 {
     JS_ASSERT(hasActiveSegment());
     currentSegment->save(regs);
-    JS_ASSERT(regs->fp->savedpc_ == JSStackFrame::sInvalidpc);
-    regs->fp->savedpc_ = regs->pc;
     setCurrentRegs(NULL);
 }
 
@@ -2093,9 +2081,6 @@ JSContext::restoreSegment()
     js::StackSegment *ccs = currentSegment;
     setCurrentRegs(ccs->getSuspendedRegs());
     ccs->restore();
-#ifdef DEBUG
-    regs->fp->savedpc_ = JSStackFrame::sInvalidpc;
-#endif
 }
 
 JSGenerator *
@@ -2267,7 +2252,7 @@ JSContext::updateJITEnabled()
 #ifdef JS_METHODJIT
     methodJitEnabled = (options & JSOPTION_METHODJIT) &&
                        !IsJITBrokenHere()
-# ifdef JS_CPU_X86
+# if defined JS_CPU_X86 || defined JS_CPU_X64
                        && JSC::MacroAssemblerX86Common::getSSEState() >=
                           JSC::MacroAssemblerX86Common::HasSSE2
 # endif
