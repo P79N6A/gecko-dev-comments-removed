a80131fca68a17b4ec03fc1cfa6023563f550a7b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:06:45 +0000

    Bug 518136: Use -showIncludes to generate dependencies for MSVC for a substantial build performance win. r=ted
    UltraBlame original commit: 6001758d1f47a8336e2a0257eda10781996a7859

---
+++ js/src/build/cl.py
@@ -0,0 +1,94 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os, os.path
+import subprocess
+import sys
+
+def InvokeClWithDependencyGeneration(cmdline):
+    target = ""
+    
+    for arg in cmdline:
+        if arg.startswith("-Fo"):
+            target = arg[3:]
+            break
+
+    if target == None:
+        print >>sys.stderr, "No target set" and sys.exit(1)
+
+    
+    depstarget = os.path.basename(target) + ".pp"
+
+    cmdline += ['-showIncludes']
+    cl = subprocess.Popen(cmdline, stdout=subprocess.PIPE)
+
+    deps = set()
+    for line in cl.stdout:
+        
+        
+        if line.startswith("Note: including file:"):
+            dep = line[21:].strip()
+            
+            
+            
+            if dep.find(' ') == -1:
+                deps.add(dep)
+        else:
+            sys.stdout.write(line) 
+                                   
+
+    ret = cl.wait()
+    if ret != 0 or target == "":
+        sys.exit(ret)
+
+    depsdir = os.path.normpath(os.path.join(os.path.dirname(target), ".deps"))
+    depstarget = os.path.join(depsdir, depstarget)
+    if not os.path.isdir(depsdir):
+        try:
+            os.makedirs(depsdir)
+        except OSError:
+            pass 
+                 
+                 
+
+    f = open(depstarget, "w")
+    for dep in sorted(deps):
+        print >>f, "%s: %s" % (target, dep)
+
+if __name__ == "__main__":
+    InvokeClWithDependencyGeneration(sys.argv[1:])
