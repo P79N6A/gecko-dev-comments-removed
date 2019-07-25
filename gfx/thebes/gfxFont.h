5055d428abf5278e8660ef28a6f47ce49b128eb5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:53:18 +0000

    bug 633453 - correctly check the last array element before deciding whether to append or insert. r=roc a=blocking
    UltraBlame original commit: caffaf707958ecc7470112128a08b1a9f600777d

--- gfx/thebes/gfxFont.h
+++ gfx/thebes/gfxFont.h
@@ -2128,7 +2128,7 @@ private:
             
             
             if (mOffsetToIndex.Length() == 0 ||
-                aOffset > mOffsetToIndex[mLastUsed].mOffset) {
+                aOffset > mOffsetToIndex[mOffsetToIndex.Length() - 1].mOffset) {
                 if (!mOffsetToIndex.AppendElement(DGRec(aOffset, detailIndex))) {
                     return nsnull;
                 }
