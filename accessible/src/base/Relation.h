c12401dd76115d034d864054b130e289491ca5e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:08:54 +0000

    Bug 739190 - change AppendTarget(nsIContent aContent) to take nsDocAccessible argument, r=surkov, f=eeejay
    UltraBlame original commit: f593f893ebd7858c375f2458d27249958a115907

--- accessible/src/base/Relation.h
+++ accessible/src/base/Relation.h
@@ -74,9 +74,9 @@ public:
     mFirstIter(nsnull), mLastIter(nsnull)
     { AppendTarget(aAcc); }
 
-  Relation(nsIContent* aContent) :
+  Relation(nsDocAccessible* aDocument, nsIContent* aContent) :
     mFirstIter(nsnull), mLastIter(nsnull)
-    { AppendTarget(aContent); }
+    { AppendTarget(aDocument, aContent); }
 
   Relation& operator = (const RelationCopyHelper& aRH)
   {
@@ -120,10 +120,10 @@ public:
 
 
 
-  inline void AppendTarget(nsIContent* aContent)
+  void AppendTarget(nsDocAccessible* aDocument, nsIContent* aContent)
   {
     if (aContent)
-      AppendTarget(GetAccService()->GetAccessible(aContent, nsnull));
+      AppendTarget(aDocument->GetAccessible(aContent));
   }
 
   
