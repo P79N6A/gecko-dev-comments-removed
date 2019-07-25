3a1dc4a88a5177c2a341036c76b8674a2c34f6f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:39:28 +0000

    Merge m-c to b-s.
    UltraBlame original commit: d1b05e332e4e8ea05d990ca44054c2cc5246b0e2

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -9125,7 +9125,7 @@ nsDocument::RequestFullScreen(Element* aElement,
   
   if (!mIsApprovedForFullscreen) {
     mIsApprovedForFullscreen =
-      GetWindow()->IsInAppOrigin() ||
+      NodePrincipal()->GetAppStatus() >= nsIPrincipal::APP_STATUS_INSTALLED ||
       nsContentUtils::IsSitePermAllow(NodePrincipal(), "fullscreen");
   }
 
