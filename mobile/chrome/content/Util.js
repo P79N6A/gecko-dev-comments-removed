c3ed6d3d0326db293c806ae932588fc663dbcc08
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:51:37 +0000

    Bug 680077 - Use Util.isTablet() everywhere to check if tablet mode is on (r=mbrubeck)
    Use it instead of checking for a 'tablet' attribute in Elements.urlbarState
    everywhere.
    
    UltraBlame original commit: 8d3475475a0be3a6ff752e6f9d4cda7469734863

--- mobile/chrome/content/Util.js
+++ mobile/chrome/content/Util.js
@@ -161,15 +161,29 @@ let Util = {
     return (!appInfo || appInfo.getService(Ci.nsIXULRuntime).processType == Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT);
   },
 
-  isTablet: function isTablet() {
+  isTablet: function isTablet(options) {
+    let forceUpdate = options && 'forceUpdate' in options && options.forceUpdate;
+
+    if ('_isTablet' in this && !forceUpdate)
+      return this._isTablet;
+
+    let tabletPref = Services.prefs.getIntPref("browser.ui.layout.tablet");
+
+    
+    
+    if (tabletPref == 0)
+      return this._isTablet = false;
+    else if (tabletPref == 1)
+      return this._isTablet = true;
+
     let dpi = this.displayDPI;
     if (dpi <= 96)
-      return (window.innerWidth > 1024);
+      return this._isTablet = (window.innerWidth > 1024);
 
     
     let tablet_panel_minwidth = 124;
     let dpmm = 25.4 * window.innerWidth / dpi;
-    return (dpmm >= tablet_panel_minwidth);
+    return this._isTablet = (dpmm >= tablet_panel_minwidth);
   },
 
   isPortrait: function isPortrait() {
