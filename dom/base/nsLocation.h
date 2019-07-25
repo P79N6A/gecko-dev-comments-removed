67e6c02d33a3559b1f323a25d2085a094f99f7f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:32:00 +0000

    Bug 670986 - Create nsLocation.h to declare nsLocation instead of doing that in nsGlobalWindow.h. r=jst
    UltraBlame original commit: 6fd16e7c196bd25b13f3cbfc6dc4a8b1be6eecf9

---
+++ dom/base/nsLocation.h
@@ -0,0 +1,97 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsLocation_h__
+#define nsLocation_h__
+
+#include "nsIDOMLocation.h"
+#include "nsString.h"
+#include "nsWeakReference.h"
+
+
+class nsIURI;
+class nsIDocShell;
+class JSContext;
+class nsIDocument;
+class nsIDocShellLoadInfo;
+
+
+
+
+
+class nsLocation : public nsIDOMLocation
+{
+public:
+  nsLocation(nsIDocShell *aDocShell);
+  virtual ~nsLocation();
+
+  NS_DECL_ISUPPORTS
+
+  void SetDocShell(nsIDocShell *aDocShell);
+  nsIDocShell *GetDocShell();
+
+  
+  NS_DECL_NSIDOMLOCATION
+
+protected:
+  
+  
+  
+  nsresult GetURI(nsIURI** aURL, PRBool aGetInnermostURI = PR_FALSE);
+  nsresult GetWritableURI(nsIURI** aURL);
+  nsresult SetURI(nsIURI* aURL, PRBool aReplace = PR_FALSE);
+  nsresult SetHrefWithBase(const nsAString& aHref, nsIURI* aBase,
+                           PRBool aReplace);
+  nsresult SetHrefWithContext(JSContext* cx, const nsAString& aHref,
+                              PRBool aReplace);
+
+  nsresult GetSourceBaseURL(JSContext* cx, nsIURI** sourceURL);
+  nsresult GetSourceDocument(JSContext* cx, nsIDocument** aDocument);
+
+  nsresult CheckURL(nsIURI *url, nsIDocShellLoadInfo** aLoadInfo);
+
+  nsString mCachedHash;
+  nsWeakPtr mDocShell;
+};
+
+#endif 
+
