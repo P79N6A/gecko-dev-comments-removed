ef41cc7f5780f1787b078afed36cecc320d1c3c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:54:26 +0000

    bug 684889 - refactor and clean up Android font-list implementation. r=jdaggett
    UltraBlame original commit: 864c08bd7c63049a15d501dc40827ee606f1e8ba

--- gfx/thebes/gfxFT2Fonts.h
+++ gfx/thebes/gfxFT2Fonts.h
@@ -47,17 +47,14 @@
 #include "gfxFontUtils.h"
 #include "gfxUserFontSet.h"
 
-typedef struct FT_FaceRec_* FT_Face;
-
-class FileAndIndex {
-public:
-    FileAndIndex(nsCString aFilename, PRUint32 aIndex) :
-        filename(aFilename), index(aIndex) {}
-    FileAndIndex(FileAndIndex* fai) :
-        filename(fai->filename), index(fai->index) {}
-    nsCString filename;
-    PRUint32 index;
+namespace mozilla {
+    namespace dom {
+        class FontListEntry;
+    };
 };
+using mozilla::dom::FontListEntry;
+
+typedef struct FT_FaceRec_* FT_Face;
 
 
 
@@ -72,13 +69,9 @@ public:
         gfxFontFamily(aName) { }
 
     FontEntry *FindFontEntry(const gfxFontStyle& aFontStyle);
-    virtual void FindStyleVariations();
-    void AddFontFileAndIndex(nsCString aFilename, PRUint32 aIndex);
 
-private:
     
-    
-    nsTArray<FileAndIndex> mFilenames;
+    void AddFacesToFontList(InfallibleTArray<FontListEntry>* aFontList);
 };
 
 class FontEntry : public gfxFontEntry
@@ -98,12 +91,22 @@ public:
         return Name();
     }
 
+    
     static FontEntry* 
     CreateFontEntry(const gfxProxyFontEntry &aProxyEntry,
                     const PRUint8 *aFontData, PRUint32 aLength);
 
+    
+    
+    
+    static FontEntry*
+    CreateFontEntry(const FontListEntry& aFLE);
+
+    
+    
     static FontEntry* 
-    CreateFontEntryFromFace(FT_Face aFace, const PRUint8 *aFontData = nsnull);
+    CreateFontEntry(FT_Face aFace, const char *aFilename, PRUint8 aIndex,
+                    const PRUint8 *aFontData = nsnull);
         
         
 
@@ -134,8 +137,11 @@ public:
 
     FontEntry *GetFontEntry();
 
+#ifndef ANDROID
     static already_AddRefed<gfxFT2Font>
     GetOrMakeFont(const nsAString& aName, const gfxFontStyle *aStyle, PRBool aNeedsBold = PR_FALSE);
+#endif
+
     static already_AddRefed<gfxFT2Font>
     GetOrMakeFont(FontEntry *aFontEntry, const gfxFontStyle *aStyle, PRBool aNeedsBold = PR_FALSE);
 
