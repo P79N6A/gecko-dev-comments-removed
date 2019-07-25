2e1d646812ff4972aef276d95d039756aedf093c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:24:02 +0000

    Bug 657953 (1/2) - Implement the layout part of the meter element. r=roc
    UltraBlame original commit: 1774f76e6867219e43248d99c67f447f2ff43806

---
+++ layout/forms/nsMeterFrame.h
@@ -0,0 +1,107 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsMeterFrame_h___
+#define nsMeterFrame_h___
+
+#include "nsContainerFrame.h"
+#include "nsIAnonymousContentCreator.h"
+#include "nsCOMPtr.h"
+
+class nsMeterFrame : public nsContainerFrame,
+                     public nsIAnonymousContentCreator
+
+{
+public:
+  NS_DECL_QUERYFRAME_TARGET(nsMeterFrame)
+  NS_DECL_QUERYFRAME
+  NS_DECL_FRAMEARENA_HELPERS
+
+  nsMeterFrame(nsStyleContext* aContext);
+  virtual ~nsMeterFrame();
+
+  virtual void DestroyFrom(nsIFrame* aDestructRoot);
+
+  NS_IMETHOD Reflow(nsPresContext*           aCX,
+                    nsHTMLReflowMetrics&     aDesiredSize,
+                    const nsHTMLReflowState& aReflowState,
+                    nsReflowStatus&          aStatus);
+
+#ifdef NS_DEBUG
+  NS_IMETHOD GetFrameName(nsAString& aResult) const {
+    return MakeFrameName(NS_LITERAL_STRING("Meter"), aResult);
+  }
+#endif
+
+  virtual bool IsLeaf() const { return true; }
+
+  
+  virtual nsresult CreateAnonymousContent(nsTArray<ContentInfo>& aElements);
+  virtual void AppendAnonymousContentTo(nsBaseContentList& aElements,
+                                        PRUint32 aFilter);
+
+  NS_IMETHOD AttributeChanged(PRInt32  aNameSpaceID,
+                              nsIAtom* aAttribute,
+                              PRInt32  aModType);
+
+  virtual nsSize ComputeAutoSize(nsRenderingContext *aRenderingContext,
+                                 nsSize aCBSize, nscoord aAvailableWidth,
+                                 nsSize aMargin, nsSize aBorder,
+                                 nsSize aPadding, bool aShrinkWrap);
+
+  virtual bool IsFrameOfType(PRUint32 aFlags) const
+  {
+    return nsContainerFrame::IsFrameOfType(aFlags &
+      ~(nsIFrame::eReplaced | nsIFrame::eReplacedContainsBlock));
+  }
+
+protected:
+  
+  void ReflowBarFrame(nsIFrame*                aBarFrame,
+                      nsPresContext*           aPresContext,
+                      const nsHTMLReflowState& aReflowState,
+                      nsReflowStatus&          aStatus);
+  
+
+
+
+  nsCOMPtr<nsIContent> mBarDiv;
+};
+
+#endif
