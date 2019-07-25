4685d47e3e37d082dae010d9179c538e2b84f574
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:32:02 +0000

    Bug 717371 - Don't use nsINode::GetChildAt in PresShell::GoToAnchor; r=bz
    UltraBlame original commit: fb8cb96c2282a2299a761a5f8d8c8f49357178cd

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -3154,8 +3154,8 @@ PresShell::GoToAnchor(const nsAString& aAnchorName, bool aScroll)
     
     
     nsRefPtr<nsIDOMRange> jumpToRange = new nsRange();
-    while (content && content->GetChildCount() > 0) {
-      content = content->GetChildAt(0);
+    while (content && content->GetFirstChild()) {
+      content = content->GetFirstChild();
     }
     nsCOMPtr<nsIDOMNode> node(do_QueryInterface(content));
     NS_ASSERTION(node, "No nsIDOMNode for descendant of anchor");
