5abff922e71653108412965bb3efecb6e67bd734
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 13:59:22 +0000

    Bug 638026 - Make sure we hold onto the right JS object here. r=jst
    UltraBlame original commit: 41ea0740c97dff3755ce68ab4a8f96e04cb7b59f

--- js/src/xpconnect/src/xpcvariant.cpp
+++ js/src/xpconnect/src/xpcvariant.cpp
@@ -63,6 +63,11 @@ XPCVariant::XPCVariant(XPCCallContext& ccx, jsval aJSVal)
     nsVariant::Initialize(&mData);
     if(!JSVAL_IS_PRIMITIVE(mJSVal))
     {
+        JSObject *obj = JSVAL_TO_OBJECT(mJSVal);
+        OBJ_TO_INNER_OBJECT(ccx, obj);
+
+        mJSVal = OBJECT_TO_JSVAL(obj);
+
         
         
         
