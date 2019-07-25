73405cace604e15d4d97ceaeb46fb96cb1b0fea6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:29:01 +0000

    Bug 715190 - Clear existing GL errors before trying to check for a new one r=clord
    UltraBlame original commit: 4d652a824f11cd35ef1a9a93784586b7d906a899

--- mobile/android/base/gfx/WidgetTileLayer.java
+++ mobile/android/base/gfx/WidgetTileLayer.java
@@ -50,6 +50,7 @@ import javax.microedition.khronos.opengles.GL10;
 
 
 public class WidgetTileLayer extends Layer {
+    private static final String LOGTAG = "WidgetTileLayer";
 
     private int[] mTextureIDs;
     private CairoImage mImage;
@@ -110,7 +111,16 @@ public class WidgetTileLayer extends Layer {
                                 0);
 
         float top = viewport.height() - (bounds.top + bounds.height());
+
+        
+        
+        while (GLES11.glGetError() != GLES11.GL_NO_ERROR);
+
         GLES11Ext.glDrawTexfOES(bounds.left, top, 0.0f, bounds.width(), bounds.height());
+        int error = GLES11.glGetError();
+        if (error != GLES11.GL_NO_ERROR) {
+            Log.i(LOGTAG, "Failed to draw texture: " + error);
+        }
     }
 }
 
