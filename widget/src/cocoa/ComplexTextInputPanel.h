4244ce94ddeac33a7a7748e9bf10777dce168a8e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:10:09 +0000

    Add support for Cocoa NPAPI complex text input. This patch contains a TSM-based implementation for 32-bit builds, and an NSInputContext-based implementation for 64-bit builds. b=512886 r=smichaud sr=jst
    UltraBlame original commit: c8f7506664304e22feb97d86ce20970ff5482e50

---
+++ widget/src/cocoa/ComplexTextInputPanel.h
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef ComplexTextInputPanel_h_
+#define ComplexTextInputPanel_h_
+
+#import <Cocoa/Cocoa.h>
+
+@interface ComplexTextInputPanel : NSPanel {
+  NSTextView *mInputTextView;
+}
+
++ (ComplexTextInputPanel*)sharedComplexTextInputPanel;
+
+- (NSTextInputContext*)inputContext;
+- (BOOL)interpretKeyEvent:(NSEvent*)event string:(NSString**)string;
+
+@end
+
+#endif 
