625ab50063e6ab3efcbe1725c60c8065e5c990c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 13:59:07 +0000

    Merge backout of bug 622072
    UltraBlame original commit: a2937d08aefbd86fd8f852565f15daeadac0ad07

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -6576,17 +6576,10 @@ nsIFrame::IsFocusable(PRInt32 *aTabIndex, PRBool aWithMouse)
         
         
         nsIScrollableFrame *scrollFrame = do_QueryFrame(this);
-        if (scrollFrame) {
-          nsIScrollableFrame::ScrollbarStyles styles =
-            scrollFrame->GetScrollbarStyles();
-          if (styles.mVertical == NS_STYLE_OVERFLOW_SCROLL ||
-              styles.mVertical == NS_STYLE_OVERFLOW_AUTO ||
-              styles.mHorizontal == NS_STYLE_OVERFLOW_SCROLL ||
-              styles.mHorizontal == NS_STYLE_OVERFLOW_AUTO) {
+        if (scrollFrame && !scrollFrame->GetActualScrollbarSizes().IsZero()) {
             
             isFocusable = PR_TRUE;
             tabIndex = 0;
-          }
         }
       }
     }
@@ -7597,8 +7590,8 @@ DR_FrameTypeInfo::DR_FrameTypeInfo(nsIAtom* aFrameType,
                                    const char* aFrameName)
 {
   mType = aFrameType;
-  strcpy(mNameAbbrev, aFrameNameAbbrev);
-  strcpy(mName, aFrameName);
+  PL_strncpyz(mNameAbbrev, aFrameNameAbbrev, sizeof(mNameAbbrev));
+  PL_strncpyz(mName, aFrameName, sizeof(mName));
 }
 
 struct DR_FrameTreeNode
