64b141e9383f07fbf7b10cd794d277a9a156d069
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:51:56 +0000

    Bug 728655 - Test for bug 713810; r=neil f=jwein
    UltraBlame original commit: b9abdbdbac1b684397697b812da11de5efd0dc1d

---
+++ toolkit/components/viewsource/test/browser/browser_bug713810.js
@@ -0,0 +1,28 @@
+
+
+
+
+var source = '<html xmlns="http://www.w3.org/1999/xhtml"><body><p>This is a paragraph.</p></body></html>';
+
+function test() {
+  waitForExplicitFinish();
+  testHTML();
+}
+
+function testHTML() {
+  openDocumentSelect("data:text/html," + source, "p", function(aWindow) {
+    is(aWindow.gBrowser.contentDocument.body.textContent,
+       "<p>This is a paragraph.</p>",
+       "Correct source for text/html");
+    closeViewSourceWindow(aWindow, testXHTML);
+  });
+}
+
+function testXHTML() {
+  openDocumentSelect("data:application/xhtml+xml," + source, "p", function(aWindow) {
+    is(aWindow.gBrowser.contentDocument.body.textContent,
+       '<p xmlns="http://www.w3.org/1999/xhtml">This is a paragraph.</p>',
+       "Correct source for application/xhtml+xml");
+    closeViewSourceWindow(aWindow, finish);
+  });
+}
