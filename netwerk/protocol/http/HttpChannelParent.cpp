ad086823b0f0a15fb3038b545658467ede07db88
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:56:00 +0000

    Bug 664163: Yes this will get backed out: see bug. r=jdm
    UltraBlame original commit: 9899522b6ad94a659643602a1549e2d7ecc51993

--- netwerk/protocol/http/HttpChannelParent.cpp
+++ netwerk/protocol/http/HttpChannelParent.cpp
@@ -450,6 +450,15 @@ HttpChannelParent::OnStartRequest(nsIRequest *aRequest, nsISupports *aContext)
   }
 
   nsHttpChannel *httpChan = static_cast<nsHttpChannel *>(mChannel.get());
+
+  
+  
+  PRNetAddr selfAddr = httpChan->GetSelfAddr();
+  PRNetAddr peerAddr = httpChan->GetPeerAddr();
+  if (selfAddr.raw.family != peerAddr.raw.family) {
+    NS_RUNTIMEABORT("Parent: socket has multiple address families!");
+  }
+
   if (mIPCClosed || 
       !SendOnStartRequest(responseHead ? *responseHead : nsHttpResponseHead(), 
                           !!responseHead,
@@ -457,7 +466,7 @@ HttpChannelParent::OnStartRequest(nsIRequest *aRequest, nsISupports *aContext)
                           isFromCache,
                           mCacheDescriptor ? true : false,
                           expirationTime, cachedCharset, secInfoSerialization,
-                          httpChan->GetSelfAddr(), httpChan->GetPeerAddr())) 
+                          selfAddr, peerAddr))
   {
     return NS_ERROR_UNEXPECTED; 
   }
