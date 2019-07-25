65a32ce077f0e3470564ca95261d754d14df8090
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:44:11 +0000

    Bug 785167 - Root while allowing cx to be NULL for some typed array APIs. r=terrence
    UltraBlame original commit: b2ba9bbf909f94860d256509b749c669a1be2227

--- js/xpconnect/src/XPCComponents.cpp
+++ js/xpconnect/src/XPCComponents.cpp
@@ -2700,7 +2700,7 @@ nsXPCComponents_Utils::LookupMethod(const JS::Value& object,
     
     if (!object.isObject())
         return NS_ERROR_XPC_BAD_CONVERT_JS;
-    JSObject *obj = &object.toObject();
+    JS::RootedObject obj(cx, &object.toObject());
 
     
     if (!JSVAL_IS_STRING(name))
