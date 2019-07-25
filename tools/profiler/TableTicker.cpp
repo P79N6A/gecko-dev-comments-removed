e74e1ea722615eb0880c55de8a2686ca8ec6f816
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:55:38 +0000

    Bug 734691 - Change profiler output format to support threads. r=mstange
    UltraBlame original commit: 34da6373715ffbedf4d2c636690fa607e00dee3e

--- tools/profiler/TableTicker.cpp
+++ tools/profiler/TableTicker.cpp
@@ -279,7 +279,7 @@ public:
       switch (entry.mTagName) {
         case 's':
           sample = b.CreateObject();
-          b.DefineProperty(sample, "name", entry.mTagData);
+          b.DefineProperty(sample, "name", (const char*)entry.mTagData);
           frames = b.CreateArray();
           b.DefineProperty(sample, "frames", frames);
           b.ArrayPush(samples, sample);
@@ -289,7 +289,10 @@ public:
           {
             if (sample) {
               JSObject *frame = b.CreateObject();
-              b.DefineProperty(frame, "location", entry.mTagData);
+              char tagBuff[1024];
+              void* pc = (void*)entry.mTagData;
+              snprintf(tagBuff, 1024, "%p", pc);
+              b.DefineProperty(frame, "location", tagBuff);
               b.ArrayPush(frames, frame);
             }
           }
@@ -370,6 +373,8 @@ class TableTicker: public Sampler {
     return &mPrimaryThreadProfile;
   }
 
+  JSObject *ToJSObject(JSContext *aCx);
+
 private:
   
   void doBacktrace(ThreadProfile &aProfile, Address pc);
@@ -436,6 +441,26 @@ void TableTicker::HandleSaveRequest()
   
   nsCOMPtr<nsIRunnable> runnable = new SaveProfileTask();
   NS_DispatchToMainThread(runnable);
+}
+
+JSObject* TableTicker::ToJSObject(JSContext *aCx)
+{
+  JSObjectBuilder b(aCx);
+
+  JSObject *profile = b.CreateObject();
+
+  
+  
+
+  
+  JSObject *threads = b.CreateArray();
+  b.DefineProperty(profile, "threads", threads);
+
+  
+  JSObject* threadSamples = GetPrimaryThreadProfile()->ToJSObject(aCx);
+  b.ArrayPush(threads, threadSamples);
+
+  return profile;
 }
 
 
@@ -685,7 +710,7 @@ JSObject *mozilla_sampler_get_profile_data(JSContext *aCx)
     return NULL;
   }
 
-  return t->GetPrimaryThreadProfile()->ToJSObject(aCx);
+  return t->ToJSObject(aCx);
 }
 
 
