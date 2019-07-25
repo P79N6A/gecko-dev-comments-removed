3f503a1ba37b9c26d71422a6782cfbccbb7007b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:27 +0000

    Bug 591927 - 'Search With' dialog keeps removing menu items on switch of AwesomeScreen panes and or rotation [r=mfinkle]
    UltraBlame original commit: 658d5bef890da575960d602e183604a9ef63537d

--- mobile/chrome/content/browser-ui.js
+++ mobile/chrome/content/browser-ui.js
@@ -2290,10 +2290,10 @@ var MenuListHelperUI = {
 
   sizeToContent: function sizeToContent() {
     
-    let popup = this._popup;
+    let popup = this._popup.lastChild;
     let preferredHeight = 0;
     for (let i=0; i<popup.childElementCount; i++) {
-      preferredHeight += popup.children[i].getBoundingClientRect().height;
+      preferredHeight += popup.childNodes[i].getBoundingClientRect().height;
     }
 
     
@@ -2301,7 +2301,7 @@ var MenuListHelperUI = {
     popup.width = popup.height = "";
 
     let rect = popup.getBoundingClientRect();
-    let height = Math.min(preferredHeight, 0.75 * window.innerWidth);
+    let height = Math.min(preferredHeight, 0.75 * window.innerHeight);
     let width = Math.min(rect.width, 0.75 * window.innerWidth);
 
     popup.height = height;
@@ -2393,7 +2393,7 @@ var ContextHelper = {
     popup.width = popup.height = "";
 
     let rect = popup.getBoundingClientRect();
-    let height = Math.min(preferredHeight, 0.75 * window.innerWidth);
+    let height = Math.min(preferredHeight, 0.75 * window.innerHeight);
     let width = Math.min(rect.width, 0.75 * window.innerWidth);
 
     popup.height = height;
