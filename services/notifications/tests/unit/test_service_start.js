da9e2474165b5b88587a29fad0f63b10bb82d72b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:27:53 +0000

    Backout bug 754062 (c53f474c502b) for xpcshell breakage
    UltraBlame original commit: 591fd1c696f0556fbbc6d49915d10a478b2695b0

--- services/notifications/tests/unit/test_service_start.js
+++
@@ -1,8 +0,0 @@
-
-function run_test() {
-  _("When imported, Service.onStartup is called.");
-  Cu.import("resource://services-notifications/service.js");
-
-  do_check_eq(Service.serverURL, "https://notifications.mozilla.org/");
-  do_check_eq(Service.ready, true);
-}
