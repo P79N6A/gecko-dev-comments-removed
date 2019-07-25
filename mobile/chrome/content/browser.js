95c385ccd26690ff8ffd46eb1f2ebb9c0450a944
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:33 +0000

    remove comments
    UltraBlame original commit: 83a825ac51e1ddb7df537b13514495513fee2fd4

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -298,7 +298,6 @@ var Browser = {
       },
 
       scrollTo: function(x, y) {
-        
         if (getBrowser().contentWindow) {
           getBrowser().contentWindow.scrollTo(x, y);
         }
@@ -309,7 +308,6 @@ var Browser = {
       },
 
       getPosition: function(scrollX, scrollY) {
-        
         if (getBrowser().contentWindow) {
           let cwu = Util.getWindowUtils(getBrowser().contentWindow);
           cwu.getScrollXY(false, scrollX, scrollY);
