334ad234f6929bbf4f2b61a0f2ec494118c0e9e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:07:00 +0000

    Bug 559714 - Backout bug 595293. Followup to the rest of bug 559714 landing. r=jduell a=blocking-fennec
    UltraBlame original commit: ddc9d52589111e2dfe3a896f49aab5734393fd06

--- netwerk/cache/nsCacheService.cpp
+++ netwerk/cache/nsCacheService.cpp
@@ -76,10 +76,6 @@
 
 #include "mozilla/FunctionTimer.h"
 
-#ifdef MOZ_IPC
-#include "mozilla/net/NeckoCommon.h"
-#endif
-
 
 
 
@@ -793,14 +789,6 @@ nsCacheService::DispatchToCacheIOThread(nsIRunnable* event)
 PRBool
 nsCacheProfilePrefObserver::DiskCacheEnabled()
 {
-#ifdef MOZ_IPC
-    
-    
-    
-    if (mozilla::net::IsNeckoChild())
-        return PR_FALSE;
-#endif
-
     if ((mDiskCacheCapacity == 0) || (!mDiskCacheParentDirectory))  return PR_FALSE;
     return mDiskCacheEnabled;
 }
@@ -858,13 +846,6 @@ nsCacheProfilePrefObserver::MemoryCacheEnabled()
 PRInt32
 nsCacheProfilePrefObserver::MemoryCacheCapacity()
 {
-#ifdef MOZ_IPC
-    
-    
-    if (mozilla::net::IsNeckoChild())
-        return 1024; 
-#endif
-
     PRInt32 capacity = mMemoryCacheCapacity;
     if (capacity >= 0) {
         CACHE_LOG_DEBUG(("Memory cache capacity forced to %d\n", capacity));
