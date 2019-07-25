8b057cb409717b79f06cff9f160221921b19fe8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:09:03 +0000

    Bug 749711 - Lots of WebGL texture conversion fixes and improvements - r=jgilbert
    * Templatize pack/unpack routines
    ** Inside anonymous namespace in a naive attempt to not hammer PGO linker
       memory usage.
    * Support conversions changing texel size
    * Support conversion from integer to float formats
    * Support RGBA32F properly
    * Avoid compiling useless paths (code size down to 17k from 44k)
    
    UltraBlame original commit: 9f87dbd4d39ce0874c90e8c124e101d65cc93e36

--- content/canvas/src/WebGLContext.h
+++ content/canvas/src/WebGLContext.h
@@ -128,16 +128,51 @@ struct BackbufferClearingStatus {
     enum { NotClearedSinceLastPresented, ClearedToDefaultValues, HasBeenDrawnTo };
 };
 
-struct WebGLTexelFormat {
-    enum { Generic, Auto, RGBA8, RGB8, RGBX8, BGRA8, BGR8, BGRX8, RGBA5551, RGBA4444, RGB565, R8, RA8, A8,
-           RGBA32F, RGB32F, A32F, R32F, RA32F };
+namespace WebGLTexelConversions {
+
+
+
+
+
+
+
+
+
+enum WebGLTexelFormat
+{
+    
+    
+    BadFormat,
+    
+    
+    
+    Auto,
+    
+    R8,
+    A8,
+    R32F, 
+    A32F, 
+    
+    RA8,
+    RA32F,
+    
+    RGB8,
+    BGRX8, 
+    RGB565,
+    RGB32F, 
+    
+    RGBA8,
+    BGRA8, 
+    RGBA5551,
+    RGBA4444,
+    RGBA32F 
 };
 
-struct WebGLTexelPremultiplicationOp {
-    enum { Generic, None, Premultiply, Unmultiply };
-};
+} 
 
-int GetWebGLTexelFormat(GLenum format, GLenum type);
+using WebGLTexelConversions::WebGLTexelFormat;
+
+WebGLTexelFormat GetWebGLTexelFormat(GLenum format, GLenum type);
 
 
 inline bool is_pot_assuming_nonnegative(WebGLsizei x)
@@ -1205,26 +1240,26 @@ protected:
                          WebGLenum format, WebGLenum type,
                          void *data, PRUint32 byteLength,
                          int jsArrayType,
-                         int srcFormat, bool srcPremultiplied);
+                         WebGLTexelFormat srcFormat, bool srcPremultiplied);
     void TexSubImage2D_base(WebGLenum target, WebGLint level,
                             WebGLint xoffset, WebGLint yoffset,
                             WebGLsizei width, WebGLsizei height, WebGLsizei srcStrideOrZero,
                             WebGLenum format, WebGLenum type,
                             void *pixels, PRUint32 byteLength,
                             int jsArrayType,
-                            int srcFormat, bool srcPremultiplied);
+                            WebGLTexelFormat srcFormat, bool srcPremultiplied);
     void TexParameter_base(WebGLenum target, WebGLenum pname,
                            WebGLint *intParamPtr, WebGLfloat *floatParamPtr);
 
     void ConvertImage(size_t width, size_t height, size_t srcStride, size_t dstStride,
                       const PRUint8*src, PRUint8 *dst,
-                      int srcFormat, bool srcPremultiplied,
-                      int dstFormat, bool dstPremultiplied,
+                      WebGLTexelFormat srcFormat, bool srcPremultiplied,
+                      WebGLTexelFormat dstFormat, bool dstPremultiplied,
                       size_t dstTexelSize);
 
     nsresult DOMElementToImageSurface(dom::Element* imageOrCanvas,
                                       gfxImageSurface **imageOut,
-                                      int *format);
+                                      WebGLTexelFormat *format);
 
     void CopyTexSubImage2D_base(WebGLenum target,
                                 WebGLint level,
---
+++ content/canvas/src/WebGLTexelConversions.cpp
@@ -0,0 +1,382 @@
+
+
+
+
+#include "WebGLTexelConversions.h"
+
+namespace mozilla {
+
+using namespace WebGLTexelConversions;
+
+namespace {
+
+
+
+
+
+
+
+
+
+
+
+
+
+class WebGLImageConverter
+{
+    const size_t mWidth, mHeight;
+    const void* const mSrcStart;
+    void* const mDstStart;
+    const ptrdiff_t mSrcStride, mDstStride;
+    bool mAlreadyRun;
+    bool mSuccess;
+
+    
+
+
+
+
+    template<int Format>
+    static size_t NumElementsPerTexelForFormat() {
+        switch (Format) {
+            case R8:
+            case A8:
+            case R32F:
+            case A32F:
+            case RGBA5551:
+            case RGBA4444:
+            case RGB565:
+                return 1;
+            case RA8:
+            case RA32F:
+                return 2;
+            case RGB8:
+            case RGB32F:
+                return 3;
+            case RGBA8:
+            case BGRA8:
+            case BGRX8:
+            case RGBA32F:
+                return 4;
+            default:
+                NS_ABORT_IF_FALSE(false, "Unknown texel format. Coding mistake?");
+                return 0;
+        }
+    }
+
+    
+
+
+
+
+
+
+
+    template<WebGLTexelFormat SrcFormat,
+             WebGLTexelFormat DstFormat,
+             WebGLTexelPremultiplicationOp PremultiplicationOp>
+    void run()
+    {
+        
+        
+        
+        
+        
+        
+
+        if (SrcFormat == DstFormat &&
+            PremultiplicationOp == NoPremultiplicationOp)
+        {
+            
+            
+            return;
+        }
+
+        
+        
+        
+        
+        const bool CanSrcFormatComeFromDOMElementOrImageData
+            = SrcFormat == BGRA8 ||
+              SrcFormat == BGRX8 ||
+              SrcFormat == A8 ||
+              SrcFormat == RGB565 ||
+              SrcFormat == RGBA8;
+        if (!CanSrcFormatComeFromDOMElementOrImageData &&
+            SrcFormat != DstFormat)
+        {
+            return;
+        }
+
+        
+        if (!CanSrcFormatComeFromDOMElementOrImageData &&
+            PremultiplicationOp == Unpremultiply)
+        {
+            return;
+        }
+
+        
+        
+        
+        
+        
+        if (!HasAlpha(SrcFormat) ||
+            !HasColor(SrcFormat) ||
+            !HasColor(DstFormat))
+        {
+
+            if (PremultiplicationOp != NoPremultiplicationOp)
+            {
+                return;
+            }
+        }
+
+        
+
+        NS_ABORT_IF_FALSE(!mAlreadyRun, "converter should be run only once!");
+        mAlreadyRun = true;
+
+        
+
+        typedef
+            typename DataTypeForFormat<SrcFormat>::Type
+            SrcType;
+        typedef
+            typename DataTypeForFormat<DstFormat>::Type
+            DstType;
+
+        const int IntermediateSrcFormat
+            = IntermediateFormat<SrcFormat>::Value;
+        const int IntermediateDstFormat
+            = IntermediateFormat<DstFormat>::Value;
+        typedef
+            typename DataTypeForFormat<IntermediateSrcFormat>::Type
+            IntermediateSrcType;
+        typedef
+            typename DataTypeForFormat<IntermediateDstFormat>::Type
+            IntermediateDstType;
+
+        const size_t NumElementsPerSrcTexel = NumElementsPerTexelForFormat<SrcFormat>();
+        const size_t NumElementsPerDstTexel = NumElementsPerTexelForFormat<DstFormat>();
+        const size_t MaxElementsPerTexel = 4;
+        NS_ABORT_IF_FALSE(NumElementsPerSrcTexel <= MaxElementsPerTexel, "unhandled format");
+        NS_ABORT_IF_FALSE(NumElementsPerDstTexel <= MaxElementsPerTexel, "unhandled format");
+
+        
+        
+        
+        
+        
+        
+        NS_ABORT_IF_FALSE(mSrcStride % sizeof(SrcType) == 0 &&
+                          mDstStride % sizeof(DstType) == 0,
+                          "Unsupported: texture stride is not a multiple of sizeof(type)");
+        const ptrdiff_t srcStrideInElements = mSrcStride / sizeof(SrcType);
+        const ptrdiff_t dstStrideInElements = mDstStride / sizeof(DstType);
+
+        const SrcType *srcRowStart = static_cast<const SrcType*>(mSrcStart);
+        DstType *dstRowStart = static_cast<DstType*>(mDstStart);
+
+        
+        for (size_t i = 0; i < mHeight; ++i) {
+            const SrcType *srcRowEnd = srcRowStart + mWidth * NumElementsPerSrcTexel;
+            const SrcType *srcPtr = srcRowStart;
+            DstType *dstPtr = dstRowStart;
+            while (srcPtr != srcRowEnd) {
+                
+                
+                
+                
+                
+                IntermediateSrcType unpackedSrc[MaxElementsPerTexel];
+                IntermediateDstType unpackedDst[MaxElementsPerTexel];
+
+                
+                
+                unpack<SrcFormat>(srcPtr, unpackedSrc);
+                
+                
+                convertType(unpackedSrc, unpackedDst);
+                
+                
+                pack<DstFormat, PremultiplicationOp>(unpackedDst, dstPtr);
+
+                srcPtr += NumElementsPerSrcTexel;
+                dstPtr += NumElementsPerDstTexel;
+            }
+            srcRowStart += srcStrideInElements;
+            dstRowStart += dstStrideInElements;
+        }
+
+        mSuccess = true;
+        return;
+    }
+
+    template<WebGLTexelFormat SrcFormat, WebGLTexelFormat DstFormat>
+    void run(WebGLTexelPremultiplicationOp premultiplicationOp)
+    {
+        #define WEBGLIMAGECONVERTER_CASE_PREMULTIPLICATIONOP(PremultiplicationOp) \
+            case PremultiplicationOp: \
+                return run<SrcFormat, DstFormat, PremultiplicationOp>();
+
+        switch (premultiplicationOp) {
+            WEBGLIMAGECONVERTER_CASE_PREMULTIPLICATIONOP(NoPremultiplicationOp)
+            WEBGLIMAGECONVERTER_CASE_PREMULTIPLICATIONOP(Premultiply)
+            WEBGLIMAGECONVERTER_CASE_PREMULTIPLICATIONOP(Unpremultiply)
+            default:
+                NS_ABORT_IF_FALSE(false, "unhandled case. Coding mistake?");
+        }
+
+        #undef WEBGLIMAGECONVERTER_CASE_PREMULTIPLICATIONOP
+    }
+
+    template<WebGLTexelFormat SrcFormat>
+    void run(WebGLTexelFormat dstFormat,
+             WebGLTexelPremultiplicationOp premultiplicationOp)
+    {
+        #define WEBGLIMAGECONVERTER_CASE_DSTFORMAT(DstFormat) \
+            case DstFormat: \
+                return run<SrcFormat, DstFormat>(premultiplicationOp);
+
+        switch (dstFormat) {
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(R8)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(A8)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(R32F)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(A32F)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(RA8)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(RA32F)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(RGB8)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(RGB565)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(RGB32F)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(RGBA8)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(RGBA5551)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(RGBA4444)
+            WEBGLIMAGECONVERTER_CASE_DSTFORMAT(RGBA32F)
+            default:
+                NS_ABORT_IF_FALSE(false, "unhandled case. Coding mistake?");
+        }
+
+        #undef WEBGLIMAGECONVERTER_CASE_DSTFORMAT
+    }
+
+public:
+
+    void run(WebGLTexelFormat srcFormat,
+             WebGLTexelFormat dstFormat,
+             WebGLTexelPremultiplicationOp premultiplicationOp)
+    {
+        #define WEBGLIMAGECONVERTER_CASE_SRCFORMAT(SrcFormat) \
+            case SrcFormat: \
+                return run<SrcFormat>(dstFormat, premultiplicationOp);
+
+        switch (srcFormat) {
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(R8)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(A8)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(R32F)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(A32F)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(RA8)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(RA32F)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(RGB8)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(BGRX8) 
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(RGB565)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(RGB32F)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(RGBA8)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(BGRA8)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(RGBA5551)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(RGBA4444)
+            WEBGLIMAGECONVERTER_CASE_SRCFORMAT(RGBA32F)
+            default:
+                NS_ABORT_IF_FALSE(false, "unhandled case. Coding mistake?");
+        }
+
+        #undef WEBGLIMAGECONVERTER_CASE_SRCFORMAT
+    }
+
+    WebGLImageConverter(size_t width, size_t height,
+                        const void* srcStart, void* dstStart,
+                        ptrdiff_t srcStride, ptrdiff_t dstStride)
+        : mWidth(width), mHeight(height),
+          mSrcStart(srcStart), mDstStart(dstStart),
+          mSrcStride(srcStride), mDstStride(dstStride),
+          mAlreadyRun(false), mSuccess(false)
+    {}
+
+    bool Success() const {
+        return mSuccess;
+    }
+};
+
+} 
+
+void
+WebGLContext::ConvertImage(size_t width, size_t height, size_t srcStride, size_t dstStride,
+                           const uint8_t* src, uint8_t *dst,
+                           WebGLTexelFormat srcFormat, bool srcPremultiplied,
+                           WebGLTexelFormat dstFormat, bool dstPremultiplied,
+                           size_t dstTexelSize)
+{
+    if (width <= 0 || height <= 0)
+        return;
+
+    const bool FormatsRequireNoPremultiplicationOp =
+        !HasAlpha(srcFormat) ||
+        !HasColor(srcFormat) ||
+        !HasColor(dstFormat);
+
+    if (srcFormat == dstFormat &&
+        (FormatsRequireNoPremultiplicationOp || srcPremultiplied == dstPremultiplied))
+    {
+        
+        
+        
+        
+        
+        
+        
+
+        NS_ABORT_IF_FALSE(mPixelStoreFlipY || srcStride != dstStride, "Performance trap -- should handle this case earlier, to avoid memcpy");
+
+        size_t row_size = width * dstTexelSize; 
+        const uint8_t* ptr = src;
+        const uint8_t* src_end = src + height * srcStride;
+
+        uint8_t* dst_row = mPixelStoreFlipY
+                           ? dst + (height-1) * dstStride
+                           : dst;
+        ptrdiff_t dstStrideSigned(dstStride);
+        ptrdiff_t dst_delta = mPixelStoreFlipY ? -dstStrideSigned : dstStrideSigned;
+
+        while(ptr != src_end) {
+            memcpy(dst_row, ptr, row_size);
+            ptr += srcStride;
+            dst_row += dst_delta;
+        }
+        return;
+    }
+
+    uint8_t* dstStart = dst;
+    ptrdiff_t signedDstStride = dstStride;
+    if (mPixelStoreFlipY) {
+        dstStart = dst + (height - 1) * dstStride;
+        signedDstStride = -dstStride;
+    }
+
+    WebGLImageConverter converter(width, height, src, dstStart, srcStride, signedDstStride);
+
+    const WebGLTexelPremultiplicationOp premultiplicationOp
+        = FormatsRequireNoPremultiplicationOp     ? NoPremultiplicationOp
+        : (!srcPremultiplied && dstPremultiplied) ? Premultiply
+        : (srcPremultiplied && !dstPremultiplied) ? Unpremultiply
+                                                  : NoPremultiplicationOp;
+
+    converter.run(srcFormat, dstFormat, premultiplicationOp);
+
+    if (!converter.Success()) {
+        
+        
+        
+        NS_RUNTIMEABORT("programming mistake in WebGL texture conversions");
+    }
+}
+
+} 
