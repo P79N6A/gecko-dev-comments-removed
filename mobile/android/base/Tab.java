836a167e859e92127f0f3c807531c44636f115d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:36:40 +0000

    Bug 779366 - Part 3: Move direct buffer allocation to DirectBufferAllocator. r=kats
    UltraBlame original commit: 4888b839508926a6da1a9f795cf18e8aaed78abe

--- mobile/android/base/Tab.java
+++ mobile/android/base/Tab.java
@@ -7,6 +7,7 @@ package org.mozilla.gecko;
 
 import org.mozilla.gecko.db.BrowserDB;
 import org.mozilla.gecko.gfx.Layer;
+import org.mozilla.gecko.mozglue.DirectBufferAllocator;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -142,9 +143,9 @@ public final class Tab {
         int capacity = getThumbnailWidth() * getThumbnailHeight() * 2 ;
         if (mThumbnailBuffer != null && mThumbnailBuffer.capacity() == capacity)
             return mThumbnailBuffer;
-        if (mThumbnailBuffer != null)
-            GeckoAppShell.freeDirectBuffer(mThumbnailBuffer); 
-        return mThumbnailBuffer = GeckoAppShell.allocateDirectBuffer(capacity);
+        freeBuffer();
+        mThumbnailBuffer = DirectBufferAllocator.allocate(capacity);
+        return mThumbnailBuffer;
     }
 
     public Bitmap getThumbnailBitmap() {
@@ -158,8 +159,7 @@ public final class Tab {
     }
 
     synchronized void freeBuffer() {
-        if (mThumbnailBuffer != null)
-            GeckoAppShell.freeDirectBuffer(mThumbnailBuffer);
+        DirectBufferAllocator.free(mThumbnailBuffer);
         mThumbnailBuffer = null;
     }
 
---
+++ mobile/android/base/mozglue/DirectBufferAllocator.java
@@ -0,0 +1,50 @@
+
+
+
+
+
+package org.mozilla.gecko.mozglue;
+
+import java.nio.ByteBuffer;
+
+
+
+
+
+
+
+public final class DirectBufferAllocator {
+    private DirectBufferAllocator() {}
+
+    public static ByteBuffer allocate(int size) {
+        if (size <= 0 || (size % 4) != 0) {
+            throw new IllegalArgumentException("Invalid size " + size);
+        }
+
+        ByteBuffer directBuffer = nativeAllocateDirectBuffer(size);
+        if (directBuffer == null) {
+            throw new OutOfMemoryError("allocateDirectBuffer() returned null");
+        } else if (!directBuffer.isDirect()) {
+            throw new AssertionError("allocateDirectBuffer() did not return a direct buffer");
+        }
+
+        return directBuffer;
+    }
+
+    public static ByteBuffer free(ByteBuffer buffer) {
+        if (buffer == null) {
+            return null;
+        }
+
+        if (!buffer.isDirect()) {
+            throw new IllegalArgumentException("buffer must be direct");
+        }
+
+        nativeFreeDirectBuffer(buffer);
+        return null;
+    }
+
+    
+    private static native ByteBuffer nativeAllocateDirectBuffer(long size);
+    private static native void nativeFreeDirectBuffer(ByteBuffer buf);
+}
