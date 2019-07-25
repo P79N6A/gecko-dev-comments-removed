cf083fa8280088b4012e0a3c0796689026012aeb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:27:12 +0000

    Bug 764718 - Part 1: Set the initial principal of in-process <iframe mozbrowser> correctly. r=bz
    UltraBlame original commit: 3a1129278710baa8e23a9d5e760622f7e418f465

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -8810,7 +8810,9 @@ nsDocShell::GetInheritedPrincipal(bool aConsiderCurrentDocument)
         inheritedFromCurrent = true;
     }
 
-    if (!document) {
+    
+    
+    if (!document && !mIsBrowserFrame) {
         nsCOMPtr<nsIDocShellTreeItem> parentItem;
         GetSameTypeParent(getter_AddRefs(parentItem));
         if (parentItem) {
