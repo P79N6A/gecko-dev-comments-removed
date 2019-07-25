aac95d4f8cee7603b152c1b12d9455a2294dd455
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:07:27 +0000

    Bug 641426. Part 4: Create Margin template. r=joe,sr=cjones
    UltraBlame original commit: c3bf4b3ec506aa649b0fd53af467346cb0ed4712

---
+++ gfx/src/BaseMargin.h
@@ -0,0 +1,105 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef MOZILLA_BASEMARGIN_H_
+#define MOZILLA_BASEMARGIN_H_
+
+#include "gfxCore.h"
+
+namespace mozilla {
+
+
+
+
+
+template <class T, class Sub>
+struct BaseMargin {
+  typedef mozilla::css::Side Side;
+
+  T top, right, bottom, left;
+
+  
+  BaseMargin() : top(0), right(0), bottom(0), left(0) {}
+  BaseMargin(T aLeft, T aTop, T aRight, T aBottom) :
+      top(aTop), right(aRight), bottom(aBottom), left(aLeft) {}
+
+  void SizeTo(T aLeft, T aTop, T aRight, T aBottom)
+  {
+    left = aLeft; top = aTop; right = aRight; bottom = aBottom;
+  }
+
+  T LeftRight() const { return left + right; }
+  T TopBottom() const { return top + bottom; }
+
+  T& side(Side aSide) {
+    NS_PRECONDITION(aSide <= NS_SIDE_LEFT, "Out of range side");
+    return *(&top + aSide);
+  }
+  T side(Side aSide) const {
+    NS_PRECONDITION(aSide <= NS_SIDE_LEFT, "Out of range side");
+    return *(&top + aSide);
+  }
+
+  
+  
+  bool operator==(const Sub& aMargin) const {
+    return left == aMargin.left && top == aMargin.top &&
+           right == aMargin.right && bottom == aMargin.bottom;
+  }
+  bool operator!=(const Sub& aMargin) const {
+    return !(*this == aMargin);
+  }
+  Sub operator+(const Sub& aMargin) const {
+    return Sub(left + aMargin.left, top + aMargin.top,
+             right + aMargin.right, bottom + aMargin.bottom);
+  }
+  Sub operator-(const Sub& aMargin) const {
+    return Sub(left - aMargin.left, top - aMargin.top,
+             right - aMargin.right, bottom - aMargin.bottom);
+  }
+  Sub& operator+=(const Sub& aMargin) {
+    left += aMargin.left;
+    top += aMargin.top;
+    right += aMargin.right;
+    bottom += aMargin.bottom;
+    return *static_cast<Sub*>(this);
+  }
+};
+
+}
+
+#endif 
