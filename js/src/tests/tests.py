7d9175dea9e1edb2a1b354b3bf8ca8af6543a251
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:15:57 +0000

    Backed out changeset a5e2394cd75d
    UltraBlame original commit: 49173592f6df108964d523864fda7f5c69a5b757

--- js/src/tests/tests.py
+++ js/src/tests/tests.py
@@ -76,12 +76,8 @@ class Test(object):
 
     def get_command(self, js_cmd_prefix):
         dir, filename = os.path.split(self.path)
-        cmd = js_cmd_prefix + Test.prefix_command(dir)
-        if self.debugMode:
-            cmd += [ '-d' ]
         
-        cmd += [ '-f', './' + self.path ]
-        return cmd
+        return js_cmd_prefix + Test.prefix_command(dir) + [ '-f', './' + self.path ]
 
     def run(self, js_cmd_prefix, timeout=30.0):
         cmd = self.get_command(js_cmd_prefix)
@@ -91,13 +87,12 @@ class Test(object):
 class TestCase(Test):
     """A test case consisting of a test and an expected result."""
 
-    def __init__(self, path, enable, expect, random, slow, debugMode):
+    def __init__(self, path, enable, expect, random, slow):
         Test.__init__(self, path)
         self.enable = enable     
         self.expect = expect     
         self.random = random     
         self.slow = slow         
-        self.debugMode = debugMode 
 
     def __str__(self):
         ans = self.path
@@ -109,8 +104,6 @@ class TestCase(Test):
             ans += ', random'
         if self.slow:
             ans += ', slow'
-        if self.debugMode:
-            ans += ', debugMode'
         return ans
 
 class TestOutput:
