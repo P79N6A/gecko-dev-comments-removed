3ea8cd45e28b9863ea788f0b56f5879f6d0acced
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:05:09 +0000

    bug 646983 - set the correct text length for the PropertyProvider. r=roc
    UltraBlame original commit: f32943a4e4e4f24f934a9f8e86b8f176ed2cd2d5

--- layout/generic/nsTextFrameThebes.cpp
+++ layout/generic/nsTextFrameThebes.cpp
@@ -6042,7 +6042,7 @@ nsTextFrame::AddInlineMinWidthForFlow(nsIRenderingContext *aRenderingContext,
   PRBool hyphenating = frag->GetLength() > 0 &&
     (mTextRun->GetFlags() & gfxTextRunFactory::TEXT_ENABLE_HYPHEN_BREAKS) != 0;
   if (hyphenating) {
-    len = GetInFlowContentLength() - iter.GetOriginalOffset();
+    len = GetContentOffset() + GetInFlowContentLength() - iter.GetOriginalOffset();
 #ifdef DEBUG
     
     
