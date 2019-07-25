0981b7b86bc2807f6afe08f273c654bef56292d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:52:22 +0000

    Bug 579262. Fix logic around mNeedsToUpdatePluginGeometry so we don't miss updating geometry for some plugins. r=tnikkel
    UltraBlame original commit: 10f6355b84759e8a628deaf0d6c0268a47e92adc

--- layout/base/nsPresContext.h
+++ layout/base/nsPresContext.h
@@ -1267,6 +1267,9 @@ public:
 
 private:
   nsTHashtable<nsPtrHashKey<nsObjectFrame> > mRegisteredPlugins;
+  
+  
+  
   nsIFrame* mUpdatePluginGeometryForFrame;
   PRPackedBool mNeedsToUpdatePluginGeometry;
 };
