c2aa036eb0ea10e5b10c691866c76bfed8769b9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:09:00 +0000

    Bug 668392: backout commit without bug#
    UltraBlame original commit: 27ecabf7e55e9a9b5d9a0900e275fb50ef14e9d3

--- toolkit/components/telemetry/TelemetryPing.js
+++ toolkit/components/telemetry/TelemetryPing.js
@@ -40,7 +40,6 @@ const Cu = Components.utils;
 
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-Cu.import("resource://gre/modules/LightweightThemeManager.jsm");
 
 
 const PAYLOAD_VERSION = 1;
@@ -234,54 +233,6 @@ TelemetryPing.prototype = {
   
 
 
-
-
-
-
-
-  getMetadata: function getMetadata(reason) {
-    let ai = Services.appinfo;
-    let ret = {
-      reason: reason,
-      OS: ai.OS,
-      appID: ai.ID,
-      appVersion: ai.version,
-      appName: ai.name,
-      appBuildID: ai.appBuildID,
-      platformBuildID: ai.platformBuildID,
-    };
-
-    
-    let sysInfo = Cc["@mozilla.org/system-info;1"].getService(Ci.nsIPropertyBag2);
-    let fields = ["cpucount", "memsize", "arch", "version", "device", "manufacturer", "hardware"];
-    for each (let field in fields) {
-      let value;
-      try {
-        value = sysInfo.getProperty(field);
-      } catch (e) {
-        continue
-      }
-      if (field == "memsize") {
-        
-        
-        value = Math.round(value / 1024 / 1024)
-      }
-      ret[field] = value
-    }
-
-    let theme = LightweightThemeManager.currentTheme;
-    if (theme)
-      ret.persona = theme.id;
-
-    if (this._addons)
-      ret.addons = this._addons;
-
-    return ret;
-  },
-
-  
-
-
   gatherMemory: function gatherMemory() {
     let mgr;
     try {
@@ -345,11 +296,10 @@ TelemetryPing.prototype = {
     this.gatherMemory();
     let payload = {
       ver: PAYLOAD_VERSION,
-      info: this.getMetadata(reason),
+      info: getMetadata(reason),
       simpleMeasurements: getSimpleMeasurements(),
       histograms: getHistograms()
     };
-
     let isTestPing = (reason == "test-ping");
     
     
@@ -455,9 +405,6 @@ TelemetryPing.prototype = {
     var server = this._server;
 
     switch (aTopic) {
-    case "Add-ons":
-      this._addons = aData;
-      break;
     case "profile-after-change":
       this.setup();
       break;
