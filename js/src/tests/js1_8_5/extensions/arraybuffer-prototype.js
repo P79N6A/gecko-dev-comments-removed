c0391204f39fb256c0839fb0b0b3532c9247dfe2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:29:17 +0000

    Bug 665355 - Make delegate object and ArrayBuffer share same prototype. r=mrbkap
    UltraBlame original commit: c3ceee49ac37061748459147fe52841be9f0bc47

---
+++ js/src/tests/js1_8_5/extensions/arraybuffer-prototype.js
@@ -0,0 +1,30 @@
+
+
+
+
+var BUGNUMBER = 665961;
+var summary =
+  "ArrayBuffer cannot access properties defined on the prototype chain.";
+print(BUGNUMBER + ": " + summary);
+
+
+
+
+
+var x = { prop: "value" };
+var a = new ArrayBuffer([]);
+a.__proto__ = x;
+assertEq(a.prop, "value");
+
+ArrayBuffer.prototype.prop = "on prototype";
+var b = new ArrayBuffer([]);
+assertEq(b.prop, "on prototype");
+
+var c = new ArrayBuffer([]);
+assertEq(c.prop, "on prototype");
+c.prop = "direct";
+assertEq(c.prop, "direct");
+
+assertEq(c.nonexistent, undefined);
+
+reportCompare(true, true);
