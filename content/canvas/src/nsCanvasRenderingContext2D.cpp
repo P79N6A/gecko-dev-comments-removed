8960dd86b092278fe9140fd2c29dd53cfb7cef86
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:28:53 +0000

    Bug 575347. canvas: remove unnecessary call to UpdateSurfaceClip(). r=vlad
    UpdateSurfaceClip isn't really useful so we can save some time by not calling
    UpdateSurfaceClip.
    
    UltraBlame original commit: f298567cdf2c2feb7ba7d0666e559f195a7e3bd9

--- content/canvas/src/nsCanvasRenderingContext2D.cpp
+++ content/canvas/src/nsCanvasRenderingContext2D.cpp
@@ -3265,17 +3265,6 @@ nsCanvasRenderingContext2D::DrawImage(nsIDOMElement *imgElt, float a1,
         mThebes->Paint(CurrentState().globalAlpha);
     }
 
-#if 1
-    
-    
-    
-    
-    
-    
-    
-    mThebes->UpdateSurfaceClip();
-#endif
-
 FINISH:
     if (NS_SUCCEEDED(rv))
         rv = Redraw(dirty);
