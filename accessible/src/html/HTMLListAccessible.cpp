ff661d8e61e4bc7294fe4dbb7808baf59f235264
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:09:01 +0000

    Bug 751828 - densify nsHTMLListAccessible, r=tbsaunde
    UltraBlame original commit: 730cc501c93998549560a2900b1c0ba37d37937e

---
+++ accessible/src/html/HTMLListAccessible.cpp
@@ -0,0 +1,201 @@
+
+
+
+
+
+
+#include "HTMLListAccessible.h"
+
+#include "nsDocAccessible.h"
+#include "Role.h"
+#include "States.h"
+
+#include "nsBlockFrame.h"
+
+using namespace mozilla;
+using namespace mozilla::a11y;
+
+
+
+
+
+NS_IMPL_ISUPPORTS_INHERITED0(HTMLListAccessible, nsHyperTextAccessible)
+
+role
+HTMLListAccessible::NativeRole()
+{
+  if (mContent->Tag() == nsGkAtoms::dl)
+    return roles::DEFINITION_LIST;
+
+  return roles::LIST;
+}
+
+PRUint64
+HTMLListAccessible::NativeState()
+{
+  return nsHyperTextAccessibleWrap::NativeState() | states::READONLY;
+}
+
+
+
+
+
+
+HTMLLIAccessible::
+  HTMLLIAccessible(nsIContent* aContent, nsDocAccessible* aDoc) :
+  nsHyperTextAccessibleWrap(aContent, aDoc), mBullet(nsnull)
+{
+  mFlags |= eHTMLListItemAccessible;
+
+  nsBlockFrame* blockFrame = do_QueryFrame(GetFrame());
+  if (blockFrame && blockFrame->HasBullet()) {
+    mBullet = new HTMLListBulletAccessible(mContent, mDoc);
+    if (!Document()->BindToDocument(mBullet, nsnull))
+      mBullet = nsnull;
+  }
+}
+
+NS_IMPL_ISUPPORTS_INHERITED0(HTMLLIAccessible, nsHyperTextAccessible)
+
+void
+HTMLLIAccessible::Shutdown()
+{
+  mBullet = nsnull;
+
+  nsHyperTextAccessibleWrap::Shutdown();
+}
+
+role
+HTMLLIAccessible::NativeRole()
+{
+  if (mContent->Tag() == nsGkAtoms::dt)
+    return roles::TERM;
+
+  return roles::LISTITEM;
+}
+
+PRUint64
+HTMLLIAccessible::NativeState()
+{
+  return nsHyperTextAccessibleWrap::NativeState() | states::READONLY;
+}
+
+NS_IMETHODIMP
+HTMLLIAccessible::GetBounds(PRInt32* aX, PRInt32* aY,
+                            PRInt32* aWidth, PRInt32* aHeight)
+{
+  nsresult rv = nsAccessibleWrap::GetBounds(aX, aY, aWidth, aHeight);
+  if (NS_FAILED(rv) || !mBullet)
+    return rv;
+
+  PRInt32 bulletX = 0, bulletY = 0, bulletWidth = 0, bulletHeight = 0;
+  rv = mBullet->GetBounds(&bulletX, &bulletY, &bulletWidth, &bulletHeight);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  *aX = bulletX; 
+  *aWidth += bulletWidth;
+  return NS_OK;
+}
+
+
+
+
+void
+HTMLLIAccessible::UpdateBullet(bool aHasBullet)
+{
+  if (aHasBullet == !!mBullet) {
+    NS_NOTREACHED("Bullet and accessible are in sync already!");
+    return;
+  }
+
+  nsDocAccessible* document = Document();
+  if (aHasBullet) {
+    mBullet = new HTMLListBulletAccessible(mContent, mDoc);
+    if (document->BindToDocument(mBullet, nsnull)) {
+      InsertChildAt(0, mBullet);
+    }
+  } else {
+    RemoveChild(mBullet);
+    document->UnbindFromDocument(mBullet);
+    mBullet = nsnull;
+  }
+
+  
+  
+}
+
+
+
+
+void
+HTMLLIAccessible::CacheChildren()
+{
+  if (mBullet)
+    AppendChild(mBullet);
+
+  
+  nsAccessibleWrap::CacheChildren();
+}
+
+
+
+
+
+
+
+
+bool
+HTMLListBulletAccessible::IsPrimaryForNode() const
+{
+  return false;
+}
+
+
+
+
+ENameValueFlag
+HTMLListBulletAccessible::Name(nsString &aName)
+{
+  aName.Truncate();
+
+  
+  nsBlockFrame* blockFrame = do_QueryFrame(mContent->GetPrimaryFrame());
+  NS_ASSERTION(blockFrame, "No frame for list item!");
+  if (blockFrame) {
+    blockFrame->GetBulletText(aName);
+
+    
+    aName.Append(' ');
+  }
+
+  return eNameOK;
+}
+
+role
+HTMLListBulletAccessible::NativeRole()
+{
+  return roles::STATICTEXT;
+}
+
+PRUint64
+HTMLListBulletAccessible::NativeState()
+{
+  PRUint64 state = nsLeafAccessible::NativeState();
+
+  state &= ~states::FOCUSABLE;
+  state |= states::READONLY;
+  return state;
+}
+
+void
+HTMLListBulletAccessible::AppendTextTo(nsAString& aText, PRUint32 aStartOffset,
+                                       PRUint32 aLength)
+{
+  nsAutoString bulletText;
+  nsBlockFrame* blockFrame = do_QueryFrame(mContent->GetPrimaryFrame());
+  NS_ASSERTION(blockFrame, "No frame for list item!");
+  if (blockFrame)
+    blockFrame->GetBulletText(bulletText);
+
+  aText.Append(Substring(bulletText, aStartOffset, aLength));
+}
---
+++ accessible/src/html/HTMLListAccessible.h
@@ -0,0 +1,104 @@
+
+
+
+
+
+
+#ifndef mozilla_a11y_HTMLListAccessible_h__
+#define mozilla_a11y_HTMLListAccessible_h__
+
+#include "nsHyperTextAccessibleWrap.h"
+#include "nsBaseWidgetAccessible.h"
+
+namespace mozilla {
+namespace a11y {
+
+class HTMLListBulletAccessible;
+
+
+
+
+class HTMLListAccessible : public nsHyperTextAccessibleWrap
+{
+public:
+  HTMLListAccessible(nsIContent* aContent, nsDocAccessible* aDoc) :
+    nsHyperTextAccessibleWrap(aContent, aDoc) { }
+  virtual ~HTMLListAccessible() { }
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  virtual a11y::role NativeRole();
+  virtual PRUint64 NativeState();
+};
+
+
+
+
+
+class HTMLLIAccessible : public nsHyperTextAccessibleWrap
+{
+public:
+  HTMLLIAccessible(nsIContent* aContent, nsDocAccessible* aDoc);
+  virtual ~HTMLLIAccessible() { }
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  virtual void Shutdown();
+
+  
+  NS_IMETHOD GetBounds(PRInt32* aX, PRInt32* aY,
+                       PRInt32* aWidth, PRInt32* aHeight);
+
+  
+  virtual a11y::role NativeRole();
+  virtual PRUint64 NativeState();
+
+  
+  void UpdateBullet(bool aHasBullet);
+
+protected:
+  
+  virtual void CacheChildren();
+
+private:
+  nsRefPtr<HTMLListBulletAccessible> mBullet;
+};
+
+
+
+
+
+class HTMLListBulletAccessible : public nsLeafAccessible
+{
+public:
+  HTMLListBulletAccessible(nsIContent* aContent, nsDocAccessible* aDoc) :
+    nsLeafAccessible(aContent, aDoc) { }
+  virtual ~HTMLListBulletAccessible() { }
+
+  
+  virtual bool IsPrimaryForNode() const;
+
+  
+  virtual ENameValueFlag Name(nsString& aName);
+  virtual a11y::role NativeRole();
+  virtual PRUint64 NativeState();
+  virtual void AppendTextTo(nsAString& aText, PRUint32 aStartOffset = 0,
+                            PRUint32 aLength = PR_UINT32_MAX);
+};
+
+} 
+} 
+
+
+inline mozilla::a11y::HTMLLIAccessible*
+nsAccessible::AsHTMLListItem()
+{
+  return mFlags & eHTMLListItemAccessible ?
+    static_cast<mozilla::a11y::HTMLLIAccessible*>(this) : nsnull;
+}
+
+#endif
