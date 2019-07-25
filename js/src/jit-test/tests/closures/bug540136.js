813f7d9547afc1d6588edb02f5a398aee6d0529c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:12:42 +0000

    Backed out changeset fd2d1860e2ac because it seems to delete hundreds of filse I didn't want deleted.
    UltraBlame original commit: b37edcf06127f390fee2a052a4c43e601ef63783

---
+++ js/src/jit-test/tests/closures/bug540136.js
@@ -0,0 +1,17 @@
+
+
+(eval("\
+ (function () {\
+   for (var[x] = function(){} in \
+     (function m(a) {\
+       if (a < 1) {\
+         x;\
+         return\
+       }\
+       return m(a - 1) + m(a - 2)\
+     })(7)\
+     (eval(\"\"))\
+   )\
+   ([])\
+ })\
+"))()
