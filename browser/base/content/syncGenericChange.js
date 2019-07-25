9c2f8169015c30a0e0a1bd6a34fdd222e3900b83
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:57:53 +0000

    Bug 590613 - Sync UI: Avoid accessing Weave.Service (= importing service.js), r=mconnor a=gavin
    UltraBlame original commit: 2ef01e996c0198a0115920cc18bcca9cf269e2d5

--- browser/base/content/syncGenericChange.js
+++ browser/base/content/syncGenericChange.js
@@ -36,7 +36,7 @@
 
 
 
-Components.utils.import("resource://services-sync/service.js");
+Components.utils.import("resource://services-sync/main.js");
 Components.utils.import("resource://gre/modules/Services.jsm");
 
 let Change = {
