001dba04c10d6dca3cdc40d24647aae300ce5871
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:31 +0000

    Bug 595625 - AutoCompleteCache does not use the new getLabelAt method [r=mfinkle]
    UltraBlame original commit: 5e510316ec4a7092230d301c20009949886c685b

--- mobile/components/AutoCompleteCache.js
+++ mobile/components/AutoCompleteCache.js
@@ -208,6 +208,7 @@ var AutoCompleteUtils = {
 
       
       result.getValueAt = function(index) this.data[index][0];
+      result.getLabelAt = function(index) this.data[index][0];
       result.getCommentAt = function(index) this.data[index][1];
       result.getStyleAt = function(index) this.data[index][2];
       result.getImageAt = function(index) this.data[index][3];
