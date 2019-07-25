909ee4140b1ae7e4b05d692767b62a5f67cc58da
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:31:05 +0000

    Bug 711914 - Fetching bookmarks information during onBeforeItemRemove may break the bookmarks cache. r=dietrich
    UltraBlame original commit: 3c371703da3c1b5d08447509e83fb83128318e87

---
+++ toolkit/components/places/tests/bookmarks/test_711914.js
@@ -0,0 +1,58 @@
+
+
+
+
+function run_test() {
+  
+
+
+
+
+  function forceBookmarkCaching(aItemId) {
+    let parent = PlacesUtils.bookmarks.getFolderIdForItem(aItemId);
+    PlacesUtils.bookmarks.getFolderIdForItem(parent);
+  }
+
+  let observer = {
+    onBeginUpdateBatch: function () forceBookmarkCaching(itemId1),
+    onEndUpdateBatch: function () forceBookmarkCaching(itemId1),
+    onItemAdded: forceBookmarkCaching,
+    onItemChanged: forceBookmarkCaching,
+    onItemMoved: forceBookmarkCaching,
+    onBeforeItemRemoved: forceBookmarkCaching,
+    onItemRemoved: function (id) {
+      try {
+        forceBookmarkCaching(id);
+        do_throw("trying to fetch a removed bookmark should throw");
+      } catch (ex) {}
+    },
+    onItemVisited: forceBookmarkCaching,
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsINavBookmarkObserver])
+  };
+  PlacesUtils.bookmarks.addObserver(observer, false);
+
+  let folder1 = PlacesUtils.bookmarks
+                           .createFolder(PlacesUtils.bookmarksMenuFolderId,
+                                         "Folder1",
+                                         PlacesUtils.bookmarks.DEFAULT_INDEX);
+  let folder2 = PlacesUtils.bookmarks
+                           .createFolder(folder1,
+                                         "Folder2",
+                                         PlacesUtils.bookmarks.DEFAULT_INDEX);
+  let itemId = PlacesUtils.bookmarks
+                           .insertBookmark(folder2,
+                                           NetUtil.newURI("http://mozilla.org/"),
+                                           PlacesUtils.bookmarks.DEFAULT_INDEX,
+                                           "Mozilla");
+
+  PlacesUtils.bookmarks.removeFolderChildren(folder1);
+
+  
+  do_check_eq(PlacesUtils.bookmarks.getItemTitle(folder1), "Folder1");
+  try {
+    PlacesUtils.bookmarks.getItemTitle(folder2);
+    do_throw("trying to fetch a removed bookmark should throw");
+  } catch (ex) {}
+
+  PlacesUtils.bookmarks.removeObserver(observer, false);
+}
