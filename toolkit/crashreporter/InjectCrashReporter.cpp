f22dc03977aa98debebb9f1a86912b24a7daef04
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:29:03 +0000

    Bug 769048 part E - Core crashreporter support for injecting a crashreporter DLL into an arbitrary process and callbacks for notifications when that process has crashed, r=ehsan
    UltraBlame original commit: 52b93da290236f84bf62bac550eb07a4e359be01

---
+++ toolkit/crashreporter/InjectCrashReporter.cpp
@@ -0,0 +1,84 @@
+
+
+
+
+
+#include "InjectCrashReporter.h"
+#include "nsDirectoryServiceUtils.h"
+#include "nsDirectoryServiceDefs.h"
+#include "client/windows/crash_generation/crash_generation_client.h"
+#include "nsExceptionHandler.h"
+#include "LoadLibraryRemote.h"
+#include "nsWindowsHelpers.h"
+
+using google_breakpad::CrashGenerationClient;
+using CrashReporter::GetChildNotificationPipe;
+
+namespace mozilla {
+
+InjectCrashRunnable::InjectCrashRunnable(DWORD pid)
+  : mPID(pid)
+{
+  nsCOMPtr<nsIFile> dll;
+  nsresult rv = NS_GetSpecialDirectory(NS_GRE_DIR, getter_AddRefs(dll));
+  if (NS_SUCCEEDED(rv)) {
+    dll->Append(NS_LITERAL_STRING("breakpadinjector.dll"));
+    dll->GetPath(mInjectorPath);
+  }
+}
+
+NS_IMETHODIMP
+InjectCrashRunnable::Run()
+{
+  if (mInjectorPath.IsEmpty())
+    return NS_OK;
+
+  nsAutoHandle hProcess(
+    OpenProcess(PROCESS_CREATE_THREAD |
+                PROCESS_QUERY_INFORMATION |
+                PROCESS_DUP_HANDLE |
+                PROCESS_VM_OPERATION |
+                PROCESS_VM_WRITE |
+                PROCESS_VM_READ, FALSE, mPID));
+  if (!hProcess) {
+    NS_WARNING("Unable to open remote process handle for crashreporter injection.");
+    return NS_OK;
+  }
+
+  void* proc = LoadRemoteLibraryAndGetAddress(hProcess, mInjectorPath.get(),
+                                              "Start");
+  if (!proc) {
+    NS_WARNING("Unable to inject crashreporter DLL.");
+    return NS_OK;
+  }
+
+  HANDLE hRemotePipe =
+    CrashGenerationClient::DuplicatePipeToClientProcess(
+      NS_ConvertASCIItoUTF16(GetChildNotificationPipe()).get(),
+      hProcess);
+  if (INVALID_HANDLE_VALUE == hRemotePipe) {
+    NS_WARNING("Unable to duplicate crash reporter pipe to process.");
+    return NS_OK;
+  }
+
+  nsAutoHandle hThread(CreateRemoteThread(hProcess, NULL, 0,
+                                          (LPTHREAD_START_ROUTINE) proc,
+                                          (void*) hRemotePipe, 0, NULL));
+  if (!hThread) {
+    NS_WARNING("Unable to CreateRemoteThread");
+
+    
+    
+    HANDLE toClose = INVALID_HANDLE_VALUE;
+    if (DuplicateHandle(hProcess, hRemotePipe, ::GetCurrentProcess(),
+                        &toClose, 0, FALSE,
+                        DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS)) {
+      CloseHandle(toClose);
+      return NS_OK;
+    }
+  }
+
+  return NS_OK;
+}
+
+} 
---
+++ toolkit/crashreporter/InjectCrashReporter.h
@@ -0,0 +1,23 @@
+
+
+
+
+
+#include "nsThreadUtils.h"
+#include <windows.h>
+
+namespace mozilla {
+
+class InjectCrashRunnable : public nsRunnable
+{
+public:
+  InjectCrashRunnable(DWORD pid);
+
+  NS_IMETHOD Run();
+
+private:
+  DWORD mPID;
+  nsString mInjectorPath;
+};
+  
+} 
