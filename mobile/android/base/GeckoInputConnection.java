a013201262cdaf28f58430961371943aecd1b96b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:36:44 +0000

    Bug 779366 - Part 2c: Query resources for DisplayMetrics, not the WindowManager. r=sriram
    UltraBlame original commit: 9dc8f2dc324a61b60a1a6ceb07f7d2bde23a4e1b

--- mobile/android/base/GeckoInputConnection.java
+++ mobile/android/base/GeckoInputConnection.java
@@ -840,7 +840,7 @@ public class GeckoInputConnection
             outAttrs.actionLabel = mIMEActionHint;
 
         GeckoApp app = GeckoApp.mAppContext;
-        DisplayMetrics metrics = app.getDisplayMetrics();
+        DisplayMetrics metrics = app.getResources().getDisplayMetrics();
         if (Math.min(metrics.widthPixels, metrics.heightPixels) > INLINE_IME_MIN_DISPLAY_SIZE) {
             
             
