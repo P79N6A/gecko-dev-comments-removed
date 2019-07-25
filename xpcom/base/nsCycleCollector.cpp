d536ae1d041bd6c6d81edb05ae701e428ccb7066
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:20:29 +0000

    Bug 757977 - Don't use uint32 in nsCycleCollector::ScanWeakMaps; r=bsmedberg
    UltraBlame original commit: 1cb527eb50412c516ae7ed744592f2f115f707e9

--- xpcom/base/nsCycleCollector.cpp
+++ xpcom/base/nsCycleCollector.cpp
@@ -2134,8 +2134,8 @@ nsCycleCollector::ScanWeakMaps()
             WeakMapping *wm = &mGraph.mWeakMaps[i];
 
             
-            uint32 mColor = wm->mMap ? wm->mMap->mColor : black;
-            uint32 kColor = wm->mKey ? wm->mKey->mColor : black;
+            uint32_t mColor = wm->mMap ? wm->mMap->mColor : black;
+            uint32_t kColor = wm->mKey ? wm->mKey->mColor : black;
             PtrInfo *v = wm->mVal;
 
             
