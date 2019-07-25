9504d28091fb41982cb3b9cf304afbfa684bfff9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:39:42 +0000

    Bug 616127 - Prevent nsIFrame.h from being used from non-internal code. r+a=bz
    UltraBlame original commit: 57a6ec830cd465d84012835d109d5db561a14cd2

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -41,6 +41,10 @@
 #ifndef nsIFrame_h___
 #define nsIFrame_h___
 
+#ifndef MOZILLA_INTERNAL_API
+#error This header/class should only be used within Mozilla code. It should not be used by extensions.
+#endif
+
 
 
 
