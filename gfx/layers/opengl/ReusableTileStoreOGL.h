cf7387dad53aca94c49f6f669b8f1b4dd0bd40d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:06:13 +0000

    Bug 745177 - Retain and re-use uploaded GL tiles. r=ajuma
    Rather than discarding uploaded tiles once they fall outside of the valid
    region, shuffle them off into a separate tile-store with all the metadata they
    need to render them, and render them in the areas that our current valid
    region doesn't cover in TiledThebesLayerOGL::RenderLayer.
    
    UltraBlame original commit: 4114c654f5d078265a6c99df8f9dca103359e28d

---
+++ gfx/layers/opengl/ReusableTileStoreOGL.h
@@ -0,0 +1,96 @@
+
+
+
+
+#ifndef GFX_REUSABLETILESTOREOGL_H
+#define GFX_REUSABLETILESTOREOGL_H
+
+#include "TiledThebesLayerOGL.h"
+#include "nsTArray.h"
+#include "nsAutoPtr.h"
+
+namespace mozilla {
+
+namespace gl {
+class GLContext;
+}
+
+namespace layers {
+
+
+
+class ReusableTiledTextureOGL
+{
+public:
+  ReusableTiledTextureOGL(TiledTexture aTexture,
+                          const nsIntRegion& aTileRegion,
+                          uint16_t aTileSize,
+                          gfxSize aResolution)
+    : mTexture(aTexture)
+    , mTileRegion(aTileRegion)
+    , mTileSize(aTileSize)
+    , mResolution(aResolution)
+  {}
+
+  ~ReusableTiledTextureOGL() {}
+
+  TiledTexture mTexture;
+  const nsIntRegion mTileRegion;
+  uint16_t mTileSize;
+  gfxSize mResolution;
+};
+
+
+
+
+
+
+
+class ReusableTileStoreOGL
+{
+public:
+  ReusableTileStoreOGL(gl::GLContext* aContext, float aSizeLimit)
+    : mContext(aContext)
+    , mSizeLimit(aSizeLimit)
+  {}
+
+  ~ReusableTileStoreOGL();
+
+  
+  
+  
+  
+  
+  void HarvestTiles(TiledLayerBufferOGL* aVideoMemoryTiledBuffer,
+                    const nsIntRegion& aOldValidRegion,
+                    const nsIntRegion& aNewValidRegion,
+                    const gfxSize& aOldResolution,
+                    const gfxSize& aNewResolution);
+
+  
+  
+  
+  void DrawTiles(TiledThebesLayerOGL* aLayer,
+                 const nsIntRegion& aValidRegion,
+                 const gfxSize& aResolution,
+                 const gfx3DMatrix& aTransform,
+                 const nsIntPoint& aRenderOffset);
+
+private:
+  
+  
+  nsRefPtr<gl::GLContext> mContext;
+
+  
+  
+  
+  float mSizeLimit;
+
+  
+  nsTArray<nsAutoPtr<ReusableTiledTextureOGL>> mTiles;
+};
+
+} 
+} 
+
+#endif 
