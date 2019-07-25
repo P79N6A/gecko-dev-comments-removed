f1423204f3c2e6d7c891929741654e42a4e48d6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:07:52 +0000

    Make nsIFrame::GetOrdinal accept all values that the CSS parser accepts (which currently adds no values).  (Bug 644514 patch 1)  r=bzbarsky
    UltraBlame original commit: 45b20f137549b8c4bb31c95fbb3ca0e4c6ac188c

--- layout/xul/base/src/nsBox.cpp
+++ layout/xul/base/src/nsBox.cpp
@@ -514,8 +514,9 @@ nsBox::GetFlex(nsBoxLayoutState& aState)
 PRUint32
 nsIFrame::GetOrdinal(nsBoxLayoutState& aState)
 {
-  PRUint32 ordinal = DEFAULT_ORDINAL_GROUP;
+  PRUint32 ordinal = GetStyleXUL()->mBoxOrdinal;
 
+  
   nsIContent* content = GetContent();
   if (content) {
     PRInt32 error;
@@ -524,14 +525,6 @@ nsIFrame::GetOrdinal(nsBoxLayoutState& aState)
     content->GetAttr(kNameSpaceID_None, nsGkAtoms::ordinal, value);
     if (!value.IsEmpty()) {
       ordinal = value.ToInteger(&error);
-    }
-    else {
-      
-      const nsStyleXUL* boxInfo = GetStyleXUL();
-      if (boxInfo->mBoxOrdinal > 1) {
-        
-        ordinal = (nscoord)boxInfo->mBoxOrdinal;
-      }
     }
   }
 
