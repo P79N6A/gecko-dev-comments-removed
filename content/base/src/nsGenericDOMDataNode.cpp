f8e7777471671849500eb2c7c5911db9a725d6d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:55:30 +0000

    Backed out changeset 58d2b33f0e0d
    UltraBlame original commit: 676b51ce62222e9910f7d861ed70bd470eb79d78

--- content/base/src/nsGenericDOMDataNode.cpp
+++ content/base/src/nsGenericDOMDataNode.cpp
@@ -846,8 +846,6 @@ nsGenericDOMDataNode::ReplaceWholeText(const nsAString& aContent,
 {
   *aResult = nsnull;
 
-  GetOwnerDoc()->WarnOnceAbout(nsIDocument::eReplaceWholeText);
-
   
   nsCOMPtr<nsIContent> parent = GetParent();
   if (!parent) {
