f5b2632260682343eff3ae8f6233121677b17613
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:49:35 +0000

    Bug 628222 - Add support for libnotify-0.7+. r=karlt a=joe
    UltraBlame original commit: ca548ef9d8c653adddc82177befb141e27c8c35f

--- toolkit/system/gnome/nsAlertsIconListener.cpp
+++ toolkit/system/gnome/nsAlertsIconListener.cpp
@@ -46,6 +46,11 @@
 
 #include <gdk/gdk.h>
 
+
+#ifndef NOTIFY_CHECK_VERSION
+#define NOTIFY_CHECK_VERSION(x,y,z) 0
+#endif
+
 static PRBool gHasActions = PR_FALSE;
 
 static void notify_action_cb(NotifyNotification *notification,
@@ -218,7 +223,13 @@ nsAlertsIconListener::ShowAlert(GdkPixbuf* aPixbuf)
 {
   mNotification = notify_notification_new(mAlertTitle.get(),
                                           mAlertText.get(),
-                                          NULL, NULL);
+                                          NULL
+
+#if !NOTIFY_CHECK_VERSION(0,7,0)
+                                          , NULL
+#endif
+                                          );
+
   if (!mNotification)
     return NS_ERROR_OUT_OF_MEMORY;
 
