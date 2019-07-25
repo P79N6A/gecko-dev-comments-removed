dba51758d3ec217d107f421fa992ea9abf4fc237
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:52:57 +0000

    Bug 683953 - Follow up, add logger argument for mobile automation to fix Android bustage; r=bustage-fix
    UltraBlame original commit: 11e104123b012649f5527f74cb8efdefb9585760

--- build/mobile/remoteautomation.py
+++ build/mobile/remoteautomation.py
@@ -96,7 +96,7 @@ class RemoteAutomation(Automation):
 
         return env
 
-    def waitForFinish(self, proc, utilityPath, timeout, maxTime, startTime, debuggerInfo, symbolsDir):
+    def waitForFinish(self, proc, utilityPath, timeout, maxTime, startTime, debuggerInfo, symbolsDir, logger):
         
         status = proc.wait(timeout = maxTime)
 
