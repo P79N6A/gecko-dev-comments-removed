9c01d9ed33de0d06b969f03952be329567c38b1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:41:38 +0000

    backout c0bf8f743419 CLOSED TREE
    UltraBlame original commit: bb414dc59262f90ea68031b6e0034ff952e56c03

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -321,18 +321,8 @@ var BrowserApp = {
 #endif
     }
 
-    if (updated) {
-        
-        
-        let browser = BrowserApp.selectedTab.browser;
-        let updatedFun = function updatedFun() {
-            browser.removeEventListener("DOMContentLoaded", updatedFun, false);
-            
-            Services.obs.notifyObservers(null, "FormHistory:Init", "");
-            Services.obs.notifyObservers(null, "Passwords:Init", "");
-        }
-        browser.addEventListener("DOMContentLoaded", updatedFun, false);
-    }
+    if (updated)
+      this.onAppUpdated();
 
     
     sendMessageToJava({
@@ -483,6 +473,12 @@ var BrowserApp = {
         }
         ContentAreaUtils.internalSave(aTarget.currentURI.spec, null, null, contentDisposition, type, false, "SaveImageTitle", null, aTarget.ownerDocument.documentURIObject, true, null);
       });
+  },
+
+  onAppUpdated: function() {
+    
+    Services.obs.notifyObservers(null, "FormHistory:Init", "");
+    Services.obs.notifyObservers(null, "Passwords:Init", "");
   },
 
   shutdown: function shutdown() {
