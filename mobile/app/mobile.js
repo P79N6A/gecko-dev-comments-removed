b0aedd1a865f2edf1656d0ae1762b718e0c94e0c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:01:12 +0000

    Bug 456083, set extra preference to force windows with features set to open in new tabs as well, r=mfinkle
    UltraBlame original commit: 707362ccbd2b14c8ef5c39269052acf5033de402

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -244,6 +244,8 @@ pref("dom.disable_window_move_resize", true);
 pref("browser.link.open_external", 3);
 pref("browser.link.open_newwindow", 3);
 
+pref("browser.link.open_newwindow.restriction", 0);
+
 
 pref("privacy.sanitize.promptOnSanitize", false);
 pref("privacy.item.cache", true);
