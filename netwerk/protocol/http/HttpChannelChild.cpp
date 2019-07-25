b0043d795f71786a30c4428f50ec37aa67eee6be
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:57:31 +0000

    Backed out changeset 5aaec7f808b6
    UltraBlame original commit: b5cf896dbc665688b2e09e735538eb1a4c2e66a8

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -287,21 +287,10 @@ HttpChannelChild::OnStartRequest(const nsHttpResponseHead& responseHead,
                          getter_AddRefs(mSecurityInfo));
   }
 
-  
-  
-  if (selfAddr.raw.family != peerAddr.raw.family) {
-    fprintf(stdout, "ERROR: Child: socket has multiple address families!\n");
-    fflush(stdout);
-  }
-fprintf(stdout, "Child: I always warned you!");
-fflush(stdout);
-
   mIsFromCache = isFromCache;
   mCacheEntryAvailable = cacheEntryAvailable;
   mCacheExpirationTime = cacheExpirationTime;
   mCachedCharset = cachedCharset;
-  mSelfAddr = selfAddr;
-  mPeerAddr = peerAddr;
 
   AutoEventEnqueuer ensureSerialDispatch(mEventQ);
 
@@ -324,6 +313,9 @@ fflush(stdout);
   rv = ApplyContentConversions();
   if (NS_FAILED(rv))
     Cancel(rv);
+
+  mSelfAddr = selfAddr;
+  mPeerAddr = peerAddr;
 }
 
 class TransportAndDataEvent : public ChannelEvent
@@ -1120,6 +1112,36 @@ HttpChannelChild::SetupFallbackChannel(const char *aFallbackKey)
 {
   DROP_DEAD();
 }
+
+
+
+
+NS_IMETHODIMP
+HttpChannelChild::GetRemoteAddress(nsACString & _result)
+{
+  return NS_ERROR_NOT_AVAILABLE;
+}
+
+NS_IMETHODIMP
+HttpChannelChild::GetRemotePort(PRInt32 * _result)
+{
+  NS_ENSURE_ARG_POINTER(_result);
+  return NS_ERROR_NOT_AVAILABLE;
+}
+
+NS_IMETHODIMP
+HttpChannelChild::GetLocalAddress(nsACString & _result)
+{
+  return NS_ERROR_NOT_AVAILABLE;
+}
+
+NS_IMETHODIMP
+HttpChannelChild::GetLocalPort(PRInt32 * _result)
+{
+  NS_ENSURE_ARG_POINTER(_result);
+  return NS_ERROR_NOT_AVAILABLE;
+}
+
 
 
 
