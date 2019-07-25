c0be4573557beb9fa91957d0a805ba8b4cad7a1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:04:27 +0000

    Bug 656824: Implemented ARB_robustness to detect driver resets in WebGL, only on GLX for now - r=bjacob
    This patch implements ARB_robustness on GLX and allows scripts to handle driver resets
    through events. The changes in this patch are very sweeping; they hit almost
    every NS_IMETHODIMP function within WebGLContextGL.cpp and WebGLContext.cpp.
    More work must be done on this to support EGL_CONTEXT_LOST.
    
    UltraBlame original commit: 4c2d39588760c800d35ea6199a2f734dea6ff6a7

---
+++ content/canvas/src/WebGLExtensionLoseContext.cpp
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdarg.h>
+
+#include "WebGLContext.h"
+#include "WebGLExtensions.h"
+
+#include "nsContentUtils.h"
+#include "mozilla/Preferences.h"
+
+using namespace mozilla;
+
+WebGLExtensionLoseContext::WebGLExtensionLoseContext(WebGLContext* context) :
+    WebGLExtension(context)
+{
+
+}
+
+WebGLExtensionLoseContext::~WebGLExtensionLoseContext()
+{
+
+}
+
+NS_IMETHODIMP 
+WebGLExtensionLoseContext::LoseContext()
+{
+    if (!mContext->LoseContext())
+        return mContext->mWebGLError = LOCAL_GL_INVALID_OPERATION;
+
+    return NS_OK;
+}
+
+NS_IMETHODIMP 
+WebGLExtensionLoseContext::RestoreContext()
+{
+    if (!mContext->RestoreContext())
+        return mContext->mWebGLError = LOCAL_GL_INVALID_OPERATION;
+
+    return NS_OK;
+}
