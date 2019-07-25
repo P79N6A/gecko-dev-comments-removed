6149767cdba98cba43fc3fa301e1c923beab3cb7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:47:27 +0000

    Back out 305a3a0e26fd (bug 680428).
    UltraBlame original commit: dff3ecc0451a911ea5f96c2cd8b8a9cdcc28584b

--- js/src/jit-test/tests/debug/onEnterFrame-05.js
+++ js/src/jit-test/tests/debug/onEnterFrame-05.js
@@ -1,15 +1,17 @@
 
 
 var g = newGlobal('new-compartment');
-g.eval("function f() { return 1; }\n");
-var N = g.N = RUNLOOP + 2;
-g.eval("function h() {\n" +
-       "    for (var i = 0; i < N; i += f()) {}\n" +
-       "}");
-g.h(); 
-
+g.n = 0;
+g.eval("function f(frame) { n++; return 42; }");
+print('ok');
 var dbg = Debugger(g);
-var log = '';
-dbg.onEnterFrame = function (frame) { log += frame.callee.name; };
-g.h();
-assertEq(log, 'h' + Array(N + 1).join('f'));
+dbg.onEnterFrame = g.f;
+
+
+var x = g.f();
+assertEq(x, 42);
+assertEq(g.n > 20, true);
+
+
+
+quit(0);
