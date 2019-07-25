ecf2c834b75a0b6f12281bafcc3df0e526dd5058
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:20:49 +0000

    bug 607758 - Crash [@ nsWindowWatcher::OpenWindowJSInternal] when trying to show profile manager r=jst a=blocking-fennec
    UltraBlame original commit: 7a62a113eeb0ab54be689c820c02f5c843c87127

--- embedding/components/windowwatcher/src/nsWindowWatcher.cpp
+++ embedding/components/windowwatcher/src/nsWindowWatcher.cpp
@@ -582,6 +582,8 @@ nsWindowWatcher::OpenWindowJSInternal(nsIDOMWindow *aParent,
   nsCOMPtr<nsIScriptSecurityManager>
     sm(do_GetService(NS_SCRIPTSECURITYMANAGER_CONTRACTID));
 
+  NS_ENSURE_TRUE(sm, NS_ERROR_FAILURE);
+
   
   
   
