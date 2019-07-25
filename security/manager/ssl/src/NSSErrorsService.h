df5d9f20535f7cf5af2bcadd583ddda7982ca916
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:15:40 +0000

    Bug 566478 - Invalid SSL cert error is not mobile-friendly [r=kaie a=blocking-fennecb2]
    UltraBlame original commit: c3886efa7e57b1b44cc4f002ce09402da4a4e944

---
+++ security/manager/ssl/src/NSSErrorsService.h
@@ -0,0 +1,68 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsINSSErrorsService.h"
+
+#include "nsIStringBundle.h"
+#include "nsCOMPtr.h"
+
+namespace mozilla {
+namespace psm {
+
+class NSSErrorsService : public nsINSSErrorsService
+{
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSINSSERRORSSERVICE
+
+public:
+  nsresult Init();
+
+private:
+  nsCOMPtr<nsIStringBundle> mPIPNSSBundle;
+  nsCOMPtr<nsIStringBundle> mNSSErrorsBundle;
+};
+
+} 
+} 
+
+#define NS_NSSERRORSSERVICE_CID \
+  { 0x9ef18451, 0xa157, 0x4d17, { 0x81, 0x32, 0x47, 0xaf, 0xef, 0x21, 0x36, 0x89 } }
