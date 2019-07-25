f85ac17c9c97ab8c81d5e0d5c4fecf2ed3aa0d18
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:45:01 +0000

    Bug 782070 - Remove NS_NewPagePrintTimer and nsPagePrintTimer::Init; r=roc
    UltraBlame original commit: 3c8361130a8119dd47ee4d595ba6424df334a438

--- layout/printing/nsPrintEngine.cpp
+++ layout/printing/nsPrintEngine.cpp
@@ -3332,15 +3332,14 @@ nsresult
 nsPrintEngine::StartPagePrintTimer(nsPrintObject* aPO)
 {
   if (!mPagePrintTimer) {
-    nsresult rv = NS_NewPagePrintTimer(&mPagePrintTimer);
-    NS_ENSURE_SUCCESS(rv, rv);
-
     
     
     int32_t printPageDelay = 50;
     mPrt->mPrintSettings->GetPrintPageDelay(&printPageDelay);
 
-    mPagePrintTimer->Init(this, mDocViewerPrint, printPageDelay);
+    nsRefPtr<nsPagePrintTimer> timer =
+      new nsPagePrintTimer(this, mDocViewerPrint, printPageDelay);
+    timer.forget(&mPagePrintTimer);
   }
 
   return mPagePrintTimer->Start(aPO);
