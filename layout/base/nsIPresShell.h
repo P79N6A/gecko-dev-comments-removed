d76d73c53057c7ea10c065d8430f74314a98cd1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:44:57 +0000

    Bug 780258: Add an interface to set the maximum line box width, accessible from chrome script. [r=dbaron]
    UltraBlame original commit: a101c20b729dd0d6cb8ea33f3acbbbb09ca7f3ca

--- layout/base/nsIPresShell.h
+++ layout/base/nsIPresShell.h
@@ -1312,6 +1312,12 @@ public:
   virtual void SysColorChanged() = 0;
   virtual void ThemeChanged() = 0;
 
+  nscoord MaxLineBoxWidth() {
+    return mMaxLineBoxWidth;
+  }
+
+  void SetMaxLineBoxWidth(nscoord aMaxLineBoxWidth);
+
 protected:
   friend class nsRefreshDriver;
 
@@ -1397,6 +1403,10 @@ protected:
   uint32_t mFontSizeInflationEmPerLine;
   uint32_t mFontSizeInflationMinTwips;
   uint32_t mFontSizeInflationLineThreshold;
+
+  
+  
+  nscoord mMaxLineBoxWidth;
 };
 
 
