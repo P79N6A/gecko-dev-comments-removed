dcbaead0c4f1548b3e61d2c2cab1db6ee7322746
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:20:19 +0000

    Merged changes
    UltraBlame original commit: b9cc20a562022bca87a1cfce8e93e4e55b027180

--- services/sync/modules/service.js
+++ services/sync/modules/service.js
@@ -102,7 +102,7 @@ Utils.lazy(Weave, 'Service', WeaveSvc);
 
 
 
-function WeaveSvc() {
+function WeaveSvc(engines) {
   this._startupFinished = false;
   this._initLogs();
   this._log.info("Weave Sync Service Initializing");
@@ -116,13 +116,19 @@ function WeaveSvc() {
   ID.setAlias('WeaveID', 'DAV:default');
   ID.setAlias('WeaveCryptoID', 'Engine:PBE:default');
 
+  if (typeof engines == "undefined")
+    engines = [
+      new BookmarksEngine(),
+      new HistoryEngine(),
+      new CookieEngine(),
+      new PasswordEngine(),
+      new FormEngine(),
+      new TabEngine()
+    ];
+
   
-  Engines.register(new BookmarksEngine());
-  Engines.register(new HistoryEngine());
-  Engines.register(new CookieEngine());
-  Engines.register(new PasswordEngine());
-  Engines.register(new FormEngine());
-  Engines.register(new TabEngine());
+  for (let i = 0; i < engines.length; i++)
+    Engines.register(engines[i]);
 
   
   Utils.prefs.addObserver("", this, false);
@@ -446,6 +452,8 @@ WeaveSvc.prototype = {
     let success = yield;
     if (!success) {
       try {
+        
+        
         this._checkUserDir.async(this, self.cb);
         yield;
       } catch (e) {  }
