d23f9b4dc8f6c16b3de592fa1f2e60b5e00dd766
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:33:14 +0000

    Bug 599475 - Fix crash reporting on MacOS 10.5 (Leopard) by making breakpad use it's internal implementation of MD5 instead of one randomly picked from libnss or libcrypto which isn't loading properly. r=ted a=blocking-beta8+
    UltraBlame original commit: 0012ca751ce10ec068e538e4e3759efcc0000398

--- toolkit/crashreporter/google-breakpad/src/common/mac/file_id.cc
+++ toolkit/crashreporter/google-breakpad/src/common/mac/file_id.cc
@@ -53,19 +53,19 @@ bool FileID::FileIdentifier(unsigned char identifier[16]) {
   if (fd == -1)
     return false;
 
-  MD5_CTX md5;
-  MD5_Init(&md5);
+  MD5Context md5;
+  MD5Init(&md5);
 
   
   
   unsigned char buffer[4096 * 2];
   size_t buffer_size = sizeof(buffer);
   while ((buffer_size = read(fd, buffer, buffer_size) > 0)) {
-    MD5_Update(&md5, buffer, buffer_size);
+    MD5Update(&md5, buffer, buffer_size);
   }
 
   close(fd);
-  MD5_Final(identifier, &md5);
+  MD5Final(identifier, &md5);
 
   return true;
 }
