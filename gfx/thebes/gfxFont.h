41ab58f88a6e1bb78318097e5b4f09f03d3b9e60
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:36:56 +0000

    bug 678181 - back out second part of bug 668813 (changeset 431a8297db1f) on suspicion of causing crashiness. r=backout
    UltraBlame original commit: 8e1dd6f8b903ccbb4fcdff817b606fda45d41311

--- gfx/thebes/gfxFont.h
+++ gfx/thebes/gfxFont.h
@@ -482,8 +482,7 @@ public:
         mFaceNamesInitialized(PR_FALSE),
         mHasStyles(PR_FALSE),
         mIsSimpleFamily(PR_FALSE),
-        mIsBadUnderlineFamily(PR_FALSE),
-        mCharacterMapInitialized(PR_FALSE)
+        mIsBadUnderlineFamily(PR_FALSE)
         { }
 
     virtual ~gfxFontFamily() { }
@@ -545,28 +544,10 @@ public:
     
     void ReadCMAP() {
         PRUint32 i, numFonts = mAvailableFonts.Length();
-        for (i = 0; i < numFonts; i++) {
-            gfxFontEntry *fe = mAvailableFonts[i];
-            if (!fe) {
-                continue;
-            }
-            fe->ReadCMAP();
-            mCharacterMap.Union(fe->mCharacterMap);
-        }
-        mCharacterMap.Compact();
-        mCharacterMapInitialized = PR_TRUE;
-    }
-
-    PRBool TestCharacterMap(PRUint32 aCh) {
-        if (!mCharacterMapInitialized) {
-            ReadCMAP();
-        }
-        return mCharacterMap.test(aCh);
-    }
-
-    void ResetCharacterMap() {
-        mCharacterMap.reset();
-        mCharacterMapInitialized = PR_FALSE;
+        
+        
+        for (i = 0; i < numFonts; i++)
+            mAvailableFonts[i]->ReadCMAP();
     }
 
     
@@ -609,14 +590,12 @@ protected:
 
     nsString mName;
     nsTArray<nsRefPtr<gfxFontEntry> >  mAvailableFonts;
-    gfxSparseBitSet mCharacterMap;
     PRPackedBool mOtherFamilyNamesInitialized;
     PRPackedBool mHasOtherFamilyNames;
     PRPackedBool mFaceNamesInitialized;
     PRPackedBool mHasStyles;
     PRPackedBool mIsSimpleFamily;
     PRPackedBool mIsBadUnderlineFamily;
-    PRPackedBool mCharacterMapInitialized;
 
     enum {
         
