b7d3459de2531652e0ca007f97424510a261947a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:39:14 +0000

    Bug 781320 - Part 2: Tests for forwarding <iframe mozbrowser>'s name from parent to child. r=smaug
    UltraBlame original commit: 1cb5ff3b90b1303f4e9524a85a1e74ee29a0aea0

---
+++ dom/browser-element/mochitest/browserElement_ForwardName.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+"use strict";
+
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addToWhitelist();
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+  iframe.setAttribute('name', 'foo');
+
+  iframe.addEventListener("mozbrowseropenwindow", function(e) {
+    ok(false, 'Got mozbrowseropenwindow, but should not have.');
+  });
+
+  iframe.addEventListener('mozbrowserlocationchange', function(e) {
+    ok(true, "Got locationchange to " + e.detail);
+    if (e.detail.endsWith("ForwardName.html#finish")) {
+      SimpleTest.finish();
+    }
+  });
+
+  
+  
+  iframe.addEventListener('mozbrowsershowmodalprompt', function(e) {
+    ok(e.detail.message.startsWith('success:'), e.detail.message);
+  });
+
+  document.body.appendChild(iframe);
+
+  
+  
+  
+  iframe.src = 'file_browserElement_ForwardName.html';
+}
+
+runTest();
