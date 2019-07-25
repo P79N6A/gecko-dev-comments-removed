49595ae5ed92599985f00712eb358095272b184a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:15:20 +0000

    Bug 570760 - Make ctrl-f and / focus the search box in the add-ons manager. r=dtownsend,dao
    UltraBlame original commit: f8a25d495522fb786ac5004bca378d56d65f4e29

---
+++ toolkit/mozapps/extensions/test/browser/browser_bug570760.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+var gManagerWindow;
+var focusCount = 0;
+
+function test() {
+  waitForExplicitFinish();
+
+  open_manager(null, function(aWindow) {
+    gManagerWindow = aWindow;
+
+    var searchBox = gManagerWindow.document.getElementById("header-search");
+    searchBox.addEventListener("focus", function() {
+      searchBox.blur();
+      focusCount++;
+    });
+    f_key_test();
+    slash_key_test();
+    searchBox.removeEventListener("focus");
+    end_test();
+  });
+}
+
+function end_test() {
+  close_manager(gManagerWindow, finish);
+}
+
+function f_key_test() {
+  EventUtils.synthesizeKey("f", { accelKey: true }, gManagerWindow);
+  is(focusCount, 1, "Search box should have been focused due to the f key");
+}
+
+function slash_key_test() {
+  EventUtils.synthesizeKey("/", { }, gManagerWindow);
+  is(focusCount, 2, "Search box should have been focused due to the / key");
+}
