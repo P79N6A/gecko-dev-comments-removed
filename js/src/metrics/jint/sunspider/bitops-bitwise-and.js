1614a87cbf0a20d11835b3f07ec27bcb67f6d0d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:20:28 +0000

    [JAEGER] Add diagnostic tool for trace JIT tuning.
    UltraBlame original commit: 628d8a832720899c6639bffa66e2104f03596b55

---
+++ js/src/metrics/jint/sunspider/bitops-bitwise-and.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bitwiseAndValue = 4294967296;
+
+for (var i = 0; i < 600000; i++)
+    bitwiseAndValue = bitwiseAndValue & i;
+
---
+++ js/src/metrics/jint/v8/run-earley-boyer.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+load('base.js');
+load('earley-boyer.js');
+
+function PrintResult(name, result) {
+  print(name + ': ' + result);
+}
+
+
+function PrintScore(score) {
+  print('----');
+  print('Score: ' + score);
+}
+
+
+BenchmarkSuite.RunSuites({ NotifyResult: PrintResult,
+                           NotifyScore: PrintScore });
