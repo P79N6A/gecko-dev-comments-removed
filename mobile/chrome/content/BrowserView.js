835c231defb90965e96c2670a74c4e1dc2f455bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:35 +0000

    Fixes
    UltraBlame original commit: 116f15d36cf181a77301424778254edb1f1f3e4c

--- mobile/chrome/content/BrowserView.js
+++ mobile/chrome/content/BrowserView.js
@@ -45,10 +45,6 @@ const kBrowserFormZoomLevelMin = 1.0;
 const kBrowserFormZoomLevelMax = 2.0;
 const kBrowserViewZoomLevelPrecision = 10000;
 
-function BrowserView(container, visibleRectFactory) {
-  Util.bindAll(this);
-  this.init(container, visibleRectFactory);
-}
 
 
 
@@ -58,56 +54,11 @@ function BrowserView(container, visibleRectFactory) {
 
 
 
-BrowserView.Util = {
-  ensureMozScrolledAreaEvent: function ensureMozScrolledAreaEvent(aBrowser, aWidth, aHeight) {
-    let message = {};
-    message.target = aBrowser;
-    message.name = "Browser:MozScrolledAreaChanged";
-    message.json = { width: aWidth, height: aHeight };
-
-    Browser._browserView.updateScrolledArea(message);
-  }
-};
-
-BrowserView.prototype = {
-
-  
-  
-  
-
-  init: function init(container, visibleRectFactory) {
-    this._container = container;
-    this._browser = null;
-    this._visibleRectFactory = visibleRectFactory;
-    messageManager.addMessageListener("Browser:MozScrolledAreaChanged", this);
-  },
-
-  uninit: function uninit() {
-  },
-
-  
 
 
-  setBrowser: function setBrowser(browser, browserViewportState) {
-    let oldBrowser = this._browser;
-    let browserChanged = (oldBrowser !== browser);
 
-    if (oldBrowser) {
-      oldBrowser.setAttribute("type", "content");
-      oldBrowser.style.display = "none";
-      oldBrowser.messageManager.sendAsyncMessage("Browser:Blur", {});
-    }
 
-    this._browser = browser;
 
-    if (browser) {
-      browser.setAttribute("type", "content-primary");
-      browser.style.display = "";
-      browser.messageManager.sendAsyncMessage("Browser:Focus", {});
-    }
-  },
 
-  getBrowser: function getBrowser() {
-    return this._browser;
-  }
-};
+
+
