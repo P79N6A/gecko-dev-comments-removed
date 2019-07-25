33fef0730ed59113dc00e3ba9266a80cdde18a63
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:44:29 +0000

    Bug 713810 - Use the HTMLness mode of the original document for the View Selection Source serialization. r=gavin.sharp.
    UltraBlame original commit: d0db3841139c05afbdebcf87d87650ce5c2976ef

--- toolkit/components/viewsource/content/viewPartialSource.js
+++ toolkit/components/viewsource/content/viewPartialSource.js
@@ -126,7 +126,9 @@ function viewPartialSourceForSelection(selection)
   
   
   
-  var dataDoc =
+  var isHTML = (doc.createElement("div").tagName == "DIV");
+  var dataDoc = isHTML ? 
+    ancestorContainer.ownerDocument.implementation.createHTMLDocument("") :
     ancestorContainer.ownerDocument.implementation.createDocument("", "", null);
   ancestorContainer = dataDoc.importNode(ancestorContainer, true);
   startContainer = ancestorContainer;
@@ -212,7 +214,9 @@ function viewPartialSourceForSelection(selection)
 
   
   var loadFlags = Components.interfaces.nsIWebNavigation.LOAD_FLAGS_NONE;
-  getWebNavigation().loadURI("view-source:data:text/html;charset=utf-8,"
+  getWebNavigation().loadURI((isHTML ?
+                              "view-source:data:text/html;charset=utf-8," :
+                              "view-source:data:application/xml;charset=utf-8,")
                              + encodeURIComponent(tmpNode.innerHTML),
                              loadFlags, null, null, null);
 }
