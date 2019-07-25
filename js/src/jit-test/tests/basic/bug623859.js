1026b96afb0120cdb67597d747dd284adea32502
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:06:53 +0000

    Bug 623859 - Add missing OOM checks during array allocation (r=lw)
    UltraBlame original commit: 6ca3394a3aa295a3492145c23bdf230ae46780f7

---
+++ js/src/jit-test/tests/basic/bug623859.js
@@ -0,0 +1,16 @@
+
+
+(function() {
+   Iterator((function() {
+           switch ((7)) {
+               default:
+                     return (Float32Array).call([], 4300018)
+                         case Proxy.create((function() {
+                                     return {
+                                               e:
+                                                         function() {}
+                                                                 }
+                                                                       })):
+                             }
+                               })())
+   })()
