3e0d93a54ed0f70d9d00d5f3a93f1e007f45b73b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:40:44 +0000

    Bug 783015 - Chrome refreshdriver runs all the time, r=khuey
    UltraBlame original commit: e33c175f680739c80f4838861e9d3521eb6b9c15

--- layout/style/ImageLoader.cpp
+++ layout/style/ImageLoader.cpp
@@ -76,6 +76,13 @@ ImageLoader::AssociateRequestToFrame(imgIRequest* aRequest,
 
     mRequestToFrameMap.Put(aRequest, newFrameSet);
     frameSet = newFrameSet.forget();
+
+    nsPresContext* presContext = GetPresContext();
+    if (presContext) {
+      nsLayoutUtils::RegisterImageRequestIfAnimated(presContext,
+                                                    aRequest,
+                                                    nullptr);
+    }
   }
 
   RequestSet* requestSet = nullptr;
@@ -365,9 +372,12 @@ ImageLoader::OnStartContainer(imgIRequest* aRequest, imgIContainer* aImage)
 NS_IMETHODIMP
 ImageLoader::OnImageIsAnimated(imgIRequest* aRequest)
 {
-  
-  
   if (!mDocument) {
+    return NS_OK;
+  }
+
+  FrameSet* frameSet = nullptr;
+  if (!mRequestToFrameMap.Get(aRequest, &frameSet)) {
     return NS_OK;
   }
 
