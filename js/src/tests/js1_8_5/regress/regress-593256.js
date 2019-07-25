6b0584618fc9fa5648907af08cf861c6643618af
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:30:57 +0000

    Bugs in dictionary-mode property table maintenance (593256, r=jorendorff).
    UltraBlame original commit: 3feb012b18a3764b75900c521df799c191dcde23

---
+++ js/src/tests/js1_8_5/regress/regress-593256.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+eval("\
+  function(){for(d in[0,Number]) {\
+    __defineGetter__(\"\",function(){}),\
+    [(__defineGetter__(\"x\",Math.pow))]\
+  }}\
+")()
+delete gc
+eval("\
+  function() {\
+    for(e in __defineSetter__(\"x\",function(){})){}\
+  }\
+")()
+delete gc
+
+reportCompare(true, true, "don't crash");
