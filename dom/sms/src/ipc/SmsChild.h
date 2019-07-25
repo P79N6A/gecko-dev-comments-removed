9f5e1609d6b6a550ec75bd1171c92a9465d2154b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:13:49 +0000

    Bug 674725 - Part G - Add PSms IPDL sub-protocol. r=smaug sr=cjones
    UltraBlame original commit: 4ff83006b7bcd20da1623a5a6a02aa18de7c0596

---
+++ dom/sms/src/ipc/SmsChild.h
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_dom_sms_SmsChild_h
+#define mozilla_dom_sms_SmsChild_h
+
+#include "mozilla/dom/sms/PSmsChild.h"
+
+namespace mozilla {
+namespace dom {
+namespace sms {
+
+class SmsChild : public PSmsChild
+{
+};
+
+} 
+} 
+} 
+
+#endif 
---
+++ dom/sms/src/ipc/SmsParent.h
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_dom_sms_SmsParent_h
+#define mozilla_dom_sms_SmsParent_h
+
+#include "mozilla/dom/sms/PSmsParent.h"
+
+namespace mozilla {
+namespace dom {
+namespace sms {
+
+class SmsParent : public PSmsParent
+{
+};
+
+} 
+} 
+} 
+
+#endif 
