54876c3f625f766ab5be46b31fe0b7bc74d8207c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:22:16 +0000

    Merge mozilla-central into mozilla-inbound
    UltraBlame original commit: 0fd6f88a7c1bb319c2672c0fbbdb90767294377c

--- widget/src/windows/nsWindow.cpp
+++ widget/src/windows/nsWindow.cpp
@@ -4458,6 +4458,35 @@ nsWindow::ProcessMessageForPlugin(const MSG &aMsg,
   return PR_TRUE;
 }
 
+static void ForceFontUpdate()
+{
+  
+  
+  
+  
+  
+  static const char kPrefName[] = "font.internaluseonly.changed";
+  PRBool fontInternalChange =
+    Preferences::GetBool(kPrefName, PR_FALSE);
+  Preferences::SetBool(kPrefName, !fontInternalChange);
+}
+
+static PRBool CleartypeSettingChanged()
+{
+  static int currentQuality = -1;
+  BYTE quality = cairo_win32_get_system_text_quality();
+
+  if (currentQuality == quality)
+    return PR_FALSE;
+
+  if (currentQuality < 0) {
+    currentQuality = quality;
+    return PR_FALSE;
+  }
+  currentQuality = quality;
+  return PR_TRUE;
+}
+
 
 PRBool nsWindow::ProcessMessage(UINT msg, WPARAM &wParam, LPARAM &lParam,
                                 LRESULT *aRetValue)
@@ -4620,15 +4649,7 @@ PRBool nsWindow::ProcessMessage(UINT msg, WPARAM &wParam, LPARAM &lParam,
         fontEnum->UpdateFontList(&didChange);
         
         if (didChange)  {
-          
-          
-          
-          
-          
-          const char* kPrefName = "font.internaluseonly.changed";
-          PRBool fontInternalChange =
-            Preferences::GetBool(kPrefName, PR_FALSE);
-          Preferences::SetBool(kPrefName, !fontInternalChange);
+          ForceFontUpdate();
         }
       } 
     }
@@ -4811,6 +4832,13 @@ PRBool nsWindow::ProcessMessage(UINT msg, WPARAM &wParam, LPARAM &lParam,
       break;
 
     case WM_PAINT:
+      if (CleartypeSettingChanged()) {
+        ForceFontUpdate();
+        gfxFontCache *fc = gfxFontCache::GetCache();
+        if (fc) {
+          fc->Flush();
+        }
+      }
       *aRetValue = (int) OnPaint(NULL, 0);
       result = PR_TRUE;
       break;
