4d8955f8cfcf4b15b6ac8b8d7eadb29ae3c3ef17
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:44:58 +0000

    Bug 785246: Make custom button inherit ShapedButton. [r=mfinkle]
    UltraBlame original commit: d89a71a8f5aead66d115b573aab0c8a47129ff12

---
+++ mobile/android/base/ShapedButton.java
@@ -0,0 +1,46 @@
+
+
+
+
+package org.mozilla.gecko;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.widget.ImageButton;
+
+public class ShapedButton extends ImageButton
+                          implements CanvasDelegate.DrawManager { 
+    protected Path mPath;
+    protected CurveTowards mSide;
+    protected CanvasDelegate mCanvasDelegate;
+
+    protected enum CurveTowards { NONE, LEFT, RIGHT };
+
+    public ShapedButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.BrowserToolbarCurve);
+        int curveTowards = a.getInt(R.styleable.BrowserToolbarCurve_curveTowards, 0x02);
+        a.recycle();
+
+        if (curveTowards == 0x00)
+            mSide = CurveTowards.NONE;
+        else if (curveTowards == 0x01)
+            mSide = CurveTowards.LEFT;
+        else
+            mSide = CurveTowards.RIGHT;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        mCanvasDelegate.draw(canvas, mPath, getWidth(), getHeight());
+    }
+
+    @Override
+    public void defaultDraw(Canvas canvas) {
+        super.draw(canvas);
+    }
+}
