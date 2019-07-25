b2cb6e8b880fea7ed5a980d3cbe7e1bfa0fa8ae6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:58:26 +0000

    Bug 599813 - nsSVGGlyphFrame::getCharacterData is expensive avoid calling it too many times. r+a=roc
    UltraBlame original commit: fcf6f703d87d6135476262b06614484919ad72bf

--- layout/svg/base/src/nsISVGGlyphFragmentLeaf.h
+++ layout/svg/base/src/nsISVGGlyphFragmentLeaf.h
@@ -70,18 +70,21 @@ public:
 
 
 
-  NS_IMETHOD_(void) GetEffectiveXY(nsTArray<float> &aX, nsTArray<float> &aY)=0;
+  NS_IMETHOD_(void) GetEffectiveXY(PRInt32 strLength,
+                                   nsTArray<float> &aX, nsTArray<float> &aY)=0;
   
 
 
 
-  NS_IMETHOD_(void) GetEffectiveDxDy(nsTArray<float> &aDx,
+  NS_IMETHOD_(void) GetEffectiveDxDy(PRInt32 strLength, 
+                                     nsTArray<float> &aDx,
                                      nsTArray<float> &aDy)=0;
   
 
 
 
-  NS_IMETHOD_(void) GetEffectiveRotate(nsTArray<float> &aRotate)=0;
+  NS_IMETHOD_(void) GetEffectiveRotate(PRInt32 strLength,
+                                       nsTArray<float> &aRotate)=0;
   NS_IMETHOD_(PRUint16) GetTextAnchor()=0;
   NS_IMETHOD_(PRBool) IsAbsolutelyPositioned()=0;
 };
