3665299552b74e34f413b3dcda253f3323525006
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:01:56 +0000

    bug 738528 - still photo support for getUserMedia r=dougt
    UltraBlame original commit: b51259905d852073bfbbe9227efeeb82501fb3ef

---
+++ dom/base/NavigatorUserMediaAndroid.cpp
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+#include "AndroidBridge.h"
+#include "nsDOMFile.h"
+#include "Navigator.h"
+
+namespace mozilla {
+namespace dom {
+
+class GetUserMediaCallback: public nsFilePickerCallback {
+public:
+  GetUserMediaCallback(nsIDOMMediaCallback* aCallback, nsIDOMErrorCallback *aError) :
+    mCallback(aCallback) , mError(aError){}
+
+  virtual void handleResult(nsAString& filePath) {
+    nsCOMPtr<nsILocalFile> file;
+    NS_NewLocalFile(filePath, true, getter_AddRefs(file));
+    bool exists;
+    if (!file || NS_FAILED(file->Exists(&exists)) || !exists) {
+      if (mError) {
+        mError->Callback(NS_LITERAL_STRING("ERROR_NO_FILE"));
+      }
+    } else {
+      mCallback->Callback(new nsDOMFileFile(file));
+    }
+  }
+
+private:
+  nsCOMPtr<nsIDOMMediaCallback> mCallback;
+  nsCOMPtr<nsIDOMErrorCallback> mError;
+};
+
+class ErrorCallbackRunnable : public nsRunnable {
+public:
+  ErrorCallbackRunnable(nsIDOMErrorCallback* aErrorCallback, const nsString& aErrorMsg) :
+    mErrorCallback(aErrorCallback), mErrorMsg(aErrorMsg) {}
+  NS_IMETHOD Run() {
+    mErrorCallback->Callback(mErrorMsg);
+    return NS_OK;
+  }
+private:
+  nsCOMPtr<nsIDOMErrorCallback> mErrorCallback;
+  const nsString mErrorMsg;
+};
+
+NS_IMETHODIMP
+Navigator::MozGetUserMedia(nsIMediaStreamOptions *params, nsIDOMMediaCallback *callback, nsIDOMErrorCallback *error)
+{
+  bool picture;
+  if (!params || !callback || NS_FAILED(params->GetPicture(&picture))) {
+    if (error) {
+      NS_DispatchToMainThread(new ErrorCallbackRunnable(error, NS_LITERAL_STRING("BAD_ARGS")));
+    }
+    return NS_OK;
+  }
+
+  if (!picture) {
+    if (error) {
+      NS_DispatchToMainThread(new ErrorCallbackRunnable(error, NS_LITERAL_STRING("NOT_IMPLEMENTED")));
+    }
+    return NS_OK;
+  }
+
+  if (mozilla::AndroidBridge::Bridge()) {
+    mozilla::AndroidBridge::Bridge()->ShowFilePickerAsync(NS_LITERAL_STRING("image/*"), new GetUserMediaCallback(callback, error));
+  } else if (error) {
+    NS_DispatchToMainThread(new ErrorCallbackRunnable(error, NS_LITERAL_STRING("INTERNAL_ERROR")));
+  }
+  return NS_OK;
+}
+}
+}
