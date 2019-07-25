5d0603fc185c6a78186de4a282b197c26216c78e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:21:58 +0000

    Bug 704313 - Add a don't-define-stdint-types frob to vpx (encapsulated in a custom downstream patch, until this makes its way upstream).  r=derf
    UltraBlame original commit: be47924d7a08ec811a473e9a72edf585505c3879

--- media/libvpx/vpx/vpx_integer.h
+++ media/libvpx/vpx/vpx_integer.h
@@ -15,6 +15,8 @@
 
 #include <stddef.h>
 
+#if !defined(VPX_DONT_DEFINE_STDINT_TYPES)
+
 #if (defined(_MSC_VER) && (_MSC_VER < 1600)) || defined(VPX_EMULATE_INTTYPES)
 typedef signed char  int8_t;
 typedef signed short int16_t;
@@ -49,6 +51,8 @@ typedef unsigned int   uintptr_t;
 #define __STDC_FORMAT_MACROS
 #endif
 #include <stdint.h>
+
+#endif
 
 #endif
 
