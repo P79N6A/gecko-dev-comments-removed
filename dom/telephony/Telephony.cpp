ad4c23e477b77ed954bf57d49977664f86f7f167
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:48:05 +0000

    Bug 721004 - Revert changes to dom/telephony/nsTelephony.cpp [a=backout]
    UltraBlame original commit: 2aab3e84a1ee8f212806e7d3999e1701ae8651a0

--- dom/telephony/Telephony.cpp
+++ dom/telephony/Telephony.cpp
@@ -57,7 +57,7 @@
 USING_TELEPHONY_NAMESPACE
 using mozilla::Preferences;
 
-#define DOM_TELEPHONY_WHITELIST "dom.telephony.whitelist"
+#define DOM_TELEPHONY_APP_PHONE_URL_PREF "dom.telephony.app.phone.url"
 
 namespace {
 
@@ -496,8 +496,12 @@ NS_NewTelephony(nsPIDOMWindow* aWindow, nsIDOMTelephony** aTelephony)
     NS_ENSURE_SUCCESS(rv, rv);
 
     
-    if (!nsContentUtils::URIIsChromeOrInPref(documentURI,
-                                             DOM_TELEPHONY_WHITELIST)) {
+    
+    nsCString phoneAppURL;
+    if (NS_FAILED(Preferences::GetCString(DOM_TELEPHONY_APP_PHONE_URL_PREF,
+                                          &phoneAppURL)) ||
+        !phoneAppURL.Equals(documentURL,
+                            nsCaseInsensitiveCStringComparator())) {
       *aTelephony = nsnull;
       return NS_OK;
     }
