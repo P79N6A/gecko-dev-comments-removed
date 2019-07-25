f9492ef985b047c4ef7b2101ed771a95c6bc74d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:51:11 +0000

    Bug 629200 part 18 - Remove unnecessary serialisation from setting nsSVGViewBox; r=jwatt
    UltraBlame original commit: b397e9d603348257f499d2daed842146fd37911e

--- content/svg/content/src/nsSVGViewBox.cpp
+++ content/svg/content/src/nsSVGViewBox.cpp
@@ -127,10 +127,16 @@ void
 nsSVGViewBox::SetBaseValue(float aX, float aY, float aWidth, float aHeight,
                            nsSVGElement *aSVGElement)
 {
+  if (mHasBaseVal && mBaseVal == nsSVGViewBoxRect(aX, aY, aWidth, aHeight)) {
+    return;
+  }
+
+  nsAttrValue emptyOrOldValue = aSVGElement->WillChangeViewBox();
+
   mBaseVal = nsSVGViewBoxRect(aX, aY, aWidth, aHeight);
   mHasBaseVal = true;
 
-  aSVGElement->DidChangeViewBox(true);
+  aSVGElement->DidChangeViewBox(emptyOrOldValue);
   if (mAnimVal) {
     aSVGElement->AnimationNeedsResample();
   }
