4fc15a7e3cb93b2352ffe3c9a5bd63e11945109b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:16:53 +0000

    Add test for inline autoincrement keys being added to object stores when they are stored. Also refactors some common test code.
    UltraBlame original commit: 77a22c0c9a979c375952a1c4aa93b5c8042d2d57

---
+++ dom/indexedDB/test/helpers.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+function continueTest(aEvent)
+{
+  gGen.send(aEvent);
+}
+
+function errorHandler(aEvent)
+{
+  ok(false, "indexedDB error (" + aEvent.code + "): " + aEvent.message);
+  finishTest();
+}
+
+function unexpectedSuccessHandler()
+{
+  ok(false, "Got success, but did not expect it!");
+  finishTest();
+}
+
+function ExpectError(aCode)
+{
+  this._code = aCode;
+}
+ExpectError.prototype = {
+  handleEvent: function(aEvent)
+  {
+    is(this._code, aEvent.code, "Expected error was thrown.");
+    continueTest(aEvent);
+  },
+};
+
+function finishTest()
+{
+  gGen.close();
+  SimpleTest.finish();
+}
