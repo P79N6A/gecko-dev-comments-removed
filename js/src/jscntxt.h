b8e71c0353e7f143277524887c1c8b751291f6ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:35:09 +0000

    merge mozilla-central to tracemonkey
    UltraBlame original commit: 6f7f0da1569382067e13026c691be579bba72b6d

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -2364,6 +2364,16 @@ struct JSContext
     void assertValidStackDepth(uintN ) {}
 #endif
 
+    enum DollarPath {
+        DOLLAR_LITERAL = 1,
+        DOLLAR_AMP,
+        DOLLAR_PLUS,
+        DOLLAR_TICK,
+        DOLLAR_QUOT
+    };
+    volatile DollarPath *dollarPath;
+    volatile jschar *blackBox;
+
 private:
 
     
@@ -2376,7 +2386,7 @@ private:
 
     
     JSContext *thisInInitializer() { return this; }
-};
+}; 
 
 #ifdef JS_THREADSAFE
 # define JS_THREAD_ID(cx)       ((cx)->thread ? (cx)->thread->id : 0)
