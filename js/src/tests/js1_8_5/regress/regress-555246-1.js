de3f421d8841e55e997672c6766d489436c61616
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:07:39 +0000

    Fix up the tests from bug 555246 to pass in the browser.
    UltraBlame original commit: 8f08ae0b74df6e0d9c001968e3998fe64359b54f

--- js/src/tests/js1_8_5/regress/regress-555246-1.js
+++ js/src/tests/js1_8_5/regress/regress-555246-1.js
@@ -4,9 +4,11 @@
 
 
 
-assertEq(typeof evalcx, "function", "")
-var cx = evalcx("");
-evalcx("function f() { return this; }", cx);
-f = cx.f;
-assertEq(f(), cx);
+if (typeof evalcx == 'function') {
+    var cx = evalcx("");
+    evalcx("function f() { return this; }", cx);
+    f = cx.f;
+    assertEq(f(), cx);
+}
+
 reportCompare(0, 0, "");
