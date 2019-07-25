d1960cf6f1a491e8e1f0090ef654edd3869d53f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:31:15 +0000

    Bug 660158: Remove all references to head_ipc.js. r=jduell
    UltraBlame original commit: 6832ae71433c77fa215443be1947e234bb3984fd

--- netwerk/test/unit_ipc/head_ipc_setup.js
+++
@@ -1,7 +0,0 @@
-
-
-var env = Components.classes["@mozilla.org/process/environment;1"]
-          .getService(Components.interfaces.nsIEnvironment);
-var log = env.get("NSPR_LOG_FILE");
-if (log) 
-  env.set("NSPR_LOG_FILE", log + ".child");
