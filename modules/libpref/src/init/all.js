eb468f5e2cee83ca533645b5bb501b7ab3d9663b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:35:19 +0000

    Bug 646641 - Part 2: Update SHistory so it understands that SHEntries may share content viewers. r=smaug
    UltraBlame original commit: f9247cadf32b85830207b6e292b63bdf7a24534d

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -109,8 +109,6 @@ pref("dom.enable_performance", true);
 
 pref("browser.sessionhistory.max_total_viewers", -1);
 
-pref("browser.sessionhistory.optimize_eviction", true);
-
 pref("ui.use_native_colors", true);
 pref("ui.click_hold_context_menus", false);
 pref("browser.display.use_document_fonts",  1);  
