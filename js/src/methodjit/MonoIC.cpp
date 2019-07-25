36203afee01a561f48e9a68a34d7b7fe3939b70b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:20:27 +0000

    [JAEGER] Fixed warning in MonoIC.cpp.
    UltraBlame original commit: 53f251829c1b6af9f16f035a75323e22b6462642

--- js/src/methodjit/MonoIC.cpp
+++ js/src/methodjit/MonoIC.cpp
@@ -1,48 +1,49 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=4 sw=4 et tw=99:
- *
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla SpiderMonkey JavaScript 1.9 code, released
- * May 28, 2008.
- *
- * The Initial Developer of the Original Code is
- *   Brendan Eich <brendan@mozilla.org>
- *
- * Contributor(s):
- *   David Anderson <danderson@mozilla.com>
- *   David Mandelin <dmandelin@mozilla.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "jsscope.h"
 #include "jsnum.h"
 #include "MonoIC.h"
 #include "StubCalls.h"
 #include "assembler/assembler/RepatchBuffer.h"
-
+#include "jsobj.h"
+#include "jsobjinlines.h"
 #include "jsscopeinlines.h"
 
 using namespace js;
@@ -88,11 +89,11 @@ ic::GetGlobalName(VMFrame &f, uint32 index)
 
     mic.u.name.touched = true;
 
-    /* Patch shape guard. */
+    
     JSC::RepatchBuffer repatch(mic.entry.executableAddress(), 50);
     repatch.repatch(mic.shape, reinterpret_cast<void*>(shape));
 
-    /* Patch loads. */
+    
     JS_ASSERT(slot >= JS_INITIAL_NSLOTS);
     slot -= JS_INITIAL_NSLOTS;
     slot *= sizeof(Value);
@@ -100,7 +101,7 @@ ic::GetGlobalName(VMFrame &f, uint32 index)
     loads.repatch(mic.load.dataLabel32AtOffset(MICInfo::GET_TYPE_OFFSET), slot + 4);
     loads.repatch(mic.load.dataLabel32AtOffset(MICInfo::GET_DATA_OFFSET), slot);
 
-    /* Do load anyway... this time. */
+    
     stubs::GetGlobalName(f);
 }
 
@@ -148,11 +149,11 @@ ic::SetGlobalName(VMFrame &f, uint32 index)
 
     mic.u.name.touched = true;
 
-    /* Patch shape guard. */
+    
     JSC::RepatchBuffer repatch(mic.entry.executableAddress(), 50);
     repatch.repatch(mic.shape, reinterpret_cast<void*>(shape));
 
-    /* Patch loads. */
+    
     JS_ASSERT(slot >= JS_INITIAL_NSLOTS);
     slot -= JS_INITIAL_NSLOTS;
     slot *= sizeof(Value);
@@ -168,8 +169,8 @@ ic::SetGlobalName(VMFrame &f, uint32 index)
     if (mic.u.name.dataWrite)
         stores.repatch(mic.load.dataLabel32AtOffset(dataOffset), slot);
 
-    /* Do load anyway... this time. */
+    
     stubs::SetGlobalName(f, atom);
 }
 
-#endif /* JS_MONOIC */
+#endif 
