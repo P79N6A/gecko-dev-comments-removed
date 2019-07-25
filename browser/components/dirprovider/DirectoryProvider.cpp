70bf891aedbbff583ccb19f0e51290b926e05fba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:32:12 +0000

    Bug 623110 - prioritise the loading of user-installed search engines over app-shipped engines. r=dolske, a=blocker
    UltraBlame original commit: a55b45fb3d7c3753c4e240ccd2f239795abec46d

--- browser/components/dirprovider/DirectoryProvider.cpp
+++ browser/components/dirprovider/DirectoryProvider.cpp
@@ -248,9 +248,18 @@ DirectoryProvider::GetFiles(const char *aKey, nsISimpleEnumerator* *aResult)
 
     nsCOMArray<nsIFile> baseFiles;
 
+    
+
+
+
+
+
+
+
+
     AppendDistroSearchDirs(dirSvc, baseFiles);
-    AppendFileKey(NS_APP_SEARCH_DIR, dirSvc, baseFiles);
     AppendFileKey(NS_APP_USER_SEARCH_DIR, dirSvc, baseFiles);
+    AppendFileKey(NS_APP_SEARCH_DIR, dirSvc, baseFiles);
 
     nsCOMPtr<nsISimpleEnumerator> baseEnum;
     rv = NS_NewArrayEnumerator(getter_AddRefs(baseEnum), baseFiles);
