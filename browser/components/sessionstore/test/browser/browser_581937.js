714227f0740d02a5f8eba7fd11b946e1e65e0224
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:28:55 +0000

    Test for bug 581937 [r=zpao, a=blocking2.0:final+]
    UltraBlame original commit: cf073a3eafdfe4c38260b50d2d87b86a05f5f771

---
+++ browser/components/sessionstore/test/browser/browser_581937.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+ let ss = Cc["@mozilla.org/browser/sessionstore;1"].
+          getService(Ci.nsISessionStore);
+
+let tab;
+function test() {
+  waitForExplicitFinish();
+
+  gPrefService.setIntPref("browser.sessionstore.max_tabs_undo", 0);
+  gPrefService.clearUserPref("browser.sessionstore.max_tabs_undo");
+
+  is(ss.getClosedTabCount(window), 0, "should be no closed tabs");
+
+  gBrowser.tabContainer.addEventListener("TabOpen", onTabOpen, true);
+
+  tab = gBrowser.addTab();
+}
+
+function onTabOpen(aEvent) {
+  gBrowser.tabContainer.removeEventListener("TabOpen", onTabOpen, true);
+
+  
+  executeSoon(function() {
+    is(gBrowser.browsers[1].currentURI.spec, "about:blank",
+       "we will be removing an about:blank tab");
+
+    gBrowser.removeTab(tab);
+
+    is(ss.getClosedTabCount(window), 0, "should still be no closed tabs");
+
+    executeSoon(finish);
+  });
+}
