f88621aa998bf446a2e0d27dc89bebf167f224ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:27:05 +0000

    Bug 712835 (part 3 of 3) - Move AtomImpl and PermanentAtomImpl out of the header.  r=khuey.
    UltraBlame original commit: fa8de405cc873ef1f68fa0e2ded9b21ce544ec8f

--- xpcom/ds/nsAtomTable.cpp
+++ xpcom/ds/nsAtomTable.cpp
@@ -78,6 +78,87 @@ static bool gStaticAtomTableSealed = false;
 
 
 
+
+
+
+
+
+class AtomImpl : public nsIAtom {
+public:
+  AtomImpl(const nsAString& aString);
+
+  
+  
+  AtomImpl(nsStringBuffer* aData, PRUint32 aLength);
+
+protected:
+  
+  
+  AtomImpl() {
+    
+    
+    NS_ASSERTION((mLength + 1) * sizeof(PRUnichar) <=
+                 nsStringBuffer::FromData(mString)->StorageSize() &&
+                 mString[mLength] == 0,
+                 "Not initialized atom");
+  }
+
+  
+  
+  ~AtomImpl();
+
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIATOM
+
+  enum { REFCNT_PERMANENT_SENTINEL = PR_UINT32_MAX };
+
+  virtual bool IsPermanent();
+
+  
+  bool IsPermanentInDestructor() {
+    return mRefCnt == REFCNT_PERMANENT_SENTINEL;
+  }
+
+  
+  nsrefcnt GetRefCount() { return mRefCnt; }
+
+  size_t SizeOfIncludingThis(nsMallocSizeOfFun aMallocSizeOf) const;
+};
+
+
+
+
+
+class PermanentAtomImpl : public AtomImpl {
+public:
+  PermanentAtomImpl(const nsAString& aString)
+    : AtomImpl(aString)
+  {}
+  PermanentAtomImpl(nsStringBuffer* aData, PRUint32 aLength)
+    : AtomImpl(aData, aLength)
+  {}
+  PermanentAtomImpl()
+  {}
+
+  ~PermanentAtomImpl();
+  NS_IMETHOD_(nsrefcnt) AddRef();
+  NS_IMETHOD_(nsrefcnt) Release();
+
+  virtual bool IsPermanent();
+
+  
+  
+
+  void* operator new(size_t size, AtomImpl* aAtom) CPP_THROW_NEW;
+  void* operator new(size_t size) CPP_THROW_NEW
+  {
+    return ::operator new(size);
+  }
+};
+
+
+
 struct AtomTableEntry : public PLDHashEntryHdr {
   AtomImpl* mAtom;
 };
