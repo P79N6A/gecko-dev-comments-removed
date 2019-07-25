a6ae245d2a41a5ff76ab0ecf0019df541a8ea9b7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:26:50 +0000

    Bug 441197: Maintain an audio session across processes. r=jimm
    UltraBlame original commit: d004850a8358c85e2fdfb0e6dea290fc1909e5ee

--- widget/src/windows/AudioSession.h
+++ widget/src/windows/AudioSession.h
@@ -41,7 +41,21 @@
 namespace mozilla {
 namespace widget {
 
+
 nsresult StartAudioSession();
+
+
+nsresult GetAudioSessionData(nsID& aID,
+                             nsString& aSessionName,
+                             nsString& aIconPath);
+
+
+
+nsresult RecvAudioSessionData(const nsID& aID,
+                              const nsString& aSessionName,
+                              const nsString& aIconPath);
+
+
 nsresult StopAudioSession();
 
 } 
