c727ccefc907ac10569f4787ac8ae48a58ae5559
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:51:09 +0000

    Convert the last remaining direct JS_Assert users to MOZ_Assert, followup to bug 717540 that seems to have been missed in my initial review.  Also switch the JS_ASSERT uses to MOZ_ASSERT in passing.  r=trivial
    UltraBlame original commit: 02287bac15136b1b46a171d0b22537f640fca25a

--- js/xpconnect/src/nsXPConnect.cpp
+++ js/xpconnect/src/nsXPConnect.cpp
@@ -42,6 +42,10 @@
 
 
 
+#include "mozilla/Assertions.h"
+#include "mozilla/Base64.h"
+#include "mozilla/Util.h"
+
 #include "xpcprivate.h"
 #include "XPCWrapper.h"
 #include "nsBaseHashtable.h"
@@ -63,10 +67,6 @@
 
 #include "XPCQuickStubs.h"
 #include "dombindings.h"
-
-#include "mozilla/Assertions.h"
-#include "mozilla/Base64.h"
-#include "mozilla/Util.h"
 
 #include "nsWrapperCacheInlines.h"
 
@@ -166,7 +166,7 @@ nsXPConnect::GetXPConnect()
     
     
     if (!NS_LIKELY(NS_IsMainThread() || NS_IsCycleCollectorThread()))
-        JS_Assert("NS_IsMainThread()", __FILE__, __LINE__);
+        MOZ_Assert("NS_IsMainThread()", __FILE__, __LINE__);
 
     if (!gSelf) {
         if (gOnceAliveNowDead)
@@ -482,7 +482,7 @@ struct NoteWeakMapChildrenTracer : public JSTracer
 static void
 TraceWeakMappingChild(JSTracer *trc, void *thing, JSGCTraceKind kind)
 {
-    JS_ASSERT(trc->callback == TraceWeakMappingChild);
+    MOZ_ASSERT(trc->callback == TraceWeakMappingChild);
     NoteWeakMapChildrenTracer *tracer =
         static_cast<NoteWeakMapChildrenTracer *>(trc);
     if (kind == JSTRACE_STRING)
@@ -513,7 +513,7 @@ TraceWeakMapping(js::WeakMapTracer *trc, JSObject *m,
                  void *k, JSGCTraceKind kkind,
                  void *v, JSGCTraceKind vkind)
 {
-    JS_ASSERT(trc->callback == TraceWeakMapping);
+    MOZ_ASSERT(trc->callback == TraceWeakMapping);
     NoteWeakMapsTracer *tracer = static_cast<NoteWeakMapsTracer *>(trc);
     if (vkind == JSTRACE_STRING)
         return;
@@ -524,7 +524,7 @@ TraceWeakMapping(js::WeakMapTracer *trc, JSObject *m,
     
     
     
-    JS_ASSERT(AddToCCKind(kkind));
+    MOZ_ASSERT(AddToCCKind(kkind));
 
     
     
@@ -1009,7 +1009,7 @@ nsXPConnect::GetOutstandingRequests(JSContext* cx)
     
     
     if (context && cx == context->GetJSContext()) {
-        JS_ASSERT(n);
+        MOZ_ASSERT(n);
         --n;
     }
     return n;
@@ -1301,7 +1301,7 @@ nsXPConnect::InitClassesWithNewWrappedGlobal(JSContext * aJSContext,
 
     
 
-    JS_ASSERT(!js::GetObjectParent(globalJSObj));
+    MOZ_ASSERT(!js::GetObjectParent(globalJSObj));
 
     JSObject* oldGlobal = JS_GetGlobalObject(aJSContext);
     if (!oldGlobal || oldGlobal == tempGlobal)
