35e4433a105695cafdf90b970574c58d0878c290
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:53:17 +0000

    Bug 731181 - Handle GC during RegExpShared::compile (r=billm)
    UltraBlame original commit: 2742bfe821d27072840432c9b15d3ce21a6b1419

--- js/src/vm/RegExpObject.cpp
+++ js/src/vm/RegExpObject.cpp
@@ -632,8 +632,6 @@ inline bool
 RegExpCompartment::get(JSContext *cx, JSAtom *keyAtom, JSAtom *source, RegExpFlag flags, Type type,
                        RegExpGuard *g)
 {
-    DebugOnly<uint64_t> gcNumberBefore = cx->runtime->gcNumber;
-
     Key key(keyAtom, flags, type);
     Map::AddPtr p = map_.lookupForAdd(key);
     if (p) {
@@ -649,12 +647,7 @@ RegExpCompartment::get(JSContext *cx, JSAtom *keyAtom, JSAtom *source, RegExpFla
         goto error;
 
     
-
-
-
-    JS_ASSERT(cx->runtime->gcNumber == gcNumberBefore);
-
-    if (!map_.add(p, key, shared))
+    if (!map_.relookupOrAdd(p, key, shared))
         goto error;
 
     
