6e3882dbba3441b623d57d17030706f59b415a1e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:05:38 +0000

    [JAEGER] Fixed assert in FrameState merging.
    UltraBlame original commit: 18bc7a3e83f479027ef97005794050d417e0e997

--- js/src/methodjit/FrameState.cpp
+++ js/src/methodjit/FrameState.cpp
@@ -385,7 +385,13 @@ FrameState::merge(Assembler &masm, uint32 iVD) const
             continue;
 
         FrameEntry *fe = entryFor(index);
-        JS_ASSERT(!fe->isCopy());
+
+        
+        if (fe->isCopy()) {
+            JS_ASSERT(!fe->data.inRegister());
+            JS_ASSERT(!fe->type.inRegister());
+            continue;
+        }
 
         if (fe->data.inRegister())
             masm.loadData32(addressOf(fe), fe->data.reg());
