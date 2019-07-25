78bfaa03cd02d05183aebcc1dacf9193f478ae51
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:56:24 +0000

    Bug 593608 - drawArrays() crash - r+a=vladimir
    UltraBlame original commit: b40c3f4138d941a6b1189e9bd128d167980804e7

--- content/canvas/src/WebGLContextGL.cpp
+++ content/canvas/src/WebGLContextGL.cpp
@@ -896,6 +896,7 @@ WebGLContext::DoFakeVertexAttrib0(WebGLuint vertexCount)
         mFakeVertexAttrib0Array[4 * i + 3] = mVertexAttrib0Vector[3];
     }
 
+    gl->fBindBuffer(LOCAL_GL_ARRAY_BUFFER, 0);
     gl->fVertexAttribPointer(0, 4, LOCAL_GL_FLOAT, LOCAL_GL_FALSE, 0, mFakeVertexAttrib0Array);
 }
 
@@ -907,12 +908,21 @@ WebGLContext::UndoFakeVertexAttrib0()
 
     mFakeVertexAttrib0Array = nsnull;
 
+    
+    
+    
+    if (mAttribBuffers[0].buf)
+        gl->fBindBuffer(LOCAL_GL_ARRAY_BUFFER, mAttribBuffers[0].buf->GLName());
+
     gl->fVertexAttribPointer(0,
                              mAttribBuffers[0].size,
                              mAttribBuffers[0].type,
                              mAttribBuffers[0].normalized,
                              mAttribBuffers[0].stride,
                              (const GLvoid *) mAttribBuffers[0].byteOffset);
+
+    
+    gl->fBindBuffer(LOCAL_GL_ARRAY_BUFFER, mBoundArrayBuffer ? mBoundArrayBuffer->GLName() : 0);
 }
 
 PRBool
