67b02ac1077e4b4437ef258fbb618c11eec5e4bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:07:20 +0000

    Bug 750859 - Remove {Disable,Revert}Capability. r=bz, PGO helper on CLOSED TREE
    UltraBlame original commit: e0d9d5a0987b667cee54e4dc3fe21500d83c9aee

--- caps/src/nsSystemPrincipal.cpp
+++ caps/src/nsSystemPrincipal.cpp
@@ -184,22 +184,6 @@ nsSystemPrincipal::EnableCapability(const char *capability, void **annotation)
 }
 
 NS_IMETHODIMP 
-nsSystemPrincipal::RevertCapability(const char *capability, void **annotation)
-{
-    *annotation = nsnull;
-    return NS_OK;
-}
-
-NS_IMETHODIMP 
-nsSystemPrincipal::DisableCapability(const char *capability, void **annotation) 
-{
-    
-    
-    *annotation = nsnull;
-    return NS_ERROR_FAILURE;
-}
-
-NS_IMETHODIMP 
 nsSystemPrincipal::GetURI(nsIURI** aURI)
 {
     *aURI = nsnull;
