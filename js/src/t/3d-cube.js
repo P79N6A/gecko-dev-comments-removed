0977929c39990f27af9d2e1dddf1ab3588b4f7f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:20:28 +0000

    [JAEGER] Reverted accidental test changes.
    UltraBlame original commit: 0b9d87e460e34112fe84f615cbaba126227c4a31

--- js/src/t/3d-cube.js
+++ js/src/t/3d-cube.js
@@ -54,7 +54,6 @@ function DrawLine(From, To) {
   NumPix = Math.round(Q.LastPx + NumPix);
 
   var i = Q.LastPx;
-
   for (; i < NumPix; i++) {
     Num += NumAdd;
     if (Num >= Den) {
@@ -65,7 +64,6 @@ function DrawLine(From, To) {
     x += IncX2;
     y += IncY2;
   }
-
   Q.LastPx = NumPix;
 }
 
@@ -79,17 +77,13 @@ function CalcCross(V0, V1) {
 
 function CalcNormal(V0, V1, V2) {
   var A = new Array();   var B = new Array(); 
-
   for (var i = 0; i < 3; i++) {
     A[i] = V0[i] - V1[i];
     B[i] = V2[i] - V1[i];
   }
-
   A = CalcCross(A, B);
   var Length = Math.sqrt(A[0]*A[0] + A[1]*A[1] + A[2]*A[2]); 
-
   for (var i = 0; i < 3; i++) A[i] = A[i] / Length;
-
   A[3] = 1;
   return A;
 }
@@ -103,14 +97,10 @@ function MMulti(M1, M2) {
   var M = [[],[],[],[]];
   var i = 0;
   var j = 0;
-
   for (; i < 4; i++) {
     j = 0;
-
     for (; j < 4; j++) M[i][j] = M1[i][0] * M2[0][j] + M1[i][1] * M2[1][j] + M1[i][2] * M2[2][j] + M1[i][3] * M2[3][j];
-
   }
-
   return M;
 }
 
@@ -118,18 +108,14 @@ function MMulti(M1, M2) {
 function VMulti(M, V) {
   var Vect = new Array();
   var i = 0;
-
   for (;i < 4; i++) Vect[i] = M[i][0] * V[0] + M[i][1] * V[1] + M[i][2] * V[2] + M[i][3] * V[3];
-
   return Vect;
 }
 
 function VMulti2(M, V) {
   var Vect = new Array();
   var i = 0;
-
   for (;i < 3; i++) Vect[i] = M[i][0] * V[0] + M[i][1] * V[1] + M[i][2] * V[2];
-
   return Vect;
 }
 
@@ -138,14 +124,10 @@ function MAdd(M1, M2) {
   var M = [[],[],[],[]];
   var i = 0;
   var j = 0;
-
   for (; i < 4; i++) {
     j = 0;
-
     for (; j < 4; j++) M[i][j] = M1[i][j] + M2[i][j];
-
   }
-
   return M;
 }
 
@@ -206,9 +188,7 @@ function DrawQube() {
   var CurN = new Array();
   var i = 5;
   Q.LastPx = 0;
-
   for (; i > -1; i--) CurN[i] = VMulti2(MQube, Q.Normal[i]);
-
   if (CurN[0][2] < 0) {
     if (!Q.Line[0]) { DrawLine(Q[0], Q[1]); Q.Line[0] = true; };
     if (!Q.Line[1]) { DrawLine(Q[1], Q[2]); Q.Line[1] = true; };
@@ -252,9 +232,7 @@ function DrawQube() {
 function Loop() {
   if (Testing.LoopCount > Testing.LoopMax) return;
   var TestingStr = String(Testing.LoopCount);
-
   while (TestingStr.length < 3) TestingStr = "0" + TestingStr;
-
   MTrans = Translate(I, -Q[8].V[0], -Q[8].V[1], -Q[8].V[2]);
   MTrans = RotateX(MTrans, 1);
   MTrans = RotateY(MTrans, 3);
@@ -262,11 +240,9 @@ function Loop() {
   MTrans = Translate(MTrans, Q[8].V[0], Q[8].V[1], Q[8].V[2]);
   MQube = MMulti(MTrans, MQube);
   var i = 8;
-
   for (; i > -1; i--) {
     Q[i].V = VMulti(MTrans, Q[i].V);
   }
-
   DrawQube();
   Testing.LoopCount++;
   Loop();
@@ -326,38 +302,30 @@ function Init(CubeSize) {
   
   
   Q.Normal = new Array();
-
   for (var i = 0; i < Q.Edge.length; i++) Q.Normal[i] = CalcNormal(Q[Q.Edge[i][0]].V, Q[Q.Edge[i][1]].V, Q[Q.Edge[i][2]].V);
-
   
   
   Q.Line = [false,false,false,false,false,false,false,false,false,false,false,false];
   
   
   Q.NumPx = 9 * 2 * CubeSize;
-
   for (var i = 0; i < Q.NumPx; i++) CreateP(0,0,0);
-
   
   MTrans = Translate(MTrans, Origin.V[0], Origin.V[1], Origin.V[2]);
   MQube = MMulti(MTrans, MQube);
 
   var i = 0;
-
   for (; i < 9; i++) {
     Q[i].V = VMulti(MTrans, Q[i].V);
   }
-
   DrawQube();
   Testing.Init = true;
   Loop();
 }
 
-
 for ( var i = 20; i <= 160; i *= 2 ) {
   Init(i);
 }
-
 
 Q = null;
 MTrans = null;
--- js/src/t/3d-morph.js
+++ js/src/t/3d-morph.js
@@ -32,33 +32,23 @@ function morph(a, f) {
     var sin = Math.sin
     var f30 = -(50 * sin(f*Math.PI*2))
     
-
     for (var i = 0; i < nz; ++i) {
-
         for (var j = 0; j < nx; ++j) {
             a[3*(i*nx+j)+1]    = sin((j-1) * PI2nx ) * -f30
         }
-
     }
-
 }
 
     
 var a = Array()
-
 for (var i=0; i < nx*nz*3; ++i) 
     a[i] = 0
-
-
 
 for (var i = 0; i < loops; ++i) {
     morph(a, i/loops)
 }
 
-
 testOutput = 0;
-
 for (var i = 0; i < nx; i++)
     testOutput += a[3*(i*nx+i)+1];
-
 a = null;
