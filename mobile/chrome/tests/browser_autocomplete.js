4d3846abdded01284fe2f4002345963f298ced00
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:04:53 +0000

    Bug 628616 - Make sure suggestions from <datalist> are shown in Firefox Mobile UI [r=mfinkle]
    UltraBlame original commit: 6ec43c0829382dee5534be71cba2b37fc51a159b

---
+++ mobile/chrome/tests/browser_autocomplete.js
@@ -0,0 +1,140 @@
+let testURL = chromeRoot + "browser_autocomplete.html";
+messageManager.loadFrameScript(chromeRoot + "remote_autocomplete.js", true);
+
+let newTab = null;
+
+
+var gTests = [];
+var gCurrentTest = null;
+
+function test() {
+  
+  waitForExplicitFinish();
+
+  
+  messageManager.addMessageListener("pageshow", function(aMessage) {
+    if (newTab && newTab.browser.currentURI.spec != "about:blank") {
+      messageManager.removeMessageListener(aMessage.name, arguments.callee);
+      BrowserUI.closeAutoComplete(true);
+      setTimeout(runNextTest, 0);
+    }
+  });
+
+  newTab = Browser.addTab(testURL, true);
+}
+
+
+
+function runNextTest() {
+  
+  if (gTests.length > 0) {
+    gCurrentTest = gTests.shift();
+    info(gCurrentTest.desc);
+    gCurrentTest.run();
+  }
+  else {
+    
+    try {
+      
+    }
+    finally {
+      
+      finish();
+    }
+  }
+}
+
+function waitForAutocomplete(aCallback) {
+  messageManager.addMessageListener("FormAssist:AutoComplete", function(aMessage) {
+    messageManager.removeMessageListener(aMessage.name, arguments.callee);
+    setTimeout(function() {
+      aCallback(aMessage.json.current.list);
+    }, 0);
+  });
+};
+
+let data = [
+  { label: "foo", value: "foo" },
+  { label: "Somewhat bar", value: "bar" },
+  { label: "foobar", value: "_" }
+];
+
+
+
+gTests.push({
+  desc: "Click on a datalist element and show suggestions",
+
+  run: function() {
+    waitForAutocomplete(gCurrentTest.checkData);
+    AsyncTests.waitFor("TestRemoteAutocomplete:Click",
+                        { id: "input-datalist-1" }, function(json) {});
+  },
+
+  
+  
+  checkData: function(aOptions) {
+    for (let i = 0; i < aOptions.length; i++) {
+      let option = aOptions[i];
+      let valid = data[i];
+
+      is(option.label, valid.label, "Label should be equal (" + option.label + ", " + valid.label +")");
+      is(option.value, valid.value, "Value should be equal (" + option.value + ", " + valid.value +")");
+    }
+
+    
+    waitFor(gCurrentTest.checkUI, function() {
+      let suggestionsBox = document.getElementById("form-helper-suggestions");
+      return suggestionsBox.childNodes.length;
+    });
+  },
+
+  
+  checkUI: function() {
+    let suggestionsBox = document.getElementById("form-helper-suggestions");
+    let suggestions = suggestionsBox.childNodes;
+
+    for (let i = 0; i < suggestions.length; i++) {
+      let suggestion = suggestions[i];
+      let valid = data[i];
+      let label = suggestion.getAttribute("value");
+      let value = suggestion.getAttribute("data");
+
+      is(label, valid.label, "Label should be equal (" + label + ", " + valid.label +")");
+      is(value, valid.value, "Value should be equal (" + value + ", " + valid.value +")");
+    }
+
+    gCurrentTest.checkUIClick(0);
+  },
+
+  
+  
+  checkUIClick: function(aIndex) {
+    let suggestionsBox = document.getElementById("form-helper-suggestions");
+
+    let suggestion = suggestionsBox.childNodes[aIndex];
+    if (!suggestion) {
+      gCurrentTest.finish();
+      return;
+    }
+
+    
+    FormHelperUI.doAutoComplete(suggestion);
+
+    AsyncTests.waitFor("TestRemoteAutocomplete:Check", { id: "input-datalist-1" }, function(json) {
+      is(json.result, suggestion.getAttribute("data"), "The target input value should be set to " + data);
+      gCurrentTest.checkUIClick(aIndex + 1);
+    });
+  },
+
+  finish: function() {
+    
+    FormHelperUI.hide();
+
+    
+    Browser.closeTab(newTab);
+
+    
+    finish();
+  }
+});
+
