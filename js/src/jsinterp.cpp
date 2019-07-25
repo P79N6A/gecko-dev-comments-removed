465fc3c98496ba0c2f7364c2510e6ccef43a9a47
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:03:20 +0000

    Undo strict mode eval changes *yet again* until I have time to figure out the jsreftest orange, if it's even due to strict mode eval changes, and if it's even real.  r=heisenorange
    UltraBlame original commit: d099c5659761a9e7feef6d0e345bf7b74e87cf0e

--- js/src/jsinterp.cpp
+++ js/src/jsinterp.cpp
@@ -966,6 +966,7 @@ Execute(JSContext *cx, JSObject *chain, JSScript *script,
         initialVarObj = (cx->options & JSOPTION_VAROBJFIX) ? chain->getGlobal() : chain;
     }
 
+#if 0 
     
 
 
@@ -983,6 +984,7 @@ Execute(JSContext *cx, JSObject *chain, JSScript *script,
         frame.fp()->setScopeChainAndCallObj(*initialVarObj);
     }
     JS_ASSERT(!initialVarObj->getOps()->defineProperty);
+#endif
 
 #if JS_HAS_SHARP_VARS
     JS_STATIC_ASSERT(SHARP_NSLOTS == 2);
