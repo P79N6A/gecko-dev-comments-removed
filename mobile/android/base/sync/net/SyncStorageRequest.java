896693c73cc96b1c94538e2ad1f0efa12c2adb2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:53:47 +0000

    Bug 732611 - Add channel (Aurora, Beta, etc.) to Android Sync user agent. r=rnewman
    UltraBlame original commit: f3a1091ccf994531a758b3d192ae9b0b5e1d6509

--- mobile/android/base/sync/net/SyncStorageRequest.java
+++ mobile/android/base/sync/net/SyncStorageRequest.java
@@ -2,39 +2,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 package org.mozilla.gecko.sync.net;
 
 import java.io.IOException;
@@ -42,6 +9,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
 import java.util.HashMap;
+
+import org.mozilla.gecko.sync.GlobalConstants;
 
 import android.util.Log;
 
@@ -163,7 +132,7 @@ public class SyncStorageRequest implements Resource {
 
     @Override
     public void addHeaders(HttpRequestBase request, DefaultHttpClient client) {
-      client.getParams().setParameter(CoreProtocolPNames.USER_AGENT, USER_AGENT);
+      client.getParams().setParameter(CoreProtocolPNames.USER_AGENT, GlobalConstants.USER_AGENT);
 
       
       String ifUnmodifiedSince = this.request.delegate.ifUnmodifiedSince();
@@ -176,7 +145,6 @@ public class SyncStorageRequest implements Resource {
     }
   }
 
-  public static String USER_AGENT = "Firefox AndroidSync 0.5";
   protected SyncResourceDelegate resourceDelegate;
   public SyncStorageRequestDelegate delegate;
   protected BaseResource resource;
