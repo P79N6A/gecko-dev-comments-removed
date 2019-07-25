42b14ef0a623214c1a2708a7d867c56318a86436
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:10:29 +0000

    Bug 522375: fix non-libxul builds + testcase nits from gavin. a=bustage,tests
    UltraBlame original commit: 392f4f212ea995720be6b5fe2ccce57cc890fe4b

--- browser/components/sessionstore/test/browser/browser_522375.js
+++ browser/components/sessionstore/test/browser/browser_522375.js
@@ -1,13 +1,11 @@
 function test() {
-  waitForExplicitFinish();
   var startup_info = Components.classes["@mozilla.org/toolkit/app-startup;1"].getService(Components.interfaces.nsIAppStartup_MOZILLA_2_0).getStartupInfo();
   
-  is(startup_info.process <= startup_info.main, true, "process created before main is run " + uneval(startup_info));
+  ok(startup_info.process <= startup_info.main, "process created before main is run " + uneval(startup_info));
 
   
   if (startup_info.firstPaint)
-    is(startup_info.main <= startup_info.firstPaint, true, "main ran before first paint " + uneval(startup_info));
+    ok(startup_info.main <= startup_info.firstPaint, "main ran before first paint " + uneval(startup_info));
 
-  is(startup_info.main < startup_info.sessionRestored, true, "Session restored after main " + uneval(startup_info));
-  finish();
+  ok(startup_info.main < startup_info.sessionRestored, "Session restored after main " + uneval(startup_info));
 }
