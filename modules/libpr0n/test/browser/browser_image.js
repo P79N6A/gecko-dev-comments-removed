fd7a5fdaf0d1900968821118919fa70be1f60f17
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:57:44 +0000

    Tests for bug 359608 - Animated GIFs are animated even when user navigates to another page.r=joe,a=blocker
    UltraBlame original commit: 7fda546db6a6b671f3baebcf02e470d6ea88ae7b

---
+++ modules/libpr0n/test/browser/browser_image.js
@@ -0,0 +1,175 @@
+waitForExplicitFinish();
+
+
+
+var gTimer;
+
+
+
+function testBFCache() {
+  function theTest() {
+    var abort = false;
+    var chances, gImage, gFrames;
+    gBrowser.selectedTab = gBrowser.addTab();
+    switchToTabHavingURI(TESTROOT + "image.html", true, function(aBrowser) {
+      var window = aBrowser.contentWindow;
+      
+      
+      if (!actOnMozImage(window.document, "img1", function(image) {
+        gImage = image;
+        gFrames = gImage.framesNotified;
+      })) {
+        gBrowser.removeCurrentTab();
+        abort = true;
+      }
+      goer.next();
+    });
+    yield;
+    if (abort) {
+      finish();
+      yield; 
+    }
+
+    
+    chances = 120;
+    do {
+      gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+      gTimer.initWithCallback(function() {
+        if (gImage.framesNotified >= 10) {
+          goer.send(true);
+        } else {
+          chances--;
+          goer.send(chances == 0); 
+        }
+      }, 500, Ci.nsITimer.TYPE_ONE_SHOT);
+    } while (!(yield));
+    is(chances > 0, true, "Must have animated a few frames so far");
+
+    
+    gBrowser.loadURI("about:blank");
+
+    
+    
+    gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+    gTimer.initWithCallback(function() {
+      gFrames = gImage.framesNotified;
+      gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+      gTimer.initWithCallback(function() {
+        
+        is(gImage.framesNotified == gFrames, true, "Must have not animated in bfcache!");
+        goer.next();
+      }, 4000, Ci.nsITimer.TYPE_ONE_SHOT);
+    }, 0, Ci.nsITimer.TYPE_ONE_SHOT); 
+    yield;
+
+    
+    gBrowser.goBack();
+
+    chances = 120;
+    do {
+      gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+      gTimer.initWithCallback(function() {
+        if (gImage.framesNotified - gFrames >= 10) {
+          goer.send(true);
+        } else {
+          chances--;
+          goer.send(chances == 0); 
+        }
+      }, 500, Ci.nsITimer.TYPE_ONE_SHOT);
+    } while (!(yield));
+    is(chances > 0, true, "Must have animated once out of bfcache!");
+
+    gBrowser.removeCurrentTab();
+
+    nextTest();
+  }
+
+  var goer = theTest();
+  goer.next();
+}
+
+
+
+function testSharedContainers() {
+  function theTest() {
+    var gImages = [];
+    var gFrames;
+
+    gBrowser.selectedTab = gBrowser.addTab();
+    switchToTabHavingURI(TESTROOT + "image.html", true, function(aBrowser) {
+      actOnMozImage(aBrowser.contentWindow.window.document, "img1", function(image) {
+        gImages[0] = image;
+        gFrames = image.framesNotified; 
+                                        
+      });
+      goer.next();
+    });
+    yield;
+
+    
+    gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+    gTimer.initWithCallback(function() {
+      gBrowser.selectedTab = gBrowser.addTab();
+      goer.next();
+    }, 1500, Ci.nsITimer.TYPE_ONE_SHOT);
+    yield;
+
+    switchToTabHavingURI(TESTROOT + "imageX2.html", true, function(aBrowser) {
+      [1,2].forEach(function(i) {
+        actOnMozImage(aBrowser.contentWindow.window.document, "img"+i, function(image) {
+          gImages[i] = image;
+        });
+      });
+      goer.next();
+    });
+    yield;
+
+    chances = 120;
+    do {
+      gTimer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+      gTimer.initWithCallback(function() {
+        if (gImages[0].framesNotified - gFrames >= 10) {
+          goer.send(true);
+        } else {
+          chances--;
+          goer.send(chances == 0); 
+        }
+      }, 500, Ci.nsITimer.TYPE_ONE_SHOT);
+    } while (!(yield));
+    is(chances > 0, true, "Must have been animating while showing several images");
+
+    
+    var theFrames = null;
+    [0,1,2].forEach(function(i) {
+      var frames = gImages[i].framesNotified;
+      if (theFrames == null) {
+        theFrames = frames;
+      } else {
+        is(theFrames, frames, "Sharing the same imgContainer means *exactly* the same frame counts!");
+      }
+    });
+
+    gBrowser.removeCurrentTab();
+    gBrowser.removeCurrentTab();
+
+    nextTest();
+  }
+
+  var goer = theTest();
+  goer.next();
+}
+
+var tests = [testBFCache, testSharedContainers];
+
+function nextTest() {
+  if (tests.length == 0) {
+    finish();
+    return;
+  }
+  tests.shift()();
+}
+
+function test() {
+  nextTest();
+}
+
