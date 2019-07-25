2a6a4fd11f378e41ddceecf2f4648460da1f38be
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:44:39 +0000

    Bug 513681 - part 3 - instantiate decoders without using COM goop.r=joe,a=blocker
    UltraBlame original commit: 724bf8e536e5dd3ad1063fece3da7f6591d6a3fb

--- modules/libpr0n/src/Image.cpp
+++ modules/libpr0n/src/Image.cpp
@@ -47,5 +47,52 @@ Image::Image() :
 {
 }
 
+
+Image::eDecoderType
+Image::GetDecoderType(const char *aMimeType)
+{
+  
+  eDecoderType rv = eDecoderType_unknown;
+
+  
+  if (!strcmp(aMimeType, "image/png"))
+    rv = eDecoderType_png;
+  else if (!strcmp(aMimeType, "image/x-png"))
+    rv = eDecoderType_png;
+
+  
+  else if (!strcmp(aMimeType, "image/gif"))
+    rv = eDecoderType_gif;
+
+
+  
+  else if (!strcmp(aMimeType, "image/jpeg"))
+    rv = eDecoderType_jpeg;
+  else if (!strcmp(aMimeType, "image/pjpeg"))
+    rv = eDecoderType_jpeg;
+  else if (!strcmp(aMimeType, "image/jpg"))
+    rv = eDecoderType_jpeg;
+
+  
+  else if (!strcmp(aMimeType, "image/bmp"))
+    rv = eDecoderType_bmp;
+  else if (!strcmp(aMimeType, "image/x-ms-bmp"))
+    rv = eDecoderType_bmp;
+
+
+  
+  else if (!strcmp(aMimeType, "image/x-icon"))
+    rv = eDecoderType_ico;
+  else if (!strcmp(aMimeType, "image/vnd.microsoft.icon"))
+    rv = eDecoderType_ico;
+
+  
+  else if (!strcmp(aMimeType, "image/icon"))
+    rv = eDecoderType_icon;
+
+  return rv;
+}
+
+
 } 
 } 
