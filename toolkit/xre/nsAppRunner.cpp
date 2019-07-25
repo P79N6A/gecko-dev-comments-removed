e06fb3060d8c59e5d1aa30db7f928b54c049872b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:02:41 +0000

    Bug 694039. Work around OpenGL quirks in the plugin process. r=bgirard
    We do the same in the main process so that we get the right
    bundle id on in Nightly and Aurora
    
    UltraBlame original commit: 17ef2ebb8ef98c90d3df71025cd896c000fd17bf

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -71,6 +71,9 @@
 #include "MacLaunchHelper.h"
 #include "MacApplicationDelegate.h"
 #include "MacAutoreleasePool.h"
+
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #endif
 
 #ifdef XP_OS2
@@ -2655,6 +2658,8 @@ XRE_main(int argc, char* argv[], const nsXREAppData* aAppData)
     NS_BREAK();
 #endif
 
+  TriggerQuirks();
+
   
   
   
@@ -3787,3 +3792,48 @@ SetupErrorHandling(const char* progname)
   fpsetmask(0);
 #endif
 }
+
+void
+TriggerQuirks()
+{
+#if defined(XP_MACOSX)
+  int mib[2];
+
+  mib[0] = CTL_KERN;
+  mib[1] = KERN_OSRELEASE;
+  
+  char release[sizeof("10.7.99")];
+  size_t len = sizeof(release);
+  
+  int ret = sysctl(mib, 2, release, &len, NULL, 0);
+  
+  
+  
+  if (ret == 0 && NS_CompareVersions(release, "10.8.0") >= 0 && NS_CompareVersions(release, "11") < 0) {
+    CFBundleRef mainBundle = CFBundleGetMainBundle();
+    if (mainBundle) {
+      CFRetain(mainBundle);
+
+      CFStringRef bundleID = CFBundleGetIdentifier(mainBundle);
+      if (bundleID) {
+        CFRetain(bundleID);
+
+        CFMutableDictionaryRef dict = (CFMutableDictionaryRef)CFBundleGetInfoDictionary(mainBundle);
+        CFDictionarySetValue(dict, CFSTR("CFBundleIdentifier"), CFSTR("org.mozilla.firefox"));
+
+        
+        SInt32 major;
+        ::Gestalt(gestaltSystemVersionMajor, &major);
+
+        
+        dict = (CFMutableDictionaryRef)CFBundleGetInfoDictionary(mainBundle);
+        CFDictionarySetValue(dict, CFSTR("CFBundleIdentifier"), bundleID);
+
+        CFRelease(bundleID);
+      }
+      CFRelease(mainBundle);
+    }
+  }
+#endif
+}
+
