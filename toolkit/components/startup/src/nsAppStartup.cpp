9f711d0c60957e37e91d10e0e36650af0fb9800a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:33:15 +0000

    Bug 630703: Provide a way to restart in 32-bit mode. r=josh, r=bsmedberg, a=blocks-betaN
    UltraBlame original commit: f00b81064d570e372b70ac673ca4b00c40556f2e

--- toolkit/components/startup/src/nsAppStartup.cpp
+++ toolkit/components/startup/src/nsAppStartup.cpp
@@ -99,6 +99,8 @@ extern PRTime gFirstPaintTimestamp;
 static PRTime gRestoredTimestamp = 0;       
 static PRTime gProcessCreationTimestamp = 0;
 
+PRUint32 gRestartMode = 0;
+
 class nsAppExitEvent : public nsRunnable {
 private:
   nsRefPtr<nsAppStartup> mService;
@@ -289,8 +291,10 @@ nsAppStartup::Quit(PRUint32 aMode)
     }
 
     mShuttingDown = PR_TRUE;
-    if (!mRestart)
+    if (!mRestart) {
       mRestart = (aMode & eRestart) != 0;
+      gRestartMode = (aMode & 0xF0);
+    }
 
     if (mRestart) {
       
