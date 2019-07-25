33a628430b114e4e36e944ced3453049e71c8388
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:07 +0000

    Bug 552832 - Make about: pages in Fennec be local, in-content UI [r=mbrubeck r=vingtetun]
    UltraBlame original commit: f96a7037c7e17d0976adf49446e677e862fdc9fc

--- mobile/chrome/content/browser-ui.js
+++ mobile/chrome/content/browser-ui.js
@@ -530,8 +530,17 @@ var BrowserUI = {
     let fixupFlags = Ci.nsIURIFixup.FIXUP_FLAG_ALLOW_KEYWORD_LOOKUP;
     let uri = gURIFixup.createFixupURI(aURI, fixupFlags);
 
-    let loadFlags = Ci.nsIWebNavigation.LOAD_FLAGS_ALLOW_THIRD_PARTY_FIXUP;
-    getBrowser().loadURIWithFlags(uri.spec, loadFlags, null, null);
+    
+    
+    let currentURI = getBrowser().currentURI;
+    let useLocal = (uri.schemeIs("about") && uri.spec != "about:blank");
+    let hasLocal = (currentURI.schemeIs("about") && currentURI.spec != "about:blank");
+    if (useLocal || hasLocal != useLocal) {
+      BrowserUI.newTab(uri.spec);
+    } else {
+      let loadFlags = Ci.nsIWebNavigation.LOAD_FLAGS_ALLOW_THIRD_PARTY_FIXUP;
+      getBrowser().loadURIWithFlags(uri.spec, loadFlags, null, null);
+    }
 
     gHistSvc.markPageAsTyped(uri);
   },
