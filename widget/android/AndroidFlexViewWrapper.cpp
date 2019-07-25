c4f99393675ecb39afb04a50ecfd6395db59c3a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:56:13 +0000

    Import jni implementation
    UltraBlame original commit: e65daf4e910dad56c7fd947d822158e016319371

--- widget/android/AndroidFlexViewWrapper.cpp
+++ widget/android/AndroidFlexViewWrapper.cpp
@@ -1,1 +1,185 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include "AndroidFlexViewWrapper.h"
+
+static AndroidGLController sController;
+
+jmethodID AndroidGLController::jSetGLVersionMethod = 0;
+jmethodID AndroidGLController::jInitGLContextMethod = 0;
+jmethodID AndroidGLController::jDisposeGLContextMethod = 0;
+jmethodID AndroidGLController::jGetEGLDisplayMethod = 0;
+jmethodID AndroidGLController::jGetEGLConfigMethod = 0;
+jmethodID AndroidGLController::jGetEGLContextMethod = 0;
+jmethodID AndroidGLController::jGetEGLSurfaceMethod = 0;
+jmethodID AndroidGLController::jHasSurfaceMethod = 0;
+jmethodID AndroidGLController::jSwapBuffersMethod = 0;
+jmethodID AndroidGLController::jCheckForLostContextMethod = 0;
+jmethodID AndroidGLController::jWaitForValidSurfaceMethod = 0;
+jmethodID AndroidGLController::jGetWidthMethod = 0;
+jmethodID AndroidGLController::jGetHeightMethod = 0;
+
+void
+AndroidGLController::Init(JNIEnv *aJEnv)
+{
+    const char *className = "org/mozilla/testnewglsurfaceview/GLController";
+    jclass jClass = reinterpret_cast<jclass>(aJEnv->NewGlobalRef(aJEnv->FindClass(className)));
+
+    jSetGLVersionMethod = aJEnv->GetMethodID(jClass, "setGLVersion", "(I)V");
+    jInitGLContextMethod = aJEnv->GetMethodID(jClass, "initGLContext", "()V");
+    jDisposeGLContextMethod = aJEnv->GetMethodID(jClass, "disposeGLContext", "()V");
+    jGetEGLDisplayMethod = aJEnv->GetMethodID(jClass, "getEGLDisplay",
+                                              "()Ljavax/microedition/khronos/egl/EGLDisplay;");
+    jGetEGLConfigMethod = aJEnv->GetMethodID(jClass, "getEGLConfig",
+                                             "()Ljavax/microedition/khronos/egl/EGLConfig;");
+    jGetEGLContextMethod = aJEnv->GetMethodID(jClass, "getEGLContext",
+                                              "()Ljavax/microedition/khronos/egl/EGLContext;");
+    jGetEGLSurfaceMethod = aJEnv->GetMethodID(jClass, "getEGLSurface",
+                                              "()Ljavax/microedition/khronos/egl/EGLSurface;");
+    jHasSurfaceMethod = aJEnv->GetMethodID(jClass, "hasSurface", "()Z");
+    jSwapBuffersMethod = aJEnv->GetMethodID(jClass, "swapBuffers", "()Z");
+    jCheckForLostContextMethod = aJEnv->GetMethodID(jClass, "checkForLostContext", "()Z");
+    jWaitForValidSurfaceMethod = aJEnv->GetMethodID(jClass, "waitForValidSurface", "()V");
+    jGetWidthMethod = aJEnv->GetMethodID(jClass, "getWidth", "()I");
+    jGetHeightMethod = aJEnv->GetMethodID(jClass, "getHeight", "()I");
+}
+
+void
+AndroidGLController::Acquire(JNIEnv* aJEnv, jobject aJObj)
+{
+    mJEnv = aJEnv;
+    mJObj = aJEnv->NewGlobalRef(aJObj);
+}
+
+void
+AndroidGLController::Acquire(JNIEnv* aJEnv)
+{
+    mJEnv = aJEnv;
+}
+
+void
+AndroidGLController::Release()
+{
+    if (mJObj) {
+        mJEnv->DeleteGlobalRef(mJObj);
+        mJObj = NULL;
+    }
+
+    mJEnv = NULL;
+}
+
+void
+AndroidGLController::SetGLVersion(int aVersion)
+{
+    mJEnv->CallVoidMethod(mJObj, jSetGLVersionMethod, aVersion);
+}
+
+void
+AndroidGLController::InitGLContext()
+{
+    mJEnv->CallVoidMethod(mJObj, jInitGLContextMethod);
+}
+
+void
+AndroidGLController::DisposeGLContext()
+{
+    mJEnv->CallVoidMethod(mJObj, jDisposeGLContextMethod);
+}
+
+EGLDisplay
+AndroidGLController::GetEGLDisplay()
+{
+    AndroidEGLDisplay jEGLDisplay(mJEnv, mJEnv->CallObjectMethod(mJObj, jGetEGLDisplayMethod));
+    return *jEGLDisplay;
+}
+
+EGLConfig
+AndroidGLController::GetEGLConfig()
+{
+    AndroidEGLConfig jEGLConfig(mJEnv, mJEnv->CallObjectMethod(mJObj, jGetEGLConfigMethod));
+    return *jEGLConfig;
+}
+
+EGLContext
+AndroidGLController::GetEGLContext()
+{
+    AndroidEGLContext jEGLContext(mJEnv, mJEnv->CallObjectMethod(mJObj, jGetEGLContextMethod));
+    return *jEGLContext;
+}
+
+EGLSurface
+AndroidGLController::GetEGLSurface()
+{
+    AndroidEGLSurface jEGLSurface(mJEnv, mJEnv->CallObjectMethod(mJObj, jGetEGLSurfaceMethod));
+    return *jEGLSurface;
+}
+
+bool
+AndroidGLController::HasSurface()
+{
+    return mJEnv->CallBooleanMethod(mJObj, jHasSurfaceMethod);
+}
+
+bool
+AndroidGLController::SwapBuffers()
+{
+    return mJEnv->CallBooleanMethod(mJObj, jSwapBuffersMethod);
+}
+
+bool
+AndroidGLController::CheckForLostContext()
+{
+    return mJEnv->CallBooleanMethod(mJObj, jCheckForLostContextMethod);
+}
+
+void
+AndroidGLController::WaitForValidSurface()
+{
+    mJEnv->CallVoidMethod(mJObj, jWaitForValidSurfaceMethod);
+}
+
+int
+AndroidGLController::GetWidth()
+{
+    return mJEnv->CallIntMethod(mJObj, jGetWidthMethod);
+}
+
+int
+AndroidGLController::GetHeight()
+{
+    return mJEnv->CallIntMethod(mJObj, jGetHeightMethod);
+}
