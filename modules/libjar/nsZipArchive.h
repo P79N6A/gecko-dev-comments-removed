a4a8253e5ac49675e140e117b9620cee2b16834f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:52:12 +0000

    Bug 711297 - add comment reading to nsZipArchive; r=taras
    UltraBlame original commit: 60d47e224c40371a3393f76ec433e68c58364ece

--- modules/libjar/nsZipArchive.h
+++ modules/libjar/nsZipArchive.h
@@ -224,6 +224,8 @@ public:
 
   const PRUint8* GetData(nsZipItem* aItem);
 
+  bool GetComment(nsACString &aComment);
+
   
 
 
@@ -242,6 +244,9 @@ private:
 
   nsZipItem*    mFiles[ZIP_TABSIZE];
   PLArenaPool   mArena;
+
+  const char*   mCommentPtr;
+  PRUint16      mCommentLen;
 
   
   bool          mBuiltSynthetics;
