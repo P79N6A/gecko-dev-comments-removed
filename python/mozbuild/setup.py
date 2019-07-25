036549c9ece61000229c38a3ee5c31d679d06851
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:37:12 +0000

    Bug 777231 - Add skeleton for mozbuild package; r=vlad
    UltraBlame original commit: da67b46ba82ca7d0bba833405722659d0388f173

---
+++ python/mozbuild/setup.py
@@ -0,0 +1,15 @@
+
+
+
+
+from setuptools import setup
+
+VERSION = '0.1'
+
+setup(
+    name='mozbuild',
+    description='Mozilla build system functionality.',
+    license='MPL 2.0',
+    packages=['mozbuild'],
+    version=VERSION
+)
