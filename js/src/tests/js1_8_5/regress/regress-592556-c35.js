718c526020e31ff5476bd9e99c6a17f2331bece2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:30:17 +0000

    Add passing test taken from bug 592556, comment 35. no_r=me. In before regression!
    UltraBlame original commit: 8ce3a4a29efc88888914cf4eea044f051c86924b

---
+++ js/src/tests/js1_8_5/regress/regress-592556-c35.js
@@ -0,0 +1,20 @@
+
+
+
+
+var obj = {a: 0, b: 1, c: 2};
+delete obj.b;  
+Object.defineProperty(obj, 'g',
+                      {get: function () { return -1; }, configurable: true});
+for (var i = 3; i < 20; i++)
+    obj['x' + i] = i;  
+for (var i = 3; i < 20; i++)
+    delete obj['x' + i];  
+delete obj.g;  
+
+
+gc();
+obj.d = 3;
+obj.e = 4;
+
+reportCompare(0, 0, 'ok');
