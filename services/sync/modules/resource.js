ce55392f09704b6e5aaf0790b16365b82d01b7d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:23:17 +0000

    Remove some unnecessary logs that follow right after the notify/event now that we only show verbose. Only log non-success response body for Trace.
    UltraBlame original commit: ea750a3618b9604dd78c3829678a6a1ad7c33fc0

--- services/sync/modules/resource.js
+++ services/sync/modules/resource.js
@@ -240,8 +240,18 @@ Resource.prototype = {
         if (this._log.level <= Log4Moz.Level.Trace)
           this._log.trace(action + " Body: " + this._data);
       }
-      else
-        this._log.debug(action + " fail: " + status + " " + this._data);
+      else {
+        let log = "debug";
+        let mesg = action + " fail: " + status;
+
+        
+        if (this._log.level <= Log4Moz.Level.Trace) {
+          log = "trace";
+          mesg += " " + this._data;
+        }
+
+        this._log[log](mesg);
+      }
     }
     
     catch(ex) {
