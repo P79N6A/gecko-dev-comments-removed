622c01244b371195e50daf471936e07d880d1f8e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:53:52 +0000

    Bug 729940 - Part 1: Add hash function utilities to mfbt. r=waldo
    UltraBlame original commit: f9f51c726fa700c932a2cac10b7d6ec7f3d41bfd

---
+++ mfbt/HashFunctions.cpp
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "mozilla/HashFunctions.h"
+#include <string.h>
+
+namespace mozilla {
+
+MFBT_API(uint32_t)
+HashBytes(const void* bytes, size_t length)
+{
+  uint32_t hash = 0;
+  const char* b = reinterpret_cast<const char*>(bytes);
+
+  
+  size_t i = 0;
+  for (; i < length - (length % sizeof(size_t)); i += sizeof(size_t)) {
+    
+    size_t data;
+    memcpy(&data, b + i, sizeof(size_t));
+
+    hash = AddToHash(hash, data, sizeof(data));
+  }
+
+  
+  for (; i < length; i++) {
+    hash = AddToHash(hash, b[i]);
+  }
+
+  return hash;
+}
+
+} 
