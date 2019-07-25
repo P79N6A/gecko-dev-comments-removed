8f4ae02e75ed320f9bfbd8e153940f518bb6e1aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:54:02 +0000

    Forgot a line in GVN, it was Important (bug 737300, r=jandem)
    UltraBlame original commit: 15da4376607bdd326e62c68d42f1831ba10007df

--- js/src/ion/ValueNumbering.h
+++ js/src/ion/ValueNumbering.h
@@ -145,12 +145,18 @@ class ValueNumberData : public TempObject {
         
         if (number == rep->valueNumber())
             return;
+
         if (classNext)
             classNext->valueNumberData()->classPrev = classPrev;
         if (classPrev)
-            classPrev->valueNumberData()->classPrev = classNext;
+            classPrev->valueNumberData()->classNext = classNext;
+
+
         classPrev = rep;
         classNext = rep->valueNumberData()->classNext;
+
+        if (rep->valueNumberData()->classNext)
+            rep->valueNumberData()->classNext->valueNumberData()->classPrev = thisDef;
         rep->valueNumberData()->classNext = thisDef;
     }
 };
