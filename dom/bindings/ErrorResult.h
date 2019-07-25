8a1073f228830c543f93acf0f5d9a63c138a98dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:08:37 +0000

    Bug 749485.  Switch new DOM bindings to using a struct for keeping track of failures for fallible methods.  r=peterv,bent
    ErrorResult is in a separate header file so it can be included from all over the
    place without having to pull in mozilla/dom/Utils.h and all the xpconnect gunk
    that needs.
    
    UltraBlame original commit: 1907bf7e6d7c2f89e484534e5db7312d224f9814

---
+++ dom/bindings/ErrorResult.h
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_ErrorResult_h
+#define mozilla_ErrorResult_h
+
+#include "nscore.h"
+
+namespace mozilla {
+
+class ErrorResult {
+public:
+  ErrorResult() {
+    mResult = NS_OK;
+  }
+
+  void Throw(nsresult rv) {
+    MOZ_ASSERT(NS_FAILED(rv), "Please don't try throwing success");
+    mResult = rv;
+  }
+
+  
+  
+  
+
+  
+  
+  
+  void operator=(nsresult rv) {
+    mResult = rv;
+  }
+
+  bool Failed() const {
+    return NS_FAILED(mResult);
+  }
+
+  nsresult ErrorCode() const {
+    return mResult;
+  }
+
+private:
+  nsresult mResult;
+};
+
+} 
+
+#endif 
