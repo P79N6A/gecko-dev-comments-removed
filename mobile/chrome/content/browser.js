4c4462c77fd1a7d4d4dda56fb721443e071fb0f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:01:16 +0000

    Bug 470477: make sure to always redraw when switching tabs, r=stuart
    UltraBlame original commit: d928e98c35ae47cd4816a77f0d2d4760c86d96a8

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -380,6 +380,10 @@ var Browser = {
       return;
 
     this._currentTab = tab;
+
+    
+    this._currentViewportBounds = { width: 0, height: 0};
+
     this._canvasBrowser.setCurrentBrowser(this.currentBrowser);
     document.getElementById("tabs").selectedItem = tab.content;
 
