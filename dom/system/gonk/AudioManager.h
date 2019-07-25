cf95e84198c37e80da138c7913f7b50e32d827a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:05:16 +0000

    Bug 736939 - AudioManager implementation. r=cjones, a=b2g-only
    UltraBlame original commit: ce9fff905894b2d253c4d9cb496e286f6f801d6a

--- dom/system/gonk/AudioManager.h
+++ dom/system/gonk/AudioManager.h
@@ -38,15 +38,22 @@
 #ifndef mozilla_dom_system_b2g_audiomanager_h__
 #define mozilla_dom_system_b2g_audiomanager_h__
 
+#include "mozilla/Observer.h"
+#include "nsAutoPtr.h"
 #include "nsIAudioManager.h"
-
 
 
 #define NS_AUDIOMANAGER_CID {0x94f6fd70, 0x7615, 0x4af9, \
       {0x89, 0x10, 0xf9, 0x3c, 0x55, 0xe6, 0x62, 0xec}}
 #define NS_AUDIOMANAGER_CONTRACTID "@mozilla.org/telephony/audiomanager;1"
 
+
 namespace mozilla {
+namespace hal {
+class SwitchEvent;
+typedef Observer<SwitchEvent> SwitchObserver;
+} 
+
 namespace dom {
 namespace gonk {
 
@@ -56,14 +63,16 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIAUDIOMANAGER
 
-  AudioManager() : mPhoneState(PHONE_STATE_CURRENT)
-  {
-  }
+  AudioManager();
+  ~AudioManager();
 
+  static void SetAudioRoute(int aRoutes);
 protected:
   PRInt32 mPhoneState;
-};
 
+private:
+  nsAutoPtr<mozilla::hal::SwitchObserver> mObserver;
+};
 
 } 
 } 
