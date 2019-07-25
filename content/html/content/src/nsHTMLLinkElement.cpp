491e5cc9f7848a36960fa216139520dfda9e302c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:28:57 +0000

    Bug 667518 - Don't ignore <link> href white-space only values.  r=bzbarsky
    UltraBlame original commit: f0d8fa54fecb05f6e4773b4fcca7e422a986565b

--- content/html/content/src/nsHTMLLinkElement.cpp
+++ content/html/content/src/nsHTMLLinkElement.cpp
@@ -397,7 +397,6 @@ nsHTMLLinkElement::GetStyleSheetURL(PRBool* aIsInline)
   *aIsInline = PR_FALSE;
   nsAutoString href;
   GetAttr(kNameSpaceID_None, nsGkAtoms::href, href);
-  href.Trim(" \t\n\r\f"); 
   if (href.IsEmpty()) {
     return nsnull;
   }
