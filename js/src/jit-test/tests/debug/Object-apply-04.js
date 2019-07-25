121e7fe3ff23605f19489a128deec0117a2148c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:37:25 +0000

    Passing test: .apply works on function proxies.
    UltraBlame original commit: 18e81fe5abac6810c744012e1236b41a5b83dbbd

---
+++ js/src/jit-test/tests/debug/Object-apply-04.js
@@ -0,0 +1,17 @@
+
+
+
+var g = newGlobal('new-compartment');
+g.eval("function f() { debugger; }");
+var dbg = Debug(g);
+var hits = 0;
+dbg.hooks = {
+    debuggerHandler: function (frame) {
+        var proxy = frame.arguments[0];
+        assertEq(proxy.name, null);
+        assertEq(proxy.apply(null, [33]).return, 34);
+        hits++;
+    }
+};
+g.eval("f(Proxy.createFunction({}, function (arg) { return arg + 1; }));");
+assertEq(hits, 1);
