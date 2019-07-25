c81e25db79e084d42f4aa34a521bd94533cde3e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:26:01 +0000

    Bug 764249 - Fix the non-reentrant-closure fix in bug 762473 (r=dvander)
    UltraBlame original commit: 925c31734424a32901febd7fed5aecac6f4f6471

--- js/src/vm/ScopeObject.cpp
+++ js/src/vm/ScopeObject.cpp
@@ -191,10 +191,10 @@ CallObject::copyUnaliasedValues(StackFrame *fp)
     
     for (unsigned i = 0; i < script->bindings.numArgs(); ++i) {
         if (!script->formalLivesInCallObject(i)) {
-            if (script->argsObjAliasesFormals())
+            if (script->argsObjAliasesFormals() && fp->hasArgsObj())
                 setArg(i, fp->argsObj().arg(i), DONT_CHECK_ALIASING);
             else
-                setArg(i, fp->unaliasedFormal(i), DONT_CHECK_ALIASING);
+                setArg(i, fp->unaliasedFormal(i, DONT_CHECK_ALIASING), DONT_CHECK_ALIASING);
         }
     }
 
@@ -1162,16 +1162,16 @@ class DebugScopeProxy : public BaseProxyHandler
                     return false;
 
                 if (maybefp) {
-                    if (script->argsObjAliasesFormals()) {
+                    if (script->argsObjAliasesFormals() && maybefp->hasArgsObj()) {
                         if (action == GET)
                             *vp = maybefp->argsObj().arg(i);
                         else
                             maybefp->argsObj().setArg(i, *vp);
                     } else {
                         if (action == GET)
-                            *vp = maybefp->unaliasedFormal(i);
+                            *vp = maybefp->unaliasedFormal(i, DONT_CHECK_ALIASING);
                         else
-                            maybefp->unaliasedFormal(i) = *vp;
+                            maybefp->unaliasedFormal(i, DONT_CHECK_ALIASING) = *vp;
                     }
                 } else {
                     if (action == GET)
