0be35c83b07a2c057945f8b87e951c2836cc7430
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:36:32 +0000

    Bug 570620, part k: Implement the compositor side of PRenderFrame, which grafts a shadow layer tree into the compositor's tree. r=mats sr=roc
    UltraBlame original commit: 9fb8965e9a5a65373511628a827593213251f562

---
+++ layout/ipc/RenderFrameParent.cpp
@@ -0,0 +1,283 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "mozilla/layers/ShadowLayersParent.h"
+
+#include "BasicLayers.h"
+#include "RenderFrameParent.h"
+
+#include "gfx3DMatrix.h"
+#include "nsFrameLoader.h"
+#include "nsViewportFrame.h"
+
+using namespace mozilla::layers;
+
+namespace mozilla {
+namespace layout {
+
+static void
+AssertInTopLevelChromeDoc(ContainerLayer* aContainer,
+                          nsIFrame* aContainedFrame)
+{
+  NS_ASSERTION(
+    (aContainer->Manager()->GetBackendType() != LayerManager::LAYERS_BASIC) ||
+    (aContainedFrame->GetNearestWidget() ==
+     static_cast<BasicLayerManager*>(aContainer->Manager())->GetRetainerWidget()),
+    "Expected frame to be in top-level chrome document");
+}
+
+
+
+
+static void
+SetTransformFor(ContainerLayer* aContainer, nsIFrame* aContainedFrame,
+                nsDisplayListBuilder* aBuilder)
+{
+  NS_ABORT_IF_FALSE(aContainer && aContainedFrame, "args must be nonnull");
+  AssertInTopLevelChromeDoc(aContainer, aContainedFrame);
+
+  
+  nsPoint offset = aBuilder->ToReferenceFrame(aContainedFrame->GetParent()) +
+                   aContainedFrame->GetContentRect().TopLeft();
+  nsIntPoint intOffset = offset.ToNearestPixels(
+    aContainedFrame->PresContext()->AppUnitsPerCSSPixel());
+
+  aContainer->SetTransform(gfx3DMatrix::Translation(intOffset.x, intOffset.y, 0));
+}
+
+static void
+AssertValidContainerOfShadowTree(ContainerLayer* aContainer,
+                                 Layer* aShadowRoot)
+{
+  NS_ABORT_IF_FALSE(
+    !aContainer || (aShadowRoot &&
+                    aShadowRoot == aContainer->GetFirstChild() &&
+                    nsnull == aShadowRoot->GetNextSibling()),
+    "container of shadow tree may only be null or have 1 child that is the shadow root");
+}
+
+static Layer*
+RootOf(ContainerLayer* aContainer)
+{
+  NS_ABORT_IF_FALSE(aContainer, "need a non-null container");
+
+  Layer* first = aContainer->GetFirstChild();
+  NS_ABORT_IF_FALSE(!first || nsnull == first->GetNextSibling(),
+                    "'root' container may only have 0 or 1 children");
+  return first;
+}
+
+
+
+
+
+static PRBool
+IsTempLayerManager(LayerManager* aManager)
+{
+  return (LayerManager::LAYERS_BASIC == aManager->GetBackendType() &&
+          !static_cast<BasicLayerManager*>(aManager)->IsRetained());
+}
+
+RenderFrameParent::RenderFrameParent(nsFrameLoader* aFrameLoader)
+  : mFrameLoader(aFrameLoader)
+{}
+
+RenderFrameParent::~RenderFrameParent()
+{}
+
+void
+RenderFrameParent::ShadowLayersUpdated()
+{
+  mFrameLoader->SetCurrentRemoteFrame(this);
+
+  nsIFrame* docFrame = mFrameLoader->GetPrimaryFrameOfOwningContent();
+  if (!docFrame) {
+    
+    
+    
+    
+    NS_WARNING("RenderFrameParent just received a layer update, but our <browser> doesn't have an nsIFrame so we can't invalidate for the update");
+    return;
+  }
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  nsRect rect = nsRect(nsPoint(0, 0), docFrame->GetRect().Size());
+  docFrame->InvalidateWithFlags(rect, nsIFrame::INVALIDATE_NO_THEBES_LAYERS);
+}
+
+already_AddRefed<Layer>
+RenderFrameParent::BuildLayer(nsDisplayListBuilder* aBuilder,
+                              nsIFrame* aFrame,
+                              LayerManager* aManager)
+{
+  NS_ABORT_IF_FALSE(aFrame,
+                    "makes no sense to have a shadow tree without a frame");
+  NS_ABORT_IF_FALSE(!mContainer ||
+                    IsTempLayerManager(aManager) ||
+                    mContainer->Manager() == aManager,
+                    "retaining manager changed out from under us ... HELP!");
+
+  if (mContainer && mContainer->Manager() != aManager) {
+    
+    
+    
+    
+    
+    
+    return nsnull;
+  }
+
+  Layer* containerRoot = mContainer ? RootOf(mContainer) : nsnull;
+  Layer* shadowRoot = GetRootLayer();
+  NS_ABORT_IF_FALSE(!shadowRoot || shadowRoot->Manager() == aManager,
+                    "retaining manager changed out from under us ... HELP!");
+
+  if (mContainer && shadowRoot != containerRoot) {
+    
+    
+    if (containerRoot) {
+      mContainer->RemoveChild(containerRoot);
+    }
+  }
+
+  if (!shadowRoot) {
+    
+    mContainer = nsnull;
+  } else if (shadowRoot != containerRoot) {
+    
+    if (!mContainer) {
+      mContainer = aManager->CreateContainerLayer();
+    }
+    NS_ABORT_IF_FALSE(!mContainer->GetFirstChild(),
+                      "container of shadow tree shouldn't have a 'root' here");
+
+    mContainer->InsertAfter(shadowRoot, nsnull);
+  }
+
+  if (mContainer) {
+    mContainer->SetClipRect(shadowRoot->GetClipRect());
+    mContainer->SetVisibleRegion(shadowRoot->GetVisibleRegion());
+    SetTransformFor(mContainer, aFrame, aBuilder);
+  }
+
+  AssertValidContainerOfShadowTree(mContainer, shadowRoot);
+  return nsRefPtr<Layer>(mContainer).forget();
+}
+
+void
+RenderFrameParent::ActorDestroy(ActorDestroyReason why)
+{
+  if (mFrameLoader->GetCurrentRemoteFrame() == this) {
+    
+    
+    
+    
+    
+    
+    mFrameLoader->SetCurrentRemoteFrame(nsnull);
+  }
+  mFrameLoader = nsnull;
+}
+
+PLayersParent*
+RenderFrameParent::AllocPLayers()
+{
+  LayerManager* lm = GetLayerManager();
+  if (LayerManager::LAYERS_BASIC != lm->GetBackendType()) {
+    NS_WARNING("shadow layers no sprechen GL backend yet");
+    return nsnull;
+  }    
+
+  BasicShadowLayerManager* bslm = static_cast<BasicShadowLayerManager*>(lm);
+  ShadowLayersParent* slp = new ShadowLayersParent(bslm);
+  bslm->SetForwarder(nsnull);   
+  bslm->SetForwarder(slp);
+  return slp;
+}
+
+bool
+RenderFrameParent::DeallocPLayers(PLayersParent* aLayers)
+{
+  delete aLayers;
+  return true;
+}
+
+LayerManager*
+RenderFrameParent::GetLayerManager() const
+{
+  nsIDocument* doc = mFrameLoader->GetOwnerDoc();
+  return doc->GetShell()->GetLayerManager();
+}
+
+ShadowLayersParent*
+RenderFrameParent::GetShadowLayers() const
+{
+  const nsTArray<PLayersParent*>& shadowParents = ManagedPLayersParent();
+  NS_ABORT_IF_FALSE(shadowParents.Length() <= 1,
+                    "can only support at most 1 ShadowLayersParent");
+  return (shadowParents.Length() == 1) ?
+    static_cast<ShadowLayersParent*>(shadowParents[0]) : nsnull;
+}
+
+Layer*
+RenderFrameParent::GetRootLayer() const
+{
+  ShadowLayersParent* shadowLayers = GetShadowLayers();
+  return shadowLayers ? shadowLayers->GetRoot() : nsnull;
+}
+
+}  
+}  
+
+already_AddRefed<Layer>
+nsDisplayRemote::BuildLayer(nsDisplayListBuilder* aBuilder,
+                            LayerManager* aManager)
+{
+  nsRefPtr<Layer> layer = mRemoteFrame->BuildLayer(aBuilder, mFrame, aManager);
+  return layer.forget();
+}
---
+++ layout/ipc/RenderFrameParent.h
@@ -0,0 +1,125 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_layout_RenderFrameParent_h
+#define mozilla_layout_RenderFrameParent_h
+
+#include "mozilla/layout/PRenderFrameParent.h"
+
+#include "nsDisplayList.h"
+
+class nsFrameLoader;
+
+namespace mozilla {
+namespace layers {
+class ContainerLayer;
+class Layer;
+class LayerManager;
+class ShadowLayersParent;
+}
+
+namespace layout {
+
+class RenderFrameParent : public PRenderFrameParent
+{
+  typedef mozilla::layers::ContainerLayer ContainerLayer;
+  typedef mozilla::layers::Layer Layer;
+  typedef mozilla::layers::LayerManager LayerManager;
+  typedef mozilla::layers::ShadowLayersParent ShadowLayersParent;
+
+public:
+  RenderFrameParent(nsFrameLoader* aFrameLoader);
+  virtual ~RenderFrameParent();
+
+  void ShadowLayersUpdated();
+
+  already_AddRefed<Layer> BuildLayer(nsDisplayListBuilder* aBuilder,
+                                     nsIFrame* aFrame,
+                                     LayerManager* aManager);
+
+protected:
+  NS_OVERRIDE void ActorDestroy(ActorDestroyReason why);
+
+  NS_OVERRIDE virtual PLayersParent* AllocPLayers();
+  NS_OVERRIDE virtual bool DeallocPLayers(PLayersParent* aLayers);
+
+private:
+  LayerManager* GetLayerManager() const;
+  ShadowLayersParent* GetShadowLayers() const;
+  Layer* GetRootLayer() const;
+
+  nsRefPtr<nsFrameLoader> mFrameLoader;
+  nsRefPtr<ContainerLayer> mContainer;
+};
+
+} 
+} 
+
+
+
+
+
+
+class nsDisplayRemote : public nsDisplayItem
+{
+  typedef mozilla::layout::RenderFrameParent RenderFrameParent;
+
+public:
+  nsDisplayRemote(nsIFrame* aFrame, RenderFrameParent* aRemoteFrame)
+    : nsDisplayItem(aFrame)
+    , mRemoteFrame(aRemoteFrame)
+  {}
+
+  NS_OVERRIDE
+  virtual LayerState GetLayerState(nsDisplayListBuilder* aBuilder,
+                                   LayerManager* aManager)
+  { return mozilla::LAYER_ACTIVE; }  
+
+  NS_OVERRIDE
+  virtual already_AddRefed<Layer>
+  BuildLayer(nsDisplayListBuilder* aBuilder, LayerManager* aManager);
+
+  NS_DISPLAY_DECL_NAME("Remote", TYPE_REMOTE)
+
+private:
+  RenderFrameParent* mRemoteFrame;
+};
+
+#endif  
