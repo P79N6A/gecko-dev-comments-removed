96191e2d7c9fa981be23feb3835b92efc546853f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:46:49 +0000

    Bug 721564 - Get module information for the profiler on Windows; r=BenWa
    UltraBlame original commit: 347bd64309233167c13bcb2c44b33db86a98a97a

--- tools/profiler/sps/shared-libraries-win32.cc
+++ tools/profiler/sps/shared-libraries-win32.cc
@@ -40,20 +40,25 @@
 #include <tlhelp32.h>
 
 #include "shared-libraries.h"
+#include "nsWindowsHelpers.h"
 
 SharedLibraryInfo SharedLibraryInfo::GetInfoForSelf()
 {
   SharedLibraryInfo sharedLibraryInfo;
 
-  
+  nsAutoHandle snap(CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, GetCurrentProcessId()));
 
-
-
-
-
-
-
-
+  MODULEENTRY32 module = {0};
+  module.dwSize = sizeof(MODULEENTRY32);
+  if (Module32First(snap, &module)) {
+    do {
+      SharedLibrary shlib((uintptr_t)module.modBaseAddr,
+                          (uintptr_t)module.modBaseAddr+module.modBaseSize,
+                          0, 
+                          module.szModule);
+      sharedLibraryInfo.AddSharedLibrary(shlib);
+    } while (Module32Next(snap, &module));
+  }
 
   return sharedLibraryInfo;
 }
