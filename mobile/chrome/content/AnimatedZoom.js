efb90307bdb323d1c6a7e59769721f1c9d3c4303
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:04:32 +0000

    Bug 628827 Blinky double-tap zoom and pinch to zoom r=mbrubeck
    UltraBlame original commit: 20e8529244f5a7c58a331829ae53a9543094e94d

--- mobile/chrome/content/AnimatedZoom.js
+++ mobile/chrome/content/AnimatedZoom.js
@@ -89,9 +89,11 @@ const AnimatedZoom = {
     let zoomRatio = window.innerWidth / nextRect.width;
     let zoomLevel = browser.scale * zoomRatio;
 
+    
+    
     let contentView = browser.getRootView();
     contentView.setScale(zoomLevel);
-    contentView.scrollTo(nextRect.left * zoomRatio, nextRect.top * zoomRatio);
+    contentView._contentView.scrollTo(nextRect.left * zoomRatio, nextRect.top * zoomRatio);
 
     this.zoomRect = nextRect;
   },
