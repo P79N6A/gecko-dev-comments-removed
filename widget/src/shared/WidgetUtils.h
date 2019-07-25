592fc00cc89702253ddba95c96c60e883441483d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:57:43 +0000

    Bug 616664 - nsIScreen interface for keeping the screen on. r=blassey sr=roc a=blocking-fennec
    UltraBlame original commit: d488f09f6f8d7652107ec264d51f7b04816afffa

--- widget/src/shared/WidgetUtils.h
+++ widget/src/shared/WidgetUtils.h
@@ -45,6 +45,7 @@
 #include "nsIWidget.h"
 #include "nsPIDOMWindow.h"
 #include "nsIDOMWindow.h"
+#include "nsIScreen.h"
 
 namespace mozilla {
 namespace widget {
@@ -58,6 +59,46 @@ public:
 
 
   static already_AddRefed<nsIWidget> DOMWindowToWidget(nsIDOMWindow *aDOMWindow);
+};
+
+
+
+
+
+class BrightnessLockingWidget : public nsIScreen_MOZILLA_2_0_BRANCH
+{
+public:
+  BrightnessLockingWidget();
+
+  NS_IMETHOD LockMinimumBrightness(PRUint32 aBrightness);
+  NS_IMETHOD UnlockMinimumBrightness(PRUint32 aBrightness);
+
+protected:
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  virtual void ApplyMinimumBrightness(PRUint32 aBrightness) = 0;
+
+  
+
+
+
+  void CheckMinimumBrightness();
+
+  PRUint32 mBrightnessLocks[nsIScreen_MOZILLA_2_0_BRANCH::BRIGHTNESS_LEVELS];
 };
 
 } 
