ad758e7a7f4e4c52ad17226812958c5750f2950b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:32:45 +0000

    Back out efd165428f08 to 4a79fefefa28 (bug 580786, bug 716639, bug 692879, bug 717921) because of reftest error
    UltraBlame original commit: 52f52eb53549961ef0e86a3b2504dacebb1cfc5a

--- gfx/2d/SourceSurfaceCG.h
+++ gfx/2d/SourceSurfaceCG.h
@@ -47,11 +47,10 @@ namespace gfx {
 class SourceSurfaceCG : public SourceSurface
 {
 public:
-  SourceSurfaceCG() {}
-  SourceSurfaceCG(CGImageRef aImage) : mImage(aImage) {}
+  SourceSurfaceCG();
   ~SourceSurfaceCG();
 
-  virtual SurfaceType GetType() const { return SURFACE_COREGRAPHICS_IMAGE; }
+  virtual SurfaceType GetType() const { return COREGRAPHICS_IMAGE; }
   virtual IntSize GetSize() const;
   virtual SurfaceFormat GetFormat() const;
   virtual TemporaryRef<DataSourceSurface> GetDataSurface();
@@ -70,39 +69,6 @@ private:
 
 
   SurfaceFormat mFormat;
-};
-
-class DataSourceSurfaceCG : public DataSourceSurface
-{
-public:
-  DataSourceSurfaceCG() {}
-  DataSourceSurfaceCG(CGImageRef aImage);
-  ~DataSourceSurfaceCG();
-
-  virtual SurfaceType GetType() const { return SURFACE_DATA; }
-  virtual IntSize GetSize() const;
-  virtual SurfaceFormat GetFormat() const { return FORMAT_B8G8R8A8; }
-
-  CGImageRef GetImage() { return mImage; }
-
-  bool InitFromData(unsigned char *aData,
-                    const IntSize &aSize,
-                    int32_t aStride,
-                    SurfaceFormat aFormat);
-
-  virtual unsigned char *GetData();
-
-  virtual int32_t Stride() { return CGImageGetBytesPerRow(mImage); }
-
-
-private:
-  CGContextRef mCg;
-  CGImageRef mImage;
-  
-  void *mData;
-  
-
-
 };
 
 }
