8f76846f0c6931121ef1a6e4dfa26ef203789f25
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:00:13 +0000

    Bug 618335: normalize old-style 20-char sync keys. r=philiKON
    UltraBlame original commit: 7ca1b3733b30ea3947faead559409d97f8491512

---
+++ services/sync/tests/unit/test_upgrade_old_sync_key.js
@@ -0,0 +1,43 @@
+Cu.import("resource://services-sync/constants.js");
+Cu.import("resource://services-sync/main.js");
+var btoa = Cu.import("resource://services-sync/util.js").btoa;
+
+
+function run_test() {
+  const PBKDF2_KEY_BYTES = 16;
+  initTestLogging("Trace");
+
+  let passphrase = "abcde-abcde-abcde-abcde";
+  do_check_false(Utils.isPassphrase(passphrase));
+
+  let normalized = Utils.normalizePassphrase(passphrase);
+  _("Normalized: " + normalized);
+
+  
+  do_check_false(Utils.isPassphrase(normalized));
+
+  
+  do_check_neq(normalized, passphrase);
+  do_check_eq(normalized, "abcdeabcdeabcdeabcde");
+
+  
+  Weave.Service.syncID = "1234567890";
+  Weave.Service.passphrase = normalized;     
+  do_check_false(Utils.isPassphrase(Weave.Service.passphrase));
+  Weave.Service.upgradeSyncKey(Weave.Service.syncID);
+  let upgraded = Weave.Service.passphrase;
+  _("Upgraded: " + upgraded);
+  do_check_true(Utils.isPassphrase(upgraded));
+
+  
+  
+  _("Sync ID: " + Weave.Service.syncID);
+  let derivedKeyStr =
+    Utils.derivePresentableKeyFromPassphrase(normalized,
+                                             btoa(Weave.Service.syncID),
+                                             PBKDF2_KEY_BYTES, true);
+  _("Derived: " + derivedKeyStr);
+
+  
+  do_check_eq(derivedKeyStr, upgraded);
+}
