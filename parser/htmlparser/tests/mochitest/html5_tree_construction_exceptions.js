924699a9fd92edc8457e905566dac3401a0c9428
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:28:27 +0000

    Bug 612530 - Sync html5lib tree builder tests into m-c. r=jgriffin, a=test-only.
    UltraBlame original commit: c08726dd68fc0da23408a312f3ff36625655a6e0

--- parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
+++ parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
@@ -8,4 +8,13 @@ var html5Exceptions = {
   "<!doctype html><keygen><frameset>" : true, 
   "<select><keygen>" : true, 
   "<math><mi><div><object><div><span></span></div></object></div></mi><mi>" : true, 
+  "<plaintext>\u0000filler\u0000text\u0000" : true, 
+  "<body><svg><foreignObject>\u0000filler\u0000text" : true, 
+  "<svg>\u0000</svg><frameset>" : true, 
+  "<svg>\u0000 </svg><frameset>" : true, 
+  "<option><span><option>" : true, 
+  "<math><annotation-xml encoding=\"application/xhtml+xml\"><div>" : true, 
+  "<math><annotation-xml encoding=\"aPPlication/xhtmL+xMl\"><div>" : true, 
+  "<math><annotation-xml encoding=\"text/html\"><div>" : true, 
+  "<math><annotation-xml encoding=\"Text/htmL\"><div>" : true, 
 }
