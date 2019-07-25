6f42ac62681edb2032ccfcc02f27b69f9c26cee6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:00:47 +0000

    Bug 735230 - Part 2: Add compositor pause/resume events to Gecko. r=kats
    UltraBlame original commit: 5303cc81321448cd5cea7b0dc60cf5a5a4bdc9ae

--- widget/android/nsWindow.cpp
+++ widget/android/nsWindow.cpp
@@ -214,9 +214,9 @@ nsWindow::~nsWindow()
         mRootAccessible = nsnull;
 #endif
     ALOG("nsWindow %p destructor", (void*)this);
-
-    AndroidBridge::Bridge()->SetCompositorParent(NULL, NULL);
-
+#ifdef MOZ_JAVA_COMPOSITOR
+    SetCompositor(NULL, NULL, NULL);
+#endif
 }
 
 bool
@@ -772,21 +772,21 @@ nsWindow::GetLayerManager(PLayersChild*, LayersBackend, LayerManagerPersistence,
         mLayerManager = CreateBasicLayerManager();
         return mLayerManager;
     }
-
+#ifdef MOZ_JAVA_COMPOSITOR
     bool useCompositor =
         Preferences::GetBool("layers.offmainthreadcomposition.enabled", false);
 
     if (useCompositor) {
         CreateCompositor();
         if (mLayerManager) {
-            AndroidBridge::Bridge()->SetCompositorParent(mCompositorParent, mCompositorThread);
+            SetCompositor(mCompositorParent, mCompositorChild, mCompositorThread);
             return mLayerManager;
         }
 
         
         sFailedToCreateGLContext = true;
     }
-
+#endif
     mUseAcceleratedRendering = GetShouldAccelerate();
 
     if (!mUseAcceleratedRendering ||
@@ -983,6 +983,31 @@ nsWindow::OnGlobalAndroidEvent(AndroidGeckoEvent *ae)
             sValidSurface = false;
             break;
 
+#ifdef MOZ_JAVA_COMPOSITOR
+        case AndroidGeckoEvent::COMPOSITOR_PAUSE:
+            
+            
+            
+            
+            if (sCompositorChild) {
+                sCompositorChild->SendPause();
+            }
+            sCompositorPaused = true;
+            break;
+
+        case AndroidGeckoEvent::COMPOSITOR_RESUME:
+            
+            
+            
+            
+            
+            
+            
+            sCompositorPaused = false;
+            win->RedrawAll();
+            break;
+#endif
+
         case AndroidGeckoEvent::GECKO_EVENT_SYNC:
             AndroidBridge::Bridge()->AcknowledgeEventSync();
             break;
@@ -1136,7 +1161,7 @@ nsWindow::OnDraw(AndroidGeckoEvent *ae)
     AndroidBridge::AutoLocalJNIFrame jniFrame;
 #ifdef MOZ_JAVA_COMPOSITOR
     
-    if (gAndroidBounds.width <= 0 || gAndroidBounds.height <= 0) {
+    if (sCompositorPaused || gAndroidBounds.width <= 0 || gAndroidBounds.height <= 0) {
         return;
     }
 
@@ -2275,13 +2300,17 @@ nsWindow::DrawWindowOverlay(LayerManager* aManager, nsIntRect aRect) {
 
 
 nsRefPtr<mozilla::layers::CompositorParent> nsWindow::sCompositorParent = 0;
+nsRefPtr<mozilla::layers::CompositorChild> nsWindow::sCompositorChild = 0;
 base::Thread * nsWindow::sCompositorThread = 0;
+bool nsWindow::sCompositorPaused = false;
 
 void
-nsWindow::SetCompositorParent(mozilla::layers::CompositorParent* aCompositorParent,
-                              ::base::Thread* aCompositorThread)
+nsWindow::SetCompositor(mozilla::layers::CompositorParent* aCompositorParent,
+                        mozilla::layers::CompositorChild* aCompositorChild,
+                        ::base::Thread* aCompositorThread)
 {
     sCompositorParent = aCompositorParent;
+    sCompositorChild = aCompositorChild;
     sCompositorThread = aCompositorThread;
 }
 
