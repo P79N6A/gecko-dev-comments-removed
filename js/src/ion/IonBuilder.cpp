d961dea4435d65d17504361fc2be98f6604fe1ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:53:45 +0000

    Mark inlined boxed JSFunction as constant. (Bug 729899, r=dvander)
    UltraBlame original commit: 150159ee5c2699df0e58a825b0c5fecbaa804513

--- js/src/ion/IonBuilder.cpp
+++ js/src/ion/IonBuilder.cpp
@@ -2314,6 +2314,13 @@ IonBuilder::jsop_call_inline(uint32 argc, IonBuilder &inlineBuilder)
     if (!discardCallArgs(argc, argv, top))
         return false;
 
+    
+    
+    MConstant *constFun = MConstant::New(ObjectValue(*data->callee));
+    MDefinition *loadFun = current->pop();
+    current->push(constFun);
+    loadFun->replaceAllUsesWith(constFun);
+
     MDefinition *thisDefn = argv[0];
 
     
