6cbc3e5673f8711a858df4aff4ed0b427b3ad396
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:20:29 +0000

    + We're now storing data in a file named after your candy, in a folder named 'tabcandy' inside your profile folder (e.g. tabcandy/revision-a.json). Currently the only data that's round-tripped is whether you have hidden the tab bar, but we are saving group data out (just not reading it yet). + The nav bar wasn't successfully hiding when first starting the browser; fixed.
    UltraBlame original commit: 17c59ee826a8244bf2f047c0c97d151baccc1acc

--- browser/base/content/tabcandy/app/storage.js
+++ browser/base/content/tabcandy/app/storage.js
@@ -1,12 +1,55 @@
 
 Storage = {
+  
   init: function() {
+  },
+  
+  
+  read: function() {
+    var data = {};
+    var file = this.getFile();
+    if(file.exists()) {
+      var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].  
+                              createInstance(Components.interfaces.nsIFileInputStream);  
+      var cstream = Components.classes["@mozilla.org/intl/converter-input-stream;1"].  
+                              createInstance(Components.interfaces.nsIConverterInputStream);  
+      fstream.init(file, -1, 0, 0);  
+      cstream.init(fstream, "UTF-8", 0, 0); 
+      
+      let (str = {}) {  
+        cstream.readString(-1, str); 
+        if(str.value)
+          data = JSON.parse(str.value);  
+      }  
+      cstream.close(); 
+    }
+   
+    return data;
+  },
+  
+  
+  write: function(data) {
+    var file = this.getFile();
+    var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].  
+                             createInstance(Components.interfaces.nsIFileOutputStream);  
+    foStream.init(file, 0x02 | 0x08 | 0x20, 0666, 0);   
+    var str = JSON.stringify(data);
+    foStream.write(str, str.length);
+    foStream.close();
+  },
+  
+  
+  getFile: function() {
     var file = Components.classes["@mozilla.org/file/directory_service;1"].  
       getService(Components.interfaces.nsIProperties).  
       get("ProfD", Components.interfaces.nsIFile);  
-
-
-    Utils.log(file); 
+      
+    file.append('tabcandy');
+    if(!file.exists())
+      file.create(Components.interfaces.nsIFile.DIRECTORY_TYPE, 0777);        
+      
+    file.append(Switch.name + '.json');
+    return file;
   }
 };
 
