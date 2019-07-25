44d4075f365af06d42ed6fbed8c97a482328cd20
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:50:27 +0000

    Discard jitcode when changing chunk limit in JS shell, bug 722598. r=dvander
    UltraBlame original commit: 65872ea859eb120aa75b62c57a5915556111e8f5

--- js/src/shell/js.cpp
+++ js/src/shell/js.cpp
@@ -3885,6 +3885,10 @@ MJitChunkLimit(JSContext *cx, uintN argc, jsval *vp)
     mjit::SetChunkLimit((uint32_t) t);
 #endif
 
+    
+    
+    JS_GC(cx);
+
     vp->setUndefined();
     return true;
 }
