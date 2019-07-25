381158c8a1864222dfac1a901b7e34dbb316d7c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:35:36 +0000

    Backout b69f5004fde8 (Bug 777776) for bustage
    UltraBlame original commit: c394a354eef7dbc32fde1fbe0be18cb592bc67dd

--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -437,8 +437,9 @@ struct JSScript : public js::gc::Cell
     
     js::types::TypeScript *types;
 
+    js::ScriptSource *source; 
+
   private:
-    js::ScriptSource *scriptSource_; 
 #ifdef JS_METHODJIT
     JITScriptSet *mJITInfo;
 #endif
@@ -619,14 +620,6 @@ struct JSScript : public js::gc::Cell
     JSFixedString *sourceData(JSContext *cx);
 
     bool loadSource(JSContext *cx, bool *worked);
-
-    js::ScriptSource *scriptSource() {
-        return scriptSource_;
-    }
-
-    void setScriptSource(JSContext *cx, js::ScriptSource *ss);
-
-  public:
 
     
     bool isForEval() { return isCachedEval || isActiveEval; }
