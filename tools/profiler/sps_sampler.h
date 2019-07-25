96815734fbf3d6c555e178c400fea9c3f4d50eab
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:21:36 +0000

    Bug 759532 - [Backout] Expose Enter/ExitProfileLabel to JS. r=ehsan
    UltraBlame original commit: 466d337a1996ec990488c6bf89c78a715d3b109a

--- tools/profiler/sps_sampler.h
+++ tools/profiler/sps_sampler.h
@@ -277,25 +277,6 @@ inline void mozilla_sampler_call_exit(void *aHandle)
   stack->pop();
 }
 
-inline void mozilla_sampler_call_exit_no_handle()
-{
-  
-  
-  if (!stack_key_initialized)
-    return;
-
-  ProfileStack *stack = tlsStack.get();
-  
-  
-  
-  
-  if (!stack) {
-    return;
-  }
-
-  stack->pop();
-}
-
 inline void mozilla_sampler_add_marker(const char *aMarker)
 {
   ProfileStack *stack = tlsStack.get();
