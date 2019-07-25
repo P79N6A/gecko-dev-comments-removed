a1614791a118928c4f914ed905df2ec77900c646
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:27:46 +0000

    Bug 758354 Stop unnecessarily preprocessing chrome r=dolske
    UltraBlame original commit: 1718e78e0cb0f3e532f4e2e33309574f25fc91f1

--- toolkit/mozapps/extensions/content/list.js
+++ toolkit/mozapps/extensions/content/list.js
@@ -1,7 +1,8 @@
-# -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http:
+
+
+
+
+
 
 const kXULNS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
 const kDialog = "dialog";
