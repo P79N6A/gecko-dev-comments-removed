d5a9ecfb8938f3283ed6a087d4f4bbadf2825254
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:10:40 +0000

    Bug 639842 and Bug 645407 - implement GfxInfoX11, using the info from the glxtest probe - r=bsmedberg,joe,karl * * * try: -b do -p linux,linux64,android-r7,maemo5-gtk,maemo5-qt -u mochitest-1 -t none
    UltraBlame original commit: 3d3e242392511d5e6b917979edd51b785ba19bae

---
+++ widget/src/xpwidgets/GfxInfoX11.h
@@ -0,0 +1,89 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef __GfxInfoX11_h__
+#define __GfxInfoX11_h__
+
+#include "GfxInfoBase.h"
+
+namespace mozilla {
+namespace widget {
+
+class GfxInfo : public GfxInfoBase
+{
+public:
+
+  
+  
+  NS_SCRIPTABLE NS_IMETHOD GetD2DEnabled(PRBool *aD2DEnabled);
+  NS_SCRIPTABLE NS_IMETHOD GetDWriteEnabled(PRBool *aDWriteEnabled);
+  NS_SCRIPTABLE NS_IMETHOD GetDWriteVersion(nsAString & aDwriteVersion);
+  NS_SCRIPTABLE NS_IMETHOD GetAdapterDescription(nsAString & aAdapterDescription);
+  NS_SCRIPTABLE NS_IMETHOD GetAdapterDriver(nsAString & aAdapterDriver);
+  NS_SCRIPTABLE NS_IMETHOD GetAdapterVendorID(PRUint32 *aAdapterVendorID);
+  NS_SCRIPTABLE NS_IMETHOD GetAdapterDeviceID(PRUint32 *aAdapterDeviceID);
+  NS_SCRIPTABLE NS_IMETHOD GetAdapterRAM(nsAString & aAdapterRAM);
+  NS_SCRIPTABLE NS_IMETHOD GetAdapterDriverVersion(nsAString & aAdapterDriverVersion);
+  NS_SCRIPTABLE NS_IMETHOD GetAdapterDriverDate(nsAString & aAdapterDriverDate);
+  using GfxInfoBase::GetFeatureStatus;
+  using GfxInfoBase::GetFeatureSuggestedDriverVersion;
+  using GfxInfoBase::GetWebGLParameter;
+
+  virtual nsresult Init();
+
+protected:
+
+  virtual nsresult GetFeatureStatusImpl(PRInt32 aFeature, PRInt32 *aStatus, nsAString & aSuggestedDriverVersion, GfxDriverInfo* aDriverInfo = nsnull);
+
+private:
+  nsCString mVendor;
+  nsCString mRenderer;
+  nsCString mVersion;
+  nsCString mAdapterDescription;
+  bool mIsMesa, mIsNVIDIA, mIsFGLRX;
+  int mMajorVersion, mMinorVersion;
+
+  void AddCrashReportAnnotations();
+  void GetData();
+};
+
+} 
+} 
+
+#endif 
