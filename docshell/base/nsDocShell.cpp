ebb31d3fb5fbfcd12daab94f8ec58266b34e3ff6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:53:20 +0000

    Bug 580819 - Don't try to "replace" the transient about:blank content viewer when loading about:blank immediately after it. r=smaug
    UltraBlame original commit: 50ea31d46c0ee86ed555f19ff633ab9ccf222d3a

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -8997,6 +8997,10 @@ nsDocShell::OnNewURI(nsIURI * aURI, nsIChannel * aChannel, nsISupports* aOwner,
     PR_LOG(gDocShellLog, PR_LOG_DEBUG,
            ("  shAvailable=%i updateHistory=%i equalURI=%i\n",
             shAvailable, updateHistory, equalUri));
+
+    if (mCurrentURI && !mOSHE) {
+        NS_ASSERTION(IsAboutBlank(mCurrentURI), "no SHEntry for a non-transient viewer?");
+    }
 #endif
 
     
@@ -9011,7 +9015,13 @@ nsDocShell::OnNewURI(nsIURI * aURI, nsIChannel * aChannel, nsISupports* aOwner,
 
 
 
+
+
+
+
+
     if (equalUri &&
+        mOSHE &&
         (mLoadType == LOAD_NORMAL ||
          mLoadType == LOAD_LINK ||
          mLoadType == LOAD_STOP_CONTENT) &&
