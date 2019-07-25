01f6d7e8b4cd5e5ceb84a77ffe27af553a054364
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:36:45 +0000

    Bug 614323 - ARM: prevent constant pool from being dumped in the middle of MICs r=dvander a=blocking-fennec
    UltraBlame original commit: 50795657150eb86e7f1af47102fc516ef7c16ba3

--- js/src/methodjit/BaseCompiler.h
+++ js/src/methodjit/BaseCompiler.h
@@ -192,6 +192,50 @@ class Repatcher : public JSC::RepatchBuffer
     { }
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef JS_CPU_ARM
+class AutoReserveICSpace {
+    typedef Assembler::Label Label;
+    static const size_t reservedSpace = 64;
+
+    Assembler           &masm;
+#ifdef DEBUG
+    Label               startLabel;
+#endif
+
+  public:
+    AutoReserveICSpace(Assembler &masm) : masm(masm) {
+        masm.ensureSpace(reservedSpace);
+#ifdef DEBUG
+        startLabel = masm.label();
+#endif
+    }
+
+    ~AutoReserveICSpace() {
+#ifdef DEBUG
+        Label endLabel = masm.label();
+        int spaceUsed = masm.differenceBetween(startLabel, endLabel);
+        JS_ASSERT(spaceUsed >= 0);
+        JS_ASSERT(size_t(spaceUsed) <= reservedSpace);
+#endif
+    }
+};
+# define RESERVE_IC_SPACE(__masm) AutoReserveICSpace arics(__masm)
+#else
+# define RESERVE_IC_SPACE(__masm)
+#endif
+
 } 
 } 
 
