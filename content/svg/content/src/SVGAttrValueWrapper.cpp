6fde7a6ed00cc972a759d2cd9cb3be38b727025a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:51:11 +0000

    Bug 629200 part 24 - Add wrapper for SVG types used in nsAttrValue so we don't need to export them; r=jwatt
    UltraBlame original commit: ffb8f5c16fb6c4bd910b819e8461e9f791182167

---
+++ content/svg/content/src/SVGAttrValueWrapper.cpp
@@ -0,0 +1,125 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "SVGAttrValueWrapper.h"
+#include "nsSVGAngle.h"
+#include "nsSVGIntegerPair.h"
+#include "nsSVGLength2.h"
+#include "nsSVGNumberPair.h"
+#include "nsSVGViewBox.h"
+#include "SVGAnimatedPreserveAspectRatio.h"
+#include "SVGLengthList.h"
+#include "SVGNumberList.h"
+#include "SVGPathData.h"
+#include "SVGPointList.h"
+#include "SVGTransformList.h"
+
+using namespace mozilla;
+
+ void
+SVGAttrValueWrapper::ToString(const nsSVGAngle* aAngle, nsAString& aResult)
+{
+  aAngle->GetBaseValueString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(const nsSVGIntegerPair* aIntegerPair,
+                              nsAString& aResult)
+{
+  aIntegerPair->GetBaseValueString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(const nsSVGLength2* aLength, nsAString& aResult)
+{
+  aLength->GetBaseValueString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(const SVGLengthList* aLengthList,
+                              nsAString& aResult)
+{
+  aLengthList->GetValueAsString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(const SVGNumberList* aNumberList,
+                              nsAString& aResult)
+{
+  aNumberList->GetValueAsString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(const nsSVGNumberPair* aNumberPair,
+                              nsAString& aResult)
+{
+  aNumberPair->GetBaseValueString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(const SVGPathData* aPathData, nsAString& aResult)
+{
+  aPathData->GetValueAsString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(const SVGPointList* aPointList,
+                              nsAString& aResult)
+{
+  aPointList->GetValueAsString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(
+  const SVGAnimatedPreserveAspectRatio* aPreserveAspectRatio,
+  nsAString& aResult)
+{
+  aPreserveAspectRatio->GetBaseValueString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(const SVGTransformList* aTransformList,
+                              nsAString& aResult)
+{
+  aTransformList->GetValueAsString(aResult);
+}
+
+ void
+SVGAttrValueWrapper::ToString(const nsSVGViewBox* aViewBox, nsAString& aResult)
+{
+  aViewBox->GetBaseValueString(aResult);
+}
---
+++ content/svg/content/src/SVGAttrValueWrapper.h
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef MOZILLA_SVGATTRVALUEWRAPPER_H__
+#define MOZILLA_SVGATTRVALUEWRAPPER_H__
+
+
+
+
+
+
+#include "nsString.h"
+
+class nsSVGAngle;
+class nsSVGIntegerPair;
+class nsSVGLength2;
+class nsSVGNumberPair;
+class nsSVGViewBox;
+
+namespace mozilla {
+class SVGLengthList;
+class SVGNumberList;
+class SVGPathData;
+class SVGPointList;
+class SVGAnimatedPreserveAspectRatio;
+class SVGTransformList;
+}
+
+namespace mozilla {
+
+class SVGAttrValueWrapper
+{
+public:
+  static void ToString(const nsSVGAngle* aAngle, nsAString& aResult);
+  static void ToString(const nsSVGIntegerPair* aIntegerPair,
+                       nsAString& aResult);
+  static void ToString(const nsSVGLength2* aLength, nsAString& aResult);
+  static void ToString(const mozilla::SVGLengthList* aLengthList,
+                       nsAString& aResult);
+  static void ToString(const mozilla::SVGNumberList* aNumberList,
+                       nsAString& aResult);
+  static void ToString(const nsSVGNumberPair* aNumberPair, nsAString& aResult);
+  static void ToString(const mozilla::SVGPathData* aPathData,
+                       nsAString& aResult);
+  static void ToString(const mozilla::SVGPointList* aPointList,
+                       nsAString& aResult);
+  static void ToString(
+    const mozilla::SVGAnimatedPreserveAspectRatio* aPreserveAspectRatio,
+    nsAString& aResult);
+  static void ToString(const mozilla::SVGTransformList* aTransformList,
+                       nsAString& aResult);
+  static void ToString(const nsSVGViewBox* aViewBox, nsAString& aResult);
+};
+
+} 
+
+#endif 
