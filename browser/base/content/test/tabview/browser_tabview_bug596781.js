05e7f1e8ded91fffc71281c40c99e0c4ce8d7613
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:35:43 +0000

    Bug 596781 - Don't allow the last group to close if there are app tabs [r=ian, a=beltzner]
    UltraBlame original commit: 041430229f5ea29a136f496206d026d09f6ac348

---
+++ browser/base/content/test/tabview/browser_tabview_bug596781.js
@@ -0,0 +1,75 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let newTab;
+
+function test() {
+  waitForExplicitFinish();
+
+  newTab = gBrowser.addTab();
+  gBrowser.pinTab(newTab);
+
+  window.addEventListener("tabviewshown", onTabViewWindowLoaded, false);
+  TabView.toggle();
+}
+
+function onTabViewWindowLoaded() {
+  window.removeEventListener("tabviewshown", onTabViewWindowLoaded, false);
+  ok(TabView.isVisible(), "Tab View is visible");
+
+  let contentWindow = document.getElementById("tab-view").contentWindow;
+
+  is(contentWindow.GroupItems.groupItems.length, 1, "Only one group exists"); 
+  is(gBrowser.tabs.length, 2, "Only one tab exists");
+  ok(newTab.pinned, "The original tab is pinned");
+
+  let groupItem = contentWindow.GroupItems.groupItems[0];
+  is(groupItem.$closeButton[0].style.display, "none", 
+     "The close button is hidden");
+
+  gBrowser.unpinTab(newTab);
+  is(groupItem.$closeButton[0].style.display, "", 
+     "The close button is visible");
+
+  function onTabViewHidden() {
+    window.removeEventListener("tabviewhidden", onTabViewHidden, false);
+    gBrowser.removeTab(newTab);
+    finish();
+  }
+  window.addEventListener("tabviewhidden", onTabViewHidden, false);
+  TabView.toggle();
+}
