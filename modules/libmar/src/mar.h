74a2f223a26703f27b307a94c9a2d451cee6dde3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:10:48 +0000

    Bug 655039 - Remove WinCE code from libmar; r=rstrong
    UltraBlame original commit: 48ba96443bca93fa493a571a5b0530fa38271dc9

--- modules/libmar/src/mar.h
+++ modules/libmar/src/mar.h
@@ -40,13 +40,7 @@
 #define MAR_H__
 
 
-#ifndef WINCE
 #include "prtypes.h"
-#else
-typedef int PRInt32;
-typedef unsigned int PRUint32;
-typedef wchar_t PRUnichar;
-#endif
 
 #ifdef __cplusplus
 extern "C" {
