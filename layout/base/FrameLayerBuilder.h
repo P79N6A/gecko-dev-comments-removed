e4f3a5cd77fcd753da2d0ea985149bb40be09f56
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:28:36 +0000

    Bug 539356 - Part 12 - Remove unnecessary LayerManagerLayerBuilder indirection. r=roc
    UltraBlame original commit: ce5e9fefee191532a6164c7c697b6b97d2a699ae

--- layout/base/FrameLayerBuilder.h
+++ layout/base/FrameLayerBuilder.h
@@ -39,27 +39,8 @@ enum LayerState {
   LAYER_SVG_EFFECTS
 };
 
-class LayerManagerLayerBuilder : public layers::LayerUserData {
-public:
-  LayerManagerLayerBuilder(FrameLayerBuilder* aBuilder, bool aDelete = true)
-    : mLayerBuilder(aBuilder)
-    , mDelete(aDelete)
-  {
-    MOZ_COUNT_CTOR(LayerManagerLayerBuilder);
-  }
-  ~LayerManagerLayerBuilder();
-
-  FrameLayerBuilder* mLayerBuilder;
-  bool mDelete;
-};
-
 extern PRUint8 gLayerManagerLayerBuilder;
 
-static inline FrameLayerBuilder *GetLayerBuilderForManager(layers::LayerManager* aManager)
-{
-  LayerManagerLayerBuilder *data = static_cast<LayerManagerLayerBuilder*>(aManager->GetUserData(&gLayerManagerLayerBuilder));
-  return data ? data->mLayerBuilder : nsnull;
-}
 
 
 
@@ -99,8 +80,7 @@ static inline FrameLayerBuilder *GetLayerBuilderForManager(layers::LayerManager*
 
 
 
-
-class FrameLayerBuilder {
+class FrameLayerBuilder : public layers::LayerUserData {
 public:
   typedef layers::ContainerLayer ContainerLayer; 
   typedef layers::Layer Layer; 
@@ -702,6 +682,11 @@ protected:
   PRUint32                            mContainerLayerGeneration;
   PRUint32                            mMaxContainerLayerGeneration;
 };
+
+static inline FrameLayerBuilder *GetLayerBuilderForManager(layers::LayerManager* aManager)
+{
+  return static_cast<FrameLayerBuilder*>(aManager->GetUserData(&gLayerManagerLayerBuilder));
+}
 
 }
 
