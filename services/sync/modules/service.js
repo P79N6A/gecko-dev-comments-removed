b2c6fb9b525e1292ef860b48127636d693359ab3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 12:07:19 +0000

    Bug 612401: could not get engine: clients. r=philiKON a=mconnor
    UltraBlame original commit: 1a0094bc7086d77609c5626b726baa5dfe37ac0b

--- services/sync/modules/service.js
+++ services/sync/modules/service.js
@@ -1861,6 +1861,10 @@ WeaveSvc.prototype = {
 
     let enabled = [eng.name for each (eng in Engines.getEnabled())];
     for (let engineName in meta.payload.engines) {
+      if (engineName == "clients") {
+        
+        continue;
+      }
       let index = enabled.indexOf(engineName);
       if (index != -1) {
         
