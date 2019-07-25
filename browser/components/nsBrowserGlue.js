c3ed46381715046179901e107775432206103d04
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:58:17 +0000

    Bug 574514 - Hide personal toolbar only if user did not customize it or edited bookmarks. r=gavin a=blocking
    UltraBlame original commit: 3f60c5c6c0b55c17aef07266ef7b43aeab7b1349

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -257,7 +257,9 @@ BrowserGlue.prototype = {
         if (data == "post-update-notification") {
           if (Services.prefs.prefHasUserValue("app.update.postupdate"))
             this._showUpdateNotification();
-          break;
+        }
+        else if (data == "force-ui-migration") {
+          this._migrateUI();
         }
         break;
     }
@@ -997,7 +999,8 @@ BrowserGlue.prototype = {
   },
 
   _migrateUI: function BG__migrateUI() {
-    const UI_VERSION = 4;
+    const UI_VERSION = 5;
+    const BROWSER_DOCURL = "chrome://browser/content/browser.xul#";
     let currentUIVersion = 0;
     try {
       currentUIVersion = Services.prefs.getIntPref("browser.migration.version");
@@ -1014,7 +1017,7 @@ BrowserGlue.prototype = {
       let currentsetResource = this._rdf.GetResource("currentset");
       let toolbars = ["nav-bar", "toolbar-menubar", "PersonalToolbar"];
       for (let i = 0; i < toolbars.length; i++) {
-        let toolbar = this._rdf.GetResource("chrome://browser/content/browser.xul#" + toolbars[i]);
+        let toolbar = this._rdf.GetResource(BROWSER_DOCURL + toolbars[i]);
         let currentset = this._getPersist(toolbar, currentsetResource);
         if (!currentset) {
           
@@ -1039,7 +1042,7 @@ BrowserGlue.prototype = {
     if (currentUIVersion < 2) {
       
       let currentsetResource = this._rdf.GetResource("currentset");
-      let toolbarResource = this._rdf.GetResource("chrome://browser/content/browser.xul#nav-bar");
+      let toolbarResource = this._rdf.GetResource(BROWSER_DOCURL + "nav-bar");
       let currentset = this._getPersist(toolbarResource, currentsetResource);
       
       if (currentset &&
@@ -1058,7 +1061,7 @@ BrowserGlue.prototype = {
     if (currentUIVersion < 3) {
       
       let currentsetResource = this._rdf.GetResource("currentset");
-      let toolbarResource = this._rdf.GetResource("chrome://browser/content/browser.xul#nav-bar");
+      let toolbarResource = this._rdf.GetResource(BROWSER_DOCURL + "nav-bar");
       let currentset = this._getPersist(toolbarResource, currentsetResource);
       
       if (currentset &&
@@ -1077,7 +1080,7 @@ BrowserGlue.prototype = {
     if (currentUIVersion < 4) {
       
       let currentsetResource = this._rdf.GetResource("currentset");
-      let toolbarResource = this._rdf.GetResource("chrome://browser/content/browser.xul#nav-bar");
+      let toolbarResource = this._rdf.GetResource(BROWSER_DOCURL + "nav-bar");
       let currentset = this._getPersist(toolbarResource, currentsetResource);
       
       if (currentset &&
@@ -1087,6 +1090,34 @@ BrowserGlue.prototype = {
                                .replace(/(^|,)bookmarks-menu-button-container($|,)/,
                                         "$1home-button,bookmarks-menu-button-container$2");
         this._setPersist(toolbarResource, currentsetResource, currentset);
+      }
+    }
+
+    if (currentUIVersion < 5) {
+      
+      
+      let toolbarResource = this._rdf.GetResource(BROWSER_DOCURL + "PersonalToolbar");
+      let collapsedResource = this._rdf.GetResource("collapsed");
+      let collapsed = this._getPersist(toolbarResource, collapsedResource);
+      
+      
+      if (collapsed === null) {
+        
+        
+        let currentsetResource = this._rdf.GetResource("currentset");
+        let toolbarIsCustomized = !!this._getPersist(toolbarResource,
+                                                     currentsetResource);
+        function getToolbarFolderCount() {
+          let toolbarFolder =
+            PlacesUtils.getFolderContents(PlacesUtils.toolbarFolderId).root;
+          let toolbarChildCount = toolbarFolder.childCount;
+          toolbarFolder.containerOpen = false;
+          return toolbarChildCount;
+        }
+
+        if (toolbarIsCustomized || getToolbarFolderCount() > 3) {
+          this._setPersist(toolbarResource, collapsedResource, "false");
+        }
       }
     }
 
@@ -1119,6 +1150,15 @@ BrowserGlue.prototype = {
       }
       else {
         this._dataSource.Assert(aSource, aProperty, this._rdf.GetLiteral(aTarget), true);
+      }
+
+      
+      
+      let docURL = aSource.ValueUTF8.split("#")[0];
+      let docResource = this._rdf.GetResource(docURL);
+      let persistResource = this._rdf.GetResource("http://home.netscape.com/NC-rdf#persist");
+      if (!this._dataSource.HasAssertion(docResource, persistResource, aSource, true)) {
+        this._dataSource.Assert(docResource, persistResource, aSource, true);
       }
     }
     catch(ex) {}
