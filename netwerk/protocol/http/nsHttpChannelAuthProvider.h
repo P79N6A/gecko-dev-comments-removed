c42fcb7ed7b9fb5e8cd90211ed2ecf6964b9a008
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:35:15 +0000

    Bug 758992 - Make the classes which use the XPCOM nsISupports implementation macros final, to avoid the warning about deleting using a pointer to a base class with virtual functions and no virtual dtor (more intl parts); blanket-r=bzbarsky
    UltraBlame original commit: e76fcfc703161873c9b7cc9293a1ccfc899fb32d

--- netwerk/protocol/http/nsHttpChannelAuthProvider.h
+++ netwerk/protocol/http/nsHttpChannelAuthProvider.h
@@ -17,6 +17,7 @@
 #include "nsHttpAuthCache.h"
 #include "nsProxyInfo.h"
 #include "nsIDNSListener.h"
+#include "mozilla/Attributes.h"
 
 class nsIHttpAuthenticator;
 
@@ -151,7 +152,7 @@ private:
     PRUint32                          mResolvedHost             : 1;
 
     
-    class DNSCallback : public nsIDNSListener
+    class DNSCallback MOZ_FINAL : public nsIDNSListener
     {
         NS_DECL_ISUPPORTS
         NS_DECL_NSIDNSLISTENER
