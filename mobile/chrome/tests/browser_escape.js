44a266126c0166fb43a4ccc1d4806494e1ccf97f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:56:44 +0000

    Bug 684558 - browser-chrome test for escape key handling [r=mfinkle]
    UltraBlame original commit: 95b7fab4037ce5811ec52bd616674729955a1e8d

---
+++ mobile/chrome/tests/browser_escape.js
@@ -0,0 +1,114 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"use strict";
+const url1 = baseURI + "browser_blank_01.html";
+const url2 = baseURI + "browser_blank_02.html";
+const url3 = baseURI + "browser_blank_03.html";
+let tab1, tab2;
+
+function test() {
+  waitForExplicitFinish();
+  testGoBack();
+
+  registerCleanupFunction(closeTabs);
+}
+
+function testGoBack() {
+  tab1 = Browser.addTab("about:blank", true);
+  tab2 = Browser.addTab("about:blank", true, tab1);
+  let browser = tab2.browser;
+
+  
+  let urls = [url1, url2, url3];
+  let i = 0, step = 1;
+  let expectedURI;
+
+  function loadNextPage() {
+    expectedURI = urls[i];
+    if (step == 1) {
+      
+      Browser.loadURI(expectedURI);
+    } else {
+      
+      Browser.selectedBrowser.focus();
+      EventUtils.synthesizeKey("VK_ESCAPE", {type: "keypress"}, window);
+    }
+  }
+
+  browser.messageManager.addMessageListener("pageshow", function listener(aMessage) {
+    let uri = browser.currentURI.spec;
+    if (uri == "about:blank") {
+      loadNextPage();
+      return;
+    }
+    is(uri, expectedURI, "Page " + i + " loaded");
+
+    if (i == urls.length - 1)
+      step = -1; 
+    i += step;
+
+    if (i >= 0) {
+      loadNextPage();
+      
+    } else {
+      
+      browser.messageManager.removeMessageListener("pageshow", listener);
+      closeTabs();
+      testReturnToOwner();
+    }
+  });
+}
+
+function testReturnToOwner() {
+  tab1 = Browser.addTab("about:blank", true);
+  tab2 = Browser.addTab("about:blank", true, tab1);
+  is(Browser.selectedTab, tab2, "tab2 is selected");
+  EventUtils.sendKey("ESCAPE", window);
+  is(Browser.selectedTab, tab1, "tab1 is selected");
+  closeTabs();
+  finish();
+}
+
+function closeTabs() {
+  try {
+    Browser.closeTab(tab1);
+    Browser.closeTab(tab2);
+  } finally {
+    tab1 = tab2 = null;
+  }
+}
