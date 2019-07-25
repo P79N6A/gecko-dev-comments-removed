84b43bcf8c7ff94be2ef7b8b3f224a13073c4ebc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:30:10 +0000

    Bug 609256 - With strict this, there's no longer any need to compute this when a syntactically direct eval doesn't resolve to the eval function.  r=luke
    UltraBlame original commit: edc26e88b2c483649a4605e28e5be2cddf2ec155

---
+++ js/src/tests/ecma_5/Global/direct-eval-but-not.js
@@ -0,0 +1,32 @@
+
+
+
+
+var BUGNUMBER = 609256;
+var summary =
+  "Don't crash doing a direct eval when eval doesn't resolve to an object " +
+  "(let alone the original eval function)";
+
+print(BUGNUMBER + ": " + summary);
+
+
+
+
+
+var eval = "";
+try
+{
+  eval();
+  throw new Error("didn't throw?");
+}
+catch (e)
+{
+  assertEq(e instanceof TypeError, true);
+}
+
+
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
+
+print("All tests passed!");
