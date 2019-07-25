5d2942978fd816056b8baab3dfd87a7e0827453d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:26:14 +0000

    Bug 587248 - Part 11: Rotate the collapsed tab items towards top right in RTL mode; r=ian a=blocking-betaN+
    UltraBlame original commit: e716f4ca4f7a0a184af64f62d132959cd57d5f9d

--- browser/base/content/tabview/groupitems.js
+++ browser/base/content/tabview/groupitems.js
@@ -1088,13 +1088,16 @@ GroupItem.prototype = Utils.extend(new Item(), new Subscribable(), {
         
 
         
-        
         var rightMostRight = 0;
-        for each (var rect in rects) {
-          if (rect.right > rightMostRight)
-            rightMostRight = rect.right;
-          else
-            break;
+        if (UI.rtl) {
+          rightMostRight = rects[0].right;
+        } else {
+          for each (var rect in rects) {
+            if (rect.right > rightMostRight)
+              rightMostRight = rect.right;
+            else
+              break;
+          }
         }
         this.xDensity = (rightMostRight - bb.left) / (bb.width);
 
@@ -1179,7 +1182,7 @@ GroupItem.prototype = Utils.extend(new Item(), new Subscribable(), {
 
         child.addClass("stacked");
         child.setBounds(box, !animate);
-        child.setRotation(self._randRotate(maxRotation, index));
+        child.setRotation((UI.rtl ? -1 : 1) * self._randRotate(maxRotation, index));
       }
     });
 
