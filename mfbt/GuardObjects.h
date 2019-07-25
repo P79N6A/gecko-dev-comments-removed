fd98df7654c8d2cc3469d5284ef653f35ef0e9af
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:45:53 +0000

    Bug 720759 - Export mfbt's guard object classes to avoid warnings using them in exported APIs.  r=Ms2ger
    UltraBlame original commit: bd75f26eee254c2b9bf1a22e18c304833060a7a0

--- mfbt/GuardObjects.h
+++ mfbt/GuardObjects.h
@@ -44,6 +44,7 @@
 #define mozilla_GuardObjects_h
 
 #include "mozilla/Assertions.h"
+#include "mozilla/Types.h"
 
 #ifdef __cplusplus
 
@@ -99,7 +100,7 @@ namespace detail {
 
 
 
-class GuardObjectNotifier
+class MOZ_EXPORT_API(GuardObjectNotifier)
 {
   private:
     bool* statementDone;
@@ -116,7 +117,7 @@ class GuardObjectNotifier
     }
 };
 
-class GuardObjectNotificationReceiver
+class MOZ_EXPORT_API(GuardObjectNotificationReceiver)
 {
   private:
     bool statementDone;
