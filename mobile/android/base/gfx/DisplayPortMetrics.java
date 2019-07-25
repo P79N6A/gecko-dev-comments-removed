ae328eb0360eb33993c9a6243e5b13ea0b41a268
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:59:56 +0000

    Bug 729528 - Add a DisplayPortMetrics to track the requested display resolution along with the area. r=Cwiiis
    UltraBlame original commit: 3e5eee67741ff61d1a18d922bdd5bc2fa26b7aa4

---
+++ mobile/android/base/gfx/DisplayPortMetrics.java
@@ -0,0 +1,51 @@
+
+
+
+
+
+package org.mozilla.gecko.gfx;
+
+import android.graphics.RectF;
+
+
+
+
+
+
+
+
+
+public final class DisplayPortMetrics {
+    private final RectF mPosition;
+    private final float mResolution;
+
+    public DisplayPortMetrics() {
+        this(0, 0, 0, 0, 1);
+    }
+
+    public DisplayPortMetrics(float left, float top, float right, float bottom, float resolution) {
+        mPosition = new RectF(left, top, right, bottom);
+        mResolution = resolution;
+    }
+
+    public boolean contains(RectF rect) {
+        return mPosition.contains(rect);
+    }
+
+    public String toJSON() {
+        StringBuffer sb = new StringBuffer(256);
+        sb.append("{ \"left\": ").append(mPosition.left)
+          .append(", \"top\": ").append(mPosition.top)
+          .append(", \"right\": ").append(mPosition.right)
+          .append(", \"bottom\": ").append(mPosition.bottom)
+          .append(", \"resolution\": ").append(mResolution)
+          .append('}');
+        return sb.toString();
+    }
+
+    public String toString() {
+        return "DisplayPortMetrics(" + mPosition.left + ","
+                + mPosition.top + "," + mPosition.right + ","
+                + mPosition.bottom + "," + mResolution + ")";
+    }
+}
