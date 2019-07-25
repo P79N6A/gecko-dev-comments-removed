e095e034df81e02433514db6560870603612fae1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:34:18 +0000

    Merge for backout of changeset d6531ef05a6f
    UltraBlame original commit: e1cf42a39e6a56e940eb165bb4ecd2b933e58b68

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -159,18 +159,18 @@ struct ConservativeGCData
     }
 };
 
-class ToSourceCache
+class SourceDataCache
 {
-    typedef HashMap<JSFunction *,
-                    JSString *,
-                    DefaultHasher<JSFunction *>,
+    typedef HashMap<ScriptSource *,
+                    JSFixedString *,
+                    DefaultHasher<ScriptSource *>,
                     SystemAllocPolicy> Map;
-    Map *map_;
-  public:
-    ToSourceCache() : map_(NULL) {}
-    JSString *lookup(JSFunction *fun);
-    void put(JSFunction *fun, JSString *);
-    void purge();
+     Map *map_;
+   public:
+    SourceDataCache() : map_(NULL) {}
+    JSFixedString *lookup(ScriptSource *ss);
+    void put(ScriptSource *ss, JSFixedString *);
+     void purge();
 };
 
 struct EvalCacheLookup
@@ -197,7 +197,7 @@ class NativeIterCache
     static const size_t SIZE = size_t(1) << 8;
 
     
-    JSObject            *data[SIZE];
+    PropertyIteratorObject *data[SIZE];
 
     static size_t getIndex(uint32_t key) {
         return size_t(key) % SIZE;
@@ -205,7 +205,7 @@ class NativeIterCache
 
   public:
     
-    JSObject            *last;
+    PropertyIteratorObject *last;
 
     NativeIterCache()
       : last(NULL) {
@@ -217,11 +217,11 @@ class NativeIterCache
         PodArrayZero(data);
     }
 
-    JSObject *get(uint32_t key) const {
+    PropertyIteratorObject *get(uint32_t key) const {
         return data[getIndex(key)];
     }
 
-    void set(uint32_t key, JSObject *iterobj) {
+    void set(uint32_t key, PropertyIteratorObject *iterobj) {
         data[getIndex(key)] = iterobj;
     }
 };
@@ -496,7 +496,8 @@ struct JSRuntime : js::RuntimeFriendFields
 
     volatile uint32_t   gcNumArenasFreeCommitted;
     js::GCMarker        gcMarker;
-    void                *gcVerifyData;
+    void                *gcVerifyPreData;
+    void                *gcVerifyPostData;
     bool                gcChunkAllocationSinceLastGC;
     int64_t             gcNextFullGCTime;
     int64_t             gcLastGCTime;
@@ -710,6 +711,8 @@ struct JSRuntime : js::RuntimeFriendFields
         return !JS_CLIST_IS_EMPTY(&contextList);
     }
 
+    JS_SourceHook       sourceHook;
+
     
     JSDebugHooks        debugHooks;
 
@@ -744,6 +747,10 @@ struct JSRuntime : js::RuntimeFriendFields
     PRLock              *gcLock;
 
     js::GCHelperThread  gcHelperThread;
+
+#ifdef JS_THREADSAFE
+    js::SourceCompressorThread sourceCompressorThread;
+#endif
 
   private:
     js::FreeOp          defaultFreeOp_;
@@ -794,7 +801,7 @@ struct JSRuntime : js::RuntimeFriendFields
     js::PropertyCache   propertyCache;
     js::NewObjectCache  newObjectCache;
     js::NativeIterCache nativeIterCache;
-    js::ToSourceCache   toSourceCache;
+    js::SourceDataCache sourceDataCache;
     js::EvalCache       evalCache;
 
     
@@ -823,6 +830,8 @@ struct JSRuntime : js::RuntimeFriendFields
     js::PreserveWrapperCallback            preserveWrapperCallback;
 
     js::ScriptFilenameTable scriptFilenameTable;
+
+    js::ScriptSource *scriptSources;
 
 #ifdef DEBUG
     size_t              noGCOrAllocationCheck;
@@ -1140,7 +1149,7 @@ struct JSContext : js::ContextFriendFields
 
   private:
     
-    js::ParseMapPool    *parseMapPool_;
+    js::frontend::ParseMapPool *parseMapPool_;
 
   public:
     
@@ -1169,7 +1178,7 @@ struct JSContext : js::ContextFriendFields
     inline js::RegExpStatics *regExpStatics();
 
   public:
-    js::ParseMapPool &parseMapPool() {
+    js::frontend::ParseMapPool &parseMapPool() {
         JS_ASSERT(parseMapPool_);
         return *parseMapPool_;
     }
@@ -1297,7 +1306,7 @@ struct JSContext : js::ContextFriendFields
     DSTOffsetCache dstOffsetCache;
 
     
-    JSObject *enumerators;
+    js::PropertyIteratorObject *enumerators;
 
   private:
     
