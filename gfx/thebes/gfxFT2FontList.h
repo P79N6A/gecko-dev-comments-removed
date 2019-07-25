ff720499e0d85d316ccd1ecf04e48426d37161d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:54:26 +0000

    bug 684889 pt2 - move and rename FontFamily/Entry classes from gfxFT2Fonts to gfxFT2FontList source file. r=jdaggett
    UltraBlame original commit: f4b1fa7a0f3147b20e67701f124c8b9ddfe0c447

--- gfx/thebes/gfxFT2FontList.h
+++ gfx/thebes/gfxFT2FontList.h
@@ -55,6 +55,69 @@ namespace mozilla {
 using mozilla::dom::FontListEntry;
 
 class FontNameCache;
+typedef struct FT_FaceRec_* FT_Face;
+
+class FT2FontEntry : public gfxFontEntry
+{
+public:
+    FT2FontEntry(const nsAString& aFaceName) :
+        gfxFontEntry(aFaceName)
+    {
+        mFTFace = nsnull;
+        mFontFace = nsnull;
+        mFTFontIndex = 0;
+    }
+
+    ~FT2FontEntry();
+
+    const nsString& GetName() const {
+        return Name();
+    }
+
+    
+    static FT2FontEntry* 
+    CreateFontEntry(const gfxProxyFontEntry &aProxyEntry,
+                    const PRUint8 *aFontData, PRUint32 aLength);
+
+    
+    
+    
+    static FT2FontEntry*
+    CreateFontEntry(const FontListEntry& aFLE);
+
+    
+    
+    static FT2FontEntry* 
+    CreateFontEntry(FT_Face aFace, const char *aFilename, PRUint8 aIndex,
+                    const PRUint8 *aFontData = nsnull);
+        
+        
+
+    virtual gfxFont *CreateFontInstance(const gfxFontStyle *aFontStyle,
+                                        PRBool aNeedsBold);
+
+    cairo_font_face_t *CairoFontFace();
+    cairo_scaled_font_t *CreateScaledFont(const gfxFontStyle *aStyle);
+
+    nsresult ReadCMAP();
+    nsresult GetFontTable(PRUint32 aTableTag, FallibleTArray<PRUint8>& aBuffer);
+
+    FT_Face mFTFace;
+    cairo_font_face_t *mFontFace;
+
+    nsCString mFilename;
+    PRUint8 mFTFontIndex;
+};
+
+class FT2FontFamily : public gfxFontFamily
+{
+public:
+    FT2FontFamily(const nsAString& aName) :
+        gfxFontFamily(aName) { }
+
+    
+    void AddFacesToFontList(InfallibleTArray<FontListEntry>* aFontList);
+};
 
 class gfxFT2FontList : public gfxPlatformFontList
 {
