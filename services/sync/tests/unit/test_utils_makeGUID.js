5faf3758395b51974ad376c56a3f6a61051a7c08
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:28:58 +0000

    Bug 606353 - Add a Utils.checkGUID() helper. r=rnewman
    UltraBlame original commit: 01b0083ca0d6fbf43db38d3291b8c6c021026e68

--- services/sync/tests/unit/test_utils_makeGUID.js
+++ services/sync/tests/unit/test_utils_makeGUID.js
@@ -1,10 +1,10 @@
-_("Make sure makeGUID makes guids of the right length/characters");
 Cu.import("resource://services-sync/util.js");
 
 const base64url =
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
 
 function run_test() {
+  _("Make sure makeGUID makes guids of the right length/characters");
   _("Create a bunch of guids to make sure they don't conflict");
   let guids = [];
   for (let i = 0; i < 1000; i++) {
@@ -21,9 +21,20 @@ function run_test() {
     }));
 
     
+    do_check_true(Utils.checkGUID(newGuid));
+
+    
     
     
     do_check_true(guids.every(function(g) { return g != newGuid; }));
     guids.push(newGuid);
   }
+
+  _("Make sure checkGUID fails for invalid GUIDs");
+  do_check_false(Utils.checkGUID(undefined));
+  do_check_false(Utils.checkGUID(null));
+  do_check_false(Utils.checkGUID(""));
+  do_check_false(Utils.checkGUID("blergh"));
+  do_check_false(Utils.checkGUID("ThisGUIDisWayTooLong"));
+  do_check_false(Utils.checkGUID("Invalid!!!!!"));
 }
