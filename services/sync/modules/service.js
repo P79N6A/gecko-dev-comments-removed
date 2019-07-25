a795bb1ee420a177cb1b936d788ce477bd8b752c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:23:54 +0000

    bug 533759 - delay real startup for 10 seconds on Fennec to get away from startup
    UltraBlame original commit: e4d505ca39586e36dfc3031623d8bd2451efd4af

--- services/sync/modules/service.js
+++ services/sync/modules/service.js
@@ -252,6 +252,9 @@ WeaveSvc.prototype = {
         let enum = Svc.WinMediator.getEnumerator("navigator:browser");
         while (enum.hasMoreElements())
           wait += enum.getNext().gBrowser.mTabs.length;
+        break;
+      case FENNEC_ID:
+        wait = 10;
     }
 
     
