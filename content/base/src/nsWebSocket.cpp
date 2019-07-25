e74bbc146ca1d6b41e5527f5b3f006a0987cfa62
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:34:32 +0000

    bug 667853: Missing close() event if websocket::init fails. r=mcmanus, jdm
    UltraBlame original commit: e337d0d2d7a92be8046b86a8587ff362db34285b

--- content/base/src/nsWebSocket.cpp
+++ content/base/src/nsWebSocket.cpp
@@ -375,7 +375,6 @@ nsWebSocketEstablishedConnection::Close()
   nsRefPtr<nsWebSocketEstablishedConnection> kungfuDeathGrip = this;
 
   if (mOwner->mReadyState == nsIMozWebSocket::CONNECTING) {
-    mOwner->SetReadyState(nsIMozWebSocket::CLOSING);
     mOwner->SetReadyState(nsIMozWebSocket::CLOSED);
     Disconnect();
     return NS_OK;
@@ -765,9 +764,12 @@ nsWebSocket::EstablishConnection()
     new nsWebSocketEstablishedConnection();
 
   rv = conn->Init(this);
-  NS_ENSURE_SUCCESS(rv, rv);
-
   mConnection = conn;
+  if (NS_FAILED(rv)) {
+    Close();
+    mConnection = nsnull;
+    return rv;
+  }
 
   return NS_OK;
 }
