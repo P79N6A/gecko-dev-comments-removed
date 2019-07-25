3f23301a423d64b8fb2dd9cc54439ac6191c68fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:15:38 +0000

    Test for bug 656331 - make sure we don't load version-incompatible components.
    UltraBlame original commit: b23a920b981e2a27b3869a978fb396f2c0e3c47e

---
+++ xpcom/tests/bug656331_component/TestComponent.cpp
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "mozilla/ModuleUtils.h"
+
+
+#define NS_TESTING_CID \
+{ 0xf18fb09b, 0x28b4, 0x4435, \
+  { 0xbc, 0x5b, 0x80, 0x27, 0xf1, 0x8d, 0xf7, 0x43 } }
+
+NS_DEFINE_NAMED_CID(NS_TESTING_CID);
+
+static nsresult
+DummyConstructorFunc(nsISupports* aOuter, const nsIID& aIID, void** aResult)
+{
+  return NS_ERROR_NOT_IMPLEMENTED;
+}
+
+static const mozilla::Module::CIDEntry kTestCIDs[] = {
+  { &kNS_TESTING_CID, false, NULL, DummyConstructorFunc },
+  { NULL }
+};
+
+static const mozilla::Module kTestModule = {
+  3, 
+  kTestCIDs
+};
+
+NSMODULE_DEFN(dummy) = &kTestModule;
