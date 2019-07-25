52651b4c94260a392a1e25e3a747af5ce9231b1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:39:47 +0000

    [INFER] Fix botched merge.
    UltraBlame original commit: 7218d92dd3d3eae87d1443b3b442080d76799cb2

--- js/src/jsarray.cpp
+++ js/src/jsarray.cpp
@@ -490,17 +490,20 @@ js_EnsureDenseArrayCapacity(JSContext *cx, JSObject *obj, jsint i)
     Class *origObjClasp = obj->clasp; 
 #endif
     jsuint u = jsuint(i);
-    JSBool ret = (obj->ensureDenseArrayElements(cx, u, 1) == JSObject::ED_OK);
+    if (obj->ensureDenseArrayElements(cx, u, 1) != JSObject::ED_OK)
+        return false;
 
     
 
 
 
     obj->setDenseArrayElement(i, UndefinedValue());
+    if (u >= obj->getArrayLength())
+        obj->setArrayLength(cx, u + 1);
 
     
     JS_ASSERT(obj->clasp == origObjClasp);
-    return ret;
+    return true;
 }
 
 JS_DEFINE_CALLINFO_3(extern, BOOL, js_EnsureDenseArrayCapacity, CONTEXT, OBJECT, INT32,
