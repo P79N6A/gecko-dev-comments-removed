12030db2eb86ceb8bfa7e841dc4f181e12094b5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:56:20 +0000

    Remove unnecessary padding from JSStackFrame
    UltraBlame original commit: edb0506b0c23056f830179ba6fda70af3f9a5d29

--- js/src/jsinterp.h
+++ js/src/jsinterp.h
@@ -129,10 +129,6 @@ struct JSStackFrame
     void                *hookData_;     
     void                *annotation_;   
 
-#if JS_BITS_PER_WORD == 32
-    void                *padding;
-#endif
-
     friend class js::StackSpace;
     friend class js::FrameRegsIter;
     friend struct JSContext;
