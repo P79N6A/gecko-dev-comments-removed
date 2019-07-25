7927640dbf7f9c2ad3997f2f3b936bc456ce824c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:04:10 +0000

    Bug 609729 - Form helper makes commenting on tor.com impossible
    UltraBlame original commit: e98088b921c12ff1f4352ca677282a2f1f4bacff

--- mobile/chrome/content/browser-ui.js
+++ mobile/chrome/content/browser-ui.js
@@ -1812,6 +1812,10 @@ var FormHelperUI = {
   _currentCaretRect: null,
   _currentElementRect: null,
 
+  get enabled() {
+    return Services.prefs.getBoolPref("formhelper.enabled");
+  },
+
   _visibleScreenArea: null,
   get visibleScreenArea() {
     let visibleRect = Rect.fromRect(Browser.selectedBrowser.getBoundingClientRect());
@@ -1909,9 +1913,8 @@ var FormHelperUI = {
         
         
         
-        let enabled = Services.prefs.getBoolPref("formhelper.enabled");
-        enabled ? this.show(json.current, json.hasPrevious, json.hasNext)
-                : SelectHelperUI.show(json.current.choices);
+        this.enabled ? this.show(json.current, json.hasPrevious, json.hasNext)
+                     : SelectHelperUI.show(json.current.choices);
         break;
 
       case "FormAssist:Hide":
