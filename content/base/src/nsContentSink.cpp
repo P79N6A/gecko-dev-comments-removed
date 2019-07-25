3298ed6f8a974cd678bb315aae2e0a16c647da98
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:34:53 +0000

    Bug 230703 - only accept link header field target when it's first in the value. r=bz
    UltraBlame original commit: 0cff49ed75df6778f7b4777c1c96ecd8d91e377c

--- content/base/src/nsContentSink.cpp
+++ content/base/src/nsContentSink.cpp
@@ -610,6 +610,9 @@ nsContentSink::ProcessLinkHeader(nsIContent* aElement,
   nsresult rv = NS_OK;
 
   
+  PRBool seenParameters = PR_FALSE;
+
+  
   nsAutoString href;
   nsAutoString rel;
   nsAutoString title;
@@ -704,12 +707,15 @@ nsContentSink::ProcessLinkHeader(nsIContent* aElement,
       if ((*start == kLessThan) && (*last == kGreaterThan)) {
         *last = kNullCh;
 
-        if (href.IsEmpty()) { 
+        
+        
+        if (href.IsEmpty() && !seenParameters) {
           href = (start + 1);
           href.StripWhitespace();
         }
       } else {
         PRUnichar* equals = start;
+        seenParameters = PR_TRUE;
 
         while ((*equals != kNullCh) && (*equals != kEqual)) {
           equals++;
@@ -791,6 +797,8 @@ nsContentSink::ProcessLinkHeader(nsIContent* aElement,
       type.Truncate();
       media.Truncate();
       anchor.Truncate();
+      
+      seenParameters = PR_FALSE;
     }
 
     start = ++end;
