95b0337384e8fce0957480533fcfa8d86c9bb0fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:56:16 +0000

    Bug 602003: add jsd API to query valid script begin and end PCs, r=sayrer,jjb
    UltraBlame original commit: 1998fa24043490e63ce38e724e5f850e08904c7a

--- js/jsd/jsdebug.h
+++ js/jsd/jsdebug.h
@@ -489,6 +489,19 @@ JSD_GetClosestPC(JSDContext* jsdc, JSDScript* jsdscript, uintN line);
 
 
 
+extern JSD_PUBLIC_API(jsuword)
+JSD_GetFirstValidPC(JSDContext* jsdc, JSDScript* jsdscript);
+
+
+
+
+
+extern JSD_PUBLIC_API(jsuword)
+JSD_GetEndPC(JSDContext* jsdc, JSDScript* jsdscript);
+
+
+
+
 
 
 extern JSD_PUBLIC_API(uintN)
