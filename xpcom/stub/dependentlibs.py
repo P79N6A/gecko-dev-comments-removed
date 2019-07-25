9fa00d71e019da9d811eae03c8defcaecf3fa3c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:26:48 +0000

    Bug 763893 - Generate dependentlibs.list from actual library dependencies. r=ted
    UltraBlame original commit: a57052eb374b42e4bc35c8015778cf150b584bb8

---
+++ xpcom/stub/dependentlibs.py
@@ -0,0 +1,102 @@
+
+
+
+
+'''Given a library, dependentlibs.py prints the list of libraries it depends
+upon that are in the same directory.
+'''
+
+from optparse import OptionParser
+import os
+import re
+import subprocess
+import sys
+
+def dependentlibs_dumpbin(lib):
+    '''Returns the list of dependencies declared in the given DLL'''
+    proc = subprocess.Popen(['dumpbin', '-imports', lib], stdout = subprocess.PIPE)
+    deps = []
+    for line in proc.stdout:
+        
+        match = re.match('    (\S+)', line)
+        if match:
+             deps.append(match.group(1))
+    proc.wait()
+    return deps
+
+def dependentlibs_readelf(lib):
+    '''Returns the list of dependencies declared in the given ELF .so'''
+    proc = subprocess.Popen(['readelf', '-d', lib], stdout = subprocess.PIPE)
+    deps = []
+    for line in proc.stdout:
+        
+        
+        
+        tmp = line.split(' ', 3)
+        if len(tmp) > 3 and tmp[2] == '(NEEDED)':
+            
+            
+            match = re.search('\[(.*)\]', tmp[3])
+            if match:
+                deps.append(match.group(1))
+    proc.wait()
+    return deps
+
+def dependentlibs_otool(lib):
+    '''Returns the list of dependencies declared in the given MACH-O dylib'''
+    proc = subprocess.Popen(['otool', '-l', lib], stdout = subprocess.PIPE)
+    deps= []
+    cmd = None
+    for line in proc.stdout:
+        
+        
+        
+        
+        
+        tmp = line.split()
+        if len(tmp) < 2:
+            continue
+        if tmp[0] == 'cmd':
+            cmd = tmp[1]
+        elif cmd == 'LC_LOAD_DYLIB' and tmp[0] == 'name':
+            deps.append(re.sub('^@executable_path/','',tmp[1]))
+    proc.wait()
+    return deps
+
+def dependentlibs(lib, libpaths, func):
+    '''For a given library, returns the list of recursive dependencies that can
+    be found in the given list of paths'''
+    assert(libpaths)
+    assert(isinstance(libpaths, list))
+    deps = []
+    for dep in func(lib):
+        if dep in deps or os.path.isabs(dep):
+            continue
+        for dir in libpaths:
+            deppath = os.path.join(dir, dep)
+            if os.path.exists(deppath):
+                deps.extend([d for d in dependentlibs(deppath, libpaths, func) if not d in deps])
+                deps.append(dep)
+                break
+
+    return deps
+
+def main():
+    parser = OptionParser()
+    parser.add_option("-L", dest="libpaths", action="append", metavar="PATH", help="Add the given path to the library search path")
+    (options, args) = parser.parse_args()
+    lib = args[0]
+    ext = os.path.splitext(lib)[1]
+    if ext == '.dll':
+        func = dependentlibs_dumpbin
+    elif ext == '.so':
+        func = dependentlibs_readelf
+    elif ext == '.dylib':
+        func = dependentlibs_otool
+    if not options.libpaths:
+        options.libpaths = [os.path.dirname(lib)]
+
+    print '\n'.join(dependentlibs(lib, options.libpaths, func))
+
+if __name__ == '__main__':
+    main()
