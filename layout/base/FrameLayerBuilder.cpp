c61bd5abf6ed92176216416624703f016f7b38c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:29:01 +0000

    Bug 770144 - Restrict ThebesLayer invalidations to the visible region. r=roc
    UltraBlame original commit: 921c2e13b45e7c915a4f16e83ef3b9fe845e06b9

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -690,6 +690,7 @@ InvalidatePostTransformRegion(ThebesLayer* aLayer, const nsIntRegion& aRegion,
   
   nsIntRegion rgn = aRegion;
   rgn.MoveBy(-aTranslation);
+  rgn.And(rgn, aLayer->GetVisibleRegion());
   aLayer->InvalidateRegion(rgn);
 #ifdef DEBUG_INVALIDATIONS
   nsCAutoString str;
