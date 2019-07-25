8d537eb6fa0ea2a62635370a968ed497a231d680
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:08:08 +0000

    Bug 751881 Set keycode to keypress event at WM_CHAR handler when charcode is 0 r=jimm
    UltraBlame original commit: 06d66915251090f2fdcac370af26ea7c3590d01c

--- widget/windows/KeyboardLayout.h
+++ widget/windows/KeyboardLayout.h
@@ -47,6 +47,8 @@
 #define VK_OEM_PLUS             0xBB   // '+' any country
 #define VK_OEM_MINUS            0xBD   // '-' any country
 
+class nsWindow;
+
 namespace mozilla {
 namespace widget {
 
@@ -134,21 +136,25 @@ public:
 class NativeKey {
 public:
   NativeKey() :
-    mVirtualKeyCode(0), mOriginalVirtualKeyCode(0),
+    mDOMKeyCode(0), mVirtualKeyCode(0), mOriginalVirtualKeyCode(0),
     mScanCode(0), mIsExtended(false)
   {
   }
 
   NativeKey(HKL aKeyboardLayout,
-            HWND aWnd,
+            nsWindow* aWindow,
             const MSG& aKeyOrCharMessage);
+
+  PRUint32 GetDOMKeyCode() const { return mDOMKeyCode; }
 
   
   PRUint32 GetKeyLocation() const;
+  WORD GetScanCode() const { return mScanCode; }
   PRUint8 GetVirtualKeyCode() const { return mVirtualKeyCode; }
   PRUint8 GetOriginalVirtualKeyCode() const { return mOriginalVirtualKeyCode; }
 
 private:
+  PRUint32 mDOMKeyCode;
   
   PRUint8 mVirtualKeyCode;
   
