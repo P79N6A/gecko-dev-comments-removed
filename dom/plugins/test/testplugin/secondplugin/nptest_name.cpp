5f235664f200751c5ec5c2b9d2a669dedaf49066
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:39:29 +0000

    Bug 749257 - Improve test plugin. r=josh, r=bsmedberg
    UltraBlame original commit: 1230521b11d32352d89f2da2bde168bde56f428b

---
+++ dom/plugins/test/testplugin/secondplugin/nptest_name.cpp
@@ -0,0 +1,7 @@
+
+
+
+
+const char *sPluginName = "Second Test Plug-in";
+const char *sPluginDescription = "Second plug-in for testing purposes.";
+const char *sMimeDescription = "application/x-second-test:ts2:Second test type";
