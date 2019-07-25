d935b63bf291f35eb6b6339486b44e9abaa3d74b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:04:46 +0000

    bug 594368 - nsFilePicker is called directly from the child process in e10s r=jdm a=blocking-fennec
    UltraBlame original commit: 6e70eb8a527118b8fb1b009b3c6e457ffe0ece46

---
+++ widget/src/xpwidgets/nsFilePickerProxy.h
@@ -0,0 +1,97 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef NSFILEPICKERPROXY_H
+#define NSFILEPICKERPROXY_H
+
+#include "nsBaseFilePicker.h"
+#include "nsString.h"
+#include "nsIURI.h"
+#include "nsTArray.h"
+#include "nsCOMArray.h"
+
+class nsIWidget;
+class nsILocalFile;
+
+
+
+
+
+
+
+class nsFilePickerProxy : public nsBaseFilePicker
+{
+public:
+    nsFilePickerProxy();
+
+    NS_DECL_ISUPPORTS
+
+    
+    NS_IMETHODIMP Init(nsIDOMWindow* parent, const nsAString& title, PRInt16 mode);
+    NS_IMETHODIMP AppendFilter(const nsAString& aTitle, const nsAString& aFilter);
+    NS_IMETHODIMP GetDefaultString(nsAString& aDefaultString);
+    NS_IMETHODIMP SetDefaultString(const nsAString& aDefaultString);
+    NS_IMETHODIMP GetDefaultExtension(nsAString& aDefaultExtension);
+    NS_IMETHODIMP SetDefaultExtension(const nsAString& aDefaultExtension);
+    NS_IMETHODIMP GetFilterIndex(PRInt32* aFilterIndex);
+    NS_IMETHODIMP SetFilterIndex(PRInt32 aFilterIndex);
+    NS_IMETHODIMP GetFile(nsILocalFile** aFile);
+    NS_IMETHODIMP GetFileURL(nsIURI** aFileURL);
+    NS_IMETHODIMP GetFiles(nsISimpleEnumerator** aFiles);
+    NS_IMETHODIMP Show(PRInt16* aReturn);
+
+private:
+    ~nsFilePickerProxy();
+    void InitNative(nsIWidget*, const nsAString&, short int);
+
+    nsCOMArray<nsILocalFile> mFiles;
+
+    PRInt16   mMode;
+    PRInt16   mSelectedType;
+    nsString  mFile;
+    nsString  mTitle;
+    nsString  mDefault;
+    nsString  mDefaultExtension;
+
+    nsTArray<nsString> mFilters;
+    nsTArray<nsString> mFilterNames;
+};
+
+#endif 
