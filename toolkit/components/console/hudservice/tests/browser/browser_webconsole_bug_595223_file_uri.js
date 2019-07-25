e62fb7f5b3b31e46347bccca004531c9d94fbd4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:11:38 +0000

    Bug 595223 - Keep track of file:// url 'loadGroups'; f=ddahl r=dcamp,dolske
    UltraBlame original commit: 0d5421e233468ffdcc5b2ca71539b2ad2e876683

---
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_bug_595223_file_uri.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const TEST_FILE = "test-network.html";
+
+function tabLoad(aEvent) {
+  browser.removeEventListener(aEvent.type, arguments.callee, true);
+
+  openConsole();
+
+  let hudId = HUDService.getHudIdByWindow(content);
+  hud = HUDService.hudReferences[hudId];
+
+  browser.addEventListener("load", tabReload, true);
+
+  content.location.reload();
+}
+
+function tabReload(aEvent) {
+  browser.removeEventListener(aEvent.type, arguments.callee, true);
+
+  let textContent = hud.outputNode.textContent;
+  isnot(textContent.indexOf("test-network.html"), -1,
+        "found test-network.html");
+  isnot(textContent.indexOf("test-image.png"), -1, "found test-image.png");
+  isnot(textContent.indexOf("testscript.js"), -1, "found testscript.js");
+  isnot(textContent.indexOf("running network console logging tests"), -1,
+        "found the console.log() message from testscript.js");
+
+  finishTest();
+}
+
+function test() {
+  let jar = getJar(getRootDirectory(gTestPath));
+  let dir = jar ?
+            extractJarToTmp(jar) :
+            getChromeDir(getResolvedURI(gTestPath));
+  dir.append(TEST_FILE);
+
+  let uri = Services.io.newFileURI(dir);
+
+  addTab(uri.spec);
+  browser.addEventListener("load", tabLoad, true);
+}
