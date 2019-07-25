84fe8f49d25b51e3b3b40d7fe473bbe33bca3d16
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:49:25 +0000

    Bug 612527 - Implement spec tweaks to U+0000 handling in the HTML5 parser. rs=jonas, a=blocking2.0-betaN.
    UltraBlame original commit: 0d032e29af92e5f7cfc9b81f654731cbad2219cd

--- parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
+++ parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
@@ -7,10 +7,6 @@
 var html5Exceptions = {
   "<!doctype html><keygen><frameset>" : true, 
   "<select><keygen>" : true, 
-  "<plaintext>\u0000filler\u0000text\u0000" : true, 
-  "<body><svg><foreignObject>\u0000filler\u0000text" : true, 
-  "<svg>\u0000</svg><frameset>" : true, 
-  "<svg>\u0000 </svg><frameset>" : true, 
   "<option><span><option>" : true, 
   "<!doctype html><div><body><frameset>" : true, 
 }
