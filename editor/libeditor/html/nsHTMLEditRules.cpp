5c2069f6f1dd1c05226140a3912f0b2f55988ce8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:39:20 +0000

    Bug 780035 - Don't cache font-style as CSS; r=ehsan
    UltraBlame original commit: 4b92b8cf09669d4a91665abe589efb4dbea12d84

--- editor/libeditor/html/nsHTMLEditRules.cpp
+++ editor/libeditor/html/nsHTMLEditRules.cpp
@@ -7107,8 +7107,9 @@ nsHTMLEditRules::CacheInlineStyles(nsIDOMNode *aNode)
   {
     bool isSet = false;
     nsAutoString outValue;
-    if (!useCSS)
-    {
+    
+    if (!useCSS || (mCachedStyles[j].tag == nsGkAtoms::font &&
+                    mCachedStyles[j].attr.EqualsLiteral("size"))) {
       mHTMLEditor->IsTextPropertySetByContent(aNode, mCachedStyles[j].tag,
                                               &(mCachedStyles[j].attr), nullptr,
                                               isSet, &outValue);
