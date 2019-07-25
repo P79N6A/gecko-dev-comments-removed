494a43421a6f6a9b3c6d7e34468afdcd2424c27d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:33:05 +0000

    Bug 773100 - Invalidate the frame bounds instead of deleting the frame property. r=roc
    UltraBlame original commit: f0be368bb1727143835c6278e7c6c551e1139153

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -2305,7 +2305,7 @@ InternalInvalidateThebesLayersInSubtree(nsIFrame* aFrame)
   if (aFrame->GetStateBits() & NS_FRAME_HAS_CONTAINER_LAYER) {
     
     
-    aFrame->Properties().Delete(ThebesLayerInvalidRegionProperty());
+    InvalidateThebesLayerContents(aFrame, aFrame->GetVisualOverflowRectRelativeToSelf());
     foundContainerLayer = true;
   }
 
