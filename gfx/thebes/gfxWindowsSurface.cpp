5c0e0b200a7ca655ddda860a71f4513c620f1c6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:31:29 +0000

    Bug 629866. Part 1: Create a DIB for a Win32 surface that's similar to a parent surface with alpha, even if the parent surface doesn't have a DIB. r=jrmuizel
    UltraBlame original commit: c7290e6e795b8eb7bc257a71416f1dc985ee1627

--- gfx/thebes/gfxWindowsSurface.cpp
+++ gfx/thebes/gfxWindowsSurface.cpp
@@ -130,6 +130,43 @@ gfxWindowsSurface::InitWithDC(PRUint32 flags)
     }
 }
 
+already_AddRefed<gfxASurface>
+gfxWindowsSurface::CreateSimilarSurface(gfxContentType aContent,
+                                        const gfxIntSize& aSize)
+{
+    if (!mSurface || !mSurfaceValid) {
+        return nsnull;
+    }
+
+    cairo_surface_t *surface;
+    if (GetContentType() == CONTENT_COLOR_ALPHA) {
+        
+        
+        
+        
+        
+        
+        
+        
+        surface =
+          cairo_win32_surface_create_with_dib(cairo_format_t(gfxASurface::FormatFromContent(aContent)),
+                                              aSize.width, aSize.height);
+    } else {
+        surface =
+          cairo_surface_create_similar(mSurface, cairo_content_t(aContent),
+                                       aSize.width, aSize.height);
+    }
+
+    if (cairo_surface_status(surface)) {
+        cairo_surface_destroy(surface);
+        return nsnull;
+    }
+
+    nsRefPtr<gfxASurface> result = Wrap(surface);
+    cairo_surface_destroy(surface);
+    return result.forget();
+}
+
 gfxWindowsSurface::~gfxWindowsSurface()
 {
     if (mOwnsDC) {
