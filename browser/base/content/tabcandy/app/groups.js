cd2e1041445f381dca610e0a868d7dd281f9c4a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:20:34 +0000

    + merge
    UltraBlame original commit: f8a393a030899d2a8d128a5d0f95a8076edf4003

--- browser/base/content/tabcandy/app/groups.js
+++ browser/base/content/tabcandy/app/groups.js
@@ -385,10 +385,17 @@ window.Group.prototype = $.extend(new Item(), new Subscribable(), {
       
     var self = this;
     
+    var wasAlreadyInThisGroup = false;
+    var oldIndex = $.inArray(item, this._children);
+    if(oldIndex != -1) {
+      this._children.splice(oldIndex, 1); 
+      wasAlreadyInThisGroup = true;
+    }
+
     
     
     function findInsertionPoint(dropPos){
-      if(self._isStacked)
+      if(self.shouldStack(self._children.length + 1))
         return 0;
         
       var best = {dist: Infinity, item: null};
@@ -422,13 +429,6 @@ window.Group.prototype = $.extend(new Item(), new Subscribable(), {
       return 0;      
     }
     
-    var wasAlreadyInThisGroup = false;
-    var oldIndex = $.inArray(item, this._children);
-    if(oldIndex != -1) {
-      this._children.splice(oldIndex, 1); 
-      wasAlreadyInThisGroup = true;
-    }
-
     
     var index = findInsertionPoint(dropPos);
     this._children.splice( index, 0, item );
@@ -515,6 +515,13 @@ window.Group.prototype = $.extend(new Item(), new Subscribable(), {
   },
     
   
+  shouldStack: function(count) {
+    var bb = this.getContentBounds();
+    var result = !(count == 1 || (bb.width * bb.height) / count > 7000);          
+    return result;
+  },
+
+  
   arrange: function(options) {
     if(this.expanded)
       return;
@@ -524,7 +531,7 @@ window.Group.prototype = $.extend(new Item(), new Subscribable(), {
       return;
 
     var bb = this.getContentBounds();
-    if((bb.width * bb.height) / count > 7000) {
+    if(!this.shouldStack(count)) {
       this._children.forEach(function(child){
           child.removeClass("stacked")
       });
@@ -759,7 +766,7 @@ window.Group.prototype = $.extend(new Item(), new Subscribable(), {
       $(this.container).resizable({
         handles: "se",
         aspectRatio: false,
-        minWidth: 60,
+        minWidth: 90,
         minHeight: 90,
         resize: function(){
           self.reloadBounds();
@@ -1087,7 +1094,10 @@ window.Groups = {
   unregister: function(group) {
     var index = $.inArray(group, this.groups);
     if(index != -1)
-      this.groups.splice(index, 1);     
+      this.groups.splice(index, 1);  
+    
+    if(group == this._activeGroup)
+      this._activeGroup = null;   
   },
   
   
