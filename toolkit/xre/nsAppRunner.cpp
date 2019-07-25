d53d64b177e3586cb4f1cd528ec617a4155a1de1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:37:23 +0000

    Merge m-c --> cedar
    UltraBlame original commit: 1ac3633a3d31b8bd0e1fe5df3a8b3063ec46705a

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3274,7 +3274,7 @@ XRE_main(int argc, char* argv[], const nsXREAppData* aAppData)
       return 1;
     }
 
-#if defined(MOZ_UPDATER)
+#if defined(MOZ_UPDATER) && !defined(ANDROID)
   
   nsCOMPtr<nsIFile> updRoot;
   PRBool persistent;
