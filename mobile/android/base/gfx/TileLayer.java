c9cc5a1b36fa7fbb727e16f850d6f6072c66f6e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:01:19 +0000

    Bug 740061. Remove unused invalidate(rect). r=kats
    UltraBlame original commit: 3cd77601c2056b1959664972626e493a97d970ea

--- mobile/android/base/gfx/TileLayer.java
+++ mobile/android/base/gfx/TileLayer.java
@@ -95,15 +95,12 @@ public abstract class TileLayer extends Layer {
 
 
 
-    public void invalidate(Rect rect) {
-        if (!inTransaction())
-            throw new RuntimeException("invalidate() is only valid inside a transaction");
-        mDirtyRect.union(rect);
-    }
 
     public void invalidate() {
+        if (!inTransaction())
+            throw new RuntimeException("invalidate() is only valid inside a transaction");
         IntSize bufferSize = mImage.getSize();
-        invalidate(new Rect(0, 0, bufferSize.width, bufferSize.height));
+        mDirtyRect.union(new Rect(0, 0, bufferSize.width, bufferSize.height));
     }
 
     public boolean isDirty() {
