8afebff614359207daccd1e6f91553730c5d7ab6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:12:10 +0000

    Bug 354273 S/MIME signed emails include duplicate cert in chain. r=nelson
    UltraBlame original commit: c4f051d0bccfa9333f4735a3f173f68388c81204

--- security/manager/ssl/src/nsCMS.cpp
+++ security/manager/ssl/src/nsCMS.cpp
@@ -718,7 +718,12 @@ NS_IMETHODIMP nsCMSMessage::CreateSigned(nsIX509Cert* aSigningCert, nsIX509Cert*
       goto loser;
     }
 
-    if (NSS_CMSSignedData_AddCertificate(sigd, ecert) != SECSuccess) {
+    
+    PRBool addEncryptionCert =
+      (ecert && (!scert || !CERT_CompareCerts(ecert, scert)));
+
+    if (addEncryptionCert &&
+        NSS_CMSSignedData_AddCertificate(sigd, ecert) != SECSuccess) {
       PR_LOG(gPIPNSSLog, PR_LOG_DEBUG, ("nsCMSMessage::CreateSigned - can't add own encryption certificate\n"));
       goto loser;
     }
