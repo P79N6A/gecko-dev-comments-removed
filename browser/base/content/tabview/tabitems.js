a309f4f2644a746e4008ff0357af9d4aec98d6d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:35:43 +0000

    Bug 587351 - Close tabs with middle click in TabCandy [r=ian, a=beltzner]
    UltraBlame original commit: 1698f57029c4eb36a7e05b88bad716656e3a8eea

--- browser/base/content/tabview/tabitems.js
+++ browser/base/content/tabview/tabitems.js
@@ -188,9 +188,10 @@ function TabItem(tab, options) {
     if (!same)
       return;
 
-    if (iQ(e.target).hasClass("close"))
+    
+    if (iQ(e.target).hasClass("close") || e.button == 1) {
       self.close();
-    else {
+    } else {
       if (!Items.item(this).isDragging)
         self.zoomIn();
     }
