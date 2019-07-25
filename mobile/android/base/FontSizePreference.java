5436696a298f95f3675b748fdf42a5cdf76819f3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:33:30 +0000

    Bug 715179 - (3 of 8) Created a simple FontSizePreference dialog with a title and two buttons. r=bnicholson
    UltraBlame original commit: 719601a386ec8135c64ea6b192f733f991411b59

---
+++ mobile/android/base/FontSizePreference.java
@@ -0,0 +1,16 @@
+
+
+
+
+
+package org.mozilla.gecko;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+class FontSizePreference extends DialogPreference {
+    public FontSizePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
