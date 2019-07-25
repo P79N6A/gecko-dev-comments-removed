c6135c06c33cfce1bf124128d9a241d3e64c113a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:45:19 +0000

    Bug 649114 - Text-anchor middle is not computed correctly for element scaled to 0. r=jwatt
    UltraBlame original commit: f500e9a0253c4c16093d76c6eb27754a2b2bc094

--- layout/svg/base/src/nsSVGAFrame.cpp
+++ layout/svg/base/src/nsSVGAFrame.cpp
@@ -138,16 +138,10 @@ nsSVGAFrame::AttributeChanged(PRInt32         aNameSpaceID,
                               nsIAtom*        aAttribute,
                               PRInt32         aModType)
 {
-  if (aNameSpaceID != kNameSpaceID_None)
-    return NS_OK;
+  if (aNameSpaceID == kNameSpaceID_None &&
+      aAttribute == nsGkAtoms::transform) {
 
-  if (aAttribute == nsGkAtoms::transform) {
-    
-
-    
-    mCanvasTM = nsnull;
-    
-    nsSVGUtils::NotifyChildrenOfSVGChange(this, TRANSFORM_CHANGED);
+    NotifySVGChanged(TRANSFORM_CHANGED);
   }
 
  return NS_OK;
