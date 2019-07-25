9cfdd7bb8b396bc588a9c0bc9cc955cd14c38d54
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:38:35 +0000

    Bug 716295 part 4 - Interpose elfhack injected code in DT_INIT_ARRAY's first entry when possible. r=nfroyd
    UltraBlame original commit: 80caa5061f404adbcf6bf1897f73e1234273cbdb

--- build/unix/elfhack/elf.cpp
+++ build/unix/elfhack/elf.cpp
@@ -347,7 +347,7 @@ ElfDynamic_Section *Elf::getDynSection()
     return NULL;
 }
 
-void Elf::write(std::ofstream &file)
+void Elf::normalize()
 {
     
     
@@ -387,6 +387,11 @@ void Elf::write(std::ofstream &file)
     ehdr->e_shoff = shdr_section->getOffset();
     ehdr->e_entry = eh_entry.getValue();
     ehdr->e_shstrndx = eh_shstrndx->getIndex();
+}
+
+void Elf::write(std::ofstream &file)
+{
+    normalize();
     for (ElfSection *section = ehdr;
          section != NULL; section = section->getNext()) {
         file.seekp(section->getOffset());
