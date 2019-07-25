5d8ba91f8e5f23945e83572e0be81e6d92a217f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:30:46 +0000

    b=682625 pass VisualID when PictFormat is not available r=cjones
    UltraBlame original commit: 6571d631dd40922a6ba6b25dcd0ddb8460894ebd

--- gfx/layers/ipc/ShadowLayerUtilsX11.h
+++ gfx/layers/ipc/ShadowLayerUtilsX11.h
@@ -60,7 +60,8 @@ struct SurfaceDescriptorX11 {
 
   SurfaceDescriptorX11(gfxXlibSurface* aSurf);
 
-  SurfaceDescriptorX11(const int aXid, const int aXrenderPictID, const gfxIntSize& aSize);
+  SurfaceDescriptorX11(Drawable aDrawable, XID aFormatID,
+                       const gfxIntSize& aSize);
 
   
 
@@ -76,8 +77,8 @@ struct SurfaceDescriptorX11 {
   already_AddRefed<gfxXlibSurface> OpenForeign() const;
 
   Drawable mId;
+  XID mFormat; 
   gfxIntSize mSize;
-  PictFormat mFormat;
 };
 
 } 
