86b3daea04f34568c892794e19fa3b4a093740d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:45:13 +0000

    Add passing test Object-isSealed-01.js. no_r=me.
    UltraBlame original commit: 7cb52e5580ff1627e7ce118b220de3da4206ca2a

---
+++ js/src/jit-test/tests/debug/Object-isSealed-01.js
@@ -0,0 +1,32 @@
+
+
+var g = newGlobal("new-compartment");
+var dbg = new Debugger;
+var gw = dbg.addDebuggee(g);
+
+function test(code) {
+    g.eval("x = (" + code + ");");
+    var xw = gw.getOwnPropertyDescriptor("x").value;
+
+    function check() {
+	assertEq(xw.isSealed(), g.Object.isSealed(g.x));
+	assertEq(xw.isFrozen(), g.Object.isFrozen(g.x));
+	assertEq(xw.isExtensible(), g.Object.isExtensible(g.x));
+    }
+
+    check();
+    g.Object.preventExtensions(g.x);
+    check();
+    g.Object.seal(g.x);
+    check();
+    g.Object.freeze(g.x);
+    check();
+}
+
+test("{}");
+test("{a: [1]}");
+test("Object.create(null, {x: {value: 3}, y: {get: Math.min}})");
+test("[]");
+test("[,,,,,]");
+test("[0, 1, 2]");
+test("this");  
