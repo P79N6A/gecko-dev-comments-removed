380f32d552438e936eac38f5361a7e374d54acad
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:25:24 +0000

    Bug 759817 - Repair an inheritance issue, r=surkov
    UltraBlame original commit: 5387220c0609850b533bba269ed449d4b8f3eeb6

--- accessible/src/html/HTMLCanvasAccessible.h
+++ accessible/src/html/HTMLCanvasAccessible.h
@@ -3,10 +3,10 @@
 
 
 
-#include "HyperTextAccessible.h"
-
 #ifndef mozilla_a11y_HTMLCanvasAccessible_h__
 #define mozilla_a11y_HTMLCanvasAccessible_h__
+
+#include "HyperTextAccessibleWrap.h"
 
 namespace mozilla {
 namespace a11y {
@@ -14,11 +14,14 @@ namespace a11y {
 
 
 
-class HTMLCanvasAccessible : public HyperTextAccessible
+class HTMLCanvasAccessible : public HyperTextAccessibleWrap
 {
 public:
   HTMLCanvasAccessible(nsIContent* aContent, DocAccessible* aDoc);
   virtual ~HTMLCanvasAccessible() { }
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
 
   
   virtual a11y::role NativeRole();
