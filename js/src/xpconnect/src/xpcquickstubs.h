63ef686e712dc52ebe7c676fc695580e885cafba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:39:23 +0000

    Workaround for bug 564171 (Difference in behaviour between quickstubbed and non-quickstubbed readonly properties). r=jorendorff.
    UltraBlame original commit: e5a3822abe5aaeac6cc277407d788f440afaa96c

--- js/src/xpconnect/src/xpcquickstubs.h
+++ js/src/xpconnect/src/xpcquickstubs.h
@@ -143,6 +143,9 @@ xpc_qsThrowBadSetterValue(JSContext *cx, nsresult rv, JSObject *obj,
                           jsval propId);
 
 
+JSBool
+xpc_qsGetterOnlyPropertyStub(JSContext *cx, JSObject *obj, jsval id, jsval *vp);
+
 
 
 inline JSBool
