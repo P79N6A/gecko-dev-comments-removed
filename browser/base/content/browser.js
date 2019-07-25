39ffead7285d62105c9e003716be8303e0f5cf3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:50:04 +0000

    Bug 631298 - Status panel: use min-width for loading status messages only. r=gavin
    UltraBlame original commit: 5c3e3621f86eaf257e726af6c0d1e6767abcb7d7

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4157,16 +4157,22 @@ var XULBrowserWindow = {
   },
 
   updateStatusField: function () {
-    var text = this.overLink;
-    if (!text && this._busyUI)
-      text = this.status;
-    if (!text)
-      text = this.jsStatus || this.jsDefaultStatus || this.defaultStatus;
+    var text, type, types = ["overLink"];
+    if (this._busyUI)
+      types.push("status");
+    types.push("jsStatus", "jsDefaultStatus", "defaultStatus");
+    for (let i = 0; !text && i < types.length; i++) {
+      type = types[i];
+      text = this[type];
+    }
 
     
     
     if (this.statusText != text) {
-      this.statusTextField.label = text;
+      let field = this.statusTextField;
+      field.setAttribute("previoustype", field.getAttribute("type"));
+      field.setAttribute("type", type);
+      field.label = text;
       this.statusText = text;
     }
   },
