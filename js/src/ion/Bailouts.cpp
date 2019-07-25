5147d4077d21f4a88695c1ca034032ac3ad09afb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:53:03 +0000

    Paper over an assert that we run into when reading a register that hasn't been initialized (no bug, r=dvander)
    UltraBlame original commit: 4610d4c6ee208b710054c42dfbe3cfd2f27ba97d

--- js/src/ion/Bailouts.cpp
+++ js/src/ion/Bailouts.cpp
@@ -102,9 +102,19 @@ class IonBailoutIterator
           case SnapshotReader::DOUBLE_REG:
             return DoubleValue(in_.machine().readFloatReg(slot.floatReg()));
 
-          case SnapshotReader::TYPED_REG:
-            return FromTypedPayload(slot.knownType(), in_.machine().readReg(slot.reg()));
-
+          case SnapshotReader::TYPED_REG: {
+            uintptr_t reg = in_.machine().readReg(slot.reg());
+            JSValueType type = slot.knownType();
+#ifdef DEBUG
+            
+            
+            
+            
+            if (type == JSVAL_TYPE_OBJECT && bailoutKind() == Bailout_ArgumentCheck && reg == 0)
+                reg = 1;
+#endif
+            return FromTypedPayload(type, reg);
+          }
           case SnapshotReader::TYPED_STACK:
           {
             JSValueType type = slot.knownType();
