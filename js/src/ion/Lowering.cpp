f777b146cd571993a8fb8105c4bcd2d69ecaf9cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:46:53 +0000

    Rename IonLowering* to Lowering* (bug 677380 part 1, r=sstangl).
    UltraBlame original commit: 9af59f5ea4aa52e76c763a0f362f9b493af868c3

--- js/src/ion/Lowering.cpp
+++ js/src/ion/Lowering.cpp
@@ -40,8 +40,8 @@
 
 
 #include "IonLIR.h"
-#include "IonLowering.h"
-#include "IonLowering-inl.h"
+#include "Lowering.h"
+#include "Lowering-inl.h"
 #include "MIR.h"
 #include "MIRGraph.h"
 #include "jsbool.h"
