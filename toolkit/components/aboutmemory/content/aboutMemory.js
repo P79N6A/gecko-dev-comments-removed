da66883c81e46d98a4f8e865c7b5d3b0fd87e5f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:51:44 +0000

    Bug 702300 (part 6) - Move some code around in aboutMemory.js.  r=jschoenick.
    UltraBlame original commit: d5598ffb1aafdb5f5b7843461349096dd50b73b5

--- toolkit/components/aboutmemory/content/aboutMemory.js
+++ toolkit/components/aboutmemory/content/aboutMemory.js
@@ -42,8 +42,6 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
 
-const gVerbose = location.href === "about:memory?verbose";
-
 let gAddedObserver = false;
 
 const KIND_NONHEAP           = Ci.nsIMemoryReporter.KIND_NONHEAP;
@@ -54,8 +52,6 @@ const UNITS_COUNT            = Ci.nsIMemoryReporter.UNITS_COUNT;
 const UNITS_COUNT_CUMULATIVE = Ci.nsIMemoryReporter.UNITS_COUNT_CUMULATIVE;
 const UNITS_PERCENTAGE       = Ci.nsIMemoryReporter.UNITS_PERCENTAGE;
 
-const kUnknown = -1;    
-
 
 
 
@@ -64,6 +60,106 @@ function makeSafe(aUnsafeStr)
 {
   return aUnsafeStr.replace(/\\/g, '/');
 }
+
+function assert(aCond, aMsg)
+{
+  if (!aCond) {
+    throw("assertion failed: " + aMsg);
+  }
+}
+
+function debug(x)
+{
+  let content = document.getElementById("content");
+  appendElementWithText(content, "div", "legend", JSON.stringify(x));
+}
+
+
+
+function onLoad()
+{
+  let os = Cc["@mozilla.org/observer-service;1"].
+      getService(Ci.nsIObserverService);
+  os.notifyObservers(null, "child-memory-reporter-request", null);
+
+  os.addObserver(ChildMemoryListener, "child-memory-reporter-update", false);
+  gAddedObserver = true;
+
+  update();
+}
+
+function onUnload()
+{
+  
+  
+  
+  if (gAddedObserver) {
+    let os = Cc["@mozilla.org/observer-service;1"].
+        getService(Ci.nsIObserverService);
+    os.removeObserver(ChildMemoryListener, "child-memory-reporter-update");
+  }
+}
+
+
+
+
+function sendHeapMinNotifications()
+{
+  function runSoon(f)
+  {
+    let tm = Cc["@mozilla.org/thread-manager;1"]
+              .getService(Ci.nsIThreadManager);
+
+    tm.mainThread.dispatch({ run: f }, Ci.nsIThread.DISPATCH_NORMAL);
+  }
+
+  function sendHeapMinNotificationsInner()
+  {
+    let os = Cc["@mozilla.org/observer-service;1"]
+             .getService(Ci.nsIObserverService);
+    os.notifyObservers(null, "memory-pressure", "heap-minimize");
+
+    if (++j < 3)
+      runSoon(sendHeapMinNotificationsInner);
+    else
+      runSoon(update);
+  }
+
+  let j = 0;
+  sendHeapMinNotificationsInner();
+}
+
+
+
+function appendTextNode(aP, aText)
+{
+  let e = document.createTextNode(aText);
+  aP.appendChild(e);
+  return e;
+}
+
+function appendElement(aP, aTagName, aClassName)
+{
+  let e = document.createElement(aTagName);
+  if (aClassName) {
+    e.className = aClassName;
+  }
+  aP.appendChild(e);
+  return e;
+}
+
+function appendElementWithText(aP, aTagName, aClassName, aText)
+{
+  let e = appendElement(aP, aTagName, aClassName);
+  appendTextNode(e, aText);
+  return e;
+}
+
+
+
+const gVerbose = location.href === "about:memory?verbose";
+
+const kUnknown = -1;    
 
 const kTreeUnsafeDescriptions = {
   'explicit' :
@@ -127,29 +223,7 @@ const kTreeNames = {
 const kMapTreePaths =
   ['smaps/resident', 'smaps/pss', 'smaps/vsize', 'smaps/swap'];
 
-function onLoad()
-{
-  let os = Cc["@mozilla.org/observer-service;1"].
-      getService(Ci.nsIObserverService);
-  os.notifyObservers(null, "child-memory-reporter-request", null);
 
-  os.addObserver(ChildMemoryListener, "child-memory-reporter-update", false);
-  gAddedObserver = true;
-
-  update();
-}
-
-function onUnload()
-{
-  
-  
-  
-  if (gAddedObserver) {
-    let os = Cc["@mozilla.org/observer-service;1"].
-        getService(Ci.nsIObserverService);
-    os.removeObserver(ChildMemoryListener, "child-memory-reporter-update");
-  }
-}
 
 function ChildMemoryListener(aSubject, aTopic, aData)
 {
@@ -179,31 +253,85 @@ function doCC()
 
 
 
-function sendHeapMinNotifications()
+
+
+function update()
 {
-  function runSoon(f)
-  {
-    let tm = Cc["@mozilla.org/thread-manager;1"]
-              .getService(Ci.nsIThreadManager);
+  
+  
+  let oldContent = document.getElementById("content");
+  let content = oldContent.cloneNode(false);
+  oldContent.parentNode.replaceChild(content, oldContent);
+  content.classList.add(gVerbose ? 'verbose' : 'non-verbose');
 
-    tm.mainThread.dispatch({ run: f }, Ci.nsIThread.DISPATCH_NORMAL);
+  let mgr = Cc["@mozilla.org/memory-reporter-manager;1"].
+      getService(Ci.nsIMemoryReporterManager);
+
+  
+  
+  let reportsByProcess = getReportsByProcess(mgr);
+  let hasMozMallocUsableSize = mgr.hasMozMallocUsableSize;
+  appendProcessElements(content, "Main", reportsByProcess["Main"],
+                        hasMozMallocUsableSize);
+  for (let process in reportsByProcess) {
+    if (process !== "Main") {
+      appendProcessElements(content, process, reportsByProcess[process],
+                            hasMozMallocUsableSize);
+    }
   }
 
-  function sendHeapMinNotificationsInner()
-  {
-    let os = Cc["@mozilla.org/observer-service;1"]
-             .getService(Ci.nsIObserverService);
-    os.notifyObservers(null, "memory-pressure", "heap-minimize");
+  appendElement(content, "hr");
 
-    if (++j < 3)
-      runSoon(sendHeapMinNotificationsInner);
-    else
-      runSoon(update);
+  
+  const UpDesc = "Re-measure.";
+  const GCDesc = "Do a global garbage collection.";
+  const CCDesc = "Do a cycle collection.";
+  const MPDesc = "Send three \"heap-minimize\" notifications in a " +
+                 "row.  Each notification triggers a global garbage " +
+                 "collection followed by a cycle collection, and causes the " +
+                 "process to reduce memory usage in other ways, e.g. by " +
+                 "flushing various caches.";
+
+  function appendButton(aTitle, aOnClick, aText, aId)
+  {
+    let b = appendElementWithText(content, "button", "", aText);
+    b.title = aTitle;
+    b.onclick = aOnClick
+    if (aId) {
+      b.id = aId;
+    }
   }
 
-  let j = 0;
-  sendHeapMinNotificationsInner();
+  
+  appendButton(UpDesc, update,                   "Update", "updateButton");
+  appendButton(GCDesc, doGlobalGC,               "GC");
+  appendButton(CCDesc, doCC,                     "CC");
+  appendButton(MPDesc, sendHeapMinNotifications, "Minimize memory usage");
+
+  let div1 = appendElement(content, "div");
+  if (gVerbose) {
+    let a = appendElementWithText(div1, "a", "option", "Less verbose");
+    a.href = "about:memory";
+  } else {
+    let a = appendElementWithText(div1, "a", "option", "More verbose");
+    a.href = "about:memory?verbose";
+  }
+
+  let div2 = appendElement(content, "div");
+  let a = appendElementWithText(div2, "a", "option",
+                                "Troubleshooting information");
+  a.href = "about:support";
+
+  let legendText1 = "Click on a non-leaf node in a tree to expand ('++') " +
+                    "or collapse ('--') its children.";
+  let legendText2 = "Hover the pointer over the name of a memory report " +
+                    "to see a description of what it measures.";
+
+  appendElementWithText(content, "div", "legend", legendText1);
+  appendElementWithText(content, "div", "legend", legendText2);
 }
+
+
 
 function Report(aUnsafePath, aKind, aUnits, aAmount, aUnsafeDesc)
 {
@@ -303,108 +431,7 @@ function getReportsByProcess(aMgr)
   return reportsByProcess;
 }
 
-function appendTextNode(aP, aText)
-{
-  let e = document.createTextNode(aText);
-  aP.appendChild(e);
-  return e;
-}
 
-function appendElement(aP, aTagName, aClassName)
-{
-  let e = document.createElement(aTagName);
-  if (aClassName) {
-    e.className = aClassName;
-  }
-  aP.appendChild(e);
-  return e;
-}
-
-function appendElementWithText(aP, aTagName, aClassName, aText)
-{
-  let e = appendElement(aP, aTagName, aClassName);
-  appendTextNode(e, aText);
-  return e;
-}
-
-
-
-
-function update()
-{
-  
-  
-  let oldContent = document.getElementById("content");
-  let content = oldContent.cloneNode(false);
-  oldContent.parentNode.replaceChild(content, oldContent);
-  content.classList.add(gVerbose ? 'verbose' : 'non-verbose');
-
-  let mgr = Cc["@mozilla.org/memory-reporter-manager;1"].
-      getService(Ci.nsIMemoryReporterManager);
-
-  
-  
-  let reportsByProcess = getReportsByProcess(mgr);
-  let hasMozMallocUsableSize = mgr.hasMozMallocUsableSize;
-  appendProcessElements(content, "Main", reportsByProcess["Main"],
-                        hasMozMallocUsableSize);
-  for (let process in reportsByProcess) {
-    if (process !== "Main") {
-      appendProcessElements(content, process, reportsByProcess[process],
-                            hasMozMallocUsableSize);
-    }
-  }
-
-  appendElement(content, "hr");
-
-  
-  const UpDesc = "Re-measure.";
-  const GCDesc = "Do a global garbage collection.";
-  const CCDesc = "Do a cycle collection.";
-  const MPDesc = "Send three \"heap-minimize\" notifications in a " +
-                 "row.  Each notification triggers a global garbage " +
-                 "collection followed by a cycle collection, and causes the " +
-                 "process to reduce memory usage in other ways, e.g. by " +
-                 "flushing various caches.";
-
-  function appendButton(aTitle, aOnClick, aText, aId)
-  {
-    let b = appendElementWithText(content, "button", "", aText);
-    b.title = aTitle;
-    b.onclick = aOnClick
-    if (aId) {
-      b.id = aId;
-    }
-  }
-
-  
-  appendButton(UpDesc, update,                   "Update", "updateButton");
-  appendButton(GCDesc, doGlobalGC,               "GC");
-  appendButton(CCDesc, doCC,                     "CC");
-  appendButton(MPDesc, sendHeapMinNotifications, "Minimize memory usage");
-
-  let div1 = appendElement(content, "div");
-  if (gVerbose) {
-    let a = appendElementWithText(div1, "a", "option", "Less verbose");
-    a.href = "about:memory";
-  } else {
-    let a = appendElementWithText(div1, "a", "option", "More verbose");
-    a.href = "about:memory?verbose";
-  }
-
-  let div2 = appendElement(content, "div");
-  let a = appendElementWithText(div2, "a", "option",
-                                "Troubleshooting information");
-  a.href = "about:support";
-
-  let legendText1 = "Click on a non-leaf node in a tree to expand ('++') " +
-                    "or collapse ('--') its children.";
-  let legendText2 = "Hover the pointer over the name of a memory report " +
-                    "to see a description of what it measures.";
-
-  appendElementWithText(content, "div", "legend", legendText1);
-  appendElementWithText(content, "div", "legend", legendText2);
-}
 
 
 
@@ -1237,6 +1264,8 @@ function appendTreeElements(aPOuter, aT, aProcess)
   appendTextNode(aPOuter, "\n");  
 }
 
+
+
 function OtherReport(aUnsafePath, aUnits, aAmount, aUnsafeDesc, aNMerged)
 {
   
@@ -1345,15 +1374,3 @@ function appendSectionHeader(aP, aText)
   appendTextNode(aP, "\n");
 }
 
-function assert(aCond, aMsg)
-{
-  if (!aCond) {
-    throw("assertion failed: " + aMsg);
-  }
-}
-
-function debug(x)
-{
-  let content = document.getElementById("content");
-  appendElementWithText(content, "div", "legend", JSON.stringify(x));
-}
