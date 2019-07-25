359f98507e635337f752a443699a9ef5694ef87d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:10:09 +0000

    Bug 584565 - TM: When f.length is resolved at record time, we can fail to emit a shape guard. r=brendan.
    UltraBlame original commit: 324b0d7b7ded5dbe4e2f4b292bbaf460905d41b6

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -9443,7 +9443,12 @@ TraceRecorder::test_property_cache(JSObject* obj, LIns* obj_ins, JSObject*& obj2
     JS_PROPERTY_CACHE(cx).test(cx, pc, aobj, obj2, entry, atom);
     if (atom) {
         
+        
         jsid id = ATOM_TO_JSID(atom);
+
+        
+        forgetGuardedShapes();
+
         JSProperty* prop;
         if (JOF_OPMODE(*pc) == JOF_NAME) {
             JS_ASSERT(aobj == obj);
