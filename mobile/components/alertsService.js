b80d8c4c790e333c58e34903fece8cb37ce31e3e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:01:29 +0000

    Bug 479279: Fennec should have a system for browser-level notifications, r=gavin
    UltraBlame original commit: fa11593b9870150966f030c0790b8a228c74976c

---
+++ mobile/components/alertsService.js
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+
+
+
+
+function AlertsService() { }
+
+AlertsService.prototype = {
+  classDescription: "Alerts Service",
+  contractID: "@mozilla.org/alerts-service;1",
+  classID: Components.ID("{fe33c107-82a4-41d6-8c64-5353267e04c9}"),
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAlertsService]),
+
+  showAlertNotification: function(aImageUrl, aTitle, aText, aTextClickable, aCookie, aAlertListener, aName) {
+    let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+    let browser = wm.getMostRecentWindow("navigator:browser");
+    browser.AlertsHelper.showAlertNotification(aImageUrl, aTitle, aText, aTextClickable, aCookie, aAlertListener);
+  }
+};
+
+function NSGetModule(aCompMgr, aFileSpec) {
+  return XPCOMUtils.generateModule([AlertsService]);
+}
