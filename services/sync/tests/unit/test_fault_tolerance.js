7228a739643d55bff7cba389823af75f420907a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:20:02 +0000

    Added a trivial appender to faultTolerance.js.
    UltraBlame original commit: a09e007a36b28e4c153c90f1dfada64096a17831

--- services/sync/tests/unit/test_fault_tolerance.js
+++ services/sync/tests/unit/test_fault_tolerance.js
@@ -1,7 +1,14 @@
 function run_test() {
+  Components.utils.import("resource://weave/log4moz.js");
   Components.utils.import("resource://weave/faultTolerance.js");
 
   
   FaultTolerance.Service._testProperty = "hi";
   do_check_eq(FaultTolerance.Service._testProperty, "hi");
+
+  FaultTolerance.Service.init();
+
+  var log = Log4Moz.Service.rootLogger;
+  log.level = Log4Moz.Level.All;
+  log.info("Testing.");
 }
