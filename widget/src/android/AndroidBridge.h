697392c97885384e92b9936e62da8ee365f8dfec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:48:59 +0000

    merge back out of changeset 7e10dcf5f763 (bug 590225) due to bustage, a=me
    UltraBlame original commit: 60f4509b6e08b33bd095b194ed119e7d05fd57b7

--- widget/src/android/AndroidBridge.h
+++ widget/src/android/AndroidBridge.h
@@ -153,7 +153,10 @@ public:
                                nsIObserver *aAlertListener,
                                const nsAString& aAlertName);
 
-    void ShowFilePicker(nsAString& aFilePath);
+    void AlertsProgressListener_OnProgress(const nsAString& aAlertName,
+                                           PRInt64 aProgress,
+                                           PRInt64 aProgressMax,
+                                           const nsAString& aAlertText);
 
     struct AutoLocalJNIFrame {
         AutoLocalJNIFrame(int nEntries = 128) : mEntries(nEntries) {
@@ -215,6 +218,7 @@ protected:
     jmethodID jGetClipboardText;
     jmethodID jSetClipboardText;
     jmethodID jShowAlertNotification;
+    jmethodID jAlertsProgressListener_OnProgress;
     jmethodID jShowFilePicker;
 
     
