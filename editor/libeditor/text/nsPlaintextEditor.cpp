6efde710d131280c336d86710def25a5e55eeb66
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:54:37 +0000

    Bug 590554 - maxlength in textarea does not prevent newline characters from being inserted; r,a=roc
    UltraBlame original commit: d5f0467b491d339dc49a8f6fac84de308db8b8d2

--- editor/libeditor/text/nsPlaintextEditor.cpp
+++ editor/libeditor/text/nsPlaintextEditor.cpp
@@ -623,7 +623,12 @@ nsPlaintextEditor::GetTextSelectionOffsets(nsISelection *aSelection,
       }
     }
 #ifdef NS_DEBUG
-    ++nodeCount;
+    
+    
+    
+    if (!SameCOMIdentity(currentNode, rootNode)) {
+      ++nodeCount;
+    }
 #endif
   }
 
@@ -862,6 +867,7 @@ NS_IMETHODIMP nsPlaintextEditor::InsertLineBreak()
   shell->MaybeInvalidateCaretPosition();
 
   nsTextRulesInfo ruleInfo(nsTextEditRules::kInsertBreak);
+  ruleInfo.maxLength = mMaxTextLength;
   PRBool cancel, handled;
   res = mRules->WillDoAction(selection, &ruleInfo, &cancel, &handled);
   NS_ENSURE_SUCCESS(res, res);
