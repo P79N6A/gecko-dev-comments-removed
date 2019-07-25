9c8b43d753088c466d89d23e5a6c7a8e058aa4f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:23:23 +0000

    cleanup: rm iQ.inArray
    UltraBlame original commit: 1c2185e29433e88693568661cdee4afd91485cef

--- browser/base/content/tabcandy/core/iq.js
+++ browser/base/content/tabcandy/core/iq.js
@@ -977,22 +977,6 @@ iQ.extend({
 
   
   
-  inArray: function( elem, array ) {
-    if ( array.indexOf ) {
-      return array.indexOf( elem );
-    }
-
-    for ( var i = 0, length = array.length; i < length; i++ ) {
-      if ( array[ i ] === elem ) {
-        return i;
-      }
-    }
-
-    return -1;
-  },
-
-  
-  
   merge: function( first, second ) {
     var i = first.length, j = 0;
 
