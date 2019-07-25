b1912497ce5a0f10c5e6bcd4dd96ecf07e307378
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:10:37 +0000

    Bug 701735 - xpc_qsJsvalToWcharStr shouldn't const_cast; r=bz
    UltraBlame original commit: f5beb767294529015f4ecc8aa8d3091ae35d6882

--- js/xpconnect/src/XPCQuickStubs.cpp
+++ js/xpconnect/src/XPCQuickStubs.cpp
@@ -36,7 +36,6 @@
 
 
 
-
 #include "mozilla/Util.h"
 
 #include "jsapi.h"
@@ -1001,7 +1000,7 @@ xpc_qsJsvalToCharStr(JSContext *cx, jsval v, JSAutoByteString *bytes)
 }
 
 JSBool
-xpc_qsJsvalToWcharStr(JSContext *cx, jsval v, jsval *pval, PRUnichar **pstr)
+xpc_qsJsvalToWcharStr(JSContext *cx, jsval v, jsval *pval, const PRUnichar **pstr)
 {
     JSString *str;
 
@@ -1020,8 +1019,7 @@ xpc_qsJsvalToWcharStr(JSContext *cx, jsval v, jsval *pval, PRUnichar **pstr)
     if (!chars)
         return JS_FALSE;
 
-    
-    *pstr = const_cast<jschar *>(chars);
+    *pstr = static_cast<const PRUnichar *>(chars);
     return JS_TRUE;
 }
 
