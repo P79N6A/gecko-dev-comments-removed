f3ac04b310f059898107de3b4c36c59a3bfc7998
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:57:27 +0000

    bug 633773 - Use Google's HTTPS search by default; r=gavin
    UltraBlame original commit: 36fd3090b006063201a8b3f98f54e57db42e02a6

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -52,7 +52,7 @@
 
 
 
-pref("keyword.URL", "http://www.google.com/search?ie=UTF-8&oe=utf-8&q=");
+pref("keyword.URL", "https://www.google.com/search?ie=UTF-8&oe=utf-8&q=");
 pref("keyword.enabled", false);
 pref("general.useragent.locale", "chrome://global/locale/intl.properties");
 pref("general.useragent.compatMode.firefox", false);
