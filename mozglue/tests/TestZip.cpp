2882b37a7c428c79d0e4cd9a4cba276e7495cddc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:30:49 +0000

    Bug 683127 part 3 - Test for the Zip reader. r=tglek
    UltraBlame original commit: ea78ef72f47fe254cd6beea23425417af9d83490

---
+++ mozglue/tests/TestZip.cpp
@@ -0,0 +1,65 @@
+
+
+
+
+#include <cstdio>
+#include <unistd.h>
+#include "Zip.h"
+
+
+
+
+
+
+
+const char *test_entries[] = {
+  "baz", "foo", "bar", "qux"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const char *no_central_dir_entries[] = {
+  "a", "b", "c", "d"
+};
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    fprintf(stderr, "TEST-FAIL | TestZip | Expecting the directory containing test Zips\n");
+    return 1;
+  }
+  chdir(argv[1]);
+  Zip::Stream s;
+  Zip z("test.zip");
+  for (size_t i = 0; i < sizeof(test_entries) / sizeof(*test_entries); i++) {
+    if (!z.GetStream(test_entries[i], &s)) {
+      fprintf(stderr, "TEST-UNEXPECTED-FAIL | TestZip | test.zip: Couldn't get entry \"%s\"\n", test_entries[i]);
+      return 1;
+    }
+  }
+  fprintf(stderr, "TEST-PASS | TestZip | test.zip could be accessed fully\n");
+
+  Zip z2("no_central_dir.zip");
+  for (size_t i = 0; i < sizeof(no_central_dir_entries)
+                         / sizeof(*no_central_dir_entries); i++) {
+    if (!z2.GetStream(no_central_dir_entries[i], &s)) {
+      fprintf(stderr, "TEST-UNEXPECTED-FAIL | TestZip | no_central_dir.zip: Couldn't get entry \"%s\"\n", no_central_dir_entries[i]);
+      return 1;
+    }
+  }
+  fprintf(stderr, "TEST-PASS | TestZip | no_central_dir.zip could be accessed in order\n");
+
+  return 0;
+}
