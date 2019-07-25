e53e4175806c28880bb11f735905ee84bed7bffd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:52:04 +0000

    Backout changesets 58d026601240, c5a9a439d72c and cfdf675266b2 because of mochitest-3 crash
    UltraBlame original commit: cc0753a23f8b1411ca0780c7b018beb4166655bc

--- dom/workers/test/chromeWorker_worker.js
+++ dom/workers/test/chromeWorker_worker.js
@@ -6,11 +6,6 @@ if (!("ctypes" in self)) {
   throw "No ctypes!";
 }
 
-
-if (ctypes.toString() != "[object ctypes]") {
-  throw "Bad ctypes object: " + ctypes.toString();
-}
-
 onmessage = function(event) {
   let worker = new ChromeWorker("chromeWorker_subworker.js");
   worker.onmessage = function(event) {
