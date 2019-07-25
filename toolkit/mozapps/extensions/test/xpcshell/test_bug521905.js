a6de401fb6af60275afe21485f8cf34b6864ba67
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 06:53:18 +0000

    Bug 557269: Port old xpcshell tests to the new API. r=robstrong
    UltraBlame original commit: 41a9898e597acf28589e27ffe1397536496a2c08

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_bug521905.js
@@ -0,0 +1,86 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const ADDON = "test_bug521905";
+const ID = "bug521905@tests.mozilla.org";
+
+
+Services.prefs.setBoolPref("extensions.checkUpdateSecurity", false);
+
+Services.prefs.setBoolPref("extensions.checkCompatibility.2.0pre", false);
+
+function run_test() {
+  do_test_pending();
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "2.0pre", "2");
+
+  startupManager();
+  installAllFiles([do_get_addon(ADDON)], function() {
+    restartManager();
+
+    AddonManager.getAddon(ID, function(addon) {
+      do_check_neq(addon, null);
+      do_check_true(addon.isActive);
+
+      run_test_1();
+    });
+  });
+}
+
+function run_test_1() {
+  Services.prefs.setBoolPref("extensions.checkCompatibility.2.0pre", true);
+
+  restartManager();
+  AddonManager.getAddon(ID, function(addon) {
+    do_check_neq(addon, null);
+    do_check_false(addon.isActive);
+
+    run_test_2();
+  });
+}
+
+function run_test_2() {
+  Services.prefs.setBoolPref("extensions.checkCompatibility.2.0p", false);
+
+  restartManager();
+  AddonManager.getAddon(ID, function(addon) {
+    do_check_neq(addon, null);
+    do_check_false(addon.isActive);
+
+    do_test_finished();
+  });
+}
