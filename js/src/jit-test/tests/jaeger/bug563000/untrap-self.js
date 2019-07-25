da705d7308d8cdb51e75d43b0970294c62619413
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:48:18 +0000

    Fix bogus offset in test.
    UltraBlame original commit: 4d1446eeacb9811685298c4f4f36f83e0e70006a

--- js/src/jit-test/tests/jaeger/bug563000/untrap-self.js
+++ js/src/jit-test/tests/jaeger/bug563000/untrap-self.js
@@ -3,12 +3,13 @@ setDebug(true);
 x = "notset";
 function main() {
   
-  untrap(main, 22);
+  untrap(main, 24);
   x = "success";
 }
+dis(main);
 function failure() { x = "failure"; }
 
 
-trap(main, 22, "failure()");
+trap(main, 24, "failure()");
 main();
 assertEq(x, "success");
