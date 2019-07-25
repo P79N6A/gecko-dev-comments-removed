782a6189e85ee9f74fc7e7aac011c23d2c868f68
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:32:24 +0000

    Bug 713260 - Add a Tilt test for content location changes. r=rcampbell
    UltraBlame original commit: 027cfe0bb45e603c506060d994c8a5b98e3ea3ed

---
+++ browser/devtools/tilt/test/browser_tilt_05_destruction-url.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+"use strict";
+
+function test() {
+  if (!isTiltEnabled()) {
+    info("Skipping destruction test because Tilt isn't enabled.");
+    return;
+  }
+  if (!isWebGLSupported()) {
+    info("Skipping destruction test because WebGL isn't supported.");
+    return;
+  }
+
+  waitForExplicitFinish();
+
+  createTab(function() {
+    createTilt({
+      onTiltOpen: function()
+      {
+        Services.obs.addObserver(cleanup, TILT_DESTROYED, false);
+        window.content.location = "about:mozilla";
+      }
+    });
+  });
+}
+
+function cleanup() {
+  let id = TiltUtils.getWindowId(gBrowser.selectedBrowser.contentWindow);
+
+  is(Tilt.visualizers[id], null,
+    "The current instance of the visualizer wasn't destroyed properly.");
+
+  Services.obs.removeObserver(cleanup, TILT_DESTROYED);
+  gBrowser.removeCurrentTab();
+  finish();
+}
