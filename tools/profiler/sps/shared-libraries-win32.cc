3512a344442134dd6aa19c68cca615c5a74037dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:27:31 +0000

    Bug 711491. Rough out SharedLibraryInfo for win32. r=bgirard
    UltraBlame original commit: d09abc8c361d006a2d1f1bb28ebb1aad136d182d

---
+++ tools/profiler/sps/shared-libraries-win32.cc
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <windows.h>
+#include <tlhelp32.h>
+
+#include "shared-libraries.h"
+
+SharedLibraryInfo SharedLibraryInfo::GetInfoForSelf()
+{
+  SharedLibraryInfo sharedLibraryInfo;
+
+  
+
+
+
+
+
+
+
+
+
+  return sharedLibraryInfo;
+}
+
