702cd0ff56ac775f547156f1ce2966c3013f44f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:49:35 +0000

    Bug 564535 - permission manager needs to be remoted [r=dwitte]
    UltraBlame original commit: 9944d9dd6aafab90f3dd7175dd7aa3edc41c2256

--- dom/ipc/ContentProcessParent.h
+++ dom/ipc/ContentProcessParent.h
@@ -47,6 +47,8 @@
 #include "nsIObserver.h"
 #include "nsIThreadInternal.h"
 #include "mozilla/Monitor.h"
+#include "nsIPrefService.h"
+#include "nsIPermissionManager.h"
 
 namespace mozilla {
 
@@ -67,7 +69,7 @@ private:
     typedef mozilla::ipc::TestShellParent TestShellParent;
 
 public:
-    static ContentProcessParent* GetSingleton();
+    static ContentProcessParent* GetSingleton(PRBool aForceNew = PR_TRUE);
 
 #if 0
     
@@ -83,7 +85,10 @@ public:
     TestShellParent* CreateTestShell();
     bool DestroyTestShell(TestShellParent* aTestShell);
 
+    void ReportChildAlreadyBlocked();
     bool RequestRunToCompletion();
+
+    bool IsAlive();
 
 protected:
     virtual void ActorDestroy(ActorDestroyReason why);
@@ -108,13 +113,49 @@ private:
     virtual PNeckoParent* AllocPNecko();
     virtual bool DeallocPNecko(PNeckoParent* necko);
 
+    virtual bool RecvGetPrefType(const nsCString& prefName,
+            PRInt32* retValue, nsresult* rv);
+
+    virtual bool RecvGetBoolPref(const nsCString& prefName,
+            PRBool* retValue, nsresult* rv);
+
+    virtual bool RecvGetIntPref(const nsCString& prefName,
+            PRInt32* retValue, nsresult* rv);
+
+    virtual bool RecvGetCharPref(const nsCString& prefName,
+            nsCString* retValue, nsresult* rv);
+
+    virtual bool RecvGetPrefLocalizedString(const nsCString& prefName,
+            nsString* retValue, nsresult* rv);
+
+    virtual bool RecvPrefHasUserValue(const nsCString& prefName,
+            PRBool* retValue, nsresult* rv);
+
+    virtual bool RecvPrefIsLocked(const nsCString& prefName,
+            PRBool* retValue, nsresult* rv);
+
+    virtual bool RecvGetChildList(const nsCString& domain,
+            nsTArray<nsCString>* list, nsresult* rv);
+
+    virtual bool RecvTestPermission(const IPC::URI&  aUri,
+                                    const nsCString& aType,
+                                    const PRBool&    aExact,
+                                    PRUint32*        retValue);
+
+    void EnsurePrefService();
+    void EnsurePermissionService();
+
     mozilla::Monitor mMonitor;
 
     GeckoChildProcessHost* mSubprocess;
 
     int mRunToCompletionDepth;
+    bool mShouldCallUnblockChild;
     nsCOMPtr<nsIThreadObserver> mOldObserver;
 
+    bool mIsAlive;
+    nsCOMPtr<nsIPrefBranch> mPrefService; 
+    nsCOMPtr<nsIPermissionManager> mPermissionService; 
 };
 
 } 
