7c71e94b13a5399232a6d1f47bdf1b2b582b5bd6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:54:12 +0000

    Bug 732363 - Avoid oranges in test_net_addr.js by retaining references to the transport and output stream. r=jdm
    UltraBlame original commit: c39fd8e1fb45dce6023324f0615bee32cec80321

--- netwerk/test/unit/test_net_addr.js
+++ netwerk/test/unit/test_net_addr.js
@@ -124,19 +124,30 @@ var connectTimeout = 5*1000;
 
 
 
+
+
+
+var testDataStore = null;
+
+
+
+
 function testIpv4() {
-  var transport;
+  testDataStore = {
+    transport : null ,
+    ouput : null
+  }
 
   serv.acceptCallback = function() {
     
     serv.timeoutCallback = function(){};
 
-    var selfAddr = transport.getScriptableSelfAddr();
-    var peerAddr = transport.getScriptablePeerAddr();
+    var selfAddr = testDataStore.transport.getScriptableSelfAddr();
+    var peerAddr = testDataStore.transport.getScriptablePeerAddr();
 
     
     do_check_eq(peerAddr.family, Ci.nsINetAddr.FAMILY_INET);
-    do_check_eq(peerAddr.port, transport.port);
+    do_check_eq(peerAddr.port, testDataStore.transport.port);
     do_check_eq(peerAddr.port, serv.port);
     do_check_eq(peerAddr.address, "127.0.0.1");
 
@@ -148,6 +159,7 @@ function testIpv4() {
     checkAddrEqual(selfAddr, serv.peerAddr);
     checkAddrEqual(peerAddr, serv.selfAddr);
 
+    testDataStore = null;
     do_execute_soon(run_next_test);
   };
 
@@ -158,8 +170,13 @@ function testIpv4() {
 
 
 
-  transport = sts.createTransport(null, 0, '127.0.0.1', serv.port, null);
-  transport.openOutputStream(Ci.nsITransport.OPEN_BLOCKING,0,0);
+  testDataStore.transport = sts.createTransport(null, 0, '127.0.0.1', serv.port, null);
+  
+
+
+
+  testDataStore.output = testDataStore.transport.openOutputStream(Ci.nsITransport.OPEN_BLOCKING,0,0);
+
   
 
 
