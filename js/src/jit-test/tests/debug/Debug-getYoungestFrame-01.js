64408723ae3ab420b5cc147412474d4dba9be541
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:37:26 +0000

    Add Debug.prototype.getYoungestFrame.
    UltraBlame original commit: 4efc9b37f30aac73f6092c231c322892074a12c7

---
+++ js/src/jit-test/tests/debug/Debug-getYoungestFrame-01.js
@@ -0,0 +1,20 @@
+
+
+
+load(libdir + "asserts.js");
+
+var g = newGlobal('new-compartment');
+var dbg = new Debug(g);
+assertEq(dbg.getYoungestFrame(), null);
+
+var frame;
+function f() {
+    frame = dbg.getYoungestFrame();
+    assertEq(frame instanceof Debug.Frame, true);
+    assertEq(frame.type, "eval");
+    assertEq(frame.older, null);
+}
+g.h = this;
+g.eval("h.f()");
+assertEq(frame.live, false);
+assertThrowsInstanceOf(function () { frame.older; }, Error);
