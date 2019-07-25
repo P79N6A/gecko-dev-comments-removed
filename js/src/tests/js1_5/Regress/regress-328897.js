f43796ce3c9228c430b29251019552eab2dc604b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:40:01 +0000

    Bug 783031 - Make js1_5/Regress/regress-328897.js throw a different way. r=jmaher
    UltraBlame original commit: 8fd107240fad02c4f3255577988d508e0c589dbb

--- js/src/tests/js1_5/Regress/regress-328897.js
+++ js/src/tests/js1_5/Regress/regress-328897.js
@@ -19,7 +19,7 @@ if (typeof window == 'undefined')
 }
 else
 {
-  expect = /(Script error.|Permission denied to access property 'classes')/;
+  expect = /NS_ERROR_XPC_NOT_ENOUGH_ARGS/;
 
   window._onerror = window.onerror;
   window.onerror = (function (msg, page, line) { 
@@ -32,7 +32,7 @@ else
   gDelayTestDriverEnd = true;
 
   
-  window.location="javascript:Components.classes = 42";
+  window.alert();
   actual = 'No Error';
 }
 
