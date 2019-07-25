658b022c2de723166a9f564e3fbd622273c9055d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:52:03 +0000

    Fixed bug 672641, implemented CharTokenizer which takes arbitrary char as delimiter. r=sicking
    UltraBlame original commit: edb32fa29bca558a5afc8c9afb363fbc98873fe0

---
+++ xpcom/ds/CharTokenizer.h
@@ -0,0 +1,126 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_CharTokenizer_h
+#define mozilla_CharTokenizer_h
+
+#include "nsDependentSubstring.h"
+
+namespace mozilla {
+
+template<PRUnichar delimiter>
+class CharTokenizer
+{
+public:
+    CharTokenizer(const nsSubstring& aSource)
+    {
+      aSource.BeginReading(mIter);
+      aSource.EndReading(mEnd);
+    }
+
+    
+
+
+    PRBool hasMoreTokens()
+    {
+      return mIter != mEnd;
+    }
+
+    
+
+
+    const nsDependentSubstring nextToken()
+    {
+      nsSubstring::const_char_iterator begin = mIter;
+      while (mIter != mEnd && (*mIter) != delimiter) {
+        ++mIter;
+      }
+      nsSubstring::const_char_iterator end = mIter;
+      if (mIter != mEnd) {
+        ++mIter;
+      }
+
+      return Substring(begin, end);
+    }
+
+private:
+    nsSubstring::const_char_iterator mIter, mEnd;
+};
+
+template<char delimiter>
+class CCharTokenizer
+{
+public:
+    CCharTokenizer(const nsCSubstring& aSource)
+    {
+      aSource.BeginReading(mIter);
+      aSource.EndReading(mEnd);
+    }
+
+    
+
+
+    PRBool hasMoreTokens()
+    {
+      return mIter != mEnd;
+    }
+
+    
+
+
+    const nsDependentCSubstring nextToken()
+    {
+      nsCSubstring::const_char_iterator begin = mIter;
+      while (mIter != mEnd && (*mIter) != delimiter) {
+        ++mIter;
+      }
+      nsCSubstring::const_char_iterator end = mIter;
+      if (mIter != mEnd) {
+        ++mIter;
+      }
+
+      return Substring(begin, end);
+    }
+
+private:
+    nsCSubstring::const_char_iterator mIter, mEnd;
+};
+
+} 
+
+#endif 
