0191a58e83ff6705f86048639876d5cfd5a040cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:35:21 +0000

    Bug 586142 - Copying text in the Web Console doesn't insert newlines properly; f=ddahl r=dietrich a=dietrich
    UltraBlame original commit: 9d2768de4f911cf98461bbcfd32cd66403a2588f

---
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_bug_586142_insert_newlines.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+const TEST_URI = "http://example.com/";
+
+XPCOMUtils.defineLazyGetter(this, "HUDService", function () {
+  Cu.import("resource://gre/modules/HUDService.jsm");
+  return HUDService;
+});
+
+function test()
+{
+  waitForExplicitFinish();
+  gBrowser.selectedTab = gBrowser.addTab(TEST_URI);
+  gBrowser.selectedBrowser.addEventListener("DOMContentLoaded", onLoad, false);
+}
+
+function onLoad() {
+  gBrowser.selectedBrowser.removeEventListener("DOMContentLoaded", onLoad,
+                                               false);
+  executeSoon(testNewlines);
+}
+
+function testNewlines() {
+  HUDService.activateHUDForContext(gBrowser.selectedTab);
+  let hudId = HUDService.displaysIndex()[0];
+  ok(hudId != null, "we have the HUD ID");
+
+  HUDService.clearDisplay(hudId);
+
+  let contentWindow = gBrowser.selectedTab.linkedBrowser.contentWindow;
+  let console = contentWindow.wrappedJSObject.console;
+  ok(console != null, "we have the console object");
+
+  for (let i = 0; i < 20; i++) {
+    console.log("Hello world!");
+  }
+
+  let hudNode = HUDService.getOutputNodeById(hudId);
+  let outputNode = hudNode.querySelector(".hud-output-node");
+  ok(outputNode != null, "we have the output node");
+
+  let labels = outputNode.querySelectorAll("label");
+  is(labels.length, 20, "we found 20 labels in the output node");
+
+  for (let i = 0; i < labels.length; i++) {
+    let value = labels[i].textContent;
+    is(value[value.length - 1], "\n", "the value of label " + i + " ends " +
+       "with a newline");
+  }
+
+  HUDService.deactivateHUDForContext(gBrowser.selectedTab);
+  gBrowser.removeCurrentTab();
+  finish();
+}
+
