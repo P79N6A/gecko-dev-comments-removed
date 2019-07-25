48e2e30eaa9fbe9538c28730035b811dd0f4cff8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:25:27 +0000

    Bug 606966 - Need an async history visit API exposed to JS Part 21 - Track and notify about title changes. r=mak a=blocking
    UltraBlame original commit: 2f9e83d546d31e54235c91d3dc0f9b74bf6be293

--- toolkit/components/places/src/History.cpp
+++ toolkit/components/places/src/History.cpp
@@ -91,6 +91,7 @@ struct VisitData {
   , typed(false)
   , transitionType(PR_UINT32_MAX)
   , visitTime(0)
+  , titleChanged(false)
   {
     guid.SetIsVoid(PR_TRUE);
     title.SetIsVoid(PR_TRUE);
@@ -105,6 +106,7 @@ struct VisitData {
   , typed(false)
   , transitionType(PR_UINT32_MAX)
   , visitTime(0)
+  , titleChanged(false)
   {
     (void)aURI->GetSpec(spec);
     (void)GetReversedHostname(aURI, revHost);
@@ -165,6 +167,9 @@ struct VisitData {
   PRTime visitTime;
   nsString title;
   nsCString referrerSpec;
+
+  
+  bool titleChanged;
 };
 
 
@@ -225,8 +230,14 @@ GetStringFromJSObject(JSContext* aCtx,
 {
   jsval val;
   JSBool rc = JS_GetProperty(aCtx, aObject, aProperty, &val);
-  if (!rc || JSVAL_IS_VOID(val) || !JSVAL_IS_STRING(val)) {
+  if (!rc || JSVAL_IS_VOID(val) ||
+      !(JSVAL_IS_NULL(val) || JSVAL_IS_STRING(val))) {
     _string.SetIsVoid(PR_TRUE);
+    return;
+  }
+  
+  if (JSVAL_IS_NULL(val)) {
+    _string.Truncate();
     return;
   }
   size_t length;
@@ -471,6 +482,45 @@ private:
 
 
 
+class NotifyTitleObservers : public nsRunnable
+{
+public:
+  
+
+
+
+
+
+
+
+  NotifyTitleObservers(const nsCString& aSpec,
+                       const nsString& aTitle)
+  : mSpec(aSpec)
+  , mTitle(aTitle)
+  {
+  }
+
+  NS_IMETHOD Run()
+  {
+    NS_PRECONDITION(NS_IsMainThread(),
+                    "This should be called on the main thread");
+
+    nsNavHistory* navHistory = nsNavHistory::GetHistoryService();
+    NS_ENSURE_TRUE(navHistory, NS_ERROR_OUT_OF_MEMORY);
+    nsCOMPtr<nsIURI> uri;
+    (void)NS_NewURI(getter_AddRefs(uri), mSpec);
+    navHistory->NotifyTitleChange(uri, mTitle);
+
+    return NS_OK;
+  }
+private:
+  const nsCString mSpec;
+  const nsString mTitle;
+};
+
+
+
+
 class NotifyCompletion : public nsRunnable
 {
 public:
@@ -643,6 +693,13 @@ public:
       nsCOMPtr<nsIRunnable> event = new NotifyVisitObservers(place, referrer);
       rv = NS_DispatchToMainThread(event);
       NS_ENSURE_SUCCESS(rv, rv);
+
+      
+      if ((!known && !place.title.IsVoid()) || place.titleChanged) {
+        event = new NotifyTitleObservers(place.spec, place.title);
+        rv = NS_DispatchToMainThread(event);
+        NS_ENSURE_SUCCESS(rv, rv);
+      }
 
       lastPlace = &mPlaces.ElementAt(i);
     }
@@ -1012,48 +1069,6 @@ private:
 
 
 
-class NotifyTitleObservers : public nsRunnable
-{
-public:
-  
-
-
-
-
-
-
-
-  NotifyTitleObservers(const nsCString& aSpec,
-                       const nsString& aTitle)
-  : mSpec(aSpec)
-  , mTitle(aTitle)
-  {
-    NS_PRECONDITION(!NS_IsMainThread(),
-                    "This should not be called on the main thread");
-  }
-
-  NS_IMETHOD Run()
-  {
-    NS_PRECONDITION(NS_IsMainThread(),
-                    "This should be called on the main thread");
-
-    nsNavHistory* navHistory = nsNavHistory::GetHistoryService();
-    NS_ENSURE_TRUE(navHistory, NS_ERROR_OUT_OF_MEMORY);
-    nsCOMPtr<nsIURI> uri;
-    (void)NS_NewURI(getter_AddRefs(uri), mSpec);
-    navHistory->NotifyTitleChange(uri, mTitle);
-
-    return NS_OK;
-  }
-private:
-  const nsCString mSpec;
-  const nsString mTitle;
-};
-
-
-
-
-
 class SetPageTitle : public nsRunnable
 {
 public:
@@ -1069,7 +1084,7 @@ public:
 
   static nsresult Start(mozIStorageConnection* aConnection,
                         nsIURI* aURI,
-                        const nsString& aTitle)
+                        const nsAString& aTitle)
   {
     NS_PRECONDITION(NS_IsMainThread(),
                     "This should be called on the main thread");
@@ -1097,7 +1112,7 @@ public:
 
     
     bool exists = mHistory->FetchPageInfo(mPlace);
-    if (!exists) {
+    if (!exists || !mPlace.titleChanged) {
       
       
       return NS_OK;
@@ -1105,13 +1120,6 @@ public:
 
     NS_ASSERTION(mPlace.placeId > 0,
                  "We somehow have an invalid place id here!");
-
-    
-    
-    if (mTitle.Equals(mPlace.title) ||
-        (mTitle.IsVoid() && mPlace.title.IsVoid())) {
-      return NS_OK;
-    }
 
     
     nsCOMPtr<mozIStorageStatement> stmt =
@@ -1127,19 +1135,22 @@ public:
       nsresult rv = stmt->BindInt64ByName(NS_LITERAL_CSTRING("page_id"),
                                           mPlace.placeId);
       NS_ENSURE_SUCCESS(rv, rv);
-      if (mTitle.IsVoid()) {
+      
+      
+      if (mPlace.title.IsEmpty()) {
         rv = stmt->BindNullByName(NS_LITERAL_CSTRING("page_title"));
       }
       else {
         rv = stmt->BindStringByName(NS_LITERAL_CSTRING("page_title"),
-                                    StringHead(mTitle, TITLE_LENGTH_MAX));
+                                    StringHead(mPlace.title, TITLE_LENGTH_MAX));
       }
       NS_ENSURE_SUCCESS(rv, rv);
       rv = stmt->Execute();
       NS_ENSURE_SUCCESS(rv, rv);
     }
 
-    nsCOMPtr<nsIRunnable> event = new NotifyTitleObservers(mPlace.spec, mTitle);
+    nsCOMPtr<nsIRunnable> event =
+      new NotifyTitleObservers(mPlace.spec, mPlace.title);
     nsresult rv = NS_DispatchToMainThread(event);
     NS_ENSURE_SUCCESS(rv, rv);
 
@@ -1148,15 +1159,14 @@ public:
 
 private:
   SetPageTitle(const nsCString& aSpec,
-               const nsString& aTitle)
-  : mTitle(aTitle)
-  , mHistory(History::GetService())
+               const nsAString& aTitle)
+  : mHistory(History::GetService())
   {
     mPlace.spec = aSpec;
+    mPlace.title = aTitle;
   }
 
   VisitData mPlace;
-  const nsString mTitle;
 
   
 
@@ -1331,7 +1341,8 @@ History::InsertPlace(const VisitData& aPlace)
   NS_ENSURE_SUCCESS(rv, rv);
   rv = URIBinder::Bind(stmt, NS_LITERAL_CSTRING("url"), aPlace.spec);
   NS_ENSURE_SUCCESS(rv, rv);
-  if (aPlace.title.IsVoid()) {
+  
+  if (aPlace.title.IsEmpty()) {
     rv = stmt->BindNullByName(NS_LITERAL_CSTRING("title"));
   }
   else {
@@ -1375,11 +1386,15 @@ History::UpdatePlace(const VisitData& aPlace)
   mozStorageStatementScoper scoper(stmt);
 
   nsresult rv;
-  if (!aPlace.title.IsVoid()) {
+  
+  if (aPlace.title.IsEmpty()) {
+    rv = stmt->BindNullByName(NS_LITERAL_CSTRING("title"));
+  }
+  else {
     rv = stmt->BindStringByName(NS_LITERAL_CSTRING("title"),
                                 StringHead(aPlace.title, TITLE_LENGTH_MAX));
-    NS_ENSURE_SUCCESS(rv, rv);
   }
+  NS_ENSURE_SUCCESS(rv, rv);
   rv = stmt->BindInt32ByName(NS_LITERAL_CSTRING("typed"), aPlace.typed);
   NS_ENSURE_SUCCESS(rv, rv);
   rv = stmt->BindInt32ByName(NS_LITERAL_CSTRING("hidden"), aPlace.hidden);
@@ -1423,9 +1438,16 @@ History::FetchPageInfo(VisitData& _place)
   rv = stmt->GetInt64(0, &_place.placeId);
   NS_ENSURE_SUCCESS(rv, false);
 
+  nsAutoString title;
+  rv = stmt->GetString(1, title);
+  NS_ENSURE_SUCCESS(rv, true);
+
+  
+  
+  _place.titleChanged = !(_place.title.Equals(title) ||
+                          (_place.title.IsVoid() && title.IsVoid()));
   if (_place.title.IsVoid()) {
-    rv = stmt->GetString(1, _place.title);
-    NS_ENSURE_SUCCESS(rv, true);
+    _place.title = title;
   }
 
   if (_place.hidden) {
@@ -1776,18 +1798,10 @@ History::SetURITitle(nsIURI* aURI, const nsAString& aTitle)
     return NS_OK;
   }
 
-  nsAutoString title;
-  if (aTitle.IsEmpty()) {
-    title.SetIsVoid(PR_TRUE);
-  }
-  else {
-    title.Assign(aTitle);
-  }
-
   mozIStorageConnection* dbConn = GetDBConn();
   NS_ENSURE_STATE(dbConn);
 
-  rv = SetPageTitle::Start(dbConn, aURI, title);
+  rv = SetPageTitle::Start(dbConn, aURI, aTitle);
   NS_ENSURE_SUCCESS(rv, rv);
 
   return NS_OK;
