f6e17d0119af712c31e72a44e2df6a2f5f4fdf1b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:02:14 +0000

    Bug 718938 - Add RAII helper to mfbt. r=waldo,luke,cjones
    UltraBlame original commit: 5cc8dc1f19ec562ff36c25bb09488478c3f71cf5

---
+++ mfbt/Scoped.h
@@ -0,0 +1,224 @@
+
+
+
+
+
+
+#ifndef mozilla_Scoped_h_
+#define mozilla_Scoped_h_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "mozilla/Attributes.h"
+#include "mozilla/GuardObjects.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+template <typename Traits>
+class Scoped
+{
+public:
+  typedef typename Traits::type Resource;
+
+  explicit Scoped(MOZ_GUARD_OBJECT_NOTIFIER_ONLY_PARAM)
+    : value(Traits::empty())
+  {
+    MOZ_GUARD_OBJECT_NOTIFIER_INIT;
+  }
+  explicit Scoped(const Resource& value
+                  MOZ_GUARD_OBJECT_NOTIFIER_PARAM)
+    : value(value)
+  {
+    MOZ_GUARD_OBJECT_NOTIFIER_INIT;
+  }
+  ~Scoped() {
+    Traits::release(value);
+  }
+
+  
+  operator const Resource&() const { return value; }
+  const Resource& operator->() const { return value; }
+  const Resource& get() const { return value; }
+  
+  Resource& rwget() { return value; }
+
+  
+
+
+
+
+
+
+
+
+  Resource forget() {
+    Resource tmp = value;
+    value = Traits::empty();
+    return tmp;
+  }
+
+  
+
+
+
+
+  void dispose() {
+    Traits::release(value);
+    value = Traits::empty();
+  }
+
+  bool operator==(const Resource& other) const {
+    return value == other;
+  }
+
+  
+
+
+
+
+
+
+
+  Scoped<Traits>& operator=(const Resource& other) {
+    return reset(other);
+  }
+  Scoped<Traits>& reset(const Resource& other) {
+    Traits::release(value);
+    value = other;
+    return *this;
+  }
+
+private:
+  explicit Scoped(const Scoped<Traits>& value) MOZ_DELETE;
+  Scoped<Traits>& operator=(const Scoped<Traits>& value) MOZ_DELETE;
+
+private:
+  Resource value;
+  MOZ_DECL_USE_GUARD_OBJECT_NOTIFIER
+};
+
+
+
+
+
+
+
+
+
+#define SCOPED_TEMPLATE(name, Traits)                          \
+template <typename Type>                                       \
+struct name : public Scoped<Traits<Type> >                     \
+{                                                              \
+  typedef Scoped<Traits<Type> > Super;                         \
+  typedef typename Super::Resource Resource;                   \
+  name& operator=(Resource ptr) {                              \
+    Super::operator=(ptr);                                     \
+    return *this;                                              \
+  }                                                            \
+  explicit name(MOZ_GUARD_OBJECT_NOTIFIER_ONLY_PARAM)          \
+    : Super(MOZ_GUARD_OBJECT_NOTIFIER_ONLY_PARAM_TO_PARENT)    \
+  {}                                                           \
+  explicit name(Resource ptr                                   \
+                MOZ_GUARD_OBJECT_NOTIFIER_PARAM)               \
+    : Super(ptr MOZ_GUARD_OBJECT_NOTIFIER_PARAM_TO_PARENT)     \
+  {}                                                           \
+private:                                                       \
+  explicit name(name& source) MOZ_DELETE;                      \
+  name& operator=(name& source) MOZ_DELETE;                    \
+};
+
+
+
+
+
+
+
+
+template <typename T>
+struct ScopedFreePtrTraits
+{
+  typedef T* type;
+  static T* empty() { return NULL; }
+  static void release(T* ptr) { free(ptr); }
+};
+SCOPED_TEMPLATE(ScopedFreePtr, ScopedFreePtrTraits)
+
+
+
+
+
+
+
+template <typename T>
+struct ScopedDeletePtrTraits : public ScopedFreePtrTraits<T> {
+  static void release(T* ptr) { delete ptr; }
+};
+SCOPED_TEMPLATE(ScopedDeletePtr, ScopedDeletePtrTraits)
+
+
+
+
+
+
+
+template <typename T>
+struct ScopedDeleteArrayTraits : public ScopedFreePtrTraits<T>
+{
+  static void release(T* ptr) { delete [] ptr; }
+};
+SCOPED_TEMPLATE(ScopedDeleteArray, ScopedDeleteArrayTraits)
+
+
+#endif 
