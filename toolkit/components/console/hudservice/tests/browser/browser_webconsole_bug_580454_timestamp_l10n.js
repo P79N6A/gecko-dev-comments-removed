d9deb1399930e077b57d8b66732a57fd2f29cb6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:00:49 +0000

    Bug 580454 - Localize console timestamps, r+a=dietrich
    UltraBlame original commit: 224777dc8afe11a6f34ddb6acc1d9e5d5a1fd62a

---
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_bug_580454_timestamp_l10n.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/HUDService.jsm");
+
+function test() {
+  const TEST_TIMESTAMP = 12345678;
+  let date = new Date(TEST_TIMESTAMP);
+  let localizedString = ConsoleUtils.timestampString(TEST_TIMESTAMP);
+  isnot(localizedString.indexOf(date.getHours()), -1, "the localized " +
+    "timestamp contains the hours");
+  isnot(localizedString.indexOf(date.getMinutes()), -1, "the localized " +
+    "timestamp contains the minutes");
+  isnot(localizedString.indexOf(date.getSeconds()), -1, "the localized " +
+    "timestamp contains the seconds");
+  isnot(localizedString.indexOf(date.getMilliseconds()), -1, "the localized " +
+    "timestamp contains the milliseconds");
+}
+
