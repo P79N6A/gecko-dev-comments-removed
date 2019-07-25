438f7e54939816342db08d508dbfb07ff536cf9e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:07:24 +0000

    Bug 688368 - Make SourceSurfaceSkia data surfaces copy-on-write. r=joe
    UltraBlame original commit: a204fcf29e6059d068d0bc16da336ad4fe4c526a

--- gfx/2d/SourceSurfaceSkia.h
+++ gfx/2d/SourceSurfaceSkia.h
@@ -46,6 +46,8 @@
 namespace mozilla {
 namespace gfx {
 
+class DrawTargetSkia;
+
 class SourceSurfaceSkia : public DataSourceSurface
 {
 public:
@@ -63,8 +65,13 @@ public:
                     int32_t aStride,
                     SurfaceFormat aFormat);
 
-  bool InitWithBitmap(SkCanvas* aBitmap,
-                      SurfaceFormat aFormat);
+  
+
+
+
+  bool InitWithBitmap(const SkBitmap& aBitmap,
+                      SurfaceFormat aFormat,
+                      DrawTargetSkia* aOwner);
 
 
   virtual unsigned char *GetData();
@@ -74,10 +81,14 @@ public:
 private:
   friend class DrawTargetSkia;
 
+  void DrawTargetWillChange();
+  void MarkIndependent();
+
   SkBitmap mBitmap;
   SurfaceFormat mFormat;
   IntSize mSize;
   int32_t mStride;
+  DrawTargetSkia* mDrawTarget;
 };
 
 }
