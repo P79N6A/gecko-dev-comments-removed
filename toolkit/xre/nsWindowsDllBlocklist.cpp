e0bda2327c0fd35e9317a6b3e81b950a5dcc4462
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:11:56 +0000

    Bug 699134, block Roboform <= ver 7.6.1, r=jonas a=akeybl CLOSED TREE
    UltraBlame original commit: cd245c2037f370ed5927f4d2a7714d48d8f1c88a

--- toolkit/xre/nsWindowsDllBlocklist.cpp
+++ toolkit/xre/nsWindowsDllBlocklist.cpp
@@ -129,7 +129,11 @@ static DllBlockInfo sWindowsDllBlocklist[] = {
 
   
   {"accelerator.dll", MAKE_VERSION(3,2,1,6)},
+
   
+  {"rf-firefox.dll", MAKE_VERSION(7,6,1,0)},
+  {"roboform.dll", MAKE_VERSION(7,6,1,0)},
+
   
   { "mozdllblockingtest.dll", ALL_VERSIONS },
   { "mozdllblockingtest_versioned.dll", 0x0000000400000000ULL },
