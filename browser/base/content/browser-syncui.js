d5f132705bade1df414ffd6368f5c7ea05ddcc4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:20:59 +0000

    Bug 568653 - Change "Last Update" to "Last Sync" in tooltip. r=rnewman
    UltraBlame original commit: 5a5e28689b4ae3906d84ad7a28cae18e1b4853bc

--- browser/base/content/browser-syncui.js
+++ browser/base/content/browser-syncui.js
@@ -328,7 +328,7 @@ let gSyncUI = {
     
     let lastSyncDate = new Date(lastSync).toLocaleFormat("%a %H:%M");
     let lastSyncLabel =
-      this._stringBundle.formatStringFromName("lastSync.label", [lastSyncDate], 1);
+      this._stringBundle.formatStringFromName("lastSync2.label", [lastSyncDate], 1);
 
     syncButton.setAttribute("tooltiptext", lastSyncLabel);
   },
