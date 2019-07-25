ee5063e5ac5efa425f9a16194ee1be0412eea7a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:27:31 +0000

    Bug 766871 - Part 2: Tests for window.open from <iframe> inside <iframe mozbrowser>. r=bz
    UltraBlame original commit: 633a85b28dbbffbb7c0930d6f5e5770a03c039fc

---
+++ dom/browser-element/mochitest/browserElement_OpenWindowInFrame.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+"use strict";
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addToWhitelist();
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  var gotPopup = false;
+  iframe.addEventListener('mozbrowseropenwindow', function(e) {
+    is(gotPopup, false, 'Should get just one popup.');
+    gotPopup = true;
+
+    document.body.appendChild(e.detail.frameElement);
+
+    ok(/file_browserElement_Open2\.html$/.test(e.detail.url),
+       "Popup's URL (got " + e.detail.url + ")");
+    is(e.detail.name, "name");
+    is(e.detail.features, "features");
+  });
+
+  iframe.addEventListener('mozbrowsershowmodalprompt', function(e) {
+    ok(gotPopup, 'Got mozbrowseropenwindow event before showmodalprompt event.');
+    if (e.detail.message.indexOf("success:") == 0) {
+      ok(true, e.detail.message);
+    }
+    else if (e.detail.message.indexOf("failure:") == 0) {
+      ok(false, e.detail.message);
+    }
+    else if (e.detail.message == "finish") {
+      SimpleTest.finish();
+    }
+    else {
+      ok(false, "Got invalid message: " + e.detail.message);
+    }
+  });
+
+  
+
+
+
+
+
+
+
+
+
+
+  iframe.src = 'file_browserElement_OpenWindowInFrame.html';
+  document.body.appendChild(iframe);
+}
+
+runTest();
