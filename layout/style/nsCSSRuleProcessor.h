8db266c986d4eea85275e54716b2ddefe13d8c6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:10:02 +0000

    Bug 661746 - Part 3: Use const selectors during selector matching.  r=bzbarsky
    UltraBlame original commit: 61534874aed701ddc982605d4864229e6d19896c

--- layout/style/nsCSSRuleProcessor.h
+++ layout/style/nsCSSRuleProcessor.h
@@ -95,7 +95,7 @@ public:
 
   static bool SelectorListMatches(const mozilla::dom::Element* aElement,
                                     TreeMatchContext& aTreeMatchContext,
-                                    nsCSSSelectorList* aSelectorList);
+                                    const nsCSSSelectorList* aSelectorList);
 
   
 
