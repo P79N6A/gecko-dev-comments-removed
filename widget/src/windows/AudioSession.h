b42fa3e4a432321bea5fa44b7d66269f8da80602
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:24:42 +0000

    imported patch 441197-1
    UltraBlame original commit: 009eb2cbd763259387170bda9dfd3bda791b8bd9

---
+++ widget/src/windows/AudioSession.h
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if MOZ_WINSDK_TARGETVER >= MOZ_NTDDI_LONGHORN
+
+namespace mozilla {
+namespace widget {
+
+nsresult StartAudioSession();
+nsresult StopAudioSession();
+
+} 
+} 
+
+#endif 
