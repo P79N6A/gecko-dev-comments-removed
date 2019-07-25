a1c649cfc6395c3680091849bb8235925bdb0cb5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:01:05 +0000

    backing out due to red
    UltraBlame original commit: 83f778ef2a77aaf2f6b75418295d580d29ea0639

--- js/src/jsatom.cpp
+++ js/src/jsatom.cpp
@@ -183,7 +183,6 @@ const char *const js_common_atom_names[] = {
     "line",                     
     "Infinity",                 
     "NaN",                      
-    "builder",                  
 
 #if JS_HAS_XML_SUPPORT
     js_etago_str,               
