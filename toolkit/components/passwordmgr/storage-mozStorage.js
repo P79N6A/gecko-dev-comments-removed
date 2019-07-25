6f23ab79a6eadd101ccf5eb4d09aad4b46d78ec1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:20:07 +0000

    Bug 756701 - Backout original patch. r=mfinkle
    UltraBlame original commit: 94f43049073b09b188e57dab61e4a550b6be0785

--- toolkit/components/passwordmgr/storage-mozStorage.js
+++ toolkit/components/passwordmgr/storage-mozStorage.js
@@ -345,6 +345,7 @@ LoginManagerStorage_mozStorage.prototype = {
         try {
             stmt = this._dbCreateStatement(query, params);
             stmt.execute();
+            this.storeDeletedLogin(storedLogin);
             transaction.commit();
         } catch (e) {
             this.log("_removeLogin failed: " + e.name + " : " + e.message);
@@ -655,6 +656,31 @@ LoginManagerStorage_mozStorage.prototype = {
 
 
 
+     storeDeletedLogin : function(aLogin) {
+#ifdef ANDROID
+          let stmt = null; 
+          try {
+              this.log("Storing " + aLogin.guid + " in deleted passwords\n");
+              let query = "INSERT INTO moz_deleted_logins (guid, timeDeleted) VALUES (:guid, :timeDeleted)";
+              let params = { guid: aLogin.guid,
+                             timeDeleted: Date.now() };
+              let stmt = this._dbCreateStatement(query, params);
+              stmt.execute();
+          } catch(ex) {
+              throw ex;
+          } finally {
+              if (stmt)
+                  stmt.reset();
+          }		
+#endif
+     },
+
+
+    
+
+
+
+
     removeAllLogins : function () {
         this.log("Removing all logins");
         let query;
@@ -667,6 +693,11 @@ LoginManagerStorage_mozStorage.prototype = {
         
         query = "DELETE FROM moz_logins";
         try {
+            let logins = this.getAllLogins();
+            for each (let login in logins) {
+                let [id, storedLogin] = this._getIdForLogin(login);
+                this.storeDeletedLogin(storedLogin);
+            }
             stmt = this._dbCreateStatement(query);
             stmt.execute();
             transaction.commit();
