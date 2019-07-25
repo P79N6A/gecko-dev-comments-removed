8b5a1d28098cb86455464154c718a15b9b5fa684
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:18:32 +0000

    Bug 658750 - Part 2: Make nsFrameSelection not inherit from nsISupports. r=roc
    UltraBlame original commit: 693717fddd933973c948ac9e7e07374bf0e8afc1

--- layout/generic/nsFrameSelection.h
+++ layout/generic/nsFrameSelection.h
@@ -204,13 +204,17 @@ class nsIScrollableFrame;
 
 
 
-class NS_FINAL_CLASS nsFrameSelection : public nsISupports {
+class NS_FINAL_CLASS nsFrameSelection {
 public:
   enum HINT { HINTLEFT = 0, HINTRIGHT = 1};  
-  
-  
-  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
-  NS_DECL_CYCLE_COLLECTION_CLASS(nsFrameSelection)
+
+  nsFrameSelection();
+private:
+  ~nsFrameSelection();
+public:
+
+  NS_INLINE_DECL_REFCOUNTING(nsFrameSelection)
+  NS_DECL_CYCLE_COLLECTION_NATIVE_CLASS(nsFrameSelection)
 
   
 
@@ -585,8 +589,6 @@ public:
 
   nsresult MaintainSelection(nsSelectionAmount aAmount = eSelectNoAmount);
 
-
-  nsFrameSelection();
 
   void StartBatchChanges();
   void EndBatchChanges();
