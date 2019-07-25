ac43678d16cada97d3fb2a5accab36d2d2545f3e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:09:23 +0000

    Bug 602331 - selection addRange cannot select nodes that are being dynamically appended to the DOM. r=roc a=blocking2.0:final
    UltraBlame original commit: b6384e736df22e75d1b5d92fc157e2cd06486d4b

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -4356,11 +4356,6 @@ nsTypedSelection::selectFrames(nsPresContext* aPresContext, nsIRange *aRange, PR
   if (!presShell)
     return NS_OK;
 
-  
-  presShell = aPresContext->GetPresShell();
-  if (!presShell)
-    return NS_OK;
-
   nsCOMPtr<nsIDOMRange> domRange = do_QueryInterface(aRange);
   if (!domRange || !aPresContext) 
     return NS_ERROR_NULL_POINTER;
@@ -4821,9 +4816,14 @@ nsTypedSelection::AddRange(nsIRange* aRange)
 
   nsRefPtr<nsPresContext>  presContext;
   GetPresContext(getter_AddRefs(presContext));
-  selectFrames(presContext, aRange, PR_TRUE);        
 
   
+  nsIPresShell* presShell = presContext ? presContext->GetPresShell() : nsnull;
+  if (presShell) {
+    presShell->FlushPendingNotifications(Flush_Frames);
+  }
+  selectFrames(presContext, aRange, PR_TRUE);        
+
   if (!mFrameSelection)
     return NS_OK;
 
