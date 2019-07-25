5663f9281332e759c214efe1fa5ffaf1e9671875
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:55:53 +0000

    Forgot the test for 600137.
    UltraBlame original commit: 376dc8dc659612e45eba13e53fab62b44990641e

---
+++ js/src/tests/js1_8_5/regress/regress-600137.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+try {
+    for (var [e] = /x/ in d) {
+        function () {}
+    }
+} catch (e) {}
+try {
+    let(x = Object.freeze(this, /x/))
+    e = #0= * .toString
+    function y() {}
+} catch (e) {}
+
+reportCompare(0, 0, "don't crash");
