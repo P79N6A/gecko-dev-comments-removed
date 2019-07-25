819218f1d49853ef6620b151568b7bbd594c0083
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:13:49 +0000

    Bug 674725 - Part F - .mozSms returns null if the platform doesn't have SMS supports. r=smaug
    UltraBlame original commit: 8456fe6baadd504f3c25385263457f4bf7503350

--- dom/sms/src/fallback/SmsService.cpp
+++ dom/sms/src/fallback/SmsService.cpp
@@ -43,6 +43,13 @@ namespace sms {
 
 NS_IMPL_ISUPPORTS1(SmsService, nsISmsService)
 
+NS_IMETHODIMP
+SmsService::HasSupport(bool* aHasSupport)
+{
+  *aHasSupport = false;
+  return NS_OK;
+}
+
 } 
 } 
 } 
