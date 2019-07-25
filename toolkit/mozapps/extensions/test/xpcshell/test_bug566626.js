70f1be7dc3f663861021d742fc5d1ec9b2abd2e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:03:18 +0000

    Bug 566626: Add-ons will randomly not appear in the UI. r=robstrong
    UltraBlame original commit: 03bd98ae2bf3deff8e1ab5d89a6ab4dad3209270

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_bug566626.js
@@ -0,0 +1,114 @@
+
+
+
+
+
+
+
+var addon1 = {
+  id: "addon1@tests.mozilla.org",
+  version: "1.0",
+  name: "Test 1",
+  targetApplications: [{
+    id: "xpcshell@tests.mozilla.org",
+    minVersion: "1",
+    maxVersion: "1"
+  }]
+};
+
+const profileDir = gProfD.clone();
+profileDir.append("extensions");
+
+var gAddon;
+
+
+function run_test() {
+  do_test_pending();
+
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+  var dest = profileDir.clone();
+  dest.append("addon1@tests.mozilla.org");
+  writeInstallRDFToDir(addon1, dest);
+
+  startupManager(1);
+
+  run_test_1();
+}
+
+
+
+function run_test_1() {
+  var count = 0;
+
+  AddonManager.getAddonByID("addon1@tests.mozilla.org", function(a1) {
+    do_check_neq(a1, null);
+    do_check_eq(a1.name, "Test 1");
+
+    if (count == 0)
+      gAddon = a1;
+    else
+      do_check_eq(a1, gAddon);
+    count++;
+    if (count == 4)
+      run_test_2();
+  });
+
+  AddonManager.getAddonByID("addon1@tests.mozilla.org", function(a1) {
+    do_check_neq(a1, null);
+    do_check_eq(a1.name, "Test 1");
+
+    if (count == 0)
+      gAddon = a1;
+    else
+      do_check_eq(a1, gAddon);
+    count++;
+    if (count == 4)
+      run_test_2();
+  });
+
+  do_execute_soon(function() {
+    AddonManager.getAddonByID("addon1@tests.mozilla.org", function(a1) {
+      do_check_neq(a1, null);
+      do_check_eq(a1.name, "Test 1");
+
+      if (count == 0)
+        gAddon = a1;
+      else
+        do_check_eq(a1, gAddon);
+      count++;
+      if (count == 4)
+        run_test_2();
+    });
+  });
+
+  do_execute_soon(function() {
+    do_execute_soon(function() {
+      AddonManager.getAddonByID("addon1@tests.mozilla.org", function(a1) {
+        do_check_neq(a1, null);
+        do_check_eq(a1.name, "Test 1");
+
+        if (count == 0)
+          gAddon = a1;
+        else
+          do_check_eq(a1, gAddon);
+        count++;
+        if (count == 4)
+          run_test_2();
+      });
+    });
+  });
+}
+
+
+function run_test_2() {
+  AddonManager.getAddonByID("addon1@tests.mozilla.org", function(a1) {
+    do_check_neq(a1, null);
+    do_check_eq(a1.name, "Test 1");
+
+    do_check_eq(a1, gAddon);
+
+    do_test_finished();
+  });
+
+}
