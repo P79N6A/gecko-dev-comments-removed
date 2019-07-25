a336f3c0feec39418eea555af0452319445cbaaa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:21:11 +0000

    Bug 707629 - Part 2: Complete most of the mozTelephony API. r=bent
    UltraBlame original commit: 40d760a401001241d3b624306832c241515f4c12

--- dom/telephony/Telephony.js
+++ dom/telephony/Telephony.js
@@ -46,6 +46,31 @@ const TELEPHONY_CALL_CID = Components.ID("{6b9b3daf-e5ea-460b-89a5-641ee20dd577}
 const TELEPHONY_CALL_CONTRACTID = "@mozilla.org/telephony-call;1";
 
 
+const DOM_RADIOSTATE_UNAVAILABLE   = "unavailable";
+const DOM_RADIOSTATE_OFF           = "off";
+const DOM_RADIOSTATE_READY         = "ready";
+
+const DOM_CARDSTATE_UNAVAILABLE    = "unavailable";
+const DOM_CARDSTATE_ABSENT         = "absent";
+const DOM_CARDSTATE_PIN_REQUIRED   = "pin_required";
+const DOM_CARDSTATE_PUK_REQUIRED   = "puk_required";
+const DOM_CARDSTATE_NETWORK_LOCKED = "network_locked";
+const DOM_CARDSTATE_NOT_READY      = "not_ready";
+const DOM_CARDSTATE_READY          = "ready";
+
+const DOM_CALL_READYSTATE_DIALING        = "dialing";
+const DOM_CALL_READYSTATE_RINGING        = "ringing";
+const DOM_CALL_READYSTATE_BUSY           = "busy";
+const DOM_CALL_READYSTATE_CONNECTING     = "connecting";
+const DOM_CALL_READYSTATE_CONNECTED      = "connected";
+const DOM_CALL_READYSTATE_DISCONNECTING  = "disconnecting";
+const DOM_CALL_READYSTATE_DISCONNECTED   = "disconnected";
+const DOM_CALL_READYSTATE_INCOMING       = "incoming";
+const DOM_CALL_READYSTATE_HOLDING        = "holding";
+const DOM_CALL_READYSTATE_HELD           = "held";
+
+const CALLINDEX_TEMPORARY_DIALING = -1;
+
 
 
 
@@ -110,6 +135,9 @@ EventTarget.prototype = {
     
     
     
+    if (!this._listeners) {
+      return;
+    }
     let handlerList = this._listeners[event.type];
     if (!handlerList) {
       return;
@@ -124,7 +152,7 @@ EventTarget.prototype = {
     
     let handlers = handlerList.slice();
     handlers.forEach(function (handler) {
-      if (handerList.indexOf(handler) == -1) {
+      if (handlerList.indexOf(handler) == -1) {
         return;
       }
       switch (typeof handler) {
@@ -139,54 +167,42 @@ EventTarget.prototype = {
   }
 };
 
-const DOM_RADIOSTATE_UNAVAILABLE = "unavailable";
-const DOM_RADIOSTATE_OFF         = "off";
-const DOM_RADIOSTATE_READY       = "ready";
-   
-const DOM_CARDSTATE_UNAVAILABLE    = "unavailable";
-const DOM_CARDSTATE_ABSENT         = "absent";
-const DOM_CARDSTATE_PIN_REQUIRED   = "pin_required";
-const DOM_CARDSTATE_PUK_REQUIRED   = "puk_required";
-const DOM_CARDSTATE_NETWORK_LOCKED = "network_locked";
-const DOM_CARDSTATE_NOT_READY      = "not_ready";
-const DOM_CARDSTATE_READY          = "ready";
 
 
 
 
 
-
-function TelephonyRadioCallback(telephony) {
+function TelephoneCallback(telephony) {
   this.telephony = telephony;
 }
-TelephonyRadioCallback.prototype = {
+TelephoneCallback.prototype = {
 
   QueryInterface: XPCOMUtils.generateQI([Ci.nsITelephoneCallback]),
 
   
 
-  onsignalstrengthchange: function onsignalstrengthchange(signalStrength) {
-    this.telephony.signalStrength = signalStrength;
+  onsignalstrengthchange: function onsignalstrengthchange(event) {
+    this.telephony.signalStrength = event.signalStrength;
     this.telephony._dispatchEventByType("signalstrengthchange");
   },
 
-  onoperatorchange: function onoperatorchange(operator) {
-    this.telephony.operator = operator;
+  onoperatorchange: function onoperatorchange(event) {
+    this.telephony.operator = event.operator;
     this.telephony._dispatchEventByType("operatorchange");
   },
 
-  onradiostatechange: function onradiostatechange(radioState) {
-    this.telephony.radioState = radioState;
+  onradiostatechange: function onradiostatechange(event) {
+    this.telephony.radioState = event.radioState;
     this.telephony._dispatchEventByType("radiostatechange");
   },
 
-  oncardstatechange: function oncardstatechange(cardState) {
-    this.telephony.cardState = cardState;
+  oncardstatechange: function oncardstatechange(event) {
+    this.telephony.cardState = event.cardState;
     this.telephony._dispatchEventByType("cardstatechange");
   },
 
-  oncallstatechange: function oncallstatechange(callState) {
-    this.telephony._processCallState(callState);
+  oncallstatechange: function oncallstatechange(event) {
+    this.telephony._processCallState(event);
   },
 
 };
@@ -214,23 +230,35 @@ Telephony.prototype = {
 
   init: function init(window) {
     this.window = window;
-    this.radioInterface = Cc["@mozilla.org/telephony/radio-interface;1"]
-                            .createInstance(Ci.nsITelephone);
-    this.radioCallback = new TelephonyRadioCallback(this);
+    this.telephone = Cc["@mozilla.org/telephony/radio-interface;1"]
+                       .createInstance(Ci.nsITelephone);
+    this.telephoneCallback = new TelephoneCallback(this);
+    
     window.addEventListener("unload", function onunload(event) {
-      this.radioInterface.unregisterCallback(this.radioCallback);
-      this.radioCallback = null;
+      this.telephone.unregisterCallback(this.telephoneCallback);
+      this.telephoneCallback = null;
       this.window = null;
     }.bind(this));
-    this.radioInterface.registerCallback(this.radioCallback);
+    this.telephone.registerCallback(this.telephoneCallback);
+    this.callsByIndex = {};
     this.liveCalls = [];
 
-    let initialState = this.radioInterface.initialState;
-    this.operator        = initialState.operator;
-    this.radioState      = initialState.radioState;
-    this.cardState       = initialState.cardState;
-    this.signalStrength  = initialState.signalStrength;
-    this._processCallState(initialState.callState);
+    
+    let currentState = this.telephone.currentState;
+    let states = currentState.currentCalls;
+    for (let i = 0; i < states.length; i++) {
+      let state = states[i];
+      let call = new TelephonyCall(this.telephone, state.callIndex);
+      call.readyState = state.callState;
+      call.number = state.number;
+      this.liveCalls.push(call);
+      this.callsByIndex[state.callIndex] = call;
+    }
+
+    this.operator        = currentState.operator;
+    this.radioState      = currentState.radioState;
+    this.cardState       = currentState.cardState;
+    this.signalStrength  = currentState.signalStrength;
   },
 
   _dispatchEventByType: function _dispatchEventByType(type) {
@@ -240,17 +268,92 @@ Telephony.prototype = {
     this.dispatchEvent(event);
   },
 
-  _processCallState: function _processCallState(callState) {
+  _dispatchCallEvent: function _dispatchCallEvent(call, type, target) {
+    let event = this.window.document.createEvent("Event");
+    event.initEvent(type, false, false);
+    event.call = call; 
     
+    target = target || call;
+    target.dispatchEvent(event);    
   },
+
+  _processCallState: function _processCallState(state) {
+    
+    
+    if (state.callState == DOM_CALL_READYSTATE_DIALING) {
+      let call = this.callsByIndex[CALLINDEX_TEMPORARY_DIALING];
+      if (call) {
+        call.callIndex = state.callIndex;
+        delete this.callsByIndex[CALLINDEX_TEMPORARY_DIALING];
+        this.callsByIndex[call.callIndex] = call;
+        
+        
+        return;
+      }
+    }
+
+    
+    
+    let call = this.callsByIndex[state.callIndex];
+    if (call) {
+      if (call.readyState == state.callState) {
+        
+        return;
+      }
+      if (state.readyState == DOM_CALL_READYSTATE_DISCONNECTED) {
+        let index = this.liveCalls.indexOf(call);
+        if (index != -1) {
+          this.liveCalls.splice(index, 1);
+        }
+        delete this.callsByIndex[call.callIndex];
+      }
+      call.readyState = state.callState;
+      this._dispatchCallEvent(call, "readystatechange");
+      this._dispatchCallEvent(call, state.callState);
+      return;
+    }
+
+    
+    
+    if (state.readyState == DOM_CALL_READYSTATE_DISCONNECTED) {
+      return;
+    }
+    call = new TelephonyCall(this.telephone, state.callIndex);
+    call.number = state.number;
+    call.readyState = state.callState;
+    this.callsByIndex[state.callIndex] = call;
+    this.liveCalls.push(call);
+
+    let target;
+    if (call.readyState == DOM_CALL_READYSTATE_INCOMING) {
+      target = this;
+    } else {
+      target = call;
+      this._dispatchCallEvent(call, "readystatechange");
+    }
+    this._dispatchCallEvent(call, state.callState, target);
+  },
+
+  callsByIndex: null,
 
   
 
   liveCalls: null,
 
   dial: function dial(number) {
-    this.radioInterface.dial(number);
-    return new TelephonyCall(number, DOM_CALL_READYSTATE_DIALING);
+    this.telephone.dial(number);
+
+    
+    
+    
+    
+    let callIndex = CALLINDEX_TEMPORARY_DIALING;
+    let call = new TelephonyCall(this.telephone, callIndex);
+    call.readyState = DOM_CALL_READYSTATE_DIALING;
+    call.number = number;
+    this.callsByIndex[callIndex] = call;
+    this.liveCalls.push(call);
+    return call;
   },
 
   
@@ -261,23 +364,17 @@ Telephony.prototype = {
   cardState: DOM_CARDSTATE_UNAVAILABLE,
 
 };
+defineEventListenerSlot(Telephony.prototype, DOM_CALL_READYSTATE_INCOMING);
+
 defineEventListenerSlot(Telephony.prototype, "radiostatechange");
 defineEventListenerSlot(Telephony.prototype, "cardstatechange");
 defineEventListenerSlot(Telephony.prototype, "signalstrengthchange");
 defineEventListenerSlot(Telephony.prototype, "operatorchange");
-defineEventListenerSlot(Telephony.prototype, "incoming");
 
 
-const DOM_CALL_READYSTATE_DIALING   = "dialing";
-const DOM_CALL_READYSTATE_DOM_CALLING   = "calling";
-const DOM_CALL_READYSTATE_INCOMING  = "incoming";
-const DOM_CALL_READYSTATE_CONNECTED = "connected";
-const DOM_CALL_READYSTATE_CLOSED    = "closed";
-const DOM_CALL_READYSTATE_BUSY      = "busy";
-
-function TelephonyCall(number, initialState) {
-  this.number = number;
-  this.readyState = initialState;
+function TelephonyCall(telephone, callIndex) {
+  this.telephone = telephone;
+  this.callIndex = callIndex;
 }
 TelephonyCall.prototype = {
 
@@ -293,21 +390,37 @@ TelephonyCall.prototype = {
   QueryInterface: XPCOMUtils.generateQI([Ci.mozIDOMTelephonyCall,
                                          Ci.nsIDOMEventTarget]),
 
+
+  callIndex: null,
+
+  
+
   number: null,
   readyState: null,
 
   answer: function answer() {
-    
+    if (this.readyState != DOM_CALL_READYSTATE_INCOMING) {
+      throw "Can only answer an incoming call!";
+    }
+    this.telephone.answerCall();
   },
 
   disconnect: function disconnect() {
-    
+    if (this.readyState == DOM_CALL_READYSTATE_INCOMING) {
+      this.telephone.rejectCall();
+    } else {
+      this.telephone.hangUp(this.callIndex);
+    }
   },
 
 };
-defineEventListenerSlot(TelephonyCall.prototype, "connect");
-defineEventListenerSlot(TelephonyCall.prototype, "disconnect");
-defineEventListenerSlot(TelephonyCall.prototype, "busy");
+defineEventListenerSlot(TelephonyCall.prototype, "readystatechange");
+defineEventListenerSlot(TelephonyCall.prototype, DOM_CALL_READYSTATE_RINGING);
+defineEventListenerSlot(TelephonyCall.prototype, DOM_CALL_READYSTATE_BUSY);
+defineEventListenerSlot(TelephonyCall.prototype, DOM_CALL_READYSTATE_CONNECTING);
+defineEventListenerSlot(TelephonyCall.prototype, DOM_CALL_READYSTATE_CONNECTED);
+defineEventListenerSlot(TelephonyCall.prototype, DOM_CALL_READYSTATE_DISCONNECTING);
+defineEventListenerSlot(TelephonyCall.prototype, DOM_CALL_READYSTATE_DISCONNECTED);
 
 
 const NSGetFactory = XPCOMUtils.generateNSGetFactory([Telephony]);
