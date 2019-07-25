b828dd5bc76c6b7f68253b1355f6757fcfdb4441
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:38:31 +0000

    Bug 561640 - <input> and <textarea> should not suffer from being too long if dirty value flag is true. r=smaug a2.0=blocking
    UltraBlame original commit: 3a7630d8f17569a90697acec8e9b242c0592c3d7

--- content/html/content/src/nsHTMLInputElement.cpp
+++ content/html/content/src/nsHTMLInputElement.cpp
@@ -2963,7 +2963,10 @@ nsHTMLInputElement::VisitGroup(nsIRadioVisitor* aVisitor, PRBool aFlushContent)
 PRBool
 nsHTMLInputElement::IsTooLong()
 {
-  
+  if (!GET_BOOLBIT(mBitField, BF_VALUE_CHANGED)) {
+    return PR_FALSE;
+  }
+
   PRInt32 maxLength = -1;
   PRInt32 textLength = -1;
 
