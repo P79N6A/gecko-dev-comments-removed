e9198cc514a680069c5e7361fa86d3461329cf5d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:52:34 +0000

    bug 685214 - use URI spec rather than file path to specify hyphenation dictionary, and read using nsIInputStream rather than stdio. r=bsmedberg
    UltraBlame original commit: 2118b37a8389aab49ec17e29b1a34a18720aff8e

--- intl/hyphenation/src/hnjalloc.h
+++ intl/hyphenation/src/hnjalloc.h
@@ -48,3 +48,36 @@
 #define hnj_realloc(p, size)  moz_xrealloc(p, size)
 #define hnj_free(p)           moz_free(p)
 
+
+
+
+
+
+
+
+#include <stdio.h> 
+
+#undef FILE
+#define FILE hnjFile
+
+#define fopen(path,mode)      hnjFopen(path,mode)
+#define fclose(file)          hnjFclose(file)
+#define fgets(buf,count,file) hnjFgets(buf,count,file)
+
+typedef struct hnjFile_ hnjFile;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+hnjFile* hnjFopen(const char* aURISpec, const char* aMode);
+
+int hnjFclose(hnjFile* f);
+
+char* hnjFgets(char* s, int n, hnjFile* f);
+
+#ifdef __cplusplus
+}
+#endif
+
+
---
+++ intl/hyphenation/src/hnjstdio.cpp
@@ -0,0 +1,137 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "hnjalloc.h"
+#include "nsNetUtil.h"
+
+#define BUFSIZE 1024
+
+struct hnjFile_ {
+    nsCOMPtr<nsIInputStream> mStream;
+    char                     mBuffer[BUFSIZE];
+    PRUint32                 mCurPos;
+    PRUint32                 mLimit;
+};
+
+
+
+hnjFile*
+hnjFopen(const char* aURISpec, const char* aMode)
+{
+    
+    NS_ASSERTION(!strcmp(aMode, "r"), "unsupported fopen() mode in hnjFopen");
+
+    nsCOMPtr<nsIURI> uri;
+    nsresult rv = NS_NewURI(getter_AddRefs(uri), aURISpec);
+    if (NS_FAILED(rv)) {
+        return nsnull;
+    }
+
+    nsCOMPtr<nsIInputStream> instream;
+    rv = NS_OpenURI(getter_AddRefs(instream), uri);
+    if (NS_FAILED(rv)) {
+        return nsnull;
+    }
+
+    hnjFile *f = new hnjFile;
+    f->mStream = instream;
+    f->mCurPos = 0;
+    f->mLimit = 0;
+
+    return f;
+}
+
+
+int
+hnjFclose(hnjFile* f)
+{
+    NS_ASSERTION(f && f->mStream, "bad argument to hnjFclose");
+
+    int result = 0;
+    nsresult rv = f->mStream->Close();
+    if (NS_FAILED(rv)) {
+        result = EOF;
+    }
+    f->mStream = nsnull;
+
+    delete f;
+    return result;
+}
+
+
+
+char*
+hnjFgets(char* s, int n, hnjFile* f)
+{
+    NS_ASSERTION(s && f, "bad argument to hnjFgets");
+
+    int i = 0;
+    while (i < n - 1) {
+        if (f->mCurPos < f->mLimit) {
+            char c = f->mBuffer[f->mCurPos++];
+            s[i++] = c;
+            if (c == '\n' || c == '\r') {
+                break;
+            }
+            continue;
+        }
+
+        f->mCurPos = 0;
+
+        nsresult rv = f->mStream->Read(f->mBuffer, BUFSIZE, &f->mLimit);
+        if (NS_FAILED(rv)) {
+            f->mLimit = 0;
+            return nsnull;
+        }
+
+        if (f->mLimit == 0) {
+            break;
+        }
+    }
+
+    if (i == 0) {
+        return nsnull; 
+    }
+
+    s[i] = '\0'; 
+    return s;
+}
