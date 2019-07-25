ee83b3c4f2ef05e5e2d33632f3d71f96405a056c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:33:25 +0000

    Bug 775039 - Combine clearing cookies and active logins. r=mfinkle
    UltraBlame original commit: 9a676a486b5d328d915f6b212e254d595f1da582

--- mobile/android/chrome/content/sanitize.js
+++ mobile/android/chrome/content/sanitize.js
@@ -79,17 +79,7 @@ Sanitizer.prototype = {
       clear: function ()
       {
         Services.cookies.removeAll();
-      },
 
-      get canClear()
-      {
-        return true;
-      }
-    },
-
-    geolocation: {
-      clear: function ()
-      {
         
         try {
           var branch = Services.prefs.getBranch("geo.wifi.access_token.");
