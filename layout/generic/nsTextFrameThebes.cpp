4da3e36b16c48e6acc4014525e1101079ad25d79
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:36:16 +0000

    Bug 676538: Fix regression that caused text-decorations on inline *child* of block to draw at the offset for the block rather than the inline.  r=dbaron
    The FrameProperty representing baseline is set when a block defines
    text-decorations and has vertically-aligned children, but we were
    retrieving it whether the block or the vertically-aligned frame itself
    defined the decorations.  As a result, we "undo" the frame offset to get
    the baseline from the child in that case.
    
    UltraBlame original commit: d7960f9e273c2ff3473a01849884867273940fb7

--- layout/generic/nsTextFrameThebes.cpp
+++ layout/generic/nsTextFrameThebes.cpp
@@ -4279,7 +4279,12 @@ nsTextFrame::GetTextDecorations(nsPresContext* aPresContext,
 
   bool nearestBlockFound = false;
 
-  for (nsIFrame* f = this, *fParent; f; f = fParent) {
+  for (nsIFrame* f = this, *fChild = nsnull;
+       f;
+       fChild = f,
+       f = nsLayoutUtils::GetParentOrPlaceholderFor(
+             aPresContext->FrameManager(), f))
+  {
     nsStyleContext *const context = f->GetStyleContext();
     if (!context->HasTextDecorationLines()) {
       break;
@@ -4297,10 +4302,7 @@ nsTextFrame::GetTextDecorations(nsPresContext* aPresContext,
         nsLayoutUtils::GetColor(f, eCSSProperty_text_decoration_color);
     }
 
-    fParent = nsLayoutUtils::GetParentOrPlaceholderFor(
-                aPresContext->FrameManager(), f);
-    const bool firstBlock = !nearestBlockFound &&
-                            nsLayoutUtils::GetAsBlock(fParent);
+    const bool firstBlock = !nearestBlockFound && nsLayoutUtils::GetAsBlock(f);
 
     
     
@@ -4308,19 +4310,28 @@ nsTextFrame::GetTextDecorations(nsPresContext* aPresContext,
     
     
     
-    if (firstBlock &&
-        (styleText->mVerticalAlign.GetUnit() != eStyleUnit_Enumerated ||
-         styleText->mVerticalAlign.GetIntValue() !=
-           NS_STYLE_VERTICAL_ALIGN_BASELINE)) {
-      baselineOffset = frameTopOffset -
-        NS_PTR_TO_INT32(f->Properties().Get(nsIFrame::LineBaselineOffset()));
+    if (firstBlock) {
+      
+      const nsStyleCoord& vAlign =
+        fChild->GetStyleContext()->GetStyleTextReset()->mVerticalAlign;
+      if (vAlign.GetUnit() != eStyleUnit_Enumerated ||
+          vAlign.GetIntValue() != NS_STYLE_VERTICAL_ALIGN_BASELINE)
+      {
+        
+        
+        
+        baselineOffset =
+          frameTopOffset - (fChild->GetRect().y - fChild->GetRelativeOffset().y)
+          - NS_PTR_TO_INT32(
+              fChild->Properties().Get(nsIFrame::LineBaselineOffset()));
+      }
     }
     else if (!nearestBlockFound) {
       baselineOffset = frameTopOffset - f->GetBaseline();
     }
 
     nearestBlockFound = nearestBlockFound || firstBlock;
-    frameTopOffset += f->GetRect().Y() - f->GetRelativeOffset().y;
+    frameTopOffset += f->GetRect().y - f->GetRelativeOffset().y;
 
     const PRUint8 style = styleText->GetDecorationStyle();
     
