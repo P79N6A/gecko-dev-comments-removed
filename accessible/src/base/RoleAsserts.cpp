49b68d36eb6827431aec8dcc7d45549e344ab407
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:09:49 +0000

    Bug 750301 - add static asserts internal and xpcom accessible roles are the same, r=tbsaunde, f=surkov
    UltraBlame original commit: ae9aef30b549960a80ded494bdf7e9cac78753eb

---
+++ accessible/src/base/RoleAsserts.cpp
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+#include "nsIAccessibleRole.h"
+#include "Role.h"
+
+#include "mozilla/Assertions.h"
+
+using namespace mozilla::a11y;
+
+#define ROLE(geckoRole, stringRole, atkRole, macRole, msaaRole, ia2Role) \
+  MOZ_STATIC_ASSERT(roles::geckoRole == nsIAccessibleRole::ROLE_ ## geckoRole, "internal and xpcom roles differ!");
+#include "RoleMap.h"
+#undef ROLE
