7f1ac4046e05c26f3847ae35a45c03ddb8cf8df4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:56:19 +0000

    Bug 580546 FrameLayerBuilder.cpp failed to compile with Sun Studio 12 r=roc a=bsmedberg
    UltraBlame original commit: 9b5aea4bf62d96ad15981c920994b5c6f650bb19

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -1075,7 +1075,7 @@ ContainerState::Finish()
       if (!layer->GetParent()) {
         
         
-        Layer* prevChild = i == 0 ? nsnull : mNewChildLayers[i - 1];
+        Layer* prevChild = i == 0 ? nsnull : mNewChildLayers[i - 1].get();
         mContainerLayer->InsertAfter(layer, prevChild);
         continue;
       }
