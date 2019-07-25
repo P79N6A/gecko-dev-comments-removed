7ff88ecb732ea5685e218a331333788275926365
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:22:26 +0000

    Backout 65cd6893941e for real (bug 731868)
    UltraBlame original commit: d9f90e9e1de90e09f2e5d69f5e46bcb45092a654

--- browser/components/tabview/ui.js
+++ browser/components/tabview/ui.js
@@ -250,6 +250,9 @@ let UI = {
       let event = document.createEvent("Events");
       event.initEvent("tabviewframeinitialized", true, false);
       dispatchEvent(event);
+
+      
+      event = null;
     } catch(e) {
       Utils.log(e);
     } finally {
