74121b44ca246a047ee7f92298c0bcb2f6b053ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:23:14 +0000

    Bug 596169 - Make <summary> and <figcaption> behave per spec. Remove <datagrid>. rs=jonas, a=blocking2.0-betaN.
    UltraBlame original commit: df1d1ff6b48909528d670a829e3be11e12cbe38c

--- parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
+++ parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
@@ -7,8 +7,4 @@
 var html5Exceptions = {
   "<!doctype html><keygen><frameset>" : true, 
   "<select><keygen>" : true, 
-  "<!doctype html><p><summary>" : true, 
-  "<!doctype html><summary><article></summary>a" : true, 
-  "<!doctype html><p><figcaption>" : true, 
-  "<!doctype html><figcaption><article></figcaption>a" : true, 
 }
