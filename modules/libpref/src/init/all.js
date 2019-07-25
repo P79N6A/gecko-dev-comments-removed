f91f7b0c439a77b3d72c031e5d78331fcbbfd3b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:39:25 +0000

    Bug 576033  - Accept-Encoding syntax causes upstream cache inefficiency.  r=jduell
    UltraBlame original commit: 60dd705d7e1dc6c7735ac36ddbd7defa7594bda5

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -694,7 +694,8 @@ pref("network.http.redirection-limit", 20);
 
 
 
-pref("network.http.accept-encoding" ,"gzip,deflate");
+
+pref("network.http.accept-encoding", "gzip, deflate");
 
 pref("network.http.pipelining"      , false);
 pref("network.http.pipelining.ssl"  , false); 
