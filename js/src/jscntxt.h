202c8af1dd308fe61b9b4298998b58e529e7623c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:22:05 +0000

    Bug 623297 - To decide JS_ON_TRACE, check the thread, not the compartment (r=gal)
    UltraBlame original commit: f3b470fb91a9741c87994f5d4b4d79dc17c06a8c

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -815,17 +815,6 @@ private:
 
 } 
 
-
-
-
-
-
-#ifdef JS_TRACER
-# define JS_ON_TRACE(cx)            (cx->compartment && JS_TRACE_MONITOR(cx).ontrace())
-#else
-# define JS_ON_TRACE(cx)            false
-#endif
-
 #ifdef DEBUG
 # define FUNCTION_KIND_METER_LIST(_)                                          \
                         _(allfun), _(heavy), _(nofreeupvar), _(onlyfreevar),  \
@@ -859,6 +848,18 @@ struct JSThreadData {
     unsigned            requestDepth;
 #endif
 
+#ifdef JS_TRACER
+    
+
+
+
+
+
+
+
+    JSCompartment       *tracerCompartment;
+#endif
+    
     
 
 
