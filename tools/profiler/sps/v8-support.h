4f338be6f8e5f1c00bc29695adf1e7f1e6a0d77d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:17:50 +0000

    Bug 698002. Add more v8 support code. r=bgirard
    These are needed for the mac port
    
    UltraBlame original commit: 8d8c62c6a4142d0ffb9d0779d5679e4da5376d2b

--- tools/profiler/sps/v8-support.h
+++ tools/profiler/sps/v8-support.h
@@ -38,5 +38,35 @@
 
 
 
+#ifndef V8_SUPPORT_H_
+#define V8_SUPPORT_H_
+
+#if defined(_M_X64) || defined(__x86_64__)
+#define V8_HOST_ARCH_X64 1
+#elif defined(_M_IX86) || defined(__i386__) || defined(__i386)
+#define V8_HOST_ARCH_IA32 1
+#elif defined(__ARMEL__)
+#define V8_HOST_ARCH_ARM 1
+#else
+#warning Please add support for your architecture in chromium_types.h
+#endif
+
+const int kMaxInt = 0x7FFFFFFF;
+const int kMinInt = -kMaxInt - 1;
+
+
+
+#define DISALLOW_COPY_AND_ASSIGN(TypeName)      \
+  TypeName(const TypeName&);                    \
+  void operator=(const TypeName&)
+
+
+
+
+template <typename T>
+static inline void USE(T) { }
+
 class Malloced {
 };
+
+#endif 
