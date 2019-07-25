726e377178a190246400f784189240ceeb2801fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:49:45 +0000

    Bustage fix for the bustage fix for bug 626602. Namespace statement is required, so just ifdef it for IPC builds. r+a=bustage fix
    UltraBlame original commit: 280e978fc6fb4c731728255a8bc9c70ded2adbea

--- layout/generic/nsObjectFrame.cpp
+++ layout/generic/nsObjectFrame.cpp
@@ -239,6 +239,9 @@ static PRLogModuleInfo *nsObjectFrameLM = PR_NewLogModule("nsObjectFrame");
 #endif
 
 using namespace mozilla;
+#ifdef MOZ_IPC
+using namespace mozilla::plugins;
+#endif
 using namespace mozilla::layers;
 
 
