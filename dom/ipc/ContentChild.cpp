65f42b84c567094e640a864b615a303e040ad144
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:55:08 +0000

    Bug 633986 Pref nice value, default to 1 on Android r=dougt a=blocking-fennec
    UltraBlame original commit: 4c2962ad0a9d688daa00ba3e2cbe2be264182c39

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -88,14 +88,6 @@
 #include "nsPermissionManager.h"
 #endif
 
-#if defined(ANDROID) || defined(LINUX)
-#include <sys/time.h>
-#include <sys/resource.h>
-
-
-static const int kRelativeNiceness = 10;
-#endif
-
 #include "nsAccelerometer.h"
 
 #if defined(ANDROID)
@@ -232,18 +224,6 @@ ContentChild::Init(MessageLoop* aIOLoop,
 #endif
 
     NS_ASSERTION(!sSingleton, "only one ContentChild per child");
-
-#if defined(ANDROID) || defined(LINUX)
-    
-    
-    
-    
-    
-    char* relativeNicenessStr = getenv("MOZ_CHILD_PROCESS_RELATIVE_NICENESS");
-    setpriority(PRIO_PROCESS, 0, getpriority(PRIO_PROCESS, 0) +
-            (relativeNicenessStr ? atoi(relativeNicenessStr) :
-             kRelativeNiceness));
-#endif
 
     Open(aChannel, aParentHandle, aIOLoop);
     sSingleton = this;
@@ -415,7 +395,7 @@ ContentChild::RecvSetOffline(const PRBool& offline)
   NS_ASSERTION(io, "IO Service can not be null");
 
   io->SetOffline(offline);
-    
+
   return true;
 }
 
@@ -435,7 +415,7 @@ ContentChild::ActorDestroy(ActorDestroyReason why)
 #endif
 
     mAlertObservers.Clear();
-    
+
     nsCOMPtr<nsIConsoleService> svc(do_GetService(NS_CONSOLESERVICE_CONTRACTID));
     if (svc) {
         svc->UnregisterListener(mConsoleListener);
