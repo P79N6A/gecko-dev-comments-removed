631765a233c1797740d653dbca971b0b91140648
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:48:26 +0000

    Remove TypeSets from MInstructions (bug 694621, r=sstangl).
    UltraBlame original commit: 7658d0234f1983a813fddec9f86a95906be245b2

--- js/src/ion/IonBuilder.cpp
+++ js/src/ion/IonBuilder.cpp
@@ -2092,7 +2092,6 @@ IonBuilder::jsop_getgname(JSAtom *atom)
     current->add(slots);
     MLoadSlot *load = MLoadSlot::New(slots, shape->slot - globalObj->numFixedSlots());
     current->add(load);
-    load->setTypeSet(types);
 
     
     if (knownType != JSVAL_TYPE_UNKNOWN && !barrier)
