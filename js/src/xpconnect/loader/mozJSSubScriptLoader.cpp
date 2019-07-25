e0cd295e692cbd02bb4beffa6775e8eb0daacac3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:30:55 +0000

    Fix bug 653926. r=jst
    UltraBlame original commit: 1e6392dd7c146865f686ccecb0aa23281b10eb51

--- js/src/xpconnect/loader/mozJSSubScriptLoader.cpp
+++ js/src/xpconnect/loader/mozJSSubScriptLoader.cpp
@@ -143,7 +143,7 @@ mozJSSubScriptLoader::ReadScript(nsIURI *uri, JSContext *cx, JSObject *target_ob
     
 
 
-    rv = mSystemPrincipal->GetJSPrincipals(cx, &jsPrincipals);
+    rv = principal->GetJSPrincipals(cx, &jsPrincipals);
     if (NS_FAILED(rv) || !jsPrincipals) {
         return ReportError(cx, LOAD_ERROR_NOPRINCIPALS);
     }
@@ -302,13 +302,24 @@ mozJSSubScriptLoader::LoadSubScript (const PRUnichar * aURL
 
     
     
+    nsCOMPtr<nsIPrincipal> principal = mSystemPrincipal;
     JSObject *result_obj = target_obj;
     target_obj = JS_FindCompilationScope(cx, target_obj);
     if (!target_obj) return NS_ERROR_FAILURE;
-#ifdef DEBUG_rginda
     if (target_obj != result_obj)
+    {
+        nsCOMPtr<nsIScriptSecurityManager> secman =
+            do_GetService(NS_SCRIPTSECURITYMANAGER_CONTRACTID);
+        if (!secman)
+            return NS_ERROR_FAILURE;
+
+        rv = secman->GetObjectPrincipal(cx, target_obj, getter_AddRefs(principal));
+        NS_ENSURE_SUCCESS(rv, rv);
+
+#ifdef DEBUG_rginda
         fprintf (stderr, "Final global: %p\n", target_obj);
 #endif
+    }
 
     JSAutoEnterCompartment ac;
     if (!ac.enter(cx, target_obj))
