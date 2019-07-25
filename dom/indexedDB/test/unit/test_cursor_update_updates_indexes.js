ac51716fc2bb35e545a3fb6e76d0a4eab14a86ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:28:40 +0000

    Bug 587797 - Tests. r=khuey
    UltraBlame original commit: ea4d463ec99fa322bc8bc8ff64a1505cb88fd3c5

---
+++ dom/indexedDB/test/unit/test_cursor_update_updates_indexes.js
@@ -0,0 +1,98 @@
+
+
+
+
+
+var testGenerator = testSteps();
+
+function testSteps()
+{
+  const nsIIDBObjectStore = Components.interfaces.nsIIDBObjectStore;
+  const nsIIDBTransaction = Components.interfaces.nsIIDBTransaction;
+
+  const name = this.window ? window.location.pathname : "Splendid Test";
+  const description = "My Test Database";
+  const START_DATA = "hi";
+  const END_DATA = "bye";
+  const objectStoreInfo = [
+    { name: "1", options: { keyPath: null }, key: 1,
+      entry: { data: START_DATA } },
+    { name: "2", options: { keyPath: "foo" },
+      entry: { foo: 1, data: START_DATA } },
+    { name: "3", options: { keyPath: null, autoIncrement: true },
+      entry: { data: START_DATA } },
+    { name: "4", options: { keyPath: "foo", autoIncrement: true },
+      entry: { data: START_DATA } },
+  ];
+
+  for (let i = 0; i < objectStoreInfo.length; i++) {
+    
+    let info = objectStoreInfo[i];
+
+    ok(true, "1");
+    request = mozIndexedDB.open(name, i + 1, description);
+    request.onerror = errorHandler;
+    request.onupgradeneeded = grabEventAndContinueHandler;
+    event = yield;
+
+    let db = event.target.result;
+
+    ok(true, "2");
+    let objectStore = info.hasOwnProperty("options") ?
+                      db.createObjectStore(info.name, info.options) :
+                      db.createObjectStore(info.name);
+
+    
+    let index = objectStore.createIndex("data_index", "data",
+                                        { unique: false });
+    let uniqueIndex = objectStore.createIndex("unique_data_index", "data",
+                                              { unique: true });
+    
+    request = info.hasOwnProperty("key") ?
+              objectStore.add(info.entry, info.key) :
+              objectStore.add(info.entry);
+    request.onerror = errorHandler;
+    request.onsuccess = grabEventAndContinueHandler;
+    event = yield;
+    ok(true, "3");
+
+    
+    request = objectStore.openCursor();
+    request.onerror = errorHandler;
+    request.onsuccess = grabEventAndContinueHandler;
+    event = yield;
+    ok(true, "4");
+
+    let cursor = request.result;
+    let obj = cursor.value;
+    obj.data = END_DATA;
+    request = cursor.update(obj);
+    request.onerror = errorHandler;
+    request.onsuccess = grabEventAndContinueHandler;
+    event = yield;
+    ok(true, "5");
+
+    
+    request = index.get(END_DATA);
+    request.onerror = errorHandler;
+    request.onsuccess = grabEventAndContinueHandler;
+    event = yield;
+    ok(true, "6");
+    ok(obj.data, event.target.result.data,
+                  "Non-unique index was properly updated.");
+
+    request = uniqueIndex.get(END_DATA);
+    request.onerror = errorHandler;
+    request.onsuccess = grabEventAndContinueHandler;
+    event = yield;
+
+    ok(true, "7");
+    ok(obj.data, event.target.result.data,
+                  "Unique index was properly updated.");
+    db.close();
+  }
+
+  finishTest();
+  yield;
+}
+
---
+++ dom/indexedDB/test/unit/test_index_empty_keyPath.js
@@ -0,0 +1,83 @@
+
+
+
+
+
+var testGenerator = testSteps();
+
+function testSteps()
+{
+  const name = this.window ? window.location.pathname : "Splendid Test";
+
+  const objectStoreData = [
+    { key: "1", value: "foo" },
+    { key: "2", value: "bar" },
+    { key: "3", value: "baz" }
+  ];
+
+  let request = mozIndexedDB.open(name, 1);
+  request.onerror = errorHandler;
+  request.onupgradeneeded = grabEventAndContinueHandler;
+  request.onsuccess = grabEventAndContinueHandler;
+  let event = yield; 
+
+  let db = event.target.result;
+
+  let objectStore = db.createObjectStore("data", { keyPath: null });
+
+  
+  let addedData = 0;
+  for (let i in objectStoreData) {
+    request = objectStore.add(objectStoreData[i].value,
+                              objectStoreData[i].key);
+    request.onerror = errorHandler;
+    request.onsuccess = function(event) {
+      if (++addedData == objectStoreData.length) {
+        testGenerator.send(event);
+      }
+    }
+  }
+  event = yield; 
+
+  
+  objectStore.createIndex("set", "", { unique: true });
+  yield; 
+
+  let trans = db.transaction("data", IDBTransaction.READ_WRITE);
+  objectStore = trans.objectStore("data");
+  index = objectStore.index("set");
+
+  let request = index.get("bar");
+  request.onerror = errorHandler;
+  request.onsuccess = grabEventAndContinueHandler;
+  
+  let event = yield;
+
+  is(event.target.result, "bar", "Got correct result");
+
+  let request = objectStore.add("foopy", 4);
+  request.onerror = errorHandler;
+  request.onsuccess = grabEventAndContinueHandler;
+
+  yield;
+
+  let request = index.get("foopy");
+  request.onerror = errorHandler;
+  request.onsuccess = grabEventAndContinueHandler;
+  
+  let event = yield;
+
+  is(event.target.result, "foopy", "Got correct result");
+
+  let request = objectStore.add("foopy", 5);
+  request.onerror = new ExpectError(IDBDatabaseException.CONSTRAINT_ERR);
+  request.onsuccess = unexpectedSuccessHandler;
+
+  trans.oncomplete = grabEventAndContinueHandler;
+
+  yield;
+  yield;
+
+  finishTest();
+  yield;
+}
