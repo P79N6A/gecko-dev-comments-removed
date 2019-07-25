9217d26c8948b7e0d825393c1198a18e77f271d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:44:45 +0000

    Bug 588383 - Compilation failure on mingw due to SEH exceptions r=benjamin a=benjamin
    UltraBlame original commit: ec8633955a785964bfb360c868f58b6153c7de8e

--- widget/src/windows/nsWindow.cpp
+++ widget/src/windows/nsWindow.cpp
@@ -4216,6 +4216,7 @@ nsWindow::IPCWindowProcHandler(UINT& msg, WPARAM& wParam, LPARAM& lParam)
 
 
 
+#ifdef _MSC_VER
 static int ReportException(EXCEPTION_POINTERS *aExceptionInfo)
 {
 #ifdef MOZ_CRASHREPORTER
@@ -4226,18 +4227,23 @@ static int ReportException(EXCEPTION_POINTERS *aExceptionInfo)
 #endif
   return EXCEPTION_EXECUTE_HANDLER;
 }
+#endif
 
 
 
 
 LRESULT CALLBACK nsWindow::WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+#ifdef _MSC_VER
   __try {
     return WindowProcInternal(hWnd, msg, wParam, lParam);
   }
   __except(ReportException(GetExceptionInformation())) {
     ::TerminateProcess(::GetCurrentProcess(), 253);
   }
+#else
+  return WindowProcInternal(hWnd, msg, wParam, lParam);
+#endif
 }
 
 LRESULT CALLBACK nsWindow::WindowProcInternal(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
