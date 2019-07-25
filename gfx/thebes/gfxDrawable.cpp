af5ccf1f2e0ecd6ad21aa88169ee1e14afb1d9a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:27:33 +0000

    Bug 572680 - Create gfxDrawable interface and implementations for surfaces, patterns and drawing callbacks. r=joe
    UltraBlame original commit: 0fa683b7233de09df0471a31a9b337e2d507045e

---
+++ gfx/thebes/gfxDrawable.cpp
@@ -0,0 +1,254 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "gfxDrawable.h"
+#include "gfxASurface.h"
+#include "gfxContext.h"
+#include "gfxPlatform.h"
+
+gfxSurfaceDrawable::gfxSurfaceDrawable(gfxASurface* aSurface,
+                                       const gfxIntSize aSize,
+                                       const gfxMatrix aTransform)
+ : gfxDrawable(aSize)
+ , mSurface(aSurface)
+ , mTransform(aTransform)
+{
+}
+
+static gfxMatrix
+DeviceToImageTransform(gfxContext* aContext,
+                       const gfxMatrix& aUserSpaceToImageSpace)
+{
+    gfxFloat deviceX, deviceY;
+    nsRefPtr<gfxASurface> currentTarget =
+        aContext->CurrentSurface(&deviceX, &deviceY);
+    gfxMatrix currentMatrix = aContext->CurrentMatrix();
+    gfxMatrix deviceToUser = gfxMatrix(currentMatrix).Invert();
+    deviceToUser.Translate(-gfxPoint(-deviceX, -deviceY));
+    return gfxMatrix(deviceToUser).Multiply(aUserSpaceToImageSpace);
+}
+
+static void 
+PreparePatternForUntiledDrawing(gfxPattern* aPattern,
+                                const gfxMatrix& aDeviceToImage,
+                                gfxASurface::gfxSurfaceType aSurfaceType,
+                                const gfxPattern::GraphicsFilter aDefaultFilter)
+{
+    
+    
+    
+    switch (aSurfaceType) {
+        case gfxASurface::SurfaceTypeXlib:
+        case gfxASurface::SurfaceTypeXcb:
+        {
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            PRBool isDownscale =
+                aDeviceToImage.xx >= 1.0 && aDeviceToImage.yy >= 1.0 &&
+                aDeviceToImage.xy == 0.0 && aDeviceToImage.yx == 0.0;
+            if (!isDownscale) {
+                aPattern->SetFilter(gfxPattern::FILTER_FAST);
+            }
+            break;
+        }
+
+        case gfxASurface::SurfaceTypeQuartz:
+        case gfxASurface::SurfaceTypeQuartzImage:
+            
+            aPattern->SetFilter(aDefaultFilter);
+            break;
+
+        default:
+            
+            
+            
+            aPattern->SetExtend(gfxPattern::EXTEND_PAD);
+            aPattern->SetFilter(aDefaultFilter);
+            break;
+    }
+}
+
+PRBool
+gfxSurfaceDrawable::Draw(gfxContext* aContext,
+                         const gfxRect& aFillRect,
+                         PRBool aRepeat,
+                         const gfxPattern::GraphicsFilter& aFilter,
+                         const gfxMatrix& aTransform)
+{
+    nsRefPtr<gfxPattern> pattern = new gfxPattern(mSurface);
+    if (aRepeat) {
+        pattern->SetExtend(gfxPattern::EXTEND_REPEAT);
+        pattern->SetFilter(aFilter);
+    } else {
+        nsRefPtr<gfxASurface> currentTarget = aContext->CurrentSurface();
+        gfxASurface::gfxSurfaceType surfaceType = currentTarget->GetType();
+        gfxMatrix deviceSpaceToImageSpace =
+            DeviceToImageTransform(aContext, aTransform);
+        PreparePatternForUntiledDrawing(pattern, deviceSpaceToImageSpace,
+                                        surfaceType, aFilter);
+    }
+#ifdef MOZ_GFX_OPTIMIZE_MOBILE
+    pattern->SetFilter(gfxPattern::FILTER_FAST); 
+#endif
+    pattern->SetMatrix(gfxMatrix(aTransform).Multiply(mTransform));
+    aContext->NewPath();
+    aContext->SetPattern(pattern);
+    aContext->Rectangle(aFillRect);
+    aContext->Fill();
+    return PR_TRUE;
+}
+
+gfxCallbackDrawable::gfxCallbackDrawable(gfxDrawingCallback* aCallback,
+                                         const gfxIntSize aSize)
+ : gfxDrawable(aSize)
+ , mCallback(aCallback)
+{
+}
+
+already_AddRefed<gfxSurfaceDrawable>
+gfxCallbackDrawable::MakeSurfaceDrawable(const gfxPattern::GraphicsFilter aFilter)
+{
+    nsRefPtr<gfxASurface> surface =
+        gfxPlatform::GetPlatform()->CreateOffscreenSurface(mSize, gfxASurface::ImageFormatARGB32);
+    if (!surface || surface->CairoStatus() != 0)
+        return nsnull;
+
+    nsRefPtr<gfxContext> ctx = new gfxContext(surface);
+    Draw(ctx, gfxRect(0, 0, mSize.width, mSize.height), PR_FALSE, aFilter);
+    nsRefPtr<gfxSurfaceDrawable> drawable = new gfxSurfaceDrawable(surface, mSize);
+    return drawable.forget();
+}
+
+PRBool
+gfxCallbackDrawable::Draw(gfxContext* aContext,
+                          const gfxRect& aFillRect,
+                          PRBool aRepeat,
+                          const gfxPattern::GraphicsFilter& aFilter,
+                          const gfxMatrix& aTransform)
+{
+    if (aRepeat && !mSurfaceDrawable) {
+        mSurfaceDrawable = MakeSurfaceDrawable(aFilter);
+    }
+
+    if (mSurfaceDrawable)
+        return mSurfaceDrawable->Draw(aContext, aFillRect, aRepeat, aFilter,
+                                      aTransform);
+
+    if (mCallback)
+        return (*mCallback)(aContext, aFillRect, aFilter, aTransform);
+
+    return PR_FALSE;
+}
+
+gfxPatternDrawable::gfxPatternDrawable(gfxPattern* aPattern,
+                                       const gfxIntSize aSize)
+ : gfxDrawable(aSize)
+ , mPattern(aPattern)
+{
+}
+
+class DrawingCallbackFromDrawable : public gfxDrawingCallback {
+public:
+    DrawingCallbackFromDrawable(gfxDrawable* aDrawable)
+     : mDrawable(aDrawable) {
+        NS_ASSERTION(aDrawable, "aDrawable is null!");
+    }
+
+    virtual ~DrawingCallbackFromDrawable() {}
+
+    virtual PRBool operator()(gfxContext* aContext,
+                              const gfxRect& aFillRect,
+                              const gfxPattern::GraphicsFilter& aFilter,
+                              const gfxMatrix& aTransform = gfxMatrix())
+    {
+        return mDrawable->Draw(aContext, aFillRect, PR_FALSE, aFilter,
+                               aTransform);
+    }
+private:
+    nsRefPtr<gfxDrawable> mDrawable;
+};
+
+already_AddRefed<gfxCallbackDrawable>
+gfxPatternDrawable::MakeCallbackDrawable()
+{
+    nsRefPtr<gfxDrawingCallback> callback =
+        new DrawingCallbackFromDrawable(this);
+    nsRefPtr<gfxCallbackDrawable> callbackDrawable =
+        new gfxCallbackDrawable(callback, mSize);
+    return callbackDrawable.forget();
+}
+
+PRBool
+gfxPatternDrawable::Draw(gfxContext* aContext,
+                         const gfxRect& aFillRect,
+                         PRBool aRepeat,
+                         const gfxPattern::GraphicsFilter& aFilter,
+                         const gfxMatrix& aTransform)
+{
+    if (!mPattern)
+        return PR_FALSE;
+
+    if (aRepeat) {
+        
+        
+        
+        
+        
+        
+        
+        nsRefPtr<gfxCallbackDrawable> callbackDrawable = MakeCallbackDrawable();
+        return callbackDrawable->Draw(aContext, aFillRect, PR_TRUE, aFilter,
+                                      aTransform);
+    }
+
+    aContext->NewPath();
+    gfxMatrix oldMatrix = mPattern->GetMatrix();
+    mPattern->SetMatrix(gfxMatrix(aTransform).Multiply(oldMatrix));
+    aContext->SetPattern(mPattern);
+    aContext->Rectangle(aFillRect);
+    aContext->Fill();
+    mPattern->SetMatrix(oldMatrix);
+    return PR_TRUE;
+}
---
+++ gfx/thebes/gfxDrawable.h
@@ -0,0 +1,169 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef GFX_DRAWABLE_H
+#define GFX_DRAWABLE_H
+
+#include "nsISupportsImpl.h"
+#include "nsAutoPtr.h"
+#include "gfxTypes.h"
+#include "gfxRect.h"
+#include "gfxColor.h"
+#include "gfxMatrix.h"
+#include "gfxPattern.h"
+
+class gfxASurface;
+class gfxContext;
+
+
+
+
+
+
+class THEBES_API gfxDrawable {
+    NS_INLINE_DECL_REFCOUNTING(gfxDrawable)
+public:
+    gfxDrawable(const gfxIntSize aSize)
+     : mSize(aSize) {}
+    virtual ~gfxDrawable() {}
+
+    
+
+
+
+
+
+
+    virtual PRBool Draw(gfxContext* aContext,
+                        const gfxRect& aFillRect,
+                        PRBool aRepeat,
+                        const gfxPattern::GraphicsFilter& aFilter,
+                        const gfxMatrix& aTransform = gfxMatrix()) = 0;
+    virtual gfxIntSize Size() { return mSize; }
+
+protected:
+    const gfxIntSize mSize;
+};
+
+
+
+
+
+class THEBES_API gfxSurfaceDrawable : public gfxDrawable {
+public:
+    gfxSurfaceDrawable(gfxASurface* aSurface, const gfxIntSize aSize,
+                       const gfxMatrix aTransform = gfxMatrix());
+    virtual ~gfxSurfaceDrawable() {}
+
+    virtual PRBool Draw(gfxContext* aContext,
+                        const gfxRect& aFillRect,
+                        PRBool aRepeat,
+                        const gfxPattern::GraphicsFilter& aFilter,
+                        const gfxMatrix& aTransform = gfxMatrix());
+
+protected:
+    nsRefPtr<gfxASurface> mSurface;
+    const gfxMatrix mTransform;
+};
+
+
+
+
+
+class THEBES_API gfxDrawingCallback {
+    NS_INLINE_DECL_REFCOUNTING(gfxDrawingCallback)
+public:
+    virtual ~gfxDrawingCallback() {}
+
+    
+
+
+
+
+
+
+    virtual PRBool operator()(gfxContext* aContext,
+                              const gfxRect& aFillRect,
+                              const gfxPattern::GraphicsFilter& aFilter,
+                              const gfxMatrix& aTransform = gfxMatrix()) = 0;
+
+};
+
+
+
+
+
+class THEBES_API gfxCallbackDrawable : public gfxDrawable {
+public:
+    gfxCallbackDrawable(gfxDrawingCallback* aCallback, const gfxIntSize aSize);
+    virtual ~gfxCallbackDrawable() {}
+
+    virtual PRBool Draw(gfxContext* aContext,
+                        const gfxRect& aFillRect,
+                        PRBool aRepeat,
+                        const gfxPattern::GraphicsFilter& aFilter,
+                        const gfxMatrix& aTransform = gfxMatrix());
+
+protected:
+    already_AddRefed<gfxSurfaceDrawable> MakeSurfaceDrawable(const gfxPattern::GraphicsFilter aFilter = gfxPattern::FILTER_FAST);
+
+    nsRefPtr<gfxDrawingCallback> mCallback;
+    nsRefPtr<gfxSurfaceDrawable> mSurfaceDrawable;
+};
+
+
+
+
+
+class THEBES_API gfxPatternDrawable : public gfxDrawable {
+public:
+    gfxPatternDrawable(gfxPattern* aPattern,
+                       const gfxIntSize aSize);
+    virtual ~gfxPatternDrawable() {}
+
+    virtual PRBool Draw(gfxContext* aContext,
+                        const gfxRect& aFillRect,
+                        PRBool aRepeat,
+                        const gfxPattern::GraphicsFilter& aFilter,
+                        const gfxMatrix& aTransform = gfxMatrix());
+
+protected:
+    already_AddRefed<gfxCallbackDrawable> MakeCallbackDrawable();
+
+    nsRefPtr<gfxPattern> mPattern;
+};
+
+#endif 
