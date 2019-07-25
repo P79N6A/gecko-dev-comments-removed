70a168df2b8252734a02608dcaa23f065feaa895
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:50:25 +0000

    Bug 600569: show build ID in about dialog for non-release builds, r=dolske, ui-review=limi, a=dolske
    UltraBlame original commit: 8972153efa0ee4bef16c4b19bbd8f8681b1de70e

--- browser/base/content/aboutDialog.js
+++ browser/base/content/aboutDialog.js
@@ -65,6 +65,14 @@ function init(aEvent)
     
   }
 
+  
+  let version = Services.appinfo.version;
+  if (version.indexOf("pre") != -1) {
+    let buildID = Services.appinfo.appBuildID;
+    let buildDate = buildID.slice(0,4) + "-" + buildID.slice(4,6) + "-" + buildID.slice(6,8);
+    document.getElementById("version").value += " (" + buildDate + ")";
+  }
+
 #ifdef MOZ_UPDATER
   gAppUpdater = new appUpdater();
 #endif
