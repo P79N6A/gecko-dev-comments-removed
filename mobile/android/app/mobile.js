0e7ba51cbed2aeae8e338582c79bae311e1a7dda
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:46:55 +0000

    Bug 708774 - Always use fullscreen landscape keyboard in native Android fennec [r=mfinkle]
    UltraBlame original commit: 0ec894d6c37c3351d5b77f2d92ca563aa4c857e4

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -604,7 +604,7 @@ pref("media.preload.auto", 2);
 
 
 
-pref("widget.ime.android.landscape_fullscreen", -1);
+pref("widget.ime.android.landscape_fullscreen", 1);
 pref("widget.ime.android.fullscreen_threshold", 250); 
 
 
