5a4b25f0a384dfeab7ab0a0731c3f2f024e2703e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:45:14 +0000

    Bug 678201 - Use CallArgs in Debugger.cpp. Part 2, move THIS_DEBUGGER just before its first use. r=luke.
    UltraBlame original commit: ab52b131c8b44ee0c8a5add653679feb25687b84

--- js/src/vm/Debugger.cpp
+++ js/src/vm/Debugger.cpp
@@ -117,38 +117,6 @@ ReportObjectRequired(JSContext *cx)
     return false;
 }
 
-Debugger *
-Debugger::fromThisValue(JSContext *cx, Value *vp, const char *fnname)
-{
-    if (!vp[1].isObject()) {
-        ReportObjectRequired(cx);
-        return NULL;
-    }
-    JSObject *thisobj = &vp[1].toObject();
-    if (thisobj->getClass() != &Debugger::jsclass) {
-        JS_ReportErrorNumber(cx, js_GetErrorMessage, NULL, JSMSG_INCOMPATIBLE_PROTO,
-                             "Debugger", fnname, thisobj->getClass()->name);
-        return NULL;
-    }
-
-    
-
-
-
-
-    Debugger *dbg = fromJSObject(thisobj);
-    if (!dbg) {
-        JS_ReportErrorNumber(cx, js_GetErrorMessage, NULL, JSMSG_INCOMPATIBLE_PROTO,
-                             "Debugger", fnname, "prototype object");
-    }
-    return dbg;
-}
-
-#define THIS_DEBUGGER(cx, vp, fnname, dbg)                                   \
-    Debugger *dbg = Debugger::fromThisValue(cx, vp, fnname);                 \
-    if (!dbg)                                                                \
-        return false
-
 
 
 
@@ -1166,6 +1134,38 @@ Class Debugger::jsclass = {
     NULL,                 
     Debugger::traceObject
 };
+
+Debugger *
+Debugger::fromThisValue(JSContext *cx, Value *vp, const char *fnname)
+{
+    if (!vp[1].isObject()) {
+        ReportObjectRequired(cx);
+        return NULL;
+    }
+    JSObject *thisobj = &vp[1].toObject();
+    if (thisobj->getClass() != &Debugger::jsclass) {
+        JS_ReportErrorNumber(cx, js_GetErrorMessage, NULL, JSMSG_INCOMPATIBLE_PROTO,
+                             "Debugger", fnname, thisobj->getClass()->name);
+        return NULL;
+    }
+
+    
+
+
+
+
+    Debugger *dbg = fromJSObject(thisobj);
+    if (!dbg) {
+        JS_ReportErrorNumber(cx, js_GetErrorMessage, NULL, JSMSG_INCOMPATIBLE_PROTO,
+                             "Debugger", fnname, "prototype object");
+    }
+    return dbg;
+}
+
+#define THIS_DEBUGGER(cx, vp, fnname, dbg)                                   \
+    Debugger *dbg = Debugger::fromThisValue(cx, vp, fnname);                 \
+    if (!dbg)                                                                \
+        return false
 
 JSBool
 Debugger::getEnabled(JSContext *cx, uintN argc, Value *vp)
