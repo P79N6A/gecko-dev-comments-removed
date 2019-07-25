9fde61030adcbe70182d6f1d73a3aa6ba9c510ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:01:27 +0000

    Bug 613551 - "JSGC_MAX_MALLOC_BYTES is too large on mobile" [r=jst] a=blocking-fennec
    UltraBlame original commit: c5428f83814ef46244681c5b3eb0e9c4f583c240

--- dom/base/nsJSEnvironment.cpp
+++ dom/base/nsJSEnvironment.cpp
@@ -4004,26 +4004,24 @@ ReportAllJSExceptionsPrefChangedCallback(const char* aPrefName, void* aClosure)
 static int
 SetMemoryHighWaterMarkPrefChangedCallback(const char* aPrefName, void* aClosure)
 {
-  PRInt32 highwatermark = nsContentUtils::GetIntPref(aPrefName, 32);
+  PRInt32 highwatermark = nsContentUtils::GetIntPref(aPrefName, 128);
 
-  if (highwatermark >= 32) {
-    
+  JS_SetGCParameter(nsJSRuntime::sRuntime, JSGC_MAX_MALLOC_BYTES,
+                    highwatermark * 1024L * 1024L);
+  return 0;
+}
 
+static int
+SetMemoryMaxPrefChangedCallback(const char* aPrefName, void* aClosure)
+{
+  PRUint32 max = nsContentUtils::GetIntPref(aPrefName, -1);
+  if (max == -1UL)
+    max = 0xffffffff;
+  else
+    max = max * 1024L * 1024L;
 
-
-
-
-
-    JS_SetGCParameter(nsJSRuntime::sRuntime, JSGC_MAX_MALLOC_BYTES,
-                      128L * 1024L * 1024L);
-    JS_SetGCParameter(nsJSRuntime::sRuntime, JSGC_MAX_BYTES,
-                      0xffffffff);
-  } else {
-    JS_SetGCParameter(nsJSRuntime::sRuntime, JSGC_MAX_MALLOC_BYTES,
-                      highwatermark * 1024L * 1024L);
-    JS_SetGCParameter(nsJSRuntime::sRuntime, JSGC_MAX_BYTES,
-                      highwatermark * 1024L * 1024L);
-  }
+  JS_SetGCParameter(nsJSRuntime::sRuntime, JSGC_MAX_BYTES,
+                    max);
   return 0;
 }
 
@@ -4160,6 +4158,12 @@ nsJSRuntime::Init()
                                        nsnull);
   SetMemoryHighWaterMarkPrefChangedCallback("javascript.options.mem.high_water_mark",
                                             nsnull);
+
+  nsContentUtils::RegisterPrefCallback("javascript.options.mem.max",
+                                       SetMemoryMaxPrefChangedCallback,
+                                       nsnull);
+  SetMemoryMaxPrefChangedCallback("javascript.options.mem.max",
+                                  nsnull);
 
   nsContentUtils::RegisterPrefCallback("javascript.options.mem.gc_frequency",
                                        SetMemoryGCFrequencyPrefChangedCallback,
