6ef375d4c308482a895cb7b26f7ab93d7cca7fc8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:11:27 +0000

    Bug 752402 - Support ArrayBufferView as a member of blobParts parameter. r=sicking
    UltraBlame original commit: 79d51b043d45a098ead242d9769a8ec895cb7eed

--- content/base/src/nsDOMBlobBuilder.cpp
+++ content/base/src/nsDOMBlobBuilder.cpp
@@ -211,19 +211,18 @@ nsDOMMultipartFile::InitInternal(JSContext* aCx,
     if (d.endings.EqualsLiteral("native")) {
       nativeEOL = true;
     } else if (!d.endings.EqualsLiteral("transparent")) {
-      return NS_ERROR_DOM_INVALID_STATE_ERR;
+      return NS_ERROR_TYPE_ERR;
     }
   }
 
   if (aArgc > 0) {
     if (!aArgv[0].isObject()) {
-      return NS_ERROR_INVALID_ARG; 
+      return NS_ERROR_TYPE_ERR; 
     }
 
     JSObject& obj = aArgv[0].toObject();
-
     if (!JS_IsArrayObject(aCx, &obj)) {
-      return NS_ERROR_INVALID_ARG; 
+      return NS_ERROR_TYPE_ERR; 
     }
 
     BlobSet blobSet;
@@ -233,7 +232,7 @@ nsDOMMultipartFile::InitInternal(JSContext* aCx,
     for (uint32_t i = 0; i < length; ++i) {
       jsval element;
       if (!JS_GetElement(aCx, &obj, i, &element))
-        return NS_ERROR_INVALID_ARG;
+        return NS_ERROR_TYPE_ERR;
 
       if (element.isObject()) {
         JSObject& obj = element.toObject();
@@ -249,18 +248,26 @@ nsDOMMultipartFile::InitInternal(JSContext* aCx,
           } else {
             blobSet.AppendBlob(blob);
           }
-        } else if (JS_IsArrayBufferObject(&obj, aCx)) {
-          blobSet.AppendArrayBuffer(&obj, aCx);
-        } else {
-          
-          return NS_ERROR_DOM_INVALID_STATE_ERR;
+          continue;
         }
+        if (JS_IsArrayBufferViewObject(&obj, aCx)) {
+          blobSet.AppendVoidPtr(JS_GetArrayBufferViewData(&obj, aCx),
+                                JS_GetArrayBufferViewByteLength(&obj, aCx));
+          continue;
+        }
+        if (JS_IsArrayBufferObject(&obj, aCx)) {
+          blobSet.AppendArrayBuffer(&obj, aCx);
+          continue;
+        }
+        
       } else if (element.isString()) {
         blobSet.AppendString(element.toString(), nativeEOL, aCx);
-      } else {
-        
-        return NS_ERROR_DOM_INVALID_STATE_ERR;
+        continue;
       }
+      
+      JSString* str = JS_ValueToString(aCx, element);
+      NS_ENSURE_TRUE(str, NS_ERROR_TYPE_ERR);
+      blobSet.AppendString(str, nativeEOL, aCx);
     }
 
     mBlobs = blobSet.GetBlobs();
