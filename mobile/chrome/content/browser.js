50dcb3c53ad532bbbbe0d8c5291ecaa975f8a3eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:02:53 +0000

    Bug 558840 - Botched tab preview for start page [r=mfinkle]
    UltraBlame original commit: 9051a192cc7c5b8e8de6806181e4a621c4faa760

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -411,6 +411,15 @@ var Browser = {
       
       BrowserUI.sizeControls(w, h);
 
+      
+      
+      
+      let bvs = bv._browserViewportState;
+      if (bvs.viewportRect.width == 1 && bvs.viewportRect.height == 1) {
+        bvs.viewportRect.width = window.innerWidth;
+        bvs.viewportRect.height = window.innerHeight;
+      }
+
       bv.updateDefaultZoom();
       if (bv.isDefaultZoom())
         
@@ -3175,7 +3184,9 @@ Tab.prototype = {
     if (!this._browser)
       return;
 
-    let browserView = (Browser.selectedBrowser == this._browser) ? Browser._browserView : null;
+    let bv = Browser._browserView;
+    let browserView = (Browser.selectedBrowser == this._browser && bv.isDefaultZoom()) ? Browser._browserView 
+                                                                                       : null;
     this._chromeTab.updateThumbnail(this._browser, browserView);
   },
 
