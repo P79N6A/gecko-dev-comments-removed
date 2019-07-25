04ae16485689bbfcd9807b22dc76ae1cf8179682
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:22:00 +0000

    Bug 698593 - (2/3) Add the "About Firefox" preference [r=sriram]
    UltraBlame original commit: bc9ef9db0bd11542482b534dfd2c5690ab0cc44d

---
+++ mobile/android/base/LinkPreference.java
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package org.mozilla.gecko;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.util.AttributeSet;
+
+class LinkPreference extends Preference {
+    private String mUrl = null;
+
+    public LinkPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mUrl = attrs.getAttributeValue(null, "url");
+    }
+    public LinkPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mUrl = attrs.getAttributeValue(null, "url");
+    }
+
+    @Override
+    protected void onClick() {
+        GeckoApp.mAppContext.loadUrl(mUrl, AwesomeBar.Type.ADD);
+        callChangeListener(mUrl);
+    }
+}
