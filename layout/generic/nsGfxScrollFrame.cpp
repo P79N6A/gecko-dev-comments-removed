3043d027b9468341b9adf4a30fdc7f9ae708761c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:26:58 +0000

    Bug 654369 - Part 9: Remove OOM checks from layout/generic r=bz
    UltraBlame original commit: acec89b9786bb24df059210436ff7db4ab1e1cf7

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -1561,14 +1561,10 @@ nsGfxScrollFrameInner::ScrollTo(nsPoint aScrollPosition,
     }
   } else {
     mAsyncScroll = new AsyncScroll;
-    if (mAsyncScroll) {
-      mAsyncScroll->mScrollTimer = do_CreateInstance("@mozilla.org/timer;1");
-      if (!mAsyncScroll->mScrollTimer) {
-        delete mAsyncScroll;
-        mAsyncScroll = nsnull;
-      }
-    }
-    if (!mAsyncScroll) {
+    mAsyncScroll->mScrollTimer = do_CreateInstance("@mozilla.org/timer;1");
+    if (!mAsyncScroll->mScrollTimer) {
+      delete mAsyncScroll;
+      mAsyncScroll = nsnull;
       
       ScrollToImpl(mDestination);
       return;
@@ -3619,10 +3615,7 @@ nsGfxScrollFrameInner::SaveState(nsIStatefulFrame::SpecialStateID aStateID)
   }
 
   nsPresState* state = new nsPresState();
-  if (!state) {
-    return nsnull;
-  }
- 
+
   state->SetScrollState(scrollPos);
 
   return state;
--- layout/generic/nsObjectFrame.cpp
+++ layout/generic/nsObjectFrame.cpp
@@ -2102,9 +2102,6 @@ nsObjectFrame::PrepareInstanceOwner()
          ("Created new instance owner %p for frame %p\n", mInstanceOwner.get(),
           this));
 
-  if (!mInstanceOwner)
-    return NS_ERROR_OUT_OF_MEMORY;
-
   
   return mInstanceOwner->Init(PresContext(), this, GetContent());
 }
