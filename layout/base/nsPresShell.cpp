0c0bc7b12f9ffdf55e35ab8a7fa0d9231a0ac037
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:29:25 +0000

    Bug 575336.  Stop flushing out layout on the whole viewmanager tree when layout is flushed on a document and results in invalidates.  r=roc
    UltraBlame original commit: 00dfcf7f4c9eee1b09c632fb45d814e9bea4faa9

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -4827,12 +4827,6 @@ PresShell::FlushPendingNotifications(mozFlushType aType)
       
       updateFlags = NS_VMREFRESH_IMMEDIATE;
     }
-    else if (aType < Flush_InterruptibleLayout) {
-      
-      
-      
-      updateFlags = NS_VMREFRESH_DEFERRED;
-    }
     batch.EndUpdateViewBatch(updateFlags);
   }
 }
