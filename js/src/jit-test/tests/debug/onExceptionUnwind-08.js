297b09a67a282740839fd42a9eb429eafdab6b1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:45:25 +0000

    Bug 717417: Don't skip ScriptDebugEpilogue when an onExceptionUnwind handler throws an uncaught exception or terminates the debuggee. r=jorendorff
    UltraBlame original commit: 650f4fa9ca1244e9538edb4daabebbc805e6eee5

---
+++ js/src/jit-test/tests/debug/onExceptionUnwind-08.js
@@ -0,0 +1,18 @@
+
+
+var g = newGlobal('new-compartment');
+var dbg = Debugger(g);
+var frame;
+dbg.onExceptionUnwind = function (f, x) {
+    frame = f;
+    assertEq(frame.live, true);
+    throw 'unhandled';
+};
+dbg.onDebuggerStatement = function(f) {
+    assertEq(f.eval('throw 42'), null);
+    assertEq(frame.live, false);
+};
+g.eval('debugger');
+
+
+quit(0);
