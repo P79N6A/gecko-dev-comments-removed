138b3706b076eb45b7db9c84cee483834b5e1fcf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:12:03 +0000

    Bug 563327 part 1.  Expose an nsIPresShell API for refresh observers.  r=roc
    UltraBlame original commit: fee5701c664e4ec3222483313ea71b52ac514d44

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -11835,8 +11835,7 @@ nsCSSFrameConstructor::PostRestyleEventInternal(PRBool aForLazyConstruction)
   
   PRBool inRefresh = aForLazyConstruction ? mInLazyFCRefresh : mInStyleRefresh;
   if (!mObservingRefreshDriver && !inRefresh) {
-    mObservingRefreshDriver = mPresShell->GetPresContext()->
-      RefreshDriver()->AddRefreshObserver(this, Flush_Style);
+    mObservingRefreshDriver = mPresShell->AddRefreshObserver(this, Flush_Style);
   }
 }
 
@@ -11848,8 +11847,7 @@ nsCSSFrameConstructor::WillRefresh(mozilla::TimeStamp aTime)
   
   
   
-  mPresShell->GetPresContext()->RefreshDriver()->
-    RemoveRefreshObserver(this, Flush_Style);
+  mPresShell->RemoveRefreshObserver(this, Flush_Style);
   mObservingRefreshDriver = PR_FALSE;
   mInLazyFCRefresh = PR_TRUE;
   mInStyleRefresh = PR_TRUE;
