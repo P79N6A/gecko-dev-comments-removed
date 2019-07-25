1c37b2e3c6d4337277986ab37e0823b509050b40
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:02:20 +0000

    Bug 740769. (Cv1a) test_bug325418.js: Create separate observers, Initialize gStartTime[12] later, Use Date.now() instead of (new Date()).getTime(), "timer = null;" a bit later, Nits. r=bzbarsky.
    UltraBlame original commit: 2374ebd5026f86aab18d3e4fbfd484e5b3f5cbbb

--- xpcom/tests/unit/test_bug325418.js
+++ xpcom/tests/unit/test_bug325418.js
@@ -1,19 +1,44 @@
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 
-var timer;
-const start_time = (new Date()).getTime();
-const expected_time = 1;
 
-var observer = {
-  observe: function observeTC(subject, topic, data) {
+const kExpectedDelay1 = 5;
+
+const kExpectedDelay2 = 1;
+
+var gStartTime1;
+var gStartTime2;
+var timer;
+
+var observer1 = {
+  observe: function observeTC1(subject, topic, data) {
     if (topic == "timer-callback") {
+      
       timer.cancel();
-      timer = null;
 
       
-      var result = Math.floor(((new Date()).getTime() - start_time) / 1000);
-      do_check_eq(result, expected_time);
+      do_check_eq(Math.floor((Date.now() - gStartTime1) / 1000),
+                  kExpectedDelay1);
+
+      timer = null;
+
+      do_print("1st timer triggered (before being cancelled). Should not have happened!");
+      do_check_true(false);
+    }
+  }
+};
+
+var observer2 = {
+  observe: function observeTC2(subject, topic, data) {
+    if (topic == "timer-callback") {
+      
+      timer.cancel();
+
+      
+      do_check_eq(Math.floor((Date.now() - gStartTime2) / 1000),
+                  kExpectedDelay2);
+
+      timer = null;
 
       do_test_finished();
     }
@@ -24,8 +49,13 @@ function run_test() {
   do_test_pending();
 
   timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+
   
-  timer.init(observer, 5000, timer.TYPE_REPEATING_PRECISE);
+  gStartTime1 = Date.now();
+  timer.init(observer1, kExpectedDelay1 * 1000, timer.TYPE_REPEATING_PRECISE);
   timer.cancel();
-  timer.init(observer, 1000, timer.TYPE_REPEATING_PRECISE);
+
+  
+  gStartTime2 = Date.now();
+  timer.init(observer2, kExpectedDelay2 * 1000, timer.TYPE_REPEATING_PRECISE);
 }
