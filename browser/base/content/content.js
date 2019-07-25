56c10ccd016a811a0d416581205215ab0e44ed40
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:31:26 +0000

    fix whitespace, DONTBUILD
    UltraBlame original commit: 65881b66e992242773c2177077c2b79ac46a4e4c

--- browser/base/content/content.js
+++ browser/base/content/content.js
@@ -42,11 +42,11 @@ const Cu = Components.utils;
 
 
 
-__defineGetter__("webNavigation", function() {
+__defineGetter__("webNavigation", function () {
   return docShell.QueryInterface(Ci.nsIWebNavigation);
 });
 
-addMessageListener("WebNavigation:LoadURI", function(message) {
+addMessageListener("WebNavigation:LoadURI", function (message) {
   let flags = message.json.flags || webNavigation.LOAD_FLAGS_NONE;
 
   webNavigation.loadURI(message.json.uri, flags, null, null, null);
