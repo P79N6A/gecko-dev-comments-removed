5be7e696760c86ca3caba4993a8c930a5861b6c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:36:03 +0000

    Another missing file!
    UltraBlame original commit: 91789d0400b9376cbf8a9b221575d74f54a21267

---
+++ dom/src/geolocation/nsGeolocationOOP.h
@@ -0,0 +1,87 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsGeolocationOOP_h
+#define nsGeolocationOOP_h
+
+#include "base/basictypes.h"
+
+#include "nsIGeolocationProvider.h"
+#include "nsString.h"
+#include "nsIDOMElement.h"
+
+#include "mozilla/dom/PGeolocationRequestParent.h"
+
+class nsGeolocationRequestProxy;
+
+namespace mozilla {
+namespace dom {
+
+class GeolocationRequestParent : public PGeolocationRequestParent
+{
+ public:
+  GeolocationRequestParent(nsIDOMElement *element, const IPC::URI& principal);
+  virtual ~GeolocationRequestParent();
+  
+  nsCOMPtr<nsIURI>           mURI;
+  nsCOMPtr<nsIDOMElement>    mElement;
+  nsGeolocationRequestProxy *mProxy;
+
+ private:  
+  virtual bool Recvprompt();
+};
+  
+} 
+} 
+
+class nsGeolocationRequestProxy : public nsIGeolocationRequest
+{
+ public:
+  nsGeolocationRequestProxy();
+  virtual ~nsGeolocationRequestProxy();
+  
+  nsresult Init(mozilla::dom::GeolocationRequestParent* parent);
+  
+  NS_DECL_ISUPPORTS;
+  NS_DECL_NSIGEOLOCATIONREQUEST;
+
+ private:
+  
+  mozilla::dom::GeolocationRequestParent* mParent;
+};
+#endif 
+
