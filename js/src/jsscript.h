319be34eb9399f8eee629defdcc7b40e3ff1b1a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:34:33 +0000

    Bug 776314 - Add a script flag indicating if the script is a generator expression. r=luke
    UltraBlame original commit: f939ff2f29fbf1869b9fd42eb1a206354d6a1f9d

--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -540,6 +540,7 @@ struct JSScript : public js::gc::Cell
 #endif
     bool            callDestroyHook:1;
     bool            isGenerator:1;    
+    bool            isGeneratorExp:1; 
     bool            hasScriptCounts:1;
 
     bool            hasSourceMap:1;   
