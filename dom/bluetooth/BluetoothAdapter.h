b2384023a55bc1805e8858f168c589fb0d653d24
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:00:26 +0000

    Bug 731361 - Change bluetooth firmware loading to use a DOMRequest based API. r=bent
    UltraBlame original commit: 722d3bd080c2241b3bcc1ce486e39758ebfca320

---
+++ dom/bluetooth/BluetoothAdapter.h
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_bluetooth_bluetoothadapter_h__
+#define mozilla_dom_bluetooth_bluetoothadapter_h__
+
+#include "BluetoothCommon.h"
+#include "nsDOMEventTargetHelper.h"
+#include "nsIDOMBluetoothAdapter.h"
+#include "nsIDOMDOMRequest.h"
+
+class nsIEventTarget;
+
+BEGIN_BLUETOOTH_NAMESPACE
+
+class BluetoothAdapter : public nsIDOMBluetoothAdapter
+                       , public nsDOMEventTargetHelper
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIDOMBLUETOOTHADAPTER
+
+  NS_FORWARD_NSIDOMEVENTTARGET(nsDOMEventTargetHelper::)
+
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(BluetoothAdapter,
+                                           nsDOMEventTargetHelper)
+
+  BluetoothAdapter(nsPIDOMWindow*);
+
+  inline void SetEnabledInternal(bool aEnabled) {mEnabled = aEnabled;}
+
+protected:
+  bool mEnabled;
+
+  NS_DECL_EVENT_HANDLER(enabled)
+
+private:
+  nsCOMPtr<nsIEventTarget> mToggleBtThread;
+};
+
+END_BLUETOOTH_NAMESPACE
+
+#endif
