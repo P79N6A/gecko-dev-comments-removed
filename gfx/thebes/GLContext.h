4583dd1afa619826ba83d9742a09da3d67489410
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:02:19 +0000

    Bug 615976 - Fix ResizeOffscreenFBO to attempt resize without AA if resize with AA failed - r=bjacob
    UltraBlame original commit: bf09dbe3fcd74c455ce5b6196b1ee752089410d5

--- gfx/thebes/GLContext.h
+++ gfx/thebes/GLContext.h
@@ -1321,7 +1321,16 @@ protected:
 
     
     
-    bool ResizeOffscreenFBO(const gfxIntSize& aSize, const bool aUseReadFBO);
+    bool ResizeOffscreenFBO(const gfxIntSize& aSize, const bool aUseReadFBO, const bool aDisableAA);
+    bool ResizeOffscreenFBO(const gfxIntSize& aSize, const bool aUseReadFBO) {
+        if (ResizeOffscreenFBO(aSize, aUseReadFBO, false))
+            return true;
+
+        if (!mCreationFormat.samples)
+            return false;
+        printf("ResizeOffscreenFBO failed with AA, retrying without...\n");
+        return ResizeOffscreenFBO(aSize, aUseReadFBO, true);
+    }
     void DeleteOffscreenFBO();
     GLuint mOffscreenDrawFBO;
     GLuint mOffscreenReadFBO;
