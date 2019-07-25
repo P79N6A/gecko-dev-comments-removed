47704005f66920039c1f6542af5bca4f9d0881a1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:26:47 +0000

    Bug 756308. Implement MacWebAppUtils to allow callers to locate and manipulate native webapps on Mac. r=josh
    UltraBlame original commit: 50cd7af728914f5b52299a3332d8e70dcbe0b191

---
+++ widget/cocoa/nsMacWebAppUtils.h
@@ -0,0 +1,21 @@
+
+
+
+#ifndef _MAC_WEB_APP_UTILS_H_
+#define _MAC_WEB_APP_UTILS_H_
+
+#include "nsIMacWebAppUtils.h"
+
+#define NS_MACWEBAPPUTILS_CONTRACTID "@mozilla.org/widget/mac-web-app-utils;1"
+#define NS_MACWEBAPPUTILS_COMPONENT_CLASSNAME "Mac Web Application Utils"
+
+class nsMacWebAppUtils : public nsIMacWebAppUtils {
+public:
+  nsMacWebAppUtils() {}
+  virtual ~nsMacWebAppUtils() {}
+  
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIMACWEBAPPUTILS
+};
+
+#endif
