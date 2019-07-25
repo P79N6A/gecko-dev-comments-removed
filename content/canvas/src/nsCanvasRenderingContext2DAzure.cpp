09e9736cbc48d139c55f108e827f8c202ad7304a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:04:51 +0000

    bug 745699 - canvas2d rendering context needs to call gfxFontGroup::UpdateFontList() before MakeTextRun() to ensure user font generation is up to date. r=roc a=blassey
    UltraBlame original commit: a4d9b327cc69b18a4e2ccd8302148fd7164cb2ff

--- content/canvas/src/nsCanvasRenderingContext2DAzure.cpp
+++ content/canvas/src/nsCanvasRenderingContext2DAzure.cpp
@@ -2970,6 +2970,7 @@ struct NS_STACK_CLASS nsCanvasBidiProcessorAzure : public nsBidiPresUtils::BidiP
 
   virtual void SetText(const PRUnichar* text, PRInt32 length, nsBidiDirection direction)
   {
+    mFontgrp->UpdateFontList(); 
     mTextRun = mFontgrp->MakeTextRun(text,
                                      length,
                                      mThebes,
