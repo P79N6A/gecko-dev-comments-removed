cc1cb7b5635ac3e8f29cd6c589674ba8f756eb0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:58:32 +0000

    Bug 621210 - Clear cached ctm when painting mask children; r=jwatt; a=roc
    UltraBlame original commit: 907d7744deb8b24a1d2e3b1129815c832c411b64

--- layout/svg/base/src/nsSVGMaskFrame.cpp
+++ layout/svg/base/src/nsSVGMaskFrame.cpp
@@ -121,6 +121,12 @@ nsSVGMaskFrame::ComputeMaskAlpha(nsSVGRenderState *aContext,
 
   for (nsIFrame* kid = mFrames.FirstChild(); kid;
        kid = kid->GetNextSibling()) {
+    
+    nsISVGChildFrame* SVGFrame = do_QueryFrame(kid);
+    if (SVGFrame) {
+      SVGFrame->NotifySVGChanged(nsISVGChildFrame::SUPPRESS_INVALIDATION |
+                                 nsISVGChildFrame::TRANSFORM_CHANGED);
+    }
     nsSVGUtils::PaintFrameWithEffects(&tmpState, nsnull, kid);
   }
 
