1227427dad39d6f9022db788e77b958ddf73ca6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:07:42 +0000

    Steal float back before reflowing it, in order to allow first continuation of a float to be pushed to the next page.  (Bug 563584, patch 15)  r=roc
    UltraBlame original commit: 35ca0a0faa1699f3ebbf4fd3e7acb0c792cdadfd

--- layout/generic/nsBlockReflowState.cpp
+++ layout/generic/nsBlockReflowState.cpp
@@ -547,8 +547,29 @@ nsBlockReflowState::AddFloat(nsLineLayout*       aLineLayout,
   NS_PRECONDITION(aFloat->GetStateBits() & NS_FRAME_OUT_OF_FLOW,
                   "aFloat must be an out-of-flow frame");
 
-  
-  aFloat->SetParent(mBlock);
+  NS_ABORT_IF_FALSE(aFloat->GetParent(), "float must have parent");
+  NS_ABORT_IF_FALSE(aFloat->GetParent()->IsFrameOfType(nsIFrame::eBlockFrame),
+                    "float's parent must be block");
+  NS_ABORT_IF_FALSE(aFloat->GetParent() == mBlock ||
+                    (aFloat->GetStateBits() & NS_FRAME_IS_FLOAT_CONTINUATION),
+                    "float should be in this block unless it was marked as "
+                    "float continuation");
+  if (aFloat->GetStateBits() & NS_FRAME_IS_FLOAT_CONTINUATION) {
+    
+    
+    
+    
+    
+    nsBlockFrame *floatParent =
+      static_cast<nsBlockFrame*>(aFloat->GetParent());
+    floatParent->StealFrame(mPresContext, aFloat);
+
+    aFloat->RemoveStateBits(NS_FRAME_IS_FLOAT_CONTINUATION);
+
+    
+    
+    mBlock->mFloats.AppendFrame(mBlock, aFloat);
+  }
 
   aReflowStatus = NS_FRAME_COMPLETE;
 
