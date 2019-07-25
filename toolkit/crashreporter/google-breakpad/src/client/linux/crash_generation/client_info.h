004aad2408507545c2e369709b10017687f2abc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:36:28 +0000

    bug 605832 - small Breakpad fixes to allow writing minidumps from the CrashGenerationServer callback. r=cjones a=blocking-fennec
    UltraBlame original commit: aa662b68d60449a73b2f8f55b92d57d0610ae292

--- toolkit/crashreporter/google-breakpad/src/client/linux/crash_generation/client_info.h
+++ toolkit/crashreporter/google-breakpad/src/client/linux/crash_generation/client_info.h
@@ -35,8 +35,12 @@ namespace google_breakpad {
 class CrashGenerationServer;
 
 struct ClientInfo {
+  pid_t pid() const { return pid_; }
+
   CrashGenerationServer* crash_server_;
   pid_t pid_;
+  char* crash_context;
+  size_t crash_context_size;
 };
 
 }
