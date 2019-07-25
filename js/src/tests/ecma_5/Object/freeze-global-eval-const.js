d9d6525ca0243e2dbed2adc093010b5befe4fd59
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:16:40 +0000

    Fix expression and value-generator decompilation to accept prolog ops, required due to ES5 metaprogramming APIs (607513, r=Waldo).
    UltraBlame original commit: 3b16a81d7832576b6aa87d56f19fb3288434166c

---
+++ js/src/tests/ecma_5/Object/freeze-global-eval-const.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+try {
+    evalcx("Object.freeze(this); eval('const q;')");
+} catch (e) {
+    assertEq(e.message, "({lazy:false}) is not extensible");
+}
+
+reportCompare(0, 0, "don't crash");
