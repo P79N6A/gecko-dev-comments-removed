e25b4d60bd217377a58234008e250c77ba22d8e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:29:41 +0000

    Bug 705175: update devicemanager, devicemanagerSUT versions of getAppRoot; r=jmaher
    UltraBlame original commit: 8ec01f6f316f94544ed57caf1255df2e5c520015

--- testing/xpcshell/remotexpcshelltests.py
+++ testing/xpcshell/remotexpcshelltests.py
@@ -37,6 +37,7 @@
 
 
 import re, sys, os
+import subprocess
 import runxpcshelltests as xpcshell
 from automationutils import *
 import devicemanager, devicemanagerADB, devicemanagerSUT
@@ -66,6 +67,22 @@ class XPCShellRemote(xpcshell.XPCShellTests, object):
         self.remoteAPK = self.remoteJoin(self.remoteBinDir, os.path.basename(options.localAPK))
         self.remoteDebugger = options.debugger
         self.remoteDebuggerArgs = options.debuggerArgs  
+        self.setAppRoot()
+
+    def setAppRoot(self):
+        
+        
+        self.appRoot = None
+        packageName = None
+        if self.options.localAPK:
+          try:
+            packageName = subprocess.check_output(["unzip", "-p", self.options.localAPK, "package-name.txt"])
+            if packageName:
+              self.appRoot = self.device.getAppRoot(packageName.strip())
+          except Exception as detail:
+            print "unable to determine app root: " + detail
+            pass
+        return None
 
     def remoteJoin(self, path1, path2):
         joined = os.path.join(path1, path2)
@@ -203,9 +220,9 @@ class XPCShellRemote(xpcshell.XPCShellTests, object):
         shellArgs = "cd "+self.remoteHere
         shellArgs += "; LD_LIBRARY_PATH="+self.remoteBinDir
         shellArgs += "; export CACHE_PATH="+self.remoteBinDir
-        if (self.device.getAppRoot()):
+        if (self.appRoot):
           
-          shellArgs += "; export GRE_HOME="+self.device.getAppRoot()
+          shellArgs += "; export GRE_HOME="+self.appRoot
         shellArgs += "; export XPCSHELL_TEST_PROFILE_DIR="+self.profileDir
         shellArgs += "; "+xpcshell+" "
         shellArgs += " ".join(cmd[1:])
