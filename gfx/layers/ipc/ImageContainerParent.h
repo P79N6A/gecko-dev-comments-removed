38e169d950367c9fe692c0dc8bafaa417a8cafd6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:40:20 +0000

    Bug 783841 - Fix build warnings in layers r=cjones
    UltraBlame original commit: c11e66f2143a5ff1f1dbc162d5752988d0e59674

--- gfx/layers/ipc/ImageContainerParent.h
+++ gfx/layers/ipc/ImageContainerParent.h
@@ -29,7 +29,7 @@ class ImageContainerParent : public PImageContainerParent
 {
 public:
 
-  ImageContainerParent(ImageBridgeParent* aBridge, PRUint32 aContainerID);
+  ImageContainerParent(PRUint32 aContainerID);
   ~ImageContainerParent();
 
   
@@ -126,7 +126,6 @@ protected:
 
 private:
   PRUint64 mID;
-  ImageBridgeParent* mBridge;
   bool  mStop;
 };
 
