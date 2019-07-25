3cf367cc1a7861caf20d97dd5de35f3c90f36e40
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:33:06 +0000

    Bug 667818.  When computing cookie expiration times, watch out for division by unsigned PR_USEC_PER_SEC screwing things up.  r=dwitte
    UltraBlame original commit: 2429a81db4262271ee9759918e71995595a9df42

---
+++ netwerk/test/unit/test_bug667818.js
@@ -0,0 +1,17 @@
+function makeURI(str) {
+    return Components.classes["@mozilla.org/network/io-service;1"]
+                     .getService(Components.interfaces.nsIIOService)
+                     .newURI(str, null, null);
+}
+
+function run_test() {
+    var serv =   Components.classes["@mozilla.org/cookieService;1"]
+                           .getService(Components.interfaces.nsICookieService);
+    var uri = makeURI("http://example.com/");
+    
+    serv.setCookieString(uri, null, "test=test; path=/; domain=example.com; expires=Sun, 31-Dec-1899 16:00:00 GMT;", null);
+    do_check_eq(serv.getCookieString(uri, null), null);
+    
+    serv.setCookieString(uri, null, "test2=test2; path=/; domain=example.com;", null);
+    do_check_eq(serv.getCookieString(uri, null), "test2=test2");
+}
