982a02d906f334ac3cf65c2dbc13a8396e2898a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:17 +0000

    Bug 581252 - Replace 'Manage' button in the Bookmarks List with a context menu item [r=mfinkle]
    UltraBlame original commit: b4baf8ea12fad015f403aeb23920fedc8d40e522

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -59,7 +59,7 @@ pref("zoom.dpiScale", 150);
 pref("ui.use_native_popup_windows", true);
 
 
-pref("ui.click_hold_context_menus", false);
+pref("ui.click_hold_context_menus", true);
 
 
 pref("browser.cache.disk.enable", false);
