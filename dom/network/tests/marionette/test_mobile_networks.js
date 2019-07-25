578a9d7f4f5d8980f9d6e1ed8ba2eae63a9cda02
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:25:22 +0000

    Bug 761482 - Rename MobileOperatorInfo to MobileNetworkInfo for consistency and update MobileConnectionInfo.network to use the new MobileNetworkInfo type. r=philikon sr=sicking
    UltraBlame original commit: d93f4177aa6b42e655dba915a0966084a94792c1

--- dom/network/tests/marionette/test_mobile_networks.js
+++ dom/network/tests/marionette/test_mobile_networks.js
@@ -9,44 +9,70 @@ const WHITELIST_PREF = "dom.mobileconnection.whitelist";
 let uriPrePath = window.location.protocol + "//" + window.location.host;
 SpecialPowers.setCharPref(WHITELIST_PREF, uriPrePath);
 
-ok(navigator.mozMobileConnection instanceof MozMobileConnection,
-   "mozMobileConnection is instanceof " + navigator.mozMobileConnection.constructor);
+let connection = navigator.mozMobileConnection;
+ok(connection instanceof MozMobileConnection,
+   "connection is instanceof " + connection.constructor);
 
-let request = navigator.mozMobileConnection.getNetworks();
-ok(request instanceof DOMRequest,
-   "request is instanceof " + request.constructor);
+function isAndroidNetwork(network) {
+  is(network.longName, "Android");
+  is(network.shortName, "Android");
+  is(network.mcc, 310);
+  is(network.mnc, 260);
+}
 
-request.onerror = function() {
-  ok(false, request.error);
-  cleanUp();
-};
+function testConnectionInfo() {
+  let voice = connection.voice;
+  is(voice.connected, true);
+  is(voice.emergencyCallsOnly, false);
+  is(voice.roaming, false);
+  isAndroidNetwork(voice.network);
 
-request.onsuccess = function() {
-  ok('result' in request, "Request did not contain a result");
-  let networks = request.result;
-
+  let data = connection.data;
   
   
   
-  is(networks.length, 2);
+  
+  isAndroidNetwork(data.network);
 
-  let network1 = networks[0];
-  is(network1.longName, "Android");
-  is(network1.shortName, "Android");
-  is(network1.mcc, 310);
-  is(network1.mnc, 260);
-  is(network1.state, "available");
+  testGetNetworks();
+}
 
-  let network2 = networks[1];
-  is(network2.longName, "TelKila");
-  is(network2.shortName, "TelKila");
-  is(network2.mcc, 310);
-  is(network2.mnc, 295);
-  is(network2.state, "available");
-  cleanUp();
-};
+function testGetNetworks() {
+  let request = connection.getNetworks();
+  ok(request instanceof DOMRequest,
+      "request is instanceof " + request.constructor);
+
+  request.onerror = function() {
+    ok(false, request.error);
+    cleanUp();
+  };
+
+  request.onsuccess = function() {
+    ok('result' in request, "Request did not contain a result");
+    let networks = request.result;
+
+    
+    
+    
+    is(networks.length, 2);
+
+    let network1 = networks[0];
+    isAndroidNetwork(network1);
+    is(network1.state, "available");
+
+    let network2 = networks[1];
+    is(network2.longName, "TelKila");
+    is(network2.shortName, "TelKila");
+    is(network2.mcc, 310);
+    is(network2.mnc, 295);
+    is(network2.state, "available");
+    cleanUp();
+  };
+}
 
 function cleanUp() {
   SpecialPowers.clearUserPref(WHITELIST_PREF);
   finish();
 }
+
+testConnectionInfo();
