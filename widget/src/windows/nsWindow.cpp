fb82530c0865a6c12658a04acf029575486f05d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:12:46 +0000

    Bug 635402 - Get rid of :not([inFullscreen]) workaround selectors for sizemode transitions. r=jmathies,dao
    UltraBlame original commit: 299c005302cd86c06fc6eae9b48693210696fa06

--- widget/src/windows/nsWindow.cpp
+++ widget/src/windows/nsWindow.cpp
@@ -1684,7 +1684,7 @@ NS_IMETHODIMP nsWindow::SetSizeMode(PRInt32 aMode) {
     ::ShowWindow(mWnd, mode);
     
     
-    if (mode == SW_RESTORE || mode == SW_MAXIMIZE)
+    if (mode == SW_RESTORE || mode == SW_MAXIMIZE || mode == SW_SHOW)
       DispatchFocusToTopLevelWindow(NS_ACTIVATE);
   }
   return rv;
