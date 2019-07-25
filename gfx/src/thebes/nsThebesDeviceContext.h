c37c370cc9f6527106521acc89cde7955f378106
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:12:00 +0000

    Bug 625311, nsThebesDeviceContext can own widget, r=roc, a=roc
    UltraBlame original commit: acb48b922e91b880e4a8a208218e86310e5b34a1

--- gfx/src/thebes/nsThebesDeviceContext.h
+++ gfx/src/thebes/nsThebesDeviceContext.h
@@ -159,7 +159,7 @@ protected:
     nsFontCache*      mFontCache;
     nsCOMPtr<nsIAtom> mLocaleLanguage; 
     nsHashtable*      mFontAliasTable;
-    nsIWidget*        mWidget;
+    nsCOMPtr<nsIWidget> mWidget;
 
 private:
     nsCOMPtr<nsIScreenManager> mScreenManager;
