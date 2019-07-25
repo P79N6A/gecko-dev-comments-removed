101572446c5374117cc9ca3adad2e7796266a1ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:49:26 +0000

    Bug 601201 - Re-enable the Error Console by default. r=beltzner,robcee
    UltraBlame original commit: 4633bdd40d42487566c022b9a0c598567555a211

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1043,7 +1043,7 @@ pref("services.sync.prefs.sync.xpinstall.whitelist.required", true);
 #endif
 
 
-pref("devtools.errorconsole.enabled", false);
+pref("devtools.errorconsole.enabled", true);
 
 
 pref("devtools.inspector.enabled", false);
