2fc830087b4defbdf5111eaa5d9f310c89d5f0b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:32:48 +0000

    Bug 674725 - Part T - WebSMS delivered event: DOM and IPC boilerplate. r=smaug
    UltraBlame original commit: 12357c16d0fa4e312826b6ed05aa410c3f266f53

--- dom/sms/src/Constants.h
+++ dom/sms/src/Constants.h
@@ -42,8 +42,9 @@ namespace mozilla {
 namespace dom {
 namespace sms {
 
-extern const char* kSmsReceivedObserverTopic; 
-extern const char* kSmsSentObserverTopic;     
+extern const char* kSmsReceivedObserverTopic;  
+extern const char* kSmsSentObserverTopic;      
+extern const char* kSmsDeliveredObserverTopic; 
 
 #define DELIVERY_RECEIVED NS_LITERAL_STRING("received")
 #define DELIVERY_SENT     NS_LITERAL_STRING("sent")
