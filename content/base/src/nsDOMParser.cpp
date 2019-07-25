b9a5c5deed2eeb858c3592299a68ac3f8642e658
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:40:49 +0000

    Bug 546857 Part 1: Fix nsDOMParser to make it useful from xpcshell tests. r=bz a=blocker
    UltraBlame original commit: 3c43a7339d02c02e964527f56c5c3e7616e83da9

--- content/base/src/nsDOMParser.cpp
+++ content/base/src/nsDOMParser.cpp
@@ -335,33 +335,40 @@ nsDOMParser::Init(nsIPrincipal* principal, nsIURI* documentURI,
   NS_ENSURE_ARG(principal || documentURI);
 
   mDocumentURI = documentURI;
+  
   if (!mDocumentURI) {
     principal->GetURI(getter_AddRefs(mDocumentURI));
-    if (!mDocumentURI) {
+    
+    
+    if (!mDocumentURI && !nsContentUtils::IsSystemPrincipal(principal)) {
       return NS_ERROR_INVALID_ARG;
     }
   }
 
   mScriptHandlingObject = do_GetWeakReference(aScriptObject);
   mPrincipal = principal;
-  nsIScriptSecurityManager* secMan = nsContentUtils::GetSecurityManager();
-  NS_ENSURE_TRUE(secMan, NS_ERROR_NOT_AVAILABLE);
   nsresult rv;
   if (!mPrincipal) {
+    nsIScriptSecurityManager* secMan = nsContentUtils::GetSecurityManager();
+    NS_ENSURE_TRUE(secMan, NS_ERROR_NOT_AVAILABLE);
     rv =
       secMan->GetCodebasePrincipal(mDocumentURI, getter_AddRefs(mPrincipal));
+    NS_ENSURE_SUCCESS(rv, rv);
     mOriginalPrincipal = mPrincipal;
   } else {
-    mOriginalPrincipal = principal;
-    PRBool isSystem;
-    rv = secMan->IsSystemPrincipal(mPrincipal, &isSystem);
-    if (NS_FAILED(rv) || isSystem) {
+    mOriginalPrincipal = mPrincipal;
+    if (nsContentUtils::IsSystemPrincipal(mPrincipal)) {
       
       
       mPrincipal = do_CreateInstance("@mozilla.org/nullprincipal;1", &rv);
+      NS_ENSURE_SUCCESS(rv, rv);
+
+      if (!mDocumentURI) {
+        rv = mPrincipal->GetURI(getter_AddRefs(mDocumentURI));
+        NS_ENSURE_SUCCESS(rv, rv);
+      }
     }
   }
-  NS_ENSURE_SUCCESS(rv, rv);
   
   mBaseURI = baseURI;
   
@@ -507,7 +514,8 @@ nsDOMParser::Initialize(nsISupports* aOwner, JSContext* cx, JSObject* obj,
 }
 
 NS_IMETHODIMP
-nsDOMParser::Init(nsIPrincipal *principal, nsIURI *documentURI, nsIURI *baseURI)
+nsDOMParser::Init(nsIPrincipal *aPrincipal, nsIURI *aDocumentURI,
+                  nsIURI *aBaseURI)
 {
   AttemptedInitMarker marker(&mAttemptedInit);
 
@@ -515,6 +523,19 @@ nsDOMParser::Init(nsIPrincipal *principal, nsIURI *documentURI, nsIURI *baseURI)
   NS_ENSURE_TRUE(cx, NS_ERROR_UNEXPECTED);
 
   nsIScriptContext* scriptContext = GetScriptContextFromJSContext(cx);
-  return Init(principal, documentURI, baseURI,
+
+  nsCOMPtr<nsIPrincipal> principal = aPrincipal;
+
+  if (!principal && !aDocumentURI) {
+    nsIScriptSecurityManager* secMan = nsContentUtils::GetSecurityManager();
+    NS_ENSURE_TRUE(secMan, NS_ERROR_UNEXPECTED);
+
+    secMan->GetSubjectPrincipal(getter_AddRefs(principal));
+
+    
+    NS_ENSURE_TRUE(principal, NS_ERROR_UNEXPECTED);
+  }
+
+  return Init(principal, aDocumentURI, aBaseURI,
               scriptContext ? scriptContext->GetGlobalObject() : nsnull);
 }
