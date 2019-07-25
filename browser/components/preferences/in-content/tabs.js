befed27d5af6dba072bb8bd39d319a0a58279e3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:09:20 +0000

    Bug 719717 - Move the tabs preferences to in-content UI. r=bmcbride
    UltraBlame original commit: c14a9f3448da84e456a71cd7920ac737d8c9527a

---
+++ browser/components/preferences/in-content/tabs.js
@@ -0,0 +1,66 @@
+
+
+
+
+var gTabsPane = {
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef XP_WIN
+  
+
+
+  init: function () {
+    try {
+      let sysInfo = Cc["@mozilla.org/system-info;1"].
+                    getService(Ci.nsIPropertyBag2);
+      let ver = parseFloat(sysInfo.getProperty("version"));
+      let showTabsInTaskbar = document.getElementById("showTabsInTaskbar");
+      showTabsInTaskbar.hidden = ver < 6.1 || (ver >= 6.1 && history.state != "tabs");
+    } catch (ex) {}
+  },
+#endif
+
+  
+
+
+
+
+  readLinkTarget: function() {
+    var openNewWindow = document.getElementById("browser.link.open_newwindow");
+    return openNewWindow.value != 2;
+  },
+
+  
+
+
+
+
+
+  writeLinkTarget: function() {
+    var linkTargeting = document.getElementById("linkTargeting");
+    return linkTargeting.checked ? 3 : 2;
+  }
+};
