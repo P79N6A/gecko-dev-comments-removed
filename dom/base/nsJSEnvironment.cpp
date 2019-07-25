14e8e7c4c56754f817a5082013a7601acece4ef4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:52:32 +0000

    Bug 627954, part 1: take out unnecessary compartment hack (r=gal)
    UltraBlame original commit: d518bc36d7b4cc29b072902efe0b2fa3d4b7b4de

--- dom/base/nsJSEnvironment.cpp
+++ dom/base/nsJSEnvironment.cpp
@@ -2305,19 +2305,12 @@ nsJSContext::GetGlobalObject()
     return nsnull;
   }
 
-  JSAutoEnterCompartment ac;
-
-  
-  
-  ac.enterAndIgnoreErrors(mContext, global);
-
-  nsCOMPtr<nsIScriptGlobalObject> sgo;
-  nsISupports *priv =
-    (nsISupports *)::JS_GetPrivate(mContext, global);
+  nsISupports *priv = (nsISupports *)global->getPrivate();
 
   nsCOMPtr<nsIXPConnectWrappedNative> wrapped_native =
     do_QueryInterface(priv);
 
+  nsCOMPtr<nsIScriptGlobalObject> sgo;
   if (wrapped_native) {
     
     
