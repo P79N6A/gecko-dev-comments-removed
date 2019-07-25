f8206c7e0c08911e99a1961f3c39a26993ff44f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:01:25 +0000

    Bug 582621 - Sharing back-end for MeeGo r=dougt a=blocking-fennec
    UltraBlame original commit: 5176c8f2691e1254943e65513152a3544d3821c3

---
+++ uriloader/exthandler/unix/nsExternalSharingAppService.h
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef NS_EXTERNAL_SHARING_APP_SERVICE_H
+#define NS_EXTERNAL_SHARING_APP_SERVICE_H
+
+#include "nsIExternalSharingAppService.h"
+#include "nsAutoPtr.h"
+
+class  ShareUiInterface;
+
+#define NS_EXTERNALSHARINGAPPSERVICE_CID \
+  {0xea782c90, 0xc6ec, 0x11df, \
+  {0xbd3, 0x08, 0x00, 0x20, 0x0c, 0x9a, 0x66, 0x74}}
+
+class nsExternalSharingAppService : public nsIExternalSharingAppService
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIEXTERNALSHARINGAPPSERVICE
+
+  nsExternalSharingAppService();
+
+private:
+  ~nsExternalSharingAppService();
+
+protected:
+  nsAutoPtr<ShareUiInterface> mShareUi;
+};
+#endif
