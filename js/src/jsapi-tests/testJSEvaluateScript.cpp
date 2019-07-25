f5d62668f16a5efebb4ea43f077744c2b41dd86a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:33:56 +0000

    Bug 775317.  Add a test for the JSOPTION_VAROBJFIX behavior of JS_EvaluateScript.  r=jorendorff
    UltraBlame original commit: f142f32a98a3849089b235818a1ed359d633084c

---
+++ js/src/jsapi-tests/testJSEvaluateScript.cpp
@@ -0,0 +1,50 @@
+
+
+
+
+#include "tests.h"
+
+BEGIN_TEST(testJSEvaluateScript)
+{
+    jsvalRoot proto(cx);
+    JSObject *obj = JS_NewObject(cx, NULL, NULL, global);
+    CHECK(obj);
+
+    uint32_t options = JS_GetOptions(cx);
+    CHECK(options & JSOPTION_VAROBJFIX);
+
+    static const char src[] = "var x = 5;";
+
+    JS::Value retval;
+    CHECK(JS_EvaluateScript(cx, obj, src, sizeof(src) - 1, __FILE__, __LINE__,
+                            &retval));
+
+    JSBool hasProp = JS_TRUE;
+    CHECK(JS_AlreadyHasOwnProperty(cx, obj, "x", &hasProp));
+    CHECK(!hasProp);
+
+    hasProp = JS_FALSE;
+    CHECK(JS_HasProperty(cx, global, "x", &hasProp));
+    CHECK(hasProp);
+
+    
+    JS_SetOptions(cx, options & ~JSOPTION_VAROBJFIX);
+
+    static const char src2[] = "var y = 5;";
+
+    CHECK(JS_EvaluateScript(cx, obj, src2, sizeof(src2) - 1, __FILE__, __LINE__,
+                            &retval));
+
+    hasProp = JS_FALSE;
+    CHECK(JS_AlreadyHasOwnProperty(cx, obj, "y", &hasProp));
+    CHECK(hasProp);
+
+    hasProp = JS_TRUE;
+    CHECK(JS_AlreadyHasOwnProperty(cx, global, "y", &hasProp));
+    CHECK(!hasProp);
+
+    return true;
+}
+END_TEST(testJSEvaluateScript)
+
+
