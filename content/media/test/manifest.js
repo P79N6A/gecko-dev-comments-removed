8be74b2ea45fe8965f3a380cf2af5f0c4e43c355
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:14:55 +0000

    Backed out changeset f54e6386c113
    UltraBlame original commit: 9ab39b858f1fd0092c737d9d6ab7289556e770de

--- content/media/test/manifest.js
+++ content/media/test/manifest.js
@@ -102,9 +102,6 @@ var gPlayTests = [
   
   { name:"seek.webm", type:"video/webm", duration:3.966 },
 
-  
-  { name:"seek.yuv", type:"video/x-raw-yuv", duration:1.833 },
-
   { name:"bogus.duh", type:"bogus/duh", duration:Number.NaN }
   
 ];
