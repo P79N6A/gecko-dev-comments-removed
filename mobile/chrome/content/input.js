763410e2bb9da0488d60f2add22559bf1255b961
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:43 +0000

    Bug 599145 - Fix double tap radius calculation [r=stechz]
    UltraBlame original commit: 8711040ec26f61470fb48c8539ceb65794b3012f

--- mobile/chrome/content/input.js
+++ mobile/chrome/content/input.js
@@ -46,11 +46,10 @@
 const kDoubleClickInterval = 400;
 
 
+const kDoubleClickRadius = 100;
+
+
 const kLongTapWait = 500;
-
-
-
-const kDoubleClickThreshold = 200;
 
 
 const kTapRadius = Services.prefs.getIntPref("ui.dragThresholdX");
@@ -389,22 +388,9 @@ MouseModule.prototype = {
 
   
   _doSingleClick: function _doSingleClick() {
-    let ev = this._downUpEvents[1];
+    let mouseUp = this._downUpEvents[1];
     this._cleanClickBuffer();
-
-    
-    let modifiers =
-      (ev.altKey   ? Ci.nsIDOMNSEvent.ALT_MASK     : 0) |
-      (ev.ctrlKey  ? Ci.nsIDOMNSEvent.CONTROL_MASK : 0) |
-      (ev.shiftKey ? Ci.nsIDOMNSEvent.SHIFT_MASK   : 0) |
-      (ev.metaKey  ? Ci.nsIDOMNSEvent.META_MASK    : 0);
-
-    let event = document.createEvent("Events");
-    event.initEvent("TapSingle", true, false);
-    event.clientX = ev.clientX;
-    event.clientY = ev.clientY;
-    event.modifiers = modifiers;
-    ev.target.dispatchEvent(event);
+    this._dispatchTap("TapSingle", mouseUp);
   },
 
   
@@ -414,13 +400,31 @@ MouseModule.prototype = {
     let mouseUp2 = this._downUpEvents[Math.min(3, this._downUpEvents.length - 1)];
     this._cleanClickBuffer();
 
+    let dx = mouseUp1.clientX - mouseUp2.clientX;
+    let dy = mouseUp1.clientY - mouseUp2.clientY;
+
+    if (dx*dx + dy*dy < kDoubleClickRadius*kDoubleClickRadius) {
+      this._dispatchTap("TapDouble", mouseUp1);
+    } else {
+      this._dispatchTap("TapSingle", mouseUp1);
+      this._dispatchTap("TapSingle", mouseUp2);
+    }
+  },
+
+  _dispatchTap: function _dispatchTap(aType, aMouseUpEvent) {
+    
+    let modifiers =
+      (aMouseUpEvent.altKey   ? Ci.nsIDOMNSEvent.ALT_MASK     : 0) |
+      (aMouseUpEvent.ctrlKey  ? Ci.nsIDOMNSEvent.CONTROL_MASK : 0) |
+      (aMouseUpEvent.shiftKey ? Ci.nsIDOMNSEvent.SHIFT_MASK   : 0) |
+      (aMouseUpEvent.metaKey  ? Ci.nsIDOMNSEvent.META_MASK    : 0);
+
     let event = document.createEvent("Events");
-    event.initEvent("TapDouble", true, false);
-    event.clientX1 = mouseUp1.clientX;
-    event.clientY1 = mouseUp1.clientY;
-    event.clientX2 = mouseUp1.clientX;
-    event.clientY2 = mouseUp1.clientY;
-    mouseUp1.target.dispatchEvent(event);
+    event.initEvent(aType, true, false);
+    event.clientX = aMouseUpEvent.clientX;
+    event.clientY = aMouseUpEvent.clientY;
+    event.modifiers = modifiers;
+    aMouseUpEvent.target.dispatchEvent(event);
   },
 
   
