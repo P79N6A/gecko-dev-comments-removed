22fec6d6b115ccaaeb016b15e381958b15191370
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:27:00 +0000

    Implement the multi-process Jetpack API (bug 556846). r=bsmedberg,bent
    UltraBlame original commit: 810a18af65ae217133a203b481706258a52c9faf

--- js/jetpack/JetpackParent.h
+++ js/jetpack/JetpackParent.h
@@ -35,29 +35,49 @@
 
 
 
+
 #ifndef mozilla_jetpack_JetpackParent_h
 #define mozilla_jetpack_JetpackParent_h
 
 #include "mozilla/jetpack/PJetpackParent.h"
 #include "mozilla/jetpack/JetpackProcessParent.h"
+#include "mozilla/jetpack/JetpackActorCommon.h"
 #include "nsIJetpack.h"
+
+#include "nsTArray.h"
+
+struct JSContext;
 
 namespace mozilla {
 namespace jetpack {
 
+class PHandleParent;
+
 class JetpackParent
   : public PJetpackParent
   , public nsIJetpack
+  , private JetpackActorCommon
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIJETPACK
 
-  JetpackParent();
+  JetpackParent(JSContext* cx);
   ~JetpackParent();
+
+protected:
+  NS_OVERRIDE virtual bool RecvSendMessage(const nsString& messageName,
+                                           const nsTArray<Variant>& data);
+  NS_OVERRIDE virtual bool RecvCallMessage(const nsString& messageName,
+                                           const nsTArray<Variant>& data,
+                                           nsTArray<Variant>* results);
+
+  NS_OVERRIDE virtual PHandleParent* AllocPHandle();
+  NS_OVERRIDE virtual bool DeallocPHandle(PHandleParent* actor);
 
 private:
   JetpackProcessParent* mSubprocess;
+  JSContext* mContext;
 
   DISALLOW_EVIL_CONSTRUCTORS(JetpackParent);
 };
