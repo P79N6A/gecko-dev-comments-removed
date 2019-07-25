f3d0add5fa517226e9b7eda2a577b09c03dd2ee7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:36:56 +0000

    Bug 673742: Allow postMessage()ing File and FileLists between same-origin windows. r=sicking
    UltraBlame original commit: 31eddfef6549ba56a9f75248f50d671f934773e4

---
+++ dom/base/StructuredCloneTags.h
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef StructuredCloneTags_h__
+#define StructuredCloneTags_h__
+
+#include "jsapi.h"
+
+namespace mozilla {
+namespace dom {
+
+enum StructuredCloneTags {
+  SCTAG_BASE = JS_SCTAG_USER_MIN,
+  SCTAG_DOM_BLOB,
+  SCTAG_DOM_FILELIST,
+  SCTAG_DOM_MAX
+};
+
+} 
+} 
+
+#endif 
