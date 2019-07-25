8f56d171801e4d67b0a0dea5e1b5a7349ed8c36f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:53:11 +0000

    Merge from mozilla-central.
    UltraBlame original commit: f1407f88d2ba716adfbf3c0da434ac1bb9fe73f0

--- js/src/ion/IonBuilder.cpp
+++ js/src/ion/IonBuilder.cpp
@@ -1832,9 +1832,9 @@ IonBuilder::tableSwitch(JSOp op, jssrcnote *sn)
     
     jsbytecode *pc2 = pc;
     pc2 += JUMP_OFFSET_LEN;
-    jsint low = GET_JUMP_OFFSET(pc2);
+    int low = GET_JUMP_OFFSET(pc2);
     pc2 += JUMP_OFFSET_LEN;
-    jsint high = GET_JUMP_OFFSET(pc2);
+    int high = GET_JUMP_OFFSET(pc2);
     pc2 += JUMP_OFFSET_LEN;
 
     
@@ -1848,7 +1848,7 @@ IonBuilder::tableSwitch(JSOp op, jssrcnote *sn)
 
     
     jsbytecode *casepc = NULL;
-    for (jsint i = 0; i < high-low+1; i++) {
+    for (int i = 0; i < high-low+1; i++) {
         casepc = pc + GET_JUMP_OFFSET(pc2);
 
         JS_ASSERT(casepc >= pc && casepc <= exitpc);
