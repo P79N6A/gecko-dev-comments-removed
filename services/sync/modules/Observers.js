7296f10a8319e08ad7f3fc97428ec660a22d07ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:20:35 +0000

    bug 436636: a system for showing various kinds of notifications in one consolidated location
    UltraBlame original commit: f6c3e10b97b0a72f27085258634e749e3da91e04

---
+++ services/sync/modules/Observers.js
@@ -0,0 +1,100 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let EXPORTED_SYMBOLS = ["Observers"];
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+let Observers = {
+  add: function(callback, topic) {
+    let observer = new Observer(callback);
+    if (!(topic in Observers._observers))
+      Observers._observers[topic] = {};
+    Observers._observers[topic][callback] = observer;
+    Observers._service.addObserver(observer, topic, true);
+    return observer;
+  },
+
+  remove: function(callback, topic) {
+    let observer = Observers._observers[topic][callback];
+    Observers._service.removeObserver(observer, topic);
+    delete this._observers[topic][callback];
+  },
+
+  notify: function(subject, topic, data) {
+    Observers._service.notifyObservers(new Subject(subject), topic, data);
+  },
+
+  _service: Cc["@mozilla.org/observer-service;1"].
+            getService(Ci.nsIObserverService),
+
+  _observers: {}
+};
+
+
+function Observer(callback) {
+  this._callback = callback;
+}
+
+Observer.prototype = {
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver, Ci.nsISupportsWeakReference]),
+  observe: function(subject, topic, data) {
+    
+    
+    
+    if (subject.wrappedJSObject)
+      this._callback(subject.wrappedJSObject, topic, data);
+    else
+      this._callback(subject, topic, data);
+  }
+}
+
+
+function Subject(object) {
+  this.wrappedJSObject = object;
+}
+
+Subject.prototype = {
+  QueryInterface: XPCOMUtils.generateQI([]),
+  getHelperForLanguage: function() {},
+  getInterfaces: function() {}
+};
---
+++ services/sync/modules/notifications.js
@@ -0,0 +1,143 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const EXPORTED_SYMBOLS = ["Notifications", "Notification", "NotificationButton",
+                          "TabsNotification"];
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+Cu.import("resource://weave/Observers.js");
+Cu.import("resource://weave/log4moz.js");
+Cu.import("resource://weave/util.js");
+
+let Notifications = {
+  
+  get PRIORITY_INFO()     1, 
+  get PRIORITY_WARNING()  4, 
+  get PRIORITY_ERROR()    7, 
+
+  
+  
+  
+  notifications: [],
+
+  _observers: [],
+
+  
+  
+
+  add: function Notifications_add(notification) {
+    this.notifications.push(notification);
+    Observers.notify(notification, "weave:notification:added", null);
+  },
+
+  remove: function Notifications_remove(notification) {
+    let index = this.notifications.indexOf(notification);
+
+    if (index != -1) {
+      this.notifications.splice(index, 1);
+      Observers.notify(notification, "weave:notification:removed", null);
+    }
+  },
+
+  
+
+
+  replace: function Notifications_replace(oldNotification, newNotification) {
+    let index = this.notifications.indexOf(oldNotification);
+
+    if (index != -1)
+      this.notifications.splice(index, 1, newNotification);
+    else {
+      this.notifications.push(newNotification);
+      
+      
+    }
+
+    
+  }
+
+};
+
+
+
+
+
+function Notification(title, description, iconURL, priority, buttons) {
+  this.title = title;
+  this.description = description;
+
+  if (iconURL)
+    this.iconURL = iconURL;
+
+  if (priority)
+    this.priority = priority;
+
+  if (buttons)
+    this.buttons = buttons;
+}
+
+
+
+
+
+Notification.prototype.priority = Notifications.PRIORITY_INFO;
+Notification.prototype.iconURL = null;
+Notification.prototype.buttons = [];
+
+
+
+
+function NotificationButton(label, accessKey, callback) {
+  this.label = label;
+  this.accessKey = accessKey;
+  this.callback = callback;
+}
+
+function TabsNotification() {
+  
+  
+  Notification.call(this, "", "", null, Notifications.PRIORITY_INFO, null);
+}
+
+
+
+
+
+TabsNotification.prototype.__proto__ = Notification.prototype;
