f7c73398da5cbb49fb824bbf0cb7ccca225c3df1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 12:05:56 +0000

    bug 636877 - Improve android restarts r=dougt a=2.0+
    UltraBlame original commit: ad545273888ca11aaa45362c61f02d58cdef0af2

--- embedding/android/GeckoApp.java
+++ embedding/android/GeckoApp.java
@@ -542,6 +542,10 @@ abstract public class GeckoApp
             Log.i("GeckoAppJava", "error doing restart", e);
         }
         finish();
+        
+        try {
+            Thread.currentThread().sleep(1000);
+        } catch (InterruptedException ie) {}
     }
 
     public void handleNotification(String action, String alertName, String alertCookie) {
