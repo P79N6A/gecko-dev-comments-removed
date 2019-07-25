75bd3c5ff310ea41b5ccffc02dbca28113ec9db4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:29:20 +0000

    Bug 769658 - Split OS.Constants.Sys and OS.Constants.Path. r=khuey
    UltraBlame original commit: df770ecef00ae682c09c440a74e128bea335742d

--- dom/system/OSFileConstants.cpp
+++ dom/system/OSFileConstants.cpp
@@ -440,6 +440,14 @@ bool DefineOSFileConstants(JSContext *cx, JSObject *global)
   }
 
   
+
+  JSObject *objPath;
+  if (!(objPath = GetOrCreateObjectProperty(cx, objConstants, "Path"))) {
+    return false;
+  }
+
+
+  
   {
     nsAutoString xulPath(*gLibDirectory);
 
@@ -458,7 +466,7 @@ bool DefineOSFileConstants(JSContext *cx, JSObject *global)
 
     JSString* strPathToLibXUL = JS_NewUCStringCopyZ(cx, xulPath.get());
     jsval valXul = STRING_TO_JSVAL(strPathToLibXUL);
-    if (!JS_SetProperty(cx, objSys, "libxulpath", &valXul)) {
+    if (!JS_SetProperty(cx, objPath, "libxul", &valXul)) {
       return false;
     }
   }
