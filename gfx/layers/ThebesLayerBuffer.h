9481667ad261d8b9cb7764f8fbe081a9c5e5140f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:45:55 +0000

    Bug 674494 - Uninitialised variable in ThebesLayerBuffer::BeginPaint. r=mattwoodrow
    UltraBlame original commit: f345becaf55b05b3f0a05fd9aea4d7d4457377a0

--- gfx/layers/ThebesLayerBuffer.h
+++ gfx/layers/ThebesLayerBuffer.h
@@ -112,6 +112,10 @@ public:
 
 
   struct PaintState {
+    PaintState()
+      : mDidSelfCopy(PR_FALSE)
+    {}
+
     nsRefPtr<gfxContext> mContext;
     nsIntRegion mRegionToDraw;
     nsIntRegion mRegionToInvalidate;
