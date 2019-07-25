47ffe8c5ce72bdff3c1156d552877ce54e393750
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:58:19 +0000

    Bug 737013 - Expose sanitizer fragments on parseFragment(), migrate callers to the new interface. r=smaug.
    UltraBlame original commit: eb7570cdfb9e80acb308ed6214cac9c9cb784804

--- testing/extensions/community/chrome/content/common.js
+++ testing/extensions/community/chrome/content/common.js
@@ -149,12 +149,11 @@ var qaTools = {
         return newArray;
     },
     writeSafeHTML : function(elementID, htmlstr) {
-        document.getElementById(elementID).innerHTML = "";  
-        var gUnescapeHTML = Components.classes["@mozilla.org/feed-unescapehtml;1"].getService(Components.interfaces.nsIScriptableUnescapeHTML);
+        document.getElementById(elementID).textContent = "";  
+        var utils = Components.classes["@mozilla.org/parserutils;1"].getService(Components.interfaces.nsIParserUtils);
         var context = document.getElementById(elementID);
-        var fragment = gUnescapeHTML.parseFragment(htmlstr, false, null, context);
+        var fragment = utils.parseFragment(htmlstr, 0, false, null, context);
         context.appendChild(fragment);
-
     },
 
     assignLinkHandlers : function(node) {
