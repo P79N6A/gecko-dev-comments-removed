532c0ec4c48507a728bc01eccf7c7fff329c92c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:09:20 +0000

    Bug 359809: properly escape keyword searches when alternative character sets are specified, r=gavin
    UltraBlame original commit: f4c738892af6d400c361372909c1189c285e0929

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2285,9 +2285,15 @@ function getShortcutOrURI(aURL, aPostDataRef) {
       } catch (e) {}
     }
 
+    
+    
+    
+    
+    
     var encodedParam = "";
-    if (charset)
-      encodedParam = escape(convertFromUnicode(charset, param));
+    if (charset && charset != "UTF-8")
+      encodedParam = escape(convertFromUnicode(charset, param)).
+                     replace(/[+@\/]+/g, encodeURIComponent);
     else 
       encodedParam = encodeURIComponent(param);
 
