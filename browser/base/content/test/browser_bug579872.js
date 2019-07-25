3805855fed758cc4295a6385cf0554e992f879ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:54:06 +0000

    Bug 579872 - Bookmarks should always opens a new tab when the current tab is an app tab. r=dao a=blocking
    UltraBlame original commit: 0152fbafc5208ce8d7c56cb11180d9f9bc8725f1

---
+++ browser/base/content/test/browser_bug579872.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  let newTab = gBrowser.addTab("http://www.example.com");
+  waitForExplicitFinish();
+  newTab.linkedBrowser.addEventListener("load", mainPart, true);
+  
+  function mainPart() {
+    gBrowser.pinTab(newTab);
+    gBrowser.selectedTab = newTab;
+    
+    openUILinkIn("javascript:var x=0;", "current");
+    is(gBrowser.tabs.length, 2, "Should open in current tab");
+    
+    openUILinkIn("http://www.example.com/1", "current");
+    is(gBrowser.tabs.length, 2, "Should open in current tab");
+    
+    openUILinkIn("http://www.example.org/", "current");
+    is(gBrowser.tabs.length, 3, "Should open in new tab");
+    
+    newTab.removeEventListener("load", mainPart, true);
+    gBrowser.removeTab(newTab);
+    gBrowser.removeTab(gBrowser.tabs[1]); 
+    finish();
+  }
+}
