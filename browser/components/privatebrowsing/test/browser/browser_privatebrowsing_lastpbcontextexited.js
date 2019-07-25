647c8338c6a3c306b483feeb446cbfb4f831608f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:01:37 +0000

    Test case for bug 725210; r=jdm
    UltraBlame original commit: ccb232b6b97ad2efb99951154ad1ecc2e960dcd0

---
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_lastpbcontextexited.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  
+  
+  let newWin = window.openDialog("chrome://browser/content/", "_blank", "chrome,all,dialog=no");
+  waitForExplicitFinish();
+  SimpleTest.waitForFocus(function() {
+    let notificationCount = 0;
+    let observer = {
+      observe: function(aSubject, aTopic, aData) {
+        is(aTopic, "last-pb-context-exited", "Correct topic should be dispatched");
+        ++notificationCount;
+      }
+    };
+    Services.obs.addObserver(observer, "last-pb-context-exited", false);
+    newWin.gPrivateBrowsingUI.privateWindow = true;
+    SimpleTest.is(notificationCount, 0, "last-pb-context-exited should not be fired yet");
+    newWin.gPrivateBrowsingUI.privateWindow = false;
+    newWin.close();
+    newWin = null;
+    window.QueryInterface(Ci.nsIInterfaceRequestor)
+          .getInterface(Ci.nsIDOMWindowUtils)
+          .garbageCollect(); 
+    SimpleTest.is(notificationCount, 1, "last-pb-context-exited should be fired once");
+    Services.obs.removeObserver(observer, "last-pb-context-exited", false);
+
+    
+    gPrefService.clearUserPref("browser.privatebrowsing.keep_current_session");
+    finish();
+  }, newWin);
+}
