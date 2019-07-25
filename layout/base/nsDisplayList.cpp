bd2f9860a72d4465ff8da42d7702e3f0dae4c0c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:41:52 +0000

    Bug 785333 - Store container layers against merged frames. r=roc
    As well as storing the container layer against the underlying frame of the
    container item, store it against its merged frames as well. In addition, check
    for old container layers against merged frames when building a container layer.
    
    This protects against losing the layer when the underlying frame of a container
    item changes to either a new frame or an existing, merged frame.
    
    UltraBlame original commit: 79345542f8535d29891b68397b7e7fc85b465fa4

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -2701,14 +2701,6 @@ nsDisplayScrollLayer::TryMerge(nsDisplayListBuilder* aBuilder,
   props.Set(nsIFrame::ScrollLayerCount(),
     reinterpret_cast<void*>(GetScrollLayerCount() - 1));
 
-  
-  
-  
-  
-  
-  nsIFrame* tmp = mFrame;
-  mFrame = other->mFrame;
-  other->mFrame = tmp;
   MergeFromTrackingMergedFrames(other);
   return true;
 }
