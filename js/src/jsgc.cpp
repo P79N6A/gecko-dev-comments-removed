0946e6a8df7c854a81a2f21ef32b36dc8edc0ffd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:21:42 +0000

    Bug 660630: Only error once in OOM in NewCompartment (r=igor)
    UltraBlame original commit: d6987081e730802b4331e7e82f0fa4aafb9c1443

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -2833,12 +2833,19 @@ NewCompartment(JSContext *cx, JSPrincipals *principals)
 
         compartment->setGCLastBytes(8192);
 
-        AutoLockGC lock(rt);
-        if (rt->compartments.append(compartment))
-            return compartment;
+        
+
+
+
+        {
+            AutoLockGC lock(rt);
+            if (rt->compartments.append(compartment))
+                return compartment;
+        }
+
+        js_ReportOutOfMemory(cx);
     }
     Foreground::delete_(compartment);
-    JS_ReportOutOfMemory(cx);
     return NULL;
 }
 
