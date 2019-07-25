5e8825065afa5baabfe822bc55972633a67729da
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:31:59 +0000

    Bug 616015 - Add a keyboard shortcut to reveal and hide the add-on bar (r=dao, a=blocker)
    UltraBlame original commit: 296a46e08491f7dacb11b9fc6e25774b59c90373

---
+++ browser/base/content/test/browser_addon_bar_shortcut.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  let addonbar = document.getElementById("addon-bar");
+  ok(addonbar.collapsed, "addon bar is collapsed by default");
+
+  
+  EventUtils.synthesizeKey("/", { accelKey: true }, window);
+  ok(!addonbar.collapsed, "addon bar is not collapsed after toggle");
+
+  
+  EventUtils.synthesizeKey("/", { accelKey: true }, window);
+
+  
+  ok(addonbar.collapsed, "addon bar is collapsed after toggle");
+}
