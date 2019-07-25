49558c962210222964192a85659176debd3bf48f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:08:29 +0000

    Bug 750850 - Delayed loading. r=ehsan
    UltraBlame original commit: 06f397d1f99724a8f83874b4091aa122dda94130

---
+++ toolkit/mozapps/update/updater/loaddlls.cpp
@@ -0,0 +1,43 @@
+
+
+
+
+
+#include <windows.h>
+
+
+
+
+struct AutoLoadSystemDependencies
+{
+  AutoLoadSystemDependencies()
+  {
+    static LPCWSTR delayDLLs[] = { L"wsock32.dll", L"crypt32.dll" };
+    WCHAR systemDirectory[MAX_PATH + 1] = { L'\0' };
+    
+    
+    GetSystemDirectory(systemDirectory, MAX_PATH + 1);
+    size_t systemDirLen = wcslen(systemDirectory);
+
+    
+    if (systemDirectory[systemDirLen - 1] != L'\\' && systemDirLen) {
+      systemDirectory[systemDirLen] = L'\\';
+      ++systemDirLen;
+      
+    }
+
+    
+    for (size_t i = 0; i < sizeof(delayDLLs) / sizeof(delayDLLs[0]); ++i) {
+      size_t fileLen = wcslen(delayDLLs[i]);
+      wcsncpy(systemDirectory + systemDirLen, delayDLLs[i], 
+              MAX_PATH - systemDirLen);
+      if (systemDirLen + fileLen <= MAX_PATH) {
+        systemDirectory[systemDirLen + fileLen] = L'\0';
+      } else {
+        systemDirectory[MAX_PATH] = L'\0';
+      }
+      LPCWSTR fullModulePath = systemDirectory; 
+      LoadLibraryW(fullModulePath);
+    }
+  }
+} loadDLLs;
