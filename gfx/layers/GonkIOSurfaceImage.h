bb81f2815c4fce4fb39b3fed2e33745a73c1f4df
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:40:07 +0000

    Bug 757341 - Move GonkIOSurfaceImage to its own header. r=roc
    UltraBlame original commit: e1ebc55a599e4fda72aaab6c50de13c26a054d80

---
+++ gfx/layers/GonkIOSurfaceImage.h
@@ -0,0 +1,96 @@
+
+
+
+
+
+#ifndef GONKIOSURFACEIMAGE_H
+#define GONKIOSURFACEIMAGE_H
+
+#ifdef MOZ_WIDGET_GONK
+
+#include "ImageLayers.h"
+
+#include <ui/GraphicBuffer.h>
+
+namespace mozilla {
+namespace layers {
+
+
+
+
+
+
+
+
+
+
+class GraphicBufferLocked {
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(GraphicBufferLocked)
+
+public:
+  GraphicBufferLocked(android::GraphicBuffer* aGraphicBuffer)
+    : mGraphicBuffer(aGraphicBuffer)
+  {}
+
+  virtual ~GraphicBufferLocked() {}
+
+  virtual void Unlock() {}
+
+  virtual void* GetNativeBuffer()
+  {
+    return mGraphicBuffer->getNativeBuffer();
+  }
+
+protected:
+  android::GraphicBuffer* mGraphicBuffer;
+};
+
+class THEBES_API GonkIOSurfaceImage : public Image {
+public:
+  struct Data {
+    nsRefPtr<GraphicBufferLocked> mGraphicBuffer;
+    gfxIntSize mPicSize;
+  };
+
+  GonkIOSurfaceImage()
+    : Image(NULL, GONK_IO_SURFACE)
+    , mSize(0, 0)
+    {}
+
+  virtual ~GonkIOSurfaceImage()
+  {
+    mGraphicBuffer->Unlock();
+  }
+
+  virtual void SetData(const Data& aData)
+  {
+    mGraphicBuffer = aData.mGraphicBuffer;
+    mSize = aData.mPicSize;
+  }
+
+  virtual gfxIntSize GetSize()
+  {
+    return mSize;
+  }
+
+  virtual already_AddRefed<gfxASurface> GetAsSurface()
+  {
+    
+    return nullptr;
+  }
+
+  void* GetNativeBuffer()
+  {
+    return mGraphicBuffer->GetNativeBuffer();
+  }
+
+private:
+  nsRefPtr<GraphicBufferLocked> mGraphicBuffer;
+  gfxIntSize mSize;
+};
+
+} 
+} 
+#endif
+
+#endif 
--- gfx/layers/ImageLayers.h
+++ gfx/layers/ImageLayers.h
@@ -27,9 +27,6 @@ struct ID3D10ShaderResourceView;
 
 typedef void* HANDLE;
 #endif
-#ifdef MOZ_WIDGET_GONK
-# include <ui/GraphicBuffer.h>
-#endif
 
 namespace mozilla {
 
@@ -953,82 +950,6 @@ private:
   void* mPluginInstanceOwner;
   UpdateSurfaceCallback mUpdateCallback;
   DestroyCallback mDestroyCallback;
-};
-#endif
-
-#ifdef MOZ_WIDGET_GONK
-
-
-
-
-
-
-
-
-
-class GraphicBufferLocked {
-  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(GraphicBufferLocked)
-
-public:
-  GraphicBufferLocked(android::GraphicBuffer* aGraphicBuffer)
-    : mGraphicBuffer(aGraphicBuffer)
-  {}
-
-  virtual ~GraphicBufferLocked() {}
-
-  virtual void Unlock() {}
-
-  virtual void* GetNativeBuffer()
-  {
-    return mGraphicBuffer->getNativeBuffer();
-  }   
-
-protected:
-  android::GraphicBuffer* mGraphicBuffer;
-};
-
-class THEBES_API GonkIOSurfaceImage : public Image {
-public:
-  struct Data {
-    nsRefPtr<GraphicBufferLocked> mGraphicBuffer;
-    gfxIntSize mPicSize;
-  };
-
-  GonkIOSurfaceImage()
-    : Image(NULL, GONK_IO_SURFACE)
-    , mSize(0, 0)
-    {}
-
-  virtual ~GonkIOSurfaceImage()
-  {
-    mGraphicBuffer->Unlock();
-  }
-
-  virtual void SetData(const Data& aData)
-  {
-    mGraphicBuffer = aData.mGraphicBuffer;
-    mSize = aData.mPicSize;
-  }
-
-  virtual gfxIntSize GetSize()
-  {
-    return mSize;
-  }
-
-  virtual already_AddRefed<gfxASurface> GetAsSurface()
-  {
-    
-    return nullptr;
-  }
-
-  void* GetNativeBuffer()
-  {
-    return mGraphicBuffer->GetNativeBuffer();
-  }
-
-private:
-  nsRefPtr<GraphicBufferLocked> mGraphicBuffer;
-  gfxIntSize mSize;
 };
 #endif
 
