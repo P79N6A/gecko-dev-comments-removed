c026f66c4bc8c0d630452b6d301af97d26c38424
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:32:51 +0000

    Bug 539356 - Part 5 - Change SVG effects painting to use a LayerManager transaction. r=roc
    UltraBlame original commit: 8a93aee3c7be8f2679aefde47e6f9e08a2d8f2cf

--- layout/svg/base/src/nsSVGIntegrationUtils.h
+++ layout/svg/base/src/nsSVGIntegrationUtils.h
@@ -10,6 +10,7 @@
 #include "gfxPattern.h"
 #include "gfxRect.h"
 #include "nsRect.h"
+#include "Layers.h"
 
 class nsDisplayList;
 class nsDisplayListBuilder;
@@ -134,7 +135,7 @@ public:
   PaintFramesWithEffects(nsRenderingContext* aCtx,
                          nsIFrame* aFrame, const nsRect& aDirtyRect,
                          nsDisplayListBuilder* aBuilder,
-                         nsDisplayList* aInnerList);
+                         mozilla::layers::LayerManager* aManager);
 
   
 
