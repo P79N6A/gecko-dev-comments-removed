30ea83481ea85935b701cecefb4c48ff8ed3a5d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:18:51 +0000

    Bug 606265 - add ability to set timezone to android agent r=ctalbert a=NPOTB
    UltraBlame original commit: 1cba6be4c95f1897604a00d743c3275211e31c7b

--- build/mobile/sutagent/android/SUTAgentAndroid.java
+++ build/mobile/sutagent/android/SUTAgentAndroid.java
@@ -1,39 +1,39 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Android SUTAgent code.
- *
- * The Initial Developer of the Original Code is
- * Bob Moss.
- * Portions created by the Initial Developer are Copyright (C) 2010
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *  Bob Moss <bmoss@mozilla.com>
- * 
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 package com.mozilla.SUTAgentAndroid;
 
@@ -48,12 +48,12 @@ import java.util.Timer;
 import com.mozilla.SUTAgentAndroid.service.ASMozStub;
 import com.mozilla.SUTAgentAndroid.service.DoCommand;
 
-// import dalvik.system.VMRuntime;
+
 import android.app.Activity;
 import android.app.KeyguardManager;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
-// import android.content.ComponentName;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -83,7 +83,7 @@ public class SUTAgentAndroid extends Activity
 	MenuItem mExitMenuItem;
 	Timer timer = null;
 	
-//	public static SUTAgentAndroid me = null;
+
     public static String sUniqueID = null;
     public static String sLocalIPAddr = null;
     public static String sACStatus = null;
@@ -105,7 +105,7 @@ public class SUTAgentAndroid extends Activity
     private PowerManager.WakeLock pwl = null;
     
     private BroadcastReceiver battReceiver = null;
-//    private ComponentName service = null;
+
 
 	public boolean onCreateOptionsMenu(Menu menu)
 		{
@@ -128,20 +128,20 @@ public class SUTAgentAndroid extends Activity
 		return(RegSvrIPAddr);
 		}
     
-    /** Called when the activity is first created. */
+    
     @Override
     public void onCreate(Bundle savedInstanceState)
     	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-//        Debug.waitForDebugger();
 
-//        long lHeapSize = VMRuntime.getRuntime().getMinimumHeapSize();
-//        lHeapSize = 16000000;
-//        VMRuntime.getRuntime().setMinimumHeapSize(lHeapSize);
+
+
+
+
         
-        // Keep phone from locking or remove lock on screen
+        
         KeyguardManager km = (KeyguardManager)getSystemService(Context.KEYGUARD_SERVICE);
         if (km != null)
         	{
@@ -150,7 +150,7 @@ public class SUTAgentAndroid extends Activity
         		kl.disableKeyguard();
         	}
         
-        // No sleeping on the job
+        
         PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
         if (pm != null)
         	{
@@ -161,7 +161,7 @@ public class SUTAgentAndroid extends Activity
         
         DoCommand dc = new DoCommand(getApplication());
         
-        // Get configuration settings from "ini" file
+        
         File dir = getFilesDir();
         File iniFile = new File(dir, "SUTAgent.ini");
         String sIniFile = iniFile.getAbsolutePath();
@@ -176,7 +176,7 @@ public class SUTAgentAndroid extends Activity
         if (getLocalIpAddress() == null)
         	setUpNetwork(sIniFile);
         
-//        me = this;
+
         
         WifiInfo wifi;
         WifiManager wifiMan = (WifiManager)getSystemService(Context.WIFI_SERVICE);
@@ -278,7 +278,7 @@ public class SUTAgentAndroid extends Activity
         	{
         	Thread thread = new Thread(null, doStartService, "StartServiceBkgnd");
         	thread.start();
-//        	ToDoListening(1,300,dc);
+
         	bNetworkingStarted = true;
         	String sRegRet = "";
         	if (RegSvrIPAddr.length() > 0)
@@ -354,17 +354,17 @@ public class SUTAgentAndroid extends Activity
 				{
 				StringBuilder sb = new StringBuilder();
 
-				int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1); // charge level from 0 to scale inclusive
-				int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1); // Max value for charge level
+				int rawlevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1); 
+				int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1); 
 				int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
 				int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, -1);
 				boolean present = intent.getBooleanExtra(BatteryManager.EXTRA_PRESENT, false);
-				int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1); //0 if the device is not plugged in; 1 if plugged into an AC power adapter; 2 if plugged in via USB.
-//				int voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1); // voltage in millivolts
-				nBatteryTemp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1); // current battery temperature in tenths of a degree Centigrade
-//				String technology = intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY);
+				int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1); 
 
-				nChargeLevel = -1;  // percentage, or -1 for unknown
+				nBatteryTemp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1); 
+
+
+				nChargeLevel = -1;  
 				if (rawlevel >= 0 && scale > 0)
 					{
 					nChargeLevel = (rawlevel * 100) / scale;
@@ -391,14 +391,14 @@ public class SUTAgentAndroid extends Activity
 					{
 					sb.append("Overheated ");
 					sb.append((((float)(nBatteryTemp))/10));
-					sb.append("(°C)");
+					sb.append("(C)");
 					}
 				else
 					{
 					switch(status)
 						{
 						case BatteryManager.BATTERY_STATUS_UNKNOWN:
-							// old emulator; maybe also when plugged in with no battery
+							
 							if (present == true)
 								sb.append(" UNKNOWN");
 							break;
@@ -446,33 +446,33 @@ public class SUTAgentAndroid extends Activity
     	String adhoc = tmpdc.GetIniData("Network Settings", "ADHOC", sIniFile);
     	
 		Toast.makeText(getApplication().getApplicationContext(), "Starting and configuring network", Toast.LENGTH_LONG).show();
-/*		
-		ContentResolver cr = getContentResolver();
-		int nRet;
-		try {
-			nRet = Settings.System.getInt(cr, Settings.System.WIFI_USE_STATIC_IP);
-			String foo2 = "" + nRet;
-		} catch (SettingNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-*/
-/*    	
-    	wc.SSID = "\"Mozilla-Build\"";
-    	wc.preSharedKey  = "\"MozillaBuildQA500\"";
-    	wc.hiddenSSID = true;
-    	wc.status = WifiConfiguration.Status.ENABLED;    
-    	wc.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
-    	wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-    	wc.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-    	wc.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-    	wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-    	wc.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-    	wc.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-*/    	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    	
 		wc.SSID = "\"" + ssid + "\"";
-//    	wc.SSID = "\"Mozilla-G\"";
-//    	wc.SSID = "\"Mozilla\"";
+
+
 		
 		if (auth.contentEquals("wpa2"))
 			{
@@ -622,101 +622,101 @@ public class SUTAgentAndroid extends Activity
 			Intent listenerService = new Intent();
 			listenerService.setAction("com.mozilla.SUTAgentAndroid.service.LISTENER_SERVICE");
 			startService(listenerService);
-//			service = startService(listenerService);
+
     		}
     	};
-/*
-    class ToDoListener extends TimerTask 
-    	{
-    	boolean 	bFirstRun = true;
-    	DoCommand	dc = null;
-    	
-    	ToDoListener() {}
-    	
-    	ToDoListener(DoCommand dc)
-    		{
-    		this.dc = dc;
-    		}
-    	
-		public void run ()
-			{
-			if (bFirstRun == true)
-				{
-				Intent listenerService = new Intent();
-				listenerService.setAction("com.mozilla.SUTAgentAndroid.service.LISTENER_SERVICE");
-				service = startService(listenerService);
-				bFirstRun = false;
-				}
-			else
-				{
-				if (dc != null)
-					{
-					String sRet = this.dc.SendPing("www.mozilla.org", null);
-					if (sRet.contains("3 received"))
-						this.dc.StopAlert();
-					else
-						this.dc.StartAlert();
-					sRet = null;
-					System.gc();
-					}
-				}
-			}
-		}
-	
-	public void ToDoListening(int delay, int interval, DoCommand dc)
-		{
-		if (timer == null)
-			timer = new Timer();
-//		timer.scheduleAtFixedRate(new ToDoListener(dc), delay * 1000, interval * 1000);
-//		timer.schedule(new ToDoListener(dc), delay * 1000);
-		timer.schedule(new ToDoListener(), delay * 1000);
-		}
 
-	class DoHeartBeat extends TimerTask
-		{
-    	PrintWriter out;
-	
-    	DoHeartBeat(PrintWriter out)
-			{
-    		this.out = out;
-			}
-	
-    	public void run ()
-			{
-    		String sRet = "";
-    		
-    		Calendar cal = Calendar.getInstance();
-    		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HH:mm:ss");
-    		sRet = sdf.format(cal.getTime());
-    		sRet += " Thump thump - " + sUniqueID + "\r\n";
 
-    		out.write(sRet);
-    		out.flush();
-			}
-		}
 
-	public void StartHeartBeat(PrintWriter out)
-		{
-		// start the heartbeat
-		this.dataOut = out;
-		if (timer == null)
-			timer = new Timer();
-		timer.scheduleAtFixedRate(new DoHeartBeat(dataOut), 0, 60000);
-		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
-	public void StopHeartBeat()
-		{
-		// stop the heartbeat
-		this.dataOut = null;
-		if (timer != null)
-			{
-			timer.cancel();
-			timer.purge();
-			timer = null;
-			System.gc();
-			}
-		}
-*/	
 	
     public String getLocalIpAddress()
 		{
