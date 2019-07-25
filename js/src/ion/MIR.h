a9842aa5dd9e4b638b7f5c410f99c68e4eb41d8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:51:41 +0000

    Bug 723636: Implement MRegExp, take 2. (r=jandem)
    UltraBlame original commit: 902afe4fb2fa763586989836a851ebc0ab8d3908

--- js/src/ion/MIR.h
+++ js/src/ion/MIR.h
@@ -553,8 +553,11 @@ class MAryInstruction : public MInstruction
     }
 };
 
+class MNullaryInstruction : public MAryInstruction<0>
+{ };
 
-class MStart : public MAryInstruction<0>
+
+class MStart : public MNullaryInstruction
 {
   public:
     enum StartType {
@@ -584,7 +587,7 @@ class MStart : public MAryInstruction<0>
 
 
 
-class MOsrEntry : public MAryInstruction<0>
+class MOsrEntry : public MNullaryInstruction
 {
   protected:
     MOsrEntry() {
@@ -599,7 +602,7 @@ class MOsrEntry : public MAryInstruction<0>
 };
 
 
-class MConstant : public MAryInstruction<0>
+class MConstant : public MNullaryInstruction
 {
     js::Value value_;
     uint32 constantPoolIndex_;
@@ -637,7 +640,7 @@ class MConstant : public MAryInstruction<0>
     }
 };
 
-class MParameter : public MAryInstruction<0>
+class MParameter : public MNullaryInstruction
 {
     int32 index_;
     types::TypeSet *typeSet_;
@@ -668,7 +671,7 @@ class MParameter : public MAryInstruction<0>
     bool congruentTo(MDefinition * const &ins) const;
 };
 
-class MCallee : public MAryInstruction<0>
+class MCallee : public MNullaryInstruction
 {
   public:
     MCallee()
@@ -925,7 +928,7 @@ class MThrow
     }
 };
 
-class MNewArray : public MAryInstruction<0>
+class MNewArray : public MNullaryInstruction
 {
     
     uint32 count_;
@@ -953,7 +956,7 @@ class MNewArray : public MAryInstruction<0>
 
 
 
-class MPrepareCall : public MAryInstruction<0>
+class MPrepareCall : public MNullaryInstruction
 {
   public:
     INSTRUCTION_HEADER(PrepareCall);
@@ -2000,7 +2003,7 @@ class MOsrScopeChain : public MAryInstruction<1>
 };
 
 
-class MCheckOverRecursed : public MAryInstruction<0>
+class MCheckOverRecursed : public MNullaryInstruction
 {
   public:
     INSTRUCTION_HEADER(CheckOverRecursed);
@@ -2008,7 +2011,7 @@ class MCheckOverRecursed : public MAryInstruction<0>
 
 
 
-class MRecompileCheck : public MAryInstruction<0>
+class MRecompileCheck : public MNullaryInstruction
 {
     MRecompileCheck() {
         setGuard();
@@ -2021,6 +2024,52 @@ class MRecompileCheck : public MAryInstruction<0>
         return new MRecompileCheck();
     }
 
+    AliasSet getAliasSet() const {
+        return AliasSet::None();
+    }
+};
+
+class MRegExp : public MNullaryInstruction
+{
+  public:
+    
+    
+    
+    
+    enum CloneBehavior {
+        UseSource,
+        MustClone
+    };
+
+  private:
+    HeapPtr<RegExpObject> source_;
+    CloneBehavior shouldClone_;
+
+    MRegExp(RegExpObject *source, CloneBehavior shouldClone)
+      : source_(source),
+        shouldClone_(shouldClone)
+    {
+        setResultType(MIRType_Object);
+
+        
+        
+        if (shouldClone == UseSource)
+            setMovable();
+    }
+
+  public:
+    INSTRUCTION_HEADER(RegExp)
+
+    static MRegExp *New(RegExpObject *source, CloneBehavior shouldClone) {
+        return new MRegExp(source, shouldClone);
+    }
+
+    const HeapPtr<RegExpObject> &source() const {
+        return source_;
+    }
+    CloneBehavior shouldClone() const {
+        return shouldClone_;
+    }
     AliasSet getAliasSet() const {
         return AliasSet::None();
     }
