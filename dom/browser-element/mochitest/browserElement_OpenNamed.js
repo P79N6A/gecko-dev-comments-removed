687f79474c823b95b5cd48a3599c0205220bcf1e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:39:14 +0000

    Bug 780546 - Add test for opening named windows inside <iframe mozbrowser>. r=mounir
    UltraBlame original commit: 6c7ed23db4b2bbf222581c3a6411f42abf191b67

---
+++ dom/browser-element/mochitest/browserElement_OpenNamed.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+"use strict";
+SimpleTest.waitForExplicitFinish();
+
+var iframe;
+var popupFrame;
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addToWhitelist();
+
+  iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  var gotPopup = false;
+  iframe.addEventListener('mozbrowseropenwindow', function(e) {
+    is(gotPopup, false, 'Should get just one popup.');
+    gotPopup = true;
+    popupFrame = e.detail.frameElement;
+
+    
+    popupFrame.addEventListener('mozbrowsershowmodalprompt', function promptlistener(e) {
+      popupFrame.removeEventListener('mozbrowsershowmodalprompt', promptlistener);
+
+      ok(gotPopup, 'Got openwindow event before showmodalprompt event.');
+      is(e.detail.message, 'success: loaded');
+      SimpleTest.executeSoon(test2);
+    });
+
+    document.body.appendChild(popupFrame);
+  });
+
+  
+  
+  
+  
+  
+  
+  
+  iframe.src = 'file_browserElement_OpenNamed.html';
+  document.body.appendChild(iframe);
+}
+
+function test2() {
+  popupFrame.addEventListener('mozbrowsershowmodalprompt', function(e) {
+    is(e.detail.message, 'success: loaded');
+    SimpleTest.finish();
+  });
+
+  iframe.src = 'file_browserElement_OpenNamed.html?test2';
+}
+
+runTest();
