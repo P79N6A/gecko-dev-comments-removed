5f805ff9ff4aff47d6579539c40fbdfb05000030
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:01:02 +0000

    Generate jump table for JSOP_TABLESWITCH (bug 591972, r=dvander)
    UltraBlame original commit: 673ae0e2f656e20dd858a59a14be50f68201d64c

--- js/src/assembler/assembler/MacroAssemblerX86Common.h
+++ js/src/assembler/assembler/MacroAssemblerX86Common.h
@@ -857,7 +857,11 @@ public:
         m_assembler.jmp_m(address.offset, address.base);
     }
 
-    
+    void jump(BaseIndex address)
+    {
+        m_assembler.jmp_m(address.offset, address.base, address.index, address.scale);
+    }
+
     
     
     
