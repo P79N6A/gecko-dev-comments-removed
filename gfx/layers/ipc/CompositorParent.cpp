161ffa0813d71c5e82cd0920904b4278f66cffec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:01:01 +0000

    Bug 728284 - Main container layer has siblings that are drawn in the background. r=bgirard
    UltraBlame original commit: c210ff20f3711e83eeb21122f60cde7d164b42c1

--- gfx/layers/ipc/CompositorParent.cpp
+++ gfx/layers/ipc/CompositorParent.cpp
@@ -221,6 +221,23 @@ CompositorParent::Composite()
 #endif
 }
 
+
+
+static void
+SetShadowProperties(Layer* aLayer)
+{
+  
+  ShadowLayer* shadow = aLayer->AsShadowLayer();
+  shadow->SetShadowTransform(aLayer->GetTransform());
+  shadow->SetShadowVisibleRegion(aLayer->GetVisibleRegion());
+  shadow->SetShadowClipRect(aLayer->GetClipRect());
+
+  for (Layer* child = aLayer->GetFirstChild();
+      child; child = child->GetNextSibling()) {
+    SetShadowProperties(child);
+  }
+}
+
 #ifdef MOZ_WIDGET_ANDROID
 
 
@@ -228,6 +245,21 @@ Layer*
 CompositorParent::GetPrimaryScrollableLayer()
 {
   Layer* root = mLayerManager->GetRoot();
+
+  
+  
+  
+  
+  
+  Layer* discardLayer = root->GetFirstChild();
+
+  while (discardLayer) {
+    if (!discardLayer->AsContainerLayer()) {
+      discardLayer->IntersectClipRect(nsIntRect());
+      SetShadowProperties(discardLayer);
+    }
+    discardLayer = discardLayer->GetNextSibling();
+  }
 
   nsTArray<Layer*> queue;
   queue.AppendElement(root);
@@ -253,23 +285,6 @@ CompositorParent::GetPrimaryScrollableLayer()
   return root;
 }
 #endif
-
-
-
-static void
-SetShadowProperties(Layer* aLayer)
-{
-  
-  ShadowLayer* shadow = aLayer->AsShadowLayer();
-  shadow->SetShadowTransform(aLayer->GetTransform());
-  shadow->SetShadowVisibleRegion(aLayer->GetVisibleRegion());
-  shadow->SetShadowClipRect(aLayer->GetClipRect());
-
-  for (Layer* child = aLayer->GetFirstChild();
-      child; child = child->GetNextSibling()) {
-    SetShadowProperties(child);
-  }
-}
 
 void
 CompositorParent::TransformShadowTree()
