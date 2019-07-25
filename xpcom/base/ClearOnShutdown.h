ffeaa9b8a6463bcdca4db35621f0ae80e27f802f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:23:16 +0000

    Bug 706958 - Add ClearOnShutdown() function which will clear a smart pointer on shutdown. r=bsmedberg
    UltraBlame original commit: ab38cc8b58667c8d719135516ac76b8033e3acdb

---
+++ xpcom/base/ClearOnShutdown.h
@@ -0,0 +1,119 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_ClearOnShutdown_h
+
+#include <nsAutoPtr.h>
+#include <nsCOMPtr.h>
+#include <nsIObserver.h>
+#include <nsIObserverService.h>
+#include <mozilla/Services.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+namespace mozilla {
+namespace ClearOnShutdown_Internal {
+
+template<class SmartPtr>
+class ShutdownObserver : public nsIObserver
+{
+public:
+  ShutdownObserver(SmartPtr *aPtr)
+    : mPtr(aPtr)
+  {}
+
+  virtual ~ShutdownObserver()
+  {}
+
+  NS_IMETHOD QueryInterface(REFNSIID aIID, void ** aInstancePtr);
+  NS_INLINE_DECL_REFCOUNTING(ShutdownObserver)
+
+  NS_IMETHOD Observe(nsISupports *aSubject, const char *aTopic,
+                     const PRUnichar *aData)
+  {
+    MOZ_ASSERT(strcmp(aTopic, NS_XPCOM_SHUTDOWN_OBSERVER_ID) == 0);
+
+    if (mPtr) {
+      *mPtr = NULL;
+    }
+
+    return NS_OK;
+  }
+
+private:
+  SmartPtr *mPtr;
+};
+
+
+template<class SmartPtr>
+NS_IMPL_QUERY_INTERFACE1(ShutdownObserver<SmartPtr>, nsIObserver)
+
+} 
+
+template<class SmartPtr>
+void ClearOnShutdown(SmartPtr *aPtr)
+{
+  nsRefPtr<ClearOnShutdown_Internal::ShutdownObserver<SmartPtr> > observer =
+    new ClearOnShutdown_Internal::ShutdownObserver<SmartPtr>(aPtr);
+
+  nsCOMPtr<nsIObserverService> os = services::GetObserverService();
+  if (!os) {
+    NS_WARNING("Could not get observer service!");
+    return;
+  }
+  os->AddObserver(observer, NS_XPCOM_SHUTDOWN_OBSERVER_ID, false);
+}
+
+} 
+
+#endif
