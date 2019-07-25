f0574518c4a09edb7b398fc2e40d01e2d52dda54
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:02:29 +0000

    Bug 623750: browser_bug596336.js cannot run by itself. r=Unfocused, a=tests
    UltraBlame original commit: 27071f97a8eb1784ef06520faadcb3312f878975

--- toolkit/mozapps/extensions/test/browser/browser_bug596336.js
+++ toolkit/mozapps/extensions/test/browser/browser_bug596336.js
@@ -19,13 +19,7 @@ function test() {
 }
 
 function end_test() {
-  
-  AddonManager.getAllInstalls(function(aInstalls) {
-    is(aInstalls.length, 1, "Should be one available install");
-    aInstalls[0].cancel();
-
-    close_manager(gManagerWindow, finish);
-  });
+  close_manager(gManagerWindow, finish);
 }
 
 function get_list_item_count() {
