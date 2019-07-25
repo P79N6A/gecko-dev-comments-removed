2a03ba339473e3abc874acceb0649f9ea204e609
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:30:22 +0000

    Bug 571849 - Fix enumerateOwn and the Iterate constructor on Xray wrappers. r=gal
    UltraBlame original commit: 67a69685dc564c8c660b844487083983dd9125c1

--- js/src/xpconnect/wrappers/XrayWrapper.cpp
+++ js/src/xpconnect/wrappers/XrayWrapper.cpp
@@ -236,7 +236,7 @@ ResolveNativeProperty(JSContext *cx, JSObject *wrapper, JSObject *holder, jsid i
     desc->attrs = JSPROP_ENUMERATE;
     desc->getter = NULL;
     desc->setter = NULL;
-    desc->shortid = 0;
+    desc->shortid = NULL;
     desc->value = JSVAL_VOID;
 
     jsval fval = JSVAL_VOID;
