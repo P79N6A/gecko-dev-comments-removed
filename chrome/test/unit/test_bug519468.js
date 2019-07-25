019eaddacab40bbab32bdacb1ace98b8f550ccea
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:40:33 +0000

    Bug 557024: Mock out the locale service to make test reliable on non en-US systems. r=bsmedberg
    UltraBlame original commit: 9819f1e610ef00ac0f6d53aab37d73902637db14

--- chrome/test/unit/test_bug519468.js
+++ chrome/test/unit/test_bug519468.js
@@ -7,6 +7,45 @@ var MANIFESTS = [
   do_get_file("data/test_bug519468.manifest")
 ];
 
+
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+let stubOSLocale = null;
+
+stubID = Components.ID("9d09d686-d913-414c-a1e6-4be8652d7d93");
+localeContractID = "@mozilla.org/intl/nslocaleservice;1";
+
+StubLocaleService = {
+  classDescription: "Stub version of Locale service for testing",
+  classID:          stubID,
+  contractID:       localeContractID,
+  QueryInterface:   XPCOMUtils.generateQI([Ci.nsILocaleService, Ci.nsISupports, Ci.nsIFactory]),
+
+  createInstance: function (outer, iid) {
+    if (outer)
+      throw Components.results.NS_ERROR_NO_AGGREGATION;
+    return this.QueryInterface(iid);
+  },
+  lockFactory: function (lock) {
+    throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+  },
+
+  getLocaleComponentForUserAgent: function SLS_getLocaleComponentForUserAgent()
+  {
+    return stubOSLocale;
+  }
+}
+
+let registrar = Components.manager.nsIComponentRegistrar;
+
+let localeCID = registrar.contractIDToCID(localeContractID)
+let originalFactory =
+      Components.manager.getClassObject(Components.classes[localeContractID],
+                                        Components.interfaces.nsIFactory);
+
+registrar.registerFactory(stubID, "Unit test Locale Service", localeContractID, StubLocaleService);
+
+
 do_test_pending()
 registerManifests(MANIFESTS);
 
@@ -15,48 +54,62 @@ var chromeReg = Cc["@mozilla.org/chrome/chrome-registry;1"]
                 .QueryInterface(Ci.nsIToolkitChromeRegistry);
 chromeReg.checkForNewChrome();
 
-var localeService = Cc["@mozilla.org/intl/nslocaleservice;1"]
-                    .getService(Ci.nsILocaleService);
-
 var prefService = Cc["@mozilla.org/preferences-service;1"]
                   .getService(Ci.nsIPrefService)
                   .QueryInterface(Ci.nsIPrefBranch);
 var os = Cc["@mozilla.org/observer-service;1"]
          .getService(Ci.nsIObserverService);
 
-var systemLocale = localeService.getLocaleComponentForUserAgent();
-var count = 0;
 var testsLocale = [
-  {matchOS: false, selected: null, locale: "en-US"},
-  {matchOS: true, selected: null, locale: systemLocale},
-  {matchOS: true, selected: "fr-FR", locale: systemLocale},
-  {matchOS: false, selected: "fr-FR", locale: "fr-FR"},
-  {matchOS: false, selected: "de-DE", locale: "de-DE"},
-  {matchOS: true, selected: null, locale: systemLocale}
+  
+  {matchOS: false, selected: "en-US", osLocale: "xx-AA", locale: "en-US"},
+  {matchOS: true, selected: "en-US", osLocale: "xx-AA", locale: "xx-AA"},
+  {matchOS: false, selected: "fr-FR", osLocale: "xx-AA", locale: "fr-FR"},
+  {matchOS: true, selected: "fr-FR", osLocale: "xx-AA", locale: "xx-AA"},
+  {matchOS: false, selected: "de-DE", osLocale: "xx-AA", locale: "de-DE"},
+  {matchOS: true, selected: "de-DE", osLocale: "xx-AA", locale: "xx-AA"},
+  
+  
+  {matchOS: false, selected: "en-US", osLocale: "xx-BB", locale: "en-US"},
+  {matchOS: true, selected: "en-US", osLocale: "xx-BB", locale: "xx-AA"},
+  {matchOS: false, selected: "fr-FR", osLocale: "xx-BB", locale: "fr-FR"},
+  {matchOS: true, selected: "fr-FR", osLocale: "xx-BB", locale: "xx-AA"},
+  
+  {matchOS: false, selected: "en-US", osLocale: "xy-BB", locale: "en-US"},
+  {matchOS: true, selected: "en-US", osLocale: "xy-BB", locale: "en-US"},
+  {matchOS: false, selected: "fr-FR", osLocale: "xy-BB", locale: "fr-FR"},
+  {matchOS: true, selected: "fr-FR", osLocale: "xy-BB", locale: "en-US"},
 ];
 
+var observedLocale = null;
+
 function test_locale(aTest) {
+  observedLocale = null;
+
+  stubOSLocale = aTest.osLocale;
   prefService.setBoolPref("intl.locale.matchOS", aTest.matchOS);
-  prefService.setCharPref("general.useragent.locale", aTest.selected || "en-US");
+  prefService.setCharPref("general.useragent.locale", aTest.selected);
 
   chromeReg.reloadChrome();
+
+  do_check_eq(observedLocale, aTest.locale);
 }
 
-function checkValidity() {
-  var selectedLocale = chromeReg.getSelectedLocale("testmatchos");
-  do_check_eq(selectedLocale, testsLocale[count].locale);
 
-  count++;
-  if (count >= testsLocale.length) {
-    os.removeObserver(checkValidity, "selected-locale-has-changed");
-    do_test_finished();
-  }
-  else {
-    test_locale(testsLocale[count]);
-  }
+
+function checkValidity() {
+  observedLocale = chromeReg.getSelectedLocale("testmatchos");
+  dump("checkValidity called back with locale = " + observedLocale + "\n");
 }
 
 function run_test() {
   os.addObserver(checkValidity, "selected-locale-has-changed", false);
-  test_locale(testsLocale[count]);
+
+  for (let count = 0 ; count < testsLocale.length ; count++) {
+    dump("count = " + count + " " + testsLocale[count].toSource() + "\n");
+    test_locale(testsLocale[count]);
+  }
+
+  os.removeObserver(checkValidity, "selected-locale-has-changed");
+  do_test_finished();
 }
