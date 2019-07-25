3c8dbea47de78b06c92970eb132a0475ca32aa28
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:02:25 +0000

    Bug 742636 - Keep observed node alive during microtask if there are transient observers, r=sicking
    UltraBlame original commit: 77f0e7d882dd2810179dee31d8240fac7555a5bd

--- content/base/src/nsDOMMutationObserver.h
+++ content/base/src/nsDOMMutationObserver.h
@@ -152,7 +152,7 @@ protected:
   nsMutationReceiverBase(nsINode* aRegisterTarget,
                          nsMutationReceiverBase* aParent)
   : mTarget(nsnull), mObserver(nsnull), mParent(aParent),
-    mRegisterTarget(aRegisterTarget)
+    mRegisterTarget(aRegisterTarget), mKungFuDeathGrip(aParent->Target())
   {
     NS_ASSERTION(mParent->Subtree(), "Should clone a non-subtree observer!");
     mRegisterTarget->AddMutationObserver(this);
@@ -195,6 +195,9 @@ protected:
   
   nsINode*                           mRegisterTarget;
   nsCOMArray<nsMutationReceiverBase> mTransientReceivers;
+  
+  
+  nsCOMPtr<nsINode>                  mKungFuDeathGrip;
   
 private:
   bool                               mSubtree;
