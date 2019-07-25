e87fd66b4f4857c1120b1f50966283d79ff6aac4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:52:41 +0000

    Bug 686694 - Don't extract libraries on Android unless we're starting with the DEBUG intent. r=dougt
    UltraBlame original commit: 49c16f43ff260cb8f07f4d12bf49e4540c7967a5

--- embedding/android/GeckoAppShell.java
+++ embedding/android/GeckoAppShell.java
@@ -93,8 +93,6 @@ public class GeckoAppShell
     static private final int NOTIFY_IME_CANCELCOMPOSITION = 2;
     static private final int NOTIFY_IME_FOCUSCHANGE = 3;
 
-    static public final long kFreeSpaceThreshold = 157286400L; 
-    static private final long kLibFreeSpaceBuffer = 20971520L; 
     static private File sCacheFile = null;
     static private int sFreeSpace = -1;
 
@@ -355,7 +353,8 @@ public class GeckoAppShell
 
         putLocaleEnv();
 
-        if (freeSpace + kLibFreeSpaceBuffer < kFreeSpaceThreshold) {
+        boolean extractLibs = GeckoApp.ACTION_DEBUG.equals(i.getAction());
+        if (!extractLibs) {
             
             File[] files = cacheFile.listFiles();
             if (files != null) {
@@ -367,7 +366,7 @@ public class GeckoAppShell
                 }
             }
         }
-        loadLibs(apkName, freeSpace > kFreeSpaceThreshold);
+        loadLibs(apkName, extractLibs);
     }
 
     private static void putLocaleEnv() {
