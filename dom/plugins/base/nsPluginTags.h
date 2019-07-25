ceafd4005d478486182cb8d7eeaa5cfd9071bac2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:12:57 +0000

    Bug 653794: Improve management of plugin MIME types, MIME descriptions, and extensions. r=bsmedberg
    UltraBlame original commit: 6824e96ca32aa7818b3d3a5c2737df9eb9eff970

--- dom/plugins/base/nsPluginTags.h
+++ dom/plugins/base/nsPluginTags.h
@@ -100,20 +100,14 @@ public:
   PRBool IsEnabled();
   void RegisterWithCategoryManager(PRBool aOverrideInternalTypes,
                                    nsRegisterType aType = ePluginRegister);
-
-  
-  
-  
-  void RemoveJavaSentinel(PRInt32 sentinelIndex);
   
   nsRefPtr<nsPluginTag> mNext;
   nsPluginHost *mPluginHost;
   nsCString     mName; 
   nsCString     mDescription; 
-  PRInt32       mVariants;
-  char          **mMimeTypeArray;
-  nsTArray<nsCString> mMimeDescriptionArray; 
-  char          **mExtensionsArray;
+  nsTArray<nsCString> mMimeTypes; 
+  nsTArray<nsCString> mMimeDescriptions; 
+  nsTArray<nsCString> mExtensions; 
   PRLibrary     *mLibrary;
   nsRefPtr<nsNPAPIPlugin> mEntryPoint;
   PRPackedBool  mCanUnloadLibrary;
