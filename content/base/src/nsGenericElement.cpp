fa362a0356917329ac138d5ac3b8e2832f465781
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 13:59:06 +0000

    Bug 631837.  Stop doing recursive tree traversal in querySelector(All).  r=sicking
    UltraBlame original commit: df90f3ca7deb0c67186123ff6ba1063d591f28f1

--- content/base/src/nsGenericElement.cpp
+++ content/base/src/nsGenericElement.cpp
@@ -5518,58 +5518,6 @@ ParseSelectorList(nsINode* aNode,
 }
 
 
-
-
-
-typedef PRBool
-(* ElementMatchedCallback)(nsIContent* aMatchingElement, void* aClosure);
-
-
-static PRBool
-TryMatchingElementsInSubtree(nsINode* aRoot,
-                             TreeMatchContext& aTreeMatchContext,
-                             nsCSSSelectorList* aSelectorList,
-                             ElementMatchedCallback aCallback,
-                             void* aClosure)
-{
-  PRBool continueIteration = PR_TRUE;
-  for (nsINode::ChildIterator iter(aRoot); !iter.IsDone(); iter.Next()) {
-    nsIContent* kid = iter;
-    if (!kid->IsElement()) {
-      continue;
-    }
-
-    if (nsCSSRuleProcessor::SelectorListMatches(kid->AsElement(),
-                                                aTreeMatchContext,
-                                                aSelectorList)) {
-      continueIteration = (*aCallback)(kid, aClosure);
-    }
-
-    if (continueIteration) {
-      continueIteration =
-        TryMatchingElementsInSubtree(kid, aTreeMatchContext, aSelectorList,
-                                     aCallback, aClosure);
-    }
-    
-    if (!continueIteration) {
-      break;
-    }
-  }
-
-  return continueIteration;
-}
-
-static PRBool
-FindFirstMatchingElement(nsIContent* aMatchingElement,
-                         void* aClosure)
-{
-  NS_PRECONDITION(aMatchingElement && aClosure, "How did that happen?");
-  nsIContent** slot = static_cast<nsIContent**>(aClosure);
-  *slot = aMatchingElement;
-  return PR_FALSE;
-}
-
-
 nsIContent*
 nsGenericElement::doQuerySelector(nsINode* aRoot, const nsAString& aSelector,
                                   nsresult *aResult)
@@ -5581,23 +5529,21 @@ nsGenericElement::doQuerySelector(nsINode* aRoot, const nsAString& aSelector,
                                getter_Transfers(selectorList));
   NS_ENSURE_SUCCESS(*aResult, nsnull);
 
-  nsIContent* foundElement = nsnull;
   TreeMatchContext matchingContext(PR_FALSE,
                                    nsRuleWalker::eRelevantLinkUnvisited,
                                    aRoot->GetOwnerDoc());
-  TryMatchingElementsInSubtree(aRoot, matchingContext, selectorList,
-                               FindFirstMatchingElement, &foundElement);
+  for (nsIContent* cur = aRoot->GetFirstChild();
+       cur;
+       cur = cur->GetNextNode(aRoot)) {
+    if (cur->IsElement() &&
+        nsCSSRuleProcessor::SelectorListMatches(cur->AsElement(),
+                                                matchingContext,
+                                                selectorList)) {
+      return cur;
+    }
+  }
 
-  return foundElement;
-}
-
-static PRBool
-AppendAllMatchingElements(nsIContent* aMatchingElement,
-                          void* aClosure)
-{
-  NS_PRECONDITION(aMatchingElement && aClosure, "How did that happen?");
-  static_cast<nsBaseContentList*>(aClosure)->AppendElement(aMatchingElement);
-  return PR_TRUE;
+  return nsnull;
 }
 
 
@@ -5620,8 +5566,16 @@ nsGenericElement::doQuerySelectorAll(nsINode* aRoot,
   TreeMatchContext matchingContext(PR_FALSE,
                                    nsRuleWalker::eRelevantLinkUnvisited,
                                    aRoot->GetOwnerDoc());
-  TryMatchingElementsInSubtree(aRoot, matchingContext, selectorList,
-                               AppendAllMatchingElements, contentList);
+  for (nsIContent* cur = aRoot->GetFirstChild();
+       cur;
+       cur = cur->GetNextNode(aRoot)) {
+    if (cur->IsElement() &&
+        nsCSSRuleProcessor::SelectorListMatches(cur->AsElement(),
+                                                matchingContext,
+                                                selectorList)) {
+      contentList->AppendElement(cur);
+    }
+  }
   return NS_OK;
 }
 
