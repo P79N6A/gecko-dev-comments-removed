ca00a91b7a51a8a010e86c7255c799e62a8b7228
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:09:48 +0000

    Bug 651947 - Build error on Windows in winEmbed.cpp:171 - missing type specifier - int assumed [r=bsmedberg]
    UltraBlame original commit: e36c9820debda56f0a2697b6d3e239b7c21050b1

--- embedding/tests/winEmbed/winEmbed.cpp
+++ embedding/tests/winEmbed/winEmbed.cpp
@@ -166,18 +166,9 @@ int main(int argc, char *argv[])
     LoadString(ghInstanceApp, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
     MyRegisterClass(ghInstanceApp);
 
-    
-    
-    static const GREVersionRange vr = {
-        "1.8a1",
-        PR_TRUE,
-        "2.0",
-        PR_FALSE
-    };
-
     char path[_MAX_PATH];
-    GetModuleFileName(ghInstanceApp, self, sizeof(self));
-    lastslash = ns_strrpbrk(xpcomPath, "/\\");
+    GetModuleFileName(ghInstanceApp, path, sizeof(path));
+    char* lastslash = ns_strrpbrk(path, "/\\");
     if (!lastslash)
         return 7;
 
