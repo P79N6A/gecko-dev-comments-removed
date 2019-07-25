79a36a5344143b0bf82eafe747600fa1f94543b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:01:30 +0000

    Bug 693422 - Unify QApplication creation. r=tatiana
    UltraBlame original commit: 09c76e77d57ccd8dbea396fb61d38c6d1fb2a6cf

--- toolkit/xre/nsQAppInstance.cpp
+++ toolkit/xre/nsQAppInstance.cpp
@@ -38,35 +38,63 @@
 
 #include "nsQAppInstance.h"
 #include <QApplication>
+#ifdef MOZ_ENABLE_MEEGOTOUCH
+#include <MComponentData>
+#include <MApplicationService>
+#endif
 #include "prenv.h"
-
-
-extern int    gArgc;
-extern char **gArgv;
+#include <stdlib.h>
 
 QApplication *nsQAppInstance::sQAppInstance = NULL;
+#ifdef MOZ_ENABLE_MEEGOTOUCH
+MComponentData* nsQAppInstance::sMComponentData = NULL;
+#endif
 int nsQAppInstance::sQAppRefCount = 0;
 
-void nsQAppInstance::AddRef(void) {
-  if (qApp) return;
+void nsQAppInstance::AddRef(int& aArgc, char** aArgv, bool aDefaultProcess) {
+  if (qApp)
+    return;
   if (!sQAppInstance) {
-    const char *graphicsSystem = PR_GetEnv("MOZ_QT_GRAPHICSSYSTEM");
-    if (graphicsSystem)
+    const char *graphicsSystem = getenv("MOZ_QT_GRAPHICSSYSTEM");
+    if (graphicsSystem) {
       QApplication::setGraphicsSystem(QString(graphicsSystem));
+    }
 #if (MOZ_PLATFORM_MAEMO == 6)
-    QApplication::setStyle(QLatin1String("windows"));
-    if (!gArgc) {
-      gArgv[gArgc] = strdup("nsQAppInstance");
-      gArgc++;
+    
+    
+    
+    if (!aDefaultProcess) {
+      QApplication::setStyle(QLatin1String("windows"));
+    }
+    if (!aArgc) {
+      aArgv[aArgc] = strdup("nsQAppInstance");
+      aArgc++;
     }
 #endif
-    sQAppInstance = new QApplication(gArgc, gArgv);
+    sQAppInstance = new QApplication(aArgc, aArgv);
+#ifdef MOZ_ENABLE_MEEGOTOUCH
+    if (aDefaultProcess) {
+      
+      
+      
+      
+      
+      
+      gArgv[gArgc] = strdup("-software");
+      gArgc++;
+      sMComponentData = new MComponentData(aArgc, aArgv, "", new MApplicationService(""));
+    }
+#endif
   }
   sQAppRefCount++;
 }
 
 void nsQAppInstance::Release(void) {
   if (sQAppInstance && !--sQAppRefCount) {
+#ifdef MOZ_ENABLE_MEEGOTOUCH
+    delete sMComponentData;
+    sMComponentData = NULL;
+#endif
     delete sQAppInstance;
     sQAppInstance = NULL;
   }
