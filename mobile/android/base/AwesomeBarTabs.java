a09f64d97c48a37de8c31524a04c4de9ce98a5ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:22:53 +0000

    Bug 761132 - Don't try to fetch display column on history items (r=margaret)
    UltraBlame original commit: ed00ab13fee9e1ff4055e9403d2e25ada75d202a

--- mobile/android/base/AwesomeBarTabs.java
+++ mobile/android/base/AwesomeBarTabs.java
@@ -481,7 +481,6 @@ public class AwesomeBarTabs extends TabHost {
             byte[] favicon = cursor.getBlob(cursor.getColumnIndexOrThrow(URLColumns.FAVICON));
             Integer bookmarkId = cursor.getInt(cursor.getColumnIndexOrThrow(Combined.BOOKMARK_ID));
             Integer historyId = cursor.getInt(cursor.getColumnIndexOrThrow(Combined.HISTORY_ID));
-            Integer display = cursor.getInt(cursor.getColumnIndexOrThrow(Combined.DISPLAY));
 
             
             
