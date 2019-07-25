55eb6425f94babd19f3f9f69264b8f007dd727e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:40:29 +0000

    [INFER] Mark generic scripted functions as having unknown properties / return type, bug 643285.
    UltraBlame original commit: 2326e700c7857d0cf88cbffd81adb1bd42f7d8dd

--- js/src/jsfun.cpp
+++ js/src/jsfun.cpp
@@ -2756,6 +2756,14 @@ js_InitFunctionClass(JSContext *cx, JSObject *obj)
     if (!proto)
         return NULL;
 
+    
+
+
+
+    TypeObject *newType = proto->getNewType(cx);
+    if (!newType || !cx->markTypeObjectUnknownProperties(newType))
+        return NULL;
+
     JSFunction *fun = js_NewFunction(cx, proto, NULL, 0, JSFUN_INTERPRETED, obj, NULL, NULL, NULL);
     if (!fun)
         return NULL;
