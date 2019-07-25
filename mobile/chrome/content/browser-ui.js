350924d5e6a099846f91ba2920d7d57820d485a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:04:04 +0000

    Bug 609813 - Avoid Browser.computeSidebarVisibility in NewTabPopup 'TabOpen' handler [r=mbrubeck]
    UltraBlame original commit: c51635273c93402b0c99c604a0b810004c5b98b3

--- mobile/chrome/content/browser-ui.js
+++ mobile/chrome/content/browser-ui.js
@@ -1469,8 +1469,12 @@ var NewTabPopup = {
   },
 
   handleEvent: function nt_handleEvent(aEvent) {
+    
+    if (!aEvent.detail)
+      return;
+
     let [tabsVisibility,,,] = Browser.computeSidebarVisibility();
-    if (tabsVisibility != 1.0 && aEvent.detail)
+    if (tabsVisibility != 1.0)
       this.show(aEvent.originalTarget);
   }
 };
