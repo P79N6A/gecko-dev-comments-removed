738861269a5a91ea46c5fcb272d4c95598946f22
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:54:59 +0000

    Back out bf5e32270935 (bug 627642) for tabview orange, a=bustage
    UltraBlame original commit: 00f2b33bcb6011b50d7e49591489e6f3e0145b92

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -1405,37 +1405,17 @@ SessionStoreService.prototype = {
     this._browserSetState = true;
 
     
-    
-    
-    
-    let lastWindow = this._getMostRecentBrowserWindow();
-    let canUseLastWindow = lastWindow &&
-                           !lastWindow.__SS_lastSessionWindowID;
-
-    
     for (let i = 0; i < lastSessionState.windows.length; i++) {
       let winState = lastSessionState.windows[i];
       let lastSessionWindowID = winState.__lastSessionWindowID;
       
       delete winState.__lastSessionWindowID;
-
       
-      
-      
-      let windowToUse = windows[lastSessionWindowID];
-      if (!windowToUse && canUseLastWindow) {
-        windowToUse = lastWindow;
-        canUseLastWindow = false;
-      }
-
-      let [canUseWindow, canOverwriteTabs] = this._prepWindowToRestoreInto(windowToUse);
-
-      
-      if (canUseWindow) {
+      if (windows[lastSessionWindowID]) {
         
         
         if (winState._closedTabs && winState._closedTabs.length) {
-          let curWinState = this._windows[windowToUse.__SSi];
+          let curWinState = this._windows[windows[lastSessionWindowID].__SSi];
           curWinState._closedTabs = curWinState._closedTabs.concat(winState._closedTabs);
           curWinState._closedTabs.splice(this._prefBranch.getIntPref("sessionstore.max_tabs_undo"));
         }
@@ -1446,8 +1426,8 @@ SessionStoreService.prototype = {
         
         
         
-        
-        this.restoreWindow(windowToUse, { windows: [winState] }, canOverwriteTabs, true);
+        this.restoreWindow(windows[lastSessionWindowID], { windows: [winState] },
+                           false, true);
       }
       else {
         this._openWindowWithState({ windows: [winState] });
@@ -1464,80 +1444,6 @@ SessionStoreService.prototype = {
                           lastSessionState.session.recentCrashes || 0;
 
     this._lastSessionState = null;
-  },
-
-
-  
-
-
-
-
-
-
-
-
-
-
-  _prepWindowToRestoreInto: function sss__prepWindowToRestoreInto(aWindow) {
-    if (!aWindow)
-      return [false, false];
-
-    
-    
-    let canOverwriteTabs = false;
-
-    
-    
-    
-    
-    let data = this.getWindowValue(aWindow, "tabview-group");
-    if (data) {
-      data = JSON.parse(data);
-
-      
-      if (Object.keys(data).length > 1) {
-        return [false, false];
-      }
-      else {
-        
-        
-        
-        let groupKey = Object.keys(data)[0];
-        if (groupKey !== "0") {
-          data["0"] = data[groupKey];
-          delete data[groupKey];
-          this.setWindowValue(aWindow, "tabview-groups", JSON.stringify(data));
-        }
-      }
-    }
-
-    
-    
-    
-    
-    
-    let homePages = aWindow.gHomeButton.getHomePage().split("|");
-    let removableTabs = [];
-    let tabbrowser = aWindow.gBrowser;
-    let normalTabsLen = tabbrowser.tabs.length - tabbrowser._numPinnedTabs;
-    for (let i = tabbrowser._numPinnedTabs; i < tabbrowser.tabs.length; i++) {
-      let tab = tabbrowser.tabs[i];
-      if (homePages.indexOf(tab.linkedBrowser.currentURI.spec) != -1) {
-        removableTabs.push(tab);
-      }
-    }
-
-    if (tabbrowser.tabs.length == removableTabs.length) {
-      canOverwriteTabs = true;
-    }
-    else {
-      
-      for (let i = removableTabs.length - 1; i >= 0; i--) {
-        tabbrowser.removeTab(removableTabs.pop(), { animate: false });
-      }
-    }
-
-    return [true, canOverwriteTabs];
   },
 
 
@@ -2521,11 +2427,6 @@ SessionStoreService.prototype = {
     
     this.restoreHistoryPrecursor(aWindow, tabs, winData.tabs,
       (aOverwriteTabs ? (parseInt(winData.selected) || 1) : 0), 0, 0);
-
-    
-    
-    
-    aWindow.TabView.init();
 
     
     tabstrip.smoothScroll = smoothScroll;
