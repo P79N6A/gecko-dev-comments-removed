57f39ce3f8177cce9b10e3b944728d7de99aacfa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:27:47 +0000

    Bug 513162 - Fix for MOZ_WINSDK_TARGETVER < MOZ_NTDDI_LONGHORN r=jmathies
    UltraBlame original commit: 1233da24d7f3cc74a97af5f07d1b2ea1983668b1

--- widget/src/windows/nsUXThemeData.h
+++ widget/src/windows/nsUXThemeData.h
@@ -187,13 +187,15 @@ public:
   static DwmSetWindowAttributeProc dwmSetWindowAttributePtr;
   static DwmInvalidateIconicBitmapsProc dwmInvalidateIconicBitmapsPtr;
   static DwmDefWindowProcProc dwmDwmDefWindowProcPtr;
+#endif 
 
   static PRBool CheckForCompositor() {
     BOOL compositionIsEnabled = FALSE;
+#if MOZ_WINSDK_TARGETVER >= MOZ_NTDDI_LONGHORN
     if(dwmIsCompositionEnabledPtr)
       dwmIsCompositionEnabledPtr(&compositionIsEnabled);
+#endif 
     return sHaveCompositor = (compositionIsEnabled != 0);
   }
-#endif 
 };
 #endif 
--- widget/src/windows/nsWindow.cpp
+++ widget/src/windows/nsWindow.cpp
@@ -4399,6 +4399,7 @@ PRBool nsWindow::ProcessMessage(UINT msg, WPARAM &wParam, LPARAM &lParam,
 
   static PRBool getWheelInfo = PR_TRUE;
 
+#if MOZ_WINSDK_TARGETVER >= MOZ_NTDDI_LONGHORN
   
   LRESULT dwmHitResult;
   if (mCustomNonClient &&
@@ -4408,6 +4409,7 @@ PRBool nsWindow::ProcessMessage(UINT msg, WPARAM &wParam, LPARAM &lParam,
     *aRetValue = dwmHitResult;
     return PR_TRUE;
   }
+#endif 
 
   switch (msg) {
 #ifndef WINCE
