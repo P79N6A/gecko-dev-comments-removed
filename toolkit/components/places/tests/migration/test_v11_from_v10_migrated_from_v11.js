fe582608945f38b0c274e3c5a348ca929b8f0b6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:54:38 +0000

    Bug 607117 - create GUIDs for all bookmark and history items automatically Part 3 - Add a test for migration from a v10 database that was formally a v11 database back to a v11 database. r=mak
    UltraBlame original commit: b44a01549b605cffa8eab154b54a2972ed6c872c

---
+++ toolkit/components/places/tests/migration/test_v11_from_v10_migrated_from_v11.js
@@ -0,0 +1,157 @@
+
+
+
+
+
+
+
+
+
+
+
+
+function test_initial_state()
+{
+  
+  
+  let dbFile = gProfD.clone();
+  dbFile.append(kDBName);
+  let db = Services.storage.openUnsharedDatabase(dbFile);
+
+  let stmt = db.createStatement("PRAGMA journal_mode");
+  do_check_true(stmt.executeStep());
+  
+  
+  do_check_neq(stmt.getString(0).toLowerCase(), "wal");
+  stmt.finalize();
+
+  do_check_true(db.indexExists("moz_bookmarks_guid_uniqueindex"));
+  do_check_true(db.indexExists("moz_places_guid_uniqueindex"));
+
+  
+  stmt = db.createStatement(
+    "SELECT COUNT(1) "
+  + "FROM moz_bookmarks "
+  + "WHERE guid IS NULL "
+  );
+  do_check_true(stmt.executeStep());
+  do_check_neq(stmt.getInt32(0), 0);
+  stmt.finalize();
+
+  
+  stmt = db.createStatement(
+    "SELECT COUNT(1) "
+  + "FROM moz_places "
+  + "WHERE guid IS NULL "
+  );
+  do_check_true(stmt.executeStep());
+  do_check_neq(stmt.getInt32(0), 0);
+  stmt.finalize();
+
+  
+  do_check_eq(db.schemaVersion, 10);
+
+  db.close();
+  run_next_test();
+}
+
+function test_bookmark_guids_non_null()
+{
+  
+  
+  let stmt = DBConn().createStatement(
+    "SELECT COUNT(1) "
+  + "FROM moz_bookmarks "
+  );
+  do_check_true(stmt.executeStep());
+  do_check_neq(stmt.getInt32(0), 0);
+  stmt.finalize();
+
+  
+  stmt = DBConn().createStatement(
+    "SELECT guid "
+  + "FROM moz_bookmarks "
+  + "WHERE guid IS NULL "
+  );
+  do_check_false(stmt.executeStep());
+  stmt.finalize();
+  run_next_test();
+}
+
+function test_place_guids_non_null()
+{
+  
+  
+  let stmt = DBConn().createStatement(
+    "SELECT COUNT(1) "
+  + "FROM moz_places "
+  );
+  do_check_true(stmt.executeStep());
+  do_check_neq(stmt.getInt32(0), 0);
+  stmt.finalize();
+
+  
+  stmt = DBConn().createStatement(
+    "SELECT guid "
+  + "FROM moz_places "
+  + "WHERE guid IS NULL "
+  );
+  do_check_false(stmt.executeStep());
+  stmt.finalize();
+  run_next_test();
+}
+
+function test_final_state()
+{
+  
+  
+  let dbFile = gProfD.clone();
+  dbFile.append(kDBName);
+  let db = Services.storage.openUnsharedDatabase(dbFile);
+
+  do_check_eq(db.schemaVersion, 11);
+
+  db.close();
+  run_next_test();
+}
+
+
+
+
+let tests = [
+  test_initial_state,
+  test_bookmark_guids_non_null,
+  test_place_guids_non_null,
+  test_final_state,
+];
+let index = 0;
+
+function run_next_test()
+{
+  function _run_next_test() {
+    if (index < tests.length) {
+      do_test_pending();
+      print("TEST-INFO | " + _TEST_FILE + " | Starting " + tests[index].name);
+
+      
+      try {
+        tests[index++]();
+      }
+      catch (e) {
+        do_throw(e);
+      }
+    }
+
+    do_test_finished();
+  }
+
+  
+  do_execute_soon(_run_next_test);
+}
+
+function run_test()
+{
+  setPlacesDatabase("places_v10_from_v11.sqlite");
+  do_test_pending();
+  run_next_test();
+}
