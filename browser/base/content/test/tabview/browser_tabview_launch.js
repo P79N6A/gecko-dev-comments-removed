8e07906402e6f50f4b2bb1826fe99aad04314590
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:52:54 +0000

    Bug 594909 - Intermittent failure in browser/base/content/test/tabview/browser_tabview_launch.js | Tab View is visible r=dietrich, a=test
    UltraBlame original commit: 7561c0da2362d3c812701bd0dfcc003fea36d9f5

--- browser/base/content/test/tabview/browser_tabview_launch.js
+++ browser/base/content/test/tabview/browser_tabview_launch.js
@@ -35,35 +35,54 @@
 
 
 
+let tabViewShownCount = 0;
+
+
 function test() {
   waitForExplicitFinish();
   
-  let tabViewShownCount = 0;
-  let onTabViewHidden = function() {
-    ok(!TabView.isVisible(), "Tab View is hidden");
-
-    if (tabViewShownCount == 1) {
-      document.getElementById("menu_tabview").doCommand();
-    } else if (tabViewShownCount == 2) {
-      EventUtils.synthesizeKey("e", { accelKey: true });
-    } else if (tabViewShownCount == 3) {
-      window.removeEventListener("tabviewshown", onTabViewShown, false);
-      window.removeEventListener("tabviewhidden", onTabViewHidden, false);
-      finish();
-    }
-  }
-  let onTabViewShown = function() {
-    
-    ok(TabView.isVisible(), "Tab View is visible. Count: " + tabViewShownCount);
-    tabViewShownCount++
-    executeSoon(function() { TabView.toggle(); });
-  }
-  window.addEventListener("tabviewshown", onTabViewShown, false);
-  window.addEventListener("tabviewhidden", onTabViewHidden, false);
   
-  ok(!TabView.isVisible(), "Tab View is hidden");
+  ok(!TabView.isVisible(), "Tab View starts hidden");
 
+  
+  window.addEventListener("tabviewshown", onTabViewLoadedAndShown, false);
   let button = document.getElementById("tabview-button");
   ok(button, "Tab View button exists");
   button.doCommand();
+}
+
+
+function onTabViewLoadedAndShown() {
+  window.removeEventListener("tabviewshown", onTabViewLoadedAndShown, false);
+  
+  ok(TabView.isVisible(), "Tab View is visible. Count: " + tabViewShownCount);
+  tabViewShownCount++;
+  
+  
+  window.addEventListener("tabviewshown", onTabViewShown, false);
+  window.addEventListener("tabviewhidden", onTabViewHidden, false);
+  TabView.toggle();
+}
+
+
+function onTabViewShown() {
+  
+  ok(TabView.isVisible(), "Tab View is visible. Count: " + tabViewShownCount);
+  tabViewShownCount++;
+  TabView.toggle();
+}
+
+
+function onTabViewHidden() {
+  ok(!TabView.isVisible(), "Tab View is hidden. Count: " + tabViewShownCount);
+
+  if (tabViewShownCount == 1) {
+    document.getElementById("menu_tabview").doCommand();
+  } else if (tabViewShownCount == 2) {
+    EventUtils.synthesizeKey("e", { accelKey: true });
+  } else if (tabViewShownCount == 3) {
+    window.removeEventListener("tabviewshown", onTabViewShown, false);
+    window.removeEventListener("tabviewhidden", onTabViewHidden, false);
+    finish();
+  }
 }
