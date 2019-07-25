f2d8f11c191d28a0a5bb6426a727dd96041741fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:18:26 +0000

    Bug 695100 - Code cleanup for GeckoBookmarks (r=lucasr)
    Add license text, remove unneeded fields and methods, add missing @Override
    tags, fix indentation, remove trailing spaces, expand wildcard imports and sort
    them, pull out magic constant strings. No functional changes.
    
    UltraBlame original commit: f8179ee6846a4099516f189623157307bc892c17

--- embedding/android/GeckoBookmarks.java
+++ embedding/android/GeckoBookmarks.java
@@ -1,66 +1,97 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package org.mozilla.gecko;
-import android.app.*;
-import android.os.*;
-import android.provider.*;
-import android.content.*;
-import android.widget.*;
-import android.provider.*;
-import android.database.*;
-import android.view.*;
-import android.util.*;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Browser;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class GeckoBookmarks extends ListActivity {
+    private static final String LOG_NAME = "GeckoBookmarks";
+    private static final String TITLE_KEY = "title";
 
-  static String[] sFromColumns = {Browser.BookmarkColumns.URL};
-  static int[] sToColumns = {0};
-  Cursor mCursor = null;
+    private Cursor mCursor;
+    private Uri mUri;
+    private String mTitle;
 
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.bookmarks);
-    mCursor = managedQuery(android.provider.Browser.BOOKMARKS_URI,
-        		null, null, null, null); 
-    startManagingCursor(mCursor);
-    
-    ListAdapter adapter = 
-      new SimpleCursorAdapter(this, R.layout.bookmark_list_row, mCursor, 
-			      new String[] {Browser.BookmarkColumns.TITLE, 
-					    Browser.BookmarkColumns.URL},
-			      new int[] {R.id.bookmark_title, R.id.bookmark_url}); 
-    setListAdapter(adapter);
-  }
- 
-  protected void onResume() {
-    super.onResume();
-  }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bookmarks);
+        mCursor = managedQuery(Browser.BOOKMARKS_URI,
+            		null, null, null, null);
+        startManagingCursor(mCursor);
 
-  protected void onPause() {
-    super.onPause();
-  }
+        ListAdapter adapter =
+          new SimpleCursorAdapter(this, R.layout.bookmark_list_row, mCursor,
+        		      new String[] {Browser.BookmarkColumns.TITLE,
+        				    Browser.BookmarkColumns.URL},
+        		      new int[] {R.id.bookmark_title, R.id.bookmark_url});
+        setListAdapter(adapter);
+    }
 
-  protected void onListItemClick(ListView l, View v, int position, long id) {
-    mCursor.moveToPosition(position);
-    String spec = mCursor.getString(mCursor.getColumnIndex(Browser.BookmarkColumns.URL));
-    Log.i("GeckoBookmark", "clicked: " + spec);
-    GeckoApp app = org.mozilla.gecko.GeckoApp.mAppContext;
-    Intent intent = new Intent(this, app.getClass());
-    intent.setAction(Intent.ACTION_VIEW);
-    intent.setData(android.net.Uri.parse(spec));
-    startActivity(intent);
-  }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        mCursor.moveToPosition(position);
+        String spec = mCursor.getString(mCursor.getColumnIndex(Browser.BookmarkColumns.URL));
+        Log.i(LOG_NAME, "clicked: " + spec);
+        Intent intent = new Intent(this, GeckoApp.class);
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(spec));
+        startActivity(intent);
+    }
 
-  public void addBookmark(View v) {
-    Browser.saveBookmark(this, mTitle, mUri.toString());
-    finish();
-  }
+    public void addBookmark(View v) {
+        Browser.saveBookmark(this, mTitle, mUri.toString());
+        finish();
+    }
 
-  @Override
-  protected void onNewIntent(Intent intent) {
-    
-    mUri = intent.getData();
-    mTitle = intent.getStringExtra("title");
-  }
-
-  android.net.Uri mUri;
-  String mTitle;
+    @Override
+    protected void onNewIntent(Intent intent) {
+      
+      mUri = intent.getData();
+      mTitle = intent.getStringExtra(TITLE_KEY);
+    }
 }
