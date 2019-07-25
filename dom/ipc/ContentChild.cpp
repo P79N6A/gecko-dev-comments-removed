65352db8265f585482f713d1b19168f2ada50435
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:42:57 +0000

    Bug 777072 - 2/7 - AddInternal() and CommonTestPermission() should use nsIPrincipal. r=sicking
    UltraBlame original commit: 0e1c51a8387d2298be9a4202301d406c80f22759

--- dom/ipc/ContentChild.cpp
+++ dom/ipc/ContentChild.cpp
@@ -858,9 +858,7 @@ ContentChild::RecvAddPermission(const IPC::Permission& permission)
   MOZ_ASSERT(secMan);
 
   nsCOMPtr<nsIPrincipal> principal;
-  nsresult rv = secMan->GetAppCodebasePrincipal(uri, permission.appId,
-                                                permission.isInBrowserElement,
-                                                getter_AddRefs(principal));
+  nsresult rv = secMan->GetNoAppCodebasePrincipal(uri, getter_AddRefs(principal));
   NS_ENSURE_SUCCESS(rv, true);
 
   permissionManager->AddInternal(principal,
