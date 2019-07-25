4c47196640c185008f45a61a81951aed68a327e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:53:17 +0000

    Bug 590563 - Manually restored tabs are not shown in the main browser view [r=paul, a=dietrich]
    UltraBlame original commit: 79266a9bfbeb20fad38c272af9f812c7dada219e

--- browser/components/sessionstore/content/aboutSessionRestore.js
+++ browser/components/sessionstore/content/aboutSessionRestore.js
@@ -251,6 +251,8 @@ function restoreSingleTab(aIx, aShifted) {
   var ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
   var tabState = gStateObject.windows[item.parent.ix]
                              .tabs[aIx - gTreeData.indexOf(item.parent) - 1];
+  
+  tabState.hidden = false;
   ss.setTabState(newTab, JSON.stringify(tabState));
   
   
