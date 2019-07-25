a9ead98675fd119994b2f592d797a2cdfdd67508
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:29:42 +0000

    Bug 644707 - Avoid calling system abort(), generate useful stack traces through a manual segfault instead. r=ted
    UltraBlame original commit: f986c9b02c1dc5d5200e7c691c2f7746de2a58fb

--- memory/mozalloc/mozalloc_abort.cpp
+++ memory/mozalloc/mozalloc_abort.cpp
@@ -47,6 +47,7 @@
 #  include <windows.h>          
 #elif defined(XP_UNIX)
 #  include <unistd.h>           
+#  include <signal.h>
 #endif
 
 #if defined(XP_WIN) || defined(XP_OS2)
@@ -72,19 +73,39 @@ mozalloc_abort(const char* const msg)
     fputs(msg, stderr);
     fputs("\n", stderr);
 
-#if defined(XP_UNIX) && !defined(XP_MACOSX)
-    abort();
-#elif defined(_MSC_VER)
+#if defined(_MSC_VER)
     __debugbreak();
 #elif defined(XP_WIN)
     DebugBreak();
 #endif
+
+    
+    
     
     
 
-    
     TouchBadMemory();
+
+    
+    
+    
+    
+    
+#if defined(XP_UNIX) && !defined(XP_MACOSX)
+    raise(SIGABRT);
+#endif
 
     
     _exit(127);
 }
+
+#if defined(XP_UNIX)
+
+
+
+void abort(void)
+{
+  mozalloc_abort("Redirecting call to abort() to mozalloc_abort\n");
+}
+#endif
+
