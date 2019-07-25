82e18c5789d87b247a8dc2338bfb039b4c275e02
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:06:49 +0000

    No bug, add header comments to Memory.h, r=none
    UltraBlame original commit: 1ed9525b2a9f96c755791cccf50ea7e3884c0867

--- js/src/gc/Memory.h
+++ js/src/gc/Memory.h
@@ -16,14 +16,19 @@ namespace gc {
 
 
 
-
-
 void InitMemorySubsystem();
+
 
 void *MapAlignedPages(size_t size, size_t alignment);
 void UnmapPages(void *p, size_t size);
 
+
+
 bool MarkPagesUnused(void *p, size_t size);
+
+
+
+
 bool MarkPagesInUse(void *p, size_t size);
 
 } 
