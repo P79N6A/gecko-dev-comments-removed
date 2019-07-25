a5a4740faa93ad37fb7205b24b320318963b5fa1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:49:11 +0000

    Bug 554300 - "close tab" context menu item should be enabled for last tab when browser.tabs.closeWindowWithLastTab=false. r=dao
    UltraBlame original commit: 9632cff74e118e8a7ab8bdb6df76f8b87ca0ef61

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -7746,6 +7746,11 @@ var TabContextMenu = {
     this.contextTab = document.popupNode.localName == "tab" ?
                       document.popupNode : gBrowser.selectedTab;
     var disabled = gBrowser.tabs.length == 1;
+
+    
+    document.getElementById("context_closeTab").disabled =
+      disabled && gBrowser.tabContainer._closeWindowWithLastTab;
+
     var menuItems = aPopupMenu.getElementsByAttribute("tbattr", "tabbrowser-multiple");
     for (var i = 0; i < menuItems.length; i++)
       menuItems[i].disabled = disabled;
