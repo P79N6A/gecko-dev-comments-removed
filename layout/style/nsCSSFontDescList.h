0e19403e8ba26c813b1889262c2145579834cd67
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:28:43 +0000

    Bug 712299 - don't require manual syncing of font data; r=bz
    UltraBlame original commit: e4fe4d48518fd965c7fcd48142c54ace1d396d0e

---
+++ layout/style/nsCSSFontDescList.h
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CSS_FONT_DESC(font-family, Family)
+CSS_FONT_DESC(font-style, Style)
+CSS_FONT_DESC(font-weight, Weight)
+CSS_FONT_DESC(font-stretch, Stretch)
+CSS_FONT_DESC(src, Src)
+CSS_FONT_DESC(unicode-range, UnicodeRange)
+CSS_FONT_DESC(-moz-font-feature-settings, FontFeatureSettings)
+CSS_FONT_DESC(-moz-font-language-override, FontLanguageOverride)
