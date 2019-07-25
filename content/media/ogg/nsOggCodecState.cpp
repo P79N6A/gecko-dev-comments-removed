8a4b016a15973f8e9ab2e55da496230dfc245d6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:34:37 +0000

    Bug 666132: Account for trailing data in the last frame of audio data in ogg backend debug code - r=cpearce
    UltraBlame original commit: e8fe55848089a88bd026842e1fec0917ee95a16c

--- content/media/ogg/nsOggCodecState.cpp
+++ content/media/ogg/nsOggCodecState.cpp
@@ -678,6 +678,12 @@ nsresult nsVorbisState::ReconstructVorbisGranulepos()
     if (packet->granulepos == -1) {
       packet->granulepos = mGranulepos + samples;
     }
+
+    
+    if (packet->e_o_s && packet->granulepos >= mGranulepos) {
+       samples = packet->granulepos - mGranulepos;
+    }
+ 
     mGranulepos = packet->granulepos;
     RecordVorbisPacketSamples(packet, samples);
     return NS_OK;
