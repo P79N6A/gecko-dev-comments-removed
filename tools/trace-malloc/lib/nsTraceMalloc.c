d40b6224fdc6140b4cc75d68cdd0124c6a4f61f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:06:04 +0000

    Bug 696281 - disable trace malloc stacks on OS X too. r=dbaron.
    This patch disables trace malloc stacks on OS X too. To make this work,
    we still have to look on the stack to decide if we must set immediate_abort,
    but we can avoid other work like decoding the addresses.
    
    UltraBlame original commit: 93e293c8758056784d259e651f7128d8d796cc24

--- tools/trace-malloc/lib/nsTraceMalloc.c
+++ tools/trace-malloc/lib/nsTraceMalloc.c
@@ -957,6 +957,18 @@ backtrace(tm_thread *t, int skip, int *immediate_abort)
     t->suppress_tracing++;
 
     if (!stacks_enabled) {
+#if defined(XP_MACOSX)
+        
+
+        info->entries = 0;
+        rv = NS_StackWalk(stack_callback, skip, info);
+        *immediate_abort = rv == NS_ERROR_UNEXPECTED;
+        if (rv == NS_ERROR_UNEXPECTED || info->entries == 0) {
+            t->suppress_tracing--;
+            return NULL;
+        }
+#endif
+
         
 
 
