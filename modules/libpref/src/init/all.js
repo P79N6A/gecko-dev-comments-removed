63685cbac315d3429181f42010f9501bffd78c73
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:09:23 +0000

    Bug 647403 - Add a pref for CreateFixupURI to use UTF-8; r=bz
    UltraBlame original commit: 0246515e5fdae93b31663d4862cc38b5275f0a14

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -319,6 +319,7 @@ pref("browser.fixup.alternate.enabled", true);
 pref("browser.fixup.alternate.prefix", "www.");
 pref("browser.fixup.alternate.suffix", ".com");
 pref("browser.fixup.hide_user_pass", true);
+pref("browser.fixup.use-utf8", false);
 
 
 pref("browser.urlbar.autocomplete.enabled", true);
@@ -988,10 +989,6 @@ pref("network.standard-url.escape-utf8", true);
 
 
 pref("network.standard-url.encode-utf8", true);
-
-
-
-pref("network.standard-url.encode-query-utf8", false);
 
 
 pref("network.ftp.idleConnectionTimeout", 300);
