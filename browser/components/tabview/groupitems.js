a828008fee10c8a46245dac162ce5d4898ed40e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:15:14 +0000

    Bug 706430 - Restoring a session without Panorama data fails with Panorama already loaded; r=dietrich
    UltraBlame original commit: 020c1f61f9d44401c6a6b96d876b8361bfd5ded3

--- browser/components/tabview/groupitems.js
+++ browser/components/tabview/groupitems.js
@@ -2236,27 +2236,29 @@ let GroupItems = {
         toClose.forEach(function(groupItem) {
           
           
-          
-          let children = groupItem.getChildren().concat();
-
-          children.forEach(function (tabItem) {
-            if (tabItem.parent && tabItem.parent.hidden)
+          groupItem.getChildren().forEach(function (tabItem) {
+            if (tabItem.parent.hidden)
               iQ(tabItem.container).show();
+
+            tabItem._reconnected = false;
 
             
             let tabData = Storage.getTabData(tabItem.tab);
-            let parentGroup = GroupItems.groupItem(tabData.groupID);
 
-            
-            if (!parentGroup || -1 < toClose.indexOf(parentGroup)) {
-              tabData.groupID = activeGroupId || Object.keys(groupItemData)[0];
-              Storage.saveTab(tabItem.tab, tabData);
+            if (tabData) {
+              let parentGroup = GroupItems.groupItem(tabData.groupID);
+
+              
+              
+              
+              if (!parentGroup || -1 < toClose.indexOf(parentGroup)) {
+                tabData.groupID = activeGroupId || Object.keys(groupItemData)[0];
+                Storage.saveTab(tabItem.tab, tabData);
+              }
             }
-
-            tabItem._reconnected = false;
-            tabItem._reconnect();
           });
 
+          
           groupItem.close({immediately: true});
         });
       }
