704a6cc737e8774bfd219d44b3b026c3cc54ece4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:19:18 +0000

    Bug 564270 - Can not search Places by transition type
    UltraBlame original commit: bb28be604c9565662b0f1efbaa4c389ee9a25303

---
+++ toolkit/components/places/tests/queries/test_transitions.js
@@ -0,0 +1,177 @@
+
+
+
+
+
+
+var beginTime = Date.now();
+var testData = [
+  {
+    isVisit: true,
+    title: "page 0",
+    uri: "http://mozilla.com/",
+    transType: Ci.nsINavHistoryService.TRANSITION_TYPED
+  },
+  {
+    isVisit: true,
+    title: "page 1",
+    uri: "http://google.com/",
+    transType: Ci.nsINavHistoryService.TRANSITION_DOWNLOAD
+  },
+  {
+    isVisit: true,
+    title: "page 2",
+    uri: "http://microsoft.com/",
+    transType: Ci.nsINavHistoryService.TRANSITION_DOWNLOAD
+  },
+  {
+    isVisit: true,
+    title: "page 3",
+    uri: "http://en.wikipedia.org/",
+    transType: Ci.nsINavHistoryService.TRANSITION_BOOKMARK
+  },
+  {
+    isVisit: true,
+    title: "page 4",
+    uri: "http://fr.wikipedia.org/",
+    transType: Ci.nsINavHistoryService.TRANSITION_DOWNLOAD
+  },
+  {
+    isVisit: true,
+    title: "page 5",
+    uri: "http://apple.com/",
+    transType: Ci.nsINavHistoryService.TRANSITION_TYPED
+  },
+  {
+    isVisit: true,
+    title: "page 6",
+    uri: "http://campus-bike-store.com/",
+    transType: Ci.nsINavHistoryService.TRANSITION_DOWNLOAD
+  },
+  {
+    isVisit: true,
+    title: "page 7",
+    uri: "http://uwaterloo.ca/",
+    transType: Ci.nsINavHistoryService.TRANSITION_TYPED
+  },
+  {
+    isVisit: true,
+    title: "page 8",
+    uri: "http://pugcleaner.com/",
+    transType: Ci.nsINavHistoryService.TRANSITION_BOOKMARK
+  },
+  {
+    isVisit: true,
+    title: "page 9",
+    uri: "http://de.wikipedia.org/",
+    transType: Ci.nsINavHistoryService.TRANSITION_TYPED
+  },
+  {
+    isVisit: true,
+    title: "arewefastyet",
+    uri: "http://arewefastyet.com/",
+    transType: Ci.nsINavHistoryService.TRANSITION_DOWNLOAD
+  },
+  {
+    isVisit: true,
+    title: "arewefastyet",
+    uri: "http://arewefastyet.com/",
+    transType: Ci.nsINavHistoryService.TRANSITION_BOOKMARK
+  }];
+
+var testDataTyped = [0, 5, 7, 9];
+var testDataDownload = [1, 2, 4, 6, 10]; 
+var testDataBookmark = [3, 8, 11];
+
+
+
+
+
+
+function run_test() {
+
+  let timeNow = Date.now();
+  for each (let item in testData) {
+    PlacesUtils.history.addVisit(uri(item.uri), timeNow++ * 1000, null,
+      item.transType, false, 0);
+  }
+
+  for (let i in testData) {
+    testData[i].title = null;
+  }
+
+  dump_table("moz_places");
+  dump_table("moz_places_temp");
+  dump_table("moz_historyvisits");
+  dump_table("moz_historyvisits_temp");
+
+  var numSortFunc = function (a,b) { return (a - b); };
+  var arrs = testDataTyped.concat(testDataDownload).concat(testDataBookmark)
+              .sort(numSortFunc);
+
+  
+  var data = arrs.filter(function (index) {
+      return (testData[index].uri.match(/arewefastyet\.com/) &&
+              testData[index].transType ==
+                Ci.nsINavHistoryService.TRANSITION_DOWNLOAD);
+    });
+
+  compareQueryToTestData("place:domain=arewefastyet.com&transition=" +
+                         Ci.nsINavHistoryService.TRANSITION_DOWNLOAD,
+                         data.slice());
+
+  compareQueryToTestData("place:transition=" +
+                         Ci.nsINavHistoryService.TRANSITION_DOWNLOAD,
+                         testDataDownload.slice());
+
+  compareQueryToTestData("place:transition=" +
+                         Ci.nsINavHistoryService.TRANSITION_TYPED,
+                         testDataTyped.slice());
+
+  compareQueryToTestData("place:transition=" +
+                         Ci.nsINavHistoryService.TRANSITION_DOWNLOAD +
+                         "&transition=" +
+                         Ci.nsINavHistoryService.TRANSITION_BOOKMARK,
+                         data);
+
+  
+  var query = {};
+  var options = {};
+  PlacesUtils.history.
+    queryStringToQueries("place:transition=" +
+                         Ci.nsINavHistoryService.TRANSITION_DOWNLOAD,
+                         query, {}, options);
+  query = (query.value)[0];
+  options = PlacesUtils.history.getNewQueryOptions();
+  var result = PlacesUtils.history.executeQuery(query, options);
+  var root = result.root;
+  root.containerOpen = true;
+  do_check_eq(testDataDownload.length, root.childCount);
+  PlacesUtils.history
+      .addVisit(PlacesUtils._uri("http://getfirefox.com"),
+                Date.now() * 1000, null,
+                Ci.nsINavHistoryService.TRANSITION_DOWNLOAD, false, 0);
+  do_check_eq(testDataDownload.length + 1, root.childCount);
+  root.containerOpen = false;
+
+  PlacesUtils.bhistory.removeAllPages();
+}
+
+
+
+
+
+function compareQueryToTestData(queryStr, data) {
+  var query = {};
+  var options = {};
+  PlacesUtils.history.queryStringToQueries(queryStr, query, {}, options);
+  query = query.value[0];
+  options = options.value;
+  var result = PlacesUtils.history.executeQuery(query, options);
+  var root = result.root;
+  for (var i = 0; i < data.length; i++) {
+    data[i] = testData[data[i]];
+    data[i].isInQuery = true;
+  }
+  compareArrayToResult(data, root);
+}
