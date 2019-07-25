b902e20fd5f98708688f63765d196073e1aeb373
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:51:17 +0000

    Bug 672515 - remove nsIAccessible getAccessibleToRight/Left/Above/Below; r=trev.saunders
    UltraBlame original commit: b38795f5d13ba43408eaaecdae7a80d8ddff6b24

--- accessible/src/base/nsAccessible.cpp
+++ accessible/src/base/nsAccessible.cpp
@@ -1974,30 +1974,6 @@ NS_IMETHODIMP nsAccessible::GetHelp(nsAString& _retval)
   return NS_ERROR_NOT_IMPLEMENTED;
 }
 
-
-NS_IMETHODIMP nsAccessible::GetAccessibleToRight(nsIAccessible **_retval)
-{
-  return NS_ERROR_NOT_IMPLEMENTED;
-}
-
-
-NS_IMETHODIMP nsAccessible::GetAccessibleToLeft(nsIAccessible **_retval)
-{
-  return NS_ERROR_NOT_IMPLEMENTED;
-}
-
-
-NS_IMETHODIMP nsAccessible::GetAccessibleAbove(nsIAccessible **_retval)
-{
-  return NS_ERROR_NOT_IMPLEMENTED;
-}
-
-
-NS_IMETHODIMP nsAccessible::GetAccessibleBelow(nsIAccessible **_retval)
-{
-  return NS_ERROR_NOT_IMPLEMENTED;
-}
-
 nsIContent*
 nsAccessible::GetAtomicRegion() const
 {
