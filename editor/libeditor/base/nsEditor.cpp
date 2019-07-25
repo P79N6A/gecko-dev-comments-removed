562441da0cad46110af657d5e7e9a6fd1ee5f0e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:17:43 +0000

    Bug 581576 hung up or too slow when press Enter key on Gmail editor which has a lot of misspelled words r=ehsan, a=dbaron
    UltraBlame original commit: f05b59f1ff3da7be6d09bb8b9945377d521be333

--- editor/libeditor/base/nsEditor.cpp
+++ editor/libeditor/base/nsEditor.cpp
@@ -368,9 +368,7 @@ nsEditor::GetDesiredSpellCheckState()
     return PR_FALSE;                    
   }
 
-  
-  
-  if (IsPasswordEditor() || IsReadonly() || IsDisabled()) {
+  if (CanEnableSpellCheck()) {
     return PR_FALSE;
   }
 
@@ -443,6 +441,12 @@ nsEditor::GetFlags(PRUint32 *aFlags)
 NS_IMETHODIMP
 nsEditor::SetFlags(PRUint32 aFlags)
 {
+  
+  
+  
+  
+
+  PRBool spellcheckerWasEnabled = CanEnableSpellCheck();
   mFlags = aFlags;
 
   if (!mDocWeak || !mPresShellWeak) {
@@ -453,8 +457,10 @@ nsEditor::SetFlags(PRUint32 aFlags)
   }
 
   
-  nsresult rv = SyncRealTimeSpell();
-  NS_ENSURE_SUCCESS(rv, rv);
+  if (CanEnableSpellCheck() != spellcheckerWasEnabled) {
+    nsresult rv = SyncRealTimeSpell();
+    NS_ENSURE_SUCCESS(rv, rv);
+  }
 
   
   
@@ -462,7 +468,7 @@ nsEditor::SetFlags(PRUint32 aFlags)
     
     
     PRUint32 newState = nsIContent::IME_STATUS_ENABLE;
-    rv = GetPreferredIMEState(&newState);
+    nsresult rv = GetPreferredIMEState(&newState);
     if (NS_SUCCEEDED(rv)) {
       
       
