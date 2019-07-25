ad78308ecedef4c3313e6d09ffcb7c458089b02b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:19:15 +0000

    Bug 697144 - Unable to interact with aboutCertError [r=mbrubeck]
    UltraBlame original commit: 9fe52ea2201f3fbfe5ecba1a1512ba2fe68e0232

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -813,6 +813,19 @@ var BrowserEventHandler = {
             title: browser.contentTitle
           }
         });
+
+        
+        
+        
+        
+        if (/^about:/.test(aEvent.originalTarget.documentURI)) {
+          let browser = BrowserApp.getBrowserForDocument(aEvent.originalTarget);
+          browser.addEventListener("click", ErrorPageEventHandler, false);
+          browser.addEventListener("pagehide", function () {
+            browser.removeEventListener("click", ErrorPageEventHandler, false);
+            browser.removeEventListener("pagehide", arguments.callee, true);
+          }, true);
+        }
         break;
       }
 
@@ -1255,6 +1268,63 @@ var BrowserEventHandler = {
     }
 
     return scrollX || scrollY;
+  }
+};
+
+
+var ErrorPageEventHandler = {
+  handleEvent: function(aEvent) {
+    switch (aEvent.type) {
+      case "click": {
+        
+        if (!aEvent.isTrusted)
+          return;
+
+        let target = aEvent.originalTarget;
+        let errorDoc = target.ownerDocument;
+
+        
+        
+        if (/^about:certerror\?e=nssBadCert/.test(errorDoc.documentURI)) {
+          let perm = errorDoc.getElementById("permanentExceptionButton");
+          let temp = errorDoc.getElementById("temporaryExceptionButton");
+          if (target == temp || target == perm) {
+            
+            try {
+              
+              let uri = Services.io.newURI(errorDoc.location.href, null, null);
+              let sslExceptions = new SSLExceptions();
+      
+              if (target == perm)
+                sslExceptions.addPermanentException(uri);
+              else
+                sslExceptions.addTemporaryException(uri);
+            } catch (e) {
+              dump("Failed to set cert exception: " + e + "\n");
+            }
+            errorDoc.location.reload();
+          } else if (target == errorDoc.getElementById("getMeOutOfHereButton")) {
+            errorDoc.location = this.getFallbackSafeURL();
+          }
+        }
+        break;
+      }
+    }
+  },
+
+  getFallbackSafeURL: function getFallbackSafeURL() {
+    
+    let prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getDefaultBranch(null);
+    let url = "about:home";
+    try {
+      url = prefs.getComplexValue("browser.startup.homepage", Ci.nsIPrefLocalizedString).data;
+      
+      if (url.indexOf("|") != -1)
+        url = url.split("|")[0];
+    } catch(e) {
+      Cu.reportError("Couldn't get homepage pref: " + e);
+    }
+    return url;
   }
 };
 
