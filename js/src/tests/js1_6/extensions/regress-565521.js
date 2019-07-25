71a743f43084deeca0970c459e21b468bf659280
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:01:15 +0000

    Bug 565521 - Add a test. r=sayrer
    UltraBlame original commit: 325ab3fd68cdb0dce003d7907c003dd26584c73d

---
+++ js/src/tests/js1_6/extensions/regress-565521.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+var gTestfile = 'regress-565521.js';
+
+
+var BUGNUMBER = 565521;
+var summary = 'for-each getter calling';
+var actual = '';
+var expect = 'PASS';
+
+
+
+test();
+
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+
+  var proto = { get prop() { return (this === proto) ? "FAIL" : "PASS"; } };
+  var obj = Object.create(proto);
+
+  var itercount = 0;
+  for each (var i in obj) {
+      ++itercount;
+      actual = i;
+  }
+
+  reportCompare(itercount, 1, summary + ': right itercount')
+  reportCompare(expect, actual, summary + ': right value');
+
+  exitFunc ('test');
+}
+
