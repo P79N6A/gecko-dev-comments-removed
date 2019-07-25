50da2269bd91a34d70353d2b08d5efdb7918e5db
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:24:37 +0000

    bug 596315 - animate opening and closing, test fix, a=orange
    UltraBlame original commit: 25df336746e4e8c7cae63288192c79e722e36543

--- toolkit/components/console/hudservice/tests/browser/browser_webconsole_bug_581231_close_button.js
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_bug_581231_close_button.js
@@ -30,15 +30,15 @@ function testCloseButton() {
     let closeButton = hudBox.querySelector(".jsterm-close-button");
     ok(closeButton != null, "we have the close button");
 
+    
 
-  
+    EventUtils.synthesizeMouse(closeButton, 0, 0, {});
 
-  EventUtils.synthesizeMouse(closeButton, 0, 0, {});
-
-  executeSoon(function (){
-    ok(!(hudId in HUDService.windowRegistry), "the console is closed when " +
-       "the close button is pressed");
-    closeButton = null;
-    finishTest();
+    executeSoon(function (){
+      ok(!(hudId in HUDService.windowRegistry), "the console is closed when " +
+         "the close button is pressed");
+      closeButton = null;
+      finishTest();
+    });
   });
 }
