35895eba99bac205a1e6eceafa60f221b9fdbd9c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:31:49 +0000

    Bug 587299 - Tighten scope when collecting data for pinned tabs only. r=dietrich
    UltraBlame original commit: c167c845d8e66dc7f58b4b48df7ef436390d0e54

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -1141,13 +1141,18 @@ SessionStoreService.prototype = {
 
 
 
-  _saveWindowHistory: function sss_saveWindowHistory(aWindow) {
+
+
+  _saveWindowHistory: function sss_saveWindowHistory(aWindow, aPinnedOnly) {
     var tabbrowser = aWindow.gBrowser;
     var tabs = tabbrowser.tabs;
     var tabsData = this._windows[aWindow.__SSi].tabs = [];
     
-    for (var i = 0; i < tabs.length; i++)
+    for (var i = 0; i < tabs.length; i++) {
+      if (aPinnedOnly && !tabs[i].pinned)
+        break;
       tabsData.push(this._collectTabData(tabs[i]));
+    }
     
     this._windows[aWindow.__SSi].selected = tabbrowser.mTabBox.selectedIndex + 1;
   },
@@ -1435,16 +1440,15 @@ SessionStoreService.prototype = {
 
   _updateTextAndScrollData: function sss_updateTextAndScrollData(aWindow) {
     var browsers = aWindow.gBrowser.browsers;
-    for (var i = 0; i < browsers.length; i++) {
+    this._windows[aWindow.__SSi].tabs.forEach(function (tabData, i) {
+      if (browsers[i].__SS_data &&
+          browsers[i].__SS_data._tabStillLoading)
+        return; 
       try {
-        var tabData = this._windows[aWindow.__SSi].tabs[i];
-        if (browsers[i].__SS_data &&
-            browsers[i].__SS_data._tabStillLoading)
-          continue; 
         this._updateTextAndScrollDataForTab(aWindow, browsers[i], tabData);
       }
       catch (ex) { debug(ex); } 
-    }
+    }, this);
   },
 
   
@@ -1769,7 +1773,7 @@ SessionStoreService.prototype = {
         if (!this._isWindowLoaded(aWindow)) 
           return;
         if (aUpdateAll || this._dirtyWindows[aWindow.__SSi] || aWindow == activeWindow) {
-          this._collectWindowData(aWindow);
+          this._collectWindowData(aWindow, aPinnedOnly);
         }
         else { 
           this._updateWindowFeatures(aWindow);
@@ -1857,12 +1861,12 @@ SessionStoreService.prototype = {
     return { windows: total };
   },
 
-  _collectWindowData: function sss_collectWindowData(aWindow) {
+  _collectWindowData: function sss_collectWindowData(aWindow, aPinnedOnly) {
     if (!this._isWindowLoaded(aWindow))
       return;
     
     
-    this._saveWindowHistory(aWindow);
+    this._saveWindowHistory(aWindow, aPinnedOnly);
     this._updateTextAndScrollData(aWindow);
     this._updateCookieHosts(aWindow);
     this._updateWindowFeatures(aWindow);
