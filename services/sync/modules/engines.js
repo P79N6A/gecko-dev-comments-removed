f8fd4d1ff1e0c29552c1462b844414afbace6951
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:18:27 +0000

    Bug 609421 - Move tracker and store implementation into engines.js. r=rnewman
    UltraBlame original commit: cb0da1fdfe9e954312002d578e52a1b53ee56c53

--- services/sync/modules/engines.js
+++ services/sync/modules/engines.js
@@ -37,7 +37,9 @@
 
 
 
-const EXPORTED_SYMBOLS = ['Engines', 'Engine', 'SyncEngine'];
+
+const EXPORTED_SYMBOLS = ['Engines', 'Engine', 'SyncEngine',
+                          'Tracker', 'Store'];
 
 const Cc = Components.classes;
 const Ci = Components.interfaces;
@@ -53,11 +55,190 @@ Cu.import("resource://services-sync/ext/Sync.js");
 Cu.import("resource://services-sync/identity.js");
 Cu.import("resource://services-sync/log4moz.js");
 Cu.import("resource://services-sync/resource.js");
-Cu.import("resource://services-sync/stores.js");
-Cu.import("resource://services-sync/trackers.js");
 Cu.import("resource://services-sync/util.js");
 
 Cu.import("resource://services-sync/main.js");    
+
+
+
+
+
+
+
+
+
+
+
+
+function Tracker(name) {
+  name = name || "Unnamed";
+  this.name = this.file = name.toLowerCase();
+
+  this._log = Log4Moz.repository.getLogger("Tracker." + name);
+  let level = Svc.Prefs.get("log.logger.engine." + this.name, "Debug");
+  this._log.level = Log4Moz.Level[level];
+
+  this._score = 0;
+  this._ignored = [];
+  this.ignoreAll = false;
+  this.changedIDs = {};
+  this.loadChangedIDs();
+}
+Tracker.prototype = {
+  
+
+
+
+
+
+
+
+
+
+  get score() {
+    return this._score;
+  },
+
+  set score(value) {
+    this._score = value;
+    Observers.notify("weave:engine:score:updated", this.name);
+  },
+
+  
+  resetScore: function T_resetScore() {
+    this._score = 0;
+  },
+
+  saveChangedIDs: function T_saveChangedIDs() {
+    Utils.delay(function() {
+      Utils.jsonSave("changes/" + this.file, this, this.changedIDs);
+    }, 1000, this, "_lazySave");
+  },
+
+  loadChangedIDs: function T_loadChangedIDs() {
+    Utils.jsonLoad("changes/" + this.file, this, function(json) {
+      this.changedIDs = json;
+    });
+  },
+
+  
+  
+  
+
+  ignoreID: function T_ignoreID(id) {
+    this.unignoreID(id);
+    this._ignored.push(id);
+  },
+
+  unignoreID: function T_unignoreID(id) {
+    let index = this._ignored.indexOf(id);
+    if (index != -1)
+      this._ignored.splice(index, 1);
+  },
+
+  addChangedID: function addChangedID(id, when) {
+    if (!id) {
+      this._log.warn("Attempted to add undefined ID to tracker");
+      return false;
+    }
+    if (this.ignoreAll || (id in this._ignored))
+      return false;
+
+    
+    if (when == null)
+      when = Math.floor(Date.now() / 1000);
+
+    
+    if ((this.changedIDs[id] || -Infinity) < when) {
+      this._log.trace("Adding changed ID: " + [id, when]);
+      this.changedIDs[id] = when;
+      this.saveChangedIDs();
+    }
+    return true;
+  },
+
+  removeChangedID: function T_removeChangedID(id) {
+    if (!id) {
+      this._log.warn("Attempted to remove undefined ID to tracker");
+      return false;
+    }
+    if (this.ignoreAll || (id in this._ignored))
+      return false;
+    if (this.changedIDs[id] != null) {
+      this._log.trace("Removing changed ID " + id);
+      delete this.changedIDs[id];
+      this.saveChangedIDs();
+    }
+    return true;
+  },
+
+  clearChangedIDs: function T_clearChangedIDs() {
+    this._log.trace("Clearing changed ID list");
+    this.changedIDs = {};
+    this.saveChangedIDs();
+  }
+};
+
+
+
+
+
+
+
+
+function Store(name) {
+  name = name || "Unnamed";
+  this.name = name.toLowerCase();
+
+  this._log = Log4Moz.repository.getLogger("Store." + name);
+  let level = Svc.Prefs.get("log.logger.engine." + this.name, "Debug");
+  this._log.level = Log4Moz.Level[level];
+}
+Store.prototype = {
+  applyIncoming: function Store_applyIncoming(record) {
+    if (record.deleted)
+      this.remove(record);
+    else if (!this.itemExists(record.id))
+      this.create(record);
+    else
+      this.update(record);
+  },
+
+  
+
+  create: function Store_create(record) {
+    throw "override create in a subclass";
+  },
+
+  remove: function Store_remove(record) {
+    throw "override remove in a subclass";
+  },
+
+  update: function Store_update(record) {
+    throw "override update in a subclass";
+  },
+
+  itemExists: function Store_itemExists(id) {
+    throw "override itemExists in a subclass";
+  },
+
+  createRecord: function Store_createRecord(id, collection) {
+    throw "override createRecord in a subclass";
+  },
+
+  changeItemID: function Store_changeItemID(oldID, newID) {
+    throw "override changeItemID in a subclass";
+  },
+
+  getAllIDs: function Store_getAllIDs() {
+    throw "override getAllIDs in a subclass";
+  },
+
+  wipe: function Store_wipe() {
+    throw "override wipe in a subclass";
+  }
+};
+
 
 
 
