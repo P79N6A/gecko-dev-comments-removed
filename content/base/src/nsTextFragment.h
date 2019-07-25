6a83e5784599d2f930734305ef41f614d5bebe05
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:14:40 +0000

    Bug 582863 - Spell checker shouldn't copy the text node contents unnecessarily; r=roc a=blocking2.0+
    UltraBlame original commit: f8a0bd477fd380b7867ff4fcd63babdbe5e19e67

--- content/base/src/nsTextFragment.h
+++ content/base/src/nsTextFragment.h
@@ -44,7 +44,7 @@
 #ifndef nsTextFragment_h___
 #define nsTextFragment_h___
 
-#include "nsAString.h"
+#include "nsString.h"
 #include "nsTraceRefcnt.h"
 class nsString;
 class nsCString;
@@ -165,14 +165,27 @@ public:
   
 
 
-  void AppendTo(nsAString& aString) const;
+  void AppendTo(nsAString& aString) const {
+    if (mState.mIs2b) {
+      aString.Append(m2b, mState.mLength);
+    } else {
+      AppendASCIItoUTF16(Substring(m1b, m1b + mState.mLength),
+                         aString);
+    }
+  }
 
   
 
 
 
 
-  void AppendTo(nsAString& aString, PRInt32 aOffset, PRInt32 aLength) const;
+  void AppendTo(nsAString& aString, PRInt32 aOffset, PRInt32 aLength) const {
+    if (mState.mIs2b) {
+      aString.Append(m2b + aOffset, aLength);
+    } else {
+      AppendASCIItoUTF16(Substring(m1b + aOffset, m1b + aOffset + aLength), aString);
+    }
+  }
 
   
 
