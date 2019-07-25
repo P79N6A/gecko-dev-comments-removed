9049d7e285393abdf267a735e5004841228da92b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:51:00 +0000

    Bug 727310: Make sure XHR unpins itself if it never recieved a loadstart event. r=bent
    UltraBlame original commit: 3a90a3dedf6c3339ae8ecc74e07719a1ee38d9cc

--- dom/workers/XMLHttpRequestPrivate.cpp
+++ dom/workers/XMLHttpRequestPrivate.cpp
@@ -1893,6 +1893,12 @@ XMLHttpRequestPrivate::MaybeDispatchPrematureAbortEvents(JSContext* aCx)
     false, false, false, false, false
   };
 
+  
+  
+  if (!mProxy->mSeenLoadStart && mProxy->mWorkerPrivate) {
+    Unpin(aCx);
+  }
+
   if (mProxy->mSeenUploadLoadStart) {
     JSObject* target = mProxy->mXMLHttpRequestPrivate->GetUploadJSObject();
     NS_ASSERTION(target, "Must have a target!");
