663213587a3d7dd7a3fbe415569185ec01125a78
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:07:08 +0000

    Bug 750589 - Update default MobileConnection permissions after Gaia refactoring (#1277). r=fabrice a=b2g-only
    UltraBlame original commit: d624dbe9e0c6bec1c84a502d2dd3845d0fbe9e9a

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -435,7 +435,7 @@ pref("dom.sms.enabled", true);
 pref("dom.sms.whitelist", "file://,http://homescreen.gaiamobile.org,http://sms.gaiamobile.org");
 
 
-pref("dom.mobileconnection.whitelist", "http://homescreen.gaiamobile.org");
+pref("dom.mobileconnection.whitelist", "http://system.gaiamobile.org,http://homescreen.gaiamobile.org,http://dialer.gaiamobile.org");
 
 
 pref("dom.mozContacts.enabled", true);
