31d2bc669ed16de9dfc3cc2cb5f204e6ad4be2c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:13:06 +0000

    Bug 562008 - HTML5 section DOM elements should be of type HTMLElement; r=mrbkap,sicking,timeless,ehsan sr=jst
    UltraBlame original commit: 2dcce82f9d660b55f3570bbb3b4ef8d3b045ec58

---
+++ content/html/content/src/nsHTMLElement.cpp
@@ -0,0 +1,87 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsGenericHTMLElement.h"
+#include "nsIDOMHTMLElement.h"
+
+
+class nsHTMLElement : public nsGenericHTMLElement,
+                      public nsIDOMHTMLElement
+{
+public:
+  nsHTMLElement(nsINodeInfo *aNodeInfo);
+  virtual ~nsHTMLElement();
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  NS_FORWARD_NSIDOMNODE(nsGenericHTMLElement::)
+
+  
+  NS_FORWARD_NSIDOMELEMENT(nsGenericHTMLElement::)
+
+  
+  NS_FORWARD_NSIDOMHTMLELEMENT(nsGenericHTMLElement::)
+
+  nsresult Clone(nsINodeInfo* aNodeInfo, nsINode** aResult) const;
+};
+
+NS_IMPL_NS_NEW_HTML_ELEMENT() 
+
+nsHTMLElement::nsHTMLElement(nsINodeInfo* aNodeInfo)
+  : nsGenericHTMLElement(aNodeInfo)
+{
+}
+
+nsHTMLElement::~nsHTMLElement()
+{
+}
+
+NS_IMPL_ADDREF_INHERITED(nsHTMLElement, nsGenericElement)
+NS_IMPL_RELEASE_INHERITED(nsHTMLElement, nsGenericElement)
+
+DOMCI_DATA(HTMLElement, nsHTMLElement)
+
+NS_INTERFACE_TABLE_HEAD(nsHTMLElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE0(nsHTMLElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE_TO_MAP_SEGUE(nsHTMLElement,
+                                               nsGenericHTMLElement)
+NS_HTML_CONTENT_INTERFACE_TABLE_TAIL_CLASSINFO(HTMLElement)
+
+NS_IMPL_ELEMENT_CLONE(nsHTMLElement)
+
