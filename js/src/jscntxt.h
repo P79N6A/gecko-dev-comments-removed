17df0be7809638677c32131bb5f03b6655626d55
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:27:29 +0000

    Backed out changeset 031e9ea92ed1 (bug 605754) due to Windows build bustage, a=red
    UltraBlame original commit: 2e61a36853f0d097773509d7d0630141a5311cde

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -2325,15 +2325,6 @@ struct JSContext
     void assertValidStackDepth(uintN ) {}
 #endif
 
-    volatile enum DollarPath {
-        DOLLAR_LITERAL = 1,
-        DOLLAR_AMP,
-        DOLLAR_PLUS,
-        DOLLAR_TICK,
-        DOLLAR_QUOT
-    } *dollarPath;
-    volatile jschar *blackBox;
-
 private:
 
     
@@ -2346,7 +2337,7 @@ private:
 
     
     JSContext *thisInInitializer() { return this; }
-}; 
+};
 
 #ifdef JS_THREADSAFE
 # define JS_THREAD_ID(cx)       ((cx)->thread ? (cx)->thread->id : 0)
