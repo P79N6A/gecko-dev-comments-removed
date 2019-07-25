94468d0dc031159e6b55a71761c3b1ce05f20fff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:29:26 +0000

    Bug 767836 - show custom newtab page in private browsing mode if configured; r=ttaubert
    UltraBlame original commit: 09b9de5e8bb937c76492c2eeef121b9a61cccf41

--- browser/base/content/utilityOverlay.js
+++ browser/base/content/utilityOverlay.js
@@ -1,4 +1,4 @@
-# -*- Mode: Java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+# -*- Mode: javascript; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http:
@@ -12,7 +12,8 @@ XPCOMUtils.defineLazyGetter(this, "BROWSER_NEW_TAB_URL", function () {
   const TOPIC = "private-browsing-transition-complete";
 
   function getNewTabPageURL() {
-    if (("gPrivateBrowsingUI" in window)) {
+    if (("gPrivateBrowsingUI" in window) &&
+        !Services.prefs.prefHasUserValue(PREF)) {
       
       
       if (!gPrivateBrowsingUI.initialized)
