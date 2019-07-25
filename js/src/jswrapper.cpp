f88c1a76150f976d52a95dd7ce87ec8b50231131
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:41:21 +0000

    Bug 597104 - Innerize cx->globalObject before using it. r=gal
    UltraBlame original commit: 7e47ca7dd0a2cf5702b65ee050a99a689b283357

--- js/src/jswrapper.cpp
+++ js/src/jswrapper.cpp
@@ -387,8 +387,16 @@ JSCompartment::wrap(JSContext *cx, Value *vp)
 
 
 
-    JSObject *global =
-        cx->hasfp() ? cx->fp()->scopeChain().getGlobal() : cx->globalObject;
+    JSObject *global;
+    if (cx->hasfp()) {
+        global = cx->fp()->scopeChain().getGlobal();
+    } else {
+        global = cx->globalObject;
+        OBJ_TO_INNER_OBJECT(cx, global);
+        if (!global)
+            return false;
+    }
+
     wrapper->setParent(global);
     return true;
 }
