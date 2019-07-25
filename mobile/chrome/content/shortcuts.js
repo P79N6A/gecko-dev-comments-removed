56211cf54c6bd483b0dab113b821c44e0e17a649
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:01:05 +0000

    add in the correct code for translating a key into the proper string. the code is much the same as what nsMenuFrame does. It reuses the same properties files and so on.
    UltraBlame original commit: c2c7c4c8b7c99265e71aa69cf4dce18141aafe50

--- mobile/chrome/content/shortcuts.js
+++ mobile/chrome/content/shortcuts.js
@@ -38,10 +38,14 @@
 
 
 
+
 const XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
 
 function ShortcutEditor()
 {
+    var prefs = Components.classes["@mozilla.org/preferences-service;1"]
+                          .getService(Components.interfaces.nsIPrefBranch2);
+
     
     function getCommandNames()
     {
@@ -109,22 +113,57 @@ function ShortcutEditor()
         return k;
     }
 
+    
+    
+    
+    
+
+    
+    var platformBundle = document.getElementById("bundle-platformKeys");
+    var platformKeys = {
+        shift: platformBundle.getString("VK_SHIFT"),
+        meta: platformBundle.getString("VK_META"),
+        alt: platformBundle.getString("VK_ALT"),
+        control: platformBundle.getString("VK_CONTROL")
+    };
+    var modifierSeparator = platformBundle.getString("MODIFIER_SEPARATOR");
+
+#ifdef XP_MACOSX
+    var accelKey = Components.interfaces.nsIDOMKeyEvent.DOM_VK_META;
+#else
+    var accelKey = Components.interfaces.nsIDOMKeyEvent.DOM_VK_CONTROL;
+#endif
+
+    try {
+        accelKey = prefs.getCharPref("ui.key.accelKey");
+    } catch (e) { }
+
+    
+    var platformAccel = { };
+    platformAccel[Components.interfaces.nsIDOMKeyEvent.DOM_VK_META] = platformKeys.meta;
+    platformAccel[Components.interfaces.nsIDOMKeyEvent.DOM_VK_ALT] = platformKeys.alt;
+    platformAccel[Components.interfaces.nsIDOMKeyEvent.DOM_VK_CONTROL] = platformKeys.control;
+    if (accelKey in platformAccel)
+        platformKeys.accel = platformAccel[accelKey];
+    else
+        platformKeys.accel = platformKeys.control;
+
     function getKeyName(key) {
         
         
-        
-        
-        
-        
-        
-
         if (!key)
             return "";
 
-        var m = key.getAttribute("modifiers").replace(" ", "-");
-        var k = key.getAttribute("key") || key.getAttribute("keycode");
+        var accel = [];
+        var keybundle = document.getElementById("bundle-keys");
+        var keyName = key.getAttribute("keytext") || key.getAttribute("key") || keybundle.getString(key.getAttribute("keycode"));
+        var modifiers = key.getAttribute("modifiers").split(" ");
+        for each (m in modifiers)
+            if (m in platformKeys)
+                accel.push(platformKeys[m]);
+        accel.push(keyName);
 
-        return m +"-"+ k;
+        return accel.join(modifierSeparator);
     }
 
     
@@ -138,7 +177,6 @@ function ShortcutEditor()
 
     this.dismiss = function()
     {
-        
         document.getElementById("shortcuts-container").hidden = true;
     };
 
@@ -154,7 +192,6 @@ function ShortcutEditor()
             
             
             
-
             var cell1 = document.createElementNS(XUL_NS, "listcell");
             cell1.setAttribute("label", name);
             var cell2 = document.createElementNS(XUL_NS, "listcell");
