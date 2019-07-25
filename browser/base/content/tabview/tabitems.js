199f620958723d294542a854673d807a5b022476
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:08:31 +0000

    Backed out changeset 6d1496942eaf a=bustage
    chael Yoshitaka Erlewine – Bug 610242 - Don't show tab icon if there isn't one? [r=ian, a=beltzner]
    
    UltraBlame original commit: 7973cf0423d4c5f61638661072826482e7093a1b

--- browser/base/content/tabview/tabitems.js
+++ browser/base/content/tabview/tabitems.js
@@ -351,7 +351,7 @@ TabItem.prototype = Utils.extend(new Item(), new Subscribable(), {
       if (tabData.groupID) {
         var groupItem = GroupItems.groupItem(tabData.groupID);
         if (groupItem) {
-          groupItem.add(this, {immediately: true});
+          groupItem.add(this, null, {immediately: true});
 
           
           
@@ -927,20 +927,12 @@ let TabItems = {
       let tabItem = tab._tabViewTabItem;
 
       
-      if (this.shouldLoadFavIcon(tab.linkedBrowser)) {
-        let iconUrl = tab.image;
-        if (!iconUrl)
-          iconUrl = Utils.defaultFaviconURL;
-  
-        if (iconUrl != tabItem.favImgEl.src)
-          tabItem.favImgEl.src = iconUrl;
-        
-        iQ(tabItem.favEl).show();
-      } else {
-        if (tabItem.favImgEl.hasAttribute("src"))
-          tabItem.favImgEl.removeAttribute("src");
-        iQ(tabItem.favEl).hide();
-      }
+      let iconUrl = tab.image;
+      if (!iconUrl)
+        iconUrl = Utils.defaultFaviconURL;
+
+      if (iconUrl != tabItem.favImgEl.src)
+        tabItem.favImgEl.src = iconUrl;
 
       
       let tabUrl = tab.linkedBrowser.currentURI.spec;
@@ -978,14 +970,6 @@ let TabItems = {
     } catch(e) {
       Utils.log(e);
     }
-  },
-
-  
-  
-  
-  shouldLoadFavIcon: function TabItems_shouldLoadFavIcon(browser) {
-    return !(browser.contentDocument instanceof window.ImageDocument) &&
-           gBrowser.shouldLoadFavIcon(browser.contentDocument.documentURIObject);
   },
 
   
