a0f8e6cc09278f5e1dc22f4748be27bb2c953d64
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:22:55 +0000

    Bug 745018. Implement webapp runtime for Linux. r=jst
    UltraBlame original commit: 0ae2ca987cf916445f253bfda6d08a00842f988a

---
+++ webapprt/linux/webapprt.cpp
@@ -0,0 +1,289 @@
+
+
+
+
+
+
+
+#include <gtk/gtk.h>
+
+
+#include <fcntl.h>
+#include <unistd.h>
+
+
+#include "nsILocalFile.h"
+#include "nsINIParser.h"
+#include "nsXPCOMGlue.h"
+#include "nsXPCOMPrivate.h"              
+#include "nsXULAppAPI.h"
+
+const char kAPP_INI[] = "application.ini";
+const char kWEBAPP_INI[] = "webapp.ini";
+const char kWEBAPPRT_INI[] = "webapprt.ini";
+const char kWEBAPPRT_PATH[] = "webapprt";
+const char kAPP_ENV_VAR[] = "XUL_APP_FILE";
+const char kAPP_RT[] = "webapprt-stub";
+
+int* pargc;
+char*** pargv;
+
+XRE_GetFileFromPathType XRE_GetFileFromPath;
+XRE_CreateAppDataType XRE_CreateAppData;
+XRE_FreeAppDataType XRE_FreeAppData;
+XRE_mainType XRE_main;
+
+const nsDynamicFunctionLoad kXULFuncs[] = {
+  { "XRE_GetFileFromPath", (NSFuncPtr*) &XRE_GetFileFromPath },
+  { "XRE_CreateAppData", (NSFuncPtr*) &XRE_CreateAppData },
+  { "XRE_FreeAppData", (NSFuncPtr*) &XRE_FreeAppData },
+  { "XRE_main", (NSFuncPtr*) &XRE_main },
+  { nsnull, nsnull }
+};
+
+class ScopedLogging
+{
+  public:
+    ScopedLogging() { NS_LogInit(); }
+    ~ScopedLogging() { NS_LogTerm(); }
+};
+
+
+void SetAllocatedString(const char *&str, const char *newvalue)
+{
+  NS_Free(const_cast<char*>(str));
+  if (newvalue) {
+    str = NS_strdup(newvalue);
+  }
+  else {
+    str = nsnull;
+  }
+}
+
+
+void ErrorDialog(const char* message)
+{
+  GtkWidget* dialog;
+
+  dialog = gtk_message_dialog_new(NULL, GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, "%s", message);
+  gtk_dialog_run(GTK_DIALOG(dialog));
+  gtk_widget_destroy(dialog);
+}
+
+
+void GetDirFromPath(char* parentDir, char* filePath)
+{
+  char* base = strrchr(filePath, '/');
+
+  if (!base) {
+    strcpy(parentDir, ".");
+  }
+  else {
+    while (base > filePath && *base == '/')
+      base--;
+
+    int len = 1 + base - filePath;
+    strncpy(parentDir, filePath, len);
+    parentDir[len] = '\0';
+  }
+}
+
+bool CopyFile(const char* inputFile, const char* outputFile)
+{
+  
+  int inputFD = open(inputFile, O_RDONLY);
+  if (!inputFD)
+    return false;
+
+  
+  int outputFD = creat(outputFile, S_IRWXU);
+  if (!outputFD)
+    return false;
+
+  
+  char buf[BUFSIZ];
+  ssize_t bytesRead;
+
+  while ((bytesRead = read(inputFD, buf, BUFSIZ)) > 0) {
+    ssize_t bytesWritten = write(outputFD, buf, bytesRead);
+    if (bytesWritten < 0) {
+      bytesRead = -1;
+      break;
+    }
+  }
+
+  
+  close(inputFD);
+  close(outputFD);
+
+  return (bytesRead >= 0);
+}
+
+bool GRELoadAndLaunch(const char* firefoxDir, const char* profile)
+{
+  char xpcomDllPath[MAXPATHLEN];
+  snprintf(xpcomDllPath, MAXPATHLEN, "%s/%s", firefoxDir, XPCOM_DLL);
+
+  if (NS_FAILED(XPCOMGlueStartup(xpcomDllPath))) {
+    ErrorDialog("Couldn't load the XPCOM library");
+    return false;
+  }
+
+  if (NS_FAILED(XPCOMGlueLoadXULFunctions(kXULFuncs))) {
+    ErrorDialog("Couldn't load libxul");
+    return false;
+  }
+
+  
+  { 
+    ScopedLogging log;
+
+    
+    char rtPath[MAXPATHLEN];
+    snprintf(rtPath, MAXPATHLEN, "%s/%s", firefoxDir, kWEBAPPRT_PATH);
+
+    
+    char rtIniPath[MAXPATHLEN];
+    snprintf(rtIniPath, MAXPATHLEN, "%s/%s", rtPath, kWEBAPPRT_INI);
+
+    
+    nsCOMPtr<nsILocalFile> rtINI;
+    if (NS_FAILED(XRE_GetFileFromPath(rtIniPath, getter_AddRefs(rtINI)))) {
+      ErrorDialog("Couldn't load the runtime INI");
+      return false;
+    }
+
+    bool exists;
+    nsresult rv = rtINI->Exists(&exists);
+    if (NS_FAILED(rv) || !exists) {
+      ErrorDialog("The runtime INI doesn't exist");
+      return false;
+    }
+
+    nsXREAppData *webShellAppData;
+    if (NS_FAILED(XRE_CreateAppData(rtINI, &webShellAppData))) {
+      ErrorDialog("Couldn't read WebappRT application.ini");
+      return false;
+    }
+
+    SetAllocatedString(webShellAppData->profile, profile);
+
+    nsCOMPtr<nsILocalFile> directory;
+    if (NS_FAILED(XRE_GetFileFromPath(rtPath, getter_AddRefs(directory)))) {
+      ErrorDialog("Couldn't open runtime directory");
+      return false;
+    }
+
+    nsCOMPtr<nsILocalFile> xreDir;
+    if (NS_FAILED(XRE_GetFileFromPath(firefoxDir, getter_AddRefs(xreDir)))) {
+      ErrorDialog("Couldn't open XRE directory");
+      return false;
+    }
+
+    xreDir.forget(&webShellAppData->xreDirectory);
+    NS_IF_RELEASE(webShellAppData->directory);
+    directory.forget(&webShellAppData->directory);
+
+    XRE_main(*pargc, *pargv, webShellAppData, 0);
+
+    XRE_FreeAppData(webShellAppData);
+  }
+
+  return true;
+}
+
+void CopyAndRelaunch(const char* firefoxDir, const char* curExePath)
+{
+  char newExePath[MAXPATHLEN];
+  snprintf(newExePath, MAXPATHLEN, "%s/%s", firefoxDir, kAPP_RT);
+
+  if (unlink(curExePath) == -1) {
+    ErrorDialog("Couldn't remove the old webapprt-stub executable");
+    return;
+  }
+
+  if (!CopyFile(newExePath, curExePath)) {
+    ErrorDialog("Couldn't copy the new webapprt-stub executable");
+    return;
+  }
+
+  execv(curExePath, *pargv);
+
+  ErrorDialog("Couldn't execute the new webapprt-stub executable");
+}
+
+int main(int argc, char *argv[])
+{
+  gtk_init(&argc, &argv);
+
+  pargc = &argc;
+  pargv = &argv;
+
+  
+  char curExePath[MAXPATHLEN];
+  if (readlink("/proc/self/exe", curExePath, MAXPATHLEN) == -1) {
+    ErrorDialog("Couldn't read current executable path");
+    return 255;
+  }
+
+  
+  char curExeDir[MAXPATHLEN];
+  GetDirFromPath(curExeDir, curExePath);
+  char webAppIniPath[MAXPATHLEN];
+  snprintf(webAppIniPath, MAXPATHLEN, "%s/%s", curExeDir, kWEBAPP_INI);
+
+  
+  nsINIParser parser;
+  if (NS_FAILED(parser.Init(webAppIniPath))) {
+    ErrorDialog("Couldn't open webapp.ini");
+    return 255;
+  }
+
+  
+  if (setenv(kAPP_ENV_VAR, webAppIniPath, 1) == -1) {
+    ErrorDialog("Couldn't set up app environment");
+    return 255;
+  }
+
+  
+  char profile[MAXPATHLEN];
+  if (NS_FAILED(parser.GetString("Webapp", "Profile", profile, MAXPATHLEN))) {
+    ErrorDialog("Couldn't retrieve profile from web app INI file");
+    return 255;
+  }
+
+  
+  char firefoxDir[MAXPATHLEN];
+  if (NS_FAILED(parser.GetString("WebappRT", "InstallDir", firefoxDir, MAXPATHLEN))) {
+    ErrorDialog("Couldn't find your Firefox install directory.");
+    return 255;
+  }
+
+  
+  
+  char appIniPath[MAXPATHLEN];
+  snprintf(appIniPath, MAXPATHLEN, "%s/%s", firefoxDir, kAPP_INI);
+
+  if (NS_FAILED(parser.Init(appIniPath))) {
+    ErrorDialog("Couldn't open Firefox application.ini");
+    return 255;
+  }
+
+  
+  char buildid[MAXPATHLEN];
+  if (NS_FAILED(parser.GetString("App", "BuildID", buildid, MAXPATHLEN))) {
+    ErrorDialog("Couldn't read BuildID from Firefox application.ini");
+    return 255;
+  }
+
+  
+  if (!strcmp(buildid, NS_STRINGIFY(GRE_BUILDID))) {
+    if (GRELoadAndLaunch(firefoxDir, profile))
+      return 0;
+  }
+  
+  else
+    CopyAndRelaunch(firefoxDir, curExePath);
+
+  return 255;
+}
