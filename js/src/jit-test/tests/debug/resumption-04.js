5e9048e6e9e8162baf7ab2d2384f168ea84bb3b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:37:24 +0000

    Add passing test for {return:} resumption from a generator frame.
    UltraBlame original commit: ee8bde5b1b7729ee7894d13eaf9bc1b5d155b93f

---
+++ js/src/jit-test/tests/debug/resumption-04.js
@@ -0,0 +1,15 @@
+
+
+
+var g = newGlobal('new-compartment');
+g.debuggeeGlobal = this;
+g.eval("var dbg = new Debug(debuggeeGlobal);" +
+       "dbg.hooks = {debuggerHandler: function () { return {return: '!'}; }};");
+
+function gen() {
+    yield '1';
+    debugger;  
+    yield '2';
+}
+var x = [v for (v in gen())];
+assertEq(x.join(","), "1");
