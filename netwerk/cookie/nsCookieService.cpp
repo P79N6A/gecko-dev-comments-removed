141277be05deba86040877ba44f0fa900ce5ad32
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:52:31 +0000

    Bug 611503 Constructing an nsTDependentString from an nsTAString makes no sense r=dbaron,dwitte
    UltraBlame original commit: f1eef5e80caf7271ef046bc0081da3c9088d1b0d

--- netwerk/cookie/nsCookieService.cpp
+++ netwerk/cookie/nsCookieService.cpp
@@ -1493,11 +1493,11 @@ nsCookieService::SetCookieStringCommon(nsIURI *aHostURI,
 }
 
 void
-nsCookieService::SetCookieStringInternal(nsIURI          *aHostURI,
-                                         bool             aIsForeign,
-                                         const nsCString &aCookieHeader,
-                                         const nsCString &aServerTime,
-                                         PRBool           aFromHttp) 
+nsCookieService::SetCookieStringInternal(nsIURI             *aHostURI,
+                                         bool                aIsForeign,
+                                         nsDependentCString &aCookieHeader,
+                                         const nsCString    &aServerTime,
+                                         PRBool              aFromHttp)
 {
   NS_ASSERTION(aHostURI, "null host!");
 
@@ -1550,9 +1550,8 @@ nsCookieService::SetCookieStringInternal(nsIURI          *aHostURI,
   }
 
   
-  nsDependentCString cookieHeader(aCookieHeader);
-  while (SetCookieInternal(aHostURI, baseDomain, requireHostMatch,
-                           cookieStatus, cookieHeader, serverTime, aFromHttp)) {
+  while (SetCookieInternal(aHostURI, baseDomain, requireHostMatch, cookieStatus,
+                           aCookieHeader, serverTime, aFromHttp)) {
     
     if (!aFromHttp)
       break;
