c40b3277bd4b94909c52a6d04d3b24d5dfae151c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:19:04 +0000

    [mq]: weakRefForms
    UltraBlame original commit: a6f137a4e2ea48b9eeff06f8364369074353b9bb

--- services/sync/modules/engines/forms.js
+++ services/sync/modules/engines/forms.js
@@ -207,14 +207,15 @@ function FormTracker(name) {
   
   
   Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService).
-    addObserver(this, "earlyformsubmit", false);
+    addObserver(this, "earlyformsubmit", true);
 }
 FormTracker.prototype = {
   __proto__: Tracker.prototype,
 
   QueryInterface: XPCOMUtils.generateQI([
     Ci.nsIFormSubmitObserver,
-    Ci.nsIObserver]),
+    Ci.nsIObserver,
+    Ci.nsISupportsWeakReference]),
 
   trackEntry: function trackEntry(name, value) {
     this.addChangedID(FormWrapper.getGUID(name, value));
