229842b5d4782222c85ce819933ad69c7c3c0a15
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:31:26 +0000

    Bug 583460 - [clang] nsClipboard: cannot initialize a parameter of type 'NSString *' with an lvalue of type 'NSString const *', r=josh, a=josh
    UltraBlame original commit: 0e4ab404540dcdedd3f177017c59249c41869b7b

--- widget/src/cocoa/nsClipboard.h
+++ widget/src/cocoa/nsClipboard.h
@@ -59,7 +59,7 @@ public:
 
   
   static NSDictionary* PasteboardDictFromTransferable(nsITransferable *aTransferable);
-  static PRBool IsStringType(const nsCString& aMIMEType, const NSString** aPasteboardType);
+  static PRBool IsStringType(const nsCString& aMIMEType, NSString** aPasteboardType);
   static NSString* WrapHtmlForSystemPasteboard(NSString* aString);
   static nsresult TransferableFromPasteboard(nsITransferable *aTransferable, NSPasteboard *pboard);
 
