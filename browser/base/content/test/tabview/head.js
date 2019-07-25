3becf85a02a1af490428a285753e2368ceb01260
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:08:30 +0000

    Bug 610242 - Don't show tab icon if there isn't one? [r=ian, a=beltzner]
    UltraBlame original commit: 6d1496942eaf17883328ba015ab91f212ea9419e

--- browser/base/content/test/tabview/head.js
+++ browser/base/content/test/tabview/head.js
@@ -35,6 +35,7 @@
 
 
 
+
 function createEmptyGroupItem(contentWindow, width, height, padding, noAnimation) {
   let pageBounds = contentWindow.Items.getPageBounds();
   pageBounds.inset(padding, padding);
@@ -48,4 +49,56 @@ function createEmptyGroupItem(contentWindow, width, height, padding, noAnimation
     new contentWindow.GroupItem([], { bounds: box, immediately: immediately });
 
   return emptyGroupItem;
+}
+
+
+function afterAllTabsLoaded(callback, win) {
+  win = win || window;
+
+  let stillToLoad = 0;
+  function onLoad() {
+    this.removeEventListener("load", onLoad, true);    
+    stillToLoad--;
+    if (!stillToLoad)
+      callback();
+  }
+
+  for (let a = 0; a < win.gBrowser.tabs.length; a++) {
+    let browser = win.gBrowser.tabs[a].linkedBrowser;
+    if (browser.webProgress.isLoadingDocument) {
+      stillToLoad++;
+      browser.addEventListener("load", onLoad, true);
+    }
+  }
+  if (!stillToLoad)
+    callback();
+}
+
+
+function afterAllTabItemsUpdated(callback, win) {
+  win = win || window;
+  let tabItems = win.document.getElementById("tab-view").contentWindow.TabItems;
+
+  for (let a = 0; a < win.gBrowser.tabs.length; a++) {
+    let tabItem = win.gBrowser.tabs[a].tabItem;
+    if (tabItem)
+      tabItems._update(win.gBrowser.tabs[a]);
+  }
+  callback();
+}
+
+
+function newWindowWithTabView(callback) {
+  let win = window.openDialog(getBrowserURL(), "_blank", 
+                              "chrome,all,dialog=no,height=800,width=800");
+  let onLoad = function() {
+    win.removeEventListener("load", onLoad, false);
+    let onShown = function() {
+      win.removeEventListener("tabviewshown", onShown, false);
+      callback(win);
+    };
+    win.addEventListener("tabviewshown", onShown, false);
+    win.TabView.toggle();
+  }
+  win.addEventListener("load", onLoad, false);
 }
