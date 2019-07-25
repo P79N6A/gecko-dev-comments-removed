dbff1b82e649847bf1008ecbb3a67479ca649d6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:27:47 +0000

    Bug 757347; Add a shutdown hook and remove MaskImageFormat(). r=roc
    UltraBlame original commit: 57077263e3c5e8d32c542376650fd3775a8e9909

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -562,6 +562,16 @@ ThebesDisplayItemLayerUserData* GetThebesDisplayItemLayerUserData(Layer* aLayer)
 
 } 
 
+
+ void
+FrameLayerBuilder::Shutdown()
+{
+  if (gMaskLayerImageCache) {
+    delete gMaskLayerImageCache;
+    gMaskLayerImageCache = nsnull;
+  }
+}
+
 void
 FrameLayerBuilder::Init(nsDisplayListBuilder* aBuilder)
 {
@@ -2897,7 +2907,7 @@ ContainerState::SetupMaskLayer(Layer *aLayer, const FrameLayerBuilder::Clip& aCl
     
     nsRefPtr<gfxASurface> surface =
       aLayer->Manager()->CreateOptimalSurface(surfaceSize,
-                                              aLayer->Manager()->MaskImageFormat());
+                                              gfxASurface::ImageFormatARGB32);
 
     
     if (!surface || surface->CairoStatus()) {
