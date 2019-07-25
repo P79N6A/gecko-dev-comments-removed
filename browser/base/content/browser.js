0f38b45213e43cd9c9b605ada5b919ea0e6e3ace
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:40:51 +0000

    Backing out changeset 3f004b291c65 from bug 571970 due to test failures. a=backout
    UltraBlame original commit: c403f53901f2c16d3b7007ddaaa468bf685bf978

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -3901,7 +3901,6 @@ var XULBrowserWindow = {
   startTime: 0,
   statusText: "",
   isBusy: false,
-  inContentWhitelist: ["about:addons"],
 
   QueryInterface: function (aIID) {
     if (aIID.equals(Ci.nsIWebProgressListener) ||
@@ -4139,16 +4138,6 @@ var XULBrowserWindow = {
         }
       }
     }
-
-    
-    var disableChrome = this.inContentWhitelist.some(function(aSpec) {
-      return aSpec == location;
-    });
-
-    if (disableChrome)
-      document.documentElement.setAttribute("disablechrome", "true");
-    else
-      document.documentElement.removeAttribute("disablechrome");
 
     
     
