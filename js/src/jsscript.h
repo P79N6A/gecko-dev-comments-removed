7ca88921c21fd582ce829b1d05f1f750f571ba45
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:45:15 +0000

    Bug 787927 - Prevent self-hosted JS script from being registered with the debugger
    UltraBlame original commit: cddd665d83f048ead04c002abb09d2f264929c9e

--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -450,6 +450,7 @@ struct JSScript : public js::gc::Cell
     bool            strictModeCode:1; 
     bool            explicitUseStrict:1; 
     bool            compileAndGo:1;   
+    bool            selfHosted:1;     
     bool            bindingsAccessedDynamically:1; 
     bool            funHasExtensibleScope:1;       
     bool            funHasAnyAliasedFormal:1;      
