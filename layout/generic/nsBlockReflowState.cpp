f428c803f9439261a88b557eca2b3d88f1218505
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:07:42 +0000

    nsBlockFrame::ReflowFloatContinuations doesn't need anything in AddFloat other than FlowAndPlaceFloat.  (Bug 563584, patch 14)  r=roc
    UltraBlame original commit: 1ff1f54dc043c3779f26878b689a5ce45fea232b

--- layout/generic/nsBlockReflowState.cpp
+++ layout/generic/nsBlockReflowState.cpp
@@ -542,7 +542,8 @@ nsBlockReflowState::AddFloat(nsLineLayout*       aLineLayout,
                              nscoord             aAvailableWidth,
                              nsReflowStatus&     aReflowStatus)
 {
-  NS_PRECONDITION(!aLineLayout || mBlock->end_lines() != mCurrentLine, "null ptr");
+  NS_PRECONDITION(aLineLayout, "must have line layout");
+  NS_PRECONDITION(mBlock->end_lines() != mCurrentLine, "null ptr");
   NS_PRECONDITION(aFloat->GetStateBits() & NS_FRAME_OUT_OF_FLOW,
                   "aFloat must be an out-of-flow frame");
 
@@ -570,11 +571,10 @@ nsBlockReflowState::AddFloat(nsLineLayout*       aLineLayout,
   
   
   nsRect floatAvailableSpace = GetFloatAvailableSpace().mRect;
-  if (!aLineLayout ||
-      (mBelowCurrentLineFloats.IsEmpty() &&
-       (aLineLayout->LineIsEmpty() ||
-        mBlock->ComputeFloatWidth(*this, floatAvailableSpace, aFloat)
-        <= aAvailableWidth))) {
+  if (mBelowCurrentLineFloats.IsEmpty() &&
+      (aLineLayout->LineIsEmpty() ||
+       mBlock->ComputeFloatWidth(*this, floatAvailableSpace, aFloat)
+       <= aAvailableWidth)) {
     nsFloatManager::SavedState floatManagerState;
     mFloatManager->PushState(&floatManagerState);
 
@@ -586,11 +586,9 @@ nsBlockReflowState::AddFloat(nsLineLayout*       aLineLayout,
       nsRect availSpace(nsPoint(floatAvailSpace.mRect.x + BorderPadding().left,
                                 mY),
                         floatAvailSpace.mRect.Size());
-      if (aLineLayout) {
-        aLineLayout->UpdateBand(availSpace, aFloat);
-        
-        mCurrentLineFloats.Append(mFloatCacheFreeList.Alloc(aFloat));
-      }
+      aLineLayout->UpdateBand(availSpace, aFloat);
+      
+      mCurrentLineFloats.Append(mFloatCacheFreeList.Alloc(aFloat));
     }
     else {
       if (placed) {
