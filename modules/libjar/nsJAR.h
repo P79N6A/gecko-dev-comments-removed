9e15cac43240c411228774d606fcc1cae0a816e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:00:28 +0000

    Bug 590242 - Do not open omnijar 3x r=mwu, sr=bsmedberg, a=beltzner
    UltraBlame original commit: 6b5f659829765728721e7d79750a8340754e98f4

--- modules/libjar/nsJAR.h
+++ modules/libjar/nsJAR.h
@@ -133,7 +133,7 @@ class nsJAR : public nsIZipReader
     
     nsCOMPtr<nsIFile>        mZipFile;        
     nsCString                mOuterZipEntry;  
-    nsZipArchive             mZip;            
+    nsAutoPtr<nsZipArchive>  mZip;            
     nsObjectHashtable        mManifestData;   
     PRBool                   mParsedManifest; 
     nsCOMPtr<nsIPrincipal>   mPrincipal;      
