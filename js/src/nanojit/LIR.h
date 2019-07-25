9e2c72283b7aa1747981773f50b575712c9a8c49
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:13:48 +0000

    Make accNames[] public;  needed for TM.  No bug, r=me.
    UltraBlame original commit: 4b12c8bfa5ce90ce60e7c647d4f16861c88d9147

--- js/src/nanojit/LIR.h
+++ js/src/nanojit/LIR.h
@@ -1777,9 +1777,9 @@ namespace nanojit
         char *formatImmD(RefBuf* buf, double c);
         void formatGuard(InsBuf* buf, LIns* ins);       
         void formatGuardXov(InsBuf* buf, LIns* ins);    
-        static const char* accNames[];                  
 
     public:
+        static const char* accNames[];                  
 
         LInsPrinter(Allocator& alloc, int embNumUsedAccs)
             : alloc(alloc), EMB_NUM_USED_ACCS(embNumUsedAccs)
