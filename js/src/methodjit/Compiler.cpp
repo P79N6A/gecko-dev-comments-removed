57cf4f569ddd3980eeeb0b3c346099edae0fe7b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:23:17 +0000

    Properly watch for indexed prototypes and configured properties in array prototype, bug 709067. r=luke
    UltraBlame original commit: 35d59a6e829dd7ef8be189561224566eef80007e

--- js/src/methodjit/Compiler.cpp
+++ js/src/methodjit/Compiler.cpp
@@ -7702,17 +7702,19 @@ mjit::Compiler::arrayPrototypeHasIndexedProperty()
 
     JSObject *proto;
     if (!js_GetClassPrototype(cx, NULL, JSProto_Array, &proto, NULL))
-        return false;
-
-    
-
-
-
-
-    if (proto->getType(cx)->unknownProperties())
         return true;
-    types::TypeSet *arrayTypes = proto->getType(cx)->getProperty(cx, JSID_VOID, false);
-    return !arrayTypes || arrayTypes->knownNonEmpty(cx);
+
+    while (proto) {
+        types::TypeObject *type = proto->getType(cx);
+        if (type->unknownProperties())
+            return true;
+        types::TypeSet *indexTypes = type->getProperty(cx, JSID_VOID, false);
+        if (!indexTypes || indexTypes->isOwnProperty(cx, type, true) || indexTypes->knownNonEmpty(cx))
+            return true;
+        proto = proto->getProto();
+    }
+
+    return false;
 }
 
 
