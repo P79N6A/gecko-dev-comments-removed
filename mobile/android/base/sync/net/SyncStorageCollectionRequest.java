8eb3987ad52e3297e873264b66f220c38d1337de
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:58:06 +0000

    Bug 736972 - java.lang.OutOfMemoryError: at java.io.BufferedReader.<init>(BufferedReader.java). r=nalexander
    UltraBlame original commit: c1e32cb0678e31b73de2dbfe299cd7313dc3a11d

--- mobile/android/base/sync/net/SyncStorageCollectionRequest.java
+++ mobile/android/base/sync/net/SyncStorageCollectionRequest.java
@@ -38,6 +38,9 @@ public class SyncStorageCollectionRequest extends SyncStorageRequest {
   public class SyncCollectionResourceDelegate extends
       SyncStorageResourceDelegate {
 
+    private static final String CONTENT_TYPE_INCREMENTAL = "application/newlines";
+    private static final int FETCH_BUFFER_SIZE = 16 * 1024;   
+
     SyncCollectionResourceDelegate(SyncStorageCollectionRequest request) {
       super(request);
     }
@@ -45,7 +48,7 @@ public class SyncStorageCollectionRequest extends SyncStorageRequest {
     @Override
     public void addHeaders(HttpRequestBase request, DefaultHttpClient client) {
       super.addHeaders(request, client);
-      request.setHeader("Accept", "application/newlines");
+      request.setHeader("Accept", CONTENT_TYPE_INCREMENTAL);
       
     }
 
@@ -58,7 +61,7 @@ public class SyncStorageCollectionRequest extends SyncStorageRequest {
 
       HttpEntity entity = response.getEntity();
       Header contentType = entity.getContentType();
-      if (!contentType.getValue().startsWith("application/newlines")) {
+      if (!contentType.getValue().startsWith(CONTENT_TYPE_INCREMENTAL)) {
         
         super.handleHttpResponse(response);
         return;
@@ -76,8 +79,7 @@ public class SyncStorageCollectionRequest extends SyncStorageRequest {
       BufferedReader br = null;
       try {
         content = entity.getContent();
-        int bufSize = 1024 * 1024;         
-        br = new BufferedReader(new InputStreamReader(content), bufSize);
+        br = new BufferedReader(new InputStreamReader(content), FETCH_BUFFER_SIZE);
         String line;
 
         
