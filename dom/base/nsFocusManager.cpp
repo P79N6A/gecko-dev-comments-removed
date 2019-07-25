118e34c53f103df1f1bf2d6ab234c9ca88718444
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:27:44 +0000

    Bug 612018 - "ASSERTION: frame must not be dirty" and non-blinking caret; r,a=roc
    UltraBlame original commit: b0d1532292908180f818be63dc1c4a8f9bd58d19

--- dom/base/nsFocusManager.cpp
+++ dom/base/nsFocusManager.cpp
@@ -1358,7 +1358,7 @@ nsFocusManager::CheckIfFocusable(nsIContent* aContent, PRUint32 aFlags)
 
   
   if (doc)
-    doc->FlushPendingNotifications(Flush_Frames);
+    doc->FlushPendingNotifications(Flush_Layout);
 
   nsIPresShell *shell = doc->GetShell();
   if (!shell)
