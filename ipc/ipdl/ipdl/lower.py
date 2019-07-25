368ccdf1c0d84affabb00cd43f8bb233b77eb09f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:31:19 +0000

    Bug 765734, part 0: Do the right thing here. r=bent
    UltraBlame original commit: 6b35f9011f269ece466b27f7a75506ba8b2ef9f6

--- ipc/ipdl/ipdl/lower.py
+++ ipc/ipdl/ipdl/lower.py
@@ -666,6 +666,9 @@ class _StructField(_CompoundTypeComponent):
         if self.recursive:
             return [ StmtExpr(ExprAssn(self.memberVar(),
                                        ExprNew(self.bareType()))) ]
+        elif self.ipdltype.isIPDL() and self.ipdltype.isActor():
+            return [ StmtExpr(ExprAssn(self.memberVar(),
+                                       ExprLiteral.NULL)) ]
         else:
             return []
 
@@ -3086,18 +3089,16 @@ class _GenerateProtocolActorCode(ipdl.ast.Visitor):
             ])
 
             dumpvar = ExprVar('aDump')
-            seqvar = ExprVar('aSequence')
             getdump = MethodDefn(MethodDecl(
                 'TakeMinidump',
-                params=[ Decl(Type('nsIFile', ptrptr=1), dumpvar.name),
-                         Decl(Type.UINT32PTR, seqvar.name)],
+                params=[ Decl(Type('nsIFile', ptrptr=1), dumpvar.name) ],
                 ret=Type.BOOL,
                 const=1))
             getdump.addstmts([
                 CppDirective('ifdef', 'MOZ_CRASHREPORTER'),
                 StmtReturn(ExprCall(
                     ExprVar('XRE_TakeMinidumpForChild'),
-                    args=[ ExprCall(otherpidvar), dumpvar, seqvar ])),
+                    args=[ ExprCall(otherpidvar), dumpvar ])),
                 CppDirective('else'),
                 StmtReturn.FALSE,
                 CppDirective('endif')
