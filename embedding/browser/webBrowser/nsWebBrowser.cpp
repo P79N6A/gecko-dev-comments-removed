ba7f3d662cfa456298a05346596c0810d6309a3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:17:46 +0000

    bug 586183 - Fix for embedding/ when --disable-ipc is used. r+a=bsmedberg
    UltraBlame original commit: 9b4466b55eaf082b15582e642225e887a6832d26

--- embedding/browser/webBrowser/nsWebBrowser.cpp
+++ embedding/browser/webBrowser/nsWebBrowser.cpp
@@ -1233,7 +1233,10 @@ NS_IMETHODIMP nsWebBrowser::Create()
     }
    mDocShellAsNav->SetSessionHistory(mInitInfo->sessionHistory);
 
-   if (XRE_GetProcessType() == GeckoProcessType_Default) {
+#ifdef MOZ_IPC
+   if (XRE_GetProcessType() == GeckoProcessType_Default)
+#endif
+   {
        
        rv = EnableGlobalHistory(mShouldEnableHistory);
        NS_WARN_IF_FALSE(NS_SUCCEEDED(rv), "EnableGlobalHistory() failed");
