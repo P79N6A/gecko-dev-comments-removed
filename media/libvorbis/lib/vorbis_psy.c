b09a961432f5418f77c95ae4d0d282d5d76972a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:54:55 +0000

    Bug 666672 - Fix OSX compile warnings in sydneyaudio and vorbis.  r=giles
    UltraBlame original commit: f61c120e00f2e3ef2b7aa22900c5e134b5abc1bb

--- media/libvorbis/lib/vorbis_psy.c
+++ media/libvorbis/lib/vorbis_psy.c
@@ -1154,27 +1154,28 @@ void _vp_couple_quantize_normalize(int blobno,
                 reM[j] += reA[j];
                 qeM[j] = fabs(reM[j]);
               }else{
+#if 0
                 
                 
 
 
 
 
+                float derate = (1.0 - de*((float)(j-limit+i) / (float)(n-limit)));
                 
-
-
-
-
-
-
-
+                if(reM[j]+reA[j]<0){
+                  reM[j] = - (qeM[j] = (fabs(reM[j])+fabs(reA[j]))*derate*derate);
+                }else{
+                  reM[j] =   (qeM[j] = (fabs(reM[j])+fabs(reA[j]))*derate*derate);
+                }
+#else
                 
                 if(reM[j]+reA[j]<0){
                   reM[j] = - (qeM[j] = fabs(reM[j])+fabs(reA[j]));
                 }else{
                   reM[j] =   (qeM[j] = fabs(reM[j])+fabs(reA[j]));
                 }
-
+#endif
 
               }
               reA[j]=qeA[j]=0.f;
