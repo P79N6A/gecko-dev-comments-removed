6c2312110b59b2d6871e2912f6b6576457c9258f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:44:40 +0000

    Bug 787818. Make sure the weak frame is alive after dispatching an event. r=mats
    UltraBlame original commit: f182ead52cd9b68f633349c0cfec6394ede2287d

--- layout/xul/base/src/nsXULPopupManager.cpp
+++ layout/xul/base/src/nsXULPopupManager.cpp
@@ -884,6 +884,7 @@ nsXULPopupManager::HidePopupCallback(nsIContent* aPopup,
   nsMouseEvent event(true, NS_XUL_POPUP_HIDDEN, nullptr, nsMouseEvent::eReal);
   nsEventDispatcher::Dispatch(aPopup, aPopupFrame->PresContext(),
                               &event, nullptr, &status);
+  ENSURE_TRUE(weakFrame.IsAlive());
 
   
   if (aNextPopup && aPopup != aLastPopup) {
