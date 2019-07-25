03bc865b3208ae2fc053f0422b9235bee5043c28
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:33:07 +0000

    Bug 524925 - Update the overflow for all the affected frames, not just the primary frame. And the ancestors of those.  part=2/6 r=roc
    UltraBlame original commit: 9d0e181c6aa41d15d5bba8a7ac60ca7a7e7cf8b5

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -8020,27 +8020,35 @@ nsCSSFrameConstructor::ProcessRestyledFrames(nsStyleChangeList& aChangeList)
         didInvalidate = true;
       }
       if (hint & nsChangeHint_UpdateOverflow) {
-        nsOverflowAreas overflowAreas;
-        nsOverflowAreas* pre = static_cast<nsOverflowAreas*>
-          (frame->Properties().Get(frame->PreTransformOverflowAreasProperty()));
-        if (pre) {
-          
-          
-          overflowAreas = *pre;
-        } else {
-          
-          
-          overflowAreas = frame->GetOverflowAreas();
-        }
-
-        frame->FinishAndStoreOverflow(overflowAreas, frame->GetSize());
-
-        
-        for (nsIFrame* ancestor = frame->GetParent(); ancestor;
-             ancestor = ancestor->GetParent()) {
-          if (!ancestor->UpdateOverflow()) {
-            break;
+        while (frame) {
+          nsOverflowAreas overflowAreas;
+          nsOverflowAreas* pre = static_cast<nsOverflowAreas*>
+            (frame->Properties().Get(frame->PreTransformOverflowAreasProperty()));
+          if (pre) {
+            
+            
+            overflowAreas = *pre;
+          } else {
+            
+            
+            overflowAreas = frame->GetOverflowAreas();
           }
+
+          frame->FinishAndStoreOverflow(overflowAreas, frame->GetSize());
+
+          nsIFrame* next =
+            nsLayoutUtils::GetNextContinuationOrSpecialSibling(frame);
+          
+          
+          if (!next || frame->GetParent() != next->GetParent()) {
+            for (nsIFrame* ancestor = frame->GetParent(); ancestor;
+                 ancestor = ancestor->GetParent()) {
+              if (!ancestor->UpdateOverflow()) {
+                break;
+              }
+            }
+          }
+          frame = next;
         }
       }
       if (hint & nsChangeHint_UpdateCursor) {
