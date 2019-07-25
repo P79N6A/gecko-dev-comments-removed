8185a95c6cbbe2f3b39903274f9d23e98fe7da73
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:41:13 +0000

    Bug 782659: Fix assertions about empty registers when profiling is enabled on ARM. r=bhackett
    UltraBlame original commit: 2a9b3c766512e7841b7393be8f425e7c78738997

--- js/src/methodjit/BaseAssembler.h
+++ js/src/methodjit/BaseAssembler.h
@@ -536,6 +536,9 @@ static const JSC::MacroAssembler::RegisterID JSParamReg_Argc  = JSC::MIPSRegiste
     
     
     void setupABICall(Registers::CallConvention convention, uint32_t generalArgs) {
+        if (sps && sps->enabled())
+            leaveBeforeCall();
+
         JS_ASSERT(!callIsAligned);
 
         uint32_t numArgRegs = Registers::numArgRegs(convention);
@@ -679,6 +682,33 @@ static const JSC::MacroAssembler::RegisterID JSParamReg_Argc  = JSC::MIPSRegiste
         }
     }
 
+  private:
+    
+    
+    
+    void leaveBeforeCall() {
+        if (availInCall.empty()) {
+            RegisterID reg = Registers(Registers::TempRegs).peekReg().reg();
+            saveReg(reg);
+            sps->leave(*this, reg);
+            restoreReg(reg);
+        } else {
+            sps->leave(*this, availInCall.peekReg().reg());
+        }
+    }
+
+    void reenterAfterCall() {
+        if (availInCall.empty()) {
+            RegisterID reg = Registers(Registers::TempRegs).peekReg().reg();
+            saveReg(reg);
+            sps->reenter(*this, reg);
+            restoreReg(reg);
+        } else {
+            sps->reenter(*this, availInCall.peekReg().reg());
+        }
+    }
+
+  public:
     
     
     
@@ -707,21 +737,15 @@ static const JSC::MacroAssembler::RegisterID JSParamReg_Argc  = JSC::MIPSRegiste
 
         JS_ASSERT(callIsAligned);
 
-        Call cl;
-        if (sps && sps->enabled()) {
-            RegisterID reg = availInCall.takeAnyReg().reg();
-            sps->leave(*this, reg);
-            cl = call();
-            sps->reenter(*this, reg);
-            availInCall.putReg(reg);
-        } else {
-            cl = call();
-        }
+        Call cl = call();
 
         callPatches.append(CallPatch(cl, fun));
+
 #ifdef JS_CPU_ARM
         JS_ASSERT(initFlushCount == flushCount());
 #endif
+        if (sps && sps->enabled())
+            reenterAfterCall();
         if (stackAdjust)
             addPtr(Imm32(stackAdjust), stackPointerRegister);
 
