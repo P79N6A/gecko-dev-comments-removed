4f9e347868386b22c9067dedd7234d0bd0f9d1cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:06:35 +0000

    Bug 707836 - Handle URI navigation outside app domain for native apps [r=myk] Add a content policy to detect off-origin navigation to redirect to the default browser.
    UltraBlame original commit: 0064e5064b4f6856b9b7b1286f10ad50f958f467

---
+++ webapprt/ContentPolicy.js
@@ -0,0 +1,54 @@
+
+
+
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+Cu.import("resource://webapprt/modules/WebappRT.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+
+const allowedOrigins = [
+  WebappRT.config.app.origin,
+];
+
+function ContentPolicy() {}
+
+ContentPolicy.prototype = {
+  classID: Components.ID("{75acd178-3d5a-48a7-bd92-fba383520ae6}"),
+
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy]),
+
+  shouldLoad: function(contentType, contentLocation, requestOrigin, context, mimeTypeGuess, extra) {
+    
+    
+    let {prePath, scheme} = contentLocation;
+    if (contentType == Ci.nsIContentPolicy.TYPE_DOCUMENT &&
+        !/^(about|chrome|resource)$/.test(scheme) &&
+        allowedOrigins.indexOf(prePath) == -1) {
+
+      
+      Cc["@mozilla.org/uriloader/external-protocol-service;1"].
+        getService(Ci.nsIExternalProtocolService).
+        getProtocolHandlerInfo(scheme).
+        launchWithURI(contentLocation);
+
+      
+      if (context.currentURI.spec == "about:blank") {
+        context.ownerDocument.defaultView.close();
+      };
+
+      return Ci.nsIContentPolicy.REJECT_SERVER;
+    }
+
+    return Ci.nsIContentPolicy.ACCEPT;
+  },
+
+  shouldProcess: function(contentType, contentLocation, requestOrigin, context, mimeType, extra) {
+    return Ci.nsIContentPolicy.ACCEPT;
+  },
+};
+
+const NSGetFactory = XPCOMUtils.generateNSGetFactory([ContentPolicy]);
