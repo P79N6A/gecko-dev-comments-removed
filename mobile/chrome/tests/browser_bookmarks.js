f441f2c53baa19151e4def95e68e168a796a088f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:22:31 +0000

    Bug 663596 - todo_is() the Android permaorange part of browser_bookmarks.js, r=mfinkle
    UltraBlame original commit: 1a34672f4337681d19492991724711b1a853a106

--- mobile/chrome/tests/browser_bookmarks.js
+++ mobile/chrome/tests/browser_bookmarks.js
@@ -120,7 +120,11 @@ gTests.push({
 
     
     waitForPageShow(function() {
-      is(gCurrentTest._currentTab.browser.currentURI.spec, testURL_01, "Opened the right bookmark");
+      if (Services.appinfo.OS == "Android")
+        todo_is(gCurrentTest._currentTab.browser.currentURI.spec, testURL_01, "Opened the right bookmark");
+      else
+        is(gCurrentTest._currentTab.browser.currentURI.spec, testURL_01, "Opened the right bookmark");
+
       Browser.closeTab(gCurrentTest._currentTab);
 
       runNextTest();
