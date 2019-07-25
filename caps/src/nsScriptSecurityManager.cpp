7800e0bc612ea3817430cc0c3e5b66c20472b144
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:32:12 +0000

    Backout out bug 672026 for causing mobile startup crashes
    UltraBlame original commit: 7a1c228f8f85526f1bc80fd3c8dc8d4e81967ce4

--- caps/src/nsScriptSecurityManager.cpp
+++ caps/src/nsScriptSecurityManager.cpp
@@ -3327,41 +3327,6 @@ nsScriptSecurityManager::Observe(nsISupports* aObject, const char* aTopic,
 
 
 
-
-
-
-
-
-
-NS_EXPORT JSPrincipals *
-NS_DefaultObjectPrincipalFinder(JSContext *cx, JSObject *obj)
-{
-    nsScriptSecurityManager *ssm = nsScriptSecurityManager::GetScriptSecurityManager();
-    if (!ssm) {
-        return nsnull;
-    }
-
-    nsCOMPtr<nsIPrincipal> principal;
-    nsresult rv = ssm->GetObjectPrincipal(cx, obj, getter_AddRefs(principal));
-    if (NS_FAILED(rv) || !principal) {
-        return nsnull;
-    }
-
-    JSPrincipals *jsPrincipals = nsnull;
-    principal->GetJSPrincipals(cx, &jsPrincipals);
-
-    
-    
-    
-
-    JSPRINCIPALS_DROP(cx, jsPrincipals);
-
-    return jsPrincipals;
-}
-
-
-
-
 nsScriptSecurityManager::nsScriptSecurityManager(void)
     : mOriginToPolicyMap(nsnull),
       mDefaultPolicy(nsnull),
@@ -3431,7 +3396,7 @@ nsresult nsScriptSecurityManager::Init()
     static JSSecurityCallbacks securityCallbacks = {
         CheckObjectAccess,
         NULL,
-        NS_DefaultObjectPrincipalFinder,
+        NULL,
         ContentSecurityPolicyPermitsJSAction
     };
 
