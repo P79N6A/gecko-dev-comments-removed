b1227a8c270e56d18a07d5723f85d205ecc02007
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:48:53 +0000

    Implement a Cache class which discard unused entries. (Bug 685841, r=mccr8,jimb)
    Add a WeakCache class to hold entries as long as the key or the value is live.
    
    ---
    
    UltraBlame original commit: fadc027ca1ecad3d51a15f9ef2a42934617a151b

---
+++ js/src/jsweakcache.h
@@ -0,0 +1,93 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef jscache_h___
+#define jscache_h___
+
+#include "jsapi.h"
+#include "jscntxt.h"
+#include "jsobj.h"
+#include "jsgcmark.h"
+
+namespace js {
+
+
+
+
+
+
+
+template <class Key, class Value,
+          class HashPolicy = DefaultHasher<Key>,
+          class AllocPolicy = RuntimeAllocPolicy>
+class WeakCache : public HashMap<Key, Value, HashPolicy, AllocPolicy> {
+  private:
+    typedef HashMap<Key, Value, HashPolicy, AllocPolicy> Base;
+    typedef typename Base::Range Range;
+    typedef typename Base::Enum Enum;
+
+  public:
+    explicit WeakCache(JSRuntime *rt) : Base(rt) { }
+    explicit WeakCache(JSContext *cx) : Base(cx) { }
+
+  public:
+    
+    void sweep(JSContext *cx) {
+
+        
+        for (Enum e(*this); !e.empty(); e.popFront()) {
+            if (!gc::IsMarked(cx, e.front().key) || !gc::IsMarked(cx, e.front().value))
+                e.removeFront();
+        }
+
+#if DEBUG
+        
+        
+        for (Range r = Base::all(); !r.empty(); r.popFront()) {
+            JS_ASSERT(gc::IsMarked(cx, r.front().key));
+            JS_ASSERT(gc::IsMarked(cx, r.front().value));
+        }
+#endif
+    }
+};
+
+}
+
+#endif
