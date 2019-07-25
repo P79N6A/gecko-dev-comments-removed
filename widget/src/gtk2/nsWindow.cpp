35b09b1eb38aab8ddaf10e98faefd900f8adfded
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:11:30 +0000

    b=644542 don't process expose event if the window is destroyed during WILL_PAINT r=roc
    UltraBlame original commit: 3865e976d54de27d5c54e718a4f5230f46692a79

--- widget/src/gtk2/nsWindow.cpp
+++ widget/src/gtk2/nsWindow.cpp
@@ -2136,6 +2136,11 @@ nsWindow::OnExposeEvent(GtkWidget *aWidget, GdkEventExpose *aEvent)
         nsPaintEvent willPaintEvent(PR_TRUE, NS_WILL_PAINT, this);
         willPaintEvent.willSendDidPaint = PR_TRUE;
         DispatchEvent(&willPaintEvent, status);
+
+        
+        
+        if (!mGdkWindow)
+            return TRUE;
     }
 
     nsPaintEvent event(PR_TRUE, NS_PAINT, this);
