ff6ded9d357fee7d14384c84fc988f922533ea09
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:26:48 +0000

    Bug 703239 - Fixing build warnings in editor/, r=ehsan
    UltraBlame original commit: 1f0062478a1f93231064f24b22c443d544ea3e64

--- editor/libeditor/base/nsEditorController.cpp
+++ editor/libeditor/base/nsEditorController.cpp
@@ -48,36 +48,34 @@
 #define NS_REGISTER_ONE_COMMAND(_cmdClass, _cmdName)                                      \
   {                                                                                       \
     _cmdClass* theCmd = new _cmdClass();                                                  \
-    NS_ENSURE_TRUE(theCmd, NS_ERROR_OUT_OF_MEMORY);                                           \
-    rv = inCommandTable->RegisterCommand(_cmdName,                                        \
+    NS_ENSURE_TRUE(theCmd, NS_ERROR_OUT_OF_MEMORY);                                       \
+    inCommandTable->RegisterCommand(_cmdName,                                             \
                                    static_cast<nsIControllerCommand *>(theCmd));          \
   }
 
 #define NS_REGISTER_FIRST_COMMAND(_cmdClass, _cmdName)                                    \
   {                                                                                       \
     _cmdClass* theCmd = new _cmdClass();                                                  \
-    NS_ENSURE_TRUE(theCmd, NS_ERROR_OUT_OF_MEMORY);                                           \
-    rv = inCommandTable->RegisterCommand(_cmdName,                                        \
+    NS_ENSURE_TRUE(theCmd, NS_ERROR_OUT_OF_MEMORY);                                       \
+    inCommandTable->RegisterCommand(_cmdName,                                             \
                                    static_cast<nsIControllerCommand *>(theCmd));
 
 #define NS_REGISTER_NEXT_COMMAND(_cmdClass, _cmdName)                                     \
-    rv = inCommandTable->RegisterCommand(_cmdName,                                        \
+    inCommandTable->RegisterCommand(_cmdName,                                             \
                                    static_cast<nsIControllerCommand *>(theCmd));
 
 #define NS_REGISTER_LAST_COMMAND(_cmdClass, _cmdName)                                     \
-    rv = inCommandTable->RegisterCommand(_cmdName,                                        \
+    inCommandTable->RegisterCommand(_cmdName,                                             \
                                    static_cast<nsIControllerCommand *>(theCmd));          \
   }
 
 
 
-nsresult nsEditorController::RegisterEditingCommands(nsIControllerCommandTable *inCommandTable)
+nsresult nsEditorController::RegisterEditorCommands(nsIControllerCommandTable *inCommandTable)
 {
-  nsresult rv;
- 
   
   
-  
+
   NS_REGISTER_ONE_COMMAND(nsUndoCommand, "cmd_undo");
   NS_REGISTER_ONE_COMMAND(nsRedoCommand, "cmd_redo");
   NS_REGISTER_ONE_COMMAND(nsClearUndoCommand, "cmd_clearUndo");
@@ -87,12 +85,12 @@ nsresult nsEditorController::RegisterEditingCommands(nsIControllerCommandTable *
   NS_REGISTER_ONE_COMMAND(nsCopyCommand, "cmd_copy");
   NS_REGISTER_ONE_COMMAND(nsCopyOrDeleteCommand, "cmd_copyOrDelete");
   NS_REGISTER_ONE_COMMAND(nsSelectAllCommand, "cmd_selectAll");
-  
+
   NS_REGISTER_ONE_COMMAND(nsPasteCommand, "cmd_paste");
   NS_REGISTER_ONE_COMMAND(nsPasteTransferableCommand, "cmd_pasteTransferable");
 
   NS_REGISTER_ONE_COMMAND(nsSwitchTextDirectionCommand, "cmd_switchTextDirection");
-  
+
   NS_REGISTER_FIRST_COMMAND(nsDeleteCommand, "cmd_delete");
   NS_REGISTER_NEXT_COMMAND(nsDeleteCommand, "cmd_deleteCharBackward");
   NS_REGISTER_NEXT_COMMAND(nsDeleteCommand, "cmd_deleteCharForward");
@@ -100,21 +98,6 @@ nsresult nsEditorController::RegisterEditingCommands(nsIControllerCommandTable *
   NS_REGISTER_NEXT_COMMAND(nsDeleteCommand, "cmd_deleteWordForward");
   NS_REGISTER_NEXT_COMMAND(nsDeleteCommand, "cmd_deleteToBeginningOfLine");
   NS_REGISTER_LAST_COMMAND(nsDeleteCommand, "cmd_deleteToEndOfLine");
-
-  
-  NS_REGISTER_ONE_COMMAND(nsInsertPlaintextCommand, "cmd_insertText");
-  NS_REGISTER_ONE_COMMAND(nsPasteQuotationCommand,  "cmd_pasteQuote");
-
-  return NS_OK;
-}
-
-
-
-nsresult nsEditorController::RegisterEditorCommands(nsIControllerCommandTable *inCommandTable)
-{
-  nsresult rv;
-
-  
 
   NS_REGISTER_FIRST_COMMAND(nsSelectionMoveCommands, "cmd_scrollTop");
   NS_REGISTER_NEXT_COMMAND(nsSelectionMoveCommands, "cmd_scrollBottom");
@@ -146,6 +129,11 @@ nsresult nsEditorController::RegisterEditorCommands(nsIControllerCommandTable *i
   NS_REGISTER_NEXT_COMMAND(nsSelectionMoveCommands, "cmd_movePageDown");
   NS_REGISTER_NEXT_COMMAND(nsSelectionMoveCommands, "cmd_selectPageUp");
   NS_REGISTER_LAST_COMMAND(nsSelectionMoveCommands, "cmd_selectPageDown");
+    
+  
+  NS_REGISTER_ONE_COMMAND(nsInsertPlaintextCommand, "cmd_insertText");
+  NS_REGISTER_ONE_COMMAND(nsPasteQuotationCommand,  "cmd_pasteQuote");
+
 
   return NS_OK;
 }
--- editor/libeditor/html/nsHTMLEditorStyle.cpp
+++ editor/libeditor/html/nsHTMLEditorStyle.cpp
@@ -539,8 +539,8 @@ nsresult nsHTMLEditor::SplitStyleAboveRange(nsIDOMRange *inRange,
   NS_ENSURE_TRUE(inRange, NS_ERROR_NULL_POINTER);
   nsresult res;
   nsCOMPtr<nsIDOMNode> startNode, endNode, origStartNode;
-  PRInt32 startOffset, endOffset, origStartOffset;
-  
+  PRInt32 startOffset, endOffset;
+
   res = inRange->GetStartContainer(getter_AddRefs(startNode));
   NS_ENSURE_SUCCESS(res, res);
   res = inRange->GetStartOffset(&startOffset);
@@ -549,10 +549,9 @@ nsresult nsHTMLEditor::SplitStyleAboveRange(nsIDOMRange *inRange,
   NS_ENSURE_SUCCESS(res, res);
   res = inRange->GetEndOffset(&endOffset);
   NS_ENSURE_SUCCESS(res, res);
-  
+
   origStartNode = startNode;
-  origStartOffset = startOffset;
-  
+
   
   {
     nsAutoTrackDOMPoint tracker(mRangeUpdater, address_of(endNode), &endOffset);
@@ -563,7 +562,7 @@ nsresult nsHTMLEditor::SplitStyleAboveRange(nsIDOMRange *inRange,
   
   res = SplitStyleAbovePoint(address_of(endNode), &endOffset, aProperty, aAttribute);
   NS_ENSURE_SUCCESS(res, res);
-  
+
   
   res = inRange->SetStart(startNode, startOffset);
   NS_ENSURE_SUCCESS(res, res);
@@ -666,10 +665,10 @@ nsresult nsHTMLEditor::RemoveStyleInside(nsIDOMNode *aNode,
   }
 
   
-  if ( !aChildrenOnly && 
-        (aProperty && NodeIsType(aNode, aProperty) || 
+  if ((!aChildrenOnly &&
+        ((aProperty && NodeIsType(aNode, aProperty)) || 
         (aProperty == nsEditProperty::href && nsHTMLEditUtils::IsLink(aNode)) || 
-        (aProperty == nsEditProperty::name && nsHTMLEditUtils::IsNamedAnchor(aNode))) || 
+        (aProperty == nsEditProperty::name && nsHTMLEditUtils::IsNamedAnchor(aNode)))) || 
         (!aProperty && NodeIsProperty(aNode)))  
   {
     
@@ -1048,16 +1047,13 @@ nsHTMLEditor::GetInlinePropertyBase(nsIAtom *aProperty,
       {
         nsString tString(*aAttribute); 
         nsString tOutString;
-        nsString *tPassString=nsnull;
-        if (outValue)
-            tPassString = &tOutString;
         mTypeInState->GetTypingState(isSet, theSetting, aProperty, tString, &tOutString);
         if (outValue)
           outValue->Assign(tOutString);
       }
       else
         mTypeInState->GetTypingState(isSet, theSetting, aProperty);
-      if (isSet) 
+      if (isSet)
       {
         *aFirst = *aAny = *aAll = theSetting;
         return NS_OK;
@@ -1067,7 +1063,7 @@ nsHTMLEditor::GetInlinePropertyBase(nsIAtom *aProperty,
         IsTextPropertySetByContent(collapsedNode, aProperty, aAttribute, aValue,
                                    isSet, getter_AddRefs(resultNode), outValue);
         *aFirst = *aAny = *aAll = isSet;
-        
+
         if (!isSet && aCheckDefaults) 
         {
           
