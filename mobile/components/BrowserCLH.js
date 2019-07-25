fd63db4c3f87172cfd881e383096e77ba0fb6911
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:02:30 +0000

    Bug 529576 - fennec should attempt to open remote command-lines in a tab, r=mfinkle
    UltraBlame original commit: e94404f59a64f6d5094df98a0b08f726593154ef

---
+++ mobile/components/BrowserCLH.js
@@ -0,0 +1,93 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+function BrowserCLH() { }
+
+BrowserCLH.prototype = {
+  
+  
+  
+  handle: function fs_handle(cmdLine) {
+    let win;
+    try {
+      var windowMediator =
+        Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
+      win = windowMediator.getMostRecentWindow("navigator:browser");
+      if (!win)
+        return;
+
+      win.focus();
+      cmdLine.preventDefault = true;
+    } catch (e) { }
+
+    
+    
+    for (let i = 0; i < cmdLine.length; i++) {
+      let arg = cmdLine.getArgument(i);
+      if (!arg || arg[0] == '-')
+        continue;
+
+      let uri = cmdLine.resolveURI(arg);
+      if (uri)
+        win.browserDOMWindow.openURI(uri, null, Ci.nsIBrowserDOMWindow.OPEN_NEWTAB, null);
+    }
+  },
+
+  
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsICommandLineHandler]),
+
+  
+  classDescription: "Command Line Handler",
+  contractID: "@mozilla.org/mobile/browser-clh;1",
+  classID: Components.ID("{be623d20-d305-11de-8a39-0800200c9a66}"),
+  _xpcom_categories: [{ category: "command-line-handler", entry: "m-browser" }],
+};
+
+var components = [ BrowserCLH ];
+
+function NSGetModule(compMgr, fileSpec) {
+  return XPCOMUtils.generateModule(components);
+}
