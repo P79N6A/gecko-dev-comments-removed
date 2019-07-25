a03b88079a12886b6e6cf10cb527ececec9c890d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:25:23 +0000

    Bug 742944 - Part 5: Tests for window.open in <iframe mozbrowser>. r=bz
    UltraBlame original commit: bc7286e373c9ddea9fe819473e25e5b05e91cfd3

---
+++ dom/browser-element/mochitest/browserElement_OpenWindowRejected.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+"use strict";
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addToWhitelist();
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  iframe.addEventListener('mozbrowseropenwindow', function(e) {
+    ok(e.detail.url.indexOf('does_not_exist.html') != -1,
+       'Opened URL; got ' + e.detail.url);
+    is(e.detail.name, '');
+    is(e.detail.features, '');
+
+    
+    
+  });
+
+  iframe.addEventListener('mozbrowsershowmodalprompt', function(e) {
+    var msg = e.detail.message;
+    if (msg.indexOf('success:') == 0) {
+      ok(true, msg);
+    }
+    else if (msg == 'finish') {
+      SimpleTest.finish();
+    }
+    else {
+      ok(false, msg);
+    }
+  });
+
+  iframe.src = 'file_browserElement_OpenWindowRejected.html';
+  document.body.appendChild(iframe);
+}
+
+runTest();
---
+++ dom/browser-element/mochitest/browserElement_OpenWindowRejected2.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+"use strict";
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addToWhitelist();
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  iframe.addEventListener('mozbrowseropenwindow', function(e) {
+    ok(e.detail.url.indexOf('does_not_exist.html') != -1,
+       'Opened URL; got ' + e.detail.url);
+    is(e.detail.name, '');
+    is(e.detail.features, '');
+
+    
+    
+    e.preventDefault();
+  });
+
+  iframe.addEventListener('mozbrowsershowmodalprompt', function(e) {
+    var msg = e.detail.message;
+    if (msg.indexOf('success:') == 0) {
+      ok(true, msg);
+    }
+    else if (msg == 'finish') {
+      SimpleTest.finish();
+    }
+    else {
+      ok(false, msg);
+    }
+  });
+
+  iframe.src = 'file_browserElement_OpenWindowRejected.html';
+}
+
+runTest();
