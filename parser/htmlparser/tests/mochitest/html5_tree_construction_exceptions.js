ad3c2bf17f0aed9362b8db761176ee8cf6225ca9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:49:25 +0000

    Bug 606925 - Get rid of the "in foreign content" insertion mode. Check the namespace of the current node instead. rs=jonas, a=blocking2.0-betaN.
    UltraBlame original commit: a6ad532d8bf87882ebaabf723d566ba1a1ea5fc1

--- parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
+++ parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
@@ -7,7 +7,6 @@
 var html5Exceptions = {
   "<!doctype html><keygen><frameset>" : true, 
   "<select><keygen>" : true, 
-  "<math><mi><div><object><div><span></span></div></object></div></mi><mi>" : true, 
   "<plaintext>\u0000filler\u0000text\u0000" : true, 
   "<body><svg><foreignObject>\u0000filler\u0000text" : true, 
   "<svg>\u0000</svg><frameset>" : true, 
