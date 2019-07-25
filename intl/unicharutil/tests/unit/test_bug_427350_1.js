5ba7be91fdd321aa928bb0984152996d5da653bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:22:11 +0000

    Backout changeset 8cbe83542596 due to test failures
    UltraBlame original commit: dcfe02c945662fa5e82cac5ddb25b1bdcfcb38bc

--- intl/unicharutil/tests/unit/test_bug_427350_1.js
+++ intl/unicharutil/tests/unit/test_bug_427350_1.js
@@ -16,13 +16,13 @@ const inMalayalamNumbers = "\u0D70\u0D71\u0D72\u0D73\u0D74\u0D75";
 
 const inDigits = "\u1091\u1BB2\u1C43\u1C54\uA625\uA8D6\uA907\uAA58\uA9D9\uABF0";
 const inRomanNumerals = "\u2185\u2186\u2187\u2188";
-const inSuperSubscripts = "\u2C7C\u2C7D\u2095\u209C";
+const inSuperSubscripts = "\u2C7C\u2C7D";
     
 const expectedTeluguFractions = "[0][1][2][3][1][2][3]";
 const expectedMalayalamNumbers = "[10][100][1000][1/4][1/2][3/4]";
 const expectedDigits = "1234567890";
 const expectedRomanNumerals = "[6][50][50000][100000]";
-const expectedSuperSubscripts = "v(j)^(V)v(h)v(t)";
+const expectedSuperSubscripts = "v(j)^(V)";
 
 const EntityAfterCharsetConv = 512;
 const transliterate = 8;
