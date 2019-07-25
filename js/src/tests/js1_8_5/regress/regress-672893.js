5931c7cce9bbca9f70652927f1c235905362a7d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:50:34 +0000

    Bug 676343 - Lexical scoping bug involving UPVAR_LEVEL_LIMIT. r=brendan.
    UltraBlame original commit: 1a29139e6bafb68172bebb92f4208762aa939367

---
+++ js/src/tests/js1_8_5/regress/regress-672893.js
@@ -0,0 +1,18 @@
+
+
+
+function f() {
+    return function () { return function () { return function () {
+    return function () { return function () { return function () {
+    return function () { return function () { return function () {
+    return function () { return function () { return function () {
+    return function () { return function () { return function (a) {
+        var v = a;
+	assertEq(v, 42);
+	return function() { return v; };
+    }; }; }; }; }; }; }; }; }; }; }; }; }; }; };
+};
+
+assertEq(f()()()()()()()()()()()()()()()(42)(), 42);
+
+reportCompare(0, 0, 'ok');
