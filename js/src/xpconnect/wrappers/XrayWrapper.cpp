5a653d3168a36bacae64628441af64a18b80073a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:01:37 +0000

    Bug 604476 - Do a full property resolve when enumerating. r=gal
    UltraBlame original commit: 41690f030f9a2bef197cdf381b26250f18000e3b

--- js/src/xpconnect/wrappers/XrayWrapper.cpp
+++ js/src/xpconnect/wrappers/XrayWrapper.cpp
@@ -415,7 +415,49 @@ Transparent(JSContext *cx, JSObject *wrapper)
             return true;
     }
 
-    return AccessCheck::documentDomainMakesSameOrigin(cx, wrapper->unwrap());
+    JSObject *scope = nsnull;
+    JSStackFrame *fp = nsnull;
+    JS_FrameIterator(cx, &fp);
+    if (fp) {
+        while (fp->isDummyFrame()) {
+            if (!JS_FrameIterator(cx, &fp))
+                break;
+        }
+
+        if (fp)
+            scope = &fp->scopeChain();
+    }
+
+    if (!scope)
+        scope = JS_GetScopeChain(cx);
+
+    nsIPrincipal *subject;
+    nsIPrincipal *object;
+
+    nsIXPConnect *xpc = nsXPConnect::GetXPConnect();
+    {
+        JSAutoEnterCompartment ac;
+
+        if (!ac.enter(cx, scope))
+            return false;
+
+        subject = xpc->GetPrincipal(JS_GetGlobalForObject(cx, scope), PR_TRUE);
+    }
+
+    {
+        JSAutoEnterCompartment ac;
+
+        JSObject *obj = wrapper->unwrap();
+        if (!ac.enter(cx, obj))
+            return false;
+
+        object = xpc->GetPrincipal(JS_GetGlobalForObject(cx, obj), PR_TRUE);
+    }
+
+    PRBool subsumes;
+    if (NS_SUCCEEDED(subject->Subsumes(object, &subsumes)) && subsumes)
+        return true;
+    return false;
 }
 
 namespace XrayUtils {
@@ -697,10 +739,10 @@ XrayWrapper<Base, Policy>::enumerate(JSContext *cx, JSObject *wrapper, js::AutoI
     
     for (size_t n = 0; n < wnProps.length(); ++n) {
         jsid id = wnProps[n];
-        JSPropertyDescriptor dummy;
-        if (!ResolveNativeProperty(cx, wrapper, holder, id, false, &dummy))
+        JSBool hasProp;
+        if (!JS_HasPropertyById(cx, wrapper, id, &hasProp))
             return false;
-        if (dummy.obj)
+        if (hasProp)
             props.append(id);
     }
     return true;
