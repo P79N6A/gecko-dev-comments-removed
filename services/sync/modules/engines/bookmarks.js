d07814fa4140ac83591e7d22b4babd8d32eaf9a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:44:12 +0000

    Bug 615410 - Use different annotations for parent and predecessor. r=mconnor
    The old annotations will no longer be valid because they point to a different kind of GUID.
    Since we're using our own GUID system now, we also don't have to munge predecessor and parent GUIDs before setting them.
    
    UltraBlame original commit: 81d8bf53ab01179d684cb78c4384c96eaa5b01fd

--- services/sync/modules/engines/bookmarks.js
+++ services/sync/modules/engines/bookmarks.js
@@ -44,8 +44,8 @@ const Ci = Components.interfaces;
 const Cu = Components.utils;
 
 const GUID_ANNO = "sync/guid";
-const PARENT_ANNO = "weave/parent";
-const PREDECESSOR_ANNO = "weave/predecessor";
+const PARENT_ANNO = "sync/parent";
+const PREDECESSOR_ANNO = "sync/predecessor";
 const SERVICE_NOT_SUPPORTED = "Service not supported on this platform";
 const FOLDER_SORTINDEX = 1000000;
 
@@ -404,17 +404,15 @@ BookmarksStore.prototype = {
         this._reparentOrphans(itemId);
 
       
-      
       if (record._orphan)
-        Utils.anno(itemId, PARENT_ANNO, "T" + parentGUID);
+        Utils.anno(itemId, PARENT_ANNO, parentGUID);
       
       else
         this._attachFollowers(itemId);
 
       
-      
       if (predGUID != null && record._insertPos == Svc.Bookmark.DEFAULT_INDEX)
-        Utils.anno(itemId, PREDECESSOR_ANNO, "R" + predGUID);
+        Utils.anno(itemId, PREDECESSOR_ANNO, predGUID);
     }
   },
 
@@ -422,13 +420,6 @@ BookmarksStore.prototype = {
 
 
   _findAnnoItems: function BStore__findAnnoItems(anno, val) {
-    
-    if (anno == PARENT_ANNO)
-      val = "T" + val;
-    
-    else if (anno == PREDECESSOR_ANNO)
-      val = "R" + val;
-
     return Svc.Annos.getItemsWithAnnotation(anno, {}).filter(function(id)
       Utils.anno(id, anno) == val);
   },
@@ -687,10 +678,10 @@ BookmarksStore.prototype = {
 
     
     this._findAnnoItems(PARENT_ANNO, oldID).forEach(function(itemId) {
-      Utils.anno(itemId, PARENT_ANNO, "T" + newID);
+      Utils.anno(itemId, PARENT_ANNO, newID);
     }, this);
     this._findAnnoItems(PREDECESSOR_ANNO, oldID).forEach(function(itemId) {
-      Utils.anno(itemId, PREDECESSOR_ANNO, "R" + newID);
+      Utils.anno(itemId, PREDECESSOR_ANNO, newID);
     }, this);
 
     
@@ -992,8 +983,7 @@ BookmarksStore.prototype = {
   _getParentGUIDForId: function BStore__getParentGUIDForId(itemId) {
     
     try {
-      
-      return Utils.anno(itemId, PARENT_ANNO).slice(1);
+      return Utils.anno(itemId, PARENT_ANNO);
     }
     catch(ex) {}
 
@@ -1009,8 +999,7 @@ BookmarksStore.prototype = {
   _getPredecessorGUIDForId: function BStore__getPredecessorGUIDForId(itemId) {
     
     try {
-      
-      return Utils.anno(itemId, PREDECESSOR_ANNO).slice(1);
+      return Utils.anno(itemId, PREDECESSOR_ANNO);
     }
     catch(ex) {}
 
