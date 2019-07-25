cf6ac03a4802a772fbaa0b33219f95716ac2bbb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 06:59:34 +0000

    Bug 542928 - Add some more debugging logs to waitForFocus in order to track down the browser-chrome random orange; r=enndeakin
    UltraBlame original commit: 63aab4bb09a94d6ae64d5fe38a1080344b12c429

--- testing/mochitest/tests/SimpleTest/SimpleTest.js
+++ testing/mochitest/tests/SimpleTest/SimpleTest.js
@@ -298,15 +298,22 @@ SimpleTest.waitForFocus = function (callback, targetWindow, expectBlankPage) {
     }
 
     function waitForEvent(event) {
-        
-        
-        if (event.type == "load" && (expectBlankPage != (event.target.location == "about:blank")))
-            return;
+        try {
+            debugFocusLog("waitForEvent called <type:" + event.type + ", target" + event.target + ">");
 
-        SimpleTest["waitForFocus_" + event.type + "ed"] = true;
-        var win = (event.type == "load") ? targetWindow : childTargetWindow;
-        win.removeEventListener(event.type, waitForEvent, true);
-        maybeRunTests();
+            
+            
+            if (event.type == "load" && (expectBlankPage != (event.target.location == "about:blank")))
+                return;
+
+            SimpleTest["waitForFocus_" + event.type + "ed"] = true;
+            var win = (event.type == "load") ? targetWindow : childTargetWindow;
+            win.removeEventListener(event.type, waitForEvent, true);
+            maybeRunTests();
+        } catch (e) {
+            SimpleTest.ok(false, "Exception caught in waitForEvent: " + e.message +
+                ", at: " + e.fileName + " (" + e.lineNumber + ")");
+        }
     }
 
     
