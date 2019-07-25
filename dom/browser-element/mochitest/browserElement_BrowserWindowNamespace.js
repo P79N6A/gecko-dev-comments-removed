586f5715aedb453856317903e592eff2a53055bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:39:14 +0000

    Bug 780351 - Tests for ensuring that mozapp divides the window name namespace, but mozbrowser does not. r=smaug
    UltraBlame original commit: e2e23255b6bf52669339e2d07444d7eef453846d

---
+++ dom/browser-element/mochitest/browserElement_BrowserWindowNamespace.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+"use strict";
+
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addToWhitelist();
+
+  var iframe1 = document.createElement('iframe');
+  iframe1.mozbrowser = true;
+
+  
+  
+  
+
+  iframe1.addEventListener('mozbrowseropenwindow', function(e) {
+    ok(true, "Got first mozbrowseropenwindow event.");
+    document.body.appendChild(e.detail.frameElement);
+
+    e.detail.frameElement.addEventListener('mozbrowserlocationchange', function(e) {
+      if (e.detail == "http://example.com/#2") {
+        ok(true, "Got locationchange to http://example.com/#2");
+        SimpleTest.finish();
+      }
+      else {
+        ok(true, "Got locationchange to " + e.detail);
+      }
+    });
+
+    SimpleTest.executeSoon(function() {
+      var iframe2 = document.createElement('iframe');
+      iframe2.mozbrowser = true;
+
+      iframe2.addEventListener('mozbrowseropenwindow', function(e) {
+        ok(false, "Got second mozbrowseropenwindow event.");
+      });
+
+      document.body.appendChild(iframe2);
+      iframe2.src = 'file_browserElement_BrowserWindowNamespace.html#2';
+    });
+  });
+
+  document.body.appendChild(iframe1);
+  iframe1.src = 'file_browserElement_BrowserWindowNamespace.html#1';
+}
+
+runTest();
