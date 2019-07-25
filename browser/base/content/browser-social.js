d9e6a4466b5f80f8156c4b893b771514eb1ab46a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:45:21 +0000

    Bug 787272. Disable recommend button on logout. r=jaws
    UltraBlame original commit: ec01a56ff70d64a41358e64e185cfe84f3b3c960

--- browser/base/content/browser-social.js
+++ browser/base/content/browser-social.js
@@ -308,6 +308,8 @@ let SocialShareButton = {
   updateProfileInfo: function SSB_updateProfileInfo() {
     let profileRow = document.getElementById("editSharePopupHeader");
     let profile = Social.provider.profile;
+    this.promptImages = null;
+    this.promptMessages = null;
     if (profile && profile.displayName) {
       profileRow.hidden = false;
       let portrait = document.getElementById("socialUserPortrait");
@@ -316,12 +318,12 @@ let SocialShareButton = {
       displayName.setAttribute("label", profile.displayName);
     } else {
       profileRow.hidden = true;
+      this.updateButtonHiddenState();
+      return;
     }
     
     
     
-    this.promptImages = null;
-    this.promptMessages = null;
     
     let port = Social.provider._getWorkerPort();
     if (port) {
@@ -388,7 +390,8 @@ let SocialShareButton = {
   updateButtonHiddenState: function SSB_updateButtonHiddenState() {
     let shareButton = this.shareButton;
     if (shareButton)
-      shareButton.hidden = !Social.uiVisible || this.promptImages == null;
+      shareButton.hidden = !Social.uiVisible || this.promptImages == null ||
+                           !Social.provider.profile || !Social.provider.profile.userName;
   },
 
   onClick: function SSB_onClick(aEvent) {
