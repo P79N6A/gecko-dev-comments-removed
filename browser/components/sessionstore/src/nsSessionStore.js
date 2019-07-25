139f8d90ec8b5b476fff735a0612f6d243489838
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:25:26 +0000

    Bug 625016 - Restore windows closed in succession to quit (non-OSX only) [r=dietrich]
    UltraBlame original commit: be404c4dd873fd2a95cbe81365135178c96b8a70

--- browser/components/sessionstore/src/nsSessionStore.js
+++ browser/components/sessionstore/src/nsSessionStore.js
@@ -115,7 +115,8 @@ const CAPABILITIES = [
 
 
 
-const INTERNAL_KEYS = ["_tabStillLoading", "_hosts", "_formDataSaved"];
+const INTERNAL_KEYS = ["_tabStillLoading", "_hosts", "_formDataSaved",
+                       "_shouldRestore"];
 
 
 const TAB_EVENTS = ["TabOpen", "TabClose", "TabSelect", "TabShow", "TabHide",
@@ -524,6 +525,8 @@ SessionStoreService.prototype = {
       
       if ("_stateBackup" in this)
         delete this._stateBackup;
+
+      this._clearRestoringWindows();
       break;
     case "browser:purge-domain-data":
       
@@ -574,6 +577,8 @@ SessionStoreService.prototype = {
       }
       if (this._loadState == STATE_RUNNING)
         this.saveState(true);
+
+      this._clearRestoringWindows();
       break;
     case "nsPref:changed": 
       switch (aData) {
@@ -649,6 +654,8 @@ SessionStoreService.prototype = {
         delete this._stateBackup;
         break;
       }
+
+      this._clearRestoringWindows();
       break;
     case "private-browsing-change-granted":
       if (aData == "enter") {
@@ -661,6 +668,8 @@ SessionStoreService.prototype = {
       
       
       this._resetRestoringState();
+
+      this._clearRestoringWindows();
       break;
     }
   },
@@ -711,6 +720,8 @@ SessionStoreService.prototype = {
         this.saveStateDelayed(win);
         break;
     }
+
+    this._clearRestoringWindows();
   },
 
   
@@ -910,7 +921,13 @@ SessionStoreService.prototype = {
                                                   tabbrowser.selectedTab);
         this._updateCookies([winData]);
       }
+
+#ifndef XP_MACOSX
       
+      
+      winData._shouldRestore = true;
+#endif
+
       
       if (winData.tabs.length > 1 ||
           (winData.tabs.length == 1 && this._shouldSaveTabState(winData.tabs[0]))) {
@@ -3385,6 +3402,22 @@ SessionStoreService.prototype = {
     if (!oState)
       return;
 
+#ifndef XP_MACOSX
+    
+    
+    
+    while (oState._closedWindows.length) {
+      let i = oState._closedWindows.length - 1;
+      if (oState._closedWindows[i]._shouldRestore) {
+        oState.windows.unshift(oState._closedWindows.pop());
+      }
+      else {
+        
+        break;
+      }
+    }
+#endif
+
     if (pinnedOnly) {
       
       
@@ -3971,6 +4004,12 @@ SessionStoreService.prototype = {
       spliceTo = normalWindowIndex + 1;
 #endif
     this._closedWindows.splice(spliceTo);
+  },
+
+  _clearRestoringWindows: function sss__clearRestoringWindows() {
+    for (let i = 0; i < this._closedWindows.length; i++) {
+      delete this._closedWindows[i]._shouldRestore;
+    }
   },
 
   
