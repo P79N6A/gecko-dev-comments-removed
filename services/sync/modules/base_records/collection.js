ca33504b92ac5ec621fc4e3e5f7a767547d39c4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:21:21 +0000

    collection class
    UltraBlame original commit: c21ebe6983698b18dda0ad3cee6c86638c3acc90

---
+++ services/sync/modules/base_records/collection.js
@@ -0,0 +1,152 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const EXPORTED_SYMBOLS = ['Collection'];
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+Cu.import("resource://weave/Observers.js");
+Cu.import("resource://weave/Preferences.js");
+Cu.import("resource://weave/log4moz.js");
+Cu.import("resource://weave/constants.js");
+Cu.import("resource://weave/util.js");
+Cu.import("resource://weave/async.js");
+Cu.import("resource://weave/crypto.js");
+Cu.import("resource://weave/resource.js");
+Cu.import("resource://weave/base_records/wbo.js");
+Cu.import("resource://weave/base_records/crypto.js");
+Cu.import("resource://weave/base_records/keys.js");
+
+Function.prototype.async = Async.sugar;
+
+function Collection(uri, authenticator) {
+  this._Coll_init(uri, authenticator);
+}
+Collection.prototype = {
+  __proto__: Resource.prototype,
+  _logName: "Collection",
+
+  _Coll_init: function Coll_init(uri, authenticator) {
+    this._init(uri, authenticator);
+    this.pushFilter(new JsonFilter());
+    this._full = true;
+    this._modified = 0;
+    this._data = [];
+  },
+
+  _rebuildURL: function Coll__rebuildURL() {
+    
+    this.uri.QueryInterface(Ci.nsIURL);
+
+    let args = [];
+    if (this.modified)
+      args.push('modified=' + this.modified);
+    if (this.full)
+      args.push('full=1');
+
+    this.uri.query = (args.length > 0)? '?' + args.join('&') : '';
+  },
+
+  
+  get full() { return this._full; },
+  set full(value) {
+    this._full = value;
+    this._rebuildURL();
+  },
+
+  
+  get modified() { return this._modified; },
+  set modified(value) {
+    this._modified = value;
+    this._rebuildURL();
+  },
+
+  get iter() {
+    if (!this._iter)
+      this._iter = new CollectionIterator(this);
+    return this._iter;
+  },
+
+  get _json() {
+    let json = Cc["@mozilla.org/dom/json;1"].createInstance(Ci.nsIJSON);
+    this.__defineGetter__("_json", function() json);
+    return this._json;
+  },
+
+  pushRecord: function Coll_pushRecord(onComplete, record) {
+    let fn = function(record) {
+      let self = yield;
+      yield record.filterUpload(self.cb); 
+      this._data.push(this._json.decode(record.data)); 
+      self.done();
+    };
+    fn.async(this, onComplete, record);
+  },
+
+  clearRecords: function Coll_clearRecords() {
+    this._data = [];
+  }
+};
+
+
+
+function CollectionIterator(coll) {
+  this._init(coll);
+}
+CollectionIterator.prototype = {
+  _init: function CollIter__init(coll) {
+    this._coll = coll;
+    this._idx = 0;
+  },
+
+  get count() { return this._coll.data.length; },
+
+  next: function CollIter_next() {
+    if (this._idx >= this.count)
+      return null;
+    let item = this._coll.data[this._idx++];
+    let wrap = new CryptoWrapper(this._coll.uri.resolve(item.id));
+    wrap.data = item;
+    return wrap;
+  },
+
+  reset: function CollIter_reset() {
+    this._idx = 0;
+  }
+};
