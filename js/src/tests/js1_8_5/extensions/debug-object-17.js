bf89ab15c1e4376504b759521b10785afe3a48fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:37:13 +0000

    Add Debug.prototype.enabled. This implementation is not ideal, since we want clearing .enabled to remove the debugger from every place where it might cause overhead. For now it just sets a flag.
    UltraBlame original commit: 92309cf1aca1be0775b411fe2bb91c0c0edacb89

---
+++ js/src/tests/js1_8_5/extensions/debug-object-17.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+var g = newGlobal('new-compartment');
+var log;
+
+var arr = [];
+for (var i = 0; i < 4; i++) {
+    arr[i] = new Debug(g);
+    arr[i].hooks = {
+        num: i,
+        debuggerHandler: function () {
+            log += this.num;
+            
+            for (var j = 0; j < arr.length; j++)
+                arr[j].enabled = false;
+        }
+    };
+}
+
+log = '';
+g.eval("debugger; debugger;");
+assertEq(log, '0');
+
+reportCompare(0, 0, 'ok');
