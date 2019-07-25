cda0c30966b597f15fc89243fbdc91470e5843c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:29:15 +0000

    Bug 715582 - Send quit-application in browser/components/privatebrowsing/test/unit. r=mak.
    UltraBlame original commit: 5ca40c028ae5f2f6f7aec3ea2195a90422ccd2ba

--- browser/components/privatebrowsing/test/unit/test_privatebrowsingwrapper_removeDataFromDomain.js
+++ browser/components/privatebrowsing/test/unit/test_privatebrowsingwrapper_removeDataFromDomain.js
@@ -42,8 +42,13 @@
 
 
 
+Components.utils.import("resource://gre/modules/Services.jsm");
+
 function run_test() {
   PRIVATEBROWSING_CONTRACT_ID = "@mozilla.org/privatebrowsing-wrapper;1";
   load("do_test_removeDataFromDomain.js");
   do_test();
+
+  
+  Services.obs.notifyObservers(null, "quit-application", null);
 }
--- browser/components/privatebrowsing/test/unit/test_privatebrowsingwrapper_removeDataFromDomain_activeDownloads.js
+++ browser/components/privatebrowsing/test/unit/test_privatebrowsingwrapper_removeDataFromDomain_activeDownloads.js
@@ -42,8 +42,13 @@
 
 
 
+Components.utils.import("resource://gre/modules/Services.jsm");
+
 function run_test() {
   PRIVATEBROWSING_CONTRACT_ID = "@mozilla.org/privatebrowsing;1";
   load("do_test_removeDataFromDomain_activeDownloads.js");
   do_test();
+
+  
+  Services.obs.notifyObservers(null, "quit-application", null);
 }
