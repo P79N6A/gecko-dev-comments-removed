4eab4f9242546c0f787422a947e8e36299830b64
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:08:44 +0000

    Backout 91eaf74fff1e.
    UltraBlame original commit: 101c3e93adec42efe58bb60022db7d9326cd5301

--- js/src/assembler/wtf/Platform.h
+++ js/src/assembler/wtf/Platform.h
@@ -159,7 +159,7 @@
 
 
 #if WTF_CPU_SPARC32 || WTF_CPU_SPARC64
-#define WTF_CPU_SPARC 1
+#define WTF_CPU_SPARC
 #endif
 
 
@@ -857,8 +857,6 @@
 
 #elif WTF_CPU_X86 && WTF_PLATFORM_WIN
     #define ENABLE_JIT 1
-#elif WTF_CPU_SPARC
-    #define ENABLE_JIT 1
 #endif
 
 #if WTF_PLATFORM_QT
@@ -922,7 +920,6 @@
 
 #if (WTF_CPU_X86 \
  || WTF_CPU_X86_64 \
- || WTF_CPU_SPARC \
  || WTF_CPU_ARM_TRADITIONAL \
  || WTF_CPU_ARM_THUMB2 \
  || WTF_CPU_X86)
