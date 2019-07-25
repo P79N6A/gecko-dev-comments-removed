f41eb9c6373b4a409213b43d391aa3857317dec1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:48:11 +0000

    Bug 677044. Backout 67044ea27b53
    This breaks existing animated gifs.
    
    UltraBlame original commit: d2cd9f583b0a1cf646860572fc50024e4bf51337

--- modules/libpr0n/decoders/nsGIFDecoder2.cpp
+++ modules/libpr0n/decoders/nsGIFDecoder2.cpp
@@ -1027,13 +1027,26 @@ nsGIFDecoder2::WriteInternal(const char *aBuffer, PRUint32 aCount)
 
     case gif_sub_block:
       mGIFStruct.count = *q;
-      
-      
-      
-      if (mGIFStruct.count && mGIFStruct.rows_remaining) {
+      if (mGIFStruct.count) {
+        
+        
+        
+        if (!mGIFStruct.rows_remaining) {
+#ifdef DONT_TOLERATE_BROKEN_GIFS
+          mGIFStruct.state = gif_error;
+          break;
+#else
+          
+          GETN(1, gif_sub_block);
+#endif
+          if (mGIFStruct.count == GIF_TRAILER) {
+            
+            GETN(1, gif_done);
+            break;
+          }
+        }
         GETN(mGIFStruct.count, gif_lzw);
       } else {
-        
         
         EndImageFrame();
         GETN(1, gif_image_start);
