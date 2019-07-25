21fa25e1da3f458dc6fc703819f158e183c5135f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:14:33 +0000

    Bug 626500 - Part 2: Test adjustments; r=dao a=blocking-final+
    UltraBlame original commit: 64c98e4e1797a06d69d6b9aa55b61bb6ca6b54aa

--- browser/base/content/test/tabview/browser_tabview_launch.js
+++ browser/base/content/test/tabview/browser_tabview_launch.js
@@ -47,8 +47,9 @@ function test() {
   
   window.addEventListener("tabviewshown", onTabViewLoadedAndShown, false);
   let button = document.getElementById("tabview-button");
-  ok(button, "Tab View button exists");
-  button.doCommand();
+  ok(!button, "Tab View button not exist by default");
+  let tabViewCommand = document.getElementById("Browser:ToggleTabView");
+  tabViewCommand.doCommand();
 }
 
 
