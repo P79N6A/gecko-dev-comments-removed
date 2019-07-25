069e3970e50cff5729fa075cc178d7e13e5bee61
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:29:50 +0000

    Bug 714787 - When clearing site permissions, also clear content prefs that keep track of how many times a permission was granted. r=dougt
    UltraBlame original commit: 3c05c5afca65c4ef85894157a9dc95613c137f02

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -3713,6 +3713,8 @@ var PermissionsHelper = {
       Services.logins.setLoginSavingEnabled(aURI.prePath, true);
     } else {
       Services.perms.remove(aURI.host, aType);
+      
+      Services.contentPrefs.removePref(aURI, aType + ".request.remember");
     }
   }
 }
