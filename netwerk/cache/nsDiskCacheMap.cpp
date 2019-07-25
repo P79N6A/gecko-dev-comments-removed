877488f459de8a8a8fc7b72e49e270c305625daa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:38:12 +0000

    Bug 746920 - Uninitialised value use in nsDiskCacheMap::FlushRecords, r=hurley
    UltraBlame original commit: 5fe07a3a84589bf244e4c27fb72cc7d2bb1ac4f3

--- netwerk/cache/nsDiskCacheMap.cpp
+++ netwerk/cache/nsDiskCacheMap.cpp
@@ -369,16 +369,18 @@ nsDiskCacheMap::ShrinkRecords()
     PRUint32 newRecordsPerBucket = oldRecordsPerBucket;
     while (maxUsage < (newRecordsPerBucket >> 1))
         newRecordsPerBucket >>= 1;
-    if (newRecordsPerBucket < kMinRecordCount) 
-        newRecordsPerBucket = kMinRecordCount;
+    if (newRecordsPerBucket < (kMinRecordCount / kBuckets))
+        newRecordsPerBucket = (kMinRecordCount / kBuckets);
+    NS_ASSERTION(newRecordsPerBucket <= oldRecordsPerBucket,
+                 "ShrinkRecords() can't grow records!");
     if (newRecordsPerBucket == oldRecordsPerBucket)
         return NS_OK;
     
-    for (bucketIndex = 0; bucketIndex < kBuckets; ++bucketIndex) {
+    for (bucketIndex = 1; bucketIndex < kBuckets; ++bucketIndex) {
         
         memmove(mRecordArray + bucketIndex * newRecordsPerBucket,
                 mRecordArray + bucketIndex * oldRecordsPerBucket,
-                mHeader.mBucketUsage[bucketIndex] * sizeof(nsDiskCacheRecord));
+                newRecordsPerBucket * sizeof(nsDiskCacheRecord));
     }
 
     
