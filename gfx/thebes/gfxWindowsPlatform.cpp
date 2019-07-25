77f219f74d5c3817fc736601cac34e20329d3c75
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:24:35 +0000

    Bug 663784 - Don't use using statement before includes r=masayuki
    UltraBlame original commit: 1fc165fdfa0717c47aa81343d57b430330384e6a

--- gfx/thebes/gfxWindowsPlatform.cpp
+++ gfx/thebes/gfxWindowsPlatform.cpp
@@ -59,17 +59,27 @@
 
 #include "gfxCrashReporterUtils.h"
 
+#ifdef MOZ_FT2_FONTS
+#include "ft2build.h"
+#include FT_FREETYPE_H
+#include "gfxFT2Fonts.h"
+#include "gfxFT2FontList.h"
+#include "cairo-ft.h"
+#include "nsAppDirectoryServiceDefs.h"
+#else
 #include "gfxGDIFontList.h"
 #include "gfxGDIFont.h"
-
 #ifdef CAIRO_HAS_DWRITE_FONT
 #include "gfxDWriteFontList.h"
 #include "gfxDWriteFonts.h"
 #include "gfxDWriteCommon.h"
 #include <dwrite.h>
 #endif
+#endif
 
-using namespace mozilla;
+#include "gfxUserFontSet.h"
+
+#include <string>
 
 #ifdef CAIRO_HAS_D2D_SURFACE
 #include "gfxD2DSurface.h"
@@ -78,7 +88,11 @@ using namespace mozilla;
 
 #include "nsIMemoryReporter.h"
 #include "nsMemory.h"
+#endif
 
+using namespace mozilla;
+
+#ifdef CAIRO_HAS_D2D_SURFACE
 class D2DCacheReporter :
     public nsIMemoryReporter
 {
@@ -160,10 +174,6 @@ public:
 NS_IMPL_ISUPPORTS1(D2DVRAMReporter, nsIMemoryReporter)
 #endif
 
-#include "gfxUserFontSet.h"
-
-#include <string>
-
 #define GFX_USE_CLEARTYPE_ALWAYS "gfx.font_rendering.cleartype.always_use_for_content"
 #define GFX_DOWNLOADABLE_FONTS_USE_CLEARTYPE "gfx.font_rendering.cleartype.use_for_downloadable_fonts"
 
@@ -173,6 +183,10 @@ NS_IMPL_ISUPPORTS1(D2DVRAMReporter, nsIMemoryReporter)
 #define GFX_CLEARTYPE_PARAMS_LEVEL     "gfx.font_rendering.cleartype_params.cleartype_level"
 #define GFX_CLEARTYPE_PARAMS_STRUCTURE "gfx.font_rendering.cleartype_params.pixel_structure"
 #define GFX_CLEARTYPE_PARAMS_MODE      "gfx.font_rendering.cleartype_params.rendering_mode"
+
+#ifdef MOZ_FT2_FONTS
+static FT_Library gPlatformFTLibrary = NULL;
+#endif
 
 #ifdef CAIRO_HAS_DWRITE_FONT
 
@@ -219,6 +233,10 @@ gfxWindowsPlatform::gfxWindowsPlatform()
     CoInitialize(NULL); 
 
     mScreenDC = GetDC(NULL);
+
+#ifdef MOZ_FT2_FONTS
+    FT_Init_FreeType(&gPlatformFTLibrary);
+#endif
 
 #ifdef CAIRO_HAS_D2D_SURFACE
     NS_RegisterMemoryReporter(new D2DCacheReporter());
@@ -433,6 +451,9 @@ gfxWindowsPlatform::CreatePlatformFontList()
 {
     mUsingGDIFonts = PR_FALSE;
     gfxPlatformFontList *pfl;
+#ifdef MOZ_FT2_FONTS
+    pfl = new gfxFT2FontList();
+#else
 #ifdef CAIRO_HAS_DWRITE_FONT
     if (AllowDirectWrite() && GetDWriteFactory()) {
         pfl = new gfxDWriteFontList();
@@ -448,6 +469,7 @@ gfxWindowsPlatform::CreatePlatformFontList()
 #endif
     pfl = new gfxGDIFontList();
     mUsingGDIFonts = PR_TRUE;
+#endif
 
     if (NS_SUCCEEDED(pfl->InitFontList())) {
         return pfl;
@@ -548,7 +570,11 @@ gfxWindowsPlatform::CreateFontGroup(const nsAString &aFamilies,
                                     const gfxFontStyle *aStyle,
                                     gfxUserFontSet *aUserFontSet)
 {
+#ifdef MOZ_FT2_FONTS
+    return new gfxFT2FontGroup(aFamilies, aStyle);
+#else
     return new gfxFontGroup(aFamilies, aStyle, aUserFontSet);
+#endif
 }
 
 gfxFontEntry* 
@@ -611,6 +637,7 @@ gfxWindowsPlatform::FindFontEntry(const nsAString& aName, const gfxFontStyle& aF
 qcms_profile*
 gfxWindowsPlatform::GetPlatformCMSOutputProfile()
 {
+#ifndef MOZ_FT2_FONTS
     WCHAR str[MAX_PATH];
     DWORD size = MAX_PATH;
     BOOL res;
@@ -641,6 +668,9 @@ gfxWindowsPlatform::GetPlatformCMSOutputProfile()
                 NS_ConvertUTF16toUTF8(str).get());
 #endif
     return profile;
+#else
+    return nsnull;
+#endif
 }
 
 PRBool
@@ -654,6 +684,14 @@ gfxWindowsPlatform::SetPrefFontEntries(const nsCString& aKey, nsTArray<nsRefPtr<
 {
     mPrefFonts.Put(aKey, array);
 }
+
+#ifdef MOZ_FT2_FONTS
+FT_Library
+gfxWindowsPlatform::GetFTLibrary()
+{
+    return gPlatformFTLibrary;
+}
+#endif
 
 PRBool
 gfxWindowsPlatform::UseClearTypeForDownloadableFonts()
