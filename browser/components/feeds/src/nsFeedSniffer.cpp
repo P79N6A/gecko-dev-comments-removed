ac9c50313ec761312636a7506d346d9c1779280b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:05:10 +0000

    Bug 739040 followup: properly enforce the sniffing limit on uncompressed feeds, r=mano, a=desktop-only
    UltraBlame original commit: 0dd02041e575b08a009b0909603bff9ad25951f2

--- browser/components/feeds/src/nsFeedSniffer.cpp
+++ browser/components/feeds/src/nsFeedSniffer.cpp
@@ -313,20 +313,22 @@ nsFeedSniffer::GetMIMETypeFromContent(nsIRequest* request,
   nsresult rv = ConvertEncodedData(request, data, length);
   if (NS_FAILED(rv))
     return rv;
-  
-  const char* testData = 
-    mDecodedData.IsEmpty() ? (const char*)data : mDecodedData.get();
 
   
   
   
-
-  
-  
-  
-  if (!mDecodedData.IsEmpty()) {
+  const char* testData;
+  if (mDecodedData.IsEmpty()) {
+    testData = (const char*)data;
+    length = NS_MIN(length, MAX_BYTES);
+  } else {
+    testData = mDecodedData.get();
     length = NS_MIN(mDecodedData.Length(), MAX_BYTES);
   }
+
+  
+  
+  
 
   
   nsDependentCSubstring dataString((const char*)testData, length);
