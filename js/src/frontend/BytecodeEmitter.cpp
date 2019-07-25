e2e46ff2f0c19c43530c79f4df0a04081141477e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:23:18 +0000

    Backout 36fa9b176a91 (bug 710516) for M1 bustage.
    UltraBlame original commit: c5ebfdbe12d9edeaa083f4cde5bedbd4b8926acd

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -1781,8 +1781,8 @@ LookupCompileTimeConstant(JSContext *cx, BytecodeEmitter *bce, JSAtom *atom, Val
 
 
                     if (!shape->writable() && !shape->configurable() &&
-                        shape->hasDefaultGetter() && shape->hasSlot()) {
-                        *constp = obj->nativeGetSlot(shape->slot());
+                        shape->hasDefaultGetter() && obj->containsSlot(shape->slot())) {
+                        *constp = obj->getSlot(shape->slot());
                     }
                 }
 
