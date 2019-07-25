81b4e6fe274ef26696edb75ae9d770ed71084570
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:02:28 +0000

    Bug 474470: implement nsISidebar* APIs in Fennec, r=mfinkle
    UltraBlame original commit: eddc52297a5b9a12a00e269c7eda0f76a9fad22c

---
+++ mobile/components/Sidebar.js
@@ -0,0 +1,188 @@
+# -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http:
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is mozilla.org code.
+#
+# The Initial Developer of the Original Code is
+# Netscape Communications Corporation.
+# Portions created by the Initial Developer are Copyright (C) 1999
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Stephen Lamm            <slamm@netscape.com>
+#   Robert John Churchill   <rjc@netscape.com>
+#   David Hyatt             <hyatt@mozilla.org>
+#   Christopher A. Aillon   <christopher@aillon.com>
+#   Myk Melez               <myk@mozilla.org>
+#   Pamela Greene           <pamg.bugs@gmail.com>
+#   Gavin Sharp             <gavin@gavinsharp.com>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+const JAVASCRIPT_GLOBAL_PROPERTY_CATEGORY = "JavaScript global property";
+
+var gStrBundleService = null;
+function srGetStrBundle(path) {
+  if (!gStrBundleService) {
+    gStrBundleService = Cc["@mozilla.org/intl/stringbundle;1"].
+                        getService(Ci.nsIStringBundleService);
+  }
+
+  return gStrBundleService.createBundle(path);
+}
+
+function Sidebar() {
+  this.searchService = Cc["@mozilla.org/browser/search-service;1"].
+                       getService(Ci.nsIBrowserSearchService);
+}
+
+Sidebar.prototype = {
+  
+  _validateSearchEngine: function validateSearchEngine(engineURL, iconURL) {
+    try {
+      
+      if (! /^(https?|ftp):\/\//i.test(engineURL))
+        throw "Unsupported search engine URL";
+    
+      
+      
+      if (iconURL &&
+          ! /^(https?|ftp):\/\/.+\.(gif|jpg|jpeg|png|ico)$/i.test(iconURL))
+        throw "Unsupported search icon URL.";
+    } catch(ex) {
+      Cu.reportError("Invalid argument passed to window.sidebar.addSearchEngine: " + ex);
+      
+      var searchBundle = srGetStrBundle("chrome://global/locale/search/search.properties");
+      var brandBundle = srGetStrBundle("chrome://branding/locale/brand.properties");
+      var brandName = brandBundle.GetStringFromName("brandShortName");
+      var title = searchBundle.GetStringFromName("error_invalid_engine_title");
+      var msg = searchBundle.formatStringFromName("error_invalid_engine_msg",
+                                                  [brandName], 1);
+      var promptService = Cc["@mozilla.org/embedcomp/prompt-service;1"].
+                          getService(Ci.nsIPromptService);
+      promptService.alert(null, title, msg);
+      return false;
+    }
+    
+    return true;
+  },
+
+  
+  addPanel: function addPanel(aTitle, aContentURL, aCustomizeURL) {
+    
+  },
+
+  addPersistentPanel: function addPersistentPanel(aTitle, aContentURL, aCustomizeURL) {
+    
+  },
+
+  addMicrosummaryGenerator: function addMicrosummaryGenerator(generatorURL) {
+    
+  },
+
+  
+  
+  addSearchEngine: function addSearchEngine(engineURL, iconURL, suggestedTitle,
+                                            suggestedCategory) {
+    if (!this._validateSearchEngine(engineURL, iconURL))
+      return;
+
+    
+    const SHERLOCK_FILE_EXT_REGEXP = /\.src$/i;
+
+    
+    
+    
+    var dataType;
+    if (SHERLOCK_FILE_EXT_REGEXP.test(engineURL))
+      dataType = Ci.nsISearchEngine.DATA_TEXT;
+    else
+      dataType = Ci.nsISearchEngine.DATA_XML;
+
+    this.searchService.addEngine(engineURL, dataType, iconURL, true);
+  },
+
+  
+  
+  
+  
+  AddSearchProvider: function AddSearchProvider(aDescriptionURL) {
+    if (!this._validateSearchEngine(aDescriptionURL, ""))
+      return;
+  
+    const typeXML = Ci.nsISearchEngine.DATA_XML;
+    this.searchService.addEngine(aDescriptionURL, typeXML, "", true);
+  },
+
+  
+  
+  
+  
+  
+  
+  
+  
+  IsSearchProviderInstalled: function IsSearchProviderInstalled(aSearchURL) {
+    return 0;
+  },
+
+  
+  flags: Ci.nsIClassInfo.DOM_OBJECT,
+  classDescription: "Sidebar",
+  getInterfaces: function getInterfaces(count) {
+    var interfaceList = [Ci.nsISidebar, Ci.nsISidebarExternal, Ci.nsIClassInfo];
+    count.value = interfaceList.length;
+    return interfaceList;
+  },
+  getHelperForLanguage: function getHelperForLanguage(count) {
+    return null;
+  },
+
+  
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsISidebar,
+                                         Ci.nsISidebarExternal,
+                                         Ci.nsIClassInfo]),
+
+  
+  classID: Components.ID("{22117140-9c6e-11d3-aaf1-00805f8a4905}"),
+  className: "Sidebar JS Component",
+  contractID: "@mozilla.org/sidebar;1",
+  _xpcom_categories: [{ category: JAVASCRIPT_GLOBAL_PROPERTY_CATEGORY,
+                        entry: "sidebar"},
+                      { category: JAVASCRIPT_GLOBAL_PROPERTY_CATEGORY,
+                        entry: "external"}]
+}
+
+function NSGetModule(compMgr, fileSpec) {
+    return XPCOMUtils.generateModule([Sidebar]);
+}
