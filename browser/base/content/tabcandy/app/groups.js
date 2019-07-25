0a5688a2cd2824f2fd5edb05dae78513b9d4a467
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:20:59 +0000

    + Removed the unused getChildrenBounds() method of a group.
    UltraBlame original commit: 24cd6442f81e48f40420e8b55e4a5e772f72c64d

--- browser/base/content/tabcandy/app/groups.js
+++ browser/base/content/tabcandy/app/groups.js
@@ -435,13 +435,6 @@ window.Group.prototype = iQ.extend(new Item(), new Subscribable(), {
       
     return box;
   },
-
-  
-  
-  
-  getChildrenBounds: function() {
-    return this._getBoundingBox([child.container for each(child in this._children)]);
-  },
   
   
   
