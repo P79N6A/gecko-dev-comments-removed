3fe41590ed19b1846d5c24588a8b7b64926652c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:03:28 +0000

    Bug 567184 - A bootstrap add-on with a target app version less than the current app version is enabled when installed. r=dtownsend
    UltraBlame original commit: 7e51fa360a014792605c0cbff068e305db6c98d7

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_bug567184.js
@@ -0,0 +1,53 @@
+
+
+
+
+function run_test() {
+  do_test_pending();
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+  startupManager(1);
+
+  run_test_1();
+}
+
+
+function run_test_1() {
+  prepare_test({ }, [
+    "onNewInstall"
+  ]);
+
+  AddonManager.getInstallForFile(do_get_addon("test_bug567184"), function(install) {
+    ensure_test_completed();
+
+    do_check_neq(install, null);
+
+    prepare_test({
+      "bug567184@tests.mozilla.org": [
+        ["onInstalling", false],
+        "onInstalled"
+      ]
+    }, [
+      "onInstallStarted",
+      "onInstallEnded",
+    ], check_test_1);
+    install.install();
+  });
+}
+
+function check_test_1() {
+  AddonManager.getAllInstalls(function(installs) {
+    
+    
+    do_check_eq(installs.length, 0);
+
+    AddonManager.getAddonByID("bug567184@tests.mozilla.org", function(b1) {
+      do_check_neq(b1, null);
+      do_check_true(b1.appDisabled);
+      do_check_false(b1.userDisabled);
+      do_check_false(b1.isActive);
+
+      do_test_finished();
+    });
+  });
+}
