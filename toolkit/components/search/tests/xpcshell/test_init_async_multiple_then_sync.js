1a0cb6ef32a846f1746f1733c61308beb17031f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:24:21 +0000

    Bug 722332: add some test coverage for the search service async initialization API, r=gavin
    UltraBlame original commit: 9487eb14320b4ff918763b96f26038240e863aa3

---
+++ toolkit/components/search/tests/xpcshell/test_init_async_multiple_then_sync.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function run_test() {
+  do_print("Setting up test");
+  do_test_pending();
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "2");
+
+  do_print("Test starting");
+
+  let numberOfInitializers = 4;
+  let pending = [];
+  let numberPending = numberOfInitializers;
+
+  
+  for (let i = 0; i < numberOfInitializers; ++i) {
+    let me = i;
+    pending[me] = true;
+    Services.search.init(function search_initialized(aStatus) {
+      do_check_true(Components.isSuccessCode(aStatus));
+      init_complete(me);
+    });
+  }
+
+  
+  let init_complete = function init_complete(i) {
+    do_check_true(pending[i]);
+    pending[i] = false;
+    numberPending--;
+    do_check_true(numberPending >= 0);
+    do_check_true(Services.search.isInitialized);
+    if (numberPending == 0) {
+      
+      let engines = Services.search.getEngines();
+      do_check_neq(engines, null);
+
+      
+      
+      do_timeout(1000, function() {
+        do_test_finished();
+      });
+    }
+  };
+
+  
+  let engines = Services.search.getEngines();
+  do_check_neq(engines, null);
+}
+
