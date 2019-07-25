9e9c112509b9b912b8c17d6ec23dd944494b5ad1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:20:06 +0000

    Added a basic testing suite for engines/passwords.js.  It currently only tests _hashLoginInfo() and PasswordSyncCore._itemExists().
    UltraBlame original commit: ae5ed22613dd8b655cb0642550766cce47243749

---
+++ services/sync/tests/unit/test_passwords.js
@@ -0,0 +1,44 @@
+function loadInSandbox(aUri) {
+  var sandbox = Components.utils.Sandbox(this);
+  var request = Components.
+                classes["@mozilla.org/xmlextras/xmlhttprequest;1"].
+                createInstance();
+
+  request.open("GET", aUri, false);
+  request.send(null);
+  Components.utils.evalInSandbox(request.responseText, sandbox);
+
+  return sandbox;
+}
+
+function run_test() {
+  
+  var passwords = loadInSandbox("resource://weave/engines/passwords.js");
+
+  
+  var fakeUser = {
+    hostname: "www.boogle.com",
+    formSubmitURL: "http://www.boogle.com/search",
+    httpRealm: "",
+    username: "",
+    password: "",
+    usernameField: "test_person",
+    passwordField: "test_password"
+    };
+
+  
+  var fakeLoginManager = {
+    getAllLogins: function() { return [fakeUser]; }
+    };
+
+  
+  var fakeUserHash = passwords._hashLoginInfo(fakeUser);
+  do_check_eq(typeof fakeUserHash, 'string');
+  do_check_eq(fakeUserHash.length, 40);
+
+  
+  var psc = new passwords.PasswordSyncCore();
+  psc.__loginManager = fakeLoginManager;
+  do_check_false(psc._itemExists("invalid guid"));
+  do_check_true(psc._itemExists(fakeUserHash));
+}
