6af2c8cb5a59d4dc59fee9f66f740b219b6f624e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:02:53 +0000

    Bug 541373 (part 2) - Provide a global VACUUM component. Use idle-daily category. r=sdwilsh a=blocking
    UltraBlame original commit: 0e4c3af3ac59797c231eaca4962616fa39807bf9

--- storage/src/VacuumManager.cpp
+++ storage/src/VacuumManager.cpp
@@ -483,14 +483,6 @@ VacuumManager::initialize()
   NS_PRECONDITION(NS_IsMainThread(), "Must be running on the main thread!");
 
   
-  nsCOMPtr<nsIObserverService> os = mozilla::services::GetObserverService();
-  NS_ENSURE_STATE(os);
-  nsresult rv = os->AddObserver(this, OBSERVER_TOPIC_IDLE_DAILY, PR_FALSE);
-  NS_ENSURE_SUCCESS(rv, rv);
-  rv = os->AddObserver(this, OBSERVER_TOPIC_XPCOM_SHUTDOWN, PR_FALSE);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  
   nsCOMPtr<nsIPrefService> prefs = do_GetService(NS_PREFSERVICE_CONTRACTID);
   NS_ENSURE_STATE(prefs);
   prefs->GetBranch(PREF_VACUUM_BRANCH, getter_AddRefs(mPrefBranch));
@@ -539,15 +531,8 @@ VacuumManager::Observe(nsISupports *aSubject,
       }
     }
     (void)mPrefBranch->SetIntPref(NS_LITERAL_CSTRING("index").get(), index);
-    return NS_OK;
   }
-  else if (strcmp(aTopic, OBSERVER_TOPIC_XPCOM_SHUTDOWN) == 0) {
-    nsCOMPtr<nsIObserverService> os = mozilla::services::GetObserverService();
-    if (os) {
-      (void)os->RemoveObserver(this, OBSERVER_TOPIC_IDLE_DAILY);
-      (void)os->RemoveObserver(this, OBSERVER_TOPIC_XPCOM_SHUTDOWN);
-    }
-  }
+
   return NS_OK;
 }
 
