04036b9ac58e762cb105f0fd03d854d30c8f333c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:58:56 +0000

    Bug 616861 - Replicate behaviour of nsHttpChannel in regards to failure status codes in OnStopRequest. r=jduell a=blocking-fennec
    UltraBlame original commit: 4f4b4a3222bb42cc5c98f827693553a241c6bf70

--- netwerk/protocol/http/HttpChannelChild.cpp
+++ netwerk/protocol/http/HttpChannelChild.cpp
@@ -374,7 +374,7 @@ HttpChannelChild::OnStopRequest(const nsresult& statusCode)
 
   mIsPending = PR_FALSE;
 
-  if (!mCanceled)
+  if (!mCanceled && NS_SUCCEEDED(mStatus))
     mStatus = statusCode;
 
   { 
@@ -382,13 +382,13 @@ HttpChannelChild::OnStopRequest(const nsresult& statusCode)
     
     AutoEventEnqueuer ensureSerialDispatch(this);
 
-    mListener->OnStopRequest(this, mListenerContext, statusCode);
+    mListener->OnStopRequest(this, mListenerContext, mStatus);
 
     mListener = 0;
     mListenerContext = 0;
     mCacheEntryAvailable = PR_FALSE;
     if (mLoadGroup)
-      mLoadGroup->RemoveRequest(this, nsnull, statusCode);
+      mLoadGroup->RemoveRequest(this, nsnull, mStatus);
   }
 
   if (!(mLoadFlags & LOAD_DOCUMENT_URI)) {
