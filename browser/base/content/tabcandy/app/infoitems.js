9fbc7acb06da8ddb24eb605f8563f13d73151f53
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:23:08 +0000

    + started work on the first run experience: everything goes into a single new group, and there is an info item as well + Added "reset" to the dev menu so we can test first run (since the reset button is currently AWOL) + Added Utils.assertThrow(), an assert that throws an exception
    UltraBlame original commit: 5b8bc3ddad62bfe857e93889a67fc57a31549b13

---
+++ browser/base/content/tabcandy/app/infoitems.js
@@ -0,0 +1,256 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.InfoItem = function(bounds, options) {
+  try {
+    Utils.assertThrow('bounds', isRect(bounds));
+    
+    if(typeof(options) == 'undefined')
+      options = {};
+  
+    this._inited = false;
+    this.isAnInfoItem = true;
+    this.defaultSize = bounds.size();
+    this.locked = (options.locked ? Utils.copy(options.locked) : {});
+    this.bounds = new Rect(bounds);
+    this.isDragging = false;
+  
+    var self = this;
+
+    var $container = iQ('<div>')
+      .addClass('info-item')
+      .css(this.bounds)
+      .appendTo('body');
+      
+    var $close = iQ('<div>')
+      .addClass('close')
+      .click(function() {
+        self.close();
+      })
+      .appendTo($container);
+  
+    
+    if(this.locked.bounds)
+      $container.css({cursor: 'default'});    
+      
+    if(this.locked.close)
+      $close.hide();
+      
+    
+    this._init($container.get(0));
+  
+    if(this.$debug) 
+      this.$debug.css({zIndex: -1000});
+    
+    
+    if(!this.locked.bounds)
+      this.draggable();
+    
+  	
+    this.snap();
+    
+    
+    if(!options.dontPush)
+      this.pushAway();   
+  
+    this._inited = true;
+    this.save();
+  } catch(e){
+    Utils.log(e);
+  }
+};
+
+
+window.InfoItem.prototype = iQ.extend(new Item(), new Subscribable(), {
+  
+  
+  
+  getStorageData: function() {
+    var data = null;
+    
+    try {
+      data = {
+        bounds: this.getBounds(), 
+        locked: Utils.copy(this.locked)
+      };
+    } catch(e) {
+      Utils.log(e);
+    }
+  
+    return data;
+  },
+
+  
+  
+  
+  save: function() {
+    try {
+      if (!this._inited) 
+        return;
+  
+      var data = this.getStorageData();
+  
+
+
+
+    } catch(e) {
+      Utils.log(e);
+    }
+  },
+  
+  
+  
+  
+  setBounds: function(rect, immediately) {
+    try {
+      Utils.assertThrow('Group.setBounds: rect must be a real rectangle!', isRect(rect));    
+
+      
+      var css = {};
+  
+      if(rect.left != this.bounds.left)
+        css.left = rect.left;
+        
+      if(rect.top != this.bounds.top) 
+        css.top = rect.top;
+        
+      if(rect.width != this.bounds.width) 
+        css.width = rect.width;
+  
+      if(rect.height != this.bounds.height)
+        css.height = rect.height; 
+        
+      if(iQ.isEmptyObject(css))
+        return;
+        
+      this.bounds = new Rect(rect);
+      Utils.assertThrow('Group.setBounds: this.bounds must be a real rectangle!', isRect(this.bounds));    
+            
+      
+      if(immediately) {
+        iQ(this.container).css(css);
+      } else {
+        TabMirror.pausePainting();
+        iQ(this.container).animate(css, {
+          duration: 350, 
+          easing: 'tabcandyBounce', 
+          complete: function() {
+            TabMirror.resumePainting();
+          }
+        });
+      }
+      
+      this._updateDebugBounds();
+      this.setTrenches(rect);  
+      this.save();
+    } catch(e) {
+      Utils.log(e);
+    }
+  },
+    
+  
+  
+  
+  setZ: function(value) {
+    try {
+      Utils.assertThrow('value must be a number', typeof(value) == 'number');
+      
+      this.zIndex = value;
+  
+      iQ(this.container).css({zIndex: value});
+  
+      if(this.$debug) 
+        this.$debug.css({zIndex: value + 1});
+    } catch(e) {
+      Utils.log(e);
+    }
+  },
+    
+  
+  
+  
+  close: function() {
+    try {
+      this._sendOnClose();
+      this.removeTrenches();
+      iQ(this.container).fadeOut(function() {
+        iQ(this).remove();
+        Items.unsquish();
+      });
+  
+  
+    } catch(e) {
+      Utils.log(e);
+    }
+  },
+  
+  
+  
+  
+  html: function(value) {
+    try {
+      Utils.assertThrow('value must be a string', typeof(value) == 'string');
+      iQ(this.container).html(value);
+    } catch(e) {
+      Utils.log(e);
+    }
+  }
+});
+
+})();
