edc18e4e6a729a0ecfb29b50074673f2b82f7299
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:06:32 +0000

    bug 748085 - continue starting up webapp after exception initializing optional registry; r=felipe
    UltraBlame original commit: cdb6c708164d1723565aea2e8a4f8f594e8d6ab8

--- webapprt/CommandLineHandler.js
+++ webapprt/CommandLineHandler.js
@@ -10,7 +10,12 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 
 
-Cu.import("resource://gre/modules/Webapps.jsm");
+
+
+
+try {
+  Cu.import("resource://gre/modules/Webapps.jsm");
+} catch(ex) {}
 
 function CommandLineHandler() {}
 
