05f74915e9196521d001b77bbb12876e51f8650b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:46:06 +0000

    Bug 596762: Allow specification of differing IPC preferences for each architecture in a universal binary. r=josh a=blocking-b7
    UltraBlame original commit: e0d1f460215cb4461ae2c64bf6ae1766709d6674

--- testing/mochitest/tests/SimpleTest/SimpleTest.js
+++ testing/mochitest/tests/SimpleTest/SimpleTest.js
@@ -33,6 +33,41 @@ if (parentRunner) {
 }
 
 
+
+
+SimpleTest.testPluginIsOOP = function () {
+    netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+    var prefservice = Components.classes["@mozilla.org/preferences-service;1"]
+                                .getService(CI.nsIPrefBranch);
+
+    var testPluginIsOOP = false;
+    if (navigator.platform.indexOf("Mac") == 0) {
+        var xulRuntime = Components.classes["@mozilla.org/xre/app-info;1"]
+                                   .getService(CI.nsIXULAppInfo)
+                                   .QueryInterface(CI.nsIXULRuntime);
+        if (xulRuntime.XPCOMABI.match(/x86-/)) {
+            try {
+                testPluginIsOOP = prefservice.getBoolPref("dom.ipc.plugins.enabled.i386.test.plugin");
+            } catch (e) {
+                testPluginIsOOP = prefservice.getBoolPref("dom.ipc.plugins.enabled.i386");
+            }
+        }
+        else if (xulRuntime.XPCOMABI.match(/x86_64-/)) {
+            try {
+                testPluginIsOOP = prefservice.getBoolPref("dom.ipc.plugins.enabled.x86_64.test.plugin");
+            } catch (e) {
+                testPluginIsOOP = prefservice.getBoolPref("dom.ipc.plugins.enabled.x86_64");
+            }
+        }
+    }
+    else {
+        testPluginIsOOP = prefservice.getBoolPref("dom.ipc.plugins.enabled");
+    }
+
+    return testPluginIsOOP;
+};
+
+
 if (parentRunner) {
     SimpleTest._logEnabled = parentRunner.logEnabled;
 }
