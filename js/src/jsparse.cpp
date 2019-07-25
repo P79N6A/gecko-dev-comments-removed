5eb18f49c21921ada3bfbf017c69d7d660a81928
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:01:50 +0000

    Fix hard-case destructurinv vs. for(var ...=... in ...) bug (558633, r=mrbkap).
    UltraBlame original commit: 0bed5abf176496224007f04a0c3ac7906670fa61

--- js/src/jsparse.cpp
+++ js/src/jsparse.cpp
@@ -3599,7 +3599,13 @@ BindDestructuringLHS(JSContext *cx, JSParseNode *pn, JSTreeContext *tc)
 
       case TOK_DOT:
       case TOK_LB:
-        pn->pn_op = JSOP_SETNAME;
+        
+
+
+
+
+        if (!(js_CodeSpec[pn->pn_op].format & JOF_SET))
+            pn->pn_op = JSOP_SETNAME;
         break;
 
       case TOK_LP:
