00e62f3398e3cc711d5f4abda568ba8931d19ba6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:24:05 +0000

    Bug 656041 Some parts of page scroll at different rates in Fennec r=roc
    UltraBlame original commit: c64dcd99b27eefefa06ea3746f6cc242ffc82aab

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -2026,7 +2026,7 @@ nsGfxScrollFrameInner::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
     
     nsDisplayScrollInfoLayer* layerItem = new (aBuilder) nsDisplayScrollInfoLayer(
       aBuilder, mScrolledFrame, mOuter);
-    set.Content()->AppendNewToBottom(layerItem);
+    set.BorderBackground()->AppendNewToBottom(layerItem);
   }
 
   nsRect clip;
