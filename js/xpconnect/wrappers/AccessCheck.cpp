fca89d41f1d639ea1888930fb0b0fa84dd843ebb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:40:14 +0000

    Bug 553102 - Make content-> access default to deny if __exposedProps__ is not defined. r=mrbkap
    UltraBlame original commit: 0f090cc7e9faf744a342c0746148df7e0792a567

--- js/xpconnect/wrappers/AccessCheck.cpp
+++ js/xpconnect/wrappers/AccessCheck.cpp
@@ -427,24 +427,6 @@ ExposedPropertiesOnly::check(JSContext *cx, JSObject *wrapper, jsid id, Wrapper:
         if (!wrapperAC.enter(cx, wrapper))
             return false;
 
-        
-        if (!JS_ObjectIsFunction(cx, wrappedObject)) {
-
-            
-            nsCOMPtr<nsPIDOMWindow> win =
-                do_QueryInterface(nsJSUtils::GetStaticScriptGlobal(cx, wrapper));
-            if (win) {
-                nsCOMPtr<nsIDocument> doc =
-                    do_QueryInterface(win->GetExtantDocument());
-                if (doc) {
-                    doc->WarnOnceAbout(nsIDocument::eNoExposedProps,
-                                        true);
-                }
-            }
-
-            perm = PermitPropertyAccess;
-            return true;
-        }
         return PermitIfUniversalXPConnect(cx, id, act, perm); 
     }
 
