42cb17bc1ea7cb063797adb73252243d4f1b798c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:33:13 +0000

    Bug 736710 - Part 3: Voicemail DOM implementation classes. r=smaug
    UltraBlame original commit: c39bb0448bad36d9644bc587db5a5c8a3c05bea3

--- dom/telephony/TelephonyFactory.h
+++ dom/telephony/TelephonyFactory.h
@@ -8,11 +8,15 @@
 #define mozilla_dom_telephony_telephonyfactory_h__
 
 #include "nsIDOMTelephony.h"
+#include "nsIDOMVoicemail.h"
 #include "nsPIDOMWindow.h"
 
 
 
 nsresult
 NS_NewTelephony(nsPIDOMWindow* aWindow, nsIDOMTelephony** aTelephony);
+
+nsresult
+NS_NewVoicemail(nsPIDOMWindow* aWindow, nsIDOMMozVoicemail** aVoicemail);
 
 #endif 
---
+++ dom/telephony/Voicemail.h
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_telephony_voicemail_h__
+#define mozilla_dom_telephony_voicemail_h__
+
+#include "TelephonyCommon.h"
+
+#include "nsDOMEvent.h"
+#include "nsDOMEventTargetHelper.h"
+#include "nsIDOMVoicemail.h"
+#include "nsIRadioInterfaceLayer.h"
+
+class nsIRILContentHelper;
+class nsIDOMMozVoicemailStatus;
+
+BEGIN_TELEPHONY_NAMESPACE
+
+class Voicemail : public nsDOMEventTargetHelper,
+                  public nsIDOMMozVoicemail
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIDOMMOZVOICEMAIL
+  NS_DECL_NSIRILVOICEMAILCALLBACK
+
+  NS_FORWARD_NSIDOMEVENTTARGET(nsDOMEventTargetHelper::)
+
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(Voicemail, nsDOMEventTargetHelper)
+
+  Voicemail(nsPIDOMWindow* aWindow, nsIRILContentHelper* aRIL);
+  virtual ~Voicemail();
+
+private:
+  nsCOMPtr<nsIRILContentHelper> mRIL;
+  nsCOMPtr<nsIRILVoicemailCallback> mRILVoicemailCallback;
+
+  NS_DECL_EVENT_HANDLER(statuschanged)
+
+  class RILVoicemailCallback : public nsIRILVoicemailCallback
+  {
+    Voicemail* mVoicemail;
+
+  public:
+    NS_DECL_ISUPPORTS
+    NS_FORWARD_NSIRILVOICEMAILCALLBACK(mVoicemail->)
+
+    RILVoicemailCallback(Voicemail* aVoicemail)
+    : mVoicemail(aVoicemail)
+    {
+      NS_ASSERTION(mVoicemail, "Null pointer!");
+    }
+  };
+};
+
+END_TELEPHONY_NAMESPACE
+
+#endif 
