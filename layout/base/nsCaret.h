cc7f920d5287950d176a01635b665b91c4fc65af
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:36:46 +0000

    Bug 607857. If caret is in a frame that needs reflow we can wrongly position the caret rect. r=roc a=blocking-final
    UltraBlame original commit: c198cabd2743bc3b417461e1ce0da960d02eea0a

--- layout/base/nsCaret.h
+++ layout/base/nsCaret.h
@@ -217,10 +217,10 @@ protected:
       nscoord mCaretWidth;        
     };
     Metrics ComputeMetrics(nsIFrame* aFrame, PRInt32 aOffset, nscoord aCaretHeight);
-    void GetGeometryForFrame(nsIFrame* aFrame,
-                             PRInt32   aFrameOffset,
-                             nsRect*   aRect,
-                             nscoord*  aBidiIndicatorSize);
+    nsresult GetGeometryForFrame(nsIFrame* aFrame,
+                                 PRInt32   aFrameOffset,
+                                 nsRect*   aRect,
+                                 nscoord*  aBidiIndicatorSize);
 
     
     
