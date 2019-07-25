675e6271f5244e5c8997182a339ca8e37921d308
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:24:14 +0000

    Bug 561005 - Use FakeSvc to grab binary crypto if js-ctypes doesn't work [r=zpao r=mconnor] Lazily load the binary component service onto FakeSvc if ;2 fails to register on Svc.
    UltraBlame original commit: 58a3bbb32871cd5f9f97044731f8c521b2e459e6

--- services/sync/modules/util.js
+++ services/sync/modules/util.js
@@ -859,24 +859,11 @@ let FakeSvc = {
 
 
 
+Utils.lazySvc(FakeSvc, "@labs.mozilla.com/Weave/Crypto;2",
+              "@labs.mozilla.com/Weave/Crypto;1", "IWeaveCrypto");
 
-let cryptoContractID = "@labs.mozilla.com/Weave/Crypto";
 
-{
-  let versSvc = Cc["@mozilla.org/xpcom/version-comparator;1"].
-                getService(Ci.nsIVersionComparator);
-  let appinfo = Cc["@mozilla.org/xre/app-info;1"].
-                getService(Ci.nsIXULAppInfo);
-  let platVers = appinfo.platformVersion;
 
-  if (versSvc.compare(platVers, "1.9.3a3") < 0) {
-    
-    cryptoContractID += ";1";
-  } else {
-    
-    cryptoContractID += ";2";
-  }
-}
 
 let Svc = {};
 Svc.Prefs = new Preferences(PREFS_BRANCH);
@@ -884,7 +871,7 @@ Svc.Obs = Observers;
 [["Annos", "@mozilla.org/browser/annotation-service;1", "nsIAnnotationService"],
  ["AppInfo", "@mozilla.org/xre/app-info;1", "nsIXULAppInfo"],
  ["Bookmark", "@mozilla.org/browser/nav-bookmarks-service;1", "nsINavBookmarksService"],
- ["Crypto", cryptoContractID, "IWeaveCrypto"],
+ ["Crypto", "@labs.mozilla.com/Weave/Crypto;2", "IWeaveCrypto"],
  ["Directory", "@mozilla.org/file/directory_service;1", "nsIProperties"],
  ["Env", "@mozilla.org/process/environment;1", "nsIEnvironment"],
  ["Favicon", "@mozilla.org/browser/favicon-service;1", "nsIFaviconService"],
