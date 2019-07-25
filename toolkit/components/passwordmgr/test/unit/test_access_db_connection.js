b3af7abd59007b351a327615db01407addc211b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:45:28 +0000

    Bug 630730 - Let consumers who probably know what they are doing access the database connection r=dolske a=Mossop
    UltraBlame original commit: ab87d34e89fa00423266c8177ee3eb1811e08f61

---
+++ toolkit/components/passwordmgr/test/unit/test_access_db_connection.js
@@ -0,0 +1,23 @@
+
+
+
+const STORAGE_TYPE = "mozStorage";
+
+function run_test()
+{
+  
+  let storage = LoginTest.initStorage(INDIR, "signons-empty.txt", OUTDIR,
+                                      "signons-empty.sqlite");
+  do_check_true(storage instanceof Ci.nsIInterfaceRequestor);
+  let db = storage.getInterface(Ci.mozIStorageConnection);
+  do_check_neq(db, null);
+  do_check_true(db.connectionReady);
+
+  
+  
+  let lm = Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager);
+  do_check_true(lm instanceof Ci.nsIInterfaceRequestor);
+  db = lm.getInterface(Ci.mozIStorageConnection);
+  do_check_neq(db, null);
+  do_check_true(db.connectionReady);
+}
