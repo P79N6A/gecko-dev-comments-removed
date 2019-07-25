6477714895c1c9532390520ad2ceae286a6845df
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:25:33 +0000

    Backing out bug 611076
    UltraBlame original commit: 1a05b65c31e17e7a2fb9999293f0f4b29db847ce

--- toolkit/mozapps/extensions/amWebInstallListener.js
+++ toolkit/mozapps/extensions/amWebInstallListener.js
@@ -157,11 +157,8 @@ Installer.prototype = {
           }, this);
         }
         break;
-      case AddonManager.STATE_CANCELLED:
-        
-        break;
       default:
-        WARN("Download of " + install.sourceURI.spec + " in unexpected state " +
+        WARN("Download of " + install.sourceURI + " in unexpected state " +
              install.state);
       }
     }
