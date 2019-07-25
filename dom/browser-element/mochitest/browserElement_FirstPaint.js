27f0c675aca2770fe1ab9f73da7324c97ed76670
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:44:51 +0000

    Bug 787378 - Part 2: Tests for mozbrowserfirstpaint event.
    UltraBlame original commit: 50e752beca26b86613adf21f4a28dcf667cde245

---
+++ dom/browser-element/mochitest/browserElement_FirstPaint.js
@@ -0,0 +1,44 @@
+
+
+
+
+"use strict";
+
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addPermission();
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  var gotFirstPaint = false;
+  var gotFirstLocationChange = false;
+  iframe.addEventListener('mozbrowserfirstpaint', function(e) {
+    ok(!gotFirstPaint, "Got only one first paint.");
+    gotFirstPaint = true;
+
+    if (gotFirstLocationChange) {
+      iframe.src = browserElementTestHelpers.emptyPage1 + '?2';
+    }
+  });
+
+  iframe.addEventListener('mozbrowserlocationchange', function(e) {
+    if (e.detail == browserElementTestHelpers.emptyPage1) {
+      gotFirstLocationChange = true;
+      if (gotFirstPaint) {
+        iframe.src = browserElementTestHelpers.emptyPage1 + '?2';
+      }
+    }
+    else if (e.detail.endsWith('?2')) {
+      SimpleTest.finish();
+    }
+  });
+
+  document.body.appendChild(iframe);
+
+  iframe.src = browserElementTestHelpers.emptyPage1;
+}
+
+runTest();
