aab32166d7a95fc94593f40ca4fba1acce762da2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:33:04 +0000

    Bug 709759 - Add stop() to mozbrowser API. r=jlebar
    UltraBlame original commit: e8ca855356d16d475f683eb074e237d59f48b3ec

---
+++ dom/browser-element/mochitest/browserElement_Stop.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+
+
+
+
+"use strict";
+SimpleTest.waitForExplicitFinish();
+
+var iframe;
+var stopped = false;
+var imgSrc = 'http://test/tests/dom/browser-element/mochitest/file_bug709759.sjs';
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addToWhitelist();
+
+  iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+
+  iframe.addEventListener('mozbrowserloadend', loadend);
+  iframe.src = 'data:text/html,<html>' +
+    '<body><img src="' + imgSrc + '" /></body></html>';
+
+  document.body.appendChild(iframe);
+
+  setTimeout(function() {
+    stopped = true;
+    iframe.stop();
+  }, 200);
+}
+
+function loadend() {
+  ok(stopped, 'Iframes network connections were stopped');
+  SimpleTest.finish();
+}
+
+runTest();
