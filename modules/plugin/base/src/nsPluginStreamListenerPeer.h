dc2c7dc3c9362b3f3d8e11be9a66a7cc3dce8f60
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:43:29 +0000

    Move nsPluginStreamListenerPeer to its own files, clean up the plugin host. b=578447 r=benwa
    UltraBlame original commit: 7028509444226713bc0e9cafdd4564a145cf26e3

---
+++ modules/plugin/base/src/nsPluginStreamListenerPeer.h
@@ -0,0 +1,160 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nscore.h"
+#include "nsIFile.h"
+#include "nsIStreamListener.h"
+#include "nsIProgressEventSink.h"
+#include "nsIHttpHeaderVisitor.h"
+#include "nsWeakReference.h"
+#include "nsNPAPIPluginStreamListener.h"
+#include "nsHashtable.h"
+
+class nsIChannel;
+
+
+
+
+
+
+
+class CachedFileHolder
+{
+public:
+  CachedFileHolder(nsIFile* cacheFile);
+  ~CachedFileHolder();
+  
+  void AddRef();
+  void Release();
+  
+  nsIFile* file() const { return mFile; }
+  
+private:
+  nsAutoRefCnt mRefCnt;
+  nsCOMPtr<nsIFile> mFile;
+};
+
+class nsPluginStreamListenerPeer : public nsIStreamListener,
+public nsIProgressEventSink,
+public nsIHttpHeaderVisitor,
+public nsSupportsWeakReference,
+public nsINPAPIPluginStreamInfo
+{
+public:
+  nsPluginStreamListenerPeer();
+  virtual ~nsPluginStreamListenerPeer();
+  
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIPROGRESSEVENTSINK
+  NS_DECL_NSIREQUESTOBSERVER
+  NS_DECL_NSISTREAMLISTENER
+  NS_DECL_NSIHTTPHEADERVISITOR
+  
+  
+  NS_DECL_NSIPLUGINSTREAMINFO
+  
+  
+  void
+  MakeByteRangeString(NPByteRange* aRangeList, nsACString &string, PRInt32 *numRequests);
+  
+  PRBool UseExistingPluginCacheFile(nsPluginStreamListenerPeer* psi);
+  
+  
+  nsresult Initialize(nsIURI *aURL,
+                      nsIPluginInstance *aInstance,
+                      nsIPluginStreamListener *aListener,
+                      PRInt32 requestCount = 1);
+  
+  nsresult InitializeEmbedded(nsIURI *aURL,
+                              nsIPluginInstance* aInstance,
+                              nsIPluginInstanceOwner *aOwner = nsnull);
+  
+  nsresult InitializeFullPage(nsIURI* aURL, nsIPluginInstance *aInstance);
+  
+  nsresult OnFileAvailable(nsIFile* aFile);
+  
+  nsresult ServeStreamAsFile(nsIRequest *request, nsISupports *ctxt);
+  
+  nsIPluginInstance *GetPluginInstance() { return mInstance; }
+  
+private:
+  nsresult SetUpStreamListener(nsIRequest* request, nsIURI* aURL);
+  nsresult SetupPluginCacheFile(nsIChannel* channel);
+  
+  nsCOMPtr<nsIURI> mURL;
+  nsCString mURLSpec; 
+  nsCOMPtr<nsIPluginInstanceOwner> mOwner;
+  nsCOMPtr<nsIPluginInstance> mInstance;
+  nsCOMPtr<nsIPluginStreamListener> mPStreamListener;
+  
+  
+  PRPackedBool            mRequestFailed;
+  
+  
+
+
+
+
+
+  PRPackedBool      mStartBinding;
+  PRPackedBool      mHaveFiredOnStartRequest;
+  
+  PRUint32                mLength;
+  PRInt32                 mStreamType;
+  
+  
+  
+  nsRefPtr<CachedFileHolder> mLocalCachedFileHolder;
+  nsCOMPtr<nsIOutputStream> mFileCacheOutputStream;
+  nsHashtable             *mDataForwardToRequest;
+  
+  nsCString mContentType;
+  PRBool mSeekable;
+  PRUint32 mModified;
+  nsCOMPtr<nsIPluginInstance> mPluginInstance;
+  PRInt32 mStreamOffset;
+  PRBool mStreamComplete;
+  
+public:
+  PRBool                  mAbort;
+  PRInt32                 mPendingRequests;
+  nsWeakPtr               mWeakPtrChannelCallbacks;
+  nsWeakPtr               mWeakPtrChannelLoadGroup;
+};
