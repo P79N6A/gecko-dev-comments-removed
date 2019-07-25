387b5ebef8a5f49c7e7845257a43515bd33b22b5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:55:03 +0000

    Bug 676281: Implement Debugger.prototype.findScripts. r=jorendorff
    UltraBlame original commit: 6692fb9f8f2127d876758843a04075510de6078f

--- js/src/jsgcinlines.h
+++ js/src/jsgcinlines.h
@@ -118,8 +118,8 @@ GetGCObjectFixedSlotsKind(size_t numFixedSlots)
 static inline bool
 IsBackgroundAllocKind(AllocKind kind)
 {
-    JS_ASSERT(kind <= FINALIZE_OBJECT_LAST);
-    return kind % 2 == 1;
+    JS_ASSERT(kind <= FINALIZE_LAST);
+    return kind <= FINALIZE_OBJECT_LAST && kind % 2 == 1;
 }
 
 static inline AllocKind
@@ -353,9 +353,13 @@ class CellIter : public CellIterImpl
       : lists(&comp->arenas),
         kind(kind)
     {
-#ifdef JS_THREADSAFE
-        JS_ASSERT(comp->arenas.doneBackgroundFinalize(kind));
-#endif
+        
+
+
+
+
+
+        JS_ASSERT(!IsBackgroundAllocKind(kind));
         if (lists->isSynchronizedFreeList(kind)) {
             lists = NULL;
         } else {
