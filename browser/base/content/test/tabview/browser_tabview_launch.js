66ea1b4ffc19771e7ecb1bce02e4e1bba5e74f0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:33:25 +0000

    Backout bug 626455 (changeset 37d01a2d846c) for random timeouts
    UltraBlame original commit: 2bf2c0cc8d437005ded970ac5ddeb8445e659d2b

--- browser/base/content/test/tabview/browser_tabview_launch.js
+++ browser/base/content/test/tabview/browser_tabview_launch.js
@@ -1,18 +1,15 @@
 
 
 
+let timerId;
 let newWin;
 
 
 function test() {
   waitForExplicitFinish();
 
-  let panelSelected = false;
-  registerCleanupFunction(function () ok(panelSelected, "panel is selected"));
-
-  let onLoad = function (win) {
-    registerCleanupFunction(function () win.close());
-
+  
+  newWindowWithTabView(function() {}, function(win) {
     newWin = win;
 
     let onSelect = function(event) {
@@ -24,26 +21,27 @@ function test() {
         return;
 
       deck.removeEventListener("select", onSelect, true);
-      panelSelected = true;
+
+      whenTabViewIsShown(function() {
+        executeSoon(function() {
+          testMethodToHideAndShowTabView(function() {
+            newWin.document.getElementById("menu_tabview").doCommand();
+          }, function() {
+            testMethodToHideAndShowTabView(function() {
+              EventUtils.synthesizeKey("e", { accelKey: true, shiftKey: true }, newWin);
+            }, finish);
+          });
+        });
+      }, win);
     };
 
     let deck = win.document.getElementById("tab-view-deck");
     deck.addEventListener("select", onSelect, true);
-  };
+  });
 
-  let onShow = function (win) {
-    executeSoon(function() {
-      testMethodToHideAndShowTabView(function() {
-        newWin.document.getElementById("menu_tabview").doCommand();
-      }, function() {
-        testMethodToHideAndShowTabView(function() {
-          EventUtils.synthesizeKey("E", { accelKey: true, shiftKey: true }, newWin);
-        }, finish);
-      });
-    });
-  };
-
-  newWindowWithTabView(onShow, onLoad);
+  registerCleanupFunction(function () {
+    newWin.close();
+  });
 }
 
 function testMethodToHideAndShowTabView(executeFunc, callback) {
