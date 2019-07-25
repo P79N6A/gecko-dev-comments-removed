d6d3c3e95f4fdc3be02b074db89941efef085342
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:04:44 +0000

    Bug 546595 - Add mochitests for the incompatibleList and incompatibleCheck pages. r=dtownsend
    UltraBlame original commit: 0425643a900dd3b4fb962d1862535f20ca9fc272

--- toolkit/mozapps/update/test/chrome/utils.js
+++ toolkit/mozapps/update/test/chrome/utils.js
@@ -74,16 +74,57 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Components.utils.import("resource://gre/modules/AddonManager.jsm");
+
+
+
 const PAGEID_DUMMY            = "dummy";                 
 const PAGEID_CHECKING         = "checking";              
 const PAGEID_PLUGIN_UPDATES   = "pluginupdatesfound";
 const PAGEID_NO_UPDATES_FOUND = "noupdatesfound";        
 const PAGEID_MANUAL_UPDATE    = "manualUpdate"; 
-const PAGEID_INCOMPAT_CHECK   = "incompatibleCheck"; 
+const PAGEID_INCOMPAT_CHECK   = "incompatibleCheck";     
 const PAGEID_FOUND_BASIC      = "updatesfoundbasic";     
 const PAGEID_FOUND_BILLBOARD  = "updatesfoundbillboard"; 
 const PAGEID_LICENSE          = "license";               
-const PAGEID_INCOMPAT_LIST    = "incompatibleList"; 
+const PAGEID_INCOMPAT_LIST    = "incompatibleList";      
 const PAGEID_DOWNLOADING      = "downloading";           
 const PAGEID_ERRORS           = "errors";                
 const PAGEID_ERROR_PATCHING   = "errorpatching";         
@@ -101,16 +142,35 @@ const URI_UPDATE_PROMPT_DIALOG  = "chrome://mozapps/content/update/updates.xul";
 
 const CRC_ERROR = 4;
 
+const ADDON_ID_SUFFIX = "@appupdatetest.mozilla.org";
+const ADDON_PREP_DIR = "appupdateprep";
+
+
+const PREF_DISABLEDADDONS = "app.update.test.disabledAddons";
+const TEST_ADDONS = [ "appdisabled_1", "appdisabled_2",
+                      "compatible_1", "compatible_2",
+                      "noupdate_1", "noupdate_2",
+                      "updatecompatibility_1", "updatecompatibility_2",
+                      "updateversion_1", "updateversion_2",
+                      "userdisabled_1", "userdisabled_2" ];
+
 const DEBUG_DUMP = false;
 
 const TEST_TIMEOUT = 30000; 
 var gTimeoutTimer;
 
+
+
+var gAppUpdateChannel; 
+var gAppUpdateEnabled; 
+var gAppUpdateURL;     
+var gExtUpdateURL;     
+
 var gTestCounter = -1;
-var gUpdateChannel;
-var gDocElem;
 var gWin;
+var gDocElem;
 var gPrefToCheck;
+var gDisableNoUpdateAddon = false;
 
 #include ../shared.js
 
@@ -124,7 +184,7 @@ function debugDump(msg) {
 
 
 __defineGetter__("gTest", function() {
-  return gTests[gTestCounter];
+  return TESTS[gTestCounter];
 });
 
 
@@ -133,8 +193,8 @@ __defineGetter__("gTest", function() {
 
 
 __defineGetter__("gCallback", function() {
-    return gTest.overrideCallback ? gTest.overrideCallback
-                                  : defaultCallback;
+  return gTest.overrideCallback ? gTest.overrideCallback
+                                : defaultCallback;
 });
 
 
@@ -172,19 +232,29 @@ __defineGetter__("gAcceptDeclineLicense", function() {
 
 
 
+__defineGetter__("gIncompatibleListbox", function() {
+  return gWin.document.getElementById("incompatibleListbox");
+});
+
+
+
+
 function runTestDefault() {
   debugDump("Entering runTestDefault");
+
+  if (!("@mozilla.org/zipwriter;1" in AUS_Cc)) {
+    ok(false, "nsIZipWriter is required to run these tests");
+    return;
+  }
 
   SimpleTest.waitForExplicitFinish();
 
   Services.ww.registerNotification(gWindowObserver);
-  setUpdateChannel();
-  Services.prefs.setIntPref(PREF_APP_UPDATE_IDLETIME, 0);
 
-  removeUpdateDirsAndFiles
+  setupPrefs();
+  removeUpdateDirsAndFiles();
   reloadUpdateManagerData();
-
-  runTest();
+  setupAddons(runTest);
 }
 
 
@@ -249,7 +319,7 @@ function onPageShowDefault(aEvent) {
 
 
 function defaultCallback(aEvent) {
-  debugDump("Entering defaultCallback - gTests[" + gTestCounter + "], " +
+  debugDump("Entering defaultCallback - TESTS[" + gTestCounter + "], " +
             "pageid: " + gTest.pageid + ", " +
             "aEvent.originalTarget.nodeName: " + aEvent.originalTarget.nodeName);
 
@@ -263,7 +333,7 @@ function defaultCallback(aEvent) {
   }
 
   is(gDocElem.currentPage.pageid, gTest.pageid,
-     "Checking currentPage.pageid equals " + gTest.pageid + " in onPageShow");
+     "Checking currentPage.pageid equals " + gTest.pageid + " in pageshow");
 
   
   if (gTest.extraCheckFunction) {
@@ -284,7 +354,7 @@ function defaultCallback(aEvent) {
 
 
 function delayedDefaultCallback() {
-  debugDump("Entering delayedDefaultCallback - gTests[" + gTestCounter + "], " +
+  debugDump("Entering delayedDefaultCallback - TESTS[" + gTestCounter + "], " +
             "pageid: " + gTest.pageid);
 
   
@@ -325,7 +395,7 @@ function delayedDefaultCallback() {
 
 
 function checkButtonStates() {
-  debugDump("Entering checkButtonStates - gTests[" + gTestCounter + "], " +
+  debugDump("Entering checkButtonStates - TESTS[" + gTestCounter + "], " +
             "pageid: " + gTest.pageid);
 
   const buttonNames = ["extra1", "extra2", "back", "next", "finish", "cancel"];
@@ -400,7 +470,7 @@ function getExpectedButtonStates() {
 
 
 function addRemoteContentLoadListener() {
-  debugDump("Entering addRemoteContentLoadListener - gTests[" + gTestCounter +
+  debugDump("Entering addRemoteContentLoadListener - TESTS[" + gTestCounter +
             "], pageid: " + gTest.pageid);
 
   gRemoteContent.addEventListener("load", remoteContentLoadListener, false);
@@ -468,7 +538,7 @@ function checkRemoteContentState() {
 
 
 function addRadioGroupSelectListenerAndClick() {
-  debugDump("Entering addRadioGroupSelectListenerAndClick - gTests[" +
+  debugDump("Entering addRadioGroupSelectListenerAndClick - TESTS[" +
             gTestCounter + "], pageid: " + gTest.pageid);
 
   gAcceptDeclineLicense.addEventListener("select", radioGroupSelectListener,
@@ -509,6 +579,19 @@ function checkRadioGroupSelectedIndex() {
 
 
 
+function checkIncompatbleList() {
+  for (let i = 0; i < gIncompatibleListbox.itemCount; i++) {
+    let label = gIncompatibleListbox.getItemAtIndex(i).label;
+    
+    ok(label.indexOf("noupdate") != -1, "Checking that only incompatible " + 
+       "add-ons that don't have an update are listed in the incompatible list");
+  }
+}
+
+
+
+
+
 
 
 
@@ -517,12 +600,13 @@ function checkRadioGroupSelectedIndex() {
 
 
 function checkPrefHasUserValue(aPrefHasValue) {
-  var prefHasUserValue = aPrefHasValue === undefined ? gTest.prefHasUserValue
+  let prefHasUserValue = aPrefHasValue === undefined ? gTest.prefHasUserValue
                                                      : aPrefHasValue;
   is(Services.prefs.prefHasUserValue(gPrefToCheck), prefHasUserValue,
      "Checking prefHasUserValue for preference " + gPrefToCheck + " equals " +
      (prefHasUserValue ? "true" : "false"));
 }
+
 
 
 
@@ -546,58 +630,390 @@ function getVersionParams(aAppVersion, aPlatformVersion) {
 
 
 
+
+
+
+
+function getNewerAppVersion() {
+  let appVersion = Services.appinfo.version.split(".")[0];
+  appVersion++;
+  return appVersion;
+}
+
+
+
+
+
+
+
+
+function getNewerPlatformVersion() {
+  let platformVersion = Services.appinfo.platformVersion.split(".")[0];
+  platformVersion++;
+  return platformVersion;
+}
+
+
+
+
 function verifyTestsRan() {
   debugDump("Entering verifyTestsRan");
 
   
-  if (!gTests) {
+  if (!TESTS) {
     return;
   }
 
   gTestCounter = -1;
-  for (let i = 0; i < gTests.length; ++i) {
+  for (let i = 0; i < TESTS.length; ++i) {
     gTestCounter++;
-    let test = gTests;
-    let msg = "Checking if gTests[" + i + "] test was performed... " +
-              "callback function name = " + gCallback.name + "," +
-              "pageid = " + (gTest.pageid ? gTest.pageid : "N/A");
-    ok(gTest.ranTest, msg);
+    let test = TESTS[i];
+    let msg = "Checking if TESTS[" + i + "] test was performed... " +
+              "callback function name = " + gCallback.name + ", " +
+              "pageid = " + test.pageid;
+    ok(test.ranTest, msg);
   }
 }
 
 
 
 
-function resetPrefs() {
-  if (gUpdateChannel) {
-    setUpdateChannel(gUpdateChannel);
+
+
+function setupPrefs() {
+  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_URL_OVERRIDE)) {
+    gAppUpdateURL = Services.prefs.setIntPref(PREF_APP_UPDATE_URL_OVERRIDE);
   }
+
+  gAppUpdateChannel = gDefaultPrefBranch.getCharPref(PREF_APP_UPDATE_CHANNEL);
+  setUpdateChannel();
+
+  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_ENABLED)) {
+    gAppUpdateEnabled = Services.prefs.getBoolPref(PREF_APP_UPDATE_ENABLED);
+  }
+
+  if (Services.prefs.prefHasUserValue(PREF_EXTENSIONS_UPDATE_URL)) {
+    gExtUpdateURL = Services.prefs.getCharPref(PREF_EXTENSIONS_UPDATE_URL);
+  }
+  let extUpdateUrl = URL_UPDATE + "?addonID=%ITEM_ID%&platformVersion=" +
+                     getNewerPlatformVersion();
+  Services.prefs.setCharPref(PREF_EXTENSIONS_UPDATE_URL, extUpdateUrl);
+  debugDump("extensions.update.url: " + extUpdateUrl);
+
+  Services.prefs.setIntPref(PREF_APP_UPDATE_IDLETIME, 0);
+}
+
+
+
+
+function resetPrefs() {
+  if (gAppUpdateURL) {
+    Services.prefs.setCharPref(PREF_APP_UPDATE_URL_OVERRIDE, gAppUpdateURL);
+  }
+  else if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_URL_OVERRIDE)) {
+    Services.prefs.clearUserPref(PREF_APP_UPDATE_URL_OVERRIDE);
+  }
+
+  if (gAppUpdateChannel) {
+    setUpdateChannel(gAppUpdateChannel);
+  }
+
+  if (gAppUpdateEnabled) {
+    Services.prefs.setBoolPref(PREF_APP_UPDATE_ENABLED, gAppUpdateEnabled);
+  }
+  else if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_ENABLED)) {
+    Services.prefs.clearUserPref(PREF_APP_UPDATE_ENABLED);
+  }
+
+  if (gExtUpdateURL) {
+    Services.prefs.setCharPref(PREF_EXTENSIONS_UPDATE_URL, gExtUpdateURL);
+  }
+  else if (Services.prefs.prefHasUserValue(PREF_EXTENSIONS_UPDATE_URL)) {
+    Services.prefs.clearUserPref(PREF_EXTENSIONS_UPDATE_URL);
+  }
+
   if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_IDLETIME)) {
     Services.prefs.clearUserPref(PREF_APP_UPDATE_IDLETIME);
   }
-  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_ENABLED)) {
-    Services.prefs.clearUserPref(PREF_APP_UPDATE_ENABLED);
-  }
-  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_LOG)) {
-    Services.prefs.clearUserPref(PREF_APP_UPDATE_LOG);
-  }
-  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_SHOW_INSTALLED_UI)) {
-    Services.prefs.clearUserPref(PREF_APP_UPDATE_SHOW_INSTALLED_UI);
-  }
+
   if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_URL_DETAILS)) {
     Services.prefs.clearUserPref(PREF_APP_UPDATE_URL_DETAILS);
   }
-  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_URL_OVERRIDE)) {
-    Services.prefs.clearUserPref(PREF_APP_UPDATE_URL_OVERRIDE);
+
+  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_SHOW_INSTALLED_UI)) {
+    Services.prefs.clearUserPref(PREF_APP_UPDATE_SHOW_INSTALLED_UI);
   }
-  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_IDLETIME)) {
-    Services.prefs.clearUserPref(PREF_APP_UPDATE_IDLETIME);
+
+  if (Services.prefs.prefHasUserValue(PREF_APP_UPDATE_LOG)) {
+    Services.prefs.clearUserPref(PREF_APP_UPDATE_LOG);
   }
+
   try {
     Services.prefs.deleteBranch(PREF_APP_UPDATE_NEVER_BRANCH);
   }
   catch(e) {
   }
+}
+
+
+
+
+
+
+
+
+
+
+
+function setupAddons(aCallback) {
+  debugDump("Entering setupAddons");
+
+  
+  
+  
+  function setNoUpdateAddonsDisabledState() {
+    AddonManager.getAllAddons(function(aAddons) {
+      aAddons.forEach(function(aAddon) {
+        if (aAddon.name.indexOf("noupdate") != 0)
+          return;
+
+        if (gDisableNoUpdateAddon) {
+          if (!aAddon.userDisabled) {
+            aAddon.userDisabled = true;
+          }
+        }
+        else {
+          if (aAddon.userDisabled) {
+            aAddon.userDisabled = false;
+          }
+        }
+      });
+      aCallback();
+    });
+  }
+
+  
+  
+  
+  
+  
+  if (Services.prefs.prefHasUserValue(PREF_DISABLEDADDONS)) {
+    setNoUpdateAddonsDisabledState();
+    return;
+  }
+
+  
+  
+  AddonManager.getAllAddons(function(aAddons) {
+    let disabledAddons = [];
+    aAddons.forEach(function(aAddon) {
+      
+      
+      
+      if (aAddon.type != "plugin" && !aAddon.appDisabled &&
+          !aAddon.userDisabled) {
+        disabledAddons.push(aAddon);
+        aAddon.userDisabled = true;
+      }
+    });
+    
+    
+    Services.prefs.setCharPref(PREF_DISABLEDADDONS, disabledAddons.join(" "));
+
+    
+    let xpiFiles = getTestAddonXPIFiles();
+    let xpiCount = xpiFiles.length;
+    let installs = [];
+    xpiFiles.forEach(function(aFile) {
+      AddonManager.getInstallForFile(aFile, function(aInstall) {
+        if (!aInstall) {
+          throw "No AddonInstall created for " + aFile.path;
+        }
+
+        installs.push(aInstall);
+
+        if (--xpiCount == 0) {
+          let installCount = installs.length;
+          function installCompleted(aInstall) {
+            aInstall.removeListener(listener);
+
+            if (getAddonTestType(aInstall.addon.name) == "userdisabled") {
+              aInstall.addon.userDisabled = true;
+            }
+            if (--installCount == 0) {
+              setNoUpdateAddonsDisabledState();
+            }
+          }
+
+          let listener = {
+            onDownloadFailed: installCompleted,
+            onDownloadCancelled: installCompleted,
+            onInstallFailed: installCompleted,
+            onInstallCancelled: installCompleted,
+            onInstallEnded: installCompleted
+          };
+
+          installs.forEach(function(aInstall) {
+            aInstall.addListener(listener);
+            aInstall.install();
+          });
+        }
+      });
+    });
+  });
+}
+
+
+
+
+
+
+
+
+function resetAddons(aCallback) {
+  debugDump("Entering resetAddons");
+  
+  
+  
+  if (!Services.prefs.prefHasUserValue(PREF_DISABLEDADDONS)) {
+    debugDump("resetAddons - preference " + PREF_DISABLEDADDONS + " doesn't " +
+              "exist... returning early");
+    aCallback();
+    return;
+  }
+
+  
+  let count = TEST_ADDONS.length;
+  function uninstallCompleted(aAddon) {
+    if (--count == 0) {
+      AddonManager.removeAddonListener(listener);
+
+      
+      
+      let disabledAddons = Services.prefs.getCharPref(PREF_DISABLEDADDONS).split(" ");
+      Services.prefs.clearUserPref(PREF_DISABLEDADDONS);
+      AddonManager.getAllAddons(function(aAddons) {
+        aAddons.forEach(function(aAddon) {
+          if (disabledAddons.indexOf(aAddon.id)) {
+            aAddon.userDisabled = false;
+          }
+        });
+        aCallback();
+      });
+    }
+  }
+
+  let listener = {
+    onUninstalled: uninstallCompleted
+  };
+
+  AddonManager.addAddonListener(listener);
+  TEST_ADDONS.forEach(function(aName) {
+    AddonManager.getAddonByID(aName + ADDON_ID_SUFFIX, function(aAddon) {
+      aAddon.uninstall();
+    });
+  });
+}
+
+
+
+
+
+
+
+
+
+
+function getAddonTestType(aName) {
+  return aName.split("_")[0];
+}
+
+
+
+
+
+
+
+function getTestAddonXPIFiles() {
+  let addonPrepDir = Services.dirsvc.get(NS_APP_USER_PROFILE_50_DIR,
+                                         AUS_Ci.nsILocalFile);
+  addonPrepDir.append(ADDON_PREP_DIR);
+
+  let bootstrap = addonPrepDir.clone();
+  bootstrap.append("bootstrap.js");
+  
+  if (!bootstrap.exists()) {
+    let bootstrapContents = "function install(data, reason){ }\n" +
+                            "function startup(data, reason){ }\n" +
+                            "function shutdown(data, reason){ }\n" +
+                            "function uninstall(data, reason){ }\n";
+    writeFile(bootstrap, bootstrapContents);
+  }
+
+  let installRDF = addonPrepDir.clone();
+  installRDF.append("install.rdf");
+
+  let xpiFiles = [];
+  TEST_ADDONS.forEach(function(aName) {
+    let xpiFile = addonPrepDir.clone();
+    xpiFile.append(aName + ".xpi");
+
+    if (installRDF.exists())
+      installRDF.remove(false);
+    writeFile(installRDF, getInstallRDFString(aName));
+    gZipW.open(xpiFile, PR_RDWR | PR_CREATE_FILE | PR_TRUNCATE);
+    gZipW.addEntryFile(installRDF.leafName,
+                       AUS_Ci.nsIZipWriter.COMPRESSION_DEFAULT, installRDF,
+                       false);
+    gZipW.addEntryFile(bootstrap.leafName,
+                       AUS_Ci.nsIZipWriter.COMPRESSION_DEFAULT, bootstrap,
+                       false);
+    gZipW.close();
+    xpiFiles.push(xpiFile);
+  });
+
+  return xpiFiles;
+}
+
+
+
+
+
+
+
+
+
+
+
+function getInstallRDFString(aName) {
+  let maxVersion = Services.appinfo.platformVersion;
+  switch (getAddonTestType(aName)) {
+    case "compatible":
+      maxVersion = getNewerPlatformVersion();
+      break;
+    case "appdisabled":
+      maxVersion = "0.1";
+      break;
+  }
+
+  return "<?xml version=\"1.0\"?>\n" +
+         "<RDF xmlns=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" +
+         "  xmlns:em=\"http://www.mozilla.org/2004/em-rdf#\">\n" +
+         "  <Description about=\"urn:mozilla:install-manifest\">\n" +
+         "    <em:id>" + aName + ADDON_ID_SUFFIX + "</em:id>\n" +
+         "    <em:version>1.0</em:version>\n" +
+         "    <em:bootstrap>true</em:bootstrap>\n" +
+         "    <em:name>" + aName + "</em:name>\n" +
+         "    <em:description>Test Description</em:description>\n" +
+         "    <em:targetApplication>\n" +
+         "      <Description>\n" +
+         "        <em:id>toolkit@mozilla.org</em:id>\n" +
+         "        <em:minVersion>undefined</em:minVersion>\n" +
+         "        <em:maxVersion>" + maxVersion + "</em:maxVersion>\n" +
+         "      </Description>\n" +
+         "    </em:targetApplication>\n" +
+         "  </Description>\n" +
+         "</RDF>";
 }
 
 
