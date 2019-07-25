28ccd10e706aaf1312bb27ee1efadf333a85e4b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:00:29 +0000

    Bug 577899, part 2: Add a DebugOnly helper to mfbt, which only contains a value in debug builds. r=luke
    UltraBlame original commit: bfef135a83dcc6d9e2a1bf9697ff5ab5ddab0172

--- mfbt/Util.h
+++ mfbt/Util.h
@@ -42,6 +42,51 @@
 
 namespace mozilla {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+template <typename T>
+struct DebugOnly
+{
+#ifdef DEBUG
+    T value;
+
+    DebugOnly() {}
+    DebugOnly(const T& other) : value(other) {}
+    DebugOnly& operator=(const T& rhs) {
+        value = rhs;
+        return *this;
+    }
+
+    operator T&() { return value; }
+    operator const T&() const { return value; }
+
+#else
+    DebugOnly() {}
+    DebugOnly(const T&) {}
+    DebugOnly& operator=(const T&) {}   
+#endif
+
+    
+    
+    
+    ~DebugOnly() {}
+};
+
 } 
 
 #endif  
