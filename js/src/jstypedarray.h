e36153cae5ef1e491fcac2eeae525fb7f1b864e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:44:06 +0000

    Bug 785167 - Copy ArrayBuffer data to separately-allocated storage when JS_GetArrayBufferData is called
    UltraBlame original commit: f6d0c187d568fdaf4bb94fc7b37a05075568922c

--- js/src/jstypedarray.h
+++ js/src/jstypedarray.h
@@ -157,6 +157,13 @@ class ArrayBufferObject : public JSObject
     bool
     allocateSlots(JSContext *cx, uint32_t size, uint8_t *contents = NULL);
 
+    
+
+
+
+    bool
+    uninlineData(JSContext *cx);
+
     inline uint32_t byteLength() const;
 
     inline uint8_t * dataPointer() const;
