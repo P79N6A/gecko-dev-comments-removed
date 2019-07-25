2e9f567ff6881556e6b54476fedcde5014105392
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:05:56 +0000

    Bug 696849 - Add test cases observing cross-domain XHR in chrome code; r=bz
    UltraBlame original commit: a0b6d0a04e4a200b24299857e7f3b76985c234e6

--- netwerk/test/unit/test_XHR_redirects.js
+++ netwerk/test/unit/test_XHR_redirects.js
@@ -6,14 +6,20 @@
 
 do_load_httpd_js();
 
-var server;
+var sSame;
+var sOther;
+
 const BUGID = "676059";
+const OTHERBUGID = "696849";
+
+const pSame = 4444;
+const pOther = 4445;
 
 function createXHR(async, method, path)
 {
   var xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"]
             .createInstance(Ci.nsIXMLHttpRequest);
-  xhr.open(method, "http://localhost:4444" + path, async);
+  xhr.open(method, "http://localhost:" + pSame + path, async);
   return xhr;
 }
 
@@ -34,17 +40,29 @@ function checkResults(xhr, method, status)
 
 function run_test() {
   
-  server = new nsHttpServer();
-
-  server.registerPathHandler("/bug" + BUGID + "-redirect301", bug676059redirect301);
-  server.registerPathHandler("/bug" + BUGID + "-redirect302", bug676059redirect302);
-  server.registerPathHandler("/bug" + BUGID + "-redirect303", bug676059redirect303);
-  server.registerPathHandler("/bug" + BUGID + "-redirect307", bug676059redirect307);
-  server.registerPathHandler("/bug" + BUGID + "-target", bug676059target);
-
-  server.start(4444);
+  sSame = new nsHttpServer();
+  
+  
+  sSame.registerPathHandler("/bug" + BUGID + "-redirect301", bug676059redirect301);
+  sSame.registerPathHandler("/bug" + BUGID + "-redirect302", bug676059redirect302);
+  sSame.registerPathHandler("/bug" + BUGID + "-redirect303", bug676059redirect303);
+  sSame.registerPathHandler("/bug" + BUGID + "-redirect307", bug676059redirect307);
 
   
+  sSame.registerPathHandler("/bug" + OTHERBUGID + "-redirect301", bug696849redirect301);
+  sSame.registerPathHandler("/bug" + OTHERBUGID + "-redirect302", bug696849redirect302);
+  sSame.registerPathHandler("/bug" + OTHERBUGID + "-redirect303", bug696849redirect303);
+  sSame.registerPathHandler("/bug" + OTHERBUGID + "-redirect307", bug696849redirect307);
+  
+  
+  sSame.registerPathHandler("/bug" + BUGID + "-target", echoMethod);
+  sSame.start(pSame);
+
+  
+  sOther = new nsHttpServer();
+  sOther.registerPathHandler("/bug" + OTHERBUGID + "-target", echoMethod);
+  sOther.start(pOther);
+
   
   
   
@@ -52,7 +70,9 @@ function run_test() {
   
   
   
-    
+  
+  
+  
   var tests = [
     
     [301, "DELETE", "GET", 200], 
@@ -84,6 +104,9 @@ function run_test() {
     [307, "PROPFIND", "PROPFIND", 200],
   ];
 
+  
+  var othertests = tests; 
+
   var xhr;
   
   for (var i = 0; i < tests.length; ++i) {
@@ -93,35 +116,80 @@ function run_test() {
     checkResults(xhr, tests[i][2], tests[i][3]);
   }  
 
-  server.stop(do_test_finished);
+  for (var i = 0; i < othertests.length; ++i) {
+    dump("Testing " + othertests[i] + " (cross-origin)\n");
+    xhr = createXHR(false, othertests[i][1], "/bug" + OTHERBUGID + "-redirect" + othertests[i][0]);
+    xhr.send(null);
+    checkResults(xhr, othertests[i][2], tests[i][3]);
+  }  
+
+  sSame.stop(do_test_finished);
+  sOther.stop(do_test_finished);
 }
  
+function redirect(metadata, response, status, port, bugid) {
+  
+  
+  var reason;
+  if (status == 301) {
+    reason = "Moved Permanently";
+  }
+  else if (status == 302) {
+    reason = "Found";
+  }
+  else if (status == 303) {
+    reason = "See Other";
+  }
+  else if (status == 307) {
+    reason = "Temporary Redirect";
+  }
+  
+  response.setStatusLine(metadata.httpVersion, status, reason);
+  response.setHeader("Location", "http://localhost:" + port + "/bug" + bugid + "-target");
+} 
  
+
 function bug676059redirect301(metadata, response) {
-  response.setStatusLine(metadata.httpVersion, 301, "Moved Permanently");
-  response.setHeader("Location", "http://localhost:4444/bug" + BUGID + "-target");
+  redirect(metadata, response, 301, pSame, BUGID);
 }
 
- 
+
+function bug696849redirect301(metadata, response) {
+  redirect(metadata, response, 301, pOther, OTHERBUGID);
+}
+
+
 function bug676059redirect302(metadata, response) {
-  response.setStatusLine(metadata.httpVersion, 302, "Found");
-  response.setHeader("Location", "http://localhost:4444/bug" + BUGID + "-target");
+  redirect(metadata, response, 302, pSame, BUGID);
 }
 
- 
+
+function bug696849redirect302(metadata, response) {
+  redirect(metadata, response, 302, pOther, OTHERBUGID);
+}
+
+
 function bug676059redirect303(metadata, response) {
-  response.setStatusLine(metadata.httpVersion, 303, "See Other");
-  response.setHeader("Location", "http://localhost:4444/bug" + BUGID + "-target");
+  redirect(metadata, response, 303, pSame, BUGID);
 }
 
- 
+
+function bug696849redirect303(metadata, response) {
+  redirect(metadata, response, 303, pOther, OTHERBUGID);
+}
+
+
 function bug676059redirect307(metadata, response) {
-  response.setStatusLine(metadata.httpVersion, 307, "Temporary Redirect");
-  response.setHeader("Location", "http://localhost:4444/bug" + BUGID + "-target");
+  redirect(metadata, response, 307, pSame, BUGID);
 }
 
- 
-function bug676059target(metadata, response) {
+
+function bug696849redirect307(metadata, response) {
+  redirect(metadata, response, 307, pOther, OTHERBUGID);
+}
+
+
+function echoMethod(metadata, response) {
   response.setStatusLine(metadata.httpVersion, 200, "OK");
   response.setHeader("X-Received-Method", metadata.method);
 }
