b4acbef8e19b007a7ff3faf5d565bdb9d9b85c75
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:33:46 +0000

    Bug 592352 - 'Assertbotch on shutdown after IDB solo mochitest -- leaked contexts?'. r=bent.
    UltraBlame original commit: 834e5fb759c2d7e97d6c18bf96847cfc48d064c9

--- js/src/jsfun.cpp
+++ js/src/jsfun.cpp
@@ -1375,24 +1375,6 @@ static void
 call_trace(JSTracer *trc, JSObject *obj)
 {
     JS_ASSERT(obj->isCall());
-    JSStackFrame *fp = (JSStackFrame *) obj->getPrivate();
-    if (fp) {
-        
-
-
-
-
-
-
-        uintN first = JSSLOT_PRIVATE + JSObject::CALL_RESERVED_SLOTS + 1;
-        JS_ASSERT(first <= JS_INITIAL_NSLOTS);
-
-        uintN count = fp->fun()->countArgsAndVars();
-        uintN fixed = JS_MIN(count, JS_INITIAL_NSLOTS - first);
-
-        SetValueRangeToUndefined(&obj->fslots[first], fixed);
-        SetValueRangeToUndefined(obj->dslots, count - fixed);
-    }
 
     MaybeMarkGenerator(trc, obj);
 }
