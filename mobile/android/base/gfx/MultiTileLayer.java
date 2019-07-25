74fbb9d40bdc9da6d112bbbd197c630e30736867
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:29:20 +0000

    Bug 709120 - Add MultiTileLayer to Android native fennec. r=pcwalton
    This adds a new layer type that uses multiple SingleTileLayer's to implement
    tiled layer support. This also assumes a back-buffer that is tiled, so that
    texture updates can remain fast (relatively speaking).
    
    UltraBlame original commit: 4a363485a656177d6cf1e4ea320943293a195c2c

---
+++ mobile/android/base/gfx/MultiTileLayer.java
@@ -0,0 +1,228 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package org.mozilla.gecko.gfx;
+
+import org.mozilla.gecko.gfx.CairoImage;
+import org.mozilla.gecko.gfx.IntSize;
+import org.mozilla.gecko.gfx.SingleTileLayer;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.util.Log;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import javax.microedition.khronos.opengles.GL10;
+
+
+
+
+
+
+public class MultiTileLayer extends Layer {
+    private static final String LOGTAG = "GeckoMultiTileLayer";
+
+    private final CairoImage mImage;
+    private IntSize mTileSize;
+    private IntSize mBufferSize;
+    private final ArrayList<SingleTileLayer> mTiles;
+
+    public MultiTileLayer(CairoImage image, IntSize tileSize) {
+        super();
+
+        mImage = image;
+        mTileSize = tileSize;
+        mBufferSize = new IntSize(0, 0);
+        mTiles = new ArrayList<SingleTileLayer>();
+    }
+
+    public void invalidate(Rect dirtyRect) {
+        if (!inTransaction())
+            throw new RuntimeException("invalidate() is only valid inside a transaction");
+
+        int x = 0, y = 0;
+        IntSize size = getSize();
+        for (SingleTileLayer layer : mTiles) {
+            Rect tileRect = new Rect(x, y, x + mTileSize.width, y + mTileSize.height);
+
+            if (tileRect.intersect(dirtyRect)) {
+                tileRect.offset(-x, -y);
+                layer.invalidate(tileRect);
+            }
+
+            x += mTileSize.width;
+            if (x >= size.width) {
+                x = 0;
+                y += mTileSize.height;
+            }
+        }
+    }
+
+    public void invalidate() {
+        for (SingleTileLayer layer : mTiles)
+            layer.invalidate();
+    }
+
+    @Override
+    public IntSize getSize() {
+        return mImage.getSize();
+    }
+
+    private void validateTiles() {
+        IntSize size = getSize();
+
+        if (size.equals(mBufferSize))
+            return;
+
+        
+        mTiles.clear();
+        int offset = 0;
+        final int format = mImage.getFormat();
+        final ByteBuffer buffer = mImage.getBuffer().slice();
+        final int bpp = CairoUtils.bitsPerPixelForCairoFormat(format) / 8;
+        for (int y = 0; y < size.height; y += mTileSize.height) {
+            for (int x = 0; x < size.width; x += mTileSize.width) {
+                
+                
+                
+                final IntSize layerSize =
+                    new IntSize(Math.min(mTileSize.width, size.width - x),
+                                Math.min(mTileSize.height, size.height - y));
+                final int tileOffset = offset;
+
+                CairoImage subImage = new CairoImage() {
+                    @Override
+                    public ByteBuffer getBuffer() {
+                        
+                        
+                        
+                        buffer.position(tileOffset);
+                        ByteBuffer tileBuffer = buffer.slice();
+                        tileBuffer.limit(layerSize.getArea() * bpp);
+
+                        return tileBuffer;
+                    }
+
+                    @Override
+                    public IntSize getSize() {
+                        return layerSize;
+                    }
+
+                    @Override
+                    public int getFormat() {
+                        return format;
+                    }
+                };
+
+                mTiles.add(new SingleTileLayer(subImage));
+                offset += layerSize.getArea() * bpp;
+            }
+        }
+
+        
+        refreshTileMetrics(getOrigin(), getResolution(), false);
+
+        mBufferSize = size;
+    }
+
+    @Override
+    protected void performUpdates(GL10 gl) {
+        super.performUpdates(gl);
+
+        validateTiles();
+
+        for (SingleTileLayer layer : mTiles)
+            layer.performUpdates(gl);
+    }
+
+    private void refreshTileMetrics(Point origin, float resolution, boolean inTransaction) {
+        int x = 0, y = 0;
+        IntSize size = getSize();
+        for (SingleTileLayer layer : mTiles) {
+            if (!inTransaction)
+                layer.beginTransaction(null);
+
+            if (origin != null)
+                layer.setOrigin(new Point(origin.x + x, origin.y + y));
+            if (resolution >= 0.0f)
+                layer.setResolution(resolution);
+
+            if (!inTransaction)
+                layer.endTransaction();
+
+            x += mTileSize.width;
+            if (x >= size.width) {
+                x = 0;
+                y += mTileSize.height;
+            }
+        }
+    }
+
+    @Override
+    public void setOrigin(Point newOrigin) {
+        super.setOrigin(newOrigin);
+        refreshTileMetrics(newOrigin, -1, true);
+    }
+
+    @Override
+    public void setResolution(float newResolution) {
+        super.setResolution(newResolution);
+        refreshTileMetrics(null, newResolution, true);
+    }
+
+    @Override
+    public void beginTransaction(LayerView aView) {
+        super.beginTransaction(aView);
+
+        for (SingleTileLayer layer : mTiles)
+            layer.beginTransaction(aView);
+    }
+
+    @Override
+    public void endTransaction() {
+        for (SingleTileLayer layer : mTiles)
+            layer.endTransaction();
+
+        super.endTransaction();
+    }
+
+    @Override
+    public void draw(RenderContext context) {
+        for (SingleTileLayer layer : mTiles)
+            layer.draw(context);
+    }
+}
+
