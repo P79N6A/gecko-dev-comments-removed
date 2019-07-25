93c72e5e92046db7ea23013e5999e240fe4c75b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:22:11 +0000

    Bug 660535 Remove useless use of PromiseFlatCString r=cjones
    UltraBlame original commit: fba5bf34ad726a881418e73b08e67cdecda08314

--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -383,7 +383,7 @@ GeckoChildProcessHost::PerformAsyncLaunch(std::vector<std::string> aExtraOpts, b
   
   static char* restoreOrigLogName = 0;
   if (!restoreOrigLogName)
-    restoreOrigLogName = strdup(PromiseFlatCString(setChildLogName).get());
+    restoreOrigLogName = strdup(setChildLogName.get());
 
   
   setChildLogName.AppendLiteral(".child-");
@@ -391,7 +391,7 @@ GeckoChildProcessHost::PerformAsyncLaunch(std::vector<std::string> aExtraOpts, b
 
   
   
-  PR_SetEnv(PromiseFlatCString(setChildLogName).get());
+  PR_SetEnv(setChildLogName.get());
   bool retval = PerformAsyncLaunchInternal(aExtraOpts, arch);
 
   
