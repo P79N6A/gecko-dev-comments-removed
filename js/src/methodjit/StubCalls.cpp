b72ab0e9c749d3cd9ecea2c8c668aafc3d6665e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:29:24 +0000

    Fix DEFFUN creating a sync state mismatch (bug 590083, r=dmandelin).
    UltraBlame original commit: 5a12156930e966aea0bf3e9e48ea7d44c489740d

--- js/src/methodjit/StubCalls.cpp
+++ js/src/methodjit/StubCalls.cpp
@@ -824,13 +824,12 @@ stubs::DecLocal(VMFrame &f, uint32 slot)
 }
 
 void JS_FASTCALL
-stubs::DefFun(VMFrame &f, uint32 index)
+stubs::DefFun(VMFrame &f, JSFunction *fun)
 {
     JSObject *obj2;
 
     JSContext *cx = f.cx;
     JSStackFrame *fp = f.fp();
-    JSScript *script = fp->getScript();
 
     
 
@@ -838,7 +837,6 @@ stubs::DefFun(VMFrame &f, uint32 index)
 
 
 
-    JSFunction *fun = script->getFunction(index);
     JSObject *obj = FUN_OBJECT(fun);
 
     if (FUN_NULL_CLOSURE(fun)) {
