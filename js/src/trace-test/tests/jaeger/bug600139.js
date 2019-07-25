2e0383335ceb9f836e4041aeb69ecf0fd72e7e7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:47:18 +0000

    Fix Bug 600163 - JM: Assertion failure: checkedFreeRegs == freeRegs. This only hits on ARM at the moment because it is hidden by PICs enabled on other platforms. r=dmandelin.
    UltraBlame original commit: 16eee4664dafb9dd17e59981a589b3fb46393662

---
+++ js/src/trace-test/tests/jaeger/bug600139.js
@@ -0,0 +1,11 @@
+
+
+function f(a, b, c) {
+    if (!a.__SSi) {
+        throw Components.returnCode = Cr.NS_ERROR_INVALID_ARG;
+    }
+    this.restoreWindow(a, b, c);
+    eval();
+}
+dis(f);
+f(1, 2, 3);
