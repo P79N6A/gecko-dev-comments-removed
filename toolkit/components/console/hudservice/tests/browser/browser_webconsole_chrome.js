9e6851a36424f0c37702ab8e0babfef6c031b1ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:04:58 +0000

    Bug 604722 - Web Console won't open on chrome content, r+a=gavin
    UltraBlame original commit: 7a48f839ccf1be7213a2a01e300f31c58942665c

---
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_chrome.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const TEST_URI = "chrome://browser/content/browser.xul";
+
+function test() {
+  addTab(TEST_URI);
+  browser.addEventListener("DOMContentLoaded", testChrome, false);
+}
+
+function testChrome() {
+  browser.removeEventListener("DOMContentLoaded", testChrome, false);
+
+  openConsole();
+
+  hudId = HUDService.displaysIndex()[0];
+  hud = HUDService.hudWeakReferences[hudId].get();
+  ok(hud, "we have a console");
+  
+  hudBox = HUDService.getHeadsUpDisplay(hudId);
+  ok(hudBox, "we have the console display");
+  
+  let jsterm = hud.jsterm;
+  ok(jsterm, "we have a jsterm");
+
+  let input = jsterm.inputNode;
+  let outputNode = hudBox.querySelector(".jsterm-input-node");
+  ok(outputNode, "we have an output node");
+
+  
+  input.value = "docu";
+  input.setSelectionRange(4, 4);
+  jsterm.complete(jsterm.COMPLETE_HINT_ONLY);
+  is(input.value, "document", "'docu' completion");
+  is(input.selectionStart, 4, "start selection is alright");
+  is(input.selectionEnd, 8, "end selection is alright");
+
+  HUD = jsterm = input = null;
+  finishTest();
+}
+
