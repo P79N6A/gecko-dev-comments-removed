06fe51419e3ddecebfabcf93fee13f8fcfe9815e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:26:36 +0000

    Fix compartment mismatches in js/jetpack (bug 624366, r=jst).
    UltraBlame original commit: 9d8a15c1f22f331c17c3e7706511a32373b60494

--- js/jetpack/Handle.h
+++ js/jetpack/Handle.h
@@ -164,14 +164,18 @@ private:
         js::AutoObjectRooter obj(mCx, mObj);
         mObj = NULL;
 
-        JSBool hasOnInvalidate;
-        if (JS_HasProperty(mCx, obj.object(), "onInvalidate",
-                           &hasOnInvalidate) && hasOnInvalidate) {
-          js::AutoValueRooter r(mCx);
-          JSBool ok = JS_CallFunctionName(mCx, obj.object(), "onInvalidate", 0,
-                                          NULL, r.jsval_addr());
-          if (!ok)
-            JS_ReportPendingException(mCx);
+        
+        JSAutoEnterCompartment ac;
+        if (ac.enter(mCx, obj.object())) {
+          JSBool hasOnInvalidate;
+          if (JS_HasProperty(mCx, obj.object(), "onInvalidate",
+                             &hasOnInvalidate) && hasOnInvalidate) {
+            js::AutoValueRooter r(mCx);
+            JSBool ok = JS_CallFunctionName(mCx, obj.object(), "onInvalidate", 0,
+                                            NULL, r.jsval_addr());
+            if (!ok)
+              JS_ReportPendingException(mCx);
+          }
         }
 
         
