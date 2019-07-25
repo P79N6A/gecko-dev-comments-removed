58394d7431961b301a6b923e7c0e11f8a3f64c12
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:04:25 +0000

    fix jsiter, convert jsmath
    UltraBlame original commit: a8434208742e340630ce2dfb63c05e9bfe70c419

--- js/src/jsnum.h
+++ js/src/jsnum.h
@@ -1,41 +1,41 @@
-/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- *
- * ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Mozilla Communicator client code, released
- * March 31, 1998.
- *
- * The Initial Developer of the Original Code is
- * Netscape Communications Corporation.
- * Portions created by the Initial Developer are Copyright (C) 1998
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either of the GNU General Public License Version 2 or later (the "GPL"),
- * or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #ifndef jsnum_h___
 #define jsnum_h___
@@ -52,20 +52,20 @@
 #include "jsstr.h"
 #include "jsobj.h"
 
-/*
- * JS number (IEEE double) interface.
- *
- * JS numbers are optimistically stored in the top 31 bits of 32-bit integers,
- * but floating point literals, results that overflow 31 bits, and division and
- * modulus operands and results require a 64-bit IEEE double.  These are GC'ed
- * and pointed to by 32-bit jsvals on the stack and in object properties.
- */
 
-/*
- * The ARM architecture supports two floating point models: VFP and FPA. When
- * targetting FPA, doubles are mixed-endian on little endian ARMs (meaning that
- * the high and low words are in big endian order).
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
 #if defined(__arm) || defined(__arm32__) || defined(__arm26__) || defined(__arm__)
 #if !defined(__VFP_FP__)
 #define FPU_IS_ARM_FPA
@@ -174,7 +174,7 @@ extern jsdouble js_NaN;
 extern jsdouble js_PositiveInfinity;
 extern jsdouble js_NegativeInfinity;
 
-/* Initialize number constants and runtime state for the first context. */
+
 extern JSBool
 js_InitRuntimeNumberState(JSContext *cx);
 
@@ -184,7 +184,7 @@ js_TraceRuntimeNumberState(JSTracer *trc);
 extern void
 js_FinishRuntimeNumberState(JSContext *cx);
 
-/* Initialize the Number class, returning its prototype object. */
+
 extern js::Class js_NumberClass;
 
 inline bool
@@ -196,9 +196,9 @@ JSObject::isNumber() const
 extern JSObject *
 js_InitNumberClass(JSContext *cx, JSObject *obj);
 
-/*
- * String constants for global function names, used in jsapi.c and jsnum.c.
- */
+
+
+
 extern const char js_Infinity_str[];
 extern const char js_NaN_str[];
 extern const char js_isNaN_str[];
@@ -212,19 +212,19 @@ js_IntToString(JSContext *cx, jsint i);
 extern JSString * JS_FASTCALL
 js_NumberToString(JSContext *cx, jsdouble d);
 
-/*
- * Convert an integer or double (contained in the given jsval) to a string and
- * append to the given buffer.
- */
+
+
+
+
 extern JSBool JS_FASTCALL
 js_NumberValueToCharBuffer(JSContext *cx, jsval v, JSCharBuffer &cb);
 
 namespace js {
 
-/*
- * Convert a value to a number, returning the converted value in 'out' if the
- * conversion succeeds. As a side effect, *vp will be mutated to match *out.
- */
+
+
+
+
 JS_ALWAYS_INLINE bool
 ValueToNumber(JSContext *cx, js::Value *vp, double *out)
 {
@@ -240,7 +240,7 @@ ValueToNumber(JSContext *cx, js::Value *vp, double *out)
     return ValueToNumberSlow(cx, vp, out);
 }
 
-/* Convert a value to a number, replacing 'vp' with the converted value. */
+
 JS_ALWAYS_INLINE bool
 ValueToNumber(JSContext *cx, js::Value *vp)
 {
@@ -253,11 +253,11 @@ ValueToNumber(JSContext *cx, js::Value *vp)
     return ValueToNumberSlow(cx, vp, &_);
 }
 
-/*
- * Convert a value to an int32 or uint32, according to the ECMA rules for
- * ToInt32 and ToUint32. Return converted value in *out on success, !ok on
- * failure. As a side effect, *vp will be mutated to match *out.
- */
+
+
+
+
+
 JS_ALWAYS_INLINE bool
 ValueToECMAInt32(JSContext *cx, js::Value *vp, int32_t *out)
 {
@@ -280,11 +280,11 @@ ValueToECMAUint32(JSContext *cx, js::Value *vp, uint32_t *out)
     return ValueToECMAUint32Slow(cx, vp, out);
 }
 
-/*
- * Convert a value to a number, then to an int32 if it fits by rounding to
- * nearest. Return converted value in *out on success, !ok on failure. As a
- * side effect, *vp will be mutated to match *out.
- */
+
+
+
+
+
 JS_ALWAYS_INLINE bool
 ValueToInt32(JSContext *cx, js::Value *vp, int32_t *out)
 {
@@ -296,11 +296,11 @@ ValueToInt32(JSContext *cx, js::Value *vp, int32_t *out)
     return ValueToInt32Slow(cx, vp, out);
 }
 
-/*
- * Convert a value to a number, then to a uint16 according to the ECMA rules
- * for ToUint16. Return converted value on success, !ok on failure. v must be a
- * copy of a rooted jsval.
- */
+
+
+
+
+
 JS_ALWAYS_INLINE bool
 ValueToUint16(JSContext *cx, js::Value *vp, uint16_t *out)
 {
@@ -312,21 +312,21 @@ ValueToUint16(JSContext *cx, js::Value *vp, uint16_t *out)
     return ValueToUint16Slow(cx, vp, out);
 }
 
-}  /* namespace js */
+}  
 
-/*
- * Specialized ToInt32 and ToUint32 converters for doubles.
- */
-/*
- * From the ES3 spec, 9.5
- *  2.  If Result(1) is NaN, +0, -0, +Inf, or -Inf, return +0.
- *  3.  Compute sign(Result(1)) * floor(abs(Result(1))).
- *  4.  Compute Result(3) modulo 2^32; that is, a finite integer value k of Number
- *      type with positive sign and less than 2^32 in magnitude such the mathematical
- *      difference of Result(3) and k is mathematically an integer multiple of 2^32.
- *  5.  If Result(4) is greater than or equal to 2^31, return Result(4)- 2^32,
- *  otherwise return Result(4).
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
 static inline int32
 js_DoubleToECMAInt32(jsdouble d)
 {
@@ -335,34 +335,34 @@ js_DoubleToECMAInt32(jsdouble d)
     uint32 di_h, u_tmp, expon, shift_amount;
     int32 mask32;
 
-    /*
-     * Algorithm Outline
-     *  Step 1. If d is NaN, +/-Inf or |d|>=2^84 or |d|<1, then return 0
-     *          All of this is implemented based on an exponent comparison.
-     *  Step 2. If |d|<2^31, then return (int)d
-     *          The cast to integer (conversion in RZ mode) returns the correct result.
-     *  Step 3. If |d|>=2^32, d:=fmod(d, 2^32) is taken  -- but without a call
-     *  Step 4. If |d|>=2^31, then the fractional bits are cleared before
-     *          applying the correction by 2^32:  d - sign(d)*2^32
-     *  Step 5. Return (int)d
-     */
+    
+
+
+
+
+
+
+
+
+
+
 
     du.d = d;
     di_h = du.s.hi;
 
     u_tmp = (di_h & 0x7ff00000) - 0x3ff00000;
     if (u_tmp >= (0x45300000-0x3ff00000)) {
-        // d is Nan, +/-Inf or +/-0, or |d|>=2^(32+52) or |d|<1, in which case result=0
+        
         return 0;
     }
 
     if (u_tmp < 0x01f00000) {
-        // |d|<2^31
+        
         return int32_t(d);
     }
 
     if (u_tmp > 0x01f00000) {
-        // |d|>=2^32
+        
         expon = u_tmp >> 20;
         shift_amount = expon - 21;
         duh.u64 = du.u64;
@@ -381,10 +381,10 @@ js_DoubleToECMAInt32(jsdouble d)
 
     di_h = du.s.hi;
 
-    // eliminate fractional bits
+    
     u_tmp = (di_h & 0x7ff00000);
     if (u_tmp >= 0x41e00000) {
-        // |d|>=2^31
+        
         expon = u_tmp >> 20;
         shift_amount = expon - (0x3ff - 11);
         mask32 = 0x80000000;
@@ -408,114 +408,114 @@ js_DoubleToECMAInt32(jsdouble d)
     uint32_t    tmp1;
     uint32_t    tmp2;
     asm (
-    // We use a pure integer solution here. In the 'softfp' ABI, the argument
-    // will start in r0 and r1, and VFP can't do all of the necessary ECMA
-    // conversions by itself so some integer code will be required anyway. A
-    // hybrid solution is faster on A9, but this pure integer solution is
-    // notably faster for A8.
+    
+    
+    
+    
+    
 
-    // %0 is the result register, and may alias either of the %[QR]1 registers.
-    // %Q4 holds the lower part of the mantissa.
-    // %R4 holds the sign, exponent, and the upper part of the mantissa.
-    // %1, %2 and %3 are used as temporary values.
+    
+    
+    
+    
 
-    // Extract the exponent.
+    
 "   mov     %1, %R4, LSR #20\n"
-"   bic     %1, %1, #(1 << 11)\n"  // Clear the sign.
+"   bic     %1, %1, #(1 << 11)\n"  
 
-    // Set the implicit top bit of the mantissa. This clobbers a bit of the
-    // exponent, but we have already extracted that.
+    
+    
 "   orr     %R4, %R4, #(1 << 20)\n"
 
-    // Special Cases
-    //   We should return zero in the following special cases:
-    //    - Exponent is 0x000 - 1023: +/-0 or subnormal.
-    //    - Exponent is 0x7ff - 1023: +/-INFINITY or NaN
-    //      - This case is implicitly handled by the standard code path anyway,
-    //        as shifting the mantissa up by the exponent will result in '0'.
-    //
-    // The result is composed of the mantissa, prepended with '1' and
-    // bit-shifted left by the (decoded) exponent. Note that because the r1[20]
-    // is the bit with value '1', r1 is effectively already shifted (left) by
-    // 20 bits, and r0 is already shifted by 52 bits.
     
-    // Adjust the exponent to remove the encoding offset. If the decoded
-    // exponent is negative, quickly bail out with '0' as such values round to
-    // zero anyway. This also catches +/-0 and subnormals.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 "   sub     %1, %1, #0xff\n"
 "   subs    %1, %1, #0x300\n"
 "   bmi     8f\n"
 
-    //  %1 = (decoded) exponent >= 0
-    //  %R4 = upper mantissa and sign
+    
+    
 
-    // ---- Lower Mantissa ----
-"   subs    %3, %1, #52\n"         // Calculate exp-52
+    
+"   subs    %3, %1, #52\n"         
 "   bmi     1f\n"
 
-    // Shift r0 left by exp-52.
-    // Ensure that we don't overflow ARM's 8-bit shift operand range.
-    // We need to handle anything up to an 11-bit value here as we know that
-    // 52 <= exp <= 1024 (0x400). Any shift beyond 31 bits results in zero
-    // anyway, so as long as we don't touch the bottom 5 bits, we can use
-    // a logical OR to push long shifts into the 32 <= (exp&0xff) <= 255 range.
+    
+    
+    
+    
+    
+    
 "   bic     %2, %3, #0xff\n"
 "   orr     %3, %3, %2, LSR #3\n"
-    // We can now perform a straight shift, avoiding the need for any
-    // conditional instructions or extra branches.
+    
+    
 "   mov     %Q4, %Q4, LSL %3\n"
 "   b       2f\n"
-"1:\n" // Shift r0 right by 52-exp.
-    // We know that 0 <= exp < 52, and we can shift up to 255 bits so 52-exp
-    // will always be a valid shift and we can sk%3 the range check for this case.
+"1:\n" 
+    
+    
 "   rsb     %3, %1, #52\n"
 "   mov     %Q4, %Q4, LSR %3\n"
 
-    //  %1 = (decoded) exponent
-    //  %R4 = upper mantissa and sign
-    //  %Q4 = partially-converted integer
+    
+    
+    
 
 "2:\n"
-    // ---- Upper Mantissa ----
-    // This is much the same as the lower mantissa, with a few different
-    // boundary checks and some masking to hide the exponent & sign bit in the
-    // upper word.
-    // Note that the upper mantissa is pre-shifted by 20 in %R4, but we shift
-    // it left more to remove the sign and exponent so it is effectively
-    // pre-shifted by 31 bits.
-"   subs    %3, %1, #31\n"          // Calculate exp-31
-"   mov     %1, %R4, LSL #11\n"     // Re-use %1 as a temporary register.
+    
+    
+    
+    
+    
+    
+    
+"   subs    %3, %1, #31\n"          
+"   mov     %1, %R4, LSL #11\n"     
 "   bmi     3f\n"
 
-    // Shift %R4 left by exp-31.
-    // Avoid overflowing the 8-bit shift range, as before.
+    
+    
 "   bic     %2, %3, #0xff\n"
 "   orr     %3, %3, %2, LSR #3\n"
-    // Perform the shift.
+    
 "   mov     %2, %1, LSL %3\n"
 "   b       4f\n"
-"3:\n" // Shift r1 right by 31-exp.
-    // We know that 0 <= exp < 31, and we can shift up to 255 bits so 31-exp
-    // will always be a valid shift and we can skip the range check for this case.
-"   rsb     %3, %3, #0\n"          // Calculate 31-exp from -(exp-31)
-"   mov     %2, %1, LSR %3\n"      // Thumb-2 can't do "LSR %3" in "orr".
+"3:\n" 
+    
+    
+"   rsb     %3, %3, #0\n"          
+"   mov     %2, %1, LSR %3\n"      
 
-    //  %Q4 = partially-converted integer (lower)
-    //  %R4 = upper mantissa and sign
-    //  %2 = partially-converted integer (upper)
+    
+    
+    
 
 "4:\n"
-    // Combine the converted parts.
+    
 "   orr     %Q4, %Q4, %2\n"
-    // Negate the result if we have to, and move it to %0 in the process. To
-    // avoid conditionals, we can do this by inverting on %R4[31], then adding
-    // %R4[31]>>31.
+    
+    
+    
 "   eor     %Q4, %Q4, %R4, ASR #31\n"
 "   add     %0, %Q4, %R4, LSR #31\n"
 "   b       9f\n"
 "8:\n"
-    // +/-INFINITY, +/-0, subnormals, NaNs, and anything else out-of-range that
-    // will result in a conversion of '0'.
+    
+    
 "   mov     %0, #0\n"
 "9:\n"
     : "=r" (i), "=&r" (tmp0), "=&r" (tmp1), "=&r" (tmp2)
@@ -545,10 +545,10 @@ js_DoubleToECMAInt32(jsdouble d)
 uint32
 js_DoubleToECMAUint32(jsdouble d);
 
-/*
- * Convert a jsdouble to an integral number, stored in a jsdouble.
- * If d is NaN, return 0.  If d is an infinity, return it without conversion.
- */
+
+
+
+
 static inline jsdouble
 js_DoubleToInteger(jsdouble d)
 {
@@ -567,29 +567,29 @@ js_DoubleToInteger(jsdouble d)
     return neg ? -d : d;
 }
 
-/*
- * Similar to strtod except that it replaces overflows with infinities of the
- * correct sign, and underflows with zeros of the correct sign.  Guaranteed to
- * return the closest double number to the given input in dp.
- *
- * Also allows inputs of the form [+|-]Infinity, which produce an infinity of
- * the appropriate sign.  The case of the "Infinity" string must match exactly.
- * If the string does not contain a number, set *ep to s and return 0.0 in dp.
- * Return false if out of memory.
- */
+
+
+
+
+
+
+
+
+
+
 extern JSBool
 js_strtod(JSContext *cx, const jschar *s, const jschar *send,
           const jschar **ep, jsdouble *dp);
 
-/*
- * Similar to strtol except that it handles integers of arbitrary size.
- * Guaranteed to return the closest double number to the given input when radix
- * is 10 or a power of 2.  Callers may see round-off errors for very large
- * numbers of a different radix than 10 or a power of 2.
- *
- * If the string does not contain a number, set *ep to s and return 0.0 in dp.
- * Return false if out of memory.
- */
+
+
+
+
+
+
+
+
+
 extern JSBool
 js_strtointeger(JSContext *cx, const jschar *s, const jschar *send,
                 const jschar **ep, jsint radix, jsdouble *dp);
@@ -613,6 +613,16 @@ Uint32ToValue(uint32_t u, Value *vp)
         vp->setDouble(u);
     else
         vp->setInt32((int32_t)u);
+}
+
+JS_ALWAYS_INLINE void
+Value::setNumber(double d)
+{
+    int32_t i;
+    if (JSDOUBLE_IS_INT32(d, i))
+        setInt32(i);
+    else
+        setDouble(d);
 }
 
 template<typename T> struct NumberTraits { };
@@ -648,9 +658,9 @@ StringToNumberType(JSContext *cx, JSString *str)
     str->getCharsAndEnd(bp, end);
     bp = js_SkipWhiteSpace(bp, end);
 
-    /* ECMA doesn't allow signed hex numbers (bug 273467). */
+    
     if (end - bp >= 2 && bp[0] == '0' && (bp[1] == 'x' || bp[1] == 'X')) {
-        /* Looks like a hex number. */
+        
         if (!js_strtointeger(cx, bp, end, &ep, 16, &d) ||
             js_SkipWhiteSpace(ep, end) != end) {
             return NumberTraits<T>::NaN();
@@ -658,13 +668,13 @@ StringToNumberType(JSContext *cx, JSString *str)
         return NumberTraits<T>::toSelfType(d);
     }
 
-    /*
-     * Note that ECMA doesn't treat a string beginning with a '0' as
-     * an octal number here. This works because all such numbers will
-     * be interpreted as decimal by js_strtod.  Also, any hex numbers
-     * that have made it here (which can only be negative ones) will
-     * be treated as 0 without consuming the 'x' by js_strtod.
-     */
+    
+
+
+
+
+
+
     if (!js_strtod(cx, bp, end, &ep, &d) ||
         js_SkipWhiteSpace(ep, end) != end) {
         return NumberTraits<T>::NaN();
@@ -674,4 +684,4 @@ StringToNumberType(JSContext *cx, JSString *str)
 }
 }
 
-#endif /* jsnum_h___ */
+#endif 
