b6fbf5f8f7e576dc342bd27d1da8cf7677a48b78
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 13:57:47 +0000

    Fix Windows optimized bustage from bug 636039 patch 15:  use placement new in a loop instead of using placement new[].
    UltraBlame original commit: 3766d8e25632cc0a4bf364b89017e9897bf37de3

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -1716,8 +1716,13 @@ struct AutoCSSValueArray {
   AutoCSSValueArray(void* aStorage, size_t aCount) {
     NS_ABORT_IF_FALSE(size_t(aStorage) % NS_ALIGNMENT_OF(nsCSSValue) == 0,
                       "bad alignment from alloca");
-    mArray = new (aStorage) nsCSSValue[aCount];
     mCount = aCount;
+    
+    
+    mArray = static_cast<nsCSSValue*>(aStorage);
+    for (size_t i = 0; i < mCount; ++i) {
+      new (mArray + i) nsCSSValue();
+    }
   }
 
   ~AutoCSSValueArray() {
