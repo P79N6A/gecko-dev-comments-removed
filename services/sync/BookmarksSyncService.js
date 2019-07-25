64ae86f3111f3897b70b9633787df76b308fd982
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:19:31 +0000

    move account 'creation' into the service
    UltraBlame original commit: 5ea79bb700900ba53277bd867e07337aa525e5c0

--- services/sync/BookmarksSyncService.js
+++ services/sync/BookmarksSyncService.js
@@ -1668,14 +1668,15 @@ DAVCollection.prototype = {
    
       this._authProvider._authFailed = false;
 
-      let request = this._makeRequest("GET", "", cont, headers);
+      
+      
+      let request = this._makeRequest("GET", "createAcct.php", cont, headers);
       request.send(null);
       let event = yield;
 
       if (this._authProvider._authFailed || event.target.status >= 400)
         return;
   
-      
       let branch = Cc["@mozilla.org/preferences-service;1"].
         getService(Ci.nsIPrefBranch);
       this._currentUser = branch.getCharPref("browser.places.sync.username");
@@ -1683,7 +1684,7 @@ DAVCollection.prototype = {
       
       let path = this._currentUser.split("@");
       this._currentUserPath = path[0];
-      this._baseURL = this._baseURL + this._currentUserPath + "/";
+      this._baseURL = this._baseURL + "user/" + this._currentUserPath + "/";
       this._loggedIn = true;
 
     } finally {
