657ed57c16820d8038b6c53eb5ae3caab79a97cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:05:57 +0000

    Bug 645860 - "Studies should have a way to write metadata" [r=mark.finkle]
    UltraBlame original commit: 64b8491e8833c4aa0b48069124745790cff4d09a

--- browser/app/profile/extensions/testpilot@labs.mozilla.com/modules/tasks.js
+++ browser/app/profile/extensions/testpilot@labs.mozilla.com/modules/tasks.js
@@ -552,6 +552,21 @@ TestPilotExperiment.prototype = {
     }
   },
 
+  getStudyMetadata: function TestPilotExperiment_getStudyMetadata() {
+    try {
+      if (this._handlers.getStudyMetadata) {
+        let metadata = this._handlers.getStudyMetadata();
+        if (metadata.length) {
+          
+          return metadata;
+        }
+      }
+    } catch(e) {
+      this._logger.warn("Error in getStudyMetadata: " + e);
+    }
+    return null;
+  },
+
   _reschedule: function TestPilotExperiment_reschedule() {
     
     
@@ -753,6 +768,15 @@ TestPilotExperiment.prototype = {
       json.metadata = md;
       json.metadata.task_guid = self.getGuid(self._id);
       json.metadata.event_headers = self._dataStore.getPropertyNames();
+      let moreMd = self.getStudyMetadata();
+      if (moreMd) {
+        for (let i = 0; i < moreMd.length; i++) {
+          if (moreMd[i].name && moreMd[i].value) {
+            json.metadata[ moreMd[i].name ] = moreMd[i].value; 
+            
+          }
+        }
+      }
       self._dataStore.getJSONRows(function(rows) {
                                     json.events = rows;
                                     callback( JSON.stringify(json) );
