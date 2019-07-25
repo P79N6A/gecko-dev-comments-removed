6a38a8cb090580e82f77c5ee5323d177a7bd0426
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:21:19 +0000

    small module to get an event at startup
    UltraBlame original commit: 029cf31c83cccde108820a48dac6b9b28eb70a3d

---
+++ services/sync/Weave.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+function WeaveService() {}
+WeaveService.prototype = {
+  classDescription: "Weave Service",
+  contractID: "@mozilla.org/weave/service;1",
+  classID: Components.ID("{74b89fb0-f200-4ae8-a3ec-dd164117f6de}"),
+  _xpcom_categories: [{ category: "app-startup", service: true }],
+
+  QueryInterface: XPCOMUtils.generateQI([Components.interfaces.nsIObserver,
+                                         Components.interfaces.nsISupportsWeakReference]),
+
+  observe: function BSS__observe(subject, topic, data) {
+    switch (topic) {
+    case "app-startup":
+      let os = Components.classes["@mozilla.org/observer-service;1"].
+        getService(Components.interfaces.nsIObserverService);
+      os.addObserver(this, "profile-after-change", true);
+      break;
+    case "profile-after-change":
+      Components.utils.import("resource://weave/service.js");
+      Weave.Service.onStartup();
+      break;
+    }
+  }
+};
+
+function NSGetModule(compMgr, fileSpec) {
+  return XPCOMUtils.generateModule([WeaveService]);
+}
+
