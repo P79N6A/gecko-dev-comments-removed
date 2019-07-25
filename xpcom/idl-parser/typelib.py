680c97cddea071d6fe73b328f7b9cfffa0e22977
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:50:56 +0000

    Backout e5815c156b6c (bug 683096), 9ca928d8095c (merge), a286edb8ca4b (bug 683875), 61dc35b8f63f (bug 683171), b0741ae44def (bug 683875), 8e01bc314326 + e2697c06468f (bug 677501), 805e28e85577 (bug 680440) for Win opt orange and purple galore, on a CLOSED TREE
    UltraBlame original commit: 472716252ea34db8e104890af192981688d01751

--- xpcom/idl-parser/typelib.py
+++ xpcom/idl-parser/typelib.py
@@ -94,9 +94,7 @@ def build_interface(iface, ifaces):
                                         reference=False)
 
         if isinstance(type, xpidl.Array):
-            
-            
-            return xpt.ArrayType(get_type(type.type, calltype, iid_is), size_is,
+            return xpt.ArrayType(get_type(type.type, calltype), size_is,
                                  
                                  size_is)
 
