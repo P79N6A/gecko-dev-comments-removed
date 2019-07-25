4e59c1291af7c4194538520b08f9cffdcad9ee45
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:26:09 +0000

    Bug 585858: fixing whitespace
    UltraBlame original commit: 669263738da5d2cb6c5f127544953ac6513515be

--- browser/base/content/test/tabview/browser_visibleTabs_contextMenu.js
+++ browser/base/content/test/tabview/browser_visibleTabs_contextMenu.js
@@ -38,49 +38,49 @@
 function test() {
   
   let [origTab] = gBrowser.visibleTabs;
-	is(gBrowser.visibleTabs.length, 1, "there is one visible tab");
+  is(gBrowser.visibleTabs.length, 1, "there is one visible tab");
   let testTab = gBrowser.addTab();
-	is(gBrowser.visibleTabs.length, 2, "there are now two visible tabs");
+  is(gBrowser.visibleTabs.length, 2, "there are now two visible tabs");
 
-	
-	popup(origTab);
-	is(TabContextMenu.contextTab, origTab, "TabContextMenu context is the original tab");
-	is(document.getElementById("context_closeTab").disabled, false, "Close Tab is enabled");
-	is(document.getElementById("context_reloadAllTabs").disabled, false, "Reload All Tabs is enabled");
+  
+  popup(origTab);
+  is(TabContextMenu.contextTab, origTab, "TabContextMenu context is the original tab");
+  is(document.getElementById("context_closeTab").disabled, false, "Close Tab is enabled");
+  is(document.getElementById("context_reloadAllTabs").disabled, false, "Reload All Tabs is enabled");
 
-	
+  
   gBrowser.selectedTab = testTab;
-	gBrowser.showOnlyTheseTabs([testTab]);
-	is(gBrowser.visibleTabs.length, 1, "now there is only one visible tab");
-	
-	
-	popup(testTab);
-	is(TabContextMenu.contextTab, testTab, "TabContextMenu context is the test tab");
-	is(document.getElementById("context_closeTab").disabled, true, "Close Tab is disabled");
-	is(document.getElementById("context_reloadAllTabs").disabled, true, "Reload All Tabs is disabled");
-	
+  gBrowser.showOnlyTheseTabs([testTab]);
+  is(gBrowser.visibleTabs.length, 1, "now there is only one visible tab");
+  
+  
+  popup(testTab);
+  is(TabContextMenu.contextTab, testTab, "TabContextMenu context is the test tab");
+  is(document.getElementById("context_closeTab").disabled, true, "Close Tab is disabled");
+  is(document.getElementById("context_reloadAllTabs").disabled, true, "Reload All Tabs is disabled");
+  
   
   
   let pinned = gBrowser.addTab();
   gBrowser.pinTab(pinned);
-	is(gBrowser.visibleTabs.length, 2, "now there are two visible tabs");
+  is(gBrowser.visibleTabs.length, 2, "now there are two visible tabs");
 
-	
-	popup(testTab);
+  
+  popup(testTab);
   is(TabContextMenu.contextTab, testTab, "TabContextMenu context is again the test tab");
-	is(document.getElementById("context_closeOtherTabs").disabled, true, "Close Other Tabs is disabled");
+  is(document.getElementById("context_closeOtherTabs").disabled, true, "Close Other Tabs is disabled");
 
-	
-	let allTabs = [tab for each (tab in gBrowser.tabs)];
-	gBrowser.showOnlyTheseTabs(allTabs);
+  
+  let allTabs = [tab for each (tab in gBrowser.tabs)];
+  gBrowser.showOnlyTheseTabs(allTabs);
 
-	
-	popup(testTab);
+  
+  popup(testTab);
   is(TabContextMenu.contextTab, testTab, "TabContextMenu context is yet again the test tab");
-	is(document.getElementById("context_closeOtherTabs").disabled, false, "Close Other Tabs is enabled");
-	
-	gBrowser.removeTab(testTab);
-	gBrowser.removeTab(pinned);
+  is(document.getElementById("context_closeOtherTabs").disabled, false, "Close Other Tabs is enabled");
+  
+  gBrowser.removeTab(testTab);
+  gBrowser.removeTab(pinned);
 }
 
 function popup(tab) {
