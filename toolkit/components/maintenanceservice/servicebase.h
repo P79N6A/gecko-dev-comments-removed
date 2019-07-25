96b6d5a514eb78b2e93da9e9430e5cf8c5b0b762
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:28:53 +0000

    Bug 709173 - updater.exe needs to be verified to be an updater, not just an executable signed by Mozilla. r=rstrong.
    UltraBlame original commit: df4ba8bfc9caf08eb89c4dcd09561e394a8d2cfe

--- toolkit/components/maintenanceservice/servicebase.h
+++ toolkit/components/maintenanceservice/servicebase.h
@@ -45,3 +45,11 @@ BOOL VerifySameFiles(LPCWSTR file1Path, LPCWSTR file2Path, BOOL &sameContent);
 
 
 #define COMPARE_BLOCKSIZE 32768
+
+
+
+
+
+#define UPDATER_IDENTITY_STRING \
+  "moz-updater.exe-4cdccec4-5ee0-4a06-9817-4cd899a9db49"
+#define IDS_UPDATER_IDENTITY 1006
