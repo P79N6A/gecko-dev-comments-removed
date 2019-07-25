689f2cdc2a73966d3e64dbc217ded093c903c36b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:05:56 +0000

    Bug 746646: Remove useless bounds-check for unsigned >= 0 in nsContentEventHandler::ExpandToClusterBoundary. r=masayuki
    UltraBlame original commit: d81023c3e5823b96c268305514f9ba0f9447306c

--- content/events/src/nsContentEventHandler.cpp
+++ content/events/src/nsContentEventHandler.cpp
@@ -357,8 +357,8 @@ static nsresult GenerateFlatTextContent(nsRange* aRange,
 
 nsresult
 nsContentEventHandler::ExpandToClusterBoundary(nsIContent* aContent,
-                                                    bool aForward,
-                                                    PRUint32* aXPOffset)
+                                               bool aForward,
+                                               PRUint32* aXPOffset)
 {
   
   
@@ -366,7 +366,7 @@ nsContentEventHandler::ExpandToClusterBoundary(nsIContent* aContent,
       *aXPOffset == 0 || *aXPOffset == aContent->TextLength())
     return NS_OK;
 
-  NS_ASSERTION(*aXPOffset >= 0 && *aXPOffset <= aContent->TextLength(),
+  NS_ASSERTION(*aXPOffset <= aContent->TextLength(),
                "offset is out of range.");
 
   nsRefPtr<nsFrameSelection> fs = mPresShell->FrameSelection();
