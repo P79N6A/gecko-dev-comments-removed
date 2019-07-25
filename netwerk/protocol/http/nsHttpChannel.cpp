44e0cfd614965540510cb6f84aaf02f6576cf755
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:33:29 +0000

    Bug 770243 - Close cache input stream as late as possible, fixing a regression with processing 401 and 407 responses, r=bsmith
    UltraBlame original commit: dcc9891978e119c44e799ee1b147309df9ee7f6c

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -1194,9 +1194,6 @@ nsHttpChannel::ProcessResponse()
     }
 
     MOZ_ASSERT(!mCachedContentIsValid);
-    if (httpStatus != 304 && httpStatus != 206) {
-        mCacheInputStream.CloseAndRelease();
-    }
 
     
     gHttpHandler->OnExamineResponse(this);
@@ -3859,7 +3856,14 @@ nsHttpChannel::InstallCacheListener(PRUint32 offset)
             LOG(("unable to mark cache entry for compression"));
         }
     } 
-      
+
+    LOG(("Trading cache input stream for output stream [channel=%p]", this));
+
+    
+    
+    
+    mCacheInputStream.CloseAndRelease();
+
     nsCOMPtr<nsIOutputStream> out;
     rv = mCacheEntry->OpenOutputStream(offset, getter_AddRefs(out));
     if (NS_FAILED(rv)) return rv;
