9d27cb4b298770cde0e28cc3df961e8a237ad0d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:09:10 +0000

    Fix bug 642338. r=smaug
    UltraBlame original commit: 223eac79b04e5f68fa1752a4ecbdd1991bb1fab2

--- dom/base/nsBarProps.h
+++ dom/base/nsBarProps.h
@@ -56,81 +56,18 @@ class nsIWebBrowserChrome;
 class nsBarProp : public nsIDOMBarProp
 {
 public:
-  nsBarProp();
+  explicit nsBarProp(nsGlobalWindow *aWindow);
   virtual ~nsBarProp();
 
   NS_DECL_ISUPPORTS
-
-  NS_IMETHOD SetWebBrowserChrome(nsIWebBrowserChrome* aBrowserChrome);
 
   NS_IMETHOD GetVisibleByFlag(PRBool *aVisible, PRUint32 aChromeFlag);
   NS_IMETHOD SetVisibleByFlag(PRBool aVisible, PRUint32 aChromeFlag);
 
 protected:
-  
-  nsIWebBrowserChrome* mBrowserChrome;
-};
+  already_AddRefed<nsIWebBrowserChrome> GetBrowserChrome();
 
-
-class nsMenubarProp : public nsBarProp
-{
-public:
-  nsMenubarProp();
-  virtual ~nsMenubarProp();
-
-  NS_DECL_NSIDOMBARPROP
-};
-
-
-class nsToolbarProp : public nsBarProp
-{
-public:
-  nsToolbarProp();
-  virtual ~nsToolbarProp();
-
-  NS_DECL_NSIDOMBARPROP
-};
-
-
-class nsLocationbarProp : public nsBarProp
-{
-public:
-  nsLocationbarProp();
-  virtual ~nsLocationbarProp();
-
-  NS_DECL_NSIDOMBARPROP
-};
-
-
-class nsPersonalbarProp : public nsBarProp
-{
-public:
-  nsPersonalbarProp();
-  virtual ~nsPersonalbarProp();
-
-  NS_DECL_NSIDOMBARPROP
-};
-
-
-class nsStatusbarProp : public nsBarProp
-{
-public:
-  nsStatusbarProp();
-  virtual ~nsStatusbarProp();
-
-  NS_DECL_NSIDOMBARPROP
-};
-
-
-class nsScrollbarsProp : public nsBarProp {
-public:
-  nsScrollbarsProp(nsGlobalWindow *aWindow);
-  virtual ~nsScrollbarsProp();
-
-  NS_DECL_NSIDOMBARPROP
-
-private:
-  nsGlobalWindow           *mDOMWindow;
+  nsGlobalWindow             *mDOMWindow;
   nsCOMPtr<nsIWeakReference>  mDOMWindowWeakref;
   
 
@@ -140,6 +77,66 @@ private:
 
 
 
+};
+
+
+class nsMenubarProp : public nsBarProp
+{
+public:
+  explicit nsMenubarProp(nsGlobalWindow *aWindow);
+  virtual ~nsMenubarProp();
+
+  NS_DECL_NSIDOMBARPROP
+};
+
+
+class nsToolbarProp : public nsBarProp
+{
+public:
+  explicit nsToolbarProp(nsGlobalWindow *aWindow);
+  virtual ~nsToolbarProp();
+
+  NS_DECL_NSIDOMBARPROP
+};
+
+
+class nsLocationbarProp : public nsBarProp
+{
+public:
+  explicit nsLocationbarProp(nsGlobalWindow *aWindow);
+  virtual ~nsLocationbarProp();
+
+  NS_DECL_NSIDOMBARPROP
+};
+
+
+class nsPersonalbarProp : public nsBarProp
+{
+public:
+  explicit nsPersonalbarProp(nsGlobalWindow *aWindow);
+  virtual ~nsPersonalbarProp();
+
+  NS_DECL_NSIDOMBARPROP
+};
+
+
+class nsStatusbarProp : public nsBarProp
+{
+public:
+  explicit nsStatusbarProp(nsGlobalWindow *aWindow);
+  virtual ~nsStatusbarProp();
+
+  NS_DECL_NSIDOMBARPROP
+};
+
+
+class nsScrollbarsProp : public nsBarProp
+{
+public:
+  explicit nsScrollbarsProp(nsGlobalWindow *aWindow);
+  virtual ~nsScrollbarsProp();
+
+  NS_DECL_NSIDOMBARPROP
 };
 
 #endif
