b7c8d1a74713683a897962e09b07d200e7022e00
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:49:18 +0000

    Make sure moveValue() adds a relocation marker (bug 701125, r=jandem).
    UltraBlame original commit: 8919425a2efcf7a2a5ec0d12c970a215bd9d1c4f

--- js/src/ion/shared/Assembler-x86-shared.cpp
+++ js/src/ion/shared/Assembler-x86-shared.cpp
@@ -65,6 +65,22 @@ TraceDataRelocations(JSTracer *trc, uint8 *buffer, CompactBufferReader &reader)
     while (reader.more()) {
         size_t offset = reader.readUnsigned();
         void *ptr = JSC::X86Assembler::getPointer(buffer + offset);
+
+#ifdef JS_PUNBOX64
+        
+        
+        {
+            uintptr_t bits = uintptr_t(ptr);
+            if (bits >> JSVAL_TAG_SHIFT) {
+                jsval_layout layout;
+                layout.asBits = bits;
+                Value v = IMPL_TO_JSVAL(layout);
+                gc::MarkValue(trc, v, "immvalue");
+                continue;
+            }
+        }
+#endif
+
         gc::MarkGCThing(trc, ptr, "immgcptr");
     }
 }
