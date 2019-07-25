56de5a26dd21f0a99036cf469686556ac8513469
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:53:44 +0000

    Bug 732552 - Remove void return in ColorLayerOGL. r=ajuma
    UltraBlame original commit: ec2c854c86eb1d8b022e170665a9365f572518de

--- gfx/layers/opengl/ColorLayerOGL.cpp
+++ gfx/layers/opengl/ColorLayerOGL.cpp
@@ -50,7 +50,7 @@ RenderColorLayer(ColorLayer* aLayer, LayerManagerOGL *aManager,
   
 
   nsIntRect visibleRect = aLayer->GetEffectiveVisibleRegion().GetBounds();
-  
+
   
 
 
@@ -77,14 +77,14 @@ void
 ColorLayerOGL::RenderLayer(int,
                            const nsIntPoint& aOffset)
 {
-  return RenderColorLayer(this, mOGLManager, aOffset);
+  RenderColorLayer(this, mOGLManager, aOffset);
 }
 
 void
 ShadowColorLayerOGL::RenderLayer(int,
                                  const nsIntPoint& aOffset)
 {
-  return RenderColorLayer(this, mOGLManager, aOffset);
+  RenderColorLayer(this, mOGLManager, aOffset);
 }
 
 
