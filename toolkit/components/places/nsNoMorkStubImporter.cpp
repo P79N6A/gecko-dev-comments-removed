b4abcc97cada483178b1ca3ac7b9c0a168debd8c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:28:53 +0000

    Bug 578268: Eliminate the last vestiges of mork from Firefox. r=mak
    UltraBlame original commit: 4e3b03de1fd3df9a76e470e0a2f32dc86eabf9ab

---
+++ toolkit/components/places/nsNoMorkStubImporter.cpp
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsNavHistory.h"
+
+NS_IMETHODIMP
+nsNavHistory::ImportHistory(nsIFile* aFile)
+{
+  return NS_ERROR_NOT_IMPLEMENTED;
+}
