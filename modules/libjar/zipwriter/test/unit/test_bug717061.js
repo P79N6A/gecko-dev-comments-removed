7d45ef0d1463c0d279550b6233ec7124c127355a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:52:53 +0000

    Bug 717061 - add tests for uncompressed->gzip conversion; r=mossop
    UltraBlame original commit: 75c291e1c27ea96ed0401e2e960b6ea8c059904c

---
+++ modules/libjar/zipwriter/test/unit/test_bug717061.js
@@ -0,0 +1,92 @@
+
+
+
+
+function BinaryComparer(file, callback) {
+  var fstream = Cc["@mozilla.org/network/file-input-stream;1"].
+                createInstance(Ci.nsIFileInputStream);
+  fstream.init(file, -1, 0, 0);
+  this.length = file.fileSize;
+  this.fileStream = Cc["@mozilla.org/binaryinputstream;1"].
+                    createInstance(Ci.nsIBinaryInputStream);
+  this.fileStream.setInputStream(fstream);
+  this.offset = 0;
+  this.callback = callback;
+}
+
+BinaryComparer.prototype = {
+  fileStream: null,
+  offset: null,
+  length: null,
+  callback: null,
+
+  onStartRequest: function(aRequest, aContext) {
+  },
+
+  onStopRequest: function(aRequest, aContext, aStatusCode) {
+    this.fileStream.close();
+    do_check_eq(aStatusCode, Components.results.NS_OK);
+    do_check_eq(this.offset, this.length);
+    this.callback();
+  },
+
+  onDataAvailable: function(aRequest, aContext, aInputStream, aOffset, aCount) {
+    var stream = Cc["@mozilla.org/binaryinputstream;1"].
+                 createInstance(Ci.nsIBinaryInputStream);
+    stream.setInputStream(aInputStream);
+    var source, actual;
+    for (var i = 0; i < aCount; i++) {
+      try {
+        source = this.fileStream.read8();
+      }
+      catch (e) {
+        do_throw("Unable to read from file at offset " + this.offset + " " + e);
+      }
+      try {
+        actual = stream.read8();
+      }
+      catch (e) {
+        do_throw("Unable to read from converted stream at offset " + this.offset + " " + e);
+      }
+      
+      
+      
+      
+      
+      if (this.offset == 9)
+	;
+      else if (source != actual)
+        do_throw("Invalid value " + actual + " at offset " + this.offset + ", should have been " + source);
+      this.offset++;
+    }
+  }
+}
+
+function comparer_callback()
+{
+  do_test_finished();
+}
+
+function run_test()
+{
+  var source = do_get_file(DATA_DIR + "test_bug717061.html");
+  var comparer = new BinaryComparer(do_get_file(DATA_DIR + "test_bug717061.gz"),
+                                    comparer_callback);
+  
+  
+  var scs = Cc["@mozilla.org/streamConverters;1"].
+            getService(Ci.nsIStreamConverterService);
+  var converter = scs.asyncConvertData("uncompressed", "gzip", comparer, null);
+
+  
+  var fstream = Cc["@mozilla.org/network/file-input-stream;1"].
+                createInstance(Ci.nsIFileInputStream);
+  fstream.init(source, -1, 0, 0);
+
+  
+  var pump = Cc["@mozilla.org/network/input-stream-pump;1"].
+             createInstance(Ci.nsIInputStreamPump);
+  pump.init(fstream, -1, -1, 0, 0, true);
+  pump.asyncRead(converter, null);
+  do_test_pending();
+}
