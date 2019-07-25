58ff564b9935063a7ee738b81011ceda2ebef7fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:26:13 +0000

    back out 9ad2c67333ba (bug 652657) since review comments weren't addressed
    UltraBlame original commit: 2c55cb8097d200b7cb6bab617e35056ccf7233b2

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -376,13 +376,8 @@ BrowserGlue.prototype = {
   
   _onBrowserStartup: function BG__onBrowserStartup() {
     
-    if (this._shouldShowRights()) {
+    if (this._shouldShowRights())
       this._showRightsNotification();
-    } else {
-      
-      this._showTelemetryNotification();
-    }
-
 
     
     if (Services.prefs.prefHasUserValue("app.update.postupdate"))
@@ -393,6 +388,9 @@ BrowserGlue.prototype = {
     if (this._isPlacesDatabaseLocked) {
       this._showPlacesLockedNotificationBox();
     }
+
+    
+    this._showTelemetryNotification();
 
     
     
