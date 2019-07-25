a2ca16d0309303d4dd53e4eb24e798994eaade43
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:38:56 +0000

    Bug 775360 - "Show me how" link should not launch Browser Intents page. r=rnewman
    UltraBlame original commit: b2cb5e0b65258930d5d76eb0c7ee5df988bd88cc

---
+++ mobile/android/base/sync/setup/activities/WebViewActivity.java
@@ -0,0 +1,59 @@
+
+
+
+
+package org.mozilla.gecko.sync.setup.activities;
+
+import org.mozilla.gecko.R;
+import org.mozilla.gecko.sync.Logger;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+
+
+
+
+public class WebViewActivity extends Activity {
+  private final static String LOG_TAG = "WebViewActivity";
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().requestFeature(Window.FEATURE_PROGRESS);
+    setContentView(R.layout.sync_setup_webview);
+    
+    Uri uri = this.getIntent().getData();
+    if (uri == null) {
+      Logger.debug(LOG_TAG, "No URI passed to display.");
+      finish();
+    }
+
+    WebView wv = (WebView) findViewById(R.id.web_engine);
+    
+    final Activity activity = this;
+    wv.setWebChromeClient(new WebChromeClient() {
+      public void onProgressChanged(WebView view, int progress) {
+        
+        
+        activity.setProgress(progress * 100);
+      }
+    });
+    wv.setWebViewClient(new WebViewClient() {
+      
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return false;
+      }
+    });
+    wv.loadUrl(uri.toString());
+
+  }
+}
