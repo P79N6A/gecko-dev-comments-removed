20a477ad2d6c642c058ab81fa748e268b5a6f63a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:30:45 +0000

    Bug 648585 - nsIHapticFeedback not defined on desktop build of fennec. r=smaug
    UltraBlame original commit: 75a6aae6dd4d46bc345e74fa2474bab64c11e81c

---
+++ dom/system/windows/nsHapticFeedback.h
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsIHapticFeedback.h"
+
+class nsHapticFeedback : public nsIHapticFeedback
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIHAPTICFEEDBACK
+};
