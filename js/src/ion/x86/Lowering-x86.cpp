de1e027a3eb65be36441d2561da5b5f4f3a06b9a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:47:00 +0000

    Bug 670628: Add floating point support to linear scan register allocator. r=dvander
    UltraBlame original commit: 1e2078273debc9d4c3bc0df278fe566c634cd789

--- js/src/ion/x86/Lowering-x86.cpp
+++ js/src/ion/x86/Lowering-x86.cpp
@@ -96,7 +96,7 @@ LIRGeneratorX86::visitBox(MBox *box)
 
     
     if (inner->type() == MIRType_Double)
-        return defineBox(new LBoxDouble(use(inner)), box);
+        return defineBox(new LBoxDouble(use(inner), temp(LDefinition::DOUBLE)), box);
 
     if (!box->isEmittedAtUses())
         return emitAtUses(box);
