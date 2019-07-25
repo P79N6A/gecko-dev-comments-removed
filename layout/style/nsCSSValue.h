9efc3eb7866f71a94d22d184dc1ab97fd5574760
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:36:19 +0000

    Bug 537890. Part 4.5: Create 'mozmm' unit for physical millimeters on screen devices. r=dbaron
    UltraBlame original commit: 3cfc1b1098e6bbf2e97c3293b05347e1711cd9ab

--- layout/style/nsCSSValue.h
+++ layout/style/nsCSSValue.h
@@ -154,7 +154,8 @@ enum nsCSSUnit {
 
   
   eCSSUnit_Millimeter   = 207,    
-  eCSSUnit_Centimeter   = 208,    
+  eCSSUnit_PhysicalMillimeter = 208,   
+  eCSSUnit_Centimeter   = 209,    
 
   
   eCSSUnit_Pica         = 301,    
