e7f24cb9e51e020ee5f878b8d564e0fd795e181e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:52:31 +0000

    Bug 599978 - Part 3: Asynchronous isVisited checks should use a read-only cloned connection Finalize the statement so we do not crash in debug builds. r=orange
    UltraBlame original commit: ac2c429e6273c6f7a915670cd5fba9f1ce974650

--- toolkit/components/places/src/History.cpp
+++ toolkit/components/places/src/History.cpp
@@ -1037,7 +1037,11 @@ History::Shutdown()
       dont_AddRef(static_cast<Step*>(mPendingVisits.PopFront()));
   }
 
+  
   if (mReadOnlyDBConn) {
+    if (mIsVisitedStatement) {
+      (void)mIsVisitedStatement->Finalize();
+    }
     (void)mReadOnlyDBConn->AsyncClose(nsnull);
   }
 }
