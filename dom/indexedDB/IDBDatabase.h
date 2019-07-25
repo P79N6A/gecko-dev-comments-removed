a7d030c13bfeea700538a99c64a6475bf4bf9dc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:43:41 +0000

    Bug 786771 - Fix for 'ASSERTION: Must have an actor here'. r=mrbkap.
    UltraBlame original commit: c568c60f284e0a804838ff373ae66881675ac337

--- dom/indexedDB/IDBDatabase.h
+++ dom/indexedDB/IDBDatabase.h
@@ -29,6 +29,7 @@ BEGIN_INDEXEDDB_NAMESPACE
 
 class AsyncConnectionHelper;
 struct DatabaseInfo;
+class IDBFactory;
 class IDBIndex;
 class IDBObjectStore;
 class IDBTransaction;
@@ -54,6 +55,7 @@ public:
 
   static already_AddRefed<IDBDatabase>
   Create(IDBWrapperCache* aOwnerCache,
+         IDBFactory* aFactory,
          already_AddRefed<DatabaseInfo> aDatabaseInfo,
          const nsACString& aASCIIOrigin,
          FileManager* aFileManager,
@@ -159,7 +161,13 @@ private:
 
   void OnUnlink();
 
+  
+  
+  
+  nsRefPtr<IDBFactory> mFactory;
+
   nsRefPtr<DatabaseInfo> mDatabaseInfo;
+
   
   
   nsRefPtr<DatabaseInfo> mPreviousDatabaseInfo;
