0b14c6f844413f7a689f4d45b82c7f3d1b1aeba1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:57:52 +0000

    Bug 734893: Faster DB accessing with priority for GeckoAsyncTask. [r=mfinkle, r=blassey]
    UltraBlame original commit: 837da0646b7dbbf6531c5085ca7416c655bcd455

--- mobile/android/base/GeckoAsyncTask.java
+++ mobile/android/base/GeckoAsyncTask.java
@@ -41,15 +41,42 @@ package org.mozilla.gecko;
 
 
 public abstract class GeckoAsyncTask<Params, Progress, Result> {
+    public static final int PRIORITY_NORMAL = 0;
+    public static final int PRIORITY_HIGH = 1;
+
+    private int mPriority;
+
+    public GeckoAsyncTask() {
+        mPriority = PRIORITY_NORMAL;
+    }
+
+    private class BackgroundTaskRunnable implements Runnable {
+        private Params[] mParams;
+
+        public BackgroundTaskRunnable(Params... params) {
+            mParams = params;
+        }
+
+        public void run() {
+            final Result result = doInBackground(mParams);
+            GeckoApp.mAppContext.runOnUiThread(new Runnable() {
+                public void run() {
+                    onPostExecute(result);
+                }
+            });
+        }
+    }
+
     public void execute(final Params... params) {
-        GeckoAppShell.getHandler().post(new Runnable() {
-            public void run() {
-                final Result result = doInBackground(params);
-                GeckoApp.mAppContext.runOnUiThread(new Runnable() {
-                    public void run() {
-                        onPostExecute(result);
-                    }});
-            }});
+        if (mPriority == PRIORITY_HIGH)
+            GeckoAppShell.getHandler().postAtFrontOfQueue(new BackgroundTaskRunnable(params));
+        else
+            GeckoAppShell.getHandler().post(new BackgroundTaskRunnable(params));
+    }
+
+    public GeckoAsyncTask<Params, Progress, Result> setPriority(int priority) {
+        mPriority = priority;
+        return this;
     }
 
     protected abstract Result doInBackground(Params... params);
