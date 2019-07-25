2ef51669a0a2c38559979ac7a770bc077b0e290d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:24:30 +0000

    Backout of f39dab2d2adb
    UltraBlame original commit: e954a042cc37e74d733f813ac7642961d91957e2

---
+++ mobile/android/base/gfx/WidgetTileLayer.java
@@ -0,0 +1,116 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+package org.mozilla.gecko.gfx;
+
+import org.mozilla.gecko.gfx.LayerController;
+import org.mozilla.gecko.gfx.SingleTileLayer;
+import org.mozilla.gecko.GeckoAppShell;
+import android.opengl.GLES11;
+import android.opengl.GLES11Ext;
+import android.graphics.RectF;
+import android.util.Log;
+import javax.microedition.khronos.opengles.GL10;
+
+
+
+
+public class WidgetTileLayer extends Layer {
+
+    private int[] mTextureIDs;
+    private CairoImage mImage;
+
+    public WidgetTileLayer(CairoImage image) {
+        mImage = image;
+    }
+
+    protected boolean initialized() { return mTextureIDs != null; }
+
+    @Override
+    public IntSize getSize() { return mImage.getSize(); }
+
+    protected void bindAndSetGLParameters() {
+        GLES11.glBindTexture(GL10.GL_TEXTURE_2D, mTextureIDs[0]);
+        GLES11.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
+        GLES11.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        if (mTextureIDs != null)
+            TextureReaper.get().add(mTextureIDs);
+    }
+
+    @Override
+    protected void performUpdates(GL10 gl) {
+        super.performUpdates(gl);
+
+        if (mTextureIDs == null) {
+            mTextureIDs = new int[1];
+            GLES11.glGenTextures(1, mTextureIDs, 0);
+        }
+
+        bindAndSetGLParameters();
+        GeckoAppShell.bindWidgetTexture();
+    }
+
+    @Override
+    public void draw(RenderContext context) {
+        
+        
+        if (!initialized())
+            return;
+
+        GLES11.glBindTexture(GL10.GL_TEXTURE_2D, mTextureIDs[0]);
+
+        RectF bounds;
+        int[] cropRect;
+        IntSize size = getSize();
+        RectF viewport = context.viewport;
+
+        bounds = getBounds(context, new FloatSize(size));
+        cropRect = new int[] { 0, size.height, size.width, -size.height };
+        bounds.offset(-viewport.left, -viewport.top);
+
+        GLES11.glTexParameteriv(GL10.GL_TEXTURE_2D, GLES11Ext.GL_TEXTURE_CROP_RECT_OES, cropRect,
+                                0);
+
+        float top = viewport.height() - (bounds.top + bounds.height());
+        GLES11Ext.glDrawTexfOES(bounds.left, top, 0.0f, bounds.width(), bounds.height());
+    }
+}
+
