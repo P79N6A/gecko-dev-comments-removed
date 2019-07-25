cfea5ceee297a6bdf967f7450073abd28846ada8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:05:56 +0000

    Bug 648368: add branding for Aurora, graphics by Sean Martell <smartell@mozilla.com>, r=dolske
    UltraBlame original commit: f7f2daa08d79efb969bf37165cfc36f97b9becde

---
+++ browser/branding/aurora/pref/firefox-branding.js
@@ -0,0 +1,25 @@
+
+pref("startup.homepage_override_url","");
+pref("startup.homepage_welcome_url","");
+
+
+pref("app.update.interval", 28800);
+
+
+pref("app.update.download.backgroundInterval", 60);
+
+
+pref("app.update.url.manual", "http://nightly.mozilla.org/");
+
+
+pref("app.update.url.details", "http://www.mozilla.org/projects/%APP%/");
+
+
+pref("app.releaseNotesURL", "http://www.mozilla.org/projects/%APP%/%VERSION%/releasenotes/");
+pref("app.vendorURL", "http://www.mozilla.org/projects/%APP%/");
+
+
+pref("browser.search.param.yahoo-fr", "");
+pref("browser.search.param.yahoo-fr-cjkt", ""); 
+pref("browser.search.param.yahoo-fr-ja", "");
+pref("browser.search.param.yahoo-f-CN", "");
