a380a15479fbc77bf8217f71007c2f8fd80f89b9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:03:29 +0000

    Bug 517636 - Assigning through wrappers defeats the "fast expando" optimization. r=jst
    UltraBlame original commit: 9c0de16298da037ec3a7c6217435ba2290c5f475

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -5139,6 +5139,10 @@ nsWindowSH::AddProperty(nsIXPConnectWrappedNative *wrapper, JSContext *cx,
 
     JSObject *innerObj;
     if (innerWin && (innerObj = innerWin->GetGlobalJSObject())) {
+      if (sResolving) {
+        return NS_OK;
+      }
+
 #ifdef DEBUG_SH_FORWARDING
       printf(" --- Forwarding add to inner window %p\n", (void *)innerWin);
 #endif
@@ -6265,6 +6269,9 @@ ContentWindowGetter(JSContext *cx, JSObject *obj, uintN argc, jsval *argv,
   return ::JS_GetProperty(cx, obj, "content", rval);
 }
 
+PRBool
+nsWindowSH::sResolving = PR_FALSE;
+
 NS_IMETHODIMP
 nsWindowSH::NewResolve(nsIXPConnectWrappedNative *wrapper, JSContext *cx,
                        JSObject *obj, jsval id, PRUint32 flags,
@@ -6334,18 +6341,44 @@ nsWindowSH::NewResolve(nsIXPConnectWrappedNative *wrapper, JSContext *cx,
 #endif
 
       jsid interned_id;
-      JSObject *pobj = NULL;
-      jsval val;
+      JSPropertyDescriptor desc;
 
-      *_retval = (::JS_ValueToId(cx, id, &interned_id) &&
-                  ::JS_LookupPropertyWithFlagsById(cx, innerObj, interned_id,
-                                                   flags, &pobj, &val));
+      *_retval = (JS_ValueToId(cx, id, &interned_id) &&
+                  JS_GetPropertyDescriptorById(cx, innerObj, interned_id,
+                                               flags, &desc));
 
-      if (*_retval && pobj) {
+      if (*_retval && desc.obj) {
 #ifdef DEBUG_SH_FORWARDING
         printf(" --- Resolve on inner window found property.\n");
 #endif
-        *objp = pobj;
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        if ((flags & JSRESOLVE_ASSIGNING) &&
+            !(desc.attrs & (JSPROP_GETTER | JSPROP_SETTER)) &&
+            desc.obj == innerObj) {
+          PRBool oldResolving = sResolving;
+          sResolving = PR_TRUE;
+
+          *_retval = JS_DefinePropertyById(cx, obj, interned_id, JSVAL_VOID,
+                                           nsnull, nsnull,
+                                           desc.attrs & JSPROP_ENUMERATE);
+
+          sResolving = oldResolving;
+          if (!*_retval) {
+            return NS_OK;
+          }
+        }
+
+        *objp = desc.obj;
       }
 
       return NS_OK;
