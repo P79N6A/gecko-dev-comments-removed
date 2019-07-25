31bc6d14db323d6f01c03f81cd3c1ed04809fcfa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:07:27 +0000

    Bug 325418.  When a repeating timer at interval X is reinitialized for interval Y, the next firing after that is incorrectly after time X+Y.  r=bzbarsky
    UltraBlame original commit: fdb29823b1d46c47647b351b61019c7752ed6f76

---
+++ xpcom/tests/unit/test_bug325418.js
@@ -0,0 +1,32 @@
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+var timer;
+const start_time = (new Date()).getTime();
+const expected_time = 1;
+
+var observer = {
+  observe: function(subject, topic, data) {
+    if (topic == "timer-callback") {
+      var stop_time = (new Date()).getTime();
+      
+      var result = Math.round((stop_time - start_time) / 1000);
+      do_check_true(result, expected_time);
+
+      do_test_finished();
+
+      timer.cancel();
+      timer = null;
+    }
+  }
+};
+
+function run_test() {
+  do_test_pending();
+
+  timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+  
+  timer.init(observer, 5000, timer.TYPE_REPEATING_PRECISE);
+  timer.cancel();
+  timer.init(observer, 1000, timer.TYPE_REPEATING_PRECISE);
+}
