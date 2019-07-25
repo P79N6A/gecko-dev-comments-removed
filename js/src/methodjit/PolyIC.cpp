a6cd13dbcc59e422b9d52de3c889e8157d4d2fa5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:49:34 +0000

    [INFER] Fix ADDPROP PIC recompilation hazard, bug 680797.
    UltraBlame original commit: c685e6d1774517f396fe5b35646bddbf7f5815a9

--- js/src/methodjit/PolyIC.cpp
+++ js/src/methodjit/PolyIC.cpp
@@ -623,6 +623,9 @@ class SetPropCompiler : public PICStubCompiler
             if (flags & Shape::METHOD)
                 obj->nativeSetSlot(shape->slot, f.regs.sp[-1]);
 
+            if (monitor.recompiled())
+                return Lookup_Uncacheable;
+
             
 
 
