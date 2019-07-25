613f7ff112f4730b88648d5bda0312b92acedc8e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:21:22 +0000

    attempt at fixing login/server setup, needs more work still
    UltraBlame original commit: 5e93e7de26922425095218bb8556d388c9e79c8e

--- services/sync/modules/service.js
+++ services/sync/modules/service.js
@@ -182,6 +182,14 @@ WeaveSvc.prototype = {
   get passphrase() { return ID.get('WeaveCryptoID').password; },
   set passphrase(value) { ID.get('WeaveCryptoID').password = value; },
 
+  
+  set onGetPassword(value) {
+    ID.get('WeaveID').onGetPassword = value;
+  },
+  set onGetPassphrase(value) {
+    ID.get('WeaveCryptoID').onGetPassword = value;
+  },
+
   get baseURL() {
     let url = Utils.prefs.getCharPref("serverURL");
     if (url && url[url.length-1] != '/')
@@ -296,7 +304,8 @@ WeaveSvc.prototype = {
            new Identity('Mozilla Services Encryption Passphrase', this.username));
 
     let url = this.baseURL + this.username;
-    PubKeys.defaultKeyUri = url + "keys/pubkey";
+    PubKeys.defaultKeyUri = url + "/keys/pubkey";
+    PrivKeys.defaultKeyUri = url + "/keys/privkey";
 
     if (Utils.prefs.getBoolPref("autoconnect") &&
         this.username && this.username != 'nobody')
@@ -490,9 +499,54 @@ WeaveSvc.prototype = {
   },
 
   
+  
+  _remoteSetup: function WeaveSvc__remoteSetup() {
+    let self = yield;
+    let ret = false; 
+
+    
+    
+    
+    let needKeys = true;
+    let pubkey = yield PubKeys.getDefaultKey(self.cb);
+    if (pubkey) {
+      
+      let privkey = yield PrivKeys.get(self.cb, pubkey.privateKeyUri);
+      if (privkey) {
+        needKeys = false;
+        ret = true;
+      }
+    }
+
+    if (needKeys) {
+      let pass = yield ID.get('WeaveCryptoID').getPassword(self.cb);
+      if (pass) {
+        let keys = PubKeys.createKeypair(pass, PubKeys.defaultKeyUri,
+                                         PrivKeys.defaultKeyUri);
+        try {
+          yield keys.pubkey.put(self.cb);
+          yield keys.privkey.put(self.cb);
+          ret = true;
+        } catch (e) {
+          this._log.error("Could not upload keys: " + Utils.exceptionStr(e));
+          this._log.error(keys.pubkey.lastRequest.responseText);
+        }
+      } else {
+        this._log.warn("Could not get encryption passphrase");
+      }
+    }
+
+    self.done(ret);
+  },
+
+  
 
   _sync: function WeaveSvc__sync() {
     let self = yield;
+
+    if (!(yield this._remoteSetup.async(this, self.cb))) {
+      throw "aborting sync, remote setup failed";
+    }
 
     
     
