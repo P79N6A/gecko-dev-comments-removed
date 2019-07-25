95df74b64ebfc07f53b21cbf485ebb9f2fdff0f3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:47:44 +0000

    Bug 709585 - Part 4, Dummy PowerManager DOM code. r=sicking
    UltraBlame original commit: 52147d521bbc77fc96c9412e6fdc7e775a6058e1

--- dom/base/Navigator.h
+++ dom/base/Navigator.h
@@ -84,6 +84,10 @@ namespace network {
 class Connection;
 } 
 
+namespace power {
+class PowerManager;
+} 
+
 class Navigator : public nsIDOMNavigator
                 , public nsIDOMClientInformation
                 , public nsIDOMNavigatorGeolocation
@@ -136,6 +140,7 @@ private:
   nsRefPtr<nsGeolocation> mGeolocation;
   nsRefPtr<nsDesktopNotificationCenter> mNotification;
   nsRefPtr<battery::BatteryManager> mBatteryManager;
+  nsRefPtr<power::PowerManager> mPowerManager;
   nsRefPtr<sms::SmsManager> mSmsManager;
 #ifdef MOZ_B2G_RIL
   nsCOMPtr<nsIDOMTelephony> mTelephony;
