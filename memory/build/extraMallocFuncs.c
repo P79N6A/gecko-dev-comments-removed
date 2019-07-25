e317c866f758f5cf0acebecb3fb907ceb8054d17
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:01:18 +0000

    Bug 736963 - Move jemalloc'ed strdup/strndup definitions. r=jlebar,r=ted
    UltraBlame original commit: c2a9fb6260384b17046911ddc5cafef4a23b2256

---
+++ memory/build/extraMallocFuncs.c
@@ -0,0 +1,34 @@
+
+
+
+
+#include <string.h>
+#include "mozilla/Types.h"
+
+#ifdef ANDROID
+#define wrap(a) __wrap_ ## a
+#endif
+
+#if defined(XP_WIN) || defined(XP_MACOSX)
+#define wrap(a) je_ ## a
+#endif
+
+#ifdef wrap
+void *wrap(malloc)(size_t);
+
+MOZ_EXPORT_API(char *)
+wrap(strndup)(const char *src, size_t len)
+{
+  char* dst = (char*) wrap(malloc)(len + 1);
+  if (dst)
+    strncpy(dst, src, len + 1);
+  return dst; 
+}
+
+MOZ_EXPORT_API(char *)
+wrap(strdup)(const char *src)
+{
+  size_t len = strlen(src);
+  return wrap(strndup)(src, len);
+}
+#endif
