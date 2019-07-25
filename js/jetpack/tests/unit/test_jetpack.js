12c47064c80469128c36d89a0a50d1d603803839
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:27:01 +0000

    Disable hanging jetpack XPCShell tests. r=sdwilsh
    UltraBlame original commit: 1c3e6be942f42976cc607bc81fee401a56c30ef1

--- js/jetpack/tests/unit/test_jetpack.js
+++ js/jetpack/tests/unit/test_jetpack.js
@@ -1,6 +1,8 @@
-var jps = jps || Components.classes["@mozilla.org/jetpack/service;1"]
-  .getService(Components.interfaces.nsIJetpackService),
-  jetpack = jps.createJetpack();
+
+
+
+
+
 
 load("handle_tests.js");
 function createHandle() {
@@ -8,6 +10,8 @@ function createHandle() {
 }
 
 function run_test() {
+  
+  return;
   run_handle_tests();
 
   jetpack.loadImplementation("file://" + do_get_file("impl.js").path);
