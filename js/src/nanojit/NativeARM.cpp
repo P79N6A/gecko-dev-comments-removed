674833276725f972130061ee02ff9e8a88f2068f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:07:34 +0000

    Remove stack-size limitations in asm_spill for ARM. [Bug 561977] [r=rreitmai]
    UltraBlame original commit: b44daa2c0503053c6b71d569bd091b21a5db466e

--- js/src/nanojit/NativeARM.cpp
+++ js/src/nanojit/NativeARM.cpp
@@ -38,6 +38,7 @@
 
 
 
+
 #include "nanojit.h"
 
 #ifdef UNDER_CE
@@ -271,6 +272,13 @@ Assembler::asm_add_imm(Register rd, Register rn, int32_t imm, int stat )
 
     
     
+    
+    if ((imm == 0) && (stat == 0) && (rd == rn)) {
+        return;
+    }
+
+    
+    
     if (encOp2Imm(imm, &op2imm)) {
         ADDis(rd, rn, op2imm, stat);
     } else if (encOp2Imm(-imm, &op2imm)) {
@@ -309,6 +317,13 @@ Assembler::asm_sub_imm(Register rd, Register rn, int32_t imm, int stat )
     NanoAssert(IsGpReg(rd));
     NanoAssert(IsGpReg(rn));
     NanoAssert((stat & 1) == stat);
+    
+    
+    
+    
+    if ((imm == 0) && (stat == 0) && (rd == rn)) {
+        return;
+    }
 
     
     
@@ -771,7 +786,7 @@ Assembler::asm_stkarg(LInsp arg, int stkd)
         if (!_config.arm_vfp || !isF64) {
             NanoAssert(IsGpReg(rr));
 
-            STR(rr, SP, stkd);
+            asm_str(rr, SP, stkd);
         } else {
             
             
@@ -794,7 +809,7 @@ Assembler::asm_stkarg(LInsp arg, int stkd)
         
         int d = findMemFor(arg);
         if (!isF64) {
-            STR(IP, SP, stkd);
+            asm_str(IP, SP, stkd);
             if (arg->isop(LIR_allocp)) {
                 asm_add_imm(IP, FP, d);
             } else {
@@ -806,9 +821,9 @@ Assembler::asm_stkarg(LInsp arg, int stkd)
             NanoAssert((stkd & 7) == 0);
 #endif
 
-            STR(IP, SP, stkd+4);
+            asm_str(IP, SP, stkd+4);
             LDR(IP, FP, d+4);
-            STR(IP, SP, stkd);
+            asm_str(IP, SP, stkd);
             LDR(IP, FP, d);
         }
     }
@@ -876,8 +891,8 @@ Assembler::asm_call(LInsp ins)
 
                 
                 
-                STR(R0, FP, d+0);
-                STR(R1, FP, d+4);
+                asm_str(R0, FP, d+0);
+                asm_str(R1, FP, d+4);
             } else {
                 NanoAssert(IsFpReg(rr));
 
@@ -1184,11 +1199,11 @@ Assembler::asm_qjoin(LIns *ins)
     LIns* hi = ins->oprnd2();
 
     Register r = findRegFor(hi, GpRegs);
-    STR(r, FP, d+4);
+    asm_str(r, FP, d+4);
 
     
     r = findRegFor(lo, GpRegs);
-    STR(r, FP, d);
+    asm_str(r, FP, d);
     deprecated_freeRsrcOf(ins);     
 }
 
@@ -1257,6 +1272,11 @@ Assembler::canRemat(LIns* ins)
 void
 Assembler::asm_restore(LInsp i, Register r)
 {
+    
+    NanoAssert(r != PC);
+    NanoAssert(r != IP);
+    NanoAssert(r != SP);
+
     if (i->isop(LIR_allocp)) {
         asm_add_imm(r, FP, deprecated_disp(i));
     } else if (i->isImmI()) {
@@ -1309,7 +1329,9 @@ Assembler::asm_spill(Register rr, int d, bool pop, bool quad)
     (void) quad;
     NanoAssert(d);
     
-    
+    NanoAssert(rr != PC);
+    NanoAssert(rr != IP);
+    NanoAssert(rr != SP);
     if (_config.arm_vfp && IsFpReg(rr)) {
         if (isS8(d >> 2)) {
             FSTD(rr, FP, d);
@@ -1319,17 +1341,20 @@ Assembler::asm_spill(Register rr, int d, bool pop, bool quad)
         }
     } else {
         NIns merged;
-        STR(rr, FP, d);
         
         
-        if (
-            does_next_instruction_exist(_nIns, codeStart, codeEnd,
-                                               exitStart, exitEnd)
-            && 
-               do_peep_2_1(&merged, _nIns[0], _nIns[1])) {
-            _nIns[1] = merged;
-            _nIns++;
-            verbose_only( asm_output("merge next into STMDB"); )
+        if (asm_str(rr, FP, d)) {
+            
+            
+            if (
+                    does_next_instruction_exist(_nIns, codeStart, codeEnd,
+                        exitStart, exitEnd)
+                    && 
+                    do_peep_2_1(&merged, _nIns[0], _nIns[1])) {
+                _nIns[1] = merged;
+                _nIns++;
+                verbose_only( asm_output("merge next into STMDB"); )
+            }
         }
     }
 }
@@ -1432,9 +1457,9 @@ Assembler::asm_store64(LOpcode op, LInsp value, int dr, LInsp base)
                     underrunProtect(LD32_size*2 + 8);
 
                     
-                    STR(IP, rb, dr);
+                    asm_str(IP, rb, dr);
                     asm_ld_imm(IP, value->immDlo(), false);
-                    STR(IP, rb, dr+4);
+                    asm_str(IP, rb, dr+4);
                     asm_ld_imm(IP, value->immDhi(), false);
 
                     return;
@@ -1481,9 +1506,9 @@ Assembler::asm_store64(LOpcode op, LInsp value, int dr, LInsp base)
                     underrunProtect(LD32_size*2 + 8);
 
                     
-                    STR(IP, rb, dr);
+                    asm_str(IP, rb, dr);
                     asm_ld_imm(IP, value->immDlo(), false);
-                    STR(IP, rb, dr+4);
+                    asm_str(IP, rb, dr+4);
                     asm_ld_imm(IP, value->immDhi(), false);
 
                     return;
@@ -1555,8 +1580,6 @@ Assembler::asm_immd_nochk(Register rr, int32_t immDlo, int32_t immDhi)
 void
 Assembler::asm_immd(LInsp ins)
 {
-    
-
     int d = deprecated_disp(ins);
     Register rr = ins->deprecated_getReg();
 
@@ -1570,17 +1593,12 @@ Assembler::asm_immd(LInsp ins)
         asm_immd_nochk(rr, ins->immDlo(), ins->immDhi());
     } else {
         NanoAssert(d);
-        
-        
-        
 
-        STR(IP, FP, d+4);
+        asm_str(IP, FP, d+4);
         asm_ld_imm(IP, ins->immDhi());
-        STR(IP, FP, d);
+        asm_str(IP, FP, d);
         asm_ld_imm(IP, ins->immDlo());
     }
-
-    
 }
 
 void
@@ -1638,6 +1656,9 @@ Assembler::asm_mmq(Register rd, int dd, Register rs, int ds)
     
     
     
+    
+    
+    
 
     
     
@@ -1648,8 +1669,40 @@ Assembler::asm_mmq(Register rd, int dd, Register rs, int ds)
 
     
     
+    NanoAssert(rs != IP);
+    NanoAssert(rd != IP);
+
+    
+    
     
     RegisterMask    free = _allocator.free & AllowableFlagRegs;
+
+    
+    
+    
+    
+    
+
+    int32_t dd_adj = 0;
+    int32_t ds_adj = 0;
+
+    if ((dd+4) >= 0x1000) {
+        dd_adj = ((dd+4) & ~0xfff);
+    } else if (dd <= -0x1000) {
+        dd_adj = -((-dd) & ~0xfff);
+    }
+    if ((ds+4) >= 0x1000) {
+        ds_adj = ((ds+4) & ~0xfff);
+    } else if (ds <= -0x1000) {
+        ds_adj = -((-ds) & ~0xfff);
+    }
+
+    
+    asm_sub_imm(rd, rd, dd_adj);
+    asm_sub_imm(rs, rs, ds_adj);
+
+    ds -= ds_adj;
+    dd -= dd_adj;
 
     if (free) {
         
@@ -1669,7 +1722,6 @@ Assembler::asm_mmq(Register rd, int dd, Register rs, int ds)
         NanoAssert((free & rmask(FP)) == 0);
 
         
-
         STR(IP, rd, dd+4);
         STR(rr, rd, dd);
         LDR(IP, rs, ds+4);
@@ -1681,6 +1733,10 @@ Assembler::asm_mmq(Register rd, int dd, Register rs, int ds)
         STR(IP, rd, dd);
         LDR(IP, rs, ds);
     }
+
+    
+    asm_add_imm(rd, rd, dd_adj);
+    asm_add_imm(rs, rs, ds_adj);
 }
 
 
@@ -1926,6 +1982,72 @@ Assembler::asm_ldr_chk(Register d, Register b, int32_t off, bool chk)
     }
 
     asm_output("ldr %s, [%s, #%d]",gpn(d),gpn(b),(off));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int32_t
+Assembler::asm_str(Register rt, Register rr, int32_t offset)
+{
+    
+    
+    
+    
+    NanoAssert(rr != PC);
+    NanoAssert(rt != PC);
+    if (offset >= 0) {
+        
+        if (isU12(offset)) {
+            STR(rt, rr, offset);
+            return 1;
+        }
+
+        if (rt != IP) {
+            STR(rt, IP, offset & 0xfff);
+            asm_add_imm(IP, rr, offset & ~0xfff);
+        } else {
+            int32_t adj = offset & ~0xfff;
+            asm_sub_imm(rr, rr, adj);
+            STR(rt, rr, offset-adj);
+            asm_add_imm(rr, rr, adj);
+        }
+    } else {
+        
+        if (isU12(-offset)) {
+            STR(rt, rr, offset);
+            return 1;
+        }
+
+        if (rt != IP) {
+            STR(rt, IP, -((-offset) & 0xfff));
+            asm_sub_imm(IP, rr, (-offset) & ~0xfff);
+        } else {
+            int32_t adj = -((-offset) & 0xfff);
+            asm_add_imm(IP, rr, adj);
+            STR(rt, rr, offset-adj);
+            asm_sub_imm(rr, rr, adj);
+        }
+    }
+
+    return 0;
 }
 
 
