69f0e3f8a2cb2bc681f4f9555c4ac1857b8b87b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:22:17 +0000

    Bug 418896 - Removed profileserver.py.in from being preprocessed; r=ted a=NPOTB
    UltraBlame original commit: ae5dcbff45dfca3518ab450d208f7e2afb341007

---
+++ build/pgo/profileserver.py
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import SimpleHTTPServer
+import SocketServer
+import socket
+import threading
+import os
+import sys
+import shutil
+from datetime import datetime
+from automation import Automation
+from automationutils import getDebuggerInfo, addCommonOptions
+
+PORT = 8888
+SCRIPT_DIR = os.path.abspath(os.path.realpath(os.path.dirname(sys.argv[0])))
+PROFILE_DIRECTORY = os.path.abspath(os.path.join(SCRIPT_DIR, "./pgoprofile"))
+MOZ_JAR_LOG_DIR = os.path.abspath(os.path.join(os.path.join(os.getenv("OBJDIR"), "dist"), "jarlog"))
+os.chdir(SCRIPT_DIR)
+
+class EasyServer(SocketServer.TCPServer):
+  allow_reuse_address = True
+
+if __name__ == '__main__':
+  from optparse import OptionParser
+  automation = Automation()
+
+  parser = OptionParser()
+  addCommonOptions(parser)
+
+  options, args = parser.parse_args()
+
+  debuggerInfo = getDebuggerInfo(".", options.debugger, options.debuggerArgs,
+          options.debuggerInteractive)
+
+  httpd = EasyServer(("", PORT), SimpleHTTPServer.SimpleHTTPRequestHandler)
+  t = threading.Thread(target=httpd.serve_forever)
+  t.setDaemon(True) 
+  t.start()
+  
+  automation.setServerInfo("localhost", PORT)
+  automation.initializeProfile(PROFILE_DIRECTORY)
+  browserEnv = automation.environment()
+  browserEnv["XPCOM_DEBUG_BREAK"] = "warn"
+  browserEnv["MOZ_JAR_LOG_DIR"] = MOZ_JAR_LOG_DIR
+
+  url = "http://localhost:%d/index.html" % PORT
+  appPath = os.path.join(SCRIPT_DIR, automation.DEFAULT_APP)
+  status = automation.runApp(url, browserEnv, appPath, PROFILE_DIRECTORY, {},
+                             debuggerInfo=debuggerInfo,
+                             
+                             
+                             timeout = None)
+  sys.exit(status)
