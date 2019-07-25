96541f184c17dc7d2d75b7886954d8f0d28728cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:01:21 +0000

    Bug 615794. Part 3: Snap bounds of border and background display items to pixels if we're sure they will be snapped. r=tnikkel a=blocking
    UltraBlame original commit: 1ca912846884b2c1d10344857284ca1233aed559

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -294,6 +294,16 @@ public:
   
 
 
+
+  PRBool IsSnappingEnabled() { return mSnappingEnabled; }
+  
+
+
+  void SetSnappingEnabled(PRBool aSnappingEnabled) { mSnappingEnabled = aSnappingEnabled; }
+
+  
+
+
   PRBool ShouldSyncDecodeImages() { return mSyncDecodeImages; }
 
   
@@ -414,6 +424,10 @@ public:
 
   NS_DECLARE_FRAME_PROPERTY(OutOfFlowDirtyRectProperty, nsIFrame::DestroyRect)
 
+  nsPresContext* CurrentPresContext() {
+    return CurrentPresShellState()->mPresShell->GetPresContext();
+  }
+
 private:
   struct PresShellState {
     nsIPresShell* mPresShell;
@@ -449,6 +463,7 @@ private:
   PRPackedBool                   mInTransform;
   PRPackedBool                   mSyncDecodeImages;
   PRPackedBool                   mIsPaintingToWindow;
+  PRPackedBool                   mSnappingEnabled;
 };
 
 class nsDisplayItem;
@@ -1319,7 +1334,8 @@ protected:
 class nsDisplayBorder : public nsDisplayItem {
 public:
   nsDisplayBorder(nsDisplayListBuilder* aBuilder, nsIFrame* aFrame) :
-    nsDisplayItem(aBuilder, aFrame) {
+    nsDisplayItem(aBuilder, aFrame),
+    mSnappingEnabled(aBuilder->IsSnappingEnabled() && !aBuilder->IsInTransform()) {
     MOZ_COUNT_CTOR(nsDisplayBorder);
   }
 #ifdef NS_BUILD_REFCNT_LOGGING
@@ -1328,10 +1344,14 @@ public:
   }
 #endif
 
+  virtual nsRect GetBounds(nsDisplayListBuilder* aBuilder);
   virtual void Paint(nsDisplayListBuilder* aBuilder, nsIRenderingContext* aCtx);
   virtual PRBool ComputeVisibility(nsDisplayListBuilder* aBuilder,
                                    nsRegion* aVisibleRegion);
   NS_DISPLAY_DECL_NAME("Border", TYPE_BORDER)
+
+protected:
+  PRPackedBool mSnappingEnabled;
 };
 
 
@@ -1414,10 +1434,12 @@ public:
   virtual void Paint(nsDisplayListBuilder* aBuilder, nsIRenderingContext* aCtx);
   NS_DISPLAY_DECL_NAME("Background", TYPE_BACKGROUND)
 protected:
-  nsRegion GetInsideClipRegion(PRUint8 aClip, const nsRect& aRect);
+  nsRegion GetInsideClipRegion(nsPresContext* aPresContext, PRUint8 aClip,
+                               const nsRect& aRect);
 
   
   PRPackedBool mIsThemed;
+  PRPackedBool mSnappingEnabled;
   nsITheme::Transparency mThemeTransparency;
 };
 
