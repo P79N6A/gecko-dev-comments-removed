fa0b68d664a73355f242e63fef38566d1cfd6fb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:25 +0000

    Bug 590779 - Virtual keyboard should not been showed on urlbar first tap - tests [r=mfinkle]
    UltraBlame original commit: 49eefc2a59257eebc77ab7b3f826e76d9f4cbefe

---
+++ mobile/chrome/tests/browser_awesomescreen.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+let gTests = [];
+let gCurrentTest = null;
+
+function test() {
+  
+  
+  waitForExplicitFinish();
+
+  
+  setTimeout(runNextTest, 200);
+}
+
+
+
+function runNextTest() {
+  
+  if (gTests.length > 0) {
+    gCurrentTest = gTests.shift();
+    info(gCurrentTest.desc);
+    gCurrentTest.run();
+  }
+  else {
+    finish();
+  }
+}
+
+
+
+gTests.push({
+  desc: "Test opening the awesome panel and checking the urlbar readonly state",
+
+  run: function() {
+    is(BrowserUI._edit.readOnly, true, "urlbar input textbox should be readonly");
+
+    let popup = document.getElementById("popup_autocomplete");
+    popup.addEventListener("popupshown", function(aEvent) {
+      popup.removeEventListener("popupshown", arguments.callee, true);
+      gCurrentTest.onPopupReady();
+    }, true);
+
+    BrowserUI.doCommand("cmd_openLocation");
+  },
+
+  onPopupReady: function() {
+    is(Elements.urlbarState.getAttribute("mode"), "edit", "bcast_urlbarState mode attribute should be equal to 'edit'");
+    is(BrowserUI._edit.readOnly, true, "urlbar input textbox be readonly");
+
+    
+    BrowserUI.doCommand("cmd_openLocation");
+    is(BrowserUI._edit.readOnly, false, "urlbar input textbox should not be readonly");
+
+    runNextTest();
+  }
+});
