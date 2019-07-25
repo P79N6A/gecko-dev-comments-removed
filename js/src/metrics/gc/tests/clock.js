10cb9db295d22eacbf8ac2f0ec146f7541250010
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:01:26 +0000

    Bug 548388 GC Benchmark Suite. r=jorendorff
    UltraBlame original commit: ed22e05bc003e90b7efcf2b06438b172f736aef7

---
+++ js/src/metrics/gc/tests/clock.js
@@ -0,0 +1,35 @@
+
+
+var t0;
+var tl;
+
+function alloc(dt) {
+    if (dt > 100)
+        dt = 100;
+    for (var i = 0; i < dt * 1000; ++i) {
+        var o = new String("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    }
+}
+
+function cycle() {
+    if (!running)
+        return;
+
+    var t1 = new Date;
+    if (t0 == undefined) t0 = t1;
+
+    if (tl != undefined) {
+        var dt = t1 - tl;
+        alloc(dt);
+    }
+
+    tl = t1;
+
+    if(t1 - t0 > (5 * 1000))
+        running = false;
+}
+
+var running = true;
+while(running)
+    cycle();
+
