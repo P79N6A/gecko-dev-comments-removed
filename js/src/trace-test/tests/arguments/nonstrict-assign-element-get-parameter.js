1bee9b3dc158c8057e90a2bc6e56acffb885e1d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:42:09 +0000

    Bug 516255 - Tests for arguments object functionality inside and outside of strict mode.  r=dmandelin
    UltraBlame original commit: 81c13c06bc82349bcc1a418876831f3038d91485

---
+++ js/src/trace-test/tests/arguments/nonstrict-assign-element-get-parameter.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+function assignElementGetParameter(a)
+{
+  arguments[0] = 17;
+  return a;
+}
+
+for (var i = 0; i < 5; i++)
+  assertEq(assignElementGetParameter(42), 17);
---
+++ js/src/trace-test/tests/arguments/nonstrict-assign-parameter-get-element.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+function assignParameterGetElement(a)
+{
+  a = 17;
+  return arguments[0];
+}
+
+for (var i = 0; i < 5; i++)
+  assertEq(assignParameterGetElement(42), 17);
