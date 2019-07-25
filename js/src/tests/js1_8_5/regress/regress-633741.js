c1758cf41c43b48422f8e6e48d5b94d89f7866f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 12:03:15 +0000

    Bug 633741 - Check result of defineProperty in js_InitFunctionAndObjectClasses.  r=jwalden, a=dmandelin
    UltraBlame original commit: d3dd12467800cfa8735b31b0724875abde2fe9b2

---
+++ js/src/tests/js1_8_5/regress/regress-633741.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+Object.preventExtensions(this);
+delete Function;
+
+try {
+    
+    Object.getOwnPropertyNames(this);
+} catch(e) {
+    reportCompare(true, false, "this shouldn't have thrown");
+}
+reportCompare(0, 0, "ok");
+
