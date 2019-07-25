8a3c626475b80d2c783bc1704c61bb6b7b16cf06
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:19:54 +0000

    Bug 703057 - Put DrawToFile on another thread. r=blassey
    UltraBlame original commit: 4b759043d4e8369401968a649d8c72501a86ec5e

--- embedding/android/GeckoApp.java
+++ embedding/android/GeckoApp.java
@@ -510,6 +510,14 @@ abstract public class GeckoApp
         String uri = lastHistoryEntry.mUri;
         String title = lastHistoryEntry.mTitle;
 
+        String lastUri = prefs.getString("last-uri", "");
+        String lastTitle = prefs.getString("last-title", uri);
+
+
+        
+        if (uri.equals(lastUri) && title.equals(lastTitle))
+            return;
+
         editor.putString("last-uri", uri);
         editor.putString("last-title", title);
 
@@ -1273,7 +1281,13 @@ abstract public class GeckoApp
     public void onPause()
     {
         Log.i(LOG_NAME, "pause");
-        rememberLastScreen(false);
+
+        
+        mMainHandler.postDelayed(new Runnable() {
+          public void run() {
+              rememberLastScreen(false);
+          }
+        }, 500);
 
         GeckoAppShell.sendEventToGecko(new GeckoEvent(GeckoEvent.ACTIVITY_PAUSING));
         
