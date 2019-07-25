15c8a275c4a0a0be61af7398baead7a3d42090c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 13:58:22 +0000

    Bug 643753 - Wrong dbghelp.lib linking argument for mingw compilation r=khuey
    UltraBlame original commit: c7bfc7dfba88fdf427692edeed2d2f0289602925

--- toolkit/library/libxul-config.mk
+++ toolkit/library/libxul-config.mk
@@ -111,9 +111,6 @@ ifneq (Android,$(OS_TARGET))
 OS_LIBS += -lrt
 endif
 endif
-ifeq (WINNT,$(OS_ARCH))
-OS_LIBS += dbghelp.lib
-endif
 endif
 
 STATIC_LIBS += \
@@ -154,7 +151,6 @@ COMPONENT_LIBS += \
 	pipboot \
 	pipnss \
 	appcomps \
-	composer \
 	$(NULL)
 
 ifdef MOZ_IPC
@@ -231,6 +227,12 @@ endif
 ifdef MOZ_UNIVERSALCHARDET
 COMPONENT_LIBS += universalchardet
 DEFINES += -DMOZ_UNIVERSALCHARDET
+endif
+
+ifndef MOZ_PLAINTEXT_EDITOR_ONLY
+COMPONENT_LIBS += composer
+else
+DEFINES += -DMOZ_PLAINTEXT_EDITOR_ONLY
 endif
 
 ifdef MOZ_RDF
