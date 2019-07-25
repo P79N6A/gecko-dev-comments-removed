8c95453819f405aa9971f4aa058c877389baaaff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:50:29 +0000

    Bug 617868 follow-up: fix test
    UltraBlame original commit: 2fa36f8b633c9da29ea58ba1a02f7bcecccd2920

--- services/sync/tests/unit/test_keys.js
+++ services/sync/tests/unit/test_keys.js
@@ -43,11 +43,12 @@ function test_keymanager() {
   
   
   
-  let sha256inputE = Utils.makeHMACKey("" + HMAC_INPUT + username + "\x01");
-  let encryptKey = Utils.sha256HMACBytes(Utils.decodeKeyBase32(testKey), sha256inputE);
+  let sha256inputE = "" + HMAC_INPUT + username + "\x01";
+  let key = Utils.makeHMACKey(Utils.decodeKeyBase32(testKey));
+  let encryptKey = Utils.sha256HMACBytes(sha256inputE, key);
   
-  let sha256inputH = Utils.makeHMACKey(encryptKey + HMAC_INPUT + username + "\x02");
-  let hmacKey = Utils.sha256HMACBytes(Utils.decodeKeyBase32(testKey), sha256inputH);
+  let sha256inputH = encryptKey + HMAC_INPUT + username + "\x02";
+  let hmacKey = Utils.sha256HMACBytes(sha256inputH, key);
   
   
   do_check_eq(btoa(encryptKey), new SyncKeyBundle(null, username, testKey).encryptionKey);
