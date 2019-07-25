fff671519863ce376b290673006af3bb7d77f5a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:42:21 +0000

    Bug 568486 - SH4 (a.k.a ST40) target support for NanoJIT  (r+nnethercote,edwsmith,rreitmai) [Cedrick Vincent]
    UltraBlame original commit: 75e337029ae0867f2e54f35bd4052f1514c04e85

--- js/src/nanojit/Assembler.cpp
+++ js/src/nanojit/Assembler.cpp
@@ -1057,7 +1057,7 @@ namespace nanojit
                 if (target->isop(LIR_jtbl)) {
                     
                     LIns *jtbl = target;
-                    NIns** native_table = (NIns**) where;
+                    NIns** native_table = (NIns**) (void *) where;
                     for (uint32_t i = 0, n = jtbl->getTableSize(); i < n; i++) {
                         LabelState* lstate = _labels.get(jtbl->getTarget(i));
                         NIns* ntarget = lstate->addr;
