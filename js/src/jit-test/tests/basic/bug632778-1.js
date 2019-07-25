dc8b12da7f40eadd7ee663373a422f00454853bd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:33:49 +0000

    Bug 632778 - Update tests to use test metalines instead, since they are in jit-test.
    UltraBlame original commit: 3bca687c261cbd7b456bd13fbe111317f027bd16

--- js/src/jit-test/tests/basic/bug632778-1.js
+++ js/src/jit-test/tests/basic/bug632778-1.js
@@ -1,7 +1,6 @@
-load(libdir + "asserts.js");
+
 function f() {
     "use strict";
 }
 g = wrap(f);
-assertThrowsInstanceOf(function () { Object.defineProperty(g, "arguments", {set: function(){}}); }, TypeError);
-
+Object.defineProperty(g, "arguments", {set: function(){}});
--- js/src/jit-test/tests/basic/bug632778-2.js
+++ js/src/jit-test/tests/basic/bug632778-2.js
@@ -1,4 +1,3 @@
-load(libdir + "asserts.js");
-obj = wrap(Number.bind());
-assertThrowsInstanceOf(function () { Object.defineProperty(obj, "caller", {set: function(){}}); }, TypeError);
 
+obj = wrap(Number.bind());
+Object.defineProperty(obj, "caller", {set: function () {}});
