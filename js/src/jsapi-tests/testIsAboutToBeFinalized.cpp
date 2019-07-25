8c2266a0447e2d6726f6b0886600058faaacafc2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:44:24 +0000

    bug 516832 followup - fixing jsapi-tests compiler warnings
    UltraBlame original commit: 210562a030b7bf8aa47a0d87dfc45cbd910a1d7a

--- js/src/jsapi-tests/testIsAboutToBeFinalized.cpp
+++ js/src/jsapi-tests/testIsAboutToBeFinalized.cpp
@@ -46,7 +46,7 @@ BEGIN_TEST(testIsAboutToBeFinalized_bug528645)
 
 
 
-    createAndTestRooted();
+    CHECK(createAndTestRooted());
     NativeFrameCleaner();
 
     JS_GC(cx);
@@ -117,5 +117,6 @@ cls_testIsAboutToBeFinalized_bug528645::createAndTestRooted()
 
     for (jsuint i = 0; i != checkPointersLength; ++i)
         CHECK(checkPointers[i]);
+    return true;
 }
 
