b91d746fd4ef512c7758c120d3530ec72dc2c286
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:57:53 +0000

    Bug 590805 - Sync UI: Revamp setup wizard (part 1), r=mconnor a=blocking2.0
    Generate Sync Key: Implement key generation and backup (email/print/save)
    
    UltraBlame original commit: e06b3042151c3f7122df50d7f2a697e2e99daab0

--- browser/base/content/syncUtils.js
+++ browser/base/content/syncUtils.js
@@ -38,7 +38,13 @@
 
 
 
+
 let gSyncUtils = {
+  get bundle() {
+    delete this.bundle;
+    return this.bundle = Services.strings.createBundle("chrome://browser/locale/syncSetup.properties");
+  },
+
   
   _openLink: function (url) {
     let thisDocEl = document.documentElement,
@@ -108,6 +114,136 @@ let gSyncUtils = {
     let url = this._baseURL + "secondrun.html";
     this._openLink(url);
   },
+
+  
+
+
+  generatePassphrase: function() {
+    let rng = Cc["@mozilla.org/security/random-generator;1"]
+                .createInstance(Ci.nsIRandomGenerator);
+    let bytes = rng.generateRandomBytes(20);
+    return [String.fromCharCode(97 + Math.floor(byte * 26 / 256))
+            for each (byte in bytes)].join("");
+  },
+
+  
+
+
+  hyphenatePassphrase: function(passphrase) {
+    return passphrase.slice(0, 5) + '-'
+         + passphrase.slice(5, 10) + '-'
+         + passphrase.slice(10, 15) + '-'
+         + passphrase.slice(15, 20);
+  },
+
+  
+
+
+  normalizePassphrase: function(pp) {
+    if (pp.length == 23 && pp[5] == '-' && pp[11] == '-' && pp[17] == '-')
+      return pp.slice(0, 5) + pp.slice(6, 11)
+           + pp.slice(12, 17) + pp.slice(18, 23);
+    return pp;
+  },
+
+  
+
+
+
+
+  passphraseEmail: function(elid) {
+    let pp = document.getElementById(elid).value;
+    let subject = this.bundle.GetStringFromName("email.synckey.subject");
+    let body = this.bundle.formatStringFromName("email.synckey.body", [pp], 1);
+    let uri = Weave.Utils.makeURI("mailto:?subject=" + subject + "&body=" + body);
+    let protoSvc = Cc["@mozilla.org/uriloader/external-protocol-service;1"]
+                     .getService(Ci.nsIExternalProtocolService);
+    protoSvc.loadURI(uri);
+  },
+
+  
+
+
+
+
+
+
+  _preparePPiframe: function(elid, callback) {
+    let pp = document.getElementById(elid).value;
+
+    
+    let iframe = document.createElement("iframe");
+    iframe.setAttribute("src", "chrome://browser/content/syncKey.xhtml");
+    iframe.collapsed = true;
+    document.documentElement.appendChild(iframe);
+    iframe.contentWindow.addEventListener("load", function() {
+      iframe.contentWindow.removeEventListener("load", arguments.callee, false);
+
+      
+      let el = iframe.contentDocument.getElementById("synckey");
+      el.firstChild.nodeValue = pp;
+
+      callback(iframe);
+    }, false);
+  },
+
+  
+
+
+
+
+  passphrasePrint: function(elid) {
+    this._preparePPiframe(elid, function(iframe) {
+      let webBrowserPrint = iframe.contentWindow
+                                  .QueryInterface(Ci.nsIInterfaceRequestor)
+                                  .getInterface(Ci.nsIWebBrowserPrint);
+      let printSettings = PrintUtils.getPrintSettings();
+
+      
+      printSettings.headerStrLeft
+        = printSettings.headerStrCenter
+        = printSettings.headerStrRight
+        = printSettings.footerStrLeft
+        = printSettings.footerStrCenter = "";
+      printSettings.footerStrRight = "&D";
+
+      try {
+        webBrowserPrint.print(printSettings, null);
+      } catch (ex) {
+        
+      }
+    });
+  },
+
+  
+
+
+
+
+  passphraseSave: function(elid) {
+    let dialogTitle = this.bundle.GetStringFromName("save.synckey.title");
+    this._preparePPiframe(elid, function(iframe) {
+      let filepicker = Cc["@mozilla.org/filepicker;1"]
+                         .createInstance(Ci.nsIFilePicker);
+      filepicker.init(window, dialogTitle, Ci.nsIFilePicker.modeSave);
+      filepicker.appendFilters(Ci.nsIFilePicker.filterHTML);
+      filepicker.defaultString = "Firefox Sync Key.html";
+      let rv = filepicker.show();
+      if (rv == Ci.nsIFilePicker.returnOK
+          || rv == Ci.nsIFilePicker.returnReplace) {
+        let stream = Cc["@mozilla.org/network/file-output-stream;1"]
+                       .createInstance(Ci.nsIFileOutputStream);
+        stream.init(filepicker.file, -1, -1, 0);
+
+        let serializer = new XMLSerializer();
+        let output = serializer.serializeToString(iframe.contentDocument);
+        output = Weave.Utils.encodeUTF8(output);
+        stream.write(output, output.length);
+      }
+      return false;
+    });
+  },
+
 
   
 
