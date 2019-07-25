8d4a71915e0f5c71c5dd1e1d45820699bf9cfcf9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:25:34 +0000

    Bug 548734.  Get rid of the footgun ui.use_native_popup_windows preference.  r=roc,khuey
    UltraBlame original commit: 9240f01e12b624d9c774e15e1dc516c8b967dc3c

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -74,9 +74,6 @@ pref("toolkit.zoomManager.zoomValues", ".2,.3,.5,.67,.8,.9,1,1.1,1.2,1.33,1.5,1.
 pref("browser.viewport.scaleRatio", -1);
 
 
-pref("ui.use_native_popup_windows", true);
-
-
 pref("ui.scrollbarsCanOverlapContent", 1);
 
 
