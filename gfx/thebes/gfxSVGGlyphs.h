e0a39f4290f34c41df4319a6aa471895132d772e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:45:11 +0000

    Bug 719286 - Support new OpenType-in-SVG table format r=roc
    UltraBlame original commit: 7ec5e5f7702392324b961ea5e28306f8f356e55f

--- gfx/thebes/gfxSVGGlyphs.h
+++ gfx/thebes/gfxSVGGlyphs.h
@@ -43,6 +43,7 @@
 #include "nsAutoPtr.h"
 #include "nsIContentViewer.h"
 #include "nsIPresShell.h"
+#include "nsClassHashtable.h"
 #include "nsBaseHashtable.h"
 #include "nsHashKeys.h"
 #include "nsIDocument.h"
@@ -55,7 +56,54 @@
 
 
 
-class gfxSVGGlyphs {
+
+
+
+
+class gfxSVGGlyphsDocument
+{
+    typedef mozilla::dom::Element Element;
+    typedef gfxFont::DrawMode DrawMode;
+
+public:
+    gfxSVGGlyphsDocument(uint8_t *aBuffer, uint32_t aBufLen,
+                         const FallibleTArray<uint8_t>& aCmapTable);
+
+    Element *GetGlyphElement(uint32_t aGlyphId);
+
+    ~gfxSVGGlyphsDocument() {
+        if (mViewer) {
+            mViewer->Destroy();
+        }
+    }
+
+private:
+    nsresult ParseDocument(uint8_t *aBuffer, uint32_t aBufLen);
+
+    nsresult SetupPresentation();
+
+    void FindGlyphElements(Element *aElement,
+                           const FallibleTArray<uint8_t> &aCmapTable);
+
+    void InsertGlyphId(Element *aGlyphElement);
+    void InsertGlyphChar(Element *aGlyphElement,
+                         const FallibleTArray<uint8_t> &aCmapTable);
+
+    nsCOMPtr<nsIDocument> mDocument;
+    nsCOMPtr<nsIContentViewer> mViewer;
+    nsCOMPtr<nsIPresShell> mPresShell;
+
+    nsBaseHashtable<nsUint32HashKey, Element*, Element*> mGlyphIdMap;
+};
+
+
+
+
+
+
+
+class gfxSVGGlyphs
+{
 private:
     typedef mozilla::dom::Element Element;
     typedef gfxFont::DrawMode DrawMode;
@@ -63,42 +111,69 @@ private:
 public:
     static const float SVG_UNITS_PER_EM;
 
-    static gfxSVGGlyphs* ParseFromBuffer(uint8_t *aBuffer, uint32_t aBufLen);
+    
+
+
+
+    gfxSVGGlyphs(FallibleTArray<uint8_t>& aSVGTable,
+                 const FallibleTArray<uint8_t>& aCmapTable);
+
+    
+
+
+    void UnmangleHeaders();
+
+    
+
+
+
+
+    gfxSVGGlyphsDocument *FindOrCreateGlyphsDocument(uint32_t aGlyphId);
+
+    
+
 
     bool HasSVGGlyph(uint32_t aGlyphId);
+
+    
+
+
+
+
 
     bool RenderGlyph(gfxContext *aContext, uint32_t aGlyphId, DrawMode aDrawMode,
                      gfxTextObjectPaint *aObjectPaint);
 
+    
+
+
+
+
     bool GetGlyphExtents(uint32_t aGlyphId, const gfxMatrix& aSVGToAppSpace,
                          gfxRect *aResult);
 
-    bool Init(const gfxFontEntry *aFont,
-              const FallibleTArray<uint8_t> &aCmapTable);
-
-    ~gfxSVGGlyphs() {
-        mViewer->Destroy();
-    }
-
 private:
-    gfxSVGGlyphs() {
-    }
+    Element *GetGlyphElement(uint32_t aGlyphId);
 
-    static nsresult ParseDocument(uint8_t *aBuffer, uint32_t aBufLen,
-                                  nsIDocument **aResult);
-
-    void FindGlyphElements(Element *aElement, const gfxFontEntry *aFontEntry,
-                           const FallibleTArray<uint8_t> &aCmapTable);
-
-    void InsertGlyphId(Element *aGlyphElement);
-    void InsertGlyphChar(Element *aGlyphElement, const gfxFontEntry *aFontEntry,
-                         const FallibleTArray<uint8_t> &aCmapTable);
-    
-    nsCOMPtr<nsIDocument> mDocument;
-    nsCOMPtr<nsIContentViewer> mViewer;
-    nsCOMPtr<nsIPresShell> mPresShell;
-
+    nsClassHashtable<nsUint32HashKey, gfxSVGGlyphsDocument> mGlyphDocs;
     nsBaseHashtable<nsUint32HashKey, Element*, Element*> mGlyphIdMap;
+
+    FallibleTArray<uint8_t> mSVGData;
+    FallibleTArray<uint8_t> mCmapData;
+
+    struct Header {
+        uint16_t mVersion;
+        uint16_t mIndexLength;
+    } *mHeader;
+
+    struct IndexEntry {
+        uint16_t mStartGlyph;
+        uint16_t mEndGlyph;
+        uint32_t mDocOffset;
+        uint32_t mDocLength;
+    } *mIndex;
+
+    static int CompareIndexEntries(const void *_a, const void *_b);
 };
 
 
