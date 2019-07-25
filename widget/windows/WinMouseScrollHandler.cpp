dd10eb7fe567ab4c332ca079d6b00fe781848909
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:54:09 +0000

    Bug 672175 part.2 MouseScrollHandler should manage system settings r=jimm
    UltraBlame original commit: 927dae404b953b3427e2d906b4e1b61c2214aa28

--- widget/windows/WinMouseScrollHandler.cpp
+++ widget/windows/WinMouseScrollHandler.cpp
@@ -11,6 +11,7 @@
 
 #include "WinMouseScrollHandler.h"
 #include "nsWindow.h"
+#include "WinUtils.h"
 
 namespace mozilla {
 namespace widget {
@@ -77,9 +78,86 @@ MouseScrollHandler::ProcessMessage(nsWindow* aWindow, UINT msg,
                                    WPARAM wParam, LPARAM lParam,
                                    LRESULT *aRetValue, bool &aEatMessage)
 {
-  return false;
+  switch (msg) {
+    case WM_SETTINGCHANGE:
+      if (!sInstance) {
+        return false;
+      }
+      if (wParam == SPI_SETWHEELSCROLLLINES ||
+          wParam == SPI_SETWHEELSCROLLCHARS) {
+        sInstance->mSystemSettings.MarkDirty();
+      }
+      return false;
+    default:
+      return false;
+  }
 }
 
+
+
+
+
+
+
+void
+MouseScrollHandler::SystemSettings::Init()
+{
+  if (mInitialized) {
+    return;
+  }
+
+  mInitialized = true;
+
+  if (!::SystemParametersInfo(SPI_GETWHEELSCROLLLINES, 0, &mScrollLines, 0)) {
+    PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+      ("MouseScroll::SystemSettings::Init(): ::SystemParametersInfo("
+         "SPI_GETWHEELSCROLLLINES) failed"));
+    mScrollLines = 3;
+  } else if (mScrollLines > WHEEL_DELTA) {
+    PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+      ("MouseScroll::SystemSettings::Init(): the result of "
+         "::SystemParametersInfo(SPI_GETWHEELSCROLLLINES) is too large: %d",
+       mScrollLines));
+    
+    
+    
+    
+    
+    
+    mScrollLines = WHEEL_PAGESCROLL;
+  }
+
+  if (!::SystemParametersInfo(SPI_GETWHEELSCROLLCHARS, 0, &mScrollChars, 0)) {
+    PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+      ("MouseScroll::SystemSettings::Init(): ::SystemParametersInfo("
+         "SPI_GETWHEELSCROLLCHARS) failed, %s",
+       WinUtils::GetWindowsVersion() >= WinUtils::VISTA_VERSION ?
+         "this is unexpected on Vista or later" :
+         "but on XP or earlier, this is not a problem"));
+    mScrollChars = 1;
+  } else if (mScrollChars > WHEEL_DELTA) {
+    PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+      ("MouseScroll::SystemSettings::Init(): the result of "
+         "::SystemParametersInfo(SPI_GETWHEELSCROLLCHARS) is too large: %d",
+       mScrollChars));
+    
+    mScrollChars = WHEEL_PAGESCROLL;
+  }
+
+  PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+    ("MouseScroll::SystemSettings::Init(): initialized, "
+       "mScrollLines=%d, mScrollChars=%d",
+     mScrollLines, mScrollChars));
+}
+
+void
+MouseScrollHandler::SystemSettings::MarkDirty()
+{
+  PR_LOG(gMouseScrollLog, PR_LOG_ALWAYS,
+    ("MouseScrollHandler::SystemSettings::MarkDirty(): "
+       "Marking SystemSettings dirty"));
+  mInitialized = false;
+}
 
 } 
 } 
