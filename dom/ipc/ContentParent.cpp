73138d1127aa713ea9624505e9d282a3262f86c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:57:24 +0000

    Bug 683245 - Make navigator.buildID and navigator.version work in content processes. r=bsmedberg
    UltraBlame original commit: 9687f4b3cefb80214c82517986eab12f6348d9d4

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -71,6 +71,7 @@
 #include "nsIScriptError.h"
 #include "nsConsoleMessage.h"
 #include "nsAppDirectoryServiceDefs.h"
+#include "nsAppRunner.h"
 #include "IDBFactory.h"
 #if defined(MOZ_SYDNEYAUDIO)
 #include "AudioParent.h"
@@ -427,6 +428,14 @@ ContentParent::ContentParent()
         static_cast<nsChromeRegistryChrome*>(registrySvc.get());
     chromeRegistry->SendRegisteredChrome(this);
     mMessageManager = nsFrameMessageManager::NewProcessMessageManager(this);
+
+    if (gAppData) {
+        nsCString version(gAppData->version);
+        nsCString buildID(gAppData->buildID);
+
+        
+        SendAppInfo(version, buildID);
+    }
 }
 
 ContentParent::~ContentParent()
