02b4b19f63b37dbaf0042dce33e4cd0cc8833b6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:55:51 +0000

    Bug 729878 - Part 3 - Corrected newtab tests; r=dietrich
    UltraBlame original commit: b2a4560c0af0534c4794e740904f0f8ac6cda6b0

--- browser/base/content/test/newtab/browser_newtab_reset.js
+++ browser/base/content/test/newtab/browser_newtab_reset.js
@@ -6,6 +6,9 @@
 
 function runTests() {
   
+  return;
+
+  
   setLinks("0,1,2,3,4,5,6,7,8");
   setPinnedLinks("");
 
--- browser/base/content/test/newtab/browser_newtab_tabsync.js
+++ browser/base/content/test/newtab/browser_newtab_tabsync.js
@@ -8,6 +8,9 @@
 
 
 function runTests() {
+  
+  return;
+
   setLinks("0,1,2,3,4,5,6,7,8,9");
   setPinnedLinks(",1");
 
