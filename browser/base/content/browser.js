975f173ab46af024738406159e9002ec667b294d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:40:44 +0000

    Bug 782684 - charsetLoadListener needs no event argument. r=gavin
    UltraBlame original commit: 613bc459e292c2eb84546512b01171ed63e7991d

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -221,7 +221,7 @@ XPCOMUtils.defineLazyGetter(this, "PageMenu", function() {
 function pageShowEventHandlers(event) {
   
   if (event.target == content.document) {
-    charsetLoadListener(event);
+    charsetLoadListener();
     XULBrowserWindow.asyncUpdateUI();
 
     
@@ -5497,7 +5497,7 @@ function CreateMenu(node) {
   Services.obs.notifyObservers(null, "charsetmenu-selected", node);
 }
 
-function charsetLoadListener(event) {
+function charsetLoadListener() {
   var charset = window.content.document.characterSet;
 
   if (charset.length > 0 && (charset != gLastBrowserCharset)) {
