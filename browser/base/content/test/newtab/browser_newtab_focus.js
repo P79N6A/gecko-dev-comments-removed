4cb52cd00ed8b6b9a87441d83cd0fc346e77e07c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:29:25 +0000

    Bug 719035 - [New Tab Page] Write keyboard navigation tests r=ttaubert
    UltraBlame original commit: a19d621cdc4f07d8e3095f0a02e3dc32b34a7756

---
+++ browser/base/content/test/newtab/browser_newtab_focus.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+function runTests() {
+  
+  
+  
+  let FOCUS_COUNT = 28; 
+  if ("nsILocalFileMac" in Ci) {
+    
+    FOCUS_COUNT = 19;
+  }
+
+  
+  setLinks("0,1,2,3,4,5,6,7,8");
+  setPinnedLinks("");
+  yield addNewTabPageTab();
+  gURLBar.focus();
+
+  
+  yield countFocus(FOCUS_COUNT);
+
+  
+  NewTabUtils.allPages.enabled = false;
+  yield countFocus(1);
+}
+
+
+
+
+function countFocus(aExpectedCount) {
+  let focusCount = 0;
+  let contentDoc = getContentDocument();
+
+  window.addEventListener("focus", function onFocus() {
+    let focusedElement = document.commandDispatcher.focusedElement;
+    if (focusedElement && focusedElement.classList.contains("urlbar-input")) {
+      window.removeEventListener("focus", onFocus, true);
+      is(focusCount, aExpectedCount, "Validate focus count in the new tab page.");
+      executeSoon(TestRunner.next);
+    } else {
+      if (focusedElement && focusedElement.ownerDocument == contentDoc &&
+          focusedElement instanceof HTMLElement) {
+        focusCount++;
+      }
+      document.commandDispatcher.advanceFocus();
+    }
+  }, true);
+
+  document.commandDispatcher.advanceFocus();
+}
