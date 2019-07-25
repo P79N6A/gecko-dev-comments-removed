e07488f2feb94199f1dbc42cc9715facd9e27438
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:45:37 +0000

    Bug 784859 - Part 1: Use QueryPerformanceCounter directly if the machine has a stable TSC; r=bbondy
    UltraBlame original commit: d291e2674dc6ccc91ac7cf7f24402819e99db8d4

--- xpcom/ds/TimeStamp_windows.cpp
+++ xpcom/ds/TimeStamp_windows.cpp
@@ -24,6 +24,25 @@
 #include "prlog.h"
 #include <stdio.h>
 
+#include <intrin.h>
+
+static bool
+HasStableTSC()
+{
+  int regs[4];
+
+  
+  __cpuid(regs, 0x80000000);
+  if (regs[0] < 0x80000007)
+          return false;
+
+  __cpuid(regs, 0x80000007);
+  
+  
+  return regs[3] & (1 << 8);
+}
+
+
 #if defined(PR_LOGGING)
 
 
@@ -46,6 +65,8 @@ static volatile ULONGLONG sResolutionSigDigs;
 static const double   kNsPerSecd  = 1000000000.0;
 static const LONGLONG kNsPerSec   = 1000000000;
 static const LONGLONG kNsPerMillisec = 1000000;
+
+static bool sHasStableTSC = false;
 
 
 
@@ -600,6 +621,8 @@ TimeStamp::Startup()
   InitThresholds();
   InitResolution();
 
+  sHasStableTSC = HasStableTSC();
+
   LOG(("TimeStamp: initial skew is %1.2fms", mt2ms_d(sSkew)));
 
   return NS_OK;
@@ -614,6 +637,9 @@ TimeStamp::Shutdown()
 TimeStamp
 TimeStamp::Now()
 {
+  if (sHasStableTSC) {
+    return TimeStamp(uint64_t(PerformanceCounter()));
+  }
   return TimeStamp(uint64_t(CalibratedPerformanceCounter()));
 }
 
