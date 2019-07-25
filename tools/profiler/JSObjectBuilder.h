422ee41d621517f8f6a64b2fff2bf07af391e2ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:55:04 +0000

    Bug 723711. Return the profile data as JS objects. r=bgirard
    UltraBlame original commit: 2ee344ca2759454690970c2402b5689f2f1646ba

---
+++ tools/profiler/JSObjectBuilder.h
@@ -0,0 +1,153 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "jsapi.h"
+
+
+
+class JSObjectBuilder
+{
+  public:
+
+  void DefineProperty(JSObject *aObject, const char *name, JSObject *aValue)
+  {
+    if (!mOk)
+      return;
+
+    mOk = JS_DefineProperty(mCx, aObject, name, OBJECT_TO_JSVAL(aValue), NULL, NULL, JSPROP_ENUMERATE);
+  }
+
+  void DefineProperty(JSObject *aObject, const char *name, int value)
+  {
+    if (!mOk)
+      return;
+
+    mOk = JS_DefineProperty(mCx, aObject, name, INT_TO_JSVAL(value), NULL, NULL, JSPROP_ENUMERATE);
+  }
+
+  void DefineProperty(JSObject *aObject, const char *name, double value)
+  {
+    if (!mOk)
+      return;
+
+    mOk = JS_DefineProperty(mCx, aObject, name, DOUBLE_TO_JSVAL(value), NULL, NULL, JSPROP_ENUMERATE);
+  }
+
+  void DefineProperty(JSObject *aObject, const char *name, nsAString &value)
+  {
+    if (!mOk)
+      return;
+
+    const nsString &flat = PromiseFlatString(value);
+    JSString *string = JS_NewUCStringCopyN(mCx, static_cast<const jschar*>(flat.get()), flat.Length());
+    if (!string)
+      mOk = JS_FALSE;
+
+    if (!mOk)
+      return;
+
+    mOk = JS_DefineProperty(mCx, aObject, name, STRING_TO_JSVAL(string), NULL, NULL, JSPROP_ENUMERATE);
+  }
+
+  void DefineProperty(JSObject *aObject, const char *name, const char *value)
+  {
+    nsAutoString string = NS_ConvertASCIItoUTF16(value);
+    DefineProperty(aObject, name, string);
+  }
+
+  void ArrayPush(JSObject *aArray, int value)
+  {
+    if (!mOk)
+      return;
+
+    jsval objval = INT_TO_JSVAL(value);
+    uint32_t length;
+    mOk = JS_GetArrayLength(mCx, aArray, &length);
+
+    if (!mOk)
+      return;
+
+    mOk = JS_SetElement(mCx, aArray, length, &objval);
+  }
+
+
+  void ArrayPush(JSObject *aArray, JSObject *aObject)
+  {
+    if (!mOk)
+      return;
+
+    jsval objval = OBJECT_TO_JSVAL(aObject);
+    uint32_t length;
+    mOk = JS_GetArrayLength(mCx, aArray, &length);
+
+    if (!mOk)
+      return;
+
+    mOk = JS_SetElement(mCx, aArray, length, &objval);
+  }
+
+  JSObject *CreateArray() {
+    JSObject *array = JS_NewArrayObject(mCx, 0, NULL);
+    if (!array)
+      mOk = JS_FALSE;
+
+    return array;
+  }
+
+  JSObject *CreateObject() {
+    JSObject *obj = JS_NewObject(mCx, NULL, NULL, NULL);
+    if (!obj)
+      mOk = JS_FALSE;
+
+    return obj;
+  }
+
+
+  
+  JSObjectBuilder(JSContext *aCx) : mCx(aCx), mOk(JS_TRUE)
+  {
+  }
+  private:
+  JSObjectBuilder(JSObjectBuilder&);
+
+  JSContext *mCx;
+  JSObject *mObj;
+  JSBool mOk;
+};
+
+
