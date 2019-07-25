a17e4c1b248617de412fe0418e551409992481a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:25:07 +0000

    Bug 704642 - Rewrite TPS add-ons functionality; style improvements to TPS module; r=rnewman
    UltraBlame original commit: 883459c2e7a774f5e204d9aa48fe4ae727be5c5a

--- services/sync/tests/tps/test_addon_sanity.js
+++ services/sync/tests/tps/test_addon_sanity.js
@@ -7,44 +7,23 @@
 
 
 
-var phases = { "phase1": "profile1",
-               "phase2": "profile1",
-               "phase3": "profile1",
-               "phase4": "profile1",
-               "phase5": "profile1" };
+EnableEngines(["addons"]);
 
+let phases = { "phase1": "profile1",
+               "phase2": "profile1" };
 
+const id = "unsigned-xpi@tests.mozilla.org";
 
+Phase("phase1", [
+  [Addons.install, [id]],
+  
+  [Addons.verifyNot, [id]],
 
-
-Phase('phase1', [
-  [Addons.install, ['unsigned-1.0.xml']],
-  [Addons.verify, ['unsigned-xpi@tests.mozilla.org'], STATE_DISABLED],
-  [Sync, SYNC_WIPE_SERVER],
+  
+  [Sync]
 ]);
 
-Phase('phase2', [
-  [Sync],
-  [Addons.verify, ['unsigned-xpi@tests.mozilla.org'], STATE_ENABLED],
-  [Addons.setState, ['unsigned-xpi@tests.mozilla.org'], STATE_DISABLED],
-  [Sync],
-]);
-
-Phase('phase3', [
-  [Sync],
-  [Addons.verify, ['unsigned-xpi@tests.mozilla.org'], STATE_DISABLED],
-  [Addons.setState, ['unsigned-xpi@tests.mozilla.org'], STATE_ENABLED],
-  [Sync],
-]);
-
-Phase('phase4', [
-  [Sync],
-  [Addons.verify, ['unsigned-xpi@tests.mozilla.org'], STATE_ENABLED],
-  [Addons.uninstall, ['unsigned-xpi@tests.mozilla.org']],
-  [Sync],
-]);
-
-Phase('phase5', [
-  [Sync],
-  [Addons.verifyNot, ['unsigned-xpi@tests.mozilla.org']],
+Phase("phase2", [
+  
+  [Addons.verify, [id], STATE_ENABLED]
 ]);
