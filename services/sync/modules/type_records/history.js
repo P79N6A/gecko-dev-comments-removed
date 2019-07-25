b0ac369429df82d1937c1b93fc7c42242b2c7db3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:21:42 +0000

    update history engine to work again with latest engine changes, and re-enable history sync
    UltraBlame original commit: 28209627fc77ff1c63338dea863a7de5c9fe31a0

---
+++ services/sync/modules/type_records/history.js
@@ -0,0 +1,83 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const EXPORTED_SYMBOLS = ['HistoryRec'];
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+Cu.import("resource://weave/log4moz.js");
+Cu.import("resource://weave/util.js");
+Cu.import("resource://weave/async.js");
+Cu.import("resource://weave/base_records/wbo.js");
+Cu.import("resource://weave/base_records/crypto.js");
+Cu.import("resource://weave/base_records/keys.js");
+
+Function.prototype.async = Async.sugar;
+
+function HistoryRec(uri, authenticator) {
+  this._HistoryRec_init(uri, authenticator);
+}
+HistoryRec.prototype = {
+  __proto__: CryptoWrapper.prototype,
+  _logName: "Record.History",
+
+  _HistoryRec_init: function HistItem_init(uri, authenticator) {
+    this._CryptoWrap_init(uri, authenticator);
+    this.cleartext = {
+    };
+  },
+
+  get histUri() this.cleartext.uri,
+  set histUri(value) {
+    if (typeof(value) == "string")
+      this.cleartext.uri = value;
+    else
+      this.cleartext.uri = value.spec;
+  },
+
+  get title() this.cleartext.title,
+  set title(value) {
+    this.cleartext.title = value;
+  },
+
+  get visits() this.cleartext.visits,
+  set visits(value) {
+    this.cleartext.visits = value;
+  }
+};
