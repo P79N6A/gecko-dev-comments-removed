58a007d4823801e2acfa3745c9fe18d937535204
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:34:38 +0000

    Bug 776480 - B2G RIL: missing handler for ipc message 'RIL:GetRadioState. r=philikon
    UltraBlame original commit: 288c6ff3dfd8cde6128fa1ba21509bb12ce471e6

--- dom/system/gonk/RILContentHelper.js
+++ dom/system/gonk/RILContentHelper.js
@@ -141,16 +141,16 @@ function RILContentHelper() {
   Services.obs.addObserver(this, "xpcom-shutdown", false);
 
   
-  let radioState = cpmm.QueryInterface(Ci.nsISyncMessageSender)
-                       .sendSyncMessage("RIL:GetRadioState")[0];
+  let rilContext = cpmm.QueryInterface(Ci.nsISyncMessageSender)
+                       .sendSyncMessage("RIL:GetRilContext")[0];
 
-  if (!radioState) {
-    debug("Received null radioState from chrome process.");
+  if (!rilContext) {
+    debug("Received null rilContext from chrome process.");
     return;
   }
-  this.cardState = radioState.cardState;
-  this.updateConnectionInfo(radioState.voice, this.voiceConnectionInfo);
-  this.updateConnectionInfo(radioState.data, this.dataConnectionInfo);
+  this.cardState = rilContext.cardState;
+  this.updateConnectionInfo(rilContext.voice, this.voiceConnectionInfo);
+  this.updateConnectionInfo(rilContext.data, this.dataConnectionInfo);
 }
 
 RILContentHelper.prototype = {
