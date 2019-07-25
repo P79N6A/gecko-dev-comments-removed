08bbaee22b973788695764230d09984c81c49eaf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:06:36 +0000

    Bug 747197 - abs works on ints, fabs works on doubles, grasshopper.  Smallest bugfix ever.  r=terrence
    UltraBlame original commit: ef7803bad0b1f468850b50f3c4c009c848d3db6b

--- js/src/jsdate.h
+++ js/src/jsdate.h
@@ -64,7 +64,7 @@ inline double
 TimeClip(double time)
 {
     
-    if (!MOZ_DOUBLE_IS_FINITE(time) || abs(time) > 8.64e15)
+    if (!MOZ_DOUBLE_IS_FINITE(time) || fabs(time) > 8.64e15)
         return js_NaN;
 
     
