28c9ce89bf5ee06fb7d7e58561ceeca845b3c3af
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:19:59 +0000

    Bug 586147 - Restore visible tabs (and hide hidden ones) [r=zpao a=beltzner] Port (copy) sessionstore changes from bug 583306 and add tests.
    UltraBlame original commit: 90b492f96ceb41c9927e429506806ce7f570913b

---
+++ browser/components/sessionstore/test/browser/browser_586147.js
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function observeOneRestore(callback) {
+  let topic = "sessionstore-browser-state-restored";
+  Services.obs.addObserver(function() {
+    Services.obs.removeObserver(arguments.callee, topic, false);
+    callback();
+  }, topic, false);
+};
+
+function test() {
+  let ss = Cc["@mozilla.org/browser/sessionstore;1"].getService(Ci.nsISessionStore);
+  waitForExplicitFinish();
+
+  
+  let [origTab] = gBrowser.visibleTabs;
+  let hiddenTab = gBrowser.addTab();
+
+  is(gBrowser.visibleTabs.length, 2, "should have 2 tabs before hiding");
+  gBrowser.showOnlyTheseTabs([origTab]);
+  is(gBrowser.visibleTabs.length, 1, "only 1 after hiding");
+  ok(hiddenTab.hidden, "sanity check that it's hidden");
+
+  let extraTab = gBrowser.addTab();
+  let state = ss.getBrowserState();
+  let stateObj = JSON.parse(state);
+  let tabs = stateObj.windows[0].tabs;
+  is(tabs.length, 3, "just checking that browser state is correct");
+  ok(!tabs[0].hidden, "first tab is visible");
+  ok(tabs[1].hidden, "second is hidden");
+  ok(!tabs[2].hidden, "third is visible");
+
+  
+  tabs[2].hidden = true;
+
+  observeOneRestore(function() {
+    let testWindow = Services.wm.getEnumerator("navigator:browser").getNext();
+    is(testWindow.gBrowser.visibleTabs.length, 1, "only restored 1 visible tab");
+    let tabs = testWindow.gBrowser.tabs;
+    ok(!tabs[0].hidden, "first is still visible");
+    ok(tabs[1].hidden, "second tab is still hidden");
+    ok(tabs[2].hidden, "third tab is now hidden");
+
+    
+    gBrowser.removeTab(hiddenTab);
+    gBrowser.removeTab(extraTab);
+    finish();
+  });
+  ss.setBrowserState(JSON.stringify(stateObj));
+}
