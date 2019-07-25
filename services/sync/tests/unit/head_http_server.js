0ac1b5cbe01b5d4e393fc49f9dd8685dff3c7502
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:02:45 +0000

    Bug 693864 - Implement /storage DELETE handling in test JS Sync server. r=philikon
    UltraBlame original commit: 2c9b36620889ee79db0547b7c4f6ab576ad66138

--- services/sync/tests/unit/head_http_server.js
+++ services/sync/tests/unit/head_http_server.js
@@ -718,6 +718,27 @@ SyncServer.prototype = {
 
 
 
+  deleteCollections: function deleteCollections(username) {
+    if (!(username in this.users)) {
+      throw new Error("Unknown user.");
+    }
+    let userCollections = this.users[username].collections;
+    for each (let [name, coll] in Iterator(userCollections)) {
+      this._log.trace("Bulk deleting " + name + " for " + username + "...");
+      coll.delete({});
+    }
+    this.users[username].collections = {};
+    return this.timestamp();
+  },
+
+  
+
+
+
+
+
+
+
 
 
   user: function user(username) {
@@ -727,11 +748,13 @@ SyncServer.prototype = {
     let modified         = function (collectionName) {
       return collection(collectionName).timestamp;
     }
+    let deleteCollections = this.deleteCollections.bind(this, username);
     return {
-      collection:       collection,
-      createCollection: createCollection,
-      createContents:   createContents,
-      modified:         modified
+      collection:        collection,
+      createCollection:  createCollection,
+      createContents:    createContents,
+      deleteCollections: deleteCollections,
+      modified:          modified
     };
   },
 
@@ -754,7 +777,7 @@ SyncServer.prototype = {
 
 
 
-  pathRE: /^\/([0-9]+(?:\.[0-9]+)?)\/([-._a-zA-Z0-9]+)\/([^\/]+)\/(.*)$/,
+  pathRE: /^\/([0-9]+(?:\.[0-9]+)?)\/([-._a-zA-Z0-9]+)(?:\/([^\/]+)(?:\/(.+))?)?$/,
   storageRE: /^([-_a-zA-Z0-9]+)(?:\/([-_a-zA-Z0-9]+)\/?)?$/,
 
   defaultHeaders: {},
@@ -826,6 +849,25 @@ SyncServer.prototype = {
 
   toplevelHandlers: {
     "storage": function handleStorage(handler, req, resp, version, username, rest) {
+      let respond = this.respond.bind(this, req, resp);
+      if (!rest || !rest.length) {
+        this._log.debug("SyncServer: top-level storage " +
+                        req.method + " request.");
+
+        
+        if (req.method != "DELETE") {
+          respond(405, "Method Not Allowed", "[]", {"Allow": "DELETE"});
+          return;
+        }
+
+        
+        let timestamp = this.user(username).deleteCollections();
+
+        
+        respond(200, "OK", JSON.stringify(timestamp));
+        return;
+      }
+
       let match = this.storageRE.exec(rest);
       if (!match) {
         this._log.warn("SyncServer: Unknown storage operation " + rest);
@@ -833,7 +875,6 @@ SyncServer.prototype = {
       }
       let [all, collection, wboID] = match;
       let coll = this.getCollection(username, collection);
-      let respond = this.respond.bind(this, req, resp);
       switch (req.method) {
         case "GET":
           if (!coll) {
