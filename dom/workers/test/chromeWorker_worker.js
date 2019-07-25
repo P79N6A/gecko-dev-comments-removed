d1743ca32e3911c2cf01cc6e3e4a210396d38584
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:52:03 +0000

    Bug 679551 - 'Workers: Deadlock in WorkerPrivate::BlockAndCollectRuntimeStats if worker is blocked (LastPass extension)'. r=mrbkap.
    UltraBlame original commit: 58d026601240cec8a26acc78a7cfdb6dc0bb55ae

--- dom/workers/test/chromeWorker_worker.js
+++ dom/workers/test/chromeWorker_worker.js
@@ -6,6 +6,11 @@ if (!("ctypes" in self)) {
   throw "No ctypes!";
 }
 
+
+if (ctypes.toString() != "[object ctypes]") {
+  throw "Bad ctypes object: " + ctypes.toString();
+}
+
 onmessage = function(event) {
   let worker = new ChromeWorker("chromeWorker_subworker.js");
   worker.onmessage = function(event) {
