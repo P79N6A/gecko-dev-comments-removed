ad71ae4b1069d528261f0a25bbdd0afd3f83883a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:04:01 +0000

    Bug 607634: Shrink our prefetch region to reduce memory usage. r=mbrubeck a=blocking-fennec
    UltraBlame original commit: f7b92a8c7b1e0af29594f11491743a8208e3b39f

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -37,8 +37,8 @@
 #filter substitution
 
 
-pref("toolkit.browser.cachePixelX", 800);
-pref("toolkit.browser.cachePixelY", 2000);
+pref("toolkit.browser.cachePixelX", 580);
+pref("toolkit.browser.cachePixelY", 1000);
 pref("toolkit.browser.recacheRatio", 60);
 
 pref("toolkit.defaultChromeURI", "chrome://browser/content/browser.xul");
