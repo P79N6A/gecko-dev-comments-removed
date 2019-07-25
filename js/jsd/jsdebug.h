201d0d1ac9a9602902d97c3cbd204ea20851091b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:56:15 +0000

    Backed out changeset af020f2b9293 due to x64 build bustage.
    UltraBlame original commit: a9ec8c6319fb0172ccb832fb4da8e0941eb04c45

--- js/jsd/jsdebug.h
+++ js/jsd/jsdebug.h
@@ -490,13 +490,6 @@ JSD_GetClosestPC(JSDContext* jsdc, JSDScript* jsdscript, uintN line);
 
 
 
-extern JSD_PUBLIC_API(jsuword)
-JSD_GetEndPC(JSDContext* jsdc, JSDScript* jsdscript);
-
-
-
-
-
 
 extern JSD_PUBLIC_API(uintN)
 JSD_GetClosestLine(JSDContext* jsdc, JSDScript* jsdscript, jsuword pc);
