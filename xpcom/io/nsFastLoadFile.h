a34fe6742c463ea2a1346cc5cbbf133e88bc040a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:02:37 +0000

    bug 588873: Catch EXCEPTION_IN_PAGE_ERROR r=ben.hsieh a=approval-2.0
    UltraBlame original commit: 30e1cfee35452d8a7789b15c618918d2b750746f

--- xpcom/io/nsFastLoadFile.h
+++ xpcom/io/nsFastLoadFile.h
@@ -56,6 +56,7 @@
 #include "nsIFastLoadService.h"
 #include "nsISeekableStream.h"
 #include "nsISupportsArray.h"
+#include "mozilla/FileUtils.h"
 
 
 
@@ -400,6 +401,12 @@ class nsFastLoadFileReader
     PRUint32 mFilePos;  
     PRFileMap *mFileMap;
     PRUint8 *mFileData; 
+#ifdef XP_WIN
+    
+    
+    
+    mozilla::AutoFDClose mFD;
+#endif
 };
 
 NS_COM nsresult
