7875ea391b4849da6e860f99ec51f3a5c26dea6b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:30:42 +0000

    Backout 751aa72f61a2 (bug 729495) for robocop testAllPagesTab failures
    UltraBlame original commit: 01a48ccbd15e79b9284ae33be868a7529858e8cb

--- mobile/android/base/awesomebar/AwesomeBarTab.java
+++ mobile/android/base/awesomebar/AwesomeBarTab.java
@@ -112,7 +112,7 @@ abstract public class AwesomeBarTab {
         
         if (TextUtils.isEmpty(title)) {
             int urlIndex = cursor.getColumnIndexOrThrow(URLColumns.URL);
-            title = StringUtils.prettyURL(cursor.getString(urlIndex));
+            title = cursor.getString(urlIndex);
         }
 
         titleView.setText(title);
@@ -122,7 +122,7 @@ abstract public class AwesomeBarTab {
         int urlIndex = cursor.getColumnIndexOrThrow(URLColumns.URL);
         String url = cursor.getString(urlIndex);
 
-        urlView.setText(StringUtils.prettyURL(url));
+        urlView.setText(url);
     }
 
     protected boolean hideSoftInput(View view) {
