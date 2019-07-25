4a51ab503f81a76b1045fa60c62692819d14ce77
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:34:04 +0000

    Backout 4987ffd173a4 (bug 687267) for bustage
    UltraBlame original commit: a501733bc7b5124e6b94492914260d56a84bcc38

---
+++ widget/android/AndroidMediaLayer.cpp
@@ -0,0 +1,148 @@
+
+
+
+
+
+#include <android/log.h>
+#include "AndroidMediaLayer.h"
+#include "AndroidBridge.h"
+
+#define LOG(args...)  __android_log_print(ANDROID_LOG_INFO, "AndroidMediaLayer" , ## args)
+
+
+namespace mozilla {
+
+AndroidMediaLayer::AndroidMediaLayer()
+  : mInverted(false), mVisible(true) {
+}
+
+AndroidMediaLayer::~AndroidMediaLayer() {
+  if (mContentData.window && AndroidBridge::Bridge()) {
+    AndroidBridge::Bridge()->ReleaseNativeWindow(mContentData.window);
+    mContentData.window = NULL;
+  }
+
+  if (mContentData.surface && AndroidBridge::Bridge()) {
+    AndroidBridge::Bridge()->DestroySurface(mContentData.surface);
+    mContentData.surface = NULL;
+  }
+
+  std::map<void*, SurfaceData*>::iterator it;
+
+  for (it = mVideoSurfaces.begin(); it != mVideoSurfaces.end(); it++) {
+    SurfaceData* data = it->second;
+
+    if (AndroidBridge::Bridge()) {
+      AndroidBridge::Bridge()->ReleaseNativeWindow(data->window);
+      AndroidBridge::Bridge()->DestroySurface(data->surface);
+    }
+
+    delete data;
+  }
+
+  mVideoSurfaces.clear();
+}
+
+bool AndroidMediaLayer::EnsureContentSurface() {
+  if (!mContentData.surface && AndroidBridge::Bridge()) {
+    mContentData.surface = AndroidBridge::Bridge()->CreateSurface();
+    if (mContentData.surface) {
+      mContentData.window = AndroidBridge::Bridge()->AcquireNativeWindow(AndroidBridge::GetJNIEnv(), mContentData.surface);
+      AndroidBridge::Bridge()->SetNativeWindowFormat(mContentData.window, 0, 0, AndroidBridge::WINDOW_FORMAT_RGBA_8888);
+    }
+  }
+
+  return mContentData.surface && mContentData.window;
+}
+
+void* AndroidMediaLayer::GetNativeWindowForContent() {
+  if (!EnsureContentSurface())
+    return NULL;
+
+  return mContentData.window;
+}
+
+void* AndroidMediaLayer::RequestNativeWindowForVideo() {
+  if (!AndroidBridge::Bridge())
+    return NULL;
+
+  jobject surface = AndroidBridge::Bridge()->CreateSurface();
+  if (surface) {
+    void* window = AndroidBridge::Bridge()->AcquireNativeWindow(AndroidBridge::GetJNIEnv(), surface);
+    if (window) {
+      AndroidBridge::Bridge()->SetNativeWindowFormat(window, 0, 0, AndroidBridge::WINDOW_FORMAT_RGBA_8888);
+      mVideoSurfaces[window] = new SurfaceData(surface, window);
+      return window;
+    } else {
+      LOG("Failed to create native window from surface");
+
+      
+      AndroidBridge::Bridge()->DestroySurface(surface);
+    }
+  }
+
+  return NULL;
+}
+
+void AndroidMediaLayer::ReleaseNativeWindowForVideo(void* aWindow) {
+  if (mVideoSurfaces.find(aWindow) == mVideoSurfaces.end() || !AndroidBridge::Bridge())
+    return;
+
+  SurfaceData* data = mVideoSurfaces[aWindow];
+
+  AndroidBridge::Bridge()->ReleaseNativeWindow(data->window);
+  AndroidBridge::Bridge()->DestroySurface(data->surface);
+
+  mVideoSurfaces.erase(aWindow);
+  delete data;
+}
+
+void AndroidMediaLayer::SetNativeWindowDimensions(void* aWindow, const gfxRect& aDimensions) {
+  if (mVideoSurfaces.find(aWindow) == mVideoSurfaces.end())
+    return;
+
+  SurfaceData* data = mVideoSurfaces[aWindow];
+  data->dimensions = aDimensions;
+}
+
+void AndroidMediaLayer::UpdatePosition(const gfxRect& aRect) {
+  if (!mVisible || !AndroidBridge::Bridge())
+    return;
+
+  std::map<void*, SurfaceData*>::iterator it;
+
+  for (it = mVideoSurfaces.begin(); it != mVideoSurfaces.end(); it++) {
+    SurfaceData* data = it->second;
+
+    gfxRect videoRect(aRect.x + data->dimensions.x, aRect.y + data->dimensions.y,
+                      data->dimensions.width, data->dimensions.height);
+
+    AndroidBridge::Bridge()->ShowSurface(data->surface, videoRect, mInverted, false);
+  }
+
+  if (EnsureContentSurface()) {
+    AndroidBridge::Bridge()->ShowSurface(mContentData.surface, aRect, mInverted, true);
+  }
+}
+
+void AndroidMediaLayer::SetVisible(bool aVisible) {
+  if (aVisible == mVisible || !AndroidBridge::Bridge())
+    return;
+
+  mVisible = aVisible;
+  if (mVisible)
+    return;
+
+  
+  std::map<void*, SurfaceData*>::iterator it;
+
+  if (EnsureContentSurface())
+    AndroidBridge::Bridge()->HideSurface(mContentData.surface);
+
+  for (it = mVideoSurfaces.begin(); it != mVideoSurfaces.end(); it++) {
+    SurfaceData* data = it->second;
+    AndroidBridge::Bridge()->HideSurface(data->surface);
+  }
+}
+
+} 
