8856dd84ae5470d0cd65d6cca0b0062d84ae86cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:06:26 +0000

    Bug 698656 - mParent pointer should be on nsSHEntry, not nsSHEntryShared. r=bz
    UltraBlame original commit: 65ffdabf1b6b1f7d58cdae08b2f1f1ecbb392fd0

--- docshell/shistory/src/nsSHEntry.cpp
+++ docshell/shistory/src/nsSHEntry.cpp
@@ -81,6 +81,7 @@ nsSHEntry::nsSHEntry(const nsSHEntry &other)
   , mID(other.mID)
   , mScrollPositionX(0)  
   , mScrollPositionY(0)  
+  , mParent(nsnull)
   , mURIWasModified(other.mURIWasModified)
   , mStateData(other.mStateData)
 {
@@ -417,7 +418,7 @@ NS_IMETHODIMP
 nsSHEntry::GetParent(nsISHEntry ** aResult)
 {
   NS_ENSURE_ARG_POINTER(aResult);
-  *aResult = mShared->mParent;
+  *aResult = mParent;
   NS_IF_ADDREF(*aResult);
   return NS_OK;
 }
@@ -430,7 +431,7 @@ nsSHEntry::SetParent(nsISHEntry * aParent)
 
 
 
-  mShared->mParent = aParent;
+  mParent = aParent;
   return NS_OK;
 }
 
