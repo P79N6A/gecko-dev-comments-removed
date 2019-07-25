f6c0e09cc3a3dbfcfe108f5d0364af30c5ae130a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:31 +0000

    Bug 591368 - Get a Feedback XPI in to mobile-browser [r=mbrubeck]
    UltraBlame original commit: 791f5b7ee42fa8b6b34e19c8cd98d793b92325db

---
+++ mobile/app/profile/extensions/feedback@mobile.mozilla.org/content/overlay.js
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var Feedback = {
+  init: function(aEvent) {
+    let appInfo = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo);
+    document.getElementById("feedback-about").setAttribute("desc", appInfo.version);
+  },
+
+  openReadme: function() {
+    let formatter = Cc["@mozilla.org/toolkit/URLFormatterService;1"].getService(Ci.nsIURLFormatter);
+    let url = formatter.formatURLPref("app.releaseNotesURL");
+    BrowserUI.newTab(url);
+  },
+
+  updateRestart: function updateRestart() {
+    let msg = document.getElementById("feedback-messages");
+    if (msg) {
+      let strings = Elements.browserBundle;
+
+      let value = "restart-app";
+      let notification = msg.getNotificationWithValue(value);
+      if (notification)
+        return;
+  
+      let restartCallback = function(aNotification, aDescription) {
+        
+        var cancelQuit = Cc["@mozilla.org/supports-PRBool;1"].createInstance(Ci.nsISupportsPRBool);
+        Services.obs.notifyObservers(cancelQuit, "quit-application-requested", "restart");
+  
+        
+        if (cancelQuit.data == false) {
+          let appStartup = Cc["@mozilla.org/toolkit/app-startup;1"].getService(Ci.nsIAppStartup);
+          appStartup.quit(Ci.nsIAppStartup.eRestart | Ci.nsIAppStartup.eAttemptQuit);
+        }
+      };
+
+      let buttons = [ {
+        label: strings.getString("notificationRestart.button"),
+        accessKey: "",
+        callback: restartCallback
+      } ];
+  
+      let message = strings.getString("notificationRestart.normal");
+      msg.appendNotification(message, value, "", msg.PRIORITY_WARNING_LOW, buttons);
+    }
+  }
+};
+
+window.addEventListener("load", Feedback.init, false);
