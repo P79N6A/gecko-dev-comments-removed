913dffcb6840ae910bdea9d5c6b75b2d115b9682
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:17:54 +0000

    Bug 658093 - BuildFileList can potentially try to read a signature beyond the end of the zip file, r=taras
    UltraBlame original commit: 8834e11cf6bf42efef90bf58a812f65977830e8d

---
+++ modules/libjar/test/unit/test_bug658093.js
@@ -0,0 +1,23 @@
+
+
+
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+
+var ios = Cc["@mozilla.org/network/io-service;1"].
+          getService(Ci.nsIIOService);
+
+function run_test() {
+  var file = do_get_file("data/test_bug658093.zip");
+  var spec = "jar:" + ios.newFileURI(file).spec + "!/0000";
+  var channel = ios.newChannel(spec, null, null);
+  var failed = false;
+  try {
+    var stream = channel.open();
+  } catch (e) {
+    failed = true;
+  }
+  do_check_true(failed);
+}
