5ce77ff77c8d69eaeeb696a4b0645177fba2dab9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:00:05 +0000

    Bug 613449. When setting Aero Glass margins, find the largest opaque rect that contains all windowed plugins, since the DWM doesn't like margins under child windows. r=jmathies,a=blocker
    UltraBlame original commit: 9713b198fe3d41b5f706deb16b42785b7164ab3f

--- widget/src/windows/nsWindow.cpp
+++ widget/src/windows/nsWindow.cpp
@@ -2542,6 +2542,8 @@ void nsWindow::SetTransparencyMode(nsTransparencyMode aMode)
   GetTopLevelWindow(PR_TRUE)->SetWindowTranslucencyInner(aMode);
 }
 
+static const PRInt32 kGlassMarginAdjustment = 2;
+
 void nsWindow::UpdatePossiblyTransparentRegion(const nsIntRegion &aDirtyRegion,
                                                const nsIntRegion &aPossiblyTransparentRegion) {
 #if MOZ_WINSDK_TARGETVER >= MOZ_NTDDI_LONGHORN
@@ -2572,8 +2574,28 @@ void nsWindow::UpdatePossiblyTransparentRegion(const nsIntRegion &aDirtyRegion,
     margins.cxLeftWidth = margins.cxRightWidth = 
       margins.cyTopHeight = margins.cyBottomHeight = -1;
   } else {
+    nsIntRect pluginBounds;
+    for (nsIWidget* child = GetFirstChild(); child; child = child->GetNextSibling()) {
+      nsWindowType type;
+      child->GetWindowType(type);
+      if (type == eWindowType_plugin) {
+        nsIntRect childBounds;
+        child->GetBounds(childBounds);
+        if (mTransparencyMode == eTransparencyBorderlessGlass) {
+          
+          
+          
+          
+          
+          childBounds.Inflate(kGlassMarginAdjustment, kGlassMarginAdjustment);
+        }
+        pluginBounds.UnionRect(pluginBounds, childBounds);
+      }
+    }
+
     
-    nsIntRect largest = opaqueRegion.GetLargestRectangle();
+    
+    nsIntRect largest = opaqueRegion.GetLargestRectangle(pluginBounds);
     margins.cxLeftWidth = largest.x;
     margins.cxRightWidth = clientBounds.width - largest.XMost();
     margins.cyBottomHeight = clientBounds.height - largest.YMost();
@@ -2610,7 +2632,6 @@ void nsWindow::UpdateGlass()
   case eTransparencyBorderlessGlass:
     
     if (margins.cxLeftWidth >= 0) {
-      const PRInt32 kGlassMarginAdjustment = 2;
       margins.cxLeftWidth += kGlassMarginAdjustment;
       margins.cyTopHeight += kGlassMarginAdjustment;
       margins.cxRightWidth += kGlassMarginAdjustment;
