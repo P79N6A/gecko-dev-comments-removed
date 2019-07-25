feca7a7709cae65fe37402edac398fa66e7185b9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:57:49 +0000

    Bug 581758 - use simple QApplication instead of nsQAppInstance for static sQAppInstance. r=dougt
    UltraBlame original commit: 7be6a577f698b2e925cda6a723a29dc41f2c1158

--- toolkit/xre/nsQAppInstance.cpp
+++ toolkit/xre/nsQAppInstance.cpp
@@ -38,23 +38,23 @@
 
 #include "nsQAppInstance.h"
 #include <QApplication>
+#include "prenv.h"
 
 
 extern int    gArgc;
 extern char **gArgv;
 
-nsQAppInstance *nsQAppInstance::sQAppInstance = NULL;
+QApplication *nsQAppInstance::sQAppInstance = NULL;
 int nsQAppInstance::sQAppRefCount = 0;
-
-nsQAppInstance::nsQAppInstance(int gArgc, char** gArgv)
-  : QApplication(gArgc, gArgv)
-{
-}
 
 void nsQAppInstance::AddRef(void) {
   if (qApp) return;
-  if (!sQAppInstance)
-    sQAppInstance = new nsQAppInstance(gArgc, gArgv);
+  if (!sQAppInstance) {
+    const char *graphicsSystem = PR_GetEnv("MOZ_QT_GRAPHICSSYSTEM");
+    if (graphicsSystem)
+      QApplication::setGraphicsSystem(QString(graphicsSystem));
+    sQAppInstance = new QApplication(gArgc, gArgv);
+  }
   sQAppRefCount++;
 }
 
