49cef22ebad6d573f8a5590b62fa64360b3eb12a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:04:42 +0000

    Bug 732016 - Don't call GetFrameFromPoint for non touchstart touch events. r=smaug
    UltraBlame original commit: d07b72d4865d24d98f0bbab28ec0f44eeb71137a

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -5935,6 +5935,12 @@ PresShell::HandleEvent(nsIFrame        *aFrame,
       }
     }
 
+    
+    if (aEvent->eventStructType == NS_TOUCH_EVENT &&
+        aEvent->message != NS_TOUCH_START) {
+      captureRetarget = true;
+    }
+
     bool isWindowLevelMouseExit = (aEvent->message == NS_MOUSE_EXIT) &&
       (static_cast<nsMouseEvent*>(aEvent)->exit == nsMouseEvent::eTopLevel);
 
