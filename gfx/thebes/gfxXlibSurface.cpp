408c93c18b95fbb954e0a46bab5b1755c836d8d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:55:12 +0000

    Bug 619601. If we are asked to create a similar surface and our underling surface isn't valid just return null. r=jrmuizel,a=joe
    UltraBlame original commit: a5413c3c1013cbb90f4b40f5793f49d50d504f5e

--- gfx/thebes/gfxXlibSurface.cpp
+++ gfx/thebes/gfxXlibSurface.cpp
@@ -183,6 +183,10 @@ already_AddRefed<gfxASurface>
 gfxXlibSurface::CreateSimilarSurface(gfxContentType aContent,
                                      const gfxIntSize& aSize)
 {
+    if (!mSurface || !mSurfaceValid) {
+      return nsnull;
+    }
+
     if (aContent == CONTENT_COLOR) {
         
         
