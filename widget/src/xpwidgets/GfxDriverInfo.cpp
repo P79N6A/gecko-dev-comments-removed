69b45850559f0919f4ac0e620550b034a78a3f94
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:15:03 +0000

    Bug 625160 - Part 1 - Split out the DriverInfo structure into a separate file. r=jrmuizel
    UltraBlame original commit: 3706c8ac95b7b1b45382777e991ba35f85e44dd9

---
+++ widget/src/xpwidgets/GfxDriverInfo.cpp
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "GfxDriverInfo.h"
+
+namespace mozilla {
+namespace widget {
+
+PRUint32 GfxDriverInfo::allAdapterVendors = 0;
+PRInt32 GfxDriverInfo::allFeatures = 0;
+GfxDeviceFamily GfxDriverInfo::allDevices = nsnull;
+
+};
+};
---
+++ widget/src/xpwidgets/GfxDriverInfo.h
@@ -0,0 +1,123 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "prtypes.h"
+#include "nsPrintfCString.h"
+
+#ifndef __mozilla_widget_GfxDriverInfo_h__
+#define __mozilla_widget_GfxDriverInfo_h__
+
+namespace mozilla {
+namespace widget {
+
+enum OperatingSystem {
+  DRIVER_OS_UNKNOWN = 0,
+  DRIVER_OS_WINDOWS_2000,
+  DRIVER_OS_WINDOWS_XP,
+  DRIVER_OS_WINDOWS_SERVER_2003,
+  DRIVER_OS_WINDOWS_VISTA,
+  DRIVER_OS_WINDOWS_7,
+  DRIVER_OS_LINUX,
+  DRIVER_OS_OS_X_10_5,
+  DRIVER_OS_OS_X_10_6,
+  DRIVER_OS_ALL
+};
+
+enum VersionComparisonOp {
+  DRIVER_LESS_THAN,             
+  DRIVER_LESS_THAN_OR_EQUAL,    
+  DRIVER_GREATER_THAN,          
+  DRIVER_GREATER_THAN_OR_EQUAL, 
+  DRIVER_EQUAL,                 
+  DRIVER_NOT_EQUAL,             
+  DRIVER_BETWEEN_EXCLUSIVE,     
+  DRIVER_BETWEEN_INCLUSIVE,     
+  DRIVER_BETWEEN_INCLUSIVE_START, 
+  DRIVER_UNKNOWN_COMPARISON
+};
+
+
+typedef const PRUint32 *GfxDeviceFamily;
+
+struct GfxDriverInfo
+{
+  OperatingSystem mOperatingSystem;
+
+  PRUint32 mAdapterVendor;
+  static PRUint32 allAdapterVendors;
+
+  GfxDeviceFamily mDevices;
+  static GfxDeviceFamily allDevices;
+
+  
+  PRInt32 mFeature;
+  static PRInt32 allFeatures;
+
+  
+  PRInt32 mFeatureStatus;
+
+  VersionComparisonOp mComparisonOp;
+
+  
+  PRUint64 mDriverVersion;
+  PRUint64 mDriverVersionMax;
+};
+
+#define GFX_DRIVER_VERSION(a,b,c,d) \
+  ((PRUint64(a)<<48) | (PRUint64(b)<<32) | (PRUint64(c)<<16) | PRUint64(d))
+
+inline bool
+ParseDriverVersion(nsAString& aVersion, PRUint64 *aNumericVersion)
+{
+  int a, b, c, d;
+  
+  if (sscanf(nsPromiseFlatCString(NS_LossyConvertUTF16toASCII(aVersion)).get(),
+             "%d.%d.%d.%d", &a, &b, &c, &d) != 4)
+    return false;
+  if (a < 0 || a > 0xffff) return false;
+  if (b < 0 || b > 0xffff) return false;
+  if (c < 0 || c > 0xffff) return false;
+  if (d < 0 || d > 0xffff) return false;
+
+  *aNumericVersion = GFX_DRIVER_VERSION(a, b, c, d);
+  return true;
+}
+
+};
+};
+
+#endif 
