c4102c6af9bbd4a8f47c34c78d3f600e2676c4a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:03:31 +0000

    Build font data structure by walking the necessary text.  (Bug 706193, patch 3)  r=roc
    Compute the amount of text in the scope of an nsFontInflationData
    object.  This walks the text that's inside of the block formatting
    context at which this object is rooted, excluding the text that's inside
    any nested BFC.  Using the amount of text, the font sizes of the text,
    and the line threshold preference, we compute whether to enable font
    size inflation within that block formatting context.
    
    UltraBlame original commit: 9cf58850cf26befc69b189d831ae6d8e5bff8a7d

--- layout/generic/nsFontInflationData.h
+++ layout/generic/nsFontInflationData.h
@@ -44,13 +44,46 @@
 #include "nsLayoutUtils.h"
 #include "nsBlockFrame.h"
 
+struct nsHTMLReflowState;
+
 class nsFontInflationData
 {
 public:
 
   static nsFontInflationData* FindFontInflationDataFor(const nsIFrame *aFrame);
 
+  static void
+    UpdateFontInflationDataWidthFor(const nsHTMLReflowState& aReflowState);
+
+  static void MarkFontInflationDataTextDirty(nsIFrame *aFrame);
+
+  bool InflationEnabled() {
+    if (mTextDirty) {
+      ScanText();
+    }
+    return mInflationEnabled;
+  }
+
 private:
+
+  nsFontInflationData(nsIFrame* aBFCFrame);
+
+  nsFontInflationData(const nsFontInflationData&) MOZ_DELETE;
+  void operator=(const nsFontInflationData&) MOZ_DELETE;
+
+  void UpdateWidth(const nsHTMLReflowState &aReflowState);
+  enum SearchDirection { eFromStart, eFromEnd };
+  static nsIFrame* FindEdgeInflatableFrameIn(nsIFrame *aFrame,
+                                             SearchDirection aDirection);
+
+  void MarkTextDirty() { mTextDirty = true; }
+  void ScanText();
+  
+  
+  
+  
+  
+  void ScanTextIn(nsIFrame *aFrame);
 
   static const nsIFrame* FlowRootFor(const nsIFrame *aFrame)
   {
@@ -60,6 +93,10 @@ private:
     return aFrame;
   }
 
+  nsIFrame *mBFCFrame;
+  nscoord mTextAmount, mTextThreshold;
+  bool mInflationEnabled; 
+  bool mTextDirty;
 };
 
 #endif 
