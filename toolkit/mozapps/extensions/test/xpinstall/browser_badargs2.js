1fc9b21c46f90c708a464cb8abab4bd27f4c3318
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:36:32 +0000

    Bug 613568: InstallTrigger.install accepts null as a URL and attempts to download the current page as an extension. r=robstrong, a=blocks-beta9
    UltraBlame original commit: d744f676a7583c67f842773935158ba331f30a92

---
+++ toolkit/mozapps/extensions/test/xpinstall/browser_badargs2.js
@@ -0,0 +1,27 @@
+
+
+
+function test() {
+  waitForExplicitFinish();
+
+  var triggers = encodeURIComponent(JSON.stringify({
+    "Unsigned XPI": {
+      URL: undefined
+    }
+  }));
+  gBrowser.selectedTab = gBrowser.addTab();
+  gBrowser.selectedBrowser.addEventListener("load", function() {
+    gBrowser.selectedBrowser.removeEventListener("load", arguments.callee, true);
+    
+    executeSoon(page_loaded);
+  }, true);
+  gBrowser.loadURI(TESTROOT + "installtrigger.html?" + triggers);
+}
+
+function page_loaded() {
+  var doc = gBrowser.contentDocument;
+  is(doc.getElementById("return").textContent, "exception", "installTrigger should have failed");
+  gBrowser.removeCurrentTab();
+  finish();
+}
+
