943e95105a31e37ac5052789d7e946f3daf4696a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:07:37 +0000

    Bug 555345 - Add profiling capability for JIT-generated code (vprof changes only) (r=rreitmai+)
    An earlier patch to tamarin-redux included vprof patches as well as a coordinated set of changes
    to other files in TR.  The vprof directory is imported from nanojit-central, and the vprof changes
    were overwritten.  This patch separates the initialization of new profile records from the logging
    of data therein, which facilitates the use of the profiling infrastructure from JIT-generated code.
    
    UltraBlame original commit: 8ab576d1484ccf8d7899b7c6320a104d37f4c2cc

--- js/src/vprof/vprof.h
+++ js/src/vprof/vprof.h
@@ -105,12 +105,11 @@
 extern "C" {
 #endif
 
-int profileValue (void** id, char* file, int line, int64_t value, ...);
-int _profileEntryValue (void* id, int64_t value);
-int histValue(void** id, char* file, int line, int64_t value, int nbins, ...);
-int _histEntryValue (void* id, int64_t value);
-int64_t _tprof_time();
-extern void* _tprof_before_id;
+int initValueProfile(void** id, char* file, int line, ...);
+int profileValue(void* id, int64_t value);
+int initHistProfile(void** id, char* file, int line, int nbins, ...);
+int histValue(void* id, int64_t value);
+uint64_t readTimestampCounter();
 
 #ifdef __cplusplus
 }
@@ -124,73 +123,97 @@ extern void* _tprof_before_id;
 #define _vprof(v,...)
 #define _hprof(h,n,...)
 #define _nhprof(e,v,n,...)
-#define _ntprof(e)
-#define _tprof_end()
+#define _ntprof_begin(e)
+#define _ntprof_end(e)
+#define _jvprof_init(id,...)
+#define _jnvprof_init(id,e,...)
+#define _jvprof(id,v)
 #endif 
 #else
 
+
+
+
+
+
+ 
 #define _vprof(v,...) \
-{ \
+do { \
     static void* id = 0; \
-    (id != 0) ? \
-        _profileEntryValue (id, (int64_t) (v)) \
-    : \
-        profileValue (&id, __FILE__, __LINE__, (int64_t) (v), ##__VA_ARGS__, NULL) \
-    ;\
-}
+    if (id == 0) \
+        initValueProfile(&id, __FILE__, __LINE__, ##__VA_ARGS__, NULL); \
+    profileValue(id, (int64_t) (v)); \
+} while (0)
 
 #define _nvprof(e,v) \
-{ \
+do { \
     static void* id = 0; \
-    (id != 0) ? \
-        _profileEntryValue (id, (int64_t) (v)) \
-    : \
-        profileValue (&id, (char*) (e), -1, (int64_t) (v), NULL) \
-    ; \
-}
+    if (id == 0) \
+        initValueProfile(&id, (char*) (e), -1, NULL); \
+    profileValue(id, (int64_t) (v)); \
+} while (0)
 
 #define _hprof(v,n,...) \
-{ \
+do { \
     static void* id = 0; \
-    (id != 0) ? \
-        _histEntryValue (id, (int64_t) (v)) \
-    : \
-        histValue (&id, __FILE__, __LINE__, (int64_t) (v), (int) (n), ##__VA_ARGS__) \
-    ; \
-}
+    if (id == 0) \
+        initHistProfile(&id, __FILE__, __LINE__, (int) (n), ##__VA_ARGS__); \
+    histValue(id, (int64_t) (v)); \
+} while (0)
 
 #define _nhprof(e,v,n,...) \
-{ \
+do { \
     static void* id = 0; \
-    (id != 0) ? \
-        _histEntryValue (id, (int64_t) (v)) \
-    : \
-        histValue (&id, (char*) (e), -1, (int64_t) (v), (int) (n), ##__VA_ARGS__) \
-    ; \
-}
+    if (id == 0) \
+        initHistProfile(&id, (char*) (e), -1, (int) (n), ##__VA_ARGS__); \
+    histValue(id, (int64_t) (v)); \
+} while (0)
 
-#define _ntprof(e) \
-{ \
-    uint64_t v = _tprof_time();\
-    (_tprof_before_id != 0) ? \
-        _profileEntryValue(_tprof_before_id, v)\
-        : 0;\
+
+
+
+
+
+#define _ntprof_begin(e) \
+do { \
     static void* id = 0; \
-    (id != 0) ? \
-        _profileEntryValue (id, (int64_t) 0) \
-    : \
-        profileValue (&id, (char*)(e), -1, (int64_t) 0, NULL) \
-    ;\
-    _tprof_before_id = id;\
-}
+    if (id == 0) \
+        initValueProfile(&id, (char*)(e), -1, NULL); \
+    ((entry_t)id)->i64var[0] = readTimestampCounter(); \
+} while (0)
 
-#define _tprof_end() \
-{\
-    uint64_t v = _tprof_time();\
-    if (_tprof_before_id)\
-        _profileEntryValue(_tprof_before_id, v);\
-    _tprof_before_id = 0;\
-}
+
+#define TICKS_PER_USEC 2600
+
+#define _ntprof_end(e) \
+do { \
+    static void* id = 0; \
+    uint64_t stop = readTimestampCounter(); \
+    if (id == 0) \
+        initValueProfile(&id, (char*)(e), -1, NULL); \
+    uint64_t start = ((entry_t)id)->i64var[0]; \
+    uint64_t usecs = (stop - start) / TICKS_PER_USEC; \
+    profileValue(id, usecs); \
+} while (0)
+
+
+
+
+
+
+#define _jvprof_init(id,...) \
+    if (*(id) == 0) \
+        initValueProfile((id), __FILE__, __LINE__, ##__VA_ARGS__, NULL)
+
+#define _jnvprof_init(id,e,...) \
+    if (*(id) == 0) \
+        initValueProfile((id), (char*) (e), -1, ##__VA_ARGS__, NULL)
+
+
+
+
+#define _jvprof(id,v) \
+    profileValue((id), (int64_t) (v))
 
 #endif
 
