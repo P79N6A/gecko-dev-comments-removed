45ac9906fac74fe9e950b5137a4d351eb3c7952a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:14:39 +0000

    Bug 472529, websockets (content patch)
    UltraBlame original commit: fb11e27cda712fbe63d0028a15a471533502303a

---
+++ content/base/public/nsDOMEventTargetWrapperCache.h
@@ -0,0 +1,101 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef nsDOMEventTargetWrapperCache_h__
+#define nsDOMEventTargetWrapperCache_h__
+
+#include "nsDOMEventTargetHelper.h"
+#include "nsWrapperCache.h"
+#include "nsIScriptContext.h"
+
+
+
+
+
+class nsDOMEventTargetWrapperCache : public nsDOMEventTargetHelper,
+                                     public nsWrapperCache
+{
+public:  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  class NS_CYCLE_COLLECTION_INNERCLASS
+    : public NS_CYCLE_COLLECTION_CLASSNAME(nsDOMEventTargetHelper)
+  {
+    NS_IMETHOD RootAndUnlinkJSObjects(void *p);
+    NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED_BODY(nsDOMEventTargetWrapperCache,
+                                                  nsDOMEventTargetHelper)
+    NS_IMETHOD_(void) Trace(void *p, TraceCallback cb, void *closure);
+  };
+  NS_CYCLE_COLLECTION_PARTICIPANT_INSTANCE
+  
+  void GetParentObject(nsIScriptGlobalObject **aParentObject)
+  {
+    if (mOwner) {
+      CallQueryInterface(mOwner, aParentObject);
+    }
+    else {
+      *aParentObject = nsnull;
+    }
+  }
+
+  static nsDOMEventTargetWrapperCache* FromSupports(nsISupports* aSupports)
+  {
+    nsPIDOMEventTarget* target =
+      static_cast<nsPIDOMEventTarget*>(aSupports);
+#ifdef DEBUG
+    {
+      nsCOMPtr<nsPIDOMEventTarget> target_qi =
+        do_QueryInterface(aSupports);
+
+      
+      
+      
+      NS_ASSERTION(target_qi == target, "Uh, fix QI!");
+    }
+#endif
+
+    return static_cast<nsDOMEventTargetWrapperCache*>(target);
+  }
+
+protected:
+  nsDOMEventTargetWrapperCache() : nsDOMEventTargetHelper(), nsWrapperCache() {}
+  virtual ~nsDOMEventTargetWrapperCache();
+};
+
+#endif  
---
+++ content/base/src/nsDOMEventTargetWrapperCache.cpp
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "nsContentUtils.h"
+#include "nsDOMEventTargetWrapperCache.h"
+#include "nsIDocument.h"
+
+nsDOMEventTargetWrapperCache::~nsDOMEventTargetWrapperCache()
+{
+  nsISupports *supports = static_cast<nsPIDOMEventTarget*>(this);
+  nsContentUtils::ReleaseWrapper(supports, this);
+}
+
+NS_IMPL_CYCLE_COLLECTION_CLASS(nsDOMEventTargetWrapperCache)
+
+NS_IMPL_CYCLE_COLLECTION_TRACE_BEGIN(nsDOMEventTargetWrapperCache)
+  NS_IMPL_CYCLE_COLLECTION_TRACE_PRESERVED_WRAPPER
+NS_IMPL_CYCLE_COLLECTION_TRACE_END
+
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(nsDOMEventTargetWrapperCache,
+                                                  nsDOMEventTargetHelper)
+  NS_IMPL_CYCLE_COLLECTION_TRAVERSE_SCRIPT_OBJECTS
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
+
+NS_IMPL_CYCLE_COLLECTION_ROOT_BEGIN(nsDOMEventTargetWrapperCache)
+  NS_IMPL_CYCLE_COLLECTION_UNLINK_PRESERVED_WRAPPER
+NS_IMPL_CYCLE_COLLECTION_ROOT_END
+
+NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(nsDOMEventTargetWrapperCache,
+                                                nsDOMEventTargetHelper)
+NS_IMPL_CYCLE_COLLECTION_UNLINK_END
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION_INHERITED(nsDOMEventTargetWrapperCache)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+NS_INTERFACE_MAP_END_INHERITING(nsDOMEventTargetHelper)
+
+NS_IMPL_ADDREF_INHERITED(nsDOMEventTargetWrapperCache, nsDOMEventTargetHelper)
+NS_IMPL_RELEASE_INHERITED(nsDOMEventTargetWrapperCache, nsDOMEventTargetHelper)
