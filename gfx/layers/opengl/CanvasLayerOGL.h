b7449170f3740155f77ec0e47d055a4dfda8750a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:54:51 +0000

    Bug 602428, part 2: Implement HW-decelerating ShadowCanvasLayerOGL. r=vlad
    UltraBlame original commit: 629729b11dc6ee19fff266f00bc8b2c9933d5b5e

--- gfx/layers/opengl/CanvasLayerOGL.h
+++ gfx/layers/opengl/CanvasLayerOGL.h
@@ -38,6 +38,11 @@
 #ifndef GFX_CANVASLAYEROGL_H
 #define GFX_CANVASLAYEROGL_H
 
+#ifdef MOZ_IPC
+# include "mozilla/layers/PLayers.h"
+# include "mozilla/layers/ShadowLayers.h"
+#endif  
+
 #include "LayerManagerOGL.h"
 #include "gfxASurface.h"
 
@@ -81,6 +86,47 @@ protected:
   PRPackedBool mGLBufferIsPremultiplied;
   PRPackedBool mNeedsYFlip;
 };
+
+#ifdef MOZ_IPC
+
+
+
+class ShadowCanvasLayerOGL : public ShadowCanvasLayer,
+                             public LayerOGL
+{
+  typedef gl::TextureImage TextureImage;
+
+public:
+  ShadowCanvasLayerOGL(LayerManagerOGL* aManager);
+  virtual ~ShadowCanvasLayerOGL();
+
+  
+  virtual void Initialize(const Data& aData);
+  
+  virtual void Updated(const nsIntRect&) {}
+
+  
+  virtual already_AddRefed<gfxSharedImageSurface>
+  Swap(gfxSharedImageSurface* aNewFront);
+
+  virtual void DestroyFrontBuffer();
+
+  
+  void Destroy();
+  Layer* GetLayer();
+  virtual void RenderLayer(int aPreviousFrameBuffer,
+                           const nsIntPoint& aOffset);
+
+private:
+  nsRefPtr<TextureImage> mTexImage;
+
+
+  
+  nsRefPtr<gfxSharedImageSurface> mDeadweight;
+
+
+};
+#endif  
 
 } 
 } 
