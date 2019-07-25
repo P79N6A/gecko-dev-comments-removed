09c3bfc087cfff76b5a493a11a68698bbde9359a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:01:07 +0000

    Bug 586482 - arguments.callee.caller not equal to proto-delegated joined function object method (r=igor).
    UltraBlame original commit: 441a46526a95af562ef2ec27691106d3af549b74

---
+++ js/src/tests/js1_8_5/regress/regress-586482.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+var expect = true;
+var actual;
+
+var checkCaller = function(me) {
+    var caller = arguments.callee.caller;
+    var callerIsMethod = (caller === me['doThing']);
+    actual = callerIsMethod;
+};
+
+var MyObj = function() {
+};
+
+MyObj.prototype.doThing = function() {
+    checkCaller(this);
+};
+
+(new MyObj()).doThing();
+
+reportCompare(expect, actual, "ok");
