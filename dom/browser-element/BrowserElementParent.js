4048c5969e9557ec6441bc03aae83cb5baba2e3f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:26:00 +0000

    Bug 764248 - Create one BrowserElementParent object for each <iframe mozbrowser>. r=vingtetun
    UltraBlame original commit: dd3791f6709c75a9b93a1e28f8fc04b3f562ab60

--- dom/browser-element/BrowserElementParent.js
+++ dom/browser-element/BrowserElementParent.js
@@ -18,25 +18,24 @@ function debug(msg) {
   
 }
 
-function sendAsyncMsg(frameElement, msg, data) {
-  let mm = frameElement.QueryInterface(Ci.nsIFrameLoaderOwner)
-                       .frameLoader
-                       .messageManager;
 
-  mm.sendAsyncMessage('browser-element-api:' + msg, data);
+
+
+
+
+
+
+
+
+
+
+
+
+function BrowserElementParentFactory() {
+  this._initialized = false;
 }
 
-
-
-
-
-
-
-
-
-
-function BrowserElementParent() {}
-BrowserElementParent.prototype = {
+BrowserElementParentFactory.prototype = {
   classID: Components.ID("{ddeafdac-cb39-47c4-9cb8-c9027ee36d26}"),
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
                                          Ci.nsISupportsWeakReference]),
@@ -61,8 +60,10 @@ BrowserElementParent.prototype = {
     debug("_init");
     this._initialized = true;
 
-    this._screenshotListeners = {};
-    this._screenshotReqCounter = 0;
+    
+    
+    
+    this._bepMap = new WeakMap();
 
     var os = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
     os.addObserver(this, 'remote-browser-frame-shown',  true);
@@ -81,175 +82,17 @@ BrowserElementParent.prototype = {
 
   _observeInProcessBrowserFrameShown: function(frameLoader) {
     debug("In-process browser frame shown " + frameLoader);
-    this._setUpMessageManagerListeners(frameLoader);
+    this._createBrowserElementParent(frameLoader);
   },
 
   _observeRemoteBrowserFrameShown: function(frameLoader) {
     debug("Remote browser frame shown " + frameLoader);
-    this._setUpMessageManagerListeners(frameLoader);
+    this._createBrowserElementParent(frameLoader);
   },
 
-  _setUpMessageManagerListeners: function(frameLoader) {
+  _createBrowserElementParent: function(frameLoader) {
     let frameElement = frameLoader.QueryInterface(Ci.nsIFrameLoader).ownerElement;
-    if (!frameElement) {
-      debug("No frame element?");
-      return;
-    }
-
-    let mm = frameLoader.messageManager;
-
-    
-    
-
-    let self = this;
-    function addMessageListener(msg, handler) {
-      mm.addMessageListener('browser-element-api:' + msg, handler.bind(self, frameElement));
-    }
-
-    addMessageListener("hello", this._recvHello);
-    addMessageListener("locationchange", this._fireEventFromMsg);
-    addMessageListener("loadstart", this._fireEventFromMsg);
-    addMessageListener("loadend", this._fireEventFromMsg);
-    addMessageListener("titlechange", this._fireEventFromMsg);
-    addMessageListener("iconchange", this._fireEventFromMsg);
-    addMessageListener("close", this._fireEventFromMsg);
-    addMessageListener("get-mozapp-manifest-url", this._sendMozAppManifestURL);
-    addMessageListener("keyevent", this._fireKeyEvent);
-    addMessageListener("showmodalprompt", this._handleShowModalPrompt);
-    mm.addMessageListener('browser-element-api:got-screenshot',
-                          this._recvGotScreenshot.bind(this));
-
-    XPCNativeWrapper.unwrap(frameElement).getScreenshot =
-      this._getScreenshot.bind(this, mm, frameElement);
-
-    XPCNativeWrapper.unwrap(frameElement).setVisible =
-      this._setVisible.bind(this, mm, frameElement);
-
-    mm.loadFrameScript("chrome://global/content/BrowserElementChild.js",
-                        true);
-  },
-
-  _recvHello: function(frameElement, data) {
-    debug("recvHello " + frameElement);
-  },
-
-  
-
-
-
-  _fireEventFromMsg: function(frameElement, data) {
-    let name = data.name.substring('browser-element-api:'.length);
-    let detail = data.json;
-
-    debug('fireEventFromMsg: ' + name + ', ' + detail);
-    let evt = this._createEvent(frameElement, name, detail,
-                                 false);
-    frameElement.dispatchEvent(evt);
-  },
-
-  _handleShowModalPrompt: function(frameElement, data) {
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    let detail = data.json;
-    debug('handleShowPrompt ' + JSON.stringify(detail));
-
-    
-    
-    let windowID = detail.windowID;
-    delete detail.windowID;
-    debug("Event will have detail: " + JSON.stringify(detail));
-    let evt = this._createEvent(frameElement, 'showmodalprompt', detail,
-                                 true);
-
-    let unblockMsgSent = false;
-    function sendUnblockMsg() {
-      if (unblockMsgSent) {
-        return;
-      }
-      unblockMsgSent = true;
-
-      
-      
-
-      let data = { windowID: windowID,
-                   returnValue: evt.detail.returnValue };
-      sendAsyncMsg(frameElement, 'unblock-modal-prompt', data);
-    }
-
-    XPCNativeWrapper.unwrap(evt.detail).unblock = function() {
-      sendUnblockMsg();
-    };
-
-    frameElement.dispatchEvent(evt);
-
-    if (!evt.defaultPrevented) {
-      
-      
-      sendUnblockMsg();
-    }
-  },
-
-  _createEvent: function(frameElement, evtName, detail, cancelable) {
-    let win = frameElement.ownerDocument.defaultView;
-    let evt;
-
-    
-    
-    if (detail !== undefined && detail !== null) {
-      evt = new win.CustomEvent('mozbrowser' + evtName,
-                                {bubbles: true, cancelable: cancelable,
-                                 detail: detail});
-    }
-    else {
-      evt = new win.Event('mozbrowser' + evtName,
-                          {bubbles: true, cancelable: cancelable});
-    }
-
-    return evt;
-  },
-
-  _sendMozAppManifestURL: function(frameElement, data) {
-    return frameElement.getAttribute('mozapp');
-  },
-
-  _recvGotScreenshot: function(data) {
-    var req = this._screenshotListeners[data.json.id];
-    delete this._screenshotListeners[data.json.id];
-    Services.DOMRequest.fireSuccess(req, data.json.screenshot);
-  },
-
-  _getScreenshot: function(mm, frameElement) {
-    let id = 'req_' + this._screenshotReqCounter++;
-    let req = Services.DOMRequest
-      .createRequest(frameElement.ownerDocument.defaultView);
-    this._screenshotListeners[id] = req;
-    mm.sendAsyncMessage('browser-element-api:get-screenshot', {id: id});
-    return req;
-  },
-
-   _setVisible: function(mm, frameElement, visible) {
-     mm.sendAsyncMessage('browser-element-api:set-visible', {visible: visible});
-   },
-
-  _fireKeyEvent: function(frameElement, data) {
-    let win = frameElement.ownerDocument.defaultView;
-    let evt = frameElement.ownerDocument.createEvent("KeyboardEvent");
-
-    evt.initKeyEvent(data.json.type, true, true, win,
-                     false, false, false, false, 
-                     data.json.keyCode,
-                     data.json.charCode);
-
-    frameElement.dispatchEvent(evt);
+    this._bepMap.set(frameElement, new BrowserElementParent(frameLoader));
   },
 
   observe: function(subject, topic, data) {
@@ -275,4 +118,180 @@ BrowserElementParent.prototype = {
   },
 };
 
-var NSGetFactory = XPCOMUtils.generateNSGetFactory([BrowserElementParent]);
+function BrowserElementParent(frameLoader) {
+  debug("Creating new BrowserElementParent object for " + frameLoader);
+  this._screenshotListeners = {};
+  this._screenshotReqCounter = 0;
+
+  this._frameElement = frameLoader.QueryInterface(Ci.nsIFrameLoader).ownerElement;
+  if (!this._frameElement) {
+    debug("No frame element?");
+    return;
+  }
+
+  this._mm = frameLoader.messageManager;
+
+  
+  
+
+  let self = this;
+  function addMessageListener(msg, handler) {
+    self._mm.addMessageListener('browser-element-api:' + msg, handler.bind(self));
+  }
+
+  addMessageListener("hello", this._recvHello);
+  addMessageListener("locationchange", this._fireEventFromMsg);
+  addMessageListener("loadstart", this._fireEventFromMsg);
+  addMessageListener("loadend", this._fireEventFromMsg);
+  addMessageListener("titlechange", this._fireEventFromMsg);
+  addMessageListener("iconchange", this._fireEventFromMsg);
+  addMessageListener("close", this._fireEventFromMsg);
+  addMessageListener("get-mozapp-manifest-url", this._sendMozAppManifestURL);
+  addMessageListener("keyevent", this._fireKeyEvent);
+  addMessageListener("showmodalprompt", this._handleShowModalPrompt);
+  addMessageListener('got-screenshot', this._recvGotScreenshot);
+
+  function defineMethod(name, fn) {
+    XPCNativeWrapper.unwrap(self._frameElement)[name] = fn.bind(self);
+  }
+
+  
+  defineMethod('getScreenshot', this._getScreenshot);
+  defineMethod('setVisible', this._setVisible);
+
+  self._mm.loadFrameScript("chrome://global/content/BrowserElementChild.js",
+                            true);
+}
+
+BrowserElementParent.prototype = {
+  get _window() {
+    return this._frameElement.ownerDocument.defaultView;
+  },
+
+  _sendAsyncMsg: function(msg, data) {
+    this._frameElement.QueryInterface(Ci.nsIFrameLoaderOwner)
+                      .frameLoader
+                      .messageManager
+                      .sendAsyncMessage('browser-element-api:' + msg, data);
+  },
+
+  _recvHello: function(data) {
+    debug("recvHello");
+  },
+
+  
+
+
+
+  _fireEventFromMsg: function(data) {
+    let name = data.name.substring('browser-element-api:'.length);
+    let detail = data.json;
+
+    debug('fireEventFromMsg: ' + name + ', ' + detail);
+    let evt = this._createEvent(name, detail,
+                                 false);
+    this._frameElement.dispatchEvent(evt);
+  },
+
+  _handleShowModalPrompt: function(data) {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    let detail = data.json;
+    debug('handleShowPrompt ' + JSON.stringify(detail));
+
+    
+    
+    let windowID = detail.windowID;
+    delete detail.windowID;
+    debug("Event will have detail: " + JSON.stringify(detail));
+    let evt = this._createEvent('showmodalprompt', detail,
+                                 true);
+
+    let self = this;
+    let unblockMsgSent = false;
+    function sendUnblockMsg() {
+      if (unblockMsgSent) {
+        return;
+      }
+      unblockMsgSent = true;
+
+      
+      
+
+      let data = { windowID: windowID,
+                   returnValue: evt.detail.returnValue };
+      self._sendAsyncMsg('unblock-modal-prompt', data);
+    }
+
+    XPCNativeWrapper.unwrap(evt.detail).unblock = function() {
+      sendUnblockMsg();
+    };
+
+    this._frameElement.dispatchEvent(evt);
+
+    if (!evt.defaultPrevented) {
+      
+      
+      sendUnblockMsg();
+    }
+  },
+
+  _createEvent: function(evtName, detail, cancelable) {
+    
+    
+    if (detail !== undefined && detail !== null) {
+      return new this._window.CustomEvent('mozbrowser' + evtName,
+                                          { bubbles: true,
+                                            cancelable: cancelable,
+                                            detail: detail });
+    }
+
+    return new this._window.Event('mozbrowser' + evtName,
+                                  { bubbles: true,
+                                    cancelable: cancelable });
+  },
+
+  _sendMozAppManifestURL: function(data) {
+    return this._frameElement.getAttribute('mozapp');
+  },
+
+
+  _getScreenshot: function() {
+    let id = 'req_' + this._screenshotReqCounter++;
+    let req = Services.DOMRequest.createRequest(this._window);
+    this._screenshotListeners[id] = req;
+    this._sendAsyncMsg('get-screenshot', {id: id});
+    return req;
+  },
+
+  _recvGotScreenshot: function(data) {
+    var req = this._screenshotListeners[data.json.id];
+    delete this._screenshotListeners[data.json.id];
+    Services.DOMRequest.fireSuccess(req, data.json.screenshot);
+  },
+
+  _setVisible: function(visible) {
+    this._sendAsyncMsg('set-visible', {visible: visible});
+  },
+
+  _fireKeyEvent: function(data) {
+    let evt = this._window.document.createEvent("KeyboardEvent");
+    evt.initKeyEvent(data.json.type, true, true, this._window,
+                     false, false, false, false, 
+                     data.json.keyCode,
+                     data.json.charCode);
+
+    this._frameElement.dispatchEvent(evt);
+  },
+};
+
+var NSGetFactory = XPCOMUtils.generateNSGetFactory([BrowserElementParentFactory]);
