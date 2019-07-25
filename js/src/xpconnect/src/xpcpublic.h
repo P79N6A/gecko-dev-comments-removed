8947a26153e09033315aa35ff0fad62d8614ac9c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:29:20 +0000

    Bug 592871 - TM: compartment for Global Object created in nsXULPDGlobalObject::EnsureScriptEnvironment r=mrbkap
    UltraBlame original commit: 58e52006756c8f63d0989228cd899a89c349c772

---
+++ js/src/xpconnect/src/xpcpublic.h
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef xpcpublic_h
+#define xpcpublic_h
+
+#include "jsapi.h"
+#include "nsAString.h"
+#include "nsIPrincipal.h"
+
+nsresult
+xpc_CreateGlobalObject(JSContext *cx, JSClass *clasp,
+                       const nsACString &origin, nsIPrincipal *principal,
+                       JSObject **global, JSCompartment **compartment);
+
+#endif
