1ea8d60924025dcd265b48a3c109df1ae840a033
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:37:12 +0000

    Bug 777233 - Add warning module to mozbuild to track compiler warnings; r=vlad
    UltraBlame original commit: d6deca9f6666164191d114e10fb4c80366aa3545

---
+++ python/mozbuild/mozbuild/util.py
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+import hashlib
+
+
+def hash_file(path):
+    """Hashes a file specified by the path given and returns the hex digest."""
+
+    
+    
+    h = hashlib.sha1()
+
+    with open(path, 'rb') as fh:
+        while True:
+            data = fh.read(8192)
+
+            if not len(data):
+                break
+
+            h.update(data)
+
+    return h.hexdigest()
