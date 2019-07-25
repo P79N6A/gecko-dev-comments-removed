ae607f7ae3485f590612ef5a3f8ab4cc20a926bf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:06:27 +0000

    Refactored Narcissus to use the module pattern (bug 583913, r=pwalton)
    UltraBlame original commit: 77ad24a32c72bffbc699e0a7bdc0d302ad889793

--- js/narcissus/jsdefs.js
+++ js/narcissus/jsdefs.js
@@ -42,139 +42,158 @@
 
 
 
-var tokens = [
+
+Narcissus = {};
+Narcissus.jsdefs = (function() {
+
+    var tokens = [
+        
+        "END",
     
-    "END",
-
+        
+        
+        "\n", ";",
+        ",",
+        "=",
+        "?", ":", "CONDITIONAL",
+        "||",
+        "&&",
+        "|",
+        "^",
+        "&",
+        "==", "!=", "===", "!==",
+        "<", "<=", ">=", ">",
+        "<<", ">>", ">>>",
+        "+", "-",
+        "*", "/", "%",
+        "!", "~", "UNARY_PLUS", "UNARY_MINUS",
+        "++", "--",
+        ".",
+        "[", "]",
+        "{", "}",
+        "(", ")",
+    
+        
+        "SCRIPT", "BLOCK", "LABEL", "FOR_IN", "CALL", "NEW_WITH_ARGS", "INDEX",
+        "ARRAY_INIT", "OBJECT_INIT", "PROPERTY_INIT", "GETTER", "SETTER",
+        "GROUP", "LIST", "LET_BLOCK", "ARRAY_COMP", "GENERATOR", "COMP_TAIL",
+    
+        
+        "IDENTIFIER", "NUMBER", "STRING", "REGEXP",
+    
+        
+        "break",
+        "case", "catch", "const", "continue",
+        "debugger", "default", "delete", "do",
+        "else",
+        "false", "finally", "for", "function",
+        "if", "in", "instanceof",
+        "let",
+        "new", "null",
+        "return",
+        "switch",
+        "this", "throw", "true", "try", "typeof",
+        "var", "void",
+        "yield",
+        "while", "with",
+    ];
     
     
-    "\n", ";",
-    ",",
-    "=",
-    "?", ":", "CONDITIONAL",
-    "||",
-    "&&",
-    "|",
-    "^",
-    "&",
-    "==", "!=", "===", "!==",
-    "<", "<=", ">=", ">",
-    "<<", ">>", ">>>",
-    "+", "-",
-    "*", "/", "%",
-    "!", "~", "UNARY_PLUS", "UNARY_MINUS",
-    "++", "--",
-    ".",
-    "[", "]",
-    "{", "}",
-    "(", ")",
-
     
-    "SCRIPT", "BLOCK", "LABEL", "FOR_IN", "CALL", "NEW_WITH_ARGS", "INDEX",
-    "ARRAY_INIT", "OBJECT_INIT", "PROPERTY_INIT", "GETTER", "SETTER",
-    "GROUP", "LIST", "LET_BLOCK", "ARRAY_COMP", "GENERATOR", "COMP_TAIL",
-
     
-    "IDENTIFIER", "NUMBER", "STRING", "REGEXP",
-
     
-    "break",
-    "case", "catch", "const", "continue",
-    "debugger", "default", "delete", "do",
-    "else",
-    "false", "finally", "for", "function",
-    "if", "in", "instanceof",
-    "let",
-    "new", "null",
-    "return",
-    "switch",
-    "this", "throw", "true", "try", "typeof",
-    "var", "void",
-    "yield",
-    "while", "with",
-];
-
-
-
-
-
-var opTypeNames = {
-    '\n':   "NEWLINE",
-    ';':    "SEMICOLON",
-    ',':    "COMMA",
-    '?':    "HOOK",
-    ':':    "COLON",
-    '||':   "OR",
-    '&&':   "AND",
-    '|':    "BITWISE_OR",
-    '^':    "BITWISE_XOR",
-    '&':    "BITWISE_AND",
-    '===':  "STRICT_EQ",
-    '==':   "EQ",
-    '=':    "ASSIGN",
-    '!==':  "STRICT_NE",
-    '!=':   "NE",
-    '<<':   "LSH",
-    '<=':   "LE",
-    '<':    "LT",
-    '>>>':  "URSH",
-    '>>':   "RSH",
-    '>=':   "GE",
-    '>':    "GT",
-    '++':   "INCREMENT",
-    '--':   "DECREMENT",
-    '+':    "PLUS",
-    '-':    "MINUS",
-    '*':    "MUL",
-    '/':    "DIV",
-    '%':    "MOD",
-    '!':    "NOT",
-    '~':    "BITWISE_NOT",
-    '.':    "DOT",
-    '[':    "LEFT_BRACKET",
-    ']':    "RIGHT_BRACKET",
-    '{':    "LEFT_CURLY",
-    '}':    "RIGHT_CURLY",
-    '(':    "LEFT_PAREN",
-    ')':    "RIGHT_PAREN"
-};
-
-
-
-var keywords = {__proto__: null};
-
-
-var tokenIds = {};
-var consts = "const ";
-for (var i = 0, j = tokens.length; i < j; i++) {
-    if (i > 0)
-        consts += ", ";
-    var t = tokens[i];
-    var name;
-    if (/^[a-z]/.test(t)) {
-        name = t.toUpperCase();
-        keywords[t] = i;
-    } else {
-        name = (/^\W/.test(t) ? opTypeNames[t] : t);
+    var opTypeNames = {
+        '\n':   "NEWLINE",
+        ';':    "SEMICOLON",
+        ',':    "COMMA",
+        '?':    "HOOK",
+        ':':    "COLON",
+        '||':   "OR",
+        '&&':   "AND",
+        '|':    "BITWISE_OR",
+        '^':    "BITWISE_XOR",
+        '&':    "BITWISE_AND",
+        '===':  "STRICT_EQ",
+        '==':   "EQ",
+        '=':    "ASSIGN",
+        '!==':  "STRICT_NE",
+        '!=':   "NE",
+        '<<':   "LSH",
+        '<=':   "LE",
+        '<':    "LT",
+        '>>>':  "URSH",
+        '>>':   "RSH",
+        '>=':   "GE",
+        '>':    "GT",
+        '++':   "INCREMENT",
+        '--':   "DECREMENT",
+        '+':    "PLUS",
+        '-':    "MINUS",
+        '*':    "MUL",
+        '/':    "DIV",
+        '%':    "MOD",
+        '!':    "NOT",
+        '~':    "BITWISE_NOT",
+        '.':    "DOT",
+        '[':    "LEFT_BRACKET",
+        ']':    "RIGHT_BRACKET",
+        '{':    "LEFT_CURLY",
+        '}':    "RIGHT_CURLY",
+        '(':    "LEFT_PAREN",
+        ')':    "RIGHT_PAREN"
+    };
+    
+    
+    
+    var keywords = {__proto__: null};
+    
+    
+    var tokenIds = {};
+    
+    
+    var consts = "const ";
+    for (var i = 0, j = tokens.length; i < j; i++) {
+        if (i > 0)
+            consts += ", ";
+        var t = tokens[i];
+        var name;
+        if (/^[a-z]/.test(t)) {
+            name = t.toUpperCase();
+            keywords[t] = i;
+        } else {
+            name = (/^\W/.test(t) ? opTypeNames[t] : t);
+        }
+        consts += name + " = " + i;
+        tokenIds[name] = i;
+        tokens[t] = i;
     }
-    consts += name + " = " + i;
-    tokenIds[name] = i;
-    tokens[t] = i;
-}
-eval(consts + ";");
+    consts += ";";
+    
+    
+    var assignOps = ['|', '^', '&', '<<', '>>', '>>>', '+', '-', '*', '/', '%'];
+    
+    for (i = 0, j = assignOps.length; i < j; i++) {
+        t = assignOps[i];
+        assignOps[t] = tokens[t];
+    }
+    
+    function defineGetter(obj, prop, fn, dontDelete, dontEnum) {
+        Object.defineProperty(obj, prop, { get: fn, configurable: !dontDelete, enumerable: !dontEnum });
+    }
+    
+    function defineProperty(obj, prop, val, dontDelete, readOnly, dontEnum) {
+        Object.defineProperty(obj, prop, { value: val, writable: !readOnly, configurable: !dontDelete, enumerable: !dontEnum });
+    }
+    
+    return {
+      "tokens": tokens,
+      "opTypeNames": opTypeNames,
+      "keywords": keywords,
+      "tokenIds": tokenIds,
+      "consts": consts,
+      "assignOps": assignOps,
+      "defineGetter": defineGetter,
+      "defineProperty": defineProperty
+    };
+}());
 
-
-var assignOps = ['|', '^', '&', '<<', '>>', '>>>', '+', '-', '*', '/', '%'];
-
-for (i = 0, j = assignOps.length; i < j; i++) {
-    t = assignOps[i];
-    assignOps[t] = tokens[t];
-}
-
-function defineGetter(obj, prop, fn, dontDelete, dontEnum) {
-    Object.defineProperty(obj, prop, { get: fn, configurable: !dontDelete, enumerable: !dontEnum });
-}
-
-function defineProperty(obj, prop, val, dontDelete, readOnly, dontEnum) {
-    Object.defineProperty(obj, prop, { value: val, writable: !readOnly, configurable: !dontDelete, enumerable: !dontEnum });
-}
