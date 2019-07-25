41b63b8a80252d3f1e00ead721f3764a849817b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:22:11 +0000

    Bug 704482 patch 2: Further restrict event-based triggering of SMIL animations when scripts are disabled. r=birtles
    UltraBlame original commit: 68c41bfa7671d8fedc34d80afce6e96860d389fa

--- content/smil/nsSMILTimeValueSpec.cpp
+++ content/smil/nsSMILTimeValueSpec.cpp
@@ -322,6 +322,32 @@ nsSMILTimeValueSpec::GetTimedElement(Element* aElement)
   return &animElement->TimedElement();
 }
 
+
+
+bool
+nsSMILTimeValueSpec::IsWhitelistedEvent()
+{
+  
+  if (mParams.mType == nsSMILTimeValueSpecParams::REPEAT) {
+    return true;
+  }
+
+  
+  
+  
+  
+  
+  if (mParams.mType == nsSMILTimeValueSpecParams::EVENT &&
+      (mParams.mEventSymbol == nsGkAtoms::repeat ||
+       mParams.mEventSymbol == NS_NewAtom("repeatEvent") ||
+       mParams.mEventSymbol == NS_NewAtom("beginEvent") ||
+       mParams.mEventSymbol == NS_NewAtom("endEvent"))) {
+    return true;
+  }
+
+  return false;
+}
+
 void
 nsSMILTimeValueSpec::RegisterEventListener(Element* aTarget)
 {
@@ -335,9 +361,10 @@ nsSMILTimeValueSpec::RegisterEventListener(Element* aTarget)
     return;
 
   
-  if (mParams.mType == nsSMILTimeValueSpecParams::ACCESSKEY &&
-      !aTarget->GetOwnerDocument()->IsScriptEnabled())
+  if (!aTarget->GetOwnerDocument()->IsScriptEnabled() &&
+      !IsWhitelistedEvent()) {
     return;
+  }
 
   if (!mEventListener) {
     mEventListener = new EventListener(this);
