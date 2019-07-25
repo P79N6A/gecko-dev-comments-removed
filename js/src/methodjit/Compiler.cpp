254b62d8d36842af445fc342b10e38948fcffc15
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:49:34 +0000

    [INFER] Don't mark the entry register allocation for a loop as needing updating until it has actually been computed, bug 680809.
    UltraBlame original commit: d7217d210771b2095930bb42291d2e4bff7dea89

--- js/src/methodjit/Compiler.cpp
+++ js/src/methodjit/Compiler.cpp
@@ -1455,9 +1455,6 @@ mjit::Compiler::generateMethod()
             continue;
         }
 
-        if (loop && !a->parent)
-            loop->setOuterPC(PC);
-
         frame.setPC(PC);
         frame.setInTryBlock(opinfo->inTryBlock);
 
@@ -1519,6 +1516,12 @@ mjit::Compiler::generateMethod()
         }
         frame.assertValidRegisterState();
         a->jumpMap[uint32(PC - script->code)] = masm.label();
+
+        
+        
+        
+        if (loop && !a->parent)
+            loop->setOuterPC(PC);
 
         SPEW_OPCODE();
         JS_ASSERT(frame.stackDepth() == opinfo->stackDepth);
