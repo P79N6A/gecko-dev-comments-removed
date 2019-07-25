b1d503699666068ab28be66626cd223a1c4fbe9b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:02:48 +0000

    Bug 582643 - Reject '0x' without any hex digits.  r=jorendorff
    UltraBlame original commit: 7d02da2df01e2b9b7d38b997f869fa4aa51984b8

---
+++ js/src/tests/ecma/Number/0x-without-following-hexdigits.js
@@ -0,0 +1,31 @@
+
+
+
+var gTestfile = '0x-without-following-hexdigits.js';
+
+var BUGNUMBER = 582643;
+var summary = "'0x' not followed by hex digits should be a syntax error";
+
+print(BUGNUMBER + ": " + summary);
+
+
+
+
+
+try
+{
+  eval("0x");
+  throw new Error("didn't throw parsing 0x (with no subsequent hex digits)");
+}
+catch (e)
+{
+  assertEq(e instanceof SyntaxError, true,
+           "bad exception thrown: " + e);
+}
+
+
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
+
+print("All tests passed!");
