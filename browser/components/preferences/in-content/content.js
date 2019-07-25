ece5b6fc1dc5a7d641e64922e8725d1ec9190800
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:09:20 +0000

    Bug 732125 - Content pane for in-content preferences. r=jaws,bmcbride
    UltraBlame original commit: 4953a39c52e6e841eddad41975d0420eabe339e3

---
+++ browser/components/preferences/in-content/content.js
@@ -0,0 +1,243 @@
+
+
+
+
+var gContentPane = {
+
+  
+
+
+  init: function ()
+  {
+    this._rebuildFonts();
+    var menulist = document.getElementById("defaultFont");
+    if (menulist.selectedIndex == -1) {
+      menulist.insertItemAt(0, "", "", "");
+      menulist.selectedIndex = 0;
+    }
+  },
+
+  
+
+  
+
+
+
+  updateButtons: function (aButtonID, aPreferenceID)
+  {
+    var button = document.getElementById(aButtonID);
+    var preference = document.getElementById(aPreferenceID);
+    button.disabled = preference.value != true;
+    return undefined;
+  },
+
+  
+
+
+  _exceptionsParams: {
+    popup:   { blockVisible: false, sessionVisible: false, allowVisible: true, 
+               prefilledHost: "", permissionType: "popup" },
+    image:   { blockVisible: true,  sessionVisible: false, allowVisible: true, 
+               prefilledHost: "", permissionType: "image" }
+  },
+
+  
+
+
+  
+  _showExceptions: function (aPermissionType)
+  {
+    var bundlePreferences = document.getElementById("bundlePreferences");
+    var params = this._exceptionsParams[aPermissionType];
+    params.windowTitle = bundlePreferences.getString(aPermissionType + "permissionstitle");
+    params.introText = bundlePreferences.getString(aPermissionType + "permissionstext");
+
+    openDialog("chrome://browser/content/preferences/permissions.xul", 
+               "Browser:Permissions", "resizable=yes", params);
+  },
+
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+
+
+
+  showPopupExceptions: function ()
+  {
+    this._showExceptions("popup");
+  },
+
+  
+
+  
+
+
+
+
+  readLoadImages: function ()
+  {
+    var pref = document.getElementById("permissions.default.image");
+    return (pref.value == 1 || pref.value == 3);
+  },
+
+  
+
+
+
+  writeLoadImages: function ()
+  { 
+    return (document.getElementById("loadImages").checked) ? 1 : 2;
+  },
+
+  
+
+
+
+  showImageExceptions: function ()
+  {
+    this._showExceptions("image");
+  },
+
+  
+
+  
+
+
+
+  showAdvancedJS: function ()
+  {
+    openDialog("chrome://browser/content/preferences/advanced-scripts.xul", 
+               "Browser:AdvancedScripts", null);  
+  },
+
+  
+
+  
+
+
+  _rebuildFonts: function ()
+  {
+    var langGroupPref = document.getElementById("font.language.group");
+    this._selectDefaultLanguageGroup(langGroupPref.value,
+                                     this._readDefaultFontTypeForLanguage(langGroupPref.value) == "serif");
+  },
+
+  
+
+
+  _selectDefaultLanguageGroup: function (aLanguageGroup, aIsSerif)
+  {
+    const kFontNameFmtSerif         = "font.name.serif.%LANG%";
+    const kFontNameFmtSansSerif     = "font.name.sans-serif.%LANG%";
+    const kFontNameListFmtSerif     = "font.name-list.serif.%LANG%";
+    const kFontNameListFmtSansSerif = "font.name-list.sans-serif.%LANG%";
+    const kFontSizeFmtVariable      = "font.size.variable.%LANG%";
+
+    var prefs = [{ format   : aIsSerif ? kFontNameFmtSerif : kFontNameFmtSansSerif,
+                   type     : "fontname",
+                   element  : "defaultFont",
+                   fonttype : aIsSerif ? "serif" : "sans-serif" },
+                 { format   : aIsSerif ? kFontNameListFmtSerif : kFontNameListFmtSansSerif,
+                   type     : "unichar",
+                   element  : null,
+                   fonttype : aIsSerif ? "serif" : "sans-serif" },
+                 { format   : kFontSizeFmtVariable,
+                   type     : "int",
+                   element  : "defaultFontSize",
+                   fonttype : null }];
+    var preferences = document.getElementById("contentPreferences");
+    for (var i = 0; i < prefs.length; ++i) {
+      var preference = document.getElementById(prefs[i].format.replace(/%LANG%/, aLanguageGroup));
+      if (!preference) {
+        preference = document.createElement("preference");
+        var name = prefs[i].format.replace(/%LANG%/, aLanguageGroup);
+        preference.id = name;
+        preference.setAttribute("name", name);
+        preference.setAttribute("type", prefs[i].type);
+        preferences.appendChild(preference);
+      }
+
+      if (!prefs[i].element)
+        continue;
+
+      var element = document.getElementById(prefs[i].element);
+      if (element) {
+        element.setAttribute("preference", preference.id);
+
+        if (prefs[i].fonttype)
+          FontBuilder.buildFontList(aLanguageGroup, prefs[i].fonttype, element);
+
+        preference.setElementValue(element);
+      }
+    }
+  },
+
+  
+
+
+
+  _readDefaultFontTypeForLanguage: function (aLanguageGroup)
+  {
+    const kDefaultFontType = "font.default.%LANG%";
+    var defaultFontTypePref = kDefaultFontType.replace(/%LANG%/, aLanguageGroup);
+    var preference = document.getElementById(defaultFontTypePref);
+    if (!preference) {
+      preference = document.createElement("preference");
+      preference.id = defaultFontTypePref;
+      preference.setAttribute("name", defaultFontTypePref);
+      preference.setAttribute("type", "string");
+      preference.setAttribute("onchange", "gContentPane._rebuildFonts();");
+      document.getElementById("contentPreferences").appendChild(preference);
+    }
+    return preference.value;
+  },
+
+  
+
+
+  
+  configureFonts: function ()
+  {
+    openDialog("chrome://browser/content/preferences/fonts.xul", 
+               "Browser:FontPreferences", null);
+  },
+
+  
+
+
+
+  configureColors: function ()
+  {
+    openDialog("chrome://browser/content/preferences/colors.xul", 
+               "Browser:ColorPreferences", null);  
+  },
+
+  
+
+  
+
+
+  showLanguages: function ()
+  {
+    openDialog("chrome://browser/content/preferences/languages.xul", 
+               "Browser:LanguagePreferences", null);
+  }
+};
