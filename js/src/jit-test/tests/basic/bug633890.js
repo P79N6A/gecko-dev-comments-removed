48ba08965100d368c4b6b34441ccfd7f350a512f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 13:59:29 +0000

    Bug 554955: Revert test and assertion changes from bug 633890. r=jorendorff
    At present, AssertValidPropertyCacheHit doesn't catch most property cache
    failures. I would like to fix it, but doing so makes
    jit-test/tests/basic/bug633890.js fail with a C++ assertion, not a JS shell
    assertion, which jit-test/jit_test.py can't handle.
    
    I think it's important to have AssertValidPropertyCacheHit doing its job;
    it would have caught problems like bug 554955 earlier. Since bug633890.js
    is a known failure (bug 458271), deleting it doesn't reduce coverage.
    
    This patch deletes that test. A later patch in the series fixes
    AssertValidPropertyCacheHit.
    
    UltraBlame original commit: c583d7c12d9db41a0ba20a9749e639c84f29efbc

--- js/src/jit-test/tests/basic/bug633890.js
+++
@@ -1,9 +0,0 @@
-
-var p = /./, x = resolver({}, p), y = resolver({lastIndex: 2}, p), v;
-var a = [];
-for (var i = 0; i < HOTLOOP; i++)
-    a[i] = x;
-a[HOTLOOP] = y;
-for (i = 0; i < a.length; i++)
-    v = a[i].lastIndex;
-assertEq(v, 2);  
