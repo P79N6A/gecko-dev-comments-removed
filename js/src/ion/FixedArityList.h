c6f9c4d87f434a7d0e972d813b2891c0e5ef4718
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:47:14 +0000

    Implement FixedList. (Bug 670484, r=dvander)
    UltraBlame original commit: 6fbb4a76ef7cdc04ec875ab98911dc1ef54a80c1

--- js/src/ion/FixedArityList.h
+++ js/src/ion/FixedArityList.h
@@ -78,6 +78,45 @@ class FixedArityList<T, 0>
 };
 
 
+template <typename T>
+class FixedList
+{
+    size_t length_;
+    T *list_;
+
+  private:
+    FixedList(const FixedList&); 
+    void operator= (const FixedList*); 
+
+  public:
+    FixedList()
+      : length_(0)
+    { }
+
+    
+    bool init(size_t length) {
+        length_ = length;
+        if (length == 0)
+            return true;
+
+        list_ = (T *)GetIonContext()->temp->allocate(length * sizeof(T *));
+        return list_ != NULL;
+    }
+
+    size_t length() const {
+        return length_;
+    }
+
+    T &operator[](size_t index) {
+        JS_ASSERT(index < length_);
+        return list_[index];
+    }
+    const T &operator [](size_t index) const {
+        JS_ASSERT(index < length_);
+        return list_[index];
+    };
+};
+
 } 
 } 
 
