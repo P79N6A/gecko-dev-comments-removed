b4a798d01fb0f950d391f57134b211b8ed65de8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:22:56 +0000

    Bug 744360 - Part 2: add WapPushManager, r=philikon
    UltraBlame original commit: 026af54fa9298fce0437d7678911f81b9f1797b7

--- dom/mms/src/ril/wap_consts.js
+++ dom/mms/src/ril/wap_consts.js
@@ -8,6 +8,14 @@
 
 const WSP_PDU_TYPE_PUSH = 0x06;
 
+
+
+const WDP_PORT_PUSH = 2948;
+
+
+
+const WDP_BEARER_GSM_SMS_GSM_MSISDN = 0x03;
+
 const ALL_CONST_SYMBOLS = Object.keys(this);
 
 const EXPORTED_SYMBOLS = ALL_CONST_SYMBOLS;
