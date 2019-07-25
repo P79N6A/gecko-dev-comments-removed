1f14961a298b987c8b571a1a4edd3c1e08b49555
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:18:28 +0000

    Bug 577990: Visible delay in showing the Language category when opening the Add-Ons Manager. r+a=dtownsend
    UltraBlame original commit: 41302fc3c620247110a67c68799dd04c4dbc30cd

---
+++ toolkit/mozapps/extensions/test/browser/browser_bug577990.js
@@ -0,0 +1,127 @@
+
+
+
+
+
+
+
+var gManagerWindow;
+var gCategoryUtilities;
+var gProvider;
+var gInstall;
+var gInstallProperties = [{
+  name: "Locale Category Test",
+  type: "locale"
+}];
+
+function test() {
+  waitForExplicitFinish();
+
+  gProvider = new MockProvider();
+
+  open_manager(null, function(aWindow) {
+    gManagerWindow = aWindow;
+    gCategoryUtilities = new CategoryUtilities(gManagerWindow);
+    run_next_test();
+  });
+}
+
+function end_test() {
+  close_manager(gManagerWindow, finish);
+}
+
+function install_locale(aCallback) {
+  gInstall = gProvider.createInstalls(gInstallProperties)[0];
+  gInstall.addTestListener({
+    onInstallEnded: function(aInstall) {
+      gInstall.removeTestListener(this);
+      aCallback();
+    }
+  });
+  gInstall.install();
+}
+
+function check_hidden(aExpectedHidden) {
+  var hidden = !gCategoryUtilities.isTypeVisible("locale");
+  is(hidden, !!aExpectedHidden, "Should have correct hidden state");
+}
+
+function run_open_test(aTestSetup, aLoadHidden, aInitializedHidden, aSelected) {
+  function loadCallback(aManagerWindow) {
+    gManagerWindow = aManagerWindow;
+    gCategoryUtilities = new CategoryUtilities(gManagerWindow);
+    check_hidden(aLoadHidden);
+  }
+
+  function run() {
+    open_manager(null, function() {
+      check_hidden(aInitializedHidden);
+      var selected = (gCategoryUtilities.selectedCategory == "locale");
+      is(selected, !!aSelected, "Should have correct selected state");
+
+      run_next_test();
+    }, loadCallback);
+  }
+
+  close_manager(gManagerWindow, function() {
+    
+    aTestSetup ? aTestSetup(run) : run();
+  });
+}
+
+
+
+add_test(function() {
+  run_open_test(null, true, true);
+});
+
+
+
+add_test(function() {
+  check_hidden(true);
+  install_locale(function() {
+    check_hidden(false);
+    run_next_test();
+  });
+});
+
+
+
+add_test(function() {
+  run_open_test(null, false, false);
+});
+
+
+
+add_test(function() {
+  gInstall.cancel();
+  run_open_test(null, false, true)
+});
+
+
+
+add_test(function() {
+  run_open_test(null, true, true);
+});
+
+
+
+
+add_test(function() {
+  run_open_test(install_locale, true, false);
+});
+
+
+add_test(function() {
+  gCategoryUtilities.openType("locale", function() {
+    run_open_test(null, false, false, true);
+  });
+});
+
+
+
+add_test(function() {
+  gInstall.cancel();
+  run_open_test(null, false, true);
+});
+
