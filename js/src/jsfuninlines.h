50ad113c4fc45852276a9011259aa1242a7a9b77
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 12:03:31 +0000

    Back out af1e10bea0f4 from a CLOSED TREE for being orangetastic on one platform.
    UltraBlame original commit: 91cd576da2af137986be12d3d432d48ccdb0c716

--- js/src/jsfuninlines.h
+++ js/src/jsfuninlines.h
@@ -49,30 +49,4 @@ JSFunction::inStrictMode() const
     return script()->strictModeCode;
 }
 
-namespace js {
-
-static inline bool
-IsSafeForLazyThisCoercion(JSContext *cx, JSObject *callee)
-{
-    
-
-
-
-
-
-
-    if (callee->isProxy()) {
-        callee = callee->unwrap();
-        if (!callee->isFunction())
-            return true; 
-
-        JSFunction *fun = callee->getFunctionPrivate();
-        if (fun->isInterpreted() && fun->inStrictMode())
-            return true;
-    }
-    return callee->getGlobal() == cx->fp()->scopeChain().getGlobal();
-}
-
-}
-
 #endif 
