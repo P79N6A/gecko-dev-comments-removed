b26a598495174890e67a1d8667ff1350c8935264
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:01:11 +0000

    Disable generator-using test until bug 565199 is fixed.
    UltraBlame original commit: 890c0d2e45b3e23cb669e6dfdbeffa79a2a64035

--- toolkit/content/tests/browser/browser_bug471962.js
+++ toolkit/content/tests/browser/browser_bug471962.js
@@ -55,94 +55,96 @@ function test() {
   const kBaseUrl =
         "http://mochi.test:8888/browser/toolkit/content/tests/browser/data/";
 
-  function pageShown(event)
-  {
-    if (event.target.location != "about:blank")
-      testRunner.continueTest();
-  }
-
+  
   function FramePostData_TestGenerator() {
-    
-    
-    
-    gBrowser.addEventListener("pageshow", pageShown, false);
-    gBrowser.loadURI(kBaseUrl + "post_form_outer.sjs");
-    yield;
-    gBrowser.removeEventListener("pageshow", pageShown, false);
-
-    try {
-      
-      
-      gBrowser.addEventListener("DOMContentLoaded",
-                                testRunner.continueAfterTwoEvents, false);
-      try {
-        gBrowser.contentDocument.getElementById("postForm").submit();
-        yield;
-      }
-      finally {
-        
-        
-        gBrowser.removeEventListener("DOMContentLoaded",
-                                     testRunner.continueAfterTwoEvents, false);
-      }
-
-      
-      var innerFrame = gBrowser.contentDocument.getElementById("innerFrame");
-
-      
-      gBrowser.addEventListener("DOMContentLoaded",
-                                testRunner.continueTest, false);
-      try {
-        innerFrame.contentDocument.getElementById("postForm").submit();
-        yield;
-      }
-      finally {
-        
-        
-        gBrowser.removeEventListener("DOMContentLoaded",
-                                     testRunner.continueTest, false);
-      }
-
-      
-      var destDir = createTemporarySaveDirectory();
-      try {
-        
-        mockFilePickerSettings.destDir = destDir;
-        mockFilePickerSettings.filterIndex = 1; 
-        callSaveWithMockObjects(function() {
-          var docToSave = innerFrame.contentDocument;
-          
-          
-          internalSave(docToSave.location.href, docToSave, null, null,
-                       docToSave.contentType, false, null, null,
-                       docToSave.referrer ? makeURI(docToSave.referrer) : null,
-                       false, null);
-        });
-
-        
-        var downloadSuccess = yield;
-        if (!downloadSuccess)
-          throw "Unexpected failure, the inner frame couldn't be saved!";
-
-        
-        var fileContents = readShortFile(mockFilePickerResults.selectedFile);
-
-        
-        const searchPattern = "inputfield=outer";
-        ok(fileContents.indexOf(searchPattern) === -1,
-           "The saved inner frame does not contain outer POST data");
-      }
-      finally {
-        
-        destDir.remove(true);
-      }
-    }
-    finally {
-      
-      gBrowser.addTab().linkedBrowser.stop();
-      gBrowser.removeCurrentTab();
-    }
+      ok(true, true);
   }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
 
