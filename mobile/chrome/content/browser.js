f2329343d2666a878644207e1ec9005bde3babdd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:02:31 +0000

    Bug 531071 - bypass the awesomescreen when the browser is being opened for specific page [r=gavin.sharp]
    UltraBlame original commit: f1eb8f851da93ba766bcfd1c2dcccd9d82abf983

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -585,6 +585,11 @@ var Browser = {
 
     this._pluginObserver = new PluginObserver(bv);
     new PreferenceToggle("plugins.enabled", this._pluginObserver);
+
+    
+    let event = document.createEvent("Events");
+    event.initEvent("UIReady", true, false);
+    window.dispatchEvent(event);
   },
 
   shutdown: function() {
