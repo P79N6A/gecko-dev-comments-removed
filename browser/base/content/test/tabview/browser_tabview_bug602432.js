25b1f31cd7422457a43954a18298920281dd9075
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:07:02 +0000

    Bug 644584 - Intermittent chrome://mochitests/content/browser/browser/base/content/test/tabview/browser_tabview_bug602432.js | Stacked group item update (x) > first item (y) [f=raymond, r=ian]
    UltraBlame original commit: 3b690b386210031535984dd956afb6da9e193a01

--- browser/base/content/test/tabview/browser_tabview_bug602432.js
+++ browser/base/content/test/tabview/browser_tabview_bug602432.js
@@ -2,71 +2,44 @@
 
 
 function test() {
-  waitForExplicitFinish();
-  newWindowWithTabView(onTabViewWindowLoaded);
-}
+  let checkUpdateTimes = function (groupItem) {
+    let children = groupItem.getChildren();
+    let earliestUpdateTime = children.shift()._testLastTabUpdateTime;
 
-let contentWindow = null;
-
-function onTabViewWindowLoaded(win) {
-  ok(win.TabView.isVisible(), "Tab View is visible");
-
-  contentWindow = win.TabView.getContentWindow();
-
-  
-  
-  let numTabs = 10;
-  let groupItem = createGroupItemWithBlankTabs(win, 150, 150, 100,
-    numTabs, false);
-
-  
-  ok(groupItem.isStacked(), "Group item is stacked");
-
-  
-  contentWindow.TabItems.pausePainting();
-  let children = groupItem.getChildren();
-  is(children.length, numTabs, "Correct number of tabitems created");
-   
-  let leftToUpdate = numTabs;
-  let testFunc = function(tabItem) {
-    tabItem.removeSubscriber(tabItem, "updated");
-    if (--leftToUpdate>0)
-      return;
-    
-    
-    let earliest = children[0]._lastTabUpdateTime;
-    for (let c=1; c<children.length; ++c)
-      ok(earliest <= children[c]._lastTabUpdateTime,
-        "Stacked group item update ("+children[c]._lastTabUpdateTime+") > first item ("+earliest+")");
-    shutDown(win, groupItem);
-  };
-
-  for (let c=0; c<children.length; ++c) {
-    let tabItem = children[c];
-    tabItem.addSubscriber(tabItem, "updated", testFunc);
-    contentWindow.TabItems.update(tabItem.tab);
+    children.forEach(function (tabItem) {
+      let updateTime = tabItem._testLastTabUpdateTime;
+      ok(earliestUpdateTime <= updateTime, "Stacked group item update (" +
+         updateTime + ") > first item (" + earliestUpdateTime + ")");
+    });
   }
 
-  
-  contentWindow.TabItems.resumePainting();
-}
+  waitForExplicitFinish();
 
-function shutDown(win, groupItem) {
-  
-  let groupItemCount = contentWindow.GroupItems.groupItems.length;
-  closeGroupItem(groupItem, function() {
-    
-    is(contentWindow.GroupItems.groupItems.length, --groupItemCount,
-       "The number of groups is decreased by 1");
-    let onTabViewHidden = function() {
-      win.removeEventListener("tabviewhidden", onTabViewHidden, false);
-      
-      ok(!TabView.isVisible(), "Tab View is hidden");
-      win.close();
-      ok(win.closed, "new window is closed");
-      finish();
-    };
-    win.addEventListener("tabviewhidden", onTabViewHidden, false);
-    win.TabView.toggle();
+  newWindowWithTabView(function (win) {
+    registerCleanupFunction(function () win.close());
+
+    let numTabsToUpdate = 10;
+    let groupItem = createGroupItemWithBlankTabs(win, 150, 150, 100, numTabsToUpdate, false);
+    ok(groupItem.isStacked(), "groupItem is stacked");
+
+    let cw = win.TabView.getContentWindow();
+    cw.TabItems.pausePainting();
+
+    groupItem.getChildren().forEach(function (tabItem) {
+      tabItem.addSubscriber(tabItem, "updated", function () {
+        tabItem.removeSubscriber(tabItem, "updated");
+        tabItem._testLastTabUpdateTime = tabItem._lastTabUpdateTime;
+
+        if (--numTabsToUpdate)
+          return;
+
+        checkUpdateTimes(groupItem);
+        finish();
+      });
+
+      cw.TabItems.update(tabItem.tab);
+    });
+
+    cw.TabItems.resumePainting();
   });
 }
