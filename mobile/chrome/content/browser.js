84db204524867cd162dd31beda889d628e1e8e05
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:02:59 +0000

    Bug 564075 - Url bar gets displaced, with open sidebar, focusing url bar and then dismissing awesome bar [r=mfinkle]
    UltraBlame original commit: fb0b212cb7bfd56f12252d0b1ea441d33d82826b

--- mobile/chrome/content/browser.js
+++ mobile/chrome/content/browser.js
@@ -781,11 +781,19 @@ var Browser = {
     if (this._selectedTab) {
       this._selectedTab.contentScrollOffset = this.getScrollboxPosition(this.contentScrollboxScroller);
       this._selectedTab.pageScrollOffset = this.getScrollboxPosition(this.pageScrollboxScroller);
+
+      
+      if (this._selectedTab.isLoading())
+        BrowserUI.unlockToolbar();
     }
 
     let isFirstTab = this._selectedTab == null;
     let lastTab = this._selectedTab;
     this._selectedTab = tab;
+
+    
+    if (this._selectedTab.isLoading())
+      BrowserUI.lockToolbar();
 
     tab.ensureBrowserExists();
 
