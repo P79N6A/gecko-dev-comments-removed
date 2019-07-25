c7dcfa96ed66b62429b6a0e06c19019e9d490120
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:01:36 +0000

    Bug 496492: Remove unused variable in flushRegion, r=mfinkle, r=combee
    UltraBlame original commit: d74ed0a4188158a9f60a8b283f7fea2c4f455ea5

--- mobile/chrome/content/CanvasBrowser.js
+++ mobile/chrome/content/CanvasBrowser.js
@@ -150,7 +150,6 @@ CanvasBrowser.prototype = {
   flushRegion: function flushRegion(viewingBoundsOnly) {
     let rgn = this._rgnPage;
 
-    let clearRegion = false;
     let drawls = [];
     let updateBounds = null;
     let pixelsInRegion = 0;
@@ -166,8 +165,6 @@ CanvasBrowser.prototype = {
         rect = rect.intersect(this._visibleBounds);
         if (!rect)
           continue;
-      } else {
-        clearRegion = true;
       }
       drawls.push(rect);
 
@@ -183,7 +180,7 @@ CanvasBrowser.prototype = {
     
     
     
-    if (clearRegion)
+    if (!viewingBoundsOnly)
       this.clearRegion();
 
     
@@ -207,7 +204,7 @@ CanvasBrowser.prototype = {
     {
       drawls = [updateBounds];
     }
-    this._drawRects(drawls, !clearRegion);
+    this._drawRects(drawls, viewingBoundsOnly);
   },
 
   _drawRects: function _drawRects(drawls, subtractRects) {
