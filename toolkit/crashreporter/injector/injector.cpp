b009b2f23dce0ee02fa396b5e6f06f99860ffc0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:29:02 +0000

    Bug 769048 part C - Build an injector DLL which can watch for crashes in Flash player processes. This involves building two copies of breakpad: one to live inside of libxul which uses mozalloc and the dynamic CRT, another to link into the injector which links the CRT statically and doesn't use mozalloc. Introduce a new define/makefile variable MOZ_CRASHREPORTER_INJECTOR rather than repeating "#if defined(XP_WIN) && !defined(HAVE_64BIT_OS) in many places. r=ehsan/khuey
    UltraBlame original commit: 141f0a09f4b60253b60bf7ecfbbea54d6a2ac2d2

---
+++ toolkit/crashreporter/injector/injector.cpp
@@ -0,0 +1,40 @@
+
+
+
+
+#include <windows.h>
+
+#include "client/windows/handler/exception_handler.h"
+
+using google_breakpad::ExceptionHandler;
+using std::wstring;
+
+BOOL WINAPI DummyEntryPoint(HINSTANCE instance,
+                            DWORD reason,
+                            void* reserved)
+{
+  __debugbreak();
+
+  return FALSE; 
+}
+
+
+extern "C" BOOL WINAPI _CRT_INIT(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved); 
+
+extern "C"
+__declspec(dllexport) DWORD Start(void* context)
+{
+  
+  
+  _CRT_INIT(NULL, DLL_PROCESS_ATTACH, NULL);
+
+  HANDLE hCrashPipe = reinterpret_cast<HANDLE>(context);
+
+  ExceptionHandler* e = new (std::nothrow)
+    ExceptionHandler(wstring(), NULL, NULL, NULL,
+                     ExceptionHandler::HANDLER_ALL,
+                     MiniDumpNormal, hCrashPipe, NULL);
+  if (e)
+    e->set_handle_debug_exceptions(true);
+  return 1;
+}
