ab947830bfbdb1d7fb635b61aca4ed1f64cdc453
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:08:56 +0000

    Bug 624835 - Exception handler ignores ::DebugBreak exceptions when it really shouldn't, r=ted a=blocker
    UltraBlame original commit: dab2f429fe209cb6dff7c8e1110e9bbad965510b

--- toolkit/crashreporter/nsExceptionHandler.cpp
+++ toolkit/crashreporter/nsExceptionHandler.cpp
@@ -766,6 +766,10 @@ nsresult SetExceptionHandler(nsILocalFile* aXREDirectory,
   if (!gExceptionHandler)
     return NS_ERROR_OUT_OF_MEMORY;
 
+#ifdef XP_WIN
+  gExceptionHandler->set_handle_debug_exceptions(true);
+#endif
+
   
   char timeString[32];
   time_t startupTime = time(NULL);
@@ -1864,6 +1868,9 @@ SetRemoteExceptionHandler(const nsACString& crashPipe)
                      MiniDumpNormal,
                      NS_ConvertASCIItoUTF16(crashPipe).BeginReading(),
                      NULL);
+#ifdef XP_WIN
+  gExceptionHandler->set_handle_debug_exceptions(true);
+#endif
 
   
   return gExceptionHandler->IsOutOfProcess();
