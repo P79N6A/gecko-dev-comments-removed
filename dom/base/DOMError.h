29a1fffb347eaccf7e7b753293fcbafd90ba0839
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:45:17 +0000

    Bug 705640 - 'Implement DOMError as defined in DOM 4'. r=mounir.
    UltraBlame original commit: 143f01714f2b68424d930a6619c61c62a61381a9

---
+++ dom/base/DOMError.h
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_domerror_h__
+#define mozilla_dom_domerror_h__
+
+#include "nsIDOMDOMError.h"
+
+#include "nsCOMPtr.h"
+#include "nsStringGlue.h"
+
+namespace mozilla {
+namespace dom {
+
+class DOMError : public nsIDOMDOMError
+{
+  nsString mName;
+
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIDOMDOMERROR
+
+  static already_AddRefed<nsIDOMDOMError>
+  CreateForDOMExceptionCode(PRUint16 aDOMExceptionCode);
+
+  static already_AddRefed<nsIDOMDOMError>
+  CreateWithName(const nsAString& aName)
+  {
+    nsCOMPtr<nsIDOMDOMError> error = new DOMError(aName);
+    return error.forget();
+  }
+
+protected:
+  DOMError(const nsAString& aName)
+  : mName(aName)
+  { }
+
+  virtual ~DOMError()
+  { }
+};
+
+} 
+} 
+
+#endif 
