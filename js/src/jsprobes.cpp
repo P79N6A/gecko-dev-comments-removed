bb05cf93d7f8aa0ed729d9ceaee657690aa79f17
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:29:10 +0000

    Bug 584175 - Unify various JS probes into a single set of static probe points. r=gal
    Part 1: Change dtrace-specific names to probes
    Part 2: Stop using the frame pointer for probes.
    Part 3: Update the set of available probes
    
    r=gal@uci.edu
    a=NPOTB
    
    UltraBlame original commit: bb3730430112d22c71faf56d97d82b1d2bbac928

---
+++ js/src/jsprobes.cpp
@@ -0,0 +1,166 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "jsapi.h"
+#include "jsutil.h"
+#include "jsatom.h"
+#include "jscntxt.h"
+#include "jsdbgapi.h"
+#include "jsfun.h"
+#include "jsinterp.h"
+#include "jsobj.h"
+#include "jsscript.h"
+#include "jsstr.h"
+
+#include "jsprobes.h"
+#include <sys/types.h>
+
+#define TYPEOF(cx,v)    (JSVAL_IS_NULL(v) ? JSTYPE_NULL : JS_TypeOfValue(cx,v))
+
+using namespace js;
+
+const char Probes::nullName[] = "(null)";
+
+const char *
+Probes::FunctionClassname(const JSFunction *fun)
+{
+    return (fun && !FUN_INTERPRETED(fun) && !(fun->flags & JSFUN_TRCINFO) && FUN_CLASP(fun))
+           ? (char *)FUN_CLASP(fun)->name
+           : nullName;
+}
+
+const char *
+Probes::ScriptFilename(JSScript *script)
+{
+    return (script && script->filename) ? (char *)script->filename : nullName;
+}
+
+int
+Probes::FunctionLineNumber(JSContext *cx, const JSFunction *fun)
+{
+    if (fun && FUN_INTERPRETED(fun))
+        return (int) JS_GetScriptBaseLineNumber(cx, FUN_SCRIPT(fun));
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static void *
+jsprobes_jsvaltovoid(JSContext *cx, const js::Value &argval)
+{
+    if (argval.isNull())
+        return (void *)JS_TYPE_STR(JSTYPE_NULL);
+
+    if (argval.isUndefined())
+        return (void *)JS_TYPE_STR(JSTYPE_VOID);
+
+    if (argval.isBoolean())
+        return (void *)argval.toBoolean();
+
+    if (argval.isString())
+        return (void *)js_GetStringBytes(cx, argval.toString());
+
+    if (argval.isNumber()) {
+        if (argval.isInt32())
+            return (void *)argval.toInt32();
+        
+        
+    }
+
+    return argval.asGCThing();
+}
+
+const char *
+Probes::FunctionName(JSContext *cx, const JSFunction *fun)
+{
+    if (!fun)
+        return nullName;
+
+    JSAtom *atom = fun->atom;
+    if (!atom) {
+        
+
+
+
+
+        return nullName;
+    }
+
+    char *name = (char *)js_GetStringBytes(cx, ATOM_TO_STRING(atom));
+    return name ? name : nullName;
+}
+
+#ifdef INCLUDE_MOZILLA_DTRACE
+
+
+
+
+
+
+
+void
+Probes::enterJSFunImpl(JSContext *cx, const JSFunction *fun)
+{
+    JAVASCRIPT_FUNCTION_ENTRY(ScriptFilename(FUN_SCRIPT(fun)), FunctionClassname(fun),
+                              FunctionName(cx, fun));
+}
+
+void
+Probes::handleFunctionReturn(JSContext *cx, JSFunction *fun)
+{
+    JAVASCRIPT_FUNCTION_RETURN(ScriptFilename(FUN_SCRIPT(fun)), FunctionClassname(fun),
+                               FunctionName(cx, fun));
+}
+
+#endif
---
+++ js/src/jsprobes.h
@@ -0,0 +1,221 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef INCLUDE_MOZILLA_DTRACE
+#include "javascript-trace.h"
+#endif
+#include "jspubtd.h"
+#include "jsprvtd.h"
+
+#ifndef _JSPROBES_H
+#define _JSPROBES_H
+
+namespace js {
+
+class Probes {
+    static const char nullName[];
+
+    static const char *FunctionClassname(const JSFunction *fun);
+    static const char *ScriptFilename(JSScript *script);
+    static int FunctionLineNumber(JSContext *cx, const JSFunction *fun);
+    static const char *FunctionName(JSContext *cx, const JSFunction *fun);
+
+    static void enterJSFunImpl(JSContext *cx, const JSFunction *fun);
+    static void handleFunctionReturn(JSContext *cx, JSFunction *fun);
+    static void finalizeObjectImpl(JSObject *obj);
+  public:
+    
+
+
+
+    static void enterJSFun(JSContext *cx, JSFunction *fun, js::Value *lval = NULL);
+    static void exitJSFun(JSContext *cx, JSFunction *fun, js::Value *lval = NULL);
+
+    static void startExecution(JSContext *cx, JSScript *script);
+    static void stopExecution(JSContext *cx, JSScript *script);
+
+    static void resizeHeap(JSCompartment *compartment, size_t oldSize, size_t newSize);
+
+    
+    static void createObject(JSContext *cx, JSObject *obj);
+
+    static void resizeObject(JSContext *cx, JSObject *obj, size_t oldSize, size_t newSize);
+
+    
+    static void finalizeObject(JSObject *obj);
+
+    
+
+
+
+
+
+
+
+    static void createString(JSContext *cx, JSString *string, size_t length);
+
+    
+
+
+    static void finalizeString(JSString *string);
+
+    static void compileScriptBegin(JSContext *cx, const char *filename, int lineno);
+    static void compileScriptEnd(JSContext *cx, JSScript *script, const char *filename, int lineno);
+
+    static void calloutBegin(JSContext *cx, JSFunction *fun);
+    static void calloutEnd(JSContext *cx, JSFunction *fun);
+
+    static void acquireMemory(JSContext *cx, void *address, size_t nbytes);
+    static void releaseMemory(JSContext *cx, void *address, size_t nbytes);
+
+    static void GCStart(JSCompartment *compartment);
+    static void GCEnd(JSCompartment *compartment);
+    static void GCStartMarkPhase(JSCompartment *compartment);
+
+    static void GCEndMarkPhase(JSCompartment *compartment);
+    static void GCStartSweepPhase(JSCompartment *compartment);
+    static void GCEndSweepPhase(JSCompartment *compartment);
+
+    static JSBool CustomMark(JSString *string);
+    static JSBool CustomMark(const char *string);
+    static JSBool CustomMark(int marker);
+};
+
+inline void
+Probes::enterJSFun(JSContext *cx, JSFunction *fun, js::Value *lval)
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (!lval || IsFunctionObject(*lval)) {
+        if (JAVASCRIPT_FUNCTION_ENTRY_ENABLED())
+            enterJSFunImpl(cx, fun);
+    }
+#endif
+#ifdef MOZ_TRACE_JSCALLS
+    cx->doFunctionCallback(fun, fun ? FUN_SCRIPT(fun) : NULL, true);
+#endif
+}
+
+inline void
+Probes::exitJSFun(JSContext *cx, JSFunction *fun, js::Value *lval)
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (!lval || IsFunctionObject(*lval)) {
+        if (JAVASCRIPT_FUNCTION_RETURN_ENABLED())
+            handleFunctionReturn(cx, fun);
+    }
+#endif
+#ifdef MOZ_TRACE_JSCALLS
+    cx->doFunctionCallback(fun, fun ? FUN_SCRIPT(fun) : NULL, false);
+#endif
+}
+
+inline void
+Probes::createObject(JSContext *cx, JSObject *obj)
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (JAVASCRIPT_OBJECT_CREATE_ENABLED()) {
+        Class *clasp = obj->getClass();
+        JAVASCRIPT_OBJECT_CREATE((char *)clasp->name, (uintptr_t)obj);
+    }
+#endif
+}
+
+inline void
+Probes::finalizeObject(JSObject *obj)
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (JAVASCRIPT_OBJECT_FINALIZE_ENABLED()) {
+        Class *clasp = obj->getClass();
+
+        
+        JAVASCRIPT_OBJECT_FINALIZE(NULL, (char *)clasp->name, (uintptr_t)obj);
+    }
+#endif
+}
+
+inline void
+Probes::startExecution(JSContext *cx, JSScript *script)
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (JAVASCRIPT_EXECUTE_START_ENABLED())
+        JAVASCRIPT_EXECUTE_START(script->filename ? (char *)script->filename : nullName,
+                                 script->lineno);
+#endif
+#ifdef MOZ_TRACE_JSCALLS
+    cx->doFunctionCallback(NULL, script, true);
+#endif
+}
+
+inline void
+Probes::stopExecution(JSContext *cx, JSScript *script)
+{
+#ifdef INCLUDE_MOZILLA_DTRACE
+    if (JAVASCRIPT_EXECUTE_DONE_ENABLED())
+        JAVASCRIPT_EXECUTE_DONE(script->filename ? (char *)script->filename : nullName,
+                                script->lineno);
+#endif
+#ifdef MOZ_TRACE_JSCALLS
+    cx->doFunctionCallback(NULL, script, false);
+#endif
+}
+
+
+
+
+
+
+inline void Probes::resizeHeap(JSCompartment *compartment, size_t oldSize, size_t newSize) {}
+inline void Probes::resizeObject(JSContext *cx, JSObject *obj, size_t oldSize, size_t newSize) {}
+inline void Probes::createString(JSContext *cx, JSString *string, size_t length) {}
+inline void Probes::finalizeString(JSString *string) {}
+inline void Probes::compileScriptBegin(JSContext *cx, const char *filename, int lineno) {}
+inline void Probes::compileScriptEnd(JSContext *cx, JSScript *script, const char *filename, int lineno) {}
+inline void Probes::calloutBegin(JSContext *cx, JSFunction *fun) {}
+inline void Probes::calloutEnd(JSContext *cx, JSFunction *fun) {}
+inline void Probes::acquireMemory(JSContext *cx, void *address, size_t nbytes) {}
+inline void Probes::releaseMemory(JSContext *cx, void *address, size_t nbytes) {}
+inline void Probes::GCStart(JSCompartment *compartment) {}
+inline void Probes::GCEnd(JSCompartment *compartment) {}
+inline void Probes::GCStartMarkPhase(JSCompartment *compartment) {}
+inline void Probes::GCEndMarkPhase(JSCompartment *compartment) {}
+inline void Probes::GCStartSweepPhase(JSCompartment *compartment) {}
+inline void Probes::GCEndSweepPhase(JSCompartment *compartment) {}
+inline JSBool Probes::CustomMark(JSString *string) { return JS_TRUE; }
+inline JSBool Probes::CustomMark(const char *string) { return JS_TRUE; }
+inline JSBool Probes::CustomMark(int marker) { return JS_TRUE; }
+
+} 
+    
+#endif 
