0615014da2785001601c2e04dbc4258080384302
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:08:03 +0000

    bug 606574 - Cocoa event loop instrumentation. r=smichaud,cjones
    UltraBlame original commit: 855e5cd3c88452721d9e2eb211745e849a3f6e95

---
+++ widget/src/cocoa/CustomCocoaEvents.h
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef WIDGET_COCOA_CUSTOMCOCOAEVENTS_H_
+#define WIDGET_COCOA_CUSTOMCOCOAEVENTS_H_
+
+
+const short kEventSubtypeNone = 0;
+
+const short kEventSubtypeTrace = 1;
+
+#endif 
