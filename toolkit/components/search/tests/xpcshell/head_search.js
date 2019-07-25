14e46e1bbae69a6e7cd1abf31da839f0359f257c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:53:44 +0000

    Back out fae3603a8d1d (bug 699856) for test hangs on Windows
    UltraBlame original commit: 66426f4a189713c57ec1c61b463f4b0a63ce019b

--- toolkit/components/search/tests/xpcshell/head_search.js
+++ toolkit/components/search/tests/xpcshell/head_search.js
@@ -1,9 +1,36 @@
 
 
 
-Components.utils.import("resource://gre/modules/Services.jsm");
-Components.utils.import("resource://gre/modules/NetUtil.jsm");
-Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const XULAPPINFO_CONTRACTID = "@mozilla.org/xre/app-info;1";
@@ -62,50 +89,4 @@ function createAppInfo(id, name, version, platformVersion)
 
 
 var gProfD = do_get_profile();
-
-function dumpn(text)
-{
-  dump(text+"\n");
-}
-
-
-
-
-function removeMetadata()
-{
-  let file = gProfD.clone();
-  file.append("search-metadata.json");
-  if (file.exists()) {
-    file.remove(false);
-  }
-
-  file = gProfD.clone();
-  file.append("search.sqlite");
-  if (file.exists()) {
-    file.remove(false);
-  }
-}
-
-
-
-
-function afterCommit(callback)
-{
-  let obs = function(result, topic, verb) {
-    if (verb == "write-metadata-to-disk-complete") {
-      callback(result);
-    } else {
-      dump("TOPIC: " + topic+ "\n");
-    }
-  }
-  Services.obs.addObserver(obs, "browser-search-service", false);
-}
-
-function  parseJsonFromStream(aInputStream) {
-  const json = Cc["@mozilla.org/dom/json;1"].createInstance(Components.interfaces.nsIJSON);
-  const data = json.decodeFromStream(aInputStream, aInputStream.available());
-  return data;
-}
-
-Services.prefs.setBoolPref("browser.search.log", true);
 
