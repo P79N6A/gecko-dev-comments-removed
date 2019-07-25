0fe11101eb75908ec12f5adb8df35e348d8a551b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:08:42 +0000

    Bug 692632 - 'IndexedDB: support empty objectstore/index names'. r=sicking.
    UltraBlame original commit: f4041047f76f0e244922f5af182589921722de76

--- dom/indexedDB/IDBDatabase.cpp
+++ dom/indexedDB/IDBDatabase.cpp
@@ -475,11 +475,6 @@ IDBDatabase::CreateObjectStore(const nsAString& aName,
 {
   NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
 
-  if (aName.IsEmpty()) {
-    
-    return NS_ERROR_DOM_INDEXEDDB_NON_TRANSIENT_ERR;
-  }
-
   IDBTransaction* transaction = AsyncConnectionHelper::GetCurrentTransaction();
 
   if (!transaction ||
