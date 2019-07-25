ee75226e9327ba1e3f27bc485e341c422ba69cea
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:22:52 +0000

    Bug 757261: Apps in the Cloud Manager and Service; r=mconnor
    UltraBlame original commit: 3b82c67c4e123041ba2a5fbc27e774e54fa3e002

---
+++ services/aitc/Aitc.js
@@ -0,0 +1,121 @@
+
+
+
+
+"use strict";
+
+const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/PlacesUtils.jsm");
+
+Cu.import("resource://services-common/utils.js");
+
+function AitcService() {
+  this.aitc = null;
+  this.wrappedJSObject = this;
+}
+AitcService.prototype = {
+  classID: Components.ID("{a3d387ca-fd26-44ca-93be-adb5fda5a78d}"),
+
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
+                                         Ci.nsINavHistoryObserver,
+                                         Ci.nsISupportsWeakReference]),
+
+  observe: function observe(subject, topic, data) {
+    switch (topic) {
+      case "app-startup":
+        
+        
+        Services.obs.addObserver(this, "sessionstore-windows-restored", true);
+        break;
+      case "sessionstore-windows-restored":
+        Services.obs.removeObserver(this, "sessionstore-windows-restored");
+
+        
+        Cu.import("resource://services-common/preferences.js");
+        if (Preferences.get("services.sync.engine.apps", false)) {
+          return;
+        }
+        
+        if (!Preferences.get("services.aitc.enabled", false)) {
+          return;
+        }
+
+        
+        
+        
+        
+
+        if (Preferences.get("apps.enabled", false)) {
+          this.start();
+          return;
+        }
+
+        
+        this.DASHBOARD_URL = CommonUtils.makeURI(
+          Preferences.get("services.aitc.dashboard.url")
+        );
+        this.MARKETPLACE_URL = CommonUtils.makeURI(
+          Preferences.get("services.aitc.marketplace.url")
+        );
+
+        if (this.hasUsedApps()) {
+          Preferences.set("apps.enabled", true);
+          this.start();
+          return;
+        }
+
+        
+        PlacesUtils.history.addObserver(this, true);
+        break;
+    }
+  },
+
+  start: function start() {
+    Cu.import("resource://services-aitc/main.js");
+    if (!this.aitc) {
+      this.aitc = new Aitc();
+    }
+  },
+
+  hasUsedApps: function hasUsedApps() {
+    
+    
+    
+    let gh = PlacesUtils.ghistory2;
+    if (gh.isVisited(this.DASHBOARD_URL)) {
+      return true;
+    }
+    if (gh.isVisited(this.MARKETPLACE_URL)) {
+      return true;
+    }
+    return false;
+  },
+
+  
+  onBeforeDeleteURI: function() {},
+  onBeginUpdateBatch: function() {},
+  onClearHistory: function() {},
+  onDeleteURI: function() {},
+  onDeleteVisits: function() {},
+  onEndUpdateBatch: function() {},
+  onPageChanged: function() {},
+  onPageExpired: function() {},
+  onTitleChanged: function() {},
+
+  onVisit: function onVisit(uri) {
+    if (!uri.equals(this.MARKETPLACE_URL) && !uri.equals(this.DASHBOARD_URL)) {
+      return;
+    }
+
+    PlacesUtils.history.removeObserver(this);
+    Preferences.set("apps.enabled", true);
+    this.start();
+    return;
+  },
+};
+
+const components = [AitcService];
+const NSGetFactory = XPCOMUtils.generateNSGetFactory(components);
--- services/aitc/services-aitc.js
+++ services/aitc/services-aitc.js
@@ -2,8 +2,22 @@ pref("services.aitc.browserid.url", "https://browserid.org/sign_in");
 pref("services.aitc.browserid.log.level", "Debug");
 
 pref("services.aitc.client.log.level", "Debug");
-pref("services.aitc.storage.log.level", "Debug");
+pref("services.aitc.client.timeout", 120); 
 
-pref("services.aitc.client.timeout", 120);
+pref("services.aitc.dashboard.url", "https://myapps.mozillalabs.com");
+
+pref("services.aitc.main.idleTime", 120000); 
+
+pref("services.aitc.manager.putFreq", 10000); 
+pref("services.aitc.manager.getActiveFreq", 120000); 
+pref("services.aitc.manager.getPassiveFreq", 7200000); 
+pref("services.aitc.manager.log.level", "Debug");
+
+pref("services.aitc.marketplace.url", "https://marketplace.mozilla.org");
+
+pref("services.aitc.service.log.level", "Debug");
+
+
+pref("services.aitc.tokenServer.url", "https://stage-token.services.mozilla.com");
 
 pref("services.aitc.storage.log.level", "Debug");
