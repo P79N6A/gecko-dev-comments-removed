20386693ee2f34bf8361f15c3e2e22c615956a89
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:25:43 +0000

    Bug 761890; build Skia on Windows. r=gw280
    UltraBlame original commit: a24de192b216ea4c1688cb5f8434cc42407f828f

---
+++ gfx/skia/src/sfnt/SkOTUtils.h
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+#ifndef SkOTUtils_DEFINED
+#define SkOTUtils_DEFINED
+
+#include "SkOTTableTypes.h"
+class SkStream;
+
+struct SkOTUtils {
+    
+
+
+    static uint32_t CalcTableChecksum(SK_OT_ULONG *data, size_t length);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    static SkData* RenameFont(SkStream* fontData,
+                              const char* fontName, int fontNameLen);
+};
+
+#endif
