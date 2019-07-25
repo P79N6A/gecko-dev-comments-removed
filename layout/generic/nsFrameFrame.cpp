ddedb4230f7a686bd9ad5c8acb3289bec70f7c06
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:36:35 +0000

    Merged with mozilla-central: 00186bbb7459
    UltraBlame original commit: 242f8b863433d4330fe20a1ec0cf468483c92378

--- layout/generic/nsFrameFrame.cpp
+++ layout/generic/nsFrameFrame.cpp
@@ -297,11 +297,6 @@ nsSubDocumentFrame::Init(nsIContent*     aContent,
   }
   nsIView* view = GetView();
 
-  if (aParent->GetStyleDisplay()->mDisplay == NS_STYLE_DISPLAY_DECK
-      && !view->HasWidget()) {
-    view->CreateWidget();
-  }
-
   
   
   
