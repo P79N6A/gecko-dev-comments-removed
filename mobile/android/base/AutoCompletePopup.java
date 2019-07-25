0835c1940dbe24c5b6072d6541eb45baf71e1fec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:26:10 +0000

    backout 243520b7d8c3 because it broke the autocomplete popup text
    UltraBlame original commit: 94a8078a745a2d47d08841f866bc1f0626c463c6

--- mobile/android/base/AutoCompletePopup.java
+++ mobile/android/base/AutoCompletePopup.java
@@ -68,8 +68,6 @@ public class AutoCompletePopup extends ListView {
 
     private static final String LOGTAG = "AutoCompletePopup";
 
-    private static int sMinWidth = 0;
-
     public AutoCompletePopup(Context context, AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
@@ -134,18 +132,7 @@ public class AutoCompletePopup extends ListView {
         
         
         if ((left + width) < viewport.width) 
-            listWidth = left < 0 ? left + width : width;
-
-        
-        if (sMinWidth == 0)
-            sMinWidth = 100 * GeckoAppShell.getDpi();
-
-        if (listWidth < sMinWidth) {
-            listWidth = sMinWidth;
-
-            if ((listLeft + listWidth) > viewport.width)
-                listLeft = (int)viewport.width - listWidth;
-        }
+            listWidth = left + width;
 
         
         
