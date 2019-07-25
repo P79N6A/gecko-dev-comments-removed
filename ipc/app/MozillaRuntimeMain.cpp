47d5ffd481c4066aa42b2602b75a4ff4450dab7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:58:20 +0000

    Backed out changeset 52388a9a6337, bug 592951. a=me
    UltraBlame original commit: 9e09716ddaf73e57dbd0d1a50492b0404275f67e

--- ipc/app/MozillaRuntimeMain.cpp
+++ ipc/app/MozillaRuntimeMain.cpp
@@ -54,17 +54,37 @@
 int
 main(int argc, char* argv[])
 {
+#if defined(MOZ_CRASHREPORTER)
+    if (argc < 2)
+        return 1;
+    const char* const crashReporterArg = argv[--argc];
+
+#  if defined(XP_WIN) || defined(XP_MACOSX)
+    
+    
+    
+    if (0 != strcmp("-", crashReporterArg)
+        && !XRE_SetRemoteExceptionHandler(crashReporterArg))
+        return 1;
+#  elif defined(OS_LINUX)
+    
+    
+    if (0 != strcmp("false", crashReporterArg)
+        && !XRE_SetRemoteExceptionHandler(NULL))
+        return 1;
+#  else
+#    error "OOP crash reporting unsupported on this platform"
+#  endif   
+#endif 
+
 #if defined(XP_WIN) && defined(DEBUG_bent)
     MessageBox(NULL, L"Hi", L"Hi", MB_OK);
 #endif
 
-    
-    
-    if (argc < 1)
-      return 1;
-    GeckoProcessType proctype = XRE_StringToChildProcessType(argv[--argc]);
+    GeckoProcessType proctype =
+        XRE_StringToChildProcessType(argv[argc - 1]);
 
-    nsresult rv = XRE_InitChildProcess(argc, argv, proctype);
+    nsresult rv = XRE_InitChildProcess(argc - 1, argv, proctype);
     NS_ENSURE_SUCCESS(rv, 1);
 
     return 0;
