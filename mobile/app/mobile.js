099b9695ac1ee3ad82e453e34a2d47aa94a335e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:03:51 +0000

    Bug 602868 - Back out changeset 9cb5e452f73c
    UltraBlame original commit: 86382dea0abba628324fe9c0340ebfe6e77d9c7f

--- mobile/app/mobile.js
+++ mobile/app/mobile.js
@@ -64,6 +64,9 @@ pref("zoom.dpiScale", 150);
 pref("ui.use_native_popup_windows", true);
 
 
+pref("ui.click_hold_context_menus", true);
+
+
 pref("browser.cache.disk.enable", false);
 pref("browser.cache.disk.capacity", 0); 
 
