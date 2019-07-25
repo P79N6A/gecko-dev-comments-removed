14b6555a0058cfc982a63cb62eca600b072d8407
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:43:32 +0000

    Bug 595277 - Part 2: Add parameter to specify requiring the permanent LayerManager. r=roc
    UltraBlame original commit: eeeb6674cffc2b739f6c2911477007cb9ce88805

--- widget/src/xpwidgets/PuppetWidget.h
+++ widget/src/xpwidgets/PuppetWidget.h
@@ -164,7 +164,8 @@ public:
   
 
 
-  virtual LayerManager*     GetLayerManager(bool* aAllowRetaining = nsnull);
+  virtual LayerManager*     GetLayerManager(LayerManagerPersistence aPersistence = LAYER_MANAGER_CURRENT,
+                                            bool* aAllowRetaining = nsnull);
 
   virtual gfxASurface*      GetThebesSurface();
 
