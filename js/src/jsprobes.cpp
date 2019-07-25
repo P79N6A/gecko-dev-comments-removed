8aba08c2ce2b144bffb0be79839e883655fcd205
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:30:24 +0000

    Bug 588537 - Implement ETW (Event Tracing for Windows) probes (r=gal)
    This also adds Probes::pauseProfilers and Probes::resumeProfilers to pause/resume whatever profilers happen to be compiled in. (They all still have their own idiosyncratic ways to start and stop; this is only for suspending and resuming.)
    
    UltraBlame original commit: 5562d165fe2bc952acaae8281b5a341ec7e165a8

--- js/src/jsprobes.cpp
+++ js/src/jsprobes.cpp
@@ -42,6 +42,7 @@
 #include "jsinterp.h"
 #include "jsobj.h"
 #include "jsscript.h"
+#include "jsstaticcheck.h"
 #include "jsstr.h"
 
 #ifdef __APPLE__
@@ -56,6 +57,63 @@
 using namespace js;
 
 const char Probes::nullName[] = "(null)";
+const char Probes::anonymousName[] = "(anonymous)";
+
+bool Probes::ProfilingActive = true;
+
+bool
+Probes::controlProfilers(JSContext *cx, bool toState)
+{
+    JSBool ok = JS_TRUE;
+#if defined(MOZ_SHARK) || defined(MOZ_CALLGRIND) || defined(MOZ_VTUNE)
+    jsval dummy;
+#endif
+
+    if (! ProfilingActive && toState) {
+#ifdef MOZ_SHARK
+        if (! js_StartShark(cx, 0, &dummy))
+            ok = JS_FALSE;
+#endif
+#ifdef MOZ_CALLGRIND
+        if (! js_StartCallgrind(cx, 0, &dummy))
+            ok = JS_FALSE;
+#endif
+#ifdef MOZ_VTUNE
+        if (! js_ResumeVtune(cx, 0, &dummy))
+            ok = JS_FALSE;
+#endif
+    } else if (ProfilingActive && ! toState) {
+#ifdef MOZ_SHARK
+        if (! js_StopShark(cx, 0, &dummy))
+            ok = JS_FALSE;
+#endif
+#ifdef MOZ_CALLGRIND
+        if (! js_StopCallgrind(cx, 0, &dummy))
+            ok = JS_FALSE;
+#endif
+#ifdef MOZ_VTUNE
+        if (! js_PauseVtune(cx, 0, &dummy))
+            ok = JS_FALSE;
+#endif
+    }
+
+    ProfilingActive = toState;
+
+    return ok;
+}
+
+void
+Probes::current_location(JSContext *cx, int* lineno, char const **filename)
+{
+    JSScript *script = js_GetCurrentScript(cx);
+    if (! script) {
+        *lineno = -1;
+        *filename = "(uninitialized)";
+        return;
+    }
+    *lineno = js_PCToLineNumber(cx, script, js_GetCurrentBytecodePC(cx));
+    *filename = ScriptFilename(script);
+}
 
 const char *
 Probes::FunctionClassname(const JSFunction *fun)
@@ -78,69 +136,6 @@ Probes::FunctionLineNumber(JSContext *cx, const JSFunction *fun)
         return (int) JS_GetScriptBaseLineNumber(cx, FUN_SCRIPT(fun));
 
     return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if 0
-static void *
-jsprobes_jsvaltovoid(JSContext *cx, const js::Value &argval)
-{
-    if (argval.isNull())
-        return (void *)JS_TYPE_STR(JSTYPE_NULL);
-
-    if (argval.isUndefined())
-        return (void *)JS_TYPE_STR(JSTYPE_VOID);
-
-    if (argval.isBoolean())
-        return (void *)argval.toBoolean();
-
-    if (argval.isNumber()) {
-        if (argval.isInt32())
-            return (void *)argval.toInt32();
-        
-        
-    }
-
-    return argval.toGCThing();
-}
-#endif
-
-const char *
-Probes::FunctionName(JSContext *cx, const JSFunction *fun, JSAutoByteString *bytes)
-{
-    if (!fun)
-        return nullName;
-
-    JSAtom *atom = fun->atom;
-    if (!atom) {
-        
-
-
-
-
-        return nullName;
-    }
-
-    return bytes->encode(cx, atom) ? bytes->ptr() : nullName;
 }
 
 #ifdef INCLUDE_MOZILLA_DTRACE
