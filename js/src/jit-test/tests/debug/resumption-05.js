9baed5156c7ffdd44c00edb1ee68c7ab1fc05a5f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:37:26 +0000

    Passing test for null resumption.
    UltraBlame original commit: 453701d5c7e7000b2f3ac3f58ce967ddbfe600d2

---
+++ js/src/jit-test/tests/debug/resumption-05.js
@@ -0,0 +1,38 @@
+
+
+
+var g = newGlobal('new-compartment');
+g.debuggeeGlobal = this;
+g.eval("(" + function () { 
+        var dbg = new Debug(debuggeeGlobal);
+        dbg.hooks = {
+            debuggerHandler: function (frame) {
+                if (frame.callee === null) {
+                    
+                    debuggeeGlobal.log += "1";
+                    var cv = frame.eval("f();");
+                    assertEq(cv, null);
+                    debuggeeGlobal.log += "2";
+                } else {
+                    
+                    debuggeeGlobal.log += "3";
+                    assertEq(frame.callee.name, "f");
+                    return null;
+                }
+            }
+        };
+    } + ")()");
+
+var log = "";
+debugger;
+
+function f() {
+    log += "4";
+    try {
+        debugger;  
+    } finally {
+        log += "5";  
+    }
+}
+
+assertEq(log, "1432");
