7589d29f37b3c4b69d0cadc7caebbc32e07d6f75
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:27:32 +0000

    Bug 751377 - Begin to implement [[GetOwnProperty]] for named properties in the new representation.  r=bhackett
    UltraBlame original commit: 53ba44b136f0377caf46cf4d529538d08575bcfb

--- js/src/jsscope.h
+++ js/src/jsscope.h
@@ -780,6 +780,16 @@ struct Shape : public js::gc::Cell
         return (attrs & (JSPROP_SETTER | JSPROP_GETTER)) != 0;
     }
 
+    PropDesc::Writability writability() const {
+        return (attrs & JSPROP_READONLY) ? PropDesc::NonWritable : PropDesc::Writable;
+    }
+    PropDesc::Enumerability enumerability() const {
+        return (attrs & JSPROP_ENUMERATE) ? PropDesc::Enumerable : PropDesc::NonEnumerable;
+    }
+    PropDesc::Configurability configurability() const {
+        return (attrs & JSPROP_PERMANENT) ? PropDesc::NonConfigurable : PropDesc::Configurable;
+    }
+
     
 
 
