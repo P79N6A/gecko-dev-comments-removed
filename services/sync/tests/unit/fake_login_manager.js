0662a30ed705dec4af88da9018982b8a52bc369e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:20:29 +0000

    Separated test_passwords into itself, which contains pure unit tests, and test_password_syncing, which contains a suite that's more along the lines of a system test, though still with plenty of stuff faked-out.  fake_login_manager.js contains code shared between the two suites.
    UltraBlame original commit: 14ceaf5daab13b5d2a9b8c5341e23830938c7e44

---
+++ services/sync/tests/unit/fake_login_manager.js
@@ -0,0 +1,38 @@
+Cu.import("resource://weave/util.js");
+
+
+
+
+
+let fakeSampleLogins = [
+  
+  {hostname: "www.boogle.com",
+   formSubmitURL: "http://www.boogle.com/search",
+   httpRealm: "",
+   username: "",
+   password: "",
+   usernameField: "test_person",
+   passwordField: "test_password"}
+];
+
+
+
+
+
+function FakeLoginManager(fakeLogins) {
+  this.fakeLogins = fakeLogins;
+
+  let self = this;
+
+  Utils.getLoginManager = function fake_getLoginManager() {
+    
+    return {
+      getAllLogins: function() { return self.fakeLogins; },
+      addLogin: function(login) {
+        getTestLogger().info("nsILoginManager.addLogin() called " +
+                             "with hostname '" + login.hostname + "'.");
+        self.fakeLogins.push(login);
+      }
+    };
+  };
+}
