d3994ec8dbf08b98b27e97f42ec6cfaf8a023750
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:56:26 +0000

    (Part 2) Stop PlaceholderLayerClient from extending LayerClient since it has nothing in common with GeckoLayerClient and makes a mockery of OOP
    UltraBlame original commit: cd5baf17f0796b14480fe6ab927dcd182573f6f7

--- mobile/android/base/gfx/LayerController.java
+++ mobile/android/base/gfx/LayerController.java
@@ -40,7 +40,6 @@ package org.mozilla.gecko.gfx;
 
 import org.mozilla.gecko.gfx.IntSize;
 import org.mozilla.gecko.gfx.Layer;
-import org.mozilla.gecko.gfx.LayerClient;
 import org.mozilla.gecko.ui.PanZoomController;
 import org.mozilla.gecko.ui.SimpleScaleGestureDetector;
 import org.mozilla.gecko.GeckoApp;
@@ -87,7 +86,7 @@ public class LayerController {
 
 
     private OnTouchListener mOnTouchListener;       
-    private LayerClient mLayerClient;               
+    private GeckoLayerClient mLayerClient;          
 
     
     private int mCheckerboardColor;
@@ -126,7 +125,7 @@ public class LayerController {
 
     public void setRoot(Layer layer) { mRootLayer = layer; }
 
-    public void setLayerClient(LayerClient layerClient) {
+    public void setLayerClient(GeckoLayerClient layerClient) {
         mLayerClient = layerClient;
         layerClient.setLayerController(this);
     }
