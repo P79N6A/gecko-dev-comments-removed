115789edf9a8fc85bb535b8dae25dfd5926e71d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:00:22 +0000

    Backed out changeset 29529018c1da (bug 734323)
    UltraBlame original commit: 26808dda208c3f837c0f94c87527ca33666a82f3

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -1465,9 +1465,6 @@ function Tab(aURL, aParams) {
   this._zoom = 1.0;
   this.userScrollPos = { x: 0, y: 0 };
   this.contentDocumentIsDisplayed = true;
-  this.clickToPlayPluginDoorhangerShown = false;
-  this.clickToPlayPluginsActivated = false;
-  this.loadEventProcessed = false;
 }
 
 Tab.prototype = {
@@ -1796,7 +1793,6 @@ Tab.prototype = {
       }
 
       case "load": {
-        this.loadEventProcessed = true;
         
         let contentWindow = this.browser.contentWindow;
         let cwu = contentWindow.QueryInterface(Ci.nsIInterfaceRequestor)
@@ -1919,20 +1915,14 @@ Tab.prototype = {
 
       case "PluginClickToPlay": {
         let plugin = aEvent.target;
-
-        if (this.clickToPlayPluginsActivated) {
-          PluginHelper.playPlugin(plugin);
-          return;
-        }
-
-        
-        
         let overlay = plugin.ownerDocument.getAnonymousElementByAttribute(plugin, "class", "mainBox");
-        if (!overlay || PluginHelper.isTooSmall(plugin, overlay)) {
-          if (overlay)
-            overlay.style.visibility = "hidden";
-          if (this.loadEventProcessed && !this.clickToPlayPluginDoorhangerShown)
-            PluginHelper.showDoorHanger(this);
+        if (!overlay)
+          return;
+
+        
+        
+        if (PluginHelper.isTooSmall(plugin, overlay)) {
+          overlay.style.visibility = "hidden";
           return;
         }
 
@@ -1943,10 +1933,7 @@ Tab.prototype = {
               return;
             e.preventDefault();
           }
-          let win = e.target.ownerDocument.defaultView.top;
-          let tab = BrowserApp.getTabForWindow(win);
-          tab.clickToPlayPluginsActivated = true;
-          PluginHelper.playAllPlugins(win);
+          PluginHelper.playAllPlugins(e.target.ownerDocument.defaultView);
         }, true);
         break;
       }
@@ -2006,11 +1993,6 @@ Tab.prototype = {
       documentURI = browser.contentDocument.documentURIObject.spec;
       contentType = browser.contentDocument.contentType;
     }
-
-    
-    this.clickToPlayPluginDoorhangerShown = false;
-    this.clickToPlayPluginsActivated = false;
-    this.loadEventProcessed = false;
 
     let message = {
       gecko: {
@@ -3920,7 +3902,6 @@ var ClipboardHelper = {
 
 var PluginHelper = {
   showDoorHanger: function(aTab) {
-    aTab.clickToPlayPluginDoorhangerShown = true;
     let message = Strings.browser.GetStringFromName("clickToPlayPlugins.message");
     let buttons = [
       {
@@ -3947,13 +3928,11 @@ var PluginHelper = {
     if (!plugins || !plugins.length)
       return;
 
-    plugins.forEach(this.playPlugin);
-  },
-
-  playPlugin: function(plugin) {
-    let objLoadingContent = plugin.QueryInterface(Ci.nsIObjectLoadingContent);
-    if (!objLoadingContent.activated)
-      objLoadingContent.playPlugin();
+    for (let plugin of plugins) {
+      let objLoadingContent = plugin.QueryInterface(Ci.nsIObjectLoadingContent);
+      if (!objLoadingContent.activated)
+        objLoadingContent.playPlugin();
+    }
   },
 
   getPluginPreference: function getPluginPreference() {
