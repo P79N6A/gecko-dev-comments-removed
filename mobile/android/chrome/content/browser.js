92b633cdef08dd6ff5ae0bdaf0764c146bbbd48c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:41:34 +0000

    Bug 784759 - Move database creation to after first page load. r=mfinkle
    UltraBlame original commit: c0bf8f74341984843682b8c0eeb1518deebc540d

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -321,8 +321,18 @@ var BrowserApp = {
 #endif
     }
 
-    if (updated)
-      this.onAppUpdated();
+    if (updated) {
+        
+        
+        let browser = BrowserApp.selectedTab.browser;
+        let updatedFun = function updatedFun() {
+            browser.removeEventListener("DOMContentLoaded", updatedFun, false);
+            
+            Services.obs.notifyObservers(null, "FormHistory:Init", "");
+            Services.obs.notifyObservers(null, "Passwords:Init", "");
+        }
+        browser.addEventListener("DOMContentLoaded", updatedFun, false);
+    }
 
     
     sendMessageToJava({
@@ -473,12 +483,6 @@ var BrowserApp = {
         }
         ContentAreaUtils.internalSave(aTarget.currentURI.spec, null, null, contentDisposition, type, false, "SaveImageTitle", null, aTarget.ownerDocument.documentURIObject, true, null);
       });
-  },
-
-  onAppUpdated: function() {
-    
-    Services.obs.notifyObservers(null, "FormHistory:Init", "");
-    Services.obs.notifyObservers(null, "Passwords:Init", "");
   },
 
   shutdown: function shutdown() {
