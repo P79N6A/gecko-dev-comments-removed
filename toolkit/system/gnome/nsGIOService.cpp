cd2d4f54642c0d4094428f7f0cde63d9787e9767
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:21:02 +0000

    Bug 660131 - Call g_type_init in loadProc of kGnomeModule to fix crash on mochitest-plain ; r=karlt
    UltraBlame original commit: d6a897278194dc9f98d92362f7408d28f78f8cd9

--- toolkit/system/gnome/nsGIOService.cpp
+++ toolkit/system/gnome/nsGIOService.cpp
@@ -299,13 +299,6 @@ nsGIOMimeApp::SetAsDefaultForURIScheme(nsACString const& aURIScheme)
   return NS_OK;
 }
 
-nsresult
-nsGIOService::Init()
-{
-  
-  return NS_OK;
-}
-
 NS_IMPL_ISUPPORTS1(nsGIOService, nsIGIOService)
 
 NS_IMETHODIMP
