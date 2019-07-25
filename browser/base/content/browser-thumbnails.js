3743c2f362ec786ba64ca0024acc9ec8784157d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:02:53 +0000

    Bug 744743 - [Page Thumbnails] Don't capture thumbnails in private browsing mode; r=dao
    UltraBlame original commit: f91fefdfcc51441aa87ac6cbd040ea1b95406faa

--- browser/base/content/browser-thumbnails.js
+++ browser/base/content/browser-thumbnails.js
@@ -98,6 +98,9 @@ let gBrowserThumbnails = {
   },
 
   _shouldCapture: function Thumbnails_shouldCapture(aBrowser) {
+    if (gPrivateBrowsingUI.privateBrowsingEnabled)
+      return false;
+
     let doc = aBrowser.contentDocument;
 
     
