112b3160091ca3d4bf6a0bf915acde5707c0ce05
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 12:00:18 +0000

    Test for getters in the tracer GETELEM IC (bug 633752, r=cdleary).
    UltraBlame original commit: 34c05b9c0079571c3e15bfaede1e6c5b58d5ada3

--- js/src/jstracer.cpp
+++ js/src/jstracer.cpp
@@ -6250,6 +6250,10 @@ IsEntryTypeCompatible(const Value &v, JSValueType type)
 static inline bool
 IsFrameObjPtrTypeCompatible(void *p, JSStackFrame *fp, JSValueType type)
 {
+    debug_only_printf(LC_TMTracer, "%c/%c ", TypeToChar(type),
+                      (p == fp->addressOfScopeChain() || fp->hasArgsObj())
+                      ? TypeToChar(JSVAL_TYPE_NONFUNOBJ)
+                      : TypeToChar(JSVAL_TYPE_NULL));
     if (p == fp->addressOfScopeChain())
         return type == JSVAL_TYPE_NONFUNOBJ;
     JS_ASSERT(p == fp->addressOfArgs());
@@ -12684,7 +12688,7 @@ GetPropertyByName(JSContext* cx, JSObject* obj, JSString** namep, Value* vp, PIC
     }
 
     
-    if (obj == holder && shape->hasSlot()) {
+    if (obj == holder && shape->hasSlot() && shape->hasDefaultGetter()) {
         
 
 
