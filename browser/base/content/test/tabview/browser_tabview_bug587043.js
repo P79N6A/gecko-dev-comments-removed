d5616ed9a7ceb6271f39dbdff85649965d1794cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:53:51 +0000

    Bug 587043 - Closing the tabcandy window closes the browser [r=dietrich,a=block2.0]
    UltraBlame original commit: 6374793c8fb644f8e4a54252e8dc2aa3afa7aff5

---
+++ browser/base/content/test/tabview/browser_tabview_bug587043.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function test() {
+  waitForExplicitFinish();
+
+  window.addEventListener("tabviewshown", onTabViewWindowLoaded, false);
+  TabView.toggle();
+}
+
+function onTabViewWindowLoaded() {
+  window.removeEventListener("tabviewshown", onTabViewWindowLoaded, false);
+
+  ok(TabView.isVisible(), "Tab View is visible");
+
+  let onTabViewHidden = function() {
+    window.removeEventListener("tabviewhidden", onTabViewHidden, false);
+    ok(!TabView.isVisible(), "Tab View is hidden");
+
+    finish();
+  };
+  window.addEventListener("tabviewhidden", onTabViewHidden, false);
+  document.getElementById("cmd_closeWindow").doCommand();
+}
