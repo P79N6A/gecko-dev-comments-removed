0171e62c101af7bf2a0965f24e6917796ccf16f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:34:38 +0000

    imported patch bug_545237_ContentProcessParent_singleton
    UltraBlame original commit: 1be9769caa2b143193ec0862471f4eadcc01a1ee

--- content/base/src/nsFrameLoader.h
+++ content/base/src/nsFrameLoader.h
@@ -189,6 +189,7 @@ private:
   PRPackedBool mRemoteWidgetCreated : 1;
   bool mRemoteFrame;
   
+  nsCOMPtr<nsIObserver> mChildHost;
   mozilla::dom::TabParent* mChildProcess;
 
 #ifdef MOZ_WIDGET_GTK2
