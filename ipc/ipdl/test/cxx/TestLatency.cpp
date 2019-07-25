1e323ac4f4f3bf8b9fb560f8ec6f8535f09674ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:31:10 +0000

    Don't run TestLatency if the timing resolution is too low and use the new TimeDuration::ToSecondsSigDigits() method to display durations to humans. No r=, test only.
    UltraBlame original commit: 9f5c1b7954ee04d6f1e1d2a48dd9db4be31bd4f8

--- ipc/ipdl/test/cxx/TestLatency.cpp
+++ ipc/ipdl/test/cxx/TestLatency.cpp
@@ -16,7 +16,6 @@ TestLatencyParent::TestLatencyParent() :
     mStart(),
     mPPTimeTotal(),
     mPP5TimeTotal(),
-    mRpcTimeTotal(),
     mPPTrialsToGo(NR_TRIALS),
     mPP5TrialsToGo(NR_TRIALS),
     mPongsToGo(0)
@@ -32,15 +31,11 @@ TestLatencyParent::~TestLatencyParent()
 void
 TestLatencyParent::Main()
 {
-    TimeDuration resolution = TimeDuration::Resolution();
-    if (resolution.ToSeconds() > kTimingResolutionCutoff) {
+    if (TimeDuration::Resolution().ToSeconds() > kTimingResolutionCutoff) {
         puts("  (skipping TestLatency, timing resolution is too poor)");
         Close();
         return;
     }
-
-    printf("  timing resolution: %g seconds\n",
-           resolution.ToSecondsSigDigits());
 
     if (mozilla::ipc::LoggingEnabled())
         NS_RUNTIMEABORT("you really don't want to log all IPC messages during this test, trust me");
@@ -52,8 +47,7 @@ void
 TestLatencyParent::PingPongTrial()
 {
     mStart = TimeStamp::Now();
-    if (!SendPing())
-        fail("sending Ping()");
+    SendPing();
 }
 
 void
@@ -63,12 +57,11 @@ TestLatencyParent::Ping5Pong5Trial()
     
     mPongsToGo = 5;
 
-    if (!SendPing5() ||
-        !SendPing5() ||
-        !SendPing5() ||
-        !SendPing5() ||
-        !SendPing5())
-        fail("sending Ping5()");
+    SendPing5();
+    SendPing5();
+    SendPing5();
+    SendPing5();
+    SendPing5();
 }
 
 void
@@ -83,7 +76,7 @@ TestLatencyParent::RecvPong()
     TimeDuration thisTrial = (TimeStamp::Now() - mStart);
     mPPTimeTotal += thisTrial;
 
-    if (0 == (mPPTrialsToGo % 1000))
+    if (0 == ((mPPTrialsToGo % 1000)))
         printf("  PP trial %d: %g\n",
                mPPTrialsToGo, thisTrial.ToSecondsSigDigits());
 
@@ -104,37 +97,18 @@ TestLatencyParent::RecvPong5()
     TimeDuration thisTrial = (TimeStamp::Now() - mStart);
     mPP5TimeTotal += thisTrial;
 
-    if (0 == (mPP5TrialsToGo % 1000))
+    if (0 == ((mPP5TrialsToGo % 1000)))
         printf("  PP5 trial %d: %g\n",
                mPP5TrialsToGo, thisTrial.ToSecondsSigDigits());
 
     if (0 < --mPP5TrialsToGo)
         Ping5Pong5Trial();
     else
-        RpcTrials();
+        Exit();
 
     return true;
 }
 
-void
-TestLatencyParent::RpcTrials()
-{
-    for (int i = 0; i < NR_TRIALS; ++i) {
-        TimeStamp start = TimeStamp::Now();
-
-        if (!CallRpc())
-            fail("can't call Rpc()");
-
-        TimeDuration thisTrial = (TimeStamp::Now() - start);
-
-        if (0 == (i % 1000))
-            printf("  Rpc trial %d: %g\n", i, thisTrial.ToSecondsSigDigits());
-
-        mRpcTimeTotal += thisTrial;
-    }
-
-    Exit();
-}
 
 
 
@@ -163,11 +137,6 @@ TestLatencyChild::RecvPing5()
     return true;
 }
 
-bool
-TestLatencyChild::AnswerRpc()
-{
-    return true;
-}
 
 } 
 } 
