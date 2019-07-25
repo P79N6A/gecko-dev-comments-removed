2598a5f87236416ee4f3beebf2c0bb6236573532
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:03:28 +0000

    Bug 745401 - Fix logic error in draw event coalescing. r=kats
    UltraBlame original commit: fad44f5a127c914601dea55ac2d40b8dd9b0c2f2

--- widget/android/nsAppShell.cpp
+++ widget/android/nsAppShell.cpp
@@ -587,7 +587,7 @@ nsAppShell::PostEvent(AndroidGeckoEvent *ae)
                 delete mQueuedDrawEvent;
             }
 
-            if (mAllowCoalescingNextDraw) {
+            if (!mAllowCoalescingNextDraw) {
                 
                 
                 
