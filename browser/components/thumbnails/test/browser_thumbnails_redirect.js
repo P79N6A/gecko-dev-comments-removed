6dc2e3c631739c1568534f316e56061a666d6e04
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:06:59 +0000

    Backout 080fc3a7cdfe (bug 744388) for xpcshell failures
    UltraBlame original commit: 23caa5d559ae2697b82e5a2102aecb604969e903

--- browser/components/thumbnails/test/browser_thumbnails_redirect.js
+++ browser/components/thumbnails/test/browser_thumbnails_redirect.js
@@ -4,6 +4,9 @@
 const URL = "http://mochi.test:8888/browser/browser/components/thumbnails/" +
             "test/background_red_redirect.sjs";
 
+let cacheService = Cc["@mozilla.org/network/cache-service;1"]
+                   .getService(Ci.nsICacheService);
+
 
 
 
@@ -17,16 +20,32 @@ function runTests() {
   yield captureAndCheckColor(255, 0, 0, "we have a red thumbnail");
 
   
-  yield whenFileExists(URL);
+  yield whenCacheEntryExists(URL);
   yield checkThumbnailColor(URL, 255, 0, 0, "referrer has a red thumbnail");
 }
 
-function whenFileExists(aURL) {
+function whenCacheEntryExists(aKey) {
   let callback = next;
 
-  let file = PageThumbsStorage.getFileForURL(aURL);
-  if (!file.exists())
-    callback = function () whenFileExists(aURL);
+  checkCacheEntryExists(aKey, function (aExists) {
+    if (!aExists)
+      callback = function () whenCacheEntryExists(aKey);
 
-  executeSoon(callback);
+    executeSoon(callback);
+  });
+}
+
+function checkCacheEntryExists(aKey, aCallback) {
+  PageThumbsCache.getReadEntry(aKey, function (aEntry) {
+    let inputStream = aEntry && aEntry.openInputStream(0);
+    let exists = inputStream && inputStream.available();
+
+    if (inputStream)
+      inputStream.close();
+
+    if (aEntry)
+      aEntry.close();
+
+    aCallback(exists);
+  });
 }
