b083c0c0d51dcad46d97aa561f61e3fc5eafaeed
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:53:16 +0000

    Bug 648581 - Annotate the crash reports in case DLL BlockListing fails; r=bsmedberg
    UltraBlame original commit: ab0a6fdf0144831c16d993df1bd271861b7c353c

--- toolkit/xre/nsWindowsDllBlocklist.cpp
+++ toolkit/xre/nsWindowsDllBlocklist.cpp
@@ -52,8 +52,88 @@
 
 #include "nsWindowsDllInterceptor.h"
 
-#define IN_WINDOWS_DLL_BLOCKLIST
-#include "nsWindowsDllBlocklist.h"
+#include "nsExceptionHandler.h"
+
+#define ALL_VERSIONS   ((unsigned long long)-1LL)
+
+
+
+
+
+#define UNVERSIONED    ((unsigned long long)0LL)
+
+
+
+#define MAKE_VERSION(a,b,c,d)\
+  ((a##ULL << 48) + (b##ULL << 32) + (c##ULL << 16) + d##ULL)
+
+struct DllBlockInfo {
+  
+  
+  const char *name;
+
+  
+  
+  
+  
+  
+  
+  
+  
+  unsigned long long maxVersion;
+};
+
+static DllBlockInfo sWindowsDllBlocklist[] = {
+  
+  
+  
+  
+  
+  
+  { "npffaddon.dll", ALL_VERSIONS},
+
+  
+  {"avgrsstx.dll", MAKE_VERSION(8,5,0,401)},
+  
+  
+  {"calc.dll", MAKE_VERSION(1,0,0,1)},
+
+  
+  {"hook.dll", ALL_VERSIONS},
+  
+  
+  
+  {"googledesktopnetwork3.dll", UNVERSIONED},
+
+  
+  {"rdolib.dll", MAKE_VERSION(6,0,88,4)},
+
+  
+  {"fgjk4wvb.dll", MAKE_VERSION(8,8,8,8)},
+  
+  
+  {"radhslib.dll", UNVERSIONED},
+
+  
+  
+  {"vksaver.dll", MAKE_VERSION(2,2,2,0)},
+
+  
+  {"rlxf.dll", MAKE_VERSION(1,2,323,1)},
+
+  
+  
+  {"psicon.dll", ALL_VERSIONS},
+
+  
+  {"accelerator.dll", MAKE_VERSION(3,2,1,6)},
+  
+  
+  { "mozdllblockingtest.dll", ALL_VERSIONS },
+  { "mozdllblockingtest_versioned.dll", 0x0000000400000000ULL },
+
+  { NULL, 0 }
+};
 
 #ifndef STATUS_DLL_NOT_FOUND
 #define STATUS_DLL_NOT_FOUND ((DWORD)0xC0000135L)
@@ -226,4 +306,8 @@ XRE_SetupDllBlocklist()
   if (!ok)
     printf_stderr ("LdrLoadDll hook failed, no dll blocklisting active\n");
 #endif
+
+  if (!ok) {
+    CrashReporter::AppendAppNotesToCrashReport(NS_LITERAL_CSTRING("DllBlockList Failed\n"));
+  }
 }
