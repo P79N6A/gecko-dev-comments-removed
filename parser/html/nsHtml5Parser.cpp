0c79ded7349532638d361b8910083c85698afd11
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:14:49 +0000

    Bug 699356 - Show the URL of the page in the title of the View Source window. r=smaug.
    UltraBlame original commit: 38814e0bafb91e05f847f7e82f5b305b40e1deaa

--- parser/html/nsHtml5Parser.cpp
+++ parser/html/nsHtml5Parser.cpp
@@ -218,7 +218,7 @@ nsHtml5Parser::IsComplete()
 }
 
 NS_IMETHODIMP
-nsHtml5Parser::Parse(nsIURI* aURL, 
+nsHtml5Parser::Parse(nsIURI* aURL,
                      nsIRequestObserver* aObserver,
                      void* aKey,
                      nsDTDMode aMode) 
@@ -232,6 +232,7 @@ nsHtml5Parser::Parse(nsIURI* aURL,
   NS_PRECONDITION(mStreamParser, 
                   "Can't call this Parse() variant on script-created parser");
   mStreamParser->SetObserver(aObserver);
+  mStreamParser->SetViewSourceTitle(aURL); 
   mExecutor->SetStreamParser(mStreamParser);
   mExecutor->SetParser(this);
   mRootContextKey = aKey;
