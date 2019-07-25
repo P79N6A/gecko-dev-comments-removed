1f51a02d5275a09ad9144507a6de68425a5ad448
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:08:32 +0000

    backout merge a=bustage
    UltraBlame original commit: fbe104a6fc9664501997127aa1ebd8339e21dc34

--- browser/base/content/tabview/tabitems.js
+++ browser/base/content/tabview/tabitems.js
@@ -351,7 +351,7 @@ TabItem.prototype = Utils.extend(new Item(), new Subscribable(), {
       if (tabData.groupID) {
         var groupItem = GroupItems.groupItem(tabData.groupID);
         if (groupItem) {
-          groupItem.add(this, null, {immediately: true});
+          groupItem.add(this, {immediately: true});
 
           
           
