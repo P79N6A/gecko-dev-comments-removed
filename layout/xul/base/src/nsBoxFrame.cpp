746fe0d01aeb4422539ec80038c2ceda1baa7273
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:50:37 +0000

    Bug 590468. Part 1: Add support for XUL 'layer' attribute. r=tnikkel sr=vlad
    UltraBlame original commit: 6f2158e6ac2d49d0da2ee7cbb3435a9d503d2f91

--- layout/xul/base/src/nsBoxFrame.cpp
+++ layout/xul/base/src/nsBoxFrame.cpp
@@ -1307,27 +1307,54 @@ nsBoxFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
                              const nsRect&           aDirtyRect,
                              const nsDisplayListSet& aLists)
 {
-  nsresult rv = DisplayBorderBackgroundOutline(aBuilder, aLists);
+  
+  PRBool forceLayer =
+    GetContent()->HasAttr(kNameSpaceID_None, nsGkAtoms::layer) &&
+    GetContent()->IsXUL();
+
+  nsDisplayListCollection tempLists;
+  const nsDisplayListSet& destination = forceLayer ? tempLists : aLists;
+
+  nsresult rv = DisplayBorderBackgroundOutline(aBuilder, destination);
   NS_ENSURE_SUCCESS(rv, rv);
 
 #ifdef DEBUG_LAYOUT
-  
   if (mState & NS_STATE_CURRENTLY_IN_DEBUG) {
-    rv = aLists.BorderBackground()->AppendNewToTop(new (aBuilder)
+    rv = destination.BorderBackground()->AppendNewToTop(new (aBuilder)
         nsDisplayGeneric(aBuilder, this, PaintXULDebugBackground,
                          "XULDebugBackground"));
     NS_ENSURE_SUCCESS(rv, rv);
-    rv = aLists.Outlines()->AppendNewToTop(new (aBuilder)
+    rv = destination.Outlines()->AppendNewToTop(new (aBuilder)
         nsDisplayXULDebug(aBuilder, this));
     NS_ENSURE_SUCCESS(rv, rv);
   }
 #endif
 
-  rv = BuildDisplayListForChildren(aBuilder, aDirtyRect, aLists);
+  rv = BuildDisplayListForChildren(aBuilder, aDirtyRect, destination);
   NS_ENSURE_SUCCESS(rv, rv);
 
   
-  return DisplaySelectionOverlay(aBuilder, aLists);
+  rv = DisplaySelectionOverlay(aBuilder, destination);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  if (forceLayer) {
+    
+    
+    
+    
+    nsDisplayList masterList;
+    masterList.AppendToTop(tempLists.BorderBackground());
+    masterList.AppendToTop(tempLists.BlockBorderBackgrounds());
+    masterList.AppendToTop(tempLists.Floats());
+    masterList.AppendToTop(tempLists.Content());
+    masterList.AppendToTop(tempLists.PositionedDescendants());
+    masterList.AppendToTop(tempLists.Outlines());
+    
+    rv = aLists.Content()->AppendNewToTop(new (aBuilder)
+        nsDisplayOwnLayer(aBuilder, this, &masterList));
+    NS_ENSURE_SUCCESS(rv, rv);
+  }
+  return NS_OK;
 }
 
 NS_IMETHODIMP
