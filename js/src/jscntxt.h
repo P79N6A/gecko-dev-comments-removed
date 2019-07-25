27fc5d15aefd340683d7dbc143b34190f5e051ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:45:30 +0000

    Bug 678998 - Make the QName localName slot store a local name that's been atomized, for simplicity.  r=luke
    UltraBlame original commit: c6a3b64dcac076dbca8ad87ff165be85d6fd3da2

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -535,7 +535,7 @@ struct JSRuntime
     js::Value           negativeInfinityValue;
     js::Value           positiveInfinityValue;
 
-    JSFlatString        *emptyString;
+    JSAtom              *emptyString;
 
     
     JSCList             contextList;
