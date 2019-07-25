e21746bce6be13d52c95f964417e0faf13fe5776
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:58:06 +0000

    Bug 737313: Make nsCSSFrameConstructor::ConstructInline more robust when one of its child frames fails to construct. r=bz
    UltraBlame original commit: a403afe78c47a2391d99bd5346e91f1cd6ef3f36

--- layout/base/nsCSSFrameConstructor.cpp
+++ layout/base/nsCSSFrameConstructor.cpp
@@ -10709,13 +10709,15 @@ nsCSSFrameConstructor::ConstructInline(nsFrameConstructorState& aState,
   nsIContent* const content = aItem.mContent;
   nsStyleContext* const styleContext = aItem.mStyleContext;
 
-  nsIFrame *newFrame;
-
   bool positioned =
     NS_STYLE_DISPLAY_INLINE == aDisplay->mDisplay &&
     (NS_STYLE_POSITION_RELATIVE == aDisplay->mPosition ||
      aDisplay->HasTransform());
-  newFrame = NS_NewInlineFrame(mPresShell, styleContext);
+
+  nsIFrame* newFrame = NS_NewInlineFrame(mPresShell, styleContext);
+  if (!newFrame) {
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
 
   
   InitAndRestoreFrame(aState, content, aParentFrame, nsnull, newFrame);
@@ -10736,6 +10738,10 @@ nsCSSFrameConstructor::ConstructInline(nsFrameConstructorState& aState,
                                             childItems);
   if (NS_FAILED(rv)) {
     
+    
+    
+    newFrame->SetInitialChildList(kPrincipalList, childItems);
+    newFrame->Destroy();
     return rv;
   }
 
