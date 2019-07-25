aba31c1168d56daa3c3e0fcc800f31ac628853e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:49:34 +0000

    Bug 558306 part 1.  Add a TimeDuration::ToMilliseconds.  r=cjones
    UltraBlame original commit: 014d2f87bd51418a1d87880654f290eea1537e96

--- xpcom/ds/TimeStamp.h
+++ xpcom/ds/TimeStamp.h
@@ -74,6 +74,9 @@ public:
   
   
   double ToSecondsSigDigits() const;
+  double ToMilliseconds() const {
+    return ToSeconds() * 1000.0;
+  }
 
   static TimeDuration FromSeconds(PRInt32 aSeconds);
   static TimeDuration FromMilliseconds(PRInt32 aMilliseconds);
