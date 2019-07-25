c94a8adb894a6a524f2eb9dab5c4cc40e63c124f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:16:36 +0000

    Backed out changeset e7436725f170 due to crashes in mochitest and elsewhere. a=backout
    UltraBlame original commit: f322c421f152e777d94ee49de220b45d5d6d2bf3

--- js/src/xpconnect/src/xpcwrappednativescope.cpp
+++ js/src/xpconnect/src/xpcwrappednativescope.cpp
@@ -415,22 +415,17 @@ WrappedNativeSuspecter(JSDHashTable *table, JSDHashEntryHdr *hdr,
     {
         NS_ASSERTION(NS_IsMainThread(), 
                      "Suspecting wrapped natives from non-main thread");
-
-        
-        
-        JSObject* obj = wrapper->GetFlatJSObject();
-        if(!xpc::ParticipatesInCycleCollection(closure->cx, obj))
-            return JS_DHASH_NEXT;
-
-        NS_ASSERTION(!JS_IsAboutToBeFinalized(closure->cx, obj),
+        NS_ASSERTION(!JS_IsAboutToBeFinalized(closure->cx, wrapper->GetFlatJSObject()),
                      "WrappedNativeSuspecter attempting to touch dead object");
 
         
         
-        if(!(closure->cb.WantAllTraces()) && !nsXPConnect::IsGray(obj))
+        if(!(closure->cb.WantAllTraces()) &&
+           !nsXPConnect::IsGray(wrapper->GetFlatJSObject()))
             return JS_DHASH_NEXT;
 
-        closure->cb.NoteRoot(nsIProgrammingLanguage::JAVASCRIPT, obj,
+        closure->cb.NoteRoot(nsIProgrammingLanguage::JAVASCRIPT,
+                             wrapper->GetFlatJSObject(),
                              nsXPConnect::GetXPConnect());
     }
 
