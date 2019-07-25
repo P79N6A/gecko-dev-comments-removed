f94b8795637ac87fb06098a971cdfd4cf91f78ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:22:36 +0000

    Bug 708200 - Do not create timer to fire geckoLoaded. r=blassey a=android-only
    UltraBlame original commit: 475d66e8929d0f7ab0d87125b524695826c21845

--- mobile/android/base/GeckoAppShell.java
+++ mobile/android/base/GeckoAppShell.java
@@ -438,16 +438,11 @@ public class GeckoAppShell
         if (url != null)
             combinedArgs += " -remote " + url;
 
-        
-        new Timer("Gecko Setup").schedule(new TimerTask() {
-            public void run() {
-                GeckoApp.mAppContext.runOnUiThread(new Runnable() {
-                    public void run() {
-                        geckoLoaded();
-                    }
-                });
-            }
-        }, 0);
+        GeckoApp.mAppContext.runOnUiThread(new Runnable() {
+                public void run() {
+                    geckoLoaded();
+                }
+            });
 
         
         GeckoAppShell.nativeRun(combinedArgs);
