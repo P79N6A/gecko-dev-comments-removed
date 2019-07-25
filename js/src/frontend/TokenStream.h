994e1045c1c6fc62d513755b6682291786b70d3e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:02:25 +0000

    Comment out busted assertions. See bug 695922. r=red.
    UltraBlame original commit: 2d649d1cc360cd4308c903a3adaf90ac70cf87b2

--- js/src/frontend/TokenStream.h
+++ js/src/frontend/TokenStream.h
@@ -213,14 +213,16 @@ struct TokenPos {
     TokenPos() {}
 
     TokenPos(const TokenPtr &begin, const TokenPtr &end) : begin(begin), end(end) {
-        JS_ASSERT(begin <= end);
+        
+        
     }
 
     
     static TokenPos box(const TokenPos &left, const TokenPos &right) {
-        JS_ASSERT(left.begin <= left.end);
-        JS_ASSERT(left.end <= right.begin);
-        JS_ASSERT(right.begin <= right.end);
+        
+        
+        
+        
         return TokenPos(left.begin, right.end);
     }
 
