74bd65f30786b32a4b5b2b56803645f02922c52e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:05:26 +0000

    Bug 596620 - Remove compat code for username-based UI. r=rnewman
    Also call Weave.Service.createAccount() with right parameters.
    
    UltraBlame original commit: a8551b4ae9f0416af49ba4afce73dff421963c5d

--- services/sync/tests/unit/test_utils_getErrorString.js
+++ services/sync/tests/unit/test_utils_getErrorString.js
@@ -6,7 +6,7 @@ function run_test() {
   
   
  
-  str = Utils.getErrorString("error.login.reason.password");
+  str = Utils.getErrorString("error.login.reason.account");
   do_check_true(str.match(/unknown/i) == null);
 
   str = Utils.getErrorString("foobar");
