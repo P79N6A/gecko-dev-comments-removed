1daaacf0c84e86f120ce8c489a437325e1ae4b93
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:53:53 +0000

    Bug 729940 - Part 2: Stop using crappy hash functions in the js engine. r=bhackett
    UltraBlame original commit: dfae37833c9b320266ed1cf6ebdd4ac45c38d199

--- js/xpconnect/src/XPCMaps.cpp
+++ js/xpconnect/src/XPCMaps.cpp
@@ -43,6 +43,9 @@
 #include "xpcprivate.h"
 
 #include "jshash.h"
+#include "mozilla/HashFunctions.h"
+
+using namespace mozilla;
 
 
 
@@ -88,9 +91,7 @@ HashNativeKey(JSDHashTable *table, const void *key)
 
     if (!Set) {
         NS_ASSERTION(Addition, "bad key");
-        
-        
-        h = (JSHashNumber) NS_PTR_TO_INT32(Addition) >> 2;
+        h = AddToHash(h, Addition);
     } else {
         XPCNativeInterface** Current = Set->GetInterfaceArray();
         PRUint16 count = Set->GetInterfaceCount();
@@ -98,13 +99,13 @@ HashNativeKey(JSDHashTable *table, const void *key)
             count++;
             for (PRUint16 i = 0; i < count; i++) {
                 if (i == Position)
-                    h ^= (JSHashNumber) NS_PTR_TO_INT32(Addition) >> 2;
+                    h = AddToHash(h, Addition);
                 else
-                    h ^= (JSHashNumber) NS_PTR_TO_INT32(*(Current++)) >> 2;
+                    h = AddToHash(h, *(Current++));
             }
         } else {
             for (PRUint16 i = 0; i < count; i++)
-                h ^= (JSHashNumber) NS_PTR_TO_INT32(*(Current++)) >> 2;
+                h = AddToHash(h, *(Current++));
         }
     }
 
@@ -553,10 +554,8 @@ XPCNativeScriptableSharedMap::Entry::Hash(JSDHashTable *table, const void *key)
     
     
 
-    h = (JSDHashNumber) obj->GetFlags();
-    for (s = (const unsigned char*) obj->GetJSClass()->name; *s != '\0'; s++)
-        h = JS_ROTATE_LEFT32(h, 4) ^ *s;
-    return h;
+    h = HashGeneric(obj->GetFlags());
+    return AddToHash(h, HashString(obj->GetJSClass()->name));
 }
 
 JSBool
