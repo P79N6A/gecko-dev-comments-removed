dfbb7e93186fabe44167f4f14413d04b507ac7a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:56:53 +0000

    Bug 580128. Remove XrayUtils::JSSLOT_PROXY_OBJ to fix leaks. r=mrbkap.
    UltraBlame original commit: c1ca077ef22858921a691239dba0c34351be8f01

--- js/src/xpconnect/wrappers/WrapperFactory.cpp
+++ js/src/xpconnect/wrappers/WrapperFactory.cpp
@@ -234,7 +234,6 @@ WrapperFactory::Rewrap(JSContext *cx, JSObject *obj, JSObject *wrappedProto, JSO
     
     
     wrapperObj->setProxyExtra(js::ObjectValue(*xrayHolder));
-    xrayHolder->setSlot(XrayUtils::JSSLOT_PROXY_OBJ, js::ObjectValue(*wrapperObj));
     return wrapperObj;
 }
 
@@ -258,7 +257,6 @@ WrapperFactory::WrapLocationObject(JSContext *cx, JSObject *obj)
     if (!wrapperObj)
         return NULL;
     wrapperObj->setProxyExtra(js::ObjectValue(*xrayHolder));
-    xrayHolder->setSlot(XrayUtils::JSSLOT_PROXY_OBJ, js::ObjectValue(*wrapperObj));
     return wrapperObj;
 }
 
