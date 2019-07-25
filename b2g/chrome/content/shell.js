9a0cbc13fea3ca915800373de823b30de5ded826
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:35:43 +0000

    Bug 778346 - Use End key as a sleep button for b2g desktop. r=vingtetun
    UltraBlame original commit: 87693d37bff2c1be6adb65e9ec7fcf127061e693

--- b2g/chrome/content/shell.js
+++ b2g/chrome/content/shell.js
@@ -207,6 +207,7 @@ var shell = {
         type = 'home-button';
         break;
       case evt.DOM_VK_SLEEP:        
+      case evt.DOM_VK_END:          
         type = 'sleep-button';
         break;
       case evt.DOM_VK_PAGE_UP:      
