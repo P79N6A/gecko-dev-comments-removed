1b4bc745f537eb2aee3fa5fd7845915c77e06c96
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:23:28 +0000

    Bug 670967 - Part 2: Wrap VirtualAlloc and friends, firing a low-memory notification if we notice that we're running low on virtual or physical memory after the wrapped function completes. r=bsmedberg
    UltraBlame original commit: 8348c44ba1ca0e2c0a0f7e06d27ee45294a8b6d2

---
+++ xpcom/base/AvailableMemoryTracker.h
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_AvailableMemoryTracker_h
+#define mozilla_AvailableMemoryTracker_h
+
+namespace mozilla {
+namespace AvailableMemoryTracker {
+
+
+
+
+
+#if defined(XP_WIN)
+void Init();
+#else
+void Init() {}
+#endif
+
+} 
+} 
+
+#endif 
