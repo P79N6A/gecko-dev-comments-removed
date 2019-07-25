c0e56460d3a16b4db4dad32eb9f4565d273bf7c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:22:17 +0000

    Bug 760071 - Debug scope proxies should allow the debugger to define new properties (r=jimb)
    UltraBlame original commit: 6b99d0e6e281fea68ed5d6c25fe6f4c9138af8a1

--- js/src/jit-test/tests/jaeger/bug563000/eif-call-newvar.js
+++ js/src/jit-test/tests/jaeger/bug563000/eif-call-newvar.js
@@ -2,20 +2,12 @@
 setDebug(true);
 
 function callee() {
-  var caught = false;
-  try {
-    
-    evalInFrame(1, "var y = 'success'");
-  } catch (e) {
-    assertEq(String.prototype.indexOf.call(e, "TypeError"), 0);
-    caught = true;
-  }
-  assertEq(caught, true);
+  evalInFrame(1, "var x = 'success'");
 }
 function caller(code) {
   eval(code);
   callee();
   return x;
 }
-assertEq(caller('var x = "success"'), "success");
+assertEq(caller('var y = "ignominy"'), "success");
 assertEq(typeof x, "undefined");
