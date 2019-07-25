357eff931a1d6da698e7404aa663a79608938f8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:24:26 +0000

    Bug 647560. Implement non-fallbacky behavior for _cairo_win32_surface_fill_rectangles on an ARGB surface, using StretchDIBits. r=jrmuizel
    UltraBlame original commit: 1631f3557eaea3629ad9610c7717425828c299dd

--- gfx/cairo/cairo/src/cairo-win32-surface.c
+++ gfx/cairo/cairo/src/cairo-win32-surface.c
@@ -1494,6 +1494,49 @@ categorize_solid_dest_operator (cairo_operator_t op,
     return DO_UNSUPPORTED;
 }
 
+static cairo_status_t
+_cairo_win32_surface_fill_rectangles_stretchdib (HDC                   dc,
+                                                 const cairo_color_t   *color,
+                                                 cairo_rectangle_int_t *rect,
+                                                 int                   num_rects)
+{
+    BITMAPINFO bi;
+    int pixel = ((color->alpha_short >> 8) << 24) |
+                ((color->red_short >> 8) << 16) |
+                ((color->blue_short >> 8) << 8) |
+                (color->green_short >> 8);
+    int i;
+
+    
+
+
+    bi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bi.bmiHeader.biWidth = 1;
+    bi.bmiHeader.biHeight = 1;
+    bi.bmiHeader.biSizeImage = 0;
+    bi.bmiHeader.biXPelsPerMeter = PELS_72DPI;
+    bi.bmiHeader.biYPelsPerMeter = PELS_72DPI;
+    bi.bmiHeader.biPlanes = 1;
+    bi.bmiHeader.biBitCount = 32;
+    bi.bmiHeader.biCompression = BI_RGB;
+    bi.bmiHeader.biClrUsed = 0;
+    bi.bmiHeader.biClrImportant = 0;
+
+    for (i = 0; i < num_rects; i++) {
+        if (!StretchDIBits (dc,
+                            
+                            rect[i].x, rect[i].y, rect[i].width, rect[i].height,
+                            
+                            0, 0, 1, 1,
+                            &pixel,
+                            &bi,
+                            DIB_RGB_COLORS,
+                            SRCCOPY))
+            return _cairo_win32_print_gdi_error ("_cairo_win32_surface_fill_rectangles_stretchdib(StretchDIBits)");
+    }
+    return CAIRO_STATUS_SUCCESS;
+}
+
 static cairo_int_status_t
 _cairo_win32_surface_fill_rectangles (void			*abstract_surface,
 				      cairo_operator_t		op,
@@ -1507,16 +1550,19 @@ _cairo_win32_surface_fill_rectangles (void			*abstract_surface,
     HBRUSH new_brush;
     int i;
 
-    
-
-
-
-    if (surface->format != CAIRO_FORMAT_RGB24)
-	return CAIRO_INT_STATUS_UNSUPPORTED;
-
     status = _cairo_win32_surface_set_clip_region (surface, NULL);
     if (status)
-	return status;
+        return status;
+
+    if (surface->format == CAIRO_FORMAT_ARGB32 &&
+        (surface->flags & CAIRO_WIN32_SURFACE_CAN_STRETCHDIB) &&
+        (op == CAIRO_OPERATOR_SOURCE ||
+         (op == CAIRO_OPERATOR_OVER && color->alpha_short >= 0xff00))) {
+        return _cairo_win32_surface_fill_rectangles_stretchdib (surface->dc,
+                                                                color, rects, num_rects);
+    }
+    if (surface->format != CAIRO_FORMAT_RGB24)
+        return CAIRO_INT_STATUS_UNSUPPORTED;
 
     
 
