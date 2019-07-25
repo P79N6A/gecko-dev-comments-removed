0af66e4122ef431c539fc52c67505d356c8f2225
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 07:07:06 +0000

    Strengthen our onlyXML checking (568148, r=gal).
    UltraBlame original commit: d5c0c147d6f0631847e405bef859df4833465573

--- js/src/jsparse.cpp
+++ js/src/jsparse.cpp
@@ -873,7 +873,7 @@ Compiler::compileScript(JSContext *cx, JSObject *scopeChain, JSStackFrame *calle
 
 
 
-    if (pn && onlyXML && (tcflags & TCF_NO_SCRIPT_RVAL)) {
+    if (pn && onlyXML) {
         parser.reportErrorNumber(NULL, JSREPORT_ERROR, JSMSG_XML_WHOLE_PROGRAM);
         goto out;
     }
