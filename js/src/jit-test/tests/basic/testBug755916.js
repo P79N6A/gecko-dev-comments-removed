bd87ba15adf35ae3bc42704e2fd4c31b643bee62
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:11:41 +0000

    Bug 755916 - All early returns of CheckStackAndEnterMethodJIT should return Jaeger_ThrowBeforeEnter (r=bhackett)
    UltraBlame original commit: 1d70f1cf90e8115b55971345a4bbb5833f1e37b3

---
+++ js/src/jit-test/tests/basic/testBug755916.js
@@ -0,0 +1,13 @@
+
+
+Object.defineProperty(this, "t2", {
+    get: function() {
+        for (p in h2) {
+            t2
+        }
+    }
+})
+h2 = {}
+mjitChunkLimit(8)
+h2.a = function() {}
+Object(t2)
