7cc577cc1beca3f66bfdd632262617c44e9eb268
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:05:49 +0000

    bug 644184 - ensure basic arabic shaping features are applied before ligature formation. r=jdaggett
    UltraBlame original commit: fd6a216b1072015857d765cb4b7e2bfc69c46da1

--- gfx/harfbuzz/src/hb-ot-map-private.hh
+++ gfx/harfbuzz/src/hb-ot-map-private.hh
@@ -39,6 +39,14 @@ HB_BEGIN_DECLS
 #define MAX_FEATURES 100 /* FIXME */
 #define MAX_LOOKUPS 1000 /* FIXME */
 
+
+
+#define FIRST_PRIORITY   100
+#define EARLY_PRIORITY   300
+#define DEFAULT_PRIORITY 500
+#define LATE_PRIORITY    700
+#define LAST_PRIORITY    900
+
 static const hb_tag_t table_tags[2] = {HB_OT_TAG_GSUB, HB_OT_TAG_GPOS};
 
 struct hb_ot_map_t {
@@ -50,7 +58,8 @@ struct hb_ot_map_t {
     unsigned int seq; 
     unsigned int max_value;
     bool global; 
-    unsigned int default_value; 
+    unsigned short default_value; 
+    unsigned short priority; 
 
     static int cmp (const feature_info_t *a, const feature_info_t *b)
     { return (a->tag != b->tag) ?  (a->tag < b->tag ? -1 : 1) : (a->seq < b->seq ? -1 : 1); }
@@ -59,7 +68,8 @@ struct hb_ot_map_t {
   struct feature_map_t {
     hb_tag_t tag; 
     unsigned int index[2]; 
-    unsigned int shift;
+    unsigned short shift;
+    unsigned short priority;
     hb_mask_t mask;
     hb_mask_t _1_mask; 
 
@@ -68,16 +78,22 @@ struct hb_ot_map_t {
   };
 
   struct lookup_map_t {
-    unsigned int index;
+    unsigned short index;
+    unsigned short priority;
     hb_mask_t mask;
 
     static int cmp (const lookup_map_t *a, const lookup_map_t *b)
-    { return a->index < b->index ? -1 : a->index > b->index ? 1 : 0; }
+    {
+      unsigned int a_key = (a->priority << 16) + a->index;
+      unsigned int b_key = (b->priority << 16) + b->index;
+      return a_key < b_key ? -1 : a_key > b_key ? 1 : 0;
+    }
   };
 
   HB_INTERNAL void add_lookups (hb_face_t    *face,
 				unsigned int  table_index,
 				unsigned int  feature_index,
+				unsigned short priority,
 				hb_mask_t     mask);
 
 
@@ -85,7 +101,7 @@ struct hb_ot_map_t {
 
   hb_ot_map_t (void) : feature_count (0) {}
 
-  void add_feature (hb_tag_t tag, unsigned int value, bool global)
+  void add_feature (hb_tag_t tag, unsigned int value, unsigned short priority, bool global)
   {
     feature_info_t *info = &feature_infos[feature_count++];
     info->tag = tag;
@@ -93,17 +109,18 @@ struct hb_ot_map_t {
     info->max_value = value;
     info->global = global;
     info->default_value = global ? value : 0;
+    info->priority = priority;
   }
 
-  inline void add_bool_feature (hb_tag_t tag, bool global = true)
-  { add_feature (tag, 1, global); }
+  inline void add_bool_feature (hb_tag_t tag, unsigned short priority, bool global = true)
+  { add_feature (tag, 1, priority, global); }
 
   HB_INTERNAL void compile (hb_face_t *face,
 			    const hb_segment_properties_t *props);
 
   inline hb_mask_t get_global_mask (void) const { return global_mask; }
 
-  inline hb_mask_t get_mask (hb_tag_t tag, unsigned int *shift = NULL) const {
+  inline hb_mask_t get_mask (hb_tag_t tag, unsigned short *shift = NULL) const {
     const feature_map_t *map = (const feature_map_t *) bsearch (&tag, feature_maps, feature_count, sizeof (feature_maps[0]), (hb_compare_func_t) feature_map_t::cmp);
     if (shift) *shift = map ? map->shift : 0;
     return map ? map->mask : 0;
