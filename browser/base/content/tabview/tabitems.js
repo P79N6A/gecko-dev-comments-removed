fc4e4f38ae7223ddb0362b281c7ba8a2a988b675
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:56:57 +0000

    Bug 590742 - Incorporate new CSS for Tab Candy appearance polish [r+a=dietrich]
    UltraBlame original commit: d0394b66e5d3d53e78559016a914373e27c4d046

--- browser/base/content/tabview/tabitems.js
+++ browser/base/content/tabview/tabitems.js
@@ -59,7 +59,7 @@ window.TabItem = function(tab) {
   
   var $div = iQ('<div>')
     .addClass('tab')
-    .html("<div class='thumb'><div class='thumb-shadow'></div>" +
+    .html("<div class='thumb'>" +
           "<img class='cached-thumb' style='display:none'/><canvas/></div>" +
           "<div class='favicon'><img/></div>" +
           "<span class='tab-title'>&nbsp;</span>"
@@ -489,17 +489,14 @@ window.TabItem.prototype = Utils.extend(new Item(), new Subscribable(), {
   
   
   makeActive: function() {
-   iQ(this.container).find("canvas").addClass("focus");
-   iQ(this.container).find("img.cached-thumb").addClass("focus");
-
+   iQ(this.container).addClass("focus");
   },
 
   
   
   
   makeDeactive: function() {
-   iQ(this.container).find("canvas").removeClass("focus");
-   iQ(this.container).find("img.cached-thumb").removeClass("focus");
+   iQ(this.container).removeClass("focus");
   },
 
   
