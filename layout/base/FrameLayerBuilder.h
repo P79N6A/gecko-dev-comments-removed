0a1d36a388adf9860d5650b565691916136ba463
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:28:36 +0000

    Bug 741682 - Disable component alpha layers with BasicLayers. r=roc
    UltraBlame original commit: 65410094add4d86920290f69be224b14885a435f

--- layout/base/FrameLayerBuilder.h
+++ layout/base/FrameLayerBuilder.h
@@ -110,7 +110,9 @@ public:
   FrameLayerBuilder() :
     mRetainingManager(nsnull),
     mDetectedDOMModification(false),
-    mInvalidateAllLayers(false)
+    mInvalidateAllLayers(false),
+    mContainerLayerGeneration(0),
+    mMaxContainerLayerGeneration(0)
   {
     MOZ_COUNT_CTOR(FrameLayerBuilder);
     mNewDisplayItemData.Init();
@@ -452,8 +454,13 @@ protected:
 
   class DisplayItemData {
   public:
-    DisplayItemData(Layer* aLayer, PRUint32 aKey, LayerState aLayerState)
-      : mLayer(aLayer), mDisplayItemKey(aKey), mLayerState(aLayerState), mUsed(false) {}
+    DisplayItemData(Layer* aLayer, PRUint32 aKey, LayerState aLayerState, PRUint32 aGeneration)
+      : mLayer(aLayer)
+      , mDisplayItemKey(aKey)
+      , mContainerLayerGeneration(aGeneration)
+      , mLayerState(aLayerState)
+      , mUsed(false)
+    {}
     
     DisplayItemData()
       : mUsed(false)
@@ -466,6 +473,7 @@ protected:
       mInactiveManager = toCopy.mInactiveManager;
       mGeometry = toCopy.mGeometry;
       mDisplayItemKey = toCopy.mDisplayItemKey;
+      mContainerLayerGeneration = toCopy.mContainerLayerGeneration;
       mLayerState = toCopy.mLayerState;
       mUsed = toCopy.mUsed;
     }
@@ -474,6 +482,7 @@ protected:
     nsRefPtr<LayerManager> mInactiveManager;
     nsAutoPtr<nsDisplayItemGeometry> mGeometry;
     PRUint32        mDisplayItemKey;
+    PRUint32        mContainerLayerGeneration;
     LayerState      mLayerState;
 
     
@@ -504,12 +513,14 @@ protected:
       
       
       mData.SwapElements(toCopy.mData);
+      mContainerLayerGeneration = toCopy.mContainerLayerGeneration;
     }
     ~DisplayItemDataEntry() { MOZ_COUNT_DTOR(DisplayItemDataEntry); }
 
     bool HasNonEmptyContainerLayer();
 
     nsAutoTArray<DisplayItemData, 1> mData;
+    PRUint32 mContainerLayerGeneration;
 
     enum { ALLOW_MEMMOVE = false };
   };
@@ -564,8 +575,8 @@ protected:
 
 
   struct ClippedDisplayItem {
-    ClippedDisplayItem(nsDisplayItem* aItem, const Clip& aClip)
-      : mItem(aItem), mClip(aClip)
+    ClippedDisplayItem(nsDisplayItem* aItem, const Clip& aClip, PRUint32 aGeneration)
+      : mItem(aItem), mClip(aClip), mContainerLayerGeneration(aGeneration)
     {
     }
 
@@ -581,6 +592,7 @@ protected:
     nsRefPtr<LayerManager> mInactiveLayer;
 
     Clip mClip;
+    PRUint32 mContainerLayerGeneration;
   };
 
   
@@ -604,6 +616,7 @@ public:
     
     
     nsIntPoint mLastPaintOffset;
+    PRUint32 mContainerLayerGeneration;
     bool mHasExplicitLastPaintOffset;
     
 
@@ -633,6 +646,12 @@ protected:
   
   static PLDHashOperator StoreNewDisplayItemData(DisplayItemDataEntry* aEntry,
                                                  void* aUserArg);
+
+  static PLDHashOperator RestoreDisplayItemData(DisplayItemDataEntry* aEntry,
+                                                void *aUserArg);
+
+  static PLDHashOperator RestoreThebesLayerItemEntries(ThebesLayerItemsEntry* aEntry,
+                                                       void *aUserArg);
 
   
 
@@ -679,6 +698,9 @@ protected:
 
 
   bool                                mInvalidateAllLayers;
+
+  PRUint32                            mContainerLayerGeneration;
+  PRUint32                            mMaxContainerLayerGeneration;
 };
 
 }
