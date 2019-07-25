4ec0676bbc867a8d0a46a2308bf0a6a2201b5bf9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:27:09 +0000

    Bug 666788: Remove JS_sscanf declaration, for which there is no definition (r=cdleary)
    UltraBlame original commit: e77af15dc4d410e0fa21683d3fb9d53f3a882a74

--- js/src/jsprf.h
+++ js/src/jsprf.h
@@ -110,41 +110,6 @@ extern JS_PUBLIC_API(char*) JS_vsmprintf(const char *fmt, va_list ap);
 extern JS_PUBLIC_API(char*) JS_vsprintf_append(char *last, const char *fmt, va_list ap);
 extern JS_PUBLIC_API(JSUint32) JS_vsxprintf(JSStuffFunc f, void *arg, const char *fmt, va_list ap);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-extern JS_PUBLIC_API(JSInt32) JS_sscanf(const char *buf, const char *fmt, ...);
-
 JS_END_EXTERN_C
 
 #endif 
