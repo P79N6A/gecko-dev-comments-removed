9c8ebe3d6ec923081abad722a9a5fda362a9953f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:25:25 +0000

    Bug 758992 - Make the classes which use the XPCOM nsISupports implementation macros final, to avoid the warning about deleting using a pointer to a base class with virtual functions and no virtual dtor (uriloader parts); r=bzbarsky
    UltraBlame original commit: 4142308ec401baf9504115c3492fd698f2c99a4f

--- uriloader/exthandler/nsExternalHelperAppService.h
+++ uriloader/exthandler/nsExternalHelperAppService.h
@@ -36,6 +36,7 @@
 #include "nsWeakReference.h"
 #include "nsIPrompt.h"
 #include "nsAutoPtr.h"
+#include "mozilla/Attributes.h"
 
 class nsExternalAppHandler;
 class nsIMIMEInfo;
@@ -205,9 +206,9 @@ protected:
 
 
 
-class nsExternalAppHandler : public nsIStreamListener,
-                             public nsIHelperAppLauncher,
-                             public nsITimerCallback
+class nsExternalAppHandler MOZ_FINAL : public nsIStreamListener,
+                                       public nsIHelperAppLauncher,
+                                       public nsITimerCallback
 {
 public:
   NS_DECL_ISUPPORTS
