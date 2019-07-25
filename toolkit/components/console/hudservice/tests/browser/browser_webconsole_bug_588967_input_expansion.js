226833c82c7bb8e929a021bb3eb35649629fb473
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:58:41 +0000

    Bug 588967 - Console input field doesn't resize r=dietrich a=dietrich
    UltraBlame original commit: 976d2a371fe3fb224b45b38fb6b2ea427bddc4c6

---
+++ toolkit/components/console/hudservice/tests/browser/browser_webconsole_bug_588967_input_expansion.js
@@ -0,0 +1,90 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/HUDService.jsm");
+
+const TEST_URI = "http://example.com/browser/toolkit/components/console/hudservice/tests/browser/test-console.html";
+
+function test() {
+  waitForExplicitFinish();
+  content.location.href = TEST_URI;
+  waitForFocus(onFocus);
+}
+
+function onFocus() {
+  gBrowser.selectedBrowser.addEventListener("DOMContentLoaded",
+                                            testInputExpansion, false);
+}
+
+function testInputExpansion() {
+  gBrowser.selectedBrowser.removeEventListener("DOMContentLoaded",
+                                               testInputExpansion, false);
+
+  HUDService.activateHUDForContext(gBrowser.selectedTab);
+
+  let hudId = HUDService.displaysIndex()[0];
+  let hudBox = HUDService.getHeadsUpDisplay(hudId);
+  let input = hudBox.querySelector(".jsterm-input-node");
+
+  input.focus();
+
+  is(input.getAttribute("multiline"), "true", "multiline is enabled");
+
+  let ordinaryHeight = input.clientHeight;
+
+  
+  input.value = "hello\nworld\n";
+  let length = input.value.length;
+  input.selectionEnd = length;
+  input.selectionStart = length;
+  
+  
+  EventUtils.synthesizeKey("d", {});
+  ok(input.clientHeight > ordinaryHeight, "the input expanded");
+
+  
+  input.value = "";
+  EventUtils.synthesizeKey("d", {});
+  is(input.clientHeight, ordinaryHeight, "the input's height is normal again");
+
+  HUDService.deactivateHUDForContext(gBrowser.selectedTab);
+  finish();
+}
+
