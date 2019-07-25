cef8a3e2e6757104f5bb7a1c1699718337eee0d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:53:47 +0000

    Bug 715796 - Part 0: extract SQLiteOpenHelper cache from AndroidBrowserHistoryDataExtender. r=rnewman
    UltraBlame original commit: 502147bd7715150461b5cd33b617382838dafd37

---
+++ mobile/android/base/sync/repositories/android/CachedSQLiteOpenHelper.java
@@ -0,0 +1,55 @@
+
+
+
+
+package org.mozilla.gecko.sync.repositories.android;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public abstract class CachedSQLiteOpenHelper extends SQLiteOpenHelper {
+
+  public CachedSQLiteOpenHelper(Context context, String name, CursorFactory factory,
+      int version) {
+    super(context, name, factory, version);
+  }
+
+  
+  
+  private static SQLiteDatabase readableDatabase;
+  private static SQLiteDatabase writableDatabase;
+
+  synchronized protected SQLiteDatabase getCachedReadableDatabase() {
+    if (readableDatabase == null) {
+      if (writableDatabase == null) {
+        readableDatabase = this.getReadableDatabase();
+        return readableDatabase;
+      } else {
+        return writableDatabase;
+      }
+    } else {
+      return readableDatabase;
+    }
+  }
+
+  synchronized protected SQLiteDatabase getCachedWritableDatabase() {
+    if (writableDatabase == null) {
+      writableDatabase = this.getWritableDatabase();
+    }
+    return writableDatabase;
+  }
+
+  synchronized public void close() {
+    if (readableDatabase != null) {
+      readableDatabase.close();
+      readableDatabase = null;
+    }
+    if (writableDatabase != null) {
+      writableDatabase.close();
+      writableDatabase = null;
+    }
+    super.close();
+  }
+}
