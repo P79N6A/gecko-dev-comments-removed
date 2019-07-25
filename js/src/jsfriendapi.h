69fd102a8c0b55293d0ff82f362ae8e10b9f6d24
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:30:35 +0000

    Bug 714264 - Part b: Move CompartmentStats to MemoryMetrics.h; r=njn
    The name member is implemented as a void*, because it needs to remain an
    nsCString, and as such the logic to get the name also remains in XPConnect.
    
    UltraBlame original commit: b9077aadd3d745c00159ac95aedc2402d94c7d39

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -196,6 +196,9 @@ JS_FRIEND_API(JSBool) obj_defineSetter(JSContext *cx, uintN argc, js::Value *vp)
 extern JS_FRIEND_API(bool)
 IsSystemCompartment(const JSCompartment *compartment);
 
+extern JS_FRIEND_API(bool)
+IsAtomsCompartmentFor(const JSContext *cx, const JSCompartment *c);
+
 
 
 
