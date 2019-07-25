6277f8d8f0a62ee25c233f9116baa34d3e482d9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:51:29 +0000

    Bug 716624 - Fix getting undefined property, r=dvander
    UltraBlame original commit: a6cdb71835b505163ea4c238ecf5300486275dff

--- js/src/ion/IonBuilder.cpp
+++ js/src/ion/IonBuilder.cpp
@@ -3307,8 +3307,12 @@ IonBuilder::jsop_getprop(JSAtom *atom)
 
     if (unary.ival == MIRType_Object) {
         MGetPropertyCache *load = MGetPropertyCache::New(obj, atom, script, pc);
-        if (!barrier)
-            load->setResultType(unary.rval);
+        if (!barrier) {
+            
+            
+            if (unary.rval != MIRType_Undefined && unary.rval != MIRType_Null)
+                load->setResultType(unary.rval);
+        }
         ins = load;
     } else {
         ins = MCallGetProperty::New(obj, atom);
