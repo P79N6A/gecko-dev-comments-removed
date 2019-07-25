2c4ceb108aaf53c23e3325a83b5b057795139407
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:39:15 +0000

    Bug 781943. nullptr macro. r=joe
    UltraBlame original commit: b441413e4c2d1aa73382f88909eb0ada247d76df

--- gfx/2d/2D.h
+++ gfx/2d/2D.h
@@ -19,6 +19,23 @@
 #include <string>
 #endif
 
+
+
+
+
+
+#if defined(MOZ_GFX) && !defined(HAVE_NULLPTR)
+#ifndef __cplusplus
+# define nullptr ((void*)0)
+#elif defined(__GNUC__)
+# define nullptr __null
+#elif defined(_WIN64)
+# define nullptr 0LL
+#else
+# define nullptr 0L
+#endif
+#endif 
+
 struct _cairo_surface;
 typedef _cairo_surface cairo_surface_t;
 
