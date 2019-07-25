b06308ff13ca537f22df784c9d925f047ba97581
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:21:21 +0000

    Bug 606145 part 2 - Import elfhack code. r=tglek,a=bsmedberg
    UltraBlame original commit: a1ccb1c489baeeb2e738ce9173b5ee079b0243f9

---
+++ build/unix/elfhack/inject.c
@@ -0,0 +1,71 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdint.h>
+#include <elf.h>
+
+
+#undef Elf_Ehdr
+#undef Elf_Addr
+
+#if BITS == 32
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Addr Elf32_Addr
+#else
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Addr Elf64_Addr
+#endif
+
+extern __attribute__((visibility("hidden"))) void original_init(int argc, char **argv, char **env);
+
+extern __attribute__((visibility("hidden"))) Elf32_Rel relhack[];
+extern __attribute__((visibility("hidden"))) Elf_Ehdr elf_header;
+
+void init(int argc, char **argv, char **env)
+{
+    Elf32_Rel *rel;
+    Elf_Addr *ptr, *start;
+    for (rel = relhack; rel->r_offset; rel++) {
+        start = (Elf_Addr *)((intptr_t)&elf_header + rel->r_offset);
+        for (ptr = start; ptr < &start[rel->r_info]; ptr++)
+            *ptr += (intptr_t)&elf_header;
+    }
+
+#ifndef NOINIT
+    original_init(argc, argv, env);
+#endif
+}
