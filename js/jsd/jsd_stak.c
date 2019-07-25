f7cab848664d2e142aed866475ee151448fe9706
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:28:42 +0000

    Bug 584912: Fix use after free in jsd_DestroyThreadState. r=timeless, a=sayrer
    UltraBlame original commit: bbfce6d8415dadec226a1f8e01e9dd2f34966ec4

--- js/jsd/jsd_stak.c
+++ js/jsd/jsd_stak.c
@@ -148,8 +148,8 @@ jsd_NewThreadState(JSDContext* jsdc, JSContext *cx )
 
 
                 JS_INIT_CLIST(&jsdthreadstate->links);
-                jsd_DestroyThreadState(jsdc, jsdthreadstate);
                 JS_EndRequest(jsdthreadstate->context);
+                jsd_DestroyThreadState(jsdc, jsdthreadstate);
                 return NULL;
             }
         }
