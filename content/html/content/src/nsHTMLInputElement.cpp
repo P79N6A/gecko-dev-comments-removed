31c4a6da923c51563191694b83e1b9d3ef2f4b85
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:58:55 +0000

    Bug 620255 add comment nsHTMLInputElement::PostHandleEvent for isMovingBack falling through, r+a=dbaron
    UltraBlame original commit: 24b63f638579fac6c9948ff178a2074da7648e8f

--- content/html/content/src/nsHTMLInputElement.cpp
+++ content/html/content/src/nsHTMLInputElement.cpp
@@ -2318,6 +2318,7 @@ nsHTMLInputElement::PostHandleEvent(nsEventChainPostVisitor& aVisitor)
               case NS_VK_UP: 
               case NS_VK_LEFT:
                 isMovingBack = PR_TRUE;
+                
               case NS_VK_DOWN:
               case NS_VK_RIGHT:
               
