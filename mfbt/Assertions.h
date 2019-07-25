1a787696ede6a4a925217ae1b560c38d7efe5d5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:26:18 +0000

    Bug 712129 - Add more assertion macro flavors to mfbt, and make the JS engine use mfbt's implementations where appropriate.  r=cjones
    UltraBlame original commit: 0d7567cf0a67a44906ab85ba9f2acc6ead70ba63

--- mfbt/Assertions.h
+++ mfbt/Assertions.h
@@ -73,11 +73,69 @@ JS_Assert(const char* s, const char* file, int ln);
 
 
 
+
+
+
+
+
+
 #ifdef DEBUG
 #  define MOZ_ASSERT(expr_)                                      \
      ((expr_) ? ((void)0) : JS_Assert(#expr_, __FILE__, __LINE__))
 #else
 #  define MOZ_ASSERT(expr_) ((void)0)
 #endif 
+
+
+
+
+
+
+
+
+
+
+#ifdef DEBUG
+#  define MOZ_ASSERT_IF(cond, expr)  ((cond) ? MOZ_ASSERT(expr) : ((void)0))
+#else
+#  define MOZ_ASSERT_IF(cond, expr)  ((void)0)
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef DEBUG
+#  define MOZ_NOT_REACHED(reason)    JS_Assert(reason, __FILE__, __LINE__)
+#else
+#  define MOZ_NOT_REACHED(reason)    ((void)0)
+#endif
+
+
+
+
+
+
+
+#ifdef DEBUG
+#  define MOZ_ALWAYS_TRUE(expr)      MOZ_ASSERT((expr))
+#  define MOZ_ALWAYS_FALSE(expr)     MOZ_ASSERT(!(expr))
+#else
+#  define MOZ_ALWAYS_TRUE(expr)      ((void)(expr))
+#  define MOZ_ALWAYS_FALSE(expr)     ((void)(expr))
+#endif
 
 #endif
