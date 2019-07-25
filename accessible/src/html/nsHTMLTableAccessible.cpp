bea7ca57fe27dd2cd1ff11d3c9ed4d2d7bcda130
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:59:25 +0000

    Bug 726005 - IDRefsIterator::Next() should use nsDocAccessible::GetAccessible(), r=tbsaunde, f=surkov
    UltraBlame original commit: de43aa36ceef74e681c1839b06c830dc67067c62

--- accessible/src/html/nsHTMLTableAccessible.cpp
+++ accessible/src/html/nsHTMLTableAccessible.cpp
@@ -334,7 +334,7 @@ nsHTMLTableCellAccessible::GetHeaderCells(PRInt32 aRowOrColumnHeaderCell,
                                           nsIArray **aHeaderCells)
 {
   
-  IDRefsIterator iter(mContent, nsGkAtoms::headers);
+  IDRefsIterator iter(mDoc, mContent, nsGkAtoms::headers);
   nsIContent* headerCellElm = iter.NextElem();
   if (headerCellElm) {
     nsresult rv = NS_OK;
