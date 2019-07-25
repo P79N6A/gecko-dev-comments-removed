52a9bfb34789de0604fba4f4f2d1a747712708f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:15:02 +0000

    Bug 581573 - Trunk builds fail when using --enable-cpp-rtti. r=gal a=bsmedberg
    UltraBlame original commit: 634d179da621fffb6f873e48af745576c5547275

--- js/src/jswrapper.h
+++ js/src/jswrapper.h
@@ -105,41 +105,41 @@ class JSWrapper : public js::JSProxyHandler {
 };
 
 
-class JSCrossCompartmentWrapper : public JSWrapper {
+class JS_FRIEND_API(JSCrossCompartmentWrapper) : public JSWrapper {
   public:
-    JS_FRIEND_API(JSCrossCompartmentWrapper(uintN flags));
+    JSCrossCompartmentWrapper(uintN flags);
 
-    virtual JS_FRIEND_API(~JSCrossCompartmentWrapper());
-
-    
-    virtual JS_FRIEND_API(bool) getPropertyDescriptor(JSContext *cx, JSObject *wrapper, jsid id,
-                                                      js::PropertyDescriptor *desc);
-    virtual JS_FRIEND_API(bool) getOwnPropertyDescriptor(JSContext *cx, JSObject *wrapper, jsid id,
-                                                         js::PropertyDescriptor *desc);
-    virtual JS_FRIEND_API(bool) defineProperty(JSContext *cx, JSObject *wrapper, jsid id,
-                                               js::PropertyDescriptor *desc);
-    virtual JS_FRIEND_API(bool) getOwnPropertyNames(JSContext *cx, JSObject *wrapper, js::AutoIdVector &props);
-    virtual JS_FRIEND_API(bool) delete_(JSContext *cx, JSObject *wrapper, jsid id, bool *bp);
-    virtual JS_FRIEND_API(bool) enumerate(JSContext *cx, JSObject *wrapper, js::AutoIdVector &props);
+    virtual ~JSCrossCompartmentWrapper();
 
     
-    virtual JS_FRIEND_API(bool) has(JSContext *cx, JSObject *wrapper, jsid id, bool *bp);
-    virtual JS_FRIEND_API(bool) hasOwn(JSContext *cx, JSObject *wrapper, jsid id, bool *bp);
-    virtual JS_FRIEND_API(bool) get(JSContext *cx, JSObject *wrapper, JSObject *receiver, jsid id, js::Value *vp);
-    virtual JS_FRIEND_API(bool) set(JSContext *cx, JSObject *wrapper, JSObject *receiver, jsid id, js::Value *vp);
-    virtual JS_FRIEND_API(bool) enumerateOwn(JSContext *cx, JSObject *wrapper, js::AutoIdVector &props);
-    virtual JS_FRIEND_API(bool) iterate(JSContext *cx, JSObject *wrapper, uintN flags, js::Value *vp);
+    virtual bool getPropertyDescriptor(JSContext *cx, JSObject *wrapper, jsid id,
+                                       js::PropertyDescriptor *desc);
+    virtual bool getOwnPropertyDescriptor(JSContext *cx, JSObject *wrapper, jsid id,
+                                          js::PropertyDescriptor *desc);
+    virtual bool defineProperty(JSContext *cx, JSObject *wrapper, jsid id,
+                                js::PropertyDescriptor *desc);
+    virtual bool getOwnPropertyNames(JSContext *cx, JSObject *wrapper, js::AutoIdVector &props);
+    virtual bool delete_(JSContext *cx, JSObject *wrapper, jsid id, bool *bp);
+    virtual bool enumerate(JSContext *cx, JSObject *wrapper, js::AutoIdVector &props);
 
     
-    virtual JS_FRIEND_API(bool) call(JSContext *cx, JSObject *wrapper, uintN argc, js::Value *vp);
-    virtual JS_FRIEND_API(bool) construct(JSContext *cx, JSObject *wrapper,
-                                          uintN argc, js::Value *argv, js::Value *rval);
-    virtual JS_FRIEND_API(JSString *) obj_toString(JSContext *cx, JSObject *wrapper);
-    virtual JS_FRIEND_API(JSString *) fun_toString(JSContext *cx, JSObject *wrapper, uintN indent);
+    virtual bool has(JSContext *cx, JSObject *wrapper, jsid id, bool *bp);
+    virtual bool hasOwn(JSContext *cx, JSObject *wrapper, jsid id, bool *bp);
+    virtual bool get(JSContext *cx, JSObject *wrapper, JSObject *receiver, jsid id, js::Value *vp);
+    virtual bool set(JSContext *cx, JSObject *wrapper, JSObject *receiver, jsid id, js::Value *vp);
+    virtual bool enumerateOwn(JSContext *cx, JSObject *wrapper, js::AutoIdVector &props);
+    virtual bool iterate(JSContext *cx, JSObject *wrapper, uintN flags, js::Value *vp);
 
-    static JS_FRIEND_API(bool) isCrossCompartmentWrapper(JSObject *obj);
+    
+    virtual bool call(JSContext *cx, JSObject *wrapper, uintN argc, js::Value *vp);
+    virtual bool construct(JSContext *cx, JSObject *wrapper,
+                           uintN argc, js::Value *argv, js::Value *rval);
+    virtual JSString *obj_toString(JSContext *cx, JSObject *wrapper);
+    virtual JSString *fun_toString(JSContext *cx, JSObject *wrapper, uintN indent);
 
-    static JS_FRIEND_API(JSCrossCompartmentWrapper) singleton;
+    static bool isCrossCompartmentWrapper(JSObject *obj);
+
+    static JSCrossCompartmentWrapper singleton;
 };
 
 namespace js {
