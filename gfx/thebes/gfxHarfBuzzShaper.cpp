f3245e70a74052ffd54905e59227fefba8c8e118
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:51:43 +0000

    bug 728866 - pt 3 - add support for Hebrew presentation forms, excluded from canonical composition. r=smontagu
    UltraBlame original commit: 969493463a87c7942ac30d75c9548e547f03b976

--- gfx/thebes/gfxHarfBuzzShaper.cpp
+++ gfx/thebes/gfxHarfBuzzShaper.cpp
@@ -700,6 +700,38 @@ HBGetEastAsianWidth(hb_unicode_funcs_t *ufuncs, hb_codepoint_t aCh, void *user_d
     return gfxUnicodeProperties::GetEastAsianWidth(aCh);
 }
 
+
+
+static const PRUnichar sDageshForms[0x05EA - 0x05D0 + 1] = {
+    0xFB30, 
+    0xFB31, 
+    0xFB32, 
+    0xFB33, 
+    0xFB34, 
+    0xFB35, 
+    0xFB36, 
+    0, 
+    0xFB38, 
+    0xFB39, 
+    0xFB3A, 
+    0xFB3B, 
+    0xFB3C, 
+    0, 
+    0xFB3E, 
+    0, 
+    0xFB40, 
+    0xFB41, 
+    0, 
+    0xFB43, 
+    0xFB44, 
+    0, 
+    0xFB46, 
+    0xFB47, 
+    0xFB48, 
+    0xFB49, 
+    0xFB4A 
+};
+
 static hb_bool_t
 HBUnicodeCompose(hb_unicode_funcs_t *ufuncs,
                  hb_codepoint_t      a,
@@ -707,7 +739,89 @@ HBUnicodeCompose(hb_unicode_funcs_t *ufuncs,
                  hb_codepoint_t     *ab,
                  void               *user_data)
 {
-    return nsUnicodeNormalizer::Compose(a, b, ab);
+    hb_bool_t found = nsUnicodeNormalizer::Compose(a, b, ab);
+
+    if (!found && (b & 0x1fff80) == 0x0580) {
+        
+        
+        switch (b) {
+        case 0x05B4: 
+            if (a == 0x05D9) { 
+                *ab = 0xFB1D;
+                found = true;
+            }
+            break;
+        case 0x05B7: 
+            if (a == 0x05F2) { 
+                *ab = 0xFB1F;
+                found = true;
+            } else if (a == 0x05D0) { 
+                *ab = 0xFB2E;
+                found = true;
+            }
+            break;
+        case 0x05B8: 
+            if (a == 0x05D0) { 
+                *ab = 0xFB2F;
+                found = true;
+            }
+            break;
+        case 0x05B9: 
+            if (a == 0x05D5) { 
+                *ab = 0xFB4B;
+                found = true;
+            }
+            break;
+        case 0x05BC: 
+            if (a >= 0x05D0 && a <= 0x05EA) {
+                *ab = sDageshForms[a - 0x05D0];
+                found = (*ab != 0);
+            } else if (a == 0xFB2A) { 
+                *ab = 0xFB2C;
+                found = true;
+            } else if (a == 0xFB2B) { 
+                *ab = 0xFB2D;
+                found = true;
+            }
+            break;
+        case 0x05BF: 
+            switch (a) {
+            case 0x05D1: 
+                *ab = 0xFB4C;
+                found = true;
+                break;
+            case 0x05DB: 
+                *ab = 0xFB4D;
+                found = true;
+                break;
+            case 0x05E4: 
+                *ab = 0xFB4E;
+                found = true;
+                break;
+            }
+            break;
+        case 0x05C1: 
+            if (a == 0x05E9) { 
+                *ab = 0xFB2A;
+                found = true;
+            } else if (a == 0xFB49) { 
+                *ab = 0xFB2C;
+                found = true;
+            }
+            break;
+        case 0x05C2: 
+            if (a == 0x05E9) { 
+                *ab = 0xFB2B;
+                found = true;
+            } else if (a == 0xFB49) { 
+                *ab = 0xFB2D;
+                found = true;
+            }
+            break;
+        }
+    }
+
+    return found;
 }
 
 static hb_bool_t
