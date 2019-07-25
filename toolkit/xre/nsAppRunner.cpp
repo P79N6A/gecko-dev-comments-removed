44fa2389c89ff6d37e5dbe83417fe6d30f6694da
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:33:03 +0000

    Bug 595384 - Disable the updater utility for Android [r=rs, a2.0=blassey]
    UltraBlame original commit: 6d0e5b481cc7d145def8cab16bf80fb7f120826f

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3272,7 +3272,7 @@ XRE_main(int argc, char* argv[], const nsXREAppData* aAppData)
       return 1;
     }
 
-#if defined(MOZ_UPDATER)
+#if defined(MOZ_UPDATER) && !defined(ANDROID)
   
   nsCOMPtr<nsIFile> updRoot;
   PRBool persistent;
