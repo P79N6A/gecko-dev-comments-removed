c3af551c1c7562730b74d00ff65ab1ae1067d22d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:15:32 +0000

    Add testcase for bug 476088.  r=jruderman
    UltraBlame original commit: c7d671e9cf5899f09e7bdc5ff1b5b20504d5498a

---
+++ js/src/tests/js1_8_5/regress/regress-476088.js
@@ -0,0 +1,17 @@
+
+
+
+
+var err;
+try {
+    f = function() { var x; x.y; }
+    trap(f, 0, "");
+    f();
+} catch (e) {
+    err = e;
+}
+assertEq(err instanceof TypeError, true);
+assertEq(err.message, "x is undefined")
+
+reportCompare(0, 0, 'ok');
+
