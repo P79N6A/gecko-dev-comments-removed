83fee032aad09af0d8bf967d3639e15f14989f34
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 08:07:55 +0000

    Bug 583493 - nsTransactionItem log spew is making working with tinderbox impossible; r=roc approval2.0=dbaron
    UltraBlame original commit: 913f5a57da18b5e946c49056c7f4364726c4f578

--- editor/txmgr/src/nsTransactionItem.cpp
+++ editor/txmgr/src/nsTransactionItem.cpp
@@ -223,8 +223,6 @@ nsTransactionItem::UndoTransaction(nsTransactionManager *aTxMgr)
     return result;
   }
 
-  
-  
   if (!mTransaction)
     return NS_OK;
 
@@ -451,7 +449,8 @@ nsTransactionItem::RecoverFromRedoError(nsTransactionManager *aTxMgr)
     return result;
   }
 
-  NS_ENSURE_TRUE(mTransaction, NS_OK);
+  if (!mTransaction)
+    return NS_OK;
 
   return mTransaction->UndoTransaction();
 }
