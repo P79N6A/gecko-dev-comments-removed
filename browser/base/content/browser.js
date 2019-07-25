a820c8ef34951272e96a9683fd1bab20f3120409
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 11:54:16 +0000

    Backed out changeset 9698ac3f1c61 because of string impact
    UltraBlame original commit: c9331bfef5b6fd724c97538b2b3fe69a1d1be7b7

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2553,6 +2553,24 @@ function PageProxyClickHandler(aEvent)
     middleMousePaste(aEvent);
 }
 
+function BrowserImport()
+{
+#ifdef XP_MACOSX
+  var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                     .getService(Components.interfaces.nsIWindowMediator);
+  var win = wm.getMostRecentWindow("Browser:MigrationWizard");
+  if (win)
+    win.focus();
+  else {
+    window.openDialog("chrome://browser/content/migration/migration.xul",
+                      "migration", "centerscreen,chrome,resizable=no");
+  }
+#else
+  window.openDialog("chrome://browser/content/migration/migration.xul",
+                    "migration", "modal,centerscreen,chrome,resizable=no");
+#endif
+}
+
 
 
 
@@ -8055,6 +8073,8 @@ let gPrivateBrowsingUI = {
 
     this._setPBMenuTitle("stop");
 
+    document.getElementById("menu_import").setAttribute("disabled", "true");
+
     
     
     document.getElementById("Tools:Sanitize").setAttribute("disabled", "true");
@@ -8101,6 +8121,8 @@ let gPrivateBrowsingUI = {
     if (gURLBar) {
       gURLBar.editor.transactionManager.clear();
     }
+
+    document.getElementById("menu_import").removeAttribute("disabled");
 
     
     
