4ee0ff412653fed099e831298e318f9851e737e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:51:34 +0000

    Turn on TI for ARM (r=dvander, Bug 722917)
    UltraBlame original commit: d283b00c1f730456431659c6888bcb1c86638365

--- js/src/ion/arm/CodeGenerator-arm.cpp
+++ js/src/ion/arm/CodeGenerator-arm.cpp
@@ -601,7 +601,7 @@ CodeGeneratorARM::visitShiftOp(LShiftOp *ins)
             
             
             if (ursh->canOverflow()) {
-                masm.ma_cmp(ToRegister(lhs), Imm32(0));
+                masm.ma_cmp(ToRegister(dest), Imm32(0));
                 if (!bailoutIf(Assembler::LessThan, ins->snapshot())) {
                     return false;
                 }
