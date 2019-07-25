124f8d45df8b2bdc0dbc66d29487a0860e7adddc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:09:05 +0000

    Bug 728017 - Implement WEBGL_compressed_texture_s3tc - r=bjacob
    UltraBlame original commit: 91a83411164efebb9ae2d7ad95d31f908e144e9b

---
+++ content/canvas/src/WebGLExtensionCompressedTextureS3TC.cpp
@@ -0,0 +1,33 @@
+
+
+
+
+#include "WebGLContext.h"
+#include "WebGLExtensions.h"
+
+using namespace mozilla;
+
+WebGLExtensionCompressedTextureS3TC::WebGLExtensionCompressedTextureS3TC(WebGLContext* context)
+    : WebGLExtension(context)
+{
+    context->mCompressedTextureFormats.AppendElement(LOCAL_GL_COMPRESSED_RGB_S3TC_DXT1_EXT);
+    context->mCompressedTextureFormats.AppendElement(LOCAL_GL_COMPRESSED_RGBA_S3TC_DXT1_EXT);
+    context->mCompressedTextureFormats.AppendElement(LOCAL_GL_COMPRESSED_RGBA_S3TC_DXT3_EXT);
+    context->mCompressedTextureFormats.AppendElement(LOCAL_GL_COMPRESSED_RGBA_S3TC_DXT5_EXT);
+}
+
+WebGLExtensionCompressedTextureS3TC::~WebGLExtensionCompressedTextureS3TC()
+{
+
+}
+
+NS_IMPL_ADDREF_INHERITED(WebGLExtensionCompressedTextureS3TC, WebGLExtension)
+NS_IMPL_RELEASE_INHERITED(WebGLExtensionCompressedTextureS3TC, WebGLExtension)
+
+DOMCI_DATA(WebGLExtensionCompressedTextureS3TC, WebGLExtensionCompressedTextureS3TC)
+
+NS_INTERFACE_MAP_BEGIN(WebGLExtensionCompressedTextureS3TC)
+  NS_INTERFACE_MAP_ENTRY(nsIWebGLExtensionCompressedTextureS3TC)
+  NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, WebGLExtension)
+  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(WebGLExtensionCompressedTextureS3TC)
+NS_INTERFACE_MAP_END_INHERITING(WebGLExtension)
