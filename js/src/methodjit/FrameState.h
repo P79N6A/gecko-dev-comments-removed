e780644c9faa92234405848f8374a12fbccc7771
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:27:12 +0000

    Fix register allocation around inline returns, bug 713209.
    UltraBlame original commit: 721203297177bcf51d6757d72e2f5786921a3c43

--- js/src/methodjit/FrameState.h
+++ js/src/methodjit/FrameState.h
@@ -877,7 +877,7 @@ class FrameState
     Address addressForDataRemat(const FrameEntry *fe) const;
 
     
-    Address addressForInlineReturn() const;
+    Address addressForInlineReturn();
 
     inline StateRemat dataRematInfo(const FrameEntry *fe) const;
 
