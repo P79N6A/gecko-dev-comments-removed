3e2f225b0e9331c2e710d38c4d344ffa25f4bbbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 10:08:54 +0000

    Bug 606279, part 1: Excise shmem-backed canvas. r=joe sr=vlad
    UltraBlame original commit: 8eb26a77a90eb518492d72d04fa8f116ea208cb7

--- content/canvas/public/nsICanvasRenderingContextInternal.h
+++ content/canvas/public/nsICanvasRenderingContextInternal.h
@@ -120,16 +120,6 @@ public:
   
   
   NS_IMETHOD SetIsIPC(PRBool isIPC) = 0;
-
-  
-  
-  NS_IMETHOD Swap(mozilla::ipc::Shmem& back,
-                  PRInt32 x, PRInt32 y, PRInt32 w, PRInt32 h) = 0;
-
-  
-  NS_IMETHOD Swap(PRUint32 nativeID,
-                  PRInt32 x, PRInt32 y, PRInt32 w, PRInt32 h) = 0;
-
 };
 
 NS_DEFINE_STATIC_IID_ACCESSOR(nsICanvasRenderingContextInternal,
