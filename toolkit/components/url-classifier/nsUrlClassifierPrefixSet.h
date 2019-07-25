b4002b28579191c78c30181042a364f0fbaa25de
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 14:52:01 +0000

    Bug 669410 - Make the url-classifier PrefixSet persistent on startup/shutdown. r=tony
    UltraBlame original commit: 3034a31ec6ad854bd983e7e76cff7a2f4b1d6c16

--- toolkit/components/url-classifier/nsUrlClassifierPrefixSet.h
+++ toolkit/components/url-classifier/nsUrlClassifierPrefixSet.h
@@ -43,10 +43,12 @@
 
 #include "nsISupportsUtils.h"
 #include "nsID.h"
+#include "nsIFile.h"
 #include "nsIUrlClassifierPrefixSet.h"
 #include "nsToolkitCompsCID.h"
 #include "mozilla/Mutex.h"
 #include "mozilla/CondVar.h"
+#include "mozilla/FileUtils.h"
 
 class nsUrlClassifierPrefixSet : public nsIUrlClassifierPrefixSet
 {
@@ -66,17 +68,23 @@ public:
   
   NS_IMETHOD Probe(PRUint32 aPrefix, PRBool* aReady, PRBool* aFound);
   NS_IMETHOD EstimateSize(PRUint32* aSize);
+  NS_IMETHOD IsEmpty(PRBool * aEmpty);
+  NS_IMETHOD LoadFromFile(nsIFile * aFile);
+  NS_IMETHOD StoreToFile(nsIFile * aFile);
 
   NS_DECL_ISUPPORTS
 
 protected:
   static const PRUint32 DELTAS_LIMIT = 100;
   static const PRUint32 MAX_INDEX_DIFF = (1 << 16);
+  static const PRUint32 PREFIXSET_VERSION_MAGIC = 1;
 
-  mozilla::Mutex mPrefixTreeLock;
-  mozilla::CondVar mTreeIsReady;
+  mozilla::Mutex mPrefixSetLock;
+  mozilla::CondVar mSetIsReady;
 
   PRUint32 BinSearch(PRUint32 start, PRUint32 end, PRUint32 target);
+  nsresult LoadFromFd(mozilla::AutoFDClose & fileFd);
+  nsresult StoreToFd(mozilla::AutoFDClose & fileFd);
 
   
   
