5edc8de89b1f23c406ddf18135242ce2bf19c1a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:39:48 +0000

    Bug 449734 part 5 - Test for child Popup widget.  a=test
    UltraBlame original commit: 81922822080fb5f7393745b03eefbdc8e2ec4d6b

---
+++ browser/base/content/test/browser_tab_dragdrop2.js
@@ -0,0 +1,53 @@
+function test()
+{
+  waitForExplicitFinish();
+
+  var level1 = false;
+  var level2 = false;
+  function test1() {
+    
+    
+    
+    
+    
+    
+    var uri = "chrome://mochikit/content/browser/" +
+              "browser/base/content/test/browser_tab_dragdrop2_frame1.xul";
+    let window_B = openDialog(location, "_blank", "chrome,all,dialog=no,left=200,top=200,width=200,height=200", uri);
+    window_B.addEventListener("load", function(aEvent) {
+      window_B.removeEventListener("load", arguments.callee, false);
+      if (level1) return; level1=true;
+      executeSoon(function () {
+        window_B.gBrowser.addEventListener("load", function(aEvent) {
+          window_B.removeEventListener("load", arguments.callee, true);
+          if (level2) return; level2=true;
+          is(window_B.gBrowser.getBrowserForTab(window_B.gBrowser.tabs[0]).contentWindow.location, uri, "sanity check");
+          
+          var windowB_tab2 = window_B.gBrowser.addTab("about:blank", {skipAnimation: true});
+          setTimeout(function () {
+            
+            window_B.gBrowser.addEventListener("pagehide", function(aEvent) {
+              window_B.gBrowser.removeEventListener("pagehide", arguments.callee, true);
+              executeSoon(function () {
+                
+                
+                window_B.close();
+
+                var doc = window_C.gBrowser.getBrowserForTab(window_C.gBrowser.tabs[0])
+                            .docShell.contentViewer.DOMDocument.wrappedJSObject;
+                var elems = document.documentElement.childNodes;
+                var calls = doc.defaultView.test_panels();
+                window_C.close();
+                finish();
+              });
+            }, true);
+            window_B.gBrowser.selectedTab = window_B.gBrowser.tabs[0];
+            var window_C = window_B.gBrowser.replaceTabWithWindow(window_B.gBrowser.tabs[0]);
+            }, 1000);  
+        }, true);
+      });
+    }, false);
+  }
+
+  test1();
+}
