a28fac6429b4688fc62d80f63405b6b881fb3988
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 16:10:06 +0000

    Bug 754625 - Use idl-parser from $(LIBXUL_DIST)/sdk/bin and only remove lex and yacc files in header.py --regen. r=khuey
    UltraBlame original commit: 9e5b25074f9fd76c4095e5cf9518ee6cc16e45ba

--- xpcom/idl-parser/header.py
+++ xpcom/idl-parser/header.py
@@ -505,9 +505,7 @@ if __name__ == '__main__':
             print >>sys.stderr, "--regen useless without --cachedir"
         
         
-        deadfiles = os.path.join(options.cachedir, "*.py*")
-        for filename in glob.glob(deadfiles):
-            print filename
+        for filename in [os.path.join(options.cachedir, f) for f in ["xpidllex.py", "xpidlyacc.py"]]:
             os.remove(filename)
 
     
