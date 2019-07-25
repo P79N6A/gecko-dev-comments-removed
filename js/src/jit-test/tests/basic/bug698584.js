f5a898fd3a9659fcaf1786d368d72776ba2df662
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 15:12:34 +0000

    Bug 702182 bug698584.js should allow oom r=evilpie
    UltraBlame original commit: cf6e30a75b0a72c9d2e4aa271a4dcd08749e8002

--- js/src/jit-test/tests/basic/bug698584.js
+++ js/src/jit-test/tests/basic/bug698584.js
@@ -4,11 +4,18 @@
 
 
 
-const MAX = 10000;
-var str = "";
-for (var i = 0; i < MAX; ++i) {
-    /x/.test(str);
-    str += str + 'xxxxxxxxxxxxxx';
+try
+{ 
+  const MAX = 10000;
+  var str = "";
+  for (var i = 0; i < MAX; ++i) {
+      /x/.test(str);
+      str += str + 'xxxxxxxxxxxxxx';
+  }
+}
+catch (e)
+{
+  assertEq(""+e, "InternalError: allocation size overflow");
 }
 
 
