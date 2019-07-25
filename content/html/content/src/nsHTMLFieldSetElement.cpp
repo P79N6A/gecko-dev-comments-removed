b234bb33b9559fd56096146b54c1be1e4eb8e3e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-25 09:34:37 +0000

    Bug 595429 - Add name IDL attribute for HTMLFieldSetElement. r=smaug a=jst
    UltraBlame original commit: 9405497abe4e10d3951a588102d0bcf25a2ac4e0

--- content/html/content/src/nsHTMLFieldSetElement.cpp
+++ content/html/content/src/nsHTMLFieldSetElement.cpp
@@ -110,14 +110,13 @@ NS_INTERFACE_TABLE_HEAD(nsHTMLFieldSetElement)
                                                nsGenericHTMLFormElement)
 NS_HTML_CONTENT_INTERFACE_TABLE_TAIL_CLASSINFO(HTMLFieldSetElement)
 
+NS_IMPL_ELEMENT_CLONE(nsHTMLFieldSetElement)
 
 
+NS_IMPL_STRING_ATTR(nsHTMLFieldSetElement, Name, name)
 
 
 NS_IMPL_NSICONSTRAINTVALIDATION(nsHTMLFieldSetElement)
-
-NS_IMPL_ELEMENT_CLONE(nsHTMLFieldSetElement)
-
 
 
 
