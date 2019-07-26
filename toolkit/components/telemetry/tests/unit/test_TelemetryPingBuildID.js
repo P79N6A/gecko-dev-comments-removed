3af177038d0528d6929bde81f3e96a2b3aec7262
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:16:43 +0000

    Bug 913070 - Move TelemetryPing to a jsm. r=Yoric, r=glandium
    UltraBlame original commit: 683e4728dbe6cbdb6f03803f35e65de9b4e6cf4b

--- toolkit/components/telemetry/tests/unit/test_TelemetryPingBuildID.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryPingBuildID.js
@@ -13,13 +13,13 @@
 
 
 
-Components.utils.import("resource://gre/modules/Services.jsm");
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/TelemetryPing.jsm");
 
 
-Services.scriptloader.loadSubScript("resource://gre/components/TelemetryPing.js");
-
-
-Services.prefs.setBoolPref(PREF_ENABLED, true);
+Services.prefs.setBoolPref(TelemetryPing.Constants.PREF_ENABLED, true);
 
 
 Cu.import("resource://testing-common/AppInfo.jsm");
@@ -28,24 +28,21 @@ updateAppInfo();
 
 
 function testFirstRun() {
-  let ping = new TelemetryPing();
-  ping.setup();
-  let metadata = ping.getMetadata();
+  TelemetryPing.reset();
+  let metadata = TelemetryPing.getMetadata();
   do_check_false("previousBuildID" in metadata);
   let appBuildID = getAppInfo().appBuildID;
-  let buildIDPref = Services.prefs.getCharPref(PREF_PREVIOUS_BUILDID);
+  let buildIDPref = Services.prefs.getCharPref(TelemetryPing.Constants.PREF_PREVIOUS_BUILDID);
   do_check_eq(appBuildID, buildIDPref);
 }
 
 
 
 function testSecondRun() {
-  let ping = new TelemetryPing();
-  ping.setup();
-  let metadata = ping.getMetadata();
+  TelemetryPing.reset();
+  let metadata = TelemetryPing.getMetadata();
   do_check_false("previousBuildID" in metadata);
 }
-
 
 
 
@@ -55,11 +52,10 @@ function testNewBuild() {
   let info = getAppInfo();
   let oldBuildID = info.appBuildID;
   info.appBuildID = NEW_BUILD_ID;
-  let ping = new TelemetryPing();
-  ping.setup();
-  let metadata = ping.getMetadata();
+  TelemetryPing.reset();
+  let metadata = TelemetryPing.getMetadata();
   do_check_eq(metadata.previousBuildID, oldBuildID);
-  let buildIDPref = Services.prefs.getCharPref(PREF_PREVIOUS_BUILDID);
+  let buildIDPref = Services.prefs.getCharPref(TelemetryPing.Constants.PREF_PREVIOUS_BUILDID);
   do_check_eq(NEW_BUILD_ID, buildIDPref);
 }
 
