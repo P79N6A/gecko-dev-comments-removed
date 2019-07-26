4b0f676299938676a02a48c7eaed3461c741b550
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:59:34 +0000

    Bug 1010348: Update mixed-content warning kb article for Android. r=margaret
    UltraBlame original commit: d1269bdb70e2891cb92cd5f0754d942c70ff29bb

--- mobile/android/base/toolbar/SiteIdentityPopup.java
+++ mobile/android/base/toolbar/SiteIdentityPopup.java
@@ -33,9 +33,8 @@ import android.widget.TextView;
 public class SiteIdentityPopup extends ArrowPopup {
     private static final String LOGTAG = "GeckoSiteIdentityPopup";
 
-    
     private static final String MIXED_CONTENT_SUPPORT_URL =
-        "https://support.mozilla.org/kb/how-does-content-isnt-secure-affect-my-safety";
+        "https://support.mozilla.org/kb/how-does-insecure-content-affect-safety-android";
 
     private SiteIdentity mSiteIdentity;
 
