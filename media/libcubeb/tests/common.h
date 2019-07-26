68aa3d97826f5a50772ffe33ee8dc57d59540f11
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:04:47 +0000

    Bug 946618 - Update libcubeb from upstream to pick up new tests. r=kinetik
    UltraBlame original commit: dd53f54dbaa9743ae33b2c612ddb89cd159eddd4

---
+++ media/libcubeb/tests/common.h
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+#if defined( _WIN32)
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+void delay(unsigned int ms)
+{
+#if defined(_WIN32)
+	Sleep(ms);
+#else
+	sleep(ms / 1000);
+	usleep(ms % 1000 * 1000);
+#endif
+}
+
+#if !defined(M_PI)
+#define M_PI 3.14159265358979323846
+#endif
+
