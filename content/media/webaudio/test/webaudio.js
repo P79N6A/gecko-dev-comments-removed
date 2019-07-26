8ddaf2099f432356220bad5a1bccdef88b131b83
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:10:25 +0000

    Bug 856361. Part 5: Implement MediaStreamAudioSourceNode. r=ehsan
    UltraBlame original commit: 9e90a81b9e37371a8c209ba1675d3391405a1afe

--- content/media/webaudio/test/webaudio.js
+++ content/media/webaudio/test/webaudio.js
@@ -56,13 +56,15 @@ function compareBuffers(buf1, buf2,
 
   is(difference, 0, "Found " + difference + " different samples, maxDifference: " +
      maxDifference + ", first bad index: " + firstBadIndex +
-     " with source offset " + sourceOffset + " and desitnation offset " +
+     " with source offset " + sourceOffset + " and destination offset " +
      destOffset);
 }
 
 function getEmptyBuffer(context, length) {
   return context.createBuffer(gTest.numberOfChannels, length, context.sampleRate);
 }
+
+
 
 
 
@@ -95,7 +97,7 @@ function runTest()
   }
 
   SimpleTest.waitForExplicitFinish();
-  addLoadEvent(function() {
+  function runTestFunction () {
     if (!gTest.numberOfChannels) {
       gTest.numberOfChannels = 2; 
     }
@@ -177,14 +179,25 @@ function runTest()
         };
         context.startRendering();
       }
+
       var context = new OfflineAudioContext(gTest.numberOfChannels, testLength, sampleRate);
       runTestOnContext(context, callback, testOutput);
     }
 
     testOnNormalContext(function() {
-      testOnOfflineContext(function() {
-        testOnOfflineContext(done, 44100);
-      }, 48000);
+      if (!gTest.skipOfflineContextTests) {
+        testOnOfflineContext(function() {
+          testOnOfflineContext(done, 44100);
+        }, 48000);
+      } else {
+        done();
+      }
     });
-  });
+  };
+
+  if (document.readyState !== 'complete') {
+    addLoadEvent(runTestFunction);
+  } else {
+    runTestFunction();
+  }
 }
