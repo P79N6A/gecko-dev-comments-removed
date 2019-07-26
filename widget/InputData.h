631f18cd334e46bd2c76b5ad056c9a2260581783
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:46:44 +0000

    Bug 832159 - Move internal part of InputData (nsEvent conversion) out from the header. r=drs, r=smaug
    UltraBlame original commit: ffa079b50b2047b62f4113eb70c4ae7f01341e5f

--- widget/InputData.h
+++ widget/InputData.h
@@ -6,10 +6,12 @@
 #ifndef InputData_h__
 #define InputData_h__
 
-#include "nsGUIEvent.h"
-#include "nsDOMTouchEvent.h"
 #include "nsDebug.h"
+#include "nsPoint.h"
+#include "nsTArray.h"
 
+class nsTouchEvent;
+class nsMouseEvent;
 namespace mozilla {
 
 
@@ -49,6 +51,10 @@ public:
   INPUTDATA_AS_CHILD_TYPE(MultiTouchInput, MULTITOUCH_INPUT)
   INPUTDATA_AS_CHILD_TYPE(PinchGestureInput, PINCHGESTURE_INPUT)
   INPUTDATA_AS_CHILD_TYPE(TapGestureInput, TAPGESTURE_INPUT)
+
+  InputData()
+  {
+  }
 
 protected:
   InputData(InputType aInputType, uint32_t aTime)
@@ -91,6 +97,10 @@ public:
   {
 
 
+  }
+
+  SingleTouchData()
+  {
   }
 
   
@@ -144,97 +154,19 @@ public:
 
   }
 
-  MultiTouchInput(const nsTouchEvent& aTouchEvent)
-    : InputData(MULTITOUCH_INPUT, aTouchEvent.time)
+  MultiTouchInput()
   {
-    NS_ABORT_IF_FALSE(NS_IsMainThread(),
-                      "Can only copy from nsTouchEvent on main thread");
-
-    switch (aTouchEvent.message) {
-      case NS_TOUCH_START:
-        mType = MULTITOUCH_START;
-        break;
-      case NS_TOUCH_MOVE:
-        mType = MULTITOUCH_MOVE;
-        break;
-      case NS_TOUCH_END:
-        mType = MULTITOUCH_END;
-        break;
-      case NS_TOUCH_ENTER:
-        mType = MULTITOUCH_ENTER;
-        break;
-      case NS_TOUCH_LEAVE:
-        mType = MULTITOUCH_LEAVE;
-        break;
-      case NS_TOUCH_CANCEL:
-        mType = MULTITOUCH_CANCEL;
-        break;
-      default:
-        NS_WARNING("Did not assign a type to a MultiTouchInput");
-        break;
-    }
-
-    for (size_t i = 0; i < aTouchEvent.touches.Length(); i++) {
-      nsDOMTouch* domTouch = (nsDOMTouch*)(aTouchEvent.touches[i].get());
-
-      
-      int32_t identifier, radiusX, radiusY;
-      float rotationAngle, force;
-      domTouch->GetIdentifier(&identifier);
-      domTouch->GetRadiusX(&radiusX);
-      domTouch->GetRadiusY(&radiusY);
-      domTouch->GetRotationAngle(&rotationAngle);
-      domTouch->GetForce(&force);
-
-      SingleTouchData data(identifier,
-                           domTouch->mRefPoint,
-                           nsIntPoint(radiusX, radiusY),
-                           rotationAngle,
-                           force);
-
-      mTouches.AppendElement(data);
-    }
   }
 
-  
-  
-  
-  
-  
-  
-  MultiTouchInput(const nsMouseEvent& aMouseEvent)
-    : InputData(MULTITOUCH_INPUT, aMouseEvent.time)
-  {
-    NS_ABORT_IF_FALSE(NS_IsMainThread(),
-                      "Can only copy from nsMouseEvent on main thread");
-    switch (aMouseEvent.message) {
-    case NS_MOUSE_BUTTON_DOWN:
-      mType = MULTITOUCH_START;
-      break;
-    case NS_MOUSE_MOVE:
-      mType = MULTITOUCH_MOVE;
-      break;
-    case NS_MOUSE_BUTTON_UP:
-      mType = MULTITOUCH_END;
-      break;
-    
-    
-    
-    
-    case NS_MOUSE_EXIT:
-      mType = MULTITOUCH_CANCEL;
-      break;
-    default:
-      NS_WARNING("Did not assign a type to a MultiTouchInput");
-      break;
-    }
+  MultiTouchInput(const nsTouchEvent& aTouchEvent);
 
-    mTouches.AppendElement(SingleTouchData(0,
-                                           aMouseEvent.refPoint,
-                                           nsIntPoint(1, 1),
-                                           180.0f,
-                                           1.0f));
-  }
+  
+  
+  
+  
+  
+  
+  MultiTouchInput(const nsMouseEvent& aMouseEvent);
 
   MultiTouchType mType;
   nsTArray<SingleTouchData> mTouches;
