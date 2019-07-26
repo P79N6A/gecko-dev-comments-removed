e958300d885c5a6f091f340e4c79037f92704602
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:02:24 +0000

    Bug 968409 - Add GeckoViewComponent for UITests; r=mcomella
    UltraBlame original commit: 385d0c08dfe25c75eba936e774f6cd0960b8d6b4

---
+++ mobile/android/base/tests/components/GeckoViewComponent.java
@@ -0,0 +1,199 @@
+
+
+
+
+package org.mozilla.gecko.tests.components;
+
+import static org.mozilla.gecko.tests.helpers.AssertionHelper.*;
+
+import org.mozilla.gecko.tests.helpers.*;
+import org.mozilla.gecko.tests.UITestContext;
+import org.mozilla.gecko.R;
+
+import com.jayway.android.robotium.solo.Condition;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
+
+
+
+
+public class GeckoViewComponent extends BaseComponent {
+
+    public interface InputConnectionTest {
+        public void test(InputConnection ic, EditorInfo info);
+    }
+
+    public final TextInput mTextInput;
+
+    public GeckoViewComponent(final UITestContext testContext) {
+        super(testContext);
+        mTextInput = new TextInput();
+    }
+
+    
+
+
+    private View getView() {
+        
+        return mSolo.getView(R.id.layer_view);
+    }
+
+    private void setContext(final Context newContext) {
+        final View geckoView = getView();
+        
+        mTestContext.getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                FrameworkHelper.setViewContext(geckoView, newContext);
+            }
+        });
+    }
+
+    public class TextInput {
+        private TextInput() {
+        }
+
+        private InputMethodManager getInputMethodManager() {
+            final InputMethodManager imm = (InputMethodManager)
+                mActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            assertNotNull("Must have an InputMethodManager", imm);
+            return imm;
+        }
+
+        
+
+
+        private boolean isActive() {
+            return getInputMethodManager().isActive(getView());
+        }
+
+        public TextInput assertActive() {
+            assertTrue("Current view should be the active input view", isActive());
+            return this;
+        }
+
+        public TextInput waitForActive() {
+            WaitHelper.waitFor("current view to become the active input view", new Condition() {
+                @Override
+                public boolean isSatisfied() {
+                    return isActive();
+                }
+            });
+            return this;
+        }
+
+        
+
+
+
+
+
+        private boolean hasInputConnection() {
+            final InputMethodManager imm = getInputMethodManager();
+            return imm.isActive(getView()) && imm.isAcceptingText();
+        }
+
+        public TextInput assertInputConnection() {
+            assertTrue("Current view should have an active InputConnection", hasInputConnection());
+            return this;
+        }
+
+        public TextInput waitForInputConnection() {
+            WaitHelper.waitFor("current view to have an active InputConnection", new Condition() {
+                @Override
+                public boolean isSatisfied() {
+                    return hasInputConnection();
+                }
+            });
+            return this;
+        }
+
+        
+
+
+
+
+
+
+
+
+
+        public TextInput testInputConnection(final InputConnectionTest test) {
+
+            assertNotNull("Test must not be null", test);
+            assertInputConnection();
+
+            
+            
+            final View geckoView = getView();
+            final Handler inputConnectionHandler = geckoView.getHandler();
+            final Context oldGeckoViewContext = FrameworkHelper.getViewContext(geckoView);
+
+            setContext(new ContextWrapper(oldGeckoViewContext) {
+                @Override
+                public Object getSystemService(String name) {
+                    if (Context.INPUT_METHOD_SERVICE.equals(name)) {
+                        return null;
+                    }
+                    return super.getSystemService(name);
+                }
+            });
+
+            (new InputConnectionTestRunner(test)).runOnHandler(inputConnectionHandler);
+
+            setContext(oldGeckoViewContext);
+            return this;
+        }
+
+        private class InputConnectionTestRunner implements Runnable {
+            private final InputConnectionTest mTest;
+            private boolean mDone;
+
+            public InputConnectionTestRunner(final InputConnectionTest test) {
+                mTest = test;
+            }
+
+            public synchronized void runOnHandler(final Handler inputConnectionHandler) {
+                
+                
+                
+                
+                assertNotSame("InputConnection should not be running on instrumentation thread",
+                    Looper.myLooper(), inputConnectionHandler.getLooper());
+
+                mDone = false;
+                inputConnectionHandler.post(this);
+                do {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        
+                    }
+                } while (!mDone);
+            }
+
+            @Override
+            public void run() {
+                final EditorInfo info = new EditorInfo();
+                final InputConnection ic = getView().onCreateInputConnection(info);
+                assertNotNull("Must have an InputConnection", ic);
+                
+                ic.clearMetaKeyStates(-1);
+                ic.finishComposingText();
+                mTest.test(ic, info);
+                synchronized (this) {
+                    
+                    mDone = true;
+                    notify();
+                }
+            }
+        }
+    }
+}
