680c3243cfe1644dca5995e99c596ae5a6b33f5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:07:58 +0000

    Bug 844431 - B2G MMS: provide nsIDOMMobileMessageManager interface (with sendMMS() first) (part 3-2, nsIMobileMessageCallback.notifyMessageSent()). r=vicamo a=leo+
    UltraBlame original commit: eae203b80dd0001fc5c9ff017621d6716727268b

---
+++ dom/mobilemessage/src/MobileMessageCallback.h
@@ -0,0 +1,37 @@
+
+
+
+
+
+#ifndef mozilla_dom_mobilemessage_MobileMessageCallback_h
+#define mozilla_dom_mobilemessage_MobileMessageCallback_h
+
+#include "nsIMobileMessageCallback.h"
+#include "nsCOMPtr.h"
+#include "DOMRequest.h"
+
+class nsIDOMMozMmsMessage;
+
+namespace mozilla {
+namespace dom {
+namespace mobilemessage {
+
+class MobileMessageCallback MOZ_FINAL : public nsIMobileMessageCallback
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIMOBILEMESSAGECALLBACK
+
+  MobileMessageCallback(DOMRequest* aDOMRequest);
+
+private:
+  ~MobileMessageCallback();
+
+  nsRefPtr<DOMRequest> mDOMRequest;
+};
+
+} 
+} 
+} 
+
+#endif 
