899a6ae894dd898e64ac98cfa268c8a22a2c4dd1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:34:14 +0000

    Bug 878300 - Switch CustomizeMode.jsm and CustomizableUI.jsm to use Console.jsm. r=Unfocused
    UltraBlame original commit: 9ea1258609dc461dd7874d7603b8de71b2b0c314

---
+++ browser/components/customizableui/src/logging.js
@@ -0,0 +1,25 @@
+#if 0
+
+
+
+#endif
+
+XPCOMUtils.defineLazyModuleGetter(this, "console",
+  "resource://gre/modules/devtools/Console.jsm");
+
+let gDebug = false;
+try {
+  gDebug = Services.prefs.getBoolPref(kPrefCustomizationDebug);
+} catch (e) {}
+
+function LOG(...args) {
+  if (gDebug) {
+    args.unshift(gModuleName);
+    console.log.apply(console, args);
+  }
+}
+
+function ERROR(...args) {
+  args.unshift(gModuleName);
+  console.error.apply(console, args);
+}
