c8df7cb9c06003c5bdb216d91df452afbe336095
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:23:22 +0000

    Bug 892837 - Support permissions in desktop webrt. ContentPermission.js changes. r=myk
    UltraBlame original commit: 3bfa589adaae10ac020e08fdfff6f9f6f791373b

--- webapprt/ContentPermission.js
+++ webapprt/ContentPermission.js
@@ -6,6 +6,8 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
 
+const UNKNOWN_FAIL = ["geolocation", "desktop-notification"];
+
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://webapprt/modules/WebappRT.jsm");
@@ -16,64 +18,83 @@ ContentPermission.prototype = {
   classID: Components.ID("{07ef5b2e-88fb-47bd-8cec-d3b0bef11ac4}"),
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPermissionPrompt]),
 
+  _getChromeWindow: function(aWindow) { 
+    return aWindow
+      .QueryInterface(Ci.nsIInterfaceRequestor)
+      .getInterface(Ci.nsIWebNavigation)
+      .QueryInterface(Ci.nsIDocShellTreeItem)
+      .rootTreeItem
+      .QueryInterface(Ci.nsIInterfaceRequestor)
+      .getInterface(Ci.nsIDOMWindow)
+      .QueryInterface(Ci.nsIDOMChromeWindow);
+  },
+
   prompt: function(request) {
     
-    if (request.type != "geolocation") {
-      return;
-    }
+    let result =
+      Services.perms.testExactPermissionFromPrincipal(request.principal,
+                                                      request.type);
 
     
-    let result = Services.perms.testExactPermissionFromPrincipal(request.principal, "geo");
+    
+    
+    if ((result == Ci.nsIPermissionManager.UNKNOWN_ACTION ||
+         result == Ci.nsIPermissionManager.PROMPT_ACTION) &&
+        request.type == "geolocation") {
+      let geoResult = Services.perms.testExactPermission(request.principal.URI,
+                                                         "geo");
+      
+      
+      if (geoResult == Ci.nsIPermissionManager.ALLOW_ACTION ||
+          geoResult == Ci.nsIPermissionManager.DENY_ACTION) {
+        result = geoResult;
+      }
+    }
+
     if (result == Ci.nsIPermissionManager.ALLOW_ACTION) {
       request.allow();
       return;
-    }
-    else if (result == Ci.nsIPermissionManager.DENY_ACTION) {
+    } else if (result == Ci.nsIPermissionManager.DENY_ACTION ||
+               (result == Ci.nsIPermissionManager.UNKNOWN_ACTION &&
+                UNKNOWN_FAIL.indexOf(request.type) >= 0)) {
       request.cancel();
       return;
-    }
-
-    function getChromeWindow(aWindow) {
-      var chromeWin = aWindow
-        .QueryInterface(Ci.nsIInterfaceRequestor)
-        .getInterface(Ci.nsIWebNavigation)
-        .QueryInterface(Ci.nsIDocShellTreeItem)
-        .rootTreeItem
-        .QueryInterface(Ci.nsIInterfaceRequestor)
-        .getInterface(Ci.nsIDOMWindow)
-        .QueryInterface(Ci.nsIDOMChromeWindow);
-      return chromeWin;
     }
 
     
     let {name} = WebappRT.config.app.manifest;
     let requestingWindow = request.window.top;
-    let chromeWin = getChromeWindow(requestingWindow);
+    let chromeWin = this._getChromeWindow(requestingWindow);
     let bundle = Services.strings.createBundle("chrome://webapprt/locale/webapp.properties");
 
     
     let remember = {value: false};
     let choice = Services.prompt.confirmEx(
       chromeWin,
-      bundle.formatStringFromName("geolocation.title", [name], 1),
-      bundle.GetStringFromName("geolocation.description"),
+      bundle.formatStringFromName(request.type + ".title", [name], 1),
+      bundle.GetStringFromName(request.type + ".description"),
       
       Ci.nsIPromptService.BUTTON_POS_1_DEFAULT |
         Ci.nsIPromptService.BUTTON_TITLE_IS_STRING * Ci.nsIPromptService.BUTTON_POS_0 |
         Ci.nsIPromptService.BUTTON_TITLE_IS_STRING * Ci.nsIPromptService.BUTTON_POS_1,
-      bundle.GetStringFromName("geolocation.sharelocation"),
-      bundle.GetStringFromName("geolocation.dontshare"),
+      bundle.GetStringFromName(request.type + ".allow"),
+      bundle.GetStringFromName(request.type + ".deny"),
       null,
-      bundle.GetStringFromName("geolocation.remember"),
+      bundle.GetStringFromName(request.type + ".remember"),
       remember);
 
-    
+    let action = Ci.nsIPermissionManager.ALLOW_ACTION;
+    if (choice != 0) {
+      action = Ci.nsIPermissionManager.DENY_ACTION;
+    }
+
     if (remember.value) {
-      let action = Ci.nsIPermissionManager.ALLOW_ACTION;
-      if (choice != 0) {
-        action = Ci.nsIPermissionManager.DENY_ACTION;
-      }
-      Services.perms.addFromPrincipal(request.principal, "geo", action);
+      
+      Services.perms.addFromPrincipal(request.principal, request.type, action);
+    } else {
+      
+      Services.perms.addFromPrincipal(request.principal, request.type, action,
+                                      Ci.nsIPermissionManager.EXPIRE_SESSION);
     }
 
     
