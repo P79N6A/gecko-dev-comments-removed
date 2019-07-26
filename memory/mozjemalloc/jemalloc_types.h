6e26eb331e0f44c913cc6da939e9663d0f0afacb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:46:52 +0000

    Bug 860254 - Part 2: Split junk settings into opt_junk (allocate) versus opt_poison (deallocate). r=glandium
    UltraBlame original commit: 5b69776cb06165d2ffe71ab3118c6f39b3b7eac2

--- memory/mozjemalloc/jemalloc_types.h
+++ memory/mozjemalloc/jemalloc_types.h
@@ -56,6 +56,7 @@ typedef struct {
 
 	jemalloc_bool	opt_abort;	
 	jemalloc_bool	opt_junk;	
+	jemalloc_bool	opt_poison;	
 	jemalloc_bool	opt_utrace;	
 	jemalloc_bool	opt_sysv;	
 	jemalloc_bool	opt_xmalloc;	
