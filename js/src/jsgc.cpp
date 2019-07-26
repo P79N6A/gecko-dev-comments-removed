ddeaf14dcfce99cec876395d23e27504fdc5d2c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:21:40 +0000

    Bug 988326 - Start GC arenas in the decommitted state; r=jonco
    UltraBlame original commit: 738a4b23019461743e0c5bbc89b98bb944bd08ad

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -667,8 +667,7 @@ ChunkPool::get(JSRuntime *rt)
         chunk = Chunk::allocate(rt);
         if (!chunk)
             return nullptr;
-        JS_ASSERT(chunk->info.numArenasFreeCommitted == ArenasPerChunk);
-        rt->gcNumArenasFreeCommitted += ArenasPerChunk;
+        JS_ASSERT(chunk->info.numArenasFreeCommitted == 0);
     }
     JS_ASSERT(chunk->unused());
     JS_ASSERT(!rt->gcChunkSet.has(chunk));
@@ -806,23 +805,18 @@ Chunk::init(JSRuntime *rt)
     bitmap.clear();
 
     
-    decommittedArenas.clear(false);
+
+
+
+    decommitAllArenas(rt);
 
     
-    info.freeArenasHead = &arenas[0].aheader;
+    info.freeArenasHead = nullptr;
     info.lastDecommittedArenaOffset = 0;
     info.numArenasFree = ArenasPerChunk;
-    info.numArenasFreeCommitted = ArenasPerChunk;
+    info.numArenasFreeCommitted = 0;
     info.age = 0;
     info.trailer.runtime = rt;
-
-    
-    for (unsigned i = 0; i < ArenasPerChunk; i++) {
-        arenas[i].aheader.setAsNotAllocated();
-        arenas[i].aheader.next = (i + 1 < ArenasPerChunk)
-                                 ? &arenas[i + 1].aheader
-                                 : nullptr;
-    }
 
     
 }
@@ -999,6 +993,7 @@ Chunk::releaseArena(ArenaHeader *aheader)
     } else {
         rt->gcChunkSet.remove(this);
         removeFromAvailableList();
+        decommitAllArenas(rt);
         rt->gcChunkPool.put(this);
     }
 }
@@ -2622,12 +2617,11 @@ GCHelperThread::threadLoop()
                 
                 if (!chunk) {
 #if JS_TRACE_LOGGING
-            logger->log(TraceLogging::GC_ALLOCATING_STOP);
+                    logger->log(TraceLogging::GC_ALLOCATING_STOP);
 #endif
                     break;
                 }
-                JS_ASSERT(chunk->info.numArenasFreeCommitted == ArenasPerChunk);
-                rt->gcNumArenasFreeCommitted += ArenasPerChunk;
+                JS_ASSERT(chunk->info.numArenasFreeCommitted == 0);
                 rt->gcChunkPool.put(chunk);
             } while (state == ALLOCATING && rt->gcChunkPool.wantBackgroundAllocation(rt));
             if (state == ALLOCATING)
