8f80dfad58695236079d91c3600fddb6bc00ecd7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:40:49 +0000

    Bug 776223 - Catch NullPointerException from Samsung's buggy clipboard API. r=blassey
    UltraBlame original commit: 1ad0d609ccd1db6d0663db79634febed3f29e6f5

--- mobile/android/base/GeckoAppShell.java
+++ mobile/android/base/GeckoAppShell.java
@@ -1297,7 +1297,14 @@ public class GeckoAppShell
                 if (android.os.Build.VERSION.SDK_INT >= 11) {
                     android.content.ClipboardManager cm = (android.content.ClipboardManager)
                         context.getSystemService(Context.CLIPBOARD_SERVICE);
-                    cm.setPrimaryClip(ClipData.newPlainText("Text", text));
+                    ClipData clip = ClipData.newPlainText("Text", text);
+                    try {
+                        cm.setPrimaryClip(clip);
+                    } catch (NullPointerException e) {
+                        
+                        
+                        
+                    }
                 } else {
                     android.text.ClipboardManager cm = (android.text.ClipboardManager)
                         context.getSystemService(Context.CLIPBOARD_SERVICE);
