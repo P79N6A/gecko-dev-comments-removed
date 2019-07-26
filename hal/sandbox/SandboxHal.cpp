4dc4a120ac798ffbb997f1a22419a13f79ea5d08
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:36:58 +0000

    Bug 819791 - Part 5: Remove an unnecessary TArray copy in hal/sandbox. r=mounir
    UltraBlame original commit: 160c727a336b0dd9b33183f46201488d92414716

--- hal/sandbox/SandboxHal.cpp
+++ hal/sandbox/SandboxHal.cpp
@@ -437,8 +437,7 @@ public:
     nsCOMPtr<nsIDOMWindow> window =
       do_QueryInterface(tabParent->GetBrowserDOMWindow());
     WindowIdentifier newID(id, window);
-    
-    hal::Vibrate(nsTArray<uint32_t>(pattern), newID);
+    hal::Vibrate(pattern, newID);
     return true;
   }
 
