144189833373126263a7230ece2fe42b157437c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:10:26 +0000

    b=851964; Odin/OSX, part 3. enable AsmJS on OSX by using new Breakpad user handler, r=luke
    UltraBlame original commit: fe29b2ae604b7e7cf570bd8c57c8d21415789c9f

--- js/src/jscntxt.h
+++ js/src/jscntxt.h
@@ -677,6 +677,18 @@ struct JSRuntime : js::RuntimeFriendFields,
     void assertValidThread() const {}
 #endif
 
+#ifdef XP_MACOSX
+    
+    
+    
+  private:
+    static PRLock *runtimeListLock_;
+    static JSRuntime *runtimeListHead_;
+    JSRuntime *runtimeListNext_;
+  public:
+    static js::AsmJSActivation* findAsmJSActivationForPC(void *pc);
+#endif
+
     
     js::StackSpace stackSpace;
 
