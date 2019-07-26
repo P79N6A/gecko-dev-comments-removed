1a383a5ee769972c5c1f2dc078d52c7067dfecc1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 21:59:43 +0000

    Bug 973563 - Correct assertion for zero-sized structs r=till
    UltraBlame original commit: cc698e2ff036745b0a62597024fb478d5f6c7b05

---
+++ js/src/jit-test/tests/TypedObject/bug973563.js
@@ -0,0 +1,9 @@
+
+
+
+if (!this.hasOwnProperty("TypedObject"))
+  quit();
+
+var PointType = new TypedObject.StructType({});
+var LineType = new TypedObject.StructType({source: PointType, target: PointType});
+var fromAToB = new LineType({source: {x: 22, y: 44}, target: {x: 66, y: 88}});
