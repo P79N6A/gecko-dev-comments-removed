9ca417c706102dfe2f9f4db4c454ff145bb9d00f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:00:14 +0000

    Bug 1010621: Remove redundant definition for helper-function 'IsCompilingAsmJS'. r=djvj
    UltraBlame original commit: dff6a8c71b3f96f8c867843852fe70ccd3936c0e

--- js/src/jit/IonMacroAssembler.cpp
+++ js/src/jit/IonMacroAssembler.cpp
@@ -1204,14 +1204,6 @@ MacroAssembler::handleFailure(ExecutionMode executionMode)
 }
 
 #ifdef DEBUG
-static inline bool
-IsCompilingAsmJS()
-{
-    
-    IonContext *ictx = MaybeGetIonContext();
-    return ictx && ictx->compartment == nullptr;
-}
-
 static void
 AssumeUnreachable_(const char *output) {
     MOZ_ReportAssertionFailure(output, __FILE__, __LINE__);
