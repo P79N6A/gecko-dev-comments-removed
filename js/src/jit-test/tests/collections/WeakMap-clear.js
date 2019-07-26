1f4086b58ce7263bf884a2dfa3b83cdf7c1c54be
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:33:42 +0000

    Bug 814562 - Implement clear() on WeakMaps
    UltraBlame original commit: b5fa6a857c3529d1f4658c0e24551a74c7d7896e

---
+++ js/src/jit-test/tests/collections/WeakMap-clear.js
@@ -0,0 +1,42 @@
+var key = {};
+var wm = WeakMap();
+
+assertEq(wm.has(key), false);
+
+wm.clear();
+assertEq(wm.has(key), false);
+
+
+wm.set(key, 42);
+assertEq(wm.has(key), true);
+wm.clear();
+assertEq(wm.has(key), false);
+
+
+wm.set(key, {});
+for (var i = 0; i < 10; i++)
+    wm.set({}, {});
+assertEq(wm.has(key), true);
+wm.clear();
+assertEq(wm.has(key), false);
+
+
+wm.set(key, {});
+for (var i = 0; i < 10; i++)
+    wm.set({}, {});
+assertEq(wm.has(key), true);
+gc();
+assertEq(wm.has(key), true);
+wm.clear();
+assertEq(wm.has(key), false);
+
+
+wm.set(key, {});
+key = null;
+wm.clear();
+gc();
+var key2 = {};
+wm.set(key2, {});
+key2 = null;
+gc();
+wm.clear();
