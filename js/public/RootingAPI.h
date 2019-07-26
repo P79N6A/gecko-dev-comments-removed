920bcf9f3f4ac09450c84e2668626edca8bb25da
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:13:47 +0000

    Bug 899976 - GC: Fix unsafe references related to ToInt* functions - js engine changes r=sfink
    UltraBlame original commit: f09bcd847699540773617fb28836189e921b04ab

--- js/public/RootingAPI.h
+++ js/public/RootingAPI.h
@@ -508,9 +508,13 @@ class MOZ_STACK_CLASS MutableHandle : public js::MutableHandleBase<T>
     }
 
     T *address() const { return ptr; }
-    T get() const { return *ptr; }
+    const T& get() const { return *ptr; }
 
-    operator T() const { return get(); }
+    
+
+
+
+    operator const T&() const { return get(); }
     T operator->() const { return get(); }
 
   private:
