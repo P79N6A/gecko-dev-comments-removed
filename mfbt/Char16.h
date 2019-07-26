6a0f7bf1cbda1db73b7b2aadf5c8e72c1111c0cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:39:35 +0000

    Bug 796948 - Create Char16.h header file for char16_t type. r=waldo
    UltraBlame original commit: 793c5c49bd9b8b2a9a6a9a40283bbcc04d252a8c

---
+++ mfbt/Char16.h
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+#ifndef mozilla_Char16_h_
+#define mozilla_Char16_h_
+
+#include "mozilla/Assertions.h"
+
+
+
+
+
+
+
+
+
+
+
+#ifdef _MSC_VER
+   
+
+
+
+
+
+
+#  define MOZ_UTF16(s) L##s
+#  include <yvals.h>
+#  define char16_t wchar_t
+#elif defined(__cplusplus) && \
+      (__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
+   
+#  define MOZ_UTF16(s) u##s
+#else
+#  error "Char16.h requires C++11 (or something like it) for UTF-16 support."
+#endif
+
+MOZ_STATIC_ASSERT(sizeof(char16_t) == 2, "Is char16_t type 16 bits?");
+MOZ_STATIC_ASSERT(sizeof(MOZ_UTF16('A')) == 2, "Is char literal 16 bits?");
+MOZ_STATIC_ASSERT(sizeof(MOZ_UTF16("")[0]) == 2, "Is string char 16 bits?");
+
+#endif 
