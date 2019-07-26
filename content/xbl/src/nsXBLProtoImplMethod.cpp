8eeab4dc10eb8a106803428cbdc6e699a0d59363
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:13:15 +0000

    Bug 855411 - Root CompileMember. r=bz,terrence
    UltraBlame original commit: 4bda5dd4a307734475b13c365aa9f71cdf344449

--- content/xbl/src/nsXBLProtoImplMethod.cpp
+++ content/xbl/src/nsXBLProtoImplMethod.cpp
@@ -135,7 +135,7 @@ nsXBLProtoImplMethod::InstallMember(JSContext* aCx,
 
 nsresult 
 nsXBLProtoImplMethod::CompileMember(nsIScriptContext* aContext, const nsCString& aClassStr,
-                                    JSObject* aClassObject)
+                                    JS::Handle<JSObject*> aClassObject)
 {
   NS_PRECONDITION(!IsCompiled(),
                   "Trying to compile an already-compiled method");
@@ -196,7 +196,6 @@ nsXBLProtoImplMethod::CompileMember(nsIScriptContext* aContext, const nsCString&
     functionUri.Truncate(hash);
   }
 
-  JSObject* methodObject = nullptr;
   AutoPushJSContext cx(aContext->GetNativeContext());
   JSAutoRequest ar(cx);
   JSAutoCompartment ac(cx, aClassObject);
@@ -206,10 +205,11 @@ nsXBLProtoImplMethod::CompileMember(nsIScriptContext* aContext, const nsCString&
          .setVersion(JSVERSION_LATEST)
          .setUserBit(true); 
   JS::RootedObject rootedNull(cx, nullptr); 
+  JS::RootedObject methodObject(cx);
   nsresult rv = nsJSUtils::CompileFunction(cx, rootedNull, options, cname,
                                            paramCount,
                                            const_cast<const char**>(args),
-                                           body, &methodObject);
+                                           body, methodObject.address());
 
   
   delete uncompiledMethod;
