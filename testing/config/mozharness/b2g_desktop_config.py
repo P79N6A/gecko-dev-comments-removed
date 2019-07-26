f02a208edea393c138b41bda718234b0071d94e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:18:01 +0000

    Bug 981030 - Create platform specific mozharness files for storing test harness options, r=aki
    UltraBlame original commit: 284661ffd813d11983da8de8457e1a976259c198

---
+++ testing/config/mozharness/b2g_desktop_config.py
@@ -0,0 +1,18 @@
+
+
+
+
+config = {
+    "mochitest_options": [
+        "--console-level=INFO", "%(test_manifest)s",
+        "--total-chunks=%(total_chunks)s", "--this-chunk=%(this_chunk)s",
+        "--profile=%(gaia_profile)s", "--app=%(application)s", "--desktop",
+        "--utility-path=%(utility_path)s", "--certificate-path=%(cert_path)s",
+        "--symbols-path=%(symbols_path)s",
+    ],
+
+    "reftest_options": [
+        "--desktop", "--profile=%(gaia_profile)s", "--appname=%(application)s",
+        "--symbols-path=%(symbols_path)s", "%(test_manifest)s",
+    ]
+}
---
+++ testing/config/mozharness/linux_config.py
@@ -0,0 +1,34 @@
+
+
+
+
+config = {
+    "reftest_options": [
+        "--appname=%(binary_path)s", "--utility-path=tests/bin",
+        "--extra-profile-file=tests/bin/plugins", "--symbols-path=%(symbols_path)s"
+    ],
+    "mochitest_options": [
+        "--appname=%(binary_path)s", "--utility-path=tests/bin",
+        "--extra-profile-file=tests/bin/plugins", "--symbols-path=%(symbols_path)s",
+        "--certificate-path=tests/certs", "--autorun", "--close-when-done",
+        "--console-level=INFO", "--setpref=webgl.force-enabled=true"
+    ],
+    "xpcshell_options": [
+        "--symbols-path=%(symbols_path)s",
+        "--test-plugin-path=%(test_plugin_path)s"
+    ],
+    "cppunittest_options": [
+        "--symbols-path=%(symbols_path)s",
+        "--xre-path=%(abs_app_dir)s"
+    ],
+    "jittest_options": [
+        "tests/bin/js",
+        "--no-slow",
+        "--no-progress",
+        "--tinderbox",
+        "--tbpl"
+    ],
+    "mozbase_options": [
+        "-b", "%(binary_path)s"
+    ],
+}
