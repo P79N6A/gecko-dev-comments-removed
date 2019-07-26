2d70dca0b074a703b4bd30ad0e0fdd613be5c276
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:41:33 +0000

    Bug 935525: Added Marionette test for turning NFC on and off, r=vyang
    UltraBlame original commit: 8fd157ab36f65d49176fc3c16bb49e80a4877d4b

---
+++ dom/nfc/tests/marionette/test_nfc_enabled.js
@@ -0,0 +1,64 @@
+
+
+
+MARIONETTE_TIMEOUT = 30000;
+
+function toggleNFC(enabled, callback) {
+  isnot(callback, null);
+  var settings = window.navigator.mozSettings;
+  isnot(settings, null);
+  ok(settings instanceof SettingsManager,
+     'settings instanceof ' + settings.constructor +
+     ', expected SettingsManager');
+
+  let req = settings.createLock().get('nfc.enabled');
+  req.onsuccess = function() {
+    if (req.result['nfc.enabled'] === enabled) {
+      callback();
+    } else {
+      let req = settings.createLock().set({'nfc.enabled': enabled});
+      req.onsuccess = function() {
+        window.setTimeout(callback, 5000); 
+      };
+      req.onerror = function() {
+        ok(false,
+           'Setting \'nfc.enabled\' to \'' + enabled +
+           '\' failed, error ' + req.error.name);
+        finish();
+      };
+    }
+  };
+  req.onerror = function() {
+    ok(false, 'Getting \'nfc.enabled\' failed, error ' + req.error.name);
+    finish();
+  };
+}
+
+function testEnableNFC() {
+  toggleNFC(true, runNextTest);
+}
+
+function testDisableNFC() {
+  toggleNFC(false, runNextTest);
+}
+
+let tests = [
+  testEnableNFC,
+  testDisableNFC
+];
+
+function runNextTest() {
+  let test = tests.shift();
+  if (!test) {
+    cleanUp();
+    return;
+  }
+  test();
+}
+
+function cleanUp() {
+  finish();
+}
+
+SpecialPowers.pushPermissions(
+  [{'type': 'settings', 'allow': true, 'context': document}], runNextTest);
