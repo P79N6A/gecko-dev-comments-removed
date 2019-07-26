2029a31d9cae398db730c70e76171b657eeaae55
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:08:25 +0000

    Bug 157846. nsTextControlFrame's padding should be inherited into its anonymous child so it appears inside the scroll area. r=roc
    The new code in nsTextControlFrame was written by Charly Molter :lahabana <charly.molter@gmail.com>
    
    UltraBlame original commit: 7273dbeaeb88a1fcc5ccf1086aff6c7d2932a373

--- layout/forms/nsTextControlFrame.cpp
+++ layout/forms/nsTextControlFrame.cpp
@@ -190,17 +190,6 @@ nsTextControlFrame::CalcIntrinsicSize(nsRenderingContext* aRenderingContext,
     if (PresContext()->CompatibilityMode() == eCompatibility_FullStandards) {
       aIntrinsicSize.width += 1;
     }
-
-    
-    
-    
-    nsMargin childPadding;
-    nsIFrame* firstChild = GetFirstPrincipalChild();
-    if (firstChild && firstChild->StylePadding()->GetPadding(childPadding)) {
-      aIntrinsicSize.width += childPadding.LeftRight();
-    } else {
-      NS_ERROR("Percentage padding on value div?");
-    }
   }
 
   
@@ -535,32 +524,28 @@ nsTextControlFrame::ReflowTextControlChild(nsIFrame*                aKid,
                                            nsHTMLReflowMetrics& aParentDesiredSize)
 {
   
-  nsSize availSize(aReflowState.ComputedWidth(), 
-                   aReflowState.ComputedHeight());
-  availSize.width = std::max(availSize.width, 0);
-  availSize.height = std::max(availSize.height, 0);
+  nsSize availSize(aReflowState.ComputedWidth() +
+                   aReflowState.ComputedPhysicalPadding().LeftRight(),
+                   aReflowState.ComputedHeight() +
+                   aReflowState.ComputedPhysicalPadding().TopBottom());
   
   nsHTMLReflowState kidReflowState(aPresContext, aReflowState, 
                                    aKid, availSize);
 
   
-  nscoord width = availSize.width;
-  width -= kidReflowState.ComputedPhysicalMargin().LeftRight() +
-              kidReflowState.ComputedPhysicalBorderPadding().LeftRight();
-  width = std::max(width, 0);
-  kidReflowState.SetComputedWidth(width);
-
-  nscoord height = availSize.height;
-  height -= kidReflowState.ComputedPhysicalMargin().TopBottom() +
-              kidReflowState.ComputedPhysicalBorderPadding().TopBottom();
-  height = std::max(height, 0);       
-  kidReflowState.SetComputedHeight(height); 
+  
+  kidReflowState.SetComputedHeight(aReflowState.ComputedHeight());
+  kidReflowState.SetComputedWidth(aReflowState.ComputedWidth());
 
   
-  nscoord xOffset = aReflowState.ComputedPhysicalBorderPadding().left
-                      + kidReflowState.ComputedPhysicalMargin().left;
-  nscoord yOffset = aReflowState.ComputedPhysicalBorderPadding().top
-                      + kidReflowState.ComputedPhysicalMargin().top;
+  kidReflowState.SetComputedHeight(aReflowState.ComputedHeight());
+  kidReflowState.SetComputedWidth(aReflowState.ComputedWidth());
+
+  
+  nscoord xOffset = aReflowState.ComputedPhysicalBorderPadding().left -
+                    aReflowState.ComputedPhysicalPadding().left;
+  nscoord yOffset = aReflowState.ComputedPhysicalBorderPadding().top -
+                    aReflowState.ComputedPhysicalPadding().top;
 
   
   nsHTMLReflowMetrics desiredSize(aReflowState.GetWritingMode());
