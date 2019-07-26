095a5ab29b372d655119a7af04c9ee14c04bc1bf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:42:04 +0000

    Bug 939680 - Implement nsINetworkLinkService.linkType on Android r=blassey
    UltraBlame original commit: 86253e0141c81a1254778525560e4ed917022531

--- netwerk/system/android/nsAndroidNetworkLinkService.cpp
+++ netwerk/system/android/nsAndroidNetworkLinkService.cpp
@@ -49,7 +49,13 @@ nsAndroidNetworkLinkService::GetLinkType(uint32_t *aLinkType)
 {
   NS_ENSURE_ARG_POINTER(aLinkType);
 
-  
-  *aLinkType = nsINetworkLinkService::LINK_TYPE_UNKNOWN;
+  if (!mozilla::AndroidBridge::Bridge()) {
+    
+    NS_WARNING("GetLinkType is not supported without a bridge connection");
+    *aLinkType = nsINetworkLinkService::LINK_TYPE_UNKNOWN;
+    return NS_OK;
+  }
+
+  *aLinkType = mozilla::AndroidBridge::Bridge()->NetworkLinkType();
   return NS_OK;
 }
