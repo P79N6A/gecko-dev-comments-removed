e5fe2c8f8263e9ec1f089c9652f6c955ef51382b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:04:31 +0000

    Bug 793025: Convert FileReaderSync to WebIDL bindings. r=bz
    UltraBlame original commit: 4050caa0b9c5684cbabfb37c55c788da955d071f

--- dom/workers/FileReaderSync.h
+++ dom/workers/FileReaderSync.h
@@ -8,17 +8,51 @@
 #define mozilla_dom_workers_filereadersync_h__
 
 #include "Workers.h"
+#include "mozilla/dom/workers/bindings/DOMBindingBase.h"
 
-#include "jspubtd.h"
+#include "nsICharsetDetectionObserver.h"
+#include "nsStringGlue.h"
+#include "mozilla/Attributes.h"
+#include "mozilla/dom/BindingUtils.h"
+
+class nsIInputStream;
+class nsIDOMBlob;
 
 BEGIN_WORKERS_NAMESPACE
 
-namespace filereadersync {
+class FileReaderSync MOZ_FINAL : public DOMBindingBase,
+                                 public nsICharsetDetectionObserver
+{
+  nsCString mCharset;
+  nsresult ConvertStream(nsIInputStream *aStream, const char *aCharset,
+                         nsAString &aResult);
+  nsresult GuessCharset(nsIInputStream *aStream, nsACString &aCharset);
 
-bool
-InitClass(JSContext* aCx, JSObject* aGlobal);
+public:
+  virtual void
+  _trace(JSTracer* aTrc) MOZ_OVERRIDE;
 
-} 
+  virtual void
+  _finalize(JSFreeOp* aFop) MOZ_OVERRIDE;
+
+  static FileReaderSync*
+  Constructor(JSContext* aCx, JSObject* aGlobal, ErrorResult& aRv);
+
+  NS_DECL_ISUPPORTS_INHERITED
+
+  FileReaderSync(JSContext* aCx);
+
+  JSObject* ReadAsArrayBuffer(JSContext* aCx, JSObject* aBlob,
+                              ErrorResult& aRv);
+  void ReadAsBinaryString(JSObject* aBlob, nsAString& aResult,
+                          ErrorResult& aRv);
+  void ReadAsText(JSObject* aBlob, const Optional<nsAString>& aEncoding,
+                  nsAString& aResult, ErrorResult& aRv);
+  void ReadAsDataURL(JSObject* aBlob, nsAString& aResult, ErrorResult& aRv);
+
+  
+  NS_IMETHOD Notify(const char *aCharset, nsDetectionConfident aConf);
+};
 
 END_WORKERS_NAMESPACE
 
