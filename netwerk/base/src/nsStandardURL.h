5e6124784b35206a7d690502fa47a5d16203586d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:17:40 +0000

    Bug 1020041 - host limit in nsStandardURL, r=jduell, r=valentin
    UltraBlame original commit: 412a7f011ebbecd33f614b1c9af5e7aea86e9358

--- netwerk/base/src/nsStandardURL.h
+++ netwerk/base/src/nsStandardURL.h
@@ -226,6 +226,9 @@ private:
 
     static void PrefsChanged(nsIPrefBranch *prefs, const char *pref);
 
+    void FindHostLimit(nsACString::const_iterator& aStart,
+                       nsACString::const_iterator& aEnd);
+
     
     nsCString mSpec;
     int32_t   mDefaultPort;
@@ -270,6 +273,7 @@ private:
     
     
     static nsIIDNService               *gIDN;
+    static char                         gHostLimitDigits[];
     static bool                         gInitialized;
     static bool                         gEscapeUTF8;
     static bool                         gAlwaysEncodeInUTF8;
