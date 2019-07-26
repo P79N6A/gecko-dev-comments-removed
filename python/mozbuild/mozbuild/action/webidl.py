9e970a9f351902f2bb0b7005ce0ed10417258a3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:42:45 +0000

    Bug 928195 - Part 6: Rewrite WebIDL build system integration
    WebIDL build system integration has been rewritten from the ground up.
    Changes:
    
    * GlobalGen.py, BindingGen.py, and ExampleGen.py have been removed in
      favor of mozwebidl.py.
    
    * Static .webidl files are now processed directly in their original location
      and aren't copied to the object directory.
    
    * Generated events <stem>.cpp files are now compiled into the unified
      sources. Previously, only the <stem>Binding.cpp files were compiled
      into unified sources.
    
    * Exported .h files are now generated directly into their final location.
      Previously, they were generated into the local directory then
      installed in their final location.
    
    * The list of globalgen-generated files now lives in Python and isn't
      duplicated in 3 places.
    
    * The make dependencies are much simpler as a result of using a single
      command to perform all code generation. The auto-generated .pp file from
      code generation sets up all dependencies necessary to reinvoke code
      generation and Python takes care of dependency management.
    
    UltraBlame original commit: 608c663f691f39bba13ded313c89e8109c061d5c

---
+++ python/mozbuild/mozbuild/action/webidl.py
@@ -0,0 +1,17 @@
+
+
+
+
+import sys
+
+from mozwebidl import BuildSystemWebIDL
+
+
+def main(argv):
+    """Perform WebIDL code generation required by the build system."""
+    manager = BuildSystemWebIDL.from_environment().manager
+    manager.generate_build_files()
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
