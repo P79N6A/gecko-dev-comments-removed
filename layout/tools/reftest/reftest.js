53f375da278a68a30fae16c642a029200de693d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:54:04 +0000

    Bug 877266 - Add AndroidVersion to reftests and modify manifests for pandaboards. r=jmaher
    UltraBlame original commit: ef53f8a79327793c3c97bd67da354383677431e1

--- layout/tools/reftest/reftest.js
+++ layout/tools/reftest/reftest.js
@@ -574,6 +574,14 @@ function BuildConditionSandbox(aURL) {
     sandbox.qtWidget = xr.widgetToolkit == "qt";
     sandbox.winWidget = xr.widgetToolkit == "windows";
 
+    if (sandbox.Android) {
+        var sysInfo = CC["@mozilla.org/system-info;1"].getService(CI.nsIPropertyBag2);
+
+        
+        
+        sandbox.AndroidVersion = sysInfo.getPropertyAsInt32("version");
+    }
+
 #if MOZ_ASAN
     sandbox.AddressSanitizer = true;
 #else
