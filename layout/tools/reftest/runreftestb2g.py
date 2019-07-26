182471e438b4414203af4cb5967d6c6d3185825f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:25:08 +0000

    Bug 899171 - Use mozprofile in the reftest harness. r=ahal
    UltraBlame original commit: de107e9ab150ed0e3e767f27c4716343bbac4357

--- layout/tools/reftest/runreftestb2g.py
+++ layout/tools/reftest/runreftestb2g.py
@@ -392,41 +392,39 @@ class B2GReftest(RefTest):
             pass
 
 
-    def createReftestProfile(self, options, profileDir, reftestlist):
-        print "profileDir: " + str(profileDir)
-        retVal = RefTest.createReftestProfile(self, options, profileDir, reftestlist, server=options.remoteWebServer)
+    def createReftestProfile(self, options, reftestlist):
+        profile = RefTest.createReftestProfile(self, options, reftestlist,
+                                               server=options.remoteWebServer,
+                                               special_powers=False)
+        profileDir = profile.profile
 
+        prefs = {}
         
-        fhandle = open(os.path.join(profileDir, "user.js"), 'a')
-        fhandle.write("""
-user_pref("browser.firstrun.show.localepicker", false);
-user_pref("browser.homescreenURL","app://system.gaiamobile.org");\n
-user_pref("browser.manifestURL","app://system.gaiamobile.org/manifest.webapp");\n
-user_pref("browser.tabs.remote", false);\n
-user_pref("dom.ipc.browser_frames.oop_by_default", true);\n
-user_pref("dom.ipc.tabs.disabled", false);\n
-user_pref("dom.mozBrowserFramesEnabled", true);\n
-user_pref("dom.mozBrowserFramesWhitelist","app://system.gaiamobile.org");\n
-user_pref("network.dns.localDomains","app://system.gaiamobile.org");\n
-user_pref("font.size.inflation.emPerLine", 0);
-user_pref("font.size.inflation.minTwips", 0);
-user_pref("reftest.browser.iframe.enabled", false);
-user_pref("reftest.remote", true);
-user_pref("reftest.uri", "%s");
-// Set a future policy version to avoid the telemetry prompt.
-user_pref("toolkit.telemetry.prompted", 999);
-user_pref("toolkit.telemetry.notifiedOptOut", 999);
-""" % reftestlist)
+        prefs["browser.firstrun.show.localepicker"] = False
+        prefs["browser.homescreenURL"] = "app://system.gaiamobile.org"
+        prefs["browser.manifestURL"] = "app://system.gaiamobile.org/manifest.webapp"
+        prefs["browser.tabs.remote"] = False
+        prefs["dom.ipc.browser_frames.oop_by_default"] = True
+        prefs["dom.ipc.tabs.disabled"] = False
+        prefs["dom.mozBrowserFramesEnabled"] = True
+        prefs["dom.mozBrowserFramesWhitelist"] = "app://system.gaiamobile.org"
+        prefs["network.dns.localDomains"] = "app://system.gaiamobile.org"
+        prefs["font.size.inflation.emPerLine"] = 0
+        prefs["font.size.inflation.minTwips"] = 0
+        prefs["reftest.browser.iframe.enabled"] = False
+        prefs["reftest.remote"] = True
+        prefs["reftest.uri"] = "%s" % reftestlist
+        
+        prefs["toolkit.telemetry.prompted"] = 999
+        prefs["toolkit.telemetry.notifiedOptOut"] = 999
 
         
         if getattr(options, 'enablePrivilege', False):
-            fhandle.write("""
-user_pref("capability.principal.codebase.p2.granted", "UniversalXPConnect");
-user_pref("capability.principal.codebase.p2.id", "http://%s:%s");
-""" % (options.remoteWebServer, options.httpPort))
+            prefs["capability.principal.codebase.p2.granted"] = "UniversalXPConnect"
+            prefs["capability.principal.codebase.p2.id"] = "http://%s:%s" % (options.remoteWebServer, options.httpPort)
 
         
-        fhandle.close()
+        profile.set_preferences(prefs)
 
         
         self._devicemanager.removeDir(self.remoteProfile)
@@ -458,10 +456,11 @@ user_pref("capability.principal.codebase.p2.id", "http://%s:%s");
         self.updateProfilesIni(self.remoteProfile)
 
         options.profilePath = self.remoteProfile
-        return retVal
+        return profile
 
-    def copyExtraFilesToProfile(self, options, profileDir):
-        RefTest.copyExtraFilesToProfile(self, options, profileDir)
+    def copyExtraFilesToProfile(self, options, profile):
+        profileDir = profile.profile
+        RefTest.copyExtraFilesToProfile(self, options, profile)
         try:
             self._devicemanager.pushDir(profileDir, options.remoteProfile)
         except DMError:
