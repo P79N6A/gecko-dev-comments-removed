48dfa4cabd563faa2bbc7cc97205480c5208e611
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:45:37 +0000

    Bug 904953 - Splits browser_inspector_markup_edit.js into 5 to avoid long running test, r=jwalker
    UltraBlame original commit: e30953666622062ee0d1f5cf03c1822188442111

--- browser/devtools/markupview/test/head.js
+++ browser/devtools/markupview/test/head.js
@@ -8,6 +8,7 @@ let {devtools} = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
 let TargetFactory = devtools.TargetFactory;
 let {console} = Cu.import("resource://gre/modules/devtools/Console.jsm", {});
 let promise = devtools.require("sdk/core/promise");
+let {getInplaceEditorForSpan: inplaceEditor} = devtools.require("devtools/shared/inplace-editor");
 
 
 function clearUserPrefs() {
@@ -18,15 +19,23 @@ function clearUserPrefs() {
 
 registerCleanupFunction(clearUserPrefs);
 
-Services.prefs.setBoolPref("devtools.debugger.log", true);
-SimpleTest.registerCleanupFunction(() => {
-  Services.prefs.clearUserPref("devtools.debugger.log");
-});
 
-function getContainerForRawNode(markupView, rawNode) {
-  let front = markupView.walker.frontForRawNode(rawNode);
-  let container = markupView.getContainer(front);
-  return container;
+
+
+
+
+function addTab(url) {
+  let def = promise.defer();
+
+  gBrowser.selectedTab = gBrowser.addTab();
+  gBrowser.selectedBrowser.addEventListener("load", function onload() {
+    gBrowser.selectedBrowser.removeEventListener("load", onload, true);
+    info("URL " + url + " loading complete into new test tab");
+    waitForFocus(def.resolve, content);
+  }, true);
+  content.location = url;
+
+  return def.promise;
 }
 
 
@@ -34,18 +43,40 @@ function getContainerForRawNode(markupView, rawNode) {
 
 
 function openInspector() {
-  let deferred = promise.defer();
+  let def = promise.defer();
 
   let target = TargetFactory.forTab(gBrowser.selectedTab);
   gDevTools.showToolbox(target, "inspector").then(function(toolbox) {
+    info("Toolbox open");
     let inspector = toolbox.getCurrentPanel();
     inspector.once("inspector-updated", () => {
-      deferred.resolve({toolbox: toolbox, inspector: inspector});
+      info("Inspector panel active and ready");
+      def.resolve({toolbox: toolbox, inspector: inspector});
     });
   }).then(null, console.error);
 
-  return deferred.promise;
+  return def.promise;
 }
+
+
+
+
+
+
+
+
+function getContainerForRawNode(markupView, rawNode) {
+  let front = markupView.walker.frontForRawNode(rawNode);
+  let container = markupView.getContainer(front);
+  ok(container, "A markup-container object was found");
+  return container;
+}
+
+
+
+
+
+
 
 function getNode(nodeOrSelector) {
   let node = nodeOrSelector;
@@ -64,10 +95,14 @@ function getNode(nodeOrSelector) {
 
 
 
-function selectNode(nodeOrSelector, inspector) {
+
+
+
+function selectNode(nodeOrSelector, inspector, reason="test") {
+  info("Selecting the node " + nodeOrSelector);
   let node = getNode(nodeOrSelector);
   let updated = inspector.once("inspector-updated");
-  inspector.selection.setNode(node, "test");
+  inspector.selection.setNode(node, reason);
   return updated;
 }
 
@@ -77,7 +112,10 @@ function selectNode(nodeOrSelector, inspector) {
 
 
 
+
+
 function hoverContainer(nodeOrSelector, inspector) {
+  info("Hovering over the markup-container for node " + nodeOrSelector);
   let highlit = inspector.toolbox.once("node-highlight");
   let container = getContainerForRawNode(inspector.markup, getNode(nodeOrSelector));
   EventUtils.synthesizeMouse(container.tagLine, 2, 2, {type: "mousemove"},
@@ -90,7 +128,10 @@ function hoverContainer(nodeOrSelector, inspector) {
 
 
 
+
+
 function clickContainer(nodeOrSelector, inspector) {
+  info("Clicking on the markup-container for node " + nodeOrSelector);
   let updated = inspector.once("inspector-updated");
   let container = getContainerForRawNode(inspector.markup, getNode(nodeOrSelector));
   EventUtils.synthesizeMouseAtCenter(container.tagLine, {type: "mousedown"},
@@ -99,6 +140,7 @@ function clickContainer(nodeOrSelector, inspector) {
     inspector.markup.doc.defaultView);
   return updated;
 }
+
 
 
 
@@ -112,16 +154,89 @@ function isHighlighterVisible() {
 
 
 
+
 function mouseLeaveMarkupView(inspector) {
-  let deferred = promise.defer();
+  info("Leaving the markup-view area");
+  let def = promise.defer();
 
   
   let btn = inspector.toolbox.doc.querySelector(".toolbox-dock-button");
 
   EventUtils.synthesizeMouse(btn, 2, 2, {type: "mousemove"},
     inspector.toolbox.doc.defaultView);
-  executeSoon(deferred.resolve);
+  executeSoon(def.resolve);
 
-  return deferred.promise;
+  return def.promise;
 }
 
+
+
+
+
+
+
+function setEditableFieldValue(field, value, inspector) {
+  field.focus();
+  EventUtils.sendKey("return", inspector.panelWin);
+  let input = inplaceEditor(field).input;
+  ok(input, "Found editable field for setting value: " + value);
+  input.value = value;
+  EventUtils.sendKey("return", inspector.panelWin);
+}
+
+
+
+
+
+
+
+
+
+
+
+function assertAttributes(element, attrs) {
+  is(element.attributes.length, Object.keys(attrs).length,
+    "Node has the correct number of attributes.");
+  for (let attr in attrs) {
+    is(element.getAttribute(attr), attrs[attr],
+      "Node has the correct " + attr + " attribute.");
+  }
+}
+
+
+
+
+
+
+
+
+function undoChange(inspector) {
+  let canUndo = inspector.markup.undo.canUndo();
+  ok(canUndo, "The last change in the markup-view can be undone");
+  if (!canUndo) {
+    return promise.reject();
+  }
+
+  let mutated = inspector.once("markupmutation");
+  inspector.markup.undo.undo();
+  return mutated;
+}
+
+
+
+
+
+
+
+
+function redoChange(inspector) {
+  let canRedo = inspector.markup.undo.canRedo();
+  ok(canRedo, "The last change in the markup-view can be redone");
+  if (!canRedo) {
+    return promise.reject();
+  }
+
+  let mutated = inspector.once("markupmutation");
+  inspector.markup.undo.redo();
+  return mutated;
+}
