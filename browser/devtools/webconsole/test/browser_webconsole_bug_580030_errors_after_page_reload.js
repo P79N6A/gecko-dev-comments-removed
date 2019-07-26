0f978d81141936dc6fbf0e913ed4574716ac2b20
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:17:46 +0000

    Bug 982627 - Fix for intermittent browser_webconsole_bug_580030_errors_after_page_reload.js | Timed out while waiting for: error message after page reload; r=me
    UltraBlame original commit: 3d002c713d6d9eec0eb8ecd2f5b9d464bc689972

--- browser/devtools/webconsole/test/browser_webconsole_bug_580030_errors_after_page_reload.js
+++ browser/devtools/webconsole/test/browser_webconsole_bug_580030_errors_after_page_reload.js
@@ -5,63 +5,38 @@
 
 
 
+
+
 const TEST_URI = "http://example.com/browser/browser/devtools/webconsole/test/test-error.html";
 
 function test() {
-  expectUncaughtException();
-  addTab(TEST_URI);
-  browser.addEventListener("load", onLoad, true);
-}
+  Task.spawn(function*() {
+    const {tab} = yield loadTab(TEST_URI);
+    const hud = yield openConsole(tab);
+    info("console opened");
 
-
-
-function onLoad(aEvent) {
-  browser.removeEventListener(aEvent.type, onLoad, true);
-
-  openConsole(null, function(hud) {
-    hud.jsterm.clearOutput();
-    browser.addEventListener("load", testErrorsAfterPageReload, true);
-    content.location.reload();
-  });
-}
-
-function testErrorsAfterPageReload(aEvent) {
-  browser.removeEventListener(aEvent.type, testErrorsAfterPageReload, true);
-
-  
-  
-
-  Services.console.registerListener(consoleObserver);
-
-  let button = content.document.querySelector("button").wrappedJSObject;
-  ok(button, "button found");
-  EventUtils.sendMouseEvent({type: "click"}, button, content.wrappedJSObject);
-}
-
-var consoleObserver = {
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver]),
-
-  observe: function test_observe(aMessage)
-  {
-    
-    if (!(aMessage instanceof Ci.nsIScriptError) ||
-      aMessage.category != "content javascript") {
-      return;
-    }
-
-    Services.console.unregisterListener(this);
-
-    let outputNode = HUDService.getHudByWindow(content).outputNode;
-
-    waitForSuccess({
-      name: "error message after page reload",
-      validatorFn: function()
-      {
-        return outputNode.textContent.indexOf("fooBazBaz") > -1;
-      },
-      successFn: finishTest,
-      failureFn: finishTest,
+    executeSoon(() => {
+      hud.jsterm.clearOutput();
+      info("wait for reload");
+      content.location.reload();
     });
-  }
-};
 
+    yield hud.target.once("navigate");
+    info("target navigated");
+
+    let button = content.document.querySelector("button");
+    ok(button, "button found");
+
+    expectUncaughtException();
+    EventUtils.sendMouseEvent({type: "click"}, button, content);
+
+    yield waitForMessages({
+      webconsole: hud,
+      messages: [{
+        text: "fooBazBaz is not defined",
+        category: CATEGORY_JS,
+        severity: SEVERITY_ERROR,
+      }],
+    });
+  }).then(finishTest);
+}
