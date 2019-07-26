f2545785386ce65b9033438bb7f11e575e5d7dbd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:37:41 +0000

    Bug 928734 - Intermittent test_419731.js | "title 2" == "new title 1". r=mano
    UltraBlame original commit: ee98436d195d934c5e9aa765ed1589d606b57106

--- toolkit/components/places/tests/unit/test_419731.js
+++ toolkit/components/places/tests/unit/test_419731.js
@@ -4,111 +4,92 @@
 
 
 
-
-try {
-  var histsvc = Cc["@mozilla.org/browser/nav-history-service;1"].
-                getService(Ci.nsINavHistoryService);
-  var bhist = histsvc.QueryInterface(Ci.nsIBrowserHistory);
-} catch(ex) {
-  do_throw("Could not get history services\n");
-}
-
-
-try {
-  var bmsvc = Cc["@mozilla.org/browser/nav-bookmarks-service;1"].
-              getService(Ci.nsINavBookmarksService);
-}
-catch(ex) {
-  do_throw("Could not get the nav-bookmarks-service\n");
-}
-
-
-try {
-  var tagssvc = Cc["@mozilla.org/browser/tagging-service;1"].
-                getService(Ci.nsITaggingService);
-} catch(ex) {
-  do_throw("Could not get tagging service\n");
-}
-
-
-
 function run_test() {
-  var uri1 = uri("http://foo.bar/");
+  let uri1 = NetUtil.newURI("http://foo.bar/");
 
   
-  var bookmark1id = bmsvc.insertBookmark(bmsvc.bookmarksMenuFolder, uri1,
-                                         bmsvc.DEFAULT_INDEX, "title 1");
-  var bookmark2id = bmsvc.insertBookmark(bmsvc.toolbarFolder, uri1,
-                                         bmsvc.DEFAULT_INDEX, "title 2");
+  let bookmark1id = PlacesUtils.bookmarks
+                               .insertBookmark(PlacesUtils.bookmarksMenuFolderId,
+                                               uri1,
+                                               PlacesUtils.bookmarks.DEFAULT_INDEX,
+                                               "title 1");
+  let bookmark2id = PlacesUtils.bookmarks
+                               .insertBookmark(PlacesUtils.toolbarFolderId,
+                                               uri1,
+                                               PlacesUtils.bookmarks.DEFAULT_INDEX,
+                                               "title 2");
   
-  tagssvc.tagURI(uri1, ["foo"]);
+  PlacesUtils.tagging.tagURI(uri1, ["foo"]);
 
   
-  var options = histsvc.getNewQueryOptions();
-  var query = histsvc.getNewQuery();
-  query.setFolders([bmsvc.tagsFolder], 1);
-  var result = histsvc.executeQuery(query, options);
-  var tagRoot = result.root;
+  let options = PlacesUtils.history.getNewQueryOptions();
+  let query = PlacesUtils.history.getNewQuery();
+  query.setFolders([PlacesUtils.tagsFolderId], 1);
+  let result = PlacesUtils.history.executeQuery(query, options);
+  let tagRoot = result.root;
   tagRoot.containerOpen = true;
-  var tagNode = tagRoot.getChild(0)
+  let tagNode = tagRoot.getChild(0)
                        .QueryInterface(Ci.nsINavHistoryContainerResultNode);
-  var tagItemId = tagNode.itemId;
+  let tagItemId = tagNode.itemId;
   tagRoot.containerOpen = false;
 
   
-  bmsvc.setItemTitle(bookmark1id, "new title 1");
+  PlacesUtils.bookmarks.setItemTitle(bookmark1id, "new title 1");
 
   
-  options = histsvc.getNewQueryOptions();
+  let bookmark2LastMod = PlacesUtils.bookmarks.getItemLastModified(bookmark2id);
+  PlacesUtils.bookmarks.setItemLastModified(bookmark1id, bookmark2LastMod + 1);
+
+  
+  options = PlacesUtils.history.getNewQueryOptions();
   options.queryType = Ci.nsINavHistoryQueryOptions.QUERY_TYPE_BOOKMARKS;
   options.resultType = options.RESULTS_AS_TAG_QUERY;
 
-  query = histsvc.getNewQuery();
-  result = histsvc.executeQuery(query, options);
-  var root = result.root;
+  query = PlacesUtils.history.getNewQuery();
+  result = PlacesUtils.history.executeQuery(query, options);
+  let root = result.root;
   root.containerOpen = true;
   do_check_eq(root.childCount, 1);
 
-  var theTag = root.getChild(0)
+  let theTag = root.getChild(0)
                    .QueryInterface(Ci.nsINavHistoryContainerResultNode);
   
   do_check_eq(theTag.title, "foo")
-  bmsvc.setItemTitle(tagItemId, "bar");
+  PlacesUtils.bookmarks.setItemTitle(tagItemId, "bar");
 
   
   do_check_neq(theTag, root.getChild(0));
-  var theTag = root.getChild(0)
+  theTag = root.getChild(0)
                    .QueryInterface(Ci.nsINavHistoryContainerResultNode);
   do_check_eq(theTag.title, "bar");
 
   
   theTag.containerOpen = true;
   do_check_eq(theTag.childCount, 1);
-  var node = theTag.getChild(0);
+  let node = theTag.getChild(0);
   do_check_eq(node.title, "new title 1");
   theTag.containerOpen = false;
   root.containerOpen = false;
 
   
-  bmsvc.setItemTitle(bookmark2id, "new title 2");
+  PlacesUtils.bookmarks.setItemTitle(bookmark2id, "new title 2");
 
   
-  var bookmark1LastMod = bmsvc.getItemLastModified(bookmark1id);
-  bmsvc.setItemLastModified(bookmark2id, bookmark1LastMod + 1);
+  let bookmark1LastMod = PlacesUtils.bookmarks.getItemLastModified(bookmark1id);
+  PlacesUtils.bookmarks.setItemLastModified(bookmark2id, bookmark1LastMod + 1);
 
   
-  options = histsvc.getNewQueryOptions();
+  options = PlacesUtils.history.getNewQueryOptions();
   options.queryType = Ci.nsINavHistoryQueryOptions.QUERY_TYPE_BOOKMARKS;
   options.resultType = options.RESULTS_AS_TAG_CONTENTS;
 
-  query = histsvc.getNewQuery();
+  query = PlacesUtils.history.getNewQuery();
   query.setFolders([tagItemId], 1);
-  result = histsvc.executeQuery(query, options);
+  result = PlacesUtils.history.executeQuery(query, options);
   root = result.root;
 
   root.containerOpen = true;
-  var cc = root.childCount;
-  do_check_eq(cc, 1);
+  do_check_eq(root.childCount, 1);
   node = root.getChild(0);
   do_check_eq(node.title, "new title 2");
   root.containerOpen = false;
