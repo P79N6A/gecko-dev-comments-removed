b27afb548762326e4caec0967aa9ac8f6fccd365
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:20:00 +0000

    Bug 809493 - Restore the blend equation state after drawing the underlay. r=joe
    UltraBlame original commit: f8989ca9a3c6ab5e161932e22f645e392964ab37

--- gfx/layers/opengl/LayerManagerOGL.cpp
+++ gfx/layers/opengl/LayerManagerOGL.cpp
@@ -993,6 +993,10 @@ LayerManagerOGL::Render()
   mWidget->DrawWindowUnderlay(this, rect);
 
   
+  mGLContext->fBlendFuncSeparate(LOCAL_GL_ONE, LOCAL_GL_ONE_MINUS_SRC_ALPHA,
+                                 LOCAL_GL_ONE, LOCAL_GL_ONE);
+
+  
   RootLayer()->RenderLayer(mGLContext->IsDoubleBuffered() ? 0 : mBackBufferFBO,
                            nsIntPoint(0, 0));
 
