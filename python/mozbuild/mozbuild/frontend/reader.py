575dc45189ea03bd9e33740d8b115e77097ca491
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:58:23 +0000

    Bug 778236 - Remove EXTERNAL_MAKE_DIRS and PARALLEL_EXTERNAL_MAKE_DIRS. r=gps
    UltraBlame original commit: f5bb944954a5dfa7d56b6ee531e83d2f978cc724

--- python/mozbuild/mozbuild/frontend/reader.py
+++ python/mozbuild/mozbuild/frontend/reader.py
@@ -760,8 +760,8 @@ class BuildReader(object):
             gyp_sandboxes.extend(read_from_gyp(self.config,
                                                mozpath.join(curdir, gyp_dir.input),
                                                mozpath.join(sandbox['OBJDIR'],
-                                               target_dir),
-                                 gyp_dir.variables))
+                                                            target_dir),
+                                               gyp_dir.variables))
 
         
         
