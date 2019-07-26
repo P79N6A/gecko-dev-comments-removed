4e0055ddd104375059b88536994ca73313d9c4e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:39:17 +0000

    Bug 825365.  Fix the softp version of MacroAssemblerARMCompat::passABIArg to not add no-op register moves to the MoveResolver.  r=mjrosenb
    UltraBlame original commit: b5ea211f23a931dad895bfdbe6e8637c907222ea

--- js/src/ion/arm/MacroAssembler-arm.cpp
+++ js/src/ion/arm/MacroAssembler-arm.cpp
@@ -2759,6 +2759,9 @@ MacroAssemblerARMCompat::passABIArg(const MoveOperand &from)
         if (from.isDouble()) {
             floatArgsInGPR[destReg.code() >> 1] = VFPRegister(from.floatReg());
             useResolver = false;
+        } else if (from.isGeneralReg() && from.reg() == destReg) {
+            
+            useResolver = false;
         } else {
             dest = MoveOperand(destReg);
         }
