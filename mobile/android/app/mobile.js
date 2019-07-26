d0f6624b28b003ea4b813e8ff5a4f7dc57dc364d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:40:36 +0000

    Backed out changeset efe0a3f79333 (bug 803299)
    UltraBlame original commit: 0291b3283729f866b8023041c0aaf6939d69859f

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -363,9 +363,6 @@ pref("gfx.displayport.strategy_pb.threshold", -1);
 pref("gfx.font_rendering.graphite.enabled", false);
 
 
-pref("gfx.android.rgb16.force", false);
-
-
 pref("dom.disable_window_move_resize", true);
 
 
