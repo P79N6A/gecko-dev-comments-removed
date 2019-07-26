d776824c717a929cc3877572fcfdabd66cfc849f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:51:08 +0000

    Bug 968803 - part 2 - statically type runnable classes that return a result; r=ekr
    UltraBlame original commit: 8a6c3ab28bbc433485d29e13fb905682495d785c

--- media/mtransport/runnable_utils.h
+++ media/mtransport/runnable_utils.h
@@ -15,11 +15,42 @@
 
 namespace mozilla {
 
+namespace detail {
+
+enum RunnableResult {
+  NoResult,
+  ReturnsResult
+};
+
+static inline nsresult
+RunOnThreadInternal(nsIEventTarget *thread, nsIRunnable *runnable, uint32_t flags)
+{
+  nsCOMPtr<nsIRunnable> runnable_ref(runnable);
+  if (thread) {
+    bool on;
+    nsresult rv;
+    rv = thread->IsOnCurrentThread(&on);
+
+    
+    if (rv != NS_ERROR_NOT_INITIALIZED) {
+      MOZ_ASSERT(NS_SUCCEEDED(rv));
+    }
+
+    NS_ENSURE_SUCCESS(rv, rv);
+    if (!on) {
+      return thread->Dispatch(runnable_ref, flags);
+    }
+  }
+  return runnable_ref->Run();
+}
+
+template<RunnableResult result>
 class runnable_args_base : public nsRunnable {
  public:
   NS_IMETHOD Run() = 0;
-  virtual bool returns_value() const { return false; }
 };
+
+}
 
 
 
@@ -38,34 +69,14 @@ class runnable_args_base : public nsRunnable {
 
 #include "runnable_utils_generated.h"
 
-
-static inline nsresult RUN_ON_THREAD(nsIEventTarget *thread, nsIRunnable *runnable, uint32_t flags) {
-  RefPtr<nsIRunnable> runnable_ref(runnable);
-  if (thread) {
-    bool on;
-    nsresult rv;
-    rv = thread->IsOnCurrentThread(&on);
-
-    
-    if (rv != NS_ERROR_NOT_INITIALIZED) {
-      MOZ_ASSERT(NS_SUCCEEDED(rv));
-    }
-
-    NS_ENSURE_SUCCESS(rv, rv);
-    if(!on) {
-      return thread->Dispatch(runnable_ref, flags);
-    }
-  }
-  return runnable_ref->Run();
+static inline nsresult RUN_ON_THREAD(nsIEventTarget *thread, detail::runnable_args_base<detail::NoResult> *runnable, uint32_t flags) {
+  return detail::RunOnThreadInternal(thread, static_cast<nsIRunnable *>(runnable), flags);
 }
 
-static inline nsresult RUN_ON_THREAD(nsIEventTarget *thread, runnable_args_base *runnable, uint32_t flags) {
-  
-  
-  
-  MOZ_ASSERT((!(runnable->returns_value()) || (flags != NS_DISPATCH_NORMAL)));
-
-  return RUN_ON_THREAD(thread, static_cast<nsIRunnable *>(runnable), flags);
+static inline nsresult
+RUN_ON_THREAD(nsIEventTarget *thread, detail::runnable_args_base<detail::ReturnsResult> *runnable)
+{
+  return detail::RunOnThreadInternal(thread, static_cast<nsIRunnable *>(runnable), NS_DISPATCH_SYNC);
 }
 
 #ifdef DEBUG
