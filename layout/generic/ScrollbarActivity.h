eeb661e17759fc07f3e194ab20d0a87cfd401a3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:29:33 +0000

    Bug 636564 - Implement Lion style scrollbars on Mac OSX 10.7+. r=roc
    UltraBlame original commit: 1840b15583fd3bd4c8f6bb40491cd68f84e39fc5

--- layout/generic/ScrollbarActivity.h
+++ layout/generic/ScrollbarActivity.h
@@ -7,14 +7,17 @@
 #define ScrollbarActivity_h___
 
 #include "nsCOMPtr.h"
+#include "nsIDOMEventListener.h"
 #include "mozilla/TimeStamp.h"
+#include "nsRefreshDriver.h"
 
 class nsIContent;
+class nsIScrollbarOwner;
 class nsITimer;
 class nsIAtom;
-class nsIScrollableFrame;
 
 namespace mozilla {
+namespace layout {
 
 
 
@@ -37,52 +40,105 @@ namespace mozilla {
 
 
 
-class ScrollbarActivity {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ScrollbarActivity : public nsIDOMEventListener,
+                          public nsARefreshObserver {
 public:
-  ScrollbarActivity(nsIScrollableFrame* aScrollableFrame)
-   : mIsActive(false)
-   , mScrollableFrame(aScrollableFrame)
+  ScrollbarActivity(nsIScrollbarOwner* aScrollableFrame)
+   : mScrollableFrame(aScrollableFrame)
+   , mNestedActivityCounter(0)
+   , mIsActive(false)
+   , mIsFading(false)
+   , mListeningForEvents(false)
+   , mHScrollbarHovered(false)
+   , mVScrollbarHovered(false)
   {}
 
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIDOMEVENTLISTENER
+
+  virtual ~ScrollbarActivity() {}
+
+  void Destroy();
+
   void ActivityOccurred();
-  void ActivityFinished();
-  ~ScrollbarActivity();
+  void ActivityStarted();
+  void ActivityStopped();
+
+  virtual void WillRefresh(TimeStamp aTime);
+
+  static void FadeBeginTimerFired(nsITimer* aTimer, void* aSelf) {
+    reinterpret_cast<ScrollbarActivity*>(aSelf)->BeginFade();
+  }
+
+  static const uint32_t kScrollbarFadeBeginDelay = 450; 
+  static const uint32_t kScrollbarFadeDuration = 200; 
 
 protected:
-  
 
+  bool IsActivityOngoing()
+  { return mNestedActivityCounter > 0; }
+  bool IsStillFading(TimeStamp aTime);
 
-
-
-  bool mIsActive;
-
-  
-
-
-
-
-  nsIScrollableFrame* mScrollableFrame;
-
-  nsCOMPtr<nsITimer> mActivityFinishedTimer;
+  void HandleEventForScrollbar(const nsAString& aType,
+                               nsIContent* aTarget,
+                               nsIContent* aScrollbar,
+                               bool* aStoredHoverState);
 
   void SetIsActive(bool aNewActive);
+  void SetIsFading(bool aNewFading);
 
-  enum { kScrollbarActivityFinishedDelay = 450 }; 
-  static void ActivityFinishedTimerFired(nsITimer* aTimer, void* aSelf) {
-    reinterpret_cast<ScrollbarActivity*>(aSelf)->ActivityFinished();
-  }
-  void StartActivityFinishedTimer();
-  void CancelActivityFinishedTimer();
+  void BeginFade();
+  void EndFade();
 
+  void StartFadeBeginTimer();
+  void CancelFadeBeginTimer();
+  void StartListeningForEvents();
+  void StartListeningForEventsOnScrollbar(nsIDOMEventTarget* aScrollbar);
+  void StopListeningForEvents();
+  void StopListeningForEventsOnScrollbar(nsIDOMEventTarget* aScrollbar);
+  void RegisterWithRefreshDriver();
+  void UnregisterFromRefreshDriver();
+
+  void UpdateOpacity(TimeStamp aTime);
+  void HoveredScrollbar(nsIContent* aScrollbar);
+
+  nsRefreshDriver* GetRefreshDriver();
   nsIContent* GetScrollbarContent(bool aVertical);
-  nsIContent* GetHorizontalScrollbar() {
-    return GetScrollbarContent(false);
+  nsIContent* GetHorizontalScrollbar() { return GetScrollbarContent(false); }
+  nsIContent* GetVerticalScrollbar() { return GetScrollbarContent(true); }
+
+  static const TimeDuration FadeDuration() {
+    return TimeDuration::FromMilliseconds(kScrollbarFadeDuration);
   }
-  nsIContent* GetVerticalScrollbar() {
-    return GetScrollbarContent(true);
-  }
+
+  nsIScrollbarOwner* mScrollableFrame;
+  TimeStamp mFadeBeginTime;
+  nsCOMPtr<nsITimer> mFadeBeginTimer;
+  nsCOMPtr<nsIDOMEventTarget> mHorizontalScrollbar; 
+  nsCOMPtr<nsIDOMEventTarget> mVerticalScrollbar;   
+  int mNestedActivityCounter;
+  bool mIsActive;
+  bool mIsFading;
+  bool mListeningForEvents;
+  bool mHScrollbarHovered;
+  bool mVScrollbarHovered;
 };
 
+} 
 } 
 
 #endif 
