7202fc5890ac0aef426a69dcff62a757cf37d517
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:44:27 +0000

    Bug 905926 - Remove watchpoints on all compartments during shutdown, not just the cx compartment. r=billm
    UltraBlame original commit: ecb564ad91402c6995264da35e837f90317d8a34

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -36,6 +36,7 @@
 #include "jsscript.h"
 #include "jsstr.h"
 #include "jstypes.h"
+#include "jswatchpoint.h"
 #include "jsworkers.h"
 
 #include "gc/Marking.h"
@@ -270,9 +271,11 @@ js::DestroyContext(JSContext *cx, DestroyContextMode mode)
         FinishCommonNames(rt);
 
         
-        for (CompartmentsIter c(rt); !c.done(); c.next())
+        for (CompartmentsIter c(rt); !c.done(); c.next()) {
             c->clearTraps(rt->defaultFreeOp());
-        JS_ClearAllWatchPoints(cx);
+            if (WatchpointMap *wpmap = c->watchpointMap)
+                wpmap->clear();
+        }
 
         
         rt->staticStrings.finish();
