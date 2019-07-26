4e961e146dbf7009342430a438e59854cd8db563
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:07:23 +0000

    Bug 895974.  Implement ParentNode on document fragments and documents and move previousElementSibling and nextElementSibling to ChildNode.  r=smaug
    UltraBlame original commit: 5b59f2f35bb3aed2d94e34b96a4241e962cadcb6

--- content/base/public/nsIDocument.h
+++ content/base/public/nsIDocument.h
@@ -53,6 +53,7 @@ class nsIDOMElement;
 class nsIDOMNodeList;
 class nsIDOMXPathExpression;
 class nsIDOMXPathNSResolver;
+class nsIHTMLCollection;
 class nsILayoutHistoryState;
 class nsIObjectLoadingContent;
 class nsIObserver;
@@ -2120,6 +2121,10 @@ public:
 
   void ObsoleteSheet(const nsAString& aSheetURI, mozilla::ErrorResult& rv);
 
+  
+  nsIHTMLCollection* Children();
+  uint32_t ChildElementCount();
+
   virtual nsHTMLDocument* AsHTMLDocument() { return nullptr; }
 
   virtual JSObject* WrapObject(JSContext *aCx,
@@ -2210,6 +2215,9 @@ protected:
   
   nsPropertyTable mPropertyTable;
   nsTArray<nsAutoPtr<nsPropertyTable> > mExtraPropertyTables;
+
+  
+  nsCOMPtr<nsIHTMLCollection> mChildrenCollection;
 
   
   nsCompatibility mCompatMode;
