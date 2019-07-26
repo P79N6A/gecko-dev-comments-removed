034c55dac628f8be29fdbfdfbb90561ffe1ca494
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:03:16 +0000

    Bug 792202 - Make mozboot work with Python 2.6.1; r=jhammel DONTBUILD
    Python 2.6.1 is what ships on OS X 10.6 and we want to support mozboot
    there.
    
    We include 3 fixes:
    
    1) optparse doesn't like unicode as its initial argument
    2) __init__(**kwargs) doesn't like unicode keys
    3) subprocess does not have check_output
    
    We took the easy solution of removing unicode_literals. This should have
    no significant consequences.
    
    UltraBlame original commit: e2f161ca30bc3e2afcf3b9fee6c70c529a69f6ad

--- python/mozboot/bin/bootstrap.py
+++ python/mozboot/bin/bootstrap.py
@@ -10,7 +10,9 @@
 
 
 
-from __future__ import print_function, unicode_literals
+
+
+from __future__ import print_function
 
 import os
 import shutil
--- python/mozboot/mozboot/bootstrap.py
+++ python/mozboot/mozboot/bootstrap.py
@@ -2,7 +2,8 @@
 
 
 
-from __future__ import print_function, unicode_literals
+
+from __future__ import print_function
 
 import platform
 import sys
