c968d40eb902435d6b57a69a309287adde8c8cce
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:21:28 +0000

    Bug 806720 - Port browser_styleeditor_private.js to the new per-tab PB APIs; r=ehsan
    UltraBlame original commit: 400d7b156d2fa2b189dafce8dae9e00c9feba484

---
+++ browser/devtools/styleeditor/test/browser_styleeditor_private_perwindowpb.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+function test() {
+  waitForExplicitFinish();
+  let windowsToClose = [];
+  let testURI = 'http://' + TEST_HOST + '/browser/browser/devtools/styleeditor/test/test_private.html';
+
+  function checkCache() {
+    checkDiskCacheFor(TEST_HOST);
+    finish();
+  }
+
+  function doTest(aWindow) {
+    aWindow.gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
+      aWindow.gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
+      cache.evictEntries(Ci.nsICache.STORE_ANYWHERE);
+      launchStyleEditorChromeFromWindow(aWindow, function(aChrome) {
+        aChrome.addChromeListener({
+          onEditorAdded: function(aChrome, aEditor) {
+            if (aEditor.isLoaded) {
+              checkCache();
+            } else {
+              aEditor.addActionListener({
+                onLoad: checkCache
+              });
+            }
+          }
+        });
+      });
+    }, true);
+
+    aWindow.gBrowser.selectedBrowser.loadURI(testURI);
+  }
+
+  function testOnWindow(options, callback) {
+    let win = OpenBrowserWindow(options);
+    win.addEventListener("load", function onLoad() {
+      win.removeEventListener("load", onLoad, false);
+      windowsToClose.push(win);
+      executeSoon(function() callback(win));
+    }, false);
+  };
+
+  registerCleanupFunction(function() {
+    windowsToClose.forEach(function(win) {
+      win.close();
+    });
+  });
+
+  testOnWindow({private: true}, function(win) {
+    doTest(win);
+  });
+}
