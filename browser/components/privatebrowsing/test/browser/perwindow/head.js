e7f1cba4c8d905b4dcf6089144dffe048bc02a4a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:33:05 +0000

    Remove the setWindowPrivate function which is now unused, no bug
    DONTBUILD since this is NPOTB for global PB builds
    
    UltraBlame original commit: 65209d041acc1b309dd74eeea428e8cc7669413d

--- browser/components/privatebrowsing/test/browser/perwindow/head.js
+++ browser/components/privatebrowsing/test/browser/perwindow/head.js
@@ -1,18 +1,3 @@
-
-
-
-
-function setPrivateWindow(aWindow, aEnable) {
-  return aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
-                 .getInterface(Ci.nsIWebNavigation)
-                 .QueryInterface(Ci.nsIDocShellTreeItem)
-                 .treeOwner
-                 .QueryInterface(Ci.nsIInterfaceRequestor)
-                 .getInterface(Ci.nsIXULWindow)
-                 .docShell.QueryInterface(Ci.nsILoadContext)
-                 .usePrivateBrowsing = aEnable;
-} 
-
 function whenNewWindowLoaded(aOptions, aCallback) {
   let win = OpenBrowserWindow(aOptions);
   win.addEventListener("load", function onLoad() {
