0fce47505a82229427a3aae5cef8e99b8364bf10
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:31:11 +0000

    Bug 909441 - Test. (r=efaust)
    UltraBlame original commit: 46345150fef4af0539d610732057cf5132ed569f

---
+++ js/src/jit-test/tests/auto-regress/bug909441.js
@@ -0,0 +1,20 @@
+
+function f(code) {
+    try {
+        g = Function(code)
+    } catch (e) {}
+    g()
+}
+f("\
+    Object.defineProperty(this,\"x\",{\
+        get: function(){\
+            evaluate(\"Array(x)\",{\
+                newContext:true,\
+                catchTermination:(function(){})\
+            })\
+        }\
+    })\
+");
+f("x");
+f(")");
+f("x");
