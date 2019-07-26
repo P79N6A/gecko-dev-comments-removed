26cb453d1612a719160cf4c5e6c066c1e2a6c90a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:27:46 +0000

    Bug 960332 - update Fxa URLs to production server. r=ckarlof.
    UltraBlame original commit: 464aa919d253f99c6d10abee582cb2f57b1f2403

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1337,12 +1337,12 @@ pref("browser.uiCustomization.debug", false);
 
 
 
-pref("identity.fxaccounts.remote.uri", "https://accounts.dev.lcip.org/?service=sync");
+pref("identity.fxaccounts.remote.uri", "https://accounts.firefox.com/?service=sync&context=fx_desktop_v1");
 
 
 
 
-pref("identity.fxaccounts.settings.uri", "https://accounts.dev.lcip.org/settings");
+pref("identity.fxaccounts.settings.uri", "https://accounts.firefox.com/settings");
 
 
-pref("identity.fxaccounts.auth.uri", "https://api-accounts.dev.lcip.org/v1");
+pref("identity.fxaccounts.auth.uri", "https://api.accounts.firefox.com/v1");
