fcec132cf7aef7490216a996df7e418f005a7764
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:31:08 +0000

    Bug 787228 - Part2: Add libomxplugin support for Gingerbread - r=cpeterson
    UltraBlame original commit: ee61282f73b5718f2d0f355fa51d679f6561a8c7

---
+++ media/omx-plugin/gb/OmxPlugin236.cpp
@@ -0,0 +1,8 @@
+
+
+
+
+
+#define MOZ_STAGEFRIGHT_OFF_T off64_t
+#define MOZ_ANDROID_GB
+#include "../OmxPlugin.cpp"
---
+++ media/omx-plugin/gb235/OmxPlugin235.cpp
@@ -0,0 +1,8 @@
+
+
+
+
+
+#define MOZ_STAGEFRIGHT_OFF_T off_t
+#define MOZ_ANDROID_GB
+#include "../OmxPlugin.cpp"
