f86f873d05b345bbe27d7605eaec1bfd168a0977
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:49:42 +0000

    Bug 998067: Add utility code for making it easier to create GTests based on NSS, r=keeler
    UltraBlame original commit: 64ce640f432e28a628b2a21ff7e392a475e87f0c

---
+++ security/pkix/test/gtest/nssgtest.h
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_pkix__nssgtest_h
+#define mozilla_pkix__nssgtest_h
+
+#include "stdint.h"
+#include "gtest/gtest.h"
+#include "prerror.h"
+#include "seccomon.h"
+
+namespace mozilla { namespace pkix { namespace test {
+
+class SECStatusWithPRErrorCode
+{
+public:
+  SECStatusWithPRErrorCode(SECStatus rv, PRErrorCode errorCode)
+    : mRv(rv)
+    , mErrorCode(errorCode)
+  {
+  }
+
+  SECStatusWithPRErrorCode(SECStatus rv)
+    : mRv(rv)
+    , mErrorCode(rv == SECSuccess ? 0 : PR_GetError())
+  {
+  }
+
+  bool operator==(const SECStatusWithPRErrorCode& other) const
+  {
+    return mRv == other.mRv && mErrorCode == other.mErrorCode;
+  }
+
+private:
+  const SECStatus mRv;
+  const PRErrorCode mErrorCode;
+
+  friend std::ostream& operator<<(std::ostream& os,
+                                  SECStatusWithPRErrorCode const& value);
+
+  void operator=(const SECStatusWithPRErrorCode&) ;
+};
+
+::std::ostream& operator<<(::std::ostream&,
+                           SECStatusWithPRErrorCode const&);
+
+} } } 
+
+#define ASSERT_SECSuccess(rv) \
+  ASSERT_EQ(::mozilla::pkix::test::SECStatusWithPRErrorCode(SECSuccess, 0), \
+            ::mozilla::pkix::test::SECStatusWithPRErrorCode(rv))
+#define EXPECT_SECSuccess(rv) \
+  EXPECT_EQ(::mozilla::pkix::test::SECStatusWithPRErrorCode(SECSuccess, 0), \
+            ::mozilla::pkix::test::SECStatusWithPRErrorCode(rv))
+
+#define ASSERT_SECFailure(expectedError, rv) \
+  ASSERT_EQ(::mozilla::pkix::test::SECStatusWithPRErrorCode(SECFailure, \
+                                                            expectedError), \
+            ::mozilla::pkix::test::SECStatusWithPRErrorCode(rv))
+#define EXPECT_SECFailure(expectedError, rv) \
+  EXPECT_EQ(::mozilla::pkix::test::SECStatusWithPRErrorCode(SECFailure, \
+                                                            expectedError), \
+            ::mozilla::pkix::test::SECStatusWithPRErrorCode(rv))
+
+#endif 
