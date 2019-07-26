5d4b2daa8365b64ca0d828529d5c4267610111e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:18:02 +0000

    Bug 805162 - c. Implement new Java to Gecko IME events in widget; r=blassey
    UltraBlame original commit: 973b0ed30b8b73fc00da5051f18030f943a2d532

--- widget/android/nsWindow.h
+++ widget/android/nsWindow.h
@@ -166,7 +166,7 @@ protected:
     bool DrawTo(gfxASurface *targetSurface);
     bool DrawTo(gfxASurface *targetSurface, const nsIntRect &aRect);
     bool IsTopLevel();
-    void OnIMEAddRange(mozilla::AndroidGeckoEvent *ae);
+    void RemoveIMEComposition();
 
     
     
@@ -183,8 +183,8 @@ protected:
     nsCOMPtr<nsIIdleServiceInternal> mIdleService;
 
     bool mIMEComposing;
+    bool mIMEMaskSelectionUpdate, mIMEMaskTextUpdate;
     nsString mIMEComposingText;
-    nsString mIMELastDispatchedComposingText;
     nsAutoTArray<nsTextRange, 4> mIMERanges;
 
     InputContext mInputContext;
