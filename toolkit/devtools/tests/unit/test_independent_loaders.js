23e8979b602cc095599312655785e1a836b5deba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:16:02 +0000

    Bug 946813 - Part 3: Use independent loaders, mark them invisible. r=past
    UltraBlame original commit: f5255c1b202f543e2c505415efa73e484f7492e6

---
+++ toolkit/devtools/tests/unit/test_independent_loaders.js
@@ -0,0 +1,23 @@
+
+
+
+const { DevToolsLoader } =
+  Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
+
+
+
+
+
+
+function run_test() {
+  let loader1 = new DevToolsLoader();
+  let loader2 = new DevToolsLoader();
+
+  let color1 = loader1.require("devtools/css-color");
+  let color2 = loader2.require("devtools/css-color");
+
+  do_check_true(color1 !== color2);
+
+  do_check_true(loader1._provider !== loader2._provider);
+  do_check_true(loader1._provider.loader !== loader2._provider.loader);
+}
