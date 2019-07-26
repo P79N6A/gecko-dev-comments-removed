3153c35bb256a5b9339d4da4c60d352c5e4ad7c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:14:25 +0000

    Bug 786235 - Rewrite TURN stack to match RFC 5766. r=abr
    UltraBlame original commit: e69693180dffb97ae302b712f7c1775d0092a960

--- media/mtransport/third_party/nICEr/src/stun/nr_socket_turn.h
+++ media/mtransport/third_party/nICEr/src/stun/nr_socket_turn.h
@@ -35,9 +35,12 @@
 #ifndef _nr_socket_turn_h
 #define _nr_socket_turn_h
 
-int nr_socket_turn_create(nr_socket *sock, int turn_state, nr_socket **sockp);
-void nr_socket_turn_set_state(nr_socket *sock, int turn_state);
-void nr_socket_turn_set_relay_addr(nr_socket *sock, nr_transport_addr *relay);
+
+
+
+
+int nr_socket_turn_create(nr_socket *sock, nr_socket **sockp);
+int nr_socket_turn_set_ctx(nr_socket *sock, nr_turn_client_ctx *ctx);
 
 #endif
 
--- media/mtransport/third_party/nICEr/src/stun/turn_client_ctx.h
+++ media/mtransport/third_party/nICEr/src/stun/turn_client_ctx.h
@@ -32,51 +32,99 @@
 
 
 
+
+
+
 #ifndef _turn_client_ctx_h
 #define _turn_client_ctx_h
+
+
+
+
+
+
+typedef struct nr_turn_stun_ctx_ {
+  struct nr_turn_client_ctx_ *tctx;
+  int mode;  
+  int retry_ct;
+  nr_stun_client_ctx *stun;
+  char *nonce;
+  char *realm;
+  NR_async_cb success_cb;
+  NR_async_cb error_cb;
+
+  STAILQ_ENTRY(nr_turn_stun_ctx_) entry;
+} nr_turn_stun_ctx;
+typedef STAILQ_HEAD(nr_turn_stun_ctx_head_, nr_turn_stun_ctx_)
+    nr_turn_stun_ctx_head;
+
+
+typedef struct nr_turn_permission_ {
+  nr_transport_addr addr;
+  nr_turn_stun_ctx *stun;
+  UINT8 last_used;
+
+  STAILQ_ENTRY(nr_turn_permission_) entry;
+} nr_turn_permission;
+typedef STAILQ_HEAD(nr_turn_permission_head_, nr_turn_permission_)
+    nr_turn_permission_head;
+
+
+
 
 typedef struct nr_turn_client_ctx_ {
   int state;
 #define NR_TURN_CLIENT_STATE_INITTED         1
-#define NR_TURN_CLIENT_STATE_RUNNING         2
+#define NR_TURN_CLIENT_STATE_ALLOCATING      2
 #define NR_TURN_CLIENT_STATE_ALLOCATED       3
-
 #define NR_TURN_CLIENT_STATE_FAILED          6
-#define NR_TURN_CLIENT_STATE_TIMED_OUT       7
 #define NR_TURN_CLIENT_STATE_CANCELLED       8
 
-  int phase;
-#define NR_TURN_CLIENT_PHASE_INITIALIZED          -1
-#define NR_TURN_CLIENT_PHASE_ALLOCATE_REQUEST1    0
-#define NR_TURN_CLIENT_PHASE_ALLOCATE_REQUEST2    1
-#define NR_TURN_CLIENT_PHASE_SET_ACTIVE_DEST      2
-
-  nr_stun_client_ctx  *stun_ctx[3];    
-
   char *label;
-  nr_transport_addr turn_server_addr;
   nr_socket *sock;
-  nr_socket *wrapping_sock;
   char *username;
   Data *password;
+  char *nonce;
+  char *realm;
 
+  nr_transport_addr turn_server_addr;
   nr_transport_addr relay_addr;
+  nr_transport_addr mapped_addr;
+
+  nr_turn_stun_ctx_head stun_ctxs;
+  nr_turn_permission_head permissions;
 
   NR_async_cb finished_cb;
   void *cb_arg;
+
+  void *refresh_timer_handle;
 } nr_turn_client_ctx;
 
 extern int NR_LOG_TURN;
 
-int nr_turn_client_ctx_create(char *label, nr_socket *sock, nr_socket *wsock, nr_transport_addr *turn_server, UINT4 RTO, nr_turn_client_ctx **ctxp);
+int nr_turn_client_ctx_create(const char *label, nr_socket *sock,
+                              const char *username, Data *password,
+                              nr_transport_addr *addr,
+                              nr_turn_client_ctx **ctxp);
 int nr_turn_client_ctx_destroy(nr_turn_client_ctx **ctxp);
-int nr_turn_client_allocate(nr_turn_client_ctx *ctx, char *username, Data *password, UINT4 bandwidth_kbps, UINT4 lifetime_secs, NR_async_cb finished_cb, void *cb_arg);
-
-int nr_turn_client_process_response(nr_turn_client_ctx *ctx, UCHAR *msg, int len, nr_transport_addr *turn_server_addr);
+int nr_turn_client_allocate(nr_turn_client_ctx *ctx,
+                            NR_async_cb finished_cb, void *cb_arg);
+int nr_turn_client_get_relayed_address(nr_turn_client_ctx *ctx,
+                                       nr_transport_addr *relayed_address);
+int nr_turn_client_get_mapped_address(nr_turn_client_ctx *ctx,
+                                      nr_transport_addr *mapped_address);
+int nr_turn_client_process_response(nr_turn_client_ctx *ctx,
+                                    UCHAR *msg, int len,
+                                    nr_transport_addr *turn_server_addr);
 int nr_turn_client_cancel(nr_turn_client_ctx *ctx);
-int nr_turn_client_relay_indication_data(nr_socket *sock, const UCHAR *msg, size_t len, int flags, nr_transport_addr *relay_addr, nr_transport_addr *remote_addr);
-int nr_turn_client_rewrite_indication_data(UCHAR *msg, size_t len, size_t *newlen, nr_transport_addr *remote_addr);
-
-
+int nr_turn_client_send_indication(nr_turn_client_ctx *ctx,
+                                   const UCHAR *msg, size_t len,
+                                   int flags, nr_transport_addr *remote_addr);
+int nr_turn_client_parse_data_indication(nr_turn_client_ctx *ctx,
+                                         nr_transport_addr *source_addr,
+                                         UCHAR *msg, size_t len,
+                                         UCHAR *newmsg, size_t *newlen,
+                                         size_t newsize,
+                                         nr_transport_addr *remote_addr);
 #endif
 
