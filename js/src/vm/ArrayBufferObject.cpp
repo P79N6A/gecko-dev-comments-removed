07b1109223d5bbdab68b9b4bdc9c515382f0c07e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:04:22 +0000

    Bug 945152 - Never allocate ArrayBuffers with unknown API contents in the nursery; r=sfink
    UltraBlame original commit: d775a9f98e300ede61025721bc2dc83db6ef27cc

--- js/src/vm/ArrayBufferObject.cpp
+++ js/src/vm/ArrayBufferObject.cpp
@@ -664,9 +664,10 @@ ArrayBufferObject::addView(ArrayBufferViewObject *view)
 }
 
 ArrayBufferObject *
-ArrayBufferObject::create(JSContext *cx, uint32_t nbytes, bool clear )
+ArrayBufferObject::create(JSContext *cx, uint32_t nbytes, bool clear ,
+                          NewObjectKind newKind )
 {
-    Rooted<ArrayBufferObject*> obj(cx, NewBuiltinClassInstance<ArrayBufferObject>(cx));
+    Rooted<ArrayBufferObject*> obj(cx, NewBuiltinClassInstance<ArrayBufferObject>(cx, newKind));
     if (!obj)
         return nullptr;
     JS_ASSERT_IF(obj->isTenured(), obj->tenuredGetAllocKind() == gc::FINALIZE_OBJECT16_BACKGROUND);
@@ -1350,7 +1351,11 @@ JS_PUBLIC_API(JSObject *)
 JS_NewArrayBufferWithContents(JSContext *cx, void *contents)
 {
     JS_ASSERT(contents);
-    JSObject *obj = ArrayBufferObject::create(cx, 0);
+
+    
+    
+    
+    JSObject *obj = ArrayBufferObject::create(cx, 0, true, TenuredObject);
     if (!obj)
         return nullptr;
     js::ObjectElements *elements = reinterpret_cast<js::ObjectElements *>(contents);
