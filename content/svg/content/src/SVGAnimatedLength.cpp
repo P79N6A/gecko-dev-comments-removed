0a8dbd2ca3c9ed7dc7828ff378a006bb7c36f0a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:45:55 +0000

    Bug 832169 - Convert SVGAnimatedLength to WebIDL r=bz
    UltraBlame original commit: e5321a44b63c1e0cb89dd86de23a2837eddc3fcd

---
+++ content/svg/content/src/SVGAnimatedLength.cpp
@@ -0,0 +1,49 @@
+
+
+
+
+
+#include "mozilla/dom/SVGAnimatedLength.h"
+#include "mozilla/dom/SVGAnimatedLengthBinding.h"
+
+DOMCI_DATA(SVGAnimatedLength, mozilla::dom::SVGAnimatedLength)
+
+namespace mozilla {
+namespace dom {
+
+NS_SVG_VAL_IMPL_CYCLE_COLLECTION_WRAPPERCACHED(SVGAnimatedLength, mSVGElement)
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(SVGAnimatedLength)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(SVGAnimatedLength)
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(SVGAnimatedLength)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsIDOMSVGAnimatedLength)
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(SVGAnimatedLength)
+NS_INTERFACE_MAP_END
+
+JSObject*
+SVGAnimatedLength::WrapObject(JSContext* aCx, JSObject* aScope, bool* aTriedToWrap)
+{
+  return SVGAnimatedLengthBinding::Wrap(aCx, aScope, this, aTriedToWrap);
+}
+
+already_AddRefed<nsIDOMSVGLength>
+SVGAnimatedLength::BaseVal()
+{
+  nsRefPtr<nsIDOMSVGLength> angle;
+  mVal->ToDOMBaseVal(getter_AddRefs(angle), mSVGElement);
+  return angle.forget();
+}
+
+already_AddRefed<nsIDOMSVGLength>
+SVGAnimatedLength::AnimVal()
+{
+  nsRefPtr<nsIDOMSVGLength> angle;
+  mVal->ToDOMAnimVal(getter_AddRefs(angle), mSVGElement);
+  return angle.forget();
+}
+
+} 
+} 
