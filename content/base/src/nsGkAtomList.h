300da39759e6619cebcb6bb87c6b44cd3d9fb28a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:56:21 +0000

    Bug 810399 - Add new css selectors and media queries for windows os versions. r=bz
    UltraBlame original commit: 87b5a0591affd80a7d0eab6e9a3a8cde373d0cdd

--- content/base/src/nsGkAtomList.h
+++ content/base/src/nsGkAtomList.h
@@ -2016,6 +2016,13 @@ GK_ATOM(windows_theme_zune, "windows-theme-zune")
 GK_ATOM(windows_theme_generic, "windows-theme-generic")
 
 
+
+GK_ATOM(windows_version_xp, "windows-version-xp")
+GK_ATOM(windows_version_vista, "windows-version-vista")
+GK_ATOM(windows_version_win7, "windows-version-win7")
+GK_ATOM(windows_version_win8, "windows-version-win8")
+
+
 GK_ATOM(_moz_scrollbar_start_backward, "-moz-scrollbar-start-backward")
 GK_ATOM(_moz_scrollbar_start_forward, "-moz-scrollbar-start-forward")
 GK_ATOM(_moz_scrollbar_end_backward, "-moz-scrollbar-end-backward")
@@ -2031,6 +2038,7 @@ GK_ATOM(_moz_windows_compositor, "-moz-windows-compositor")
 GK_ATOM(_moz_windows_classic, "-moz-windows-classic")
 GK_ATOM(_moz_windows_glass, "-moz-windows-glass")
 GK_ATOM(_moz_windows_theme, "-moz-windows-theme")
+GK_ATOM(_moz_os_version, "-moz-os-version")
 GK_ATOM(_moz_touch_enabled, "-moz-touch-enabled")
 GK_ATOM(_moz_maemo_classic, "-moz-maemo-classic")
 GK_ATOM(_moz_menubar_drag, "-moz-menubar-drag")
