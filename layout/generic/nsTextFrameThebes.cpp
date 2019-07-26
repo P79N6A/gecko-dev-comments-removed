e032331dd9513d73344a4ab9e00307e8fbc3d974
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:45:18 +0000

    Bug 821479.  r=roc
    UltraBlame original commit: 63ba23c13e9bfd61d6fb996c31c99ebab5380634

--- layout/generic/nsTextFrameThebes.cpp
+++ layout/generic/nsTextFrameThebes.cpp
@@ -104,8 +104,9 @@ struct TabWidth {
 };
 
 struct TabWidthStore {
-  TabWidthStore()
+  TabWidthStore(int32_t aValidForContentOffset)
     : mLimit(0)
+    , mValidForContentOffset(aValidForContentOffset)
   { }
 
   
@@ -118,7 +119,10 @@ struct TabWidthStore {
   
   
   uint32_t mLimit;
+ 
   
+  int32_t mValidForContentOffset;
+
   
   nsTArray<TabWidth> mWidths;
 };
@@ -2477,6 +2481,11 @@ nsTextFrame::EnsureTextRun(TextRunType aWhichTextRun,
       static const gfxSkipChars emptySkipChars;
       return gfxSkipCharsIterator(emptySkipChars, 0);
     }
+    TabWidthStore* tabWidths =
+      static_cast<TabWidthStore*>(Properties().Get(TabWidthProperty()));
+    if (tabWidths && tabWidths->mValidForContentOffset != GetContentOffset()) {
+      Properties().Delete(TabWidthProperty());
+    }
   }
 
   if (textRun->GetFlags() & nsTextFrameUtils::TEXT_IS_SIMPLE_FLOW) {
@@ -3064,7 +3073,7 @@ PropertyProvider::CalcTabWidths(uint32_t aStart, uint32_t aLength)
         }
       } else {
         if (!mTabWidths) {
-          mTabWidths = new TabWidthStore();
+          mTabWidths = new TabWidthStore(mFrame->GetContentOffset());
           mFrame->Properties().Set(TabWidthProperty(), mTabWidths);
         }
         double nextTab = AdvanceToNextTab(mOffsetFromBlockOriginForTabs,
