1551488e7ed72e2e7ba4591f72ce36c61c781429
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:35:01 +0000

    Remove two extra instances of whenNewWindowLoaded, no bug
    It's usually enough to define each function once in Javascript.
    No need to keep repeating things to the compiler.  ;-)
    
    UltraBlame original commit: ec65853affc878329af4367147bd9c4d0ca29181

--- toolkit/components/places/tests/browser/head.js
+++ toolkit/components/places/tests/browser/head.js
@@ -115,22 +115,6 @@ function fieldForUrl(aURI, aFieldName, aCallback)
   stmt.finalize();
 }
 
-function whenNewWindowLoaded(aOptions, aCallback) {
-  let win = OpenBrowserWindow(aOptions);
-  win.addEventListener("load", function onLoad() {
-    win.removeEventListener("load", onLoad, false);
-    aCallback(win);
-  }, false);
-}
-
-function whenNewWindowLoaded(aOptions, aCallback) {
-  let win = OpenBrowserWindow(aOptions);
-  win.addEventListener("load", function onLoad() {
-    win.removeEventListener("load", onLoad, false);
-    aCallback(win);
-  }, false);
-}
-
 
 
 
