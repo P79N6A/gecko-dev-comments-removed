c3dbca08ff590f2ce07b44fa270755ad3adb0518
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:23:06 +0000

    Bug 1025875 part 7 - Make Sprinter::putString work with Latin1 strings. r=njn
    UltraBlame original commit: 56e4788545db4fc443b60980a56b0a0bf38b0879

---
+++ js/src/jit-test/tests/latin1/decompiler.js
@@ -0,0 +1,22 @@
+var s = toLatin1("someName");
+
+
+function f(someName) {
+    someName();
+}
+try {
+    f(3);
+} catch(e) {
+    assertEq(e.message.contains("someName"), true);
+}
+
+
+function g(someName\u1200) {
+    someName\u1200();
+}
+try {
+    g(3);
+} catch(e) {
+    
+    assertEq(e.message.contains("someName"), true);
+}
