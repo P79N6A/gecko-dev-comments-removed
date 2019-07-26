85c25d64f07f00496a9f28bd790e4cc2484f9a5b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:58:57 +0000

    Backout Bug 1009624, better patch needed
    UltraBlame original commit: 19bd36676edf28795d52f876cb7612fa2735f213

--- content/xul/content/src/nsXULElement.cpp
+++ content/xul/content/src/nsXULElement.cpp
@@ -2628,7 +2628,7 @@ nsXULPrototypeScript::Compile(JS::SourceBufferHolder& aSrcBuf,
                               nsIOffThreadScriptReceiver *aOffThreadReceiver )
 {
     
-    NS_ENSURE_STATE(aProtoDoc);
+    MOZ_ASSERT(aProtoDoc);
     NS_ENSURE_TRUE(xpc::GetCompilationScope(), NS_ERROR_UNEXPECTED);
     AutoSafeJSContext cx;
     JSAutoCompartment ac(cx, xpc::GetCompilationScope());
