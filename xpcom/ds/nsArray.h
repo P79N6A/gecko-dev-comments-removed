ca1267152dc03964b26a112e898bf26745d4d939
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:04:36 +0000

    Bug 792169 - introduce better, safer constructors for nsArray; r=bsmedberg
    UltraBlame original commit: 711db2ce69f083b080a3a7dbca817eb966c4059c

--- xpcom/ds/nsArray.h
+++ xpcom/ds/nsArray.h
@@ -20,23 +20,27 @@
 { 0x35c66fd1, 0x95e9, 0x4e0a, \
   { 0x80, 0xc5, 0xc3, 0xbd, 0x2b, 0x37, 0x54, 0x81 } }
 
-
-
-
-
-
 class nsArray : public nsIMutableArray
 {
 public:
-    nsArray() { }
-    nsArray(const nsCOMArray_base& aBaseArray) : mArray(aBaseArray)
-    { }
-    
     NS_DECL_ISUPPORTS
     NS_DECL_NSIARRAY
     NS_DECL_NSIMUTABLEARRAY
 
+    
+
+
+    static already_AddRefed<nsIMutableArray> Create();
+    
+
+    static nsresult XPCOMConstructor(nsISupports* aOuter, const nsIID& aIID,
+                                     void** aResult);
 protected:
+    nsArray() { }
+    nsArray(const nsArray& other);
+    nsArray(const nsCOMArray_base& aBaseArray) : mArray(aBaseArray)
+    { }
+    
     virtual ~nsArray(); 
 
     nsCOMArray_base mArray;
@@ -44,15 +48,17 @@ protected:
 
 class nsArrayCC MOZ_FINAL : public nsArray
 {
+    friend class nsArray;
+
 public:
-    nsArrayCC() : nsArray() { }
-    nsArrayCC(const nsCOMArray_base& aBaseArray) : nsArray(aBaseArray)
-    { }
-    
     NS_DECL_CYCLE_COLLECTING_ISUPPORTS
     NS_DECL_CYCLE_COLLECTION_CLASS(nsArrayCC)
-};
 
-nsresult nsArrayConstructor(nsISupports *aOuter, const nsIID& aIID, void **aResult);
+private:
+    nsArrayCC() : nsArray() { }
+    nsArrayCC(const nsArrayCC& other);
+    nsArrayCC(const nsCOMArray_base& aBaseArray) : nsArray(aBaseArray)
+    { }
+};
 
 #endif
