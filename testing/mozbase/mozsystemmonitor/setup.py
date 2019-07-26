bbf01c6bf57cc04a31419a96111b13d99afa279a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:49:08 +0000

    Bug 883205 - Add mozsystemmonitor to mozbase; r=ahal
    UltraBlame original commit: 46dc449aa545282e11104a0ef8c014f196908122

---
+++ testing/mozbase/mozsystemmonitor/setup.py
@@ -0,0 +1,29 @@
+
+
+
+
+import os
+
+from setuptools import setup
+
+PACKAGE_VERSION = '0.0'
+
+try:
+    pwd = os.path.dirname(os.path.abspath(__file__))
+    description = open(os.path.join(here, 'README.rst')).read()
+except:
+    description = ''
+
+setup(
+    name='mozsystemmonitor',
+    description='Monitor system resource usage.',
+    long_description=description,
+    license='MPL 2.0',
+    keywords='mozilla',
+    author='Mozilla Automation and Tools Team',
+    author_email='tools@lists.mozilla.org',
+    url='https://wiki.mozilla.org/Auto-tools/Projects/Mozbase',
+    packages=['mozsystemmonitor'],
+    version=PACKAGE_VERSION,
+    install_requires=['psutil >= 0.7.1'],
+)
