169a08291ea41137ed0c05173685c527bd552af2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:44:19 +0000

    Bug 878319: Move makeInfallible into its own JSM. r=dcamp
    UltraBlame original commit: 24e9d3a53f8b761d188c7b8077015af8dc991c9d

---
+++ toolkit/devtools/DevToolsUtils.jsm
@@ -0,0 +1,25 @@
+
+
+
+
+"use strict";
+
+
+
+
+
+
+
+
+
+this.EXPORTED_SYMBOLS = [ "DevToolsUtils" ];
+
+Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+  .getService(Components.interfaces.mozIJSSubScriptLoader)
+  .loadSubScript("resource://gre/modules/devtools/DevToolsUtils.js", this);
+
+this.DevToolsUtils = {
+  safeErrorString: safeErrorString,
+  reportException: reportException,
+  makeInfallible: makeInfallible
+};
--- toolkit/devtools/server/transport.js
+++ toolkit/devtools/server/transport.js
@@ -8,62 +8,6 @@
 Components.utils.import("resource://gre/modules/NetUtil.jsm");
 
 
-function safeErrorString(aError) {
-  try {
-    var s = aError.toString();
-    if (typeof s === "string")
-      return s;
-  } catch (ee) { }
-
-  return "<failed trying to find error description>";
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function makeInfallible(aHandler, aName) {
-  if (!aName)
-    aName = aHandler.name;
-
-  return function () {
-    try {
-      return aHandler.apply(this, arguments);
-    } catch (ex) {
-      let msg = "Handler function ";
-      if (aName) {
-        msg += aName + " ";
-      }
-      msg += "threw an exception: " + safeErrorString(ex);
-      if (ex.stack) {
-        msg += "\nCall stack:\n" + ex.stack;
-      }
-
-      dump(msg + "\n");
-
-      if (Cu.reportError) {
-        
-
-
-
-
-        Cu.reportError(msg);
-      }
-    }
-  }
-}
-
-
 
 
 
