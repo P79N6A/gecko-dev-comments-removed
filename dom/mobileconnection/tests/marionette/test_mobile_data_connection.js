820e98307567ed2f21f799355f224168bea0ecac
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:01:55 +0000

    Bug 979134 (follow-up): convert test_mobile_data_connection.js to Promise. r=vicamo
    UltraBlame original commit: 3d50702c465ae0e001f781da2b54b3e7ae471d28

--- dom/mobileconnection/tests/marionette/test_mobile_data_connection.js
+++ dom/mobileconnection/tests/marionette/test_mobile_data_connection.js
@@ -2,350 +2,110 @@
 
 
 MARIONETTE_TIMEOUT = 60000;
+MARIONETTE_HEAD_JS = "head.js";
 
-const DATA_KEY = "ril.data.enabled";
-const DATA_ROAMING_KEY = "ril.data.roaming_enabled";
-const APN_KEY = "ril.data.apnSettings";
-
-SpecialPowers.setBoolPref("dom.mozSettings.enabled", true);
-SpecialPowers.addPermission("mobileconnection", true, document);
-SpecialPowers.addPermission("settings-read", true, document);
-SpecialPowers.addPermission("settings-write", true, document);
-
-let settings = window.navigator.mozSettings;
-let connection = window.navigator.mozMobileConnections[0];
-ok(connection instanceof MozMobileConnection,
-   "connection is instanceof " + connection.constructor);
-
-
-let pendingEmulatorCmdCount = 0;
-function sendCmdToEmulator(cmd, callback) {
-  ++pendingEmulatorCmdCount;
-
-  runEmulatorCmd(cmd, function(result) {
-    --pendingEmulatorCmdCount;
-
-    is(result[0], "OK", "Emulator response");
-
-    if (callback) {
-      callback();
-    }
-  });
-}
-
-let tasks = {
-  
-  
-  _tasks: [],
-  _nextTaskIndex: 0,
-
-  push: function(func) {
-    this._tasks.push(func);
-  },
-
-  next: function() {
-    let index = this._nextTaskIndex++;
-    let task = this._tasks[index];
-    try {
-      task();
-    } catch (ex) {
-      ok(false, "test task[" + index + "] throws: " + ex);
-      
-      if (index != this._tasks.length - 1) {
-        this.finish();
-      }
-    }
-  },
-
-  finish: function() {
-    this._tasks[this._tasks.length - 1]();
-  },
-
-  run: function() {
-    this.next();
-  }
-};
-
-function setSetting(key, value, callback) {
-  let setLock = settings.createLock();
-  let obj = {};
-  obj[key] = value;
-
-  let setReq = setLock.set(obj);
-  setReq.addEventListener("success", function onSetSuccess() {
-    ok(true, "set '" + key + "' to " + obj[key]);
-    if (callback) {
-      callback();
-    }
-  });
-  setReq.addEventListener("error", function onSetError() {
-    ok(false, "cannot set '" + key + "'");
-    tasks.finish();
-  });
-}
-
-function getSetting(key, callback) {
-  let getLock = settings.createLock();
-
-  let getReq = getLock.get(key);
-  getReq.addEventListener("success", function onGetSuccess() {
-    ok(true, "get " + key + " setting okay");
-    let value = getReq.result[key];
-    callback(value);
-  });
-  getReq.addEventListener("error", function onGetError() {
-    ok(false, "cannot get '" + key + "'");
-    tasks.finish();
-  });
-}
-
-function setEmulatorAPN(callback) {
-  let apn =
-    [
-      [
-        {"carrier":"T-Mobile US",
-         "apn":"epc.tmobile.com",
-         "mmsc":"http://mms.msg.eng.t-mobile.com/mms/wapenc",
-         "types":["default","supl","mms"]}
-      ]
-    ];
-  setSetting(APN_KEY, apn, callback);
-}
-
-function setEmulatorRoaming(roaming, callback) {
-  log("Setting emulator roaming state: " + roaming + ".");
-
-  
-  let cmd = "gsm voice " + (roaming ? "roaming" : "home");
-  sendCmdToEmulator(cmd, function() {
-
-    connection.addEventListener("voicechange", function onvoicechange() {
-      connection.removeEventListener("voicechange", onvoicechange);
-      log("mobileConnection.voice.roaming is now '"
-        + connection.voice.roaming + "'.");
-      is(connection.voice.roaming, roaming, "voice.roaming");
-
-      let cmd = "gsm data " + (roaming ? "roaming" : "home");
-      sendCmdToEmulator(cmd, function() {
-
-        connection.addEventListener("datachange", function ondatachange() {
-          connection.removeEventListener("datachange", ondatachange);
-          log("mobileConnection.data.roaming is now '"
-            + connection.data.roaming + "'.");
-          is(connection.data.roaming, roaming, "data.roaming");
-          if (callback) {
-            callback();
-          }
-        });
-      });
-    });
-  });
-}
-
-function setEmulatorHome(callback) {
-  let voiceRegistration = false;
-  let dataRegistration = false;
-
-  if (connection.voice.state != "registered") {
-    sendCmdToEmulator("gsm voice home", function() {
-      connection.addEventListener("voicechange", function onvoicechange() {
-        connection.removeEventListener("voicechange", onvoicechange);
-        log("mobileConnection.voice.state is now '"
-          + connection.voice.state + "'.");
-        is(connection.voice.state, "registered", "voice.state");
-        voiceRegistration = true;
-      });
-    });
-  } else {
-    voiceRegistration = true;
+function checkOrWaitForDataState(connected) {
+  if (mobileConnection.data.connected == connected) {
+    log("data.connected is now " + mobileConnection.data.connected);
+    return;
   }
 
-  if (connection.data.state != "registered") {
-    sendCmdToEmulator("gsm data home", function() {
-      connection.addEventListener("datachange", function ondatachange() {
-        connection.removeEventListener("datachange", ondatachange);
-        log("mobileConnection.data.state is now '"
-          + connection.data.state + "'.");
-        is(connection.data.state, "registered", "data.state");
-        dataRegistration = true;
-      });
-    });
-  } else {
-    dataRegistration = true;
-  }
-
-  waitFor(callback, function() {
-    return (voiceRegistration && dataRegistration);
-  });
+  return waitForManagerEvent("datachange")
+    .then(() => checkOrWaitForDataState(connected));
 }
 
-
-tasks.push(function verifyInitialState() {
+function verifyInitialState() {
   log("Verifying initial state.");
 
   
-  
-  setEmulatorHome(function() {
-    
-    
-    getSetting(DATA_KEY, function(result) {
-      let value = result;
-      log("Starting data enabled: " + value);
-      if (value) {
-        setSetting(DATA_KEY, false);
-
-        connection.addEventListener("datachange", function ondatachange() {
-          connection.removeEventListener("datachange", ondatachange);
-          log("mobileConnection.data.connected is now '"
-            + connection.data.connected + "'.");
-          is(connection.data.connected, false, "data.connected");
-          setEmulatorAPN(function() {
-            tasks.next();
-          });
-        });
-      } else {
-        setEmulatorAPN(function() {
-          tasks.next();
-        });
-      }
+  return Promise.resolve()
+    .then(function() {
+      is(mobileConnection.voice.state, "registered", "voice.state");
+      is(mobileConnection.data.state, "registered", "data.state");
+      is(mobileConnection.voice.roaming, false, "voice.roaming");
+      is(mobileConnection.data.roaming, false, "data.roaming");
+    })
+    .then(getDataEnabled)
+    .then(function(aResult) {
+      is(aResult, false, "Data must be off.")
     });
-  });
-});
+}
 
-tasks.push(function testEnableData() {
+function testEnableData() {
   log("Turn data on.");
 
-  connection.addEventListener("datachange", function ondatachange() {
-    connection.removeEventListener("datachange", ondatachange);
-    log("mobileConnection.data.connected is now '"
-      + connection.data.connected + "'.");
-    is(connection.data.connected, true, "data.connected");
-    tasks.next();
-  });
+  return setDataEnabledAndWait(true);
+}
 
-  setSetting(DATA_KEY, true);
-});
-
-tasks.push(function testUnregisterDataWhileDataEnabled() {
+function testUnregisterDataWhileDataEnabled() {
   log("Set data registration unregistered while data enabled.");
 
   
   
-  sendCmdToEmulator("gsm data unregistered", function() {
-    connection.addEventListener("datachange", function ondatachange() {
-      log("mobileConnection.data.state is now '"
-        + connection.data.state + "'.");
-      if (connection.data.state == "notSearching") {
-        connection.removeEventListener("datachange", ondatachange);
-        log("mobileConnection.data.connected is now '"
-          + connection.data.connected + "'.");
-        is(connection.data.connected, false, "data.connected");
-        tasks.next();
-      }
-    });
-  });
-});
+  return setEmulatorVoiceDataStateAndWait("data", "unregistered")
+    .then(() => checkOrWaitForDataState(false));
+}
 
-tasks.push(function testRegisterDataWhileDataEnabled() {
+function testRegisterDataWhileDataEnabled() {
   log("Set data registration home while data enabled.");
 
   
   
-  sendCmdToEmulator("gsm data home", function() {
-    connection.addEventListener("datachange", function ondatachange() {
-      connection.removeEventListener("datachange", ondatachange);
-      log("mobileConnection.data.state is now '"
-        + connection.data.state + "'.");
-      is(connection.data.state, "registered", "data.state");
+  return setEmulatorVoiceDataStateAndWait("data", "home")
+    .then(() => checkOrWaitForDataState(true));
+}
 
-      connection.addEventListener("datachange", function ondatachange() {
-        connection.removeEventListener("datachange", ondatachange);
-        log("mobileConnection.data.connected is now '"
-          + connection.data.connected + "'.");
-        is(connection.data.connected, true, "data.connected");
-        tasks.next();
-      });
-    });
-  });
-});
-
-tasks.push(function testDisableDataRoamingWhileRoaming() {
+function testDisableDataRoamingWhileRoaming() {
   log("Disable data roaming while roaming.");
 
-  setSetting(DATA_ROAMING_KEY, false);
-
   
   
-  setEmulatorRoaming(true, function() {
-    connection.addEventListener("datachange", function ondatachange() {
-      connection.removeEventListener("datachange", ondatachange);
-      log("mobileConnection.data.connected is now '"
-        + connection.data.connected + "'.");
-      is(connection.data.connected, false, "data.connected");
-      tasks.next();
-    });
-  });
-});
+  return setEmulatorRoamingAndWait(true)
+    .then(() => checkOrWaitForDataState(false));
+}
 
-tasks.push(function testEnableDataRoamingWhileRoaming() {
+function testEnableDataRoamingWhileRoaming() {
   log("Enable data roaming while roaming.");
 
   
-  connection.addEventListener("datachange", function ondatachange() {
-    connection.removeEventListener("datachange", ondatachange);
-    log("mobileConnection.data.connected is now '"
-      + connection.data.connected + "'.");
-    is(connection.data.connected, true, "data.connected");
-    tasks.next();
-  });
+  return setDataRoamingEnabled(true)
+    .then(() => checkOrWaitForDataState(true));
+}
 
-  setSetting(DATA_ROAMING_KEY, true);
-});
-
-tasks.push(function testDisableDataRoamingWhileNotRoaming() {
-  log("Disable data roaming while not roaming.");
-
-  
-  
-  setEmulatorRoaming(false, function() {
-    setSetting(DATA_ROAMING_KEY, false);
-
-    
-    
-    window.setTimeout(function() {
-      is(connection.data.connected, true, "data.connected");
-      tasks.next();
-    }, 1000);
-  });
-});
-
-tasks.push(function testDisableData() {
+function testDisableData() {
   log("Turn data off.");
 
-  connection.addEventListener("datachange", function ondatachange() {
-    connection.removeEventListener("datachange", ondatachange);
-    log("mobileConnection.data.connected is now '"
-      + connection.data.connected + "'.");
-    is(connection.data.connected, false, "data.connected");
-    tasks.next();
-  });
+  return setDataEnabledAndWait(false);
+}
 
-  setSetting(DATA_KEY, false);
-});
+startTestCommon(function() {
 
+  let origApnSettings;
+  return verifyInitialState()
+    .then(() => getDataApnSettings())
+    .then(value => {
+      origApnSettings = value;
+    })
+    .then(() => {
+      let apnSettings = [[{ "carrier": "T-Mobile US",
+                            "apn": "epc.tmobile.com",
+                            "mmsc": "http://mms.msg.eng.t-mobile.com/mms/wapenc",
+                            "types": ["default","supl","mms"] }]];
+      return setDataApnSettings(apnSettings);
+    })
+    .then(() => testEnableData())
+    .then(() => testUnregisterDataWhileDataEnabled())
+    .then(() => testRegisterDataWhileDataEnabled())
+    .then(() => testDisableDataRoamingWhileRoaming())
+    .then(() => testEnableDataRoamingWhileRoaming())
+    .then(() => testDisableData())
+    
+    .then(() => {
+      if (origApnSettings) {
+        return setDataApnSettings(origApnSettings);
+      }
+    })
+    .then(() => setEmulatorRoamingAndWait(false))
+    .then(() => setDataRoamingEnabled(false));
 
-tasks.push(function cleanUp() {
-  if (pendingEmulatorCmdCount) {
-    window.setTimeout(cleanUp, 100);
-    return;
-  }
-
-  SpecialPowers.removePermission("mobileconnection", document);
-  SpecialPowers.removePermission("settings-write", document);
-  SpecialPowers.removePermission("settings-read", document);
-  SpecialPowers.clearUserPref("dom.mozSettings.enabled");
-  finish();
-});
-
-tasks.run();
-
+}, ["settings-read", "settings-write"]);
