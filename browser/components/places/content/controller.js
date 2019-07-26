79cfcc07be04a2a98fb2b1628a292b937b65882d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:47:07 +0000

    Bug 832949 - Disable common Organize options when the Downloads view is in use. r=mak.
    UltraBlame original commit: ff45a29c20725893ef0b47d6253623212d7a7e1b

--- browser/components/places/content/controller.js
+++ browser/components/places/content/controller.js
@@ -1614,11 +1614,9 @@ XPCOMUtils.defineLazyServiceGetter(PlacesControllerDragHelper, "dragService",
 function goUpdatePlacesCommands() {
   
   var placesController = doGetPlacesControllerForCommand("placesCmd_open");
-  if (!placesController)
-    return;
-
   function updatePlacesCommand(aCommand) {
-    goSetCommandEnabled(aCommand, placesController.isCommandEnabled(aCommand));
+    goSetCommandEnabled(aCommand, placesController &&
+                                  placesController.isCommandEnabled(aCommand));
   }
 
   updatePlacesCommand("placesCmd_open");
