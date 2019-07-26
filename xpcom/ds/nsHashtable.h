a85dc4dda42b9c5622338626250a6c1feb91af07
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:45:12 +0000

    Bug 968012, part 3 - Remove nsVoidKey which was only used by nsWindowDataSource. r=froydnj
    UltraBlame original commit: bbe567913a6dd6424a3ee378521a3ce37d309f32

--- xpcom/ds/nsHashtable.h
+++ xpcom/ds/nsHashtable.h
@@ -259,43 +259,6 @@ public:
 };
 
 
-
-
-class nsVoidKey : public nsHashKey {
-  protected:
-    void* mKey;
-
-  public:
-    nsVoidKey(const nsVoidKey& aKey) : mKey(aKey.mKey) {
-#ifdef DEBUG
-        mKeyType = aKey.mKeyType;
-#endif
-    }
-
-    nsVoidKey(void* key) {
-#ifdef DEBUG
-        mKeyType = VoidKey;
-#endif
-        mKey = key;
-    }
-
-    uint32_t HashCode(void) const {
-        return NS_PTR_TO_INT32(mKey);
-    }
-
-    bool Equals(const nsHashKey *aKey) const {
-        NS_ASSERTION(aKey->GetKeyType() == VoidKey, "mismatched key types");
-        return (mKey == ((const nsVoidKey *) aKey)->mKey);
-    }
-
-    nsHashKey *Clone() const {
-        return new nsVoidKey(mKey);
-    }
-
-    void* GetValue() { return mKey; }
-};
-
-
 class nsCStringKey : public nsHashKey {
   public:
 
