347bd075d196f74b330d17e74eb2a38f28a0c3c8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:37:31 +0000

    Bug 815352 (Part 2) - Add a stub view to implement in-content downloads. r=mak
    UltraBlame original commit: 8881a95c8b2d4c51d3a57bb008809fc3c7d16845

---
+++ browser/components/downloads/content/contentAreaDownloadsView.js
@@ -0,0 +1,10 @@
+
+
+
+
+let ContentAreaDownloadsView = {
+  init: function CADV_init() {
+    let view = new DownloadsPlacesView(document.getElementById("downloadsRichListBox"));
+    view.place = "place:transition=7&sort=4";
+  }
+};
