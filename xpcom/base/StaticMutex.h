a4bae5af43d75c0e48a44f708712b65b76f9864a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:39:41 +0000

    Bug 873801 - Add StaticMutex, which is like StaticAutoPtr<OffTheBooksMutex>, except it's lazily initialized in a threadsafe manner. r=khuey
    UltraBlame original commit: bfe1d6ce6f3ecb3db8f1f82b9fc0fb13d0ce77fd

---
+++ xpcom/base/StaticMutex.h
@@ -0,0 +1,83 @@
+#ifndef mozilla_StaticMutex_h
+#define mozilla_StaticMutex_h
+
+#include "mozilla/Atomics.h"
+#include "mozilla/Mutex.h"
+
+namespace mozilla {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class StaticMutex
+{
+public:
+  
+  
+  
+#ifdef DEBUG
+  StaticMutex()
+  {
+    MOZ_ASSERT(!mMutex);
+  }
+#endif
+
+  void Lock()
+  {
+    Mutex()->Lock();
+  }
+
+  void Unlock()
+  {
+    Mutex()->Unlock();
+  }
+
+private:
+  OffTheBooksMutex* Mutex()
+  {
+    if (mMutex) {
+      return mMutex;
+    }
+
+    OffTheBooksMutex* mutex = new OffTheBooksMutex("StaticMutex");
+    if (!mMutex.compareExchange(nullptr, mutex)) {
+      delete mutex;
+    }
+
+    return mMutex;
+  }
+
+  Atomic<OffTheBooksMutex*> mMutex;
+
+
+  
+  
+  
+  
+#ifdef DEBUG
+  StaticMutex(StaticMutex &other);
+#endif
+
+  
+  StaticMutex& operator=(StaticMutex* rhs);
+  static void* operator new(size_t) CPP_THROW_NEW;
+  static void operator delete(void*);
+};
+
+typedef BaseAutoLock<StaticMutex> StaticMutexAutoLock;
+typedef BaseAutoUnlock<StaticMutex> StaticMutexAutoUnlock;
+
+} 
+
+#endif
