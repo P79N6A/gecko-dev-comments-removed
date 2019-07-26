46f396c1dfa4f82ea31fd3a0ac5610a13b0387af
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:00:03 +0000

    Bug 983489 - Delay destroying mCompositor to prevent crash. r=nical
    UltraBlame original commit: 3939dc732041919f02d2c092bdbc902f7633a8bf

--- gfx/layers/ipc/CompositorParent.cpp
+++ gfx/layers/ipc/CompositorParent.cpp
@@ -251,7 +251,11 @@ CompositorParent::Destroy()
 
   
   mLayerManager = nullptr;
+  if (mCompositor) {
+    mCompositor->Destroy();
+  }
   mCompositor = nullptr;
+
   mCompositionManager = nullptr;
   mApzcTreeManager->ClearTree();
   mApzcTreeManager = nullptr;
@@ -283,7 +287,6 @@ CompositorParent::RecvWillStop()
     }
     mLayerManager->Destroy();
     mLayerManager = nullptr;
-    mCompositor = nullptr;
     mCompositionManager = nullptr;
   }
 
