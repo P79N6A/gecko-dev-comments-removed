35241748ec4e4f74d6a83d8c2f2a54b564de7631
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:03:30 +0000

    Bug 823236 - Fix the lifetime of SharedPlanarYCbCrImage/SharedRGBImage. r=nical
    UltraBlame original commit: 13974e5104c95c52d04b7209edd48ddd7e777802

--- gfx/layers/ipc/ImageContainerChild.cpp
+++ gfx/layers/ipc/ImageContainerChild.cpp
@@ -127,11 +127,31 @@ bool ImageContainerChild::RecvReturnImage(const SharedImage& aImage)
 {
   SharedImage* img = new SharedImage(aImage);
   
+  
+  
+  nsRefPtr<Image> image;
+  
   if (mImageQueue.Length() > 0) {
+    image = mImageQueue[0];
     mImageQueue.RemoveElementAt(0);
   }
-  if (!AddSharedImageToPool(img) || mStop) {
-    DestroySharedImage(*img);
+  bool isSharedImage;
+  if (image && image->GetFormat() == PLANAR_YCBCR) {
+    SharedPlanarYCbCrImage* sharedYCbCr
+      = static_cast<PlanarYCbCrImage*>(image.get())->AsSharedPlanarYCbCrImage();
+    isSharedImage = !!sharedYCbCr;
+  } else if (image && image->GetFormat() == SHARED_RGB) {
+    SharedRGBImage *rgbImage = static_cast<SharedRGBImage*>(image.get());
+    isSharedImage = !!rgbImage;
+  } else {
+    isSharedImage = false;
+  }
+  if (!isSharedImage) {
+    if (!AddSharedImageToPool(img) || mStop) {
+      delete img;
+      DestroySharedImage(*img);
+    }
+  } else {
     delete img;
   }
   return true;
@@ -383,6 +403,7 @@ void ImageContainerChild::SendImageAsync(ImageContainer* aContainer,
 
   if (InImageBridgeChildThread()) {
     SendImageNow(aImage);
+    return;
   }
 
   
@@ -451,7 +472,7 @@ bool ImageContainerChild::AllocUnsafeShmemSync(size_t aBufSize,
     return false;
   }
   if (InImageBridgeChildThread()) {
-    AllocUnsafeShmem(aBufSize, aType, aShmem);
+    return AllocUnsafeShmem(aBufSize, aType, aShmem);
   }
   ReentrantMonitor barrier("ImageContainerChild::AllocUnsafeShmemSync");
   ReentrantMonitorAutoEnter autoBarrier(barrier);
