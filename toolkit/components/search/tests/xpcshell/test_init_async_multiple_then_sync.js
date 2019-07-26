63e69b6a45582d829448ab9478540d93cd21ea44
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:00:36 +0000

    Bug 843479 - More logging for test_init_async_multiple_then_sync.js.
    UltraBlame original commit: 35d80dce901a35db8dd026dda4fe7b14eeef1bb6

--- toolkit/components/search/tests/xpcshell/test_init_async_multiple_then_sync.js
+++ toolkit/components/search/tests/xpcshell/test_init_async_multiple_then_sync.js
@@ -36,26 +36,33 @@ function run_test() {
 
   
   let init_complete = function init_complete(i) {
+    do_print("init complete " + i);
     do_check_true(pending[i]);
     pending[i] = false;
     numberPending--;
     do_check_true(numberPending >= 0);
     do_check_true(Services.search.isInitialized);
-    if (numberPending == 0) {
-      
-      let engines = Services.search.getEngines();
-      do_check_neq(engines, null);
-
-      
-      
-      do_timeout(1000, function() {
-        do_test_finished();
-      });
+    if (numberPending != 0) {
+      do_print("Still waiting for the following initializations: " + JSON.stringify(pending));
+      return;
     }
+    do_print("All initializations have completed");
+    
+    let engines = Services.search.getEngines();
+    do_check_neq(engines, null);
+
+    do_print("Waiting a second before quitting");
+    
+    
+    do_timeout(1000, function() {
+      do_print("Test is complete");
+      do_test_finished();
+    });
   };
 
   
   let engines = Services.search.getEngines();
   do_check_neq(engines, null);
+  do_print("Synchronous part of the test complete");
 }
 
