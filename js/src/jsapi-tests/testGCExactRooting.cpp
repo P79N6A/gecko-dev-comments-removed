941920b2055a8dc9984d063071170012a1cf96a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:48:06 +0000

    Bug 880697 - Add JSRuntime constructor for Rooted. r=terrence
    UltraBlame original commit: 444fffdcf768a6dc1988dd511e9e9583e465cb63

---
+++ js/src/jsapi-tests/testGCExactRooting.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+#include "tests.h"
+
+BEGIN_TEST(testGCExactRooting)
+{
+    JS::RootedObject rootCx(cx, JS_NewObject(cx, NULL, NULL, NULL));
+    JS::RootedObject rootRt(cx->runtime(), JS_NewObject(cx, NULL, NULL, NULL));
+
+    JS_GC(cx->runtime());
+
+    
+    JS_DefineProperty(cx, rootCx, "foo", JS::DoubleValue(0), NULL, NULL, 0);
+    JS_DefineProperty(cx, rootRt, "foo", JS::DoubleValue(0), NULL, NULL, 0);
+
+    return true;
+}
+END_TEST(testGCExactRooting)
