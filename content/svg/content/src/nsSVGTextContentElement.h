fa9796011781cb5ae081370ea4f66606d8ca9ba2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:41:18 +0000

    Bug 824229 Part 2: SVGGraphicsElement should implement SVGTests r=longsonr
    UltraBlame original commit: 0515cea84af1d3b291bfd1f73208d5041c1fd30d

--- content/svg/content/src/nsSVGTextContentElement.h
+++ content/svg/content/src/nsSVGTextContentElement.h
@@ -6,7 +6,6 @@
 #ifndef __NS_SVGTEXTCONTENTELEMENTBASE_H__
 #define __NS_SVGTEXTCONTENTELEMENTBASE_H__
 
-#include "DOMSVGTests.h"
 #include "nsIDOMSVGTextContentElement.h"
 #include "nsSVGElement.h"
 #include "nsSVGTextContainerFrame.h"
@@ -19,8 +18,7 @@ typedef nsSVGElement nsSVGTextContentElementBase;
 
 
 
-class nsSVGTextContentElement : public nsSVGTextContentElementBase,
-                                public DOMSVGTests
+class nsSVGTextContentElement : public nsSVGTextContentElementBase
 {
 public:
   NS_DECL_ISUPPORTS_INHERITED
