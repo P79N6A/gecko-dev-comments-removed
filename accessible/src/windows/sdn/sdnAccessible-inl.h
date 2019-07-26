faf7165c05b9530bad7ce563d7cb7bec1c08a952
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:29:59 +0000

    Bug 767756 - try implementing ISimpleDOMNode with a tear off, r=tbsaunde, f=marcoz
    UltraBlame original commit: 19eb532fad5af9abcc06348b36e5bd395bfad9bd

---
+++ accessible/src/windows/sdn/sdnAccessible-inl.h
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+#ifndef mozilla_a11y_sdnAccessible_inl_h_
+#define mozilla_a11y_sdnAccessible_inl_h_
+
+#include "sdnAccessible.h"
+
+#include "DocAccessible.h"
+#include "nsAccessibilityService.h"
+
+namespace mozilla {
+namespace a11y {
+
+inline DocAccessible*
+sdnAccessible::GetDocument() const
+{
+  DocManager* docMgr = GetAccService();
+  return docMgr ?
+    docMgr->GetDocAccessibleFromCache(mNode->OwnerDoc()) : nullptr;
+}
+
+inline Accessible*
+sdnAccessible::GetAccessible() const
+{
+  DocAccessible* document = GetDocument();
+  return document ? document->GetAccessible(mNode) : nullptr;
+}
+
+} 
+} 
+
+#endif 
---
+++ accessible/src/windows/sdn/sdnAccessible.h
@@ -0,0 +1,113 @@
+
+
+
+
+
+
+#ifndef mozilla_a11y_sdnAccessible_h_
+#define mozilla_a11y_sdnAccessible_h_
+
+#include "ISimpleDOMNode.h"
+#include "AccessibleWrap.h"
+
+#include "mozilla/Attributes.h"
+
+namespace mozilla {
+namespace a11y {
+
+class sdnAccessible : public ISimpleDOMNode
+{
+public:
+  sdnAccessible(nsINode* aNode) : mNode(aNode) { }
+  ~sdnAccessible() { }
+
+  
+
+
+  bool IsDefunct() const { return !GetDocument(); }
+
+  
+
+
+  DocAccessible* GetDocument() const;
+
+  
+
+
+  Accessible* GetAccessible() const;
+
+  
+  DECL_IUNKNOWN
+
+  virtual  HRESULT STDMETHODCALLTYPE get_nodeInfo(
+     BSTR __RPC_FAR* aNodeName,
+     short __RPC_FAR* aNameSpaceID,
+     BSTR __RPC_FAR* aNodeValue,
+     unsigned int __RPC_FAR* aNumChildren,
+     unsigned int __RPC_FAR* aUniqueID,
+     unsigned short __RPC_FAR* aNodeType);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_attributes(
+     unsigned short aMaxAttribs,
+     BSTR __RPC_FAR* aAttribNames,
+     short __RPC_FAR* aNameSpaceIDs,
+     BSTR __RPC_FAR* aAttribValues,
+     unsigned short __RPC_FAR* aNumAttribs);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_attributesForNames(
+     unsigned short aMaxAttribs,
+     BSTR __RPC_FAR* aAttribNames,
+     short __RPC_FAR* aNameSpaceID,
+     BSTR __RPC_FAR* aAttribValues);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_computedStyle(
+     unsigned short aMaxStyleProperties,
+     boolean aUseAlternateView,
+     BSTR __RPC_FAR* aStyleProperties,
+     BSTR __RPC_FAR* aStyleValues,
+     unsigned short __RPC_FAR* aNumStyleProperties);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_computedStyleForProperties(
+     unsigned short aNumStyleProperties,
+     boolean aUseAlternateView,
+     BSTR __RPC_FAR* aStyleProperties,
+     BSTR __RPC_FAR* aStyleValues);
+
+  virtual HRESULT STDMETHODCALLTYPE scrollTo( boolean aScrollTopLeft);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_parentNode(
+     ISimpleDOMNode __RPC_FAR *__RPC_FAR* aNode);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_firstChild(
+     ISimpleDOMNode __RPC_FAR *__RPC_FAR* aNode);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_lastChild(
+     ISimpleDOMNode __RPC_FAR *__RPC_FAR* aNode);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_previousSibling(
+     ISimpleDOMNode __RPC_FAR *__RPC_FAR* aNode);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_nextSibling(
+     ISimpleDOMNode __RPC_FAR *__RPC_FAR* aNode);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_childAt(
+     unsigned aChildIndex,
+     ISimpleDOMNode __RPC_FAR *__RPC_FAR* aNode);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_innerHTML(
+     BSTR __RPC_FAR* aInnerHTML);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_localInterface(
+     void __RPC_FAR *__RPC_FAR* aLocalInterface);
+
+  virtual  HRESULT STDMETHODCALLTYPE get_language(
+     BSTR __RPC_FAR* aLanguage);
+
+private:
+  nsCOMPtr<nsINode> mNode;
+};
+
+} 
+} 
+
+#endif 
