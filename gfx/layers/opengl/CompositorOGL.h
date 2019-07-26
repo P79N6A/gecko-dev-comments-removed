d8a3837477dc112bc151bbc1d11510bb9904e6e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:16:21 +0000

    Merge inbound to m-c
    UltraBlame original commit: df75761307b6330ad14f68edbc15de3b8d7e606f

--- gfx/layers/opengl/CompositorOGL.h
+++ gfx/layers/opengl/CompositorOGL.h
@@ -206,6 +206,7 @@ public:
 
 
   virtual void EndFrame() MOZ_OVERRIDE;
+  virtual void SetFBAcquireFence(Layer* aLayer) MOZ_OVERRIDE;
   virtual void EndFrameForExternalComposition(const gfx::Matrix& aTransform) MOZ_OVERRIDE;
   virtual void AbortFrame() MOZ_OVERRIDE;
 
