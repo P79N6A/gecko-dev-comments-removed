703c7f726d83dd4ce73a2650767ab9e637518b03
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:02:25 +0000

    Bug 969786: Record the introduction script in ScriptSourceObjects. r=sfink
    UltraBlame original commit: 6f174fcd2b20bbad88f9f019a6c97afc034b45fb

--- js/src/jsscript.cpp
+++ js/src/jsscript.cpp
@@ -1270,6 +1270,31 @@ ScriptSourceObject::elementAttributeName() const
 }
 
 void
+ScriptSourceObject::initIntroductionScript(JSScript *script)
+{
+    JS_ASSERT(!getReservedSlot(INTRODUCTION_SCRIPT_SLOT).toPrivate());
+
+    
+    
+    
+    
+    
+    if (script && script->compartment() == compartment())
+        setReservedSlot(INTRODUCTION_SCRIPT_SLOT, PrivateValue(script));
+}
+
+void
+ScriptSourceObject::trace(JSTracer *trc, JSObject *obj)
+{
+    ScriptSourceObject *sso = static_cast<ScriptSourceObject *>(obj);
+
+    if (JSScript *script = sso->introductionScript()) {
+        MarkScriptUnbarriered(trc, &script, "ScriptSourceObject introductionScript");
+        sso->setReservedSlot(INTRODUCTION_SCRIPT_SLOT, PrivateValue(script));
+    }
+}
+
+void
 ScriptSourceObject::finalize(FreeOp *fop, JSObject *obj)
 {
     
@@ -1287,7 +1312,11 @@ const Class ScriptSourceObject::class_ = {
     JS_EnumerateStub,
     JS_ResolveStub,
     JS_ConvertStub,
-    ScriptSourceObject::finalize
+    finalize,
+    nullptr,                
+    nullptr,                
+    nullptr,                
+    trace
 };
 
 ScriptSourceObject *
@@ -1306,6 +1335,9 @@ ScriptSourceObject::create(ExclusiveContext *cx, ScriptSource *source,
         sourceObject->initSlot(ELEMENT_PROPERTY_SLOT, StringValue(options.elementAttributeName()));
     else
         sourceObject->initSlot(ELEMENT_PROPERTY_SLOT, UndefinedValue());
+
+    sourceObject->initSlot(INTRODUCTION_SCRIPT_SLOT, PrivateValue(nullptr));
+    sourceObject->initIntroductionScript(options.introductionScript());
 
     return sourceObject;
 }
