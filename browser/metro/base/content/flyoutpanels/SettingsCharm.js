9d7c6c3490daba5d945bc081809b75d1c6f4216b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:51:37 +0000

    Bug 941292 - Move charms controller in with flyout code. r=sfoster
    UltraBlame original commit: bf8935bf584724365aac0414b079375c2c83e4fd

---
+++ browser/metro/base/content/flyoutpanels/SettingsCharm.js
@@ -0,0 +1,67 @@
+
+
+
+
+var SettingsCharm = {
+  _entries: new Map(),
+  _nextId: 0,
+
+  
+
+
+
+
+  addEntry: function addEntry(aEntry) {
+    try {
+      let id = Services.metro.addSettingsPanelEntry(aEntry.label);
+      this._entries.set(id, aEntry);
+    } catch (e) {
+      
+      Cu.reportError(e);
+    }
+  },
+
+  init: function SettingsCharm_init() {
+    Services.obs.addObserver(this, "metro-settings-entry-selected", false);
+
+    
+    this.addEntry({
+        label: Strings.browser.GetStringFromName("optionsCharm"),
+        onselected: function() FlyoutPanelsUI.show('PrefsFlyoutPanel')
+    });
+
+
+
+
+
+
+
+
+
+    
+    this.addEntry({
+        label: Strings.browser.GetStringFromName("aboutCharm1"),
+        onselected: function() FlyoutPanelsUI.show('AboutFlyoutPanel')
+    });
+    
+    this.addEntry({
+        label: Strings.browser.GetStringFromName("helpOnlineCharm"),
+        onselected: function() {
+          let url = Services.urlFormatter.formatURLPref("app.support.baseURL");
+          BrowserUI.addAndShowTab(url, Browser.selectedTab);
+        }
+    });
+  },
+
+  observe: function SettingsCharm_observe(aSubject, aTopic, aData) {
+    if (aTopic == "metro-settings-entry-selected") {
+      let entry = this._entries.get(parseInt(aData, 10));
+      if (entry)
+        entry.onselected();
+    }
+  },
+
+  uninit: function SettingsCharm_uninit() {
+    Services.obs.removeObserver(this, "metro-settings-entry-selected");
+  }
+};
