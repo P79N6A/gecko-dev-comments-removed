60fb38cf57ba3c8ab968273d49b10bf1ac98e156
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:07:02 +0000

    Bug 784739 - Switch from NULL to nullptr in accessible/; r=ehsan
    UltraBlame original commit: 6120f41441ffccbd9870673f3b793d8f26236401

--- accessible/src/windows/msaa/ServiceProvider.cpp
+++ accessible/src/windows/msaa/ServiceProvider.cpp
@@ -37,7 +37,7 @@ ServiceProvider::QueryService(REFGUID aGuidService, REFIID aIID,
   if (!aInstancePtr)
     return E_INVALIDARG;
 
-  *aInstancePtr = NULL;
+  *aInstancePtr = nullptr;
 
   
   if (aGuidService == IID_IAccessibleEx &&
