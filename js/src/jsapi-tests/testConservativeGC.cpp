9adb82847573ca2cc21f87a05fee545d52f19155
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:46:09 +0000

    Bug 831750 - GC: testConservativeGC jsapi test fails with rooting analysis r=terrence a=nonlibxul
    UltraBlame original commit: bedb55033fe72a9c584a65b0b98a7e7504502b99

--- js/src/jsapi-tests/testConservativeGC.cpp
+++ js/src/jsapi-tests/testConservativeGC.cpp
@@ -2,6 +2,8 @@
 
 
 
+#if !defined(JSGC_ROOT_ANALYSIS) && !defined(JSGC_USE_EXACT_ROOTING)
+
 #include "tests.h"
 #include "jsobj.h"
 #include "vm/String.h"
@@ -10,7 +12,6 @@
 
 BEGIN_TEST(testConservativeGC)
 {
-#ifndef JSGC_USE_EXACT_ROOTING
     js::RootedValue v2(cx);
     EVAL("({foo: 'bar'});", v2.address());
     CHECK(v2.isObject());
@@ -52,7 +53,6 @@ BEGIN_TEST(testConservativeGC)
 
     checkObjectFields((JSObject *)obj2Copy, obj2);
     CHECK(!memcmp(str2Copy, str2, sizeof(str2Copy)));
-#endif 
 
     return true;
 }
@@ -86,3 +86,5 @@ BEGIN_TEST(testDerivedValues)
   return true;
 }
 END_TEST(testDerivedValues)
+
+#endif 
