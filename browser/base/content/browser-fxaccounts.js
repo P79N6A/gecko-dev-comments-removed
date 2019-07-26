4a9207ec7b6c6339ecb1e9be010c10ef605a63eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:36:48 +0000

    Bug 966511: Tweak Sync UI, r=ttaubert
    Changes:
    - rename browser-fxaccounts.js "toggle" to onMenuPanelCommand
    - rename openSignInPage->openSignInAgainPage and change it to point to about:accounts?signin=true
    - introduce openAccountsPage and change the menu panel "signed in"'s UI to call it
    - remove period at the end of "Firefox is now syncing" in the success doorhanger
    - rename syncErrorPanel.title->syncErrorPanel.heading for consistency
    - remove the "Preferences" button from the "you need to sign in again" doorhanger
    - "manage"->"change" in the "What would you like to Sync" dialog
    - change "Unlink Browser" to "Disconnect", in both the prefs button and confirmation dialog title
    - adjust string in the "Disconnect" confirmation dialog
    - change "Device"->"computer" in the relink verification dialog
    - fix success panel heading styling (should be bold)
    
    UltraBlame original commit: 08ef155181f6a1c487db997ce12854678099cf32

--- browser/base/content/browser-fxaccounts.js
+++ browser/base/content/browser-fxaccounts.js
@@ -190,7 +190,7 @@ let gFxAccounts = {
     if (event.originalTarget.hasAttribute("signedin")) {
       this.openPreferences();
     } else {
-      this.openSignInPage();
+      this.openAccountsPage();
     }
 
     PanelUI.hide();
@@ -200,7 +200,12 @@ let gFxAccounts = {
     openPreferences("paneSync");
   },
 
-  openSignInPage: function () {
+  openAccountsPage: function () {
     switchToTabHavingURI("about:accounts", true);
+  },
+
+  openSignInAgainPage: function () {
+    
+    switchToTabHavingURI("about:accounts?signin=true", true);
   }
 };
