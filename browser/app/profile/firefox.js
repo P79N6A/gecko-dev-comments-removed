3285ebba0e0fa27d423780bbdf3669e11f5971cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:14:43 +0000

    Bug 738818 part 2: remove keyword.URL, r=gavin, sr=bz
    UltraBlame original commit: 8998ff84caefb042b7821b126ba317d814e00de8

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -211,9 +211,6 @@ pref("lightweightThemes.update.enabled", true);
 
 pref("keyword.enabled", true);
 
-
-pref("keyword.URL", "");
-
 pref("general.useragent.locale", "@AB_CD@");
 pref("general.skins.selectedSkin", "classic/1.0");
 
