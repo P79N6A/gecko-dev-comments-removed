c7b336a5b7fe6c3d9a79cd7d24190d1f22edd463
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:41:24 +0000

    Bug 825732: Convert SVGTSpanElement to WebIDL r=bz
    UltraBlame original commit: c4ba770505d9067d2f7705410ff868790d751159

---
+++ content/svg/content/src/SVGTSpanElement.cpp
@@ -0,0 +1,87 @@
+
+
+
+
+
+#include "mozilla/dom/SVGTSpanElement.h"
+#include "mozilla/dom/SVGTSpanElementBinding.h"
+
+DOMCI_NODE_DATA(SVGTSpanElement, mozilla::dom::SVGTSpanElement)
+
+NS_IMPL_NS_NEW_NAMESPACED_SVG_ELEMENT(TSpan)
+
+namespace mozilla {
+namespace dom {
+
+JSObject*
+SVGTSpanElement::WrapNode(JSContext *aCx, JSObject *aScope, bool *aTriedToWrap)
+{
+  return SVGTSpanElementBinding::Wrap(aCx, aScope, this, aTriedToWrap);
+}
+
+
+
+
+
+NS_IMPL_ADDREF_INHERITED(SVGTSpanElement,SVGTSpanElementBase)
+NS_IMPL_RELEASE_INHERITED(SVGTSpanElement,SVGTSpanElementBase)
+
+NS_INTERFACE_TABLE_HEAD(SVGTSpanElement)
+  NS_NODE_INTERFACE_TABLE6(SVGTSpanElement, nsIDOMNode, nsIDOMElement,
+                           nsIDOMSVGElement, nsIDOMSVGTSpanElement,
+                           nsIDOMSVGTextPositioningElement,
+                           nsIDOMSVGTextContentElement)
+  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(SVGTSpanElement)
+NS_INTERFACE_MAP_END_INHERITING(SVGTSpanElementBase)
+
+
+
+
+SVGTSpanElement::SVGTSpanElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+  : SVGTSpanElementBase(aNodeInfo)
+{
+  SetIsDOMBinding();
+}
+
+
+
+
+
+
+NS_IMPL_ELEMENT_CLONE_WITH_INIT(SVGTSpanElement)
+
+
+
+
+
+
+
+
+
+
+NS_IMETHODIMP_(bool)
+SVGTSpanElement::IsAttributeMapped(const nsIAtom* name) const
+{
+  static const MappedAttributeEntry* const map[] = {
+    sColorMap,
+    sFillStrokeMap,
+    sFontSpecificationMap,
+    sGraphicsMap,
+    sTextContentElementsMap
+  };
+
+  return FindAttributeDependence(name, map) ||
+    SVGTSpanElementBase::IsAttributeMapped(name);
+}
+
+
+
+
+bool
+SVGTSpanElement::IsEventName(nsIAtom* aName)
+{
+  return nsContentUtils::IsEventAttributeName(aName, EventNameType_SVGGraphic);
+}
+
+} 
+} 
---
+++ content/svg/content/src/SVGTSpanElement.h
@@ -0,0 +1,60 @@
+
+
+
+
+
+#ifndef mozilla_dom_SVGTSpanElement_h
+#define mozilla_dom_SVGTSpanElement_h
+
+#include "mozilla/dom/SVGTextPositioningElement.h"
+#include "nsIDOMSVGTSpanElement.h"
+
+nsresult NS_NewSVGTSpanElement(nsIContent **aResult,
+                               already_AddRefed<nsINodeInfo> aNodeInfo);
+
+namespace mozilla {
+namespace dom {
+
+typedef SVGTextPositioningElement SVGTSpanElementBase;
+
+class SVGTSpanElement MOZ_FINAL : public SVGTSpanElementBase, 
+                                  public nsIDOMSVGTSpanElement
+{
+protected:
+  friend nsresult (::NS_NewSVGTSpanElement(nsIContent **aResult,
+                                           already_AddRefed<nsINodeInfo> aNodeInfo));
+  SVGTSpanElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual JSObject* WrapNode(JSContext *cx, JSObject *scope, bool *triedToWrap) MOZ_OVERRIDE;
+
+public:
+  
+
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIDOMSVGTSPANELEMENT
+
+  
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+  NS_FORWARD_NSIDOMSVGELEMENT(SVGTSpanElementBase::)
+  NS_FORWARD_NSIDOMSVGTEXTCONTENTELEMENT(SVGTSpanElementBase::)
+  NS_FORWARD_NSIDOMSVGTEXTPOSITIONINGELEMENT(SVGTSpanElementBase::)
+
+  
+  NS_IMETHOD_(bool) IsAttributeMapped(const nsIAtom* aAttribute) const;
+
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+
+  virtual nsXPCClassInfo* GetClassInfo();
+
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+protected:
+
+  
+  virtual bool IsEventName(nsIAtom* aName);
+};
+
+} 
+} 
+
+#endif 
