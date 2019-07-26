257f6ca4a15d316ff6ab2a6a51b807e100e9fb40
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 21:58:16 +0000

    Bug 963294 - [e10s] Implement a proxy for the color picker. r=jdm
    UltraBlame original commit: 5e4cee2fcbc682e19f12cee896243dd7b3bdad7f

---
+++ widget/xpwidgets/nsColorPickerProxy.h
@@ -0,0 +1,32 @@
+
+
+
+
+
+#ifndef nsColorPickerProxy_h
+#define nsColorPickerProxy_h
+
+#include "nsIColorPicker.h"
+
+#include "mozilla/dom/PColorPickerChild.h"
+
+class nsColorPickerProxy MOZ_FINAL : public nsIColorPicker,
+                                     public mozilla::dom::PColorPickerChild
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSICOLORPICKER
+
+  nsColorPickerProxy() {}
+  ~nsColorPickerProxy() {}
+
+  virtual bool RecvUpdate(const nsString& aColor) MOZ_OVERRIDE;
+  virtual bool Recv__delete__(const nsString& aColor) MOZ_OVERRIDE;
+
+private:
+  nsCOMPtr<nsIColorPickerShownCallback> mCallback;
+  nsString mTitle;
+  nsString mInitialColor;
+};
+
+#endif 
