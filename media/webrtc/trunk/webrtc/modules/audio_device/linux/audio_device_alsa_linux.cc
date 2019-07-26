f3f09a5c4a7a783206a6bb6bd339c40609c19ff5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:24:18 +0000

    Bug 943353 - Don't include X11 headers if USE_X11 is undefined. r=jesup
    UltraBlame original commit: 532334f941089fc22241ed1ba5e18979d0c72d52

--- media/webrtc/trunk/webrtc/modules/audio_device/linux/audio_device_alsa_linux.cc
+++ media/webrtc/trunk/webrtc/modules/audio_device/linux/audio_device_alsa_linux.cc
@@ -190,6 +190,7 @@ int32_t AudioDeviceLinuxALSA::Init()
         return 0;
     }
 
+#ifdef USE_X11
     
     _XDisplay = XOpenDisplay(NULL);
     if (!_XDisplay)
@@ -197,6 +198,7 @@ int32_t AudioDeviceLinuxALSA::Init()
         WEBRTC_TRACE(kTraceWarning, kTraceAudioDevice, _id,
           "  failed to open X display, typing detection will not work");
     }
+#endif
 
     _playWarning = 0;
     _playError = 0;
@@ -264,11 +266,13 @@ int32_t AudioDeviceLinuxALSA::Terminate()
         _critSect.Enter();
     }
 
+#ifdef USE_X11
     if (_XDisplay)
     {
       XCloseDisplay(_XDisplay);
       _XDisplay = NULL;
     }
+#endif
 
     _initialized = false;
     _outputDeviceIsSpecified = false;
@@ -2370,11 +2374,13 @@ bool AudioDeviceLinuxALSA::KeyPressed() const{
   unsigned int i = 0;
   char state = 0;
 
+#ifdef USE_X11
   if (!_XDisplay)
     return false;
 
   
   XQueryKeymap(_XDisplay, szKey);
+#endif
 
   
   for (i = 0; i < sizeof(szKey); i++)
