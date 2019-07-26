7fa35cc4b0fc7e8d6083a4e633336ef34c60e21d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:10:55 +0000

    Bug 795173: Make Debugger.Object.prototype.evalInGlobalWithBindings pass the right 'this' value. r=jorendorff
    This also adds a test for Debugger.Frame.prototype.evalWithBindings, to
    make sure it puts new variables on the right object; I couldn't find any
    existing tests for the non-strict case (Frame-evalWithBindings-11.js
    handles the strict mode case).
    
    UltraBlame original commit: e061305143b6c7615947777531edede64cc7aced

---
+++ js/src/jit-test/tests/debug/Object-evalInGlobal-06.js
@@ -0,0 +1,8 @@
+
+
+var dbg = new Debugger;
+var g1 = newGlobal();
+var g1w = dbg.addDebuggee(g1);
+
+assertEq(g1w.evalInGlobal('this').return, g1w);
+assertEq(g1w.evalInGlobalWithBindings('this', { x:42 }).return, g1w);
