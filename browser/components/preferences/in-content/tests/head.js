6d554bd6f9ddf8fdf73fb3842dbbb3007da2c214
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:30:03 +0000

    Bug 1021618 - Create the foundation for converting preference dialogs to be in-content. r=jaws a11y=MarcoZ f=Paenglab,davidb
    UltraBlame original commit: 24fb014f1ae3d7372a8777d1b2a5e8dcd9cf3cad

--- browser/components/preferences/in-content/tests/head.js
+++ browser/components/preferences/in-content/tests/head.js
@@ -1,6 +1,11 @@
 
 
 
+Components.utils.import("resource://gre/modules/Promise.jsm");
+
+const kDefaultWait = 2000;
+const kDefaultInterval = 50;
+
 function is_hidden(aElement) {
   var style = aElement.ownerDocument.defaultView.getComputedStyle(aElement, "");
   if (style.display == "none")
@@ -33,3 +38,83 @@ function open_preferences(aCallback) {
     aCallback(gBrowser.contentWindow);
   }, true);
 }
+
+function openAndLoadSubDialog(aURL, aFeatures = null, aParams = null, aClosingCallback = null) {
+  let dialog = content.gSubDialog.open(aURL, aFeatures, aParams, aClosingCallback);
+  let deferred = Promise.defer();
+
+  content.gSubDialog._frame.addEventListener("load", function load(aEvent) {
+    if (aEvent.target.contentWindow.location == "about:blank")
+      return;
+    content.gSubDialog._frame.removeEventListener("load", load);
+
+    ise(content.gSubDialog._frame.contentWindow.location.toString(), aURL,
+        "Check the proper URL is loaded");
+
+    
+    is_element_visible(content.gSubDialog._overlay, "Overlay is visible");
+
+    
+    let expectedStyleSheetURLs = content.gSubDialog._injectedStyleSheets.slice(0);
+    for (let styleSheet of content.document.styleSheets) {
+      let i = expectedStyleSheetURLs.indexOf(styleSheet.href);
+      if (i >= 0) {
+        info("found " + styleSheet.href);
+        expectedStyleSheetURLs.splice(i, 1);
+      }
+    }
+    ise(expectedStyleSheetURLs.length, 0, "All expectedStyleSheetURLs should have been found");
+
+    deferred.resolve(dialog);
+  });
+
+  return deferred.promise;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function waitForEvent(aSubject, aEventName, aTimeoutMs, aTarget) {
+  let eventDeferred = Promise.defer();
+  let timeoutMs = aTimeoutMs || kDefaultWait;
+  let stack = new Error().stack;
+  let timerID = setTimeout(function wfe_canceller() {
+    aSubject.removeEventListener(aEventName, listener);
+    eventDeferred.reject(new Error(aEventName + " event timeout at " + stack));
+  }, timeoutMs);
+
+  var listener = function (aEvent) {
+    if (aTarget && aTarget !== aEvent.target)
+        return;
+
+    
+    clearTimeout(timerID);
+    eventDeferred.resolve(aEvent);
+  };
+
+  function cleanup(aEventOrError) {
+    
+    aSubject.removeEventListener(aEventName, listener);
+    return aEventOrError;
+  }
+  aSubject.addEventListener(aEventName, listener, false);
+  return eventDeferred.promise.then(cleanup, cleanup);
+}
+
