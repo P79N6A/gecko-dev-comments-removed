0dc82ff6366861453f9c76603ba17c1cf19e3669
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:10:22 +0000

    Bug 788829 - Call SetSizeConstraints even if a popup is not open. r=enndeakin
    UltraBlame original commit: 54079ce3e83633654bdd7d24bf5ae32323c843d2

--- layout/xul/base/src/nsMenuPopupFrame.cpp
+++ layout/xul/base/src/nsMenuPopupFrame.cpp
@@ -445,20 +445,20 @@ nsMenuPopupFrame::LayoutPopup(nsBoxLayoutState& aState, nsIFrame* aParentMenu, b
   }
 
   nsPresContext* pc = PresContext();
+  nsIView* view = GetView();
+
+  if (sizeChanged) {
+    
+    nsIWidget* widget = view->GetWidget();
+    if (widget) {
+      SetSizeConstraints(pc, widget, minSize, maxSize);
+    }
+  }
+
   if (isOpen) {
-    nsIView* view = GetView();
     nsIViewManager* viewManager = view->GetViewManager();
     nsRect rect = GetRect();
     rect.x = rect.y = 0;
-
-    if (sizeChanged) {
-      
-      nsIWidget* widget = view->GetWidget();
-      if (widget) {
-        SetSizeConstraints(pc, widget, minSize, maxSize);
-      }
-    }
-
     viewManager->ResizeView(view, rect);
 
     viewManager->SetViewVisibility(view, nsViewVisibility_kShow);
