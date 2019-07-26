db4758d3aef8e81ff3cf39a6d7c575c0f6e9bc12
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:17:39 +0000

    Bug 899388 - Part 3: Remove XUL-related interfaces and ChromeWindow from content. r=bz
    UltraBlame original commit: 48073e4207470c601673d72f4c5007cce7a3e531

--- dom/base/nsDOMClassInfoClasses.h
+++ dom/base/nsDOMClassInfoClasses.h
@@ -128,6 +128,8 @@ DOMCI_CLASS(CSSPageRule)
 DOMCI_CLASS(MediaQueryList)
 
 #ifdef MOZ_B2G_RIL
+DOMCI_CLASS(Telephony)
+DOMCI_CLASS(TelephonyCall)
 DOMCI_CLASS(MozVoicemail)
 DOMCI_CLASS(MozIccManager)
 #endif
@@ -156,6 +158,8 @@ DOMCI_CLASS(UserDataHandler)
 DOMCI_CLASS(GeoPositionError)
 DOMCI_CLASS(LoadStatus)
 DOMCI_CLASS(XPathNamespace)
+DOMCI_CLASS(XULControlElement)
+DOMCI_CLASS(XULLabeledControlElement)
 DOMCI_CLASS(XULButtonElement)
 DOMCI_CLASS(XULCheckboxElement)
 DOMCI_CLASS(XULPopupElement)
