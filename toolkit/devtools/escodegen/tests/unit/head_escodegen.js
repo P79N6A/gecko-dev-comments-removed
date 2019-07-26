7ecbc3dc2fe59a25c0d4dc9e43542e5bfd676295
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:32:18 +0000

    Bug 908913 - integrate escodegen with devtools; r=robcee,gps,dcamp
    UltraBlame original commit: daebcfc9338b16eba55055aaea1fc438052c46f2

---
+++ toolkit/devtools/escodegen/tests/unit/head_escodegen.js
@@ -0,0 +1,40 @@
+"use strict";
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+const Cr = Components.results;
+
+const { devtools } = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
+const { require } = devtools;
+
+
+
+let errorCount = 0;
+let listener = {
+  observe: function (aMessage) {
+    errorCount++;
+    try {
+      
+      
+      var scriptError = aMessage.QueryInterface(Ci.nsIScriptError);
+      dump(aMessage.sourceName + ":" + aMessage.lineNumber + ": " +
+           scriptErrorFlagsToKind(aMessage.flags) + ": " +
+           aMessage.errorMessage + "\n");
+      var string = aMessage.errorMessage;
+    } catch (x) {
+      
+      
+      try {
+        var string = "" + aMessage.message;
+      } catch (x) {
+        var string = "<error converting error message to string>";
+      }
+    }
+
+    do_throw("head_dbg.js got console message: " + string + "\n");
+  }
+};
+
+let consoleService = Cc["@mozilla.org/consoleservice;1"].getService(Ci.nsIConsoleService);
+consoleService.registerListener(listener);
+
