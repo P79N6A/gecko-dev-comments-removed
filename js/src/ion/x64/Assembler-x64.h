ee5ec4b412f56da60bafc874d8427c024858ffa2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:17:41 +0000

    Bug 861543 - x86-64: Use movl instead of movabsq for 32-bits immediates. r=nbp
    Make the implementation of mov in the x86-64 assembler use
    movl instead of movabsq for loading immediates which fit in 32-bit
    immediate fields.  This reduces the size of the instructions.
    
    Also, change several places to call mov instead of using movq directly,
    so that they can take advantage of this optimization.
    
    UltraBlame original commit: 7b7d888e7b7dd0ffbea80b32e05d0215e32f23fd

--- js/src/ion/x64/Assembler-x64.h
+++ js/src/ion/x64/Assembler-x64.h
@@ -510,7 +510,16 @@ class Assembler : public AssemblerX86Shared
     }
 
     void mov(ImmWord word, const Register &dest) {
-        movq(word, dest);
+        
+        
+        
+        if (word.value <= UINT32_MAX) {
+            uint32_t value32 = static_cast<uint32_t>(word.value);
+            Imm32 imm32(static_cast<int32_t>(value32));
+            movl(imm32, dest);
+        } else {
+            movq(word, dest);
+        }
     }
     void mov(const Imm32 &imm32, const Register &dest) {
         movl(imm32, dest);
