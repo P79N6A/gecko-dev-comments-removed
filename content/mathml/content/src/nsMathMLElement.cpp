3f2dbe7d70a4f52234b1d0336db7b4f81e4dc36e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:41:18 +0000

    Bug 764996 - Dir attribute mapping. r=karlt
    UltraBlame original commit: 40be35413245fabae262becb97a556482decf772

--- content/mathml/content/src/nsMathMLElement.cpp
+++ content/mathml/content/src/nsMathMLElement.cpp
@@ -191,6 +191,11 @@ static Element::MappedAttributeEntry sCommonPresStyles[] = {
   { nullptr }
 };
 
+static Element::MappedAttributeEntry sDirStyles[] = {
+  { &nsGkAtoms::dir },
+  { nullptr }
+};
+
 bool
 nsMathMLElement::IsAttributeMapped(const nsIAtom* aAttribute) const
 {
@@ -200,22 +205,28 @@ nsMathMLElement::IsAttributeMapped(const nsIAtom* aAttribute) const
   };
   static const MappedAttributeEntry* const tokenMap[] = {
     sTokenStyles,
-    sCommonPresStyles
+    sCommonPresStyles,
+    sDirStyles
   };
   static const MappedAttributeEntry* const mstyleMap[] = {
     sTokenStyles,
     sEnvironmentStyles,
-    sCommonPresStyles
+    sCommonPresStyles,
+    sDirStyles
   };
   static const MappedAttributeEntry* const commonPresMap[] = {
     sCommonPresStyles
   };
-  
+  static const MappedAttributeEntry* const mrowMap[] = {
+    sCommonPresStyles,
+    sDirStyles
+  };
+
   
   nsIAtom* tag = Tag();
   if (tag == nsGkAtoms::ms_ || tag == nsGkAtoms::mi_ ||
       tag == nsGkAtoms::mn_ || tag == nsGkAtoms::mo_ ||
-      tag == nsGkAtoms::mtext_ || tag == nsGkAtoms::mspace_)
+      tag == nsGkAtoms::mtext_)
     return FindAttributeDependence(aAttribute, tokenMap);
   if (tag == nsGkAtoms::mstyle_ ||
       tag == nsGkAtoms::math)
@@ -223,6 +234,9 @@ nsMathMLElement::IsAttributeMapped(const nsIAtom* aAttribute) const
 
   if (tag == nsGkAtoms::mtable_)
     return FindAttributeDependence(aAttribute, mtableMap);
+
+  if (tag == nsGkAtoms::mrow_)
+    return FindAttributeDependence(aAttribute, mrowMap);
 
   if (tag == nsGkAtoms::maction_ ||
       tag == nsGkAtoms::maligngroup_ ||
@@ -236,7 +250,6 @@ nsMathMLElement::IsAttributeMapped(const nsIAtom* aAttribute) const
       tag == nsGkAtoms::mphantom_ ||
       tag == nsGkAtoms::mprescripts_ ||
       tag == nsGkAtoms::mroot_ ||
-      tag == nsGkAtoms::mrow_ ||
       tag == nsGkAtoms::msqrt_ ||
       tag == nsGkAtoms::msub_ ||
       tag == nsGkAtoms::msubsup_ ||
@@ -725,6 +738,44 @@ nsMathMLElement::MapMathMLAttributesInto(const nsMappedAttributes* aAttributes,
     }
   }
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  if (aData->mSIDs & NS_STYLE_INHERIT_BIT(Visibility)) {
+    const nsAttrValue* value = aAttributes->GetAttr(nsGkAtoms::dir);
+    nsCSSValue* direction = aData->ValueForDirection();
+    if (value && value->Type() == nsAttrValue::eString &&
+        direction->GetUnit() == eCSSUnit_Null) {
+      nsAutoString str(value->GetStringValue());
+      static const char dirs[][4] = { "ltr", "rtl" };
+      static const int32_t dirValues[NS_ARRAY_LENGTH(dirs)] = {
+        NS_STYLE_DIRECTION_LTR, NS_STYLE_DIRECTION_RTL
+      };
+      for (uint32_t i = 0; i < ArrayLength(dirs); ++i) {
+        if (str.EqualsASCII(dirs[i])) {
+          direction->SetIntValue(dirValues[i], eCSSUnit_Enumerated);
+          break;
+        }
+      }
+    }
+  }
 }
 
 nsresult
