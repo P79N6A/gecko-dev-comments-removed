c1b15253bc2e0908baee1d2c75ae460b72780c8f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:40:53 +0000

    Bug 884492 - run TabsInTitlebar._update when nodes are added/removed in tab/menubar, r=mconley
    UltraBlame original commit: 903d51e5303e6105dabaf17084709e5acae791c9

---
+++ browser/base/content/test/general/browser_tabbar_big_widgets.js
@@ -0,0 +1,29 @@
+
+
+
+
+const kButtonId = "test-tabbar-size-with-large-buttons";
+
+function test() {
+  registerCleanupFunction(cleanup);
+  let titlebar = document.getElementById("titlebar");
+  let originalHeight = titlebar.getBoundingClientRect().height;
+  let button = document.createElement("toolbarbutton");
+  button.id = kButtonId;
+  button.setAttribute("style", "min-height: 100px");
+  gNavToolbox.palette.appendChild(button);
+  CustomizableUI.addWidgetToArea(kButtonId, CustomizableUI.AREA_TABSTRIP);
+  let currentHeight = titlebar.getBoundingClientRect().height;
+  ok(currentHeight > originalHeight, "Titlebar should have grown");
+  CustomizableUI.removeWidgetFromArea(kButtonId);
+  currentHeight = titlebar.getBoundingClientRect().height;
+  is(currentHeight, originalHeight, "Titlebar should have gone back to its original size.");
+}
+
+function cleanup() {
+  let btn = document.getElementById(kButtonId);
+  if (btn) {
+    btn.remove();
+  }
+}
+
