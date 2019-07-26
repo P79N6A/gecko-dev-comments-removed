fb2dd4ad4b012b5c1e982d7de9132ea158f7ec32
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:42:54 +0000

    Bug 825427 - Intermittent browser_responsiveruleview.js; r=miker
    UltraBlame original commit: 6d1315e1a0268c25b67263230fec947a63052e56

--- browser/devtools/responsivedesign/test/head.js
+++ browser/devtools/responsivedesign/test/head.js
@@ -15,27 +15,109 @@ SimpleTest.registerCleanupFunction(() => {
   gDevTools.testing = false;
 });
 
-function openInspector(callback)
-{
+
+
+
+
+let openInspector = Task.async(function*() {
+  info("Opening the inspector");
   let target = TargetFactory.forTab(gBrowser.selectedTab);
-  gDevTools.showToolbox(target, "inspector").then(function(toolbox) {
-    callback(toolbox.getCurrentPanel());
-  });
+
+  let inspector, toolbox;
+
+  
+  
+  
+  toolbox = gDevTools.getToolbox(target);
+  if (toolbox) {
+    inspector = toolbox.getPanel("inspector");
+    if (inspector) {
+      info("Toolbox and inspector already open");
+      return {
+        toolbox: toolbox,
+        inspector: inspector
+      };
+    }
+  }
+
+  info("Opening the toolbox");
+  toolbox = yield gDevTools.showToolbox(target, "inspector");
+  yield waitForToolboxFrameFocus(toolbox);
+  inspector = toolbox.getPanel("inspector");
+
+  info("Waiting for the inspector to update");
+  yield inspector.once("inspector-updated");
+
+  return {
+    toolbox: toolbox,
+    inspector: inspector
+  };
+});
+
+
+
+
+
+
+function waitForToolboxFrameFocus(toolbox) {
+  info("Making sure that the toolbox's frame is focused");
+  let def = promise.defer();
+  let win = toolbox.frame.contentWindow;
+  waitForFocus(def.resolve, win);
+  return def.promise;
 }
 
-function openView(name, callback)
-{
-  openInspector(inspector => {
-    function onReady() {
-      inspector.sidebar.select(name);
-      let { view } = inspector.sidebar.getWindowForTab(name)[name];
-      callback(inspector, view);
-    }
 
-    if (inspector.sidebar.getTab(name)) {
-      onReady();
-    } else {
-      inspector.sidebar.once(name + "-ready", onReady);
-    }
-  });
+
+
+
+
+
+let openInspectorSideBar = Task.async(function*(id) {
+  let {toolbox, inspector} = yield openInspector();
+
+  if (!hasSideBarTab(inspector, id)) {
+    info("Waiting for the " + id + " sidebar to be ready");
+    yield inspector.sidebar.once(id + "-ready");
+  }
+
+  info("Selecting the " + id + " sidebar");
+  inspector.sidebar.select(id);
+
+  return {
+    toolbox: toolbox,
+    inspector: inspector,
+    view: inspector.sidebar.getWindowForTab(id)[id].view
+  };
+});
+
+
+
+
+
+
+
+
+function hasSideBarTab(inspector, id) {
+  return !!inspector.sidebar.getWindowForTab(id);
+}
+
+
+
+
+
+
+
+function openComputedView() {
+  return openInspectorSideBar("computedview");
+}
+
+
+
+
+
+
+
+function openRuleView() {
+  return openInspectorSideBar("ruleview");
 }
