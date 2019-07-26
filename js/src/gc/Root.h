173a08141a9bed5e72e2a72264d93868a23fe275
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 08:57:33 +0000

    Bug 766347 - Adjust Handle<T> constructors to only accept Rooted<S> or Handle<S> where S is convertible to T.  r=luke
    UltraBlame original commit: 24e8950269e3bc088a0c7aee017b0d0561e3d874

--- js/src/gc/Root.h
+++ js/src/gc/Root.h
@@ -1,108 +1,118 @@
-/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- * vim: set ts=8 sw=4 et tw=78:
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
+
 
 #ifndef jsgc_root_h__
 #define jsgc_root_h__
 
+#ifdef __cplusplus
+
+#include "mozilla/TypeTraits.h"
+
 #include "jspubtd.h"
 
 #include "js/Utility.h"
+
 
 #ifdef __cplusplus
 
 namespace js {
 namespace gc {
 struct Cell;
-} /* namespace gc */
-} /* namespace js */
+} 
+} 
 
 namespace JS {
 
-/*
- * Moving GC Stack Rooting
- *
- * A moving GC may change the physical location of GC allocated things, even
- * when they are rooted, updating all pointers to the thing to refer to its new
- * location. The GC must therefore know about all live pointers to a thing,
- * not just one of them, in order to behave correctly.
- *
- * The classes below are used to root stack locations whose value may be held
- * live across a call that can trigger GC (i.e. a call which might allocate any
- * GC things). For a code fragment such as:
- *
- * Foo();
- * ... = obj->lastProperty();
- *
- * If Foo() can trigger a GC, the stack location of obj must be rooted to
- * ensure that the GC does not move the JSObject referred to by obj without
- * updating obj's location itself. This rooting must happen regardless of
- * whether there are other roots which ensure that the object itself will not
- * be collected.
- *
- * If Foo() cannot trigger a GC, and the same holds for all other calls made
- * between obj's definitions and its last uses, then no rooting is required.
- *
- * Several classes are available for rooting stack locations. All are templated
- * on the type T of the value being rooted, for which RootMethods<T> must
- * have an instantiation.
- *
- * - Rooted<T> declares a variable of type T, whose value is always rooted.
- *   Rooted<T> may be automatically coerced to a Handle<T>, below. Rooted<T>
- *   should be used whenever a local variable's value may be held live across a
- *   call which can allocate GC things or otherwise trigger a GC.
- *
- * - Handle<T> is a const reference to a Rooted<T>. Functions which take GC
- *   things or values as arguments and need to root those arguments should
- *   generally use handles for those arguments and avoid any explicit rooting.
- *   This has two benefits. First, when several such functions call each other
- *   then redundant rooting of multiple copies of the GC thing can be avoided.
- *   Second, if the caller does not pass a rooted value a compile error will be
- *   generated, which is quicker and easier to fix than when relying on a
- *   separate rooting analysis.
- */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 template <typename T> class Rooted;
 
 template <typename T>
 struct RootMethods { };
 
-/*
- * Reference to a T that has been rooted elsewhere. This is most useful
- * as a parameter type, which guarantees that the T lvalue is properly
- * rooted. See "Move GC Stack Rooting" above.
- */
+
+
+
+
+
 template <typename T>
 class Handle
 {
   public:
-    /* Copy handles of different types, with implicit coercion. */
-    template <typename S> Handle(Handle<S> handle) {
-        testAssign<S>();
+    
+    template <typename S>
+    Handle(Handle<S> handle,
+           typename mozilla::EnableIf<mozilla::IsConvertible<S, T>::value, int>::Type dummy = 0)
+    {
         ptr = reinterpret_cast<const T *>(handle.address());
     }
 
-    /*
-     * This may be called only if the location of the T is guaranteed
-     * to be marked (for some reason other than being a Rooted),
-     * e.g., if it is guaranteed to be reachable from an implicit root.
-     *
-     * Create a Handle from a raw location of a T.
-     */
+    
+
+
+
+
+
+
     static Handle fromMarkedLocation(const T *p) {
         Handle h;
         h.ptr = p;
         return h;
     }
 
-    /*
-     * Construct a handle from an explicitly rooted location. This is the
-     * normal way to create a handle, and normally happens implicitly.
-     */
-    template <typename S> inline Handle(Rooted<S> &root);
+    
+
+
+
+    template <typename S>
+    inline
+    Handle(Rooted<S> &root,
+           typename mozilla::EnableIf<mozilla::IsConvertible<S, T>::value, int>::Type dummy = 0);
 
     const T *address() const { return ptr; }
     T value() const { return *ptr; }
@@ -114,16 +124,6 @@ class Handle
     Handle() {}
 
     const T *ptr;
-
-    template <typename S>
-    void testAssign() {
-#ifdef DEBUG
-        T a = RootMethods<T>::initial();
-        S b = RootMethods<S>::initial();
-        a = b;
-        (void)a;
-#endif
-    }
 };
 
 typedef Handle<JSObject*>    HandleObject;
@@ -141,11 +141,11 @@ struct RootMethods<T *>
     static bool poisoned(T *v) { return IsPoisonedPtr(v); }
 };
 
-/*
- * Local variable of type T whose value is always rooted. This is typically
- * used for local variables, or for non-rooted values being passed to a
- * function that requires a handle, e.g. Foo(Root<T>(cx, x)).
- */
+
+
+
+
+
 template <typename T>
 class Rooted
 {
@@ -214,9 +214,9 @@ class Rooted
 
 template<typename T> template <typename S>
 inline
-Handle<T>::Handle(Rooted<S> &root)
+Handle<T>::Handle(Rooted<S> &root,
+                  typename mozilla::EnableIf<mozilla::IsConvertible<S, T>::value, int>::Type dummy)
 {
-    testAssign<S>();
     ptr = reinterpret_cast<const T *>(root.address());
 }
 
@@ -227,12 +227,12 @@ typedef Rooted<JSString*>    RootedString;
 typedef Rooted<jsid>         RootedId;
 typedef Rooted<Value>        RootedValue;
 
-/*
- * Mark a stack location as a root for the rooting analysis, without actually
- * rooting it in release builds. This should only be used for stack locations
- * of GC things that cannot be relocated by a garbage collection, and that
- * are definitely reachable via another path.
- */
+
+
+
+
+
+
 class SkipRoot
 {
 #if defined(DEBUG) && defined(JSGC_ROOT_ANALYSIS)
@@ -280,7 +280,7 @@ class SkipRoot
         return v >= start && v + len <= end;
     }
 
-#else /* DEBUG && JSGC_ROOT_ANALYSIS */
+#else 
 
   public:
     template <typename T>
@@ -297,15 +297,15 @@ class SkipRoot
         JS_GUARD_OBJECT_NOTIFIER_INIT;
     }
 
-#endif /* DEBUG && JSGC_ROOT_ANALYSIS */
+#endif 
 
     JS_DECL_USE_GUARD_OBJECT_NOTIFIER
 };
 
-/*
- * Hook for dynamic root analysis. Checks the native stack and poisons
- * references to GC things which have not been rooted.
- */
+
+
+
+
 #if  defined(DEBUG) && defined(JS_GC_ZEAL) && defined(JSGC_ROOT_ANALYSIS) && !defined(JS_THREADSAFE)
 void CheckStackRoots(JSContext *cx);
 inline void MaybeCheckStackRoots(JSContext *cx) { CheckStackRoots(cx); }
@@ -313,7 +313,7 @@ inline void MaybeCheckStackRoots(JSContext *cx) { CheckStackRoots(cx); }
 inline void MaybeCheckStackRoots(JSContext *cx) {}
 #endif
 
-/* Base class for automatic read-only object rooting during compilation. */
+
 class CompilerRootNode
 {
   protected:
@@ -331,8 +331,8 @@ class CompilerRootNode
     js::gc::Cell *ptr;
 };
 
-}  /* namespace JS */
+}  
 
-#endif  /* __cplusplus */
+#endif  
 
-#endif  /* jsgc_root_h___ */
+#endif  
