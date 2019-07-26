5bddc74372f68bd220c2d21a6423b649a6eb1344
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:23:25 +0000

    Bug 986114 - Part 1: ReadingListProvider and BrowserProvider should share DB accessors. r=nalexander * * * Bug 986114 - Follow-up: Fix bustage on a CLOSED TREE.
    UltraBlame original commit: 49ca7b29bc93ed38c6d80d442ffbfd6e49025e2e

---
+++ mobile/android/base/db/SharedBrowserDatabaseProvider.java
@@ -0,0 +1,115 @@
+
+
+
+
+package org.mozilla.gecko.db;
+
+import org.mozilla.gecko.db.BrowserContract.CommonColumns;
+import org.mozilla.gecko.db.BrowserContract.SyncColumns;
+import org.mozilla.gecko.db.PerProfileDatabases.DatabaseHelperFactory;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.util.Log;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public abstract class SharedBrowserDatabaseProvider extends AbstractPerProfileDatabaseProvider {
+    private static final String LOGTAG = SharedBrowserDatabaseProvider.class.getSimpleName();
+
+    private static PerProfileDatabases<BrowserDatabaseHelper> databases;
+
+    @Override
+    protected PerProfileDatabases<BrowserDatabaseHelper> getDatabases() {
+        return databases;
+    }
+
+    @Override
+    public boolean onCreate() {
+        
+        synchronized (SharedBrowserDatabaseProvider.class) {
+            if (databases != null) {
+                return true;
+            }
+
+            final DatabaseHelperFactory<BrowserDatabaseHelper> helperFactory = new DatabaseHelperFactory<BrowserDatabaseHelper>() {
+                @Override
+                public BrowserDatabaseHelper makeDatabaseHelper(Context context, String databasePath) {
+                    return new BrowserDatabaseHelper(context, databasePath);
+                }
+            };
+
+            databases = new PerProfileDatabases<BrowserDatabaseHelper>(getContext(), BrowserDatabaseHelper.DATABASE_NAME, helperFactory);
+        }
+
+        return true;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+    protected void cleanUpSomeDeletedRecords(Uri fromUri, String tableName) {
+        Log.d(LOGTAG, "Cleaning up deleted records from " + tableName);
+
+        
+        
+        
+
+        
+        
+        
+
+        
+        final long MAX_AGE_OF_DELETED_RECORDS = 86400000 * 20;
+
+        
+        final long DELETED_RECORDS_PURGE_LIMIT = 5;
+
+        
+        
+        final long now = System.currentTimeMillis();
+        final String selection = SyncColumns.IS_DELETED + " = 1 AND " +
+                SyncColumns.DATE_MODIFIED + " <= " +
+                (now - MAX_AGE_OF_DELETED_RECORDS);
+
+        final String profile = fromUri.getQueryParameter(BrowserContract.PARAM_PROFILE);
+        final SQLiteDatabase db = getWritableDatabaseForProfile(profile, isTest(fromUri));
+        final String[] ids;
+        final String limit = Long.toString(DELETED_RECORDS_PURGE_LIMIT, 10);
+        final Cursor cursor = db.query(tableName, new String[] { CommonColumns._ID }, selection, null, null, null, null, limit);
+        try {
+            ids = new String[cursor.getCount()];
+            int i = 0;
+            while (cursor.moveToNext()) {
+                ids[i++] = Long.toString(cursor.getLong(0), 10);
+            }
+        } finally {
+            cursor.close();
+        }
+
+        final String inClause = computeSQLInClause(ids.length,
+                CommonColumns._ID);
+        db.delete(tableName, inClause, ids);
+    }
+}
