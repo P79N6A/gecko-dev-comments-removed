5998740d3a2f1b8b9b2430e59a843c78d24a235d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:51:22 +0000

    Bug 884124 (part 2) - Add a StopIterationObject class.  r=jwalden.
    UltraBlame original commit: 033d8c89ed6c96fa3be949e1e7cc192c408413eb

---
+++ js/src/vm/StopIterationObject.h
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+#ifndef vm_StopIterationObject_h
+#define vm_StopIterationObject_h
+
+#include "jsobj.h"
+
+namespace js {
+
+class StopIterationObject : public JSObject
+{
+  public:
+    static Class class_;
+};
+
+} 
+
+#endif 
