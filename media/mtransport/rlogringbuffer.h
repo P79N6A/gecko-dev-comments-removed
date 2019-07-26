38a9a4b3a5cf8793680c194ca058fbf62e38b4cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:25:22 +0000

    Bug 906990 - Part 5: Allow logging related to a given candidate pair to be fetched. r=ekr
    UltraBlame original commit: ff8128d5a5d998b783f38653ca13c7496d64dc83

---
+++ media/mtransport/rlogringbuffer.h
@@ -0,0 +1,122 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef rlogringbuffer_h__
+#define rlogringbuffer_h__
+
+#include <deque>
+#include <string>
+#include <vector>
+
+#include "mozilla/Mutex.h"
+
+#include "m_cpp_utils.h"
+
+namespace mozilla {
+
+class RLogRingBuffer {
+  public:
+    
+
+
+
+    static RLogRingBuffer* CreateInstance();
+    static RLogRingBuffer* GetInstance();
+    static void DestroyInstance();
+
+    
+
+
+
+
+
+    void Filter(const std::string& substring,
+                uint32_t limit,
+                std::deque<std::string>* matching_logs);
+
+    void FilterAny(const std::vector<std::string>& substrings,
+                   uint32_t limit,
+                   std::deque<std::string>* matching_logs);
+
+    inline void GetAny(uint32_t limit,
+                       std::deque<std::string>* matching_logs) {
+      Filter("", limit, matching_logs);
+    }
+
+    void SetLogLimit(uint32_t new_limit);
+
+    void Log(std::string&& log);
+
+  private:
+    RLogRingBuffer();
+    ~RLogRingBuffer();
+    void RemoveOld();
+    static RLogRingBuffer* instance;
+
+    
+
+
+
+
+    std::deque<std::string> log_messages_;
+    
+    uint32_t log_limit_;
+    mozilla::Mutex mutex_;
+
+    DISALLOW_COPY_ASSIGN(RLogRingBuffer);
+}; 
+
+} 
+
+#endif 
+
+
