5d6e68fe140101dcae9753e3f51aa1764a6e6fcb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:23:43 +0000

    Bug 871650 - Restore search suggestions support (r=bnicholson)
    UltraBlame original commit: ea79867c3ffda6170afe96fb8a8bdd2085548a84

--- mobile/android/base/BrowserSearch.java
+++ mobile/android/base/BrowserSearch.java
@@ -9,8 +9,14 @@ import org.mozilla.gecko.db.BrowserContract.Combined;
 import org.mozilla.gecko.db.BrowserDB;
 import org.mozilla.gecko.db.BrowserDB.URLColumns;
 import org.mozilla.gecko.gfx.BitmapUtils;
+import org.mozilla.gecko.util.GeckoEventListener;
 import org.mozilla.gecko.util.ThreadUtils;
 import org.mozilla.gecko.home.TwoLinePageRow;
+import org.mozilla.gecko.widget.FaviconView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.ContentResolver;
@@ -22,21 +28,30 @@ import android.content.res.Configuration;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 
 
 
-public class BrowserSearch extends Fragment implements AdapterView.OnItemClickListener {
+public class BrowserSearch extends Fragment implements AdapterView.OnItemClickListener,
+                                                       GeckoEventListener {
+    
+    private static final String LOGTAG = "GeckoBrowserSearch";
+
     
     private static final int SEARCH_LOADER_ID = 0;
 
@@ -44,7 +59,16 @@ public class BrowserSearch extends Fragment implements AdapterView.OnItemClickLi
     private static final int FAVICONS_LOADER_ID = 1;
 
     
+    private static final int SUGGESTION_LOADER_ID = 2;
+
+    
     private static final String FAVICONS_LOADER_URLS_ARG = "urls";
+
+    
+    private static final int SUGGESTION_TIMEOUT = 3000;
+
+    
+    private static final int SUGGESTION_MAX = 3;
 
     
     private String mSearchTerm;
@@ -56,13 +80,42 @@ public class BrowserSearch extends Fragment implements AdapterView.OnItemClickLi
     private ListView mList;
 
     
+    private SuggestClient mSuggestClient;
+
+    
+    private ArrayList<SearchEngine> mSearchEngines;
+
+    
+    private boolean mSuggestionsEnabled;
+
+    
     private CursorLoaderCallbacks mCursorLoaderCallbacks;
+
+    
+    private SuggestionLoaderCallbacks mSuggestionLoaderCallbacks;
+
+    
+    private LayoutInflater mInflater;
 
     
     private OnUrlOpenListener mUrlOpenListener;
 
+    
+    private OnSearchListener mSearchListener;
+
+    
+    private OnEditSuggestionListener mEditSuggestionListener;
+
     public interface OnUrlOpenListener {
         public void onUrlOpen(String url);
+    }
+
+    public interface OnSearchListener {
+        public void onSearch(SearchEngine engine, String text);
+    }
+
+    public interface OnEditSuggestionListener {
+        public void onEditSuggestion(String suggestion);
     }
 
     public static BrowserSearch newInstance() {
@@ -71,6 +124,26 @@ public class BrowserSearch extends Fragment implements AdapterView.OnItemClickLi
 
     public BrowserSearch() {
         mSearchTerm = "";
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        registerEventListener("SearchEngines:Data");
+
+        
+        
+        if (mSearchEngines == null) {
+            mSearchEngines = new ArrayList<SearchEngine>();
+            GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("SearchEngines:Get", null));
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterEventListener("SearchEngines:Data");
     }
 
     @Override
@@ -83,6 +156,22 @@ public class BrowserSearch extends Fragment implements AdapterView.OnItemClickLi
             throw new ClassCastException(activity.toString()
                     + " must implement BrowserSearch.OnUrlOpenListener");
         }
+
+        try {
+            mSearchListener = (OnSearchListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement BrowserSearch.OnSearchListener");
+        }
+
+        try {
+            mEditSuggestionListener = (OnEditSuggestionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement BrowserSearch.OnEditSuggestionListener");
+        }
+
+        mInflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -90,6 +179,8 @@ public class BrowserSearch extends Fragment implements AdapterView.OnItemClickLi
         super.onDetach();
 
         mUrlOpenListener = null;
+        mSearchListener = null;
+        mEditSuggestionListener = null;
     }
 
     @Override
@@ -115,10 +206,101 @@ public class BrowserSearch extends Fragment implements AdapterView.OnItemClickLi
         mAdapter = new SearchAdapter(getActivity());
         mList.setAdapter(mAdapter);
 
+        
+        mSuggestionLoaderCallbacks = null;
+
+        
         mCursorLoaderCallbacks = new CursorLoaderCallbacks();
 
         
         getLoaderManager().initLoader(SEARCH_LOADER_ID, null, mCursorLoaderCallbacks);
+    }
+
+    @Override
+    public void handleMessage(String event, final JSONObject message) {
+        if (event.equals("SearchEngines:Data")) {
+            ThreadUtils.postToUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    setSearchEngines(message);
+                }
+            });
+        }
+    }
+
+    private void filterSuggestions() {
+        if (mSuggestClient == null || !mSuggestionsEnabled) {
+            return;
+        }
+
+        if (mSuggestionLoaderCallbacks == null) {
+            mSuggestionLoaderCallbacks = new SuggestionLoaderCallbacks();
+        }
+
+        getLoaderManager().restartLoader(SUGGESTION_LOADER_ID, null, mSuggestionLoaderCallbacks);
+    }
+
+    private void setSuggestions(ArrayList<String> suggestions) {
+        mSearchEngines.get(0).suggestions = suggestions;
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private void setSearchEngines(JSONObject data) {
+        try {
+            final JSONObject suggest = data.getJSONObject("suggest");
+            final String suggestEngine = suggest.optString("engine", null);
+            final String suggestTemplate = suggest.optString("template", null);
+            final boolean suggestionsPrompted = suggest.getBoolean("prompted");
+            final JSONArray engines = data.getJSONArray("searchEngines");
+
+            mSuggestionsEnabled = suggest.getBoolean("enabled");
+
+            ArrayList<SearchEngine> searchEngines = new ArrayList<SearchEngine>();
+            for (int i = 0; i < engines.length(); i++) {
+                final JSONObject engineJSON = engines.getJSONObject(i);
+                final String name = engineJSON.getString("name");
+                final String identifier = engineJSON.getString("identifier");
+                final String iconURI = engineJSON.getString("iconURI");
+                final Bitmap icon = BitmapUtils.getBitmapFromDataURI(iconURI);
+
+                if (name.equals(suggestEngine) && suggestTemplate != null) {
+                    
+                    searchEngines.add(0, new SearchEngine(name, identifier, icon));
+
+                    
+                    
+                    
+                    
+                    Tab tab = Tabs.getInstance().getSelectedTab();
+                    if (tab == null || !tab.isPrivate()) {
+                        mSuggestClient = new SuggestClient(getActivity(), suggestTemplate,
+                                SUGGESTION_TIMEOUT, SUGGESTION_MAX);
+                    }
+                } else {
+                    searchEngines.add(new SearchEngine(name, identifier, icon));
+                }
+            }
+
+            mSearchEngines = searchEngines;
+
+            if (mAdapter != null) {
+                mAdapter.notifyDataSetChanged();
+            }
+
+            
+        } catch (JSONException e) {
+            Log.e(LOGTAG, "Error getting search engine JSON", e);
+        }
+
+        filterSuggestions();
+    }
+
+    private void registerEventListener(String eventName) {
+        GeckoAppShell.getEventDispatcher().registerEventListener(eventName, this);
+    }
+
+    private void unregisterEventListener(String eventName) {
+        GeckoAppShell.getEventDispatcher().unregisterEventListener(eventName, this);
     }
 
     private ArrayList<String> getUrlsWithoutFavicon(Cursor c) {
@@ -168,8 +350,13 @@ public class BrowserSearch extends Fragment implements AdapterView.OnItemClickLi
         mSearchTerm = searchTerm;
 
         if (isVisible()) {
-            getLoaderManager().restartLoader(SEARCH_LOADER_ID, null, mCursorLoaderCallbacks);
+            
+            
+            mAdapter.notifyDataSetChanged();
 
+            
+            getLoaderManager().restartLoader(SEARCH_LOADER_ID, null, mCursorLoaderCallbacks);
+            filterSuggestions();
         }
     }
 
@@ -240,30 +427,191 @@ public class BrowserSearch extends Fragment implements AdapterView.OnItemClickLi
         }
     }
 
+    private static class SuggestionAsyncLoader extends AsyncTaskLoader<ArrayList<String>> {
+        private final SuggestClient mSuggestClient;
+        private final String mSearchTerm;
+        private ArrayList<String> mSuggestions;
+
+        public SuggestionAsyncLoader(Context context, SuggestClient suggestClient, String searchTerm) {
+            super(context);
+            mSuggestClient = suggestClient;
+            mSearchTerm = searchTerm;
+            mSuggestions = null;
+        }
+
+        @Override
+        public ArrayList<String> loadInBackground() {
+            return mSuggestClient.query(mSearchTerm);
+        }
+
+        @Override
+        public void deliverResult(ArrayList<String> suggestions) {
+            mSuggestions = suggestions;
+
+            if (isStarted()) {
+                super.deliverResult(mSuggestions);
+            }
+        }
+
+        @Override
+        protected void onStartLoading() {
+            if (mSuggestions != null) {
+                deliverResult(mSuggestions);
+            }
+
+            if (takeContentChanged() || mSuggestions == null) {
+                forceLoad();
+            }
+        }
+
+        @Override
+        protected void onStopLoading() {
+            cancelLoad();
+        }
+
+        @Override
+        protected void onReset() {
+            super.onReset();
+
+            onStopLoading();
+            mSuggestions = null;
+        }
+    }
+
+    private class SearchEntryViewHolder {
+        public FlowLayout suggestionView;
+        public FaviconView iconView;
+        public LinearLayout userEnteredView;
+        public TextView userEnteredTextView;
+    }
+
     private class SearchAdapter extends SimpleCursorAdapter {
+        private static final int ROW_SEARCH = 0;
+        private static final int ROW_STANDARD = 1;
+        private static final int ROW_SUGGEST = 2;
+
+        private static final int ROW_TYPE_COUNT = 3;
+
         public SearchAdapter(Context context) {
             super(context, -1, null, new String[] {}, new int[] {});
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final TwoLinePageRow row;
-            if (convertView == null) {
-                row = (TwoLinePageRow) LayoutInflater.from(getActivity()).inflate(R.layout.home_item_row, null);
-            } else {
-                row = (TwoLinePageRow) convertView;
+        public int getItemViewType(int position) {
+            final int engine = getEngineIndex(position);
+
+            if (engine == -1) {
+                return ROW_STANDARD;
+            } else if (engine == 0 && mSuggestionsEnabled) {
+                
+                
+                
+                
+                return ROW_SUGGEST;
             }
 
-            final Cursor c = getCursor();
-            if (!c.moveToPosition(position)) {
-                throw new IllegalStateException("Couldn't move cursor to position " + position);
+            return ROW_SEARCH;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            
+            
+            return ROW_TYPE_COUNT;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            
+            
+            
+            
+            final int index = getEngineIndex(position);
+            if (index != -1) {
+                return mSearchEngines.get(index).suggestions.isEmpty();
             }
 
-            row.updateFromCursor(c);
+            return true;
+        }
+
+        
+        @Override
+        public int getCount() {
+            final int resultCount = super.getCount();
 
             
+            if (TextUtils.isEmpty(mSearchTerm)) {
+                return resultCount;
+            }
 
-            return row;
+            return resultCount + mSearchEngines.size();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final int type = getItemViewType(position);
+
+            if (type == ROW_SEARCH || type == ROW_SUGGEST) {
+                final SearchEngineRow row;
+                if (convertView == null) {
+                    row = (SearchEngineRow) mInflater.inflate(R.layout.home_search_item_row, mList, false);
+                    row.setOnUrlOpenListener(mUrlOpenListener);
+                    row.setOnSearchListener(mSearchListener);
+                    row.setOnEditSuggestionListener(mEditSuggestionListener);
+                } else {
+                    row = (SearchEngineRow) convertView;
+                }
+
+                row.setSearchTerm(mSearchTerm);
+
+                final SearchEngine engine = mSearchEngines.get(getEngineIndex(position));
+                row.updateFromSearchEngine(engine);
+
+                return row;
+            } else {
+                final TwoLinePageRow row;
+                if (convertView == null) {
+                    row = (TwoLinePageRow) mInflater.inflate(R.layout.home_item_row, mList, false);
+                } else {
+                    row = (TwoLinePageRow) convertView;
+                }
+
+                
+                position -= getSuggestEngineCount();
+
+                final Cursor c = getCursor();
+                if (!c.moveToPosition(position)) {
+                    throw new IllegalStateException("Couldn't move cursor to position " + position);
+                }
+
+                row.updateFromCursor(c);
+
+                
+
+                return row;
+            }
+        }
+
+        private int getSuggestEngineCount() {
+            return (TextUtils.isEmpty(mSearchTerm) || mSuggestClient == null || !mSuggestionsEnabled) ? 0 : 1;
+        }
+
+        private int getEngineIndex(int position) {
+            final int resultCount = super.getCount();
+            final int suggestEngineCount = getSuggestEngineCount();
+
+            
+            if (position < suggestEngineCount) {
+                return position;
+            }
+
+            
+            if (position - suggestEngineCount < resultCount) {
+                return -1;
+            }
+
+            
+            return position - resultCount;
         }
     }
 
@@ -319,6 +667,23 @@ public class BrowserSearch extends Fragment implements AdapterView.OnItemClickLi
                 
                 break;
             }
+        }
+    }
+
+    private class SuggestionLoaderCallbacks implements LoaderCallbacks<ArrayList<String>> {
+        @Override
+        public Loader<ArrayList<String>> onCreateLoader(int id, Bundle args) {
+            return new SuggestionAsyncLoader(getActivity(), mSuggestClient, mSearchTerm);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<ArrayList<String>> loader, ArrayList<String> suggestions) {
+            setSuggestions(suggestions);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<ArrayList<String>> loader) {
+            setSuggestions(new ArrayList<String>());
         }
     }
 }
--- mobile/android/base/SearchEngineRow.java
+++ mobile/android/base/SearchEngineRow.java
@@ -5,27 +5,24 @@
 
 package org.mozilla.gecko;
 
-import org.mozilla.gecko.AwesomeBarTabs.OnUrlOpenListener;
-import org.mozilla.gecko.util.GamepadUtils;
+import org.mozilla.gecko.BrowserSearch.OnEditSuggestionListener;
+import org.mozilla.gecko.BrowserSearch.OnSearchListener;
+import org.mozilla.gecko.BrowserSearch.OnUrlOpenListener;
 import org.mozilla.gecko.util.StringUtils;
 import org.mozilla.gecko.widget.FaviconView;
 
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
-import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 class SearchEngineRow extends AnimatedHeightLayout {
-    
-    private static final int ANIMATION_DURATION = 250;
 
     
     private final FlowLayout mSuggestionView;
@@ -40,14 +37,17 @@ class SearchEngineRow extends AnimatedHeightLayout {
     private SearchEngine mSearchEngine;
 
     
-    private int mSelectedView = 0;
-
-    
     private final OnClickListener mClickListener;
     private final OnLongClickListener mLongClickListener;
 
     
     private OnUrlOpenListener mUrlOpenListener;
+
+    
+    private OnSearchListener mSearchListener;
+
+    
+    private OnEditSuggestionListener mEditSuggestionListener;
 
     public SearchEngineRow(Context context) {
         this(context, null);
@@ -68,12 +68,12 @@ class SearchEngineRow extends AnimatedHeightLayout {
                 
                 
                 
-                if (mUrlOpenListener != null) {
-                    if (v != mUserEnteredView && !StringUtils.isSearchQuery(suggestion, false)) {
-                        mUrlOpenListener.onUrlOpen(suggestion, null);
-                    } else {
-                        mUrlOpenListener.onSearch(mSearchEngine, suggestion);
+                if (v != mUserEnteredView && !StringUtils.isSearchQuery(suggestion, false)) {
+                    if (mUrlOpenListener != null) {
+                        mUrlOpenListener.onUrlOpen(suggestion);
                     }
+                } else if (mSearchListener != null) {
+                    mSearchListener.onSearch(mSearchEngine, suggestion);
                 }
             }
         };
@@ -81,9 +81,9 @@ class SearchEngineRow extends AnimatedHeightLayout {
         mLongClickListener = new OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                if (mUrlOpenListener != null) {
+                if (mEditSuggestionListener != null) {
                     final String suggestion = getSuggestionTextFromView(v);
-                    mUrlOpenListener.onEditSuggestion(suggestion);
+                    mEditSuggestionListener.onEditSuggestion(suggestion);
                     return true;
                 }
 
@@ -122,7 +122,15 @@ class SearchEngineRow extends AnimatedHeightLayout {
         mUrlOpenListener = listener;
     }
 
-    public void updateFromSearchEngine(SearchEngine searchEngine, boolean doAnimation) {
+    public void setOnSearchListener(OnSearchListener listener) {
+        mSearchListener = listener;
+    }
+
+    public void setOnEditSuggestionListener(OnEditSuggestionListener listener) {
+        mEditSuggestionListener = listener;
+    }
+
+    public void updateFromSearchEngine(SearchEngine searchEngine) {
         
         mSearchEngine = searchEngine;
 
@@ -141,7 +149,7 @@ class SearchEngineRow extends AnimatedHeightLayout {
                 suggestionItem = mSuggestionView.getChildAt(i + 1);
                 suggestionItem.setVisibility(View.VISIBLE);
             } else {
-                suggestionItem = mInflater.inflate(R.layout.suggestion_item, null);
+                suggestionItem = mInflater.inflate(R.layout.awesomebar_suggestion_item, null);
 
                 suggestionItem.setOnClickListener(mClickListener);
                 suggestionItem.setOnLongClickListener(mLongClickListener);
@@ -155,77 +163,11 @@ class SearchEngineRow extends AnimatedHeightLayout {
 
             final String suggestion = mSearchEngine.suggestions.get(i);
             setSuggestionOnView(suggestionItem, suggestion);
-
-            if (doAnimation) {
-                AlphaAnimation anim = new AlphaAnimation(0, 1);
-                anim.setDuration(ANIMATION_DURATION);
-                anim.setStartOffset(i * ANIMATION_DURATION);
-                suggestionItem.startAnimation(anim);
-            }
         }
 
         
         for (int i = suggestionCount + 1; i < recycledSuggestionCount; i++) {
             mSuggestionView.getChildAt(i).setVisibility(View.GONE);
         }
-
-        
-        if (mSelectedView >= mSuggestionView.getChildCount()) {
-            mSelectedView = mSuggestionView.getChildCount() - 1;
-        }
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, android.view.KeyEvent event) {
-        final View suggestion = mSuggestionView.getChildAt(mSelectedView);
-
-        if (event.getAction() != android.view.KeyEvent.ACTION_DOWN) {
-            return false;
-        }
-
-        switch (event.getKeyCode()) {
-        case KeyEvent.KEYCODE_DPAD_RIGHT:
-            final View nextSuggestion = mSuggestionView.getChildAt(mSelectedView + 1);
-            if (nextSuggestion != null) {
-                changeSelectedSuggestion(suggestion, nextSuggestion);
-                mSelectedView++;
-                return true;
-            }
-            break;
-
-        case KeyEvent.KEYCODE_DPAD_LEFT:
-            final View prevSuggestion = mSuggestionView.getChildAt(mSelectedView - 1);
-            if (prevSuggestion != null) {
-                changeSelectedSuggestion(suggestion, prevSuggestion);
-                mSelectedView--;
-                return true;
-            }
-            break;
-
-        case KeyEvent.KEYCODE_BUTTON_A:
-            
-            return suggestion.performClick();
-        }
-
-        return false;
-    }
-
-    private void changeSelectedSuggestion(View oldSuggestion, View newSuggestion) {
-        oldSuggestion.setDuplicateParentStateEnabled(false);
-        newSuggestion.setDuplicateParentStateEnabled(true);
-        oldSuggestion.refreshDrawableState();
-        newSuggestion.refreshDrawableState();
-    }
-
-    public void onSelected() {
-        mSelectedView = 0;
-        mUserEnteredView.setDuplicateParentStateEnabled(true);
-        mUserEnteredView.refreshDrawableState();
-    }
-
-    public void onDeselected() {
-        final View suggestion = mSuggestionView.getChildAt(mSelectedView);
-        suggestion.setDuplicateParentStateEnabled(false);
-        suggestion.refreshDrawableState();
     }
 }
