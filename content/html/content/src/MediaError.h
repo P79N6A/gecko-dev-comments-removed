8d6461faba118c0250fa8b81dcc25bf7536695bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:56:03 +0000

    Bug 840559 - Convert MediaError to WebIDL. r=smaug
    UltraBlame original commit: d0ff1ebd98858de16508f59da7075e7d3fcd5127

--- content/html/content/src/MediaError.h
+++ content/html/content/src/MediaError.h
@@ -8,26 +8,44 @@
 #define mozilla_dom_MediaError_h
 
 #include "nsIDOMMediaError.h"
+#include "nsHTMLMediaElement.h"
+#include "nsWrapperCache.h"
 #include "nsISupports.h"
 #include "mozilla/Attributes.h"
 
 namespace mozilla {
 namespace dom {
 
-class MediaError MOZ_FINAL : public nsIDOMMediaError
+class MediaError MOZ_FINAL : public nsIDOMMediaError,
+                             public nsWrapperCache
 {
 public:
-  MediaError(uint16_t aCode);
+  MediaError(nsHTMLMediaElement* aParent, uint16_t aCode);
 
   
-  NS_DECL_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(MediaError)
 
   
   NS_DECL_NSIDOMMEDIAERROR
 
+  nsHTMLMediaElement* GetParentObject() const
+  {
+    return mParent;
+  }
+
+  virtual JSObject* WrapObject(JSContext* aCx, JSObject* aScope, bool* aTriedToWrap);
+
+  uint16_t Code() const
+  {
+    return mCode;
+  }
+
 private:
+  nsRefPtr<nsHTMLMediaElement> mParent;
+
   
-  uint16_t mCode;
+  const uint16_t mCode;
 };
 
 } 
