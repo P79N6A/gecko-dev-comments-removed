e531235097be54ab705e653194de2d28cf30be0f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:15:47 +0000

    Bug 900260: Explicitly cast 64-bit value to size_t, to fix build warning on 32-bit platforms (which won't run this code anyway). r=froydnj
    UltraBlame original commit: 263073d5b26282d4a91cb15d838c37ca4ba983f9

--- content/base/src/nsTextFragmentImpl.h
+++ content/base/src/nsTextFragmentImpl.h
@@ -16,7 +16,15 @@ template<> struct Non8BitParameters<4> {
 };
 
 template<> struct Non8BitParameters<8> {
-  static inline size_t mask() { return 0xff00ff00ff00ff00; }
+  static inline size_t mask() {
+    static const uint64_t maskAsUint64 = 0xff00ff00ff00ff00ULL;
+    
+    
+    
+    
+    
+    return (size_t)maskAsUint64;
+  }
   static inline uint32_t alignMask() { return 0x7; }
   static inline uint32_t numUnicharsPerWord() { return 4; }
 };
