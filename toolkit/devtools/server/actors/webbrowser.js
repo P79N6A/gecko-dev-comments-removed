a2523e8b9f9f074b0f0bc178536d6f866370621c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:01:04 +0000

    Backed out changeset 812c5f165aca (bug 864098) for XPC Bustage on a CLOSED TREE
    UltraBlame original commit: 06c1e1d366d6b63643170809884fa7a3844473bd

--- toolkit/devtools/server/actors/webbrowser.js
+++ toolkit/devtools/server/actors/webbrowser.js
@@ -712,12 +712,7 @@ BrowserTabActor.prototype = {
 
     this._attach();
 
-    return {
-      type: "tabAttached",
-      threadActor: this.threadActor.actorID,
-      cacheEnabled: this._getCacheEnabled(),
-      javascriptEnabled: this._getJavascriptEnabled()
-    };
+    return { type: "tabAttached", threadActor: this.threadActor.actorID };
   },
 
   onDetach: function BTA_onDetach(aRequest) {
@@ -750,99 +745,6 @@ BrowserTabActor.prototype = {
       this.window.location = aRequest.url;
     }, "BrowserTabActor.prototype.onNavigateTo's delayed body"), 0);
     return {};
-  },
-
-  
-
-
-  onReconfigure: function (aRequest) {
-    let options = aRequest.options || {};
-
-    this._toggleJsOrCache(options);
-    return {};
-  },
-
-  
-
-
-  _toggleJsOrCache: function(options) {
-    
-    
-    let reload = false;
-
-    if (typeof options.javascriptEnabled !== "undefined" &&
-        options.javascriptEnabled !== this._getJavascriptEnabled()) {
-      this._setJavascriptEnabled(options.javascriptEnabled);
-      reload = true;
-    }
-    if (typeof options.cacheEnabled !== "undefined" &&
-        options.cacheEnabled !== this._getCacheEnabled()) {
-      this._setCacheEnabled(options.cacheEnabled);
-      reload = true;
-    }
-
-    if (reload) {
-      this.onReload();
-    }
-  },
-
-  
-
-
-  _setCacheEnabled: function(allow) {
-    let enable =  Ci.nsIRequest.LOAD_NORMAL;
-    let disable = Ci.nsIRequest.LOAD_BYPASS_CACHE |
-                  Ci.nsIRequest.INHIBIT_CACHING;
-    let docShell = this.window
-                       .QueryInterface(Ci.nsIInterfaceRequestor)
-                       .getInterface(Ci.nsIDocShell);
-
-    docShell.defaultLoadFlags = allow ? enable : disable;
-  },
-
-  
-
-
-  _setJavascriptEnabled: function(allow) {
-    let docShell = this.window
-                       .QueryInterface(Ci.nsIInterfaceRequestor)
-                       .getInterface(Ci.nsIDocShell);
-
-    docShell.allowJavascript = allow;
-  },
-
-  
-
-
-  _getCacheEnabled: function() {
-    if (!this.window) {
-      
-      return;
-    }
-
-    let disable = Ci.nsIRequest.LOAD_BYPASS_CACHE |
-                  Ci.nsIRequest.INHIBIT_CACHING;
-    let docShell = this.window
-                       .QueryInterface(Ci.nsIInterfaceRequestor)
-                       .getInterface(Ci.nsIDocShell);
-
-    return docShell.defaultLoadFlags !== disable;
-  },
-
-  
-
-
-  _getJavascriptEnabled: function() {
-    if (!this.window) {
-      
-      return;
-    }
-
-    let docShell = this.window
-                       .QueryInterface(Ci.nsIInterfaceRequestor)
-                       .getInterface(Ci.nsIDocShell);
-
-    return docShell.allowJavascript;
   },
 
   
@@ -936,8 +838,7 @@ BrowserTabActor.prototype.requestTypes = {
   "attach": BrowserTabActor.prototype.onAttach,
   "detach": BrowserTabActor.prototype.onDetach,
   "reload": BrowserTabActor.prototype.onReload,
-  "navigateTo": BrowserTabActor.prototype.onNavigateTo,
-  "reconfigure": BrowserTabActor.prototype.onReconfigure
+  "navigateTo": BrowserTabActor.prototype.onNavigateTo
 };
 
 function BrowserAddonList(aConnection)
