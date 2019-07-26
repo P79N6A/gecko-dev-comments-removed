2038a654b0fc2744a79b545c8a3a370fcc67d2d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:38:06 +0000

    Bug 816778 Part 1.5: Create nsISVGPoint and make nsSVGTranslatePoint::DOMVal a subclass r=bz
    UltraBlame original commit: 347128caddd541d65a16c830dc0310212668df93

---
+++ content/svg/content/src/nsISVGPoint.h
@@ -0,0 +1,63 @@
+
+
+
+
+
+#pragma once
+
+#include "nsAutoPtr.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsIDOMSVGPoint.h"
+#include "nsWrapperCache.h"
+#include "mozilla/dom/SVGPointBinding.h"
+
+class nsSVGElement;
+
+
+#define MOZILLA_NSISVGPOINT_IID \
+  { 0xd6b6c440, 0xaf8d, 0x40ee, \
+    { 0x85, 0x6b, 0x02, 0xa3, 0x17, 0xca, 0xb2, 0x75 } }
+
+namespace mozilla {
+
+
+
+
+
+
+
+class nsISVGPoint : public nsIDOMSVGPoint,
+                    public nsWrapperCache
+{
+public:
+  NS_DECLARE_STATIC_IID_ACCESSOR(MOZILLA_NSISVGPOINT_IID)
+
+  
+
+
+  explicit nsISVGPoint()
+  {
+    SetIsDOMBinding();
+  }
+
+  using nsIDOMSVGPoint::SetX;
+  using nsIDOMSVGPoint::SetY;
+  using nsIDOMSVGPoint::MatrixTransform;
+
+  
+  virtual float X() = 0;
+  virtual void SetX(float aX, ErrorResult& rv) = 0;
+  virtual float Y() = 0;
+  virtual void SetY(float aY, ErrorResult& rv) = 0;
+  virtual already_AddRefed<nsISVGPoint> MatrixTransform(nsIDOMSVGMatrix* matrix) = 0;
+  virtual JSObject* WrapObject(JSContext *cx, JSObject *scope,
+                               bool *triedToWrap)
+    { return dom::SVGPointBinding::Wrap(cx, scope, this, triedToWrap); }
+
+  virtual nsISupports* GetParentObject() = 0;
+};
+
+NS_DEFINE_STATIC_IID_ACCESSOR(nsISVGPoint, MOZILLA_NSISVGPOINT_IID)
+
+} 
+
