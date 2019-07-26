c29d2c556bc5b8a202d7c992db812161d482fd4f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:43:16 +0000

    Bug 939655 - Remove LOG macro definition from MediaStreamGraphImpl.h - r=roc Removing this reduces opportunity for macro name clashes and using the wrong log object if this header file is included out of order in a .cpp file.
    UltraBlame original commit: 5dfb6c2488f95aece2d344c985b743481618d742

--- content/media/MediaStreamGraphImpl.h
+++ content/media/MediaStreamGraphImpl.h
@@ -19,13 +19,6 @@ namespace mozilla {
 template <typename T>
 class LinkedList;
 
-#ifdef PR_LOGGING
-extern PRLogModuleInfo* gMediaStreamGraphLog;
-#define LOG(type, msg) PR_LOG(gMediaStreamGraphLog, type, msg)
-#else
-#define LOG(type, msg)
-#endif
-
 
 
 
@@ -120,12 +113,7 @@ public:
 
 
   explicit MediaStreamGraphImpl(bool aRealtime);
-  ~MediaStreamGraphImpl()
-  {
-    NS_ASSERTION(IsEmpty(),
-                 "All streams should have been destroyed by messages from the main thread");
-    LOG(PR_LOG_DEBUG, ("MediaStreamGraph %p destroyed", this));
-  }
+  ~MediaStreamGraphImpl();
 
   
   
