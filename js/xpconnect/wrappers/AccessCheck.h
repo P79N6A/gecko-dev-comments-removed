141f67097daff8399afbd45a10063df3c411d300
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:18:40 +0000

    Bug 805807 - Rearchitect filtering policies so that check() doesn't throw on denial. r=mrbkap
    This is another one of those annoying situaitons in XPConnect right now where we
    can't ask a question without potentially throwing if the answer is no. There's
    also a bunch of unused cruft in here (like the Perm*Access stuff), so this stuff
    was ripe for a spring cleaning. Unfortunately, I wasn't able to divide this patch
    up nicely. Sorry for the big diff. :-(
    
    In a nutshell, this patch changes things so that Policy::check() just becomes
    a predicate that says whether the access is allowed or not. There's the remote
    possibility that one of the underlying JSAPI calls in a ::check() implementation
    might throw, so callers to ::check() should check JS_IsExceptionPending
    afterwards (this doesn't catch OOM, but we can just continue along until the
    next OOM-triggering operation and throw there).
    
    Aside from exceptional cases, callers should call Policy::deny if they want to
    report the failure. Policy::deny returns success value that should be returned
    to the wrapper's consumer.
    
    UltraBlame original commit: 23c9f61a243b4bbb833b9ac139d425c1805a6660

--- js/xpconnect/wrappers/AccessCheck.h
+++ js/xpconnect/wrappers/AccessCheck.h
@@ -39,33 +39,16 @@ class AccessCheck {
 };
 
 struct Policy {
-    typedef js::Wrapper::Permission Permission;
-
-    static const Permission PermitObjectAccess = js::Wrapper::PermitObjectAccess;
-    static const Permission PermitPropertyAccess = js::Wrapper::PermitPropertyAccess;
-    static const Permission DenyAccess = js::Wrapper::DenyAccess;
-};
-
-
-struct Permissive : public Policy {
-    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act,
-                      Permission &perm) {
-        perm = PermitObjectAccess;
-        return true;
-    }
 };
 
 
 
 struct OnlyIfSubjectIsSystem : public Policy {
-    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act,
-                      Permission &perm) {
-        if (AccessCheck::isSystemOnlyAccessPermitted(cx)) {
-            perm = PermitObjectAccess;
-            return true;
-        }
-        perm = DenyAccess;
-        JSAutoCompartment ac(cx, wrapper);
+    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act) {
+        return AccessCheck::isSystemOnlyAccessPermitted(cx);
+    }
+
+    static bool deny(JSContext *cx, jsid id, js::Wrapper::Action act) {
         AccessCheck::deny(cx, id);
         return false;
     }
@@ -74,17 +57,12 @@ struct OnlyIfSubjectIsSystem : public Policy {
 
 
 struct CrossOriginAccessiblePropertiesOnly : public Policy {
-    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act,
-                      Permission &perm) {
+    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act) {
         
         MOZ_ASSERT(!WrapperFactory::IsLocationObject(js::UnwrapObject(wrapper)));
-
-        if (AccessCheck::isCrossOriginAccessPermitted(cx, wrapper, id, act)) {
-            perm = PermitPropertyAccess;
-            return true;
-        }
-        perm = DenyAccess;
-        JSAutoCompartment ac(cx, wrapper);
+        return AccessCheck::isCrossOriginAccessPermitted(cx, wrapper, id, act);
+    }
+    static bool deny(JSContext *cx, jsid id, js::Wrapper::Action act) {
         AccessCheck::deny(cx, id);
         return false;
     }
@@ -114,23 +92,19 @@ struct CrossOriginAccessiblePropertiesOnly : public Policy {
 
 
 struct LocationPolicy : public Policy {
-    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act,
-                      Permission &perm) {
+    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act) {
         
         MOZ_ASSERT(WrapperFactory::IsLocationObject(js::UnwrapObject(wrapper)));
-
-        
-        perm = DenyAccess;
 
         
         if (act != js::Wrapper::PUNCTURE &&
             (AccessCheck::isCrossOriginAccessPermitted(cx, wrapper, id, act) ||
              AccessCheck::isLocationObjectSameOrigin(cx, wrapper))) {
-            perm = PermitPropertyAccess;
             return true;
         }
-
-        JSAutoCompartment ac(cx, wrapper);
+        return false;
+    }
+    static bool deny(JSContext *cx, jsid id, js::Wrapper::Action act) {
         AccessCheck::deny(cx, id);
         return false;
     }
@@ -139,14 +113,26 @@ struct LocationPolicy : public Policy {
 
 
 struct ExposedPropertiesOnly : public Policy {
-    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act,
-                      Permission &perm);
+    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act);
+
+    static bool deny(JSContext *cx, jsid id, js::Wrapper::Action act) {
+        
+        if (act == js::Wrapper::GET)
+            return true;
+        
+        AccessCheck::deny(cx, id);
+        return false;
+    }
 };
 
 
 struct ComponentsObjectPolicy : public Policy {
-    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act,
-                      Permission &perm);
+    static bool check(JSContext *cx, JSObject *wrapper, jsid id, js::Wrapper::Action act);
+
+    static bool deny(JSContext *cx, jsid id, js::Wrapper::Action act) {
+        AccessCheck::deny(cx, id);
+        return false;
+    }
 };
 
 }
