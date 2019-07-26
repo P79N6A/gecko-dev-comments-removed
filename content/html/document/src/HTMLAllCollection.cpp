0cdbb1721067d15725c5e1f0f0fd8b4b1e3577e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:42:28 +0000

    Bug 968766 - Part a: Move the custom HTMLAllCollection code from nsDOMClassInfo.cpp to HTMLAllCollection.cpp; r=jst
    UltraBlame original commit: 9b021ca77ea5845732b97a4ede66f47124ad1e95

--- content/html/document/src/HTMLAllCollection.cpp
+++ content/html/document/src/HTMLAllCollection.cpp
@@ -6,11 +6,48 @@
 
 #include "mozilla/dom/HTMLAllCollection.h"
 
+#include "jsapi.h"
+#include "mozilla/dom/BindingUtils.h"
+#include "mozilla/dom/HTMLCollectionBinding.h"
 #include "mozilla/HoldDropJSObjects.h"
 #include "nsDOMClassInfo.h"
 #include "nsHTMLDocument.h"
-#include "jsapi.h"
+#include "nsJSUtils.h"
 #include "nsWrapperCacheInlines.h"
+#include "xpcpublic.h"
+
+using namespace mozilla;
+using namespace mozilla::dom;
+
+class nsHTMLDocumentSH
+{
+protected:
+  static bool GetDocumentAllNodeList(JSContext *cx, JS::Handle<JSObject*> obj,
+                                     nsDocument *doc,
+                                     nsContentList **nodeList);
+public:
+  static bool DocumentAllGetProperty(JSContext *cx, JS::Handle<JSObject*> obj, JS::Handle<jsid> id,
+                                     JS::MutableHandle<JS::Value> vp);
+  static bool DocumentAllNewResolve(JSContext *cx, JS::Handle<JSObject*> obj, JS::Handle<jsid> id,
+                                    unsigned flags, JS::MutableHandle<JSObject*> objp);
+  static void ReleaseDocument(JSFreeOp *fop, JSObject *obj);
+  static bool CallToGetPropMapper(JSContext *cx, unsigned argc, JS::Value *vp);
+};
+
+const JSClass sHTMLDocumentAllClass = {
+  "HTML document.all class",
+  JSCLASS_HAS_PRIVATE | JSCLASS_PRIVATE_IS_NSISUPPORTS | JSCLASS_NEW_RESOLVE |
+  JSCLASS_EMULATES_UNDEFINED | JSCLASS_HAS_RESERVED_SLOTS(1),
+  JS_PropertyStub,                                         
+  JS_DeletePropertyStub,                                   
+  nsHTMLDocumentSH::DocumentAllGetProperty,                
+  JS_StrictPropertyStub,                                   
+  JS_EnumerateStub,
+  (JSResolveOp)nsHTMLDocumentSH::DocumentAllNewResolve,
+  JS_ConvertStub,
+  nsHTMLDocumentSH::ReleaseDocument,
+  nsHTMLDocumentSH::CallToGetPropMapper
+};
 
 namespace mozilla {
 namespace dom {
@@ -75,3 +112,295 @@ HTMLAllCollection::GetObject(JSContext* aCx, ErrorResult& aRv)
 
 } 
 } 
+
+static nsHTMLDocument*
+GetDocument(JSObject *obj)
+{
+  MOZ_ASSERT(js::GetObjectJSClass(obj) == &sHTMLDocumentAllClass);
+  return static_cast<nsHTMLDocument*>(
+    static_cast<nsINode*>(JS_GetPrivate(obj)));
+}
+
+static inline nsresult
+WrapNative(JSContext *cx, JSObject *scope, nsISupports *native,
+           nsWrapperCache *cache, const nsIID* aIID, JS::MutableHandle<JS::Value> vp,
+           bool aAllowWrapping)
+{
+  if (!native) {
+    vp.setNull();
+
+    return NS_OK;
+  }
+
+  JSObject *wrapper = xpc_FastGetCachedWrapper(cache, scope, vp);
+  if (wrapper) {
+    return NS_OK;
+  }
+
+  return nsDOMClassInfo::XPConnect()->WrapNativeToJSVal(cx, scope, native,
+                                                        cache, aIID,
+                                                        aAllowWrapping, vp);
+}
+
+static inline nsresult
+WrapNative(JSContext *cx, JSObject *scope, nsISupports *native,
+           nsWrapperCache *cache, bool aAllowWrapping,
+           JS::MutableHandle<JS::Value> vp)
+{
+  return WrapNative(cx, scope, native, cache, nullptr, vp, aAllowWrapping);
+}
+
+
+bool
+nsHTMLDocumentSH::GetDocumentAllNodeList(JSContext *cx,
+                                         JS::Handle<JSObject*> obj,
+                                         nsDocument *domdoc,
+                                         nsContentList **nodeList)
+{
+  
+  
+  
+  
+  
+  
+  nsresult rv = NS_OK;
+
+  JS::Rooted<JS::Value> collection(cx, JS_GetReservedSlot(obj, 0));
+
+  if (!JSVAL_IS_PRIMITIVE(collection)) {
+    
+    JS::Rooted<JSObject*> obj(cx, JSVAL_TO_OBJECT(collection));
+    nsIHTMLCollection* htmlCollection;
+    rv = UNWRAP_OBJECT(HTMLCollection, obj, htmlCollection);
+    if (NS_SUCCEEDED(rv)) {
+      NS_ADDREF(*nodeList = static_cast<nsContentList*>(htmlCollection));
+    }
+    else {
+      nsISupports *native = nsDOMClassInfo::XPConnect()->GetNativeOfWrapper(cx, obj);
+      if (native) {
+        NS_ADDREF(*nodeList = nsContentList::FromSupports(native));
+        rv = NS_OK;
+      }
+      else {
+        rv = NS_ERROR_FAILURE;
+      }
+    }
+  } else {
+    
+
+    nsRefPtr<nsContentList> list =
+      domdoc->GetElementsByTagName(NS_LITERAL_STRING("*"));
+    if (!list) {
+      rv = NS_ERROR_OUT_OF_MEMORY;
+    }
+
+    nsresult tmp = WrapNative(cx, JS::CurrentGlobalOrNull(cx),
+                              static_cast<nsINodeList*>(list), list, false,
+                              &collection);
+    if (NS_FAILED(tmp)) {
+      rv = tmp;
+    }
+
+    list.forget(nodeList);
+
+    
+    JS_SetReservedSlot(obj, 0, collection);
+  }
+
+  if (NS_FAILED(rv)) {
+    xpc::Throw(cx, NS_ERROR_FAILURE);
+
+    return false;
+  }
+
+  return *nodeList != nullptr;
+}
+
+bool
+nsHTMLDocumentSH::DocumentAllGetProperty(JSContext *cx, JS::Handle<JSObject*> obj_,
+                                         JS::Handle<jsid> id, JS::MutableHandle<JS::Value> vp)
+{
+  JS::Rooted<JSObject*> obj(cx, obj_);
+
+  
+  
+  
+  
+  if (nsDOMClassInfo::sItem_id == id || nsDOMClassInfo::sNamedItem_id == id) {
+    return true;
+  }
+
+  JS::Rooted<JSObject*> proto(cx);
+  while (js::GetObjectJSClass(obj) != &sHTMLDocumentAllClass) {
+    if (!js::GetObjectProto(cx, obj, &proto)) {
+      return false;
+    }
+
+    if (!proto) {
+      NS_ERROR("The JS engine lies!");
+      return true;
+    }
+
+    obj = proto;
+  }
+
+  nsHTMLDocument *doc = GetDocument(obj);
+  nsISupports *result;
+  nsWrapperCache *cache;
+  nsresult rv = NS_OK;
+
+  if (JSID_IS_STRING(id)) {
+    if (nsDOMClassInfo::sLength_id == id) {
+      
+      
+      
+
+      nsRefPtr<nsContentList> nodeList;
+      if (!GetDocumentAllNodeList(cx, obj, doc, getter_AddRefs(nodeList))) {
+        return false;
+      }
+
+      uint32_t length;
+      rv = nodeList->GetLength(&length);
+
+      if (NS_FAILED(rv)) {
+        xpc::Throw(cx, rv);
+
+        return false;
+      }
+
+      vp.set(INT_TO_JSVAL(length));
+
+      return true;
+    }
+
+    
+    nsDependentJSString str(id);
+    result = doc->GetDocumentAllResult(str, &cache, &rv);
+
+    if (NS_FAILED(rv)) {
+      xpc::Throw(cx, rv);
+      return false;
+    }
+  } else if (JSID_IS_INT(id) && JSID_TO_INT(id) >= 0) {
+    
+    
+
+    nsRefPtr<nsContentList> nodeList;
+    if (!GetDocumentAllNodeList(cx, obj, doc, getter_AddRefs(nodeList))) {
+      return false;
+    }
+
+    nsIContent *node = nodeList->Item(JSID_TO_INT(id));
+
+    result = node;
+    cache = node;
+  } else {
+    result = nullptr;
+  }
+
+  if (result) {
+    rv = WrapNative(cx, JS::CurrentGlobalOrNull(cx), result, cache, true, vp);
+    if (NS_FAILED(rv)) {
+      xpc::Throw(cx, rv);
+
+      return false;
+    }
+  } else {
+    vp.setUndefined();
+  }
+
+  return true;
+}
+
+bool
+nsHTMLDocumentSH::DocumentAllNewResolve(JSContext *cx, JS::Handle<JSObject*> obj,
+                                        JS::Handle<jsid> id, unsigned flags,
+                                        JS::MutableHandle<JSObject*> objp)
+{
+  JS::Rooted<JS::Value> v(cx);
+
+  if (nsDOMClassInfo::sItem_id == id || nsDOMClassInfo::sNamedItem_id == id) {
+    
+
+    JSFunction *fnc = ::JS_DefineFunctionById(cx, obj, id, CallToGetPropMapper,
+                                              0, JSPROP_ENUMERATE);
+    objp.set(obj);
+
+    return fnc != nullptr;
+  }
+
+  if (nsDOMClassInfo::sLength_id == id) {
+    
+    
+    
+    
+
+    v = JSVAL_ONE;
+  } else {
+    if (!DocumentAllGetProperty(cx, obj, id, &v)) {
+      return false;
+    }
+  }
+
+  bool ok = true;
+
+  if (v.get() != JSVAL_VOID) {
+    ok = ::JS_DefinePropertyById(cx, obj, id, v, nullptr, nullptr, 0);
+    objp.set(obj);
+  }
+
+  return ok;
+}
+
+void
+nsHTMLDocumentSH::ReleaseDocument(JSFreeOp *fop, JSObject *obj)
+{
+  nsIHTMLDocument* doc = GetDocument(obj);
+  if (doc) {
+    nsContentUtils::DeferredFinalize(doc);
+  }
+}
+
+bool
+nsHTMLDocumentSH::CallToGetPropMapper(JSContext *cx, unsigned argc, jsval *vp)
+{
+  JS::CallArgs args = JS::CallArgsFromVp(argc, vp);
+  
+
+  if (args.length() != 1) {
+    
+    
+    
+    xpc::Throw(cx, NS_ERROR_INVALID_ARG);
+
+    return false;
+  }
+
+  
+  JS::Rooted<JSString*> str(cx, JS::ToString(cx, args[0]));
+  if (!str) {
+    return false;
+  }
+
+  
+  
+  JS::Rooted<JSObject*> self(cx);
+  if (args.calleev().isObject() &&
+      JS_GetClass(&args.calleev().toObject()) == &sHTMLDocumentAllClass) {
+    self = &args.calleev().toObject();
+  } else {
+    self = JS_THIS_OBJECT(cx, vp);
+    if (!self)
+      return false;
+  }
+
+  size_t length;
+  JS::Anchor<JSString *> anchor(str);
+  const jschar *chars = ::JS_GetStringCharsAndLength(cx, str, &length);
+  if (!chars) {
+    return false;
+  }
+
+  return ::JS_GetUCProperty(cx, self, chars, length, args.rval());
+}
