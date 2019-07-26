ea72b3a5a6a1bc19e3b3ad071c68cd0d9ca0519d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:41:20 +0000

    Bug 911951 part.1 Redesign nsIDOMWindowUtils::SendTextEvent() with nsICompositionStringSynthesizer r=smaug, sr=roc
    UltraBlame original commit: 50a2ec396387d6f8fd27e00142fcf9dd974941c7

---
+++ dom/base/CompositionStringSynthesizer.h
@@ -0,0 +1,45 @@
+
+
+
+
+
+#ifndef mozilla_dom_compositionstringsynthesizer_h__
+#define mozilla_dom_compositionstringsynthesizer_h__
+
+#include "nsICompositionStringSynthesizer.h"
+#include "nsGUIEvent.h"
+#include "nsString.h"
+#include "nsTArray.h"
+#include "nsWeakReference.h"
+#include "mozilla/Attributes.h"
+
+class nsIWidget;
+class nsPIDOMWindow;
+
+namespace mozilla {
+namespace dom {
+
+class CompositionStringSynthesizer MOZ_FINAL :
+  public nsICompositionStringSynthesizer
+{
+public:
+  CompositionStringSynthesizer(nsPIDOMWindow* aWindow);
+  ~CompositionStringSynthesizer();
+
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSICOMPOSITIONSTRINGSYNTHESIZER
+
+private:
+  nsWeakPtr mWindow; 
+  nsString mString;
+  nsAutoTArray<nsTextRange, 10> mClauses;
+  nsTextRange mCaret;
+
+  nsIWidget* GetWidget();
+  void ClearInternal();
+};
+
+} 
+} 
+
+#endif 
