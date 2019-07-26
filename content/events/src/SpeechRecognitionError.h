b829a09ed78fa992784b9fdde252e861aff6e110
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:54:07 +0000

    Bug 884407 - Use an enum in SpeechRecognitionError. r=smaug
    UltraBlame original commit: da5456a94c1b5b9a9f01c4da389d539813b114df

---
+++ content/events/src/SpeechRecognitionError.h
@@ -0,0 +1,61 @@
+
+
+
+
+
+#ifndef SpeechRecognitionError_h__
+#define SpeechRecognitionError_h__
+
+#include "nsDOMEvent.h"
+#include "mozilla/dom/SpeechRecognitionErrorBinding.h"
+
+namespace mozilla {
+namespace dom {
+
+class SpeechRecognitionError : public nsDOMEvent
+{
+public:
+  SpeechRecognitionError(mozilla::dom::EventTarget* aOwner,
+                         nsPresContext* aPresContext,
+                         nsEvent* aEvent);
+  virtual ~SpeechRecognitionError();
+
+  static already_AddRefed<SpeechRecognitionError> Constructor(const GlobalObject& aGlobal,
+                                                              const nsAString& aType,
+                                                              const SpeechRecognitionErrorInit& aParam,
+                                                              ErrorResult& aRv);
+
+  virtual JSObject* WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope) MOZ_OVERRIDE
+  {
+    return mozilla::dom::SpeechRecognitionErrorBinding::Wrap(aCx, aScope, this);
+  }
+
+  void
+  GetMessage(nsAString& aString)
+  {
+    aString = mMessage;
+  }
+
+  SpeechRecognitionErrorCode
+  Error()
+  {
+    return mError;
+  }
+
+  void
+  InitSpeechRecognitionError(const nsAString& aType,
+                             bool aCanBubble,
+                             bool aCancelable,
+                             SpeechRecognitionErrorCode aError,
+                             const nsAString& aMessage,
+                             ErrorResult& aRv);
+
+protected:
+  SpeechRecognitionErrorCode mError;
+  nsString mMessage;
+};
+
+}
+}
+
+#endif 
