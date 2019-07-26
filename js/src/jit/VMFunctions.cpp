a5e3f93f3fee3ee1e99b7036312817442d96255b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:25:48 +0000

    Backout 5c4e5ee49fbe for bustage on a CLOSED TREE.
    UltraBlame original commit: 5e7bc6009afca70d5343db407596272d96078388

--- js/src/jit/VMFunctions.cpp
+++ js/src/jit/VMFunctions.cpp
@@ -458,17 +458,7 @@ JSObject *
 NewCallObject(JSContext *cx, HandleScript script,
               HandleShape shape, HandleTypeObject type, HeapSlot *slots)
 {
-    JSObject *obj = CallObject::create(cx, script, shape, type, slots);
-
-#ifdef JSGC_GENERATIONAL
-    
-    
-    
-    if (!IsInsideNursery(cx->runtime(), obj))
-        cx->runtime()->gcStoreBuffer.putWholeCell(obj);
-#endif
-
-    return obj;
+    return CallObject::create(cx, script, shape, type, slots);
 }
 
 JSObject *
