5cf06dd7c9c20ef4b64922926a79d2327d988927
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:47:14 +0000

    Bug 962490 - Add a search field to the new tab page (part 1: ContentSearch). r=felipe
    UltraBlame original commit: eeafc69ebfb15c487e0b8cebedae42003c31fefb

---
+++ browser/modules/test/contentSearch.js
@@ -0,0 +1,21 @@
+
+
+
+
+const TEST_MSG = "ContentSearchTest";
+const SERVICE_EVENT_TYPE = "ContentSearchService";
+const CLIENT_EVENT_TYPE = "ContentSearchClient";
+
+
+content.addEventListener(SERVICE_EVENT_TYPE, event => {
+  sendAsyncMessage(TEST_MSG, event.detail);
+});
+
+
+addMessageListener(TEST_MSG, msg => {
+  content.dispatchEvent(
+    new content.CustomEvent(CLIENT_EVENT_TYPE, {
+      detail: msg.data,
+    })
+  );
+});
