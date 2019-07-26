90bc6517ac96f9eec1def792cd321ab39748a9de
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:56:14 +0000

    Bug 940259 - Test Case for the support of CDMA WAP Push. r=gene
    UltraBlame original commit: 66bc0f216f6e96db150e27a5ec1b54425ea15fb5

--- dom/system/gonk/tests/test_ril_worker_sms_cdma.js
+++ dom/system/gonk/tests/test_ril_worker_sms_cdma.js
@@ -29,6 +29,164 @@ function _getWorker() {
 
 
 
+
+
+
+function hexStringToBytes(hexString) {
+  let bytes = [];
+
+  let length = hexString.length;
+
+  for (let i = 0; i < length; i += 2) {
+    bytes.push(Number.parseInt(hexString.substr(i, 2), 16));
+  }
+
+  return bytes;
+}
+
+
+
+
+
+
+
+function bytesToHexString(bytes) {
+  let hexString = "";
+  let hex;
+
+  for (let i = 0; i < bytes.length; i++) {
+    hex = bytes[i].toString(16).toUpperCase();
+    if (hex.length === 1) {
+      hexString += "0";
+    }
+    hexString += hex;
+  }
+
+  return hexString;
+}
+
+
+
+
+
+
+
+
+
+function encodeOpaqueUserData(bitBufferHelper, options) {
+  let bearerDataBuffer = [];
+  bitBufferHelper.startWrite(bearerDataBuffer);
+
+  
+  bitBufferHelper.writeBits(PDU_CDMA_MSG_USERDATA_MSG_ID, 8);
+  bitBufferHelper.writeBits(3, 8);
+  bitBufferHelper.writeBits(options.msg_type, 4); 
+  bitBufferHelper.writeBits(1, 16);               
+  bitBufferHelper.flushWithPadding();             
+
+  
+  bitBufferHelper.writeBits(PDU_CDMA_MSG_USERDATA_BODY, 8);
+  let dataLength = options.data.length;
+  bitBufferHelper.writeBits(2 + dataLength, 8);   
+  bitBufferHelper.writeBits(PDU_CDMA_MSG_CODING_OCTET, 5); 
+                                                  
+  bitBufferHelper.writeBits(dataLength, 8);       
+  for (let i = 0; i < dataLength; i++) {          
+    bitBufferHelper.writeBits(options.data[i], 8);
+  }
+  bitBufferHelper.flushWithPadding();             
+
+  return bearerDataBuffer;
+}
+
+function newSmsParcel(cdmaPduHelper, pdu) {
+  return newIncomingParcel(-1,
+                           RESPONSE_TYPE_UNSOLICITED,
+                           UNSOLICITED_RESPONSE_CDMA_NEW_SMS,
+                           pduToParcelData(cdmaPduHelper, pdu));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function pduToParcelData(cdmaPduHelper, pdu) {
+
+  let addrInfo = cdmaPduHelper.encodeAddr(pdu.address);
+  
+  
+  
+  
+  let dataLength = 4 + 4 + 4
+                 + (5 + addrInfo.address.length) * 4
+                 + 3 * 4
+                 + 4 + pdu.bearerData.length * 4;
+
+  let data = new Uint8Array(dataLength);
+  let offset = 0;
+
+  function writeInt(value) {
+    data[offset++] = value & 0xFF;
+    data[offset++] = (value >>  8) & 0xFF;
+    data[offset++] = (value >> 16) & 0xFF;
+    data[offset++] = (value >> 24) & 0xFF;
+  }
+
+  function writeByte(value) {
+    data[offset++] = value & 0xFF;
+    data[offset++] = 0;
+    data[offset++] = 0;
+    data[offset++] = 0;
+  }
+
+  
+  writeInt(pdu.teleservice);
+
+  
+  writeByte(0);
+
+  
+  writeInt(PDU_CDMA_MSG_CATEGORY_UNSPEC);
+
+  
+  writeByte(addrInfo.digitMode);
+  writeByte(addrInfo.numberMode);
+  writeByte(addrInfo.numberType);
+  writeByte(addrInfo.numberPlan);
+  let addressLength = addrInfo.address.length;
+  writeByte(addressLength);
+  for (let i = 0; i < addressLength; i++) {
+    writeByte(addrInfo.address[i]);
+  }
+
+  
+  writeByte(0);
+  writeByte(0);
+  writeByte(0);
+
+  
+  let dataLength = pdu.bearerData.length;
+  writeByte(dataLength);
+
+  
+  for (let i = 0; i < dataLength; i++) {
+    writeByte(pdu.bearerData[i]);
+  }
+
+  return data;
+}
+
+
+
+
 add_test(function test_processCdmaSmsStatusReport() {
   let workerHelper = _getWorker();
   let worker = workerHelper.worker;
@@ -73,7 +231,7 @@ add_test(function test_processCdmaSmsStatusReport() {
     let postedMessage = workerHelper.postedMessage;
 
     
-    do_check_true((errorClass === 2)? !!sentSmsMap[msgId]: !sentSmsMap[msgId]);
+    do_check_true((errorClass === 2) ? !!sentSmsMap[msgId] : !sentSmsMap[msgId]);
 
     
     if (errorClass === -1) {
@@ -95,6 +253,70 @@ add_test(function test_processCdmaSmsStatusReport() {
   test_StatusReport(0, 0);   
   test_StatusReport(2, 4);   
   test_StatusReport(3, 5);   
+
+  run_next_test();
+});
+
+
+
+
+add_test(function test_processCdmaSmsWapPush() {
+  let workerHelper = _getWorker(),
+      worker = workerHelper.worker,
+      bitBufferHelper = worker.BitBufferHelper,
+      cdmaPduHelper = worker.CdmaPDUHelper;
+
+  function test_CdmaSmsWapPdu(wdpData, reversed) {
+    let orig_address = "0987654321",
+        hexString,
+        fullDataHexString = "";
+
+    for (let i = 0; i < wdpData.length; i++) {
+      let dataIndex = (reversed) ? (wdpData.length - i - 1) : i;
+      hexString  = "00";                                
+      hexString += bytesToHexString([wdpData.length]);  
+      hexString += bytesToHexString([dataIndex]);       
+      if ((dataIndex === 0)) {
+        hexString += "23F00B84";                        
+      }
+      hexString += wdpData[dataIndex];                  
+
+      do_print("hexString: " + hexString);
+
+      fullDataHexString += wdpData[i];
+
+      let pdu = {
+        teleservice: PDU_CDMA_MSG_TELESERIVCIE_ID_WAP,
+        address:     orig_address,
+        bearerData:  encodeOpaqueUserData(bitBufferHelper,
+                                          { msg_type: PDU_CDMA_MSG_TYPE_DELIVER,
+                                            data:     hexStringToBytes(hexString) })
+      };
+
+      worker.onRILMessage(newSmsParcel(cdmaPduHelper, pdu));
+    }
+
+    let postedMessage = workerHelper.postedMessage;
+
+    do_print("fullDataHexString: " + fullDataHexString);
+
+    do_check_eq("sms-received", postedMessage.rilMessageType);
+    do_check_eq(PDU_CDMA_MSG_TELESERIVCIE_ID_WAP, postedMessage.teleservice);
+    do_check_eq(orig_address, postedMessage.sender);
+    do_check_eq(0x23F0, postedMessage.header.originatorPort);
+    do_check_eq(0x0B84, postedMessage.header.destinationPort);
+    do_check_eq(fullDataHexString, bytesToHexString(postedMessage.fullData));
+  }
+
+  
+  test_CdmaSmsWapPdu(["000102030405060708090A0B0C0D0E0F"]);
+
+  
+  test_CdmaSmsWapPdu(["000102030405060708090A0B0C0D0E0F", "0F0E0D0C0B0A09080706050403020100"]);
+
+  
+  
+  test_CdmaSmsWapPdu(["000102030405060708090A0B0C0D0E0F", "0F0E0D0C0B0A09080706050403020100"], true);
 
   run_next_test();
 });
