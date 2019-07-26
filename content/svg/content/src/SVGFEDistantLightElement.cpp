c806d4d90dfb52765747d1a5b73d75825224f47a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:07:57 +0000

    Bug 832155: Convert SVGFEDistantLightElement to WebIDL r=Ms2ger
    UltraBlame original commit: 9606fa13b8e30f6b611569883f349adf241dbf88

--- content/svg/content/src/SVGFEDistantLightElement.cpp
+++ content/svg/content/src/SVGFEDistantLightElement.cpp
@@ -4,10 +4,20 @@
 
 
 #include "mozilla/dom/SVGFEDistantLightElement.h"
+#include "mozilla/dom/SVGFEDistantLightElementBinding.h"
 
-NS_IMPL_NS_NEW_SVG_ELEMENT(FEDistantLight)
+NS_IMPL_NS_NEW_NAMESPACED_SVG_ELEMENT(FEDistantLight)
 
-nsSVGElement::NumberInfo nsSVGFEDistantLightElement::sNumberInfo[2] =
+namespace mozilla {
+namespace dom {
+
+JSObject*
+SVGFEDistantLightElement::WrapNode(JSContext* aCx, JSObject* aScope)
+{
+  return SVGFEDistantLightElementBinding::Wrap(aCx, aScope, this);
+}
+
+nsSVGElement::NumberInfo SVGFEDistantLightElement::sNumberInfo[2] =
 {
   { &nsGkAtoms::azimuth,   0, false },
   { &nsGkAtoms::elevation, 0, false }
@@ -16,48 +26,52 @@ nsSVGElement::NumberInfo nsSVGFEDistantLightElement::sNumberInfo[2] =
 
 
 
-NS_IMPL_ADDREF_INHERITED(nsSVGFEDistantLightElement,nsSVGFEDistantLightElementBase)
-NS_IMPL_RELEASE_INHERITED(nsSVGFEDistantLightElement,nsSVGFEDistantLightElementBase)
+NS_IMPL_ADDREF_INHERITED(SVGFEDistantLightElement,SVGFEDistantLightElementBase)
+NS_IMPL_RELEASE_INHERITED(SVGFEDistantLightElement,SVGFEDistantLightElementBase)
 
-DOMCI_NODE_DATA(SVGFEDistantLightElement, nsSVGFEDistantLightElement)
-
-NS_INTERFACE_TABLE_HEAD(nsSVGFEDistantLightElement)
-  NS_NODE_INTERFACE_TABLE4(nsSVGFEDistantLightElement, nsIDOMNode,
-                           nsIDOMElement, nsIDOMSVGElement,
-                           nsIDOMSVGFEDistantLightElement)
-  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(SVGFEDistantLightElement)
-NS_INTERFACE_MAP_END_INHERITING(nsSVGFEDistantLightElementBase)
+NS_INTERFACE_TABLE_HEAD(SVGFEDistantLightElement)
+  NS_NODE_INTERFACE_TABLE3(SVGFEDistantLightElement, nsIDOMNode,
+                           nsIDOMElement, nsIDOMSVGElement)
+NS_INTERFACE_MAP_END_INHERITING(SVGFEDistantLightElementBase)
 
 
 
 
-NS_IMPL_ELEMENT_CLONE_WITH_INIT(nsSVGFEDistantLightElement)
+NS_IMPL_ELEMENT_CLONE_WITH_INIT(SVGFEDistantLightElement)
 
 
 
 
 bool
-nsSVGFEDistantLightElement::AttributeAffectsRendering(int32_t aNameSpaceID,
-                                                      nsIAtom* aAttribute) const
+SVGFEDistantLightElement::AttributeAffectsRendering(int32_t aNameSpaceID,
+                                                    nsIAtom* aAttribute) const
 {
   return aNameSpaceID == kNameSpaceID_None &&
          (aAttribute == nsGkAtoms::azimuth ||
           aAttribute == nsGkAtoms::elevation);
 }
 
-
-
-
-NS_IMETHODIMP
-nsSVGFEDistantLightElement::GetAzimuth(nsIDOMSVGAnimatedNumber **aAzimuth)
+already_AddRefed<nsIDOMSVGAnimatedNumber>
+SVGFEDistantLightElement::Azimuth()
 {
-  return mNumberAttributes[AZIMUTH].ToDOMAnimatedNumber(aAzimuth,
-                                                        this);
+  return mNumberAttributes[AZIMUTH].ToDOMAnimatedNumber(this);
 }
 
-NS_IMETHODIMP
-nsSVGFEDistantLightElement::GetElevation(nsIDOMSVGAnimatedNumber **aElevation)
+already_AddRefed<nsIDOMSVGAnimatedNumber>
+SVGFEDistantLightElement::Elevation()
 {
-  return mNumberAttributes[ELEVATION].ToDOMAnimatedNumber(aElevation,
-                                                          this);
+  return mNumberAttributes[ELEVATION].ToDOMAnimatedNumber(this);
 }
+
+
+
+
+nsSVGElement::NumberAttributesInfo
+SVGFEDistantLightElement::GetNumberInfo()
+{
+  return NumberAttributesInfo(mNumberAttributes, sNumberInfo,
+                              ArrayLength(sNumberInfo));
+}
+
+} 
+} 
