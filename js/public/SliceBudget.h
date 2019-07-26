14ecf0052582e3ef59e59f1f1dc7a104a61ead83
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:51:12 +0000

    Bug 937751, part 2 - Move SliceBudget into js/public. r=billm
    UltraBlame original commit: dc1be6cd4cf650a8b22993c873177268f47d2cd0

---
+++ js/public/SliceBudget.h
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+#ifndef js_SliceBudget_h
+#define js_SliceBudget_h
+
+namespace js {
+
+
+
+
+
+
+
+struct JS_PUBLIC_API(SliceBudget) {
+    int64_t deadline; 
+    intptr_t counter;
+
+    static const intptr_t CounterReset = 1000;
+
+    static const int64_t Unlimited = 0;
+    static int64_t TimeBudget(int64_t millis);
+    static int64_t WorkBudget(int64_t work);
+
+    
+    SliceBudget();
+
+    
+    SliceBudget(int64_t budget);
+
+    void reset() {
+        deadline = INT64_MAX;
+        counter = INTPTR_MAX;
+    }
+
+    void step(intptr_t amt = 1) {
+        counter -= amt;
+    }
+
+    bool checkOverBudget();
+
+    bool isOverBudget() {
+        if (counter >= 0)
+            return false;
+        return checkOverBudget();
+    }
+};
+
+} 
+
+#endif 
