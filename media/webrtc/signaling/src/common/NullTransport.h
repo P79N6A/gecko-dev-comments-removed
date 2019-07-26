8a7120ca3fdacf3204dcbd92c86f647cdfe7e815
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:27:27 +0000

    Bug 811695: disable internal socket transports for getUserMedia Audio capture r=derf
    UltraBlame original commit: 045367d6644efb8e12759d96bdc6ae0608a513e4

---
+++ media/webrtc/signaling/src/common/NullTransport.h
@@ -0,0 +1,44 @@
+
+
+
+
+
+#ifndef NULL_TRANSPORT_H_
+#define NULL_TRANSPORT_H_
+
+#include "mozilla/Attributes.h"
+
+#include "common_types.h"
+
+namespace mozilla {
+
+
+
+
+class NullTransport : public webrtc::Transport
+{
+public:
+  virtual int SendPacket(int channel, const void *data, int len)
+  {
+    (void) channel; (void) data;
+    return len;
+  }
+
+  virtual int SendRTCPPacket(int channel, const void *data, int len)
+  {
+    (void) channel; (void) data;
+    return len;
+  }
+
+  NullTransport() {}
+
+  virtual ~NullTransport() {};
+
+private:
+  NullTransport(const NullTransport& other) MOZ_DELETE;
+  void operator=(const NullTransport& other) MOZ_DELETE;
+};
+
+} 
+
+#endif
