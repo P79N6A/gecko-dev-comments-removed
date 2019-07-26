e56ce80101a3f2284e28971a8c83c29b1a244789
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:53:07 +0000

    Bug 879558 - Add entry point to about:healthreport in Settings > Data choices on Android. r=rnewman
    UltraBlame original commit: 38244599c28210a1a502d17139372be9bfc97b88

---
+++ mobile/android/base/AlignRightLinkPreference.java
@@ -0,0 +1,22 @@
+
+
+
+
+
+package org.mozilla.gecko;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+class AlignRightLinkPreference extends LinkPreference {
+
+    public AlignRightLinkPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_rightalign_icon);
+    }
+
+    public AlignRightLinkPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setLayoutResource(R.layout.preference_rightalign_icon);
+    }
+}
