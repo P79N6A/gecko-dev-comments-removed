fce47ed02be0bf2afa5877b420e93bfe185d918a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:08:17 +0000

    Bug 849603 - Make sure we use StartRemoveFrame for the first (existing) list to remove from.  r=bzbarsky
    UltraBlame original commit: fb7e2b6eef1b95a47dc33aec8bb596a0837ca9ff

--- layout/generic/nsContainerFrame.cpp
+++ layout/generic/nsContainerFrame.cpp
@@ -1218,11 +1218,10 @@ nsContainerFrame::DisplayOverflowContainers(nsDisplayListBuilder*   aBuilder,
 
 static bool
 TryRemoveFrame(nsIFrame* aFrame, FramePropertyTable* aPropTable,
-               const FramePropertyDescriptor* aProp, nsIFrame* aChildToRemove,
-               bool (nsFrameList::*aRemoveMethod)(nsIFrame* aFrame))
+               const FramePropertyDescriptor* aProp, nsIFrame* aChildToRemove)
 {
   nsFrameList* list = static_cast<nsFrameList*>(aPropTable->Get(aFrame, aProp));
-  if (list && (list->*aRemoveMethod)(aChildToRemove)) {
+  if (list && list->StartRemoveFrame(aChildToRemove)) {
     
     if (list->IsEmpty()) {
       aPropTable->Remove(aFrame, aProp);
@@ -1261,12 +1260,12 @@ nsContainerFrame::StealFrame(nsPresContext* aPresContext,
     FramePropertyTable* propTable = aPresContext->PropertyTable();
     
     removed = ::TryRemoveFrame(this, propTable, OverflowContainersProperty(),
-                               aChild, &nsFrameList::StartRemoveFrame);
+                               aChild);
     if (!removed) {
       
       removed = ::TryRemoveFrame(this, propTable,
                                  ExcessOverflowContainersProperty(),
-                                 aChild, &nsFrameList::ContinueRemoveFrame);
+                                 aChild);
     }
   } else {
     removed = mFrames.StartRemoveFrame(aChild);
