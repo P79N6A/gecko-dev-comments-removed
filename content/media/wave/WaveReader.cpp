4931df0218b43ac5542f2b70c26decd5ede91b31
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:47:02 +0000

    Bug 879924 - Don't shorten zero-length strings. r=derf
    We were trying to strip the optional null terminator
    even when we knew there wasn't one there.
    
    UltraBlame original commit: 2912073936087029874748e744c7a55278dc07cf

--- content/media/wave/WaveReader.cpp
+++ content/media/wave/WaveReader.cpp
@@ -579,8 +579,10 @@ WaveReader::LoadListChunk(uint32_t aChunkSize,
       break;
     }
 
+    
+    
     nsCString val(p, length);
-    if (val[length - 1] == '\0') {
+    if (length > 0 && val[length - 1] == '\0') {
       val.SetLength(length - 1);
     }
 
