ad1a46b9645f89cba2cc878691d146397cbc7296
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:20:50 +0000

    Bug 950430 - Potential fix for intermittent browser_webconsole_chrome.js | Test timed out; r=me
    UltraBlame original commit: c1c8a7310289b938ccdbebf7a4ee51039cf21b6f

--- browser/devtools/webconsole/test/browser_webconsole_chrome.js
+++ browser/devtools/webconsole/test/browser_webconsole_chrome.js
@@ -6,30 +6,31 @@
 
 
 function test() {
-  addTab("about:credits");
-  browser.addEventListener("load", function onLoad() {
-    browser.removeEventListener("load", onLoad, true);
-    openConsole(null, testChrome);
-  }, true);
+  Task.spawn(function*() {
+    const {tab} = yield loadTab("about:credits");
+    ok(tab, "tab loaded");
+
+    const hud = yield openConsole(tab);
+    ok(hud, "we have a console");
+    ok(hud.iframeWindow, "we have the console UI window");
+
+    let jsterm = hud.jsterm;
+    ok(jsterm, "we have a jsterm");
+
+    let input = jsterm.inputNode;
+    ok(hud.outputNode, "we have an output node");
+
+    
+    input.value = "docu";
+    input.setSelectionRange(4, 4);
+
+    let deferred = promise.defer();
+
+    jsterm.complete(jsterm.COMPLETE_HINT_ONLY, function() {
+      is(jsterm.completeNode.value, "    ment", "'docu' completion");
+      deferred.resolve(null);
+    });
+
+    yield deferred.promise;
+  }).then(finishTest);
 }
-
-function testChrome(hud) {
-  ok(hud, "we have a console");
-
-  ok(hud.iframeWindow, "we have the console UI window");
-
-  let jsterm = hud.jsterm;
-  ok(jsterm, "we have a jsterm");
-
-  let input = jsterm.inputNode;
-  ok(hud.outputNode, "we have an output node");
-
-  
-  input.value = "docu";
-  input.setSelectionRange(4, 4);
-  jsterm.complete(jsterm.COMPLETE_HINT_ONLY, function() {
-    is(jsterm.completeNode.value, "    ment", "'docu' completion");
-    executeSoon(finishTest);
-  });
-}
-
