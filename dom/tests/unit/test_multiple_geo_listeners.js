5175707dc34c8286d87beb79eb0f21a2d82ea9cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:16:41 +0000

    Bug 850442 - Part 4 - Make nsIDOMGeoGeolocation non-scriptable. r=bz
    UltraBlame original commit: 23e907c7c1068a8ec48a8e60d68adc5af4113e74

--- dom/tests/unit/test_multiple_geo_listeners.js
+++ dom/tests/unit/test_multiple_geo_listeners.js
@@ -10,34 +10,26 @@ var geolocation = null;
 var success = false;
 var watchId = -1;
 
+let gAccuracy = 42;
 function geoHandler(metadata, response)
 {
-    var georesponse = {
-        status: "OK",
-        location: {
-            lat: 42,
-            lng: 42,
-        },
-        accuracy: 42,
-    };
+  var georesponse = {
+      status: "OK",
+      location: {
+          lat: 0.45,
+          lng: 0.45,
+      },
+      accuracy: gAccuracy,
+  };
   var position = JSON.stringify(georesponse);
-  response.processAsync();
   response.setStatusLine("1.0", 200, "OK");
   response.setHeader("Cache-Control", "no-cache", false);
   response.setHeader("Content-Type", "aplication/x-javascript", false);
-  do_timeout(5000, function() {
-    response.write(position);
-    response.finish();
-  });
-}
-
-function successCallback() {
-  do_check_true(false);
-  do_test_finished();
+  response.write(position);
 }
 
 function errorCallback() {
-  do_check_true(true);
+  do_check_true(false);
   do_test_finished();
 }
 
@@ -53,10 +45,26 @@ function run_test()
         .processType == Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT) {
     var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
     prefs.setBoolPref("geo.wifi.scan", false);
-    prefs.setCharPref("geo.wifi.uri", "http://localhost:4444/geo");  
+    prefs.setCharPref("geo.wifi.uri", "http://localhost:4444/geo");
     prefs.setBoolPref("geo.testing.ignore_ipc_principal", true);
   }
 
-  geolocation = Cc["@mozilla.org/geolocation;1"].getService(Ci.nsISupports);
-  geolocation.getCurrentPosition(successCallback, errorCallback, {timeout: 2000});
+  let timesCalled = 0;
+  geolocation = Cc["@mozilla.org/geolocation;1"].createInstance(Ci.nsISupports);
+  geolocation.watchPosition(function(pos) {
+    do_check_eq(++timesCalled, 1);
+    do_check_eq(pos.coords.accuracy, gAccuracy);
+
+    gAccuracy = 420;
+    geolocation2 = Cc["@mozilla.org/geolocation;1"].createInstance(Ci.nsISupports);
+    geolocation2.getCurrentPosition(function(pos) {
+      do_check_eq(pos.coords.accuracy, gAccuracy);
+
+      gAccuracy = 400;
+      geolocation2.getCurrentPosition(function(pos) {
+        do_check_eq(pos.coords.accuracy, 42);
+        do_test_finished();
+      }, errorCallback);
+    }, errorCallback, {maximumAge: 0});
+  }, errorCallback, {maximumAge: 0});
 }
