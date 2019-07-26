7dd8231e987c53ff3347e2d590c3ea5827e20670
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:22:59 +0000

    Bug 906368 - IonMonkey: Define a proper CountPopulation32 function, and use it in place of manual code in RegisterSets.h. r=nbp
    UltraBlame original commit: 8d3b2cb5d698934a49dfa315f8ba7bcb55a20dc2

---
+++ mfbt/tests/TestCountPopulation.cpp
@@ -0,0 +1,32 @@
+
+
+
+
+
+#include "mozilla/MathAlgorithms.h"
+
+using mozilla::CountPopulation32;
+
+static void
+TestCountPopulation32()
+{
+  MOZ_ASSERT(CountPopulation32(0xFFFFFFFF) == 32);
+  MOZ_ASSERT(CountPopulation32(0xF0FF1000) == 13);
+  MOZ_ASSERT(CountPopulation32(0x7F8F0001) == 13);
+  MOZ_ASSERT(CountPopulation32(0x3FFF0100) == 15);
+  MOZ_ASSERT(CountPopulation32(0x1FF50010) == 12);
+  MOZ_ASSERT(CountPopulation32(0x00800000) == 1);
+  MOZ_ASSERT(CountPopulation32(0x00400000) == 1);
+  MOZ_ASSERT(CountPopulation32(0x00008000) == 1);
+  MOZ_ASSERT(CountPopulation32(0x00004000) == 1);
+  MOZ_ASSERT(CountPopulation32(0x00000080) == 1);
+  MOZ_ASSERT(CountPopulation32(0x00000040) == 1);
+  MOZ_ASSERT(CountPopulation32(0x00000001) == 1);
+  MOZ_ASSERT(CountPopulation32(0x00000000) == 0);
+}
+
+int main()
+{
+  TestCountPopulation32();
+  return 0;
+}
