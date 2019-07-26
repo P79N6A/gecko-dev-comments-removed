7d0f5ee4f7b46fbef7ed79d2c19f0593aa1caaf8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:06:30 +0000

    Bug 895104 - Remove call to no-longer-existant DOMRequestIpcHelper.observe in SystemMessageManager.observe. r=fabrice
    DOMRequestIpcHelper.observe was removed in bug 889984.
    
    UltraBlame original commit: 7bb5b6909d3e4e6c2ae1e851e77b7e72c7170d42

--- dom/messages/SystemMessageManager.js
+++ dom/messages/SystemMessageManager.js
@@ -282,8 +282,6 @@ SystemMessageManager.prototype = {
     if (aTopic === kSystemMessageInternalReady) {
       this._registerManifest();
     }
-    
-    this.__proto__.__proto__.observe.call(this, aSubject, aTopic, aData);
   },
 
   _registerManifest: function sysMessMgr_registerManifest() {
