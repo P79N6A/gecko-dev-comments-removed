0115f9bb573dea9ba5388aac9e2dc1e86edf9d59
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:16:08 +0000

    Bug 801488. Don't flush notifications when dragging a XUL splitter. r=bzbarsky
    UltraBlame original commit: 1ebc3b1b8feb8eb7c3e24a7772b66a462c6b9fa9

--- layout/xul/base/src/nsSplitterFrame.cpp
+++ layout/xul/base/src/nsSplitterFrame.cpp
@@ -901,10 +901,6 @@ nsSplitterFrameInner::AdjustChildren(nsPresContext* aPresContext)
 
   AdjustChildren(aPresContext, mChildInfosBefore, mChildInfosBeforeCount, isHorizontal);
   AdjustChildren(aPresContext, mChildInfosAfter, mChildInfosAfterCount, isHorizontal);
-   
-   
-
-  aPresContext->PresShell()->FlushPendingNotifications(Flush_Display);
 }
 
 static nsIFrame* GetChildBoxForContent(nsIFrame* aParentBox, nsIContent* aContent)
