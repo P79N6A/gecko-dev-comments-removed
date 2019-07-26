52f1340f7dcecaa83642ad420966804ecd59bbdc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:53:26 +0000

    Merge m-c to inbound.
    UltraBlame original commit: 2b398758366ce3cae904394dc9d79c35c75f5e89

--- gfx/layers/composite/TextureHost.cpp
+++ gfx/layers/composite/TextureHost.cpp
@@ -104,6 +104,13 @@ TextureHost::Create(uint64_t aID,
       return CreateTextureHostOGL(aID, aDesc, aDeallocator, aFlags);
     case LAYERS_BASIC:
       return CreateTextureHostBasic(aID, aDesc, aDeallocator, aFlags);
+#ifdef MOZ_WIDGET_GONK
+    case LAYERS_NONE:
+      
+      
+      
+      return CreateTextureHostOGL(aID, aDesc, aDeallocator, aFlags);
+#endif
 #ifdef XP_WIN
     case LAYERS_D3D11:
     case LAYERS_D3D9:
