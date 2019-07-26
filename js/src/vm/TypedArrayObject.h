23dea43d9b067b05510a6c80339aac7fc5c6463b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:46:26 +0000

    Bug 942304 - Avoid rooting hazard in ArrayBufferObject::neuterViews. r=terrence
    UltraBlame original commit: 46d26b2bc1f01f69d85a7ab3da1c06dabb9d1862

--- js/src/vm/TypedArrayObject.h
+++ js/src/vm/TypedArrayObject.h
@@ -198,7 +198,7 @@ class ArrayBufferObject : public JSObject
     
 
 
-    bool neuterViews(JSContext *cx);
+    static bool neuterViews(JSContext *cx, Handle<ArrayBufferObject*> buffer);
 
     inline uint8_t * dataPointer() const {
         return (uint8_t *) elements;
