21422d1bd00e3614bb911cc44c9a887074ca0db9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:51:11 +0000

    Bug 804387. Part 4: Move MediaStreamGraphImpl to its own header file. r=jesup
    UltraBlame original commit: 7faccaa6b26bb7cf1586ce1ecba015cb1c33cf42

---
+++ content/media/MediaStreamGraphImpl.h
@@ -0,0 +1,498 @@
+
+
+
+
+
+#ifndef MOZILLA_MEDIASTREAMGRAPHIMPL_H_
+#define MOZILLA_MEDIASTREAMGRAPHIMPL_H_
+
+#include "MediaStreamGraph.h"
+
+#include "mozilla/Monitor.h"
+#include "mozilla/TimeStamp.h"
+#include "nsIThread.h"
+#include "nsIRunnable.h"
+
+namespace mozilla {
+
+#ifdef PR_LOGGING
+extern PRLogModuleInfo* gMediaStreamGraphLog;
+#define LOG(type, msg) PR_LOG(gMediaStreamGraphLog, type, msg)
+#else
+#define LOG(type, msg)
+#endif
+
+
+
+
+
+
+static const int MEDIA_GRAPH_TARGET_PERIOD_MS = 10;
+
+
+
+
+
+static const int SCHEDULE_SAFETY_MARGIN_MS = 10;
+
+
+
+
+
+
+
+
+
+static const int AUDIO_TARGET_MS = 2*MEDIA_GRAPH_TARGET_PERIOD_MS +
+    SCHEDULE_SAFETY_MARGIN_MS;
+
+
+
+
+
+
+
+static const int VIDEO_TARGET_MS = 2*MEDIA_GRAPH_TARGET_PERIOD_MS +
+    SCHEDULE_SAFETY_MARGIN_MS;
+
+
+
+
+
+struct StreamUpdate {
+  int64_t mGraphUpdateIndex;
+  nsRefPtr<MediaStream> mStream;
+  StreamTime mNextMainThreadCurrentTime;
+  bool mNextMainThreadFinished;
+};
+
+
+
+
+
+class ControlMessage {
+public:
+  ControlMessage(MediaStream* aStream) : mStream(aStream)
+  {
+    MOZ_COUNT_CTOR(ControlMessage);
+  }
+  
+  virtual ~ControlMessage()
+  {
+    MOZ_COUNT_DTOR(ControlMessage);
+  }
+  
+  
+  
+  
+  virtual void Run() = 0;
+  
+  
+  virtual void RunDuringShutdown() {}
+  MediaStream* GetStream() { return mStream; }
+
+protected:
+  
+  
+  
+  MediaStream* mStream;
+};
+
+
+
+
+
+
+
+
+class MediaStreamGraphImpl : public MediaStreamGraph {
+public:
+  MediaStreamGraphImpl();
+  ~MediaStreamGraphImpl()
+  {
+    NS_ASSERTION(IsEmpty(),
+                 "All streams should have been destroyed by messages from the main thread");
+    LOG(PR_LOG_DEBUG, ("MediaStreamGraph %p destroyed", this));
+  }
+
+  
+  
+
+
+
+
+
+  void RunInStableState();
+  
+
+
+
+
+  void EnsureRunInStableState();
+  
+
+
+  void ApplyStreamUpdate(StreamUpdate* aUpdate);
+  
+
+
+
+  void AppendMessage(ControlMessage* aMessage);
+  
+
+
+
+
+
+  void ForceShutDown();
+  
+
+
+  void ShutdownThreads();
+
+  
+  
+  
+
+
+
+  void RunThread();
+  
+
+
+
+  void EnsureNextIteration();
+  
+
+
+  void EnsureNextIterationLocked(MonitorAutoLock& aLock);
+  
+
+
+
+  void EnsureImmediateWakeUpLocked(MonitorAutoLock& aLock);
+  
+
+
+
+
+  void EnsureStableStateEventPosted();
+  
+
+
+
+  void PrepareUpdatesToMainThreadState();
+  
+  
+
+
+
+  void UpdateCurrentTime();
+  
+
+
+
+  void UpdateConsumptionState(SourceMediaStream* aStream);
+  
+
+
+  void ExtractPendingInput(SourceMediaStream* aStream,
+                           GraphTime aDesiredUpToTime,
+                           bool* aEnsureNextIteration);
+  
+
+
+  void UpdateBufferSufficiencyState(SourceMediaStream* aStream);
+  
+
+
+
+  void UpdateStreamOrderForStream(nsTArray<MediaStream*>* aStack,
+                                  already_AddRefed<MediaStream> aStream);
+  
+
+
+  static void MarkConsumed(MediaStream* aStream);
+  
+
+
+
+
+  void UpdateStreamOrder();
+  
+
+
+
+
+
+  void RecomputeBlocking(GraphTime aEndBlockingDecisions);
+  
+  
+
+
+
+
+  void AddBlockingRelatedStreamsToSet(nsTArray<MediaStream*>* aStreams,
+                                      MediaStream* aStream);
+  
+
+
+
+
+  void MarkStreamBlocking(MediaStream* aStream);
+  
+
+
+
+
+
+  void RecomputeBlockingAt(const nsTArray<MediaStream*>& aStreams,
+                           GraphTime aTime, GraphTime aEndBlockingDecisions,
+                           GraphTime* aEnd);
+  
+
+
+
+
+
+  bool WillUnderrun(MediaStream* aStream, GraphTime aTime,
+                    GraphTime aEndBlockingDecisions, GraphTime* aEnd);
+  
+
+
+
+  StreamTime GraphTimeToStreamTime(MediaStream* aStream, GraphTime aTime);
+  enum {
+    INCLUDE_TRAILING_BLOCKED_INTERVAL = 0x01
+  };
+  
+
+
+
+
+
+
+
+
+  GraphTime StreamTimeToGraphTime(MediaStream* aStream, StreamTime aTime,
+                                  uint32_t aFlags = 0);
+  
+
+
+  GraphTime GetAudioPosition(MediaStream* aStream);
+  
+
+
+  void NotifyHasCurrentData(MediaStream* aStream);
+  
+
+
+
+  void CreateOrDestroyAudioStreams(GraphTime aAudioOutputStartTime,
+                                   MediaStream* aStream);
+  
+
+
+
+  void PlayAudio(MediaStream* aStream, GraphTime aFrom, GraphTime aTo);
+  
+
+
+  void PlayVideo(MediaStream* aStream);
+  
+
+
+
+
+  void FinishStream(MediaStream* aStream);
+  
+
+
+  StreamTime GetDesiredBufferEnd(MediaStream* aStream);
+  
+
+
+  bool IsEmpty() { return mStreams.IsEmpty() && mPortCount == 0; }
+
+  
+  
+
+
+  int64_t GetProcessingGraphUpdateIndex() { return mProcessingGraphUpdateIndex; }
+  
+
+
+  void AddStream(MediaStream* aStream);
+  
+
+
+
+  void RemoveStream(MediaStream* aStream);
+  
+
+
+  void DestroyPort(MediaInputPort* aPort);
+
+  
+
+  
+
+
+
+  nsCOMPtr<nsIThread> mThread;
+
+  
+  
+  
+
+  nsTArray<nsRefPtr<MediaStream> > mStreams;
+  
+
+
+
+  GraphTime mCurrentTime;
+  
+
+
+
+
+  GraphTime mStateComputedTime;
+  
+
+
+  TimeStamp mInitialTimeStamp;
+  
+
+
+  TimeStamp mCurrentTimeStamp;
+  
+
+
+  int64_t mProcessingGraphUpdateIndex;
+  
+
+
+  int32_t mPortCount;
+
+  
+  
+  
+  
+  
+  Monitor mMonitor;
+
+  
+  
+
+  
+
+
+  nsTArray<StreamUpdate> mStreamUpdates;
+  
+
+
+  nsTArray<nsCOMPtr<nsIRunnable> > mUpdateRunnables;
+  struct MessageBlock {
+    int64_t mGraphUpdateIndex;
+    nsTArray<nsAutoPtr<ControlMessage> > mMessages;
+  };
+  
+
+
+
+  nsTArray<MessageBlock> mMessageQueue;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  enum LifecycleState {
+    
+    LIFECYCLE_THREAD_NOT_STARTED,
+    
+    LIFECYCLE_RUNNING,
+    
+    
+    
+    
+    
+    
+    LIFECYCLE_WAITING_FOR_MAIN_THREAD_CLEANUP,
+    
+    
+    LIFECYCLE_WAITING_FOR_THREAD_SHUTDOWN,
+    
+    
+    
+    LIFECYCLE_WAITING_FOR_STREAM_DESTRUCTION
+  };
+  LifecycleState mLifecycleState;
+  
+
+
+  enum WaitState {
+    
+    WAITSTATE_RUNNING,
+    
+    
+    WAITSTATE_WAITING_FOR_NEXT_ITERATION,
+    
+    WAITSTATE_WAITING_INDEFINITELY,
+    
+    
+    WAITSTATE_WAKING_UP
+  };
+  WaitState mWaitState;
+  
+
+
+  bool mNeedAnotherIteration;
+  
+
+
+  bool mForceShutDown;
+  
+
+
+
+  bool mPostedRunInStableStateEvent;
+
+  
+
+  
+
+
+
+
+
+  nsTArray<nsAutoPtr<ControlMessage> > mCurrentTaskMessageQueue;
+  
+
+
+
+
+  bool mDetectedNotRunning;
+  
+
+
+
+  bool mPostedRunInStableState;
+};
+
+}
+
+#endif 
