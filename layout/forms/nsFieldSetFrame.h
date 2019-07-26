089a2530d4893d5dc417448bca84f2901f1609b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:20:20 +0000

    Bug 960277. Part 2: Move nsFieldSetFrame declaration to nsFieldSetFrame.h. r=mats
    UltraBlame original commit: 0cb7841990d8de527acc92dafacfca106c68b74d

---
+++ layout/forms/nsFieldSetFrame.h
@@ -0,0 +1,101 @@
+
+
+
+
+
+#ifndef nsFieldSetFrame_h___
+#define nsFieldSetFrame_h___
+
+#include "nsContainerFrame.h"
+
+class nsFieldSetFrame MOZ_FINAL : public nsContainerFrame
+{
+public:
+  NS_DECL_FRAMEARENA_HELPERS
+
+  nsFieldSetFrame(nsStyleContext* aContext);
+
+  NS_HIDDEN_(nscoord)
+    GetIntrinsicWidth(nsRenderingContext* aRenderingContext,
+                      nsLayoutUtils::IntrinsicWidthType);
+  virtual nscoord GetMinWidth(nsRenderingContext* aRenderingContext);
+  virtual nscoord GetPrefWidth(nsRenderingContext* aRenderingContext);
+  virtual nsSize ComputeSize(nsRenderingContext *aRenderingContext,
+                             nsSize aCBSize, nscoord aAvailableWidth,
+                             nsSize aMargin, nsSize aBorder, nsSize aPadding,
+                             uint32_t aFlags) MOZ_OVERRIDE;
+  virtual nscoord GetBaseline() const;
+
+  
+
+
+
+  virtual nsRect VisualBorderRectRelativeToSelf() const MOZ_OVERRIDE;
+
+  NS_IMETHOD Reflow(nsPresContext*           aPresContext,
+                    nsHTMLReflowMetrics&     aDesiredSize,
+                    const nsHTMLReflowState& aReflowState,
+                    nsReflowStatus&          aStatus);
+                               
+  virtual void BuildDisplayList(nsDisplayListBuilder*   aBuilder,
+                                const nsRect&           aDirtyRect,
+                                const nsDisplayListSet& aLists) MOZ_OVERRIDE;
+
+  void PaintBorderBackground(nsRenderingContext& aRenderingContext,
+    nsPoint aPt, const nsRect& aDirtyRect, uint32_t aBGFlags);
+
+  NS_IMETHOD AppendFrames(ChildListID    aListID,
+                          nsFrameList&   aFrameList);
+  NS_IMETHOD InsertFrames(ChildListID    aListID,
+                          nsIFrame*      aPrevFrame,
+                          nsFrameList&   aFrameList);
+  NS_IMETHOD RemoveFrame(ChildListID    aListID,
+                         nsIFrame*      aOldFrame);
+
+  virtual nsIAtom* GetType() const;
+  virtual bool IsFrameOfType(uint32_t aFlags) const
+  {
+    return nsContainerFrame::IsFrameOfType(aFlags &
+             ~nsIFrame::eCanContainOverflowContainers);
+  }
+  virtual nsIScrollableFrame* GetScrollTargetFrame() MOZ_OVERRIDE
+  {
+    return do_QueryFrame(GetInner());
+  }
+
+#ifdef ACCESSIBILITY  
+  virtual mozilla::a11y::AccType AccessibleType() MOZ_OVERRIDE;
+#endif
+
+#ifdef DEBUG
+  NS_IMETHOD SetInitialChildList(ChildListID    aListID,
+                                 nsFrameList&   aChildList);
+#endif
+
+#ifdef DEBUG_FRAME_DUMP
+  NS_IMETHOD GetFrameName(nsAString& aResult) const {
+    return MakeFrameName(NS_LITERAL_STRING("FieldSet"), aResult);
+  }
+#endif
+
+  
+
+
+
+
+
+  nsIFrame* GetInner() const;
+
+  
+
+
+
+
+  nsIFrame* GetLegend() const;
+
+protected:
+  nsRect    mLegendRect;
+  nscoord   mLegendSpace;
+};
+
+#endif 
