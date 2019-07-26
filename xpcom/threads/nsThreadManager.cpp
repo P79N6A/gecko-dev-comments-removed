fdf855429ddde98ebfe31ad8016b85ae2a995080
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:44:32 +0000

    bug 877850 - fix static constructors in xpcom/ r=jlebar
    UltraBlame original commit: d96df10fe4f53d3a62d31b9d602de4005b93d295

--- xpcom/threads/nsThreadManager.cpp
+++ xpcom/threads/nsThreadManager.cpp
@@ -40,10 +40,6 @@ AppendAndRemoveThread(PRThread *key, nsRefPtr<nsThread> &thread, void *arg)
 }
 
 
-
-nsThreadManager nsThreadManager::sInstance;
-
-
 NS_IMETHODIMP_(nsrefcnt) nsThreadManager::AddRef() { return 2; }
 NS_IMETHODIMP_(nsrefcnt) nsThreadManager::Release() { return 1; }
 NS_IMPL_CLASSINFO(nsThreadManager, NULL,
