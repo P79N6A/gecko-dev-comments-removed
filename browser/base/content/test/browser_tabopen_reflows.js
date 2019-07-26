03677bbc7b2f0635ef73a23fe5004a4c77099b82
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:42:52 +0000

    Bug 876374 - Eliminate uninterruptible reflow caused by calling adjustTabstrip() in addTab(); r=dao
    UltraBlame original commit: 52806760a6da1fe3ad78aee51fad5e4570044d54

---
+++ browser/base/content/test/browser_tabopen_reflows.js
@@ -0,0 +1,96 @@
+
+
+
+XPCOMUtils.defineLazyGetter(this, "docShell", () => {
+  return window.QueryInterface(Ci.nsIInterfaceRequestor)
+               .getInterface(Ci.nsIWebNavigation)
+               .QueryInterface(Ci.nsIDocShell);
+});
+
+const EXPECTED_REFLOWS = [
+  
+  "stop@chrome://global/content/bindings/browser.xml|" +
+    "addTab@chrome://browser/content/tabbrowser.xml|",
+
+  
+  "adjustTabstrip@chrome://browser/content/tabbrowser.xml|" +
+    "_handleNewTab@chrome://browser/content/tabbrowser.xml|" +
+    "onxbltransitionend@chrome://browser/content/tabbrowser.xml|",
+
+  
+  "updateCurrentBrowser@chrome://browser/content/tabbrowser.xml|" +
+    "onselect@chrome://browser/content/browser.xul|",
+
+  
+  "openLinkIn@chrome://browser/content/utilityOverlay.js|" +
+    "openUILinkIn@chrome://browser/content/utilityOverlay.js|" +
+    "BrowserOpenTab@chrome://browser/content/browser.js|",
+
+  
+  "get_scrollPosition@chrome://global/content/bindings/scrollbox.xml|" +
+    "_fillTrailingGap@chrome://browser/content/tabbrowser.xml|" +
+    "_handleNewTab@chrome://browser/content/tabbrowser.xml|" +
+    "onxbltransitionend@chrome://browser/content/tabbrowser.xml|"
+];
+
+
+
+
+
+function test() {
+  waitForExplicitFinish();
+
+  
+  docShell.addWeakReflowObserver(observer);
+  BrowserOpenTab();
+
+  
+  waitForTransitionEnd(function () {
+    
+    docShell.removeWeakReflowObserver(observer);
+    gBrowser.removeCurrentTab();
+
+    finish();
+  });
+}
+
+let observer = {
+  reflow: function (start, end) {
+    
+    let path = (new Error().stack).split("\n").slice(1).map(line => {
+      return line.replace(/:\d+$/, "");
+    }).join("|");
+
+    
+    if (path === "") {
+      return;
+    }
+
+    
+    for (let stack of EXPECTED_REFLOWS) {
+      if (path.startsWith(stack)) {
+        ok(true, "expected uninterruptible reflow '" + stack + "'");
+        return;
+      }
+    }
+
+    ok(false, "unexpected uninterruptible reflow '" + path + "'");
+  },
+
+  reflowInterruptible: function (start, end) {
+    
+  },
+
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIReflowObserver,
+                                         Ci.nsISupportsWeakReference])
+};
+
+function waitForTransitionEnd(callback) {
+  let tab = gBrowser.selectedTab;
+  tab.addEventListener("transitionend", function onEnd(event) {
+    if (event.propertyName === "max-width") {
+      tab.removeEventListener("transitionend", onEnd);
+      executeSoon(callback);
+    }
+  });
+}
