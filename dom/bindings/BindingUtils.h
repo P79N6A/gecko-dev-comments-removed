ff0b3684210052266b1628f7e84a5e354e4722db
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:12:47 +0000

    Bug 956806 part 2.  Share generic getters/setters/methods across all bindings.  r=peterv
    UltraBlame original commit: d8a6bde7629390a84a31f999ab78a6e385ec7afe

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -93,6 +93,11 @@ ThrowInvalidThis(JSContext* aCx, const JS::CallArgs& aArgs,
                  const ErrNum aErrorNumber,
                  const char* aInterfaceName);
 
+bool
+ThrowInvalidThis(JSContext* aCx, const JS::CallArgs& aArgs,
+                 const ErrNum aErrorNumber,
+                 prototypes::ID aProtoId);
+
 inline bool
 ThrowMethodFailedWithDetails(JSContext* cx, ErrorResult& rv,
                              const char* ifaceName,
@@ -202,9 +207,10 @@ IsDOMObject(JSObject* obj)
 
 
 
-template <prototypes::ID PrototypeID, class T, typename U>
+template <class T, typename U>
 MOZ_ALWAYS_INLINE nsresult
-UnwrapObject(JSObject* obj, U& value)
+UnwrapObject(JSObject* obj, U& value, prototypes::ID protoID,
+             uint32_t protoDepth)
 {
   
   const DOMClass* domClass = GetDOMClass(obj);
@@ -230,14 +236,21 @@ UnwrapObject(JSObject* obj, U& value)
   
 
 
-  if (domClass->mInterfaceChain[PrototypeTraits<PrototypeID>::Depth] ==
-      PrototypeID) {
+  if (domClass->mInterfaceChain[protoDepth] == protoID) {
     value = UnwrapDOMObject<T>(obj);
     return NS_OK;
   }
 
   
   return NS_ERROR_XPC_BAD_CONVERT_JS;
+}
+
+template <prototypes::ID PrototypeID, class T, typename U>
+MOZ_ALWAYS_INLINE nsresult
+UnwrapObject(JSObject* obj, U& value)
+{
+  return UnwrapObject<T>(obj, value, PrototypeID,
+                         PrototypeTraits<PrototypeID>::Depth);
 }
 
 inline bool
@@ -2349,6 +2362,15 @@ CreateGlobal(JSContext* aCx, T* aObject, nsWrapperCache* aCache,
 
   return global;
 }
+
+bool
+GenericBindingGetter(JSContext* cx, unsigned argc, JS::Value* vp);
+
+bool
+GenericBindingSetter(JSContext* cx, unsigned argc, JS::Value* vp);
+
+bool
+GenericBindingMethod(JSContext* cx, unsigned argc, JS::Value* vp);
 
 } 
 } 
