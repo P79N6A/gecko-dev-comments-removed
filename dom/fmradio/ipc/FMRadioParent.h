13942daa63dfe0024c56ff1805c7a696ae790f63
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:29:49 +0000

    Bug 870676: Part 2 - Implement IPDL for mozFMRadio. r=khuey
    UltraBlame original commit: d495cd4206fa4a09c4d1353b41bf102317dfe480

---
+++ dom/fmradio/ipc/FMRadioParent.h
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_fmradioparent_h__
+#define mozilla_dom_fmradioparent_h__
+
+#include "FMRadioCommon.h"
+#include "mozilla/dom/PFMRadioParent.h"
+#include "mozilla/HalTypes.h"
+
+BEGIN_FMRADIO_NAMESPACE
+
+class PFMRadioRequestParent;
+
+class FMRadioParent MOZ_FINAL : public PFMRadioParent
+                              , public FMRadioEventObserver
+{
+public:
+  FMRadioParent();
+  ~FMRadioParent();
+
+  virtual bool
+  RecvGetStatusInfo(StatusInfo* aStatusInfo) MOZ_OVERRIDE;
+
+  virtual PFMRadioRequestParent*
+  AllocPFMRadioRequestParent(const FMRadioRequestArgs& aArgs) MOZ_OVERRIDE;
+
+  virtual bool
+  DeallocPFMRadioRequestParent(PFMRadioRequestParent* aActor) MOZ_OVERRIDE;
+
+  
+  virtual void Notify(const FMRadioEventType& aType) MOZ_OVERRIDE;
+};
+
+END_FMRADIO_NAMESPACE
+
+#endif 
+
---
+++ dom/fmradio/ipc/FMRadioRequestParent.h
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_fmradiorequestparent_h__
+#define mozilla_dom_fmradiorequestparent_h__
+
+#include "FMRadioCommon.h"
+#include "mozilla/dom/PFMRadioRequestParent.h"
+#include "FMRadioService.h"
+
+BEGIN_FMRADIO_NAMESPACE
+
+class FMRadioRequestParent MOZ_FINAL : public PFMRadioRequestParent
+                                     , public ReplyRunnable
+{
+public:
+  FMRadioRequestParent();
+  ~FMRadioRequestParent();
+
+  virtual void ActorDestroy(ActorDestroyReason aWhy) MOZ_OVERRIDE;
+
+  NS_IMETHOD Run();
+
+private:
+  bool mActorDestroyed;
+};
+
+END_FMRADIO_NAMESPACE
+
+#endif 
+
