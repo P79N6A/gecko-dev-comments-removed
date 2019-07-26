8c60da431fa39c376efb7584f7b71652fe8dd0f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:25:44 +0000

    Bug 1027528 part 6 - Make asm.js PropertyName serialization handle Latin1 strings. r=luke
    UltraBlame original commit: c6db1bc87a22e63529a6f48e5071873e3dcde28b

---
+++ js/src/jit-test/tests/latin1/asm.js
@@ -0,0 +1,33 @@
+load(libdir + "asm.js");
+
+if (!isAsmJSCompilationAvailable() || !isCachingEnabled())
+    quit();
+
+
+
+
+toLatin1("funName"); 
+var body1 = "'use asm'; function funName() { return 42 } return funName";
+var m = new Function(body1);
+assertEq(isAsmJSModule(m), true);
+assertEq(m()(), 42);
+var m = new Function(body1);
+assertEq(isAsmJSModuleLoadedFromCache(m), true);
+assertEq(m()(), 42);
+
+var f = m();
+assertEq(isLatin1(f.name), true);
+assertEq(f.name, "funName");
+
+
+var body1 = "'use asm'; function funName\u1200() { return 42 } return funName\u1200";
+var m = new Function(body1);
+assertEq(isAsmJSModule(m), true);
+assertEq(m()(), 42);
+var m = new Function(body1);
+assertEq(isAsmJSModuleLoadedFromCache(m), true);
+assertEq(m()(), 42);
+
+var f = m();
+assertEq(isLatin1(f.name), false);
+assertEq(f.name, "funName\u1200");
