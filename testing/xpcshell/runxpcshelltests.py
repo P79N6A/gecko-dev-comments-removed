7ab6de48f1b0a33fc0f6d99f050a47bb06a71d4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:45:45 +0000

    Bug 707976 - Remove manifestdestiny from its old location in m-c and use that in testing/mozbase;r=ahal
    UltraBlame original commit: b7d48e8170414f79ac688de30ca5b559f525240d

--- testing/xpcshell/runxpcshelltests.py
+++ testing/xpcshell/runxpcshelltests.py
@@ -11,7 +11,6 @@ from optparse import OptionParser
 from subprocess import Popen, PIPE, STDOUT
 from tempfile import mkdtemp, gettempdir
 from threading import Timer
-import manifestparser
 import mozinfo
 import random
 import socket
@@ -28,17 +27,19 @@ HARNESS_TIMEOUT = 5 * 60
 here = os.path.dirname(__file__)
 mozbase = os.path.realpath(os.path.join(os.path.dirname(here), 'mozbase'))
 
-try:
-    import mozcrash
-except:
-    deps = ['mozcrash',
-            'mozfile',
-            'mozlog']
-    for dep in deps:
-        module = os.path.join(mozbase, dep)
-        if module not in sys.path:
-            sys.path.append(module)
-    import mozcrash
+
+modules = [('mozcrash', ['mozcrash', 'mozfile', 'mozlog']),
+           ('manifestparser', ['manifestdestiny'])]
+
+for module, deps in modules:
+    try:
+        globals()[module] = __import__(module)
+    except ImportError:
+        for dep in deps:
+            module_path = os.path.join(mozbase, dep)
+            if module_path not in sys.path:
+                sys.path.append(module_path)
+        globals()[module] = __import__(module)
 
 
 
