2ddf53cd6cb287f990c4324feede2012e2f20b8d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:48:00 +0000

    Bug 999755 - Add neuter() variants to vary data pointer, r=Waldo
    UltraBlame original commit: e9cb7cf27ce7a4d5deeeeff7c9ac9415dce31c70

--- js/src/jit-test/tests/TypedObject/atopneuteredbuffer.js
+++ js/src/jit-test/tests/TypedObject/atopneuteredbuffer.js
@@ -8,16 +8,17 @@ load(libdir + "asserts.js")
 
 var {StructType, uint32, Object, Any, storage, objectType} = TypedObject;
 
-function main() { 
+function main(variant) { 
   var Uints = uint32.array();
   var Unit = new StructType({});   
   var buffer = new ArrayBuffer(0); 
   var p = new Unit(buffer);        
-  neuter(buffer);
+  neuter(buffer, variant);
   assertThrowsInstanceOf(() => new Unit(buffer), TypeError,
                          "Able to instantiate atop neutered buffer");
   assertThrowsInstanceOf(() => new Uints(buffer, 0), TypeError,
                          "Able to instantiate atop neutered buffer");
 }
 
-main();
+main("same-data");
+main("change-data");
--- js/src/jit-test/tests/TypedObject/bug976697.js
+++ js/src/jit-test/tests/TypedObject/bug976697.js
@@ -4,10 +4,12 @@
 
 
 
-if (!this.hasOwnProperty("TypedObject"))
-  quit();
+x = ArrayBuffer();
+neuter(x, "same-data");
+Uint32Array(x);
+gc();
 
 x = ArrayBuffer();
-neuter(x);
+neuter(x, "change-data");
 Uint32Array(x);
 gc();
