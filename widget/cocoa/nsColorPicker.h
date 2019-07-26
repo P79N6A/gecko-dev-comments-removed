dcc46850148e86ac9d74f6065460008971656160
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:21:57 +0000

    Bug 875756 - Implement nsIColorPicker for Cocoa. r=mstange
    UltraBlame original commit: 119f9755938673e138446d3cdb7cde822c013407

---
+++ widget/cocoa/nsColorPicker.h
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+#ifndef nsColorPicker_h_
+#define nsColorPicker_h_
+
+#include "nsIColorPicker.h"
+#include "nsString.h"
+#include "nsCOMPtr.h"
+
+class nsIColorPickerShownCallback;
+class nsIDOMWindow;
+@class NSColorPanelWrapper;
+@class NSColor;
+
+class nsColorPicker MOZ_FINAL : public nsIColorPicker
+{
+public:
+  NS_DECL_ISUPPORTS
+
+  NS_IMETHOD Init(nsIDOMWindow* aParent, const nsAString& aTitle,
+                  const nsAString& aInitialColor);
+  NS_IMETHOD Open(nsIColorPickerShownCallback* aCallback);
+
+  
+  void Update(NSColor* aColor);
+  void Done();
+
+private:
+  static NSColor* GetNSColorFromHexString(const nsAString& aColor);
+  static void GetHexStringFromNSColor(NSColor* aColor, nsAString& aResult);
+
+  nsString             mTitle;
+  nsString             mColor;
+  NSColorPanelWrapper* mColorPanel;
+  nsCOMPtr<nsIColorPickerShownCallback> mCallback;
+};
+
+#endif 
