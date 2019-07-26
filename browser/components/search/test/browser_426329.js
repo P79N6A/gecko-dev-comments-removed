8b866d9e55496ce21d98b142807fe96ade7f358b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:49:50 +0000

    Bug 493051: avoid having addEngine select the engine by default, by adding an optional callback to let callers observe the successful addition of the engine, r=MattN
    UltraBlame original commit: 423d565f5637c208014b58bf27042e7fa8c3a472

--- browser/components/search/test/browser_426329.js
+++ browser/components/search/test/browser_426329.js
@@ -30,8 +30,7 @@ function test() {
       case "engine-added":
         var engine = ss.getEngineByName("Bug 426329");
         ok(engine, "Engine was added.");
-        
-        
+        ss.currentEngine = engine;
         break;
       case "engine-current":
         ok(ss.currentEngine.name == "Bug 426329", "currentEngine set");
