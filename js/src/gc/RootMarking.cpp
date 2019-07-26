ca80a3b7aa1e96079dab98a47558ff86865d33a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:56:28 +0000

    Bug 1008336: Move MarkIfGCThingWord() and IsAddressableGCThing() inside the #ifndef that guards their only callers. r=billm
    UltraBlame original commit: 91b7dcd04e658ed0d628fae9b23ae8dcaeab72d2

--- js/src/gc/RootMarking.cpp
+++ js/src/gc/RootMarking.cpp
@@ -112,6 +112,7 @@ enum ConservativeGCTest
 
 
 
+#ifndef JSGC_USE_EXACT_ROOTING
 static inline ConservativeGCTest
 IsAddressableGCThing(JSRuntime *rt, uintptr_t w,
                      bool skipUncollectedCompartments,
@@ -231,7 +232,6 @@ MarkIfGCThingWord(JSTracer *trc, uintptr_t w)
     return CGCT_VALID;
 }
 
-#ifndef JSGC_USE_EXACT_ROOTING
 static void
 MarkWordConservatively(JSTracer *trc, uintptr_t w)
 {
