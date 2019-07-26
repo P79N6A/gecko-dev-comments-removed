258fbdc94ef825aa229319b6b7a6286d7d29cc75
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:06:53 +0000

    Bug 849916 - Part 3: Create an engine for PannerNodes and make sure that we deliver the panner and listener parameters to it; r=padenot
    UltraBlame original commit: 10d17cb9cd1d3259d08edad93b5d8ada65f29bd4

--- content/media/webaudio/PannerNode.cpp
+++ content/media/webaudio/PannerNode.cpp
@@ -5,12 +5,110 @@
 
 
 #include "PannerNode.h"
+#include "AudioNodeEngine.h"
+#include "AudioNodeStream.h"
+#include "AudioListener.h"
 
 namespace mozilla {
 namespace dom {
 
+class PannerNodeEngine : public AudioNodeEngine
+{
+public:
+  PannerNodeEngine()
+    
+    : mPanningModel(PanningModelTypeValues::HRTF)
+    , mDistanceModel(DistanceModelTypeValues::Inverse)
+    , mPosition()
+    , mOrientation(1., 0., 0.)
+    , mVelocity()
+    , mRefDistance(1.)
+    , mMaxDistance(10000.)
+    , mRolloffFactor(1.)
+    , mConeInnerAngle(360.)
+    , mConeOuterAngle(360.)
+    , mConeOuterGain(0.)
+    
+    
+    , mListenerDopplerFactor(0.)
+    , mListenerSpeedOfSound(0.)
+  {
+  }
+
+  virtual void SetInt32Parameter(uint32_t aIndex, int32_t aParam) MOZ_OVERRIDE
+  {
+    switch (aIndex) {
+    case PannerNode::PANNING_MODEL:
+      mPanningModel = PanningModelType(aParam);
+      break;
+    case PannerNode::DISTANCE_MODEL:
+      mDistanceModel = DistanceModelType(aParam);
+      break;
+    default:
+      NS_ERROR("Bad PannerNodeEngine Int32Parameter");
+    }
+  }
+  virtual void SetThreeDPointParameter(uint32_t aIndex, const ThreeDPoint& aParam) MOZ_OVERRIDE
+  {
+    switch (aIndex) {
+    case PannerNode::LISTENER_POSITION: mListenerPosition = aParam; break;
+    case PannerNode::LISTENER_ORIENTATION: mListenerOrientation = aParam; break;
+    case PannerNode::LISTENER_UPVECTOR: mListenerUpVector = aParam; break;
+    case PannerNode::LISTENER_VELOCITY: mListenerVelocity = aParam; break;
+    case PannerNode::POSITION: mPosition = aParam; break;
+    case PannerNode::ORIENTATION: mOrientation = aParam; break;
+    case PannerNode::VELOCITY: mVelocity = aParam; break;
+    default:
+      NS_ERROR("Bad PannerNodeEngine ThreeDPointParameter");
+    }
+  }
+  virtual void SetDoubleParameter(uint32_t aIndex, double aParam) MOZ_OVERRIDE
+  {
+    switch (aIndex) {
+    case PannerNode::LISTENER_DOPPLER_FACTOR: mListenerDopplerFactor = aParam; break;
+    case PannerNode::LISTENER_SPEED_OF_SOUND: mListenerSpeedOfSound = aParam; break;
+    case PannerNode::REF_DISTANCE: mRefDistance = aParam; break;
+    case PannerNode::MAX_DISTANCE: mMaxDistance = aParam; break;
+    case PannerNode::ROLLOFF_FACTOR: mRolloffFactor = aParam; break;
+    case PannerNode::CONE_INNER_ANGLE: mConeInnerAngle = aParam; break;
+    case PannerNode::CONE_OUTER_ANGLE: mConeOuterAngle = aParam; break;
+    case PannerNode::CONE_OUTER_GAIN: mConeOuterGain = aParam; break;
+    default:
+      NS_ERROR("Bad PannerNodeEngine DoubleParameter");
+    }
+  }
+
+  virtual void ProduceAudioBlock(AudioNodeStream* aStream,
+                                 const AudioChunk& aInput,
+                                 AudioChunk* aOutput,
+                                 bool *aFinished) MOZ_OVERRIDE
+  {
+    
+    *aOutput = aInput;
+  }
+
+  PanningModelType mPanningModel;
+  DistanceModelType mDistanceModel;
+  ThreeDPoint mPosition;
+  ThreeDPoint mOrientation;
+  ThreeDPoint mVelocity;
+  double mRefDistance;
+  double mMaxDistance;
+  double mRolloffFactor;
+  double mConeInnerAngle;
+  double mConeOuterAngle;
+  double mConeOuterGain;
+  ThreeDPoint mListenerPosition;
+  ThreeDPoint mListenerOrientation;
+  ThreeDPoint mListenerUpVector;
+  ThreeDPoint mListenerVelocity;
+  double mListenerDopplerFactor;
+  double mListenerSpeedOfSound;
+};
+
 PannerNode::PannerNode(AudioContext* aContext)
   : AudioNode(aContext)
+  
   , mPanningModel(PanningModelTypeValues::HRTF)
   , mDistanceModel(DistanceModelTypeValues::Inverse)
   , mPosition()
@@ -23,6 +121,15 @@ PannerNode::PannerNode(AudioContext* aContext)
   , mConeOuterAngle(360.)
   , mConeOuterGain(0.)
 {
+  mStream = aContext->Graph()->CreateAudioNodeStream(new PannerNodeEngine());
+  
+  Context()->Listener()->RegisterPannerNode(this);
+}
+
+PannerNode::~PannerNode()
+{
+  Context()->Listener()->UnregisterPannerNode(this);
+  DestroyMediaStream();
 }
 
 JSObject*
