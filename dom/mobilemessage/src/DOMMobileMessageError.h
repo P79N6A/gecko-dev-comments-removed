5c50a631bd264646698ece1d737e7c38db583bb8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:07:22 +0000

    Bug 824717 - Part 1: Define DOMMobileMessageError:DOMError to hook an additional nsISupports object like nsIDOMMozSmsMessage/nsIDOMMozMmsMessage when error occurs. r=hsinyi, r=smaug
    UltraBlame original commit: 98cb5673fc0feb8b9733868e3a52620e054537a8

---
+++ dom/mobilemessage/src/DOMMobileMessageError.h
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_MobileMessageError_h
+#define mozilla_dom_MobileMessageError_h
+
+#include "mozilla/dom/DOMError.h"
+
+class nsIDOMMozMmsMessage;
+class nsIDOMMozSmsMessage;
+
+namespace mozilla {
+namespace dom {
+
+class OwningMozSmsMessageOrMozMmsMessage;
+
+class DOMMobileMessageError MOZ_FINAL : public DOMError
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(DOMMobileMessageError, DOMError)
+
+  DOMMobileMessageError(nsPIDOMWindow* aWindow, const nsAString& aName,
+                        nsIDOMMozSmsMessage* aSms);
+
+  DOMMobileMessageError(nsPIDOMWindow* aWindow, const nsAString& aName,
+                        nsIDOMMozMmsMessage* aMms);
+
+  virtual JSObject*
+  WrapObject(JSContext* aCx) MOZ_OVERRIDE;
+
+  void GetData(OwningMozSmsMessageOrMozMmsMessage& aRetVal) const;
+
+private:
+  nsCOMPtr<nsIDOMMozSmsMessage> mSms;
+  nsCOMPtr<nsIDOMMozMmsMessage> mMms;
+};
+
+} 
+} 
+
+#endif 
