7966c37450d08bf613c8d2b392ece8a9fa05a363
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 21:59:50 +0000

    Bug 974120: Add helpers for using Promse::MaybeResolve/MaybeReject from C++. r=bz
    Using JSAPI is tedious and error-prone.  It's much better to handle that once in the Promise code and allow callers to pass C++ objects.
    
    UltraBlame original commit: e581cbef0dd93a2d0b4f3ed4466f4a26253ef959

--- dom/promise/Promise.h
+++ dom/promise/Promise.h
@@ -9,13 +9,15 @@
 
 #include "mozilla/Attributes.h"
 #include "mozilla/ErrorResult.h"
+#include "mozilla/TypeTraits.h"
 #include "mozilla/dom/BindingDeclarations.h"
 #include "nsCycleCollectionParticipant.h"
 #include "mozilla/dom/PromiseBinding.h"
 #include "nsWrapperCache.h"
 #include "nsAutoPtr.h"
-#include "nsPIDOMWindow.h"
 #include "js/TypeDecls.h"
+
+class nsIGlobalObject;
 
 namespace mozilla {
 namespace dom {
@@ -38,12 +40,16 @@ class Promise MOZ_FINAL : public nsISupports,
   friend class WorkerPromiseTask;
   friend class WrapperPromiseCallback;
 
+  ~Promise();
+
 public:
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
   NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(Promise)
 
-  Promise(nsPIDOMWindow* aWindow);
-  ~Promise();
+  Promise(nsIGlobalObject* aGlobal);
+
+  typedef void (Promise::*MaybeFunc)(JSContext* aCx,
+                                     JS::Handle<JS::Value> aValue);
 
   void MaybeResolve(JSContext* aCx,
                     JS::Handle<JS::Value> aValue);
@@ -51,10 +57,23 @@ public:
                    JS::Handle<JS::Value> aValue);
 
   
+  
+  
+  template <typename T>
+  void MaybeResolve(T& aArg) {
+    MaybeSomething(aArg, &Promise::MaybeResolve);
+  }
 
-  nsPIDOMWindow* GetParentObject() const
+  template <typename T>
+  void MaybeReject(T& aArg) {
+    MaybeSomething(aArg, &Promise::MaybeReject);
+  }
+
+  
+
+  nsIGlobalObject* GetParentObject() const
   {
-    return mWindow;
+    return mGlobal;
   }
 
   virtual JSObject*
@@ -69,7 +88,7 @@ public:
           JS::Handle<JS::Value> aValue, ErrorResult& aRv);
 
   static already_AddRefed<Promise>
-  Resolve(nsPIDOMWindow* aWindow, JSContext* aCx,
+  Resolve(nsIGlobalObject* aGlobal, JSContext* aCx,
           JS::Handle<JS::Value> aValue, ErrorResult& aRv);
 
   static already_AddRefed<Promise>
@@ -77,7 +96,7 @@ public:
          JS::Handle<JS::Value> aValue, ErrorResult& aRv);
 
   static already_AddRefed<Promise>
-  Reject(nsPIDOMWindow* aWindow, JSContext* aCx,
+  Reject(nsIGlobalObject* aGlobal, JSContext* aCx,
          JS::Handle<JS::Value> aValue, ErrorResult& aRv);
 
   already_AddRefed<Promise>
@@ -155,6 +174,64 @@ private:
                       PromiseTaskSync aSync = AsyncTask);
 
   
+  JSObject* GetOrCreateWrapper(JSContext* aCx);
+
+  
+  
+
+  
+  bool
+  ArgumentToJSValue(const nsAString& aArgument,
+                    JSContext* aCx,
+                    JSObject* aScope,
+                    JS::MutableHandle<JS::Value> aValue);
+
+  
+  
+  template <class T>
+  typename EnableIf<IsBaseOf<nsWrapperCache, T>::value &&
+                    IsBaseOf<nsISupports, T>::value, bool>::Type
+  ArgumentToJSValue(T& aArgument,
+                    JSContext* aCx,
+                    JSObject* aScope,
+                    JS::MutableHandle<JS::Value> aValue)
+  {
+    JS::Rooted<JSObject*> scope(aCx, aScope);
+
+    return WrapNewBindingObject(aCx, scope, aArgument, aValue);
+  }
+
+  template <template <typename> class SmartPtr, typename T>
+  bool
+  ArgumentToJSValue(const SmartPtr<T>& aArgument,
+                    JSContext* aCx,
+                    JSObject* aScope,
+                    JS::MutableHandle<JS::Value> aValue)
+  {
+    return ArgumentToJSValue(*aArgument.get(), aCx, aScope, aValue);
+  }
+
+  template <typename T>
+  void MaybeSomething(T& aArgument, MaybeFunc aFunc) {
+    ThreadsafeAutoJSContext cx;
+
+    JSObject* wrapper = GetOrCreateWrapper(cx);
+    if (!wrapper) {
+      HandleException(cx);
+      return;
+    }
+
+    JSAutoCompartment ac(cx, wrapper);
+    JS::Rooted<JS::Value> val(cx);
+    if (!ArgumentToJSValue(aArgument, cx, wrapper, &val)) {
+      HandleException(cx);
+      return;
+    }
+
+    (this->*aFunc)(cx, val);
+  }
+
+  
   static bool
   JSCallback(JSContext *aCx, unsigned aArgc, JS::Value *aVp);
 
@@ -175,7 +252,7 @@ private:
 
   void HandleException(JSContext* aCx);
 
-  nsRefPtr<nsPIDOMWindow> mWindow;
+  nsRefPtr<nsIGlobalObject> mGlobal;
 
   nsTArray<nsRefPtr<PromiseCallback> > mResolveCallbacks;
   nsTArray<nsRefPtr<PromiseCallback> > mRejectCallbacks;
