94d07516caca5d55307ad5346177b80dea1fb03b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:39:23 +0000

    Bug 868816 - Remove the notifications for the displayed app when we go out of the lock screen. r=jlebar
    UltraBlame original commit: 31aeadc708443e689d6d73a73cba67f3ddaca088

---
+++ dom/browser-element/mochitest/browserElement_VisibilityChange.js
@@ -0,0 +1,43 @@
+
+
+
+
+'use strict';
+
+SimpleTest.waitForExplicitFinish();
+browserElementTestHelpers.setEnabledPref(true);
+browserElementTestHelpers.addPermission();
+
+var iframe1 = null;
+function runTest() {
+  iframe1 = document.createElement('iframe');
+  SpecialPowers.wrap(iframe1).mozbrowser = true;
+  document.body.appendChild(iframe1);
+
+  iframe1.src = 'data:text/html,<html><head><title>Title</title></head><body></body></html>';
+  checkVisibilityFalse();
+}
+
+function checkVisibilityFalse() {
+  iframe1.addEventListener('mozbrowservisibilitychange', function onvisibilitychange(e) {
+    iframe1.removeEventListener(e.type, onvisibilitychange);
+
+    is(e.detail.visible, false, 'Visibility should be false');
+    checkVisibilityTrue();
+  });
+
+  iframe1.setVisible(false);
+}
+
+function checkVisibilityTrue() {
+  iframe1.addEventListener('mozbrowservisibilitychange', function onvisibilitychange(e) {
+    iframe1.removeEventListener(e.type, onvisibilitychange);
+
+    is(e.detail.visible, true, 'Visibility should be true');
+    SimpleTest.finish();
+  });
+
+  iframe1.setVisible(true);
+}
+
+addEventListener('testready', runTest);
