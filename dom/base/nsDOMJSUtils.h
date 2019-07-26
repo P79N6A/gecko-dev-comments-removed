f2a49903341ed39b0109a3c624001f638b2a9dcc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:31:27 +0000

    Bug 868122 - Remove nsIScriptContextPrincipal. r=gabor
    \o/
    
    UltraBlame original commit: ac264325e7f62dd0c9ba220c24d4ccab016d6898

--- dom/base/nsDOMJSUtils.h
+++ dom/base/nsDOMJSUtils.h
@@ -29,22 +29,6 @@ GetScriptContextFromJSContext(JSContext *cx)
   return scx;
 }
 
-inline nsIScriptContextPrincipal*
-GetScriptContextPrincipalFromJSContext(JSContext *cx)
-{
-  if (!(::JS_GetOptions(cx) & JSOPTION_PRIVATE_IS_NSISUPPORTS)) {
-    return nullptr;
-  }
-
-  nsCOMPtr<nsIScriptContextPrincipal> scx =
-    do_QueryInterface(static_cast<nsISupports *>
-                                 (::JS_GetContextPrivate(cx)));
-
-  
-  
-  return scx;
-}
-
 
 
 
