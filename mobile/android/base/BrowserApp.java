1b125479b71e8163bb21224d32e8937f1dba482b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:44:42 +0000

    Bug 890392 - Replace the page title when sharing. r=sriram
    UltraBlame original commit: 8f1c23df15daee1d4f0809e6563eeb1f92205e98

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -1911,14 +1911,17 @@ abstract public class BrowserApp extends GeckoApp
             if (provider != null) {
                 Intent shareIntent = provider.getIntent();
 
+                
                 if (shareIntent == null) {
-                    shareIntent = GeckoAppShell.getShareIntent(this, url,
-                                                               "text/plain", tab.getDisplayTitle());
+                    shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
                     provider.setIntent(shareIntent);
-                } else {
-                    shareIntent.putExtra(Intent.EXTRA_TEXT, url);
-                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, tab.getDisplayTitle());
                 }
+
+                
+                shareIntent.putExtra(Intent.EXTRA_TEXT, url);
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, tab.getDisplayTitle());
+                shareIntent.putExtra(Intent.EXTRA_TITLE, tab.getDisplayTitle());
             }
         }
 
