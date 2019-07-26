e837c1db8e8293659b8a97d5182a3cee5a92ee34
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:18:40 +0000

    Bug 859339 - blur reftests that don't need focus; r=dbaron
    This patch opens a second window during non-remote reftests.  This second
    window is used purely as a place to place focus when tests don't need
    focus themselves.  Unfocusing the reftests windows when tests don't require
    focus means that tests that ought to have needs-focus cause failures more
    consistently.
    
    We don't use a second window for remote reftests.  Even though we
    theoretically could (on desktop platforms, for instance), our primary
    remote reftest use-cases are for Android and B2G.  And those platforms do
    not support multiple windows.
    
    UltraBlame original commit: 06c3ed54ed62ce6e61b7728552d06037f3806807

--- layout/tools/reftest/reftest.js
+++ layout/tools/reftest/reftest.js
@@ -407,7 +407,9 @@ function InitAndStartRefTests()
     }
 
     
-    gBrowser.focus();
+    if (gFocusFilterMode != FOCUS_FILTER_NON_NEEDS_FOCUS_TESTS) {
+        gBrowser.focus();
+    }
 
     StartTests();
 }
@@ -1154,6 +1156,15 @@ function Focus()
     return true;
 }
 
+function Blur()
+{
+    
+    
+    
+    
+    gContainingWindow.blur();
+}
+
 function StartCurrentTest()
 {
     gTestLog = [];
@@ -1186,6 +1197,9 @@ function StartCurrentTest()
     }
     else {
         gDumpLog("REFTEST TEST-START | " + gURLs[0].prettyPath + "\n");
+        if (!gURLs[0].needsFocus) {
+            Blur();
+        }
         var currentTest = gTotalTests - gURLs.length;
         gContainingWindow.document.title = "reftest: " + currentTest + " / " + gTotalTests +
             " (" + Math.floor(100 * (currentTest / gTotalTests)) + "%)";
