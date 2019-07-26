a2539e11862520b3a811d9d0d3c8dc3ead1b755b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:16:44 +0000

    Backed out changeset 933bf791b773 (bug 897735) for Windows test bustage. CLOSED TREE
    UltraBlame original commit: 716a6ec17ceca5d433e04a18685db82c1a75b129

--- toolkit/mozapps/extensions/test/xpcshell/test_bug406118.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_bug406118.js
@@ -3,165 +3,24 @@
 
 
 
-let addonIDs = ["test_bug393285_1@tests.mozilla.org",
-                "test_bug393285_2@tests.mozilla.org",
-                "test_bug393285_3a@tests.mozilla.org",
-                "test_bug393285_4@tests.mozilla.org"];
-
-const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
-
-const URI_EXTENSION_BLOCKLIST_DIALOG = "chrome://mozapps/content/extensions/blocklist.xul";
-
-Cu.import("resource://testing-common/httpd.js");
-var testserver = new HttpServer();
-testserver.start(-1);
-gPort = testserver.identity.primaryPort;
-
-
-mapFile("/data/test_bug393285.xml", testserver);
-
-const profileDir = gProfD.clone();
-profileDir.append("extensions");
-
-
-var WindowWatcher = {
-  openWindow: function(parent, url, name, features, arguments) {
-    
-    do_check_eq(url, URI_EXTENSION_BLOCKLIST_DIALOG);
-
-    
-    var list = arguments.wrappedJSObject.list;
-    list.forEach(function(aItem) {
-      if (!aItem.blocked)
-        aItem.disable = true;
-    });
-
-    
-    Services.obs.notifyObservers(null, "addon-blocklist-closed", null);
-
-  },
-
-  QueryInterface: function(iid) {
-    if (iid.equals(Ci.nsIWindowWatcher)
-     || iid.equals(Ci.nsISupports))
-      return this;
-
-    throw Cr.NS_ERROR_NO_INTERFACE;
-  }
-};
-
-var WindowWatcherFactory = {
-  createInstance: function createInstance(outer, iid) {
-    if (outer != null)
-      throw Cr.NS_ERROR_NO_AGGREGATION;
-    return WindowWatcher.QueryInterface(iid);
-  }
-};
-
-var registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
-registrar.registerFactory(Components.ID("{1dfeb90a-2193-45d5-9cb8-864928b2af55}"),
-                          "Fake Window Watcher",
-                          "@mozilla.org/embedcomp/window-watcher;1",
-                          WindowWatcherFactory);
-
-
-function load_blocklist(aFile, aCallback) {
-  Services.obs.addObserver(function() {
-    Services.obs.removeObserver(arguments.callee, "blocklist-updated");
-
-    do_execute_soon(aCallback);
-  }, "blocklist-updated", false);
-
-  Services.prefs.setCharPref("extensions.blocklist.url", "http://localhost:" +
-                             gPort + "/data/" + aFile);
-  var blocklist = Cc["@mozilla.org/extensions/blocklist;1"].
-                  getService(Ci.nsITimerCallback);
-  blocklist.notify(null);
-}
-
-
-function end_test() {
-  testserver.stop(do_test_finished);
-}
-
 function run_test() {
-  do_test_pending();
-
   createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9");
 
-  writeInstallRDFForExtension({
-    id: "test_bug393285_1@tests.mozilla.org",
-    name: "extension 1",
-    version: "1.0",
-    targetApplications: [{
-      id: "xpcshell@tests.mozilla.org",
-      minVersion: "1",
-      maxVersion: "3"
-    }]
-  }, profileDir);
+  
+  var blocklistFile = gProfD.clone();
+  blocklistFile.append("blocklist.xml");
+  if (blocklistFile.exists())
+    blocklistFile.remove(false);
+  var source = do_get_file("data/test_bug393285.xml");
+  source.copyTo(gProfD, "blocklist.xml");
 
+  var blocklist = Components.classes["@mozilla.org/extensions/blocklist;1"]
+                            .getService(Components.interfaces.nsIBlocklistService);
+  
+  
+  do_check_false(blocklist.isAddonBlocklisted("test_bug393285_1@tests.mozilla.org", "1", null, null));
+  do_check_true(blocklist.isAddonBlocklisted("test_bug393285_2@tests.mozilla.org", "1", null, null));
+  do_check_true(blocklist.isAddonBlocklisted("test_bug393285_3@tests.mozilla.org", "1", null, null));
+  do_check_true(blocklist.isAddonBlocklisted("test_bug393285_4@tests.mozilla.org", "1", null, null));
 
-  writeInstallRDFForExtension({
-    id: "test_bug393285_2@tests.mozilla.org",
-    name: "extension 2",
-    version: "1.0",
-    targetApplications: [{
-      id: "xpcshell@tests.mozilla.org",
-      minVersion: "1",
-      maxVersion: "3"
-    }]
-  }, profileDir);
-
-  writeInstallRDFForExtension({
-    id: "test_bug393285_3a@tests.mozilla.org",
-    name: "extension 3a",
-    version: "1.0",
-    targetApplications: [{
-      id: "xpcshell@tests.mozilla.org",
-      minVersion: "1",
-      maxVersion: "3"
-    }]
-  }, profileDir);
-
-  writeInstallRDFForExtension({
-    id: "test_bug393285_4@tests.mozilla.org",
-    name: "extension 4",
-    version: "1.0",
-    targetApplications: [{
-      id: "xpcshell@tests.mozilla.org",
-      minVersion: "1",
-      maxVersion: "3"
-    }]
-  }, profileDir);
-
-  startupManager();
-
-  AddonManager.getAddonsByIDs(addonIDs, function(addons) {
-    for (addon of addons) {
-      do_check_eq(addon.blocklistState, Ci.nsIBlocklistService.STATE_NOT_BLOCKED);
-    }
-    run_test_1();
-  });
-}
-
-function run_test_1() {
-  load_blocklist("test_bug393285.xml", function() {
-    restartManager();
-
-    var blocklist = Cc["@mozilla.org/extensions/blocklist;1"]
-                    .getService(Ci.nsIBlocklistService);
-
-    AddonManager.getAddonsByIDs(addonIDs,
-                               function([a1, a2, a3, a4]) {
-      
-      do_check_false(blocklist.isAddonBlocklisted(a1, null, null));
-
-      
-      do_check_true(blocklist.isAddonBlocklisted(a2, null, null));
-      do_check_true(blocklist.isAddonBlocklisted(a3, null, null));
-      do_check_true(blocklist.isAddonBlocklisted(a4, null, null));
-
-      end_test();
-    });
-  });
 }
