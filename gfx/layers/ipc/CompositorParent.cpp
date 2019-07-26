142f59169974aec1038215919486e1977260341d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:42:37 +0000

    Backed out changeset b032a90ee083 (bug 983489) for m-e10s test failures
    UltraBlame original commit: 28e35ed195ab75f2bdb3d899292258f01fe4912a

--- gfx/layers/ipc/CompositorParent.cpp
+++ gfx/layers/ipc/CompositorParent.cpp
@@ -241,9 +241,7 @@ CompositorParent::Destroy()
 
   
   mLayerManager = nullptr;
-  mCompositor->Destroy();
   mCompositor = nullptr;
-
   mCompositionManager = nullptr;
   mApzcTreeManager->ClearTree();
   mApzcTreeManager = nullptr;
@@ -275,6 +273,7 @@ CompositorParent::RecvWillStop()
     }
     mLayerManager->Destroy();
     mLayerManager = nullptr;
+    mCompositor = nullptr;
     mCompositionManager = nullptr;
   }
 
