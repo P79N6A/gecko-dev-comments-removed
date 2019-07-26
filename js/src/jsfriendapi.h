fe54e0b301af0c957d10075ed3987203b54fe01b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:31:42 +0000

    Bug 910220 part 1.  Add friend API for determinining whether we're in a strict-mode script.  r=waldo
    UltraBlame original commit: f6e727237de21bc57321be9d5ce8114c8ea92774

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -266,6 +266,15 @@ IsAtomsCompartment(JSCompartment *comp);
 extern JS_FRIEND_API(bool)
 ReportIfUndeclaredVarAssignment(JSContext *cx, JS::HandleString propname);
 
+
+
+
+
+
+
+extern JS_FRIEND_API(bool)
+IsInNonStrictPropertySet(JSContext *cx);
+
 struct WeakMapTracer;
 
 
