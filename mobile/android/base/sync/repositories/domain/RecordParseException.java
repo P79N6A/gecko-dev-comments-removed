91f51d1bdbcc187bea165299409722d5e32c13ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:55:00 +0000

    Bug 847178 - Avoid exceptions in CryptoRecord.fromJSONRecord. r=rnewman
    UltraBlame original commit: 632e3a94bd224ecd178f9e7f4a2249828a26087e

---
+++ mobile/android/base/sync/repositories/domain/RecordParseException.java
@@ -0,0 +1,14 @@
+
+
+
+
+package org.mozilla.gecko.sync.repositories.domain;
+
+
+public class RecordParseException extends Exception {
+  private static final long serialVersionUID = -5145494854722254491L;
+
+  public RecordParseException(String detailMessage) {
+    super(detailMessage);
+  }
+}
