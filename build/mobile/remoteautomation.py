0a91990b4897bdd711d5c8beebec7dffda2f003a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:32:50 +0000

    Bug 892118 - Suppress annoying Android x86 emu logcat message; r=jmaher
    UltraBlame original commit: bc71ea414dc803431085f49adbfd20cc80e267b9

--- build/mobile/remoteautomation.py
+++ build/mobile/remoteautomation.py
@@ -15,7 +15,8 @@ import mozcrash
 
 
 fennecLogcatFilters = [ "The character encoding of the HTML document was not declared",
-                           "Use of Mutation Events is deprecated. Use MutationObserver instead." ]
+                        "Use of Mutation Events is deprecated. Use MutationObserver instead.",
+                        "Unexpected value from nativeGetEnabledTags: 0" ]
 
 class RemoteAutomation(Automation):
     _devicemanager = None
