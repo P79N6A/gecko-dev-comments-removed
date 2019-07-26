50c00d38ba907cc08bde2238b0d9e68addf76453
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:35:46 +0000

    Bug 979134: convert test_mobile_data_state.js to Promise. r=hsinyi
    UltraBlame original commit: fb0c8c9f331a45a86e1dd868ba1bca5967c1464a

--- dom/mobileconnection/tests/marionette/test_mobile_data_state.js
+++ dom/mobileconnection/tests/marionette/test_mobile_data_state.js
@@ -1,101 +1,32 @@
 
 
 
-MARIONETTE_TIMEOUT = 30000;
-MARIONETTE_HEAD_JS = "mobile_header.js";
+MARIONETTE_TIMEOUT = 60000;
+MARIONETTE_HEAD_JS = "head.js";
 
-function setEmulatorDataState(state) {
-  emulatorHelper.sendCommand("gsm data " + state);
-}
+const INITIAL_STATES = {
+  state: "registered",
+  connected: false,
+  emergencyCallsOnly: false,
+  roaming: false,
+  signalStrength: -99,
+  relSignalStrength: 44,
 
-function waitForDataChangeEvent(callback) {
-  mobileConnection.addEventListener("datachange", function ondatachange() {
-    mobileConnection.removeEventListener("datachange", ondatachange);
-
-    if (callback && typeof callback === "function") {
-      callback();
-    }
-  });
-}
-
-
-taskHelper.push(function testInitialDataInfo() {
-  log("Test initial data connection info");
-
-  let data = mobileConnection.data;
-  
-  
-  is(data.connected, false, "check data.connected");
-  is(data.state, "registered", "check data.state");
-  is(data.emergencyCallsOnly, false, "check data.emergencyCallsOnly");
-  is(data.roaming, false, "check data.roaming");
-  
-  is(data.signalStrength, -99, "check data.signalStrength");
-  is(data.relSignalStrength, 44, "check data.relSignalStrength");
-
-  let cell = data.cell;
-  ok(data.cell, "location available");
-  
-  
-  is(cell.gsmLocationAreaCode, 65535, "check data.cell.gsmLocationAreaCode");
-  is(cell.gsmCellId, 268435455, "check data.cell.gsmCellId");
-  is(cell.cdmaBaseStationId, -1, "check data.cell.cdmaBaseStationId");
-  is(cell.cdmaBaseStationLatitude, -2147483648,
-     "check data.cell.cdmaBaseStationLatitude");
-  is(cell.cdmaBaseStationLongitude, -2147483648,
-     "check data.cell.cdmaBaseStationLongitude");
-  is(cell.cdmaSystemId, -1, "check data.cell.cdmaSystemId");
-  is(cell.cdmaNetworkId, -1, "check data.cell.cdmaNetworkId");
-
-  taskHelper.runNext();
-});
-
-
-taskHelper.push(function testDataStateUpdate() {
-  
-  function doTestDataState(state, expect, callback) {
-    log("Test data info with state='" + state + "'");
-
-    waitForDataChangeEvent(function() {
-      let data = mobileConnection.data;
-      is(data.state, expect.state, "check data.state");
-      is(data.connected, expect.connected, "check data.connected");
-      is(data.emergencyCallsOnly, expect.emergencyCallsOnly,
-         "check data.emergencyCallsOnly");
-      is(data.roaming, expect.roaming, "check data.roaming");
-      is(data.signalStrength, expect.signalStrength,
-         "check data.signalStrength");
-      is(data.relSignalStrength, expect.relSignalStrength,
-         "check data.relSignalStrength");
-
-      let cell = data.cell;
-      if (!expect.cell) {
-        ok(!cell, "check data.cell");
-      } else {
-        is(cell.gsmLocationAreaCode, expect.cell.gsmLocationAreaCode,
-           "check data.cell.gsmLocationAreaCode");
-        is(cell.gsmCellId, expect.cell.gsmCellId, "check data.cell.gsmCellId");
-        is(cell.cdmaBaseStationId, -1, "check data.cell.cdmaBaseStationId");
-        is(cell.cdmaBaseStationLatitude, -2147483648,
-           "check data.cell.cdmaBaseStationLatitude");
-        is(cell.cdmaBaseStationLongitude, -2147483648,
-           "check data.cell.cdmaBaseStationLongitude");
-        is(cell.cdmaSystemId, -1, "check data.cell.cdmaSystemId");
-        is(cell.cdmaNetworkId, -1, "check data.cell.cdmaNetworkId");
-      }
-
-      if (callback && typeof callback === "function") {
-        callback();
-      }
-    });
-
-    setEmulatorDataState(state);
+  cell: {
+    gsmLocationAreaCode: 65535,
+    gsmCellId: 268435455,
+    cdmaBaseStationId: -1,
+    cdmaBaseStationLatitude: -2147483648,
+    cdmaBaseStationLongitude: -2147483648,
+    cdmaSystemId: -1,
+    cdmaNetworkId: -1,
   }
+};
 
-  let testData = [
+const TEST_DATA = [{
     
-    {state: "unregistered",
-     expect: {
+    state: "unregistered",
+    expected: {
       state: "notSearching",
       connected: false,
       emergencyCallsOnly: true,
@@ -103,10 +34,11 @@ taskHelper.push(function testDataStateUpdate() {
       signalStrength: null,
       relSignalStrength: null,
       cell: null
-    }},
+    }
+  }, {
     
-    {state: "searching",
-     expect: {
+    state: "searching",
+    expected: {
       state: "searching",
       connected: false,
       emergencyCallsOnly: true,
@@ -114,10 +46,11 @@ taskHelper.push(function testDataStateUpdate() {
       signalStrength: null,
       relSignalStrength: null,
       cell: null
-    }},
+    }
+  }, {
     
-    {state: "denied",
-     expect: {
+    state: "denied",
+    expected: {
       state: "denied",
       connected: false,
       emergencyCallsOnly: true,
@@ -125,13 +58,14 @@ taskHelper.push(function testDataStateUpdate() {
       signalStrength: null,
       relSignalStrength: null,
       cell: null
-    }},
+    }
+  }, {
     
     
     
     
-    {state: "roaming",
-     expect: {
+    state: "roaming",
+    expected: {
       state: "registered",
       connected: false,
       emergencyCallsOnly: false,
@@ -141,10 +75,12 @@ taskHelper.push(function testDataStateUpdate() {
       cell: {
         gsmLocationAreaCode: 65535,
         gsmCellId: 268435455
-    }}},
+      }
+    }
+  }, {
     
-    {state: "home",
-     expect: {
+    state: "home",
+    expected: {
       state: "registered",
       connected: false,
       emergencyCallsOnly: false,
@@ -154,19 +90,53 @@ taskHelper.push(function testDataStateUpdate() {
       cell: {
         gsmLocationAreaCode: 65535,
         gsmCellId: 268435455
-    }}}
-  ];
+      }
+    }
+  }
+];
+
+function compareTo(aPrefix, aFrom, aTo) {
+  for (let field in aTo) {
+    let fullName = aPrefix + field;
+
+    let lhs = aFrom[field];
+    let rhs = aTo[field];
+    ok(true, "lhs=" + JSON.stringify(lhs) + ", rhs=" + JSON.stringify(rhs));
+    if (typeof rhs !== "object") {
+      is(lhs, rhs, fullName);
+    } else if (rhs) {
+      ok(lhs, fullName);
+      compareTo(fullName + ".", lhs, rhs);
+    } else {
+      is(lhs, null, fullName);
+    }
+  }
+}
+
+function verifyDataInfo(aExpected) {
+  compareTo("data.", mobileConnection.data, aExpected);
+}
+
+
+function testDataStateUpdate(aNewState, aExpected) {
+  log("Test data info with state='" + aNewState + "'");
 
   
-  (function do_call() {
-    let next = testData.shift();
-    if (!next) {
-      taskHelper.runNext();
-      return;
-    }
-    doTestDataState(next.state, next.expect, do_call);
-  })();
+  return setEmulatorVoiceDataStateAndWait("data", aNewState)
+    .then(() => verifyDataInfo(aExpected));
+}
+
+startTestCommon(function() {
+  log("Test initial data connection info");
+
+  verifyDataInfo(INITIAL_STATES);
+
+  let promise = Promise.resolve();
+  for (let i = 0; i < TEST_DATA.length; i++) {
+    let entry = TEST_DATA[i];
+    promise =
+      promise.then(testDataStateUpdate.bind(null, entry.state, entry.expected));
+  }
+
+  return promise;
 });
-
-
-taskHelper.runNext();
