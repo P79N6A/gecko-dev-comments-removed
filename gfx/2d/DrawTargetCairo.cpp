ca84720a078cd4c536065a4cf4c59868b0ca9828
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:08:24 +0000

    Bug 753237 - Implement DrawTargetCairo::Mask(). r=joe
    UltraBlame original commit: fcbe63ccf09b3ca537be1f593be3ab66ff1b6cb6

--- gfx/2d/DrawTargetCairo.cpp
+++ gfx/2d/DrawTargetCairo.cpp
@@ -659,7 +659,15 @@ DrawTargetCairo::Mask(const Pattern &aSource,
                       const DrawOptions &aOptions )
 {
   AutoPrepareForDrawing prep(this, mContext);
-  
+
+  cairo_pattern_t* source = GfxPatternToCairoPattern(aSource, aOptions.mAlpha);
+  cairo_set_source(mContext, source);
+
+  cairo_pattern_t* mask = GfxPatternToCairoPattern(aMask, aOptions.mAlpha);
+  cairo_mask(mContext, mask);
+
+  cairo_pattern_destroy(mask);
+  cairo_pattern_destroy(source);
 }
 
 void
