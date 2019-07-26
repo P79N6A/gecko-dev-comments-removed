16c5218fa2c32217c643958b75db6b9a6209e605
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:39:21 +0000

    bug 824940 - rename nsHTMLTitleElement -> mozila::dom::HTMLTitleElement r=bz
    UltraBlame original commit: 470bffebe490650e320cd5f1031869099c588af5

---
+++ content/html/content/src/HTMLTitleElement.cpp
@@ -0,0 +1,146 @@
+
+
+
+
+
+#include "mozilla/dom/HTMLTitleElement.h"
+
+#include "nsStyleConsts.h"
+#include "nsIDocument.h"
+#include "nsContentUtils.h"
+
+
+NS_IMPL_NS_NEW_HTML_ELEMENT(Title)
+
+namespace mozilla {
+namespace dom {
+
+HTMLTitleElement::HTMLTitleElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+  : nsGenericHTMLElement(aNodeInfo)
+{
+  AddMutationObserver(this);
+}
+
+HTMLTitleElement::~HTMLTitleElement()
+{
+}
+
+
+NS_IMPL_ADDREF_INHERITED(HTMLTitleElement, Element)
+NS_IMPL_RELEASE_INHERITED(HTMLTitleElement, Element)
+
+} 
+} 
+
+DOMCI_NODE_DATA(HTMLTitleElement, mozilla::dom::HTMLTitleElement)
+
+namespace mozilla {
+namespace dom {
+
+
+NS_INTERFACE_TABLE_HEAD(HTMLTitleElement)
+  NS_HTML_CONTENT_INTERFACE_TABLE2(HTMLTitleElement,
+                                   nsIDOMHTMLTitleElement,
+                                   nsIMutationObserver)
+  NS_HTML_CONTENT_INTERFACE_TABLE_TO_MAP_SEGUE(HTMLTitleElement,
+                                               nsGenericHTMLElement)
+NS_HTML_CONTENT_INTERFACE_TABLE_TAIL_CLASSINFO(HTMLTitleElement)
+
+
+NS_IMPL_ELEMENT_CLONE(HTMLTitleElement)
+
+
+NS_IMETHODIMP 
+HTMLTitleElement::GetText(nsAString& aTitle)
+{
+  nsContentUtils::GetNodeTextContent(this, false, aTitle);
+  return NS_OK;
+}
+
+NS_IMETHODIMP 
+HTMLTitleElement::SetText(const nsAString& aTitle)
+{
+  return nsContentUtils::SetNodeTextContent(this, aTitle, true);
+}
+
+void
+HTMLTitleElement::CharacterDataChanged(nsIDocument *aDocument,
+                                       nsIContent *aContent,
+                                       CharacterDataChangeInfo *aInfo)
+{
+  SendTitleChangeEvent(false);
+}
+
+void
+HTMLTitleElement::ContentAppended(nsIDocument *aDocument,
+                                  nsIContent *aContainer,
+                                  nsIContent *aFirstNewContent,
+                                  int32_t aNewIndexInContainer)
+{
+  SendTitleChangeEvent(false);
+}
+
+void
+HTMLTitleElement::ContentInserted(nsIDocument *aDocument,
+                                  nsIContent *aContainer,
+                                  nsIContent *aChild,
+                                  int32_t aIndexInContainer)
+{
+  SendTitleChangeEvent(false);
+}
+
+void
+HTMLTitleElement::ContentRemoved(nsIDocument *aDocument,
+                                 nsIContent *aContainer,
+                                 nsIContent *aChild,
+                                 int32_t aIndexInContainer,
+                                 nsIContent *aPreviousSibling)
+{
+  SendTitleChangeEvent(false);
+}
+
+nsresult
+HTMLTitleElement::BindToTree(nsIDocument *aDocument,
+                             nsIContent *aParent,
+                             nsIContent *aBindingParent,
+                             bool aCompileEventHandlers)
+{
+  
+  nsresult rv = nsGenericHTMLElement::BindToTree(aDocument, aParent,
+                                                 aBindingParent,
+                                                 aCompileEventHandlers);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  SendTitleChangeEvent(true);
+
+  return NS_OK;
+}
+
+void
+HTMLTitleElement::UnbindFromTree(bool aDeep, bool aNullParent)
+{
+  SendTitleChangeEvent(false);
+
+  
+  nsGenericHTMLElement::UnbindFromTree(aDeep, aNullParent);
+}
+
+void
+HTMLTitleElement::DoneAddingChildren(bool aHaveNotified)
+{
+  if (!aHaveNotified) {
+    SendTitleChangeEvent(false);
+  }
+}
+
+void
+HTMLTitleElement::SendTitleChangeEvent(bool aBound)
+{
+  nsIDocument* doc = GetCurrentDoc();
+  if (doc) {
+    doc->NotifyPossibleTitleChange(aBound);
+  }
+}
+
+} 
+} 
---
+++ content/html/content/src/HTMLTitleElement.h
@@ -0,0 +1,69 @@
+
+
+
+
+
+#ifndef mozilla_dom_HTMLTITLEElement_h_
+#define mozilla_dom_HTMLTITLEElement_h_
+
+#include "nsIDOMHTMLTitleElement.h"
+#include "nsGenericHTMLElement.h"
+#include "nsStubMutationObserver.h"
+
+namespace mozilla {
+namespace dom {
+
+class HTMLTitleElement : public nsGenericHTMLElement,
+                         public nsIDOMHTMLTitleElement,
+                         public nsStubMutationObserver
+{
+public:
+  using Element::GetText;
+  using Element::SetText;
+
+  HTMLTitleElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual ~HTMLTitleElement();
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+
+  
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+
+  
+  NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
+
+  
+  NS_DECL_NSIDOMHTMLTITLEELEMENT
+
+  
+  NS_DECL_NSIMUTATIONOBSERVER_CHARACTERDATACHANGED
+  NS_DECL_NSIMUTATIONOBSERVER_CONTENTAPPENDED
+  NS_DECL_NSIMUTATIONOBSERVER_CONTENTINSERTED
+  NS_DECL_NSIMUTATIONOBSERVER_CONTENTREMOVED
+
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+
+  virtual nsresult BindToTree(nsIDocument *aDocument, nsIContent *aParent,
+                              nsIContent *aBindingParent,
+                              bool aCompileEventHandlers);
+
+  virtual void UnbindFromTree(bool aDeep = true,
+                              bool aNullParent = true);
+
+  virtual void DoneAddingChildren(bool aHaveNotified);
+
+  virtual nsXPCClassInfo* GetClassInfo();
+
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+private:
+  void SendTitleChangeEvent(bool aBound);
+};
+
+} 
+} 
+
+#endif 
