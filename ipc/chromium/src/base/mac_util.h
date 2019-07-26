d66afd60194354f1ebadd7a13991fac5add5aaa2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:38:02 +0000

    Bug 913953 - Part c: Remove unused FSRef code; r=ehsan
    UltraBlame original commit: e6eadf6177a9f2185ee1f08a8060d85cd3391152

--- ipc/chromium/src/base/mac_util.h
+++ ipc/chromium/src/base/mac_util.h
@@ -5,14 +5,7 @@
 #ifndef BASE_MAC_UTIL_H_
 #define BASE_MAC_UTIL_H_
 
-struct FSRef;
-
-#include <string>
-
 namespace mac_util {
-
-std::string PathFromFSRef(const FSRef& ref);
-bool FSRefFromPath(const std::string& path, FSRef* ref);
 
 
 bool AmIBundled();
