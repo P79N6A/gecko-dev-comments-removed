4d1722f12609e340b66fea4c21682f6ee4d959e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:56:37 +0000

    Bug 947736 - Build modules/libpref/ in unified mode; r=bsmedberg
    UltraBlame original commit: 2fe4ca46eec84ddd86acaf78cd91179e06ad1b6b

--- modules/libpref/src/nsPrefBranch.h
+++ modules/libpref/src/nsPrefBranch.h
@@ -3,6 +3,9 @@
 
 
 
+#ifndef nsPrefBranch_h
+#define nsPrefBranch_h
+
 #include "nsCOMPtr.h"
 #include "nsIObserver.h"
 #include "nsIPrefBranch.h"
@@ -191,7 +194,7 @@ public:
 
   nsresult RemoveObserverFromMap(const char *aDomain, nsISupports *aObserver);
 
-  static nsresult NotifyObserver(const char *newpref, void *data);
+  static void NotifyObserver(const char *newpref, void *data);
 
   size_t SizeOfIncludingThis(mozilla::MallocSizeOf aMallocSizeOf);
 
@@ -260,3 +263,5 @@ private:
   nsCOMPtr<nsIFile> mFile;
   nsCString mRelativeToKey;
 };
+
+#endif
