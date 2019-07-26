fb9d97aedce438dcfdf555d339dfe6bb145d5bb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:32:55 +0000

    Backed out changesets 6be352955252 and 1bc459138617 (bug 770840) for Android nightly bustage (bug 870782). CLOSED TREE
    UltraBlame original commit: 2673016e7df4dc1fbad0b6ec93d8bdee917a888b

---
+++ js/xpconnect/tests/unit/test_bug608142.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+function run_test() { 
+    var tm = Components.classes["@mozilla.org/thread-manager;1"].getService();
+    var thr = tm.newThread(0);
+
+    var foundThreadError = false;
+ 
+    var listener = {
+        observe: function(message) {
+            if (/JS function on a different thread/.test(message.message))
+                foundThreadError = true;
+        }
+    };
+
+    var cs = Components.classes["@mozilla.org/consoleservice;1"].
+        getService(Components.interfaces.nsIConsoleService);
+    cs.registerListener(listener);
+
+    thr.dispatch({
+        run: function() {
+            do_check_true(false);
+        }
+    }, Components.interfaces.nsIThread.DISPATCH_NORMAL);
+
+    thr.shutdown();
+
+    cs.unregisterListener(listener);
+    do_check_true(foundThreadError);
+}
+
