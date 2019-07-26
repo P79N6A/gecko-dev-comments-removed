7393e5962bce1355fe48ef51378d0ff612be298a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:06:40 +0000

    Backed out changeset 190563720411 (bug 895465) for Linux32 jit-test failures.
    UltraBlame original commit: b6e1a6d7f62266cfa0768a5d0c9350010c335437

--- js/src/ion/Lowering.cpp
+++ js/src/ion/Lowering.cpp
@@ -520,27 +520,6 @@ ReorderComparison(JSOp op, MDefinition **lhsp, MDefinition **rhsp)
     return op;
 }
 
-static void
-ReorderCommutative(MDefinition **lhsp, MDefinition **rhsp)
-{
-    MDefinition *lhs = *lhsp;
-    MDefinition *rhs = *rhsp;
-
-    
-    
-    
-
-    if (rhs->isConstant())
-        return;
-
-    if (lhs->isConstant() ||
-        (rhs->defUseCount() == 1 && lhs->defUseCount() > 1))
-    {
-        *rhsp = lhs;
-        *lhsp = rhs;
-    }
-}
-
 bool
 LIRGenerator::visitTest(MTest *test)
 {
@@ -693,15 +672,6 @@ LIRGenerator::visitTest(MTest *test)
                 return false;
             return add(lir, comp);
         }
-    }
-
-    
-    
-    if (opd->isBitAnd() && opd->isEmittedAtUses()) {
-        MDefinition *lhs = opd->getOperand(0);
-        MDefinition *rhs = opd->getOperand(1);
-        ReorderCommutative(&lhs, &rhs);
-        return lowerForBitAndAndBranch(new LBitAndAndBranch(ifTrue, ifFalse), test, lhs, rhs);
     }
 
     if (opd->type() == MIRType_Double)
@@ -878,6 +848,27 @@ LIRGenerator::visitCompare(MCompare *comp)
     MOZ_ASSUME_UNREACHABLE("Unrecognized compare type.");
 }
 
+static void
+ReorderCommutative(MDefinition **lhsp, MDefinition **rhsp)
+{
+    MDefinition *lhs = *lhsp;
+    MDefinition *rhs = *rhsp;
+
+    
+    
+    
+
+    if (rhs->isConstant())
+        return;
+
+    if (lhs->isConstant() ||
+        (rhs->defUseCount() == 1 && lhs->defUseCount() > 1))
+    {
+        *rhsp = lhs;
+        *lhsp = rhs;
+    }
+}
+
 bool
 LIRGenerator::lowerBitOp(JSOp op, MInstruction *ins)
 {
@@ -937,33 +928,9 @@ LIRGenerator::visitBitNot(MBitNot *ins)
     return assignSafepoint(lir, ins);
 }
 
-static bool
-CanEmitBitAndAtUses(MInstruction *ins)
-{
-    if (!ins->canEmitAtUses())
-        return false;
-
-    MUseDefIterator iter(ins);
-    if (!iter)
-        return false;
-
-    if (!iter.def()->isTest())
-        return false;
-
-    iter++;
-    return !iter;
-}
-
 bool
 LIRGenerator::visitBitAnd(MBitAnd *ins)
 {
-    
-    
-    
-    
-    if (CanEmitBitAndAtUses(ins))
-        return emitAtUses(ins);
-
     return lowerBitOp(JSOP_BITAND, ins);
 }
 
