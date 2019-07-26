51c801c37d6875e11dcb5710e2f6f09eb1887e1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:15:22 +0000

    Bug 857831 - Remove WBMP content sniffer. r=jmuizelaar
    UltraBlame original commit: c6dae4b543df3a830044fc8a722db487e7b9ee20

--- image/src/imgLoader.cpp
+++ image/src/imgLoader.cpp
@@ -2041,16 +2041,6 @@ nsresult imgLoader::GetMimeTypeFromContent(const char* aContents, uint32_t aLeng
     aContentType.AssignLiteral(IMAGE_ICO);
   }
 
-#ifdef MOZ_WBMP
-  
-  
-  else if (aLength >= 2 && (static_cast<unsigned char>(aContents[0]) == 0x00 &&
-                            (static_cast<unsigned char>(aContents[1]) & 0x9F) == 0x00))
-  {
-    aContentType.AssignLiteral(IMAGE_WBMP);
-  }
-#endif
-
   else {
     
     return NS_ERROR_NOT_AVAILABLE;
