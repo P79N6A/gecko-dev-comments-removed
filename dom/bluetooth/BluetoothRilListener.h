4003af47e75269d2491bf8e7d18e7e7d27d8ea4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:06:21 +0000

    Bug 792345 - patch 1: Listened to RIL status, r=qdot
    UltraBlame original commit: 8bf5132fa24377a352c459e16b3480d354c9a744

---
+++ dom/bluetooth/BluetoothRilListener.h
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_bluetooth_bluetoothrillistener_h__
+#define mozilla_dom_bluetooth_bluetoothrillistener_h__
+
+#include "BluetoothCommon.h"
+
+#include "nsCOMPtr.h"
+#include "nsIRadioInterfaceLayer.h"
+
+BEGIN_BLUETOOTH_NAMESPACE
+
+class BluetoothRilListener
+{
+public:
+  BluetoothRilListener();
+
+  bool StartListening();
+  bool StopListening();
+
+private:
+  nsCOMPtr<nsIRILTelephonyCallback> mRILTelephonyCallback;
+};
+
+END_BLUETOOTH_NAMESPACE
+
+#endif
