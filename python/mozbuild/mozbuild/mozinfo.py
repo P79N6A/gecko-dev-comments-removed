8699bbe049e4bac4ad60fad8bc734c44ec49abe3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:16:31 +0000

    Bug 902619 - Write mozinfo.json as part of config.status; r=ted
    There are both mechanical and logical changes in this patch.
    
    The mechanical changes involve moving some files into the mozbuild
    package.
    
    The logical changes include move writing of mozinfo.json into
    config.status (from configure.in). There were some variable assignments
    being performed in configure.in. These variables were read from
    writemozinfo.py. However, these variables don't appear to be necessary!
    Now that mozinfo has full access to the underlying config.status data
    structure, it can now access these variables directly. I verified that
    every variable being assigned in configure.in had a corresponding
    AC_SUBST earlier in the file.
    
    The only variable that's a bit weird is the TOPSRCDIR and MOZCONFIG
    environment variables. mozinfo continues to look in the environment for
    MOZCONFIG. However TOPSRCDIR is now coming from config.status.
    
    As part of moving the code, I also modernized the test file, cleaned up
    some style, and removed some unused imports.
    
    UltraBlame original commit: da018ddfa0c0f9c2d2c4475386281f63563613fe

--- python/mozbuild/mozbuild/mozinfo.py
+++ python/mozbuild/mozbuild/mozinfo.py
@@ -5,25 +5,17 @@
 
 
 
-
-
-
-
-from __future__ import print_function
 import os
 import re
-import sys
 import json
 
-import buildconfig
 
-def build_dict(env=None):
+def build_dict(config, env=os.environ):
     """
     Build a dict containing data about the build configuration from
     the environment.
     """
-    substs = env or buildconfig.substs
-    env = env or os.environ
+    substs = config.substs
 
     
     required = ["TARGET_CPU", "OS_TARGET", "MOZ_WIDGET_TOOLKIT"]
@@ -33,12 +25,11 @@ def build_dict(env=None):
                         ', '.join(missing))
 
     d = {}
-    d['topsrcdir'] = substs.get('TOPSRCDIR', buildconfig.topsrcdir)
+    d['topsrcdir'] = config.topsrcdir
 
     if 'MOZCONFIG' in env:
         mozconfig = env["MOZCONFIG"]
-        if 'TOPSRCDIR' in env:
-            mozconfig = os.path.join(env["TOPSRCDIR"], mozconfig)
+        mozconfig = os.path.join(config.topsrcdir, mozconfig)
         d['mozconfig'] = os.path.normpath(mozconfig)
 
     
@@ -89,24 +80,17 @@ def build_dict(env=None):
 
     return d
 
-def write_json(file, env=None):
-    """
-    Write JSON data about the configuration specified in |env|
-    to |file|, which may be a filename or file-like object.
+
+def write_mozinfo(file, config, env=os.environ):
+    """Write JSON data about the configuration specified in config and an
+    environment variable dict to |file|, which may be a filename or file-like
+    object.
     See build_dict for information about what  environment variables are used,
     and what keys are produced.
     """
-    build_conf = build_dict(env=env)
+    build_conf = build_dict(config, env)
     if isinstance(file, basestring):
         with open(file, "w") as f:
             json.dump(build_conf, f)
     else:
         json.dump(build_conf, file)
-
-
-if __name__ == '__main__':
-    try:
-        write_json(sys.argv[1] if len(sys.argv) > 1 else sys.stdout)
-    except Exception as e:
-        print(str(e), file=sys.stderr)
-        sys.exit(1)
