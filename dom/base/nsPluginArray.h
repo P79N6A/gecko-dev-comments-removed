c7994eb231aaae83efe9a94d8eba20db2cdbdc4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:15:29 +0000

    bug 856777 - observe plugin-info-updated in nsPluginArray, not Navigator r=khuey
    UltraBlame original commit: bc4dd82e27cd96481d97f8f586f52c8542efdd34

--- dom/base/nsPluginArray.h
+++ dom/base/nsPluginArray.h
@@ -12,6 +12,7 @@
 #include "nsIPluginHost.h"
 #include "nsIURL.h"
 #include "nsWeakReference.h"
+#include "nsIObserver.h"
 
 namespace mozilla {
 namespace dom {
@@ -24,6 +25,8 @@ class nsIDocShell;
 
 
 class nsPluginArray : public nsIDOMPluginArray
+                    , public nsIObserver
+                    , public nsSupportsWeakReference
 {
 public:
   nsPluginArray(mozilla::dom::Navigator* navigator, nsIDocShell *aDocShell);
@@ -33,6 +36,14 @@ public:
 
   
   NS_DECL_NSIDOMPLUGINARRAY
+  
+  NS_DECL_NSIOBSERVER
+
+  
+  
+  
+  
+  void Init();
 
   nsresult GetPluginHost(nsIPluginHost** aPluginHost);
 
@@ -53,7 +64,7 @@ public:
     }
 #endif
 
-    return static_cast<nsPluginArray*>(aSupports);
+    return static_cast<nsPluginArray*>(static_cast<nsIDOMPluginArray*>(aSupports));
   }
 
 private:
