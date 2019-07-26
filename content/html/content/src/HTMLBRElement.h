2d4342f81b6da44ec0bb1a5ab641fba1988b6fc5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:50:40 +0000

    Bug 837520 - Part 1: Rename nsHTMLBRElement to mozilla::dom::HTMLBRElement; r=bzbarsky
    UltraBlame original commit: eef81a08397653c2fdcc902ebc870ce2c8b50444

---
+++ content/html/content/src/HTMLBRElement.h
@@ -0,0 +1,52 @@
+
+
+
+
+
+#ifndef mozilla_dom_HTMLBRElement_h
+#define mozilla_dom_HTMLBRElement_h
+
+#include "nsIDOMHTMLBRElement.h"
+#include "nsGenericHTMLElement.h"
+
+namespace mozilla {
+namespace dom {
+
+class HTMLBRElement MOZ_FINAL : public nsGenericHTMLElement,
+                                public nsIDOMHTMLBRElement
+{
+public:
+  HTMLBRElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual ~HTMLBRElement();
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+
+  
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+
+  
+  NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
+
+  
+  NS_DECL_NSIDOMHTMLBRELEMENT
+
+  virtual bool ParseAttribute(int32_t aNamespaceID,
+                                nsIAtom* aAttribute,
+                                const nsAString& aValue,
+                                nsAttrValue& aResult);
+  NS_IMETHOD_(bool) IsAttributeMapped(const nsIAtom* aAttribute) const;
+  virtual nsMapRuleToAttributesFunc GetAttributeMappingFunction() const;
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+  virtual nsXPCClassInfo* GetClassInfo();
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+};
+
+} 
+} 
+
+#endif
+
