b771a5adafbb9fcbeb23410d0bd1db51835d9493
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:17:26 +0000

    Bug 897062 - Handle special clicks in e10s. r=felipe,smaug
    UltraBlame original commit: 57b34c38a191731de4497fde68b7630d9487190e

--- browser/base/content/content.js
+++ browser/base/content/content.js
@@ -195,3 +195,97 @@ let AboutHomeListener = {
   }
 };
 AboutHomeListener.init();
+
+var global = this;
+
+let ClickEventHandler = {
+  init: function init() {
+    Cc["@mozilla.org/eventlistenerservice;1"]
+      .getService(Ci.nsIEventListenerService)
+      .addSystemEventListener(global, "click", this, true);
+  },
+
+  handleEvent: function(event) {
+    
+    
+    if (!event.isTrusted || event.defaultPrevented || event.button == 2)
+      return;
+
+    let [href, node] = this._hrefAndLinkNodeForClickEvent(event);
+
+    let json = { button: event.button, shiftKey: event.shiftKey,
+                 ctrlKey: event.ctrlKey, metaKey: event.metaKey,
+                 altKey: event.altKey, href: null, title: null,
+                 bookmark: false };
+
+    if (href) {
+      json.href = href;
+      if (node) {
+        json.title = node.getAttribute("title");
+
+        if (event.button == 0 && !event.ctrlKey && !event.shiftKey &&
+            !event.altKey && !event.metaKey) {
+          json.bookmark = node.getAttribute("rel") == "sidebar";
+          if (json.bookmark)
+            event.preventDefault(); 
+        }
+      }
+
+      sendAsyncMessage("Content:Click", json);
+      return;
+    }
+
+    
+    if (event.button == 1)
+      sendAsyncMessage("Content:Click", json);
+  },
+
+  
+
+
+
+
+
+
+
+
+
+  _hrefAndLinkNodeForClickEvent: function(event) {
+    function isHTMLLink(aNode) {
+      
+      return ((aNode instanceof content.HTMLAnchorElement && aNode.href) ||
+              (aNode instanceof content.HTMLAreaElement && aNode.href) ||
+              aNode instanceof content.HTMLLinkElement);
+    }
+
+    function makeURLAbsolute(aBase, aUrl) {
+      
+      return makeURI(aUrl, null, makeURI(aBase)).spec;
+    }
+
+    let node = event.target;
+    while (node && !isHTMLLink(node)) {
+      node = node.parentNode;
+    }
+
+    if (node)
+      return [node.href, node];
+
+    
+    let href, baseURI;
+    node = event.target;
+    while (node && !href) {
+      if (node.nodeType == content.Node.ELEMENT_NODE) {
+        href = node.getAttributeNS("http://www.w3.org/1999/xlink", "href");
+        if (href)
+          baseURI = node.baseURI;
+      }
+      node = node.parentNode;
+    }
+
+    
+    
+    return [href ? makeURLAbsolute(baseURI, href) : null, null];
+  }
+};
+ClickEventHandler.init();
---
+++ browser/modules/ContentClick.jsm
@@ -0,0 +1,82 @@
+
+
+
+
+"use strict";
+
+let Cc = Components.classes;
+let Ci = Components.interfaces;
+let Cu = Components.utils;
+
+this.EXPORTED_SYMBOLS = [ "ContentClick" ];
+
+Cu.import("resource:///modules/PlacesUIUtils.jsm");
+Cu.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+let ContentClick = {
+  init: function() {
+    let mm = Cc["@mozilla.org/globalmessagemanager;1"].getService(Ci.nsIMessageListenerManager);
+    mm.addMessageListener("Content:Click", this);
+  },
+
+  receiveMessage: function (message) {
+    switch (message.name) {
+      case "Content:Click":
+        this.contentAreaClick(message.json, message.target)
+        break;
+    }
+  },
+
+  contentAreaClick: function (json, browser) {
+    
+    
+    let window = browser.ownerDocument.defaultView;
+
+    if (!json.href) {
+      
+      if (Services.prefs.getBoolPref("middlemouse.contentLoadURL") &&
+          !Services.prefs.getBoolPref("general.autoScroll")) {
+        window.middleMousePaste(json);
+      }
+      return;
+    }
+
+    if (json.bookmark) {
+      
+      
+      
+      PlacesUIUtils.showBookmarkDialog({ action: "add"
+                                       , type: "bookmark"
+                                       , uri: Services.io.newURI(json.href, null, null)
+                                       , title: json.title
+                                       , loadBookmarkInSidebar: true
+                                       , hiddenRows: [ "description"
+                                                     , "location"
+                                                     , "keyword" ]
+                                       }, window);
+      return;
+    }
+
+    
+
+    
+    var where = window.whereToOpenLink(json);
+    if (where == "current")
+      return false;
+
+    
+
+    window.openLinkIn(json.href, where, { referrerURI: browser.documentURI,
+                                          charset: browser.characterSet });
+
+    
+    
+    
+    
+    try {
+      if (!PrivateBrowsingUtils.isWindowPrivate(window))
+        PlacesUIUtils.markPageAsFollowedLink(href);
+    } catch (ex) {  }
+  }
+};
