e490542613f418956fc88d3399c59350090bfb85
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:00:17 +0000

    Bug 891215 (part 7) - Remove FindSCCs-inl.h.  r=terrence.
    UltraBlame original commit: defc6801c431ca073e5fecf33dc00203b146a2d7

--- js/src/gc/FindSCCs.h
+++ js/src/gc/FindSCCs.h
@@ -63,20 +63,74 @@ template<class Node>
 class ComponentFinder
 {
   public:
-    ComponentFinder(uintptr_t stackLimit);
-    ~ComponentFinder();
+    ComponentFinder(uintptr_t sl)
+      : clock(1),
+        stack(NULL),
+        firstComponent(NULL),
+        cur(NULL),
+        stackLimit(sl),
+        stackFull(false)
+    {}
+
+    ~ComponentFinder() {
+        JS_ASSERT(!stack);
+        JS_ASSERT(!firstComponent);
+    }
 
     
     void useOneComponent() { stackFull = true; }
 
-    void addNode(Node *v);
-    Node *getResultsList();
+    void addNode(Node *v) {
+        if (v->gcDiscoveryTime == Undefined) {
+            JS_ASSERT(v->gcLowLink == Undefined);
+            processNode(v);
+        }
+    }
 
-    static void mergeGroups(Node *first);
+    Node *getResultsList() {
+        if (stackFull) {
+            
+
+
+
+            Node *firstGoodComponent = firstComponent;
+            for (Node *v = stack; v; v = stack) {
+                stack = v->gcNextGraphNode;
+                v->gcNextGraphComponent = firstGoodComponent;
+                v->gcNextGraphNode = firstComponent;
+                firstComponent = v;
+            }
+            stackFull = false;
+        }
+
+        JS_ASSERT(!stack);
+
+        Node *result = firstComponent;
+        firstComponent = NULL;
+
+        for (Node *v = result; v; v = v->gcNextGraphNode) {
+            v->gcDiscoveryTime = Undefined;
+            v->gcLowLink = Undefined;
+        }
+
+        return result;
+    }
+
+    static void mergeGroups(Node *first) {
+        for (Node *v = first; v; v = v->gcNextGraphNode)
+            v->gcNextGraphComponent = NULL;
+    }
 
   public:
     
-    void addEdgeTo(Node *w);
+    void addEdgeTo(Node *w) {
+        if (w->gcDiscoveryTime == Undefined) {
+            processNode(w);
+            cur->gcLowLink = Min(cur->gcLowLink, w->gcLowLink);
+        } else if (w->gcDiscoveryTime != Finished) {
+            cur->gcLowLink = Min(cur->gcLowLink, w->gcDiscoveryTime);
+        }
+    }
 
   private:
     
@@ -85,7 +139,54 @@ class ComponentFinder
     
     static const unsigned Finished = (unsigned)-1;
 
-    void processNode(Node *v);
+    void processNode(Node *v) {
+        v->gcDiscoveryTime = clock;
+        v->gcLowLink = clock;
+        ++clock;
+
+        v->gcNextGraphNode = stack;
+        stack = v;
+
+        int stackDummy;
+        if (stackFull || !JS_CHECK_STACK_SIZE(stackLimit, &stackDummy)) {
+            stackFull = true;
+            return;
+        }
+
+        Node *old = cur;
+        cur = v;
+        cur->findOutgoingEdges(*this);
+        cur = old;
+
+        if (stackFull)
+            return;
+
+        if (v->gcLowLink == v->gcDiscoveryTime) {
+            Node *nextComponent = firstComponent;
+            Node *w;
+            do {
+                JS_ASSERT(stack);
+                w = stack;
+                stack = w->gcNextGraphNode;
+
+                
+
+
+
+                w->gcDiscoveryTime = Finished;
+
+                
+                w->gcNextGraphComponent = nextComponent;
+
+                
+
+
+
+                w->gcNextGraphNode = firstComponent;
+                firstComponent = w;
+            } while (w != v);
+        }
+    }
 
   private:
     unsigned       clock;
