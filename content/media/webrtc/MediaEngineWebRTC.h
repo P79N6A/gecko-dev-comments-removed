eeebcce189275af4934ee4649eb5a0b2956a6c23
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:58:46 +0000

    Bug 843929 - Part 1: Remove no-longer-used private variables mTrackID & mLastEndTime from MediaEngineWebRTCVideoSource. r=jesup
    UltraBlame original commit: 6f8bb7c41d95c344293bb8a637ad6d5103ee2b5a

--- content/media/webrtc/MediaEngineWebRTC.h
+++ content/media/webrtc/MediaEngineWebRTC.h
@@ -74,7 +74,6 @@ public:
     , mCapabilityChosen(false)
     , mWidth(aWidth)
     , mHeight(aHeight)
-    , mLastEndTime(0)
     , mMonitor("WebRTCCamera.Monitor")
     , mFps(aFps)
     , mMinFps(aMinFps)
@@ -144,8 +143,6 @@ private:
   int mCaptureIndex;
   bool mCapabilityChosen;
   int mWidth, mHeight;
-  TrackID mTrackID;
-  TrackTicks mLastEndTime;
 
   
   
