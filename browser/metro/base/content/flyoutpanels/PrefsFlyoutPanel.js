d1f4b33f16cddbf4ba25e7b3e6f2a40e4b3c1582
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:15:41 +0000

    bug 897121. Move handling of DNT pref to prefsFlyout.js. Correctly (hopefully :) deal with the various pref values that are possible. r=rsilveira
    UltraBlame original commit: 959c593c6bd31343b620e6e37ab5860aff585cc1

--- browser/metro/base/content/flyoutpanels/PrefsFlyoutPanel.js
+++ browser/metro/base/content/flyoutpanels/PrefsFlyoutPanel.js
@@ -22,8 +22,6 @@ let PrefsFlyoutPanel = {
     this._elements = {};
     [
       ['PrefsFlyoutPanel',  'prefs-flyoutpanel'],
-      ['dntNoPref',    'prefs-dnt-nopref'],
-      ['telemetryPref','prefs-telemetry'],
     ].forEach(function(aElement) {
       let [name, id] = aElement;
       XPCOMUtils.defineLazyGetter(self._elements, name, function() {
@@ -31,6 +29,7 @@ let PrefsFlyoutPanel = {
       });
     });
 
+    this._prefObserver(null, null, "privacy.donottrackheader.value");
     this._topmostElement = this._elements.PrefsFlyoutPanel;
   },
 
@@ -38,19 +37,50 @@ let PrefsFlyoutPanel = {
     if (!this._hasShown) {
       SanitizeUI.init();
       this._hasShown = true;
+
+      Services.prefs.addObserver("privacy.donottrackheader.value",
+                                 this._prefObserver,
+                                 false);
+      Services.prefs.addObserver("privacy.donottrackheader.enabled",
+                                 this._prefObserver,
+                                 false);
     }
 
-    this._elements.PrefsFlyoutPanel.show();
+    this._topmostElement.show();
   },
 
-  onDNTPreferenceChanged: function onDNTPreferenceChanged() {
-    let selected = this._elements.dntNoPref.selected;
+  _prefObserver: function(subject, topic, data) {
+    let value = -1;
+    try {
+      value = Services.prefs.getIntPref("privacy.donottrackheader.value");
+    } catch(e) {
+    }
 
-    
-    Services.prefs.setBoolPref("privacy.donottrackheader.enabled", !selected);
+    let isEnabled = Services.prefs.getBoolPref("privacy.donottrackheader.enabled");
+
+    switch (data) {
+      case "privacy.donottrackheader.value":
+        
+        
+        
+        if (((1 == value) || (0 == value)) && !isEnabled) {
+          Services.prefs.setBoolPref('privacy.donottrackheader.enabled', true);
+        }
+
+        
+        
+        if (((1 != value) && (0 != value)) && isEnabled) {
+          Services.prefs.setBoolPref('privacy.donottrackheader.enabled', false);
+        }
+        break;
+
+      case "privacy.donottrackheader.enabled":
+        
+        
+        if (((1 == value) || (0 == value)) && !isEnabled) {
+          Services.prefs.setIntPref('privacy.donottrackheader.value', -1);
+        }
+        break;
+    }
   },
-
-  onTelemetryPreferenceChanged: function onTelemetryPreferenceChanged(aBool) {
-    Services.prefs.setBoolPref("toolkit.telemetry.enabled", aBool);
-  }
 };
