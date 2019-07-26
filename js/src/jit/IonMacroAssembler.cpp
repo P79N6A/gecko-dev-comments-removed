e95f7ff7885be00ec3b24a763fdf596bf7ebedb9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:58:45 +0000

    Bug 969012 - Fix an octane-raytrace regression introduced by the previous patch; r=jandem
    UltraBlame original commit: 18f592b84cd661495c8d58710d220c06fb2ce85e

--- js/src/jit/IonMacroAssembler.cpp
+++ js/src/jit/IonMacroAssembler.cpp
@@ -750,7 +750,7 @@ MacroAssembler::initGCSlots(Register obj, Register slots, JSObject *templateObj)
     if (nslots == 0)
         return;
 
-    uint32_t nfixed = templateObj->numFixedSlots();
+    uint32_t nfixed = Min(templateObj->numFixedSlots(), nslots);
     uint32_t ndynamic = templateObj->numDynamicSlots();
 
     
