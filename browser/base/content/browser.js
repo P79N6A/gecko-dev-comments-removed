cb5ceb57174db9ca5692cf1284a357894b1eec8d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:10:35 +0000

    Bug 898192 - Skip SocialMark and SocialShare code in onLocationChange when SocialUI is disabled. f=mhammond,r=jaws
    UltraBlame original commit: 91c8657b0fa98c32d036b9ab52a34c85588e1aff

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -3941,8 +3941,10 @@ var XULBrowserWindow = {
 
         
         BookmarkingUI.updateStarState();
-        SocialMark.updateMarkState();
-        SocialShare.update();
+        if (SocialUI.enabled) {
+          SocialMark.updateMarkState();
+          SocialShare.update();
+        }
       }
 
       
