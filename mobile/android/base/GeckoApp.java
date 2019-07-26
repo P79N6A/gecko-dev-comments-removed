5f77b4bb40a710cf2f983a4ba95b5c4c4a289ba4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:53:13 +0000

    Bug 839359 - Crash -- java.lang.NullPointerException, r=kats
    UltraBlame original commit: cee6c0592d6ea61df28e80773a1cc8bd61a84c5a

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -523,7 +523,7 @@ abstract public class GeckoApp
     @Override
     public boolean onMenuOpened(int featureId, Menu menu) {
         
-        if (mLayerView.isFullScreen()) {
+        if (mLayerView != null && mLayerView.isFullScreen()) {
             GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("FullScreen:Exit", null));
         }
 
