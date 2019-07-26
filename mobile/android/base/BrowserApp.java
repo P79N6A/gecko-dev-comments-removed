208128cca1e60c9aa0728cfc4fb3dec82a4df779
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:24:11 +0000

    Bug 887268 - Open items from "tabs from last time" on a new tabs (r=bnicholson)
    UltraBlame original commit: c5d17b84a826acca8f11c51a0cc0930095615a16

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -84,6 +84,7 @@ abstract public class BrowserApp extends GeckoApp
                                             GeckoLayerClient.OnMetricsChangedListener,
                                             BrowserSearch.OnSearchListener,
                                             BrowserSearch.OnEditSuggestionListener,
+                                            HomePager.OnNewTabsListener,
                                             HomePager.OnUrlOpenListener {
     private static final String LOGTAG = "GeckoBrowserApp";
 
@@ -1965,10 +1966,18 @@ abstract public class BrowserApp extends GeckoApp
 
     
     @Override
+    public void onNewTabs(String[] urls) {
+        for (String url : urls) {
+            openUrl(url, EditingTarget.NEW_TAB);
+        }
+    }
+
+    
+    @Override
     public void onUrlOpen(String url) {
         openUrl(url);
     }
-
+ 
     
     @Override
     public void onSearch(String engineId, String text) {
