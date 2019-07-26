e42d53045e6bfeb417cb6e4ba6421ed6911dca50
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:03:46 +0000

    Bug 920196 - Make QueryInterface opt-in for WebIDL interfaces. r=bz.
    UltraBlame original commit: 6ea361ff5333ea0f1cb7ebbf712b1a419ca63e6b

--- dom/bindings/Configuration.py
+++ dom/bindings/Configuration.py
@@ -356,8 +356,6 @@ class Descriptor(DescriptorProvider):
                                 (self.interface.identifier.name, self.nativeOwnership))
         self.customTrace = desc.get('customTrace', self.workers)
         self.customFinalize = desc.get('customFinalize', self.workers)
-        if desc.get('wantsQI', None) != None:
-            self._wantsQI = desc.get('wantsQI', None)
         self.wrapperCache = (not self.interface.isCallback() and
                              (self.nativeOwnership == 'worker' or
                               (self.nativeOwnership != 'owned' and
@@ -476,26 +474,6 @@ class Descriptor(DescriptorProvider):
             self.interface.hasInterfacePrototypeObject() or
             any((m.isAttr() or m.isMethod()) and m.isStatic() for m
                 in self.interface.members))
-
-    def wantsQI(self):
-        
-        if hasattr(self, '_wantsQI'):
-            return self._wantsQI
-
-        
-        
-        
-        def allAncestorsAbstract(iface):
-            if not iface.parent:
-                return True
-            desc = self.getDescriptor(iface.parent.identifier.name)
-            if desc.concrete:
-                return False
-            return allAncestorsAbstract(iface.parent)
-        return (not self.workers and
-                self.interface.hasInterfacePrototypeObject() and
-                self.concrete and
-                allAncestorsAbstract(self.interface))
 
 
 def getTypesFromDescriptor(descriptor):
