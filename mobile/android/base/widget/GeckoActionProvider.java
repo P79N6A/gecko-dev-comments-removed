2b27c330bc7ccfc3daee5a066fb0a4a9f3055f51
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:03:32 +0000

    Bug 1009560 - Download images to share on a background thread. r=mcomella
    UltraBlame original commit: 501b00809f1022db09e6b8624ab628a48cc95b79

--- mobile/android/base/widget/GeckoActionProvider.java
+++ mobile/android/base/widget/GeckoActionProvider.java
@@ -9,6 +9,7 @@ import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.Telemetry;
 import org.mozilla.gecko.TelemetryContract;
 import org.mozilla.gecko.menu.MenuItemActionView;
+import org.mozilla.gecko.util.ThreadUtils;
 
 import android.content.Context;
 import android.content.Intent;
@@ -182,17 +183,23 @@ public class GeckoActionProvider {
     private class Callbacks implements OnMenuItemClickListener,
                                        OnClickListener {
         private void chooseActivity(int index) { 
-            ActivityChooserModel dataModel = ActivityChooserModel.get(mContext, mHistoryFileName);
-            Intent launchIntent = dataModel.chooseActivity(index);
+            final ActivityChooserModel dataModel = ActivityChooserModel.get(mContext, mHistoryFileName);
+            final Intent launchIntent = dataModel.chooseActivity(index);
             if (launchIntent != null) {
                 
-                String type = launchIntent.getType();
-                if (Intent.ACTION_SEND.equals(launchIntent.getAction()) && type != null && type.startsWith("image/")) {
-                    GeckoAppShell.downloadImageForIntent(launchIntent);
-                }
+                ThreadUtils.postToBackgroundThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        
+                        String type = launchIntent.getType();
+                        if (Intent.ACTION_SEND.equals(launchIntent.getAction()) && type != null && type.startsWith("image/")) {
+                            GeckoAppShell.downloadImageForIntent(launchIntent);
+                        }
 
-                launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-                mContext.startActivity(launchIntent);
+                        launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+                        mContext.startActivity(launchIntent);
+                    }
+                });
             }
 
             if (mOnTargetListener != null) {
