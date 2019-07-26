2291eda351b274df836fc474b7f87a2e993c90cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:32:23 +0000

    Bug 870167 - Create a mini test framework for Web Audio; r=roc
    UltraBlame original commit: 5d965d4738971517722400c7db36aa87b82d048c

--- content/media/webaudio/test/webaudio.js
+++ content/media/webaudio/test/webaudio.js
@@ -56,3 +56,92 @@ function compareBuffers(buf1, buf2,
      " with source offset " + sourceOffset + " and desitnation offset " +
      destOffset);
 }
+
+function getEmptyBuffer(context, length) {
+  return context.createBuffer(gTest.numberOfChannels, length, context.sampleRate);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function runTest()
+{
+  function done() {
+    SpecialPowers.clearUserPref("media.webaudio.enabled");
+    SimpleTest.finish();
+  }
+
+  SimpleTest.waitForExplicitFinish();
+  addLoadEvent(function() {
+    SpecialPowers.setBoolPref("media.webaudio.enabled", true);
+
+    if (!gTest.numberOfChannels) {
+      gTest.numberOfChannels = 2; 
+    }
+
+    var context = new AudioContext();
+    if (!gTest.createExpectedBuffers) {
+      
+      var expectedBuffers = getEmptyBuffer(context, gTest.length);
+    } else {
+      var expectedBuffers = gTest.createExpectedBuffers(context);
+    }
+    if (!(expectedBuffers instanceof Array)) {
+      expectedBuffers = [expectedBuffers];
+    }
+    var expectedFrames = 0;
+    for (var i = 0; i < expectedBuffers.length; ++i) {
+      is(expectedBuffers[i].numberOfChannels, gTest.numberOfChannels,
+         "Correct number of channels for expected buffer " + i);
+      expectedFrames += expectedBuffers[i].length;
+    }
+    is(expectedFrames, gTest.length, "Correct number of expected frames");
+
+    if (gTest.createGraphAsync) {
+      gTest.createGraphAsync(context, function(nodeToInspect) {
+        testOutput(nodeToInspect);
+      });
+    } else {
+      testOutput(gTest.createGraph(context));
+    }
+
+    function testOutput(nodeToInspect) {
+      var sp = context.createScriptProcessor(expectedBuffers[0].length, gTest.numberOfChannels);
+      nodeToInspect.connect(sp);
+      sp.connect(context.destination);
+      sp.onaudioprocess = function(e) {
+        var expectedBuffer = expectedBuffers.shift();
+        is(e.inputBuffer.numberOfChannels, expectedBuffer.numberOfChannels,
+           "Correct number of input buffer channels");
+        for (var i = 0; i < e.inputBuffer.numberOfChannels; ++i) {
+          compareBuffers(e.inputBuffer.getChannelData(i), expectedBuffer.getChannelData(i));
+        }
+        if (expectedBuffers.length == 0) {
+          sp.onaudioprocess = null;
+          done();
+        }
+      };
+    }
+  });
+}
