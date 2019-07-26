2740bdc8c3c8808240a077d398063e8563c87664
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:09:12 +0000

    Revert "Bug 797496 - Ignore hidden PC even with try notes. r=luke"
    This reverts changeset 3b7a134bc3aa.
    
    UltraBlame original commit: 66b4f2ea351d297c57e424ba6c5dfc03f19807d2

--- js/src/jit-test/tests/basic/bug797496.js
+++
@@ -1,8 +0,0 @@
-
-var set = Set(['a']);
-var n = 5;
-for (let v of set) {
-    if (n === 0)
-        break;
-    let g = set(Set(0xffffffff, n), 1);
-}
--- js/src/jsopcode.cpp
+++ js/src/jsopcode.cpp
@@ -6436,27 +6436,31 @@ ReconstructPCStack(JSContext *cx, JSScript *script, jsbytecode *target, jsbyteco
                 continue;
             }
 
+            if (!script->hasTrynotes()) {
+                
+                hpcdepth = unsigned(-1);
+                continue;
+            }
+
             
 
 
 
-            if (script->hasTrynotes()) {
-                JSTryNote *tn = script->trynotes()->vector;
-                JSTryNote *tnEnd = tn + script->trynotes()->length;
-                for (; tn != tnEnd; tn++) {
-                    jsbytecode *start = script->main() + tn->start;
-                    jsbytecode *end = start + tn->length;
-                    if (start < pc && pc <= end && end <= target)
-                        break;
-                }
+            JSTryNote *tn = script->trynotes()->vector;
+            JSTryNote *tnEnd = tn + script->trynotes()->length;
+            for (; tn != tnEnd; tn++) {
+                jsbytecode *start = script->main() + tn->start;
+                jsbytecode *end = start + tn->length;
+                if (start < pc && pc <= end && end <= target)
+                    break;
+            }
 
-                if (tn != tnEnd) {
-                    pcdepth = tn->stackDepth;
-                    hpcdepth = unsigned(-1);
-                    oplen = 0;
-                    pc = script->main() + tn->start + tn->length;
-                    continue;
-                }
+            if (tn != tnEnd) {
+                pcdepth = tn->stackDepth;
+                hpcdepth = unsigned(-1);
+                oplen = 0;
+                pc = script->main() + tn->start + tn->length;
+                continue;
             }
 
             
@@ -6465,10 +6469,6 @@ ReconstructPCStack(JSContext *cx, JSScript *script, jsbytecode *target, jsbyteco
 
             if (JSOp(*(pc + oplen)) == JSOP_THROWING)
                 hpcdepth = pcdepth + 2;
-            else
-                
-                hpcdepth = unsigned(-1);
-
             continue;
         }
 
