61427f64c061c1bf598658219411521a8ba86928
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:55:43 +0000

    Bug 941357 - (Part 1) Consolidate per-profile content provider logic in PerProfileDatabases, starting with TabsProvider. r=rnewman
    UltraBlame original commit: f120be7494106b413fd51f1d177aaa21d6e0bce3

---
+++ mobile/android/base/db/PerProfileDatabases.java
@@ -0,0 +1,69 @@
+
+
+
+
+package org.mozilla.gecko.db;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.mozilla.gecko.GeckoProfile;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.text.TextUtils;
+
+
+
+
+public class PerProfileDatabases<T extends SQLiteOpenHelper> {
+
+    private final HashMap<String, T> mStorages = new HashMap<String, T>();
+
+    private final Context mContext;
+    private final String mDatabaseName;
+    private final DatabaseHelperFactory<T> mHelperFactory;
+
+    public interface DatabaseHelperFactory<T> {
+        public T makeDatabaseHelper(Context context, String databasePath);
+    }
+
+    public PerProfileDatabases(final Context context, final String databaseName, final DatabaseHelperFactory<T> helperFactory) {
+        mContext = context;
+        mDatabaseName = databaseName;
+        mHelperFactory = helperFactory;
+    }
+
+    public String getDatabasePathForProfile(String profile) {
+        final File profileDir = GeckoProfile.get(mContext, profile).getDir();
+        if (profileDir == null) {
+            return null;
+        }
+
+        return new File(profileDir, mDatabaseName).getAbsolutePath();
+    }
+
+    public T getDatabaseHelperForProfile(String profile) {
+        
+        if (TextUtils.isEmpty(profile)) {
+            profile = GeckoProfile.get(mContext).getName();
+        }
+
+        synchronized (this) {
+            if (mStorages.containsKey(profile)) {
+                return mStorages.get(profile);
+            }
+
+            final String databasePath = getDatabasePathForProfile(profile);
+            if (databasePath == null) {
+                throw new IllegalStateException("Database path is null for profile: " + profile);
+            }
+
+            final T helper = mHelperFactory.makeDatabaseHelper(mContext, databasePath);
+            DBUtils.ensureDatabaseIsNotLocked(helper, databasePath);
+
+            mStorages.put(profile, helper);
+            return helper;
+        }
+    }
+}
