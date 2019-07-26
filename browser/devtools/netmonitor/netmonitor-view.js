c3cf42a717c8b28f401bf465b995b3b5f213a3f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:33:20 +0000

    Bug 866623 - Firefox spoils cyrilic text in the Web Developer Console, r=rcampbell
    UltraBlame original commit: ba062746f3e7ee7b8f5fa7e2c36f2f8962f45a7f

--- browser/devtools/netmonitor/netmonitor-view.js
+++ browser/devtools/netmonitor/netmonitor-view.js
@@ -1252,7 +1252,7 @@ create({ constructor: NetworkDetailsView, proto: MenuContainer.prototype }, {
         $("#response-content-textarea-box").hidden = false;
         NetMonitorView.editor("#response-content-textarea").then((aEditor) => {
           aEditor.setMode(SourceEditor.MODES.TEXT);
-          aEditor.setText(aString);
+          aEditor.setText(NetworkHelper.convertToUnicode(aString, "UTF-8"));
 
           
           for (let key in CONTENT_MIME_TYPE_MAPPINGS) {
---
+++ browser/devtools/netmonitor/test/browser_net_cyrillic.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+function test() {
+  initNetMonitor(CYRILLIC_URL).then(([aTab, aDebuggee, aMonitor]) => {
+    info("Starting test... ");
+
+    let { document, L10N, SourceEditor, NetMonitorView } = aMonitor.panelWin;
+    let { RequestsMenu } = NetMonitorView;
+
+    RequestsMenu.lazyUpdate = false;
+
+    waitForNetworkEvents(aMonitor, 1).then(() => {
+      verifyRequestItemTarget(RequestsMenu.getItemAtIndex(0),
+        "GET", CONTENT_TYPE_SJS + "?fmt=txt", {
+          status: 200,
+          statusText: "DA DA DA"
+        });
+
+      EventUtils.sendMouseEvent({ type: "mousedown" },
+        document.getElementById("details-pane-toggle"));
+      EventUtils.sendMouseEvent({ type: "mousedown" },
+        document.querySelectorAll("#details-pane tab")[3]);
+
+      NetMonitorView.editor("#response-content-textarea").then((aEditor) => {
+        is(aEditor.getText().indexOf("\u044F"), 26, 
+          "The text shown in the source editor is incorrect.");
+        is(aEditor.getMode(), SourceEditor.MODES.TEXT,
+          "The mode active in the source editor is incorrect.");
+
+        teardown(aMonitor).then(finish);
+      });
+    });
+
+    aDebuggee.performRequests();
+  });
+}
