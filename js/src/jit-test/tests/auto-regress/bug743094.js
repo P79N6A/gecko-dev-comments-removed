8a3b406b2f2a601ed4e809063cf2807e8afed43e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:14:00 +0000

    Backed out changeset b45bdd149e34 (bug 867856) for jit-test failures on a CLOSED TREE.
    UltraBlame original commit: 4a2c34a667f7fb187ed0ff668a7821ac7429e329

--- js/src/jit-test/tests/auto-regress/bug743094.js
+++ js/src/jit-test/tests/auto-regress/bug743094.js
@@ -2,14 +2,10 @@
 
 
 
-var i = 0;
 
-gczeal(2);
+
+gczeal(2)
 function test() {
-  if (i++ > 10000)
-    return "function";
-  var res = typeof (new test("1")) != 'function';
-  return res ? "function" : "string";
+  typeof (new test("1")) != 'function'
 }
-
 test();
