941bfe0f6683cf98df5de0e16e2cecaf4037e857
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:12:51 +0000

    Bug 857579 - Monitor ICGetIntrinsic_Fallback for when bailing Ion->Baseline during eager compilation. (r=djvj)
    UltraBlame original commit: 9cbf2e118de6437a9bbc4fcd7a1ccdc78cd57268

---
+++ js/src/jit-test/tests/baseline/bug857579.js
@@ -0,0 +1,18 @@
+
+
+function testMonitorIntrinsic() {
+  var N = 2;
+  
+  var p = new ParallelArray([N,N], function () 0);
+  
+  for (var i = 0; i < N+1; i++) {
+    for (var j = 0; j < 2; j++) {
+      
+      
+      
+      p.get(i).get(j);
+    }
+  }
+}
+
+testMonitorIntrinsic();
