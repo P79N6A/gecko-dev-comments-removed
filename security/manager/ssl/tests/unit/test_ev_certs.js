52395600f2ffa4115007c6e8dcfe70ff6cb70750
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:17:07 +0000

    Bug 923304, Part 2: Adjust EV tests, r=keeler
    UltraBlame original commit: 7de70e5c5bb9ca53000a5bd8abab91feff048f8e

--- security/manager/ssl/tests/unit/test_ev_certs.js
+++ security/manager/ssl/tests/unit/test_ev_certs.js
@@ -5,6 +5,9 @@
 
 "use strict";
 
+
+
+
 do_get_profile(); 
 const certdb = Cc["@mozilla.org/security/x509certdb;1"]
                  .getService(Ci.nsIX509CertDB);
@@ -31,16 +34,28 @@ function load_ca(ca_name) {
   addCertFromFile(certdb, "test_ev_certs/" + ca_filename, 'CTu,CTu,CTu');
 }
 
-var gHttpServer;
-var gOCSPResponseCounter = 0;
+const SERVER_PORT = 8888;
 
-function start_ocsp_responder() {
-  const SERVER_PORT = 8888;
-  gHttpServer = new HttpServer();
-  gHttpServer.registerPrefixHandler("/",
+function failingOCSPResponder() {
+  let httpServer = new HttpServer();
+  httpServer.registerPrefixHandler("/", function(request, response) {
+    do_check_true(false);
+  });
+  httpServer.start(SERVER_PORT);
+  return httpServer;
+}
+
+function start_ocsp_responder(expectedCertNames) {
+  let httpServer = new HttpServer();
+  httpServer.registerPrefixHandler("/",
       function handleServerCallback(aRequest, aResponse) {
         do_check_neq(aRequest.host, "crl.example.com"); 
         let cert_nick = aRequest.path.slice(1, aRequest.path.length - 1);
+
+        do_check_true(expectedCertNames.length >= 1);
+        let expected_nick = expectedCertNames.shift();
+        do_check_eq(cert_nick, expected_nick);
+
         do_print("Generating ocsp response for '" + cert_nick + "'");
         aResponse.setStatusLine(aRequest.httpVersion, 200, "OK");
         aResponse.setHeader("Content-Type", "application/ocsp-response");
@@ -54,11 +69,16 @@ function start_ocsp_responder() {
         let retArray = generateOCSPResponses(arg_array, "test_ev_certs");
         let responseBody = retArray[0];
         aResponse.bodyOutputStream.write(responseBody, responseBody.length);
-        gOCSPResponseCounter++;
       });
-  gHttpServer.identity.setPrimary("http", "www.example.com", SERVER_PORT);
-  gHttpServer.identity.add("http", "crl.example.com", SERVER_PORT);
-  gHttpServer.start(SERVER_PORT);
+  httpServer.identity.setPrimary("http", "www.example.com", SERVER_PORT);
+  httpServer.identity.add("http", "crl.example.com", SERVER_PORT);
+  httpServer.start(SERVER_PORT);
+  return {
+    stop: function(callback) {
+      do_check_eq(expectedCertNames.length, 0);
+      httpServer.stop(callback);
+    }
+  };
 }
 
 function check_cert_err(cert_name, expected_error) {
@@ -77,11 +97,7 @@ function check_ee_for_ev(cert_name, expected_ev) {
     let verifiedChain = {};
     let error = certdb.verifyCertNow(cert, certificateUsageSSLServer,
                                      NO_FLAGS, verifiedChain, hasEVPolicy);
-    if (isDebugBuild) {
-      do_check_eq(hasEVPolicy.value, expected_ev);
-    } else {
-      do_check_false(hasEVPolicy.value);
-    }
+    do_check_eq(hasEVPolicy.value, expected_ev);
     do_check_eq(0, error);
 }
 
@@ -97,44 +113,57 @@ function run_test() {
   Services.prefs.setCharPref("network.dns.localDomains",
                              'www.example.com, crl.example.com');
 
-  start_ocsp_responder();
-
   run_next_test();
 }
 
-
 add_test(function() {
-  check_ee_for_ev("ev-valid", true);
-  run_next_test();
+  clearOCSPCache();
+  let ocspResponder = start_ocsp_responder(
+                        isDebugBuild ? ["int-ev-valid", "ev-valid"]
+                                     : ["ev-valid"]);
+  check_ee_for_ev("ev-valid", isDebugBuild);
+  ocspResponder.stop(run_next_test);
 });
 
 add_test(function() {
+  clearOCSPCache();
+  let ocspResponder = start_ocsp_responder(["non-ev-root"]);
   check_ee_for_ev("non-ev-root", false);
-  run_next_test();
+  ocspResponder.stop(run_next_test);
 });
 
 add_test(function() {
+  clearOCSPCache();
+  let ocspResponder = failingOCSPResponder();
   check_ee_for_ev("no-ocsp-url-cert", false);
-  run_next_test();
+  ocspResponder.stop(run_next_test);
 });
 
 
-add_test(function () {
-  const nsIX509Cert = Ci.nsIX509Cert;
+const nsIX509Cert = Ci.nsIX509Cert;
+add_test(function() {
   let evRootCA = certdb.findCertByNickname(null, evrootnick);
   certdb.setCertTrust(evRootCA, nsIX509Cert.CA_CERT, 0);
+
+  clearOCSPCache();
+  let ocspResponder = failingOCSPResponder();
   check_cert_err("ev-valid", SEC_ERROR_UNTRUSTED_ISSUER);
+  ocspResponder.stop(run_next_test);
+});
+
+
+
+add_test(function() {
+  let evRootCA = certdb.findCertByNickname(null, evrootnick);
   certdb.setCertTrust(evRootCA, nsIX509Cert.CA_CERT,
                       Ci.nsIX509CertDB.TRUSTED_SSL |
                       Ci.nsIX509CertDB.TRUSTED_EMAIL |
                       Ci.nsIX509CertDB.TRUSTED_OBJSIGN);
-  check_ee_for_ev("ev-valid", true);
-  run_next_test();
+
+  clearOCSPCache();
+  let ocspResponder = start_ocsp_responder(
+                        isDebugBuild ? ["int-ev-valid", "ev-valid"]
+                                     : ["ev-valid"]);
+  check_ee_for_ev("ev-valid", isDebugBuild);
+  ocspResponder.stop(run_next_test);
 });
-
-
-add_test(function() {
-  do_check_eq(4, gOCSPResponseCounter);
-  gHttpServer.stop(run_next_test);
-});
-
