4f372e4323b1ab8759eb166860d788d6d742d65c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:27:38 +0000

    Bug 934225 - GLSL code should be syntax highlighted, r=anton
    UltraBlame original commit: 7c3f6ee5b59da78d68f2dfa2509fd9ac7edeefe9

--- browser/devtools/shadereditor/shadereditor.js
+++ browser/devtools/shadereditor/shadereditor.js
@@ -38,7 +38,6 @@ const SHADERS_AUTOGROW_ITEMS = 4;
 const GUTTER_ERROR_PANEL_OFFSET_X = 7; 
 const GUTTER_ERROR_PANEL_DELAY = 100; 
 const DEFAULT_EDITOR_CONFIG = {
-  mode: Editor.modes.text,
   gutters: ["errors"],
   lineNumbers: true,
   showAnnotationRuler: true
@@ -389,6 +388,7 @@ let ShadersEditorsView = {
     
     let parent = $("#" + type +"-editor");
     let editor = new Editor(DEFAULT_EDITOR_CONFIG);
+    editor.config.mode = Editor.modes[type];
     editor.appendTo(parent).then(() => deferred.resolve(editor));
 
     return deferred.promise;
