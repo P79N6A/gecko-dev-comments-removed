526a979f645e4826aefa63f9771781eac03ac5de
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:50:16 +0000

    Bug 877762 - GC: Post-barrier cycle collector participants - 3 Add hashtable type that stores Heap<T> values r=jlebar
    UltraBlame original commit: 50d0df61059ca9b8ab8c2f8014090600a48a9d54

---
+++ xpcom/glue/nsJSThingHashtable.h
@@ -0,0 +1,58 @@
+
+
+
+
+
+#ifndef nsJSThingHashtable_h__
+#define nsJSThingHashtable_h__
+
+#include "nsHashKeys.h"
+#include "nsBaseHashtable.h"
+
+namespace JS {
+template <class T>
+class Heap;
+} 
+
+
+
+
+
+
+
+
+
+
+template<class T>
+class nsHashKeyDisallowMemmove : public T
+{
+ public:
+  nsHashKeyDisallowMemmove(const T& key) : T(key) {}
+  enum { ALLOW_MEMMOVE = false };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+template<class KeyClass,class DataType>
+class nsJSThingHashtable :
+  public nsBaseHashtable<nsHashKeyDisallowMemmove<KeyClass>, JS::Heap<DataType>, DataType>
+{ };
+
+#endif 
