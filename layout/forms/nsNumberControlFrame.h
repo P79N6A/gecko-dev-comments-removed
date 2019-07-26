d7f9a686bcd3d8f3811c91c02a40e72069f83968
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:57:06 +0000

    Bug 948475 - Implement HTMLInputElement.select() for <input type=number> (contrary to the HTML5 spec) since content needs it. r=smaug
    UltraBlame original commit: fa744c4a894883e51366be357cc2cb296fa634ce

--- layout/forms/nsNumberControlFrame.h
+++ layout/forms/nsNumberControlFrame.h
@@ -129,6 +129,11 @@ public:
 
   void HandleFocusEvent(WidgetEvent* aEvent);
 
+  
+
+
+  nsresult HandleSelectCall();
+
   virtual Element* GetPseudoElement(nsCSSPseudoElements::Type aType) MOZ_OVERRIDE;
 
   bool ShouldUseNativeStyleForSpinner() const;
