40ab4f89cbf8786cbb3971e3179a3705ae0c0af3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:30:16 +0000

    Bug 989641 - Move FlattenedMResumePointIter into LRecover. r=jandem
    UltraBlame original commit: 704fd93fb58dcc80bb28e8be7e7b4cacbe1b2389

--- js/src/jit/LIR.cpp
+++ js/src/jit/LIR.cpp
@@ -119,28 +119,48 @@ TotalOperandCount(MResumePoint *mir)
     return accum;
 }
 
-LRecoverInfo::LRecoverInfo(MResumePoint *mir)
-  : mir_(mir),
+LRecoverInfo::LRecoverInfo(TempAllocator &alloc)
+  : instructions_(alloc),
     recoverOffset_(INVALID_RECOVER_OFFSET)
 { }
 
 LRecoverInfo *
 LRecoverInfo::New(MIRGenerator *gen, MResumePoint *mir)
 {
-    LRecoverInfo *recover = new(gen->alloc()) LRecoverInfo(mir);
-    if (!recover)
+    LRecoverInfo *recoverInfo = new(gen->alloc()) LRecoverInfo(gen->alloc());
+    if (!recoverInfo || !recoverInfo->init(mir))
         return nullptr;
 
-    IonSpew(IonSpew_Snapshots, "Generating LIR recover %p from MIR (%p)",
-            (void *)recover, (void *)mir);
+    IonSpew(IonSpew_Snapshots, "Generating LIR recover info %p from MIR (%p)",
+            (void *)recoverInfo, (void *)mir);
 
-    return recover;
+    return recoverInfo;
 }
 
-LSnapshot::LSnapshot(LRecoverInfo *recover, BailoutKind kind)
-  : numSlots_(TotalOperandCount(recover->mir()) * BOX_PIECES),
+bool
+LRecoverInfo::init(MResumePoint *rp)
+{
+    MResumePoint *it = rp;
+
+    
+    
+    
+    
+    do {
+        if (!instructions_.append(it))
+            return false;
+        it = it->caller();
+    } while (it);
+
+    Reverse(instructions_.begin(), instructions_.end());
+    MOZ_ASSERT(mir() == rp);
+    return true;
+}
+
+LSnapshot::LSnapshot(LRecoverInfo *recoverInfo, BailoutKind kind)
+  : numSlots_(TotalOperandCount(recoverInfo->mir()) * BOX_PIECES),
     slots_(nullptr),
-    recoverInfo_(recover),
+    recoverInfo_(recoverInfo),
     snapshotOffset_(INVALID_SNAPSHOT_OFFSET),
     bailoutId_(INVALID_BAILOUT_ID),
     bailoutKind_(kind)
