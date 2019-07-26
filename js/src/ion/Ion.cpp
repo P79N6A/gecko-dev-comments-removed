099b731810fa73c97e0185982883519877582ebb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:18:39 +0000

    Bug #807380: Report to the compiler the function is constructing when entering at a branch, r=dvander
    UltraBlame original commit: e692328bf6a33db14810fa19c51d7ba105db386a

--- js/src/ion/Ion.cpp
+++ js/src/ion/Ion.cpp
@@ -1270,7 +1270,7 @@ ion::CanEnterAtBranch(JSContext *cx, HandleScript script, StackFrame *fp, jsbyte
 
     
     JSFunction *fun = fp->isFunctionFrame() ? fp->fun() : NULL;
-    MethodStatus status = Compile(cx, script, fun, pc, false);
+    MethodStatus status = Compile(cx, script, fun, pc, fp->isConstructing());
     if (status != Method_Compiled) {
         if (status == Method_CantCompile)
             ForbidCompilation(cx, script);
