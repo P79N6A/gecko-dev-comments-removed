530e948fa8599cfffcb78478345722ee47eccc00
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:11:32 +0000

    Bug 795674 - Add DisplayItemData debugging code. r=roc
    UltraBlame original commit: b1c3b7ae35719a30ce1e129bb0bfc6c14766a8a8

--- layout/base/FrameLayerBuilder.h
+++ layout/base/FrameLayerBuilder.h
@@ -645,6 +645,8 @@ protected:
   static PLDHashOperator RemoveDisplayItemDataForFrame(nsRefPtrHashKey<DisplayItemData>* aEntry,
                                                        void* aClosure);
 
+  static PLDHashOperator DumpDisplayItemDataForFrame(nsRefPtrHashKey<DisplayItemData>* aEntry,
+                                                     void* aClosure);
   
 
 
