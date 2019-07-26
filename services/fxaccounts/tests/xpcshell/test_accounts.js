cc52209b6868b68eb694d28527f230e35bdcb81a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:03:11 +0000

    Bug 972070 - followup to address review comments previously missed. r=jedp
    UltraBlame original commit: e041e26bc0f890b25351204d9c4e97142a8e6223

--- services/fxaccounts/tests/xpcshell/test_accounts.js
+++ services/fxaccounts/tests/xpcshell/test_accounts.js
@@ -507,7 +507,7 @@ add_test(function test_resend_email() {
     log.debug("Alice signing in");
 
     
-    do_check_neq(fxa.internal.currentAccountState, initialState);
+    do_check_true(fxa.internal.currentAccountState !== initialState);
     let aliceState = fxa.internal.currentAccountState;
 
     
@@ -524,7 +524,7 @@ add_test(function test_resend_email() {
         do_check_eq(result, "alice's session token");
 
         
-        do_check_eq(fxa.internal.currentAccountState, aliceState);
+        do_check_true(fxa.internal.currentAccountState === aliceState);
 
         
         do_check_true(fxa.internal.currentTimer > 0);
