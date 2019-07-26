d46e410a6b7cf3915f7bfb75f53ef3ca934c1bb1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:22:54 +0000

    Bug 924679. Part 7: Remove gfxAlphaRecovery::Analysis since no-one uses it. r=mattwoodrow
    UltraBlame original commit: f31b00b83f666c1f6ec46872912b8c77b8f9e35f

--- gfx/thebes/gfxAlphaRecovery.h
+++ gfx/thebes/gfxAlphaRecovery.h
@@ -15,13 +15,6 @@ class gfxImageSurface;
 
 class gfxAlphaRecovery {
 public:
-    struct Analysis {
-        bool uniformColor;
-        bool uniformAlpha;
-        gfxFloat alpha;
-        gfxFloat r, g, b;
-    };
-
     
 
 
@@ -39,8 +32,7 @@ public:
 
 
     static bool RecoverAlpha (gfxImageSurface *blackSurface,
-                                const gfxImageSurface *whiteSurface,
-                                Analysis *analysis = nullptr);
+                                const gfxImageSurface *whiteSurface);
 
 #ifdef MOZILLA_MAY_SUPPORT_SSE2
     
