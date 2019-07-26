713b0497107865b71ef577495833726c39f03565
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:18:03 +0000

    Bug 806184 - Can't use 'delete' method in WebIDL. r=bz
    UltraBlame original commit: ea8a3b0290792aa9bd535b1e79237901692256a8

--- dom/bindings/test/TestBindingHeader.h
+++ dom/bindings/test/TestBindingHeader.h
@@ -682,6 +682,20 @@ public:
   uint32_t Length();
 };
 
+class TestCppKeywordNamedMethodsInterface : public nsISupports,
+                                            public nsWrapperCache
+{
+public:
+  NS_DECL_ISUPPORTS
+
+  
+  virtual nsISupports* GetParentObject();
+
+  bool Continue();
+  bool Delete();
+  int32_t Volatile();
+};
+
 } 
 } 
 
