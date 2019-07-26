d2dac1b969a120dd325e505d4a907436b6943e7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:11:27 +0000

    Bug 891650. Use mTransform instead of GetDTTransform() in Mask(). r=bas
    GetDTTransform includes the device offset. We need to avoid that in order for things to work properly.
    
    UltraBlame original commit: d1cbd2f59347ae368ccb5488263aaf25d7ecbb93

--- gfx/thebes/gfxContext.cpp
+++ gfx/thebes/gfxContext.cpp
@@ -1427,10 +1427,10 @@ gfxContext::Mask(gfxPattern *pattern)
       if (pattern->IsAzure() || pattern->GetType() == gfxPattern::PATTERN_SURFACE) {
         RefPtr<SourceSurface> mask = pattern->GetAzureSurface();
         Matrix mat = ToMatrix(pattern->GetInverseMatrix());
-        Matrix old = GetDTTransform();
+        Matrix old = mTransform;
         
         
-        mat = mat * GetDTTransform();
+        mat = mat * mTransform;
 
         ChangeTransform(mat);
         mDT->MaskSurface(GeneralPattern(this), mask, offset, DrawOptions(1.0f, CurrentState().op, CurrentState().aaMode));
