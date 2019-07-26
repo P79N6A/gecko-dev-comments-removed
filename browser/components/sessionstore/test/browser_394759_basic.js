7edcdb9b6749298dca83811a7e3d7cc6ef7ce4b2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:21:50 +0000

    Bug 898744 - Make browser_394759_basic.js wait for tab invalidation before closing the window; r=yoric
    UltraBlame original commit: cc07397c7ad47ce93e0091b5b61d96bf32a5f3ca

--- browser/components/sessionstore/test/browser_394759_basic.js
+++ browser/components/sessionstore/test/browser_394759_basic.js
@@ -2,74 +2,85 @@
 
 
 
+const TEST_URL = "data:text/html;charset=utf-8,<input%20id=txt>" +
+                 "<input%20type=checkbox%20id=chk>";
+
+
+
+
+
+
 function test() {
   waitForExplicitFinish();
 
-  let testURL = "about:config";
   let uniqueKey = "bug 394759";
   let uniqueValue = "unik" + Date.now();
   let uniqueText = "pi != " + Math.random();
 
   
-  Services.prefs.setBoolPref("general.warnOnAboutConfig", false);
-
-  
-  let max_windows_undo = Services.prefs.getIntPref("browser.sessionstore.max_windows_undo");
-  Services.prefs.setIntPref("browser.sessionstore.max_windows_undo", max_windows_undo + 1);
-  let closedWindowCount = ss.getClosedWindowCount();
+  while (SessionStore.getClosedWindowCount()) {
+    SessionStore.forgetClosedWindow(0);
+  }
 
   provideWindow(function onTestURLLoaded(newWin) {
     newWin.gBrowser.addTab().linkedBrowser.stop();
 
     
     ss.setWindowValue(newWin, uniqueKey, uniqueValue);
-    let textbox = newWin.content.document.getElementById("textbox");
-    textbox.value = uniqueText;
-
-    newWin.close();
-
-    is(ss.getClosedWindowCount(), closedWindowCount + 1,
-       "The closed window was added to Recently Closed Windows");
-    let data = JSON.parse(ss.getClosedWindowData())[0];
-    ok(data.title == testURL && JSON.stringify(data).indexOf(uniqueText) > -1,
-       "The closed window data was stored correctly");
+    let [txt, chk] = newWin.content.document.querySelectorAll("#txt, #chk");
+    txt.value = uniqueText;
 
     
-    let newWin2 = ss.undoCloseWindow(0);
+    EventUtils.sendMouseEvent({type: "click"}, chk);
 
-    ok(newWin2 instanceof ChromeWindow,
-       "undoCloseWindow actually returned a window");
-    is(ss.getClosedWindowCount(), closedWindowCount,
-       "The reopened window was removed from Recently Closed Windows");
+    let browser = newWin.gBrowser.selectedBrowser;
+    waitForContentMessage(browser, "SessionStore:input", 1000, result => {
+      ok(result, "received message for input changes");
 
-    
-    let restoredTabs = 0;
-    let expectedTabs = data.tabs.length;
-    newWin2.addEventListener("SSTabRestored", function sstabrestoredListener(aEvent) {
-      ++restoredTabs;
-      info("Restored tab " + restoredTabs + "/" + expectedTabs);
-      if (restoredTabs < expectedTabs) {
-        return;
-      }
+      newWin.close();
 
-      newWin2.removeEventListener("SSTabRestored", sstabrestoredListener, true);
-
-      is(newWin2.gBrowser.tabs.length, 2,
-         "The window correctly restored 2 tabs");
-      is(newWin2.gBrowser.currentURI.spec, testURL,
-         "The window correctly restored the URL");
-
-      let textbox = newWin2.content.document.getElementById("textbox");
-      is(textbox.value, uniqueText,
-         "The window correctly restored the form");
-      is(ss.getWindowValue(newWin2, uniqueKey), uniqueValue,
-         "The window correctly restored the data associated with it");
+      is(ss.getClosedWindowCount(), 1,
+         "The closed window was added to Recently Closed Windows");
+      let data = JSON.parse(ss.getClosedWindowData())[0];
+      ok(data.title == TEST_URL && JSON.stringify(data).indexOf(uniqueText) > -1,
+         "The closed window data was stored correctly");
 
       
-      newWin2.close();
-      Services.prefs.clearUserPref("browser.sessionstore.max_windows_undo");
-      Services.prefs.clearUserPref("general.warnOnAboutConfig");
-      finish();
-    }, true);
-  }, testURL);
+      let newWin2 = ss.undoCloseWindow(0);
+
+      ok(newWin2 instanceof ChromeWindow,
+         "undoCloseWindow actually returned a window");
+      is(ss.getClosedWindowCount(), 0,
+         "The reopened window was removed from Recently Closed Windows");
+
+      
+      let restoredTabs = 0;
+      let expectedTabs = data.tabs.length;
+      newWin2.addEventListener("SSTabRestored", function sstabrestoredListener(aEvent) {
+        ++restoredTabs;
+        info("Restored tab " + restoredTabs + "/" + expectedTabs);
+        if (restoredTabs < expectedTabs) {
+          return;
+        }
+
+        is(restoredTabs, expectedTabs, "correct number of tabs restored");
+        newWin2.removeEventListener("SSTabRestored", sstabrestoredListener, true);
+
+        is(newWin2.gBrowser.tabs.length, 2,
+           "The window correctly restored 2 tabs");
+        is(newWin2.gBrowser.currentURI.spec, TEST_URL,
+           "The window correctly restored the URL");
+
+        let [txt, chk] = newWin2.content.document.querySelectorAll("#txt, #chk");
+        ok(txt.value == uniqueText && chk.checked,
+           "The window correctly restored the form");
+        is(ss.getWindowValue(newWin2, uniqueKey), uniqueValue,
+           "The window correctly restored the data associated with it");
+
+        
+        newWin2.close();
+        finish();
+      }, true);
+    });
+  }, TEST_URL);
 }
