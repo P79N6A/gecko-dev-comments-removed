13be59ae2f58b88e16657c323cd380514c9be27e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:15:36 +0000

    Bug 803826 - Restore the layer builder pointer on return from PaintForFrame. r=mattwoodrow
    It's possible for nsDisplayList::PaintForFrame to reset the layer builder
    pointer on a layer manager it didn't create. Instead of setting it to null,
    store the existing pointer and restore it on return.
    
    UltraBlame original commit: 6b9d82bf9a94e8d16cc1151c23d19aeef65cbaf8

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -1014,6 +1014,9 @@ void nsDisplayList::PaintForFrame(nsDisplayListBuilder* aBuilder,
     layerManager = new BasicLayerManager();
   }
 
+  
+  FrameLayerBuilder *oldBuilder = layerManager->GetLayerBuilder();
+
   FrameLayerBuilder *layerBuilder = new FrameLayerBuilder();
   layerBuilder->Init(aBuilder, layerManager);
 
@@ -1050,13 +1053,13 @@ void nsDisplayList::PaintForFrame(nsDisplayListBuilder* aBuilder,
   nsRefPtr<ContainerLayer> root = layerBuilder->
     BuildContainerLayerFor(aBuilder, layerManager, aForFrame, nullptr, *this,
                            containerParameters, nullptr);
-  
+
   if (widgetTransaction) {
     aForFrame->ClearInvalidationStateBits();
   }
 
   if (!root) {
-    layerManager->RemoveUserData(&gLayerManagerLayerBuilder);
+    layerManager->SetUserData(&gLayerManagerLayerBuilder, oldBuilder);
     return;
   }
   
@@ -1135,7 +1138,7 @@ void nsDisplayList::PaintForFrame(nsDisplayListBuilder* aBuilder,
   }
 
   nsCSSRendering::DidPaint();
-  layerManager->RemoveUserData(&gLayerManagerLayerBuilder);
+  layerManager->SetUserData(&gLayerManagerLayerBuilder, oldBuilder);
 }
 
 uint32_t nsDisplayList::Count() const {
