62e2abc1a95a0ac7d92ae2436e4b423bcc984ba3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:31:19 +0000

    Bug 1021618 - Create the foundation for converting preference dialogs to be in-content. r=jaws a11y=MarcoZ f=Paenglab,davidb
    UltraBlame original commit: 0c9ed31fcb968125a708238d68aaedbaa16cd29e

---
+++ browser/components/preferences/in-content/subdialogs.js
@@ -0,0 +1,138 @@
+
+
+
+
+"use strict";
+
+let gSubDialog = {
+  _closingCallback: null,
+  _frame: null,
+  _overlay: null,
+  _injectedStyleSheets: ["chrome://mozapps/content/preferences/preferences.css",
+                         "chrome://browser/skin/preferences/preferences.css",
+                         "chrome://browser/skin/preferences/in-content/preferences.css"],
+
+  init: function() {
+    this._frame = document.getElementById("dialogFrame");
+    this._overlay = document.getElementById("dialogOverlay");
+
+    
+    let dialogClose = document.getElementById("dialogClose");
+    dialogClose.addEventListener("command", this.close.bind(this));
+
+    
+    let chromeBrowser = window.QueryInterface(Ci.nsIInterfaceRequestor)
+                              .getInterface(Ci.nsIWebNavigation)
+                              .QueryInterface(Ci.nsIDocShell)
+                              .chromeEventHandler;
+    chromeBrowser.addEventListener("DOMTitleChanged", this.updateTitle, true);
+
+    
+    window.addEventListener("DOMFrameContentLoaded", this._onContentLoaded.bind(this), true);
+
+    
+    
+    this._frame.addEventListener("load", this._onLoad.bind(this));
+  },
+
+  uninit: function() {
+    let chromeBrowser = window.QueryInterface(Ci.nsIInterfaceRequestor)
+                              .getInterface(Ci.nsIWebNavigation)
+                              .QueryInterface(Ci.nsIDocShell)
+                              .chromeEventHandler;
+    chromeBrowser.removeEventListener("DOMTitleChanged", gSubDialog.updateTitle, true);
+  },
+
+  updateTitle: function(aEvent) {
+    if (aEvent.target != gSubDialog._frame.contentDocument)
+      return;
+    document.getElementById("dialogTitle").textContent = gSubDialog._frame.contentDocument.title;
+  },
+
+  injectXMLStylesheet: function(aStylesheetURL) {
+    let contentStylesheet = this._frame.contentDocument.createProcessingInstruction(
+      'xml-stylesheet',
+      'href="' + aStylesheetURL + '" type="text/css"'
+    );
+    this._frame.contentDocument.insertBefore(contentStylesheet,
+                                             this._frame.contentDocument.documentElement);
+  },
+
+  open: function(aURL, aFeatures = null, aParams = null, aClosingCallback = null) {
+    let features = aFeatures || "modal,centerscreen,resizable=no";
+    let dialog = window.openDialog(aURL, "dialogFrame", features, aParams);
+    if (aClosingCallback) {
+      this._closingCallback = aClosingCallback.bind(dialog);
+    }
+    return dialog;
+  },
+
+  close: function(aEvent = null) {
+    if (this._closingCallback) {
+      try {
+        this._closingCallback.call(null, aEvent);
+      } catch (ex) {
+        Cu.reportError(ex);
+      }
+      this._closingCallback = null;
+    }
+
+    this._overlay.style.visibility = "";
+    
+    this._frame.removeAttribute("style");
+
+    setTimeout(() => {
+      
+      
+      this._frame.loadURI("about:blank");
+    }, 0);
+  },
+
+  
+
+  _onContentLoaded: function(aEvent) {
+    if (aEvent.target != this._frame || aEvent.target.contentWindow.location == "about:blank")
+      return;
+
+    for (let styleSheetURL of this._injectedStyleSheets) {
+      this.injectXMLStylesheet(styleSheetURL);
+    }
+
+    
+    let oldClose = this._frame.contentWindow.close;
+    this._frame.contentWindow.close = function() {
+      var closingEvent = new CustomEvent("dialogclosing", {
+        bubbles: true,
+        detail: { button: null },
+      });
+      gSubDialog._frame.contentWindow.dispatchEvent(closingEvent);
+
+      oldClose.call(gSubDialog._frame.contentWindow);
+    };
+
+    
+    
+    
+    this._overlay.style.visibility = "visible";
+    this._overlay.style.opacity = "0.01";
+
+    this._frame.contentWindow.addEventListener("dialogclosing", function closingDialog(aEvent) {
+      gSubDialog._frame.contentWindow.removeEventListener("dialogclosing", closingDialog);
+      gSubDialog.close(aEvent);
+    });
+  },
+
+  _onLoad: function(aEvent) {
+    if (aEvent.target.contentWindow.location == "about:blank")
+      return;
+
+    
+    let docEl = this._frame.contentDocument.documentElement;
+    this._frame.style.width = docEl.style.width || docEl.scrollWidth + "px";
+    this._frame.style.height = docEl.style.height || docEl.scrollHeight + "px";
+
+    this._overlay.style.visibility = "visible";
+    this._frame.focus();
+    this._overlay.style.opacity = ""; 
+  },
+};
