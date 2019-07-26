466363bc99dc7c7fef72df412f91f84ddac7df20
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:00:24 +0000

    Bug 970914 - Avoid confusion where if there's already a console client (attachConsole), we get the same WebConsoleClient instance and startListeners is not called. r=past
    UltraBlame original commit: 4572f97a634e0cc30eafc50c365c6f81331ae4eb

---
+++ browser/devtools/webconsole/test/browser_webconsole_start_netmon_first.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+function test() {
+  Task.spawn(runner).then(finishTest);
+
+  function* runner() {
+    const {tab} = yield loadTab("data:text/html;charset=utf8,<p>hello");
+
+    const target = TargetFactory.forTab(tab);
+    const toolbox = yield gDevTools.showToolbox(target, "netmonitor");
+
+    const hud = yield openConsole(tab);
+
+    hud.jsterm.execute("console.log('foobar bug970914')");
+
+    yield waitForMessages({
+      webconsole: hud,
+      messages: [{
+        name: "console.log",
+        text: "foobar bug970914",
+        category: CATEGORY_WEBDEV,
+        severity: SEVERITY_LOG,
+      }],
+    });
+
+    let text = hud.outputNode.textContent;
+    isnot(text.indexOf("foobar bug970914"), -1, "console.log message confirmed");
+    ok(!/logging API|disabled by a script/i.test(text),
+       "no warning about disabled console API");
+  }
+}
+
