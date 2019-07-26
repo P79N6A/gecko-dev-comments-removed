df433f75918fc0f64e457cd9e8ad723cdfea572e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:04:22 +0000

    Bug 1014382 - Add nm-symbolicate.py helper script. r=jrmuizel DONTBUILD as NPOTB
    UltraBlame original commit: b26d1dee92a51511ec4186d4d3dc8b5a16997516

---
+++ tools/profiler/nm-symbolicate.py
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+import sys, subprocess
+
+def NMSymbolicate(library, addresses):
+  args = [
+    "nm", "-D", "-S", library
+  ]
+  nm_lines = subprocess.check_output(args).split("\n")
+  symbol_table = []
+  for line in nm_lines:
+    pieces = line.split(" ", 4)
+    if len(pieces) != 4 or pieces[2] != "T":
+      continue
+    start = int(pieces[0], 16)
+    end = int(pieces[1], 16)
+    symbol = pieces[3]
+    symbol_table.append({
+      "start": int(pieces[0], 16),
+      "end": int(pieces[0], 16) + int(pieces[1], 16),
+      "funcName": pieces[3]
+    });
+
+  for addressStr in addresses:
+    address = int(addressStr, 16)
+    symbolForAddress = None
+    for symbol in symbol_table:
+      if address >= symbol["start"] and address <= symbol["end"]:
+        symbolForAddress = symbol
+        break
+    if symbolForAddress:
+      print symbolForAddress["funcName"]
+    else:
+      print "??" 
+    print ":0" 
+
+if len(sys.argv) > 1:
+    NMSymbolicate(sys.argv[1], sys.argv[2:])
+    sys.exit(0)
+
+print "Usage: nm-symbolicate.py <library> <addresses> > merged.sym"
+
+
