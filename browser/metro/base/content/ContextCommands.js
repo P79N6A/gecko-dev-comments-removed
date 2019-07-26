8c2ed53649ccf334bcdd32f416d1b198c0b12edf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:14:29 +0000

    Backout 9eb6532ccfc3 for bustage on some slaves. r=me
    UltraBlame original commit: 85eedb253f255e287b80074668a8675435577529

--- browser/metro/base/content/ContextCommands.js
+++ browser/metro/base/content/ContextCommands.js
@@ -216,6 +216,11 @@ var ContextCommands = {
     BrowserUI.newTab(ContextMenuUI.popupState.mediaURL, Browser.selectedTab);
   },
 
+  openVideoInFullscreen: function cc_openVideoInFullscreen() {
+    
+    this.sendCommand('videotab');
+  },
+
   
 
   editBookmark: function cc_editBookmark() {
