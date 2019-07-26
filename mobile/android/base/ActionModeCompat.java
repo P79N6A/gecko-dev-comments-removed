decb1f4190245b02f4fa513888f81962ac4df920
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:42:32 +0000

    Bug 768667 - Provide an action mode for compatibility with old Android versions. r=sriram
    UltraBlame original commit: 3c9dbe653a4e731aaf9c65bfc181d026e0ef2c50

---
+++ mobile/android/base/ActionModeCompat.java
@@ -0,0 +1,101 @@
+
+
+
+
+package org.mozilla.gecko;
+
+import org.mozilla.gecko.widget.GeckoPopupMenu;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+class ActionModeCompat implements GeckoPopupMenu.OnMenuItemClickListener,
+                                  View.OnClickListener {
+    private final String LOGTAG = "GeckoActionModeCompat";
+
+    private Callback mCallback;
+    private ActionModeCompatView mView;
+    private Presenter mPresenter;
+
+    
+
+    public static interface Callback {
+        
+        public boolean onCreateActionMode(ActionModeCompat mode, Menu menu);
+
+        
+
+        public boolean onPrepareActionMode(ActionModeCompat mode, Menu menu);
+
+        
+        public boolean onActionItemClicked(ActionModeCompat mode, MenuItem item);
+
+        
+        public void onDestroyActionMode(ActionModeCompat mode);
+    }
+
+    
+
+    public static interface Presenter {
+        
+        public void startActionModeCompat(final Callback callback);
+
+        
+        public void endActionModeCompat();
+    }
+
+    public ActionModeCompat(Presenter presenter, Callback callback, ActionModeCompatView view) {
+        mPresenter = presenter;
+        mCallback = callback;
+
+        mView = view;
+        mView.initForMode(this);
+    }
+
+    public void finish() {
+        
+        mView.getMenu().clear();
+        if (mCallback != null) {
+            mCallback.onDestroyActionMode(this);
+        }
+    }
+
+    public CharSequence getTitle() {
+        return mView.getTitle();
+    }
+
+    public void setTitle(CharSequence title) {
+        mView.setTitle(title);
+    }
+
+    public void setTitle(int resId) {
+        mView.setTitle(resId);
+    }
+
+    public Menu getMenu() {
+        return mView.getMenu();
+    }
+
+    public void invalidate() {
+        if (mCallback != null) {
+            mCallback.onPrepareActionMode(this, mView.getMenu());
+        }
+        mView.invalidate();
+    }
+
+    
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        if (mCallback != null) {
+            return mCallback.onActionItemClicked(this, item);
+        }
+        return false;
+    }
+
+    
+    @Override
+    public void onClick(View v) {
+        mPresenter.endActionModeCompat();
+    }
+}
