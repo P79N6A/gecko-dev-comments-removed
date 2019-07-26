25995f331250ee59b7e1ac9456d7dd8e8113334e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:51:51 +0000

    Backed out changeset 090bd7835f82 (bug 1001419) for Linux mochitest-other failures.
    UltraBlame original commit: bbd77de6be4b5a1ca6421cd0092eebaa7d6b60f6

--- content/media/MediaResource.h
+++ content/media/MediaResource.h
@@ -422,7 +422,7 @@ public:
     
     
     size_t size = MediaResource::SizeOfExcludingThis(aMallocSizeOf);
-    size += mContentType.SizeOfExcludingThisIfUnshared(aMallocSizeOf);
+    size += mContentType.SizeOfIncludingThisIfUnshared(aMallocSizeOf);
 
     return size;
   }
