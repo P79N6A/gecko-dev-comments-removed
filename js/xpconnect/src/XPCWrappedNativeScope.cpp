847d75be834c54e1f624d1a16cf9e4e89a3d26d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:12:36 +0000

    Bug 820170 - assert in XPCWrappedNativeScope::SetGlobal. r=bholley
    UltraBlame original commit: 91e4887f38c93168264ebee1d750ca8f30220cf4

--- js/xpconnect/src/XPCWrappedNativeScope.cpp
+++ js/xpconnect/src/XPCWrappedNativeScope.cpp
@@ -129,7 +129,11 @@ XPCWrappedNativeScope::XPCWrappedNativeScope(JSContext *cx,
         mIsXBLScope(false)
 {
     
-    {   
+    {
+        MOZ_ASSERT(aGlobal);
+        MOZ_ASSERT(js::GetObjectClass(aGlobal)->flags & (JSCLASS_PRIVATE_IS_NSISUPPORTS |
+                                                         JSCLASS_HAS_PRIVATE)); 
+        
         XPCAutoLock lock(XPCJSRuntime::Get()->GetMapLock());
 
 #ifdef DEBUG
