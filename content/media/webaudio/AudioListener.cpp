943009c0b2817b5ac1ccc16516a8b53fd4be24a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:10:01 +0000

    Bug 884368 - Part 1: Add a memory reporter for AudioContexts; r=roc,njn
    UltraBlame original commit: fd8248fed936d08f34ffebf58c6bff219afc28fe

--- content/media/webaudio/AudioListener.cpp
+++ content/media/webaudio/AudioListener.cpp
@@ -118,6 +118,15 @@ void AudioListener::UpdatePannersVelocity()
   }
 }
 
+size_t
+AudioListener::SizeOfIncludingThis(MallocSizeOf aMallocSizeOf) const
+{
+  size_t amount = aMallocSizeOf(this);
+  
+  amount += mPanners.SizeOfExcludingThis(aMallocSizeOf);
+  return amount;
+}
+
 }
 }
 
