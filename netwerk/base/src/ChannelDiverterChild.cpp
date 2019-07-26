fe36ff3de2e3c6d9ac3b7b2900202dfe88d1a72f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:09:25 +0000

    Bug 975338 - Add PChannelDiverter and new messages to PHttpChannel r=jduell
    UltraBlame original commit: 97858e5c1f13d4c9602e436008f0ef01d3638870

---
+++ netwerk/base/src/ChannelDiverterChild.cpp
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+#include "mozilla/net/ChannelDiverterChild.h"
+#include "mozilla/net/NeckoChannelParams.h"
+#include "mozilla/net/HttpChannelChild.h"
+#include "mozilla/net/FTPChannelChild.h"
+#include "mozilla/net/PHttpChannelChild.h"
+#include "mozilla/net/PFTPChannelChild.h"
+#include "nsIDivertableChannel.h"
+
+namespace mozilla {
+namespace net {
+
+ChannelDiverterChild::ChannelDiverterChild()
+{
+}
+
+ChannelDiverterChild::~ChannelDiverterChild()
+{
+}
+
+} 
+} 
