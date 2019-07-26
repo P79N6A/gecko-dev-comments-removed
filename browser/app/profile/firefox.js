83e10010b7a72944afb57fff7926df8ca4e3ffbf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:47:48 +0000

    Bug 881939 - Enable source maps by default; r=past
    UltraBlame original commit: ac87c834bb7ca5af7e145420fde433f1b6f31a9e

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -144,16 +144,7 @@ pref("app.update.auto", true);
 
 
 
-
-pref("app.update.mode", 0);
-
-#ifdef XP_WIN
-#ifdef MOZ_METRO
-
-
-pref("app.update.metro.enabled", true);
-#endif
-#endif
+pref("app.update.mode", 1);
 
 
 pref("app.update.silent", false);
@@ -1076,7 +1067,7 @@ pref("devtools.debugger.remote-host", "localhost");
 pref("devtools.debugger.remote-autoconnect", false);
 pref("devtools.debugger.remote-connection-retries", 3);
 pref("devtools.debugger.remote-timeout", 20000);
-pref("devtools.debugger.source-maps-enabled", false);
+pref("devtools.debugger.source-maps-enabled", true);
 
 
 pref("devtools.debugger.ui.win-x", 0);
