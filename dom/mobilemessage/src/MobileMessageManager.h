9f7dc96e453f5ee226af44a461767de219146f3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:07:58 +0000

    Bug 844431 - B2G MMS: provide nsIDOMMobileMessageManager interface (with sendMMS() first) (part 1, nsIDOMMobileMessageManager). r=vicamo sr=sicking a=leo+
    UltraBlame original commit: a999914becb7746be559072b2c4c188e8be5f7c3

---
+++ dom/mobilemessage/src/MobileMessageManager.h
@@ -0,0 +1,51 @@
+
+
+
+
+
+#ifndef mozilla_dom_mobilemessage_MobileMessageManager_h
+#define mozilla_dom_mobilemessage_MobileMessageManager_h
+
+#include "nsIDOMMobileMessageManager.h"
+#include "nsIObserver.h"
+#include "nsDOMEventTargetHelper.h"
+
+class nsIDOMMozSmsMessage;
+
+namespace mozilla {
+namespace dom {
+
+class MobileMessageManager : public nsDOMEventTargetHelper
+                           , public nsIDOMMozMobileMessageManager
+                           , public nsIObserver
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIOBSERVER
+  NS_DECL_NSIDOMMOZMOBILEMESSAGEMANAGER
+
+  NS_FORWARD_NSIDOMEVENTTARGET(nsDOMEventTargetHelper::)
+
+  void Init(nsPIDOMWindow *aWindow);
+  void Shutdown();
+
+private:
+  
+
+
+  nsresult Send(JSContext* aCx, JSObject* aGlobal, JSString* aNumber,
+                const nsAString& aMessage, jsval* aRequest);
+
+  
+
+
+  nsresult Delete(int32_t aId, nsIDOMMozSmsRequest** aRequest);
+
+  nsresult DispatchTrustedSmsEventToSelf(const nsAString& aEventName,
+                                         nsIDOMMozSmsMessage* aMessage);
+};
+
+} 
+} 
+
+#endif 
