0d1767888479da351dda22c2cb3ded0d141f763b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:41:21 +0000

    Bug 825147: Convert SVGLineElement to WebIDL r=bz
    UltraBlame original commit: 2a404732e63848fde3bbf7a92a4f5f8a12a4c32d

---
+++ content/svg/content/src/SVGLineElement.h
@@ -0,0 +1,72 @@
+
+
+
+
+
+#ifndef mozilla_dom_SVGLineElement_h
+#define mozilla_dom_SVGLineElement_h
+
+#include "nsSVGPathGeometryElement.h"
+#include "nsIDOMSVGLineElement.h"
+#include "nsSVGLength2.h"
+
+nsresult NS_NewSVGLineElement(nsIContent **aResult,
+                              already_AddRefed<nsINodeInfo> aNodeInfo);
+
+namespace mozilla {
+namespace dom {
+
+typedef nsSVGPathGeometryElement SVGLineElementBase;
+
+class SVGLineElement MOZ_FINAL : public SVGLineElementBase,
+                                 public nsIDOMSVGLineElement
+{
+protected:
+  SVGLineElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual JSObject* WrapNode(JSContext *cx, JSObject *scope, bool *triedToWrap) MOZ_OVERRIDE;
+  friend nsresult (::NS_NewSVGLineElement(nsIContent **aResult,
+                                          already_AddRefed<nsINodeInfo> aNodeInfo));
+
+public:
+  
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIDOMSVGLINEELEMENT
+
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+  NS_FORWARD_NSIDOMSVGELEMENT(SVGLineElementBase::)
+
+  
+  NS_IMETHOD_(bool) IsAttributeMapped(const nsIAtom* name) const;
+
+  
+  virtual bool IsMarkable() { return true; }
+  virtual void GetMarkPoints(nsTArray<nsSVGMark> *aMarks);
+  virtual void ConstructPath(gfxContext *aCtx);
+
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+
+  virtual nsXPCClassInfo* GetClassInfo();
+
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+
+  
+  already_AddRefed<nsIDOMSVGAnimatedLength> X1();
+  already_AddRefed<nsIDOMSVGAnimatedLength> Y1();
+  already_AddRefed<nsIDOMSVGAnimatedLength> X2();
+  already_AddRefed<nsIDOMSVGAnimatedLength> Y2();
+
+protected:
+
+  virtual LengthAttributesInfo GetLengthInfo();
+
+  enum { ATTR_X1, ATTR_Y1, ATTR_X2, ATTR_Y2 };
+  nsSVGLength2 mLengthAttributes[4];
+  static LengthInfo sLengthInfo[4];
+};
+
+} 
+} 
+
+#endif 
