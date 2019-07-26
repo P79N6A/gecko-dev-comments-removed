94672202643057db9cc1b16e49968c83e0ab1ff0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:02:58 +0000

    Bug 976648 - JavaScript build/config for powerpc64le-linux support. r=bsmedberg
    UltraBlame original commit: 183ca3e3aa20a217070610b997c09934bf3ee570

--- js/src/assembler/wtf/Platform.h
+++ js/src/assembler/wtf/Platform.h
@@ -165,15 +165,21 @@
     || defined(__POWERPC__) \
     || defined(_M_PPC)      \
     || defined(__PPC)
+#if !defined(__ppc64__) && !defined(__PPC64__)
 #define WTF_CPU_PPC 1
+#endif
+#if !defined(__LITTLE_ENDIAN__)
 #define WTF_CPU_BIG_ENDIAN 1
+#endif
 #endif
 
 
 #if   defined(__ppc64__) \
     || defined(__PPC64__)
 #define WTF_CPU_PPC64 1
+#if !defined(__LITTLE_ENDIAN__)
 #define WTF_CPU_BIG_ENDIAN 1
+#endif
 #endif
 
 
