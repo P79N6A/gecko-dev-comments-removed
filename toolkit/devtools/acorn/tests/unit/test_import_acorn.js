ed3af21f687d4b4236ef62d7e23f42a2ca7b5fc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:25:14 +0000

    Bug 924466 - Integrate acorn with the devtools; r=dcamp,robcee,gps
    UltraBlame original commit: 7e81c446b6caf2581f9af98524aa52fe86cc3a01

---
+++ toolkit/devtools/acorn/tests/unit/test_import_acorn.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+function run_test() {
+  const acorn = require("acorn/acorn");
+  const acorn_loose = require("acorn/acorn_loose");
+  do_check_true(isObject(acorn));
+  do_check_true(isObject(acorn_loose));
+  do_check_eq(typeof acorn.parse, "function");
+  do_check_eq(typeof acorn_loose.parse_dammit, "function");
+}
