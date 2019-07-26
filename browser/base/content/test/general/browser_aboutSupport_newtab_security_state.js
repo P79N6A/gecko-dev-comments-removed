67d16521dd52dd57f646554b167e895c6dcf2d7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:19:16 +0000

    Bug 908534 - test that chromeUI notification is shown for about:support when opened as a new tab, r=ttaubert
    UltraBlame original commit: 2bd196dc1b2c631fdc80ab04ff75df077371aeb4

---
+++ browser/base/content/test/general/browser_aboutSupport_newtab_security_state.js
@@ -0,0 +1,20 @@
+
+
+
+add_task(function* checkIdentityOfAboutSupport() {
+  let tab = gBrowser.loadOneTab("about:support", {
+    referrerURI: null,
+    inBackground: false,
+    allowThirdPartyFixup: false,
+    relatedToCurrent: false,
+    skipAnimation: true,
+    disableMCB: false
+  });
+
+  yield promiseTabLoaded(tab);
+  let identityBox = document.getElementById("identity-box");
+  is(identityBox.className, gIdentityHandler.IDENTITY_MODE_CHROMEUI,
+     "Should know that we're chrome.");
+  gBrowser.removeTab(tab);
+});
+
