30e1d1127343dbf66795cd0f46fcb8ebdc21271a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:34:12 +0000

    Backed out changeset 764540e94692 (bug 871350) because it broke the Jetpack tests
    UltraBlame original commit: 1ef25db6822296367f3bca0cb801b4609160203a

--- addon-sdk/source/lib/sdk/tabs/tab-firefox.js
+++ addon-sdk/source/lib/sdk/tabs/tab-firefox.js
@@ -1,4 +1,4 @@
- 
+
 
 
 'use strict';
@@ -14,8 +14,6 @@ const { activateTab, getOwnerWindow, getBrowserForTab, getTabTitle, setTabTitle,
         getTabURL, setTabURL, getTabContentType, getTabId } = require('./utils');
 const { getOwnerWindow: getPBOwnerWindow } = require('../private-browsing/window/utils');
 const viewNS = require('sdk/core/namespace').ns();
-const { deprecateUsage } = require('sdk/util/deprecate');
-const { getURL } = require('sdk/url/utils');
 
 
 const TABS = [];
@@ -64,9 +62,6 @@ const TabTrait = Trait.compose(EventEmitter, {
 
     viewNS(this._public).tab = this._tab;
     getPBOwnerWindow.implement(this._public, getChromeTab);
-
-    
-    getURL.implement(this._public, (function (obj) this._public.url).bind(this));
 
     
     
@@ -176,13 +171,7 @@ const TabTrait = Trait.compose(EventEmitter, {
 
 
 
-  get favicon() {
-    deprecateUsage(
-      'tab.favicon is deprecated, ' +
-      'please use require("sdk/places/favicon").getFavicon instead.'
-    );
-    return this._tab ? getFaviconURIForLocation(this.url) : undefined
-  },
+  get favicon() this._tab ? getFaviconURIForLocation(this.url) : undefined,
   
 
 
