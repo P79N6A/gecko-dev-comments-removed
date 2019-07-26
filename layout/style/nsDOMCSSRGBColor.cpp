8e9711d3c09e005bfe28fabe54e25495591034be
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:56:12 +0000

    bug 840906 - densISupportsify nsDOMRGBCSSColor r=mccr8
    UltraBlame original commit: 8c0f88c09869875d52325cb86daf94bb6f0b9be6

--- layout/style/nsDOMCSSRGBColor.cpp
+++ layout/style/nsDOMCSSRGBColor.cpp
@@ -1,5 +1,4 @@
-
-
+ 
 
 
 
@@ -8,8 +7,6 @@
 #include "nsDOMCSSRGBColor.h"
 
 #include "mozilla/dom/RGBColorBinding.h"
-#include "nsISupports.h"
-#include "nsCOMPtr.h"
 #include "nsIDOMCSSPrimitiveValue.h"
 #include "nsContentUtils.h"
 #include "nsROCSSPrimitiveValue.h"
@@ -31,14 +28,10 @@ nsDOMCSSRGBColor::~nsDOMCSSRGBColor(void)
 {
 }
 
-NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(nsDOMCSSRGBColor)
-  NS_INTERFACE_MAP_ENTRY(nsISupports)
-  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
-NS_INTERFACE_MAP_END
-
 NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_4(nsDOMCSSRGBColor, mAlpha,  mBlue, mGreen, mRed)
-NS_IMPL_CYCLE_COLLECTING_ADDREF(nsDOMCSSRGBColor)
-NS_IMPL_CYCLE_COLLECTING_RELEASE(nsDOMCSSRGBColor)
+
+NS_IMPL_CYCLE_COLLECTION_ROOT_NATIVE(nsDOMCSSRGBColor, AddRef)
+NS_IMPL_CYCLE_COLLECTION_UNROOT_NATIVE(nsDOMCSSRGBColor, Release)
 
 JSObject*
 nsDOMCSSRGBColor::WrapObject(JSContext *aCx, JSObject *aScope, bool *aTried)
