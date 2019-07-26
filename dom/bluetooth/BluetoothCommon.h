38a80c552e3f706d5883c97ca2616be37c97be33
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:04:45 +0000

    Bug 942104 - Add an event listener "ondiscoverystatechanged", r=gyeh, r=mrbkap
    UltraBlame original commit: b6f247ae6aef612177143f86ed74a277cf7a8e08

--- dom/bluetooth/BluetoothCommon.h
+++ dom/bluetooth/BluetoothCommon.h
@@ -114,6 +114,11 @@ extern bool gBluetoothDebugFlag;
 
 
 
+#define DISCOVERY_STATE_CHANGED_ID           "discoverystatechanged"
+
+
+
+
 
 #define REQUEST_MEDIA_PLAYSTATUS_ID          "requestmediaplaystatus"
 
