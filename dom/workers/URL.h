80a95d460b73c8da98707e20a7f307cc7d71ad05
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:51:59 +0000

    Bug 813253 - URL.createobjectURL in WebWorker. r=bent
    UltraBlame original commit: 7f82f74de11f43c1b7b11187b82339f92263dd69

---
+++ dom/workers/URL.h
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_workers_url_h__
+#define mozilla_dom_workers_url_h__
+
+#include "mozilla/dom/URLBinding.h"
+
+#include "EventTarget.h"
+
+BEGIN_WORKERS_NAMESPACE
+
+class URL : public EventTarget
+{
+public: 
+  static void
+  CreateObjectURL(const WorkerGlobalObject& aGlobal,
+                  JSObject* aArg, const objectURLOptionsWorkers& aOptions,
+                  nsString& aResult, ErrorResult& aRv);
+
+  static void
+  RevokeObjectURL(const WorkerGlobalObject& aGlobal, const nsAString& aUrl);
+};
+
+END_WORKERS_NAMESPACE
+
+#endif 
