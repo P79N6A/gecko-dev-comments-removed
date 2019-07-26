a6d4f55b26d3b5643eceaae721bedeab9e4c5c8d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:58:43 +0000

    Bug 984123, part 2 - Converted nsTArray<char*> to nsTArray<nsCString>. r=ehsan
    UltraBlame original commit: e1adb8f51fa94eb33e2b63a390589e298960805f

--- embedding/components/commandhandler/src/nsCommandGroup.h
+++ embedding/components/commandhandler/src/nsCommandGroup.h
@@ -3,16 +3,12 @@
 
 
 
-
-
 #ifndef nsCommandGroup_h__
 #define nsCommandGroup_h__
 
 #include "nsIController.h"
-#include "nsHashtable.h"
 #include "nsClassHashtable.h"
 #include "nsHashKeys.h"
-
 
 
 #define NS_CONTROLLER_COMMAND_GROUP_CID \
@@ -20,7 +16,6 @@
 
 #define NS_CONTROLLER_COMMAND_GROUP_CONTRACTID \
  "@mozilla.org/embedcomp/controller-command-group;1"
-
 
 class nsControllerCommandGroup : public nsIControllerCommandGroup
 {
@@ -32,16 +27,14 @@ public:
   NS_DECL_NSICONTROLLERCOMMANDGROUP
 
 public:
-  typedef nsClassHashtable<nsCStringHashKey, nsTArray<char*>> GroupsHashtable;
+  typedef nsClassHashtable<nsCStringHashKey, nsTArray<nsCString>> GroupsHashtable;
 
 protected:
   void ClearGroupsHash();
-  static PLDHashOperator ClearEnumerator(const nsACString &aKey, nsTArray<char*> *aData, void *closure);
 
 protected:
-	nsClassHashtable<nsCStringHashKey, nsTArray<char*>> mGroupsHash; 
-	                                                                 
-
+  GroupsHashtable mGroupsHash; 
+                               
 };
 
 #endif 
