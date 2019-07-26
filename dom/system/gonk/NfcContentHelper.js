b07c362f8d907c5e54cbbcf103b20badaea7bd8b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:57:10 +0000

    Backed out changeset ac002daf081b (bug 947100) for B2G mochitest-3 orange.
    UltraBlame original commit: ae8513dfb00f31969d6ff80c4fd568b04e27e09c

--- dom/system/gonk/NfcContentHelper.js
+++ dom/system/gonk/NfcContentHelper.js
@@ -26,9 +26,6 @@ Cu.import("resource://gre/modules/DOMRequestHelper.jsm");
 let NFC = {};
 Cu.import("resource://gre/modules/nfc_consts.js", NFC);
 
-Cu.import("resource://gre/modules/systemlibs.js");
-const NFC_ENABLED = libcutils.property_get("ro.moz.nfc.enabled", "false") === "true";
-
 
 let DEBUG = NFC.DEBUG_CONTENT_HELPER;
 
@@ -387,6 +384,4 @@ NfcContentHelper.prototype = {
   },
 };
 
-if (NFC_ENABLED) {
-  this.NSGetFactory = XPCOMUtils.generateNSGetFactory([NfcContentHelper]);
-}
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([NfcContentHelper]);
