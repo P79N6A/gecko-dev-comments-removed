9026ae00b915fdc3268e8319c9dd2ccf1acc5f30
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:31:22 +0000

    Bug 868604 - Fix "test for equality mistyped as assignment?" warning in Android's downloads.js. r=bnicholson
    UltraBlame original commit: 75c66fa0109eb60b867adeabe3047796563c0bf2

--- mobile/android/chrome/content/downloads.js
+++ mobile/android/chrome/content/downloads.js
@@ -3,6 +3,8 @@
 
 
 
+"use strict";
+
 function dump(a) {
   Cc["@mozilla.org/consoleservice;1"].getService(Ci.nsIConsoleService).logStringMessage(a);
 }
@@ -100,7 +102,7 @@ var Downloads = {
     let alertsService = Cc["@mozilla.org/alerts-service;1"].getService(Ci.nsIAlertsService);
     let progressListener = alertsService.QueryInterface(Ci.nsIAlertsProgressListener);
     let download;
-    while (download = this._privateDownloads.pop()) {
+    while ((download = this._privateDownloads.pop())) {
       try {
         let notificationName = download.target.spec.replace("file:", "download:");
         progressListener.onCancel(notificationName);
