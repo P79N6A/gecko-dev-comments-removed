4dc7cc8b6466a0ab955eb7ffee69bb3733614fa9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:15:17 +0000

    Bug 566746 - Changes to shared form history tests files to support asynchronous form history, p=enndeakin,felix, r=mak
    UltraBlame original commit: 1417a970d5d34e2e2ec2994604eb35af43e3362b

--- toolkit/components/satchel/test/unit/head_satchel.js
+++ toolkit/components/satchel/test/unit/head_satchel.js
@@ -2,8 +2,13 @@
 
 
 
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/FormHistory.jsm");
+
 const Ci = Components.interfaces;
 const Cc = Components.classes;
+const Cu = Components.utils;
 
 const CURRENT_SCHEMA = 4;
 const PR_HOURS = 60 * 60 * 1000000;
@@ -12,6 +17,12 @@ do_get_profile();
 
 var dirSvc = Cc["@mozilla.org/file/directory_service;1"].
              getService(Ci.nsIProperties);
+
+
+
+var formHistoryStartup = Cc["@mozilla.org/satchel/form-history-startup;1"].
+                         getService(Ci.nsIObserver);
+formHistoryStartup.observe(null, "profile-after-change", null);
 
 function getDBVersion(dbfile) {
     var ss = Cc["@mozilla.org/storage/service;1"].
@@ -25,10 +36,63 @@ function getDBVersion(dbfile) {
 
 const isGUID = /[A-Za-z0-9\+\/]{16}/;
 
-function getGUIDforID(conn, id) {
-    var stmt = conn.createStatement("SELECT guid from moz_formhistory WHERE id = " + id);
-    stmt.executeStep();
-    var guid = stmt.getString(0);
-    stmt.finalize();
-    return guid;
+
+function searchEntries(terms, params, iter) {
+  FormHistory.search(terms, params, { onSuccess: function (results) { iter.send(results); },
+                                      onFailure: function (error) {
+                                        do_throw("Error occurred searching form history: " + error);
+                                      }
+                                    });
+}
+
+
+
+function countEntries(name, value, then) {
+  var obj = {};
+  if (name !== null)
+    obj.fieldname = name;
+  if (value !== null)
+    obj.value = value;
+
+  FormHistory.count(obj, { onSuccess: function (num) then(num),
+                           onFailure: function (error) {
+                             do_throw("Error occurred searching form history: " + error);
+                           }
+                         });
+}
+
+
+function updateEntry(op, name, value, then) {
+  var obj = { op: op };
+  if (name !== null)
+    obj.fieldname = name;
+  if (value !== null)
+    obj.value = value;
+  updateFormHistory(obj, then);
+}
+
+
+function addEntry(name, value, then) {
+  let now = Date.now() * 1000;
+  updateFormHistory({ op: "add", fieldname: name, value: value, timesUsed: 1,
+                      firstUsed: now, lastUsed: now }, then);
+}
+
+
+function updateFormHistory(changes, then) {
+  FormHistory.update(changes, { onSuccess: then,
+                                onFailure: function (error) {
+                                  do_throw("Error occurred updating form history: " + error);
+                                }
+                              });
+}
+
+
+
+
+
+
+
+function do_log_info(aMessage) {
+  print("TEST-INFO | " + _TEST_FILE + " | " + aMessage);
 }
