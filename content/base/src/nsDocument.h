d999c382e8abc777932c14f9478c1a82986a1994
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:05:52 +0000

    Backout Bug 768029 because that breaks Gaia (see https://github.com/mozilla-b2g/gaia/issues/5177)
    UltraBlame original commit: f4e6abb6160f644546f14533640e81d3c54fc24d

--- content/base/src/nsDocument.h
+++ content/base/src/nsDocument.h
@@ -1274,7 +1274,7 @@ private:
   void DoUnblockOnload();
 
   nsresult CheckFrameOptions();
-  nsresult InitCSP(nsIChannel* aChannel);
+  nsresult InitCSP(nsIChannel* aChannel, nsIContentSecurityPolicy **aCSP);
 
   
   
