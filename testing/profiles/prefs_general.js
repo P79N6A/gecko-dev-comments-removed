8c57e159922f6ed0974109ea548960c5aacecf80
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:42:37 +0000

    Bug 995995 - set testing prefs to redirect to the test proxy server for RSS feeds; r=jmaher
    UltraBlame original commit: 6863631867ea7700a0db20b69dc2d517731b8ecd

--- testing/profiles/prefs_general.js
+++ testing/profiles/prefs_general.js
@@ -57,9 +57,6 @@ user_pref("experiments.supported", true);
 user_pref("experiments.logging.level", "Trace");
 
 
-user_pref("experiments.manifest.uri", "http://%(server)s/experiments-dummy/manifest");
-
-
 
 user_pref("extensions.enabledScopes", 5);
 
@@ -180,3 +177,12 @@ user_pref("browser.snippets.syncPromo.enabled", false);
 
 
 user_pref("browser.cache.use_new_backend_temp", false);
+
+
+
+user_pref('browser.contentHandlers.types.0.uri', 'http://test1.example.org/rss?url=%%s')
+user_pref('browser.contentHandlers.types.1.uri', 'http://test1.example.org/rss?url=%%s')
+user_pref('browser.contentHandlers.types.2.uri', 'http://test1.example.org/rss?url=%%s')
+user_pref('browser.contentHandlers.types.3.uri', 'http://test1.example.org/rss?url=%%s')
+user_pref('browser.contentHandlers.types.4.uri', 'http://test1.example.org/rss?url=%%s')
+user_pref('browser.contentHandlers.types.5.uri', 'http://test1.example.org/rss?url=%%s')
