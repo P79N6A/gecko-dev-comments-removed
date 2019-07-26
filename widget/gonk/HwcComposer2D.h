b290c3785fa9b733b66aabf3fa12dbc58371ca16
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:10:48 +0000

    Backed out changeset e02e1b107db7 (bug 919676) for causing bug 924129.
    UltraBlame original commit: 64b497e6f59339df30812595a780df973bb49cac

--- widget/gonk/HwcComposer2D.h
+++ widget/gonk/HwcComposer2D.h
@@ -62,11 +62,7 @@ public:
     
     bool TryRender(layers::Layer* aRoot, const gfxMatrix& aGLWorldTransform) MOZ_OVERRIDE;
 
-    bool Render(EGLDisplay dpy, EGLSurface sur);
-
 private:
-    void Prepare(buffer_handle_t fbHandle, int fence);
-    bool Commit();
     bool TryHwComposition();
     bool ReallocLayerList();
     bool PrepareLayerList(layers::Layer* aContainer, const nsIntRect& aClip,
