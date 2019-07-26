81ab5a177249a25afc15486bbf1b7161942739a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:30:07 +0000

    Bug 909447 - Test. r=bholley DONTBUILD
    UltraBlame original commit: 3300c750cd537d7ade9e8d3262ef00e0bc8c5937

---
+++ js/src/jit-test/tests/auto-regress/bug909447.js
@@ -0,0 +1,21 @@
+
+
+function f(code) {
+    try {
+        g = Function(code)
+    } catch (e) {}
+    g()
+}
+f("\
+    Object.defineProperty(this,\"x\",{\
+        get: function(){\
+            evaluate(\"Array(x)\",{\
+                newContext:true,\
+                catchTermination:(function(){})\
+            })\
+        }\
+    })\
+");
+f("x");
+f(")");
+f("x");
