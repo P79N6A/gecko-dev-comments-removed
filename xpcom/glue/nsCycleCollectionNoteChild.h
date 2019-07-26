cf7b6975d2da259494289104bb0b607df8c98f00
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:27:14 +0000

    Bug 806279 - CC macros refactoring: part 3: fold the TRAVERSE_NATIVE and AMBIGUOUS cases - r=mccr8,smaug
    UltraBlame original commit: 17f8e44632a6c3143372110608cc07c4fa011221

--- xpcom/glue/nsCycleCollectionNoteChild.h
+++ xpcom/glue/nsCycleCollectionNoteChild.h
@@ -11,6 +11,7 @@
 
 #include "nsCycleCollectionTraversalCallback.h"
 #include "mozilla/Likely.h"
+#include "mozilla/TypeTraits.h"
 
 enum {
   CycleCollectionEdgeNameArrayFlag = 1
@@ -31,6 +32,57 @@ CycleCollectionNoteEdgeName(nsCycleCollectionTraversalCallback& aCallback,
   if (MOZ_UNLIKELY(aCallback.WantDebugInfo())) {
     CycleCollectionNoteEdgeNameImpl(aCallback, aName, aFlags);
   }
+}
+
+#define NS_CYCLE_COLLECTION_INNERCLASS                                         \
+        cycleCollection
+
+#define NS_CYCLE_COLLECTION_INNERNAME                                          \
+        _cycleCollectorGlobal
+
+#define NS_CYCLE_COLLECTION_PARTICIPANT(_class)                                \
+        _class::NS_CYCLE_COLLECTION_INNERNAME.GetParticipant()
+
+template <typename T>
+nsISupports* ToSupports(T* p, typename T::NS_CYCLE_COLLECTION_INNERCLASS* dummy = 0)
+{
+  return T::NS_CYCLE_COLLECTION_INNERCLASS::Upcast(p);
+}
+
+
+
+template <typename T,
+          bool IsXPCOM = mozilla::IsBaseOf<nsISupports, T>::value>
+struct CycleCollectionNoteChildImpl
+{
+};
+
+template <typename T>
+struct CycleCollectionNoteChildImpl<T, true>
+{
+  static void Run(nsCycleCollectionTraversalCallback& aCallback, T* aChild)
+  {
+    aCallback.NoteXPCOMChild(ToSupports(aChild));
+  }
+};
+
+template <typename T>
+struct CycleCollectionNoteChildImpl<T, false>
+{
+  static void Run(nsCycleCollectionTraversalCallback& aCallback, T* aChild)
+  {
+    aCallback.NoteNativeChild(aChild, NS_CYCLE_COLLECTION_PARTICIPANT(T));
+  }
+};
+
+template <typename T>
+inline void CycleCollectionNoteChild(nsCycleCollectionTraversalCallback& aCallback,
+                                     T* aChild,
+                                     const char* aName,
+                                     uint32_t aFlags = 0)
+{
+  CycleCollectionNoteEdgeName(aCallback, aName, aFlags);
+  CycleCollectionNoteChildImpl<T>::Run(aCallback, aChild);
 }
 
 #endif 
