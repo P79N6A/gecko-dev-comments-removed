67885e843c3deb758f75d9833a44aac6d115e53b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:47:55 +0000

    Bug 888084 - Rolling mean for MFBT; r=waldo
    UltraBlame original commit: 468598d17533a1d1a401aea5479da63f4ee74f5f

---
+++ mfbt/RollingMean.h
@@ -0,0 +1,109 @@
+
+
+
+
+
+
+
+#ifndef mozilla_RollingMean_h_
+#define mozilla_RollingMean_h_
+
+#include "mozilla/Assertions.h"
+#include "mozilla/TypeTraits.h"
+#include "mozilla/Vector.h"
+
+#include <algorithm>
+#include <stddef.h>
+#include <stdint.h>
+
+namespace mozilla {
+
+
+
+
+
+
+
+
+
+
+template<typename T, typename S>
+class RollingMean
+{
+  private:
+    size_t mInsertIndex;
+    size_t mMaxValues;
+    Vector<T> mValues;
+    S mTotal;
+
+  public:
+    static_assert(!IsFloatingPoint<T>::value,
+                  "floating-point types are unsupported due to rounding "
+                  "errors");
+
+    RollingMean(size_t aMaxValues)
+      : mInsertIndex(0),
+        mMaxValues(aMaxValues),
+        mTotal(0)
+    {
+      MOZ_ASSERT(aMaxValues > 0);
+    }
+
+    RollingMean& operator=(RollingMean&& aOther) {
+      MOZ_ASSERT(this != &aOther, "self-assignment is forbidden");
+      this->~RollingMean();
+      new(this) RollingMean(aOther.mMaxValues);
+      mInsertIndex = aOther.mInsertIndex;
+      mTotal = aOther.mTotal;
+      mValues.swap(aOther.mValues);
+      return *this;
+    }
+
+    
+
+
+    bool insert(T aValue) {
+      MOZ_ASSERT(mValues.length() <= mMaxValues);
+
+      if (mValues.length() == mMaxValues) {
+        mTotal = mTotal - mValues[mInsertIndex] + aValue;
+        mValues[mInsertIndex] = aValue;
+      } else {
+        if (!mValues.append(aValue))
+          return false;
+        mTotal = mTotal + aValue;
+      }
+
+      mInsertIndex = (mInsertIndex + 1) % mMaxValues;
+      return true;
+    }
+
+    
+
+
+    T mean() {
+      MOZ_ASSERT(!empty());
+      return T(mTotal / mValues.length());
+    }
+
+    bool empty() {
+      return mValues.empty();
+    }
+
+    
+
+
+    void clear() {
+      mValues.clear();
+      mInsertIndex = 0;
+      mTotal = T(0);
+    }
+
+    size_t maxValues() {
+      return mMaxValues;
+    }
+};
+
+} 
+
+#endif 
