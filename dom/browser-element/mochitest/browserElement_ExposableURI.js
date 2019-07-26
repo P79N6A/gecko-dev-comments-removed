bb8639904bb10179835c32ae5d6bba866a51ddeb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:12:36 +0000

    Bug 795317: Test case r=jlebar
    UltraBlame original commit: 2112a3c02c739c0875962d08066a8a197e8e0dd9

---
+++ dom/browser-element/mochitest/browserElement_ExposableURI.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+"use strict";
+SimpleTest.waitForExplicitFinish();
+
+var iframe;
+
+
+function testPassword() {
+  function locationchange(e) {
+    var uri = e.detail;
+    is(uri, 'http://mochi.test:8888/tests/dom/browser-element/mochitest/file_empty.html',
+       "Username and password shouldn't be exposed in uri.");
+    SimpleTest.finish();
+  }
+
+  iframe.addEventListener('mozbrowserlocationchange', locationchange);
+  iframe.src = "http://iamuser:iampassword@mochi.test:8888/tests/dom/browser-element/mochitest/file_empty.html";
+}
+
+function testWyciwyg() {
+  var locationChangeCount = 0;
+
+  function locationchange(e) {
+    
+    
+    
+    if (locationChangeCount == 0) {
+      locationChangeCount ++;
+    } else if (locationChangeCount == 1) {
+      var uri = e.detail;
+      is(uri, 'http://mochi.test:8888/tests/dom/browser-element/mochitest/file_wyciwyg.html', "Scheme in string shouldn't be wyciwyg");
+      iframe.removeEventListener('mozbrowserlocationchange', locationchange);
+      SimpleTest.executeSoon(testPassword);
+    }
+  }
+
+  
+  iframe.src = 'file_wyciwyg.html';
+  iframe.addEventListener('mozbrowserlocationchange', locationchange);
+}
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addPermission();
+
+  iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+  document.body.appendChild(iframe);
+  testWyciwyg();
+}
+
+addEventListener('load', function() { SimpleTest.executeSoon(runTest); });
