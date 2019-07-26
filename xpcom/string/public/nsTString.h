86729256f89db0da71de59addce11996c2bed826
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:34:09 +0000

    Bug 910795 part 1.  Add a way to Rebind() an nsString to be a dependent string.  r=bsmedberg
    UltraBlame original commit: d42f8528d09f8b41bd8b15efe878eed35694a201

--- xpcom/string/public/nsTString.h
+++ xpcom/string/public/nsTString.h
@@ -358,6 +358,18 @@ class nsTString_CharT : public nsTSubstring_CharT
 
 #endif 
 
+      void Rebind( const char_type* data, size_type length );
+
+        
+
+
+      void AssertValidDepedentString()
+        {
+          NS_ASSERTION(mData, "nsTDependentString must wrap a non-NULL buffer");
+          NS_ASSERTION(mLength != size_type(-1), "nsTDependentString has bogus length");
+          NS_ASSERTION(mData[mLength] == 0, "nsTDependentString must wrap only null-terminated strings");
+        }
+
 
     protected:
 
@@ -368,7 +380,7 @@ class nsTString_CharT : public nsTSubstring_CharT
         
       nsTString_CharT( char_type* data, size_type length, uint32_t flags )
         : substring_type(data, length, flags) {}
-  };
+};
 
 
 class nsTFixedString_CharT : public nsTString_CharT
--- xpcom/string/src/nsTDependentString.cpp
+++ xpcom/string/src/nsTDependentString.cpp
@@ -5,18 +5,6 @@
 
 
 void
-nsTDependentString_CharT::Rebind( const char_type* data, size_type length )
-  {
-    
-    Finalize();
-
-    mData = const_cast<char_type*>(data);
-    mLength = length;
-    SetDataFlags(F_TERMINATED);
-    AssertValid();
-  }
-
-void
 nsTDependentString_CharT::Rebind( const string_type& str, uint32_t startPos )
   {
     
