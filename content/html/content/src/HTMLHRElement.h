ba14199fcb3ecf7fc027df61c6737e03470f3d0e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:52:34 +0000

    Bug 838233 - Rename nsHTMLHRElement to HTMLHRElement. r=Ms2ger
    UltraBlame original commit: 28418193e7016c30556b68c2b749584bb2ca861d

---
+++ content/html/content/src/HTMLHRElement.h
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_HTMLHRElement_h
+#define mozilla_dom_HTMLHRElement_h
+
+#include "nsGenericHTMLElement.h"
+#include "nsIDOMHTMLHRElement.h"
+#include "nsIDOMEventTarget.h"
+#include "nsMappedAttributes.h"
+#include "nsAttrValueInlines.h"
+#include "nsRuleData.h"
+
+namespace mozilla {
+namespace dom {
+
+class HTMLHRElement : public nsGenericHTMLElement,
+                      public nsIDOMHTMLHRElement
+{
+public:
+  HTMLHRElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual ~HTMLHRElement();
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+
+  
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+
+  
+  NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
+
+  
+  NS_DECL_NSIDOMHTMLHRELEMENT
+
+  virtual bool ParseAttribute(int32_t aNamespaceID,
+                                nsIAtom* aAttribute,
+                                const nsAString& aValue,
+                                nsAttrValue& aResult);
+  NS_IMETHOD_(bool) IsAttributeMapped(const nsIAtom* aAttribute) const;
+  virtual nsMapRuleToAttributesFunc GetAttributeMappingFunction() const;
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+  virtual nsXPCClassInfo* GetClassInfo();
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+};
+
+} 
+} 
+
+#endif 
