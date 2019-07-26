9e8cd8d17ef61179a09d9439ba54cc5a80091d4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:11:34 +0000

    Bug 815492 - Pause Web Audio playback when entering the bfcache, and resume it when coming out of the bfcache; r=bzbarsky
    UltraBlame original commit: fe50f4cbc119bb7743e11da5e9c5c4584d4668b2

--- dom/base/nsGlobalWindow.cpp
+++ dom/base/nsGlobalWindow.cpp
@@ -10870,6 +10870,11 @@ nsGlobalWindow::SuspendTimeouts(uint32_t aIncrease,
         t->Release();
       }
     }
+
+    
+    for (uint32_t i = 0; i < mAudioContexts.Length(); ++i) {
+      mAudioContexts[i]->Suspend();
+    }
   }
 
   
@@ -10925,6 +10930,11 @@ nsGlobalWindow::ResumeTimeouts(bool aThawChildren)
         ac->AddWindowListener(mEnabledSensors[i], this);
     }
     EnableGamepadUpdates();
+
+    
+    for (uint32_t i = 0; i < mAudioContexts.Length(); ++i) {
+      mAudioContexts[i]->Resume();
+    }
 
     
     nsIScriptContext *scx = GetContextInternal();
