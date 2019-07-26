f4dbdb8b1e5be0ef45092db9a6945ffff34fc042
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:07:17 +0000

    Bug 884061 - Part 3e: Use NS_DECL_THREADSAFE_ISUPPORTS in docshell/, r=bz
    UltraBlame original commit: eb49396e9def51a527bbf267895af764949fb0db

--- docshell/base/nsDSURIContentListener.cpp
+++ docshell/base/nsDSURIContentListener.cpp
@@ -52,8 +52,8 @@ nsDSURIContentListener::Init()
 
 
 
-NS_IMPL_THREADSAFE_ADDREF(nsDSURIContentListener)
-NS_IMPL_THREADSAFE_RELEASE(nsDSURIContentListener)
+NS_IMPL_ADDREF(nsDSURIContentListener)
+NS_IMPL_RELEASE(nsDSURIContentListener)
 
 NS_INTERFACE_MAP_BEGIN(nsDSURIContentListener)
     NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, nsIURIContentListener)
