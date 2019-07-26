98e12a887f1ac741498b709b9b082c679c04cab8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:15:00 +0000

    Bug 981936 - Fix hardcoded paths in WebGL conformance tests. - r=kamidphish
    UltraBlame original commit: 14f78cd007ac6b5f123822b0490f7e65bab64aca

---
+++ content/canvas/test/webgl-conformance/resources/cors-util.js
@@ -0,0 +1,31 @@
+
+
+
+
+function getExampleDotComURL(conformanceDirRelativePath) {
+  var prefix = 'http://example.com/tests/content/canvas/test/webgl-conformance/';
+  return prefix + conformanceDirRelativePath;
+}
+
+function setExampleDotComImage(imageElemID, imagePath, relPathToBase, onLoadCallback) {
+  var img = document.getElementById(imageElemID);
+  if (!img)
+    throw 'Bad `imageElemID`: ' + imageElemID;
+
+  if (onLoadCallback)
+    img.onload = onLoadCallback;
+
+  img.onerror = function() {
+    console.log('Failed to load image from: ' + img.src);
+    console.log('Loading same-domain backup image.');
+
+    img.onerror = function() {
+      console.log('Failed to load backup image from: ' + img.src);
+      return;
+    };
+
+    img.src = relPathToBase + imagePath;
+  };
+
+  img.src = getExampleDotComURL(imagePath);
+}
