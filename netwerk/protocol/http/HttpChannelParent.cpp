78fbc746f9c2827f1a2962b6eb2097b9f73d5601
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:35:35 +0000

    bug 871289 [1 of 2] standardize netwerk/protocol/http on moz_assert r=jduell
    UltraBlame original commit: 7400723d23bc45e3f57f776047fba23e4632fc2f

--- netwerk/protocol/http/HttpChannelParent.cpp
+++ netwerk/protocol/http/HttpChannelParent.cpp
@@ -47,7 +47,7 @@ HttpChannelParent::HttpChannelParent(PBrowserParent* iframeEmbedding,
   
   nsIHttpProtocolHandler* handler;
   CallGetService(NS_NETWORK_PROTOCOL_CONTRACTID_PREFIX "http", &handler);
-  NS_ASSERTION(handler, "no http handler");
+  MOZ_ASSERT(handler);
 
   mTabParent = static_cast<mozilla::dom::TabParent*>(iframeEmbedding);
 }
