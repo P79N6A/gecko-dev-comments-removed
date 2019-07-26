dbaa7fb393f22d64a24ac581f2b335462f09b41a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:36:42 +0000

    Bug 821160: Bustage fix for Froyo debug build - r=bustage
    UltraBlame original commit: d433d1a9fd78fa6a78931cd82595294d75be2d25

--- media/omx-plugin/OmxPlugin.cpp
+++ media/omx-plugin/OmxPlugin.cpp
@@ -506,7 +506,7 @@ bool OmxDecoder::SetVideoFormat() {
   
   
 
-#ifdef DEBUG
+#if defined(DEBUG) && !defined(MOZ_ANDROID_FROYO)
   int32_t unexpected;
   if (format->findInt32(kKeyStride, &unexpected))
     LOG("Expected kKeyWidth, but found kKeyStride %d", unexpected);
