bda556b9888ed2f5c87bffd4852d677611cf6635
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:29:23 +0000

    Bug 874210 - Fix pymake stack depth issues when many targets and prerequisites are present. r=gps
    UltraBlame original commit: 41bec0e7bc304a0f63af073b7a96b6ffb214337c

--- build/pymake/make.py
+++ build/pymake/make.py
@@ -17,15 +17,6 @@ if __name__ == '__main__':
 
   gc.disable()
 
-  
-  
-  
-  
-  try:
-      sys.setrecursionlimit(10000)
-  except Exception:
-      print >>sys.stderr, 'Unable to increase Python recursion limit.'
-
   pymake.command.main(sys.argv[1:], os.environ, os.getcwd(), cb=sys.exit)
   pymake.process.ParallelContext.spin()
   assert False, "Not reached"
