effd49efedd80860208e8c00316913c44b450990
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:12:27 +0000

    Bug 899759 - Bustage fix on a CLOSED TREE.
    UltraBlame original commit: 3ce7cb22607a9e8e7a79e7234e34093d255de144

--- js/src/jsweakmap.cpp
+++ js/src/jsweakmap.cpp
@@ -342,7 +342,7 @@ JS_NondeterministicGetWeakMapKeys(JSContext *cx, JSObject *objArg, JSObject **re
     ObjectValueMap *map = obj->as<WeakMapObject>().getMap();
     if (map) {
         
-        AutoSuppressGC suppress(cx);
+        gc::AutoSuppressGC suppress(cx);
         for (ObjectValueMap::Base::Range r = map->all(); !r.empty(); r.popFront()) {
             RootedObject key(cx, r.front().key);
             if (!JS_WrapObject(cx, key.address()))
