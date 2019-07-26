12675f70d1ccfe3d04997acda10cbe63b0af205d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:46:07 +0000

    Backed out changeset f38875c274c0 (bug 921918) Windows XP Debug Bustage on a CLOSED TREE
    UltraBlame original commit: 38e2398c0c7d6d9932c21d761aa195e6543692be

--- dom/mobilemessage/src/gonk/MmsService.js
+++ dom/mobilemessage/src/gonk/MmsService.js
@@ -1838,8 +1838,11 @@ MmsService.prototype = {
 
     if (DEBUG) debug("Updating the delivery status to: " + deliveryStatus);
     gMobileMessageDatabaseService
-      .setMessageDeliveryStatusByEnvelopeId(envelopeId, address, deliveryStatus,
-                                            (function(aRv, aDomMessage) {
+      .setMessageDeliveryByEnvelopeId(envelopeId,
+                                      address,
+                                      null,
+                                      deliveryStatus,
+                                      (function notifySetDeliveryResult(aRv, aDomMessage) {
       if (DEBUG) debug("Marking the delivery status is done.");
       
 
