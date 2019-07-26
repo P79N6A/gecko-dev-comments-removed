f85ad83c39b352e95cfb8999ac40b9cc86042961
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:16:42 +0000

    Bug 860892 - IPC failure handling in CompositorChild and ShadowLayers. r=jmuizelaar
    UltraBlame original commit: 7fb4bd994bdf394e86194eb3060582da28b57c29

--- gfx/layers/ipc/CompositorChild.cpp
+++ gfx/layers/ipc/CompositorChild.cpp
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set sw=2 ts=2 et tw=80 : */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 
 #include "CompositorChild.h"
 #include "CompositorParent.h"
@@ -14,7 +14,7 @@ using mozilla::layers::ShadowLayersChild;
 namespace mozilla {
 namespace layers {
 
-/*static*/ CompositorChild* CompositorChild::sCompositor;
+ CompositorChild* CompositorChild::sCompositor;
 
 CompositorChild::CompositorChild(LayerManager *aLayerManager)
   : mLayerManager(aLayerManager)
@@ -40,16 +40,16 @@ CompositorChild::Destroy()
   SendStop();
 }
 
-/*static*/ PCompositorChild*
+ PCompositorChild*
 CompositorChild::Create(Transport* aTransport, ProcessId aOtherProcess)
 {
-  // There's only one compositor per child process.
+  
   MOZ_ASSERT(!sCompositor);
 
   nsRefPtr<CompositorChild> child(new CompositorChild(nullptr));
   ProcessHandle handle;
   if (!base::OpenProcessHandle(aOtherProcess, &handle)) {
-    // We can't go on without a compositor.
+    
     NS_RUNTIMEABORT("Couldn't OpenProcessHandle() to parent process.");
     return nullptr;
   }
@@ -58,14 +58,14 @@ CompositorChild::Create(Transport* aTransport, ProcessId aOtherProcess)
     NS_RUNTIMEABORT("Couldn't Open() Compositor channel.");
     return nullptr;
   }
-  // We release this ref in ActorDestroy().
+  
   return sCompositor = child.forget().get();
 }
 
-/*static*/ PCompositorChild*
+ PCompositorChild*
 CompositorChild::Get()
 {
-  // This is only expected to be used in child processes.
+  
   MOZ_ASSERT(XRE_GetProcessType() != GeckoProcessType_Default);
   return sCompositor;
 }
@@ -89,17 +89,22 @@ void
 CompositorChild::ActorDestroy(ActorDestroyReason aWhy)
 {
   MOZ_ASSERT(sCompositor == this);
+
+  if (aWhy == AbnormalShutdown) {
+    NS_RUNTIMEABORT("ActorDestroy by IPC channel failure at CompositorChild");
+  }
+
   sCompositor = NULL;
-  // We don't want to release the ref to sCompositor here, during
-  // cleanup, because that will cause it to be deleted while it's
-  // still being used.  So defer the deletion to after it's not in
-  // use.
+  
+  
+  
+  
   MessageLoop::current()->PostTask(
     FROM_HERE,
     NewRunnableMethod(this, &CompositorChild::Release));
 }
 
 
-} // namespace layers
-} // namespace mozilla
+} 
+} 
 
