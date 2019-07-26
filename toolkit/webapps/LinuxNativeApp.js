b7f1d07033c06176aeea0de6f4b6408af1d7ce07
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:35:49 +0000

    Bug 994216 - ensure desktop INI file parent directory exists to fix bustage on CLOSED TREE; r=marco"
    UltraBlame original commit: 690c810c8e3e3a07b6809688e56d1c3fa33b78a3

--- toolkit/webapps/LinuxNativeApp.js
+++ toolkit/webapps/LinuxNativeApp.js
@@ -277,6 +277,9 @@ NativeApp.prototype = {
 
     
     let desktopINIfile = getFile(this.desktopINI);
+    if (desktopINIfile.parent && !desktopINIfile.parent.exists()) {
+      desktopINIfile.parent.create(Ci.nsIFile.DIRECTORY_TYPE, PERMS_DIRECTORY);
+    }
 
     let writer = Cc["@mozilla.org/xpcom/ini-processor-factory;1"].
                  getService(Ci.nsIINIParserFactory).
