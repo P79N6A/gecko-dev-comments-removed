ae17e7877771cd9320b5cadd7327e0cb803475ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:08:56 +0000

    Bug 566370. Remove return type from AddProxy(). r=joe
    No one checks it, and it's not really useful.
    
    UltraBlame original commit: bd25449d4be4d4b4070e5a943a2cd015ad2fecc7

--- image/src/imgRequest.h
+++ image/src/imgRequest.h
@@ -65,7 +65,7 @@ public:
                 int32_t aCORSMode);
 
   
-  nsresult AddProxy(imgRequestProxy *proxy);
+  void AddProxy(imgRequestProxy *proxy);
 
   
   nsresult RemoveProxy(imgRequestProxy *proxy, nsresult aStatus, bool aNotify);
