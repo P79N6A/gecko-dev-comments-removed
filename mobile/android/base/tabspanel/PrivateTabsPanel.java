6d22ac7bf79614c04d88964fa875e787e54d5767
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:22:39 +0000

    Bug 1002303 - Part 2: Show a description page when there are no private tabs. r=lucasr
    UltraBlame original commit: 1a18c37601b7f2359cae33a2dd7fb40ed5c84eb8

---
+++ mobile/android/base/tabspanel/PrivateTabsPanel.java
@@ -0,0 +1,77 @@
+
+
+
+
+
+package org.mozilla.gecko.tabspanel;
+
+import java.util.Locale;
+
+import org.mozilla.gecko.BrowserLocaleManager;
+import org.mozilla.gecko.R;
+import org.mozilla.gecko.Tabs;
+import org.mozilla.gecko.tabspanel.TabsPanel.PanelView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+
+
+
+
+
+
+class PrivateTabsPanel extends FrameLayout implements PanelView {
+    private TabsPanel tabsPanel;
+    private TabsTray tabsTray;
+
+    public PrivateTabsPanel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        LayoutInflater.from(context).inflate(R.layout.private_tabs_panel, this);
+        tabsTray = (TabsTray) findViewById(R.id.private_tabs_tray);
+
+        final View emptyView = findViewById(R.id.private_tabs_empty);
+        tabsTray.setEmptyView(emptyView);
+
+        final View learnMore = findViewById(R.id.private_tabs_learn_more);
+        learnMore.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String locale = BrowserLocaleManager.getLanguageTag(Locale.getDefault());
+                final String url =
+                        getResources().getString(R.string.private_tabs_panel_learn_more_link, locale);
+                Tabs.getInstance().loadUrlInTab(url);
+                if (tabsPanel != null) {
+                    tabsPanel.autoHidePanel();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void setTabsPanel(TabsPanel panel) {
+        tabsPanel = panel;
+        tabsTray.setTabsPanel(panel);
+    }
+
+    @Override
+    public void show() {
+        tabsTray.show();
+        setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hide() {
+        setVisibility(View.GONE);
+        tabsTray.hide();
+    }
+
+    @Override
+    public boolean shouldExpand() {
+        return tabsTray.shouldExpand();
+    }
+}
