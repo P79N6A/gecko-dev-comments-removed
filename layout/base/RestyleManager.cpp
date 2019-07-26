b58557c1a454d9dcf8227022c9e1c6a7d1638903
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:03:50 +0000

    Bug 828312 patch 9b:  Make handling of RecomputePosition hint check continuations.  r=bzbarsky
    This is part of the patch stack making change hints apply across all
    continuations and block-in-inline siblings.
    
    UltraBlame original commit: bab54dc717fe8c94198ba980aaed1af68b2661ed

--- layout/base/RestyleManager.cpp
+++ layout/base/RestyleManager.cpp
@@ -345,38 +345,39 @@ RestyleManager::RecomputePosition(nsIFrame* aFrame)
         break;
     }
 
-    nsIFrame* cb = aFrame->GetContainingBlock();
-    nsMargin newOffsets;
 
     
     if (display->mPosition == NS_STYLE_POSITION_STICKY) {
       
       
-      if (!aFrame->GetPrevContinuation()) {
-        StickyScrollContainer::ComputeStickyOffsets(aFrame);
-        StickyScrollContainer* ssc =
-          StickyScrollContainer::GetStickyScrollContainerForFrame(aFrame);
-        if (ssc) {
-          ssc->PositionContinuations(aFrame);
-        }
+      StickyScrollContainer::ComputeStickyOffsets(aFrame);
+      StickyScrollContainer* ssc =
+        StickyScrollContainer::GetStickyScrollContainerForFrame(aFrame);
+      if (ssc) {
+        ssc->PositionContinuations(aFrame);
       }
     } else {
       MOZ_ASSERT(NS_STYLE_POSITION_RELATIVE == display->mPosition,
                  "Unexpected type of positioning");
-      const nsSize size = cb->GetContentRectRelativeToSelf().Size();
+      for (nsIFrame *cont = aFrame; cont;
+           cont = nsLayoutUtils::GetNextContinuationOrSpecialSibling(cont)) {
+        nsIFrame* cb = cont->GetContainingBlock();
+        nsMargin newOffsets;
+        const nsSize size = cb->GetContentRectRelativeToSelf().Size();
 
-      nsHTMLReflowState::ComputeRelativeOffsets(
-          cb->StyleVisibility()->mDirection,
-          aFrame, size.width, size.height, newOffsets);
-      NS_ASSERTION(newOffsets.left == -newOffsets.right &&
-                   newOffsets.top == -newOffsets.bottom,
-                   "ComputeRelativeOffsets should return valid results");
+        nsHTMLReflowState::ComputeRelativeOffsets(
+            cb->StyleVisibility()->mDirection,
+            cont, size.width, size.height, newOffsets);
+        NS_ASSERTION(newOffsets.left == -newOffsets.right &&
+                     newOffsets.top == -newOffsets.bottom,
+                     "ComputeRelativeOffsets should return valid results");
 
-      
-      
-      
-      aFrame->SetPosition(aFrame->GetNormalPosition() +
+        
+        
+        
+        cont->SetPosition(cont->GetNormalPosition() +
                           nsPoint(newOffsets.left, newOffsets.top));
+      }
     }
 
     return true;
