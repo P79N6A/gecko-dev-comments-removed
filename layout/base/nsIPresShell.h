c8729c8dba22bcb766c8e43a72e9bad12d2a8dad
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:42:14 +0000

    Bug 508725 - Part 7: Optimize restyling when a scoped style sheet changes. r=dbaron
    UltraBlame original commit: 0f146c4352496e083d34ac60c94e10467b8c1fc0

--- layout/base/nsIPresShell.h
+++ layout/base/nsIPresShell.h
@@ -1424,6 +1424,9 @@ protected:
   
   RenderFlags               mRenderFlags;
 
+  
+  
+  
   bool                      mStylesHaveChanged : 1;
   bool                      mDidInitialize : 1;
   bool                      mIsDestroying : 1;
@@ -1446,6 +1449,15 @@ protected:
 
   bool                      mSuppressInterruptibleReflows : 1;
   bool                      mScrollPositionClampingScrollPortSizeSet : 1;
+
+  
+  
+  
+  
+  
+  
+  
+  nsAutoTArray<nsRefPtr<mozilla::dom::Element>,1> mChangedScopeStyleRoots;
 
   static nsIContent*        gKeyDownTarget;
 
