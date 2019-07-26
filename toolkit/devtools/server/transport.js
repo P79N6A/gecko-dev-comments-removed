9d04d441a623560072a347a38bcf1bb35d65a2da
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:42:33 +0000

    Back out b4913818b95e (bug 878319) for browser-chrome bustage CLOSED TREE
    UltraBlame original commit: fc1972bf14104cbd000ef24f6dceb9fa1149cc0b

--- toolkit/devtools/server/transport.js
+++ toolkit/devtools/server/transport.js
@@ -7,8 +7,61 @@
 "use strict";
 Components.utils.import("resource://gre/modules/NetUtil.jsm");
 
-Components.utils.import("resource://gre/modules/devtools/DevToolsUtils.jsm");
-var { makeInfallible } = DevToolsUtils;
+
+function safeErrorString(aError) {
+  try {
+    var s = aError.toString();
+    if (typeof s === "string")
+      return s;
+  } catch (ee) { }
+
+  return "<failed trying to find error description>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function makeInfallible(aHandler, aName) {
+  if (!aName)
+    aName = aHandler.name;
+
+  return function () {
+    try {
+      return aHandler.apply(this, arguments);
+    } catch (ex) {
+      let msg = "Handler function ";
+      if (aName) {
+        msg += aName + " ";
+      }
+      msg += "threw an exception: " + safeErrorString(ex);
+      if (ex.stack) {
+        msg += "\nCall stack:\n" + ex.stack;
+      }
+
+      dump(msg + "\n");
+
+      if (Cu.reportError) {
+        
+
+
+
+
+        Cu.reportError(msg);
+      }
+    }
+  }
+}
 
 
 
