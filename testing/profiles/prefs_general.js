a307de3e8b0f5620ee04b0529d42f7671b8556ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:14:22 +0000

    Bug 835930 - move b2g unittest profile handling on top of mozbase instead of automation.py, r=jgriffin
    UltraBlame original commit: 947b9aa5e2d3961f8334f8656616c68634fd744c

---
+++ testing/profiles/prefs_general.js
@@ -0,0 +1,110 @@
+
+
+user_pref("browser.console.showInPanel", true);
+user_pref("browser.dom.window.dump.enabled", true);
+user_pref("browser.firstrun.show.localepicker", false);
+user_pref("browser.firstrun.show.uidiscovery", false);
+user_pref("browser.startup.page", 0); 
+user_pref("browser.ui.layout.tablet", 0); 
+user_pref("dom.allow_scripts_to_close_windows", true);
+user_pref("dom.disable_open_during_load", false);
+user_pref("dom.experimental_forms", true); 
+user_pref("dom.experimental_forms_range", true); 
+user_pref("dom.max_script_run_time", 0); 
+user_pref("hangmonitor.timeout", 0); 
+user_pref("dom.max_chrome_script_run_time", 0);
+user_pref("dom.popup_maximum", -1);
+user_pref("dom.send_after_paint_to_content", true);
+user_pref("dom.successive_dialog_time_limit", 0);
+user_pref("signed.applets.codebase_principal_support", true);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("shell.checkDefaultClient", false);
+user_pref("browser.warnOnQuit", false);
+user_pref("accessibility.typeaheadfind.autostart", false);
+user_pref("javascript.options.showInConsole", true);
+user_pref("devtools.errorconsole.enabled", true);
+user_pref("devtools.debugger.remote-port", 6023);
+user_pref("layout.debug.enable_data_xbl", true);
+user_pref("browser.EULA.override", true);
+user_pref("javascript.options.jit_hardening", true);
+user_pref("gfx.color_management.force_srgb", true);
+user_pref("network.manage-offline-status", false);
+user_pref("dom.min_background_timeout_value", 1000);
+user_pref("test.mousescroll", true);
+user_pref("security.default_personal_cert", "Select Automatically"); 
+user_pref("network.http.prompt-temp-redirect", false);
+user_pref("media.cache_size", 100);
+user_pref("media.volume_scale", "0.01");
+user_pref("security.warn_viewing_mixed", false);
+user_pref("app.update.enabled", false);
+user_pref("app.update.staging.enabled", false);
+user_pref("browser.panorama.experienced_first_run", true); 
+user_pref("dom.w3c_touch_events.enabled", 1);
+user_pref("dom.undo_manager.enabled", true);
+user_pref("dom.webcomponents.enabled", true);
+
+user_pref("toolkit.telemetry.prompted", 999);
+user_pref("toolkit.telemetry.notifiedOptOut", 999);
+
+user_pref("font.size.inflation.emPerLine", 0);
+user_pref("font.size.inflation.minTwips", 0);
+
+
+
+user_pref("extensions.enabledScopes", 5);
+
+user_pref("extensions.getAddons.cache.enabled", false);
+
+user_pref("extensions.installDistroAddons", false);
+
+user_pref("extensions.testpilot.runStudies", false);
+user_pref("extensions.testpilot.alreadyCustomizedToolbar", true);
+
+user_pref("geo.wifi.uri", "http://%(server)s/tests/dom/tests/mochitest/geolocation/network_geolocation.sjs");
+user_pref("geo.wifi.testing", true);
+user_pref("geo.ignore.location_filter", true);
+
+user_pref("camino.warn_when_closing", false); 
+
+
+user_pref("urlclassifier.updateinterval", 172800);
+
+user_pref("browser.safebrowsing.gethashURL", "http://%(server)s/safebrowsing-dummy/gethash");
+user_pref("browser.safebrowsing.keyURL", "http://%(server)s/safebrowsing-dummy/newkey");
+user_pref("browser.safebrowsing.updateURL", "http://%(server)s/safebrowsing-dummy/update");
+
+user_pref("extensions.update.url", "http://%(server)s/extensions-dummy/updateURL");
+user_pref("extensions.update.background.url", "http://%(server)s/extensions-dummy/updateBackgroundURL");
+user_pref("extensions.blocklist.url", "http://%(server)s/extensions-dummy/blocklistURL");
+user_pref("extensions.hotfix.url", "http://%(server)s/extensions-dummy/hotfixURL");
+
+user_pref("extensions.update.enabled", false);
+
+user_pref("extensions.webservice.discoverURL", "http://%(server)s/extensions-dummy/discoveryURL");
+
+user_pref("extensions.getAddons.maxResults", 0);
+user_pref("extensions.getAddons.get.url", "http://%(server)s/extensions-dummy/repositoryGetURL");
+user_pref("extensions.getAddons.getWithPerformance.url", "http://%(server)s/extensions-dummy/repositoryGetWithPerformanceURL");
+user_pref("extensions.getAddons.search.browseURL", "http://%(server)s/extensions-dummy/repositoryBrowseURL");
+user_pref("extensions.getAddons.search.url", "http://%(server)s/extensions-dummy/repositorySearchURL");
+
+user_pref("plugins.update.url", "http://%(server)s/plugins-dummy/updateCheckURL");
+
+
+user_pref("security.notification_enable_delay", 0);
+
+
+user_pref("security.turn_off_all_security_so_that_viruses_can_take_over_this_computer", true);
+
+
+user_pref("network.activity.blipIntervalMilliseconds", 250);
+
+
+user_pref("datareporting.policy.dataSubmissionPolicyBypassAcceptance", true);
+
+
+
+user_pref("datareporting.healthreport.documentServerURI", "http://%(server)s/healthreport/");
+
+
+user_pref("layout.css.report_errors", true);
