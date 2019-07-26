6c6bcd57d75a91e51b8a145fc1ff5d3993d2709a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:28:22 +0000

    Bug 984994 - Reintroduce 'network.http.use-cache' pref, r=jduell
    UltraBlame original commit: 4cd63770fd2984c39f5081fd9a7e4ffb49e9dff3

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -315,6 +315,10 @@ nsHttpChannel::Connect()
         return NS_ERROR_DOCUMENT_NOT_CACHED;
     }
 
+    if (!gHttpHandler->UseCache()) {
+        return ContinueConnect();
+    }
+
     
     rv = OpenCacheEntry(usingSSL);
 
