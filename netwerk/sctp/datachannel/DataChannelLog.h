8e5a65bf276708d14361b7094ad7dc556b3eb797
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:08:58 +0000

    Bug 729512: DataChannel protocol rollup r=mcmanus
    UltraBlame original commit: 37ac46f7dd404f375d11a7fd3ac6d937ed315878

---
+++ netwerk/sctp/datachannel/DataChannelLog.h
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+#ifndef DataChannelLog_h
+#define DataChannelLog_h
+
+#ifdef MOZ_LOGGING
+#define FORCE_PR_LOG
+#endif
+
+#if defined(PR_LOG)
+#error "This file must be #included before any IPDL-generated files or other files that #include prlog.h"
+#endif
+
+#include "base/basictypes.h"
+#include "prlog.h"
+
+#ifdef PR_LOGGING
+extern PRLogModuleInfo* dataChannelLog;
+#endif
+
+#undef LOG
+#define LOG(args) PR_LOG(dataChannelLog, PR_LOG_DEBUG, args)
+
+#endif
---
+++ netwerk/sctp/datachannel/DataChannelProtocol.h
@@ -0,0 +1,81 @@
+
+
+
+
+
+
+#ifndef NETWERK_SCTP_DATACHANNEL_DATACHANNELPROTOCOL_H_
+#define NETWERK_SCTP_DATACHANNEL_DATACHANNELPROTOCOL_H_
+
+#if defined(__GNUC__)
+#define SCTP_PACKED __attribute__((packed))
+#elif defined(_MSC_VER)
+#pragma pack (push, 1)
+#define SCTP_PACKED
+#else
+#error "Unsupported compiler"
+#endif
+
+#define DATA_CHANNEL_PPID_CONTROL   50
+#define DATA_CHANNEL_PPID_DOMSTRING 51
+#define DATA_CHANNEL_PPID_BINARY    52
+#define DATA_CHANNEL_PPID_BINARY_LAST 53
+
+#define DATA_CHANNEL_MAX_BINARY_FRAGMENT 0x4000
+
+#define DATA_CHANNEL_FLAGS_SEND_REQ             0x00000001
+#define DATA_CHANNEL_FLAGS_SEND_RSP             0x00000002
+#define DATA_CHANNEL_FLAGS_SEND_ACK             0x00000004
+#define DATA_CHANNEL_FLAGS_OUT_OF_ORDER_ALLOWED 0x00000008
+#define DATA_CHANNEL_FLAGS_SEND_DATA            0x00000010
+#define DATA_CHANNEL_FLAGS_FINISH_OPEN          0x00000020
+#define DATA_CHANNEL_FLAGS_FINISH_RSP           0x00000040
+
+#define INVALID_STREAM (0xFFFF)
+
+#define MAX_NUM_STREAMS (2048)
+
+struct rtcweb_datachannel_open_request {
+  uint8_t  msg_type; 
+  uint8_t  channel_type;  
+  uint16_t flags;
+  uint16_t reliability_params;
+  int16_t  priority;
+  char     label[1]; 
+} SCTP_PACKED;
+
+struct rtcweb_datachannel_open_response {
+  uint8_t  msg_type; 
+  uint8_t  error;    
+  uint16_t flags;
+  uint16_t reverse_stream;
+} SCTP_PACKED;
+
+struct rtcweb_datachannel_ack {
+  uint8_t  msg_type; 
+} SCTP_PACKED;
+
+
+#define DATA_CHANNEL_OPEN_REQUEST             0
+#define DATA_CHANNEL_OPEN_RESPONSE            1
+#define DATA_CHANNEL_ACK                      2
+
+
+#define DATA_CHANNEL_RELIABLE                 0
+#define DATA_CHANNEL_PARTIAL_RELIABLE_REXMIT  1
+#define DATA_CHANNEL_PARTIAL_RELIABLE_TIMED   2
+
+
+#define DATA_CHANNEL_FLAG_OUT_OF_ORDER_ALLOWED 0x0001
+
+
+
+#define ERR_DATA_CHANNEL_ALREADY_OPEN   1
+#define ERR_DATA_CHANNEL_NONE_AVAILABLE 2
+
+#if defined(_MSC_VER)
+#pragma pack (pop)
+#undef SCTP_PACKED
+#endif
+
+#endif
