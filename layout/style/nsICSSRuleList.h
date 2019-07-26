ddd481aa998f55626bf6a9f999097e2db225276b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:07:29 +0000

    Bug 892489 - Implement the WebIDL API for CSSRuleList; r=bz
    UltraBlame original commit: 9a01239e24335dca6c0c813678e8f1c6d46bb81b

--- layout/style/nsICSSRuleList.h
+++ layout/style/nsICSSRuleList.h
@@ -3,22 +3,44 @@
 
 
 
-#ifndef nsICSSRuleList_h___
-#define nsICSSRuleList_h___
+#ifndef nsICSSRuleList_h
+#define nsICSSRuleList_h
 
+#include "nsIDOMCSSRule.h"
 #include "nsIDOMCSSRuleList.h"
 
 
 #define NS_ICSSRULELIST_IID \
-{ 0x7ae746fd, 0x259a, 0x4a69, \
- { 0x97, 0x2d, 0x2c, 0x10, 0xf7, 0xb0, 0x04, 0xa1 } }
+{ 0xccc0135a, 0xbcb6, 0x4654, \
+  { 0x8c, 0x78, 0x74, 0x35, 0x97, 0x9b, 0x88, 0x19 } }
 
 class nsICSSRuleList : public nsIDOMCSSRuleList
 {
 public:
   NS_DECLARE_STATIC_IID_ACCESSOR(NS_ICSSRULELIST_IID)
 
-  virtual nsIDOMCSSRule* GetItemAt(uint32_t aIndex, nsresult* aResult) = 0;
+  NS_IMETHOD
+  GetLength(uint32_t* aLength) MOZ_OVERRIDE MOZ_FINAL
+  {
+    *aLength = Length();
+    return NS_OK;
+  }
+  NS_IMETHOD
+  Item(uint32_t aIndex, nsIDOMCSSRule** aReturn) MOZ_OVERRIDE MOZ_FINAL
+  {
+    NS_IF_ADDREF(*aReturn = Item(aIndex));
+    return NS_OK;
+  }
+
+  
+  nsIDOMCSSRule* Item(uint32_t aIndex)
+  {
+    bool unused;
+    return IndexedGetter(aIndex, unused);
+  }
+
+  virtual nsIDOMCSSRule* IndexedGetter(uint32_t aIndex, bool& aFound) = 0;
+  virtual uint32_t Length() = 0;
 };
 
 NS_DEFINE_STATIC_IID_ACCESSOR(nsICSSRuleList, NS_ICSSRULELIST_IID)
