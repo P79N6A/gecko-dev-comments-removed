fbb4080693036e7d61404e6b342727485dd2dbe1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:15:17 +0000

    bug 980197 - on long press of a phone number, select the whole phone number r=mfinkle
    UltraBlame original commit: a0e6d0b027dca70b350e700fcbce28f517ce1f7a

--- mobile/android/chrome/content/SelectionHandler.js
+++ mobile/android/chrome/content/SelectionHandler.js
@@ -276,6 +276,42 @@ var SelectionHandler = {
       return false;
     }
 
+    if (this._isPhoneNumber(selection.toString())) {
+      let anchorNode = selection.anchorNode;
+      let anchorOffset = selection.anchorOffset;
+      let focusNode = null;
+      let focusOffset = null;
+      while (this._isPhoneNumber(selection.toString().trim())) {
+        focusNode = selection.focusNode;
+        focusOffset = selection.focusOffset;
+        selection.modify("extend", "forward", "word");
+        
+        if (focusNode == selection.focusNode && focusOffset == selection.focusOffset) {
+          break;
+        }
+      }
+
+      
+      selection.collapse(focusNode, focusOffset);
+      selection.extend(anchorNode, anchorOffset);
+
+      anchorNode = focusNode;
+      anchorOffset = focusOffset
+
+      while (this._isPhoneNumber(selection.toString().trim())) {
+        focusNode = selection.focusNode;
+        focusOffset = selection.focusOffset;
+        selection.modify("extend", "backward", "word");
+        
+        if (focusNode == selection.focusNode && focusOffset == selection.focusOffset) {
+          break;
+        }
+      }
+
+      selection.collapse(focusNode, focusOffset);
+      selection.extend(anchorNode, anchorOffset);
+    }
+
     
     selection.QueryInterface(Ci.nsISelectionPrivate).addSelectionListener(this);
     this._activeType = this.TYPE_SELECTION;
@@ -759,12 +795,14 @@ var SelectionHandler = {
     this._closeSelection();
   },
 
-  _phoneRegex: /(?:\s|^)[\+]?(\(?\d{1,8}\)?)?([- \.]+\(?\d{1,8}\)?)+( ?(x|ext) ?\d{1,3})?(?:\s|$)/,
+  _phoneRegex: /^\+?[0-9\s,-.\(\)*#pw]{1,30}$/,
 
-  _getSelectedPhoneNumber: function sh_isPhoneNumber() {
-    let selectedText = this._getSelectedText();
-    return (selectedText.length && this._phoneRegex.test(selectedText) ?
-            selectedText : null);
+  _getSelectedPhoneNumber: function sh_getSelectedPhoneNumber() {
+    return this._isPhoneNumber(this._getSelectedText().trim());
+  },
+
+  _isPhoneNumber: function sh_isPhoneNumber(selectedText) {
+    return (this._phoneRegex.test(selectedText) ? selectedText : null);
   },
 
   callSelection: function sh_callSelection() {
