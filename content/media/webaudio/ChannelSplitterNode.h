f8d0e6016fd2b550c6928d65511b710910829cbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:30:48 +0000

    Bug 865247 - Part 4: Implement ChannelSplitterNode; r=roc
    UltraBlame original commit: ab91fc927a9fdb5d19e003df90e3903f3f986d96

---
+++ content/media/webaudio/ChannelSplitterNode.h
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+#ifndef ChannelSplitterNode_h_
+#define ChannelSplitterNode_h_
+
+#include "AudioNode.h"
+
+namespace mozilla {
+namespace dom {
+
+class AudioContext;
+
+class ChannelSplitterNode : public AudioNode
+{
+public:
+  ChannelSplitterNode(AudioContext* aContext,
+                      uint16_t aOutputCount);
+
+  NS_DECL_ISUPPORTS_INHERITED
+
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
+
+  virtual uint16_t NumberOfOutputs() const { return mOutputCount; }
+
+private:
+  const uint16_t mOutputCount;
+};
+
+}
+}
+
+#endif
+
