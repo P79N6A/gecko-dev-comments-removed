546ffe3ce34069ec0055f6f5389ed1532e1acb7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:59:09 +0000

    Bug 929513 - Add YCbCrUtils class and change gfxIntSize in BasicCompositor r=nical
    The purpose of this patch is to replace usages of gfxIntSize with
    gfx::IntSize in BasicCompositor.
    
    The new class YCbCrUtils has two methods ported from gfxUtils in thebes.
    BasicCompositor now uses these methods instead of the ones in gfxUtils.
    Also changed gfxIntSize to gfx::IntSize and gfxImageFormat to
    gfx::SurfaceFormat.
    
    UltraBlame original commit: a4ec1ee06e9d253be6f38014d6b09a4458042a82

---
+++ gfx/ycbcr/YCbCrUtils.cpp
@@ -0,0 +1,156 @@
+
+
+
+
+
+#include "gfx2DGlue.h"
+
+#include "YCbCrUtils.h"
+#include "yuv_convert.h"
+#include "ycbcr_to_rgb565.h"
+
+namespace mozilla {
+namespace gfx {
+
+void
+GetYCbCrToRGBDestFormatAndSize(const layers::PlanarYCbCrData& aData,
+                               SurfaceFormat& aSuggestedFormat,
+                               IntSize& aSuggestedSize)
+{
+  YUVType yuvtype =
+    TypeFromSize(aData.mYSize.width,
+                 aData.mYSize.height,
+                 aData.mCbCrSize.width,
+                 aData.mCbCrSize.height);
+
+  
+  
+  bool prescale = aSuggestedSize.width > 0 && aSuggestedSize.height > 0 &&
+                  aSuggestedSize != aData.mPicSize;
+
+  if (aSuggestedFormat == FORMAT_R5G6B5) {
+#if defined(HAVE_YCBCR_TO_RGB565)
+    if (prescale &&
+        !IsScaleYCbCrToRGB565Fast(aData.mPicX,
+                                  aData.mPicY,
+                                  aData.mPicSize.width,
+                                  aData.mPicSize.height,
+                                  aSuggestedSize.width,
+                                  aSuggestedSize.height,
+                                  yuvtype,
+                                  FILTER_BILINEAR) &&
+        IsConvertYCbCrToRGB565Fast(aData.mPicX,
+                                   aData.mPicY,
+                                   aData.mPicSize.width,
+                                   aData.mPicSize.height,
+                                   yuvtype)) {
+      prescale = false;
+    }
+#else
+    
+    aSuggestedFormat = FORMAT_B8G8R8X8;
+#endif
+  }
+  else if (aSuggestedFormat != FORMAT_B8G8R8X8) {
+    
+    aSuggestedFormat = FORMAT_B8G8R8X8;
+  }
+  if (aSuggestedFormat == FORMAT_B8G8R8X8) {
+    
+
+    if (aData.mPicX != 0 || aData.mPicY != 0 || yuvtype == YV24)
+      prescale = false;
+  }
+  if (!prescale) {
+    aSuggestedSize = aData.mPicSize;
+  }
+}
+
+void
+ConvertYCbCrToRGB(const layers::PlanarYCbCrData& aData,
+                  const SurfaceFormat& aDestFormat,
+                  const IntSize& aDestSize,
+                  unsigned char* aDestBuffer,
+                  int32_t aStride)
+{
+  
+  
+  MOZ_ASSERT((aData.mCbCrSize.width == aData.mYSize.width ||
+              aData.mCbCrSize.width == (aData.mYSize.width + 1) >> 1) &&
+             (aData.mCbCrSize.height == aData.mYSize.height ||
+              aData.mCbCrSize.height == (aData.mYSize.height + 1) >> 1));
+  YUVType yuvtype =
+    TypeFromSize(aData.mYSize.width,
+                 aData.mYSize.height,
+                 aData.mCbCrSize.width,
+                 aData.mCbCrSize.height);
+
+  
+  if (aDestSize != ToIntSize(aData.mPicSize)) {
+#if defined(HAVE_YCBCR_TO_RGB565)
+    if (aDestFormat == FORMAT_R5G6B5) {
+      ScaleYCbCrToRGB565(aData.mYChannel,
+                         aData.mCbChannel,
+                         aData.mCrChannel,
+                         aDestBuffer,
+                         aData.mPicX,
+                         aData.mPicY,
+                         aData.mPicSize.width,
+                         aData.mPicSize.height,
+                         aDestSize.width,
+                         aDestSize.height,
+                         aData.mYStride,
+                         aData.mCbCrStride,
+                         aStride,
+                         yuvtype,
+                         FILTER_BILINEAR);
+    } else
+#endif
+      ScaleYCbCrToRGB32(aData.mYChannel, 
+                        aData.mCbChannel,
+                        aData.mCrChannel,
+                        aDestBuffer,
+                        aData.mPicSize.width,
+                        aData.mPicSize.height,
+                        aDestSize.width,
+                        aDestSize.height,
+                        aData.mYStride,
+                        aData.mCbCrStride,
+                        aStride,
+                        yuvtype,
+                        ROTATE_0,
+                        FILTER_BILINEAR);
+  } else { 
+#if defined(HAVE_YCBCR_TO_RGB565)
+    if (aDestFormat == FORMAT_R5G6B5) {
+      ConvertYCbCrToRGB565(aData.mYChannel,
+                           aData.mCbChannel,
+                           aData.mCrChannel,
+                           aDestBuffer,
+                           aData.mPicX,
+                           aData.mPicY,
+                           aData.mPicSize.width,
+                           aData.mPicSize.height,
+                           aData.mYStride,
+                           aData.mCbCrStride,
+                           aStride,
+                           yuvtype);
+    } else 
+#endif
+      ConvertYCbCrToRGB32(aData.mYChannel, 
+                          aData.mCbChannel,
+                          aData.mCrChannel,
+                          aDestBuffer,
+                          aData.mPicX,
+                          aData.mPicY,
+                          aData.mPicSize.width,
+                          aData.mPicSize.height,
+                          aData.mYStride,
+                          aData.mCbCrStride,
+                          aStride,
+                          yuvtype);
+  }
+}
+
+}
+}
---
+++ gfx/ycbcr/YCbCrUtils.h
@@ -0,0 +1,30 @@
+
+
+
+
+
+#ifndef MOZILLA_GFX_UTILS_H_
+#define MOZILLA_GFX_UTILS_H_
+
+#include "mozilla/gfx/Types.h"
+#include "ImageContainer.h"
+
+namespace mozilla {
+namespace gfx {
+
+void
+GetYCbCrToRGBDestFormatAndSize(const layers::PlanarYCbCrData& aData,
+                               SurfaceFormat& aSuggestedFormat,
+                               IntSize& aSuggestedSize);
+
+void
+ConvertYCbCrToRGB(const layers::PlanarYCbCrData& aData,
+                  const SurfaceFormat& aDestFormat,
+                  const IntSize& aDestSize,
+                  unsigned char* aDestBuffer,
+                  int32_t aStride);
+
+}
+}
+
+#endif 
