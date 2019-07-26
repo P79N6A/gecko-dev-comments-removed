bf686f38d72954c723499a6a1e34104977fa1eff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:37:51 +0000

    Bug 654352: Implement (blassey) and fix (jwir3) document.caretPositionFromPoint so that it utilizes frame-relative coordinates to get caret position. (original: [r=smaug,roc]) (fixes: [r=blassey,Ms2ger])
    UltraBlame original commit: e01ca7212c8a1dc5111ee698dc95c49b26f42da7

---
+++ content/base/src/nsDOMCaretPosition.cpp
@@ -0,0 +1,40 @@
+
+
+
+
+#include "nsDOMCaretPosition.h"
+#include "mozilla/dom/CaretPositionBinding.h"
+#include "nsContentUtils.h"
+
+nsDOMCaretPosition::nsDOMCaretPosition(nsINode* aNode, uint32_t aOffset)
+  : mOffset(aOffset), mOffsetNode(aNode)
+{
+  SetIsDOMBinding();
+}
+
+nsDOMCaretPosition::~nsDOMCaretPosition()
+{
+}
+
+nsINode* nsDOMCaretPosition::GetOffsetNode() const
+{
+  return mOffsetNode;
+}
+
+JSObject*
+nsDOMCaretPosition::WrapObject(JSContext *aCx, JSObject *aScope,
+                              bool *aTried)
+{
+  return mozilla::dom::CaretPositionBinding::Wrap(aCx, aScope, this, aTried);
+}
+
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_1(nsDOMCaretPosition, mOffsetNode)
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(nsDOMCaretPosition)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(nsDOMCaretPosition)
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(nsDOMCaretPosition)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_END
+
---
+++ content/base/src/nsDOMCaretPosition.h
@@ -0,0 +1,63 @@
+
+
+
+
+#ifndef nsDOMCaretPosition_h
+#define nsDOMCaretPosition_h
+
+#include "nsCycleCollectionParticipant.h"
+#include "nsCOMPtr.h"
+#include "nsINode.h"
+#include "nsWrapperCache.h"
+
+
+
+
+
+
+
+
+
+class nsDOMCaretPosition : public nsISupports,
+                           public nsWrapperCache
+{
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(nsDOMCaretPosition)
+
+  nsDOMCaretPosition(nsINode* aNode, uint32_t aOffset);
+
+  
+
+
+
+
+
+  uint32_t Offset() const { return mOffset; }
+
+  
+
+
+
+
+
+
+
+
+  nsINode* GetOffsetNode() const;
+
+  nsISupports* GetParentObject() const
+  {
+    return GetOffsetNode();
+  }
+
+  virtual JSObject* WrapObject(JSContext *aCx, JSObject *aScope, bool *aTried)
+    MOZ_OVERRIDE MOZ_FINAL;
+
+protected:
+  virtual ~nsDOMCaretPosition();
+  uint32_t mOffset;
+  nsCOMPtr<nsINode> mOffsetNode;
+};
+#endif
+
