ca3fdc425b7effdab66f73a336aae77168d3194d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:57:30 +0000

    Bug 970321 - Australis' UITour: make menu panel not break if tour tab is opened in new window, r=Unfocused
    UltraBlame original commit: 74f8a4d343a054fab5f166b2b731a07ef2d0e615

---
+++ browser/modules/test/browser_UITour_detach_tab.js
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+
+"use strict";
+
+let gTestTab;
+let gContentAPI;
+let gContentWindow;
+let gContentDoc;
+let highlight = document.getElementById("UITourHighlight");
+let tooltip = document.getElementById("UITourTooltip");
+
+Components.utils.import("resource:///modules/UITour.jsm");
+
+function test() {
+  registerCleanupFunction(function() {
+    gContentDoc = null;
+  });
+  UITourTest();
+}
+
+let tests = [
+  function test_move_tab_to_new_window(done) {
+    let gOpenedWindow;
+    let onVisibilityChange = (aEvent) => {
+      if (!document.hidden && window != UITour.getChromeWindow(aEvent.target)) {
+        gContentAPI.showHighlight("appMenu");
+      }
+    };
+    let onDOMWindowDestroyed = (aWindow, aTopic, aData) => {
+      if (gOpenedWindow && aWindow == gOpenedWindow) {
+        Services.obs.removeObserver(onDOMWindowDestroyed, "dom-window-destroyed", false);
+        done();
+      }
+    };
+    let onBrowserDelayedStartup = (aWindow, aTopic, aData) => {
+      gOpenedWindow = aWindow;
+      Services.obs.removeObserver(onBrowserDelayedStartup, "browser-delayed-startup-finished");
+      try {
+        let newWindowHighlight = gOpenedWindow.document.getElementById("UITourHighlight");
+        let selectedTab = aWindow.gBrowser.selectedTab;
+        is(selectedTab.linkedBrowser && selectedTab.linkedBrowser.contentDocument, gContentDoc, "Document should be selected in new window");
+        ok(UITour.originTabs && UITour.originTabs.has(aWindow), "Window should be known");
+        ok(UITour.originTabs.get(aWindow).has(selectedTab), "Tab should be known");
+        waitForElementToBeVisible(newWindowHighlight, function checkHighlightIsThere() {
+          gContentAPI.showMenu("appMenu");
+          isnot(aWindow.PanelUI.panel.state, "closed", "Panel should be open");
+          ok(aWindow.PanelUI.contents.children.length > 0, "Panel contents should have children");
+          gContentAPI.hideHighlight();
+          gContentAPI.hideMenu("appMenu");
+          gTestTab = null;
+          aWindow.close();
+        }, "Highlight should be shown in new window.");
+      } catch (ex) {
+        Cu.reportError(ex);
+        ok(false, "An error occurred running UITour tab detach test.");
+      } finally {
+        gContentDoc.removeEventListener("visibilitychange", onVisibilityChange, false);
+        Services.obs.addObserver(onDOMWindowDestroyed, "dom-window-destroyed", false);
+      }
+    };
+
+    Services.obs.addObserver(onBrowserDelayedStartup, "browser-delayed-startup-finished", false);
+    
+    
+    
+    gContentDoc = gBrowser.selectedTab.linkedBrowser.contentDocument;
+    gContentDoc.addEventListener("visibilitychange", onVisibilityChange, false);
+    gContentAPI.showHighlight("appMenu");
+    waitForElementToBeVisible(highlight, function checkForInitialHighlight() {
+      gBrowser.replaceTabWithWindow(gBrowser.selectedTab);
+    });
+
+  },
+];
+
