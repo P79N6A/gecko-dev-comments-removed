26b3602a76246f138d8e02883e062b9f51891397
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:10:08 +0000

    Bug 891954 - Skip Necko IPC security checks for desktop Firefox (r=jduell)
    UltraBlame original commit: 44b9c24077812f06afad46469081d867d6e6b3b2

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1283,3 +1283,7 @@ pref("dom.debug.propagate_gesture_events_through_content", false);
 
 
 pref("geo.wifi.uri", "https://www.googleapis.com/geolocation/v1/geolocate?key=%GOOGLE_API_KEY%");
+
+
+
+pref("network.disable.ipc.security", true);
