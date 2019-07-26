75ce4cdb16e790dfeed3ec15aea2cbd2b9880b82
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:18:05 +0000

    Bug 811635 - Part 5: Start wpa_supplicant with proper 'p2pSupported' flag. r=vchang
    UltraBlame original commit: 3f7ee595646d5f06d63ae5194633c9c9e802be47

--- dom/wifi/WifiUtils.cpp
+++ dom/wifi/WifiUtils.cpp
@@ -5,6 +5,7 @@
 #include "WifiUtils.h"
 #include <dlfcn.h>
 #include <errno.h>
+#include <cutils/properties.h>
 #include "prinit.h"
 #include "js/CharacterEncoding.h"
 #include "NetUtils.h"
@@ -32,6 +33,14 @@ GetSharedLibrary()
 {
   PR_CallOnce(&sInitWifiLib, InitWifiLib);
   return sWifiLib;
+}
+
+static bool
+GetWifiP2pSupported()
+{
+  char propP2pSupported[PROPERTY_VALUE_MAX];
+  property_get("ro.moz.wifi.p2p_supported", propP2pSupported, "0");
+  return (0 == strcmp(propP2pSupported, "1"));
 }
 
 
@@ -141,6 +150,7 @@ class ICSWpaSupplicantImpl : public WpaSupplicantImpl
 public:
   DEFAULT_IMPL(wifi_load_driver, int32_t, )
   DEFAULT_IMPL(wifi_unload_driver, int32_t, )
+  DEFAULT_IMPL(wifi_stop_supplicant, int32_t, )
 
   DEFINE_DLFUNC(wifi_wait_for_event, int32_t, char*, size_t)
   int32_t do_wifi_wait_for_event(const char *iface, char *buf, size_t len) {
@@ -158,12 +168,6 @@ public:
   int32_t do_wifi_start_supplicant(int32_t) {
     USE_DLFUNC(wifi_start_supplicant)
     return wifi_start_supplicant();
-  }
-
-  DEFINE_DLFUNC(wifi_stop_supplicant, int32_t)
-  int32_t do_wifi_stop_supplicant(int32_t) {
-    USE_DLFUNC(wifi_stop_supplicant)
-    return wifi_stop_supplicant();
   }
 
   DEFINE_DLFUNC(wifi_connect_to_supplicant, int32_t, )
@@ -202,12 +206,6 @@ public:
     return wifi_start_supplicant(arg);
   }
 
-  DEFINE_DLFUNC(wifi_stop_supplicant, int32_t, int32_t)
-  int32_t do_wifi_stop_supplicant(int32_t arg) {
-    USE_DLFUNC(wifi_stop_supplicant)
-    return wifi_stop_supplicant(arg);
-  }
-
   DEFINE_DLFUNC(wifi_connect_to_supplicant, int32_t, const char*)
   int32_t do_wifi_connect_to_supplicant(const char* iface) {
     USE_DLFUNC(wifi_connect_to_supplicant)
@@ -222,32 +220,12 @@ public:
 };
 
 
-
-class KKWpaSupplicantImpl : public ICSWpaSupplicantImpl
-{
-public:
-  DEFINE_DLFUNC(wifi_start_supplicant, int32_t, int32_t)
-  int32_t do_wifi_start_supplicant(int32_t arg) {
-    USE_DLFUNC(wifi_start_supplicant)
-    return wifi_start_supplicant(arg);
-  }
-
-  DEFINE_DLFUNC(wifi_stop_supplicant, int32_t, int32_t)
-  int32_t do_wifi_stop_supplicant(int32_t arg) {
-    USE_DLFUNC(wifi_stop_supplicant)
-    return wifi_stop_supplicant(arg);
-  }
-};
-
-
 WpaSupplicant::WpaSupplicant()
 {
   if (NetUtils::SdkVersion() < 16) {
     mImpl = new ICSWpaSupplicantImpl();
-  } else if (NetUtils::SdkVersion() < 19) {
-    mImpl = new JBWpaSupplicantImpl();
   } else {
-    mImpl = new KKWpaSupplicantImpl();
+    mImpl = new JBWpaSupplicantImpl();
   }
   mNetUtils = new NetUtils();
 };
@@ -307,9 +285,9 @@ bool WpaSupplicant::ExecuteCommand(CommandOptions aOptions,
   } else if (aOptions.mCmd.EqualsLiteral("unload_driver")) {
     aResult.mStatus = mImpl->do_wifi_unload_driver();
   } else if (aOptions.mCmd.EqualsLiteral("start_supplicant")) {
-    aResult.mStatus = mImpl->do_wifi_start_supplicant(0);
+    aResult.mStatus = mImpl->do_wifi_start_supplicant(GetWifiP2pSupported() ? 1 : 0);
   } else if (aOptions.mCmd.EqualsLiteral("stop_supplicant")) {
-    aResult.mStatus = mImpl->do_wifi_stop_supplicant(0);
+    aResult.mStatus = mImpl->do_wifi_stop_supplicant();
   } else if (aOptions.mCmd.EqualsLiteral("connect_to_supplicant")) {
     aResult.mStatus = mImpl->do_wifi_connect_to_supplicant(aInterface.get());
   } else if (aOptions.mCmd.EqualsLiteral("ifc_enable")) {
