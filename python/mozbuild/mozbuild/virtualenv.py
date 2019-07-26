e56dc432ec84ad4c510ab293f038727b20d4181e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:21:37 +0000

    Bug 987984 - VirtualenvManager.call_setup() should use self.python_path instead of sys.executable, r=gps
    UltraBlame original commit: 74f3ae65bdf6f554ba98c58dee02060e7dc5cdb0

--- python/mozbuild/mozbuild/virtualenv.py
+++ python/mozbuild/mozbuild/virtualenv.py
@@ -343,7 +343,7 @@ class VirtualenvManager(object):
         """Calls setup.py in a directory."""
         setup = os.path.join(directory, 'setup.py')
 
-        program = [sys.executable, setup]
+        program = [self.python_path, setup]
         program.extend(arguments)
 
         
