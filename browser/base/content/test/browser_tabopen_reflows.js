0b15c4a312ee9d31492b097067bc3bec44040d86
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:44:58 +0000

    Bug 878747 - part 2 - use the new 'nodefaultsrc' attribute for browsers; r=dao
    UltraBlame original commit: 73483f4906a71db306ad9a996aee6840250c0db3

--- browser/base/content/test/browser_tabopen_reflows.js
+++ browser/base/content/test/browser_tabopen_reflows.js
@@ -9,10 +9,6 @@ XPCOMUtils.defineLazyGetter(this, "docShell", () => {
 
 const EXPECTED_REFLOWS = [
   
-  "stop@chrome://global/content/bindings/browser.xml|" +
-    "addTab@chrome://browser/content/tabbrowser.xml|",
-
-  
   "adjustTabstrip@chrome://browser/content/tabbrowser.xml|" +
     "_handleNewTab@chrome://browser/content/tabbrowser.xml|" +
     "onxbltransitionend@chrome://browser/content/tabbrowser.xml|",
@@ -30,7 +26,20 @@ const EXPECTED_REFLOWS = [
   "get_scrollPosition@chrome://global/content/bindings/scrollbox.xml|" +
     "_fillTrailingGap@chrome://browser/content/tabbrowser.xml|" +
     "_handleNewTab@chrome://browser/content/tabbrowser.xml|" +
-    "onxbltransitionend@chrome://browser/content/tabbrowser.xml|"
+    "onxbltransitionend@chrome://browser/content/tabbrowser.xml|",
+
+  
+  "iQClass_height@chrome://browser/content/tabview.js|" +
+    "GroupItem_getContentBounds@chrome://browser/content/tabview.js|" +
+    "GroupItem_shouldStack@chrome://browser/content/tabview.js|" +
+    "GroupItem_arrange@chrome://browser/content/tabview.js|" +
+    "GroupItem_add@chrome://browser/content/tabview.js|" +
+    "GroupItems_newTab@chrome://browser/content/tabview.js|" +
+    "TabItem__reconnect@chrome://browser/content/tabview.js|" +
+    "TabItem@chrome://browser/content/tabview.js|" +
+    "TabItems_link@chrome://browser/content/tabview.js|" +
+    "@chrome://browser/content/tabview.js|" +
+    "addTab@chrome://browser/content/tabbrowser.xml|"
 ];
 
 const PREF_PRELOAD = "browser.newtab.preload";
