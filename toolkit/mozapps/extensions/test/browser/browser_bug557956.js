4f69a17f812f9cf00b3da861b967524b50816a2c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:55:05 +0000

    Bug 1000695 - Part 2: Yield to event loop after receiving onInstallEnded, rewrite to Task.jsm. r=unfocused
    UltraBlame original commit: 0552c02f54b056d9b453fbeb21cb25d2153b2049

--- toolkit/mozapps/extensions/test/browser/browser_bug557956.js
+++ toolkit/mozapps/extensions/test/browser/browser_bug557956.js
@@ -75,7 +75,7 @@ function install_test_addons(aCallback) {
         
         Services.prefs.setCharPref(PREF_UPDATEURL, TESTROOT + "browser_bug557956.rdf");
 
-        aCallback();
+        executeSoon(aCallback);
       }
     }
   };
