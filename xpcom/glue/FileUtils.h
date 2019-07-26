6e871a105d0edaa7c5f495f9e307d27f92626321
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:03:01 +0000

    Bug 845907: Adds generic ReadAhead functions r=glandium
    UltraBlame original commit: 779b7ec8c9ecee3fa60668ba69d447be58f584f0

--- xpcom/glue/FileUtils.h
+++ xpcom/glue/FileUtils.h
@@ -16,9 +16,40 @@
 #include "prio.h"
 
 #include "mozilla/Scoped.h"
+#include "nsIFile.h"
 #include <errno.h>
+#include <limits.h>
 
 namespace mozilla {
+
+#if defined(XP_WIN)
+typedef void* filedesc_t;
+typedef const wchar_t* pathstr_t;
+#else
+typedef int filedesc_t;
+typedef const char* pathstr_t;
+#endif
+
+
+
+
+
+
+struct ScopedCloseFDTraits
+{
+  typedef int type;
+  static type empty() { return -1; }
+  static void release(type fd) {
+    if (fd != -1) {
+      while ((close(fd) == -1) && (errno == EINTR)) {
+        ;
+      }
+    }
+  }
+};
+typedef Scoped<ScopedCloseFDTraits> ScopedClose;
+
+#if !defined(XPCOM_GLUE)
 
 
 
@@ -42,30 +73,77 @@ typedef Scoped<ScopedClosePRFDTraits> AutoFDClose;
 
 
 
-struct ScopedCloseFDTraits
-{
-  typedef int type;
-  static type empty() { return -1; }
-  static void release(type fd) {
-    if (fd != -1) {
-      while ((close(fd) == -1) && (errno == EINTR)) {
-        ;
-      }
-    }
-  }
-};
-typedef Scoped<ScopedCloseFDTraits> ScopedClose;
-
-
-
-
-
-
 
 
 
 
 NS_COM_GLUE bool fallocate(PRFileDesc *aFD, int64_t aLength);
+
+
+
+
+
+
+
+
+NS_COM_GLUE void ReadAheadLib(nsIFile* aFile);
+
+
+
+
+
+
+
+
+
+
+
+
+NS_COM_GLUE void ReadAheadFile(nsIFile* aFile, const size_t aOffset = 0,
+                               const size_t aCount = SIZE_MAX,
+                               filedesc_t* aOutFd = nullptr);
+
+#endif 
+
+
+
+
+
+
+
+
+NS_COM_GLUE void ReadAheadLib(pathstr_t aFilePath);
+
+
+
+
+
+
+
+
+
+
+
+
+NS_COM_GLUE void ReadAheadFile(pathstr_t aFilePath, const size_t aOffset = 0,
+                               const size_t aCount = SIZE_MAX,
+                               filedesc_t* aOutFd = nullptr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+NS_COM_GLUE void ReadAhead(filedesc_t aFd, const size_t aOffset = 0,
+                           const size_t aCount = SIZE_MAX);
+
 
 } 
 #endif
