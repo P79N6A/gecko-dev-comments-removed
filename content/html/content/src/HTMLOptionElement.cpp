b3d9286594baa6455d941b9c165f0291b946479c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:27:47 +0000

    Bug 927796.  Make sure we don't end up with options in a bogus state where their DefaultSelected() doesn't match mIsSelected but they think their mIsSelected value hasn't changed from the default.  r=sicking
    UltraBlame original commit: 7a04c52fe9d4f21be30ab90797e775bea50d9836

--- content/html/content/src/HTMLOptionElement.cpp
+++ content/html/content/src/HTMLOptionElement.cpp
@@ -211,7 +211,8 @@ HTMLOptionElement::BeforeSetAttr(int32_t aNamespaceID, nsIAtom* aName,
 
   int32_t index = Index();
   uint32_t mask = HTMLSelectElement::SET_DISABLED;
-  if (aValue) {
+  bool defaultSelected = aValue;
+  if (defaultSelected) {
     mask |= HTMLSelectElement::IS_SELECTED;
   }
 
@@ -227,8 +228,10 @@ HTMLOptionElement::BeforeSetAttr(int32_t aNamespaceID, nsIAtom* aName,
   
   
   mIsInSetDefaultSelected = inSetDefaultSelected;
-  mSelectedChanged = false;
   
+  
+  
+  mSelectedChanged = mIsSelected != defaultSelected;
 
   return NS_OK;
 }
