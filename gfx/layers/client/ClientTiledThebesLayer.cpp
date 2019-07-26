760580f510350688f57acad394a1bacd457c4a14
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:52:49 +0000

    Bug 883646 - Scale the critical displayport by the widget scale as well. r=Cwiiis
    UltraBlame original commit: b91036bab3f7e3747fe98b81d373d0c8c383ff95

--- gfx/layers/client/ClientTiledThebesLayer.cpp
+++ gfx/layers/client/ClientTiledThebesLayer.cpp
@@ -57,8 +57,9 @@ ClientTiledThebesLayer::BeginPaint()
 
   
   mPaintData.mLayerCriticalDisplayPort.SetEmpty();
+  const FrameMetrics& metrics = GetParent()->GetFrameMetrics();
   const gfx::Rect& criticalDisplayPort =
-    GetParent()->GetFrameMetrics().mCriticalDisplayPort.ToUnknownRect();
+    (metrics.mCriticalDisplayPort * metrics.mDevPixelsPerCSSPixel).ToUnknownRect();
   if (!criticalDisplayPort.IsEmpty()) {
     gfxRect transformedCriticalDisplayPort =
       mPaintData.mTransformScreenToLayer.TransformBounds(
