2f373132b8faff68ca583bad5f06be096531dbf8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:56:10 +0000

    Bug 973832 - Update NetUtils for gonk-kk. r=vchang
    UltraBlame original commit: 57c2112ea885b571b16fb744f97f90c2559e56d4

--- dom/network/src/NetUtils.cpp
+++ dom/network/src/NetUtils.cpp
@@ -174,6 +174,15 @@ int32_t NetUtils::do_dhcp_do_request(const char *ifname,
     char domains[PROPERTY_VALUE_MAX];
     ret = dhcp_do_request(ifname, ipaddr, gateway, prefixLength, dns,
                           server, lease, vendorinfo, domains);
+  } else if (sdkVersion == 19) {
+    
+    
+    DEFINE_DLFUNC(dhcp_do_request, int32_t, const char*, char*, char*,  uint32_t*, char**, char*, uint32_t*, char*, char*, char*)
+    USE_DLFUNC(dhcp_do_request)
+    char *dns[3] = {dns1, dns2, nullptr};
+    char domains[PROPERTY_VALUE_MAX];
+    char mtu[PROPERTY_VALUE_MAX];
+    ret = dhcp_do_request(ifname, ipaddr, gateway, prefixLength, dns, server, lease, vendorinfo, domains, mtu);
   } else {
     NS_WARNING("Unable to perform do_dhcp_request: unsupported sdk version!");
   }
