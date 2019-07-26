5bbf1817b91e0da347d5e34822c52ff3bb9d5e1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:19:27 +0000

    Bug 982359 - Increase timeout for browser_net_filter-02.js to avoid it failing on Ubuntu
    UltraBlame original commit: 884a711d95479b1553162c5f5cb8c96ccb51bc00

--- browser/devtools/netmonitor/test/browser_net_filter-02.js
+++ browser/devtools/netmonitor/test/browser_net_filter-02.js
@@ -9,6 +9,9 @@ function test() {
   initNetMonitor(FILTERING_URL).then(([aTab, aDebuggee, aMonitor]) => {
     info("Starting test... ");
 
+    
+    requestLongerTimeout(2);
+
     let { $, NetMonitorView } = aMonitor.panelWin;
     let { RequestsMenu } = NetMonitorView;
 
