126b21dc396c29ce3567bfaaafa78bb09fab4302
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:38:42 +0000

    Bug 912524: Do not reinflate SuggestionOptIn ViewStub. r=bnicholson
    UltraBlame original commit: 6d8738e8dc7118fef4855c98127e58075a320b94

--- mobile/android/base/home/BrowserSearch.java
+++ mobile/android/base/home/BrowserSearch.java
@@ -457,6 +457,12 @@ public class BrowserSearch extends HomeFragment
     }
 
     private void showSuggestionsOptIn() {
+        
+        
+        if (mSuggestionsOptInPrompt != null) {
+            return;
+        }
+
         mSuggestionsOptInPrompt = ((ViewStub) mView.findViewById(R.id.suggestions_opt_in_prompt)).inflate();
 
         TextView promptText = (TextView) mSuggestionsOptInPrompt.findViewById(R.id.suggestions_prompt_title);
