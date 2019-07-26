5337c6fd76f9f7ca713e3114220a702f52c8fdd2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:22:36 +0000

    Bug 968308 - Part 2: Session for about:home. r=liuche
    UltraBlame original commit: ea74c69f4dbf3061b37f63f4d8bfed45b549193c

---
+++ mobile/android/base/TelemetryContract.java
@@ -0,0 +1,39 @@
+
+
+
+
+
+package org.mozilla.gecko;
+
+
+
+
+public interface TelemetryContract {
+
+    
+
+
+
+    public interface Event {}
+
+    
+
+
+
+    public interface Method {}
+
+    
+
+
+
+    public interface Session {
+        
+        public static final String HOME = "home.v1";
+    }
+
+    
+
+
+
+    public interface Reason {}
+}
