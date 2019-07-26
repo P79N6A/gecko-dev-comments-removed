145a2473a67f6c8f5deca6b499d362b632585644
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:19:02 +0000

    Bug 1022577 - Ensure we flush the repaint _after_ taking the scroll offset from content. r=botond
    UltraBlame original commit: fc3a95387d57c868a717f46a78d19c18ef3d420a

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -2261,13 +2261,16 @@ void AsyncPanZoomController::NotifyLayersUpdated(const FrameMetrics& aLayerMetri
     mFrameMetrics.mHasScrollgrab = aLayerMetrics.mHasScrollgrab;
 
     if (scrollOffsetUpdated) {
-      CancelAnimation();
-
       APZC_LOG("%p updating scroll offset from (%f, %f) to (%f, %f)\n", this,
         mFrameMetrics.GetScrollOffset().x, mFrameMetrics.GetScrollOffset().y,
         aLayerMetrics.GetScrollOffset().x, aLayerMetrics.GetScrollOffset().y);
 
       mFrameMetrics.CopyScrollInfoFrom(aLayerMetrics);
+
+      
+      
+      
+      CancelAnimation();
 
       
       
