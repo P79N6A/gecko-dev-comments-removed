b702714203c368d855289afaf45e60f0ac41808d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:44:48 +0000

    Bug 877654 - Remove thisptr offset tables - Replace HTML element offset table macros with generic QI macros. r=Ms2ger.
    UltraBlame original commit: 51fc52f897289d993d377bf86216468a498d479d

--- content/html/content/src/HTMLSelectElement.cpp
+++ content/html/content/src/HTMLSelectElement.cpp
@@ -149,12 +149,11 @@ NS_IMPL_ADDREF_INHERITED(HTMLSelectElement, Element)
 NS_IMPL_RELEASE_INHERITED(HTMLSelectElement, Element)
 
 
-
 NS_INTERFACE_TABLE_HEAD_CYCLE_COLLECTION_INHERITED(HTMLSelectElement)
   NS_HTML_CONTENT_INTERFACES(nsGenericHTMLFormElement)
-  NS_HTML_CONTENT_INTERFACE_TABLE2(HTMLSelectElement,
-                                   nsIDOMHTMLSelectElement,
-                                   nsIConstraintValidation)
+  NS_INTERFACE_TABLE_INHERITED2(HTMLSelectElement,
+                                nsIDOMHTMLSelectElement,
+                                nsIConstraintValidation)
   NS_HTML_CONTENT_INTERFACE_TABLE_TO_MAP_SEGUE(HTMLSelectElement,
                                                nsGenericHTMLFormElement)
 NS_HTML_CONTENT_INTERFACE_MAP_END
