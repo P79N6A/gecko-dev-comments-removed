e0cf582ff851dcaf4dfb43c2eef64bbb3341fb96
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:06:37 +0000

    Bug 770778 - Make TCPSocket e10s-friendly. r=bent
    UltraBlame original commit: 806fa3680a9138de39b0441ef2bb139069c08772

---
+++ dom/network/src/TCPSocketParent.h
@@ -0,0 +1,49 @@
+
+
+
+
+#include "mozilla/net/PTCPSocketParent.h"
+#include "nsITCPSocketParent.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsCOMPtr.h"
+#include "nsIDOMTCPSocket.h"
+
+class JSContext;
+class JSObject;
+
+namespace mozilla {
+namespace dom {
+
+class PBrowserParent;
+
+class TCPSocketParent : public mozilla::net::PTCPSocketParent
+                      , public nsITCPSocketParent
+{
+public:
+  NS_DECL_CYCLE_COLLECTION_CLASS(TCPSocketParent)
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_NSITCPSOCKETPARENT
+
+  TCPSocketParent() : mIntermediaryObj(nullptr), mIPCOpen(true) {}
+
+  bool Init(const nsString& aHost, const uint16_t& aPort,
+            const bool& useSSL, const nsString& aBinaryType,
+            PBrowserParent* aBrowser);
+
+  virtual bool RecvSuspend() MOZ_OVERRIDE;
+  virtual bool RecvResume() MOZ_OVERRIDE;
+  virtual bool RecvClose() MOZ_OVERRIDE;
+  virtual bool RecvData(const SendableData& aData) MOZ_OVERRIDE;
+  virtual bool RecvRequestDelete() MOZ_OVERRIDE;
+
+private:
+  virtual void ActorDestroy(ActorDestroyReason why) MOZ_OVERRIDE;
+
+  nsCOMPtr<nsITCPSocketIntermediary> mIntermediary;
+  nsCOMPtr<nsIDOMTCPSocket> mSocket;
+  JSObject* mIntermediaryObj;
+  bool mIPCOpen;
+};
+
+} 
+} 
---
+++ dom/network/src/TCPSocketParentIntermediary.js
@@ -0,0 +1,55 @@
+
+
+
+
+"use strict";
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+function TCPSocketParentIntermediary() {
+}
+
+TCPSocketParentIntermediary.prototype = {
+  open: function(aParentSide, aHost, aPort, aUseSSL, aBinaryType) {
+    aParentSide.initJS(this);
+
+    let baseSocket = Cc["@mozilla.org/tcp-socket;1"].createInstance(Ci.nsIDOMTCPSocket);
+    let socket = this._socket = baseSocket.open(aHost, aPort,
+                                                {useSSL: aUseSSL,
+                                                binaryType: aBinaryType});
+    if (!socket)
+      return null;
+
+    
+    
+    ["onopen", "ondrain", "ondata", "onerror", "onclose"].forEach(
+      function(p) {
+        socket[p] = function(data) {
+          aParentSide.sendCallback(p, data.data, socket.readyState,
+                                   socket.bufferedAmount);
+        };
+      }
+    );
+
+    return socket;
+  },
+
+  sendString: function(aData) {
+    return this._socket.send(aData);
+  },
+
+  sendArrayBuffer: function(aData) {
+    return this._socket.send(aData);
+  },
+
+  classID: Components.ID("{afa42841-a6cb-4a91-912f-93099f6a3d18}"),
+  QueryInterface: XPCOMUtils.generateQI([
+    Ci.nsITCPSocketIntermediary
+  ])
+};
+
+const NSGetFactory = XPCOMUtils.generateNSGetFactory([TCPSocketParentIntermediary]);
