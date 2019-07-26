a4221646af336469a56d24869eeeeeda94918883
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:18:54 +0000

    Bug 925416 - Report on memory used by blob URLs. r=khuey
    By default, the reporter path includes the blob owner's URI and the
    blob URI in the path.  If a blob has multiple references, its size is
    divided among them so that the totals still work; the blob's address and
    refcount are included in the path (to find the other refs), and all of
    this is noted in the description.
    
    If memory.blob_report.stack_frames is a nonzero value, that many JS
    stack frames (file and line number, if available) will be added to the
    path, with the file root-relative to the owner if same-origin.
    
    UltraBlame original commit: 1e90119dec6fb10da60b86b5ced113598107da05

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -4294,6 +4294,9 @@ pref("memory.free_dirty_pages", false);
 pref("memory.system_memory_reporter", false);
 #endif
 
+
+pref("memory.blob_report.stack_frames", 0);
+
 pref("social.enabled", false);
 
 
