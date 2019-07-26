ece817229c5315ac05bc904501a11df6cdbd046c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:19:28 +0000

    Bug 793013 - Add EnumSet to replace 1 << n bit type bitfields. r=Waldo
    UltraBlame original commit: 40a702087f7d20d967aeff97c9e3de533b9063ba

---
+++ mfbt/EnumSet.h
@@ -0,0 +1,175 @@
+
+
+
+
+
+
+
+#ifndef mozilla_EnumSet_h
+#define mozilla_EnumSet_h
+
+#include "mozilla/Assertions.h"
+#include "mozilla/StandardInteger.h"
+
+namespace mozilla {
+
+
+
+
+
+
+template<typename T>
+class EnumSet
+{
+  public:
+    EnumSet()
+      : mBitField(0)
+    { }
+
+    EnumSet(T aEnum)
+      : mBitField(aEnum)
+    { }
+
+    EnumSet(T aEnum1, T aEnum2)
+      : mBitField(bitFor(aEnum1) |
+                  bitFor(aEnum2))
+    { }
+
+    EnumSet(T aEnum1, T aEnum2, T aEnum3)
+      : mBitField(bitFor(aEnum1) |
+                  bitFor(aEnum2) |
+                  bitFor(aEnum3))
+    { }
+
+    EnumSet(T aEnum1, T aEnum2, T aEnum3, T aEnum4)
+     : mBitField(bitFor(aEnum1) |
+                 bitFor(aEnum2) |
+                 bitFor(aEnum3) |
+                 bitFor(aEnum4))
+    { }
+
+    EnumSet(const EnumSet& aEnumSet)
+     : mBitField(aEnumSet.mBitField)
+    { }
+
+    
+
+
+    void operator+=(T aEnum) {
+      mBitField |= bitFor(aEnum);
+    }
+
+    
+
+
+    EnumSet<T> operator+(T aEnum) const {
+      EnumSet<T> result(*this);
+      result += aEnum;
+      return result;
+    }
+
+    
+
+
+    void operator+=(const EnumSet<T> aEnumSet) {
+      mBitField |= aEnumSet.mBitField;
+    }
+
+    
+
+
+    EnumSet<T> operator+(const EnumSet<T> aEnumSet) const {
+      EnumSet<T> result(*this);
+      result += aEnumSet;
+      return result;
+    }
+
+    
+
+
+    void operator-=(T aEnum) {
+      mBitField &= ~(bitFor(aEnum));
+    }
+
+    
+
+
+    EnumSet<T> operator-(T aEnum) const {
+      EnumSet<T> result(*this);
+      result -= aEnum;
+      return result;
+    }
+
+    
+
+
+    void operator-=(const EnumSet<T> aEnumSet) {
+      mBitField &= ~(aEnumSet.mBitField);
+    }
+
+    
+
+
+    EnumSet<T> operator-(const EnumSet<T> aEnumSet) const {
+      EnumSet<T> result(*this);
+      result -= aEnumSet;
+      return result;
+    }
+
+    
+
+
+    void operator&=(const EnumSet<T> aEnumSet) {
+      mBitField &= aEnumSet.mBitField;
+    }
+
+    
+
+
+    EnumSet<T> operator&(const EnumSet<T> aEnumSet) const {
+      EnumSet<T> result(*this);
+      result &= aEnumSet;
+      return result;
+    }
+
+    
+
+
+
+    bool operator==(const EnumSet<T> aEnumSet) const {
+      return mBitField == aEnumSet.mBitField;
+    }
+
+    
+
+
+    bool contains(T aEnum) const {
+      return mBitField & bitFor(aEnum);
+    }
+
+    
+
+
+
+    uint8_t size() {
+      uint8_t count = 0;
+      for (uint32_t bitField = mBitField; bitField; bitField >>= 1) {
+        if (bitField & 1)
+          count++;
+      }
+      return count;
+    }
+
+  private:
+    static uint32_t bitFor(T aEnum) {
+      uint32_t bitNumber(aEnum);
+      MOZ_ASSERT(bitNumber < 32);
+      return 1U << bitNumber;
+    }
+
+    uint32_t mBitField;
+};
+
+} 
+
+#endif 
