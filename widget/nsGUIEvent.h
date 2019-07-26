d377cca41975e8a7b1e13c618074d3d0684c4ec4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:32:35 +0000

    Bug 910978 part.10 Implement nsTouchEvent::AssignTouchEventData() r=smaug
    UltraBlame original commit: 0dd47ac83acae23d4cdc10a90eed854a33d382a1

--- widget/nsGUIEvent.h
+++ widget/nsGUIEvent.h
@@ -1785,6 +1785,13 @@ public:
   }
 
   nsTArray< nsRefPtr<mozilla::dom::Touch> > touches;
+
+  void AssignTouchEventData(const nsTouchEvent& aEvent, bool aCopyTargets)
+  {
+    AssignInputEventData(aEvent, aCopyTargets);
+
+    
+  }
 };
 
 
