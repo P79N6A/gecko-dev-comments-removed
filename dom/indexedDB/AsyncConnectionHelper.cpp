392114b6fc944399d1c72d79465c3f4d953cb16e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:21:47 +0000

    Bug 861287 - 'Integrate IndexedDB into the gecko profiler'. r=khuey.
    UltraBlame original commit: 8dbc29b317ec43383d25734d21318605763c18bc

--- dom/indexedDB/AsyncConnectionHelper.cpp
+++ dom/indexedDB/AsyncConnectionHelper.cpp
@@ -19,6 +19,7 @@
 #include "IDBEvents.h"
 #include "IDBTransaction.h"
 #include "IndexedDatabaseManager.h"
+#include "ProfilerHelpers.h"
 #include "TransactionThreadPool.h"
 
 #include "ipc/IndexedDBChild.h"
@@ -191,6 +192,8 @@ NS_IMETHODIMP
 AsyncConnectionHelper::Run()
 {
   if (NS_IsMainThread()) {
+    PROFILER_MAIN_THREAD_LABEL("IndexedDB", "AsyncConnectionHelper::Run");
+
     if (mTransaction &&
         mTransaction->IsAborted()) {
       
@@ -220,6 +223,11 @@ AsyncConnectionHelper::Run()
           if (NS_SUCCEEDED(mResultCode) && NS_FAILED(rv)) {
             mResultCode = rv;
           }
+
+          IDB_PROFILER_MARK("IndexedDB Request %llu: Running main thread "
+                            "response (rv = %lu)",
+                            "IDBRequest[%llu] MT Done",
+                            mRequest->GetSerialNumber(), mResultCode);
         }
 
         
@@ -265,6 +273,13 @@ AsyncConnectionHelper::Run()
   }
 
   NS_ASSERTION(IndexedDatabaseManager::IsMainProcess(), "Wrong process!");
+
+  PROFILER_LABEL("IndexedDB", "AsyncConnectionHelper::Run");
+
+  IDB_PROFILER_MARK_IF(mRequest,
+                       "IndexedDB Request %llu: Beginning database work",
+                       "IDBRequest[%llu] DT Start",
+                       mRequest->GetSerialNumber());
 
   nsresult rv = NS_OK;
   nsCOMPtr<mozIStorageConnection> connection;
@@ -340,6 +355,12 @@ AsyncConnectionHelper::Run()
     }
 #endif
   }
+
+  IDB_PROFILER_MARK_IF(mRequest,
+                       "IndexedDB Request %llu: Finished database work "
+                       "(rv = %lu)",
+                       "IDBRequest[%llu] DT Done", mRequest->GetSerialNumber(),
+                       mResultCode);
 
   return NS_DispatchToMainThread(this, NS_DISPATCH_NORMAL);
 }
@@ -433,6 +454,8 @@ AsyncConnectionHelper::OnSuccess()
   NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
   NS_ASSERTION(mRequest, "Null request!");
 
+  PROFILER_MAIN_THREAD_LABEL("IndexedDB", "AsyncConnectionHelper::OnSuccess");
+
   nsRefPtr<nsIDOMEvent> event = CreateSuccessEvent(mRequest);
   if (!event) {
     NS_ERROR("Failed to create event!");
@@ -466,6 +489,8 @@ AsyncConnectionHelper::OnError()
 {
   NS_ASSERTION(NS_IsMainThread(), "Wrong thread!");
   NS_ASSERTION(mRequest, "Null request!");
+
+  PROFILER_MAIN_THREAD_LABEL("IndexedDB", "AsyncConnectionHelper::OnError");
 
   
   nsRefPtr<nsIDOMEvent> event =
@@ -555,6 +580,11 @@ AsyncConnectionHelper::MaybeSendResponseToChildProcess(nsresult aResultCode)
   if (!actor) {
     return Success_NotSent;
   }
+
+  IDB_PROFILER_MARK("IndexedDB Request %llu: Sending response to child "
+                    "process (rv = %lu)",
+                    "IDBRequest[%llu] MT Done",
+                    mRequest->GetSerialNumber(), aResultCode);
 
   return SendResponseToChildProcess(aResultCode);
 }
---
+++ dom/indexedDB/ProfilerHelpers.h
@@ -0,0 +1,203 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_indexeddb_profilerhelpers_h__
+#define mozilla_dom_indexeddb_profilerhelpers_h__
+
+#include "GeckoProfiler.h"
+
+
+
+
+
+
+
+
+#if defined(IDB_PROFILER_USE_MARKS) && !defined(MOZ_ENABLE_PROFILER_SPS)
+#error Cannot use IDB_PROFILER_USE_MARKS without MOZ_ENABLE_PROFILER_SPS!
+#endif
+
+#if defined(IDB_PROFILER_MARK_DETAILS) && !defined(IDB_PROFILER_USE_MARKS)
+#error Cannot use IDB_PROFILER_MARK_DETAILS without IDB_PROFILER_USE_MARKS!
+#endif
+
+#ifdef IDB_PROFILER_USE_MARKS
+
+#ifdef IDB_PROFILER_MARK_DETAILS
+
+#include "IDBCursor.h"
+#include "IDBDatabase.h"
+#include "IDBIndex.h"
+#include "IDBKeyRange.h"
+#include "IDBObjectStore.h"
+#include "IDBTransaction.h"
+#include "Key.h"
+
+BEGIN_INDEXEDDB_NAMESPACE
+
+class ProfilerString : public nsAutoCString
+{
+  static const char kQuote = '\"';
+  static const char kOpenBracket = '[';
+  static const char kCloseBracket = ']';
+  static const char kOpenParen = '(';
+  static const char kCloseParen = ')';
+
+public:
+  explicit
+  ProfilerString(IDBDatabase* aDatabase)
+  {
+    MOZ_ASSERT(aDatabase);
+
+    Append(kQuote);
+    AppendUTF16toUTF8(aDatabase->Name(), *this);
+    Append(kQuote);
+  }
+
+  explicit
+  ProfilerString(IDBTransaction* aTransaction)
+  {
+    MOZ_ASSERT(aTransaction);
+
+    switch (aTransaction->GetMode()) {
+      case IDBTransaction::READ_ONLY:
+        AppendLiteral("\"readonly\"");
+        break;
+      case IDBTransaction::READ_WRITE:
+        AppendLiteral("\"readwrite\"");
+        break;
+      case IDBTransaction::VERSION_CHANGE:
+        AppendLiteral("\"versionchange\"");
+        break;
+      default:
+        MOZ_NOT_REACHED("Unknown mode!");
+    };
+  }
+
+  explicit
+  ProfilerString(IDBObjectStore* aObjectStore)
+  {
+    MOZ_ASSERT(aObjectStore);
+
+    Append(kQuote);
+    AppendUTF16toUTF8(aObjectStore->Name(), *this);
+    Append(kQuote);
+  }
+
+  explicit
+  ProfilerString(IDBIndex* aIndex)
+  {
+    MOZ_ASSERT(aIndex);
+
+    Append(kQuote);
+    AppendUTF16toUTF8(aIndex->Name(), *this);
+    Append(kQuote);
+  }
+
+  explicit
+  ProfilerString(IDBKeyRange* aKeyRange)
+  {
+    if (aKeyRange) {
+      if (aKeyRange->IsOnly()) {
+        Append(ProfilerString(aKeyRange->Lower()));
+      }
+      else {
+        Append(aKeyRange->IsLowerOpen() ? kOpenParen : kOpenBracket);
+        Append(ProfilerString(aKeyRange->Lower()));
+        AppendLiteral(", ");
+        Append(ProfilerString(aKeyRange->Upper()));
+        Append(aKeyRange->IsUpperOpen() ? kCloseParen : kCloseBracket);
+      }
+    }
+  }
+
+  explicit
+  ProfilerString(const Key& aKey)
+  {
+    if (aKey.IsUnset()) {
+      Assign("null");
+    }
+    else if (aKey.IsFloat()) {
+      AppendPrintf("%g", aKey.ToFloat());
+    }
+    else if (aKey.IsDate()) {
+      AppendPrintf("<Date %g>", aKey.ToDateMsec());
+    }
+    else if (aKey.IsString()) {
+      nsAutoString str;
+      aKey.ToString(str);
+      AppendPrintf("\"%s\"", NS_ConvertUTF16toUTF8(str).get());
+    }
+    else {
+      MOZ_ASSERT(aKey.IsArray());
+      AppendLiteral("<Array>");
+    }
+  }
+
+  explicit
+  ProfilerString(const IDBCursor::Direction aDirection)
+  {
+    switch (aDirection) {
+      case IDBCursor::NEXT:
+        AppendLiteral("\"next\"");
+        break;
+      case IDBCursor::NEXT_UNIQUE:
+        AppendLiteral("\"nextunique\"");
+        break;
+      case IDBCursor::PREV:
+        AppendLiteral("\"prev\"");
+        break;
+      case IDBCursor::PREV_UNIQUE:
+        AppendLiteral("\"prevunique\"");
+        break;
+      default:
+        MOZ_NOT_REACHED("Unknown direction!");
+    };
+  }
+};
+
+END_INDEXEDDB_NAMESPACE
+
+#define IDB_PROFILER_MARK(_detailedFmt, _conciseFmt, ...)                      \
+  do {                                                                         \
+    nsAutoCString _mark;                                                       \
+    _mark.AppendPrintf(_detailedFmt, ##__VA_ARGS__);                           \
+    PROFILER_MARKER(_mark.get());                                              \
+  } while (0)
+
+#define IDB_PROFILER_STRING(_arg)                                              \
+  mozilla::dom::indexedDB::ProfilerString((_arg)).get()
+
+#else 
+
+#define IDB_PROFILER_MARK(_detailedFmt, _conciseFmt, ...)                      \
+  do {                                                                         \
+    nsAutoCString _mark;                                                       \
+    _mark.AppendPrintf(_conciseFmt, ##__VA_ARGS__);                            \
+    PROFILER_MARKER(_mark.get());                                              \
+  } while (0)
+
+#define IDB_PROFILER_STRING(_arg) ""
+
+#endif 
+
+#define IDB_PROFILER_MARK_IF(_cond, _detailedFmt, _conciseFmt, ...)            \
+  do {                                                                         \
+    if (_cond) {                                                               \
+      IDB_PROFILER_MARK(_detailedFmt, _conciseFmt, __VA_ARGS__);               \
+    }                                                                          \
+  } while (0)
+
+#else 
+
+#define IDB_PROFILER_MARK(...) do { } while(0)
+#define IDB_PROFILER_MARK_IF(_cond, ...) do { } while(0)
+#define IDB_PROFILER_MARK2(_detailedFmt, _notdetailedFmt, ...) do { } while(0)
+#define IDB_PROFILER_STRING(_arg) ""
+
+#endif 
+
+#endif 
