fa91e10c3be93bd5838ef4bb3311e8fca2eb4edd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:10:33 +0000

    Bug 1015917 part 3 - Add tests. r=luke
    UltraBlame original commit: cdc1cbc3ce548df6cde15976ed42f63c06867bd6

--- js/src/jit-test/tests/basic/latin1.js
+++ js/src/jit-test/tests/basic/latin1.js
@@ -27,3 +27,89 @@ s = "foo01\u00c7";
 s = toLatin1(s);
 testEq(s);
 testEq(s);
+
+function testConcat() {
+    function concat(s1, s2) {
+	return s1 + s2;
+    }
+
+    
+    assertEq(concat("abc", "def"), "abcdef");
+    var s1 = toLatin1("ABC");
+    var s2 = toLatin1("DEF");
+    assertEq(concat(s1, s2), "ABCDEF");
+    assertEq(concat(s1, "GHI\u0580"), "ABCGHI\u0580");
+    assertEq(concat("GHI\u0580", s2), "GHI\u0580DEF");
+    assertEq(concat(concat("GHI\u0580", s2), s1), "GHI\u0580DEFABC");
+    assertEq(isLatin1(s1), true);
+    assertEq(isLatin1(s2), true);
+
+    
+    var s3 = toLatin1("0123456789012345678901234567890123456789");
+    var rope = concat(s1, s3);
+    assertEq(isLatin1(rope), true);
+    assertEq(rope, "ABC0123456789012345678901234567890123456789");
+    assertEq(isLatin1(rope), true); 
+
+    
+    assertEq(isLatin1(s3), true);
+    rope = concat(s3, "someTwoByteString\u0580");
+    assertEq(isLatin1(rope), false);
+    assertEq(rope, "0123456789012345678901234567890123456789someTwoByteString\u0580");
+    assertEq(isLatin1(rope), false);
+
+    assertEq(isLatin1(s3), true);
+    rope = concat("twoByteString\u0580", concat(s3, "otherTwoByte\u0580"));
+    assertEq(isLatin1(rope), false);
+    assertEq(rope, "twoByteString\u05800123456789012345678901234567890123456789otherTwoByte\u0580");
+    assertEq(isLatin1(rope), false);
+
+    
+    var s4 = toLatin1("adsfasdfjkasdfkjasdfasasdfasdf");
+    for (var i=0; i<5; i++) {
+	s4 = concat(s4, s1);
+	assertEq(s4 === ".".repeat(s4.length), false); 
+    }
+
+    assertEq(isLatin1(s4), true);
+
+    
+    s4 = concat(s4, "--\u0580");
+    assertEq(s4, "adsfasdfjkasdfkjasdfasasdfasdfABCABCABCABCABC--\u0580");
+}
+testConcat();
+
+function testFlattenDependent() {
+    function concat(s1, s2) {
+	return s1 + s2;
+    }
+
+    
+    var s1 = toLatin1("Foo0123456789bar012345---");
+    var s2 = toLatin1("Foo0123456789bar012345+++");
+    assertEq(isLatin1(s1), true);
+    assertEq(isLatin1(s2), true);
+
+    
+    var rope1 = concat(s1, s1);
+    assertEq(isLatin1(rope1), true);
+
+    var rope2 = concat(rope1, s2);
+    assertEq(isLatin1(rope2), true);
+
+    var rope3 = concat("twoByte\u0581", rope2);
+    assertEq(isLatin1(rope3), false);
+
+    
+    assertEq(rope3, "twoByte\u0581Foo0123456789bar012345---Foo0123456789bar012345---Foo0123456789bar012345+++");
+    assertEq(isLatin1(rope3), false);
+
+    
+    
+    assertEq(isLatin1(rope1), false);
+    assertEq(isLatin1(rope2), false);
+
+    assertEq(rope1, "Foo0123456789bar012345---Foo0123456789bar012345---");
+    assertEq(rope2, "Foo0123456789bar012345---Foo0123456789bar012345---Foo0123456789bar012345+++");
+}
+testFlattenDependent();
