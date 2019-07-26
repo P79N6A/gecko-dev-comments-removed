f17ad7e469b9ece267f6694991fd800c729353b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:08:10 +0000

    Bug 788021 - Part 1: Add nsGZFileWriter. r=bsmedberg
    UltraBlame original commit: 15cc0aabab67288f2a2f3ed6408060450bc5cd1d

---
+++ xpcom/base/nsGZFileWriter.h
@@ -0,0 +1,41 @@
+
+
+
+
+
+#include "nsIGZFileWriter.h"
+#include <stdio.h>
+#include "zlib.h"
+
+
+
+
+class nsGZFileWriter : public nsIGZFileWriter
+{
+public:
+  nsGZFileWriter();
+  virtual ~nsGZFileWriter();
+
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIGZFILEWRITER
+
+  
+
+
+
+
+  nsresult Write(const char* aStr)
+  {
+    return nsIGZFileWriter::Write(aStr);
+  }
+
+  nsresult Write(const char* aStr, uint32_t aLen)
+  {
+    return nsIGZFileWriter::Write(aStr, aLen);
+  }
+
+private:
+  bool mInitialized;
+  bool mFinished;
+  gzFile mGZFile;
+};
