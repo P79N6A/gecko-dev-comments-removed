c7e50a55c3da43e5a7948d37e12ddcac035ddbb3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:12:14 +0000

    Bug 957086 - patch 2 - DataStoreService in C++, r=ehsan, r=bz, r=janv
    UltraBlame original commit: d296d4109a41785e076c82c32c7d91f19be6d55c

---
+++ dom/datastore/DataStoreDB.h
@@ -0,0 +1,82 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_DataStoreDB_h
+#define mozilla_dom_DataStoreDB_h
+
+#include "mozilla/dom/IDBTransactionBinding.h"
+#include "nsAutoPtr.h"
+#include "nsIDOMEventListener.h"
+#include "nsISupportsImpl.h"
+#include "nsString.h"
+
+#define DATASTOREDB_REVISION       "revision"
+
+namespace mozilla {
+namespace dom {
+
+namespace indexedDB {
+class IDBDatabase;
+class IDBFactory;
+class IDBObjectStore;
+class IDBOpenDBRequest;
+class IDBTransaction;
+}
+
+class DataStoreDBCallback;
+
+class DataStoreDB MOZ_FINAL : public nsIDOMEventListener
+{
+public:
+  NS_DECL_ISUPPORTS
+
+  DataStoreDB(const nsAString& aManifestURL, const nsAString& aName);
+  ~DataStoreDB();
+
+  nsresult Open(IDBTransactionMode aMode, const Sequence<nsString>& aDb,
+                DataStoreDBCallback* aCallback);
+
+  nsresult Delete();
+
+  indexedDB::IDBTransaction* Transaction() const;
+
+  
+  NS_IMETHOD HandleEvent(nsIDOMEvent* aEvent);
+
+private:
+  nsresult CreateFactoryIfNeeded();
+
+  nsresult UpgradeSchema();
+
+  nsresult DatabaseOpened();
+
+  nsresult AddEventListeners();
+
+  nsresult RemoveEventListeners();
+
+  nsString mDatabaseName;
+
+  nsRefPtr<indexedDB::IDBFactory> mFactory;
+  nsRefPtr<indexedDB::IDBOpenDBRequest> mRequest;
+  nsRefPtr<indexedDB::IDBDatabase> mDatabase;
+  nsRefPtr<indexedDB::IDBTransaction> mTransaction;
+
+  nsRefPtr<DataStoreDBCallback> mCallback;
+
+  
+  enum StateType {
+    Inactive,
+    Active
+  } mState;
+
+  IDBTransactionMode mTransactionMode;
+  Sequence<nsString> mObjectStores;
+};
+
+} 
+} 
+
+#endif 
--- dom/datastore/DataStoreImpl.js
+++ dom/datastore/DataStoreImpl.js
@@ -59,7 +59,6 @@ function validateId(aId) {
 
 function DataStore() {
   debug("DataStore created");
-  this.wrappedJSObject = this;
 }
 
 DataStore.prototype = {
