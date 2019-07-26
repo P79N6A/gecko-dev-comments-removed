0680aae90233947c6d3c18fc9b647feef0476e98
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:22:34 +0000

    Bug 856472: Move CanvasGradient to its own file r=bz
    UltraBlame original commit: f127889eda58ded37745f792a2930bd2916fb8d9

---
+++ content/canvas/src/CanvasGradient.h
@@ -0,0 +1,65 @@
+
+
+
+
+#ifndef mozilla_dom_CanvasGradient_h
+#define mozilla_dom_CanvasGradient_h
+
+#include "nsIDOMCanvasRenderingContext2D.h"
+#include "nsTArray.h"
+#include "mozilla/RefPtr.h"
+
+#define NS_CANVASGRADIENTAZURE_PRIVATE_IID \
+    {0x28425a6a, 0x90e0, 0x4d42, {0x9c, 0x75, 0xff, 0x60, 0x09, 0xb3, 0x10, 0xa8}}
+
+namespace mozilla {
+namespace dom {
+
+class CanvasGradient : public nsIDOMCanvasGradient
+{
+public:
+  NS_DECLARE_STATIC_IID_ACCESSOR(NS_CANVASGRADIENTAZURE_PRIVATE_IID)
+
+  enum Type
+  {
+    LINEAR = 0,
+    RADIAL
+  };
+
+  Type GetType()
+  {
+    return mType;
+  }
+
+
+  mozilla::gfx::GradientStops *
+  GetGradientStopsForTarget(mozilla::gfx::DrawTarget *aRT)
+  {
+    if (mStops && mStops->GetBackendType() == aRT->GetType()) {
+      return mStops;
+    }
+
+    mStops = aRT->CreateGradientStops(mRawStops.Elements(), mRawStops.Length());
+
+    return mStops;
+  }
+
+  NS_DECL_ISUPPORTS
+
+  
+  NS_IMETHOD AddColorStop(float offset, const nsAString& colorstr);
+
+protected:
+  CanvasGradient(Type aType) : mType(aType)
+  {}
+
+  nsTArray<mozilla::gfx::GradientStop> mRawStops;
+  mozilla::RefPtr<mozilla::gfx::GradientStops> mStops;
+  Type mType;
+  virtual ~CanvasGradient() {}
+};
+
+} 
+} 
+
+#endif 
