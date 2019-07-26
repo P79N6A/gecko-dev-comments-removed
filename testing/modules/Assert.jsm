ff1fbc13791507d325af5c043ca2f2b05910dab6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:42:25 +0000

    Bug 873126: introduce a common JS Assert library. r=gps,Unfocused
    UltraBlame original commit: 7d7d59c0e0fa9ddc6f188d8bf08908fe3f9df851

---
+++ testing/modules/Assert.jsm
@@ -0,0 +1,426 @@
+
+
+
+
+
+
+
+
+
+
+
+
+"use strict";
+
+this.EXPORTED_SYMBOLS = [
+  "Assert"
+];
+
+
+
+
+
+
+
+
+
+let Assert = this.Assert = function(reporterFunc) {
+  if (reporterFunc)
+    this.setReporter(reporterFunc);
+};
+
+function instanceOf(object, type) {
+  return Object.prototype.toString.call(object) == "[object " + type + "]";
+}
+
+function replacer(key, value) {
+  if (value === undefined) {
+    return "" + value;
+  }
+  if (typeof value === "number" && (isNaN(value) || !isFinite(value))) {
+    return value.toString();
+  }
+  if (typeof value === "function" || instanceOf(value, "RegExp")) {
+    return value.toString();
+  }
+  return value;
+}
+
+const kTruncateLength = 128;
+
+function truncate(text, newLength = kTruncateLength) {
+  if (typeof text == "string") {
+    return text.length < newLength ? text : text.slice(0, newLength);
+  } else {
+    return text;
+  }
+}
+
+function getMessage(error) {
+  return truncate(JSON.stringify(error.actual, replacer)) + " " +
+         (error.operator ? error.operator + " " : "") +
+         truncate(JSON.stringify(error.expected, replacer));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Assert.AssertionError = function(options) {
+  this.name = "AssertionError";
+  this.actual = options.actual;
+  this.expected = options.expected;
+  this.operator = options.operator;
+  this.message = options.message || getMessage(this);
+  
+  let stack = Components.stack;
+  do {
+    stack = stack.caller;
+  } while(stack.filename && stack.filename.contains("Assert.jsm"))
+  this.stack = stack;
+};
+
+
+Assert.AssertionError.prototype = Object.create(Error.prototype, {
+  constructor: {
+    value: Assert.AssertionError,
+    enumerable: false,
+    writable: true,
+    configurable: true
+  }
+});
+
+let proto = Assert.prototype;
+
+proto._reporter = null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+proto.setReporter = function(reporterFunc) {
+  this._reporter = reporterFunc;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+proto.report = function(failed, actual, expected, message, operator) {
+  let err = new Assert.AssertionError({
+    message: message,
+    actual: actual,
+    expected: expected,
+    operator: operator
+  });
+  if (!this._reporter) {
+    
+    if (failed) {
+      throw err;
+    }
+  } else {
+    this._reporter(failed ? err : null, message, err.stack);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+proto.ok = function(value, message) {
+  this.report(!value, value, true, message, "==");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+proto.equal = function equal(actual, expected, message) {
+  this.report(actual != expected, actual, expected, message, "==");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+proto.notEqual = function notEqual(actual, expected, message) {
+  this.report(actual == expected, actual, expected, message, "!=");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+proto.deepEqual = function deepEqual(actual, expected, message) {
+  this.report(!_deepEqual(actual, expected), actual, expected, message, "deepEqual");
+};
+
+function _deepEqual(actual, expected) {
+  
+  if (actual === expected) {
+    return true;
+  
+  
+  } else if (instanceOf(actual, "Date") && instanceOf(expected, "Date")) {
+    return actual.getTime() === expected.getTime();
+  
+  
+  
+  } else if (instanceOf(actual, "RegExp") && instanceOf(expected, "RegExp")) {
+    return actual.source === expected.source &&
+           actual.global === expected.global &&
+           actual.multiline === expected.multiline &&
+           actual.lastIndex === expected.lastIndex &&
+           actual.ignoreCase === expected.ignoreCase;
+  
+  
+  } else if (typeof actual != "object" && typeof expected != "object") {
+    return actual == expected;
+  
+  
+  
+  
+  
+  
+  } else {
+    return objEquiv(actual, expected);
+  }
+}
+
+function isUndefinedOrNull(value) {
+  return value === null || value === undefined;
+}
+
+function isArguments(object) {
+  return instanceOf(object, "Arguments");
+}
+
+function objEquiv(a, b) {
+  if (isUndefinedOrNull(a) || isUndefinedOrNull(b)) {
+    return false;
+  }
+  
+  if (a.prototype !== b.prototype) {
+    return false;
+  }
+  
+  
+  if (isArguments(a)) {
+    if (!isArguments(b)) {
+      return false;
+    }
+    a = pSlice.call(a);
+    b = pSlice.call(b);
+    return _deepEqual(a, b);
+  }
+  let ka, kb, key, i;
+  try {
+    ka = Object.keys(a);
+    kb = Object.keys(b);
+  } catch (e) {
+    
+    return false;
+  }
+  
+  
+  if (ka.length != kb.length)
+    return false;
+  
+  ka.sort();
+  kb.sort();
+  
+  
+  for (i = ka.length - 1; i >= 0; i--) {
+    key = ka[i];
+    if (!_deepEqual(a[key], b[key])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+proto.notDeepEqual = function notDeepEqual(actual, expected, message) {
+  this.report(_deepEqual(actual, expected), actual, expected, message, "notDeepEqual");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+proto.strictEqual = function strictEqual(actual, expected, message) {
+  this.report(actual !== expected, actual, expected, message, "===");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+proto.notStrictEqual = function notStrictEqual(actual, expected, message) {
+  this.report(actual === expected, actual, expected, message, "!==");
+};
+
+function expectedException(actual, expected) {
+  if (!actual || !expected) {
+    return false;
+  }
+
+  if (instanceOf(expected, "RegExp")) {
+    return expected.test(actual);
+  } else if (actual instanceof expected) {
+    return true;
+  } else if (expected.call({}, actual) === true) {
+    return true;
+  }
+
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+proto.throws = function(block, expected, message) {
+  let actual;
+
+  if (typeof expected === "string") {
+    message = expected;
+    expected = null;
+  }
+
+  try {
+    block();
+  } catch (e) {
+    actual = e;
+  }
+
+  message = (expected && expected.name ? " (" + expected.name + ")." : ".") +
+            (message ? " " + message : ".");
+
+  if (!actual) {
+    this.report(true, actual, expected, "Missing expected exception" + message);
+  }
+
+  if ((actual && expected && !expectedException(actual, expected))) {
+    throw actual;
+  }
+
+  this.report(false, expected, expected, message);
+};
---
+++ testing/modules/tests/xpcshell/test_assert.js
@@ -0,0 +1,281 @@
+
+
+
+
+
+
+
+function run_test() {
+  let ns = {};
+  Components.utils.import("resource://testing-common/Assert.jsm", ns);
+  let assert = new ns.Assert();
+
+  function makeBlock(f, ...args) {
+    return function() {
+      return f.apply(assert, args);
+    };
+  }
+
+  function protoCtrChain(o) {
+    let result = [];
+    while (o = o.__proto__) {
+      result.push(o.constructor);
+    }
+    return result.join();
+  }
+
+  function indirectInstanceOf(obj, cls) {
+    if (obj instanceof cls) {
+      return true;
+    }
+    let clsChain = protoCtrChain(cls.prototype);
+    let objChain = protoCtrChain(obj);
+    return objChain.slice(-clsChain.length) === clsChain;
+  };
+
+  assert.ok(indirectInstanceOf(ns.Assert.AssertionError.prototype, Error),
+            "Assert.AssertionError instanceof Error");
+
+  assert.throws(makeBlock(assert.ok, false),
+                ns.Assert.AssertionError, "ok(false)");
+
+  assert.ok(true, "ok(true)");
+
+  assert.ok("test", "ok('test')");
+
+  assert.throws(makeBlock(assert.equal, true, false), ns.Assert.AssertionError, "equal");
+
+  assert.equal(null, null, "equal");
+
+  assert.equal(undefined, undefined, "equal");
+
+  assert.equal(null, undefined, "equal");
+
+  assert.equal(true, true, "equal");
+
+  assert.notEqual(true, false, "notEqual");
+
+  assert.throws(makeBlock(assert.notEqual, true, true),
+                ns.Assert.AssertionError, "notEqual");
+
+  assert.throws(makeBlock(assert.strictEqual, 2, "2"),
+                ns.Assert.AssertionError, "strictEqual");
+
+  assert.throws(makeBlock(assert.strictEqual, null, undefined),
+                ns.Assert.AssertionError, "strictEqual");
+
+  assert.notStrictEqual(2, "2", "notStrictEqual");
+
+  
+  
+  assert.deepEqual(new Date(2000, 3, 14), new Date(2000, 3, 14), "deepEqual date");
+
+  assert.throws(makeBlock(assert.deepEqual, new Date(), new Date(2000, 3, 14)),
+                ns.Assert.AssertionError,
+                "deepEqual date");
+
+  
+  assert.deepEqual(/a/, /a/);
+  assert.deepEqual(/a/g, /a/g);
+  assert.deepEqual(/a/i, /a/i);
+  assert.deepEqual(/a/m, /a/m);
+  assert.deepEqual(/a/igm, /a/igm);
+  assert.throws(makeBlock(assert.deepEqual, /ab/, /a/));
+  assert.throws(makeBlock(assert.deepEqual, /a/g, /a/));
+  assert.throws(makeBlock(assert.deepEqual, /a/i, /a/));
+  assert.throws(makeBlock(assert.deepEqual, /a/m, /a/));
+  assert.throws(makeBlock(assert.deepEqual, /a/igm, /a/im));
+
+  let re1 = /a/;
+  re1.lastIndex = 3;
+  assert.throws(makeBlock(assert.deepEqual, re1, /a/));
+
+  
+  assert.deepEqual(4, "4", "deepEqual == check");
+  assert.deepEqual(true, 1, "deepEqual == check");
+  assert.throws(makeBlock(assert.deepEqual, 4, "5"),
+                ns.Assert.AssertionError,
+                "deepEqual == check");
+
+  
+  
+  assert.deepEqual({a: 4}, {a: 4});
+  assert.deepEqual({a: 4, b: "2"}, {a: 4, b: "2"});
+  assert.deepEqual([4], ["4"]);
+  assert.throws(makeBlock(assert.deepEqual, {a: 4}, {a: 4, b: true}),
+                ns.Assert.AssertionError);
+  assert.deepEqual(["a"], {0: "a"});
+
+  let a1 = [1, 2, 3];
+  let a2 = [1, 2, 3];
+  a1.a = "test";
+  a1.b = true;
+  a2.b = true;
+  a2.a = "test";
+  assert.throws(makeBlock(assert.deepEqual, Object.keys(a1), Object.keys(a2)),
+                ns.Assert.AssertionError);
+  assert.deepEqual(a1, a2);
+
+  let nbRoot = {
+    toString: function() { return this.first + " " + this.last; }
+  };
+
+  function nameBuilder(first, last) {
+    this.first = first;
+    this.last = last;
+    return this;
+  }
+  nameBuilder.prototype = nbRoot;
+
+  function nameBuilder2(first, last) {
+    this.first = first;
+    this.last = last;
+    return this;
+  }
+  nameBuilder2.prototype = nbRoot;
+
+  let nb1 = new nameBuilder("Ryan", "Dahl");
+  let nb2 = new nameBuilder2("Ryan", "Dahl");
+
+  assert.deepEqual(nb1, nb2);
+
+  nameBuilder2.prototype = Object;
+  nb2 = new nameBuilder2("Ryan", "Dahl");
+  assert.throws(makeBlock(assert.deepEqual, nb1, nb2), ns.Assert.AssertionError);
+
+  
+  assert.throws(makeBlock(assert.deepEqual, "a", {}), ns.Assert.AssertionError);
+
+  
+  function thrower(errorConstructor) {
+    throw new errorConstructor("test");
+  }
+  let aethrow = makeBlock(thrower, ns.Assert.AssertionError);
+  aethrow = makeBlock(thrower, ns.Assert.AssertionError);
+
+  
+  assert.throws(makeBlock(thrower, ns.Assert.AssertionError),
+                ns.Assert.AssertionError, "message");
+  assert.throws(makeBlock(thrower, ns.Assert.AssertionError), ns.Assert.AssertionError);
+  assert.throws(makeBlock(thrower, ns.Assert.AssertionError));
+
+  
+  assert.throws(makeBlock(thrower, TypeError));
+
+  
+  let threw = false;
+  try {
+    assert.throws(makeBlock(thrower, TypeError), ns.Assert.AssertionError);
+  } catch (e) {
+    threw = true;
+    assert.ok(e instanceof TypeError, "type");
+  }
+  assert.equal(true, threw,
+               "Assert.throws with an explicit error is eating extra errors",
+               ns.Assert.AssertionError);
+  threw = false;
+
+  function ifError(err) {
+    if (err) {
+      throw err;
+    }
+  }
+  assert.throws(function() {
+    ifError(new Error("test error"));
+  });
+
+  
+  threw = false;
+  try {
+    assert.throws(
+      function() {
+        throw ({});
+      },
+      Array
+    );
+  } catch (e) {
+    threw = true;
+  }
+  assert.ok(threw, "wrong constructor validation");
+
+  
+  assert.throws(makeBlock(thrower, TypeError), /test/);
+
+  
+  assert.throws(makeBlock(thrower, TypeError), function(err) {
+    if ((err instanceof TypeError) && /test/.test(err)) {
+      return true;
+    }
+  });
+
+  
+
+  let b = {};
+  b.b = b;
+
+  let c = {};
+  c.b = c;
+
+  let gotError = false;
+  try {
+    assert.deepEqual(b, c);
+  } catch (e) {
+    gotError = true;
+  }
+
+  dump("All OK\n");
+  assert.ok(gotError);
+
+  function testAssertionMessage(actual, expected) {
+    try {
+      assert.equal(actual, "");
+    } catch (e) {
+      assert.equal(e.toString(),
+          ["AssertionError:", expected, "==", '""'].join(" "));
+    }
+  }
+  testAssertionMessage(undefined, '"undefined"');
+  testAssertionMessage(null, "null");
+  testAssertionMessage(true, "true");
+  testAssertionMessage(false, "false");
+  testAssertionMessage(0, "0");
+  testAssertionMessage(100, "100");
+  testAssertionMessage(NaN, '"NaN"');
+  testAssertionMessage(Infinity, '"Infinity"');
+  testAssertionMessage(-Infinity, '"-Infinity"');
+  testAssertionMessage("", '""');
+  testAssertionMessage("foo", '"foo"');
+  testAssertionMessage([], "[]");
+  testAssertionMessage([1, 2, 3], "[1,2,3]");
+  testAssertionMessage(/a/, '"/a/"');
+  testAssertionMessage(/abc/gim, '"/abc/gim"');
+  testAssertionMessage(function f() {}, '"function f() {}"');
+  testAssertionMessage({}, "{}");
+  testAssertionMessage({a: undefined, b: null}, '{"a":"undefined","b":null}');
+  testAssertionMessage({a: NaN, b: Infinity, c: -Infinity},
+      '{"a":"NaN","b":"Infinity","c":"-Infinity"}');
+
+  
+  try {
+    assert.throws(function () {
+      ifError(null);
+    });
+  } catch (e) {
+    threw = true;
+    assert.equal(e.message, "Missing expected exception..");
+  }
+  assert.ok(threw);
+
+  
+  try {
+    assert.equal(1, 2);
+  } catch (e) {
+    assert.equal(e.toString().split("\n")[0], "AssertionError: 1 == 2")
+  }
+
+  try {
+    assert.equal(1, 2, "oh no");
+  } catch (e) {
+    assert.equal(e.toString().split("\n")[0], "AssertionError: oh no")
+  }
+}
