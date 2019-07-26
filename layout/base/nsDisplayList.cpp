49a033692b70b1ae1f7b6d26ea83678e1590253f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:05:29 +0000

    Bug 918682 - Fix the check for finding the root content document's root scroll frame. r=tn
    UltraBlame original commit: e69483402ee3590435dfdc66684f9353019f050f

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -729,8 +729,8 @@ static void RecordFrameMetrics(nsIFrame* aForFrame,
   
   
   bool useWidgetBounds = false;
-  bool isRootContentDocRootScrollFrame = aForFrame->GetParent() == nullptr
-                                      && presContext->IsRootContentDocument();
+  bool isRootContentDocRootScrollFrame = presContext->IsRootContentDocument()
+                                      && aScrollFrame == presShell->GetRootScrollFrame();
   if (isRootContentDocRootScrollFrame) {
     if (nsIWidget* widget = aForFrame->GetNearestWidget()) {
       nsIntRect bounds;
@@ -2360,12 +2360,6 @@ nsDisplayThemedBackground::PaintInternal(nsDisplayListBuilder* aBuilder,
   theme->DrawWidgetBackground(aCtx, mFrame, mAppearance, borderArea, drawing);
 }
 
-bool nsDisplayThemedBackground::IsWindowActive()
-{
-  nsEventStates docState = mFrame->GetContent()->OwnerDoc()->GetDocumentState();
-  return !docState.HasState(NS_DOCUMENT_STATE_WINDOW_INACTIVE);
-}
-
 void nsDisplayThemedBackground::ComputeInvalidationRegion(nsDisplayListBuilder* aBuilder,
                                                           const nsDisplayItemGeometry* aGeometry,
                                                           nsRegion* aInvalidRegion)
@@ -2384,11 +2378,6 @@ void nsDisplayThemedBackground::ComputeInvalidationRegion(nsDisplayListBuilder* 
     
     
     aInvalidRegion->Xor(bounds, geometry->mBounds);
-  }
-  nsITheme* theme = mFrame->PresContext()->GetTheme();
-  if (theme->WidgetAppearanceDependsOnWindowFocus(mAppearance) &&
-      IsWindowActive() != geometry->mWindowIsActive) {
-    aInvalidRegion->Or(*aInvalidRegion, bounds);
   }
 }
 
