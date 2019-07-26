22b2cf4c27a57540c0a185eec2debd402775edd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:24:16 +0000

    Bug 1026680 part 5 - Make escape work with Latin1 strings. r=njn
    UltraBlame original commit: 0784505c202aea8b270f2fa7b1622f4b1a9f16f8

--- js/src/jit-test/tests/latin1/escape-unescape.js
+++ js/src/jit-test/tests/latin1/escape-unescape.js
@@ -8,3 +8,11 @@ assertEq(unescape(s), "a+ def\x00A0\u1200");
 
 s += "\u1200";
 assertEq(unescape(s), "a+ def\x00A0\u1200\u1200");
+
+
+s = toLatin1("abc \u00ff");
+assertEq(escape(s), "abc%20%FF");
+
+
+s += "\u1200";
+assertEq(escape(s), "abc%20%FF%u1200");
