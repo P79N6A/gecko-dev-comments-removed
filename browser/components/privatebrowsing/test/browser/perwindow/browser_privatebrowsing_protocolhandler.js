5de052f56501c87b49a65ca7d6cd9a4d446a0d7d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:27:26 +0000

    Bug 806695 - Port browser_privatebrowsing_protocolhandler.js to the new per-window PB APIs; r=ehsan
    UltraBlame original commit: 2ed2255f26ada942ac7fd22608f804aacfb98bac

---
+++ browser/components/privatebrowsing/test/browser/perwindow/browser_privatebrowsing_protocolhandler.js
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+
+function test() {
+  
+  waitForExplicitFinish();
+  let windowsToClose = [];
+  let notificationValue = "Protocol Registration: testprotocol";
+  let testURI = "http://example.com/browser/" +
+    "browser/components/privatebrowsing/test/browser/perwindow/browser_privatebrowsing_protocolhandler_page.html";
+
+  function doTest(aIsPrivateMode, aWindow, aCallback) {
+    aWindow.gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
+      aWindow.gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
+
+      setTimeout(function() {
+        let notificationBox = aWindow.gBrowser.getNotificationBox();
+        let notification = notificationBox.getNotificationWithValue(notificationValue);
+
+        if (aIsPrivateMode) {
+          
+          ok(!notification, "Notification box should not be displayed inside of private browsing mode");
+        } else {
+          
+          ok(notification, "Notification box should be displaying outside of private browsing mode");
+        }
+
+        aCallback();
+      }, 100); 
+    }, true);
+
+    aWindow.gBrowser.selectedBrowser.loadURI(testURI);
+  }
+
+  function testOnWindow(aOptions, aCallback) {
+    whenNewWindowLoaded(aOptions, function(aWin) {
+      windowsToClose.push(aWin);
+      
+      
+      
+      executeSoon(function() aCallback(aWin));
+    });
+  };
+
+   
+  registerCleanupFunction(function() {
+    windowsToClose.forEach(function(aWin) {
+      aWin.close();
+    });
+  });
+
+  
+  testOnWindow({}, function(aWin) {
+    doTest(false, aWin, function() {
+      
+      testOnWindow({private: true}, function(aWin) {
+        doTest(true, aWin, finish);
+      });
+    });
+  });
+}
