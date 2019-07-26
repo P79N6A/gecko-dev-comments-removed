ac4b7e766e2e2277792d0dfeb72c25220d70c675
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:22:49 +0000

    Bug 984794 - Switch to sticky axis locking on B2G. r=Cwiiis
    UltraBlame original commit: 2fbe01ee395571afd8fa5080c1e68fefa130875f

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -899,6 +899,8 @@ pref("apz.x_stationary_size_multiplier", "1.5");
 pref("apz.y_stationary_size_multiplier", "1.8");
 pref("apz.enlarge_displayport_when_clipped", true);
 
+pref("apz.axis_lock_mode", 2);
+
 
 
 
