ce5b919c6ef4da77bc12ae638e0b72a7f32ca214
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:36:01 +0000

    Bug 745194 - [jsdbg2] Crash on Heap, trying to execute NULL, with Debugger forced return, methodjit, and GC. r=bhackett.
    UltraBlame original commit: 96b591267cb3ac21eadce063289f81734a60498c

---
+++ js/src/jit-test/tests/debug/Debugger-onEnterFrame-resumption-07.js
@@ -0,0 +1,16 @@
+
+
+
+var g = newGlobal('new-compartment');
+g.eval("function f() {}" +
+       "function h() { return new f; }");
+var dbg = Debugger(g);
+dbg.onEnterFrame = function (frame) {
+    if (frame.constructing) {
+        gc();
+        return { return: 0 };
+    }
+};
+var result = g.eval("h()");
+assertEq(typeof result, 'object');
+assertEq(Object.getPrototypeOf(result), g.f.prototype);
