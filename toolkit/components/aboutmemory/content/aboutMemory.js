7d3da9b32e67bdcbe3f94f28d66682ad1bde6509
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:50:07 +0000

    Bug 833913 - Part 2: Make about:memory sort reporters with equal amounts by name, and update the tests to match this. r=njn
    UltraBlame original commit: da129718ccb144269ffd50a9b5b5b0042434f0f4

--- toolkit/components/aboutmemory/content/aboutMemory.js
+++ toolkit/components/aboutmemory/content/aboutMemory.js
@@ -558,33 +558,38 @@ function appendAboutMemoryMain(aBody, aProcess, aHasMozMallocUsableSize,
                     heapTotalByProcess, aForceShowSmaps);
 
   
-  
-  
   let processes = Object.keys(treesByProcess);
-  processes.sort(function(a, b) {
-    assert(a != b, "Elements of Object.keys() should be unique, but " +
-                   "saw duplicate " + a + " elem.");
+  processes.sort(function(aProcessA, aProcessB) {
+    assert(aProcessA != aProcessB,
+           "Elements of Object.keys() should be unique, but " +
+           "saw duplicate '" + aProcessA + "' elem.");
 
-    if (a == gUnnamedProcessStr) {
+    
+    if (aProcessA == gUnnamedProcessStr) {
       return -1;
     }
-
-    if (b == gUnnamedProcessStr) {
+    if (aProcessB == gUnnamedProcessStr) {
       return 1;
     }
 
-    let nodeA = degeneratesByProcess[a]['resident'];
-    let nodeB = degeneratesByProcess[b]['resident'];
+    
+    let nodeA = degeneratesByProcess[aProcessA]['resident'];
+    let nodeB = degeneratesByProcess[aProcessB]['resident'];
+    let residentA = nodeA ? nodeA._amount : -1;
+    let residentB = nodeB ? nodeB._amount : -1;
 
-    if (nodeA && nodeB) {
-      return TreeNode.compareAmounts(nodeA, nodeB);
-    }
-
-    if (nodeA) {
+    if (residentA > residentB) {
       return -1;
     }
+    if (residentA < residentB) {
+      return 1;
+    }
 
-    if (nodeB) {
+    
+    if (aProcessA < aProcessB) {
+      return -1;
+    }
+    if (aProcessA > aProcessB) {
       return 1;
     }
 
@@ -878,8 +883,17 @@ TreeNode.prototype = {
   }
 };
 
+
+
+
 TreeNode.compareAmounts = function(a, b) {
-  return b._amount - a._amount;
+  if (a._amount > b._amount) {
+    return -1;
+  }
+  if (a._amount < b._amount) {
+    return 1;
+  }
+  return TreeNode.compareUnsafeNames(a, b);
 };
 
 TreeNode.compareUnsafeNames = function(a, b) {
