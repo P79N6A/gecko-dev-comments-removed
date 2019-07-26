6f660ebfe80817479c139dc73315413d189f512b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:42:30 +0000

    Bug 876454 - Implement JSOP_LEAVEBLOCKEXPR and JSOP_LEAVEFORLETIN in the baseline compiler. r=evilpie
    UltraBlame original commit: fc0a521fad4314d8f31f6c47f55aaf39ad5b9ed5

--- js/src/ion/BaselineCompiler.cpp
+++ js/src/ion/BaselineCompiler.cpp
@@ -2350,7 +2350,7 @@ typedef bool (*LeaveBlockFn)(JSContext *, BaselineFrame *);
 static const VMFunction LeaveBlockInfo = FunctionInfo<LeaveBlockFn>(ion::LeaveBlock);
 
 bool
-BaselineCompiler::emit_JSOP_LEAVEBLOCK()
+BaselineCompiler::emitLeaveBlock()
 {
     
     prepareVMCall();
@@ -2358,12 +2358,41 @@ BaselineCompiler::emit_JSOP_LEAVEBLOCK()
     masm.loadBaselineFramePtr(BaselineFrameReg, R0.scratchReg());
     pushArg(R0.scratchReg());
 
-    if (!callVM(LeaveBlockInfo))
+    return callVM(LeaveBlockInfo);
+}
+
+bool
+BaselineCompiler::emit_JSOP_LEAVEBLOCK()
+{
+    if (!emitLeaveBlock())
         return false;
 
     
-    size_t n = StackUses(script, pc);
-    frame.popn(n);
+    frame.popn(GET_UINT16(pc));
+    return true;
+}
+
+bool
+BaselineCompiler::emit_JSOP_LEAVEBLOCKEXPR()
+{
+    if (!emitLeaveBlock())
+        return false;
+
+    
+    
+    frame.popRegsAndSync(1);
+    frame.popn(GET_UINT16(pc));
+    frame.push(R0);
+    return true;
+}
+
+bool
+BaselineCompiler::emit_JSOP_LEAVEFORLETIN()
+{
+    if (!emitLeaveBlock())
+        return false;
+
+    
     return true;
 }
 
