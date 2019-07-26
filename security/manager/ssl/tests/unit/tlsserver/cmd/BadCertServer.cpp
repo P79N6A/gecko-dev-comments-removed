136a4eba13cfedb38bd059be996396d97820799c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:47:23 +0000

    bug 967975 - certificate error override telemetry r=briansmith
    UltraBlame original commit: b6aa1e70e482a47b7a8177193b209c5fe148742a

---
+++ security/manager/ssl/tests/unit/tlsserver/cmd/BadCertServer.cpp
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+
+#include "TLSServer.h"
+
+using namespace mozilla;
+using namespace mozilla::test;
+
+struct BadCertHost
+{
+  const char *mHostName;
+  const char *mCertName;
+};
+
+const BadCertHost sBadCertHosts[] =
+{
+  { "expired.example.com", "expired" },
+  { "selfsigned.example.com", "selfsigned" },
+  { "unknownissuer.example.com", "unknownissuer" },
+  { "mismatch.example.com", "mismatch" },
+  { "expiredissuer.example.com", "expiredissuer" },
+  { "md5signature.example.com", "md5signature" },
+  { "untrusted.example.com", "localhostAndExampleCom" },
+  { "inadequatekeyusage.example.com", "inadequatekeyusage" },
+  { "untrustedissuer.example.com", "untrustedissuer" },
+  { nullptr, nullptr }
+};
+
+int32_t
+DoSNISocketConfig(PRFileDesc *aFd, const SECItem *aSrvNameArr,
+                  uint32_t aSrvNameArrSize, void *aArg)
+{
+  const BadCertHost *host = GetHostForSNI(aSrvNameArr, aSrvNameArrSize,
+                                          sBadCertHosts);
+  if (!host) {
+    return SSL_SNI_SEND_ALERT;
+  }
+
+  if (gDebugLevel >= DEBUG_VERBOSE) {
+    fprintf(stderr, "found pre-defined host '%s'\n", host->mHostName);
+  }
+
+  ScopedCERTCertificate cert;
+  SSLKEAType certKEA;
+  if (SECSuccess != ConfigSecureServerWithNamedCert(aFd, host->mCertName,
+                                                    &cert, &certKEA)) {
+    return SSL_SNI_SEND_ALERT;
+  }
+
+  return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <NSS DB directory>\n", argv[0]);
+    return 1;
+  }
+
+  return StartServer(argv[1], DoSNISocketConfig, nullptr);
+}
