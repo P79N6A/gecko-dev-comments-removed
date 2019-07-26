19ce440aaffade325a3a0d7f94801a5fbc4abd8b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:01:28 +0000

    Bug 979692 - Calculate hit detection and fill in region parameter to mouse events. r=roc
    UltraBlame original commit: 1f34d528c0f12425e4d9bfa2d2214b7c46f2064f

--- content/canvas/src/CanvasRenderingContext2D.h
+++ content/canvas/src/CanvasRenderingContext2D.h
@@ -537,6 +537,11 @@ public:
 
   virtual void GetImageBuffer(uint8_t** aImageBuffer, int32_t* aFormat);
 
+
+  
+  nsString GetHitRegion(const mozilla::gfx::Point& aPoint);
+
+
   
   bool GetHitRegionRect(Element* aElement, nsRect& aRect);
 
