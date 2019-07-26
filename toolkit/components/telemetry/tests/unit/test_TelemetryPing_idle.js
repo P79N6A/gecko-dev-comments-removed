f46acdcca7355743eec31248bc0be34eb2876905
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:13:47 +0000

    Bug 913070 - Move TelemetryPing to a jsm. r=Yoric, r=glandium
    UltraBlame original commit: ceb7d7bffe8ca6d9e6ff60624bdf0b9e9ea3a52d

--- toolkit/components/telemetry/tests/unit/test_TelemetryPing_idle.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryPing_idle.js
@@ -3,7 +3,10 @@
 
 
 
-Components.utils.import("resource://gre/modules/Services.jsm");
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/TelemetryPing.jsm");
 
 function run_test() {
   do_test_pending();
@@ -13,7 +16,5 @@ function run_test() {
     do_test_finished();
   }, "gather-telemetry", false);
 
-  Components.classes["@mozilla.org/base/telemetry-ping;1"]
-    .getService(Components.interfaces.nsITelemetryPing)
-    .observe(null, "idle-daily", null);
+  TelemetryPing.observe(null, "idle-daily", null);
 }
