0f889870102fc3d1accd64673a69c8dd525162f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:27:29 +0000

    Bug 1027900 - IonMonkey: Extend trySplitAfterLastRegister use to also split after register definitions. r=bhackett
    UltraBlame original commit: e5853a427dd2584269fb86cb52d23db89721d8ce

--- js/src/jit/BacktrackingAllocator.cpp
+++ js/src/jit/BacktrackingAllocator.cpp
@@ -1559,6 +1559,16 @@ BacktrackingAllocator::trySplitAfterLastRegisterUse(LiveInterval *interval, Live
 
     CodePosition lastRegisterFrom, lastRegisterTo, lastUse;
 
+    
+    
+    if (isRegisterDefinition(interval)) {
+        CodePosition spillStart = minimalDefEnd(insData[interval->start()].ins()).next();
+        if (!conflict || spillStart < conflict->start()) {
+            lastUse = lastRegisterFrom = interval->start();
+            lastRegisterTo = spillStart;
+        }
+    }
+
     for (UsePositionIterator iter(interval->usesBegin());
          iter != interval->usesEnd();
          iter++)
