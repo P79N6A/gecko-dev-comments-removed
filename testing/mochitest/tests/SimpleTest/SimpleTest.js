015ace3a4b4ad91b1f4a3d40a1e2f15de07eb310
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:32:06 +0000

    Bug 765200 - Shorten errors of type: "an unexpected uncaught JS exception reported through window.onerror - ..."; r=jmaher
    UltraBlame original commit: b87db9d6055421a76b05e325a630b9296b67fef5

--- testing/mochitest/tests/SimpleTest/SimpleTest.js
+++ testing/mochitest/tests/SimpleTest/SimpleTest.js
@@ -1058,9 +1058,8 @@ window.onerror = function simpletestOnerror(errorMsg, url, lineNumber) {
     
     
     
-    var href = SpecialPowers.getPrivilegedProps(window, 'location.href');
     var isExpected = !!SimpleTest._expectingUncaughtException;
-    var message = "an " + (isExpected ? "" : "un") + "expected uncaught JS exception reported through window.onerror";
+    var message = (isExpected ? "expected " : "") + "uncaught exception";
     var error = errorMsg + " at " + url + ":" + lineNumber;
     if (!SimpleTest._ignoringAllUncaughtExceptions) {
         SimpleTest.ok(isExpected, message, error);
