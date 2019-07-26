d189ab3ecb028e649f89a1f9665b812566f49b4e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 21:59:39 +0000

    Bug 958596 - Add a content description field to FrameMetrics and populate it if the apz.printtree pref is on. r=kats,tn
    UltraBlame original commit: a29e486e22144b82241299004734744163c5ef42

--- gfx/layers/composite/APZCTreeManager.cpp
+++ gfx/layers/composite/APZCTreeManager.cpp
@@ -16,6 +16,7 @@
 #include "mozilla/MouseEvents.h"
 #include "mozilla/mozalloc.h"           
 #include "mozilla/TouchEvents.h"
+#include "mozilla/Preferences.h"        
 #include "nsDebug.h"                    
 #include "nsPoint.h"                    
 #include "nsThreadUtils.h"              
@@ -30,12 +31,16 @@ namespace layers {
 
 float APZCTreeManager::sDPI = 160.0;
 
+
+static bool gPrintApzcTree = false;
+
 APZCTreeManager::APZCTreeManager()
     : mTreeLock("APZCTreeLock"),
       mTouchCount(0)
 {
   MOZ_ASSERT(NS_IsMainThread());
   AsyncPanZoomController::InitializeGlobalState();
+  Preferences::AddBoolVarCache(&gPrintApzcTree, "apz.printtree", gPrintApzcTree);
 }
 
 APZCTreeManager::~APZCTreeManager()
