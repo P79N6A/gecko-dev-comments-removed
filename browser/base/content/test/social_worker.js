b9acdc3e6b5893fcbdb8d9160704e5cb25be309b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:15:20 +0000

    Backed out changeset ca101d926240 (bug 787767)
    UltraBlame original commit: 1772372ea5275c0c4e18ed7ed106d01559aadee5

--- browser/base/content/test/social_worker.js
+++ browser/base/content/test/social_worker.js
@@ -76,10 +76,6 @@ onconnect = function(e) {
       case "test-worker-chat":
         apiPort.postMessage({topic: "social.request-chat", data: event.data.data });
         break;
-      case "test-worker-spam-message":
-        
-        apiPort.postMessage({topic: "social.cookies-get"});
-        break;
       case "social.initialize":
         
         apiPort = port;
