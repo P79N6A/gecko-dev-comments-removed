89cbd0e6559c8d6100f777901af56a3029d874c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:51:13 +0000

    Bug 978407 - Use glClear to clear deferred-init textures. - r=kamidphish
    UltraBlame original commit: e7576bf025af238825b9ddebdfc17ce7dcca7dc5

--- gfx/gl/ScopedGLHelpers.cpp
+++ gfx/gl/ScopedGLHelpers.cpp
@@ -102,6 +102,7 @@ ScopedBindTextureUnit::UnwrapImpl() {
 ScopedTexture::ScopedTexture(GLContext* aGL)
     : ScopedGLWrapper<ScopedTexture>(aGL)
 {
+    MOZ_ASSERT(mGL->IsCurrent());
     mGL->fGenTextures(1, &mTexture);
 }
 
@@ -111,8 +112,45 @@ ScopedTexture::UnwrapImpl()
     
     
     MOZ_ASSERT(mGL->IsCurrent());
-
     mGL->fDeleteTextures(1, &mTexture);
+}
+
+
+
+
+ScopedFramebuffer::ScopedFramebuffer(GLContext* aGL)
+    : ScopedGLWrapper<ScopedFramebuffer>(aGL)
+{
+    MOZ_ASSERT(mGL->IsCurrent());
+    mGL->fGenFramebuffers(1, &mFB);
+}
+
+void
+ScopedFramebuffer::UnwrapImpl()
+{
+    
+    
+    MOZ_ASSERT(mGL->IsCurrent());
+    mGL->fDeleteFramebuffers(1, &mFB);
+}
+
+
+
+
+ScopedRenderbuffer::ScopedRenderbuffer(GLContext* aGL)
+    : ScopedGLWrapper<ScopedRenderbuffer>(aGL)
+{
+    MOZ_ASSERT(mGL->IsCurrent());
+    mGL->fGenRenderbuffers(1, &mRB);
+}
+
+void
+ScopedRenderbuffer::UnwrapImpl()
+{
+    
+    
+    MOZ_ASSERT(mGL->IsCurrent());
+    mGL->fDeleteRenderbuffers(1, &mRB);
 }
 
 
