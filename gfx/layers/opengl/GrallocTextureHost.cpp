d02837afe4d7c16e44955dd1a695e04b66e8d390
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:11:05 +0000

    Bug 980647 - Part 2 - Use the PerFrame pool with all layer types on b2g. r=Cwiiis
    UltraBlame original commit: dcd632ee9a3e98997c40b3087b57ee1961bd162a

--- gfx/layers/opengl/GrallocTextureHost.cpp
+++ gfx/layers/opengl/GrallocTextureHost.cpp
@@ -93,7 +93,6 @@ GrallocTextureSourceOGL::GrallocTextureSourceOGL(CompositorOGL* aCompositor,
   , mGraphicBuffer(aGraphicBuffer)
   , mEGLImage(0)
   , mFormat(aFormat)
-  , mNeedsReset(true)
 {
   MOZ_ASSERT(mGraphicBuffer.get());
 }
@@ -107,14 +106,6 @@ GrallocTextureSourceOGL::~GrallocTextureSourceOGL()
 void
 GrallocTextureSourceOGL::BindTexture(GLenum aTextureUnit)
 {
-  
-
-
-
-
-
-
-
   MOZ_ASSERT(gl());
   if (!IsValid()) {
     return;
@@ -127,23 +118,18 @@ GrallocTextureSourceOGL::BindTexture(GLenum aTextureUnit)
   gl()->fActiveTexture(aTextureUnit);
   gl()->fBindTexture(textureTarget, tex);
 
-  if (mCompositableBackendData) {
-    
-    
-    
-    
-    if (!mEGLImage) {
-      mEGLImage = EGLImageCreateFromNativeBuffer(gl(), mGraphicBuffer->getNativeBuffer());
-    }
-    gl()->fEGLImageTargetTexture2D(textureTarget, mEGLImage);
-  }
-
   gl()->fActiveTexture(LOCAL_GL_TEXTURE0);
 }
 
 void GrallocTextureSourceOGL::Lock()
 {
-  if (mCompositableBackendData) return;
+  
+
+
+
+
+
+
 
   MOZ_ASSERT(IsValid());
 
@@ -163,7 +149,7 @@ void GrallocTextureSourceOGL::Lock()
 bool
 GrallocTextureSourceOGL::IsValid() const
 {
-  return !!gl() && !!mGraphicBuffer.get() && (!!mCompositor || !!mCompositableBackendData);
+  return !!gl() && !!mGraphicBuffer.get();
 }
 
 gl::GLContext*
@@ -201,51 +187,6 @@ GrallocTextureSourceOGL::GetFormat() const {
     return gfx::SurfaceFormat::R8G8B8A8;
   }
   return mFormat;
-}
-
-void
-GrallocTextureSourceOGL::SetCompositableBackendSpecificData(CompositableBackendSpecificData* aBackendData)
-{
-  if (!aBackendData) {
-    mCompositableBackendData = nullptr;
-    DeallocateDeviceData();
-    return;
-  }
-
-  if (mCompositableBackendData != aBackendData) {
-    mNeedsReset = true;
-  }
-
-  if (!mNeedsReset) {
-    
-    gl()->MakeCurrent();
-    GLuint tex = GetGLTexture();
-    GLuint textureTarget = GetTextureTarget();
-    gl()->fActiveTexture(LOCAL_GL_TEXTURE0);
-    gl()->fBindTexture(textureTarget, tex);
-    gl()->fEGLImageTargetTexture2D(textureTarget, mEGLImage);
-    return;
-  }
-
-  mCompositableBackendData = aBackendData;
-
-  if (!mCompositor) {
-    return;
-  }
-
-  
-  DeallocateDeviceData();
-
-  gl()->MakeCurrent();
-  GLuint tex = GetGLTexture();
-  GLuint textureTarget = GetTextureTarget();
-
-  gl()->fActiveTexture(LOCAL_GL_TEXTURE0);
-  gl()->fBindTexture(textureTarget, tex);
-  
-  mEGLImage = EGLImageCreateFromNativeBuffer(gl(), mGraphicBuffer->getNativeBuffer());
-  gl()->fEGLImageTargetTexture2D(textureTarget, mEGLImage);
-  mNeedsReset = false;
 }
 
 gfx::IntSize
@@ -416,11 +357,6 @@ GrallocTextureSourceOGL::GetAsSurface() {
 GLuint
 GrallocTextureSourceOGL::GetGLTexture()
 {
-  if (mCompositableBackendData) {
-    mCompositableBackendData->SetCompositor(mCompositor);
-    return static_cast<CompositableDataGonkOGL*>(mCompositableBackendData.get())->GetTexture();
-  }
-
   return mTexture;
 }
 
