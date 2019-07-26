8ee1420d5b95af16b43818c280d309a5973e0611
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:34:40 +0000

    Bug 805507 -  Only invalidate tables when the visual overflow changes. r=roc
    UltraBlame original commit: b608a82bda90b371981d81d52b73bbbd609094a8

--- layout/tables/nsTableFrame.cpp
+++ layout/tables/nsTableFrame.cpp
@@ -7297,8 +7297,9 @@ nsTableFrame::InvalidateTableFrame(nsIFrame* aFrame,
     
     aFrame->InvalidateFrame();
     parent->InvalidateFrameWithRect(aOrigVisualOverflow + aOrigRect.TopLeft());
-  } else {
-    aFrame->InvalidateFrameWithRect(aOrigVisualOverflow);;
+  } else if (aOrigRect.Size() != aFrame->GetSize() ||
+             aOrigVisualOverflow.Size() != visualOverflow.Size()){
+    aFrame->InvalidateFrameWithRect(aOrigVisualOverflow);
     aFrame->InvalidateFrame();
     parent->InvalidateFrameWithRect(aOrigRect);;
     parent->InvalidateFrame();
