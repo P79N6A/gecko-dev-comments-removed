adf7b8d276b15478f8f588b2fea029b042b384a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:42:47 +0000

    Backed out 4 changesets (c0e8f2c0465f::608c663f691f) (bug 928195) for landing prematurely
    UltraBlame original commit: 644408afbf21fe1f06cfa6693d5688ad15c8b337

---
+++ dom/bindings/ExampleGen.py
@@ -0,0 +1,46 @@
+
+
+
+
+import os
+import cPickle
+from Configuration import Configuration
+from Codegen import CGExampleRoot, replaceFileIfChanged
+
+def generate_interface_example(config, interfaceName):
+    """
+    |config| Is the configuration object.
+    |interfaceName| is the name of the interface we're generating an example for.
+    """
+
+    root = CGExampleRoot(config, interfaceName)
+    exampleHeader = interfaceName + "-example.h"
+    exampleImpl = interfaceName + "-example.cpp"
+    replaceFileIfChanged(exampleHeader, root.declare())
+    replaceFileIfChanged(exampleImpl, root.define())
+
+def main():
+
+    
+    from optparse import OptionParser
+    usagestring = "usage: %prog configFile interfaceName"
+    o = OptionParser(usage=usagestring)
+    o.add_option("--verbose-errors", action='store_true', default=False,
+                 help="When an error happens, display the Python traceback.")
+    (options, args) = o.parse_args()
+
+    if len(args) != 2:
+        o.error(usagestring)
+    configFile = os.path.normpath(args[0])
+    interfaceName = args[1]
+
+    
+    f = open('ParserResults.pkl', 'rb')
+    config = cPickle.load(f)
+    f.close()
+
+    
+    generate_interface_example(config, interfaceName)
+
+if __name__ == '__main__':
+    main()
