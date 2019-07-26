ff21190b43804424ca71eabc515b2497335cc83d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:49:14 +0000

    Bug 943839 - Make sure that JS_AnchorPtr is never inlined; r=jandem,terrence
    UltraBlame original commit: 3e9d8536f7a8da711dbb7e8879e52abaab191746

---
+++ js/src/jsanchor.cpp
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+#include "jsapi.h"
+
+
+
+#ifdef MOZ_UNIFIED_BUILD
+#error "This file cannot be built in unified mode"
+#endif
+
+JS_NEVER_INLINE JS_PUBLIC_API(void)
+JS_AnchorPtr(void *p)
+{
+}
+
