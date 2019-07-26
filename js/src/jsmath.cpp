17ba4f5978336d43fa5459277b90f5b08f7c98bf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:27:18 +0000

    Bug 899712 - Prevent infinite recursion in fallback asinh, r=jorendorff
    UltraBlame original commit: 025be6169eac6aa558eddd848fa5197440a95ba6

--- js/src/jsmath.cpp
+++ js/src/jsmath.cpp
@@ -1017,7 +1017,9 @@ js::math_acosh(JSContext *cx, unsigned argc, Value *vp)
 }
 
 #if !HAVE_ASINH
-double asinh(double x)
+
+
+static double my_asinh(double x)
 {
     const double SQUARE_ROOT_EPSILON = sqrt(std::numeric_limits<double>::epsilon());
     const double FOURTH_ROOT_EPSILON = sqrt(SQUARE_ROOT_EPSILON);
@@ -1032,7 +1034,7 @@ double asinh(double x)
         else
             return log(x + sqrt(x * x + 1));
     } else if (x <= -FOURTH_ROOT_EPSILON) {
-        return -asinh(-x);
+        return -my_asinh(-x);
     } else {
         
         
@@ -1052,7 +1054,11 @@ double asinh(double x)
 double
 js::math_asinh_impl(MathCache *cache, double x)
 {
+#ifdef HAVE_ASINH
     return cache->lookup(asinh, x);
+#else
+    return cache->lookup(my_asinh, x);
+#endif
 }
 
 bool
