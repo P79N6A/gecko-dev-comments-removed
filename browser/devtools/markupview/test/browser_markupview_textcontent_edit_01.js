3cef22199bca4c12ada354212f3b4f689fe44746
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:20:49 +0000

    Bug 968727 - Split the markupview bc attribute edition test; r=bgrins
    UltraBlame original commit: 5308c7eee7a025a79ae1e4d23063a98d87be024e

---
+++ browser/devtools/markupview/test/browser_markupview_textcontent_edit_01.js
@@ -0,0 +1,32 @@
+
+
+
+
+"use strict";
+
+
+
+const TEST_URL = TEST_URL_ROOT + "doc_markup_edit.html";
+
+let test = asyncTest(function*() {
+  let {inspector} = yield addTab(TEST_URL).then(openInspector);
+
+  info("Expanding all nodes");
+  yield inspector.markup.expandAll();
+
+  let node = getNode(".node6").firstChild;
+  is(node.nodeValue, "line6", "The test node's text content is correct");
+
+  info("Changing the text content");
+
+  info("Listening to the markupmutation event");
+  let onMutated = inspector.once("markupmutation");
+  let editor = getContainerForRawNode(node, inspector).editor;
+  let field = editor.elt.querySelector("pre");
+  setEditableFieldValue(field, "New text", inspector);
+  yield onMutated;
+
+  is(node.nodeValue, "New text", "Test test node's text content has changed");
+
+  yield inspector.once("inspector-updated");
+});
--- browser/devtools/markupview/test/head.js
+++ browser/devtools/markupview/test/head.js
@@ -381,6 +381,13 @@ function runAddAttributesTests(tests, nodeOrSelector, inspector) {
 
 
 
+
+
+
+
+
+
+
 function* runAddAttributesTest(test, nodeOrSelector, inspector) {
   let element = getNode(nodeOrSelector);
 
@@ -390,11 +397,92 @@ function* runAddAttributesTest(test, nodeOrSelector, inspector) {
   info("Assert that the attribute(s) has/have been applied correctly");
   assertAttributes(element, test.expectedAttributes);
 
+  if (test.validate) {
+    test.validate(element, getContainerForRawNode(element, inspector), inspector);
+  }
+
   info("Undo the change");
   yield undoChange(inspector);
 
   info("Assert that the attribute(s) has/have been removed correctly");
   assertAttributes(element, {});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function runEditAttributesTests(tests, inspector) {
+  info("Running " + tests.length + " edit-attributes tests");
+  return Task.spawn(function*() {
+    info("Expanding all nodes in the markup-view");
+    yield inspector.markup.expandAll();
+
+    for (let test of tests) {
+      yield runEditAttributesTest(test, inspector);
+    }
+
+    yield inspector.once("inspector-updated");
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function* runEditAttributesTest(test, inspector) {
+  info("Starting edit-attribute test: " + test.desc);
+
+  info("Selecting the test node " + test.node);
+  yield selectNode(test.node, inspector);
+
+  info("Asserting that the node has the right attributes to start with");
+  assertAttributes(test.node, test.originalAttributes);
+
+  info("Editing attribute " + test.name + " with value " + test.value);
+
+  let container = getContainerForRawNode(test.node, inspector);
+  ok(container && container.editor, "The markup-container for " + test.node +
+    " was found");
+
+  info("Listening for the markupmutation event");
+  let nodeMutated = inspector.once("markupmutation");
+  let attr = container.editor.attrs[test.name].querySelector(".editable");
+  setEditableFieldValue(attr, test.value, inspector);
+  yield nodeMutated;
+
+  info("Asserting the new attributes after edition");
+  assertAttributes(test.node, test.expectedAttributes);
+
+  info("Undo the change and assert that the attributes have been changed back");
+  yield undoChange(inspector);
+  assertAttributes(test.node, test.originalAttributes);
+
+  info("Redo the change and assert that the attributes have been changed again");
+  yield redoChange(inspector);
+  assertAttributes(test.node, test.expectedAttributes);
 }
 
 
