884a23a1cc3a50e0378a463ddbcb19b597b3b35c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:33:17 +0000

    bug 818962 ConnectionManager ProcessPendingQForEntry needs to consider that TryToDispatch.. can modify queue r=honzab
    UltraBlame original commit: a850f2aafdb145e3a4dcaee79ee88860934cee66

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -922,7 +922,6 @@ nsHttpConnectionMgr::ProcessPendingQForEntry(nsConnectionEntry *ent, bool consid
 
     ProcessSpdyPendingQ(ent);
 
-    uint32_t count = ent->mPendingQ.Length();
     nsHttpTransaction *trans;
     nsresult rv;
     bool dispatchedSuccessfully = false;
@@ -930,7 +929,7 @@ nsHttpConnectionMgr::ProcessPendingQForEntry(nsConnectionEntry *ent, bool consid
     
     
     
-    for (uint32_t i = 0; i < count; ++i) {
+    for (uint32_t i = 0; i < ent->mPendingQ.Length(); ) {
         trans = ent->mPendingQ[i];
 
         
@@ -954,22 +953,19 @@ nsHttpConnectionMgr::ProcessPendingQForEntry(nsConnectionEntry *ent, bool consid
                 LOG(("  removing pending transaction based on "
                      "TryDispatchTransaction returning hard error %x\n", rv));
 
-            ent->mPendingQ.RemoveElementAt(i);
-            NS_RELEASE(trans);
+            if (ent->mPendingQ.RemoveElement(trans)) {
+                dispatchedSuccessfully = true;
+                NS_RELEASE(trans);
+                continue; 
+            }
 
-            
-            dispatchedSuccessfully = true;
-            count = ent->mPendingQ.Length();
-            --i;
-            continue;
+            LOG(("  transaction not found in pending queue\n"));
         }
 
         if (dispatchedSuccessfully && !considerAll)
             break;
-    
-        NS_ABORT_IF_FALSE(count == ent->mPendingQ.Length(),
-                          "something mutated pending queue from "
-                          "GetConnection()");
+
+        ++i;
     }
     return dispatchedSuccessfully;
 }
