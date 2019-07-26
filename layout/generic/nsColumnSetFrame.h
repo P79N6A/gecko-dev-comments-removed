214adff6cb163799eb35423d558980338aab1065
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:12:13 +0000

    Bug 1015844 - part 1, Only force the search to normal flow child lists for true overflow containers.  r=roc
    UltraBlame original commit: 516ad717d7f4bdae076c5c45ae13ccee1ec93c75

--- layout/generic/nsColumnSetFrame.h
+++ layout/generic/nsColumnSetFrame.h
@@ -7,6 +7,7 @@
 
 #include "mozilla/Attributes.h"
 #include "nsContainerFrame.h"
+#include "nsIFrameInlines.h" 
 
 class nsColumnSetFrame : public nsContainerFrame {
 public:
@@ -51,8 +52,11 @@ public:
   }
 
   virtual nsresult StealFrame(nsIFrame* aChild, bool aForceNormal) MOZ_OVERRIDE
-  { 
-    return nsContainerFrame::StealFrame(aChild, true);
+  {
+    
+    
+    return nsContainerFrame::StealFrame(aChild,
+                                        IS_TRUE_OVERFLOW_CONTAINER(aChild));
   }
 
   virtual bool IsFrameOfType(uint32_t aFlags) const MOZ_OVERRIDE
