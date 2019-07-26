fe3a0eca32b58f0b4d99a0ac2e93548befb7f45a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 21:59:03 +0000

    Bug 939672 - Patch 1/2: [bluedroid] Add fallback BluetoothHfpManager, r=gyeh
    UltraBlame original commit: 498aa10dff2f7e2914dbb3c67fc6981c77361104

---
+++ dom/bluetooth/bluedroid/hfp-fallback/BluetoothHfpManager.h
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_bluetooth_bluetoothhfpmanager_h__
+#define mozilla_dom_bluetooth_bluetoothhfpmanager_h__
+
+#include "BluetoothHfpManagerBase.h"
+
+
+
+
+
+
+
+
+
+BEGIN_BLUETOOTH_NAMESPACE
+
+class BluetoothHfpManager : public BluetoothHfpManagerBase
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIOBSERVER
+  BT_DECL_HFP_MGR_BASE
+  virtual void GetName(nsACString& aName)
+  {
+    aName.AssignLiteral("Fallback HFP/HSP");
+  }
+
+  static BluetoothHfpManager* Get();
+  virtual ~BluetoothHfpManager() { }
+
+  bool ConnectSco();
+  bool DisconnectSco();
+
+private:
+  BluetoothHfpManager() { }
+  bool Init();
+  void HandleShutdown();
+};
+
+END_BLUETOOTH_NAMESPACE
+
+#endif
