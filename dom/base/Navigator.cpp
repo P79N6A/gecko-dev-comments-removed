ec2d8601ed166609831b254d56c701f935ce1143
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:23:37 +0000

    Merge birch to inbound.
    UltraBlame original commit: 2e90c00318f314731c79c233bc8e3760ef940a56

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -19,7 +19,6 @@
 #include "nsIWebContentHandlerRegistrar.h"
 #include "nsICookiePermission.h"
 #include "nsIScriptSecurityManager.h"
-#include "nsIJSContextStack.h"
 #include "nsCharSeparatedTokenizer.h"
 #include "nsContentUtils.h"
 #include "nsUnicharUtils.h"
@@ -75,8 +74,6 @@ DOMCI_DATA(Navigator, mozilla::dom::Navigator)
 
 namespace mozilla {
 namespace dom {
-
-static const char sJSStackContractID[] = "@mozilla.org/js/xpc/ContextStack;1";
 
 static bool sDoNotTrackEnabled = false;
 static bool sVibratorEnabled   = false;
@@ -890,11 +887,7 @@ Navigator::MozIsLocallyAvailable(const nsAString &aURI,
   }
 
   
-  nsCOMPtr<nsIJSContextStack> stack = do_GetService(sJSStackContractID);
-  NS_ENSURE_TRUE(stack, NS_ERROR_FAILURE);
-
-  JSContext* cx = nullptr;
-  rv = stack->Peek(&cx);
+  JSContext *cx = nsContentUtils::GetCurrentJSContext();
   NS_ENSURE_TRUE(cx, NS_ERROR_FAILURE);
 
   rv = nsContentUtils::GetSecurityManager()->CheckSameOrigin(cx, uri);
