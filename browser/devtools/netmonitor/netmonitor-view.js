71365b1918f1a9fb0c9d0e042369fe2983c6ac8d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:21:19 +0000

    Backed out changeset dff900f56c49 (bug 987310) for orange in bc2 linux tests
    UltraBlame original commit: a26977bf2252f5fae46c9fc5da299ccb4d14aceb

--- browser/devtools/netmonitor/netmonitor-view.js
+++ browser/devtools/netmonitor/netmonitor-view.js
@@ -700,11 +700,6 @@ RequestsMenuView.prototype = Heritage.extend(WidgetMethods, {
 
   _enableFilter: function (aType) {
     
-    if (Object.keys(this._allFilterPredicates).indexOf(aType) == -1) {
-      return;
-    }
-
-    
     this._activeFilters.push(aType);
 
     
@@ -722,7 +717,18 @@ RequestsMenuView.prototype = Heritage.extend(WidgetMethods, {
 
 
   get _filterPredicate() {
-    let filterPredicates = this._allFilterPredicates;
+    let filterPredicates = {
+      "all": () => true,
+      "html": this.isHtml,
+      "css": this.isCss,
+      "js": this.isJs,
+      "xhr": this.isXHR,
+      "fonts": this.isFont,
+      "images": this.isImage,
+      "media": this.isMedia,
+      "flash": this.isFlash,
+      "other": this.isOther
+    };
 
      if (this._activeFilters.length === 1) {
        
@@ -736,22 +742,6 @@ RequestsMenuView.prototype = Heritage.extend(WidgetMethods, {
        };
      }
   },
-
-  
-
-
-  get _allFilterPredicates() ({
-    all: () => true,
-    html: this.isHtml,
-    css: this.isCss,
-    js: this.isJs,
-    xhr: this.isXHR,
-    fonts: this.isFont,
-    images: this.isImage,
-    media: this.isMedia,
-    flash: this.isFlash,
-    other: this.isOther
-  }),
 
   
 
