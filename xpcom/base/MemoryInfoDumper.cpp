10f6b21d06a048d60e2eed9afcd617f6b878813e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:12:30 +0000

    Bug 800486 - Part 2: Move signal-watching and memory-report-dumping code out of nsMemoryReporterManager and into a new file, MemoryInfoDumper. r=njn
    UltraBlame original commit: 295110f8c8922817f9e0e001a6b47d7cc9beb9a3

---
+++ xpcom/base/MemoryInfoDumper.cpp
@@ -0,0 +1,599 @@
+
+
+
+
+
+
+#include "mozilla/MemoryInfoDumper.h"
+
+#include "mozilla/ClearOnShutdown.h"
+#include "mozilla/FileUtils.h"
+#include "mozilla/StaticPtr.h"
+#include "mozilla/unused.h"
+#include "mozilla/dom/ContentParent.h"
+#include "mozilla/dom/ContentChild.h"
+#include "nsIConsoleService.h"
+#include "nsICycleCollectorListener.h"
+#include "nsDirectoryServiceDefs.h"
+#include "nsGZFileWriter.h"
+#include "nsJSEnvironment.h"
+#include "nsPrintfCString.h"
+
+#ifdef XP_LINUX
+#include <fcntl.h>
+#endif
+
+#ifdef ANDROID
+#include <sys/types.h>
+#include <sys/stat.h>
+#endif
+
+using namespace mozilla;
+using namespace mozilla::dom;
+
+namespace mozilla {
+namespace {
+
+class DumpMemoryReportsRunnable : public nsRunnable
+{
+public:
+  DumpMemoryReportsRunnable(const nsAString& aIdentifier,
+                            bool aMinimizeMemoryUsage,
+                            bool aDumpChildProcesses)
+
+      : mIdentifier(aIdentifier)
+      , mMinimizeMemoryUsage(aMinimizeMemoryUsage)
+      , mDumpChildProcesses(aDumpChildProcesses)
+  {}
+
+  NS_IMETHOD Run()
+  {
+    MemoryInfoDumper::DumpMemoryReportsToFile(
+      mIdentifier, mMinimizeMemoryUsage, mDumpChildProcesses);
+    return NS_OK;
+  }
+
+private:
+  const nsString mIdentifier;
+  const bool mMinimizeMemoryUsage;
+  const bool mDumpChildProcesses;
+};
+
+class GCAndCCLogDumpRunnable : public nsRunnable
+{
+public:
+  GCAndCCLogDumpRunnable(const nsAString& aIdentifier,
+                         bool aDumpChildProcesses)
+    : mIdentifier(aIdentifier)
+    , mDumpChildProcesses(aDumpChildProcesses)
+  {}
+
+  NS_IMETHOD Run()
+  {
+    MemoryInfoDumper::DumpGCAndCCLogsToFile(
+      mIdentifier, mDumpChildProcesses);
+    return NS_OK;
+  }
+
+private:
+  const nsString mIdentifier;
+  const bool mDumpChildProcesses;
+};
+
+} 
+
+#ifdef XP_LINUX 
+namespace {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static int sDumpAboutMemorySignum;         
+static int sDumpAboutMemoryAfterMMUSignum; 
+static int sGCAndCCDumpSignum;             
+
+
+
+static int sDumpAboutMemoryPipeWriteFd;
+
+void
+DumpAboutMemorySignalHandler(int aSignum)
+{
+  
+  
+
+  if (sDumpAboutMemoryPipeWriteFd != 0) {
+    uint8_t signum = static_cast<int>(aSignum);
+    write(sDumpAboutMemoryPipeWriteFd, &signum, sizeof(signum));
+  }
+}
+
+class SignalPipeWatcher : public MessageLoopForIO::Watcher
+{
+public:
+  SignalPipeWatcher()
+  {}
+
+  ~SignalPipeWatcher()
+  {
+    
+    
+    
+    int pipeWriteFd = sDumpAboutMemoryPipeWriteFd;
+    PR_ATOMIC_SET(&sDumpAboutMemoryPipeWriteFd, 0);
+
+    
+    mReadWatcher.StopWatchingFileDescriptor();
+
+    close(pipeWriteFd);
+    close(mPipeReadFd);
+  }
+
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(SignalPipeWatcher)
+
+  bool Start()
+  {
+    MOZ_ASSERT(XRE_GetIOMessageLoop() == MessageLoopForIO::current());
+
+    sDumpAboutMemorySignum = SIGRTMIN;
+    sDumpAboutMemoryAfterMMUSignum = SIGRTMIN + 1;
+    sGCAndCCDumpSignum = SIGRTMIN + 2;
+
+    
+    
+    int pipeFds[2];
+    if (pipe(pipeFds)) {
+      NS_WARNING("Failed to create pipe.");
+      return false;
+    }
+
+    
+    fcntl(pipeFds[0], F_SETFD, FD_CLOEXEC);
+    fcntl(pipeFds[1], F_SETFD, FD_CLOEXEC);
+
+    mPipeReadFd = pipeFds[0];
+    sDumpAboutMemoryPipeWriteFd = pipeFds[1];
+
+    struct sigaction action;
+    memset(&action, 0, sizeof(action));
+    sigemptyset(&action.sa_mask);
+    action.sa_handler = DumpAboutMemorySignalHandler;
+
+    if (sigaction(sDumpAboutMemorySignum, &action, nullptr)) {
+      NS_WARNING("Failed to register about:memory dump signal handler.");
+    }
+    if (sigaction(sDumpAboutMemoryAfterMMUSignum, &action, nullptr)) {
+      NS_WARNING("Failed to register about:memory dump after MMU signal handler.");
+    }
+    if (sigaction(sGCAndCCDumpSignum, &action, nullptr)) {
+      NS_WARNING("Failed to register GC+CC dump signal handler.");
+    }
+
+    
+    return MessageLoopForIO::current()->WatchFileDescriptor(
+      mPipeReadFd,  true,
+      MessageLoopForIO::WATCH_READ,
+      &mReadWatcher, this);
+  }
+
+  virtual void OnFileCanReadWithoutBlocking(int aFd)
+  {
+    MOZ_ASSERT(XRE_GetIOMessageLoop() == MessageLoopForIO::current());
+
+    uint8_t signum;
+    ssize_t numReceived = read(aFd, &signum, sizeof(signum));
+    if (numReceived != sizeof(signum)) {
+      NS_WARNING("Error reading from buffer in "
+                 "SignalPipeWatcher::OnFileCanReadWithoutBlocking.");
+      return;
+    }
+
+    if (signum == sDumpAboutMemorySignum ||
+        signum == sDumpAboutMemoryAfterMMUSignum) {
+      
+      nsRefPtr<DumpMemoryReportsRunnable> runnable =
+        new DumpMemoryReportsRunnable(
+             EmptyString(),
+            signum == sDumpAboutMemoryAfterMMUSignum,
+             true);
+      NS_DispatchToMainThread(runnable);
+    }
+    else if (signum == sGCAndCCDumpSignum) {
+      
+      nsRefPtr<GCAndCCLogDumpRunnable> runnable =
+        new GCAndCCLogDumpRunnable(
+             EmptyString(),
+             true);
+      NS_DispatchToMainThread(runnable);
+    }
+    else {
+      NS_WARNING("Got unexpected signum.");
+    }
+  }
+
+  virtual void OnFileCanWriteWithoutBlocking(int aFd)
+  {}
+
+private:
+  int mPipeReadFd;
+  MessageLoopForIO::FileDescriptorWatcher mReadWatcher;
+};
+
+StaticRefPtr<SignalPipeWatcher> sSignalPipeWatcher;
+
+void
+InitializeSignalWatcher()
+{
+  MOZ_ASSERT(NS_IsMainThread());
+  MOZ_ASSERT(!sSignalPipeWatcher);
+
+  sSignalPipeWatcher = new SignalPipeWatcher();
+  ClearOnShutdown(&sSignalPipeWatcher);
+
+  XRE_GetIOMessageLoop()->PostTask(
+      FROM_HERE,
+      NewRunnableMethod(sSignalPipeWatcher.get(),
+                        &SignalPipeWatcher::Start));
+}
+
+} 
+#endif 
+
+ void
+MemoryInfoDumper::Initialize()
+{
+#ifdef XP_LINUX
+  InitializeSignalWatcher();
+#endif
+}
+
+   void
+MemoryInfoDumper::DumpMemoryReportsToFile(
+    const nsAString& aIdentifier,
+    bool aMinimizeMemoryUsage,
+    bool aDumpChildProcesses)
+{
+  
+  
+  
+  
+  nsString identifier(aIdentifier);
+  if (identifier.IsEmpty()) {
+    identifier.AppendInt(static_cast<int64_t>(PR_Now()) / 1000000);
+  }
+
+  
+  
+  
+  
+  if (aDumpChildProcesses) {
+    nsTArray<ContentParent*> children;
+    ContentParent::GetAll(children);
+    for (uint32_t i = 0; i < children.Length(); i++) {
+      unused << children[i]->SendDumpMemoryReportsToFile(
+          identifier, aMinimizeMemoryUsage, aDumpChildProcesses);
+    }
+  }
+
+  if (aMinimizeMemoryUsage) {
+    
+    nsRefPtr<DumpMemoryReportsRunnable> callback =
+      new DumpMemoryReportsRunnable(identifier,
+           false,
+           false);
+    nsCOMPtr<nsIMemoryReporterManager> mgr =
+      do_GetService("@mozilla.org/memory-reporter-manager;1");
+    NS_ENSURE_TRUE(mgr,);
+    mgr->MinimizeMemoryUsage(callback);
+  }
+
+  DumpMemoryReportsToFileImpl(identifier);
+}
+
+ void
+MemoryInfoDumper::DumpGCAndCCLogsToFile(
+  const nsAString& aIdentifier,
+  bool aDumpChildProcesses)
+{
+  
+  
+  
+  
+  nsString identifier(aIdentifier);
+  if (identifier.IsEmpty()) {
+    identifier.AppendInt(static_cast<int64_t>(PR_Now()) / 1000000);
+  }
+
+  if (aDumpChildProcesses) {
+    nsTArray<ContentParent*> children;
+    ContentParent::GetAll(children);
+    for (uint32_t i = 0; i < children.Length(); i++) {
+      unused << children[i]->SendDumpGCAndCCLogsToFile(
+          identifier, aDumpChildProcesses);
+    }
+  }
+
+  nsCOMPtr<nsICycleCollectorListener> logger =
+    do_CreateInstance("@mozilla.org/cycle-collector-logger;1");
+  logger->SetFilenameIdentifier(identifier);
+
+  nsJSContext::CycleCollectNow(logger);
+}
+
+#define DUMP(o, s) \
+  do { \
+    nsresult rv = (o)->Write(s); \
+    NS_ENSURE_SUCCESS(rv, rv); \
+  } while (0)
+
+static nsresult
+DumpReport(nsIGZFileWriter *aWriter, bool aIsFirst,
+  const nsACString &aProcess, const nsACString &aPath, int32_t aKind,
+  int32_t aUnits, int64_t aAmount, const nsACString &aDescription)
+{
+  DUMP(aWriter, aIsFirst ? "[" : ",");
+
+  
+  
+  
+  if (!aProcess.IsEmpty()) {
+    return NS_OK;
+  }
+
+  
+  
+  
+  nsAutoCString processId;
+  if (XRE_GetProcessType() == GeckoProcessType_Default) {
+    
+    processId.AssignLiteral("Main Process ");
+  } else if (ContentChild *cc = ContentChild::GetSingleton()) {
+    
+    nsAutoString processName;
+    cc->GetProcessName(processName);
+    processId.Assign(NS_ConvertUTF16toUTF8(processName));
+    if (!processId.IsEmpty()) {
+      processId.AppendLiteral(" ");
+    }
+  }
+
+  
+  unsigned pid = getpid();
+  processId.Append(nsPrintfCString("(pid %u)", pid));
+
+  DUMP(aWriter, "\n    {\"process\": \"");
+  DUMP(aWriter, processId);
+
+  DUMP(aWriter, "\", \"path\": \"");
+  nsCString path(aPath);
+  path.ReplaceSubstring("\\", "\\\\");    
+  DUMP(aWriter, path);
+
+  DUMP(aWriter, "\", \"kind\": ");
+  DUMP(aWriter, nsPrintfCString("%d", aKind));
+
+  DUMP(aWriter, ", \"units\": ");
+  DUMP(aWriter, nsPrintfCString("%d", aUnits));
+
+  DUMP(aWriter, ", \"amount\": ");
+  DUMP(aWriter, nsPrintfCString("%lld", aAmount));
+
+  nsCString description(aDescription);
+  description.ReplaceSubstring("\\", "\\\\");    
+  description.ReplaceSubstring("\"", "\\\"");    
+  description.ReplaceSubstring("\n", "\\n");     
+  DUMP(aWriter, ", \"description\": \"");
+  DUMP(aWriter, description);
+  DUMP(aWriter, "\"}");
+
+  return NS_OK;
+}
+
+class DumpMultiReporterCallback MOZ_FINAL : public nsIMemoryMultiReporterCallback
+{
+  public:
+    NS_DECL_ISUPPORTS
+
+      NS_IMETHOD Callback(const nsACString &aProcess, const nsACString &aPath,
+          int32_t aKind, int32_t aUnits, int64_t aAmount,
+          const nsACString &aDescription,
+          nsISupports *aData)
+      {
+        nsCOMPtr<nsIGZFileWriter> writer = do_QueryInterface(aData);
+        NS_ENSURE_TRUE(writer, NS_ERROR_FAILURE);
+
+        
+        
+        
+        return DumpReport(writer,  false, aProcess, aPath,
+            aKind, aUnits, aAmount, aDescription);
+        return NS_OK;
+      }
+};
+
+NS_IMPL_ISUPPORTS1(
+    DumpMultiReporterCallback
+    , nsIMemoryMultiReporterCallback
+    )
+
+ nsresult
+MemoryInfoDumper::DumpMemoryReportsToFileImpl(
+  const nsAString& aIdentifier)
+{
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  nsCOMPtr<nsIFile> tmpFile;
+  nsresult rv = NS_GetSpecialDirectory(NS_OS_TEMP_DIR,
+                                       getter_AddRefs(tmpFile));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  
+  
+  nsAutoCString filename;
+  filename.AppendLiteral("memory-report");
+  if (!aIdentifier.IsEmpty()) {
+    filename.AppendLiteral("-");
+    filename.Append(NS_ConvertUTF16toUTF8(aIdentifier));
+  }
+  filename.AppendLiteral("-");
+  filename.AppendInt(getpid());
+  filename.AppendLiteral(".json.gz");
+
+  rv = tmpFile->AppendNative(NS_LITERAL_CSTRING("incomplete-") + filename);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  
+  
+#ifdef ANDROID
+    
+    
+    mode_t mask = umask(0);
+#endif
+    rv = tmpFile->CreateUnique(nsIFile::NORMAL_FILE_TYPE, 0644);
+#ifdef ANDROID
+    umask(mask);
+#endif
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsRefPtr<nsGZFileWriter> writer = new nsGZFileWriter();
+  rv = writer->Init(tmpFile);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  
+
+  
+  DUMP(writer, "{\n  \"version\": 1,\n");
+
+  DUMP(writer, "  \"hasMozMallocUsableSize\": ");
+
+  nsCOMPtr<nsIMemoryReporterManager> mgr =
+    do_GetService("@mozilla.org/memory-reporter-manager;1");
+  NS_ENSURE_STATE(mgr);
+
+  DUMP(writer, mgr->GetHasMozMallocUsableSize() ? "true" : "false");
+  DUMP(writer, ",\n");
+  DUMP(writer, "  \"reports\": ");
+
+  
+  bool isFirst = true;
+  bool more;
+  nsCOMPtr<nsISimpleEnumerator> e;
+  mgr->EnumerateReporters(getter_AddRefs(e));
+  while (NS_SUCCEEDED(e->HasMoreElements(&more)) && more) {
+    nsCOMPtr<nsIMemoryReporter> r;
+    e->GetNext(getter_AddRefs(r));
+
+    nsCString process;
+    rv = r->GetProcess(process);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    nsCString path;
+    rv = r->GetPath(path);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    int32_t kind;
+    rv = r->GetKind(&kind);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    int32_t units;
+    rv = r->GetUnits(&units);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    int64_t amount;
+    rv = r->GetAmount(&amount);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    nsCString description;
+    rv = r->GetDescription(description);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    rv = DumpReport(writer, isFirst, process, path, kind, units, amount,
+                    description);
+    NS_ENSURE_SUCCESS(rv, rv);
+
+    isFirst = false;
+  }
+
+  
+  nsCOMPtr<nsISimpleEnumerator> e2;
+  mgr->EnumerateMultiReporters(getter_AddRefs(e2));
+  nsRefPtr<DumpMultiReporterCallback> cb = new DumpMultiReporterCallback();
+  while (NS_SUCCEEDED(e2->HasMoreElements(&more)) && more) {
+    nsCOMPtr<nsIMemoryMultiReporter> r;
+    e2->GetNext(getter_AddRefs(r));
+    r->CollectReports(cb, writer);
+  }
+
+  DUMP(writer, "\n  ]\n}");
+
+  rv = writer->Finish();
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  
+  
+
+  nsCOMPtr<nsIFile> dstFile;
+  rv = NS_GetSpecialDirectory(NS_OS_TEMP_DIR, getter_AddRefs(dstFile));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  rv = dstFile->AppendNative(filename);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  rv = dstFile->CreateUnique(nsIFile::NORMAL_FILE_TYPE, 0600);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsAutoString dstFileName;
+  rv = dstFile->GetLeafName(dstFileName);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  rv = tmpFile->MoveTo( nullptr, dstFileName);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<nsIConsoleService> cs =
+      do_GetService(NS_CONSOLESERVICE_CONTRACTID, &rv);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsString path;
+  tmpFile->GetPath(path);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsString msg = NS_LITERAL_STRING("nsIMemoryReporterManager::dumpReports() "
+                                   "dumped reports to ");
+  msg.Append(path);
+  return cs->LogStringMessage(msg.get());
+}
+
+#undef DUMP
+} 
