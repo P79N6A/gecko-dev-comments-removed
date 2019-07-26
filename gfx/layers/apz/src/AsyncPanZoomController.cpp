30cea7c533c6a9a93f11737e41710c8ca8b67d58
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:15:38 +0000

    Bug 1018255 - Fix a short-circuit evaluation bug in OverscrollSnapBackAnimation::Sample. r=kats
    UltraBlame original commit: 1495e351283ef0d3474f7bfc2835fcfa61ae6890

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -488,8 +488,10 @@ public:
   virtual bool Sample(FrameMetrics& aFrameMetrics,
                       const TimeDuration& aDelta) MOZ_OVERRIDE
   {
-    return mApzc.mX.SampleSnapBack(aDelta)
-        || mApzc.mY.SampleSnapBack(aDelta);
+    
+    bool continueX = mApzc.mX.SampleSnapBack(aDelta);
+    bool continueY = mApzc.mY.SampleSnapBack(aDelta);
+    return continueX || continueY;
   }
 
 private:
