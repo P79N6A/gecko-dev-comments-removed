e451f10d1651f171dde2e4d9e704a2bbe7de7a86
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:53:00 +0000

    Bug 825804 - Popup notifications and notification bars shouldn't persist across page reloads. r=gavin
    UltraBlame original commit: 3f192cae69e2c7d3087470c6191e2128e00ebb08

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4196,7 +4196,6 @@ var XULBrowserWindow = {
   },
 
   onLocationChange: function (aWebProgress, aRequest, aLocationURI, aFlags) {
-    const nsIWebProgressListener = Ci.nsIWebProgressListener;
     var location = aLocationURI ? aLocationURI.spec : "";
     this._hostChanged = true;
 
@@ -4222,38 +4221,6 @@ var XULBrowserWindow = {
             break;
           }
         }
-      }
-    }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    var selectedBrowser = gBrowser.selectedBrowser;
-    if (selectedBrowser.lastURI) {
-      let oldSpec = selectedBrowser.lastURI.spec;
-      let oldIndexOfHash = oldSpec.indexOf("#");
-      if (oldIndexOfHash != -1)
-        oldSpec = oldSpec.substr(0, oldIndexOfHash);
-      let newSpec = location;
-      let newIndexOfHash = newSpec.indexOf("#");
-      if (newIndexOfHash != -1)
-        newSpec = newSpec.substr(0, newIndexOfHash);
-      if (newSpec != oldSpec) {
-        
-        
-        let nBox = gBrowser.getNotificationBox(selectedBrowser);
-        nBox.removeTransientNotifications();
-
-        
-        
-        
-        if (!__lookupGetter__("PopupNotifications"))
-          PopupNotifications.locationChange();
       }
     }
 
@@ -4288,6 +4255,18 @@ var XULBrowserWindow = {
         
         PlacesStarButton.updateState();
         SocialShareButton.updateShareState();
+      }
+
+      
+      
+      if (aRequest) {
+        
+        
+        
+        
+        
+        if (!__lookupGetter__("PopupNotifications"))
+          PopupNotifications.locationChange();
       }
 
       
@@ -4332,7 +4311,7 @@ var XULBrowserWindow = {
           (aLocationURI.schemeIs("about") || aLocationURI.schemeIs("chrome"))) {
         
         
-        if (!(aFlags & nsIWebProgressListener.LOCATION_CHANGE_SAME_DOCUMENT)) {
+        if (!(aFlags & Ci.nsIWebProgressListener.LOCATION_CHANGE_SAME_DOCUMENT)) {
           if (content.document.readyState == "interactive" || content.document.readyState == "complete")
             disableFindCommands(shouldDisableFind(content.document));
           else {
@@ -4708,12 +4687,17 @@ var TabsProgressListener = {
     if (aBrowser.contentWindow == aWebProgress.DOMWindow) {
       
       
-      if (aRequest) {
+      if (!(aFlags & Ci.nsIWebProgressListener.LOCATION_CHANGE_SAME_DOCUMENT)) {
         
         aBrowser._clickToPlayPluginsActivated = new Map();
         aBrowser._clickToPlayAllPluginsActivated = false;
         aBrowser._pluginScriptedState = gPluginHandler.PLUGIN_SCRIPTED_STATE_NONE;
+
+        
+        
+        gBrowser.getNotificationBox(aBrowser).removeTransientNotifications();
       }
+
       FullZoom.onLocationChange(aLocationURI, false, aBrowser);
     }
   },
