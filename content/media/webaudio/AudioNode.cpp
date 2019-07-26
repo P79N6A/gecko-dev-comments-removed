79b0f6b2958be346cb0019b0dfa481d940863805
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:14:26 +0000

    Backed out changeset 9c080ffa640f (bug 853551) because of mochitest-1 crashes
    UltraBlame original commit: d65cf43c471748c967c1d3b52f70aa3f0e23b72d

--- content/media/webaudio/AudioNode.cpp
+++ content/media/webaudio/AudioNode.cpp
@@ -173,9 +173,6 @@ AudioNode::Connect(AudioNode& aDestination, uint32_t aOutput,
     input->mStreamPort =
       ps->AllocateInputPort(mStream, MediaInputPort::FLAG_BLOCK_INPUT);
   }
-
-  
-  Context()->UpdatePannerSource();
 }
 
 void
@@ -242,9 +239,6 @@ AudioNode::Disconnect(uint32_t aOutput, ErrorResult& aRv)
   for (uint32_t i = 0; i < outputsToUpdate.Length(); ++i) {
     outputsToUpdate[i]->UpdateOutputEnded();
   }
-
-  
-  Context()->UpdatePannerSource();
 }
 
 }
