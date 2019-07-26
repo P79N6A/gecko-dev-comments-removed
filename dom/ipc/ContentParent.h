17af6f4a19a8d6cd0c02cb67aabbf195eafbef9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:10:46 +0000

    Bug 847656: Change ContentParent::GetNewOrUsed to return an already_AddRefed value. r=khuey
    UltraBlame original commit: 42d5d3080bc4a6ca6eb35167dfc82ad462d0e5e2

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -81,7 +81,8 @@ public:
 
     static void JoinAllSubprocesses();
 
-    static ContentParent* GetNewOrUsed(bool aForBrowserElement = false);
+    static already_AddRefed<ContentParent>
+    GetNewOrUsed(bool aForBrowserElement = false);
 
     
 
