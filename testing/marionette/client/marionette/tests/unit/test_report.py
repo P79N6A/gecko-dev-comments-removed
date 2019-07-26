9dd925a3d79918f03f79877c2ca7616115535b4e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:55:01 +0000

    Bug 886741 - Fix support for expectedFailure and skip, r=jgriffin
    UltraBlame original commit: 33c2369d8db2e6f65818830ccb7f2b1cc33c7cc2

---
+++ testing/marionette/client/marionette/tests/unit/test_report.py
@@ -0,0 +1,30 @@
+
+
+
+
+import unittest
+from marionette_test import MarionetteTestCase, expectedFailure, skip
+
+
+class TestReport(MarionetteTestCase):
+
+    def test_pass(self):
+        assert True
+
+    def test_fail(self):
+        assert False
+
+    @skip('Skip Message')
+    def test_skip(self):
+        assert False
+
+    @expectedFailure
+    def test_expected_fail(self):
+        assert False
+
+    @expectedFailure
+    def test_unexpected_pass(self):
+        assert True
+
+    def test_error(self):
+        raise Exception()
