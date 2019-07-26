d574e338c079bb5fbbeecd2b50ddff12770d456a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:49:30 +0000

    Bug 990713 - Update directoryLinks to have actual links and images [r=adw] Also fixes Bug 1000459 - revert bug 993581 to turn directory tiles on Allows for smaller than full size images by centering vertically and horizontally.
    UltraBlame original commit: 79989d4a8d24a5135fa5b02bedf1ce0c24052f30

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1428,7 +1428,7 @@ pref("browser.newtabpage.rows", 3);
 
 pref("browser.newtabpage.columns", 3);
 
-pref("browser.newtabpage.directorySource", "data:application/json,{}");
+pref("browser.newtabpage.directorySource", "chrome://global/content/directoryLinks.json");
 
 
 pref("full-screen-api.enabled", true);
