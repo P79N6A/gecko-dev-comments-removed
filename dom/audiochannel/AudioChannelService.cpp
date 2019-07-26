d8735fc6c5e9025704831f2acc8433a2326e1268
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:47:43 +0000

    Bug 832665 - Switching back and forth between the music app and FM radio (using home key) causes some unexpected behavior., r=mchen, a=tef+
    UltraBlame original commit: a0c4559f5d2e8aff1acff339960594b5122c16a2

--- dom/audiochannel/AudioChannelService.cpp
+++ dom/audiochannel/AudioChannelService.cpp
@@ -67,6 +67,7 @@ NS_IMPL_ISUPPORTS0(AudioChannelService)
 
 AudioChannelService::AudioChannelService()
 : mCurrentHigherChannel(AUDIO_CHANNEL_LAST)
+, mActiveContentChildIDsFrozen(false)
 {
   
   mAgents.Init();
@@ -170,6 +171,27 @@ AudioChannelService::GetMutedInternal(AudioChannelType aType, uint64_t aChildID,
   }
 
   
+  if (newType == AUDIO_CHANNEL_INT_CONTENT &&
+      oldType == AUDIO_CHANNEL_INT_CONTENT_HIDDEN &&
+      !mActiveContentChildIDs.Contains(aChildID)) {
+
+    if (mActiveContentChildIDsFrozen) {
+      mActiveContentChildIDsFrozen = false;
+      mActiveContentChildIDs.Clear();
+    }
+
+    mActiveContentChildIDs.AppendElement(aChildID);
+  }
+
+  
+  else if (newType == AUDIO_CHANNEL_INT_CONTENT_HIDDEN &&
+           oldType == AUDIO_CHANNEL_INT_CONTENT &&
+           !mActiveContentChildIDsFrozen &&
+           mChannelCounters[AUDIO_CHANNEL_INT_CONTENT].IsEmpty()) {
+    mActiveContentChildIDsFrozen = true;
+  }
+
+  
   if (!aElementHidden) {
     return false;
   }
@@ -179,8 +201,7 @@ AudioChannelService::GetMutedInternal(AudioChannelType aType, uint64_t aChildID,
   
   if (newType == AUDIO_CHANNEL_INT_NORMAL_HIDDEN ||
       (newType == AUDIO_CHANNEL_INT_CONTENT_HIDDEN &&
-       (!mChannelCounters[AUDIO_CHANNEL_INT_CONTENT].IsEmpty() ||
-        HasMoreThanOneContentChannelHidden()))) {
+       !mActiveContentChildIDs.Contains(aChildID))) {
     muted = true;
   }
 
@@ -197,25 +218,6 @@ AudioChannelService::ContentChannelIsActive()
 {
   return !mChannelCounters[AUDIO_CHANNEL_INT_CONTENT].IsEmpty() ||
          !mChannelCounters[AUDIO_CHANNEL_INT_CONTENT_HIDDEN].IsEmpty();
-}
-
-bool
-AudioChannelService::HasMoreThanOneContentChannelHidden()
-{
-  uint32_t childId = CONTENT_PARENT_UNKNOWN_CHILD_ID;
-  bool empty = true;
-  for (uint32_t i = 0;
-       i < mChannelCounters[AUDIO_CHANNEL_INT_CONTENT_HIDDEN].Length();
-       ++i) {
-    if (empty) {
-      childId = mChannelCounters[AUDIO_CHANNEL_INT_CONTENT_HIDDEN][i];
-      empty = false;
-    } else if (childId != mChannelCounters[AUDIO_CHANNEL_INT_CONTENT_HIDDEN][i]) {
-      return true;
-    }
-  }
-
-  return false;
 }
 
 void
@@ -279,8 +281,7 @@ AudioChannelService::SendAudioChannelChangedNotification()
   }
 
   
-  else if ((!mChannelCounters[AUDIO_CHANNEL_INT_CONTENT_HIDDEN].IsEmpty() &&
-            !HasMoreThanOneContentChannelHidden())) {
+  else if (!mActiveContentChildIDs.IsEmpty()) {
     higher = AUDIO_CHANNEL_CONTENT;
   }
 
@@ -390,6 +391,10 @@ AudioChannelService::Observe(nsISupports* aSubject, const char* aTopic, const PR
       int32_t index;
       while ((index = mChannelCounters[type].IndexOf(childID)) != -1) {
         mChannelCounters[type].RemoveElementAt(index);
+      }
+
+      if ((index = mActiveContentChildIDs.IndexOf(childID)) != -1) {
+        mActiveContentChildIDs.RemoveElementAt(index);
       }
     }
 
