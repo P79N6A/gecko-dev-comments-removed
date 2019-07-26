fab26686a2912df5ddf303add2e34a24a28ff28a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:00:07 +0000

    Bug 717872 - Move frame ownership to a separate container object, FrameSequence, which only exposes const access to its frames. r=seth
    The eventual goal here is to have FrameBlenders be able to share
    FrameSequences.
    
    UltraBlame original commit: a96a7eed6741a6133c11acea835b23ce3b076bbc

---
+++ image/src/FrameSequence.cpp
@@ -0,0 +1,106 @@
+
+
+
+
+
+#include "FrameSequence.h"
+
+using namespace mozilla;
+using namespace mozilla::image;
+
+namespace mozilla {
+namespace image {
+
+FrameSequence::~FrameSequence()
+{
+  ClearFrames();
+}
+
+const FrameDataPair&
+FrameSequence::GetFrame(uint32_t framenum) const
+{
+  if (framenum >= mFrames.Length()) {
+    static FrameDataPair empty;
+    return empty;
+  }
+
+  return mFrames[framenum];
+}
+
+uint32_t
+FrameSequence::GetNumFrames() const
+{
+  return mFrames.Length();
+}
+
+void
+FrameSequence::RemoveFrame(uint32_t framenum)
+{
+  NS_ABORT_IF_FALSE(framenum < mFrames.Length(), "Deleting invalid frame!");
+
+  mFrames.RemoveElementAt(framenum);
+}
+
+void
+FrameSequence::ClearFrames()
+{
+  
+  
+  mFrames.Clear();
+}
+
+void
+FrameSequence::InsertFrame(uint32_t framenum, imgFrame* aFrame)
+{
+  NS_ABORT_IF_FALSE(framenum <= mFrames.Length(), "Inserting invalid frame!");
+  mFrames.InsertElementAt(framenum, aFrame);
+  if (GetNumFrames() > 1) {
+    
+    
+    if (GetNumFrames() == 2) {
+      mFrames[0].LockAndGetData();
+    }
+
+    
+    
+    mFrames[framenum].LockAndGetData();
+  }
+}
+
+imgFrame*
+FrameSequence::SwapFrame(uint32_t framenum, imgFrame* aFrame)
+{
+  NS_ABORT_IF_FALSE(framenum < mFrames.Length(), "Swapping invalid frame!");
+
+  FrameDataPair ret;
+
+  
+  if (framenum < mFrames.Length()) {
+    ret = mFrames[framenum];
+  }
+
+  if (aFrame) {
+    mFrames.ReplaceElementAt(framenum, aFrame);
+  } else {
+    mFrames.RemoveElementAt(framenum);
+  }
+
+  return ret.Forget();
+}
+
+size_t
+FrameSequence::SizeOfDecodedWithComputedFallbackIfHeap(gfxASurface::MemoryLocation aLocation,
+                                                       MallocSizeOf aMallocSizeOf) const
+{
+  size_t n = 0;
+  for (uint32_t i = 0; i < mFrames.Length(); ++i) {
+    imgFrame* frame = mFrames.SafeElementAt(i, FrameDataPair());
+    NS_ABORT_IF_FALSE(frame, "Null frame in frame array!");
+    n += frame->SizeOfExcludingThisWithComputedFallbackIfHeap(aLocation, aMallocSizeOf);
+  }
+
+  return n;
+}
+
+} 
+} 
---
+++ image/src/FrameSequence.h
@@ -0,0 +1,185 @@
+
+
+
+
+
+
+#ifndef mozilla_imagelib_FrameSequence_h_
+#define mozilla_imagelib_FrameSequence_h_
+
+#include "nsTArray.h"
+#include "mozilla/MemoryReporting.h"
+#include "gfxASurface.h"
+#include "imgFrame.h"
+
+namespace mozilla {
+namespace image {
+
+
+
+
+
+
+
+
+
+class FrameDataPair
+{
+public:
+  explicit FrameDataPair(imgFrame* frame)
+    : mFrame(frame)
+    , mFrameData(nullptr)
+  {}
+
+  FrameDataPair()
+    : mFrame(nullptr)
+    , mFrameData(nullptr)
+  {}
+
+  FrameDataPair(FrameDataPair& other)
+  {
+    mFrame = other.mFrame;
+    mFrameData = other.mFrameData;
+
+    
+    
+    
+    other.mFrameData = nullptr;
+  }
+
+  ~FrameDataPair()
+  {
+    if (mFrameData) {
+      mFrame->UnlockImageData();
+    }
+  }
+
+  
+  
+  void LockAndGetData()
+  {
+    if (mFrame) {
+      if (NS_SUCCEEDED(mFrame->LockImageData())) {
+        if (mFrame->GetIsPaletted()) {
+          mFrameData = reinterpret_cast<uint8_t*>(mFrame->GetPaletteData());
+        } else {
+          mFrameData = mFrame->GetImageData();
+        }
+      }
+    }
+  }
+
+  
+  
+  imgFrame* Forget()
+  {
+    if (mFrameData) {
+      mFrame->UnlockImageData();
+    }
+
+    imgFrame* frame = mFrame.forget();
+    mFrameData = nullptr;
+    return frame;
+  }
+
+  bool HasFrameData() const
+  {
+    if (mFrameData) {
+      MOZ_ASSERT(!!mFrame);
+    }
+    return !!mFrameData;
+  }
+
+  uint8_t* GetFrameData() const
+  {
+    return mFrameData;
+  }
+
+  imgFrame* GetFrame() const
+  {
+    return mFrame;
+  }
+
+  
+  
+  void SetFrame(imgFrame* frame)
+  {
+    if (mFrameData) {
+      mFrame->UnlockImageData();
+    }
+
+    mFrame = frame;
+    mFrameData = nullptr;
+  }
+
+  operator imgFrame*() const
+  {
+    return GetFrame();
+  }
+
+  imgFrame* operator->() const
+  {
+    return GetFrame();
+  }
+
+  bool operator==(imgFrame* other) const
+  {
+    return mFrame == other;
+  }
+
+private:
+  nsAutoPtr<imgFrame> mFrame;
+  uint8_t* mFrameData;
+};
+
+
+
+
+
+class FrameSequence
+{
+public:
+
+  ~FrameSequence();
+
+  
+
+
+  const FrameDataPair& GetFrame(uint32_t aIndex) const;
+
+  
+
+
+  void InsertFrame(uint32_t framenum, imgFrame* aFrame);
+
+  
+
+
+  void RemoveFrame(uint32_t framenum);
+
+  
+
+
+
+  imgFrame* SwapFrame(uint32_t framenum, imgFrame* aFrame);
+
+  
+
+
+  void ClearFrames();
+
+  
+  uint32_t GetNumFrames() const;
+
+  size_t SizeOfDecodedWithComputedFallbackIfHeap(gfxASurface::MemoryLocation aLocation,
+                                                 mozilla::MallocSizeOf aMallocSizeOf) const;
+
+private: 
+  
+  nsTArray<FrameDataPair> mFrames;
+};
+
+} 
+} 
+
+#endif 
