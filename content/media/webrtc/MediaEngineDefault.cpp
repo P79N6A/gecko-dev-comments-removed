233bc0e28f6cb4a819f3978d7cc004dc8f67e44b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:29:15 +0000

    Backed out changeset 974c4db3003e (bug 818822)
    UltraBlame original commit: 423df46d8d570b55348c735fc2d0fc446c44807c

--- content/media/webrtc/MediaEngineDefault.cpp
+++ content/media/webrtc/MediaEngineDefault.cpp
@@ -132,12 +132,7 @@ MediaEngineDefaultVideoSource::Start(SourceMediaStream* aStream, TrackID aID)
   mTrackID = aID;
 
   
-#if defined(MOZ_WIDGET_GONK) && defined(DEBUG)
-
-  mTimer->InitWithCallback(this, (1000 / mOpts.mFPS)*10, nsITimer::TYPE_REPEATING_SLACK);
-#else
   mTimer->InitWithCallback(this, 1000 / mOpts.mFPS, nsITimer::TYPE_REPEATING_SLACK);
-#endif
   mState = kStarted;
 
   return NS_OK;
@@ -405,14 +400,8 @@ MediaEngineDefaultAudioSource::Start(SourceMediaStream* aStream, TrackID aID)
   mTrackID = aID;
 
   
-#if defined(MOZ_WIDGET_GONK) && defined(DEBUG)
-
-  mTimer->InitWithCallback(this, MediaEngine::DEFAULT_AUDIO_TIMER_MS*10,
-                           nsITimer::TYPE_REPEATING_PRECISE);
-#else
   mTimer->InitWithCallback(this, MediaEngine::DEFAULT_AUDIO_TIMER_MS,
                            nsITimer::TYPE_REPEATING_PRECISE);
-#endif
   mState = kStarted;
 
   return NS_OK;
