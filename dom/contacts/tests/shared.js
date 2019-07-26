d5a2c76fcf619c90faacc84ec439d64eebb1a882
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:16:22 +0000

    Backed out changeset f835989efa64 (bug 949537) for mochitest failures. CLOSED TREE
    UltraBlame original commit: c49653286c9606c40a2c00295abcb13b21fd0f9d

--- dom/contacts/tests/shared.js
+++ dom/contacts/tests/shared.js
@@ -402,17 +402,15 @@ function next() {
   }
 }
 
-SimpleTest.waitForExplicitFinish();
-
 function start_tests() {
   
   let androidVersion = SpecialPowers.Cc['@mozilla.org/system-info;1']
                                     .getService(SpecialPowers.Ci.nsIPropertyBag2)
                                     .getProperty('version');
   if (!isAndroid || androidVersion >= 14) {
-    next();
+    SimpleTest.waitForExplicitFinish();
+    addLoadEvent(next);
   } else {
     ok(true, "Skip tests on Android < 4.0 (bugs 897924 & 888891");
-    SimpleTest.finish();
   }
 }
