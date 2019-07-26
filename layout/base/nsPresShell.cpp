d4e5526cf125b03d9d75d0d5344ec1f998ba92cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:30:55 +0000

    Bug 930381 - Follow the COM rules when calling ProcessSynthMouseMoveEvent. r=mats, smaug, tnikkel
    UltraBlame original commit: d59e42825c4409f9b0d2f27ec434b74e7ec30d12

--- layout/base/nsPresShell.cpp
+++ layout/base/nsPresShell.cpp
@@ -3455,6 +3455,9 @@ PresShell::DispatchSynthMouseMove(WidgetGUIEvent* aEvent,
   if (!targetView)
     return;
   targetView->GetViewManager()->DispatchEvent(aEvent, targetView, &status);
+  if (MOZ_UNLIKELY(mIsDestroying)) {
+    return;
+  }
   if (aFlushOnHoverChange &&
       hoverGenerationBefore != restyleManager->GetHoverGeneration()) {
     
