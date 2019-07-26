7d010171c7642eb43c0adb736bededcf157c5c87
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:01:27 +0000

    Bug 282097 - Part 3: Add key bindings recorder to nsCocoaUtils. r=masayuki, r=smichaud
    UltraBlame original commit: d4404aa6430031b5684ab0304c76e48eafcb410b

--- widget/cocoa/nsCocoaUtils.h
+++ widget/cocoa/nsCocoaUtils.h
@@ -12,6 +12,7 @@
 #include "imgIContainer.h"
 #include "nsEvent.h"
 #include "npapi.h"
+#include "nsTArray.h"
 
 
 #include "nsObjCExceptions.h"
@@ -77,6 +78,26 @@ private:
 - (void)setHelpMenu:(NSMenu *)helpMenu;
 
 @end
+
+struct KeyBindingsCommand
+{
+  SEL selector;
+  id data;
+};
+
+@interface NativeKeyBindingsRecorder : NSResponder
+{
+@private
+  nsTArray<KeyBindingsCommand>* mCommands;
+}
+
+- (void)startRecording:(nsTArray<KeyBindingsCommand>&)aCommands;
+
+- (void)doCommandBySelector:(SEL)aSelector;
+
+- (void)insertText:(id)aString;
+
+@end 
 
 class nsCocoaUtils
 {
@@ -286,6 +307,14 @@ class nsCocoaUtils
 
 
   static bool HiDPIEnabled();
+
+  
+
+
+
+
+  static void GetCommandsFromKeyEvent(NSEvent* aEvent,
+                                      nsTArray<KeyBindingsCommand>& aCommands);
 };
 
 #endif 
