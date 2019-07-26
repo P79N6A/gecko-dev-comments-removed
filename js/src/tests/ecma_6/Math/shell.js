b57e58c7df70069ba4352bdd368e260079b33463
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:42:09 +0000

    Bug 892671 - Specify the tolerance in each new ES6 Math test. r=jandem.
    UltraBlame original commit: a45ee4ead45f903b9e02844bdd29dcc9ded470bd

--- js/src/tests/ecma_6/Math/shell.js
+++ js/src/tests/ecma_6/Math/shell.js
@@ -2,10 +2,73 @@
 const ONE_PLUS_EPSILON = 1 + Math.pow(2, -52);  
 const ONE_MINUS_EPSILON = 1 - Math.pow(2, -53);  
 
-function assertNear(actual, expected) {
-    var error = Math.abs(actual - expected);
+{
+    var fail = function (msg) {
+        var exc = new Error(msg);
+        try {
+            
+            
+            
+            var frames = exc.stack.trim().split("\n");
+            if (frames.length > 2) {
+                var m = /@([^@:]*):([0-9]+)$/.exec(frames[2]);
+                if (m) {
+                    exc.fileName = m[1];
+                    exc.lineNumber = +m[2];
+                }
+            }
+        } catch (ignore) { throw ignore;}
+        throw exc;
+    };
 
-    if (error > 1e-300 && error > Math.abs(actual) * 1e-12)
-        throw 'Assertion failed: got "' + actual + '", expected "' + expected + '" (rel error = ' + (error / Math.abs(actual)) + ')';
+    var ENDIAN;  
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    var f = new Float64Array([0, 0]);
+    var u = new Uint32Array(f.buffer);
+    var diff = function (a, b) {
+        f[0] = a;
+        f[1] = b;
+        
+        return Math.abs((u[3-ENDIAN] - u[1-ENDIAN]) * 0x100000000 + u[2+ENDIAN] - u[0+ENDIAN]);
+    };
+
+    
+    ENDIAN = 0;  
+    if (diff(2, 4) === 0x100000)  
+        ENDIAN = 1;
+    assertEq(diff(2,4), 0x10000000000000);
+    assertEq(diff(0, Number.MIN_VALUE), 1);
+    assertEq(diff(1, ONE_PLUS_EPSILON), 1);
+    assertEq(diff(1, ONE_MINUS_EPSILON), 1);
+
+    var assertNear = function assertNear(a, b, tolerance=1) {
+        if (!Number.isFinite(b)) {
+            fail("second argument to assertNear (expected value) must be a finite number");
+        } else if (Number.isNaN(a)) {
+            fail("got NaN, expected a number near " + b);
+        } else if (!Number.isFinite(a)) {
+            if (b * Math.sign(a) < Number.MAX_VALUE)
+                fail("got " + a + ", expected a number near " + b);
+        } else {
+            
+            
+            
+            var target = b === 0 ? a * 0 : b;
+            var err = diff(a, target);
+            if (err > tolerance) {
+                fail("got " + a + ", expected a number near " + b +
+                     " (relative error: " + err + ")");
+            }
+        }
+    };
 }
-
