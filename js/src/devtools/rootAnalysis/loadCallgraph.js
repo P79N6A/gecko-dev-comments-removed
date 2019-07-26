3daa8c2398fef178355bbe6ed4432dc1748f12da
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:45:12 +0000

    Bug 963735 - Switch from DFS -> BFS for finding GC paths, r=terrence
    UltraBlame original commit: 8d079291ebd1c39fdf68e2a9c69a930afd9eee0f

--- js/src/devtools/rootAnalysis/loadCallgraph.js
+++ js/src/devtools/rootAnalysis/loadCallgraph.js
@@ -174,7 +174,7 @@ function loadCallgraph(file)
 
     
     while (worklist.length) {
-        name = worklist.pop();
+        name = worklist.shift();
         assert(name in gcFunctions);
         if (!(name in callerGraph))
             continue;
