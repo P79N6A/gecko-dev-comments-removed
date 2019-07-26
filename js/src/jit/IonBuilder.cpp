fbae9fdf377c1124f744818f6436cdff2a877213
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:46:23 +0000

    Bug 971319 - Stop passing redundant barriering information through to GetPropertyIC. (r=jandem)
    UltraBlame original commit: 7e1478b9d44dfdf55a42253260366292b56833b2

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -8723,7 +8723,6 @@ IonBuilder::getPropTryCache(bool *emitted, PropertyName *name,
                             bool barrier, types::TemporaryTypeSet *types)
 {
     JS_ASSERT(*emitted == false);
-    bool accessGetter = inspector->hasSeenAccessedGetter(pc);
 
     MDefinition *obj = current->peek(-1);
 
@@ -8735,7 +8734,9 @@ IonBuilder::getPropTryCache(bool *emitted, PropertyName *name,
             return true;
     }
 
-    if (accessGetter)
+    
+    
+    if (inspector->hasSeenAccessedGetter(pc))
         barrier = true;
 
     if (needsToMonitorMissingProperties(types))
@@ -8766,10 +8767,6 @@ IonBuilder::getPropTryCache(bool *emitted, PropertyName *name,
         if (!annotateGetPropertyCache(obj, load, obj->resultTypeSet(), types))
             return false;
     }
-
-    
-    if (accessGetter)
-        load->setAllowGetters();
 
     current->add(load);
     current->push(load);
