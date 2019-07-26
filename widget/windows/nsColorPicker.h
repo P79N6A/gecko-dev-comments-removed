5b2352ec18c40114c9e034246b32513fffc402c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:05:02 +0000

    Bug 875754 - Implement <input type="color">: Windows widget/color picker. r=jimm
    UltraBlame original commit: de1f0de356a131ddc7b08f0f72caa4a516780af0

---
+++ widget/windows/nsColorPicker.h
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+#ifndef nsColorPicker_h__
+#define nsColorPicker_h__
+
+#include <windows.h>
+#include <commdlg.h>
+
+#include "nsCOMPtr.h"
+#include "nsIColorPicker.h"
+#include "nsString.h"
+#include "nsThreadUtils.h"
+
+class nsIWidget;
+
+class AsyncColorChooser :
+  public nsRunnable
+{
+public:
+  AsyncColorChooser(DWORD aInitialColor, nsIWidget* aParentWidget, nsIColorPickerShownCallback* aCallback);
+  NS_IMETHOD Run() MOZ_OVERRIDE;
+
+private:
+  DWORD mInitialColor;
+  nsCOMPtr<nsIWidget> mParentWidget;
+  nsCOMPtr<nsIColorPickerShownCallback> mCallback;
+  nsString mColor;
+};
+
+class nsColorPicker :
+  public nsIColorPicker
+{
+public:
+  nsColorPicker();
+  virtual ~nsColorPicker();
+
+  NS_DECL_ISUPPORTS
+
+  NS_IMETHOD Init(nsIDOMWindow* parent, const nsAString& title, const nsAString& aInitialColor);
+  NS_IMETHOD Open(nsIColorPickerShownCallback* aCallback);
+
+protected:
+  DWORD mInitialColor;
+  nsCOMPtr<nsIWidget> mParentWidget;
+};
+
+#endif 
