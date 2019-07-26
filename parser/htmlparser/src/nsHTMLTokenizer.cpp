74c373a06dfb44edc7ed0dc85535b4e4577b61bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:28:16 +0000

    bug 908347 - gut nsHTMLTokenizer a bit more r=mrbkap
    UltraBlame original commit: b1d28d79788585732efb9a4edb7d63087c78561b

--- parser/htmlparser/src/nsHTMLTokenizer.cpp
+++ parser/htmlparser/src/nsHTMLTokenizer.cpp
@@ -14,10 +14,9 @@
 
 
 
-#include "nsIAtom.h"
 #include "nsHTMLTokenizer.h"
+#include "nsIParser.h"
 #include "nsParserConstants.h"
-#include "nsIHTMLContentSink.h"
 
 
 
@@ -31,44 +30,9 @@ NS_IMPL_ISUPPORTS1(nsHTMLTokenizer, nsITokenizer)
 
 
 
-
-
-
-
-nsHTMLTokenizer::nsHTMLTokenizer(nsDTDMode aParseMode,
-                                 eParserDocType aDocType,
-                                 eParserCommands aCommand,
-                                 uint32_t aFlags)
+nsHTMLTokenizer::nsHTMLTokenizer()
 {
   
-  MOZ_ASSERT(!(aFlags & NS_IPARSER_FLAG_XML));
-}
-
-
-
-
-nsHTMLTokenizer::~nsHTMLTokenizer()
-{
-}
-
- uint32_t
-nsHTMLTokenizer::GetFlags(const nsIContentSink* aSink)
-{
-  uint32_t flags = 0;
-  nsCOMPtr<nsIHTMLContentSink> sink =
-    do_QueryInterface(const_cast<nsIContentSink*>(aSink));
-  if (sink) {
-    bool enabled = true;
-    sink->IsEnabled(eHTMLTag_frameset, &enabled);
-    if (enabled) {
-      flags |= NS_IPARSER_FLAG_FRAMES_ENABLED;
-    }
-    sink->IsEnabled(eHTMLTag_script, &enabled);
-    if (enabled) {
-      flags |= NS_IPARSER_FLAG_SCRIPT_ENABLED;
-    }
-  }
-  return flags;
 }
 
 nsresult
