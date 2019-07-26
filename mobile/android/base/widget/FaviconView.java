659ac602dda1a33a1e7b029399e122091d007931
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:40:38 +0000

    Bug 888326 - Part 5: Make the FaviconView able to resize strangely sized inputs to more appropriate sizes, if enabled. r=mleibovic
    UltraBlame original commit: 1fe12c9597f45c54ae2d1bb0e7faa84e0e374a3c

--- mobile/android/base/widget/FaviconView.java
+++ mobile/android/base/widget/FaviconView.java
@@ -13,7 +13,6 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -22,34 +21,144 @@ import android.widget.ImageView;
 
 
 public class FaviconView extends ImageView {
+    private Bitmap mIconBitmap;
+
+    
+    
+    private Bitmap mUnscaledBitmap;
+
+    
+    
+    
+    private String mIconKey;
+
+    private int mActualWidth;
+    private int mActualHeight;
+
+    
+    private boolean mScalingExpected;
 
     public FaviconView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setScaleType(ImageView.ScaleType.CENTER);
     }
 
+    @Override
+    protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld){
+        super.onSizeChanged(xNew, yNew, xOld, yOld);
+
+        
+        if (xNew == mActualHeight && yNew == mActualWidth) {
+            return;
+        }
+        mActualWidth = xNew;
+        mActualHeight = yNew;
+        formatImage();
+    }
+
     
 
 
 
-    public void updateImage(Bitmap bitmap, String key) {
-        if (bitmap == null) {
-            
-            setImageResource(R.drawable.favicon);
-            
-            setBackgroundResource(0);
-        } else if (Favicons.isLargeFavicon(bitmap)) {
-            setImageBitmap(bitmap);
-            
-            setBackgroundResource(0);
-        } else {
-            setImageBitmap(bitmap);
-            
-            int color = Favicons.getFaviconColor(bitmap, key);
-            color = Color.argb(70, Color.red(color), Color.green(color), Color.blue(color));
-            Drawable drawable = getResources().getDrawable(R.drawable.favicon_bg);
-            drawable.setColorFilter(color, Mode.SRC_ATOP);
-            setBackgroundDrawable(drawable);
+
+    private void formatImage() {
+        
+        if (mIconBitmap == null) {
+            setImageResource(0);
+            hideBackground();
+            return;
         }
+
+        
+        if (mActualWidth == 0 || mActualHeight == 0) {
+            hideBackground();
+            return;
+        }
+
+        if (mScalingExpected && mActualWidth != mIconBitmap.getWidth()) {
+            scaleBitmap();
+            
+            mScalingExpected = false;
+        }
+
+        setImageBitmap(mIconBitmap);
+
+        
+        
+        
+        
+        if (Math.abs(mIconBitmap.getWidth() - mActualWidth) > 3) {
+            showBackground();
+        } else {
+            hideBackground();
+        }
+    }
+
+    private void scaleBitmap() {
+        
+        
+        int doubledSize = mIconBitmap.getWidth()*2;
+        if (mActualWidth > doubledSize) {
+            
+            
+            mIconBitmap = Bitmap.createScaledBitmap(mIconBitmap, doubledSize, doubledSize, true);
+        } else {
+            
+            mIconBitmap = Bitmap.createScaledBitmap(mIconBitmap, mActualWidth, mActualWidth, true);
+        }
+    }
+
+    
+
+
+
+    private void showBackground() {
+        int color = Favicons.getFaviconColor(mIconBitmap, mIconKey);
+        color = Color.argb(70, Color.red(color), Color.green(color), Color.blue(color));
+        final Drawable drawable = getResources().getDrawable(R.drawable.favicon_bg);
+        drawable.setColorFilter(color, Mode.SRC_ATOP);
+        setBackgroundDrawable(drawable);
+    }
+
+    
+
+
+    private void hideBackground() {
+        setBackgroundResource(0);
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    private void updateImageInternal(Bitmap bitmap, String key, boolean allowScaling) {
+        
+        if (mUnscaledBitmap == bitmap) {
+            return;
+        }
+        mUnscaledBitmap = bitmap;
+        mIconBitmap = bitmap;
+        mIconKey = key;
+        mScalingExpected = allowScaling;
+
+        
+        formatImage();
+    }
+
+    public void updateAndScaleImage(Bitmap bitmap, String key) {
+        updateImageInternal(bitmap, key, true);
+    }
+
+    public void updateImage(Bitmap bitmap, String key) {
+        updateImageInternal(bitmap, key, false);
     }
 }
