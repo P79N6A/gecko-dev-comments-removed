3bf37dd8a8b547faacb343f0fa19a19501e1dcb7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:57:08 +0000

    Bug 983502 - navigator.getFeature implementation (support hardware.memory). r=bz
    UltraBlame original commit: 2f7a4bba1dac203f1a95056e1037fedf64c8daf7

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -1471,6 +1471,52 @@ Navigator::GetDataStores(const nsAString& aName, ErrorResult& aRv)
   return GetDataStores(mWindow, aName, aRv);
 }
 
+already_AddRefed<Promise>
+Navigator::GetFeature(const nsAString& aName)
+{
+  nsCOMPtr<nsIGlobalObject> go = do_QueryInterface(mWindow);
+  nsRefPtr<Promise> p = new Promise(go);
+
+#if defined(XP_LINUX)
+  if (aName.EqualsLiteral("hardware.memory")) {
+    static int memLevel = 1;
+    if (memLevel == 1) {
+      FILE* f = fopen("/proc/meminfo", "r");
+      if (!f) {
+        p->MaybeReject(NS_LITERAL_STRING("CannotOpenMeminfo"));
+        return p.forget();
+      }
+
+      int memTotal;
+      int n = fscanf(f, "MemTotal: %d kB\n", &memTotal);
+      fclose(f);
+
+      if (memTotal == 0 || n != 1) {
+        p->MaybeReject(NS_LITERAL_STRING("Abnormal"));
+        return p.forget();
+      }
+      
+      memTotal /= 1024;
+
+      
+      while (memLevel <= memTotal) {
+        memLevel *= 2;
+      }
+    }
+    p->MaybeResolve(memLevel);
+  } 
+  else
+#endif
+  {
+    
+    p->MaybeResolve(JS::UndefinedHandleValue);
+  }
+
+  return p.forget();
+
+}
+
+
 PowerManager*
 Navigator::GetMozPower(ErrorResult& aRv)
 {
@@ -2443,6 +2489,15 @@ Navigator::HasNetworkStatsSupport(JSContext* , JSObject* aGlobal)
   nsCOMPtr<nsPIDOMWindow> win = GetWindowFromGlobal(aGlobal);
   return CheckPermission(win, "networkstats-manage");
 }
+
+
+bool
+Navigator::HasFeatureDetectionSupport(JSContext* , JSObject* aGlobal)
+{
+  nsCOMPtr<nsPIDOMWindow> win = GetWindowFromGlobal(aGlobal);
+  return CheckPermission(win, "feature-detection");
+}
+
 
 
 already_AddRefed<nsPIDOMWindow>
