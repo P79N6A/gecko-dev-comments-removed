5247bc399c1aa0656218af35c9d8e231aa17607f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:49:13 +0000

    Bug 973189 - Get rid of the browser.ctrlTab.recentlyUsedLimit pref. r=ttaubert
    UltraBlame original commit: 2d8dd2ea8a242449817513506a451e5d6c5b9001

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -468,7 +468,6 @@ pref("browser.tabs.closeButtons", 1);
 pref("browser.tabs.selectOwnerOnClose", true);
 
 pref("browser.ctrlTab.previews", false);
-pref("browser.ctrlTab.recentlyUsedLimit", 7);
 
 
 
