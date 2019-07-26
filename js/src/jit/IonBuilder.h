fd58af460096bc33d7de86c62911b79a6ffedde5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:02:18 +0000

    Backed out changeset 11751c0efe27 (bug 951439) for mass bustage.
    UltraBlame original commit: a6f4adade9df75711e2aefeed860255ffd16e1a4

--- js/src/jit/IonBuilder.h
+++ js/src/jit/IonBuilder.h
@@ -630,7 +630,6 @@ class IonBuilder : public MIRGenerator
     InliningStatus inlineStrCharAt(CallInfo &callInfo);
 
     
-    InliningStatus inlineRegExpExec(CallInfo &callInfo);
     InliningStatus inlineRegExpTest(CallInfo &callInfo);
 
     
