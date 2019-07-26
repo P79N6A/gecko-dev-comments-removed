a795939fce9471d74de5dc5d6e8ab5ba5f8abefc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:02:42 +0000

    Bug 951739: Remove mPluginThread from PluginModuleParent. r=bsmedberg
    UltraBlame original commit: 1f44c2efcd35ecb2342353881c66d46258065100

--- dom/plugins/ipc/PluginModuleParent.h
+++ dom/plugins/ipc/PluginModuleParent.h
@@ -296,8 +296,6 @@ private:
 #endif
 
     PluginProcessParent* mSubprocess;
-    
-    NativeThreadId mPluginThread;
     bool mShutdown;
     bool mClearSiteDataSupported;
     bool mGetSitesWithDataSupported;
