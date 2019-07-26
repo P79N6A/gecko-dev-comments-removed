3f7cf8a57a5dd8a12b1a99053db7f24fced65028
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:18:36 +0000

    Bug 807533 - Implement DelayNode; r=bzbarsky
    UltraBlame original commit: 54ef53ee4af9a9a56eee967c1943dd3f0ffed7f0

---
+++ content/media/webaudio/DelayNode.cpp
@@ -0,0 +1,42 @@
+
+
+
+
+
+
+#include "DelayNode.h"
+#include "mozilla/dom/DelayNodeBinding.h"
+
+namespace mozilla {
+namespace dom {
+
+NS_IMPL_CYCLE_COLLECTION_CLASS(DelayNode)
+NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN_INHERITED(DelayNode, AudioNode)
+  NS_IMPL_CYCLE_COLLECTION_UNLINK_NSCOMPTR(mDelay)
+NS_IMPL_CYCLE_COLLECTION_UNLINK_END
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN_INHERITED(DelayNode, AudioNode)
+  NS_IMPL_CYCLE_COLLECTION_TRAVERSE_NATIVE_PTR(tmp->mDelay, AudioParam, "delay value")
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION_INHERITED(DelayNode)
+NS_INTERFACE_MAP_END_INHERITING(AudioNode)
+
+NS_IMPL_ADDREF_INHERITED(DelayNode, AudioNode)
+NS_IMPL_RELEASE_INHERITED(DelayNode, AudioNode)
+
+DelayNode::DelayNode(AudioContext* aContext, float aMaxDelay)
+  : AudioNode(aContext)
+  , mDelay(new AudioParam(aContext, 0.0f, 0.0f, aMaxDelay))
+{
+}
+
+JSObject*
+DelayNode::WrapObject(JSContext* aCx, JSObject* aScope,
+                      bool* aTriedToWrap)
+{
+  return DelayNodeBinding::Wrap(aCx, aScope, this, aTriedToWrap);
+}
+
+}
+}
+
---
+++ content/media/webaudio/DelayNode.h
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+#ifndef DelayNode_h_
+#define DelayNode_h_
+
+#include "AudioNode.h"
+#include "AudioParam.h"
+
+namespace mozilla {
+namespace dom {
+
+class AudioContext;
+
+class DelayNode : public AudioNode
+{
+public:
+  DelayNode(AudioContext* aContext, float aMaxDelay);
+
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(DelayNode, AudioNode)
+
+  virtual JSObject* WrapObject(JSContext* aCx, JSObject* aScope,
+                               bool* aTriedToWrap);
+
+  virtual uint32_t MaxNumberOfInputs() const MOZ_FINAL MOZ_OVERRIDE
+  {
+    return 1;
+  }
+  virtual uint32_t MaxNumberOfOutputs() const MOZ_FINAL MOZ_OVERRIDE
+  {
+    return 1;
+  }
+
+  AudioParam* DelayTime() const
+  {
+    return mDelay;
+  }
+
+private:
+  nsRefPtr<AudioParam> mDelay;
+};
+
+}
+}
+
+#endif
+
