8918f4ce702e2d9f2a86ff179f3795646d198572
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:26:30 +0000

    Bug 904743 - Add helpers to create simple makefiles and iterate loaded python modules. r=gps
    UltraBlame original commit: 5791f8bba15076e26f6ff019056d7da3f04bfec6

---
+++ python/mozbuild/mozbuild/pythonutil.py
@@ -0,0 +1,23 @@
+
+
+
+
+import os
+import sys
+
+
+def iter_modules_in_path(*paths):
+    paths = [os.path.abspath(os.path.normcase(p)) + os.sep
+             for p in paths]
+    for name, module in sys.modules.items():
+        if not hasattr(module, '__file__'):
+            continue
+
+        path = module.__file__
+
+        if path.endswith('.pyc'):
+            path = path[:-1]
+        path = os.path.abspath(os.path.normcase(path))
+
+        if any(path.startswith(p) for p in paths):
+            yield path
