899fad8ddf20b063cc201a439df754264fe144fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:02:44 +0000

    Bug 950049 - Rename VTTCue::TextTrackCueAlign to AlignSetting. r=rillian
    UltraBlame original commit: 40bb8646e3f8860ae5a98702b93b4b67052e2ee2

--- content/media/TextTrackCue.h
+++ content/media/TextTrackCue.h
@@ -216,12 +216,12 @@ public:
     CueChanged();
   }
 
-  TextTrackCueAlign Align() const
+  AlignSetting Align() const
   {
     return mAlign;
   }
 
-  void SetAlign(TextTrackCueAlign& aAlign)
+  void SetAlign(AlignSetting& aAlign)
   {
     if (mAlign == aAlign)
       return;
@@ -323,7 +323,7 @@ private:
   nsString mRegionId;
   DirectionSetting mVertical;
   int mLine;
-  TextTrackCueAlign mAlign;
+  AlignSetting mAlign;
 
   
   
