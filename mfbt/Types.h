15d5b8d31372981178aa84adbb4baaabc7a4d591
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:45:55 +0000

    b=880492 use GCC's typeof when decltype is not available r=Waldo
    UltraBlame original commit: 03b0f599a0031258b5477c65b2cf75a9363dc476

--- mfbt/Types.h
+++ mfbt/Types.h
@@ -133,4 +133,12 @@
 #  define MOZ_END_EXTERN_C
 #endif
 
+
+
+
+#if defined(__GNUC__) && defined(__cplusplus) && \
+  !defined(__GXX_EXPERIMENTAL_CXX0X__) && __cplusplus < 201103L
+#  define decltype __typeof__
+#endif
+
 #endif  
