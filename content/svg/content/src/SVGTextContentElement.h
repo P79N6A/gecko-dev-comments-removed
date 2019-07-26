fb28671a9e1d58ea91a04d28589d1710e4c97b2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:41:24 +0000

    Bug 825732: Convert SVGTextContentElement to WebIDL r=bz
    UltraBlame original commit: 02f069bc1cc375a3d7f7d750d33db7aa25f82068

---
+++ content/svg/content/src/SVGTextContentElement.h
@@ -0,0 +1,56 @@
+
+
+
+
+
+#ifndef mozilla_dom_SVGTextContentElement_h
+#define mozilla_dom_SVGTextContentElement_h
+
+#include "nsIDOMSVGTextContentElement.h"
+#include "mozilla/dom/SVGGraphicsElement.h"
+
+class nsSVGTextContainerFrame;
+
+namespace mozilla {
+class nsISVGPoint;
+
+namespace dom {
+
+typedef SVGGraphicsElement SVGTextContentElementBase;
+
+class SVGTextContentElement : public SVGTextContentElementBase
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIDOMSVGTEXTCONTENTELEMENT
+
+  using FragmentOrElement::TextLength;
+
+  
+  nsCOMPtr<nsIDOMSVGAnimatedLength> GetTextLength(ErrorResult& rv);
+  nsCOMPtr<nsIDOMSVGAnimatedEnumeration> GetLengthAdjust(ErrorResult& rv);
+  int32_t GetNumberOfChars();
+  float GetComputedTextLength();
+  float GetSubStringLength(uint32_t charnum, uint32_t nchars, ErrorResult& rv);
+  already_AddRefed<nsISVGPoint> GetStartPositionOfChar(uint32_t charnum, ErrorResult& rv);
+  already_AddRefed<nsISVGPoint> GetEndPositionOfChar(uint32_t charnum, ErrorResult& rv);
+  already_AddRefed<nsIDOMSVGRect> GetExtentOfChar(uint32_t charnum, ErrorResult& rv);
+  float GetRotationOfChar(uint32_t charnum, ErrorResult& rv);
+  int32_t GetCharNumAtPosition(nsISVGPoint& point);
+  void SelectSubString(uint32_t charnum, uint32_t nchars, ErrorResult& rv);
+
+protected:
+
+  SVGTextContentElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+    : SVGTextContentElementBase(aNodeInfo)
+  {}
+
+  nsSVGTextContainerFrame* GetTextContainerFrame();
+
+  virtual JSObject* WrapNode(JSContext *cx, JSObject *scope, bool *triedToWrap) MOZ_OVERRIDE;
+};
+
+} 
+} 
+
+#endif 
