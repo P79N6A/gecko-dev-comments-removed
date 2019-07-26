4ec0d0a5878dfa8319281cdca4414c25f69639a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:28:53 +0000

    Bug 806693 - Port browser_privatebrowsing_placestitle.js to the new per-window PB APIs; r=ehsan
    UltraBlame original commit: 58b09385cd7023c8f66b1dcc6f66c665f1a8eeaf

---
+++ browser/components/privatebrowsing/test/browser/perwindow/browser_privatebrowsing_placestitle.js
@@ -0,0 +1,109 @@
+
+
+
+
+
+
+
+function test() {
+  waitForExplicitFinish();
+
+  const TEST_URL = "http://mochi.test:8888/browser/browser/components/" +
+                   "privatebrowsing/test/browser/perwindow/title.sjs";
+  let cm = Cc["@mozilla.org/cookiemanager;1"].getService(Ci.nsICookieManager);
+
+  function waitForCleanup(aCallback) {
+    
+    cm.removeAll();
+    
+    Services.obs.addObserver(function observeCH(aSubject, aTopic, aData) {
+      Services.obs.removeObserver(observeCH, PlacesUtils.TOPIC_EXPIRATION_FINISHED);
+      aCallback();
+    }, PlacesUtils.TOPIC_EXPIRATION_FINISHED, false);
+    PlacesUtils.bhistory.removeAllPages();
+  }
+
+  let testNumber = 0;
+  let historyObserver = {
+    onTitleChanged: function(aURI, aPageTitle) {
+      if (aURI.spec != TEST_URL)
+        return;
+      switch (++testNumber) {
+        case 1:
+          
+          is(aPageTitle, "No Cookie",
+             "The page should be loaded without any cookie for the first time");
+          openTestPage(selectedWin);
+          break;
+        case 2:
+          
+          is(aPageTitle, "Cookie",
+             "The page should be loaded with a cookie for the second time");
+          waitForCleanup(function () {
+            openTestPage(selectedWin);
+          });
+          break;
+        case 3:
+          
+          is(aPageTitle, "No Cookie",
+             "The page should be loaded without any cookie again");
+          testOnWindow(true, function(win) {
+            whenPageLoad(win, function() {
+              waitForCleanup(finish);
+            });
+          });
+          break;
+        default:
+          
+          
+          ok(false, "Title changed. Unexpected pass: " + testNumber);
+      }
+    },
+
+    onBeginUpdateBatch: function () {},
+    onEndUpdateBatch: function () {},
+    onVisit: function () {},
+    onBeforeDeleteURI: function () {},
+    onDeleteURI: function () {},
+    onClearHistory: function () {},
+    onPageChanged: function () {},
+    onDeleteVisits: function() {},
+    QueryInterface: XPCOMUtils.generateQI([Ci.nsINavHistoryObserver])
+  };
+  PlacesUtils.history.addObserver(historyObserver, false);
+
+  let selectedWin = null;
+  let windowsToClose = [];
+  registerCleanupFunction(function() {
+    PlacesUtils.history.removeObserver(historyObserver);
+    windowsToClose.forEach(function(win) {
+      win.close();
+    });
+  });
+
+  function openTestPage(aWin) {
+    aWin.gBrowser.selectedTab = aWin.gBrowser.addTab(TEST_URL);
+  }
+
+  function whenPageLoad(aWin, aCallback) {
+    aWin.gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
+      aWin.gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
+      aCallback();
+    }, true);
+    aWin.gBrowser.selectedBrowser.loadURI(TEST_URL);
+  }
+
+  function testOnWindow(aPrivate, aCallback) {
+    whenNewWindowLoaded({ private: aPrivate }, function(win) {
+      selectedWin = win;
+      windowsToClose.push(win);
+      executeSoon(function() { aCallback(win) });
+    });
+  }
+
+  waitForCleanup(function() {
+    testOnWindow(false, function(win) {
+      openTestPage(win);
+    });
+  });
+}
