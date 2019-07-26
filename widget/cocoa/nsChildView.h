b69759d0286ba5c1347b68f2216accdc9f5dbcba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:38:57 +0000

    Merge mozilla-central and ux
    UltraBlame original commit: 485938514513680125dd2f655de120f4686bf059

--- widget/cocoa/nsChildView.h
+++ widget/cocoa/nsChildView.h
@@ -193,12 +193,6 @@ typedef NSInteger NSEventGestureAxis;
 #endif 
 #endif 
 
-#if !defined(MAC_OS_X_VERSION_10_8) || MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_8
-enum {
-  NSEventPhaseMayBegin    = 0x1 << 5
-};
-#endif 
-
 
 
 @interface NSEvent (ScrollPhase)
@@ -245,11 +239,6 @@ enum {
   NSMutableArray* mPendingDirtyRects;
   BOOL mPendingFullDisplay;
   BOOL mPendingDisplay;
-  
-  
-  
-  
-  BOOL mExpectingWheelStop;
 
   
   
@@ -352,8 +341,6 @@ enum {
 - (void)smartMagnifyWithEvent:(NSEvent *)anEvent;
 - (void)rotateWithEvent:(NSEvent *)anEvent;
 - (void)endGestureWithEvent:(NSEvent *)anEvent;
-
-- (void)scrollWheel:(NSEvent *)anEvent;
 
 
 + (BOOL)isLionSmartMagnifyEvent:(NSEvent*)anEvent;
