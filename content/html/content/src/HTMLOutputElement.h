eae686688229fc485529714beb71ad6f13066c67
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:54:47 +0000

    Bug 838160 - Part a: Rename nsHTMLOutputElement to mozilla::dom::HTMLOutputElement; r=bz
    UltraBlame original commit: 5251a87b7b7ccd7395bfc3704f647e5f0b5c3de5

---
+++ content/html/content/src/HTMLOutputElement.h
@@ -0,0 +1,91 @@
+
+
+
+
+
+#ifndef mozilla_dom_HTMLOutputElement_h
+#define mozilla_dom_HTMLOutputElement_h
+
+#include "nsGenericHTMLElement.h"
+#include "nsIDOMHTMLOutputElement.h"
+#include "nsStubMutationObserver.h"
+#include "nsIConstraintValidation.h"
+
+namespace mozilla {
+namespace dom {
+
+class HTMLOutputElement : public nsGenericHTMLFormElement,
+                          public nsIDOMHTMLOutputElement,
+                          public nsStubMutationObserver,
+                          public nsIConstraintValidation
+{
+public:
+  using nsIConstraintValidation::GetValidationMessage;
+
+  HTMLOutputElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual ~HTMLOutputElement();
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+
+  
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+
+  
+  NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
+
+  
+  NS_DECL_NSIDOMHTMLOUTPUTELEMENT
+
+  
+  NS_IMETHOD_(uint32_t) GetType() const { return NS_FORM_OUTPUT; }
+  NS_IMETHOD Reset();
+  NS_IMETHOD SubmitNamesValues(nsFormSubmission* aFormSubmission);
+
+  virtual bool IsDisabled() const { return false; }
+
+  nsresult Clone(nsINodeInfo* aNodeInfo, nsINode** aResult) const;
+
+  bool ParseAttribute(int32_t aNamespaceID, nsIAtom* aAttribute,
+                        const nsAString& aValue, nsAttrValue& aResult);
+
+  nsEventStates IntrinsicState() const;
+
+  virtual nsresult BindToTree(nsIDocument* aDocument, nsIContent* aParent,
+                               nsIContent* aBindingParent,
+                               bool aCompileEventHandlers);
+
+  
+  
+  void DescendantsChanged();
+
+  
+  NS_DECL_NSIMUTATIONOBSERVER_CHARACTERDATACHANGED
+  NS_DECL_NSIMUTATIONOBSERVER_CONTENTAPPENDED
+  NS_DECL_NSIMUTATIONOBSERVER_CONTENTINSERTED
+  NS_DECL_NSIMUTATIONOBSERVER_CONTENTREMOVED
+
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(HTMLOutputElement,
+                                           nsGenericHTMLFormElement)
+
+  virtual nsXPCClassInfo* GetClassInfo();
+
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+protected:
+  enum ValueModeFlag {
+    eModeDefault,
+    eModeValue
+  };
+
+  ValueModeFlag                     mValueModeFlag;
+  nsString                          mDefaultValue;
+  nsRefPtr<nsDOMSettableTokenList>  mTokenList;
+};
+
+} 
+} 
+
+#endif
