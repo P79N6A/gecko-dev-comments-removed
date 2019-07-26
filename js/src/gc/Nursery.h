55361a649409c9bd78ce61f350f13ae07f36671b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:38:36 +0000

    Bug 994054 - MacOS's madvise implementation is unworkably slow; r=jandem
    UltraBlame original commit: bff509087141eee9311861eb579deb88dbd2e32d

--- js/src/gc/Nursery.h
+++ js/src/gc/Nursery.h
@@ -212,9 +212,13 @@ class Nursery
     void updateDecommittedRegion() {
 #ifndef JS_GC_ZEAL
         if (numActiveChunks_ < NumNurseryChunks) {
+            
+            
+# ifndef XP_MACOSX
             uintptr_t decommitStart = chunk(numActiveChunks_).start();
             JS_ASSERT(decommitStart == AlignBytes(decommitStart, 1 << 20));
             gc::MarkPagesUnused(runtime(), (void *)decommitStart, heapEnd() - decommitStart);
+# endif
         }
 #endif
     }
