becb1780b45fbe3e7e7b5b1f7badeaefe1e4151b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:32:49 +0000

    Bug 817999 - remove DownloadMonitorPanel remnants. r=dao
    UltraBlame original commit: 33292b24d5d2d2397a724b49b81c7f1c73148923

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -16,7 +16,6 @@ var gPrevCharset = null;
 var gProxyFavIcon = null;
 var gLastValidURLStr = "";
 var gInPrintPreviewMode = false;
-var gDownloadMgr = null;
 var gContextMenu = null; 
 var gStartupRan = false;
 
@@ -1339,8 +1338,7 @@ var gBrowserInit = {
     
     
     setTimeout(function() {
-      gDownloadMgr = Cc["@mozilla.org/download-manager;1"].
-                     getService(Ci.nsIDownloadManager);
+      Cc["@mozilla.org/download-manager;1"].getService(Ci.nsIDownloadManager);
 
 #ifdef XP_WIN
       if (Win7Features) {
@@ -6869,140 +6867,6 @@ var gIdentityHandler = {
     dt.setData("text/html", htmlString);
     dt.setDragImage(gProxyFavIcon, 16, 16);
   }
-};
-
-let DownloadMonitorPanel = {
-  
-  
-
-  _panel: null,
-  _activeStr: null,
-  _pausedStr: null,
-  _lastTime: Infinity,
-  _listening: false,
-
-  get DownloadUtils() {
-    delete this.DownloadUtils;
-    Cu.import("resource://gre/modules/DownloadUtils.jsm", this);
-    return this.DownloadUtils;
-  },
-
-  
-  
-
-  
-
-
-  init: function DMP_init() {
-    
-    this._panel = document.getElementById("download-monitor");
-
-    
-    this._activeStr = gNavigatorBundle.getString("activeDownloads1");
-    this._pausedStr = gNavigatorBundle.getString("pausedDownloads1");
-
-    gDownloadMgr.addListener(this);
-    this._listening = true;
-
-    this.updateStatus();
-  },
-
-  uninit: function DMP_uninit() {
-    if (this._listening)
-      gDownloadMgr.removeListener(this);
-  },
-
-  inited: function DMP_inited() {
-    return this._panel != null;
-  },
-
-  
-
-
-  updateStatus: function DMP_updateStatus() {
-    if (!this.inited())
-      return;
-
-    let numActive = gDownloadMgr.activeDownloadCount;
-
-    
-    if (numActive == 0) {
-      this._panel.hidden = true;
-      this._lastTime = Infinity;
-
-      return;
-    }
-
-    
-    let numPaused = 0;
-    let maxTime = -Infinity;
-    let dls = gDownloadMgr.activeDownloads;
-    while (dls.hasMoreElements()) {
-      let dl = dls.getNext();
-      if (dl.state == gDownloadMgr.DOWNLOAD_DOWNLOADING) {
-        
-        if (dl.speed > 0 && dl.size > 0)
-          maxTime = Math.max(maxTime, (dl.size - dl.amountTransferred) / dl.speed);
-        else
-          maxTime = -1;
-      }
-      else if (dl.state == gDownloadMgr.DOWNLOAD_PAUSED)
-        numPaused++;
-    }
-
-    
-    let timeLeft;
-    [timeLeft, this._lastTime] =
-      this.DownloadUtils.getTimeLeft(maxTime, this._lastTime);
-
-    
-    let numDls = numActive - numPaused;
-    let status = this._activeStr;
-
-    
-    if (numDls == 0) {
-      numDls = numPaused;
-      status = this._pausedStr;
-    }
-
-    
-    
-    status = PluralForm.get(numDls, status);
-    status = status.replace("#1", numDls);
-    status = status.replace("#2", timeLeft);
-
-    
-    this._panel.label = status;
-    this._panel.hidden = false;
-  },
-
-  
-  
-
-  
-
-
-  onProgressChange: function() {
-    this.updateStatus();
-  },
-
-  
-
-
-  onDownloadStateChange: function() {
-    this.updateStatus();
-  },
-
-  onStateChange: function(aWebProgress, aRequest, aStateFlags, aStatus, aDownload) {
-  },
-
-  onSecurityChange: function(aWebProgress, aRequest, aState, aDownload) {
-  },
-
-  
-  
-
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIDownloadProgressListener]),
 };
 
 function getNotificationBox(aWindow) {
