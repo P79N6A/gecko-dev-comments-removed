c2ad0985d1c0995dc4ed409a7913f1f0a7a55224
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:27:46 +0000

    Bug 957425 - make landing pages for sync on about:accounts. r=markh
    UltraBlame original commit: d81113054e1338aa3c95c27e83ebda4cb2382e15

--- browser/base/content/aboutaccounts/aboutaccounts.js
+++ browser/base/content/aboutaccounts/aboutaccounts.js
@@ -129,5 +129,53 @@ let wrapper = {
   },
 };
 
-wrapper.init();
 
+
+function handleOldSync() {
+  
+  window.location = "https://services.mozilla.com/legacysync";
+}
+
+function getStarted() {
+  hide("intro");
+  hide("stage");
+  show("remote");
+}
+
+function openPrefs() {
+  window.openPreferences("paneSync");
+}
+
+function init() {
+  let signinQuery = window.location.href.match(/signin=true$/);
+
+  if (signinQuery) {
+    show("remote");
+    wrapper.init();
+  } else {
+    
+    fxAccounts.getSignedInUser().then(user => {
+      if (user) {
+        show("stage");
+        show("manage");
+      } else {
+        show("stage");
+        show("intro");
+        
+        wrapper.init();
+      }
+    });
+  }
+}
+
+function show(id) {
+  document.getElementById(id).style.display = 'block';
+}
+function hide(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+document.addEventListener("DOMContentLoaded", function onload() {
+  document.removeEventListener("DOMContentLoaded", onload, true);
+  init();
+}, true);
