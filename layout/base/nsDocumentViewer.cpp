4f8b16eb97276f68961b52207db18dc6068baaf7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:24:24 +0000

    Backed out changeset 6a613a7288e1 (bug 931399) for breaking the build on at least Windows on a CLOSED TREE
    UltraBlame original commit: 031133c03e865b0fe4535cd862d3f71017e43334

--- layout/base/nsDocumentViewer.cpp
+++ layout/base/nsDocumentViewer.cpp
@@ -33,7 +33,6 @@
 #include "nsIDOMHTMLElement.h"
 #include "nsContentUtils.h"
 #include "nsLayoutStylesheetCache.h"
-#include "mozilla/a11y/DocAccessible.h"
 #include "mozilla/BasicEvents.h"
 #include "mozilla/Preferences.h"
 #include "mozilla/dom/EncodingUtils.h"
@@ -1549,16 +1548,6 @@ nsDocumentViewer::Destroy()
     
     
     shEntry->SyncPresentationState();
-
-    
-#ifdef ACCESSIBILITY
-    if (mPresShell) {
-      a11y::DocAccessible* docAcc = mPresShell->GetDocAccessible();
-      if (docAcc) {
-        docAcc->Shutdown();
-      }
-    }
-#endif
 
     
     
