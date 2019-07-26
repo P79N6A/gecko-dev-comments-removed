b7948f224fe0c95c11a6b70fb4ad736e219e5ca0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:01:31 +0000

    Bug 784841 - Part 16: Use moz.build files to build the tree; r=ted, glandium
    UltraBlame original commit: 23673de2aa432551cb25c993238ccc1be2ae9b1f

--- media/webrtc/trunk/tools/gyp/pylib/gyp/generator/mozmake.py
+++ media/webrtc/trunk/tools/gyp/pylib/gyp/generator/mozmake.py
@@ -2,9 +2,6 @@
 
 
 
-
-from __future__ import with_statement
-
 import collections
 import gyp
 import gyp.common
@@ -35,6 +32,8 @@ topsrcdir	= %(topsrcdir)s
 srcdir          = %(srcdir)s
 VPATH           = %(srcdir)s
 
+EXTERNALLY_MANAGED_MAKE_FILE := 1
+
 """
 
 COMMON_FOOTER = """
@@ -55,13 +54,11 @@ CFLAGS += $(CPPFLAGS_Debug) $(CFLAGS_Debug)
 CXXFLAGS += $(CPPFLAGS_Debug) $(CXXFLAGS_Debug)
 DEFINES += $(DEFINES_Debug)
 LOCAL_INCLUDES += $(INCLUDES_Debug)
-ASFLAGS += $(ASFLAGS_Debug)
 else # non-MOZ_DEBUG
 CFLAGS += $(CPPFLAGS_Release) $(CFLAGS_Release)
 CXXFLAGS += $(CPPFLAGS_Release) $(CXXFLAGS_Release)
 DEFINES += $(DEFINES_Release)
 LOCAL_INCLUDES += $(INCLUDES_Release)
-ASFLAGS += $(ASFLAGS_Release)
 endif
 
 ifeq (WINNT,$(OS_TARGET))
@@ -168,10 +165,6 @@ def striplib(name):
     return name[3:]
   return name
 
-AS_EXTENSIONS = set([
-  '.s',
-  '.S'
-])
 CPLUSPLUS_EXTENSIONS = set([
   '.cc',
   '.cpp',
@@ -326,15 +319,12 @@ class MakefileGenerator(object):
       cflags_mozilla = config.get('cflags_mozilla')
       if cflags_mozilla:
         data['CPPFLAGS_%s' % configname] = cflags_mozilla
-      asflags_mozilla = config.get('asflags_mozilla')
-      if asflags_mozilla:
-        data['ASFLAGS_%s' % configname] = asflags_mozilla
     sources = {
       'CPPSRCS': {'exts': CPLUSPLUS_EXTENSIONS, 'files': []},
       'CSRCS': {'exts': ['.c'], 'files': []},
       'CMSRCS': {'exts': ['.m'], 'files': []},
       'CMMSRCS': {'exts': ['.mm'], 'files': []},
-      'SSRCS': {'exts': AS_EXTENSIONS, 'files': []},
+      'ASFILES': {'exts': ['.s'], 'files': []},
       }
     copy_srcs = []
     for s in spec.get('sources', []):
