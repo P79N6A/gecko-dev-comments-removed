960f184a5e2a660101c625a077a50f940f03bd0c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:54:16 +0000

    Bug 997660 - Use TopSites.TYPE* in top sites grid UI (r=wesj)
    UltraBlame original commit: 308935276434af38b377733734bc3133a9498765

--- mobile/android/base/home/TopSitesPanel.java
+++ mobile/android/base/home/TopSitesPanel.java
@@ -285,8 +285,8 @@ public class TopSitesPanel extends HomeFragment {
         TopSitesGridContextMenuInfo info = (TopSitesGridContextMenuInfo) menuInfo;
         menu.setHeaderTitle(info.getDisplayTitle());
 
-        if (!TextUtils.isEmpty(info.url)) {
-            if (info.isPinned) {
+        if (info.type != TopSites.TYPE_BLANK) {
+            if (info.type == TopSites.TYPE_PINNED) {
                 menu.findItem(R.id.top_sites_pin).setVisible(false);
             } else {
                 menu.findItem(R.id.top_sites_unpin).setVisible(false);
@@ -514,12 +514,11 @@ public class TopSitesPanel extends HomeFragment {
             final String url = cursor.getString(cursor.getColumnIndexOrThrow(TopSites.URL));
             final String title = cursor.getString(cursor.getColumnIndexOrThrow(TopSites.TITLE));
             final int type = cursor.getInt(cursor.getColumnIndexOrThrow(TopSites.TYPE));
-            final boolean pinned = (type == TopSites.TYPE_PINNED);
 
             final TopSitesGridItemView view = (TopSitesGridItemView) bindView;
 
             
-            if (TextUtils.isEmpty(url)) {
+            if (type == TopSites.TYPE_BLANK) {
                 
                 if (mThumbnails != null) {
                     view.blankOut();
@@ -533,7 +532,7 @@ public class TopSitesPanel extends HomeFragment {
 
             
             
-            final boolean updated = view.updateState(title, url, pinned, thumbnail);
+            final boolean updated = view.updateState(title, url, type, thumbnail);
 
             
             
