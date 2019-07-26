e114f2748526f24dc9cde86fc77b7686f7d59ccb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:10:50 +0000

    Bug 946065. Part 2: Move content/xslt to dom/xslt and flatten away 'public' and 'src'. r=ms2ger
    UltraBlame original commit: 67ff9392766b53c51bb876b5f74e5e6602347668

--- dom/xslt/base/txNamespaceMap.cpp
+++ dom/xslt/base/txNamespaceMap.cpp
@@ -3,39 +3,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #include "txNamespaceMap.h"
 #include "nsGkAtoms.h"
 #include "txXPathNode.h"
@@ -53,12 +20,12 @@ txNamespaceMap::txNamespaceMap(const txNamespaceMap& aOther)
 nsresult
 txNamespaceMap::mapNamespace(nsIAtom* aPrefix, const nsAString& aNamespaceURI)
 {
-    nsIAtom* prefix = aPrefix == nsGkAtoms::_empty ? nsnull : aPrefix;
+    nsIAtom* prefix = aPrefix == nsGkAtoms::_empty ? nullptr : aPrefix;
 
-    PRInt32 nsId;
+    int32_t nsId;
     if (prefix && aNamespaceURI.IsEmpty()) {
         
-        PRInt32 index = mPrefixes.IndexOf(prefix);
+        int32_t index = mPrefixes.IndexOf(prefix);
         if (index >= 0) {
             mPrefixes.RemoveObjectAt(index);
             mNamespaces.RemoveElementAt(index);
@@ -77,7 +44,7 @@ txNamespaceMap::mapNamespace(nsIAtom* aPrefix, const nsAString& aNamespaceURI)
     }
 
     
-    PRInt32 index = mPrefixes.IndexOf(prefix);
+    int32_t index = mPrefixes.IndexOf(prefix);
     if (index >= 0) {
         mNamespaces.ElementAt(index) = nsId;
 
@@ -89,7 +56,7 @@ txNamespaceMap::mapNamespace(nsIAtom* aPrefix, const nsAString& aNamespaceURI)
         return NS_ERROR_OUT_OF_MEMORY;
     }
     
-    if (mNamespaces.AppendElement(nsId) == nsnull) {
+    if (mNamespaces.AppendElement(nsId) == nullptr) {
         mPrefixes.RemoveObjectAt(mPrefixes.Count() - 1);
 
         return NS_ERROR_OUT_OF_MEMORY;
@@ -98,7 +65,7 @@ txNamespaceMap::mapNamespace(nsIAtom* aPrefix, const nsAString& aNamespaceURI)
     return NS_OK;
 }
 
-PRInt32
+int32_t
 txNamespaceMap::lookupNamespace(nsIAtom* aPrefix)
 {
     if (aPrefix == nsGkAtoms::xml) {
@@ -107,7 +74,7 @@ txNamespaceMap::lookupNamespace(nsIAtom* aPrefix)
 
     nsIAtom* prefix = aPrefix == nsGkAtoms::_empty ? 0 : aPrefix;
 
-    PRInt32 index = mPrefixes.IndexOf(prefix);
+    int32_t index = mPrefixes.IndexOf(prefix);
     if (index >= 0) {
         return mNamespaces.SafeElementAt(index, kNameSpaceID_Unknown);
     }
@@ -119,7 +86,7 @@ txNamespaceMap::lookupNamespace(nsIAtom* aPrefix)
     return kNameSpaceID_Unknown;
 }
 
-PRInt32
+int32_t
 txNamespaceMap::lookupNamespaceWithDefault(const nsAString& aPrefix)
 {
     nsCOMPtr<nsIAtom> prefix = do_GetAtom(aPrefix);
@@ -127,5 +94,5 @@ txNamespaceMap::lookupNamespaceWithDefault(const nsAString& aPrefix)
         return lookupNamespace(prefix);
     }
 
-    return lookupNamespace(nsnull);
+    return lookupNamespace(nullptr);
 }
