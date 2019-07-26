9369db1054dc64202fe1ad9c8744df1ea89fa7f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:05:35 +0000

    Bug 896224 - Remove support for the version 1 manifest from the updater. r=bbondy
    UltraBlame original commit: 6230354e8d0530b5a61cbd604d8b343ff261594f

--- toolkit/mozapps/update/updater/updater.cpp
+++ toolkit/mozapps/update/updater/updater.cpp
@@ -26,10 +26,6 @@
 
 
 
-
-
-
-
 #include "bspatch.h"
 #include "progressui.h"
 #include "archivereader.h"
@@ -3575,6 +3571,7 @@ int AddPreCompleteActions(ActionList *list)
     LOG(("AddPreCompleteActions: error getting contents of precomplete " \
          "manifest"));
     
+    
     return OK;
   }
 
@@ -3659,19 +3656,21 @@ int DoUpdate()
       return PARSE_ERROR;
     }
 
-    if (isFirstAction && NS_tstrcmp(token, NS_T("type")) == 0) {
-      const NS_tchar *type = mstrtok(kQuote, &line);
-      LOG(("UPDATE TYPE " LOG_S, type));
-      if (NS_tstrcmp(type, NS_T("complete")) == 0) {
-        rv = AddPreCompleteActions(&list);
-        if (rv)
-          return rv;
-      }
+    if (isFirstAction) {
       isFirstAction = false;
-      continue;
+      
+      
+      if (NS_tstrcmp(token, NS_T("type")) == 0) {
+        const NS_tchar *type = mstrtok(kQuote, &line);
+        LOG(("UPDATE TYPE " LOG_S, type));
+        if (NS_tstrcmp(type, NS_T("complete")) == 0) {
+          rv = AddPreCompleteActions(&list);
+          if (rv)
+            return rv;
+        }
+        continue;
+      }
     }
-
-    isFirstAction = false;
 
     Action *action = nullptr;
     if (NS_tstrcmp(token, NS_T("remove")) == 0) { 
@@ -3705,9 +3704,6 @@ int DoUpdate()
     }
     else if (NS_tstrcmp(token, NS_T("patch-if")) == 0) { 
       action = new PatchIfFile();
-    }
-    else if (NS_tstrcmp(token, NS_T("add-cc")) == 0) { 
-      continue;
     }
     else {
       LOG(("DoUpdate: unknown token: " LOG_S, token));
