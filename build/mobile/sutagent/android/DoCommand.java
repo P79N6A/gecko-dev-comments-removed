746b9c61fa946a91b55dd566586cb4cbeb7d32eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:09:51 +0000

    Bug 852246 - Allow installing over an app with >= version in Android 4.2 via the agent;r=jmaher
    UltraBlame original commit: 9e6e38d4ae0bb948df7c8d837e10bbaab15f6d2a

--- build/mobile/sutagent/android/DoCommand.java
+++ build/mobile/sutagent/android/DoCommand.java
@@ -3317,7 +3317,15 @@ private void CancelNotification()
 
         try
             {
-            pProc = Runtime.getRuntime().exec(this.getSuArgs("pm install -r " + sApp + " Cleanup;exit"));
+            
+            
+            String sPmCmd;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                sPmCmd = "pm install -r -d " + sApp + " Cleanup;exit";
+            } else {
+                sPmCmd = "pm install -r " + sApp + " Cleanup;exit";
+            }
+            pProc = Runtime.getRuntime().exec(this.getSuArgs(sPmCmd));
             RedirOutputThread outThrd3 = new RedirOutputThread(pProc, out);
             outThrd3.start();
             try {
