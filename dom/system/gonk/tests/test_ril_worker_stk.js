b0502cd31404481123ddc59edac87426389ff5dd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:43:38 +0000

    Bug 908075 - Part2: USER_ACTIVITY stk event xpcshell test. r=yoshi
    UltraBlame original commit: 9cf2be195a9a83a2179c4071c21f6443a7b602b7

--- dom/system/gonk/tests/test_ril_worker_stk.js
+++ dom/system/gonk/tests/test_ril_worker_stk.js
@@ -881,6 +881,54 @@ add_test(function test_stk_event_download_language_selection() {
 
 
 
+add_test(function test_stk_event_download_user_activity() {
+  let worker = newUint8SupportOutgoingIndexWorker();
+  let buf = worker.Buf;
+  let pduHelper = worker.GsmPDUHelper;
+
+  buf.sendParcel = function () {
+    
+    do_check_eq(this.readUint32(), REQUEST_STK_SEND_ENVELOPE_COMMAND);
+
+    
+    this.readUint32();
+
+    
+    do_check_eq(this.readUint32(), 18);
+
+    
+    do_check_eq(pduHelper.readHexOctet(), BER_EVENT_DOWNLOAD_TAG);
+
+    
+    do_check_eq(pduHelper.readHexOctet(), 7);
+
+    
+    do_check_eq(pduHelper.readHexOctet(), COMPREHENSIONTLV_TAG_DEVICE_ID |
+                                          COMPREHENSIONTLV_FLAG_CR);
+    do_check_eq(pduHelper.readHexOctet(), 2);
+    do_check_eq(pduHelper.readHexOctet(), STK_DEVICE_ID_ME);
+    do_check_eq(pduHelper.readHexOctet(), STK_DEVICE_ID_SIM);
+
+    
+    do_check_eq(pduHelper.readHexOctet(), COMPREHENSIONTLV_TAG_EVENT_LIST |
+                                          COMPREHENSIONTLV_FLAG_CR);
+    do_check_eq(pduHelper.readHexOctet(), 1);
+    do_check_eq(pduHelper.readHexOctet(), STK_EVENT_TYPE_USER_ACTIVITY);
+
+    run_next_test();
+  };
+
+  let event = {
+    eventType: STK_EVENT_TYPE_USER_ACTIVITY
+  };
+  worker.RIL.sendStkEventDownload({
+    event: event
+  });
+});
+
+
+
+
 add_test(function test_stk_event_download_idle_screen_available() {
   let worker = newUint8SupportOutgoingIndexWorker();
   let buf = worker.Buf;
