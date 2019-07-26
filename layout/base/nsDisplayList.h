e2a4fcffe1fc3d57f44e5f03d0e617128c5a709d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:02:39 +0000

    Bug 959847. Part 8. Don't let ProcessDisplayItems set the clip on subdocument items when they are being used to draw the display port content. r=roc
    UltraBlame original commit: 3bf7def1b7f4c9e6583437bd9156222484452a71

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -1233,6 +1233,11 @@ public:
     mClip = aBuilder->AllocateDisplayItemClip(aClip);
   }
 
+  
+  
+  
+  virtual bool SetVisibleRegionOnLayer() { return true; }
+
 protected:
   friend class nsDisplayList;
 
@@ -2715,6 +2720,9 @@ public:
   virtual already_AddRefed<Layer> BuildLayer(nsDisplayListBuilder* aBuilder,
                                              LayerManager* aManager,
                                              const ContainerLayerParameters& aContainerParameters) MOZ_OVERRIDE;
+
+  virtual bool SetVisibleRegionOnLayer() MOZ_OVERRIDE { return !(mFlags & GENERATE_SCROLLABLE_LAYER); }
+
   NS_DISPLAY_DECL_NAME("SubDocument", TYPE_SUBDOCUMENT)
 };
 
@@ -2843,6 +2851,8 @@ public:
 
   virtual nsIFrame* GetScrollFrame() { return mScrollFrame; }
   virtual nsIFrame* GetScrolledFrame() { return mScrolledFrame; }
+
+  virtual bool SetVisibleRegionOnLayer() MOZ_OVERRIDE { return false; }
 
 #ifdef MOZ_DUMP_PAINTING
   virtual void WriteDebugInfo(nsACString& aTo) MOZ_OVERRIDE;
@@ -3201,6 +3211,8 @@ public:
                                                 nsIFrame* aFrame,
                                                 bool aLogAnimations = false);
   bool CanUseAsyncAnimations(nsDisplayListBuilder* aBuilder) MOZ_OVERRIDE;
+
+  virtual bool SetVisibleRegionOnLayer() MOZ_OVERRIDE { return false; }
 
 private:
   static gfx3DMatrix GetResultingTransformMatrixInternal(const FrameTransformProperties& aProperties,
