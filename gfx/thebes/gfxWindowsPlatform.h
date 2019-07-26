403d8d8095d840b48e7f8982b4edfff23180d682
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:11:52 +0000

    Bug 844954 - Add support for DX9.3 feature level for Metro and cleanup CreateDevice code. r=bas
    UltraBlame original commit: 0ed97a9289b6f331be3658a30c2f315fe2d5f529

--- gfx/thebes/gfxWindowsPlatform.h
+++ gfx/thebes/gfxWindowsPlatform.h
@@ -28,6 +28,10 @@
 #include <windows.h>
 #include <objbase.h>
 
+#ifdef CAIRO_HAS_D2D_SURFACE
+#include <dxgi.h>
+#endif
+
 class nsIMemoryMultiReporter;
 
 
@@ -146,6 +150,10 @@ public:
 
 
     void VerifyD2DDevice(bool aAttemptForce);
+
+#ifdef CAIRO_HAS_D2D_SURFACE
+    HRESULT CreateDevice(nsRefPtr<IDXGIAdapter1> &adapter1, int featureLevelIndex);
+#endif
 
     HDC GetScreenDC() { return mScreenDC; }
 
