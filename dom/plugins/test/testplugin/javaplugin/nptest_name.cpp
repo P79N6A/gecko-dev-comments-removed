61b67c7b006eaa15148c3786a6794b66efbda116
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 21:59:49 +0000

    Bug 971279 - Add nptest java plugin. r=bsmedberg
    UltraBlame original commit: 7254cd0a6d75a0db751e8ef8e524039d1ce1b409

---
+++ dom/plugins/test/testplugin/javaplugin/nptest_name.cpp
@@ -0,0 +1,7 @@
+
+
+
+
+const char *sPluginName = "Java Test Plug-in";
+const char *sPluginDescription = "Dummy Java plug-in for testing purposes.";
+const char *sMimeDescription = "application/x-java-test:tstjava:Dummy java type";
