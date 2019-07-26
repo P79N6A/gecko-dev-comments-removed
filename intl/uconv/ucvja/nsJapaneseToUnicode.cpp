fd6388d229494debe8f8c1d7efc502aa42e662a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:29:57 +0000

    Bug 936899 - Build all of the uconv library in unified mode; r=smontagu
    UltraBlame original commit: bd6b4c8bae1e5d95637b982d6209454923b9aa7a

--- intl/uconv/ucvja/nsJapaneseToUnicode.cpp
+++ intl/uconv/ucvja/nsJapaneseToUnicode.cpp
@@ -13,8 +13,6 @@
 
 #include "mozilla/Assertions.h"
 
-static NS_DEFINE_CID(kCharsetConverterManagerCID, NS_ICHARSETCONVERTERMANAGER_CID);
-
 #ifdef XP_OS2
   
   
@@ -388,6 +386,8 @@ NS_IMETHODIMP nsISO2022JPToUnicodeV2::Convert(
    const char * aSrc, int32_t * aSrcLen,
      PRUnichar * aDest, int32_t * aDestLen)
 {
+   static NS_DEFINE_CID(kCharsetConverterManagerCID, NS_ICHARSETCONVERTERMANAGER_CID);
+
    static const uint16_t fbIdx[128] =
    {
 
