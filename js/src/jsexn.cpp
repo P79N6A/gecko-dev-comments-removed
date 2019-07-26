25141575751f14a24d2acd5fb764f42eb362ab73
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:29:02 +0000

    Bug 957688 - Make ComputeStackString pass a principal and remove checkAccess call. r=mrbkap
    Now that we have the principal-based filtering for stack walking, we can do this.
    This isn't technically equivalent to the old behavior, since a stack that goes:
    
    A -> B -> A
    
    would previous have only seen the second set of |A| frames, whereas now we'd
    see both sets. But this seems strictly better (also, it doesn't happen on the
    web).
    
    As noted, I've filed a bug for making this context- and saveFrameChain-agnostic.
    
    UltraBlame original commit: f6fb2082c9964d0a5930381cd7bf4ae71e2cb20a

--- js/src/jsexn.cpp
+++ js/src/jsexn.cpp
@@ -207,23 +207,18 @@ struct SuppressErrorsGuard
 static JSString *
 ComputeStackString(JSContext *cx)
 {
-    JSCheckAccessOp checkAccess = cx->runtime()->securityCallbacks->checkObjectAccess;
-
     StringBuffer sb(cx);
 
     {
         RootedAtom atom(cx);
         SuppressErrorsGuard seg(cx);
-        for (NonBuiltinScriptFrameIter i(cx); !i.done(); ++i) {
-            
-            if (checkAccess && i.isNonEvalFunctionFrame()) {
-                RootedValue v(cx);
-                RootedId callerid(cx, NameToId(cx->names().caller));
-                RootedObject obj(cx, i.callee());
-                if (!checkAccess(cx, obj, callerid, JSACC_READ, &v))
-                    break;
-            }
-
+        
+        
+        for (NonBuiltinScriptFrameIter i(cx, ScriptFrameIter::CURRENT_CONTEXT,
+                                         ScriptFrameIter::STOP_AT_SAVED,
+                                         cx->compartment()->principals);
+            !i.done(); ++i)
+        {
             
             atom = nullptr;
             if (i.isNonEvalFunctionFrame() && i.callee()->displayAtom())
