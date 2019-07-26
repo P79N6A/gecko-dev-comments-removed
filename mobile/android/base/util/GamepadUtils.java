b6bcb5d7c27c18810d6246d0fc845f12d690e94c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:38:53 +0000

    Bug 790775 - Sony Ericsson Xperia Play's "circle" and "cross" D-pad buttons generate unexpected keycodes. r=cpeterson
    UltraBlame original commit: cd0d9ac28abb83aae48a21a59d363e2d2800b804

--- mobile/android/base/util/GamepadUtils.java
+++ mobile/android/base/util/GamepadUtils.java
@@ -7,6 +7,7 @@ package org.mozilla.gecko.util;
 
 import android.os.Build;
 import android.view.InputDevice;
+import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,6 +15,8 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 public final class GamepadUtils {
+    private static final int SONY_XPERIA_GAMEPAD_DEVICE_ID = 196611;
+
     private static View.OnKeyListener sClickDispatcher;
     private static View.OnKeyListener sListItemClickDispatcher;
 
@@ -96,5 +99,54 @@ public final class GamepadUtils {
             };
         }
         return sListItemClickDispatcher;
+    }
+
+    public static KeyEvent translateSonyXperiaGamepadKeys(int keyCode, KeyEvent event) {
+        
+        
+        boolean areKeysSwapped = areSonyXperiaGamepadKeysSwapped();
+
+        
+        
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                keyCode = (areKeysSwapped ? KeyEvent.KEYCODE_BUTTON_A : KeyEvent.KEYCODE_BUTTON_B);
+                break;
+
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+                keyCode = (areKeysSwapped ? KeyEvent.KEYCODE_BUTTON_B : KeyEvent.KEYCODE_BUTTON_A);
+                break;
+
+            default:
+                return event;
+        }
+
+        return new KeyEvent(event.getAction(), keyCode);
+    }
+
+    public static boolean isSonyXperiaGamepadKeyEvent(KeyEvent event) {
+        return (event.getDeviceId() == SONY_XPERIA_GAMEPAD_DEVICE_ID &&
+                "Sony Ericsson".equals(Build.MANUFACTURER) &&
+                ("R800".equals(Build.MODEL) || "R800i".equals(Build.MODEL)));
+    }
+
+    private static boolean areSonyXperiaGamepadKeysSwapped() {
+        
+        
+        
+        final char DEFAULT_O_BUTTON_LABEL = 0x25CB;
+
+        boolean swapped = false;
+        int[] deviceIds = InputDevice.getDeviceIds();
+
+        for (int i= 0; deviceIds != null && i < deviceIds.length; i++) {
+            KeyCharacterMap keyCharacterMap = KeyCharacterMap.load(deviceIds[i]);
+            if (keyCharacterMap != null && DEFAULT_O_BUTTON_LABEL ==
+                keyCharacterMap.getDisplayLabel(KeyEvent.KEYCODE_DPAD_CENTER)) {
+                swapped = true;
+                break;
+            }
+        }
+        return swapped;
     }
 }
