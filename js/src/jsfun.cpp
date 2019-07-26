429f74dd1e2098d36a5662a5689b5cac4b652257
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:22:40 +0000

    Bug 866112 - GC: Fix some misc rooting hazards in the shell r=terrence
    UltraBlame original commit: 0d5eff7d8bb3f109c589542fe8c238f206a22641

--- js/src/jsfun.cpp
+++ js/src/jsfun.cpp
@@ -176,11 +176,11 @@ fun_getProperty(JSContext *cx, HandleObject obj_, HandleId id, MutableHandleValu
         
 
 
-        JSObject &caller = vp.toObject();
-        if (caller.isWrapper() && !Wrapper::wrapperHandler(&caller)->isSafeToUnwrap()) {
+        RootedObject caller(cx, &vp.toObject());
+        if (caller->isWrapper() && !Wrapper::wrapperHandler(caller)->isSafeToUnwrap()) {
             vp.setNull();
-        } else if (caller.isFunction()) {
-            JSFunction *callerFun = caller.toFunction();
+        } else if (caller->isFunction()) {
+            JSFunction *callerFun = caller->toFunction();
             if (callerFun->isInterpreted() && callerFun->strict()) {
                 JS_ReportErrorFlagsAndNumber(cx, JSREPORT_ERROR, js_GetErrorMessage, NULL,
                                              JSMSG_CALLER_IS_STRICT);
