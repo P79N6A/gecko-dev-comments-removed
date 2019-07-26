6254ce691f7d47487376d16d9255bb977d02f85f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:31:02 +0000

    Bug 1012712 - Add adb.py to devicemanager package, r=wlach. DONTBUILD
    UltraBlame original commit: 1fd65e3c2f4af65ec50a68443d4650907245fa62

--- testing/mozbase/mozdevice/mozdevice/__init__.py
+++ testing/mozbase/mozdevice/mozdevice/__init__.py
@@ -2,6 +2,8 @@
 
 
 
+from adb import ADBError, ADBRootError, ADBTimeoutError, ADBProcess, ADBCommand, ADBHost, ADBDevice
+from adb_android import ADBAndroid
 from devicemanager import DeviceManager, DMError, ZeroconfListener
 from devicemanagerADB import DeviceManagerADB
 from devicemanagerSUT import DeviceManagerSUT
