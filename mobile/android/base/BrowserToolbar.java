ad8b435ce291452630827a7e54442457970a51f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:54:38 +0000

    Bug 886528: Postpone popup menu initialization. [r=mfinkle]
    UltraBlame original commit: 333e96cb832ea91c586cb7eb01c481a15573c2a7

--- mobile/android/base/BrowserToolbar.java
+++ mobile/android/base/BrowserToolbar.java
@@ -417,29 +417,8 @@ public class BrowserToolbar implements Tabs.OnTabsChangedListener,
         }
 
         if (Build.VERSION.SDK_INT >= 11) {
-            View panel = mActivity.getMenuPanel();
-
             
-            
-            
-            
-
-            if (panel == null) {
-                mActivity.onCreatePanelMenu(Window.FEATURE_OPTIONS_PANEL, null);
-                panel = mActivity.getMenuPanel();
-
-                if (mHasSoftMenuButton) {
-                    mMenuPopup = new MenuPopup(mActivity);
-                    mMenuPopup.setPanelView(panel);
-
-                    mMenuPopup.setOnDismissListener(new PopupWindow.OnDismissListener() {
-                        @Override
-                        public void onDismiss() {
-                            mActivity.onOptionsMenuClosed(null);
-                        }
-                    });
-                }
-            }
+            mActivity.onCreatePanelMenu(Window.FEATURE_OPTIONS_PANEL, null);
         }
 
         mFocusOrder = Arrays.asList(mBack, mForward, mLayout, mReader, mSiteSecurity, mStop, mTabs);
@@ -1167,8 +1146,22 @@ public class BrowserToolbar implements Tabs.OnTabsChangedListener,
         if (!mHasSoftMenuButton)
             return false;
 
+        
+        if (mMenuPopup == null) {
+            View panel = mActivity.getMenuPanel();
+            mMenuPopup = new MenuPopup(mActivity);
+            mMenuPopup.setPanelView(panel);
+
+            mMenuPopup.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                @Override
+                public void onDismiss() {
+                    mActivity.onOptionsMenuClosed(null);
+                }
+            });
+        }
+
         GeckoAppShell.getGeckoInterface().invalidateOptionsMenu();
-        if (mMenuPopup != null && !mMenuPopup.isShowing())
+        if (!mMenuPopup.isShowing())
             mMenuPopup.showAsDropDown(mMenu);
 
         return true;
@@ -1178,7 +1171,7 @@ public class BrowserToolbar implements Tabs.OnTabsChangedListener,
         if (!mHasSoftMenuButton)
             return false;
 
-        if (mMenuPopup != null && mMenuPopup.isShowing())
+        if (mMenuPopup.isShowing())
             mMenuPopup.dismiss();
 
         return true;
