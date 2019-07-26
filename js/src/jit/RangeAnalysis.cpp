a39e01465bd61b0e569e0792b187c887b07e3dbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:12:01 +0000

    Backed out changeset 44eee084be1d (bug 924660) for jit-test failures
    UltraBlame original commit: f03466d0e283e907f43731f6bab341ffd63ab4bc

--- js/src/jit/RangeAnalysis.cpp
+++ js/src/jit/RangeAnalysis.cpp
@@ -1990,12 +1990,21 @@ MDiv::truncate()
     
     setTruncated(true);
 
-    
-    
-    if (specialization() == MIRType_Int32 && tryUseUnsignedOperands()) {
-        unsigned_ = true;
+    if (type() == MIRType_Double || type() == MIRType_Int32) {
+        specialization_ = MIRType_Int32;
+        setResultType(MIRType_Int32);
+        if (range())
+            range()->wrapAroundToInt32();
+
+        
+        
+        if (tryUseUnsignedOperands())
+            unsigned_ = true;
+
         return true;
     }
+
+    JS_ASSERT(specialization() != MIRType_Int32); 
 
     
     return false;
