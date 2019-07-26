ca9088a813bcae02249828ce56c51ab3262ac5ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:33:53 +0000

    Bug 906640 - add a pretty print button to the scratchpad; r=anton
    UltraBlame original commit: cff37b5ea5fb7da9d737981d959c63318b562b13

---
+++ browser/devtools/scratchpad/test/browser_scratchpad_pprint.js
@@ -0,0 +1,26 @@
+
+
+
+
+function test()
+{
+  waitForExplicitFinish();
+
+  gBrowser.selectedTab = gBrowser.addTab();
+  gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
+    gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
+    openScratchpad(runTests);
+  }, true);
+
+  content.location = "data:text/html;charset=utf8,test Scratchpad pretty print.";
+}
+
+function runTests(sw)
+{
+  const sp = sw.Scratchpad;
+  sp.setText("function main() { console.log(5); }");
+  sp.prettyPrint();
+  const prettyText = sp.getText();
+  ok(prettyText.contains("\n"));
+  finish();
+}
