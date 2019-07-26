828d2243186b368f92f2f3376a8550c37146e820
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:03:01 +0000

    Bug 953360 - <a ping> attribute should split on white space not only space r=jst
    UltraBlame original commit: c2c91ff3ec49173f8d46492ea3bf6f6fbbe675db

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -73,6 +73,7 @@
 #include "nsIChannel.h"
 #include "IHistory.h"
 #include "nsViewSourceHandler.h"
+#include "nsWhitespaceTokenizer.h"
 
 
 
@@ -366,28 +367,15 @@ ForEachPing(nsIContent *content, ForEachPingCallback callback, void *closure)
 
   nsIDocument *doc = content->OwnerDoc();
 
-  
-  const char16_t *start = value.BeginReading();
-  const char16_t *end   = value.EndReading();
-  const char16_t *iter  = start;
-  for (;;) {
-    if (iter < end && *iter != ' ') {
-      ++iter;
-    } else {  
-      while (*start == ' ' && start < iter)
-        ++start;
-      if (iter != start) {
-        nsCOMPtr<nsIURI> uri, baseURI = content->GetBaseURI();
-        ios->NewURI(NS_ConvertUTF16toUTF8(Substring(start, iter)),
-                    doc->GetDocumentCharacterSet().get(),
-                    baseURI, getter_AddRefs(uri));
-        if (CheckPingURI(uri, content)) {
-          callback(closure, content, uri, ios);
-        }
-      }
-      start = iter = iter + 1;
-      if (iter >= end)
-        break;
+  nsWhitespaceTokenizer tokenizer(value);
+
+  while (tokenizer.hasMoreTokens()) {
+    nsCOMPtr<nsIURI> uri, baseURI = content->GetBaseURI();
+    ios->NewURI(NS_ConvertUTF16toUTF8(tokenizer.nextToken()),
+                doc->GetDocumentCharacterSet().get(),
+                baseURI, getter_AddRefs(uri));
+    if (CheckPingURI(uri, content)) {
+      callback(closure, content, uri, ios);
     }
   }
 }
