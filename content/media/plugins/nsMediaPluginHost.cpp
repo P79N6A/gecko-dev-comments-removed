b8cf561642b632eb289fc8948babb3e482fb3bb1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:02:44 +0000

    Bug 791213 - Don't return nsresult from a function that returns bool; r=doublec
    UltraBlame original commit: 33087122ace7734b6aa9aaaab97f1a5c94ab498e

--- content/media/plugins/nsMediaPluginHost.cpp
+++ content/media/plugins/nsMediaPluginHost.cpp
@@ -71,8 +71,8 @@ static bool GetIntPref(const char* aPref, int32_t* aResult)
 {
   
   
-  NS_ENSURE_ARG_POINTER(aPref);
-  NS_ENSURE_ARG_POINTER(aResult);
+  NS_ENSURE_TRUE(aPref, false);
+  NS_ENSURE_TRUE(aResult, false);
   nsCOMPtr<GetIntPrefEvent> event = new GetIntPrefEvent(aPref, aResult);
   return NS_SUCCEEDED(NS_DispatchToMainThread(event, NS_DISPATCH_SYNC));
 }
