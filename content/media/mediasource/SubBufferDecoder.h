3069b81f5ca55c1668c3b94ca0a4eddc6a3a70e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:49:42 +0000

    Backout 07196a58650f (bug 1002320) so it can be relanded with a follow-up fix.
    UltraBlame original commit: 2141c8507506db7dc01ed15dffe5aa94dc574c1c

--- content/media/mediasource/SubBufferDecoder.h
+++ content/media/mediasource/SubBufferDecoder.h
@@ -21,7 +21,6 @@ public:
   
   SubBufferDecoder(MediaResource* aResource, MediaSourceDecoder* aParentDecoder)
     : BufferDecoder(aResource), mParentDecoder(aParentDecoder), mReader(nullptr)
-    , mMediaDuration(-1), mMediaStartTime(0)
   {
   }
 
@@ -73,21 +72,10 @@ public:
     return mMediaDuration;
   }
 
-  int64_t GetMediaStartTime()
-  {
-    return mMediaStartTime;
-  }
-
-  void SetMediaStartTime(int64_t aMediaStartTime)
-  {
-    mMediaStartTime = aMediaStartTime;
-  }
-
 private:
   MediaSourceDecoder* mParentDecoder;
   nsAutoPtr<MediaDecoderReader> mReader;
   int64_t mMediaDuration;
-  int64_t mMediaStartTime;
 };
 
 } 
