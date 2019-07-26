1524ee898299b4f3a0f3cd40e1a86d478b56137f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:38:55 +0000

    Backed out changeset 8b8fa0cf1e2f (Bug 994740 ) for causing crashtest failures on a CLOSED TREE
    UltraBlame original commit: 2e83d29d7f6b3a57bcea00ff6894985020e704b8

--- content/xml/document/src/nsXMLFragmentContentSink.cpp
+++ content/xml/document/src/nsXMLFragmentContentSink.cpp
@@ -362,8 +362,6 @@ nsXMLFragmentContentSink::FinishFragmentParsing(nsIDOMDocumentFragment** aFragme
   mContentStack.Clear();
   mDocumentURI = nullptr;
   mDocShell = nullptr;
-  mDocElement = nullptr;
-  mCurrentHead = nullptr;
   if (mParseError) {
     
     mRoot = nullptr;
