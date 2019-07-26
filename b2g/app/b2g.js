e3869b2c229d3566f0e2a98aeda52b0be79b1ca7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:25:50 +0000

    Bug 989720 - Geolocation not working and shows mock location. We were using the wrong API key value in preferences. r=mmc
    UltraBlame original commit: 15f36e5e47ace084978997fda3b4d8bbd44b896f

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -189,7 +189,7 @@ pref("privacy.item.syncAccount", true);
 
 pref("geo.provider.use_mls", false);
 pref("geo.cell.scan", true);
-pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZ_MOZILLA_API_KEY%");
+pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
 
 
 pref("geo.enabled", true);
