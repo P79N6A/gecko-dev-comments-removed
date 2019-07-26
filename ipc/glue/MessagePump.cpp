81003e1bbb1ed880b00db441375c4096ea14538b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:06:06 +0000

    Backed out changeset 0904d8d19947 (bug 913985)
    UltraBlame original commit: 6d82bdf0976509332fb82bab859bf7d94972239e

--- ipc/glue/MessagePump.cpp
+++ ipc/glue/MessagePump.cpp
@@ -91,7 +91,7 @@ MessagePump::Run(MessagePump::Delegate* aDelegate)
     
     
     
-    did_work |= GeckoAppShell::PumpMessageLoop();
+    did_work |= AndroidBridge::Bridge()->PumpMessageLoop();
 #endif
 
     did_work |= aDelegate->DoDelayedWork(&delayed_work_time_);
--- xpcom/base/nsSystemInfo.cpp
+++ xpcom/base/nsSystemInfo.cpp
@@ -22,7 +22,6 @@
 
 #ifdef MOZ_WIDGET_ANDROID
 #include "AndroidBridge.h"
-using namespace mozilla::widget::android;
 #endif
 
 #ifdef MOZ_WIDGET_GONK
@@ -142,7 +141,7 @@ nsSystemInfo::Init()
         android_sdk_version = version;
         if (version >= 8 && mozilla::AndroidBridge::Bridge()->GetStaticStringField("android/os/Build", "HARDWARE", str))
             SetPropertyAsAString(NS_LITERAL_STRING("hardware"), str);
-        bool isTablet = GeckoAppShell::IsTablet();
+        bool isTablet = mozilla::AndroidBridge::Bridge()->IsTablet();
         SetPropertyAsBool(NS_LITERAL_STRING("tablet"), isTablet);
         
         
