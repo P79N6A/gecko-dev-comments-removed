723d958362bbc7acf19555df012b9dd0f8ac901a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:15:46 +0000

    Bug 1022033 - Disable NS_lroundup30 on clang-cl; r=jrmuizel
    This function contains inline assembly which clang-cl can't compile yet.
    
    UltraBlame original commit: 299ed3d227d4429111c6f2ea316d30040f23197c

--- xpcom/ds/nsMathUtils.h
+++ xpcom/ds/nsMathUtils.h
@@ -35,7 +35,7 @@ inline int32_t NS_lround(double x)
 
 
 
-#if defined(XP_WIN32) && defined(_M_IX86) && !defined(__GNUC__)
+#if defined(XP_WIN32) && defined(_M_IX86) && !defined(__GNUC__) && !defined(__clang__)
 inline int32_t NS_lroundup30(float x)
 {
     
