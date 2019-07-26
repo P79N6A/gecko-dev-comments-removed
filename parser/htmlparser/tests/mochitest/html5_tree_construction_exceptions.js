d71995658082c480f23d94b6c9d0093f55c2672d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:41:42 +0000

    Bug 937003 - Use correct adjusted insertion location for foster parenting in template element. r=hsivonen
    UltraBlame original commit: 41607a557cad81f7d431eca0f6de1aa8e3682a65

--- parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
+++ parser/htmlparser/tests/mochitest/html5_tree_construction_exceptions.js
@@ -8,5 +8,4 @@ var html5Exceptions = {
   "<!doctype html><keygen><frameset>" : true, 
   "<select><keygen>" : true, 
   "<!DOCTYPE html><body><keygen>A" : true, 
-  "<template><template><table>Foo" : true, 
 }
