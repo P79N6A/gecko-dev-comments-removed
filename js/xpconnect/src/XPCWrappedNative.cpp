1c067080b7cc2ddeebb26922e325107889a8f98e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:11:02 +0000

    Backed out changeset 99eb8a370b21 (bug 952840) for permament B2G ICS Emulator Opt test failure
    UltraBlame original commit: ce408ac339c5af0f028ef408714e9d1e6dd47caf

--- js/xpconnect/src/XPCWrappedNative.cpp
+++ js/xpconnect/src/XPCWrappedNative.cpp
@@ -1513,14 +1513,17 @@ XPCWrappedNative::FindTearOff(XPCNativeInterface* aInterface,
         to = newChunk->mTearOffs;
     }
 
-    AutoMarkingWrappedNativeTearOffPtr tearoff(cx, to);
-    rv = InitTearOff(to, aInterface, needJSObject);
-    
-    
-    
-    to->Unmark();
-    if (NS_FAILED(rv))
-        to = nullptr;
+    {
+        
+        AutoMarkingWrappedNativeTearOffPtr tearoff(cx, to);
+        rv = InitTearOff(to, aInterface, needJSObject);
+        
+        
+        
+        to->Unmark();
+        if (NS_FAILED(rv))
+            to = nullptr;
+    }
 
     if (pError)
         *pError = rv;
