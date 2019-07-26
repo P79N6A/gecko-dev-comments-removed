73395947ae9fcbd271ee61e8b4650507132462c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:22:11 +0000

    Bug 864633 - broken site identity info bar part2 (tests from 813418). r=bsmith
    UltraBlame original commit: 45e73d8d89c978a0e7967bcb8e915578b8530e97

--- security/manager/ssl/src/nsIdentityChecking.cpp
+++ security/manager/ssl/src/nsIdentityChecking.cpp
@@ -115,6 +115,21 @@ static struct nsMyTrustedEVInfo myTrustedEVInfos[] = {
 
   {
     
+    
+    "1.3.6.1.4.1.13769.666.666.666.1.500.9.1",
+    "DEBUGtesting EV OID",
+    SEC_OID_UNKNOWN,
+    "AD:FE:0E:44:16:45:B0:17:46:8B:76:01:74:B7:FF:64:5A:EC:35:91",
+    "MIHhMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWlu"
+    "IFZpZXcxIzAhBgNVBAoTGk1vemlsbGEgLSBFViBkZWJ1ZyB0ZXN0IENBMR0wGwYD"
+    "VQQLExRTZWN1cml0eSBFbmdpbmVlcmluZzEmMCQGA1UEAxMdRVYgVGVzdGluZyAo"
+    "dW50cnVzdHdvcnRoeSkgQ0ExEzARBgNVBCkTCmV2LXRlc3QtY2ExLDAqBgkqhkiG"
+    "9w0BCQEWHWNoYXJsYXRhbkB0ZXN0aW5nLmV4YW1wbGUuY29t",
+    "AK/FPSJmJkky",
+    nullptr
+  },
+  {
+    
     "2.16.840.1.114171.500.9",
     "WellsSecure EV OID",
     SEC_OID_UNKNOWN,
@@ -1065,7 +1080,11 @@ nsNSSComponent::IdentityInfoInit()
     ias.serialNumber.type = siUnsignedInteger;
 
     entry.cert = CERT_FindCertByIssuerAndSN(nullptr, &ias);
-    NS_ASSERTION(entry.cert, "Could not find EV root in NSS storage");
+
+    
+    if (iEV != 0) {
+       NS_ASSERTION(entry.cert, "Could not find EV root in NSS storage");
+    }
 
     SECITEM_FreeItem(&ias.derIssuer, false);
     SECITEM_FreeItem(&ias.serialNumber, false);
