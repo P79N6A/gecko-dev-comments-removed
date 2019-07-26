50312b066bef1399c01b590237a4f5ebf9b2571a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:30:07 +0000

    Bug 994263 - Destroy GC'd nodes in the web audio editor views. r=vp
    UltraBlame original commit: b043afd9261faa92ac9a511c59c170a5b98195bd

---
+++ browser/devtools/webaudioeditor/test/browser_wa_destroy-node-01.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+function spawnTest() {
+  let [target, debuggee, panel] = yield initWebAudioEditor(DESTROY_NODES_URL);
+  let { panelWin } = panel;
+  let { gFront, $, $$, EVENTS } = panelWin;
+
+  let started = once(gFront, "start-context");
+
+  reload(target);
+
+  let destroyed = getN(panelWin, EVENTS.DESTROY_NODE, 10);
+
+  forceCC();
+
+  let [created] = yield Promise.all([
+    getNSpread(panelWin, EVENTS.CREATE_NODE, 13),
+    waitForGraphRendered(panelWin, 13, 2)
+  ]);
+
+  
+  
+  let actorIDs = created.map(ev => ev[1]);
+
+  
+  yield clickGraphNode(panelWin, actorIDs[5]);
+
+  forceCC();
+
+  
+  yield Promise.all([destroyed, waitForGraphRendered(panelWin, 3, 2)]);
+
+  
+  ok(panelWin.AudioNodes.length, 3, "All nodes should be GC'd except one gain, osc and dest node.");
+
+  
+  ok(findGraphNode(panelWin, actorIDs[0]), "dest should be in graph");
+  ok(findGraphNode(panelWin, actorIDs[1]), "osc should be in graph");
+  ok(findGraphNode(panelWin, actorIDs[2]), "gain should be in graph");
+
+  let { nodes, edges } = countGraphObjects(panelWin);
+
+  is(nodes, 3, "Only 3 nodes rendered in graph.");
+  is(edges, 2, "Only 2 edges rendered in graph.");
+
+  
+  ok(isVisible($("#web-audio-editor-details-pane-empty")),
+    "InspectorView empty message should show if the currently selected node gets collected.");
+
+  yield teardown(panel);
+  finish();
+}
+
