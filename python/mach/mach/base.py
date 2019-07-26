a714522d052f3a0c1a245e76640a02d7c8d720d3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:19:15 +0000

    Bug 808336 - Part 1: Refactor mach command handler management; r=jhammel
    Previously we were tighly coupled with MozbuildObject. This was not in
    the spirit of mach being a generic tool. Now, instead of passing multiple
    arguments to __init__ of the class providing the mach command we either
    pass 0 or 1. The number of arguments is detected when processing the
    @CommandProvider decorator. The optional argument is a named tuple
    containing mach run-time state.
    
    Capturing of mach command provider information is now captured in a
    class (as opposed to an anoymous tuple). We also capture these in a rich
    data structure which is passed as part of the run-time data to the
    command provider class. This allows mach commands to inspect the mach
    environment.
    
    Mach decorators have been moved to mach.decorators. mach.base is
    reserved for generic mach data/container classes.
    
    Existing mach command classes derived from MozbuildObject have been
    replaced with either object or mozbuild.base.MachCommandBase. This
    latter class translates the mach context instance passed to __init__
    into the constructor arguments for MozbuildObject.__init__.
    
    Support for registering function handlers with mach has been removed.
    All handlers must be inside classes.
    
    UltraBlame original commit: e0e1220d2c5a1086a4911be8d69e2937b842d116

--- python/mach/mach/base.py
+++ python/mach/mach/base.py
@@ -4,91 +4,49 @@
 
 from __future__ import unicode_literals
 
-import types
-
-from mach.registrar import register_method_handler
+from collections import namedtuple
 
 
-def CommandProvider(cls):
-    """Class decorator to denote that it provides subcommands for Mach.
+CommandContext = namedtuple('CommandContext', ['topdir', 'cwd',
+    'settings', 'log_manager', 'commands'])
 
-    When this decorator is present, mach looks for commands being defined by
-    methods inside the class.
+
+class MethodHandler(object):
+    """Describes a Python method that implements a mach command.
+
+    Instances of these are produced by mach when it processes classes
+    defining mach commands.
     """
+    __slots__ = (
+        
+        
+        
+        'cls',
 
-    
-    
-    
-    
-    
-    
+        
+        
+        
+        'pass_context',
 
-    
-    
-    
-    
-    for attr in sorted(cls.__dict__.keys()):
-        value = cls.__dict__[attr]
+        
+        
+        
+        'method',
 
-        if not isinstance(value, types.FunctionType):
-            continue
+        
+        
+        'parser_args',
 
-        parser_args = getattr(value, '_mach_command', None)
-        if parser_args is None:
-            continue
+        
+        
+        'arguments',
+    )
 
-        arguments = getattr(value, '_mach_command_args', None)
+    def __init__(self, cls, method, parser_args, arguments=None,
+        pass_context=False):
 
-        register_method_handler(cls, attr, (parser_args[0], parser_args[1]),
-            arguments or [])
-
-    return cls
-
-
-class Command(object):
-    """Decorator for functions or methods that provide a mach subcommand.
-
-    The decorator accepts arguments that would be passed to add_parser() of an
-    ArgumentParser instance created via add_subparsers(). Essentially, it
-    accepts the arguments one would pass to add_argument().
-
-    For example:
-
-        @Command('foo', help='Run the foo action')
-        def foo(self):
-            pass
-    """
-    def __init__(self, *args, **kwargs):
-        self._command_args = (args, kwargs)
-
-    def __call__(self, func):
-        func._mach_command = self._command_args
-
-        return func
-
-
-class CommandArgument(object):
-    """Decorator for additional arguments to mach subcommands.
-
-    This decorator should be used to add arguments to mach commands. Arguments
-    to the decorator are proxied to ArgumentParser.add_argument().
-
-    For example:
-
-        @Command('foo', help='Run the foo action')
-        @CommandArgument('-b', '--bar', action='store_true', default=False,
-            help='Enable bar mode.')
-        def foo(self):
-            pass
-    """
-    def __init__(self, *args, **kwargs):
-        self._command_args = (args, kwargs)
-
-    def __call__(self, func):
-        command_args = getattr(func, '_mach_command_args', [])
-
-        command_args.append(self._command_args)
-
-        func._mach_command_args = command_args
-
-        return func
+        self.cls = cls
+        self.method = method
+        self.parser_args = parser_args
+        self.arguments = arguments or []
+        self.pass_context = pass_context
---
+++ python/mach/mach/decorators.py
@@ -0,0 +1,113 @@
+
+
+
+
+from __future__ import unicode_literals
+
+import inspect
+import types
+
+from .base import MethodHandler
+from .registrar import Registrar
+
+
+def CommandProvider(cls):
+    """Class decorator to denote that it provides subcommands for Mach.
+
+    When this decorator is present, mach looks for commands being defined by
+    methods inside the class.
+    """
+
+    
+    
+    
+    
+    
+    
+
+    
+    pass_context = False
+
+    if inspect.ismethod(cls.__init__):
+        spec = inspect.getargspec(cls.__init__)
+
+        if len(spec.args) > 2:
+            msg = 'Mach @CommandProvider class %s implemented incorrectly. ' + \
+                  '__init__() must take 1 or 2 arguments. From %s'
+            msg = msg % (cls.__name__, inspect.getsourcefile(cls))
+            raise Exception(msg)
+
+        if len(spec.args) == 2:
+            pass_context = True
+
+    
+    
+    
+    
+    for attr in sorted(cls.__dict__.keys()):
+        value = cls.__dict__[attr]
+
+        if not isinstance(value, types.FunctionType):
+            continue
+
+        parser_args = getattr(value, '_mach_command', None)
+        if parser_args is None:
+            continue
+
+        arguments = getattr(value, '_mach_command_args', None)
+
+        handler = MethodHandler(cls, attr, (parser_args[0], parser_args[1]),
+            arguments=arguments, pass_context=pass_context)
+
+        Registrar.register_command_handler(handler)
+
+    return cls
+
+
+class Command(object):
+    """Decorator for functions or methods that provide a mach subcommand.
+
+    The decorator accepts arguments that would be passed to add_parser() of an
+    ArgumentParser instance created via add_subparsers(). Essentially, it
+    accepts the arguments one would pass to add_argument().
+
+    For example:
+
+        @Command('foo', help='Run the foo action')
+        def foo(self):
+            pass
+    """
+    def __init__(self, *args, **kwargs):
+        self._command_args = (args, kwargs)
+
+    def __call__(self, func):
+        func._mach_command = self._command_args
+
+        return func
+
+
+class CommandArgument(object):
+    """Decorator for additional arguments to mach subcommands.
+
+    This decorator should be used to add arguments to mach commands. Arguments
+    to the decorator are proxied to ArgumentParser.add_argument().
+
+    For example:
+
+        @Command('foo', help='Run the foo action')
+        @CommandArgument('-b', '--bar', action='store_true', default=False,
+            help='Enable bar mode.')
+        def foo(self):
+            pass
+    """
+    def __init__(self, *args, **kwargs):
+        self._command_args = (args, kwargs)
+
+    def __call__(self, func):
+        command_args = getattr(func, '_mach_command_args', [])
+
+        command_args.append(self._command_args)
+
+        func._mach_command_args = command_args
+
+        return func
