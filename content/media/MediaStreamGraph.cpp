4636276b3af741c0c54270136b9d879b6e1ac843
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:39:37 +0000

    Bug 875402 - Do the work for initializing a MediaInputPort even if the message is handled during graph shutdown; r=roc
    UltraBlame original commit: dc96b47626762c9e668b7c44d380dad23b3b9903

--- content/media/MediaStreamGraph.cpp
+++ content/media/MediaStreamGraph.cpp
@@ -1885,9 +1885,6 @@ MediaInputPort::Destroy()
     {
       Run();
     }
-    
-    
-    
     MediaInputPort* mPort;
   };
   GraphImpl()->AppendMessage(new Message(this));
@@ -1928,6 +1925,10 @@ ProcessedMediaStream::AllocateInputPort(MediaStream* aStream, uint32_t aFlags,
       mPort->Init();
       
       mPort.forget();
+    }
+    virtual void RunDuringShutdown()
+    {
+      Run();
     }
     nsRefPtr<MediaInputPort> mPort;
   };
