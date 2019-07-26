f973445ddb42177d154b86ef475c897ee0159bbd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:45:33 +0000

    Bug 905262 - Create a JS add-on API for adding content to the promotional banner. r=wesj
    UltraBlame original commit: 5040ab6bfbc51ed127f0981e4ea3225e418f50a6

--- mobile/android/base/home/HomeBanner.java
+++ mobile/android/base/home/HomeBanner.java
@@ -5,16 +5,31 @@
 
 package org.mozilla.gecko.home;
 
+import org.mozilla.gecko.GeckoAppShell;
+import org.mozilla.gecko.GeckoEvent;
 import org.mozilla.gecko.R;
+import org.mozilla.gecko.gfx.BitmapUtils;
+import org.mozilla.gecko.util.GeckoEventListener;
+import org.mozilla.gecko.util.ThreadUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class HomeBanner extends LinearLayout {
+public class HomeBanner extends LinearLayout
+                        implements GeckoEventListener {
+    private static final String LOGTAG = "GeckoHomeBanner";
 
     public HomeBanner(Context context) {
         this(context, null);
@@ -43,9 +58,78 @@ public class HomeBanner extends LinearLayout {
                 HomeBanner.this.setVisibility(View.GONE);
             }
         });
+
+        setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("HomeBanner:Click", (String) getTag()));
+            }
+        });
+
+        GeckoAppShell.getEventDispatcher().registerEventListener("HomeBanner:Data", this);
+        GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("HomeBanner:Get", null));
     }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+
+        GeckoAppShell.getEventDispatcher().unregisterEventListener("HomeBanner:Data", this);
+     }
 
     public boolean isDismissed() {
         return (getVisibility() == View.GONE);
+    }
+
+    @Override
+    public void handleMessage(String event, JSONObject message) {
+        try {
+            
+            setTag(message.getString("id"));
+
+            final String text = message.getString("text");
+            final TextView textView = (TextView) findViewById(R.id.text);
+
+            
+            ThreadUtils.postToUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText(text);
+                    setVisibility(View.VISIBLE);
+                }
+            });
+        } catch (JSONException e) {
+            Log.e(LOGTAG, "Exception handling " + event + " message", e);
+            return;
+        }
+
+        final String iconURI = message.optString("iconURI");
+        final ImageView iconView = (ImageView) findViewById(R.id.icon);
+
+        if (TextUtils.isEmpty(iconURI)) {
+            
+            iconView.setVisibility(View.GONE);
+            return;
+        }
+
+        BitmapUtils.getDrawable(getContext(), iconURI, new BitmapUtils.BitmapLoader() {
+            @Override
+            public void onBitmapFound(final Drawable d) {
+                
+                if (d == null) {
+                    iconView.setVisibility(View.GONE);
+                    return;
+                }
+
+                
+                ThreadUtils.postToUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        iconView.setImageDrawable(d);
+                    }
+                });
+            }
+        });
     }
 }
---
+++ mobile/android/modules/Home.jsm
@@ -0,0 +1,132 @@
+
+
+
+
+
+"use strict";
+
+this.EXPORTED_SYMBOLS = ["Home"];
+
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+
+function resolveGeckoURI(aURI) {
+  if (!aURI)
+    throw "Can't resolve an empty uri";
+
+  if (aURI.startsWith("chrome://")) {
+    let registry = Cc['@mozilla.org/chrome/chrome-registry;1'].getService(Ci["nsIChromeRegistry"]);
+    return registry.convertChromeURL(Services.io.newURI(aURI, null, null)).spec;
+  } else if (aURI.startsWith("resource://")) {
+    let handler = Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
+    return handler.resolveURI(Services.io.newURI(aURI, null, null));
+  }
+  return aURI;
+}
+
+function sendMessageToJava(message) {
+  return Services.androidBridge.handleGeckoMessage(JSON.stringify(message));
+}
+
+function BannerMessage(options) {
+  let uuidgen = Cc["@mozilla.org/uuid-generator;1"].getService(Ci.nsIUUIDGenerator);
+  this.id = uuidgen.generateUUID().toString();
+
+  if ("text" in options && options.text != null)
+    this.text = options.text;
+
+  if ("icon" in options && options.icon != null)
+    this.iconURI = resolveGeckoURI(options.icon);
+
+  if ("onclick" in options && typeof options.onclick === "function")
+    this.onclick = options.onclick;
+}
+
+let HomeBanner = {
+  
+  _messages: {},
+
+  
+  _queue: [],
+
+  observe: function(subject, topic, data) {
+    switch(topic) {
+      case "HomeBanner:Get":
+        this._handleGet();
+        break;
+
+      case "HomeBanner:Click":
+        this._handleClick(data);
+        break;
+    }
+  },
+
+  _handleGet: function() {
+    
+    
+    let id = this._queue.shift();
+    this._queue.push(id);
+
+    let message = this._messages[id];
+    sendMessageToJava({
+      type: "HomeBanner:Data",
+      id: message.id,
+      text: message.text,
+      iconURI: message.iconURI
+    });
+  },
+
+  _handleClick: function(id) {
+    let message = this._messages[id];
+    if (message.onclick)
+      message.onclick();
+  },
+
+  
+
+
+
+
+  add: function(options) {
+    let message = new BannerMessage(options);
+    this._messages[message.id] = message;
+
+    
+    this._queue.push(message.id);
+
+    
+    
+    if (Object.keys(this._messages).length == 1) {
+      Services.obs.addObserver(this, "HomeBanner:Get", false);
+      Services.obs.addObserver(this, "HomeBanner:Click", false);
+    }
+
+    return message.id;
+  },
+
+  
+
+
+
+
+  remove: function(id) {
+    delete this._messages[id];
+
+    
+    let index = this._queue.indexOf(id);
+    this._queue.splice(index, 1);
+
+    
+    if (Object.keys(this._messages).length == 0) {
+      Services.obs.removeObserver(this, "HomeBanner:Get");
+      Services.obs.removeObserver(this, "HomeBanner:Click");
+    }
+  }
+};
+
+
+this.Home = {
+  banner: HomeBanner
+}
