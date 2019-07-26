a6e12d0f2928835a0e55c1f49d968b1efe431001
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:08:44 +0000

    Bug 955888. Part 1: Add ChaosMode.h base functionality. r=waldo
    UltraBlame original commit: f656d2fd42644436d984504570af35e6d57bc5ec

---
+++ mfbt/ChaosMode.h
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+#ifndef mozilla_ChaosMode_h
+#define mozilla_ChaosMode_h
+
+#include <stdint.h>
+#include <stdlib.h>
+
+namespace mozilla {
+
+
+
+
+
+
+class ChaosMode
+{
+  public:
+    static bool isActive()
+    {
+      
+      return false;
+    }
+
+    
+
+
+
+    static uint32_t randomUint32LessThan(uint32_t aBound)
+    {
+      return uint32_t(rand()) % aBound;
+    }
+};
+
+} 
+
+#endif 
