4c3998baf0179985449b6b8bbf5818d50019db53
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:06:24 +0000

    Bug 883002 - Add opensearch support to the mozbrowser API r=jlebar
    UltraBlame original commit: 5dfb22ea0888ef74eec1ccacf9babbfc6dd7fc98

--- dom/browser-element/BrowserElementChildPreload.js
+++ dom/browser-element/BrowserElementChildPreload.js
@@ -171,7 +171,7 @@ BrowserElementChild.prototype = {
                       false);
 
     addEventListener('DOMLinkAdded',
-                     this._iconChangedHandler.bind(this),
+                     this._linkAddedHandler.bind(this),
                       true,
                       false);
 
@@ -447,22 +447,45 @@ BrowserElementChild.prototype = {
   },
 
   _iconChangedHandler: function(e) {
-    debug("Got iconchanged: (" + e.target.href + ")");
-    var hasIcon = e.target.rel.split(' ').some(function(x) {
-      return x.toLowerCase() === 'icon';
-    });
+    debug('Got iconchanged: (' + e.target.href + ')');
 
-    if (hasIcon) {
-      var win = e.target.ownerDocument.defaultView;
-      
-      
-      if (win == content) {
-        sendAsyncMsg('iconchange', { _payload_: e.target.href });
-      }
-      else {
-        debug("Not top level!");
-      }
+    sendAsyncMsg('iconchange', { _payload_: e.target.href });
+  },
+
+  _openSearchHandler: function(e) {
+    debug('Got opensearch: (' + e.target.href + ')');
+
+    if (e.target.type !== "application/opensearchdescription+xml") {
+      return;
     }
+
+    sendAsyncMsg('opensearch', { title: e.target.title,
+                                 href: e.target.href });
+
+  },
+
+  
+  _linkAddedHandler: function(e) {
+    let win = e.target.ownerDocument.defaultView;
+    
+    
+    if (win != content) {
+      debug('Not top level!');
+      return;
+    }
+
+    let handlers = {
+      'icon': this._iconChangedHandler,
+      'search': this._openSearchHandler
+    };
+
+    debug('Got linkAdded: (' + e.target.href + ') ' + e.target.rel);
+    e.target.rel.split(' ').forEach(function(x) {
+      let token = x.toLowerCase();
+      if (handlers[token]) {
+        handlers[token](e);
+      }
+    }, this);
   },
 
   _addMozAfterPaintHandler: function(callback) {
---
+++ dom/browser-element/mochitest/browserElement_Opensearch.js
@@ -0,0 +1,109 @@
+
+
+
+
+"use strict";
+
+SimpleTest.waitForExplicitFinish();
+browserElementTestHelpers.setEnabledPref(true);
+browserElementTestHelpers.addPermission();
+
+function createHtml(link) {
+  return 'data:text/html,<html><head>' + link + '<body></body></html>';
+}
+
+function createLink(name) {
+  return '<link rel="search" title="Test OpenSearch" type="application/opensearchdescription+xml" href="http://example.com/' + name + '.xml">';
+}
+
+function runTest() {
+  var iframe1 = document.createElement('iframe');
+  SpecialPowers.wrap(iframe1).mozbrowser = true;
+  document.body.appendChild(iframe1);
+
+  
+  
+  
+  var iframe2 = document.createElement('iframe');
+  SpecialPowers.wrap(iframe2).mozbrowser = true;
+  document.body.appendChild(iframe2);
+
+  
+  
+  var iframe3 = document.createElement('iframe');
+  document.body.appendChild(iframe3);
+
+  var numLinkChanges = 0;
+
+  iframe1.addEventListener('mozbrowseropensearch', function(e) {
+
+    numLinkChanges++;
+
+    if (numLinkChanges == 1) {
+      is(e.detail.title, 'Test OpenSearch');
+      is(e.detail.href, 'http://example.com/mysearch.xml');
+
+      
+      
+      SpecialPowers.getBrowserFrameMessageManager(iframe1)
+                   .loadFrameScript("data:,content.document.title='New title';",
+                                     false);
+
+      SpecialPowers.getBrowserFrameMessageManager(iframe1)
+                   .loadFrameScript("data:,content.document.head.insertAdjacentHTML('beforeend', '<link rel=SEARCH type=application/opensearchdescription+xml href=http://example.com/newsearch.xml>')",
+                                     false);
+
+      SpecialPowers.getBrowserFrameMessageManager(iframe2)
+                   .loadFrameScript("data:,content.document.head.insertAdjacentHTML('beforeend', '<link rel=SEARCH type=application/opensearchdescription+xml href=http://example.com/newsearch.xml>')",
+                                     false);
+    }
+    else if (numLinkChanges == 2) {
+      is(e.detail.href, 'http://example.com/newsearch.xml');
+
+      
+      iframe1.src = createHtml(createLink('3rdsearch'));
+    }
+    else if (numLinkChanges == 3) {
+      is(e.detail.href, 'http://example.com/3rdsearch.xml');
+
+      
+      
+      SpecialPowers.getBrowserFrameMessageManager(iframe1)
+                   .loadFrameScript("data:,content.document.head.insertAdjacentHTML('beforeend', '<link rel=someopensearch type=application/opensearchdescription+xml href=http://example.com/newsearch.xml>')",
+                                     false);
+      
+      iframe1.src = createHtml(createLink('another') + createLink('search'));
+    }
+    else if (numLinkChanges == 4) {
+      is(e.detail.href, 'http://example.com/another.xml');
+      
+    }
+    else if (numLinkChanges == 5) {
+      is(e.detail.href, 'http://example.com/search.xml');
+
+      
+      SpecialPowers.getBrowserFrameMessageManager(iframe1)
+                   .loadFrameScript("data:,content.document.head.insertAdjacentHTML('beforeend', '<link rel=SEARCH type=application/opensearchdescription+xml href=http://example.com/ucasesearch.xml>')",
+                                     false);
+    }
+    else if (numLinkChanges == 6) {
+      is(e.detail.href, 'http://example.com/ucasesearch.xml');
+      SimpleTest.finish();
+    } else {
+      ok(false, 'Too many opensearch events.');
+    }
+  });
+
+  iframe3.addEventListener('mozbrowseropensearch', function(e) {
+    ok(false, 'Should not get a opensearch event for iframe3.');
+  });
+
+
+  iframe1.src = createHtml(createLink('mysearch'));
+  
+  iframe2.src = createHtml(createLink('mysearch'));
+  iframe3.src = createHtml(createLink('mysearch'));
+
+}
+
+addEventListener('testready', runTest);
