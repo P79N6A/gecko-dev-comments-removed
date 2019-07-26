02d9be4db3084d4ad5551eb311edd16f4c528e8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:00:06 +0000

    Bug 781248, eagerly allocate arrays with argument count higher than 1
    UltraBlame original commit: 98004d8529b54f8316fca8fa4d13734cdb7d4295

--- js/src/ion/MCallOptimize.cpp
+++ js/src/ion/MCallOptimize.cpp
@@ -168,10 +168,13 @@ IonBuilder::InliningStatus
 IonBuilder::inlineArray(uint32 argc, bool constructing)
 {
     uint32_t initLength = 0;
+    MNewArray::AllocatingBehaviour allocating = MNewArray::NewArray_Unallocating;
 
     
-    if (argc >= 2)
+    if (argc >= 2) {
         initLength = argc;
+        allocating = MNewArray::NewArray_Allocating;
+    }
 
     
     if (argc == 1) {
@@ -195,7 +198,7 @@ IonBuilder::inlineArray(uint32 argc, bool constructing)
     if (!templateObject)
         return InliningStatus_Error;
 
-    MNewArray *ins = new MNewArray(initLength, templateObject, MNewArray::NewArray_Unallocating);
+    MNewArray *ins = new MNewArray(initLength, templateObject, allocating);
     current->add(ins);
     current->push(ins);
 
