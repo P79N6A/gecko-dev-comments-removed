8fe4116ce5c275ae26139b491c9b1c6a29efdae8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:31:24 +0000

    Bug 867650 - Update awesomescreen results correctly when there's so autocomplete text. r=wesj
    UltraBlame original commit: 623728226a0de561bf5a36e064e7da1e082c0091

--- mobile/android/base/AwesomeBar.java
+++ mobile/android/base/AwesomeBar.java
@@ -788,7 +788,7 @@ public class AwesomeBar extends GeckoActivity
         }
 
         
-        if (mAutoCompleteResult == null || !mAutoCompleteResult.equals(text)) {
+        if (TextUtils.isEmpty(mAutoCompleteResult) || !mAutoCompleteResult.equals(text)) {
             mAwesomeTabs.filter(text, useHandler ? this : null);
             mAutoCompletePrefix = text;
 
