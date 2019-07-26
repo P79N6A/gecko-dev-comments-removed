cb0ca07ca33cae412d53915a3883f4c9e2683df9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:50:33 +0000

    Bug 833446: Remove nsIDOMSVGAnimationElement r=peterv
    UltraBlame original commit: 71060161c7e186257ef36840d99fa288087a8b02

--- content/svg/content/src/SVGAnimationElement.cpp
+++ content/svg/content/src/SVGAnimationElement.cpp
@@ -22,7 +22,6 @@ NS_IMPL_RELEASE_INHERITED(SVGAnimationElement, SVGAnimationElementBase)
 
 NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(SVGAnimationElement)
   NS_INTERFACE_MAP_ENTRY(nsISMILAnimationElement)
-  NS_INTERFACE_MAP_ENTRY(nsIDOMElementTimeControl)
   NS_INTERFACE_MAP_ENTRY(DOMSVGTests)
 NS_INTERFACE_MAP_END_INHERITING(SVGAnimationElementBase)
 
@@ -166,22 +165,6 @@ SVGAnimationElement::TimedElement()
   return mTimedElement;
 }
 
-
-
-
-
-NS_IMETHODIMP
-SVGAnimationElement::GetTargetElement(nsIDOMSVGElement** aTarget)
-{
-  
-  nsSVGElement* target = GetTargetElement();
-
-  nsCOMPtr<nsIDOMSVGElement> targetSVG = do_QueryInterface(target);
-  targetSVG.forget(aTarget);
-
-  return NS_OK;
-}
-
 nsSVGElement*
 SVGAnimationElement::GetTargetElement()
 {
@@ -191,15 +174,6 @@ SVGAnimationElement::GetTargetElement()
   nsIContent* target = GetTargetElementContent();
 
   return (target && target->IsSVG()) ? static_cast<nsSVGElement*>(target) : nullptr;
-}
-
-
-NS_IMETHODIMP
-SVGAnimationElement::GetStartTime(float* retval)
-{
-  ErrorResult rv;
-  *retval = GetStartTime(rv);
-  return rv.ErrorCode();
 }
 
 float
@@ -216,14 +190,6 @@ SVGAnimationElement::GetStartTime(ErrorResult& rv)
   return float(double(startTime.GetMillis()) / PR_MSEC_PER_SEC);
 }
 
-
-NS_IMETHODIMP
-SVGAnimationElement::GetCurrentTime(float* retval)
-{
-  *retval = GetCurrentTime();
-  return NS_OK;
-}
-
 float
 SVGAnimationElement::GetCurrentTime()
 {
@@ -235,15 +201,6 @@ SVGAnimationElement::GetCurrentTime()
   }
 
   return 0.0f;
-}
-
-
-NS_IMETHODIMP
-SVGAnimationElement::GetSimpleDuration(float* retval)
-{
-  ErrorResult rv;
-  *retval = GetSimpleDuration(rv);
-  return rv.ErrorCode();
 }
 
 float
@@ -442,7 +399,8 @@ SVGAnimationElement::ActivateByHyperlink()
     
     
   } else {
-    BeginElement();
+    ErrorResult rv;
+    BeginElement(rv);
   }
 }
 
@@ -461,25 +419,6 @@ SVGAnimationElement::GetTimeContainer()
   return nullptr;
 }
 
-
-
-NS_IMETHODIMP
-SVGAnimationElement::BeginElement(void)
-{
-  return BeginElementAt(0.f);
-}
-
-
-NS_IMETHODIMP
-SVGAnimationElement::BeginElementAt(float offset)
-{
-  NS_ENSURE_FINITE(offset, NS_ERROR_ILLEGAL_VALUE);
-
-  ErrorResult rv;
-  BeginElementAt(offset, rv);
-  return rv.ErrorCode();
-}
-
 void
 SVGAnimationElement::BeginElementAt(float offset, ErrorResult& rv)
 {
@@ -496,24 +435,6 @@ SVGAnimationElement::BeginElementAt(float offset, ErrorResult& rv)
   
   
   FlushAnimations();
-}
-
-
-NS_IMETHODIMP
-SVGAnimationElement::EndElement(void)
-{
-  return EndElementAt(0.f);
-}
-
-
-NS_IMETHODIMP
-SVGAnimationElement::EndElementAt(float offset)
-{
-  NS_ENSURE_FINITE(offset, NS_ERROR_ILLEGAL_VALUE);
-
-  ErrorResult rv;
-  EndElementAt(offset, rv);
-  return rv.ErrorCode();
 }
 
 void
