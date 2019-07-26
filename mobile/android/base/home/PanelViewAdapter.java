4ddcf94763f6238f6cb0f90d6b244015284e6dd7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:56:16 +0000

    Bug 966022 - Use ItemType to define panel view items (r=margaret)
    UltraBlame original commit: c2d4afef1eb1802dbd604bbab998158e96cbab9b

---
+++ mobile/android/base/home/PanelViewAdapter.java
@@ -0,0 +1,37 @@
+
+
+
+
+
+package org.mozilla.gecko.home;
+
+import org.mozilla.gecko.R;
+import org.mozilla.gecko.home.HomeConfig.ItemType;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+class PanelViewAdapter extends CursorAdapter {
+	private final Context mContext;
+	private final ItemType mItemType;
+
+    public PanelViewAdapter(Context context, ItemType itemType) {
+        super(context, null, 0);
+        mContext = context;
+        mItemType = itemType;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        final PanelItemView item = (PanelItemView) view;
+        item.updateFromCursor(cursor);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return PanelItemView.create(mContext, mItemType);
+    }
+}
