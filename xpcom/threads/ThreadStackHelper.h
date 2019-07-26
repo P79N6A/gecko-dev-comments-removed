3ede2551dbb7d9e679d656de9ce49f287bf131c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:45:40 +0000

    Bug 935092 - Add ThreadStackHelper to get a thread's pesudo-stack; r=BenWa
    UltraBlame original commit: 288456164d94cf77ac4eab6f12c16da3194aa30a

---
+++ xpcom/threads/ThreadStackHelper.h
@@ -0,0 +1,100 @@
+
+
+
+
+
+#ifndef mozilla_ThreadStackHelper_h
+#define mozilla_ThreadStackHelper_h
+
+#include "mozilla/ThreadHangStats.h"
+
+#include "GeckoProfiler.h"
+
+#include <stddef.h>
+
+#if defined(XP_LINUX)
+#include <signal.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#elif defined(XP_WIN)
+#include <windows.h>
+#elif defined(XP_MACOSX)
+#include <mach/mach.h>
+#endif
+
+namespace mozilla {
+
+
+
+
+
+
+
+
+
+
+
+class ThreadStackHelper
+{
+public:
+  typedef Telemetry::HangHistogram::Stack Stack;
+
+private:
+  const PseudoStack* const mPseudoStack;
+  Stack mStackBuffer;
+  size_t mMaxStackSize;
+
+  bool PrepareStackBuffer(Stack& aStack);
+  void FillStackBuffer();
+
+public:
+  
+
+
+  static void Startup();
+  
+
+
+  static void Shutdown();
+
+  
+
+
+  ThreadStackHelper();
+
+  ~ThreadStackHelper();
+
+  
+
+
+
+
+
+  void GetStack(Stack& aStack);
+
+#if defined(XP_LINUX)
+private:
+  static int sInitialized;
+  static sem_t sSem;
+  static struct sigaction sOldSigAction;
+  static ThreadStackHelper* sCurrent;
+
+  static void SigAction(int aSignal, siginfo_t* aInfo, void* aContext);
+
+  pid_t mThreadID;
+
+#elif defined(XP_WIN)
+private:
+  bool mInitialized;
+  HANDLE mThreadID;
+
+#elif defined(XP_MACOSX)
+private:
+  thread_act_t mThreadID;
+
+#endif
+};
+
+} 
+
+#endif 
