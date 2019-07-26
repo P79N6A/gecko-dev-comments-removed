8eb062b9bcd156139bfb774628dab1b54f2555b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:14:28 +0000

    Bug 1016523 - Test. (r=jimb)
    UltraBlame original commit: 4c794b0e6e9cba4324152562377df20d2d0d5931

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -4660,6 +4660,7 @@ class JS_PUBLIC_API(AutoSaveExceptionState)
 {
   private:
     JSContext *context;
+    bool wasPropagatingForcedReturn;
     bool wasThrowing;
     RootedValue exceptionValue;
 
@@ -4681,6 +4682,7 @@ class JS_PUBLIC_API(AutoSaveExceptionState)
 
 
     void drop() {
+        wasPropagatingForcedReturn = false;
         wasThrowing = false;
         exceptionValue.setUndefined();
     }
