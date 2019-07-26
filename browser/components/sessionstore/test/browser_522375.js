71f3ab0d7f15991a9a98551340473103edf262b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:06:16 +0000

    Bug 689256 - Record invalid PROCESS_CREATION timestamps and adjust tests to cope with them. r=glandium
    UltraBlame original commit: d6f51f2bac4bd129110acb00f4908741900464d8

--- browser/components/sessionstore/test/browser_522375.js
+++ browser/components/sessionstore/test/browser_522375.js
@@ -1,7 +1,17 @@
 function test() {
   var startup_info = Components.classes["@mozilla.org/toolkit/app-startup;1"].getService(Components.interfaces.nsIAppStartup).getStartupInfo();
   
-  ok(startup_info.process <= startup_info.main, "process created before main is run " + uneval(startup_info));
+
+  
+  
+  var telemetry = Cc["@mozilla.org/base/telemetry;1"].getService(Ci.nsITelemetry);
+  var snapshot = telemetry.getHistogramById("STARTUP_MEASUREMENT_ERRORS")
+                          .snapshot();
+
+  if (snapshot.counts[0] == 0)
+    ok(startup_info.process <= startup_info.main, "process created before main is run " + uneval(startup_info));
+  else
+    todo(false, "An error occurred while recording the process creation timestamp, skipping this test");
 
   
   if (startup_info.firstPaint)
