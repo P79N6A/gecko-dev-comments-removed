51a1895c64335a66156e1aabdfbcb1ad5ff5c2d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:56:42 +0000

    Backout bug 901597 for valgrind leaks
    UltraBlame original commit: d163502f0c6fbadce80bdd7791e8b25d0e07c186

--- xpcom/base/nsCycleCollector.cpp
+++ xpcom/base/nsCycleCollector.cpp
@@ -215,11 +215,6 @@ using namespace mozilla;
 
 
 
-
-
-
-
-
 MOZ_NEVER_INLINE void
 CC_AbortIfNull(void *ptr)
 {
@@ -235,27 +230,17 @@ struct nsCycleCollectorParams
     bool mLogAll;
     bool mLogShutdown;
     bool mAllTracesAtShutdown;
-    bool mLogThisThread;
 
     nsCycleCollectorParams() :
         mLogAll      (PR_GetEnv("MOZ_CC_LOG_ALL") != nullptr),
         mLogShutdown (PR_GetEnv("MOZ_CC_LOG_SHUTDOWN") != nullptr),
-        mAllTracesAtShutdown (PR_GetEnv("MOZ_CC_ALL_TRACES_AT_SHUTDOWN") != nullptr),
-        mLogThisThread(true)
+        mAllTracesAtShutdown (PR_GetEnv("MOZ_CC_ALL_TRACES_AT_SHUTDOWN") != nullptr)
     {
-        const char* logThreadEnv = PR_GetEnv("MOZ_CC_LOG_THREAD");
-        if (logThreadEnv && !!strcmp(logThreadEnv, "all")) {
-            if (NS_IsMainThread()) {
-                mLogThisThread = !strcmp(logThreadEnv, "main");
-            } else {
-                mLogThisThread = !strcmp(logThreadEnv, "worker");
-            }
-        }
     }
 
     bool LogThisCC(bool aIsShutdown)
     {
-        return (mLogAll || (aIsShutdown && mLogShutdown)) && mLogThisThread;
+        return mLogAll || (aIsShutdown && mLogShutdown);
     }
 };
 
