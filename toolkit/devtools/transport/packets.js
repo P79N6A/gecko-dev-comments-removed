61c9c1a35dee1509c6371cc8f23ef4812d1d98ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:15:11 +0000

    Bug 1012988 - Remove heritage dependency to packets.js. r=jimb
    UltraBlame original commit: 1023a50167c5957c5924f9881703fe36ce1c6fef

--- toolkit/devtools/transport/packets.js
+++ toolkit/devtools/transport/packets.js
@@ -25,7 +25,6 @@
 
 
 const { Cc, Ci, Cu } = require("chrome");
-const Heritage = require("sdk/core/heritage");
 const DevToolsUtils = require("devtools/toolkit/DevToolsUtils");
 const { dumpn, dumpv } = DevToolsUtils;
 const StreamUtils = require("devtools/toolkit/transport/stream-utils");
@@ -128,82 +127,84 @@ JSONPacket.fromHeader = function(header, transport) {
 
 JSONPacket.HEADER_PATTERN = /^(\d+):$/;
 
-JSONPacket.prototype = Heritage.extend(Packet.prototype, {
+JSONPacket.prototype = Object.create(Packet.prototype);
+
+Object.defineProperty(JSONPacket.prototype, "object", {
+  
+
+
+  get: function() { return this._object; },
 
   
 
 
-  get object() { return this._object; },
-
-  
-
-
-  set object(object) {
+  set: function(object) {
     this._object = object;
     let data = JSON.stringify(object);
     this._data = unicodeConverter.ConvertFromUnicode(data);
     this.length = this._data.length;
-  },
+  }
+});
 
-  read: function(stream, scriptableStream) {
-    dumpv("Reading JSON packet");
+JSONPacket.prototype.read = function(stream, scriptableStream) {
+  dumpv("Reading JSON packet");
 
+  
+  this._readData(stream, scriptableStream);
+
+  if (!this.done) {
     
-    this._readData(stream, scriptableStream);
-
-    if (!this.done) {
-      
-      return;
-    }
-
-    let json = this._data;
-    try {
-      json = unicodeConverter.ConvertToUnicode(json);
-      this._object = JSON.parse(json);
-    } catch(e) {
-      let msg = "Error parsing incoming packet: " + json + " (" + e +
-                " - " + e.stack + ")";
-      if (Cu.reportError) {
-        Cu.reportError(msg);
-      }
-      dumpn(msg);
-      return;
-    }
-
-    this._transport._onJSONObjectReady(this._object);
-  },
-
-  _readData: function(stream, scriptableStream) {
-    if (dumpv.wantVerbose) {
-      dumpv("Reading JSON data: _l: " + this.length + " dL: " +
-            this._data.length + " sA: " + stream.available());
-    }
-    let bytesToRead = Math.min(this.length - this._data.length,
-                               stream.available());
-    this._data += scriptableStream.readBytes(bytesToRead);
-    this._done = this._data.length === this.length;
-  },
-
-  write: function(stream) {
-    dumpv("Writing JSON packet");
-
-    if (this._outgoing === undefined) {
-      
-      this._outgoing = this.length + ":" + this._data;
-    }
-
-    let written = stream.write(this._outgoing, this._outgoing.length);
-    this._outgoing = this._outgoing.slice(written);
-    this._done = !this._outgoing.length;
-  },
-
-  get done() { return this._done; },
-
-  toString: function() {
-    return JSON.stringify(this._object, null, 2);
+    return;
   }
 
+  let json = this._data;
+  try {
+    json = unicodeConverter.ConvertToUnicode(json);
+    this._object = JSON.parse(json);
+  } catch(e) {
+    let msg = "Error parsing incoming packet: " + json + " (" + e +
+              " - " + e.stack + ")";
+    if (Cu.reportError) {
+      Cu.reportError(msg);
+    }
+    dumpn(msg);
+    return;
+  }
+
+  this._transport._onJSONObjectReady(this._object);
+}
+
+JSONPacket.prototype._readData = function(stream, scriptableStream) {
+  if (dumpv.wantVerbose) {
+    dumpv("Reading JSON data: _l: " + this.length + " dL: " +
+          this._data.length + " sA: " + stream.available());
+  }
+  let bytesToRead = Math.min(this.length - this._data.length,
+                             stream.available());
+  this._data += scriptableStream.readBytes(bytesToRead);
+  this._done = this._data.length === this.length;
+}
+
+JSONPacket.prototype.write = function(stream) {
+  dumpv("Writing JSON packet");
+
+  if (this._outgoing === undefined) {
+    
+    this._outgoing = this.length + ":" + this._data;
+  }
+
+  let written = stream.write(this._outgoing, this._outgoing.length);
+  this._outgoing = this._outgoing.slice(written);
+  this._done = !this._outgoing.length;
+}
+
+Object.defineProperty(JSONPacket.prototype, "done", {
+  get: function() { return this._done; }
 });
+
+JSONPacket.prototype.toString = function() {
+  return JSON.stringify(this._object, null, 2);
+}
 
 exports.JSONPacket = JSONPacket;
 
@@ -257,96 +258,99 @@ BulkPacket.fromHeader = function(header, transport) {
 
 BulkPacket.HEADER_PATTERN = /^bulk ([^: ]+) ([^: ]+) (\d+):$/;
 
-BulkPacket.prototype = Heritage.extend(Packet.prototype, {
+BulkPacket.prototype = Object.create(Packet.prototype);
 
-  read: function(stream) {
-    dumpv("Reading bulk packet, handing off input stream");
+BulkPacket.prototype.read = function(stream) {
+  dumpv("Reading bulk packet, handing off input stream");
 
+  
+  this._transport.pauseIncoming();
+
+  let deferred = promise.defer();
+
+  this._transport._onBulkReadReady({
+    actor: this.actor,
+    type: this.type,
+    length: this.length,
+    copyTo: (output) => {
+      dumpv("CT length: " + this.length);
+      deferred.resolve(StreamUtils.copyStream(stream, output, this.length));
+      return deferred.promise;
+    },
+    stream: stream,
+    done: deferred
+  });
+
+  
+  deferred.promise.then(() => {
+    dumpv("onReadDone called, ending bulk mode");
+    this._done = true;
+    this._transport.resumeIncoming();
+  }, this._transport.close);
+
+  
+  this.read = () => {
+    throw new Error("Tried to read() a BulkPacket's stream multiple times.");
+  };
+}
+
+BulkPacket.prototype.write = function(stream) {
+  dumpv("Writing bulk packet");
+
+  if (this._outgoingHeader === undefined) {
+    dumpv("Serializing bulk packet header");
     
-    this._transport.pauseIncoming();
+    this._outgoingHeader = "bulk " + this.actor + " " + this.type + " " +
+                           this.length + ":";
+  }
 
-    let deferred = promise.defer();
+  
+  if (this._outgoingHeader.length) {
+    dumpv("Writing bulk packet header");
+    let written = stream.write(this._outgoingHeader,
+                               this._outgoingHeader.length);
+    this._outgoingHeader = this._outgoingHeader.slice(written);
+    return;
+  }
 
-    this._transport._onBulkReadReady({
-      actor: this.actor,
-      type: this.type,
-      length: this.length,
-      copyTo: (output) => {
-        dumpv("CT length: " + this.length);
-        deferred.resolve(StreamUtils.copyStream(stream, output, this.length));
-        return deferred.promise;
-      },
-      stream: stream,
-      done: deferred
-    });
+  dumpv("Handing off output stream");
 
-    
-    deferred.promise.then(() => {
-      dumpv("onReadDone called, ending bulk mode");
-      this._done = true;
-      this._transport.resumeIncoming();
-    }, this._transport.close);
+  
+  this._transport.pauseOutgoing();
 
-    
-    this.read = () => {
-      throw new Error("Tried to read() a BulkPacket's stream multiple times.");
-    };
-  },
+  let deferred = promise.defer();
 
-  write: function(stream) {
-    dumpv("Writing bulk packet");
+  this._readyForWriting.resolve({
+    copyFrom: (input) => {
+      dumpv("CF length: " + this.length);
+      deferred.resolve(StreamUtils.copyStream(input, stream, this.length));
+      return deferred.promise;
+    },
+    stream: stream,
+    done: deferred
+  });
 
-    if (this._outgoingHeader === undefined) {
-      dumpv("Serializing bulk packet header");
-      
-      this._outgoingHeader = "bulk " + this.actor + " " + this.type + " " +
-                             this.length + ":";
-    }
+  
+  deferred.promise.then(() => {
+    dumpv("onWriteDone called, ending bulk mode");
+    this._done = true;
+    this._transport.resumeOutgoing();
+  }, this._transport.close);
 
-    
-    if (this._outgoingHeader.length) {
-      dumpv("Writing bulk packet header");
-      let written = stream.write(this._outgoingHeader,
-                                 this._outgoingHeader.length);
-      this._outgoingHeader = this._outgoingHeader.slice(written);
-      return;
-    }
+  
+  this.write = () => {
+    throw new Error("Tried to write() a BulkPacket's stream multiple times.");
+  };
+}
 
-    dumpv("Handing off output stream");
-
-    
-    this._transport.pauseOutgoing();
-
-    let deferred = promise.defer();
-
-    this._readyForWriting.resolve({
-      copyFrom: (input) => {
-        dumpv("CF length: " + this.length);
-        deferred.resolve(StreamUtils.copyStream(input, stream, this.length));
-        return deferred.promise;
-      },
-      stream: stream,
-      done: deferred
-    });
-
-    
-    deferred.promise.then(() => {
-      dumpv("onWriteDone called, ending bulk mode");
-      this._done = true;
-      this._transport.resumeOutgoing();
-    }, this._transport.close);
-
-    
-    this.write = () => {
-      throw new Error("Tried to write() a BulkPacket's stream multiple times.");
-    };
-  },
-
-  get streamReadyForWriting() {
+Object.defineProperty(BulkPacket.prototype, "streamReadyForWriting", {
+  get: function() {
     return this._readyForWriting.promise;
-  },
+  }
+});
 
-  get header() {
+Object.defineProperty(BulkPacket.prototype, "header", {
+  get: function() {
     return {
       actor: this.actor,
       type: this.type,
@@ -354,19 +358,21 @@ BulkPacket.prototype = Heritage.extend(Packet.prototype, {
     };
   },
 
-  set header(header) {
+  set: function(header) {
     this.actor = header.actor;
     this.type = header.type;
     this.length = header.length;
   },
-
-  get done() { return this._done; },
-
-  toString: function() {
-    return "Bulk: " + JSON.stringify(this.header, null, 2);
-  }
-
 });
+
+Object.defineProperty(BulkPacket.prototype, "done", {
+  get: function() { return this._done; },
+});
+
+
+BulkPacket.prototype.toString = function() {
+  return "Bulk: " + JSON.stringify(this.header, null, 2);
+}
 
 exports.BulkPacket = BulkPacket;
 
@@ -385,21 +391,21 @@ function RawPacket(transport, data) {
   this._done = false;
 }
 
-RawPacket.prototype = Heritage.extend(Packet.prototype, {
+RawPacket.prototype = Object.create(Packet.prototype);
 
-  read: function(stream) {
-    
-    throw Error("Not implmented.");
-  },
+RawPacket.prototype.read = function(stream) {
+  
+  throw Error("Not implmented.");
+}
 
-  write: function(stream) {
-    let written = stream.write(this._data, this._data.length);
-    this._data = this._data.slice(written);
-    this._done = !this._data.length;
-  },
+RawPacket.prototype.write = function(stream) {
+  let written = stream.write(this._data, this._data.length);
+  this._data = this._data.slice(written);
+  this._done = !this._data.length;
+}
 
-  get done() { return this._done; }
-
+Object.defineProperty(RawPacket.prototype, "done", {
+  get: function() { return this._done; }
 });
 
 exports.RawPacket = RawPacket;
