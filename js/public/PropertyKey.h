c6da2a9c20f2494961cab730f3ccd0302bbb0571
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:52:00 +0000

    Bug 837773 - Add a PropertyKey.h header.  r=jorendorff
    UltraBlame original commit: 43a5c9890c22e54152c997f8800956b517819a3a

---
+++ js/public/PropertyKey.h
@@ -0,0 +1,99 @@
+
+
+
+
+
+
+
+
+
+#ifndef js_PropertyKey_h___
+#define js_PropertyKey_h___
+
+#include "mozilla/Attributes.h"
+
+#include "js/Value.h"
+
+struct JSContext;
+
+namespace JS {
+
+class PropertyKey;
+
+namespace detail {
+
+extern JS_PUBLIC_API(bool)
+ToPropertyKeySlow(JSContext *cx, HandleValue v, PropertyKey *key);
+
+} 
+
+
+
+
+
+
+
+
+
+class PropertyKey
+{
+    Value v;
+    friend bool detail::ToPropertyKeySlow(JSContext *cx, HandleValue v, PropertyKey *key);
+
+  public:
+    explicit PropertyKey(uint32_t index) : v(PrivateUint32Value(index)) {}
+
+    
+
+
+
+
+    bool isIndex(uint32_t *index) {
+        
+        
+        
+        if (!v.isInt32())
+            return false;
+        *index = v.toPrivateUint32();
+        return true;
+    }
+
+    
+
+
+
+
+    bool isName(JSString **str) {
+        uint32_t dummy;
+        if (isIndex(&dummy))
+            return false;
+        *str = v.toString();
+        return true;
+    }
+
+    
+
+
+
+
+
+
+    bool isSymbol() {
+        return false;
+    }
+};
+
+inline bool
+ToPropertyKey(JSContext *cx, HandleValue v, PropertyKey *key)
+{
+    if (v.isInt32() && v.toInt32() >= 0) {
+        *key = PropertyKey(uint32_t(v.toInt32()));
+        return true;
+    }
+
+    return detail::ToPropertyKeySlow(cx, v, key);
+}
+
+} 
+
+#endif 
---
+++ js/src/vm/PropertyKey.cpp
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+#include "mozilla/Assertions.h"
+
+#include "gc/Root.h"
+#include "js/PropertyKey.h"
+#include "js/Value.h"
+#include "vm/String.h"
+
+using namespace js;
+
+bool
+JS::detail::ToPropertyKeySlow(JSContext *cx, HandleValue v, PropertyKey *key)
+{
+    MOZ_ASSERT_IF(v.isInt32(), v.toInt32() < 0);
+
+    RootedAtom atom(cx);
+    if (!ToAtom<CanGC>(cx, v))
+        return false;
+
+    uint32_t index;
+    if (atom->isIndex(&index)) {
+        *key = PropertyKey(index);
+        return true;
+    }
+
+    key->v.setString(atom->asPropertyName());
+    return true;
+}
