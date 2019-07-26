3e4eeb83c69b84dd94ebaa1c4fdf36893cca4749
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:08:23 +0000

    Bug 938571 - Add discovery support for second-screen devices (test) r=rnewman
    UltraBlame original commit: b6a3eb865983d77ecfd658f2695fb28ac0f13896

---
+++ mobile/android/base/tests/testSimpleDiscovery.js
@@ -0,0 +1,46 @@
+
+
+
+
+"use strict";
+
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/SimpleServiceDiscovery.jsm");
+
+function discovery_observer(subject, topic, data) {
+  do_print("Observer: " + data);
+
+  let service = SimpleServiceDiscovery.findServiceForLocation(data);
+  if (!service)
+    return;
+
+  do_check_eq(service.friendlyName, "Pretend Device");
+  do_check_eq(service.uuid, "uuid:5ec9ff92-e8b2-4a94-a72c-76b34e6dabb1");
+  do_check_eq(service.manufacturer, "Copy Cat Inc.");
+  do_check_eq(service.modelName, "Eureka Dongle");
+
+  run_next_test();
+};
+
+add_test(function test_default() {
+  do_register_cleanup(function cleanup() {
+    Services.obs.removeObserver(discovery_observer, "ssdp-service-found");
+  });
+
+  Services.obs.addObserver(discovery_observer, "ssdp-service-found", false);
+
+  
+  let service = {
+    location: "http://mochi.test:8888/tests/robocop/simpleservice.xml",
+    target: "test:service"
+  };
+
+  do_print("Force a detailed ping from a pretend service");
+
+  
+  SimpleServiceDiscovery._found(service);
+});
+
+run_next_test();
