606ad11faad39bbfb17a7c97edbdddd2afe34eeb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:19:29 +0000

    Bug 801487 - Implement ErrorResult::ThrowTypeError. r=bz
    UltraBlame original commit: 71c0fa0964c3a7a667357bb82d4a7d3f9986f807

--- dom/bindings/ErrorResult.h
+++ dom/bindings/ErrorResult.h
@@ -11,21 +11,47 @@
 #ifndef mozilla_ErrorResult_h
 #define mozilla_ErrorResult_h
 
+#include <stdarg.h>
+
+#include "jsapi.h"
 #include "nscore.h"
 #include "mozilla/Assertions.h"
 
 namespace mozilla {
+
+namespace dom {
+
+enum ErrNum {
+#define MSG_DEF(_name, _argc, _str) \
+  _name,
+#include "mozilla/dom/Errors.msg"
+#undef MSG_DEF
+  Err_Limit
+};
+
+} 
 
 class ErrorResult {
 public:
   ErrorResult() {
     mResult = NS_OK;
   }
+#ifdef DEBUG
+  ~ErrorResult() {
+    MOZ_ASSERT_IF(IsTypeError(), !mMessage);
+  }
+#endif
 
   void Throw(nsresult rv) {
     MOZ_ASSERT(NS_FAILED(rv), "Please don't try throwing success");
+    MOZ_ASSERT(rv != NS_ERROR_TYPE_ERR, "Use ThrowTypeError()");
+    MOZ_ASSERT(!IsTypeError(), "Don't overwite TypeError");
     mResult = rv;
   }
+
+  void ThrowTypeError(const dom::ErrNum errorNumber, ...);
+  void ReportTypeError(JSContext* cx);
+  bool IsTypeError() const { return ErrorCode() == NS_ERROR_TYPE_ERR; }
 
   
   
@@ -35,6 +61,8 @@ public:
   
   
   void operator=(nsresult rv) {
+    MOZ_ASSERT(rv != NS_ERROR_TYPE_ERR, "Use ThrowTypeError()");
+    MOZ_ASSERT(!IsTypeError(), "Don't overwite TypeError");
     mResult = rv;
   }
 
@@ -48,6 +76,9 @@ public:
 
 private:
   nsresult mResult;
+  struct Message;
+  
+  Message* mMessage;
 
   
   
