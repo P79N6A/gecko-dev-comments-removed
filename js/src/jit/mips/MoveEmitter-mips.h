081e66f5faffb55611aacf4947cbcd99ae826686
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:13:04 +0000

    Bug 969375 - MIPS port: Added MoveEmitter-mips files. r=nbp
    UltraBlame original commit: 05532668570bf5e1205a54136998bd31963ad321

---
+++ js/src/jit/mips/MoveEmitter-mips.h
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+#ifndef jit_mips_MoveEmitter_mips_h
+#define jit_mips_MoveEmitter_mips_h
+
+#include "jit/IonMacroAssembler.h"
+#include "jit/MoveResolver.h"
+
+namespace js {
+namespace jit {
+
+class CodeGenerator;
+
+class MoveEmitterMIPS
+{
+    bool inCycle_;
+    MacroAssemblerMIPSCompat &masm;
+
+    
+    uint32_t pushedAtStart_;
+
+    
+    
+    
+    int32_t pushedAtCycle_;
+    int32_t pushedAtSpill_;
+
+    
+    
+    
+    Register spilledReg_;
+    FloatRegister spilledFloatReg_;
+
+    void assertDone();
+    Register tempReg();
+    FloatRegister tempFloatReg();
+    Address cycleSlot() const;
+    int32_t getAdjustedOffset(const MoveOperand &operand);
+    Address getAdjustedAddress(const MoveOperand &operand);
+
+    void emitMove(const MoveOperand &from, const MoveOperand &to);
+    void emitFloat32Move(const MoveOperand &from, const MoveOperand &to);
+    void emitDoubleMove(const MoveOperand &from, const MoveOperand &to);
+    void breakCycle(const MoveOperand &from, const MoveOperand &to, MoveOp::Type type);
+    void completeCycle(const MoveOperand &from, const MoveOperand &to, MoveOp::Type type);
+    void emit(const MoveOp &move);
+
+  public:
+    MoveEmitterMIPS(MacroAssemblerMIPSCompat &masm);
+    ~MoveEmitterMIPS();
+    void emit(const MoveResolver &moves);
+    void finish();
+};
+
+typedef MoveEmitterMIPS MoveEmitter;
+
+} 
+} 
+
+#endif 
