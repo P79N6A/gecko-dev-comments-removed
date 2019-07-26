9fe5c0457571399a329a08f41e749a8a50012869
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:00:33 +0000

    Bug 945973 (part 3) - Fix some trivial reporter/report confusions in aboutMemory.js.  r=mccr8.
    UltraBlame original commit: 0a276a083a26ee59a019ac0a61c8b5394aee9adc

--- toolkit/components/aboutmemory/content/aboutMemory.js
+++ toolkit/components/aboutmemory/content/aboutMemory.js
@@ -1101,7 +1101,7 @@ function addHeapUnclassifiedNode(aT, aHeapAllocatedNode, aHeapTotal)
   let heapUnclassifiedT = new TreeNode("heap-unclassified", UNITS_BYTES);
   heapUnclassifiedT._amount = heapAllocatedBytes - aHeapTotal;
   heapUnclassifiedT._description =
-      "Memory not classified by a more specific reporter. This includes " +
+      "Memory not classified by a more specific report. This includes " +
       "slop bytes due to internal fragmentation in the heap allocator " +
       "(caused when the allocator rounds up request sizes).";
   aT._kids.push(heapUnclassifiedT);
@@ -1527,7 +1527,7 @@ function appendMrNameSpan(aP, aDescription, aUnsafeName, aIsInvalid, aNMerged,
     let noteSpan = appendElementWithText(aP, "span", "mrNote", noteText);
     noteSpan.title =
       "This value is the sum of " + aNMerged +
-      " memory reporters that all have the same path.";
+      " memory reports that all have the same path.";
   }
 
   if (aPresence) {
