306bf069c1caf77b98bc93bf974649945f30aabb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:05:58 +0000

    Bug 979454 - manifest parser needs to support skip-if in the [default] section and || that with the skip-if from the [test] section. r=ted
    UltraBlame original commit: 7373f3e5d57af35ed6ae51fc09c872f54692475a

---
+++ testing/mozbase/manifestdestiny/tests/test_default_skipif.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+import os
+import unittest
+from manifestparser import ManifestParser
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+class TestDefaultSkipif(unittest.TestCase):
+    """test applying a skip-if condition in [DEFAULT] and || with the value for the test"""
+
+
+    def test_defaults(self):
+
+        default = os.path.join(here, 'default-skipif.ini')
+        parser = ManifestParser(manifests=(default,))
+        for test in parser.tests:
+            if test['name'] == 'test1':
+                self.assertEqual(test['skip-if'], "(os == 'win' && debug ) || (debug)")
+            elif test['name'] == 'test2':
+                self.assertEqual(test['skip-if'], "(os == 'win' && debug ) || (os == 'linux')")
+            elif test['name'] == 'test3':
+                self.assertEqual(test['skip-if'], "(os == 'win' && debug ) || (os == 'win')")
+            elif test['name'] == 'test4':
+                self.assertEqual(test['skip-if'], "(os == 'win' && debug ) || (os == 'win' && debug)")
+            elif test['name'] == 'test5':
+                self.assertEqual(test['skip-if'], "os == 'win' && debug # a pesky comment")
+            elif test['name'] == 'test6':
+                self.assertEqual(test['skip-if'], "(os == 'win' && debug ) || (debug )")
+
+if __name__ == '__main__':
+    unittest.main()
