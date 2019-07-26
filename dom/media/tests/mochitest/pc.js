ea3179b2433f7e8fe05fe63c1cb4054c2a387ce8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:50:00 +0000

    bug 837028 - Add logging statements for basic peer connection tests. r=jesup
    UltraBlame original commit: 4444d9e3af9a24af8f4081eacc3148756c76a802

--- dom/media/tests/mochitest/pc.js
+++ dom/media/tests/mochitest/pc.js
@@ -18,26 +18,31 @@ var PeerConnection = {
 
     function onCreateOfferSuccess(aOffer) {
       pc1_offer = aOffer;
+      info("Calling setLocalDescription on local peer connection");
       aPCLocal.setLocalDescription(aOffer, onSetLocalDescriptionSuccess1,
                                    unexpectedCallbackAndFinish);
     }
 
     function onSetLocalDescriptionSuccess1() {
+      info("Calling setRemoteDescription on remote peer connection");
       aPCRemote.setRemoteDescription(pc1_offer, onSetRemoteDescriptionSuccess1,
                                      unexpectedCallbackAndFinish);
     }
 
     function onSetRemoteDescriptionSuccess1() {
+      info("Calling createAnswer on remote peer connection");
       aPCRemote.createAnswer(onCreateAnswerSuccess, unexpectedCallbackAndFinish);
     }
 
     function onCreateAnswerSuccess(aAnswer) {
       pc2_answer = aAnswer;
+      info("Calling setLocalDescription on remote peer connection");
       aPCRemote.setLocalDescription(aAnswer, onSetLocalDescriptionSuccess2,
                                     unexpectedCallbackAndFinish);
     }
 
     function onSetLocalDescriptionSuccess2() {
+      info("Calling setRemoteDescription on local peer connection");
       aPCLocal.setRemoteDescription(pc2_answer, onSetRemoteDescriptionSuccess2,
                                     unexpectedCallbackAndFinish);
     }
@@ -46,6 +51,7 @@ var PeerConnection = {
       aSuccessCallback();
     }
 
+    info("Calling createOffer on local peer connection");
     aPCLocal.createOffer(onCreateOfferSuccess, unexpectedCallbackAndFinish);
   },
 
