44fee08bd730f7e1a8acaecec4d8c4cf72f822af
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:50:16 +0000

    Bug 835214 - Avoid re-stripping executables already stripped. r=ted
    Also rework UnifiedExecutableFile so that it leaves stripping to ExecutableFile.
    
    UltraBlame original commit: ba515e203813b34f6feaaa4ad0d0a971f828e7f5

--- python/mozbuild/mozpack/files.py
+++ python/mozbuild/mozpack/files.py
@@ -142,7 +142,12 @@ class ExecutableFile(File):
     '''
     def copy(self, dest, skip_if_older=True):
         assert isinstance(dest, basestring)
-        File.copy(self, dest, skip_if_older)
+        
+        
+        
+        if not File.copy(self, dest, skip_if_older) and \
+                os.path.getsize(self.path) > os.path.getsize(dest):
+            return False
         try:
             if may_strip(dest):
                 strip(dest)
