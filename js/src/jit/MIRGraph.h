11f7c21b62b83e3cf394537c3f057d1ef2f2f90a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:55:56 +0000

    Bug 943442 - IonMonkey: Step the instruction id by 1 instead of 2, to facilitate using it as a table index. r=dvander
    UltraBlame original commit: 15768548e7e37bc8081ad9d6b0cd1e19254b2610

--- js/src/jit/MIRGraph.h
+++ js/src/jit/MIRGraph.h
@@ -550,7 +550,7 @@ class MIRGraph
       : alloc_(alloc),
         returnAccumulator_(nullptr),
         blockIdGen_(0),
-        idGen_(0),
+        idGen_(1),
         osrBlock_(nullptr),
         osrStart_(nullptr),
         numBlocks_(0),
@@ -595,7 +595,9 @@ class MIRGraph
         numBlocks_ = 0;
     }
     void resetInstructionNumber() {
-        idGen_ = 0;
+        
+        
+        idGen_ = 1;
     }
     MBasicBlockIterator begin() {
         return blocks_.begin();
@@ -635,12 +637,9 @@ class MIRGraph
         return blockIdGen_;
     }
     void allocDefinitionId(MDefinition *ins) {
-        
-        
-        idGen_ += 2;
-        ins->setId(idGen_);
+        ins->setId(idGen_++);
     }
-    uint32_t getMaxInstructionId() {
+    uint32_t getNumInstructionIds() {
         return idGen_;
     }
     MResumePoint *entryResumePoint() {
