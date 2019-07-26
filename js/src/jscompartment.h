90ca8db14a8c1bd3a13b19ce551e87c7895415c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:12:44 +0000

    Bug 957075 - Add missing parentheses, silence an MSVC warning. r=shu
    UltraBlame original commit: 8c26da1b05ba37534454f466d184378cea8bc9f8

--- js/src/jscompartment.h
+++ js/src/jscompartment.h
@@ -502,7 +502,7 @@ class js::AutoDebugModeInvalidation
         
         
         MOZ_ASSERT_IF(needInvalidation_ != NoNeed,
-                      needInvalidation_ == debugMode ? ToggledOn : ToggledOff);
+                      needInvalidation_ == (debugMode ? ToggledOn : ToggledOff));
         needInvalidation_ = debugMode ? ToggledOn : ToggledOff;
     }
 };
