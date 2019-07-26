5537b4a282859b3cb9fdbe03437186ee0e191620
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:15:20 +0000

    Bug 409624 - Sanitizer should clear findBar data. r=adw
    UltraBlame original commit: 4f3a1bcdc429425a6edbd1f680ce6fa17ff67c4e

---
+++ browser/base/content/test/browser_bug409624.js
@@ -0,0 +1,41 @@
+
+
+
+
+function test() {
+  waitForExplicitFinish();
+
+  let prefService = Cc["@mozilla.org/preferences-service;1"]
+                    .getService(Components.interfaces.nsIPrefBranch2);
+
+  let findBar = gFindBar;
+  let textbox = gFindBar.getElement("findbar-textbox");
+
+  let tempScope = {};
+  Cc["@mozilla.org/moz/jssubscript-loader;1"].getService(Ci.mozIJSSubScriptLoader)
+                                             .loadSubScript("chrome://browser/content/sanitize.js", tempScope);
+  let Sanitizer = tempScope.Sanitizer;
+  let s = new Sanitizer();
+  s.prefDomain = "privacy.cpd.";
+  let prefBranch = prefService.getBranch(s.prefDomain);
+
+  prefBranch.setBoolPref("cache", false);
+  prefBranch.setBoolPref("cookies", false);
+  prefBranch.setBoolPref("downloads", false);
+  prefBranch.setBoolPref("formdata", true);
+  prefBranch.setBoolPref("history", false);
+  prefBranch.setBoolPref("offlineApps", false);
+  prefBranch.setBoolPref("passwords", false);
+  prefBranch.setBoolPref("sessions", false);
+  prefBranch.setBoolPref("siteSettings", false);
+
+  
+  s.sanitize();
+  ok(!s.canClearItem("formdata"), "pre-test baseline for sanitizer");
+  textbox.value = "m";
+  ok(s.canClearItem("formdata"), "formdata can be cleared after input");
+  s.sanitize();
+  is(textbox.value, "", "findBar textbox should be empty after sanitize");
+  ok(!s.canClearItem("formdata"), "canClear now false after sanitize");
+  finish();
+}
