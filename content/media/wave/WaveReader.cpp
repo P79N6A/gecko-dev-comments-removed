7f5526ac7d3580934e8f2fd307980e23fac3bb29
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:00:20 +0000

    Bug 1010163 - Cast out warning demons on a CLOSED TREE.
    UltraBlame original commit: 72230d707f866b451d4a784514fe70c0f655a3c0

--- content/media/wave/WaveReader.cpp
+++ content/media/wave/WaveReader.cpp
@@ -577,7 +577,7 @@ WaveReader::LoadListChunk(uint32_t aChunkSize,
     uint32_t length = ReadUint32LE(&p);
 
     
-    if (end - p < length) {
+    if (uint32_t(end - p) < length) {
       break;
     }
 
