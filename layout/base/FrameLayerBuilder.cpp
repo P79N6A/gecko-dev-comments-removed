ff19d41b70b596c533807bf78b13e81c60a4e5db
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:19:42 +0000

    Bug 856807. Add apparently-pointless call to SetClipRect(nullptr) to avoid Android regression. r=mattwoodrow
    UltraBlame original commit: 4a55715677d75f1086a82520519b96ecbde27a01

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -1299,7 +1299,6 @@ ContainerState::CreateOrRecycleThebesLayer(const nsIFrame* aActiveScrolledRoot,
     layer = mRecycledThebesLayers[mNextFreeRecycledThebesLayer];
     ++mNextFreeRecycledThebesLayer;
     
-    
     layer->SetMaskLayer(nullptr);
 
     data = static_cast<ThebesDisplayItemLayerUserData*>
@@ -1541,14 +1540,15 @@ ContainerState::PopThebesLayerData()
   ThebesLayerData* data = mThebesLayerDataStack[lastIndex];
 
   nsRefPtr<Layer> layer;
+  ThebesLayer* thebesLayer = data->mLayer;
   nsRefPtr<ImageContainer> imageContainer = data->CanOptimizeImageLayer(mBuilder);
 
   if ((data->mIsSolidColorInVisibleRegion || imageContainer) &&
-      data->mLayer->GetValidRegion().IsEmpty()) {
+      thebesLayer->GetValidRegion().IsEmpty()) {
     NS_ASSERTION(!(data->mIsSolidColorInVisibleRegion && imageContainer),
                  "Can't be a solid color as well as an image!");
     if (imageContainer) {
-      nsRefPtr<ImageLayer> imageLayer = CreateOrRecycleImageLayer(data->mLayer);
+      nsRefPtr<ImageLayer> imageLayer = CreateOrRecycleImageLayer(thebesLayer);
       imageLayer->SetContainer(imageContainer);
       data->mImage->ConfigureLayer(imageLayer, mParameters.mOffset);
       imageLayer->SetPostScale(mParameters.mXScale,
@@ -1564,13 +1564,13 @@ ContainerState::PopThebesLayerData()
       mLayerBuilder->StoreOptimizedLayerForFrame(data->mImage,
                                                  imageLayer);
     } else {
-      nsRefPtr<ColorLayer> colorLayer = CreateOrRecycleColorLayer(data->mLayer);
-      colorLayer->SetIsFixedPosition(data->mLayer->GetIsFixedPosition());
+      nsRefPtr<ColorLayer> colorLayer = CreateOrRecycleColorLayer(thebesLayer);
+      colorLayer->SetIsFixedPosition(thebesLayer->GetIsFixedPosition());
       colorLayer->SetColor(data->mSolidColor);
 
       
-      colorLayer->SetBaseTransform(data->mLayer->GetBaseTransform());
-      colorLayer->SetPostScale(data->mLayer->GetPostXScale(), data->mLayer->GetPostYScale());
+      colorLayer->SetBaseTransform(thebesLayer->GetBaseTransform());
+      colorLayer->SetPostScale(thebesLayer->GetPostXScale(), thebesLayer->GetPostYScale());
 
       
       
@@ -1592,13 +1592,16 @@ ContainerState::PopThebesLayerData()
 
     
     
+    
+    
+    thebesLayer->SetClipRect(nullptr);
     nsIntRect emptyRect;
-    data->mLayer->SetClipRect(&emptyRect);
-    data->mLayer->SetVisibleRegion(nsIntRegion());
+    thebesLayer->SetClipRect(&emptyRect);
+    thebesLayer->SetVisibleRegion(nsIntRegion());
   } else {
-    layer = data->mLayer;
+    layer = thebesLayer;
+    thebesLayer->SetClipRect(nullptr);
     imageContainer = nullptr;
-    layer->SetClipRect(nullptr);
   }
 
   gfxMatrix transform;
