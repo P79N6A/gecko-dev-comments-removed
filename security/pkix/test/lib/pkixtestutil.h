a74816143f89d385e94ab35dece5d0bed3a425d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:52:32 +0000

    Bug 1005198: Make it easy to create test certificates in GTest tests, r=keeler
    UltraBlame original commit: 2a5624d51bd3b079e6e618e810b45556a2783f53

--- security/pkix/test/lib/pkixtestutil.h
+++ security/pkix/test/lib/pkixtestutil.h
@@ -18,11 +18,85 @@
 #ifndef mozilla_pkix_test__pkixtestutils_h
 #define mozilla_pkix_test__pkixtestutils_h
 
-#include "pkix/ScopedPtr.h"
+#include <stdint.h>
+#include <stdio.h>
+
+#include "pkix/enumclass.h"
 #include "pkix/pkixtypes.h"
+#include "pkix/ScopedPtr.h"
 #include "seccomon.h"
 
 namespace mozilla { namespace pkix { namespace test {
+
+namespace {
+
+inline void
+fclose_void(FILE* file) {
+  (void) fclose(file);
+}
+
+inline void
+SECITEM_FreeItem_true(SECItem* item)
+{
+  SECITEM_FreeItem(item, true);
+}
+
+} 
+
+typedef mozilla::pkix::ScopedPtr<FILE, fclose_void> ScopedFILE;
+typedef mozilla::pkix::ScopedPtr<SECItem, SECITEM_FreeItem_true> ScopedSECItem;
+typedef mozilla::pkix::ScopedPtr<SECKEYPrivateKey, SECKEY_DestroyPrivateKey>
+  ScopedSECKEYPrivateKey;
+
+FILE* OpenFile(const char* dir, const char* filename, const char* mode);
+
+extern const PRTime ONE_DAY;
+
+SECStatus GenerateKeyPair( ScopedSECKEYPublicKey& publicKey,
+                           ScopedSECKEYPrivateKey& privateKey);
+
+
+
+
+enum Version { v1 = 0, v2 = 1, v3 = 2 };
+
+
+
+
+
+
+
+
+
+
+
+SECItem* CreateEncodedCertificate(PLArenaPool* arena, long version,
+                                  SECOidTag signature, long serialNumber,
+                                  const char* issuerASCII, PRTime notBefore,
+                                  PRTime notAfter, const char* subjectASCII,
+                      SECItem const* const* extensions,
+                      SECKEYPrivateKey* issuerPrivateKey,
+                                  SECOidTag signatureHashAlg,
+                           ScopedSECKEYPrivateKey& privateKey);
+
+MOZILLA_PKIX_ENUM_CLASS ExtensionCriticality { NotCritical = 0, Critical = 1 };
+
+
+SECItem* CreateEncodedBasicConstraints(PLArenaPool* arena, bool isCA,
+                                       long pathLenConstraint,
+                                       ExtensionCriticality criticality);
+
+
+
+
+
+
+SECItem* CreateEncodedEKUExtension(PLArenaPool* arena,
+                                   const SECOidTag* ekus, size_t ekusCount,
+                                   ExtensionCriticality criticality);
+
+
+
 
 class OCSPResponseExtension
 {
@@ -41,35 +115,62 @@ public:
   PLArenaPool* arena;
   
   pkix::ScopedCERTCertificate cert; 
-  pkix::ScopedCERTCertificate issuerCert; 
-  pkix::ScopedCERTCertificate signerCert; 
+
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  enum OCSPResponseStatus {
+    successful = 0,
+    malformedRequest = 1,
+    internalError = 2,
+    tryLater = 3,
+    
+    sigRequired = 5,
+    unauthorized = 6,
+  };
   uint8_t responseStatus; 
   bool skipResponseBytes; 
 
-  static const uint32_t MaxIncludedCertificates = 4;
-  pkix::ScopedCERTCertificate includedCertificates[MaxIncludedCertificates];
+  
+  const SECItem* issuerNameDER; 
+  const CERTSubjectPublicKeyInfo* issuerSPKI; 
+  const SECItem* signerNameDER; 
+                                
+                                
 
-  
-  
   PRTime producedAt;
-  PRTime thisUpdate;
-  PRTime nextUpdate;
-  bool includeNextUpdate;
-  SECOidTag certIDHashAlg;
-  uint8_t certStatus;     
-  PRTime revocationTime; 
-  bool badSignature; 
-
-  enum ResponderIDType {
-    ByName = 1,
-    ByKeyHash = 2
-  };
-  ResponderIDType responderIDType;
 
   OCSPResponseExtension* extensions;
   bool includeEmptyExtensions; 
                                
                                
+  ScopedSECKEYPrivateKey signerPrivateKey;
+  bool badSignature; 
+  SECItem const* const* certs; 
+
+  
+  
+  SECOidTag certIDHashAlg;
+  enum CertStatus {
+    good = 0,
+    revoked = 1,
+    unknown = 2,
+  };
+  uint8_t certStatus; 
+  PRTime revocationTime; 
+  PRTime thisUpdate;
+  PRTime nextUpdate;
+  bool includeNextUpdate;
 };
 
 
@@ -81,4 +182,4 @@ SECItem* CreateEncodedOCSPResponse(OCSPResponseContext& context);
 
 } } } 
 
-#endif 
+#endif
