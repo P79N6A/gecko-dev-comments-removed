71b93bb005d2e40af32fd950d4f07d6240e223fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:45:57 +0000

    Bug 872907 - Patch 1: Implement BluetoothA2dpManager, r=echou, r=mrbkap
    UltraBlame original commit: 72653e055a56da634250db2c5a5cb2bfb97359ed

---
+++ dom/bluetooth/BluetoothA2dpManager.h
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_bluetooth_bluetootha2dpmanager_h__
+#define mozilla_dom_bluetooth_bluetootha2dpmanager_h__
+
+#include "BluetoothCommon.h"
+
+BEGIN_BLUETOOTH_NAMESPACE
+
+enum SinkState {
+  SINK_DISCONNECTED = 1,
+  SINK_CONNECTING,
+  SINK_CONNECTED,
+  SINK_PLAYING,
+  SINK_DISCONNECTING
+};
+
+class BluetoothA2dpManagerObserver;
+class BluetoothValue;
+class BluetoothSocket;
+
+class BluetoothA2dpManager
+{
+public:
+  static BluetoothA2dpManager* Get();
+  ~BluetoothA2dpManager();
+
+  bool Connect(const nsAString& aDeviceAddress);
+  void Disconnect();
+  void HandleSinkPropertyChanged(const BluetoothSignal& aSignal);
+  nsresult HandleShutdown();
+
+private:
+  BluetoothA2dpManager();
+  bool Init();
+  void Cleanup();
+
+  bool mConnected;
+  bool mPlaying;
+  nsString mDeviceAddress;
+  SinkState mSinkState;
+};
+
+END_BLUETOOTH_NAMESPACE
+
+#endif
