bd5bb6d5b1b28fbe88db5a22ca90105bdc9ef9cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:26:17 +0000

    Bug 860493 - Enable history swipe animations by default on nightly and aurora (implemented in bug 678392). r=smichaud
    UltraBlame original commit: 09725c69fd76ad2a21edf3bb02264c89fd2adc18

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -351,8 +351,18 @@ pref("browser.download.debug", false);
 pref("browser.download.saveLinkAsFilenameTimeout", 4000);
 
 pref("browser.download.useDownloadDir", true);
+
 pref("browser.download.folderList", 1);
+pref("browser.download.manager.showAlertOnComplete", true);
+pref("browser.download.manager.showAlertInterval", 2000);
+pref("browser.download.manager.retention", 2);
+pref("browser.download.manager.showWhenStarting", true);
+pref("browser.download.manager.closeWhenDone", false);
+pref("browser.download.manager.focusWhenStarting", false);
+pref("browser.download.manager.flashCount", 2);
 pref("browser.download.manager.addToRecentDocs", true);
+pref("browser.download.manager.quitBehavior", 0);
+pref("browser.download.manager.scanWhenDone", true);
 pref("browser.download.manager.resumeOnWakeDelay", 10000);
 
 
@@ -361,6 +371,10 @@ pref("browser.download.animateNotifications", true);
 
 
 pref("browser.download.panel.shown", false);
+
+
+
+pref("browser.download.panel.firstSessionCompleted", false);
 
 #ifndef XP_MACOSX
 pref("browser.helperApps.deleteTempFileOnExit", true);
@@ -574,7 +588,11 @@ pref("browser.gesture.twist.left", "cmd_gestureRotateLeft");
 pref("browser.gesture.twist.end", "cmd_gestureRotateEnd");
 pref("browser.gesture.tap", "cmd_fullZoomReset");
 
+#ifndef RELEASE_BUILD
+pref("browser.snapshots.limit", 5);
+#else
 pref("browser.snapshots.limit", 0);
+#endif
 
 
 
@@ -1005,6 +1023,10 @@ pref("services.sync.prefs.sync.addons.ignoreUserEnabledChanges", true);
 
 
 pref("services.sync.prefs.sync.app.update.mode", true);
+pref("services.sync.prefs.sync.browser.download.manager.closeWhenDone", true);
+pref("services.sync.prefs.sync.browser.download.manager.retention", true);
+pref("services.sync.prefs.sync.browser.download.manager.scanWhenDone", true);
+pref("services.sync.prefs.sync.browser.download.manager.showWhenStarting", true);
 pref("services.sync.prefs.sync.browser.formfill.enable", true);
 pref("services.sync.prefs.sync.browser.link.open_newwindow", true);
 pref("services.sync.prefs.sync.browser.offline-apps.notify", true);
