5353a82df84bf3e2135d1890b18e7dc0062b7ef8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:34:16 +0000

    Bug 755593 - Remove tabs-on-bottom mode. r=dao
    UltraBlame original commit: c197150217d6f72466879135c23eaf71fa22bafe

--- browser/base/content/browser-fullScreen.js
+++ browser/base/content/browser-fullScreen.js
@@ -571,11 +571,9 @@ var FullScreen = {
       document.documentElement.setAttribute("inFullscreen", true);
     }
 
-    
-    
     var fullscreenctls = document.getElementById("window-controls");
     var navbar = document.getElementById("nav-bar");
-    var ctlsOnTabbar = window.toolbar.visible && (navbar.collapsed || TabsOnTop.enabled);
+    var ctlsOnTabbar = window.toolbar.visible;
     if (fullscreenctls.parentNode == navbar && ctlsOnTabbar) {
       fullscreenctls.removeAttribute("flex");
       document.getElementById("TabsToolbar").appendChild(fullscreenctls);
