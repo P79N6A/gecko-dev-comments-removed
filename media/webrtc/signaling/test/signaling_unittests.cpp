0721a52119c9e0134aadcedc3b4b1001e6935f2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:05:27 +0000

    Backed out changeset 893cf3614356 (bug 837919) for crashtest & mochitest orange.
    UltraBlame original commit: 084e5f07208bd9482ed626330106cd96fc0cf40e

--- media/webrtc/signaling/test/signaling_unittests.cpp
+++ media/webrtc/signaling/test/signaling_unittests.cpp
@@ -27,10 +27,6 @@ using namespace std;
 #include "runnable_utils.h"
 #include "nsStaticComponents.h"
 #include "nsIDOMRTCPeerConnection.h"
-#include "nsServiceManagerUtils.h"
-#include "nsNetUtil.h"
-#include "nsIIOService.h"
-#include "nsIDNSService.h"
 #include "nsWeakReference.h"
 #include "nricectx.h"
 
@@ -1143,6 +1139,7 @@ public:
   SignalingAgent a1_;  
   SignalingAgent a2_;  
 };
+
 
 TEST_F(SignalingTest, JustInit)
 {
