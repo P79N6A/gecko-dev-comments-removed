9ab7f909287a6bfc336e403853f45d642c471e00
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:40:54 +0000

    Bug 982398 - Fix the second case as well. r=jandem
    UltraBlame original commit: ea688f9353895db67bcadc3b0da136c9e0176de8

--- js/src/jit/BaselineIC.cpp
+++ js/src/jit/BaselineIC.cpp
@@ -4374,6 +4374,7 @@ ICGetElemNativeCompiler::generateStubCode(MacroAssembler &masm)
             
             masm.loadPtr(Address(BaselineStubReg, ICGetElemNativeGetterStub::offsetOfGetter()),
                          scratchReg);
+            masm.branchIfFunctionHasNoScript(scratchReg, popR1 ? &failurePopR1 : &failure);
             masm.loadPtr(Address(scratchReg, JSFunction::offsetOfNativeOrScript()), scratchReg);
             masm.loadBaselineOrIonRaw(scratchReg, scratchReg, SequentialExecution,
                                       popR1 ? &failurePopR1 : &failure);
