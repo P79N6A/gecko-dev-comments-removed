2986dccf93109df517b3cff036f861fbc7455967
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:03:21 +0000

    Backed out 6 changesets (bug 976648) for android x86 bustage on a CLOSED TREE
    Backed out changeset 18f113ab1a35 (bug 976648)
    Backed out changeset 3f0f28593331 (bug 976648)
    Backed out changeset 2789ef4e1223 (bug 976648)
    Backed out changeset 4a5fff440cb5 (bug 976648)
    Backed out changeset 94ec75013788 (bug 976648)
    Backed out changeset 084e0311ff2a (bug 976648)
    
    UltraBlame original commit: 49d61a8f1e5c40e16d599a055e14f6371472e813

--- js/src/ctypes/libffi/src/powerpc/ffitarget.h
+++ js/src/ctypes/libffi/src/powerpc/ffitarget.h
@@ -25,14 +25,8 @@
 
 
 
-
-
 #ifndef LIBFFI_TARGET_H
 #define LIBFFI_TARGET_H
-
-#ifndef LIBFFI_H
-#error "Please do not include ffitarget.h directly into your source.  Use ffi.h instead."
-#endif
 
 
 
@@ -63,14 +57,18 @@ typedef enum ffi_abi {
   FFI_LINUX64,
   FFI_LINUX,
   FFI_LINUX_SOFT_FLOAT,
-# if defined(POWERPC64)
+# ifdef POWERPC64
   FFI_DEFAULT_ABI = FFI_LINUX64,
-# elif defined(__NO_FPRS__)
-  FFI_DEFAULT_ABI = FFI_LINUX_SOFT_FLOAT,
-# elif (__LDBL_MANT_DIG__ == 106)
-  FFI_DEFAULT_ABI = FFI_LINUX,
 # else
+#  if (!defined(__NO_FPRS__) && (__LDBL_MANT_DIG__ == 106))
+  FFI_DEFAULT_ABI = FFI_LINUX,
+#  else
+#   ifdef __NO_FPRS__
+  FFI_DEFAULT_ABI = FFI_LINUX_SOFT_FLOAT,
+#   else
   FFI_DEFAULT_ABI = FFI_GCC_SYSV,
+#   endif
+#  endif
 # endif
 #endif
 
@@ -103,10 +101,6 @@ typedef enum ffi_abi {
 
 #define FFI_CLOSURES 1
 #define FFI_NATIVE_RAW_API 0
-#if defined (POWERPC) || defined (POWERPC_FREEBSD)
-# define FFI_TARGET_SPECIFIC_VARIADIC 1
-# define FFI_EXTRA_CIF_FIELDS unsigned nfixedargs
-#endif
 
 
 
@@ -119,19 +113,10 @@ typedef enum ffi_abi {
 
 #define FFI_SYSV_TYPE_SMALL_STRUCT (FFI_TYPE_LAST + 2)
 
-
-#define FFI_V2_TYPE_FLOAT_HOMOG		(FFI_TYPE_LAST + 1)
-#define FFI_V2_TYPE_DOUBLE_HOMOG	(FFI_TYPE_LAST + 2)
-#define FFI_V2_TYPE_SMALL_STRUCT	(FFI_TYPE_LAST + 3)
-
-#if _CALL_ELF == 2
-# define FFI_TRAMPOLINE_SIZE 32
-#else
-# if defined(POWERPC64) || defined(POWERPC_AIX)
-#  define FFI_TRAMPOLINE_SIZE 24
-# else 
-#  define FFI_TRAMPOLINE_SIZE 40
-# endif
+#if defined(POWERPC64) || defined(POWERPC_AIX)
+#define FFI_TRAMPOLINE_SIZE 24
+#else 
+#define FFI_TRAMPOLINE_SIZE 40
 #endif
 
 #ifndef LIBFFI_ASM
