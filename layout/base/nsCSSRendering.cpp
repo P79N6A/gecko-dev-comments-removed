610e124076029733f5b46141068d6f5a0f02b853
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 21:58:56 +0000

    Bug 973321 - Prevent int overflow in border image width calculation. r=dbaron
    UltraBlame original commit: dccc80373461374bc83e5e3595ec3e57a8c16023

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -3110,7 +3110,10 @@ DrawBorderImage(nsPresContext*       aPresContext,
         value = 0;
         break;
     }
-    border.Side(s) = NS_lround(value);
+    
+    
+    MOZ_ASSERT(value >= 0);
+    border.Side(s) = NSToCoordRoundWithClamp(value);
     MOZ_ASSERT(border.Side(s) >= 0);
   }
 
