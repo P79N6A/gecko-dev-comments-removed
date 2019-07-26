d4b7eeb7922fac2573a2d212bb57447c16d2b65c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:47:25 +0000

    Bug 912321: Define the 'runOffThreadScript' function in the JavaScript shell, for testing off-thread compilation. r=bhackett1024
    UltraBlame original commit: b3c5a1f9193b56a936e410e4090d40d8e5b6ced1

---
+++ js/src/jit-test/tests/basic/offThreadCompileScript.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+load(libdir + 'asserts.js');
+
+if (!getBuildConfiguration().threadsafe)
+  quit(0);
+
+offThreadCompileScript('Math.sin(Math.PI/2)');
+assertEq(runOffThreadScript(), 1);
+
+offThreadCompileScript('a string which cannot be reduced to the start symbol');
+assertThrowsInstanceOf(runOffThreadScript, SyntaxError);
+
+offThreadCompileScript('smerg;');
+assertThrowsInstanceOf(runOffThreadScript, ReferenceError);
+
+offThreadCompileScript('throw "blerg";');
+assertThrowsValue(runOffThreadScript, 'blerg');
---
+++ js/src/jit-test/tests/debug/onNewScript-off-main-thread.js
@@ -0,0 +1,18 @@
+
+
+if (!getBuildConfiguration().threadsafe)
+  quit(0);
+
+var g = newGlobal();
+var dbg = new Debugger(g);
+
+var log;
+dbg.onNewScript = function (s) {
+  log += 's';
+  assertEq(s.source.text, '"t" + "wine"');
+}
+
+log = '';
+g.offThreadCompileScript('"t" + "wine"');
+assertEq(runOffThreadScript(), 'twine');
+assertEq(log, 's');
