d34716c1b7a245e3422bdb896adad99a334bbd06
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:57:19 +0000

    Bug 1007959 - Reduce HTTP Idle connection timeout on FxOS and Android for conservative battery use r=mcmanus
    UltraBlame original commit: 7863e387bf4f8839fecb19e0a1c8ea3ecc4c5715

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -77,14 +77,10 @@ pref("network.http.pipelining", true);
 pref("network.http.pipelining.ssl", true);
 pref("network.http.proxy.pipelining", true);
 pref("network.http.pipelining.maxrequests" , 6);
-pref("network.http.keep-alive.timeout", 600);
+pref("network.http.keep-alive.timeout", 109);
 pref("network.http.max-connections", 20);
 pref("network.http.max-persistent-connections-per-server", 6);
 pref("network.http.max-persistent-connections-per-proxy", 20);
-
-
-
-pref("network.cookie.cookieBehavior", 0);
 
 
 pref("network.http.spdy.push-allowance", 32768);
