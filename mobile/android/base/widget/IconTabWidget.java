cb4a521ae3febfe9ea526345475867bd654167f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:49:33 +0000

    Bug 836043: Use icon-tabs for tab switcher in tabs-ui. [r=mfinkle]
    UltraBlame original commit: 4271cc336457923d2f53569eb20d4ccbe3323939

---
+++ mobile/android/base/widget/IconTabWidget.java
@@ -0,0 +1,59 @@
+
+
+
+
+package org.mozilla.gecko.widget;
+
+import org.mozilla.gecko.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TabWidget;
+
+public class IconTabWidget extends TabWidget {
+    private Context mContext;
+    private OnTabChangedListener mListener;
+
+    public static interface OnTabChangedListener {
+        public void onTabChanged(int tabIndex);
+    }
+
+    public IconTabWidget(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+    }
+
+    public void addTab(int resId) {
+        ImageButton button = (ImageButton) LayoutInflater.from(mContext).inflate(R.layout.tabs_panel_indicator, null);
+        button.setImageResource(resId);
+
+        addView(button);
+        button.setOnClickListener(new TabClickListener(getTabCount() - 1));
+        button.setOnFocusChangeListener(this);
+    }
+
+    public void setTabSelectionListener(OnTabChangedListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+    }
+
+    private class TabClickListener implements OnClickListener {
+        private final int mIndex;
+
+        public TabClickListener(int index) {
+            mIndex = index;
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mListener != null)
+                mListener.onTabChanged(mIndex);
+        }
+    }
+}
