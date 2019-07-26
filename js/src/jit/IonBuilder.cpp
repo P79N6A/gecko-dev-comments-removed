3f2437ae46855194a63d15771e6853fe181578a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:07:34 +0000

    Bug 980119 - Watch for typed arrays with unknown properties when baking in their elements, r=jandem.
    UltraBlame original commit: 288cea1386a4e5068d092f2c69064f32539c643f

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -7181,10 +7181,12 @@ IonBuilder::getTypedArrayElements(MDefinition *obj)
             
             
             types::TypeObjectKey *tarrType = types::TypeObjectKey::get(tarr);
-            tarrType->watchStateChangeForTypedArrayBuffer(constraints());
+            if (!tarrType->unknownProperties()) {
+                tarrType->watchStateChangeForTypedArrayBuffer(constraints());
 
-            obj->setImplicitlyUsedUnchecked();
-            return MConstantElements::New(alloc(), data);
+                obj->setImplicitlyUsedUnchecked();
+                return MConstantElements::New(alloc(), data);
+            }
         }
     }
     return MTypedArrayElements::New(alloc(), obj);
