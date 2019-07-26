d47a18c81c3fad400f6d63bbbf8bf65c676c6677
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:18:48 +0000

    Bug 898263 (part 9) - Move IdValuePair from jsprvtd.h to ds/IdValuePair.h.  r=jorendorff.
    UltraBlame original commit: 8a78350b9749807b43dc10e9132c44490b679c2c

---
+++ js/src/ds/IdValuePair.h
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+#ifndef ds_IdValuePair_h
+#define ds_IdValuePair_h
+
+#include "jsapi.h"
+
+namespace js {
+
+struct IdValuePair
+{
+    jsid id;
+    Value value;
+
+    IdValuePair() {}
+    IdValuePair(jsid idArg)
+      : id(idArg), value(UndefinedValue())
+    {}
+};
+
+} 
+
+#endif 
