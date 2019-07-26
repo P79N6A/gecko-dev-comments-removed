7e1e6531d232482bf8ef193fa6a76d7b1c0fcf70
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:03:32 +0000

    Bug 1013601 - Part 2: Change callers to use uptimeMillis(). r=rnewman
    UltraBlame original commit: e563583ad13779762f535a51a8c223127e45dadb

--- mobile/android/chrome/content/aboutReader.js
+++ mobile/android/chrome/content/aboutReader.js
@@ -289,10 +289,8 @@ AboutReader.prototype = {
     this._isReadingListItem = (this._isReadingListItem == 1) ? 0 : 1;
     this._updateToggleButton();
 
-    
-    let uptime = Date.now() - Services.startup.getStartupInfo().linkerInitialized;
-
     if (this._isReadingListItem == 1) {
+      let uptime = UITelemetry.uptimeMillis();
       gChromeWin.Reader.storeArticleInCache(this._article, function(success) {
         dump("Reader:Add (in reader) success=" + success);
 
@@ -320,7 +318,7 @@ AboutReader.prototype = {
       
       Services.obs.notifyObservers(null, "Reader:Remove", this._article.url);
 
-      UITelemetry.addEvent("unsave.1", "button", uptime, "reader");
+      UITelemetry.addEvent("unsave.1", "button", null, "reader");
     }
   },
 
@@ -334,9 +332,7 @@ AboutReader.prototype = {
       title: this._article.title
     });
 
-    
-    let uptime = Date.now() - Services.startup.getStartupInfo().linkerInitialized;
-    UITelemetry.addEvent("share.1", "list", uptime);
+    UITelemetry.addEvent("share.1", "list", null);
   },
 
   _setFontSize: function Reader_setFontSize(newFontSize) {
@@ -705,10 +701,8 @@ AboutReader.prototype = {
         aEvent.stopPropagation();
 
         
-        let uptime = Date.now() - Services.startup.getStartupInfo().linkerInitialized;
         
-        
-        UITelemetry.addEvent("action.1", "button", uptime, id);
+        UITelemetry.addEvent("action.1", "button", null, id);
 
         let items = segmentedButton.children;
         for (let j = items.length - 1; j >= 0; j--) {
