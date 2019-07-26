cea8daa0ea918ff82e9a75b70358d5b32098c408
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:29:19 +0000

    Bug 933885 part 3: remove wholly redundant dump()s from the W3C testharness.js shim in dom/encoding/test/unit. r=gps
    UltraBlame original commit: bc1e7d605099d169f37c2e10190f5628b7084781

--- dom/encoding/test/unit/head.js
+++ dom/encoding/test/unit/head.js
@@ -3,52 +3,93 @@
 
 
 
-const { 'classes': Cc, 'interfaces': Ci } = Components;
+
+
+
+
+
+
+
+
+
+
+
 
 function assert_equals(a, b, msg) {
-  dump("assert_equals(" + a + ", " + b + ", \"" + msg + "\")");
-  do_check_eq(a, b, Components.stack.caller);
+  let text = msg + ": " + _wrap_with_quotes_if_necessary(a) +
+                 " == " + _wrap_with_quotes_if_necessary(b);
+  do_report_result(a == b, text, Components.stack.caller, false);
 }
 
 function assert_not_equals(a, b, msg) {
-  dump("assert_not_equals(" + a + ", " + b + ", \"" + msg + "\")");
-  do_check_neq(a, b, Components.stack.caller);
+  let text = msg + ": " + _wrap_with_quotes_if_necessary(a) +
+                 " != " + _wrap_with_quotes_if_necessary(b);
+  do_report_result(a != b, text, Components.stack.caller, false);
 }
 
 function assert_array_equals(a, b, msg) {
-  dump("assert_array_equals(\"" + msg + "\")");
-  do_check_eq(a.length, b.length, Components.stack.caller);
-  for (var i = 0; i < a.length; ++i) {
+  do_report_result(a.length == b.length,
+                   msg + ": (length) " + a.length + " == " + b.length,
+                   Components.stack.caller, false);
+  for (let i = 0; i < a.length; ++i) {
     if (a[i] !== b[i]) {
-      do_check_eq(a[i], b[i], Components.stack.caller);
+        do_report_result(false,
+                         msg + ": [" + i + "] " +
+                               _wrap_with_quotes_if_necessary(a[i]) +
+                               " === " +
+                               _wrap_with_quotes_if_necessary(b[i]),
+                         Components.stack.caller, false);
     }
   }
+  
+  do_report_result(true, msg + ": all array elements equal",
+                   Components.stack.caller, false);
 }
 
 function assert_true(cond, msg) {
-  dump("assert_true(" + cond + ", \"" + msg + "\")");
-  do_check_true(!!cond, Components.stack.caller); 
+  do_report_result(!!cond, msg + ": " + uneval(cond),
+                   Components.stack.caller, false);
 }
 
 function assert_throws(ex, func) {
-  dump("assert_throws(\"" + ex + "\", " + func + ")");
+  if (!('name' in ex))
+    do_throw("first argument to assert_throws must be of the form " +
+             "{'name': something}");
+
+  let msg = "expected to catch an exception named " + ex.name;
+
   try {
     func();
-    do_check_true(false, Components.stack.caller); 
   } catch (e) {
-    do_check_eq(e.name, ex.name, Components.stack.caller); 
+    if ('name' in e)
+      do_report_result(e.name == ex.name,
+                       msg + ", got " + e.name,
+                       Components.stack.caller, false);
+    else
+      do_report_result(false,
+                       msg + ", got " + legible_exception(ex),
+                       Components.stack.caller, false);
+
+    return;
   }
+
+  
+  
+  do_report_result(false, msg + ", but returned normally",
+                   Components.stack.caller, false);
 }
 
-var tests = [];
+let tests = [];
 
 function test(func, msg) {
-  tests.push({msg: msg, func: func});
+  tests.push({msg: msg, func: func,
+              filename: Components.stack.caller.filename });
 }
 
 function run_test() {
   tests.forEach(function(t) {
-    dump("test(\"" + t.msg + "\")");
+    _log("test_info", {source_file: t.filename,
+                       diagnostic: "test group: " + t.msg});
     t.func();
   });
 };
