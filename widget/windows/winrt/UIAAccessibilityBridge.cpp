d54576b39f5dca397a55913d295be0f48739345c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:13:05 +0000

    Bug 855232 - revamp UIABridge such that it queries for element state in real-time rather than relying on accessible observer events. r=bbondy
    UltraBlame original commit: 1225c6016d106c0d9207b6aa7247fb13189e8f59

--- widget/windows/winrt/UIAAccessibilityBridge.cpp
+++ widget/windows/winrt/UIAAccessibilityBridge.cpp
@@ -19,16 +19,6 @@
 
 #include <wrl/implements.h>
 
-
-#if !defined(DEBUG_BRIDGE)
-#undef LogThread
-#undef LogFunction
-#undef Log
-#define LogThread() 
-#define LogFunction()
-#define Log(...)
-#endif
-
 using namespace mozilla::a11y;
 
 namespace mozilla {
@@ -49,89 +39,12 @@ AccessibilityBridge::Observe(nsISupports *aSubject, const char *aTopic, const PR
 
   uint32_t eventType = 0;
   ev->GetEventType(&eventType);
-
-  switch (eventType) {
-    case nsIAccessibleEvent::EVENT_FOCUS:
-      Log(L"EVENT_FOCUS");
-      {
-        nsCOMPtr<nsIAccessible> item;
-        ev->GetAccessible(getter_AddRefs(item));
-        Accessible* access = (Accessible*)item.get();
-        Log(L"Focus element flags: %d %d %d",
-          ((access->NativeState() & mozilla::a11y::states::EDITABLE) > 0),
-          ((access->NativeState() & mozilla::a11y::states::FOCUSABLE) > 0),
-          ((access->NativeState() & mozilla::a11y::states::READONLY) > 0)
-          );
-        bool focusable = (((access->NativeState() & mozilla::a11y::states::EDITABLE) > 0) &&
-                          ((access->NativeState() & mozilla::a11y::states::READONLY) == 0));
-
-        if (focusable) {
-          Log(L"focus item can be focused");
-          
-          Microsoft::WRL::ComPtr<IUIAElement> bridgePtr;
-          mBridge.As(&bridgePtr);
-          if (bridgePtr) {
-            bridgePtr->SetFocusInternal(reinterpret_cast<LONG_PTR>(item.get()));
-          }
-        } else {
-          Log(L"focus item can't have focus");
-          Microsoft::WRL::ComPtr<IUIAElement> bridgePtr;
-          mBridge.As(&bridgePtr);
-          if (bridgePtr) {
-            bridgePtr->ClearFocus();
-          }
-        }
-      }
-    break;
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    default:
-    return NS_OK;
+  if (eventType == nsIAccessibleEvent::EVENT_FOCUS) {
+    Microsoft::WRL::ComPtr<IUIABridge> bridgePtr;
+    mBridge.As(&bridgePtr);
+    if (bridgePtr) {
+      bridgePtr->FocusChangeEvent();
+    }
   }
 
   return NS_OK;
