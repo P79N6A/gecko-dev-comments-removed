8ce84a0529d521685837b1d6fe5ce1f23308ae65
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:07:31 +0000

    Bug 684887 part A - Make the XPCOM component manager non-reentrant, and make sure that nsFactoryEntry.mFactory is protected by it, r=cjones
    UltraBlame original commit: 625ba43c97c6c374d4dfdf9d96eeb7ae9a7c663e

--- xpcom/components/nsComponentManager.h
+++ xpcom/components/nsComponentManager.h
@@ -15,7 +15,7 @@
 #include "nsIFile.h"
 #include "mozilla/Module.h"
 #include "mozilla/ModuleLoader.h"
-#include "mozilla/ReentrantMonitor.h"
+#include "mozilla/Mutex.h"
 #include "nsXULAppAPI.h"
 #include "nsNativeComponentLoader.h"
 #include "nsIFactory.h"
@@ -112,7 +112,7 @@ public:
     nsDataHashtable<nsIDHashKey, nsFactoryEntry*> mFactories;
     nsDataHashtable<nsCStringHashKey, nsFactoryEntry*> mContractIDs;
 
-    mozilla::ReentrantMonitor mMon;
+    mozilla::Mutex mLock;
 
     static void InitializeStaticModules();
     static void InitializeModuleLocations();
@@ -198,12 +198,17 @@ public:
     
     nsClassHashtable<nsCStringHashKey, KnownModule> mKnownModules;
 
+    
     void RegisterModule(const mozilla::Module* aModule,
                         mozilla::FileLocation* aFile);
-    void RegisterCIDEntry(const mozilla::Module::CIDEntry* aEntry,
-                          KnownModule* aModule);
-    void RegisterContractID(const mozilla::Module::ContractIDEntry* aEntry);
 
+
+    
+    void RegisterCIDEntryLocked(const mozilla::Module::CIDEntry* aEntry,
+                          KnownModule* aModule);
+    void RegisterContractIDLocked(const mozilla::Module::ContractIDEntry* aEntry);
+
+    
     void RegisterManifest(NSLocationType aType, mozilla::FileLocation &aFile,
                           bool aChromeOnly);
 
