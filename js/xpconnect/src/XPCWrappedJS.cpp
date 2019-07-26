00a5a49198d52a5fc4ca9e90696012f245d45e35
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:22:48 +0000

    Bug 961551, part 9 - Split scope of |wrapper|, plus other cleanup. r=bholley
    UltraBlame original commit: a8129fa38c35666f8773b9712276452da722d4f9

--- js/xpconnect/src/XPCWrappedJS.cpp
+++ js/xpconnect/src/XPCWrappedJS.cpp
@@ -327,10 +327,8 @@ nsXPCWrappedJS::GetNewOrUsed(JS::HandleObject jsObj,
         MOZ_CRASH();
 
     AutoJSContext cx;
-    JSObject2WrappedJSMap* map;
     XPCJSRuntime* rt = nsXPConnect::GetRuntimeInstance();
-
-    map = rt->GetWrappedJSMap();
+    JSObject2WrappedJSMap* map = rt->GetWrappedJSMap();
     if (!map) {
         MOZ_ASSERT(map,"bad map");
         return NS_ERROR_FAILURE;
@@ -341,15 +339,13 @@ nsXPCWrappedJS::GetNewOrUsed(JS::HandleObject jsObj,
     if (!clasp)
         return NS_ERROR_FAILURE;
 
-    
     JS::RootedObject rootJSObj(cx, clasp->GetRootJSObject(cx, jsObj));
     if (!rootJSObj)
         return NS_ERROR_FAILURE;
 
     nsRefPtr<nsXPCWrappedJS> root = map->Find(rootJSObj);
-    nsRefPtr<nsXPCWrappedJS> wrapper;
     if (root) {
-        wrapper = root->FindOrFindInherited(aIID);
+        nsRefPtr<nsXPCWrappedJS> wrapper = root->FindOrFindInherited(aIID);
         if (wrapper) {
             wrapper.forget(wrapperResult);
             return NS_OK;
@@ -368,11 +364,7 @@ nsXPCWrappedJS::GetNewOrUsed(JS::HandleObject jsObj,
         root = new nsXPCWrappedJS(cx, rootJSObj, rootClasp, nullptr);
     }
 
-    
-    MOZ_ASSERT(clasp, "bad clasp");
-    MOZ_ASSERT(!wrapper, "no wrapper found yet");
-
-    wrapper = new nsXPCWrappedJS(cx, jsObj, clasp, root);
+    nsRefPtr<nsXPCWrappedJS> wrapper = new nsXPCWrappedJS(cx, jsObj, clasp, root);
     wrapper.forget(wrapperResult);
     return NS_OK;
 }
