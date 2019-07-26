9d3fe543a5a431adc58e221b5761dcac5d44bd0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:51:22 +0000

    Bug 945085 - Improve comments in PlatformDecoderModule.h. r=kinetik
    Adds comments to PlatformDecoderModule.h, and describes the call model
    better. Renames CreateAudioDecoder and CreateVideoDecoder to
    CreateAACDecoder and CreateH264Decoder. We may change them to accept a
    codec parameter and have more general names in future when we need to
    support more codecs here.
    
    UltraBlame original commit: 4531393b5af60091a724103e26a5d338180b83f1

--- content/media/fmp4/PlatformDecoderModule.h
+++ content/media/fmp4/PlatformDecoderModule.h
@@ -16,7 +16,72 @@ namespace layers {
 class ImageContainer;
 }
 
+class MediaDataDecoder;
 typedef int64_t Microseconds;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class PlatformDecoderModule {
+public:
+
+  
+  
+  
+  
+  
+  
+  static PlatformDecoderModule* Create();
+
+  
+  
+  
+  
+  
+  virtual nsresult Shutdown() = 0;
+
+  
+  
+  
+  
+  
+  virtual MediaDataDecoder* CreateH264Decoder(layers::LayersBackend aLayersBackend,
+                                              layers::ImageContainer* aImageContainer) = 0;
+
+  
+  
+  
+  
+  
+  
+  virtual MediaDataDecoder* CreateAACDecoder(uint32_t aChannelCount,
+                                             uint32_t aSampleRate,
+                                             uint16_t aBitsPerSample,
+                                             const uint8_t* aAACConfig,
+                                             uint32_t aAACConfigLength) = 0;
+
+  
+  virtual void OnDecodeThreadStart() {}
+
+  
+  virtual void OnDecodeThreadFinish() {}
+
+  virtual ~PlatformDecoderModule() {}
+protected:
+  PlatformDecoderModule() {}
+};
+
 
 enum DecoderStatus {
   DECODE_STATUS_NOT_ACCEPTING, 
@@ -25,12 +90,31 @@ enum DecoderStatus {
   DECODE_STATUS_ERROR
 };
 
+
+
+
+
+
+
+
+
 class MediaDataDecoder {
 public:
   virtual ~MediaDataDecoder() {};
 
-  virtual nsresult Shutdown() = 0;
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   
   
@@ -41,42 +125,34 @@ public:
                               int64_t aOffsetInStream) = 0;
 
   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   virtual DecoderStatus Output(nsAutoPtr<MediaData>& aOutData) = 0;
 
+  
+  
+  
+  
+  
   virtual DecoderStatus Flush() = 0;
-};
-
-class PlatformDecoderModule {
-public:
 
   
   
   
-  
-  static PlatformDecoderModule* Create();
-
   
   virtual nsresult Shutdown() = 0;
 
-  
-  
-  virtual MediaDataDecoder* CreateVideoDecoder(layers::LayersBackend aLayersBackend,
-                                               layers::ImageContainer* aImageContainer) = 0;
-
-  
-  virtual MediaDataDecoder* CreateAudioDecoder(uint32_t aChannelCount,
-                                               uint32_t aSampleRate,
-                                               uint16_t aBitsPerSample,
-                                               const uint8_t* aUserData,
-                                               uint32_t aUserDataLength) = 0;
-
-  
-  virtual void OnDecodeThreadStart();
-  virtual void OnDecodeThreadFinish();
-
-  virtual ~PlatformDecoderModule() {}
-protected:
-  PlatformDecoderModule() {}
 };
 
 } 
