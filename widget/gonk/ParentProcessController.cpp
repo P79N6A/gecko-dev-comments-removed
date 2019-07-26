6786eb9e843353b1b940ff468fecf6c62a1ff98b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:45:26 +0000

    Bug 912657 - Add support for APZC in the B2G parent process (without turning it on everywhere). r=ajones
    UltraBlame original commit: c67f5daf8e21a90609a712a2ac78dcec7fbfb287

---
+++ widget/gonk/ParentProcessController.cpp
@@ -0,0 +1,60 @@
+
+
+
+
+
+#include "ParentProcessController.h"
+#include "nsIContent.h"
+#include "nsLayoutUtils.h"
+#include "APZCCallbackHelper.h"
+#include "base/message_loop.h"
+
+namespace mozilla {
+namespace widget {
+
+class RequestContentRepaintEvent : public nsRunnable
+{
+    typedef mozilla::layers::FrameMetrics FrameMetrics;
+
+public:
+    RequestContentRepaintEvent(const FrameMetrics& aFrameMetrics)
+        : mFrameMetrics(aFrameMetrics)
+    {
+    }
+
+    NS_IMETHOD Run() {
+        MOZ_ASSERT(NS_IsMainThread());
+        nsCOMPtr<nsIContent> content = nsLayoutUtils::FindContentFor(mFrameMetrics.mScrollId);
+        if (content) {
+            APZCCallbackHelper::UpdateSubFrame(content, mFrameMetrics);
+        }
+        return NS_OK;
+    }
+
+protected:
+    const FrameMetrics mFrameMetrics;
+};
+
+void
+ParentProcessController::RequestContentRepaint(const FrameMetrics& aFrameMetrics)
+{
+    if (aFrameMetrics.mScrollId == FrameMetrics::NULL_SCROLL_ID) {
+        return;
+    }
+
+    nsCOMPtr<nsIRunnable> r = new RequestContentRepaintEvent(aFrameMetrics);
+    if (!NS_IsMainThread()) {
+        NS_DispatchToMainThread(r);
+    } else {
+        r->Run();
+    }
+}
+
+void
+ParentProcessController::PostDelayedTask(Task* aTask, int aDelayMs)
+{
+    MessageLoop::current()->PostDelayedTask(FROM_HERE, aTask, aDelayMs);
+}
+
+}
+}
---
+++ widget/gonk/ParentProcessController.h
@@ -0,0 +1,34 @@
+
+
+
+
+
+#ifndef __mozilla_widget_DynamicToolbarController_h__
+#define __mozilla_widget_DynamicToolbarController_h__
+
+#include "mozilla/layers/GeckoContentController.h"
+
+namespace mozilla {
+namespace widget {
+
+class ParentProcessController : public mozilla::layers::GeckoContentController
+{
+    typedef mozilla::layers::FrameMetrics FrameMetrics;
+
+public:
+    virtual void RequestContentRepaint(const FrameMetrics& aFrameMetrics) MOZ_OVERRIDE;
+    virtual void PostDelayedTask(Task* aTask, int aDelayMs) MOZ_OVERRIDE;
+
+    
+    virtual void HandleDoubleTap(const CSSIntPoint& aPoint) MOZ_OVERRIDE {}
+    virtual void HandleSingleTap(const CSSIntPoint& aPoint) MOZ_OVERRIDE {}
+    virtual void HandleLongTap(const CSSIntPoint& aPoint) MOZ_OVERRIDE {}
+    virtual void SendAsyncScrollDOMEvent(bool aIsRoot,
+                                         const CSSRect &aContentRect,
+                                         const CSSSize &aScrollableSize) MOZ_OVERRIDE {}
+};
+
+}
+}
+
+#endif 
