737c4c76919558f936181d2e8f7bb6653aa224f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:58:14 +0000

    Bug 627699 - Port GTK2 to GTK3, ThemeRenderer part. r=karlt
    UltraBlame original commit: 0c8e1a272c78100ec6082fc29334be4a93695bbd

--- widget/gtk2/nsNativeThemeGTK.cpp
+++ widget/gtk2/nsNativeThemeGTK.cpp
@@ -612,6 +612,7 @@ nsNativeThemeGTK::GetGtkWidgetAndState(uint8_t aWidgetType, nsIFrame* aFrame,
   return true;
 }
 
+#if defined(MOZ_WIDGET_GTK2)
 class ThemeRenderer : public gfxGdkNativeRenderer {
 public:
   ThemeRenderer(GtkWidgetState aState, GtkThemeWidgetType aGTKWidgetType,
@@ -654,6 +655,7 @@ ThemeRenderer::DrawWithGDK(GdkDrawable * drawable, gint offsetX,
 
   return NS_OK;
 }
+#endif
 
 bool
 nsNativeThemeGTK::GetExtraSizeForWidget(nsIFrame* aFrame, uint8_t aWidgetType,
@@ -782,23 +784,8 @@ nsNativeThemeGTK::DrawWidgetBackground(nsRenderingContext* aContext,
 
   
 
-  
-  
-  GdkRectangle gdk_clip = {0, 0, drawingRect.width, drawingRect.height};
-
   GdkRectangle gdk_rect = {-drawingRect.x, -drawingRect.y,
                            widgetRect.width, widgetRect.height};
-
-  ThemeRenderer renderer(state, gtkWidgetType, flags, direction,
-                         gdk_rect, gdk_clip);
-
-  
-  
-  
-  uint32_t rendererFlags = 0;
-  if (GetWidgetTransparency(aFrame, aWidgetType) == eOpaque) {
-    rendererFlags |= gfxGdkNativeRenderer::DRAW_IS_OPAQUE;
-  }
 
   
   gfxContextAutoSaveRestore autoSR(ctx);
@@ -817,11 +804,31 @@ nsNativeThemeGTK::DrawWidgetBackground(nsRenderingContext* aContext,
     gdk_error_trap_push ();
   }
 
+#if defined(MOZ_WIDGET_GTK2)
+  
+  
+  GdkRectangle gdk_clip = {0, 0, drawingRect.width, drawingRect.height};
+
+  ThemeRenderer renderer(state, gtkWidgetType, flags, direction,
+                         gdk_rect, gdk_clip);
+
+  
+  
+  
+  uint32_t rendererFlags = 0;
+  if (GetWidgetTransparency(aFrame, aWidgetType) == eOpaque) {
+    rendererFlags |= gfxGdkNativeRenderer::DRAW_IS_OPAQUE;
+  }
+
   
   
   GdkColormap* colormap = moz_gtk_widget_get_colormap();
 
   renderer.Draw(ctx, drawingRect.Size(), rendererFlags, colormap);
+#else 
+  moz_gtk_widget_paint(gtkWidgetType, ctx->GetCairo(), &gdk_rect, 
+                       &state, flags, direction);
+#endif
 
   if (!safeState) {
     gdk_flush();
