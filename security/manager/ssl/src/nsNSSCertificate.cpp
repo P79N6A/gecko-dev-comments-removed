93d47079108600edb9ebfecd1699bceb78813aa4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:57:23 +0000

    Bug 938046 - Part 3. Iterate only through valid users on getchain  r=dkeeler
    UltraBlame original commit: 080cd15c7c907c0fe66a6b6ae6a5044a87020393

--- security/manager/ssl/src/nsNSSCertificate.cpp
+++ security/manager/ssl/src/nsNSSCertificate.cpp
@@ -856,10 +856,18 @@ nsNSSCertificate::GetChain(nsIArray **_rvChain)
                                  nullptr, 
                                  CertVerifier::FLAG_LOCAL_ONLY,
                                  &pkixNssChain);
+  
+  
+  const int otherUsagesToTest = certificateUsageSSLClient |
+                                certificateUsageSSLCA |
+                                certificateUsageEmailSigner |
+                                certificateUsageEmailRecipient |
+                                certificateUsageObjectSigner |
+                                certificateUsageStatusResponder;
   for (int usage = certificateUsageSSLClient;
        usage < certificateUsageAnyCA && !pkixNssChain;
        usage = usage << 1) {
-    if (usage == certificateUsageSSLServer) {
+    if ((usage & otherUsagesToTest) == 0) {
       continue;
     }
     PR_LOG(gPIPNSSLog, PR_LOG_DEBUG, ("pipnss: PKIX attempting chain(%d) for '%s'\n",usage, mCert->nickname));
