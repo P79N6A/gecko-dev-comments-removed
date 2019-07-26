35ff397bd33b3897e0992b98a9b4c72f08ad704c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:33:54 +0000

    Bug 806724 - Port browser_ConsoleStoragePBTest.js to the new per-tab PB APIs; r=ehsan
    DONTBUILD since this is NPOTB in global PB builds
    
    UltraBlame original commit: 3ce22cb51e5613f19ca524c5eccc1d59e6e9153c

---
+++ dom/tests/browser/browser_ConsoleStoragePBTest_perwindowpb.js
@@ -0,0 +1,88 @@
+
+
+
+function test() {
+  
+  waitForExplicitFinish();
+  let windowsToClose = [];
+  let innerID;
+  let beforeEvents;
+  let afterEvents;
+  let storageShouldOccur;
+  let consoleObserver;
+  let testURI =
+    "http://example.com/browser/dom/tests/browser/test-console-api.html";
+  let CSS = {};
+  Cu.import("resource://gre/modules/ConsoleAPIStorage.jsm", CSS);
+
+  function getInnerWindowId(aWindow) {
+    return aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+                  .getInterface(Ci.nsIDOMWindowUtils)
+                  .currentInnerWindowID;
+  }
+
+  function whenNewWindowLoaded(aOptions, aCallback) {
+    let win = OpenBrowserWindow(aOptions);
+    win.addEventListener("load", function onLoad() {
+      win.removeEventListener("load", onLoad, false);
+      aCallback(win);
+    }, false);
+  }
+
+  function doTest(aIsPrivateMode, aWindow, aCallback) {
+    aWindow.gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
+      aWindow.gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
+
+      consoleObserver = {
+        observe: function(aSubject, aTopic, aData) {
+          if (aTopic == "console-api-log-event") {
+            afterEvents = CSS.ConsoleAPIStorage.getEvents(innerID);
+            is(beforeEvents.length == afterEvents.length - 1, storageShouldOccur,
+              "storage should" + (storageShouldOccur ? "" : " not") + " occur");
+
+            executeSoon(function() {
+              Services.obs.removeObserver(consoleObserver, "console-api-log-event");
+              aCallback();
+            });
+          }
+        }
+      };
+
+      aWindow.Services.obs.addObserver(
+        consoleObserver, "console-api-log-event", false);
+      aWindow.console.log("foo bar baz (private: " + aIsPrivateMode + ")");
+    }, true);
+
+    storageShouldOccur = !aIsPrivateMode;
+    innerID = getInnerWindowId(aWindow);
+    beforeEvents = CSS.ConsoleAPIStorage.getEvents(innerID);
+    aWindow.gBrowser.selectedBrowser.loadURI(testURI);
+  }
+
+  function testOnWindow(aOptions, aCallback) {
+    whenNewWindowLoaded(aOptions, function(aWin) {
+      windowsToClose.push(aWin);
+      
+      
+      
+      executeSoon(function() aCallback(aWin));
+    });
+  };
+
+   
+  registerCleanupFunction(function() {
+    windowsToClose.forEach(function(aWin) {
+      aWin.close();
+    });
+  });
+
+  
+  testOnWindow({}, function(aWin) {
+    doTest(false, aWin, function() {
+      
+      testOnWindow({private: true}, function(aWin) {
+        doTest(true, aWin, finish);
+      });
+    });
+  });
+}
