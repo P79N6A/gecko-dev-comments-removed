3d2fcfe1e235e243d4ada0831cc46281de8e8655
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:47:19 +0000

    Bug 924403. Linux users who had previously opted in to OpenGL get OMTC OpenGL. r=mattwoodrow
    UltraBlame original commit: aa0066b3865c3d19fc278880e6297c6b9acfe649

--- gfx/thebes/gfxPlatform.cpp
+++ gfx/thebes/gfxPlatform.cpp
@@ -2108,7 +2108,13 @@ gfxPlatform::GetPrefLayersAccelerationForceEnabled()
 bool gfxPlatform::OffMainThreadCompositionRequired()
 {
   InitLayersAccelerationPrefs();
+#if defined(MOZ_WIDGET_GTK) && defined(NIGHTLY_BUILD)
+  
+  return sPrefBrowserTabsRemote ||
+         sPrefLayersAccelerationForceEnabled;
+#else
   return sPrefBrowserTabsRemote;
+#endif
 }
 
 bool
