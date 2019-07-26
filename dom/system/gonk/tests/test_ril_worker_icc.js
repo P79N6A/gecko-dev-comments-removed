d6bace5ba430089557d6183db14c1568d777185f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:47:43 +0000

    Bug 831628 - 0005. Test case for Event Download Command : Idle Screen Available. r=yoshi
    UltraBlame original commit: 5c0e27333599c0424a691c30001cf7bdbd9bd5cd

--- dom/system/gonk/tests/test_ril_worker_icc.js
+++ dom/system/gonk/tests/test_ril_worker_icc.js
@@ -1513,3 +1513,51 @@ add_test(function test_stk_terminal_response() {
   };
   worker.RIL.sendStkTerminalResponse(response);
 });
+
+
+
+
+add_test(function test_stk_event_download_idle_screen_available() {
+  let worker = newUint8SupportOutgoingIndexWorker();
+  let buf = worker.Buf;
+  let pduHelper = worker.GsmPDUHelper;
+
+  buf.sendParcel = function () {
+    
+    do_check_eq(this.readUint32(), REQUEST_STK_SEND_ENVELOPE_COMMAND)
+
+    
+    this.readUint32();
+
+    
+    do_check_eq(this.readUint32(), 18);
+
+    
+    do_check_eq(pduHelper.readHexOctet(), BER_EVENT_DOWNLOAD_TAG);
+
+    
+    do_check_eq(pduHelper.readHexOctet(), 7);
+
+    
+    do_check_eq(pduHelper.readHexOctet(), COMPREHENSIONTLV_TAG_DEVICE_ID |
+                                          COMPREHENSIONTLV_FLAG_CR);
+    do_check_eq(pduHelper.readHexOctet(), 2);
+    do_check_eq(pduHelper.readHexOctet(), STK_DEVICE_ID_DISPLAY);
+    do_check_eq(pduHelper.readHexOctet(), STK_DEVICE_ID_SIM);
+
+    
+    do_check_eq(pduHelper.readHexOctet(), COMPREHENSIONTLV_TAG_EVENT_LIST |
+                                          COMPREHENSIONTLV_FLAG_CR);
+    do_check_eq(pduHelper.readHexOctet(), 1);
+    do_check_eq(pduHelper.readHexOctet(), STK_EVENT_TYPE_IDLE_SCREEN_AVAILABLE);
+
+    run_next_test();
+  };
+
+  let event = {
+    eventType: STK_EVENT_TYPE_IDLE_SCREEN_AVAILABLE
+  };
+  worker.RIL.sendStkEventDownload({
+    event: event
+  });
+});
