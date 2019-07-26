f9717886aea96b2511b1b8145ff4a7a8213b4d9d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:44:45 +0000

    Bug 939654 - Remove unnecessary wait from instantiating MediaResourceManagerService. r=mwu
    UltraBlame original commit: d76b78d917412fe99cbb659ad0377d0fce3e719d

--- content/media/omx/mediaresourcemanager/MediaResourceManagerService.cpp
+++ content/media/omx/mediaresourcemanager/MediaResourceManagerService.cpp
@@ -16,41 +16,8 @@
 
 namespace android {
 
-int waitBeforeAdding(const android::String16& serviceName)
-{
-  android::sp<android::IServiceManager> sm = android::defaultServiceManager();
-  for ( int i = 0 ; i < 5; i++ ) {
-    if ( sm->checkService ( serviceName ) != NULL ) {
-      sleep(1);
-    }
-    else {
-      
-      return 0;
-    }
-  }
-  
- return -1;
-}
-
-
-void
-waitServiceManager()
-{
-  android::sp<android::IServiceManager> sm;
-  do {
-    sm = android::defaultServiceManager();
-    if (sm.get()) {
-      break;
-    }
-    usleep(50000); 
-  } while(true);
-}
-
 
 void MediaResourceManagerService::instantiate() {
-  waitServiceManager();
-  waitBeforeAdding( android::String16("media.resource_manager") );
-
   defaultServiceManager()->addService(
             String16("media.resource_manager"), new MediaResourceManagerService());
 }
