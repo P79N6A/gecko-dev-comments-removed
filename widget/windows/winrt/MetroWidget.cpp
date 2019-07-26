95dd01a06aca2c65f73cfef98c2e6ba5f9d20e37
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:21:15 +0000

    Bug 905058 - Before accessing the UIA root element make sure the global com ptr is populated. r=bbondy
    UltraBlame original commit: f47dfe10d30192410ed8158a59cd2b74b05bf1ba

--- widget/windows/winrt/MetroWidget.cpp
+++ widget/windows/winrt/MetroWidget.cpp
@@ -686,16 +686,17 @@ MetroWidget::WindowProcedure(HWND aWnd, UINT aMsg, WPARAM aWParam, LPARAM aLPara
       
       
       
-      if (dwObjId == UiaRootObjectId) {
-        NS_ASSERTION(gProviderRoot.Get(), "gProviderRoot is null??");
+      if (dwObjId == UiaRootObjectId && gProviderRoot) {
         ComPtr<IRawElementProviderSimple> simple;
         gProviderRoot.As(&simple);
-        LRESULT res = UiaReturnRawElementProvider(aWnd, aWParam, aLParam, simple.Get());
-        if (res) {
-          return res;
+        if (simple) {
+          LRESULT res = UiaReturnRawElementProvider(aWnd, aWParam, aLParam, simple.Get());
+          if (res) {
+            return res;
+          }
+          NS_ASSERTION(res, "UiaReturnRawElementProvider failed!");
+          Log("UiaReturnRawElementProvider failed! GetLastError=%X", GetLastError());
         }
-        NS_ASSERTION(res, "UiaReturnRawElementProvider failed!");
-        Log("UiaReturnRawElementProvider failed! GetLastError=%X", GetLastError());
       }
       break;
     }
