ea82e70cd1490906b103300bc89dd80c69da5480
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:38:56 +0000

    Bug 511803 patch 1:  Fix serialization of font-variant-alternates values with more than one function.  (Tested in patch 2.)  r=jdaggett
    UltraBlame original commit: ea47adb687387bf698234e0be47f026e085979d3

--- layout/style/nsStyleUtil.cpp
+++ layout/style/nsStyleUtil.cpp
@@ -313,6 +313,7 @@ nsStyleUtil::SerializeFunctionalAlternates(
       NS_ASSERTION(!funcName.IsEmpty(), "unknown property value name");
 
       
+      funcParams.Truncate();
       AppendEscapedCSSIdent(v.value, funcParams);
     } else {
       if (!funcParams.IsEmpty()) {
