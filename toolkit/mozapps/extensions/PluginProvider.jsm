d8f71a0d5e36fd185fdad4e7b41600e6ba639b1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:56:38 +0000

    Bug 554780: Make plugins provider correctly handle plugins being added and removed through detection at runtime. r=Unfocused
    UltraBlame original commit: 4b19fa00a8aac5774e82e9617d2c7de708bdedaf

--- toolkit/mozapps/extensions/PluginProvider.jsm
+++ toolkit/mozapps/extensions/PluginProvider.jsm
@@ -14,6 +14,7 @@ Components.utils.import("resource://gre/modules/Services.jsm");
 
 const URI_EXTENSION_STRINGS  = "chrome://mozapps/locale/extensions/extensions.properties";
 const STRING_TYPE_NAME       = "type.%ID%.name";
+const LIST_UPDATED_TOPIC     = "plugins-list-updated";
 
 for (let name of ["LOG", "WARN", "ERROR"]) {
   this.__defineGetter__(name, function() {
@@ -52,6 +53,7 @@ var PluginProvider = {
   plugins: null,
 
   startup: function PL_startup() {
+    Services.obs.addObserver(this, LIST_UPDATED_TOPIC, false);
     Services.obs.addObserver(this, AddonManager.OPTIONS_NOTIFICATION_DISPLAYED, false);
   },
 
@@ -62,24 +64,43 @@ var PluginProvider = {
   shutdown: function PL_shutdown() {
     this.plugins = null;
     Services.obs.removeObserver(this, AddonManager.OPTIONS_NOTIFICATION_DISPLAYED);
+    Services.obs.removeObserver(this, LIST_UPDATED_TOPIC);
   },
 
   observe: function(aSubject, aTopic, aData) {
-    this.getAddonByID(aData, function(plugin) {
-      if (!plugin)
-        return;
+    switch (aTopic) {
+    case AddonManager.OPTIONS_NOTIFICATION_DISPLAYED:
+      this.getAddonByID(aData, function PL_displayPluginInfo(plugin) {
+        if (!plugin)
+          return;
 
-      let libLabel = aSubject.getElementById("pluginLibraries");
-      libLabel.textContent = plugin.pluginLibraries.join(", ");
+        let libLabel = aSubject.getElementById("pluginLibraries");
+        libLabel.textContent = plugin.pluginLibraries.join(", ");
 
-      let typeLabel = aSubject.getElementById("pluginMimeTypes"), types = [];
-      for (let type of plugin.pluginMimeTypes) {
-        let extras = [type.description.trim(), type.suffixes].
-                     filter(function(x) x).join(": ");
-        types.push(type.type + (extras ? " (" + extras + ")" : ""));
-      }
-      typeLabel.textContent = types.join(",\n");
-    });
+        let typeLabel = aSubject.getElementById("pluginMimeTypes"), types = [];
+        for (let type of plugin.pluginMimeTypes) {
+          let extras = [type.description.trim(), type.suffixes].
+                       filter(function(x) x).join(": ");
+          types.push(type.type + (extras ? " (" + extras + ")" : ""));
+        }
+        typeLabel.textContent = types.join(",\n");
+      });
+      break;
+    case LIST_UPDATED_TOPIC:
+      if (this.plugins)
+        this.updatePluginList();
+      break;
+    }
+  },
+
+  
+
+
+  buildWrapper: function PL_buildWrapper(aPlugin) {
+    return new PluginWrapper(aPlugin.id,
+                             aPlugin.name,
+                             aPlugin.description,
+                             aPlugin.tags);
   },
 
   
@@ -94,16 +115,10 @@ var PluginProvider = {
     if (!this.plugins)
       this.buildPluginList();
 
-    if (aId in this.plugins) {
-      let name = this.plugins[aId].name;
-      let description = this.plugins[aId].description;
-      let tags = this.plugins[aId].tags;
-
-      aCallback(new PluginWrapper(aId, name, description, tags));
-    }
-    else {
+    if (aId in this.plugins)
+      aCallback(this.buildWrapper(this.plugins[aId]));
+    else
       aCallback(null);
-    }
   },
 
   
@@ -158,32 +173,105 @@ var PluginProvider = {
     aCallback([]);
   },
 
-  buildPluginList: function PL_buildPluginList() {
+  
+
+
+
+
+  getPluginList: function PL_getPluginList() {
     let tags = Cc["@mozilla.org/plugin/host;1"].
                getService(Ci.nsIPluginHost).
                getPluginTags({});
 
-    this.plugins = {};
-    let plugins = {};
+    let list = {};
+    let seenPlugins = {};
     for (let tag of tags) {
-      if (!(tag.name in plugins))
-        plugins[tag.name] = {};
-      if (!(tag.description in plugins[tag.name])) {
+      if (!(tag.name in seenPlugins))
+        seenPlugins[tag.name] = {};
+      if (!(tag.description in seenPlugins[tag.name])) {
         let plugin = {
+          id: getIDHashForString(tag.name + tag.description),
           name: tag.name,
           description: tag.description,
           tags: [tag]
         };
 
-        let id = getIDHashForString(tag.name + tag.description);
-
-        plugins[tag.name][tag.description] = plugin;
-        this.plugins[id] = plugin;
+        seenPlugins[tag.name][tag.description] = plugin;
+        list[plugin.id] = plugin;
       }
       else {
-        plugins[tag.name][tag.description].tags.push(tag);
+        seenPlugins[tag.name][tag.description].tags.push(tag);
       }
     }
+
+    return list;
+  },
+
+  
+
+
+  buildPluginList: function PL_buildPluginList() {
+    this.plugins = this.getPluginList();
+  },
+
+  
+
+
+
+
+  updatePluginList: function PL_updatePluginList() {
+    let newList = this.getPluginList();
+
+    let lostPlugins = [this.buildWrapper(this.plugins[id])
+                       for each (id in Object.keys(this.plugins)) if (!(id in newList))];
+    let newPlugins = [this.buildWrapper(newList[id])
+                      for each (id in Object.keys(newList)) if (!(id in this.plugins))];
+    let matchedIDs = [id for each (id in Object.keys(newList)) if (id in this.plugins)];
+
+    
+    
+    
+    
+    let changedWrappers = [];
+    for (let id of matchedIDs) {
+      let oldWrapper = this.buildWrapper(this.plugins[id]);
+      let newWrapper = this.buildWrapper(newList[id]);
+
+      if (newWrapper.isActive != oldWrapper.isActive) {
+        AddonManagerPrivate.callAddonListeners(newWrapper.isActive ?
+                                               "onEnabling" : "onDisabling",
+                                               newWrapper, false);
+        changedWrappers.push(newWrapper);
+      }
+    }
+
+    
+    for (let plugin of newPlugins) {
+      AddonManagerPrivate.callInstallListeners("onExternalInstall", null,
+                                               plugin, null, false);
+      AddonManagerPrivate.callAddonListeners("onInstalling", plugin, false);
+    }
+
+    
+    for (let plugin of lostPlugins)
+      AddonManagerPrivate.callAddonListeners("onUninstalling", plugin, false);
+
+    this.plugins = newList;
+
+    
+    for (let plugin of newPlugins)
+      AddonManagerPrivate.callAddonListeners("onInstalled", plugin);
+
+    
+    for (let wrapper of changedWrappers) {
+      AddonManagerPrivate.callAddonListeners(wrapper.isActive ?
+                                             "onEnabled" : "onDisabled",
+                                             wrapper);
+    }
+
+    
+    for (let plugin of lostPlugins)
+      AddonManagerPrivate.callAddonListeners("onUninstalled", plugin);
   }
 };
 
---
+++ toolkit/mozapps/extensions/test/xpcshell/test_pluginchange.js
@@ -0,0 +1,290 @@
+
+
+
+
+const LIST_UPDATED_TOPIC     = "plugins-list-updated";
+
+
+var { getIDHashForString } = Components.utils.import("resource://gre/modules/PluginProvider.jsm");
+
+function PluginTag(name, description) {
+  this.name = name;
+  this.description = description;
+}
+
+PluginTag.prototype = {
+  name: null,
+  description: null,
+  version: "1.0",
+  filename: null,
+  fullpath: null,
+  disabled: false,
+  blocklisted: false,
+  clicktoplay: false,
+
+  mimeTypes: [],
+
+  getMimeTypes: function(count) {
+    count.value = this.mimeTypes.length;
+    return this.mimeTypes;
+  }
+};
+
+PLUGINS = [
+  
+  new PluginTag("Java", "A mock Java plugin"),
+
+  
+  new PluginTag("Flash", "A mock Flash plugin"),
+  new PluginTag("Flash", "A mock Flash plugin")
+];
+
+gPluginHost = {
+  
+  getPluginTags: function(count) {
+    count.value = PLUGINS.length;
+    return PLUGINS;
+  },
+
+  QueryInterface: XPCOMUtils.generateQI([AM_Ci.nsIPluginHost])
+};
+
+var PluginHostFactory = {
+  createInstance: function (outer, iid) {
+    if (outer != null)
+      throw Components.results.NS_ERROR_NO_AGGREGATION;
+    return gPluginHost.QueryInterface(iid);
+  }
+};
+
+var registrar = Components.manager.QueryInterface(AM_Ci.nsIComponentRegistrar);
+registrar.registerFactory(Components.ID("{aa6f9fef-cbe2-4d55-a2fa-dcf5482068b9}"), "PluginHost",
+                          "@mozilla.org/plugin/host;1", PluginHostFactory);
+
+
+
+function run_test() {
+  do_test_pending();
+  createAppInfo("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+  startupManager();
+  AddonManager.addAddonListener(AddonListener);
+  AddonManager.addInstallListener(InstallListener);
+
+  run_test_1();
+}
+
+function end_test() {
+  do_test_finished();
+}
+
+function sortAddons(addons) {
+  addons.sort(function(a, b) {
+    return a.name.localeCompare(b.name);
+  });
+}
+
+
+function run_test_1() {
+  AddonManager.getAddonsByTypes(["plugin"], function(addons) {
+    sortAddons(addons);
+
+    do_check_eq(addons.length, 2);
+
+    do_check_eq(addons[0].name, "Flash");
+    do_check_false(addons[0].userDisabled);
+    do_check_eq(addons[1].name, "Java");
+    do_check_false(addons[1].userDisabled);
+
+    run_test_2();
+  });
+}
+
+
+function run_test_2() {
+  
+  let tag = PLUGINS[0];
+  PLUGINS[0] = PLUGINS[2];
+  PLUGINS[2] = PLUGINS[1];
+  PLUGINS[1] = tag;
+
+  Services.obs.notifyObservers(null, LIST_UPDATED_TOPIC, null);
+
+  AddonManager.getAddonsByTypes(["plugin"], function(addons) {
+    sortAddons(addons);
+
+    do_check_eq(addons.length, 2);
+
+    do_check_eq(addons[0].name, "Flash");
+    do_check_false(addons[0].userDisabled);
+    do_check_eq(addons[1].name, "Java");
+    do_check_false(addons[1].userDisabled);
+
+    run_test_3();
+  });
+}
+
+
+function run_test_3() {
+  let tag = new PluginTag("Quicktime", "A mock Quicktime plugin");
+  PLUGINS.push(tag);
+  let id = getIDHashForString(tag.name + tag.description);
+
+  let test_params = {};
+  test_params[id] = [
+    ["onInstalling", false],
+    "onInstalled"
+  ];
+
+  prepare_test(test_params, [
+    "onExternalInstall"
+  ]);
+
+  Services.obs.notifyObservers(null, LIST_UPDATED_TOPIC, null);
+
+  ensure_test_completed();
+
+  AddonManager.getAddonsByTypes(["plugin"], function(addons) {
+    sortAddons(addons);
+
+    do_check_eq(addons.length, 3);
+
+    do_check_eq(addons[0].name, "Flash");
+    do_check_false(addons[0].userDisabled);
+    do_check_eq(addons[1].name, "Java");
+    do_check_false(addons[1].userDisabled);
+    do_check_eq(addons[2].name, "Quicktime");
+    do_check_false(addons[2].userDisabled);
+
+    run_test_4();
+  });
+}
+
+
+function run_test_4() {
+  let tag = PLUGINS.splice(1, 1)[0];
+  let id = getIDHashForString(tag.name + tag.description);
+
+  let test_params = {};
+  test_params[id] = [
+    ["onUninstalling", false],
+    "onUninstalled"
+  ];
+
+  prepare_test(test_params);
+
+  Services.obs.notifyObservers(null, LIST_UPDATED_TOPIC, null);
+
+  ensure_test_completed();
+
+  AddonManager.getAddonsByTypes(["plugin"], function(addons) {
+    sortAddons(addons);
+
+    do_check_eq(addons.length, 2);
+
+    do_check_eq(addons[0].name, "Flash");
+    do_check_false(addons[0].userDisabled);
+    do_check_eq(addons[1].name, "Quicktime");
+    do_check_false(addons[1].userDisabled);
+
+    run_test_5();
+  });
+}
+
+
+function run_test_5() {
+  PLUGINS.splice(0, 1);
+
+  Services.obs.notifyObservers(null, LIST_UPDATED_TOPIC, null);
+
+  ensure_test_completed();
+
+  AddonManager.getAddonsByTypes(["plugin"], function(addons) {
+    sortAddons(addons);
+
+    do_check_eq(addons.length, 2);
+
+    do_check_eq(addons[0].name, "Flash");
+    do_check_false(addons[0].userDisabled);
+    do_check_eq(addons[1].name, "Quicktime");
+    do_check_false(addons[1].userDisabled);
+
+    run_test_6();
+  });
+}
+
+
+function run_test_6() {
+  let oldTag = PLUGINS.splice(0, 1)[0];
+  let newTag = new PluginTag("Flash 2", "A new crash-free Flash!");
+  newTag.disabled = true;
+  PLUGINS.push(newTag);
+
+  let test_params = {};
+  test_params[getIDHashForString(oldTag.name + oldTag.description)] = [
+    ["onUninstalling", false],
+    "onUninstalled"
+  ];
+  test_params[getIDHashForString(newTag.name + newTag.description)] = [
+    ["onInstalling", false],
+    "onInstalled"
+  ];
+
+  prepare_test(test_params, [
+    "onExternalInstall"
+  ]);
+
+  Services.obs.notifyObservers(null, LIST_UPDATED_TOPIC, null);
+
+  ensure_test_completed();
+
+  AddonManager.getAddonsByTypes(["plugin"], function(addons) {
+    sortAddons(addons);
+
+    do_check_eq(addons.length, 2);
+
+    do_check_eq(addons[0].name, "Flash 2");
+    do_check_true(addons[0].userDisabled);
+    do_check_eq(addons[1].name, "Quicktime");
+    do_check_false(addons[1].userDisabled);
+
+    run_test_7();
+  });
+}
+
+
+
+function run_test_7() {
+  PLUGINS[0] = new PluginTag("Quicktime", "A mock Quicktime plugin");
+  PLUGINS[0].disabled = true;
+  PLUGINS[1] = new PluginTag("Flash 2", "A new crash-free Flash!");
+
+  let test_params = {};
+  test_params[getIDHashForString(PLUGINS[0].name + PLUGINS[0].description)] = [
+    ["onDisabling", false],
+    "onDisabled"
+  ];
+  test_params[getIDHashForString(PLUGINS[1].name + PLUGINS[1].description)] = [
+    ["onEnabling", false],
+    "onEnabled"
+  ];
+
+  prepare_test(test_params);
+
+  Services.obs.notifyObservers(null, LIST_UPDATED_TOPIC, null);
+
+  ensure_test_completed();
+
+  AddonManager.getAddonsByTypes(["plugin"], function(addons) {
+    sortAddons(addons);
+
+    do_check_eq(addons.length, 2);
+
+    do_check_eq(addons[0].name, "Flash 2");
+    do_check_false(addons[0].userDisabled);
+    do_check_eq(addons[1].name, "Quicktime");
+    do_check_true(addons[1].userDisabled);
+
+    end_test();
+  });
+}
