f9fd7ce62b88d7aa1a104e8cb11c86bffdf5977b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:40:09 +0000

    Bug 826029 - Assertion in mozPoisonWriteMac due to Mac camera code trying to write a defaults file on exit | Assertion failure: ok, at ../../../xpcom/build/mozPoisonWriteMac.cpp:90. r=ehsan.
    UltraBlame original commit: bfeb3bc3da4e62acdf2754fe8467554bbdfc062f

--- browser/app/nsBrowserApp.cpp
+++ browser/app/nsBrowserApp.cpp
@@ -107,6 +107,7 @@ XRE_SetupDllBlocklistType XRE_SetupDllBlocklist;
 XRE_TelemetryAccumulateType XRE_TelemetryAccumulate;
 XRE_StartupTimelineRecordType XRE_StartupTimelineRecord;
 XRE_mainType XRE_main;
+XRE_DisableWritePoisoningType XRE_DisableWritePoisoning;
 
 static const nsDynamicFunctionLoad kXULFuncs[] = {
     { "XRE_GetFileFromPath", (NSFuncPtr*) &XRE_GetFileFromPath },
@@ -118,6 +119,7 @@ static const nsDynamicFunctionLoad kXULFuncs[] = {
     { "XRE_TelemetryAccumulate", (NSFuncPtr*) &XRE_TelemetryAccumulate },
     { "XRE_StartupTimelineRecord", (NSFuncPtr*) &XRE_StartupTimelineRecord },
     { "XRE_main", (NSFuncPtr*) &XRE_main },
+    { "XRE_DisableWritePoisoning", (NSFuncPtr*) &XRE_DisableWritePoisoning },
     { nullptr, nullptr }
 };
 
@@ -280,5 +282,13 @@ int main(int argc, char* argv[])
   }
 
   XPCOMGlueShutdown();
+
+  
+  
+  
+  
+  
+  XRE_DisableWritePoisoning();
+
   return result;
 }
