95127c769722b923df34b11432c464f66b392e70
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:41:09 +0000

    Bug 922924 - Centralize the code for network per-app metering. r=jduell
    UltraBlame original commit: 5d4d19ebcd70512af5c3b960af94ec2c795001eb

--- netwerk/protocol/http/nsHttpTransaction.h
+++ netwerk/protocol/http/nsHttpTransaction.h
@@ -340,10 +340,6 @@ private:
 
 
 
-public:
-    const static uint64_t NETWORK_STATS_THRESHOLD = 65536;
-
-private:
     uint64_t                           mCountRecv;
     uint64_t                           mCountSent;
     uint32_t                           mAppId;
@@ -351,7 +347,6 @@ private:
     nsMainThreadPtrHandle<nsINetworkInterface> mActiveNetwork;
 #endif
     nsresult                           SaveNetworkStats(bool);
-    void                               GetActiveNetwork();
     void                               CountRecvBytes(uint64_t recvBytes)
     {
         mCountRecv += recvBytes;
