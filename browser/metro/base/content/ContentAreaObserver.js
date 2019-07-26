03401283c60f31938cb618e8d413175c5db5072b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:21:51 +0000

    Bug 883953 - Part 2: Implement infobar behaviours. r=mbrubeck
    UltraBlame original commit: f0f8ef217538590ca9a574ffc1bcabbf9cbeaaed

--- browser/metro/base/content/ContentAreaObserver.js
+++ browser/metro/base/content/ContentAreaObserver.js
@@ -133,6 +133,17 @@ var ContentAreaObserver = {
     let newWidth = width || this.width;
     let newHeight = height || this.contentHeight;
 
+    if (Browser.selectedBrowser) {
+      let notificationBox = Browser.getNotificationBox();
+
+      
+      
+      if (ContextUI.navbarVisible && !notificationBox.notificationsHidden &&
+          notificationBox.allNotifications.length != 0) {
+        newHeight -= Elements.navbar.getBoundingClientRect().height;
+      }
+    }
+
     if (newHeight == oldHeight && newWidth == oldWidth)
       return;
 
