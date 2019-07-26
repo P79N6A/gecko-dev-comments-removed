6998a8178b0a28a83661f300334cb51b45749cd9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:03:06 +0000

    Bug 959742 - Close dialogs when inputs change if there are no buttons on the dialog. r=bnicholson
    UltraBlame original commit: f89196ce3d3ea017636579637a41aa423c4f3432

--- mobile/android/base/prompts/Prompt.java
+++ mobile/android/base/prompts/Prompt.java
@@ -38,7 +38,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class Prompt implements OnClickListener, OnCancelListener, OnItemClickListener {
+public class Prompt implements OnClickListener, OnCancelListener, OnItemClickListener,
+                               PromptInput.OnChangeListener {
     private static final String LOGTAG = "GeckoPromptService";
 
     private String[] mButtons;
@@ -148,6 +149,10 @@ public class Prompt implements OnClickListener, OnCancelListener, OnItemClickLis
 
 
     private void addListResult(final JSONObject result, int which) {
+        if (mAdapter == null) {
+            return;
+        }
+
         try {
             JSONArray selected = new JSONArray();
 
@@ -203,23 +208,7 @@ public class Prompt implements OnClickListener, OnCancelListener, OnItemClickLis
     @Override
     public void onClick(DialogInterface dialog, int which) {
         ThreadUtils.assertOnUiThread();
-        JSONObject ret = new JSONObject();
-        try {
-            addButtonResult(ret, which);
-            addInputValues(ret);
-
-            if (mAdapter != null) {
-                addListResult(ret, which);
-            }
-        } catch(Exception ex) {
-            Log.i(LOGTAG, "Error building return: " + ex);
-        }
-
-        if (dialog != null) {
-            dialog.dismiss();
-        }
-
-        finishDialog(ret);
+        closeDialog(which);
     }
 
     
@@ -275,7 +264,12 @@ public class Prompt implements OnClickListener, OnCancelListener, OnItemClickLis
 
     private void addSingleSelectList(AlertDialog.Builder builder, PromptListItem[] listItems) {
         mAdapter = new PromptListAdapter(mContext, R.layout.select_dialog_singlechoice, listItems);
-        builder.setSingleChoiceItems(mAdapter, mAdapter.getSelectedIndex(), this);
+        builder.setSingleChoiceItems(mAdapter, mAdapter.getSelectedIndex(), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                closeIfNoButtons(which);
+            }
+        });
     }
 
     
@@ -360,6 +354,20 @@ public class Prompt implements OnClickListener, OnCancelListener, OnItemClickLis
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         ThreadUtils.assertOnUiThread();
         mAdapter.toggleSelected(position);
+
+        
+        
+        
+        closeIfNoButtons(position);
+    }
+
+    private boolean closeIfNoButtons(int selected) {
+        ThreadUtils.assertOnUiThread();
+        if (mButtons == null || mButtons.length == 0) {
+            closeDialog(selected);
+            return true;
+        }
+        return false;
     }
 
     
@@ -384,6 +392,20 @@ public class Prompt implements OnClickListener, OnCancelListener, OnItemClickLis
             ret.put("button", -1);
         } catch(Exception ex) { }
         addInputValues(ret);
+        finishDialog(ret);
+    }
+
+    
+
+
+    private void closeDialog(int which) {
+        JSONObject ret = new JSONObject();
+        mDialog.dismiss();
+
+        addButtonResult(ret, which);
+        addListResult(ret, which);
+        addInputValues(ret);
+
         finishDialog(ret);
     }
 
@@ -421,6 +443,7 @@ public class Prompt implements OnClickListener, OnCancelListener, OnItemClickLis
         for (int i = 0; i < mInputs.length; i++) {
             try {
                 mInputs[i] = PromptInput.getInput(inputs.getJSONObject(i));
+                mInputs[i].setListener(this);
             } catch(Exception ex) { }
         }
 
@@ -435,6 +458,15 @@ public class Prompt implements OnClickListener, OnCancelListener, OnItemClickLis
         }
 
         show(title, text, menuitems, choiceMode);
+    }
+
+    
+    @Override
+    public void onChange(PromptInput input) {
+        
+        
+        
+        closeIfNoButtons(-1);
     }
 
     private static JSONArray getSafeArray(JSONObject json, String key) {
