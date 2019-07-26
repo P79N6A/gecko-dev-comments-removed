bd4a8712065be62eb4f7cddf0a3677fa537f7ab0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:25:25 +0000

    Bug 901126 - Split browser_newtab_drag_drop.js into two tests. r=ttaubert
    UltraBlame original commit: 4eb0c1631e83b54266e6c03c1fd977ec780acee6

---
+++ browser/base/content/test/newtab/browser_newtab_drag_drop_ext.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+
+
+function runTests() {
+  
+  yield setLinks("0,1,2,3,4,5,6,7,8");
+  setPinnedLinks(",,,,,,,7,8");
+
+  yield addNewTabPageTab();
+  checkGrid("0,1,2,3,4,5,6,7p,8p");
+
+  yield simulateExternalDrop(0);
+  checkGrid("99p,0,1,2,3,4,5,7p,8p");
+
+  
+  
+  yield setLinks("0,1,2,3,4,5,6,7,8");
+  setPinnedLinks(",,,,,,,7,8");
+
+  yield addNewTabPageTab();
+  checkGrid("0,1,2,3,4,5,6,7p,8p");
+
+  yield simulateExternalDrop(7);
+  checkGrid("0,1,2,3,4,5,7p,99p,8p");
+
+  
+  
+  yield setLinks("0,1,2,3,4,5,6,7,8");
+  setPinnedLinks(",,,,,,,,8");
+
+  yield addNewTabPageTab();
+  checkGrid("0,1,2,3,4,5,6,7,8p");
+
+  yield simulateExternalDrop(7);
+  checkGrid("0,1,2,3,4,5,6,99p,8p");
+
+  
+  
+  yield setLinks("0,1,2,3,4,5,6,7,8");
+  setPinnedLinks("0,1,2,,,,,,");
+
+  yield addNewTabPageTab();
+  checkGrid("0p,1p,2p");
+
+  yield simulateExternalDrop(1);
+  checkGrid("0p,99p,1p,2p,3,4,5,6,7");
+}
