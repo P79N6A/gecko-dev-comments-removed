752f849368443cc5a894a65d25f766107b81868a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:35:14 +0000

    Bug 772365, Part 3: Add B2G-specific customizations to NSS certificate trust database with tests, r=rrelyea r=honzab
    UltraBlame original commit: 6a0ed6484811bc7233c39eec1687bb8ce09cb98e

---
+++ security/manager/ssl/tests/unit/test_signed_apps-marketplace.js
@@ -0,0 +1,56 @@
+"use strict";
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+const Cr = Components.results;
+
+const isB2G = ("@mozilla.org/b2g-keyboard;1" in Components.classes);
+
+Cu.import("resource://gre/modules/FileUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+do_get_profile(); 
+const certdb = Cc["@mozilla.org/security/x509certdb;1"].getService(Ci.nsIX509CertDB);
+
+function run_test() {
+  run_next_test();
+}
+
+function check_open_result(name, expectedRv) {
+  if (expectedRv == Cr.NS_OK && !isB2G) {
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    const NS_ERROR_SEC_ERROR_UNKNOWN_ISSUER = 0x80000000 
+                                            + (    (0x45 + 21) << 16)
+                                            + (-(-0x2000 + 13)      );
+    expectedRv = NS_ERROR_SEC_ERROR_UNKNOWN_ISSUER;
+  }
+
+  return function openSignedJARFileCallback(rv, aZipReader, aSignerCert) {
+    do_print("openSignedJARFileCallback called for " + name);
+    do_check_eq(rv, expectedRv);
+    do_check_eq(aZipReader != null,  Components.isSuccessCode(expectedRv));
+    do_check_eq(aSignerCert != null, Components.isSuccessCode(expectedRv));
+    run_next_test();
+  };
+}
+
+function original_app_path(test_name) {
+  return do_get_file("test_signed_apps/" + test_name + ".zip", false);
+}
+
+add_test(function () {
+  certdb.openSignedJARFileAsync(
+    original_app_path("privileged-app-test-1.0"),
+    check_open_result("privileged-app-test-1.0", Cr.NS_OK));
+});
