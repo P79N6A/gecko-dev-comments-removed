97bb124a265aba3cd220f9c8e80b1026472c4b4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:40:54 +0000

    Bug 826144 - Add MOZ_UTF16_HELPER() intermediary macro to handle UTF-16 stringification and concatenation correctly. r=waldo
    UltraBlame original commit: 5d05c702079709c311ad042edc47666143cd26d1

--- mfbt/Char16.h
+++ mfbt/Char16.h
@@ -28,16 +28,26 @@
 
 
 
-#  define MOZ_UTF16(s) L##s
+#  define MOZ_UTF16_HELPER(s) L##s
 #  include <yvals.h>
 #  define char16_t wchar_t
 #elif defined(__cplusplus) && \
       (__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
    
-#  define MOZ_UTF16(s) u##s
+#  define MOZ_UTF16_HELPER(s) u##s
 #else
 #  error "Char16.h requires C++11 (or something like it) for UTF-16 support."
 #endif
+
+
+
+
+
+
+
+
+
+#define MOZ_UTF16(s) MOZ_UTF16_HELPER(s)
 
 MOZ_STATIC_ASSERT(sizeof(char16_t) == 2, "Is char16_t type 16 bits?");
 MOZ_STATIC_ASSERT(sizeof(MOZ_UTF16('A')) == 2, "Is char literal 16 bits?");
