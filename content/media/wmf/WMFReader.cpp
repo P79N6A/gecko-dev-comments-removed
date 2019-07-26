b1cd85c647fac297c506a47d0aa45faed9fd9022
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:10:33 +0000

    Bug 945645 - Only init WMF video decoding if we have a video container. r=padenot.
    UltraBlame original commit: b276b4e0bbcf967601f970a0ae9e030a70a23126

--- content/media/wmf/WMFReader.cpp
+++ content/media/wmf/WMFReader.cpp
@@ -105,6 +105,7 @@ WMFReader::InitializeDXVA()
   if (!Preferences::GetBool("media.windows-media-foundation.use-dxva", false)) {
     return false;
   }
+  MOZ_ASSERT(mDecoder->GetImageContainer());
 
   
   
@@ -151,7 +152,8 @@ WMFReader::Init(MediaDecoderReader* aCloneDonor)
   rv = mByteStream->Init();
   NS_ENSURE_SUCCESS(rv, rv);
 
-  if (IsVideoContentType(mDecoder->GetResource()->GetContentType())) {
+  if (mDecoder->GetImageContainer() != nullptr &&
+      IsVideoContentType(mDecoder->GetResource()->GetContentType())) {
     mUseHwAccel = InitializeDXVA();
   } else {
     mUseHwAccel = false;
@@ -343,6 +345,11 @@ WMFReader::ConfigureVideoDecoder()
       !SourceReaderHasStream(mSourceReader, MF_SOURCE_READER_FIRST_VIDEO_STREAM)) {
     
     return S_OK;
+  }
+
+  if (!mDecoder->GetImageContainer()) {
+    
+    return mSourceReader->SetStreamSelection(MF_SOURCE_READER_FIRST_VIDEO_STREAM, FALSE);
   }
 
   static const GUID MP4VideoTypes[] = {
