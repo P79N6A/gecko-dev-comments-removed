ded0de33930ef0879785ebbbd30973c9b61fa405
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:40:50 +0000

    Bug 867641 - Find bar covers text
    UltraBlame original commit: 482b8c3e91f76e3ba38706b7c42eb75855355f1a

--- browser/metro/base/content/browser-ui.js
+++ browser/metro/base/content/browser-ui.js
@@ -594,8 +594,6 @@ var BrowserUI = {
       }
       Elements.windowState.setAttribute("viewstate", currViewState);
     }
-    
-    document.getElementById("content-navigator").contentHasChanged();
   },
 
   _titleChanged: function(aBrowser) {
@@ -761,7 +759,7 @@ var BrowserUI = {
     }
 
     
-    let contentHelper = document.getElementById("content-navigator");
+    let contentHelper = Elements.contentNavigator;
     if (contentHelper.isActive) {
       contentHelper.model.hide();
       return;
