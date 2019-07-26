e8b575352918128a2e1bd34fab3395ab15037210
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:59:22 +0000

    Bug 857862 - Tests. r=sfoster
    UltraBlame original commit: fd19f8ea452e01102c58da986898c833c22f98a3

---
+++ browser/metro/base/tests/mochitest/browser_selection_contenteditable.js
@@ -0,0 +1,76 @@
+
+
+
+
+
+"use strict";
+let gWindow = null;
+
+const kMarkerOffsetY = 12;
+const kCommonWaitMs = 5000;
+const kCommonPollMs = 100;
+
+
+
+
+
+gTests.push({
+  desc: "normalize browser",
+  run: function test() {
+    info(chromeRoot + "browser_selection_contenteditable.html");
+    yield addTab(chromeRoot + "browser_selection_contenteditable.html");
+    yield waitForCondition(function () {
+        return !StartUI.isStartPageVisible;
+      }, kCommonWaitMs, kCommonPollMs);
+
+    yield hideContextUI();
+
+    gWindow = Browser.selectedTab.browser.contentWindow;
+
+    InputSourceHelper.isPrecise = false;
+    InputSourceHelper.fireUpdate();
+  },
+});
+
+gTests.push({
+  desc: "simple test to make sure content editable selection works",
+  run: function test() {
+    let div = gWindow.document.getElementById("testdiv");
+    ok(div, "have the div");
+
+    sendElementTap(gWindow, div, 295); 
+
+    yield waitForCondition(function () {
+        return SelectionHelperUI.isCaretUIVisible;
+      }, kCommonWaitMs, kCommonPollMs);
+
+    let xpos = SelectionHelperUI.caretMark.xPos;
+    let ypos = SelectionHelperUI.caretMark.yPos + 10;
+    var touchdrag = new TouchDragAndHold();
+    yield touchdrag.start(gWindow, xpos, ypos, 100, ypos); 
+    touchdrag.end();
+    yield waitForCondition(function () {
+        return !SelectionHelperUI.hasActiveDrag;
+      }, kCommonWaitMs, kCommonPollMs);
+    yield SelectionHelperUI.pingSelectionHandler();
+    let str = getTrimmedSelection(gWindow).toString();
+    is(str, "sending an email in the outlook.com", "selected string matches");
+  },
+  tearDown: function () {
+    emptyClipboard();
+    if (gWindow)
+      clearSelection(gWindow);
+    yield waitForCondition(function () {
+      return !SelectionHelperUI.isSelectionUIVisible;
+    }, kCommonWaitMs, kCommonPollMs);
+  }
+});
+
+function test() {
+  if (!isLandscapeMode()) {
+    todo(false, "browser_selection_tests need landscape mode to run.");
+    return;
+  }
+  setDevPixelEqualToPx();
+  runTests();
+}
