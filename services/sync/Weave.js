855972a53159778ac433e6a429f1e7218d4a24d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:06:02 +0000

    Bug 949695 - integrate weave with browserid_identity on elm.
    UltraBlame original commit: 90dc9c1209d9056c4f2b6abba33803a68e899827

--- services/sync/Weave.js
+++ services/sync/Weave.js
@@ -9,6 +9,7 @@ const Cu = Components.utils;
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/FileUtils.jsm");
+Cu.import("resource://services-sync/util.js");
 
 const SYNC_PREFS_BRANCH = "services.sync.";
 
@@ -64,12 +65,59 @@ WeaveService.prototype = {
     Weave.Service;
   },
 
+  maybeInitWithFxAccountsAndEnsureLoaded: function() {
+    Components.utils.import("resource://services-sync/main.js");
+    
+    Cu.import("resource://gre/modules/FxAccounts.jsm");
+
+    
+    
+    
+    return fxAccounts.getSignedInUser().then(
+      (accountData) => {
+        if (accountData) {
+          Cu.import("resource://services-sync/browserid_identity.js");
+          Cu.import("resource://services-common/tokenserverclient.js");
+          
+          
+          
+          
+          
+          Weave.Service.identity = Weave.Status._authManager = new BrowserIDManager(fxAccounts, new TokenServerClient()),
+          
+          
+          
+          Weave.Service.identity.initWithLoggedInUser().then(function () {
+            
+            Weave.Service.clusterURL = Weave.Service.identity.clusterURL;
+            
+            
+            if (Weave.Status.checkSetup() != Weave.CLIENT_NOT_CONFIGURED) {
+              
+              Svc.Obs.notify("weave:service:setup-complete");
+              
+              
+              Weave.Utils.nextTick(Weave.Service.sync, Weave.Service);
+              this.ensureLoaded();
+            }
+          }.bind(this));
+        } else if (Weave.Status.checkSetup() != Weave.CLIENT_NOT_CONFIGURED) {
+          
+          this.ensureLoaded();
+        }
+      },
+      (err) => {dump("err in getting logged in account "+err.message)}
+    ).then(null, (err) => {dump("err in processing logged in account "+err.message)});
+  },
+
   observe: function (subject, topic, data) {
     switch (topic) {
     case "app-startup":
       let os = Cc["@mozilla.org/observer-service;1"].
                getService(Ci.nsIObserverService);
       os.addObserver(this, "final-ui-startup", true);
+      os.addObserver(this, "fxaccounts:onlogin", true);
+      os.addObserver(this, "fxaccounts:onlogout", true);
       break;
 
     case "final-ui-startup":
@@ -79,7 +127,6 @@ WeaveService.prototype = {
         notify: function() {
           
           let prefs = Services.prefs.getBranch(SYNC_PREFS_BRANCH);
-
           if (!prefs.prefHasUserValue("username")) {
             return;
           }
@@ -89,12 +136,32 @@ WeaveService.prototype = {
           
           
           
-          Components.utils.import("resource://services-sync/main.js");
-          if (Weave.Status.checkSetup() != Weave.CLIENT_NOT_CONFIGURED) {
-            this.ensureLoaded();
-          }
+          this.maybeInitWithFxAccountsAndEnsureLoaded();
         }.bind(this)
-      }, 10000, Ci.nsITimer.TYPE_ONE_SHOT);
+      }, 1000, Ci.nsITimer.TYPE_ONE_SHOT);
+      break;
+
+    case 'fxaccounts:onlogin':
+        
+        
+        
+        
+        
+        Components.utils.import("resource://services-sync/main.js"); 
+        Weave.Svc.Prefs.set("firstSync", "resetClient");
+        this.maybeInitWithFxAccountsAndEnsureLoaded().then(() => {
+          
+          
+          
+          
+          
+        });
+      break;
+    case 'fxaccounts:onlogout':
+      Components.utils.import("resource://services-sync/main.js"); 
+      
+      
+      
       break;
     }
   }
