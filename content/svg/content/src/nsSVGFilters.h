2a28ffb592d02d69feedf77265d6e8b3f6cd6703
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:14:59 +0000

    Followup to bug 840417 to fix static checking errors, r=bz
    UltraBlame original commit: 62face029ee06f2b849c9b4e8ea030f29ddf8b9e

--- content/svg/content/src/nsSVGFilters.h
+++ content/svg/content/src/nsSVGFilters.h
@@ -137,6 +137,8 @@ public:
   NS_IMETHOD_(bool) IsAttributeMapped(const nsIAtom* aAttribute) const;
 
   
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const MOZ_OVERRIDE = 0;
+
   virtual bool HasValidDimensions() const;
 
   bool IsNodeOfType(uint32_t aFlags) const
@@ -239,6 +241,8 @@ protected:
     : SVGFEUnstyledElementBase(aNodeInfo) {}
 
 public:
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const MOZ_OVERRIDE = 0;
+
   
   
   virtual bool AttributeAffectsRendering(
