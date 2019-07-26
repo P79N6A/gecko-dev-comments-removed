31cd4ca920ee40b175266a4ba1739d0b6ab25266
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:31:57 +0000

    Bug 778093 - Part 9/9: xpcshell test cases, r=hsinyi
    UltraBlame original commit: 8c9d5a11ea3e292c5d60c9e3d40ae81622615124

--- dom/system/gonk/tests/header_helpers.js
+++ dom/system/gonk/tests/header_helpers.js
@@ -145,7 +145,7 @@ function newRadioInterfaceLayer() {
 
 
 
-function do_check_throws(func, result, stack)
+function do_check_throws(func, message, stack)
 {
   if (!stack)
     stack = Components.stack.caller;
@@ -153,13 +153,15 @@ function do_check_throws(func, result, stack)
   try {
     func();
   } catch (exc) {
-    if (exc.result == result)
+    if (exc.message === message) {
       return;
-    do_throw("expected result " + result + ", caught " + exc, stack);
+    }
+    do_throw("expecting exception '" + message
+             + "', caught '" + exc.message + "'", stack);
   }
 
-  if (result) {
-    do_throw("expected result " + result + ", none thrown", stack);
+  if (message) {
+    do_throw("expecting exception '" + message + "', none thrown", stack);
   }
 }
 
--- dom/system/gonk/tests/test_ril_worker_buf.js
+++ dom/system/gonk/tests/test_ril_worker_buf.js
@@ -48,7 +48,7 @@ function add_test_incoming_parcel(parcel, handler) {
 
 add_test_incoming_parcel(null,
   function test_normal_parcel_handling(worker) {
-    do_check_throws(function normal_handler(worker) {
+    do_check_throws(function normal_handler() {
       
       worker.Buf.readUint32();
     });
@@ -61,7 +61,7 @@ add_test_incoming_parcel(null,
     do_check_throws(function under_read_handler() {
       
       worker.Buf.readUint16();
-    }, false);
+    });
   }
 );
 
@@ -78,7 +78,27 @@ add_test_incoming_parcel(null,
     do_check_throws(function over_read_handler() {
       
       buf.readUint8();
-    }, new Error("Trying to read data beyond the parcel end!").result);
+    },"Trying to read data beyond the parcel end!");
   }
 );
 
+
+add_test_incoming_parcel(null,
+  function test_buf_readUint8Array(worker) {
+    let buf = worker.Buf;
+
+    let u8array = buf.readUint8Array(1);
+    do_check_eq(u8array instanceof Uint8Array, true);
+    do_check_eq(u8array.length, 1);
+    do_check_eq(buf.readAvailable, 3);
+
+    u8array = buf.readUint8Array(2);
+    do_check_eq(u8array.length, 2);
+    do_check_eq(buf.readAvailable, 1);
+
+    do_check_throws(function over_read_handler() {
+      
+      u8array = buf.readUint8Array(2);
+    }, "Trying to read data beyond the parcel end!");
+  }
+);
