c02065bc4f80a3e07d1f0af16c244bd33867783a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:11:34 +0000

    Bug 796733 - Re-order the calling sequence of disable FM radio and change audio state. r=mwu
    UltraBlame original commit: e513d3aa6a1e5491e3ef47c50df1747d76092009

--- dom/fm/FMRadio.cpp
+++ dom/fm/FMRadio.cpp
@@ -144,14 +144,16 @@ NS_IMETHODIMP FMRadio::Enable(nsIFMRadioSettings *settings)
 
 NS_IMETHODIMP FMRadio::Disable()
 {
+  
+  
+  
+  DisableFMRadio();
+  
   nsCOMPtr<nsIAudioManager> audioManager =
     do_GetService(NS_AUDIOMANAGER_CONTRACTID);
   NS_ENSURE_TRUE(audioManager, NS_OK);
 
   audioManager->SetFmRadioAudioEnabled(false);
-
-  DisableFMRadio();
-
   return NS_OK;
 }
 
