8c0569b5b827c32eb1b437188a2475cdbe137b32
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:28:59 +0000

    Bug 902966 - Pressing escape while editing a style property should undo current changes; r=mratcliffe
    UltraBlame original commit: 5de48b36c5747af1ae008ecafceb88c23e64127c

---
+++ browser/devtools/styleinspector/test/browser_ruleview_livepreview.js
@@ -0,0 +1,90 @@
+
+
+
+
+
+
+let doc;
+let testElement;
+let ruleWindow;
+let ruleView;
+let inspector;
+
+
+
+
+
+
+let testData = [
+  {value: "inline", expected: "inline"},
+  {value: "something", expected: "inline"}
+];
+
+function startTest()
+{
+  let style = '#testid {display:block;}';
+
+  let styleNode = addStyle(doc, style);
+  doc.body.innerHTML = '<div id="testid">Styled Node</div><span>inline element</span>';
+  testElement = doc.getElementById("testid");
+
+  openRuleView((aInspector, aRuleView) => {
+    inspector = aInspector;
+    ruleView = aRuleView;
+    ruleWindow = aRuleView.doc.defaultView;
+    inspector.selection.setNode(testElement);
+    inspector.once("inspector-updated", () => loopTestData(0));
+  });
+}
+
+function loopTestData(index)
+{
+  if(index === testData.length) {
+    finishTest();
+    return;
+  }
+
+  let idRuleEditor = ruleView.element.children[1]._ruleEditor;
+  let propEditor = idRuleEditor.rule.textProps[0].editor;
+  waitForEditorFocus(propEditor.element, function(aEditor) {
+    is(inplaceEditor(propEditor.valueSpan), aEditor, "Focused editor should be the value.");
+
+    
+    for (let ch of testData[index].value) {
+      EventUtils.sendChar(ch, ruleWindow);
+    }
+
+    
+    executeSoon(() => {
+      is(content.getComputedStyle(testElement).display,
+        testData[index].expected,
+        "Element should be previewed as " + testData[index].expected);
+
+      EventUtils.synthesizeKey("VK_RETURN", {});
+      loopTestData(index + 1);
+    });
+  });
+
+  EventUtils.synthesizeMouse(propEditor.valueSpan, 1, 1, {}, ruleWindow);
+}
+
+function finishTest()
+{
+  inspector = ruleWindow = ruleView = null;
+  doc = null;
+  gBrowser.removeCurrentTab();
+  finish();
+}
+
+function test()
+{
+  waitForExplicitFinish();
+  gBrowser.selectedTab = gBrowser.addTab();
+  gBrowser.selectedBrowser.addEventListener("load", function changedValues_load(evt) {
+    gBrowser.selectedBrowser.removeEventListener(evt.type, changedValues_load, true);
+    doc = content.document;
+    waitForFocus(startTest, content);
+  }, true);
+
+  content.location = "data:text/html,test rule view live preview on user changes";
+}
