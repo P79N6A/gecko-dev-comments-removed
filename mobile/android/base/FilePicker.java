94b6a2f046a87c8c64ff301c2e88317a341a478d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:07:52 +0000

    Bug 942611 - Store temp files in Android's cache dir. r=rewman
    UltraBlame original commit: 8abcaed46e5139d07a9a7a8135c74a162976740f

--- mobile/android/base/FilePicker.java
+++ mobile/android/base/FilePicker.java
@@ -202,7 +202,7 @@ public class FilePicker implements GeckoEventListener {
 
 
     protected void showFilePickerAsync(String mimeType, final ResultHandler handler) {
-        final FilePickerResultHandler fileHandler = new FilePickerResultHandler(handler);
+        final FilePickerResultHandler fileHandler = new FilePickerResultHandler(handler, mContext);
         getFilePickerIntentAsync(mimeType, fileHandler, new IntentHandler() {
             @Override
             public void gotIntent(Intent intent) {
