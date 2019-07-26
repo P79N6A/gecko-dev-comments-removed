a5639313dfbfb8fcfcd95532ff1fecd16a0d0100
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:06:21 +0000

    Bug 979951: Always preserve the worker wrapper to ensure that we have something to tell us what compartment to enter. r=bent
    UltraBlame original commit: b077aaea25e77f121ba0dada32f66520cfa5e7fb

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -2142,7 +2142,15 @@ WorkerPrivateParent<Derived>::WrapObject(JSContext* aCx,
 
   AssertIsOnParentThread();
 
-  return WorkerBinding::Wrap(aCx, aScope, ParentAsWorkerPrivate());
+  
+  
+  JS::Rooted<JSObject*> wrapper(aCx,
+    WorkerBinding::Wrap(aCx, aScope, ParentAsWorkerPrivate()));
+  if (wrapper) {
+    MOZ_ALWAYS_TRUE(TryPreserveWrapper(wrapper));
+  }
+
+  return wrapper;
 }
 
 template <class Derived>
