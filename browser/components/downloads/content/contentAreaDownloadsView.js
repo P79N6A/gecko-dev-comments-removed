d01118def09331e52007afede0ed90a10f6cc0ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:39:22 +0000

    Bug 825295 - Do not display the Places downloads in the in-content download views opened in private windows; r=Mano
    UltraBlame original commit: a9e417c899924e5b3df12437fbae9aa92c6a295a

--- browser/components/downloads/content/contentAreaDownloadsView.js
+++ browser/components/downloads/content/contentAreaDownloadsView.js
@@ -2,9 +2,14 @@
 
 
 
+Components.utils.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
+
 let ContentAreaDownloadsView = {
   init: function CADV_init() {
     let view = new DownloadsPlacesView(document.getElementById("downloadsRichListBox"));
-    view.place = "place:transition=7&sort=4";
+    
+    if (!PrivateBrowsingUtils.isWindowPrivate(window)) {
+      view.place = "place:transition=7&sort=4";
+    }
   }
 };
