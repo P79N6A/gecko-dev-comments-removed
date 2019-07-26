227b7ff473c3e3c822a80055b3f1018abd197acd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:28:45 +0000

    Bug 905479 - Part 4: Add test for change call barring password. r=yoshi, r=hsinyi
    UltraBlame original commit: a15eb6b9e7a6d8a15d656e8a34171ebc9401427b

---
+++ dom/network/tests/marionette/test_call_barring_change_password.js
@@ -0,0 +1,63 @@
+
+
+
+MARIONETTE_TIMEOUT = 60000;
+
+SpecialPowers.addPermission("mobileconnection", true, document);
+
+
+
+let ifr = document.createElement("iframe");
+let connection;
+ifr.onload = function() {
+  connection = ifr.contentWindow.navigator.mozMobileConnection;
+
+  ok(connection instanceof ifr.contentWindow.MozMobileConnection,
+     "connection is instanceof " + connection.constructor);
+
+  setTimeout(testChangeCallBarringPasswordWithFailure, 0);
+};
+document.body.appendChild(ifr);
+
+function testChangeCallBarringPasswordWithFailure() {
+  
+  let options = [
+    {pin: null, newPin: '0000'},
+    {pin: '0000', newPin: null},
+    {pin: null, newPin: null},
+    {pin: '000', newPin: '0000'},
+    {pin: '00000', newPin: '1111'},
+    {pin: 'abcd', newPin: 'efgh'},
+  ];
+
+  function do_test() {
+    for (let i = 0; i < options.length; i++) {
+      let request = connection.changeCallBarringPassword(options[i]);
+
+      request.onsuccess = function() {
+        ok(false, 'Unexpected result.');
+        setTimeout(cleanUp , 0);
+      };
+
+      request.onerror = function() {
+        ok(request.error.name === 'InvalidPassword', 'InvalidPassword');
+        if (i >= options.length) {
+          setTimeout(testChangeCallBarringPasswordWithSuccess, 0);
+        }
+      };
+    }
+  }
+
+  do_test();
+}
+
+function testChangeCallBarringPasswordWithSuccess() {
+  
+  
+  setTimeout(cleanUp , 0);
+}
+
+function cleanUp() {
+  SpecialPowers.removePermission("mobileconnection", document);
+  finish();
+}
