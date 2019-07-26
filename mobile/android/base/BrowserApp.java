c50ce842df3016bc5e34d08f756f0ceed78c4acb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:21:28 +0000

    Bug 861138: Remove landscape resources for browser-toolbar. [r=mfinkle]
    UltraBlame original commit: 0921c42708f22ed87e554a1bc8573b8ec5a55876

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -650,21 +650,6 @@ abstract public class BrowserApp extends GeckoApp
 
     @Override
     public void refreshChrome() {
-        
-        if (Build.VERSION.SDK_INT >= 14 && !HardwareUtils.isTablet()) {
-            int index = mMainLayout.indexOfChild(mBrowserToolbar.getLayout());
-            mMainLayout.removeViewAt(index);
-
-            LinearLayout actionBar = (LinearLayout) getActionBarLayout();
-            mMainLayout.addView(actionBar, index);
-            mBrowserToolbar.from(actionBar);
-            mBrowserToolbar.refresh();
-
-            
-            if (mDoorHangerPopup != null)
-                mDoorHangerPopup.setAnchor(mBrowserToolbar.mFavicon);
-        }
-
         invalidateOptionsMenu();
         updateSideBarState();
         mTabsPanel.refresh();
