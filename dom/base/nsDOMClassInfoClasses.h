1f5be51d88943c9696b8d27f6a5c915aa1665205
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:04:31 +0000

    Bug 847116: WebIDL bindings for SVGFEBlendElement r=Ms2ger
    UltraBlame original commit: 35f383c97e07ad0fdf8c685a15388a69d7fddca9

--- dom/base/nsDOMClassInfoClasses.h
+++ dom/base/nsDOMClassInfoClasses.h
@@ -121,7 +121,6 @@ DOMCI_CLASS(SVGDocument)
 
 
 DOMCI_CLASS(TimeEvent)
-DOMCI_CLASS(SVGFEBlendElement)
 DOMCI_CLASS(SVGFEColorMatrixElement)
 DOMCI_CLASS(SVGFEComponentTransferElement)
 DOMCI_CLASS(SVGFECompositeElement)
