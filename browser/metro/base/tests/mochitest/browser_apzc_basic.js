02caca9089cf4b7cb734f9cbb97fe27cdcc05cd6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:59:35 +0000

    Bug 945765 - Add some basic front end apzc tests to catch regressions. r=mbrubeck
    UltraBlame original commit: 4ad9d2b82a4c54113f40151f6263d66d08e743e8

---
+++ browser/metro/base/tests/mochitest/browser_apzc_basic.js
@@ -0,0 +1,117 @@
+
+
+
+
+"use strict";
+
+function test() {
+  if (!isLandscapeMode()) {
+    todo(false, "browser_snapped_tests need landscape mode to run.");
+    return;
+  }
+
+  runTests();
+}
+
+let kTransformTimeout = 5000;
+
+let gEdit = null;
+let tabAdded = false;
+
+function setUp() {
+  if (!tabAdded) {
+    yield addTab(chromeRoot + "res/textdivs01.html");
+    tabAdded = true;
+  }
+  yield hideContextUI();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gTests.push({
+  desc: "native long tap works",
+  setUp: setUp,
+  run: function() {
+    let edit = Browser.selectedBrowser.contentDocument.getElementById("textinput");
+    let promise = waitForEvent(document, "popupshown");
+    sendNativeLongTap(edit);
+    yield promise;
+    ContextMenuUI.hide();
+  },
+  tearDown: function () {
+    clearNativeTouchSequence();
+  }
+});
+
+gTests.push({
+  desc: "double tap transforms",
+  setUp: setUp,
+  run: function() {
+    let beginPromise = waitForObserver("apzc-transform-begin", kTransformTimeout);
+    let endPromise = waitForObserver("apzc-transform-end", kTransformTimeout);
+
+    sendNativeDoubleTap(Browser.selectedBrowser.contentDocument.getElementById("second"));
+
+    yield beginPromise;
+    yield endPromise;
+
+    beginPromise = waitForObserver("apzc-transform-begin", kTransformTimeout);
+    endPromise = waitForObserver("apzc-transform-end", kTransformTimeout);
+
+    sendNativeDoubleTap(Browser.selectedBrowser.contentDocument.getElementById("second"));
+
+    yield beginPromise;
+    yield endPromise;
+  },
+  tearDown: function () {
+    clearNativeTouchSequence();
+  }
+});
+
+gTests.push({
+  desc: "scroll transforms",
+  setUp: setUp,
+  run: function() {
+    let beginPromise = waitForObserver("apzc-transform-begin", kTransformTimeout);
+    let endPromise = waitForObserver("apzc-transform-end", kTransformTimeout);
+
+    var touchdrag = new TouchDragAndHold();
+    touchdrag.useNativeEvents = true;
+    touchdrag.nativePointerId = 1;
+    yield touchdrag.start(Browser.selectedTab.browser.contentWindow,
+                          10, 100, 10, 10);
+    touchdrag.end();
+
+    yield beginPromise;
+    yield endPromise;
+  },
+  tearDown: function () {
+    clearNativeTouchSequence();
+  }
+});
