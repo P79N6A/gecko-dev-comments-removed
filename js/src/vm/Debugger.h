ba86e9710a84f3cd49129ae1cf165b1e846a2ab5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:09:10 +0000

    Backed out changeset 7de39a071ca6 (bug 981167) for Test Bustage on a CLOSED TREE
    UltraBlame original commit: fb23f20f9c6cdebb4a674851bb4cff1c100c5b63

--- js/src/vm/Debugger.h
+++ js/src/vm/Debugger.h
@@ -370,13 +370,6 @@ class Debugger : private mozilla::LinkedListElement<Debugger>
 
     void fireNewScript(JSContext *cx, HandleScript script);
 
-    
-
-
-
-    bool getScriptFrameWithIter(JSContext *cx, AbstractFramePtr frame,
-                                const ScriptFrameIter *maybeIter, MutableHandleValue vp);
-
     inline Breakpoint *firstBreakpoint() const;
 
     static inline Debugger *fromOnNewGlobalObjectWatchersLink(JSCList *link);
@@ -490,9 +483,7 @@ class Debugger : private mozilla::LinkedListElement<Debugger>
 
 
 
-    bool getScriptFrame(JSContext *cx, AbstractFramePtr frame, MutableHandleValue vp) {
-        return getScriptFrameWithIter(cx, frame, nullptr, vp);
-    }
+    bool getScriptFrame(JSContext *cx, AbstractFramePtr frame, MutableHandleValue vp);
 
     
 
@@ -503,7 +494,11 @@ class Debugger : private mozilla::LinkedListElement<Debugger>
 
 
     bool getScriptFrame(JSContext *cx, const ScriptFrameIter &iter, MutableHandleValue vp) {
-        return getScriptFrameWithIter(cx, iter.abstractFramePtr(), &iter, vp);
+        AbstractFramePtr data = iter.copyDataAsAbstractFramePtr();
+        if (!data || !getScriptFrame(cx, iter.abstractFramePtr(), vp))
+            return false;
+        vp.toObject().setPrivate(data.raw());
+        return true;
     }
 
     
