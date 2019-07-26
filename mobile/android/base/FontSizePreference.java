ddbce93f625b11bac55d928520702e1bcfa3f955
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:28:59 +0000

    Bug 788106 - Band-aid ArrayIndexOutOfBoundsException. r=bnicholson
    UltraBlame original commit: 296a47893d907bb3e1af16fe4b1155379826d89f

--- mobile/android/base/FontSizePreference.java
+++ mobile/android/base/FontSizePreference.java
@@ -81,7 +81,8 @@ class FontSizePreference extends DialogPreference {
         setButtonState(mPreviewFontIndex);
         mDecreaseFontButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                updatePreviewFontSize(mFontTwipValues[--mPreviewFontIndex]);
+                mPreviewFontIndex = Math.max(mPreviewFontIndex - 1, 0);
+                updatePreviewFontSize(mFontTwipValues[mPreviewFontIndex]);
                 mIncreaseFontButton.setEnabled(true);
                 
                 if (mPreviewFontIndex == 0) {
@@ -91,7 +92,8 @@ class FontSizePreference extends DialogPreference {
         });
         mIncreaseFontButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                updatePreviewFontSize(mFontTwipValues[++mPreviewFontIndex]);
+                mPreviewFontIndex = Math.min(mPreviewFontIndex + 1, mFontTwipValues.length - 1);
+                updatePreviewFontSize(mFontTwipValues[mPreviewFontIndex]);
 
                 mDecreaseFontButton.setEnabled(true);
                 
