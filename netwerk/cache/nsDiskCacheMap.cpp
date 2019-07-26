236a2e5642f8869dac24fc58599bb5f109dd6fc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:15:15 +0000

    Bug 854105 - Avoid gcc 4.7 complaining about array out of bounds in nsDiskCacheMap::WriteDataCacheBlocks. r=michal
    UltraBlame original commit: 072adf9514eba89472eb832418aeb1a95364e2b8

--- netwerk/cache/nsDiskCacheMap.cpp
+++ netwerk/cache/nsDiskCacheMap.cpp
@@ -1034,7 +1034,10 @@ nsDiskCacheMap::WriteDataCacheBlocks(nsDiskCacheBinding * binding, char * buffer
     int32_t   startBlock = 0;
 
     if (size > 0) {
-        while (1) {
+        
+        
+        MOZ_ASSERT(fileIndex);
+        while (fileIndex) {
             uint32_t  blockSize  = GetBlockSizeForIndex(fileIndex);
             blockCount = ((size - 1) / blockSize) + 1;
 
