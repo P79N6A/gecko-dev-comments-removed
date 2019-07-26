171ea2463f1ab2d539f470388f86ee7d3013141b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:01:56 +0000

    Backout 66f4df0b1cb8 because the commit message got mangled; r=me
    UltraBlame original commit: 951090191c431bceb89dcd7656bc4e9248d4fcaa

--- js/src/frontend/TokenStream.cpp
+++ js/src/frontend/TokenStream.cpp
@@ -274,6 +274,7 @@ TokenStream::TokenStream(ExclusiveContext *cx, const CompileOptions &options,
     prevLinebase(NULL),
     userbuf(cx, base - options.column, length + options.column), 
     filename(options.filename),
+    sourceURL_(NULL),
     sourceMapURL_(NULL),
     tokenbuf(cx),
     cx(cx),
@@ -333,6 +334,7 @@ TokenStream::TokenStream(ExclusiveContext *cx, const CompileOptions &options,
 
 TokenStream::~TokenStream()
 {
+    js_free(sourceURL_);
     js_free(sourceMapURL_);
 
     JS_ASSERT_IF(originPrincipals, originPrincipals->refcount);
@@ -807,7 +809,7 @@ CharsMatch(const jschar *p, const char *q) {
 }
 
 bool
-TokenStream::getSourceMappingURL(bool isMultiline, bool shouldWarnDeprecated)
+TokenStream::getDirectives(bool isMultiline, bool shouldWarnDeprecated)
 {
     
     
@@ -816,15 +818,29 @@ TokenStream::getSourceMappingURL(bool isMultiline, bool shouldWarnDeprecated)
     
     
     
+
+    if (!getSourceURL(isMultiline, shouldWarnDeprecated))
+        return false;
+    if (!getSourceMappingURL(isMultiline, shouldWarnDeprecated))
+        return false;
+
+    return true;
+}
+
+bool
+TokenStream::getDirective(bool isMultiline, bool shouldWarnDeprecated,
+                          const char *directive, int directiveLength,
+                          const char *errorMsgPragma, jschar **destination) {
+    JS_ASSERT(directiveLength <= 18);
     jschar peeked[18];
     int32_t c;
 
-    if (peekChars(18, peeked) && CharsMatch(peeked, " sourceMappingURL=")) {
-        if (shouldWarnDeprecated && !reportWarning(JSMSG_DEPRECATED_SOURCE_MAP)) {
+    if (peekChars(directiveLength, peeked) && CharsMatch(peeked, directive)) {
+        if (shouldWarnDeprecated &&
+            !reportWarning(JSMSG_DEPRECATED_PRAGMA, errorMsgPragma))
             return false;
-        }
 
-        skipChars(18);
+        skipChars(directiveLength);
         tokenbuf.clear();
 
         while ((c = peekChar()) && c != EOF && !IsSpaceOrBOM2(c)) {
@@ -844,17 +860,38 @@ TokenStream::getSourceMappingURL(bool isMultiline, bool shouldWarnDeprecated)
             
             return true;
 
-        size_t sourceMapURLLength = tokenbuf.length();
+        size_t length = tokenbuf.length();
 
-        js_free(sourceMapURL_);
-        sourceMapURL_ = cx->pod_malloc<jschar>(sourceMapURLLength + 1);
-        if (!sourceMapURL_)
+        js_free(*destination);
+        *destination = cx->pod_malloc<jschar>(length + 1);
+        if (!*destination)
             return false;
 
-        PodCopy(sourceMapURL_, tokenbuf.begin(), sourceMapURLLength);
-        sourceMapURL_[sourceMapURLLength] = '\0';
+        PodCopy(*destination, tokenbuf.begin(), length);
+        (*destination)[length] = '\0';
     }
+
     return true;
+}
+
+bool
+TokenStream::getSourceURL(bool isMultiline, bool shouldWarnDeprecated)
+{
+    
+    
+
+    return getDirective(isMultiline, shouldWarnDeprecated, " sourceURL=", 11,
+                        "sourceURL", &sourceURL_);
+}
+
+bool
+TokenStream::getSourceMappingURL(bool isMultiline, bool shouldWarnDeprecated)
+{
+    
+    
+
+    return getDirective(isMultiline, shouldWarnDeprecated, " sourceMappingURL=", 18,
+                        "sourceMappingURL", &sourceMapURL_);
 }
 
 JS_ALWAYS_INLINE Token *
@@ -1506,7 +1543,8 @@ TokenStream::getTokenInternal(Modifier modifier)
         if (matchChar('/')) {
             c = peekChar();
             if (c == '@' || c == '#') {
-                if (!getSourceMappingURL(false, getChar() == '@'))
+                bool shouldWarn = getChar() == '@';
+                if (!getDirectives(false, shouldWarn))
                     goto error;
             }
 
@@ -1524,7 +1562,8 @@ TokenStream::getTokenInternal(Modifier modifier)
             while ((c = getChar()) != EOF &&
                    !(c == '*' && matchChar('/'))) {
                 if (c == '@' || c == '#') {
-                    if (!getSourceMappingURL(true, c == '@'))
+                    bool shouldWarn = c == '@';
+                    if (!getDirectives(true, shouldWarn))
                         goto error;
                 }
             }
--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -304,6 +304,7 @@ class ScriptSource
     uint32_t length_;
     uint32_t compressedLength_;
     char *filename_;
+    jschar *sourceURL_;
     jschar *sourceMapURL_;
     JSPrincipals *originPrincipals_;
 
@@ -320,6 +321,7 @@ class ScriptSource
         length_(0),
         compressedLength_(0),
         filename_(NULL),
+        sourceURL_(NULL),
         sourceMapURL_(NULL),
         originPrincipals_(originPrincipals),
         sourceRetrievable_(false),
@@ -341,7 +343,7 @@ class ScriptSource
                        uint32_t length,
                        bool argumentsNotIncluded,
                        SourceCompressionTask *tok);
-    void setSource(const jschar *src, size_t length);
+    void setSource(const jschar *src, uint32_t length);
     bool ready() const { return ready_; }
     void setSourceRetrievable() { sourceRetrievable_ = true; }
     bool sourceRetrievable() const { return sourceRetrievable_; }
@@ -366,6 +368,11 @@ class ScriptSource
     const char *filename() const {
         return filename_;
     }
+
+    
+    bool setSourceURL(ExclusiveContext *cx, const jschar *sourceURL);
+    const jschar *sourceURL();
+    bool hasSourceURL() const { return sourceURL_ != NULL; }
 
     
     bool setSourceMapURL(ExclusiveContext *cx, const jschar *sourceMapURL);
