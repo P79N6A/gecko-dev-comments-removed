90d4c33df4a23817d975271a8882f23153624741
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:29:47 +0000

    Bug 963771. Remove code accidentally landed that was only ever intended for local debugging.
    UltraBlame original commit: 72bcdad2b539a318d0370b06f7c19a46a495c171

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -2400,7 +2400,7 @@ ScrollFrameHelper::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
   
   
   
-  mShouldBuildScrollableLayer = usingDisplayport || nsContentUtils::HasScrollgrab(mOuter->GetContent()) || !mOuter->PresContext()->IsChrome();
+  mShouldBuildScrollableLayer = usingDisplayport || nsContentUtils::HasScrollgrab(mOuter->GetContent());
   bool shouldBuildLayer = false;
   if (mShouldBuildScrollableLayer) {
     shouldBuildLayer = true;
