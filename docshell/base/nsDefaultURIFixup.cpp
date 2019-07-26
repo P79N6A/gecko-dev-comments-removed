2e6c98afe42ffe660339eea3c3f21244f3329f87
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:15:07 +0000

    Bug 823230 - (Part 1) Pass search term along with "keyword-search" notification. r=gavin
    UltraBlame original commit: 26bc83f8c555135569a0ece338104c1f7994b6ba

--- docshell/base/nsDefaultURIFixup.cpp
+++ docshell/base/nsDefaultURIFixup.cpp
@@ -363,9 +363,7 @@ NS_IMETHODIMP nsDefaultURIFixup::KeywordToURI(const nsACString& aKeyword,
                 
                 nsCOMPtr<nsIObserverService> obsSvc = mozilla::services::GetObserverService();
                 if (obsSvc) {
-                  nsAutoString name;
-                  defaultEngine->GetName(name);
-                  obsSvc->NotifyObservers(nullptr, "keyword-search", name.get());
+                    obsSvc->NotifyObservers(defaultEngine, "keyword-search", NS_ConvertUTF8toUTF16(keyword).get());
                 }
 
                 return submission->GetUri(aURI);
