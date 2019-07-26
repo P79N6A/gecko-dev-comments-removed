e5e212a4cf15ef319d46840813f92678640f5e91
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 21:57:21 +0000

    Bug 926830 - Test cleanup for plugin enabledState usage, toolkit. r=bsmedberg
    UltraBlame original commit: 44dbd2276837ebb221f5971a1c45b695518b2c51

--- toolkit/mozapps/extensions/test/browser/head.js
+++ toolkit/mozapps/extensions/test/browser/head.js
@@ -1268,3 +1268,17 @@ function getTestPluginTag() {
   ok(false, "Unable to find plugin");
   return null;
 }
+
+
+
+
+function setTestPluginEnabledState(newEnabledState, pluginTag) {
+  if (!pluginTag) {
+    pluginTag = getTestPluginTag();
+  }
+  var oldEnabledState = pluginTag.enabledState;
+  pluginTag.enabledState = newEnabledState;
+  SimpleTest.registerCleanupFunction(function() {
+    pluginTag.enabledState = oldEnabledState;
+  });
+}
