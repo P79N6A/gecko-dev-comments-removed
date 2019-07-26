b6ea3b3be04a3e09ca7e3772bea32067f7942d9b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:32:58 +0000

    Bug 965064 - Part 2: back out FirefoxAccountsHelper. r=nalexander
    UltraBlame original commit: 77baf0b8fb9ed5a42685d0900546a9e511fa4687

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -179,8 +179,6 @@ abstract public class BrowserApp extends GeckoApp
 
     private OrderedBroadcastHelper mOrderedBroadcastHelper;
 
-    private FirefoxAccountsHelper mFirefoxAccountsHelper;
-
     private BrowserHealthReporter mBrowserHealthReporter;
 
     
@@ -546,7 +544,6 @@ abstract public class BrowserApp extends GeckoApp
         JavaAddonManager.getInstance().init(getApplicationContext());
         mSharedPreferencesHelper = new SharedPreferencesHelper(getApplicationContext());
         mOrderedBroadcastHelper = new OrderedBroadcastHelper(getApplicationContext());
-        mFirefoxAccountsHelper = new FirefoxAccountsHelper(getApplicationContext());
         mBrowserHealthReporter = new BrowserHealthReporter();
 
         if (AppConstants.MOZ_ANDROID_BEAM && Build.VERSION.SDK_INT >= 14) {
@@ -841,11 +838,6 @@ abstract public class BrowserApp extends GeckoApp
         if (mOrderedBroadcastHelper != null) {
             mOrderedBroadcastHelper.uninit();
             mOrderedBroadcastHelper = null;
-        }
-
-        if (mFirefoxAccountsHelper != null) {
-            mFirefoxAccountsHelper.uninit();
-            mFirefoxAccountsHelper = null;
         }
 
         if (mBrowserHealthReporter != null) {
@@ -1362,15 +1354,15 @@ abstract public class BrowserApp extends GeckoApp
         }
 
         final Tabs tabs = Tabs.getInstance();
-        final Tab tab = tabs.getFirstTabForUrl(url, tabs.getSelectedTab().isPrivate());
-        if (tab == null) {
+        final int tabId = tabs.getTabIdForUrl(url, tabs.getSelectedTab().isPrivate());
+        if (tabId < 0) {
             return false;
         }
 
         
         
         mTargetTabForEditingMode = null;
-        tabs.selectTab(tab.getId());
+        Tabs.getInstance().selectTab(tabId);
 
         mBrowserToolbar.cancelEdit();
 
@@ -1657,7 +1649,7 @@ abstract public class BrowserApp extends GeckoApp
 
         if (mHomePager == null) {
             final ViewStub homePagerStub = (ViewStub) findViewById(R.id.home_pager_stub);
-            mHomePager = (HomePager) homePagerStub.inflate().findViewById(R.id.home_pager);
+            mHomePager = (HomePager) homePagerStub.inflate();
         }
 
         mHomePager.show(getSupportLoaderManager(),
