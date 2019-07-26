2e89b0c309a2f49878bf1935cd6c8500148e136b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:54:05 +0000

    Bug 885463 - Warn about 'yield' without operand. r=Waldo.
    UltraBlame original commit: 818298a5a18322afe655f8303f88f7cf2f9c2186

--- js/src/jit-test/lib/asserts.js
+++ js/src/jit-test/lib/asserts.js
@@ -38,3 +38,64 @@ if (typeof assertThrowsValue === 'undefined') {
         throw new Error(fullmsg);
     };
 }
+
+if (typeof assertWarning === 'undefined') {
+    var assertWarning = function assertWarning(f, errorClass, msg) {
+        var hadWerror = options().split(",").indexOf("werror") !== -1;
+
+        
+        if (hadWerror)
+            options("werror");
+
+        try {
+            f();
+        } catch (exc) {
+            if (hadWerror)
+                options("werror");
+
+            
+            
+            if (msg)
+                print("assertWarning: " + msg);
+            print("assertWarning: Unexpected exception calling " + f +
+                  " with warnings-as-errors disabled");
+            throw exc;
+        }
+
+        
+        options("werror");
+
+        try {
+            assertThrowsInstanceOf(f, errorClass, msg);
+        } catch (exc) {
+            if (msg)
+                print("assertWarning: " + msg);
+            throw exc;
+        } finally {
+            if (!hadWerror)
+                options("werror");
+        }
+    };
+}
+
+if (typeof assertNoWarning === 'undefined') {
+    var assertNoWarning = function assertWarning(f, msg) {
+        
+        var hadWerror = options().split(",").indexOf("werror") !== -1;
+        if (!hadWerror)
+            options("werror");
+
+        try {
+            f();
+        } catch (exc) {
+            if (msg)
+                print("assertNoWarning: " + msg);
+            print("assertNoWarning: Unexpected exception calling " + f +
+                  "with warnings-as-errors enabled");
+            throw exc;
+        } finally {
+            if (!hadWerror)
+                options("werror");
+        }
+    };
+}
