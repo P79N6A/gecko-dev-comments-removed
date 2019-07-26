908d176aa4b9b3e4dbe76f8bfb9b018eacce687e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:54:50 +0000

    Backout 35d48d9d80d5 (bug 752982) for landing without proper review.
    UltraBlame original commit: a4f19181ebc2f11106d95014ee3f9bb3910fffe4

--- services/captivedetect/services-captivedetect.js
+++
@@ -1,9 +0,0 @@
-
-
-
-
-pref("services.captivedetect.canonicalURL", "http://localhost/test.html");
-pref("services.captivedetect.canonicalContent", "true");
-pref("services.captivedetect.maxWaitingTime", 5000);
-pref("services.captivedetect.pollingTime", 3000);
-pref("services.captivedetect.maxRetryCount", 5);
