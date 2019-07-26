0dd2435fcf69db3504ed5d70e33d34c653b854a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:58:13 +0000

    Bug 1000660 - Remove TextureParent::RecvRemoveTextureSync() r=nical
    UltraBlame original commit: 7c62af2b1ac99509a62c8722eeaecabfab4dbbb9

--- gfx/layers/composite/TextureHost.cpp
+++ gfx/layers/composite/TextureHost.cpp
@@ -88,13 +88,15 @@ public:
 
   virtual bool RecvClientRecycle() MOZ_OVERRIDE;
 
-  virtual bool RecvRemoveTexture() MOZ_OVERRIDE;
+  virtual bool RecvClearTextureHostSync() MOZ_OVERRIDE;
 
-  virtual bool RecvRemoveTextureSync() MOZ_OVERRIDE;
+  virtual bool RecvRemoveTexture() MOZ_OVERRIDE;
 
   TextureHost* GetTextureHost() { return mTextureHost; }
 
   void ActorDestroy(ActorDestroyReason why) MOZ_OVERRIDE;
+
+  void ClearTextureHost();
 
   CompositableParentManager* mCompositableManager;
   RefPtr<TextureHost> mWaitForClientRecycle;
@@ -737,20 +739,15 @@ TextureParent::RecvRemoveTexture()
 }
 
 bool
-TextureParent::RecvRemoveTextureSync()
+TextureParent::RecvClearTextureHostSync()
 {
-  
-  
-  return PTextureParent::Send__delete__(this);
+  ClearTextureHost();
+  return true;
 }
 
 void
 TextureParent::ActorDestroy(ActorDestroyReason why)
 {
-  if (!mTextureHost) {
-    return;
-  }
-
   switch (why) {
   case AncestorDeletion:
   case Deletion:
@@ -759,6 +756,16 @@ TextureParent::ActorDestroy(ActorDestroyReason why)
     break;
   case FailedConstructor:
     NS_RUNTIMEABORT("FailedConstructor isn't possible in PTexture");
+  }
+
+  ClearTextureHost();
+}
+
+void
+TextureParent::ClearTextureHost()
+{
+  if (!mTextureHost) {
+    return;
   }
 
   if (mTextureHost->GetFlags() & TextureFlags::RECYCLE) {
