cdcadbb965ca20b4c9d0db2d2408789310be7457
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:39:16 +0000

    Bug 825195 - Update method signatures of TextDecoder and TextEncoder per the latest spec. r=jonas
    UltraBlame original commit: abfe3957551b2a11ba2dc601a0d5a24ba24e8f38

--- dom/encoding/TextDecoderBase.h
+++ dom/encoding/TextDecoderBase.h
@@ -61,8 +61,9 @@ public:
 
 
 
-  void Decode(const ArrayBufferView* aView, const bool aStream,
-              nsAString& aOutDecodedString, ErrorResult& aRv);
+  void Decode(const char* aInput, const int32_t aLength,
+              const bool aStream, nsAString& aOutDecodedString,
+              ErrorResult& aRv);
 
 private:
   nsCString mEncoding;
