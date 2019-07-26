ba19ced8e0e8099914bad40afe664da2ca6fa5e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:15:06 +0000

    bug 854467 - fix nsIPluginTag enabled state API r=bsmedberg r=unfocused
    UltraBlame original commit: e30393f6f85b8740f643cc59a13616b6825e414a

--- dom/plugins/base/nsPluginTags.h
+++ dom/plugins/base/nsPluginTags.h
@@ -27,10 +27,12 @@ public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIPLUGINTAG
 
+  
   enum PluginState {
     ePluginState_Disabled = 0,
     ePluginState_Clicktoplay = 1,
     ePluginState_Enabled = 2,
+    ePluginState_MaxValue = 3,
   };
 
   nsPluginTag(nsPluginTag* aPluginTag);
