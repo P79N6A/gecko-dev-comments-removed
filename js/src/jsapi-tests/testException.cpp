cbe2c419832db576e8235ef842d0a76b24a875d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:40:52 +0000

    Bug 860435 - Don't use an iterator that's already at the end of the stack. r=terrence
    UltraBlame original commit: 6b42d26c2a93e1ef5d901a98ba9a694812a4f26d

---
+++ js/src/jsapi-tests/testException.cpp
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+#include "tests.h"
+
+BEGIN_TEST(testException_bug860435)
+{
+    JS::RootedValue fun(cx);
+
+    EVAL("ReferenceError", fun.address());
+    CHECK(fun.isObject());
+
+    JS::RootedValue v(cx);
+    JS_CallFunctionValue(cx, global, fun, 0, v.address(), v.address());
+    CHECK(v.isObject());
+
+    JS_GetProperty(cx, &v.toObject(), "stack", v.address());
+    CHECK(v.isString());
+    return true;
+}
+END_TEST(testException_bug860435)
