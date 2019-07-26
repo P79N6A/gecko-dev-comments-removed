ff36e4dce9e7d971b356a26aa3e48354a8f91f8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:02:58 +0000

    Bug 790465 - Refactor Marionette testrunner to make it easier to write Gaia tests, r=mdas
    UltraBlame original commit: 8d96a5392ff4caa0e611a2efae13fa94df431330

--- testing/marionette/client/marionette/__init__.py
+++ testing/marionette/client/marionette/__init__.py
@@ -3,6 +3,7 @@
 
 
 from marionette import Marionette, HTMLElement
-from marionette_test import MarionetteTestCase
+from marionette_test import MarionetteTestCase, CommonTestCase
 from emulator import Emulator
+from runtests import MarionetteTestRunner
 
