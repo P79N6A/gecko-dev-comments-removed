12c5daf0f0a56694add2160f42091f6d75b39316
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:24:53 +0000

    Merge m-c to fig
    UltraBlame original commit: e9c576ab368957705df4aee014863c407cf47f32

--- mobile/android/base/Tabs.java
+++ mobile/android/base/Tabs.java
@@ -448,6 +448,7 @@ public class Tabs implements GeckoEventListener {
                     
                     tab.setBackgroundColor(Color.WHITE);
                 }
+                tab.setErrorType(message.optString("errorType"));
                 notifyListeners(tab, Tabs.TabEvents.LOADED);
             } else if (event.equals("DOMTitleChanged")) {
                 tab.updateTitle(message.getString("title"));
