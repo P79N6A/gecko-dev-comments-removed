a1722c7fbeeaa37772df38edb66eb64120657ab5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:40:44 +0000

    Bug 875401 - Switch HealthReportGenerator to use org.json.JSONObject/JSONArray. r=nalexander
    UltraBlame original commit: 54d82ae49d3b6082b05c1918484dea6614da7ff9

--- mobile/android/base/background/healthreport/HealthReportUtils.java
+++ mobile/android/base/background/healthreport/HealthReportUtils.java
@@ -5,9 +5,17 @@
 package org.mozilla.gecko.background.healthreport;
 
 import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Locale;
+import java.util.Set;
+import java.util.SortedSet;
 import java.util.TimeZone;
+import java.util.TreeSet;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
 import org.mozilla.apache.commons.codec.digest.DigestUtils;
 
 import android.content.ContentUris;
@@ -44,5 +52,89 @@ public class HealthReportUtils {
 
   public static Uri getEventURI(Uri environmentURI) {
     return environmentURI.buildUpon().path("/events/" + ContentUris.parseId(environmentURI) + "/").build();
+  }
+
+  
+
+
+  private static <T extends Set<String>> T intoKeySet(T keys, JSONObject o) {
+    if (o == null || o == JSONObject.NULL) {
+      return keys;
+    }
+
+    @SuppressWarnings("unchecked")
+    Iterator<String> it = o.keys();
+    while (it.hasNext()) {
+      keys.add(it.next());
+    }
+    return keys;
+  }
+
+  
+
+
+
+
+
+
+  public static SortedSet<String> sortedKeySet(JSONObject o) {
+    return intoKeySet(new TreeSet<String>(), o);
+  }
+
+  
+
+
+
+
+  public static Set<String> keySet(JSONObject o) {
+    return intoKeySet(new HashSet<String>(), o);
+  }
+
+  
+
+
+
+
+
+
+
+
+
+  public static JSONObject shallowCopyObject(JSONObject o) throws JSONException {
+    if (o == null) {
+      return null;
+    }
+
+    JSONObject out = new JSONObject();
+    @SuppressWarnings("unchecked")
+    Iterator<String> keys = out.keys();
+    while (keys.hasNext()) {
+      final String key = keys.next();
+      out.put(key, o.get(key));
+    }
+    return out;
+  }
+
+  
+
+
+
+  public static void append(JSONObject o, String key, Object value) throws JSONException {
+    if (!o.has(key)) {
+      JSONArray arr = new JSONArray();
+      arr.put(value);
+      o.put(key, arr);
+      return;
+    }
+    Object dest = o.get(key);
+    if (dest instanceof JSONArray) {
+      ((JSONArray) dest).put(value);
+      return;
+    }
+    JSONArray arr = new JSONArray();
+    arr.put(dest);
+    arr.put(value);
+    o.put(key, arr);
+    return;
   }
 }
