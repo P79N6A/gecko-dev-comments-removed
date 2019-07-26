9445b6fcca1fc0e62270460827ae79dca0ac600a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:14:33 +0000

    Bug 897919: Correctly de-lazify functions before trying to inspect their scripts. r=sfink
    UltraBlame original commit: b27b567e0bdc82a95311b7f69f3c8a05c20429ef

---
+++ js/src/jit-test/tests/debug/Object-script-lazy.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+var g1 = newGlobal();
+g1.eval('function f() { return "from f"; }');
+g1.eval('function g() { return "from g"; }');
+
+
+var g2 = newGlobal();
+var dbg = new Debugger;
+var g2w = dbg.addDebuggee(g2);
+g2.f = g1.f;
+g2.g = g1.g;
+
+
+
+
+var fDO = g2w.getOwnPropertyDescriptor('f').value;
+assertEq(fDO.global, g2w);
+assertEq(fDO.unwrap().global === g2w, false);
+assertEq(fDO.unwrap().script instanceof Debugger.Script, true);
+
+
+var gDO = g2w.getOwnPropertyDescriptor('g').value;
+assertEq(gDO.global, g2w);
+assertEq(gDO.unwrap().global === g2w, false);
+assertEq(gDO.unwrap().parameterNames instanceof Array, true);
