a46c074c960ccc38d6db41c56a25e9f2d8b0d3ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:38:46 +0000

    Bug 991400 - Prevent gfxPath instances from being created on the stack (they're refcounted). r=dholbert
    UltraBlame original commit: 31dbf8a76158024d0bf33f28c494b1977c163ad0

--- gfx/thebes/gfxPath.h
+++ gfx/thebes/gfxPath.h
@@ -23,7 +23,7 @@ class Path;
 
 
 
-class gfxPath {
+class gfxPath MOZ_FINAL {
     NS_INLINE_DECL_REFCOUNTING(gfxPath)
 
     friend class gfxContext;
@@ -32,9 +32,11 @@ class gfxPath {
 
 public:
     gfxPath(mozilla::gfx::Path* aPath);
-    virtual ~gfxPath();
 
 private:
+    
+    ~gfxPath();
+
     cairo_path_t* mPath;
     mozilla::RefPtr<mozilla::gfx::Path> mMoz2DPath;
 };
