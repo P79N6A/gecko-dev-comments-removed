b461939e9c1cffe28f3cf688f0671837dfb12d4d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:47:27 +0000

    Bug 831365 - Always show the click-to-play doorhanger when the Navigation Toolbar is collapsed. r=gavin
    UltraBlame original commit: 798608217a81e08832d079708f5954a8d02be562

--- browser/base/content/browser-plugins.js
+++ browser/base/content/browser-plugins.js
@@ -655,6 +655,9 @@ var gPluginHandler = {
     }];
     let notification = PopupNotifications.getNotification("click-to-play-plugins", aBrowser);
     let dismissed = notification ? notification.dismissed : true;
+    
+    if (!isElementVisible(gURLBar))
+      dismissed = false;
     let options = { dismissed: dismissed, centerActions: centerActions };
     let icon = haveVulnerablePlugin ? "blocked-plugins-notification-icon" : "plugins-notification-icon"
     PopupNotifications.show(aBrowser, "click-to-play-plugins",
