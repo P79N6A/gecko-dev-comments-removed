951d7fa14e951fcd5a03ccebdcda8c30a3cd719a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:50:23 +0000

    Bug 793928: Ship the Add-on SDK APIs as part of the platform. r=gps
    UltraBlame original commit: 729d655f29f6fe09bd65ba1fe861b1051da480d4

---
+++ addon-sdk/mach_commands.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+import os
+import sys
+
+from mozbuild.base import (
+    MachCommandBase,
+    MozbuildObject,
+)
+
+from mach.decorators import (
+    CommandArgument,
+    CommandProvider,
+    Command,
+)
+
+class JetpackRunner(MozbuildObject):
+    """Run jetpack tests."""
+    def run_tests(self, **kwargs):
+        self._run_make(target='jetpack-tests')
+
+@CommandProvider
+class MachCommands(MachCommandBase):
+    @Command('jetpack-test', help='Runs the jetpack test suite.')
+    def run_jetpack_test(self, **params):
+        
+        
+        
+        self._ensure_state_subdir_exists('.')
+
+        jetpack = self._spawn(JetpackRunner)
+
+        jetpack.run_tests(**params)
