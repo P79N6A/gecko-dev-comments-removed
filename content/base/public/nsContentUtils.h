2d87fad9a806978ce16c41b247df9fe983d13fde
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:12:33 +0000

    Bug 899812 - Error reporters should consistently report errors to stderr (r=bholley)
    UltraBlame original commit: f775819f6b482387e9e1fbd23d4aa43bc525d2e7

--- content/base/public/nsContentUtils.h
+++ content/base/public/nsContentUtils.h
@@ -2116,6 +2116,11 @@ public:
                                   const nsAString& aFeature,
                                   const nsAString& aVersion);
 
+  
+
+
+  static bool DOMWindowDumpEnabled();
+
 private:
   static bool InitializeEventTable();
 
@@ -2225,6 +2230,10 @@ private:
   static nsString* sOSText;
   static nsString* sAltText;
   static nsString* sModifierSeparator;
+
+#if !(defined(DEBUG) || defined(MOZ_ENABLE_JS_DUMP))
+  static bool sDOMWindowDumpEnabled;
+#endif
 };
 
 typedef nsCharSeparatedTokenizerTemplate<nsContentUtils::IsHTMLWhitespace>
