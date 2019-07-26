6fc3d6cda1c443b84bc3ea0cd35f1c2bad29161e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:30:33 +0000

    Bug 1023507 - Handle SDK failures when calling connect, r=dmose, ui-r=darrin
    UltraBlame original commit: b30d5443b70e0db90d2502a0c24c69049ae81ddb

--- browser/components/loop/content/shared/js/router.js
+++ browser/components/loop/content/shared/js/router.js
@@ -103,8 +103,20 @@ loop.shared.router = (function(l10n) {
                                         this._onPeerHungup);
       this.listenTo(this._conversation, "session:network-disconnected",
                                         this._onNetworkDisconnected);
+      this.listenTo(this._conversation, "session:connection-error",
+                    this._notifyError);
 
       BaseRouter.apply(this, arguments);
+    },
+
+    
+
+
+
+    _notifyError: function(error) {
+      console.log(error);
+      this._notifier.errorL10n("connection_error_see_console_notification");
+      this.endCall();
     },
 
     
