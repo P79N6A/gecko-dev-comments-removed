f25819359f0a88127bebe5bcce3048104dabfd10
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:55:23 +0000

    back out 0528e2561bde (bug 719318) for tickling browser_social_chatwindow.js's bustedness
    UltraBlame original commit: a20345bfe817e5d2842f2aacb3e2ce816e339cf8

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1256,33 +1256,19 @@ var gBrowserInit = {
 
     
     if (!document.documentElement.hasAttribute("width")) {
-      let defaultWidth;
+      let defaultWidth = 994;
       let defaultHeight;
-
-      
-      
-      
-      
-      
       if (screen.availHeight <= 600) {
         document.documentElement.setAttribute("sizemode", "maximized");
         defaultWidth = 610;
         defaultHeight = 450;
       }
       else {
-        if (screen.availWidth <= screen.availHeight) {
-          defaultWidth = screen.availWidth * .9;
-          defaultHeight = screen.availHeight * .75;
-        }
-        else if (screen.availWidth >= 2048) {
+        
+        
+        if (screen.availWidth >= 1600)
           defaultWidth = (screen.availWidth / 2) - 20;
-          defaultHeight = screen.availHeight - 10;
-        }
-        else {
-          defaultWidth = screen.availWidth * .75;
-          defaultHeight = screen.availHeight * .75;
-        }
-
+        defaultHeight = screen.availHeight - 10;
 #ifdef MOZ_WIDGET_GTK2
         
         
