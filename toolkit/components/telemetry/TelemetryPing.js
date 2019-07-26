c94aa71610915cf7d740e27d004ca60a801a7956
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:09:03 +0000

    Bug 841028: Add last build ID to telemetry system info if build ID has changed. r=froydnj
    UltraBlame original commit: 13ddefe3068a9263384578c5c0e2931b80b16e57

--- toolkit/components/telemetry/TelemetryPing.js
+++ toolkit/components/telemetry/TelemetryPing.js
@@ -23,12 +23,15 @@ const PAYLOAD_VERSION = 1;
 
 #expand const HISTOGRAMS_FILE_VERSION = "__HISTOGRAMS_FILE_VERSION__";
 
-const PREF_SERVER = "toolkit.telemetry.server";
+const PREF_BRANCH = "toolkit.telemetry.";
+const PREF_SERVER = PREF_BRANCH + "server";
 #ifdef MOZ_TELEMETRY_ON_BY_DEFAULT
-const PREF_ENABLED = "toolkit.telemetry.enabledPreRelease";
+const PREF_ENABLED = PREF_BRANCH + "enabledPreRelease";
 #else
-const PREF_ENABLED = "toolkit.telemetry.enabled";
+const PREF_ENABLED = PREF_BRANCH + "enabled";
 #endif
+const PREF_PREVIOUS_BUILDID = PREF_BRANCH + "previousBuildID";
+
 
 const TELEMETRY_INTERVAL = 60000;
 
@@ -182,6 +185,9 @@ TelemetryPing.prototype = {
   
   _savedSimpleMeasurements: null,
   _savedInfo: null,
+  
+  
+  _previousBuildID: undefined,
 
   
 
@@ -385,6 +391,9 @@ TelemetryPing.prototype = {
       revision: HISTOGRAMS_FILE_VERSION,
       locale: getLocale()
     };
+    if (this._previousBuildID) {
+      ret.previousBuildID = this._previousBuildID;
+    }
 
     
     let sysInfo = Cc["@mozilla.org/system-info;1"].getService(Ci.nsIPropertyBag2);
@@ -753,6 +762,22 @@ TelemetryPing.prototype = {
 
 
   setup: function setup() {
+    
+    
+    let previousBuildID = undefined;
+    try {
+      previousBuildID = Services.prefs.getCharPref(PREF_PREVIOUS_BUILDID);
+    } catch (e) {
+      
+    }
+    let thisBuildID = Services.appinfo.appBuildID;
+    
+    
+    if (previousBuildID != thisBuildID) {
+      this._previousBuildID = previousBuildID;
+      Services.prefs.setCharPref(PREF_PREVIOUS_BUILDID, thisBuildID);
+    }
+
 #ifdef MOZILLA_OFFICIAL
     if (!Telemetry.canSend) {
       
