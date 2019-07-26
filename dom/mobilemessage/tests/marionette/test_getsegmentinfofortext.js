8bd133632ce352b9bb0ee7a548f0337fc770f12b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:39:19 +0000

    Bug 872219 - 2/2: marionette test cases. r=gene
    UltraBlame original commit: 93d20e12ec9757558c5761419f8bd6371d6affdb

---
+++ dom/mobilemessage/tests/marionette/test_getsegmentinfofortext.js
@@ -0,0 +1,106 @@
+
+
+
+MARIONETTE_TIMEOUT = 60000;
+
+
+const PDU_MAX_USER_DATA_7BIT = 160;
+
+SpecialPowers.setBoolPref("dom.sms.enabled", true);
+SpecialPowers.addPermission("sms", true, document);
+
+let sms = window.navigator.mozSms;
+ok(sms instanceof MozSmsManager, "mozSmsManager");
+
+let tasks = {
+  
+  
+  _tasks: [],
+  _nextTaskIndex: 0,
+
+  push: function push(func) {
+    this._tasks.push(func);
+  },
+
+  next: function next() {
+    let index = this._nextTaskIndex++;
+    let task = this._tasks[index];
+    try {
+      task();
+    } catch (ex) {
+      ok(false, "test task[" + index + "] throws: " + ex);
+      
+      if (index != this._tasks.length - 1) {
+        this.finish();
+      }
+    }
+  },
+
+  finish: function finish() {
+    this._tasks[this._tasks.length - 1]();
+  },
+
+  run: function run() {
+    this.next();
+  }
+};
+
+function addTest(text, segments, charsPerSegment, charsAvailableInLastSegment) {
+  tasks.push(function () {
+    log("Testing '" + text + "' ...");
+    let info = sms.getSegmentInfoForText(text);
+    is(info.segments, segments, "info.segments");
+    is(info.charsPerSegment, charsPerSegment, "info.charsPerSegment");
+    is(info.charsAvailableInLastSegment, charsAvailableInLastSegment,
+       "info.charsAvailableInLastSegment");
+
+    tasks.next();
+  });
+}
+
+function addTestThrows(text) {
+  tasks.push(function () {
+    log("Testing '" + text + "' ...");
+    try {
+      let info = sms.getSegmentInfoForText(text);
+
+      ok(false, "Not thrown");
+      tasks.finish();
+    } catch (e) {
+      tasks.next();
+    }
+  });
+}
+
+addTestThrows(null);
+
+
+addTest(undefined, 1, PDU_MAX_USER_DATA_7BIT,
+        PDU_MAX_USER_DATA_7BIT - "undefined".length);
+
+
+addTest(0,   1, PDU_MAX_USER_DATA_7BIT, PDU_MAX_USER_DATA_7BIT - "0".length);
+addTest(1.0, 1, PDU_MAX_USER_DATA_7BIT, PDU_MAX_USER_DATA_7BIT - "1".length);
+
+
+
+
+addTest({}, 1, PDU_MAX_USER_DATA_7BIT,
+        PDU_MAX_USER_DATA_7BIT - (("" + {}).length + 2));
+
+
+let date = new Date();
+addTest(date, 1, PDU_MAX_USER_DATA_7BIT,
+        PDU_MAX_USER_DATA_7BIT - ("" + date).length);
+
+addTest("", 0, PDU_MAX_USER_DATA_7BIT,
+        PDU_MAX_USER_DATA_7BIT - "".length);
+
+
+tasks.push(function cleanUp() {
+  SpecialPowers.removePermission("sms", document);
+  SpecialPowers.clearUserPref("dom.sms.enabled");
+  finish();
+});
+
+tasks.run();
