50b223dde4262d211f73da70c5355c7629373d5a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:03:20 +0000

    Bug 834172 - Don't export codec functions from DecoderTraits. r=cpearce
    All codec-specific code is hidden within DecoderTraits and available
    from generic interfaces. This patch removes the codec-specific
    functions from DecoderTraits' public interface.
    
    UltraBlame original commit: f06028922aaf138e735c90ac9508d658d37b0f5e

--- content/media/DecoderTraits.h
+++ content/media/DecoderTraits.h
@@ -41,45 +41,6 @@ public:
   
   static bool ShouldHandleMediaType(const char* aMIMEType);
 
-#ifdef MOZ_RAW
-  static bool IsRawType(const nsACString& aType);
-#endif
-
-#ifdef MOZ_OGG
-  static bool IsOggType(const nsACString& aType);
-#endif
-
-#ifdef MOZ_WAVE
-  static bool IsWaveType(const nsACString& aType);
-#endif
-
-#ifdef MOZ_WEBM
-  static bool IsWebMType(const nsACString& aType);
-#endif
-
-#ifdef MOZ_GSTREAMER
-  
-  
-  static bool IsGStreamerSupportedType(const nsACString& aType);
-  static bool IsH264Type(const nsACString& aType);
-#endif
-
-#ifdef MOZ_WIDGET_GONK
-  static bool IsOmxSupportedType(const nsACString& aType);
-#endif
-
-#ifdef MOZ_MEDIA_PLUGINS
-  static bool IsMediaPluginsType(const nsACString& aType);
-#endif
-
-#ifdef MOZ_DASH
-  static bool IsDASHMPDType(const nsACString& aType);
-#endif
-
-#ifdef MOZ_WMF
-  static bool IsWMFSupportedType(const nsACString& aType);
-#endif
-
   
   
   static already_AddRefed<MediaDecoder> CreateDecoder(const nsACString& aType,
