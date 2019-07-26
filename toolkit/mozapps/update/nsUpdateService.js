21f19347632e46fce0445bcb581275948bf50ad3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:51:21 +0000

    Main patch - Bug 882007 - Restart notification is not shown when staging is enabled. r=bbondy
    UltraBlame original commit: c7f6325c015ed7318f9eabe1ae8e752cd0a1dd09

--- toolkit/mozapps/update/nsUpdateService.js
+++ toolkit/mozapps/update/nsUpdateService.js
@@ -43,7 +43,7 @@ const PREF_APP_UPDATE_POSTUPDATE          = "app.update.postupdate";
 const PREF_APP_UPDATE_PROMPTWAITTIME      = "app.update.promptWaitTime";
 const PREF_APP_UPDATE_SHOW_INSTALLED_UI   = "app.update.showInstalledUI";
 const PREF_APP_UPDATE_SILENT              = "app.update.silent";
-const PREF_APP_UPDATE_STAGE_ENABLED       = "app.update.staging.enabled";
+const PREF_APP_UPDATE_STAGING_ENABLED     = "app.update.staging.enabled";
 const PREF_APP_UPDATE_URL                 = "app.update.url";
 const PREF_APP_UPDATE_URL_DETAILS         = "app.update.url.details";
 const PREF_APP_UPDATE_URL_OVERRIDE        = "app.update.url.override";
@@ -81,11 +81,11 @@ const KEY_UPDATE_ARCHIVE_DIR = "UpdArchD"
 #endif
 
 #ifdef XP_WIN
-#define SKIP_STAGE_UPDATES_TEST
+#define CHECK_CAN_USE_SERVICE
 #elifdef MOZ_WIDGET_GONK
 
 
-#define SKIP_STAGE_UPDATES_TEST
+#define CHECK_CAN_USE_SERVICE
 #endif
 
 const DIR_UPDATES         = "updates";
@@ -455,7 +455,7 @@ function getPerInstallationMutexName(aGlobal) {
     hasher.init(hasher.SHA1);
 
   Components.utils.import("resource://gre/modules/Services.jsm");
-  var exeFile = Services.dirsvc.get("XREExeF", Components.interfaces.nsILocalFile);
+  var exeFile = Services.dirsvc.get(KEY_EXECUTABLE, Components.interfaces.nsILocalFile);
 
   let converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].
                   createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
@@ -466,6 +466,7 @@ function getPerInstallationMutexName(aGlobal) {
   return (aGlobal ? "Global\\" : "") + "MozillaUpdateMutex-" + hasher.finish(true);
 }
 #endif 
+
 
 
 
@@ -490,13 +491,13 @@ function hasUpdateMutex() {
 
 XPCOMUtils.defineLazyGetter(this, "gCanApplyUpdates", function aus_gCanApplyUpdates() {
   function submitHasPermissionsTelemetryPing(val) {
-      try {
-        let h = Services.telemetry.getHistogramById("UPDATER_HAS_PERMISSIONS");
-        h.add(+val);
-      } catch(e) {
-        
-        Components.utils.reportError(e);
-      }
+    try {
+      let h = Services.telemetry.getHistogramById("UPDATER_HAS_PERMISSIONS");
+      h.add(+val);
+    } catch(e) {
+      
+      Components.utils.reportError(e);
+    }
   }
 
   try {
@@ -584,8 +585,9 @@ XPCOMUtils.defineLazyGetter(this, "gCanApplyUpdates", function aus_gCanApplyUpda
   }
 
   if (!hasUpdateMutex()) {
-    LOG("gCanApplyUpdates - unable to apply updates because another " +
-        "browser is already handling updates for this installation.");
+    LOG("gCanApplyUpdates - unable to apply updates because another instance" +
+        "of the application is already handling updates for this " +
+        "installation.");
     return false;
   }
 
@@ -594,54 +596,74 @@ XPCOMUtils.defineLazyGetter(this, "gCanApplyUpdates", function aus_gCanApplyUpda
   return true;
 });
 
-XPCOMUtils.defineLazyGetter(this, "gCanStageUpdates", function aus_gCanStageUpdates() {
+
+
+
+
+
+function getCanStageUpdates() {
   
-  if (!getPref("getBoolPref", PREF_APP_UPDATE_STAGE_ENABLED, false)) {
-    LOG("gCanStageUpdates - staging updates is disabled by preference " + PREF_APP_UPDATE_STAGE_ENABLED);
+  if (!getPref("getBoolPref", PREF_APP_UPDATE_STAGING_ENABLED, false)) {
+    LOG("getCanStageUpdates - staging updates is disabled by preference " +
+        PREF_APP_UPDATE_STAGING_ENABLED);
     return false;
   }
 
-#ifdef SKIP_STAGE_UPDATES_TEST
+#ifdef CHECK_CAN_USE_SERVICE
   if (getPref("getBoolPref", PREF_APP_UPDATE_SERVICE_ENABLED, false)) {
     
     
-    LOG("gCanStageUpdates - able to stage updates because we'll use the service");
+    LOG("getCanStageUpdates - able to stage updates because we'll use the service");
     return true;
   }
 #endif
 
-  try {
-    var updateTestFile = getInstallDirRoot();
-    updateTestFile.append(FILE_PERMS_TEST);
-    LOG("gCanStageUpdates - testing write access " + updateTestFile.path);
-    testWriteAccess(updateTestFile, true);
-#ifndef XP_MACOSX
-    
-    
-    
-    updateTestFile = getInstallDirRoot().parent;
-    updateTestFile.append(FILE_PERMS_TEST);
-    LOG("gCanStageUpdates - testing write access " + updateTestFile.path);
-    updateTestFile.createUnique(Ci.nsILocalFile.DIRECTORY_TYPE,
-                                FileUtils.PERMS_DIRECTORY);
-    updateTestFile.remove(false);
-#endif
-  }
-  catch (e) {
-     LOG("gCanStageUpdates - unable to stage updates. Exception: " + e);
-    
-    return false;
-  }
-
   if (!hasUpdateMutex()) {
-    LOG("gCanStageUpdates - unable to stage updates because another " +
-        "browser is already handling updates for this installation.");
+    LOG("getCanStageUpdates - unable to apply updates because another " +
+        "instance of the application is already handling updates for this " +
+        "installation.");
     return false;
   }
 
-  LOG("gCanStageUpdates - able to stage updates");
-  return true;
-});
+  
+
+
+
+
+
+  XPCOMUtils.defineLazyGetter(this, "canStageUpdatesSession", function canStageUpdatesSession() {
+    try {
+      var updateTestFile = getInstallDirRoot();
+      updateTestFile.append(FILE_PERMS_TEST);
+      LOG("canStageUpdatesSession - testing write access " +
+          updateTestFile.path);
+      testWriteAccess(updateTestFile, true);
+#ifndef XP_MACOSX
+      
+      
+      
+      updateTestFile = getInstallDirRoot().parent;
+      updateTestFile.append(FILE_PERMS_TEST);
+      LOG("canStageUpdatesSession - testing write access " +
+          updateTestFile.path);
+      updateTestFile.createUnique(Ci.nsILocalFile.DIRECTORY_TYPE,
+                                  FileUtils.PERMS_DIRECTORY);
+      updateTestFile.remove(false);
+#endif
+    }
+    catch (e) {
+       LOG("canStageUpdatesSession - unable to stage updates. Exception: " +
+           e);
+      
+      return false;
+    }
+
+    LOG("canStageUpdatesSession - able to stage updates");
+    return true;
+  });
+
+  return canStageUpdatesSession;
+}
 
 XPCOMUtils.defineLazyGetter(this, "gIsMetro", function aus_gIsMetro() {
 #ifdef XP_WIN
@@ -663,8 +685,8 @@ XPCOMUtils.defineLazyGetter(this, "gMetroUpdatesEnabled", function aus_gMetroUpd
   if (gIsMetro) {
     let metroUpdate = getPref("getBoolPref", PREF_APP_UPDATE_METRO_ENABLED, true);
     if (!metroUpdate) {
-      LOG("gMetroUpdatesEnabled - unable to automatically check for metro" +
-          " updates, disabled by pref");
+      LOG("gMetroUpdatesEnabled - unable to automatically check for metro " +
+          "updates, disabled by pref");
       return false;
     }
   }
@@ -681,7 +703,7 @@ XPCOMUtils.defineLazyGetter(this, "gCanCheckForUpdates", function aus_gCanCheckF
   var enabled = getPref("getBoolPref", PREF_APP_UPDATE_ENABLED, true);
   if (!enabled && Services.prefs.prefIsLocked(PREF_APP_UPDATE_ENABLED)) {
     LOG("gCanCheckForUpdates - unable to automatically check for updates, " +
-        "disabled by pref");
+        "the preference is disabled and admistratively locked.");
     return false;
   }
 
@@ -704,7 +726,8 @@ XPCOMUtils.defineLazyGetter(this, "gCanCheckForUpdates", function aus_gCanCheckF
 
   if (!hasUpdateMutex()) {
     LOG("gCanCheckForUpdates - unable to apply updates because another " +
-        "browser is already handling updates for this installation.");
+        "instance of the application is already handling updates for this " +
+        "installation.");
     return false;
   }
 
@@ -771,7 +794,7 @@ function getUpdateDirCreate(pathArray) {
   return FileUtils.getDir(KEY_UPDROOT, pathArray, true);
 }
 
- 
+
 
 
 
@@ -782,6 +805,7 @@ function getUpdateDirCreate(pathArray) {
 function getUpdateDirNoCreate(pathArray) {
   return FileUtils.getDir(KEY_UPDROOT, pathArray, false);
 }
+
 
 
 
@@ -1842,6 +1866,7 @@ const UpdateServiceFactory = {
 function UpdateService() {
   LOG("Creating UpdateService");
   Services.obs.addObserver(this, "xpcom-shutdown", false);
+  Services.prefs.addObserver("app.update.log", this, false);
 #ifdef MOZ_WIDGET_GONK
   
   
@@ -1895,11 +1920,17 @@ UpdateService.prototype = {
     case "network:offline-status-changed":
       this._offlineStatusChanged(data);
       break;
+    case "nsPref:changed":
+      if (data == PREF_APP_UPDATE_LOG) {
+        gLogEnabled = getPref("getBoolPref", PREF_APP_UPDATE_LOG, false);
+      }
+      break;
 #ifdef MOZ_WIDGET_GONK
     case "profile-change-net-teardown": 
 #endif
     case "xpcom-shutdown":
       Services.obs.removeObserver(this, topic);
+      Services.prefs.removeObserver("app.update.log", this);
 
       if (this._retryTimer) {
         this._retryTimer.cancel();
@@ -2065,7 +2096,7 @@ UpdateService.prototype = {
                                       "UPDATER_UPDATES_METRO_ENABLED");
       this._sendBoolPrefTelemetryPing(PREF_APP_UPDATE_AUTO,
                                       "UPDATER_UPDATES_AUTOMATIC");
-      this._sendBoolPrefTelemetryPing(PREF_APP_UPDATE_STAGE_ENABLED,
+      this._sendBoolPrefTelemetryPing(PREF_APP_UPDATE_STAGING_ENABLED,
                                       "UPDATER_STAGE_ENABLED");
 
 #ifdef XP_WIN
@@ -2688,7 +2719,7 @@ UpdateService.prototype = {
 
 
   get canStageUpdates() {
-    return gCanStageUpdates;
+    return getCanStageUpdates();
   },
 
   
@@ -2775,26 +2806,6 @@ UpdateService.prototype = {
     update.previousAppVersion = Services.appinfo.version;
     this._downloader = new Downloader(background, this);
     return this._downloader.downloadUpdate(update);
-  },
-
-  applyUpdateInBackground: function AUS_applyUpdateInBackground(update) {
-    
-    if (!gCanStageUpdates) {
-      return;
-    }
-
-    LOG("UpdateService:applyUpdateInBackground called with the following update: " +
-        update.name);
-
-    
-    try {
-      Cc["@mozilla.org/updates/update-processor;1"].
-        createInstance(Ci.nsIUpdateProcessor).
-        processUpdate(update);
-    } catch (e) {
-      
-      
-    }
   },
 
   
@@ -3127,6 +3138,7 @@ UpdateManager.prototype = {
 
     
     
+#ifdef MOZ_WIDGET_GONK
     if (update.state == STATE_APPLIED) {
       
       
@@ -3137,6 +3149,23 @@ UpdateManager.prototype = {
     } else {
       releaseSDCardMountLock();
     }
+#else
+    
+    let windowType = getPref("getCharPref", PREF_APP_UPDATE_ALTWINDOWTYPE, null);
+    if (Services.wm.getMostRecentWindow(UPDATE_WINDOW_NAME) ||
+        windowType && Services.wm.getMostRecentWindow(windowType)) {
+      return;
+    }
+
+    if (update.state == STATE_APPLIED || update.state == STATE_APPLIED_SVC ||
+        update.state == STATE_PENDING || update.state == STATE_PENDING_SVC) {
+      
+      
+      var prompter = Cc["@mozilla.org/updates/update-prompt;1"].
+                     createInstance(Ci.nsIUpdatePrompt);
+      prompter.showUpdateDownloaded(update, true);
+    }
+#endif
   },
 
   classID: Components.ID("{093C2356-4843-4C65-8709-D7DBCBBE7DFB}"),
@@ -3968,12 +3997,9 @@ Downloader.prototype = {
     if (Components.isSuccessCode(status)) {
       if (this._verifyDownload()) {
         state = shouldUseService() ? STATE_PENDING_SVC : STATE_PENDING
-
-        
-        
-        
-        if (this.background)
-          shouldShowPrompt = !getPref("getBoolPref", PREF_APP_UPDATE_STAGE_ENABLED, false);
+        if (this.background) {
+          shouldShowPrompt = !getCanStageUpdates();
+        }
 
         
         writeStatusFile(getUpdatesDir(), state);
@@ -3984,7 +4010,6 @@ Downloader.prototype = {
       else {
         LOG("Downloader:onStopRequest - download verification failed");
         state = STATE_DOWNLOAD_FAILED;
-
         status = Cr.NS_ERROR_CORRUPTED_CONTENT;
 
         
@@ -4112,6 +4137,28 @@ Downloader.prototype = {
       return;
     }
 
+    if (state == STATE_PENDING || state == STATE_PENDING_SVC) {
+      if (getCanStageUpdates()) {
+        LOG("Downloader:onStopRequest - attempting to stage update: " +
+            this._update.name);
+
+        
+        try {
+          Cc["@mozilla.org/updates/update-processor;1"].
+            createInstance(Ci.nsIUpdateProcessor).
+            processUpdate(this._update);
+        } catch (e) {
+          
+          
+          LOG("Downloader:onStopRequest - failed to stage update. Exception: " +
+              e);
+          if (this.background) {
+            shouldShowPrompt = true;
+          }
+        }
+      }
+    }
+
     
     
     if (shouldShowPrompt) {
@@ -4121,13 +4168,6 @@ Downloader.prototype = {
       var prompter = Cc["@mozilla.org/updates/update-prompt;1"].
                      createInstance(Ci.nsIUpdatePrompt);
       prompter.showUpdateDownloaded(this._update, true);
-    }
-
-    if (state == STATE_PENDING || state == STATE_PENDING_SVC) {
-      
-      Cc["@mozilla.org/updates/update-service;1"].
-        getService(Ci.nsIApplicationUpdateService).
-        applyUpdateInBackground(this._update);
     }
 
     if (shouldRegisterOnlineObserver) {
