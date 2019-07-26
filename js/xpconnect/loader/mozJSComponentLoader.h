21fd669e337090de69449e33b462d66fb1e4b33f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:42:11 +0000

    Bug 890545 - provide a way to enumerate registered manifests, r=froydnj,f=glandium
    UltraBlame original commit: e28e95dc0795f065383948d776a29fe1835b1fea

--- js/xpconnect/loader/mozJSComponentLoader.h
+++ js/xpconnect/loader/mozJSComponentLoader.h
@@ -4,9 +4,13 @@
 
 
 
+#ifndef mozJSComponentLoader_h
+#define mozJSComponentLoader_h
+
 #include "mozilla/ModuleLoader.h"
 #include "nsISupports.h"
 #include "nsIObserver.h"
+#include "nsIURI.h"
 #include "xpcIJSModuleLoader.h"
 #include "nsClassHashtable.h"
 #include "nsDataHashtable.h"
@@ -142,3 +146,5 @@ class mozJSComponentLoader : public mozilla::ModuleLoader,
     bool mInitialized;
     bool mReuseLoaderGlobal;
 };
+
+#endif 
