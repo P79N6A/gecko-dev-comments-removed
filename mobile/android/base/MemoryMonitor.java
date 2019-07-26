c247eec9459adba9da40f8662b57593f985aecb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:49:55 +0000

    bug 882196 - Android crash in nsXPCWrappedJS::AddRef, remove nsAppShell::NotifyObservers r=kats
    UltraBlame original commit: 35b943a379dded84def27a77d9d8dc907a4abfe5

--- mobile/android/base/MemoryMonitor.java
+++ mobile/android/base/MemoryMonitor.java
@@ -39,6 +39,7 @@ class MemoryMonitor extends BroadcastReceiver {
     private static final String ACTION_MEMORY_DUMP = "org.mozilla.gecko.MEMORY_DUMP";
     private static final String ACTION_FORCE_PRESSURE = "org.mozilla.gecko.FORCE_MEMORY_PRESSURE";
 
+    
     private static final int MEMORY_PRESSURE_NONE = 0;
     private static final int MEMORY_PRESSURE_CLEANUP = 1;
     private static final int MEMORY_PRESSURE_LOW = 2;
@@ -148,8 +149,9 @@ class MemoryMonitor extends BroadcastReceiver {
 
         
         if (level >= MEMORY_PRESSURE_MEDIUM) {
+            
             if (GeckoThread.checkLaunchState(GeckoThread.LaunchState.GeckoRunning)) {
-                GeckoAppShell.onLowMemory();
+                GeckoAppShell.sendEventToGecko(GeckoEvent.createLowMemoryEvent(level));
             }
 
             Favicons.getInstance().clearMemCache();
