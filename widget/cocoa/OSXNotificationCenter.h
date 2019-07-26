684bf3b381e41bfe3e73bb6b3ec1f0668f32af84
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:52:37 +0000

    Bug 852648: Add support for native Notification Center alerts on OS X. r=wchen,mstange
    UltraBlame original commit: c732b8ff38697d6841d6cb1ef0cd14dbe87205f9

---
+++ widget/cocoa/OSXNotificationCenter.h
@@ -0,0 +1,48 @@
+
+
+
+
+
+#ifndef OSXNotificationCenter_h
+#define OSXNotificationCenter_h
+
+#import <Foundation/Foundation.h>
+#include "nsIAlertsService.h"
+#include "imgINotificationObserver.h"
+#include "nsITimer.h"
+#include "nsTArray.h"
+#include "mozilla/RefPtr.h"
+
+@class mozNotificationCenterDelegate;
+
+namespace mozilla {
+
+class OSXNotificationInfo;
+
+class OSXNotificationCenter : public nsIAlertsService,
+                              public imgINotificationObserver,
+                              public nsITimerCallback
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIALERTSSERVICE
+  NS_DECL_IMGINOTIFICATIONOBSERVER
+  NS_DECL_NSITIMERCALLBACK
+
+  OSXNotificationCenter();
+  virtual ~OSXNotificationCenter();
+
+  nsresult Init();
+  void CloseAlertCocoaString(NSString *aAlertName);
+  void OnClick(NSString *aAlertName);
+  void ShowPendingNotification(OSXNotificationInfo *osxni);
+
+private:
+  mozNotificationCenterDelegate *mDelegate;
+  nsTArray<nsRefPtr<OSXNotificationInfo> > mActiveAlerts;
+  nsTArray<nsRefPtr<OSXNotificationInfo> > mPendingAlerts;
+};
+
+} 
+
+#endif 
