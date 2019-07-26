542bca3c5b916d41c6c0f7e751a7b08d04040387
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:01:21 +0000

    Bug 1006285: make MediaCodec bitrate setting work for KitKat-based builds r=jhlin
    UltraBlame original commit: effb5d2a8111c5df38e030e3e2bf31dbfe2b4179

--- content/media/omx/OMXCodecWrapper.cpp
+++ content/media/omx/OMXCodecWrapper.cpp
@@ -443,7 +443,12 @@ nsresult
 OMXVideoEncoder::SetBitrate(int32_t aKbps)
 {
   sp<AMessage> msg = new AMessage();
+#if ANDROID_VERSION >= 19
+  
+  msg->setInt32("video-bitrate", aKbps * 1000 );
+#else
   msg->setInt32("videoBitrate", aKbps * 1000 );
+#endif
   status_t result = mCodec->setParameters(msg);
   MOZ_ASSERT(result == OK);
   return result == OK ? NS_OK : NS_ERROR_FAILURE;
