8edfba587d42bc6b481171c7141c14156ae60d48
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:31:01 +0000

    Bug 257073 - Handle the Catalan middle dot correctly in the spell checker; r=ehsan
    UltraBlame original commit: 8372a7defa6931aae3a4dd1b757f02d2513bb641

--- extensions/spellcheck/src/mozInlineSpellWordUtil.cpp
+++ extensions/spellcheck/src/mozInlineSpellWordUtil.cpp
@@ -44,7 +44,8 @@ inline bool IsIgnorableCharacter(PRUnichar ch)
 inline bool IsConditionalPunctuation(PRUnichar ch)
 {
   return (ch == '\'' ||
-          ch == 0x2019); 
+          ch == 0x2019 || 
+          ch == 0x00B7); 
 }
 
 
