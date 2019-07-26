f8439686968ae2ae35d1a3a953610df4bc7b2743
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:44:36 +0000

    Bug 820735 - [toolbox] Tools shortcuts should work from the toolbox window, r=paul
    UltraBlame original commit: 6d8524aaaebbc43b3f90abf9ff7a07365897a3f8

---
+++ browser/devtools/framework/test/browser_toolbox_window_shortcuts.js
@@ -0,0 +1,73 @@
+
+
+
+let temp = {};
+Cu.import("resource:///modules/devtools/Toolbox.jsm", temp);
+let Toolbox = temp.Toolbox;
+temp = null;
+
+let toolbox, toolIDs, idIndex;
+
+function test() {
+  waitForExplicitFinish();
+
+  addTab("about:blank", function() {
+    toolIDs = [];
+    for (let [id, definition] of gDevTools._tools) {
+      if (definition.key) {
+        toolIDs.push(id);
+      }
+    }
+    let target = TargetFactory.forTab(gBrowser.selectedTab);
+    idIndex = 0;
+    gDevTools.showToolbox(target, toolIDs[0], Toolbox.HostType.WINDOW)
+             .then(testShortcuts);
+  });
+}
+
+function testShortcuts(aToolbox, aIndex) {
+  if (aIndex == toolIDs.length) {
+    tidyUp();
+    return;
+  }
+
+  toolbox = aToolbox;
+  info("Toolbox fired a `ready` event");
+
+  toolbox.once("select", selectCB);
+
+  if (aIndex != null) {
+    
+    
+    
+
+    let key = gDevTools._tools.get(toolIDs[aIndex]).key;
+    let toolModifiers = gDevTools._tools.get(toolIDs[aIndex]).modifiers;
+    let modifiers = {
+      accelKey: toolModifiers.contains("accel"),
+      altKey: toolModifiers.contains("alt"),
+      shiftKey: toolModifiers.contains("shift"),
+    };
+    idIndex = aIndex;
+    info("Testing shortcut for tool " + aIndex + ":" + toolIDs[aIndex] +
+         " using key " + key);
+    EventUtils.synthesizeKey(key, modifiers, toolbox.doc.defaultView.parent);
+  }
+}
+
+function selectCB(event, id) {
+  info("toolbox-select event from " + id);
+
+  is(toolIDs.indexOf(id), idIndex,
+     "Correct tool is selected on pressing the shortcut for " + id);
+
+  testShortcuts(toolbox, idIndex + 1);
+}
+
+function tidyUp() {
+  toolbox.destroy();
+  gBrowser.removeCurrentTab();
+
+  toolbox = toolIDs = idIndex = Toolbox = null;
+  finish();
+}
