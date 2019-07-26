610e736a04f7f4ce7477aba05002202b30821d4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:18:55 +0000

    bug 808288 - part 3 - draw missing-glyph hexboxes for <canvas> text, similar to missing glyphs in HTML text. r=bas
    UltraBlame original commit: 5eec0a5b76ff2e180382fa0d84a202f3b433dbd1

--- content/canvas/src/CanvasRenderingContext2D.cpp
+++ content/canvas/src/CanvasRenderingContext2D.cpp
@@ -59,6 +59,7 @@
 #include "gfxFont.h"
 #include "gfxBlur.h"
 #include "gfxUtils.h"
+#include "gfxFontMissingGlyphs.h"
 
 #include "nsFrameManager.h"
 #include "nsFrameLoader.h"
@@ -3039,16 +3040,51 @@ struct NS_STACK_CLASS CanvasBidiProcessor : public nsBidiPresUtils::BidiProcesso
           mTextRun->GetDetailedGlyphs(i);
 
         if (glyphs[i].IsMissing()) {
-          newGlyph.mIndex = 0;
+          float xpos;
+          float advance = detailedGlyphs[0].mAdvance * devUnitsPerAppUnit;
           if (mTextRun->IsRightToLeft()) {
-            newGlyph.mPosition.x = baselineOrigin.x - advanceSum -
-              detailedGlyphs[0].mAdvance * devUnitsPerAppUnit;
+            xpos = baselineOrigin.x - advanceSum - advance;
           } else {
-            newGlyph.mPosition.x = baselineOrigin.x + advanceSum;
+            xpos = baselineOrigin.x + advanceSum;
           }
-          newGlyph.mPosition.y = baselineOrigin.y;
-          advanceSum += detailedGlyphs[0].mAdvance * devUnitsPerAppUnit;
-          glyphBuf.push_back(newGlyph);
+          advanceSum += advance;
+
+          
+          
+          if (advance > 0) {
+            
+            
+
+            
+            
+            Matrix matrix = mCtx->mTarget->GetTransform();
+            nsRefPtr<gfxContext> thebes;
+            if (gfxPlatform::GetPlatform()->SupportsAzureContent()) {
+              
+              
+              
+              
+              
+              
+              
+              
+              thebes = new gfxContext(mCtx->mTarget);
+            } else {
+              nsRefPtr<gfxASurface> drawSurf;
+              mCtx->GetThebesSurface(getter_AddRefs(drawSurf));
+              thebes = new gfxContext(drawSurf);
+            }
+            thebes->SetMatrix(gfxMatrix(matrix._11, matrix._12, matrix._21,
+                                        matrix._22, matrix._31, matrix._32));
+
+            gfxFloat height = font->GetMetrics().maxAscent;
+            gfxRect glyphRect(xpos, baselineOrigin.y - height,
+                              advance, height);
+            gfxFontMissingGlyphs::DrawMissingGlyph(thebes, glyphRect,
+                                                   detailedGlyphs[0].mGlyphID);
+
+            mCtx->mTarget->SetTransform(matrix);
+          }
           continue;
         }
 
