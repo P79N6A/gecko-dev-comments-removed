9e87c02c95f70ebda58060ad00eb17005e089891
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:33:17 +0000

    Bug 747066 - Inline xpc_UnmarkGray (r=terrence)
    UltraBlame original commit: eadbe7e8bfa2a744fa2bd5b1cacce041c3e6c401

--- js/xpconnect/src/xpcpublic.h
+++ js/xpconnect/src/xpcpublic.h
@@ -128,7 +128,7 @@ xpc_FastGetCachedWrapper(nsWrapperCache *cache, JSObject *scope, jsval *vp)
 inline JSBool
 xpc_IsGrayGCThing(void *thing)
 {
-    return js::GCThingIsMarkedGray(thing);
+    return JS::GCThingIsMarkedGray(thing);
 }
 
 
