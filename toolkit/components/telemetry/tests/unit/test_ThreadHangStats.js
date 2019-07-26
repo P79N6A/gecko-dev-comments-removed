fc5f6312465cd317e6af7f0446244edefeba9946
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:25:52 +0000

    Bug 1021774 - Add test for background hang monitor / thread hang stats; r=vladan
    UltraBlame original commit: bb4422ab5f084d2c6d7f2a18d033446819698dd4

---
+++ toolkit/components/telemetry/tests/unit/test_ThreadHangStats.js
@@ -0,0 +1,77 @@
+const Ci = Components.interfaces;
+const Cc = Components.classes;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+function getMainThreadHangStats() {
+  let threads = Services.telemetry.threadHangStats;
+  return threads.find((thread) => (thread.name === "Gecko"));
+}
+
+function run_test() {
+  let startHangs = getMainThreadHangStats();
+
+  
+  
+  
+  if (!startHangs) {
+    ok("Hang reporting not enabled.");
+    return;
+  }
+
+  
+  
+  
+
+  do_execute_soon(() => {
+    
+    let startTime = Date.now();
+    while ((Date.now() - startTime) < 1000) {
+    }
+  });
+
+  do_execute_soon(() => {
+    do_test_pending();
+
+    let check_results = () => {
+      let endHangs = getMainThreadHangStats();
+
+      
+      
+      
+      if (endHangs.hangs.length === startHangs.hangs.length) {
+        do_timeout(100, check_results);
+        return;
+      }
+
+      let check_histogram = (histogram) => {
+        equal(typeof histogram, "object");
+        equal(histogram.histogram_type, 0);
+        equal(typeof histogram.min, "number");
+        equal(typeof histogram.max, "number");
+        equal(typeof histogram.sum, "number");
+        ok(Array.isArray(histogram.ranges));
+        ok(Array.isArray(histogram.counts));
+        equal(histogram.counts.length, histogram.ranges.length);
+      };
+
+      
+      equal(typeof endHangs, "object");
+      check_histogram(endHangs.activity);
+
+      ok(Array.isArray(endHangs.hangs));
+      notEqual(endHangs.hangs.length, 0);
+
+      ok(Array.isArray(endHangs.hangs[0].stack));
+      notEqual(endHangs.hangs[0].stack.length, 0);
+      equal(typeof endHangs.hangs[0].stack[0], "string");
+
+      check_histogram(endHangs.hangs[0].histogram);
+
+      do_test_finished();
+    };
+
+    check_results();
+  });
+}
