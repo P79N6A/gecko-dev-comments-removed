c8729d2b9afc684547993c7e9a8e3e386e3793c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:21:51 +0000

    Bug 953381 - Move CastingApps into a separate JS file r=wesj
    UltraBlame original commit: 2c992bf97aa115031253cd16dd835707b84db067

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -85,6 +85,7 @@ XPCOMUtils.defineLazyModuleGetter(this, "WebappManager",
   ["PluginHelper", "chrome://browser/content/PluginHelper.js"],
   ["OfflineApps", "chrome://browser/content/OfflineApps.js"],
   ["Linkifier", "chrome://browser/content/Linkify.js"],
+  ["CastingApps", "chrome://browser/content/CastingApps.js"],
 ].forEach(function (aScript) {
   let [name, script] = aScript;
   XPCOMUtils.defineLazyGetter(window, name, function() {
@@ -389,7 +390,7 @@ var BrowserApp = {
     Reader.init();
     UserAgentOverrides.init();
     DesktopUserAgent.init();
-    CastApps.init();
+    CastingApps.init();
     Distribution.init();
     Tabs.init();
 #ifdef ACCESSIBILITY
@@ -731,7 +732,7 @@ var BrowserApp = {
     UserAgentOverrides.uninit();
     DesktopUserAgent.uninit();
     ExternalApps.uninit();
-    CastApps.uninit();
+    CastingApps.uninit();
     Distribution.uninit();
     Tabs.uninit();
   },
@@ -8100,233 +8101,6 @@ var ExternalApps = {
     NativeWindow.pageactions.remove(this._pageActionId);
     delete this._pageActionId;
   },
-};
-
-var CastApps = {
-  _castMenuId: -1,
-
-  init: function ca_init() {
-    
-    SimpleServiceDiscovery.search(120000);
-
-    this._castMenuId = NativeWindow.contextmenus.add(
-      Strings.browser.GetStringFromName("contextmenu.castToScreen"),
-      this.filterCast,
-      this.openExternal.bind(this)
-    );
-  },
-
-  uninit: function ca_uninit() {
-    NativeWindow.contextmenus.remove(this._castMenuId);
-  },
-
-  makeURI: function makeURI(aURL, aOriginCharset, aBaseURI) {
-    return Services.io.newURI(aURL, aOriginCharset, aBaseURI);
-  },
-
-  getVideo: function(aElement, aX, aY) {
-    dump("XXX start looking for a video")
-    
-    let video = this._getVideo(aElement);
-    if (video) {
-      return video;
-    }
-
-    
-    
-    try {
-      
-      
-      let elements = aElement.ownerDocument.querySelectorAll("video");
-      for (let element of elements) {
-        
-        let rect = element.getBoundingClientRect();
-        if (aY >= rect.top && aX >= rect.left && aY <= rect.bottom && aX <= rect.right) {
-          video = this._getVideo(element);
-          if (video) {
-            break;
-          }
-        }
-      }
-    } catch(e) {}
-
-    
-    return video;
-  },
-
-  _getVideo: function(aElement) {
-    dump("XXX getvideo for: " + aElement.localName);
-    
-    if (!aElement instanceof HTMLVideoElement) {
-      return null;
-    }
-
-    function allowableExtension(aURI) {
-      if (aURI && aURI instanceof Ci.nsIURL) {
-        return (aURI.fileExtension == "mp4");
-      }
-      return false;
-    }
-
-    
-    let posterURL = aElement.poster;
-
-    
-    let sourceURL = aElement.src;
-
-    
-    if (!sourceURL) {
-      sourceURL = aElement.currentSrc;
-    }
-
-    dump("XXX got video src: " + sourceURL);
-    if (sourceURL) {
-      
-      let sourceURI = this.makeURI(sourceURL, null, this.makeURI(aElement.baseURI));
-      if (allowableExtension(sourceURI)) {
-        dump("XXX: found source via ext: " + sourceURI.spec)
-        return { video: aElement, source: sourceURI.spec, poster: posterURL };
-      }
-    }
-
-    
-    
-    let sourceNodes = aElement.getElementsByTagName("source");
-    for (let sourceNode of sourceNodes) {
-      let sourceURI = this.makeURI(sourceNode.src, null, this.makeURI(sourceNode.baseURI));
-
-      
-      if (sourceNode.type == "video/mp4") {
-        dump("XXX: found source via mime: " + sourceURI.spec)
-        return { video: aElement, source: sourceURI.spec, poster: posterURL };
-      }
-
-      
-      if (allowableExtension(sourceURI)) {
-        dump("XXX: found source via ext: " + sourceURI.spec)
-        return { video: aElement, source: sourceURI.spec, poster: posterURL };
-      }
-    }
-
-    return null;
-  },
-
-  filterCast: {
-    matches: function(aElement, aX, aY) {
-      dump("XXX filterCast")
-      if (SimpleServiceDiscovery.services.length == 0)
-        return false;
-      let video = CastApps.getVideo(aElement, aX, aY);
-      if (CastApps.session) {
-        return (video && CastApps.session.data.source != video.source);
-      }
-      return (video != null);
-    }
-  },
-
-  prompt: function(aCallback) {
-    let items = [];
-    SimpleServiceDiscovery.services.forEach(function(aService) {
-      let item = {
-        label: aService.friendlyName,
-        selected: false
-      };
-      items.push(item);
-    });
-
-    let prompt = new Prompt({
-      title: Strings.browser.GetStringFromName("casting.prompt")
-    }).setSingleChoiceItems(items).show(function(data) {
-      let selected = data.button;
-      let service = selected == -1 ? null : SimpleServiceDiscovery.services[selected];
-      if (aCallback)
-        aCallback(service);
-    });
-  },
-
-  openExternal: function(aElement, aX, aY) {
-    
-    let video = this.getVideo(aElement, aX, aY);
-    if (video) {
-      this.prompt(function(aService) {
-        if (!aService)
-          return;
-
-        
-        let app = SimpleServiceDiscovery.findAppForService(aService, "video-sharing");
-        if (!app)
-          return;
-
-        video.title = aElement.ownerDocument.defaultView.top.document.title;
-        if (video.element) {
-          
-          if (!video.element.paused) {
-            video.element.pause();
-          }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
-        dump("XXX: " + video.poster);
-
-        app.stop(function() {
-          app.start("", function() {
-            app.remoteMedia(function(aRemoteMedia) {
-              dump("XXX starting remote media")
-              this.session = {
-                service: aService,
-                app: app,
-                data: {
-                  title: video.title,
-                  source: video.source,
-                  poster: video.poster
-                }
-              };
-            }.bind(this), this);
-          }.bind(this));
-        }.bind(this));
-      }.bind(this));
-    }
-  },
-
-  
-  onRemoteMediaStart: function(aRemoteMedia) {
-    dump("XXX remote media ready")
-    if (this.session) {
-      dump("XXX remote media load");
-      aRemoteMedia.load(this.session.data);
-    }
-  },
-
-  onRemoteMediaStop: function(aRemoteMedia) {
-    dump("XXX remote media shutting down")
-  },
-
-  onRemoteMediaStatus: function(aRemoteMedia) {
-    dump("XXX remote media status")
-    if (this.session) {
-      let status = aRemoteMedia.status;
-      if (status == "completed") {
-        dump("XXX got a completed");
-        aRemoteMedia.shutdown();
-        this.session.app.stop();
-        delete this.session;
-      }
-    }
-  }
 };
 
 var Distribution = {
