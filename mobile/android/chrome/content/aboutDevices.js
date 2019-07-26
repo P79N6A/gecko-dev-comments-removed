53cd583a5b6e46243e7d6b428ad031eb0d791249
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:53:07 +0000

    Bug 1000676 - Part 1: Add aboutDevices.* files. r=mfinkle
    UltraBlame original commit: 9935f62639400bfd63bf6f1e0b4c555c150a8a57

---
+++ mobile/android/chrome/content/aboutDevices.js
@@ -0,0 +1,97 @@
+
+
+
+
+
+"use strict";
+
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+
+Cu.import("resource://gre/modules/Services.jsm"); 
+Cu.import("resource://gre/modules/Messaging.jsm");
+Cu.import("resource://gre/modules/SimpleServiceDiscovery.jsm"); 
+
+
+const EVENT_SERVICE_FOUND = "ssdp-service-found";
+const EVENT_SERVICE_LOST = "ssdp-service-lost";
+
+
+
+
+const SEARCH_INTERVAL_IN_MILLISECONDS = 5 * 1000;
+
+function dump(s) {
+  Services.console.logStringMessage("aboutDevices :: " + s);
+}
+
+var Devices = {
+  _savedSearchInterval: -1,
+
+  init: function() {
+    dump("Initializing.");
+    Services.obs.addObserver(this, EVENT_SERVICE_FOUND, false);
+    Services.obs.addObserver(this, EVENT_SERVICE_LOST, false);
+
+    let button = document.getElementById("refresh");
+    button.addEventListener("click", () => {
+      this.updateDeviceList();
+    }, false);
+
+    
+    
+    this._savedSearchInterval = SimpleServiceDiscovery._searchInterval;
+    SimpleServiceDiscovery.search(SEARCH_INTERVAL_IN_MILLISECONDS);
+
+    this.updateDeviceList();
+  },
+
+  uninit: function() {
+    dump("Uninitializing.");
+    Services.obs.removeObserver(this, EVENT_SERVICE_FOUND);
+    Services.obs.removeObserver(this, EVENT_SERVICE_LOST);
+
+    if (this._savedSearchInterval > 0) {
+      SimpleServiceDiscovery.search(this._savedSearchInterval);
+    }
+  },
+
+  _createItemForDevice: function(device) {
+    let item = document.createElement("div");
+
+    let friendlyName = document.createElement("div");
+    friendlyName.classList.add("name");
+    friendlyName.textContent = device.friendlyName;
+    item.appendChild(friendlyName);
+
+    let location = document.createElement("div");
+    location.classList.add("location");
+    location.textContent = device.location;
+    item.appendChild(location);
+
+    return item;
+  },
+
+  updateDeviceList: function() {
+    let services = SimpleServiceDiscovery.services;
+    dump("Updating device list with " + services.length + " services.");
+
+    let list = document.getElementById("devices-list");
+    while (list.firstChild) {
+      list.removeChild(list.firstChild);
+    }
+
+    for (let service of services) {
+      let item = this._createItemForDevice(service);
+      list.appendChild(item);
+    }
+  },
+
+  observe: function(subject, topic, data) {
+    if (topic == EVENT_SERVICE_FOUND || topic == EVENT_SERVICE_LOST) {
+      this.updateDeviceList();
+    }
+  },
+};
+
+window.addEventListener("load", Devices.init.bind(Devices), false);
+window.addEventListener("unload", Devices.uninit.bind(Devices), false);
