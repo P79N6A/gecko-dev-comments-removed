d49310fa793a9d70ebd1a3b3003b7f7dc8e175b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:42:55 +0000

    Bug 872143 - Make InputWidgetHelper use async prompts. r=mfinkle
    UltraBlame original commit: d3106edd4a5a31029afad97c1c3fa4bf13eb2fd6

--- mobile/android/chrome/content/InputWidgetHelper.js
+++ mobile/android/chrome/content/InputWidgetHelper.js
@@ -23,44 +23,41 @@ var InputWidgetHelper = {
 
   show: function(aElement) {
     let type = aElement.getAttribute('type');
-    let msg = {
-      type: "Prompt:Show",
+    let p = new Prompt({
+      window: aElement.ownerDocument.defaultView,
       title: Strings.browser.GetStringFromName("inputWidgetHelper." + aElement.getAttribute('type')),
       buttons: [
         Strings.browser.GetStringFromName("inputWidgetHelper.set"),
         Strings.browser.GetStringFromName("inputWidgetHelper.clear"),
         Strings.browser.GetStringFromName("inputWidgetHelper.cancel")
       ],
-      inputs: [
-        { type: type, value: aElement.value }
-      ]
-    };
-
-    let data = JSON.parse(sendMessageToJava(msg));
-
-    let changed = false;
-    if (data.button == -1) {
-      
-      return;
-    }
-    if (data.button == 1) {
-      
-      if (aElement.value != "") {
-        aElement.value = "";
-        changed = true;
+    }).addDatePicker({
+      value: aElement.value,
+      type: type,
+    }).show((function(data) {
+      let changed = false;
+      if (data.button == -1) {
+        
+        return;
       }
-    } else if (data.button == 0) {
-      
-      if (aElement.value != data[type]) {
-        aElement.value = data[type];
-        changed = true;
+      if (data.button == 1) {
+        
+        if (aElement.value != "") {
+          aElement.value = "";
+          changed = true;
+        }
+      } else if (data.button == 0) {
+        
+        if (aElement.value != data[type]) {
+          aElement.value = data[type + "0"];
+          changed = true;
+        }
       }
-    }
-    
+      
 
-    if (changed)
-      this.fireOnChange(aElement);
-
+      if (changed)
+        this.fireOnChange(aElement);
+    }).bind(this));
   },
 
   _isValidInput: function(aElement) {
