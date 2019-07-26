43f45940acab7783466b50f1ad4f8e40d0a45dcb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:14:12 +0000

    Bug 855471 - Rename OnInterceptTouchListener to TouchEventInterceptor. r=mfinkle
    UltraBlame original commit: 8d6834c316fe40a289645f89c69a026c97677f81

--- mobile/android/base/TouchEventInterceptor.java
+++ mobile/android/base/TouchEventInterceptor.java
@@ -8,7 +8,7 @@ package org.mozilla.gecko;
 import android.view.MotionEvent;
 import android.view.View;
 
-public interface OnInterceptTouchListener extends View.OnTouchListener {
+public interface TouchEventInterceptor extends View.OnTouchListener {
     
     public boolean onInterceptTouchEvent(View view, MotionEvent event);
 }
