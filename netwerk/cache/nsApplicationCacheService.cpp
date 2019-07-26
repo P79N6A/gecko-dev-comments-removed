ad8d35ebef0ecb42ccf8620559d544a43fe2eb73
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:08:26 +0000

    Bug 786299 - Delete app-cache related to an app when uninstalled. r=honza,jduell
    UltraBlame original commit: ad81145cad9d29ead4f8d2b16333f162ddc0b457

--- netwerk/cache/nsApplicationCacheService.cpp
+++ netwerk/cache/nsApplicationCacheService.cpp
@@ -6,12 +6,17 @@
 #include "nsDiskCacheDeviceSQL.h"
 #include "nsCacheService.h"
 #include "nsApplicationCacheService.h"
-
+#include "nsCRT.h"
 #include "nsNetUtil.h"
+#include "nsIObserverService.h"
 
 using namespace mozilla;
 
 static NS_DEFINE_CID(kCacheServiceCID, NS_CACHESERVICE_CID);
+
+
+
+
 
 NS_IMPL_ISUPPORTS1(nsApplicationCacheService, nsIApplicationCacheService)
 
@@ -130,6 +135,18 @@ nsApplicationCacheService::CacheOpportunistically(nsIApplicationCache* cache,
 }
 
 NS_IMETHODIMP
+nsApplicationCacheService::DiscardByAppId(int32_t appID, bool isInBrowser)
+{
+    if (!mCacheService)
+        return NS_ERROR_UNEXPECTED;
+
+    nsRefPtr<nsOfflineCacheDevice> device;
+    nsresult rv = mCacheService->GetOfflineDevice(getter_AddRefs(device));
+    NS_ENSURE_SUCCESS(rv, rv);
+    return device->DiscardByAppId(appID, isInBrowser);
+}
+
+NS_IMETHODIMP
 nsApplicationCacheService::GetGroups(uint32_t *count,
                                      char ***keys)
 {
@@ -154,3 +171,53 @@ nsApplicationCacheService::GetGroupsTimeOrdered(uint32_t *count,
     NS_ENSURE_SUCCESS(rv, rv);
     return device->GetGroupsTimeOrdered(count, keys);
 }
+
+
+
+
+
+
+namespace {
+
+class AppCacheClearDataObserver MOZ_FINAL : public nsIObserver {
+public:
+    NS_DECL_ISUPPORTS
+
+    
+    NS_IMETHODIMP
+    Observe(nsISupports *aSubject, const char *aTopic, const PRUnichar *aData)
+    {
+        MOZ_ASSERT(!nsCRT::strcmp(aTopic, TOPIC_WEB_APP_CLEAR_DATA));
+
+        uint32_t appId = NECKO_UNKNOWN_APP_ID;
+        bool browserOnly = false;
+        nsresult rv = NS_GetAppInfoFromClearDataNotification(aSubject, &appId,
+                                                             &browserOnly);
+        NS_ENSURE_SUCCESS(rv, rv);
+
+        nsCOMPtr<nsIApplicationCacheService> cacheService =
+            do_GetService(NS_APPLICATIONCACHESERVICE_CONTRACTID, &rv);
+        NS_ENSURE_SUCCESS(rv, rv);
+
+        return cacheService->DiscardByAppId(appId, browserOnly);
+    }
+};
+
+NS_IMPL_ISUPPORTS1(AppCacheClearDataObserver, nsIObserver)
+
+} 
+
+
+void
+nsApplicationCacheService::AppClearDataObserverInit()
+{
+    nsCOMPtr<nsIObserverService> observerService =
+        do_GetService("@mozilla.org/observer-service;1");
+    if (observerService) {
+        nsRefPtr<AppCacheClearDataObserver> obs
+            = new AppCacheClearDataObserver();
+        observerService->AddObserver(obs, TOPIC_WEB_APP_CLEAR_DATA,
+                                      false);
+    }
+}
+
