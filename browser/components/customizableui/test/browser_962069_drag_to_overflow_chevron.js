47767a6a04f595166bce6c27c73cefc3630c6667
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:04:53 +0000

    Bug 962069 - Dragging an item on to the toolbar overflow chevron should open the overflow popup. r=mikedeboer
    UltraBlame original commit: 8132875a45153a33dbb289c02bc24aa84addf537

---
+++ browser/components/customizableui/test/browser_962069_drag_to_overflow_chevron.js
@@ -0,0 +1,39 @@
+
+
+
+
+"use strict";
+
+let originalWindowWidth;
+
+
+add_task(function*() {
+  originalWindowWidth = window.outerWidth;
+  let navbar = document.getElementById(CustomizableUI.AREA_NAVBAR);
+  ok(!navbar.hasAttribute("overflowing"), "Should start with a non-overflowing toolbar.");
+  ok(CustomizableUI.inDefaultState, "Should start in default state.");
+  let oldChildCount = navbar.customizationTarget.childElementCount;
+  window.resizeTo(400, window.outerHeight);
+  yield waitForCondition(() => navbar.hasAttribute("overflowing"));
+  ok(navbar.hasAttribute("overflowing"), "Should have an overflowing toolbar.");
+
+  let widgetOverflowPanel = document.getElementById("widget-overflow");
+  let panelShownPromise = promisePanelElementShown(window, widgetOverflowPanel);
+  let identityBox = document.getElementById("identity-box");
+  let overflowChevron = document.getElementById("nav-bar-overflow-button");
+  ChromeUtils.synthesizeDrop(identityBox, overflowChevron, [], null);
+  yield panelShownPromise;
+
+  ok(true, "Overflow panel is shown.");
+
+  let panelHiddenPromise = promisePanelElementHidden(window, widgetOverflowPanel);
+  EventUtils.synthesizeKey("VK_ESCAPE", {});
+  yield panelHiddenPromise;
+});
+
+add_task(function*() {
+  window.resizeTo(originalWindowWidth, window.outerHeight);
+  let navbar = document.getElementById(CustomizableUI.AREA_NAVBAR);
+  yield waitForCondition(() => !navbar.hasAttribute("overflowing"));
+  ok(!navbar.hasAttribute("overflowing"), "Should not have an overflowing toolbar.");
+});
