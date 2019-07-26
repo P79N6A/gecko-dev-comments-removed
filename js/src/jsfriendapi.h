7c325da3e832e6b28264bddac9dab72e7d4b3bef
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:35:39 +0000

    Bug 820862.  JSJitInfo should say what sort of function it is.  r=jandem
    UltraBlame original commit: 55b2d38ac36c986f3d76117be170f17e7dbde297

--- js/src/jsfriendapi.h
+++ js/src/jsfriendapi.h
@@ -1424,9 +1424,16 @@ typedef bool
                   void *specializedThis, unsigned argc, JS::Value *vp);
 
 struct JSJitInfo {
+    enum OpType {
+        Getter,
+        Setter,
+        Method
+    };
+
     JSJitPropertyOp op;
     uint32_t protoID;
     uint32_t depth;
+    OpType type;
     bool isInfallible;    
     bool isConstant;      
 };
