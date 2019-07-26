6c41eadff71c8f9ceceb0fa8200566a7368350d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:15:09 +0000

    Bug 957358 - Restrict everything in Char16.h to C++; r=jcranmer
    UltraBlame original commit: 6a9730f94bae96bc7ab3fc1b7b74e2922fbd9f60

--- mfbt/Char16.h
+++ mfbt/Char16.h
@@ -9,6 +9,7 @@
 #ifndef mozilla_Char16_h
 #define mozilla_Char16_h
 
+#ifdef __cplusplus
 
 
 
@@ -29,17 +30,11 @@
 
 
 
-
 #  define MOZ_UTF16_HELPER(s) L##s
 #  define _CHAR16T
-#  ifdef __cplusplus
-     typedef wchar_t char16_t;
-#  else
-     typedef unsigned short char16_t;
-#  endif
+   typedef wchar_t char16_t;
    typedef unsigned int char32_t;
-#elif defined(__cplusplus) && \
-      (__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
+#else
    
 #  define MOZ_UTF16_HELPER(s) u##s
    
@@ -50,20 +45,6 @@
 #  ifdef WIN32
 #    define MOZ_USE_CHAR16_WRAPPER
 #  endif
-#elif !defined(__cplusplus)
-#  if defined(WIN32)
-#    include <yvals.h>
-     typedef wchar_t char16_t;
-#  else
-     
-
-
-
-
-     typedef unsigned short char16_t;
-#  endif
-#else
-#  error "Char16.h requires C++11 (or something like it) for UTF-16 support."
 #endif
 
 #ifdef MOZ_USE_CHAR16_WRAPPER
@@ -178,12 +159,11 @@ typedef const char16_t* char16ptr_t;
 
 #define MOZ_UTF16(s) MOZ_UTF16_HELPER(s)
 
-#if defined(__cplusplus) && \
-    (__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
 static_assert(sizeof(char16_t) == 2, "Is char16_t type 16 bits?");
 static_assert(char16_t(-1) > char16_t(0), "Is char16_t type unsigned?");
 static_assert(sizeof(MOZ_UTF16('A')) == 2, "Is char literal 16 bits?");
 static_assert(sizeof(MOZ_UTF16("")[0]) == 2, "Is string char 16 bits?");
+
 #endif
 
 #endif 
