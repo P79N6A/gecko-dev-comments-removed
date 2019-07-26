5901e734c2bc0e1642603e9d34c2b14f89a71cea
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:59:53 +0000

    Bug 1002300: Only lazily create the backbuffer for ContentClientDoubleBuffered. r=nical
    UltraBlame original commit: 96395ad2aac99f732bee3e62e1c741a22bc72009

--- gfx/layers/client/ContentClient.h
+++ gfx/layers/client/ContentClient.h
@@ -268,9 +268,15 @@ protected:
                            const nsIntRect& aRect,
                            uint32_t aFlags);
 
+  void CreateBackBuffer(const nsIntRect& aBufferRect);
+
+
   
   
-  virtual void CreateFrontBuffer(const nsIntRect& aBufferRect) = 0;
+  virtual void EnsureBackBufferIfFrontBuffer() {}
+
+  
+  
   virtual void DestroyFrontBuffer() {}
 
   bool CreateAndAllocateTextureClient(RefPtr<TextureClient>& aClient,
@@ -331,8 +337,9 @@ public:
 
   virtual void FinalizeFrame(const nsIntRegion& aRegionToDraw) MOZ_OVERRIDE;
 
+  virtual void EnsureBackBufferIfFrontBuffer() MOZ_OVERRIDE;
+
 protected:
-  virtual void CreateFrontBuffer(const nsIntRect& aBufferRect) MOZ_OVERRIDE;
   virtual void DestroyFrontBuffer() MOZ_OVERRIDE;
 
 private:
@@ -373,9 +380,6 @@ public:
   virtual ~ContentClientSingleBuffered() {}
 
   virtual void FinalizeFrame(const nsIntRegion& aRegionToDraw) MOZ_OVERRIDE;
-
-protected:
-  virtual void CreateFrontBuffer(const nsIntRect& aBufferRect) MOZ_OVERRIDE {}
 };
 
 
