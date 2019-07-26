1a21c2c8169e83fa61069775443770c12516829e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:26:21 +0000

    Bug 932274: Use Moz2D size classes in ipdl r=nical
    UltraBlame original commit: bad6c063b01f64a18986a847d71346cacf51a788

--- content/base/src/nsFrameLoader.h
+++ content/base/src/nsFrameLoader.h
@@ -375,7 +375,7 @@ private:
   bool TryRemoteBrowser();
 
   
-  bool ShowRemoteFrame(const nsIntSize& size,
+  bool ShowRemoteFrame(const mozilla::ScreenIntSize& size,
                        nsSubDocumentFrame *aFrame = nullptr);
 
   bool AddTreeItemToTreeOwner(nsIDocShellTreeItem* aItem,
