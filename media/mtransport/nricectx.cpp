a65771bcc7847530781ec85d11916d468b6b0edc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:22:50 +0000

    Bug 906990 - Part 11. Enable r_log and RLogRingBuffer so logging can be scraped. Also, tweak log levels so the RLogRingBuffer isn't rapidly overwritten by media packet logging. r=ekr
    UltraBlame original commit: 1cbb486b4fb73f257088ce3d3a1db974a3b79c88

--- media/mtransport/nricectx.cpp
+++ media/mtransport/nricectx.cpp
@@ -83,6 +83,7 @@ extern "C" {
 #include "nricemediastream.h"
 #include "nr_socket_prsock.h"
 #include "nrinterfaceprioritizer.h"
+#include "rlogringbuffer.h"
 
 namespace mozilla {
 
@@ -339,6 +340,7 @@ RefPtr<NrIceCtx> NrIceCtx::Create(const std::string& name,
   
   if (!initialized) {
     NR_reg_init(NR_REG_MODE_LOCAL);
+    RLogRingBuffer::CreateInstance();
     nr_crypto_vtbl = &nr_ice_crypto_nss_vtbl;
     initialized = true;
 
