a6bc8595aacfffe74c7341abc7fb07ef2e7bef67
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:21:40 +0000

    Bug 983910 - Set mParentListener for HttpChannelParent objects created after HTTP redirects r=jduell
    UltraBlame original commit: 636fa31743682646c76d8d93fcef5d900e3d2ee5

--- netwerk/protocol/http/nsHttpChannel.h
+++ netwerk/protocol/http/nsHttpChannel.h
@@ -185,6 +185,10 @@ public:
 
     void ForcePending(bool aForcePending);
 
+    already_AddRefed<nsIStreamListener> GetListener() {
+        return nsCOMPtr<nsIStreamListener>(mListener).forget();
+    }
+
 private:
     typedef nsresult (nsHttpChannel::*nsContinueRedirectionFunc)(nsresult result);
 
