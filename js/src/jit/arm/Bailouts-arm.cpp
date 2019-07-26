d88a85fe4a964ec5a021147ddd1c6c9f440a8bca
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:23:11 +0000

    Bug 962095 - Make sure BailoutStack is 8-byte aligned on ARM. r=nbp
    UltraBlame original commit: 2745ef89421e7f0730d7e14abeef3ab072105180

--- js/src/jit/arm/Bailouts-arm.cpp
+++ js/src/jit/arm/Bailouts-arm.cpp
@@ -76,11 +76,12 @@ class BailoutStack
         uintptr_t tableOffset_;
     };
 
-  private:
+  protected: 
     mozilla::Array<double, FloatRegisters::Total> fpregs_;
     mozilla::Array<uintptr_t, Registers::Total> regs_;
 
     uintptr_t snapshotOffset_;
+    uintptr_t padding_;
 
   public:
     FrameSizeClass frameClass() const {
@@ -108,6 +109,9 @@ class BailoutStack
         return (uint8_t *)this + offsetof(BailoutStack, snapshotOffset_);
     }
 };
+
+
+static_assert((sizeof(BailoutStack) % 8) == 0, "BailoutStack should be 8-byte aligned.");
 
 } 
 } 
