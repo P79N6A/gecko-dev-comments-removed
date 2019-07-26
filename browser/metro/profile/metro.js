18380f9f64b4849893a4a3d379c533adf1da2606
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:13:45 +0000

    Bug 854881 - Remove broken Master Password support from /browser/metro [r=mbrubeck]
    UltraBlame original commit: 5c4bad14048d6b6551bf11baaa1c583715af9e95

--- browser/metro/profile/metro.js
+++ browser/metro/profile/metro.js
@@ -172,7 +172,6 @@ pref("browser.helperApps.deleteTempFileOnExit", false);
 
 
 pref("signon.rememberSignons", true);
-pref("signon.expireMasterPassword", false);
 pref("signon.SignonFileName", "signons.txt");
 
 
