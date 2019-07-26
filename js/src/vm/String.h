5fdcf502680cf1323275808e33c1ab646ad2471d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:04:26 +0000

    Bug 1013917 part 2 - Add shell functions for testing latin1 strings. r=luke
    UltraBlame original commit: eb24106356d9348e09f36eacac9bd8b367b4c01f

--- js/src/vm/String.h
+++ js/src/vm/String.h
@@ -592,6 +592,9 @@ class JSLinearString : public JSString
         JS_ASSERT(JSString::isLinear());
         return JS::TwoByteChars(chars(), length());
     }
+
+    
+    void debugUnsafeConvertToLatin1();
 };
 
 JS_STATIC_ASSERT(sizeof(JSLinearString) == sizeof(JSString));
