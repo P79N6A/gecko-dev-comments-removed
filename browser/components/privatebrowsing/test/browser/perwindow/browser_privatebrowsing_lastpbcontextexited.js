caf9371dc07c0e1ab4ea4fa34f1cc286512374aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:44:55 +0000

    Test case for bug 828780; r=jdm
    UltraBlame original commit: e6ab6c9b58856f260587bd74c123abb189b43281

--- browser/components/privatebrowsing/test/browser/perwindow/browser_privatebrowsing_lastpbcontextexited.js
+++ browser/components/privatebrowsing/test/browser/perwindow/browser_privatebrowsing_lastpbcontextexited.js
@@ -5,22 +5,45 @@
 function test() {
   
   
-  let newWin = OpenBrowserWindow({private: true});
+  function runTest(aCloseWindow, aCallback) {
+    let newWin = OpenBrowserWindow({private: true});
+    SimpleTest.waitForFocus(function() {
+      let expectedExiting = true;
+      let expectedExited = false;
+      let observerExiting = {
+        observe: function(aSubject, aTopic, aData) {
+          is(aTopic, "last-pb-context-exiting", "Correct topic should be dispatched (exiting)");
+          is(expectedExiting, true, "notification not expected yet (exiting)");
+          expectedExited = true;
+          Services.obs.removeObserver(observerExiting, "last-pb-context-exiting", false);
+        }
+      };
+      let observerExited = {
+        observe: function(aSubject, aTopic, aData) {
+          is(aTopic, "last-pb-context-exited", "Correct topic should be dispatched (exited)");
+          is(expectedExited, true, "notification not expected yet (exited)");
+          Services.obs.removeObserver(observerExited, "last-pb-context-exited", false);
+          aCallback();
+        }
+      };
+      Services.obs.addObserver(observerExiting, "last-pb-context-exiting", false);
+      Services.obs.addObserver(observerExited, "last-pb-context-exited", false);
+      expectedExiting = true;
+      aCloseWindow(newWin);
+      newWin = null;
+      SpecialPowers.forceGC();
+    }, newWin);
+  }
+
   waitForExplicitFinish();
-  SimpleTest.waitForFocus(function() {
-    let expected = false;
-    let observer = {
-      observe: function(aSubject, aTopic, aData) {
-        is(aTopic, "last-pb-context-exited", "Correct topic should be dispatched");
-        is(expected, true, "notification not expected yet");
-        Services.obs.removeObserver(observer, "last-pb-context-exited", false);
-        finish();
-      }
-    };
-    Services.obs.addObserver(observer, "last-pb-context-exited", false);
-    expected = true;
-    newWin.close(); 
-    newWin = null;
-    SpecialPowers.forceGC();
-  }, newWin);
+
+  runTest(function(newWin) {
+      
+      newWin.document.getElementById("cmd_closeWindow").doCommand();
+    }, function () {
+      runTest(function(newWin) {
+          
+          newWin.document.getElementById("cmd_close").doCommand();
+        }, finish);
+    });
 }
