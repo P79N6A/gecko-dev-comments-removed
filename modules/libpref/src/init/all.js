cd258e67481471989c61dacd33ecdfa9e1b5a77d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:01:43 +0000

    Backed out changeset c744c837c732 (bug 789261) for Gaia-unit test bustage on a CLOSED TREE
    UltraBlame original commit: b06c542b2bf9ea3148fa33d93fcd67871e21fb34

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -766,7 +766,7 @@ pref("dom.webapps.useCurrentProfile", false);
 
 pref("dom.cycle_collector.incremental", true);
 
-pref("dom.window_experimental_bindings", true);
+pref("dom.window_experimental_bindings", false);
 
 
 #ifndef XP_WIN
