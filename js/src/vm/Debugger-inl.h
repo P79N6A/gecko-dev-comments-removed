08cec7f06eed2c8e6e2bd5492bbe989a9ea96871
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:45:18 +0000

    Bug 879831, part 6 - Factor out Debugger inlines that depend on other "inlines" headers into vm/Debugger-inl.h. r=njn.
    UltraBlame original commit: 9d940604f3be5fa1f035b492bfc5e27b34ad7725

---
+++ js/src/vm/Debugger-inl.h
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+#ifndef Debugger_inl_h__
+#define Debugger_inl_h__
+
+#include "vm/Debugger.h"
+
+#include "vm/Stack-inl.h"
+
+bool
+js::Debugger::onLeaveFrame(JSContext *cx, AbstractFramePtr frame, bool ok)
+{
+    
+    bool evalTraps = frame.isEvalFrame() &&
+                     frame.script()->hasAnyBreakpointsOrStepMode();
+    if (!cx->compartment->getDebuggees().empty() || evalTraps)
+        ok = slowPathOnLeaveFrame(cx, frame, ok);
+    return ok;
+}
+
+#endif  
