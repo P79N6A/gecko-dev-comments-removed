81b4cd089fa6fc5acdf314cc8db2a6e96c783229
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:00:25 +0000

    Bug 977071 - Add run_info to structured log suite_start message and fix some small bugs. r=ahal
    UltraBlame original commit: 9adf062bc5a344d2314498ce168d915632367c75

--- testing/mozbase/mozlog/mozlog/__init__.py
+++ testing/mozbase/mozlog/mozlog/__init__.py
@@ -9,8 +9,7 @@ It simply wraps Python's logging_ module and adds a few convenience methods
 for logging test results and events.
 
 The structured submodule takes a different approach and implements a
-JSON-based logging protocol designed for recording test results.
-"""
+JSON-based logging protocol designed for recording test results."""
 
 from logger import *
 from loglistener import LogMessageServer
@@ -22,3 +21,4 @@ except ImportError:
     
     
     pass
+
--- testing/mozbase/mozlog/mozlog/structured/formatters/html/html.py
+++ testing/mozbase/mozlog/mozlog/structured/formatters/html/html.py
@@ -4,6 +4,7 @@
 
 
 import sys
+import json
 import datetime
 import os
 
