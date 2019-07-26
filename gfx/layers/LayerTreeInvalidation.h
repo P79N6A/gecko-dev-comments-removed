2a99788c642c507c5a320c8b4db0b0ede038ae02
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:20:50 +0000

    Bug 805197 - Compute a region in LayerTreeInvalidation. r=roc
    UltraBlame original commit: 82cb8e969e0c40855a6d7b0e3f2003799323e9de

--- gfx/layers/LayerTreeInvalidation.h
+++ gfx/layers/LayerTreeInvalidation.h
@@ -57,8 +57,8 @@ struct LayerProperties
 
 
 
-  virtual nsIntRect ComputeDifferences(Layer* aRoot, 
-                                       NotifySubDocInvalidationFunc aCallback) = 0;
+  virtual nsIntRegion ComputeDifferences(Layer* aRoot, 
+                                         NotifySubDocInvalidationFunc aCallback) = 0;
   
   
   virtual void MoveBy(const nsIntPoint& aOffset) = 0;
