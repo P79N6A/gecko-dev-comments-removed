4a715e63d0159d43ef5d50f917da9593c991681a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:31:51 +0000

    Bug 816168 - Split out WebGLRenderbuffer into separate files - r=bjacob
    UltraBlame original commit: b8b88df64dba01b77d415d6134e119210d75eb30

---
+++ content/canvas/src/WebGLRenderbuffer.h
@@ -0,0 +1,70 @@
+
+
+
+
+
+#ifndef WEBGLRENDERBUFFER_H_
+#define WEBGLRENDERBUFFER_H_
+
+#include "WebGLObjectModel.h"
+
+#include "nsWrapperCache.h"
+
+#include "mozilla/LinkedList.h"
+
+namespace mozilla {
+
+class WebGLRenderbuffer MOZ_FINAL
+    : public nsISupports
+    , public WebGLRefCountedObject<WebGLRenderbuffer>
+    , public LinkedListElement<WebGLRenderbuffer>
+    , public WebGLRectangleObject
+    , public WebGLContextBoundObject
+    , public nsWrapperCache
+{
+public:
+    WebGLRenderbuffer(WebGLContext *context);
+
+    ~WebGLRenderbuffer() {
+        DeleteOnce();
+    }
+
+    void Delete();
+
+    bool HasEverBeenBound() { return mHasEverBeenBound; }
+    void SetHasEverBeenBound(bool x) { mHasEverBeenBound = x; }
+    WebGLuint GLName() const { return mGLName; }
+
+    bool Initialized() const { return mInitialized; }
+    void SetInitialized(bool aInitialized) { mInitialized = aInitialized; }
+
+    WebGLenum InternalFormat() const { return mInternalFormat; }
+    void SetInternalFormat(WebGLenum aInternalFormat) { mInternalFormat = aInternalFormat; }
+
+    WebGLenum InternalFormatForGL() const { return mInternalFormatForGL; }
+    void SetInternalFormatForGL(WebGLenum aInternalFormatForGL) { mInternalFormatForGL = aInternalFormatForGL; }
+
+    int64_t MemoryUsage() const;
+
+    WebGLContext *GetParentObject() const {
+        return Context();
+    }
+
+    virtual JSObject* WrapObject(JSContext *cx, JSObject *scope, bool *triedToWrap);
+
+    NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+    NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(WebGLRenderbuffer)
+
+protected:
+
+    WebGLuint mGLName;
+    WebGLenum mInternalFormat;
+    WebGLenum mInternalFormatForGL;
+    bool mHasEverBeenBound;
+    bool mInitialized;
+
+    friend class WebGLFramebuffer;
+};
+} 
+
+#endif
