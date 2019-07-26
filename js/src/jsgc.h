5b9718adda04f36f7bf2f8c85d9fadf3b5360db4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:48:38 +0000

    Bug 943827 - Fix out of memory memory handling for buffered gray roots r=billm
    UltraBlame original commit: dd032c97a9a84559ae7dacb564eea13e4f63673b

--- js/src/jsgc.h
+++ js/src/jsgc.h
@@ -1281,7 +1281,14 @@ struct GCMarker : public JSTracer {
     
     mozilla::DebugOnly<size_t> markLaterArenas;
 
-    bool grayFailed;
+    enum GrayBufferState
+    {
+        GRAY_BUFFER_UNUSED,
+        GRAY_BUFFER_OK,
+        GRAY_BUFFER_FAILED
+    };
+
+    GrayBufferState grayBufferState;
 };
 
 void
