938d482dc59ec851513f4c95b16b11f3e9298744
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:37:56 +0000

    Bug 822804: Mark variables destIdx & destPos in GfxDriverInfo.h as unsigned, to fix build warning for unsigned/signed comparison. r=Bas
    UltraBlame original commit: 84320dffec6e55f92fd01bde61ea367b325850ed

--- widget/xpwidgets/GfxDriverInfo.h
+++ widget/xpwidgets/GfxDriverInfo.h
@@ -149,8 +149,8 @@ inline bool SplitDriverVersion(const char *aSource, char *aAStr, char *aBStr, ch
   
   int len = strlen(aSource);
   char *dest[4] = { aAStr, aBStr, aCStr, aDStr };
-  int destIdx = 0;
-  int destPos = 0;
+  unsigned destIdx = 0;
+  unsigned destPos = 0;
 
   for (int i = 0; i < len; i++) {
     if (destIdx > ArrayLength(dest)) {
