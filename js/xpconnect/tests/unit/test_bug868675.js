1e08fcea0e1de4fee7be3f04caa664acf876a24d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:31:42 +0000

    Bug 868675 - Tests. r=gabor
    UltraBlame original commit: 7b440e4f094ccca2ea6b46f70d996ad922f24224

---
+++ js/xpconnect/tests/unit/test_bug868675.js
@@ -0,0 +1,25 @@
+const Cu = Components.utils;
+function run_test() {
+
+  
+  var result = "threw";
+  try { result = XPCNativeWrapper.unwrap(2); } catch (e) {}
+  do_check_eq(result, 2);
+  result = "threw";
+  try { result = XPCNativeWrapper(2); } catch (e) {}
+  do_check_eq(result, 2);
+
+  
+  
+  var sb = new Cu.Sandbox('http://www.example.com', {wantXHRConstructor: true});
+  Cu.evalInSandbox('this.xhr = new XMLHttpRequest();', sb);
+  Cu.evalInSandbox('this.jsobj = {mynative: xhr};', sb);
+  do_check_true(!Cu.isXrayWrapper(XPCNativeWrapper.unwrap(sb.xhr)));
+  do_check_true(Cu.isXrayWrapper(sb.jsobj.mynative));
+  do_check_true(!Cu.isXrayWrapper(XPCNativeWrapper.unwrap(sb.jsobj).mynative));
+
+  
+  var waived = Cu.waiveXrays(sb.xhr);
+  do_check_true(!Cu.isXrayWrapper(waived));
+  do_check_true(Cu.isXrayWrapper(Cu.unwaiveXrays(waived)));
+}
