ee2b47a620f040fd6fe847cade7d8f586ab80c20
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:54:27 +0000

    Bug 866852 - Enable off main thread compositing by default in metrofx. r=bbondy
    UltraBlame original commit: 40c6c0f358551e0a18b261da5a5bc4031964081f

--- browser/metro/profile/metro.js
+++ browser/metro/profile/metro.js
@@ -22,7 +22,7 @@ pref("metro.debug.selection.dumpRanges", false);
 pref("metro.debug.selection.dumpEvents", false);
 
 
-pref("layers.offmainthreadcomposition.enabled", false);
+pref("layers.offmainthreadcomposition.enabled", true);
 
 
 pref("intl.enable_tsf_support", true);
