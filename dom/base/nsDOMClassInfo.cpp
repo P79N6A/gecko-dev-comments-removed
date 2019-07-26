5349e8590b375c4e8c2e1ba5a26a0bfc3abb3d57
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:42:19 +0000

    Bug 871287 - Part c: Move DeviceStorage to WebIDL; r=smaug
    UltraBlame original commit: 49c962578899b2e25150351a8aa6bf16aef29ed8

--- dom/base/nsDOMClassInfo.cpp
+++ dom/base/nsDOMClassInfo.cpp
@@ -200,9 +200,6 @@
 #include "DOMStorage.h"
 
 
-#include "nsIDOMDeviceStorage.h"
-
-
 #include "nsIDOMDataTransfer.h"
 
 
@@ -670,9 +667,6 @@ static nsDOMClassInfoData sClassInfoData[] = {
   NS_DEFINE_CLASSINFO_DATA(ModalContentWindow, nsWindowSH,
                            DEFAULT_SCRIPTABLE_FLAGS |
                            WINDOW_SCRIPTABLE_FLAGS)
-
-  NS_DEFINE_CLASSINFO_DATA(DeviceStorage, nsEventTargetSH,
-                           EVENTTARGET_SCRIPTABLE_FLAGS)
 
   NS_DEFINE_CLASSINFO_DATA(GeoPositionCoords, nsDOMGenericSH,
                            DOM_DEFAULT_SCRIPTABLE_FLAGS)
@@ -1775,11 +1769,6 @@ nsDOMClassInfo::Init()
 #ifdef MOZ_WEBSPEECH
     DOM_CLASSINFO_MAP_ENTRY(nsISpeechSynthesisGetter)
 #endif
-  DOM_CLASSINFO_MAP_END
-
-  DOM_CLASSINFO_MAP_BEGIN(DeviceStorage, nsIDOMDeviceStorage)
-     DOM_CLASSINFO_MAP_ENTRY(nsIDOMDeviceStorage)
-     DOM_CLASSINFO_MAP_ENTRY(nsIDOMEventTarget)
   DOM_CLASSINFO_MAP_END
 
   DOM_CLASSINFO_MAP_BEGIN(GeoPositionCoords, nsIDOMGeoPositionCoords)
--- dom/base/nsDOMClassInfoClasses.h
+++ dom/base/nsDOMClassInfoClasses.h
@@ -115,8 +115,6 @@ DOMCI_CLASS(File)
 
 DOMCI_CLASS(ModalContentWindow)
 
-DOMCI_CLASS(DeviceStorage)
-
 
 DOMCI_CLASS(GeoPositionCoords)
 
