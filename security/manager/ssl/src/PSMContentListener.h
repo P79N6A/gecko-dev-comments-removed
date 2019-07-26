110b8d487b3e77227a6a4373cc6851a6c102e9e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:53:13 +0000

    Bug 879137, Part 3: Move PSMContentListener to its own source file, r=keeler
    UltraBlame original commit: 6b2f29bc6da8d490c0d8f1f0f4c1c79768b3052b

---
+++ security/manager/ssl/src/PSMContentListener.h
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+#ifndef mozilla_psm_PSMCOntentListener_h_
+#define mozilla_psm_PSMCOntentListener_h_
+
+#include "nsCOMPtr.h"
+#include "nsIURIContentListener.h"
+#include "nsWeakReference.h"
+
+#define NS_PSMCONTENTLISTEN_CID {0xc94f4a30, 0x64d7, 0x11d4, {0x99, 0x60, 0x00, 0xb0, 0xd0, 0x23, 0x54, 0xa0}}
+#define NS_PSMCONTENTLISTEN_CONTRACTID "@mozilla.org/security/psmdownload;1"
+
+namespace mozilla { namespace psm {
+
+class PSMContentListener : public nsIURIContentListener,
+                            public nsSupportsWeakReference {
+public:
+  PSMContentListener();
+  virtual ~PSMContentListener();
+  nsresult init();
+
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIURICONTENTLISTENER
+private:
+  nsCOMPtr<nsISupports> mLoadCookie;
+  nsCOMPtr<nsIURIContentListener> mParentContentListener;
+};
+
+} } 
+
+#endif 
--- security/manager/ssl/src/nsNSSModule.cpp
+++ security/manager/ssl/src/nsNSSModule.cpp
@@ -27,7 +27,7 @@
 #include "nsCryptoHash.h"
 
 #include "nsDOMCID.h"
-
+#include "nsNetCID.h"
 #include "nsCMSSecureMessage.h"
 #include "nsCertPicker.h"
 #include "nsCURILoader.h"
@@ -44,6 +44,9 @@
 #include "nsNSSVersion.h"
 
 #include "nsXULAppAPI.h"
+
+#include "PSMContentListener.h"
+
 #define NS_IS_PROCESS_DEFAULT                                                 \
     (GeckoProcessType_Default == XRE_GetProcessType())
 
