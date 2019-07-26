c9c6dd55935b6a383cfbb80671537394fba932a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:29:57 +0000

    Bug 867911 - Add OpenBSD and NetBSD to list of sparc64 systems with 8k pages. r=terrence
    UltraBlame original commit: 11bbf55bbad1c7d05f3e5d03c5a62e84feb79fcb

--- js/public/HeapAPI.h
+++ js/public/HeapAPI.h
@@ -19,7 +19,7 @@ namespace gc {
 
 
 
-#if (defined(SOLARIS) || defined(__FreeBSD__)) && \
+#if (defined(SOLARIS) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)) && \
     (defined(__sparc) || defined(__sparcv9) || defined(__ia64))
 const size_t PageShift = 13;
 const size_t ArenaShift = PageShift;
