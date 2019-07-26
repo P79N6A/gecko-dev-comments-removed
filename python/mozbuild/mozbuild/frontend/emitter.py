dd95167e7aada24e031936a053ccab801967cf89
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:38:53 +0000

    bug 870406: move CSRCS to moz.build (logic) r=gps
    UltraBlame original commit: e3faa44c33e46f8b82813306d747e2a220faeff6

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -80,6 +80,7 @@ class TreeMetadataEmitter(object):
         varmap = dict(
             
             ASFILES='ASFILES',
+            CSRCS='CSRCS',
             MODULE='MODULE',
             XPIDL_FLAGS='XPIDL_FLAGS',
             XPIDL_MODULE='XPIDL_MODULE',
