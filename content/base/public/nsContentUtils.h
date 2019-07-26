e885b56af4b508760ca528bb056ed1ede0965dff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:23:16 +0000

    Bug 865729 - Make nsContentUtils munge the cx stack with sXPConnect directly. r=gabor
    UltraBlame original commit: dab57d93467c844e087ce16c1b686b7c7f0a35f7

--- content/base/public/nsContentUtils.h
+++ content/base/public/nsContentUtils.h
@@ -64,7 +64,6 @@ class nsIFragmentContentSink;
 class nsIImageLoadingContent;
 class nsIInterfaceRequestor;
 class nsIIOService;
-class nsIJSContextStack;
 class nsIJSRuntimeService;
 class nsILineBreaker;
 class nsIMIMEHeaderParam;
@@ -82,7 +81,6 @@ class nsIScriptSecurityManager;
 class nsIStringBundle;
 class nsIStringBundleService;
 class nsISupportsHashKey;
-class nsIThreadJSContextStack;
 class nsIURI;
 class nsIWidget;
 class nsIWordBreaker;
@@ -1627,11 +1625,6 @@ public:
   static nsresult CheckSameOrigin(nsIChannel *aOldChannel, nsIChannel *aNewChannel);
   static nsIInterfaceRequestor* GetSameOriginChecker();
 
-  static nsIThreadJSContextStack* ThreadJSContextStack()
-  {
-    return sThreadJSContextStack;
-  }
-
   
   static void TraceSafeJSContext(JSTracer* aTrc);
 
@@ -2182,8 +2175,6 @@ private:
   static nsIXPConnect *sXPConnect;
 
   static nsIScriptSecurityManager *sSecurityManager;
-
-  static nsIThreadJSContextStack *sThreadJSContextStack;
 
   static nsIParserService *sParserService;
 
