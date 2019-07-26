11eedcffb2b2665bf326ed5ad120defd95011962
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:55:41 +0000

    Merge inbound to m-c.
    UltraBlame original commit: de6ebd46e7d0a0a0875c685e2d0101d7cf443551

--- python/mozbuild/mozbuild/frontend/data.py
+++ python/mozbuild/mozbuild/frontend/data.py
@@ -24,7 +24,7 @@ from mozbuild.util import (
     shell_quote,
     StrictOrderingOnAppendList,
 )
-from .sandbox_symbols import compute_final_target
+from .sandbox_symbols import FinalTargetValue
 
 
 class TreeMetadata(object):
@@ -504,6 +504,6 @@ class InstallationTarget(SandboxDerived):
         """Returns whether or not the target is not derived from the default
         given xpiname and subdir."""
 
-        return compute_final_target(dict(
+        return FinalTargetValue(dict(
             XPI_NAME=self.xpiname,
             DIST_SUBDIR=self.subdir)) == self.target
