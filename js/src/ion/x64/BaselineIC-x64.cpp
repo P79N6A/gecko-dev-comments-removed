56996866c03f8e229f7fa5cfa8f85e1af95061a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:07:19 +0000

    Bug 814061 - BaselineCompiler: Fix compares on x64. r=djvj
    UltraBlame original commit: 37ad58ffe32b8173ff26a975eae0d9dd6639c316

--- js/src/ion/x64/BaselineIC-x64.cpp
+++ js/src/ion/x64/BaselineIC-x64.cpp
@@ -38,9 +38,7 @@ ICCompare_Int32::Compiler::generateStubCode(MacroAssembler &masm)
     masm.branchTestInt32(Assembler::NotEqual, R1, &failure);
 
     
-    masm.unboxNonDouble(R0, rdx);
-    masm.unboxNonDouble(R1, ScratchReg);
-    masm.cmpq(rdx, ScratchReg);
+    masm.cmpl(R0.valueReg(), R1.valueReg());
     masm.setCC(cond, ScratchReg);
     masm.movzxbl(ScratchReg, ScratchReg);
 
