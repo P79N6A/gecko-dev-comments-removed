674d2b1f10bde6c8c25e8bd196ec9639e8d3f548
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:14:38 +0000

    Backout changeset 5abbf6fd5466 (Bug 830100) for sctp_unittest failure ON A CLOSED TREE r=ryanvm
    UltraBlame original commit: 75406eaf8351f14a1ea0b99aff0c24604c10edd7

--- media/mtransport/transportflow.h
+++ media/mtransport/transportflow.h
@@ -15,34 +15,8 @@
 
 #include "nscore.h"
 #include "nsISupportsImpl.h"
-#include "mozilla/Scoped.h"
 #include "transportlayer.h"
 #include "m_cpp_utils.h"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -52,12 +26,10 @@ class TransportFlow : public sigslot::has_slots<> {
  public:
   TransportFlow()
     : id_("(anonymous)"),
-      state_(TransportLayer::TS_NONE),
-      layers_(new std::deque<TransportLayer *>) {}
+      state_(TransportLayer::TS_NONE) {}
   TransportFlow(const std::string id)
     : id_(id),
-      state_(TransportLayer::TS_NONE),
-      layers_(new std::deque<TransportLayer *>) {}
+      state_(TransportLayer::TS_NONE) {}
 
   ~TransportFlow();
 
@@ -100,39 +72,14 @@ class TransportFlow : public sigslot::has_slots<> {
  private:
   DISALLOW_COPY_ASSIGN(TransportFlow);
 
-  
-  void CheckThread() const {
-    if (!CheckThreadInt())
-      MOZ_CRASH();
-  }
-
-  bool CheckThreadInt() const {
-    bool on;
-
-    if (!target_)  
-      return true;
-    if (NS_FAILED(target_->IsOnCurrentThread(&on)))
-      return false;
-
-    return on;
-  }
-
-  void EnsureSameThread(TransportLayer *layer);
-
   void StateChange(TransportLayer *layer, TransportLayer::State state);
   void StateChangeInt(TransportLayer::State state);
   void PacketReceived(TransportLayer* layer, const unsigned char *data,
       size_t len);
-  static void DestroyFinal(nsAutoPtr<std::deque<TransportLayer *> > layers);
-
-  
-  static void ClearLayers(std::deque<TransportLayer *>* layers);
-  static void ClearLayers(std::queue<TransportLayer *>* layers);
 
   std::string id_;
+  std::deque<TransportLayer *> layers_;
   TransportLayer::State state_;
-  ScopedDeletePtr<std::deque<TransportLayer *> > layers_;
-  nsCOMPtr<nsIEventTarget> target_;
 };
 
 }  
