e43be8cb48ff74c9e57bec097c0c50c58086c09c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:51:43 +0000

    Bug 751205 - (Part 2) Un-singleton-ify SiteIdentityPopup. r=wesj
    UltraBlame original commit: 6b17a76cd79a0608b1b40e5590df222206d9bbae

--- mobile/android/base/DoorHangerPopup.java
+++ mobile/android/base/DoorHangerPopup.java
@@ -24,7 +24,6 @@ public class DoorHangerPopup extends ArrowPopup
                              implements GeckoEventListener, Tabs.OnTabsChangedListener {
     private static final String LOGTAG = "GeckoDoorHangerPopup";
 
-    private GeckoApp mActivity;
     private View mAnchor;
 
     
@@ -33,7 +32,6 @@ public class DoorHangerPopup extends ArrowPopup
 
     DoorHangerPopup(GeckoApp aActivity, View aAnchor) {
         super(aActivity);
-        mActivity = aActivity;
         mAnchor = aAnchor;
 
         mDoorHangers = new HashSet<DoorHanger>();
