ec4c555187d54f18627ed24c2058ed80945ea329
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:10:13 +0000

    Bug 976833 - Override JUnit methods in UITest. r=lucasr
    UltraBlame original commit: 88184c55d33510529026f467babf602a79cda44c

--- mobile/android/base/tests/testInputConnection.java
+++ mobile/android/base/tests/testInputConnection.java
@@ -75,10 +75,10 @@ public class testInputConnection extends UITest {
             assertTextAndSelectionAt("Can finish composition", ic, "foobar", 6);
 
             
-            assertEquals("Can retrieve text before cursor", "bar", ic.getTextBeforeCursor(3, 0));
+            fAssertEquals("Can retrieve text before cursor", "bar", ic.getTextBeforeCursor(3, 0));
 
             
-            assertEquals("Can retrieve text after cursor", "", ic.getTextAfterCursor(3, 0));
+            fAssertEquals("Can retrieve text after cursor", "", ic.getTextAfterCursor(3, 0));
         }
     }
 }
