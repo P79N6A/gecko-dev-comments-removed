8d3483f321014a806e1be24bdd1d2e3f3bd5ebdc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:48:16 +0000

    Bug 882520 - Two minor tweaks to the memory reporting paths.  r=bhackett.
    UltraBlame original commit: 22c88d3d898ae4dd2af8a398949b6ad773f53fbc

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -1472,7 +1472,7 @@ public:
     
     xpc::ZoneStatsExtras* extras = new xpc::ZoneStatsExtras;
     extras->pathPrefix = mRtPath;
-    extras->pathPrefix += nsPrintfCString("zone(%p)/", (void *)aZone);
+    extras->pathPrefix += nsPrintfCString("zone(0x%p)/", (void *)aZone);
     aZoneStats->extra = extras;
   }
 
@@ -1490,7 +1490,7 @@ public:
     
     
     extras->jsPathPrefix.Assign(mRtPath);
-    extras->jsPathPrefix += nsPrintfCString("zone(%p)/",
+    extras->jsPathPrefix += nsPrintfCString("zone(0x%p)/",
                                             (void *)js::GetCompartmentZone(aCompartment));
     extras->jsPathPrefix += js::IsAtomsCompartment(aCompartment)
                             ? NS_LITERAL_CSTRING("compartment(web-worker-atoms)/")
