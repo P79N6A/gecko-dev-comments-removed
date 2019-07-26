ab186e11b801d0dfd1a5dded55ba458621b6c592
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:30:42 +0000

    Bug 808220 - [SMS API] Retrieve a 'sent' message show field 'sender' as 'undefined' STRING, not undefined [r=ferjm]
    UltraBlame original commit: 5b625f3f1fe5d66ee4bbfb03ab36d958dedf6ee6

--- dom/sms/src/ril/SmsDatabaseService.js
+++ dom/sms/src/ril/SmsDatabaseService.js
@@ -451,6 +451,12 @@ SmsDatabaseService.prototype = {
   saveReceivedMessage: function saveReceivedMessage(sender, body, messageClass, date) {
     let receiver = this.mRIL.rilContext.icc ? this.mRIL.rilContext.icc.msisdn : null;
 
+    
+    
+    if (receiver === undefined || receiver === "undefined") {
+      receiver = null;
+    }
+
     let message = {delivery:       DELIVERY_RECEIVED,
                    deliveryStatus: DELIVERY_STATUS_SUCCESS,
                    sender:         sender,
@@ -464,6 +470,12 @@ SmsDatabaseService.prototype = {
 
   saveSentMessage: function saveSentMessage(receiver, body, date) {
     let sender = this.mRIL.rilContext.icc ? this.mRIL.rilContext.icc.msisdn : null;
+
+    
+    
+    if (sender === undefined || sender === "undefined") {
+      sender = null;
+    }
 
     let message = {delivery:       DELIVERY_SENT,
                    deliveryStatus: DELIVERY_STATUS_PENDING,
