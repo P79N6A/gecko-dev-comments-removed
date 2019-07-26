6f9a7b3adfd7f47cc12273751faf155ae4d62092
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:42:34 +0000

    Bug 969719 - get rid of level='top' workaround for Australis standalone panels, r=MattN
    UltraBlame original commit: bb4595b333419bc2730dbacd124e07443e91f577

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -295,7 +295,6 @@ const PanelUI = {
       tempPanel.setAttribute("type", "arrow");
       tempPanel.setAttribute("id", "customizationui-widget-panel");
       tempPanel.setAttribute("class", "cui-widget-panel");
-      tempPanel.setAttribute("level", "top");
       tempPanel.setAttribute("context", "");
       document.getElementById(CustomizableUI.AREA_NAVBAR).appendChild(tempPanel);
       
