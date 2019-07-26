8ae5259ccabfcf3583654994429c7bc804ecb986
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:01:39 +0000

    Bug 787517: Test case for reproducing bug r=jlebar
    UltraBlame original commit: 9374b6502e7a2ddd78acc3c7cc50abe2caf286a9

---
+++ dom/browser-element/mochitest/browserElement_RemoveBrowserElement.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+"use strict";
+
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addPermission();
+
+  var iframe = document.createElement('iframe');
+  iframe.mozbrowser = true;
+  document.body.appendChild(iframe);
+
+  iframe.addEventListener("mozbrowsershowmodalprompt", function(e) {
+    document.body.removeChild(iframe);
+    SimpleTest.executeSoon(function() {
+      ok(true);
+      SimpleTest.finish();
+    });
+  });
+
+  iframe.src = "data:text/html,<html><body><script>alert(\"test\")</script>" +
+               "</body></html>";
+}
+
+runTest();
+
