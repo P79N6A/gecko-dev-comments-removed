7c82f29cde5dd45a29a4ffbe8eb1bd24611282cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:09:35 +0000

    Bug 940455 - Add LayerManager functonality to clear out a portion of the window for the OS to paint. r=roc,Bas
    UltraBlame original commit: f4911ad70a26af88843d8be8919626fe09bd60c7

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -596,11 +596,18 @@ public:
 
   bool IsInTransaction() const { return mInTransaction; }
 
+  virtual void SetRegionToClear(const nsIntRegion& aRegion)
+  {
+    mRegionToClear = aRegion;
+  }
+
 protected:
   nsRefPtr<Layer> mRoot;
   gfx::UserData mUserData;
   bool mDestroyed;
   bool mSnapEffectiveTransforms;
+
+  nsIntRegion mRegionToClear;
 
   
   
@@ -1334,6 +1341,7 @@ public:
 #endif
 
   virtual LayerRenderState GetRenderState() { return LayerRenderState(); }
+
 
   void Mutated()
   {
