b4dd6d833b167050a5d40643a47b868f1045db3e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:37:22 +0000

    Bug 995316 - Expose the OnMetricsChangedListener via LayerView instead of GeckoLayerClient. r=snorp
    UltraBlame original commit: 2a404bf79a6be04cf9524432e8e69211293d6d08

--- mobile/android/base/gfx/GeckoLayerClient.java
+++ mobile/android/base/gfx/GeckoLayerClient.java
@@ -86,7 +86,7 @@ public class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
 
 
     private volatile ImmutableViewportMetrics mViewportMetrics;
-    private OnMetricsChangedListener mViewportChangeListener;
+    private LayerView.OnMetricsChangedListener mViewportChangeListener;
 
     private ZoomConstraints mZoomConstraints;
 
@@ -914,11 +914,6 @@ public class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
         }
     }
 
-    public interface OnMetricsChangedListener {
-        public void onMetricsChanged(ImmutableViewportMetrics viewport);
-        public void onPanZoomStopped();
-    }
-
     
     @Override
     public void forceRedraw(DisplayPortMetrics displayPort) {
@@ -993,7 +988,7 @@ public class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
         return layerPoint;
     }
 
-    public void setOnMetricsChangedListener(OnMetricsChangedListener listener) {
+    void setOnMetricsChangedListener(LayerView.OnMetricsChangedListener listener) {
         mViewportChangeListener = listener;
     }
 
