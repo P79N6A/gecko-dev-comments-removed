90fdc9cf945ba4338190ed86545ebc3bb2b8f9a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:54:40 +0000

    Bug 839839 - Minor nsPlaceholderFrame code cleanup.  r=dholbert
    UltraBlame original commit: bf5d5599aa6a44204b1e4b8480e961e8542b5d58

--- layout/generic/nsPlaceholderFrame.h
+++ layout/generic/nsPlaceholderFrame.h
@@ -57,7 +57,7 @@ nsIFrame* NS_NewPlaceholderFrame(nsIPresShell* aPresShell,
 
 
 
-class nsPlaceholderFrame : public nsFrame {
+class nsPlaceholderFrame MOZ_FINAL : public nsFrame {
 public:
   NS_DECL_FRAMEARENA_HELPERS
 
@@ -68,8 +68,8 @@ public:
   friend nsIFrame* NS_NewPlaceholderFrame(nsIPresShell* aPresShell,
                                           nsStyleContext* aContext,
                                           nsFrameState aTypeBit);
-  nsPlaceholderFrame(nsStyleContext* aContext, nsFrameState aTypeBit) :
-    nsFrame(aContext)
+  nsPlaceholderFrame(nsStyleContext* aContext, nsFrameState aTypeBit)
+    : nsFrame(aContext)
   {
     NS_PRECONDITION(aTypeBit == PLACEHOLDER_FOR_FLOAT ||
                     aTypeBit == PLACEHOLDER_FOR_ABSPOS ||
@@ -78,10 +78,9 @@ public:
                     "Unexpected type bit");
     AddStateBits(aTypeBit);
   }
-  virtual ~nsPlaceholderFrame();
 
   
-  nsIFrame*  GetOutOfFlowFrame() const {return mOutOfFlowFrame;}
+  nsIFrame*  GetOutOfFlowFrame() const { return mOutOfFlowFrame; }
   void       SetOutOfFlowFrame(nsIFrame* aFrame) {
                NS_ASSERTION(!aFrame || !aFrame->GetPrevContinuation(),
                             "OOF must be first continuation");
@@ -91,31 +90,30 @@ public:
   
   
   
-  virtual nscoord GetMinWidth(nsRenderingContext *aRenderingContext) MOZ_OVERRIDE;
-  virtual nscoord GetPrefWidth(nsRenderingContext *aRenderingContext) MOZ_OVERRIDE;
-  virtual void AddInlineMinWidth(nsRenderingContext *aRenderingContext,
-                                 InlineMinWidthData *aData) MOZ_OVERRIDE;
-  virtual void AddInlinePrefWidth(nsRenderingContext *aRenderingContext,
-                                  InlinePrefWidthData *aData) MOZ_OVERRIDE;
+  virtual void AddInlineMinWidth(nsRenderingContext* aRenderingContext,
+                                 InlineMinWidthData* aData) MOZ_OVERRIDE;
+  virtual void AddInlinePrefWidth(nsRenderingContext* aRenderingContext,
+                                  InlinePrefWidthData* aData) MOZ_OVERRIDE;
   virtual nsSize GetMinSize(nsBoxLayoutState& aBoxLayoutState) MOZ_OVERRIDE;
   virtual nsSize GetPrefSize(nsBoxLayoutState& aBoxLayoutState) MOZ_OVERRIDE;
   virtual nsSize GetMaxSize(nsBoxLayoutState& aBoxLayoutState) MOZ_OVERRIDE;
+
   NS_IMETHOD Reflow(nsPresContext* aPresContext,
                     nsHTMLReflowMetrics& aDesiredSize,
                     const nsHTMLReflowState& aReflowState,
                     nsReflowStatus& aStatus) MOZ_OVERRIDE;
 
-  virtual void DestroyFrom(nsIFrame* aDestructRoot);
+  virtual void DestroyFrom(nsIFrame* aDestructRoot) MOZ_OVERRIDE;
 
-  
 #if defined(DEBUG) || (defined(MOZ_REFLOW_PERF_DSP) && defined(MOZ_REFLOW_PERF))
   NS_IMETHOD BuildDisplayList(nsDisplayListBuilder*   aBuilder,
                               const nsRect&           aDirtyRect,
-                              const nsDisplayListSet& aLists);
+                              const nsDisplayListSet& aLists) MOZ_OVERRIDE;
 #endif 
   
 #ifdef DEBUG
   NS_IMETHOD List(FILE* out, int32_t aIndent, uint32_t aFlags = 0) const MOZ_OVERRIDE;
+  NS_IMETHOD GetFrameName(nsAString& aResult) const MOZ_OVERRIDE;
 #endif 
 
   
@@ -124,10 +122,6 @@ public:
 
 
   virtual nsIAtom* GetType() const MOZ_OVERRIDE;
-
-#ifdef DEBUG
-  NS_IMETHOD GetFrameName(nsAString& aResult) const MOZ_OVERRIDE;
-#endif
 
   virtual bool IsEmpty() MOZ_OVERRIDE { return true; }
   virtual bool IsSelfEmpty() MOZ_OVERRIDE { return true; }
