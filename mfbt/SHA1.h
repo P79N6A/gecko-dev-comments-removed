56f0371e5e0bbc0a464c76040bbb5f1ebe6ce7ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:11:11 +0000

    Bug 798634 - Style patrol on mfbt/SHA1.*.  r=espindola
    UltraBlame original commit: 2a8a8a60451193e8b6e737860899cf5da1f06510

--- mfbt/SHA1.h
+++ mfbt/SHA1.h
@@ -5,42 +5,56 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #ifndef mozilla_SHA1_h_
 #define mozilla_SHA1_h_
 
 #include "mozilla/StandardInteger.h"
-namespace mozilla {
-class SHA1Sum {
-  union {
-    uint32_t w[16];         
-    uint8_t  b[64];
-  } u;
-  uint64_t size;            
-  unsigned H[22];           
-  bool mDone;
 
-public:
-  static const unsigned int HashSize = 20;
-  SHA1Sum();
-  void update(const uint8_t *dataIn, uint32_t len);
-  void finish(uint8_t hashout[20]);
+#include <stddef.h>
+
+namespace mozilla {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class SHA1Sum
+{
+    union {
+        uint32_t w[16]; 
+        uint8_t b[64];
+    } u;
+    uint64_t size; 
+    unsigned H[22]; 
+    bool mDone;
+
+  public:
+    SHA1Sum();
+
+    static const size_t HashSize = 20;
+    typedef uint8_t Hash[HashSize];
+
+    
+    void update(const uint8_t* dataIn, uint32_t len);
+
+    
+    void finish(SHA1Sum::Hash& hashOut);
 };
-}
+
+} 
 
 #endif 
