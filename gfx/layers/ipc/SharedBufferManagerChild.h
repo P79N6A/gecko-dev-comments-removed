4ee6516b5662e86888158a64c70a10d05b98f8a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:49:45 +0000

    Bug 959089 - Separate GrallocBuffer allocations from Compositor thread(part1), r=vlad,gal
    UltraBlame original commit: ffe32dd3b558c13db2a9d042c5abb06e85b1f9bf

---
+++ gfx/layers/ipc/SharedBufferManagerChild.h
@@ -0,0 +1,165 @@
+
+
+
+
+
+
+#ifndef SharedBufferManagerCHILD_H_
+#define SharedBufferManagerCHILD_H_
+
+#include "mozilla/layers/PSharedBufferManagerChild.h"
+#ifdef MOZ_HAVE_SURFACEDESCRIPTORGRALLOC
+#include "mozilla/Mutex.h"
+#endif
+
+namespace base {
+class Thread;
+}
+
+namespace mozilla {
+#ifdef MOZ_HAVE_SURFACEDESCRIPTORGRALLOC
+class Mutex;
+#endif
+
+namespace layers {
+class SharedBufferManagerParent;
+
+struct GrallocParam {
+  gfx::IntSize size;
+  uint32_t format;
+  uint32_t usage;
+  mozilla::layers::MaybeMagicGrallocBufferHandle* buffer;
+
+  GrallocParam(const gfx::IntSize& aSize,
+               const uint32_t& aFormat,
+               const uint32_t& aUsage,
+               mozilla::layers::MaybeMagicGrallocBufferHandle* aBuffer)
+    : size(aSize)
+    , format(aFormat)
+    , usage(aUsage)
+    , buffer(aBuffer)
+  {}
+};
+
+class SharedBufferManagerChild : public PSharedBufferManagerChild {
+public:
+  SharedBufferManagerChild();
+  
+
+
+
+
+
+  static void StartUp();
+
+  static PSharedBufferManagerChild*
+  StartUpInChildProcess(Transport* aTransport, ProcessId aOtherProcess);
+
+  
+
+
+  static bool StartUpOnThread(base::Thread* aThread);
+
+  
+
+
+
+
+
+
+  static void DestroyManager();
+
+  
+
+
+
+
+
+
+  static void ShutDown();
+
+  
+
+
+
+
+  static SharedBufferManagerChild* GetSingleton();
+
+  
+
+
+  void ConnectAsync(SharedBufferManagerParent* aParent);
+
+  
+
+
+  bool
+  AllocGrallocBuffer(const gfx::IntSize&, const uint32_t&, const uint32_t&, mozilla::layers::MaybeMagicGrallocBufferHandle*);
+
+  
+
+
+
+
+  void
+  DeallocGrallocBuffer(const mozilla::layers::MaybeMagicGrallocBufferHandle& aBuffer);
+
+  virtual bool RecvDropGrallocBuffer(const mozilla::layers::MaybeMagicGrallocBufferHandle& handle);
+
+#ifdef MOZ_HAVE_SURFACEDESCRIPTORGRALLOC
+  android::sp<android::GraphicBuffer> GetGraphicBuffer(int index);
+#endif
+
+  base::Thread* GetThread() const;
+
+  MessageLoop* GetMessageLoop() const;
+
+  static bool IsCreated();
+
+  static base::Thread* sSharedBufferManagerChildThread;
+  static SharedBufferManagerChild* sSharedBufferManagerChildSingleton;
+  static SharedBufferManagerParent* sSharedBufferManagerParentSingleton; 
+
+protected:
+  
+
+
+
+
+
+
+  bool
+  AllocGrallocBufferNow(const gfx::IntSize& aSize,
+                        const uint32_t& aFormat,
+                        const uint32_t& aUsage,
+                        mozilla::layers::MaybeMagicGrallocBufferHandle* aBuffer);
+
+  
+  static void
+  AllocGrallocBufferSync(const GrallocParam& aParam,
+                         Monitor* aBarrier,
+                         bool* aDone);
+
+  
+
+
+
+
+
+
+  void
+  DeallocGrallocBufferNow(const mozilla::layers::MaybeMagicGrallocBufferHandle& handle);
+
+  
+  static void
+  DeallocGrallocBufferSync(const mozilla::layers::MaybeMagicGrallocBufferHandle& handle);
+
+#ifdef MOZ_HAVE_SURFACEDESCRIPTORGRALLOC
+  std::map<int, android::sp<android::GraphicBuffer> > mBuffers;
+  Mutex mBufferMutex;
+#endif
+};
+
+} 
+} 
+#endif 
---
+++ gfx/layers/ipc/SharedBufferManagerParent.h
@@ -0,0 +1,109 @@
+
+
+
+
+
+
+#ifndef SharedBufferManagerPARENT_H_
+#define SharedBufferManagerPARENT_H_
+
+#include "mozilla/layers/PSharedBufferManagerParent.h"
+#include "mozilla/StaticPtr.h"
+
+#ifdef MOZ_HAVE_SURFACEDESCRIPTORGRALLOC
+#include "mozilla/ipc/ProtocolUtils.h"
+#include "mozilla/Mutex.h"            
+
+namespace android {
+class GraphicBuffer;
+}
+#endif
+
+namespace base {
+class Thread;
+}
+
+namespace mozilla {
+#ifdef MOZ_HAVE_SURFACEDESCRIPTORGRALLOC
+class Mutex;
+#endif
+
+namespace layers {
+
+class SharedBufferManagerParent : public PSharedBufferManagerParent
+{
+public:
+  
+
+
+  static PSharedBufferManagerParent* Create(Transport* aTransport, ProcessId aOtherProcess);
+
+  
+
+
+
+  static SharedBufferManagerParent* GetInstance(ProcessId id);
+#ifdef MOZ_HAVE_SURFACEDESCRIPTORGRALLOC
+  android::sp<android::GraphicBuffer> GetGraphicBuffer(int key);
+  static android::sp<android::GraphicBuffer> GetGraphicBuffer(GrallocBufferRef aRef);
+#endif
+  
+
+
+  SharedBufferManagerParent(Transport* aTransport, ProcessId aOwner, base::Thread* aThread);
+  virtual ~SharedBufferManagerParent();
+
+  
+
+
+  virtual void ActorDestroy(ActorDestroyReason aWhy) MOZ_OVERRIDE;
+
+  virtual bool RecvAllocateGrallocBuffer(const IntSize&, const uint32_t&, const uint32_t&, mozilla::layers::MaybeMagicGrallocBufferHandle*);
+  virtual bool RecvDropGrallocBuffer(const mozilla::layers::MaybeMagicGrallocBufferHandle& handle);
+
+  
+
+
+  void DropGrallocBuffer(mozilla::layers::SurfaceDescriptor aDesc);
+
+  
+  IToplevelProtocol*
+  CloneToplevel(const InfallibleTArray<ProtocolFdMapping>& aFds,
+                base::ProcessHandle aPeerProcess,
+                mozilla::ipc::ProtocolCloneContext* aCtx) MOZ_OVERRIDE;
+  MessageLoop* GetMessageLoop();
+
+protected:
+  
+
+
+  static std::map<base::ProcessId, SharedBufferManagerParent*> sManagers;
+
+  
+
+
+
+
+  void DropGrallocBufferImpl(mozilla::layers::SurfaceDescriptor aDesc);
+
+  
+  static void DropGrallocBufferSync(SharedBufferManagerParent* mgr, mozilla::layers::SurfaceDescriptor aDesc);
+
+#ifdef MOZ_HAVE_SURFACEDESCRIPTORGRALLOC
+  
+
+
+  std::map<int, android::sp<android::GraphicBuffer> > mBuffers;
+  Mutex mBuffersMutex;
+#endif
+  
+  Transport* mTransport;
+  base::ProcessId mOwner;
+  base::Thread* mThread;
+  static int sBufferKey;
+  static StaticAutoPtr<Monitor> sManagerMonitor;
+};
+
+} 
+} 
+#endif 
