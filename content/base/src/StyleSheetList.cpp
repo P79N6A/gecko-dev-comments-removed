054f608fc66944cb7e28997c632951e0638db356
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:36:49 +0000

    Bug 738196 - Part b: move StyleSheetList to WebIDL; r=bz
    UltraBlame original commit: 02d0214c5e376eed361db12ed8678bebc60525cc

---
+++ content/base/src/StyleSheetList.cpp
@@ -0,0 +1,46 @@
+
+
+
+
+
+#include "mozilla/dom/StyleSheetList.h"
+
+#include "mozilla/dom/StyleSheetListBinding.h"
+#include "nsCSSStyleSheet.h"
+
+namespace mozilla {
+namespace dom {
+
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_0(StyleSheetList)
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(StyleSheetList)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsIDOMStyleSheetList)
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_END
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(StyleSheetList)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(StyleSheetList)
+
+ JSObject*
+StyleSheetList::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope)
+{
+  return StyleSheetListBinding::Wrap(aCx, aScope, this);
+}
+
+NS_IMETHODIMP
+StyleSheetList::GetLength(uint32_t* aLength)
+{
+  *aLength = Length();
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+StyleSheetList::SlowItem(uint32_t aIndex, nsIDOMStyleSheet** aItem)
+{
+  NS_IF_ADDREF(*aItem = Item(aIndex));
+  return NS_OK;
+}
+
+} 
+} 
