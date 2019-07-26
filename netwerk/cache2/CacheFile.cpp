7ec74cf6cdae790ef85a83bc1dae2c4ddc37cd8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:47:14 +0000

    Bug 971975 - Proper state and non-null check for CacheFile::WriteMetadataIfNeededLocked(), r=michal
    UltraBlame original commit: 4bae2b5cb32a288a7ba52a4f81f533e77dff9db2

--- netwerk/cache2/CacheFile.cpp
+++ netwerk/cache2/CacheFile.cpp
@@ -177,8 +177,10 @@ CacheFile::~CacheFile()
   LOG(("CacheFile::~CacheFile() [this=%p]", this));
 
   MutexAutoLock lock(mLock);
-  if (!mMemoryOnly)
+  if (!mMemoryOnly && mReady) {
+    
     WriteMetadataIfNeededLocked(true);
+  }
 }
 
 nsresult
@@ -1334,6 +1336,11 @@ CacheFile::WriteMetadataIfNeededLocked(bool aFireAndForget)
 
   AssertOwnsLock();
   MOZ_ASSERT(!mMemoryOnly);
+
+  if (!mMetadata) {
+    MOZ_CRASH("Must have metadata here");
+    return;
+  }
 
   if (!aFireAndForget) {
     
