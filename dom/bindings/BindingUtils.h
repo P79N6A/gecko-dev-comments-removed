616de69340522e61466085a4ecc9eb41fff04c86
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:01:42 +0000

    Bug 1010658 part 2.  Stop storing a DOMClass* in a slot on DOM prototypes.  r=peterv
    UltraBlame original commit: a200f79a7b8fcec1902639e8898f1a48825a7be2

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -591,8 +591,6 @@ struct NamedConstructor
 
 
 
-
-
 void
 CreateInterfaceObjects(JSContext* cx, JS::Handle<JSObject*> global,
                        JS::Handle<JSObject*> protoProto,
@@ -600,7 +598,7 @@ CreateInterfaceObjects(JSContext* cx, JS::Handle<JSObject*> global,
                        JS::Handle<JSObject*> interfaceProto,
                        const JSClass* constructorClass, const JSNativeHolder* constructor,
                        unsigned ctorNargs, const NamedConstructor* namedConstructors,
-                       JS::Heap<JSObject*>* constructorCache, const DOMClass* domClass,
+                       JS::Heap<JSObject*>* constructorCache,
                        const NativeProperties* regularProperties,
                        const NativeProperties* chromeOnlyProperties,
                        const char* name, bool defineOnGlobal);
