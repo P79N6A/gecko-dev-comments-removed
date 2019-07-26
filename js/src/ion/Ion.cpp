d94a9dd45147543cdbea91b655d4f20c1d833f58
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:49:54 +0000

    Bug 883383 - Fix some more static root analysis failures in SpiderMonkey; r=sfink
    UltraBlame original commit: ec253175bb51751b8223fbf651497f525eaf5b6a

--- js/src/ion/Ion.cpp
+++ js/src/ion/Ion.cpp
@@ -1655,7 +1655,7 @@ ion::CanEnterAtBranch(JSContext *cx, JSScript *script, AbstractFramePtr fp,
 }
 
 MethodStatus
-ion::CanEnter(JSContext *cx, JSScript *script, AbstractFramePtr fp, bool isConstructing)
+ion::CanEnter(JSContext *cx, HandleScript script, AbstractFramePtr fp, bool isConstructing)
 {
     JS_ASSERT(ion::IsEnabled(cx));
 
@@ -1675,13 +1675,11 @@ ion::CanEnter(JSContext *cx, JSScript *script, AbstractFramePtr fp, bool isConst
     
     
     if (isConstructing && fp.thisValue().isPrimitive()) {
-        RootedScript scriptRoot(cx, script);
         RootedObject callee(cx, fp.callee());
         RootedObject obj(cx, CreateThisForFunction(cx, callee, fp.useNewType()));
         if (!obj || !ion::IsEnabled(cx)) 
             return Method_Skipped;
         fp.thisValue().setObject(*obj);
-        script = scriptRoot;
     }
 
     
