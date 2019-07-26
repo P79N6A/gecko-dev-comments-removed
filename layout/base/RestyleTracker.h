f743de434620e6a120f9d2bd7010f30a12df5298
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:10:54 +0000

    Bug 850672 r=roc
    UltraBlame original commit: b9f77eff452682da4a3203b0530d6bfd69764d99

--- layout/base/RestyleTracker.h
+++ layout/base/RestyleTracker.h
@@ -48,8 +48,10 @@ public:
 
 
   void AddFrame(nsIFrame* aFrame) {
-    if (!mEntryList.contains(Entry(aFrame, true))) {
-      mEntryList.insert(new Entry(aFrame, true));
+    uint32_t depth = aFrame->GetDepthInFrameTree();
+    if (mEntryList.empty() ||
+        !mEntryList.contains(Entry(aFrame, depth, true))) {
+      mEntryList.insert(new Entry(aFrame, depth, true));
     }
   }
 
@@ -57,8 +59,13 @@ public:
 
 
   void RemoveFrame(nsIFrame* aFrame) {
-    if (mEntryList.contains(Entry(aFrame, 0, false))) {
-      delete mEntryList.remove(Entry(aFrame, 0, false));
+    if (mEntryList.empty()) {
+      return;
+    }
+
+    uint32_t depth = aFrame->GetDepthInFrameTree();
+    if (mEntryList.contains(Entry(aFrame, depth, false))) {
+      delete mEntryList.remove(Entry(aFrame, depth, false));
     }
   }
 
