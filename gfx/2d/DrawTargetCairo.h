3ae8055401626b60a0b755d9e0f8183381013558
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:54:34 +0000

    Bug 1003707 - Pass surfaces sizes in to CreateSourceSurfaceFromNativeSurface instead of trying to extract it from cairo. r=Bas
    UltraBlame original commit: 3b5fb4abaa3fe006cbcc34fa0bfcb940b1101c53

--- gfx/2d/DrawTargetCairo.h
+++ gfx/2d/DrawTargetCairo.h
@@ -169,10 +169,6 @@ public:
 
   static cairo_surface_t *GetDummySurface();
 
-  static TemporaryRef<SourceSurface>
-      CreateSourceSurfaceForCairoSurface(cairo_surface_t* aSurface,
-                                         SurfaceFormat aFormat);
-
 private: 
   
   bool InitAlreadyReferenced(cairo_surface_t* aSurface, const IntSize& aSize, SurfaceFormat* aFormat = nullptr);
