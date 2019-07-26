31dc101efb65fc8ac2ec7d8dfe1387e73278d984
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:13:30 +0000

    Bug 345123: Remove nsGetterAddRefs<T>'s operator nsISupports**.  r=bsmedberg
    UltraBlame original commit: 1123c64bca9ee646173261434e46bde88b26454a

--- netwerk/streamconv/src/nsStreamConverterService.cpp
+++ netwerk/streamconv/src/nsStreamConverterService.cpp
@@ -125,9 +125,11 @@ nsStreamConverterService::BuildGraph() {
     if (NS_FAILED(rv)) return rv;
 
     
+    nsCOMPtr<nsISupports> supports;
     nsCOMPtr<nsISupportsCString> entry;
-    rv = entries->GetNext(getter_AddRefs(entry));
+    rv = entries->GetNext(getter_AddRefs(supports));
     while (NS_SUCCEEDED(rv)) {
+        entry = do_QueryInterface(supports);
 
         
         nsAutoCString entryString;
@@ -142,7 +144,7 @@ nsStreamConverterService::BuildGraph() {
         rv = AddAdjacency(contractID.get());
         if (NS_FAILED(rv)) return rv;
 
-        rv = entries->GetNext(getter_AddRefs(entry));
+        rv = entries->GetNext(getter_AddRefs(supports));
     }
 
     return NS_OK;
