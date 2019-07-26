dbf07ee0c98912d46528b9905045a7952f33f492
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:13:52 +0000

    Bug 821877 - Browser Mochitest for CSP error logging to Web Console. r=msucan Also fixes another CSP browser chrome test that was broken because it uses the old CSP headers and assumed that only one warning would be logged to the Web Console.
    UltraBlame original commit: beb76e04039063e8a7e7035d50d7b27762fcccc0

---
+++ browser/devtools/webconsole/test/browser_webconsole_bug_821877_csp_errors.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+const TEST_URI = "https://example.com/browser/browser/devtools/webconsole/test/test-bug-821877-csperrors.html";
+const CSP_DEPRECATED_HEADER_MSG = "The X-Content-Security-Policy and X-Content-Security-Report-Only headers will be deprecated in the future. Please use the Content-Security-Policy and Content-Security-Report-Only headers with CSP spec compliant syntax instead.";
+
+function test()
+{
+  addTab(TEST_URI);
+  browser.addEventListener("load", function onLoad(aEvent) {
+    browser.removeEventListener(aEvent.type, onLoad, true);
+    openConsole(null, function testCSPErrorLogged (hud) {
+      waitForSuccess({
+        name: "CSP error displayed successfully",
+        validatorFn: function () {
+          return hud.outputNode.textContent.indexOf(CSP_DEPRECATED_HEADER_MSG) > -1;
+        },
+        successFn: finishTest,
+        failureFn: finishTest,
+      });
+    });
+  }, true);
+}
