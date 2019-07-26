126d3141825690eb7a5869070cc59796d99e4daf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:34:45 +0000

    Bug 810543 - do_get_profile now sends the correct events. r=froydnj,khuey
    UltraBlame original commit: 64d3313397e3cf850611448bbba2bfa48338887b

---
+++ toolkit/components/osfile/tests/xpcshell/test_profiledir.js
@@ -0,0 +1,45 @@
+
+
+
+
+"use strict";
+
+Components.utils.import("resource://gre/modules/osfile.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+function run_test() {
+  run_next_test();
+}
+
+add_test(function test_initialize_profileDir() {
+  
+  do_check_false(!!OS.Constants.Path.profileDir);
+  do_check_false(!!OS.Constants.Path.localProfileDir);
+
+  
+  do_get_profile();
+
+  
+  do_check_true("profileDir" in OS.Constants.Path);
+  do_check_true(!!OS.Constants.Path.profileDir);
+  do_check_eq(OS.Constants.Path.profileDir,
+              Services.dirsvc.get("ProfD", Components.interfaces.nsIFile).path);
+
+  do_check_true("localProfileDir" in OS.Constants.Path);
+  do_check_true(!!OS.Constants.Path.localProfileDir);
+  do_check_eq(OS.Constants.Path.localProfileDir,
+              Services.dirsvc.get("ProfLD", Components.interfaces.nsIFile).path);
+
+  let promise = OS.File.makeDir(OS.Path.join(OS.Constants.Path.profileDir, "foobar"));
+  promise.then(
+    function onSuccess() {
+      do_print("Directory creation succeeded");
+      run_next_test();
+    },
+
+    function onFailure(reason) {
+      do_fail(reason);
+      run_next_test();
+    }
+  );
+});
