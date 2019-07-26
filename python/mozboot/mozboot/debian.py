20859ab58a51d122ef206cc1ff542df32505394f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:48:32 +0000

    Bug 880371 - Add debian bootstrapping for mach. r=gps
    Nearly the same as Ubuntu bootstrap, but uses the build dependencies from Debian's "iceweasel" package instead.
    
    UltraBlame original commit: 37b52a498f434d9ac4a1867a0b577eeeadb62a64

---
+++ python/mozboot/mozboot/debian.py
@@ -0,0 +1,29 @@
+
+
+
+
+import os
+
+from mozboot.base import BaseBootstrapper
+
+class DebianBootstrapper(BaseBootstrapper):
+    def __init__(self, version, dist_id):
+        BaseBootstrapper.__init__(self)
+
+        self.version = version
+        self.dist_id = dist_id
+
+    def install_system_packages(self):
+        self.run_as_root(['apt-get', 'build-dep', 'iceweasel'])
+
+        self.apt_install(
+            'autoconf2.13',
+            'libasound2-dev',
+            'libcurl4-openssl-dev',
+            'libiw-dev',
+            'libnotify-dev',
+            'libxt-dev',
+            'mercurial',
+            'mesa-common-dev',
+            'uuid',
+            'yasm')
