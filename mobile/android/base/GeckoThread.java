05d8a85884e7572c012848f0bfa4d566f58ffd56
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:36:14 +0000

    Bug 901426 - Don't overwrite passed in profile args. r=mfinkle
    UltraBlame original commit: 4f076c424cd94d2aaba717efd3936c1e27967ea2

--- mobile/android/base/GeckoThread.java
+++ mobile/android/base/GeckoThread.java
@@ -147,7 +147,9 @@ public class GeckoThread extends Thread implements GeckoEventListener {
                 if (args == null || !args.contains(BrowserApp.GUEST_BROWSING_ARG)) {
                     guest = " " + BrowserApp.GUEST_BROWSING_ARG;
                 }
-            } else if (GeckoApp.sIsUsingCustomProfile) {
+            } else if (!GeckoApp.sIsUsingCustomProfile) {
+                
+                
                 profile = " -P " + GeckoAppShell.getGeckoInterface().getProfile().getName();
             }
         }
