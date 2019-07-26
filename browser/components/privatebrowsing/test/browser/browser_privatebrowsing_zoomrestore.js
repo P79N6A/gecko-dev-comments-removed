5d77b57bf3c5d4f173016020f077c04402b5d9ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:15:49 +0000

    Bug 856366 - Fix intermittent browser_privatebrowsing_zoomrestore.js failure and add logging to debug underlying problem. r=test
    UltraBlame original commit: 98b7ab4fa80f0a4521f1aac044b7919032fa9c24

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_zoomrestore.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_zoomrestore.js
@@ -11,10 +11,54 @@ function test() {
   let windowsToClose = [];
   let windowsToReset = [];
 
+  
+  
+  function debugLogCPS(aWindow, aCallback) {
+    let pb = { usePrivateBrowsing: true };
+    let queue = [
+      ["getByDomainAndName", ["about:blank", aWindow.FullZoom.name, null]],
+      ["getByDomainAndName", ["about:blank", aWindow.FullZoom.name, pb]],
+      ["getGlobal", [aWindow.FullZoom.name, null]],
+      ["getGlobal", [aWindow.FullZoom.name, pb]],
+    ];
+    debugCallCPS(queue, aCallback);
+  }
+  function debugCallCPS(aQueue, aCallback) {
+    if (!aQueue.length) {
+      aCallback();
+      return;
+    }
+    let [methodName, args] = aQueue.shift();
+    let argsDup = args.slice();
+    argsDup.push({
+      handleResult: function debug_handleResult(pref) {
+        info("handleResult " + methodName + "(" + args.toSource() +
+             ") pref=" + pref.value);
+      },
+      handleCompletion: function debug_handleCompletion() {
+        info("handleCompletion " + methodName + "(" + args.toSource() + ")");
+        debugCallCPS(aQueue, aCallback);
+      },
+    });
+    let cps = Cc["@mozilla.org/content-pref/service;1"].
+              getService(Ci.nsIContentPrefService2);
+    cps[methodName].apply(cps, argsDup);
+  }
+
   function doTest(aIsZoomedWindow, aWindow, aCallback) {
     aWindow.gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
       aWindow.gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
       if (aIsZoomedWindow) {
+        info("about:blank zoom level should be 1: " + aWindow.ZoomManager.zoom);
+        if (aWindow.ZoomManager.zoom != 1) {
+          
+          
+          
+          
+          info("zoom level != 1, logging content prefs");
+          debugLogCPS(aWindow, aCallback);
+          return;
+        }
         
         aWindow.FullZoom.enlarge(function () {
           isnot(aWindow.ZoomManager.zoom, 1, "Zoom level for about:blank should be changed");
