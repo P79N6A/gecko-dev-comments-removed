dddd89360fc822ce8e6bde1408e0f7ff10c96118
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:21:33 +0000

    Bug 908945 - Fix automation.py's exit code handling; r=jmaher
    UltraBlame original commit: bc2e311097d43226803c84068028baff9a2af81a

--- build/automationutils.py
+++ build/automationutils.py
@@ -7,8 +7,10 @@ from __future__ import with_statement
 import glob, logging, os, platform, shutil, subprocess, sys, tempfile, urllib2, zipfile
 import base64
 import re
+import os
 from urlparse import urlparse
 from operator import itemgetter
+import signal
 
 try:
   import mozinfo
@@ -154,6 +156,43 @@ def isURL(thing):
   """Return True if |thing| looks like a URL."""
   
   return len(urlparse(thing).scheme) >= 2
+
+
+
+def strsig(n):
+  
+  
+  _sigtbl = [None]*signal.NSIG
+  for k in dir(signal):
+    if k.startswith("SIG") and not k.startswith("SIG_") and k != "SIGCLD" and k != "SIGPOLL":
+      _sigtbl[getattr(signal, k)] = k
+  
+  if hasattr(signal, "SIGRTMIN") and hasattr(signal, "SIGRTMAX"):
+    for r in range(signal.SIGRTMIN+1, signal.SIGRTMAX+1):
+      _sigtbl[r] = "SIGRTMIN+" + str(r - signal.SIGRTMIN)
+  
+  for i in range(signal.NSIG):
+    if _sigtbl[i] is None:
+      _sigtbl[i] = "unrecognized signal, number " + str(i)
+  if n < 0 or n >= signal.NSIG:
+    return "out-of-range signal, number "+str(n)
+  return _sigtbl[n]
+
+def printstatus(status, name = ""):
+  
+  if os.name != 'posix':
+    
+    if status < 0:
+      status += 2**32
+    print "TEST-INFO | %s: exit status %x\n" % (name, status)
+  elif os.WIFEXITED(status):
+    print "TEST-INFO | %s: exit %d\n" % (name, os.WEXITSTATUS(status))
+  elif os.WIFSIGNALED(status):
+    
+    print "TEST-INFO | {}: killed by {}".format(name,strsig(os.WTERMSIG(status)))
+  else:
+    
+    print "TEST-INFO | %s: undecodable exit status %04x\n" % (name, status)
 
 def addCommonOptions(parser, defaults={}):
   parser.add_option("--xre-path",
@@ -499,10 +538,13 @@ def dumpScreen(utilityPath):
   
   if mozinfo.isUnix:
     utility = [os.path.join(utilityPath, "screentopng")]
+    utilityname = "screentopng"
   elif mozinfo.isMac:
     utility = ['/usr/sbin/screencapture', '-C', '-x', '-t', 'png']
+    utilityname = "screencapture"
   elif mozinfo.isWin:
     utility = [os.path.join(utilityPath, "screenshot.exe")]
+    utilityname = "screenshot"
 
   
   parent_dir = os.environ.get('MOZ_UPLOAD_DIR', None)
@@ -515,14 +557,11 @@ def dumpScreen(utilityPath):
     tmpfd, imgfilename = tempfile.mkstemp(prefix='mozilla-test-fail-screenshot_', suffix='.png', dir=parent_dir)
     os.close(tmpfd)
     returncode = subprocess.call(utility + [imgfilename])
+    printstatus(returncode, utilityname)
   except OSError, err:
     log.info("Failed to start %s for screenshot: %s",
              utility[0], err.strerror)
     return
-
-  
-  if returncode != 0:
-    log.info("%s exited with code %d", utility, returncode)
 
 class ShutdownLeaks(object):
   """
