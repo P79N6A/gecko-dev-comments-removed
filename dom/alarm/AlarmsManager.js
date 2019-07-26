99e1318a50b81f1f3ff9f323cdbba3b39c5dad6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:40:55 +0000

    Bug 963258 - Prevent exception when using webconsole in alarm code. r=gene
    UltraBlame original commit: 72da2b2f029cfaf832dce62401467d50f9e84c18

--- dom/alarm/AlarmsManager.js
+++ dom/alarm/AlarmsManager.js
@@ -180,7 +180,7 @@ AlarmsManager.prototype = {
     
     let appsService = Cc["@mozilla.org/AppsService;1"]
                         .getService(Ci.nsIAppsService);
-    this._pageURL = principal.URI.spec;
+    this._pageURL = aWindow.location.href;
     this._manifestURL = appsService.getManifestURLByLocalId(principal.appId);
     this._window = aWindow;
   },
