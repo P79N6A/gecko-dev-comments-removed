76ee8bf3d58a0b811d842dfa32ea37effabe4382
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:16:52 +0000

    Bug 983697 - Fill loop-client repository with basic server & readme. r=dmose
    UltraBlame original commit: 80911ee357f0c819b3cab4b8e429b73b2e868a66

---
+++ static/js/webapp.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+var loop = loop || {};
+loop.webapp = (function() {
+  "use strict";
+
+  function init() {
+    console.log("Loop app started.");
+  }
+
+  return {
+    init: init
+  };
+})();
