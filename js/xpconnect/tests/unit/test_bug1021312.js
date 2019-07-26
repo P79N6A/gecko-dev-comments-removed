298d3c5134e5989d524849cbbdd1e78641b4b0ea
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:15:43 +0000

    Backed out 2 changesets (bug 1021312, bug 1020460) for apparently causing jsreftest orange on a CLOSED TREE
    Backed out changeset eca7bdeb0c6e (bug 1020460)
    Backed out changeset 965578443062 (bug 1021312)
    
    UltraBlame original commit: 6502442e61332740457eecde69db45aa0e9dc15b

--- js/xpconnect/tests/unit/test_bug1021312.js
+++ js/xpconnect/tests/unit/test_bug1021312.js
@@ -7,7 +7,7 @@ function run_test() {
   let sb = new Cu.Sandbox(this);
   var called = false;
 
-  Cu.exportFunction(function(str) { do_check_true(/someString/.test(str)); called = true; },
+  Cu.exportFunction(function(str) { do_check_true(str, "someString"); called = true; },
                     sb, { defineAs: "func" });
   
   Cu.evalInSandbox("var str = 'someString'; for (var i = 0; i < 10; ++i) str += i;", sb);
