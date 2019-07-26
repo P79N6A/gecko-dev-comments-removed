6ef59f5879d81bef465f8d4456f863cb693f3c33
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:09:08 +0000

    Bug 940107 - [Australis] Home icon not shown when moved to Bookmarks toolbar, only text. r=mconley
    UltraBlame original commit: 88af4d70b4dbfff453586736cd863214a1c2cf19

---
+++ browser/components/customizableui/test/browser_940107_home_button_in_bookmarks_toolbar.js
@@ -0,0 +1,41 @@
+
+
+
+
+"use strict";
+
+
+add_task(function() {
+  ok(CustomizableUI.inDefaultState, "Should be in default state when test starts.");
+  let bookmarksToolbar = document.getElementById(CustomizableUI.AREA_BOOKMARKS);
+  bookmarksToolbar.collapsed = false;
+
+  let homeButton = document.getElementById("home-button");
+  ok(homeButton.classList.contains("toolbarbutton-1"), "Home Button should have toolbarbutton-1 when in the nav-bar");
+  ok(!homeButton.classList.contains("bookmark-item"), "Home Button should not be displayed as a bookmarks item");
+
+  yield startCustomizing();
+  CustomizableUI.addWidgetToArea(homeButton.id, CustomizableUI.AREA_BOOKMARKS);
+  yield endCustomizing();
+  ok(homeButton.classList.contains("bookmark-item"), "Home Button should be displayed as a bookmarks item");
+  ok(!homeButton.classList.contains("toolbarbutton-1"), "Home Button should not be displayed as a nav-bar item");
+
+  gCustomizeMode.addToPanel(homeButton);
+  let panelShownPromise = promisePanelShown(window);
+  PanelUI.toggle();
+  yield panelShownPromise;
+
+  ok(homeButton.classList.contains("toolbarbutton-1"), "Home Button should have toolbarbutton-1 when in the panel");
+  ok(!homeButton.classList.contains("bookmark-item"), "Home Button should not be displayed as a bookmarks item");
+
+  gCustomizeMode.addToToolbar(homeButton);
+  let panelHiddenPromise = promisePanelHidden(window);
+  PanelUI.toggle();
+  yield panelHiddenPromise;
+
+  ok(homeButton.classList.contains("toolbarbutton-1"), "Home Button should have toolbarbutton-1 when in the nav-bar");
+  ok(!homeButton.classList.contains("bookmark-item"), "Home Button should not be displayed as a bookmarks item");
+
+  bookmarksToolbar.collapsed = true;
+  CustomizableUI.reset();
+});
