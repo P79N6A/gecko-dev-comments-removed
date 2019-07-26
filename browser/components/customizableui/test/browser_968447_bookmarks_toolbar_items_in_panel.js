087098d8eb4d08c8d26b27db851b7af7d5c8da35
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:02:11 +0000

    Bug 968447 - The Bookmarks Toolbar Items doesn't appear as a normal menu panel button in new windows. r=Gijs
    UltraBlame original commit: c10115ce8c0dd25e2b98dd09b87f82f58b584364

---
+++ browser/components/customizableui/test/browser_968447_bookmarks_toolbar_items_in_panel.js
@@ -0,0 +1,32 @@
+
+
+
+
+"use strict";
+
+
+
+add_task(function() {
+  const buttonId = "bookmarks-toolbar-placeholder";
+  yield startCustomizing();
+  CustomizableUI.addWidgetToArea("personal-bookmarks", CustomizableUI.AREA_PANEL);
+  yield endCustomizing();
+  yield PanelUI.show();
+  let bookmarksToolbarPlaceholder = document.getElementById(buttonId);
+  ok(bookmarksToolbarPlaceholder.classList.contains("toolbarbutton-1"),
+     "Button should have toolbarbutton-1 class");
+  is(bookmarksToolbarPlaceholder.getAttribute("wrap"), "true",
+     "Button should have the 'wrap' attribute");
+  yield PanelUI.hide();
+
+  let newWin = yield openAndLoadWindow();
+  yield newWin.PanelUI.show();
+  let newWinBookmarksToolbarPlaceholder = newWin.document.getElementById(buttonId);
+  ok(newWinBookmarksToolbarPlaceholder.classList.contains("toolbarbutton-1"),
+     "Button in new window should have toolbarbutton-1 class");
+  is(newWinBookmarksToolbarPlaceholder.getAttribute("wrap"), "true",
+     "Button in new window should have 'wrap' attribute");
+  yield newWin.PanelUI.hide();
+  newWin.close();
+  CustomizableUI.reset();
+});
