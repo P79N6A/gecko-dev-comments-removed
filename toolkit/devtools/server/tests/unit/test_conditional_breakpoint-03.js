0c6728d9d29ccd33b22c96cb949f73df5d4025f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:19:59 +0000

    Bug 812172 - Add conditional breakpoint handling to the debugger server. r=past
    UltraBlame original commit: 3281b969f3d6dd643c672fc9c99efdb7b5d0b384

---
+++ toolkit/devtools/server/tests/unit/test_conditional_breakpoint-03.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+var gDebuggee;
+var gClient;
+var gThreadClient;
+
+function run_test()
+{
+  initTestDebuggerServer();
+  gDebuggee = addTestGlobal("test-conditional-breakpoint");
+  gClient = new DebuggerClient(DebuggerServer.connectPipe());
+  gClient.connect(function () {
+    attachTestTabAndResume(gClient, "test-conditional-breakpoint", function (aResponse, aTabClient, aThreadClient) {
+      gThreadClient = aThreadClient;
+      test_simple_breakpoint();
+    });
+  });
+  do_test_pending();
+}
+
+function test_simple_breakpoint()
+{
+  gThreadClient.addOneTimeListener("paused", function (aEvent, aPacket) {
+    gThreadClient.setBreakpoint({
+      url: "test.js",
+      line: 3,
+      condition: "throw new Error()"
+    }, function (aResponse, bpClient) {
+      gThreadClient.addOneTimeListener("paused", function (aEvent, aPacket) {
+        
+        do_check_eq(aPacket.why.type, "debuggerStatement");
+        do_check_eq(aPacket.frame.where.line, 4);
+
+        
+        bpClient.remove(function (aResponse) {
+          gThreadClient.resume(function () {
+            finishClient(gClient);
+          });
+        });
+
+      });
+      
+      gThreadClient.resume();
+
+    });
+
+  });
+
+  Components.utils.evalInSandbox("debugger;\n" +   
+                                 "var a = 1;\n" +  
+                                 "var b = 2;\n" +  
+                                 "debugger;",      
+                                 gDebuggee,
+                                 "1.8",
+                                 "test.js",
+                                 1);
+}
