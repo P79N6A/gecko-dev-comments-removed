bae6f974b5a26c38972ea5a27a7189b217387507
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:12:38 +0000

    Bug 839841 - Load Android classes needed by getUserMedia reliably. r=blassey
    UltraBlame original commit: 18670d48447f5b391313a4ade11dafb63d75dca7

--- media/webrtc/trunk/webrtc/modules/video_capture/android/java/org/webrtc/videoengine/VideoCaptureAndroid.java
+++ media/webrtc/trunk/webrtc/modules/video_capture/android/java/org/webrtc/videoengine/VideoCaptureAndroid.java
@@ -84,9 +84,9 @@ public class VideoCaptureAndroid implements PreviewCallback, Callback {
 
         Log.d(TAG, "tryStartCapture " + width +
                 " height " + height +" frame rate " + frameRate +
-                "isCaptureRunning " + isCaptureRunning +
-                "isSurfaceReady " + isSurfaceReady +
-                "isCaptureStarted " + isCaptureStarted);
+                " isCaptureRunning " + isCaptureRunning +
+                " isSurfaceReady " + isSurfaceReady +
+                " isCaptureStarted " + isCaptureStarted);
 
         if (isCaptureRunning || !isSurfaceReady || !isCaptureStarted) {
             return 0;
@@ -179,8 +179,8 @@ public class VideoCaptureAndroid implements PreviewCallback, Callback {
         previewBufferLock.lock();
 
         
-        
-        
+        Log.v(TAG, "preview frame length " + data.length +
+              " context" + context);
         if (isCaptureRunning) {
             
             
