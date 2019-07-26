ea4f8ed6fe4333d53162daae8f8665994519a747
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:41:34 +0000

    Bug 914348 - Name the SamplerThread on linux. r=jld
    UltraBlame original commit: 8863624225d5a275e2dd6e004061eb9dd5deb090

--- tools/profiler/platform-linux.cc
+++ tools/profiler/platform-linux.cc
@@ -39,6 +39,7 @@
 #include <sys/resource.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
+#include <sys/prctl.h> 
 #include <stdlib.h>
 #include <sched.h>
 #ifdef ANDROID
@@ -241,6 +242,8 @@ Sampler::FreePlatformData(PlatformData* aData)
 }
 
 static void* SignalSender(void* arg) {
+  
+  prctl(PR_SET_NAME, "SamplerThread", 0, 0, 0);
 # if defined(ANDROID)
   
   void* initialize_atfork = NULL;
