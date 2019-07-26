de243d8bdab1b6476aee86fc3070d7bc3ff8b4b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:18:48 +0000

    Bug 898263 (part 8) - Move the Handle* and Rooted* types out of jsprvtd.h.  r=jorendorff.
    UltraBlame original commit: e27c1441baa1c3026f7ad6a9ec90153ef5982c19

---
+++ js/src/gc/Rooting.h
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+#ifndef gc_Rooting_h
+#define gc_Rooting_h
+
+#include "js/RootingAPI.h"
+
+class JSAtom;
+
+namespace js {
+
+class PropertyName;
+class Shape;
+
+namespace types { struct TypeObject; }
+
+
+
+typedef JS::Handle<Shape*>             HandleShape;
+typedef JS::Handle<types::TypeObject*> HandleTypeObject;
+typedef JS::Handle<JSAtom*>            HandleAtom;
+typedef JS::Handle<PropertyName*>      HandlePropertyName;
+
+typedef JS::MutableHandle<Shape*>      MutableHandleShape;
+typedef JS::MutableHandle<JSAtom*>     MutableHandleAtom;
+
+typedef JS::Rooted<Shape*>             RootedShape;
+typedef JS::Rooted<types::TypeObject*> RootedTypeObject;
+typedef JS::Rooted<JSAtom*>            RootedAtom;
+typedef JS::Rooted<PropertyName*>      RootedPropertyName;
+
+} 
+
+#endif 
