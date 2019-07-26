910f30c12f77d3d91e057bd54bcab3a10f8b509e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:53:53 +0000

    Bug 834091, re-apply an unreleased patch that Mozilla has chosen to use on top of NSS
    UltraBlame original commit: 29dd80c95b7dc9e26b547bd90444789f474ebe14

--- security/nss/lib/pkcs7/secpkcs7.h
+++ security/nss/lib/pkcs7/secpkcs7.h
@@ -141,6 +141,23 @@ extern PRBool SEC_PKCS7VerifyDetachedSignature(SEC_PKCS7ContentInfo *cinfo,
 
 
 
+
+
+extern PRBool SEC_PKCS7VerifyDetachedSignatureAtTime(SEC_PKCS7ContentInfo *cinfo,
+						     SECCertUsage certusage,
+						     const SECItem *detached_digest,
+						     HASH_HashType digest_type,
+						     PRBool keepcerts,
+						     PRTime atTime);
+
+
+
+
+
+
+
+
+
 extern char *SEC_PKCS7GetSignerCommonName(SEC_PKCS7ContentInfo *cinfo);
 extern char *SEC_PKCS7GetSignerEmailAddress(SEC_PKCS7ContentInfo *cinfo);
 
