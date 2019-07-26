e0ba2b729141d3f3a4d85e787fe1dea8936f3988
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:19:27 +0000

    Bug 986114 - Part 1: ReadingListProvider and BrowserProvider should share DB accessors. r=nalexander
    UltraBlame original commit: d9e1c755f554df7e000bed5a4a8741f93bc55131

--- mobile/android/base/db/AbstractTransactionalProvider.java
+++ mobile/android/base/db/AbstractTransactionalProvider.java
@@ -1,22 +1,10 @@
-
-
-
-
 package org.mozilla.gecko.db;
 
-import org.mozilla.gecko.db.BrowserContract.CommonColumns;
-import org.mozilla.gecko.db.BrowserContract.SyncColumns;
-import org.mozilla.gecko.db.PerProfileDatabases.DatabaseHelperFactory;
-import org.mozilla.gecko.mozglue.RobocopTarget;
-
 import android.content.ContentProvider;
-import android.content.ContentUris;
 import android.content.ContentValues;
-import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.os.Build;
 import android.text.TextUtils;
@@ -27,173 +15,48 @@ import android.util.Log;
 
 
 
-public abstract class TransactionalProvider<T extends SQLiteOpenHelper> extends ContentProvider {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@SuppressWarnings("javadoc")
+public abstract class AbstractTransactionalProvider extends ContentProvider {
     private static final String LOGTAG = "GeckoTransProvider";
-    protected Context mContext;
-    protected PerProfileDatabases<T> mDatabases;
 
-    
+    private static boolean logDebug = Log.isLoggable(LOGTAG, Log.DEBUG);
+    private static boolean logVerbose = Log.isLoggable(LOGTAG, Log.VERBOSE);
 
+    protected abstract SQLiteDatabase getReadableDatabase(Uri uri);
+    protected abstract SQLiteDatabase getWritableDatabase(Uri uri);
 
+    public abstract SQLiteDatabase getWritableDatabaseForTesting(Uri uri);
 
-
-
-    abstract protected String getDatabaseName();
-
-    
-
-
-
-
-
-
-
-    abstract protected T createDatabaseHelper(Context context, String databasePath);
-
-    
-
-
-
-
-
-
-    abstract protected Uri insertInTransaction(Uri uri, ContentValues values);
-
-    
-
-
-
-
-
-
-
-
-    abstract protected int deleteInTransaction(Uri uri, String selection, String[] selectionArgs);
-
-    
-
-
-
-
-
-
-
-
-
-    abstract protected int updateInTransaction(Uri uri, ContentValues values, String selection, String[] selectionArgs);
-
-    
-
-
-
-
-
-
-
-    protected SQLiteDatabase getReadableDatabase(Uri uri) {
-        String profile = null;
-        if (uri != null) {
-            profile = uri.getQueryParameter(BrowserContract.PARAM_PROFILE);
-        }
-
-        return mDatabases.getDatabaseHelperForProfile(profile, isTest(uri)).getReadableDatabase();
-    }
-
-    
-
-
-
-
-
-
-
-    protected SQLiteDatabase getWritableDatabase(Uri uri) {
-        String profile = null;
-        if (uri != null) {
-            profile = uri.getQueryParameter(BrowserContract.PARAM_PROFILE);
-        }
-
-        return mDatabases.getDatabaseHelperForProfile(profile, isTest(uri)).getWritableDatabase();
-    }
-
-    
-
-
-
-
-
-
-    @RobocopTarget
-    public SQLiteDatabase getWritableDatabaseForTesting(Uri uri) {
-        return getWritableDatabase(uri);
-    }
-
-    
-
-
-
-    public static boolean isCallerSync(Uri uri) {
-        String isSync = uri.getQueryParameter(BrowserContract.PARAM_IS_SYNC);
-        return !TextUtils.isEmpty(isSync);
-    }
-
-    
-
-
-
-
-    public static boolean shouldShowDeleted(Uri uri) {
-        String showDeleted = uri.getQueryParameter(BrowserContract.PARAM_SHOW_DELETED);
-        return !TextUtils.isEmpty(showDeleted);
-    }
-
-    
-
-
-
-
-    public static boolean shouldUpdateOrInsert(Uri uri) {
-        String insertIfNeeded = uri.getQueryParameter(BrowserContract.PARAM_INSERT_IF_NEEDED);
-        return Boolean.parseBoolean(insertIfNeeded);
-    }
-
-    
-
-
-
-    public static boolean isTest(Uri uri) {
-        String isTest = uri.getQueryParameter(BrowserContract.PARAM_IS_TEST);
-        return !TextUtils.isEmpty(isTest);
-    }
-
-    protected SQLiteDatabase getWritableDatabaseForProfile(String profile, boolean isTest) {
-        return mDatabases.getDatabaseHelperForProfile(profile, isTest).getWritableDatabase();
-    }
-
-    @Override
-    public boolean onCreate() {
-        synchronized (this) {
-            mContext = getContext();
-            mDatabases = new PerProfileDatabases<T>(
-                getContext(), getDatabaseName(), new DatabaseHelperFactory<T>() {
-                    @Override
-                    public T makeDatabaseHelper(Context context, String databasePath) {
-                        return createDatabaseHelper(context, databasePath);
-                    }
-                });
-        }
-
-        return true;
-    }
-
-    
-
-
-
-    @SuppressWarnings("static-method")
-    protected boolean shouldUseTransactions() {
-        return Build.VERSION.SDK_INT >= 11;
-    }
+    protected abstract Uri insertInTransaction(Uri uri, ContentValues values);
+    protected abstract int deleteInTransaction(Uri uri, String selection, String[] selectionArgs);
+    protected abstract int updateInTransaction(Uri uri, ContentValues values, String selection, String[] selectionArgs);
 
     
 
@@ -221,6 +84,29 @@ public abstract class TransactionalProvider<T extends SQLiteOpenHelper> extends 
 
 
     final ThreadLocal<Boolean> isInBatchOperation = new ThreadLocal<Boolean>();
+
+    
+
+
+
+    @SuppressWarnings("static-method")
+    protected boolean shouldUseTransactions() {
+        return Build.VERSION.SDK_INT >= 11;
+    }
+
+    protected static String computeSQLInClause(int items, String field) {
+        final StringBuilder builder = new StringBuilder(field);
+        builder.append(" IN (");
+        int i = 0;
+        for (; i < items - 1; ++i) {
+            builder.append("?, ");
+        }
+        if (i < items) {
+            builder.append("?");
+        }
+        builder.append(")");
+        return builder.toString();
+    }
 
     private boolean isInBatch() {
         final Boolean isInBatch = isInBatchOperation.get();
@@ -304,23 +190,6 @@ public abstract class TransactionalProvider<T extends SQLiteOpenHelper> extends 
     
 
 
-    protected static String computeSQLInClause(int items, String field) {
-        final StringBuilder builder = new StringBuilder(field);
-        builder.append(" IN (");
-        int i = 0;
-        for (; i < items - 1; ++i) {
-            builder.append("?, ");
-        }
-        if (i < items) {
-            builder.append("?");
-        }
-        builder.append(")");
-        return builder.toString();
-    }
-
-    
-
-
 
 
     protected static String computeSQLInClauseFromLongs(final Cursor cursor, String field) {
@@ -385,10 +254,8 @@ public abstract class TransactionalProvider<T extends SQLiteOpenHelper> extends 
         return result;
     }
 
-
     @Override
-    public int update(Uri uri, ContentValues values, String selection,
-            String[] selectionArgs) {
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         trace("Calling update on URI: " + uri + ", " + selection + ", " + selectionArgs);
 
         final SQLiteDatabase db = getWritableDatabase(uri);
@@ -438,7 +305,7 @@ public abstract class TransactionalProvider<T extends SQLiteOpenHelper> extends 
 
         if (successes > 0) {
             final boolean shouldSyncToNetwork = !isCallerSync(uri);
-            mContext.getContentResolver().notifyChange(uri, null, shouldSyncToNetwork);
+            getContext().getContentResolver().notifyChange(uri, null, shouldSyncToNetwork);
         }
 
         return successes;
@@ -449,60 +316,42 @@ public abstract class TransactionalProvider<T extends SQLiteOpenHelper> extends 
 
 
 
-
-
-
-
-
-
-    protected void cleanupSomeDeletedRecords(Uri fromUri, Uri targetUri, String tableName) {
-        Log.d(LOGTAG, "Cleaning up deleted records from " + tableName);
-
-        
-        
-        
-
-        
-        
-        
-
-        
-        final long MAX_AGE_OF_DELETED_RECORDS = 86400000 * 20;
-
-        
-        final long DELETED_RECORDS_PURGE_LIMIT = 5;
-
-        
-        
-        final long now = System.currentTimeMillis();
-        final String selection = SyncColumns.IS_DELETED + " = 1 AND " +
-                                 SyncColumns.DATE_MODIFIED + " <= " +
-                                 (now - MAX_AGE_OF_DELETED_RECORDS);
-
-        final String profile = fromUri.getQueryParameter(BrowserContract.PARAM_PROFILE);
-        final SQLiteDatabase db = getWritableDatabaseForProfile(profile, isTest(fromUri));
-        final String[] ids;
-        final String limit = Long.toString(DELETED_RECORDS_PURGE_LIMIT, 10);
-        final Cursor cursor = db.query(tableName, new String[] { CommonColumns._ID }, selection, null, null, null, null, limit);
-        try {
-            ids = new String[cursor.getCount()];
-            int i = 0;
-            while (cursor.moveToNext()) {
-                ids[i++] = Long.toString(cursor.getLong(0), 10);
-            }
-        } finally {
-            cursor.close();
-        }
-
-        final String inClause = computeSQLInClause(ids.length,
-                                                   CommonColumns._ID);
-        db.delete(tableName, inClause, ids);
+    protected static boolean shouldShowDeleted(Uri uri) {
+        String showDeleted = uri.getQueryParameter(BrowserContract.PARAM_SHOW_DELETED);
+        return !TextUtils.isEmpty(showDeleted);
     }
 
     
+
+
+
+
+    protected static boolean shouldUpdateOrInsert(Uri uri) {
+        String insertIfNeeded = uri.getQueryParameter(BrowserContract.PARAM_INSERT_IF_NEEDED);
+        return Boolean.parseBoolean(insertIfNeeded);
+    }
+
     
-    private static boolean logDebug  = Log.isLoggable(LOGTAG, Log.DEBUG);
-    private static boolean logVerbose = Log.isLoggable(LOGTAG, Log.VERBOSE);
+
+
+
+    protected static boolean isTest(Uri uri) {
+        if (uri == null) {
+            return false;
+        }
+        String isTest = uri.getQueryParameter(BrowserContract.PARAM_IS_TEST);
+        return !TextUtils.isEmpty(isTest);
+    }
+
+    
+
+
+
+    protected static boolean isCallerSync(Uri uri) {
+        String isSync = uri.getQueryParameter(BrowserContract.PARAM_IS_SYNC);
+        return !TextUtils.isEmpty(isSync);
+    }
+
     protected static void trace(String message) {
         if (logVerbose) {
             Log.v(LOGTAG, message);
