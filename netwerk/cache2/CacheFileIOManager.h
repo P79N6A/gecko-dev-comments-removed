be8116bcb0a1b1816f0126abc713c58bdaaae4f1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:52:48 +0000

    Bug 1003208 - HTTP cache v2: CacheFileHandle::mHash is accessed after the memory is freed, r=honzab
    UltraBlame original commit: 0941e6502ee8cd9625e24c5c5871dabc1c2334db

--- netwerk/cache2/CacheFileIOManager.h
+++ netwerk/cache2/CacheFileIOManager.h
@@ -55,7 +55,7 @@ public:
   bool IsPriority() const { return mPriority; }
   bool FileExists() const { return mFileExists; }
   bool IsClosed() const { return mClosed; }
-  bool IsSpecialFile() const { return !mHash; }
+  bool IsSpecialFile() const { return mSpecialFile; }
   nsCString & Key() { return mKey; }
 
   
@@ -73,6 +73,7 @@ private:
   bool                 mIsDoomed;
   bool                 mPriority;
   bool                 mClosed;
+  bool                 mSpecialFile;
   bool                 mInvalid;
   bool                 mFileExists; 
                                     
