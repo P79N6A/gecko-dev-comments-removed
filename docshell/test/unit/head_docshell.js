0997035f67d0807bd03b746a7ab8f4e092bfef3f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:01:35 +0000

    Bug 887543 - Windows debug bustage follow-up on a CLOSED TREE.
    UltraBlame original commit: c8f6d17961cf9ba80f0f67f85d81ad1fefb2cda2

--- docshell/test/unit/head_docshell.js
+++ docshell/test/unit/head_docshell.js
@@ -9,23 +9,3 @@ const Cr = Components.results;
 var dirSvc = Cc["@mozilla.org/file/directory_service;1"].
              getService(Ci.nsIProperties);
 var profileDir = do_get_profile();
-
-function cleanup()
-{
-  
-  try {
-    if (profileDir.exists())
-      profileDir.remove(true);
-  } catch (e) {
-    
-    
-    
-    
-  }
-}
-
-
-cleanup();
-
-
-profileDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0755);
