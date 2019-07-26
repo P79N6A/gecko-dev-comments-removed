c5ecc441d93fedb1a5890a666f67468640c671e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:56:55 +0000

    Fix for bug 839529 (Remove various CC participants that don't do anything). r=smaug.
    UltraBlame original commit: 159954333c5ebe635ad1c463253f81f1f190a913

--- extensions/spellcheck/src/mozGenericWordUtils.cpp
+++ extensions/spellcheck/src/mozGenericWordUtils.cpp
@@ -4,16 +4,7 @@
 
 #include "mozGenericWordUtils.h"
 
-NS_IMPL_CYCLE_COLLECTING_ADDREF(mozGenericWordUtils)
-NS_IMPL_CYCLE_COLLECTING_RELEASE(mozGenericWordUtils)
-
-NS_INTERFACE_MAP_BEGIN(mozGenericWordUtils)
-  NS_INTERFACE_MAP_ENTRY(mozISpellI18NUtil)
-  NS_INTERFACE_MAP_ENTRY_AMBIGUOUS(nsISupports, mozISpellI18NUtil)
-  NS_INTERFACE_MAP_ENTRIES_CYCLE_COLLECTION(mozGenericWordUtils)
-NS_INTERFACE_MAP_END
-
-NS_IMPL_CYCLE_COLLECTION_0(mozGenericWordUtils)
+NS_IMPL_ISUPPORTS1(mozGenericWordUtils, mozISpellI18NUtil)
 
   
 
