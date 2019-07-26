9090e802ebff82447b03d174b54a2fea7b995755
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:42:28 +0000

    Bug 826703 part 1.  Rename nsXMLCDATASection to mozilla::dom::CDATASection.  r=peterv
    UltraBlame original commit: fe88a7f31340f8a5d27f2676ec9765dc16788231

---
+++ content/xml/content/src/CDATASection.h
@@ -0,0 +1,60 @@
+
+
+
+
+
+#ifndef mozilla_dom_CDATASection_h
+#define mozilla_dom_CDATASection_h
+
+#include "nsIDOMCDATASection.h"
+#include "nsGenericDOMDataNode.h"
+
+namespace mozilla {
+namespace dom {
+
+class CDATASection : public nsGenericDOMDataNode,
+                     public nsIDOMCDATASection
+{
+public:
+  CDATASection(already_AddRefed<nsINodeInfo> aNodeInfo)
+    : nsGenericDOMDataNode(aNodeInfo)
+  {
+    NS_ABORT_IF_FALSE(mNodeInfo->NodeType() == nsIDOMNode::CDATA_SECTION_NODE,
+                      "Bad NodeType in aNodeInfo");
+  }
+  virtual ~CDATASection();
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+
+  
+  NS_FORWARD_NSIDOMCHARACTERDATA(nsGenericDOMDataNode::)
+
+  
+  NS_FORWARD_NSIDOMTEXT(nsGenericDOMDataNode::)
+
+  
+  
+
+  
+  virtual bool IsNodeOfType(uint32_t aFlags) const;
+
+  virtual nsGenericDOMDataNode* CloneDataNode(nsINodeInfo *aNodeInfo,
+                                              bool aCloneText) const;
+
+  virtual nsXPCClassInfo* GetClassInfo();
+
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+#ifdef DEBUG
+  virtual void List(FILE* out, int32_t aIndent) const;
+  virtual void DumpContent(FILE* out, int32_t aIndent,bool aDumpAll) const;
+#endif
+};
+
+} 
+} 
+
+#endif 
