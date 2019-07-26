0297157c5e0adb303fd5b97edd3b89f304c997b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:06:31 +0000

    Bug 783054 - part 4: add success/fail histogram for reading saved ping files; r=taras
    UltraBlame original commit: 96fd99a249cd6519567c766b2e696ffa61e2a363

--- toolkit/components/telemetry/TelemetryPing.js
+++ toolkit/components/telemetry/TelemetryPing.js
@@ -720,6 +720,8 @@ TelemetryPing.prototype = {
   },
 
   addToPendingPings: function addToPendingPings(file, stream) {
+    let success = false;
+
     try {
       let string = NetUtil.readInputStreamToString(stream, stream.available(), { charset: "UTF-8" });
       stream.close();
@@ -734,11 +736,14 @@ TelemetryPing.prototype = {
           this._pingLoadsCompleted == this._pingsLoaded) {
         Services.obs.notifyObservers(null, "telemetry-test-load-complete", null);
       }
+      success = true;
     } catch (e) {
       
       stream.close();           
       file.remove(true);
     }
+    let success_histogram = Telemetry.getHistogramById("READ_SAVED_PING_SUCCESS");
+    success_histogram.add(success);
   },
 
   loadHistograms: function loadHistograms(file, sync) {
