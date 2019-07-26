156a72be2ee612c5e7ef5a0d5c49b737ebf1e749
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:30:22 +0000

    Bug 991036 - Improve temperamental assertion in js::jit::Label::~Label() when doing OOM testing. r=jandem.
    UltraBlame original commit: 6d4aa25553268113fda175a10c838c9c4928005d

--- js/src/jit/shared/Assembler-shared.h
+++ js/src/jit/shared/Assembler-shared.h
@@ -356,8 +356,15 @@ class Label : public LabelBase
     { }
     ~Label()
     {
-        if (MaybeGetIonContext())
-            JS_ASSERT_IF(!GetIonContext()->runtime->hadOutOfMemory(), !used());
+#ifdef DEBUG
+        
+        if (OOM_counter > OOM_maxAllocations)
+            return;
+        if (MaybeGetIonContext() && GetIonContext()->runtime->hadOutOfMemory())
+            return;
+
+        MOZ_ASSERT(!used());
+#endif
     }
 };
 
