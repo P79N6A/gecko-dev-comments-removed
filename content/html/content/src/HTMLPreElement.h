944f232395c8f54f706e7e4e9936c22226d27421
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:40:52 +0000

    Bug 824823 part 12.  Rename nsHTMLPreElement to mozilla::dom::HTMLPreElement and move the class declaration into a header.  r=peterv
    UltraBlame original commit: dacf95e8cfcdadaeab0a26239b95107cc9f93683

---
+++ content/html/content/src/HTMLPreElement.h
@@ -0,0 +1,60 @@
+
+
+
+
+
+#ifndef mozilla_dom_HTMLPreElement_h
+#define mozilla_dom_HTMLPreElement_h
+
+#include "mozilla/Util.h"
+
+#include "nsIDOMHTMLPreElement.h"
+#include "nsGenericHTMLElement.h"
+
+namespace mozilla {
+namespace dom {
+
+class HTMLPreElement : public nsGenericHTMLElement,
+                       public nsIDOMHTMLPreElement
+{
+public:
+  HTMLPreElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+    : nsGenericHTMLElement(aNodeInfo)
+  {
+  }
+  virtual ~HTMLPreElement();
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+
+  
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+
+  
+  NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
+
+  
+  NS_IMETHOD GetWidth(int32_t* aWidth);
+  NS_IMETHOD SetWidth(int32_t aWidth);
+
+  virtual bool ParseAttribute(int32_t aNamespaceID,
+                                nsIAtom* aAttribute,
+                                const nsAString& aValue,
+                                nsAttrValue& aResult);
+  NS_IMETHOD_(bool) IsAttributeMapped(const nsIAtom* aAttribute) const;
+  virtual nsMapRuleToAttributesFunc GetAttributeMappingFunction() const;
+
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+
+  virtual nsXPCClassInfo* GetClassInfo();
+
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+};
+
+} 
+} 
+
+#endif 
