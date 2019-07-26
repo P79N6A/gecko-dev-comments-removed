0a1ce3a75aaf093d0e4c266b537407bdc7264bf3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:29:06 +0000

    Bug 904893 - Snapped view testing r=sfoster
    UltraBlame original commit: 7c71879e11718af41d38b6884369bf5c22a3dab3

--- browser/metro/base/tests/mochitest/helpers/ViewStateHelper.js
+++ browser/metro/base/tests/mochitest/helpers/ViewStateHelper.js
@@ -6,13 +6,10 @@
 "use strict";
 
 const snappedSize = 330;
-const portraitSize = 660;
+const portraitSize = 900;
 
 function setSnappedViewstate() {
   ok(isLandscapeMode(), "setSnappedViewstate expects landscape mode to work.");
-
-  
-  Services.obs.notifyObservers(null, 'metro_viewstate_changed', 'snapped');
 
   let browser = Browser.selectedBrowser;
 
@@ -23,13 +20,14 @@ function setSnappedViewstate() {
   browser.style.borderRight = padding + "px solid gray";
 
   
+  Services.obs.notifyObservers(null, 'metro_viewstate_changed', 'snapped');
+
+  
   yield waitForMs(0);
 }
 
 function setPortraitViewstate() {
   ok(isLandscapeMode(), "setPortraitViewstate expects landscape mode to work.");
-
-  Services.obs.notifyObservers(null, 'metro_viewstate_changed', 'portrait');
 
   let browser = Browser.selectedBrowser;
 
@@ -37,6 +35,8 @@ function setPortraitViewstate() {
   let padding = fullWidth - portraitSize;
 
   browser.style.borderRight = padding + "px solid gray";
+
+  Services.obs.notifyObservers(null, 'metro_viewstate_changed', 'portrait');
 
   
   yield waitForMs(0);
