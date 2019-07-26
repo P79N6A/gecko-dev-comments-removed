915498fd9a041f92057051f29646c94b0b19203c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:24:34 +0000

    Bug 933087.  Make sure we don't violate stack discipline for Rooted, even when playing Maybe<Rooted> games.  r=khuey
    UltraBlame original commit: 9ca072ad958c599bab378ecbd68bcee0e3bafe61

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -4839,6 +4839,18 @@ if (global.Failed()) {
         else:
             assert self.argCount == 0
 
+        if needsUnwrap:
+            
+            
+            
+            
+            cgThings.append(CGGeneric(
+                    "bool objIsXray = xpc::WrapperFactory::IsXrayWrapper(obj);"))
+            if needsUnwrappedVar:
+                cgThings.append(CGIfWrapper(
+                        CGGeneric("unwrappedObj.construct(cx, obj);"),
+                        "objIsXray"))
+
         cgThings.extend([CGArgumentConverter(arguments[i], i, self.descriptor,
                                              argDescription % { "index": i + 1 },
                                              invalidEnumValueFatal=not setter,
@@ -4849,10 +4861,6 @@ if (global.Failed()) {
         if needsUnwrap:
             
             xraySteps = []
-            if needsUnwrappedVar:
-                xraySteps.append(
-                    CGGeneric("unwrappedObj.construct(cx, obj);"))
-
             
             
             xraySteps.append(
@@ -4878,7 +4886,7 @@ if (!${obj}) {
 
             cgThings.append(
                 CGIfWrapper(CGList(xraySteps, "\n"),
-                            "xpc::WrapperFactory::IsXrayWrapper(obj)"))
+                            "objIsXray"))
 
         cgThings.append(CGCallGenerator(
                     self.getErrorReport() if self.isFallible() else None,
