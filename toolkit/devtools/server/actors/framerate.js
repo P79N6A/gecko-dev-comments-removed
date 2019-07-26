b4dcc948fa73c4780abb6c66cceb3ead678aefc8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:00:25 +0000

    Bug 1007200 - Create a framerate actor, r=rcampbell,benwa
    UltraBlame original commit: 5fff5735d8fa5eaf16068107aa51898b26f187f7

---
+++ toolkit/devtools/server/actors/framerate.js
@@ -0,0 +1,119 @@
+
+
+
+"use strict";
+
+const {Cc, Ci, Cu, Cr} = require("chrome");
+const Services = require("Services");
+const events = require("sdk/event/core");
+const protocol = require("devtools/server/protocol");
+const DevToolsUtils = require("devtools/toolkit/DevToolsUtils.js");
+
+const {on, once, off, emit} = events;
+const {method, custom, Arg, Option, RetVal} = protocol;
+
+exports.register = function(handle) {
+  handle.addTabActor(FramerateActor, "framerateActor");
+};
+
+exports.unregister = function(handle) {
+  handle.removeTabActor(FramerateActor);
+};
+
+
+
+
+let FramerateActor = exports.FramerateActor = protocol.ActorClass({
+  typeName: "framerate",
+  initialize: function(conn, tabActor) {
+    protocol.Actor.prototype.initialize.call(this, conn);
+    this.tabActor = tabActor;
+    this._contentWin = tabActor.window;
+    this._onRefreshDriverTick = this._onRefreshDriverTick.bind(this);
+  },
+  destroy: function(conn) {
+    protocol.Actor.prototype.destroy.call(this, conn);
+    this.finalize();
+  },
+
+  
+
+
+  startRecording: method(function() {
+    if (this._recording) {
+      return;
+    }
+    this._recording = true;
+    this._ticks = [];
+
+    this._startTime = this._contentWin.performance.now();
+    this._contentWin.requestAnimationFrame(this._onRefreshDriverTick);
+  }, {
+  }),
+
+  
+
+
+
+  stopRecording: method(function(resolution = 100) {
+    if (!this._recording) {
+      return {};
+    }
+    this._recording = false;
+
+    let timeline = {};
+    let ticks = this._ticks;
+    let totalTicks = ticks.length;
+
+    
+    this._ticks = null;
+
+    
+    
+    if (totalTicks == 0) {
+      timeline[resolution] = 0;
+      return timeline;
+    }
+
+    let pivotTick = 0;
+    let lastTick = ticks[totalTicks - 1];
+
+    for (let bucketTime = resolution; bucketTime < lastTick; bucketTime += resolution) {
+      let frameCount = 0;
+      while (ticks[pivotTick++] < bucketTime) frameCount++;
+
+      let framerate = 1000 / (bucketTime / frameCount);
+      timeline[bucketTime] = framerate;
+    }
+
+    return timeline;
+  }, {
+    request: { resolution: Arg(0, "nullable:number") },
+    response: { timeline: RetVal("json") }
+  }),
+
+  
+
+
+  _onRefreshDriverTick: function() {
+    if (!this._recording) {
+      return;
+    }
+    this._contentWin.requestAnimationFrame(this._onRefreshDriverTick);
+
+    
+    let currentTime = this._contentWin.performance.now();
+    let elapsedTime = currentTime - this._startTime;
+    this._ticks.push(elapsedTime);
+  }
+});
+
+
+
+
+let FramerateFront = exports.FramerateFront = protocol.FrontClass(FramerateActor, {
+  initialize: function(client, { framerateActor }) {
+    protocol.Front.prototype.initialize.call(this, client, { actor: framerateActor });
+    this.manage(this);
+  }
+});
