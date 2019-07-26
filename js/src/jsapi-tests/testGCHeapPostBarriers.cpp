832960bbbae46b32f6c847fa7729e7c92c97bdad
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:27:20 +0000

    Bug 1028358; r=terrence
    UltraBlame original commit: dd07ffb3af8246f21c891f7903c1e44ad08b1e65

---
+++ js/src/jsapi-tests/testGCHeapPostBarriers.cpp
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+
+#ifdef JSGC_GENERATIONAL
+
+#include "js/RootingAPI.h"
+#include "jsapi-tests/tests.h"
+
+BEGIN_TEST(testGCHeapPostBarriers)
+{
+    
+    JS_GC(cx->runtime());
+    JS::RootedObject obj(cx, NurseryObject());
+    CHECK(js::gc::IsInsideNursery(obj.get()));
+    JS_GC(cx->runtime());
+    CHECK(!js::gc::IsInsideNursery(obj.get()));
+    JS::RootedObject tenuredObject(cx, obj);
+
+    
+    CHECK(TestHeapPostBarriers(NurseryObject()));
+    CHECK(TestHeapPostBarriers(NurseryFunction()));
+
+    return true;
+}
+
+template <typename T>
+bool
+TestHeapPostBarriers(T initalObj)
+{
+    CHECK(initalObj != nullptr);
+    CHECK(js::gc::IsInsideNursery(initalObj));
+
+    
+    JS::Heap<T> *heapData = new JS::Heap<T>();
+    CHECK(heapData);
+    CHECK(heapData->get() == nullptr);
+    heapData->set(initalObj);
+
+    
+    js::MinorGC(cx, JS::gcreason::API);
+    CHECK(heapData->get() != initalObj);
+    CHECK(!js::gc::IsInsideNursery(heapData->get()));
+
+    
+    Rooted<T> obj(cx, heapData->get());
+    RootedValue value(cx);
+    CHECK(JS_GetProperty(cx, obj, "x", &value));
+    CHECK(value.isInt32());
+    CHECK(value.toInt32() == 42);
+
+    delete heapData;
+    return true;
+}
+
+JSObject *NurseryObject()
+{
+    RootedObject obj(cx, JS_NewObject(cx, nullptr, JS::NullPtr(), JS::NullPtr()));
+    if (!obj)
+        return nullptr;
+    JS_DefineProperty(cx, obj, "x", 42, 0);
+    return obj;
+}
+
+JSFunction *NurseryFunction()
+{
+    
+
+
+
+    return static_cast<JSFunction *>(NurseryObject());
+}
+
+END_TEST(testGCHeapPostBarriers)
+
+#endif
