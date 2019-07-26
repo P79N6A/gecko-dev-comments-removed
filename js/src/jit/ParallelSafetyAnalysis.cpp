0ae78c0f77e288c700ac571af56102048e07b7ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:58:20 +0000

    Bug 939614: IonMonkey: Add recompile check, r=jandem
    UltraBlame original commit: 7d3c745a811f383c2a3ddf8c48126cbcd3740fa7

--- js/src/jit/ParallelSafetyAnalysis.cpp
+++ js/src/jit/ParallelSafetyAnalysis.cpp
@@ -311,6 +311,7 @@ class ParallelSafetyVisitor : public MInstructionVisitor
     UNSAFE_OP(AsmJSParameter)
     UNSAFE_OP(AsmJSCall)
     UNSAFE_OP(AsmJSCheckOverRecursed)
+    DROP_OP(RecompileCheck)
 
     
     UNSAFE_OP(ConvertElementsToDoubles)
