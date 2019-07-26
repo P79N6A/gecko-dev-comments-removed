491fdbc339ceee076e5c28c6f93a6af334917574
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:24:05 +0000

    Bug 972936 - Overall impressions metrics for New Tab [r=adw] Add a histogram for each of the 3 directory link types and count how many were shown.
    UltraBlame original commit: a695139e96eef27277cda3bb71fb7d3fc34462a6

--- browser/base/content/newtab/page.js
+++ browser/base/content/newtab/page.js
@@ -66,9 +66,12 @@ let gPage = {
   
 
 
-  update: function Page_update() {
+
+
+  update: function Page_update(aOnlyIfHidden=false) {
+    let skipUpdate = aOnlyIfHidden && this.allowBackgroundCaptures;
     
-    if (gGrid.ready) {
+    if (gGrid.ready && !skipUpdate) {
       gGrid.refresh();
     }
   },
@@ -87,10 +90,28 @@ let gPage = {
       if (this.allowBackgroundCaptures) {
         Services.telemetry.getHistogramById("NEWTAB_PAGE_SHOWN").add(true);
 
+        
+        let directoryCount = {};
+        for (let type of DirectoryLinksProvider.linkTypes) {
+          directoryCount[type] = 0;
+        }
+
         for (let site of gGrid.sites) {
           if (site) {
             site.captureIfMissing();
+            let {type} = site.link;
+            if (type in directoryCount) {
+              directoryCount[type]++;
+            }
           }
+        }
+
+        
+        
+        for (let [type, count] of Iterator(directoryCount)) {
+          let shownId = "NEWTAB_PAGE_DIRECTORY_" + type.toUpperCase() + "_SHOWN";
+          let shownCount = Math.min(10, count);
+          Services.telemetry.getHistogramById(shownId).add(shownCount);
         }
       }
     });
