c04144e7843af691dae23672f15f6ef3c2080bea
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:07:21 +0000

    Merge from mozilla-central.
    UltraBlame original commit: 49833ea0f315b662b0ba389108967feacbc266a4

---
+++ accessible/src/msaa/nsAccessNodeWrap.cpp
@@ -0,0 +1,251 @@
+
+
+
+
+
+#include "nsAccessNodeWrap.h"
+
+#include "AccessibleApplication.h"
+#include "ApplicationAccessibleWrap.h"
+#include "sdnAccessible.h"
+
+#include "Compatibility.h"
+#include "nsAccessibilityService.h"
+#include "nsAccUtils.h"
+#include "nsCoreUtils.h"
+#include "nsWinUtils.h"
+#include "RootAccessible.h"
+#include "Statistics.h"
+
+#include "nsAttrName.h"
+#include "nsIDOMNodeList.h"
+#include "nsIDOMHTMLElement.h"
+#include "nsINameSpaceManager.h"
+#include "nsPIDOMWindow.h"
+#include "nsIServiceManager.h"
+
+using namespace mozilla;
+using namespace mozilla::a11y;
+
+AccTextChangeEvent* nsAccessNodeWrap::gTextEvent = nullptr;
+
+
+
+
+
+nsAccessNodeWrap::
+  nsAccessNodeWrap(nsIContent* aContent, DocAccessible* aDoc) :
+  nsAccessNode(aContent, aDoc)
+{
+}
+
+nsAccessNodeWrap::~nsAccessNodeWrap()
+{
+}
+
+
+
+
+
+NS_IMPL_ISUPPORTS_INHERITED1(nsAccessNodeWrap, nsAccessNode, nsIWinAccessNode)
+
+
+
+
+
+NS_IMETHODIMP
+nsAccessNodeWrap::QueryNativeInterface(REFIID aIID, void** aInstancePtr)
+{
+  
+  return static_cast<nsresult>(QueryInterface(aIID, aInstancePtr));
+}
+
+STDMETHODIMP nsAccessNodeWrap::QueryInterface(REFIID iid, void** ppv)
+{
+  *ppv = nullptr;
+
+  if (IID_IUnknown == iid) {
+    *ppv = static_cast<IUnknown*>(this);
+  } else {
+    return E_NOINTERFACE; 
+  }
+
+  (reinterpret_cast<IUnknown*>(*ppv))->AddRef();
+  return S_OK;
+}
+
+STDMETHODIMP
+nsAccessNodeWrap::QueryService(REFGUID guidService, REFIID iid, void** ppv)
+{
+  *ppv = nullptr;
+
+  
+  
+  
+  
+  
+  static const GUID SID_IAccessibleContentDocument =
+    { 0xa5d8e1f3,0x3571,0x4d8f,{0x95,0x21,0x07,0xed,0x28,0xfb,0x07,0x2e} };
+  if (guidService == SID_IAccessibleContentDocument) {
+    if (iid != IID_IAccessible)
+      return E_NOINTERFACE;
+
+    nsCOMPtr<nsIDocShellTreeItem> docShellTreeItem = 
+      nsCoreUtils::GetDocShellTreeItemFor(mContent);
+    if (!docShellTreeItem)
+      return E_UNEXPECTED;
+
+    
+    
+    nsCOMPtr<nsIDocShellTreeItem> root;
+    docShellTreeItem->GetSameTypeRootTreeItem(getter_AddRefs(root));
+    if (!root)
+      return E_UNEXPECTED;
+
+
+    
+    
+    int32_t itemType;
+    root->GetItemType(&itemType);
+    if (itemType != nsIDocShellTreeItem::typeContent)
+      return E_NOINTERFACE;
+
+    
+    DocAccessible* docAcc = nsAccUtils::GetDocAccessibleFor(root);
+    if (!docAcc)
+      return E_UNEXPECTED;
+
+    *ppv = static_cast<IAccessible*>(docAcc);
+
+    (reinterpret_cast<IUnknown*>(*ppv))->AddRef();
+    return S_OK;
+  }
+
+  
+  if (guidService == IID_IAccessibleApplication) {
+    ApplicationAccessible* applicationAcc = ApplicationAcc();
+    if (!applicationAcc)
+      return E_NOINTERFACE;
+
+    nsresult rv = applicationAcc->QueryNativeInterface(iid, ppv);
+    return NS_SUCCEEDED(rv) ? S_OK : E_NOINTERFACE;
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+  static const GUID IID_SimpleDOMDeprecated =
+    { 0x0c539790,0x12e4,0x11cf,{0xb6,0x61,0x00,0xaa,0x00,0x4c,0xd6,0xd8} };
+  if (guidService == IID_ISimpleDOMNode ||
+      guidService == IID_SimpleDOMDeprecated ||
+      guidService == IID_IAccessible ||  guidService == IID_IAccessible2)
+    return QueryInterface(iid, ppv);
+
+  return E_INVALIDARG;
+}
+ 
+void nsAccessNodeWrap::InitAccessibility()
+{
+  Compatibility::Init();
+
+  nsWinUtils::MaybeStartWindowEmulation();
+}
+
+void nsAccessNodeWrap::ShutdownAccessibility()
+{
+  NS_IF_RELEASE(gTextEvent);
+  ::DestroyCaret();
+
+  nsWinUtils::ShutdownWindowEmulation();
+}
+
+int nsAccessNodeWrap::FilterA11yExceptions(unsigned int aCode, EXCEPTION_POINTERS *aExceptionInfo)
+{
+  if (aCode == EXCEPTION_ACCESS_VIOLATION) {
+#ifdef MOZ_CRASHREPORTER
+    
+    
+    
+    nsCOMPtr<nsICrashReporter> crashReporter =
+      do_GetService("@mozilla.org/toolkit/crash-reporter;1");
+    if (crashReporter) {
+      crashReporter->WriteMinidumpForException(aExceptionInfo);
+    }
+#endif
+  }
+  else {
+    NS_NOTREACHED("We should only be catching crash exceptions");
+  }
+  return EXCEPTION_CONTINUE_SEARCH;
+}
+
+HRESULT
+GetHRESULT(nsresult aResult)
+{
+  switch (aResult) {
+    case NS_OK:
+      return S_OK;
+
+    case NS_ERROR_INVALID_ARG: case NS_ERROR_INVALID_POINTER:
+      return E_INVALIDARG;
+
+    case NS_ERROR_OUT_OF_MEMORY:
+      return E_OUTOFMEMORY;
+
+    case NS_ERROR_NOT_IMPLEMENTED:
+      return E_NOTIMPL;
+
+    default:
+      return E_FAIL;
+  }
+}
+
+nsRefPtrHashtable<nsPtrHashKey<void>, DocAccessible> nsAccessNodeWrap::sHWNDCache;
+
+LRESULT CALLBACK
+nsAccessNodeWrap::WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+  
+  
+  
+
+  switch (msg) {
+    case WM_GETOBJECT:
+    {
+      if (lParam == OBJID_CLIENT) {
+        DocAccessible* document = sHWNDCache.GetWeak(static_cast<void*>(hWnd));
+        if (document) {
+          IAccessible* msaaAccessible = NULL;
+          document->GetNativeInterface((void**)&msaaAccessible); 
+          if (msaaAccessible) {
+            LRESULT result = ::LresultFromObject(IID_IAccessible, wParam,
+                                                 msaaAccessible); 
+            msaaAccessible->Release(); 
+            return result;
+          }
+        }
+      }
+      return 0;
+    }
+    case WM_NCHITTEST:
+    {
+      LRESULT lRet = ::DefWindowProc(hWnd, msg, wParam, lParam);
+      if (HTCLIENT == lRet)
+        lRet = HTTRANSPARENT;
+      return lRet;
+    }
+  }
+
+  return ::DefWindowProcW(hWnd, msg, wParam, lParam);
+}
---
+++ accessible/src/msaa/nsAccessNodeWrap.h
@@ -0,0 +1,109 @@
+
+
+
+
+
+
+
+
+
+#ifndef _nsAccessNodeWrap_H_
+#define _nsAccessNodeWrap_H_
+
+
+
+
+
+
+#ifdef _MSC_VER
+#pragma warning( disable : 4509 )
+#endif
+
+#include "nsCOMPtr.h"
+#include "nsIAccessible.h"
+#include "nsIAccessibleEvent.h"
+#include "nsIWinAccessNode.h"
+#include "nsIDOMElement.h"
+#include "nsIContent.h"
+#include "nsAccessNode.h"
+#include "oleidl.h"
+#include "oleacc.h"
+#include <winuser.h>
+#ifdef MOZ_CRASHREPORTER
+#include "nsICrashReporter.h"
+#endif
+
+#include "nsRefPtrHashtable.h"
+
+#define A11Y_TRYBLOCK_BEGIN                                                    \
+  MOZ_SEH_TRY {
+
+#define A11Y_TRYBLOCK_END                                                             \
+  } MOZ_SEH_EXCEPT(nsAccessNodeWrap::FilterA11yExceptions(::GetExceptionCode(),       \
+                                                          GetExceptionInformation())) \
+  { }                                                                                 \
+  return E_FAIL;
+
+namespace mozilla {
+namespace a11y {
+
+class AccTextChangeEvent;
+
+#ifdef __GNUC__
+
+
+
+#pragma GCC diagnostic ignored "-Woverloaded-virtual"
+#endif
+
+class nsAccessNodeWrap : public nsAccessNode,
+                         public nsIWinAccessNode,
+                         public IServiceProvider
+{
+  public:
+    NS_DECL_ISUPPORTS_INHERITED
+    NS_DECL_NSIWINACCESSNODE
+
+public: 
+  nsAccessNodeWrap(nsIContent* aContent, DocAccessible* aDoc);
+  virtual ~nsAccessNodeWrap();
+
+  
+  virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID aIID,
+                                                   void** aInstancePtr);
+
+  
+  virtual HRESULT STDMETHODCALLTYPE QueryService(REFGUID aGuidService,
+                                                 REFIID aIID,
+                                                 void** aInstancePtr);
+
+
+    static void InitAccessibility();
+    static void ShutdownAccessibility();
+
+    static int FilterA11yExceptions(unsigned int aCode, EXCEPTION_POINTERS *aExceptionInfo);
+
+  static LRESULT CALLBACK WindowProc(HWND hWnd, UINT Msg,
+                                     WPARAM WParam, LPARAM lParam);
+
+  static nsRefPtrHashtable<nsPtrHashKey<void>, DocAccessible> sHWNDCache;
+
+protected:
+
+    
+
+
+
+    static AccTextChangeEvent* gTextEvent;
+};
+
+} 
+} 
+
+
+
+
+HRESULT GetHRESULT(nsresult aResult);
+
+#endif
+
