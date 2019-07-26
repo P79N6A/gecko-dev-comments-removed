5e9ff13c23bf7762208d0c37efc3d850d92ff4d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:54:27 +0000

    Bug 1002014 - Error case leak in AsmJSCache.cpp AllocEntryParent(); r=luke
    UltraBlame original commit: e8b73142e2e86680ab5adcb0a8b858617ac6f1b3

--- dom/asmjscache/AsmJSCache.cpp
+++ dom/asmjscache/AsmJSCache.cpp
@@ -1323,23 +1323,22 @@ AllocEntryParent(OpenMode aOpenMode,
                  WriteParams aWriteParams,
                  nsIPrincipal* aPrincipal)
 {
-  ParentProcessRunnable* runnable =
+  nsRefPtr<ParentProcessRunnable> runnable =
     new ParentProcessRunnable(aPrincipal, aOpenMode, aWriteParams);
-
-  
-  runnable->AddRef();
 
   nsresult rv = NS_DispatchToMainThread(runnable);
   NS_ENSURE_SUCCESS(rv, nullptr);
 
-  return runnable;
+  
+  return runnable.forget().take();
 }
 
 void
 DeallocEntryParent(PAsmJSCacheEntryParent* aActor)
 {
   
-  static_cast<ParentProcessRunnable*>(aActor)->Release();
+  nsRefPtr<ParentProcessRunnable> op =
+    dont_AddRef(static_cast<ParentProcessRunnable*>(aActor));
 }
 
 namespace {
