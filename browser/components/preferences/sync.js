1d78af732a8511e8537a096402d743b942e4faa6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:32:37 +0000

    Backed out changeset 2b4f723f6c60 (bug 965461) for XPC Bustage on a CLOSED TREE
    UltraBlame original commit: 47c1c68b8a852fac65da788a796222f0b7e3db42

--- browser/components/preferences/sync.js
+++ browser/components/preferences/sync.js
@@ -127,10 +127,7 @@ let gSyncPane = {
           fxaLoginStatus.selectedIndex = FXA_LOGIN_UNVERIFIED;
           enginesListDisabled = true;
         
-        
-        
-        } else if (Weave.Service.identity.readyToAuthenticate &&
-                   Weave.Status.login != Weave.LOGIN_SUCCEEDED) {
+        } else if (Weave.Status.login != Weave.LOGIN_SUCCEEDED) {
           fxaLoginStatus.selectedIndex = FXA_LOGIN_FAILED;
           enginesListDisabled = true;
         
