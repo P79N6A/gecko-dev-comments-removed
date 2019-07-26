632687eca65522dead14a051195d67f576564b3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:58:50 +0000

    Bug 888083 (part 1) - Remove all but one of the cases where a vanilla .h file #includes an inline-header.  r=jandem.
    UltraBlame original commit: efefd3410901bc28ed8c2a284c349da5f5e4ec2f

--- js/src/jsapi-tests/tests.h
+++ js/src/jsapi-tests/tests.h
@@ -10,12 +10,12 @@
 #include "mozilla/Util.h"
 
 #include "jsapi.h"
+#include "jscntxt.h"
 #include "jsprvtd.h"
 #include "jsalloc.h"
 
 
 #include "jsgc.h"
-#include "jsgcinlines.h"
 
 #include "js/Vector.h"
 
