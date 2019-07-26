81b1b7b871858d55a800ab683d6328676d76fdec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:15:22 +0000

    Back out a2cdb1000e51 (bug 801627) because of Android crashes on a CLOSED TREE
    UltraBlame original commit: 8101371a2a4e9be8e2ced881f5d997cee176bc3c

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -1357,15 +1357,15 @@ abstract public class GeckoApp
 
         
         
-        mMainHandler.post(new Runnable() {
+        mMainHandler.post(new Runnable() { 
             public void run() {
-                mLayerView.show();
+                mLayerView.setVisibility(View.VISIBLE);
             }
         });
 
         FrameLayout decor = (FrameLayout)getWindow().getDecorView();
         decor.removeView(mFullScreenPluginContainer);
-
+        
         mFullScreenPluginView = null;
 
         GeckoScreenOrientationListener.getInstance().unlockScreenOrientation();
@@ -2651,7 +2651,7 @@ abstract public class GeckoApp
 
             mMainHandler.post(new Runnable() { 
                 public void run() {
-                    mLayerView.hide();
+                    mLayerView.setVisibility(View.INVISIBLE);
                 }
             });
         }
