ff5b13670b0b1f21cb3a83ae1c726f72d7e72d9a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:13:18 +0000

    Bug 1015894 - Use window.realFrameElement into b2g/components/ContentPermissionPrompt.js to dispatch the permission request to the correct iframe. r=ochameau
    UltraBlame original commit: 3220b7b2efc9022dde350eaa21528d065e6c7db9

--- b2g/components/test/mochitest/systemapp_helper.js
+++ b2g/components/test/mochitest/systemapp_helper.js
@@ -8,6 +8,7 @@ Services.scriptloader.loadSubScript("resource://gre/modules/SystemAppProxy.jsm",
 const { SystemAppProxy } = scope;
 
 let frame;
+let customEventTarget;
 
 let index = -1;
 function next() {
@@ -47,6 +48,10 @@ function listener(event) {
   } else if (n == 4) {
     assert.equal(event.type, "mozChromeEvent");
     assert.equal(event.detail.name, "fourth");
+  } else if (n == 5) {
+    assert.equal(event.type, "mozChromeEvent");
+    assert.equal(event.detail.name, "fifth");
+    assert.equal(event.target, customEventTarget);
 
     next(); 
   } else {
@@ -78,6 +83,8 @@ let steps = [
     let doc = win.document;
     frame = doc.createElement("iframe");
     doc.documentElement.appendChild(frame);
+
+    customEventTarget = frame.contentDocument.body;
 
     
     
@@ -118,6 +125,7 @@ let steps = [
     
     SystemAppProxy._sendCustomEvent("custom", { name: "third" });
     SystemAppProxy.dispatchEvent({ name: "fourth" });
+    SystemAppProxy._sendCustomEvent("custom", { name: "fifth" }, false, customEventTarget);
     
   },
 
