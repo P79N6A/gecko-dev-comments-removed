97d8515a76ada73040ccebd78cb3381929259041
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:22:32 +0000

    Bug 919414 - Part 1: Clear existing calls in test setup. r=hsinyi
    UltraBlame original commit: 8ee95615470320ce3ce0c100dccd935290581563

--- dom/telephony/test/marionette/head.js
+++ dom/telephony/test/marionette/head.js
@@ -2,6 +2,7 @@
 
 
 let Promise = SpecialPowers.Cu.import("resource://gre/modules/Promise.jsm").Promise;
+let telephony;
 
 
 
@@ -46,7 +47,71 @@ let emulator = (function() {
   };
 }());
 
-function startTest(test) {
+
+
+
+(function() {
+  function checkInitialState() {
+    log("Verify initial state.");
+    ok(telephony, 'telephony');
+    is(telephony.active, null, 'telephony.active');
+    ok(telephony.calls, 'telephony.calls');
+    is(telephony.calls.length, 0, 'telephony.calls.length');
+  }
+
+  
+
+
+  function clearCalls() {
+    let deferred = Promise.defer();
+
+    log("Clear existing calls.");
+    emulator.run("gsm clear", function(result) {
+      if (result[0] == "OK") {
+        waitFor(function() {
+          deferred.resolve();
+        }, function() {
+          return telephony.calls.length === 0;
+        });
+      } else {
+        log("Failed to clear existing calls.");
+        deferred.reject();
+      }
+    });
+
+    return deferred.promise;
+  }
+
+  this.checkInitialState = checkInitialState;
+  this.clearCalls = clearCalls;
+}());
+
+function _startTest(permissions, test) {
+  function permissionSetUp() {
+    SpecialPowers.setBoolPref("dom.mozSettings.enabled", true);
+    SpecialPowers.setBoolPref("dom.promise.enabled", true);
+    for (let per of permissions) {
+      SpecialPowers.addPermission(per, true, document);
+    }
+  }
+
+  function permissionTearDown() {
+    SpecialPowers.clearUserPref("dom.mozSettings.enabled");
+    SpecialPowers.clearUserPref("dom.promise.enabled");
+    for (let per of permissions) {
+      SpecialPowers.removePermission(per, document);
+    }
+  }
+
+  function setUp() {
+    log('== Test SetUp ==');
+    permissionSetUp();
+    
+    telephony = window.navigator.mozTelephony;
+    ok(telephony);
+    return clearCalls().then(checkInitialState);
+  }
+
   
   finish = (function() {
     let originalFinish = finish;
@@ -54,6 +119,7 @@ function startTest(test) {
     function tearDown() {
       log('== Test TearDown ==');
       emulator.waitFinish()
+        .then(permissionTearDown)
         .then(function() {
           originalFinish.apply(this, arguments);
         });
@@ -62,6 +128,24 @@ function startTest(test) {
     return tearDown.bind(this);
   }());
 
-  
-  test();
+  function mainTest() {
+    setUp()
+      .then(function onSuccess() {
+        log('== Test Start ==');
+        test();
+      }, function onError(error) {
+        SpecialPowers.Cu.reportError(error);
+        ok(false, 'SetUp error');
+      });
+  }
+
+  mainTest();
+}
+
+function startTest(test) {
+  _startTest(['telephony'], test);
+}
+
+function startTestWithPermissions(permissions, test) {
+  _startTest(permissions.concat('telephony'), test);
 }
