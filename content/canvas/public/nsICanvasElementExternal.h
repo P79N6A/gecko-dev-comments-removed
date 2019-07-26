7d5dfca75922cc04da5411c22b869d5c0de72842
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:41:58 +0000

    Backed out changeset 29ed3c34af5e (bug 997014)
    UltraBlame original commit: f5b8418508dd9499225db408128283723e54b55c

---
+++ content/canvas/public/nsICanvasElementExternal.h
@@ -0,0 +1,58 @@
+
+
+
+
+
+#ifndef nsICanvasElementExternal_h___
+#define nsICanvasElementExternal_h___
+
+#include "nsISupports.h"
+#include "GraphicsFilter.h"
+
+class gfxContext;
+class nsIFrame;
+struct gfxRect;
+
+#define NS_ICANVASELEMENTEXTERNAL_IID \
+  { 0x51870f54, 0x6c4c, 0x469a, {0xad, 0x46, 0xf0, 0xa9, 0x8e, 0x32, 0xa7, 0xe2 } }
+
+class nsRenderingContext;
+class nsICanvasRenderingContextInternal;
+
+struct _cairo_surface;
+
+
+
+
+
+
+
+
+
+
+
+class nsICanvasElementExternal : public nsISupports {
+public:
+  NS_DECLARE_STATIC_IID_ACCESSOR(NS_ICANVASELEMENTEXTERNAL_IID)
+
+  enum {
+    RenderFlagPremultAlpha = 0x1
+  };
+
+  
+
+
+  NS_IMETHOD_(nsIntSize) GetSizeExternal() = 0;
+
+  
+
+
+
+  NS_IMETHOD RenderContextsExternal(gfxContext *ctx,
+                                    GraphicsFilter aFilter,
+                                    uint32_t aFlags = RenderFlagPremultAlpha) = 0;
+};
+
+NS_DEFINE_STATIC_IID_ACCESSOR(nsICanvasElementExternal, NS_ICANVASELEMENTEXTERNAL_IID)
+
+#endif 
