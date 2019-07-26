dae015a58b457a30e2c2ae618a83daa3d1a131f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:00:59 +0000

    Bug 950703 - SpiderMonkey: Merge x86 and ARM's IonFrames.h files into a single file. r=jandem
    UltraBlame original commit: 02ead7ab22c4240fc7d633167ac3df817af50d63

--- js/src/jit/IonFrames.h
+++ js/src/jit/IonFrames.h
@@ -9,6 +9,8 @@
 
 #ifdef JS_ION
 
+#include <stdint.h>
+
 #include "jscntxt.h"
 #include "jsfun.h"
 
@@ -300,19 +302,545 @@ GetTopIonJSScript(uint8_t *ionTop, void **returnAddrOut, ExecutionMode mode)
     return iter.script();
 }
 
-} 
-} 
+static IonCode *const ION_FRAME_DOMGETTER       = (IonCode *)0x1;
+static IonCode *const ION_FRAME_DOMSETTER       = (IonCode *)0x2;
+static IonCode *const ION_FRAME_DOMMETHOD       = (IonCode *)0x3;
+static IonCode *const ION_FRAME_OOL_NATIVE      = (IonCode *)0x4;
+static IonCode *const ION_FRAME_OOL_PROPERTY_OP = (IonCode *)0x5;
+static IonCode *const ION_FRAME_OOL_PROXY       = (IonCode *)0x6;
 
-#if defined(JS_CPU_X86) || defined (JS_CPU_X64)
-# include "jit/shared/IonFrames-x86-shared.h"
-#elif defined (JS_CPU_ARM)
-# include "jit/arm/IonFrames-arm.h"
-#else
-# error "unsupported architecture"
-#endif
 
-namespace js {
-namespace jit {
+
+class IonCommonFrameLayout
+{
+    uint8_t *returnAddress_;
+    uintptr_t descriptor_;
+
+    static const uintptr_t FrameTypeMask = (1 << FRAMETYPE_BITS) - 1;
+
+  public:
+    static size_t offsetOfDescriptor() {
+        return offsetof(IonCommonFrameLayout, descriptor_);
+    }
+    static size_t offsetOfReturnAddress() {
+        return offsetof(IonCommonFrameLayout, returnAddress_);
+    }
+    FrameType prevType() const {
+        return FrameType(descriptor_ & FrameTypeMask);
+    }
+    void changePrevType(FrameType type) {
+        descriptor_ &= ~FrameTypeMask;
+        descriptor_ |= type;
+    }
+    size_t prevFrameLocalSize() const {
+        return descriptor_ >> FRAMESIZE_SHIFT;
+    }
+    void setFrameDescriptor(size_t size, FrameType type) {
+        descriptor_ = (size << FRAMESIZE_SHIFT) | type;
+    }
+    uint8_t *returnAddress() const {
+        return returnAddress_;
+    }
+    void setReturnAddress(uint8_t *addr) {
+        returnAddress_ = addr;
+    }
+};
+
+class IonJSFrameLayout : public IonCommonFrameLayout
+{
+    CalleeToken calleeToken_;
+    uintptr_t numActualArgs_;
+
+  public:
+    CalleeToken calleeToken() const {
+        return calleeToken_;
+    }
+    void replaceCalleeToken(CalleeToken calleeToken) {
+        calleeToken_ = calleeToken;
+    }
+
+    static size_t offsetOfCalleeToken() {
+        return offsetof(IonJSFrameLayout, calleeToken_);
+    }
+    static size_t offsetOfNumActualArgs() {
+        return offsetof(IonJSFrameLayout, numActualArgs_);
+    }
+    static size_t offsetOfThis() {
+        IonJSFrameLayout *base = nullptr;
+        return reinterpret_cast<size_t>(&base->argv()[0]);
+    }
+    static size_t offsetOfActualArgs() {
+        IonJSFrameLayout *base = nullptr;
+        
+        return reinterpret_cast<size_t>(&base->argv()[1]);
+    }
+    static size_t offsetOfActualArg(size_t arg) {
+        return offsetOfActualArgs() + arg * sizeof(Value);
+    }
+
+    Value thisv() {
+        return argv()[0];
+    }
+    Value *argv() {
+        return (Value *)(this + 1);
+    }
+    uintptr_t numActualArgs() const {
+        return numActualArgs_;
+    }
+
+    
+    
+    uintptr_t *slotRef(uint32_t slot) {
+        return (uintptr_t *)((uint8_t *)this - (slot * STACK_SLOT_SIZE));
+    }
+
+    static inline size_t Size() {
+        return sizeof(IonJSFrameLayout);
+    }
+};
+
+
+class IonEntryFrameLayout : public IonJSFrameLayout
+{
+  public:
+    static inline size_t Size() {
+        return sizeof(IonEntryFrameLayout);
+    }
+};
+
+class IonRectifierFrameLayout : public IonJSFrameLayout
+{
+  public:
+    static inline size_t Size() {
+        return sizeof(IonRectifierFrameLayout);
+    }
+};
+
+
+class IonUnwoundRectifierFrameLayout : public IonRectifierFrameLayout
+{
+  public:
+    static inline size_t Size() {
+        
+        
+        
+        return sizeof(IonUnwoundRectifierFrameLayout);
+    }
+};
+
+
+class IonExitFooterFrame
+{
+    const VMFunction *function_;
+    IonCode *ionCode_;
+
+  public:
+    static inline size_t Size() {
+        return sizeof(IonExitFooterFrame);
+    }
+    inline IonCode *ionCode() const {
+        return ionCode_;
+    }
+    inline IonCode **addressOfIonCode() {
+        return &ionCode_;
+    }
+    inline const VMFunction *function() const {
+        return function_;
+    }
+
+    
+    template <typename T>
+    T *outParam() {
+        return reinterpret_cast<T *>(reinterpret_cast<char *>(this) - sizeof(T));
+    }
+};
+
+class IonNativeExitFrameLayout;
+class IonOOLNativeExitFrameLayout;
+class IonOOLPropertyOpExitFrameLayout;
+class IonOOLProxyExitFrameLayout;
+class IonDOMExitFrameLayout;
+
+
+class IonExitFrameLayout : public IonCommonFrameLayout
+{
+    inline uint8_t *top() {
+        return reinterpret_cast<uint8_t *>(this + 1);
+    }
+
+  public:
+    static inline size_t Size() {
+        return sizeof(IonExitFrameLayout);
+    }
+    static inline size_t SizeWithFooter() {
+        return Size() + IonExitFooterFrame::Size();
+    }
+
+    inline IonExitFooterFrame *footer() {
+        uint8_t *sp = reinterpret_cast<uint8_t *>(this);
+        return reinterpret_cast<IonExitFooterFrame *>(sp - IonExitFooterFrame::Size());
+    }
+
+    
+    
+    
+    inline uint8_t *argBase() {
+        JS_ASSERT(footer()->ionCode() != nullptr);
+        return top();
+    }
+
+    inline bool isWrapperExit() {
+        return footer()->function() != nullptr;
+    }
+    inline bool isNativeExit() {
+        return footer()->ionCode() == nullptr;
+    }
+    inline bool isOOLNativeExit() {
+        return footer()->ionCode() == ION_FRAME_OOL_NATIVE;
+    }
+    inline bool isOOLPropertyOpExit() {
+        return footer()->ionCode() == ION_FRAME_OOL_PROPERTY_OP;
+    }
+    inline bool isOOLProxyExit() {
+        return footer()->ionCode() == ION_FRAME_OOL_PROXY;
+    }
+    inline bool isDomExit() {
+        IonCode *code = footer()->ionCode();
+        return
+            code == ION_FRAME_DOMGETTER ||
+            code == ION_FRAME_DOMSETTER ||
+            code == ION_FRAME_DOMMETHOD;
+    }
+
+    inline IonNativeExitFrameLayout *nativeExit() {
+        
+        JS_ASSERT(isNativeExit());
+        return reinterpret_cast<IonNativeExitFrameLayout *>(footer());
+    }
+    inline IonOOLNativeExitFrameLayout *oolNativeExit() {
+        JS_ASSERT(isOOLNativeExit());
+        return reinterpret_cast<IonOOLNativeExitFrameLayout *>(footer());
+    }
+    inline IonOOLPropertyOpExitFrameLayout *oolPropertyOpExit() {
+        JS_ASSERT(isOOLPropertyOpExit());
+        return reinterpret_cast<IonOOLPropertyOpExitFrameLayout *>(footer());
+    }
+    inline IonOOLProxyExitFrameLayout *oolProxyExit() {
+        JS_ASSERT(isOOLProxyExit());
+        return reinterpret_cast<IonOOLProxyExitFrameLayout *>(footer());
+    }
+    inline IonDOMExitFrameLayout *DOMExit() {
+        JS_ASSERT(isDomExit());
+        return reinterpret_cast<IonDOMExitFrameLayout *>(footer());
+    }
+};
+
+
+
+class IonNativeExitFrameLayout
+{
+  protected: 
+    IonExitFooterFrame footer_;
+    IonExitFrameLayout exit_;
+    uintptr_t argc_;
+
+    
+    
+    uint32_t loCalleeResult_;
+    uint32_t hiCalleeResult_;
+
+  public:
+    static inline size_t Size() {
+        return sizeof(IonNativeExitFrameLayout);
+    }
+
+    static size_t offsetOfResult() {
+        return offsetof(IonNativeExitFrameLayout, loCalleeResult_);
+    }
+    inline Value *vp() {
+        return reinterpret_cast<Value*>(&loCalleeResult_);
+    }
+    inline uintptr_t argc() const {
+        return argc_;
+    }
+};
+
+class IonOOLNativeExitFrameLayout
+{
+  protected: 
+    IonExitFooterFrame footer_;
+    IonExitFrameLayout exit_;
+
+    
+    IonCode *stubCode_;
+
+    uintptr_t argc_;
+
+    
+    
+    uint32_t loCalleeResult_;
+    uint32_t hiCalleeResult_;
+
+    
+    uint32_t loThis_;
+    uint32_t hiThis_;
+
+  public:
+    static inline size_t Size(size_t argc) {
+        
+        return sizeof(IonOOLNativeExitFrameLayout) + (argc * sizeof(Value));
+    }
+
+    static size_t offsetOfResult() {
+        return offsetof(IonOOLNativeExitFrameLayout, loCalleeResult_);
+    }
+
+    inline IonCode **stubCode() {
+        return &stubCode_;
+    }
+    inline Value *vp() {
+        return reinterpret_cast<Value*>(&loCalleeResult_);
+    }
+    inline Value *thisp() {
+        return reinterpret_cast<Value*>(&loThis_);
+    }
+    inline uintptr_t argc() const {
+        return argc_;
+    }
+};
+
+class IonOOLPropertyOpExitFrameLayout
+{
+  protected: 
+    IonExitFooterFrame footer_;
+    IonExitFrameLayout exit_;
+
+    
+    JSObject *obj_;
+
+    
+    jsid id_;
+
+    
+    
+    uint32_t vp0_;
+    uint32_t vp1_;
+
+    
+    IonCode *stubCode_;
+
+  public:
+    static inline size_t Size() {
+        return sizeof(IonOOLPropertyOpExitFrameLayout);
+    }
+
+    static size_t offsetOfResult() {
+        return offsetof(IonOOLPropertyOpExitFrameLayout, vp0_);
+    }
+
+    inline IonCode **stubCode() {
+        return &stubCode_;
+    }
+    inline Value *vp() {
+        return reinterpret_cast<Value*>(&vp0_);
+    }
+    inline jsid *id() {
+        return &id_;
+    }
+    inline JSObject **obj() {
+        return &obj_;
+    }
+};
+
+
+
+
+
+class IonOOLProxyExitFrameLayout
+{
+  protected: 
+    IonExitFooterFrame footer_;
+    IonExitFrameLayout exit_;
+
+    
+    JSObject *proxy_;
+
+    
+    JSObject *receiver_;
+
+    
+    jsid id_;
+
+    
+    
+    uint32_t vp0_;
+    uint32_t vp1_;
+
+    
+    IonCode *stubCode_;
+
+  public:
+    static inline size_t Size() {
+        return sizeof(IonOOLProxyExitFrameLayout);
+    }
+
+    static size_t offsetOfResult() {
+        return offsetof(IonOOLProxyExitFrameLayout, vp0_);
+    }
+
+    inline IonCode **stubCode() {
+        return &stubCode_;
+    }
+    inline Value *vp() {
+        return reinterpret_cast<Value*>(&vp0_);
+    }
+    inline jsid *id() {
+        return &id_;
+    }
+    inline JSObject **receiver() {
+        return &receiver_;
+    }
+    inline JSObject **proxy() {
+        return &proxy_;
+    }
+};
+
+class IonDOMExitFrameLayout
+{
+  protected: 
+    IonExitFooterFrame footer_;
+    IonExitFrameLayout exit_;
+    JSObject *thisObj;
+
+    
+    
+    uint32_t loCalleeResult_;
+    uint32_t hiCalleeResult_;
+
+  public:
+    static inline size_t Size() {
+        return sizeof(IonDOMExitFrameLayout);
+    }
+
+    static size_t offsetOfResult() {
+        return offsetof(IonDOMExitFrameLayout, loCalleeResult_);
+    }
+    inline Value *vp() {
+        return reinterpret_cast<Value*>(&loCalleeResult_);
+    }
+    inline JSObject **thisObjAddress() {
+        return &thisObj;
+    }
+    inline bool isMethodFrame() {
+        return footer_.ionCode() == ION_FRAME_DOMMETHOD;
+    }
+};
+
+struct IonDOMMethodExitFrameLayoutTraits;
+
+class IonDOMMethodExitFrameLayout
+{
+  protected: 
+    IonExitFooterFrame footer_;
+    IonExitFrameLayout exit_;
+    
+    
+    JSObject *thisObj_;
+    Value *argv_;
+    uintptr_t argc_;
+
+    
+    
+    uint32_t loCalleeResult_;
+    uint32_t hiCalleeResult_;
+
+    friend struct IonDOMMethodExitFrameLayoutTraits;
+
+  public:
+    static inline size_t Size() {
+        return sizeof(IonDOMMethodExitFrameLayout);
+    }
+
+    static size_t offsetOfResult() {
+        return offsetof(IonDOMMethodExitFrameLayout, loCalleeResult_);
+    }
+
+    inline Value *vp() {
+        
+        JS_STATIC_ASSERT(offsetof(IonDOMMethodExitFrameLayout, loCalleeResult_) ==
+                         (offsetof(IonDOMMethodExitFrameLayout, argc_) + sizeof(uintptr_t)));
+        return reinterpret_cast<Value*>(&loCalleeResult_);
+    }
+    inline JSObject **thisObjAddress() {
+        return &thisObj_;
+    }
+    inline uintptr_t argc() {
+        return argc_;
+    }
+};
+
+struct IonDOMMethodExitFrameLayoutTraits {
+    static const size_t offsetOfArgcFromArgv =
+        offsetof(IonDOMMethodExitFrameLayout, argc_) -
+        offsetof(IonDOMMethodExitFrameLayout, argv_);
+};
+
+class IonOsrFrameLayout : public IonJSFrameLayout
+{
+  public:
+    static inline size_t Size() {
+        return sizeof(IonOsrFrameLayout);
+    }
+};
+
+class ICStub;
+
+class IonBaselineStubFrameLayout : public IonCommonFrameLayout
+{
+  public:
+    static inline size_t Size() {
+        return sizeof(IonBaselineStubFrameLayout);
+    }
+
+    static inline int reverseOffsetOfStubPtr() {
+        return -int(sizeof(void *));
+    }
+    static inline int reverseOffsetOfSavedFramePtr() {
+        return -int(2 * sizeof(void *));
+    }
+
+    inline ICStub *maybeStubPtr() {
+        uint8_t *fp = reinterpret_cast<uint8_t *>(this);
+        return *reinterpret_cast<ICStub **>(fp + reverseOffsetOfStubPtr());
+    }
+};
+
+
+class InvalidationBailoutStack
+{
+    mozilla::Array<double, FloatRegisters::Total> fpregs_;
+    mozilla::Array<uintptr_t, Registers::Total> regs_;
+    IonScript   *ionScript_;
+    uint8_t       *osiPointReturnAddress_;
+
+  public:
+    uint8_t *sp() const {
+        return (uint8_t *) this + sizeof(InvalidationBailoutStack);
+    }
+    IonJSFrameLayout *fp() const;
+    MachineState machine() {
+        return MachineState::FromBailout(regs_, fpregs_);
+    }
+
+    IonScript *ionScript() const {
+        return ionScript_;
+    }
+    uint8_t *osiPointReturnAddress() const {
+        return osiPointReturnAddress_;
+    }
+
+    void checkInvariants() const;
+};
 
 void
 GetPcScript(JSContext *cx, JSScript **scriptRes, jsbytecode **pcRes);
