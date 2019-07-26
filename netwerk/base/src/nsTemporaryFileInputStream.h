4c8730f34bfbf1b443b6d63e43efc8563fb8d78f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:58:58 +0000

    Bug 803414 - Part 2: Add DOMTemporaryFileBlob and TemporaryFileInputStream. r=roc
    UltraBlame original commit: d093ceb34b2ea6d1ee24309aaa393c236ac2ade2

---
+++ netwerk/base/src/nsTemporaryFileInputStream.h
@@ -0,0 +1,52 @@
+
+
+
+
+
+#ifndef nsTemporaryFileInputStream_h__
+#define nsTemporaryFileInputStream_h__
+
+#include "nsFileStreams.h"
+#include "mozilla/Mutex.h"
+
+class nsTemporaryFileInputStream : public nsIInputStream
+{
+public:
+  
+  class FileDescOwner
+  {
+    friend class nsTemporaryFileInputStream;
+  public:
+    NS_INLINE_DECL_THREADSAFE_REFCOUNTING(FileDescOwner)
+    FileDescOwner(PRFileDesc* aFD)
+      : mFD(aFD),
+        mMutex("FileDescOwner::mMutex")
+    {
+      MOZ_ASSERT(aFD);
+    }
+    ~FileDescOwner()
+    {
+      PR_Close(mFD);
+    }
+    mozilla::Mutex& FileMutex() { return mMutex; }
+
+  private:
+    PRFileDesc* mFD;
+    mozilla::Mutex mMutex;
+  };
+
+  nsTemporaryFileInputStream(FileDescOwner* aFileDescOwner, uint64_t aStartPos, uint64_t aEndPos);
+
+  virtual ~nsTemporaryFileInputStream() { }
+
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIINPUTSTREAM
+
+private:
+  nsRefPtr<FileDescOwner> mFileDescOwner;
+  uint64_t mStartPos;
+  uint64_t mEndPos;
+  bool mClosed;
+};
+
+#endif 
