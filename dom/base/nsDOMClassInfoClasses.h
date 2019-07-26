dde4cf31eedddee346adeae2439a974ce4b4dd7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:30:19 +0000

    Bug 910088 - Use webIDL dictionaries for DOMWindowResize event details r=bz
    UltraBlame original commit: f3d8e568ec114f7595494a151fb486676226833c

--- dom/base/nsDOMClassInfoClasses.h
+++ dom/base/nsDOMClassInfoClasses.h
@@ -131,17 +131,11 @@ DOMCI_CLASS(MozVoicemail)
 DOMCI_CLASS(MozIccManager)
 #endif
 
-#ifdef MOZ_B2G_FM
-DOMCI_CLASS(FMRadio)
-#endif
-
 #ifdef MOZ_B2G_BT
 DOMCI_CLASS(BluetoothDevice)
 #endif
 
 DOMCI_CLASS(CameraCapabilities)
-
-DOMCI_CLASS(DOMWindowResizeEventDetail)
 
 DOMCI_CLASS(LockedFile)
 
