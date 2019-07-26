e2227e0b711478f8d8862c8081b9e42478510613
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:18:35 +0000

    Bug 803719: Add a getClientRect API to CaretPosition and use it to maintain position for reflow-on-zoom feature. [r=kats,tn]
    UltraBlame original commit: 6e5159fc2289e7771bb08c2a3b203b4cfedcd69a

--- content/base/src/nsDOMCaretPosition.h
+++ content/base/src/nsDOMCaretPosition.h
@@ -8,7 +8,10 @@
 #include "nsCycleCollectionParticipant.h"
 #include "nsCOMPtr.h"
 #include "nsINode.h"
+#include "nsRange.h"
 #include "nsWrapperCache.h"
+#include "nsRect.h"
+#include "nsClientRect.h"
 
 
 
@@ -46,6 +49,32 @@ public:
 
   nsINode* GetOffsetNode() const;
 
+  
+
+
+
+
+
+
+  already_AddRefed<nsClientRect> GetClientRect() const;
+
+  
+
+
+
+
+
+
+
+
+
+
+
+  void SetAnonymousContentNode(nsINode* aNode)
+  {
+    mAnonymousContentNode = aNode;
+  }
+
   nsISupports* GetParentObject() const
   {
     return GetOffsetNode();
@@ -56,8 +85,10 @@ public:
 
 protected:
   virtual ~nsDOMCaretPosition();
+
   uint32_t mOffset;
   nsCOMPtr<nsINode> mOffsetNode;
+  nsCOMPtr<nsINode> mAnonymousContentNode;
 };
 #endif
 
