138ca7994aed4b736e547a8ddc33f6fa9a3ed5be
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:23:06 +0000

    Backed out changeset 8059895497c1 (bug 988363)
    UltraBlame original commit: ba4174092535f09d0024b6c20c8b6d7e3ac8a190

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -669,22 +669,6 @@ CouldBeDOMBinding(nsWrapperCache* aCache)
   return aCache->IsDOMBinding();
 }
 
-inline bool
-TryToOuterize(JSContext* cx, JS::MutableHandle<JS::Value> rval)
-{
-  if (js::IsInnerObject(&rval.toObject())) {
-    JS::Rooted<JSObject*> obj(cx, &rval.toObject());
-    obj = JS_ObjectToOuterObject(cx, obj);
-    if (!obj) {
-      return false;
-    }
-
-    rval.set(JS::ObjectValue(*obj));
-  }
-
-  return true;
-}
-
 
 
 MOZ_ALWAYS_INLINE
@@ -714,9 +698,9 @@ MaybeWrapObjectValue(JSContext* cx, JS::MutableHandle<JS::Value> rval)
   }
 
   
-  
   if (IsDOMObject(obj)) {
-    return TryToOuterize(cx, rval);
+    rval.set(JS::ObjectValue(*obj));
+    return true;
   }
 
   
@@ -837,17 +821,14 @@ WrapNewBindingObject(JSContext* cx, JS::Handle<JSObject*> scope, T* value,
   MOZ_ASSERT(js::IsObjectInContextCompartment(scope, cx));
 #endif
 
-  rval.set(JS::ObjectValue(*obj));
-
   bool sameCompartment =
     js::GetObjectCompartment(obj) == js::GetContextCompartment(cx);
   if (sameCompartment && couldBeDOMBinding) {
-    
-    
-    return IsBaseOf<nsGlobalWindow, T>::value || IsSame<EventTarget, T>::value ?
-           TryToOuterize(cx, rval) : true;
+    rval.set(JS::ObjectValue(*obj));
+    return true;
   }
 
+  rval.set(JS::ObjectValue(*obj));
   return JS_WrapValue(cx, rval);
 }
 
