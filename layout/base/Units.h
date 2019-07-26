9cb9c8c07f781580926210f7e26ae5a0fed706eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:49:01 +0000

    Bug 880676 - Units.h cleanup for easier reading of future patches. r=BenWa
    UltraBlame original commit: a26c17d194c13b06ba4a4f6efebc78b4b5834fb9

--- layout/base/Units.h
+++ layout/base/Units.h
@@ -8,68 +8,93 @@
 #define MOZ_UNITS_H_
 
 #include "mozilla/gfx/Point.h"
+#include "mozilla/gfx/Rect.h"
 #include "nsDeviceContext.h"
 
 namespace mozilla {
+
+struct CSSPixel;
+struct LayerPixel;
+struct ScreenPixel;
+
+typedef gfx::PointTyped<CSSPixel> CSSPoint;
+typedef gfx::IntPointTyped<CSSPixel> CSSIntPoint;
+typedef gfx::SizeTyped<CSSPixel> CSSSize;
+typedef gfx::IntSizeTyped<CSSPixel> CSSIntSize;
+typedef gfx::RectTyped<CSSPixel> CSSRect;
+typedef gfx::IntRectTyped<CSSPixel> CSSIntRect;
+
+typedef gfx::PointTyped<LayerPixel> LayerPoint;
+typedef gfx::IntPointTyped<LayerPixel> LayerIntPoint;
+typedef gfx::SizeTyped<LayerPixel> LayerSize;
+typedef gfx::IntSizeTyped<LayerPixel> LayerIntSize;
+typedef gfx::RectTyped<LayerPixel> LayerRect;
+typedef gfx::IntRectTyped<LayerPixel> LayerIntRect;
+
+typedef gfx::PointTyped<ScreenPixel> ScreenPoint;
+typedef gfx::IntPointTyped<ScreenPixel> ScreenIntPoint;
+typedef gfx::SizeTyped<ScreenPixel> ScreenSize;
+typedef gfx::IntSizeTyped<ScreenPixel> ScreenIntSize;
+typedef gfx::RectTyped<ScreenPixel> ScreenRect;
+typedef gfx::IntRectTyped<ScreenPixel> ScreenIntRect;
 
 
 
 
 struct CSSPixel {
-  static gfx::IntPointTyped<CSSPixel> RoundToInt(const gfx::PointTyped<CSSPixel>& aPoint) {
-    return gfx::IntPointTyped<CSSPixel>(NS_lround(aPoint.x),
-                                        NS_lround(aPoint.y));
+
+  
+
+  static CSSIntPoint RoundToInt(const CSSPoint& aPoint) {
+    return CSSIntPoint(NS_lround(aPoint.x),
+                       NS_lround(aPoint.y));
   }
 
-  static gfx::PointTyped<CSSPixel> FromAppUnits(const nsPoint& aPoint) {
-    return gfx::PointTyped<CSSPixel>(NSAppUnitsToFloatPixels(aPoint.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
-                                     NSAppUnitsToFloatPixels(aPoint.y, float(nsDeviceContext::AppUnitsPerCSSPixel())));
+  
+
+  static CSSPoint FromAppUnits(const nsPoint& aPoint) {
+    return CSSPoint(NSAppUnitsToFloatPixels(aPoint.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
+                    NSAppUnitsToFloatPixels(aPoint.y, float(nsDeviceContext::AppUnitsPerCSSPixel())));
   }
 
-  static gfx::IntPointTyped<CSSPixel> FromAppUnitsRounded(const nsPoint& aPoint) {
-    return gfx::IntPointTyped<CSSPixel>(NSAppUnitsToIntPixels(aPoint.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
-                                        NSAppUnitsToIntPixels(aPoint.y, float(nsDeviceContext::AppUnitsPerCSSPixel())));
+  static CSSRect FromAppUnits(const nsRect& aRect) {
+    return CSSRect(NSAppUnitsToFloatPixels(aRect.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
+                   NSAppUnitsToFloatPixels(aRect.y, float(nsDeviceContext::AppUnitsPerCSSPixel())),
+                   NSAppUnitsToFloatPixels(aRect.width, float(nsDeviceContext::AppUnitsPerCSSPixel())),
+                   NSAppUnitsToFloatPixels(aRect.height, float(nsDeviceContext::AppUnitsPerCSSPixel())));
   }
 
-  static nsPoint ToAppUnits(const gfx::PointTyped<CSSPixel>& aPoint) {
+  static CSSIntPoint FromAppUnitsRounded(const nsPoint& aPoint) {
+    return CSSIntPoint(NSAppUnitsToIntPixels(aPoint.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
+                       NSAppUnitsToIntPixels(aPoint.y, float(nsDeviceContext::AppUnitsPerCSSPixel())));
+  }
+
+  static CSSIntRect FromAppUnitsRounded(const nsRect& aRect) {
+    return CSSIntRect(NSAppUnitsToIntPixels(aRect.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
+                      NSAppUnitsToIntPixels(aRect.y, float(nsDeviceContext::AppUnitsPerCSSPixel())),
+                      NSAppUnitsToIntPixels(aRect.width, float(nsDeviceContext::AppUnitsPerCSSPixel())),
+                      NSAppUnitsToIntPixels(aRect.height, float(nsDeviceContext::AppUnitsPerCSSPixel())));
+  }
+
+  
+
+  static nsPoint ToAppUnits(const CSSPoint& aPoint) {
     return nsPoint(NSFloatPixelsToAppUnits(aPoint.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
                    NSFloatPixelsToAppUnits(aPoint.y, float(nsDeviceContext::AppUnitsPerCSSPixel())));
   }
 
-  static nsPoint ToAppUnits(const gfx::IntPointTyped<CSSPixel>& aPoint)
-  {
+  static nsPoint ToAppUnits(const CSSIntPoint& aPoint) {
     return nsPoint(NSIntPixelsToAppUnits(aPoint.x, nsDeviceContext::AppUnitsPerCSSPixel()),
                    NSIntPixelsToAppUnits(aPoint.y, nsDeviceContext::AppUnitsPerCSSPixel()));
   }
 
-  static gfx::RectTyped<CSSPixel> FromAppUnits(const nsRect& aRect) {
-    return gfx::RectTyped<CSSPixel>(NSAppUnitsToFloatPixels(aRect.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
-                                    NSAppUnitsToFloatPixels(aRect.y, float(nsDeviceContext::AppUnitsPerCSSPixel())),
-                                    NSAppUnitsToFloatPixels(aRect.width, float(nsDeviceContext::AppUnitsPerCSSPixel())),
-                                    NSAppUnitsToFloatPixels(aRect.height, float(nsDeviceContext::AppUnitsPerCSSPixel())));
-  }
-
-  static nsRect ToAppUnits(const gfx::RectTyped<CSSPixel>& aRect) {
+  static nsRect ToAppUnits(const CSSRect& aRect) {
     return nsRect(NSFloatPixelsToAppUnits(aRect.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
                   NSFloatPixelsToAppUnits(aRect.y, float(nsDeviceContext::AppUnitsPerCSSPixel())),
                   NSFloatPixelsToAppUnits(aRect.width, float(nsDeviceContext::AppUnitsPerCSSPixel())),
                   NSFloatPixelsToAppUnits(aRect.height, float(nsDeviceContext::AppUnitsPerCSSPixel())));
   }
-
-  static gfx::IntRectTyped<CSSPixel> FromAppUnitsRounded(const nsRect& aRect)
-  {
-    return gfx::IntRectTyped<CSSPixel>(NSAppUnitsToIntPixels(aRect.x, float(nsDeviceContext::AppUnitsPerCSSPixel())),
-                                       NSAppUnitsToIntPixels(aRect.y, float(nsDeviceContext::AppUnitsPerCSSPixel())),
-                                       NSAppUnitsToIntPixels(aRect.width, float(nsDeviceContext::AppUnitsPerCSSPixel())),
-                                       NSAppUnitsToIntPixels(aRect.height, float(nsDeviceContext::AppUnitsPerCSSPixel())));
-  }
 };
-
-typedef gfx::PointTyped<CSSPixel> CSSPoint;
-typedef gfx::IntPointTyped<CSSPixel> CSSIntPoint;
-typedef gfx::SizeTyped<CSSPixel> CSSSize;
-typedef gfx::RectTyped<CSSPixel> CSSRect;
-typedef gfx::IntRectTyped<CSSPixel> CSSIntRect;
 
 
 
@@ -80,40 +105,39 @@ typedef gfx::IntRectTyped<CSSPixel> CSSIntRect;
 
 
 struct LayerPixel {
-  static gfx::PointTyped<LayerPixel> FromCSSPoint(const CSSPoint& aPoint, float aResolutionX, float aResolutionY) {
-    return gfx::PointTyped<LayerPixel>(aPoint.x * aResolutionX,
-                                       aPoint.y * aResolutionY);
+
+  
+
+  static LayerIntRect RoundToInt(const LayerRect& aRect) {
+    return LayerIntRect(NS_lround(aRect.x),
+                        NS_lround(aRect.y),
+                        NS_lround(aRect.width),
+                        NS_lround(aRect.height));
   }
 
-  static gfx::IntPointTyped<LayerPixel> FromCSSPointRounded(const CSSPoint& aPoint, float aResolutionX, float aResolutionY) {
-    return gfx::IntPointTyped<LayerPixel>(NS_lround(aPoint.x * aResolutionX),
-                                          NS_lround(aPoint.y * aResolutionY));
+  
+
+  static LayerPoint FromCSSPoint(const CSSPoint& aPoint, float aResolutionX, float aResolutionY) {
+    return LayerPoint(aPoint.x * aResolutionX,
+                      aPoint.y * aResolutionY);
   }
 
-  static gfx::IntRectTyped<LayerPixel> RoundToInt(const gfx::RectTyped<LayerPixel>& aRect) {
-    return gfx::IntRectTyped<LayerPixel>(NS_lround(aRect.x),
-                                         NS_lround(aRect.y),
-                                         NS_lround(aRect.width),
-                                         NS_lround(aRect.height));
+  static LayerIntPoint FromCSSPointRounded(const CSSPoint& aPoint, float aResolutionX, float aResolutionY) {
+    return LayerIntPoint(NS_lround(aPoint.x * aResolutionX),
+                         NS_lround(aPoint.y * aResolutionY));
   }
 
-  static gfx::RectTyped<LayerPixel> FromCSSRect(const CSSRect& aRect, float aResolutionX, float aResolutionY) {
-    return gfx::RectTyped<LayerPixel>(aRect.x * aResolutionX,
-                                      aRect.y * aResolutionY,
-                                      aRect.width * aResolutionX,
-                                      aRect.height * aResolutionY);
+  static LayerRect FromCSSRect(const CSSRect& aRect, float aResolutionX, float aResolutionY) {
+    return LayerRect(aRect.x * aResolutionX,
+                     aRect.y * aResolutionY,
+                     aRect.width * aResolutionX,
+                     aRect.height * aResolutionY);
   }
 
-  static gfx::IntRectTyped<LayerPixel> FromCSSRectRounded(const CSSRect& aRect, float aResolutionX, float aResolutionY) {
+  static LayerIntRect FromCSSRectRounded(const CSSRect& aRect, float aResolutionX, float aResolutionY) {
     return RoundToInt(FromCSSRect(aRect, aResolutionX, aResolutionY));
   }
 };
-
-typedef gfx::PointTyped<LayerPixel> LayerPoint;
-typedef gfx::IntPointTyped<LayerPixel> LayerIntPoint;
-typedef gfx::IntSizeTyped<LayerPixel> LayerIntSize;
-typedef gfx::RectTyped<LayerPixel> LayerRect;
-typedef gfx::IntRectTyped<LayerPixel> LayerIntRect;
 
 
 
@@ -125,29 +149,27 @@ typedef gfx::IntRectTyped<LayerPixel> LayerIntRect;
 
 
 struct ScreenPixel {
-  static gfx::PointTyped<ScreenPixel> FromCSSPoint(const CSSPoint& aPoint, float aResolutionX, float aResolutionY) {
-    return gfx::PointTyped<ScreenPixel>(aPoint.x * aResolutionX,
-                                        aPoint.y * aResolutionY);
+
+  
+
+  static ScreenPoint FromCSSPoint(const CSSPoint& aPoint, float aResolutionX, float aResolutionY) {
+    return ScreenPoint(aPoint.x * aResolutionX,
+                       aPoint.y * aResolutionY);
   }
 
-  static CSSPoint ToCSSPoint(const gfx::PointTyped<ScreenPixel>& aPoint, float aResolutionX, float aResolutionY) {
+  
+
+  static CSSPoint ToCSSPoint(const ScreenPoint& aPoint, float aResolutionX, float aResolutionY) {
     return CSSPoint(aPoint.x * aResolutionX,
                     aPoint.y * aResolutionY);
   }
 
-  static CSSIntRect ToCSSIntRectRoundIn(const gfx::IntRectTyped<ScreenPixel>& aRect, float aResolutionX, float aResolutionY) {
-    gfx::IntRectTyped<CSSPixel> ret(aRect.x, aRect.y, aRect.width, aRect.height);
+  static CSSIntRect ToCSSIntRectRoundIn(const ScreenIntRect& aRect, float aResolutionX, float aResolutionY) {
+    CSSIntRect ret(aRect.x, aRect.y, aRect.width, aRect.height);
     ret.ScaleInverseRoundIn(aResolutionX, aResolutionY);
     return ret;
   }
 };
-
-typedef gfx::PointTyped<ScreenPixel> ScreenPoint;
-typedef gfx::IntPointTyped<ScreenPixel> ScreenIntPoint;
-typedef gfx::SizeTyped<ScreenPixel> ScreenSize;
-typedef gfx::IntSizeTyped<ScreenPixel> ScreenIntSize;
-typedef gfx::RectTyped<ScreenPixel> ScreenRect;
-typedef gfx::IntRectTyped<ScreenPixel> ScreenIntRect;
 
 };
 
