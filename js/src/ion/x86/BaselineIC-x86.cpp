7cd4ed05b92d908f505216052efb6a6459361f45
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:07:30 +0000

    BaselineCompiler: Fix x86 build error. r=tbpl-red
    UltraBlame original commit: ca9f459a5854c6646ae3201bf962a78169300fdc

--- js/src/ion/x86/BaselineIC-x86.cpp
+++ js/src/ion/x86/BaselineIC-x86.cpp
@@ -135,7 +135,7 @@ ICBinaryArith_Int32::Compiler::generateStubCode(MacroAssembler &masm)
         masm.bind(&done);
         
         JS_ASSERT(R0.payloadReg() == edx);
-        JS_ASSERT(R0.tagReg() == ecx);
+        JS_ASSERT(R0.typeReg() == ecx);
         break;
       }
       case JSOP_BITOR:
