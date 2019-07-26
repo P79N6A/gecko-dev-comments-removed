976e2847fa921639f14f843302a989c5d31c3e01
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:00:04 +0000

    Bug 975688 part.6 Rename nsDOMCompositionEvent to mozilla::dom::CompositionEvent r=smaug
    UltraBlame original commit: 15320fc9ba1c8cb982ebc87661a3814ec9ab8cf3

---
+++ dom/events/CompositionEvent.h
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_CompositionEvent_h_
+#define mozilla_dom_CompositionEvent_h_
+
+#include "nsDOMUIEvent.h"
+#include "nsIDOMCompositionEvent.h"
+#include "mozilla/dom/CompositionEventBinding.h"
+#include "mozilla/EventForwards.h"
+
+namespace mozilla {
+namespace dom {
+
+class CompositionEvent : public nsDOMUIEvent,
+                         public nsIDOMCompositionEvent
+{
+public:
+  CompositionEvent(EventTarget* aOwner,
+                   nsPresContext* aPresContext,
+                   WidgetCompositionEvent* aEvent);
+
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_FORWARD_TO_NSDOMUIEVENT
+  NS_DECL_NSIDOMCOMPOSITIONEVENT
+
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aScope) MOZ_OVERRIDE
+  {
+    return CompositionEventBinding::Wrap(aCx, aScope, this);
+  }
+
+  void InitCompositionEvent(const nsAString& aType,
+                            bool aCanBubble,
+                            bool aCancelable,
+                            nsIDOMWindow* aView,
+                            const nsAString& aData,
+                            const nsAString& aLocale,
+                            ErrorResult& aRv)
+  {
+    aRv = InitCompositionEvent(aType, aCanBubble, aCancelable, aView,
+                               aData, aLocale);
+  }
+
+protected:
+  nsString mData;
+  nsString mLocale;
+};
+
+} 
+} 
+
+#endif 
