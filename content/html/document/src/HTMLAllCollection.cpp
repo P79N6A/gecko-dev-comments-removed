28011388657e300d73815085d28f6596130fcbc9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:42:28 +0000

    Bug 968766 - Part b: Store the document.all NodeList in a C++ member rather than a reserved slot; r=jst
    UltraBlame original commit: f4c8f7295a26ea3d41310e3b4841a82628005531

--- content/html/document/src/HTMLAllCollection.cpp
+++ content/html/document/src/HTMLAllCollection.cpp
@@ -7,9 +7,8 @@
 #include "mozilla/dom/HTMLAllCollection.h"
 
 #include "jsapi.h"
-#include "mozilla/dom/BindingUtils.h"
-#include "mozilla/dom/HTMLCollectionBinding.h"
 #include "mozilla/HoldDropJSObjects.h"
+#include "nsContentUtils.h"
 #include "nsDOMClassInfo.h"
 #include "nsHTMLDocument.h"
 #include "nsJSUtils.h"
@@ -22,9 +21,10 @@ using namespace mozilla::dom;
 class nsHTMLDocumentSH
 {
 protected:
-  static bool GetDocumentAllNodeList(JSContext *cx, JS::Handle<JSObject*> obj,
-                                     nsDocument *doc,
-                                     nsContentList **nodeList);
+  static bool GetDocumentAllNodeList(JSContext*,
+                                     JS::Handle<JSObject*>,
+                                     nsHTMLDocument* aDocument,
+                                     nsContentList** aNodeList);
 public:
   static bool DocumentAllGetProperty(JSContext *cx, JS::Handle<JSObject*> obj, JS::Handle<jsid> id,
                                      JS::MutableHandle<JS::Value> vp);
@@ -37,7 +37,7 @@ public:
 const JSClass sHTMLDocumentAllClass = {
   "HTML document.all class",
   JSCLASS_HAS_PRIVATE | JSCLASS_PRIVATE_IS_NSISUPPORTS | JSCLASS_NEW_RESOLVE |
-  JSCLASS_EMULATES_UNDEFINED | JSCLASS_HAS_RESERVED_SLOTS(1),
+  JSCLASS_EMULATES_UNDEFINED,
   JS_PropertyStub,                                         
   JS_DeletePropertyStub,                                   
   nsHTMLDocumentSH::DocumentAllGetProperty,                
@@ -73,11 +73,13 @@ NS_IMPL_CYCLE_COLLECTION_CLASS(HTMLAllCollection)
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE_BEGIN(HTMLAllCollection)
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE_SCRIPT_OBJECTS
   NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mDocument)
+  NS_IMPL_CYCLE_COLLECTION_TRAVERSE(mCollection)
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE_END
 
 NS_IMPL_CYCLE_COLLECTION_UNLINK_BEGIN(HTMLAllCollection)
   tmp->mObject = nullptr;
   NS_IMPL_CYCLE_COLLECTION_UNLINK(mDocument)
+  NS_IMPL_CYCLE_COLLECTION_UNLINK(mCollection)
 NS_IMPL_CYCLE_COLLECTION_UNLINK_END
 
 NS_IMPL_CYCLE_COLLECTION_TRACE_BEGIN(HTMLAllCollection)
@@ -108,6 +110,17 @@ HTMLAllCollection::GetObject(JSContext* aCx, ErrorResult& aRv)
 
   JS::ExposeObjectToActiveJS(mObject);
   return mObject;
+}
+
+nsContentList*
+HTMLAllCollection::Collection()
+{
+  if (!mCollection) {
+    nsIDocument* document = mDocument;
+    mCollection = document->GetElementsByTagName(NS_LITERAL_STRING("*"));
+    MOZ_ASSERT(mCollection);
+  }
+  return mCollection;
 }
 
 } 
@@ -152,68 +165,14 @@ WrapNative(JSContext *cx, JSObject *scope, nsISupports *native,
 
 
 bool
-nsHTMLDocumentSH::GetDocumentAllNodeList(JSContext *cx,
-                                         JS::Handle<JSObject*> obj,
-                                         nsDocument *domdoc,
-                                         nsContentList **nodeList)
+nsHTMLDocumentSH::GetDocumentAllNodeList(JSContext*,
+                                         JS::Handle<JSObject*>,
+                                         nsHTMLDocument* aDocument,
+                                         nsContentList** aNodeList)
 {
-  
-  
-  
-  
-  
-  
-  nsresult rv = NS_OK;
-
-  JS::Rooted<JS::Value> collection(cx, JS_GetReservedSlot(obj, 0));
-
-  if (!JSVAL_IS_PRIMITIVE(collection)) {
-    
-    JS::Rooted<JSObject*> obj(cx, JSVAL_TO_OBJECT(collection));
-    nsIHTMLCollection* htmlCollection;
-    rv = UNWRAP_OBJECT(HTMLCollection, obj, htmlCollection);
-    if (NS_SUCCEEDED(rv)) {
-      NS_ADDREF(*nodeList = static_cast<nsContentList*>(htmlCollection));
-    }
-    else {
-      nsISupports *native = nsDOMClassInfo::XPConnect()->GetNativeOfWrapper(cx, obj);
-      if (native) {
-        NS_ADDREF(*nodeList = nsContentList::FromSupports(native));
-        rv = NS_OK;
-      }
-      else {
-        rv = NS_ERROR_FAILURE;
-      }
-    }
-  } else {
-    
-
-    nsRefPtr<nsContentList> list =
-      domdoc->GetElementsByTagName(NS_LITERAL_STRING("*"));
-    if (!list) {
-      rv = NS_ERROR_OUT_OF_MEMORY;
-    }
-
-    nsresult tmp = WrapNative(cx, JS::CurrentGlobalOrNull(cx),
-                              static_cast<nsINodeList*>(list), list, false,
-                              &collection);
-    if (NS_FAILED(tmp)) {
-      rv = tmp;
-    }
-
-    list.forget(nodeList);
-
-    
-    JS_SetReservedSlot(obj, 0, collection);
-  }
-
-  if (NS_FAILED(rv)) {
-    xpc::Throw(cx, NS_ERROR_FAILURE);
-
-    return false;
-  }
-
-  return *nodeList != nullptr;
+  nsRefPtr<nsContentList> collection = aDocument->All()->Collection();
+  collection.forget(aNodeList);
+  return true;
 }
 
 bool
