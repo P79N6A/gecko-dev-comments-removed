20b8255d745e0b0c460e3d89bf2d3663644d5aa9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:14:38 +0000

    Bug 987111 - Make JSProto_Object COWs take precedence over Xrays. r=gabor
    UltraBlame original commit: d9788144b427606456bfa6bbc5206c88bac76ac4

--- js/xpconnect/wrappers/WrapperFactory.cpp
+++ js/xpconnect/wrappers/WrapperFactory.cpp
@@ -120,6 +120,21 @@ WrapperFactory::DoubleWrap(JSContext *cx, HandleObject obj, unsigned flags)
     return obj;
 }
 
+
+
+
+static bool
+ForceCOWBehavior(JSObject *obj)
+{
+    if (IdentifyStandardInstanceOrPrototype(obj) == JSProto_Object) {
+        MOZ_ASSERT(GetXrayType(obj) == XrayForJSObject,
+                   "We should use XrayWrappers for standard ES Object instances "
+                   "modulo this hack");
+        return true;
+    }
+    return false;
+}
+
 JSObject *
 WrapperFactory::PrepareForWrapping(JSContext *cx, HandleObject scope,
                                    HandleObject objArg, unsigned flags)
@@ -169,7 +184,7 @@ WrapperFactory::PrepareForWrapping(JSContext *cx, HandleObject scope,
     bool subsumes = AccessCheck::subsumes(js::GetContextCompartment(cx),
                                           js::GetObjectCompartment(obj));
     XrayType xrayType = GetXrayType(obj);
-    if (!subsumes && xrayType == NotXray) {
+    if (!subsumes && (xrayType == NotXray || ForceCOWBehavior(obj))) {
         JSProtoKey key = JSProto_Null;
         {
             JSAutoCompartment ac(cx, obj);
@@ -418,7 +433,12 @@ WrapperFactory::Rewrap(JSContext *cx, HandleObject existing, HandleObject obj,
 
     
     
-    else if (originIsChrome && !targetIsChrome && xrayType == NotXray) {
+    
+    
+    
+    else if (originIsChrome && !targetIsChrome &&
+             (xrayType == NotXray || ForceCOWBehavior(obj)))
+    {
         wrapper = &ChromeObjectWrapper::singleton;
     }
 
