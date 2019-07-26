930f1e5e4a983056464bb26acb87759f8f2f4da9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:13:24 +0000

    Bug 987111 - Proxy::set should create enumerable properties. r=efaust
    Properties are supposed to be enumerable by default. It's unfortunate that
    the default is reversed in SpiderMonkey.
    
    UltraBlame original commit: 944d128f135aeebe1d91d940f5ad1d81917db58f

--- js/src/jsproxy.cpp
+++ js/src/jsproxy.cpp
@@ -2587,6 +2587,7 @@ Proxy::set(JSContext *cx, HandleObject proxy, HandleObject receiver, HandleId id
     
     Rooted<PropertyDescriptor> newDesc(cx);
     newDesc.value().set(vp);
+    newDesc.setAttributes(JSPROP_ENUMERATE);
     return handler->defineProperty(cx, receiver, id, &newDesc);
 }
 
