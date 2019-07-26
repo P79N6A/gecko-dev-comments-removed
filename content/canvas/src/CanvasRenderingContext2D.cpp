51fa570ad1af19011d419257d1916b149a8ddc74
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:29:23 +0000

    Bug 866575: Throw when creating a pattern with a large source r=nrc, mattwoodrow
    UltraBlame original commit: 1edc14e711676f65de9920b262ffe7ba312c5895

--- content/canvas/src/CanvasRenderingContext2D.cpp
+++ content/canvas/src/CanvasRenderingContext2D.cpp
@@ -1455,6 +1455,7 @@ CanvasRenderingContext2D::CreatePattern(const HTMLImageOrCanvasOrVideoElement& e
 
   
   if (!res.mSurface->CairoSurface() || res.mSurface->CairoStatus()) {
+    error.Throw(NS_ERROR_NOT_AVAILABLE);
     return nullptr;
   }
 
