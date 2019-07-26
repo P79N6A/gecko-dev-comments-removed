78e64d9b9c0a136b9c4bed2beea022452abae5b7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:18:56 +0000

    Bug 960351 - browser_bug726427_csstransform_tooltip.js times out, r=pbrosset
    UltraBlame original commit: 92f64142decc010babb6610ba528879b95370132

--- browser/devtools/styleinspector/test/browser_bug726427_csstransform_tooltip.js
+++ browser/devtools/styleinspector/test/browser_bug726427_csstransform_tooltip.js
@@ -147,7 +147,13 @@ function assertTooltipShownOn(tooltip, element, cb) {
     tooltip.panel.removeEventListener("popupshown", shown, true);
     cb();
   }, true);
-  tooltip._showOnHover(element);
+
+  
+  
+  
+  element.ownerDocument.defaultView.requestAnimationFrame(() => {
+      executeSoon(() => { tooltip._showOnHover(element); });
+    });
 }
 
 function assertTooltipNotShownOn(tooltip, element, cb) {
