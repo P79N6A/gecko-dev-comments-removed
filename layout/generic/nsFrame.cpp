41990cede942234cac52e6d4ed6623ac35c5298a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:12:18 +0000

    Bug 970363 - Fix selection moving over readonly elements in contenteditble blocks. r=roc
    UltraBlame original commit: f52f0588f43163eae13c98c76ff3059d92ff9628

--- layout/generic/nsFrame.cpp
+++ layout/generic/nsFrame.cpp
@@ -6271,6 +6271,7 @@ nsIFrame::PeekOffset(nsPeekOffsetStruct* aPos)
       bool eatingNonRenderableWS = false;
       nsIFrame::FrameSearchResult peekSearchState = CONTINUE;
       bool jumpedLine = false;
+      bool movedOverNonSelectableText = false;
       
       while (peekSearchState != FOUND) {
         bool movingInFrameDirection =
@@ -6281,6 +6282,8 @@ nsIFrame::PeekOffset(nsPeekOffsetStruct* aPos)
         else
           peekSearchState = current->PeekOffsetCharacter(movingInFrameDirection, &offset,
                                               aPos->mAmount == eSelectCluster);
+
+        movedOverNonSelectableText |= (peekSearchState == CONTINUE_UNSELECTABLE);
 
         if (peekSearchState != FOUND) {
           result =
@@ -6294,6 +6297,15 @@ nsIFrame::PeekOffset(nsPeekOffsetStruct* aPos)
           
           if (jumpedLine)
             eatingNonRenderableWS = true;
+        }
+
+        
+        
+        if (peekSearchState == FOUND && movedOverNonSelectableText)
+        {
+          int32_t start, end;
+          current->GetOffsets(start, end);
+          offset = aPos->mDirection == eDirNext ? 0 : end - start;
         }
       }
 
