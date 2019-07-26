6e6753f1b031066f11fef999e6beabba612967d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:12:22 +0000

    Bug 983204 - Ensure RTSP OMX reader's decoder is made active during loadmetadata. r=vchang
    UltraBlame original commit: d42a36615c7e4ff3225c44e37c99f3c65bb14184

--- content/media/omx/RtspOmxReader.h
+++ content/media/omx/RtspOmxReader.h
@@ -44,6 +44,9 @@ public:
     MOZ_COUNT_DTOR(RtspOmxReader);
   }
 
+  virtual nsresult ReadMetadata(MediaInfo* aInfo,
+                                MetadataTags** aTags) MOZ_OVERRIDE;
+
   
   virtual nsresult Seek(int64_t aTime, int64_t aStartTime, int64_t aEndTime,
                         int64_t aCurrentTime) MOZ_FINAL MOZ_OVERRIDE;
