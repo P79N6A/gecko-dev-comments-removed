45c7a8531d1d5ea1d9078d52483d8783109c8d73
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:59:43 +0000

    Bug 938108 - Wait for media state machine thread to shutdown during XPCOM shutdown before returning. r=roc
    Add a MediaShutdownManager and have that as the only xpcom-shutdown
    observer. This then shutsdown the MediaDecoders, and blocks waiting for
    the media state machine's shared thread to complete shutdown before
    exiting from the xpcom-shutdown observer. This ensures that the
    MediaDecoder infrastructure does not use XPCOM on any thread after XPCOM
    has shutdown, which is a logical error.
    
    UltraBlame original commit: 377f51d0e354b1f8e0ea77a770a48364b6db4259

---
+++ content/media/MediaShutdownManager.h
@@ -0,0 +1,168 @@
+
+
+
+
+
+
+#if !defined(MediaShutdownManager_h_)
+#define MediaShutdownManager_h_
+
+#include "nsIObserver.h"
+#include "mozilla/Monitor.h"
+#include "mozilla/RefPtr.h"
+#include "mozilla/StaticPtr.h"
+#include "nsIThread.h"
+#include "nsCOMPtr.h"
+#include "nsTHashtable.h"
+#include "nsHashKeys.h"
+
+namespace mozilla {
+
+class MediaDecoder;
+class StateMachineThread;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class MediaShutdownManager : public nsIObserver {
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIOBSERVER
+
+  
+  
+  static MediaShutdownManager& Instance();
+
+  
+  
+  void Register(MediaDecoder* aDecoder);
+
+  
+  
+  
+  void Unregister(MediaDecoder* aDecoder);
+
+  
+  
+  
+  
+  void Register(StateMachineThread* aThread);
+
+  
+  
+  
+  
+  void Unregister(StateMachineThread* aThread);
+
+private:
+
+  MediaShutdownManager();
+  virtual ~MediaShutdownManager();
+
+  void Shutdown();
+
+  
+  
+  void EnsureCorrectShutdownObserverState();
+
+  static StaticRefPtr<MediaShutdownManager> sInstance;
+
+  
+  
+  nsTHashtable<nsPtrHashKey<MediaDecoder>> mDecoders;
+
+  
+  
+  
+  
+  
+  nsTHashtable<nsRefPtrHashKey<StateMachineThread>> mStateMachineThreads;
+
+  
+  bool mIsObservingShutdown;
+
+  bool mIsDoingXPCOMShutDown;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class StateMachineThread {
+public:
+  StateMachineThread();
+  ~StateMachineThread();
+
+  NS_INLINE_DECL_REFCOUNTING(StateMachineThread);
+
+  
+  nsresult Init();
+
+  
+  
+  nsIThread* GetThread();
+
+  
+  
+  
+  
+  void Shutdown();
+
+  
+  
+  
+  void SpinUntilShutdownComplete();
+
+private:
+  void ShutdownThread();
+  nsCOMPtr<nsIThread> mThread;
+};
+
+} 
+
+#endif
