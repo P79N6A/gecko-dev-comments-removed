4b5aa9a02b1e4d897dcc8d7c83d2a3e70e7c83ac
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:23:25 +0000

    Bug 872762: Page title strip for the HomePager. [r=margaret]
    UltraBlame original commit: 0f895e8c3d7f123878fa31c8745eb2db5be22bea

---
+++ mobile/android/base/home/HomePagerTabStrip.java
@@ -0,0 +1,35 @@
+
+
+
+
+
+package org.mozilla.gecko.home;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.view.PagerTabStrip;
+import android.util.AttributeSet;
+
+import org.mozilla.gecko.R;
+
+
+
+
+
+
+public class HomePagerTabStrip extends PagerTabStrip {
+
+    public HomePagerTabStrip(Context context) {
+        super(context);
+    }
+
+    public HomePagerTabStrip(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.HomePagerTabStrip);
+        int color = a.getColor(R.styleable.HomePagerTabStrip_tabIndicatorColor, 0x00);
+        a.recycle();
+
+        setTabIndicatorColor(color);
+    }
+}
