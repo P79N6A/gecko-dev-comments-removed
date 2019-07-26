bc7c92f0711f65bfd434b21e491e5627525e96b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:38:54 +0000

    Bug 913558 - Improve GGC store buffer threadsafe assertions, r=terrence.
    UltraBlame original commit: 0979fdfd27176db326a7dd1afd9e41bc7ef1bc88

--- js/src/gc/StoreBuffer.h
+++ js/src/gc/StoreBuffer.h
@@ -120,7 +120,7 @@ class StoreBuffer
         
         void put(const T &t) {
             mozilla::ReentrancyGuard g(*this);
-            JS_ASSERT(!owner->inParallelSection());
+            JS_ASSERT(CurrentThreadCanAccessRuntime(owner->runtime));
 
             if (!enabled_)
                 return;
@@ -159,7 +159,7 @@ class StoreBuffer
 
         
         void unput(const T &v) {
-            JS_ASSERT(!this->owner->inParallelSection());
+            JS_ASSERT(CurrentThreadCanAccessRuntime(this->owner->runtime));
             MonoTypeBuffer<T>::put(v.tagged());
         }
     };
@@ -194,7 +194,7 @@ class StoreBuffer
         template <typename T>
         void put(const T &t) {
             mozilla::ReentrancyGuard g(*this);
-            JS_ASSERT(!owner->inParallelSection());
+            JS_ASSERT(CurrentThreadCanAccessRuntime(owner->runtime));
 
             
             (void)static_cast<const BufferableRef*>(&t);
