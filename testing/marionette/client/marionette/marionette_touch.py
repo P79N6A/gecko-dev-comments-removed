b5a08409b37dd24481b77821abbe11647cb7705b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:46:16 +0000

    Bug 833079 - python touch layer should check if element is displayed before acting on it, r=jgriffin
    UltraBlame original commit: 742adca7ab38de50380c80677f4b13fc03dc0d3e

--- testing/marionette/client/marionette/marionette_touch.py
+++ testing/marionette/client/marionette/marionette_touch.py
@@ -3,6 +3,7 @@
 
 
 import os
+from errors import ElementNotVisibleException
 
 """
 Adds touch support in Marionette
@@ -17,20 +18,28 @@ class MarionetteTouchMixin(object):
         self.library_name = library_name or "SyntheticGestures"
         self.import_script(self.library)
 
+    def check_element(self, element):
+        if not element.is_displayed():
+            raise ElementNotVisibleException
+
     def tap(self, element):
+        self.check_element(element)
         
         
         send_all = self.execute_script("return typeof window.wrappedJSObject.MouseEventShim === 'undefined';") 
         self.execute_script("%s.tap(arguments[0], null, null, null, null, arguments[1]);" % self.library_name, [element, send_all])
 
     def double_tap(self, element):
+        self.check_element(element)
         self.execute_script("%s.dbltap(arguments[0]);" % self.library_name, [element])
 
     def long_press(self, element, holdtime=2000):
+        self.check_element(element)
         
         self.execute_script("%s.hold.apply(this, arguments);" % self.library_name, [element, holdtime, 0, 0, 0, 0, 0])
         
     def flick(self, element, x1, y1, x2, y2, duration=200):
+        self.check_element(element)
         
         self.execute_script("%s.swipe.apply(this, arguments);" % self.library_name, [element, x1, y1, x2, y2, duration])
 
