75cac5f96195894a7801efa20191e0355948a97c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:15:05 +0000

    Bug 927863 - Back out 14207ffc2b2f (bug 911574) for breaking iframe scrolling. r=backout
    UltraBlame original commit: 16de756941cf46b8eb2bccd1d3e393b130832a71

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -4170,7 +4170,7 @@ var BrowserEventHandler = {
       if (this._scrollableElement != null) {
         
         let doc = BrowserApp.selectedBrowser.contentDocument;
-        if (this._scrollableElement != doc.documentElement)
+        if (this._scrollableElement != doc.body && this._scrollableElement != doc.documentElement)
           sendMessageToJava({ type: "Panning:Override" });
       }
     }
@@ -4257,6 +4257,7 @@ var BrowserEventHandler = {
 
           let doc = BrowserApp.selectedBrowser.contentDocument;
           if (this._scrollableElement == null ||
+              this._scrollableElement == doc.body ||
               this._scrollableElement == doc.documentElement) {
             sendMessageToJava({ type: "Panning:CancelOverride" });
             return;
@@ -4631,10 +4632,11 @@ var BrowserEventHandler = {
 
 
 
+
       if (checkElem) {
         if ((elem.scrollTopMax > 0 || elem.scrollLeftMax > 0) &&
             (this._hasScrollableOverflow(elem) ||
-             elem.mozMatchesSelector("html, textarea")) ||
+             elem.mozMatchesSelector("html, body, textarea")) ||
             (elem instanceof HTMLSelectElement && (elem.size > 1 || elem.multiple))) {
           scrollable = true;
           break;
