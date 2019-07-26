889fe7524cb792ba7e52293803bc3e7eb367de77
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:29:40 +0000

    Bug 901607 - Add front end sub-frame scroll support when apz is disabled using BrowserElementPanning for metrofx. r=mbrubeck
    UltraBlame original commit: b3bea36ce719131ece2ff0842256512ca8f12a3f

--- browser/metro/base/content/browser.js
+++ browser/metro/base/content/browser.js
@@ -68,9 +68,11 @@ var Browser = {
       messageManager.loadFrameScript("chrome://browser/content/contenthandlers/SelectionHandler.js", true);
       messageManager.loadFrameScript("chrome://browser/content/contenthandlers/ContextMenuHandler.js", true);
       messageManager.loadFrameScript("chrome://browser/content/contenthandlers/FindHandler.js", true);
-      
-      
       messageManager.loadFrameScript("chrome://browser/content/contenthandlers/ConsoleAPIObserver.js", true);
+      if (!Services.prefs.getBoolPref(kAsyncPanZoomEnabled)) {
+        messageManager.loadFrameScript("chrome://global/content/BrowserElementPanning.js", true);
+      }
+      
       
     } catch (e) {
       
