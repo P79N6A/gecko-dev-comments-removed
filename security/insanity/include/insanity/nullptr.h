4d53be58cc24623d3485afd5b210455b71395aa1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:40:10 +0000

    Bug 921888: Fix nullptr workaround to work with clang, r=cviecco
    UltraBlame original commit: 94dd775ce69818a690ae0563c3d5de5c941b638b

---
+++ security/insanity/include/insanity/nullptr.h
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef insanity_pkix__nullptr_h
+#define insanity_pkix__nullptr_h
+
+
+#if defined(__GNUC__) && !defined(__clang__)
+#if __GNUC__ * 100 + __GNUC_MINOR__ < 406
+#define nullptr __null
+#endif
+#endif
+
+#endif 
