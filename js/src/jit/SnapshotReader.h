bc2ec7f9e39ece0c8ed70f9a62f814aae7ac2f59
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:43:15 +0000

    Bug 962555 part 1 - Extract SnapshotReader::Slot. r=h4writer
    UltraBlame original commit: 2f8f6414bb62e5aa5f9eb0907e8bde5675dec1de

--- js/src/jit/SnapshotReader.h
+++ js/src/jit/SnapshotReader.h
@@ -11,6 +11,7 @@
 #include "jit/IonCode.h"
 #include "jit/IonTypes.h"
 #include "jit/Registers.h"
+#include "jit/Slot.h"
 
 namespace js {
 namespace jit {
@@ -49,162 +50,6 @@ class SnapshotReader
 
     void readSnapshotHeader();
     void readFrameHeader();
-
-  public:
-    enum SlotMode
-    {
-        CONSTANT,           
-        DOUBLE_REG,         
-        FLOAT32_REG,        
-        FLOAT32_STACK,      
-        TYPED_REG,          
-        TYPED_STACK,        
-        UNTYPED,            
-        JS_UNDEFINED,       
-        JS_NULL,            
-        JS_INT32            
-    };
-
-    class Location
-    {
-        friend class SnapshotReader;
-
-        
-        static const int32_t InvalidStackSlot = -1;
-
-        Register::Code reg_;
-        int32_t stackSlot_;
-
-        static Location From(const Register &reg) {
-            Location loc;
-            loc.reg_ = reg.code();
-            loc.stackSlot_ = InvalidStackSlot;
-            return loc;
-        }
-        static Location From(int32_t stackSlot) {
-            JS_ASSERT(stackSlot != InvalidStackSlot);
-            Location loc;
-            loc.reg_ = Register::Code(0);      
-            loc.stackSlot_ = stackSlot;
-            return loc;
-        }
-
-      public:
-        Register reg() const {
-            JS_ASSERT(!isStackSlot());
-            return Register::FromCode(reg_);
-        }
-        int32_t stackSlot() const {
-            JS_ASSERT(isStackSlot());
-            return stackSlot_;
-        }
-        bool isStackSlot() const {
-            return stackSlot_ != InvalidStackSlot;
-        }
-    };
-
-    class Slot
-    {
-        friend class SnapshotReader;
-
-        SlotMode mode_;
-
-        union {
-            FloatRegister::Code fpu_;
-            struct {
-                JSValueType type;
-                Location payload;
-            } known_type_;
-#if defined(JS_NUNBOX32)
-            struct {
-                Location type;
-                Location payload;
-            } unknown_type_;
-#elif defined(JS_PUNBOX64)
-            struct {
-                Location value;
-            } unknown_type_;
-#endif
-            int32_t value_;
-        };
-
-        Slot(SlotMode mode, JSValueType type, const Location &loc)
-          : mode_(mode)
-        {
-            known_type_.type = type;
-            known_type_.payload = loc;
-        }
-        Slot(const FloatRegister &reg)
-          : mode_(DOUBLE_REG)
-        {
-            fpu_ = reg.code();
-        }
-        Slot(SlotMode mode, const FloatRegister &reg)
-          : mode_(mode)
-        {
-            JS_ASSERT(mode == FLOAT32_REG);
-            fpu_ = reg.code();
-        }
-        Slot(SlotMode mode, const Location &loc)
-          : mode_(mode)
-        {
-            JS_ASSERT(mode == FLOAT32_STACK);
-            known_type_.payload = loc;
-        }
-        Slot(SlotMode mode)
-          : mode_(mode)
-        { }
-        Slot(SlotMode mode, uint32_t index)
-          : mode_(mode)
-        {
-            JS_ASSERT(mode == CONSTANT || mode == JS_INT32);
-            value_ = index;
-        }
-
-      public:
-        SlotMode mode() const {
-            return mode_;
-        }
-        uint32_t constantIndex() const {
-            JS_ASSERT(mode() == CONSTANT);
-            return value_;
-        }
-        int32_t int32Value() const {
-            JS_ASSERT(mode() == JS_INT32);
-            return value_;
-        }
-        JSValueType knownType() const {
-            JS_ASSERT(mode() == TYPED_REG || mode() == TYPED_STACK);
-            return known_type_.type;
-        }
-        Register reg() const {
-            JS_ASSERT(mode() == TYPED_REG && knownType() != JSVAL_TYPE_DOUBLE);
-            return known_type_.payload.reg();
-        }
-        FloatRegister floatReg() const {
-            JS_ASSERT(mode() == DOUBLE_REG || mode() == FLOAT32_REG);
-            return FloatRegister::FromCode(fpu_);
-        }
-        int32_t stackSlot() const {
-            JS_ASSERT(mode() == TYPED_STACK || mode() == FLOAT32_STACK);
-            return known_type_.payload.stackSlot();
-        }
-#if defined(JS_NUNBOX32)
-        Location payload() const {
-            JS_ASSERT(mode() == UNTYPED);
-            return unknown_type_.payload;
-        }
-        Location type() const {
-            JS_ASSERT(mode() == UNTYPED);
-            return unknown_type_.type;
-        }
-#elif defined(JS_PUNBOX64)
-        Location value() const {
-            JS_ASSERT(mode() == UNTYPED);
-            return unknown_type_.value;
-        }
-#endif
-    };
 
   public:
     SnapshotReader(const uint8_t *buffer, const uint8_t *end);
@@ -247,4 +92,4 @@ class SnapshotReader
 }
 }
 
-#endif
+#endif 
