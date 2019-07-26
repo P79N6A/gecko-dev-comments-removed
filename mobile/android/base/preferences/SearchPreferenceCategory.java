14b283a0b18d435afba73679ebc47be7eccfc721
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:14:13 +0000

    Bug 892113 - Dialog to manage installed search providers. r=liuche
    UltraBlame original commit: aa054b0f118b4d2764bb7e129bb5eb3b8a4c75ad

---
+++ mobile/android/base/preferences/SearchPreferenceCategory.java
@@ -0,0 +1,162 @@
+
+
+
+
+package org.mozilla.gecko.preferences;
+
+import android.content.Context;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.util.AttributeSet;
+import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mozilla.gecko.GeckoAppShell;
+import org.mozilla.gecko.GeckoEvent;
+import org.mozilla.gecko.util.GeckoEventListener;
+
+public class SearchPreferenceCategory extends PreferenceCategory implements GeckoEventListener {
+    public static final String LOGTAG = "SearchPrefCategory";
+
+    private SearchEnginePreference mDefaultEngineReference;
+
+    
+    
+
+    public SearchPreferenceCategory(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SearchPreferenceCategory(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SearchPreferenceCategory(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onAttachedToActivity() {
+        super.onAttachedToActivity();
+
+        
+        setOrderingAsAdded(false);
+
+        
+        GeckoAppShell.registerEventListener("SearchEngines:Data", this);
+        GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("SearchEngines:Get", null));
+    }
+
+    @Override
+    public void handleMessage(String event, final JSONObject data) {
+        if (event.equals("SearchEngines:Data")) {
+            
+            JSONArray engines;
+            JSONObject defaultEngine;
+            final String defaultEngineName;
+            try {
+                engines = data.getJSONArray("searchEngines");
+                if (engines.length() == 0) {
+                    return;
+                }
+                defaultEngine = engines.getJSONObject(0);
+                defaultEngineName = defaultEngine.getString("name");
+            } catch (JSONException e) {
+                Log.e(LOGTAG, "Unable to decode search engine data from Gecko.", e);
+                return;
+            }
+
+            
+            for (int i = 0; i < engines.length(); i++) {
+                try {
+                    JSONObject engineJSON = engines.getJSONObject(i);
+                    final String engineName = engineJSON.getString("name");
+
+                    SearchEnginePreference enginePreference = new SearchEnginePreference(getContext(), this);
+                    enginePreference.setSearchEngineFromJSON(engineJSON);
+                    if (engineName.equals(defaultEngineName)) {
+                        
+                        
+                        
+                        enginePreference.setIsDefaultEngine(true);
+                        mDefaultEngineReference = enginePreference;
+                    }
+
+                    enginePreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+                        @Override
+                        public boolean onPreferenceClick(Preference preference) {
+                            SearchEnginePreference sPref = (SearchEnginePreference) preference;
+                            
+                            sPref.showDialog();
+                            return true;
+                        }
+                    });
+
+                    addPreference(enginePreference);
+                } catch (JSONException e) {
+                    Log.e(LOGTAG, "JSONException parsing engine at index " + i, e);
+                }
+            }
+        }
+
+        
+        
+        GeckoAppShell.unregisterEventListener("SearchEngines:Data", this);
+    }
+
+    
+
+
+
+    private void setFallbackDefaultEngine() {
+        if (getPreferenceCount() > 0) {
+            SearchEnginePreference aEngine = (SearchEnginePreference) getPreference(0);
+            setDefault(aEngine);
+        }
+    }
+
+    
+
+
+
+
+    private void sendGeckoEngineEvent(String event, SearchEnginePreference engine) {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("engine", engine.getTitle());
+        } catch (JSONException e) {
+            Log.e(LOGTAG, "JSONException creating search engine configuration change message for Gecko.", e);
+            return;
+        }
+        GeckoAppShell.notifyGeckoOfEvent(GeckoEvent.createBroadcastEvent(event, json.toString()));
+    }
+
+    
+
+    
+
+
+
+    public void uninstall(SearchEnginePreference engine) {
+        removePreference(engine);
+        if (engine == mDefaultEngineReference) {
+            
+            setFallbackDefaultEngine();
+        }
+
+        sendGeckoEngineEvent("SearchEngines:Remove", engine);
+    }
+
+    
+
+
+
+    public void setDefault(SearchEnginePreference engine) {
+        engine.setIsDefaultEngine(true);
+        mDefaultEngineReference.setIsDefaultEngine(false);
+        mDefaultEngineReference = engine;
+
+        sendGeckoEngineEvent("SearchEngines:SetDefault", engine);
+    }
+}
