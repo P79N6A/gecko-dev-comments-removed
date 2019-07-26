989f1d1dcb62806ff5dc15fd4dac4f1e29706a2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:33:29 +0000

    Bug 899348 - Make reload of the about:tabcrashed work as expected. r=jaws
    UltraBlame original commit: 7d8523c3dc5740aa4e5119089e750d8eff41bfae

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -2341,6 +2341,9 @@ let BrowserOnClick = {
              ownerDoc.documentURI.toLowerCase() == "about:newtab") {
       this.onE10sAboutNewTab(aEvent, ownerDoc);
     }
+    else if (ownerDoc.documentURI.startsWith("about:tabcrashed")) {
+      this.onAboutTabCrashed(aEvent, ownerDoc);
+    }
   },
 
   onAboutCertError: function BrowserOnClick_onAboutCertError(aTargetElm, aOwnerDoc) {
@@ -2473,6 +2476,22 @@ let BrowserOnClick = {
     }
   },
 
+  
+
+
+
+  onAboutTabCrashed: function(aEvent, aOwnerDoc) {
+    let isTopFrame = (aOwnerDoc.defaultView.parent === aOwnerDoc.defaultView);
+    if (!isTopFrame) {
+      return;
+    }
+
+    let button = aEvent.originalTarget;
+    if (button.id == "tryAgain") {
+      openUILinkIn(button.getAttribute("url"), "current");
+    }
+  },
+
   ignoreWarningButton: function BrowserOnClick_ignoreWarningButton(aIsMalware) {
     
     
@@ -2582,6 +2601,16 @@ function getWebNavigation()
 }
 
 function BrowserReloadWithFlags(reloadFlags) {
+  let url = gBrowser.currentURI.spec;
+  if (gBrowser._updateBrowserRemoteness(gBrowser.selectedBrowser,
+                                        gBrowser._shouldBrowserBeRemote(url))) {
+    
+    
+    
+    gBrowser.loadURIWithFlags(url, reloadFlags);
+    return;
+  }
+
   
 
 
