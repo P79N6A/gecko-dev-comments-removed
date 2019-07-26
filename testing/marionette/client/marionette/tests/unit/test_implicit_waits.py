6a7ec0a46642de556d4986feed0947b13dae7956
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:15:35 +0000

    Bug 758195 - Add test for implicit waits. r=jgriffin
    UltraBlame original commit: a1bfe3e4d8070c9353c02c72b5ed90a7f99f7700

---
+++ testing/marionette/client/marionette/tests/unit/test_implicit_waits.py
@@ -0,0 +1,28 @@
+
+
+
+
+from marionette_test import MarionetteTestCase
+from errors import NoSuchElementException
+
+class TestImplicitWaits(MarionetteTestCase):
+    def testShouldImplicitlyWaitForASingleElement(self):
+        test_html = self.marionette.absolute_url("test_dynamic.html")
+        self.marionette.navigate(test_html)
+        add = self.marionette.find_element("id", "adder")
+        self.marionette.set_search_timeout("3000")
+        add.click()
+        
+        self.marionette.find_element("id", "box0")
+
+    def testShouldStillFailToFindAnElementWhenImplicitWaitsAreEnabled(self):
+        test_html = self.marionette.absolute_url("test_dynamic.html")
+        self.marionette.navigate(test_html)
+        self.marionette.set_search_timeout("3000")
+        try:
+            self.marionette.find_element("id", "box0")
+            self.fail("Should have thrown a a NoSuchElementException")
+        except NoSuchElementException:
+            pass
+        except Exception:
+            self.fail("Should have thrown a NoSuchElementException")
