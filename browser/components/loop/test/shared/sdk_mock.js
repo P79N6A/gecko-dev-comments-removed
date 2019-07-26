c940a24ea265665e1fd14964e5fca9dffbf30bf6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:16:58 +0000

    Bug 972941: Landing page for non WebRTC-compliant browsers on supported devices. r=nperriault
    UltraBlame original commit: d601d88a01a2e6af033213f0de9e7bec657ff9d3

---
+++ browser/components/loop/test/shared/sdk_mock.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+(function (window) {
+  "use strict";
+
+  if (!window.OT) {
+    window.OT = {};
+  }
+
+  window.OT.checkSystemRequirements = function() {
+    return true;
+  };
+
+})(window);
+
