4b34fccb5e65b81ac866bc7249a3b98cfbbf7494
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:57:38 +0000

    Bug 838343 - Convert HTMLFrameElement to WebIDL r=peterv
    UltraBlame original commit: abba7ec43f009b480e8d57c6e71da246c3d85ea4

--- content/html/content/src/HTMLFrameElement.cpp
+++ content/html/content/src/HTMLFrameElement.cpp
@@ -4,6 +4,7 @@
 
 
 #include "HTMLFrameElement.h"
+#include "mozilla/dom/HTMLFrameElementBinding.h"
 
 class nsIDOMDocument;
 
@@ -17,6 +18,7 @@ HTMLFrameElement::HTMLFrameElement(already_AddRefed<nsINodeInfo> aNodeInfo,
                                    FromParser aFromParser)
   : nsGenericHTMLFrameElement(aNodeInfo, aFromParser)
 {
+  SetIsDOMBinding();
 }
 
 HTMLFrameElement::~HTMLFrameElement()
@@ -112,6 +114,40 @@ nsMapRuleToAttributesFunc
 HTMLFrameElement::GetAttributeMappingFunction() const
 {
   return &MapAttributesIntoRule;
+}
+
+already_AddRefed<nsIDocument>
+HTMLFrameElement::GetContentDocument(ErrorResult& aRv)
+{
+  nsCOMPtr<nsIDOMDocument> doc;
+  nsresult rv = nsGenericHTMLFrameElement::GetContentDocument(getter_AddRefs(doc));
+  if (NS_FAILED(rv)) {
+    aRv.Throw(rv);
+    return nullptr;
+  }
+
+  nsCOMPtr<nsIDocument> ret = do_QueryInterface(doc);
+  return ret.forget();
+}
+
+already_AddRefed<nsIDOMWindow>
+HTMLFrameElement::GetContentWindow(ErrorResult& aRv)
+{
+  nsCOMPtr<nsIDOMWindow> win;
+  nsresult rv = nsGenericHTMLFrameElement::GetContentWindow(getter_AddRefs(win));
+  if (NS_FAILED(rv)) {
+    aRv.Throw(rv);
+    return nullptr;
+  }
+
+  return win.forget();
+}
+
+JSObject*
+HTMLFrameElement::WrapNode(JSContext* aCx, JSObject* aScope,
+                           bool* aTriedToWrap)
+{
+  return HTMLFrameElementBinding::Wrap(aCx, aScope, this, aTriedToWrap);
 }
 
 } 
--- content/html/content/src/HTMLFrameElement.h
+++ content/html/content/src/HTMLFrameElement.h
@@ -49,6 +49,66 @@ public:
   virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
   virtual nsXPCClassInfo* GetClassInfo();
   virtual nsIDOMNode* AsDOMNode() { return this; }
+
+  
+  
+  void SetFrameBorder(const nsAString& aFrameBorder, ErrorResult& aError)
+  {
+    SetHTMLAttr(nsGkAtoms::frameborder, aFrameBorder, aError);
+  }
+
+  
+  void SetLongDesc(const nsAString& aLongDesc, ErrorResult& aError)
+  {
+    SetAttrHelper(nsGkAtoms::longdesc, aLongDesc);
+  }
+
+  
+  void SetMarginHeight(const nsAString& aMarginHeight, ErrorResult& aError)
+  {
+    SetHTMLAttr(nsGkAtoms::marginheight, aMarginHeight, aError);
+  }
+
+  
+  void SetMarginWidth(const nsAString& aMarginWidth, ErrorResult& aError)
+  {
+    SetHTMLAttr(nsGkAtoms::marginwidth, aMarginWidth, aError);
+  }
+
+  
+  void SetName(const nsAString& aName, ErrorResult& aError)
+  {
+    SetHTMLAttr(nsGkAtoms::name, aName, aError);
+  }
+
+  bool NoResize() const
+  {
+   return GetBoolAttr(nsGkAtoms::noresize);
+  }
+  void SetNoResize(bool& aNoResize, ErrorResult& aError)
+  {
+    SetHTMLBoolAttr(nsGkAtoms::noresize, aNoResize, aError);
+  }
+
+  
+  void SetScrolling(const nsAString& aScrolling, ErrorResult& aError)
+  {
+    SetHTMLAttr(nsGkAtoms::scrolling, aScrolling, aError);
+  }
+
+  
+  void SetSrc(const nsAString& aSrc, ErrorResult& aError)
+  {
+    SetAttrHelper(nsGkAtoms::src, aSrc);
+  }
+
+  already_AddRefed<nsIDocument> GetContentDocument(ErrorResult& aRv);
+
+  already_AddRefed<nsIDOMWindow> GetContentWindow(ErrorResult& aRv);
+
+protected:
+  virtual JSObject* WrapNode(JSContext* aCx, JSObject* aScope,
+                             bool* aTriedToWrap) MOZ_OVERRIDE;
 };
 
 } 
