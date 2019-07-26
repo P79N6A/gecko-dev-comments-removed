bf716c98ab0c95ac02ef29cf810fb90d8114eb46
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:06:00 +0000

    Bug 791197 - [b2g-bluetooth] BluetoothHfpManager prototype, r=qdot
    UltraBlame original commit: 4dfa1754f818d123305f55c3e9fee6449a0294eb

---
+++ dom/bluetooth/BluetoothHfpManager.h
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_bluetooth_bluetoothhfpmanager_h__
+#define mozilla_dom_bluetooth_bluetoothhfpmanager_h__
+
+#include "BluetoothCommon.h"
+#include "mozilla/ipc/UnixSocket.h"
+
+BEGIN_BLUETOOTH_NAMESPACE
+
+class BluetoothReplyRunnable;
+
+class BluetoothHfpManager : public mozilla::ipc::UnixSocketConsumer
+{
+public:
+  ~BluetoothHfpManager();
+
+  static BluetoothHfpManager* Get();
+  void ReceiveSocketData(mozilla::ipc::UnixSocketRawData* aMessage);
+
+  bool Connect(const nsAString& aDeviceObjectPath,
+               BluetoothReplyRunnable* aRunnable);
+  void Disconnect();
+
+private:
+  BluetoothHfpManager();
+};
+
+END_BLUETOOTH_NAMESPACE
+
+#endif
