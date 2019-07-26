e2492aa167b0134d0affdedddcd0f6fd2597593f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 08:55:28 +0000

    Recover overflow of arguments from the stack. (Bug 754720, r=)
    UltraBlame original commit: 7223b430719826265fae65749380ae740f2beac0

--- js/src/ion/IonFrameIterator-inl.h
+++ js/src/ion/IonFrameIterator-inl.h
@@ -57,21 +57,39 @@ InlineFrameIterator::forEachCanonicalActualArg(Op op, unsigned start, unsigned c
     unsigned end = start + count;
     JS_ASSERT(start <= end && end <= nactual);
 
+    unsigned nformal = callee()->nargs;
+    unsigned formalEnd = end;
+    if (!more() && end > nformal)
+        formalEnd = nformal;
+    else
+        
+        
+        
+        
+        JS_ASSERT(end <= nformal);
+
     SnapshotIterator s(si_);
-    
+
     s.skip(); 
     s.skip(); 
 
     unsigned i = 0;
     for (; i < start; i++)
         s.skip();
-    for (; i < end; i++) {
+    for (; i < formalEnd; i++) {
         
         
         
         Value v = s.maybeRead();
         if (!op(i, &v))
             return false;
+    }
+    if (formalEnd != end) {
+        Value *argv = frame_->argv() + 1;
+        for (; i < end; i++) {
+            if (!op(i, &argv[i]))
+                return false;
+        }
     }
     return true;
 }
