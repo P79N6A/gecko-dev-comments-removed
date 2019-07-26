71fec8ec62cba90ed17daedf0204e1e5e7f9d9c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:41:27 +0000

    Bug 785156 - Lock icon should toggle site security popup. r=wesj
    UltraBlame original commit: 3869dc547459a665f9ed0c8f9c1c94bc6a5f6ef3

--- mobile/android/base/SiteIdentityPopup.java
+++ mobile/android/base/SiteIdentityPopup.java
@@ -70,6 +70,11 @@ public class SiteIdentityPopup extends PopupWindow {
     private void init() {
         setBackgroundDrawable(new BitmapDrawable());
         setOutsideTouchable(true);
+
+        
+        
+        setFocusable(true);
+
         setWindowLayoutMode(HardwareUtils.isTablet() ? LayoutParams.WRAP_CONTENT : LayoutParams.FILL_PARENT,
                 LayoutParams.WRAP_CONTENT);
 
