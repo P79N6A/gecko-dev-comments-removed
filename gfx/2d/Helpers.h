7974949a8fdc08dd013d28ef47c5f4f4cabf448f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:30:37 +0000

    Support 3D transforms in the basic compositor (bug 935265, r=mattwoodrow).
    UltraBlame original commit: 4213b23a0737e71dbcd424d2a65bbaf9527d8fdb

---
+++ gfx/2d/Helpers.h
@@ -0,0 +1,35 @@
+
+
+
+
+
+#ifndef MOZILLA_GFX_2D_HELPERS_H_
+#define MOZILLA_GFX_2D_HELPERS_H_
+
+#include "2D.h"
+
+namespace mozilla {
+namespace gfx {
+
+class AutoSaveTransform
+{
+ public:
+  AutoSaveTransform(DrawTarget *aTarget)
+   : mDrawTarget(aTarget),
+     mOldTransform(aTarget->GetTransform())
+  {
+  }
+  ~AutoSaveTransform()
+  {
+    mDrawTarget->SetTransform(mOldTransform);
+  }
+
+ private:
+  RefPtr<DrawTarget> mDrawTarget;
+  Matrix mOldTransform;
+};
+
+} 
+} 
+
+#endif 
