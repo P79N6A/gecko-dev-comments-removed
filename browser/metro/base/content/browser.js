615c1b6b8515395a2071df8550a3059bc7ebc177
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:05:58 +0000

    Bug 847620 - Tapping the browser should hide the Metro pop-up blocker notification [r=rsilveira]
    UltraBlame original commit: 1d65c8f296d986f08bffb6956067db7e4b2126fb

--- browser/metro/base/content/browser.js
+++ browser/metro/base/content/browser.js
@@ -70,6 +70,7 @@ var Browser = {
     ScrollwheelModule.init(Elements.browsers);
     GestureModule.init();
     BrowserTouchHandler.init();
+    PopupBlockerObserver.init();
 
     
     
@@ -1223,8 +1224,22 @@ nsBrowserAccess.prototype = {
 
 
 var PopupBlockerObserver = {
-  onUpdatePageReport: function onUpdatePageReport(aEvent)
-  {
+  init: function init() {
+    Elements.browsers.addEventListener("mousedown", this, true);
+  },
+
+  handleEvent: function handleEvent(aEvent) {
+    switch (aEvent.type) {
+      case "mousedown":
+        let box = Browser.getNotificationBox();
+        let notification = box.getNotificationWithValue("popup-blocked");
+        if (notification)
+          box.removeNotification(notification);
+        break;
+    }
+  },
+
+  onUpdatePageReport: function onUpdatePageReport(aEvent) {
     var cBrowser = Browser.selectedBrowser;
     if (aEvent.originalTarget != cBrowser)
       return;
