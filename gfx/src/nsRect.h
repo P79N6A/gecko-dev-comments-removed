1ed18a321ffddf2b27bec537deac76e15d311025
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:37:09 +0000

    Bug 913603 - Trim nsRect.h - r=jrmuizel
    UltraBlame original commit: d39afeecef6602ecb8704e82fd4661d1a8e9a594

--- gfx/src/nsRect.h
+++ gfx/src/nsRect.h
@@ -8,21 +8,19 @@
 #define NSRECT_H
 
 #include <stdio.h>                      
-#include <sys/types.h>                  
-#include <algorithm>                    
-#include <climits>                      
+#include <stdint.h>                     
 #include "gfxCore.h"                    
 #include "mozilla/Likely.h"             
 #include "mozilla/gfx/BaseRect.h"       
 #include "nsCoord.h"                    
-#include "nsDebug.h"                    
-#include "nsMargin.h"                   
 #include "nsPoint.h"                    
 #include "nsSize.h"                     
 #include "nsTraceRefcnt.h"              
 #include "nscore.h"                     
 
 struct nsIntRect;
+struct nsMargin;
+struct nsIntMargin;
 
 struct NS_GFX nsRect :
   public mozilla::gfx::BaseRect<nscoord, nsRect, nsPoint, nsSize, nsMargin> {
@@ -58,54 +56,7 @@ struct NS_GFX nsRect :
   
   
   
-  void SaturatingInflate(const nsMargin& aMargin)
-  {
-#ifdef NS_COORD_IS_FLOAT
-    Inflate(aMargin);
-#else
-    int64_t nx = int64_t(x) - aMargin.left;
-    int64_t w = int64_t(width) + int64_t(aMargin.left) + aMargin.right;
-    if (MOZ_UNLIKELY(w > nscoord_MAX)) {
-      NS_WARNING("Overflowed nscoord_MAX in conversion to nscoord width");
-      int64_t xdiff = nx - nscoord_MIN / 2;
-      if (xdiff < 0) {
-        
-        nx = nscoord_MIN / 2;
-        w += xdiff;
-      }
-      if (MOZ_UNLIKELY(w > nscoord_MAX)) {
-        w = nscoord_MAX;
-      }
-    }
-    width = nscoord(w);
-    if (MOZ_UNLIKELY(nx < nscoord_MIN)) {
-      NS_WARNING("Underflowed nscoord_MIN in conversion to nscoord x");
-      nx = nscoord_MIN;
-    }
-    x = nscoord(nx);
-
-    int64_t ny = int64_t(y) - aMargin.top;
-    int64_t h = int64_t(height) + int64_t(aMargin.top) + aMargin.bottom;
-    if (MOZ_UNLIKELY(h > nscoord_MAX)) {
-      NS_WARNING("Overflowed nscoord_MAX in conversion to nscoord height");
-      int64_t ydiff = ny - nscoord_MIN / 2;
-      if (ydiff < 0) {
-        
-        ny = nscoord_MIN / 2;
-        h += ydiff;
-      }
-      if (MOZ_UNLIKELY(h > nscoord_MAX)) {
-        h = nscoord_MAX;
-      }
-    }
-    height = nscoord(h);
-    if (MOZ_UNLIKELY(ny < nscoord_MIN)) {
-      NS_WARNING("Underflowed nscoord_MIN in conversion to nscoord y");
-      ny = nscoord_MIN;
-    }
-    y = nscoord(ny);
-#endif
-  }
+  void SaturatingInflate(const nsMargin& aMargin);
 
   
   
@@ -122,40 +73,7 @@ struct NS_GFX nsRect :
     }
   }
 
-  nsRect SaturatingUnionEdges(const nsRect& aRect) const
-  {
-#ifdef NS_COORD_IS_FLOAT
-    return UnionEdges(aRect);
-#else
-    nsRect result;
-    result.x = std::min(aRect.x, x);
-    int64_t w = std::max(int64_t(aRect.x) + aRect.width, int64_t(x) + width) - result.x;
-    if (MOZ_UNLIKELY(w > nscoord_MAX)) {
-      NS_WARNING("Overflowed nscoord_MAX in conversion to nscoord width");
-      
-      result.x = std::max(result.x, nscoord_MIN / 2);
-      w = std::max(int64_t(aRect.x) + aRect.width, int64_t(x) + width) - result.x;
-      if (MOZ_UNLIKELY(w > nscoord_MAX)) {
-        w = nscoord_MAX;
-      }
-    }
-    result.width = nscoord(w);
-
-    result.y = std::min(aRect.y, y);
-    int64_t h = std::max(int64_t(aRect.y) + aRect.height, int64_t(y) + height) - result.y;
-    if (MOZ_UNLIKELY(h > nscoord_MAX)) {
-      NS_WARNING("Overflowed nscoord_MAX in conversion to nscoord height");
-      
-      result.y = std::max(result.y, nscoord_MIN / 2);
-      h = std::max(int64_t(aRect.y) + aRect.height, int64_t(y) + height) - result.y;
-      if (MOZ_UNLIKELY(h > nscoord_MAX)) {
-        h = nscoord_MAX;
-      }
-    }
-    result.height = nscoord(h);
-    return result;
-#endif
-  }
+  nsRect SaturatingUnionEdges(const nsRect& aRect) const;
 
 #ifndef NS_COORD_IS_FLOAT
   
@@ -237,7 +155,7 @@ struct NS_GFX nsIntRect :
   
   
   static const nsIntRect& GetMaxSizedIntRect() {
-    static const nsIntRect r(0, 0, INT_MAX, INT_MAX);
+    static const nsIntRect r(0, 0, INT32_MAX, INT32_MAX);
     return r;
   }
 
