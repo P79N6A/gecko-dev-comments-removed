d0bb476fb22f8f1f013927b98d6049e9b8fd9dc7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:24:40 +0000

    Bug 911307 - Reflect changes to top sites immediately in about:newtab (part 2, front-end patch). r=ttaubert
    UltraBlame original commit: 621ab0e66a89c5c73a87c1efe5593d20c2d4aa85

---
+++ browser/base/content/test/newtab/browser_newtab_update.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+function runTests() {
+  if (NewTabUtils.allPages.updateScheduledForHiddenPages) {
+    
+    yield whenPagesUpdated(null, true);
+  }
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  setLinks([]);
+  whenPagesUpdated(null, true);
+  yield null;
+  yield null;
+
+  
+  fillHistory([link(1)]);
+  yield whenPagesUpdated(null, true);
+  yield addNewTabPageTab();
+  checkGrid("1,,,,,,,,");
+
+  fillHistory([link(2)]);
+  yield whenPagesUpdated(null, true);
+  yield addNewTabPageTab();
+  checkGrid("2,1,,,,,,,");
+
+  fillHistory([link(1)]);
+  yield whenPagesUpdated(null, true);
+  yield addNewTabPageTab();
+  checkGrid("1,2,,,,,,,");
+
+  
+  yield fillHistory([link(2), link(3), link(4)], TestRunner.next);
+  yield whenPagesUpdated(null, true);
+  yield addNewTabPageTab();
+  checkGrid("2,1,3,4,,,,,");
+}
+
+function link(id) {
+  return { url: "http://example.com/#" + id, title: "site#" + id };
+}
