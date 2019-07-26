af7f8e9becacb8121afd69de5a43cc80bac8672c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:09:23 +0000

    Bug 915328 - Metro front-end fixes for cross slide with async panning [r=sfoster]
    UltraBlame original commit: e46cfd222fbdfe943f567692521a4439ca5385b5

--- browser/metro/profile/metro.js
+++ browser/metro/profile/metro.js
@@ -43,7 +43,8 @@ pref("gfx.axis.fling_friction", "0.002");
 pref("gfx.axis.fling_stopped_threshold", "0.2");
 
 
-pref("apzc.axis_lock_mode", 1);
+pref("apzc.axis_lock_mode", 2);
+pref("apzc.cross_slide.enabled", true);
 
 
 pref("intl.enable_tsf_support", true);
