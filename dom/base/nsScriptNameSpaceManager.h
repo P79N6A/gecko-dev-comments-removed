3d1370b0ab4cceed991186bd092b499e546ba688
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:18:14 +0000

    Bug 807222.  Fix getOwnPropertyNames to work correctly on Window.  r=jst
    One note: it might be better to change the signature of Enumerate() to
    indicate whether we're enumerating just the enumerable properties or
    all of them, so we can skip doing this when someone does for...in on
    the window.
    
    UltraBlame original commit: 2682f9a1fc581a5209d870dde452bf87db6b378e

--- dom/base/nsScriptNameSpaceManager.h
+++ dom/base/nsScriptNameSpaceManager.h
@@ -143,6 +143,12 @@ public:
     mozilla::dom::DefineInterface aDefineDOMInterface,
     mozilla::dom::PrefEnabled aPrefEnabled);
 
+  typedef PLDHashOperator
+  (* GlobalNameEnumerator)(const nsAString& aGlobalName, void* aClosure);
+
+  void EnumerateGlobalNames(GlobalNameEnumerator aEnumerator,
+                            void* aClosure);
+
 private:
   
   
