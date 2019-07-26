cab7a2f8cadc6bc56378aeaf68574b336429dcc8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:25:28 +0000

    Bug 932419. Stop NS_ASSERT's from popping up a modal dialog in non-release builds, and fix aurora builds to be considered non-release builds for such purposes. r=dao
    UltraBlame original commit: a96aea73b2cf2d8954ef06db5748abf70517d9a0

--- toolkit/modules/debug.js
+++ toolkit/modules/debug.js
@@ -40,6 +40,7 @@ this.NS_ASSERT = function NS_ASSERT(condition, message) {
   try {
     switch (defB.getCharPref("app.update.channel")) {
       case "nightly":
+      case "aurora":
       case "beta":
       case "default":
         releaseBuild = false;
@@ -49,15 +50,14 @@ this.NS_ASSERT = function NS_ASSERT(condition, message) {
   var caller = arguments.callee.caller;
   var assertionText = "ASSERT: " + message + "\n";
 
+  
+  Components.utils.reportError(assertionText);
+
   if (releaseBuild) {
-    
-    Components.utils.reportError(assertionText);
     return;
   }
 
   
-  dump(assertionText);
-
   var stackText = "";
   if (gTraceOnAssert) {
     stackText = "Stack Trace: \n";
@@ -75,16 +75,5 @@ this.NS_ASSERT = function NS_ASSERT(condition, message) {
     }
   }
 
-  var environment = Components.classes["@mozilla.org/process/environment;1"].
-                    getService(Components.interfaces.nsIEnvironment);
-  if (environment.exists("XUL_ASSERT_PROMPT") &&
-      !parseInt(environment.get("XUL_ASSERT_PROMPT")))
-    return;
-
-  var source = null;
-  if (this.window)
-    source = this.window;
-  var ps = Components.classes["@mozilla.org/embedcomp/prompt-service;1"].
-           getService(Components.interfaces.nsIPromptService);
-  ps.alert(source, "Assertion Failed", assertionText + stackText);
+  dump(assertionText + stackText);
 }
