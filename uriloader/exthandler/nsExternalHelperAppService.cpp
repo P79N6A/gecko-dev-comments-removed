3ec26353d2da8bf5ca326f33742e8434a6848ecd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:06:02 +0000

    Use the new ReplaceChars with char16_t array. Bug 979556, r=bsmedberg
    UltraBlame original commit: d3005bbf540f9602da24793c83af0d779615cf4f

--- uriloader/exthandler/nsExternalHelperAppService.cpp
+++ uriloader/exthandler/nsExternalHelperAppService.cpp
@@ -1254,12 +1254,11 @@ nsExternalAppHandler::nsExternalAppHandler(nsIMIMEInfo * aMIMEInfo,
     char16_t(0x2066), 
     char16_t(0x2067), 
     char16_t(0x2068), 
-    char16_t(0x2069)  
+    char16_t(0x2069), 
+    char16_t(0)
   };
-  for (uint32_t i = 0; i < ArrayLength(unsafeBidiCharacters); ++i) {
-    mSuggestedFileName.ReplaceChar(unsafeBidiCharacters[i], '_');
-    mTempFileExtension.ReplaceChar(unsafeBidiCharacters[i], '_');
-  }
+  mSuggestedFileName.ReplaceChar(unsafeBidiCharacters, '_');
+  mTempFileExtension.ReplaceChar(unsafeBidiCharacters, '_');
 
   
   EnsureSuggestedFileName();
