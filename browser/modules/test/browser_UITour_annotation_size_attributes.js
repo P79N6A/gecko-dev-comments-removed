4da87e0fd34b04743ee250b2d82f4a28bf22f9cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:07:59 +0000

    Bug 970788 - UITour: Remove the incorrect size attributes on annotation panels on Linux to workaround Ubuntu glitches. r=Gijs
    UltraBlame original commit: a48993c2af06542da3ca0e31d5401b7a8b4b9c37

---
+++ browser/modules/test/browser_UITour_annotation_size_attributes.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+"use strict";
+
+let gTestTab;
+let gContentAPI;
+let gContentWindow;
+let highlight = document.getElementById("UITourHighlightContainer");
+let tooltip = document.getElementById("UITourTooltip");
+
+Components.utils.import("resource:///modules/UITour.jsm");
+
+function test() {
+  UITourTest();
+}
+
+let tests = [
+  function test_highlight_size_attributes(done) {
+    gContentAPI.showHighlight("appMenu");
+    waitForElementToBeVisible(highlight, function moveTheHighlight() {
+      gContentAPI.showHighlight("urlbar");
+      waitForElementToBeVisible(highlight, function checkPanelAttributes() {
+        SimpleTest.executeSoon(() => {
+          ise(highlight.height, "", "Highlight panel should have no explicit height set");
+          ise(highlight.width, "", "Highlight panel should have no explicit width set");
+          done();
+        });
+      }, "Highlight should be moved to the urlbar");
+    }, "Highlight should be shown after showHighlight() for the appMenu");
+  },
+
+  function test_info_size_attributes(done) {
+    gContentAPI.showInfo("appMenu", "test title", "test text");
+    waitForElementToBeVisible(tooltip, function moveTheTooltip() {
+      gContentAPI.showInfo("urlbar", "new title", "new text");
+      waitForElementToBeVisible(tooltip, function checkPanelAttributes() {
+        SimpleTest.executeSoon(() => {
+          ise(tooltip.height, "", "Info panel should have no explicit height set");
+          ise(tooltip.width, "", "Info panel should have no explicit width set");
+          done();
+        });
+      }, "Tooltip should be moved to the urlbar");
+    }, "Tooltip should be shown after showInfo() for the appMenu");
+  },
+
+];
