9e93975f196d446372810bd9619e48e9e7c56dfa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:52:50 +0000

    Bug 837212: convert browser_TelemetryTimestamps.js to an xpcshell test r=Felipe
    UltraBlame original commit: c1a747d02e87265dc9b57b2d005fb5338bcaeba3

---
+++ toolkit/modules/tests/xpcshell/test_TelemetryTimestamps.js
@@ -0,0 +1,72 @@
+
+
+
+const Cu = Components.utils;
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+Cu.import("resource://gre/modules/Services.jsm");
+
+
+
+
+Cu.import("resource://testing-common/AppInfo.jsm");
+updateAppInfo();
+
+function getSimpleMeasurementsFromTelemetryPing() {
+  const TelemetryPing = Cc["@mozilla.org/base/telemetry-ping;1"].getService(Ci.nsITelemetryPing);
+  let ping = TelemetryPing.getPayload();
+
+  return ping.simpleMeasurements;
+}
+
+function run_test() {
+  do_test_pending();
+  const Telemetry = Services.telemetry;
+  Telemetry.asyncFetchTelemetryData(function () {
+    try {
+      actualTest();
+    }
+    catch(e) {
+      do_throw("Failed: " + e);
+    }
+    do_test_finished();
+  });
+}
+
+function actualTest() {
+  
+  let tmp = {};
+  Cu.import("resource:///modules/TelemetryTimestamps.jsm", tmp);
+  let TelemetryTimestamps = tmp.TelemetryTimestamps;
+  let now = Date.now();
+  TelemetryTimestamps.add("foo");
+  do_check_true(TelemetryTimestamps.get().foo != null); 
+  do_check_true(TelemetryTimestamps.get().foo >= now); 
+
+  
+  
+  
+  const YEAR_4000_IN_MS = 64060588800000;
+  TelemetryTimestamps.add("bar", YEAR_4000_IN_MS);
+  do_check_eq(TelemetryTimestamps.get().bar, YEAR_4000_IN_MS); 
+
+  
+  TelemetryTimestamps.add("bar", 2);
+  do_check_eq(TelemetryTimestamps.get().bar, YEAR_4000_IN_MS); 
+
+  let threw = false;
+  try {
+    TelemetryTimestamps.add("baz", "this isn't a number");
+  } catch (ex) {
+    threw = true;
+  }
+  do_check_true(threw); 
+  do_check_null(TelemetryTimestamps.get().baz); 
+
+  
+  let simpleMeasurements = getSimpleMeasurementsFromTelemetryPing();
+  do_check_true(simpleMeasurements != null); 
+  do_check_true(simpleMeasurements.foo > 1); 
+  do_check_true(simpleMeasurements.bar > 1); 
+  do_check_null(simpleMeasurements.baz); 
+}
