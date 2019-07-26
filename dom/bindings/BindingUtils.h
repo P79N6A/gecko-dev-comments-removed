b7dc0a8c22612c1f1ebfd7a42829e6362a1c4005
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:24:17 +0000

    Bug 958576 part 2.  Move FakeDependentString to the binding_detail namespace.  r=peterv
    I took the opportunity to move away from the NonNull<nsAString> hack
    we had for string arguments, since just passing in a
    FakeDependentString works fine and callees are now less likely to
    declare their arg as being of that type.
    
    In the process of doing that, I ran into what looks like a substantive
    bug in the "owning union with string default value" case: We were
    doing mValue.mString.Value() without ever having constructed
    mValue.mString!
    
    UltraBlame original commit: 1ee233ff37f80df3d2820eccf728bc5e8ce60422

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -1456,6 +1456,8 @@ AppendNamedPropertyIds(JSContext* cx, JS::Handle<JSObject*> proxy,
                        nsTArray<nsString>& names,
                        bool shadowPrototypeProperties, JS::AutoIdVector& props);
 
+namespace binding_detail {
+
 
 
 struct FakeDependentString {
@@ -1533,6 +1535,8 @@ private:
   };
 };
 
+} 
+
 enum StringificationBehavior {
   eStringify,
   eEmpty,
@@ -1545,7 +1549,7 @@ ConvertJSValueToString(JSContext* cx, JS::Handle<JS::Value> v,
                        JS::MutableHandle<JS::Value> pval,
                        StringificationBehavior nullBehavior,
                        StringificationBehavior undefinedBehavior,
-                       FakeDependentString& result)
+                       binding_detail::FakeDependentString& result)
 {
   JSString *s;
   if (v.isString()) {
@@ -2044,6 +2048,39 @@ T& NonNullHelper(OwningNonNull<T>& aArg)
 
 template<typename T>
 const T& NonNullHelper(const OwningNonNull<T>& aArg)
+{
+  return aArg;
+}
+
+inline
+void NonNullHelper(NonNull<binding_detail::FakeDependentString>& aArg)
+{
+  
+  
+  
+  
+}
+
+inline
+void NonNullHelper(const NonNull<binding_detail::FakeDependentString>& aArg)
+{
+  
+  
+  
+  
+}
+
+inline
+void NonNullHelper(binding_detail::FakeDependentString& aArg)
+{
+  
+  
+  
+  
+}
+
+MOZ_ALWAYS_INLINE
+const nsAString& NonNullHelper(const binding_detail::FakeDependentString& aArg)
 {
   return aArg;
 }
