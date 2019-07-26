ddbe2755adde2fe2fb3c4b079f422c5a4e8c775c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:32:12 +0000

    Backout 3d411a8554e0 (bug 801466) for build failures
    UltraBlame original commit: b104b77e8b257f679e75a74d1d1aff039e09b150

--- netwerk/base/src/nsSerializationHelper.cpp
+++ netwerk/base/src/nsSerializationHelper.cpp
@@ -4,8 +4,9 @@
 
 #include "nsSerializationHelper.h"
 
+#include "plbase64.h"
+#include "prmem.h"
 
-#include "mozilla/Base64.h"
 #include "nsISerializable.h"
 #include "nsIObjectOutputStream.h"
 #include "nsIObjectInputStream.h"
@@ -14,8 +15,6 @@
 #include "nsAutoPtr.h"
 #include "nsComponentManagerUtils.h"
 #include "nsStringStream.h"
-
-using namespace mozilla;
 
 nsresult
 NS_SerializeToString(nsISerializable* obj, nsCSubstring& str)
@@ -39,12 +38,30 @@ NS_SerializeToString(nsISerializable* obj, nsCSubstring& str)
 nsresult
 NS_DeserializeObject(const nsCSubstring& str, nsISupports** obj)
 {
-  nsCString decodedData;
-  nsresult rv = Base64Decode(str, decodedData);
-  NS_ENSURE_SUCCESS(rv, rv);
+  
+  
+  
+  
+  
+  
+  
 
+  uint32_t size = str.Length();
+  if (size > 0 && str[size-1] == '=') {
+    if (size > 1 && str[size-2] == '=') {
+      size -= 2;
+    } else {
+      size -= 1;
+    }
+  }
+  size = (size * 3) / 4;
+  char* buf = PL_Base64Decode(str.BeginReading(), str.Length(), nullptr);
+  if (!buf)
+    return NS_ERROR_OUT_OF_MEMORY;
   nsCOMPtr<nsIInputStream> stream;
-  rv = NS_NewCStringInputStream(getter_AddRefs(stream), decodedData);
+  nsresult rv = NS_NewCStringInputStream(getter_AddRefs(stream),
+                                         Substring(buf, size));
+  PR_Free(buf);
   NS_ENSURE_SUCCESS(rv, rv);
 
   nsCOMPtr<nsIObjectInputStream> objstream =
