8e9c950bfabe84e8c96f7fee11d6992f9ea7cc1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:17:19 +0000

    Bug 861748 - Disable synchronous scroll to top or bottom of the page when page bounce behavior is turned off. r=smichaud
    UltraBlame original commit: c9662bed94d47bd80879de2b2c9acd203c3f40b0

--- browser/base/content/browser-gestureSupport.js
+++ browser/base/content/browser-gestureSupport.js
@@ -192,15 +192,17 @@ let gGestureSupport = {
                                           aEvent.DIRECTION_LEFT;
 
     let isVerticalSwipe = false;
-    if (aEvent.direction == aEvent.DIRECTION_UP) {
-      isVerticalSwipe = true;
-      
-      content.scrollTo(content.scrollX, 0);
-    }
-    else if (aEvent.direction == aEvent.DIRECTION_DOWN) {
-      isVerticalSwipe = true;
-      
-      content.scrollTo(content.scrollX, content.scrollMaxY);
+    if (gHistorySwipeAnimation.active) {
+      if (aEvent.direction == aEvent.DIRECTION_UP) {
+        isVerticalSwipe = true;
+        
+        content.scrollTo(content.scrollX, 0);
+      }
+      else if (aEvent.direction == aEvent.DIRECTION_DOWN) {
+        isVerticalSwipe = true;
+        
+        content.scrollTo(content.scrollX, content.scrollMaxY);
+      }
     }
 
     gHistorySwipeAnimation.startAnimation(isVerticalSwipe);
