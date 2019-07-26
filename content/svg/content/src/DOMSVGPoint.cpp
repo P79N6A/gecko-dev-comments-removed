fc028e8998811ea2806c53618e864a2e4e0c0612
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:08:05 +0000

    Merge from mozilla-central.
    UltraBlame original commit: 7c187e490f4ded18e829a33e0cb3bfe735a37e85

--- content/svg/content/src/DOMSVGPoint.cpp
+++ content/svg/content/src/DOMSVGPoint.cpp
@@ -6,10 +6,12 @@
 #include "DOMSVGPoint.h"
 #include "DOMSVGPointList.h"
 #include "SVGPoint.h"
+#include "SVGAnimatedPointList.h"
 #include "nsSVGElement.h"
 #include "nsError.h"
 #include "nsContentUtils.h" 
 #include "mozilla/dom/SVGMatrix.h"
+#include "mozilla/dom/SVGPointBinding.h"
 
 
 
