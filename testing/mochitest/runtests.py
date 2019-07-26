5356b39302a63546f6899f1911990fe1d3799be2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:30:00 +0000

    Bug 908874 - Add mach command to run b2g mochitests, r=jgriffin,gps, DONTBUILD, a=NPOTB
    UltraBlame original commit: 2cc63b8363a52119c6293eda099864f35909a9bd

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -390,7 +390,7 @@ toolbar#nav-bar {
       else:
         
         chrometestDir = os.path.abspath(".") + "/"
-        if self.automation.IS_WIN32:
+        if mozinfo.isWin:
           chrometestDir = "file:///" + chrometestDir.replace("\\", "/")
         manifestFile.write("content mochitests %s contentaccessible=yes\n" % chrometestDir)
 
