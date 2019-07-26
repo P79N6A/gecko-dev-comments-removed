d97865239b7ad6aad963ce8c9f9d80a6155e3633
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:55:33 +0000

    Bug 840614.  Fix the browserelement auth tests to not throw exceptions and actually test what they mean to test.  r=jdm
    UltraBlame original commit: a9cfb608a6332b3c23e3d03aeb40e1559a3626ad

--- dom/browser-element/mochitest/browserElement_Auth.js
+++ dom/browser-element/mochitest/browserElement_Auth.js
@@ -135,11 +135,19 @@ function testAuthJarInterfere(e) {
 
   
   
-  iframe.addEventListener("mozbrowserusernameandpasswordrequired", testFinish);
+  var gotusernamepasswordrequired = false;
+  function onUserNameAndPasswordRequired() {
+      gotusernamepasswordrequired = true;
+  }
+  iframe.addEventListener("mozbrowserusernameandpasswordrequired",
+                          onUserNameAndPasswordRequired);
   iframe.addEventListener("mozbrowsertitlechange", function onTitleChange(e) {
     iframe.removeEventListener("mozbrowsertitlechange", onTitleChange);
-    iframe.removeEventListener("mozbrowserusernameandpasswordrequired", testFinish);
-    SimpleTest.execute(testFail);
+    iframe.removeEventListener("mozbrowserusernameandpasswordrequired",
+                               onUserNameAndPasswordRequired);
+    ok(gotusernamepasswordrequired,
+       "Should have dispatched mozbrowserusernameandpasswordrequired event");
+    testFinish();
   });
 
   
@@ -148,8 +156,6 @@ function testAuthJarInterfere(e) {
 }
 
 function testFinish() {
-  iframe.removeEventListener("mozbrowserusernameandpasswordrequired", testFinish);
-
   
   var authMgr = SpecialPowers.Cc['@mozilla.org/network/http-auth-manager;1']
     .getService(SpecialPowers.Ci.nsIHttpAuthManager);
