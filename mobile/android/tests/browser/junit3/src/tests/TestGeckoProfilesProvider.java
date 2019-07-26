baf2aa5e9cdae7e1401e2585fd06c1db42df703e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:16:46 +0000

    Bug 707123 - Part 2: content provider for Fennec profiles. r=nalexander
    UltraBlame original commit: 6f60d60ea58d32abc59dc4041c0bd796bb9ca091

---
+++ mobile/android/tests/browser/junit3/src/tests/TestGeckoProfilesProvider.java
@@ -0,0 +1,50 @@
+
+
+
+
+package org.mozilla.gecko.browser.tests;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mozilla.gecko.db.BrowserContract;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.RemoteException;
+
+public class TestGeckoProfilesProvider extends BrowserTestCase {
+    private static final String[] NAME_AND_PATH = new String[] { BrowserContract.Profiles.NAME, BrowserContract.Profiles.PATH };
+
+    
+
+
+    public void testQueryDefault() throws RemoteException {
+        final ContentResolver contentResolver = getActivity().getContentResolver();
+        final Uri uri = BrowserContract.PROFILES_AUTHORITY_URI.buildUpon().appendPath("profiles").build();
+        final Cursor c = contentResolver.query(uri, NAME_AND_PATH, null, null, null);
+        assertNotNull(c);
+        try {
+            assertTrue(c.moveToFirst());
+            assertTrue(c.getCount() > 0);
+            Map<String, String> profiles = new HashMap<String, String>();
+            while (!c.isAfterLast()) {
+                final String name = c.getString(0);
+                final String path = c.getString(1);
+                profiles.put(name, path);
+                c.moveToNext();
+            }
+
+            assertTrue(profiles.containsKey("default"));
+            final String path = profiles.get("default");
+            assertTrue(path.endsWith(".default"));          
+            assertTrue(path.startsWith("/data/"));          
+            assertTrue(path.contains("/mozilla/"));         
+            assertTrue(new File(path).exists());
+        } finally {
+            c.close();
+        }
+    }
+}
