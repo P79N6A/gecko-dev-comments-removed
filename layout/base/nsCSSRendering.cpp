8f422d82daba15518a4dabcdc23c276f04ab9e13
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:56:14 +0000

    Bug 827227 - Fixed int overflow when scaling border-image-width. r=dbaron
    UltraBlame original commit: 4eed73abf01c76a6546d43f39e81f443d2e56913

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -3113,16 +3113,21 @@ DrawBorderImage(nsPresContext*       aPresContext,
         break;
     }
     border.Side(s) = NS_lround(value);
+    MOZ_ASSERT(border.Side(s) >= 0);
   }
 
   
   
   
-  double scaleX = border.left + border.right > borderImgArea.width
-                  ? borderImgArea.width / double(border.left + border.right)
+  uint32_t combinedBorderWidth = uint32_t(border.left) +
+                                 uint32_t(border.right);
+  double scaleX = combinedBorderWidth > uint32_t(borderImgArea.width)
+                  ? borderImgArea.width / double(combinedBorderWidth)
                   : 1.0;
-  double scaleY = border.top + border.bottom > borderImgArea.height
-                  ? borderImgArea.height / double(border.top + border.bottom)
+  uint32_t combinedBorderHeight = uint32_t(border.top) +
+                                  uint32_t(border.bottom);
+  double scaleY = combinedBorderHeight > uint32_t(borderImgArea.height)
+                  ? borderImgArea.height / double(combinedBorderHeight)
                   : 1.0;
   double scale = std::min(scaleX, scaleY);
   if (scale < 1.0) {
