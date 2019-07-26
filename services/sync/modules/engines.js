366e746aab0a7a67ec941ee1fe71f5e2a3596ab2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:32:09 +0000

    Backed out changeset a76b3c746743 (bug 960887) for xpcshell bustage
    UltraBlame original commit: bf49e44289068c200029888e2f246a0f9732329f

--- services/sync/modules/engines.js
+++ services/sync/modules/engines.js
@@ -651,7 +651,8 @@ SyncEngine.prototype = {
   
   applyIncomingBatchSize: DEFAULT_STORE_BATCH_SIZE,
 
-  get storageURL() this.service.storageURL,
+  get storageURL() Svc.Prefs.get("clusterURL") + SYNC_API_VERSION +
+    "/" + this.service.identity.username + "/storage/",
 
   get engineURL() this.storageURL + this.name,
 
