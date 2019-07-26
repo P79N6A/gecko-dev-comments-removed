b69104270c3dba1c2b387a068cd777f2366ccf24
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:03:57 +0000

    Bug 787519 - Test case r=jlebar
    UltraBlame original commit: 909bd3f7c66a36c3f90de68463170390f839ac41

---
+++ dom/browser-element/mochitest/browserElement_DOMRequestError.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+"use strict";
+
+SimpleTest.waitForExplicitFinish();
+
+function runTest() {
+  browserElementTestHelpers.setEnabledPref(true);
+  browserElementTestHelpers.addPermission();
+
+  var iframe1 = document.createElement('iframe');
+  iframe1.mozbrowser = true;
+  iframe1.src = 'data:text/html,<html>' +
+    '<body style="background:green">hello</body></html>';
+  document.body.appendChild(iframe1);
+
+  function testIframe(beforeRun, isErrorExpected, nextTest) {
+    return function() {
+      var error = false;
+      if (beforeRun)
+        beforeRun();
+      function testEnd() {
+        is(isErrorExpected, error);
+        SimpleTest.executeSoon(nextTest);
+      }
+
+      var domRequest = iframe1.getScreenshot();
+      domRequest.onsuccess = function(e) {
+        testEnd();
+      }
+      domRequest.onerror = function(e) {
+        error = true;
+        testEnd();
+      }
+    };
+  }
+
+  function iframeLoadedHandler() {
+    iframe1.removeEventListener('mozbrowserloadend', iframeLoadedHandler);
+    
+    
+    
+    var test3 = testIframe(
+      function() {
+        document.body.appendChild(iframe1);
+      }, false,
+      function() {
+        SimpleTest.finish();
+      })
+    ;
+    var test2 = testIframe(function() {
+      document.body.removeChild(iframe1);
+    }, true, test3);
+    var test1 = testIframe(null, false, test2);
+    SimpleTest.executeSoon(test1);
+  }
+
+  iframe1.addEventListener('mozbrowserloadend', iframeLoadedHandler);
+}
+
+addEventListener('load', function() { SimpleTest.executeSoon(runTest); });
