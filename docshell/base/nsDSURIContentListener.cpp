dac806060ede56e8ed61b7df2930ea0cb651ae14
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:15:05 +0000

    Bug 836132 - update x-frame-options ALLOW-FROM parsing to conform to IETF internet draft. (r=bz,geekboy)
    UltraBlame original commit: e6a95781f51c1445ab5f953559abb9c4e5f66c4f

--- docshell/base/nsDSURIContentListener.cpp
+++ docshell/base/nsDSURIContentListener.cpp
@@ -262,7 +262,7 @@ nsDSURIContentListener::SetParentContentListener(nsIURIContentListener*
 
 bool nsDSURIContentListener::CheckOneFrameOptionsPolicy(nsIRequest *request,
                                                         const nsAString& policy) {
-    static const char allowFrom[] = "allow-from ";
+    static const char allowFrom[] = "allow-from";
     const uint32_t allowFromLen = ArrayLength(allowFrom) - 1;
     bool isAllowFrom =
         StringHead(policy, allowFromLen).LowerCaseEqualsLiteral(allowFrom);
@@ -376,6 +376,12 @@ bool nsDSURIContentListener::CheckOneFrameOptionsPolicy(nsIRequest *request,
     
     
     if (isAllowFrom) {
+        if (policy.Length() == allowFromLen ||
+            (policy[allowFromLen] != ' ' &&
+             policy[allowFromLen] != '\t')) {
+            ReportXFOViolation(curDocShellItem, uri, eALLOWFROM);
+            return false;
+        }
         rv = NS_NewURI(getter_AddRefs(uri),
                        Substring(policy, allowFromLen));
         if (NS_FAILED(rv))
