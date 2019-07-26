90e5772f420ef1a2c55c6895a6d31f66398b0ded
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:44:13 +0000

    Bug 936940 - Completely disable text zoom in metrofx r=jimm
    UltraBlame original commit: 182650a21d4b2a960111d9de5d5664e177142390

--- browser/metro/profile/metro.js
+++ browser/metro/profile/metro.js
@@ -92,9 +92,9 @@ pref("toolkit.telemetry.prompted", 2);
 pref("toolkit.screen.lock", false);
 
 
-pref("zoom.minPercent", 20);
-pref("zoom.maxPercent", 400);
-pref("toolkit.zoomManager.zoomValues", ".2,.3,.5,.67,.8,.9,1,1.1,1.2,1.33,1.5,1.7,2,2.4,3,4");
+pref("zoom.minPercent", 100);
+pref("zoom.maxPercent", 100);
+pref("toolkit.zoomManager.zoomValues", "1");
 
 
 pref("browser.viewport.scaleRatio", -1);
