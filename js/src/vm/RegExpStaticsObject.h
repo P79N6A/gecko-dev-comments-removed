cd9b9c5841d4da1b864f0bf400a902999cc0428f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:54:13 +0000

    Bug 884124 (part 8) - Add a RegExpStaticObject class.  r=jwalden.
    UltraBlame original commit: 6a4a979ca127e053d5e25049137fa4418aa1cce1

---
+++ js/src/vm/RegExpStaticsObject.h
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+#ifndef vm_RegExpStaticsObject_h
+#define vm_RegExpStaticsObject_h
+
+#include "jsobj.h"
+
+namespace js {
+
+class RegExpStaticsObject : public JSObject
+{
+  public:
+    static Class class_;
+};
+
+} 
+
+#endif 
