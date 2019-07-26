4f14d8265783fa897aaaa00f2aa2f2016eebecaf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:29:51 +0000

    Bug 870676: Part 1 - Implement mozFMRadio in WebIDL. r=khuey,baku
    UltraBlame original commit: cd99d10fc6655f4ecd27ef1673f16a38484a09de

---
+++ dom/fmradio/FMRadio.h
@@ -0,0 +1,92 @@
+
+
+
+
+
+#ifndef mozilla_dom_FMRadio_h
+#define mozilla_dom_FMRadio_h
+
+#include "FMRadioCommon.h"
+#include "nsDOMEventTargetHelper.h"
+#include "nsCycleCollectionParticipant.h"
+#include "mozilla/HalTypes.h"
+#include "nsWeakReference.h"
+
+class nsPIDOMWindow;
+class nsIScriptContext;
+
+BEGIN_FMRADIO_NAMESPACE
+
+class DOMRequest;
+
+class FMRadio MOZ_FINAL : public nsDOMEventTargetHelper
+                        , public hal::SwitchObserver
+                        , public FMRadioEventObserver
+                        , public nsSupportsWeakReference
+{
+  friend class FMRadioRequest;
+
+public:
+  FMRadio();
+
+  NS_DECL_ISUPPORTS_INHERITED
+
+  NS_REALLY_FORWARD_NSIDOMEVENTTARGET(nsDOMEventTargetHelper)
+
+  void Init(nsPIDOMWindow *aWindow);
+  void Shutdown();
+
+  
+  virtual void Notify(const hal::SwitchEvent& aEvent) MOZ_OVERRIDE;
+  
+  virtual void Notify(const FMRadioEventType& aType) MOZ_OVERRIDE;
+
+  nsPIDOMWindow* GetParentObject() const
+  {
+    return GetOwner();
+  }
+
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
+
+  static bool Enabled();
+
+  bool AntennaAvailable() const;
+
+  Nullable<double> GetFrequency() const;
+
+  double FrequencyUpperBound() const;
+
+  double FrequencyLowerBound() const;
+
+  double ChannelWidth() const;
+
+  already_AddRefed<DOMRequest> Enable(double aFrequency);
+
+  already_AddRefed<DOMRequest> Disable();
+
+  already_AddRefed<DOMRequest> SetFrequency(double aFrequency);
+
+  already_AddRefed<DOMRequest> SeekUp();
+
+  already_AddRefed<DOMRequest> SeekDown();
+
+  already_AddRefed<DOMRequest> CancelSeek();
+
+  IMPL_EVENT_HANDLER(enabled);
+  IMPL_EVENT_HANDLER(disabled);
+  IMPL_EVENT_HANDLER(antennaavailablechange);
+  IMPL_EVENT_HANDLER(frequencychange);
+
+private:
+  ~FMRadio();
+
+  hal::SwitchState mHeadphoneState;
+  bool mHasInternalAntenna;
+  bool mIsShutdown;
+};
+
+END_FMRADIO_NAMESPACE
+
+#endif 
+
