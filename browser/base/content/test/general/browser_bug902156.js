5e6339f07ebe34b7106f94f26fbe0963a4d8f22d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:20:44 +0000

    Bug 931169 - waitForCondition should guard the condition evaluation. r=dkeeler
    UltraBlame original commit: 954741a373f33169f3253e0477ae2a5ddd825284

--- browser/base/content/test/general/browser_bug902156.js
+++ browser/base/content/test/general/browser_bug902156.js
@@ -44,26 +44,6 @@ function cleanUpAfterTests() {
 
 
 
-
-function waitForCondition(condition, nextTest, errorMsg) {
-  var tries = 0;
-  var interval = setInterval(function() {
-    if (tries >= 30) {
-      ok(false, errorMsg);
-      moveOn();
-    }
-    if (condition()) {
-      moveOn();
-    }
-    tries++;
-  }, 100);
-  var moveOn = function() {
-    clearInterval(interval); nextTest();
-  };
-}
-
-
-
 function test1A() {
   
   
