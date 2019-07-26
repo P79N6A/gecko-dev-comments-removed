616ebf03b2f96a882f904df14c2453e3dd06ae6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:03:22 +0000

    Bug 784739 - Switch from NULL to nullptr in js/src/{devtools,ds,gdb,perf}/; r=ehsan
    UltraBlame original commit: 5d34afe94163e58ad32ded6f3bc47e3c389b1e60

--- js/src/gdb/gdb-tests.cpp
+++ js/src/gdb/gdb-tests.cpp
@@ -1,6 +1,6 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,7 +12,7 @@
 
 using namespace JS;
 
-/* The class of the global object. */
+
 const JSClass global_class = {
     "global", JSCLASS_GLOBAL_FLAGS,
     JS_PropertyStub,  JS_DeletePropertyStub, JS_PropertyStub,  JS_StrictPropertyStub,
@@ -35,7 +35,7 @@ checkBool(bool success)
     abort();
 }
 
-/* The error reporter callback. */
+
 void reportError(JSContext *cx, const char *message, JSErrorReport *report)
 {
     fprintf(stderr, "%s:%u: %s\n",
@@ -44,17 +44,17 @@ void reportError(JSContext *cx, const char *message, JSErrorReport *report)
             message);
 }
 
-// prolog.py sets a breakpoint on this function; test functions can call it
-// to easily return control to GDB where desired.
+
+
 void breakpoint() {
-    // If we leave this function empty, the linker will unify it with other
-    // empty functions throughout SpiderMonkey. If we then set a GDB
-    // breakpoint on it, that breakpoint will hit at all sorts of random
-    // times. So make it perform a distinctive side effect.
+    
+    
+    
+    
     fprintf(stderr, "Called " __FILE__ ":breakpoint\n");
 }
 
-GDBFragment *GDBFragment::allFragments = NULL;
+GDBFragment *GDBFragment::allFragments = nullptr;
 
 int
 main (int argc, const char **argv)
@@ -68,17 +68,17 @@ main (int argc, const char **argv)
 
     JSAutoRequest ar(cx);
 
-    /* Create the global object. */
+    
     JS::CompartmentOptions options;
     options.setVersion(JSVERSION_LATEST);
-    RootedObject global(cx, checkPtr(JS_NewGlobalObject(cx, &global_class, NULL,
-                        JS::FireOnNewGlobalHook, options)));
+    RootedObject global(cx, checkPtr(JS_NewGlobalObject(cx, &global_class,
+                        nullptr, JS::FireOnNewGlobalHook, options)));
     js::SetDefaultObjectForContext(cx, global);
 
     JSAutoCompartment ac(cx, global);
 
-    /* Populate the global object with the standard globals,
-       like Object and Array. */
+    
+
     checkBool(JS_InitStandardClasses(cx, global));
 
     argv++;
