70b5efdad4ae6f3793a14c6f7317e8e3e647736b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:37:49 +0000

    Bug 845690 - Print a warning for developers if we apply our implicit backwards-compatible meta-viewport tag. r=ehsan,f=vingtetun
    UltraBlame original commit: a85e47332d6749fddba5ecf3e56f664cf4e94e13

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -6915,6 +6915,14 @@ nsDocument::GetViewportInfo(const ScreenIntSize& aDisplaySize)
       
       nsCOMPtr<nsIDocShell> docShell(mDocumentContainer);
       if (docShell && docShell->GetIsApp()) {
+        nsString uri;
+        GetDocumentURI(uri);
+        if (!uri.EqualsLiteral("about:blank")) {
+          nsContentUtils::ReportToConsole(nsIScriptError::warningFlag,
+                                          NS_LITERAL_CSTRING("DOM"), this,
+                                          nsContentUtils::eDOM_PROPERTIES,
+                                          "ImplicitMetaViewportTagFallback");
+        }
         mViewportType = DisplayWidthHeightNoZoom;
         return nsViewportInfo(aDisplaySize,  false);
       }
