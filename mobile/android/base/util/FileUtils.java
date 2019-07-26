d65deb93a93351bab9ff96580a499e59f403feda
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:07:53 +0000

    Bug 942611 - Delete old temp files onPause. r=rnewman
    UltraBlame original commit: 4166c73bace4785747cf6cfbcc4e2aedc78c6ccf

---
+++ mobile/android/base/util/FileUtils.java
@@ -0,0 +1,81 @@
+
+
+
+
+package org.mozilla.gecko.util;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FilenameFilter;
+
+public class FileUtils {
+    private static final String LOGTAG= "GeckoFileUtils";
+    
+
+
+    static public class NameAndAgeFilter implements FilenameFilter {
+        final private String mName;
+        final private double mMaxAge;
+
+        public NameAndAgeFilter(String name, double age) {
+            mName = name;
+            mMaxAge = age;
+        }
+
+        @Override
+        public boolean accept(File dir, String filename) {
+            if (mName == null || mName.matches(filename)) {
+                File f = new File(dir, filename);
+
+                if (mMaxAge < 0 || System.currentTimeMillis() - f.lastModified() > mMaxAge) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public static void delTree(File dir, FilenameFilter filter, boolean recurse) {
+        String[] files = null;
+
+        if (filter != null) {
+          files = dir.list(filter);
+        } else {
+          files = dir.list();
+        }
+
+        if (files == null) {
+          return;
+        }
+
+        for (String file : files) {
+            File f = new File(dir, file);
+            delete(f, recurse);
+        }
+    }
+
+    public static boolean delete(File file) throws IOException {
+        return delete(file, true);
+    }
+
+    public static boolean delete(File file, boolean recurse) {
+        if (file.isDirectory() && recurse) {
+            
+            String files[] = file.list();
+            for (String temp : files) {
+                File fileDelete = new File(file, temp);
+                try {
+                    delete(fileDelete);
+                } catch(IOException ex) {
+                    Log.i(LOGTAG, "Error deleting " + fileDelete.getPath(), ex);
+                }
+            }
+        }
+
+        
+        return file.delete();
+    }
+}
