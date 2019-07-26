6f60f8167f693f0acefea034e276f4be24212221
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:33:52 +0000

    Bug 799401 - Throttle APZC paint requests. r=cjones
    UltraBlame original commit: 8b1d3367ca5d8ea0059a80b52d4da6c38adc3e17

---
+++ gfx/layers/ipc/TaskThrottler.cpp
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+#include "base/basictypes.h"
+#include "base/message_loop.h"
+#include "TaskThrottler.h"
+
+namespace mozilla {
+namespace layers {
+
+TaskThrottler::TaskThrottler()
+  : mOutstanding(false)
+  , mQueuedTask(nullptr)
+{ }
+
+void
+TaskThrottler::PostTask(const tracked_objects::Location& aLocation,
+                        CancelableTask* aTask)
+{
+  aTask->SetBirthPlace(aLocation);
+
+  if (mOutstanding) {
+    if (mQueuedTask) {
+      mQueuedTask->Cancel();
+    }
+    mQueuedTask = aTask;
+  } else {
+    aTask->Run();
+    delete aTask;
+    mOutstanding = true;
+  }
+}
+
+void
+TaskThrottler::TaskComplete()
+{
+  if (mQueuedTask) {
+    mQueuedTask->Run();
+    mQueuedTask = nullptr;
+  } else {
+    mOutstanding = false;
+  }
+}
+
+}
+}
---
+++ gfx/layers/ipc/TaskThrottler.h
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_TaskThrottler_h
+#define mozilla_dom_TaskThrottler_h
+
+#include "nsAutoPtr.h"
+
+class CancelableTask;
+namespace tracked_objects {
+  class Location;
+}
+
+namespace mozilla {
+namespace layers {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class TaskThrottler {
+public:
+  TaskThrottler();
+
+  
+
+
+
+
+
+
+  void PostTask(const tracked_objects::Location& aLocation,
+                CancelableTask* aTask);
+  void TaskComplete();
+
+private:
+  bool mOutstanding;
+  nsAutoPtr<CancelableTask> mQueuedTask;
+};
+
+}
+}
+
+#endif 
