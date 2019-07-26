262beb175aec2e2b09819281a727d1d973ca2626
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:01:36 +0000

    Bug 889356 - Add test for app `redirects` manifest feature. r=fabrice
    UltraBlame original commit: b22b41460ba319600b50ccdb25071369e720b886

---
+++ toolkit/devtools/apps/tests/unit/head_apps.js
@@ -0,0 +1,128 @@
+
+
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+const Cr = Components.results;
+const CC = Components.Constructor;
+
+Cu.import("resource://gre/modules/devtools/dbg-server.jsm");
+Cu.import("resource://gre/modules/devtools/dbg-client.jsm");
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/FileUtils.jsm");
+
+let gClient, gActor;
+
+function connect(onDone) {
+  
+  DebuggerServer.init(function () { return true; });
+  
+  
+  DebuggerServer.addBrowserActors();
+
+  
+  gClient = new DebuggerClient(DebuggerServer.connectPipe());
+  gClient.connect(function onConnect() {
+    gClient.listTabs(function onListTabs(aResponse) {
+      gActor = aResponse.webappsActor;
+      onDone();
+    });
+  });
+}
+
+function webappActorRequest(request, onResponse) {
+  if (!gActor) {
+    connect(webappActorRequest.bind(null, request, onResponse));
+    return;
+  }
+
+  request.to = gActor;
+  gClient.request(request, onResponse);
+}
+
+
+function installTestApp(zipName, appId, onDone) {
+  
+  let zip = do_get_file("data/" + zipName);
+  let appDir = FileUtils.getDir("TmpD", ["b2g", appId], true, true);
+  zip.copyTo(appDir, "application.zip");
+
+  let request = {type: "install", appId: appId};
+  webappActorRequest(request, function (aResponse) {
+    do_check_eq(aResponse.appId, appId);
+  });
+
+  
+  
+  gClient.addListener("webappsEvent", function (aState, aType, aPacket) {
+    do_check_eq(aType.appId, appId);
+    if ("error" in aType) {
+      do_throw("Error: " + aType.error);
+    }
+    if ("message" in aType) {
+      do_throw("Error message: " + aType.message);
+    }
+    do_check_false("error" in aType);
+
+    onDone();
+  });
+};
+
+function setup() {
+  
+  
+  do_get_profile();
+
+  
+  
+  do_get_webappsdir();
+
+  
+  Components.utils.import("resource://testing-common/AppInfo.jsm");
+  updateAppInfo();
+
+  
+  
+  Components.utils.import('resource://gre/modules/Webapps.jsm');
+  DOMApplicationRegistry.allAppsLaunchable = true;
+}
+
+function do_get_webappsdir() {
+  var webappsDir = Services.dirsvc.get("ProfD", Ci.nsILocalFile);
+  webappsDir.append("test_webapps");
+  if (!webappsDir.exists())
+    webappsDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0755);
+
+  var coreAppsDir = Services.dirsvc.get("ProfD", Ci.nsILocalFile);
+  coreAppsDir.append("test_coreapps");
+  if (!coreAppsDir.exists())
+    coreAppsDir.create(Ci.nsIFile.DIRECTORY_TYPE, 0755);
+
+  
+  
+  var provider = {
+    getFile: function(prop, persistent) {
+      persistent.value = true;
+      if (prop == "webappsDir") {
+        return webappsDir.clone();
+      }
+      else if (prop == "coreAppsDir") {
+        return coreAppsDir.clone();
+      }
+      throw Cr.NS_ERROR_FAILURE;
+    },
+    QueryInterface: function(iid) {
+      if (iid.equals(Ci.nsIDirectoryServiceProvider) ||
+          iid.equals(Ci.nsISupports)) {
+        return this;
+      }
+      throw Cr.NS_ERROR_NO_INTERFACE;
+    }
+  };
+  Services.dirsvc.QueryInterface(Ci.nsIDirectoryService).registerProvider(provider);
+}
+
+
