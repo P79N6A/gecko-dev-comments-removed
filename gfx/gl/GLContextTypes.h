7de7be26706191c35569b27af2efdc7b93f93454
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:29:41 +0000

    Bug 811958 - Pull GLContext out of Cocoa stuff - r=bgirard
    UltraBlame original commit: ec29e09a270e3e5c268f30d409ab392f7b2c288f

---
+++ gfx/gl/GLContextTypes.h
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+#ifndef GLCONTEXTSTUFF_H_
+#define GLCONTEXTSTUFF_H_
+
+
+
+
+
+typedef unsigned int GLenum;
+typedef unsigned int GLbitfield;
+typedef unsigned int GLuint;
+typedef int GLint;
+typedef int GLsizei;
+
+namespace mozilla {
+namespace gl {
+
+enum ShaderProgramType {
+    RGBALayerProgramType,
+    RGBALayerExternalProgramType,
+    BGRALayerProgramType,
+    RGBXLayerProgramType,
+    BGRXLayerProgramType,
+    RGBARectLayerProgramType,
+    RGBAExternalLayerProgramType,
+    ColorLayerProgramType,
+    YCbCrLayerProgramType,
+    ComponentAlphaPass1ProgramType,
+    ComponentAlphaPass2ProgramType,
+    Copy2DProgramType,
+    Copy2DRectProgramType,
+    NumProgramTypes
+};
+
+} 
+} 
+
+#endif 
