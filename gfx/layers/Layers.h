a496923d736f34a2d4c89598515f2833efca6614
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:05:24 +0000

    Bug 828173 patch 2:  Make Layer::AddAnimation have the caller fill in the data instead of taking parameters.  r=mattwoodrow
    (This is a bigger simplification later in the patch queue, when I add a
    variant of AddAnimation called AddAnimationForNextTransaction.)
    
    UltraBlame original commit: b999b6e2ccaba1bf9dafb6b6aa408512481a5c51

--- gfx/layers/Layers.h
+++ gfx/layers/Layers.h
@@ -943,10 +943,7 @@ public:
 
   
   
-  
-  Animation* AddAnimation(mozilla::TimeStamp aStart, mozilla::TimeDuration aDuration,
-                          float aIterations, int aDirection,
-                          nsCSSProperty aProperty, const AnimationData& aData);
+  Animation* AddAnimation();
   
   void ClearAnimations();
   
