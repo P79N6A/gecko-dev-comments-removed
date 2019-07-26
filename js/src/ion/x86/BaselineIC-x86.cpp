5e8f8805ce3f1c6540586f7456660345f0f75977
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:07:08 +0000

    Bug 806568 - Add platform-specific files for ICs. r=djvj
    UltraBlame original commit: 99f8dd9802cddc5cdad2c69a18c04dc4fa6d0e12

---
+++ js/src/ion/x86/BaselineIC-x86.cpp
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+#include "ion/BaselineJIT.h"
+#include "ion/BaselineIC.h"
+#include "ion/BaselineCompiler.h"
+#include "ion/IonLinker.h"
+
+using namespace js;
+using namespace js::ion;
