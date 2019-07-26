669e9600d2918ce721e8aac202256dde0e0a667e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:44:40 +0000

    Bug 819377 - Don't offset the clock by the start time of the video in AdvanceFrame, and set the media as playing earlier. r=kinetik
    UltraBlame original commit: 5add564d4546e24e49e3c0e8aebca9a4c7246f30

--- content/media/MediaDecoderStateMachine.cpp
+++ content/media/MediaDecoderStateMachine.cpp
@@ -2073,6 +2073,13 @@ nsresult MediaDecoderStateMachine::RunStateMachine()
         StopPlayback();
       }
 
+      if (mDecoder->GetState() == MediaDecoder::PLAY_STATE_PLAYING &&
+          !IsPlaying()) {
+        
+        
+        StartPlayback();
+      }
+
       if (IsPausedAndDecoderWaiting()) {
         
         
@@ -2347,9 +2354,9 @@ void MediaDecoderStateMachine::AdvanceFrame()
     
     
     if (frame && !currentFrame) {
-      int64_t now = IsPlaying() ? clock_time : mPlayDuration;
+      int64_t now = IsPlaying() ? clock_time : mPlayDuration + mStartTime;
 
-      remainingTime = frame->mTime - mStartTime - now;
+      remainingTime = frame->mTime - now;
     }
   }
 
@@ -2396,7 +2403,7 @@ void MediaDecoderStateMachine::AdvanceFrame()
       return;
     }
     mDecoder->GetFrameStatistics().NotifyPresentedFrame();
-    remainingTime = currentFrame->mEndTime - mStartTime - clock_time;
+    remainingTime = currentFrame->mEndTime - clock_time;
     currentFrame = nullptr;
   }
 
