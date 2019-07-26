178d59d2af9a030a8c1a03ede07bc1f4691c4b4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:04:12 +0000

    Bug 883004 - Part 3: Add Direct2D 1.1 backend to Moz2D. r=jrmuizel
    UltraBlame original commit: abf08bdc5e9306814863e191de30615c949c1e83

---
+++ gfx/2d/SourceSurfaceD2D1.h
@@ -0,0 +1,85 @@
+
+
+
+
+
+#ifndef MOZILLA_GFX_SOURCESURFACED2DTARGET_H_
+#define MOZILLA_GFX_SOURCESURFACED2DTARGET_H_
+
+#include "2D.h"
+#include "HelpersD2D.h"
+#include <vector>
+#include <d3d11.h>
+#include <d2d1_1.h>
+
+namespace mozilla {
+namespace gfx {
+
+class DrawTargetD2D1;
+
+class SourceSurfaceD2D1 : public SourceSurface
+{
+public:
+  SourceSurfaceD2D1(ID2D1Image* aImage, ID2D1DeviceContext *aDC,
+                    SurfaceFormat aFormat, const IntSize &aSize,
+                    DrawTargetD2D1 *aDT = nullptr);
+  ~SourceSurfaceD2D1();
+
+  virtual SurfaceType GetType() const { return SURFACE_D2D1_1_IMAGE; }
+  virtual IntSize GetSize() const { return mSize; }
+  virtual SurfaceFormat GetFormat() const { return mFormat; }
+  virtual TemporaryRef<DataSourceSurface> GetDataSurface();
+
+  ID2D1Image *GetImage() { return mImage; }
+
+private:
+  friend class DrawTargetD2D1;
+
+  void EnsureRealizedBitmap();
+
+  
+  
+  
+  void DrawTargetWillChange();
+
+  
+  
+  void MarkIndependent();
+
+  RefPtr<ID2D1Image> mImage;
+  
+  
+  RefPtr<ID2D1Bitmap1> mRealizedBitmap;
+  RefPtr<ID2D1DeviceContext> mDC;
+
+  SurfaceFormat mFormat;
+  IntSize mSize;
+  RefPtr<DrawTargetD2D1> mDrawTarget;
+};
+
+class DataSourceSurfaceD2D1 : public DataSourceSurface
+{
+public:
+  DataSourceSurfaceD2D1(ID2D1Bitmap1 *aMappableBitmap, SurfaceFormat aFormat);
+  ~DataSourceSurfaceD2D1();
+
+  virtual SurfaceType GetType() const { return SURFACE_DATA; }
+  virtual IntSize GetSize() const;
+  virtual SurfaceFormat GetFormat() const { return mFormat; }
+  virtual uint8_t *GetData();
+  virtual int32_t Stride();
+
+private:
+  friend class SourceSurfaceD2DTarget;
+  void EnsureMapped();
+
+  mutable RefPtr<ID2D1Bitmap1> mBitmap;
+  SurfaceFormat mFormat;
+  D2D1_MAPPED_RECT mMap;
+  bool mMapped;
+};
+
+}
+}
+
+#endif 
