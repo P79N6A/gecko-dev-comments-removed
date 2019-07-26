3296c7d317cf543dcccc311e96d6426a092439f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:02:07 +0000

    Bug 835581 - Check for "@mozilla.org/xpcom/mac-utils;1" in nsURLFormatter.js to avoid ReferenceError. r=mak
    UltraBlame original commit: 89202194cc4f3977f31f5c36d9c923efe90cded9

--- toolkit/components/urlformatter/nsURLFormatter.js
+++ toolkit/components/urlformatter/nsURLFormatter.js
@@ -38,12 +38,14 @@ function nsURLFormatterService() {
     try {
       ABI = this.appInfo.XPCOMABI;
 
-      
-      
-      let macutils = Cc["@mozilla.org/xpcom/mac-utils;1"].
-                      getService(Ci.nsIMacUtils);
-      if (macutils && macutils.isUniversalBinary) {
-        ABI = "Universal-gcc3";
+      if ("@mozilla.org/xpcom/mac-utils;1" in Cc) {
+        
+        
+        let macutils = Cc["@mozilla.org/xpcom/mac-utils;1"]
+                         .getService(Ci.nsIMacUtils);
+        if (macutils && macutils.isUniversalBinary) {
+          ABI = "Universal-gcc3";
+        }
       }
     } catch (e) {}
 
