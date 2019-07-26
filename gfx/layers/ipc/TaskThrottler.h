a691350655a05e0b0820e6316d469e5574412128
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:00:16 +0000

    Bug 864447 - Move timing tracking out of APZC; r=BenWa
    UltraBlame original commit: fcf0c2f4fb0b81da056123ab750ab134195af987

--- gfx/layers/ipc/TaskThrottler.h
+++ gfx/layers/ipc/TaskThrottler.h
@@ -8,6 +8,8 @@
 #define mozilla_dom_TaskThrottler_h
 
 #include "nsAutoPtr.h"
+#include "nsTArray.h"
+#include "mozilla/TimeStamp.h"
 
 class CancelableTask;
 namespace tracked_objects {
@@ -32,7 +34,7 @@ namespace layers {
 
 class TaskThrottler {
 public:
-  TaskThrottler();
+  TaskThrottler(const TimeStamp& aTimeStamp);
 
   
 
@@ -42,15 +44,50 @@ public:
 
 
   void PostTask(const tracked_objects::Location& aLocation,
-                CancelableTask* aTask);
+                CancelableTask* aTask, const TimeStamp& aTimeStamp);
   
 
 
-  bool TaskComplete();
+  void TaskComplete(const TimeStamp& aTimeStamp);
+
+  
+
+
+
+  TimeDuration AverageDuration();
+
+  
+
+
+  bool IsOutstanding() { return mOutstanding; }
+
+  
+
+
+  TimeDuration TimeSinceLastRequest(const TimeStamp& aTimeStamp);
+
+  
+
+
+  void ClearHistory() { mDurations.Clear(); }
+
+  
+
+
+
+  void SetMaxDurations(uint32_t aMaxDurations)
+  {
+    mMaxDurations = aMaxDurations;
+  }
 
 private:
   bool mOutstanding;
   nsAutoPtr<CancelableTask> mQueuedTask;
+  TimeStamp mStartTime;
+
+  
+  nsTArray<TimeDuration> mDurations;
+  uint32_t mMaxDurations;
 };
 
 }
