49d0e1f7bfa01cd334a41ad3d7137ce61c49cfec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:05:40 +0000

    Bug 882543 - Use a linked list for ordering stream instead of an array. r=ehsan
    UltraBlame original commit: b36516aab38927533a5e4aa6835f0c4035079039

--- content/media/MediaStreamGraphImpl.h
+++ content/media/MediaStreamGraphImpl.h
@@ -15,6 +15,9 @@
 
 namespace mozilla {
 
+template <typename T>
+class LinkedList;
+
 #ifdef PR_LOGGING
 extern PRLogModuleInfo* gMediaStreamGraphLog;
 #define LOG(type, msg) PR_LOG(gMediaStreamGraphLog, type, msg)
@@ -221,7 +224,7 @@ public:
 
 
 
-  void UpdateStreamOrderForStream(nsTArray<MediaStream*>* aStack,
+  void UpdateStreamOrderForStream(mozilla::LinkedList<MediaStream>* aStack,
                                   already_AddRefed<MediaStream> aStream);
   
 
