5a33f2f53ac08a5b334190c1efb4795286907331
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:03:36 +0000

    Bug 842828: Check local list to suppress remote lookups (r=paolo)
    UltraBlame original commit: bb76828c5f22e236f848fa7df7aacd0b58522315

--- toolkit/components/downloads/ApplicationReputation.h
+++ toolkit/components/downloads/ApplicationReputation.h
@@ -11,15 +11,16 @@
 #include "nsIRequestObserver.h"
 #include "nsIStreamListener.h"
 #include "nsISupports.h"
+#include "nsIUrlClassifierDBService.h"
 
+#include "nsAutoPtr.h"
 #include "nsCOMPtr.h"
 #include "nsString.h"
 
-class nsIApplicationReputationListener;
-class nsIChannel;
-class nsIObserverService;
 class nsIRequest;
-class PRLogModuleInfo;
+class nsIUrlClassifierDBService;
+class nsIScriptSecurityManager;
+class PendingLookup;
 
 class ApplicationReputationService MOZ_FINAL :
   public nsIApplicationReputationService {
@@ -35,10 +36,16 @@ private:
 
 
   static ApplicationReputationService* gApplicationReputationService;
+  
+
+
+  nsCOMPtr<nsIUrlClassifierDBService> mDBService;
+  nsCOMPtr<nsIScriptSecurityManager> mSecurityManager;
+  
+
 
   ApplicationReputationService();
   ~ApplicationReputationService();
-
   
 
 
@@ -46,53 +53,4 @@ private:
   nsresult QueryReputationInternal(nsIApplicationReputationQuery* aQuery,
                                    nsIApplicationReputationCallback* aCallback);
 };
-
-
-
-
-
-
-class ApplicationReputationQuery MOZ_FINAL :
-  public nsIApplicationReputationQuery,
-  public nsIStreamListener {
-public:
-  NS_DECL_ISUPPORTS
-  NS_DECL_NSIREQUESTOBSERVER
-  NS_DECL_NSISTREAMLISTENER
-  NS_DECL_NSIAPPLICATIONREPUTATIONQUERY
-
-  ApplicationReputationQuery();
-  ~ApplicationReputationQuery();
-
-private:
-  
-
-
-  nsString mSuggestedFileName;
-  nsCOMPtr<nsIURI> mURI;
-  uint32_t mFileSize;
-  nsCString mSha256Hash;
-
-  
-
-
-  nsCOMPtr<nsIApplicationReputationCallback> mCallback;
-
-  
-
-
-
-
-  nsCString mResponse;
-
-  
-
-
-
-  nsresult OnStopRequestInternal(nsIRequest *aRequest,
-                                 nsISupports *aContext,
-                                 nsresult aResult,
-                                 bool* aShouldBlock);
-};
-
 #endif 
