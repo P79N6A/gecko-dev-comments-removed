cc31a1876b55b2021b3f736fd7e1b4784fc7bcbe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:41:20 +0000

    Bug 825147: Convert SVGRectElement to WebIDL r=bz
    UltraBlame original commit: 04feda337f8b75c1e09d2465ed2a90c3bad64a08

---
+++ content/svg/content/src/SVGRectElement.cpp
@@ -0,0 +1,220 @@
+
+
+
+
+
+#include "mozilla/dom/SVGRectElement.h"
+#include "nsGkAtoms.h"
+#include "gfxContext.h"
+#include "mozilla/dom/SVGRectElementBinding.h"
+
+DOMCI_NODE_DATA(SVGRectElement, mozilla::dom::SVGRectElement)
+
+NS_IMPL_NS_NEW_NAMESPACED_SVG_ELEMENT(Rect)
+
+namespace mozilla {
+namespace dom {
+
+JSObject*
+SVGRectElement::WrapNode(JSContext *aCx, JSObject *aScope, bool *aTriedToWrap)
+{
+  return SVGRectElementBinding::Wrap(aCx, aScope, this, aTriedToWrap);
+}
+
+nsSVGElement::LengthInfo SVGRectElement::sLengthInfo[6] =
+{
+  { &nsGkAtoms::x, 0, nsIDOMSVGLength::SVG_LENGTHTYPE_NUMBER, SVGContentUtils::X },
+  { &nsGkAtoms::y, 0, nsIDOMSVGLength::SVG_LENGTHTYPE_NUMBER, SVGContentUtils::Y },
+  { &nsGkAtoms::width, 0, nsIDOMSVGLength::SVG_LENGTHTYPE_NUMBER, SVGContentUtils::X },
+  { &nsGkAtoms::height, 0, nsIDOMSVGLength::SVG_LENGTHTYPE_NUMBER, SVGContentUtils::Y },
+  { &nsGkAtoms::rx, 0, nsIDOMSVGLength::SVG_LENGTHTYPE_NUMBER, SVGContentUtils::X },
+  { &nsGkAtoms::ry, 0, nsIDOMSVGLength::SVG_LENGTHTYPE_NUMBER, SVGContentUtils::Y }
+};
+
+
+
+
+NS_IMPL_ADDREF_INHERITED(SVGRectElement,SVGRectElementBase)
+NS_IMPL_RELEASE_INHERITED(SVGRectElement,SVGRectElementBase)
+
+NS_INTERFACE_TABLE_HEAD(SVGRectElement)
+  NS_NODE_INTERFACE_TABLE4(SVGRectElement, nsIDOMNode, nsIDOMElement,
+                           nsIDOMSVGElement,
+                           nsIDOMSVGRectElement)
+  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(SVGRectElement)
+NS_INTERFACE_MAP_END_INHERITING(SVGRectElementBase)
+
+
+
+
+SVGRectElement::SVGRectElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+  : SVGRectElementBase(aNodeInfo)
+{
+  SetIsDOMBinding();
+}
+
+
+
+
+NS_IMPL_ELEMENT_CLONE_WITH_INIT(SVGRectElement)
+
+
+
+
+
+NS_IMETHODIMP SVGRectElement::GetX(nsIDOMSVGAnimatedLength * *aX)
+{
+  *aX = X().get();
+  return NS_OK;
+}
+
+already_AddRefed<nsIDOMSVGAnimatedLength>
+SVGRectElement::X()
+{
+  nsCOMPtr<nsIDOMSVGAnimatedLength> x;
+  mLengthAttributes[ATTR_X].ToDOMAnimatedLength(getter_AddRefs(x), this);
+  return x.forget();
+}
+
+
+NS_IMETHODIMP SVGRectElement::GetY(nsIDOMSVGAnimatedLength * *aY)
+{
+  *aY = Y().get();
+  return NS_OK;
+}
+
+already_AddRefed<nsIDOMSVGAnimatedLength>
+SVGRectElement::Y()
+{
+  nsCOMPtr<nsIDOMSVGAnimatedLength> y;
+  mLengthAttributes[ATTR_Y].ToDOMAnimatedLength(getter_AddRefs(y), this);
+  return y.forget();
+}
+
+
+NS_IMETHODIMP SVGRectElement::GetWidth(nsIDOMSVGAnimatedLength * *aWidth)
+{
+  *aWidth = Width().get();
+  return NS_OK;
+}
+
+already_AddRefed<nsIDOMSVGAnimatedLength>
+SVGRectElement::Width()
+{
+  nsCOMPtr<nsIDOMSVGAnimatedLength> width;
+  mLengthAttributes[ATTR_WIDTH].ToDOMAnimatedLength(getter_AddRefs(width), this);
+  return width.forget();
+}
+
+
+NS_IMETHODIMP SVGRectElement::GetHeight(nsIDOMSVGAnimatedLength * *aHeight)
+{
+  *aHeight = Height().get();
+  return NS_OK;
+}
+
+already_AddRefed<nsIDOMSVGAnimatedLength>
+SVGRectElement::Height()
+{
+  nsCOMPtr<nsIDOMSVGAnimatedLength> height;
+  mLengthAttributes[ATTR_HEIGHT].ToDOMAnimatedLength(getter_AddRefs(height), this);
+  return height.forget();
+}
+
+
+NS_IMETHODIMP SVGRectElement::GetRx(nsIDOMSVGAnimatedLength * *aRx)
+{
+  *aRx = Rx().get();
+  return NS_OK;
+}
+
+already_AddRefed<nsIDOMSVGAnimatedLength>
+SVGRectElement::Rx()
+{
+  nsCOMPtr<nsIDOMSVGAnimatedLength> Rx;
+  mLengthAttributes[ATTR_RX].ToDOMAnimatedLength(getter_AddRefs(Rx), this);
+  return Rx.forget();
+}
+
+
+NS_IMETHODIMP SVGRectElement::GetRy(nsIDOMSVGAnimatedLength * *aRy)
+{
+  *aRy = Ry().get();
+  return NS_OK;
+}
+
+already_AddRefed<nsIDOMSVGAnimatedLength>
+SVGRectElement::Ry()
+{
+  nsCOMPtr<nsIDOMSVGAnimatedLength> Ry;
+  mLengthAttributes[ATTR_RY].ToDOMAnimatedLength(getter_AddRefs(Ry), this);
+  return Ry.forget();
+}
+
+
+
+
+ bool
+SVGRectElement::HasValidDimensions() const
+{
+  return mLengthAttributes[ATTR_WIDTH].IsExplicitlySet() &&
+         mLengthAttributes[ATTR_WIDTH].GetAnimValInSpecifiedUnits() > 0 &&
+         mLengthAttributes[ATTR_HEIGHT].IsExplicitlySet() &&
+         mLengthAttributes[ATTR_HEIGHT].GetAnimValInSpecifiedUnits() > 0;
+}
+
+nsSVGElement::LengthAttributesInfo
+SVGRectElement::GetLengthInfo()
+{
+  return LengthAttributesInfo(mLengthAttributes, sLengthInfo,
+                              ArrayLength(sLengthInfo));
+}
+
+
+
+
+void
+SVGRectElement::ConstructPath(gfxContext *aCtx)
+{
+  float x, y, width, height, rx, ry;
+
+  GetAnimatedLengthValues(&x, &y, &width, &height, &rx, &ry, nullptr);
+
+  
+
+  if (width <= 0 || height <= 0)
+    return;
+
+  rx = NS_MAX(rx, 0.0f);
+  ry = NS_MAX(ry, 0.0f);
+
+  
+  if (rx == 0 && ry == 0) {
+    aCtx->Rectangle(gfxRect(x, y, width, height));
+    return;
+  }
+
+  
+
+  bool hasRx = mLengthAttributes[ATTR_RX].IsExplicitlySet();
+  bool hasRy = mLengthAttributes[ATTR_RY].IsExplicitlySet();
+  if (hasRx && !hasRy)
+    ry = rx;
+  else if (hasRy && !hasRx)
+    rx = ry;
+
+  
+  float halfWidth  = width/2;
+  float halfHeight = height/2;
+  if (rx > halfWidth)
+    rx = halfWidth;
+  if (ry > halfHeight)
+    ry = halfHeight;
+
+  gfxSize corner(rx, ry);
+  aCtx->RoundedRectangle(gfxRect(x, y, width, height),
+                         gfxCornerSizes(corner, corner, corner, corner));
+}
+
+} 
+} 
---
+++ content/svg/content/src/SVGRectElement.h
@@ -0,0 +1,72 @@
+
+
+
+
+
+#ifndef mozilla_dom_SVGRectElement_h
+#define mozilla_dom_SVGRectElement_h
+
+#include "nsSVGPathGeometryElement.h"
+#include "nsIDOMSVGRectElement.h"
+#include "nsSVGLength2.h"
+
+nsresult NS_NewSVGRectElement(nsIContent **aResult,
+                              already_AddRefed<nsINodeInfo> aNodeInfo);
+
+typedef nsSVGPathGeometryElement SVGRectElementBase;
+
+namespace mozilla {
+namespace dom {
+
+class SVGRectElement MOZ_FINAL : public SVGRectElementBase,
+                                 public nsIDOMSVGRectElement
+{
+protected:
+  SVGRectElement(already_AddRefed<nsINodeInfo> aNodeInfo);
+  virtual JSObject* WrapNode(JSContext *cx, JSObject *scope, bool *triedToWrap) MOZ_OVERRIDE;
+  friend nsresult (::NS_NewSVGRectElement(nsIContent **aResult,
+                                          already_AddRefed<nsINodeInfo> aNodeInfo));
+
+public:
+  
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_NSIDOMSVGRECTELEMENT
+
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+  NS_FORWARD_NSIDOMSVGELEMENT(SVGRectElementBase::)
+
+  
+  virtual bool HasValidDimensions() const;
+
+  
+  virtual void ConstructPath(gfxContext *aCtx);
+
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+
+  virtual nsXPCClassInfo* GetClassInfo();
+
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+
+  
+  already_AddRefed<nsIDOMSVGAnimatedLength> X();
+  already_AddRefed<nsIDOMSVGAnimatedLength> Y();
+  already_AddRefed<nsIDOMSVGAnimatedLength> Height();
+  already_AddRefed<nsIDOMSVGAnimatedLength> Width();
+  already_AddRefed<nsIDOMSVGAnimatedLength> Rx();
+  already_AddRefed<nsIDOMSVGAnimatedLength> Ry();
+
+protected:
+
+  virtual LengthAttributesInfo GetLengthInfo();
+
+  enum { ATTR_X, ATTR_Y, ATTR_WIDTH, ATTR_HEIGHT, ATTR_RX, ATTR_RY };
+  nsSVGLength2 mLengthAttributes[6];
+  static LengthInfo sLengthInfo[6];
+};
+
+} 
+} 
+
+#endif 
