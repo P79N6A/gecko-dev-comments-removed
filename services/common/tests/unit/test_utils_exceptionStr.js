aa6317dbf3bf3e2c32226021393af13b5b271ee6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:07:38 +0000

    Bug 851209 - Make exceptionStr robust against null exceptions. r=gps
    UltraBlame original commit: 7036707114197511489a9e080bcc41f3ed10c07e

---
+++ services/common/tests/unit/test_utils_exceptionStr.js
@@ -0,0 +1,17 @@
+
+
+
+"use strict";
+
+function run_test() {
+  run_next_test();
+}
+
+add_test(function test_exceptionStr_non_exceptions() {
+  do_check_eq(CommonUtils.exceptionStr(null), "null");
+  do_check_eq(CommonUtils.exceptionStr(false), "false");
+  do_check_eq(CommonUtils.exceptionStr(undefined), "undefined");
+  do_check_eq(CommonUtils.exceptionStr(12), "12 No traceback available");
+
+  run_next_test();
+});
