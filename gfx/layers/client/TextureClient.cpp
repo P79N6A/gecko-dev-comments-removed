d0e6a799cf02fd840e3b0f8264b8f0d01282503b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:29:07 +0000

    Bug 907792 - Refactor texture flags so that we set the deallocate flags based on the texture type. r=nical
    UltraBlame original commit: a7d0dd73fc2538578929ced6dddba7d367a86753

--- gfx/layers/client/TextureClient.cpp
+++ gfx/layers/client/TextureClient.cpp
@@ -38,6 +38,7 @@ namespace layers {
 TextureClient::TextureClient(TextureFlags aFlags)
   : mID(0)
   , mFlags(aFlags)
+  , mShared(false)
 {}
 
 TextureClient::~TextureClient()
@@ -46,9 +47,18 @@ TextureClient::~TextureClient()
 bool
 TextureClient::ShouldDeallocateInDestructor() const
 {
-  return IsAllocated() &&
-         !IsSharedWithCompositor() &&
-         !(GetFlags() & (TEXTURE_DEALLOCATE_HOST | TEXTURE_DEALLOCATE_CLIENT));
+  if (!IsAllocated()) {
+    return false;
+  }
+  if (GetFlags() & TEXTURE_DEALLOCATE_CLIENT) {
+    return true;
+  }
+
+  
+  
+  
+  return (GetFlags() & TEXTURE_DEALLOCATE_HOST) &&
+         !IsSharedWithCompositor();
 }
 
 bool
