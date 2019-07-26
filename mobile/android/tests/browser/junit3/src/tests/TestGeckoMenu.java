b9df8036607136de99949508cf793aa5c740ae36
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:17:23 +0000

    Bug 975838 - Part 2: JUnit3 test that a GeckoMenu call will fail on the wrong thread. r=wesj
    UltraBlame original commit: 67a7d2c4e9871b9011875794317b9649533fc7ea

---
+++ mobile/android/tests/browser/junit3/src/tests/TestGeckoMenu.java
@@ -0,0 +1,71 @@
+
+
+
+
+package org.mozilla.gecko.browser.tests;
+
+import org.mozilla.gecko.AppConstants;
+import org.mozilla.gecko.menu.GeckoMenu;
+import org.mozilla.gecko.util.ThreadUtils;
+
+public class TestGeckoMenu extends BrowserTestCase {
+
+    private volatile Exception exception;
+    private void setException(Exception e) {
+        this.exception = e;
+    }
+
+    public void testMenuThreading() throws InterruptedException {
+        final GeckoMenu menu = new GeckoMenu(getActivity());
+        final Object semaphore = new Object();
+
+        ThreadUtils.postToUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    menu.add("test1");
+                } catch (Exception e) {
+                    setException(e);
+                }
+
+                synchronized (semaphore) {
+                    semaphore.notify();
+                }
+            }
+        });
+        synchronized (semaphore) {
+            semaphore.wait();
+        }
+
+        
+        assertNull(exception);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    menu.add("test2");
+                } catch (Exception e) {
+                    setException(e);
+                }
+
+                synchronized (semaphore) {
+                    semaphore.notify();
+                }
+            }
+        }).start();
+
+        synchronized (semaphore) {
+            semaphore.wait();
+        }
+
+        if (AppConstants.RELEASE_BUILD) {
+            
+            assertNull(exception);
+            return;
+        }
+
+        assertNotNull(exception);
+        assertEquals(exception.getClass(), IllegalThreadStateException.class);
+    }
+}
