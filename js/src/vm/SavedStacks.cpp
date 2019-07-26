8a7a65a72ba735a568d46fe93bd7a1b243232851
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:01:12 +0000

    Bug 1006876 - Add a recursion check to SavedStacks::insertFrames. r=ejpbruel
    UltraBlame original commit: 7db77c1072cb0b191509c8c8d7654b141b3b1e4d

--- js/src/vm/SavedStacks.cpp
+++ js/src/vm/SavedStacks.cpp
@@ -9,6 +9,7 @@
 
 #include "jsapi.h"
 #include "jscompartment.h"
+#include "jsfriendapi.h"
 #include "jsnum.h"
 
 #include "vm/GlobalObject.h"
@@ -424,6 +425,14 @@ SavedStacks::insertFrames(JSContext *cx, ScriptFrameIter &iter, MutableHandle<Sa
         frame.set(nullptr);
         return true;
     }
+
+    
+    
+    
+    
+    
+    
+    JS_CHECK_RECURSION_DONT_REPORT(cx, return false);
 
     ScriptFrameIter thisFrame(iter);
     Rooted<SavedFrame*> parentFrame(cx);
