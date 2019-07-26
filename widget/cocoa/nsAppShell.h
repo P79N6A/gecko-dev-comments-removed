1ea0ee4866ad6697c526b14c7e0a1e2163033cb9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:56:34 +0000

    Bug 996848 - Rewrite Mac nsAppShell native event handling. r=spohl
    UltraBlame original commit: d8eb7fe678835ee63dccf5ff4c5ed06738a0072a

--- widget/cocoa/nsAppShell.h
+++ widget/cocoa/nsAppShell.h
@@ -16,36 +16,6 @@ class nsCocoaWindow;
 #include "nsBaseAppShell.h"
 #include "nsTArray.h"
 
-typedef struct _nsCocoaAppModalWindowListItem {
-  _nsCocoaAppModalWindowListItem(NSWindow *aWindow, NSModalSession aSession) :
-    mWindow(aWindow), mSession(aSession), mWidget(nullptr) {}
-  _nsCocoaAppModalWindowListItem(NSWindow *aWindow, nsCocoaWindow *aWidget) :
-    mWindow(aWindow), mSession(nil), mWidget(aWidget) {}
-  NSWindow *mWindow;       
-  NSModalSession mSession; 
-  nsCocoaWindow *mWidget;  
-} nsCocoaAppModalWindowListItem;
-
-class nsCocoaAppModalWindowList {
-public:
-  nsCocoaAppModalWindowList() {}
-  ~nsCocoaAppModalWindowList() {}
-  
-  nsresult PushCocoa(NSWindow *aWindow, NSModalSession aSession);
-  
-  nsresult PopCocoa(NSWindow *aWindow, NSModalSession aSession);
-  
-  nsresult PushGecko(NSWindow *aWindow, nsCocoaWindow *aWidget);
-  
-  nsresult PopGecko(NSWindow *aWindow, nsCocoaWindow *aWidget);
-  
-  NSModalSession CurrentSession();
-  
-  bool GeckoModalAboveCocoaModal();
-private:
-  nsTArray<nsCocoaAppModalWindowListItem> mList;
-};
-
 
 
 
@@ -82,8 +52,6 @@ protected:
   virtual void ScheduleNativeEventCallback();
   virtual bool ProcessNextNativeEvent(bool aMayWait);
 
-  bool InGeckoMainEventLoop();
-
   static void ProcessGeckoEvents(void* aInfo);
 
 protected:
@@ -99,17 +67,6 @@ protected:
   bool               mSkippedNativeCallback;
   bool               mRunningCocoaEmbedded;
 
-  
-  
-  uint32_t               mHadMoreEventsCount;
-  
-  
-  
-  
-  
-  static const uint32_t  kHadMoreEventsCountMax = 3;
-
-  int32_t            mRecursionDepth;
   int32_t            mNativeEventCallbackDepth;
   
   int32_t            mNativeEventScheduledDepth;
