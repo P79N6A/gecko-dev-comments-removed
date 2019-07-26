60ab56d85614367761cc4925f8a6e9cb12014ddd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:25:24 +0000

    Bug 847863 - Part 7 of 8 - Convert Downloads Panel tests. r=enn
    UltraBlame original commit: a63da361fb7a3a0eeeb083f5f7e45fef17764253

--- browser/components/downloads/test/browser/head.js
+++ browser/components/downloads/test/browser/head.js
@@ -10,10 +10,16 @@
 
 
 
-XPCOMUtils.defineLazyModuleGetter(this, "FileUtils",
-                                  "resource://gre/modules/FileUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "Downloads",
+                                  "resource://gre/modules/Downloads.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "DownloadsCommon",
                                   "resource:///modules/DownloadsCommon.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "FileUtils",
+                                  "resource://gre/modules/FileUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "Promise",
+                                  "resource://gre/modules/Promise.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "Task",
+                                  "resource://gre/modules/Task.jsm");
 const nsIDM = Ci.nsIDownloadManager;
 
 let gTestTargetFile = FileUtils.getFile("TmpD", ["dm-ui-test.file"]);
@@ -25,250 +31,82 @@ registerCleanupFunction(function () {
 
 
 
-let gDownloadRowTemplate = {
-  name: "test-download.txt",
-  source: "http://www.example.com/test-download.txt",
-  target: NetUtil.newURI(gTestTargetFile).spec,
-  startTime: 1180493839859230,
-  endTime: 1180493839859234,
-  state: nsIDM.DOWNLOAD_FINISHED,
-  currBytes: 0,
-  maxBytes: -1,
-  preferredAction: 0,
-  autoResume: 0
-};
-
-
-
-
-
 function test()
 {
-  testRunner.runTest(this.gen_test);
+  waitForExplicitFinish();
+  Task.spawn(test_task).then(null, ex => ok(false, ex)).then(finish);
 }
 
 
 
 
-
-
-
-var testRunner = {
-  _testIterator: null,
-  _lastEventResult: undefined,
-  _testRunning: false,
-  _eventRaised: false,
-
-  
-
-  
-
-
-
-
-
-
-
-
-
-
-  runTest: function TR_runTest(aGenerator) {
-    waitForExplicitFinish();
-    testRunner._testIterator = aGenerator();
-    testRunner.continueTest();
-  },
-
-  
-
-
-
-
-
-  continueTest: function TR_continueTest(aEventResult) {
-    
-    testRunner._lastEventResult = aEventResult;
-
-    
-    if (testRunner._testRunning) {
-      testRunner._eventRaised = true;
-      return;
-    }
-
-    
-    testRunner._testRunning = true;
-    try {
-      do {
-        
-        
-        testRunner._eventRaised = false;
-        testRunner._testIterator.send(testRunner._lastEventResult);
-      } while (testRunner._eventRaised);
-    }
-    catch (e) {
-      
-      
-      
-      if (!(e instanceof StopIteration))
-        ok(false, e);
-      
-      finish();
-    }
-
-    
-    testRunner._testRunning = false;
-  }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-function gen_resetState(aData)
+function promiseFocus()
 {
-  let statement = Services.downloads.DBConnection.createAsyncStatement(
-                  "DELETE FROM moz_downloads");
-  try {
-    statement.executeAsync({
-      handleResult: function(aResultSet) { },
-      handleError: function(aError)
-      {
-        Cu.reportError(aError);
-      },
-      handleCompletion: function(aReason)
-      {
-        testRunner.continueTest();
-      }
-    });
-    yield undefined;
-  } finally {
-    statement.finalize();
+  let deferred = Promise.defer();
+  waitForFocus(deferred.resolve);
+  return deferred.promise;
+}
+
+function promisePanelOpened()
+{
+  let deferred = Promise.defer();
+
+  
+  let originalOnPopupShown = DownloadsPanel.onPopupShown;
+  DownloadsPanel.onPopupShown = function () {
+    DownloadsPanel.onPopupShown = originalOnPopupShown;
+    originalOnPopupShown.apply(this, arguments);
+
+    
+    
+    setTimeout(deferred.resolve, 0);
+  };
+
+  return deferred.promise;
+}
+
+function task_resetState()
+{
+  
+  let publicList = yield Downloads.getPublicDownloadList();
+  let downloads = yield publicList.getAll();
+  for (let download of downloads) {
+    publicList.remove(download);
+    yield download.finalize(true);
   }
 
   
   Services.prefs.clearUserPref("browser.download.panel.shown");
 
-  
-  aData.clear();
-  aData._loadState = aData.kLoadNone;
   DownloadsPanel.hidePanel();
 
-  
-  waitForFocus(testRunner.continueTest);
-  yield undefined;
+  yield promiseFocus();
 }
 
-function gen_addDownloadRows(aDataRows)
+function task_addDownloads(aItems)
 {
-  let columnNames = Object.keys(gDownloadRowTemplate).join(", ");
-  let parameterNames = Object.keys(gDownloadRowTemplate)
-                             .map(function(n) ":" + n)
-                             .join(", ");
-  let statement = Services.downloads.DBConnection.createAsyncStatement(
-                  "INSERT INTO moz_downloads (" + columnNames +
-                  ", guid) VALUES(" + parameterNames + ", GENERATE_GUID())");
-  try {
-    
-    for (let i = aDataRows.length - 1; i >= 0; i--) {
-      let dataRow = aDataRows[i];
+  let startTimeMs = Date.now();
 
-      
-      for (let columnName in gDownloadRowTemplate) {
-        if (!(columnName in dataRow)) {
-          
-          
-          dataRow[columnName] = gDownloadRowTemplate[columnName];
-        }
-        statement.params[columnName] = dataRow[columnName];
-      }
-
-      
-      statement.executeAsync({
-        handleResult: function(aResultSet) { },
-        handleError: function(aError)
-        {
-          Cu.reportError(aError.message + " (Result = " + aError.result + ")");
-        },
-        handleCompletion: function(aReason)
-        {
-          testRunner.continueTest();
-        }
-      });
-      yield undefined;
-
-      
-      
-      
-      gDownloadRowTemplate.startTime++;
-      gDownloadRowTemplate.endTime++;
-    }
-  } finally {
-    statement.finalize();
+  let publicList = yield Downloads.getPublicDownloadList();
+  for (let item of aItems) {
+    publicList.add(yield Downloads.createDownload({
+      source: "http://www.example.com/test-download.txt",
+      target: gTestTargetFile,
+      succeeded: item.state == nsIDM.DOWNLOAD_FINISHED,
+      canceled: item.state == nsIDM.DOWNLOAD_CANCELED ||
+                item.state == nsIDM.DOWNLOAD_PAUSED,
+      error: item.state == nsIDM.DOWNLOAD_FAILED ? new Error("Failed.") : null,
+      hasPartialData: item.state == nsIDM.DOWNLOAD_PAUSED,
+      startTime: new Date(startTimeMs++),
+    }));
   }
 }
 
-function gen_openPanel(aData)
+function task_openPanel()
 {
-  
-  let originalOnViewLoadCompleted = DownloadsPanel.onViewLoadCompleted;
-  DownloadsPanel.onViewLoadCompleted = function () {
-    DownloadsPanel.onViewLoadCompleted = originalOnViewLoadCompleted;
-    originalOnViewLoadCompleted.apply(this);
-    testRunner.continueTest();
-  };
+  yield promiseFocus();
 
-  
-  aData.ensurePersistentDataLoaded(false);
-
-  
-  waitForFocus(testRunner.continueTest);
-  yield undefined;
-
-  
+  let promise = promisePanelOpened();
   DownloadsPanel.showPanel();
-  yield undefined;
-}
-
-
-
-
-
-
-
-
-
-function waitFor(aSeconds)
-{
-  setTimeout(function() {
-    testRunner.continueTest();
-  }, aSeconds * 1000);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-function prepareForPanelOpen()
-{
-  
-  let originalOnPopupShown = DownloadsPanel.onPopupShown;
-  DownloadsPanel.onPopupShown = function (aEvent) {
-    DownloadsPanel.onPopupShown = originalOnPopupShown;
-    DownloadsPanel.onPopupShown.apply(this, [aEvent]);
-    testRunner.continueTest();
-  };
+  yield promise;
 }
