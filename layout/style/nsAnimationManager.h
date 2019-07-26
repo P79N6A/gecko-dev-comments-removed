4a85f99926d69eb4316f80df518a85e34e6e2e61
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:26:50 +0000

    Bug 880596 part 5 - Make ElementAnimation::HasAnimationOfProperty no longer virtual; r=dbaron
    ElementAnimation::HasAnimationOfProperty doesn't seem to be overridden anywhere.
    I suspect it was a copy-paste mistake because the methods of the same name on
    ElementAnimations, ElementTransitions, and CommonElementAnimationData are
    virtual.
    
    UltraBlame original commit: 762aa62425b91b71654ea89c56d19077a5147915

--- layout/style/nsAnimationManager.h
+++ layout/style/nsAnimationManager.h
@@ -92,7 +92,7 @@ struct ElementAnimation
     return mPlayState == NS_STYLE_ANIMATION_PLAY_STATE_PAUSED;
   }
 
-  virtual bool HasAnimationOfProperty(nsCSSProperty aProperty) const;
+  bool HasAnimationOfProperty(nsCSSProperty aProperty) const;
   bool IsRunningAt(mozilla::TimeStamp aTime) const;
 
   
