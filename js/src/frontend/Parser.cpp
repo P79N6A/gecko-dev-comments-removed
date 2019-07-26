c99b1302c6cb657c0462b575fc2cd4d4a0988195
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:19:34 +0000

    Bug 861841 - Add an entry in script bindings for 'arguments' when used within a 'with' statement, r=luke.
    UltraBlame original commit: 584da23482ffe699e345af42881116dea8411505

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -2493,7 +2493,11 @@ Parser<ParseHandler>::bindVarOrConst(JSContext *cx, BindData<ParseHandler> *data
 
 
 
-            if (name == cx->names().arguments) {
+            HandlePropertyName arguments = cx->names().arguments;
+            if (name == arguments) {
+                Node pn = parser->handler.newName(arguments, pc);
+                if (!pc->define(parser->context, arguments, pn, Definition::VAR))
+                    return false;
                 funbox->setArgumentsHasLocalBinding();
                 funbox->setDefinitelyNeedsArgsObj();
             }
