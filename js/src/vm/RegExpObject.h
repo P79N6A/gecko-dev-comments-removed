dce70481d9016b60022485dead4ed596eebfd348
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:52:32 +0000

    Bug 965712: Part 1 - Use our string matching logic for regexps when possible, r=luke
    UltraBlame original commit: 7e5dc2e26dd3ad42a0e03ed9e5cf5c0646d005c3

--- js/src/vm/RegExpObject.h
+++ js/src/vm/RegExpObject.h
@@ -145,6 +145,7 @@ class RegExpShared
 
     RegExpFlag         flags;
     unsigned           parenCount;
+    bool               canStringMatch;
 
 #if ENABLE_YARR_JIT
     
@@ -204,7 +205,11 @@ class RegExpShared
 
     
 
-    size_t getParenCount() const        { JS_ASSERT(isCompiled()); return parenCount; }
+    size_t getParenCount() const {
+        JS_ASSERT(isCompiled() || canStringMatch);
+        return parenCount;
+    }
+
     void incRef()                       { activeUseCount++; }
     void decRef()                       { JS_ASSERT(activeUseCount > 0); activeUseCount--; }
 
