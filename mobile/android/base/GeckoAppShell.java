f9d2ae7e4df2b4d1cbd1c4589b144fa58345e732
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:07:19 +0000

    Bug 827347 - Allow force-downloading and installing of updates from about:firefox. r=snorp,blassey
    UltraBlame original commit: 0081c622b8390a6ab7730810ac6485c632f58b6d

--- mobile/android/base/GeckoAppShell.java
+++ mobile/android/base/GeckoAppShell.java
@@ -2155,11 +2155,6 @@ public class GeckoAppShell
         ((SurfaceTexture)surfaceTexture).setOnFrameAvailableListener(null);
     }
 
-    public static void notifyCheckUpdateResult(boolean result) {
-        if (GeckoApp.mAppContext != null)
-            GeckoApp.mAppContext.notifyCheckUpdateResult(result);
-    }
-
     public static boolean unlockProfile() {
         
         GeckoAppShell.killAnyZombies();
