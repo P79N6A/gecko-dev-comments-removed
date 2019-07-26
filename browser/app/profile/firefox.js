92148ab125239ce5e0ce8ccddc07d63b31e76d24
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:55:28 +0000

    Bug 1000459 - revert bug 993581 to turn directory tiles on [r=adw] Undo hardcoding of empty object.
    UltraBlame original commit: 7dfffba2e3aad6e4148a3fafded84b80f080ed1f

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1452,7 +1452,7 @@ pref("browser.newtabpage.rows", 3);
 
 pref("browser.newtabpage.columns", 3);
 
-pref("browser.newtabpage.directorySource", "data:application/json,{}");
+pref("browser.newtabpage.directorySource", "chrome://global/content/directoryLinks.json");
 
 
 pref("full-screen-api.enabled", true);
