6c21f6776ed43c46244fd8e4f0ae62cd48e7ef02
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:04:16 +0000

    Backed out changeset 8dd41701dd92 (bug 989921) on a CLOSED TREE
    UltraBlame original commit: d1b03ddc31613b0090c1815bfaeaa49215254287

--- content/media/AudioMixer.h
+++ content/media/AudioMixer.h
@@ -9,17 +9,13 @@
 #include "AudioSampleFormat.h"
 #include "nsTArray.h"
 #include "mozilla/PodOperations.h"
-#include "mozilla/LinkedList.h"
 
 namespace mozilla {
-
-struct MixerCallbackReceiver {
-  virtual void MixerCallback(AudioDataValue* aMixedBuffer,
-                             AudioSampleFormat aFormat,
-                             uint32_t aChannels,
-                             uint32_t aFrames,
-                             uint32_t aSampleRate) = 0;
-};
+typedef void(*MixerFunc)(AudioDataValue* aMixedBuffer,
+                         AudioSampleFormat aFormat,
+                         uint32_t aChannels,
+                         uint32_t aFrames,
+                         uint32_t aSampleRate);
 
 
 
@@ -36,29 +32,21 @@ struct MixerCallbackReceiver {
 class AudioMixer
 {
 public:
-  AudioMixer()
-    : mFrames(0),
+  AudioMixer(MixerFunc aCallback)
+    : mCallback(aCallback),
+      mFrames(0),
       mChannels(0),
       mSampleRate(0)
   { }
 
-  ~AudioMixer()
-  {
-    mCallbacks.clear();
-  }
-
   
 
   void FinishMixing() {
-    MOZ_ASSERT(mChannels && mFrames && mSampleRate, "Mix not called for this cycle?");
-    for (MixerCallback* cb = mCallbacks.getFirst();
-         cb != nullptr; cb = cb->getNext()) {
-      cb->mReceiver->MixerCallback(mMixedAudio.Elements(),
-                                   AudioSampleTypeToFormat<AudioDataValue>::Format,
-                                   mChannels,
-                                   mFrames,
-                                   mSampleRate);
-    }
+    mCallback(mMixedAudio.Elements(),
+              AudioSampleTypeToFormat<AudioDataValue>::Format,
+              mChannels,
+              mFrames,
+              mSampleRate);
     PodZero(mMixedAudio.Elements(), mMixedAudio.Length());
     mSampleRate = mChannels = mFrames = 0;
   }
@@ -83,21 +71,6 @@ public:
       mMixedAudio[i] += aSamples[i];
     }
   }
-
-  void AddCallback(MixerCallbackReceiver* aReceiver) {
-    mCallbacks.insertBack(new MixerCallback(aReceiver));
-  }
-
-  bool RemoveCallback(MixerCallbackReceiver* aReceiver) {
-    for (MixerCallback* cb = mCallbacks.getFirst();
-         cb != nullptr; cb = cb->getNext()) {
-      if (cb->mReceiver == aReceiver) {
-        cb->remove();
-        return true;
-      }
-    }
-    return false;
-  }
 private:
   void EnsureCapacityAndSilence() {
     if (mFrames * mChannels > mMixedAudio.Length()) {
@@ -106,17 +79,8 @@ private:
     PodZero(mMixedAudio.Elements(), mMixedAudio.Length());
   }
 
-  class MixerCallback : public LinkedListElement<MixerCallback>
-  {
-  public:
-    MixerCallback(MixerCallbackReceiver* aReceiver)
-      : mReceiver(aReceiver)
-    { }
-    MixerCallbackReceiver* mReceiver;
-  };
-
   
-  LinkedList<MixerCallback> mCallbacks;
+  MixerFunc mCallback;
   
   uint32_t mFrames;
   
