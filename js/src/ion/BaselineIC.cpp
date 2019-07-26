cc90fcdf31e0e0c6ebbd90c6a48c7b1223e4d70f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:11:24 +0000

    Bug 847425 - Handle OOM under GuardFunApplyArgumentsOptimization. r=djvj
    UltraBlame original commit: c0197fba12c9cb45170e47e0c650e03965ccdaab

--- js/src/ion/BaselineIC.cpp
+++ js/src/ion/BaselineIC.cpp
@@ -5434,8 +5434,10 @@ DoCallFallback(JSContext *cx, BaselineFrame *frame, ICCall_Fallback *stub, uint3
     Value *args = vp + 2;
 
     
-    if (op == JSOP_FUNAPPLY && argc == 2 && args[1].isMagic(JS_OPTIMIZED_ARGUMENTS))
-        GuardFunApplyArgumentsOptimization(cx, frame, callee, args, argc);
+    if (op == JSOP_FUNAPPLY && argc == 2 && args[1].isMagic(JS_OPTIMIZED_ARGUMENTS)) {
+        if (!GuardFunApplyArgumentsOptimization(cx, frame, callee, args, argc))
+            return false;
+    }
 
     
     bool constructing = (op == JSOP_NEW);
