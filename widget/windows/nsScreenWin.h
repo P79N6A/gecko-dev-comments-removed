1be970a80d527315b4335e38b4d72abd639c7a94
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:09:59 +0000

    bug 824386 - (win-hidpi) nsScreenWin needs to account for logical dpi (resolution scale factor). r=jimm
    UltraBlame original commit: d37c97ba174f73b552831396b5a08c3c8edf0470

--- widget/windows/nsScreenWin.h
+++ widget/windows/nsScreenWin.h
@@ -17,8 +17,17 @@ public:
   nsScreenWin ( HMONITOR inScreen );
   ~nsScreenWin();
 
+  
   NS_IMETHOD GetRect(int32_t* aLeft, int32_t* aTop, int32_t* aWidth, int32_t* aHeight);
   NS_IMETHOD GetAvailRect(int32_t* aLeft, int32_t* aTop, int32_t* aWidth, int32_t* aHeight);
+
+  
+  
+  NS_IMETHOD GetRectDisplayPix(int32_t *outLeft,  int32_t *outTop,
+                               int32_t *outWidth, int32_t *outHeight);
+  NS_IMETHOD GetAvailRectDisplayPix(int32_t *outLeft,  int32_t *outTop,
+                                    int32_t *outWidth, int32_t *outHeight);
+
   NS_IMETHOD GetPixelDepth(int32_t* aPixelDepth);
   NS_IMETHOD GetColorDepth(int32_t* aColorDepth);
 
