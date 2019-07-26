7a2443a525106e72ab6a273f78f99028657fd752
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:58:27 +0000

    Bug 839628 - Add MIN_UNDEQUEUED_BUFFERS check in GonkNativeWindow::dequeueBuffer(). r=kchen, r=mikeh
    UltraBlame original commit: ceb97502eb31cb1a9f19e43edf33af570109b73a

--- dom/camera/GonkNativeWindow.cpp
+++ dom/camera/GonkNativeWindow.cpp
@@ -263,6 +263,7 @@ int GonkNativeWindow::dequeueBuffer(android_native_buffer_t** buffer)
 
         int found = -1;
         int dequeuedCount = 0;
+        int renderingCount = 0;
         bool tryAgain = true;
 
         CNW_LOGD("dequeueBuffer: E");
@@ -274,10 +275,14 @@ int GonkNativeWindow::dequeueBuffer(android_native_buffer_t** buffer)
             
             found = INVALID_BUFFER_SLOT;
             dequeuedCount = 0;
+            renderingCount = 0;
             for (int i = 0; i < mBufferCount; i++) {
                 const int state = mSlots[i].mBufferState;
                 if (state == BufferSlot::DEQUEUED) {
                     dequeuedCount++;
+                }
+                else if (state == BufferSlot::RENDERING) {
+                    renderingCount++;
                 }
                 else if (state == BufferSlot::FREE) {
                     
@@ -289,6 +294,22 @@ int GonkNativeWindow::dequeueBuffer(android_native_buffer_t** buffer)
                         mSlots[i].mFrameNumber < mSlots[found].mFrameNumber) {
                         found = i;
                     }
+                }
+            }
+
+            
+            
+            
+            if (renderingCount > 0) {
+                
+                
+                const int avail = mBufferCount - (dequeuedCount + 1);
+                if (avail < MIN_UNDEQUEUED_BUFFERS) {
+                    CNW_LOGE("dequeueBuffer: MIN_UNDEQUEUED_BUFFERS=%d exceeded "
+                            "(dequeued=%d)",
+                            MIN_UNDEQUEUED_BUFFERS,
+                            dequeuedCount);
+                    return -EBUSY;
                 }
             }
 
