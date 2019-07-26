00bec0f1eb35bd67afff3c75d1c2139d3c807559
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:26:20 +0000

    Bug 894658 - Implement ES6 Array.prototype.{keys,entries}. r=jorendorff
    UltraBlame original commit: 31e1a183f9c1de4ddcb5f531c92217d9cc46ab67

--- js/src/jit-test/tests/for-of/array-iterator-null.js
+++ js/src/jit-test/tests/for-of/array-iterator-null.js
@@ -6,4 +6,6 @@ load(libdir + "iteration.js");
 for (var v of [undefined, null]) {
     
     assertThrowsInstanceOf(function () { Array.prototype[std_iterator].call(v); }, TypeError);
+    assertThrowsInstanceOf(function () { Array.prototype.keys.call(v); }, TypeError);
+    assertThrowsInstanceOf(function () { Array.prototype.entries.call(v); }, TypeError);
 }
