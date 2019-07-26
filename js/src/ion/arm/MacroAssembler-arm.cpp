83d663ae48ea6b46cd151271317dcc9e703af217
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:04:27 +0000

    Qualcomm's snapdragon does not interpret "push sp" as a valid instruction, so don't emit it. (bug 791462, r=dvander)
    UltraBlame original commit: 9ae014a115b2dd3cd0864052e0e18a7f03df6677

--- js/src/ion/arm/MacroAssembler-arm.cpp
+++ js/src/ion/arm/MacroAssembler-arm.cpp
@@ -1031,6 +1031,11 @@ MacroAssemblerARM::ma_pop(Register r)
 void
 MacroAssemblerARM::ma_push(Register r)
 {
+    
+    if (r == sp) {
+        ma_mov(sp, ScratchRegister);
+        r = ScratchRegister;
+    }
     ma_dtr(IsStore, sp,Imm32(-4), r, PreIndex);
 }
 
