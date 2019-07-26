d7ddb8b382b8aa05341010f151766a1fa16f7af6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:50:55 +0000

    Bug 834266 - Use XPCNativeWrapper to compare elements, r=mdas
    UltraBlame original commit: 92970e076a4dfd9925cf01f57c66d86af50e0021

--- testing/marionette/marionette-listener.js
+++ testing/marionette/marionette-listener.js
@@ -1162,9 +1162,16 @@ function switchToFrame(msg) {
   }
   if (msg.json.element != undefined) {
     if (elementManager.seenItems[msg.json.element] != undefined) {
-      let wantedFrame = elementManager.getKnownElement(msg.json.element, curWindow); 
+      let wantedFrame;
+      try {
+        wantedFrame = elementManager.getKnownElement(msg.json.element, curWindow); 
+      }
+      catch(e) {
+        sendError(e.message, e.code, e.stack, command_id);
+      }
       for (let i = 0; i < frames.length; i++) {
-        if (frames[i].isEqualNode(wantedFrame)) {
+        
+        if (XPCNativeWrapper(frames[i]) == XPCNativeWrapper(wantedFrame)) {
           curWindow = frames[i]; 
           foundFrame = i;
         }
