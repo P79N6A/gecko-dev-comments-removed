244d7faaf4697f22c5730f7b89f23a124ee44726
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:04:29 +0000

    Bug 919265 - Part 2: Add ClearOnShutdown() call on the static WebVTTParserWrapper. r=bz
    UltraBlame original commit: 07e358bb9954fb6ab0f7a6ba66223e0aeb16ae2b

--- content/media/TextTrackCue.cpp
+++ content/media/TextTrackCue.cpp
@@ -8,6 +8,7 @@
 #include "nsIFrame.h"
 #include "nsVideoFrame.h"
 #include "nsComponentManagerUtils.h"
+#include "mozilla/ClearOnShutdown.h"
 
 
 #define WEBVTT_AUTO -1
@@ -162,6 +163,7 @@ TextTrackCue::GetCueAsHTML()
       return mDocument->CreateDocumentFragment();
     }
     sParserWrapper = parserWrapper;
+    ClearOnShutdown(&sParserWrapper);
   }
 
   nsPIDOMWindow* window = mDocument->GetWindow();
