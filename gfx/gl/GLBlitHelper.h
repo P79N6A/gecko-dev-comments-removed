6ac31a725fda05e9f21aa8587d487055643f95b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:52:31 +0000

    Bug 945023 - Extract CreateTextureXXX and friends from GLContext. r=bjacob
    UltraBlame original commit: 9d673e562fa695054a08f92e73f0773511845aed

--- gfx/gl/GLBlitHelper.h
+++ gfx/gl/GLBlitHelper.h
@@ -18,6 +18,49 @@ namespace gl {
 
 class GLContext;
 
+
+
+
+
+
+
+GLuint CreateTextureForOffscreen(GLContext* aGL, const GLFormats& aFormats,
+                                 const gfxIntSize& aSize);
+
+
+
+
+
+
+
+
+
+
+
+GLuint CreateTexture(GLContext* aGL, GLenum aInternalFormat, GLenum aFormat,
+                     GLenum aType, const gfxIntSize& aSize);
+
+
+
+
+
+
+GLuint CreateRenderbuffer(GLContext* aGL, GLenum aFormat, GLsizei aSamples,
+                          const gfxIntSize& aSize);
+
+
+
+
+
+
+
+void CreateRenderbuffersForOffscreen(GLContext* aGL, const GLFormats& aFormats,
+                                     const gfxIntSize& aSize, bool aMultisample,
+                                     GLuint* aColorMSRB, GLuint* aDepthRB,
+                                     GLuint* aStencilRB);
+
+
+
 class GLBlitHelper MOZ_FINAL
 {
     
