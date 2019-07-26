5dd5a1bf3752732345d2e4140ce0a8dee1cc8716
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:53:33 +0000

    Bug 1003808 part 1 - Clean up DOMPoint; r=ehsan
    UltraBlame original commit: 0318807177f30a59b9bfa1f8d4b67ad716978481

--- editor/libeditor/html/nsHTMLEditRules.cpp
+++ editor/libeditor/html/nsHTMLEditRules.cpp
@@ -6470,22 +6470,10 @@ nsHTMLEditRules::GetNodesFromPoint(::DOMPoint point,
                                    nsCOMArray<nsIDOMNode> &arrayOfNodes,
                                    bool dontTouchContent)
 {
-  nsresult res;
-
-  
-  nsCOMPtr<nsIDOMNode> node;
-  int32_t offset;
-  point.GetPoint(node, offset);
-  
-  
-  nsCOMPtr<nsINode> nativeNode = do_QueryInterface(node);
-  NS_ENSURE_STATE(nativeNode);
-  nsRefPtr<nsRange> range = new nsRange(nativeNode);
-  res = range->SetStart(node, offset);
+  NS_ENSURE_STATE(point.node);
+  nsRefPtr<nsRange> range = new nsRange(point.node);
+  nsresult res = range->SetStart(point.node, point.offset);
   NS_ENSURE_SUCCESS(res, res);
-  
-
-
   
   
   res = PromoteRange(range, operation);
