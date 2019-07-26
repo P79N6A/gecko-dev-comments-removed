c756b2c664d775152ba369218458fc61c8796772
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:16:53 +0000

    Bug 979880: Added basic view toggling utility in app router. r=dmose,r=ametaireau
    UltraBlame original commit: 16e055e25d64cd133e5b093d73897f481dfe9863

--- static/js/webapp.js
+++ static/js/webapp.js
@@ -8,11 +8,118 @@ var loop = loop || {};
 loop.webapp = (function() {
   "use strict";
 
+  var router;
+
+  
+
+
+  var BaseView = Backbone.View.extend({
+    hide: function() {
+      this.$el.hide();
+      return this;
+    },
+
+    show: function() {
+      this.$el.show();
+      return this;
+    }
+  });
+
+  
+
+
+  var HomeView = BaseView.extend({
+    el: "#home"
+  });
+
+  
+
+
+  var CallLauncherView = BaseView.extend({
+    el: "#call-launcher",
+
+    events: {
+      "click button": "launchCall"
+    },
+
+    initialize: function(options) {
+      options = options || {};
+      if (!options.token) {
+        throw new Error("missing required token");
+      }
+      this.token = options.token;
+    },
+
+    launchCall: function(event) {
+      event.preventDefault();
+      
+    }
+  });
+
+  
+
+
+  var CallView = BaseView.extend({
+    el: "#call"
+  });
+
+  
+
+
+
+
+  var Router = Backbone.Router.extend({
+    view: undefined,
+
+    routes: {
+        "": "home",
+        "call/:token": "call"
+    },
+
+    initialize: function() {
+      this.loadView(new HomeView());
+    },
+
+    
+
+
+
+
+    loadView : function(view) {
+      this.view && this.view.hide();
+      this.view = view.render().show();
+    },
+
+    
+
+
+    home: function() {
+      this.loadView(new HomeView());
+    },
+
+    
+
+
+
+
+    call: function(token) {
+      this.loadView(new CallLauncherView({token: token}));
+    }
+  });
+
+  
+
+
   function init() {
-    console.log("Loop app started.");
+    router = new Router();
+    Backbone.history.start();
   }
 
   return {
-    init: init
+    init: init,
+    BaseView: BaseView,
+    HomeView: HomeView,
+    Router: Router,
+    CallLauncherView: CallLauncherView
   };
 })();
--- test/webapp_test.js
+++ test/webapp_test.js
@@ -2,12 +2,42 @@
 
 
 
+
+
 var expect = chai.expect;
 
-describe("test", function() {
+describe("Router", function() {
   "use strict";
 
-  it("is true", function() {
-    expect(true).eql(true);
+  var router;
+
+  beforeEach(function() {
+    router = new loop.webapp.Router();
+  });
+
+  describe("#constructor", function() {
+    it("should define a default active view", function() {
+      expect(router.view).to.be.an.instanceOf(loop.webapp.HomeView);
+    });
+  });
+
+  describe("#loadView", function() {
+    it("should set the active view", function() {
+      router.loadView(new loop.webapp.CallLauncherView({token: "fake"}));
+
+      expect(router.view).to.be.an.instanceOf(loop.webapp.CallLauncherView);
+    });
+  });
+});
+
+describe("CallLauncherView", function() {
+  "use strict";
+
+  describe("#constructor", function() {
+    it("should require a token option", function() {
+      expect(function() {
+        new loop.webapp.CallLauncherView();
+      }).to.Throw(Error, /missing required token/);
+    });
   });
 });
