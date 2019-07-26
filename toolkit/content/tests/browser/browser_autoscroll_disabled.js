c6d9967a72fb9776439926f5c63e38e82165ea8e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:16:17 +0000

    Bug 984037 - Make sure we respect the general.autoScroll pref (r=felipe)
    UltraBlame original commit: 25d89e7774d9d23e0fda4c6a4bc4580560f0fc64

---
+++ toolkit/content/tests/browser/browser_autoscroll_disabled.js
@@ -0,0 +1,77 @@
+function test()
+{
+  const kPrefName_AutoScroll = "general.autoScroll";
+  Services.prefs.setBoolPref(kPrefName_AutoScroll, false);
+
+  var doc;
+
+  function startLoad(dataUri) {
+    gBrowser.selectedBrowser.addEventListener("pageshow", onLoad, false);
+    gBrowser.loadURI(dataUri);
+  }
+
+  function onLoad() {
+    gBrowser.selectedBrowser.removeEventListener("pageshow", onLoad, false);
+    waitForFocus(onFocus, content);
+  }
+
+  function onFocus() {
+    doc = gBrowser.contentDocument;
+    runChecks();
+  }
+
+  function endTest() {
+    
+    if (Services.prefs.prefHasUserValue(kPrefName_AutoScroll))
+      Services.prefs.clearUserPref(kPrefName_AutoScroll);
+
+    
+    gBrowser.addTab("about:blank");
+    gBrowser.removeCurrentTab();
+
+    
+    waitForFocus(finish);
+  }
+
+  waitForExplicitFinish();
+
+  let dataUri = 'data:text/html,<html><body id="i" style="overflow-y: scroll"><div style="height: 2000px"></div>\
+      <iframe id="iframe" style="display: none;"></iframe>\
+</body></html>';
+  startLoad(dataUri);
+
+  function runChecks() {
+    var elem = doc.getElementById('i');
+    
+    
+    var skipFrames = 1;
+    var checkScroll = function () {
+      if (skipFrames--) {
+        window.mozRequestAnimationFrame(checkScroll);
+        return;
+      }
+      ok(elem.scrollTop == 0, "element should not have scrolled vertically");
+      ok(elem.scrollLeft == 0, "element should not have scrolled horizontally");
+
+      endTest();
+    };
+    EventUtils.synthesizeMouse(elem, 50, 50, { button: 1 },
+                               gBrowser.contentWindow);
+
+    var iframe = gBrowser.contentDocument.getElementById("iframe");
+    var e = iframe.contentDocument.createEvent("pagetransition");
+    e.initPageTransitionEvent("pagehide", true, true, false);
+    iframe.contentDocument.dispatchEvent(e);
+    iframe.contentDocument.documentElement.dispatchEvent(e);
+
+    EventUtils.synthesizeMouse(elem, 100, 100,
+                               { type: "mousemove", clickCount: "0" },
+                               gBrowser.contentWindow);
+    
+
+
+
+
+    window.mozRequestAnimationFrame(checkScroll);
+  }
+}
