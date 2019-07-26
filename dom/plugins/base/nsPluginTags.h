b4f4103c474a0eca6b429d8cbfe7c404eeeb22f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:50:28 +0000

    Bug 882339 - Cache the blocklist state on plugin tags to avoid querying the blocklist service constantly, r=johns
    UltraBlame original commit: d3da96f774b36e34d900f584b5b82936fe5a72d5

--- dom/plugins/base/nsPluginTags.h
+++ dom/plugins/base/nsPluginTags.h
@@ -85,8 +85,14 @@ public:
   nsCString     mVersion;  
   int64_t       mLastModifiedTime;
   nsCOMPtr<nsITimer> mUnloadTimer;
+
+  uint32_t      GetBlocklistState();
+  void          InvalidateBlocklistState();
+
 private:
   nsCString     mNiceFileName; 
+  uint16_t      mCachedBlocklistState;
+  bool          mCachedBlocklistStateValid;
 
   void InitMime(const char* const* aMimeTypes,
                 const char* const* aMimeDescriptions,
