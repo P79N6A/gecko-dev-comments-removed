0ec353e4304be700f3cfc919a9f241694481e1ba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:23:07 +0000

    Bug 916135 - Part 2: Don't call OMXCodec::pause in emulator mode to prevent getting stuck in paused state of OMXCodec. r=cdouble
    UltraBlame original commit: 21c8ab0245dca830c13933dcce36f27a7a750447

--- content/media/omx/OmxDecoder.cpp
+++ content/media/omx/OmxDecoder.cpp
@@ -500,6 +500,13 @@ bool OmxDecoder::IsWaitingMediaResources()
   return false;
 }
 
+static bool isInEmulator()
+{
+  char propQemu[PROPERTY_VALUE_MAX];
+  property_get("ro.kernel.qemu", propQemu, "");
+  return !strncmp(propQemu, "1", 1);
+}
+
 bool OmxDecoder::AllocateMediaResources()
 {
   
@@ -524,9 +531,7 @@ bool OmxDecoder::AllocateMediaResources()
     
     int flags = kHardwareCodecsOnly;
 
-    char propQemu[PROPERTY_VALUE_MAX];
-    property_get("ro.kernel.qemu", propQemu, "");
-    if (!strncmp(propQemu, "1", 1)) {
+    if (isInEmulator()) {
       
       flags = 0;
     }
@@ -966,6 +971,16 @@ nsresult OmxDecoder::Play()
 
 void OmxDecoder::Pause()
 {
+  
+
+
+
+
+
+  if (isInEmulator()) {
+    return;
+  }
+
   if (mVideoPaused || mAudioPaused) {
     return;
   }
