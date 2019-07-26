f69f5b587d06a5fea81d657a0248993d7aea984e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:23:15 +0000

    Bug 958024 - Wrap last exception from Marionette's Wait in TimeoutException. r=davehunt, r=mdas
    UltraBlame original commit: 3c5f276268e8fc64783c60eb80cc1bfef1633101

---
+++ testing/marionette/client/marionette/tests/unit/test_errors.py
@@ -0,0 +1,46 @@
+
+
+
+
+import sys
+
+import errors
+import marionette_test
+
+from errors import ErrorCodes
+
+def fake_cause():
+    try:
+        raise ValueError("bar")
+    except ValueError as e:
+        return sys.exc_info()
+
+message = "foo"
+status = ErrorCodes.TIMEOUT
+cause = fake_cause()
+stacktrace = "first\nsecond"
+
+class TestMarionetteException(marionette_test.MarionetteTestCase):
+    def test_defaults(self):
+        exc = errors.MarionetteException()
+        self.assertIsNone(exc.msg)
+        self.assertEquals(exc.status, ErrorCodes.MARIONETTE_ERROR)
+        self.assertIsNone(exc.cause)
+        self.assertIsNone(exc.stacktrace)
+
+    def test_construction(self):
+        exc = errors.MarionetteException(
+            message=message, status=status, cause=cause, stacktrace=stacktrace)
+        self.assertEquals(exc.msg, message)
+        self.assertEquals(exc.status, status)
+        self.assertEquals(exc.cause, cause)
+        self.assertEquals(exc.stacktrace, stacktrace)
+
+    def test_str(self):
+        exc = errors.MarionetteException(
+            message=message, status=status, cause=cause, stacktrace=stacktrace)
+        s = str(exc)
+        self.assertIn(message, s)
+        self.assertIn(", caused by %r" % cause[0], s)
+        self.assertIn("\nstacktrace:\n\tfirst\n\tsecond\n", s)
+        self.assertIn("MarionetteException:", s)
