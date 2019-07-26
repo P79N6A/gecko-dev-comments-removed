dbd241708b590a48f9c14a15e7c746ca4c3c3617
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:44:23 +0000

    Bug 962511 - Implement an eventlooplag actor. r=past
    UltraBlame original commit: 42053cfdc5ff19035c7a3c4eaa0d75779db1c3ed

---
+++ toolkit/devtools/server/tests/unit/test_eventlooplag_actor.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+
+
+"use strict";
+
+function run_test()
+{
+  let {EventLoopLagFront} = devtools.require("devtools/server/actors/eventlooplag");
+
+  DebuggerServer.init(function () { return true; });
+  DebuggerServer.addBrowserActors();
+
+  
+  let threshold = 20;
+  let interval = 10;
+
+
+  let front;
+  let client = new DebuggerClient(DebuggerServer.connectPipe());
+
+  
+  client.connect(function () {
+    client.listTabs(function(resp) {
+      front = new EventLoopLagFront(client, resp);
+      front.start().then(success => {
+        do_check_true(success);
+        front.once("event-loop-lag", gotLagEvent);
+        do_execute_soon(lag);
+      });
+    });
+  });
+
+  
+  function lag() {
+    let start = new Date();
+    let duration = threshold + interval + 1;
+    while (true) {
+      if (((new Date()) - start) > duration) {
+        break;
+      }
+    }
+  }
+
+  
+  
+  function gotLagEvent(time) {
+    do_print("lag: " + time);
+    do_check_true(time >= threshold);
+    front.stop().then(() => {
+      finishClient(client);
+    });
+  }
+
+  do_test_pending();
+}
