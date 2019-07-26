0b45ed46ada8835ff3849d2ab6f9e8e4aa17a101
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:28:56 +0000

    Bug 880118 - Allow GeckoView to be used as a view from a library project. r=blassey
    UltraBlame original commit: 59f669c61da5cff513832bc083714015f3c09448

---
+++ mobile/android/base/BaseGeckoInterface.java
@@ -0,0 +1,137 @@
+
+
+
+
+
+package org.mozilla.gecko;
+
+import org.mozilla.gecko.util.HardwareUtils;
+import org.mozilla.gecko.util.ThreadUtils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.RectF;
+import android.hardware.SensorEventListener;
+import android.location.LocationListener;
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AbsoluteLayout;
+
+public class BaseGeckoInterface implements GeckoAppShell.GeckoInterface {
+    
+    
+    
+    
+    
+
+    private Context mContext;
+    private GeckoProfile mProfile;
+
+    public BaseGeckoInterface(Context context) {
+        mContext = context;
+    }
+
+    public GeckoProfile getProfile() {
+        
+        if (mProfile == null) {
+            mProfile = GeckoProfile.get(mContext);
+        }
+        return mProfile;
+    }
+
+    
+    public PromptService getPromptService() {
+        return null;
+    }
+
+    public Activity getActivity() {
+        return (Activity)mContext;
+    }
+
+    public String getDefaultUAString() {
+        return HardwareUtils.isTablet() ? AppConstants.USER_AGENT_FENNEC_TABLET :
+                                          AppConstants.USER_AGENT_FENNEC_MOBILE;
+    }
+
+    
+    public LocationListener getLocationListener() {
+        return null;
+    }
+
+    
+    public SensorEventListener getSensorEventListener() {
+        return null;
+    }
+
+    
+    public void doRestart() {}
+
+    public void setFullScreen(final boolean fullscreen) {
+        ThreadUtils.postToUiThread(new Runnable() {
+            @Override
+            public void run() {
+                
+                Window window = ((Activity)mContext).getWindow();
+                window.setFlags(fullscreen ?
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN : 0,
+                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+                if (Build.VERSION.SDK_INT >= 11)
+                    window.getDecorView().setSystemUiVisibility(fullscreen ? 1 : 0);
+            }
+        });
+    }
+
+    
+    public void addPluginView(final View view, final RectF rect, final boolean isFullScreen) {}
+
+    
+    public void removePluginView(final View view, final boolean isFullScreen) {}
+
+    
+    public void enableCameraView() {}
+
+    
+    public void disableCameraView() {}
+
+    
+    public void addAppStateListener(GeckoAppShell.AppStateListener listener) {}
+
+    
+    public void removeAppStateListener(GeckoAppShell.AppStateListener listener) {}
+
+    
+    public View getCameraView() {
+        return null;
+    }
+
+    
+    public void notifyWakeLockChanged(String topic, String state) {}
+
+    
+    public FormAssistPopup getFormAssistPopup() {
+        return null;
+    }
+
+    public boolean areTabsShown() {
+        return false;
+    }
+
+    
+    public AbsoluteLayout getPluginContainer() {
+        return null;
+    }
+
+    public void notifyCheckUpdateResult(String result) {
+        GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("Update:CheckResult", result));
+    }
+
+    public boolean hasTabsSideBar() {
+        return false;
+    }
+
+    
+    public void invalidateOptionsMenu() {}
+}
