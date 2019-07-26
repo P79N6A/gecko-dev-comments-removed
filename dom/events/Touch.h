ecf9be430f56b6c7177181e3f615ee148fa5667f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:02:59 +0000

    Bug 975688 part.20 Rename nsDOMTouchEvent and nsDOMTouchList to mozilla::dom::TouchEvent and TouchList r=smaug
    UltraBlame original commit: 07ed9eef2248236c008b8c232df236b23a3f8312

--- dom/events/Touch.h
+++ dom/events/Touch.h
@@ -3,8 +3,8 @@
 
 
 
-#ifndef mozilla_dom_Touch_h
-#define mozilla_dom_Touch_h
+#ifndef mozilla_dom_Touch_h_
+#define mozilla_dom_Touch_h_
 
 #include "mozilla/Attributes.h"
 #include "mozilla/EventForwards.h"
@@ -27,7 +27,7 @@ class Touch MOZ_FINAL : public nsISupports
 public:
   static bool PrefEnabled(JSContext* aCx, JSObject* aGlobal);
 
-  Touch(mozilla::dom::EventTarget* aTarget,
+  Touch(EventTarget* aTarget,
         int32_t aIdentifier,
         int32_t aPageX,
         int32_t aPageY,
@@ -52,7 +52,7 @@ public:
 
   void InitializePoints(nsPresContext* aPresContext, WidgetEvent* aEvent);
 
-  void SetTarget(mozilla::dom::EventTarget* aTarget);
+  void SetTarget(EventTarget* aTarget);
 
   bool Equals(Touch* aTouch);
 
