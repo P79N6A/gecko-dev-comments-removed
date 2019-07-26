d688eb53429b8a729f8d485e111ede2032e8fec8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:09:03 +0000

    Bug 987995, part 4 - Add new crash reporter annotations for JS out-of-memory conditions. r=mccr8,r=bsmedberg.
    UltraBlame original commit: ca1f7ae987ee13a4040d575b8371944872ee86ca

--- xpcom/base/CycleCollectedJSRuntime.h
+++ xpcom/base/CycleCollectedJSRuntime.h
@@ -206,6 +206,41 @@ private:
 
   void FinalizeDeferredThings(DeferredFinalizeType aType);
 
+public:
+  
+  
+  MOZ_BEGIN_NESTED_ENUM_CLASS(OOMState, uint32_t)
+    
+    OK,
+
+    
+    
+    
+    
+    
+    
+    
+    Reporting,
+
+    
+    
+    
+    
+    
+    
+    
+    
+    Reported,
+
+    
+    
+    
+    
+    Recovered
+  MOZ_END_NESTED_ENUM_CLASS(OOMState)
+
+private:
+  void AnnotateAndSetOutOfMemory(OOMState *aStatePtr, OOMState aNewState);
   void OnGC(JSGCStatus aStatus);
   void OnOutOfMemory();
   void OnLargeAllocationFailure();
@@ -269,7 +304,12 @@ private:
   nsRefPtr<IncrementalFinalizeRunnable> mFinalizeRunnable;
 
   nsCOMPtr<nsIException> mPendingException;
+
+  OOMState mOutOfMemoryState;
+  OOMState mLargeAllocationFailureState;
 };
+
+MOZ_FINISH_NESTED_ENUM_CLASS(CycleCollectedJSRuntime::OOMState)
 
 } 
 
