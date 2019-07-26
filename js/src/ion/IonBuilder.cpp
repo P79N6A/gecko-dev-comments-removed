1b9ac33ae73277e5367a9593191d007927856763
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:18:50 +0000

    Bug 861165: IonMonkey: Correct behaviour for JSOP_IN on dense native with negative index, r=bhackett
    UltraBlame original commit: f01ec31f9f281bef244fdc144570365d84f8712f

--- js/src/ion/IonBuilder.cpp
+++ js/src/ion/IonBuilder.cpp
@@ -7394,7 +7394,7 @@ IonBuilder::jsop_in_dense()
     current->add(initLength);
 
     
-    MInArray *ins = MInArray::New(elements, id, initLength, needsHoleCheck);
+    MInArray *ins = MInArray::New(elements, id, initLength, obj, needsHoleCheck);
 
     current->add(ins);
     current->push(ins);
