41020a06c8467ebf80a91a16af05ac4105ad9209
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:53:16 +0000

    Bug 927685 - Enable --ion-parallel-compile=on by default in the shell; r=jandem,sr=jorendorff
    UltraBlame original commit: c14453ff87648f20be5b3805695d16257fd08212

--- js/src/tests/lib/tests.py
+++ js/src/tests/lib/tests.py
@@ -9,6 +9,17 @@ from threading import Thread
 
 from results import TestOutput
 
+
+TBPL_FLAGS = [
+    [], 
+    ['--ion-eager', '--ion-parallel-compile=off'], 
+    ['--ion-eager', '--ion-parallel-compile=off', '--ion-check-range-analysis', '--no-sse3'],
+    ['--baseline-eager'],
+    ['--baseline-eager', '--no-ti', '--no-fpu'],
+    ['--no-baseline', '--no-ion'],
+    ['--no-baseline', '--no-ion', '--no-ti'],
+]
+
 def do_run_cmd(cmd):
     l = [ None, None ]
     th_run_cmd(cmd, l)
