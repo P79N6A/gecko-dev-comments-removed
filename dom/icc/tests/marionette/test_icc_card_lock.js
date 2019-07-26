213ee66db7b3db1767442c64cc14bc7f58a44f1a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:12:16 +0000

    Bug 873380 - Part 3: RIL changes for icc card lock error. r=allstars.chh
    UltraBlame original commit: 365bc42758cd71e2c086cd44e27d58099acde782

--- dom/icc/tests/marionette/test_icc_card_lock.js
+++ dom/icc/tests/marionette/test_icc_card_lock.js
@@ -38,31 +38,12 @@ function testPinChangeFailed() {
 
   request.onerror = function onerror() {
     is(request.error.name, "IncorrectPassword");
+    is(request.error.lockType, "pin");
+    
+    is(request.error.retryCount, 2);
 
     resetPinRetries("0000", runNextTest);
   };
-}
-
-
-function testPinChangeFailedNotification() {
-  icc.addEventListener("icccardlockerror", function onicccardlockerror(result) {
-    icc.removeEventListener("icccardlockerror", onicccardlockerror);
-
-    is(result.lockType, "pin");
-    
-    is(result.retryCount, 2);
-
-    resetPinRetries("0000", runNextTest);
-  });
-
-  
-  let request = icc.setCardLock(
-    {lockType: "pin",
-     pin: "1111",
-     newPin: "0000"});
-
-  ok(request instanceof DOMRequest,
-     "request instanceof " + request.constructor);
 }
 
 
@@ -159,7 +140,6 @@ function testInvalidCardLockRetryCount() {
 
 let tests = [
   testPinChangeFailed,
-  testPinChangeFailedNotification,
   testPinChangeSuccess,
   testPinCardLockRetryCount,
   testPukCardLockRetryCount,
