205dfbe3b33fbd2b7844311c880e4237747841c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:10:00 +0000

    Bug 894941 - Patch the speex resampler to do runtime checks for SSE.  r=ehsan,glandium
    UltraBlame original commit: adeb3608cd4114359cc0eff753f704ed578deb11

---
+++ media/libspeex_resampler/src/sse_detect.h
@@ -0,0 +1,20 @@
+
+
+
+
+
+#ifndef SSE_DETECT
+#define SSE_DETECT
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+  int moz_has_sse2();
+  int moz_has_sse();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
