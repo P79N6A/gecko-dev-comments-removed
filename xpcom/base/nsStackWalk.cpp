d5313316985c1fd556a8b20d94593e34f887b21c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:17:00 +0000

    Bug 806209 - Prevent the Windows implementation of NS_StackWalk from accessing the stdio APIs in order to avoid deadlocks when profiling on Windows; r=BenWa
    UltraBlame original commit: 3b340e47ccbc95941ee4e542d2f55b6338fb772b

--- xpcom/base/nsStackWalk.cpp
+++ xpcom/base/nsStackWalk.cpp
@@ -306,7 +306,7 @@ WalkStackMain64(struct WalkStackData* data)
     
     memset(&context, 0, sizeof(CONTEXT));
     context.ContextFlags = CONTEXT_FULL;
-    if (!GetThreadContext(myThread, &context)) {
+    if (!GetThreadContext(myThread, &context) && data->walkCallingThread) {
         PrintError("GetThreadContext");
         return;
     }
@@ -365,7 +365,9 @@ WalkStackMain64(struct WalkStackData* data)
          } else {
             addr = 0;
             spaddr = 0;
-            PrintError("WalkStack64");
+            if (data->walkCallingThread) {
+                PrintError("WalkStack64");
+            }
         }
 
         if (!ok || (addr == 0)) {
@@ -474,13 +476,19 @@ NS_StackWalk(NS_WalkStackCallback aCallback, uint32_t aSkipFrames,
     }
 
     
+    
+    const bool shouldBeThreadSafe = !!aThread;
+
+    
     if (!myProcess) {
         if (!::DuplicateHandle(::GetCurrentProcess(),
                                ::GetCurrentProcess(),
                                ::GetCurrentProcess(),
                                &myProcess,
                                PROCESS_ALL_ACCESS, FALSE, 0)) {
-            PrintError("DuplicateHandle (process)");
+            if (!shouldBeThreadSafe) {
+                PrintError("DuplicateHandle (process)");
+            }
             return NS_ERROR_FAILURE;
         }
     }
@@ -489,7 +497,9 @@ NS_StackWalk(NS_WalkStackCallback aCallback, uint32_t aSkipFrames,
                            ::GetCurrentProcess(),
                            &myThread,
                            THREAD_ALL_ACCESS, FALSE, 0)) {
-        PrintError("DuplicateHandle (thread)");
+        if (!shouldBeThreadSafe) {
+            PrintError("DuplicateHandle (thread)");
+        }
         return NS_ERROR_FAILURE;
     }
 
@@ -529,7 +539,7 @@ NS_StackWalk(NS_WalkStackCallback aCallback, uint32_t aSkipFrames,
 
         walkerReturn = ::SignalObjectAndWait(data.eventStart,
                            data.eventEnd, INFINITE, FALSE);
-        if (walkerReturn != WAIT_OBJECT_0)
+        if (walkerReturn != WAIT_OBJECT_0 && !shouldBeThreadSafe)
             PrintError("SignalObjectAndWait (1)");
         if (data.pc_count > data.pc_size) {
             data.pcs = (void**) _alloca(data.pc_count * sizeof(void*));
@@ -541,7 +551,7 @@ NS_StackWalk(NS_WalkStackCallback aCallback, uint32_t aSkipFrames,
             ::PostThreadMessage(gStackWalkThread, WM_USER, 0, (LPARAM)&data);
             walkerReturn = ::SignalObjectAndWait(data.eventStart,
                                data.eventEnd, INFINITE, FALSE);
-            if (walkerReturn != WAIT_OBJECT_0)
+            if (walkerReturn != WAIT_OBJECT_0 && !shouldBeThreadSafe)
                 PrintError("SignalObjectAndWait (2)");
         }
 
