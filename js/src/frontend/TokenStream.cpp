7bb49ef5acda78bc3c72c51cc7491d1a7e736090
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:19:59 +0000

    Bug 930720 - SpiderMonkey: Fix a harmless off-by-one error. r=njn
    UltraBlame original commit: 253b0a696a1d843e736ad6cd19bfe8d7f54ba027

--- js/src/frontend/TokenStream.cpp
+++ js/src/frontend/TokenStream.cpp
@@ -1123,7 +1123,7 @@ TokenStream::getTokenInternal(Modifier modifier)
 
     
     
-    if (c1kind < OneChar_Max) {
+    if (c1kind <= OneChar_Max) {
         tp = newToken(-1);
         tp->type = TokenKind(c1kind);
         goto out;
