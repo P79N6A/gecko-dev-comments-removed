50471713e3c6de7fbb94f888ba035183b4bdf04d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:08:59 +0000

    Bug 1008338 - Add WorkerScriptExecutedSuccessfully(). r=bent
    UltraBlame original commit: 388f59954ef629d4bde6ca6953d503a81e3b4e17

--- dom/workers/WorkerPrivate.h
+++ dom/workers/WorkerPrivate.h
@@ -785,6 +785,7 @@ class WorkerPrivate : public WorkerPrivateParent<WorkerPrivate>
   bool mCancelAllPendingRunnables;
   bool mPeriodicGCTimerRunning;
   bool mIdleGCTimerRunning;
+  bool mWorkerScriptExecutedSuccessfully;
 
 #ifdef DEBUG
   PRThread* mPRThread;
@@ -1067,6 +1068,23 @@ public:
 #else
   { }
 #endif
+
+  void
+  SetWorkerScriptExecutedSuccessfully()
+  {
+    AssertIsOnWorkerThread();
+    
+    MOZ_ASSERT(!mWorkerScriptExecutedSuccessfully);
+    mWorkerScriptExecutedSuccessfully = true;
+  }
+
+  
+  bool
+  WorkerScriptExecutedSuccessfully() const
+  {
+    AssertIsOnWorkerThread();
+    return mWorkerScriptExecutedSuccessfully;
+  }
 
 private:
   WorkerPrivate(JSContext* aCx, WorkerPrivate* aParent,
