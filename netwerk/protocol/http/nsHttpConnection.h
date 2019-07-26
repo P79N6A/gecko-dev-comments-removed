ed7493dc3a149b0126629a02feedd6bf1f83ec26
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:47:39 +0000

    bug 904170 - telemetry for daily http data consumption r=jduell
    UltraBlame original commit: c93c3f52689cb870d71cfd3d59ba19c060c63150

--- netwerk/protocol/http/nsHttpConnection.h
+++ netwerk/protocol/http/nsHttpConnection.h
@@ -191,6 +191,9 @@ private:
     
     nsresult AddTransaction(nsAHttpTransaction *, int32_t);
 
+    
+    void ReportDataUsage(bool);
+
 private:
     nsCOMPtr<nsISocketTransport>    mSocketTransport;
     nsCOMPtr<nsIAsyncInputStream>   mSocketIn;
@@ -223,6 +226,10 @@ private:
     int64_t                         mMaxBytesRead;       
     int64_t                         mTotalBytesRead;     
     int64_t                         mTotalBytesWritten;  
+
+    
+    uint64_t                        mUnreportedBytesRead;     
+    uint64_t                        mUnreportedBytesWritten;  
 
     nsRefPtr<nsIAsyncInputStream>   mInputOverflow;
 
