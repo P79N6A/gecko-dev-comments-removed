0e757e7feec7559cad10e08513b4c9d094e2112d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:34:15 +0000

    Bug 899080 - Test fixup part 2 - dom/plugins. r=bsmedberg
    UltraBlame original commit: 3bcbfbc9591bda8c4de01d18371bc70cbe9a763f

--- dom/plugins/test/mochitest/utils.js
+++ dom/plugins/test/mochitest/utils.js
@@ -15,3 +15,30 @@ function paintCountIs(plugin, expected, msg) {
      " independent paints, expected " + realExpected + " logged paints, got " +
      count + " actual paints)");
 }
+
+function getTestPlugin(pluginName) {
+  var ph = SpecialPowers.Cc["@mozilla.org/plugin/host;1"]
+                                 .getService(SpecialPowers.Ci.nsIPluginHost);
+  var tags = ph.getPluginTags();
+  var name = pluginName || "Test Plug-in";
+  for (var tag of tags) {
+    if (tag.name == name) {
+      return tag;
+    }
+  }
+
+  ok(false, "Could not find plugin tag with plugin name '" + name + "'");
+  return null;
+}
+
+
+
+
+function setTestPluginEnabledState(newEnabledState, pluginName) {
+  var plugin = getTestPlugin(pluginName);
+  var oldEnabledState = plugin.enabledState;
+  plugin.enabledState = newEnabledState;
+  SimpleTest.registerCleanupFunction(function() {
+    getTestPlugin(pluginName).enabledState = oldEnabledState;
+  });
+}
