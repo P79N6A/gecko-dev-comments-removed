096b85ba8961d85a474dbbf0474bbfd4c7aebc25
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:18:46 +0000

    Backed out 2 changesets (bug 957652) because bug 957431 is being backed out
    Backed out changeset 92efd61cfca5 (bug 957652)
    Backed out changeset 9d405c64a7bf (bug 957652)
    
    UltraBlame original commit: 528cdf00642b3de878eebb28acb322eb0f3a5216

--- content/base/src/nsINode.cpp
+++ content/base/src/nsINode.cpp
@@ -774,10 +774,10 @@ nsINode::CompareDocumentPosition(nsINode& aOtherNode) const
   const nsINode *node1 = &aOtherNode, *node2 = this;
 
   
-  const Attr* attr1 = nullptr;
+  const nsIAttribute* attr1 = nullptr;
   if (node1->IsNodeOfType(nsINode::eATTRIBUTE)) {
-    attr1 = static_cast<const Attr*>(node1);
-    const nsIContent* elem = attr1->GetElement();
+    attr1 = static_cast<const nsIAttribute*>(node1);
+    const nsIContent* elem = attr1->GetContent();
     
     
     if (elem) {
@@ -786,8 +786,8 @@ nsINode::CompareDocumentPosition(nsINode& aOtherNode) const
     }
   }
   if (node2->IsNodeOfType(nsINode::eATTRIBUTE)) {
-    const Attr* attr2 = static_cast<const Attr*>(node2);
-    const nsIContent* elem = attr2->GetElement();
+    const nsIAttribute* attr2 = static_cast<const nsIAttribute*>(node2);
+    const nsIContent* elem = attr2->GetContent();
     if (elem == node1 && attr1) {
       
       
