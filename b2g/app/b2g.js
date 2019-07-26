717080893eb67e62a517dbea2a057fb12ffeac6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:37:55 +0000

    Backed out changeset 5aa4d9daa13c because it's about to fail some tests
    UltraBlame original commit: cd54f3557ae6203e8339fd9de4a74b3177cfd71a

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -367,8 +367,6 @@ pref("security.fileuri.strict_origin_policy", false);
 pref("security.apps.privileged.CSP.default", "default-src *; script-src 'self'; object-src 'none'; style-src 'self' 'unsafe-inline'");
 pref("security.apps.certified.CSP.default", "default-src *; script-src 'self'; object-src 'none'; style-src 'self'");
 
-pref("security.csp.reports.enable", false);
-
 
 
 
