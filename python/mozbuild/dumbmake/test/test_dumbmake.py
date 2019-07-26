f009e1487c5c7e424bca8f00c9fdf1f0d11a697c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:29:21 +0000

    Bug 677452 - Add smartmake-like functionality to |mach build DIR|. r=gps
    UltraBlame original commit: e5d90d0479ccd39c8f571d63300e8ecd16df7abc

---
+++ python/mozbuild/dumbmake/test/test_dumbmake.py
@@ -0,0 +1,108 @@
+
+
+
+from __future__ import unicode_literals
+
+import unittest
+
+from mozunit import (
+    main,
+)
+
+from dumbmake.dumbmake import (
+    add_extra_dependencies,
+    all_dependencies,
+    dependency_map,
+    indentation,
+)
+
+class TestDumbmake(unittest.TestCase):
+    def test_indentation(self):
+        self.assertEqual(indentation(""), 0)
+        self.assertEqual(indentation("x"), 0)
+        self.assertEqual(indentation(" x"), 1)
+        self.assertEqual(indentation("\tx"), 1)
+        self.assertEqual(indentation(" \tx"), 2)
+        self.assertEqual(indentation("\t x"), 2)
+        self.assertEqual(indentation(" x  "), 1)
+        self.assertEqual(indentation("\tx\t"), 1)
+        self.assertEqual(indentation("  x"), 2)
+        self.assertEqual(indentation("    x"), 4)
+
+    def test_dependency_map(self):
+        self.assertEqual(dependency_map([]), {})
+        self.assertEqual(dependency_map(["a"]), {"a": []})
+        self.assertEqual(dependency_map(["a", "b"]), {"a": [], "b": []})
+        self.assertEqual(dependency_map(["a", "b", "c"]), {"a": [], "b": [], "c": []})
+        
+        self.assertEqual(dependency_map(["a", "\tb", "a", "\tc"]), {"a": [], "b": ["a"], "c": ["a"]})
+        self.assertEqual(dependency_map(["a", "\tb", "\t\tc"]), {"a": [], "b": ["a"], "c": ["b", "a"]})
+        self.assertEqual(dependency_map(["a", "\tb", "\t\tc", "\td", "\te", "f"]), {"a": [], "b": ["a"], "c": ["b", "a"], "d": ["a"], "e": ["a"], "f": []})
+        
+        self.assertEqual(dependency_map(["\ta", "b"]), {"a": [], "b": []})
+        self.assertEqual(dependency_map(["a", "\t\t\tb", "\t\tc"]), {"a": [], "b": ["a"], "c": ["a"]})
+        self.assertEqual(dependency_map(["a", "\t\tb", "\t\t\tc", "\t\td", "\te", "f"]), {"a": [], "b": ["a"], "c": ["b", "a"], "d": ["a"], "e": ["a"], "f": []})
+        
+        self.assertEqual(dependency_map(["a", "\tb", "a", "\tb"]), {"a": [], "b": ["a"]})
+        self.assertEqual(dependency_map(["a", "\tb", "\t\tc", "b", "\td", "\t\te"]), {"a": [], "b": ["a"], "d": ["b"], "e": ["d", "b"], "c": ["b", "a"]})
+        
+        self.assertEqual(dependency_map(["a", "\tb", "\t\ta"]), {"a": ["b", "a"], "b": ["a"]})
+
+    def test_all_dependencies(self):
+        dm = {"a": [], "b": ["a"], "c": ["b", "a"], "d": ["a"], "e": ["a"], "f": []}
+        self.assertEqual(all_dependencies("a", dependency_map=dm), ["a"])
+        self.assertEqual(all_dependencies("b", dependency_map=dm), ["b", "a"])
+        self.assertEqual(all_dependencies("c", "a", "b", dependency_map=dm), ["c", "b", "a"])
+        self.assertEqual(all_dependencies("d", dependency_map=dm), ["d", "a"])
+        self.assertEqual(all_dependencies("d", "f", "c", dependency_map=dm), ["d", "f", "c", "b", "a"])
+        self.assertEqual(all_dependencies("a", "b", dependency_map=dm), ["b", "a"])
+        self.assertEqual(all_dependencies("b", "b", dependency_map=dm), ["b", "a"])
+
+    def test_missing_entry(self):
+        
+        dm = {"a": ["b"]}
+        self.assertEqual(all_dependencies("a", dependency_map=dm), ["a", "b"])
+        self.assertEqual(all_dependencies("a", "b", dependency_map=dm), ["a", "b"])
+        self.assertEqual(all_dependencies("b", dependency_map=dm), ["b"])
+
+    def test_two_dependencies(self):
+        dm = {"a": ["c"], "b": ["c"], "c": []}
+        
+        self.assertEqual(all_dependencies("a", "b", dependency_map=dm), ["a", "b", "c"])
+        
+        self.assertEqual(all_dependencies("b", "a", dependency_map=dm), ["b", "a", "c"])
+
+    def test_nested_dependencies(self):
+        
+        dm = {"a": ["b", "c", "d"], "b": ["c", "d"], "c": ["d"]}
+        self.assertEqual(all_dependencies("b", "a", dependency_map=dm), ["a", "b", "c", "d"])
+        self.assertEqual(all_dependencies("c", "a", dependency_map=dm), ["a", "b", "c", "d"])
+
+    def test_add_extra_dependencies(self):
+        
+        dm = {"a": ["b", "c", "d"], "b": ["c", "d"], "c": ["d"]}
+        
+        self.assertEqual(list(add_extra_dependencies([], dependency_map=dm)),
+                         [])
+        self.assertEqual(list(add_extra_dependencies([(None, "package")], dependency_map=dm)),
+                         [(None, "package")])
+        
+        self.assertEqual(list(add_extra_dependencies([("b", None)], dependency_map=dm)),
+                         [("b", None), ("c", None), ("d", None)])
+        
+        self.assertEqual(list(add_extra_dependencies([("b", None),
+                                                      (None, "package"),
+                                                      ("c", None)], dependency_map=dm)),
+                         [("b", None), ("c", None), ("d", None),
+                          (None, "package"),
+                          ("c", None), ("d", None)])
+        
+        self.assertEqual(list(add_extra_dependencies([("a", None), ("b", None),
+                                                      (None, "package"), (None, "install"),
+                                                      ("c", None), ("d", None)], dependency_map=dm)),
+                         [("a", None), ("b", None), ("c", None), ("d", None),
+                          (None, "package"), (None, "install"),
+                          ("c", None), ("d", None)])
+
+if __name__ == '__main__':
+    main()
