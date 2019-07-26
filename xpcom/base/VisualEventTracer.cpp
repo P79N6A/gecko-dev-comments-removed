63baee31ef6b77c2fa7df7342a008c8125e26e25
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:14:14 +0000

    Bug 729182 - Implement visual event tracer, part2 - event tracer service, r=ehsan
    UltraBlame original commit: 85dd7094b78d0cce60b28adab74b14b9536e0846

--- xpcom/base/VisualEventTracer.cpp
+++ xpcom/base/VisualEventTracer.cpp
@@ -19,7 +19,7 @@ namespace mozilla { namespace eventtracer {
 
 namespace {
 
-const uint32_t kBatchSize = 0x1000;
+const uint32_t kBatchSize = 256;
 const char kTypeChars[eventtracer::eLast] = {' ','N','S','W','E','D'};
 
 
@@ -27,29 +27,49 @@ mozilla::Monitor * gMonitor = nullptr;
 
 
 
-bool gInitialized = false;
+bool volatile gInitialized = false;
+
+
+bool volatile gCapture = false;
+
+
+mozilla::TimeStamp * gProfilerStart;
+
+
+mozilla::TimeDuration * gMaxBacklogTime;
+
 
 
 class Record {
 public:
   Record() 
     : mType(::mozilla::eventtracer::eNone)
-    , mTime(0)
     , mItem(nullptr)
     , mText(nullptr)
     , mText2(nullptr) 
   {
     MOZ_COUNT_CTOR(Record);
   } 
+
+  Record& operator=(const Record & aOther)
+  {
+    mType = aOther.mType;
+    mTime = aOther.mTime;
+    mItem = aOther.mItem;
+    mText = PL_strdup(aOther.mText);
+    mText2 = aOther.mText2 ? PL_strdup(aOther.mText2) : nullptr;
+    return *this;
+  }
+
   ~Record() 
   {
-    PL_strfree(mText); 
     PL_strfree(mText2);
+    PL_strfree(mText); 
     MOZ_COUNT_DTOR(Record);
   }
 
   uint32_t mType;
-  double mTime;
+  TimeStamp mTime;
   void * mItem;
   char * mText;
   char * mText2;
@@ -74,12 +94,14 @@ char * DupCurrentThreadName()
 
 class RecordBatch {
 public:
-  RecordBatch()
-    : mRecordsHead(new Record[kBatchSize])
-    , mRecordsTail(mRecordsHead + kBatchSize)
+  RecordBatch(size_t aLength = kBatchSize,
+              char * aThreadName = DupCurrentThreadName())
+    : mRecordsHead(new Record[aLength])
+    , mRecordsTail(mRecordsHead + aLength)
     , mNextRecord(mRecordsHead)
     , mNextBatch(nullptr)
-    , mThreadNameCopy(DupCurrentThreadName())
+    , mThreadNameCopy(aThreadName)
+    , mClosed(false)
   {
     MOZ_COUNT_CTOR(RecordBatch);
   }
@@ -91,41 +113,203 @@ public:
     MOZ_COUNT_DTOR(RecordBatch);
   }
 
-  static void FlushBatch(void * aData);
+  void Close() { mClosed = true; }
+
+  size_t Length() const { return mNextRecord - mRecordsHead; }
+  bool CanBeDeleted(const TimeStamp& aUntil) const;
+
+  static RecordBatch * Register();
+  static void Close(void * data); 
+  static RecordBatch * Clone(RecordBatch * aLog, const TimeStamp& aSince);
+  static void Delete(RecordBatch * aLog);
+
+  static RecordBatch * CloneLog();
+  static void GCLog(const TimeStamp& aUntil);
+  static void DeleteLog();
 
   Record * mRecordsHead;
   Record * mRecordsTail;
-  Record * mNextRecord;
+  Record * volatile mNextRecord;
 
   RecordBatch * mNextBatch;
   char * mThreadNameCopy;
+  bool mClosed;
 };
 
 
 
-RecordBatch * gLogHead = nullptr;
-RecordBatch * gLogTail = nullptr;
+RecordBatch * volatile gLogHead = nullptr;
+RecordBatch * volatile gLogTail = nullptr;
+
+
+
+RecordBatch *
+RecordBatch::Register()
+{
+  MonitorAutoLock mon(*gMonitor);
+
+  if (!gInitialized)
+    return nullptr;
+
+  if (gLogHead)
+    RecordBatch::GCLog(TimeStamp::Now() - *gMaxBacklogTime);
+
+  RecordBatch * batch = new RecordBatch();
+  if (!gLogHead)
+    gLogHead = batch;
+  else 
+    gLogTail->mNextBatch = batch;
+  gLogTail = batch;
+
+  mon.Notify();
+  return batch;
+}
+
+void
+RecordBatch::Close(void * data)
+{
+  RecordBatch * batch = static_cast<RecordBatch*>(data);
+  batch->Close();
+}
+
+
+RecordBatch *
+RecordBatch::Clone(RecordBatch * aOther, const TimeStamp& aSince)
+{
+  if (!aOther)
+    return nullptr;
+
+  size_t length = aOther->Length();
+  size_t min = 0;
+  size_t max = length;
+  Record * record = nullptr;
+
+  
+  size_t i;
+  while (min < max) {
+    i = (max + min) / 2;
+
+    record = aOther->mRecordsHead + i;
+    if (record->mTime >= aSince)
+      max = i;
+    else
+      min = i+1;
+  }
+  i = (max + min) / 2;
+
+  
+  size_t toCopy = length - i;
+  if (!toCopy)
+    return RecordBatch::Clone(aOther->mNextBatch, aSince);
+
+  
+  RecordBatch * clone = new RecordBatch(toCopy, PL_strdup(aOther->mThreadNameCopy));
+  for (; i < length; ++i) {
+    record = aOther->mRecordsHead + i;
+    *clone->mNextRecord = *record;
+    ++clone->mNextRecord;
+  }
+  clone->mNextBatch = RecordBatch::Clone(aOther->mNextBatch, aSince);
+
+  return clone;
+}
 
 
 void
-RecordBatch::FlushBatch(void * aData)
+RecordBatch::Delete(RecordBatch * aLog)
 {
-  RecordBatch * threadLogPrivate = static_cast<RecordBatch *>(aData);
+  while (aLog) {
+    RecordBatch * batch = aLog;
+    aLog = aLog->mNextBatch;
+    delete batch;
+  }
+}
+
+
+RecordBatch *
+RecordBatch::CloneLog()
+{
+  TimeStamp startEpoch = *gProfilerStart;
+  TimeStamp backlogEpoch = TimeStamp::Now() - *gMaxBacklogTime;
+
+  TimeStamp since = (startEpoch > backlogEpoch) ? startEpoch : backlogEpoch;
 
   MonitorAutoLock mon(*gMonitor);
 
-  if (!gInitialized) {
-    delete threadLogPrivate;
-    return;
+  return RecordBatch::Clone(gLogHead, since);
+}
+
+
+void
+RecordBatch::GCLog(const TimeStamp& aUntil)
+{
+  
+  gMonitor->AssertCurrentThreadOwns();
+
+  RecordBatch *volatile * referer = &gLogHead;
+  gLogTail = nullptr;
+
+  RecordBatch * batch = *referer;
+  while (batch) {
+    if (batch->CanBeDeleted(aUntil)) {
+      
+      
+      
+      *referer = batch->mNextBatch;
+      delete batch;
+      batch = *referer;
+    }
+    else {
+      
+      
+      gLogTail = batch;
+      
+      batch = batch->mNextBatch;
+      
+      
+      referer = &((*referer)->mNextBatch);
+    }
+  }
+}
+
+
+void
+RecordBatch::DeleteLog()
+{
+  RecordBatch * batch;
+  {
+    MonitorAutoLock mon(*gMonitor);
+    batch = gLogHead;
+    gLogHead = nullptr;
+    gLogTail = nullptr;
   }
 
-  if (!gLogHead)
-    gLogHead = threadLogPrivate;
-  else 
-    gLogTail->mNextBatch = threadLogPrivate;
-  gLogTail = threadLogPrivate;
+  RecordBatch::Delete(batch);
+}
 
-  mon.Notify();  
+bool
+RecordBatch::CanBeDeleted(const TimeStamp& aUntil) const
+{
+  if (mClosed) {
+    
+    
+    
+    
+
+    if (!Length()) {
+      
+      return true;
+    }
+
+    if ((mNextRecord-1)->mTime <= aUntil) {
+      
+      
+      return true;
+    }
+  }
+
+  
+  return false;
 }
 
 
@@ -203,111 +387,7 @@ bool gStopFlushingThread = false;
 
 
 EventFilter * gEventFilter = nullptr;
-const char * gLogFilePath = nullptr;
-PRThread * gFlushingThread = nullptr;
 unsigned gThreadPrivateIndex;
-mozilla::TimeStamp gProfilerStart;
-
-
-
-
-
-
-void FlushingThread(void * aArg)
-{
-  PRFileDesc * logFile = PR_Open(gLogFilePath, 
-                         PR_WRONLY | PR_TRUNCATE | PR_CREATE_FILE,
-                         0644);
-
-  MonitorAutoLock mon(*gMonitor);
-
-  if (!logFile) {
-    gInitialized = false;
-    return;
-  }
-
-  int32_t rv;
-  bool ioError = false;
-
-  const char logHead[] = "{\n\"version\": 1,\n\"records\":[\n";
-  rv = PR_Write(logFile, logHead, sizeof(logHead) - 1);
-  ioError |= (rv < 0);
-
-  bool firstBatch = true;
-  while (!gStopFlushingThread || gLogHead) {
-    if (ioError) {
-      gInitialized = false;
-      break;
-    }
-
-    mon.Wait();
-
-    
-    RecordBatch * batch = gLogHead;
-    gLogHead = nullptr;
-    gLogTail = nullptr;
-
-    MonitorAutoUnlock unlock(*gMonitor); 
-
-    while (batch) {
-      if (!firstBatch) {
-        const char threadDelimiter[] = ",\n";
-        rv = PR_Write(logFile, threadDelimiter, sizeof(threadDelimiter) - 1);
-        ioError |= (rv < 0);
-      }
-      firstBatch = false;
-
-      static const int kBufferSize = 2048;
-      char buf[kBufferSize];
-
-      PR_snprintf(buf, kBufferSize, "{\"thread\":\"%s\",\"log\":[\n", 
-                  batch->mThreadNameCopy);
-
-      rv = PR_Write(logFile, buf, strlen(buf));
-      ioError |= (rv < 0);
-
-      for (Record * record = batch->mRecordsHead;
-           record < batch->mNextRecord && !ioError;
-           ++record) {
-
-        
-        
-        
-        uint32_t type = record->mType & 0xffffUL;
-        uint32_t flags = record->mType >> 16;
-        PR_snprintf(buf, kBufferSize, 
-          "{\"e\":\"%c\",\"t\":%f,\"f\":%d,\"i\":\"%p\",\"n\":\"%s%s\"}%s\n",
-          kTypeChars[type],
-          record->mTime,
-          flags,
-          record->mItem,
-          record->mText,
-          record->mText2 ? record->mText2 : "",
-          (record == batch->mNextRecord - 1) ? "" : ",");
-
-        rv = PR_Write(logFile, buf, strlen(buf));
-        ioError |= (rv < 0);
-      }
-
-      const char threadTail[] = "]}\n";
-      rv = PR_Write(logFile, threadTail, sizeof(threadTail) - 1);
-      ioError |= (rv < 0);
-
-      RecordBatch * next = batch->mNextBatch;
-      delete batch;
-      batch = next;
-    }
-  }
-
-  const char logTail[] = "]}\n";
-  rv = PR_Write(logFile, logTail, sizeof(logTail) - 1);
-  ioError |= (rv < 0);
-
-  PR_Close(logFile);
-
-  if (ioError)
-    PR_Delete(gLogFilePath);
-}
 
 
 bool CheckEventFilters(uint32_t aType, void * aItem, const char * aText)
@@ -316,9 +396,6 @@ bool CheckEventFilters(uint32_t aType, void * aItem, const char * aText)
     return true;
 
   if (aType == eName)
-    return true;
-
-  if (aItem == gFlushingThread) 
     return true;
 
   return gEventFilter->EventPasses(aText);
@@ -332,20 +409,11 @@ bool CheckEventFilters(uint32_t aType, void * aItem, const char * aText)
 void Init()
 {
 #ifdef MOZ_VISUAL_EVENT_TRACER
-  const char * logFile = PR_GetEnv("MOZ_PROFILING_FILE");
-  if (!logFile || !*logFile)
-    return;
-
-  gLogFilePath = logFile;
-
   const char * logEvents = PR_GetEnv("MOZ_PROFILING_EVENTS");
   if (logEvents && *logEvents)
     gEventFilter = EventFilter::Build(logEvents);
 
-  gProfilerStart = mozilla::TimeStamp::Now();
-
-  PRStatus status = PR_NewThreadPrivateIndex(&gThreadPrivateIndex, 
-                                             &RecordBatch::FlushBatch);
+  PRStatus status = PR_NewThreadPrivateIndex(&gThreadPrivateIndex, &RecordBatch::Close);
   if (status != PR_SUCCESS)
     return;
 
@@ -353,20 +421,10 @@ void Init()
   if (!gMonitor)
     return;
 
-  gFlushingThread = PR_CreateThread(PR_USER_THREAD, 
-                                    &FlushingThread,
-                                    nullptr,
-                                    PR_PRIORITY_LOW,
-                                    PR_LOCAL_THREAD,
-                                    PR_JOINABLE_THREAD,
-                                    32768);
-  if (!gFlushingThread)
-    return;
-    
-  gInitialized = true;
+  gProfilerStart = new mozilla::TimeStamp();
+  gMaxBacklogTime = new mozilla::TimeDuration();
 
-  MOZ_EVENT_TRACER_NAME_OBJECT(gFlushingThread, "Profiler");
-  MOZ_EVENT_TRACER_MARK(gFlushingThread, "Profiling Start");
+  gInitialized = true;
 #endif
 }
 
@@ -374,25 +432,10 @@ void Init()
 void Shutdown()
 {
 #ifdef MOZ_VISUAL_EVENT_TRACER
-  MOZ_EVENT_TRACER_MARK(gFlushingThread, "Profiling End");
+  gCapture = false;
+  gInitialized = false;
 
-  
-  
-
-  
-  PR_SetThreadPrivate(gThreadPrivateIndex, nullptr);
-
-  if (gFlushingThread) {
-    {
-      MonitorAutoLock mon(*gMonitor);
-      gInitialized = false;
-      gStopFlushingThread = true;
-      mon.Notify();
-    }
-
-    PR_JoinThread(gFlushingThread);
-    gFlushingThread = nullptr;
-  }
+  RecordBatch::DeleteLog();
 
   if (gMonitor) {
     delete gMonitor;
@@ -403,6 +446,16 @@ void Shutdown()
     delete gEventFilter;
     gEventFilter = nullptr;
   }
+
+  if (gProfilerStart) {
+    delete gProfilerStart;
+    gProfilerStart = nullptr;
+  }
+
+  if (gMaxBacklogTime) {
+    delete gMaxBacklogTime;
+    gMaxBacklogTime = nullptr;
+  }
 #endif
 }
 
@@ -410,7 +463,7 @@ void Shutdown()
 void Mark(uint32_t aType, void * aItem, const char * aText, const char * aText2)
 {
 #ifdef MOZ_VISUAL_EVENT_TRACER
-  if (!gInitialized)
+  if (!gInitialized || !gCapture)
     return;
 
   if (aType == eNone)
@@ -422,14 +475,16 @@ void Mark(uint32_t aType, void * aItem, const char * aText, const char * aText2)
   RecordBatch * threadLogPrivate = static_cast<RecordBatch *>(
       PR_GetThreadPrivate(gThreadPrivateIndex));
   if (!threadLogPrivate) {
-    
-    threadLogPrivate = new RecordBatch();
+    threadLogPrivate = RecordBatch::Register();
+    if (!threadLogPrivate)
+      return;
+
     PR_SetThreadPrivate(gThreadPrivateIndex, threadLogPrivate);
   }
 
   Record * record = threadLogPrivate->mNextRecord;
   record->mType = aType;
-  record->mTime = (mozilla::TimeStamp::Now() - gProfilerStart).ToMilliseconds();
+  record->mTime = mozilla::TimeStamp::Now();
   record->mItem = aItem;
   record->mText = PL_strdup(aText);
   record->mText2 = aText2 ? PL_strdup(aText2) : nullptr;
@@ -437,10 +492,156 @@ void Mark(uint32_t aType, void * aItem, const char * aText, const char * aText2)
   ++threadLogPrivate->mNextRecord;
   if (threadLogPrivate->mNextRecord == threadLogPrivate->mRecordsTail) {
     
+    
     PR_SetThreadPrivate(gThreadPrivateIndex, nullptr);
   }
 #endif
 }
+
+
+#ifdef MOZ_VISUAL_EVENT_TRACER
+
+
+
+class VisualEventTracerLog : public nsIVisualEventTracerLog
+{
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIVISUALEVENTTRACERLOG
+
+  VisualEventTracerLog(RecordBatch* aBatch)
+    : mBatch(aBatch)
+    , mProfilerStart(*gProfilerStart)
+  {}
+
+  virtual ~VisualEventTracerLog();
+
+protected:
+  RecordBatch * mBatch;
+  TimeStamp mProfilerStart;
+};
+
+NS_IMPL_ISUPPORTS1(VisualEventTracerLog, nsIVisualEventTracerLog)
+
+VisualEventTracerLog::~VisualEventTracerLog()
+{
+  RecordBatch::Delete(mBatch);
+}
+
+NS_IMETHODIMP
+VisualEventTracerLog::GetJSONString(nsACString & _retval)
+{
+  nsCString buffer;
+
+  buffer.Assign(NS_LITERAL_CSTRING("{\n\"version\": 1,\n\"records\":[\n"));
+
+  RecordBatch * batch = mBatch;
+  while (batch) {
+    if (batch != mBatch) {
+      
+      buffer.Append(NS_LITERAL_CSTRING(",\n"));
+    }
+
+    buffer.Append(NS_LITERAL_CSTRING("{\"thread\":\""));
+    buffer.Append(batch->mThreadNameCopy);
+    buffer.Append(NS_LITERAL_CSTRING("\",\"log\":[\n"));
+
+    static const int kBufferSize = 2048;
+    char buf[kBufferSize];
+
+    for (Record * record = batch->mRecordsHead;
+         record < batch->mNextRecord;
+         ++record) {
+
+      
+      
+      
+      uint32_t type = record->mType & 0xffffUL;
+      uint32_t flags = record->mType >> 16;
+      PR_snprintf(buf, kBufferSize,
+        "{\"e\":\"%c\",\"t\":%f,\"f\":%d,\"i\":\"%p\",\"n\":\"%s%s\"}%s\n",
+        kTypeChars[type],
+        (record->mTime - mProfilerStart).ToMilliseconds(),
+        flags,
+        record->mItem,
+        record->mText,
+        record->mText2 ? record->mText2 : "",
+        (record == batch->mNextRecord - 1) ? "" : ",");
+
+      buffer.Append(buf);
+    }
+
+    buffer.Append(NS_LITERAL_CSTRING("]}\n"));
+
+    RecordBatch * next = batch->mNextBatch;
+    batch = next;
+  }
+
+  buffer.Append(NS_LITERAL_CSTRING("]}\n"));
+  _retval.Assign(buffer);
+
+  return NS_OK;
+}
+
+NS_IMPL_ISUPPORTS1(VisualEventTracer, nsIVisualEventTracer)
+
+NS_IMETHODIMP
+VisualEventTracer::Start(const uint32_t aMaxBacklogSeconds)
+{
+  if (!gInitialized)
+    return NS_ERROR_UNEXPECTED;
+
+  if (gCapture) {
+    NS_WARNING("VisualEventTracer has already been started");
+    return NS_ERROR_ALREADY_INITIALIZED;
+  }
+
+  *gMaxBacklogTime = TimeDuration::FromMilliseconds(aMaxBacklogSeconds * 1000);
+
+  *gProfilerStart = mozilla::TimeStamp::Now();
+  {
+    MonitorAutoLock mon(*gMonitor);
+    RecordBatch::GCLog(*gProfilerStart);
+  }
+  gCapture = true;
+
+  MOZ_EVENT_TRACER_MARK(this, "trace::start");
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+VisualEventTracer::Stop()
+{
+  if (!gInitialized)
+    return NS_ERROR_UNEXPECTED;
+
+  if (!gCapture) {
+    NS_WARNING("VisualEventTracer is not runing");
+    return NS_ERROR_NOT_INITIALIZED;
+  }
+
+  MOZ_EVENT_TRACER_MARK(this, "trace::stop");
+
+  gCapture = false;
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+VisualEventTracer::Snapshot(nsIVisualEventTracerLog ** _result)
+{
+  if (!gInitialized)
+    return NS_ERROR_UNEXPECTED;
+
+  RecordBatch * batch = RecordBatch::CloneLog();
+
+  nsRefPtr<VisualEventTracerLog> log = new VisualEventTracerLog(batch);
+  log.forget(_result);
+
+  return NS_OK;
+}
+
+#endif
 
 } 
 } 
