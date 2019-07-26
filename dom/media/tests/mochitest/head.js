d9dd8bbbbcd8a15e8a4cc5cc333bc6273594e91c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:05:57 +0000

    Backed out changeset 4740975a1063 (bug 837458) for Android mochitest failures.
    UltraBlame original commit: 8e80f634fc8b089a1adfcd2973df89a6134fc21f

--- dom/media/tests/mochitest/head.js
+++ dom/media/tests/mochitest/head.js
@@ -17,73 +17,28 @@ var FAKE_ENABLED = true;
 
 
 
-
-
-
-
-
-function createHTML(meta) {
-  var test = document.getElementById('test');
+function runTest(aCallback, desktopSupportedOnly) {
+  SimpleTest.waitForExplicitFinish();
 
   
-  var elem = document.createElement('meta');
-  elem.setAttribute('charset', 'utf-8');
-  document.head.appendChild(elem);
-
-  var title = document.createElement('title');
-  title.textContent = meta.title;
-  document.head.appendChild(title);
-
   
-  var anchor = document.createElement('a');
-  anchor.setAttribute('target', '_blank');
-
-  if (meta.bug) {
-    anchor.setAttribute('href', 'https://bugzilla.mozilla.org/show_bug.cgi?id=' + meta.bug);
+  if(desktopSupportedOnly && (navigator.userAgent.indexOf('Android') > -1 ||
+     navigator.platform === '')) {
+    ok(true, navigator.userAgent + ' currently not supported');
+    SimpleTest.finish();
+  } else {
+    SpecialPowers.pushPrefEnv({'set': [
+      ['media.peerconnection.enabled', true],
+      ['media.navigator.permission.denied', true]]}, function () {
+      try {
+        aCallback();
+      }
+      catch (err) {
+        unexpectedCallbackAndFinish(err);
+      }
+    });
   }
-
-  anchor.textContent = meta.title;
-  document.body.insertBefore(anchor, test);
-
-  var display = document.createElement('p');
-  display.setAttribute('id', 'display');
-  document.body.insertBefore(display, test);
-
-  var content = document.createElement('div');
-  content.setAttribute('id', 'content');
-  content.style.display = meta.visible ? 'block' : "none";
-  document.body.appendChild(content);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-function createMediaElement(type, label) {
-  var id = label + '_' + type;
-  var element = document.getElementById(id);
-
-  
-  if (element)
-    return element;
-
-  element = document.createElement(type === 'audio' ? 'audio' : 'video');
-  element.setAttribute('id', id);
-  element.setAttribute('height', 100);
-  element.setAttribute('width', 150);
-  element.setAttribute('controls', 'controls');
-  document.getElementById('content').appendChild(element);
-
-  return element;
-}
-
 
 
 
@@ -98,47 +53,8 @@ function createMediaElement(type, label) {
 
 function getUserMedia(constraints, onSuccess, onError) {
   constraints["fake"] = FAKE_ENABLED;
-
-  info("Call getUserMedia for " + JSON.stringify(constraints));
   navigator.mozGetUserMedia(constraints, onSuccess, onError);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-function runTest(aCallback, desktopSupportedOnly) {
-  SimpleTest.waitForExplicitFinish();
-
-  
-  
-  if(desktopSupportedOnly && (navigator.userAgent.indexOf('Android') > -1 ||
-     navigator.platform === '')) {
-    ok(true, navigator.userAgent + ' currently not supported');
-    SimpleTest.finish();
-  } else {
-    SpecialPowers.pushPrefEnv({'set': [
-        ['media.peerconnection.enabled', true],
-        ['media.navigator.permission.denied', true]]
-      }, function () {
-      try {
-        aCallback();
-      }
-      catch (err) {
-        unexpectedCallbackAndFinish(err);
-      }
-    });
-  }
-}
-
-
 
 
 
