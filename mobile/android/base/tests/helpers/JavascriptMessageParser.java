94da620eed6e7d636d92eb8244d37a7946239d9d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:24:45 +0000

    Bug 988553 - Move JavascriptTestMessageParser into helper class; r=nalexander
    UltraBlame original commit: a99a220d3c565492830cd1c704aa20a27c82db1c

---
+++ mobile/android/base/tests/helpers/JavascriptMessageParser.java
@@ -0,0 +1,73 @@
+
+
+
+
+package org.mozilla.gecko.tests.helpers;
+
+import org.mozilla.gecko.Assert;
+
+import junit.framework.AssertionFailedError;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+
+
+public final class JavascriptMessageParser {
+
+    
+    
+    private static final Pattern testMessagePattern =
+        Pattern.compile("\n+TEST-(.*) \\| (.*) \\| (.*)\n*");
+
+    private final Assert asserter;
+    
+    private String lastTestName = "";
+    
+    private boolean testFinishedMessageSeen = false;
+
+    public JavascriptMessageParser(final Assert asserter) {
+        this.asserter = asserter;
+    }
+
+    public boolean isTestFinished() {
+        return testFinishedMessageSeen;
+    }
+
+    public void logMessage(final String str) {
+        final Matcher m = testMessagePattern.matcher(str);
+
+        if (m.matches()) {
+            final String type = m.group(1);
+            final String name = m.group(2);
+            final String message = m.group(3);
+
+            if ("INFO".equals(type)) {
+                asserter.info(name, message);
+                testFinishedMessageSeen = testFinishedMessageSeen ||
+                                          "exiting test".equals(message);
+            } else if ("PASS".equals(type)) {
+                asserter.ok(true, name, message);
+            } else if ("UNEXPECTED-FAIL".equals(type)) {
+                try {
+                    asserter.ok(false, name, message);
+                } catch (AssertionFailedError e) {
+                    
+                    
+                }
+            } else if ("KNOWN-FAIL".equals(type)) {
+                asserter.todo(false, name, message);
+            } else if ("UNEXPECTED-PASS".equals(type)) {
+                asserter.todo(true, name, message);
+            }
+
+            lastTestName = name;
+        } else {
+            
+            
+            asserter.info(lastTestName, str.trim());
+        }
+    }
+}
