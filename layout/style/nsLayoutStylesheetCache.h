02f54b63acab4be10e2b500cc3f62532386f3239
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:00:19 +0000

    Bug 974430 - Put the |input[type=number]| rule in forms.css into a separate sheet so we can make it respect the dom.forms.number pref. r=dbaron
    UltraBlame original commit: f92329a2b0b521ccf1c45723ed42f6f562b7493b

--- layout/style/nsLayoutStylesheetCache.h
+++ layout/style/nsLayoutStylesheetCache.h
@@ -33,6 +33,9 @@ class nsLayoutStylesheetCache MOZ_FINAL
 
   static nsCSSStyleSheet* ScrollbarsSheet();
   static nsCSSStyleSheet* FormsSheet();
+  
+  
+  static nsCSSStyleSheet* NumberControlSheet();
   static nsCSSStyleSheet* UserContentSheet();
   static nsCSSStyleSheet* UserChromeSheet();
   static nsCSSStyleSheet* UASheet();
@@ -58,6 +61,7 @@ private:
   static mozilla::css::Loader* gCSSLoader;
   nsRefPtr<nsCSSStyleSheet> mScrollbarsSheet;
   nsRefPtr<nsCSSStyleSheet> mFormsSheet;
+  nsRefPtr<nsCSSStyleSheet> mNumberControlSheet;
   nsRefPtr<nsCSSStyleSheet> mUserContentSheet;
   nsRefPtr<nsCSSStyleSheet> mUserChromeSheet;
   nsRefPtr<nsCSSStyleSheet> mUASheet;
