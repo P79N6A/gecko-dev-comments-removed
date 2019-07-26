7b03d4f69f5ea93821b94e92f56794588dafff04
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:08:13 +0000

    Teach RangeAnalysis how to deal with unreachable blocks (bug 765119, r=dvander)
    UltraBlame original commit: 7895a56d434da2e97879ad4ae62395a0312ac809

--- js/src/ion/RangeAnalysis.h
+++ js/src/ion/RangeAnalysis.h
@@ -21,9 +21,9 @@ class MIRGraph;
 class RangeAnalysis
 {
   protected:
-	bool blockDominates(MBasicBlock *b, MBasicBlock *b2);
-	void replaceDominatedUsesWith(MDefinition *orig, MDefinition *dom,
-	                              MBasicBlock *block);
+    bool blockDominates(MBasicBlock *b, MBasicBlock *b2);
+    void replaceDominatedUsesWith(MDefinition *orig, MDefinition *dom,
+                                  MBasicBlock *block);
 
   protected:
     MIRGraph &graph_;
@@ -37,143 +37,142 @@ class RangeAnalysis
 
 struct RangeChangeCount;
 class Range {
-    private:
-        
-        
-        
-        
+  private:
+    
+    
+    
+    
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        int32 lower_;
-        bool lower_infinite_;
-        int32 upper_;
-        bool upper_infinite_;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    int32 lower_;
+    bool lower_infinite_;
+    int32 upper_;
+    bool upper_infinite_;
 
-    public:
-        Range()
-          : lower_(JSVAL_INT_MIN),
-            lower_infinite_(true),
-            upper_(JSVAL_INT_MAX),
-            upper_infinite_(true)
-        {}
+  public:
+    Range()
+        : lower_(JSVAL_INT_MIN),
+          lower_infinite_(true),
+          upper_(JSVAL_INT_MAX),
+          upper_infinite_(true)
+    {}
 
-        Range(int64_t l, int64_t h) {
-            setLower(l);
-            setUpper(h);
-        }
+    Range(int64_t l, int64_t h) {
+        setLower(l);
+        setUpper(h);
+    }
 
-        Range(const Range &other)
-          : lower_(other.lower_),
-            lower_infinite_(other.lower_infinite_),
-            upper_(other.upper_),
-            upper_infinite_(other.upper_infinite_)
-        {}
+    Range(const Range &other)
+    : lower_(other.lower_),
+      lower_infinite_(other.lower_infinite_),
+      upper_(other.upper_),
+      upper_infinite_(other.upper_infinite_)
+    {}
 
-        static int64_t abs64(int64_t x) {
+    static int64_t abs64(int64_t x) {
 #ifdef WTF_OS_WINDOWS
-            return _abs64(x);
+        return _abs64(x);
 #else
-            return llabs(x);
+        return llabs(x);
 #endif
-        }
+    }
 
-        void printRange(FILE *fp);
-        bool update(const Range *other);
-        bool update(const Range &other) {
-            return update(&other);
-        }
+    void printRange(FILE *fp);
+    bool update(const Range *other);
+    bool update(const Range &other) {
+        return update(&other);
+    }
 
-        
-        
-        
-        
+    
+    
+    
+    
     void unionWith(const Range *other);
     void unionWith(RangeChangeCount *other);
+    static Range intersect(const Range *lhs, const Range *rhs, bool *nullRange);
+    static Range add(const Range *lhs, const Range *rhs);
+    static Range sub(const Range *lhs, const Range *rhs);
+    static Range mul(const Range *lhs, const Range *rhs);
+    static Range and_(const Range *lhs, const Range *rhs);
+    static Range shl(const Range *lhs, int32 c);
+    static Range shr(const Range *lhs, int32 c);
 
-        static Range intersect(const Range *lhs, const Range *rhs);
-        static Range add(const Range *lhs, const Range *rhs);
-        static Range sub(const Range *lhs, const Range *rhs);
-        static Range mul(const Range *lhs, const Range *rhs);
-        static Range and_(const Range *lhs, const Range *rhs);
-        static Range shl(const Range *lhs, int32 c);
-        static Range shr(const Range *lhs, int32 c);
+    inline void makeLowerInfinite() {
+        lower_infinite_ = true;
+        lower_ = JSVAL_INT_MIN;
+    }
+    inline void makeUpperInfinite() {
+        upper_infinite_ = true;
+        upper_ = JSVAL_INT_MAX;
+    }
+    inline void makeRangeInfinite() {
+        makeLowerInfinite();
+        makeUpperInfinite();
+    }
 
-        inline void makeLowerInfinite() {
-            lower_infinite_ = true;
-            lower_ = JSVAL_INT_MIN;
-        }
-        inline void makeUpperInfinite() {
-            upper_infinite_ = true;
-            upper_ = JSVAL_INT_MAX;
-        }
-        inline void makeRangeInfinite() {
+    inline bool isLowerInfinite() const {
+        return lower_infinite_;
+    }
+    inline bool isUpperInfinite() const {
+        return upper_infinite_;
+    }
+
+    inline bool isFinite() const {
+        return !isLowerInfinite() && !isUpperInfinite();
+    }
+
+    inline int32 lower() const {
+        return lower_;
+    }
+
+    inline int32 upper() const {
+        return upper_;
+    }
+
+    inline void setLower(int64_t x) {
+        if (x > JSVAL_INT_MAX) { 
+            lower_ = JSVAL_INT_MAX;
+        } else if (x < JSVAL_INT_MIN) {
             makeLowerInfinite();
+        } else {
+            lower_ = (int32)x;
+            lower_infinite_ = false;
+        }
+    }
+    inline void setUpper(int64_t x) {
+        if (x > JSVAL_INT_MAX) {
             makeUpperInfinite();
+        } else if (x < JSVAL_INT_MIN) { 
+            upper_ = JSVAL_INT_MIN;
+        } else {
+            upper_ = (int32)x;
+            upper_infinite_ = false;
         }
-
-        inline bool isLowerInfinite() const {
-            return lower_infinite_;
-        }
-        inline bool isUpperInfinite() const {
-            return upper_infinite_;
-        }
-
-        inline bool isFinite() const {
-            return !isLowerInfinite() && !isUpperInfinite();
-        }
-
-        inline int32 lower() const {
-            return lower_;
-        }
-
-        inline int32 upper() const {
-            return upper_;
-        }
-
-        inline void setLower(int64_t x) {
-            if (x > JSVAL_INT_MAX) { 
-                lower_ = JSVAL_INT_MAX;
-            } else if (x < JSVAL_INT_MIN) {
-                makeLowerInfinite();
-            } else {
-                lower_ = (int32)x;
-                lower_infinite_ = false;
-            }
-        }
-        inline void setUpper(int64_t x) {
-            if (x > JSVAL_INT_MAX) {
-                makeUpperInfinite();
-            } else if (x < JSVAL_INT_MIN) { 
-                upper_ = JSVAL_INT_MIN;
-            } else {
-                upper_ = (int32)x;
-                upper_infinite_ = false;
-            }
-        }
-        void set(int64_t l, int64_t h) {
-            setLower(l);
-            setUpper(h);
-        }
+    }
+    void set(int64_t l, int64_t h) {
+        setLower(l);
+        setUpper(h);
+    }
 };
 
 struct RangeChangeCount {
