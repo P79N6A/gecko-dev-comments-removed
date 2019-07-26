fbce760f13270408610cfaaf31b33930f17111c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:23:08 +0000

    Bug 678377: IonMonkey: Follow-up fixing three small issues, r=bhackett
    UltraBlame original commit: 827a95147b4faa027c8a445e63ef6c50b242c27c

--- js/src/ion/LICM.cpp
+++ js/src/ion/LICM.cpp
@@ -87,12 +87,15 @@ Loop::init()
         
         
         
-        if (block->immediateDominator() == block)
+        if (block->immediateDominator() == block) {
+            while (!worklist_.empty())
+                popFromWorklist();
             return LoopReturn_Skip;
+        }
 
         
         if (block != header_) {
-            for (size_t i = 0; i < block->numPredecessors(); i--) {
+            for (size_t i = 0; i < block->numPredecessors(); i++) {
                 MBasicBlock *pred = block->getPredecessor(i);
                 if (pred->isMarked())
                     continue;
@@ -111,7 +114,7 @@ Loop::init()
         for (MInstructionIterator i = block->begin(); i != block->end(); i++) {
             MInstruction *ins = *i;
 
-            if (ins->isMovable() && !ins->isEffectful()) {
+            if (isHoistable(ins)) {
                 if (!insertInWorklist(ins))
                     return LoopReturn_Error;
             }
