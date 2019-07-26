61feb7d104ffb662a71407a2995697335bf734e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:09:24 +0000

    Bug 743402, Part 4: Refactor ApplySkipSides() and GetSkipSides() to return the correct value during reflow by adding an optional nsHTMLReflowState parameter. [r=roc]
    UltraBlame original commit: 352501d3f390a01f44036782973c154a13f7b9ad

--- layout/generic/nsFlexContainerFrame.cpp
+++ layout/generic/nsFlexContainerFrame.cpp
@@ -1100,7 +1100,7 @@ nsFlexContainerFrame::GetType() const
 
 
 int
-nsFlexContainerFrame::GetSkipSides() const
+nsFlexContainerFrame::GetSkipSides(const nsHTMLReflowState* aReflowState) const
 {
   
   if (IS_TRUE_OVERFLOW_CONTAINER(this)) {
