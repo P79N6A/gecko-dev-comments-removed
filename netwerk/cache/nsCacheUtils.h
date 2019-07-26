c8ef8e46b8cb5939a5e9480c1fc5ad98e87c4158
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:17:55 +0000

    Bug 829419 - Don't allow processing pending events while shutting down cache IO thread, r=jduell
    UltraBlame original commit: d8b73ccd6725315f34a5971290e8da51af32ca95

---
+++ netwerk/cache/nsCacheUtils.h
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+#ifndef _nsCacheUtils_h_
+#define _nsCacheUtils_h_
+
+#include "nsThreadUtils.h"
+#include "nsCOMPtr.h"
+#include "mozilla/Mutex.h"
+#include "mozilla/CondVar.h"
+
+class nsIThread;
+
+
+
+
+class nsShutdownThread : public nsRunnable {
+public:
+  nsShutdownThread(nsIThread *aThread);
+  ~nsShutdownThread();
+
+  NS_IMETHOD Run();
+
+
+
+
+  static nsresult Shutdown(nsIThread *aThread);
+
+
+
+
+
+  static nsresult BlockingShutdown(nsIThread *aThread);
+
+private:
+  mozilla::Mutex      mLock;
+  mozilla::CondVar    mCondVar;
+  nsCOMPtr<nsIThread> mThread;
+};
+
+#endif
