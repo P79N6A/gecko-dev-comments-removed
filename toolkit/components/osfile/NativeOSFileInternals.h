36a04882a1c80e22dfaa04f429730f23915c1a9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:12:41 +0000

    Bug 961665 - Native implementation of OS.File.read, native bits. r=froydnj, r=bz, r=paolo
    UltraBlame original commit: b56dd8bf8150c946aa9e0538cabfe8a6bd0bcd96

---
+++ toolkit/components/osfile/NativeOSFileInternals.h
@@ -0,0 +1,24 @@
+
+
+
+
+#ifndef mozilla_nativeosfileinternalservice_h__
+#define mozilla_nativeosfileinternalservice_h__
+
+#include "nsINativeOSFileInternals.h"
+#include "mozilla/Attributes.h"
+
+namespace mozilla {
+
+class NativeOSFileInternalsService MOZ_FINAL : public nsINativeOSFileInternalsService {
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSINATIVEOSFILEINTERNALSSERVICE
+private:
+  
+  void operator=(const NativeOSFileInternalsService& other) MOZ_DELETE;
+};
+
+} 
+
+#endif 
