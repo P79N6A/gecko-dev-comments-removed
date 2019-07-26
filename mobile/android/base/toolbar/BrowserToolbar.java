b7a01fcba2cb4883bfb29f56d8ea6cfa9f7f87ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:50:54 +0000

    Bug 1000149 - Part 1: Remove go button from toolbar. r=lucasr
    UltraBlame original commit: 27c4e67174a21ea2f2e70cf84e3e55f195c9f327

--- mobile/android/base/toolbar/BrowserToolbar.java
+++ mobile/android/base/toolbar/BrowserToolbar.java
@@ -328,6 +328,7 @@ public class BrowserToolbar extends ThemedRelativeLayout
         urlEditLayout.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
+                
                 setSelected(hasFocus);
                 if (focusChangeListener != null) {
                     focusChangeListener.onFocusChange(v, hasFocus);
@@ -850,7 +851,9 @@ public class BrowserToolbar extends ThemedRelativeLayout
     }
 
     private void setUrlEditLayoutVisibility(final boolean showEditLayout, PropertyAnimator animator) {
-        urlEditLayout.prepareAnimation(showEditLayout, animator);
+        if (showEditLayout) {
+            urlEditLayout.prepareShowAnimation(animator);
+        }
 
         final View viewToShow = (showEditLayout ? urlEditLayout : urlDisplayLayout);
         final View viewToHide = (showEditLayout ? urlDisplayLayout : urlEditLayout);
@@ -1020,9 +1023,6 @@ public class BrowserToolbar extends ThemedRelativeLayout
             final int entryTranslation, final int curveTranslation) {
         if (isAnimatingEntry)
             return;
-
-        
-        setSelected(true);
 
         urlDisplayLayout.prepareStartEditingAnimation();
 
