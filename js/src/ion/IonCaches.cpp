75c770c11bc1be53e4cb5e1e3c5ba3ed22474770
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:41:48 +0000

    Bug 876098.  Make sure to not skip calling addProperty hooks when objects have them.  Otherwise DOM expandos can go AWOL.  r=djvj
    UltraBlame original commit: db53707fb4d744fb2d355b4c062a41c01ba8ea6f

--- js/src/ion/IonCaches.cpp
+++ js/src/ion/IonCaches.cpp
@@ -2057,6 +2057,11 @@ IsPropertyAddInlineable(JSContext *cx, HandleObject obj, HandleId id, uint32_t o
     if (obj->getClass()->resolve != JS_ResolveStub)
         return false;
 
+    
+    
+    if (obj->getClass()->addProperty != JS_PropertyStub)
+        return false;
+
     if (!obj->isExtensible() || !shape->writable())
         return false;
 
