ffc0b3241eaf796fd978a6250927db77e7ab623d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:16:23 +0000

    Bug 792581 - part 20: Replace LL_UI2L macro with int64_t cast. r=ehsan
    UltraBlame original commit: 5fb440887096d1c497b2a15adeebe2a20a15f229

--- netwerk/streamconv/converters/ParseFTPList.cpp
+++ netwerk/streamconv/converters/ParseFTPList.cpp
@@ -486,10 +486,7 @@ int ParseFTPList(const char *line, struct list_state *state,
 
 
 
-              uint64_t fsz, factor;
-              LL_UI2L(fsz, strtoul(tokens[1], (char **)0, 10));
-              LL_UI2L(factor, 512);
-              fsz *= factor;
+              uint64_t fsz = uint64_t(strtoul(tokens[1], (char **)0, 10) * 512);
               PR_snprintf(result->fe_size, sizeof(result->fe_size), 
                           "%lld", fsz);
             } 
