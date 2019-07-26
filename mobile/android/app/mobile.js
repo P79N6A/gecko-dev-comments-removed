6f809616e92684c1e588fcead3557927b8af7c6a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:15:52 +0000

    Bug 901818 - Only enable Android backend for Contacts API on Nightly builds. r=reuben
    UltraBlame original commit: b3ea93ee7659f57c0260e00ad5b922e53803e6f0

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -755,10 +755,12 @@ pref("dom.payment.provider.0.type", "mozilla/payments/pay/v1");
 pref("dom.payment.provider.0.requestMethod", "GET");
 #endif
 
+#ifdef NIGHTLY_BUILD
 
 pref("dom.mozContacts.enabled", true);
 pref("dom.navigator-property.disable.mozContacts", false);
 pref("dom.global-constructor.disable.mozContact", false);
+#endif
 
 
 
