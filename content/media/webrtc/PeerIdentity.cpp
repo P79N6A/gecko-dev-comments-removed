4296b6ff463628768abe5eed499d024ed1c79dfd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:41:09 +0000

    Bug 942367 - DOMMediaStream supports PeerIdentity. r=bobbyholley
    UltraBlame original commit: 3d0426ac98f17e41d7ce214a88d17d647c5c026c

---
+++ content/media/webrtc/PeerIdentity.cpp
@@ -0,0 +1,83 @@
+
+
+
+
+
+
+#include "PeerIdentity.h"
+
+#include "nsCOMPtr.h"
+#include "nsIIDNService.h"
+#include "nsNetCID.h"
+
+namespace mozilla {
+
+bool
+PeerIdentity::Equals(const PeerIdentity& aOther) const
+{
+  return Equals(aOther.mPeerIdentity);
+}
+
+bool
+PeerIdentity::Equals(const nsAString& aOtherString) const
+{
+  nsString user;
+  GetUser(mPeerIdentity, user);
+  nsString otherUser;
+  GetUser(aOtherString, otherUser);
+  if (user != otherUser) {
+    return false;
+  }
+
+  nsString host;
+  GetHost(mPeerIdentity, host);
+  nsString otherHost;
+  GetHost(aOtherString, otherHost);
+
+  nsresult rv;
+  nsCOMPtr<nsIIDNService> idnService
+    = do_GetService(NS_IDNSERVICE_CONTRACTID, &rv);
+  if (NS_WARN_IF(NS_FAILED(rv))) {
+    return host == otherHost;
+  }
+
+  nsCString normHost;
+  GetNormalizedHost(idnService, host, normHost);
+  nsCString normOtherHost;
+  GetNormalizedHost(idnService, otherHost, normOtherHost);
+  return normHost == normOtherHost;
+}
+
+ void
+PeerIdentity::GetUser(const nsAString& aPeerIdentity, nsAString& aUser)
+{
+  int32_t at = aPeerIdentity.FindChar('@');
+  if (at >= 0) {
+    aUser = Substring(aPeerIdentity, 0, at);
+  } else {
+    aUser.Truncate();
+  }
+}
+
+ void
+PeerIdentity::GetHost(const nsAString& aPeerIdentity, nsAString& aHost)
+{
+  int32_t at = aPeerIdentity.FindChar('@');
+  if (at >= 0) {
+    aHost = Substring(aPeerIdentity, at + 1);
+  } else {
+    aHost = aPeerIdentity;
+  }
+}
+
+ void
+PeerIdentity::GetNormalizedHost(const nsCOMPtr<nsIIDNService>& aIdnService,
+                                const nsAString& aHost,
+                                nsACString& aNormalizedHost)
+{
+  nsCString chost = NS_ConvertUTF16toUTF8(aHost);
+  nsresult rv = aIdnService->ConvertUTF8toACE(chost, aNormalizedHost);
+  NS_WARN_IF(NS_FAILED(rv));
+}
+
+} 
