560eb12834c4aff4f90affbd4ad431c9ad2af41f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:22:29 +0000

    Bug 960040 - Part 4: Refactor state management of analysis. r=jandem
    UltraBlame original commit: a7ba0d202c1af802494b4838c692fccf277e5dcd

--- js/src/jsanalyze.h
+++ js/src/jsanalyze.h
@@ -53,11 +53,6 @@ class ScriptAnalysis
 
     bool *escapedSlots;
 
-    
-    bool ranBytecode_;
-    bool ranSSA_;
-    bool ranLifetimes_;
-
 #ifdef DEBUG
     
     bool originalDebugMode_: 1;
@@ -82,7 +77,6 @@ class ScriptAnalysis
 #endif
     }
 
-    bool ranBytecode() { return ranBytecode_; }
     JS_WARN_UNUSED_RESULT
     bool analyzeBytecode(JSContext *cx);
 
@@ -95,8 +89,6 @@ class ScriptAnalysis
     bool isReachable(const jsbytecode *pc) { return maybeCode(pc); }
 
   private:
-    bool ranSSA() { return ranSSA_; }
-    bool ranLifetimes() { return ranLifetimes_; }
     JS_WARN_UNUSED_RESULT
     bool analyzeSSA(JSContext *cx);
     JS_WARN_UNUSED_RESULT
