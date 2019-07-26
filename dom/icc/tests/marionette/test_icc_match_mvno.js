58814a3e7b2626773b04464720e165500250b5a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:31:59 +0000

    Bug 963516 - Part 4: test cases for matchMvno(). f=edgar r=hsinyi
    UltraBlame original commit: 9a57b98477b4a0d9cc555871018d3d6376287150

---
+++ dom/icc/tests/marionette/test_icc_match_mvno.js
@@ -0,0 +1,56 @@
+
+
+
+MARIONETTE_TIMEOUT = 30000;
+MARIONETTE_HEAD_JS = "icc_header.js";
+
+
+let testCases = [
+  
+  ["imsi", "3102600",            true, true               ],
+  
+  ["imsi", "31026xx0",           true, true               ],
+  ["imsi", "310260x0x",          true, true               ],
+  ["imsi", "310260X00",          true, true               ],
+  ["imsi", "310260XX1",          true, false              ],
+  ["imsi", "31026012",           true, false              ],
+  ["imsi", "310260000000000",    true, true               ],
+  ["imsi", "310260000000000123", true, false              ],
+  ["imsi", "",                   false, "InvalidParameter"],
+  
+  ["spn",  "Android",            false, "ModeNotSupported"]
+];
+
+function matchMvno(mvnoType, mvnoData, success, expectedResult) {
+  log("matchMvno: " + mvnoType + ", " + mvnoData);
+  let request = icc.matchMvno(mvnoType, mvnoData);
+  request.onsuccess = function onsuccess() {
+    log("onsuccess: " + request.result);
+    ok(success, "onsuccess while error expected");
+    is(request.result, expectedResult);
+    testMatchMvno();
+  }
+  request.onerror = function onerror() {
+    log("onerror: " + request.error.name);
+    ok(!success, "onerror while success expected");
+    is(request.error.name, expectedResult);
+    testMatchMvno();
+  }
+}
+
+function testMatchMvno() {
+  let testCase = testCases.shift();
+  if (!testCase) {
+    taskHelper.runNext();
+    return;
+  }
+  matchMvno(testCase[0], testCase[1], testCase[2], testCase[3]);
+}
+
+taskHelper.push(
+  testMatchMvno
+);
+
+
+taskHelper.runNext();
+
