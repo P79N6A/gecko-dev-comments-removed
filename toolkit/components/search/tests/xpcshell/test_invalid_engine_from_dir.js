0c0eb9a63a92edd96cf5aed7d76c2ee4deb2d652
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:57:22 +0000

    Bug 943571 - Introduce a test for invalid engine files loaded from a dir. r=gavin
    UltraBlame original commit: 1e3bcdef8157b765a4976ea658725b8f9b6f497c

---
+++ toolkit/components/search/tests/xpcshell/test_invalid_engine_from_dir.js
@@ -0,0 +1,37 @@
+
+
+
+const Ci = Components.interfaces;
+
+
+
+
+
+function run_test() {
+  do_test_pending();
+
+  removeMetadata();
+  removeCacheFile();
+
+  do_check_false(Services.search.isInitialized);
+
+  let engineFile = gProfD.clone();
+  engineFile.append("searchplugins");
+  engineFile.append("test-search-engine.xml");
+  engineFile.parent.create(Ci.nsIFile.DIRECTORY_TYPE, FileUtils.PERMS_DIRECTORY);
+
+  
+  let engineTemplateFile = do_get_file("data/invalid-engine.xml");
+  engineTemplateFile.copyTo(engineFile.parent, "test-search-engine.xml");
+
+  Services.search.init(function search_initialized(aStatus) {
+    
+    
+    do_check_true(Components.isSuccessCode(aStatus));
+    do_check_true(Services.search.isInitialized);
+
+    removeMetadata();
+    removeCacheFile();
+    do_test_finished();
+  });
+}
