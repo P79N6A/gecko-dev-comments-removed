22bc208823679f6e0615dba9b6950978a5baf15d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:56:09 +0000

    Bug 886270 - Move SVGAnimatedInteger to WebIDL; r=dzbarsky
    UltraBlame original commit: e8151bf1db51412f681287415569dbed0f8fe312

---
+++ content/svg/content/src/SVGAnimatedInteger.cpp
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+#include "mozilla/dom/SVGAnimatedInteger.h"
+
+#include "mozilla/dom/SVGAnimatedIntegerBinding.h"
+
+namespace mozilla {
+namespace dom {
+
+NS_SVG_VAL_IMPL_CYCLE_COLLECTION_WRAPPERCACHED(SVGAnimatedInteger,
+                                               mSVGElement)
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(SVGAnimatedInteger)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(SVGAnimatedInteger)
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(SVGAnimatedInteger)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_END
+
+JSObject*
+SVGAnimatedInteger::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope)
+{
+  return SVGAnimatedIntegerBinding::Wrap(aCx, aScope, this);
+}
+
+} 
+} 
---
+++ content/svg/content/src/SVGAnimatedInteger.h
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_SVGAnimatedInteger_h
+#define mozilla_dom_SVGAnimatedInteger_h
+
+#include "nsIDOMSVGAnimatedEnum.h"
+#include "nsWrapperCache.h"
+
+#include "nsSVGElement.h"
+
+namespace mozilla {
+namespace dom {
+
+class SVGAnimatedInteger : public nsISupports
+                         , public nsWrapperCache
+{
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(SVGAnimatedInteger)
+
+  nsSVGElement* GetParentObject() const
+  {
+    return mSVGElement;
+  }
+
+  virtual JSObject* WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope)
+    MOZ_OVERRIDE MOZ_FINAL;
+
+  virtual int32_t BaseVal() = 0;
+  virtual void SetBaseVal(int32_t aBaseVal) = 0;
+  virtual int32_t AnimVal() = 0;
+
+protected:
+  explicit SVGAnimatedInteger(nsSVGElement* aSVGElement)
+    : mSVGElement(aSVGElement)
+  {
+    SetIsDOMBinding();
+  }
+  virtual ~SVGAnimatedInteger() {};
+
+  nsRefPtr<nsSVGElement> mSVGElement;
+};
+
+} 
+} 
+
+#endif 
