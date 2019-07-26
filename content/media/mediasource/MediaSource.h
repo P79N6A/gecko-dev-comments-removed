8cd9bd37179b2e0aa129464a35aca39a6a59dab1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:57:51 +0000

    Bug 855130 - Implement a minimal working subset of the Media Source
     Extensions specification.  r=roc
    
    UltraBlame original commit: 26bac32e8738d23feb3374d1aff90fe5bc33d995

---
+++ content/media/mediasource/MediaSource.h
@@ -0,0 +1,127 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_MediaSource_h_
+#define mozilla_dom_MediaSource_h_
+
+#include "AsyncEventRunner.h"
+#include "mozilla/Attributes.h"
+#include "mozilla/Monitor.h"
+#include "mozilla/dom/MediaSourceBinding.h"
+#include "mozilla/dom/TypedArray.h"
+#include "nsCOMPtr.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsDOMEventTargetHelper.h"
+#include "nsWrapperCache.h"
+#include "nscore.h"
+
+namespace mozilla {
+namespace dom {
+
+class HTMLMediaElement;
+class MediaSourceInputAdapter;
+class SourceBufferList;
+class SourceBuffer;
+class TimeRanges;
+
+class MediaSource MOZ_FINAL : public nsDOMEventTargetHelper
+{
+public:
+  
+  static already_AddRefed<MediaSource> Constructor(const GlobalObject& aGlobal, ErrorResult& aRv);
+
+  SourceBufferList* SourceBuffers();
+  SourceBufferList* ActiveSourceBuffers();
+  MediaSourceReadyState ReadyState();
+
+  double Duration();
+  void SetDuration(double aDuration, ErrorResult& aRv);
+
+  already_AddRefed<SourceBuffer> AddSourceBuffer(const nsAString& aType, ErrorResult& aRv);
+  void RemoveSourceBuffer(SourceBuffer& aSourceBuffer, ErrorResult& aRv);
+
+  void EndOfStream(const Optional<MediaSourceEndOfStreamError>& aError, ErrorResult& aRv);
+  static bool IsTypeSupported(const GlobalObject& aGlobal, const nsAString& aType);
+  
+
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(MediaSource, nsDOMEventTargetHelper)
+
+  nsPIDOMWindow* GetParentObject() const;
+
+  JSObject* WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
+
+  const nsString& GetType()
+  {
+    return mContentType;
+  }
+
+  already_AddRefed<nsIInputStream> CreateInternalStream();
+
+
+  void AppendData(const uint8_t* aData, uint32_t aLength, ErrorResult& aRv);
+
+  
+  nsTArray<uint8_t> const& GetData()
+  {
+    return mData;
+  }
+
+  Monitor& GetMonitor()
+  {
+    return mMonitor;
+  }
+
+  bool AppendDone() const
+  {
+    return mReadyState == MediaSourceReadyState::Closed;
+  }
+
+  
+  bool AttachElement(HTMLMediaElement* aElement);
+  void DetachElement();
+
+  
+  void SetReadyState(MediaSourceReadyState aState);
+
+  void GetBuffered(TimeRanges* aRanges);
+
+private:
+  explicit MediaSource(nsPIDOMWindow* aWindow);
+
+  friend class AsyncEventRunnner<MediaSource>;
+  void DispatchSimpleEvent(const char* aName);
+  void QueueAsyncSimpleEvent(const char* aName);
+
+  void NotifyListeners();
+
+  void DurationChange(double aNewDuration, ErrorResult& aRv);
+  void EndOfStreamInternal(const Optional<MediaSourceEndOfStreamError>& aError, ErrorResult& aRv);
+
+  static bool IsTypeSupportedInternal(const nsAString& aType, ErrorResult& aRv);
+
+  double mDuration;
+
+  nsTArray<nsRefPtr<MediaSourceInputAdapter> > mAdapters;
+
+  
+  nsTArray<uint8_t> mData;
+
+  
+  Monitor mMonitor;
+
+  nsRefPtr<SourceBufferList> mSourceBuffers;
+  nsRefPtr<SourceBufferList> mActiveSourceBuffers;
+
+  nsRefPtr<HTMLMediaElement> mElement;
+
+  nsString mContentType;
+  MediaSourceReadyState mReadyState;
+};
+
+} 
+} 
+#endif 
