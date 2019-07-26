c14b9338be8fd73849b3acc12a10c8e9f6520203
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:03:47 +0000

    Bug 820834 - Abstract about:home storage and make it async-ready. r=gavin
    UltraBlame original commit: d958753bd1a23b1a34bf32067b98bc1957b53b94

--- browser/base/content/abouthome/aboutHome.js
+++ browser/base/content/abouthome/aboutHome.js
@@ -86,7 +86,7 @@ let gObserver = new MutationObserver(function (mutations) {
     if (mutation.attributeName == "searchEngineURL") {
       gObserver.disconnect();
       setupSearchEngine();
-      loadSnippets();
+      ensureSnippetsMapThen(loadSnippets);
       return;
     }
   }
@@ -99,6 +99,69 @@ window.addEventListener("load", function () {
   fitToWidth();
   window.addEventListener("resize", fitToWidth);
 });
+
+
+
+
+let gSnippetsMap;
+let gSnippetsMapCallbacks = [];
+
+
+
+
+
+
+
+
+
+function ensureSnippetsMapThen(aCallback)
+{
+  if (gSnippetsMap) {
+    aCallback(gSnippetsMap);
+    return;
+  }
+
+  
+  gSnippetsMapCallbacks.push(aCallback);
+  if (gSnippetsMapCallbacks.length > 1) {
+    
+    return;
+  }
+
+  
+  
+  setTimeout(function() {
+    
+    let cache = new Map();
+    for (let key of [ "snippets-last-update",
+                      "snippets" ]) {
+      cache.set(key, localStorage[key]);
+    }
+
+    gSnippetsMap = Object.freeze({
+      get: function (aKey) cache.get(aKey),
+      set: function (aKey, aValue) {
+        localStorage[aKey] = aValue;
+        return cache.set(aKey, aValue);
+      },
+      has: function(aKey) cache.has(aKey),
+      delete: function(aKey) {
+        delete localStorage[aKey];
+        return cache.delete(aKey);
+      },
+      clear: function() {
+        localStorage.clear();
+        return cache.clear();
+      },
+      get size() cache.size
+    });
+
+    for (let callback of gSnippetsMapCallbacks) {
+      callback(gSnippetsMap);
+    }
+    gSnippetsMapCallbacks.length = 0;
+  }, 0);
+}
 
 function onSearchSubmit(aEvent)
 {
@@ -157,13 +220,21 @@ function setupSearchEngine()
 
 }
 
+
+
+
+
 function loadSnippets()
 {
+  if (!gSnippetsMap)
+    throw new Error("Snippets map has not properly been initialized");
+
   
-  let lastUpdate = localStorage["snippets-last-update"];
+  let lastUpdate = gSnippetsMap.get("snippets-last-update");
   let updateURL = document.documentElement.getAttribute("snippetsURL");
-  if (updateURL && (!lastUpdate ||
-                    Date.now() - lastUpdate > SNIPPETS_UPDATE_INTERVAL_MS)) {
+  let shouldUpdate = !lastUpdate ||
+                     Date.now() - lastUpdate > SNIPPETS_UPDATE_INTERVAL_MS;
+  if (updateURL && shouldUpdate) {
     
     let xhr = new XMLHttpRequest();
     try {
@@ -174,14 +245,14 @@ function loadSnippets()
     }
     
     
-    localStorage["snippets-last-update"] = Date.now();
+    gSnippetsMap.set("snippets-last-update", Date.now());
     xhr.onerror = function (event) {
       showSnippets();
     };
     xhr.onload = function (event)
     {
       if (xhr.status == 200) {
-        localStorage["snippets"] = xhr.responseText;
+        gSnippetsMap.set("snippets", xhr.responseText);
       }
       showSnippets();
     };
@@ -191,10 +262,27 @@ function loadSnippets()
   }
 }
 
+
+
+
+
+
+
+let _snippetsShown = false;
 function showSnippets()
 {
+  if (!gSnippetsMap)
+    throw new Error("Snippets map has not properly been initialized");
+  if (_snippetsShown) {
+    
+    
+    showDefaultSnippets();
+    throw new Error("showSnippets should never be invoked multiple times");
+  }
+  _snippetsShown = true;
+
   let snippetsElt = document.getElementById("snippets");
-  let snippets = localStorage["snippets"];
+  let snippets = gSnippetsMap.get("snippets");
   
   if (snippets) {
     
@@ -213,6 +301,18 @@ function showSnippets()
       
     }
   }
+
+  showDefaultSnippets();
+}
+
+
+
+
+function showDefaultSnippets()
+{
+  
+  let snippetsElt = document.getElementById("snippets");
+  snippetsElt.innerHTML = "";
 
   
   let defaultSnippetsElt = document.getElementById("defaultSnippets");
