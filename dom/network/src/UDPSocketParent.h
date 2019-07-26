4f3255961a69b6fccd1f6b50d7cbcd29bffcd79f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:28:09 +0000

    Bug 869869 - Part 2, add PUDPSocket IPDL interface, r=jdm.
    UltraBlame original commit: 18584f4297df60332db2deb33a2c46dc73252924

---
+++ dom/network/src/UDPSocketParent.h
@@ -0,0 +1,45 @@
+
+
+
+
+#ifndef mozilla_dom_UDPSocketParent_h__
+#define mozilla_dom_UDPSocketParent_h__
+
+#include "mozilla/net/PUDPSocketParent.h"
+#include "nsCOMPtr.h"
+#include "nsIUDPSocket.h"
+
+namespace mozilla {
+namespace dom {
+
+class UDPSocketParent : public mozilla::net::PUDPSocketParent
+                      , public nsIUDPSocketListener
+{
+public:
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSIUDPSOCKETLISTENER
+
+  UDPSocketParent() : mIPCOpen(true) {}
+  virtual ~UDPSocketParent();
+
+  bool Init(const nsCString& aHost, const uint16_t aPort);
+
+  virtual bool RecvClose() MOZ_OVERRIDE;
+  virtual bool RecvData(const InfallibleTArray<uint8_t>& aData,
+                        const nsCString& aRemoteAddress,
+                        const uint16_t& aPort) MOZ_OVERRIDE;
+  virtual bool RecvDataWithAddress( const InfallibleTArray<uint8_t>& data,
+                                    const mozilla::net::NetAddr& addr);
+  virtual bool RecvRequestDelete() MOZ_OVERRIDE;
+
+private:
+  virtual void ActorDestroy(ActorDestroyReason why) MOZ_OVERRIDE;
+
+  bool mIPCOpen;
+  nsCOMPtr<nsIUDPSocket> mSocket;
+};
+
+} 
+} 
+
+#endif 
