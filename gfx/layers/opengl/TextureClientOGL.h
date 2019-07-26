74ecdcd64478e8998bbb8796de11efcb9b7da85b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:24:31 +0000

    Bug 887791 - Add a TextureClient implementation for MacIOSurface. r=nical
    UltraBlame original commit: 2d521539a285442263b8784f2db1130be18ae84c

--- gfx/layers/opengl/TextureClientOGL.h
+++ gfx/layers/opengl/TextureClientOGL.h
@@ -13,6 +13,9 @@
 #include "mozilla/layers/CompositorTypes.h"
 #include "mozilla/layers/LayersSurfaces.h"  
 #include "mozilla/layers/TextureClient.h"  
+#ifdef XP_MACOSX
+#include "mozilla/gfx/MacIOSurface.h"
+#endif
 
 namespace mozilla {
 namespace layers {
@@ -57,6 +60,44 @@ protected:
   bool mInverted;
 };
 
+#ifdef XP_MACOSX
+class MacIOSurfaceTextureClientOGL : public TextureClient
+{
+public:
+  MacIOSurfaceTextureClientOGL(TextureFlags aFlags)
+    : TextureClient(aFlags)
+  {}
+
+  virtual bool IsAllocated() const MOZ_OVERRIDE { return !!mSurface; }
+
+  virtual bool ToSurfaceDescriptor(SurfaceDescriptor& aOutDescriptor) MOZ_OVERRIDE;
+
+  void InitWith(MacIOSurface* aSurface)
+  {
+    MOZ_ASSERT(IsValid());
+    MOZ_ASSERT(!IsAllocated());
+    mSurface = aSurface;
+  }
+
+  virtual gfx::IntSize GetSize() const
+  {
+    return gfx::IntSize(mSurface->GetDevicePixelWidth(), mSurface->GetDevicePixelHeight());
+  }
+
+  virtual TextureClientData* DropTextureData() MOZ_OVERRIDE
+  {
+    
+    
+    
+    mSurface = nullptr;
+    MarkInvalid();
+    return nullptr;
+  }
+
+protected:
+  RefPtr<MacIOSurface> mSurface;
+};
+#endif
 
 class DeprecatedTextureClientSharedOGL : public DeprecatedTextureClient
 {
