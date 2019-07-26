e95b54cc592fab01ebeb5106dc02fd35537b5f20
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:01:21 +0000

    Bug 682048 - Change Metro frame script handling to support anon/global scope (r=jimm,mbrubeck)
    UltraBlame original commit: 4a15ec074e4789bfcaba2398b26471a08595c2a0

--- browser/metro/base/content/Util.js
+++ browser/metro/base/content/Util.js
@@ -2,6 +2,9 @@
 
 
 
+let Cc = Components.classes;
+let Ci = Components.interfaces;
+
 Components.utils.import("resource:///modules/ContentUtil.jsm");
 
 let Util = {
@@ -409,3 +412,5 @@ Util.Timeout.prototype = {
       Util[name] = copy;
   }
 }
+
+this.Util = Util;
--- browser/metro/base/content/contenthandlers/ContextMenuHandler.js
+++ browser/metro/base/content/contenthandlers/ContextMenuHandler.js
@@ -2,7 +2,10 @@
 
 
 
-const kXLinkNamespace = "http://www.w3.org/1999/xlink";
+let Ci = Components.interfaces;
+let Cc = Components.classes;
+
+this.kXLinkNamespace = "http://www.w3.org/1999/xlink";
 
 dump("### ContextMenuHandler.js loaded\n");
 
@@ -429,5 +432,6 @@ var ContextMenuHandler = {
     this._types = this._types.filter(function(type) type.name != aName);
   }
 };
+this.ContextMenuHandler = ContextMenuHandler;
 
 ContextMenuHandler.init();
