646c9cd8e157624b4a856fb60f8f40847ee09b11
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:45:13 +0000

    Bug 941793 - Always remove store buffer entries on relocation r=terrence
    UltraBlame original commit: 4de097a9f7a69a10e312bdfde008af4885706579

---
+++ js/src/jsapi-tests/testGCStoreBufferRemoval.cpp
@@ -0,0 +1,114 @@
+
+
+
+
+
+
+
+#ifdef JSGC_GENERATIONAL
+
+#include "gc/Barrier.h"
+#include "jsapi-tests/tests.h"
+
+using namespace JS;
+using namespace js;
+
+BEGIN_TEST(testGCStoreBufferRemoval)
+{
+    
+    JS_GC(cx->runtime());
+    JS::RootedObject obj(cx, NurseryObject());
+    CHECK(js::gc::IsInsideNursery(rt, obj.get()));
+    JS_GC(cx->runtime());
+    CHECK(!js::gc::IsInsideNursery(rt, obj.get()));
+    JS::RootedObject tenuredObject(cx, obj);
+
+    
+    {
+        JSObject *badObject = reinterpret_cast<JSObject*>(1);
+        JSObject *punnedPtr = nullptr;
+        RelocatablePtrObject* relocPtr =
+            reinterpret_cast<RelocatablePtrObject*>(&punnedPtr);
+        new (relocPtr) RelocatablePtrObject;
+        *relocPtr = NurseryObject();
+        relocPtr->~RelocatablePtrObject();
+        punnedPtr = badObject;
+        JS_GC(cx->runtime());
+
+        new (relocPtr) RelocatablePtrObject;
+        *relocPtr = NurseryObject();
+        *relocPtr = tenuredObject;
+        relocPtr->~RelocatablePtrObject();
+        punnedPtr = badObject;
+        JS_GC(cx->runtime());
+
+        new (relocPtr) RelocatablePtrObject;
+        *relocPtr = NurseryObject();
+        *relocPtr = nullptr;
+        relocPtr->~RelocatablePtrObject();
+        punnedPtr = badObject;
+        JS_GC(cx->runtime());
+    }
+
+    
+    {
+        Value punnedValue;
+        RelocatableValue *relocValue = reinterpret_cast<RelocatableValue*>(&punnedValue);
+        new (relocValue) RelocatableValue;
+        *relocValue = ObjectValue(*NurseryObject());
+        relocValue->~RelocatableValue();
+        punnedValue = ObjectValueCrashOnTouch();
+        JS_GC(cx->runtime());
+
+        new (relocValue) RelocatableValue;
+        *relocValue = ObjectValue(*NurseryObject());
+        *relocValue = ObjectValue(*tenuredObject);
+        relocValue->~RelocatableValue();
+        punnedValue = ObjectValueCrashOnTouch();
+        JS_GC(cx->runtime());
+
+        new (relocValue) RelocatableValue;
+        *relocValue = ObjectValue(*NurseryObject());
+        *relocValue = NullValue();
+        relocValue->~RelocatableValue();
+        punnedValue = ObjectValueCrashOnTouch();
+        JS_GC(cx->runtime());
+    }
+
+    
+    {
+        JSObject *badObject = reinterpret_cast<JSObject*>(1);
+        JSObject *punnedPtr = nullptr;
+        Heap<JSObject*>* heapPtr =
+            reinterpret_cast<Heap<JSObject*>*>(&punnedPtr);
+        new (heapPtr) Heap<JSObject*>;
+        *heapPtr = NurseryObject();
+        heapPtr->~Heap<JSObject*>();
+        punnedPtr = badObject;
+        JS_GC(cx->runtime());
+
+        new (heapPtr) Heap<JSObject*>;
+        *heapPtr = NurseryObject();
+        *heapPtr = tenuredObject;
+        heapPtr->~Heap<JSObject*>();
+        punnedPtr = badObject;
+        JS_GC(cx->runtime());
+
+        new (heapPtr) Heap<JSObject*>;
+        *heapPtr = NurseryObject();
+        *heapPtr = nullptr;
+        heapPtr->~Heap<JSObject*>();
+        punnedPtr = badObject;
+        JS_GC(cx->runtime());
+    }
+
+    return true;
+}
+
+JSObject *NurseryObject()
+{
+    return JS_NewObject(cx, nullptr, nullptr, nullptr);
+}
+END_TEST(testGCStoreBufferRemoval)
+
+#endif
