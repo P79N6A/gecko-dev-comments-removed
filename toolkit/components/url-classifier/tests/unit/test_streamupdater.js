c6fa5ce26db6e13545f6630d7c0a4ac25eedc54b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:18:48 +0000

    Bug 783047 - Update tests to remove MAC support for SafeBrowsing. r=mmc,dcamp
    UltraBlame original commit: db7475f5aecbe8b4f4006b5f0dad794cb0757730

--- toolkit/components/url-classifier/tests/unit/test_streamupdater.js
+++ toolkit/components/url-classifier/tests/unit/test_streamupdater.js
@@ -1,29 +1,9 @@
-var gClientKeyRaw="TESTCLIENTKEY";
-
-var gClientKey = "VEVTVENMSUVOVEtFWQ==";
-
-function MAC(content, clientKey)
-{
-  var hmac = Cc["@mozilla.org/security/hmac;1"].createInstance(Ci.nsICryptoHMAC);
-  var converter = Cc["@mozilla.org/intl/scriptableunicodeconverter"].
-                  createInstance(Ci.nsIScriptableUnicodeConverter);
-  converter.charset = "UTF-8";
-
-  var keyObject = Cc["@mozilla.org/security/keyobjectfactory;1"]
-    .getService(Ci.nsIKeyObjectFactory).keyFromString(Ci.nsIKeyObject.HMAC, clientKey);
-  hmac.init(Ci.nsICryptoHMAC.SHA1, keyObject);
-
-  var data = converter.convertToByteArray(content);
-  hmac.update(data, data.length);
-  return hmac.finish(true);
-}
-
-function doTest(updates, assertions, expectError, clientKey)
+function doTest(updates, assertions, expectError)
 {
   if (expectError) {
-    doUpdateTest(updates, assertions, updateError, runNextTest, clientKey);
+    doUpdateTest(updates, assertions, updateError, runNextTest);
   } else {
-    doUpdateTest(updates, assertions, runNextTest, updateError, clientKey);
+    doUpdateTest(updates, assertions, runNextTest, updateError);
   }
 }
 
@@ -181,180 +161,6 @@ function testMultipleTables() {
   doTest([update], assertions, false);
 }
 
-
-function testValidMAC() {
-  var addUrls = [ "foo.com/a", "foo.com/b", "bar.com/c" ];
-  var update = buildPhishingUpdate(
-        [
-          { "chunkNum" : 1,
-            "urls" : addUrls
-          }]);
-  update = "m:" + MAC(update, gClientKeyRaw) + "\n" + update;
-
-  var assertions = {
-    "tableData" : "test-phish-simple;a:1",
-    "urlsExist" : addUrls
-  };
-
-  doTest([update], assertions, false, gClientKey);
-}
-
-
-function testInvalidMAC() {
-  var addUrls = [ "foo.com/a", "foo.com/b", "bar.com/c" ];
-  var update = buildPhishingUpdate(
-        [
-          { "chunkNum" : 1,
-            "urls" : addUrls
-          }]);
-  update = "m:INVALIDMAC\n" + update;
-
-  var assertions = {
-    "tableData" : "",
-    "urlsDontExist" : addUrls
-  };
-
-  doTest([update], assertions, true, gClientKey);
-}
-
-
-
-function testNoMAC() {
-  var addUrls = [ "foo.com/a", "foo.com/b", "bar.com/c" ];
-  var update = buildPhishingUpdate(
-        [
-          { "chunkNum" : 1,
-            "urls" : addUrls
-          }]);
-
-  var assertions = {
-    "tableData" : "",
-    "urlsDontExist" : addUrls
-  };
-
-  doTest([update], assertions, true, gClientKey);
-}
-
-
-function testValidForwardMAC() {
-  var add1Urls = [ "foo.com/a", "bar.com/c" ];
-  var add2Urls = [ "foo.com/b" ];
-  var add3Urls = [ "bar.com/d" ];
-
-  var update = "n:1000\n";
-  update += "i:test-phish-simple\n";
-
-  var update1 = buildBareUpdate(
-    [{ "chunkNum" : 1,
-       "urls" : add1Urls }]);
-  update += "u:data:," + encodeURIComponent(update1) +
-    "," + MAC(update1, gClientKeyRaw) + "\n";
-
-  var update2 = buildBareUpdate(
-    [{ "chunkNum" : 2,
-       "urls" : add2Urls }]);
-  update += "u:data:," + encodeURIComponent(update2) +
-    "," + MAC(update2, gClientKeyRaw) + "\n";
-
-
-  var update3 = buildBareUpdate(
-    [{ "chunkNum" : 3,
-       "urls" : add3Urls }]);
-  update += "u:data:," + encodeURIComponent(update3) +
-    "," + MAC(update3, gClientKeyRaw) + "\n";
-
-  var assertions = {
-    "tableData" : "test-phish-simple;a:1-3",
-    "urlsExist" : add1Urls.concat(add2Urls).concat(add3Urls)
-  };
-
-  update = "m:" + MAC(update, gClientKeyRaw) + "\n" + update;
-
-  doTest([update], assertions, false, gClientKey);
-}
-
-
-
-function testInvalidForwardMAC() {
-  var add1Urls = [ "foo.com/a", "bar.com/c" ];
-  var add2Urls = [ "foo.com/b" ];
-  var add3Urls = [ "bar.com/d" ];
-
-  var update = "n:1000\n";
-  update += "i:test-phish-simple\n";
-
-  var update1 = buildBareUpdate(
-    [{ "chunkNum" : 1,
-       "urls" : add1Urls }]);
-  update += "u:data:," + encodeURIComponent(update1) +
-    ",BADMAC\n";
-
-  var update2 = buildBareUpdate(
-    [{ "chunkNum" : 2,
-       "urls" : add2Urls }]);
-  update += "u:data:," + encodeURIComponent(update2) +
-    ",BADMAC\n";
-
-
-  var update3 = buildBareUpdate(
-    [{ "chunkNum" : 3,
-       "urls" : add3Urls }]);
-  update += "u:data:," + encodeURIComponent(update3) +
-    ",BADMAC\n";
-
-  var assertions = {
-    "tableData" : "",
-    "urlsDontExist" : add1Urls.concat(add2Urls).concat(add3Urls)
-  };
-
-  update = "m:" + MAC(update, gClientKeyRaw) + "\n" + update;
-
-  doTest([update], assertions, true, gClientKey);
-}
-
-
-
-function testNoForwardMAC() {
-  var add1Urls = [ "foo.com/a", "bar.com/c" ];
-  var add2Urls = [ "foo.com/b" ];
-  var add3Urls = [ "bar.com/d" ];
-
-  var update = "n:1000\n";
-  update += "i:test-phish-simple\n";
-
-  
-  
-  
-
-  
-  
-  
-
-  var update1 = buildBareUpdate(
-    [{ "chunkNum" : 1,
-       "urls" : add1Urls }]);
-  update += "u:data:" + encodeURIComponent(update1) + "\n";
-
-  var update2 = buildBareUpdate(
-    [{ "chunkNum" : 2,
-       "urls" : add2Urls }]);
-  update += "u:data:" + encodeURIComponent(update2) + "\n";
-
-  var update3 = buildBareUpdate(
-    [{ "chunkNum" : 3,
-       "urls" : add3Urls }]);
-  update += "u:data:" + encodeURIComponent(update3) + "\n";
-
-  var assertions = {
-    "tableData" : "",
-    "urlsDontExist" : add1Urls.concat(add2Urls).concat(add3Urls)
-  };
-
-  update = "m:" + MAC(update, gClientKeyRaw) + "\n" + update;
-
-  doTest([update], assertions, true, gClientKey);
-}
-
 function Observer(callback) {
   this.observe = callback;
 }
@@ -370,44 +176,6 @@ QueryInterface: function(iid)
   return this;
 }
 };
-
-var gGotRekey;
-
-gAssertions.gotRekey = function(data, cb)
-{
-  do_check_eq(gGotRekey, data);
-  cb();
-}
-
-
-function testRekey() {
-  var addUrls = [ "foo.com/a", "foo.com/b", "bar.com/c" ];
-  var update = buildPhishingUpdate(
-        [
-          { "chunkNum" : 1,
-            "urls" : addUrls
-          }]);
-  update = "e:pleaserekey\n" + update;
-
-  var assertions = {
-    "tableData" : "",
-    "urlsDontExist" : addUrls,
-    "gotRekey" : true
-  };
-
-  gGotRekey = false;
-  var observerService = Cc["@mozilla.org/observer-service;1"].getService(Ci.nsIObserverService);
-
-  observerService.addObserver(new Observer(function(subject, topic, data) {
-        if (topic == "url-classifier-rekey-requested") {
-          gGotRekey = true;
-        }
-      }),
-    "url-classifier-rekey-requested",
-    false);
-
-  doTest([update], assertions, true, gClientKey);
-}
 
 
 function testReset() {
@@ -445,16 +213,7 @@ function run_test()
     testInvalidUrlForward,
     testErrorUrlForward,
     testMultipleTables,
-    testReset,
-    
-    
-    testValidMAC,
-    testInvalidMAC,
-    testNoMAC,
-    testValidForwardMAC,
-    testInvalidForwardMAC,
-    testNoForwardMAC,
-    testRekey
+    testReset
   ]);
 }
 
