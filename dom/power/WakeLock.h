c78c09f99a5e3b794ced3317ef22fe6c94ab9c73
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:56:15 +0000

    Bug 836654 - Part 4: Add PowerManagerService::NewWakeLockOnBehalfOfProcess. r=cjones
    UltraBlame original commit: 691f0a62b261e89137d8a5de1191e4d1578a4acc

--- dom/power/WakeLock.h
+++ dom/power/WakeLock.h
@@ -9,6 +9,7 @@
 #include "nsCOMPtr.h"
 #include "nsIDOMWakeLock.h"
 #include "nsIDOMEventListener.h"
+#include "nsIObserver.h"
 #include "nsString.h"
 #include "nsWeakReference.h"
 
@@ -16,21 +17,35 @@ class nsIDOMWindow;
 
 namespace mozilla {
 namespace dom {
+
+class ContentParent;
+
 namespace power {
 
 class WakeLock
   : public nsIDOMMozWakeLock
   , public nsIDOMEventListener
+  , public nsIObserver
+  , public nsSupportsWeakReference
 {
 public:
   NS_DECL_ISUPPORTS
   NS_DECL_NSIDOMMOZWAKELOCK
   NS_DECL_NSIDOMEVENTLISTENER
+  NS_DECL_NSIOBSERVER
 
   WakeLock();
   virtual ~WakeLock();
 
-  nsresult Init(const nsAString &aTopic, nsIDOMWindow *aWindow);
+  
+  
+  
+  nsresult Init(const nsAString &aTopic, nsIDOMWindow* aWindow);
+
+  
+  
+  
+  nsresult Init(const nsAString &aTopic, ContentParent* aContentParent);
 
 private:
   void     DoUnlock();
@@ -40,6 +55,11 @@ private:
 
   bool      mLocked;
   bool      mHidden;
+
+  
+  
+  
+  uint64_t  mContentParentID;
   nsString  mTopic;
 
   
