11dc5ade0c35e97034ccac9e9c3de7809ae1716a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:20:20 +0000

    Bug 1024688 - Add a MaybeOneOf class to MFBT. r=luke
    UltraBlame original commit: 7244e4bee4a24cb4bc61e26400b3c5c0260d48cc

---
+++ mfbt/MaybeOneOf.h
@@ -0,0 +1,131 @@
+
+
+
+
+
+
+#ifndef mozilla_MaybeOneOf_h
+#define mozilla_MaybeOneOf_h
+
+#include "mozilla/Alignment.h"
+#include "mozilla/Assertions.h"
+#include "mozilla/Move.h"
+#include "mozilla/TemplateLib.h"
+
+
+#include <new>
+
+namespace mozilla {
+
+
+
+
+
+
+
+
+
+
+template<class T1, class T2>
+class MaybeOneOf
+{
+  AlignedStorage<tl::Max<sizeof(T1), sizeof(T2)>::value> storage;
+
+  enum State { None, SomeT1, SomeT2 } state;
+  template <class T, class Ignored = void> struct Type2State {};
+
+  template <class T>
+  T& as() {
+    MOZ_ASSERT(state == Type2State<T>::result);
+    return *(T*)storage.addr();
+  }
+
+  template <class T>
+  const T& as() const {
+    MOZ_ASSERT(state == Type2State<T>::result);
+    return *(T*)storage.addr();
+  }
+
+ public:
+  MaybeOneOf() : state(None) {}
+  ~MaybeOneOf() { destroyIfConstructed(); }
+
+  bool empty() const { return state == None; }
+
+  template <class T>
+  bool constructed() const { return state == Type2State<T>::result; }
+
+  template <class T>
+  void construct() {
+    MOZ_ASSERT(state == None);
+    state = Type2State<T>::result;
+    ::new (storage.addr()) T();
+  }
+
+  template <class T, class U>
+  void construct(U&& u) {
+    MOZ_ASSERT(state == None);
+    state = Type2State<T>::result;
+    ::new (storage.addr()) T(Move(u));
+  }
+
+  template <class T, class U1>
+  void construct(const U1& u1) {
+    MOZ_ASSERT(state == None);
+    state = Type2State<T>::result;
+    ::new (storage.addr()) T(u1);
+  }
+
+  template <class T, class U1, class U2>
+  void construct(const U1& u1, const U2& u2) {
+    MOZ_ASSERT(state == None);
+    state = Type2State<T>::result;
+    ::new (storage.addr()) T(u1, u2);
+  }
+
+  template <class T>
+  T& ref() {
+    return as<T>();
+  }
+
+  template <class T>
+  const T& ref() const {
+    return as<T>();
+  }
+
+  void destroy() {
+    MOZ_ASSERT(state == SomeT1 || state == SomeT2);
+    if (state == SomeT1)
+      as<T1>().~T1();
+    else if (state == SomeT2)
+      as<T2>().~T2();
+    state = None;
+  }
+
+  void destroyIfConstructed() {
+    if (!empty())
+      destroy();
+  }
+
+  private:
+    MaybeOneOf(const MaybeOneOf& other) MOZ_DELETE;
+    const MaybeOneOf& operator=(const MaybeOneOf& other) MOZ_DELETE;
+};
+
+template <class T1, class T2>
+template <class Ignored>
+struct MaybeOneOf<T1, T2>::Type2State<T1, Ignored> {
+  typedef MaybeOneOf<T1, T2> Enclosing;
+  static const typename Enclosing::State result = Enclosing::SomeT1;
+};
+
+template <class T1, class T2>
+template <class Ignored>
+struct MaybeOneOf<T1, T2>::Type2State<T2, Ignored> {
+  typedef MaybeOneOf<T1, T2> Enclosing;
+  static const typename Enclosing::State result = Enclosing::SomeT2;
+};
+
+} 
+
+#endif 
