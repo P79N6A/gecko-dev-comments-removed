d79b2b90948aadb9574b8f23a4e6b9992db94061
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:43:21 +0000

    bug 951959 - GeckoScreenOrientationListener is invoked hundreds of times during page load r=rnewman
    UltraBlame original commit: ac3d8111a80674670218c0d07671b3be495c8930

--- mobile/android/base/GeckoScreenOrientationListener.java
+++ mobile/android/base/GeckoScreenOrientationListener.java
@@ -2,6 +2,7 @@
 
 
 
+
 package org.mozilla.gecko;
 
 import android.content.Context;
@@ -25,7 +26,7 @@ public class GeckoScreenOrientationListener {
 
         @Override
         public void onOrientationChanged(int aOrientation) {
-            GeckoScreenOrientationListener.getInstance().updateScreenOrientation();
+            GeckoScreenOrientationListener.getInstance().updateScreenOrientation(aOrientation);
         }
     }
 
@@ -149,23 +150,27 @@ public class GeckoScreenOrientationListener {
     }
 
     private void updateScreenOrientation() {
-      Context context = GeckoAppShell.getContext();
-      int rotation = mDefaultOrientation;
-      if (context instanceof Activity) {
-        rotation = ((Activity)context).getWindowManager().getDefaultDisplay().getRotation();
-      }
+        Context context = GeckoAppShell.getContext();
+        int rotation = mDefaultOrientation;
+        if (context instanceof Activity) {
+            rotation = ((Activity)context).getWindowManager().getDefaultDisplay().getRotation();
+        }
+        updateScreenOrientation(rotation * 90);
+    }
+
+    private void updateScreenOrientation(int aOrientation) {
         short previousOrientation = mOrientation;
 
-        if (rotation == Surface.ROTATION_0) {
+        if (aOrientation >= 315  || aOrientation < 45) {
             mOrientation = eScreenOrientation_PortraitPrimary;
-        } else if (rotation == Surface.ROTATION_180) {
-            mOrientation = eScreenOrientation_PortraitSecondary;
-        } else if (rotation == Surface.ROTATION_270) {
-            mOrientation = eScreenOrientation_LandscapeSecondary;
-        } else if (rotation == Surface.ROTATION_90) {
+        } else if (aOrientation >= 45 && aOrientation < 135) {
             mOrientation = eScreenOrientation_LandscapePrimary;
+        } else if (aOrientation >= 135 && aOrientation < 225) {
+            mOrientation = eScreenOrientation_PortraitSecondary;
+        } else if (aOrientation >= 225 && aOrientation < 315) {
+            mOrientation = eScreenOrientation_LandscapeSecondary;
         } else {
-            Log.e(LOGTAG, "Unexpected value received! (" + rotation + ")");
+            Log.e(LOGTAG, "Unexpected value received! (" + aOrientation + ")");
             return;
         }
 
@@ -207,18 +212,18 @@ public class GeckoScreenOrientationListener {
             Log.e(LOGTAG, "Unexpected value received! (" + aOrientation + ")");
             return;
         }
-	if (GeckoAppShell.getContext() instanceof Activity)
-	  ((Activity)GeckoAppShell.getContext()).setRequestedOrientation(orientation);
+        if (GeckoAppShell.getContext() instanceof Activity)
+            ((Activity)GeckoAppShell.getContext()).setRequestedOrientation(orientation);
         updateScreenOrientation();
     }
 
     public void unlockScreenOrientation() {
-      if (!(GeckoAppShell.getContext() instanceof Activity))
-	return;
-      if (((Activity)GeckoAppShell.getContext()).getRequestedOrientation() == mDefaultOrientation)
-	return;
+        if (!(GeckoAppShell.getContext() instanceof Activity))
+            return;
+        if (((Activity)GeckoAppShell.getContext()).getRequestedOrientation() == mDefaultOrientation)
+            return;
 
-      ((Activity)GeckoAppShell.getContext()).setRequestedOrientation(mDefaultOrientation);
+        ((Activity)GeckoAppShell.getContext()).setRequestedOrientation(mDefaultOrientation);
         updateScreenOrientation();
     }
 }
