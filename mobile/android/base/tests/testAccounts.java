5f577243cc105db42bad3588e19d4500ab237481
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:56:15 +0000

    Bug 917942 - Create a JS interface to Sync configuration. r=margaret
    UltraBlame original commit: 2af0db58021bf6fbc0633fea08f46fe81fd11afc

---
+++ mobile/android/base/tests/testAccounts.java
@@ -0,0 +1,28 @@
+
+
+
+
+package org.mozilla.gecko.tests;
+
+import org.mozilla.gecko.*;
+import org.mozilla.gecko.fxa.activities.FxAccountGetStartedActivity;
+
+public class testAccounts extends JavascriptTest {
+    public testAccounts() {
+        super("testAccounts.js");
+    }
+
+    @Override
+    public void testJavascript() throws Exception {
+        super.testJavascript();
+
+        
+        
+        
+        
+
+
+
+
+    }
+}
