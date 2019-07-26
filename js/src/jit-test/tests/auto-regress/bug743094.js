4288f3af3902326df475a74a87fd9499f3f0ebf8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:14:00 +0000

    Bug 867856 - Fix intermittent timeout of bug743094.js test case. r=h4writer
    DONTBUILD
    
    UltraBlame original commit: b45bdd149e3441b06be8554d790d326425917c52

--- js/src/jit-test/tests/auto-regress/bug743094.js
+++ js/src/jit-test/tests/auto-regress/bug743094.js
@@ -2,10 +2,14 @@
 
 
 
+var i = 0;
 
-
-gczeal(2)
+gczeal(2);
 function test() {
-  typeof (new test("1")) != 'function'
+  if (i++ > 10000)
+    return "function";
+  var res = typeof (new test("1")) != 'function';
+  return res ? "function" : "string";
 }
+
 test();
