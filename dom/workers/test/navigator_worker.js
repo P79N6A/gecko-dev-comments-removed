ef3f114bd2ce1ce62fb93b1b4bec5648fb908a5f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:51:00 +0000

    Bug 949325 - C++ wrapper to support DataStore API on the worker (part 3-1, fix tests to support navigator.getDataStores on worker). r=khuey
    UltraBlame original commit: 2bbfeafe352cd73226044341d78eefc92be99ac5

--- dom/workers/test/navigator_worker.js
+++ dom/workers/test/navigator_worker.js
@@ -2,10 +2,13 @@
 
 
 
+
+
 var supportedProps = [
   "appCodeName",
   "appName",
   "appVersion",
+  { name: "getDataStores", b2g: true },
   "platform",
   "product",
   "taintEnabled",
@@ -13,26 +16,55 @@ var supportedProps = [
   "onLine"
 ];
 
+var isDesktop = !/Mobile|Tablet/.test(navigator.userAgent);
+var isB2G = !isDesktop && !navigator.userAgent.contains("Android");
+
+
+
+var interfaceMap = {};
+
+for (var prop of supportedProps) {
+  if (typeof(prop) === "string") {
+    interfaceMap[prop] = true;
+    continue;
+  }
+
+  if (prop.b2g === !isB2G) {
+    interfaceMap[prop.name] = false;
+    continue;
+  }
+
+  interfaceMap[prop.name] = true;
+}
+
 for (var prop in navigator) {
   
-  if (supportedProps.indexOf(prop) == -1) {
+  if (!interfaceMap[prop]) {
     throw "Navigator has the '" + prop + "' property that isn't in the list!";
   }
 }
 
 var obj;
 
-for (var index = 0; index < supportedProps.length; index++) {
-  var prop = supportedProps[index];
+for (var prop in interfaceMap) {
+  
+  if (!interfaceMap[prop]) {
+    continue;
+  }
 
   if (typeof navigator[prop] == "undefined") {
     throw "Navigator has no '" + prop + "' property!";
   }
 
-  obj = {
-    name:  prop,
-    value: prop === "taintEnabled" ? navigator[prop]() : navigator[prop]
-  };
+  obj = { name:  prop };
+
+  if (prop === "taintEnabled") {
+    obj.value = navigator[prop]();
+  } else if (prop === "getDataStores") {
+    obj.value = typeof navigator[prop];
+  } else {
+    obj.value = navigator[prop];
+  }
 
   postMessage(JSON.stringify(obj));
 }
