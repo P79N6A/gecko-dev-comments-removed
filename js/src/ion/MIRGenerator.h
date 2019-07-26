2ec6611abc6c8fa17a5159826816581f507dd12a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:16:20 +0000

    Allow canceling in-progress off thread ion compilations, bug 785761. r=dvander
    UltraBlame original commit: 8a6b6b5f3e559767d2dd7861ba3ebcee965bbcae

--- js/src/ion/MIRGenerator.h
+++ js/src/ion/MIRGenerator.h
@@ -64,6 +64,14 @@ class MIRGenerator
         return compartment->rt->spsProfiler.enabled();
     }
 
+    
+    bool shouldCancel(const char *why) {
+        return cancelBuild_;
+    }
+    void cancel() {
+        cancelBuild_ = 1;
+    }
+
   public:
     JSCompartment *compartment;
 
@@ -74,6 +82,7 @@ class MIRGenerator
     uint32 nslots_;
     MIRGraph *graph_;
     bool error_;
+    size_t cancelBuild_;
 };
 
 } 
