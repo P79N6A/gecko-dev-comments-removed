bc7ee271813b63bbfd16d9d9e0932ebf6669419c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:55:18 +0000

    Bug 587780 - Part 5 - Test Google search params with @purpose. r=gavin
    UltraBlame original commit: 1ba8ddf7bef0f0e4cebae28bec3d7dde41dcbcb6

---
+++ browser/components/search/test/head.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+function isSubObjectOf(expectedObj, actualObj, name) {
+  for (let prop in expectedObj) {
+    if (typeof expectedObj[prop] == 'function')
+      continue;
+    if (expectedObj[prop] instanceof Object) {
+      is(actualObj[prop].length, expectedObj[prop].length, name + "[" + prop + "]");
+      isSubObjectOf(expectedObj[prop], actualObj[prop], name + "[" + prop + "]");
+    } else {
+      is(actualObj[prop], expectedObj[prop], name + "[" + prop + "]");
+    }
+  }
+}
