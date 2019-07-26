02fd2236cfb763ca3be5749348fa6c7af52b9973
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:11:39 +0000

    Back out b65289e2b06c (bug 853642) because of mochitest failures on a CLOSED TREE
    UltraBlame original commit: 109739e766626d94f22ae3086fcb34b8c40c2523

--- browser/base/content/browser-social.js
+++ browser/base/content/browser-social.js
@@ -170,10 +170,12 @@ let SocialUI = {
   },
 
   _updateMenuItems: function () {
-    let provider = Social.provider || Social.defaultProvider;
+    if (!Social.provider)
+      return;
+
     
     for (let id of ["menu_socialSidebar", "menu_socialAmbientMenu"])
-      document.getElementById(id).setAttribute("label", provider.name);
+      document.getElementById(id).setAttribute("label", Social.provider.name);
   },
 
   
