c2499c1f9d92ff3e158fdc1ea5b230a3d0b278fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:00:30 +0000

    Bug 830551 - Send file-transfer-complete message after receiving unexpected response code from remote, r=echou
    UltraBlame original commit: 924c07c3cb32d4120380fafec06c1873322e1a63

--- dom/bluetooth/BluetoothOppManager.cpp
+++ dom/bluetooth/BluetoothOppManager.cpp
@@ -361,6 +361,7 @@ BluetoothOppManager::SendFile(BlobParent* aActor)
 
   SendConnectRequest();
   mTransferMode = false;
+  StartFileTransfer();
 
   return true;
 }
@@ -798,23 +799,25 @@ BluetoothOppManager::ClientDataHandler(UnixSocketRawData* aMessage)
   }
 
   
-  if (mLastCommand == ObexRequestCode::Put &&
-      opCode != ObexResponseCode::Continue) {
-    NS_WARNING("[OPP] Put(0x02) failed");
-    SendDisconnectRequest();
-    return;
-  } else if (mLastCommand == ObexRequestCode::Abort ||
-             mLastCommand == ObexRequestCode::Connect ||
-             mLastCommand == ObexRequestCode::Disconnect ||
-             mLastCommand == ObexRequestCode::PutFinal){
-    if (opCode != ObexResponseCode::Success) {
-      nsAutoCString str;
-      str += "[OPP] 0x";
-      str += mLastCommand;
-      str += " failed";
-      NS_WARNING(str.get());
-      return;
+  
+  uint8_t expectedOpCode = ObexResponseCode::Success;
+  if (mLastCommand == ObexRequestCode::Put) {
+    expectedOpCode = ObexResponseCode::Continue;
+  }
+
+  if (opCode != expectedOpCode) {
+    if (mLastCommand == ObexRequestCode::Put ||
+        mLastCommand == ObexRequestCode::Abort ||
+        mLastCommand == ObexRequestCode::PutFinal) {
+      SendDisconnectRequest();
     }
+    nsAutoCString str;
+    str += "[OPP] 0x";
+    str.AppendInt(mLastCommand, 16);
+    str += " failed";
+    NS_WARNING(str.get());
+    FileTransferComplete();
+    return;
   }
 
   if (mLastCommand == ObexRequestCode::PutFinal) {
@@ -849,7 +852,6 @@ BluetoothOppManager::ClientDataHandler(UnixSocketRawData* aMessage)
 
     if (ExtractBlobHeaders()) {
       sInstance->SendPutHeaderRequest(sFileName, sFileLength);
-      StartFileTransfer();
     }
   } else if (mLastCommand == ObexRequestCode::Put) {
 
