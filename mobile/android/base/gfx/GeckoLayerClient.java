4d9c0818e8d5ba25357604ed0794fb917c9b954d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:08:02 +0000

    Bug 960146. r=kats,wesj
    UltraBlame original commit: 28537b55795f2adbef214acc9a41cbb694c3d5d5

--- mobile/android/base/gfx/GeckoLayerClient.java
+++ mobile/android/base/gfx/GeckoLayerClient.java
@@ -887,7 +887,7 @@ public class GeckoLayerClient implements LayerView.Listener, PanZoomTarget
 
 
     @Override
-    public void onSubdocumentScrollBy(float dx, float dy) {
+    public void scrollMarginsBy(float dx, float dy) {
         ImmutableViewportMetrics newMarginsMetrics =
             mMarginsAnimator.scrollBy(mViewportMetrics, dx, dy);
         mViewportMetrics = mViewportMetrics.setMarginsFrom(newMarginsMetrics);
