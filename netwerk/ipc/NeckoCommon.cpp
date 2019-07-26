bc53d3d08c393caa7a77729440051eaea1588327
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:47:53 +0000

    Bug 833935 - Stop using one static var per compilation unit. r=jdm
    UltraBlame original commit: 629d2d1485e291ceebb21837e2aeab9c72c6ebc1

---
+++ netwerk/ipc/NeckoCommon.cpp
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+#include "NeckoCommon.h"
+
+namespace mozilla {
+namespace net {
+
+namespace NeckoCommonInternal {
+  bool gSecurityDisabled = true;
+  bool gRegisteredBool = false;
+}
+
+} 
+} 
