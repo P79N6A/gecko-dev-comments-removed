cf921d5692e8ef1c410ac0b612040bb62ff3e9c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:03:10 +0000

    Bug 935799 - Add mochitest-e10s-utils.js to help tests when run in an e10s environment. r=felipe,ted
    UltraBlame original commit: 2ed56735cb6af92b1f7dd80dae1b084fa1e1fa24

---
+++ testing/mochitest/mochitest-e10s-utils-content.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+["load", "DOMContentLoaded", "pageshow"].forEach(eventName => {
+  addEventListener(eventName, function eventHandler(event) {
+    
+    
+    if (event.target == content.document) {
+      sendAsyncMessage("Test:Event", {name: event.type});
+    }
+  }, true);
+});
