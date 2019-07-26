3dc1f3282180af912e342b3223a670bfb241480b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:23:45 +0000

    Bug 863523 - Avoid post-barrier on global slots for JIT code; r=billm,terrence
    UltraBlame original commit: c0789f4c3113b6c06761fdf49b3d6188b2c39108

--- js/src/gc/RootMarking.cpp
+++ js/src/gc/RootMarking.cpp
@@ -653,6 +653,28 @@ JSPropertyDescriptor::trace(JSTracer *trc)
     }
 }
 
+static inline void
+MarkGlobalForMinorGC(JSTracer *trc, JSCompartment *compartment)
+{
+    
+
+
+
+
+    JS_ASSERT(trc->runtime->isHeapMinorCollecting());
+
+    if (!compartment->ionCompartment())
+        return;
+
+    GlobalObject *global = compartment->maybeGlobal();
+    if (!global)
+        return;
+
+    
+    for (size_t i = JSCLASS_RESERVED_SLOTS(global->getClass()); i < global->slotSpan(); ++i)
+        MarkValueRoot(trc, global->nativeGetSlotRef(i).unsafeGet(), "MinorGlobalRoot");
+}
+
 void
 js::gc::MarkRuntime(JSTracer *trc, bool useSavedRoots)
 {
@@ -740,6 +762,9 @@ js::gc::MarkRuntime(JSTracer *trc, bool useSavedRoots)
     for (CompartmentsIter c(rt); !c.done(); c.next()) {
         if (IS_GC_MARKING_TRACER(trc) && !c->zone()->isCollecting())
             continue;
+
+        if (trc->runtime->isHeapMinorCollecting())
+            MarkGlobalForMinorGC(trc, c);
 
         
         if (!IS_GC_MARKING_TRACER(trc)) {
