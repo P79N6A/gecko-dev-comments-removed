f8ad4b821a54049d781c0ffead9e0459e1d71ab6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:51:56 +0000

    Bug 986160 - Create unit tests for the Gecko Profiler. r=benwa
    UltraBlame original commit: 6e6296b804c29f90e7f2e1b55bbe4c89b4d606b8

---
+++ tools/profiler/tests/gtest/ThreadProfileTest.cpp
@@ -0,0 +1,69 @@
+
+
+
+
+
+#include "gtest/gtest.h"
+
+#include "ProfileEntry.h"
+
+
+TEST(ThreadProfile, Initialization) {
+  PseudoStack stack;
+  Thread::tid_t tid = 1000;
+  ThreadProfile tp("testThread", 10, &stack, tid, nullptr, true, nullptr);
+}
+
+
+TEST(ThreadProfile, InsertOneTag) {
+  PseudoStack stack;
+  Thread::tid_t tid = 1000;
+  ThreadProfile tp("testThread", 10, &stack, tid, nullptr, true, nullptr);
+  tp.addTag(ProfileEntry('t', 123.1f));
+  ASSERT_TRUE(tp.mEntries != nullptr);
+  ASSERT_TRUE(tp.mEntries[tp.mReadPos].mTagName == 't');
+  ASSERT_TRUE(tp.mEntries[tp.mReadPos].mTagFloat == 123.1f);
+}
+
+
+TEST(ThreadProfile, InsertTagsNoWrap) {
+  PseudoStack stack;
+  Thread::tid_t tid = 1000;
+  ThreadProfile tp("testThread", 100, &stack, tid, nullptr, true, nullptr);
+  int test_size = 50;
+  for (int i = 0; i < test_size; i++) {
+    tp.addTag(ProfileEntry('t', i));
+  }
+  ASSERT_TRUE(tp.mEntries != nullptr);
+  int readPos = tp.mReadPos;
+  while (readPos != tp.mWritePos) {
+    ASSERT_TRUE(tp.mEntries[readPos].mTagName == 't');
+    ASSERT_TRUE(tp.mEntries[readPos].mTagLine == readPos);
+    readPos = (readPos + 1) % tp.mEntrySize;
+  }
+}
+
+
+TEST(ThreadProfile, InsertTagsWrap) {
+  PseudoStack stack;
+  Thread::tid_t tid = 1000;
+  
+  int tags = 24;
+  int buffer_size = tags + 1;
+  ThreadProfile tp("testThread", buffer_size, &stack, tid, nullptr, true, nullptr);
+  int test_size = 43;
+  for (int i = 0; i < test_size; i++) {
+    tp.addTag(ProfileEntry('t', i));
+  }
+  ASSERT_TRUE(tp.mEntries != nullptr);
+  int readPos = tp.mReadPos;
+  int ctr = 0;
+  while (readPos != tp.mWritePos) {
+    ASSERT_TRUE(tp.mEntries[readPos].mTagName == 't');
+    
+    ASSERT_TRUE(tp.mEntries[readPos].mTagLine == ctr + (test_size - tags));
+    ctr++;
+    readPos = (readPos + 1) % tp.mEntrySize;
+  }
+}
+
