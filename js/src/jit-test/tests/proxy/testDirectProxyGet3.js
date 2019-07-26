8d7e6287bbe096e799bfce4b0a650d95dac2c23d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:01:24 +0000

    Bug 814892 - Actually compare the right values when making sure a proxy [[Get]] trap returns the correct value. r=efaust
    UltraBlame original commit: 5bcd468384a86605dc92dd6ce2986b1a5f6214ba

--- js/src/jit-test/tests/proxy/testDirectProxyGet3.js
+++ js/src/jit-test/tests/proxy/testDirectProxyGet3.js
@@ -17,3 +17,48 @@ assertThrowsInstanceOf(function () {
         }
     })['foo'];
 }, TypeError);
+
+
+
+
+
+assertEq(new Proxy(target, {
+        get: function (target, name, receiver) {
+            return 'bar';
+        }
+    })['foo'],
+    'bar');
+
+
+
+
+
+
+Object.defineProperty(target, 'prop', {
+    value: 'bar',
+    writable: true,
+    configurable: false
+});
+assertEq(new Proxy(target, {
+        get: function (target, name, receiver) {
+            return 'baz';
+        }
+    })['prop'],
+    'baz');
+
+
+
+
+
+
+Object.defineProperty(target, 'prop2', {
+    value: 'bar',
+    writable: false,
+    configurable: true
+});
+assertEq(new Proxy(target, {
+        get: function (target, name, receiver) {
+            return 'baz';
+        }
+    })['prop2'],
+    'baz');
