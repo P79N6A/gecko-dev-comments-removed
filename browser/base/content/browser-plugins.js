baa02e966be647149e9d895846eb58abb529f25f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:04:27 +0000

    Bug 916542 - Guard against non-plugin elements in DOMWindowUtils.plugins. r=jaws
    UltraBlame original commit: 688d314d5c2f140e1d61efab5b272bd03d11226d

--- browser/base/content/browser-plugins.js
+++ browser/base/content/browser-plugins.js
@@ -424,7 +424,7 @@ var gPluginHandler = {
   
   
   openPluginUpdatePage: function (aEvent) {
-    openUILinkIn(Services.urlFormatter.formatURLPref("plugins.update.url"), "tab");
+    openURL(Services.urlFormatter.formatURLPref("plugins.update.url"));
   },
 
 #ifdef MOZ_CRASHREPORTER
@@ -804,7 +804,9 @@ var gPluginHandler = {
     let contentDoc = aBrowser.contentDocument;
     let cwu = contentWindow.QueryInterface(Ci.nsIInterfaceRequestor)
                            .getInterface(Ci.nsIDOMWindowUtils);
-    let plugins = cwu.plugins;
+    
+    let plugins = cwu.plugins.filter((plugin) =>
+      plugin.getContentTypeForMIMEType(plugin.actualType) == Ci.nsIObjectLoadingContent.TYPE_PLUGIN);
     if (plugins.length == 0) {
       if (notification) {
         PopupNotifications.remove(notification);
