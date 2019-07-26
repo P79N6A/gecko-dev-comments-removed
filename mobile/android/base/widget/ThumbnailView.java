706a83f197d88cce1b959b841604292fd6c41e19
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:01:15 +0000

    Bug 845618 - Remove unused imports. r=kats
    UltraBlame original commit: e3738346f3075b556fb4ba4ec48c8700bfa5ad52

--- mobile/android/base/widget/ThumbnailView.java
+++ mobile/android/base/widget/ThumbnailView.java
@@ -10,7 +10,6 @@ import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.graphics.Matrix;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageView;
 
 
