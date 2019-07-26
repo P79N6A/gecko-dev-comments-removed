fcfb32c3412f5ca55d33916c7ce4810c8d51af38
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:09:08 +0000

    Bug 851611 - Part 4: Split out GeckoProfilerFunc.h & PseudoStack.h. r=jrmuizel
    UltraBlame original commit: ecce21507ea0ebdf25cee4187fb22cae51b468ba

---
+++ tools/profiler/PseudoStack.h
@@ -0,0 +1,251 @@
+
+
+
+
+
+#ifndef PROFILER_PSEUDO_STACK_H_
+#define PROFILER_PSEUDO_STACK_H_
+
+#include "mozilla/NullPtr.h"
+#include "mozilla/StandardInteger.h"
+#include "jsfriendapi.h"
+#include <stdlib.h>
+
+
+
+#if defined(_M_X64) || defined(__x86_64__)
+#define V8_HOST_ARCH_X64 1
+#elif defined(_M_IX86) || defined(__i386__) || defined(__i386)
+#define V8_HOST_ARCH_IA32 1
+#elif defined(__ARMEL__)
+#define V8_HOST_ARCH_ARM 1
+#else
+#warning Please add support for your architecture in chromium_types.h
+#endif
+
+
+
+
+
+#ifdef V8_HOST_ARCH_ARM
+
+
+
+typedef void (*LinuxKernelMemoryBarrierFunc)(void);
+LinuxKernelMemoryBarrierFunc pLinuxKernelMemoryBarrier __attribute__((weak)) =
+    (LinuxKernelMemoryBarrierFunc) 0xffff0fa0;
+
+# define STORE_SEQUENCER() pLinuxKernelMemoryBarrier()
+#elif defined(V8_HOST_ARCH_IA32) || defined(V8_HOST_ARCH_X64)
+# if defined(_MSC_VER)
+#if _MSC_VER > 1400
+#  include <intrin.h>
+#else 
+    
+#ifdef _WINNT_
+#  define _interlockedbittestandreset _interlockedbittestandreset_NAME_CHANGED_TO_AVOID_MSVS2005_ERROR
+#  define _interlockedbittestandset _interlockedbittestandset_NAME_CHANGED_TO_AVOID_MSVS2005_ERROR
+#  include <intrin.h>
+#else
+#  include <intrin.h>
+#  define _interlockedbittestandreset _interlockedbittestandreset_NAME_CHANGED_TO_AVOID_MSVS2005_ERROR
+#  define _interlockedbittestandset _interlockedbittestandset_NAME_CHANGED_TO_AVOID_MSVS2005_ERROR
+#endif
+   
+   
+#  pragma intrinsic(_ReadWriteBarrier)
+#endif 
+#  define STORE_SEQUENCER() _ReadWriteBarrier();
+# elif defined(__INTEL_COMPILER)
+#  define STORE_SEQUENCER() __memory_barrier();
+# elif __GNUC__
+#  define STORE_SEQUENCER() asm volatile("" ::: "memory");
+# else
+#  error "Memory clobber not supported for your compiler."
+# endif
+#else
+# error "Memory clobber not supported for your platform."
+#endif
+
+
+
+
+
+
+
+
+
+class StackEntry : public js::ProfileEntry
+{
+public:
+
+  bool isCopyLabel() volatile {
+    return !((uintptr_t)stackAddress() & 0x1);
+  }
+
+  void setStackAddressCopy(void *sparg, bool copy) volatile {
+    
+    
+    
+    
+    
+    if (copy) {
+      setStackAddress(reinterpret_cast<void*>(
+                        reinterpret_cast<uintptr_t>(sparg) & ~0x1));
+    } else {
+      setStackAddress(reinterpret_cast<void*>(
+                        reinterpret_cast<uintptr_t>(sparg) | 0x1));
+    }
+  }
+};
+
+
+
+struct PseudoStack
+{
+public:
+  PseudoStack()
+    : mStackPointer(0)
+    , mSignalLock(false)
+    , mMarkerPointer(0)
+    , mQueueClearMarker(false)
+    , mRuntime(nullptr)
+    , mStartJSSampling(false)
+  { }
+
+  void addMarker(const char *aMarker)
+  {
+    char* markerCopy = strdup(aMarker);
+    mSignalLock = true;
+    STORE_SEQUENCER();
+
+    if (mQueueClearMarker) {
+      clearMarkers();
+    }
+    if (!aMarker) {
+      return; 
+    }
+    if (size_t(mMarkerPointer) == mozilla::ArrayLength(mMarkers)) {
+      return; 
+    }
+    mMarkers[mMarkerPointer] = markerCopy;
+    mMarkerPointer++;
+
+    mSignalLock = false;
+    STORE_SEQUENCER();
+  }
+
+  
+  const char* getMarker(int aMarkerId)
+  {
+    
+    
+    
+    
+    
+    
+    
+    if (mSignalLock || mQueueClearMarker || aMarkerId < 0 ||
+      static_cast<mozilla::sig_safe_t>(aMarkerId) >= mMarkerPointer) {
+      return nullptr;
+    }
+    return mMarkers[aMarkerId];
+  }
+
+  
+  void clearMarkers()
+  {
+    for (mozilla::sig_safe_t i = 0; i < mMarkerPointer; i++) {
+      free(mMarkers[i]);
+    }
+    mMarkerPointer = 0;
+    mQueueClearMarker = false;
+  }
+
+  void push(const char *aName, uint32_t line)
+  {
+    push(aName, nullptr, false, line);
+  }
+
+  void push(const char *aName, void *aStackAddress, bool aCopy, uint32_t line)
+  {
+    if (size_t(mStackPointer) >= mozilla::ArrayLength(mStack)) {
+      mStackPointer++;
+      return;
+    }
+
+    
+    
+    mStack[mStackPointer].setLabel(aName);
+    mStack[mStackPointer].setStackAddressCopy(aStackAddress, aCopy);
+    mStack[mStackPointer].setLine(line);
+
+    
+    STORE_SEQUENCER();
+    mStackPointer++;
+  }
+  void pop()
+  {
+    mStackPointer--;
+  }
+  bool isEmpty()
+  {
+    return mStackPointer == 0;
+  }
+  uint32_t stackSize() const
+  {
+    return std::min<uint32_t>(mStackPointer, mozilla::ArrayLength(mStack));
+  }
+
+  void sampleRuntime(JSRuntime *runtime) {
+    mRuntime = runtime;
+    if (!runtime) {
+      
+      return;
+    }
+
+    JS_STATIC_ASSERT(sizeof(mStack[0]) == sizeof(js::ProfileEntry));
+    js::SetRuntimeProfilingStack(runtime,
+                                 (js::ProfileEntry*) mStack,
+                                 (uint32_t*) &mStackPointer,
+                                 mozilla::ArrayLength(mStack));
+    if (mStartJSSampling)
+      enableJSSampling();
+  }
+  void enableJSSampling() {
+    if (mRuntime) {
+      js::EnableRuntimeProfilingStack(mRuntime, true);
+      mStartJSSampling = false;
+    } else {
+      mStartJSSampling = true;
+    }
+  }
+  void disableJSSampling() {
+    mStartJSSampling = false;
+    if (mRuntime)
+      js::EnableRuntimeProfilingStack(mRuntime, false);
+  }
+
+  
+  StackEntry volatile mStack[1024];
+  
+  char* mMarkers[1024];
+ private:
+  
+  
+  mozilla::sig_safe_t mStackPointer;
+  
+  volatile bool mSignalLock;
+ public:
+  volatile mozilla::sig_safe_t mMarkerPointer;
+  
+  
+  volatile mozilla::sig_safe_t mQueueClearMarker;
+  
+  JSRuntime *mRuntime;
+  
+  bool mStartJSSampling;
+};
+
+#endif
+
