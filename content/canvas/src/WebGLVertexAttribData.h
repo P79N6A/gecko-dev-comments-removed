008a590c46f79ec9dd2b71c2644e2006299abf35
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:30:44 +0000

    Bug 815915 - Split out WebGLVertexAttribData into a separate header. r=bjacob
    UltraBlame original commit: 04c1cc1cb18ebb6f9a0729b13019ee84bdf445a5

---
+++ content/canvas/src/WebGLVertexAttribData.h
@@ -0,0 +1,77 @@
+
+
+
+
+
+#ifndef WEBGLVERTEXATTRIBDATA_H_
+#define WEBGLVERTEXATTRIBDATA_H_
+
+#include "WebGLObjectModel.h"
+
+namespace mozilla {
+
+class WebGLBuffer;
+
+struct WebGLVertexAttribData {
+    
+    WebGLVertexAttribData()
+        : buf(0), stride(0), size(4), byteOffset(0),
+          type(LOCAL_GL_FLOAT), enabled(false), normalized(false)
+    { }
+
+    WebGLRefPtr<WebGLBuffer> buf;
+    WebGLuint stride;
+    WebGLuint size;
+    GLuint byteOffset;
+    GLenum type;
+    bool enabled;
+    bool normalized;
+
+    GLuint componentSize() const {
+        switch(type) {
+            case LOCAL_GL_BYTE:
+                return sizeof(GLbyte);
+                break;
+            case LOCAL_GL_UNSIGNED_BYTE:
+                return sizeof(GLubyte);
+                break;
+            case LOCAL_GL_SHORT:
+                return sizeof(GLshort);
+                break;
+            case LOCAL_GL_UNSIGNED_SHORT:
+                return sizeof(GLushort);
+                break;
+            
+            case LOCAL_GL_FLOAT:
+                return sizeof(GLfloat);
+                break;
+            default:
+                NS_ERROR("Should never get here!");
+                return 0;
+        }
+    }
+
+    GLuint actualStride() const {
+        if (stride) return stride;
+        return size * componentSize();
+    }
+};
+
+} 
+
+inline void ImplCycleCollectionUnlink(mozilla::WebGLVertexAttribData& aField)
+{
+  aField.buf = nullptr;
+}
+
+inline void
+ImplCycleCollectionTraverse(nsCycleCollectionTraversalCallback& aCallback,
+                            mozilla::WebGLVertexAttribData& aField,
+                            const char* aName,
+                            uint32_t aFlags = 0)
+{
+  CycleCollectionNoteEdgeName(aCallback, aName, aFlags);
+  aCallback.NoteXPCOMChild(aField.buf);
+}
+
+#endif
