46432e2f6790f54794a40f70896d2a80ec8cf195
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:14:37 +0000

    Bug 895047 - Make char16_t available everywhere and use that to define PRUnichar and jschar; r=jcranmer,jorendorff,glandium
    UltraBlame original commit: ee100983f9210e21d5030bbf176a9fe075f3ac14

--- mfbt/Char16.h
+++ mfbt/Char16.h
@@ -16,9 +16,6 @@
 
 
 
-
-
-
 #ifdef _MSC_VER
    
 
@@ -39,9 +36,25 @@
 
 
 #  define MOZ_CHAR16_IS_NOT_WCHAR
+#elif !defined(__cplusplus)
+#  if defined(WIN32)
+#    include <yvals.h>
+     typedef wchar_t char16_t;
+#  else
+     
+
+
+
+
+     typedef unsigned short char16_t;
+#  endif
 #else
 #  error "Char16.h requires C++11 (or something like it) for UTF-16 support."
 #endif
+
+
+#define __PRUNICHAR__
+typedef char16_t PRUnichar;
 
 
 
@@ -53,9 +66,12 @@
 
 #define MOZ_UTF16(s) MOZ_UTF16_HELPER(s)
 
+#if defined(__cplusplus) && \
+    (__cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__))
 static_assert(sizeof(char16_t) == 2, "Is char16_t type 16 bits?");
 static_assert(char16_t(-1) > char16_t(0), "Is char16_t type unsigned?");
 static_assert(sizeof(MOZ_UTF16('A')) == 2, "Is char literal 16 bits?");
 static_assert(sizeof(MOZ_UTF16("")[0]) == 2, "Is string char 16 bits?");
+#endif
 
 #endif 
