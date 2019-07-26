93905339dc5b688f5ff375153b76641727c14718
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:14:09 +0000

    Backed out changeset f8e64e91e443 (bug 843668) for webgl test failures
    UltraBlame original commit: 3f2c42551ac62aabac71af2c8b62a924b5d79134

--- gfx/gl/GLContextFeatures.cpp
+++ gfx/gl/GLContextFeatures.cpp
@@ -250,15 +250,6 @@ static const FeatureInfo sFeatureInfoArr[] = {
         }
     },
     {
-        "sRGB",
-        300, 
-        300, 
-        {
-            GLContext::EXT_sRGB,
-            GLContext::Extensions_End
-        }
-    },
-    {
         "standard_derivatives",
         200, 
         300, 
@@ -395,14 +386,6 @@ GLContext::InitFeatures()
                 break;
             }
         }
-    }
-
-    
-    
-    
-    if (IsExtensionSupported(EXT_texture_sRGB) &&
-        (IsExtensionSupported(ARB_framebuffer_sRGB) || IsExtensionSupported(EXT_framebuffer_sRGB))) {
-        mAvailableFeatures[GLFeature::sRGB] = true;
     }
 }
 
