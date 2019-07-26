e82afabaf65b6678f90bf5c9df6d3c4c6a082988
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:14:22 +0000

    Bug 857427 - Try to load the page again if not yet loaded. r=ehsan
    UltraBlame original commit: 1a50af446d9c8cf12f19d13454308e62315b40a4

--- browser/base/content/test/general/browser_save_link-perwindowpb.js
+++ browser/base/content/test/general/browser_save_link-perwindowpb.js
@@ -16,10 +16,13 @@ function triggerSave(aWindow, aCallback) {
   var fileName;
   let testBrowser = aWindow.gBrowser.selectedBrowser;
   
-  testBrowser.loadURI("http://mochi.test:8888/browser/browser/base/content/test/general/bug792517-2.html");
+  let testURI = "http://mochi.test:8888/browser/browser/base/content/test/general/bug792517-2.html";
+  testBrowser.loadURI(testURI);
   testBrowser.addEventListener("pageshow", function pageShown(event) {
-    if (event.target.location == "about:blank")
+    if (event.target.location != testURI) {
+      testBrowser.loadURI(testURI);
       return;
+    }
     testBrowser.removeEventListener("pageshow", pageShown, false);
 
     executeSoon(function () {
