d29d1f2cb09d12325d43d1bd691c2c9b4eecc14f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:09:03 +0000

    Bug 979668 - [Bluetooth] Sender name is not shown in transfer requests, f=tzimmermann, r=kyle
    UltraBlame original commit: a149c02682ef20ebd6e64a9cb28846d76ff7eea2

--- ipc/unixfd/UnixSocketWatcher.h
+++ ipc/unixfd/UnixSocketWatcher.h
@@ -7,10 +7,33 @@
 #ifndef mozilla_ipc_UnixSocketWatcher_h
 #define mozilla_ipc_UnixSocketWatcher_h
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#ifdef MOZ_B2G_BT_BLUEZ
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/sco.h>
+#include <bluetooth/l2cap.h>
+#include <bluetooth/rfcomm.h>
+#endif
 #include "UnixFdWatcher.h"
 
 namespace mozilla {
 namespace ipc {
+
+union sockaddr_any {
+  sockaddr_storage storage; 
+  sockaddr_un un;
+  sockaddr_in in;
+  sockaddr_in6 in6;
+#ifdef MOZ_B2G_BT_BLUEZ
+  sockaddr_sco sco;
+  sockaddr_rc rc;
+  sockaddr_l2 l2;
+#endif
+  
+};
 
 class UnixSocketWatcher : public UnixFdWatcher
 {
@@ -38,7 +61,8 @@ public:
   nsresult Listen(const struct sockaddr* aAddr, socklen_t aAddrLen);
 
   
-  virtual void OnAccepted(int aFd) {};
+  virtual void OnAccepted(int aFd, const sockaddr_any* aAddr,
+                          socklen_t aAddrLen) {};
 
   
   virtual void OnConnected() {};
