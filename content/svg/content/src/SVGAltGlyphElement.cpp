418efb6f48f493ce66a3c99051711353ff82d73c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:41:25 +0000

    Bug 825732: Convert SVGAltGlyphElement to WebIDL r=bz
    UltraBlame original commit: 59ae61d1d05916c4e332867448c29df114cfee53

---
+++ content/svg/content/src/SVGAltGlyphElement.cpp
@@ -0,0 +1,140 @@
+
+
+
+
+
+#include "mozilla/dom/SVGAltGlyphElement.h"
+#include "mozilla/dom/SVGAltGlyphElementBinding.h"
+#include "nsContentUtils.h"
+
+DOMCI_NODE_DATA(SVGAltGlyphElement, mozilla::dom::SVGAltGlyphElement)
+
+NS_IMPL_NS_NEW_NAMESPACED_SVG_ELEMENT(AltGlyph)
+
+namespace mozilla {
+namespace dom {
+
+JSObject*
+SVGAltGlyphElement::WrapNode(JSContext *aCx, JSObject *aScope, bool *aTriedToWrap)
+{
+  return SVGAltGlyphElementBinding::Wrap(aCx, aScope, this, aTriedToWrap);
+}
+
+nsSVGElement::StringInfo SVGAltGlyphElement::sStringInfo[1] =
+{
+  { &nsGkAtoms::href, kNameSpaceID_XLink, false }
+};
+
+
+
+
+
+NS_IMPL_ADDREF_INHERITED(SVGAltGlyphElement,SVGAltGlyphElementBase)
+NS_IMPL_RELEASE_INHERITED(SVGAltGlyphElement,SVGAltGlyphElementBase)
+
+NS_INTERFACE_TABLE_HEAD(SVGAltGlyphElement)
+  NS_NODE_INTERFACE_TABLE7(SVGAltGlyphElement, nsIDOMNode, nsIDOMElement,
+                           nsIDOMSVGElement, nsIDOMSVGAltGlyphElement,
+                           nsIDOMSVGTextPositioningElement, nsIDOMSVGTextContentElement,
+                           nsIDOMSVGURIReference)
+  NS_DOM_INTERFACE_MAP_ENTRY_CLASSINFO(SVGAltGlyphElement)
+NS_INTERFACE_MAP_END_INHERITING(SVGAltGlyphElementBase)
+
+
+
+
+SVGAltGlyphElement::SVGAltGlyphElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+  : SVGAltGlyphElementBase(aNodeInfo)
+{
+  SetIsDOMBinding();
+}
+
+
+
+
+
+NS_IMPL_ELEMENT_CLONE_WITH_INIT(SVGAltGlyphElement)
+
+
+
+
+
+NS_IMETHODIMP SVGAltGlyphElement::GetHref(nsIDOMSVGAnimatedString * *aHref)
+{
+  *aHref = Href().get();
+  return NS_OK;
+}
+
+already_AddRefed<nsIDOMSVGAnimatedString>
+SVGAltGlyphElement::Href()
+{
+  nsCOMPtr<nsIDOMSVGAnimatedString> href;
+  mStringAttributes[HREF].ToDOMAnimatedString(getter_AddRefs(href), this);
+  return href.forget();
+}
+
+
+
+
+
+NS_IMETHODIMP SVGAltGlyphElement::GetGlyphRef(nsAString & aGlyphRef)
+{
+  GetAttr(kNameSpaceID_None, nsGkAtoms::glyphRef, aGlyphRef);
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP SVGAltGlyphElement::SetGlyphRef(const nsAString & aGlyphRef)
+{
+  return SetAttr(kNameSpaceID_None, nsGkAtoms::glyphRef, aGlyphRef, true);
+}
+
+
+NS_IMETHODIMP SVGAltGlyphElement::GetFormat(nsAString & aFormat)
+{
+  GetAttr(kNameSpaceID_None, nsGkAtoms::format, aFormat);
+
+  return NS_OK;
+}
+
+NS_IMETHODIMP SVGAltGlyphElement::SetFormat(const nsAString & aFormat)
+{
+  return SetAttr(kNameSpaceID_None, nsGkAtoms::format, aFormat, true);
+}
+
+
+
+
+NS_IMETHODIMP_(bool)
+SVGAltGlyphElement::IsAttributeMapped(const nsIAtom* name) const
+{
+  static const MappedAttributeEntry* const map[] = {
+    sColorMap,
+    sFillStrokeMap,
+    sFontSpecificationMap,
+    sGraphicsMap,
+    sTextContentElementsMap
+  };
+
+  return FindAttributeDependence(name, map) ||
+    SVGAltGlyphElementBase::IsAttributeMapped(name);
+}
+
+
+
+
+bool
+SVGAltGlyphElement::IsEventName(nsIAtom* aName)
+{
+  return nsContentUtils::IsEventAttributeName(aName, EventNameType_SVGGraphic);
+}
+
+nsSVGElement::StringAttributesInfo
+SVGAltGlyphElement::GetStringInfo()
+{
+  return StringAttributesInfo(mStringAttributes, sStringInfo,
+                              ArrayLength(sStringInfo));
+}
+
+} 
+} 
