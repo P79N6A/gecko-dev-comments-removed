84f8dd8c607f6c4712e1ac99293db52f8926f9d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:15:39 +0000

    Backout of changeset 6f1121e69ee9
    UltraBlame original commit: 22effc39c25074d1996f980d725f154dab1471df

--- netwerk/protocol/http/nsHttpChannelAuthProvider.cpp
+++ netwerk/protocol/http/nsHttpChannelAuthProvider.cpp
@@ -92,8 +92,19 @@ nsHttpChannelAuthProvider::ProcessAuthentication(uint32_t httpStatus,
         if (!mProxyInfo) return NS_ERROR_NO_INTERFACE;
     }
 
+    uint32_t loadFlags;
+    rv = mAuthChannel->GetLoadFlags(&loadFlags);
+    if (NS_FAILED(rv)) return rv;
+
     nsAutoCString challenges;
     mProxyAuth = (httpStatus == 407);
+
+    
+    if ((loadFlags & nsIRequest::LOAD_ANONYMOUS) &&
+        (!mProxyAuth || !UsingHttpProxy())) {
+        LOG(("Skipping authentication for anonymous non-proxy request\n"));
+        return NS_ERROR_NOT_AVAILABLE;
+    }
 
     rv = PrepareForAuthentication(mProxyAuth);
     if (NS_FAILED(rv))
@@ -665,10 +676,6 @@ nsHttpChannelAuthProvider::GetCredentialsForChallenge(const char *challenge,
                                  path, ident, continuationState);
     if (NS_FAILED(rv)) return rv;
 
-    uint32_t loadFlags;
-    rv = mAuthChannel->GetLoadFlags(&loadFlags);
-    if (NS_FAILED(rv)) return rv;
-
     if (!proxyAuth) {
         
         
@@ -676,18 +683,6 @@ nsHttpChannelAuthProvider::GetCredentialsForChallenge(const char *challenge,
             GetIdentityFromURI(authFlags, mIdent);
             identFromURI = !mIdent.IsEmpty();
         }
-
-        if ((loadFlags & nsIRequest::LOAD_ANONYMOUS) && !identFromURI) {
-            LOG(("Skipping authentication for anonymous non-proxy request\n"));
-            return NS_ERROR_NOT_AVAILABLE;
-        }
-
-        
-        
-    }
-    else if ((loadFlags & nsIRequest::LOAD_ANONYMOUS) && !UsingHttpProxy()) {
-        LOG(("Skipping authentication for anonymous non-proxy request\n"));
-        return NS_ERROR_NOT_AVAILABLE;
     }
 
     
@@ -734,9 +729,8 @@ nsHttpChannelAuthProvider::GetCredentialsForChallenge(const char *challenge,
                 }
             }
             else if (!identFromURI ||
-                     (nsCRT::strcmp(ident->User(),
-                                    entry->Identity().User()) == 0 &&
-                     !(loadFlags && nsIChannel::LOAD_ANONYMOUS))) {
+                     nsCRT::strcmp(ident->User(),
+                                   entry->Identity().User()) == 0) {
                 LOG(("  taking identity from auth cache\n"));
                 
                 
