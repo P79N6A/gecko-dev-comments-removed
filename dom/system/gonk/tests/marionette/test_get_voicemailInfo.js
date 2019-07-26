14ff946464e07a86c54c616bc8adc11c31ec1042
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:57:20 +0000

    Bug 814581 - Part 7/8: fix test cases. r=yoshi
    UltraBlame original commit: 697be373be37deba4b0f3d0cf6c1d5b94a62bdaf

--- dom/system/gonk/tests/marionette/test_get_voicemailInfo.js
+++ dom/system/gonk/tests/marionette/test_get_voicemailInfo.js
@@ -7,15 +7,20 @@ let Cc = SpecialPowers.Cc;
 let Ci = SpecialPowers.Ci;
 
 
-let RIL = Cc["@mozilla.org/ril;1"].getService(Ci.nsIRadioInterfaceLayer);
-ok(RIL);
+let radioInterfaceLayer =
+  Cc["@mozilla.org/ril;1"].getService(Ci.nsIRadioInterfaceLayer);
+ok(radioInterfaceLayer);
 
 
-ok(RIL.voicemailInfo);
-ok(RIL.voicemailInfo.number);
-ok(RIL.voicemailInfo.displayName);
+let radioInterface = radioInterfaceLayer.getRadioInterface(0);
+ok(radioInterface);
 
-is(RIL.voicemailInfo.number, "+15552175049");
-is(RIL.voicemailInfo.displayName, "Voicemail");
+
+ok(radioInterface.voicemailInfo);
+ok(radioInterface.voicemailInfo.number);
+ok(radioInterface.voicemailInfo.displayName);
+
+is(radioInterface.voicemailInfo.number, "+15552175049");
+is(radioInterface.voicemailInfo.displayName, "Voicemail");
 
 finish();
