adbfbf2975427159565ef9e3d0e992d5e8409695
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:02:20 +0000

    Bug 846621 - "Open link in new tab" in Metro should open a background tab [r=jimm]
    UltraBlame original commit: 8ebbd7f40f5f569fcf861816481f200fa024fe3f

--- browser/metro/base/content/ContextCommands.js
+++ browser/metro/base/content/ContextCommands.js
@@ -128,7 +128,8 @@ var ContextCommands = {
   
 
   openLinkInNewTab: function cc_openLinkInNewTab() {
-    BrowserUI.newTab(ContextMenuUI.popupState.linkURL, Browser.selectedTab);
+    Browser.addTab(ContextMenuUI.popupState.linkURL, false, Browser.selectedTab);
+    ContextUI.peekTabs();
   },
 
   copyLink: function cc_copyLink() {
