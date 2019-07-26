3fb15660bb683f8572db946c2d59481f281704fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:06:11 +0000

    Bug 849114: Add parser support for spatial media fragments. r=cpearce
    UltraBlame original commit: 14f662af1c91d0673e8b5570e978b9ae9621962d

---
+++ netwerk/base/src/nsMediaFragmentURIParser.h
@@ -0,0 +1,93 @@
+
+
+
+
+
+#if !defined(nsMediaFragmentURIParser_h__)
+#define nsMediaFragmentURIParser_h__
+
+#include "nsIURI.h"
+#include "nsString.h"
+#include "nsRect.h"
+
+
+
+
+
+
+
+
+
+
+namespace mozilla { namespace net {
+
+enum ClipUnit
+{
+  eClipUnit_Pixel,
+  eClipUnit_Percent,
+};
+
+class nsMediaFragmentURIParser
+{
+public:
+  
+  nsMediaFragmentURIParser(nsIURI* aURI);
+
+  
+  bool HasStartTime() const { return !mStart.empty(); }
+
+  
+  
+  double GetStartTime() const { return mStart.ref(); }
+
+  
+  bool HasEndTime() const { return !mEnd.empty(); }
+
+  
+  
+  double GetEndTime() const { return mEnd.ref(); }
+
+  
+  bool HasClip() const { return !mClip.empty(); }
+
+  
+  
+  
+  nsIntRect GetClip() const { return mClip.ref(); }
+
+  
+  
+  ClipUnit GetClipUnit() const { return mClipUnit; }
+
+private:
+  
+  
+  void Parse(nsACString& aRef);
+
+  
+  
+  
+  
+  
+  
+  bool ParseNPT(nsDependentSubstring aString);
+  bool ParseNPTTime(nsDependentSubstring& aString, double& aTime);
+  bool ParseNPTSec(nsDependentSubstring& aString, double& aSec);
+  bool ParseNPTFraction(nsDependentSubstring& aString, double& aFraction);
+  bool ParseNPTMMSS(nsDependentSubstring& aString, double& aTime);
+  bool ParseNPTHHMMSS(nsDependentSubstring& aString, double& aTime);
+  bool ParseNPTHH(nsDependentSubstring& aString, uint32_t& aHour);
+  bool ParseNPTMM(nsDependentSubstring& aString, uint32_t& aMinute);
+  bool ParseNPTSS(nsDependentSubstring& aString, uint32_t& aSecond);
+  bool ParseXYWH(nsDependentSubstring aString);
+
+  
+  Maybe<double>    mStart;
+  Maybe<double>    mEnd;
+  Maybe<nsIntRect> mClip;
+  ClipUnit         mClipUnit;
+};
+
+}} 
+
+#endif
