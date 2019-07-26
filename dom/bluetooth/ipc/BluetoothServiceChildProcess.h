59d638a351e53afdd01f4a69a8ac4dc9537eaaf1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:19:25 +0000

    Bug 860075 - Avoid duplicate BT signal observer registration from child process. r=echou, r=gyeh
    UltraBlame original commit: 022a5ed6195b457ee8caef8a5b3972ede82fc9f9

--- dom/bluetooth/ipc/BluetoothServiceChildProcess.h
+++ dom/bluetooth/ipc/BluetoothServiceChildProcess.h
@@ -174,6 +174,11 @@ private:
   
   virtual nsresult
   PrepareAdapterInternal() MOZ_OVERRIDE;
+
+  bool
+  IsSignalRegistered(const nsAString& aNodeName) {
+    return !!mBluetoothSignalObserverTable.Get(aNodeName);
+  }
 };
 
 END_BLUETOOTH_NAMESPACE
