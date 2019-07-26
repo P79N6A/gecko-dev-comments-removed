56fbccb7a66006d0db29a54860694d1bdc7d35b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:27:41 +0000

    Bug 862179 - Implement asynchronous loading of search engines. r=gavin
    UltraBlame original commit: 4c0f044453a32f72e68f2e1342849ab29f94a9fe

---
+++ toolkit/components/search/tests/xpcshell/test_sync_delay_fallback.js
@@ -0,0 +1,57 @@
+
+
+
+function run_test() {
+  do_test_pending();
+
+  removeMetadata();
+  removeCacheFile();
+
+  do_load_manifest("data/chrome.manifest");
+
+  let url  = "chrome://testsearchplugin/locale/searchplugins/";
+  Services.prefs.setCharPref("browser.search.jarURIs", url);
+  Services.prefs.setBoolPref("browser.search.loadFromJars", true);
+
+  do_check_false(Services.search.isInitialized);
+  let fallback = false;
+
+  Services.search.init(function search_initialized(aStatus) {
+    do_check_true(fallback);
+    do_check_true(Components.isSuccessCode(aStatus));
+    do_check_true(Services.search.isInitialized);
+
+    
+    let engines = Services.search.getEngines();
+    do_check_true(engines.length > 1);
+
+    
+    let engine = Services.search.getEngineByName("bug645970");
+    do_check_neq(engine, null);
+
+    Services.prefs.clearUserPref("browser.search.jarURIs");
+    Services.prefs.clearUserPref("browser.search.loadFromJars");
+
+    do_test_finished();
+  });
+
+  
+  Services.obs.addObserver(function searchServiceObserver(aResult, aTopic, aVerb) {
+    if (aVerb == "find-jar-engines") {
+      Services.obs.removeObserver(searchServiceObserver, aTopic);
+      fallback = true;
+
+      do_check_false(Services.search.isInitialized);
+
+      
+      let engines = Services.search.getEngines();
+      do_check_true(engines.length > 1);
+
+      
+      let engine = Services.search.getEngineByName("bug645970");
+      do_check_neq(engine, null);
+
+      do_check_true(Services.search.isInitialized);
+    }
+  }, "browser-search-service", false);
+}
