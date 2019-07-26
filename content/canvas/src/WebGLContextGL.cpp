83b4b7fa80bec84b6ed72b07f2e783f0475aacd5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:05:00 +0000

    Bug 892169 - [WebGL 2.0] Bypass ANGLE shader compilation (GL_EXT_gpu_shader4). r=jgilbert
    UltraBlame original commit: f57adec8f404fed0db383f9dff485d46535ff6d8

--- content/canvas/src/WebGLContextGL.cpp
+++ content/canvas/src/WebGLContextGL.cpp
@@ -4265,6 +4265,57 @@ WebGLContext::CompileShader(WebGLShader *shader)
 
         const char *s = sourceCString.get();
 
+#define WEBGL2_BYPASS_ANGLE
+#ifdef WEBGL2_BYPASS_ANGLE
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        static const char *bypassPrefixSearch = "#version proto-200";
+        static const char *bypassANGLEPrefix[2] = {"precision mediump float;\n"
+                                                   "#define gl_VertexID 0\n"
+                                                   "#define gl_InstanceID 0\n",
+
+                                                   "precision mediump float;\n"
+                                                   "#extension GL_EXT_draw_buffers : enable\n"
+                                                   "#define gl_PrimitiveID 0\n"};
+
+        const bool bypassANGLE = IsWebGL2() && (strstr(s, bypassPrefixSearch) != 0);
+
+        const char *angleShaderCode = s;
+        nsTArray<char> bypassANGLEShaderCode;
+        nsTArray<char> bypassDriverShaderCode;
+
+        if (bypassANGLE) {
+            const int bypassStage = (shader->ShaderType() == LOCAL_GL_FRAGMENT_SHADER) ? 1 : 0;
+            const char *originalShader = strstr(s, bypassPrefixSearch) + strlen(bypassPrefixSearch);
+            int originalShaderSize = strlen(s) - (originalShader - s);
+            int bypassShaderCodeSize = originalShaderSize + 4096 + 1;
+
+            bypassANGLEShaderCode.SetLength(bypassShaderCodeSize);
+            strcpy(bypassANGLEShaderCode.Elements(), bypassANGLEPrefix[bypassStage]);
+            strcat(bypassANGLEShaderCode.Elements(), originalShader);
+
+            bypassDriverShaderCode.SetLength(bypassShaderCodeSize);
+            strcpy(bypassDriverShaderCode.Elements(), "#extension GL_EXT_gpu_shader4 : enable\n");
+            strcat(bypassDriverShaderCode.Elements(), originalShader);
+
+            angleShaderCode = bypassANGLEShaderCode.Elements();
+        }
+#endif
+
         compiler = ShConstructCompiler((ShShaderType) shader->ShaderType(),
                                        SH_WEBGL_SPEC,
                                        targetShaderSourceLanguage,
@@ -4297,7 +4348,11 @@ WebGLContext::CompileShader(WebGLShader *shader)
 #endif
         }
 
+#ifdef WEBGL2_BYPASS_ANGLE
+        if (!ShCompile(compiler, &angleShaderCode, 1, compileOptions)) {
+#else
         if (!ShCompile(compiler, &s, 1, compileOptions)) {
+#endif
             size_t len = 0;
             ShGetInfo(compiler, SH_INFO_LOG_LENGTH, &len);
 
@@ -4386,7 +4441,17 @@ WebGLContext::CompileShader(WebGLShader *shader)
 
             const char *ts = translatedSrc.get();
 
+#ifdef WEBGL2_BYPASS_ANGLE
+            if (bypassANGLE) {
+                const char* driverShaderCode = bypassDriverShaderCode.Elements();
+                gl->fShaderSource(shadername, 1, (const GLchar**) &driverShaderCode, nullptr);
+            }
+            else {
+                gl->fShaderSource(shadername, 1, &ts, nullptr);
+            }
+#else
             gl->fShaderSource(shadername, 1, &ts, nullptr);
+#endif
         } else { 
             
             
