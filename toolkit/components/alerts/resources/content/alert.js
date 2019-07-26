5371e8ab33525f503891bdc9c9c7c4495f73e8a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:09:04 +0000

    Bug 786125 - followup, store the magic number in a const. r=Unfocused
    UltraBlame original commit: 984026798238b2a6de2452982d8f30a02e04c951

--- toolkit/components/alerts/resources/content/alert.js
+++ toolkit/components/alerts/resources/content/alert.js
@@ -53,6 +53,7 @@ function prefillAlertInfo() {
 }
 
 function onAlertLoad() {
+  const ALERT_DURATION_IMMEDIATE = 4000;
   
   
   
@@ -86,7 +87,7 @@ function onAlertLoad() {
   window.moveTo(x, y);
 
   if (Services.prefs.getBoolPref("alerts.disableSlidingEffect")) {
-    setTimeout(closeAlert, 4000);
+    setTimeout(closeAlert, ALERT_DURATION_IMMEDIATE);
     return;
   }
 
