df9a5922c3577612062ebc6213fce486b5db6cb2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:09:57 +0000

    Bug 846918: Adds internal httpchannel idl for communication between documents and channels. r=jlebar
    UltraBlame original commit: 4e11fe1c46cff265bceab2a5af16f7a8e4e20f70

---
+++ xpcom/base/nsSecurityConsoleMessage.h
@@ -0,0 +1,28 @@
+
+
+
+
+#ifndef nsSecurityConsoleMessage_h__
+#define nsSecurityConsoleMessage_h__
+#include "nsISecurityConsoleMessage.h"
+#include "nsString.h"
+
+class nsSecurityConsoleMessage : public nsISecurityConsoleMessage
+{
+  public:
+    NS_DECL_ISUPPORTS
+    NS_DECL_NSISECURITYCONSOLEMESSAGE
+
+    nsSecurityConsoleMessage();
+
+  private:
+    ~nsSecurityConsoleMessage();
+
+  protected:
+    nsString mTag;
+    nsString mCategory;
+};
+
+#define NS_SECURITY_CONSOLE_MESSAGE_CID \
+  {0x43ebf210, 0x8a7b, 0x4ddb, {0xa8, 0x3d, 0xb8, 0x7c, 0x51, 0xa0, 0x58, 0xdb}}
+#endif 
