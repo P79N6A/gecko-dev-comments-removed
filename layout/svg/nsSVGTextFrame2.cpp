5106d5bdd5e2d24e2d356cfc0ac9e0c97080e8ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:16:10 +0000

    Bug 860653 - Make paint-order work on SVG text with layout.svg.css-frames.enabled. r=longsonr
    UltraBlame original commit: 46c36b3006172b13250882dd255246d5db1cc2b2

--- layout/svg/nsSVGTextFrame2.cpp
+++ layout/svg/nsSVGTextFrame2.cpp
@@ -2620,7 +2620,18 @@ private:
   
 
 
-  void FillAndStroke();
+
+  void FillAndStrokeGeometry();
+
+  
+
+
+  void FillGeometry();
+
+  
+
+
+  void StrokeGeometry();
 
   gfxContext* gfx;
   uint16_t mRenderMode;
@@ -2731,7 +2742,7 @@ SVGTextDrawPathCallbacks::NotifySelectionDecorationLinePathEmitted()
     return;
   }
 
-  FillAndStroke();
+  FillAndStrokeGeometry();
   gfx->Restore();
 }
 
@@ -2778,7 +2789,7 @@ SVGTextDrawPathCallbacks::HandleTextGeometry()
     gfxContextMatrixAutoSaveRestore saveMatrix(gfx);
     gfx->SetMatrix(mCanvasTM);
 
-    FillAndStroke();
+    FillAndStrokeGeometry();
   }
 }
 
@@ -2799,7 +2810,7 @@ SVGTextDrawPathCallbacks::SetFillColor()
 }
 
 void
-SVGTextDrawPathCallbacks::FillAndStroke()
+SVGTextDrawPathCallbacks::FillAndStrokeGeometry()
 {
   bool pushedGroup = false;
   if (mColor == NS_40PERCENT_FOREGROUND_COLOR) {
@@ -2807,21 +2818,49 @@ SVGTextDrawPathCallbacks::FillAndStroke()
     gfx->PushGroup(gfxASurface::CONTENT_COLOR_ALPHA);
   }
 
-  if (SetFillColor()) {
-    gfx->Fill();
-  }
-
-  if (mColor == NS_SAME_AS_FOREGROUND_COLOR ||
-      mColor == NS_40PERCENT_FOREGROUND_COLOR) {
-    
-    if (nsSVGUtils::SetupCairoStroke(mFrame, gfx)) {
-      gfx->Stroke();
+  uint32_t paintOrder = mFrame->StyleSVG()->mPaintOrder;
+  if (paintOrder == NS_STYLE_PAINT_ORDER_NORMAL) {
+    FillGeometry();
+    StrokeGeometry();
+  } else {
+    while (paintOrder) {
+      uint32_t component =
+        paintOrder & ((1 << NS_STYLE_PAINT_ORDER_BITWIDTH) - 1);
+      switch (component) {
+        case NS_STYLE_PAINT_ORDER_FILL:
+          FillGeometry();
+          break;
+        case NS_STYLE_PAINT_ORDER_STROKE:
+          StrokeGeometry();
+          break;
+      }
+      paintOrder >>= NS_STYLE_PAINT_ORDER_BITWIDTH;
     }
   }
 
   if (pushedGroup) {
     gfx->PopGroupToSource();
     gfx->Paint(0.4);
+  }
+}
+
+void
+SVGTextDrawPathCallbacks::FillGeometry()
+{
+  if (SetFillColor()) {
+    gfx->Fill();
+  }
+}
+
+void
+SVGTextDrawPathCallbacks::StrokeGeometry()
+{
+  if (mColor == NS_SAME_AS_FOREGROUND_COLOR ||
+      mColor == NS_40PERCENT_FOREGROUND_COLOR) {
+    
+    if (nsSVGUtils::SetupCairoStroke(mFrame, gfx)) {
+      gfx->Stroke();
+    }
   }
 }
 
