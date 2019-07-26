5f418e8d870ce366579ef21026a18eb5118e2140
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:11:48 +0000

    Bug 898209 patch 11:  Create and use member variables for accessibility notifications.  r=heycam
    UltraBlame original commit: 89f45106a984c22f931a06ebbb53ed0957236b53

--- layout/base/RestyleManager.h
+++ layout/base/RestyleManager.h
@@ -292,20 +292,6 @@ public:
                   const ElementRestyler& aParentFrameRestyler,
                   nsIFrame* aFrame);
 
-public: 
-  enum DesiredA11yNotifications {
-    eSkipNotifications,
-    eSendAllNotifications,
-    eNotifyIfShown
-  };
-
-  enum A11yNotificationType {
-    eDontNotify,
-    eNotifyShown,
-    eNotifyHidden
-  };
-
-public:
   
 
 
@@ -316,7 +302,6 @@ public:
 
 
   void Restyle(nsRestyleHint aRestyleHint,
-               DesiredA11yNotifications aDesiredA11yNotifications,
                nsTArray<nsIContent*>& aVisibleKidsOfHiddenElement,
                TreeMatchContext &aTreeMatchContext);
 
@@ -333,6 +318,18 @@ private:
   void CaptureChange(nsStyleContext* aOldContext,
                      nsStyleContext* aNewContext,
                      nsChangeHint aChangeToAssume);
+
+  enum DesiredA11yNotifications {
+    eSkipNotifications,
+    eSendAllNotifications,
+    eNotifyIfShown
+  };
+
+  enum A11yNotificationType {
+    eDontNotify,
+    eNotifyShown,
+    eNotifyHidden
+  };
 
 private:
   nsPresContext* const mPresContext;
@@ -353,8 +350,12 @@ private:
   nsChangeHint mParentFrameHintsNotHandledForDescendants;
   nsChangeHint mHintsNotHandledForDescendants;
   RestyleTracker& mRestyleTracker;
+
+  const DesiredA11yNotifications mDesiredA11yNotifications;
+  DesiredA11yNotifications mKidsDesiredA11yNotifications;
+  A11yNotificationType mOurA11yNotification;
 };
 
 } 
 
-#endif
+#endif 
