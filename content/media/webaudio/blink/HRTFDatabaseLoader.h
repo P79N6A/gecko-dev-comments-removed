eeb7171c5d26eebea426e3a1def485ac916bf948
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:19:50 +0000

    b=865241 make HRTFDatabaseLoader ref-counting thread-safe r=ehsan
    UltraBlame original commit: c19e2f326d156dfa40341a34c3454004bff0d006

--- content/media/webaudio/blink/HRTFDatabaseLoader.h
+++ content/media/webaudio/blink/HRTFDatabaseLoader.h
@@ -39,7 +39,7 @@ namespace WebCore {
 
 
 
-class HRTFDatabaseLoader : public mozilla::RefCounted<HRTFDatabaseLoader> {
+class HRTFDatabaseLoader {
 public:
     
     
@@ -48,8 +48,33 @@ public:
     static mozilla::TemporaryRef<HRTFDatabaseLoader> createAndLoadAsynchronouslyIfNecessary(float sampleRate);
 
     
-    ~HRTFDatabaseLoader();
-    
+    void AddRef()
+    {
+#if defined(DEBUG) || defined(NS_BUILD_REFCNT_LOGGING)
+        int count =
+#endif
+          ++m_refCnt;
+        MOZ_ASSERT(count > 0, "invalid ref count");
+        NS_LOG_ADDREF(this, count, "HRTFDatabaseLoader", sizeof(*this));
+    }
+
+    void Release()
+    {
+        
+        
+        
+        int count = m_refCnt;
+        MOZ_ASSERT(count > 0, "extra release");
+        
+        
+        if (count != 1 && m_refCnt.compareExchange(count, count - 1)) {
+            NS_LOG_RELEASE(this, count - 1, "HRTFDatabaseLoader");
+            return;
+        }
+
+        ProxyRelease();
+    }
+
     
     bool isLoaded() const;
 
@@ -67,7 +92,12 @@ public:
 private:
     
     explicit HRTFDatabaseLoader(float sampleRate);
+    ~HRTFDatabaseLoader();
     
+    void ProxyRelease(); 
+    void MainThreadRelease(); 
+    class ProxyReleaseEvent;
+
     
     
     void loadAsynchronously();
@@ -85,6 +115,8 @@ private:
     
     static nsTHashtable<LoaderByRateEntry> *s_loaderMap; 
 
+    mozilla::Atomic<int> m_refCnt;
+
     nsAutoRef<HRTFDatabase> m_hrtfDatabase;
 
     
@@ -96,4 +128,4 @@ private:
 
 } 
 
-#endif 
+#endif
