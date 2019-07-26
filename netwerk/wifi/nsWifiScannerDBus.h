0bfe5f008e20ddf1c22d93b4a863e8af686524ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:01:30 +0000

    Bug 668194 - iwlib only returns one ac resulting in an awful accuracy. Implement DBUS Geolocation provider r=dougt
    UltraBlame original commit: 7612ff8a7fced7da4c4e93e538093a996179e69c

---
+++ netwerk/wifi/nsWifiScannerDBus.h
@@ -0,0 +1,43 @@
+
+
+
+
+#ifndef NSWIFIAPSCANNERDBUS_H_
+#define NSWIFIAPSCANNERDBUS_H_
+
+#include "nsCOMArray.h"
+
+#define DBUS_API_SUBJECT_TO_CHANGE
+#include <dbus/dbus.h>
+
+class nsWifiAccessPoint;
+
+namespace mozilla {
+
+class nsWifiScannerDBus MOZ_FINAL
+{
+public:
+  nsWifiScannerDBus(nsCOMArray<nsWifiAccessPoint>* aAccessPoints);
+  ~nsWifiScannerDBus();
+
+  nsresult Scan();
+
+private:
+  nsresult SendMessage(const char* aInterface,
+                       const char* aPath,
+                       const char* aFuncCall);
+  nsresult IdentifyDevices(DBusMessage* aMsg);
+  nsresult IdentifyDeviceType(DBusMessage* aMsg, const char* aDevicePath);
+  nsresult IdentifyAccessPoints(DBusMessage* aMsg);
+  nsresult IdentifyAPProperties(DBusMessage* aMsg);
+  nsresult StoreSsid(DBusMessageIter* aVariant, nsWifiAccessPoint* aAp);
+  nsresult SetMac(DBusMessageIter* aVariant, nsWifiAccessPoint* aAp);
+  nsresult GetDBusIterator(DBusMessage* aMsg, DBusMessageIter* aIterArray);
+
+  DBusConnection* mConnection;
+  nsCOMArray<nsWifiAccessPoint>* mAccessPoints;
+};
+
+} 
+
+#endif 
