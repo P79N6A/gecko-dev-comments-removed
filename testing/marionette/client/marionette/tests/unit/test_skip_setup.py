71cda22529b104b651187f812591a47980718d91
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:57:21 +0000

    Bug 888506 - Check that the start_time attribute exists before trying to use it. r=jgriffin
    UltraBlame original commit: df89dc4a968db9f4c86372a2321c61f9a4e6832e

---
+++ testing/marionette/client/marionette/tests/unit/test_skip_setup.py
@@ -0,0 +1,34 @@
+
+
+
+
+from marionette_test import MarionetteTestCase, SkipTest
+
+class TestSetUpSkipped(MarionetteTestCase):
+
+    testVar = {'test':'SkipTest'}
+
+    def setUp(self):
+        try:
+            self.testVar['email']
+        except KeyError:
+            raise SkipTest('email key not present in dict, skip ...')
+        MarionetteTestCase.setUp(self)
+
+    def test_assert(self):
+        assert True
+
+class TestSetUpNotSkipped(MarionetteTestCase):
+
+    testVar = {'test':'SkipTest'}
+
+    def setUp(self):
+        try:
+            self.testVar['test']
+        except KeyError:
+            raise SkipTest('email key not present in dict, skip ...')
+        MarionetteTestCase.setUp(self)
+
+    def test_assert(self):
+        assert True
+
