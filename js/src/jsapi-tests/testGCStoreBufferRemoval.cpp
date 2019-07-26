b36e5671c055a283af7cd67b06ec255e64e4adee
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:56:41 +0000

    Bug 968616 - "Implement" AutoIgnoreRootingHazards, r=terrence
    UltraBlame original commit: 7f6f5e970345d4ec0bfd45d5f9145bceee007ee6

--- js/src/jsapi-tests/testGCStoreBufferRemoval.cpp
+++ js/src/jsapi-tests/testGCStoreBufferRemoval.cpp
@@ -13,6 +13,14 @@
 using namespace JS;
 using namespace js;
 
+struct AutoIgnoreRootingHazards {
+    
+    static volatile int depth;
+    AutoIgnoreRootingHazards() { depth++; }
+    ~AutoIgnoreRootingHazards() { depth--; }
+};
+volatile int AutoIgnoreRootingHazards::depth = 0;
+
 BEGIN_TEST(testGCStoreBufferRemoval)
 {
     
@@ -24,12 +32,12 @@ BEGIN_TEST(testGCStoreBufferRemoval)
     JS::RootedObject tenuredObject(cx, obj);
 
     
-    typedef JSObject *ObjectPtr;
+    AutoIgnoreRootingHazards ignore;
 
     
     {
-        ObjectPtr badObject = reinterpret_cast<JSObject*>(1);
-        ObjectPtr punnedPtr = nullptr;
+        JSObject *badObject = reinterpret_cast<JSObject*>(1);
+        JSObject *punnedPtr = nullptr;
         RelocatablePtrObject* relocPtr =
             reinterpret_cast<RelocatablePtrObject*>(&punnedPtr);
         new (relocPtr) RelocatablePtrObject;
@@ -80,8 +88,8 @@ BEGIN_TEST(testGCStoreBufferRemoval)
 
     
     {
-        ObjectPtr badObject = reinterpret_cast<JSObject*>(1);
-        ObjectPtr punnedPtr = nullptr;
+        JSObject *badObject = reinterpret_cast<JSObject*>(1);
+        JSObject *punnedPtr = nullptr;
         Heap<JSObject*>* heapPtr =
             reinterpret_cast<Heap<JSObject*>*>(&punnedPtr);
         new (heapPtr) Heap<JSObject*>;
