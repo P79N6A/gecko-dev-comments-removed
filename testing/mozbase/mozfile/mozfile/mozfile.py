0a10e38073f1a0bc38524327a8df21e55994528d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:41:41 +0000

    Revert accidentally commited files in 492fdd7c06fd. r=backout
    UltraBlame original commit: 4b84bbad669dde2f0ef059f2e3bc641804e54254

--- testing/mozbase/mozfile/mozfile/mozfile.py
+++ testing/mozbase/mozfile/mozfile/mozfile.py
@@ -112,23 +112,15 @@ def rmtree(dir):
 
     This is a replacement for shutil.rmtree that works better under
     windows."""
-    try:
-        
-        if not os.path.exists(dir):
-            return
-        if os.path.islink(dir):
-            try:
-                os.remove(dir)
-            except WindowsError, e:
-                print "exception occured when removing %s" % dir
-                raise e
-            return
+    
+    if not os.path.exists(dir):
+        return
+    if os.path.islink(dir):
+        os.remove(dir)
+        return
 
-        
-        os.chmod(dir, 0700)
-    except WindowsError, e:
-        print "exception occured when removing dir %s" % dir
-        raise e
+    
+    os.chmod(dir, 0700)
 
     
     
@@ -147,37 +139,21 @@ def rmtree(dir):
         
         
         if os.name == 'nt':
-            try:
-                if not os.access(full_name, os.W_OK):
-                    
-                    
-                    
-                    try:
-                        os.chmod(full_name, 0600)
-                    except WindowsError, e:
-                        print "exception occured when chmod file %s" % full_name
-                        raise e
-            except WindowsError, e:
-                print "exception occured when access file %s" % full_name
-                raise e
+            if not os.access(full_name, os.W_OK):
+                
+                
+                
+                os.chmod(full_name, 0600)
 
-        try:
-            if os.path.islink(full_name):
-                os.remove(full_name)
-            elif os.path.isdir(full_name):
-                rmtree(full_name)
-            else:
-                if os.path.isfile(full_name):
-                    os.chmod(full_name, 0700)
-                os.remove(full_name)
-        except WindowsError, e:
-            print "exception occured when removing file %s" % full_name
-            raise e
-    try:
-        os.rmdir(dir)
-    except WindowsError, e:
-        print "exception occured when removing dir %s" % dir
-        raise e
+        if os.path.islink(full_name):
+            os.remove(full_name)
+        elif os.path.isdir(full_name):
+            rmtree(full_name)
+        else:
+            if os.path.isfile(full_name):
+                os.chmod(full_name, 0700)
+            os.remove(full_name)
+    os.rmdir(dir)
 
 
 class NamedTemporaryFile(object):
