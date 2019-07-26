791222a12e85bbeb3858dae59ffe5bc0f4ff4b6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:17:18 +0000

    merge mozilla-inbound to mozilla-central
    UltraBlame original commit: 358d369c8388836ebe71f4a10b18b0253987306f

--- layout/generic/nsGfxScrollFrame.cpp
+++ layout/generic/nsGfxScrollFrame.cpp
@@ -1552,7 +1552,7 @@ ScrollFrameHelper::ScrollFrameHelper(nsContainerFrame* aOuter,
   , mOuter(aOuter)
   , mAsyncScroll(nullptr)
   , mOriginOfLastScroll(nsGkAtoms::other)
-  , mScrollGeneration(1) 
+  , mScrollGeneration(0)
   , mDestination(0, 0)
   , mScrollPosAtLastPaint(0, 0)
   , mRestorePos(-1, -1)
