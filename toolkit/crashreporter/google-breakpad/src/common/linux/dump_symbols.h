92343e01ad945279d761df9c17055c81444c1b16
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:17:18 +0000

    Bug 861141 - Connect Breakpad on Android to faulty.lib's mmap interface.  r=glandium,ted
    UltraBlame original commit: fa1a9fa71db4d0d33f77fde2a0a3f99b14919396

--- toolkit/crashreporter/google-breakpad/src/common/linux/dump_symbols.h
+++ toolkit/crashreporter/google-breakpad/src/common/linux/dump_symbols.h
@@ -41,6 +41,7 @@
 
 #include "common/symbol_data.h"
 #include "common/using_std_string.h"
+#include "google_breakpad/common/breakpad_types.h"
 
 namespace google_breakpad {
 
@@ -64,6 +65,14 @@ bool ReadSymbolData(const string& obj_file,
                     const std::vector<string>& debug_dirs,
                     SymbolData symbol_data,
                     Module** module);
+
+
+
+bool ReadSymbolDataInternal(const uint8_t* obj_file,
+                            const string& obj_filename,
+                            const std::vector<string>& debug_dirs,
+                            SymbolData symbol_data,
+                            Module** module);
 
 }  
 
--- tools/profiler/local_debug_info_symbolizer.cc
+++ tools/profiler/local_debug_info_symbolizer.cc
@@ -26,6 +26,16 @@
 # error "Unknown platform"
 #endif
 
+#if defined(SPS_OS_android) && !defined(MOZ_WIDGET_GONK)
+# include "mozilla/Types.h"
+# include "ElfLoader.h"
+# include <dlfcn.h>
+# include <sys/mman.h>
+# include "nsString.h"
+# include "nsDirectoryServiceUtils.h"
+# include "nsDirectoryServiceDefs.h"
+#endif
+
 #include "local_debug_info_symbolizer.h"
 
 namespace google_breakpad {
@@ -39,6 +49,98 @@ LocalDebugInfoSymbolizer::~LocalDebugInfoSymbolizer() {
   }
 # endif
 }
+
+#if defined(SPS_OS_android) && !defined(MOZ_WIDGET_GONK)
+
+
+
+
+static char* get_installation_lib_dir ( void )
+{
+  nsCOMPtr<nsIProperties>
+    directoryService(do_GetService(NS_DIRECTORY_SERVICE_CONTRACTID));
+  if (!directoryService) return NULL;
+  nsCOMPtr<nsIFile> greDir;
+  nsresult rv = directoryService->Get(NS_GRE_DIR, NS_GET_IID(nsIFile),
+                                      getter_AddRefs(greDir));
+  if (NS_FAILED(rv)) return NULL;
+  nsCString path;
+  rv = greDir->GetNativePath(path);
+  if (NS_FAILED(rv)) return NULL;
+  return strdup(path.get());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static bool ReadSymbolData_ANDROID(const string& obj_filename,
+                                   const std::vector<string>& debug_dirs,
+                                   SymbolData symbol_data,
+                                   Module** module)
+{
+  string obj_file_to_use = obj_filename;
+
+  
+  if (obj_file_to_use == "libmozglue.so") {
+    char* libdir = get_installation_lib_dir();
+    if (libdir) {
+      obj_file_to_use = string(libdir) + "/lib/" + obj_file_to_use;
+      free(libdir);
+    }
+  }
+
+  
+  
+  
+  void* hdl = dlopen(obj_file_to_use.c_str(), RTLD_GLOBAL | RTLD_LAZY);
+  if (!hdl) {
+    BPLOG(INFO) << "ReadSymbolData_APK: Failed to get handle for ELF file \'"
+                << obj_file_to_use << "\'";
+    return false;
+  }
+
+  size_t sz = __dl_get_mappable_length(hdl);
+  if (sz == 0) {
+    dlclose(hdl);
+    BPLOG(INFO) << "ReadSymbolData_APK: Unable to get size for ELF file \'"
+                << obj_file_to_use << "\'";
+    return false;
+  }
+
+  void* image = __dl_mmap(hdl, NULL, sz, 0);
+  if (image == MAP_FAILED) {
+    dlclose(hdl);
+    BPLOG(INFO) << "ReadSymbolData_APK: Failed to mmap ELF file \'"
+                << obj_file_to_use << "\'";
+    return false;
+  }
+
+  bool ok = ReadSymbolDataInternal((const uint8_t*)image,
+                                   obj_file_to_use, debug_dirs,
+                                   symbol_data, module);
+  __dl_munmap(hdl, image, sz);
+  dlclose(hdl);
+
+  return ok;
+}
+#endif 
+
 
 StackFrameSymbolizer::SymbolizerResult
 LocalDebugInfoSymbolizer::FillSourceLineInfo(const CodeModules* modules,
@@ -61,10 +163,17 @@ LocalDebugInfoSymbolizer::FillSourceLineInfo(const CodeModules* modules,
         no_symbol_modules_.end()) {
       return kNoError;
     }
-    if (!ReadSymbolData(module->code_file(),
-                        debug_dirs_,
-                        ONLY_CFI,
-                        &debug_info_module)) {
+
+    bool ok = false;
+#   if defined(SPS_OS_android) && !defined(MOZ_WIDGET_GONK)
+    ok = ReadSymbolData_ANDROID(module->code_file(), debug_dirs_,
+                                ONLY_CFI, &debug_info_module);
+#   else
+    ok = ReadSymbolData(module->code_file(), debug_dirs_,
+                        ONLY_CFI, &debug_info_module);
+#   endif
+
+    if (!ok) {
       if (debug_info_module)
         delete debug_info_module;
       no_symbol_modules_.insert(module->code_file());
