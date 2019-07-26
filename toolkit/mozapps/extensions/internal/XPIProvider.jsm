c597b4debfb8a21d568227f3709e3bea6693452f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:30:10 +0000

    Bug 957089 - Fix mapURIToAddonID for non-bootstrapped add-ons. r=bmcbride
    UltraBlame original commit: 8eef124486621bf0d089dddcc9c1882dcabf773d

--- toolkit/mozapps/extensions/internal/XPIProvider.jsm
+++ toolkit/mozapps/extensions/internal/XPIProvider.jsm
@@ -1664,7 +1664,8 @@ this.XPIProvider = {
 
     
     let enabled = Object.create(null);
-    for (let a of this.enabledAddons.split(",")) {
+    let enabledAddons = this.enabledAddons || "";
+    for (let a of enabledAddons.split(",")) {
       a = decodeURIComponent(a.split(":")[0]);
       enabled[a] = null;
     }
@@ -1900,6 +1901,11 @@ this.XPIProvider = {
       }
 
       this.enabledAddons = Prefs.getCharPref(PREF_EM_ENABLED_ADDONS, "");
+
+      
+      
+      delete this._uriMappings;
+
       if ("nsICrashReporter" in Ci &&
           Services.appinfo instanceof Ci.nsICrashReporter) {
         
--- toolkit/mozapps/extensions/test/xpcshell/test_mapURIToAddonID.js
+++ toolkit/mozapps/extensions/test/xpcshell/test_mapURIToAddonID.js
@@ -71,9 +71,43 @@ function run_test() {
 
   resetPrefs();
 
+  run_test_early();
+}
+
+function run_test_early() {
   startupManager();
 
-  run_test_nomapping();
+  installAllFiles([do_get_addon("test_chromemanifest_1")], function() {
+    restartManager();
+    AddonManager.getAddonByID("addon1@tests.mozilla.org", function(addon) {
+      let uri = addon.getResourceURI(".");
+      let id = addon.id;
+      check_mapping(uri, id);
+
+      shutdownManager();
+
+      
+      
+      
+
+      
+      let s = Components.utils.import(
+        "resource://gre/modules/addons/XPIProvider.jsm", {});
+
+      
+      do_check_null(s.XPIProvider.mapURIToAddonID(uri));
+      do_check_null(AddonManager.mapURIToAddonID(uri));
+
+      
+      startupManager(false);
+
+      
+      check_mapping(uri, id);
+      do_check_eq(s.XPIProvider.mapURIToAddonID(uri), id);
+
+      run_test_nomapping();
+    });
+  });
 }
 
 function run_test_nomapping() {
