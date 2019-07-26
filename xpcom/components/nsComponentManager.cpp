3244f24a5036e32974bd76fe6dd3884cc65b3674
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:42:05 +0000

    Bug 938437 - Replace nsStaticXULComponents.cpp with smart use of sections. r=bsmedberg,irc-r=decoder
    UltraBlame original commit: 002e260353670281764b1e6c1a60f4c33011a056

--- xpcom/components/nsComponentManager.cpp
+++ xpcom/components/nsComponentManager.cpp
@@ -79,10 +79,6 @@ using namespace mozilla;
 
 PRLogModuleInfo* nsComponentManagerLog = nullptr;
 
-
-
-extern mozilla::Module const *const *const kPStaticModules[];
-
 #if 0 || defined (DEBUG_timeless)
  #define SHOW_DENIED_ON_SHUTDOWN
  #define SHOW_CI_ON_EXISTING_SERVICE
@@ -322,6 +318,15 @@ nsComponentManagerImpl::nsComponentManagerImpl()
 
 nsTArray<const mozilla::Module*>* nsComponentManagerImpl::sStaticModules;
 
+NSMODULE_DEFN(start_kPStaticModules);
+NSMODULE_DEFN(end_kPStaticModules);
+
+
+
+
+
+
+MOZ_ASAN_BLACKLIST
  void
 nsComponentManagerImpl::InitializeStaticModules()
 {
@@ -329,9 +334,10 @@ nsComponentManagerImpl::InitializeStaticModules()
         return;
 
     sStaticModules = new nsTArray<const mozilla::Module*>;
-    for (const mozilla::Module *const *const *staticModules = kPStaticModules;
-         *staticModules; ++staticModules)
-        sStaticModules->AppendElement(**staticModules);
+    for (const mozilla::Module *const *staticModules = &NSMODULE_NAME(start_kPStaticModules) + 1;
+         staticModules < &NSMODULE_NAME(end_kPStaticModules); ++staticModules)
+        if (*staticModules) 
+            sStaticModules->AppendElement(*staticModules);
 }
 
 nsTArray<nsComponentManagerImpl::ComponentLocation>*
