239820355ccc3879c1f972fbde2b95c6c5fe0031
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:45:46 +0000

    Force composition on expose events (bug 915809, r=mattwoodrow).
    UltraBlame original commit: 4ab3f6f9c004ba57cfc5bd7e6b5e19fcfeaac815

--- widget/gtk2/nsWindow.cpp
+++ widget/gtk2/nsWindow.cpp
@@ -105,6 +105,7 @@ extern "C" {
 #include "LayerManagerOGL.h"
 #include "GLContextProvider.h"
 #include "mozilla/gfx/2D.h"
+#include "mozilla/layers/CompositorParent.h"
 
 #ifdef MOZ_X11
 #include "gfxXlibSurface.h"
@@ -1974,6 +1975,12 @@ nsWindow::OnExposeEvent(cairo_t *cr)
         mAttachedWidgetListener ? mAttachedWidgetListener : mWidgetListener;
     if (!listener)
         return FALSE;
+
+    
+    
+    if (GetLayerManager()->GetBackendType() == LAYERS_CLIENT && mCompositorParent) {
+        mCompositorParent->ScheduleRenderOnCompositorThread();
+    }
 
     
     
