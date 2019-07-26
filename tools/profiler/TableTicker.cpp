89065d877efb5ab11c978f0fac7927a24cd752e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:47:04 +0000

    Bug 999071 - Don't notify observers in profiler when used by Android ANR reporter; r=BenWa
    UltraBlame original commit: b82a8c50aa904edada187a844e21f8dc9d96e955

--- tools/profiler/TableTicker.cpp
+++ tools/profiler/TableTicker.cpp
@@ -283,13 +283,15 @@ void TableTicker::StreamJSObject(JSStreamWriter& b)
         }
       }
 
-      
-      
-      SubprocessClosure closure(&b);
-      nsCOMPtr<nsIObserverService> os = mozilla::services::GetObserverService();
-      if (os) {
-        nsRefPtr<ProfileSaveEvent> pse = new ProfileSaveEvent(SubProcessCallback, &closure);
-        os->NotifyObservers(pse, "profiler-subprocess", nullptr);
+      if (Sampler::CanNotifyObservers()) {
+        
+        
+        SubprocessClosure closure(&b);
+        nsCOMPtr<nsIObserverService> os = mozilla::services::GetObserverService();
+        if (os) {
+          nsRefPtr<ProfileSaveEvent> pse = new ProfileSaveEvent(SubProcessCallback, &closure);
+          os->NotifyObservers(pse, "profiler-subprocess", nullptr);
+        }
       }
 
   #if defined(SPS_OS_android) && !defined(MOZ_WIDGET_GONK)
@@ -306,7 +308,6 @@ void TableTicker::StreamJSObject(JSStreamWriter& b)
     b.EndArray();
 
   b.EndObject();
-
 }
 
 
