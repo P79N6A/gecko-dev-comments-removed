f86d066172e3773d2984cec85c6b9d79f54ac439
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:21:35 +0000

    Bug 979481 - Prepare the cx stack machinery for a world where a cx has no default compartment object. r=bz
    UltraBlame original commit: 4846ef638fad7164a372def044fab677f113d6d1

--- js/xpconnect/src/XPCJSContextStack.cpp
+++ js/xpconnect/src/XPCJSContextStack.cpp
@@ -80,16 +80,16 @@ XPCJSContextStack::Push(JSContext *cx)
             
             
             
+            
             RootedObject defaultScope(cx, GetDefaultScopeFromJSContext(cx));
-
-            nsIPrincipal *currentPrincipal =
-              GetCompartmentPrincipal(js::GetContextCompartment(cx));
-            nsIPrincipal *defaultPrincipal = GetObjectPrincipal(defaultScope);
-            bool equal = false;
-            currentPrincipal->Equals(defaultPrincipal, &equal);
-            if (equal) {
-                mStack.AppendElement(cx);
-                return true;
+            if (defaultScope) {
+                nsIPrincipal *currentPrincipal =
+                  GetCompartmentPrincipal(js::GetContextCompartment(cx));
+                nsIPrincipal *defaultPrincipal = GetObjectPrincipal(defaultScope);
+                if (currentPrincipal->Equals(defaultPrincipal)) {
+                    mStack.AppendElement(cx);
+                    return true;
+                }
             }
         }
 
