9d988b33aba6056766a72b156ed45a2692553708
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:13:19 +0000

    Bug 852014 - Defect - Issues downloading and installing msi. r=sfoster.
    UltraBlame original commit: 855ddcd081e82ced8a378c8bad67a8a6445906b3

--- browser/metro/components/HelperAppDialog.js
+++ browser/metro/components/HelperAppDialog.js
@@ -25,7 +25,9 @@ HelperAppLauncherDialog.prototype = {
 
   show: function hald_show(aLauncher, aContext, aReason) {
     
-    if (aLauncher.MIMEInfo.hasDefaultHandler) {
+    
+    
+    if (aLauncher.MIMEInfo.hasDefaultHandler && !aLauncher.targetFileIsExecutable) {
       aLauncher.MIMEInfo.preferredAction = Ci.nsIMIMEInfo.useSystemDefault;
       aLauncher.launchWithApplication(null, false);
     } else {
@@ -208,7 +210,7 @@ HelperAppLauncherDialog.prototype = {
     let notifier = Cc[aCallback ? "@mozilla.org/alerts-service;1" : "@mozilla.org/toaster-alerts-service;1"].getService(Ci.nsIAlertsService);
     notifier.showAlertNotification(URI_GENERIC_ICON_DOWNLOAD,
                                    bundle.GetStringFromName("alertDownloads"),
-                                   bundle.GetStringFromName("alertCantOpenDownload"),
+                                   bundle.GetStringFromName("alertTapToSave"),
                                    true, "", aCallback, "downloadopen-fail");
   }
 };
