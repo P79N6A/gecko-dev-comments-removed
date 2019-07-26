8f809dc0326337aa060975d25c71ed13eaba98e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:08:38 +0000

    Bug 658909 - Implement carefully-checked unwrapping in XPCCallContext. r=mrbkap
    UltraBlame original commit: d4253db9e56065538c084b95ab87405172af85a6

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -1248,6 +1248,9 @@ private:
               jsval *argv,
               jsval *rval);
 
+    XPCWrappedNative* UnwrapThisIfAllowed(JSObject *obj, JSObject *fun,
+                                          unsigned argc);
+
 private:
     
     enum State {
