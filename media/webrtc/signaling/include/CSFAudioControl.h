0681ec83fe74569725f84aa4023363382c396126
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 10:04:27 +0000

    Bug 848423 - Change nearly all WebRTC instances of linked_ptr<> to nsRefPtr<> r=ehugg
    UltraBlame original commit: fe7fa32e9423472c9eda59789a4643c47420c5f4

--- media/webrtc/signaling/include/CSFAudioControl.h
+++ media/webrtc/signaling/include/CSFAudioControl.h
@@ -8,10 +8,11 @@
 
 namespace CSF
 {
-	DECLARE_PTR(AudioControl);
+        DECLARE_NS_PTR(AudioControl)
 	class ECC_API AudioControl
 	{
 	public:
+                NS_INLINE_DECL_THREADSAFE_REFCOUNTING(AudioControl)
 		
 
 		virtual std::vector<std::string> getRecordingDevices() = 0;
