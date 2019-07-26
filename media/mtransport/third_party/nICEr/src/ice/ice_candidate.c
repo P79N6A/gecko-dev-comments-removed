74cf5e727860fbba8fc2f59af81abeaf57fcd6fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:35:34 +0000

    Bug 819821, adds more logging for STUN, r=jesup
    UltraBlame original commit: 0ba4220f82ff42b773bbb1d4527d7fa59cf359a8

--- media/mtransport/third_party/nICEr/src/ice/ice_candidate.c
+++ media/mtransport/third_party/nICEr/src/ice/ice_candidate.c
@@ -474,6 +474,8 @@ static void nr_ice_srvrflx_stun_finished_cb(NR_SOCKET sock, int how, void *cb_ar
     int _status;
     nr_ice_candidate *cand=cb_arg;
 
+    r_log(LOG_ICE,LOG_DEBUG,"ICE(%s): %s for %s",cand->ctx->label,__FUNCTION__,cand->label);
+
     
     if(cand->u.srvrflx.stun_handle){ 
       nr_ice_socket_deregister(cand->isock,cand->u.srvrflx.stun_handle);
