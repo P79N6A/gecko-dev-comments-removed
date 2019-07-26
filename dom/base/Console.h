76d835b016c82b39b299cadbccd03cf77d6de4e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:03:29 +0000

    Bug 978522 - Console must use LinkedList instead a nsTArray, r=smaug
    UltraBlame original commit: f05e05787c07793b591f8ca26154ac54fd4ad11e

--- dom/base/Console.h
+++ dom/base/Console.h
@@ -128,10 +128,10 @@ private:
          const Sequence<JS::Value>& aData);
 
   void
-  AppendCallData(const ConsoleCallData& aData);
+  AppendCallData(ConsoleCallData* aData);
 
   void
-  ProcessCallData(ConsoleCallData& aData);
+  ProcessCallData(ConsoleCallData* aData);
 
   
   
@@ -181,11 +181,14 @@ private:
   IncreaseCounter(JSContext* aCx, const ConsoleStackEntry& aFrame,
                    const nsTArray<JS::Heap<JS::Value>>& aArguments);
 
+  void
+  ClearConsoleData();
+
   nsCOMPtr<nsPIDOMWindow> mWindow;
   nsCOMPtr<nsITimer> mTimer;
   nsCOMPtr<nsIConsoleAPIStorage> mStorage;
 
-  nsTArray<ConsoleCallData> mQueuedCalls;
+  LinkedList<ConsoleCallData> mQueuedCalls;
   nsDataHashtable<nsStringHashKey, DOMHighResTimeStamp> mTimerRegistry;
   nsDataHashtable<nsStringHashKey, uint32_t> mCounterRegistry;
 
