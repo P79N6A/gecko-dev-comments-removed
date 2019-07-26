c7e6ad7108b3a33cfc53424323d72d31db91f241
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:20:28 +0000

    Bug 958456 - Hides highlighter when hovering over non-visible nodes in markup-view; r=jwalker
    UltraBlame original commit: 60395c47c63bb2125323e8fbf9327a161877ded2

---
+++ browser/devtools/inspector/test/browser_inspector_bug_958456_highlight_comments.js
@@ -0,0 +1,107 @@
+
+
+
+
+
+
+
+
+
+let {devtools} = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
+let promise = devtools.require("sdk/core/promise");
+let {Task} = Cu.import("resource://gre/modules/Task.jsm", {});
+
+const TEST_PAGE = "http://mochi.test:8888/browser/browser/devtools/inspector/test/browser_inspector_bug_958456_highlight_comments.html";
+let inspector, markupView, doc;
+
+function test() {
+  waitForExplicitFinish();
+  gBrowser.selectedTab = gBrowser.addTab();
+  gBrowser.selectedBrowser.addEventListener("load", function onload() {
+    gBrowser.selectedBrowser.removeEventListener("load", onload, true);
+    doc = content.document;
+
+    waitForFocus(function() {
+      openInspector((aInspector, aToolbox) => {
+        inspector = aInspector;
+        markupView = inspector.markup;
+        inspector.once("inspector-updated", startTests);
+      });
+    }, content);
+  }, true);
+  content.location = TEST_PAGE;
+}
+
+function startTests() {
+  Task.spawn(function() {
+    yield prepareHighlighter();
+
+    yield hoverElement("#id1");
+    assertHighlighterShownOn("#id1");
+
+    yield hoverComment();
+    assertHighlighterHidden();
+
+    yield hoverElement("#id2");
+    assertHighlighterShownOn("#id2");
+
+    yield hoverElement("script");
+    assertHighlighterHidden();
+
+    yield hoverElement("#id3");
+    assertHighlighterShownOn("#id3");
+
+    yield hoverElement("#id4");
+    assertHighlighterHidden();
+  }).then(null, Cu.reportError).then(finishTest);
+}
+
+function finishTest() {
+  doc = inspector = markupView = null;
+  gBrowser.removeCurrentTab();
+  finish();
+}
+
+function prepareHighlighter() {
+  
+  let deferred = promise.defer();
+  inspector.selection.setNode(doc.querySelector("p"), null);
+  inspector.once("inspector-updated", () => {
+    getHighlighterOutline().setAttribute("disable-transitions", "true");
+    deferred.resolve();
+  });
+  return deferred.promise;
+}
+
+function hoverContainer(container) {
+  let deferred = promise.defer();
+  EventUtils.synthesizeMouse(container.tagLine, 2, 2, {type: "mousemove"},
+      markupView.doc.defaultView);
+  inspector.markup.once("node-highlight", deferred.resolve);
+  return deferred.promise;
+}
+
+function hoverElement(selector) {
+  info("Hovering node " + selector + " in the markup view");
+  let container = getContainerForRawNode(markupView, doc.querySelector(selector));
+  return hoverContainer(container);
+}
+
+function hoverComment() {
+  info("Hovering the comment node in the markup view");
+  for (let [node, container] of markupView._containers) {
+    if (node.nodeType === Ci.nsIDOMNode.COMMENT_NODE) {
+      return hoverContainer(container);
+    }
+  }
+}
+
+function assertHighlighterShownOn(selector) {
+  let node = doc.querySelector(selector);
+  let highlightNode = getHighlitNode();
+  is(node, highlightNode, "Highlighter is shown on the right node: " + selector);
+}
+
+function assertHighlighterHidden() {
+  ok(!isHighlighting(), "Highlighter is hidden");
+}
