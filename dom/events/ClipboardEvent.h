87f4182258a10e04356d8bf1b7e02ace168a3be5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:00:04 +0000

    Bug 975688 part.4 Rename nsDOMClipboardEvent to mozilla::dom::ClipboardEvent r=smaug
    UltraBlame original commit: 6abc19b10e911c976bb3006b890a0e14d7802d0c

---
+++ dom/events/ClipboardEvent.h
@@ -0,0 +1,50 @@
+
+
+
+
+
+#ifndef mozilla_dom_ClipboardEvent_h_
+#define mozilla_dom_ClipboardEvent_h_
+
+#include "nsIDOMClipboardEvent.h"
+#include "nsDOMEvent.h"
+#include "mozilla/EventForwards.h"
+#include "mozilla/dom/ClipboardEventBinding.h"
+
+namespace mozilla {
+namespace dom {
+
+class ClipboardEvent : public nsDOMEvent,
+                       public nsIDOMClipboardEvent
+{
+public:
+  ClipboardEvent(EventTarget* aOwner,
+                 nsPresContext* aPresContext,
+                 InternalClipboardEvent* aEvent);
+
+  NS_DECL_ISUPPORTS_INHERITED
+
+  NS_DECL_NSIDOMCLIPBOARDEVENT
+
+  
+  NS_FORWARD_TO_NSDOMEVENT
+
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aScope) MOZ_OVERRIDE
+  {
+    return ClipboardEventBinding::Wrap(aCx, aScope, this);
+  }
+
+  static already_AddRefed<ClipboardEvent>
+  Constructor(const GlobalObject& aGlobal,
+              const nsAString& aType,
+              const ClipboardEventInit& aParam,
+              ErrorResult& aRv);
+
+  nsIDOMDataTransfer* GetClipboardData();
+};
+
+} 
+} 
+
+#endif 
