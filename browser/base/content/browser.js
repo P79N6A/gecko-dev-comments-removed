8fde51934f1260bf050e65a9ce54b7099dc02c36
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:20:52 +0000

    Bug 1016138 - Add telemetry probe for master password usage. r=dolske
    UltraBlame original commit: cff3a3c904b5ea405802a3bd3335f18ee3c77c8f

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -1226,6 +1226,21 @@ var gBrowserInit = {
 
       SocialUI.init();
       TabView.init();
+
+      
+      
+      setTimeout(() => {
+        if (window.closed) {
+          return;
+        }
+        let secmodDB = Cc["@mozilla.org/security/pkcs11moduledb;1"]
+                       .getService(Ci.nsIPKCS11ModuleDB);
+        let slot = secmodDB.findSlotByName("");
+        let mpEnabled = slot &&
+                        slot.status != Ci.nsIPKCS11Slot.SLOT_UNINITIALIZED &&
+                        slot.status != Ci.nsIPKCS11Slot.SLOT_READY;
+        Services.telemetry.getHistogramById("MASTER_PASSWORD_ENABLED").add(mpEnabled);
+      }, 5000);
     });
     this.delayedStartupFinished = true;
 
