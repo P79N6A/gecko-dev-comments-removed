90e92a9e577c1dfc55fcc3b6f2ae86a089bdbdf3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:40:00 +0000

    Bug 846696 - GCLI helpers.js should be shared rather than copied; r=mratcliffe
    UltraBlame original commit: 776fafd014db7e08e7f4a5a8609672450f006fa5

--- browser/devtools/responsivedesign/test/head.js
+++ browser/devtools/responsivedesign/test/head.js
@@ -8,7 +8,7 @@ let TargetFactory = devtools.TargetFactory;
 
 
 let testDir = gTestPath.substr(0, gTestPath.lastIndexOf("/"));
-Services.scriptloader.loadSubScript(testDir + "/helpers.js", this);
+Services.scriptloader.loadSubScript(testDir + "../../../commandline/test/helpers.js", this);
 
 function openInspector(callback)
 {
