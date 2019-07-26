357351640e32cfe82f708d218efabec89a23fb53
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:50:31 +0000

    Bug 952650 (part 16) - Remove JSVAL_TO_PRIVATE. r=till.
    UltraBlame original commit: 697506d9c32459abcbf817fbf54fb793078d49d3

--- js/public/Value.h
+++ js/public/Value.h
@@ -1882,13 +1882,6 @@ PRIVATE_TO_JSVAL(void *ptr)
     return IMPL_TO_JSVAL(PRIVATE_PTR_TO_JSVAL_IMPL(ptr));
 }
 
-static inline void *
-JSVAL_TO_PRIVATE(jsval v)
-{
-    MOZ_ASSERT(v.isDouble());
-    return JSVAL_TO_PRIVATE_PTR_IMPL(JSVAL_TO_IMPL(v));
-}
-
 
 
 
