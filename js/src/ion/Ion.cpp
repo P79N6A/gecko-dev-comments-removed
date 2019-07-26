a3ae39f34cffc272872e82097b06d053a63e2b04
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 08:56:57 +0000

    Bug 763989 - Mark the ArgumentsRectifier via ImmGCPtr. r=dvander
    UltraBlame original commit: 5f86bbdfe5048ef1596136ade3e9bda43cf396a1

--- js/src/ion/Ion.cpp
+++ js/src/ion/Ion.cpp
@@ -171,29 +171,43 @@ IonCompartment::initialize(JSContext *cx)
     return true;
 }
 
+static void
+MaybeMarkIonCodeRoot(JSTracer *trc, IonCode **code, const char *desc)
+{
+    if (*code)
+        MarkIonCodeRoot(trc, code, desc);
+}
+
 void
 IonCompartment::mark(JSTracer *trc, JSCompartment *compartment)
 {
     
-    
-    
-    
-    
+    if (compartment->isPreservingCode()) {
 
-    bool mustMarkEnterJIT = false;
-    for (IonActivationIterator iter(trc->runtime); iter.more(); ++iter) {
-        if (iter.activation()->compartment() != compartment)
-            continue;
+        MaybeMarkIonCodeRoot(trc, enterJIT_.unsafeGet(), "enterJIT");
+        MaybeMarkIonCodeRoot(trc, bailoutHandler_.unsafeGet(), "bailoutHandler");
+        MaybeMarkIonCodeRoot(trc, argumentsRectifier_.unsafeGet(), "argumentsRectifier");
+        MaybeMarkIonCodeRoot(trc, invalidator_.unsafeGet(), "invalidator");
+        MaybeMarkIonCodeRoot(trc, preBarrier_.unsafeGet(), "preBarrier");
 
         
         
-        mustMarkEnterJIT = true;
+        return;
     }
 
     
     
-    if (mustMarkEnterJIT)
-        MarkIonCodeRoot(trc, enterJIT_.unsafeGet(), "enterJIT");
+    
+    
+    
+    
+    
+    for (IonActivationIterator iter(trc->runtime); iter.more(); ++iter) {
+        if (iter.activation()->compartment() == compartment) {
+            MarkIonCodeRoot(trc, enterJIT_.unsafeGet(), "enterJIT");
+            return;
+        }
+    }
 
     
     
