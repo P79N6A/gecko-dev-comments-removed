de4857795160aeddcc825dddd249568e4d09ee42
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:09:23 +0000

    Bug 894547 - move tests abstraction up for at caret offset text testing, r=tbsaunde
    UltraBlame original commit: cf03ffaa23cce203eedcf7301e99f0b9c0bdbce2

--- accessible/tests/mochitest/events.js
+++ accessible/tests/mochitest/events.js
@@ -1440,6 +1440,29 @@ function moveToLineEnd(aID, aCaretOffset)
 
 
 
+function moveToPrevLineEnd(aID, aCaretOffset)
+{
+  this.__proto__ = new synthAction(aID, new caretMoveChecker(aCaretOffset, aID));
+
+  this.invoke = function moveToPrevLineEnd_invoke()
+  {
+    synthesizeKey("VK_UP", { });
+
+    if (MAC)
+      synthesizeKey("VK_RIGHT", { metaKey: true });
+    else
+      synthesizeKey("VK_END", { });
+  }
+
+  this.getID = function moveToPrevLineEnd_getID()
+  {
+    return "move to previous line end in " + prettyName(aID);
+  }
+}
+
+
+
+
 function moveToLineStart(aID, aCaretOffset)
 {
   if (MAC) {
