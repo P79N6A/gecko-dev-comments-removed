b0ed5c76baba1d34efe6f917a5c57622285fc493
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:08:28 +0000

    Bug 975239 - (Part 3) Refactor HomeBanner.handleMessage to handle being called multiple times. r=bnicholson
    UltraBlame original commit: be4a906fa5fadd86537afc39cc5dd2fd662900cc

--- mobile/android/base/home/HomeBanner.java
+++ mobile/android/base/home/HomeBanner.java
@@ -131,42 +131,42 @@ public class HomeBanner extends LinearLayout
 
     @Override
     public void handleMessage(String event, JSONObject message) {
-        try {
-            
-            setTag(message.getString("id"));
-
-            
-            final Spanned text = Html.fromHtml(message.getString("text"));
-
-            
-            ThreadUtils.postToUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mTextView.setText(text);
-                    setVisibility(VISIBLE);
-                    GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("HomeBanner:Shown", (String) getTag()));
-
-                    
-                    if (mActive) {
-                        animateUp();
-                    }
-                }
-            });
-        } catch (JSONException e) {
-            Log.e(LOGTAG, "Exception handling " + event + " message", e);
-            return;
-        }
-
+        final String id = message.optString("id");
+        final String text = message.optString("text");
         final String iconURI = message.optString("iconURI");
 
-        BitmapUtils.getDrawable(getContext(), iconURI, new BitmapUtils.BitmapLoader() {
+        
+        ThreadUtils.postToUiThread(new Runnable() {
             @Override
-            public void onBitmapFound(final Drawable d) {
+            public void run() {
                 
-                if (d == null) {
-                    mIconView.setVisibility(View.GONE);
-                } else {
-                    mIconView.setImageDrawable(d);
+                if (TextUtils.isEmpty(id) || TextUtils.isEmpty(text)) {
+                    setVisibility(View.GONE);
+                    return;
+                }
+
+                
+                setTag(id);
+                mTextView.setText(Html.fromHtml(text));
+
+                BitmapUtils.getDrawable(getContext(), iconURI, new BitmapUtils.BitmapLoader() {
+                    @Override
+                    public void onBitmapFound(final Drawable d) {
+                        
+                        if (d == null) {
+                            mIconView.setVisibility(View.GONE);
+                        } else {
+                            mIconView.setImageDrawable(d);
+                        }
+                    }
+                });
+
+                setVisibility(View.VISIBLE);
+                GeckoAppShell.sendEventToGecko(GeckoEvent.createBroadcastEvent("HomeBanner:Shown", id));
+
+                
+                if (mActive) {
+                    animateUp();
                 }
             }
         });
