ef956d91aa6d760959298cb5b55fe15de4be3d34
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:38:30 +0000

    Bug 824448 part 1.  Rename nsHTMLHeadingElement to mozilla::dom::HTMLHeadingElement.  r=peterv
    UltraBlame original commit: dc616866209937ed941d1abeb1c939c32888a003

---
+++ content/html/content/src/HTMLHeadingElement.h
@@ -0,0 +1,54 @@
+
+
+
+
+
+#ifndef mozilla_dom_HTMLHeadingElement_h
+#define mozilla_dom_HTMLHeadingElement_h
+
+#include "nsIDOMHTMLHeadingElement.h"
+#include "nsGenericHTMLElement.h"
+
+namespace mozilla {
+namespace dom {
+
+class HTMLHeadingElement MOZ_FINAL : public nsGenericHTMLElement,
+				     public nsIDOMHTMLHeadingElement
+{
+public:
+  HTMLHeadingElement(already_AddRefed<nsINodeInfo> aNodeInfo)
+    : nsGenericHTMLElement(aNodeInfo)
+  {
+  }
+  virtual ~HTMLHeadingElement();
+
+  
+  NS_DECL_ISUPPORTS_INHERITED
+
+  
+  NS_FORWARD_NSIDOMNODE_TO_NSINODE
+
+  
+  NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
+
+  
+  NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
+
+  
+  NS_DECL_NSIDOMHTMLHEADINGELEMENT
+
+  virtual bool ParseAttribute(int32_t aNamespaceID,
+                              nsIAtom* aAttribute,
+                              const nsAString& aValue,
+                              nsAttrValue& aResult);
+  NS_IMETHOD_(bool) IsAttributeMapped(const nsIAtom* aAttribute) const;
+  nsMapRuleToAttributesFunc GetAttributeMappingFunction() const;
+  virtual nsresult Clone(nsINodeInfo *aNodeInfo, nsINode **aResult) const;
+  virtual nsXPCClassInfo* GetClassInfo();
+  virtual nsIDOMNode* AsDOMNode() { return this; }
+};
+
+} 
+} 
+
+#endif 
