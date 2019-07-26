4f94b84e5543b716dc1166da1a3ce9d261abe9e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:21:53 +0000

    Bug 897399: handle the inner window disappearing after beforescriptexecute without crashing, r=sicking
    UltraBlame original commit: bfdaf248533bf6e507b7644cd27eb702e68ff181

--- content/base/src/nsScriptLoader.cpp
+++ content/base/src/nsScriptLoader.cpp
@@ -739,6 +739,12 @@ nsScriptLoader::ProcessRequest(nsScriptLoadRequest* aRequest)
                                          true, true, &runScript);
   }
 
+  
+  pwin = mDocument->GetInnerWindow();
+  if (!pwin) {
+    runScript = false;
+  }
+
   nsresult rv = NS_OK;
   if (runScript) {
     if (doc) {
