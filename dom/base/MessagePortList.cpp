309d443ac691de7b69cc3422aba0165537b60651
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:39:34 +0000

    Bug 848294 - Update MessageEvent to be compatible with the spec, r=bz
    UltraBlame original commit: 83075b26f91b7dc0207f837c79c175154e5dfb44

---
+++ dom/base/MessagePortList.cpp
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+#include "MessagePortList.h"
+#include "mozilla/dom/MessagePortListBinding.h"
+#include "mozilla/dom/MessagePort.h"
+
+namespace mozilla {
+namespace dom {
+
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE_2(MessagePortList, mOwner, mPorts)
+NS_IMPL_CYCLE_COLLECTING_ADDREF(MessagePortList)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(MessagePortList)
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(MessagePortList)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_END
+
+JSObject*
+MessagePortList::WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope)
+{
+  return MessagePortListBinding::Wrap(aCx, aScope, this);
+}
+
+} 
+} 
---
+++ dom/base/MessagePortList.h
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_MessagePortList_h
+#define mozilla_dom_MessagePortList_h
+
+#include "mozilla/Attributes.h"
+#include "mozilla/ErrorResult.h"
+#include "nsCycleCollectionParticipant.h"
+#include "mozilla/dom/MessagePort.h"
+#include "nsWrapperCache.h"
+#include "nsAutoPtr.h"
+#include "nsTArray.h"
+
+namespace mozilla {
+namespace dom {
+
+class MessagePortList MOZ_FINAL : public nsISupports
+                                , public nsWrapperCache
+{
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(MessagePortList)
+
+public:
+  MessagePortList(nsISupports* aOwner, nsTArray<nsRefPtr<MessagePort> >& aPorts)
+    : mOwner(aOwner)
+    , mPorts(aPorts)
+  {
+    SetIsDOMBinding();
+  }
+
+  nsISupports*
+  GetParentObject() const
+  {
+    return mOwner;
+  }
+
+  virtual JSObject*
+  WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
+
+  uint32_t
+  Length() const
+  {
+    return mPorts.Length();
+  }
+
+  MessagePort*
+  Item(uint32_t aIndex)
+  {
+    return mPorts.SafeElementAt(aIndex);
+  }
+
+  MessagePort*
+  IndexedGetter(uint32_t aIndex, bool &aFound)
+  {
+    aFound = aIndex < mPorts.Length();
+    if (!aFound) {
+      return nullptr;
+    }
+    return mPorts[aIndex];
+  }
+
+public:
+  nsCOMPtr<nsISupports> mOwner;
+  nsTArray<nsRefPtr<MessagePort> > mPorts;
+};
+
+} 
+} 
+
+#endif 
+
