3eadc4fdf3cecbb420a41e30d0fdcf2570d657ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:37:01 +0000

    Bug 966416 - Uplift Add-on SDK to Firefox
    UltraBlame original commit: c6f045e87c351de03e09ec5a7d9e95b704437790

---
+++ addon-sdk/source/test/addons/simple-prefs-l10n/main.js
@@ -0,0 +1,57 @@
+
+
+
+'use strict';
+
+const { Cu } = require('chrome');
+const sp = require('sdk/simple-prefs');
+const app = require('sdk/system/xul-app');
+const self = require('sdk/self');
+const tabs = require('sdk/tabs');
+const { preferencesBranch } = require('sdk/self');
+
+const { AddonManager } = Cu.import('resource://gre/modules/AddonManager.jsm', {});
+
+exports.testAOMLocalization = function(assert, done) {
+    tabs.open({
+    	url: 'about:addons',
+    	onReady: function(tab) {
+        tab.attach({
+          contentScriptWhen: 'end',
+        	contentScript: 'function onLoad() {\n' +
+                           'unsafeWindow.removeEventListener("load", onLoad, false);\n' +
+                           'AddonManager.getAddonByID("' + self.id + '", function(aAddon) {\n' +
+                             'unsafeWindow.gViewController.viewObjects.detail.node.addEventListener("ViewChanged", function whenViewChanges() {\n' +
+                               'unsafeWindow.gViewController.viewObjects.detail.node.removeEventListener("ViewChanged", whenViewChanges, false);\n' +
+                               'setTimeout(function() {\n' + 
+                                   'self.postMessage({\n' +
+                                     'somePreference: getAttributes(unsafeWindow.document.querySelector("setting[data-jetpack-id=\'' + self.id + '\']"))\n' +
+                                   '});\n' +
+                               '}, 250);\n' +
+                             '}, false);\n' +
+                             'unsafeWindow.gViewController.commands.cmd_showItemDetails.doCommand(aAddon, true);\n' +
+                           '});\n' +
+                           'function getAttributes(ele) {\n' +
+                             'if (!ele) return {};\n' +
+                             'return {\n' +
+                               'title: ele.getAttribute("title")\n' +
+                             '}\n' +
+                           '}\n' +
+                         '}\n' +
+                         
+                         'if (document.readyState == "complete") {\n' +
+                           'onLoad()\n' +
+                         '} else {\n' +
+                           'unsafeWindow.addEventListener("load", onLoad, false);\n' +
+                         '}\n',
+          onMessage: function(msg) {
+            
+            assert.equal(msg.somePreference.title, 'A', 'somePreference title is correct');
+            tab.close(done);
+          }
+        });
+    	}
+    });
+}
+
+require('sdk/test/runner').runTestsFromModule(module);
