522c0561a0754de2e9d1704247c25b4105637408
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:29:34 +0000

    Bug 992105 - Remember topics for screensaver lock in Windows WakeLockListener, so that toggling fullscreen while playing video doesn't prevent screen wakelock being unlocked. r=edwin
    UltraBlame original commit: a28cd2a8b3970721f0cd2c93bc33cfbc968764e8

--- widget/windows/nsAppShell.cpp
+++ widget/windows/nsAppShell.cpp
@@ -18,6 +18,7 @@
 #include "nsIDOMWakeLockListener.h"
 #include "nsIPowerManagerService.h"
 #include "mozilla/StaticPtr.h"
+#include "nsTHashtable.h"
 #include "GeckoProfiler.h"
 
 using namespace mozilla;
@@ -32,15 +33,33 @@ public:
 
 private:
   NS_IMETHOD Callback(const nsAString& aTopic, const nsAString& aState) {
-    if (aState.Equals(NS_LITERAL_STRING("locked-foreground"))) {
-      
-      SetThreadExecutionState(ES_DISPLAY_REQUIRED|ES_CONTINUOUS);
+    bool isLocked = mLockedTopics.Contains(aTopic);
+    bool shouldLock = aState.Equals(NS_LITERAL_STRING("locked-foreground"));
+    if (isLocked == shouldLock) {
+      return NS_OK;
+    }
+    if (shouldLock) {
+      if (!mLockedTopics.Count()) {
+        
+        
+        SetThreadExecutionState(ES_DISPLAY_REQUIRED|ES_CONTINUOUS);
+      }
+      mLockedTopics.PutEntry(aTopic);
     } else {
-      
-      SetThreadExecutionState(ES_CONTINUOUS);
+      mLockedTopics.RemoveEntry(aTopic);
+      if (!mLockedTopics.Count()) {
+        
+        
+        SetThreadExecutionState(ES_CONTINUOUS);
+      }
    }
     return NS_OK;
   }
+
+  
+  
+  
+  nsTHashtable<nsStringHashKey> mLockedTopics;
 };
 
 NS_IMPL_ISUPPORTS1(WinWakeLockListener, nsIDOMMozWakeLockListener)
