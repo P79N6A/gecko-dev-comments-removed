b3cfb179f740d977aa6bf61c86628f416b0432cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:44:06 +0000

    Bug 818224 - Show all chrome script errors originated from private windows; r=bholley
    UltraBlame original commit: 59a4df8eb964e8b7ed5260e72fa432bab3ef2e2c

--- js/xpconnect/src/nsScriptError.cpp
+++ js/xpconnect/src/nsScriptError.cpp
@@ -137,7 +137,9 @@ nsScriptError::InitWithWindowID(const nsAString& message,
             nsIDocShell* docShell = window->GetDocShell();
             nsCOMPtr<nsILoadContext> loadContext = do_QueryInterface(docShell);
             if (loadContext) {
-              mIsFromPrivateWindow = loadContext->UsePrivateBrowsing();
+              
+              mIsFromPrivateWindow = !nsContentUtils::IsCallerChrome() &&
+                                     loadContext->UsePrivateBrowsing();
             }
         }
     }
