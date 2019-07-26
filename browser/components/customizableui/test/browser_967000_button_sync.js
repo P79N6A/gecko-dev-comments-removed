79f754a7ac1fb9045a9e3deaddab7ea117fa26fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:20:49 +0000

    Bug 967000 - add test for panel menu non-default buttons: sync. r=Gijs
    UltraBlame original commit: e3f74e8f72e65e5359a22d44ea67baa183e06bda

---
+++ browser/components/customizableui/test/browser_967000_button_sync.js
@@ -0,0 +1,44 @@
+
+
+
+
+"use strict";
+
+let initialLocation = gBrowser.currentURI.spec;
+let newTab = null;
+
+add_task(function() {
+  info("Check Sync button functionality");
+
+  
+  CustomizableUI.addWidgetToArea("sync-button", CustomizableUI.AREA_PANEL);
+
+  
+  yield PanelUI.show();
+
+  let syncButton = document.getElementById("sync-button");
+  ok(syncButton, "The Sync button was added to the Panel Menu");
+  syncButton.click();
+
+  newTab = gBrowser.selectedTab;
+  yield promiseTabLoadEvent(newTab, "about:accounts");
+
+  is(gBrowser.currentURI.spec, "about:accounts", "Firefox Sync page opened");
+  ok(!isPanelUIOpen(), "The panel closed");
+
+  if(isPanelUIOpen()) {
+    let panelHidePromise = promisePanelHidden(window);
+    PanelUI.hide();
+    yield panelHidePromise;
+  }
+});
+
+add_task(function asyncCleanup() {
+  
+  yield resetCustomization();
+  ok(CustomizableUI.inDefaultState, "The panel UI is in default state again.");
+
+  
+  gBrowser.addTab(initialLocation);
+  gBrowser.removeTab(newTab);
+});
