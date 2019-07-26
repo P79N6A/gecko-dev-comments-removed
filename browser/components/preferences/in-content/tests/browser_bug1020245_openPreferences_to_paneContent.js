0af99e94c44e9ab99a92718715f8fa336f6b97a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:19:38 +0000

    Bug 1020245 - Call openPreferences from utilityOverlay.js. r=jaws
    UltraBlame original commit: 156e9796cad400bbb34e6549c4fc6093517b5d4e

---
+++ browser/components/preferences/in-content/tests/browser_bug1020245_openPreferences_to_paneContent.js
@@ -0,0 +1,25 @@
+
+
+
+add_task(function test() {
+  waitForExplicitFinish();
+  let deferred = Promise.defer();
+  gBrowser.selectedTab = gBrowser.addTab("about:blank");
+  openPreferences("paneContent");
+  let newTabBrowser = gBrowser.getBrowserForTab(gBrowser.selectedTab);
+
+  newTabBrowser.addEventListener("Initialized", function PrefInit() {
+    newTabBrowser.removeEventListener("Initialized", PrefInit, true);
+    newTabBrowser.contentWindow.addEventListener("load", function prefLoad() {
+      newTabBrowser.contentWindow.removeEventListener("load", prefLoad);
+      let sel = gBrowser.contentWindow.history.state;
+      is(sel, "paneContent", "Content pane was selected");
+      deferred.resolve();
+      gBrowser.removeCurrentTab();
+    });
+  }, true);
+
+  yield deferred.promise;
+
+  finish();
+});
