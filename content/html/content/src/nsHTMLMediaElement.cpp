2e66c6c6286566be0316edfc71d27d6195deea24
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:41:57 +0000

    Bug 819852 - Modify conditions from media element of judging playing state for audio channel agent with consideration of loop property. r=roc, a=blocking-basecamp
    UltraBlame original commit: 0f42175818fe5a460f75b91797ddb620bd61a239

--- content/html/content/src/nsHTMLMediaElement.cpp
+++ content/html/content/src/nsHTMLMediaElement.cpp
@@ -3570,9 +3570,10 @@ void nsHTMLMediaElement::UpdateAudioChannelPlayingState()
   
 #ifdef MOZ_B2G
   bool playingThroughTheAudioChannel =
-     (mReadyState >= nsIDOMHTMLMediaElement::HAVE_CURRENT_DATA &&
-      !mPaused &&
-      !IsPlaybackEnded());
+     (!mPaused &&
+      (HasAttr(kNameSpaceID_None, nsGkAtoms::loop) ||
+      mReadyState >= nsIDOMHTMLMediaElement::HAVE_CURRENT_DATA &&
+      !IsPlaybackEnded()));
   if (playingThroughTheAudioChannel != mPlayingThroughTheAudioChannel) {
     mPlayingThroughTheAudioChannel = playingThroughTheAudioChannel;
 
