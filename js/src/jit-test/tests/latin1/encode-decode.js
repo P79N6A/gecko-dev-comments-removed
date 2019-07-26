daf23140ac6046963f41912ccdf4019a971843fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 23:23:50 +0000

    Backed out changeset aea4ea200313 (bug 1026680)
    UltraBlame original commit: 15128e1b49a2220872c8f0dea780116efe39f65f

--- js/src/jit-test/tests/latin1/encode-decode.js
+++ js/src/jit-test/tests/latin1/encode-decode.js
@@ -1,13 +1,10 @@
-
 s = toLatin1("a%2b%20def%00A0");
 assertEq(decodeURI(s), "a%2b def\x00A0");
 assertEq(decodeURIComponent(s), "a+ def\x00A0");
 
-
 s += "\u1200";
 assertEq(decodeURI(s), "a%2b def\x00A0\u1200");
 assertEq(decodeURIComponent(s), "a+ def\x00A0\u1200");
-
 
 try {
     decodeURI(toLatin1("abc%80"));
@@ -15,26 +12,8 @@ try {
 } catch(e) {
     assertEq(e instanceof URIError, true);
 }
-
-
 try {
     decodeURI("abc%80\u1200");
-    assertEq(0, 1);
-} catch(e) {
-    assertEq(e instanceof URIError, true);
-}
-
-
-assertEq(encodeURI(toLatin1("a%2b def\x00A0")), "a%252b%20def%00A0");
-assertEq(encodeURIComponent(toLatin1("a+ def\x00A0")), "a%2B%20def%00A0");
-
-
-assertEq(encodeURI("a%2b def\x00A0\u1200"), "a%252b%20def%00A0%E1%88%80");
-assertEq(encodeURIComponent("a+ def\x00A0\u1200"), "a%2B%20def%00A0%E1%88%80");
-
-
-try {
-    encodeURI("a\uDB00");
     assertEq(0, 1);
 } catch(e) {
     assertEq(e instanceof URIError, true);
