ab3aa783686576e711e374891bb9fdece9e10888
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:34:31 +0000

    Bug 964349 - Move CrashReporter.annotateCrashReport() calls out of sessionstore r=gavin
    UltraBlame original commit: 78925fbd7e9c43eabbb47883b627d01a13d09f5a

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -3733,6 +3733,22 @@ var XULBrowserWindow = {
       setTimeout(function () { XULBrowserWindow.asyncUpdateUI(); }, 0);
     else
       this.asyncUpdateUI();
+
+#ifdef MOZ_CRASHREPORTER
+    if (aLocationURI) {
+      let uri = aLocationURI.clone();
+      try {
+        
+        uri.userPass = "";
+      } catch (ex) {  }
+
+      try {
+        Services.appinfo.annotateCrashReport("URL", uri.spec);
+      } catch (ex if ex.result == Components.results.NS_ERROR_NOT_INITIALIZED) {
+        
+      }
+    }
+#endif
   },
 
   asyncUpdateUI: function () {
@@ -3992,15 +4008,6 @@ var CombinedStopReload = {
 
 var TabsProgressListener = {
   onStateChange: function (aBrowser, aWebProgress, aRequest, aStateFlags, aStatus) {
-#ifdef MOZ_CRASHREPORTER
-    if (aRequest instanceof Ci.nsIChannel &&
-        aStateFlags & Ci.nsIWebProgressListener.STATE_START &&
-        aStateFlags & Ci.nsIWebProgressListener.STATE_IS_DOCUMENT &&
-        gCrashReporter.enabled) {
-      gCrashReporter.annotateCrashReport("URL", aRequest.URI.spec);
-    }
-#endif
-
     
     if (aWebProgress.isTopLevel) {
       if (aStateFlags & Ci.nsIWebProgressListener.STATE_IS_WINDOW) {
