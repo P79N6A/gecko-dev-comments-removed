4ef528f9083b98f621d739119f7ed41f600beaf9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:23:12 +0000

    Bug 959787 - Handlify has property APIs r=terrence r=bz
    UltraBlame original commit: 6f5e29e2dc7723ac7a87c43857d5d87eb2fff991

--- dom/bindings/BindingUtils.h
+++ dom/bindings/BindingUtils.h
@@ -2350,6 +2350,36 @@ CreateGlobal(JSContext* aCx, T* aObject, nsWrapperCache* aCache,
   return global;
 }
 
+
+
+
+
+class InternedStringId
+{
+  jsid id;
+
+ public:
+  InternedStringId() : id(JSID_VOID) {}
+
+  bool init(JSContext *cx, const char *string) {
+    MOZ_ASSERT(id == JSID_VOID);
+    JSString* str = JS_InternString(cx, string);
+    if (!str)
+      return false;
+    id = INTERNED_STRING_TO_JSID(cx, str);
+    return true;
+  }
+
+  operator const jsid& () {
+    return id;
+  }
+
+  operator JS::Handle<jsid> () {
+    
+    return JS::Handle<jsid>::fromMarkedLocation(&id);
+  }
+};
+
 } 
 } 
 
