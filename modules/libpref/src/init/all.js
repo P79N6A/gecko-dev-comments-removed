48759614d8d5815b1ef3970ad4b48efbc187e484
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:28:57 +0000

    Bug 900133 - Backout enabing new textures on OSX
    UltraBlame original commit: 5f81cbac8d62600ff7fef4e9cf1910311a9098bf

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -4070,6 +4070,9 @@ pref("layers.async-video.enabled",false);
 pref("layers.acceleration.disabled", false);
 
 
+pref("layers.use-deprecated-textures", true);
+
+
 #ifdef ANDROID
 
 
@@ -4089,11 +4092,8 @@ pref("layers.max-active", -1);
 
 #ifdef XP_MACOSX
 pref("layers.offmainthreadcomposition.enabled", true);
-
-pref("layers.use-deprecated-textures", false);
 #else
 pref("layers.offmainthreadcomposition.enabled", false);
-pref("layers.use-deprecated-textures", true);
 #endif
 
 
