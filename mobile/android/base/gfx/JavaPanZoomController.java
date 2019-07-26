83ef5c92f41497d5c5b9057bcbebcda619323254
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:33:55 +0000

    Bug 711959 - Make FlingRenderTask time-based. r=kats
    UltraBlame original commit: f13c3491c9c82f259d72d0d160c7ffabd0790cc1

--- mobile/android/base/gfx/JavaPanZoomController.java
+++ mobile/android/base/gfx/JavaPanZoomController.java
@@ -828,6 +828,10 @@ class JavaPanZoomController
 
 
         protected long mCurrentFrameStartTime;
+        
+
+
+        protected long mLastFrameTimeDelta;
 
         private final Runnable mRunnable = new Runnable() {
             @Override
@@ -848,6 +852,7 @@ class JavaPanZoomController
         protected final boolean internalRun(long timeDelta, long currentFrameStartTime) {
 
             mCurrentFrameStartTime = currentFrameStartTime;
+            mLastFrameTimeDelta = timeDelta;
 
             mTarget.post(mRunnable);
             return mContinueAnimation;
@@ -965,8 +970,8 @@ class JavaPanZoomController
             }
 
             
-            boolean flingingX = mX.advanceFling();
-            boolean flingingY = mY.advanceFling();
+            boolean flingingX = mX.advanceFling(mLastFrameTimeDelta);
+            boolean flingingY = mY.advanceFling(mLastFrameTimeDelta);
 
             boolean overscrolled = (mX.overscrolled() || mY.overscrolled());
 
