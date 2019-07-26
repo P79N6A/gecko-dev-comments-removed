ae8e0d684fdc07fd09977eff81e72431650bc9c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:16:28 +0000

    Bug 805254. Part 11: Move Vorbis-specific definitons into a new file VorbisUtils.h. r=kinetik
    UltraBlame original commit: c3fcdbf501049aec59009d7804e05d793abaeacc

---
+++ content/media/VorbisUtils.h
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+#ifndef VORBISUTILS_H_
+#define VORBISUTILS_H_
+
+#ifdef MOZ_SAMPLE_TYPE_S16
+#include <ogg/os_types.h>
+typedef ogg_int32_t VorbisPCMValue;
+
+#define MOZ_CLIP_TO_15(x) ((x)<-32768?-32768:(x)<=32767?(x):32767)
+
+#define MOZ_CONVERT_VORBIS_SAMPLE(x) \
+ (static_cast<AudioDataValue>(MOZ_CLIP_TO_15((x)>>9)))
+
+#else 
+
+typedef float VorbisPCMValue;
+
+#define MOZ_CONVERT_VORBIS_SAMPLE(x) (x)
+
+#endif
+
+#endif 
--- content/media/nsBuiltinDecoderReader.h
+++ content/media/nsBuiltinDecoderReader.h
@@ -54,23 +54,6 @@ public:
   bool mHasVideo;
 };
 
-#ifdef MOZ_SAMPLE_TYPE_S16
-#include <ogg/os_types.h>
-typedef ogg_int32_t VorbisPCMValue;
-
-#define MOZ_CLIP_TO_15(x) ((x)<-32768?-32768:(x)<=32767?(x):32767)
-
-#define MOZ_CONVERT_VORBIS_SAMPLE(x) \
- (static_cast<AudioDataValue>(MOZ_CLIP_TO_15((x)>>9)))
-
-#else 
-
-typedef float VorbisPCMValue;
-
-#define MOZ_CONVERT_VORBIS_SAMPLE(x) (x)
-
-#endif
-
 
 class AudioData {
 public:
