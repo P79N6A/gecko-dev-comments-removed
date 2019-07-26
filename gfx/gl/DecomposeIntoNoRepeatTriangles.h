679fcfa855ca3d7088140b9d7dc48bb4a7abbaf9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 22:06:04 +0000

    Bug 975772 - Cleanup RectTriangles and allow DrawQuads to take triangles or triangle strips. r=bjacob
    UltraBlame original commit: ac4f9ca4fb27e57db7b27108ab6664130da9357f

--- gfx/gl/DecomposeIntoNoRepeatTriangles.h
+++ gfx/gl/DecomposeIntoNoRepeatTriangles.h
@@ -19,50 +19,45 @@ namespace gl {
 
 class RectTriangles {
 public:
-    RectTriangles() : mIsSimpleQuad(false) { }
+    typedef struct { GLfloat x,y; } coord;
 
     
     
     
     void addRect(GLfloat x0, GLfloat y0, GLfloat x1, GLfloat y1,
-                  GLfloat tx0, GLfloat ty0, GLfloat tx1, GLfloat ty1,
-                  bool flip_y = false);
+                 GLfloat tx0, GLfloat ty0, GLfloat tx1, GLfloat ty1,
+                 bool flip_y = false);
 
     
     
     
     
     
-    bool IsSimpleQuad(gfx3DMatrix& aOutTextureTransform) const {
-      aOutTextureTransform = mTextureTransform;
-      return mIsSimpleQuad;
+    bool isSimpleQuad(gfx3DMatrix& aOutTextureTransform) const;
+
+    
+
+
+
+
+    InfallibleTArray<coord>& vertCoords() {
+        return mVertexCoords;
     }
 
-    
-
-
-
-
-    float* vertexPointer() {
-        return &vertexCoords[0].x;
-    }
-
-    float* texCoordPointer() {
-        return &texCoords[0].u;
+    InfallibleTArray<coord>& texCoords() {
+        return mTexCoords;
     }
 
     unsigned int elements() {
-        return vertexCoords.Length();
+        return mVertexCoords.Length();
     }
-
-    typedef struct { GLfloat x,y; } vert_coord;
-    typedef struct { GLfloat u,v; } tex_coord;
 private:
     
-    nsAutoTArray<vert_coord, 6> vertexCoords;
-    nsAutoTArray<tex_coord, 6>  texCoords;
-    gfx3DMatrix mTextureTransform;
-    bool mIsSimpleQuad;
+    nsAutoTArray<coord, 6> mVertexCoords;
+    nsAutoTArray<coord, 6> mTexCoords;
+
+    static void
+    AppendRectToCoordArray(InfallibleTArray<coord>& array, GLfloat x0, GLfloat y0, GLfloat x1, GLfloat y1);
 };
 
 
