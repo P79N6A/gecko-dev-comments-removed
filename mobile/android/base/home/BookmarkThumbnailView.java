2ca154ef801c20b6772ae27c9e10ffb4ce806aa4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 14:23:46 +0000

    Bug 882365: Add a border to TopBookmarkItemView if its showing a thumbnail. [r=lucasr]
    UltraBlame original commit: e2a28f0025f10afb9e5adf1c249b8855f8868200

--- mobile/android/base/home/BookmarkThumbnailView.java
+++ mobile/android/base/home/BookmarkThumbnailView.java
@@ -9,6 +9,8 @@ import org.mozilla.gecko.R;
 import org.mozilla.gecko.ThumbnailHelper;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -26,8 +28,27 @@ public class BookmarkThumbnailView extends ImageView {
     
     private static final int DEFAULT_COLOR = 0x46ECF0F3;
 
+    
+    private boolean mShowBorder = true;
+
+    
+    private final float mStrokeWidth = getResources().getDisplayMetrics().density * 2;
+
+    
+    private static Paint sBorderPaint;
+
+    
+    static {
+        sBorderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        sBorderPaint.setColor(0xFFCFD9E1);
+        sBorderPaint.setStyle(Paint.Style.STROKE);
+    }
+
     public BookmarkThumbnailView(Context context) {
         this(context, null);
+
+        
+        setWillNotDraw(false);
     }
 
     public BookmarkThumbnailView(Context context, AttributeSet attrs) {
@@ -59,6 +80,19 @@ public class BookmarkThumbnailView extends ImageView {
     
 
 
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (mShowBorder) {
+            sBorderPaint.setStrokeWidth(mStrokeWidth);
+            canvas.drawRect(0, 0, getWidth(), getHeight(), sBorderPaint);
+        }
+    }
+
+    
+
+
 
 
     @Override
@@ -67,5 +101,6 @@ public class BookmarkThumbnailView extends ImageView {
         Drawable drawable = getResources().getDrawable(R.drawable.favicon_bg);
         drawable.setColorFilter(colorFilter, Mode.SRC_ATOP);
         setBackgroundDrawable(drawable);
+        mShowBorder = false;
     }
 }
