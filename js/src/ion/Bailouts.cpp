e677ee1ef50d9e6eacda63082d6e48c45a730921
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 08:56:12 +0000

    Remove Stack iteration with unsafe locations. (Bug 756615, r=dvander)
    UltraBlame original commit: 003e7b33fd8c7c5fb563933fca680454f02dd3ef

--- js/src/ion/Bailouts.cpp
+++ js/src/ion/Bailouts.cpp
@@ -223,7 +223,11 @@ ConvertFrames(JSContext *cx, IonActivation *activation, IonBailoutIterator &it)
     
     it.ionScript()->forbidOsr();
 
-    BailoutClosure *br = cx->new_<BailoutClosure>();
+    
+    
+    
+    
+    BailoutClosure *br = OffTheBooks::new_<BailoutClosure>();
     if (!br)
         return BAILOUT_RETURN_FATAL_ERROR;
     activation->setBailout(br);
@@ -342,6 +346,8 @@ uint32
 ion::Bailout(BailoutStack *sp)
 {
     JSContext *cx = GetIonContext()->cx;
+    
+    cx->runtime->ionTop = NULL;
     IonActivationIterator ionActivations(cx);
     IonBailoutIterator iter(ionActivations, sp);
     IonActivation *activation = ionActivations.activation();
@@ -369,6 +375,8 @@ ion::InvalidationBailout(InvalidationBailoutStack *sp, size_t *frameSizeOut)
     sp->checkInvariants();
 
     JSContext *cx = GetIonContext()->cx;
+    
+    cx->runtime->ionTop = NULL;
     IonActivationIterator ionActivations(cx);
     IonBailoutIterator iter(ionActivations, sp);
     IonActivation *activation = ionActivations.activation();
