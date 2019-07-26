1992eab4264a7964baf751fc77f91be73d026627
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:22:02 +0000

    Bug 844323 - Part 3: ProcessPriorityManager tests. r=nsm,khuey
    UltraBlame original commit: e8e47474ef41bb470dd4183918cccef5100b2224

--- dom/browser-element/mochitest/browserElementTestHelpers.js
+++ dom/browser-element/mochitest/browserElementTestHelpers.js
@@ -61,18 +61,42 @@ const browserElementTestHelpers = {
     this.tempPermissions.push(location.href)
   },
 
-  removeAllTempPermissions: function() {
-    for(var i = 0; i < this.tempPermissions.length; i++) {
-      SpecialPowers.removePermission("browser", this.tempPermissions[i]);
-    }
-  },
-
+  'tempPermissions': [],
   addPermissionForUrl: function(url) {
     SpecialPowers.addPermission("browser", true, url);
     this.tempPermissions.push(url);
   },
 
-  'tempPermissions': [],
+  _observers: [],
+
+  
+  
+  
+  
+  
+  addProcessPriorityObserver: function(processPriorityTopic, observerFn) {
+    var topic = "process-priority-manager:TEST-ONLY:" + processPriorityTopic;
+
+    
+    
+    var observer = {
+      observe: observerFn
+    };
+
+    SpecialPowers.addObserver(observer, topic,  false);
+    this._observers.push([observer, topic]);
+  },
+
+  cleanUp: function() {
+    for (var i = 0; i < this.tempPermissions.length; i++) {
+      SpecialPowers.removePermission("browser", this.tempPermissions[i]);
+    }
+
+    for (var i = 0; i < this._observers.length; i++) {
+      SpecialPowers.removeObserver(this._observers[i][0],
+                                   this._observers[i][1]);
+    }
+  },
 
   
   'emptyPage1': 'http://example.com' + _getPath() + '/file_empty.html',
@@ -80,6 +104,92 @@ const browserElementTestHelpers = {
   'emptyPage3': 'http://test1.example.org' + _getPath() + '/file_empty.html',
   'focusPage': 'http://example.org' + _getPath() + '/file_focus.html',
 };
+
+
+
+function expectProcessCreated() {
+  var deferred = Promise.defer();
+
+  var observed = false;
+  browserElementTestHelpers.addProcessPriorityObserver(
+    "process-created",
+    function(subject, topic, data) {
+      
+      
+      if (observed) {
+        return;
+      }
+      observed = true;
+
+      var childID = parseInt(data);
+      ok(true, 'Got new process, id=' + childID);
+      deferred.resolve(childID);
+    }
+  );
+
+  return deferred.promise;
+}
+
+
+
+function expectOnlyOneProcessCreated() {
+  var p = expectProcessCreated();
+  p.then(function() {
+    expectProcessCreated().then(function(childID) {
+      ok(false, 'Got unexpected process creation, childID=' + childID);
+    });
+  });
+  return p;
+}
+
+
+
+
+function expectPriorityChange(childID, expectedPriority) {
+  var deferred = Promise.defer();
+
+  var observed = false;
+  browserElementTestHelpers.addProcessPriorityObserver(
+    'process-priority-set',
+    function(subject, topic, data) {
+      if (observed) {
+        return;
+      }
+
+      [id, priority] = data.split(":");
+      if (id != childID) {
+        return;
+      }
+
+      
+      
+      observed = true;
+
+      is(priority, expectedPriority,
+         'Expected priority of childID ' + childID +
+         ' to change to ' + expectedPriority);
+
+      if (priority == expectedPriority) {
+        deferred.resolve(priority);
+      } else {
+        deferred.reject(priority);
+      }
+    }
+  );
+
+  return deferred.promise;
+}
+
+
+
+function expectMozbrowserEvent(iframe, eventName) {
+  var deferred = Promise.defer();
+  iframe.addEventListener('mozbrowser' + eventName, function handler(e) {
+    iframe.removeEventListener('mozbrowser' + eventName, handler);
+    deferred.resolve(e);
+  });
+  return deferred.promise;
+}
 
 
 
@@ -129,11 +239,284 @@ const browserElementTestHelpers = {
 })();
 
 addEventListener('unload', function() {
-  browserElementTestHelpers.removeAllTempPermissions();
+  browserElementTestHelpers.cleanUp();
 });
 
 
 browserElementTestHelpers.lockTestReady();
 addEventListener('load', function() {
   SimpleTest.executeSoon(browserElementTestHelpers.unlockTestReady.bind(browserElementTestHelpers));
+});
+
+
+
+
+
+
+;(function(id, factory) { 
+    var globals = this;
+    factory(function require(id) {
+      return globals[id];
+    }, (globals[id] = {}), { uri: document.location.href + '#' + id, id: id });
+}).call(this, 'Promise', function Promise(require, exports, module) {
+
+'use strict';
+
+module.metadata = {
+  "stability": "unstable"
+};
+
+
+
+
+
+
+function fulfilled(value) {
+  return { then: function then(fulfill) { fulfill(value); } };
+}
+
+
+
+
+
+
+function rejected(reason) {
+  return { then: function then(fulfill, reject) { reject(reason); } };
+}
+
+
+
+
+
+function isPromise(value) {
+  return value && typeof(value.then) === 'function';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function defer(prototype) {
+  
+  
+  var observers = [];
+
+  
+  
+  
+  
+  var result = null;
+
+  prototype = (prototype || prototype === null) ? prototype : Object.prototype;
+
+  
+  var promise = Object.create(prototype, {
+    then: { value: function then(onFulfill, onError) {
+      var deferred = defer(prototype);
+
+      function resolve(value) {
+        
+        
+        
+        try {
+          deferred.resolve(onFulfill ? onFulfill(value) : value);
+        }
+        
+        
+        catch(error) {
+          if (exports._reportErrors && typeof(console) === 'object')
+            console.error(error);
+          
+          
+          deferred.resolve(rejected(error));
+        }
+      }
+
+      function reject(reason) {
+        try {
+          if (onError) deferred.resolve(onError(reason));
+          else deferred.resolve(rejected(reason));
+        }
+        catch(error) {
+          if (exports._reportErrors && typeof(console) === 'object')
+            console.error(error)
+          deferred.resolve(rejected(error));
+        }
+      }
+
+      
+      
+      
+      
+      
+      if (observers) {
+        observers.push({ resolve: resolve, reject: reject });
+      }
+      
+      else {
+        result.then(resolve, reject);
+      }
+
+      return deferred.promise;
+    }}
+  })
+
+  var deferred = {
+    promise: promise,
+    
+
+
+
+
+
+
+    resolve: function resolve(value) {
+      if (!result) {
+        
+        
+        
+        
+        result = isPromise(value) ? value : fulfilled(value);
+
+        
+        
+        
+        
+        
+        
+        while (observers.length) {
+          var observer = observers.shift();
+          result.then(observer.resolve, observer.reject);
+        }
+
+        
+        
+        observers = null;
+      }
+    },
+    
+
+
+
+
+    reject: function reject(reason) {
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      deferred.resolve(rejected(reason));
+    }
+  };
+
+  return deferred;
+}
+exports.defer = defer;
+
+
+
+
+
+
+function resolve(value, prototype) {
+  var deferred = defer(prototype);
+  deferred.resolve(value);
+  return deferred.promise;
+}
+exports.resolve = resolve;
+
+
+
+
+
+
+function reject(reason, prototype) {
+  var deferred = defer(prototype);
+  deferred.reject(reason);
+  return deferred.promise;
+}
+exports.reject = reject;
+
+var promised = (function() {
+  
+  
+  
+
+  var call = Function.call;
+  var concat = Array.prototype.concat;
+
+  
+  
+  function execute(args) { return call.apply(call, args) }
+
+  
+  
+  function promisedConcat(promises, unknown) {
+    return promises.then(function(values) {
+      return resolve(unknown).then(function(value) {
+        return values.concat([ value ])
+      });
+    });
+  }
+
+  return function promised(f, prototype) {
+    
+
+
+
+
+
+
+
+
+
+
+
+    return function promised() {
+      
+      return concat.apply([ f, this ], arguments).
+        
+        reduce(promisedConcat, resolve([], prototype)).
+        
+        then(execute);
+    }
+  }
+})();
+exports.promised = promised;
+
+var all = promised(Array);
+exports.all = all;
+
 });
