b105dbac9d7b4123365a3d6d7fd65f030cf12633
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 18:01:39 +0000

    Bug 950919 - (Part 1) Get rid of loadHomePage method. r=mcomella
    UltraBlame original commit: 9c9c3e3e7bc25ba3beb7f0e55a7ea7ae57749900

--- mobile/android/base/GeckoApp.java
+++ mobile/android/base/GeckoApp.java
@@ -1411,21 +1411,13 @@ public abstract class GeckoApp
             if (!mShouldRestore) {
                 
                 
-                loadHomePage(Tabs.LOADURL_NEW_TAB);
+                Tabs.getInstance().loadUrl(AboutPages.HOME, Tabs.LOADURL_NEW_TAB);
             }
         } else {
             
             int flags = Tabs.LOADURL_NEW_TAB | Tabs.LOADURL_USER_ENTERED | Tabs.LOADURL_EXTERNAL;
             Tabs.getInstance().loadUrl(url, flags);
         }
-    }
-
-    protected Tab loadHomePage() {
-        return loadHomePage(Tabs.LOADURL_NONE);
-    }
-
-    protected Tab loadHomePage(int flags) {
-        return Tabs.getInstance().loadUrl(AboutPages.HOME, flags);
     }
 
     private void initialize() {
