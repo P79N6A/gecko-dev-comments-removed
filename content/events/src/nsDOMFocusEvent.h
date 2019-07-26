e75ee9a43856d52c04e4fd0c38f524c3b4e88f7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:44:15 +0000

    Bug 855741 - Update Focus webidl from Event to FocusEvent. r=smaug
    UltraBlame original commit: 81871f331faa85550417d7ac782e09233d268f8a

---
+++ content/events/src/nsDOMFocusEvent.h
@@ -0,0 +1,47 @@
+
+
+
+
+#ifndef nsDOMFocusEvent_h_
+#define nsDOMFocusEvent_h_
+
+#include "nsDOMUIEvent.h"
+#include "nsIDOMFocusEvent.h"
+#include "mozilla/dom/FocusEventBinding.h"
+
+class nsDOMFocusEvent : public nsDOMUIEvent,
+                        public nsIDOMFocusEvent
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIDOMFOCUSEVENT
+
+  
+  NS_FORWARD_TO_NSDOMUIEVENT
+
+  virtual JSObject* WrapObject(JSContext* aCx,
+                               JS::Handle<JSObject*> aScope) MOZ_OVERRIDE
+  {
+    return mozilla::dom::FocusEventBinding::Wrap(aCx, aScope, this);
+  }
+
+  nsDOMFocusEvent(mozilla::dom::EventTarget* aOwner,
+                  nsPresContext* aPresContext, nsFocusEvent* aEvent);
+
+  mozilla::dom::EventTarget* GetRelatedTarget();
+
+  static already_AddRefed<nsDOMFocusEvent> Constructor(const mozilla::dom::GlobalObject& aGlobal,
+                                                       const nsAString& aType,
+                                                       const mozilla::dom::FocusEventInit& aParam,
+                                                       mozilla::ErrorResult& aRv);
+protected:
+  nsresult InitFocusEvent(const nsAString& aType,
+                          bool aCanBubble,
+                          bool aCancelable,
+                          nsIDOMWindow* aView,
+                          int32_t aDetail,
+                          mozilla::dom::EventTarget* aRelatedTarget);
+  ~nsDOMFocusEvent();
+};
+
+#endif 
