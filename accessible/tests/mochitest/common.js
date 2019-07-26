dbee4fa2a909fb94152ffccdeb3d8f770c43bda7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 09:21:33 +0000

    backout 94e3a41d532e (increased orange)
    UltraBlame original commit: e5324de8c39841c2775c3e950200d0ca0854bde9

--- accessible/tests/mochitest/common.js
+++ accessible/tests/mochitest/common.js
@@ -653,23 +653,6 @@ function shortenString(aString, aMaxLength)
 
 
 
- function getTopChromeWindow(aWindow)
- {
-   return aWindow.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-                 .getInterface(Components.interfaces.nsIWebNavigation)
-                 .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
-                 .rootTreeItem
-                 .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-                 .getInterface(Components.interfaces.nsIDOMWindow);
-}
-
-
-
-
-
-
-
-
 
 function getNodePrettyName(aNode)
 {
