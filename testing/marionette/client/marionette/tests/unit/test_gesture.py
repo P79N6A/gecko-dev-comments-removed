aea31ddca8e5824d631f1211481d47baf592df7b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:22:45 +0000

    Bug 845925 - Add scroll/pinch actions using touch action chains to the python touch layer, r=mdas
    UltraBlame original commit: 1a16a93a396bb1561e0c6f4569222d5386065cfe

---
+++ testing/marionette/client/marionette/tests/unit/test_gesture.py
@@ -0,0 +1,23 @@
+
+
+
+
+import time
+from marionette_test import MarionetteTestCase
+from gestures import smooth_scroll, pinch
+
+class testGestures(MarionetteTestCase):
+    def test_smooth_scroll(self):
+        testTouch = self.marionette.absolute_url("testAction.html")
+        self.marionette.navigate(testTouch)
+        button = self.marionette.find_element("id", "mozLinkScrollStart")
+        smooth_scroll(self.marionette, button, "y",  -1, 250)
+        time.sleep(15)
+        self.assertEqual("End", self.marionette.execute_script("return document.getElementById('mozLinkScroll').innerHTML;"))
+
+    def test_pinch(self):
+        testTouch = self.marionette.absolute_url("testAction.html")
+        self.marionette.navigate(testTouch)
+        button = self.marionette.find_element("id", "mozLinkScrollStart")
+        pinch(self.marionette, button, 0, 0, 0, 0, 0, -50, 0, 50)
+        time.sleep(15)
