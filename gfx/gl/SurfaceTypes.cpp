02c61fcd9a22f2c860c03ca744df17af5edef20b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:51:09 +0000

    Bug 944703 - Part 1: make SurfaceCaps hold a strong reference to its ISurfaceAllocator - r=jgilbert
    UltraBlame original commit: 101f294ad43cef67b1d0319e317eb252da48d829

---
+++ gfx/gl/SurfaceTypes.cpp
@@ -0,0 +1,59 @@
+
+
+
+
+
+#include "SurfaceTypes.h"
+#include "mozilla/layers/ISurfaceAllocator.h"
+
+namespace mozilla {
+namespace gfx {
+
+SurfaceCaps::SurfaceCaps()
+{
+  Clear();
+}
+
+SurfaceCaps::SurfaceCaps(const SurfaceCaps& other)
+{
+  *this = other;
+}
+
+SurfaceCaps&
+SurfaceCaps::operator=(const SurfaceCaps& other)
+{
+  any = other.any;
+  color = other.color;
+  alpha = other.alpha;
+  bpp16 = other.bpp16;
+  depth = other.depth;
+  stencil = other.stencil;
+  antialias = other.antialias;
+  preserve = other.preserve;
+  surfaceAllocator = other.surfaceAllocator;
+
+  return *this;
+}
+
+void
+SurfaceCaps::Clear()
+{
+  any = false;
+  color = false;
+  alpha = false;
+  bpp16 = false;
+  depth = false;
+  stencil = false;
+  antialias = false;
+  preserve = false;
+  surfaceAllocator = nullptr;
+}
+
+
+
+SurfaceCaps::~SurfaceCaps()
+{
+}
+
+}
+}
