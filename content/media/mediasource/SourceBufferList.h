8094da1796dcf60d40c35f7a988643b4e137c38b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 13:57:09 +0000

    Bug 855130 - Implement a minimal working subset of the Media Source Extensions API.  r=roc
    ---
     content/media/mediasource/AsyncEventRunner.h       |  35 ++
     content/media/mediasource/Makefile.in              |  18 +
     content/media/mediasource/MediaSource.cpp          | 395 +++++++++++++++++++++
     content/media/mediasource/MediaSource.h            | 127 +++++++
     .../media/mediasource/MediaSourceInputAdapter.cpp  | 176 +++++++++
     .../media/mediasource/MediaSourceInputAdapter.h    |  43 +++
     content/media/mediasource/SourceBuffer.cpp         | 249 +++++++++++++
     content/media/mediasource/SourceBuffer.h           | 115 ++++++
     content/media/mediasource/SourceBufferList.cpp     | 143 ++++++++
     content/media/mediasource/SourceBufferList.h       |  79 +++++
     content/media/mediasource/moz.build                |  24 ++
     content/media/moz.build                            |   2 +
     dom/bindings/Bindings.conf                         |  13 +
     dom/dom-config.mk                                  |   1 +
     dom/webidl/MediaSource.webidl                      |  38 ++
     dom/webidl/SourceBuffer.webidl                     |  44 +++
     dom/webidl/SourceBufferList.webidl                 |  17 +
     dom/webidl/WebIDL.mk                               |   3 +
     layout/build/Makefile.in                           |   4 +
     modules/libpref/src/init/all.js                    |   3 +
     20 files changed, 1529 insertions(+)
     create mode 100644 content/media/mediasource/AsyncEventRunner.h
     create mode 100644 content/media/mediasource/Makefile.in
     create mode 100644 content/media/mediasource/MediaSource.cpp
     create mode 100644 content/media/mediasource/MediaSource.h
     create mode 100644 content/media/mediasource/MediaSourceInputAdapter.cpp
     create mode 100644 content/media/mediasource/MediaSourceInputAdapter.h
     create mode 100644 content/media/mediasource/SourceBuffer.cpp
     create mode 100644 content/media/mediasource/SourceBuffer.h
     create mode 100644 content/media/mediasource/SourceBufferList.cpp
     create mode 100644 content/media/mediasource/SourceBufferList.h
     create mode 100644 content/media/mediasource/moz.build
     create mode 100644 dom/webidl/MediaSource.webidl
     create mode 100644 dom/webidl/SourceBuffer.webidl
     create mode 100644 dom/webidl/SourceBufferList.webidl
    
    UltraBlame original commit: 1788eb4682b5cbd26e7e18908e0f5ac36a4982ad

---
+++ content/media/mediasource/SourceBufferList.h
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_SourceBufferList_h_
+#define mozilla_dom_SourceBufferList_h_
+
+#include "AsyncEventRunner.h"
+#include "MediaSource.h"
+#include "SourceBuffer.h"
+#include "mozilla/Attributes.h"
+#include "nsCOMPtr.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsDOMEventTargetHelper.h"
+#include "nsWrapperCache.h"
+#include "nscore.h"
+
+namespace mozilla {
+namespace dom {
+
+class MediaSource;
+
+class SourceBufferList MOZ_FINAL : public nsDOMEventTargetHelper
+{
+public:
+  
+  SourceBuffer* IndexedGetter(uint32_t aIndex, bool& aFound);
+
+  uint32_t Length();
+  
+
+  NS_DECL_ISUPPORTS_INHERITED
+  NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED(SourceBufferList, nsDOMEventTargetHelper)
+
+  explicit SourceBufferList(MediaSource* aMediaSource);
+
+  MediaSource* GetParentObject() const;
+
+  JSObject* WrapObject(JSContext* aCx, JS::Handle<JSObject*> aScope) MOZ_OVERRIDE;
+
+  
+  void Append(SourceBuffer* aSourceBuffer);
+
+  
+  void Remove(SourceBuffer* aSourceBuffer);
+
+  
+  bool Contains(SourceBuffer* aSourceBuffer);
+
+  
+  void Clear();
+
+  
+  bool IsEmpty();
+
+  
+  void DetachAndClear();
+
+  
+  bool AnyUpdating();
+
+  
+  
+  void Remove(double aStart, double aEnd, ErrorResult& aRv);
+
+private:
+  friend class AsyncEventRunnner<SourceBufferList>;
+  void DispatchSimpleEvent(const char* aName);
+  void QueueAsyncSimpleEvent(const char* aName);
+
+  nsRefPtr<MediaSource> mMediaSource;
+  nsTArray<nsRefPtr<SourceBuffer> > mSourceBuffers;
+};
+
+} 
+} 
+#endif 
