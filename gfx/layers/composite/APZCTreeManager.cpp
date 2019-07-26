73e26c6adee85076c55ecd4e1d000ab7f3bc25a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:31:30 +0000

    Bug 937688 - Make UpdateCompositionBounds affect all of the root APZCs for the layers id. r=botond
    UltraBlame original commit: 0f8d106c7bd5dde97a62bb7d983710151a0b27ee

--- gfx/layers/composite/APZCTreeManager.cpp
+++ gfx/layers/composite/APZCTreeManager.cpp
@@ -508,9 +508,13 @@ void
 APZCTreeManager::UpdateRootCompositionBounds(const uint64_t& aLayersId,
                                              const ScreenIntRect& aCompositionBounds)
 {
-  nsRefPtr<AsyncPanZoomController> apzc = GetRootAPZCFor(aLayersId);
-  if (apzc) {
-    apzc->UpdateCompositionBounds(aCompositionBounds);
+  
+  
+  
+  nsTArray< nsRefPtr<AsyncPanZoomController> > rootApzcs;
+  GetRootAPZCsFor(aLayersId, &rootApzcs);
+  for (size_t i = 0; i < rootApzcs.Length(); i++) {
+    rootApzcs[i]->UpdateCompositionBounds(aCompositionBounds);
   }
 }
 
@@ -651,19 +655,15 @@ APZCTreeManager::GetTargetAPZC(const ScreenPoint& aPoint)
   return target.forget();
 }
 
-already_AddRefed<AsyncPanZoomController>
-APZCTreeManager::GetRootAPZCFor(const uint64_t& aLayersId)
+void
+APZCTreeManager::GetRootAPZCsFor(const uint64_t& aLayersId,
+                                 nsTArray< nsRefPtr<AsyncPanZoomController> >* aOutRootApzcs)
 {
   MonitorAutoLock lock(mTreeLock);
-  nsRefPtr<AsyncPanZoomController> target;
   
   for (AsyncPanZoomController* apzc = mRootApzc; apzc; apzc = apzc->GetPrevSibling()) {
-    target = FindRootAPZC(apzc, aLayersId);
-    if (target) {
-      break;
-    }
+    FindRootAPZCs(apzc, aLayersId, aOutRootApzcs);
   }
-  return target.forget();
 }
 
 AsyncPanZoomController*
@@ -742,21 +742,23 @@ APZCTreeManager::GetAPZCAtPoint(AsyncPanZoomController* aApzc, const gfxPoint& a
   return nullptr;
 }
 
-AsyncPanZoomController*
-APZCTreeManager::FindRootAPZC(AsyncPanZoomController* aApzc, const uint64_t& aLayersId)
+void
+APZCTreeManager::FindRootAPZCs(AsyncPanZoomController* aApzc,
+                               const uint64_t& aLayersId,
+                               nsTArray< nsRefPtr<AsyncPanZoomController> >* aOutRootApzcs)
 {
   mTreeLock.AssertCurrentThreadOwns();
 
   if (aApzc->IsRootForLayersId(aLayersId)) {
-    return aApzc;
+    aOutRootApzcs->AppendElement(aApzc);
+    
+    
+    return;
   }
+
   for (AsyncPanZoomController* child = aApzc->GetLastChild(); child; child = child->GetPrevSibling()) {
-    AsyncPanZoomController* match = FindRootAPZC(child, aLayersId);
-    if (match) {
-      return match;
-    }
+    FindRootAPZCs(child, aLayersId, aOutRootApzcs);
   }
-  return nullptr;
 }
 
 
