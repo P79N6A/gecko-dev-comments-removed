7746b89f9ee4b8a62e12b3e7b1de6138642fdaa6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-26 17:58:20 +0000

    Bug 949360 - Check for Image AllocateBuffer() failures. r=nical
    UltraBlame original commit: 6d9cf51a012a74c503df861208f3188677a17673

--- gfx/layers/ipc/SharedPlanarYCbCrImage.cpp
+++ gfx/layers/ipc/SharedPlanarYCbCrImage.cpp
@@ -126,14 +126,16 @@ SharedPlanarYCbCrImage::AllocateAndGetNewBuffer(uint32_t aSize)
 {
   NS_ABORT_IF_FALSE(!mTextureClient->IsAllocated(), "This image already has allocated data");
   size_t size = YCbCrImageDataSerializer::ComputeMinBufferSize(aSize);
+
+  
+  if (!mTextureClient->Allocate(size)) {
+    return nullptr;
+  }
+
   
   mBufferSize = size;
 
-  
-  bool status = mTextureClient->Allocate(mBufferSize);
-  MOZ_ASSERT(status);
   YCbCrImageDataSerializer serializer(mTextureClient->GetBuffer());
-
   return serializer.GetData();
 }
 
@@ -248,16 +250,18 @@ DeprecatedSharedPlanarYCbCrImage::AllocateAndGetNewBuffer(uint32_t aSize)
 {
   NS_ABORT_IF_FALSE(!mAllocated, "This image already has allocated data");
   size_t size = YCbCrImageDataSerializer::ComputeMinBufferSize(aSize);
+
+  
+  if (!AllocateBuffer(size)) {
+    return nullptr;
+  }
+
   
   mBufferSize = size;
 
-  
-  AllocateBuffer(mBufferSize);
   YCbCrImageDataSerializer serializer(mShmem.get<uint8_t>());
-
   return serializer.GetData();
 }
-
 
 void
 DeprecatedSharedPlanarYCbCrImage::SetDataNoCopy(const Data &aData)
