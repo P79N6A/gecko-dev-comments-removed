9578b724fcbad625831fb6e117410fa2c2d3f084
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:57:30 +0000

    Bug 1125202 - SpiderMonkey: Move AssemberBuffer spew code out of line r=h4writer
    UltraBlame original commit: 4a117d2d2ff4f6632a4e5a6ed2eac507f9047749

---
+++ js/src/jit/shared/AssemblerBuffer-x86-shared.cpp
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+#include "jit/shared/AssemblerBuffer-x86-shared.h"
+
+#include "jsopcode.h"
+
+void js::jit::GenericAssembler::spew(const char *fmt, va_list va)
+{
+    
+    
+    char buf[200];
+
+    int i = vsnprintf(buf, sizeof(buf), fmt, va);
+
+    if (i > -1) {
+        if (printer)
+            printer->printf("%s\n", buf);
+        js::jit::JitSpew(js::jit::JitSpew_Codegen, "%s", buf);
+    }
+}
