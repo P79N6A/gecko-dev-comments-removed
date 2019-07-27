a833057d7ee30eddd852d15a4d0b40cfc6809a49
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:22:02 +0000

    Bug 1161020: Implement new socket-connector interface for NFC, r=allstars.chh
    This patch moves |NfcConnector| into its own file and implements
    the new socket-connector interface.
    
    UltraBlame original commit: 13753f9043f72975ad483ae1564dc955c19f95e7

---
+++ ipc/nfc/NfcConnector.cpp
@@ -0,0 +1,265 @@
+
+
+
+
+
+
+
+#include "NfcConnector.h"
+#include <fcntl.h>
+#include <sys/un.h>
+#include "nsISupportsImpl.h" 
+#include "nsThreadUtils.h" 
+
+namespace mozilla {
+namespace ipc {
+
+NfcConnector::NfcConnector(const nsACString& aAddressString)
+  : mAddressString(aAddressString)
+{
+  MOZ_COUNT_CTOR_INHERITED(NfcConnector, UnixSocketConnector);
+}
+
+NfcConnector::~NfcConnector()
+{
+  MOZ_COUNT_DTOR_INHERITED(NfcConnector, UnixSocketConnector);
+}
+
+nsresult
+NfcConnector::CreateSocket(int& aFd) const
+{
+  aFd = socket(AF_LOCAL, SOCK_STREAM, 0);
+  if (aFd < 0) {
+    NS_WARNING("Could not open NFC socket!");
+    return NS_ERROR_FAILURE;
+  }
+
+  return NS_OK;
+}
+
+nsresult
+NfcConnector::SetSocketFlags(int aFd) const
+{
+  static const int sReuseAddress = 1;
+
+  
+  int flags = TEMP_FAILURE_RETRY(fcntl(aFd, F_GETFD));
+  if (flags < 0) {
+    return NS_ERROR_FAILURE;
+  }
+  flags |= FD_CLOEXEC;
+  int res = TEMP_FAILURE_RETRY(fcntl(aFd, F_SETFD, flags));
+  if (res < 0) {
+    return NS_ERROR_FAILURE;
+  }
+
+  
+  flags = TEMP_FAILURE_RETRY(fcntl(aFd, F_GETFL));
+  if (flags < 0) {
+    return NS_ERROR_FAILURE;
+  }
+  flags |= O_NONBLOCK;
+  res = TEMP_FAILURE_RETRY(fcntl(aFd, F_SETFL, flags));
+  if (res < 0) {
+    return NS_ERROR_FAILURE;
+  }
+
+  
+  res = setsockopt(aFd, SOL_SOCKET, SO_REUSEADDR, &sReuseAddress,
+                   sizeof(sReuseAddress));
+  if (res < 0) {
+    return NS_ERROR_FAILURE;
+  }
+
+  return NS_OK;
+}
+
+nsresult
+NfcConnector::CreateAddress(struct sockaddr& aAddress,
+                            socklen_t& aAddressLength) const
+{
+  static const size_t sNameOffset = 1;
+
+  struct sockaddr_un* address =
+    reinterpret_cast<struct sockaddr_un*>(&aAddress);
+
+  size_t namesiz = mAddressString.Length() + 1; 
+
+  if (NS_WARN_IF((sNameOffset + namesiz) > sizeof(address->sun_path))) {
+    return NS_ERROR_FAILURE;
+  }
+
+  address->sun_family = AF_UNIX;
+  memset(address->sun_path, '\0', sNameOffset); 
+  memcpy(address->sun_path + sNameOffset, mAddressString.get(), namesiz);
+
+  aAddressLength =
+    offsetof(struct sockaddr_un, sun_path) + sNameOffset + namesiz;
+
+  return NS_OK;
+}
+
+
+
+
+nsresult
+NfcConnector::ConvertAddressToString(const struct sockaddr& aAddress,
+                                     socklen_t aAddressLength,
+                                     nsACString& aAddressString)
+{
+  MOZ_ASSERT(aAddress.sa_family == AF_UNIX);
+
+  const struct sockaddr_un* un =
+    reinterpret_cast<const struct sockaddr_un*>(&aAddress);
+
+  size_t len = aAddressLength - offsetof(struct sockaddr_un, sun_path);
+
+  aAddressString.Assign(un->sun_path, len);
+
+  return NS_OK;
+}
+
+nsresult
+NfcConnector::CreateListenSocket(struct sockaddr* aAddress,
+                                 socklen_t* aAddressLength,
+                                 int& aListenFd)
+{
+  ScopedClose fd;
+
+  nsresult rv = CreateSocket(fd.rwget());
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+  rv = SetSocketFlags(fd);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+  if (aAddress && aAddressLength) {
+    rv = CreateAddress(*aAddress, *aAddressLength);
+    if (NS_FAILED(rv)) {
+      return rv;
+    }
+  }
+
+  aListenFd = fd.forget();
+
+  return NS_OK;
+}
+
+nsresult
+NfcConnector::AcceptStreamSocket(int aListenFd,
+                                 struct sockaddr* aAddress,
+                                 socklen_t* aAddressLength,
+                                 int& aStreamFd)
+{
+  ScopedClose fd(
+    TEMP_FAILURE_RETRY(accept(aListenFd, aAddress, aAddressLength)));
+  if (fd < 0) {
+    NS_WARNING("Cannot accept file descriptor!");
+    return NS_ERROR_FAILURE;
+  }
+  nsresult rv = SetSocketFlags(fd);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+
+  aStreamFd = fd.forget();
+
+  return NS_OK;
+}
+
+nsresult
+NfcConnector::CreateStreamSocket(struct sockaddr* aAddress,
+                                 socklen_t* aAddressLength,
+                                 int& aStreamFd)
+{
+  ScopedClose fd;
+
+  nsresult rv = CreateSocket(fd.rwget());
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+  rv = SetSocketFlags(fd);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+  if (aAddress && aAddressLength) {
+    rv = CreateAddress(*aAddress, *aAddressLength);
+    if (NS_FAILED(rv)) {
+      return rv;
+    }
+  }
+
+  aStreamFd = fd.forget();
+
+  return NS_OK;
+}
+
+
+
+int
+NfcConnector::Create()
+{
+  MOZ_ASSERT(!NS_IsMainThread());
+
+  int fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+  if (fd < 0) {
+    NS_WARNING("Could not open nfc socket!");
+    return -1;
+  }
+
+  if (!SetUp(fd)) {
+    NS_WARNING("Could not set up socket!");
+  }
+  return fd;
+}
+
+bool
+NfcConnector::CreateAddr(bool aIsServer,
+                         socklen_t& aAddrSize,
+                         sockaddr_any& aAddr,
+                         const char* aAddress)
+{
+  static const size_t sNameOffset = 1;
+
+  nsDependentCString socketName("nfcd");
+
+  size_t namesiz = socketName.Length() + 1; 
+
+  if ((sNameOffset + namesiz) > sizeof(aAddr.un.sun_path)) {
+    NS_WARNING("Address too long for socket struct!");
+    return false;
+  }
+
+  memset(aAddr.un.sun_path, '\0', sNameOffset); 
+  memcpy(aAddr.un.sun_path + sNameOffset, socketName.get(), namesiz);
+  aAddr.un.sun_family = AF_UNIX;
+
+  aAddrSize = offsetof(struct sockaddr_un, sun_path) + sNameOffset + namesiz;
+
+  return true;
+}
+
+bool
+NfcConnector::SetUp(int aFd)
+{
+  
+  return true;
+}
+
+bool
+NfcConnector::SetUpListenSocket(int aFd)
+{
+  
+  return true;
+}
+
+void
+NfcConnector::GetSocketAddr(const sockaddr_any& aAddr,
+                            nsAString& aAddrStr)
+{
+  MOZ_CRASH("This should never be called!");
+}
+
+}
+}
