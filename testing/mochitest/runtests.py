be2818da5fe0bdb311e7a2fa29f422954fe5b7b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:10:46 +0000

    Backed out changeset 188d22215166 (bug 1057512) for causing dt2 memory leaks
    UltraBlame original commit: 7b56755a63ba48c3316536d8eaa1508d02da67cb

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -1712,8 +1712,7 @@ class Mochitest(MochitestUtilsMixin):
     self.setTestRoot(options)
 
     
-    
-    if options.browserChrome:
+    if options.browserChrome and not options.subsuite:
       options.runByDir = True
 
     if not options.runByDir:
