ae8a1b5c84dc1e13f05fb10aee4997c44b68efac
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:37:40 +0000

    Bug 1114962 - Update the in-tree copies of acorn and pretty-fast. r=past
    acorn is now at db59bd0296c16796a07e82d2dc284861f464603e (untagged commit after
    0.11.0 release).
    
    pretty-fast is now at abc8c1133cc5443fb3bb50c426731821d27300a6 (tagged 0.2.0).
    
    UltraBlame original commit: 083244e156748d4864ce572f21ceadf6f69028a9

--- toolkit/devtools/pretty-fast/pretty-fast.js
+++ toolkit/devtools/pretty-fast/pretty-fast.js
@@ -5,9 +5,11 @@
 
 
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
+  "use strict";
+
+  if (typeof define === "function" && define.amd) {
     define(factory);
-  } else if (typeof exports === 'object') {
+  } else if (typeof exports === "object") {
     module.exports = factory();
   } else {
     root.prettyFast = factory();
@@ -92,7 +94,9 @@
     if (lastToken.type.isAssign) {
       return true;
     }
-    return !!PRE_ARRAY_LITERAL_TOKENS[lastToken.type.keyword || lastToken.type.type];
+    return !!PRE_ARRAY_LITERAL_TOKENS[
+      lastToken.type.keyword || lastToken.type.type
+    ];
   }
 
   
@@ -209,7 +213,9 @@
     if (token.startLoc.line === lastToken.startLoc.line) {
       return false;
     }
-    if (PREVENT_ASI_AFTER_TOKENS[lastToken.type.type || lastToken.type.keyword]) {
+    if (PREVENT_ASI_AFTER_TOKENS[
+      lastToken.type.type || lastToken.type.keyword
+    ]) {
       return false;
     }
     if (PREVENT_ASI_BEFORE_TOKENS[token.type.type || token.type.keyword]) {
@@ -520,11 +526,11 @@
       + ")";
     var escapeCharactersRegExp = new RegExp(regExpString, "g");
 
-    return function(str) {
+    return function (str) {
       return str.replace(escapeCharactersRegExp, function (_, c) {
         return escapeCharacters[c];
       });
-    }
+    };
   }());
   
 
@@ -534,11 +540,13 @@
 
 
 
-
-
-  function addToken(token, write, options) {
+  function addToken(token, write) {
     if (token.type.type == "string") {
       write("'" + sanitize(token.value) + "'",
+            token.startLoc.line,
+            token.startLoc.column);
+    } else if (token.type.type == "regexp") {
+      write(String(token.value.value),
             token.startLoc.line,
             token.startLoc.column);
     } else {
@@ -584,7 +592,7 @@
 
   function decrementsIndent(tokenType, stack) {
     return tokenType == "}"
-      || (tokenType == "]" && stack[stack.length - 1] == "[\n")
+      || (tokenType == "]" && stack[stack.length - 1] == "[\n");
   }
 
   
@@ -690,12 +698,13 @@
           for (var i = 0, len = buffer.length; i < len; i++) {
             lineStr += buffer[i];
           }
-          result.add(new SourceNode(bufferLine, bufferColumn, options.url, lineStr));
+          result.add(new SourceNode(bufferLine, bufferColumn, options.url,
+                                    lineStr));
           buffer.splice(0, buffer.length);
           bufferLine = -1;
           bufferColumn = -1;
         }
-      }
+      };
     }());
 
     
@@ -773,7 +782,7 @@
       }
     });
 
-    while (true) {
+    for (;;) {
       token = getToken();
 
       ttk = token.type.keyword;
@@ -807,8 +816,8 @@
 
       prependWhiteSpace(token, lastToken, addedNewline, write, options,
                         indentLevel, stack);
-      addToken(token, write, options);
-      if (commentQueue.length == 0 || !commentQueue[0].trailing) {
+      addToken(token, write);
+      if (commentQueue.length === 0 || !commentQueue[0].trailing) {
         addedNewline = appendNewline(token, write, stack);
       }
 
