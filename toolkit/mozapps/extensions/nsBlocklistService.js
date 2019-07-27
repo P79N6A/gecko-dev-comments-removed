adb009ffc0aaaa23a5b30d1b4908d4e80876a351
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:28:14 +0000

    backout changeset 339049fcea42 (bug 1024809) for android and b2g bustage on a CLOSED TREE
    UltraBlame original commit: 5a082a183de4bc87e28ec3e29557162ed20fb632

--- toolkit/mozapps/extensions/nsBlocklistService.js
+++ toolkit/mozapps/extensions/nsBlocklistService.js
@@ -74,10 +74,6 @@ XPCOMUtils.defineLazyServiceGetter(this, "gVersionChecker",
                                    "@mozilla.org/xpcom/version-comparator;1",
                                    "nsIVersionComparator");
 
-XPCOMUtils.defineLazyServiceGetter(this, "gCertBlocklistService",
-                                   "@mozilla.org/security/certblocklist;1",
-                                   "nsICertBlocklist");
-
 XPCOMUtils.defineLazyGetter(this, "gPref", function bls_gPref() {
   return Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).
          QueryInterface(Ci.nsIPrefBranch);
@@ -732,13 +728,6 @@ Blocklist.prototype = {
 
 
 
-
-
-
-
-
-
-
   _loadBlocklistFromFile: function Blocklist_loadBlocklistFromFile(file) {
     if (!gBlocklistEnabled) {
       LOG("Blocklist::_loadBlocklistFromFile: blocklist is disabled");
@@ -873,17 +862,12 @@ Blocklist.prototype = {
           this._pluginEntries = this._processItemNodes(element.childNodes, "plugin",
                                                        this._handlePluginItemNode);
           break;
-        case "certItems":
-          this._processItemNodes(element.childNodes, "cert",
-                                 this._handleCertItemNode.bind(this));
-          break;
         default:
           Services.obs.notifyObservers(element,
                                        "blocklist-data-" + element.localName,
                                        null);
         }
       }
-      gCertBlocklistService.saveEntries();
     }
     catch (e) {
       LOG("Blocklist::_loadBlocklistFromFile: Error constructing blocklist " + e);
@@ -903,20 +887,6 @@ Blocklist.prototype = {
       handler(blocklistElement, result);
     }
     return result;
-  },
-
-  _handleCertItemNode: function Blocklist_handleCertItemNode(blocklistElement,
-                                                             result) {
-    let issuer = blocklistElement.getAttribute("issuerName");
-    for (let snElement of blocklistElement.children) {
-      try {
-        gCertBlocklistService.addRevokedCert(issuer, snElement.textContent);
-      } catch (e) {
-        
-        
-        LOG("Blocklist::_handleCertItemNode: Error adding revoked cert " + e);
-      }
-    }
   },
 
   _handleEmItemNode: function Blocklist_handleEmItemNode(blocklistElement, result) {
