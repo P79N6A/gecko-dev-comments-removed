ff250906ad10fb5cc775e332c06e3624676739b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:30:40 +0000

    Bug 1144607 part 2 - Support string value for list-style-tyle. r=dbaron
    UltraBlame original commit: 0a3c37c82aa08274d413a74c3ef1fef8755ed28c

--- layout/style/nsRuleNode.cpp
+++ layout/style/nsRuleNode.cpp
@@ -7191,6 +7191,13 @@ nsRuleNode::ComputeListData(void* aStartStruct,
       list->SetListStyleType(typeIdent, mPresContext);
       break;
     }
+    case eCSSUnit_String: {
+      nsString str;
+      typeValue->GetStringValue(str);
+      list->SetListStyleType(NS_LITERAL_STRING(""),
+                             new AnonymousCounterStyle(str));
+      break;
+    }
     case eCSSUnit_Enumerated: {
       
       
