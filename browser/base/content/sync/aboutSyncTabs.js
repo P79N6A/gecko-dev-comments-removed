871aff8250189afffaf9e7e4e03d24d6dae1ea26
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:04:05 +0000

    Backed out changeset 2df073cb512c (bug 993584) for OSX browser_946320_tabs_from_other_computers.js failures.
    UltraBlame original commit: afb1867dba70b7274907529dfc6085dc6f26b940

--- browser/base/content/sync/aboutSyncTabs.js
+++ browser/base/content/sync/aboutSyncTabs.js
@@ -4,18 +4,10 @@
 
 const Cu = Components.utils;
 
-Cu.import("resource://services-common/utils.js");
 Cu.import("resource://services-sync/main.js");
 Cu.import("resource:///modules/PlacesUIUtils.jsm");
 Cu.import("resource://gre/modules/PlacesUtils.jsm", this);
 Cu.import("resource://gre/modules/Services.jsm");
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-
-XPCOMUtils.defineLazyModuleGetter(this, "Promise",
-                                  "resource://gre/modules/Promise.jsm");
-
-XPCOMUtils.defineLazyModuleGetter(this, "CloudSync",
-                                  "resource://gre/modules/CloudSync.jsm");
 
 let RemoteTabViewer = {
   _tabsList: null,
@@ -23,8 +15,6 @@ let RemoteTabViewer = {
   init: function () {
     Services.obs.addObserver(this, "weave:service:login:finish", false);
     Services.obs.addObserver(this, "weave:engine:sync:finish", false);
-
-    Services.obs.addObserver(this, "cloudsync:tabs:update", false);
 
     this._tabsList = document.getElementById("tabsList");
 
@@ -34,62 +24,63 @@ let RemoteTabViewer = {
   uninit: function () {
     Services.obs.removeObserver(this, "weave:service:login:finish");
     Services.obs.removeObserver(this, "weave:engine:sync:finish");
-
-    Services.obs.removeObserver(this, "cloudsync:tabs:update");
   },
 
-  createItem: function (attrs) {
+  buildList: function(force) {
+    if (!Weave.Service.isLoggedIn || !this._refetchTabs(force))
+      return;
+    
+    
+
+    this._generateTabList();
+  },
+
+  createItem: function(attrs) {
     let item = document.createElement("richlistitem");
 
     
-    for (let attr in attrs) {
+    for (let attr in attrs)
       item.setAttribute(attr, attrs[attr]);
-    }
 
-    if (attrs["type"] == "tab") {
+    if (attrs["type"] == "tab")
       item.label = attrs.title != "" ? attrs.title : attrs.url;
-    }
 
     return item;
   },
 
-  filterTabs: function (event) {
+  filterTabs: function(event) {
     let val = event.target.value.toLowerCase();
     let numTabs = this._tabsList.getRowCount();
     let clientTabs = 0;
     let currentClient = null;
-
-    for (let i = 0; i < numTabs; i++) {
+    for (let i = 0;i < numTabs;i++) {
       let item = this._tabsList.getItemAtIndex(i);
       let hide = false;
       if (item.getAttribute("type") == "tab") {
-        if (!item.getAttribute("url").toLowerCase().contains(val) &&
-            !item.getAttribute("title").toLowerCase().contains(val)) {
+        if (!item.getAttribute("url").toLowerCase().contains(val) && 
+            !item.getAttribute("title").toLowerCase().contains(val))
           hide = true;
-        } else {
+        else
           clientTabs++;
-        }
       }
       else if (item.getAttribute("type") == "client") {
         if (currentClient) {
-          if (clientTabs == 0) {
+          if (clientTabs == 0)
             currentClient.hidden = true;
-          }
         }
         currentClient = item;
         clientTabs = 0;
       }
       item.hidden = hide;
     }
-    if (clientTabs == 0) {
+    if (clientTabs == 0)
       currentClient.hidden = true;
-    }
   },
 
-  openSelected: function () {
+  openSelected: function() {
     let items = this._tabsList.selectedItems;
     let urls = [];
-    for (let i = 0; i < items.length; i++) {
+    for (let i = 0;i < items.length;i++) {
       if (items[i].getAttribute("type") == "tab") {
         urls.push(items[i].getAttribute("url"));
         let index = this._tabsList.getIndexOfItem(items[i]);
@@ -102,7 +93,7 @@ let RemoteTabViewer = {
     }
   },
 
-  bookmarkSingleTab: function () {
+  bookmarkSingleTab: function() {
     let item = this._tabsList.selectedItems[0];
     let uri = Weave.Utils.makeURI(item.getAttribute("url"));
     let title = item.getAttribute("title");
@@ -117,15 +108,14 @@ let RemoteTabViewer = {
                                      }, window.top);
   },
 
-  bookmarkSelectedTabs: function () {
+  bookmarkSelectedTabs: function() {
     let items = this._tabsList.selectedItems;
     let URIs = [];
-    for (let i = 0; i < items.length; i++) {
+    for (let i = 0;i < items.length;i++) {
       if (items[i].getAttribute("type") == "tab") {
         let uri = Weave.Utils.makeURI(items[i].getAttribute("url"));
-        if (!uri) {
+        if (!uri)
           continue;
-        }
 
         URIs.push(uri);
       }
@@ -143,7 +133,7 @@ let RemoteTabViewer = {
     try {
       let iconURI = Weave.Utils.makeURI(iconUri);
       return PlacesUtils.favicons.getFaviconLinkForIcon(iconURI).spec;
-    } catch (ex) {
+    } catch(ex) {
       
     }
 
@@ -151,58 +141,16 @@ let RemoteTabViewer = {
     return defaultIcon || PlacesUtils.favicons.defaultFavicon.spec;
   },
 
-  _waitingForBuildList: false,
-
-  _buildListRequested: false,
-
-  buildList: function (force) {
-    if (this._waitingForBuildList) {
-      this._buildListRequested = true;
-      return;
-    }
-
-    this._waitingForBuildList = true;
-    this._buildListRequested = false;
-
-    this._clearTabList();
-
-    if (Weave.Service.isLoggedIn && this._refetchTabs(force)) {
-      this._generateWeaveTabList();
-    } else {
-      
-      
-    }
-
-    function complete() {
-      this._waitingForBuildList = false;
-      if (this._buildListRequested) {
-        CommonUtils.nextTick(this.buildList, this);
-      }
-    }
-
-    if (CloudSync.ready && CloudSync().tabsReady && CloudSync().tabs.hasRemoteTabs()) {
-      this._generateCloudSyncTabList()
-          .then(complete, complete);
-    } else {
-      complete();
-    }
-  },
-
-  _clearTabList: function () {
+  _generateTabList: function() {
+    let engine = Weave.Service.engineManager.get("tabs");
     let list = this._tabsList;
 
     
     let count = list.getRowCount();
     if (count > 0) {
-      for (let i = count - 1; i >= 0; i--) {
+      for (let i = count - 1; i >= 0; i--)
         list.removeItemAt(i);
-      }
     }
-  },
-
-  _generateWeaveTabList: function () {
-    let engine = Weave.Service.engineManager.get("tabs");
-    let list = this._tabsList;
 
     let seenURLs = new Set();
     let localURLs = engine.getOpenURLs();
@@ -241,37 +189,7 @@ let RemoteTabViewer = {
     }
   },
 
-  _generateCloudSyncTabList: function () {
-    let updateTabList = function (remoteTabs) {
-      let list = this._tabsList;
-
-      for each (let client in remoteTabs) {
-        let clientAttrs = {
-          type: "client",
-          clientName: client.name,
-        };
-
-        let clientEnt = this.createItem(clientAttrs);
-        list.appendChild(clientEnt);
-
-        for (let tab of client.tabs) {
-          let tabAttrs = {
-            type: "tab",
-            title: tab.title,
-            url: tab.url,
-            icon: this.getIcon(tab.icon),
-          };
-          let tabEnt = this.createItem(tabAttrs);
-          list.appendChild(tabEnt);
-        }
-      }
-    }.bind(this);
-
-    return CloudSync().tabs.getRemoteTabs()
-                           .then(updateTabList, Promise.reject);
-  },
-
-  adjustContextMenu: function (event) {
+  adjustContextMenu: function(event) {
     let mode = "all";
     switch (this._tabsList.selectedItems.length) {
       case 0:
@@ -283,40 +201,33 @@ let RemoteTabViewer = {
         mode = "multiple";
         break;
     }
-
     let menu = document.getElementById("tabListContext");
     let el = menu.firstChild;
     while (el) {
       let showFor = el.getAttribute("showFor");
-      if (showFor) {
+      if (showFor)
         el.hidden = showFor != mode && showFor != "all";
-      }
 
       el = el.nextSibling;
     }
   },
 
-  _refetchTabs: function (force) {
+  _refetchTabs: function(force) {
     if (!force) {
       
       let lastFetch = 0;
       try {
         lastFetch = Services.prefs.getIntPref("services.sync.lastTabFetch");
       }
-      catch (e) {
-        
-      }
-
+      catch (e) {  }
       let now = Math.floor(Date.now() / 1000);
-      if (now - lastFetch < 30) {
+      if (now - lastFetch < 30)
         return false;
-      }
     }
 
     
-    if (Weave.Service.clientsEngine.lastSync == 0) {
+    if (Weave.Service.clientsEngine.lastSync == 0)
       Weave.Service.clientsEngine.sync();
-    }
 
     
     let engine = Weave.Service.engineManager.get("tabs");
@@ -328,26 +239,21 @@ let RemoteTabViewer = {
     return true;
   },
 
-  observe: function (subject, topic, data) {
+  observe: function(subject, topic, data) {
     switch (topic) {
       case "weave:service:login:finish":
         this.buildList(true);
         break;
       case "weave:engine:sync:finish":
-        if (subject == "tabs") {
-          this.buildList(false);
-        }
-        break;
-      case "cloudsync:tabs:update":
-        this.buildList(false);
+        if (subject == "tabs")
+          this._generateTabList();
         break;
     }
   },
 
-  handleClick: function (event) {
-    if (event.target.getAttribute("type") != "tab") {
+  handleClick: function(event) {
+    if (event.target.getAttribute("type") != "tab")
       return;
-    }
 
     if (event.button == 1) {
       let url = event.target.getAttribute("url");
