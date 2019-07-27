60576085c2075af52f375afb161854c9e5bb31a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:19:51 +0000

    Backed out changeset 97707eff3199 (bug 1059308)
    UltraBlame original commit: 7848485e515f6ecdac5bf20fbbf85687b6955512

--- toolkit/devtools/server/actors/webbrowser.js
+++ toolkit/devtools/server/actors/webbrowser.js
@@ -1019,22 +1019,8 @@ TabActor.prototype = {
     if (webProgress.DOMWindow == this._originalWindow) {
       
       
-      let rootDocShells = this.docShells
-                              .filter(d => {
-                                return d != this.docShell &&
-                                       this._isRootDocShell(d);
-                              });
-      if (rootDocShells.length > 0) {
-        let newRoot = rootDocShells[0];
-        this._originalWindow = newRoot.DOMWindow;
-        this._changeTopLevelDocument(this._originalWindow);
-      } else {
-        
-        
-        
-        
-        this.exit();
-      }
+      
+      this.exit();
       return;
     }
 
