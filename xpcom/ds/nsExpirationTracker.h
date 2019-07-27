53c7199ebb41d522de735391064769fdb899c7d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:33:50 +0000

    Bug 1048239 - Fix more bad implicit constructors in XPCOM; r=froydnj
    UltraBlame original commit: b81d755b1b376ea2b23b38fd0de5ac1d92c31239

--- xpcom/ds/nsExpirationTracker.h
+++ xpcom/ds/nsExpirationTracker.h
@@ -83,7 +83,7 @@ public:
 
 
 
-  nsExpirationTracker(uint32_t aTimerPeriod)
+  explicit nsExpirationTracker(uint32_t aTimerPeriod)
     : mTimerPeriod(aTimerPeriod)
     , mNewestGeneration(0)
     , mInAgeOneGeneration(false)
@@ -238,7 +238,7 @@ public:
     uint32_t mGeneration;
     uint32_t mIndex;
   public:
-    Iterator(nsExpirationTracker<T, K>* aTracker)
+    explicit Iterator(nsExpirationTracker<T, K>* aTracker)
       : mTracker(aTracker)
       , mGeneration(0)
       , mIndex(0)
