4ac4cd320e9d4781bf4c0559564cefcbfa4a5e9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:24:21 +0000

    Backed out changeset 97382f93c043 (bug 1169096)
    UltraBlame original commit: 747d201febba4c32311e8e22bdcc6da077726146

--- toolkit/devtools/server/actors/object.js
+++ toolkit/devtools/server/actors/object.js
@@ -651,8 +651,6 @@ ObjectActor.prototype.requestTypes = {
 
 
 
-
-
 function PropertyIteratorActor(objectActor, options){
   this.objectActor = objectActor;
 
@@ -710,20 +708,7 @@ function PropertyIteratorActor(objectActor, options){
     let { query } = options;
     query = query.toLowerCase();
     names = names.filter(name => {
-      
-      if (name.toLowerCase().includes(query)) {
-        return true;
-      }
-      
-      let desc;
-      try {
-        desc = this.obj.getOwnPropertyDescriptor(name);
-      } catch(e) {}
-      if (desc && desc.value &&
-          String(desc.value).includes(query)) {
-        return true;
-      }
-      return false;
+      return name.toLowerCase().includes(query);
     });
   }
 
