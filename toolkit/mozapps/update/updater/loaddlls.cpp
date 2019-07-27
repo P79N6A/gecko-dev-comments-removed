179b4de64b5be6633b31c59e0f704edbc4810d32
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:56:24 +0000

    bug 945192 - add x64 support to loaddlls.cpp and general cleanup. r=bbondy
    UltraBlame original commit: 8f65c2be0e01735b074018af1a1c12dd337f09b3

--- toolkit/mozapps/update/updater/loaddlls.cpp
+++ toolkit/mozapps/update/updater/loaddlls.cpp
@@ -16,15 +16,60 @@ struct AutoLoadSystemDependencies
     
     SetDllDirectory(L"");
 
+    HMODULE module = ::GetModuleHandleW(L"kernel32.dll");
+    if (module) {
+      
+      
+      
+      decltype(SetDefaultDllDirectories)* setDefaultDllDirectories =
+        (decltype(SetDefaultDllDirectories)*) GetProcAddress(module, "SetDefaultDllDirectories");
+      if (setDefaultDllDirectories) {
+        setDefaultDllDirectories(LOAD_LIBRARY_SEARCH_SYSTEM32);
+        return;
+      }
+    }
+
     
     
     
-    static LPCWSTR delayDLLs[] = { L"profapi.dll", L"wsock32.dll",
-                                   L"crypt32.dll", L"cryptsp.dll",
-                                   L"cryptbase.dll", L"msasn1.dll",
-                                   L"userenv.dll", L"secur32.dll",
-                                   L"ws2_32.dll", L"ws2help.dll",
-                                   L"apphelp.dll", L"bcryptprimitives.dll" };
+#ifdef HAVE_64BIT_BUILD
+    
+    
+    
+    static LPCWSTR delayDLLs[] = { L"apphelp.dll",
+                                   L"cryptbase.dll",
+                                   L"cryptsp.dll",
+                                   L"dwmapi.dll",
+                                   L"mpr.dll",
+                                   L"ntmarta.dll",
+                                   L"profapi.dll",
+                                   L"propsys.dll",
+                                   L"sspicli.dll",
+                                   L"wsock32.dll" };
+
+#else
+    
+    
+    
+    static LPCWSTR delayDLLs[] = { L"apphelp.dll",
+                                   L"crypt32.dll",
+                                   L"cryptbase.dll",
+                                   L"cryptsp.dll",
+                                   L"dwmapi.dll",
+                                   L"mpr.dll",
+                                   L"msasn1.dll",
+                                   L"ntmarta.dll",
+                                   L"profapi.dll",
+                                   L"propsys.dll",
+                                   L"psapi.dll",
+                                   L"secur32.dll",
+                                   L"sspicli.dll",
+                                   L"userenv.dll",
+                                   L"uxtheme.dll",
+                                   L"ws2_32.dll",
+                                   L"ws2help.dll",
+                                   L"wsock32.dll" };
+#endif
 
     WCHAR systemDirectory[MAX_PATH + 1] = { L'\0' };
     
@@ -42,7 +87,7 @@ struct AutoLoadSystemDependencies
     
     for (size_t i = 0; i < sizeof(delayDLLs) / sizeof(delayDLLs[0]); ++i) {
       size_t fileLen = wcslen(delayDLLs[i]);
-      wcsncpy(systemDirectory + systemDirLen, delayDLLs[i], 
+      wcsncpy(systemDirectory + systemDirLen, delayDLLs[i],
               MAX_PATH - systemDirLen);
       if (systemDirLen + fileLen <= MAX_PATH) {
         systemDirectory[systemDirLen + fileLen] = L'\0';
@@ -50,7 +95,9 @@ struct AutoLoadSystemDependencies
         systemDirectory[MAX_PATH] = L'\0';
       }
       LPCWSTR fullModulePath = systemDirectory; 
-      LoadLibraryW(fullModulePath);
+      
+      
+      LoadLibraryExW(fullModulePath, nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
     }
   }
 } loadDLLs;
