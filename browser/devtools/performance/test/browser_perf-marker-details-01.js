e5127a7e2da84694e29b9eb253ec0aeb71b04cfc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:41:57 +0000

    Bug 1171539 - ConsoleTime markers should render endStacks. r=vp
    UltraBlame original commit: d441e867529a23859f14d136dd973a6f48bb90c1

---
+++ browser/devtools/performance/test/browser_perf-marker-details-01.js
@@ -0,0 +1,113 @@
+
+
+
+
+
+
+
+
+function* spawnTest() {
+  let { target, panel } = yield initPerformance(MARKERS_URL);
+  let { $, $$, EVENTS, PerformanceController, OverviewView, WaterfallView } = panel.panelWin;
+  let { L10N } = devtools.require("devtools/performance/global");
+
+  
+  
+  
+  
+  WaterfallView._prepareWaterfallTree = markers => {
+    return { submarkers: markers };
+  };
+
+  const MARKER_TYPES = [
+    "Styles", "Reflow", "Paint", "ConsoleTime", "TimeStamp"
+  ];
+
+  yield startRecording(panel);
+  yield waitUntil(() => {
+    
+    let markers = PerformanceController.getCurrentRecording().getMarkers();
+    return MARKER_TYPES.every(type => markers.some(m => m.name === type));
+  });
+  yield stopRecording(panel);
+
+  
+  let timeline = OverviewView.graphs.get("timeline");
+  let rerendered = WaterfallView.once(EVENTS.WATERFALL_RENDERED);
+  timeline.setSelection({ start: 0, end: timeline.width });
+  yield rerendered;
+
+  let bars = $$(".waterfall-marker-bar");
+  let markers = PerformanceController.getCurrentRecording().getMarkers();
+
+  ok(bars.length >= MARKER_TYPES.length, `Got at least ${MARKER_TYPES.length} markers (1)`);
+  ok(markers.length >= MARKER_TYPES.length, `Got at least ${MARKER_TYPES.length} markers (2)`);
+
+  const tests = {
+    ConsoleTime: function (marker) {
+      shouldHaveLabel($, L10N.getStr("timeline.markerDetail.consoleTimerName"), "!!!", marker);
+      shouldHaveStack($, "startStack", marker);
+      shouldHaveStack($, "endStack", marker);
+      return true;
+    },
+    TimeStamp: function (marker) {
+      shouldHaveLabel($, "Label:", "go", marker);
+      shouldHaveStack($, "stack", marker);
+      return true;
+    },
+    Styles: function (marker) {
+      if (marker.restyleHint) {
+        shouldHaveLabel($, "Restyle Hint:", marker.restyleHint.replace(/eRestyle_/g, ""), marker);
+      }
+      if (marker.stack) {
+        shouldHaveStack($, "stack", marker);
+        return true;
+      }
+    },
+    Reflow: function (marker) {
+      if (marker.stack) {
+        shouldHaveStack($, "stack", marker);
+        return true;
+      }
+    }
+  };
+
+  
+  
+  
+  let testsDone = [];
+  let TOTAL_TESTS = 4;
+
+  for (let i = 0; i < bars.length; i++) {
+    let bar = bars[i];
+    let m = markers[i];
+    EventUtils.sendMouseEvent({ type: "mousedown" }, bar);
+
+    if (testsDone.indexOf(m.name) === -1 && tests[m.name]) {
+      let fullTestComplete = tests[m.name](m);
+      if (fullTestComplete) {
+        testsDone.push(m.name);
+      }
+    } else {
+      info(`TODO: Need to add marker details tests for ${m.name}`);
+    }
+
+    if (testsDone.length === TOTAL_TESTS) {
+      break;
+    }
+  }
+
+  yield teardown(panel);
+  finish();
+}
+
+function shouldHaveStack ($, type, marker) {
+  ok($(`#waterfall-details .marker-details-stack[type=${type}]`), `${marker.name} has a stack: ${type}`);
+}
+
+function shouldHaveLabel ($, name, value, marker) {
+  info(name);
+  let $name = $(`#waterfall-details .marker-details-labelcontainer .marker-details-labelname[value="${name}"]`);
+  let $value = $name.parentNode.querySelector(".marker-details-labelvalue");
+  is($value.getAttribute("value"), value, `${marker.name} has correct label for ${name}:${value}`);
+}
