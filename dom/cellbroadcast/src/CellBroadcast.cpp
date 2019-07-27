2c0e554104b2b095d1f69df9aef9f8a3ea8cf314
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:58:06 +0000

    Bug 1034582: add MOZ_FINAL to suppress -Wdelete-non-virtual-dtor when MOZ_B2G_RIL is enabled. r=khuey
    UltraBlame original commit: 323c8a4cb90e9ef8a9cb1171a1d8bc6661184792

--- dom/cellbroadcast/src/CellBroadcast.cpp
+++ dom/cellbroadcast/src/CellBroadcast.cpp
@@ -17,7 +17,7 @@ using namespace mozilla::dom;
 
 
 
-class CellBroadcast::Listener : public nsICellBroadcastListener
+class CellBroadcast::Listener MOZ_FINAL : public nsICellBroadcastListener
 {
 private:
   CellBroadcast* mCellBroadcast;
