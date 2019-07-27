e4317892976b218bcce5295d42e79f3060041730
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:39:26 +0000

    Bug 1150858 - Unwrap the correct object in ArrayBuffer.transfer (r=sfink)
    UltraBlame original commit: e45901374cd43e4fba82c1df64cec32665136861

--- js/src/jit-test/tests/basic/testArrayBufferTransfer.js
+++ js/src/jit-test/tests/basic/testArrayBufferTransfer.js
@@ -56,6 +56,12 @@ assertEq(buf2.byteLength, 9);
 assertThrowsInstanceOf(()=>XF(buf1), TypeError);
 
 
+var buf3 = newGlobal().eval("new ArrayBuffer(10)");
+var buf4 = XF(buf3, 20);
+assertEq(buf4.byteLength, 20);
+assertThrowsInstanceOf(()=>XF(buf3), TypeError);
+
+
 function test(N1, N2) {
     var buf1 = new ArrayBuffer(N1);
     var i32 = new Int32Array(buf1);
