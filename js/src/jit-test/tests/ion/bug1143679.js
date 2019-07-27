783c1f7e4497866e57c8a43671f7bfed697ef80d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:30:00 +0000

    Bug 1143679 followup - Add testcase. r=me
    UltraBlame original commit: 12722aeb2499b4287d8ba1024ee62ba3daf55859

---
+++ js/src/jit-test/tests/ion/bug1143679.js
@@ -0,0 +1,15 @@
+
+function foo() {
+    function gen() {
+        try {
+            yield 1;
+        } finally {
+            throw 98;
+        }
+    }
+    for (i in gen()) {
+        for each (var i in this)
+            return false;
+    }
+}
+foo();
