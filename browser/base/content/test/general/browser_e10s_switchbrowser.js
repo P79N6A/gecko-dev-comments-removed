ad0da519d4caeef2fd43e4ec2c64036fb065ad95
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:42:59 +0000

    Bug 1072198 - updateBrowserRemoteness() needs to keep .permanentKey when updating browsers r=billm
    UltraBlame original commit: e16c4c4eb07a99753935057ff91d9a8966d9d613

--- browser/base/content/test/general/browser_e10s_switchbrowser.js
+++ browser/base/content/test/general/browser_e10s_switchbrowser.js
@@ -58,45 +58,54 @@ add_task(function*() {
   info("1");
   
   gBrowser.selectedTab = gBrowser.addTab("about:blank", {skipAnimation: true});
+  let {permanentKey} = gBrowser.selectedBrowser;
   yield waitForLoad("http://example.org/" + DUMMY_PATH);
   is(gBrowser.selectedTab.getAttribute("remote"), expectedRemote, "Remote attribute should be correct");
+  is(gBrowser.selectedBrowser.permanentKey, permanentKey, "browser.permanentKey is still the same");
 
   info("2");
   
   yield waitForLoad("http://example.com/" + DUMMY_PATH);
   is(gBrowser.selectedTab.getAttribute("remote"), expectedRemote, "Remote attribute should be correct");
+  is(gBrowser.selectedBrowser.permanentKey, permanentKey, "browser.permanentKey is still the same");
   check_history();
 
   info("3");
   
   yield waitForLoad("about:robots");
   is(gBrowser.selectedTab.getAttribute("remote"), "", "Remote attribute should be correct");
+  is(gBrowser.selectedBrowser.permanentKey, permanentKey, "browser.permanentKey is still the same");
   check_history();
 
   info("4");
   
   yield waitForLoad("http://example.org/" + DUMMY_PATH);
   is(gBrowser.selectedTab.getAttribute("remote"), expectedRemote, "Remote attribute should be correct");
+  is(gBrowser.selectedBrowser.permanentKey, permanentKey, "browser.permanentKey is still the same");
   check_history();
 
   info("5");
   yield back();
   is(gBrowser.selectedTab.getAttribute("remote"), "", "Remote attribute should be correct");
+  is(gBrowser.selectedBrowser.permanentKey, permanentKey, "browser.permanentKey is still the same");
   check_history();
 
   info("6");
   yield back();
   is(gBrowser.selectedTab.getAttribute("remote"), expectedRemote, "Remote attribute should be correct");
+  is(gBrowser.selectedBrowser.permanentKey, permanentKey, "browser.permanentKey is still the same");
   check_history();
 
   info("7");
   yield forward();
   is(gBrowser.selectedTab.getAttribute("remote"), "", "Remote attribute should be correct");
+  is(gBrowser.selectedBrowser.permanentKey, permanentKey, "browser.permanentKey is still the same");
   check_history();
 
   info("8");
   yield forward();
   is(gBrowser.selectedTab.getAttribute("remote"), expectedRemote, "Remote attribute should be correct");
+  is(gBrowser.selectedBrowser.permanentKey, permanentKey, "browser.permanentKey is still the same");
   check_history();
 
   info("9");
