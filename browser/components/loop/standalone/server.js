2da4bd21d8cc1d118647a6dc1a14492412ff089d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:12:51 +0000

    Bug 1081189 - Move URLs out of loop.en-us.properties and into the config file and Makefile. r=NiKo`
    UltraBlame original commit: 570579f4295414fc0efb2a4cb862c2b21d7cc73a

--- browser/components/loop/standalone/server.js
+++ browser/components/loop/standalone/server.js
@@ -24,6 +24,9 @@ function getConfigFile(req, res) {
     
     
     "loop.config.marketplaceUrl = 'http://fake-market.herokuapp.com/iframe-install.html'",
+    "loop.config.brandWebsiteUrl = 'https://www.mozilla.org/firefox/';",
+    "loop.config.privacyWebsiteUrl = 'https://www.mozilla.org/privacy';",
+    "loop.config.legalWebsiteUrl = '/legal/terms';",
     "loop.config.fxosApp = loop.config.fxosApp || {};",
     "loop.config.fxosApp.name = 'Loop';",
     "loop.config.fxosApp.manifestUrl = 'http://fake-market.herokuapp.com/apps/packagedApp/manifest.webapp';"
