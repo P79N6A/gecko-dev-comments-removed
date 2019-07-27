a8d4a4472b6d7a371f3923b970103c39aada80c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:56:20 +0000

    Bug 1054630 - Part 1: Add plumbing for SpiderMonkey parser telemetry. r=till
    UltraBlame original commit: be39ba3aa672a448a3390b6f25602ac162d86cbe

--- js/src/frontend/Parser.cpp
+++ js/src/frontend/Parser.cpp
@@ -449,6 +449,8 @@ Parser<ParseHandler>::Parser(ExclusiveContext *cx, LifoAlloc *alloc,
 template <typename ParseHandler>
 Parser<ParseHandler>::~Parser()
 {
+    accumulateTelemetry();
+
     alloc.release(tempPoolMark);
 
     
@@ -7565,6 +7567,29 @@ Parser<ParseHandler>::exprInParens()
 #endif 
 
     return pn;
+}
+
+template <typename ParseHandler>
+void
+Parser<ParseHandler>::accumulateTelemetry()
+{
+    JSContext* cx = context->maybeJSContext();
+    if (!cx)
+        return;
+    JSAccumulateTelemetryDataCallback cb = cx->runtime()->telemetryCallback;
+    if (!cb)
+        return;
+    const char* filename = getFilename();
+    if (!filename)
+        return;
+
+    bool isHTTP = strncmp(filename, "http://", 7) == 0 || strncmp(filename, "https://", 8) == 0;
+
+    
+    if (!isHTTP)
+        return;
+
+    
 }
 
 template class Parser<FullParseHandler>;
