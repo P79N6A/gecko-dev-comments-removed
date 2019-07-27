8a9258bea29203617db785eb1c4250f37da833fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:55:59 +0000

    Bug 1172517 - track sharedness in global.  r=luke
    UltraBlame original commit: f5ce4d1cb3c294d740a7a625ff12e65957e08c71

--- js/src/jit-test/tests/asm.js/gating.js
+++ js/src/jit-test/tests/asm.js/gating.js
@@ -7,32 +7,25 @@
 
 
 
+function module_a(stdlib, foreign, heap) {
+    "use asm";
 
+    
+    var view = stdlib.SharedInt32Array;
+    var i32a = new view(heap);
+    var ld = stdlib.Atomics.load;
 
+    function do_load() {
+	var v = 0;
+	v = ld(i32a, 0)|0;
+	return v|0;
+    }
 
+    return { load: do_load };
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (this.SharedArrayBuffer)
+    assertEq(isAsmJSModule(module_a), true);
 
 function module_b(stdlib, foreign, heap) {
     "use asm";
