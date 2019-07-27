c0bc67a48dd08e15263e6fe8ed572313b91246ca
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:11:33 +0000

    Bug 1060873 - Object.isExtensible() should return false when given primitive values as input. r=till
    UltraBlame original commit: 8e75a5c333c8f8637466a9d134f369d64663da86

--- js/src/builtin/Object.cpp
+++ js/src/builtin/Object.cpp
@@ -1015,17 +1015,21 @@ obj_defineProperties(JSContext *cx, unsigned argc, Value *vp)
     return DefineProperties(cx, obj, props);
 }
 
+
 static bool
 obj_isExtensible(JSContext *cx, unsigned argc, Value *vp)
 {
     CallArgs args = CallArgsFromVp(argc, vp);
-    RootedObject obj(cx);
-    if (!GetFirstArgumentAsObject(cx, args, "Object.isExtensible", &obj))
-        return false;
 
-    bool extensible;
-    if (!JSObject::isExtensible(cx, obj, &extensible))
-        return false;
+    
+    bool extensible = false;
+
+    
+    if (args.get(0).isObject()) {
+        RootedObject obj(cx, &args.get(0).toObject());
+        if (!JSObject::isExtensible(cx, obj, &extensible))
+            return false;
+    }
     args.rval().setBoolean(extensible);
     return true;
 }
