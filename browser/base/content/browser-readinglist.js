38e274d32775c9f0afb32ad21282d8696e3880ff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:13:37 +0000

    Bug 1124400 - [ReadingList] Add section to bookmarks popup for reading list items and actions, r=Unfocussed.
    UltraBlame original commit: 46aebcd9481e071cf5c280865d85e99313757fd4

--- browser/base/content/browser-readinglist.js
+++ browser/base/content/browser-readinglist.js
@@ -4,6 +4,9 @@
 
 
 
+XPCOMUtils.defineLazyModuleGetter(this, "ReadingList",
+  "resource:///modules/readinglist/ReadingList.jsm");
+
 let ReadingListUI = {
   
 
@@ -65,4 +68,70 @@ let ReadingListUI = {
       SidebarUI.hide();
     }
   },
+
+  onReadingListPopupShowing(target) {
+    if (target.id == "BMB_readingListPopup") {
+      
+      
+      document.getElementById("BMB_viewReadingListSidebar")
+              .classList.add("panel-subview-footer");
+    }
+
+    while (!target.firstChild.id)
+      target.firstChild.remove();
+
+    let classList = "menuitem-iconic bookmark-item menuitem-with-favicon";
+    let insertPoint = target.firstChild;
+    if (insertPoint.classList.contains("subviewbutton"))
+      classList += " subviewbutton";
+
+    ReadingList.getItems().then(items => {
+      for (let item of items) {
+        let menuitem = document.createElement("menuitem");
+        menuitem.setAttribute("label", item.title || item.url.spec);
+        menuitem.setAttribute("class", classList);
+
+        let node = menuitem._placesNode = {
+          
+          
+          type: Ci.nsINavHistoryResultNode.RESULT_TYPE_URI,
+
+          
+          
+          parent: {type: Ci.nsINavHistoryResultNode.RESULT_TYPE_FOLDER},
+
+          
+          
+          
+          
+          itemId: -1,
+
+          
+          uri: item.url.spec,
+
+          
+          title: item.title
+        };
+
+        Favicons.getFaviconURLForPage(item.url, uri => {
+          if (uri) {
+            menuitem.setAttribute("image",
+                                  Favicons.getFaviconLinkForIcon(uri).spec);
+          }
+        });
+
+        target.insertBefore(menuitem, insertPoint);
+      }
+
+      if (!items.length) {
+        let menuitem = document.createElement("menuitem");
+        let bundle =
+          Services.strings.createBundle("chrome://browser/locale/places/places.properties");
+        menuitem.setAttribute("label", bundle.GetStringFromName("bookmarksMenuEmptyFolder"));
+        menuitem.setAttribute("class", "bookmark-item");
+        menuitem.setAttribute("disabled", true);
+        target.insertBefore(menuitem, insertPoint);
+      }
+    });
+  }
 };
