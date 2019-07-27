1479ae9ed4284e7ba59e58e3d2b9cf09e230c6e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:34:45 +0000

    Back out changeset 616e32e3da77 (bug 1146333) for widespread test bustage.
    UltraBlame original commit: 8cdd3bb8d11c33e5314de72052072ceac2178422

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -1147,13 +1147,13 @@ class CGHeaders(CGWrapper):
             
             declareIncludes.add("mozilla/dom/CallbackFunction.h")
             
-            declareIncludes.add("mozilla/dom/ToJSValue.h")
+            declareIncludes.add("mozilla/dom/BindingUtils.h")
 
         if len(callbackDescriptors) != 0 or len(jsImplementedDescriptors) != 0:
             
             declareIncludes.add("mozilla/dom/CallbackInterface.h")
             
-            declareIncludes.add("mozilla/dom/ToJSValue.h")
+            declareIncludes.add("mozilla/dom/BindingUtils.h")
 
         
         
@@ -13825,7 +13825,7 @@ class CGCallback(CGClass):
             """
             $*{setupCall}
             JS::Rooted<JS::Value> thisValJS(s.GetContext());
-            if (!ToJSValue(s.GetContext(), thisVal, &thisValJS)) {
+            if (!WrapCallThisValue(s.GetContext(), thisVal, &thisValJS)) {
               aRv.Throw(NS_ERROR_FAILURE);
               return${errorReturn};
             }
