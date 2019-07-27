609aaa744dd208537443cfed15007dd0c8bc762e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:23:59 +0000

    Bug 1157916 - Add tooltip for displaying start/end times on marker details' duration field. r=vp
    UltraBlame original commit: 10fc07861f4ce67bdf85fc03adcec67356e3a9b3

--- browser/devtools/performance/test/browser_timeline-waterfall-sidebar.js
+++ browser/devtools/performance/test/browser_timeline-waterfall-sidebar.js
@@ -42,12 +42,17 @@ function spawnTest () {
     is($("#waterfall-details .marker-details-type").getAttribute("value"), getMarkerLabel(m),
       "sidebar title matches markers name");
 
+    let tooltip = $(".marker-details-duration").getAttribute("tooltiptext");
     let printedDuration = $(".marker-details-duration .marker-details-labelvalue").getAttribute("value");
 
     let toMs = ms => L10N.getFormatStrWithNumbers("timeline.tick", ms);
 
     
     is(toMs(m.end - m.start), printedDuration, "sidebar duration is valid");
+    
+    
+    ok(tooltip.indexOf(toMs(m.start)) !== -1, "tooltip has start time");
+    ok(tooltip.indexOf(toMs(m.end)) !== -1, "tooltip has end time");
   }
   yield teardown(panel);
   finish();
