a1396b12e61a34f66377496e80d3810ae446bd2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:15:07 +0000

    bug 1172655 - Add GeckoEmbed XCode project (NPOTB)
    UltraBlame original commit: c20329242058cfc25f1035b7500178546c32ee66

---
+++ embedding/ios/GeckoEmbed/GeckoEmbed/AppDelegate.h
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ embedding/ios/GeckoEmbed/GeckoEmbed/ViewController.h
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
