ac8bb05dc28c6035a2a189754537ee44b1bed926
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:28:58 +0000

    Bug 1094821 - Install a devedition lightweight theme on startup, then wait for it to be applied before applying devedition styles;r=Gijs
    UltraBlame original commit: 01482cdccd72b4f36f10ff3706a92c4c77c48253

--- browser/devtools/framework/toolbox-options.js
+++ browser/devtools/framework/toolbox-options.js
@@ -9,9 +9,6 @@ const Services = require("Services");
 const promise = require("promise");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 XPCOMUtils.defineLazyModuleGetter(this, "gDevTools", "resource:///modules/devtools/gDevTools.jsm");
-XPCOMUtils.defineLazyModuleGetter(this, "CustomizeMode", "resource:///modules/CustomizeMode.jsm");
-const kDeveditionChangedNotification = "devedition-theme-state-changed";
-const DEVEDITION_THEME_PREF = "browser.devedition.theme.enabled";
 
 exports.OptionsPanel = OptionsPanel;
 
@@ -87,7 +84,6 @@ function OptionsPanel(iframeWindow, toolbox) {
 
   this._addListeners();
 
-  Services.obs.addObserver(this, kDeveditionChangedNotification, false);
   const EventEmitter = require("devtools/toolkit/event-emitter");
   EventEmitter.decorate(this);
 }
@@ -112,7 +108,6 @@ OptionsPanel.prototype = {
       this.setupToolsList();
       this.setupToolbarButtonsList();
       this.setupThemeList();
-      this.setupBrowserThemeButton();
       this.populatePreferences();
       this.updateDefaultTheme();
     }).then(() => {
@@ -146,8 +141,6 @@ OptionsPanel.prototype = {
     }
     else if (data.pref === "devtools.theme") {
       this.updateCurrentTheme();
-    } else if (data.pref === "browser.devedition.theme.enabled") {
-      this.updateBrowserTheme();
     }
   },
 
@@ -283,52 +276,6 @@ OptionsPanel.prototype = {
     this.updateCurrentTheme();
   },
 
-  
-
-
-
-  setupBrowserThemeButton: function() {
-    let checkbox = this.panelDoc.getElementById("devtools-browser-theme");
-
-    checkbox.addEventListener("command", function() {
-      setPrefAndEmit(DEVEDITION_THEME_PREF, this.checked);
-    }.bind(checkbox));
-
-    this.updateBrowserThemeButton();
-  },
-
-  
-
-
-  updateBrowserThemeButton: function() {
-    let checkbox = this.panelDoc.getElementById("devtools-browser-theme");
-
-    
-    
-    
-    if (this._isDevEditionThemeOn()) {
-      checkbox.setAttribute("checked", "true");
-    } else {
-      checkbox.removeAttribute("checked");
-    }
-
-    
-    if (GetPref("browser.devedition.theme.showCustomizeButton")) {
-      checkbox.removeAttribute("hidden");
-    } else {
-      checkbox.setAttribute("hidden", "true");
-    }
-  },
-
-  
-
-
-
-  updateBrowserTheme: function() {
-    let enabled = GetPref("browser.devedition.theme.enabled");
-    CustomizeMode.prototype.toggleDevEditionTheme.call(this, enabled);
-  },
-
   populatePreferences: function() {
     let prefCheckboxes = this.panelDoc.querySelectorAll("checkbox[data-pref]");
     for (let checkbox of prefCheckboxes) {
@@ -417,25 +364,6 @@ OptionsPanel.prototype = {
     this.target.activeTab.reconfigure(options);
   },
 
-  
-
-
-
-  _isDevEditionThemeOn: function() {
-    let win = Services.wm.getMostRecentWindow("navigator:browser");
-    return !!(win && win.DevEdition.styleSheet);
-  },
-
-  
-
-
-
-  observe: function(aSubject, aTopic, aData) {
-    if (aTopic === kDeveditionChangedNotification) {
-      this.updateBrowserThemeButton();
-    }
-  },
-
   destroy: function() {
     if (this.destroyPromise) {
       return this.destroyPromise;
@@ -459,8 +387,6 @@ OptionsPanel.prototype = {
     }
 
     this.panelWin = this.panelDoc = this.disableJSNode = null;
-
-    Services.obs.removeObserver(this, kDeveditionChangedNotification);
 
     return deferred.promise;
   }
