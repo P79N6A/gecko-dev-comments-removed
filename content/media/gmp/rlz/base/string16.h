01c1e8e2976ad8cb947335a7afb120edcec1e223
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:57:00 +0000

    Bug 1060179 - Import librlz for device identity generation on Windows. r=glandium,dmajor
    UltraBlame original commit: ff97841a1f5468353a48846b4354df89aa0d2672

---
+++ content/media/gmp/rlz/base/string16.h
@@ -0,0 +1,12 @@
+
+
+
+
+
+#ifndef FAKE_STRING16_H
+#define FAKE_STRING16_H
+
+#include <string>
+typedef std::wstring string16;
+
+#endif
---
+++ content/media/gmp/rlz/lib/assert.h
@@ -0,0 +1,14 @@
+
+
+
+
+
+#ifndef FAKE_ASSERT_H_
+#define FAKE_ASSERT_H_
+
+#include <assert.h>
+
+#define ASSERT_STRING(x) { assert(false); }
+#define VERIFY(x) { assert(x); };
+
+#endif
