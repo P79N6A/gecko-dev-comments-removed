80a6c9cb33d54fde7369e2ec72dd4e8f2dfa318d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:03:15 +0000

    Bug 1090444 - The Printing Progress dialog does not show progress or close when opened from the child process. r=blassey.
    We now proxy progress updates from the child up to the parent over IPC.
    
    UltraBlame original commit: 9a50f667e675285fff5912a2041dcd3d818301a6

---
+++ embedding/components/printingui/ipc/PrintProgressDialogChild.h
@@ -0,0 +1,40 @@
+
+
+
+
+#ifndef mozilla_embedding_PrintProgressDialogChild_h
+#define mozilla_embedding_PrintProgressDialogChild_h
+
+#include "mozilla/embedding/PPrintProgressDialogChild.h"
+#include "nsIPrintProgressParams.h"
+#include "nsIWebProgressListener.h"
+
+class nsIObserver;
+
+namespace mozilla {
+namespace embedding {
+
+class PrintProgressDialogChild MOZ_FINAL : public PPrintProgressDialogChild,
+                                           public nsIWebProgressListener,
+                                           public nsIPrintProgressParams
+{
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIWEBPROGRESSLISTENER
+  NS_DECL_NSIPRINTPROGRESSPARAMS
+
+public:
+  MOZ_IMPLICIT PrintProgressDialogChild(nsIObserver* aOpenObserver);
+
+  virtual bool RecvDialogOpened();
+
+private:
+  virtual ~PrintProgressDialogChild();
+  nsCOMPtr<nsIObserver> mOpenObserver;
+  nsString mDocTitle;
+  nsString mDocURL;
+};
+
+} 
+} 
+
+#endif
---
+++ embedding/components/printingui/ipc/PrintProgressDialogParent.cpp
@@ -0,0 +1,113 @@
+
+
+
+
+#include "mozilla/unused.h"
+#include "nsIPrintProgressParams.h"
+#include "nsIWebProgressListener.h"
+#include "PrintProgressDialogParent.h"
+
+using mozilla::unused;
+
+namespace mozilla {
+namespace embedding {
+
+NS_IMPL_ISUPPORTS(PrintProgressDialogParent, nsIObserver)
+
+MOZ_IMPLICIT PrintProgressDialogParent::PrintProgressDialogParent() :
+  mActive(true)
+{
+  MOZ_COUNT_CTOR(PrintProgressDialogParent);
+}
+
+MOZ_IMPLICIT PrintProgressDialogParent::~PrintProgressDialogParent()
+{
+  MOZ_COUNT_DTOR(PrintProgressDialogParent);
+}
+
+void
+PrintProgressDialogParent::SetWebProgressListener(nsIWebProgressListener* aListener)
+{
+  mWebProgressListener = aListener;
+}
+
+void
+PrintProgressDialogParent::SetPrintProgressParams(nsIPrintProgressParams* aParams)
+{
+  mPrintProgressParams = aParams;
+}
+
+bool
+PrintProgressDialogParent::RecvStateChange(const long& stateFlags,
+                                           const nsresult& status)
+{
+  if (mWebProgressListener) {
+    mWebProgressListener->OnStateChange(nullptr, nullptr, stateFlags, status);
+  }
+  return true;
+}
+
+bool
+PrintProgressDialogParent::RecvProgressChange(const long& curSelfProgress,
+                                              const long& maxSelfProgress,
+                                              const long& curTotalProgress,
+                                              const long& maxTotalProgress)
+{
+  if (mWebProgressListener) {
+    mWebProgressListener->OnProgressChange(nullptr, nullptr, curSelfProgress,
+                                           maxSelfProgress, curTotalProgress,
+                                           maxTotalProgress);
+  }
+  return true;
+}
+
+bool
+PrintProgressDialogParent::RecvDocTitleChange(const nsString& newTitle)
+{
+  if (mPrintProgressParams) {
+    mPrintProgressParams->SetDocTitle(newTitle.get());
+  }
+  return true;
+}
+
+bool
+PrintProgressDialogParent::RecvDocURLChange(const nsString& newURL)
+{
+  if (mPrintProgressParams) {
+    mPrintProgressParams->SetDocURL(newURL.get());
+  }
+  return true;
+}
+
+void
+PrintProgressDialogParent::ActorDestroy(ActorDestroyReason aWhy)
+{
+}
+
+bool
+PrintProgressDialogParent::Recv__delete__()
+{
+  
+  
+  mActive = false;
+  return true;
+}
+
+
+NS_IMETHODIMP
+PrintProgressDialogParent::Observe(nsISupports *aSubject, const char *aTopic,
+                                   const char16_t *aData)
+{
+  if (mActive) {
+    unused << SendDialogOpened();
+  } else {
+    NS_WARNING("The print progress dialog finished opening, but communications "
+               "with the child have been closed.");
+  }
+
+  return NS_OK;
+}
+
+
+} 
+} 
