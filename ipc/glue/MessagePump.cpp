5bef8dc56576d356c2656ca0ed50f688a4879ba3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:00:37 +0000

    Bug 1157908 - Give Gecko thread Looper low priority; r=snorp
    UltraBlame original commit: 7164a2488b286057a748e0c412389b3665ecfd94

--- ipc/glue/MessagePump.cpp
+++ ipc/glue/MessagePump.cpp
@@ -23,10 +23,6 @@
 #include "nsXULAppAPI.h"
 #include "prthread.h"
 
-#ifdef MOZ_WIDGET_ANDROID
-#include "AndroidBridge.h"
-#endif
-
 #ifdef MOZ_NUWA_PROCESS
 #include "ipc/Nuwa.h"
 #endif
@@ -104,15 +100,6 @@ MessagePump::Run(MessagePump::Delegate* aDelegate)
     
     
     
-
-#ifdef MOZ_WIDGET_ANDROID
-    
-    
-    
-    if (MOZ_LIKELY(AndroidBridge::HasEnv())) {
-        did_work |= mozilla::widget::GeckoAppShell::PumpMessageLoop();
-    }
-#endif
 
     did_work |= aDelegate->DoDelayedWork(&delayed_work_time_);
 
