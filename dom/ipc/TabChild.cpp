cf72fd5982f78a92ad022bc17a19ede6ea4f82fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:27:09 +0000

    Bug 1036264 - Always pass true for ignoreRootScrollFrame. r=kats
    UltraBlame original commit: 0b8ecdc86b9c9d29969edb1a019961cfd6914009

--- dom/ipc/TabChild.cpp
+++ dom/ipc/TabChild.cpp
@@ -516,6 +516,7 @@ TabChildBase::DispatchSynthesizedMouseEvent(uint32_t aMsg, uint64_t aTime,
   event.time = aTime;
   event.button = WidgetMouseEvent::eLeftButton;
   event.inputSource = nsIDOMMouseEvent::MOZ_SOURCE_TOUCH;
+  event.ignoreRootScrollFrame = true;
   if (aMsg != NS_MOUSE_MOVE) {
     event.clickCount = 1;
   }
@@ -1845,7 +1846,7 @@ TabChild::RecvHandleLongTap(const CSSPoint& aPoint, const ScrollableLayerGuid& a
   bool eventHandled =
       DispatchMouseEvent(NS_LITERAL_STRING("contextmenu"),
                          APZCCallbackHelper::ApplyCallbackTransform(aPoint, aGuid),
-                         2, 1, 0, false,
+                         2, 1, 0, true,
                          nsIDOMMouseEvent::MOZ_SOURCE_TOUCH);
 
   
@@ -2078,7 +2079,7 @@ TabChild::FireContextMenuEvent()
                                              2 ,
                                              1 ,
                                              0 ,
-                                             false ,
+                                             true ,
                                              nsIDOMMouseEvent::MOZ_SOURCE_TOUCH);
 
   
