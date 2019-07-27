c7e7869a9938ea210ffb134c1dd51bde1bd4c368
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:46:12 +0000

    Backed out changeset 2507a02fae1a (bug 1153521) for frequent test failures in cache-add.https.html
    UltraBlame original commit: 6562cb71c376f6526e9de89d1d83cbb32cd838c0

--- testing/web-platform/tests/XMLHttpRequest/resources/xmlhttprequest-timeout-abortedonmain.js
+++ testing/web-platform/tests/XMLHttpRequest/resources/xmlhttprequest-timeout-abortedonmain.js
@@ -5,4 +5,4 @@
 
 
 runTestRequests([ new AbortedRequest(true, 0),
-                  new AbortedRequest(true, TIME_DELAY) ]);
+		  new AbortedRequest(true, TIME_DELAY) ]);
--- testing/web-platform/tests/XMLHttpRequest/resources/xmlhttprequest-timeout-overrides.js
+++ testing/web-platform/tests/XMLHttpRequest/resources/xmlhttprequest-timeout-overrides.js
@@ -7,5 +7,5 @@ if (this.document === undefined)
 
 
 runTestRequests([ new RequestTracker(true, "timeout disabled after initially set", TIME_NORMAL_LOAD, TIME_REGULAR_TIMEOUT, 0),
-                  new RequestTracker(true, "timeout overrides load after a delay", TIME_NORMAL_LOAD, TIME_DELAY, TIME_REGULAR_TIMEOUT),
-                  new RequestTracker(true, "timeout enabled after initially disabled", 0, TIME_REGULAR_TIMEOUT, TIME_NORMAL_LOAD * 10) ]);
+		  new RequestTracker(true, "timeout overrides load after a delay", TIME_NORMAL_LOAD, TIME_DELAY, TIME_REGULAR_TIMEOUT),
+		  new RequestTracker(true, "timeout enabled after initially disabled", 0, TIME_REGULAR_TIMEOUT, TIME_NORMAL_LOAD * 10) ]);
