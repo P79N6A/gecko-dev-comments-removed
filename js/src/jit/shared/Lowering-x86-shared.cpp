5d3a34930637e0de482e040ecc94fb4a2ccbdffd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:19:16 +0000

    Bug 1065339 - IonMonkey: x86 VEX encoding support for several operators r=jandem
    UltraBlame original commit: becc884363307cea6ed3710da7ef3d5668989ef5

--- js/src/jit/shared/Lowering-x86-shared.cpp
+++ js/src/jit/shared/Lowering-x86-shared.cpp
@@ -100,13 +100,37 @@ LIRGeneratorX86Shared::lowerForALU(LInstructionHelper<1, 2, 0> *ins, MDefinition
     return defineReuseInput(ins, mir, 0);
 }
 
+static bool
+UseAVXEncoding(MIRType type)
+{
+    if (!Assembler::HasAVX())
+        return false;
+
+    
+    
+    if (IsFloatingPointType(type))
+        return true;
+    if (IsSimdType(type) && IsFloatingPointType(SimdTypeToScalarType(type)))
+        return true;
+
+    return false;
+}
+
 template<size_t Temps>
 bool
 LIRGeneratorX86Shared::lowerForFPU(LInstructionHelper<1, 2, Temps> *ins, MDefinition *mir, MDefinition *lhs, MDefinition *rhs)
 {
+    
+    
+    if (!UseAVXEncoding(mir->type())) {
+        ins->setOperand(0, useRegisterAtStart(lhs));
+        ins->setOperand(1, lhs != rhs ? use(rhs) : useAtStart(rhs));
+        return defineReuseInput(ins, mir, 0);
+    }
+
     ins->setOperand(0, useRegisterAtStart(lhs));
-    ins->setOperand(1, lhs != rhs ? use(rhs) : useAtStart(rhs));
-    return defineReuseInput(ins, mir, 0);
+    ins->setOperand(1, useAtStart(rhs));
+    return define(ins, mir);
 }
 
 template bool LIRGeneratorX86Shared::lowerForFPU(LInstructionHelper<1, 2, 0> *ins, MDefinition *mir,
