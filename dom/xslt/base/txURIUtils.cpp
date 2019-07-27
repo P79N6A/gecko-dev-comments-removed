e7123559162c487710617e033a2abc434eb92884
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:37:08 +0000

    Bug 1038756: Callsites creating a channel in /dom/xslt/ (r=peterv)
    UltraBlame original commit: 5609cb03aaccbf31e24c13cbd0f7d2dd1ce9aeb4

--- dom/xslt/base/txURIUtils.cpp
+++ dom/xslt/base/txURIUtils.cpp
@@ -1,7 +1,7 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
 
 #include "txURIUtils.h"
 #include "nsNetUtil.h"
@@ -11,16 +11,16 @@
 
 using mozilla::LoadInfo;
 
-/**
- * URIUtils
- * A set of utilities for handling URIs
-**/
 
-/**
- * Resolves the given href argument, using the given documentBase
- * if necessary.
- * The new resolved href will be appended to the given dest String
-**/
+
+
+
+
+
+
+
+
+
 void URIUtils::resolveHref(const nsAString& href, const nsAString& base,
                            nsAString& dest) {
     if (base.IsEmpty()) {
@@ -38,15 +38,15 @@ void URIUtils::resolveHref(const nsAString& href, const nsAString& base,
         NS_MakeAbsoluteURI(resultHref, href, pURL);
         dest.Append(resultHref);
     }
-} //-- resolveHref
+} 
 
-// static
+
 void
 URIUtils::ResetWithSource(nsIDocument *aNewDoc, nsIDOMNode *aSourceNode)
 {
     nsCOMPtr<nsINode> node = do_QueryInterface(aSourceNode);
     if (!node) {
-        // XXXbz passing nullptr as the first arg to Reset is illegal
+        
         aNewDoc->Reset(nullptr, nullptr);
         return;
     }
@@ -54,27 +54,28 @@ URIUtils::ResetWithSource(nsIDocument *aNewDoc, nsIDOMNode *aSourceNode)
     nsCOMPtr<nsIDocument> sourceDoc = node->OwnerDoc();
     nsIPrincipal* sourcePrincipal = sourceDoc->NodePrincipal();
 
-    // Copy the channel and loadgroup from the source document.
+    
     nsCOMPtr<nsILoadGroup> loadGroup = sourceDoc->GetDocumentLoadGroup();
     nsCOMPtr<nsIChannel> channel = sourceDoc->GetChannel();
     if (!channel) {
-        // Need to synthesize one
-        if (NS_FAILED(NS_NewChannel(getter_AddRefs(channel),
+        
+        nsresult rv = NS_NewChannel(getter_AddRefs(channel),
                                     sourceDoc->GetDocumentURI(),
-                                    nullptr,
-                                    loadGroup))) {
+                                    sourceDoc,
+                                    nsILoadInfo::SEC_FORCE_INHERIT_PRINCIPAL,
+                                    nsIContentPolicy::TYPE_OTHER,
+                                    nullptr,   
+                                    loadGroup);
+
+        if (NS_FAILED(rv)) {
             return;
         }
-        nsCOMPtr<nsILoadInfo> loadInfo =
-            new LoadInfo(sourcePrincipal, LoadInfo::eInheritPrincipal,
-                         LoadInfo::eNotSandboxed);
-        channel->SetLoadInfo(loadInfo);
     }
     aNewDoc->Reset(channel, loadGroup);
     aNewDoc->SetPrincipal(sourcePrincipal);
     aNewDoc->SetBaseURI(sourceDoc->GetDocBaseURI());
 
-    // Copy charset
+    
     aNewDoc->SetDocumentCharacterSetSource(
           sourceDoc->GetDocumentCharacterSetSource());
     aNewDoc->SetDocumentCharacterSet(sourceDoc->GetDocumentCharacterSet());
