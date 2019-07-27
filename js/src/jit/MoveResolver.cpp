e6541f9bfca2fff6a00e282a68597b22fd8f33cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:55:53 +0000

    Bug 1123874 - Optimize MoveGroups with multiple targets for the same source, r=sunfish.
    UltraBlame original commit: b2430b424a00701871090e47043395960a6e8ced

--- js/src/jit/MoveResolver.cpp
+++ js/src/jit/MoveResolver.cpp
@@ -113,14 +113,6 @@ MoveResolver::resolve()
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
     while (!pending_.empty()) {
         PendingMove *pm = pending_.popBack();
 
@@ -160,7 +152,7 @@ MoveResolver::resolve()
                 
                 
                 PendingMove *done = stack.popBack();
-                if (!orderedMoves_.append(*done))
+                if (!addOrderedMove(*done))
                     return false;
                 movePool_.free(done);
             }
@@ -174,4 +166,49 @@ MoveResolver::resolve()
     }
 
     return true;
+}
+
+bool
+MoveResolver::addOrderedMove(const MoveOp &move)
+{
+    
+    
+    
+    MOZ_ASSERT(!move.from().aliases(move.to()));
+
+    if (!move.from().isMemory() || move.isCycleBegin() || move.isCycleEnd())
+        return orderedMoves_.append(move);
+
+    
+    
+    for (int i = orderedMoves_.length() - 1; i >= 0; i--) {
+        const MoveOp &existing = orderedMoves_[i];
+
+        if (existing.from() == move.from() &&
+            !existing.to().aliases(move.to()) &&
+            existing.type() == move.type() &&
+            !existing.isCycleBegin() &&
+            !existing.isCycleEnd())
+        {
+            MoveOp *after = orderedMoves_.begin() + i + 1;
+            if (existing.to().isGeneralReg() || existing.to().isFloatReg()) {
+                MoveOp nmove(existing.to(), move.to(), move.type());
+                return orderedMoves_.insert(after, nmove);
+            } else if (move.to().isGeneralReg() || move.to().isFloatReg()) {
+                MoveOp nmove(move.to(), existing.to(), move.type());
+                orderedMoves_[i] = move;
+                return orderedMoves_.insert(after, nmove);
+            }
+        }
+
+        if (existing.to().aliases(move.from()) ||
+            existing.to().aliases(move.to()) ||
+            existing.from().aliases(move.to()) ||
+            existing.from().aliases(move.from()))
+        {
+            break;
+        }
+    }
+
+    return orderedMoves_.append(move);
 }
