75fb56b503952205e1d97e5cca8dea58bdab1f49
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:05:08 +0000

    Bug 1138384 - Enable CSS writing-mode support for all channels. r=dbaron,dholbert
    UltraBlame original commit: f561d08e90772b1709831ccd5c8fc322b8e6644b

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2246,11 +2246,7 @@ pref("layout.css.scope-pseudo.enabled", true);
 pref("layout.css.background-blend-mode.enabled", true);
 
 
-#ifdef RELEASE_BUILD
-pref("layout.css.vertical-text.enabled", false);
-#else
 pref("layout.css.vertical-text.enabled", true);
-#endif
 
 
 pref("layout.css.object-fit-and-position.enabled", true);
