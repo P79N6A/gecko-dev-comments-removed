e9b32d55c5a899c1681837534a6df12fa89115b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:57:49 +0000

    Bug 1032208, part 1 - Fix JSON.stringify treatment of symbols. r=jandem.
    UltraBlame original commit: f81a509fc014e4c9ec4fbc200535ff94152d4cbc

---
+++ js/src/tests/ecma_6/Symbol/json-stringify-values.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+var symbols = [
+    Symbol(),
+    Symbol.for("ponies"),
+    Symbol.iterator
+];
+
+for (var sym of symbols) {
+    assertEq(JSON.stringify(sym), undefined);
+    assertEq(JSON.stringify([sym]), "[null]");
+
+    
+    assertEq(JSON.stringify({x: sym}), '{}');
+
+    
+    var replacer = function (key, val) {
+        assertEq(typeof this, "object");
+        if (typeof val === "symbol") {
+            assertEq(val, sym);
+            return "ding";
+        }
+        return val;
+    };
+    assertEq(JSON.stringify(sym, replacer), '"ding"');
+    assertEq(JSON.stringify({x: sym}, replacer), '{"x":"ding"}');
+}
+
+if (typeof reportCompare === 'function')
+    reportCompare(0, 0, 'ok');
