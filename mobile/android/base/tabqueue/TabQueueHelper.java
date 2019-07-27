7a53d6d4c773463485862d74b5fb55845defa1a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:22:57 +0000

    Bug 1129433 - Show &quot;# tabs opened in background&quot; notification in system tray (r=mcomella)
    UltraBlame original commit: 4e033cac2931ca8425706701424afccbe09e4aad

--- mobile/android/base/tabqueue/TabQueueHelper.java
+++ mobile/android/base/tabqueue/TabQueueHelper.java
@@ -5,20 +5,24 @@
 
 package org.mozilla.gecko.tabqueue;
 
-import org.mozilla.gecko.GeckoProfile;
-import org.mozilla.gecko.util.ThreadUtils;
-
-import android.text.TextUtils;
-import android.util.Log;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v4.app.NotificationCompat;
 import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.io.IOException;
+import org.mozilla.gecko.BrowserApp;
+import org.mozilla.gecko.GeckoProfile;
+import org.mozilla.gecko.R;
+import org.mozilla.gecko.util.ThreadUtils;
 
 public class TabQueueHelper {
     private static final String LOGTAG = "Gecko" + TabQueueHelper.class.getSimpleName();
 
     public static final String FILE_NAME = "tab_queue_url_list.json";
+    public static final String LOAD_URLS_ACTION = "TAB_QUEUE_LOAD_URLS_ACTION";
+    public static final int TAB_QUEUE_NOTIFICATION_ID = R.id.tabQueueNotification;
 
     
 
@@ -28,7 +32,8 @@ public class TabQueueHelper {
 
 
 
-    public static void queueURL(final GeckoProfile profile, final String url, final String filename) {
+
+    public static int queueURL(final GeckoProfile profile, final String url, final String filename) {
         ThreadUtils.assertNotOnUiThread();
 
         JSONArray jsonArray = profile.readJSONArrayFromFile(filename);
@@ -36,5 +41,40 @@ public class TabQueueHelper {
         jsonArray.put(url);
 
         profile.writeFile(filename, jsonArray.toString());
+
+        return jsonArray.length();
+    }
+
+    
+
+
+
+
+
+
+    static public void showNotification(Context context, int tabsQueued) {
+        Intent resultIntent = new Intent(context, BrowserApp.class);
+        resultIntent.setAction(TabQueueHelper.LOAD_URLS_ACTION);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        String title, text;
+        final Resources resources = context.getResources();
+        if(tabsQueued == 1) {
+            title = resources.getString(R.string.tab_queue_notification_title_singular);
+            text = resources.getString(R.string.tab_queue_notification_text_singular);
+        } else {
+            title = resources.getString(R.string.tab_queue_notification_title_plural);
+            text = resources.getString(R.string.tab_queue_notification_text_plural, tabsQueued);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                                                                   .setSmallIcon(R.drawable.ic_status_logo)
+                                                                   .setContentTitle(title)
+                                                                   .setContentText(text)
+                                                                   .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(TabQueueHelper.TAB_QUEUE_NOTIFICATION_ID, builder.build());
     }
 }
