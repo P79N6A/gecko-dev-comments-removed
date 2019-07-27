88507da81430512762b78b2f76462f173cc919d1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:31:04 +0000

    Bug 1132301: Part 2 - add navigator.mozLoop methods to allow interaction between Loop and the Social API. r=Standard8,mixedpuppy
    UltraBlame original commit: 7d3633bdd4a33914fccbb1d7d7eced48018d865a

--- browser/components/loop/test/mochitest/head.js
+++ browser/components/loop/test/mochitest/head.js
@@ -71,6 +71,34 @@ function promiseGetMozLoopAPI() {
   });
 }
 
+function waitForCondition(condition, nextTest, errorMsg) {
+  var tries = 0;
+  var interval = setInterval(function() {
+    if (tries >= 30) {
+      ok(false, errorMsg);
+      moveOn();
+    }
+    var conditionPassed;
+    try {
+      conditionPassed = condition();
+    } catch (e) {
+      ok(false, e + "\n" + e.stack);
+      conditionPassed = false;
+    }
+    if (conditionPassed) {
+      moveOn();
+    }
+    tries++;
+  }, 100);
+  var moveOn = function() { clearInterval(interval); nextTest(); };
+}
+
+function promiseWaitForCondition(aConditionFn) {
+  let deferred = Promise.defer();
+  waitForCondition(aConditionFn, deferred.resolve, "Condition didn't pass.");
+  return deferred.promise;
+}
+
 
 
 
