ccbb5ac66ed8d440fe9ca5d624e8504e2747c777
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:57:57 +0000

    Bug 1006320 - Add a marionette test for verifying the functionality BluetoothManager based on bluetooth API v2. r=btian
    UltraBlame original commit: 68efa2e3d9bfb22ad6d9e790df90ae859284065a

---
+++ dom/bluetooth2/tests/marionette/test_dom_BluetoothManager_API2.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+MARIONETTE_TIMEOUT = 60000;
+MARIONETTE_HEAD_JS = 'head.js';
+
+
+
+
+
+
+
+function waitForManagerAttributeChanged() {
+  let deferred = Promise.defer();
+
+  bluetoothManager.onattributechanged = function(aEvent) {
+    if(aEvent.attrs.indexOf("defaultAdapter")) {
+      bluetoothManager.onattributechanged = null;
+      ok(true, "BluetoothManager event 'onattributechanged' got.");
+      deferred.resolve(aEvent);
+    }
+  };
+
+  return deferred.promise;
+}
+
+startBluetoothTestBase(["settings-read", "settings-write"],
+                       function testCaseMain() {
+  let adapters = bluetoothManager.getAdapters();
+  ok(Array.isArray(adapters), "Can not got the array of adapters");
+  ok(adapters.length, "The number of adapters should not be zero");
+  ok(bluetoothManager.defaultAdapter, "defaultAdapter should not be null.");
+});
