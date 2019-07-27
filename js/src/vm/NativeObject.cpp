2aec34e9c95531925296aacfb58ec3f8782c8072
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:56:09 +0000

    Bug 1148750, part 13 - Simplify code to fill in desc.writable, if not present, from the existing shape. r=efaust.
    UltraBlame original commit: fe7d4e55db5542632f640725a96961f2169a5f9e

--- js/src/vm/NativeObject.cpp
+++ js/src/vm/NativeObject.cpp
@@ -1447,16 +1447,10 @@ js::NativeDefineProperty(ExclusiveContext* cx, HandleNativeObject obj, HandleId 
         if (frozen && desc.hasWritable() && desc.writable() && !skipRedefineChecks)
             return result.fail(JSMSG_CANT_REDEFINE_PROP);
 
-        if (desc.hasValue()) {
-            
-            
-            if (IsImplicitDenseOrTypedArrayElement(shape)) {
-                desc.setAttributes(ApplyAttributes(desc.attributes(), true, true,
-                                                   !IsAnyTypedArray(obj)));
-            } else {
-                desc.setAttributes(ApplyOrDefaultAttributes(desc.attributes(), shape));
-            }
-        } else {
+        if (!desc.hasWritable())
+            desc.setWritable(IsWritable(shapeAttrs));
+
+        if (!desc.hasValue()) {
             
             
 
@@ -1468,14 +1462,8 @@ js::NativeDefineProperty(ExclusiveContext* cx, HandleNativeObject obj, HandleId 
                 shape = obj->lookup(cx, id);
             }
 
-            desc.setAttributes(ApplyOrDefaultAttributes(desc.attributes(), shape));
-
             
             
-            
-            uint32_t propMask = JSPROP_ENUMERATE | JSPROP_READONLY | JSPROP_PERMANENT;
-            desc.setAttributes((shape->attributes() & ~propMask) |
-                               (desc.attributes() & propMask));
             desc.setGetter(shape->getter());
             desc.setSetter(shape->setter());
             if (shape->hasSlot())
