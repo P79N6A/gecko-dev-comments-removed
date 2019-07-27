7506409f9d795b800c7077cee5ec89d370feb986
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:33:51 +0000

    Bug 1148770 - Rewrite browser_styleeditor_bug_870339.js to fix intermittent leaks. r=ejpbruel
    UltraBlame original commit: 18ee8f5f75eb8abd31a8732535324a95d1490d03

--- browser/devtools/styleeditor/test/browser_styleeditor_bug_870339.js
+++ browser/devtools/styleeditor/test/browser_styleeditor_bug_870339.js
@@ -1,46 +1,44 @@
 
 
+"use strict";
+const SIMPLE = TEST_BASE_HTTP + "simple.css";
+const DOCUMENT_WITH_ONE_STYLESHEET = "data:text/html;charset=UTF-8," +
+        encodeURIComponent(
+          ["<!DOCTYPE html>",
+           "<html>",
+           " <head>",
+           "  <title>Bug 870339</title>",
+           '  <link rel="stylesheet" type="text/css" href="'+SIMPLE+'">',
+           " </head>",
+           " <body>",
+           " </body>",
+           "</html>"
+          ].join("\n"));
 
-function test()
-{
-  const SIMPLE = TEST_BASE_HTTP + "simple.css";
-  const DOCUMENT_WITH_ONE_STYLESHEET = "data:text/html;charset=UTF-8," +
-          encodeURIComponent(
-            ["<!DOCTYPE html>",
-             "<html>",
-             " <head>",
-             "  <title>Bug 870339</title>",
-             '  <link rel="stylesheet" type="text/css" href="'+SIMPLE+'">',
-             " </head>",
-             " <body>",
-             " </body>",
-             "</html>"
-            ].join("\n"));
+add_task(function* () {
+  let { ui } = yield openStyleEditorForURL(DOCUMENT_WITH_ONE_STYLESHEET);
 
-  waitForExplicitFinish();
-  addTabAndOpenStyleEditors(1, function (aPanel) {
-    let UI = aPanel.UI;
+  
+  
+  const SPAM_COUNT = 2;
+  for (let i=0; i<SPAM_COUNT; ++i) {
+    ui._onNewDocument();
+  }
 
-    
-    
-    const SPAM_COUNT = 2;
-    for (let i=0; i<SPAM_COUNT; ++i) {
-      UI._onNewDocument();
-    }
-
-    
-    
+  
+  
+  yield new Promise(resolve => {
     let loadCount = 0;
-    UI.on("stylesheets-reset", function () {
+    ui.on("stylesheets-reset", function onReset() {
       ++loadCount;
       if (loadCount == SPAM_COUNT) {
+        ui.off("stylesheets-reset", onReset);
         
         
         
-        is(UI.editors.length, 1, "correct style sheet count");
-        finish();
+        is(ui.editors.length, 1, "correct style sheet count");
+        resolve();
       }
     });
   });
-  content.location = DOCUMENT_WITH_ONE_STYLESHEET;
-}
+});
