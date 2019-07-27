0ee377689cbb1ae2eff1f20cec9e675aa6403307
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:03:57 +0000

    Bug 1057966 - Fix Help menu in e10s windows. r=billm.
    The help menu wasn't populating properly because gSafeBrowsing was attempting
    to get the URI for the loaded page by getting content.document.documentURI.
    Since bug 1051017 landed, content is unavailable for e10s windows. We use
    the selected browser's currentURI instead.
    
    UltraBlame original commit: 5b89084d3dd65db7461998db03f554e614a59a79

--- browser/base/content/browser-safebrowsing.js
+++ browser/base/content/browser-safebrowsing.js
@@ -6,9 +6,9 @@
 var gSafeBrowsing = {
 
   setReportPhishingMenu: function() {
-
     
-    var isPhishingPage = content.document.documentURI.startsWith("about:blocked?e=phishingBlocked");
+    var uri = getBrowser().currentURI;
+    var isPhishingPage = uri && uri.spec.startsWith("about:blocked?e=phishingBlocked");
 
     
     document.getElementById("menu_HelpPopup_reportPhishingtoolmenu")
@@ -24,7 +24,6 @@ var gSafeBrowsing = {
     if (!broadcaster)
       return;
 
-    var uri = getBrowser().currentURI;
     if (uri && (uri.schemeIs("http") || uri.schemeIs("https")))
       broadcaster.removeAttribute("disabled");
     else
