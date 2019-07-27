e6881020216202b64c5b0b5a3b65dc581817801a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:03:10 +0000

    Bug 1085246 - Make browser_fxa_login.js ignore the load for the placeholder tab in openFxASettings. r=gavin, r=Mossop, a=me
    UltraBlame original commit: a926116946f8e108599e4d86196b0e7da7bc1644

--- browser/components/loop/test/mochitest/browser_fxa_login.js
+++ browser/components/loop/test/mochitest/browser_fxa_login.js
@@ -421,6 +421,10 @@ add_task(function* openFxASettings() {
   yield new Promise((resolve, reject) => {
     let progressListener = {
       onLocationChange: function onLocationChange(aBrowser) {
+        if (aBrowser.currentURI.spec == BASE_URL) {
+          
+          return;
+        }
         gBrowser.removeTabsProgressListener(progressListener);
         let contentURI = Services.io.newURI(params.content_uri, null, null);
         is(aBrowser.currentURI.spec, Services.io.newURI("/settings", null, contentURI).spec,
