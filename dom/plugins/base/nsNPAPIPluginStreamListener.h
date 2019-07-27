74f05bf92dd2c681c34d281a6cc29903c137a2de
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:16:59 +0000

    Bug 1136930: Fix nsNPAPIPluginStreamListener so that destroystream calls are made during async plugin init; r=jimm
    UltraBlame original commit: 00a32fed278ea562e44b247a696b0cc1add8211b

--- dom/plugins/base/nsNPAPIPluginStreamListener.h
+++ dom/plugins/base/nsNPAPIPluginStreamListener.h
@@ -105,6 +105,14 @@ public:
   void URLRedirectResponse(NPBool allow);
 
 protected:
+
+  enum StreamState
+  {
+    eStreamStopped = 0, 
+    eNewStreamCalled,   
+    eStreamTypeSet      
+  };
+
   virtual ~nsNPAPIPluginStreamListener();
   char* mStreamBuffer;
   char* mNotifyURL;
@@ -113,7 +121,7 @@ protected:
   uint32_t mStreamBufferSize;
   int32_t mStreamBufferByteCount;
   int32_t mStreamType;
-  bool mStreamStarted;
+  StreamState mStreamState;
   bool mStreamCleanedUp;
   bool mCallNotify;
   bool mIsSuspended;
@@ -128,4 +136,4 @@ public:
   nsRefPtr<nsPluginStreamListenerPeer> mStreamListenerPeer;
 };
 
-#endif 
+#endif
