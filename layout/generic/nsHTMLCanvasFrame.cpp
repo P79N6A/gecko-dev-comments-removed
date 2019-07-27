cdbff332c4314ec87ab1a9217c9b5264a57b13f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:14:20 +0000

    Bug 1104354: Give nsHTMLCanvasFrame its own GetIntrinsicSize() impl. r=mats
    UltraBlame original commit: 4772b662ccef0415421aa17073bdee628ffee79f

--- layout/generic/nsHTMLCanvasFrame.cpp
+++ layout/generic/nsHTMLCanvasFrame.cpp
@@ -8,6 +8,7 @@
 #include "nsHTMLCanvasFrame.h"
 
 #include "nsGkAtoms.h"
+#include "mozilla/Assertions.h"
 #include "mozilla/dom/HTMLCanvasElement.h"
 #include "nsDisplayList.h"
 #include "nsLayoutUtils.h"
@@ -20,6 +21,41 @@ using namespace mozilla;
 using namespace mozilla::dom;
 using namespace mozilla::layers;
 using namespace mozilla::gfx;
+
+
+
+
+
+
+
+
+
+static IntrinsicSize
+IntrinsicSizeFromCanvasSize(const nsIntSize& aCanvasSizeInPx)
+{
+  IntrinsicSize intrinsicSize;
+  intrinsicSize.width.SetCoordValue(
+    nsPresContext::CSSPixelsToAppUnits(aCanvasSizeInPx.width));
+  intrinsicSize.height.SetCoordValue(
+    nsPresContext::CSSPixelsToAppUnits(aCanvasSizeInPx.height));
+
+  return intrinsicSize;
+}
+
+
+
+
+
+
+
+
+
+static nsSize
+IntrinsicRatioFromCanvasSize(const nsIntSize& aCanvasSizeInPx)
+{
+  return nsSize(nsPresContext::CSSPixelsToAppUnits(aCanvasSizeInPx.width),
+                nsPresContext::CSSPixelsToAppUnits(aCanvasSizeInPx.height));
+}
 
 class nsDisplayCanvas : public nsDisplayItem {
 public:
@@ -117,6 +153,9 @@ nsHTMLCanvasFrame::GetCanvasSize()
     HTMLCanvasElement::FromContentOrNull(GetContent());
   if (canvas) {
     size = canvas->GetSize();
+    MOZ_ASSERT(size.width >= 0 && size.height >= 0,
+               "we should've required <canvas> width/height attrs to be "
+               "unsigned (non-negative) values");
   } else {
     NS_NOTREACHED("couldn't get canvas size");
   }
@@ -144,12 +183,16 @@ nsHTMLCanvasFrame::GetPrefISize(nsRenderingContext *aRenderingContext)
   return result;
 }
 
+ IntrinsicSize
+nsHTMLCanvasFrame::GetIntrinsicSize()
+{
+  return IntrinsicSizeFromCanvasSize(GetCanvasSize());
+}
+
  nsSize
 nsHTMLCanvasFrame::GetIntrinsicRatio()
 {
-  nsIntSize size(GetCanvasSize());
-  return nsSize(nsPresContext::CSSPixelsToAppUnits(size.width),
-                nsPresContext::CSSPixelsToAppUnits(size.height));
+  return IntrinsicRatioFromCanvasSize(GetCanvasSize());
 }
 
 
