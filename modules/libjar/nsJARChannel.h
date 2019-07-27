54b942657367db4e5b68bbc5d366a78de1d1138b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:57:05 +0000

    Bug 1174078 - Calling "fetch" inside Service Worker's "onfetch" handler in b2g causes "onfetch" again that leads to an infinite loop. r=nsm
    UltraBlame original commit: e8d2c117e4b194f0d3154d51b367af567f8ecd1d

--- modules/libjar/nsJARChannel.h
+++ modules/libjar/nsJARChannel.h
@@ -134,8 +134,11 @@ private:
     nsCString                       mJarEntry;
     nsCString                       mInnerJarEntry;
 
-    nsRefPtr<nsInputStreamPump> mSynthesizedResponsePump;
-    int64_t mSynthesizedStreamLength;
+    nsRefPtr<nsInputStreamPump>     mSynthesizedResponsePump;
+    int64_t                         mSynthesizedStreamLength;
+
+    
+    bool                            mForceNoIntercept;
 
     friend class mozilla::net::InterceptedJARChannel;
 };
