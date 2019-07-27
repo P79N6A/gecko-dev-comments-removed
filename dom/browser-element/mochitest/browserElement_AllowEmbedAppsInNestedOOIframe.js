1488db4c69e821300368e9d537ad7ba52d72fdb4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:05:50 +0000

    Bug 1097479 - Allow nested content process to embed apps and add a nested pref check function. r=kanru
    UltraBlame original commit: 69cff2238ff98ccac986c9f7c15746448d2aa0b4

---
+++ dom/browser-element/mochitest/browserElement_AllowEmbedAppsInNestedOOIframe.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+"use strict";
+
+SimpleTest.waitForExplicitFinish();
+browserElementTestHelpers.setEnabledPref(true);
+browserElementTestHelpers.addPermission();
+
+SpecialPowers.setAllAppsLaunchable(true);
+
+function runTest() {
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('mozbrowser', 'true');
+  iframe.addEventListener('mozbrowsershowmodalprompt', function(e) {
+    is(e.detail.message == 'app', true, e.detail.message);
+    SimpleTest.finish();
+  });
+
+  document.body.appendChild(iframe);
+
+  var context = { 'url': 'http://example.org',
+                  'appId': SpecialPowers.Ci.nsIScriptSecurityManager.NO_APP_ID,
+                  'isInBrowserElement': true };
+  SpecialPowers.pushPermissions([
+    {'type': 'browser', 'allow': 1, 'context': context},
+    {'type': 'embed-apps', 'allow': 1, 'context': context}
+  ], function() {
+    iframe.src = 'http://example.org/tests/dom/browser-element/mochitest/file_browserElement_AllowEmbedAppsInNestedOOIframe.html';
+  });
+}
+
+addEventListener('testready', () => {
+  SpecialPowers.pushPrefEnv({"set": [["dom.ipc.tabs.nested.enabled", true]]}, runTest);
+});
