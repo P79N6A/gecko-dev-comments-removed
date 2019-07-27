5fdae6c9def659b7c3420950a52be36e13462415
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:19:12 +0000

    Bug 1083344 - Tighten rules for Mac OS content process sandbox on 10.9 and 10.10. r=smichaud Allow read to whole filesystem until chrome:// and file:// URLs are brokered through another process. Except $HOME/Library in which we allow only access to profile add-ons subdir. Add level 2, which allows read only from $HOME and /tmp (while still restricting $HOME/Library. Change default back to 1.
    UltraBlame original commit: 9af90dce2c8364a1c7f28638b0dbce94afcec680

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -285,7 +285,7 @@ pref("browser.slowStartup.maxSamples", 5);
 pref("browser.aboutHomeSnippets.updateUrl", "https://snippets.cdn.mozilla.net/%STARTPAGE_VERSION%/%NAME%/%VERSION%/%APPBUILDID%/%BUILD_TARGET%/%LOCALE%/%CHANNEL%/%OS_VERSION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%/");
 
 pref("browser.enable_automatic_image_resizing", true);
-pref("browser.casting.enabled", false);
+pref("browser.casting.enabled", true);
 pref("browser.chrome.site_icons", true);
 pref("browser.chrome.favicons", true);
 
@@ -1231,7 +1231,7 @@ pref("security.sandbox.windows.log.stackTraceDepth", 0);
 
 
 
-pref("security.sandbox.content.level", 0);
+pref("security.sandbox.content.level", 1);
 #endif
 
 
