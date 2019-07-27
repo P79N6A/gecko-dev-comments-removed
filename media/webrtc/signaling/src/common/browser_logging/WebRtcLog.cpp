7e92d1b9b6c43cb40f2f709ac773e8f3ac4ab245
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:18:24 +0000

    Bug 1163859: Only update webrtc.debug prefs from gecko thread. r=rjesup
    UltraBlame original commit: ad07670653a29076602ae2acf2867bbb28729dd9

--- media/webrtc/signaling/src/common/browser_logging/WebRtcLog.cpp
+++ media/webrtc/signaling/src/common/browser_logging/WebRtcLog.cpp
@@ -11,6 +11,7 @@
 #include "nscore.h"
 #ifdef MOZILLA_INTERNAL_API
 #include "nsString.h"
+#include "nsXULAppAPI.h"
 #if !defined(MOZILLA_XPCOMRT_API)
 #include "mozilla/Preferences.h"
 #endif 
@@ -146,9 +147,11 @@ void ConfigWebRtcLog(uint32_t trace_mask, nsCString &aLogFile, nsCString &aAECLo
     }
   }
 #if !defined(MOZILLA_EXTERNAL_LINKAGE)
-  
-  mozilla::Preferences::SetCString("media.webrtc.debug.log_file", aLogFile);
-  mozilla::Preferences::SetCString("media.webrtc.debug.aec_log_dir", aAECLogDir);
+  if (XRE_GetProcessType() == GeckoProcessType_Default) {
+    
+    mozilla::Preferences::SetCString("media.webrtc.debug.log_file", aLogFile);
+    mozilla::Preferences::SetCString("media.webrtc.debug.aec_log_dir", aAECLogDir);
+  }
 #endif
   return;
 }
