6dd8e288ebc23e1084ceda70939ef68bf24a6831
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:56:43 +0000

    Bug 1112212 - Implement dummy-key-events mode for Android; r=esawin
    UltraBlame original commit: 0a03c1ef13d098287902a197db167b23577dcb14

--- widget/android/nsWindow.cpp
+++ widget/android/nsWindow.cpp
@@ -1681,6 +1681,23 @@ nsWindow::RemoveIMEComposition()
     DispatchEvent(&compositionCommitEvent);
 }
 
+
+
+
+
+
+void
+nsWindow::SendIMEDummyKeyEvents()
+{
+    WidgetKeyboardEvent downEvent(true, NS_KEY_DOWN, this);
+    MOZ_ASSERT(downEvent.keyCode == 0);
+    DispatchEvent(&downEvent);
+
+    WidgetKeyboardEvent upEvent(true, NS_KEY_UP, this);
+    MOZ_ASSERT(upEvent.keyCode == 0);
+    DispatchEvent(&upEvent);
+}
+
 void
 nsWindow::OnIMEEvent(AndroidGeckoEvent *ae)
 {
@@ -1837,6 +1854,10 @@ nsWindow::OnIMEEvent(AndroidGeckoEvent *ae)
                         true, NS_COMPOSITION_COMMIT_AS_IS, this);
                 InitEvent(compositionCommitEvent, nullptr);
                 DispatchEvent(&compositionCommitEvent);
+            }
+
+            if (mInputContext.mMayBeIMEUnaware) {
+                SendIMEDummyKeyEvents();
             }
 
             FlushIMEChanges();
@@ -2087,8 +2108,6 @@ nsWindow::SetInputContext(const InputContext& aContext,
             aContext.mIMEState.mEnabled, aContext.mIMEState.mOpen,
             aAction.mCause, aAction.mFocusChange);
 
-    mInputContext = aContext;
-
     
     
     if (aContext.mIMEState.mEnabled != IMEState::DISABLED && 
@@ -2108,6 +2127,7 @@ nsWindow::SetInputContext(const InputContext& aContext,
         enabled = IMEState::DISABLED;
     }
 
+    mInputContext = aContext;
     mInputContext.mIMEState.mEnabled = enabled;
 
     if (enabled == IMEState::ENABLED && aAction.UserMightRequestOpenVKB()) {
