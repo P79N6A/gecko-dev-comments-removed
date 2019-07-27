1f8c7926b1f5dcb7645c5963bf95bbbfa7eb472a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:12:50 +0000

    Bug 1097694 - js::HasOwnProperty sanity surgery. r=efaust.
    *   Rename the clownshoes js::HasOwnProperty signature to js::NonProxyLookupOwnProperty,
        which is what it really is.
    
    *   Change the sane js::HasOwnProperty signature to use the hasOwn handler when the
        argument is a proxy, as that's what it's there for.
    
    *   Change the DirectProxyHandler::hasOwn implementation to use js::HasOwnProperty, so
        that when target is also a proxy, we end up calling its hasOwn handler.  Similar
        changes in ScriptedDirectProxyHandler.cpp.
    
    UltraBlame original commit: ff97ac763705dd01700fc605500047a1bd374297

--- js/src/proxy/ScriptedDirectProxyHandler.cpp
+++ js/src/proxy/ScriptedDirectProxyHandler.cpp
@@ -143,16 +143,6 @@ IsSealed(JSContext* cx, HandleObject obj, HandleId id, bool *bp)
     return true;
 }
 
-static bool
-HasOwn(JSContext *cx, HandleObject obj, HandleId id, bool *bp)
-{
-    Rooted<PropertyDescriptor> desc(cx);
-    if (!JS_GetPropertyDescriptorById(cx, obj, id, &desc))
-        return false;
-    *bp = (desc.object() == obj);
-    return true;
-}
-
 
 static JSObject *
 GetDirectProxyHandlerObject(JSObject *proxy)
@@ -209,7 +199,7 @@ ArrayToIdVector(JSContext *cx, HandleObject proxy, HandleObject target, HandleVa
 
         
         bool isFixed;
-        if (!HasOwn(cx, target, id, &isFixed))
+        if (!HasOwnProperty(cx, target, id, &isFixed))
             return false;
 
         
@@ -256,7 +246,7 @@ ArrayToIdVector(JSContext *cx, HandleObject proxy, HandleObject target, HandleVa
 
         
         bool isFixed;
-        if (!HasOwn(cx, target, id, &isFixed))
+        if (!HasOwnProperty(cx, target, id, &isFixed))
             return false;
 
         
