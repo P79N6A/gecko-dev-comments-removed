d347cbb6a9be841b11a9b89ac9c073de5287272c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:19:19 +0000

    Bug 1060090 part 3: Refactor getComputedStyle implementation for 'background-position' to use helper-method for <position> & its sub-parts. r=heycam
    UltraBlame original commit: d8175b16840e11cf18157eb33e43a9fda049c6bc

--- layout/style/nsComputedDOMStyle.h
+++ layout/style/nsComputedDOMStyle.h
@@ -519,9 +519,16 @@ private:
   mozilla::dom::CSSValue* DoGetCustomProperty(const nsAString& aPropertyName);
 
   nsDOMCSSValueList* GetROCSSValueList(bool aCommaDelimited);
+
+  
   void SetToRGBAColor(nsROCSSPrimitiveValue* aValue, nscolor aColor);
   void SetValueToStyleImage(const nsStyleImage& aStyleImage,
                             nsROCSSPrimitiveValue* aValue);
+  void SetValueToPositionCoord(
+    const nsStyleBackground::Position::PositionCoord& aCoord,
+    nsROCSSPrimitiveValue* aValue);
+  void SetValueToPosition(const nsStyleBackground::Position& aPosition,
+                          nsDOMCSSValueList* aValueList);
 
   
 
