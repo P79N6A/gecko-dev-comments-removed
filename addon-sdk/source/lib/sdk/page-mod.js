02303be7a3e91414da38b4d1181da855b04f2594
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:01:44 +0000

    Backed out changeset 434a86b48ee0 (bug 1129567) because it wasn't meant to land on trunk.
    UltraBlame original commit: 7a3357ffc0e5aa966f34e4b715697975322caa9f

--- addon-sdk/source/lib/sdk/page-mod.js
+++ addon-sdk/source/lib/sdk/page-mod.js
@@ -14,11 +14,10 @@ const { getAttachEventType, WorkerHost } = require('./content/utils');
 const { Class } = require('./core/heritage');
 const { Disposable } = require('./core/disposable');
 const { WeakReference } = require('./core/reference');
-const { Worker } = require('./deprecated/sync-worker');
+const { Worker } = require('./content/worker');
 const { EventTarget } = require('./event/target');
 const { on, emit, once, setListeners } = require('./event/core');
 const { on: domOn, removeListener: domOff } = require('./dom/events');
-const { pipe } = require('./event/utils');
 const { isRegExp, isUndefined } = require('./lang/type');
 const { merge } = require('./util/object');
 const { windowIterator } = require('./deprecated/window-utils');
@@ -114,7 +113,6 @@ const PageMod = Class({
     modContract.properties(modelFor),
     EventTarget,
     Disposable,
-    WeakReference
   ],
   extends: WorkerHost(workerFor),
   setup: function PageMod(options) {
@@ -191,6 +189,10 @@ function applyOnExistingDocuments (mod) {
   getTabs().forEach(tab => {
     
     let window = getTabContentWindow(tab);
+    
+    
+    if (!window || !window.frames)
+      return;
     let uri = getTabURI(tab);
     if (has(mod.attachTo, "top") && modMatchesURI(mod, uri))
       onContent(mod, window);
@@ -213,11 +215,15 @@ function createWorker (mod, window) {
     onError: (e) => emit(mod, 'error', e)
   });
   workers.set(mod, worker);
-  pipe(worker, mod);
-  emit(mod, 'attach', worker);
-  once(worker, 'detach', function detach() {
-    worker.destroy();
-  });
+  worker.on('*', (event, ...args) => {
+    
+    
+    if (event === 'attach')
+      emit(mod, event, worker)
+    else
+      emit(mod, event, ...args);
+  })
+  once(worker, 'detach', () => worker.destroy());
 }
 
 function onContent (mod, window) {
@@ -256,6 +262,20 @@ function onContent (mod, window) {
       return;
     domOff(window, eventName, onReady, true);
     createWorker(mod, window);
+
+    
+    
+    if (window.document.readyState == "complete") {
+      mod.on('attach', worker => {
+        try {
+          worker.send('pageshow');
+          emit(worker, 'pageshow');
+        }
+        catch (e) {
+          
+        }
+      });
+    }
   }, true);
 }
 
