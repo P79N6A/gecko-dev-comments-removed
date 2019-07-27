a2717c6b45d734c88e5cccf5aa0728a9eaf36129
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:09:06 +0000

    Bug 960465 patch 18 - Remove aElementOrPseudoElement parameter to ReparentStyleContext (added in bug 1057129).  r=birtles
    UltraBlame original commit: a4225263cb35129a967ee89e4e92dd263fb7d758

--- layout/style/nsStyleSet.h
+++ layout/style/nsStyleSet.h
@@ -247,16 +247,10 @@ class nsStyleSet
   
   
   
-  
-  
-  
-  
-  
   already_AddRefed<nsStyleContext>
   ReparentStyleContext(nsStyleContext* aStyleContext,
                        nsStyleContext* aNewParentContext,
-                       mozilla::dom::Element* aElement,
-                       mozilla::dom::Element* aElementOrPseudoElement);
+                       mozilla::dom::Element* aElement);
 
   
   bool HasDocumentStateDependentStyle(nsPresContext* aPresContext,
