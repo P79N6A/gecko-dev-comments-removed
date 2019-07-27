e589b6f2c31106c9a0a2f41f6da1a4d281a54ac8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:48:01 +0000

    Bug 1024015 - Only enable HTTP response timeout when TCP Keepalives are disabled for HTTP r=mcmanus
    UltraBlame original commit: 2700ebff7d64911ec97a62668508af24e3e5bdc6

--- netwerk/test/unit/test_httpResponseTimeout.js
+++ netwerk/test/unit/test_httpResponseTimeout.js
@@ -1,17 +1,28 @@
+
+
+
+
+
+
+"use strict";
+
 Cu.import("resource://testing-common/httpd.js");
 
 var baseURL;
 const kResponseTimeoutPref = "network.http.response.timeout";
 const kResponseTimeout = 1;
+const kShortLivedKeepalivePref =
+  "network.http.tcp_keepalive.short_lived_connections";
+const kLongLivedKeepalivePref =
+  "network.http.tcp_keepalive.long_lived_connections";
 
 const prefService = Cc["@mozilla.org/preferences-service;1"]
                        .getService(Ci.nsIPrefBranch);
 
 var server = new HttpServer();
 
-function TimeoutListener(enableTimeout, nextTest) {
-  this.enableTimeout = enableTimeout;
-  this.nextTest = nextTest;
+function TimeoutListener(expectResponse) {
+  this.expectResponse = expectResponse;
 }
 
 TimeoutListener.prototype = {
@@ -22,17 +33,13 @@ TimeoutListener.prototype = {
   },
 
   onStopRequest: function (request, ctx, status) {
-    if (this.enableTimeout) {
-      do_check_eq(status, Cr.NS_ERROR_NET_TIMEOUT);
-    } else {
+    if (this.expectResponse) {
       do_check_eq(status, Cr.NS_OK);
+    } else {
+      do_check_eq(status, Cr.NS_ERROR_NET_TIMEOUT);
     }
 
-    if (this.nextTest) {
-      this.nextTest();
-    } else {
-      server.stop(serverStopListener);
-    }
+    run_next_test();
   },
 };
 
@@ -40,7 +47,7 @@ function serverStopListener() {
   do_test_finished();
 }
 
-function testTimeout(timeoutEnabled, nextTest) {
+function testTimeout(timeoutEnabled, expectResponse) {
   
   if (timeoutEnabled) {
     prefService.setIntPref(kResponseTimeoutPref, kResponseTimeout);
@@ -52,19 +59,82 @@ function testTimeout(timeoutEnabled, nextTest) {
   .getService(Ci.nsIIOService);
   var chan = ios.newChannel(baseURL, null, null)
   .QueryInterface(Ci.nsIHttpChannel);
-  var listener = new TimeoutListener(timeoutEnabled, nextTest);
+  var listener = new TimeoutListener(expectResponse);
   chan.asyncOpen(listener, null);
 }
 
 function testTimeoutEnabled() {
-  testTimeout(true, testTimeoutDisabled);
+  
+  testTimeout(true, false);
 }
 
 function testTimeoutDisabled() {
-  testTimeout(false, null);
+  
+  testTimeout(false, true);
 }
 
-function run_test() {
+function testTimeoutDisabledByShortLivedKeepalives() {
+  
+  prefService.setBoolPref(kShortLivedKeepalivePref, true);
+  prefService.setBoolPref(kLongLivedKeepalivePref, false);
+
+  
+  testTimeout(true, true);
+}
+
+function testTimeoutDisabledByLongLivedKeepalives() {
+  
+  prefService.setBoolPref(kShortLivedKeepalivePref, false);
+  prefService.setBoolPref(kLongLivedKeepalivePref, true);
+
+  
+  testTimeout(true, true);
+}
+
+function testTimeoutDisabledByBothKeepalives() {
+  
+  prefService.setBoolPref(kShortLivedKeepalivePref, true);
+  prefService.setBoolPref(kLongLivedKeepalivePref, true);
+
+  
+  testTimeout(true, true);
+}
+
+function setup_tests() {
+  
+  
+  if (prefService.getBoolPref(kShortLivedKeepalivePref)) {
+    prefService.setBoolPref(kShortLivedKeepalivePref, false);
+    do_register_cleanup(function() {
+      prefService.setBoolPref(kShortLivedKeepalivePref, true);
+    });
+  }
+  if (prefService.getBoolPref(kLongLivedKeepalivePref)) {
+    prefService.setBoolPref(kLongLivedKeepalivePref, false);
+    do_register_cleanup(function() {
+      prefService.setBoolPref(kLongLivedKeepalivePref, true);
+    });
+  }
+
+  var tests = [
+    
+    testTimeoutEnabled,
+    
+    testTimeoutDisabled,
+    
+    testTimeoutDisabledByShortLivedKeepalives,
+    
+    testTimeoutDisabledByLongLivedKeepalives,
+    
+    testTimeoutDisabledByBothKeepalives
+  ];
+
+  for (var i=0; i < tests.length; i++) {
+    add_test(tests[i]);
+  }
+}
+
+function setup_http_server() {
   
   server.start(-1);
   baseURL = "http://localhost:" + server.identity.primaryPort + "/";
@@ -79,11 +149,15 @@ function run_test() {
       response.finish();
     });
   });
-
-  
-  
-  testTimeoutEnabled();
-
-  do_test_pending();
+  do_register_cleanup(function() {
+    server.stop(serverStopListener);
+  });
 }
 
+function run_test() {
+  setup_http_server();
+
+  setup_tests();
+
+  run_next_test();
+}
