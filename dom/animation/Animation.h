fe5ea57bee7904bf8521e9e8a50b8feedbaa9fb9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:00:59 +0000

    Bug 1150807 part 2 - Expose Animation::Cancel in WebIDL; r=smaug
    UltraBlame original commit: 72571965f94d3fa4d9624ef06e765a80c1e3c1ce

--- dom/animation/Animation.h
+++ dom/animation/Animation.h
@@ -94,6 +94,7 @@ public:
   AnimationPlayState PlayState() const;
   virtual Promise* GetReady(ErrorResult& aRv);
   virtual Promise* GetFinished(ErrorResult& aRv);
+  void Cancel();
   virtual void Finish(ErrorResult& aRv);
   virtual void Play(LimitBehavior aLimitBehavior);
   virtual void Pause();
@@ -200,8 +201,6 @@ public:
 
 
   Nullable<TimeDuration> GetCurrentOrPendingStartTime() const;
-
-  void Cancel();
 
   const nsString& Name() const
   {
