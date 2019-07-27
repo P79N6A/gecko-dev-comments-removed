587a4d544bc0a2d15bd701d126d01c9f13006036
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:41:17 +0000

    Bug 1102907 - Script for allowing web-platform-tests changes to be upstreamed, r=ahal
    This extends the upstream update script with steps for pushing
    local changes to upstream. The general approach is to look for all
    commits to the tests directory since the last sync, rewrite those
    so they apply to upstream at the last sync point, then rebase onto
    the sync commit, before creating and merging a PR for each in turn.
    
    UltraBlame original commit: 64e1d633f7de66cf4d01d9907ffe6d251cd777d9

---
+++ testing/web-platform/update/__init__.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+import os
+import subprocess
+import sys
+
+from mozlog.structured import structuredlog
+
+here = os.path.split(__file__)[0]
+
+sys.path.insert(0, os.path.abspath(os.path.join(here, os.pardir, "harness")))
+sys.path.insert(0, os.path.abspath(os.path.join(here, os.pardir, "tests", "tools", "scripts")))
+
+from wptrunner.update import setup_logging, WPTUpdate
+from wptrunner.update.base import exit_unclean
+
+from . import updatecommandline
+from .update import UpdateRunner
+
+def run_update(logger, **kwargs):
+    updater = WPTUpdate(logger, runner_cls=UpdateRunner, **kwargs)
+    return updater.run()
+
+
+if __name__ == "__main__":
+    args = updatecommandline.parse_args()
+    logger = setup_logging(args, {})
+    assert structuredlog.get_default_logger() is not None
+
+
+    rv = run_update(logger, **args)
+    if rv is exit_unclean:
+        sys.exit(1)
+    else:
+        sys.exit(0)
