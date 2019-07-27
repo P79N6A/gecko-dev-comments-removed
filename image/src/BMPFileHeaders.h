5b218da55e80f65d22e375ea9c146ebbc2c9b93e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:41:55 +0000

    Bug 1102048 (Part 03) - Make image/src files comply with the Mozilla Coding Style Guide. r=seth
    UltraBlame original commit: 5ab8bc709c1bbf2695becfe07894be8f11c1a4e3

--- image/src/BMPFileHeaders.h
+++ image/src/BMPFileHeaders.h
@@ -2,20 +2,20 @@
 
 
 
-#ifndef MOZILLA_IMAGELIB_BMPHEADERS_H_
-#define MOZILLA_IMAGELIB_BMPHEADERS_H_
+#ifndef mozilla_image_src_BMPFileHeaders_h
+#define mozilla_image_src_BMPFileHeaders_h
 
 namespace mozilla {
-  namespace image {
+namespace image {
 
-    struct BMPFILEHEADER {
-      char signature[2]; 
-      uint32_t filesize;
-      int32_t reserved; 
-      uint32_t dataoffset; 
+struct BMPFILEHEADER {
+  char signature[2];   
+  uint32_t filesize;
+  int32_t reserved;    
+  uint32_t dataoffset; 
 
-      uint32_t bihsize;
-    };
+  uint32_t bihsize;
+};
 
 
 #define BFH_LENGTH 14
@@ -27,9 +27,12 @@ namespace mozilla {
 #define WIN_V3_INTERNAL_BIH_LENGTH 36
 #define WIN_V5_INTERNAL_BIH_LENGTH 120
 
-#define OS2_BIH_LENGTH 12 // This is the real BIH size (as contained in the bihsize field of BMPFILEHEADER)
-#define WIN_V3_BIH_LENGTH 40 // This is the real BIH size (as contained in the bihsize field of BMPFILEHEADER)
-#define WIN_V5_BIH_LENGTH 124 // This is the real BIH size (as contained in the bihsize field of BMPFILEHEADER)
+#define OS2_BIH_LENGTH 12     // This is the real BIH size (as contained in the
+                              
+#define WIN_V3_BIH_LENGTH 40  // This is the real BIH size (as contained in the
+                              
+#define WIN_V5_BIH_LENGTH 124 // This is the real BIH size (as contained in the
+                              
 
 #define OS2_HEADER_LENGTH (BFH_INTERNAL_LENGTH + OS2_INTERNAL_BIH_LENGTH)
 #define WIN_V3_HEADER_LENGTH (BFH_INTERNAL_LENGTH + WIN_V3_INTERNAL_BIH_LENGTH)
@@ -39,62 +42,63 @@ namespace mozilla {
 #define LCS_sRGB 0x73524742
 #endif
 
-    struct xyz {
-      int32_t x, y, z;
-    };
+struct xyz {
+  int32_t x, y, z;
+};
 
-    struct xyzTriple {
-      xyz r, g, b;
-    };
+struct xyzTriple {
+  xyz r, g, b;
+};
 
-    struct BITMAPV5HEADER {
-      int32_t width; 
-      int32_t height; 
-      uint16_t planes; 
-      uint16_t bpp; 
-      
-      uint32_t compression; 
-      uint32_t image_size; 
-      uint32_t xppm; 
-      uint32_t yppm; 
-      uint32_t colors; 
-      uint32_t important_colors; 
-      uint32_t red_mask;   
-      uint32_t green_mask; 
-      uint32_t blue_mask;  
-      uint32_t alpha_mask; 
-      uint32_t color_space; 
-      
-      xyzTriple white_point; 
-      uint32_t gamma_red;   
-      uint32_t gamma_green; 
-      uint32_t gamma_blue;  
-      uint32_t intent; 
-      
-      uint32_t profile_offset; 
-      uint32_t profile_size; 
-      uint32_t reserved; 
-    };
+struct BITMAPV5HEADER {
+  int32_t width;             
+  int32_t height;            
+  uint16_t planes;           
+  uint16_t bpp;              
+  
+  uint32_t compression;      
+  uint32_t image_size;       
+                             
+  uint32_t xppm;             
+  uint32_t yppm;             
+  uint32_t colors;           
+  uint32_t important_colors; 
+  uint32_t red_mask;         
+  uint32_t green_mask;       
+  uint32_t blue_mask;        
+  uint32_t alpha_mask;       
+  uint32_t color_space;      
+  
+  xyzTriple white_point;     
+  uint32_t gamma_red;        
+  uint32_t gamma_green;      
+  uint32_t gamma_blue;       
+  uint32_t intent;           
+  
+  uint32_t profile_offset;   
+  uint32_t profile_size;     
+  uint32_t reserved;         
+};
 
-    struct colorTable {
-      uint8_t red;
-      uint8_t green;
-      uint8_t blue;
-    };
+struct colorTable {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+};
 
-    struct bitFields {
-      uint32_t red;
-      uint32_t green;
-      uint32_t blue;
-      uint8_t redLeftShift;
-      uint8_t redRightShift;
-      uint8_t greenLeftShift;
-      uint8_t greenRightShift;
-      uint8_t blueLeftShift;
-      uint8_t blueRightShift;
-    };
+struct bitFields {
+  uint32_t red;
+  uint32_t green;
+  uint32_t blue;
+  uint8_t redLeftShift;
+  uint8_t redRightShift;
+  uint8_t greenLeftShift;
+  uint8_t greenRightShift;
+  uint8_t blueLeftShift;
+  uint8_t blueRightShift;
+};
 
-  } 
+} 
 } 
 
 #define BITFIELD_LENGTH 12 // Length of the bitfields structure in the bmp file
@@ -130,9 +134,11 @@ enum ERLEState {
   eRLEStateInitial,
   eRLEStateNeedSecondEscapeByte,
   eRLEStateNeedXDelta,
-  eRLEStateNeedYDelta,    
-  eRLEStateAbsoluteMode,  
+  eRLEStateNeedYDelta,        
+  eRLEStateAbsoluteMode,      
+                              
   eRLEStateAbsoluteModePadded 
+                              
 };
 
-#endif
+#endif 
--- image/src/ICOFileHeaders.h
+++ image/src/ICOFileHeaders.h
@@ -3,79 +3,79 @@
 
 
 
-#ifndef MOZILLA_IMAGELIB_ICOHEADERS_H_
-#define MOZILLA_IMAGELIB_ICOHEADERS_H_
+#ifndef mozilla_image_src_ICOFileHeaders_h
+#define mozilla_image_src_ICOFileHeaders_h
 
 namespace mozilla {
-  namespace image {
+namespace image {
 
-    #define ICONFILEHEADERSIZE 6
-    #define ICODIRENTRYSIZE 16
-    #define PNGSIGNATURESIZE 8
-    #define BMPFILEHEADERSIZE 14
-
-    
-
-
-
-    struct IconFileHeader
-    {
-      
-
-
-      uint16_t   mReserved;
-      
-
-
-
-      uint16_t   mType;
-      
-
-
-      uint16_t   mCount;
-    };
-
-    
+#define ICONFILEHEADERSIZE 6
+#define ICODIRENTRYSIZE 16
+#define PNGSIGNATURESIZE 8
+#define BMPFILEHEADERSIZE 14
 
 
 
 
 
-    struct IconDirEntry
-    {
-      uint8_t   mWidth;
-      uint8_t   mHeight;
-      
+struct IconFileHeader
+{
+  
+
+
+  uint16_t   mReserved;
+  
 
 
 
-      uint8_t   mColorCount;
-      
+  uint16_t   mType;
+  
 
 
-      uint8_t   mReserved;
-      union {
-        uint16_t mPlanes;   
-        uint16_t mXHotspot; 
-      };
-      union {
-        uint16_t mBitCount; 
-        uint16_t mYHotspot; 
-      };
-      
+  uint16_t   mCount;
+};
 
 
 
-      uint32_t  mBytesInRes;
-      
 
 
 
-      uint32_t  mImageOffset;
-    };
+
+struct IconDirEntry
+{
+  uint8_t   mWidth;
+  uint8_t   mHeight;
+  
 
 
-  } 
+
+  uint8_t   mColorCount;
+  
+
+
+  uint8_t   mReserved;
+  union {
+    uint16_t mPlanes;   
+    uint16_t mXHotspot; 
+  };
+  union {
+    uint16_t mBitCount; 
+    uint16_t mYHotspot; 
+  };
+  
+
+
+
+  uint32_t  mBytesInRes;
+  
+
+
+
+  uint32_t  mImageOffset;
+};
+
+
+} 
 } 
 
-#endif
+#endif 
