e579fdc36c5fb09a430413c9c44aef513e85ef55
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:02:06 +0000

    Bug 1050108 - Avoid race condition during memory report collection. r=njn
    UltraBlame original commit: b17fa4f3eae4b9488ce4e8e06b7a995db93d7d76

--- toolkit/components/url-classifier/nsUrlClassifierPrefixSet.h
+++ toolkit/components/url-classifier/nsUrlClassifierPrefixSet.h
@@ -19,6 +19,7 @@
 #include "mozilla/Mutex.h"
 #include "mozilla/CondVar.h"
 #include "mozilla/FileUtils.h"
+#include "mozilla/Atomics.h"
 
 class nsUrlClassifierPrefixSet MOZ_FINAL
   : public nsIUrlClassifierPrefixSet
@@ -35,11 +36,10 @@ public:
   NS_IMETHOD LoadFromFile(nsIFile* aFile);
   NS_IMETHOD StoreToFile(nsIFile* aFile);
 
+  size_t SizeInMemory() { return mMemoryInUse; };
+
   NS_DECL_THREADSAFE_ISUPPORTS
   NS_DECL_NSIMEMORYREPORTER
-
-  
-  size_t SizeOfIncludingThis(mozilla::MallocSizeOf mallocSizeOf);
 
 protected:
   virtual ~nsUrlClassifierPrefixSet();
@@ -52,6 +52,9 @@ protected:
   uint32_t BinSearch(uint32_t start, uint32_t end, uint32_t target);
   nsresult LoadFromFd(mozilla::AutoFDClose& fileFd);
   nsresult StoreToFd(mozilla::AutoFDClose& fileFd);
+
+  
+  size_t SizeOfIncludingThis(mozilla::MallocSizeOf mallocSizeOf);
 
   
   
@@ -67,6 +70,9 @@ protected:
   
   uint32_t mTotalPrefixes;
 
+  
+  
+  mozilla::Atomic<size_t> mMemoryInUse;
   nsCString mMemoryReportPath;
 };
 
