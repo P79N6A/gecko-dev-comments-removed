1264332ecef4475be9d5b8b0bbff529a158b98d5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:43:26 +0000

    Bug 1051715 - Part 1: Add temporary clir test. r=hsinyi
    UltraBlame original commit: af8ce1cffd8c0c986d98e76bb6a9426c0a79e55b

---
+++ dom/telephony/test/marionette/test_temporary_clir.js
@@ -0,0 +1,37 @@
+
+
+
+MARIONETTE_TIMEOUT = 60000;
+MARIONETTE_HEAD_JS = 'head.js';
+
+const clirPrefix = "*31#";
+const number = "0912345678";
+
+startTest(function() {
+  let outCall;
+
+  telephony.dial(clirPrefix + number).then(call => {
+    outCall = call;
+
+    ok(call);
+    is(call.id.number, number);
+    is(call.state, "dialing");
+
+    let deferred = Promise.defer();
+
+    call.onalerting = function onalerting(event) {
+      call.onalerting = null;
+      log("Received 'onalerting' call event.");
+      is(call.id.number, number);
+      deferred.resolve(call);
+    };
+
+    return deferred.promise;
+  })
+  .then(() => gRemoteHangUp(outCall))
+  
+  .then(null, error => {
+    ok(false, 'promise rejects during test.');
+  })
+  .then(finish);
+});
