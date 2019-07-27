5c304c5ffd4d733a03b32a16023f5b6f4504980f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:41:16 +0000

    Bug 1041126 - Set singleton types on interned object literals and fix cloning and XDRing singleton-typed objects. (r=bhackett)
    UltraBlame original commit: cd9c422f6bda3b9386835d676b9d442fe42e5144

--- js/src/vm/Xdr.h
+++ js/src/vm/Xdr.h
@@ -28,7 +28,7 @@ namespace js {
 
 
 
-static const uint32_t XDR_BYTECODE_VERSION = uint32_t(0xb973c0de - 179);
+static const uint32_t XDR_BYTECODE_VERSION = uint32_t(0xb973c0de - 180);
 
 class XDRBuffer {
   public:
