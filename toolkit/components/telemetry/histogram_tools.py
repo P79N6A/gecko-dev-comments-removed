2c96158a0cd0ee4e7374f09e526f2632dcd1e9ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:38:36 +0000

    Bug 1168409 - part 2 - avoiding importing usecounters in histogram_tools.py; r=gfritzsche
    UltraBlame original commit: 1b20b43558e980a21b4d244e577b706ee74f6195

--- toolkit/components/telemetry/histogram_tools.py
+++ toolkit/components/telemetry/histogram_tools.py
@@ -24,8 +24,6 @@ except ImportError:
     
     pass
 
-import usecounters
-
 from collections import OrderedDict
 
 def table_dispatch(kind, table, body):
@@ -283,8 +281,16 @@ def from_UseCounters_conf(filename):
 
 FILENAME_PARSERS = {
     'Histograms.json': from_Histograms_json,
-    'UseCounters.conf': from_UseCounters_conf,
 }
+
+
+
+try:
+    import usecounters
+
+    FILENAME_PARSERS['UseCounters.conf'] = from_UseCounters_conf
+except ImportError:
+    pass
 
 def from_files(filenames):
     """Return an iterator that provides a sequence of Histograms for
