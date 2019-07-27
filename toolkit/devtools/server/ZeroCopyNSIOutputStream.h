fb07b17252efd5d522929c3b11d2639113fe4849
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:34:39 +0000

    Bug 1024774 - Part 2: Implement a google::protobuf::ZeroCopyOutputStream wrapper around nsIOutputStream; r=jimb
    UltraBlame original commit: c16549c293bcf5acbeb65d81aab218e983c14006

---
+++ toolkit/devtools/server/ZeroCopyNSIOutputStream.h
@@ -0,0 +1,70 @@
+
+
+
+
+
+#ifndef mozilla_devtools_ZeroCopyNSIOutputStream__
+#define mozilla_devtools_ZeroCopyNSIOutputStream__
+
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/stubs/common.h>
+
+#include "nsCOMPtr.h"
+#include "nsIOutputStream.h"
+
+namespace mozilla {
+namespace devtools {
+
+
+
+
+
+
+
+
+class MOZ_STACK_CLASS ZeroCopyNSIOutputStream
+  : public ::google::protobuf::io::ZeroCopyOutputStream
+{
+  static const int BUFFER_SIZE = 8192;
+
+  
+  nsCOMPtr<nsIOutputStream> &out;
+
+  
+  char buffer[BUFFER_SIZE];
+
+  
+  nsresult result_;
+
+  
+  int amountUsed;
+
+  
+  
+  
+  int64_t writtenCount;
+
+  
+  nsresult writeBuffer();
+
+public:
+  explicit ZeroCopyNSIOutputStream(nsCOMPtr<nsIOutputStream> &out);
+
+  nsresult flush() { return writeBuffer(); }
+
+  
+  bool failed() const { return NS_FAILED(result_); }
+
+  nsresult result() const { return result_; }
+
+  
+  virtual ~ZeroCopyNSIOutputStream() override;
+  virtual bool Next(void **data, int *size) override;
+  virtual void BackUp(int count) override;
+  virtual ::google::protobuf::int64 ByteCount() const override;
+};
+
+} 
+} 
+
+#endif 
