66993c07cb57637ff66d1449cde3244bf56b93ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:38:44 +0000

    Bug 1066450 - Fix regression of web audio tools not reinitializing after navigation. r=vp
    UltraBlame original commit: 5a03ab3af00fb0c17a461b8ead430c66a79c8793

--- toolkit/devtools/server/actors/webaudio.js
+++ toolkit/devtools/server/actors/webaudio.js
@@ -284,6 +284,7 @@ let WebAudioActor = exports.WebAudioActor = protocol.ActorClass({
 
     this._onDestroyNode = this._onDestroyNode.bind(this);
     this._onGlobalDestroyed = this._onGlobalDestroyed.bind(this);
+    this._onGlobalCreated = this._onGlobalCreated.bind(this);
   },
 
   destroy: function(conn) {
@@ -322,6 +323,9 @@ let WebAudioActor = exports.WebAudioActor = protocol.ActorClass({
       holdWeak: true,
       storeCalls: false
     });
+    
+    
+    on(this.tabActor, "window-ready", this._onGlobalCreated);
     
     
     on(this.tabActor, "window-destroyed", this._onGlobalDestroyed);
@@ -395,6 +399,7 @@ let WebAudioActor = exports.WebAudioActor = protocol.ActorClass({
     this.tabActor = null;
     this._initialized = false;
     off(this.tabActor, "window-destroyed", this._onGlobalDestroyed);
+    off(this.tabActor, "window-ready", this._onGlobalCreated);
     this._nativeToActorID = null;
     this._callWatcher.eraseRecording();
     this._callWatcher.finalize();
@@ -567,6 +572,14 @@ let WebAudioActor = exports.WebAudioActor = protocol.ActorClass({
       this._nativeToActorID.delete(nativeID);
       emit(this, "destroy-node", actor);
     }
+  },
+
+  
+
+
+
+  _onGlobalCreated: function () {
+    this._callWatcher.resumeRecording();
   },
 
   
