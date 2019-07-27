92715de1f6c5eace35513608eaccd9d9d8746fa3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:52:47 +0000

    Bug 1125497 - Removed redundant TelemetryStopwatch.jsm import. r=dao
    UltraBlame original commit: 0e1056fb88049104de004ebd500a2f76a452db56

--- browser/base/content/browser-gestureSupport.js
+++ browser/base/content/browser-gestureSupport.js
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http:
 
-Cu.import("resource://gre/modules/TelemetryStopwatch.jsm", this);
-
 
 
 
