14212261d9ccadbf91afe118b11c76ebd2df7c95
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:58:42 +0000

    Bug 1014125 - Bisection Base Patch. r=ahal
    UltraBlame original commit: c3c48c1861b1562594d2cc209b50e6545c77d80d

--- testing/mochitest/runtestsremote.py
+++ testing/mochitest/runtestsremote.py
@@ -380,13 +380,13 @@ class MochiRemote(Mochitest):
         options.logFile = self.localLog
         return retVal
 
-    def buildTestPath(self, options):
+    def buildTestPath(self, options, testsToFilter=None):
         if options.robocopIni != "":
             
             
             return self.buildTestURL(options)
         else:
-            return super(MochiRemote, self).buildTestPath(options)
+            return super(MochiRemote, self).buildTestPath(options, testsToFilter)
 
     def installChromeFile(self, filename, options):
         parts = options.app.split('/')
