7f4f3e1eea51a7f7b14a32e942cfdf01ea2f93dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:54:23 +0000

    Bug 1032116 - Outparamdel nsIStyleSheetLinkingElement::GetStyleSheet; r=dbaron
    UltraBlame original commit: 4f7db7e63a84f12c9dad67dbf39ca214236f1fc0

--- content/base/public/nsIStyleSheetLinkingElement.h
+++ content/base/public/nsIStyleSheetLinkingElement.h
@@ -15,8 +15,6 @@ class nsIURI;
 { 0xd753c84a, 0x17fd, 0x4d5f, \
  { 0xb2, 0xe9, 0x63, 0x52, 0x8c, 0x87, 0x99, 0x7a } }
 
-class nsIStyleSheet;
-
 namespace mozilla {
 class CSSStyleSheet;
 } 
@@ -39,8 +37,7 @@ public:
 
 
 
-
-  NS_IMETHOD GetStyleSheet(nsIStyleSheet*& aStyleSheet) = 0;
+  NS_IMETHOD_(mozilla::CSSStyleSheet*) GetStyleSheet() = 0;
 
   
 
