2a34ec51a224bfcc09e0935326c50fe0d6d2d904
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:37:42 +0000

    Bug 923512 - Transition the TransformTo<>() functions from gfx3DMatrix to Matrix4x4. r=kats
    UltraBlame original commit: dff51863663c0a8d3788f07b1e6f140920ce1d88

--- layout/base/UnitTransforms.h
+++ layout/base/UnitTransforms.h
@@ -8,6 +8,7 @@
 #define MOZ_UNIT_TRANSFORMS_H_
 
 #include "Units.h"
+#include "mozilla/gfx/Matrix.h"
 
 namespace mozilla {
 
@@ -42,7 +43,11 @@ gfx::PointTyped<TargetUnits> ViewAs(const gfxPoint& aPoint) {
   return gfx::PointTyped<TargetUnits>(aPoint.x, aPoint.y);
 }
 template <class TargetUnits>
-gfx::RectTyped<TargetUnits> ViewAs(const gfxRect& aRect) {
+gfx::PointTyped<TargetUnits> ViewAs(const gfx::Point& aPoint) {
+  return gfx::PointTyped<TargetUnits>(aPoint.x, aPoint.y);
+}
+template <class TargetUnits>
+gfx::RectTyped<TargetUnits> ViewAs(const gfx::Rect& aRect) {
   return gfx::RectTyped<TargetUnits>(aRect.x, aRect.y, aRect.width, aRect.height);
 }
 template <class TargetUnits>
@@ -60,29 +65,23 @@ gfx::IntRectTyped<TargetUnits> ViewAs(const nsIntRect& aRect) {
 
 
 
-
-template <class SourceUnits>
-gfxPoint ViewAsUntyped(const gfx::PointTyped<SourceUnits>& aPoint) {
-  return gfxPoint(aPoint.x, aPoint.y);
-}
-template <class SourceUnits>
-gfxRect ViewAsUntyped(const gfx::RectTyped<SourceUnits>& aRect) {
-  return gfxRect(aRect.x, aRect.y, aRect.width, aRect.height);
-}
-
-
-
 template <typename TargetUnits, typename SourceUnits>
-static gfx::PointTyped<TargetUnits> TransformTo(const gfx3DMatrix& aTransform,
+static gfx::PointTyped<TargetUnits> TransformTo(const gfx::Matrix4x4& aTransform,
                                                 const gfx::PointTyped<SourceUnits>& aPoint)
 {
-  return ViewAs<TargetUnits>(aTransform.Transform(ViewAsUntyped(aPoint)));
+  return ViewAs<TargetUnits>(aTransform * aPoint.ToUnknownPoint());
 }
 template <typename TargetUnits, typename SourceUnits>
-static gfx::RectTyped<TargetUnits> TransformTo(const gfx3DMatrix& aTransform,
+static gfx::IntPointTyped<TargetUnits> TransformTo(const gfx::Matrix4x4& aTransform,
+                                                   const gfx::IntPointTyped<SourceUnits>& aPoint)
+{
+  return RoundedToInt(TransformTo<TargetUnits>(aTransform, gfx::PointTyped<SourceUnits>(aPoint)));
+}
+template <typename TargetUnits, typename SourceUnits>
+static gfx::RectTyped<TargetUnits> TransformTo(const gfx::Matrix4x4& aTransform,
                                                const gfx::RectTyped<SourceUnits>& aRect)
 {
-  return ViewAs<TargetUnits>(aTransform.TransformBounds(ViewAsUntyped(aRect)));
+  return ViewAs<TargetUnits>(aTransform.TransformBounds(aRect.ToUnknownRect()));
 }
 
 
