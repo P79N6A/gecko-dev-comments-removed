6883f24a8b802fc40cdbbc7c770cdae106624cac
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:30:34 +0000

    Bug 1045756 Test DOM interfaces exposed on worker global. r=bz
    UltraBlame original commit: 8cfa55304cc018c893972c949761ac31a2a43b04

---
+++ dom/workers/test/test_worker_interfaces.js
@@ -0,0 +1,215 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var ecmaGlobals =
+  [
+    "Array",
+    "ArrayBuffer",
+    "Boolean",
+    "DataView",
+    "Date",
+    "Error",
+    "EvalError",
+    "Float32Array",
+    "Float64Array",
+    "Function",
+    
+    
+    
+    {name: "Infinity"},
+    "Int16Array",
+    "Int32Array",
+    "Int8Array",
+    "InternalError",
+    {name: "Intl", desktop: true},
+    "Iterator",
+    "JSON",
+    "Map",
+    "Math",
+    {name: "NaN"},
+    "Number",
+    "Object",
+    "Proxy",
+    "RangeError",
+    "ReferenceError",
+    "RegExp",
+    "Set",
+    {name: "SharedArrayBuffer", nightly: true},
+    {name: "SIMD", nightly: true},
+    "StopIteration",
+    "String",
+    "Symbol",
+    "SyntaxError",
+    {name: "TypedObject", nightly: true},
+    "TypeError",
+    "Uint16Array",
+    "Uint32Array",
+    "Uint8Array",
+    "Uint8ClampedArray",
+    "URIError",
+    "WeakMap",
+  ];
+
+
+
+
+var interfaceNamesInGlobalScope =
+  [
+
+    "Blob",
+
+    "Console",
+
+    "DedicatedWorkerGlobalScope",
+
+    "DOMException",
+
+    "Event",
+
+    "EventTarget",
+
+    "File",
+
+    "FileReaderSync",
+
+    { name: "Headers", pref: "dom.fetch.enabled" },
+
+    "ImageData",
+
+    "MessageEvent",
+
+    "MessagePort",
+
+    "Promise",
+
+    "TextDecoder",
+
+    "TextEncoder",
+
+    "XMLHttpRequest",
+
+    "XMLHttpRequestUpload",
+
+    "URL",
+
+    "URLSearchParams",
+
+    "Worker",
+
+    "WorkerGlobalScope",
+
+    "WorkerLocation",
+
+    "WorkerNavigator",
+
+  ];
+
+
+function createInterfaceMap(prefMap, permissionMap, version, userAgent) {
+  var isNightly = version.endsWith("a1");
+  var isRelease = !version.contains("a");
+  var isDesktop = !/Mobile|Tablet/.test(userAgent);
+  var isB2G = !isDesktop && !userAgent.contains("Android");
+
+  var interfaceMap = {};
+
+  function addInterfaces(interfaces)
+  {
+    for (var entry of interfaces) {
+      if (typeof(entry) === "string") {
+        interfaceMap[entry] = true;
+      } else if ((entry.nightly === !isNightly) ||
+                 (entry.desktop === !isDesktop) ||
+                 (entry.b2g === !isB2G) ||
+                 (entry.release === !isRelease) ||
+                 (entry.pref && !prefMap[entry.pref])  ||
+                 (entry.permission && !permissionMap[entry.permission])) {
+        interfaceMap[entry.name] = false;
+      } else {
+        interfaceMap[entry.name] = true;
+      }
+    }
+  }
+
+  addInterfaces(ecmaGlobals);
+  addInterfaces(interfaceNamesInGlobalScope);
+
+  return interfaceMap;
+}
+
+function runTest(prefMap, permissionMap, version, userAgent) {
+  var interfaceMap = createInterfaceMap(prefMap, permissionMap, version, userAgent);
+  for (var name of Object.getOwnPropertyNames(self)) {
+    
+    if (!/^[A-Z]/.test(name)) {
+      continue;
+    }
+    ok(interfaceMap[name],
+       "If this is failing: DANGER, are you sure you want to expose the new interface " + name +
+       " to all webpages as a property on the worker? Do not make a change to this file without a " +
+       " review from a DOM peer for that specific change!!! (or a JS peer for changes to ecmaGlobals)");
+    delete interfaceMap[name];
+  }
+  for (var name of Object.keys(interfaceMap)) {
+    ok(name in self === interfaceMap[name],
+       name + " should " + (interfaceMap[name] ? "" : " NOT") + " be defined on the global scope");
+    if (!interfaceMap[name]) {
+      delete interfaceMap[name];
+    }
+  }
+  is(Object.keys(interfaceMap).length, 0,
+     "The following interface(s) are not enumerated: " + Object.keys(interfaceMap).join(", "));
+}
+
+function appendPrefs(prefs, interfaces) {
+  for (var entry of interfaces) {
+    if (entry.pref !== undefined && prefs.indexOf(entry.pref) === -1) {
+      prefs.push(entry.pref);
+    }
+  }
+}
+
+var prefs = [];
+appendPrefs(prefs, ecmaGlobals);
+appendPrefs(prefs, interfaceNamesInGlobalScope);
+
+function appendPermissions(permissions, interfaces) {
+  for (var entry of interfaces) {
+    if (entry.permission !== undefined &&
+        permissions.indexOf(entry.permission) === -1) {
+      permissions.push(entry.permission);
+    }
+  }
+}
+
+var permissions = [];
+appendPermissions(permissions, ecmaGlobals);
+appendPermissions(permissions, interfaceNamesInGlobalScope);
+
+workerTestGetPrefs(prefs, function(prefMap) {
+  workerTestGetPermissions(permissions, function(permissionMap) {
+    workerTestGetVersion(function(version) {
+      workerTestGetUserAgent(function(userAgent) {
+        runTest(prefMap, permissionMap, version, userAgent);
+        workerTestDone();
+      });
+    });
+  });
+});
---
+++ dom/workers/test/worker_driver.js
@@ -0,0 +1,82 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function workerTestExec(script) {
+  SimpleTest.waitForExplicitFinish();
+  var worker = new Worker('worker_wrapper.js');
+  worker.onmessage = function(event) {
+    if (event.data.type == 'finish') {
+      SimpleTest.finish();
+
+    } else if (event.data.type == 'status') {
+      ok(event.data.status, event.data.msg);
+
+    } else if (event.data.type == 'getPrefs') {
+      var result = {};
+      event.data.prefs.forEach(function(pref) {
+        result[pref] = SpecialPowers.Services.prefs.getBoolPref(pref);
+      });
+      worker.postMessage({
+        type: 'returnPrefs',
+        prefs: event.data.prefs,
+        result: result
+      });
+
+    } else if (event.data.type == 'getPermissions') {
+      var result = {};
+      event.data.permissions.forEach(function(permission) {
+        result[permission] = SpecialPowers.hasPermission(permission, window.document);
+      });
+      worker.postMessage({
+        type: 'returnPermissions',
+        permissions: event.data.permissions,
+        result: result
+      });
+
+    } else if (event.data.type == 'getVersion') {
+      var result = SpecialPowers.Cc['@mozilla.org/xre/app-info;1'].getService(SpecialPowers.Ci.nsIXULAppInfo).version;
+      worker.postMessage({
+        type: 'returnVersion',
+        result: result
+      });
+
+    } else if (event.data.type == 'getUserAgent') {
+      worker.postMessage({
+        type: 'returnUserAgent',
+        result: navigator.userAgent
+      });
+    }
+  }
+
+  worker.onerror = function(event) {
+    ok(false, 'Worker had an error: ' + event.data);
+    SimpleTest.finish();
+  };
+
+  worker.postMessage({ script: script });
+}
