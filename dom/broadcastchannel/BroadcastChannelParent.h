ee95b1df2baab2d453182357d966a9aa8134b627
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:42:15 +0000

    Bug 966439 - BroadcastChannel API - patch 1 - BroadcastChannel for main-thread, r=smaug, r=bent
    UltraBlame original commit: 3d23e775033aab8f7cd901b014f9ba91394047a7

---
+++ dom/broadcastchannel/BroadcastChannelParent.h
@@ -0,0 +1,48 @@
+
+
+
+
+#ifndef mozilla_dom_BroadcastChannelParent_h
+#define mozilla_dom_BroadcastChannelParent_h
+
+#include "mozilla/dom/PBroadcastChannelParent.h"
+
+namespace mozilla {
+
+namespace ipc {
+class BackgroundParentImpl;
+}
+
+namespace dom {
+
+class BroadcastChannelService;
+
+class BroadcastChannelParent MOZ_FINAL : public PBroadcastChannelParent
+{
+  friend class mozilla::ipc::BackgroundParentImpl;
+
+public:
+  void CheckAndDeliver(const nsString& aMessage,
+                       const nsString& aOrigin,
+                       const nsString& aChannel);
+
+private:
+  BroadcastChannelParent(const nsAString& aOrigin,
+                         const nsAString& aChannel);
+  ~BroadcastChannelParent();
+
+  virtual bool RecvPostMessage(const nsString& aMessage) MOZ_OVERRIDE;
+
+  virtual bool RecvClose() MOZ_OVERRIDE;
+
+  virtual void ActorDestroy(ActorDestroyReason aWhy) MOZ_OVERRIDE;
+
+  nsRefPtr<BroadcastChannelService> mService;
+  nsString mOrigin;
+  nsString mChannel;
+};
+
+} 
+} 
+
+#endif 
