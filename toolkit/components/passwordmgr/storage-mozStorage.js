b430d8988e085070fcf19d15da6593970803d132
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:35:38 +0000

    Bug 1147561 - Fallback to logins with the same hostPort but different scheme for the form action. r=MattN
    UltraBlame original commit: 2614fbb8d3856cc85cfa770bc109b8ddfa6f2f3e

--- toolkit/components/passwordmgr/storage-mozStorage.js
+++ toolkit/components/passwordmgr/storage-mozStorage.js
@@ -492,8 +492,8 @@ LoginManagerStorage_mozStorage.prototype = {
         
         case "formSubmitURL":
           if (value != null) {
-              conditions.push("formSubmitURL = :formSubmitURL OR formSubmitURL = ''");
-              params["formSubmitURL"] = value;
+              
+              
               break;
           }
         
@@ -531,7 +531,7 @@ LoginManagerStorage_mozStorage.prototype = {
     }
 
     let stmt;
-    let logins = [], ids = [];
+    let logins = [], ids = [], fallbackLogins = [], fallbackIds = [];;
     try {
       stmt = this._dbCreateStatement(query, params);
       
@@ -550,8 +550,18 @@ LoginManagerStorage_mozStorage.prototype = {
         login.timeLastUsed = stmt.row.timeLastUsed;
         login.timePasswordChanged = stmt.row.timePasswordChanged;
         login.timesUsed = stmt.row.timesUsed;
-        logins.push(login);
-        ids.push(stmt.row.id);
+        if (login.formSubmitURL == "" ||
+            login.formSubmitURL == matchData["formSubmitURL"]) {
+            logins.push(login);
+            ids.push(stmt.row.id);
+        } else {
+          let loginURI = Services.io.newURI(login.formSubmitURL, null, null);
+          let matchURI = Services.io.newURI(matchData.formSubmitURL, null, null);
+          if (loginURI.hostPort == matchURI.hostPort) {
+            fallbackLogins.push(login);
+            fallbackIds.push(stmt.row.id);
+          }
+        }
       }
     } catch (e) {
       this.log("_searchLogins failed: " + e.name + " : " + e.message);
@@ -561,6 +571,10 @@ LoginManagerStorage_mozStorage.prototype = {
       }
     }
 
+    if (!logins.length && fallbackLogins.length) {
+      this.log("_searchLogins: returning " + fallbackLogins.length + " fallback logins");
+      return [fallbackLogins, fallbackIds];
+    }
     this.log("_searchLogins: returning " + logins.length + " logins");
     return [logins, ids];
   },
