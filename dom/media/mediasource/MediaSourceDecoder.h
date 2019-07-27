6f7650b0e512856ba07c35ea4fdf5efa5dc365e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:06:46 +0000

    Bug 1130948: Part1. Use mediasource duration to determine media element duration. r=cajbir
    UltraBlame original commit: 8df52c8e52e3856bc75f013d0f6f45d3824b926d

--- dom/media/mediasource/MediaSourceDecoder.h
+++ dom/media/mediasource/MediaSourceDecoder.h
@@ -56,6 +56,9 @@ public:
   void Ended();
   bool IsExpectingMoreData() MOZ_OVERRIDE;
 
+  
+  virtual double GetDuration() MOZ_OVERRIDE;
+
   void SetInitialDuration(int64_t aDuration);
   void SetMediaSourceDuration(double aDuration, MSRangeRemovalAction aAction);
   double GetMediaSourceDuration();
