8284485f7c2d5e206109827f80f13245e5e9819d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:26:28 +0000

    Bug 1059899 - When cubeb_stream_init fails, fallback to using a SystemClockDriver. r=jesup
    UltraBlame original commit: 1cd5483ff2fec2a9d68c38b03ba3dd0526c58641

--- content/media/GraphDriver.cpp
+++ content/media/GraphDriver.cpp
@@ -575,7 +575,13 @@ AudioCallbackDriver::Init()
                         DataCallback_s, StateCallback_s, this) == CUBEB_OK) {
     mAudioStream.own(stream);
   } else {
-    NS_WARNING("Could not create a cubeb stream for MediaStreamGraph.");
+    NS_WARNING("Could not create a cubeb stream for MediaStreamGraph, falling back to a SystemClockDriver");
+    
+    mNextDriver = new SystemClockDriver(GraphImpl());
+    mNextDriver->SetGraphTime(this, mIterationStart, mIterationEnd,
+                               mStateComputedTime, mNextStateComputedTime);
+    mGraphImpl->SetCurrentDriver(mNextDriver);
+    mNextDriver->Start();
     return;
   }
 
