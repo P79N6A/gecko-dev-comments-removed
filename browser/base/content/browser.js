611be424e3f954afe98567d6c3b3ac52c5ef725a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:24:29 +0000

    Bug 893553 - Offer an override option for users who block mixed display content by default. r=dolske
    UltraBlame original commit: 8ed5bf80757f5dc658cd299e577d1f1c6f233de9

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6816,11 +6816,17 @@ var gIdentityHandler = {
     
     
     
+    
+    
     if (state &
         (nsIWebProgressListener.STATE_BLOCKED_MIXED_ACTIVE_CONTENT |
          nsIWebProgressListener.STATE_LOADED_MIXED_ACTIVE_CONTENT  |
+         nsIWebProgressListener.STATE_BLOCKED_MIXED_DISPLAY_CONTENT|
          nsIWebProgressListener.STATE_BLOCKED_TRACKING_CONTENT     |
-         nsIWebProgressListener.STATE_LOADED_TRACKING_CONTENT)) {
+         nsIWebProgressListener.STATE_LOADED_TRACKING_CONTENT)     ||
+        
+        (gPrefService.getBoolPref("security.mixed_content.block_display_content")
+        && (state & nsIWebProgressListener.STATE_LOADED_MIXED_DISPLAY_CONTENT))) {
       this.showBadContentDoorhanger(state);
     } else if (gPrefService.getBoolPref("privacy.trackingprotection.enabled")) {
       
@@ -6861,8 +6867,11 @@ var gIdentityHandler = {
       histogram.add(3);
     }
     if (state &
-        (Ci.nsIWebProgressListener.STATE_LOADED_MIXED_ACTIVE_CONTENT |
-         Ci.nsIWebProgressListener.STATE_LOADED_TRACKING_CONTENT)) {
+        (Ci.nsIWebProgressListener.STATE_LOADED_MIXED_ACTIVE_CONTENT  |
+         Ci.nsIWebProgressListener.STATE_LOADED_TRACKING_CONTENT)     ||
+        
+        (gPrefService.getBoolPref("security.mixed_content.block_display_content")
+        && (state & Ci.nsIWebProgressListener.STATE_LOADED_MIXED_DISPLAY_CONTENT))) {
       iconState = "bad-content-unblocked-notification-icon";
     }
 
