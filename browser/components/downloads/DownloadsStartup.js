c571286dacd4c04063b2ec294a5ac8f0cbd7b357
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:40:15 +0000

    Bug 1114614 - Remove nsIDownloadManagerUI from Firefox for Desktop. r=mak
    UltraBlame original commit: a4335e10398f32262f94d20a3b0e3f35db3d3bd5

--- browser/components/downloads/DownloadsStartup.js
+++ browser/components/downloads/DownloadsStartup.js
@@ -25,12 +25,6 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 
 
-const kDownloadsUICid = Components.ID("{4d99321e-d156-455b-81f7-e7aa2308134f}");
-const kDownloadsUIContractId = "@mozilla.org/download-manager-ui;1";
-
-
-
-
 const kTransferCid = Components.ID("{1b4c85df-cbdd-4bb6-b04e-613caece083c}");
 const kTransferContractId = "@mozilla.org/transfer;1";
 
@@ -54,18 +48,6 @@ DownloadsStartup.prototype = {
 
   observe: function DS_observe(aSubject, aTopic, aData)
   {
-    if (aTopic != "profile-after-change") {
-      Cu.reportError("Unexpected observer notification.");
-      return;
-    }
-
-    
-    
-    
-    Components.manager.QueryInterface(Ci.nsIComponentRegistrar)
-                      .registerFactory(kDownloadsUICid, "",
-                                       kDownloadsUIContractId, null);
-
     
     
     Components.manager.QueryInterface(Ci.nsIComponentRegistrar)
