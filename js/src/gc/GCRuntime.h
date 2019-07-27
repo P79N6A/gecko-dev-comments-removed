d87653dbd84bf40d2a0b8e2b312dc30298c49cd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:38:22 +0000

    Bug 1119584 - Return number of allocations until next zeal GC, r=terrence
    UltraBlame original commit: 20ed57d8a4e7ecc2d3a10d28533b03675b749f74

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -344,7 +344,7 @@ class GCRuntime
 
 #ifdef JS_GC_ZEAL
     const void *addressOfZealMode() { return &zealMode; }
-    void getZeal(uint8_t *zeal, uint32_t *frequency);
+    void getZeal(uint8_t *zeal, uint32_t *frequency, uint32_t *nextScheduled);
     void setZeal(uint8_t zeal, uint32_t frequency);
     bool parseAndSetZeal(const char *str);
     void setNextScheduled(uint32_t count);
