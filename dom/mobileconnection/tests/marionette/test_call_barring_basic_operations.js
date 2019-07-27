78f1d3be5be4e0302c1b99dc07a34b6c3dc16eb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:44:38 +0000

    Bug 1027546 - Part 3: Create a new testcase. r=aknow
    UltraBlame original commit: 47fbd6a8b2496a2242d6429e5a597752fddbd0b2

---
+++ dom/mobileconnection/tests/marionette/test_call_barring_basic_operations.js
@@ -0,0 +1,107 @@
+
+
+
+MARIONETTE_TIMEOUT = 60000;
+MARIONETTE_HEAD_JS = "head.js";
+
+const AO = MozMobileConnection.CALL_BARRING_PROGRAM_ALL_OUTGOING;
+const OI = MozMobileConnection.CALL_BARRING_PROGRAM_OUTGOING_INTERNATIONAL;
+const OX = MozMobileConnection.CALL_BARRING_PROGRAM_OUTGOING_INTERNATIONAL_EXCEPT_HOME;
+let outgoingPrograms = [null, AO, OI, OX];
+
+const AI = MozMobileConnection.CALL_BARRING_PROGRAM_ALL_INCOMING;
+const IR = MozMobileConnection.CALL_BARRING_PROGRAM_INCOMING_ROAMING;
+let incomingPrograms = [null, AI, IR];
+
+const SERVICE_CLASS_VOICE = MozMobileConnection.ICC_SERVICE_CLASS_VOICE;
+
+function getProgram(aProgram, aEnabled) {
+  if (aProgram === null) {
+    return Promise.resolve();
+  }
+
+  return Promise.resolve()
+    .then(() => getCallBarringOption({
+      program: aProgram,
+      serviceClass: SERVICE_CLASS_VOICE
+    }))
+
+    .then(result => {
+      is(result.program, aProgram, "Program");
+      is(result.enabled, aEnabled, "Enabled");
+      is(result.serviceClass, SERVICE_CLASS_VOICE, "ServiceClass");
+    });
+}
+
+function setProgram(aProgram, aEnabled) {
+  if (aProgram === null) {
+    return Promise.resolve();
+  }
+
+  return Promise.resolve()
+    .then(() => setCallBarringOption({
+      program: aProgram,
+      serviceClass: SERVICE_CLASS_VOICE,
+      enabled: aEnabled,
+      password: "0000" 
+    }));
+}
+
+function setAndCheckProgram(aActiveProgram, aAllPrograms) {
+  let promise = Promise.resolve();
+
+  if (aActiveProgram !== null) {
+    promise = promise.then(() => setProgram(aActiveProgram, true));
+  } else {
+    
+    promise = aAllPrograms.reduce((previousPromise, program) => {
+      return previousPromise.then(() => setProgram(program, false));
+    }, promise);
+  }
+
+  
+  promise = aAllPrograms.reduce((previousPromise, program) => {
+    return previousPromise.then(() => getProgram(program, program === aActiveProgram));
+  }, promise);
+
+  return promise;
+}
+
+function testSingleProgram(aOriginProgram, aTargetPrograms, aOriginPrograms) {
+  let promise = setAndCheckProgram(aOriginProgram, aOriginPrograms);
+
+  return aTargetPrograms.reduce((previousPromise, targetProgram) => {
+    return previousPromise
+      .then(() => log(aOriginProgram + " <-> " + targetProgram))
+      .then(() => setAndCheckProgram(targetProgram, aOriginPrograms))
+      .then(() => setAndCheckProgram(aOriginProgram, aOriginPrograms));
+  }, promise);
+}
+
+function testAllPrograms(aPrograms) {
+  let targetPrograms = aPrograms.slice();
+
+  return aPrograms.reduce((previousPromise, program) => {
+    return previousPromise
+      .then(() => {
+        targetPrograms.shift();
+        return testSingleProgram(program, targetPrograms, aPrograms);
+      });
+  }, Promise.resolve());
+}
+
+
+startTestCommon(function() {
+  return Promise.resolve()
+    .then(() => setProgram(null, outgoingPrograms))
+    .then(() => setProgram(null, incomingPrograms))
+
+    .then(() => log("=== Test outgoing call barring programs ==="))
+    .then(() => testAllPrograms(outgoingPrograms))
+    .then(() => log("=== Test incoming call barring programs ==="))
+    .then(() => testAllPrograms(incomingPrograms))
+
+    .catch(aError => ok(false, "promise rejects during test: " + aError))
+    .then(() => setProgram(null, outgoingPrograms))
+    .then(() => setProgram(null, incomingPrograms));
+});
