832f999d2855fdc72764a2f0846b1112ba645dfb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:24:48 +0000

    Bug 1043143: Step 2a: Add a class like nsDownloader but using memory instead of a file.
    UltraBlame original commit: 879c78ef29ec7b0b7c06ea3e76e8f5faecbaf21b

---
+++ netwerk/base/MemoryDownloader.cpp
@@ -0,0 +1,91 @@
+
+
+
+
+
+#include "MemoryDownloader.h"
+
+#include "mozilla/Assertions.h"
+
+namespace mozilla {
+namespace net {
+
+NS_IMPL_ISUPPORTS(MemoryDownloader,
+		  nsIStreamListener,
+		  nsIRequestObserver)
+
+MemoryDownloader::MemoryDownloader(IObserver* aObserver)
+: mObserver(aObserver)
+{
+}
+
+MemoryDownloader::~MemoryDownloader()
+{
+}
+
+NS_IMETHODIMP
+MemoryDownloader::OnStartRequest(nsIRequest* aRequest, nsISupports* aCtxt)
+{
+  MOZ_ASSERT(!mData);
+  mData.reset(new FallibleTArray<uint8_t>());
+  mStatus = NS_OK;
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+MemoryDownloader::OnStopRequest(nsIRequest* aRequest,
+                                nsISupports* aCtxt,
+                                nsresult aStatus)
+{
+  MOZ_ASSERT_IF(NS_FAILED(mStatus), NS_FAILED(aStatus));
+  MOZ_ASSERT(!mData == NS_FAILED(mStatus));
+  Data data;
+  data.swap(mData);
+  nsRefPtr<IObserver> observer;
+  observer.swap(mObserver);
+  observer->OnDownloadComplete(this, aRequest, aCtxt, aStatus,
+                               mozilla::Move(data));
+  return NS_OK;
+}
+
+NS_METHOD
+MemoryDownloader::ConsumeData(nsIInputStream* aIn,
+                              void* aClosure,
+                              const char* aFromRawSegment,
+                              uint32_t aToOffset,
+                              uint32_t aCount,
+                              uint32_t* aWriteCount)
+{
+  MemoryDownloader* self = static_cast<MemoryDownloader*>(aClosure);
+  if (!self->mData->AppendElements(aFromRawSegment, aCount)) {
+    
+    
+    self->mStatus = NS_ERROR_OUT_OF_MEMORY;
+    return NS_ERROR_OUT_OF_MEMORY;
+  }
+  *aWriteCount = aCount;
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+MemoryDownloader::OnDataAvailable(nsIRequest* aRequest,
+                                  nsISupports* aCtxt,
+                                  nsIInputStream* aInStr,
+                                  uint64_t aSourceOffset,
+                                  uint32_t aCount)
+{
+  uint32_t n;
+  MOZ_ASSERT(mData);
+  nsresult rv = aInStr->ReadSegments(ConsumeData, this, aCount, &n);
+  if (NS_SUCCEEDED(mStatus) && NS_FAILED(rv)) {
+    mStatus = rv;
+  }
+  if (NS_WARN_IF(NS_FAILED(mStatus))) {
+    mData.reset(nullptr);
+    return mStatus;
+  }
+  return NS_OK;
+}
+
+} 
+} 
---
+++ netwerk/base/MemoryDownloader.h
@@ -0,0 +1,65 @@
+
+
+
+
+
+#ifndef mozilla_net_MemoryDownloader_h__
+#define mozilla_net_MemoryDownloader_h__
+
+#include "mozilla/UniquePtr.h"
+#include "nsCOMPtr.h"
+#include "nsIStreamListener.h"
+#include "nsTArray.h"
+
+
+
+
+
+
+
+
+
+
+namespace mozilla {
+namespace net {
+
+class MemoryDownloader MOZ_FINAL : public nsIStreamListener
+{
+public:
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIREQUESTOBSERVER
+  NS_DECL_NSISTREAMLISTENER
+
+  typedef mozilla::UniquePtr<FallibleTArray<uint8_t>> Data;
+
+  class IObserver : public nsISupports {
+  public:
+    
+    virtual void OnDownloadComplete(MemoryDownloader* aDownloader,
+                                    nsIRequest* aRequest,
+                                    nsISupports* aCtxt,
+                                    nsresult aStatus,
+                                    Data aData) = 0;
+  };
+
+  explicit MemoryDownloader(IObserver* aObserver);
+
+private:
+  virtual ~MemoryDownloader();
+
+  static NS_METHOD ConsumeData(nsIInputStream *in,
+                               void           *closure,
+                               const char     *fromRawSegment,
+                               uint32_t        toOffset,
+                               uint32_t        count,
+                               uint32_t       *writeCount);
+
+  nsRefPtr<IObserver> mObserver;
+  Data mData;
+  nsresult mStatus;
+};
+
+} 
+} 
+
+#endif 
