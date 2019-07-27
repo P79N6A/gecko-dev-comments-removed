2849e8f8c54930986bd5545c2058bdb7c32c7bbd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:29:37 +0000

    Bug 1110534: Use AVCC wrapper for mac decoder. Adds AnnexB and AVC3 support. r=cpearce
    UltraBlame original commit: 8b50d291a835729e799dd16369fee314f16a9cbd

--- dom/media/fmp4/PlatformDecoderModule.cpp
+++ dom/media/fmp4/PlatformDecoderModule.cpp
@@ -5,6 +5,8 @@
 
 
 #include "PlatformDecoderModule.h"
+#include "AVCCDecoderModule.h"
+
 #ifdef XP_WIN
 #include "WMFDecoderModule.h"
 #endif
@@ -149,7 +151,7 @@ PlatformDecoderModule::CreatePDM()
   }
 #endif
 #ifdef MOZ_APPLEMEDIA
-  nsRefPtr<PlatformDecoderModule> m(new AppleDecoderModule());
+  nsRefPtr<PlatformDecoderModule> m(new AVCCDecoderModule(new AppleDecoderModule()));
   return m.forget();
 #endif
 #ifdef MOZ_GONK_MEDIACODEC
