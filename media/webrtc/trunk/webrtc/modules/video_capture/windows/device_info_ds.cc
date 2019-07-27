feb727245459fbcd775bd6ad58ffccf04d29d912
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:43:07 +0000

    Bug 1158372: clean up windows CreateCapabilityMap for video capture r=dmajor
    UltraBlame original commit: ca6e5063bd8075574f119c3d736508b61d18a2c1

--- media/webrtc/trunk/webrtc/modules/video_capture/windows/device_info_ds.cc
+++ media/webrtc/trunk/webrtc/modules/video_capture/windows/device_info_ds.cc
@@ -42,7 +42,7 @@ const DelayValues WindowsCaptureDelays[NoWindowsCaptureDelays] = {
 };
 
 
-  void _FreeMediaType(AM_MEDIA_TYPE& mt)
+void _FreeMediaType(AM_MEDIA_TYPE& mt)
 {
     if (mt.cbFormat != 0)
     {
@@ -616,6 +616,10 @@ int32_t DeviceInfoDS::CreateCapabilityMap(
                     else
                         capability.maxFPS = 0;
                 }
+
+                if (frameDurationList) {
+                  CoTaskMemFree((PVOID)frameDurationList); 
+                }
             }
             else 
             {
@@ -673,24 +677,27 @@ int32_t DeviceInfoDS::CreateCapabilityMap(
                 StringFromGUID2(pmt->subtype, strGuid, 39);
                 WEBRTC_TRACE( webrtc::kTraceWarning,
                              webrtc::kTraceVideoCapture, _id,
-                             "Device support unknown media type %ls, width %d, height %d",
+                             "Device supports unknown media type %ls",
                              strGuid);
-                continue;
+                
+                assert(capability.rawType == kVideoUnknown);
             }
 
-            
-            capability.expectedCaptureDelay
-                            = GetExpectedCaptureDelay(WindowsCaptureDelays,
-                                                      NoWindowsCaptureDelays,
-                                                      productId,
-                                                      capability.width,
-                                                      capability.height);
-            _captureCapabilities.push_back(capability);
-            _captureCapabilitiesWindows.push_back(capability);
-            WEBRTC_TRACE( webrtc::kTraceInfo, webrtc::kTraceVideoCapture, _id,
-                         "Camera capability, width:%d height:%d type:%d fps:%d",
-                         capability.width, capability.height,
-                         capability.rawType, capability.maxFPS);
+            if (capability.rawType != kVideoUnknown) {
+              
+              capability.expectedCaptureDelay
+                = GetExpectedCaptureDelay(WindowsCaptureDelays,
+                                          NoWindowsCaptureDelays,
+                                          productId,
+                                          capability.width,
+                                          capability.height);
+              _captureCapabilities.push_back(capability);
+              _captureCapabilitiesWindows.push_back(capability);
+              WEBRTC_TRACE( webrtc::kTraceInfo, webrtc::kTraceVideoCapture, _id,
+                            "Camera capability, width:%d height:%d type:%d fps:%d",
+                            capability.width, capability.height,
+                            capability.rawType, capability.maxFPS);
+            }
         }
         _FreeMediaType(*pmt);
         pmt = NULL;
