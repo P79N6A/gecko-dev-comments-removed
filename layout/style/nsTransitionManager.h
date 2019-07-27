3501e379b97aecc83a842aa4cff13115063d9504
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:19:28 +0000

    Bug 1179111 part 2 - Implement CSSTransition.transitionProperty; r=smaug, r=jwatt
    UltraBlame original commit: 4eb184faf92c77013d5f8ce8968cff837910b6cf

--- layout/style/nsTransitionManager.h
+++ layout/style/nsTransitionManager.h
@@ -89,6 +89,10 @@ public:
 
   virtual CSSTransition* AsCSSTransition() override { return this; }
 
+  
+  void GetTransitionProperty(nsString& aRetVal) const { aRetVal = Name(); }
+
+  
   virtual AnimationPlayState PlayStateFromJS() const override;
   virtual void PlayFromJS(ErrorResult& aRv) override;
 
