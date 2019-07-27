7b51ee17697a6310aca58e6824d683e5dbe10e3d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:06:43 +0000

    Bug 1002354 - Proxy nsIScreenManager and nsIScreen's from the child process to the parent process, with caching. r=roc,jimm,smichaud,snorp.
    Changes to nsIScreen and nsIScreenManager were reviewed by roc.
    Changes to dom/ipc were reviewed by jimm.
    Changes to gfx/src/nsDeviceContext.cpp were reviewed by roc.
    Changes to widget/android were reviewed by snorp.
    Changes to widget/cocoa were reviewed by smichaud.
    Changes to widget/gtk were reviewed by roc.
    Changes to widget/windows were reviewed by jimm.
    Changes to widget/xpwidgets were reviewed by roc.
    
    UltraBlame original commit: bdf72cebcd85962092b1cc6772c2d5afd839e441

---
+++ dom/ipc/ScreenManagerParent.h
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_ScreenManagerParent_h
+#define mozilla_dom_ScreenManagerParent_h
+
+#include "mozilla/dom/PScreenManagerParent.h"
+#include "nsIScreenManager.h"
+
+namespace mozilla {
+namespace dom {
+
+class ScreenManagerParent : public PScreenManagerParent
+{
+ public:
+  ScreenManagerParent(uint32_t* aNumberOfScreens,
+                      float* aSystemDefaultScale,
+                      bool* aSuccess);
+  ~ScreenManagerParent() {};
+
+  virtual bool RecvRefresh(uint32_t* aNumberOfScreens,
+                           float* aSystemDefaultScale,
+                           bool* aSuccess) MOZ_OVERRIDE;
+
+  virtual bool RecvScreenRefresh(const uint32_t& aId,
+                                 ScreenDetails* aRetVal,
+                                 bool* aSuccess) MOZ_OVERRIDE;
+
+  virtual void ActorDestroy(ActorDestroyReason aWhy) MOZ_OVERRIDE;
+
+  virtual bool RecvGetPrimaryScreen(ScreenDetails* aRetVal,
+                                    bool* aSuccess) MOZ_OVERRIDE;
+
+  virtual bool RecvScreenForRect(const int32_t& aLeft,
+                                 const int32_t& aTop,
+                                 const int32_t& aWidth,
+                                 const int32_t& aHeight,
+                                 ScreenDetails* aRetVal,
+                                 bool* aSuccess) MOZ_OVERRIDE;
+
+  virtual bool RecvScreenForBrowser(PBrowserParent* aBrowser,
+                                    ScreenDetails* aRetVal,
+                                    bool* aSuccess);
+
+ private:
+  bool ExtractScreenDetails(nsIScreen* aScreen, ScreenDetails &aDetails);
+  nsCOMPtr<nsIScreenManager> mScreenMgr;
+};
+
+} 
+} 
+
+#endif 
