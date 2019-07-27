65116ce5958659d2c495a32867cc61697efbc7d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:47:19 +0000

    Bug 1071207: Remove trickle ICE pref. r=mt
    UltraBlame original commit: 2304db57666e7af0455eec5cc707a2715730bb2f

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -341,7 +341,6 @@ pref("media.peerconnection.video.max_bitrate", 2000);
 #endif
 pref("media.navigator.permission.disabled", false);
 pref("media.peerconnection.default_iceservers", "[{\"url\": \"stun:stun.services.mozilla.com\"}]");
-pref("media.peerconnection.trickle_ice", true);
 pref("media.peerconnection.use_document_iceservers", true);
 
 
