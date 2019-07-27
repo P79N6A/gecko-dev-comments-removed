55c016b8c93e655887e899b1d77c58ed646a8f66
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:22:35 +0000

    Merge inbound to m-c a=merge
    UltraBlame original commit: 5288b15d22de803f6405b37c57cc2b08296d115e

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -800,6 +800,8 @@ pref("devtools.remote.wifi.scan", false);
 
 pref("devtools.remote.wifi.visible", false);
 
+pref("devtools.remote.tls-handshake-timeout", 10000);
+
 
 pref("view_source.syntax_highlight", true);
 pref("view_source.wrap_long_lines", false);
@@ -4392,7 +4394,6 @@ pref("beacon.enabled", true);
 #endif
 
 
-pref("camera.control.autofocus_moving_callback.enabled", true);
 pref("camera.control.face_detection.enabled", true);
 
 
