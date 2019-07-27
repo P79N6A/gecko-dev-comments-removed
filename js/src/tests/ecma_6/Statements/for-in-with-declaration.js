2276b16a1e2536e839bdd314a198bbe02a9218da
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:37:46 +0000

    Bug 1163851 - Pass the correct |InHandling| argument when parsing the initializer in a LexicalDeclaration in a for-in loop.  r=efaust
    UltraBlame original commit: 5de5618316a80dcbbbedbb3caeeb4bfba0771bf1

---
+++ js/src/tests/ecma_6/Statements/for-in-with-declaration.js
@@ -0,0 +1,31 @@
+
+
+
+
+var gTestfile = "for-in-with-declaration.js";
+var BUGNUMBER = 1163851;
+var summary =
+  "Declarations in for-in loop heads must not contain |in|-expression " +
+  "initializers";
+
+print(BUGNUMBER + ": " + summary);
+
+
+
+
+
+assertThrowsInstanceOf(() => Function("for (var x = 3 in {}; ; ) break;"), SyntaxError);
+assertThrowsInstanceOf(() => Function("for (var x, y = 3 in {}; ; ) break;"), SyntaxError);
+assertThrowsInstanceOf(() => Function("for (var x = 5, y = 3 in {}; ; ) break;"), SyntaxError);
+assertThrowsInstanceOf(() => Function("for (const x = 3 in {}; ; ) break;"), SyntaxError);
+assertThrowsInstanceOf(() => Function("for (const x = 5, y = 3 in {}; ; ) break;"), SyntaxError);
+assertThrowsInstanceOf(() => Function("for (let x = 3 in {}; ; ) break;"), SyntaxError);
+assertThrowsInstanceOf(() => Function("for (let x, y = 3 in {}; ; ) break;"), SyntaxError);
+assertThrowsInstanceOf(() => Function("for (let x = 2, y = 3 in {}; ; ) break;"), SyntaxError);
+
+
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
+
+print("Tests complete");
