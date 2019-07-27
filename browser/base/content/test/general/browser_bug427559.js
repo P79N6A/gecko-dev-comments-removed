21dd4840ce2410310b219d68e624f8112d3bf378
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:08:58 +0000

    Bug 1159497 - Fix intermittent browser_bug427559.js failures r=MattN
    UltraBlame original commit: 25259bc37ac5ddbdd053064f0a9d511489ce57bf

--- browser/base/content/test/general/browser_bug427559.js
+++ browser/base/content/test/general/browser_bug427559.js
@@ -1,3 +1,4 @@
+"use strict";
 
 
 
@@ -6,44 +7,47 @@
 
 
 
+const URL = 'data:text/html;charset=utf-8,' +
+            '<body><button onblur="this.remove()">' +
+            '<script>document.body.firstChild.focus()</script></body>';
 
-
-
-let testPage = 'data:text/html,<body><button onblur="this.parentNode.removeChild(this);"><script>document.body.firstChild.focus();</script></body>';
-
-function test() {
-  waitForExplicitFinish();
-
-  gBrowser.selectedTab = gBrowser.addTab();
-  var browser = gBrowser.selectedBrowser;
-
-  browser.addEventListener("load", function () {
-    browser.removeEventListener("load", arguments.callee, true);
-    executeSoon(function () {
-      var testPageWin = content;
-
-      is(browser.contentDocumentAsCPOW.activeElement.localName, "button", "button is focused");
-
-      addEventListener("focus", function focusedWindow(event) {
-        if (!String(event.target.location).startsWith("data:"))
-          return;
-
-        removeEventListener("focus", focusedWindow, true);
-
-        
-        is(browser.contentDocumentAsCPOW.activeElement.localName, "body", "body is focused");
-
-        gBrowser.removeCurrentTab();
-        finish();
-      }, true);
-
-      
-      
-      
-      gBrowser.selectedTab = gBrowser.addTab();
-      gBrowser.removeCurrentTab();
-    });
-  }, true);
-
-  content.location = testPage;
+function getFocusedLocalName(browser) {
+  return ContentTask.spawn(browser, null, function* () {
+    return content.document.activeElement.localName;
+  });
 }
+
+add_task(function* () {
+  gBrowser.selectedTab = gBrowser.addTab(URL);
+  let browser = gBrowser.selectedBrowser;
+  yield BrowserTestUtils.browserLoaded(browser);
+
+  is((yield getFocusedLocalName(browser)), "button", "button is focused");
+
+  let promiseFocused = ContentTask.spawn(browser, null, function* () {
+    return new Promise(resolve => {
+      content.addEventListener("focus", function onFocus({target}) {
+        if (String(target.location).startsWith("data:")) {
+          content.removeEventListener("focus", onFocus);
+          resolve();
+        }
+      });
+    });
+  });
+
+  
+  
+  
+  gBrowser.selectedTab = gBrowser.addTab("about:blank");
+  yield BrowserTestUtils.browserLoaded(gBrowser.selectedBrowser);
+  gBrowser.removeCurrentTab();
+
+  
+  yield promiseFocused;
+
+  
+  is((yield getFocusedLocalName(browser)), "body", "body is focused");
+
+  
+  gBrowser.removeCurrentTab();
+});
