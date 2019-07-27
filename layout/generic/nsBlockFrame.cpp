71d38130b684dd53b742e60e8b610bb4e4f4bb48
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:15:49 +0000

    Bug 1077528 - part 2 - Make nsBlockFrame consider resize flags logically instead of physically. r=smontagu
    UltraBlame original commit: 2548c7a4a66a2b6d6203e90444ee30ca001ba7ad

--- layout/generic/nsBlockFrame.cpp
+++ layout/generic/nsBlockFrame.cpp
@@ -1130,7 +1130,7 @@ nsBlockFrame::Reflow(nsPresContext*           aPresContext,
   
   
   
-  if (!(GetStateBits() & NS_FRAME_IS_DIRTY) && reflowState->IsHResize()) {
+  if (!(GetStateBits() & NS_FRAME_IS_DIRTY) && reflowState->IsIResize()) {
     PrepareResizeReflow(state);
   }
 
@@ -2026,7 +2026,7 @@ nsBlockFrame::ReflowDirtyLines(nsBlockReflowState& aState)
 #endif
 
   bool selfDirty = (GetStateBits() & NS_FRAME_IS_DIRTY) ||
-                     (aState.mReflowState.IsVResize() &&
+                     (aState.mReflowState.IsBResize() &&
                       (GetStateBits() & NS_FRAME_CONTAINS_RELATIVE_HEIGHT));
 
   
@@ -2145,7 +2145,7 @@ nsBlockFrame::ReflowDirtyLines(nsBlockReflowState& aState)
     
     if (!line->IsDirty() &&
         aState.mReflowState.AvailableBSize() != NS_UNCONSTRAINEDSIZE &&
-        (deltaBCoord != 0 || aState.mReflowState.IsVResize() ||
+        (deltaBCoord != 0 || aState.mReflowState.IsBResize() ||
          aState.mReflowState.mFlags.mMustReflowPlaceholders) &&
         (line->IsBlock() || line->HasFloats() || line->HadFloatPushed())) {
       line->MarkDirty();
