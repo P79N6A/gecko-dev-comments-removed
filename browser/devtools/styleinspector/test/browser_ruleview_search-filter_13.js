b256d9a54305c0a82fd126be94393f32fc59d3e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:24:55 +0000

    Bug 1165380 - [rule view] Filter rules should also highlight stylesheet names. r=bgrins
    UltraBlame original commit: f7d2ce7b3433f1c3cebbd8332e20d6e7b2787469

---
+++ browser/devtools/styleinspector/test/browser_ruleview_search-filter_13.js
@@ -0,0 +1,35 @@
+
+
+
+
+"use strict";
+
+
+
+
+const SEARCH = "doc_urls_clickable.css: url";
+const TEST_URI = TEST_URL_ROOT + "doc_urls_clickable.html";
+
+add_task(function*() {
+  yield addTab(TEST_URI);
+  let {toolbox, inspector, view} = yield openRuleView();
+  yield selectNode(".relative1", inspector);
+  yield testAddTextInFilter(inspector, view);
+});
+
+function* testAddTextInFilter(inspector, ruleView) {
+  info("Setting filter text to \"" + SEARCH + "\"");
+
+  let win = ruleView.doc.defaultView;
+  let searchField = ruleView.searchField;
+  let onRuleViewFiltered = inspector.once("ruleview-filtered");
+
+  searchField.focus();
+  synthesizeKeys(SEARCH, win);
+  yield onRuleViewFiltered;
+
+  info("Check that the correct rules are visible");
+  is(ruleView.element.children.length, 1, "Should have 1 rules.");
+  is(getRuleViewRuleEditor(ruleView, 0).rule.selectorText, "element",
+    "First rule is inline element.");
+}
