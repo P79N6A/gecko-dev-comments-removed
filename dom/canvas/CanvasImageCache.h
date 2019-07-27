a4af7a7f4ab4df7dd879c82a7eb3ab55aa80ef2f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:58:07 +0000

    Bug 1092555 - Replace some occurences of gfxIntSize by gfx::IntSize in CanvasRenderingContext2D. r=nical"
    UltraBlame original commit: 1dd40ce6fc5a3774ecf69536b9b240135ffd6f2f

--- dom/canvas/CanvasImageCache.h
+++ dom/canvas/CanvasImageCache.h
@@ -43,7 +43,7 @@ public:
 
   static SourceSurface* Lookup(dom::Element* aImage,
                                dom::HTMLCanvasElement* aCanvas,
-                               gfxIntSize* aSize);
+                               gfx::IntSize* aSize);
 
   
 
