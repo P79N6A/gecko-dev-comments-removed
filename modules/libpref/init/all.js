f7c8376a62fe91fc0684ccb68808d2b2bd86c10e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:27:28 +0000

    Merge m-c to inbound. a=merge
    UltraBlame original commit: 47fa87252df0352a66c4698ceee71e351e8f2c8b

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -838,6 +838,9 @@ pref("devtools.remote.wifi.visible", false);
 pref("devtools.remote.tls-handshake-timeout", 10000);
 
 
+pref("devtools.devices.url", "https://code.cdn.mozilla.net/devices/devices.json");
+
+
 pref("view_source.syntax_highlight", true);
 pref("view_source.wrap_long_lines", false);
 pref("view_source.editor.external", false);
@@ -4529,7 +4532,7 @@ pref("browser.addon-watch.interval", 15000);
 #else
 pref("browser.addon-watch.interval", -1);
 #endif
-pref("browser.addon-watch.ignore", "[\"mochikit@mozilla.org\",\"special-powers@mozilla.org\"]");
+pref("browser.addon-watch.ignore", "[\"mochikit@mozilla.org\",\"special-powers@mozilla.org\",\"fxdevtools-adapters@mozilla.org\",\"fx-devtools\"]");
 
 pref("browser.addon-watch.percentage-limit", 5);
 
