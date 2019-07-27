d48aceb93c387581992d5ad9ed73a151a0640d0d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:00:18 +0000

    Backed out changeset 7ab2fdaadd1a (bug 1078337) for crashes and reftest failures.
    UltraBlame original commit: f547cf19d10415162339116376904b94241874ca

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -5333,13 +5333,15 @@ nsLayoutUtils::DrawSingleImage(nsRenderingContext*    aRenderingContext,
     image = aImage;
   }
 
+  nsRect dest = nsLayoutUtils::GetWholeImageDestination(imageSize, source,
+                                                        aDest);
   
   
   
   nsRect fill;
-  fill.IntersectRect(aDest, aDest);
-  return DrawImageInternal(aRenderingContext, aPresContext, image,
-                           aGraphicsFilter, aDest, fill, fill.TopLeft(),
+  fill.IntersectRect(aDest, dest);
+  return DrawImageInternal(aRenderingContext, aPresContext, aImage,
+                           aGraphicsFilter, dest, fill, fill.TopLeft(),
                            aDirty, aSVGContext, aImageFlags);
 }
 
