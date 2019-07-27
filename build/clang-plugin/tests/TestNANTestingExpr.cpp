7b15c6deb812e3c6cc58c75267903147e2a9a549
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:45:29 +0000

    Bug 927430 - Add a static analysis to detect is-NaN testing using |x == x| or |x != x|; r=jrmuizel
    UltraBlame original commit: fae8ad41a60416696c88968265cb7baef87bd648

---
+++ build/clang-plugin/tests/TestNANTestingExpr.cpp
@@ -0,0 +1,16 @@
+void test(bool x);
+void foo() {
+  float f, f2;
+  typedef double mydouble;
+  mydouble d;
+  double d2;
+  test(f == f); 
+  test(d == d); 
+  test(f != f); 
+  test(d != d); 
+  test(f != d);
+  test(d == (d - f));
+  test(f == f2);
+  test(d == d2);
+  test(d + 1 == d);
+}
