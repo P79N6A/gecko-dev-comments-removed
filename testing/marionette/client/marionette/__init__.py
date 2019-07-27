8feedd5bd5641e85e7794e0a031d21c23133132f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:15:22 +0000

    Bug 1107336: update marionette client runners to use the new marionette driver; r=jgriffin
    UltraBlame original commit: 473f27ac176083096d8d87c7a6af2018bdd4e9e5

--- testing/marionette/client/marionette/__init__.py
+++ testing/marionette/client/marionette/__init__.py
@@ -2,33 +2,7 @@
 
 
 
-from gestures import smooth_scroll, pinch
-from by import By
-from marionette import Marionette, HTMLElement, Actions, MultiActions
 from marionette_test import MarionetteTestCase, MarionetteJSTestCase, CommonTestCase, expectedFailure, skip, SkipTest
-from errors import (
-    ElementNotVisibleException,
-    ElementNotAccessibleException,
-    ErrorCodes,
-    FrameSendFailureError,
-    FrameSendNotInitializedError,
-    InvalidCookieDomainException,
-    InvalidElementStateException,
-    InvalidResponseException,
-    InvalidSelectorException,
-    JavascriptException,
-    MarionetteException,
-    MoveTargetOutOfBoundsException,
-    NoAlertPresentException,
-    NoSuchElementException,
-    NoSuchFrameException,
-    NoSuchWindowException,
-    ScriptTimeoutException,
-    StaleElementException,
-    TimeoutException,
-    UnableToSetCookieException,
-    XPathLookupException,
-)
 from runner import (
         B2GTestCaseMixin,
         B2GTestResultMixin,
@@ -50,6 +24,3 @@ from runner import (
         TestResult,
         TestResultCollection
 )
-from wait import Wait
-from date_time_value import DateTimeValue
-import decorators
