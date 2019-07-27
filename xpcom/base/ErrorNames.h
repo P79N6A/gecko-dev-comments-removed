ae82e6a771ab39e671be5a6880bcf32fd5ed1f7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:54:59 +0000

    Bug 907327 - Added ErrorNames.h and ErrorNames.cpp. r=nfroyd
    UltraBlame original commit: 9965ddd32fd643290eb3d1437cd5554039db5b93

---
+++ xpcom/base/ErrorNames.h
@@ -0,0 +1,24 @@
+
+
+
+
+
+#ifndef mozilla_ErrorNames_h
+#define mozilla_ErrorNames_h
+
+#include "nsError.h"
+
+class nsACString;
+
+namespace mozilla {
+
+
+
+
+
+
+void GetErrorName(nsresult rv, nsACString& name);
+
+}
+
+#endif 
