076e59434561645daaa4fcd2918ad4e2d1a78c50
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:28:23 +0000

    Merge fx-team to m-c a=merge
    UltraBlame original commit: a5ece94513431a417a36ba82e063fbb777308bc0

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -676,7 +676,7 @@ function gKeywordURIFixup({ target: browser, data: fixupInfo }) {
   
   
   let alternativeURI = deserializeURI(fixupInfo.fixedURI);
-  if (!fixupInfo.fixupUsedKeyword || !alternativeURI) {
+  if (!fixupInfo.fixupUsedKeyword || !alternativeURI || !alternativeURI.host) {
     return;
   }
 
