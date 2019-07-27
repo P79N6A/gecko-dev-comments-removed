a8701d8e18f612617a8078fea02affa86b7d008c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:26:23 +0000

    Bug 1173934 Show a message if a docshell fails to load due to SW intercept failure. r=ehsan r=jdm
    UltraBlame original commit: adf95d1cd0aa212d0c6738beeb985b2442f4be67

--- netwerk/protocol/http/InterceptedChannel.cpp
+++ netwerk/protocol/http/InterceptedChannel.cpp
@@ -230,15 +230,17 @@ InterceptedChannelChrome::FinishSynthesizedResponse()
 }
 
 NS_IMETHODIMP
-InterceptedChannelChrome::Cancel()
+InterceptedChannelChrome::Cancel(nsresult aStatus)
 {
+  MOZ_ASSERT(NS_FAILED(aStatus));
+
   if (!mChannel) {
     return NS_ERROR_FAILURE;
   }
 
   
   
-  nsresult rv = mChannel->AsyncAbort(NS_BINDING_ABORTED);
+  nsresult rv = mChannel->AsyncAbort(aStatus);
   NS_ENSURE_SUCCESS(rv, rv);
   return NS_OK;
 }
@@ -335,15 +337,17 @@ InterceptedChannelContent::FinishSynthesizedResponse()
 }
 
 NS_IMETHODIMP
-InterceptedChannelContent::Cancel()
+InterceptedChannelContent::Cancel(nsresult aStatus)
 {
+  MOZ_ASSERT(NS_FAILED(aStatus));
+
   if (!mChannel) {
     return NS_ERROR_FAILURE;
   }
 
   
   
-  nsresult rv = mChannel->AsyncAbort(NS_BINDING_ABORTED);
+  nsresult rv = mChannel->AsyncAbort(aStatus);
   NS_ENSURE_SUCCESS(rv, rv);
   mChannel = nullptr;
   mStreamListener = nullptr;
