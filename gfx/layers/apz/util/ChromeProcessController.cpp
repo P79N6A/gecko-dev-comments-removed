9dbccdb2073c15bb7f86e98ff77e583f2772d16b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:57:23 +0000

    Bug 1124452 - Set a displayport for the root scroll frame of the root document in the chrome process. r=kats,tn
    UltraBlame original commit: 638841804bcbf0c402698ab93ec192f720a0d0d7

--- gfx/layers/apz/util/ChromeProcessController.cpp
+++ gfx/layers/apz/util/ChromeProcessController.cpp
@@ -9,7 +9,10 @@
 #include "base/message_loop.h"  
 #include "mozilla/layers/CompositorParent.h"
 #include "mozilla/layers/APZCCallbackHelper.h"
+#include "nsIDocument.h"
+#include "nsIPresShell.h"
 #include "nsLayoutUtils.h"
+#include "nsView.h"
 
 using namespace mozilla;
 using namespace mozilla::layers;
@@ -21,6 +24,35 @@ ChromeProcessController::ChromeProcessController(nsIWidget* aWidget)
 {
   
   MOZ_ASSERT(NS_IsMainThread());
+
+  mUILoop->PostTask(
+      FROM_HERE,
+      NewRunnableMethod(this, &ChromeProcessController::InitializeRoot));
+}
+
+void
+ChromeProcessController::InitializeRoot()
+{
+  
+  
+  
+  
+  
+  
+  
+  nsView* view = nsView::GetViewFor(mWidget);
+  MOZ_ASSERT(view);
+  nsIPresShell* presShell = view->GetPresShell();
+  MOZ_ASSERT(presShell);
+  MOZ_ASSERT(presShell->GetDocument());
+  nsIContent* content = presShell->GetDocument()->GetDocumentElement();
+  MOZ_ASSERT(content);
+  uint32_t presShellId;
+  FrameMetrics::ViewID viewId;
+  if (APZCCallbackHelper::GetOrCreateScrollIdentifiers(content, &presShellId, &viewId)) {
+    nsLayoutUtils::SetDisplayPortMargins(content, presShell, ScreenMargin(), 0,
+        nsLayoutUtils::RepaintMode::DoNotRepaint);
+  }
 }
 
 void
