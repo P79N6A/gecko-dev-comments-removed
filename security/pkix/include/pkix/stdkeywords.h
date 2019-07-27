338f571f63caaa07d5dc35a30d4fb5d4323396d8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:42:55 +0000

    Bug 1115906, Part 1: Add workarounds for missing final/override support in GCC before version 4.7, r=keeler
    UltraBlame original commit: 75174df7140a8327472f9275bf6cbb137481e612

--- security/pkix/include/pkix/stdkeywords.h
+++ security/pkix/include/pkix/stdkeywords.h
@@ -22,14 +22,22 @@
 
 
 
-#ifndef mozilla_pkix__nullptr_h
-#define mozilla_pkix__nullptr_h
-
+#ifndef mozilla_pkix__stdkeywords_h
+#define mozilla_pkix__stdkeywords_h
 
 #if defined(__GNUC__) && !defined(__clang__)
+
+
 #if __GNUC__ * 100 + __GNUC_MINOR__ < 406
 #define nullptr __null
 #endif
+
+
+#if __GNUC__ * 100 + __GNUC_MINOR__ < 407
+#define final
+#define override
 #endif
+
+#endif 
 
 #endif 
