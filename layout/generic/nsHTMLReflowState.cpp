306d35785832bb56ad0bf284fec466c14d754e3b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:31:34 +0000

    Bug 1131451 followup - Remove one more bidi-in-vertical-mode hack that is no longer required. r=dholbert
    UltraBlame original commit: 54c94c8b16c93ec78c2bb6362ccbd455b165105f

--- layout/generic/nsHTMLReflowState.cpp
+++ layout/generic/nsHTMLReflowState.cpp
@@ -1268,15 +1268,9 @@ nsHTMLReflowState::CalculateHypotheticalBox(nsPresContext*    aPresContext,
   nsSize containerSize = containingBlock->GetStateBits() & NS_FRAME_IN_REFLOW
     ? cbrs->ComputedSizeAsContainerIfConstrained()
     : containingBlock->GetSize();
-  LogicalPoint placeholderOffset(wm, aPlaceholderFrame->GetOffsetTo(containingBlock),
-                                 containerSize);
-
-  
-  if (wm.IsVertical() && !wm.IsBidiLTR()) {
-    placeholderOffset.I(wm) = cbrs->ComputedHeight() +
-      cbrs->ComputedLogicalBorderPadding().TopBottom(cbwm) -
-      placeholderOffset.I(wm);
-  }
+  LogicalPoint
+    placeholderOffset(wm, aPlaceholderFrame->GetOffsetTo(containingBlock),
+                      containerSize);
 
   
   
