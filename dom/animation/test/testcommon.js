0172af6ac5fa6f3d66b8788de7f2ef427c6c7354
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:24:50 +0000

    Bug 1141710, part 7 - Create helpers to get the startTime corresponding to various points through the active duration. r=dholbert
    UltraBlame original commit: e1330135150d1d502d83ef79de95e1ac18a235e5

--- dom/animation/test/testcommon.js
+++ dom/animation/test/testcommon.js
@@ -44,3 +44,20 @@ function waitForFrame() {
 function waitForAllPlayers(players) {
   return Promise.all(players.map(function(player) { return player.ready; }));
 }
+
+
+
+
+
+
+function waitForTwoAnimationFrames()
+{
+   return new Promise(function(resolve, reject) {
+     window.requestAnimationFrame(function() {
+       window.requestAnimationFrame(function() {
+         resolve();
+       });
+     });
+   });
+}
+
