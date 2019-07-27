9909447c811f9c5bbbd00ba149a3ffa64d4613fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:59:53 +0000

    Bug 832700 - Add private browsing to b2g, tests r=smaug
    UltraBlame original commit: 89b935e3119d64d6235751bab28f289d99fd8a17

---
+++ dom/browser-element/mochitest/browserElement_PrivateBrowsing.js
@@ -0,0 +1,50 @@
+
+
+
+
+"use strict";
+
+SimpleTest.waitForExplicitFinish();
+browserElementTestHelpers.setEnabledPref(true);
+browserElementTestHelpers.addPermission();
+
+function createFrame(aIsPrivate) {
+  var iframe = document.createElement("iframe");
+  SpecialPowers.wrap(iframe).mozbrowser = true;
+  if (aIsPrivate) {
+    iframe.setAttribute("mozprivatebrowsing", "true");
+  }
+  return iframe;
+}
+
+function createTest(aIsPrivate, aExpected, aClearStorage) {
+  info("createTest " + aIsPrivate + " " + aExpected);
+  var deferred = Promise.defer();
+
+  var iframe = createFrame(aIsPrivate);
+  document.body.appendChild(iframe);
+
+  iframe.addEventListener("mozbrowsershowmodalprompt", function(e) {
+    is(e.detail.message, aExpected, "Checking localstorage");
+    deferred.resolve();
+  });
+
+  var src = "file_browserElement_PrivateBrowsing.html";
+  iframe.src = aClearStorage ? src + "?clear=true" : src;
+
+  return deferred.promise;
+}
+
+function runTest() {
+  
+  
+  
+  
+  createTest(false, "CLEAR", true)
+  .then(() => { return createTest(false, "EMPTY", false); })
+  .then(() => { return createTest(false, "bar", false); })
+  .then(() => { return createTest(true, "EMPTY", false); })
+  .then(SimpleTest.finish);
+}
+
+addEventListener("testready", runTest);
