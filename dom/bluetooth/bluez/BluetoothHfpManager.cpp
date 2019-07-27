001272b97b82c9fc4d5cbe730a726e3dff062b79
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:43:49 +0000

    Bug 1146355: Prepare BlueZ for Bluetooth v2, r=brsun
    UltraBlame original commit: bdd00b0f2f483bd32fc6dd4f24b692511a43cd13

--- dom/bluetooth/bluez/BluetoothHfpManager.cpp
+++ dom/bluetooth/bluez/BluetoothHfpManager.cpp
@@ -418,6 +418,12 @@ BluetoothHfpManager::Reset()
 bool
 BluetoothHfpManager::Init()
 {
+#ifdef MOZ_B2G_BT_API_V2
+  
+  MOZ_ASSERT(IsMainProcess());
+#else
+
+#endif
   MOZ_ASSERT(NS_IsMainThread());
 
   nsCOMPtr<nsIObserverService> obs = services::GetObserverService();
@@ -567,7 +573,11 @@ BluetoothHfpManager::HandleVolumeChanged(nsISupports* aSubject)
   
   
 
+#ifdef MOZ_B2G_BT_API_V2
+  RootedDictionary<dom::SettingChangeNotification> setting(nsContentUtils::RootingCx());
+#else
   RootedDictionary<SettingChangeNotification> setting(nsContentUtils::RootingCx());
+#endif
   if (!WrappedJSToDictionary(aSubject, setting)) {
     return;
   }
@@ -1081,11 +1091,19 @@ BluetoothHfpManager::ReceiveSocketData(BluetoothSocket* aSocket,
     }
 #endif
   } else {
+#ifdef MOZ_B2G_BT_API_V2
+    nsCString warningMsg;
+    warningMsg.Append(NS_LITERAL_CSTRING("Unsupported AT command: "));
+    warningMsg.Append(msg);
+    warningMsg.Append(NS_LITERAL_CSTRING(", reply with ERROR"));
+    BT_WARNING(warningMsg.get());
+#else
     nsCString warningMsg;
     warningMsg.AppendLiteral("Unsupported AT command: ");
     warningMsg.Append(msg);
     warningMsg.AppendLiteral(", reply with ERROR");
     BT_WARNING(warningMsg.get());
+#endif
 
     SendLine("ERROR");
     return;
@@ -1654,7 +1672,11 @@ BluetoothHfpManager::HandleCallStateChanged(uint32_t aCallIndex,
           GetNumberOfCalls(nsITelephonyService::CALL_STATE_DISCONNECTED)) {
         
         
+#ifdef MOZ_B2G_BT_API_V2
+        if (!(aError.Equals(NS_LITERAL_STRING("BusyError")))) {
+#else
         if (!(aError.EqualsLiteral("BusyError"))) {
+#endif
           DisconnectSco();
         } else {
           
@@ -1888,8 +1910,12 @@ BluetoothHfpManager::OnScoConnectSuccess()
 {
   
   if (mScoRunnable) {
+#ifdef MOZ_B2G_BT_API_V2
+    DispatchReplySuccess(mScoRunnable);
+#else
     DispatchBluetoothReply(mScoRunnable,
                            BluetoothValue(true), EmptyString());
+#endif
     mScoRunnable = nullptr;
   }
 
@@ -1903,9 +1929,13 @@ void
 BluetoothHfpManager::OnScoConnectError()
 {
   if (mScoRunnable) {
+#ifdef MOZ_B2G_BT_API_V2
+    DispatchReplyError(mScoRunnable,
+                       NS_LITERAL_STRING("Failed to create SCO socket!"));
+#else
     NS_NAMED_LITERAL_STRING(replyError, "Failed to create SCO socket!");
     DispatchBluetoothReply(mScoRunnable, BluetoothValue(), replyError);
-
+#endif
     mScoRunnable = nullptr;
   }
 
