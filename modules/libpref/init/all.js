8efa519e995801ff7f5a4edfdf19c597459e2daa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:31:56 +0000

    Bug 998863: Asynchronous Plugin Initialization, Part 9: PluginModuleParent changes; r=jimm
    UltraBlame original commit: 8a4c2bf5ec09977e94cc055267c0bc6280914966

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2367,6 +2367,8 @@ pref("dom.ipc.plugins.reportCrashURL", true);
 
 pref("dom.ipc.plugins.unloadTimeoutSecs", 30);
 
+pref("dom.ipc.plugins.asyncInit", false);
+
 pref("dom.ipc.processCount", 1);
 
 
