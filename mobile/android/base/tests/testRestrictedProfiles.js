062ee97f83922d7e0774e3f342df838867b8e036
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:18:55 +0000

    Bug 1058150 - Use parental controls to block things in guest mode. r=mfinkle
    UltraBlame original commit: 43e6d3d43b50f7a67880cf08b9d0870510c55f8e

--- mobile/android/base/tests/testRestrictedProfiles.js
+++ mobile/android/base/tests/testRestrictedProfiles.js
@@ -18,28 +18,38 @@ add_task(function test_isUserRestricted() {
   
   do_check_false(pc.parentalControlsEnabled);
 
-  
+  do_check_true(pc.isAllowed(Ci.nsIParentalControlsService.DOWNLOAD));
+  do_check_true(pc.isAllowed(Ci.nsIParentalControlsService.INSTALL_EXTENSION));
+  do_check_true(pc.isAllowed(Ci.nsIParentalControlsService.INSTALL_APP));
+  do_check_true(pc.isAllowed(Ci.nsIParentalControlsService.VISIT_FILE_URLS));
+  do_check_true(pc.isAllowed(Ci.nsIParentalControlsService.SHARE));
+  do_check_true(pc.isAllowed(Ci.nsIParentalControlsService.BOOKMARK));
+  do_check_true(pc.isAllowed(Ci.nsIParentalControlsService.INSTALL_EXTENSION));
+
+  run_next_test();
 });
 
+add_task(function test_getUserRestrictions() {
+  
+  
+  let restrictions = "{}";
 
+  var jenv = null;
+  try {
+    jenv = JNI.GetForThread();
+    var geckoAppShell = JNI.LoadClass(jenv, "org.mozilla.gecko.RestrictedProfile", {
+      static_methods: [
+        { name: "getUserRestrictions", sig: "()Ljava/lang/String;" },
+      ],
+    });
+    restrictions = JNI.ReadString(jenv, geckoAppShell.getUserRestrictions());
+  } finally {
+    if (jenv) {
+      JNI.UnloadClasses(jenv);
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  do_check_eq(restrictions, "{}");
+});
 
 run_next_test();
