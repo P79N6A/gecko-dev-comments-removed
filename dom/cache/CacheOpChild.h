68ab735f5b38bffb596ecd0531fbf7e475b99374
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:47:14 +0000

    Bug 1110485 P1 Refactor Cache IPC requests to use a separate actor. r=baku
    UltraBlame original commit: 12deb121d126d59bec12953eecdf5bfe0d1af7bf

---
+++ dom/cache/CacheOpChild.h
@@ -0,0 +1,78 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_cache_CacheOpChild_h
+#define mozilla_dom_cache_CacheOpChild_h
+
+#include "mozilla/dom/cache/ActorChild.h"
+#include "mozilla/dom/cache/PCacheOpChild.h"
+#include "mozilla/dom/cache/TypeUtils.h"
+#include "nsRefPtr.h"
+
+class nsIGlobalObject;
+
+namespace mozilla {
+namespace dom {
+
+class Promise;
+
+namespace cache {
+
+class CacheOpChild final : public PCacheOpChild
+                         , public ActorChild
+                         , public TypeUtils
+{
+public:
+  CacheOpChild(Feature* aFeature, nsIGlobalObject* aGlobal,
+               nsISupports* aParent, Promise* aPromise);
+  ~CacheOpChild();
+
+private:
+  
+  virtual void
+  ActorDestroy(ActorDestroyReason aReason) override;
+
+  virtual bool
+  Recv__delete__(const nsresult& aStatus, const CacheOpResult& aResult) override;
+
+  
+  virtual void
+  StartDestroy() override;
+
+  
+  virtual nsIGlobalObject*
+  GetGlobalObject() const override;
+
+#ifdef DEBUG
+  virtual void
+  AssertOwningThread() const override;
+#endif
+
+  virtual CachePushStreamChild*
+  CreatePushStream(nsIAsyncInputStream* aStream) override;
+
+  
+  void
+  HandleResponse(const PCacheResponseOrVoid& aResponseOrVoid);
+
+  void
+  HandleResponseList(const nsTArray<PCacheResponse>& aResponseList);
+
+  void
+  HandleRequestList(const nsTArray<PCacheRequest>& aRequestList);
+
+  nsCOMPtr<nsIGlobalObject> mGlobal;
+  nsCOMPtr<nsISupports> mParent;
+  nsRefPtr<Promise> mPromise;
+
+  NS_DECL_OWNINGTHREAD
+};
+
+} 
+} 
+} 
+
+#endif
