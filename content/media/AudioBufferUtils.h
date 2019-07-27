bea2416976766078a0bc7ceae448e1f015babac0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:59:47 +0000

    Bug 848954 - Part 8 - Implement classes to ensure safe audio buffer manipulation. r=roc
    UltraBlame original commit: e7e57c1f224e51b4599d45eee240ab90d26200b5

---
+++ content/media/AudioBufferUtils.h
@@ -0,0 +1,149 @@
+
+
+
+
+
+#ifndef MOZILLA_SCRATCHBUFFER_H_
+#define MOZILLA_SCRATCHBUFFER_H_
+#include <mozilla/PodOperations.h>
+#include <algorithm>
+
+namespace mozilla {
+
+
+
+
+
+
+static inline uint32_t FramesToSamples(uint32_t aChannels, uint32_t aFrames) {
+  return aFrames * aChannels;
+}
+
+static inline uint32_t SamplesToFrames(uint32_t aChannels, uint32_t aSamples) {
+  MOZ_ASSERT(!(aSamples % aChannels), "Frame alignment is wrong.");
+  return aSamples / aChannels;
+}
+
+
+
+
+
+
+template<typename T, uint32_t CHANNELS>
+class AudioCallbackBufferWrapper
+{
+public:
+  AudioCallbackBufferWrapper()
+    : mBuffer(nullptr),
+      mSamples(0),
+      mSampleWriteOffset(0)
+  {}
+  
+
+
+
+  void SetBuffer(T* aBuffer, uint32_t aFrames) {
+    MOZ_ASSERT(!mBuffer && !mSamples && mSampleWriteOffset,
+        "SetBuffer called twice.");
+    mBuffer = aBuffer;
+    mSamples = FramesToSamples(CHANNELS, aFrames);
+    mSampleWriteOffset = 0;
+  }
+
+  
+
+
+
+  void WriteFrames(T* aBuffer, uint32_t aFrames) {
+    MOZ_ASSERT(aFrames <= Available(),
+        "Writing more that we can in the audio buffer.");
+
+    PodCopy(mBuffer + mSampleWriteOffset, aBuffer, FramesToSamples(CHANNELS,
+                                                                   aFrames));
+    mSampleWriteOffset += FramesToSamples(CHANNELS, aFrames);
+  }
+
+  
+
+
+  uint32_t Available() {
+    return SamplesToFrames(CHANNELS, mSamples - mSampleWriteOffset);
+  }
+
+  
+
+
+
+  void BufferFilled() {
+    MOZ_ASSERT(Available() == 0,
+        "Audio Buffer is not full by the end of the callback.");
+    MOZ_ASSERT(mSamples && mSampleWriteOffset && mBuffer,
+               "Buffer not set.");
+    mSamples = 0;
+    mSampleWriteOffset = 0;
+    mBuffer = nullptr;
+  }
+
+private:
+  
+
+  T* mBuffer;
+  
+  uint32_t mSamples;
+  
+
+  uint32_t mSampleWriteOffset;
+};
+
+
+
+
+
+
+
+template<typename T, uint32_t BLOCK_SIZE, uint32_t CHANNELS>
+class SpillBuffer
+{
+public:
+  SpillBuffer()
+  : mPosition(0)
+  {
+    PodArrayZero(mBuffer);
+  }
+  
+
+  uint32_t Empty(AudioCallbackBufferWrapper<T, CHANNELS>& aBuffer) {
+    uint32_t framesToWrite = std::min(aBuffer.Available(),
+                                      SamplesToFrames(CHANNELS, mPosition));
+
+    aBuffer.WriteFrames(mBuffer, framesToWrite);
+
+    mPosition -= FramesToSamples(CHANNELS, framesToWrite);
+
+    return framesToWrite;
+  }
+  
+
+  uint32_t Fill(T* aInput, uint32_t aFrames) {
+    uint32_t framesToWrite = std::min(aFrames,
+                                      BLOCK_SIZE - SamplesToFrames(CHANNELS,
+                                                                   mPosition));
+
+    PodCopy(mBuffer + mPosition, aInput, FramesToSamples(CHANNELS,
+                                                         framesToWrite));
+
+    mPosition += FramesToSamples(CHANNELS, framesToWrite);
+
+    return framesToWrite;
+  }
+private:
+  
+  T mBuffer[BLOCK_SIZE * CHANNELS];
+  
+
+  uint32_t mPosition;
+};
+
+}
+
+#endif 
---
+++ content/media/compiledtest/TestAudioBuffers.cpp
@@ -0,0 +1,59 @@
+
+
+
+
+
+#include <stdint.h>
+#include <assert.h>
+#include "AudioBufferUtils.h"
+
+const uint32_t FRAMES = 256;
+const uint32_t CHANNELS = 2;
+const uint32_t SAMPLES = CHANNELS * FRAMES;
+
+int main() {
+  mozilla::AudioCallbackBufferWrapper<float, CHANNELS> mBuffer;
+  mozilla::SpillBuffer<float, 128, CHANNELS> b;
+  float fromCallback[SAMPLES];
+  float other[SAMPLES];
+
+  for (uint32_t i = 0; i < SAMPLES; i++) {
+    other[i] = 1.0;
+    fromCallback[i] = 0.0;
+  }
+
+  
+  mBuffer.SetBuffer(fromCallback, FRAMES);
+
+  
+  assert(b.Fill(other, 15) == 15);
+  assert(b.Fill(other, 17) == 17);
+  for (uint32_t i = 0; i < 32 * CHANNELS; i++) {
+    other[i] = 0.0;
+  }
+
+  
+  assert(b.Empty(mBuffer) == 32);
+
+  
+  assert(mBuffer.Available() == FRAMES - 32);
+
+  
+  mBuffer.WriteFrames(other + 32 * CHANNELS, FRAMES - 32);
+
+  
+  assert(mBuffer.Available() == 0);
+
+  for (uint32_t i = 0 ; i < SAMPLES; i++) {
+    if (fromCallback[i] != 1.0) {
+      fprintf(stderr, "Difference at %d (%f != %f)\n", i, fromCallback[i], 1.0);
+      assert(false);
+    }
+  }
+
+  assert(b.Fill(other, FRAMES) == 128);
+  assert(b.Fill(other, FRAMES) == 0);
+  assert(b.Empty(mBuffer) == 0);
+
+  return 0;
+}
