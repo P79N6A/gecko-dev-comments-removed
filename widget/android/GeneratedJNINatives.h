129badd1e0e0cb8ee52659e9aa128ab54ebfec9d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:26:18 +0000

    Bug 1178850 - Update generated files; r=me
    UltraBlame original commit: 79085d3894e8877c9b998c18c7085081a713b430

---
+++ widget/android/GeneratedJNINatives.h
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+#ifndef GeneratedJNINatives_h
+#define GeneratedJNINatives_h
+
+#include "GeneratedJNIWrappers.h"
+#include "mozilla/jni/Natives.h"
+
+namespace mozilla {
+namespace widget {
+
+
+} 
+} 
+#endif 
