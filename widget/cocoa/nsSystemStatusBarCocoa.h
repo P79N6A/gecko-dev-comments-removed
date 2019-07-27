6dac1c22e7aa22d41f0502f31b3df828d7a449f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:14:02 +0000

    Bug 1037433 - Add nsSystemStatusBarCocoa implementation of nsISystemStatusBar. . r=smichaud
    This can be accessed from script using Cc["@mozilla.org/widget/macsystemstatusbar;1"].getService(Ci.nsISystemStatusBar).
    
    UltraBlame original commit: bfa9065c5da1eb93c0f8ec10e5197af4cefe68e8

---
+++ widget/cocoa/nsSystemStatusBarCocoa.h
@@ -0,0 +1,40 @@
+
+
+
+
+
+#ifndef nsSystemStatusBarCocoa_h_
+#define nsSystemStatusBarCocoa_h_
+
+#include "nsISystemStatusBar.h"
+#include "nsClassHashtable.h"
+#include "nsAutoPtr.h"
+
+class nsStandaloneNativeMenu;
+@class NSStatusItem;
+
+class nsSystemStatusBarCocoa : public nsISystemStatusBar
+{
+public:
+  nsSystemStatusBarCocoa() {}
+
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSISYSTEMSTATUSBAR
+
+protected:
+  virtual ~nsSystemStatusBarCocoa() {}
+
+  struct StatusItem
+  {
+    StatusItem(nsStandaloneNativeMenu* aMenu);
+    ~StatusItem();
+
+  private:
+    nsRefPtr<nsStandaloneNativeMenu> mMenu;
+    NSStatusItem* mStatusItem;
+  };
+
+  nsClassHashtable<nsISupportsHashKey, StatusItem> mItems;
+};
+
+#endif 
