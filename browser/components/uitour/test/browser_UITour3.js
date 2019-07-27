db4bb8b9fa643d465d2d104330e9caa536391eb3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:22:33 +0000

    Bug 1113038 - Move test_openSearchPanel from browser_UITour3.js to its own file (browser_openSearchPanel.js).
    UltraBlame original commit: d0d90fe86052d020ebc16ee47bc1b190fc29a1cd

--- browser/components/uitour/test/browser_UITour3.js
+++ browser/components/uitour/test/browser_UITour3.js
@@ -176,24 +176,4 @@ let tests = [
     
     waitForCondition(() => searchbar.value == "", done, "Search term cleared");
   },
-
-  function test_openSearchPanel(done) {
-    let searchbar = document.getElementById("searchbar");
-
-    
-    
-    Services.prefs.setBoolPref("browser.search.suggest.enabled", false);
-    registerCleanupFunction(() => {
-      Services.prefs.clearUserPref("browser.search.suggest.enabled");
-    });
-
-    ok(!searchbar.textbox.open, "Popup starts as closed");
-    gContentAPI.openSearchPanel(() => {
-      ok(searchbar.textbox.open, "Popup was opened");
-      searchbar.textbox.closePopup();
-      ok(!searchbar.textbox.open, "Popup was closed");
-      done();
-    });
-  },
-
 ];
---
+++ browser/components/uitour/test/browser_openSearchPanel.js
@@ -0,0 +1,36 @@
+
+
+
+"use strict";
+
+let gTestTab;
+let gContentAPI;
+let gContentWindow;
+
+Components.utils.import("resource:///modules/UITour.jsm");
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+function test() {
+  UITourTest();
+}
+
+let tests = [
+  function test_openSearchPanel(done) {
+    let searchbar = document.getElementById("searchbar");
+
+    
+    
+    Services.prefs.setBoolPref("browser.search.suggest.enabled", false);
+    registerCleanupFunction(() => {
+      Services.prefs.clearUserPref("browser.search.suggest.enabled");
+    });
+
+    ok(!searchbar.textbox.open, "Popup starts as closed");
+    gContentAPI.openSearchPanel(() => {
+      ok(searchbar.textbox.open, "Popup was opened");
+      searchbar.textbox.closePopup();
+      ok(!searchbar.textbox.open, "Popup was closed");
+      done();
+    });
+  },
+];
