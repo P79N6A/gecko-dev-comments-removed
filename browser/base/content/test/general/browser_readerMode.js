6d8552680f323688a1a330c15162ad5ff1d36bed
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:56:07 +0000

    Bug 1123471 - Create basic browser-chrome test for reader mode toolbar button. r=jaws
    UltraBlame original commit: 34cada2f7c27ff5c5c3574946422754ca64b0605

---
+++ browser/base/content/test/general/browser_readerMode.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+const PREF = "reader.parse-on-load.enabled";
+
+const TEST_PATH = "http://example.com/browser/browser/base/content/test/general/";
+
+let readerButton = document.getElementById("reader-mode-button");
+
+add_task(function* () {
+  registerCleanupFunction(function() {
+    Services.prefs.clearUserPref(PREF);
+    while (gBrowser.tabs.length > 1) {
+      gBrowser.removeCurrentTab();
+    }
+  });
+
+  
+  Services.prefs.setBoolPref(PREF, true);
+
+  let tab = gBrowser.selectedTab = gBrowser.addTab();
+  is_element_hidden(readerButton, "Reader mode button is not present on a new tab");
+
+  
+  let url = TEST_PATH + "readerModeArticle.html";
+  yield promiseTabLoadEvent(tab, url);
+  yield promiseWaitForCondition(() => !readerButton.hidden);
+  is_element_visible(readerButton, "Reader mode button is present on a reader-able page");
+
+  readerButton.click();
+  yield promiseTabLoadEvent(tab);
+
+  ok(gBrowser.selectedBrowser.currentURI.spec.startsWith("about:reader"), "about:reader loaded after clicking reader mode button");
+  is_element_visible(readerButton, "Reader mode button is present on about:reader");
+
+  readerButton.click();
+  yield promiseTabLoadEvent(tab);
+  is(gBrowser.selectedBrowser.currentURI.spec, url, "Original page loaded after clicking active reader mode button");
+
+  
+  let newTab = gBrowser.selectedTab = gBrowser.addTab();
+  yield promiseTabLoadEvent(newTab, TEST_PATH + "download_page.html");
+  yield promiseWaitForCondition(() => readerButton.hidden);
+  is_element_hidden(readerButton, "Reader mode button is not present on a non-reader-able page");
+
+  
+  gBrowser.removeCurrentTab();
+  yield promiseWaitForCondition(() => !readerButton.hidden);
+  is_element_visible(readerButton, "Reader mode button is present on a reader-able page");
+});
