15ba3110304d775f49d790749f42922c1967620f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:31:32 +0000

    Bug 1095278 - Part 2: Disable testAboutPasswords on non-Nightly builds. r=rnewman
    UltraBlame original commit: a32868881bc7d9a21650ad198578c527383e0ec9

--- mobile/android/base/tests/testAboutPasswords.java
+++ mobile/android/base/tests/testAboutPasswords.java
@@ -4,8 +4,22 @@
 
 package org.mozilla.gecko.tests;
 
+import org.mozilla.gecko.AppConstants;
+
 public class testAboutPasswords extends JavascriptTest {
+    private static final String LOGTAG = testAboutPasswords.class.getSimpleName();
+
     public testAboutPasswords() {
         super("testAboutPasswords.js");
+    }
+
+    @Override
+    public void testJavascript() throws Exception {
+        
+        if (!AppConstants.NIGHTLY_BUILD) {
+            mAsserter.dumpLog(LOGTAG + " is disabled on non-Nightly builds: returning");
+            return;
+        }
+        super.testJavascript();
     }
 }
