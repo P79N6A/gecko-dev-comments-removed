980395e7d5bb21d993bef77f7812743c05117020
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:55:51 +0000

    Backed out changeset 57e490053263 (bug 1018928) for causing the intermittent test failures in bug 1032991
    UltraBlame original commit: 2ff73cfaac1300bc4fae71eb3796a9a411b39868

--- mobile/android/base/tests/testGetUserMedia.java
+++ mobile/android/base/tests/testGetUserMedia.java
@@ -8,13 +8,8 @@ public class testGetUserMedia extends BaseTest {
         String GUM_URL = getAbsoluteUrl("/robocop/robocop_getusermedia.html");
 
         String GUM_MESSAGE = "Would you like to share your camera and microphone with";
-        String GUM_ALLOW = "^Share$";
-        String GUM_DENY = "^Don't Share$";
-
-        String GUM_PAGE_FAILED = "failed gumtest";
-        String GUM_PAGE_AUDIO = "audio gumtest";
-        String GUM_PAGE_VIDEO = "video gumtest";
-        String GUM_PAGE_AUDIOVIDEO = "audiovideo gumtest";
+        String GUM_ALLOW = "Share";
+        String GUM_DENY = "Don't share";
 
         blockForGeckoReady();
 
@@ -23,45 +18,13 @@ public class testGetUserMedia extends BaseTest {
         
         
         
-        if (Build.VERSION.SDK_INT < 9) {
-            return;
+        if (Build.VERSION.SDK_INT >= 9) {
+            if (Camera.getNumberOfCameras() > 0) {
+                
+                inputAndLoadUrl(GUM_URL);
+                waitForText(GUM_MESSAGE);
+                mAsserter.is(mSolo.searchText(GUM_MESSAGE), true, "GetUserMedia doorhanger has been displayed");
+            }
         }
-
-        if (Camera.getNumberOfCameras() <= 0) {
-            return;
-        }
-        
-        inputAndLoadUrl(GUM_URL);
-        waitForText(GUM_MESSAGE);
-        mAsserter.is(mSolo.searchText(GUM_MESSAGE), true, "GetUserMedia doorhanger has been displayed");
-        mSolo.clickOnButton(GUM_DENY);
-        verifyPageTitle(GUM_PAGE_FAILED);
-
-        inputAndLoadUrl(GUM_URL);
-        waitForText(GUM_MESSAGE);
-        
-        mSolo.clickOnText("Back facing camera");
-        mSolo.clickOnText("Choose a tab to stream");
-        mSolo.clickOnButton(GUM_ALLOW);
-        mSolo.clickOnText("gUM Test Page");
-        verifyPageTitle(GUM_PAGE_AUDIOVIDEO);
-
-        inputAndLoadUrl(GUM_URL);
-        waitForText(GUM_MESSAGE);
-        mSolo.clickOnText("Back facing camera");
-        mSolo.clickOnText("No Video");
-        mSolo.clickOnButton(GUM_ALLOW);
-        verifyPageTitle(GUM_PAGE_AUDIO);
-
-        inputAndLoadUrl(GUM_URL);
-        waitForText(GUM_MESSAGE);
-        
-        mSolo.clickOnText("Back facing camera");
-        mSolo.clickOnText("Choose a tab to stream");
-        mSolo.clickOnText("Microphone 1");
-        mSolo.clickOnText("No Audio");
-        mSolo.clickOnButton(GUM_ALLOW);
-        mSolo.clickOnText("gUM Test Page");
-        verifyPageTitle(GUM_PAGE_VIDEO);
     }
 }
