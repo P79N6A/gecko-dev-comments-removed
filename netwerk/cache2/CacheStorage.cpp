b1ee0c632e2aba9a17fcf732ed83224f2a0fab27
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:09:32 +0000

    Bug 1095281 - Open entries from a chosen appcache as read-only, r=michal
    UltraBlame original commit: a1e78007197e5759dd41b2f91034b7c2413df8b8

--- netwerk/cache2/CacheStorage.cpp
+++ netwerk/cache2/CacheStorage.cpp
@@ -69,6 +69,11 @@ NS_IMETHODIMP CacheStorage::AsyncOpenURI(nsIURI *aURI,
   if (LookupAppCache()) {
     rv = ChooseApplicationCache(noRefURI, getter_AddRefs(appCache));
     NS_ENSURE_SUCCESS(rv, rv);
+
+    if (appCache) {
+      
+      aFlags &= ~nsICacheStorage::OPEN_TRUNCATE;
+    }
   }
 
   if (appCache) {
