807c453c7a644b7309c207fea9471eecc522d154
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:28:07 +0000

    Bug 1049087 - pre-populate the whitelist for screensharing in Fx33 r=jesup
    UltraBlame original commit: efb085f286fd70e54c66db881745e4035df52c11

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -383,10 +383,10 @@ pref("media.getusermedia.screensharing.enabled", true);
 #endif
 
 #ifdef RELEASE_BUILD
-pref("media.getusermedia.screensharing.allowed_domains", "");
+pref("media.getusermedia.screensharing.allowed_domains", "webex.com,*.webex.com,collaborate.com,*.collaborate.com");
 #else
  
-pref("media.getusermedia.screensharing.allowed_domains", "mozilla.github.io");
+pref("media.getusermedia.screensharing.allowed_domains", "mozilla.github.io,webex.com,*.webex.com,collaborate.com,*.collaborate.com");
 #endif
 
 pref("media.getusermedia.screensharing.allow_on_old_platforms", false);
