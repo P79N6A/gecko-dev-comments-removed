de2b7f7a9b4ab53aa03de4c49ef4a562dcdfe350
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:12:18 +0000

    Bug 926838 - [Part 4] Implement AlignedTArray for 32-byte alignment is required by openmax dl. Also modify callers. r=ehsan
    UltraBlame original commit: af920a0895a7c461de2648464601e7e9dddb3d19

---
+++ dom/media/webaudio/AlignedTArray.h
@@ -0,0 +1,85 @@
+
+
+
+
+
+
+#ifndef AlignedTArray_h__
+#define AlignedTArray_h__
+
+#include "mozilla/Alignment.h"
+#include "nsTArray.h"
+
+
+
+
+
+template <typename E, int N, typename Alloc>
+class AlignedTArray_Impl : public nsTArray_Impl<E, Alloc>
+{
+  static_assert((N & (N-1)) == 0, "N must be power of 2");
+  typedef nsTArray_Impl<E, Alloc>                    base_type;
+public:
+  typedef E                                          elem_type;
+  typedef typename base_type::size_type              size_type;
+  typedef typename base_type::index_type             index_type;
+
+  AlignedTArray_Impl() {}
+  explicit AlignedTArray_Impl(size_type capacity) : base_type(capacity+sExtra) {}
+  elem_type* Elements() { return getAligned(base_type::Elements()); }
+  const elem_type* Elements() const { return getAligned(base_type::Elements()); }
+  elem_type& operator[](index_type i) { return Elements()[i];}
+  const elem_type& operator[](index_type i) const { return Elements()[i]; }
+
+  typename Alloc::ResultType SetLength(size_type newLen) {
+    return base_type::SetLength(newLen + sExtra);
+  }
+  size_type Length() const {
+    return base_type::Length() <= sExtra ? 0 : base_type::Length() - sExtra;
+  }
+
+private:
+  AlignedTArray_Impl(const AlignedTArray_Impl& other) = delete;
+  void operator=(const AlignedTArray_Impl& other) = delete;
+
+  static const size_type sPadding = N <= MOZ_ALIGNOF(E) ? 0 : N - MOZ_ALIGNOF(E);
+  static const size_type sExtra = (sPadding + sizeof(E) - 1) / sizeof(E);
+
+  template <typename U>
+  static U* getAligned(U* p)
+  {
+    return reinterpret_cast<U*>(((uintptr_t)p + N - 1) & ~(N-1));
+  }
+};
+
+template <typename E, int N=32>
+class AlignedTArray : public AlignedTArray_Impl<E, N, nsTArrayInfallibleAllocator>
+{
+public:
+  typedef AlignedTArray_Impl<E, N, nsTArrayInfallibleAllocator> base_type;
+  typedef AlignedTArray<E, N>                                   self_type;
+  typedef typename base_type::size_type                         size_type;
+
+  AlignedTArray() {}
+  explicit AlignedTArray(size_type capacity) : base_type(capacity) {}
+private:
+  AlignedTArray(const AlignedTArray& other) = delete;
+  void operator=(const AlignedTArray& other) = delete;
+};
+
+template <typename E, int N=32>
+class AlignedFallibleTArray : public AlignedTArray_Impl<E, N, nsTArrayFallibleAllocator>
+{
+public:
+  typedef AlignedTArray_Impl<E, N, nsTArrayFallibleAllocator> base_type;
+  typedef AlignedFallibleTArray<E, N>                         self_type;
+  typedef typename base_type::size_type                       size_type;
+
+  AlignedFallibleTArray() {}
+  explicit AlignedFallibleTArray(size_type capacity) : base_type(capacity) {}
+private:
+  AlignedFallibleTArray(const AlignedFallibleTArray& other) = delete;
+  void operator=(const AlignedFallibleTArray& other) = delete;
+};
+
+#endif 
