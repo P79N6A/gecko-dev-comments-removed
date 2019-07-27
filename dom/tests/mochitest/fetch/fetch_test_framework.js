d0d14ef3bdb51a32a9b828720ba1fbbe99204565
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:18:12 +0000

    Bug 1164304 - Run all fetch tests in the service worker context as well; r=nsm
    This brings this mini test suite on par with the one that we
    have been using for DOM Cache.
    
    UltraBlame original commit: 02c55409bb067ac74854b9cb34d8bee16bf3d82f

--- dom/tests/mochitest/fetch/fetch_test_framework.js
+++ dom/tests/mochitest/fetch/fetch_test_framework.js
@@ -1,12 +1,25 @@
 function testScript(script) {
+  function setupPrefs() {
+    return new Promise(function(resolve, reject) {
+      SpecialPowers.pushPrefEnv({
+        "set": [["dom.serviceWorkers.enabled", true],
+                ["dom.serviceWorkers.testing.enabled", true],
+                ["dom.serviceWorkers.exemptFromPerDomainMax", true]]
+      }, resolve);
+    });
+  }
+
   function workerTest() {
     return new Promise(function(resolve, reject) {
       var worker = new Worker("worker_wrapper.js");
       worker.onmessage = function(event) {
+        if (event.data.context != "Worker") {
+          return;
+        }
         if (event.data.type == 'finish') {
           resolve();
         } else if (event.data.type == 'status') {
-          ok(event.data.status, "Worker fetch test: " + event.data.msg);
+          ok(event.data.status, event.data.context + ": " + event.data.msg);
         }
       }
       worker.onerror = function(event) {
@@ -14,6 +27,48 @@ function testScript(script) {
       };
 
       worker.postMessage({ "script": script });
+    });
+  }
+
+  function serviceWorkerTest() {
+    var isB2G = !navigator.userAgent.includes("Android") &&
+                /Mobile|Tablet/.test(navigator.userAgent);
+    if (isB2G) {
+      
+      dump("Skipping running the test in SW until bug 1137683 gets fixed.\n");
+      return Promise.resolve();
+    }
+    return new Promise(function(resolve, reject) {
+      function setupSW(registration) {
+        var worker = registration.waiting ||
+                     registration.active;
+
+        window.addEventListener("message",function onMessage(event) {
+          if (event.data.context != "ServiceWorker") {
+            return;
+          }
+          if (event.data.type == 'finish') {
+            window.removeEventListener("message", onMessage);
+            registration.unregister()
+              .then(resolve)
+              .catch(reject);
+          } else if (event.data.type == 'status') {
+            ok(event.data.status, event.data.context + ": " + event.data.msg);
+          }
+        }, false);
+
+        worker.onerror = reject;
+
+        var iframe = document.createElement("iframe");
+        iframe.src = "message_receiver.html";
+        iframe.onload = function() {
+          worker.postMessage({ script: script });
+        };
+        document.body.appendChild(iframe);
+      }
+
+      navigator.serviceWorker.ready.then(setupSW);
+      navigator.serviceWorker.register("worker_wrapper.js", {scope: "."});
     });
   }
 
@@ -32,9 +87,15 @@ function testScript(script) {
   
   
   
-  windowTest()
+  setupPrefs()
+    .then(function() {
+      return windowTest();
+    })
     .then(function() {
       return workerTest();
+    })
+    .then(function() {
+      return serviceWorkerTest();
     })
     .catch(function(e) {
       ok(false, "Some test failed in " + script);
