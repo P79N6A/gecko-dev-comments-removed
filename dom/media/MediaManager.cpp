f9c34fe0adb43d78e1e7313f21cba41b4dae9ecc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:59:33 +0000

    Bug 1122032 Part 3 - Automatically allow screensharing for the Loop in-desktop pages. r=jesup
    UltraBlame original commit: 09c2a262ed60f40ffb073a5e6fda885c3e0669dc

--- dom/media/MediaManager.cpp
+++ dom/media/MediaManager.cpp
@@ -1683,6 +1683,18 @@ MediaManager::GetUserMedia(
 
   nsIURI* docURI = aWindow->GetDocumentURI();
 
+  bool isLoop = false;
+  nsCOMPtr<nsIURI> loopURI;
+  nsresult rv = NS_NewURI(getter_AddRefs(loopURI), "about:loopconversation");
+  NS_ENSURE_SUCCESS(rv, rv);
+  rv = docURI->EqualsExceptRef(loopURI, &isLoop);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  if (isLoop) {
+    privileged = true;
+  }
+
+
   if (c.mVideo.IsMediaTrackConstraints()) {
     auto& tc = c.mVideo.GetAsMediaTrackConstraints();
     MediaSourceEnum src = StringToEnum(dom::MediaSourceEnumValues::strings,
@@ -1725,6 +1737,17 @@ MediaManager::GetUserMedia(
     default:
       return task->Denied(NS_LITERAL_STRING("NotFoundError"));
     }
+
+    
+    
+    
+    
+    if (isLoop &&
+        (src == dom::MediaSourceEnum::Window ||
+         src == dom::MediaSourceEnum::Application ||
+         src == dom::MediaSourceEnum::Screen)) {
+       privileged = false;
+    }
   }
 
 #ifdef MOZ_B2G_CAMERA
@@ -1732,17 +1755,6 @@ MediaManager::GetUserMedia(
     mCameraManager = nsDOMCameraManager::CreateInstance(aWindow);
   }
 #endif
-
-  bool isLoop = false;
-  nsCOMPtr<nsIURI> loopURI;
-  nsresult rv = NS_NewURI(getter_AddRefs(loopURI), "about:loopconversation");
-  NS_ENSURE_SUCCESS(rv, rv);
-  rv = docURI->EqualsExceptRef(loopURI, &isLoop);
-  NS_ENSURE_SUCCESS(rv, rv);
-
-  if (isLoop) {
-    privileged = true;
-  }
 
   
   if (privileged ||
