cc050ff1e48fe73013a8bebb84256a4488434e56
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:06:15 +0000

    Bug 1088488 - Copy freebl's SHA256 code into plugin container, stripping out non-256 SHA code. r=hsivonen
    UltraBlame original commit: abf353e4a1a7513c7c8a3557491e1118c9cb6c9e

---
+++ ipc/app/sha256.h
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+#ifndef _SHA256_H_
+#define _SHA256_H_
+
+#define SHA256_LENGTH 32 
+
+#include "prtypes.h"	
+#include "prlong.h"
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
+struct SHA256Context {
+  union {
+    PRUint32 w[64];	    
+    PRUint8  b[256];
+  } u;
+  PRUint32 h[8];		
+  PRUint32 sizeHi,sizeLo;	
+};
+
+typedef struct SHA256Context SHA256Context;
+
+extern void
+SHA256_Begin(SHA256Context *ctx);
+
+extern void 
+SHA256_Update(SHA256Context *ctx, const unsigned char *input,
+		    unsigned int inputLen);
+
+extern void
+SHA256_End(SHA256Context *ctx, unsigned char *digest,
+           unsigned int *digestLen, unsigned int maxDigestLen);
+
+#ifdef __cplusplus 
+} 
+#endif
+
+#endif
