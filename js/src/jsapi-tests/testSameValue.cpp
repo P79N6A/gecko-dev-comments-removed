c4d24768b5b9c5b088bb7e3cf04240d4a1024c40
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:09:21 +0000

    Bug 1132045 - Add handles to various equality operations. r=jandem
    UltraBlame original commit: e00ed1b014d0047b2a3ac15fa563b8f3019f2b16

--- js/src/jsapi-tests/testSameValue.cpp
+++ js/src/jsapi-tests/testSameValue.cpp
@@ -17,8 +17,8 @@ BEGIN_TEST(testSameValue)
 
 
 
-    jsval v1 = DOUBLE_TO_JSVAL(0.0);
-    jsval v2 = DOUBLE_TO_JSVAL(-0.0);
+    JS::RootedValue v1(cx, JS::DoubleValue(0.0));
+    JS::RootedValue v2(cx, JS::DoubleValue(-0.0));
     bool same;
     CHECK(JS_SameValue(cx, v1, v2, &same));
     CHECK(!same);
