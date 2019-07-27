a8261476c28129cdc8ed981b7f18988b92854be0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:23:49 +0000

    Bug 1135042: Optimize SIMD.storeX/storeXY/storeXYZ in Ion; r=bhackett
    UltraBlame original commit: 44aa05cc400f82eabbb9a9e3880dda178eca5633

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -757,8 +757,10 @@ class MacroAssembler : public MacroAssemblerSpecific
     template<typename S, typename T>
     void atomicBinopToTypedIntArray(AtomicOp op, Scalar::Type arrayType, const S &value, const T &mem);
 
-    void storeToTypedFloatArray(Scalar::Type arrayType, FloatRegister value, const BaseIndex &dest);
-    void storeToTypedFloatArray(Scalar::Type arrayType, FloatRegister value, const Address &dest);
+    void storeToTypedFloatArray(Scalar::Type arrayType, FloatRegister value, const BaseIndex &dest,
+                                unsigned numElems = 0);
+    void storeToTypedFloatArray(Scalar::Type arrayType, FloatRegister value, const Address &dest,
+                                unsigned numElems = 0);
 
     
     template <typename T>
