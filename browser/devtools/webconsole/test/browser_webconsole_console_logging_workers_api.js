db226f3539333f5354886cbe08bc019584f99b4a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:00:37 +0000

    Bug 1125205 - Display console API messages from Shared or Service Workers to the webconsole, r=past
    UltraBlame original commit: a12f0f05779c7d020bc130b844129be72a53110c

---
+++ browser/devtools/webconsole/test/browser_webconsole_console_logging_workers_api.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+"use strict";
+
+const TEST_URI = "http://example.com/browser/browser/devtools/webconsole/test/test-console-workers.html";
+
+function pushPrefEnv()
+{
+  let deferred = promise.defer();
+  let options = {'set': [["dom.workers.sharedWorkers.enabled", true]]};
+  SpecialPowers.pushPrefEnv(options, deferred.resolve);
+  return deferred.promise;
+}
+
+let test = asyncTest(function*() {
+  yield pushPrefEnv();
+  yield loadTab(TEST_URI);
+
+  let hud = yield openConsole();
+  let outputNode = hud.outputNode;
+
+  yield waitForMessages({
+    webconsole: hud,
+    messages: [{
+      text: "foo-bar-shared-worker"
+    }],
+  });
+
+  hud.setFilterState('sharedworkers', false);
+
+  is(outputNode.querySelectorAll(".filtered-by-type").length, 1,
+     "1 message hidden for sharedworkers (logging turned off)")
+
+  hud.setFilterState('sharedworkers', true);
+
+  is(outputNode.querySelectorAll(".filtered-by-type").length, 0,
+     "1 message shown for sharedworkers (logging turned off)")
+});
