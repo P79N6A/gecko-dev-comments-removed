48e613988c359fa428e0e86cafa5d68aa43bf479
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:16:49 +0000

    Bug 1177013 - Use CancelCurrentTransaction to avoid crashes (r=dvander)
    UltraBlame original commit: f7de893911bc2eea6e779a72bcb88c0aee10e63d

--- xpcom/threads/nsThread.cpp
+++ xpcom/threads/nsThread.cpp
@@ -738,8 +738,9 @@ nsThread::ProcessNextEvent(bool aMayWait, bool* aResult)
 
 #if !defined(MOZILLA_XPCOMRT_API)
   
-  MOZ_RELEASE_ASSERT(mIsMainThread != MAIN_THREAD ||
-                     !ipc::ParentProcessIsBlocked());
+  if (mIsMainThread == MAIN_THREAD) {
+    ipc::CancelCPOWs();
+  }
 #endif 
 
   if (NS_WARN_IF(PR_GetCurrentThread() != mThread)) {
