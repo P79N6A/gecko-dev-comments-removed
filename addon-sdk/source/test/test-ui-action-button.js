e43be26ca68cd2b1d60fc9a1fa60ddec3309df7f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:48:56 +0000

    Backed out changeset 31a25f47264b (bug 1067942) for Linux mochitest-other crashes. CLOSED TREE
    UltraBlame original commit: 7719fd510b39c0461e2b20dba6b7c0cbae5fd737

--- addon-sdk/source/test/test-ui-action-button.js
+++ addon-sdk/source/test/test-ui-action-button.js
@@ -253,7 +253,7 @@ exports['test button global state updated'] = function(assert) {
   
 
   assert.throws(() => button.id = 'another-id',
-    /^setting getter-only property/,
+    /^setting a property that has only a getter/,
     'id cannot be set at runtime');
 
   assert.equal(button.id, 'my-button-4',
