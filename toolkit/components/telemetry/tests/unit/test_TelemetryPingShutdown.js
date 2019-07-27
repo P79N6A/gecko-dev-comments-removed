5faa9e88e3ac11208495578b7586817d658c0b0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:37:06 +0000

    Bug 1139460 - Part 5 - Fix race leading to TelemetryPing shutdown timeout by aborting all pending ping requests on shutdown. r=vladan,f=yoric
    UltraBlame original commit: f9f66f6aaa8679739bfc70fbd4a3087cc88277cb

---
+++ toolkit/components/telemetry/tests/unit/test_TelemetryPingShutdown.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+"use strict";
+
+const { utils: Cu, interfaces: Ci, classes: Cc } = Components;
+
+Cu.import("resource://gre/modules/Services.jsm", this);
+Cu.import("resource://gre/modules/TelemetryPing.jsm", this);
+Cu.import("resource://gre/modules/Timer.jsm", this);
+Cu.import("resource://gre/modules/XPCOMUtils.jsm", this);
+Cu.import("resource://gre/modules/AsyncShutdown.jsm", this);
+Cu.import("resource://testing-common/httpd.js", this);
+
+const PREF_BRANCH = "toolkit.telemetry.";
+const PREF_ENABLED = PREF_BRANCH + "enabled";
+const PREF_FHR_UPLOAD_ENABLED = "datareporting.healthreport.uploadEnabled";
+
+function contentHandler(metadata, response)
+{
+  dump("contentHandler called for path: " + metadata._path + "\n");
+  
+  
+  response.processAsync();
+  response.setHeader("Content-Type", "text/plain");
+}
+
+function run_test() {
+  
+  do_get_profile();
+  loadAddonManager("xpcshell@tests.mozilla.org", "XPCShell", "1", "1.9.2");
+
+  Services.prefs.setBoolPref(PREF_ENABLED, true);
+  Services.prefs.setBoolPref(PREF_FHR_UPLOAD_ENABLED, true);
+
+  
+  
+  if (HAS_DATAREPORTINGSERVICE) {
+    let drs = Cc["@mozilla.org/datareporting/service;1"]
+                .getService(Ci.nsISupports)
+                .wrappedJSObject;
+    drs.observe(null, "app-startup", null);
+    drs.observe(null, "profile-after-change", null);
+  }
+
+  run_next_test();
+}
+
+add_task(function* test_sendTimeout() {
+  const TIMEOUT = 100;
+  
+  
+  Services.prefs.setBoolPref("toolkit.asyncshutdown.testing", true);
+  Services.prefs.setIntPref("toolkit.asyncshutdown.crash_timeout", TIMEOUT);
+
+  let httpServer = new HttpServer();
+  httpServer.registerPrefixHandler("/", contentHandler);
+  httpServer.start(-1);
+
+  yield TelemetryPing.setup();
+  TelemetryPing.setServer("http://localhost:" + httpServer.identity.primaryPort);
+  TelemetryPing.send("test-ping-type", {});
+
+  
+  AsyncShutdown.profileBeforeChange._trigger();
+  AsyncShutdown.sendTelemetry._trigger();
+
+  
+  Assert.ok(true, "Didn't time out on shutdown.");
+});
