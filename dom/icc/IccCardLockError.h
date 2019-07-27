54c059396107d22a439371a05b027b95b3fa23ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:02:21 +0000

    Bug 1081810 - Part 1: Implement IccCardLockError in C++. r=smaug
    UltraBlame original commit: 547119c5331081590958889b49c666bcccf466be

---
+++ dom/icc/IccCardLockError.h
@@ -0,0 +1,54 @@
+
+
+
+
+#ifndef mozilla_dom_IccCardLockError_h
+#define mozilla_dom_IccCardLockError_h
+
+#include "mozilla/dom/DOMError.h"
+
+namespace mozilla {
+namespace dom {
+
+class IccCardLockError MOZ_FINAL : public DOMError
+{
+public:
+  NS_DECL_ISUPPORTS_INHERITED
+
+  IccCardLockError(nsPIDOMWindow* aWindow, const nsAString& aName,
+                   const nsAString& aLockType, int16_t aRetryCount);
+
+  static already_AddRefed<IccCardLockError>
+  Constructor(const GlobalObject& aGlobal, const nsAString& aLockType,
+              const nsAString& aName, int16_t aRetryCount,
+              ErrorResult& aRv);
+
+  virtual JSObject*
+  WrapObject(JSContext* aCx) MOZ_OVERRIDE;
+
+  
+
+  void
+  GetLockType(nsString& aLockType) const
+  {
+    aLockType = mLockType;
+  }
+
+  int16_t
+  RetryCount() const
+  {
+    return mRetryCount;
+  }
+
+private:
+  ~IccCardLockError() {}
+
+private:
+  nsString mLockType;
+  int16_t mRetryCount;
+};
+
+} 
+} 
+
+#endif 
