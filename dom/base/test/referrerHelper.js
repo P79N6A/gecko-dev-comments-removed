f238a81c6fd57030f91b02ddf2a6d33cb3754fe6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:29:57 +0000

    Bug 1161221 - Split http referrer tests and enable them on all platforms except https on b2g. r=sstamm
    UltraBlame original commit: 27ebc65071e5c42dd1b5184678540ee13bdfb21b

--- dom/base/test/referrerHelper.js
+++ dom/base/test/referrerHelper.js
@@ -20,7 +20,6 @@ window.addEventListener("message", function(event) {
 
 
 
-
 function doXHR(url, onSuccess, onFail) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
@@ -51,7 +50,7 @@ function resetCounter() {
 
 
 
-function checkResults(testname, expected) {
+function checkIndividualResults(testname, expected) {
   doXHR('/tests/dom/base/test/bug704320_counter.sjs?results',
         function(xhr) {
           var results = JSON.parse(xhr.responseText);
@@ -74,3 +73,163 @@ function checkResults(testname, expected) {
           SimpleTest.finish();
         });
 }
+
+
+
+
+function checkExpectedGlobalResults() {
+  var url = 'bug704320.sjs?action=get-test-results';
+  doXHR(url,
+	function(xhr) {
+	      var response = JSON.parse(xhr.response);
+
+	      for (type in response) {
+		for (scheme in response[type]) {
+		  for (policy in response[type][scheme]) {
+		    var expectedResult = EXPECTED_RESULTS[type] === undefined ?
+		      	EXPECTED_RESULTS['default'][scheme][policy] :
+		      	EXPECTED_RESULTS[type][scheme][policy];
+		    is(response[type][scheme][policy], expectedResult, type + ' ' + scheme + ' ' + policy);
+		  }
+		}
+	      }
+		advance();
+	},
+	function(xhr) {
+          	ok(false, "Can't get results from the counter server.");
+		SimpleTest.finish();
+	});
+}
+
+
+var EXPECTED_RESULTS = {
+  
+  
+  
+  
+  'link-ping': {
+    
+    'http-to-http': {
+      'no-referrer': '',
+      'unsafe-url': '',
+      'origin': '',
+      'origin-when-cross-origin': '',
+      'no-referrer-when-downgrade': ''
+    },
+    'http-to-https': {
+      'no-referrer': '',
+      'unsafe-url': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=http&scheme-to=https&policy=unsafe-url',
+      'origin': 'http://example.com',
+      'origin-when-cross-origin': 'http://example.com',
+      'no-referrer-when-downgrade': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=http&scheme-to=https&policy=no-referrer-when-downgrade'
+    },
+    
+    'https-to-http': {
+      'no-referrer': '',
+      'unsafe-url': '',
+      'origin': '',
+      'origin-when-cross-origin': '',
+      'no-referrer-when-downgrade': ''
+    },
+    
+    'https-to-https': {
+      'no-referrer': '',
+      'unsafe-url': '',
+      'origin': '',
+      'origin-when-cross-origin': '',
+      'no-referrer-when-downgrade': ''
+    }
+  },
+  
+  'form': {
+    'http-to-http': {
+      'no-referrer': '',
+      'unsafe-url': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=http&policy=unsafe-url&type=form',
+      'origin': 'http://example.com',
+      'origin-when-cross-origin': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=http&policy=origin-when-cross-origin&type=form',
+      'no-referrer-when-downgrade': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=http&policy=no-referrer-when-downgrade&type=form'
+    },
+    'http-to-https': {
+      'no-referrer': '',
+      'unsafe-url': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=https&policy=unsafe-url&type=form',
+      'origin': 'http://example.com',
+      'origin-when-cross-origin': 'http://example.com',
+      'no-referrer-when-downgrade': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=https&policy=no-referrer-when-downgrade&type=form'
+    },
+    'https-to-http': {
+      'no-referrer': '',
+      'unsafe-url': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=https&scheme-to=http&policy=unsafe-url&type=form',
+      'origin': 'https://example.com',
+      'origin-when-cross-origin': 'https://example.com',
+      'no-referrer-when-downgrade': ''
+    },
+    'https-to-https': {
+      'no-referrer': '',
+      'unsafe-url': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=https&scheme-to=https&policy=unsafe-url&type=form',
+      'origin': 'https://example.com',
+     'origin-when-cross-origin': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=https&scheme-to=https&policy=origin-when-cross-origin&type=form',
+      'no-referrer-when-downgrade': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=https&scheme-to=https&policy=no-referrer-when-downgrade&type=form'
+    }
+  },
+  
+  'window.location': {
+    'http-to-http': {
+      'no-referrer': '',
+      'unsafe-url': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=http&policy=unsafe-url&type=window.location',
+     'origin': 'http://example.com',
+      'origin-when-cross-origin': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=http&policy=origin-when-cross-origin&type=window.location',
+      'no-referrer-when-downgrade': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=http&policy=no-referrer-when-downgrade&type=window.location'
+    },
+    'http-to-https': {
+      'no-referrer': '',
+      'unsafe-url': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=https&policy=unsafe-url&type=window.location',
+      'origin': 'http://example.com',
+      'origin-when-cross-origin': 'http://example.com',
+      'no-referrer-when-downgrade': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=http&scheme-to=https&policy=no-referrer-when-downgrade&type=window.location'
+    },
+    'https-to-http': {
+      'no-referrer': '',
+      'unsafe-url': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=https&scheme-to=http&policy=unsafe-url&type=window.location',
+      'origin': 'https://example.com',
+      'origin-when-cross-origin': 'https://example.com',
+      'no-referrer-when-downgrade': ''
+    },
+    'https-to-https': {
+      'no-referrer': '',
+      'unsafe-url': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=https&scheme-to=https&policy=unsafe-url&type=window.location',
+      'origin': 'https://example.com',
+      'origin-when-cross-origin': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=https&scheme-to=https&policy=origin-when-cross-origin&type=window.location',
+      'no-referrer-when-downgrade': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-2nd-level-iframe&scheme-from=https&scheme-to=https&policy=no-referrer-when-downgrade&type=window.location'
+    }
+  },
+  'default': {
+    'http-to-http': {
+      'no-referrer': '',
+      'unsafe-url': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=http&scheme-to=http&policy=unsafe-url',
+      'origin': 'http://example.com',
+      'origin-when-cross-origin': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=http&scheme-to=http&policy=origin-when-cross-origin',
+      'no-referrer-when-downgrade': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=http&scheme-to=http&policy=no-referrer-when-downgrade'
+    },
+    'http-to-https': {
+      'no-referrer': '',
+      'unsafe-url': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=http&scheme-to=https&policy=unsafe-url',
+      'origin': 'http://example.com',
+      'origin-when-cross-origin': 'http://example.com',
+      'no-referrer-when-downgrade': 'http://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=http&scheme-to=https&policy=no-referrer-when-downgrade'
+    },
+    'https-to-http': {
+      'no-referrer': '',
+      'unsafe-url': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=https&scheme-to=http&policy=unsafe-url',
+      'origin': 'https://example.com',
+      'origin-when-cross-origin': 'https://example.com',
+      'no-referrer-when-downgrade': ''
+    },
+    'https-to-https': {
+      'no-referrer': '',
+      'unsafe-url': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=https&scheme-to=https&policy=unsafe-url',
+      'origin': 'https://example.com',
+      'origin-when-cross-origin': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=https&scheme-to=https&policy=origin-when-cross-origin',
+      'no-referrer-when-downgrade': 'https://example.com/tests/dom/base/test/bug704320.sjs?action=create-1st-level-iframe&scheme-from=https&scheme-to=https&policy=no-referrer-when-downgrade'
+    }
+  }
+};
