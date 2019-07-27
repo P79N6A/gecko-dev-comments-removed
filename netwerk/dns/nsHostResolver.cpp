9599743d86bec897685716e289bd15d938b906ad
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:40:22 +0000

    Bug 939318 - Detect network interface changes on windows. r=mcmanus
    Now supports IPv6 as well if a new enough windows version is used.
    Which notification function to use is detect at run-time.
    
    Now sends CHANGED event if the online interface(s) are different in any
    way since it was previously checked and considered UP. CHANGED is sent
    before UP in case both are detected. It does not send any CHANGED events
    during the first 2 seconds after startup.
    
    nIOService: split up the network event receiver function from the
    network status init function and have the event receiver act on the
    incoming event.
    
    DNSservice: acts on network changes (flushes the host cache and restarts
    ongoing name resolves)
    
    HttpHandler: acts on network changes
    
    UltraBlame original commit: 89d06d103c107dfa032af6cab9a7bf29a2b60907

--- netwerk/dns/nsHostResolver.cpp
+++ netwerk/dns/nsHostResolver.cpp
@@ -470,6 +470,21 @@ nsHostRecord::GetPriority(uint16_t aFlags)
 
 
 
+bool
+nsHostRecord::RemoveOrRefresh()
+{
+  
+  
+  
+    if (resolving && !onQueue) {
+        mResolveAgain = true;
+        return false;
+    }
+    return true; 
+}
+
+
+
 struct nsHostDBEnt : PLDHashEntryHdr
 {
     nsHostRecord *rec;
@@ -579,6 +594,21 @@ HostDB_RemoveEntry(PLDHashTable *table,
                    void *arg)
 {
     return PL_DHASH_REMOVE;
+}
+
+static PLDHashOperator
+HostDB_PruneEntry(PLDHashTable *table,
+                  PLDHashEntryHdr *hdr,
+                  uint32_t number,
+                  void *arg)
+{
+    nsHostDBEnt* ent = static_cast<nsHostDBEnt *>(hdr);
+
+    
+    if (ent->rec->RemoveOrRefresh()) {
+        return PL_DHASH_REMOVE;
+    }
+    return PL_DHASH_NEXT;
 }
 
 
@@ -746,6 +776,41 @@ nsHostResolver::ClearPendingQueue(PRCList *aPendingQ)
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+void
+nsHostResolver::FlushCache()
+{
+    PRCList evictionQ;
+    PR_INIT_CLIST(&evictionQ);
+
+    {
+        MutexAutoLock lock(mLock);
+        MoveCList(mEvictionQ, evictionQ);
+        mEvictionQSize = 0;
+
+        
+        PL_DHashTableEnumerate(&mDB, HostDB_PruneEntry, nullptr);
+    }
+
+    if (!PR_CLIST_IS_EMPTY(&evictionQ)) {
+        PRCList *node = evictionQ.next;
+        while (node != &evictionQ) {
+            nsHostRecord *rec = static_cast<nsHostRecord *>(node);
+            node = node->next;
+            NS_RELEASE(rec);
+        }
+    }
+}
+
 void
 nsHostResolver::Shutdown()
 {
@@ -772,7 +837,7 @@ nsHostResolver::Shutdown()
 
     {
         MutexAutoLock lock(mLock);
-        
+
         mShutdown = true;
 
         MoveCList(mHighQ, pendingQHigh);
@@ -781,14 +846,14 @@ nsHostResolver::Shutdown()
         MoveCList(mEvictionQ, evictionQ);
         mEvictionQSize = 0;
         mPendingCount = 0;
-        
+
         if (mNumIdleThreads)
             mIdleThreadCV.NotifyAll();
-        
+
         
         PL_DHashTableEnumerate(&mDB, HostDB_RemoveEntry, nullptr);
     }
-    
+
     ClearPendingQueue(&pendingQHigh);
     ClearPendingQueue(&pendingQMed);
     ClearPendingQueue(&pendingQLow);
@@ -1331,7 +1396,12 @@ nsHostResolver::PrepareRecordExpiration(nsHostRecord* rec) const
          rec->host, lifetime, grace, sDnsVariant));
 }
 
-void
+
+
+
+
+
+nsHostResolver::LookupStatus
 nsHostResolver::OnLookupComplete(nsHostRecord* rec, nsresult status, AddrInfo* result)
 {
     
@@ -1340,6 +1410,11 @@ nsHostResolver::OnLookupComplete(nsHostRecord* rec, nsresult status, AddrInfo* r
     PR_INIT_CLIST(&cbs);
     {
         MutexAutoLock lock(mLock);
+
+        if (rec->mResolveAgain && (status != NS_ERROR_ABORT)) {
+            rec->mResolveAgain = false;
+            return LOOKUP_RESOLVEAGAIN;
+        }
 
         
         MoveCList(rec->callbacks, cbs);
@@ -1419,6 +1494,8 @@ nsHostResolver::OnLookupComplete(nsHostRecord* rec, nsresult status, AddrInfo* r
 #endif
 
     NS_RELEASE(rec);
+
+    return LOOKUP_OK;
 }
 
 void
@@ -1502,9 +1579,10 @@ nsHostResolver::ThreadFunc(void *arg)
     nsResState rs;
 #endif
     nsHostResolver *resolver = (nsHostResolver *)arg;
-    nsHostRecord *rec;
+    nsHostRecord *rec  = nullptr;
     AddrInfo *ai = nullptr;
-    while (resolver->GetHostToLookup(&rec)) {
+
+    while (rec || resolver->GetHostToLookup(&rec)) {
         LOG(("DNS lookup thread - Calling getaddrinfo for host [%s].\n",
              rec->host));
 
@@ -1548,7 +1626,13 @@ nsHostResolver::ThreadFunc(void *arg)
         
         LOG(("DNS lookup thread - lookup completed for host [%s]: %s.\n",
              rec->host, ai ? "success" : "failure: unknown host"));
-        resolver->OnLookupComplete(rec, status, ai);
+        if (LOOKUP_RESOLVEAGAIN == resolver->OnLookupComplete(rec, status, ai)) {
+            
+            LOG(("DNS lookup thread - Re-resolving host [%s].\n",
+                 rec->host));
+        } else {
+            rec = nullptr;
+        }
     }
     NS_RELEASE(resolver);
     LOG(("DNS lookup thread - queue empty, thread finished.\n"));
