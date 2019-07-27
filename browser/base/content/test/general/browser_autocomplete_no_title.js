5dfe4af4659df88db9f350df3f63445fe3109223
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:12:07 +0000

    Bug 1060642 - Improve display of entries without a title in the Awesomebar. r=mak
    UltraBlame original commit: 45e442b7d671fe11322dd2acd4fb10da2eacfece

---
+++ browser/base/content/test/general/browser_autocomplete_no_title.js
@@ -0,0 +1,27 @@
+
+
+
+function* check_title(inputText, expectedTitle) {
+  gURLBar.focus();
+  gURLBar.value = inputText.slice(0, -1);
+  EventUtils.synthesizeKey(inputText.slice(-1) , {});
+  yield promiseSearchComplete();
+
+  ok(gURLBar.popup.richlistbox.children.length > 1, "Should get at least 2 results");
+  let result = gURLBar.popup.richlistbox.children[1];
+  is(result._title.textContent, expectedTitle, "Result title should be as expected");
+}
+
+add_task(function*() {
+  
+  if (!Services.prefs.getBoolPref("browser.urlbar.unifiedcomplete"))
+    return;
+
+  let uri = NetUtil.newURI("http://bug1060642.example.com/beards/are/pretty/great");
+  yield PlacesTestUtils.addVisits([{uri: uri, title: ""}]);
+
+  yield check_title("bug1060642", "bug1060642.example.com");
+
+  gURLBar.popup.hidePopup();
+  yield promisePopupHidden(gURLBar.popup);
+});
