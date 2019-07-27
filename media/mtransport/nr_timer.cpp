df2b6c0498b7dd3151c0a528821b24c5dca631b9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:50:48 +0000

    Bug 1151080: Rewrite NR_async_timer_set(0) to use direct dispatch. r=mt
    UltraBlame original commit: 6013a3ee3fdffffca943f9d6649ec83f7b68cd58

--- media/mtransport/nr_timer.cpp
+++ media/mtransport/nr_timer.cpp
@@ -67,20 +67,15 @@ extern "C" {
 
 namespace mozilla {
 
-class nrappkitTimerCallback : public nsITimerCallback
-{
-public:
-  
-  NS_DECL_THREADSAFE_ISUPPORTS
-  NS_DECL_NSITIMERCALLBACK
-
-  nrappkitTimerCallback(NR_async_cb cb, void *cb_arg,
-                        const char *function, int line)
+class nrappkitCallback  {
+ public:
+  nrappkitCallback(NR_async_cb cb, void *cb_arg,
+                   const char *function, int line)
     : cb_(cb), cb_arg_(cb_arg), function_(function), line_(line) {
   }
+  virtual ~nrappkitCallback() {}
 
-private:
-  virtual ~nrappkitTimerCallback() {}
+  virtual void Cancel() = 0;
 
 protected:
   
@@ -90,38 +85,113 @@ protected:
   int line_;
 };
 
+class nrappkitTimerCallback : public nrappkitCallback,
+                              public nsITimerCallback {
+ public:
+  
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSITIMERCALLBACK
+
+  nrappkitTimerCallback(NR_async_cb cb, void *cb_arg,
+                        const char *function, int line,
+                        nsITimer *timer)
+      : nrappkitCallback(cb, cb_arg, function, line),
+        timer_(timer) {}
+
+  virtual void Cancel() override {
+    AddRef();  
+               
+    timer_->Cancel();
+    timer_->Release();
+    Release(); 
+  }
+
+ private:
+  nsITimer* timer_;
+  virtual ~nrappkitTimerCallback() {}
+};
+
 NS_IMPL_ISUPPORTS(nrappkitTimerCallback, nsITimerCallback)
 
 NS_IMETHODIMP nrappkitTimerCallback::Notify(nsITimer *timer) {
   r_log(LOG_GENERIC, LOG_DEBUG, "Timer callback fired (set in %s:%d)",
         function_.c_str(), line_);
-
+  MOZ_ASSERT(timer == timer_);
   cb_(0, 0, cb_arg_);
 
   
   timer->Release();
   return NS_OK;
 }
+
+class nrappkitScheduledCallback : public nrappkitCallback {
+ public:
+
+  nrappkitScheduledCallback(NR_async_cb cb, void *cb_arg,
+                            const char *function, int line)
+      : nrappkitCallback(cb, cb_arg, function, line) {}
+
+  void Run() {
+    if (cb_) {
+      cb_(0, 0, cb_arg_);
+    }
+  }
+
+  virtual void Cancel() override {
+    cb_ = nullptr;
+  }
+
+  ~nrappkitScheduledCallback() {}
+};
+
 }  
 
 
 using namespace mozilla;
 
-
-
-static void CheckSTSThread() {
+static nsCOMPtr<nsIEventTarget> GetSTSThread() {
   nsresult rv;
 
   nsCOMPtr<nsIEventTarget> sts_thread;
 
   sts_thread = do_GetService(NS_SOCKETTRANSPORTSERVICE_CONTRACTID, &rv);
-
   MOZ_ASSERT(NS_SUCCEEDED(rv));
+
+  return sts_thread;
+}
+
+
+
+static void CheckSTSThread() {
+  nsCOMPtr<nsIEventTarget> sts_thread = GetSTSThread();
+
   ASSERT_ON_THREAD(sts_thread);
 }
 
-int NR_async_timer_set(int timeout, NR_async_cb cb, void *arg, char *func,
-                       int l, void **handle) {
+static int nr_async_timer_set_zero(NR_async_cb cb, void *arg,
+                                   char *func, int l,
+                                   nrappkitCallback **handle) {
+  nrappkitScheduledCallback* callback(new nrappkitScheduledCallback(
+      cb, arg, func, l));
+
+  nsresult rv = GetSTSThread()->Dispatch(WrapRunnable(
+      nsAutoPtr<nrappkitScheduledCallback>(callback),
+      &nrappkitScheduledCallback::Run),
+                        NS_DISPATCH_NORMAL);
+  if (NS_FAILED(rv))
+    return R_FAILED;
+
+  *handle = callback;
+
+  
+  
+  
+  return 0;
+}
+
+static int nr_async_timer_set_nonzero(int timeout, NR_async_cb cb, void *arg,
+                                      char *func, int l,
+                                      nrappkitCallback **handle) {
   nsresult rv;
   CheckSTSThread();
 
@@ -130,8 +200,9 @@ int NR_async_timer_set(int timeout, NR_async_cb cb, void *arg, char *func,
     return(R_FAILED);
   }
 
-  rv = timer->InitWithCallback(new nrappkitTimerCallback(cb, arg, func, l),
-                               timeout, nsITimer::TYPE_ONE_SHOT);
+  nrappkitTimerCallback* callback =
+      new nrappkitTimerCallback(cb, arg, func, l, timer);
+  rv = timer->InitWithCallback(callback, timeout, nsITimer::TYPE_ONE_SHOT);
   if (NS_FAILED(rv)) {
     return R_FAILED;
   }
@@ -139,11 +210,29 @@ int NR_async_timer_set(int timeout, NR_async_cb cb, void *arg, char *func,
   
   timer->AddRef();
 
+  *handle = callback;
+
+  return 0;
+}
+
+int NR_async_timer_set(int timeout, NR_async_cb cb, void *arg,
+                       char *func, int l, void **handle) {
+  CheckSTSThread();
+
+  nrappkitCallback *callback;
+  int r;
+
+  if (!timeout) {
+    r = nr_async_timer_set_zero(cb, arg, func, l, &callback);
+  } else {
+    r = nr_async_timer_set_nonzero(timeout, cb, arg, func, l, &callback);
+  }
+
+  if (r)
+    return r;
+
   if (handle)
-    *handle = timer.get();
-  
-  
-  
+    *handle = callback;
 
   return 0;
 }
@@ -163,11 +252,8 @@ int NR_async_timer_cancel(void *handle) {
 
   CheckSTSThread();
 
-  nsITimer *timer = static_cast<nsITimer *>(handle);
-
-  timer->Cancel();
-  
-  timer->Release();
+  nrappkitCallback* callback = static_cast<nrappkitCallback *>(handle);
+  callback->Cancel();
 
   return 0;
 }
