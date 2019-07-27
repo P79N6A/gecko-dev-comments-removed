68f77d7a386cfd2f1329f7df0863ba8eaffb4438
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:36:32 +0000

    Bug 1069795 - Use promise and pushEnv instead of setEnv. r=jwwang
    UltraBlame original commit: 7fef64119cf40061d8cbfcfb0ed3ec458ff53c27

--- content/media/test/can_play_type_ogg.js
+++ content/media/test/can_play_type_ogg.js
@@ -1,38 +1,78 @@
-function check_ogg(v, enabled) {
+
+function check_ogg(v, enabled, finish) {
   function check(type, expected) {
     is(v.canPlayType(type), enabled ? expected : "", type);
   }
 
-  
-  check("video/ogg", "maybe");
-  check("audio/ogg", "maybe");
-  check("application/ogg", "maybe");
+  function basic_test() {
+    return new Promise(function(resolve, reject) {
+      
+      check("video/ogg", "maybe");
+      check("audio/ogg", "maybe");
+      check("application/ogg", "maybe");
+
+      
+      check("audio/ogg; codecs=vorbis", "probably");
+      check("video/ogg; codecs=vorbis", "probably");
+      check("video/ogg; codecs=vorbis,theora", "probably");
+      check("video/ogg; codecs=\"vorbis, theora\"", "probably");
+      check("video/ogg; codecs=theora", "probably");
+
+      resolve();
+    });
+  }
 
   
-  check("audio/ogg; codecs=vorbis", "probably");
-  check("video/ogg; codecs=vorbis", "probably");
-  check("video/ogg; codecs=vorbis,theora", "probably");
-  check("video/ogg; codecs=\"vorbis, theora\"", "probably");
-  check("video/ogg; codecs=theora", "probably");
+  function verify_opus_support() {
+    return new Promise(function(resolve, reject) {
+      var OpusEnabled = undefined;
+      try {
+        OpusEnabled = SpecialPowers.getBoolPref("media.opus.enabled");
+      } catch (ex) {
+        
+        console.log("media.opus.enabled pref not found; skipping Opus validation");
+      }
+      if (OpusEnabled != undefined) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  }
 
-  
-  var OpusEnabled = undefined;
-  try {
-    OpusEnabled = SpecialPowers.getBoolPref("media.opus.enabled");
-  } catch (ex) {
+  function opus_enable() {
+    return new Promise(function(resolve, reject) {
+      SpecialPowers.pushPrefEnv({"set": [['media.opus.enabled', true]]},
+                                function() {
+                                  check("audio/ogg; codecs=opus", "probably");
+                                  resolve();
+                                });
+    });
+  }
+
+  function opus_disable() {
+    return new Promise(function(resolve, reject) {
+      SpecialPowers.pushPrefEnv({"set": [['media.opus.enabled', false]]},
+                                function() {
+                                  check("audio/ogg; codecs=opus", "");
+                                  resolve();
+                                });
+    });
+  }
+
+  function unspported_ogg() {
     
-    console.log("media.opus.enabled pref not found; skipping Opus validation");
-  }
-  if (OpusEnabled !== undefined) {
-    SpecialPowers.setBoolPref("media.opus.enabled", true);
-    check("audio/ogg; codecs=opus", "probably");
-    SpecialPowers.setBoolPref("media.opus.enabled", false);
-    check("audio/ogg; codecs=opus", "");
-    SpecialPowers.setBoolPref("media.opus.enabled", OpusEnabled);
+    check("video/ogg; codecs=xyz", "");
+    check("video/ogg; codecs=xyz,vorbis", "");
+    check("video/ogg; codecs=vorbis,xyz", "");
+
+    finish.call();
   }
 
-  
-  check("video/ogg; codecs=xyz", "");
-  check("video/ogg; codecs=xyz,vorbis", "");
-  check("video/ogg; codecs=vorbis,xyz", "");
+  basic_test()
+  .then(verify_opus_support)
+  .then(opus_enable)
+  .then(opus_disable)
+  .then(unspported_ogg, unspported_ogg);
+
 }
