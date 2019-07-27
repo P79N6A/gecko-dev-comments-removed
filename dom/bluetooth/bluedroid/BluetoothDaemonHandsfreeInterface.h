974b6c0f3963e77fe9fc162837f6f2c97f4a09f0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:12:19 +0000

    Bug 1091588: Add Handsfree interface for Bluetooth daemon, r=shuang
    UltraBlame original commit: f5c572ee0ba4c537c830343a72602c9f7fe02d26

--- dom/bluetooth/bluedroid/BluetoothDaemonHandsfreeInterface.h
+++ dom/bluetooth/bluedroid/BluetoothDaemonHandsfreeInterface.h
@@ -323,6 +323,90 @@ protected:
   static BluetoothHandsfreeNotificationHandler* sNotificationHandler;
 };
 
+class BluetoothDaemonHandsfreeInterface MOZ_FINAL
+  : public BluetoothHandsfreeInterface
+{
+  class CleanupResultHandler;
+  class InitResultHandler;
+
+  enum {
+    MODE_HEADSET = 0x00,
+    MODE_NARROWBAND_SPEECH = 0x01,
+    MODE_NARRAWBAND_WIDEBAND_SPEECH = 0x02
+  };
+
+public:
+  BluetoothDaemonHandsfreeInterface(BluetoothDaemonHandsfreeModule* aModule);
+  ~BluetoothDaemonHandsfreeInterface();
+
+  void Init(
+    BluetoothHandsfreeNotificationHandler* aNotificationHandler,
+    BluetoothHandsfreeResultHandler* aRes);
+  void Cleanup(BluetoothHandsfreeResultHandler* aRes);
+
+  
+
+  void Connect(const nsAString& aBdAddr,
+               BluetoothHandsfreeResultHandler* aRes);
+  void Disconnect(const nsAString& aBdAddr,
+                  BluetoothHandsfreeResultHandler* aRes);
+  void ConnectAudio(const nsAString& aBdAddr,
+                    BluetoothHandsfreeResultHandler* aRes);
+  void DisconnectAudio(const nsAString& aBdAddr,
+                       BluetoothHandsfreeResultHandler* aRes);
+
+  
+
+  void StartVoiceRecognition(BluetoothHandsfreeResultHandler* aRes);
+  void StopVoiceRecognition(BluetoothHandsfreeResultHandler* aRes);
+
+  
+
+  void VolumeControl(BluetoothHandsfreeVolumeType aType, int aVolume,
+                     BluetoothHandsfreeResultHandler* aRes);
+
+  
+
+  void DeviceStatusNotification(BluetoothHandsfreeNetworkState aNtkState,
+                                BluetoothHandsfreeServiceType aSvcType,
+                                int aSignal, int aBattChg,
+                                BluetoothHandsfreeResultHandler* aRes);
+
+  
+
+  void CopsResponse(const char* aCops,
+                    BluetoothHandsfreeResultHandler* aRes);
+  void CindResponse(int aSvc, int aNumActive, int aNumHeld,
+                    BluetoothHandsfreeCallState aCallSetupState,
+                    int aSignal, int aRoam, int aBattChg,
+                    BluetoothHandsfreeResultHandler* aRes);
+  void FormattedAtResponse(const char* aRsp,
+                           BluetoothHandsfreeResultHandler* aRes);
+  void AtResponse(BluetoothHandsfreeAtResponse aResponseCode, int aErrorCode,
+                  BluetoothHandsfreeResultHandler* aRes);
+  void ClccResponse(int aIndex, BluetoothHandsfreeCallDirection aDir,
+                    BluetoothHandsfreeCallState aState,
+                    BluetoothHandsfreeCallMode aMode,
+                    BluetoothHandsfreeCallMptyType aMpty,
+                    const nsAString& aNumber,
+                    BluetoothHandsfreeCallAddressType aType,
+                    BluetoothHandsfreeResultHandler* aRes);
+
+  
+
+  void PhoneStateChange(int aNumActive, int aNumHeld,
+                        BluetoothHandsfreeCallState aCallSetupState,
+                        const nsAString& aNumber,
+                        BluetoothHandsfreeCallAddressType aType,
+                        BluetoothHandsfreeResultHandler* aRes);
+
+private:
+  void DispatchError(BluetoothHandsfreeResultHandler* aRes,
+                     BluetoothStatus aStatus);
+
+  BluetoothDaemonHandsfreeModule* mModule;
+};
+
 END_BLUETOOTH_NAMESPACE
 
 #endif
