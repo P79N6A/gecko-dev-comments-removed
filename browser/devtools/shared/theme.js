13f9087d23c08c87715dc7e74f7e9bcce6e89dfc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:41:34 +0000

    Bug 1169993 - Fix setTheme in theme.js to really trigger a theme change in Developer Tools. r=bgrins
    UltraBlame original commit: 4c65f2781bacc32135d6928f986173cfd5593556

--- browser/devtools/shared/theme.js
+++ browser/devtools/shared/theme.js
@@ -85,10 +85,12 @@ const getColor = exports.getColor = (type, theme) => {
 
 
 const setTheme = exports.setTheme = (newTheme) => {
+  let oldTheme = getTheme();
+
   Services.prefs.setCharPref("devtools.theme", newTheme);
   gDevTools.emit("pref-changed", {
     pref: "devtools.theme",
     newValue: newTheme,
-    oldValue: getTheme()
+    oldValue: oldTheme
   });
 };
