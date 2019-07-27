68b949ff020aed7a8d48d52d882d3b3c23e8eca5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:05:53 +0000

    Bug 901351 - Tests.  r=jorendorff
    UltraBlame original commit: d3527ee9909dc3c57b076983e503a61e281007dc

---
+++ js/src/tests/ecma_5/extensions/reviver-mutates-holder-array-ccw.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+var BUGNUMBER = 901351;
+var summary = "Behavior when the JSON.parse reviver mutates the holder array";
+
+print(BUGNUMBER + ": " + summary);
+
+
+
+
+
+if (typeof newGlobal !== "function")
+{
+  var newGlobal = function()
+  {
+    return { evaluate: eval };
+  };
+}
+
+var proxyObj = null;
+
+var arr = JSON.parse('[0, 1]', function(prop, v) {
+  if (prop === "0")
+  {
+    proxyObj = newGlobal().evaluate("({ c: 17, d: 42 })");
+    this[1] = proxyObj;
+  }
+  return v;
+});
+
+assertEq(arr[0], 0);
+assertEq(arr[1], proxyObj);
+assertEq(arr[1].c, 17);
+assertEq(arr[1].d, 42);
+
+
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
+
+print("Tests complete");
