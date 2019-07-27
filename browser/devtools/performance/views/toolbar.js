650027860706c94390b8a26106ad2d20b0acab1f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:40:22 +0000

    Bug 1150112 - Markers overview should react to theme change, and other marker views now use CSS to automatically use theme change. r=vporof
    UltraBlame original commit: 59e7246ffb378d56fec406270438a86a1cf1a8f0

--- browser/devtools/performance/views/toolbar.js
+++ browser/devtools/performance/views/toolbar.js
@@ -53,13 +53,9 @@ let ToolbarView = {
       menuitem.setAttribute("flex", "1");
       menuitem.setAttribute("label", markerDetails.label);
       menuitem.setAttribute("marker-type", markerName);
+      menuitem.className = markerDetails.colorName;
 
       menuitem.addEventListener("command", this._onHiddenMarkersChanged);
-
-      
-      let bulletStyle = `--bullet-bg: ${markerDetails.fill};`
-      bulletStyle += `--bullet-border: ${markerDetails.stroke}`;
-      menuitem.setAttribute("style", bulletStyle);
 
       $("#performance-filter-menupopup").appendChild(menuitem);
     }
--- browser/devtools/shared/timeline/global.js
+++ browser/devtools/shared/timeline/global.js
@@ -25,41 +25,37 @@ const L10N = new ViewHelpers.L10N(STRINGS_URI);
 
 
 
+
+
 const TIMELINE_BLUEPRINT = {
   "Styles": {
     group: 0,
-    fill: "hsl(285,50%,68%)",
-    stroke: "hsl(285,50%,48%)",
+    colorName: "highlight-pink",
     label: L10N.getStr("timeline.label.styles2")
   },
   "Reflow": {
     group: 0,
-    fill: "hsl(285,50%,68%)",
-    stroke: "hsl(285,50%,48%)",
+    colorName: "highlight-pink",
     label: L10N.getStr("timeline.label.reflow2")
   },
   "Paint": {
     group: 0,
-    fill: "hsl(104,57%,71%)",
-    stroke: "hsl(104,57%,51%)",
+    colorName: "highlight-green",
     label: L10N.getStr("timeline.label.paint")
   },
   "DOMEvent": {
     group: 1,
-    fill: "hsl(39,82%,69%)",
-    stroke: "hsl(39,82%,49%)",
+    colorName: "highlight-lightorange",
     label: L10N.getStr("timeline.label.domevent")
   },
   "Javascript": {
     group: 1,
-    fill: "hsl(39,82%,69%)",
-    stroke: "hsl(39,82%,49%)",
+    colorName: "highlight-lightorange",
     label: L10N.getStr("timeline.label.javascript2")
   },
   "ConsoleTime": {
     group: 2,
-    fill: "hsl(0,0%,80%)",
-    stroke: "hsl(0,0%,60%)",
+    colorName: "highlight-bluegrey",
     label: L10N.getStr("timeline.label.consoleTime")
   },
 };
