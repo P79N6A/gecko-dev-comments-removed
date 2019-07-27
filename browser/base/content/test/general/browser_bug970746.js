8c5147316fdd96761708bebc28bcfcdd5d691322
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:04:09 +0000

    Bug 1158805, e10s, fix and re-enable browser_bug970746.js, r=mconley
    UltraBlame original commit: 21adc2a4ce5e4f1646303f8225e619a559f85e2e

--- browser/base/content/test/general/browser_bug970746.js
+++ browser/base/content/test/general/browser_bug970746.js
@@ -1,125 +1,121 @@
 
 
-function test() {
-  waitForExplicitFinish();
+add_task(function *() {
+  const url = "http://mochi.test:8888/browser/browser/base/content/test/general/browser_bug970746.xhtml";
+  yield BrowserTestUtils.openNewForegroundTab(gBrowser, url);
 
-  gBrowser.selectedTab = gBrowser.addTab();
+  const ellipsis = "\u2026";
 
-  gBrowser.selectedBrowser.addEventListener("load", function() {
-    gBrowser.selectedBrowser.removeEventListener("load", arguments.callee, true);
+  let contentAreaContextMenu = document.getElementById("contentAreaContextMenu");
 
-    let doc = gBrowser.contentDocument;
-    let contentAreaContextMenu = document.getElementById("contentAreaContextMenu");
-    let ellipsis = "\u2026";
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    let testElement = function(opts) {
-      let element = doc.getElementById(opts.id);
-      document.popupNode = element;
-
-      let selection = content.getSelection();
-      selection.removeAllRanges();
-
-      if(opts.isSelected) {
-        selection.selectAllChildren(element);
-      }
-
-      let contextMenu = new nsContextMenu(contentAreaContextMenu);
-      let menuItem = document.getElementById("context-searchselect");
-
-      is(document.getElementById("context-searchselect").hidden, !opts.shouldBeShown, "search context menu item is shown for  '#" + opts.id + "' and selected is '" + opts.isSelected + "'");
-
-      if(opts.shouldBeShown) {
-        ok(menuItem.label.contains(opts.expectedLabelContents), "Menu item text '" + menuItem.label  + "' contains the correct search terms '" + opts.expectedLabelContents  + "'");
-      }
-    }
-
-    testElement({
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  let tests = [
+    {
       id: "link",
       isSelected: true,
       shouldBeShown: true,
       expectedLabelContents: "I'm a link!",
-    });
-    testElement({
+    },
+    {
       id: "link",
       isSelected: false,
       shouldBeShown: true,
       expectedLabelContents: "I'm a link!",
-    });
-
-    testElement({
+    },
+    {
       id: "longLink",
       isSelected: true,
       shouldBeShown: true,
       expectedLabelContents: "I'm a really lo" + ellipsis,
-    });
-    testElement({
+    },
+    {
       id: "longLink",
       isSelected: false,
       shouldBeShown: true,
       expectedLabelContents: "I'm a really lo" + ellipsis,
-    });
-
-    testElement({
+    },
+    {
       id: "plainText",
       isSelected: true,
       shouldBeShown: true,
       expectedLabelContents: "Right clicking " + ellipsis,
-    });
-    testElement({
+    },
+    {
       id: "plainText",
       isSelected: false,
       shouldBeShown: false,
-    });
-
-    testElement({
+    },
+    {
       id: "mixedContent",
       isSelected: true,
       shouldBeShown: true,
       expectedLabelContents: "I'm some text, " + ellipsis,
-    });
-    testElement({
+    },
+    {
       id: "mixedContent",
       isSelected: false,
       shouldBeShown: false,
-    });
-
-    testElement({
+    },
+    {
       id: "partialLink",
       isSelected: true,
       shouldBeShown: true,
       expectedLabelContents: "link selection",
-    });
-
-    testElement({
+    },
+    {
       id: "partialLink",
       isSelected: false,
       shouldBeShown: true,
       expectedLabelContents: "A partial link " + ellipsis,
-    });
-
-    testElement({
+    },
+    {
       id: "surrogatePair",
       isSelected: true,
       shouldBeShown: true,
       expectedLabelContents: "This character\uD83D\uDD25" + ellipsis,
+    }
+  ];
+
+  for (let test of tests) {
+    yield ContentTask.spawn(gBrowser.selectedBrowser,
+                            { selectElement: test.isSelected ? test.id : null },
+                            function* (arg) {
+      let selection = content.getSelection();
+      selection.removeAllRanges();
+
+      if (arg.selectElement) {
+        selection.selectAllChildren(content.document.getElementById(arg.selectElement));
+      }
     });
 
-    
-    document.popupNode = null;
-    gBrowser.removeCurrentTab();
-    finish();
-  }, true);
+    let popupShownPromise = BrowserTestUtils.waitForEvent(contentAreaContextMenu, "popupshown");
+    yield BrowserTestUtils.synthesizeMouseAtCenter("#" + test.id,
+          { type: "contextmenu", button: 2}, gBrowser.selectedBrowser);
+    yield popupShownPromise;
 
-  content.location = "http://mochi.test:8888/browser/browser/base/content/test/general/browser_bug970746.xhtml";
-}
+    let menuItem = document.getElementById("context-searchselect");
+    is(menuItem.hidden, !test.shouldBeShown,
+        "search context menu item is shown for  '#" + test.id + "' and selected is '" + test.isSelected + "'");
+
+    if (test.shouldBeShown) {
+      ok(menuItem.label.contains(test.expectedLabelContents),
+         "Menu item text '" + menuItem.label  + "' contains the correct search terms '" + test.expectedLabelContents  + "'");
+    }
+
+    let popupHiddenPromise = BrowserTestUtils.waitForEvent(contentAreaContextMenu, "popuphidden");
+    contentAreaContextMenu.hidePopup();
+    yield popupHiddenPromise;
+  }
+
+  
+  gBrowser.removeCurrentTab();
+});
