34dfd1f76d67cba3dca16121c6a112be3a42cae1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:18:35 +0000

    Bug 1050376 - Test docshell recording of restyle/reflow/paint; r=smaug
    UltraBlame original commit: f3643ddd721fcf5882d9414ba7301e6025803b06

---
+++ docshell/test/browser/browser_timelineMarkers-01.js
@@ -0,0 +1,46 @@
+
+
+
+"use strict";
+
+
+
+let test = Task.async(function*() {
+  waitForExplicitFinish();
+
+  yield openUrl("data:text/html;charset=utf-8,Test page");
+
+  let docShell = content.QueryInterface(Ci.nsIInterfaceRequestor)
+                        .getInterface(Ci.nsIWebNavigation)
+                        .QueryInterface(Ci.nsIDocShell);
+
+  ok("recordProfileTimelineMarkers" in docShell,
+    "The recordProfileTimelineMarkers attribute exists");
+  ok("popProfileTimelineMarkers" in docShell,
+    "The popProfileTimelineMarkers function exists");
+  ok(docShell.recordProfileTimelineMarkers === false,
+    "recordProfileTimelineMarkers is false by default");
+  ok(docShell.popProfileTimelineMarkers().length === 0,
+    "There are no markers by default");
+
+  docShell.recordProfileTimelineMarkers = true;
+  ok(docShell.recordProfileTimelineMarkers === true,
+    "recordProfileTimelineMarkers can be set to true");
+
+  docShell.recordProfileTimelineMarkers = false;
+  ok(docShell.recordProfileTimelineMarkers === false,
+    "recordProfileTimelineMarkers can be set to false");
+
+  gBrowser.removeCurrentTab();
+  finish();
+});
+
+function openUrl(url) {
+  return new Promise(function(resolve, reject) {
+    gBrowser.selectedTab = gBrowser.addTab(url);
+    gBrowser.selectedBrowser.addEventListener("load", function onload() {
+      gBrowser.selectedBrowser.removeEventListener("load", onload, true);
+      waitForFocus(resolve, content);
+    }, true);
+  });
+}
