6b84a1279939f863bb465d197ee8c83c6568edc7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:46:40 +0000

    Bug 1085533, part 1 - Add MakePathForRoundedRect and MakePathForEllipse helpers to Moz2D. r=mattwoodrow
    UltraBlame original commit: 6decbde116bc7f899ae63c0dbf503895da1b88f6

--- gfx/2d/PathHelpers.h
+++ gfx/2d/PathHelpers.h
@@ -146,6 +146,16 @@ GFX2D_API void AppendRoundedRectToPath(PathBuilder* aPathBuilder,
                                        const Size(& aCornerRadii)[4],
                                        bool aDrawClockwise = true);
 
+inline TemporaryRef<Path> MakePathForRoundedRect(const DrawTarget& aDrawTarget,
+                                                 const Rect& aRect,
+                                                 const Size(& aCornerRadii)[4],
+                                                 bool aDrawClockwise = true)
+{
+  RefPtr<PathBuilder> builder = aDrawTarget.CreatePathBuilder();
+  AppendRoundedRectToPath(builder, aRect, aCornerRadii, aDrawClockwise);
+  return builder->Finish();
+}
+
 
 
 
@@ -156,6 +166,15 @@ GFX2D_API void AppendRoundedRectToPath(PathBuilder* aPathBuilder,
 GFX2D_API void AppendEllipseToPath(PathBuilder* aPathBuilder,
                                    const Point& aCenter,
                                    const Size& aDimensions);
+
+inline TemporaryRef<Path> MakePathForEllipse(const DrawTarget& aDrawTarget,
+                                             const Point& aCenter,
+                                             const Size& aDimensions)
+{
+  RefPtr<PathBuilder> builder = aDrawTarget.CreatePathBuilder();
+  AppendEllipseToPath(builder, aCenter, aDimensions);
+  return builder->Finish();
+}
 
 
 
