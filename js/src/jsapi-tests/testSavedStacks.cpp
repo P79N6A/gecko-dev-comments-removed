b1f12699607c721c2232988e0b6c50abb4d53526
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:15:52 +0000

    Bug 1031152 - Part 2: Define a JSAPI test for the SavedFrame public API; r=jandem
    UltraBlame original commit: f9faafa3cd45d86fdfe775f236244dee91176b79

--- js/src/jsapi-tests/testSavedStacks.cpp
+++ js/src/jsapi-tests/testSavedStacks.cpp
@@ -22,3 +22,45 @@ BEGIN_TEST(testSavedStacks_withNoStack)
     return true;
 }
 END_TEST(testSavedStacks_withNoStack)
+
+BEGIN_TEST(testSavedStacks_ApiDefaultValues)
+{
+    js::RootedSavedFrame savedFrame(cx, nullptr);
+
+    
+    JS::RootedString str(cx);
+    JS::SavedFrameResult result = JS::GetSavedFrameSource(cx, savedFrame, &str);
+    CHECK(result == JS::SavedFrameResult::AccessDenied);
+    CHECK(str.get() == cx->runtime()->emptyString);
+
+    
+    uint32_t line = 123;
+    result = JS::GetSavedFrameLine(cx, savedFrame, &line);
+    CHECK(result == JS::SavedFrameResult::AccessDenied);
+    CHECK(line == 0);
+
+    
+    uint32_t column = 123;
+    result = JS::GetSavedFrameColumn(cx, savedFrame, &column);
+    CHECK(result == JS::SavedFrameResult::AccessDenied);
+    CHECK(column == 0);
+
+    
+    result = JS::GetSavedFrameFunctionDisplayName(cx, savedFrame, &str);
+    CHECK(result == JS::SavedFrameResult::AccessDenied);
+    CHECK(str.get() == nullptr);
+
+    
+    JS::RootedObject parent(cx);
+    result = JS::GetSavedFrameParent(cx, savedFrame, &parent);
+    CHECK(result == JS::SavedFrameResult::AccessDenied);
+    CHECK(parent.get() == nullptr);
+
+    
+    result = JS::GetSavedFrameSource(cx, savedFrame, &str);
+    CHECK(result == JS::SavedFrameResult::AccessDenied);
+    CHECK(str.get() == cx->runtime()->emptyString);
+
+    return true;
+}
+END_TEST(testSavedStacks_ApiDefaultValues)
