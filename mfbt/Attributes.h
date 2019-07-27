077aaad6df1fdf12ccc8de3c1d541f57ba8191f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:34:49 +0000

    Bug 1114987 - Add MOZ_UNSAFE_REF and mark GlobalObject::mGlobalObject as such; r=smaug
    UltraBlame original commit: 67b3ac5b553ad88f7d1c4ba968da2042cd28ff83

--- mfbt/Attributes.h
+++ mfbt/Attributes.h
@@ -521,6 +521,10 @@
 
 
 
+
+
+
+
 #ifdef MOZ_CLANG_PLUGIN
 #  define MOZ_MUST_OVERRIDE __attribute__((annotate("moz_must_override")))
 #  define MOZ_STACK_CLASS __attribute__((annotate("moz_stack_class")))
@@ -537,6 +541,7 @@
 #  define MOZ_NO_ARITHMETIC_EXPR_IN_ARGUMENT __attribute__((annotate("moz_no_arith_expr_in_arg")))
 #  define MOZ_OWNING_REF __attribute__((annotate("moz_strong_ref")))
 #  define MOZ_NON_OWNING_REF __attribute__((annotate("moz_weak_ref")))
+#  define MOZ_UNSAFE_REF(reason) __attribute__((annotate("moz_strong_ref")))
 
 
 
@@ -558,6 +563,7 @@
 #  define MOZ_HEAP_ALLOCATOR
 #  define MOZ_OWNING_REF
 #  define MOZ_NON_OWNING_REF
+#  define MOZ_UNSAFE_REF(reason)
 #endif 
 
 
