8c42c63dc1db1072c57fdcd0d3ce3bd4ecad1d90
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:12:09 +0000

    Bug 1085092 - Fix stroking of SVG polygon elements. r=longsonr
    UltraBlame original commit: 5b17709f660b181e5d198410fc19edcf65f2551c

--- content/svg/content/src/SVGPolylineElement.h
+++ content/svg/content/src/SVGPolylineElement.h
@@ -24,6 +24,9 @@ protected:
   friend nsresult (::NS_NewSVGPolylineElement(nsIContent **aResult,
                                               already_AddRefed<mozilla::dom::NodeInfo>&& aNodeInfo));
 
+  
+  virtual mozilla::TemporaryRef<Path> BuildPath(PathBuilder* aBuilder) MOZ_OVERRIDE;
+
 public:
   
   virtual nsresult Clone(mozilla::dom::NodeInfo *aNodeInfo, nsINode **aResult) const;
