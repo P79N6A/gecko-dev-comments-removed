61aa49b5020d6e4506765ca75f9639fd05ce1939
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:37:50 +0000

    Bug 1070992 - Remove classes for ISO-8859-1 decoder as well as ISO-8859-9, ISO-8859-11 and TIS-620, which are mapped to the corresponding windows-125* encodings in the Encoding Standard. r=emk.
    UltraBlame original commit: 0c7648470c6ff30cc9ce844ce4e9008bcc59f558

--- intl/uconv/nsISO88591ToUnicode.h
+++ intl/uconv/nsISO88591ToUnicode.h
@@ -6,25 +6,11 @@
 #ifndef nsISO88591ToUnicode_h___
 #define nsISO88591ToUnicode_h___
 
-#include "nsID.h"
-
-class nsISupports;
-
 
 
 #define NS_ISO88591TOUNICODE_CID \
   { 0xa3254cb0, 0x8e20, 0x11d2, {0x8a, 0x98, 0x0, 0x60, 0x8, 0x11, 0xa8, 0x36}}
 
 #define NS_ISO88591TOUNICODE_CONTRACTID "@mozilla.org/intl/unicode/decoder;1?charset=ISO-8859-1"
-
-
-
-
-
-
-
-nsresult
-nsISO88591ToUnicodeConstructor(nsISupports *aOuter, REFNSIID aIID,
-                               void **aResult);
 
 #endif 
