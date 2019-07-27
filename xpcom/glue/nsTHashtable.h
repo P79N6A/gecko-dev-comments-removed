323e2e9d0bd6513a4556954f3fbb9a7d2dc03f26
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:20:33 +0000

    Bug 1165206 - Tweak nsTHashtable's move constructor. r=froydnj.
    The SetOps() call is no longer necessary now that PLDHashTable has a move
    constructor.
    
    This change originally landed in one of the patches from bug 1161377, which was
    subsequently backed out.
    
    UltraBlame original commit: 923d7a4a2398011fa8cc731496a3644ce5ebdaad

--- xpcom/glue/nsTHashtable.h
+++ xpcom/glue/nsTHashtable.h
@@ -406,10 +406,6 @@ nsTHashtable<EntryType>::nsTHashtable(nsTHashtable<EntryType>&& aOther)
   
   
   MOZ_MAKE_MEM_UNDEFINED(&aOther.mTable, sizeof(aOther.mTable));
-
-  
-  
-  aOther.mTable.SetOps(nullptr);
 }
 
 template<class EntryType>
