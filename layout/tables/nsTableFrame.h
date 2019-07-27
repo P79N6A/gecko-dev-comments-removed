5e717afd2a74ff16bbc0142b36dade7d199f18aa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:12:53 +0000

    Bug 1022612. Part 23: Remove nsDisplayItem::IsVaryingRelativeToMovingFrame. r=mattwoodrow
    It's obsolete and no-one calls it.
    
    UltraBlame original commit: 2f4e9da0e4b6893c321b918d4e2b373c8a5bf5d9

--- layout/tables/nsTableFrame.h
+++ layout/tables/nsTableFrame.h
@@ -50,8 +50,6 @@ public:
       nsDisplayItem(aBuilder, aFrame),
       mPartHasFixedBackground(false) {}
 
-  virtual bool IsVaryingRelativeToMovingFrame(nsDisplayListBuilder* aBuilder,
-                                                nsIFrame* aFrame) MOZ_OVERRIDE;
   
   
   
