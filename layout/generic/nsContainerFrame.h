f74411bb6b0e48e8163767638e74d17a584b879a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:35:43 +0000

    Bug 1079154 patch 4: Logical versions of ReflowChild and FinishReflowChild, r=jfkthame
    UltraBlame original commit: c16d59bb79d13ff7221511a13b134241dbdc7f44

--- layout/generic/nsContainerFrame.h
+++ layout/generic/nsContainerFrame.h
@@ -245,8 +245,9 @@ public:
                    nsPresContext*                 aPresContext,
                    nsHTMLReflowMetrics&           aDesiredSize,
                    const nsHTMLReflowState&       aReflowState,
-                   nscoord                        aX,
-                   nscoord                        aY,
+                   const mozilla::WritingMode&    aWM,
+                   const mozilla::LogicalPoint&   aPos,
+                   nscoord                        aContainerWidth,
                    uint32_t                       aFlags,
                    nsReflowStatus&                aStatus,
                    nsOverflowContinuationTracker* aTracker = nullptr);
@@ -268,6 +269,28 @@ public:
 
 
 
+  static void FinishReflowChild(nsIFrame*                    aKidFrame,
+                                nsPresContext*               aPresContext,
+                                const nsHTMLReflowMetrics&   aDesiredSize,
+                                const nsHTMLReflowState*     aReflowState,
+                                const mozilla::WritingMode&  aWM,
+                                const mozilla::LogicalPoint& aPos,
+                                nscoord                      aContainerWidth,
+                                uint32_t                     aFlags);
+
+  
+  
+  
+  void ReflowChild(nsIFrame*                      aKidFrame,
+                   nsPresContext*                 aPresContext,
+                   nsHTMLReflowMetrics&           aDesiredSize,
+                   const nsHTMLReflowState&       aReflowState,
+                   nscoord                        aX,
+                   nscoord                        aY,
+                   uint32_t                       aFlags,
+                   nsReflowStatus&                aStatus,
+                   nsOverflowContinuationTracker* aTracker = nullptr);
+
   static void FinishReflowChild(nsIFrame*                  aKidFrame,
                                 nsPresContext*             aPresContext,
                                 const nsHTMLReflowMetrics& aDesiredSize,
@@ -276,7 +299,6 @@ public:
                                 nscoord                    aY,
                                 uint32_t                   aFlags);
 
-  
   static void PositionChildViews(nsIFrame* aFrame);
 
   
