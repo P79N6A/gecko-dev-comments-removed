d8bf8faa138dd5e04172c4dc043b56e93ec53459
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:23:00 +0000

    Bug 1144245 - Add a LoginTestUtils.jsm module. r=MattN
    UltraBlame original commit: a89f634ec7a45b0e2bb4c887a978870205f7fcdf

---
+++ toolkit/components/passwordmgr/test/LoginTestUtils.jsm
@@ -0,0 +1,239 @@
+
+
+
+
+
+
+
+"use strict";
+
+this.EXPORTED_SYMBOLS = [
+  "LoginTestUtils",
+];
+
+const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
+
+Cu.import("resource://gre/modules/Promise.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/Task.jsm");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+Cu.import("resource://testing-common/TestUtils.jsm");
+
+const LoginInfo =
+      Components.Constructor("@mozilla.org/login-manager/loginInfo;1",
+                             "nsILoginInfo", "init");
+
+
+let Assert = null;
+
+this.LoginTestUtils = {
+  set Assert(assert) {
+    Assert = assert;
+  },
+
+  
+
+
+
+  reloadData() {
+    Services.obs.notifyObservers(null, "passwordmgr-storage-replace", null);
+    yield TestUtils.topicObserved("passwordmgr-storage-replace-complete");
+  },
+
+  
+
+
+  clearData() {
+    Services.logins.removeAllLogins();
+    for (let hostname of Services.logins.getAllDisabledHosts()) {
+      Services.logins.setLoginSavingEnabled(hostname, true);
+    }
+  },
+
+  
+
+
+
+
+  checkLogins(expectedLogins) {
+    this.assertLoginListsEqual(Services.logins.getAllLogins(), expectedLogins);
+  },
+
+  
+
+
+
+
+
+  assertLoginListsEqual(actual, expected) {
+    Assert.equal(expected.length, actual.length);
+    Assert.ok(expected.every(e => actual.some(a => a.equals(e))));
+  },
+
+  
+
+
+
+  assertDisabledHostsEqual(actual, expected) {
+    Assert.deepEqual(actual.sort(), expected.sort());
+  },
+
+  
+
+
+
+  assertTimeIsAboutNow(timeMs) {
+    Assert.ok(Math.abs(timeMs - Date.now()) < 30000);
+  },
+};
+
+
+
+
+
+
+
+
+
+this.LoginTestUtils.testData = {
+  
+
+
+
+
+
+
+  formLogin(modifications) {
+    let loginInfo = new LoginInfo("http://www3.example.com",
+                                  "http://www.example.com", null,
+                                  "the username", "the password",
+                                  "form_field_username", "form_field_password");
+    loginInfo.QueryInterface(Ci.nsILoginMetaInfo);
+    if (modifications) {
+      for (let [name, value] of Iterator(modifications)) {
+        loginInfo[name] = value;
+      }
+    }
+    return loginInfo;
+  },
+
+  
+
+
+
+
+
+
+  authLogin(modifications) {
+    let loginInfo = new LoginInfo("http://www.example.org", null,
+                                  "The HTTP Realm", "the username",
+                                  "the password", "", "");
+    loginInfo.QueryInterface(Ci.nsILoginMetaInfo);
+    if (modifications) {
+      for (let [name, value] of Iterator(modifications)) {
+        loginInfo[name] = value;
+      }
+    }
+    return loginInfo;
+  },
+
+  
+
+
+
+  loginList() {
+    return [
+      
+
+      
+      new LoginInfo("http://www.example.com", "http://www.example.com", null,
+                    "the username", "the password for www.example.com",
+                    "form_field_username", "form_field_password"),
+
+      
+      new LoginInfo("https://www.example.com", "https://www.example.com", null,
+                    "the username", "the password for https",
+                    "form_field_username", "form_field_password"),
+
+      
+      new LoginInfo("https://example.com", "https://example.com", null,
+                    "the username", "the password for example.com",
+                    "form_field_username", "form_field_password"),
+
+      
+      
+      new LoginInfo("http://www3.example.com", "http://www.example.com", null,
+                    "the username", "the password",
+                    "form_field_username", "form_field_password"),
+      new LoginInfo("http://www3.example.com", "https://www.example.com", null,
+                    "the username", "the password",
+                    "form_field_username", "form_field_password"),
+      new LoginInfo("http://www3.example.com", "http://example.com", null,
+                    "the username", "the password",
+                    "form_field_username", "form_field_password"),
+
+      
+      
+      
+      new LoginInfo("http://www4.example.com", "http://www4.example.com", null,
+                    "username one", "password one",
+                    "form_field_username", "form_field_password"),
+      new LoginInfo("http://www4.example.com", "http://www4.example.com", null,
+                    "username two", "password two",
+                    "form_field_username", "form_field_password"),
+      new LoginInfo("http://www4.example.com", "http://www4.example.com", null,
+                    "", "password three",
+                    "form_field_username", "form_field_password"),
+
+      
+      new LoginInfo("http://www5.example.com", "http://www5.example.com", null,
+                    "multi username", "multi password", "", ""),
+
+      
+      new LoginInfo("http://www6.example.com", "http://www6.example.com", null,
+                    "", "12345", "", "form_field_password"),
+
+      
+
+      
+      new LoginInfo("http://www.example.org", null, "The HTTP Realm",
+                    "the username", "the password", "", ""),
+
+      
+      new LoginInfo("ftp://ftp.example.org", null, "ftp://ftp.example.org",
+                    "the username", "the password", "", ""),
+
+      
+      new LoginInfo("http://www2.example.org", null, "The HTTP Realm",
+                    "the username", "the password", "", ""),
+      new LoginInfo("http://www2.example.org", null, "The HTTP Realm Other",
+                    "the username other", "the password other", "", ""),
+
+      
+
+      new LoginInfo("http://example.net", "http://example.net", null,
+                    "the username", "the password",
+                    "form_field_username", "form_field_password"),
+      new LoginInfo("http://example.net", "http://www.example.net", null,
+                    "the username", "the password",
+                    "form_field_username", "form_field_password"),
+      new LoginInfo("http://example.net", "http://www.example.net", null,
+                    "username two", "the password",
+                    "form_field_username", "form_field_password"),
+      new LoginInfo("http://example.net", null, "The HTTP Realm",
+                    "the username", "the password", "", ""),
+      new LoginInfo("http://example.net", null, "The HTTP Realm Other",
+                    "username two", "the password", "", ""),
+      new LoginInfo("ftp://example.net", null, "ftp://example.net",
+                    "the username", "the password", "", ""),
+
+      
+
+      new LoginInfo("chrome://example_extension", null, "Example Login One",
+                    "the username", "the password one", "", ""),
+      new LoginInfo("chrome://example_extension", null, "Example Login Two",
+                    "the username", "the password two", "", ""),
+    ];
+  },
+};
--- toolkit/components/passwordmgr/test/unit/head.js
+++ toolkit/components/passwordmgr/test/unit/head.js
@@ -32,6 +32,13 @@ const LoginInfo =
                              "nsILoginInfo", "init");
 
 
+XPCOMUtils.defineLazyModuleGetter(this, "LoginTest",
+                                  "resource://testing-common/LoginTestUtils.jsm",
+                                  "LoginTestUtils");
+LoginTest.Assert = Assert;
+const TestData = LoginTest.testData;
+
+
 
 
 function run_test()
@@ -96,29 +103,6 @@ function getTempFile(aLeafName)
 
 
 
-function promiseTopicObserved(aTopic)
-{
-  let deferred = Promise.defer();
-
-  Services.obs.addObserver(
-    function PTO_observe(aSubject, aTopic, aData) {
-      Services.obs.removeObserver(PTO_observe, aTopic);
-      deferred.resolve([aSubject, aData]);
-    }, aTopic, false);
-
-  return deferred.promise;
-}
-
-
-
-
-
-
-
-
-
-
-
 function newPropertyBag(aProperties)
 {
   let propertyBag = Cc["@mozilla.org/hash-property-bag;1"]
@@ -134,70 +118,6 @@ function newPropertyBag(aProperties)
 }
 
 
-
-
-const LoginTest = {
-  
-
-
-
-  reloadData: function ()
-  {
-    Services.obs.notifyObservers(null, "passwordmgr-storage-replace", null);
-    yield promiseTopicObserved("passwordmgr-storage-replace-complete");
-  },
-
-  
-
-
-  clearData: function ()
-  {
-    Services.logins.removeAllLogins();
-    for (let hostname of Services.logins.getAllDisabledHosts()) {
-      Services.logins.setLoginSavingEnabled(hostname, true);
-    }
-  },
-
-  
-
-
-
-
-  checkLogins: function (aExpectedLogins)
-  {
-    this.assertLoginListsEqual(Services.logins.getAllLogins(), aExpectedLogins);
-  },
-
-  
-
-
-
-
-
-  assertLoginListsEqual: function (aActual, aExpected)
-  {
-    do_check_eq(aExpected.length, aActual.length);
-    do_check_true(aExpected.every(e => aActual.some(a => a.equals(e))));
-  },
-
-  
-
-
-
-  assertDisabledHostsEqual: function (aActual, aExpected)
-  {
-    Assert.deepEqual(aActual.sort(), aExpected.sort());
-  },
-
-  
-
-
-
-  assertTimeIsAboutNow: function (aTimeMs)
-  {
-    do_check_true(Math.abs(aTimeMs - Date.now()) < 30000);
-  }
-};
 
 const RecipeHelpers = {
   initNewParent() {
@@ -237,163 +157,6 @@ const RecipeHelpers = {
     return form;
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-const TestData = {
-  
-
-
-
-
-
-
-  formLogin: function (aModifications)
-  {
-    let loginInfo = new LoginInfo("http://www3.example.com",
-                                  "http://www.example.com", null,
-                                  "the username", "the password",
-                                  "form_field_username", "form_field_password");
-    loginInfo.QueryInterface(Ci.nsILoginMetaInfo);
-    if (aModifications) {
-      for (let [name, value] of Iterator(aModifications)) {
-        loginInfo[name] = value;
-      }
-    }
-    return loginInfo;
-  },
-
-  
-
-
-
-
-
-
-  authLogin: function (aModifications)
-  {
-    let loginInfo = new LoginInfo("http://www.example.org", null,
-                                  "The HTTP Realm", "the username",
-                                  "the password", "", "");
-    loginInfo.QueryInterface(Ci.nsILoginMetaInfo);
-    if (aModifications) {
-      for (let [name, value] of Iterator(aModifications)) {
-        loginInfo[name] = value;
-      }
-    }
-    return loginInfo;
-  },
-
-  
-
-
-
-  loginList: function ()
-  {
-    return [
-      
-
-      
-      new LoginInfo("http://www.example.com", "http://www.example.com", null,
-                    "the username", "the password for www.example.com",
-                    "form_field_username", "form_field_password"),
-
-      
-      new LoginInfo("https://www.example.com", "https://www.example.com", null,
-                    "the username", "the password for https",
-                    "form_field_username", "form_field_password"),
-
-      
-      new LoginInfo("https://example.com", "https://example.com", null,
-                    "the username", "the password for example.com",
-                    "form_field_username", "form_field_password"),
-
-      
-      
-      new LoginInfo("http://www3.example.com", "http://www.example.com", null,
-                    "the username", "the password",
-                    "form_field_username", "form_field_password"),
-      new LoginInfo("http://www3.example.com", "https://www.example.com", null,
-                    "the username", "the password",
-                    "form_field_username", "form_field_password"),
-      new LoginInfo("http://www3.example.com", "http://example.com", null,
-                    "the username", "the password",
-                    "form_field_username", "form_field_password"),
-
-      
-      
-      
-      new LoginInfo("http://www4.example.com", "http://www4.example.com", null,
-                    "username one", "password one",
-                    "form_field_username", "form_field_password"),
-      new LoginInfo("http://www4.example.com", "http://www4.example.com", null,
-                    "username two", "password two",
-                    "form_field_username", "form_field_password"),
-      new LoginInfo("http://www4.example.com", "http://www4.example.com", null,
-                    "", "password three",
-                    "form_field_username", "form_field_password"),
-
-      
-      new LoginInfo("http://www5.example.com", "http://www5.example.com", null,
-                    "multi username", "multi password", "", ""),
-
-      
-      new LoginInfo("http://www6.example.com", "http://www6.example.com", null,
-                    "", "12345", "", "form_field_password"),
-
-      
-
-      
-      new LoginInfo("http://www.example.org", null, "The HTTP Realm",
-                    "the username", "the password", "", ""),
-
-      
-      new LoginInfo("ftp://ftp.example.org", null, "ftp://ftp.example.org",
-                    "the username", "the password", "", ""),
-
-      
-      new LoginInfo("http://www2.example.org", null, "The HTTP Realm",
-                    "the username", "the password", "", ""),
-      new LoginInfo("http://www2.example.org", null, "The HTTP Realm Other",
-                    "the username other", "the password other", "", ""),
-
-      
-
-      new LoginInfo("http://example.net", "http://example.net", null,
-                    "the username", "the password",
-                    "form_field_username", "form_field_password"),
-      new LoginInfo("http://example.net", "http://www.example.net", null,
-                    "the username", "the password",
-                    "form_field_username", "form_field_password"),
-      new LoginInfo("http://example.net", "http://www.example.net", null,
-                    "username two", "the password",
-                    "form_field_username", "form_field_password"),
-      new LoginInfo("http://example.net", null, "The HTTP Realm",
-                    "the username", "the password", "", ""),
-      new LoginInfo("http://example.net", null, "The HTTP Realm Other",
-                    "username two", "the password", "", ""),
-      new LoginInfo("ftp://example.net", null, "ftp://example.net",
-                    "the username", "the password", "", ""),
-
-      
-
-      new LoginInfo("chrome://example_extension", null, "Example Login One",
-                    "the username", "the password one", "", ""),
-      new LoginInfo("chrome://example_extension", null, "Example Login Two",
-                    "the username", "the password two", "", ""),
-    ];
-  },
-};
-
 
 
 
