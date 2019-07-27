439ad1e9bd2ca4678739395f89579ccca7af7bcd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:14:03 +0000

    bug 1038304 - websockets offline hang r=sworkman
    UltraBlame original commit: 0fd5c666d35cca02d9a15151321928e4bddaccd4

---
+++ netwerk/test/unit/test_websocket_offline.js
@@ -0,0 +1,45 @@
+
+
+
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+
+
+var url = "ws://localhost";
+var chan;
+var offlineStatus;
+
+var listener = {
+  onAcknowledge: function(aContext, aSize) {},
+  onBinaryMessageAvailable: function(aContext, aMsg) {},
+  onMessageAvailable: function(aContext, aMsg) {},
+  onServerClose: function(aContext, aCode, aReason) {},
+  onStart: function(aContext)
+  {
+    
+    do_check_true(false);
+  },
+  onStop: function(aContext, aStatusCode)
+  {
+    do_check_neq(aStatusCode, Cr.NS_OK);
+    Services.io.offline = offlineStatus;
+    do_test_finished();
+  }
+};
+
+function run_test() {
+  offlineStatus = Services.io.offline;
+  Services.io.offline = true;
+
+  try {
+    chan = Cc["@mozilla.org/network/protocol;1?name=ws"].
+      createInstance(Components.interfaces.nsIWebSocketChannel);
+    var uri = Services.io.newURI(url, null, null);
+    chan.asyncOpen(uri, url, listener, null);
+    do_test_pending();
+  } catch (x) {
+    dump("throwing " + x);
+    do_throw(x);
+  }
+}
