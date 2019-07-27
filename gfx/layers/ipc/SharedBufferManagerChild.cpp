ff452525d400794883659dccaaade5312c9b06f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:21:23 +0000

    Bug 1099437 - Part 1: Protect against negative sizes and overflow. r=nical
    UltraBlame original commit: 5597611e315fa2d1590549f165ecf71fba70d182

--- gfx/layers/ipc/SharedBufferManagerChild.cpp
+++ gfx/layers/ipc/SharedBufferManagerChild.cpp
@@ -8,6 +8,7 @@
 #include "base/task.h"                  
 #include "base/thread.h"                
 #include "base/tracked.h"               
+#include "mozilla/gfx/Logging.h"        
 #include "mozilla/layers/SharedBufferManagerChild.h"
 #include "mozilla/layers/SharedBufferManagerParent.h"
 #include "mozilla/StaticPtr.h"          
@@ -239,6 +240,11 @@ SharedBufferManagerChild::AllocGrallocBuffer(const gfx::IntSize& aSize,
                                              const uint32_t& aUsage,
                                              mozilla::layers::MaybeMagicGrallocBufferHandle* aBuffer)
 {
+  if (aSize.width <= 0 || aSize.height <= 0) {
+    gfxDebug() << "Asking for gralloc of invalid size " << aSize.width << "x" << aSize.height;
+    return false;
+  }
+
   if (InSharedBufferManagerChildThread()) {
     return SharedBufferManagerChild::AllocGrallocBufferNow(aSize, aFormat, aUsage, aBuffer);
   }
@@ -264,6 +270,9 @@ SharedBufferManagerChild::AllocGrallocBufferNow(const IntSize& aSize,
                                                 const uint32_t& aUsage,
                                                 mozilla::layers::MaybeMagicGrallocBufferHandle* aHandle)
 {
+  
+  MOZ_ASSERT(aSize.width >= 0 && aSize.height >= 0);
+
 #ifdef MOZ_HAVE_SURFACEDESCRIPTORGRALLOC
   mozilla::layers::MaybeMagicGrallocBufferHandle handle;
   SendAllocateGrallocBuffer(aSize, aFormat, aUsage, &handle);
