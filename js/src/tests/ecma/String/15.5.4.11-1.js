8d1830383e4f635d838502a0818e1d485999e3c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:33:38 +0000

    Bug 911142 - Make the "length" property of function objects configurable. r=Waldo.
    Thanks to Till Schneidereit for a bunch of test fixes.
    
    UltraBlame original commit: c61b88b3479a7356136fe0916c6e5df8e00e67ff

--- js/src/tests/ecma/String/15.5.4.11-1.js
+++ js/src/tests/ecma/String/15.5.4.11-1.js
@@ -33,8 +33,6 @@ var TITLE   = "String.prototype.toLowerCase()";
 writeHeaderToLog( SECTION + " "+ TITLE);
 
 new TestCase( SECTION,  "String.prototype.toLowerCase.length",        0,          String.prototype.toLowerCase.length );
-new TestCase( SECTION,  "delete String.prototype.toLowerCase.length", false,      delete String.prototype.toLowerCase.length );
-new TestCase( SECTION,  "delete String.prototype.toLowerCase.length; String.prototype.toLowerCase.length", 0,      eval("delete String.prototype.toLowerCase.length; String.prototype.toLowerCase.length") );
 
 
 for ( var i = 0; i <= 0x017f; i++ ) {
