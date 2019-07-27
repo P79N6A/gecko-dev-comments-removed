6309987e5a4d7125505d0d81a49cc4f5f9e9197c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:01:43 +0000

    Bug 1129697 - Fix browser_555547.js r=mak
    UltraBlame original commit: 78fe0a9ea65b6c8e571e86a3e2e962ab921e9c4c

--- browser/components/places/tests/browser/browser_555547.js
+++ browser/components/places/tests/browser/browser_555547.js
@@ -2,58 +2,63 @@
 
 
 
-add_task(function* test() {
-  waitForExplicitFinish();
+"use strict";
 
+add_task(function* test() {
   let sidebarBox = document.getElementById("sidebar-box");
   is(sidebarBox.hidden, true, "The sidebar should be hidden");
 
   
   let toolbar = document.getElementById("PersonalToolbar");
   let wasCollapsed = toolbar.collapsed;
-  if (wasCollapsed)
+  if (wasCollapsed) {
     yield promiseSetToolbarVisibility(toolbar, true);
+  }
 
-  let sidebar = document.getElementById("sidebar");
-  sidebar.addEventListener("load", function() {
-    sidebar.removeEventListener("load", arguments.callee, true);
-    let tree = sidebar.contentDocument.getElementById("bookmarks-view");
+  let sidebar = yield promiseLoadedSidebar("viewBookmarksSidebar");
+  registerCleanupFunction(toggleSidebar);
 
-    
-    executeSoon(function() {
-      
-      tree.focus();
+  
+  let tree = sidebar.contentDocument.getElementById("bookmarks-view");
+  tree.focus();
 
-      let controller = doGetPlacesControllerForCommand("placesCmd_copy");
-      let treeController = tree.controllers
-                               .getControllerForCommand("placesCmd_copy");
-      ok(controller == treeController, "tree controller was returned");
+  let controller = doGetPlacesControllerForCommand("placesCmd_copy");
+  let treeController = tree.controllers
+                           .getControllerForCommand("placesCmd_copy");
+  ok(controller == treeController, "tree controller was returned");
 
-      
-      
-      let toolbarItems = document.getElementById("PlacesToolbarItems");
-      EventUtils.synthesizeMouse(toolbarItems.childNodes[0],
-                                 4, 4, { type: "contextmenu", button: 2 },
-                                 window);
-      controller = doGetPlacesControllerForCommand("placesCmd_copy");
-      let toolbarController = document.getElementById("PlacesToolbar")
-                                      .controllers
-                                      .getControllerForCommand("placesCmd_copy");
-      ok(controller == toolbarController, "the toolbar controller was returned");
+  
+  
+  let toolbarItems = document.getElementById("PlacesToolbarItems");
+  EventUtils.synthesizeMouse(toolbarItems.childNodes[0],
+                             4, 4, { type: "contextmenu", button: 2 },
+                             window);
+  controller = doGetPlacesControllerForCommand("placesCmd_copy");
+  let toolbarController = document.getElementById("PlacesToolbar")
+                                  .controllers
+                                  .getControllerForCommand("placesCmd_copy");
+  ok(controller == toolbarController, "the toolbar controller was returned");
 
-      document.getElementById("placesContext").hidePopup();
+  document.getElementById("placesContext").hidePopup();
 
-      
-      tree.focus();
-      controller = doGetPlacesControllerForCommand("placesCmd_copy");
-      ok(controller == treeController, "tree controller was returned");
+  
+  tree.focus();
+  controller = doGetPlacesControllerForCommand("placesCmd_copy");
+  ok(controller == treeController, "tree controller was returned");
 
-      toggleSidebar();
-      if (wasCollapsed)
-        yield promiseSetToolbarVisibility(toolbar, false);
-
-      finish();
-    });
-  }, true);
-  toggleSidebar("viewBookmarksSidebar", true);
+  if (wasCollapsed) {
+    yield promiseSetToolbarVisibility(toolbar, false);
+  }
 });
+
+function promiseLoadedSidebar(cmd) {
+  return new Promise(resolve => {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.addEventListener("load", function onLoad() {
+      sidebar.removeEventListener("load", onLoad, true);
+      resolve(sidebar);
+    }, true);
+
+    toggleSidebar(cmd, true);
+  });
+}
