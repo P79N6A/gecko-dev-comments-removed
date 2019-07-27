60948fe251184ced7ddf045dcb31411fb449d7af
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:28:32 +0000

    Bug 1122329 - Remove `= null` from `instance = null` in StringHelper. r=mhaigh
    UltraBlame original commit: adfa7cdbf5e68653c6a6648a592d699a5a45ae67

--- mobile/android/tests/browser/robocop/StringHelper.java
+++ mobile/android/tests/browser/robocop/StringHelper.java
@@ -10,7 +10,7 @@ import android.content.res.Resources;
 import org.mozilla.gecko.R;
 
 public class StringHelper {
-    private static StringHelper instance = null;
+    private static StringHelper instance;
 
     
     public static String STATIC_ABOUT_HOME_URL = "about:home";
