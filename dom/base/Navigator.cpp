be650cdfb0126e95d918a39d42b23845cca6587f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:55:34 +0000

    Bug 1055477 - Part 1. Add CheckPermission. r=smaug
    From 3c5b491a6dfeca7cd7e93b596f368f67b40d9632 Mon Sep 17 00:00:00 2001
    ---
     dom/base/Navigator.cpp   | 3 +--
     dom/webidl/MozNFC.webidl | 3 ++-
     2 files changed, 3 insertions(+), 3 deletions(-)
    
    UltraBlame original commit: 8507f750761a2e89326b67a69686c04849201544

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -2311,12 +2311,7 @@ Navigator::HasNFCSupport(JSContext* , JSObject* aGlobal)
 
   
   nsCOMPtr<nsISupports> contentHelper = do_GetService("@mozilla.org/nfc/content-helper;1");
-  if (!contentHelper) {
-    return false;
-  }
-
-  return win && (CheckPermission(win, "nfc-read") ||
-                 CheckPermission(win, "nfc-write"));
+  return !!contentHelper;
 }
 #endif 
 
