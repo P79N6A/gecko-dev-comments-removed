bdcff7285a1ed7154b5fa9f21423dda8a57ea1c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:04:56 +0000

    Bug 1034477 - Save stacks that cross contexts in js::SavedStacks. r=shu
    UltraBlame original commit: 5048d0b0b825eb024da2464db1d283cf0188e227

---
+++ js/src/jit-test/tests/saved-stacks/new-context.js
@@ -0,0 +1,24 @@
+
+
+const stack = (function iife() {
+  return evaluate("(" + function evalFrame() {
+    return saveStack();
+  } + "())", {
+    newContext: true,
+    fileName: "evaluated"
+  });
+}());
+
+print(stack);
+
+assertEq(stack.functionDisplayName, "evalFrame");
+assertEq(stack.source, "evaluated");
+
+assertEq(stack.parent.source, "evaluated");
+
+assertEq(stack.parent.parent.functionDisplayName, "iife");
+assertEq(/new-context\.js$/.test(stack.parent.parent.source), true);
+
+assertEq(/new-context\.js$/.test(stack.parent.parent.parent.source), true);
+
+assertEq(stack.parent.parent.parent.parent, null);
