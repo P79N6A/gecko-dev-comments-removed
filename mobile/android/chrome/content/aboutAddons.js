76243b2c4c51303b3b86cded0e6ae65721d62ea5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:04:40 +0000

    Bug 1131106 - Hide options header in add-on details view if there are no valid options. r=mfinkle
    UltraBlame original commit: 3b25c782402cd292f410c9e0e7c891f242a9300b

--- mobile/android/chrome/content/aboutAddons.js
+++ mobile/android/chrome/content/aboutAddons.js
@@ -348,6 +348,10 @@ var Addons = {
             let event = document.createEvent("Events");
             event.initEvent("AddonOptionsLoad", true, false);
             window.dispatchEvent(event);
+          } else {
+            
+            
+            detailItem.setAttribute("optionsURL", "");
           }
 
           
