8b87a742b82e366c2fff1b07d3b095bcf3c06763
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:25:32 +0000

    Bug 1110931 - Do not use volatile for synchronization; r=jonco
    UltraBlame original commit: e7664dbdbfd6dd830b2b5b88f475ac08f334cabd

--- js/src/gc/GCRuntime.h
+++ js/src/gc/GCRuntime.h
@@ -697,7 +697,7 @@ class GCRuntime
 
     bool grayBitsValid;
 
-    volatile uintptr_t majorGCRequested;
+    mozilla::Atomic<uintptr_t> majorGCRequested;
     JS::gcreason::Reason majorGCTriggerReason;
 
     bool minorGCRequested;
@@ -792,7 +792,7 @@ class GCRuntime
 
 
 
-    volatile uintptr_t interFrameGC;
+    mozilla::Atomic<uintptr_t> interFrameGC;
 
     
     int64_t sliceBudget;
@@ -836,7 +836,7 @@ class GCRuntime
 
     bool poked;
 
-    volatile js::HeapState heapState;
+    mozilla::Atomic<js::HeapState> heapState;
 
     
 
