ee79e9908ba5c7d555b9fbfd1a635a4916b77a58
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:17:40 +0000

    Bug 1100530 - Clean up OS X version constants. f=jgilbert, r=mstange
    UltraBlame original commit: f25e20a0f238bcfacd88cd0afd5016532327d3fd

--- widget/cocoa/nsCocoaFeatures.h
+++ widget/cocoa/nsCocoaFeatures.h
@@ -8,6 +8,10 @@
 
 #include <stdint.h>
 
+
+
+
+
 class nsCocoaFeatures {
 public:
   static int32_t OSXVersion();
@@ -22,6 +26,16 @@ public:
   static bool AccelerateByDefault();
 
   static bool IsAtLeastVersion(int32_t aMajor, int32_t aMinor, int32_t aBugFix=0);
+
+  
+  
+  
+  
+  static void GetSystemVersion(int &aMajor, int &aMinor, int &aBugFix);
+  static int32_t GetVersion(int32_t aMajor, int32_t aMinor, int32_t aBugFix);
+  static int32_t ExtractMajorVersion(int32_t aVersion);
+  static int32_t ExtractMinorVersion(int32_t aVersion);
+  static int32_t ExtractBugFixVersion(int32_t aVersion);
 
 private:
   static void InitializeVersionNumbers();
