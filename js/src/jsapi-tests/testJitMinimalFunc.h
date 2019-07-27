c35b37209ba0b0051e1a0a5f8bc2784c7a3b9f7d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:29:27 +0000

    Bug 1031396 - Part 2 Tests - GVN misses DCE when an MDefinition is used multiple times by the same MNode. r=sunfish
    Factor out MinimalFunc and add 2 tests for DCE.
    
    UltraBlame original commit: a42af29d7dc2cd7b067df7cefadd403d0bc9ed0d

---
+++ js/src/jsapi-tests/testJitMinimalFunc.h
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+#ifndef jsapi_tests_jitTestGVN_h
+#define jsapi_tests_jitTestGVN_h
+
+#include "jit/IonAnalysis.h"
+#include "jit/MIRGenerator.h"
+#include "jit/MIRGraph.h"
+#include "jit/ValueNumbering.h"
+
+namespace js {
+namespace jit {
+
+struct MinimalFunc
+{
+    LifoAlloc lifo;
+    TempAllocator alloc;
+    JitCompileOptions options;
+    CompileInfo info;
+    MIRGraph graph;
+    MIRGenerator mir;
+    uint32_t numParams;
+
+    MinimalFunc()
+      : lifo(4096),
+        alloc(&lifo),
+        options(),
+        info(0, SequentialExecution),
+        graph(&alloc),
+        mir(static_cast<CompileCompartment *>(nullptr), options, &alloc, &graph,
+            &info, static_cast<const OptimizationInfo *>(nullptr)),
+        numParams(0)
+    { }
+
+    MBasicBlock *createEntryBlock()
+    {
+        MBasicBlock *block = MBasicBlock::NewAsmJS(graph, info, nullptr, MBasicBlock::NORMAL);
+        graph.addBlock(block);
+        return block;
+    }
+
+    MBasicBlock *createBlock(MBasicBlock *pred)
+    {
+        MBasicBlock *block = MBasicBlock::NewAsmJS(graph, info, pred, MBasicBlock::NORMAL);
+        graph.addBlock(block);
+        return block;
+    }
+
+    MParameter *createParameter()
+    {
+        MParameter *p = MParameter::New(alloc, numParams++, nullptr);
+        return p;
+    }
+
+    bool runGVN()
+    {
+        if (!SplitCriticalEdges(graph))
+            return false;
+        if (!RenumberBlocks(graph))
+            return false;
+        if (!BuildDominatorTree(graph))
+            return false;
+        ValueNumberer gvn(&mir, graph);
+        if (!gvn.run(ValueNumberer::DontUpdateAliasAnalysis))
+            return false;
+        return true;
+    }
+};
+
+} 
+} 
+
+#endif
