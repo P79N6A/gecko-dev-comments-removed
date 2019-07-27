eb8ad32da5f4bb2f29ad951f743280a3b6cc190e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:17:31 +0000

    Bug 1136252 - Part 1: Increase the number of instances of signaling_unittests that can be run in parallel. r=mt
    UltraBlame original commit: 4ec8bb1c0cf8e7325d316bcf1516a0ee905446ef

--- media/mtransport/test/stunserver.cpp
+++ media/mtransport/test/stunserver.cpp
@@ -250,7 +250,7 @@ TestStunServer* TestStunServer::Create() {
   }
 
   NR_SOCKET fd;
-  int tries = 10;
+  int tries = 100;
   while (tries--) {
     
     r = server->TryOpenListenSocket(&addrs[0], instance_port);
