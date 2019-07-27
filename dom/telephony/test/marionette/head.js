dd8d1d7bbde527a9a4754ee7a9e98325e7b22e7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:12:40 +0000

    Bug 978639 - Part 6: Add test case. r=hsinyi
    UltraBlame original commit: 2cdf87a14e8c45262b055bad1ba92b05f1238192

--- dom/telephony/test/marionette/head.js
+++ dom/telephony/test/marionette/head.js
@@ -1115,6 +1115,44 @@ let emulator = (function() {
 
 
 
+  function hangUpConference() {
+    log("Hangup conference.");
+
+    let deferred = Promise.defer();
+    let done = function() {
+      deferred.resolve();
+    };
+
+    let pending = ["conference.hangUp", "conference.onstatechange"];
+    let receive = function(name) {
+      receivedPending(name, pending, done);
+    };
+
+    for (let call of conference.calls) {
+      let callName = "Call (" + call.id.number + ')';
+
+      let onstatechange = callName + ".onstatechange";
+      pending.push(onstatechange);
+      check_onstatechange(call, callName, 'disconnected',
+                          receive.bind(null, onstatechange));
+    }
+
+    check_onstatechange(conference, 'conference', '', function() {
+      receive("conference.onstatechange");
+    });
+
+    conference.hangUp().then(() => {
+      receive("conference.hangUp");
+    });
+
+    return deferred.promise;
+  }
+
+  
+
+
+
+
 
 
 
@@ -1254,6 +1292,7 @@ let emulator = (function() {
   this.gResumeConference = resumeConference;
   this.gRemoveCallInConference = removeCallInConference;
   this.gHangUpCallInConference = hangUpCallInConference;
+  this.gHangUpConference = hangUpConference;
   this.gSetupConference = setupConference;
   this.gReceivedPending = receivedPending;
 }());
