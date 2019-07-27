df363db8a6f7bc96362dadbf7b695c3416b499de
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:05:59 +0000

    Bug 1129116 - Part 1 -  Add support for performing XMLHttpRequests with devtools frame-script-utils.js. r=vporof
    UltraBlame original commit: 5736d0c605dc569afe73ae8aa4fde88f548cb6a1

--- browser/devtools/shared/frame-script-utils.js
+++ browser/devtools/shared/frame-script-utils.js
@@ -3,6 +3,11 @@
 
 
 "use strict";
+const Cu = Components.utils;
+
+const { devtools } = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
+devtools.lazyImporter(this, "promise", "resource://gre/modules/Promise.jsm", "Promise");
+devtools.lazyImporter(this, "Task", "resource://gre/modules/Task.jsm", "Task");
 
 addMessageListener("devtools:test:history", function ({ data }) {
   content.history[data.direction]();
@@ -21,6 +26,79 @@ addMessageListener("devtools:test:console", function ({ data }) {
   let method = data.shift();
   content.console[method].apply(content.console, data);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function promiseXHR(data) {
+  let xhr = new content.XMLHttpRequest();
+
+  let method = data.method || "GET";
+  let url = data.url || content.location.href;
+  let body = data.body || "";
+
+  if (data.nocache) {
+    url += "?devtools-cachebust=" + Math.random();
+  }
+
+  let deferred = promise.defer();
+  xhr.addEventListener("loadend", function loadend(event) {
+    xhr.removeEventListener("loadend", loadend);
+    deferred.resolve({ status: xhr.status, response: xhr.response });
+  });
+
+  xhr.open(method, url);
+  xhr.send(body);
+  return deferred.promise;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+addMessageListener("devtools:test:xhr", Task.async(function* ({ data }) {
+  let requests = Array.isArray(data) ? data : [data];
+  let responses = [];
+
+  for (let request of requests) {
+    let response = yield promiseXHR(request);
+    responses.push(response);
+  }
+
+  sendAsyncMessage("devtools:test:xhr", responses);
+}));
 
 
 
