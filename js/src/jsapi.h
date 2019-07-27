5470a97f59198fa967dc9bc1a04aa3895b0847e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:03:16 +0000

    Bug 1058695 - Add member to nsIGlobalObject to detect it is going away. Make promises use it. r=bholley
    UltraBlame original commit: 531631f340210c8e885b0f9a34ec00e1d474a15b

--- js/src/jsapi.h
+++ js/src/jsapi.h
@@ -3948,8 +3948,10 @@ extern JS_PUBLIC_API(bool)
 Construct(JSContext* cx, JS::HandleValue fun,
           const JS::HandleValueArray& args,
           MutableHandleValue rval);
-
 } 
+
+extern JS_PUBLIC_API(bool)
+JS_CheckForInterrupt(JSContext* cx);
 
 
 
