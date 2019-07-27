20e588175f2508286d09e5530dbf4985af3e0f1d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:31:11 +0000

    Bug 1090535 - Add Windows Firewall whitelist entry. r=bbondy
    UltraBlame original commit: cd3e066846f1bee1f467feac6c66e0bf1ef3b962

---
+++ other-licenses/nsis/Contrib/liteFirewall/exdll.h
@@ -0,0 +1,97 @@
+#ifndef _EXDLL_H_
+#define _EXDLL_H_
+
+
+
+
+#define EXDLL_INIT()           {  \
+        g_stringsize=string_size; \
+        g_stacktop=stacktop;      \
+        g_variables=variables; }
+
+
+#define WM_NOTIFY_OUTER_NEXT (WM_USER+0x8)
+#define WM_NOTIFY_CUSTOM_READY (WM_USER+0xd)
+#define NOTIFY_BYE_BYE 'x'
+
+typedef struct _stack_t {
+  struct _stack_t *next;
+  TCHAR text[1]; 
+} stack_t;
+
+
+static unsigned int g_stringsize;
+static stack_t **g_stacktop;
+static TCHAR *g_variables;
+
+static int __stdcall popstring(TCHAR *str); 
+static void __stdcall pushstring(const TCHAR *str);
+
+enum
+{
+INST_0,         
+INST_1,         
+INST_2,         
+INST_3,         
+INST_4,         
+INST_5,         
+INST_6,         
+INST_7,         
+INST_8,         
+INST_9,         
+INST_R0,        
+INST_R1,        
+INST_R2,        
+INST_R3,        
+INST_R4,        
+INST_R5,        
+INST_R6,        
+INST_R7,        
+INST_R8,        
+INST_R9,        
+INST_CMDLINE,   
+INST_INSTDIR,   
+INST_OUTDIR,    
+INST_EXEDIR,    
+INST_LANG,      
+__INST_LAST
+};
+
+
+
+static int __stdcall popstring(TCHAR *str)
+{
+  stack_t *th;
+  if (!g_stacktop || !*g_stacktop) return 1;
+  th=(*g_stacktop);
+  lstrcpy(str,th->text);
+  *g_stacktop = th->next;
+  GlobalFree((HGLOBAL)th);
+  return 0;
+}
+
+static void __stdcall pushstring(const TCHAR *str)
+{
+  stack_t *th;
+  if (!g_stacktop) return;
+  th=(stack_t*)GlobalAlloc(GPTR,sizeof(stack_t)+g_stringsize*sizeof(TCHAR));
+  lstrcpyn(th->text,str,g_stringsize);
+  th->next=*g_stacktop;
+  *g_stacktop=th;
+}
+
+static TCHAR * __stdcall getuservariable(int varnum)
+{
+  if (varnum < 0 || varnum >= __INST_LAST) return NULL;
+  return g_variables+varnum*g_stringsize;
+}
+
+static void __stdcall setuservariable(int varnum, const TCHAR *var)
+{
+	if (var != NULL && varnum >= 0 && varnum < __INST_LAST) 
+		lstrcpy(g_variables + varnum*g_stringsize, var);
+}
+
+
+
+#endif
---
+++ other-licenses/nsis/Contrib/liteFirewall/liteFirewall.cpp
@@ -0,0 +1,408 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <windows.h>
+#include <tchar.h>
+#include <shlwapi.h>
+
+
+#ifdef NSIS
+#include "exdll.h"
+#endif
+
+
+#import "netfw.tlb"
+#include <netfw.h>
+using namespace NetFwTypeLib;
+
+
+#pragma comment( lib, "ole32.lib" )
+#pragma comment( lib, "oleaut32.lib" )
+
+
+#ifdef NSIS
+HINSTANCE g_hInstance;
+#endif
+
+HRESULT     WFCOMInitialize(INetFwPolicy2** ppNetFwPolicy2);
+
+HRESULT AddRule(LPCTSTR ExceptionName, LPCTSTR ProcessPath)
+{
+	HRESULT result = CoInitialize(NULL);
+	if (FAILED(result))
+		return result;
+	result = REGDB_E_CLASSNOTREG;
+
+	HRESULT hrComInit = S_OK;
+	HRESULT hr = S_OK;
+
+	INetFwPolicy2 *pNetFwPolicy2 = NULL;
+	INetFwRules *pFwRules = NULL;
+	INetFwRule *pFwRule = NULL;
+
+	INetFwRule *pFwRuleExisting = NULL;
+
+
+
+
+	BSTR bstrRuleName = SysAllocString(ExceptionName);
+	BSTR bstrApplicationName = SysAllocString(ProcessPath);
+	BSTR bstrRuleInterfaceType = SysAllocString(L"All");
+
+	
+	hrComInit = CoInitializeEx(
+		0,
+		COINIT_APARTMENTTHREADED
+		);
+
+	
+	
+	
+	if (hrComInit != RPC_E_CHANGED_MODE)
+	{
+		if (FAILED(hrComInit))
+		{
+			printf("CoInitializeEx failed: 0x%08lx\n", hrComInit);
+			goto Cleanup;
+		}
+	}
+
+	
+	hr = WFCOMInitialize(&pNetFwPolicy2);
+	if (FAILED(hr))
+	{
+		try
+		{
+			INetFwMgrPtr fwMgr(L"HNetCfg.FwMgr");
+			if (fwMgr)
+			{
+				INetFwAuthorizedApplicationPtr app(L"HNetCfg.FwAuthorizedApplication");
+				if (app)
+				{
+					app->ProcessImageFileName = ProcessPath;
+					app->Name = ExceptionName;
+					app->Scope = NetFwTypeLib::NET_FW_SCOPE_ALL;
+					app->IpVersion = NetFwTypeLib::NET_FW_IP_VERSION_ANY;
+					app->Enabled = VARIANT_TRUE;
+					fwMgr->LocalPolicy->CurrentProfile->AuthorizedApplications->Add(app);
+				}
+			}
+		}
+		catch (_com_error& e)
+		{
+			printf("%s", e.Error());
+		}
+		goto Cleanup;
+	}
+
+	
+	hr = pNetFwPolicy2->get_Rules(&pFwRules);
+	if (FAILED(hr))
+	{
+		printf("get_Rules failed: 0x%08lx\n", hr);
+		goto Cleanup;
+	}
+
+
+	
+	hr = pFwRules->Item(bstrRuleName, &pFwRuleExisting);
+	
+	if (pFwRuleExisting != NULL)
+	{
+		pFwRuleExisting->Release();
+	}
+	if (SUCCEEDED(hr))
+	{
+		printf("Firewall profile already exists\n");
+		goto Cleanup;
+	}
+
+	
+
+
+
+
+
+
+
+	
+	
+
+
+
+
+
+
+	
+	hr = CoCreateInstance(
+		__uuidof(NetFwRule),
+		NULL,
+		CLSCTX_INPROC_SERVER,
+		__uuidof(INetFwRule),
+		(void**)&pFwRule);
+	if (FAILED(hr))
+	{
+		printf("CoCreateInstance for Firewall Rule failed: 0x%08lx\n", hr);
+		goto Cleanup;
+	}
+
+    
+	pFwRule->put_Name(bstrRuleName);
+	pFwRule->put_Protocol(NetFwTypeLib::NET_FW_IP_PROTOCOL_TCP);
+	pFwRule->put_InterfaceTypes(bstrRuleInterfaceType);
+	pFwRule->put_Profiles(NET_FW_PROFILE2_PRIVATE);
+	pFwRule->put_Action(NET_FW_ACTION_ALLOW);
+	pFwRule->put_Enabled(VARIANT_TRUE);
+
+	pFwRule->put_ApplicationName(bstrApplicationName);
+	
+	hr = pFwRules->Add(pFwRule);
+	if (FAILED(hr))
+	{
+		printf("Firewall Rule Add failed: 0x%08lx\n", hr);
+		goto Cleanup;
+	}
+
+	pFwRule->Release();
+
+
+	
+	hr = CoCreateInstance(
+		__uuidof(NetFwRule),
+		NULL,
+		CLSCTX_INPROC_SERVER,
+		__uuidof(INetFwRule),
+		(void**)&pFwRule);
+	if (FAILED(hr))
+	{
+		printf("CoCreateInstance for Firewall Rule failed: 0x%08lx\n", hr);
+		goto Cleanup;
+	}
+
+	
+	pFwRule->put_Name(bstrRuleName);
+
+
+	pFwRule->put_Protocol(NetFwTypeLib::NET_FW_IP_PROTOCOL_UDP);
+
+	pFwRule->put_InterfaceTypes(bstrRuleInterfaceType);
+
+
+	pFwRule->put_Profiles(NET_FW_PROFILE2_PRIVATE);
+
+	pFwRule->put_Action(NET_FW_ACTION_ALLOW);
+	pFwRule->put_Enabled(VARIANT_TRUE);
+
+	pFwRule->put_ApplicationName(bstrApplicationName);
+	
+	hr = pFwRules->Add(pFwRule);
+	if (FAILED(hr))
+	{
+		printf("Firewall Rule Add failed: 0x%08lx\n", hr);
+		goto Cleanup;
+	}
+
+Cleanup:
+
+	
+	SysFreeString(bstrRuleName);
+	SysFreeString(bstrApplicationName);
+	SysFreeString(bstrRuleInterfaceType);
+
+	
+	if (pFwRule != NULL)
+	{
+		pFwRule->Release();
+	}
+
+	
+	if (pFwRules != NULL)
+	{
+		pFwRules->Release();
+	}
+
+	
+	if (pNetFwPolicy2 != NULL)
+	{
+		pNetFwPolicy2->Release();
+	}
+
+	CoUninitialize();
+    return 0;
+}
+
+HRESULT RemoveRule(LPCTSTR ExceptionName, LPCTSTR ProcessPath)
+{
+    HRESULT result = CoInitialize(NULL);
+	if (FAILED(result))
+        return result;
+	try
+	{
+		INetFwMgrPtr fwMgr(L"HNetCfg.FwMgr");
+        if (fwMgr)
+        {
+		    fwMgr->LocalPolicy->CurrentProfile->AuthorizedApplications->Remove(ProcessPath);
+            result = S_OK;
+        }
+	}
+	catch (_com_error& e)
+	{
+		e;
+	}
+	HRESULT hrComInit = S_OK;
+	HRESULT hr = S_OK;
+
+	INetFwPolicy2 *pNetFwPolicy2 = NULL;
+	INetFwRules *pFwRules = NULL;
+
+
+
+
+
+	BSTR bstrRuleName = SysAllocString(ExceptionName);
+
+	
+	hr = WFCOMInitialize(&pNetFwPolicy2);
+	if (FAILED(hr))
+	{
+		goto Cleanup;
+	}
+
+	
+	hr = pNetFwPolicy2->get_Rules(&pFwRules);
+	if (FAILED(hr))
+	{
+		printf("get_Rules failed: 0x%08lx\n", hr);
+		goto Cleanup;
+	}
+
+
+	
+
+
+
+
+
+
+
+	
+	
+
+
+
+
+
+
+
+	
+	hr = pFwRules->Remove(bstrRuleName);
+	if (FAILED(hr))
+	{
+		printf("Firewall Rule Remove failed: 0x%08lx\n", hr);
+		goto Cleanup;
+	}
+
+Cleanup:
+
+	
+	SysFreeString(bstrRuleName);
+
+	
+	if (pFwRules != NULL)
+	{
+		pFwRules->Release();
+	}
+
+	
+	if (pNetFwPolicy2 != NULL)
+	{
+		pNetFwPolicy2->Release();
+	}
+
+	CoUninitialize();
+	return 0;
+}
+
+
+#ifdef NSIS
+extern "C" void __declspec(dllexport) AddRule(HWND hwndParent, int string_size, 
+                                      TCHAR *variables, stack_t **stacktop)
+{
+	EXDLL_INIT();
+	
+	TCHAR ExceptionName[256], ProcessPath[MAX_PATH];
+    popstring(ProcessPath);
+    popstring(ExceptionName);
+    HRESULT result = AddRule(ExceptionName, ProcessPath);
+	
+    TCHAR intBuffer[16];
+	wsprintf(intBuffer, _T("%d"), result);
+	pushstring(intBuffer);
+}
+
+extern "C" void __declspec(dllexport) RemoveRule(HWND hwndParent, int string_size, 
+                                      TCHAR *variables, stack_t **stacktop)
+{
+	EXDLL_INIT();
+	
+	TCHAR ExceptionName[256], ProcessPath[MAX_PATH];
+    popstring(ProcessPath);
+	popstring(ExceptionName);
+    HRESULT result = RemoveRule(ExceptionName, ProcessPath);
+	
+    TCHAR intBuffer[16];
+	wsprintf(intBuffer, _T("%d"), result);
+	pushstring(intBuffer);
+}
+
+extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD, LPVOID)
+{
+	g_hInstance = hInstance;
+	return TRUE;
+}
+#endif
+
+
+
+
+HRESULT WFCOMInitialize(INetFwPolicy2** ppNetFwPolicy2)
+{
+	HRESULT hr = S_OK;
+
+	hr = CoCreateInstance(
+		__uuidof(NetFwPolicy2), 
+		NULL, 
+		CLSCTX_INPROC_SERVER, 
+		__uuidof(INetFwPolicy2), 
+		(void**)ppNetFwPolicy2);
+
+	if (FAILED(hr))
+	{
+		printf("CoCreateInstance for INetFwPolicy2 failed: 0x%08lx\n", hr);
+		goto Cleanup;        
+	}
+
+Cleanup:
+	return hr;
+}
