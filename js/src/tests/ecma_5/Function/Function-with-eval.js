4c8d2021b14c7a1579a77f7f97465b571d251ee2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:59:22 +0000

    Bug 915805 - Don't treat unbound names in Function() code as globals when the Function() code contains a direct eval or similarly-extensible scope. r=jorendorff
    UltraBlame original commit: f08884c972231f6a0bf38dbd3758bbb86966b2b6

---
+++ js/src/tests/ecma_5/Function/Function-with-eval.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+assertEq(new Function(
+            "eval('var foo = 915805');" +
+            "return foo;"
+         )(),
+         915805);
+
+assertEq(new Function(
+            "eval('function foo() {" +
+                      "return 915805;" +
+                  "}');" +
+            "return foo;"
+         )()(),
+         915805);
+
+
+
+if (typeof reportCompare === "function")
+    reportCompare(true, true);
+
+print("Tests complete");
