e0ac90150618f507f790cd26a67957023fbae94f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:41:59 +0000

    Bug 967325 - Implement GeckoRequest tests. r=jchen,mcomella
    UltraBlame original commit: 02870060541b933ea2ac207a7e6007e44de6df2e

--- mobile/android/base/util/GeckoRequest.java
+++ mobile/android/base/util/GeckoRequest.java
@@ -5,6 +5,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import org.mozilla.gecko.mozglue.RobocopTarget;
+
 import android.util.Log;
 
 public abstract class GeckoRequest {
@@ -24,6 +26,7 @@ public abstract class GeckoRequest {
 
 
 
+    @RobocopTarget
     public GeckoRequest(String name, Object data) {
         this.name = name;
         final JSONObject message = new JSONObject();
@@ -68,6 +71,7 @@ public abstract class GeckoRequest {
 
 
 
+    @RobocopTarget
     public abstract void onResponse(NativeJSObject nativeJSObject);
 
     
@@ -80,6 +84,7 @@ public abstract class GeckoRequest {
 
 
 
+    @RobocopTarget
     public void onError() {
         throw new RuntimeException("Unhandled error for GeckoRequest: " + name);
     }
