100326ca6951b98a4056b21b9897164a51fd9daf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:37:47 +0000

    Bug 1163851 - Actually respect the InHandling parameter in the parser.  |pc->parsingForInit| is now unused except for testing; it'll be removed once this has stuck a bit, as this change *does* change semantics in some edge cases.  r=efaust
    UltraBlame original commit: 64722e4b3310a2dc632e37b1403db7ebd878b864

---
+++ js/src/tests/ecma_6/Statements/arrow-function-in-for-statement-head.js
@@ -0,0 +1,33 @@
+
+
+
+
+var gTestfile = "arrow-function-in-for-statement-head.js";
+var BUGNUMBER = 1163851;
+var summary =
+  "|for (x => 0 in 1;;) break;| must be a syntax error per ES6, not an " +
+  "elaborate nop";
+
+print(BUGNUMBER + ": " + summary);
+
+
+
+
+
+try
+{
+  Function("for (x => 0 in 1;;) break;");
+  throw new Error("didn't throw");
+}
+catch (e)
+{
+  assertEq(e instanceof SyntaxError, true,
+           "expected syntax error, got " + e);
+}
+
+
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
+
+print("Tests complete");
