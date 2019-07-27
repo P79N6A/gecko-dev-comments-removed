37fb2a26e7525fb6b26066d141d39d826c70e05b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:26:38 +0000

    Bug 1152041 - Do not try to reconnect if we are offline. r=mayhemer
    UltraBlame original commit: 69fcbc1da823e8d9edd471b6a86ac84405ed24d4

--- netwerk/base/nsSocketTransport2.cpp
+++ netwerk/base/nsSocketTransport2.cpp
@@ -1922,7 +1922,8 @@ nsSocketTransport::OnSocketDetached(PRFileDesc *fd)
         }
     }
 
-    if (RecoverFromError())
+    
+    if (!gIOService->IsOffline() && RecoverFromError())
         mCondition = NS_OK;
     else {
         mState = STATE_CLOSED;
