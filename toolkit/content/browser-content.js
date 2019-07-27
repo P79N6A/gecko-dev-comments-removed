7092897bfb1f5c4616534e373af4c3dc601b7170
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:20:14 +0000

    Bug 1068087: Add a simple mechanism for content pages to communicate with chrome. r=mconley
    UltraBlame original commit: 6dc164972d9a9f9bf50dd91f5c257ecd1f282f49

--- toolkit/content/browser-content.js
+++ toolkit/content/browser-content.js
@@ -10,6 +10,11 @@ let Cr = Components.results;
 
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/RemotePageManager.jsm");
+
+
+
+new PageListener(this);
 
 var global = this;
 
