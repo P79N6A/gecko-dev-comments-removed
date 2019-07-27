2ceb3b096196a7d70953cbc7551e23e3c4408faa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:58:13 +0000

    Bug 1152370 part 1 - Add android aosp VirtualDisplaySurface r=mwu
    UltraBlame original commit: 6869f1714918f1694de2332dd622c7bff354db9f

---
+++ widget/gonk/libdisplay/VirtualDisplaySurface.h
@@ -0,0 +1,244 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef ANDROID_SF_VIRTUAL_DISPLAY_SURFACE_H
+#define ANDROID_SF_VIRTUAL_DISPLAY_SURFACE_H
+
+#include <gui/ConsumerBase.h>
+#include <gui/IGraphicBufferProducer.h>
+
+#include "DisplaySurface.h"
+
+
+namespace android {
+
+
+class HWComposer;
+class IProducerListener;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class VirtualDisplaySurface : public DisplaySurface,
+                              public BnGraphicBufferProducer,
+                              private ConsumerBase {
+public:
+    VirtualDisplaySurface(HWComposer& hwc, int32_t dispId,
+            const sp<IGraphicBufferProducer>& sink,
+            const sp<IGraphicBufferProducer>& bqProducer,
+            const sp<IGraphicBufferConsumer>& bqConsumer,
+            const String8& name);
+
+    
+    
+    
+    virtual status_t beginFrame(bool mustRecompose);
+    virtual status_t prepareFrame(CompositionType compositionType);
+    virtual status_t compositionComplete();
+    virtual status_t advanceFrame();
+    virtual void onFrameCommitted();
+    virtual void dump(String8& result) const;
+    virtual void resizeBuffers(const uint32_t w, const uint32_t h);
+
+private:
+    enum Source {SOURCE_SINK = 0, SOURCE_SCRATCH = 1};
+
+    virtual ~VirtualDisplaySurface();
+
+    
+    
+    
+    virtual status_t requestBuffer(int pslot, sp<GraphicBuffer>* outBuf);
+    virtual status_t setBufferCount(int bufferCount);
+    virtual status_t dequeueBuffer(int* pslot, sp<Fence>* fence, bool async,
+            uint32_t w, uint32_t h, uint32_t format, uint32_t usage);
+    virtual status_t detachBuffer(int slot);
+    virtual status_t detachNextBuffer(sp<GraphicBuffer>* outBuffer,
+            sp<Fence>* outFence);
+    virtual status_t attachBuffer(int* slot, const sp<GraphicBuffer>& buffer);
+    virtual status_t queueBuffer(int pslot,
+            const QueueBufferInput& input, QueueBufferOutput* output);
+    virtual void cancelBuffer(int pslot, const sp<Fence>& fence);
+    virtual int query(int what, int* value);
+    virtual status_t connect(const sp<IProducerListener>& listener,
+            int api, bool producerControlledByApp, QueueBufferOutput* output);
+    virtual status_t disconnect(int api);
+    virtual status_t setSidebandStream(const sp<NativeHandle>& stream);
+    virtual void allocateBuffers(bool async, uint32_t width, uint32_t height,
+            uint32_t format, uint32_t usage);
+
+    
+    
+    
+    static Source fbSourceForCompositionType(CompositionType type);
+    status_t dequeueBuffer(Source source, uint32_t format, uint32_t usage,
+            int* sslot, sp<Fence>* fence);
+    void updateQueueBufferOutput(const QueueBufferOutput& qbo);
+    void resetPerFrameState();
+    status_t refreshOutputBuffer();
+
+    
+    
+    
+    
+    
+    
+    static int mapSource2ProducerSlot(Source source, int sslot);
+    static int mapProducer2SourceSlot(Source source, int pslot);
+
+    
+    
+    
+    HWComposer& mHwc;
+    const int32_t mDisplayId;
+    const String8 mDisplayName;
+    sp<IGraphicBufferProducer> mSource[2]; 
+    uint32_t mDefaultOutputFormat;
+
+    
+    
+    
+
+    
+    
+    
+    
+    uint32_t mOutputFormat;
+    uint32_t mOutputUsage;
+
+    
+    
+    
+    
+    
+    
+    uint64_t mProducerSlotSource;
+    sp<GraphicBuffer> mProducerBuffers[BufferQueue::NUM_BUFFER_SLOTS];
+
+    
+    
+    
+    QueueBufferOutput mQueueBufferOutput;
+
+    
+    
+    uint32_t mSinkBufferWidth, mSinkBufferHeight;
+
+    
+    
+    
+
+    
+    
+    CompositionType mCompositionType;
+
+    
+    
+    sp<Fence> mFbFence;
+
+    
+    
+    sp<Fence> mOutputFence;
+
+    
+    
+    int mFbProducerSlot;
+    int mOutputProducerSlot;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    enum DbgState {
+        
+        DBG_STATE_IDLE,
+        
+        DBG_STATE_BEGUN,
+        
+        
+        DBG_STATE_PREPARED,
+        
+        DBG_STATE_GLES,
+        
+        DBG_STATE_GLES_DONE,
+        
+        DBG_STATE_HWC,
+    };
+    DbgState mDbgState;
+    CompositionType mDbgLastCompositionType;
+
+    const char* dbgStateStr() const;
+    static const char* dbgSourceStr(Source s);
+
+    bool mMustRecompose;
+};
+
+
+} 
+
+
+#endif
+
