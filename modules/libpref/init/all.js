fd60e01f1bf6d3c24405ada043c5e749fd073ee3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:26:58 +0000

    Bug 1135018 - Make devices.js use a CDN. r=ochameau
    UltraBlame original commit: d462c69631569305ef3046275dc1d232cf04107f

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -838,6 +838,9 @@ pref("devtools.remote.wifi.visible", false);
 pref("devtools.remote.tls-handshake-timeout", 10000);
 
 
+pref("devtools.devices.url", "https://code.cdn.mozilla.net/devices/devices.json");
+
+
 pref("view_source.syntax_highlight", true);
 pref("view_source.wrap_long_lines", false);
 pref("view_source.editor.external", false);
