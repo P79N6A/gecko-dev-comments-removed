daa997f304b8a458e1195eecee8564b44909d353
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:57:00 +0000

    Bug 1024774 - Part 9: Deserialize heap snapshots; r=jimb
    UltraBlame original commit: ad71c9a3446bf404ad17c4058c21ac92406121da

---
+++ toolkit/devtools/server/DeserializedNode.cpp
@@ -0,0 +1,109 @@
+
+
+
+
+
+#include "mozilla/devtools/DeserializedNode.h"
+
+namespace mozilla {
+namespace devtools {
+
+DeserializedEdge::DeserializedEdge()
+  : referent(0)
+  , name(nullptr)
+{ }
+
+DeserializedEdge::DeserializedEdge(DeserializedEdge &&rhs)
+{
+  referent = rhs.referent;
+  name = rhs.name;
+}
+
+DeserializedEdge &DeserializedEdge::operator=(DeserializedEdge &&rhs)
+{
+  MOZ_ASSERT(&rhs != this);
+  this->~DeserializedEdge();
+  new(this) DeserializedEdge(Move(rhs));
+  return *this;
+}
+
+bool
+DeserializedEdge::init(const protobuf::Edge &edge, HeapSnapshot &owner)
+{
+  
+  
+  
+  if (!edge.has_referent())
+    return false;
+  referent = edge.referent();
+
+  if (edge.has_name()) {
+    const char16_t* duplicateEdgeName = reinterpret_cast<const char16_t*>(edge.name().c_str());
+    name = owner.borrowUniqueString(duplicateEdgeName, edge.name().length() / sizeof(char16_t));
+    if (!name)
+      return false;
+  }
+
+  return true;
+}
+
+ UniquePtr<DeserializedNode>
+DeserializedNode::Create(const protobuf::Node &node, HeapSnapshot &owner)
+{
+  if (!node.has_id())
+    return nullptr;
+  NodeId id = node.id();
+
+  if (!node.has_typename_())
+    return nullptr;
+
+  const char16_t* duplicatedTypeName = reinterpret_cast<const char16_t*>(node.typename_().c_str());
+  const char16_t* uniqueTypeName = owner.borrowUniqueString(duplicatedTypeName,
+                                                            node.typename_().length() / sizeof(char16_t));
+  if (!uniqueTypeName)
+    return nullptr;
+
+  auto edgesLength = node.edges_size();
+  EdgeVector edges;
+  if (!edges.reserve(edgesLength))
+    return nullptr;
+  for (decltype(edgesLength) i = 0; i < edgesLength; i++) {
+    DeserializedEdge edge;
+    if (!edge.init(node.edges(i), owner))
+      return nullptr;
+    edges.infallibleAppend(Move(edge));
+  }
+
+  if (!node.has_size())
+    return nullptr;
+  uint64_t size = node.size();
+
+  return MakeUnique<DeserializedNode>(id,
+                                      uniqueTypeName,
+                                      size,
+                                      Move(edges),
+                                      owner);
+}
+
+DeserializedNode::DeserializedNode(NodeId id,
+                                   const char16_t *typeName,
+                                   uint64_t size,
+                                   EdgeVector &&edges,
+                                   HeapSnapshot &owner)
+  : id(id)
+  , typeName(typeName)
+  , size(size)
+  , edges(Move(edges))
+  , owner(&owner)
+{ }
+
+DeserializedNode &
+DeserializedNode::getEdgeReferent(const DeserializedEdge &edge)
+{
+  auto ptr = owner->nodes.lookup(edge.referent);
+  MOZ_ASSERT(ptr);
+  return *ptr->value();
+}
+
+} 
+} 
