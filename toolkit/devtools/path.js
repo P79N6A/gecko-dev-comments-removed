1e3f89e0b40645c4fb38c1fcb6030554f32c4cb1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:21:13 +0000

    Bug 1107541 - Display eval sources with a sourceURL pragma correctly in the debugger. r=fitzgen
    UltraBlame original commit: f04adec1982d8898b6e87bfd2bfb13885acfffd8

---
+++ toolkit/devtools/path.js
@@ -0,0 +1,38 @@
+
+
+
+
+"use strict";
+
+const Services = require("Services");
+
+
+
+
+exports.dirname = path => {
+  return Services.io.newURI(
+    ".", null, Services.io.newURI(path, null, null)).spec;
+}
+
+
+
+
+
+exports.joinURI = (initialPath, ...paths) => {
+  let uri;
+
+  try {
+    uri = Services.io.newURI(initialPath, null, null);
+  }
+  catch(e) {
+    return;
+  }
+
+  for(let path of paths) {
+    if(path) {
+      uri = Services.io.newURI(path, null, uri);
+    }
+  }
+
+  return uri.spec;
+}
--- toolkit/devtools/server/actors/script.js
+++ toolkit/devtools/server/actors/script.js
@@ -12,6 +12,7 @@ const { ActorPool, getOffsetColumn } = require("devtools/server/actors/common");
 const { DebuggerServer } = require("devtools/server/main");
 const DevToolsUtils = require("devtools/toolkit/DevToolsUtils");
 const { dbg_assert, dumpn, update, fetch } = DevToolsUtils;
+const { dirname, joinURI } = require("devtools/toolkit/path");
 const { SourceMapConsumer, SourceMapGenerator } = require("source-map");
 const promise = require("promise");
 const PromiseDebugging = require("PromiseDebugging");
@@ -5373,7 +5374,9 @@ ThreadSources.prototype = {
 
 
   _sourceForScript: function (aScript) {
-    let url = getSourceURL(aScript.source);
+    
+    
+    let url = isEvalSource(aScript.source) ? null : aScript.source.url;
     let spec = {
       source: aScript.source
     };
@@ -5864,17 +5867,30 @@ function getSymbolName(symbol) {
   return name || undefined;
 }
 
-function getSourceURL(source) {
+function isEvalSource(source) {
   let introType = source.introductionType;
   
   
-  
-  
-  if (introType === 'eval' ||
-      introType === 'Function' ||
-      introType === 'eventHandler' ||
-      introType === 'setTimeout' ||
-      introType === 'setInterval') {
+  return (introType === 'eval' ||
+          introType === 'Function' ||
+          introType === 'eventHandler' ||
+          introType === 'setTimeout' ||
+          introType === 'setInterval');
+}
+
+function getSourceURL(source) {
+  if(isEvalSource(source)) {
+    
+    
+    
+
+    if(source.displayURL &&
+       source.introductionScript &&
+       !isEvalSource(source.introductionScript.source)) {
+      return joinURI(dirname(source.introductionScript.source.url),
+                     source.displayURL);
+    }
+
     return source.displayURL;
   }
   return source.url;
