328b645d0d491c4ef1747f34eb878d2c93e96a75
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:30:04 +0000

    Bug 1183682 - Remove the default browser A/B test for Windows 10 (keep Settings approach). r=gijs
    UltraBlame original commit: 4fc84033bdcf7c09734350cb970dd5e6504154b4

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -271,7 +271,6 @@ pref("general.autoScroll", true);
 pref("browser.shell.checkDefaultBrowser", true);
 pref("browser.shell.shortcutFavicons",true);
 pref("browser.shell.mostRecentDateSetAsDefault", "");
-pref("browser.shell.windows10DefaultBrowserABTest", -1);
 
 
 
