836559613c0976b433140dfb8164b3bf3bb1dcd8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:29:51 +0000

    Bug 1113723: play a sound when someone joins a room. r=MattN
    UltraBlame original commit: 3d3e53c0b21ad1c12c61f251d9f87828ac5f8ad8

--- browser/base/content/browser-loop.js
+++ browser/base/content/browser-loop.js
@@ -237,8 +237,9 @@ XPCOMUtils.defineLazyModuleGetter(this, "PanelFrame", "resource:///modules/Panel
       if (options.sound) {
         
         notificationOptions.mozbehavior = {
-          soundFile: `chrome://browser/content/loop/shared/sounds/${options.sound}.ogg`
+          soundFile: ""
         };
+        this.playSound(options.sound);
       }
 
       let notification = new window.Notification(options.title, notificationOptions);
