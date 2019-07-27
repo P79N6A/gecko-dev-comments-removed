bcecf315e91a51e382e113d5eb170b7fc7fe5229
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:47:23 +0000

    Bug 1088625, part 4 - Remove the includes from nsRenderingContext.h that are no longer necessary
    UltraBlame original commit: b7050902ed0311a331b3886f1ec3e6aa1a97f14f

--- gfx/src/nsRenderingContext.h
+++ gfx/src/nsRenderingContext.h
@@ -6,23 +6,16 @@
 #ifndef NSRENDERINGCONTEXT__H__
 #define NSRENDERINGCONTEXT__H__
 
-#include <stdint.h>                     
-#include <sys/types.h>                  
-#include "gfxContext.h"                 
-#include "mozilla/Assertions.h"         
-#include "mozilla/gfx/2D.h"
-#include "nsAutoPtr.h"                  
-#include "nsBoundingMetrics.h"          
-#include "nsColor.h"                    
-#include "nsCoord.h"                    
-#include "nsFontMetrics.h"              
-#include "nsISupports.h"                
-#include "nsString.h"               
-#include "nscore.h"                     
+#include "gfxContext.h"
+#include "mozilla/Attributes.h"
+#include "nsISupportsImpl.h"
+#include "nsRefPtr.h"
 
-class nsIntRegion;
-struct nsPoint;
-struct nsRect;
+namespace mozilla {
+namespace gfx {
+class DrawTarget;
+}
+}
 
 class nsRenderingContext MOZ_FINAL
 {
