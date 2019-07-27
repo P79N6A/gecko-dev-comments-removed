14c1e0ba87e710669e2d68fea4b13cc306cbb806
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:52:07 +0000

    Bug 1087968 - Part 2: Rewrite test_stk_* with Promise. r=hsinyi
    UltraBlame original commit: 28ed9a1001302eb08766b6062209d19837d98185

--- dom/icc/tests/marionette/test_stk_refresh.js
+++ dom/icc/tests/marionette/test_stk_refresh.js
@@ -1,25 +1,39 @@
 
 
 
-MARIONETTE_HEAD_JS = "stk_helper.js";
+MARIONETTE_TIMEOUT = 60000;
+MARIONETTE_HEAD_JS = "head.js";
 
-function testRefresh(command, expect) {
-  log("STK CMD " + JSON.stringify(command));
-  is(command.typeOfCommand, iccManager.STK_CMD_REFRESH, expect.name);
-  is(command.commandQualifier, expect.commandQualifier, expect.name);
-
-  runNextTest();
-}
-
-let tests = [
+const TEST_DATA = [
   {command: "d0108103010101820281829205013f002fe2",
-   func: testRefresh,
-   expect: {name: "refresh_cmd_1",
-            commandQualifier: 0x01}},
+   expect: {commandQualifier: 0x01}},
   {command: "d009810301010482028182",
-   func: testRefresh,
-   expect: {name: "refresh_cmd_2",
-            commandQualifier: 0x04}}
+   expect: {commandQualifier: 0x04}}
 ];
 
-runNextTest();
+function testRefresh(aCommand, aExpect) {
+  is(aCommand.typeOfCommand, MozIccManager.STK_CMD_REFRESH, "typeOfCommand");
+  is(aCommand.commandQualifier, aExpect.commandQualifier, "commandQualifier");
+}
+
+
+startTestCommon(function() {
+  let icc = getMozIcc();
+  let promise = Promise.resolve();
+  for (let i = 0; i < TEST_DATA.length; i++) {
+    let data = TEST_DATA[i];
+    promise = promise.then(() => {
+      log("refresh_cmd: " + data.command);
+
+      let promises = [];
+      
+      promises.push(waitForTargetEvent(icc, "stkcommand")
+        .then((aEvent) => testRefresh(aEvent.command, data.expect)));
+      
+      promises.push(sendEmulatorStkPdu(data.command));
+
+      return Promise.all(promises);
+    });
+  }
+  return promise;
+});
