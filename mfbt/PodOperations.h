628e9687c891c533fd319d29265d818e2a375f2c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:50:14 +0000

    Bug 1153382 - Make poison values more toxic when used as a Value; r=sfink
    UltraBlame original commit: 42ed856b37d93aedc448585c50fc89dcb063be10

--- mfbt/PodOperations.h
+++ mfbt/PodOperations.h
@@ -91,6 +91,18 @@ PodAssign(T* aDst, const T* aSrc)
 
 
 
+template<typename T>
+static MOZ_ALWAYS_INLINE void
+PodSet(T* aDst, T aSrc, size_t aNElem)
+{
+  for (const T* dstend = aDst + aNElem; aDst < dstend; aDst++) {
+    *aDst = aSrc;
+  }
+}
+
+
+
+
 
 template<typename T>
 static MOZ_ALWAYS_INLINE void
