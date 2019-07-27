37c4d5c5b3666c2a7b40f30515f49ca8372f2c20
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:04:13 +0000

    Backed out changesets c61c91ea40fe and 8ac5fed37830 (bug 1071769) for causing frequent Android reftest failures.
    UltraBlame original commit: 1c77f152f2805750f47835692e29aaa440b14c18

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3936,10 +3936,6 @@ pref("layers.enable-tiles", true);
 pref("layers.tiled-drawtarget.enabled", true);
 #endif
 
-#ifdef MOZ_WIDGET_GONK
-pref("layers.tiled-drawtarget.enabled", true);
-#endif
-
 
 #ifdef ANDROID
 pref("layers.offmainthreadcomposition.enabled", true);
