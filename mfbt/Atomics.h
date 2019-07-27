5db0d5a352721c401e974217c268bba188857566
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:45:50 +0000

    Bug 1073709 - Disable C++11 <atomic> when using GCC with libc++. r=waldo
    UltraBlame original commit: 7a96fbcf0c020568ab44b72e4f272bfb1ef38d10

--- mfbt/Atomics.h
+++ mfbt/Atomics.h
@@ -35,9 +35,11 @@
 
 
 
+
+
 #  if MOZ_USING_LIBSTDCXX && MOZ_LIBSTDCXX_VERSION_AT_LEAST(4, 7, 0)
 #    define MOZ_HAVE_CXX11_ATOMICS
-#  elif MOZ_USING_LIBCXX
+#  elif MOZ_USING_LIBCXX && defined(__clang__)
 #    define MOZ_HAVE_CXX11_ATOMICS
 #  endif
 
