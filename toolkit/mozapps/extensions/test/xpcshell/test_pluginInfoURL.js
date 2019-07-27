7e1963e6ba44186db53664c8d99f53364c00fc0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:02:45 +0000

    Bug 1038145 - Add XPCSHELL tests for getPluginInfoURL in the blocklist service. r=irving
    UltraBlame original commit: 579a8ed2fe3b43de0660c8a171f843290ea01876

---
+++ toolkit/mozapps/extensions/test/xpcshell/test_pluginInfoURL.js
@@ -0,0 +1,68 @@
+
+
+
+
+
+const Ci = Components.interfaces;
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+
+
+
+function MockPlugin(name, version, enabledState) {
+  this.name = name;
+  this.version = version;
+  this.enabledState = enabledState;
+}
+
+MockPlugin.prototype = {
+  get blocklisted() {
+    let bls = Services.blocklist;
+    return bls.getPluginBlocklistState(this) == bls.STATE_BLOCKED;
+  },
+
+  get disabled() {
+    return this.enabledState == Ci.nsIPluginTag.STATE_DISABLED;
+  }
+};
+
+
+const PLUGINS = [
+  new MockPlugin('test_with_infoURL', '5', Ci.nsIPluginTag.STATE_ENABLED),
+  new MockPlugin('test_no_infoURL', '5', Ci.nsIPluginTag.STATE_ENABLED)
+];
+
+
+
+
+
+function run_test() {
+  copyBlocklistToProfile(do_get_file('data/pluginInfoURL_block.xml'));
+
+  createAppInfo('xpcshell@tests.mozilla.org', 'XPCShell', '3', '8');
+  startupManager();
+
+  run_next_test();
+}
+
+
+
+
+
+add_task(function* test_infoURL() {
+  
+  
+  let testInfoURL = 'http://test.url.com/';
+
+  Assert.strictEqual(Services.blocklist.getPluginInfoURL(PLUGINS[0]),
+    testInfoURL, 'Plugin info urls should match');
+});
+
+
+
+
+
+add_task(function* test_infoURL_missing() {
+  Assert.strictEqual(Services.blocklist.getPluginInfoURL(PLUGINS[1]), null,
+    'Should be null when no infoURL tag is available.');
+});
