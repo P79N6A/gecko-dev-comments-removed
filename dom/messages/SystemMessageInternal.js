b0c6c7bce032a66c2da4e913c40385f84f0bb689
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:42:36 +0000

    Backed out changeset 3200ed3305db (bug 1014023) for causing bug 1051164.
    UltraBlame original commit: ddcf4f4af7d932e67127711b37040a485151d815

--- dom/messages/SystemMessageInternal.js
+++ dom/messages/SystemMessageInternal.js
@@ -192,48 +192,31 @@ SystemMessageInternal.prototype = {
     
     let messageID = gUUIDGenerator.generateUUID().toString();
 
-    let manifestURL = aManifestURI.spec;
-    let pageURLs = [];
-    if (aPageURI) {
-      pageURLs.push(aPageURI.spec);
-    } else {
-      
-      
-      for (let i = 0; i < this._pages.length; i++) {
-        let page = this._pages[i];
-        if (page.type === aType && page.manifestURL === manifestURL) {
-          pageURLs.push(page.pageURL);
-        }
-      }
+    debug("Sending " + aType + " " + JSON.stringify(aMessage) +
+      " for " + aPageURI.spec + " @ " + aManifestURI.spec +
+      '; extra: ' + JSON.stringify(aExtra));
+
+    let result = this._sendMessageCommon(aType,
+                                         aMessage,
+                                         messageID,
+                                         aPageURI.spec,
+                                         aManifestURI.spec,
+                                         aExtra);
+    debug("Returned status of sending message: " + result);
+
+    
+    
+    if (result === MSG_SENT_FAILURE_PERM_DENIED) {
+      return;
     }
 
-    pageURLs.forEach(function(aPageURL) {
-      debug("Sending " + aType + " " + JSON.stringify(aMessage) +
-        " for " + aPageURL + " @ " + manifestURL +
-        '; extra: ' + JSON.stringify(aExtra));
-
-      let result = this._sendMessageCommon(aType,
-                                           aMessage,
-                                           messageID,
-                                           aPageURL,
-                                           manifestURL,
-                                           aExtra);
-      debug("Returned status of sending message: " + result);
-
+    let page = this._findPage(aType, aPageURI.spec, aManifestURI.spec);
+    if (page) {
       
-      
-      if (result === MSG_SENT_FAILURE_PERM_DENIED) {
-        return;
-      }
+      this._queueMessage(page, aMessage, messageID);
 
-      let page = this._findPage(aType, aPageURL, manifestURL);
-      if (page) {
-        
-        this._queueMessage(page, aMessage, messageID);
-
-        this._openAppPage(page, aMessage, aExtra, result);
-      }
-    }, this);
+      this._openAppPage(page, aMessage, aExtra, result);
+    }
   },
 
   broadcastMessage: function(aType, aMessage, aExtra) {
