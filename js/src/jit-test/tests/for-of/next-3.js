060fe7730d35d137b95bdcdf1bb3a9d965755db2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:25:08 +0000

    Bug 1111170.  Make ArrayIterator and StringIterator next() methods work even with cross-compartment wrappers for those objects as this values.  r=waldo
    UltraBlame original commit: 5e513880db47fb34486a946963775b7a3f7684c5

--- js/src/jit-test/tests/for-of/next-3.js
+++ js/src/jit-test/tests/for-of/next-3.js
@@ -1,13 +1,10 @@
 
 
 
-
-
-
 load(libdir + "asserts.js");
 load(libdir + "iteration.js");
 
 var g = newGlobal();
 g.eval(`var it = [1, 2][${uneval(std_iterator)}]();`);
 assertIteratorNext(g.it, 1);
-assertThrowsInstanceOf([][std_iterator]().next.bind(g.it), TypeError)
+assertDeepEq([][std_iterator]().next.call(g.it), { value: 2, done: false })
