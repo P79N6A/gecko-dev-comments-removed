f0344d4ef4f0fd3241b1e3497e108b43950b8037
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:21:46 +0000

    Backed out changeset 53a5746425f9 (bug 834830)
    UltraBlame original commit: 741bb6d3448bdf093be282481a8d3a223076bdd7

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4377,10 +4377,6 @@ var XULBrowserWindow = {
         gURLBar.removeAttribute("level");
     }
 
-    
-    
-    gURLBar.formatValue();
-
     try {
       uri = Services.uriFixup.createExposableURI(uri);
     } catch (e) {}
