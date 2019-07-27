b49326007dcb67073f0169427d20b1548ad846e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:29:04 +0000

    Bug 1120339 - Add setPlaybackRate method to AnimationPlayerActor; r=past
    UltraBlame original commit: 16dff8194cdeb9268b29d1b5216ed6037d038190

---
+++ toolkit/devtools/server/tests/browser/browser_animation_actors_12.js
@@ -0,0 +1,46 @@
+
+
+
+
+"use strict";
+
+
+
+const {AnimationsFront} = require("devtools/server/actors/animation");
+const {InspectorFront} = require("devtools/server/actors/inspector");
+
+add_task(function*() {
+  let doc = yield addTab(MAIN_DOMAIN + "animation.html");
+
+  initDebuggerServer();
+  let client = new DebuggerClient(DebuggerServer.connectPipe());
+  let form = yield connectDebuggerClient(client);
+  let inspector = InspectorFront(client, form);
+  let walker = yield inspector.getWalker();
+  let animations = AnimationsFront(client, form);
+
+  info("Retrieve an animated node");
+  let node = yield walker.querySelector(walker.rootNode, ".simple-animation");
+
+  info("Retrieve the animation player for the node");
+  let [player] = yield animations.getAnimationPlayersForNode(node);
+
+  ok(player.setPlaybackRate, "Player has the setPlaybackRate method");
+
+  info("Change the rate to 10");
+  yield player.setPlaybackRate(10);
+
+  info("Query the state again");
+  let state = yield player.getCurrentState();
+  is(state.playbackRate, 10, "The playbackRate was updated");
+
+  info("Change the rate back to 1");
+  yield player.setPlaybackRate(1);
+
+  info("Query the state again");
+  state = yield player.getCurrentState();
+  is(state.playbackRate, 1, "The playbackRate was changed back");
+
+  yield closeDebuggerClient(client);
+  gBrowser.removeCurrentTab();
+});
