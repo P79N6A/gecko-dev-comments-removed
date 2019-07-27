37cd3f6f73d32d1e1b825a710d1a8a2867317a8c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:42:49 +0000

    Backout bug 1038243 for not actually helping.
    UltraBlame original commit: 821f18e5c94cf4b547d4b92666cd7003559cc580

--- content/base/src/Attr.h
+++ content/base/src/Attr.h
@@ -81,14 +81,6 @@ public:
   virtual nsIDOMNode* AsDOMNode() { return this; }
 
   
-  
-  static Attr* FromDOMAttr(nsIDOMAttr* aDOMAttr)
-  {
-    nsCOMPtr<nsIAttribute> iattr = do_QueryInterface(aDOMAttr);
-    return static_cast<mozilla::dom::Attr*>(iattr.get());
-  }
-
-  
   virtual JSObject* WrapObject(JSContext* aCx) MOZ_OVERRIDE;
 
   
