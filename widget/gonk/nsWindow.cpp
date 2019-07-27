f94dd887fdb13a634138fd9c55d3b2f603c84885
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:18:33 +0000

    Bug 1042947 - Remove Framebuffer.cpp, r=sotaro
    UltraBlame original commit: a0d61b6bb680d3494f9696e7ca2213e7037a97b2

--- widget/gonk/nsWindow.cpp
+++ widget/gonk/nsWindow.cpp
@@ -25,7 +25,6 @@
 #include "mozilla/Services.h"
 #include "mozilla/FileUtils.h"
 #include "mozilla/ClearOnShutdown.h"
-#include "Framebuffer.h"
 #include "gfxContext.h"
 #include "gfxPlatform.h"
 #include "gfxUtils.h"
@@ -126,43 +125,47 @@ displayEnabledCallback(bool enabled)
 
 nsWindow::nsWindow()
 {
-    if (!sScreenInitialized) {
-        sScreenOnEvent = new ScreenOnOffEvent(true);
-        ClearOnShutdown(&sScreenOnEvent);
-        sScreenOffEvent = new ScreenOnOffEvent(false);
-        ClearOnShutdown(&sScreenOffEvent);
-        GetGonkDisplay()->OnEnabled(displayEnabledCallback);
+    if (sScreenInitialized)
+        return;
 
-        nsIntSize screenSize;
-        bool gotFB = Framebuffer::GetSize(&screenSize);
-        if (!gotFB) {
-            NS_RUNTIMEABORT("Failed to get size from framebuffer, aborting...");
-        }
-        gScreenBounds = nsIntRect(nsIntPoint(0, 0), screenSize);
+    sScreenOnEvent = new ScreenOnOffEvent(true);
+    ClearOnShutdown(&sScreenOnEvent);
+    sScreenOffEvent = new ScreenOnOffEvent(false);
+    ClearOnShutdown(&sScreenOffEvent);
+    GetGonkDisplay()->OnEnabled(displayEnabledCallback);
 
-        char propValue[PROPERTY_VALUE_MAX];
-        property_get("ro.sf.hwrotation", propValue, "0");
-        sPhysicalScreenRotation = atoi(propValue) / 90;
+    nsIntSize screenSize;
 
-        sVirtualBounds = gScreenBounds;
+    ANativeWindow *win = GetGonkDisplay()->GetNativeWindow();
 
-        sScreenInitialized = true;
-
-        nsAppShell::NotifyScreenInitialized();
-
-        
-        
-        
-        
-        
-        
-        gfxPlatform::GetPlatform();
-        if (!ShouldUseOffMainThreadCompositing()) {
-            MOZ_CRASH("How can we render apps, then?");
-        }
-        
-        Preferences::AddBoolVarCache(&sUsingHwc, "layers.composer2d.enabled");
+    if (win->query(win, NATIVE_WINDOW_WIDTH, &screenSize.width) ||
+        win->query(win, NATIVE_WINDOW_HEIGHT, &screenSize.height)) {
+        NS_RUNTIMEABORT("Failed to get native window size, aborting...");
     }
+    gScreenBounds = nsIntRect(nsIntPoint(0, 0), screenSize);
+
+    char propValue[PROPERTY_VALUE_MAX];
+    property_get("ro.sf.hwrotation", propValue, "0");
+    sPhysicalScreenRotation = atoi(propValue) / 90;
+
+    sVirtualBounds = gScreenBounds;
+
+    sScreenInitialized = true;
+
+    nsAppShell::NotifyScreenInitialized();
+
+    
+    
+    
+    
+    
+    
+    gfxPlatform::GetPlatform();
+    if (!ShouldUseOffMainThreadCompositing()) {
+        MOZ_CRASH("How can we render apps, then?");
+    }
+    
+    Preferences::AddBoolVarCache(&sUsingHwc, "layers.composer2d.enabled");
 }
 
 nsWindow::~nsWindow()
