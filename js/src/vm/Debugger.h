b52b32f4d7a2be44548ee6fcb0c1092d0a48c2d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:33:34 +0000

    Bug 1117267 - Uninline Debugger::isDebuggee() to fix -Wundefined-inline warning. r=shu
    UltraBlame original commit: 595796592856148c45672b9024d483b6e0659a22

--- js/src/vm/Debugger.h
+++ js/src/vm/Debugger.h
@@ -22,13 +22,13 @@
 #include "vm/GlobalObject.h"
 #include "vm/SavedStacks.h"
 
-typedef enum JSTrapStatus {
+enum JSTrapStatus {
     JSTRAP_ERROR,
     JSTRAP_CONTINUE,
     JSTRAP_RETURN,
     JSTRAP_THROW,
     JSTRAP_LIMIT
-} JSTrapStatus;
+};
 
 namespace js {
 
@@ -234,10 +234,7 @@ class Debugger : private mozilla::LinkedListElement<Debugger>
 
     
     
-    bool isDebuggee(const JSCompartment *compartment) const {
-        MOZ_ASSERT(compartment);
-        return compartment->isDebuggee() && debuggees.has(compartment->maybeGlobal());
-    }
+    bool isDebuggee(const JSCompartment *compartment) const;
 
   private:
     HeapPtrNativeObject object;         
