4b8dffc2ba0742150eb48453724722a98c6e1c07
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:55:58 +0000

    Backed out changeset b818ba8aabc2 (bug 1079314) for reftest failures on a CLOSED TREE
    UltraBlame original commit: 931302c829b4e78ad355063a42622ad176e124e7

--- layout/generic/WritingModes.h
+++ layout/generic/WritingModes.h
@@ -34,7 +34,7 @@
 
 
 #define CHECK_WRITING_MODE(param) \
-   NS_ASSERTION(param == GetWritingMode(), "writing-mode mismatch")
+   NS_ASSERTION(param == mWritingMode, "writing-mode mismatch")
 
 namespace mozilla {
 
@@ -563,9 +563,7 @@ private:
     return mPoint.y;
   }
 
-#ifdef DEBUG
   WritingMode mWritingMode;
-#endif
 
   
   
@@ -685,35 +683,21 @@ public:
       *this : LogicalSize(aToMode, GetPhysicalSize(aFromMode));
   }
 
-  
-
-
-  bool IsAllZero() const
-  {
-    return ISize() == 0 && BSize() == 0;
-  }
-
-  
-
-
-
   bool operator==(const LogicalSize& aOther) const
   {
-    CHECK_WRITING_MODE(aOther.GetWritingMode());
-    return mSize == aOther.mSize;
+    return mWritingMode == aOther.mWritingMode && mSize == aOther.mSize;
   }
 
   bool operator!=(const LogicalSize& aOther) const
   {
-    CHECK_WRITING_MODE(aOther.GetWritingMode());
-    return mSize != aOther.mSize;
+    return mWritingMode != aOther.mWritingMode || mSize != aOther.mSize;
   }
 
   LogicalSize operator+(const LogicalSize& aOther) const
   {
     CHECK_WRITING_MODE(aOther.GetWritingMode());
-    return LogicalSize(GetWritingMode(), ISize() + aOther.ISize(),
-                                         BSize() + aOther.BSize());
+    return LogicalSize(mWritingMode, ISize() + aOther.ISize(),
+                                     BSize() + aOther.BSize());
   }
   LogicalSize& operator+=(const LogicalSize& aOther)
   {
@@ -726,8 +710,8 @@ public:
   LogicalSize operator-(const LogicalSize& aOther) const
   {
     CHECK_WRITING_MODE(aOther.GetWritingMode());
-    return LogicalSize(GetWritingMode(), ISize() - aOther.ISize(),
-                                         BSize() - aOther.BSize());
+    return LogicalSize(mWritingMode, ISize() - aOther.ISize(),
+                                     BSize() - aOther.BSize());
   }
   LogicalSize& operator-=(const LogicalSize& aOther)
   {
@@ -766,9 +750,7 @@ private:
     return mSize.height;
   }
 
-#ifdef DEBUG
   WritingMode mWritingMode;
-#endif
   nsSize      mSize;
 };
 
@@ -1064,9 +1046,7 @@ private:
     return mMargin.TopBottom();
   }
 
-#ifdef DEBUG
   WritingMode mWritingMode;
-#endif
   nsMargin    mMargin;
 };
 
@@ -1496,9 +1476,7 @@ private:
     return mRect.height;
   }
 
-#ifdef DEBUG
   WritingMode mWritingMode;
-#endif
   nsRect      mRect;
 };
 
--- layout/tables/nsTableOuterFrame.cpp
+++ layout/tables/nsTableOuterFrame.cpp
@@ -512,7 +512,8 @@ nsTableOuterFrame::ComputeAutoSize(nsRenderingContext *aRenderingContext,
                                    bool aShrinkWrap)
 {
   nscoord kidAvailableWidth = aAvailableISize - aMargin.ISize(aWM);
-  NS_ASSERTION(aBorder.IsAllZero() && aPadding.IsAllZero(),
+  NS_ASSERTION(aBorder == LogicalSize(aWM) &&
+               aPadding == LogicalSize(aWM),
                "Table outer frames cannot have borders or paddings");
 
   
