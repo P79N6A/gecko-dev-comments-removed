c393071ede1e95598db38124aed0b44f4814bbc7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:06:24 +0000

    Bug 1060182 - Cache MP4 buffered ranges; r=cpearce
    UltraBlame original commit: cccee89ab1bf14c12cde3915a92ce90bec540a42

--- content/media/MediaResource.h
+++ content/media/MediaResource.h
@@ -142,6 +142,10 @@ public:
     return mStart == 0 && mEnd == 0;
   }
 
+  bool operator==(const MediaByteRange& aRange) const {
+    return mStart == aRange.mStart && mEnd == aRange.mEnd;
+  }
+
   
   void Clear() {
     mStart = 0;
