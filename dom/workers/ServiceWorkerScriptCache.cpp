f890c115b1250aad3db1e69bd640d1175a0c0468
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:58:04 +0000

    Backed out changeset 439e1f841ea2 (bug 1148935) for Werror bustage. CLOSED TREE
    UltraBlame original commit: 9f1eac8fb874d6a508ab92b83cb13e4bdff01197

--- dom/workers/ServiceWorkerScriptCache.cpp
+++ dom/workers/ServiceWorkerScriptCache.cpp
@@ -88,13 +88,10 @@ public:
       return rv;
     }
 
-    
-    
-    
     rv = NS_NewChannel(getter_AddRefs(mChannel),
                        uri, aPrincipal,
                        nsILoadInfo::SEC_NORMAL,
-                       nsIContentPolicy::TYPE_SCRIPT);
+                       nsIContentPolicy::TYPE_SCRIPT); 
     if (NS_WARN_IF(NS_FAILED(rv))) {
       return rv;
     }
