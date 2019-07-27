1d5bb7f6370c46137282fc1f47757d77ea7dcda3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:03:48 +0000

    Bug 1074702 - Part 2: Room views for Loop standalone. r=Standard8
    UltraBlame original commit: 35a67873666d42d47ce532306debc686638fa894

--- browser/components/loop/standalone/content/js/standaloneRoomViews.js
+++ browser/components/loop/standalone/content/js/standaloneRoomViews.js
@@ -7,11 +7,12 @@
 
 
 var loop = loop || {};
-loop.standaloneRoomViews = (function() {
+loop.standaloneRoomViews = (function(mozL10n) {
   "use strict";
 
   var ROOM_STATES = loop.store.ROOM_STATES;
   var sharedActions = loop.shared.actions;
+  var sharedViews = loop.shared.views;
 
   var StandaloneRoomView = React.createClass({displayName: 'StandaloneRoomView',
     mixins: [Backbone.Events],
@@ -23,7 +24,11 @@ loop.standaloneRoomViews = (function() {
     },
 
     getInitialState: function() {
-      return this.props.activeRoomStore.getStoreState();
+      var storeState = this.props.activeRoomStore.getStoreState();
+      return _.extend({}, storeState, {
+        
+        roomState: this.props.roomState || storeState.roomState
+      });
     },
 
     componentWillMount: function() {
@@ -41,8 +46,55 @@ loop.standaloneRoomViews = (function() {
       this.setState(this.props.activeRoomStore.getStoreState());
     },
 
+    
+
+
+
+
+    _getElement: function(className) {
+      return this.getDOMNode().querySelector(className);
+    },
+
+     
+
+
+    _getPublisherConfig: function() {
+      
+      
+      return {
+        insertMode: "append",
+        width: "100%",
+        height: "100%",
+        publishVideo: true,
+        style: {
+          audioLevelDisplayMode: "off",
+          bugDisplayMode: "off",
+          buttonDisplayMode: "off",
+          nameDisplayMode: "off",
+          videoDisabledDisplayMode: "off"
+        }
+      };
+    },
+
     componentWillUnmount: function() {
       this.stopListening(this.props.activeRoomStore);
+    },
+
+    
+
+
+
+
+
+    componentWillUpdate: function(nextProps, nextState) {
+      if (this.state.roomState === ROOM_STATES.READY &&
+          nextState.roomState === ROOM_STATES.JOINED) {
+        this.props.dispatcher.dispatch(new sharedActions.SetupStreamElements({
+          publisherConfig: this._getPublisherConfig(),
+          getLocalElementFunc: this._getElement.bind(this, ".local"),
+          getRemoteElementFunc: this._getElement.bind(this, ".remote")
+        }));
+      }
     },
 
     joinRoom: function() {
@@ -54,29 +106,81 @@ loop.standaloneRoomViews = (function() {
     },
 
     
+
+
+
+
+
+    publishStream: function(type, enabled) {
+      this.props.dispatcher.dispatch(new sharedActions.SetMute({
+        type: type,
+        enabled: enabled
+      }));
+    },
+
     
-    render: function() {
-      switch(this.state.roomState) {
-        case ROOM_STATES.READY: {
-          return (
-            React.DOM.div(null, React.DOM.button({onClick: this.joinRoom}, "Join"))
-          );
-        }
-        case ROOM_STATES.JOINED: {
-          return (
-            React.DOM.div(null, React.DOM.button({onClick: this.leaveRoom}, "Leave"))
-          );
-        }
-        default: {
-          return (
-            React.DOM.div(null, this.state.roomState)
-          );
-        }
+
+
+
+
+    _roomIsActive: function() {
+      return this.state.roomState === ROOM_STATES.JOINED            ||
+             this.state.roomState === ROOM_STATES.SESSION_CONNECTED ||
+             this.state.roomState === ROOM_STATES.HAS_PARTICIPANTS;
+    },
+
+    _renderActionButtons: function() {
+      
+      
+      if (this.state.roomState === ROOM_STATES.INIT ||
+          this.state.roomState === ROOM_STATES.READY) {
+        return (
+          React.DOM.div({className: "room-inner-action-area"}, 
+            React.DOM.button({className: "btn btn-join btn-info", onClick: this.joinRoom}, 
+              mozL10n.get("rooms_room_join_label")
+            )
+          )
+        );
       }
+    },
+
+    render: function() {
+      var localStreamClasses = React.addons.classSet({
+        hide: !this._roomIsActive(),
+        local: true,
+        "local-stream": true,
+        "local-stream-audio": false
+      });
+
+      return (
+        React.DOM.div({className: "room-conversation-wrapper"}, 
+          this._renderActionButtons(), 
+          React.DOM.div({className: "video-layout-wrapper"}, 
+            React.DOM.div({className: "conversation room-conversation"}, 
+              React.DOM.h2({className: "room-name"}, this.state.roomName), 
+              React.DOM.div({className: "media nested"}, 
+                React.DOM.div({className: "video_wrapper remote_wrapper"}, 
+                  React.DOM.div({className: "video_inner remote"})
+                ), 
+                React.DOM.div({className: localStreamClasses})
+              ), 
+              sharedViews.ConversationToolbar({
+                video: {enabled: !this.state.videoMuted,
+                        visible: this._roomIsActive()}, 
+                audio: {enabled: !this.state.audioMuted,
+                        visible: this._roomIsActive()}, 
+                publishStream: this.publishStream, 
+                hangup: this.leaveRoom, 
+                hangupButtonLabel: mozL10n.get("rooms_leave_button_label"), 
+                enableHangup: this._roomIsActive()})
+            )
+          )
+        )
+      );
     }
   });
 
   return {
     StandaloneRoomView: StandaloneRoomView
   };
-})();
+})(navigator.mozL10n);
