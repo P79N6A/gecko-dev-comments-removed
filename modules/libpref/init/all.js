d1dbe3770c8f7d6536e58f4a5176f14475637bd6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:56:27 +0000

    bug 1071880 - Notify user of addons that are slowing their browser down significantly r=mossop
    UltraBlame original commit: f84850059d9e4eb3abb18fa02b8c36bde999928b

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4465,6 +4465,11 @@ pref("dom.mozSettings.SettingsService.verbose.enabled", false);
 pref("dom.mozSettings.allowForceReadOnly", false);
 
 
+pref("browser.addon-watch.interval", 15000);
+
+pref("browser.addon-watch.percentage-limit", 1);
+
+
 pref("dom.requestSync.enabled", false);
 
 
