294436fa05d9e1b0ad674794e7e8f2a6a6c5d0bf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:32:39 +0000

    Bug 1068597 - Enable h264 video decoder on 10.6. r=rillian
    UltraBlame original commit: 2bca09c404ad6ea979a24c1ca2da65b53b38275a

--- content/media/fmp4/apple/AppleVTDecoder.cpp
+++ content/media/fmp4/apple/AppleVTDecoder.cpp
@@ -322,21 +322,23 @@ AppleVTDecoder::CreateDecoderExtensions()
   const void* extensionKeys[] =
     { kCVImageBufferChromaLocationBottomFieldKey,
       kCVImageBufferChromaLocationTopFieldKey,
-      AppleCMLinker::skPropFullRangeVideo,
-      AppleCMLinker::skPropExtensionAtoms };
+      AppleCMLinker::skPropExtensionAtoms,
+      AppleCMLinker::skPropFullRangeVideo  };
 
   const void* extensionValues[] =
     { kCVImageBufferChromaLocation_Left,
       kCVImageBufferChromaLocation_Left,
-      kCFBooleanTrue,
-      atoms };
+      atoms,
+      kCFBooleanTrue };
   static_assert(ArrayLength(extensionKeys) == ArrayLength(extensionValues),
                 "Non matching keys/values array size");
 
   return CFDictionaryCreate(kCFAllocatorDefault,
                             extensionKeys,
                             extensionValues,
-                            ArrayLength(extensionKeys),
+                            AppleCMLinker::skPropFullRangeVideo ?
+                              ArrayLength(extensionKeys) :
+                              ArrayLength(extensionKeys) - 1,
                             &kCFTypeDictionaryKeyCallBacks,
                             &kCFTypeDictionaryValueCallBacks);
 }
