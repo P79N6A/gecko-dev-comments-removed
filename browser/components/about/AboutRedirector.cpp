51148f27c25ae9f3ab5cec12919397e48692dde5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:22:45 +0000

    Bug 1087442 - Attach LoadInfo inside each individual ProtocolHandler - browser/ changes (r=sicking)
    UltraBlame original commit: 1d85d298042d01c6732818bbdc83b139aeea5b19

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -153,8 +153,22 @@ AboutRedirector::NewChannel(nsIURI* aURI,
   for (int i = 0; i < kRedirTotal; i++) {
     if (!strcmp(path.get(), kRedirMap[i].id)) {
       nsCOMPtr<nsIChannel> tempChannel;
-      rv = ioService->NewChannel(nsDependentCString(kRedirMap[i].url),
-                                 nullptr, nullptr, getter_AddRefs(tempChannel));
+      nsCOMPtr<nsIURI> tempURI;
+      rv = NS_NewURI(getter_AddRefs(tempURI),
+                     nsDependentCString(kRedirMap[i].url));
+      NS_ENSURE_SUCCESS(rv, rv);
+      
+      
+      
+      
+      if (aLoadInfo) {
+        rv = NS_NewChannelInternal(getter_AddRefs(tempChannel),
+                                   tempURI,
+                                   aLoadInfo);
+      }
+      else {
+        rv = ioService->NewChannelFromURI(tempURI, getter_AddRefs(tempChannel));
+      }
       NS_ENSURE_SUCCESS(rv, rv);
 
       tempChannel->SetOriginalURI(aURI);
