6b1b35863aa208c78ae75586f34276b3b027f1c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:38:26 +0000

    Bug 1086545 - Bind speech task streams to a parent stream held by voice registry. r=padenot
    UltraBlame original commit: 717878ab4b45772d449be621a270a800ccbe33be

--- dom/media/webspeech/synth/nsSpeechTask.cpp
+++ dom/media/webspeech/synth/nsSpeechTask.cpp
@@ -118,6 +118,7 @@ nsSpeechTask::nsSpeechTask(float aVolume, const nsAString& aText)
 
 nsSpeechTask::~nsSpeechTask()
 {
+  LOG(PR_LOG_DEBUG, ("~nsSpeechTask"));
   if (mStream) {
     if (!mStream->IsDestroyed()) {
       mStream->Destroy();
@@ -125,6 +126,18 @@ nsSpeechTask::~nsSpeechTask()
 
     mStream = nullptr;
   }
+
+  if (mPort) {
+    mPort->Destroy();
+    mPort = nullptr;
+  }
+}
+
+void
+nsSpeechTask::BindStream(ProcessedMediaStream* aStream)
+{
+  mStream = MediaStreamGraph::GetInstance()->CreateSourceStream(nullptr);
+  mPort = aStream->AllocateInputPort(mStream, 0);
 }
 
 NS_IMETHODIMP
@@ -137,16 +150,16 @@ nsSpeechTask::Setup(nsISpeechTaskCallback* aCallback,
 
   mCallback = aCallback;
 
-  if (argc < 2) {
+  if (mIndirectAudio) {
+    if (argc > 0) {
+      NS_WARNING("Audio info arguments in Setup() are ignored for indirect audio services.");
+    }
     return NS_OK;
   }
 
-  if (mIndirectAudio) {
-    NS_WARNING("Audio info arguments in Setup() are ignored for indirect audio services.");
-  }
-
   
-  mStream = MediaStreamGraph::GetInstance()->CreateSourceStream(nullptr);
+  MOZ_ASSERT(mStream);
+
   mStream->AddListener(new SynthStreamListener(this));
 
   
