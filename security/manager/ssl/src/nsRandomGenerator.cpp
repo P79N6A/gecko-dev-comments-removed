c50caf6770e954d6467a9a6bf7cee1fd2cc66eff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:36:47 +0000

    bug 1114741 - have nsRandomGenerator guard against NSS shutdown r=jcj
    nsRandomGenerator uses NSS resources but does not prevent against NSS shutting
    down while doing so. To fix this, nsRandomGenerator must implement
    nsNSSShutDownObject.
    
    UltraBlame original commit: f5889fb4ad1e492298f8953afee8480dcfa00c1a

--- security/manager/ssl/src/nsRandomGenerator.cpp
+++ security/manager/ssl/src/nsRandomGenerator.cpp
@@ -3,41 +3,41 @@
 
 
 #include "nsRandomGenerator.h"
-#include "pk11pub.h"
-#include "secerr.h"
-#include "prerror.h"
+
 #include "nsNSSComponent.h"
-
-
-
+#include "pk11pub.h"
+#include "prerror.h"
+#include "secerr.h"
 
 NS_IMPL_ISUPPORTS(nsRandomGenerator, nsIRandomGenerator)
 
 
 
-
-
-
 NS_IMETHODIMP
 nsRandomGenerator::GenerateRandomBytes(uint32_t aLength,
-                                       uint8_t **aBuffer)
+                                       uint8_t** aBuffer)
 {
   NS_ENSURE_ARG_POINTER(aBuffer);
   *aBuffer = nullptr;
+
+  nsNSSShutDownPreventionLock locker;
+  if (isAlreadyShutDown()) {
+    return NS_ERROR_NOT_AVAILABLE;
+  }
 
   mozilla::ScopedPK11SlotInfo slot(PK11_GetInternalSlot());
   if (!slot) {
     return NS_ERROR_FAILURE;
   }
 
-  uint8_t *buf = reinterpret_cast<uint8_t *>(NS_Alloc(aLength));
+  uint8_t* buf = reinterpret_cast<uint8_t*>(NS_Alloc(aLength));
   if (!buf) {
     return NS_ERROR_OUT_OF_MEMORY;
   }
 
   SECStatus srv = PK11_GenerateRandomOnSlot(slot, buf, aLength);
 
-  if (SECSuccess != srv) {
+  if (srv != SECSuccess) {
     NS_Free(buf);
     return NS_ERROR_FAILURE;
   }
@@ -45,4 +45,13 @@ nsRandomGenerator::GenerateRandomBytes(uint32_t aLength,
   *aBuffer = buf;
 
   return NS_OK;
+}
+
+nsRandomGenerator::~nsRandomGenerator()
+{
+  nsNSSShutDownPreventionLock locker;
+  if (isAlreadyShutDown()) {
+    return;
+  }
+  shutdown(calledFromObject);
 }
