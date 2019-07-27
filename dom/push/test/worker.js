9958c133e0d8d5486fd597bbd3cc8d8c3dd44dec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:58:45 +0000

    Bug 1157108 - onpush EventHandler support. r=ehsan
    UltraBlame original commit: cbabce5695c4b0091a78226207e38b2640a6c916

--- dom/push/test/worker.js
+++ dom/push/test/worker.js
@@ -1,7 +1,9 @@
 
 
 
-addEventListener("push", function(event) {
+this.onpush = handlePush;
+
+function handlePush(event) {
 
   self.clients.matchAll().then(function(result) {
     if (event instanceof PushEvent &&
@@ -13,4 +15,4 @@ addEventListener("push", function(event) {
     }
     result[0].postMessage({type: "finished", okay: "no"});
   });
-});
+}
