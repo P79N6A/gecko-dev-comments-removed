a14def97599af787ba695b2a7bb053be54407cb8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:44:28 +0000

    Bug 1148759 - Ensure that we can inspect objects that reference a Promise; r=past
    UltraBlame original commit: f74e35a6a1e4dde4cc9d380089386427b77957b0

---
+++ browser/devtools/webconsole/test/browser_webconsole_promise.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+
+
+const TEST_URI = "data:text/html;charset=utf8,test for console and promises";
+
+let {DebuggerServer} = Cu.import("resource://gre/modules/devtools/dbg-server.jsm", {});
+
+let LONG_STRING_LENGTH = DebuggerServer.LONG_STRING_LENGTH;
+let LONG_STRING_INITIAL_LENGTH = DebuggerServer.LONG_STRING_INITIAL_LENGTH;
+DebuggerServer.LONG_STRING_LENGTH = 100;
+DebuggerServer.LONG_STRING_INITIAL_LENGTH = 50;
+
+let longString = (new Array(DebuggerServer.LONG_STRING_LENGTH + 4)).join("a");
+let initialString = longString.substring(0, DebuggerServer.LONG_STRING_INITIAL_LENGTH);
+
+let inputTests = [
+  
+  {
+    input: "({ x: Promise.resolve() })",
+    output: "Object { x: Promise }",
+    printOutput: "[object Object]",
+    inspectable: true,
+    variablesViewLabel: "Object"
+  },
+];
+
+longString = initialString = null;
+
+function test() {
+  requestLongerTimeout(2);
+
+  registerCleanupFunction(() => {
+    DebuggerServer.LONG_STRING_LENGTH = LONG_STRING_LENGTH;
+    DebuggerServer.LONG_STRING_INITIAL_LENGTH = LONG_STRING_INITIAL_LENGTH;
+  });
+
+  Task.spawn(function*() {
+    let {tab} = yield loadTab(TEST_URI);
+    let hud = yield openConsole(tab);
+    return checkOutputForInputs(hud, inputTests);
+  }).then(finishUp);
+}
+
+function finishUp() {
+  longString = initialString = inputTests = null;
+  finishTest();
+}
