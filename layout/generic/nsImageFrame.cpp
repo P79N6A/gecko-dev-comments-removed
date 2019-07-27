6a52d16fd1e60c023f4069aececa5727aceb14b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:21:42 +0000

    Bug 1141376 - Do not call OnSizeAvailable in nsImageFrame::Init. r=tn
    UltraBlame original commit: 915f028b125ec2fb2ded4e1c388ba5ec0f226bfb

--- layout/generic/nsImageFrame.cpp
+++ layout/generic/nsImageFrame.cpp
@@ -289,13 +289,6 @@ nsImageFrame::Init(nsIContent*       aContent,
   nsCOMPtr<nsISupportsPriority> p = do_QueryInterface(currentRequest);
   if (p)
     p->AdjustPriority(-1);
-
-  
-  if (currentRequest) {
-    nsCOMPtr<imgIContainer> image;
-    currentRequest->GetImage(getter_AddRefs(image));
-    OnSizeAvailable(currentRequest, image);
-  }
 }
 
 bool
