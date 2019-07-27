3a4343c14ec3c54b30d9645844e9e111dfd16a0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:52:24 +0000

    Bug 1169867 - XMLHttpRequest::SendInternal should not unpin itself when the worker goes away. r=bent
    UltraBlame original commit: 281a5a64be5e4c30d551c3605f996352ee0cc483

--- dom/workers/XMLHttpRequest.cpp
+++ dom/workers/XMLHttpRequest.cpp
@@ -1882,6 +1882,11 @@ XMLHttpRequest::SendInternal(const nsAString& aStringBody,
     new SendRunnable(mWorkerPrivate, mProxy, aStringBody, Move(aBody),
                      aClonedObjects, syncLoopTarget, hasUploadListeners);
   if (!runnable->Dispatch(cx)) {
+    
+    
+    if (!mRooted) {
+      autoUnpin.Clear();
+    }
     aRv.Throw(NS_ERROR_FAILURE);
     return;
   }
