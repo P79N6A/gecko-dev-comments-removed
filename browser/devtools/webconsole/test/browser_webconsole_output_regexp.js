30d58c478ed3316e9adc33c74ac7226e4a9465cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:20:54 +0000

    Bug 1079919 - Part 3: Add test for RegExp.prototype properties in webconsole. r=fitzgen
    UltraBlame original commit: 113dd9e05803a578d3e1b13826da76f74ac7cc15

---
+++ browser/devtools/webconsole/test/browser_webconsole_output_regexp.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+const TEST_URI = "http://example.com/browser/browser/devtools/webconsole/test/test-console-output-regexp.html";
+
+let dateNow = Date.now();
+
+let inputTests = [
+  
+  {
+    input: "/foo/igym",
+    output: "/foo/gimy",
+    printOutput: "Error: source called",
+    inspectable: true,
+  },
+];
+
+function test() {
+  requestLongerTimeout(2);
+  Task.spawn(function*() {
+    let {tab} = yield loadTab(TEST_URI);
+    let hud = yield openConsole(tab);
+    return checkOutputForInputs(hud, inputTests);
+  }).then(finishUp);
+}
+
+function finishUp() {
+  inputTests = dateNow = null;
+  finishTest();
+}
