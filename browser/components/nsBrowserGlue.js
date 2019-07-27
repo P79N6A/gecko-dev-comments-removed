7d383cc0e84f208ad25d46863f4adf31f96eb1ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:58:10 +0000

    Bug 1071272 - Remove the clearUserPref("loop.hawk-session-token.fxa") hack from bug 1071259 since 1071247 has been deployed. r=MattN
    UltraBlame original commit: d09f5f86a80354563a0272758fd67ff84ef2501d

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -740,13 +740,6 @@ BrowserGlue.prototype = {
 #endif
     webrtcUI.uninit();
     FormValidationHandler.uninit();
-
-    
-    
-    if (Services.prefs.getPrefType("loop.autologin-after-restart") != Ci.nsIPrefBranch.PREF_BOOL ||
-        !Services.prefs.getBoolPref("loop.autologin-after-restart")) {
-      Services.prefs.clearUserPref("loop.hawk-session-token.fxa");
-    }
   },
 
   
