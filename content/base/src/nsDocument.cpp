88dbb66cafb4429300af34237a3347f497e32efc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:55:52 +0000

    Bug 1017257 - Attach Loop CSP Policy r=sstamm,dmose
    UltraBlame original commit: 547ad4574166f3e6f9f7d51558725213df9caf2a

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -555,7 +555,7 @@ namespace {
 struct PositionComparator
 {
   Element* const mElement;
-  explicit PositionComparator(Element* const aElement) : mElement(aElement) {}
+  PositionComparator(Element* const aElement) : mElement(aElement) {}
 
   int operator()(void* aElement) const {
     Element* curElement = static_cast<Element*>(aElement);
@@ -2752,6 +2752,33 @@ AppendCSPFromHeader(nsIContentSecurityPolicy* csp,
   return NS_OK;
 }
 
+bool
+nsDocument::IsLoopDocument(nsIChannel *aChannel)
+{
+  nsCOMPtr<nsIURI> chanURI;
+  nsresult rv = aChannel->GetOriginalURI(getter_AddRefs(chanURI));
+  NS_ENSURE_SUCCESS(rv, false);
+
+  bool isAbout = false;
+  bool isLoop = false;
+  rv = chanURI->SchemeIs("about", &isAbout);
+  NS_ENSURE_SUCCESS(rv, false);
+  if (isAbout) {
+    nsCOMPtr<nsIURI> loopURI;
+    rv = NS_NewURI(getter_AddRefs(loopURI), "about:loopconversation");
+    NS_ENSURE_SUCCESS(rv, false);
+    rv = chanURI->EqualsExceptRef(loopURI, &isLoop);
+    NS_ENSURE_SUCCESS(rv, false);
+    if (!isLoop) {
+      rv = NS_NewURI(getter_AddRefs(loopURI), "about:looppanel");
+      NS_ENSURE_SUCCESS(rv, false);
+      rv = chanURI->EqualsExceptRef(loopURI, &isLoop);
+      NS_ENSURE_SUCCESS(rv, false);
+    }
+  }
+  return isLoop;
+}
+
 nsresult
 nsDocument::InitCSP(nsIChannel* aChannel)
 {
@@ -2805,9 +2832,13 @@ nsDocument::InitCSP(nsIChannel* aChannel)
     }
   }
 
+ 
+ bool applyLoopCSP = IsLoopDocument(aChannel);
+
   
   if (!applyAppDefaultCSP &&
       !applyAppManifestCSP &&
+      !applyLoopCSP &&
       cspHeaderValue.IsEmpty() &&
       cspROHeaderValue.IsEmpty()) {
 #ifdef PR_LOGGING
@@ -2878,6 +2909,17 @@ nsDocument::InitCSP(nsIChannel* aChannel)
   
   if (applyAppManifestCSP) {
     csp->AppendPolicy(appManifestCSP, false);
+  }
+
+  
+  if (applyLoopCSP) {
+    nsAdoptingString loopCSP;
+    loopCSP = Preferences::GetString("loop.CSP");
+    NS_ASSERTION(loopCSP, "Missing loop.CSP preference");
+    
+    if (loopCSP) {
+      csp->AppendPolicy(loopCSP, false);
+    }
   }
 
   
