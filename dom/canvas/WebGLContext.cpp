7b4ff238a6184ef0b31e4aed4df935fb7aa2c6ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:59:21 +0000

    Bug 1066280 - Fixes for SurfaceStream removal. - r=kamidphish,mattwoodrow
    UltraBlame original commit: 49531b0af9b802c859cda9bd4241b611ed025c38

--- dom/canvas/WebGLContext.cpp
+++ dom/canvas/WebGLContext.cpp
@@ -665,6 +665,9 @@ CreateOffscreen(GLContext* gl,
     baseCaps.preserve = options.preserveDrawingBuffer;
     baseCaps.stencil = options.stencil;
 
+    if (!baseCaps.alpha)
+        baseCaps.premultAlpha = true;
+
     
     
     
