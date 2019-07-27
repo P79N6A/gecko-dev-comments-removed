3db1e1fe102f02349b1776a951592ccdf4fed35a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:52:35 +0000

    bug 1056133 - add h2 to coalescing list r=hurley
    UltraBlame original commit: 15fd872a8a845d52a4c595c8f97fb55f08246b1c

--- netwerk/protocol/http/nsHttpConnectionMgr.cpp
+++ netwerk/protocol/http/nsHttpConnectionMgr.cpp
@@ -805,21 +805,19 @@ nsHttpConnectionMgr::GetSpdyPreferredEnt(nsConnectionEntry *aOriginalEntry)
         return nullptr;
     }
 
-    if (gHttpHandler->SpdyInfo()->ProtocolEnabled(0))
-        rv = sslSocketControl->JoinConnection(gHttpHandler->SpdyInfo()->VersionString[0],
-                                              aOriginalEntry->mConnInfo->GetHost(),
-                                              aOriginalEntry->mConnInfo->Port(),
-                                              &isJoined);
-    else
-        rv = NS_OK;                               
-
     
-    
-    if (NS_SUCCEEDED(rv) && !isJoined && gHttpHandler->SpdyInfo()->ProtocolEnabled(1)) {
-        rv = sslSocketControl->JoinConnection(gHttpHandler->SpdyInfo()->VersionString[1],
-                                              aOriginalEntry->mConnInfo->GetHost(),
-                                              aOriginalEntry->mConnInfo->Port(),
-                                              &isJoined);
+    const SpdyInformation *info = gHttpHandler->SpdyInfo();
+    for (uint32_t index = SpdyInformation::kCount;
+         NS_SUCCEEDED(rv) && index > 0; --index) {
+        if (info->ProtocolEnabled(index - 1)) {
+            rv = sslSocketControl->JoinConnection(info->VersionString[index - 1],
+                                                  aOriginalEntry->mConnInfo->GetHost(),
+                                                  aOriginalEntry->mConnInfo->Port(),
+                                                  &isJoined);
+            if (NS_SUCCEEDED(rv) && isJoined) {
+                break;
+            }
+        }
     }
 
     if (NS_FAILED(rv) || !isJoined) {
