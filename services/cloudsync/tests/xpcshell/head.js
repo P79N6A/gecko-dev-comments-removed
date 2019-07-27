991a3f66605cae646672d61f2ff0e706521ee15f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:15:35 +0000

    Bug 993584 - Initial landing for CloudSync. r=rnewman
    UltraBlame original commit: bb965f10472c3b99add211332e55f87ef1e91f6c

---
+++ services/cloudsync/tests/xpcshell/head.js
@@ -0,0 +1,10 @@
+
+
+
+const {classes: Cc, interfaces: Ci, results: Cr, utils: Cu} = Components;
+
+"use strict";
+
+(function initCloudSyncTestingInfrastructure () {
+  do_get_profile();
+}).call(this);
