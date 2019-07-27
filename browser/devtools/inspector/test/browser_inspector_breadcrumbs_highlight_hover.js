eba72b2bda4296c9593f899658bb0f6fa9037442
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:30:55 +0000

    Bug 993445 - Highlight nodes on hover of breadcrumbs. r=pbrosset, r=bgrins
    UltraBlame original commit: 2813f8efa36699d72c5aaac3c951cbf5ab91fcd3

---
+++ browser/devtools/inspector/test/browser_inspector_breadcrumbs_highlight_hover.js
@@ -0,0 +1,32 @@
+
+
+
+
+"use strict";
+
+
+
+let test = asyncTest(function*() {
+  info("Loading the test document and opening the inspector");
+  yield addTab("data:text/html;charset=utf-8,<h1>foo</h1><span>bar</span>");
+  let {toolbox, inspector} = yield openInspector();
+  info("Selecting the test node");
+  yield selectNode("span", inspector);
+  let bcButtons = inspector.breadcrumbs["container"];
+
+  let onNodeHighlighted = toolbox.once("node-highlight");
+  let button = bcButtons.childNodes[1];
+  EventUtils.synthesizeMouseAtCenter(button, {type: "mousemove"}, button.ownerDocument.defaultView);
+  yield onNodeHighlighted;
+  ok(isHighlighting(), "The highlighter is shown on a markup container hover");
+  is(getHighlitNode(), getNode("body"), "The highlighter highlights the right node");
+
+  let onNodeHighlighted = toolbox.once("node-highlight");
+  let button = bcButtons.childNodes[2];
+  EventUtils.synthesizeMouseAtCenter(button, {type: "mousemove"}, button.ownerDocument.defaultView);
+  yield onNodeHighlighted;
+  ok(isHighlighting(), "The highlighter is shown on a markup container hover");
+  is(getHighlitNode(), getNode("span"), "The highlighter highlights the right node");
+
+  gBrowser.removeCurrentTab();
+});
