b38f7933dd4e7975aa5ac0f3b9f5d8b238fd0840
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:08:33 +0000

    Bug 992077 patch 3 - Stop using OverflowChangedTracker::CHILDREN_AND_PARENT_CHANGED for the UpdateOverflow and related hints.  r=mats
    This makes UpdateOverflow hint handling (which definitely did previously
    need CHILDREN_AND_PARENT_CHANGED) and handling of related hints (which I
    believe never actually needed CHILDREN_AND_PARENT_CHANGED) use the
    CHILDREN_CHANGED hint in the overflow changed tracker, since use of
    CHILDREN_AND_PARENT_CHANGED is no longer needed following the
    introduction of the UpdateParentOverflow hint in patch 2.
    
    UltraBlame original commit: 1fb87cd3cbfa829d6411055b035a6e3a1777c269

--- layout/base/RestyleManager.cpp
+++ layout/base/RestyleManager.cpp
@@ -589,7 +589,7 @@ RestyleManager::AddSubtreeToOverflowTracker(nsIFrame* aFrame)
 {
   mOverflowChangedTracker.AddFrame(
       aFrame,
-      OverflowChangedTracker::CHILDREN_AND_PARENT_CHANGED);
+      OverflowChangedTracker::CHILDREN_CHANGED);
   nsIFrame::ChildListIterator lists(aFrame);
   for (; !lists.IsDone(); lists.Next()) {
     nsFrameList::Enumerator childFrames(lists.CurrentList());
@@ -855,7 +855,7 @@ RestyleManager::ProcessRestyledFrames(nsStyleChangeList& aChangeList)
             if (!(childFrame->GetStateBits() &
                   (NS_FRAME_IS_DIRTY | NS_FRAME_HAS_DIRTY_CHILDREN))) {
               mOverflowChangedTracker.AddFrame(childFrame,
-                           OverflowChangedTracker::CHILDREN_AND_PARENT_CHANGED);
+                           OverflowChangedTracker::CHILDREN_CHANGED);
             }
             NS_ASSERTION(!nsLayoutUtils::GetNextContinuationOrIBSplitSibling(childFrame),
                          "SVG frames should not have continuations "
@@ -878,7 +878,7 @@ RestyleManager::ProcessRestyledFrames(nsStyleChangeList& aChangeList)
             
             if (hint & (nsChangeHint_UpdateOverflow |
                         nsChangeHint_UpdateSubtreeOverflow)) {
-              changeKind = OverflowChangedTracker::CHILDREN_AND_PARENT_CHANGED;
+              changeKind = OverflowChangedTracker::CHILDREN_CHANGED;
             } else {
               changeKind = OverflowChangedTracker::TRANSFORM_CHANGED;
             }
