79caf08d1249074f3f77470816b0665463b835fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:11:31 +0000

    Bug 1137483 - Add search engine bar to the bottom of BrowserSearch. r=liuche
    UltraBlame original commit: 0602fff3681db69f5dc2d6ef7745f4b8fabfa5f6

--- mobile/android/base/home/BrowserSearch.java
+++ mobile/android/base/home/BrowserSearch.java
@@ -65,7 +65,8 @@ import android.widget.TextView;
 
 
 public class BrowserSearch extends HomeFragment
-                           implements GeckoEventListener {
+                           implements GeckoEventListener,
+                                      SearchEngineBar.OnSearchBarClickListener {
 
     @RobocopTarget
     public interface SuggestClientFactory {
@@ -123,6 +124,9 @@ public class BrowserSearch extends HomeFragment
 
     
     private HomeListView mList;
+
+    
+    private SearchEngineBar mSearchEngineBar;
 
     
     
@@ -268,6 +272,7 @@ public class BrowserSearch extends HomeFragment
         
         mView = (LinearLayout) inflater.inflate(R.layout.browser_search, container, false);
         mList = (HomeListView) mView.findViewById(R.id.home_list_view);
+        mSearchEngineBar = (SearchEngineBar) mView.findViewById(R.id.search_engine_bar);
 
         return mView;
     }
@@ -278,6 +283,9 @@ public class BrowserSearch extends HomeFragment
 
         EventDispatcher.getInstance().unregisterGeckoThreadListener(this,
             "SearchEngines:Data");
+
+        mSearchEngineBar.setAdapter(null);
+        mSearchEngineBar = null;
 
         mList.setAdapter(null);
         mList = null;
@@ -350,6 +358,12 @@ public class BrowserSearch extends HomeFragment
         registerForContextMenu(mList);
         EventDispatcher.getInstance().registerGeckoThreadListener(this,
             "SearchEngines:Data");
+
+        
+        
+        
+        mSearchEngineBar.setSearchEngines(mSearchEngines);
+        mSearchEngineBar.setOnSearchBarClickListener(this);
     }
 
     @Override
@@ -587,6 +601,8 @@ public class BrowserSearch extends HomeFragment
                 mAdapter.notifyDataSetChanged();
             }
 
+            mSearchEngineBar.setSearchEngines(mSearchEngines);
+
             
             
             if (!mSuggestionsEnabled && !suggestionsPrompted && mSuggestClient != null) {
@@ -597,6 +613,11 @@ public class BrowserSearch extends HomeFragment
         }
 
         filterSuggestions();
+    }
+
+    @Override
+    public void onSearchBarClickListener(final SearchEngine searchEngine) {
+        mSearchListener.onSearch(searchEngine, mSearchTerm);
     }
 
     private void maybeSetSuggestClient(final String suggestTemplate, final boolean isPrivate) {
