515734fe70c295f005e975d16bd4187b21d70029
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:18:03 +0000

    Bug 1123523 - Part 1: Add an nsIAnimationObserver interface to watch adding/changing/removing AnimationPlayer objects. r=smaug
    UltraBlame original commit: ca29a89ca26ba70a9094c6553691556c11a00f98

---
+++ dom/base/nsStubAnimationObserver.cpp
@@ -0,0 +1,8 @@
+
+
+
+
+
+#include "nsStubAnimationObserver.h"
+
+NS_IMPL_NSIANIMATIONOBSERVER_STUB(nsStubAnimationObserver)
---
+++ dom/base/nsStubAnimationObserver.h
@@ -0,0 +1,16 @@
+
+
+
+
+
+#ifndef nsStubAnimationObserver_h_
+#define nsStubAnimationObserver_h_
+
+#include "nsIAnimationObserver.h"
+
+class nsStubAnimationObserver : public nsIAnimationObserver {
+public:
+  NS_DECL_NSIANIMATIONOBSERVER
+};
+
+#endif
