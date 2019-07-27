3f0fccac79e7382d4571f8e0d76aa790aa8bdf74
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:27:30 +0000

    Bug 1110546 - Allow null as the principal argument for the sandbox constructor. r=gabor
    UltraBlame original commit: 36f9d31965036534c74757e73c3ce0b8bdd71f20

--- js/xpconnect/src/Sandbox.cpp
+++ js/xpconnect/src/Sandbox.cpp
@@ -1415,6 +1415,10 @@ nsXPCComponents_utils_Sandbox::CallOrConstruct(nsIXPConnectWrappedNative *wrappe
         } else {
             ok = GetPrincipalOrSOP(cx, obj, getter_AddRefs(prinOrSop));
         }
+    } else if (args[0].isNull()) {
+        
+        
+        ok = true;
     }
 
     if (!ok)
