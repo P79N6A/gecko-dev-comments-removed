b22cbe2575149fd0066176206a0b8eee736a4c81
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:13:36 +0000

    Bug 980770 - Fully enable (for RELEASE_BUILD) off-main-thread animations on all platforms with off-main-thread compositing.  r=birtles
    UltraBlame original commit: b9808ed3cacff7959d75f0fc7d4c409fd2e5b39b

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -297,7 +297,6 @@ pref("ui.dragThresholdY", 25);
 
 
 pref("layers.offmainthreadcomposition.enabled", true);
-pref("layers.offmainthreadcomposition.async-animations", true);
 #ifndef MOZ_WIDGET_GONK
 pref("dom.ipc.tabs.disabled", true);
 #else
