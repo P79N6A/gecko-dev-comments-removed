3bb99e407aabe2f41759eeb1d55497a532411e9b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:52:19 +0000

    Bug 1073238 - Split UITour.jsm into chrome and content parts that communicate via messages. r=MattN
    UltraBlame original commit: 0121cf3ba5d72182926ee3b7294543d07eda26ce

--- browser/modules/test/browser_UITour_detach_tab.js
+++ browser/modules/test/browser_UITour_detach_tab.js
@@ -11,8 +11,6 @@ let gTestTab;
 let gContentAPI;
 let gContentWindow;
 let gContentDoc;
-let highlight = document.getElementById("UITourHighlight");
-let tooltip = document.getElementById("UITourTooltip");
 
 Components.utils.import("resource:///modules/UITour.jsm");
 
@@ -23,61 +21,69 @@ function test() {
   UITourTest();
 }
 
+
+
+
+
+
 let tests = [
-  function test_move_tab_to_new_window(done) {
-    let gOpenedWindow;
+  taskify(function* test_move_tab_to_new_window(done) {
     let onVisibilityChange = (aEvent) => {
       if (!document.hidden && window != UITour.getChromeWindow(aEvent.target)) {
         gContentAPI.showHighlight("appMenu");
       }
     };
-    let onDOMWindowDestroyed = (aWindow, aTopic, aData) => {
-      if (gOpenedWindow && aWindow == gOpenedWindow) {
+
+    let highlight = document.getElementById("UITourHighlight");
+    let windowDestroyedDeferred = Promise.defer();
+    let onDOMWindowDestroyed = (aWindow) => {
+      if (gContentWindow && aWindow == gContentWindow) {
         Services.obs.removeObserver(onDOMWindowDestroyed, "dom-window-destroyed", false);
-        done();
-      }
-    };
-    let onBrowserDelayedStartup = (aWindow, aTopic, aData) => {
-      gOpenedWindow = aWindow;
-      Services.obs.removeObserver(onBrowserDelayedStartup, "browser-delayed-startup-finished");
-      try {
-        let newWindowHighlight = gOpenedWindow.document.getElementById("UITourHighlight");
-        let selectedTab = aWindow.gBrowser.selectedTab;
-        is(selectedTab.linkedBrowser && selectedTab.linkedBrowser.contentDocument, gContentDoc, "Document should be selected in new window");
-        ok(UITour.originTabs && UITour.originTabs.has(aWindow), "Window should be known");
-        ok(UITour.originTabs.get(aWindow).has(selectedTab), "Tab should be known");
-        waitForElementToBeVisible(newWindowHighlight, function checkHighlightIsThere() {
-          let shownPromise = promisePanelShown(aWindow);
-          gContentAPI.showMenu("appMenu");
-          shownPromise.then(() => {
-            isnot(aWindow.PanelUI.panel.state, "closed", "Panel should be open");
-            ok(aWindow.PanelUI.contents.children.length > 0, "Panel contents should have children");
-            gContentAPI.hideHighlight();
-            gContentAPI.hideMenu("appMenu");
-            gTestTab = null;
-            aWindow.close();
-          }).then(null, Components.utils.reportError);
-        }, "Highlight should be shown in new window.");
-      } catch (ex) {
-        Cu.reportError(ex);
-        ok(false, "An error occurred running UITour tab detach test.");
-      } finally {
-        gContentDoc.removeEventListener("visibilitychange", onVisibilityChange, false);
-        Services.obs.addObserver(onDOMWindowDestroyed, "dom-window-destroyed", false);
+        windowDestroyedDeferred.resolve();
       }
     };
 
-    Services.obs.addObserver(onBrowserDelayedStartup, "browser-delayed-startup-finished", false);
+    let browserStartupDeferred = Promise.defer();
+    Services.obs.addObserver(function onBrowserDelayedStartup(aWindow) {
+      Services.obs.removeObserver(onBrowserDelayedStartup, "browser-delayed-startup-finished");
+      browserStartupDeferred.resolve(aWindow);
+    }, "browser-delayed-startup-finished", false);
+
     
     
     
     gContentDoc = gBrowser.selectedTab.linkedBrowser.contentDocument;
     gContentDoc.addEventListener("visibilitychange", onVisibilityChange, false);
     gContentAPI.showHighlight("appMenu");
-    waitForElementToBeVisible(highlight, function checkForInitialHighlight() {
-      gBrowser.replaceTabWithWindow(gBrowser.selectedTab);
-    });
 
-  },
+    yield elementVisiblePromise(highlight);
+
+    gBrowser.replaceTabWithWindow(gBrowser.selectedTab);
+
+    gContentWindow = yield browserStartupDeferred.promise;
+
+    
+    let newWindowHighlight = gContentWindow.document.getElementById("UITourHighlight");
+    yield elementVisiblePromise(newWindowHighlight);
+
+    let selectedTab = gContentWindow.gBrowser.selectedTab;
+    is(selectedTab.linkedBrowser && selectedTab.linkedBrowser.contentDocument, gContentDoc, "Document should be selected in new window");
+    ok(UITour.originTabs && UITour.originTabs.has(gContentWindow), "Window should be known");
+    ok(UITour.originTabs.get(gContentWindow).has(selectedTab), "Tab should be known");
+
+    let shownPromise = promisePanelShown(gContentWindow);
+    gContentAPI.showMenu("appMenu");
+    yield shownPromise;
+
+    isnot(gContentWindow.PanelUI.panel.state, "closed", "Panel should be open");
+    ok(gContentWindow.PanelUI.contents.children.length > 0, "Panel contents should have children");
+    gContentAPI.hideHighlight();
+    gContentAPI.hideMenu("appMenu");
+    gTestTab = null;
+
+    Services.obs.addObserver(onDOMWindowDestroyed, "dom-window-destroyed", false);
+    gContentWindow.close();
+
+    yield windowDestroyedDeferred.promise;
+  }),
 ];
-
