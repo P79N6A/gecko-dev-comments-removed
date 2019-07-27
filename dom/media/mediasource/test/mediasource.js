54e84c9e72c03cd380ad5381d91bbd01502e14f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:44:38 +0000

    Bug 1121692 - Tests. r=mattwoodrow,r=cpearce
    UltraBlame original commit: 3d0dd2ae7e6c2cdb3d43b080b22ac95dd757247f

--- dom/media/mediasource/test/mediasource.js
+++ dom/media/mediasource/test/mediasource.js
@@ -79,3 +79,22 @@ function loadSegment(sb, typedArrayOrArrayBuffer) {
     sb.appendBuffer(typedArray);
   });
 }
+
+function fetchAndLoad(sb, prefix, chunks, suffix) {
+
+  
+  var buffers = {};
+  var fetches = [];
+  for (var chunk of chunks) {
+    fetches.push(fetchWithXHR(prefix + chunk + suffix).then(((c, x) => buffers[c] = x).bind(null, chunk)));
+  }
+
+  
+  return Promise.all(fetches).then(function() {
+    var rv = Promise.resolve();
+    for (var chunk of chunks) {
+      rv = rv.then(loadSegment.bind(null, sb, buffers[chunk]));
+    }
+    return rv;
+  });
+}
