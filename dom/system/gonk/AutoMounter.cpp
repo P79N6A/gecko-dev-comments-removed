4fdc37a9112f00e351e22c5d9ec0f478cf3a9b33
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:28:39 +0000

    Bug 1179691 - [Automounter] Fix the problem of get format and share request in the same time. r=dhylands
    UltraBlame original commit: a8de926c8ea522ae8b2c47c3d38dbf5cc4488a6e

--- dom/system/gonk/AutoMounter.cpp
+++ dom/system/gonk/AutoMounter.cpp
@@ -1056,18 +1056,23 @@ AutoMounter::UpdateState()
           LOG("UpdateState: Volume %s is %s", vol->NameStr(), vol->StateStr());
           if (vol->IsFormatting() && !vol->IsFormatRequested()) {
             vol->SetFormatRequested(false);
-            LOG("UpdateState: Mounting %s", vol->NameStr());
-            vol->StartMount(mResponseCallback);
-            break;
+            if (!(tryToShare && vol->IsSharingEnabled()) && volState == nsIVolume::STATE_IDLE) {
+              LOG("UpdateState: Mounting %s", vol->NameStr());
+              vol->StartMount(mResponseCallback);
+              break;
+            }
           }
-          if (tryToShare && vol->IsSharingEnabled() && volState == nsIVolume::STATE_IDLE) {
-            
-            LOG("UpdateState: Sharing %s", vol->NameStr());
-            vol->StartShare(mResponseCallback);
-          } else if (vol->IsFormatRequested()){
+
+          
+          
+          if (vol->IsFormatRequested()) {
             
             LOG("UpdateState: Formatting %s", vol->NameStr());
             vol->StartFormat(mResponseCallback);
+          } else if (tryToShare && vol->IsSharingEnabled() && volState == nsIVolume::STATE_IDLE) {
+            
+            LOG("UpdateState: Sharing %s", vol->NameStr());
+            vol->StartShare(mResponseCallback);
           }
           return; 
         }
