66d898083e99e957f91bec2c531735d99714fb2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:30:35 +0000

    Bug 1141555 - HTTP cache v2 generates corrupted entries, r=jduell
    UltraBlame original commit: 1b374964865c550409812bc72d3aa3804ce599ca

--- netwerk/cache2/CacheFileMetadata.cpp
+++ netwerk/cache2/CacheFileMetadata.cpp
@@ -285,7 +285,7 @@ CacheFileMetadata::WriteMetadata(uint32_t aOffset,
   }
 
   rv = CacheFileIOManager::Write(mHandle, aOffset, writeBuffer, p - writeBuffer,
-                                 true, aListener ? this : nullptr);
+                                 true, true, aListener ? this : nullptr);
   if (NS_FAILED(rv)) {
     LOG(("CacheFileMetadata::WriteMetadata() - CacheFileIOManager::Write() "
          "failed synchronously. [this=%p, rv=0x%08x]", this, rv));
@@ -773,6 +773,11 @@ CacheFileMetadata::InitEmptyMetadata()
   mMetaHdr.mKeySize = mKey.Length();
 
   DoMemoryReport(MemoryUsage());
+
+  
+  if (mHandle && mHandle->FileExists() && mHandle->FileSize()) {
+    CacheFileIOManager::TruncateSeekSetEOF(mHandle, 0, 0, nullptr);
+  }
 }
 
 nsresult
