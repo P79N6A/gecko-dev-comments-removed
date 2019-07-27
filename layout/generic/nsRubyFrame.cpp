852ccf294e0b04889751643458e7bf50674ffc77
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:30:42 +0000

    Bug 1115264 - Fix -Wsometimes-uninitialized clang warning in nsRubyFrame.cpp. r=dholbert
    UltraBlame original commit: 9e4bd48463a7433578f4d652e9f576f9e4861b6b

--- layout/generic/nsRubyFrame.cpp
+++ layout/generic/nsRubyFrame.cpp
@@ -432,6 +432,7 @@ nsRubyFrame::ReflowSegment(nsPresContext* aPresContext,
       } else {
         
         MOZ_ASSERT_UNREACHABLE("Unsupported ruby-position");
+        y = offsetRect.Y();
       }
     }
     FinishReflowChild(textContainer, aPresContext, textMetrics,
