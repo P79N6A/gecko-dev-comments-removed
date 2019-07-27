e297dac25aab0802b3a1c7db85d060801dbb79c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:57:42 +0000

    Bug 944164 - Move a bunch of file-related stuff onto an os.file object, creating aliases from the old names, r=jorendorff
    UltraBlame original commit: 1ae18f4c151fb6d8fc5fad0dd1bc7895e08a34ff

--- js/src/shell/OSObject.h
+++ js/src/shell/OSObject.h
@@ -12,11 +12,21 @@
 #include "jsapi.h"
 
 namespace js {
+namespace shell {
 
 
 bool
 DefineOS(JSContext* cx, JS::HandleObject global);
 
+enum PathResolutionMode {
+    RootRelative,
+    ScriptRelative
+};
+
+JSString*
+ResolvePath(JSContext* cx, JS::HandleString filenameStr, PathResolutionMode resolveMode);
+
+}
 }
 
 #endif 
