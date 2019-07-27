6efbacb50ec87da078ce407592a630451234113f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:27:10 +0000

    Bug 986201 - Return correct specificity for element styles instead of causing an exception. r=jwalker
    UltraBlame original commit: a9c0471594e5d452f3e68a385ae2387daea40ac4

--- toolkit/devtools/styleinspector/css-logic.js
+++ toolkit/devtools/styleinspector/css-logic.js
@@ -1506,6 +1506,14 @@ CssSelector.prototype = {
 
   get specificity()
   {
+    if (this.elementStyle) {
+      
+      
+      
+      
+      return 0x01000000;
+    }
+
     if (this._specificity) {
       return this._specificity;
     }
