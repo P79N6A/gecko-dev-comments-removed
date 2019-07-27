5cdf7e4207afe51be571ab58a27a39e4f1a83106
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:27:01 +0000

    Bug 1164632 - use new worker helpers in debugger for pretty-printing r=jsantell
    UltraBlame original commit: 7235089b51914e4196de689a16fd9ee9060fbc0c

--- toolkit/devtools/shared/worker-helper.js
+++ toolkit/devtools/shared/worker-helper.js
@@ -1,100 +1,112 @@
 
 
 
-"use strict";
+(function (root, factory) {
+  "use strict";
 
+  if (typeof define === "function" && define.amd) {
+    define(factory);
+  } else if (typeof exports === "object") {
+    module.exports = factory();
+  } else {
+    root.workerHelper = factory();
+  }
+}(this, function () {
+  "use strict";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function createTask (self, name, fn) {
   
-  if (!self._tasks) {
-    self._tasks = {};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+  function createTask (self, name, fn) {
+    
+    if (!self._tasks) {
+      self._tasks = {};
+    }
+
+    
+    if (!self.onmessage) {
+      self.onmessage = createHandler(self);
+    }
+
+    
+    self._tasks[name] = fn;
   }
 
   
-  if (!self.onmessage) {
-    self.onmessage = createHandler(self);
+
+
+
+
+
+  function createHandler (self) {
+    return function (e) {
+      let { id, task, data } = e.data;
+      let taskFn = self._tasks[task];
+
+      if (!taskFn) {
+        self.postMessage({ id, error: `Task "${task}" not found in worker.` });
+        return;
+      }
+
+      try {
+        let results;
+        handleResponse(taskFn(data));
+      } catch (e) {
+        handleError(e);
+      }
+
+      function handleResponse (response) {
+        
+        if (response && typeof response.then === "function") {
+          response.then(val => self.postMessage({ id, response: val }), handleError);
+        }
+        
+        else if (response instanceof Error) {
+          handleError(response);
+        }
+        
+        else {
+          self.postMessage({ id, response });
+        }
+      }
+
+      function handleError (e="Error") {
+        self.postMessage({ id, error: e.message || e });
+      }
+    }
   }
 
-  
-  self._tasks[name] = fn;
-}
-
-exports.createTask = createTask;
-
-
-
-
-
-
-
-function createHandler (self) {
-  return function (e) {
-    let { id, task, data } = e.data;
-    let taskFn = self._tasks[task];
-
-    if (!taskFn) {
-      self.postMessage({ id, error: `Task "${task}" not found in worker.` });
-      return;
-    }
-
-    try {
-      let results;
-      handleResponse(taskFn(data));
-    } catch (e) {
-      handleError(e);
-    }
-
-    function handleResponse (response) {
-      
-      if (response && typeof response.then === "function") {
-        response.then(val => self.postMessage({ id, response: val }), handleError);
-      }
-      
-      else if (response instanceof Error) {
-        handleError(response);
-      }
-      
-      else {
-        self.postMessage({ id, response });
-      }
-    }
-
-    function handleError (e="Error") {
-      self.postMessage({ id, error: e.message || e });
-    }
-  }
-}
+  return { createTask: createTask };
+}.bind(this)));
