a25ff284a6e09e0e26bfc63906b132fe6b97bd0b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:08:51 +0000

    Bug 1131581: move screensharing options into a dropdown anchored to the screenshare toolbar button. r=Standard8
    UltraBlame original commit: 2737d0c349229e0404135038648720a6db10ea96

--- browser/components/loop/content/shared/js/mixins.js
+++ browser/components/loop/content/shared/js/mixins.js
@@ -101,6 +101,31 @@ loop.shared.mixins = (function() {
     componentDidMount: function() {
       this.documentBody.addEventListener("click", this._onBodyClick);
       this.documentBody.addEventListener("blur", this.hideDropdownMenu);
+
+      var menu = this.refs.menu;
+      if (!menu) {
+        return;
+      }
+
+      
+      var menuNode = menu.getDOMNode();
+      var menuNodeRect = menuNode.getBoundingClientRect();
+      var bodyRect = {
+        height: this.documentBody.offsetHeight,
+        width: this.documentBody.offsetWidth
+      };
+
+      
+      var y = menuNodeRect.top + menuNodeRect.height;
+      if (y >= bodyRect.height) {
+        menuNode.style.marginTop = bodyRect.height - y + "px";
+      }
+
+      
+      var x = menuNodeRect.left + menuNodeRect.width;
+      if (x >= bodyRect.width) {
+        menuNode.style.marginLeft = bodyRect.width - x + "px";
+      }
     },
 
     componentWillUnmount: function() {
