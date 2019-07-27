a73eb4ef4897de57479eb00d1696f0fba3a27220
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:32:28 +0000

    Merge m-c to inbound a=merge
    UltraBlame original commit: 21aa17588defe40c117cd4a47ad8d91008a720f6

--- js/src/jscntxt.cpp
+++ js/src/jscntxt.cpp
@@ -229,6 +229,13 @@ ReportError(JSContext *cx, const char *message, JSErrorReport *reportp,
     if (cx->options().autoJSAPIOwnsErrorReporting() || JS_IsRunning(cx)) {
         if (ErrorToException(cx, message, reportp, callback, userRef))
             return;
+
+        
+
+
+
+        if (cx->options().autoJSAPIOwnsErrorReporting())
+            return;
     }
 
     
