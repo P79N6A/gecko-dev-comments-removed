fcebe71b70988755434997fdd82b96bcc7f51ccf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:15:30 +0000

    Backed out changeset 851347e26940 (bug 1038839) for spidermonkey failures CLOSED TREE
    UltraBlame original commit: 4bdf782dda8221f5eb626f0e9c7b2f0f6698104c

--- js/src/vm/TypeInference.h
+++ js/src/vm/TypeInference.h
@@ -492,8 +492,6 @@ class TypeSet
         return this->isSubset(other) && other->isSubset(this);
     }
 
-    bool objectsIntersect(const TypeSet *other) const;
-
     
     bool addTypesToConstraint(JSContext *cx, TypeConstraint *constraint);
 
