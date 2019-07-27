fcf17321c0f6d7001101a14fc559c66eaf168c01
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:29:16 +0000

    Bug 1024668 - Extract core logic of abouthealth.js into new WebIDL interface. r=bz,gps,wchen
    UltraBlame original commit: e620a9b140faf4aecf9ade69c689861feddd750c

---
+++ browser/components/selfsupport/SelfSupportService.js
@@ -0,0 +1,74 @@
+
+
+
+
+"use strict";
+
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+const policy = Cc["@mozilla.org/datareporting/service;1"]
+                 .getService(Ci.nsISupports)
+                 .wrappedJSObject
+                 .policy;
+
+XPCOMUtils.defineLazyGetter(this, "reporter", () => {
+  return Cc["@mozilla.org/datareporting/service;1"]
+           .getService(Ci.nsISupports)
+           .wrappedJSObject
+           .healthReporter;
+});
+
+function MozSelfSupportInterface() {
+}
+
+MozSelfSupportInterface.prototype = {
+  classDescription: "MozSelfSupport",
+  classID: Components.ID("{d30aae8b-f352-4de3-b936-bb9d875df0bb}"),
+  contractID: "@mozilla.org/mozselfsupport;1",
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIDOMGlobalPropertyInitializer,
+                                         Ci.nsIObserver]),
+
+  _window: null,
+
+  init: function (window) {
+    this._window = window;
+
+    
+    
+    
+    return window.MozSelfSupportImpl._create(this._window, this);
+  },
+
+  get healthReportDataSubmissionEnabled() {
+    return policy.healthReportUploadEnabled;
+  },
+
+  set healthReportDataSubmissionEnabled(enabled) {
+    let reason = "Self-support interface sent " +
+                 (enabled ? "opt-in" : "opt-out") +
+                 " command.";
+    policy.recordHealthReportUploadEnabled(enabled, reason);
+  },
+
+  getHealthReportPayload: function () {
+    return new this._window.Promise(function (aResolve, aReject) {
+      if (reporter) {
+        let resolvePayload = function () {
+          reporter.collectAndObtainJSONPayload(true).then(aResolve, aReject);
+        };
+
+        if (reporter.initialized) {
+          resolvePayload();
+        } else {
+          reporter.onInit().then(resolvePayload, aReject);
+        }
+      } else {
+        aReject(new Error("No reporter"));
+      }
+    }.bind(this));
+  },
+}
+
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([MozSelfSupportInterface]);
