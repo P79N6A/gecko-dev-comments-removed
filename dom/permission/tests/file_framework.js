4490ab12435fc6166b1d5b4c4f31e7b7fe7c8de6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:48:21 +0000

    Bug 1065185 - Fix permissions tests. r=bz
    UltraBlame original commit: cc213ad991a8e16efc44689af8d060acda9d0f10

--- dom/permission/tests/file_framework.js
+++ dom/permission/tests/file_framework.js
@@ -168,11 +168,9 @@ function expandPermissions(aPerms) {
   var perms = [];
   aPerms.forEach(function(el) {
     var access = permTable[el].access ? "readwrite" : null;
-    var expanded = SpecialPowers.unwrap(expand(el, access));
-    
-    
+    var expanded = expand(el, access);
     for (let i = 0; i < expanded.length; i++) {
-      perms.push(expanded[i]);
+      perms.push(SpecialPowers.unwrap(expanded[i]));
     }
   });
 
