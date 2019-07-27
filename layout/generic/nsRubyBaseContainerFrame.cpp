0a3a8a0deee8a67a65bf692b6d34ac1114811d9b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:43:49 +0000

    Bug 1121738 - Ruby base container frames should not support CSS transforms.  r=xidorn
    Following bug 1055667 patch 5, nsRubyBaseContainerFrame is the only ruby
    frame type with this bug.  It should not support CSS transforms given
    the spec wording in
    http://dev.w3.org/csswg/css-transforms/#transformable-element
    
    UltraBlame original commit: 1b19f19def53410ba887bdf144edb6fa74873e14

--- layout/generic/nsRubyBaseContainerFrame.cpp
+++ layout/generic/nsRubyBaseContainerFrame.cpp
@@ -265,7 +265,10 @@ nsRubyBaseContainerFrame::AddInlinePrefISize(
  bool 
 nsRubyBaseContainerFrame::IsFrameOfType(uint32_t aFlags) const 
 {
-  return nsContainerFrame::IsFrameOfType(aFlags & 
+  if (aFlags & eSupportsCSSTransforms) {
+    return false;
+  }
+  return nsContainerFrame::IsFrameOfType(aFlags &
          ~(nsIFrame::eLineParticipant));
 }
 
