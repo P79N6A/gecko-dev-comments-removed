f0f3bf1b66fa99e67f613d2fb752a154c6f59fba
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:59:41 +0000

    Bug 1022181 - Mobile ID Tests. Part 2: Mobile Identity Service Tests. r=jedp
    UltraBlame original commit: 05e7b540b5c1d31be38c23c8388f959dec34c789

---
+++ services/mobileid/tests/xpcshell/head.js
@@ -0,0 +1,16 @@
+
+
+
+const {classes: Cc, interfaces: Ci, results: Cr, utils: Cu} = Components;
+
+"use strict";
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+(function initMobileIdTestingInfrastructure() {
+  do_get_profile();
+
+  Services.prefs.setCharPref("services.mobileid.loglevel", "Debug");
+
+}).call(this);
