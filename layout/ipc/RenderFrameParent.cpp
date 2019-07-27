a734307e8ce369201fd5745a7032e8b3c5e509b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:06:47 +0000

    Bug 1085655 - Improve TabChild::InitRenderingState() r=bent
    UltraBlame original commit: cabc3b75da324bfafe71b451f0117ad44fc67d20

--- layout/ipc/RenderFrameParent.cpp
+++ layout/ipc/RenderFrameParent.cpp
@@ -565,6 +565,19 @@ RenderFrameParent::HitTest(const nsRect& aRect)
   return mTouchRegion.Contains(aRect);
 }
 
+void
+RenderFrameParent::GetTextureFactoryIdentifier(TextureFactoryIdentifier* aTextureFactoryIdentifier)
+{
+  nsRefPtr<LayerManager> lm = GetFrom(mFrameLoader);
+  
+  if (lm && lm->GetBackendType() == LayersBackend::LAYERS_CLIENT) {
+    *aTextureFactoryIdentifier =
+      static_cast<ClientLayerManager*>(lm.get())->GetTextureFactoryIdentifier();
+  } else {
+    *aTextureFactoryIdentifier = TextureFactoryIdentifier();
+  }
+}
+
 }  
 }  
 
