2cf377cf49821a86a9120dd00a807a42e848bd59
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:34:52 +0000

    Bug 1162330 - Prepare satchel mochitests for e10s and turn a few of them on. r=MattN
    UltraBlame original commit: 13874e7fbfdaf4c9c3a0dfd5ec110fc41f7f2aa4

--- toolkit/components/satchel/test/satchel_common.js
+++ toolkit/components/satchel/test/satchel_common.js
@@ -3,6 +3,8 @@
 
 
 var Services = SpecialPowers.Services;
+var gPopupShownListener;
+var gLastAutoCompleteResults;
 
 
 
@@ -53,33 +55,36 @@ function doKey(aKey, modifier) {
     wutils.sendKeyEvent("keyup",    key, 0, modifier);
 }
 
-
-function getAutocompletePopup() {
-    var Ci = SpecialPowers.Ci;
-    chromeWin = SpecialPowers.wrap(window)
-                    .QueryInterface(Ci.nsIInterfaceRequestor)
-                    .getInterface(Ci.nsIWebNavigation)
-                    .QueryInterface(Ci.nsIDocShellTreeItem)
-                    .rootTreeItem
-                    .QueryInterface(Ci.nsIInterfaceRequestor)
-                    .getInterface(Ci.nsIDOMWindow)
-                    .QueryInterface(Ci.nsIDOMChromeWindow);
-    autocompleteMenu = chromeWin.document.getElementById("PopupAutoComplete");
-    ok(autocompleteMenu, "Got autocomplete popup");
-
-    return autocompleteMenu;
+function registerPopupShownListener(listener) {
+  if (gPopupShownListener) {
+    ok(false, "got too many popupshownlisteners");
+    return;
+  }
+  gPopupShownListener = listener;
 }
 
+function getMenuEntries() {
+  if (!gLastAutoCompleteResults) {
+    throw new Error("no autocomplete results");
+  }
 
-function cleanUpFormHist() {
-  SpecialPowers.formHistory.update({ op : "remove" });
+  var results = gLastAutoCompleteResults;
+  gLastAutoCompleteResults = null;
+  return results;
 }
-cleanUpFormHist();
-
 
 var checkObserver = {
   verifyStack: [],
   callback: null,
+
+  init() {
+    script.sendAsyncMessage("addObserver");
+    script.addMessageListener("satchel-storage-changed", this.observe.bind(this));
+  },
+
+  uninit() {
+    script.sendAsyncMessage("removeObserver");
+  },
 
   waitForChecks: function(callback) {
     if (this.verifyStack.length == 0)
@@ -88,7 +93,7 @@ var checkObserver = {
       this.callback = callback;
   },
 
-  observe: function(subject, topic, data) {
+  observe: function({ subject, topic, data }) {
     if (data != "formhistory-add" && data != "formhistory-update")
       return;
     ok(this.verifyStack.length > 0, "checking if saved form data was expected");
@@ -120,6 +125,22 @@ function checkForSave(name, value, message) {
   checkObserver.verifyStack.push({ name : name, value: value, message: message });
 }
 
+function NonE10SgetAutocompletePopup() {
+  var Ci = SpecialPowers.Ci;
+  chromeWin = SpecialPowers.wrap(window)
+                .QueryInterface(Ci.nsIInterfaceRequestor)
+                .getInterface(Ci.nsIWebNavigation)
+                .QueryInterface(Ci.nsIDocShellTreeItem)
+                .rootTreeItem
+                .QueryInterface(Ci.nsIInterfaceRequestor)
+                .getInterface(Ci.nsIDOMWindow)
+                .QueryInterface(Ci.nsIDOMChromeWindow);
+  autocompleteMenu = chromeWin.document.getElementById("PopupAutoComplete");
+  ok(autocompleteMenu, "Got autocomplete popup");
+
+  return autocompleteMenu;
+}
+
 
 function getFormSubmitButton(formNum) {
   var form = $("form" + formNum); 
@@ -137,28 +158,54 @@ function getFormSubmitButton(formNum) {
 
 
 function countEntries(name, value, then) {
-  var obj = {};
-  if (name !== null)
-    obj.fieldname = name;
-  if (value !== null)
-    obj.value = value;
+  script.sendAsyncMessage("countEntries", { name, value });
+  script.addMessageListener("entriesCounted", function counted(data) {
+    script.removeMessageListener("entriesCounted", counted);
+    if (!data.ok) {
+      ok(false, "Error occurred counting form history");
+      SimpleTest.finish();
+      return;
+    }
 
-  var count = 0;
-  SpecialPowers.formHistory.count(obj, SpecialPowers.wrapCallbackObject({ handleResult: function (result) { count = result },
-                                         handleError: function (error) {
-                                           ok(false, "Error occurred searching form history: " + error.message);
-                                           SimpleTest.finish();
-                                         },
-                                         handleCompletion: function (reason) { if (!reason) then(count); }
-                                       }));
+    then(data.count);
+  });
 }
 
 
 function updateFormHistory(changes, then) {
-  SpecialPowers.formHistory.update(changes, SpecialPowers.wrapCallbackObject({ handleError: function (error) {
-                                                ok(false, "Error occurred updating form history: " + error.message);
-                                                SimpleTest.finish();
-                                              },
-                                              handleCompletion: function (reason) { if (!reason) then(); },
-                                            }));
+  script.sendAsyncMessage("updateFormHistory", { changes });
+  script.addMessageListener("formHistoryUpdated", function updated({ ok }) {
+    script.removeMessageListener("formHistoryUpdated", updated);
+    if (!ok) {
+      ok(false, "Error occurred updating form history");
+      SimpleTest.finish();
+      return;
+    }
+
+    then();
+  });
 }
+
+function notifyMenuChanged(expectedCount, expectedFirstValue, then) {
+  script.sendAsyncMessage("waitForMenuChange",
+                          { expectedCount,
+                            expectedFirstValue });
+  script.addMessageListener("gotMenuChange", function changed({ results }) {
+    script.removeMessageListener("gotMenuChange", changed);
+    gLastAutoCompleteResults = results;
+    then();
+  });
+}
+
+var chromeURL = SimpleTest.getTestFileURL("parent_utils.js");
+var script = SpecialPowers.loadChromeScript(chromeURL);
+script.addMessageListener("onpopupshown", ({ results }) => {
+  gLastAutoCompleteResults = results;
+  if (gPopupShownListener)
+    gPopupShownListener();
+});
+
+SimpleTest.registerCleanupFunction(() => {
+  script.sendAsyncMessage("cleanup");
+  script.destroy();
+});
