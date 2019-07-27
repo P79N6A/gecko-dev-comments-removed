91aa82e60db5e0447ee075f6840bd2c005851845
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:20:50 +0000

    Bug 1162199 - Use unboxed objects by default, r=jandem.
    UltraBlame original commit: 356231081116c8e31d76fe7719283f82ed4b8f10

--- js/src/vm/NativeObject.cpp
+++ js/src/vm/NativeObject.cpp
@@ -357,6 +357,8 @@ NativeObject::setLastPropertyMakeNative(ExclusiveContext* cx, Shape* shape)
     size_t oldSpan = shape->numFixedSlots();
     size_t newSpan = shape->slotSpan();
 
+    initializeSlotRange(0, oldSpan);
+
     
     
     if (oldSpan != newSpan && !updateSlotsForSpan(cx, oldSpan, newSpan))
