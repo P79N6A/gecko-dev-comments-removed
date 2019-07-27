2ec42ad2b60f86325e13bfac71f3a5c7e9ab30a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:22:40 +0000

    Bug 1016825 - Show legacy-Sync-to-Firefox-Accounts migration notifications in the main browser window. r=markh
    UltraBlame original commit: 1f86aebdcc2e86d32674ab9d09f047418e68656e

--- browser/base/content/test/general/browser_fxa_migrate.js
+++ browser/base/content/test/general/browser_fxa_migrate.js
@@ -2,6 +2,7 @@
 
 
 const STATE_CHANGED_TOPIC = "fxa-migration:state-changed";
+const NOTIFICATION_TITLE = "fxa-migration";
 
 let imports = {};
 Cu.import("resource://services-sync/FxaMigrator.jsm", imports);
@@ -9,30 +10,42 @@ Cu.import("resource://services-sync/FxaMigrator.jsm", imports);
 add_task(function* test() {
   
   let buttonPromise = promiseButtonMutation();
-  Services.obs.notifyObservers(null, "fxa-migration:state-changed",
+  Services.obs.notifyObservers(null, STATE_CHANGED_TOPIC,
                                fxaMigrator.STATE_USER_FXA);
   let buttonState = yield buttonPromise;
   assertButtonState(buttonState, "migrate-signup", true);
+  Assert.ok(Weave.Notifications.notifications.some(n => {
+    return n.title == NOTIFICATION_TITLE;
+  }), "Needs-user notification should be present");
 
   
   buttonPromise = promiseButtonMutation();
   let email = Cc["@mozilla.org/supports-string;1"].
               createInstance(Ci.nsISupportsString);
   email.data = "foo@example.com";
-  Services.obs.notifyObservers(email, "fxa-migration:state-changed",
+  Services.obs.notifyObservers(email, STATE_CHANGED_TOPIC,
                                fxaMigrator.STATE_USER_FXA_VERIFIED);
   buttonState = yield buttonPromise;
   assertButtonState(buttonState, "migrate-verify", true,
                     "foo@example.com not verified");
+  let note = Weave.Notifications.notifications.find(n => {
+    return n.title == NOTIFICATION_TITLE;
+  });
+  Assert.ok(!!note, "Needs-verification notification should be present");
+  Assert.ok(note.description.contains(email.data),
+            "Needs-verification notification should include email");
 
   
   buttonPromise = promiseButtonMutation();
-  Services.obs.notifyObservers(null, "fxa-migration:state-changed", null);
+  Services.obs.notifyObservers(null, STATE_CHANGED_TOPIC, null);
   buttonState = yield buttonPromise;
   
   
   
   assertButtonState(buttonState, "", true);
+  Assert.ok(!Weave.Notifications.notifications.some(n => {
+    return n.title == NOTIFICATION_TITLE;
+  }), "Migration notifications should no longer be present");
 });
 
 function assertButtonState(buttonState, expectedStatus, expectedVisible,
