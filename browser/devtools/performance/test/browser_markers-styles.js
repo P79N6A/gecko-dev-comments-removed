93661487ab263e5ade3e1ead8f803187a2fd9a57
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:22:59 +0000

    Bug 1164338 - Tests that the style markers have restyleHints. r=pbrosset
    UltraBlame original commit: 0787f6ae2337b810f30579dfed919df063793035

---
+++ browser/devtools/performance/test/browser_markers-styles.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+function* spawnTest () {
+  let { target, front } = yield initBackend(SIMPLE_URL);
+  let markers = [];
+
+  front.on("timeline-data", handler);
+  let model = yield front.startRecording({ withTicks: true });
+
+  yield waitUntil(() => {
+    return markers.some(({restyleHint}) => restyleHint != void 0);
+  });
+
+  front.off("timeline-data", handler);
+  yield front.stopRecording(model);
+
+  info(`Got ${markers.length} markers.`);
+
+  ok(markers.every(({name}) => name === "Styles"), "All markers found are Styles markers");
+  ok(markers.length, "found some restyle markers");
+
+  ok(markers.some(({restyleHint}) => restyleHint != void 0), "some markers have a restyleHint property");
+
+  yield removeTab(target.tab);
+  finish();
+
+  function handler (_, name, m) {
+    if (name === "markers") {
+      markers = markers.concat(m.filter(marker => marker.name === "Styles"));
+    }
+  }
+}
