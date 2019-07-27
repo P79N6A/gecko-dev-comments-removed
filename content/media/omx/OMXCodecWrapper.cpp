e14c144186af2af93b16c9e8d3781fc8f8b10590
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:27:50 +0000

    Bug 997593 - Handle error from OMXCodecWrapper in OmxVideoTrackEncoder::GetEncodedTrack(). r=roc
    UltraBlame original commit: 88d0ae140cc128bd5c795fcc072ac1c6a8960d98

--- content/media/omx/OMXCodecWrapper.cpp
+++ content/media/omx/OMXCodecWrapper.cpp
@@ -417,6 +417,10 @@ OMXVideoEncoder::Encode(const Image* aImage, int aWidth, int aHeight,
   
   uint32_t index;
   result = mCodec->dequeueInputBuffer(&index, INPUT_BUFFER_TIMEOUT_US);
+  if (result == -EAGAIN) {
+    
+    return NS_OK;
+  }
   NS_ENSURE_TRUE(result == OK, NS_ERROR_FAILURE);
 
   const sp<ABuffer>& inBuf = mInputBufs.itemAt(index);
