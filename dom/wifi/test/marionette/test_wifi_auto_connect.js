845fa80fdc97708a9d4c5cac8e91a6422d51c2c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:50:49 +0000

    Bug 1166660 - Use custom hostapd for testing and restart the hostapd after re-enabling wifi. r=vchang.
    There are a couple of reasons which make this test case failed on KK emulator.
    The first one is the stock hostapd is no longer available for unknown reason
    and the second one is that the the scan behavior is weird after re-enabling wifi.
    There is no perfect solution for these two issue. For testing purpose,
    we can use the custom hostapd instead of stock hostapd and restart the hostapd
    after re-enabling wifi. The test coverage remains the same after applying
    these changes.
    
    UltraBlame original commit: ad7d3f3e62b283e3067310801ba9036fbc892e38

--- dom/wifi/test/marionette/test_wifi_auto_connect.js
+++ dom/wifi/test/marionette/test_wifi_auto_connect.js
@@ -4,15 +4,41 @@
 MARIONETTE_TIMEOUT = 60000;
 MARIONETTE_HEAD_JS = 'head.js';
 
-gTestSuite.doTest(function() {
+const TESTING_HOSTAPD = [{ ssid: 'ap0' }];
+
+gTestSuite.doTestWithoutStockAp(function() {
   let firstNetwork;
   return gTestSuite.ensureWifiEnabled(true)
+    
+    .then(() => gTestSuite.startHostapds(TESTING_HOSTAPD))
+    .then(() => gTestSuite.verifyNumOfProcesses('hostapd', TESTING_HOSTAPD.length))
+
+    
     .then(gTestSuite.requestWifiScan)
     .then(function(networks) {
       firstNetwork = networks[0];
       return gTestSuite.testAssociate(firstNetwork);
     })
+
+    
+    
+
+    
     .then(() => gTestSuite.requestWifiEnabled(false))
+    .then(gTestSuite.killAllHostapd)
+    .then(() => gTestSuite.verifyNumOfProcesses('hostapd', 0))
+
+    
     .then(() => gTestSuite.requestWifiEnabled(true))
-    .then(() => gTestSuite.waitForConnected(firstNetwork));
+
+    
+    .then(() => gTestSuite.startHostapds(TESTING_HOSTAPD))
+    .then(() => gTestSuite.verifyNumOfProcesses('hostapd', TESTING_HOSTAPD.length))
+
+    
+    .then(() => gTestSuite.waitForConnected(firstNetwork))
+
+    
+    .then(gTestSuite.killAllHostapd)
+    .then(() => gTestSuite.verifyNumOfProcesses('hostapd', 0))
 });
