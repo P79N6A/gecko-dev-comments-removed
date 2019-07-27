6690ba4844e6837e677f76c78e771995c3b0a509
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:05:39 +0000

    Bug 1113474 - Keep MediaKeys alive until it has resolved all of its stored promises - r=cpearce
    UltraBlame original commit: e3b66bc377064b06a8484b04270778b210638e0e

--- dom/media/eme/MediaKeys.cpp
+++ dom/media/eme/MediaKeys.cpp
@@ -169,6 +169,11 @@ MediaKeys::StorePromise(Promise* aPromise)
   static uint32_t sEMEPromiseCount = 1;
   MOZ_ASSERT(aPromise);
   uint32_t id = sEMEPromiseCount++;
+
+  
+  
+  AddRef();
+
   mPromises.Put(id, aPromise);
   return id;
 }
@@ -179,6 +184,7 @@ MediaKeys::RetrievePromise(PromiseId aId)
   MOZ_ASSERT(mPromises.Contains(aId));
   nsRefPtr<Promise> promise;
   mPromises.Remove(aId, getter_AddRefs(promise));
+  Release();
   return promise.forget();
 }
 
