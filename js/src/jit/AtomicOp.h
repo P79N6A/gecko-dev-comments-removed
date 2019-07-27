839ce8d1ab4bb1151ab5dd92403e128c818fbce2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:13:38 +0000

    Bug 1102820 - Use MOZ_CONSTEXPR appropriately.  r=mh+mozilla
    UltraBlame original commit: 76097ee6d6f9630e109616d413daf560aeeb8312

--- js/src/jit/AtomicOp.h
+++ js/src/jit/AtomicOp.h
@@ -39,33 +39,33 @@ enum MemoryBarrierBits {
     MembarAllbits = 31,
 };
 
-inline MemoryBarrierBits
+static inline MOZ_CONSTEXPR MemoryBarrierBits
 operator|(MemoryBarrierBits a, MemoryBarrierBits b)
 {
     return MemoryBarrierBits(int(a) | int(b));
 }
 
-inline MemoryBarrierBits
+static inline MOZ_CONSTEXPR MemoryBarrierBits
 operator&(MemoryBarrierBits a, MemoryBarrierBits b)
 {
     return MemoryBarrierBits(int(a) & int(b));
 }
 
-inline MemoryBarrierBits
+static inline MOZ_CONSTEXPR MemoryBarrierBits
 operator~(MemoryBarrierBits a)
 {
     return MemoryBarrierBits(~int(a));
 }
 
 
-static const MemoryBarrierBits MembarFull = MembarLoadLoad|MembarLoadStore|MembarStoreLoad|MembarStoreStore;
+static MOZ_CONSTEXPR_VAR MemoryBarrierBits MembarFull = MembarLoadLoad|MembarLoadStore|MembarStoreLoad|MembarStoreStore;
 
 
 
-static const MemoryBarrierBits MembarBeforeLoad = MembarNobits;
-static const MemoryBarrierBits MembarAfterLoad = MembarLoadLoad|MembarLoadStore;
-static const MemoryBarrierBits MembarBeforeStore = MembarStoreStore;
-static const MemoryBarrierBits MembarAfterStore = MembarStoreLoad;
+static MOZ_CONSTEXPR_VAR MemoryBarrierBits MembarBeforeLoad = MembarNobits;
+static MOZ_CONSTEXPR_VAR MemoryBarrierBits MembarAfterLoad = MembarLoadLoad|MembarLoadStore;
+static MOZ_CONSTEXPR_VAR MemoryBarrierBits MembarBeforeStore = MembarStoreStore;
+static MOZ_CONSTEXPR_VAR MemoryBarrierBits MembarAfterStore = MembarStoreLoad;
 
 } 
 } 
