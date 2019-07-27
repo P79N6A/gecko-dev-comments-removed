8ead57574c566e903f3b46cd2c8e7827455977f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:00:17 +0000

    Bug 1055259 - Correctly initialize SharedTextureClientOGL::mIsLocked r=nical
    UltraBlame original commit: 0a95764bbef66153fa275b68291fe4db51526fae

--- gfx/layers/opengl/TextureClientOGL.cpp
+++ gfx/layers/opengl/TextureClientOGL.cpp
@@ -21,6 +21,7 @@ SharedTextureClientOGL::SharedTextureClientOGL(TextureFlags aFlags)
   : TextureClient(aFlags)
   , mHandle(0)
   , mInverted(false)
+  , mIsLocked(false)
 {
   
   mFlags |= TextureFlags::DEALLOCATE_CLIENT;
