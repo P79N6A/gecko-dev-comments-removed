0898c26b68e89e13911703183a678ea7bdece826
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:23:55 +0000

    Bug 1141386 - Test case for gmp-clearkey's base64 key/keyId decoding. r=edwin
    UltraBlame original commit: c2de53dd52d40ef6675ddbb232aae5902f2967c4

---
+++ media/gmp-clearkey/0.1/ClearKeyBase64.h
@@ -0,0 +1,19 @@
+
+
+
+
+#ifndef __ClearKeyBase64_h__
+#define __ClearKeyBase64_h__
+
+#include <vector>
+#include <string>
+#include <stdint.h>
+
+
+
+
+
+bool
+DecodeBase64KeyOrId(const std::string& aEncoded, std::vector<uint8_t>& aOutDecoded);
+
+#endif
--- media/gmp-clearkey/0.1/ClearKeyUtils.cpp
+++ media/gmp-clearkey/0.1/ClearKeyUtils.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include "ClearKeyUtils.h"
+#include "ClearKeyBase64.h"
 #include "mozilla/ArrayUtils.h"
 #include "mozilla/Assertions.h"
 #include "mozilla/Endian.h"
@@ -360,70 +361,6 @@ GetNextLabel(ParserContext& aCtx, string& aOutLabel)
   }
 
   return false;
-}
-
-
-
-
-
-static bool
-Decode6Bit(string& aStr)
-{
-  for (size_t i = 0; i < aStr.length(); i++) {
-    if (aStr[i] >= 'A' && aStr[i] <= 'Z') {
-      aStr[i] -= 'A';
-    } else if (aStr[i] >= 'a' && aStr[i] <= 'z') {
-      aStr[i] -= 'a' - 26;
-    } else if (aStr[i] >= '0' && aStr[i] <= '9') {
-      aStr[i] -= '0' - 52;
-    } else if (aStr[i] == '-' || aStr[i] == '+') {
-      aStr[i] = 62;
-    } else if (aStr[i] == '_' || aStr[i] == '/') {
-      aStr[i] = 63;
-    } else {
-      
-      if (aStr[i] != '=') {
-        aStr.erase(i, string::npos);
-        return false;
-      }
-      aStr[i] = '\0';
-      aStr.resize(i);
-      break;
-    }
-  }
-
-  return true;
-}
-
-static bool
-DecodeBase64KeyOrId(string& aEncoded, vector<uint8_t>& aOutDecoded)
-{
-  if (!Decode6Bit(aEncoded) ||
-      aEncoded.size() != 22) { 
-    return false;
-  }
-
-  
-  int shift = 0;
-
-  aOutDecoded.resize(16);
-  vector<uint8_t>::iterator out = aOutDecoded.begin();
-  for (size_t i = 0; i < aEncoded.length(); i++) {
-    if (!shift) {
-      *out = aEncoded[i] << 2;
-    } else {
-      *out |= aEncoded[i] >> (6 - shift);
-      out++;
-      if (out == aOutDecoded.end()) {
-        
-        break;
-      }
-      *out = aEncoded[i] << (shift + 2);
-    }
-    shift = (shift + 2) % 8;
-  }
-
-  return true;
 }
 
 static bool
