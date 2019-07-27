1e082ad467e5918bcdb625be038c3f5837f336f6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:04:50 +0000

    Bug 1154325 P1 Add convenience routines to bind and get Blobs as strings. r=asuth
    UltraBlame original commit: 0ee4a1086547d150ec6bfd1c0ecd7be8e6ff6a2f

--- storage/src/mozStoragePrivateHelpers.h
+++ storage/src/mozStoragePrivateHelpers.h
@@ -88,6 +88,54 @@ already_AddRefed<nsIRunnable> newCompletionEvent(
   mozIStorageCompletionCallback *aCallback
 );
 
+
+
+
+
+template<class T, class V>
+nsresult
+DoGetBlobAsString(T* aThis, uint32_t aIndex, V& aValue)
+{
+  typedef typename V::char_type char_type;
+
+  uint32_t size;
+  char_type* blob;
+  nsresult rv =
+    aThis->GetBlob(aIndex, &size, reinterpret_cast<uint8_t**>(&blob));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  aValue.Adopt(blob, size / sizeof(char_type));
+  return NS_OK;
+}
+
+
+
+
+
+template<class T, class V>
+nsresult
+DoBindStringAsBlobByName(T* aThis, const nsACString& aName, const V& aValue)
+{
+  typedef typename V::char_type char_type;
+  return aThis->BindBlobByName(aName,
+                        reinterpret_cast<const uint8_t*>(aValue.BeginReading()),
+                        aValue.Length() * sizeof(char_type));
+}
+
+
+
+
+
+template<class T, class V>
+nsresult
+DoBindStringAsBlobByIndex(T* aThis, uint32_t aIndex, const V& aValue)
+{
+  typedef typename V::char_type char_type;
+  return aThis->BindBlobByIndex(aIndex,
+                        reinterpret_cast<const uint8_t*>(aValue.BeginReading()),
+                        aValue.Length() * sizeof(char_type));
+}
+
 } 
 } 
 
