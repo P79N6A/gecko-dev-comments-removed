d82a18b5e92cffb25d6036a6d68f5b035ccd0da6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:02:55 +0000

    Backed out changeset 73b8ce222f9f (bug 1129360) for Android reftest failures.
    UltraBlame original commit: 71c769cf61bbb76c9f7984c20b134d3e6905ab60

--- gfx/layers/client/TiledContentClient.cpp
+++ gfx/layers/client/TiledContentClient.cpp
@@ -1294,7 +1294,6 @@ ClientTiledLayerBuffer::ValidateTile(TileClient aTile,
   tileRegion.SubOut(GetValidRegion());
   tileRegion.SubOut(aDirtyRegion); 
 
-  backBuffer->Unlock();
   backBuffer->SetWaste(tileRegion.Area() * mResolution * mResolution);
 
   if (createdTextureClient) {
