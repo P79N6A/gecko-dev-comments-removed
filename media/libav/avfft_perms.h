383c8cf477c88dd888b6377d08b67eb6ea84517a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:26:24 +0000

    Bug 1157768 - Build files and config headers for libav fft; r=glandium
    UltraBlame original commit: e5e50d1d1e4fbd4eacab2da53c9b4561984ae1ae

---
+++ media/libav/avfft_perms.h
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+#ifndef MOZILLA_AVFFT_PERMS_H
+#define MOZILLA_AVFFT_PERMS_H
+
+#pragma GCC visibility push(default)
+#include "libavcodec/avfft.h"
+#pragma GCC visibility pop
+
+#endif 
