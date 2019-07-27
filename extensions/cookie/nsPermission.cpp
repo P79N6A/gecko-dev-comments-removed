0d0b869f278ac72d6f3d5c86f3732f731f2242ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:29:01 +0000

    Bug 1173523 - Part 1: Expose an nsIPrincipal from nsIPermission rather than a host string, appId, and isInBrowserElement, r=ehsan
    UltraBlame original commit: cf498d466b85edd8067efafa2cffd3f5beb33747

--- extensions/cookie/nsPermission.cpp
+++ extensions/cookie/nsPermission.cpp
@@ -4,48 +4,33 @@
 
 
 #include "nsPermission.h"
+#include "nsContentUtils.h"
 #include "nsIClassInfoImpl.h"
+#include "nsIEffectiveTLDService.h"
 
 
 
 NS_IMPL_CLASSINFO(nsPermission, nullptr, 0, {0})
 NS_IMPL_ISUPPORTS_CI(nsPermission, nsIPermission)
 
-nsPermission::nsPermission(const nsACString &aHost,
-                           uint32_t aAppId,
-                           bool aIsInBrowserElement,
+nsPermission::nsPermission(nsIPrincipal*    aPrincipal,
                            const nsACString &aType,
                            uint32_t         aCapability,
                            uint32_t         aExpireType,
                            int64_t          aExpireTime)
- : mHost(aHost)
+ : mPrincipal(aPrincipal)
  , mType(aType)
  , mCapability(aCapability)
  , mExpireType(aExpireType)
  , mExpireTime(aExpireTime)
- , mAppId(aAppId)
- , mIsInBrowserElement(aIsInBrowserElement)
 {
 }
 
 NS_IMETHODIMP
-nsPermission::GetHost(nsACString &aHost)
+nsPermission::GetPrincipal(nsIPrincipal** aPrincipal)
 {
-  aHost = mHost;
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsPermission::GetAppId(uint32_t* aAppId)
-{
-  *aAppId = mAppId;
-  return NS_OK;
-}
-
-NS_IMETHODIMP
-nsPermission::GetIsInBrowserElement(bool* aIsInBrowserElement)
-{
-  *aIsInBrowserElement = mIsInBrowserElement;
+  nsCOMPtr<nsIPrincipal> copy = mPrincipal;
+  copy.forget(aPrincipal);
   return NS_OK;
 }
 
@@ -75,4 +60,98 @@ nsPermission::GetExpireTime(int64_t *aExpireTime)
 {
   *aExpireTime = mExpireTime;
   return NS_OK;
+}
+
+NS_IMETHODIMP
+nsPermission::Matches(nsIPrincipal* aPrincipal, bool aExactHost, bool* aMatches)
+{
+  NS_ENSURE_ARG_POINTER(aPrincipal);
+  NS_ENSURE_ARG_POINTER(aMatches);
+
+  *aMatches = false;
+
+  
+  if (mPrincipal->Equals(aPrincipal)) {
+    *aMatches = true;
+    return NS_OK;
+  }
+
+  
+  nsAutoCString theirSuffix;
+  nsresult rv = aPrincipal->GetOriginSuffix(theirSuffix);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsAutoCString ourSuffix;
+  rv = mPrincipal->GetOriginSuffix(ourSuffix);
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  if (theirSuffix != ourSuffix) {
+    return NS_OK;
+  }
+
+  
+  nsCOMPtr<nsIURI> theirURI;
+  rv = aPrincipal->GetURI(getter_AddRefs(theirURI));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsCOMPtr<nsIURI> ourURI;
+  rv = mPrincipal->GetURI(getter_AddRefs(ourURI));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  
+  nsAutoCString theirHost;
+  rv = theirURI->GetHost(theirHost);
+  if (NS_FAILED(rv) || theirHost.IsEmpty()) {
+    return NS_OK;
+  }
+
+  nsAutoCString ourHost;
+  rv = ourURI->GetHost(ourHost);
+  if (NS_FAILED(rv) || ourHost.IsEmpty()) {
+    return NS_OK;
+  }
+
+  if (aExactHost) { 
+    *aMatches = theirHost == ourHost;
+    return NS_OK;
+  }
+
+  nsCOMPtr<nsIEffectiveTLDService> tldService =
+    do_GetService(NS_EFFECTIVETLDSERVICE_CONTRACTID);
+  if (!tldService) {
+    NS_ERROR("Should have a tld service!");
+    return NS_ERROR_FAILURE;
+  }
+
+  
+  
+  
+  while (theirHost != ourHost) {
+    rv = tldService->GetNextSubDomain(theirHost, theirHost);
+    if (NS_FAILED(rv)) {
+      if (rv == NS_ERROR_INSUFFICIENT_DOMAIN_LEVELS) {
+        return NS_OK;
+      } else {
+        return rv;
+      }
+    }
+  }
+
+  *aMatches = true;
+  return NS_OK;
+}
+
+NS_IMETHODIMP
+nsPermission::MatchesURI(nsIURI* aURI, bool aExactHost, bool* aMatches)
+{
+  NS_ENSURE_ARG_POINTER(aURI);
+
+  nsIScriptSecurityManager* secMan = nsContentUtils::GetSecurityManager();
+  NS_ENSURE_TRUE(secMan, NS_ERROR_FAILURE);
+
+  nsCOMPtr<nsIPrincipal> principal;
+  nsresult rv = secMan->GetNoAppCodebasePrincipal(aURI, getter_AddRefs(principal));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  return Matches(principal, aExactHost, aMatches);
 }
