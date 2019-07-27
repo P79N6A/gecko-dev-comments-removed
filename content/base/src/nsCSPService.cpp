e374c8a3c7482b8f9e9caa8e94fe183d1ea88d8a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:09:48 +0000

    Backed out 1 changesets (bug 1005225) for Gu permared
    Backed out changeset 452d8502bea1 (bug 1005225)
    * * *
    Backed out 2 changesets (bug 1005225, bug 1030936) for Gu permared
    
    Backed out changeset 32a1fa1b28d5 (bug 1030936)
    Backed out changeset 61d41b381f30 (bug 1005225)
    
    UltraBlame original commit: 28dfed91343651e9958183f4ba9a88037f09985e

--- content/base/src/nsCSPService.cpp
+++ content/base/src/nsCSPService.cpp
@@ -110,6 +110,55 @@ CSPService::ShouldLoad(uint32_t aContentType,
 
   
   
+
+  
+  uint16_t status = nsIPrincipal::APP_STATUS_NOT_INSTALLED;
+  nsAutoCString contentOrigin;
+  aContentLocation->GetPrePath(contentOrigin);
+  if (aRequestPrincipal && !mAppStatusCache.Get(contentOrigin, &status)) {
+    aRequestPrincipal->GetAppStatus(&status);
+    mAppStatusCache.Put(contentOrigin, status);
+  }
+
+  if (status == nsIPrincipal::APP_STATUS_CERTIFIED) {
+    
+    
+    
+    
+    
+    
+
+    switch (aContentType) {
+      case nsIContentPolicy::TYPE_SCRIPT:
+      case nsIContentPolicy::TYPE_STYLESHEET:
+        {
+          nsAutoCString sourceOrigin;
+          aRequestOrigin->GetPrePath(sourceOrigin);
+          if (!sourceOrigin.Equals(contentOrigin)) {
+            *aDecision = nsIContentPolicy::REJECT_SERVER;
+          }
+        }
+        break;
+
+      case nsIContentPolicy::TYPE_OBJECT:
+        *aDecision = nsIContentPolicy::REJECT_SERVER;
+        break;
+
+      default:
+        *aDecision = nsIContentPolicy::ACCEPT;
+    }
+
+    
+    
+    if (*aDecision == nsIContentPolicy::ACCEPT) {
+      return NS_OK;
+    }
+  }
+
+  
+
+  
+  
   nsCOMPtr<nsINode> node(do_QueryInterface(aRequestContext));
   nsCOMPtr<nsIPrincipal> principal;
   nsCOMPtr<nsIContentSecurityPolicy> csp;
@@ -169,7 +218,56 @@ CSPService::ShouldProcess(uint32_t         aContentType,
   if (!aContentLocation)
     return NS_ERROR_FAILURE;
 
+  
   *aDecision = nsIContentPolicy::ACCEPT;
+
+  
+  if (!sCSPEnabled)
+    return NS_OK;
+
+  
+  
+  nsCOMPtr<nsINode> node(do_QueryInterface(aRequestContext));
+  nsCOMPtr<nsIPrincipal> principal;
+  nsCOMPtr<nsIContentSecurityPolicy> csp;
+  if (node) {
+    principal = node->NodePrincipal();
+    principal->GetCsp(getter_AddRefs(csp));
+
+    if (csp) {
+#ifdef PR_LOGGING
+      {
+        uint32_t numPolicies = 0;
+        nsresult rv = csp->GetPolicyCount(&numPolicies);
+        if (NS_SUCCEEDED(rv)) {
+          for (uint32_t i=0; i<numPolicies; i++) {
+            nsAutoString policy;
+            csp->GetPolicy(i, policy);
+            PR_LOG(gCspPRLog, PR_LOG_DEBUG,
+                   ("shouldProcess - document has policy[%d]: %s", i,
+                   NS_ConvertUTF16toUTF8(policy).get()));
+          }
+        }
+      }
+#endif
+      
+      csp->ShouldProcess(aContentType,
+                         aContentLocation,
+                         aRequestOrigin,
+                         aRequestContext,
+                         aMimeTypeGuess,
+                         aExtra,
+                         aDecision);
+    }
+  }
+#ifdef PR_LOGGING
+  else {
+    nsAutoCString uriSpec;
+    aContentLocation->GetSpec(uriSpec);
+    PR_LOG(gCspPRLog, PR_LOG_DEBUG,
+           ("COULD NOT get nsINode for location: %s", uriSpec.get()));
+  }
+#endif
   return NS_OK;
 }
 
