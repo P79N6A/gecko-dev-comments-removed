8f0e7ea17b81a9a50326e341ed983ae9098371fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:21:36 +0000

    Bug 1059001 - Part 5: Reset connection options before connecting. r=paul
    UltraBlame original commit: aa53dedec65f9e30d54a8ddf7f4e319cc809cd9a

--- browser/devtools/webide/modules/app-manager.js
+++ browser/devtools/webide/modules/app-manager.js
@@ -346,6 +346,8 @@ let AppManager = exports.AppManager = {
       this.connection.on(Connection.Events.CONNECTED, onConnectedOrDisconnected);
       this.connection.on(Connection.Events.DISCONNECTED, onConnectedOrDisconnected);
       try {
+        
+        this.connection.resetOptions();
         this.selectedRuntime.connect(this.connection).then(
           () => {},
           deferred.reject.bind(deferred));
