85df6c9775e0866651ea337bddbe64d021c42235
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:55:11 +0000

    Bug 1058243 - (Part 2) Update testAddonManager to open a blank new tab instead of trying to click link in about:addons. r=nalexander
    UltraBlame original commit: 92bcf77b60ed876b8277d5b67526c2d8ad1bc836

--- mobile/android/base/tests/testAddonManager.java
+++ mobile/android/base/tests/testAddonManager.java
@@ -5,13 +5,15 @@ import org.mozilla.gecko.Actions;
 
 import android.util.DisplayMetrics;
 
+
+
+
+
+
+
+
+
 public class testAddonManager extends PixelTest  {
-    
-
-
-
-
-
     public void testAddonManager() {
         Actions.EventExpecter tabEventExpecter;
         Actions.EventExpecter contentEventExpecter;
@@ -44,33 +46,11 @@ public class testAddonManager extends PixelTest  {
         verifyPageTitle(StringHelper.ADDONS_LABEL);
 
         
-        JSONObject jsonPref = new JSONObject();
-        try {
-            jsonPref.put("name", "extensions.getAddons.browseAddons");
-            jsonPref.put("type", "string");
-            jsonPref.put("value", getAbsoluteUrl(StringHelper.ROBOCOP_BLANK_PAGE_01_URL));
-            setPreferenceAndWaitForChange(jsonPref);
-
-        } catch (Exception ex) { 
-            mAsserter.ok(false, "exception in testAddonManager", ex.toString());
-        }
-
-        
-        DisplayMetrics dm = new DisplayMetrics();
-        getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-
-        
-
-
-        float top = mDriver.getGeckoTop() + 25 * dm.density;;
-        float right = mDriver.getGeckoLeft() + mDriver.getGeckoWidth() - 25 * dm.density;;
-
-        
         tabEventExpecter = mActions.expectGeckoEvent("Tab:Added");
         contentEventExpecter = mActions.expectGeckoEvent("DOMContentLoaded");
 
         
-        mSolo.clickOnScreen(right, top);
+        addTab(getAbsoluteUrl(StringHelper.ROBOCOP_BLANK_PAGE_01_URL));
 
         
         tabEventExpecter.blockForEvent();
