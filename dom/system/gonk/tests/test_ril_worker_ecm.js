5ae8a2b47a52ed90a9a3cb55fcf5fcc7062281e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:21:41 +0000

    Bug 1093014 - Part 3: Implement in call MMI. r=hsinyi
    UltraBlame original commit: 65de6bc63a107008be3147d74977fd171a6feff5

--- dom/system/gonk/tests/test_ril_worker_ecm.js
+++ dom/system/gonk/tests/test_ril_worker_ecm.js
@@ -122,9 +122,9 @@ add_test(function test_request_exit_emergencyCbMode_when_dial() {
   };
 
   
-  context.RIL.dialNonEmergencyNumber({number: "0912345678",
-                                      isEmergency: false,
-                                      isDialEmergency: false});
+  context.RIL.dial({number: "0912345678",
+                    isEmergency: false,
+                    isDialEmergency: false});
 
   
   do_check_eq(context.RIL._exitEmergencyCbModeTimeoutID, null);
@@ -167,4 +167,3 @@ add_test(function test_request_exit_emergencyCbMode_explicitly() {
 
   run_next_test();
 });
-
