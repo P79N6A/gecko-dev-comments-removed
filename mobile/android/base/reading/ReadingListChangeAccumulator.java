69531d44a3deca6f65d0a3607354eeb8431e8e0c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:33:54 +0000

    Bug 1148464 - Remove redundant addUploadedRecord method.
    UltraBlame original commit: 688df0133cc83eb623e83f7a5156d5d9dc5b73bf

--- mobile/android/base/reading/ReadingListChangeAccumulator.java
+++ mobile/android/base/reading/ReadingListChangeAccumulator.java
@@ -9,12 +9,12 @@ package org.mozilla.gecko.reading;
 
 
 
-
 public interface ReadingListChangeAccumulator {
   void addDeletion(String guid);
   void addDeletion(ClientReadingListRecord record);
+
+  
   void addChangedRecord(ClientReadingListRecord record);
-  void addUploadedRecord(ClientReadingListRecord up, ServerReadingListRecord down);
   void addDownloadedRecord(ServerReadingListRecord down);
   void finish() throws Exception;
 }
