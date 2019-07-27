2111bd0ff5592eeb7c055ca6e4ee14d10dee912e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:49:03 +0000

    Bug 1031166 - Fix non-unified OS X build's warnings-as-errors in toolkit/components/places. r=mak
    UltraBlame original commit: c09a5006b5f057ae5edfb2a981cca111fe5a9bc9

--- toolkit/components/places/nsNavHistory.cpp
+++ toolkit/components/places/nsNavHistory.cpp
@@ -102,8 +102,6 @@ using namespace mozilla::places;
 
 #define RENEW_CACHED_NOW_TIMEOUT ((int32_t)3 * PR_MSEC_PER_SEC)
 
-static const int64_t USECS_PER_DAY = (int64_t)PR_USEC_PER_SEC * 60 * 60 * 24;
-
 
 #define CHARSET_ANNO NS_LITERAL_CSTRING("URIProperties/characterSet")
 
