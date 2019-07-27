34102c1ffcc2bfe6d9201dfd5b1fc5ede127b021
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:23:01 +0000

    merge mozilla-inbound to mozilla-central a=merge
    UltraBlame original commit: d656ef3c4936c03226eee3f5ed50fae978e2265f

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -467,7 +467,6 @@ pref("app.faqURL", "http://www.mozilla.com/%LOCALE%/mobile/beta/faq/");
 #else
 pref("app.faqURL", "http://www.mozilla.com/%LOCALE%/mobile/faq/");
 #endif
-pref("app.marketplaceURL", "https://marketplace.firefox.com/");
 
 
 pref("security.alternate_certificate_error_page", "certerror");
