f4e5856694ce43f08851a406c04647787dbae8c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:38:35 +0000

    Bug 1149897 - PerformanceStatsService data is not monotonic;r=jandem
    UltraBlame original commit: 39d74e5c3df5181aa86f4b06285f8155399025fe

--- js/src/vm/Interpreter.cpp
+++ js/src/vm/Interpreter.cpp
@@ -567,11 +567,14 @@ struct AutoStopwatch final
         ULARGE_INTEGER userTimeInt;
         kernelTimeInt.LowPart = kernelFileTime.dwLowDateTime;
         kernelTimeInt.HighPart = kernelFileTime.dwHighDateTime;
-        *systemTime = kernelTimeInt.QuadPart / 10; 
+        
+        *systemTime = runtime_-> stopwatch.systemTimeFix.monotonize(kernelTimeInt.QuadPart / 10);
 
         userTimeInt.LowPart = userFileTime.dwLowDateTime;
         userTimeInt.HighPart = userFileTime.dwHighDateTime;
-        *userTime = userTimeInt.QuadPart / 10; 
+        
+        *userTime = runtime_-> stopwatch.userTimeFix.monotonize(userTimeInt.QuadPart / 10);
+
 #endif 
 
         return true;
