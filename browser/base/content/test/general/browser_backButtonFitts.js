e8b4a2ff532c05bb44b038197d754449256275d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:16:01 +0000

    Bug 1099154, e10s, fix and reenable browser_backButtonFitts.js, r=gijs
    UltraBlame original commit: 41ca098906eb231f1d99bc60c82b8b7dddda43a9

--- browser/base/content/test/general/browser_backButtonFitts.js
+++ browser/base/content/test/general/browser_backButtonFitts.js
@@ -2,29 +2,41 @@
 
 
 
-function test () {
-  waitForExplicitFinish();
-  var firstLocation = "http://example.org/browser/browser/base/content/test/general/dummy_page.html";
-  gBrowser.selectedTab = gBrowser.addTab(firstLocation);
-  gBrowser.selectedBrowser.addEventListener("pageshow", function onPageShow1() {
-    gBrowser.selectedBrowser.removeEventListener("pageshow", onPageShow1);
-    gBrowser.selectedBrowser.contentWindow.history.pushState("page2", "page2", "page2");
-    window.maximize();
+add_task(function* () {
+  let firstLocation = "http://example.org/browser/browser/base/content/test/general/dummy_page.html";
+  yield BrowserTestUtils.openNewForegroundTab(gBrowser, firstLocation);
+
+  yield ContentTask.spawn(gBrowser.selectedBrowser, {}, function* () {
+    
+    content.history.pushState("page2", "page2", "page2");
 
     
-    var navBar = document.getElementById("nav-bar");
-    var boundingRect = navBar.getBoundingClientRect();
-    var yPixel = boundingRect.top + Math.floor(boundingRect.height / 2);
-    var xPixel = 0; 
+    
+    content.addEventListener("popstate", function onPopState() {
+      content.removeEventListener("popstate", onPopState, false);
+      sendAsyncMessage("Test:PopStateOccurred", { location: content.document.location.href });
+    }, false);
+  });
 
-    gBrowser.selectedBrowser.contentWindow.addEventListener("popstate", function onPopState() {
-      gBrowser.selectedBrowser.contentWindow.removeEventListener("popstate", onPopState);
-      is(gBrowser.selectedBrowser.contentDocument.location.href, firstLocation,
-         "Clicking the first pixel should have navigated back.");
-      window.restore();
-      gBrowser.removeCurrentTab();
-      finish();
+  window.maximize();
+
+  
+  var navBar = document.getElementById("nav-bar");
+  var boundingRect = navBar.getBoundingClientRect();
+  var yPixel = boundingRect.top + Math.floor(boundingRect.height / 2);
+  var xPixel = 0; 
+
+  let resultLocation = yield new Promise(resolve => {
+    messageManager.addMessageListener("Test:PopStateOccurred", function statePopped(message) {
+      messageManager.removeMessageListener("Test:PopStateOccurred", statePopped);
+      resolve(message.data.location);
     });
+
     EventUtils.synthesizeMouseAtPoint(xPixel, yPixel, {}, window);
   });
-}
+
+  is(resultLocation, firstLocation, "Clicking the first pixel should have navigated back.");
+  window.restore();
+
+  gBrowser.removeCurrentTab();
+});
