08373324c2ddfe980ac68c5cfc5b997e3ef65306
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:36:31 +0000

    Bug 889737 - Part 10: Fix xpcshell tests. r=vicamo
    UltraBlame original commit: 923dbd546a96d5537626ad61b7aae95092a63551

---
+++ dom/telephony/test/xpcshell/header_helpers.js
@@ -0,0 +1,9 @@
+
+
+
+"use strict";
+
+const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
+
+let subscriptLoader = Cc["@mozilla.org/moz/jssubscript-loader;1"]
+                        .getService(Ci.mozIJSSubScriptLoader);
