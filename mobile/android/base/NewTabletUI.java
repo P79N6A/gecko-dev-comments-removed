422091fb2a6f0678304f4f47fbb14182fd110f66
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:58:22 +0000

    Bug 1093621 - Enable new tablet UI by default in Nightly (r=mcomella)
    UltraBlame original commit: 3b36af7151ec0af6632c5e0df1b8e4e4509070a0

--- mobile/android/base/NewTabletUI.java
+++ mobile/android/base/NewTabletUI.java
@@ -7,12 +7,15 @@ package org.mozilla.gecko;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.preferences.GeckoPreferences;
 import org.mozilla.gecko.util.HardwareUtils;
 
 public class NewTabletUI {
     
-    private static final boolean DEFAULT = false;
+    
+    
+    private static final boolean DEFAULT = !AppConstants.RELEASE_BUILD;
 
     private static Boolean sNewTabletUI;
 
