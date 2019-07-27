c3c3db5142cefaabe4b7de6d134909b5ca89257d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:35:43 +0000

    Bug 1079154 patch 2: add SetPosition with a LogicalPoint to nsIFrame, r=jfkthame
    UltraBlame original commit: 679016653e34a44324437abdebe9d4b5012e3272

--- layout/generic/nsIFrame.h
+++ layout/generic/nsIFrame.h
@@ -726,6 +726,15 @@ public:
     SetRect(nsRect(mRect.TopLeft(), aSize));
   }
   void SetPosition(const nsPoint& aPt) { mRect.MoveTo(aPt); }
+  void SetPosition(mozilla::WritingMode aWritingMode,
+                   const mozilla::LogicalPoint& aPt,
+                   nscoord aContainerWidth) {
+    
+    
+    
+    mRect.MoveTo(aPt.GetPhysicalPoint(aWritingMode,
+                                      aContainerWidth - mRect.width));
+  }
 
   
 
