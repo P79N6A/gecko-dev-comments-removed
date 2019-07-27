ba5ddcab11ca379ff414b2f3513aa90db025354b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:59:49 +0000

    Backed out changeset f9101e882c96 (bug 1128494) for dt-tests regression on a CLOSED TREE
    UltraBlame original commit: 19e5147095cd24025c215ca077f8f6e8faa13373

--- dom/media/webaudio/PannerNode.cpp
+++ dom/media/webaudio/PannerNode.cpp
@@ -45,7 +45,7 @@ public:
   explicit PannerNodeEngine(AudioNode* aNode)
     : AudioNodeEngine(aNode)
     
-    , mPanningModelFunction(&PannerNodeEngine::EqualPowerPanningFunction)
+    , mPanningModelFunction(&PannerNodeEngine::HRTFPanningFunction)
     , mDistanceModelFunction(&PannerNodeEngine::InverseGainFunction)
     , mPosition()
     , mOrientation(1., 0., 0.)
@@ -228,7 +228,7 @@ PannerNode::PannerNode(AudioContext* aContext)
               ChannelCountMode::Clamped_max,
               ChannelInterpretation::Speakers)
   
-  , mPanningModel(PanningModelType::Equalpower)
+  , mPanningModel(PanningModelType::HRTF)
   , mDistanceModel(DistanceModelType::Inverse)
   , mPosition()
   , mOrientation(1., 0., 0.)
