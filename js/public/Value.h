8153b2efa71c3f1d25130b750a32e30672600e31
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:46:55 +0000

    Bug 1030400 - Add sym to the 32 bit big endian branch. r=jorendorff
    UltraBlame original commit: 433d3d69cecb4e024eda7fc951786c8f814692b6

--- js/public/Value.h
+++ js/public/Value.h
@@ -306,6 +306,7 @@ typedef union jsval_layout
             uint32_t       u32;
             uint32_t       boo;     
             JSString       *str;
+            JS::Symbol     *sym;
             JSObject       *obj;
             js::gc::Cell   *cell;
             void           *ptr;
