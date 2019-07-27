d5ef3545db8350515d62bd278976d978176d0569
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:02:54 +0000

    Bug 1129875 - Remove unused AssertOnScriptEntryHook. r=luke
    UltraBlame original commit: c8d97a8ed806dfacd2aa14c060de0062e25a1d0a

--- js/src/vm/Runtime.h
+++ js/src/vm/Runtime.h
@@ -987,10 +987,6 @@ struct JSRuntime : public JS::shadow::Runtime,
 
     mozilla::UniquePtr<js::SourceHook> sourceHook;
 
-#ifdef NIGHTLY_BUILD
-    js::AssertOnScriptEntryHook assertOnScriptEntryHook_;
-#endif
-
     
     js::SPSProfiler     spsProfiler;
 
