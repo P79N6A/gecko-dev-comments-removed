573fd2975a19862b28c9a3320b02c7f891b4119a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:25:56 +0000

    Bug 1110952 - Add module for managing devtools colors and
    From 7d463d4891a7a4f57652cb962e3113eb28dea876 Mon Sep 17 00:00:00 2001
     themes. r=bgrins
    
    UltraBlame original commit: 68dd4d75420a21bccd1e1a8fb0d66bde9ee74023

---
+++ browser/devtools/shared/theme.js
@@ -0,0 +1,90 @@
+
+
+
+
+"use strict";
+
+
+
+
+
+
+const { Cu } = require("chrome");
+const { NetUtil } = Cu.import("resource://gre/modules/NetUtil.jsm", {});
+loader.lazyRequireGetter(this, "Services");
+loader.lazyImporter(this, "gDevTools", "resource:///modules/devtools/gDevTools.jsm");
+
+const themeURIs = {
+  light: "chrome://browser/skin/devtools/light-theme.css",
+  dark: "chrome://browser/skin/devtools/dark-theme.css"
+}
+
+const cachedThemes = {};
+
+
+
+
+function readURI (uri) {
+  let stream = NetUtil.newChannel(uri, "UTF-8", null).open();
+  let count = stream.available();
+  let data = NetUtil.readInputStreamToString(stream, count, { charset: "UTF-8" });
+  stream.close();
+  return data;
+}
+
+
+
+
+
+function getThemeFile (name) {
+  
+  let themeFile = cachedThemes[name] || readURI(themeURIs[name]).match(/--theme-.*: .*;/g).join("\n");
+
+  
+  if (!cachedThemes[name]) {
+    cachedThemes[name] = themeFile;
+  }
+
+  return themeFile;
+}
+
+
+
+
+
+const getTheme = exports.getTheme = () => Services.prefs.getCharPref("devtools.theme");
+
+
+
+
+
+
+
+const getColor = exports.getColor = (type, theme) => {
+  let themeName = theme || getTheme();
+
+  
+  if (!themeURIs[themeName]) {
+    themeName = "light";
+  }
+
+  let themeFile = getThemeFile(themeName);
+  let match;
+
+  
+  return (match = themeFile.match(new RegExp("--theme-" + type + ": (.*);"))) ? match[1] : null;
+};
+
+
+
+
+
+
+const setTheme = exports.setTheme = (newTheme) => {
+  Services.prefs.setCharPref("devtools.theme", newTheme);
+  gDevTools.emit("pref-changed", {
+    pref: "devtools.theme",
+    newValue: newTheme,
+    oldValue: getTheme()
+  });
+};
