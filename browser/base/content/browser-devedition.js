583e5ad3aa1294a63d37603d604eb7881a48dc36
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:57:25 +0000

    Bug 1053434 - Listen for lightweight theme changes and call relevant listeners for devedition theme;r=Gijs
    UltraBlame original commit: e6e66d6d4a92161c509dd1c81e29e0a59da4dbd2

--- browser/base/content/browser-devedition.js
+++ browser/base/content/browser-devedition.js
@@ -14,10 +14,11 @@ let DevEdition = {
 
   styleSheetLocation: "chrome://browser/skin/devedition.css",
   styleSheet: null,
+  defaultThemeID: "{972ce4c6-7e08-4474-a285-3208198ce6fd}",
 
   init: function () {
     this._updateDevtoolsThemeAttribute();
-    this._updateStyleSheet();
+    this._updateStyleSheetFromPrefs();
 
     
     
@@ -25,14 +26,27 @@ let DevEdition = {
     Services.prefs.addObserver(this._lwThemePrefName, this, false);
     Services.prefs.addObserver(this._prefName, this, false);
     Services.prefs.addObserver(this._devtoolsThemePrefName, this, false);
+    Services.obs.addObserver(this, "lightweight-theme-styling-update", false);
   },
 
   observe: function (subject, topic, data) {
+    if (topic == "lightweight-theme-styling-update") {
+      let newTheme = JSON.parse(data);
+      if (!newTheme || newTheme.id === this.defaultThemeID) {
+        
+        this._updateStyleSheetFromPrefs();
+      } else {
+        
+        
+        this._toggleStyleSheet(false);
+      }
+    }
+
     if (topic == "nsPref:changed") {
       if (data == this._devtoolsThemePrefName) {
         this._updateDevtoolsThemeAttribute();
       } else {
-        this._updateStyleSheet();
+        this._updateStyleSheetFromPrefs();
       }
     }
   },
@@ -42,11 +56,10 @@ let DevEdition = {
     
     document.documentElement.setAttribute("devtoolstheme",
       Services.prefs.getCharPref(this._devtoolsThemePrefName));
+    this._updateStyleSheetFromPrefs();
   },
 
-  _updateStyleSheet: function() {
-    
-    
+  _updateStyleSheetFromPrefs: function() {
     let lightweightThemeSelected = false;
     try {
       lightweightThemeSelected = Services.prefs.getBoolPref(this._lwThemePrefName);
@@ -57,21 +70,37 @@ let DevEdition = {
        defaultThemeSelected = Services.prefs.getCharPref(this._themePrefName) == "classic/1.0";
     } catch(e) {}
 
-    let deveditionThemeEnabled = Services.prefs.getBoolPref(this._prefName) &&
-      !lightweightThemeSelected && defaultThemeSelected;
+    let devtoolsIsDark = false;
+    try {
+       devtoolsIsDark = Services.prefs.getCharPref(this._devtoolsThemePrefName) == "dark";
+    } catch(e) {}
 
+    let deveditionThemeEnabled = Services.prefs.getBoolPref(this._prefName) &&
+      !lightweightThemeSelected && defaultThemeSelected && devtoolsIsDark;
+
+    this._toggleStyleSheet(deveditionThemeEnabled);
+  },
+
+  handleEvent: function(e) {
+    if (e.type === "load") {
+      this.styleSheet.removeEventListener("load", this);
+      gBrowser.tabContainer._positionPinnedTabs();
+      ToolbarIconColor.inferFromText();
+    }
+  },
+
+  _toggleStyleSheet: function(deveditionThemeEnabled) {
     if (deveditionThemeEnabled && !this.styleSheet) {
       let styleSheetAttr = `href="${this.styleSheetLocation}" type="text/css"`;
-      let styleSheet = this.styleSheet = document.createProcessingInstruction(
+      this.styleSheet = document.createProcessingInstruction(
         'xml-stylesheet', styleSheetAttr);
-      this.styleSheet.addEventListener("load", function onLoad() {
-        styleSheet.removeEventListener("load", onLoad);
-        ToolbarIconColor.inferFromText();
-      });
+      this.styleSheet.addEventListener("load", this);
       document.insertBefore(this.styleSheet, document.documentElement);
     } else if (!deveditionThemeEnabled && this.styleSheet) {
+      this.styleSheet.removeEventListener("load", this);
       this.styleSheet.remove();
       this.styleSheet = null;
+      gBrowser.tabContainer._positionPinnedTabs();
       ToolbarIconColor.inferFromText();
     }
   },
@@ -80,6 +109,10 @@ let DevEdition = {
     Services.prefs.removeObserver(this._lwThemePrefName, this);
     Services.prefs.removeObserver(this._prefName, this);
     Services.prefs.removeObserver(this._devtoolsThemePrefName, this);
+    Services.obs.removeObserver(this, "lightweight-theme-styling-update", false);
+    if (this.styleSheet) {
+      this.styleSheet.removeEventListener("load", this);
+    }
     this.styleSheet = null;
   }
 };
