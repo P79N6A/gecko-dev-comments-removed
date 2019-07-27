912f670d8372894ee8c8cdd237b101e0ed51e46b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:06:54 +0000

    Bug 1156689: Part6. Add ResourceStream class. r=kentuckyfriedtakahe
    A Stream abstract over a MediaResource
    
    UltraBlame original commit: cfb189bc93684ce7bbf40b39f24f2af6dfef697b

---
+++ media/libstagefright/binding/include/mp4_demuxer/ResourceStream.h
@@ -0,0 +1,49 @@
+
+
+
+
+#ifndef RESOURCESTREAM_H_
+#define RESOURCESTREAM_H_
+
+#include "MediaResource.h"
+#include "mp4_demuxer/Stream.h"
+#include "nsRefPtr.h"
+
+namespace mp4_demuxer
+{
+
+class ResourceStream : public Stream
+{
+public:
+  explicit ResourceStream(mozilla::MediaResource* aResource);
+
+  virtual bool ReadAt(int64_t offset, void* aBuffer, size_t aCount,
+                      size_t* aBytesRead) override;
+  virtual bool CachedReadAt(int64_t aOffset, void* aBuffer, size_t aCount,
+                            size_t* aBytesRead) override;
+  virtual bool Length(int64_t* size) override;
+
+  void Pin()
+  {
+    mResource->Pin();
+    ++mPinCount;
+  }
+
+  void Unpin()
+  {
+    mResource->Unpin();
+    MOZ_ASSERT(mPinCount);
+    --mPinCount;
+  }
+
+protected:
+  virtual ~ResourceStream();
+
+private:
+  nsRefPtr<mozilla::MediaResource> mResource;
+  uint32_t mPinCount;
+};
+
+}
+
+#endif 
