b426094996e6a0e7560be175632e9026657eeb16
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:36:56 +0000

    Bug 1073003 - Remove an unused function in Nuwa.cpp. r=glandium
    UltraBlame original commit: 6d55adbfd24d6f80b4868d4b1b4d8c6c8940994a

--- mozglue/build/Nuwa.cpp
+++ mozglue/build/Nuwa.cpp
@@ -330,32 +330,6 @@ GetThreadInfo(pthread_t threadID) {
   return tinfo;
 }
 
-
-
-
-
-
-static thread_info_t*
-GetThreadInfo(pid_t threadID) {
-  if (sIsNuwaProcess) {
-    REAL(pthread_mutex_lock)(&sThreadCountLock);
-  }
-  thread_info_t *thrinfo = nullptr;
-  for (thread_info_t *tinfo = sAllThreads.getFirst();
-       tinfo;
-       tinfo = tinfo->getNext()) {
-    if (tinfo->origNativeThreadID == threadID) {
-      thrinfo = tinfo;
-      break;
-    }
-  }
-  if (sIsNuwaProcess) {
-    pthread_mutex_unlock(&sThreadCountLock);
-  }
-
-  return thrinfo;
-}
-
 #if !defined(HAVE_THREAD_TLS_KEYWORD)
 
 
