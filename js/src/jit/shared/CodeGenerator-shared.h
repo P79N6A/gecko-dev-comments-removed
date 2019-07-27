892cc96cf68815d3dad1b1eb5552c49fa7342ed4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:49:38 +0000

    Bug 1168500 - Replace the operator, by variadic templates. r=Waldo
    UltraBlame original commit: 54be257c53133b4efb8e3b77d2aecdfb7d9f5206

--- js/src/jit/shared/CodeGenerator-shared.h
+++ js/src/jit/shared/CodeGenerator-shared.h
@@ -8,6 +8,7 @@
 #define jit_shared_CodeGenerator_shared_h
 
 #include "mozilla/Alignment.h"
+#include "mozilla/Move.h"
 
 #include "jit/JitFrames.h"
 #include "jit/LIR.h"
@@ -591,56 +592,44 @@ class OutOfLineCodeBase : public OutOfLineCode
 
 
 
-template <class SeqType, typename LastType>
-class ArgSeq : public SeqType
-{
-  private:
-    typedef ArgSeq<SeqType, LastType> ThisType;
-    LastType last_;
-
-  public:
-    ArgSeq(const SeqType& seq, const LastType& last)
-      : SeqType(seq),
-        last_(last)
-    { }
-
-    template <typename NextType>
-    inline ArgSeq<ThisType, NextType>
-    operator, (const NextType& last) const {
-        return ArgSeq<ThisType, NextType>(*this, last);
-    }
-
-    inline void generate(CodeGeneratorShared* codegen) const {
-        codegen->pushArg(last_);
-        this->SeqType::generate(codegen);
-    }
-};
-
+template <typename... ArgTypes>
+class ArgSeq;
 
 template <>
-class ArgSeq<void, void>
+class ArgSeq<>
 {
-  private:
-    typedef ArgSeq<void, void> ThisType;
-
   public:
     ArgSeq() { }
-    ArgSeq(const ThisType&) { }
-
-    template <typename NextType>
-    inline ArgSeq<ThisType, NextType>
-    operator, (const NextType& last) const {
-        return ArgSeq<ThisType, NextType>(*this, last);
-    }
 
     inline void generate(CodeGeneratorShared* codegen) const {
     }
 };
 
-inline ArgSeq<void, void>
-ArgList()
+template <typename HeadType, typename... TailTypes>
+class ArgSeq<HeadType, TailTypes...> : public ArgSeq<TailTypes...>
 {
-    return ArgSeq<void, void>();
+  private:
+    HeadType head_;
+
+  public:
+    explicit ArgSeq(HeadType&& head, TailTypes&&... tail)
+      : ArgSeq<TailTypes...>(mozilla::Move(tail)...),
+        head_(mozilla::Move(head))
+    { }
+
+    
+    
+    inline void generate(CodeGeneratorShared* codegen) const {
+        this->ArgSeq<TailTypes...>::generate(codegen);
+        codegen->pushArg(head_);
+    }
+};
+
+template <typename... ArgTypes>
+inline ArgSeq<ArgTypes...>
+ArgList(ArgTypes... args)
+{
+    return ArgSeq<ArgTypes...>(mozilla::Move(args)...);
 }
 
 
