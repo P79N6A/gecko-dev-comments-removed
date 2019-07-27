7e1ed22a1ce113a402cb99b7fc3c3dcf8dcb47a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:22:55 +0000

    Bug 1181714 nsTextStore should store previous focused document manager until new document manager actually gets focus r=m_kato
    UltraBlame original commit: 0356149a037593f1dacb31240673f3ccd90d49e1

--- widget/windows/nsTextStore.cpp
+++ widget/windows/nsTextStore.cpp
@@ -4176,9 +4176,10 @@ nsTextStore::OnFocusChange(bool aGotFocus,
     return NS_ERROR_NOT_AVAILABLE;
   }
 
+  nsRefPtr<ITfDocumentMgr> prevFocusedDocumentMgr;
+
   
   if (ThinksHavingFocus()) {
-    nsRefPtr<ITfDocumentMgr> prevFocusedDocumentMgr;
     DebugOnly<HRESULT> hr =
       sThreadMgr->AssociateFocus(
         sEnabledTextStore->mWidget->GetWindowHandle(),
