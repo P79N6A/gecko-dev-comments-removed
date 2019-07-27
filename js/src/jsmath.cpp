53716f4ed44d7336e0c2d70159c793d6404b5f05
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:54:25 +0000

    Bug 1028704 - IonMonkey: Implement RMinMax recover instruction. r=nbp
    UltraBlame original commit: 924708eb83751ccabf9298d62a89cc684db5a43d

--- js/src/jsmath.cpp
+++ js/src/jsmath.cpp
@@ -552,6 +552,15 @@ js::math_log(JSContext *cx, unsigned argc, Value *vp)
     return true;
 }
 
+static double
+max_double(double x, double y)
+{
+    
+    if (x > y || IsNaN(x) || (x == y && IsNegative(y)))
+        return x;
+    return y;
+}
+
 bool
 js_math_max(JSContext *cx, unsigned argc, Value *vp)
 {
@@ -562,12 +571,19 @@ js_math_max(JSContext *cx, unsigned argc, Value *vp)
         double x;
         if (!ToNumber(cx, args[i], &x))
             return false;
-        
-        if (x > maxval || IsNaN(x) || (x == maxval && IsNegative(maxval)))
-            maxval = x;
+        maxval = max_double(x, maxval);
     }
     args.rval().setNumber(maxval);
     return true;
+}
+
+static double
+min_double(double x, double y)
+{
+    
+    if (x < y || IsNaN(x) || (x == y && IsNegativeZero(x)))
+        return x;
+    return y;
 }
 
 bool
@@ -580,11 +596,27 @@ js_math_min(JSContext *cx, unsigned argc, Value *vp)
         double x;
         if (!ToNumber(cx, args[i], &x))
             return false;
-        
-        if (x < minval || IsNaN(x) || (x == minval && IsNegativeZero(x)))
-            minval = x;
+        minval = min_double(x, minval);
     }
     args.rval().setNumber(minval);
+    return true;
+}
+
+bool
+js_minmax_impl(JSContext *cx, bool max, HandleValue a, HandleValue b, MutableHandleValue res)
+{
+    double x, y;
+
+    if (!ToNumber(cx, a, &x))
+        return false;
+    if (!ToNumber(cx, b, &y))
+        return false;
+
+    if (max)
+        res.setNumber(max_double(x, y));
+    else
+        res.setNumber(min_double(x, y));
+
     return true;
 }
 
