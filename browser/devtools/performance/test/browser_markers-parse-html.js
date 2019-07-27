96830635ecac2550fda78eeec87df7957bdba154
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:42:57 +0000

    Bug 1151703 - Part 3: Add a test for HTML parsing markers; r=jsantell
    UltraBlame original commit: bf3d9456b067552a6c3d82332e5d8e7c126dc1e1

---
+++ browser/devtools/performance/test/browser_markers-parse-html.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+const TEST_URL = EXAMPLE_URL + "doc_innerHTML.html"
+
+function* getMarkers(front) {
+  const { promise, resolve } = Promise.defer();
+  const handler = (_, markers) => {
+    resolve(markers);
+  };
+  front.on("markers", handler);
+
+  yield front.startRecording({ withTicks: true });
+
+  const markers = yield promise;
+  front.off("markers", handler);
+  yield front.stopRecording();
+
+  return markers;
+}
+
+function* spawnTest () {
+  let { target, front } = yield initBackend(TEST_URL);
+
+  const markers = yield getMarkers(front);
+  info("markers = " + JSON.stringify(markers, null, 2));
+  ok(markers.some(m => m.name === "Parse HTML" && m.stack != undefined),
+     "Should get some Parse HTML markers");
+
+  yield removeTab(target.tab);
+  finish();
+}
