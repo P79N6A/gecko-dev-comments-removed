c1bf9345176d41c295cacfbfd3c834ea8d679210
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:28:53 +0000

    Backed out changeset d50d7e88f35e (bug 1012951) for LSan failures
    UltraBlame original commit: 99e1f3c50a12775a3f59843cc60b9febc3e0785e

--- security/sandbox/linux/SandboxFilter.h
+++ security/sandbox/linux/SandboxFilter.h
@@ -11,25 +11,17 @@ struct sock_fprog;
 struct sock_filter;
 
 namespace mozilla {
-
-enum SandboxType {
-  kSandboxContentProcess,
-  kSandboxMediaPlugin
-};
-
-class SandboxFilter {
-  sock_filter *mFilter;
-  sock_fprog *mProg;
-  const sock_fprog **mStored;
-public:
+  class SandboxFilter {
+    sock_filter *mFilter;
+    sock_fprog *mProg;
+    const sock_fprog **mStored;
+  public:
   
   
   
-  SandboxFilter(const sock_fprog** aStored, SandboxType aBox,
-                bool aVerbose = false);
-  ~SandboxFilter();
-};
-
-} 
+    SandboxFilter(const sock_fprog** aStored, bool aVerbose = false);
+    ~SandboxFilter();
+  };
+}
 
 #endif
