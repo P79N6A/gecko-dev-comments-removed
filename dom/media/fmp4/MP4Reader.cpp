99fedd404d834669799b30f0a8fd5e24086b896d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:12:23 +0000

    Bug 1161444 - Change MP4Reader's IsMediaSeekable() to only check with Demuxer. r=cpearce
    UltraBlame original commit: b091b66b50a77aba522cddef716ff4b9ffdefdbe

--- dom/media/fmp4/MP4Reader.cpp
+++ dom/media/fmp4/MP4Reader.cpp
@@ -587,9 +587,8 @@ bool
 MP4Reader::IsMediaSeekable()
 {
   
-  
   MonitorAutoLock mon(mDemuxerMonitor);
-  return mDecoder->GetResource()->IsTransportSeekable();
+  return mDemuxer->CanSeek();
 }
 
 bool
@@ -1077,7 +1076,7 @@ MP4Reader::Seek(int64_t aTime, int64_t aEndTime)
   LOG("aTime=(%lld)", aTime);
   MOZ_ASSERT(GetTaskQueue()->IsCurrentThreadIn());
   MonitorAutoLock mon(mDemuxerMonitor);
-  if (!mDecoder->GetResource()->IsTransportSeekable() || !mDemuxer->CanSeek()) {
+  if (!mDemuxer->CanSeek()) {
     VLOG("Seek() END (Unseekable)");
     return SeekPromise::CreateAndReject(NS_ERROR_FAILURE, __func__);
   }
