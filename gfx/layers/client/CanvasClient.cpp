c91711554f7f0ae0e543aefd8f04b2f52806e182
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:00:23 +0000

    Backed out changeset ba77f00867e8 (bug 1027601)
    UltraBlame original commit: 79d0d9ac4de1aedcafecd841a50432272825cade

--- gfx/layers/client/CanvasClient.cpp
+++ gfx/layers/client/CanvasClient.cpp
@@ -134,7 +134,7 @@ CanvasClient2D::CreateTextureClientForCanvas(gfx::SurfaceFormat aFormat,
   
   
   return TextureClient::CreateForRawBufferAccess(GetForwarder(),
-                                                 aFormat, aSize, backend,
+                                                 aFormat, aSize, BackendType::CAIRO,
                                                  mTextureInfo.mTextureFlags | aFlags);
 #endif
 }
