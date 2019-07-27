cd6b08bc5f9874f5c7611e795300a087afb83473
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:29:43 +0000

    Bug 1071769 - Enable DrawTargetTiled on b2g. r=Bas
    UltraBlame original commit: 342ab0e1b537724ac74d8fcc3ba9e7fef3861dc4

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3952,6 +3952,10 @@ pref("layers.tiled-drawtarget.enabled", true);
 pref("layers.offmainthreadcomposition.enabled", true);
 #endif
 
+#ifdef MOZ_WIDGET_GONK
+pref("layers.tiled-drawtarget.enabled", true);
+#endif
+
 
 
 pref("layers.offmainthreadcomposition.testing.enabled", false);
