f1c38aaefe93a87ff9b63d7e37e393e3de0a41c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:03:44 +0000

    Bug 1057637 - Extract a re-usable ExpandableListAdapter out of RemoteTabsList. r=mcomella
    This patch does a few other small things as well:
    
    * It exposes the client device type.  This will be used later, as part of
      the visual refresh.
    * Aligns the field names in remote_tabs_child with the names used by
      TwoLinePageRow.  This will be used later, when we finally use said
      standard view class.
    
    UltraBlame original commit: 95b0cf5c746099ae9100993b7b63d9a6153e1c08

---
+++ mobile/android/base/RemoteTabsExpandableListAdapter.java
@@ -0,0 +1,149 @@
+
+
+
+
+package org.mozilla.gecko;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mozilla.gecko.TabsAccessor.RemoteClient;
+import org.mozilla.gecko.TabsAccessor.RemoteTab;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+
+
+
+
+
+
+
+public class RemoteTabsExpandableListAdapter extends BaseExpandableListAdapter {
+    protected final ArrayList<RemoteClient> clients;
+    protected int groupLayoutId;
+    protected int childLayoutId;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    public RemoteTabsExpandableListAdapter(int groupLayoutId, int childLayoutId, List<RemoteClient> clients) {
+        this.groupLayoutId = groupLayoutId;
+        this.childLayoutId = childLayoutId;
+        this.clients = new ArrayList<TabsAccessor.RemoteClient>();
+        if (clients != null) {
+            this.clients.addAll(clients);
+        }
+    }
+
+    public void replaceClients(List<RemoteClient> clients) {
+        this.clients.clear();
+        if (clients != null) {
+            this.clients.addAll(clients);
+            this.notifyDataSetChanged();
+        } else {
+            this.notifyDataSetInvalidated();
+        }
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false; 
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return clients.get(groupPosition).guid.hashCode();
+    }
+
+    @Override
+    public int getGroupCount() {
+        return clients.size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return clients.get(groupPosition);
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return clients.get(groupPosition).tabs.size();
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        final Context context = parent.getContext();
+        final View view;
+        if (convertView != null) {
+            view = convertView;
+        } else {
+            final LayoutInflater inflater = LayoutInflater.from(context);
+            view = inflater.inflate(groupLayoutId, parent, false);
+        }
+
+        final RemoteClient client = clients.get(groupPosition);
+
+        final TextView nameView = (TextView) view.findViewById(R.id.client);
+        nameView.setText(client.name);
+
+        final TextView lastModifiedView = (TextView) view.findViewById(R.id.last_synced);
+        final long now = System.currentTimeMillis();
+        lastModifiedView.setText(TabsAccessor.getLastSyncedString(context, now, client.lastModified));
+
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return clients.get(groupPosition).tabs.get(childPosition);
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return clients.get(groupPosition).tabs.get(childPosition).hashCode();
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final Context context = parent.getContext();
+        final View view;
+        if (convertView != null) {
+            view = convertView;
+        } else {
+            final LayoutInflater inflater = LayoutInflater.from(context);
+            view = inflater.inflate(childLayoutId, parent, false);
+        }
+
+        final RemoteClient client = clients.get(groupPosition);
+        final RemoteTab tab = client.tabs.get(childPosition);
+
+        final TextView titleView = (TextView) view.findViewById(R.id.title);
+        titleView.setText(TextUtils.isEmpty(tab.title) ? tab.url : tab.title);
+
+        final TextView urlView = (TextView) view.findViewById(R.id.url);
+        urlView.setText(tab.url);
+
+        return view;
+    }
+}
--- mobile/android/base/TabsAccessor.java
+++ mobile/android/base/TabsAccessor.java
@@ -4,69 +4,209 @@
 
 package org.mozilla.gecko;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.mozilla.gecko.db.BrowserContract;
 import org.mozilla.gecko.util.ThreadUtils;
 import org.mozilla.gecko.util.UIAsyncTask;
-import org.json.JSONArray;
-import org.json.JSONException;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.text.TextUtils;
+import android.text.format.DateUtils;
 import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.regex.Pattern;
 
 public final class TabsAccessor {
     private static final String LOGTAG = "GeckoTabsAccessor";
 
-    private static final String[] CLIENTS_AVAILABILITY_PROJECTION = new String[] {
-                                                                        BrowserContract.Clients.GUID
-                                                                    };
-
-    private static final String[] TABS_PROJECTION_COLUMNS = new String[] {
+    public static final String[] TABS_PROJECTION_COLUMNS = new String[] {
                                                                 BrowserContract.Tabs.TITLE,
                                                                 BrowserContract.Tabs.URL,
                                                                 BrowserContract.Clients.GUID,
                                                                 BrowserContract.Clients.NAME,
                                                                 BrowserContract.Clients.LAST_MODIFIED,
+                                                                BrowserContract.Clients.DEVICE_TYPE,
                                                             };
 
-    
-    public static enum TABS_COLUMN {
-        TITLE,
-        URL,
-        GUID,
-        NAME,
-        LAST_MODIFIED,
-    };
+    private static final String LOCAL_TABS_SELECTION = BrowserContract.Tabs.CLIENT_GUID + " IS NULL";
+    private static final String REMOTE_TABS_SELECTION = BrowserContract.Tabs.CLIENT_GUID + " IS NOT NULL";
 
-    private static final String CLIENTS_SELECTION = BrowserContract.Clients.GUID + " IS NOT NULL";
-    private static final String TABS_SELECTION = BrowserContract.Tabs.CLIENT_GUID + " IS NOT NULL";
+    private static final String REMOTE_TABS_SORT_ORDER =
+            
+            BrowserContract.Clients.LAST_MODIFIED + " DESC, " +
+            
+            
+            BrowserContract.Clients.GUID + " DESC, " +
+            
+            BrowserContract.Tabs.LAST_USED + " DESC";
 
     private static final String LOCAL_CLIENT_SELECTION = BrowserContract.Clients.GUID + " IS NULL";
-    private static final String LOCAL_TABS_SELECTION = BrowserContract.Tabs.CLIENT_GUID + " IS NULL";
+
     private static final Pattern FILTERED_URL_PATTERN = Pattern.compile("^(about|chrome|wyciwyg|file):");
 
+    
+
+
+
+
+
+    public static class RemoteClient {
+        public final String guid;
+        public final String name;
+        public final long lastModified;
+        public final String deviceType;
+        public final ArrayList<RemoteTab> tabs;
+
+        public RemoteClient(String guid, String name, long lastModified, String deviceType) {
+            this.guid = guid;
+            this.name = name;
+            this.lastModified = lastModified;
+            this.deviceType = deviceType;
+            this.tabs = new ArrayList<RemoteTab>();
+        }
+    }
+
+    
+
+
+
+
+
+
     public static class RemoteTab {
-        public String title;
-        public String url;
-        public String guid;
-        public String name;
-        
+        public final String title;
+        public final String url;
+
+        public RemoteTab(String title, String url) {
+            this.title = title;
+            this.url = url;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((title == null) ? 0 : title.hashCode());
+            result = prime * result + ((url == null) ? 0 : url.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            RemoteTab other = (RemoteTab) obj;
+            if (title == null) {
+                if (other.title != null) {
+                    return false;
+                }
+            } else if (!title.equals(other.title)) {
+                return false;
+            }
+            if (url == null) {
+                if (other.url != null) {
+                    return false;
+                }
+            } else if (!url.equals(other.url)) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    
 
 
 
-        public long lastModified;
+
+
+
+
+
+
+
+
+    public static List<RemoteClient> getClientsFromCursor(final Cursor cursor) {
+        final ArrayList<RemoteClient> clients = new ArrayList<TabsAccessor.RemoteClient>();
+
+        final int originalPosition = cursor.getPosition();
+        try {
+            if (!cursor.moveToFirst()) {
+                return clients;
+            }
+
+            final int tabTitleIndex = cursor.getColumnIndex(BrowserContract.Tabs.TITLE);
+            final int tabUrlIndex = cursor.getColumnIndex(BrowserContract.Tabs.URL);
+            final int clientGuidIndex = cursor.getColumnIndex(BrowserContract.Clients.GUID);
+            final int clientNameIndex = cursor.getColumnIndex(BrowserContract.Clients.NAME);
+            final int clientLastModifiedIndex = cursor.getColumnIndex(BrowserContract.Clients.LAST_MODIFIED);
+            final int clientDeviceTypeIndex = cursor.getColumnIndex(BrowserContract.Clients.DEVICE_TYPE);
+
+            
+            
+            
+            RemoteClient lastClient = null;
+            while (!cursor.isAfterLast()) {
+                final String clientGuid = cursor.getString(clientGuidIndex);
+                if (lastClient == null || !TextUtils.equals(lastClient.guid, clientGuid)) {
+                    final String clientName = cursor.getString(clientNameIndex);
+                    final long lastModified = cursor.getLong(clientLastModifiedIndex);
+                    final String deviceType = cursor.getString(clientDeviceTypeIndex);
+                    lastClient = new RemoteClient(clientGuid, clientName, lastModified, deviceType);
+                    clients.add(lastClient);
+                }
+
+                final String tabTitle = cursor.getString(tabTitleIndex);
+                final String tabUrl = cursor.getString(tabUrlIndex);
+                lastClient.tabs.add(new RemoteTab(tabTitle, tabUrl));
+
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.moveToPosition(originalPosition);
+        }
+
+        return clients;
+    }
+
+    public static Cursor getRemoteTabsCursor(Context context) {
+        return getRemoteTabsCursor(context, -1);
+    }
+
+    public static Cursor getRemoteTabsCursor(Context context, int limit) {
+        Uri uri = BrowserContract.Tabs.CONTENT_URI;
+
+        if (limit > 0) {
+            uri = uri.buildUpon()
+                     .appendQueryParameter(BrowserContract.PARAM_LIMIT, String.valueOf(limit))
+                     .build();
+        }
+
+        final Cursor cursor =  context.getContentResolver().query(uri,
+                                                            TABS_PROJECTION_COLUMNS,
+                                                            REMOTE_TABS_SELECTION,
+                                                            null,
+                                                            REMOTE_TABS_SORT_ORDER);
+        return cursor;
     }
 
     public interface OnQueryTabsCompleteListener {
-        public void onQueryTabsComplete(List<RemoteTab> tabs);
+        public void onQueryTabsComplete(List<RemoteClient> clients);
     }
 
     
@@ -82,49 +222,23 @@ public final class TabsAccessor {
         if (listener == null)
             return;
 
-        (new UIAsyncTask.WithoutParams<List<RemoteTab>>(ThreadUtils.getBackgroundHandler()) {
+        (new UIAsyncTask.WithoutParams<List<RemoteClient>>(ThreadUtils.getBackgroundHandler()) {
             @Override
-            protected List<RemoteTab> doInBackground() {
-                Uri uri = BrowserContract.Tabs.CONTENT_URI;
-
-                if (limit > 0) {
-                    uri = uri.buildUpon()
-                             .appendQueryParameter(BrowserContract.PARAM_LIMIT, String.valueOf(limit))
-                             .build();
-                }
-                    
-                Cursor cursor =  context.getContentResolver().query(uri,
-                                                                    TABS_PROJECTION_COLUMNS,
-                                                                    TABS_SELECTION,
-                                                                    null,
-                                                                    null);
-                
+            protected List<RemoteClient> doInBackground() {
+                final Cursor cursor = getRemoteTabsCursor(context, limit);
                 if (cursor == null)
                     return null;
-                
-                RemoteTab tab;
-                final ArrayList<RemoteTab> tabs = new ArrayList<RemoteTab> ();
-                try {
-                    while (cursor.moveToNext()) {
-                        tab = new RemoteTab();
-                        tab.title = cursor.getString(TABS_COLUMN.TITLE.ordinal());
-                        tab.url = cursor.getString(TABS_COLUMN.URL.ordinal());
-                        tab.guid = cursor.getString(TABS_COLUMN.GUID.ordinal());
-                        tab.name = cursor.getString(TABS_COLUMN.NAME.ordinal());
-                        tab.lastModified = cursor.getLong(TABS_COLUMN.LAST_MODIFIED.ordinal());
 
-                        tabs.add(tab);
-                    }
+                try {
+                    return Collections.unmodifiableList(getClientsFromCursor(cursor));
                 } finally {
                     cursor.close();
                 }
-
-                return Collections.unmodifiableList(tabs);
-           }
+            }
 
             @Override
-            protected void onPostExecute(List<RemoteTab> tabs) {
-                listener.onQueryTabsComplete(tabs);
+            protected void onPostExecute(List<RemoteClient> clients) {
+                listener.onQueryTabsComplete(clients);
             }
         }).execute();
     }
@@ -209,5 +323,17 @@ public final class TabsAccessor {
 
     private static boolean isFilteredURL(String url) {
         return FILTERED_URL_PATTERN.matcher(url).lookingAt();
+    }
+
+    
+
+
+
+
+
+
+    public static String getLastSyncedString(Context context, long now, long time) {
+        final CharSequence relativeTimeSpanString = DateUtils.getRelativeTimeSpanString(time, now, DateUtils.MINUTE_IN_MILLIS);
+        return context.getResources().getString(R.string.remote_tabs_last_synced, relativeTimeSpanString);
     }
 }
