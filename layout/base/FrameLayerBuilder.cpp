bf3a0bdbfa4d357e1047fe305d55a0d34db1d00a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:08:07 +0000

    Bug 1022612. Part 34: Skip RecomputeVisibilityForItems in inactive layers. r=mattwoodrow
    RecomputeVisibilityForItems for the retained ThebesLayer already recomputes
    visibility for all items in that layer, including items nested in other items.
    
    UltraBlame original commit: 6e190d419511e96b222fd7461fdb74b0e3a0dd88

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -4229,13 +4229,17 @@ FrameLayerBuilder::DrawThebesLayer(ThebesLayer* aLayer,
   
   gfxContextMatrixAutoSaveRestore saveMatrix(aContext);
   nsIntPoint offset = GetTranslationForThebesLayer(aLayer);
-
   nsPresContext* presContext = entry->mContainerLayerFrame->PresContext();
-  int32_t appUnitsPerDevPixel = presContext->AppUnitsPerDevPixel();
 
-  RecomputeVisibilityForItems(entry->mItems, builder, aRegionToDraw,
-                              offset, appUnitsPerDevPixel,
-                              userData->mXScale, userData->mYScale);
+  if (!layerBuilder->GetContainingThebesLayerData()) {
+    
+    
+    
+    int32_t appUnitsPerDevPixel = presContext->AppUnitsPerDevPixel();
+    RecomputeVisibilityForItems(entry->mItems, builder, aRegionToDraw,
+                                offset, appUnitsPerDevPixel,
+                                userData->mXScale, userData->mYScale);
+  }
 
   nsRefPtr<nsRenderingContext> rc = new nsRenderingContext();
   rc->Init(presContext->DeviceContext(), aContext);
