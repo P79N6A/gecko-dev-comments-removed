03549543dd18fd35945d73ed1a387b21127f6eb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:02:59 +0000

    Bug 1093573 part 7 - Allow OSR for generator scripts. r=wingo
    UltraBlame original commit: 7ce270448bb3b6fc5c36be77194dc8aea91cf037

--- js/src/jit/BaselineJIT.cpp
+++ js/src/jit/BaselineJIT.cpp
@@ -60,11 +60,6 @@ static const unsigned BASELINE_MAX_ARGS_LENGTH = 20000;
 static bool
 CheckFrame(InterpreterFrame *fp)
 {
-    if (fp->script()->isGenerator()) {
-        JitSpew(JitSpew_BaselineAbort, "generator frame");
-        return false;
-    }
-
     if (fp->isDebuggerFrame()) {
         
         
