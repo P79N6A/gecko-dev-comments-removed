08400c8843a801cad922e35659237209ad93858d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:12:03 +0000

    Bug 896139 - Breakpoints not triggering when reloading script;r=jimb
    UltraBlame original commit: 7ec5c7a4830fe194f9b837c49b7efc63ac6e375d

---
+++ browser/devtools/debugger/test/browser_dbg_bug-896139.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+const TAB_URL = "doc_bug-896139.html";
+const SCRIPT_URL = "code_bug-896139.js";
+
+function test() {
+  Task.spawn(function* () {
+    function testBreakpoint() {
+      let promise = waitForDebuggerEvents(panel, win.EVENTS.FETCHED_SCOPES);
+      callInTab(tab, "f");
+      return promise.then(() => doResume(panel));
+    }
+
+    let [tab,, panel] = yield initDebugger(EXAMPLE_URL + TAB_URL);
+    let win = panel.panelWin;
+    yield waitForSourceShown(panel, SCRIPT_URL);
+    yield panel.addBreakpoint({
+      actor: getSourceActor(win.DebuggerView.Sources, EXAMPLE_URL + SCRIPT_URL),
+      line: 3
+    });
+
+    yield testBreakpoint();
+    yield reloadActiveTab(panel, win.EVENTS.SOURCE_SHOWN);
+    yield testBreakpoint();
+
+    yield closeDebuggerAndFinish(panel);
+  });
+}
