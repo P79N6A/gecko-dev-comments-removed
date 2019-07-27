5bc80f807e364d5cf70f201453a2a4061b84445c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:14:59 +0000

    Merge f-t to m-c, a=merge
    UltraBlame original commit: 77d92f6d76797fbbb80819a13ff4dbd6147aa328

--- browser/components/pocket/panels/js/messages.js
+++ browser/components/pocket/panels/js/messages.js
@@ -1,11 +1,27 @@
 
 
 
-var Messaging = (function() {
+var pktPanelMessaging = (function() {
 
-	function addMessageListener(messageId, callback) {
+ function panelIdFromURL(url) {
+   var panelId = url.match(/panelId=([\w|\d|\.]*)&?/);
+        if (panelId && panelId.length > 1) {
+            return panelId[1];
+        }
 
-		document.addEventListener('PKT_' + messageId, function(e) {
+        return 0;
+ }
+
+ function prefixedMessageId(messageId) {
+   return 'PKT_' + messageId;
+ }
+
+ function panelPrefixedMessageId(panelId, messageId) {
+   return prefixedMessageId(panelId + '_' + messageId);
+ }
+
+ function addMessageListener(panelId, messageId, callback) {
+   document.addEventListener(panelPrefixedMessageId(panelId, messageId), function(e) {
 
 			callback(JSON.parse(e.target.getAttribute("payload"))[0]);
 
@@ -16,33 +32,36 @@ var Messaging = (function() {
 
 	}
 
-	function removeMessageListener(messageId, callback) {
-
-		document.removeEventListener('PKT_' + messageId, callback);
-
+	function removeMessageListener(panelId, messageId, callback) {
+   document.removeEventListener(panelPrefixedMessageId(panelId, messageId), callback);
 	}
 
-	function sendMessage(messageId, payload, callback) {
+ function sendMessage(panelId, messageId, payload, callback) {
+   
+   
+   var messagePayload = {
+     panelId: panelId,
+     data: (payload || {})
+   };
 
 		
 		if (callback) {
-			 
 	        var messageResponseId = messageId + "Response";
 	        var responseListener = function(responsePayload) {
 	            callback(responsePayload);
-	            removeMessageListener(messageResponseId, responseListener);
+	            removeMessageListener(panelId, messageResponseId, responseListener);
 	        }
 
-	        addMessageListener(messageResponseId, responseListener);
+	        addMessageListener(panelId, messageResponseId, responseListener);
 	    }
 
 	    
 		var element = document.createElement("PKTMessageFromPanelElement");
-		element.setAttribute("payload", JSON.stringify([payload]));
+		element.setAttribute("payload", JSON.stringify([messagePayload]));
 		document.documentElement.appendChild(element);
 
 		var evt = document.createEvent("Events");
-		evt.initEvent('PKT_'+messageId, true, false);
+		evt.initEvent(prefixedMessageId(messageId), true, false);
 		element.dispatchEvent(evt);
 	}
 
@@ -51,6 +70,7 @@ var Messaging = (function() {
 
 
     return {
+      panelIdFromURL: panelIdFromURL,
         addMessageListener : addMessageListener,
         removeMessageListener : removeMessageListener,
         sendMessage: sendMessage
