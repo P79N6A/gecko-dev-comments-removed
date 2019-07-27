faf2ed1dd92cbf6e608e0479c9502c6ed8c1994d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:58:15 +0000

    Bug 1150668 - Assume EXT_texture_format_BGRA8888 supported on Android emulator; r=jgilbert
    UltraBlame original commit: 4bae26d5e06f263cfbd03ffa6adbcec06da18abf

--- gfx/gl/GLContext.cpp
+++ gfx/gl/GLContext.cpp
@@ -1674,6 +1674,9 @@ GLContext::InitExtensions()
             
             
             MarkExtensionSupported(OES_rgb8_rgba8);
+            
+            
+            MarkExtensionSupported(EXT_texture_format_BGRA8888);
         }
 
         if (Vendor() == GLVendor::VMware &&
