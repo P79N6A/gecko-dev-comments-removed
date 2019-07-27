b3ecc6307c7c08cf6d5a16812f51c0b87397dc6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:40:44 +0000

    Bug 1165515 - Part 7: Convert PR_LOG_DEBUG + 1 to PR_LOG_VERBOSE. rs=froydnj
    Used the command: |sed -E -i -e 's/PR_LOG_DEBUG\s*\+\s*1/PR_LOG_VERBOSE/g'|
    
    UltraBlame original commit: 20ddb568f139a4c1496c65bd074b87587dd0e702

--- dom/media/mediasource/MediaSourceReader.cpp
+++ dom/media/mediasource/MediaSourceReader.cpp
@@ -26,7 +26,7 @@
 extern PRLogModuleInfo* GetMediaSourceLog();
 
 #define MSE_DEBUG(arg, ...) MOZ_LOG(GetMediaSourceLog(), PR_LOG_DEBUG, ("MediaSourceReader(%p)::%s: " arg, this, __func__, ##__VA_ARGS__))
-#define MSE_DEBUGV(arg, ...) MOZ_LOG(GetMediaSourceLog(), PR_LOG_DEBUG + 1, ("MediaSourceReader(%p)::%s: " arg, this, __func__, ##__VA_ARGS__))
+#define MSE_DEBUGV(arg, ...) MOZ_LOG(GetMediaSourceLog(), PR_LOG_VERBOSE, ("MediaSourceReader(%p)::%s: " arg, this, __func__, ##__VA_ARGS__))
 
 
 
