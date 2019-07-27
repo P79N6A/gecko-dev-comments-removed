307f5d96fc45ac0b39b2443f5942d9df68e329c3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:31:08 +0000

    Bug 1049436 - Stop showing color swatch for CSS vars r=bgrins
    UltraBlame original commit: 52d616db7d5a27d45f3ae4826a207e70da5087c6

--- toolkit/devtools/output-parser.js
+++ toolkit/devtools/output-parser.js
@@ -14,8 +14,13 @@ const MAX_ITERATIONS = 100;
 const REGEX_QUOTES = /^".*?"|^".*|^'.*?'|^'.*/;
 const REGEX_WHITESPACE = /^\s+/;
 const REGEX_FIRST_WORD_OR_CHAR = /^\w+|^./;
-const REGEX_CSS_PROPERTY_VALUE = /(^[^;]+)/;
 const REGEX_CUBIC_BEZIER = /^linear|^ease-in-out|^ease-in|^ease-out|^ease|^cubic-bezier\(([0-9.\- ]+,){3}[0-9.\- ]+\)/;
+
+
+
+
+
+const REGEX_CSS_VAR = /\bvar\(\s*--[-_a-zA-Z0-9\u00A0-\u10FFFF]+\s*\)/;
 
 
 
@@ -180,6 +185,15 @@ OutputParser.prototype = {
       }
 
       matched = text.match(REGEX_QUOTES);
+      if (matched) {
+        let match = matched[0];
+
+        text = this._trimMatchFromStart(text, match);
+        this._appendTextNode(match);
+        continue;
+      }
+
+      matched = text.match(REGEX_CSS_VAR);
       if (matched) {
         let match = matched[0];
 
