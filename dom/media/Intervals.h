f7c439a0d16584123644af664ac312052d0eaf9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:49:08 +0000

    Bug 1171330: P2. Add -= and - operator to IntervalSet. r=mattwoodrow
    Remove an interval from an interval set.
    
    UltraBlame original commit: 7afec49604d3ffa95914121bd816ae8ae5009740

--- dom/media/Intervals.h
+++ dom/media/Intervals.h
@@ -390,6 +390,30 @@ public:
   }
 
   
+  
+  
+  SelfType& operator-= (const ElemType& aInterval)
+  {
+    if (aInterval.IsEmpty() || mIntervals.IsEmpty()) {
+      return *this;
+    }
+    T firstEnd = std::max(mIntervals[0].mStart, aInterval.mStart);
+    T secondStart = std::min(mIntervals.LastElement().mEnd, aInterval.mEnd);
+    ElemType startInterval(mIntervals[0].mStart, firstEnd, aInterval.mFuzz);
+    ElemType endInterval(secondStart, mIntervals.LastElement().mEnd, aInterval.mFuzz);
+    SelfType intervals(Move(startInterval));
+    intervals += Move(endInterval);
+    return Intersection(intervals);
+  }
+
+  SelfType operator- (const ElemType& aInterval)
+  {
+    SelfType intervals(*this);
+    intervals -= aInterval;
+    return intervals;
+  }
+
+  
   SelfType& Union(const SelfType& aOther)
   {
     Add(aOther);
