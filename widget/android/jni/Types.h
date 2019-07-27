fed76565b51f0a527e4421ee347d1394cbb56c6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:26:50 +0000

    Bug 1178850 - Make mozilla::jni::Param more intuitive to use; r=snorp
    UltraBlame original commit: f032df69e2edff301bd883531a1c54656b1cbf48

--- widget/android/jni/Types.h
+++ widget/android/jni/Types.h
@@ -65,7 +65,7 @@ template<class Cls> constexpr void
 
 
 
-template<> struct TypeAdapter<class Param<String>::Type>
+template<> struct TypeAdapter<Param<String>>
         : public TypeAdapter<String::Ref>
 {};
 
