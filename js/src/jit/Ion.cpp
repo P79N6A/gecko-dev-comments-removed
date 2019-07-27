b1bfb97bb48b605a797e780f0e1766246f695041
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:07:46 +0000

    Bug 1099463 - Clear parallel entry script flag when removing script from active table r=shu
    UltraBlame original commit: 0b76fb98e53ca1bfb525bdd27d29fbca967edaec

--- js/src/jit/Ion.cpp
+++ js/src/jit/Ion.cpp
@@ -591,8 +591,7 @@ JitCompartment::mark(JSTracer *trc, JSCompartment *compartment)
             
             
             if (!script->hasParallelIonScript() ||
-                !script->parallelIonScript()->isParallelEntryScript() ||
-                trc->runtime()->gc.shouldCleanUpEverything())
+                !script->parallelIonScript()->isParallelEntryScript())
             {
                 e.removeFront();
                 continue;
@@ -604,7 +603,9 @@ JitCompartment::mark(JSTracer *trc, JSCompartment *compartment)
             
             
             
-            if (script->parallelIonScript()->shouldPreserveParallelCode(IonScript::IncreaseAge)) {
+            if (!trc->runtime()->gc.shouldCleanUpEverything() &&
+                script->parallelIonScript()->shouldPreserveParallelCode(IonScript::IncreaseAge))
+            {
                 MarkScript(trc, const_cast<PreBarrieredScript *>(&e.front()), "par-script");
                 MOZ_ASSERT(script == e.front());
             } else {
