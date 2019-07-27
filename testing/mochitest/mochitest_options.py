f23f58db54673520360707f6abcb8f75ada5a7f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:34:15 +0000

    Bug 1038943: Update the b2g mochitest leak threshold. r=me a=me
    UltraBlame original commit: 9a1da88e17e21e0e27034bcb0101a553036d367a

--- testing/mochitest/mochitest_options.py
+++ testing/mochitest/mochitest_options.py
@@ -756,7 +756,7 @@ class B2GOptions(MochitestOptions):
         defaults["testPath"] = ""
         defaults["extensionsToExclude"] = ["specialpowers"]
         
-        defaults["leakThreshold"] = 5200
+        defaults["leakThreshold"] = 5099
         self.set_defaults(**defaults)
 
     def verifyRemoteOptions(self, options):
