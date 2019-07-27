cdd96113d37aea1942be05372390b5cee8265e53
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:59:52 +0000

    Add OS snapshotting to the gfx sanity test and report whether or not it matches the compositing test results. (bug 1173117 part 4, r=mattwoodrow,vladan)
    UltraBlame original commit: b0a391c7c14aa92ec14f9a56e46f5f4e2d65fc65

--- toolkit/components/gfx/SanityTest.js
+++ toolkit/components/gfx/SanityTest.js
@@ -17,12 +17,19 @@ const DEVICE_PREF="sanity-test.device-id";
 const VERSION_PREF="sanity-test.version";
 const DISABLE_VIDEO_PREF="media.hardware-video-decoding.failed";
 const RUNNING_PREF="sanity-test.running";
+const OS_SNAPSHOT_TIMEOUT_SEC=3;
 
 
 const TEST_PASSED=0;
 const TEST_FAILED_RENDER=1;
 const TEST_FAILED_VIDEO=2;
 const TEST_CRASHED=3;
+
+
+const SNAPSHOT_OK=0;
+const SNAPSHOT_INCORRECT=1;
+const SNAPSHOT_ERROR=2;
+const SNAPSHOT_TIMEOUT=3;
 
 function testPixel(ctx, x, y, r, g, b, a, fuzz) {
   var data = ctx.getImageData(x, y, 1, 1);
@@ -46,14 +53,41 @@ function reportResult(val) {
   Services.prefs.savePrefFile(null);
 }
 
+function reportSnapshotResult(val) {
+  let histogram = Services.telemetry.getHistogramById("GRAPHICS_SANITY_TEST_OS_SNAPSHOT");
+  histogram.add(val);
+}
+
 function takeWindowSnapshot(win, ctx) {
-  
-  
   
   
   
   var flags = ctx.DRAWWINDOW_DRAW_CARET | ctx.DRAWWINDOW_DRAW_VIEW | ctx.DRAWWINDOW_USE_WIDGET_LAYERS;
   ctx.drawWindow(win.ownerGlobal, 0, 0, PAGE_WIDTH, PAGE_HEIGHT, "rgb(255,255,255)", flags);
+}
+
+function takeWidgetSnapshot(win, canvas, ctx) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawWidgetAsOnScreen(win.ownerGlobal);
+}
+
+function testWidgetSnapshot(win, canvas, ctx) {
+  try {
+    takeWidgetSnapshot(win, canvas, ctx);
+    if (verifyVideoRendering(ctx)) {
+      reportSnapshotResult(SNAPSHOT_OK);
+    } else {
+      reportSnapshotResult(SNAPSHOT_INCORRECT);
+    }
+  } catch (e) {
+    reportSnapshotResult(SNAPSHOT_ERROR);
+  }
+}
+
+function setTimeout(aMs, aCallback) {
+  var timer = Components.classes["@mozilla.org/timer;1"]
+              .createInstance(Components.interfaces.nsITimer);
+  timer.initWithCallback(aCallback, aMs, Ci.nsITimer.TYPE_ONE_SHOT);
 }
 
 
@@ -92,11 +126,27 @@ let listener = {
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver]),
 
   win: null,
+  utils: null,
   canvas: null,
+
+  
+  
+  
+  
+  
+  
+  windowLoaded: false,   
+  windowReady: false,    
 
   scheduleTest: function(win) {
     this.win = win;
     this.win.onload = this.onWindowLoaded.bind(this);
+    this.utils = this.win.QueryInterface(Ci.nsIInterfaceRequestor)
+                         .getInterface(Ci.nsIDOMWindowUtils);
+
+    let observerService = Cc["@mozilla.org/observer-service;1"].
+                          getService(Components.interfaces.nsIObserverService);
+    observerService.addObserver(this, "widget-first-paint", false);
   },
 
   onWindowLoaded: function() {
@@ -105,10 +155,83 @@ let listener = {
     this.canvas.setAttribute("height", PAGE_HEIGHT);
     this.ctx = this.canvas.getContext("2d");
 
-    testCompositor(this.win, this.ctx);
+    
+    
+    if (!testCompositor(this.win, this.ctx)) {
+      this.endTest();
+      return;
+    }
+
+    
+    
+    setTimeout(OS_SNAPSHOT_TIMEOUT_SEC * 1000, (() => {
+      if (this.win) {
+        reportSnapshotResult(SNAPSHOT_TIMEOUT);
+        this.endTest();
+      }
+    }));
+
+    this.windowLoaded = true;
+    if (this.windowReady) {
+      this.waitForPaintsFlushed();
+    }
+  },
+
+  
+  observe: function(aSubject, aTopic, aData) {
+    if (aSubject != this.win || aTopic != "widget-first-paint") {
+      return;
+    }
+
+    this.windowReady = true;
+    if (this.windowLoaded) {
+      this.waitForPaintsFlushed();
+    }
+  },
+
+  
+  waitForPaintsFlushed: function() {
+    
+    if (!this.win) {
+      return;
+    }
+
+    if (this.utils.isMozAfterPaintPending) {
+      let paintListener = (() => {
+        if (this.utils && this.utils.isMozAfterPaintPending) {
+          return;
+        }
+
+        this.win.removeEventListener("MozAfterPaint", paintListener);
+
+        if (this.utils) {
+          
+          
+          this.waitForPaintsFlushed();
+        }
+      });
+      this.win.addEventListener("MozAfterPaint", paintListener);
+      return;
+    }
+
+    testWidgetSnapshot(this.win, this.canvas, this.ctx);
+    this.endTest();
+  },
+
+  endTest: function() {
+    if (!this.win) {
+      return;
+    }
 
     this.win.ownerGlobal.close();
-  },
+    this.win = null;
+    this.utils = null;
+    this.canvas = null;
+
+    let observerService = Cc["@mozilla.org/observer-service;1"].
+                          getService(Components.interfaces.nsIObserverService);
+    observerService.removeObserver(this, "widget-first-paint");
+  }
 };
 
 function SanityTest() {}
