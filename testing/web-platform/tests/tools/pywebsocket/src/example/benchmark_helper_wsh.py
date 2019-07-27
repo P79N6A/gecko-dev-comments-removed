ab8edf8cd1733ac707c36779ae87e2e4926f7360
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:09:40 +0000

    Backed out changeset 07d09412e9a8 (bug 1084367) for frequent web-platform-2 bustage on a CLOSED TREE
    UltraBlame original commit: 223e2f4b0d47b0b739c2da4dbd7ec2e1602c58ba

--- testing/web-platform/tests/tools/pywebsocket/src/example/benchmark_helper_wsh.py
+++ testing/web-platform/tests/tools/pywebsocket/src/example/benchmark_helper_wsh.py
@@ -40,46 +40,21 @@ def web_socket_transfer_data(request):
     data = ''
 
     while True:
-        command = request.ws_stream.receive_message()
-        if command is None:
+        line = request.ws_stream.receive_message()
+        if line is None:
             return
 
-        if not isinstance(command, unicode):
-            raise ValueError('Invalid command data:' + command)
-        commands = command.split(' ')
-        if len(commands) == 0:
-            raise ValueError('Invalid command data: ' + command)
-
-        if commands[0] == 'receive':
-            if len(commands) != 2:
-                raise ValueError(
-                        'Illegal number of arguments for send command' +
-                        command)
-            size = int(commands[1])
-
+        if isinstance(line, unicode):
             
-            if len(data) != size:
+            size = int(line)
+            if len(data) < size:
                 data = 'a' * size
             request.ws_stream.send_message(data, binary=True)
-        elif commands[0] == 'send':
-            if len(commands) != 2:
-                raise ValueError(
-                        'Illegal number of arguments for receive command' +
-                        command)
-            verify_data = commands[1] == '1'
-
-            data = request.ws_stream.receive_message()
-            if data is None:
-                raise ValueError('Payload not received')
-            size = len(data)
-
-            if verify_data:
-                if data != 'a' * size:
-                    raise ValueError('Payload verification failed')
-
-            request.ws_stream.send_message(str(size))
         else:
-            raise ValueError('Invalid command: ' + commands[0])
+            
+            if line != 'a' * len(line):
+                return
+            request.ws_stream.send_message(str(len(line)));
 
 
 
