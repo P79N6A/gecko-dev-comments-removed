9f0430847ba395ab51e3fe7a8b24ca1d10a66164
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:19:01 +0000

    Bug 1149975 - Part 2 of 2 - Test manual login fill. r=MattN
    UltraBlame original commit: 03fdeedb03af625617b576b10515761f574ebb92

---
+++ toolkit/components/passwordmgr/test/browser/browser_filldoorhanger.js
@@ -0,0 +1,75 @@
+
+
+
+Cu.import("resource://testing-common/LoginTestUtils.jsm", this);
+
+
+
+
+
+add_task(function* test_initialize() {
+  Services.prefs.setBoolPref("signon.ui.experimental", true);
+  Services.prefs.setBoolPref("signon.autofillForms", false);
+  registerCleanupFunction(function () {
+    Services.prefs.clearUserPref("signon.ui.experimental");
+    Services.prefs.clearUserPref("signon.autofillForms");
+  });
+});
+
+
+
+
+add_task(function* test_fill() {
+  Services.logins.addLogin(LoginTestUtils.testData.formLogin({
+    hostname: "https://example.com",
+    formSubmitURL: "https://example.com",
+    username: "username",
+    password: "password",
+  }));
+
+  
+  
+  
+  let anchor = document.getElementById("login-fill-notification-icon");
+  let promiseAnchorShown =
+      TestUtils.topicObserved("PopupNotifications-updateNotShowing",
+                              () => anchor.hasAttribute("showing"));
+
+  yield BrowserTestUtils.withNewTab({
+    gBrowser,
+    url: "https://example.com/browser/toolkit/components/" +
+         "passwordmgr/test/browser/form_basic.html",
+  }, function* (browser) {
+    yield promiseAnchorShown;
+
+    let promiseShown = BrowserTestUtils.waitForEvent(PopupNotifications.panel,
+                                                     "Shown");
+    anchor.click();
+    yield promiseShown;
+
+    let list = document.getElementById("login-fill-list");
+    Assert.equal(list.childNodes.length, 1,
+                 "list.childNodes.length === 1");
+
+    let promiseHidden = BrowserTestUtils.waitForEvent(PopupNotifications.panel,
+                                                      "popuphidden");
+    list.focus();
+    EventUtils.sendMouseEvent({ type: "dblclick" }, list.childNodes[0]);
+    yield promiseHidden;
+
+    let result = yield ContentTask.spawn(browser, null, function* () {
+      let doc = content.document;
+      return {
+        username: doc.getElementById("form-basic-username").value,
+        password: doc.getElementById("form-basic-password").value,
+      }
+    });
+
+    Assert.equal(result.username, "username",
+                 'result.username === "username"');
+    Assert.equal(result.password, "password",
+                 'result.password === "password"');
+  });
+
+  Services.logins.removeAllLogins();
+});
