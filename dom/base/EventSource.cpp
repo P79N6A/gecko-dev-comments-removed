32d6473f065b390afc02dad471f944691fe44ced
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:34:38 +0000

    Bug 1117851 - Make GetCallingLocation take an nsA{,C}String. r=smaug
    UltraBlame original commit: 9035e4de3c038aadba2a78a3927be5a48fab69ab

--- dom/base/EventSource.cpp
+++ dom/base/EventSource.cpp
@@ -201,11 +201,7 @@ EventSource::Init(nsISupports* aOwner,
 
   
   if (JSContext *cx = nsContentUtils::GetCurrentJSContext()) {
-    const char *filename;
-    if (nsJSUtils::GetCallingLocation(cx, &filename, &mScriptLine)) {
-      mScriptFile.AssignASCII(filename);
-    }
-
+    nsJSUtils::GetCallingLocation(cx, mScriptFile, &mScriptLine);
     mInnerWindowID = nsJSUtils::GetCurrentlyRunningCodeInnerWindowID(cx);
   }
 
