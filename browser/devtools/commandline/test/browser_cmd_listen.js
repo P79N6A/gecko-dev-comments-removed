e98bf71181af36e706ade8560c9a45d8d7cba9c2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:33:30 +0000

    Add an unlisten command for stopping any active debugger server instaces (bug 976977). r=jwalker
    UltraBlame original commit: a6325318d1c6b958bb0d3dc4251cc753cf1b41ed

---
+++ browser/devtools/commandline/test/browser_cmd_listen.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+const TEST_URI = "http://example.com/browser/browser/devtools/commandline/"+
+                 "test/browser_cmd_cookie.html";
+
+function test() {
+  return Task.spawn(testTask).then(finish, helpers.handleError);
+}
+
+let tests = {
+  testInput: function(options) {
+    return helpers.audit(options, [
+      {
+        setup:    'listen',
+        check: {
+          input:  'listen',
+          markup: 'VVVVVV',
+          status: 'VALID'
+        },
+      },
+      {
+        setup:    'unlisten',
+        check: {
+          input:  'unlisten',
+          markup: 'VVVVVVVV',
+          status: 'VALID'
+        },
+        exec: {
+          output: 'All TCP ports closed'
+        }
+      },
+      {
+        setup: function() {
+          return helpers.setInput(options, 'listen');
+        },
+        check: {
+          input:  'listen',
+          hints:        ' [port]',
+          markup: 'VVVVVV',
+          status: 'VALID'
+        },
+        exec: {
+          output: 'Listening on port 6080'
+        }
+      },
+      {
+        setup: function() {
+          return helpers.setInput(options, 'listen 8000');
+        },
+        exec: {
+          output: 'Listening on port 8000'
+        }
+      },
+      {
+        setup: function() {
+          return helpers.setInput(options, 'unlisten');
+        },
+        exec: {
+          output: 'All TCP ports closed'
+        }
+      }
+    ]);
+  },
+};
+
+function* testTask() {
+    Services.prefs.setBoolPref('devtools.debugger.remote-enabled', true);
+    let options = yield helpers.openTab(TEST_URI);
+    yield helpers.openToolbar(options);
+
+    yield helpers.runTests(options, tests);
+
+    yield helpers.closeToolbar(options);
+    yield helpers.closeTab(options);
+    Services.prefs.clearUserPref('devtools.debugger.remote-enabled');
+}
