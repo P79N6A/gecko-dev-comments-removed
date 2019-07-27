e1b8338f6873159e53160b00ccacc711ec2abe59
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:04:53 +0000

    Backed out changeset e36492ae23a6 (bug 1094434) for Android Reftest Failures on a CLOSED TREE
    UltraBlame original commit: eb4d49f980d51b33fb5ea4e5d5fdec5b97a5d904

--- layout/xul/nsResizerFrame.cpp
+++ layout/xul/nsResizerFrame.cpp
@@ -502,28 +502,21 @@ nsResizerFrame::GetDirection()
      {-1,  1},          {1,  1}
     };
 
-  if (!GetContent()) {
+  if (!GetContent())
     return directions[0]; 
-  }
 
   int32_t index = GetContent()->FindAttrValueIn(kNameSpaceID_None,
                                                 nsGkAtoms::dir,
                                                 strings, eCaseMatters);
-  if (index < 0) {
+  if(index < 0)
     return directions[0]; 
-  }
-
-  if (index >= 8) {
+  else if (index >= 8 && StyleVisibility()->mDirection == NS_STYLE_DIRECTION_RTL) {
     
     
-    WritingMode wm = GetWritingMode();
-    if (!(wm.IsVertical() ? wm.IsVerticalLR() : wm.IsBidiLTR())) {
-      Direction direction = directions[index];
-      direction.mHorizontal *= -1;
-      return direction;
-    }
+    Direction direction = directions[index];
+    direction.mHorizontal *= -1;
+    return direction;
   }
-
   return directions[index];
 }
 
