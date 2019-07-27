f778647abfb8fb316a7b5ac655173f772267a7b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:15:36 +0000

    Bug 1105208 - regression: (null) title for Bookmarks Toolbar, Bookmarks Menu, Unsorted Bookmarks. r=mak.
    UltraBlame original commit: 601b38d36da897c197ac07e27e3038d8e6f3708c

--- toolkit/components/places/nsNavHistory.cpp
+++ toolkit/components/places/nsNavHistory.cpp
@@ -3905,16 +3905,7 @@ nsNavHistory::RowToResult(mozIStorageValueArray* aRow,
 
   if (IsQueryURI(url)) {
     
-    nsRefPtr<nsNavHistoryResultNode> resultNode;
-    rv = QueryRowToResult(itemId, url, title, accessCount, time, favicon,
-                          getter_AddRefs(resultNode));
-    NS_ENSURE_SUCCESS(rv,rv);
-
     if (itemId != -1) {
-      rv = aRow->GetUTF8String(nsNavBookmarks::kGetChildrenIndex_Guid,
-                               resultNode->mBookmarkGuid);
-      NS_ENSURE_SUCCESS(rv, rv);
-
       
       
       
@@ -3923,7 +3914,18 @@ nsNavHistory::RowToResult(mozIStorageValueArray* aRow,
       nsNavBookmarks *bookmarks = nsNavBookmarks::GetBookmarksService();
       NS_ENSURE_TRUE(bookmarks, NS_ERROR_OUT_OF_MEMORY);
 
-      rv = bookmarks->GetItemTitle(itemId, resultNode->mTitle);
+      rv = bookmarks->GetItemTitle(itemId, title);
+      NS_ENSURE_SUCCESS(rv, rv);
+    }
+
+    nsRefPtr<nsNavHistoryResultNode> resultNode;
+    rv = QueryRowToResult(itemId, url, title, accessCount, time, favicon,
+                          getter_AddRefs(resultNode));
+    NS_ENSURE_SUCCESS(rv,rv);
+
+    if (itemId != -1) {
+      rv = aRow->GetUTF8String(nsNavBookmarks::kGetChildrenIndex_Guid,
+                               resultNode->mBookmarkGuid);
       NS_ENSURE_SUCCESS(rv, rv);
     }
 
