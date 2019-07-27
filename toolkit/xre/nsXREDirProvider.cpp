7388eceb2fec4208dca162dff94f4d7ac34483b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:48:34 +0000

    Backed out changeset 006353e45fa7 (bug 833143) for robocop crashes; CLOSED TREE
    UltraBlame original commit: d014b12e6afc752c6f91bbc6ade0bae996cef5f3

--- toolkit/xre/nsXREDirProvider.cpp
+++ toolkit/xre/nsXREDirProvider.cpp
@@ -883,6 +883,19 @@ nsXREDirProvider::DoShutdown()
       obsSvc->NotifyObservers(nullptr, "profile-change-teardown", kShutdownPersist);
 
       
+      
+
+      nsCOMPtr<nsIJSRuntimeService> rtsvc
+        (do_GetService("@mozilla.org/js/xpc/RuntimeService;1"));
+      if (rtsvc)
+      {
+        JSRuntime *rt = nullptr;
+        rtsvc->GetRuntime(&rt);
+        if (rt)
+          ::JS_GC(rt);
+      }
+
+      
       obsSvc->NotifyObservers(nullptr, "profile-before-change", kShutdownPersist);
       obsSvc->NotifyObservers(nullptr, "profile-before-change2", kShutdownPersist);
     }
