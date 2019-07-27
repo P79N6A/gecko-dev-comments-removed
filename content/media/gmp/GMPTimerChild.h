63ebee4a35d45122f84b68cd78b77cf0efc0e1ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:33:14 +0000

    Bug 1048107 - Add GMPTimer implementation and placeholder GMP clock. r=jesup
    UltraBlame original commit: 238e2c78035cf2d3837869b29aad00497b0716dc

---
+++ content/media/gmp/GMPTimerChild.h
@@ -0,0 +1,46 @@
+
+
+
+
+
+#ifndef GMPTimerChild_h_
+#define GMPTimerChild_h_
+
+#include "mozilla/gmp/PGMPTimerChild.h"
+#include "mozilla/Monitor.h"
+#include "nsDataHashtable.h"
+#include "nsHashKeys.h"
+#include "gmp-errors.h"
+#include "gmp-platform.h"
+
+namespace mozilla {
+namespace gmp {
+
+class GMPChild;
+
+class GMPTimerChild : public PGMPTimerChild
+{
+public:
+  NS_INLINE_DECL_REFCOUNTING(GMPTimerChild)
+
+  GMPTimerChild(GMPChild* aPlugin);
+
+  GMPErr SetTimer(GMPTask* aTask, int64_t aTimeoutMS);
+
+protected:
+  
+  virtual bool RecvTimerExpired(const uint32_t& aTimerId) MOZ_OVERRIDE;
+
+private:
+  ~GMPTimerChild();
+
+  nsDataHashtable<nsUint32HashKey, GMPTask*> mTimers;
+  uint32_t mTimerCount;
+
+  GMPChild* mPlugin;
+};
+
+} 
+} 
+
+#endif 
---
+++ content/media/gmp/GMPTimerParent.cpp
@@ -0,0 +1,88 @@
+
+
+
+
+
+#include "GMPTimerParent.h"
+#include "nsComponentManagerUtils.h"
+#include "mozilla/unused.h"
+
+namespace mozilla {
+namespace gmp {
+
+GMPTimerParent::GMPTimerParent(nsIThread* aGMPThread)
+  : mGMPThread(aGMPThread)
+{
+}
+
+bool
+GMPTimerParent::RecvSetTimer(const uint32_t& aTimerId,
+                             const uint32_t& aTimeoutMs)
+{
+  MOZ_ASSERT(mGMPThread == NS_GetCurrentThread());
+
+  nsresult rv;
+  nsAutoPtr<Context> ctx(new Context());
+  ctx->mTimer = do_CreateInstance("@mozilla.org/timer;1", &rv);
+  NS_ENSURE_SUCCESS(rv, true);
+
+  ctx->mId = aTimerId;
+  rv = ctx->mTimer->SetTarget(mGMPThread);
+  NS_ENSURE_SUCCESS(rv, true);
+  ctx->mParent = this;
+
+  rv = ctx->mTimer->InitWithFuncCallback(&GMPTimerParent::GMPTimerExpired,
+                                          ctx,
+                                          aTimeoutMs,
+                                          nsITimer::TYPE_ONE_SHOT);
+  NS_ENSURE_SUCCESS(rv, true);
+
+  mTimers.PutEntry(ctx.forget());
+
+  return true;
+}
+
+
+PLDHashOperator
+GMPTimerParent::CancelTimers(nsPtrHashKey<Context>* aContext, void* aClosure)
+{
+  auto context = aContext->GetKey();
+  context->mTimer->Cancel();
+  delete context;
+  return PL_DHASH_NEXT;
+}
+
+void
+GMPTimerParent::ActorDestroy(ActorDestroyReason aWhy)
+{
+  MOZ_ASSERT(mGMPThread == NS_GetCurrentThread());
+  mTimers.EnumerateEntries(GMPTimerParent::CancelTimers, nullptr);
+  mTimers.Clear();
+}
+
+
+void
+GMPTimerParent::GMPTimerExpired(nsITimer *aTimer, void *aClosure)
+{
+  MOZ_ASSERT(aClosure);
+  nsAutoPtr<Context> ctx(static_cast<Context*>(aClosure));
+  MOZ_ASSERT(ctx->mParent);
+  if (ctx->mParent) {
+    ctx->mParent->TimerExpired(ctx);
+  }
+}
+
+void
+GMPTimerParent::TimerExpired(Context* aContext)
+{
+  MOZ_ASSERT(mGMPThread == NS_GetCurrentThread());
+
+  uint32_t id = aContext->mId;
+  mTimers.RemoveEntry(aContext);
+  if (id) {
+    unused << SendTimerExpired(id);
+  }
+}
+
+} 
+} 
