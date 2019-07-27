b6fd7bd4a306158350f4522a57088961b91b29b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:29:20 +0000

    Bug 1147766 - Part 1: Add a mozilla::RangedArray class, for fixed length arrays with a non-zero base index. r=waldo
    UltraBlame original commit: a6fa096f98530c99d636cb849f1d5aa7adccd993

---
+++ mfbt/RangedArray.h
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ifndef mozilla_RangedArray_h
+#define mozilla_RangedArray_h
+
+#include "mozilla/Array.h"
+
+namespace mozilla {
+
+template<typename T, size_t MinIndex, size_t Length>
+class RangedArray
+{
+public:
+  T& operator[](size_t aIndex)
+  {
+    MOZ_ASSERT(MinIndex == 0 || aIndex > MinIndex);
+    return mArr[aIndex - MinIndex];
+  }
+
+  const T& operator[](size_t aIndex) const
+  {
+    MOZ_ASSERT(MinIndex == 0 || aIndex > MinIndex);
+    return mArr[aIndex - MinIndex];
+  }
+
+private:
+  Array<T, Length> mArr;
+};
+
+} 
+
+#endif 
