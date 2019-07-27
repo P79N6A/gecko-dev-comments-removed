72650c53d8362029cf8d6438e30af10ba0d87130
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:56:47 +0000

    Backed out changeset 38831e922852 (bug 1026314) for test_interfaces.html failures
    UltraBlame original commit: 225cc2ec22317156acd2bc5842f9c611a44da61f

--- dom/crypto/Key.h
+++ dom/crypto/Key.h
@@ -4,8 +4,8 @@
 
 
 
-#ifndef mozilla_dom_CryptoKey_h
-#define mozilla_dom_CryptoKey_h
+#ifndef mozilla_dom_Key_h
+#define mozilla_dom_Key_h
 
 #include "nsCycleCollectionParticipant.h"
 #include "nsWrapperCache.h"
@@ -53,13 +53,13 @@ namespace dom {
 
 
 
-class CryptoKey MOZ_FINAL : public nsISupports,
-                            public nsWrapperCache,
-                            public nsNSSShutDownObject
+class Key MOZ_FINAL : public nsISupports,
+                      public nsWrapperCache,
+                      public nsNSSShutDownObject
 {
 public:
   NS_DECL_CYCLE_COLLECTING_ISUPPORTS
-  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(CryptoKey)
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(Key)
 
   static const uint32_t CLEAR_EXTRACTABLE = 0xFFFFFFE;
   static const uint32_t EXTRACTABLE = 0x00000001;
@@ -86,7 +86,7 @@ public:
     UNWRAPKEY  = 0x00800000
   };
 
-  CryptoKey(nsIGlobalObject* aWindow);
+  Key(nsIGlobalObject* aWindow);
 
   nsIGlobalObject* GetParentObject() const
   {
@@ -154,7 +154,7 @@ public:
   bool ReadStructuredClone(JSStructuredCloneReader* aReader);
 
 private:
-  ~CryptoKey();
+  ~Key();
 
   nsRefPtr<nsIGlobalObject> mGlobal;
   uint32_t mAttributes; 
