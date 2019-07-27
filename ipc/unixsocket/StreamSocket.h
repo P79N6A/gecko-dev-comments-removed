2c1c7f29c015beeadad36aed999e7cd34bfbfa6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:16:51 +0000

    Bug 1093025: Add |StreamSocket|, r=kyle
    |StreamSocket| implements a |ConnectionOrientedSocket| for stream
    sockets. It utilizes |ListenSocket| to wait for incoming connection
    requests.
    
    UltraBlame original commit: 73e1dda14ec9fcdc43523bace69ac6cda2c86661

---
+++ ipc/unixsocket/StreamSocket.h
@@ -0,0 +1,93 @@
+
+
+
+
+
+
+#ifndef mozilla_ipc_streamsocket_h
+#define mozilla_ipc_streamsocket_h
+
+#include "mozilla/ipc/SocketBase.h"
+#include "ConnectionOrientedSocket.h"
+
+namespace mozilla {
+namespace ipc {
+
+class StreamSocketIO;
+class UnixSocketConnector;
+
+class StreamSocket : public SocketConsumerBase
+                   , public ConnectionOrientedSocket
+{
+public:
+  StreamSocket();
+
+  
+
+
+
+
+
+
+
+  bool SendSocketData(UnixSocketRawData* aMessage);
+
+  
+
+
+
+
+
+
+
+
+  bool SendSocketData(const nsACString& aMessage);
+
+  
+
+
+
+
+
+
+
+
+
+  bool Connect(UnixSocketConnector* aConnector,
+               const char* aAddress,
+               int aDelayMs = 0);
+
+  
+
+
+
+  void Close();
+
+  
+
+
+  void GetSocketAddr(nsAString& aAddrStr);
+
+protected:
+  virtual ~StreamSocket();
+
+  
+  
+  
+  ConnectionOrientedSocketIO* PrepareAccept(UnixSocketConnector* aConnector);
+
+private:
+
+  
+  void CloseSocket() MOZ_OVERRIDE
+  {
+    Close();
+  }
+
+  StreamSocketIO* mIO;
+};
+
+} 
+} 
+
+#endif 
