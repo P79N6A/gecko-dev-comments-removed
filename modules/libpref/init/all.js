80b75412316857d34485f850f061852120f26680
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:59:28 +0000

    Bug 1128767 - Add the layers.dump-decision pref to all.js when it's available. r=roc
    UltraBlame original commit: 24aa35a0cca83db2373b4afd3abc1012342db410

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3922,6 +3922,7 @@ pref("layers.dump", false);
 #ifdef MOZ_DUMP_PAINTING
 
 pref("layers.dump-texture", false);
+pref("layers.dump-decision", false);
 #endif
 pref("layers.draw-borders", false);
 pref("layers.draw-tile-borders", false);
