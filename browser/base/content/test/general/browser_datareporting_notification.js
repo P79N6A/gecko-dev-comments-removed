d3174a171adef1c71f0e0b297fba784596612333
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:28:41 +0000

    Bug 1063696 - fix datareporting timeout orange by not closing the prefs tab prematurely. r=gfritzsche
    UltraBlame original commit: 5c47fd7e9b4aa5f06bb6211daa5410443f81a4e3

--- browser/base/content/test/general/browser_datareporting_notification.js
+++ browser/base/content/test/general/browser_datareporting_notification.js
@@ -132,7 +132,7 @@ function test_multiple_windows() {
 
     let [policy, promise] = sendNotifyRequest("multiple_window_behavior");
     let displayCount = 0;
-    let prefWindowClosed = false;
+    let prefWindowOpened = false;
     let mutationObserversRemoved = false;
 
     function onAlertDisplayed() {
@@ -147,8 +147,8 @@ function test_multiple_windows() {
       
       
       function maybeFinish() {
-        if (!prefWindowClosed) {
-          dump("Not finishing test yet because pref pane isn't closed.\n");
+        if (!prefWindowOpened) {
+          dump("Not finishing test yet because pref pane hasn't yet appeared.\n");
           return;
         }
 
@@ -200,9 +200,7 @@ function test_multiple_windows() {
 
         ok(true, "Advanced preferences opened on info bar button press.");
         executeSoon(function soon() {
-          dump("Closing preferences.\n");
-          prefWin.close();
-          prefWindowClosed = true;
+          prefWindowOpened = true;
           maybeFinish();
         });
       }, "advanced-pane-loaded", false);
