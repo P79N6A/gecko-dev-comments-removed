74ddb6db4b075399cea619f240779fb47252d7b5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:27:45 +0000

    Bug 1142727 - Do not intercept sandboxed iframes with service workers; r=nsm
    This is temporary until we clarify what we want to do with these
    iframes in the spec.
    
    UltraBlame original commit: 2c2fa3984d844d598cb231c1f10637cd92db2452

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -13950,6 +13950,11 @@ NS_IMETHODIMP
 nsDocShell::ShouldPrepareForIntercept(nsIURI* aURI, bool aIsNavigate, bool* aShouldIntercept)
 {
   *aShouldIntercept = false;
+  if (mSandboxFlags) {
+    
+    return NS_OK;
+  }
+
   nsCOMPtr<nsIServiceWorkerManager> swm = mozilla::services::GetServiceWorkerManager();
   if (!swm) {
     return NS_OK;
