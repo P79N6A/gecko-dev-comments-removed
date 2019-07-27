48c6968c8e05478b94ff24d897cdc42e69c2a81b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:02:25 +0000

    Bug 1094984 - B2G RIL: Handle empty record for EFPNN more properly. r=Edgar
    UltraBlame original commit: d1b7ed051d447ddf8c8ce50222ec109020ec7d7f

--- dom/system/gonk/ril_worker.js
+++ dom/system/gonk/ril_worker.js
@@ -13967,13 +13967,10 @@ SimRecordHelperObject.prototype = {
       }
       Buf.readStringDelimiter(strLen);
 
-      if (pnnElement) {
-        pnn.push(pnnElement);
-      }
+      pnn.push(pnnElement);
 
       let RIL = this.context.RIL;
-      
-      if (pnnElement && options.p1 < options.totalRecords) {
+      if (options.p1 < options.totalRecords) {
         ICCIOHelper.loadNextRecord(options);
       } else {
         if (DEBUG) {
