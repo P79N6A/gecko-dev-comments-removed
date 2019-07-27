be88d7da9a10600866237fe67e7b793fb4e2c741
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:22:22 +0000

    Bug 1134872 - Fixing getElementValueOfCssProperty to support chrome elements. r=dburns
    UltraBlame original commit: efa7c6cf88fce711716619cb1b37035fa48387bc

---
+++ testing/marionette/client/marionette/tests/unit/test_chrome_element_css.py
@@ -0,0 +1,21 @@
+
+
+
+
+from marionette import MarionetteTestCase
+
+
+class TestChromeElementCSS(MarionetteTestCase):
+
+    def test_we_can_get_css_value_on_chrome_element(self):
+        self.marionette.navigate("about:blank")
+        with self.marionette.using_context("chrome"):
+            element = self.marionette.find_element("id", "page-proxy-favicon")
+            favicon_image = element.value_of_css_property("list-style-image")
+
+            self.assertIn("identity-icons-generic.png", favicon_image)
+
+            element = self.marionette.find_element("id", "identity-box")
+            background_colour = element.value_of_css_property("background-color")
+
+            self.assertEqual("transparent", background_colour)
