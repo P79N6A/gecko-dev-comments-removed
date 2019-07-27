5f99aba4df5400da6f273f85c6b81b5d47d8826a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:25:36 +0000

    Bug 783829 - Some tests for the enumerate trap. r=efaust
    UltraBlame original commit: b1346d8602279a8f43a6239bd93596c3520e942d

---
+++ js/src/jit-test/tests/proxy/testDirectProxyEnumerate2.js
@@ -0,0 +1,16 @@
+
+load(libdir + "asserts.js");
+
+let handler = new Proxy({}, {
+    get: function(target, name) {
+        assertEq(name, "enumerate");
+        throw new SyntaxError();
+    }
+});
+
+let proxy = new Proxy({}, handler);
+
+assertThrowsInstanceOf(function() {
+    for (let x in proxy)
+        assertEq(true, false);
+}, SyntaxError)
---
+++ js/src/jit-test/tests/proxy/testDirectProxyEnumerate4.js
@@ -0,0 +1,15 @@
+
+load(libdir + "asserts.js");
+
+let handler = {
+    enumerate: function() {
+        return undefined;
+    }
+};
+
+let proxy = new Proxy({}, handler);
+
+assertThrowsInstanceOf(function() {
+    for (let x in proxy)
+        assertEq(true, false);
+}, TypeError)
