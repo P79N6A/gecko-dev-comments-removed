d3fb01f055566141b106295e9ae046314741381c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:59:45 +0000

    Bug 1171602 - Run mochitest using mach from a tests.zip, r=chmanchester
    UltraBlame original commit: 43e3ece52f5572b6a11b39d15d89eb4d6620a580

---
+++ testing/mochitest/mach_test_package_commands.py
@@ -0,0 +1,41 @@
+
+
+
+
+from __future__ import unicode_literals
+
+from argparse import Namespace
+import os
+
+from mach.decorators import (
+    CommandProvider,
+    Command,
+)
+
+
+def run_mochitest(context, **kwargs):
+    args = Namespace(**kwargs)
+    args.certPath = context.certs_dir
+    args.utilityPath = context.bin_dir
+    args.extraProfileFiles.append(os.path.join(context.bin_dir, 'plugins'))
+
+    from runtests import run_test_harness
+    return run_test_harness(args)
+
+
+def setup_argument_parser():
+    from mochitest_options import MochitestArgumentParser
+    return MochitestArgumentParser(app='generic')
+
+
+@CommandProvider
+class MochitestCommands(object):
+
+    def __init__(self, context):
+        self.context = context
+
+    @Command('mochitest', category='testing',
+             description='Run the mochitest harness.',
+             parser=setup_argument_parser)
+    def mochitest(self, **kwargs):
+        return run_mochitest(self.context, **kwargs)
