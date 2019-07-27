107d60a0a8c5948dd07111c9ce922b46a7d4958f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:31:56 +0000

    Bug 1046945 - Move ExposeGCThingToActiveJS to js::gc:: and use the typed wrappers instead; r=jonco
    UltraBlame original commit: 5b2dab72041e96b0a45fb05336d788238a025947

--- js/src/vm/Debugger.cpp
+++ js/src/vm/Debugger.cpp
@@ -2874,7 +2874,7 @@ Debugger::findAllGlobals(JSContext *cx, unsigned argc, Value *vp)
 
 
 
-            JS::ExposeGCThingToActiveJS(global, JSTRACE_OBJECT);
+            JS::ExposeObjectToActiveJS(global);
 
             RootedValue globalValue(cx, ObjectValue(*global));
             if (!dbg->wrapDebuggeeValue(cx, &globalValue))
