0d4a5ac93630ff4f914e3a8d1638a472ad3ab25e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:08:00 +0000

    Bug 1022612. Part 24: Simplify nsDisplayList::ComputeVisibilityForSublist now that FrameLayerBuilder does most of the work. r=mattwoodrow
    UltraBlame original commit: f5ec6bcf251fc6f53ba7a235543399cfc4579c66

--- layout/base/nsDisplayList.h
+++ layout/base/nsDisplayList.h
@@ -1687,9 +1687,6 @@ private:
   
   void* operator new(size_t sz) CPP_THROW_NEW;
   
-  
-  void FlattenTo(nsTArray<nsDisplayItem*>* aElements);
-  
   nsDisplayItemLink  mSentinel;
   nsDisplayItemLink* mTop;
 
