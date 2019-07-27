8370cbcee7ed92a09b6c206f9c6e896163879b50
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:33:06 +0000

    Bug 1017988 part 5. Store the set of known global names on our outermost IDLScope. r=khuey
    UltraBlame original commit: 3c98d58a6168963f7edecedf2ac186db443a81e3

--- dom/bindings/parser/WebIDL.py
+++ dom/bindings/parser/WebIDL.py
@@ -9,6 +9,7 @@ import re
 import os
 import traceback
 import math
+from collections import defaultdict
 
 
 
@@ -219,6 +220,10 @@ class IDLScope(IDLObject):
             self._name = None
 
         self._dict = {}
+        self.globalNames = set()
+        
+        
+        self.globalNameMapping = defaultdict(set)
 
     def __str__(self):
         return self.QName()
@@ -1097,6 +1102,9 @@ class IDLInterface(IDLObjectWithScope):
                     self.globalNames = attr.args()
                 else:
                     self.globalNames = [ self.identifier.name ]
+                self.parentScope.globalNames.update(self.globalNames)
+                for globalName in self.globalNames:
+                    self.parentScope.globalNameMapping[globalName].add(self.identifier.name)
                 self._isOnGlobalProtoChain = True
             elif (identifier == "NeedNewResolve" or
                   identifier == "OverrideBuiltins" or
