84d5c6571dd05158d282e8a07e565692af87376a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:10:56 +0000

    Bug 983504 - ViECapturer changes for screen sharing. r=jesup
    UltraBlame original commit: 911234d51c81820df05b0b7222289a6543e316a4

--- media/webrtc/trunk/webrtc/video_engine/include/vie_capture.h
+++ media/webrtc/trunk/webrtc/video_engine/include/vie_capture.h
@@ -47,6 +47,19 @@ struct CaptureCapability {
   }
 };
 
+enum CaptureDeviceType {
+  Camera = 0,
+  Screen = 1,
+  Application = 2,
+  Window = 3
+};
+struct CaptureDeviceInfo {
+  CaptureDeviceType type;
+
+  CaptureDeviceInfo() : type(CaptureDeviceType::Camera) {}
+  CaptureDeviceInfo(CaptureDeviceType t) : type(t) {}
+};
+
 
 enum Brightness {
   Normal = 0,
@@ -257,4 +270,4 @@ class WEBRTC_DLLEXPORT ViECapture {
 
 }  
 
-#endif  
+#endif
