f77e7b4998991d1d7397910738638df03e16ca38
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:29:27 +0000

    Bug 1107666, Part 2: Further fix for SSL_OCSP_STAPLING telemetry, r=keeler
    UltraBlame original commit: 33d139e87c8901cf69ad42ef8cd704a9079b627e

--- security/certverifier/NSSCertDBTrustDomain.cpp
+++ security/certverifier/NSSCertDBTrustDomain.cpp
@@ -372,6 +372,10 @@ NSSCertDBTrustDomain::CheckRevocation(EndEntityOrCA endEntityOrCA,
   
   
   
+  
+  
+  
+  
   Result stapledOCSPResponseResult = Success;
   if (stapledOCSPResponse) {
     PR_ASSERT(endEntityOrCA == EndEntityOrCA::MustBeEndEntity);
@@ -401,7 +405,7 @@ NSSCertDBTrustDomain::CheckRevocation(EndEntityOrCA endEntityOrCA,
              ("NSSCertDBTrustDomain: stapled OCSP response: failure"));
       return stapledOCSPResponseResult;
     }
-  } else {
+  } else if (endEntityOrCA == EndEntityOrCA::MustBeEndEntity) {
     
     mOCSPStaplingStatus = CertVerifier::OCSP_STAPLING_NONE;
     PR_LOG(gCertVerifierLog, PR_LOG_DEBUG,
