58d282ff9792bade189dd0afec7065225567cd73
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:33:52 +0000

    Bug 901952 - RSS live bookmark history should get updated after Recent history is cleared. r=mak
    UltraBlame original commit: ce0eabb6eb5a3921d72f2a91426b56984195c4bc

--- toolkit/components/places/nsLivemarkService.js
+++ toolkit/components/places/nsLivemarkService.js
@@ -472,7 +472,11 @@ LivemarkService.prototype = {
   onPageChanged:      function () {},
   onTitleChanged:     function () {},
   onDeleteVisits:     function () {},
-  onClearHistory:     function () {},
+  onClearHistory() {
+    for each (let livemark in this._livemarks) {
+      livemark.updateURIVisitedStatus(null, false);
+    }
+  },
 
   onDeleteURI: function PS_onDeleteURI(aURI) {
     for each (let livemark in this._livemarks) {
@@ -803,11 +807,20 @@ Livemark.prototype = {
     }
   },
 
+  
+
+
+
+
+
+
+
+
   updateURIVisitedStatus:
   function LM_updateURIVisitedStatus(aURI, aVisitedStatus)
   {
     for (let i = 0; i < this.children.length; i++) {
-      if (this.children[i].uri.equals(aURI)) {
+      if (!aURI || this.children[i].uri.equals(aURI)) {
         this.children[i].visited = aVisitedStatus;
       }
     }
@@ -819,7 +832,7 @@ Livemark.prototype = {
         let nodes = this._nodes.get(container);
         for (let j = 0; j < nodes.length; j++) {
           let node = nodes[j];
-          if (node.uri == aURI.spec) {
+          if (!aURI || node.uri == aURI.spec) {
             Services.tm.mainThread.dispatch((function () {
               observer.nodeHistoryDetailsChanged(node, 0, aVisitedStatus);
             }).bind(this), Ci.nsIThread.DISPATCH_NORMAL);
