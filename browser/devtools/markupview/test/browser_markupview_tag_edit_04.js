35937c87d4c325b297aadbff13628d99f3e47897
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:12:01 +0000

    Bug 1094622 - Deleting node in inspector now selects previous sibling and not parent. r=bgrins
    ---
     browser/devtools/markupview/markup-view.js         | 15 +++++----
     .../test/browser_markupview_tag_edit_04.js         | 38 +++++++++++++++-------
     browser/devtools/markupview/test/head.js           |  2 +-
     toolkit/devtools/server/actors/inspector.js        | 14 +++++++-
     .../tests/mochitest/test_inspector-remove.html     | 23 +++++++++----
     5 files changed, 65 insertions(+), 27 deletions(-)
    
    UltraBlame original commit: 8f2f3001ab0b293d7c1a76bef5ec988a57ee307e

--- browser/devtools/markupview/test/browser_markupview_tag_edit_04.js
+++ browser/devtools/markupview/test/browser_markupview_tag_edit_04.js
@@ -6,25 +6,39 @@
 
 
 
-const TEST_URL = "data:text/html,<div id='delete-me'></div>";
 
-let test = asyncTest(function*() {
-  let {toolbox, inspector} = yield addTab(TEST_URL).then(openInspector);
 
-  info("Selecting the test node by clicking on it to make sure it receives focus");
-  let node = content.document.querySelector("#delete-me");
-  yield clickContainer("#delete-me", inspector);
+const TEST_URL = "data:text/html,<div id='parent'><div id='first'></div><div id='second'></div><div id='third'></div></div>";
 
-  info("Deleting the element with the keyboard");
+function* checkDeleteAndSelection(inspector, nodeSelector, focusedNodeSelector) {
+  yield selectNode(nodeSelector, inspector);
+  yield clickContainer(nodeSelector, inspector);
+
+  info("Deleting the element \"" + nodeSelector + "\" with the keyboard");
   let mutated = inspector.once("markupmutation");
   EventUtils.sendKey("delete", inspector.panelWin);
-  yield mutated;
+
+  yield Promise.all([mutated, inspector.once("inspector-updated")]);
+
+  let nodeFront = yield getNodeFront(focusedNodeSelector, inspector);
+  is(inspector.selection.nodeFront, nodeFront,
+    focusedNodeSelector + " should be selected after " + nodeSelector + " node gets deleted.");
 
   info("Checking that it's gone, baby gone!");
-  ok(!content.document.querySelector("#delete-me"), "The test node does not exist");
+  ok(!content.document.querySelector(nodeSelector), "The test node does not exist");
 
   yield undoChange(inspector);
-  ok(content.document.querySelector("#delete-me"), "The test node is back!");
+  ok(content.document.querySelector(nodeSelector), "The test node is back!");
+}
+
+let test = asyncTest(function*() {
+  let {inspector} = yield addTab(TEST_URL).then(openInspector);
+
+  info("Selecting the test node by clicking on it to make sure it receives focus");
+
+  yield checkDeleteAndSelection(inspector, "#first", "#parent");
+  yield checkDeleteAndSelection(inspector, "#second", "#first");
+  yield checkDeleteAndSelection(inspector, "#third", "#second");
 
   yield inspector.once("inspector-updated");
 });
