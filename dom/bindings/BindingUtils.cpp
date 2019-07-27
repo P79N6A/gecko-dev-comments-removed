9822135b4d149b69a13a77f8c84728212dd53d3d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:51:47 +0000

    Bug 1125351 - Make nsIDocShell.isPrerendered infallible; r=bzbarsky
    UltraBlame original commit: fb315ec11af4f5d0d9b297a66bf0d88a205cbb1e

--- dom/bindings/BindingUtils.cpp
+++ dom/bindings/BindingUtils.cpp
@@ -2415,9 +2415,7 @@ EnforceNotInPrerendering(JSContext* aCx, JSObject* aObj)
     return true;
   }
 
-  bool isPrerendered = false;
-  docShell->GetIsPrerendered(&isPrerendered);
-  if (isPrerendered) {
+  if (docShell->GetIsPrerendered()) {
     HandlePrerenderingViolation(window);
     
     
