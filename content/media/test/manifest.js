1e98987715c9e9958b3750e4a2587c4072c85e38
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:13:01 +0000

    Backed out changeset 2ccde1bdcb7f (bug 902686) for android m3 test failures
    UltraBlame original commit: b0cc3073ef81579b8e9f36be0d82d9ca96fdae0b

--- content/media/test/manifest.js
+++ content/media/test/manifest.js
@@ -666,8 +666,7 @@ function MediaTestManager() {
     this.numTestsRunning = 0;
     
     SimpleTest.waitForExplicitFinish();
-
-    setMediaTestsPrefs(function() {this.nextTest()}.bind(this));
+    this.nextTest();
   }
 
   
@@ -772,9 +771,42 @@ function mediaTestCleanup() {
     SpecialPowers.forceGC();
 }
 
-function setMediaTestsPrefs(aCallback, aExtraPrefs) {
-    var prefs = [["media.preload.default", 2], ["media.preload.auto", 3]];
-    if (aExtraPrefs)
-      prefs = prefs.concat(aExtraPrefs);
-    SpecialPowers.pushPrefEnv({"set": prefs}, aCallback);
-}
+(function() {
+  
+  var prefService = SpecialPowers.wrap(SpecialPowers.Components)
+                                 .classes["@mozilla.org/preferences-service;1"]
+                                 .getService(SpecialPowers.Ci.nsIPrefService);
+  var branch = prefService.getBranch("media.");
+  var oldDefault = 2;
+  var oldAuto = 3;
+  var oldAppleMedia = undefined;
+  var oldGStreamer = undefined;
+  var oldOpus = undefined;
+
+  try { oldAppleMedia = SpecialPowers.getBoolPref("media.apple.mp3.enabled"); } catch(ex) { }
+  try { oldGStreamer = SpecialPowers.getBoolPref("media.gstreamer.enabled"); } catch(ex) { }
+  try { oldDefault   = SpecialPowers.getIntPref("media.preload.default"); } catch(ex) { }
+  try { oldAuto      = SpecialPowers.getIntPref("media.preload.auto"); } catch(ex) { }
+  try { oldOpus      = SpecialPowers.getBoolPref("media.opus.enabled"); } catch(ex) { }
+
+  SpecialPowers.setIntPref("media.preload.default", 2); 
+  SpecialPowers.setIntPref("media.preload.auto", 3); 
+  
+  if (oldOpus !== undefined)
+    SpecialPowers.setBoolPref("media.opus.enabled", true);
+  if (oldGStreamer !== undefined)
+    SpecialPowers.setBoolPref("media.gstreamer.enabled", true);
+  if (oldAppleMedia !== undefined)
+    SpecialPowers.setBoolPref("media.apple.mp3.enabled", true);
+
+  window.addEventListener("unload", function() {
+    if (oldGStreamer !== undefined)
+      SpecialPowers.setBoolPref("media.gstreamer.enabled", oldGStreamer);
+    if (oldAppleMedia !== undefined)
+      SpecialPowers.setBoolPref("media.apple.mp3.enabled", oldAppleMedia);
+    SpecialPowers.setIntPref("media.preload.default", oldDefault);
+    SpecialPowers.setIntPref("media.preload.auto", oldAuto);
+    if (oldOpus !== undefined)
+      SpecialPowers.setBoolPref("media.opus.enabled", oldOpus);
+  }, false);
+ })();
