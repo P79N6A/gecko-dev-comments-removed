6316c580847fd83fb7b4e153445c2310a2612085
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:26:23 +0000

    Bug 1111246 - Add error message in Marionette client on malformed packet. r=dburns
    UltraBlame original commit: 97565b594f027c93656e97a7d0b5b23dd42a6247

---
+++ testing/marionette/client/marionette/tests/unit/test_marionette.py
@@ -0,0 +1,22 @@
+
+
+
+
+import errors
+import marionette_test
+
+
+class TestHandleError(marionette_test.MarionetteTestCase):
+    def test_malformed_packet(self):
+        for t in [{}, {"error": None}]:
+            with self.assertRaisesRegexp(errors.MarionetteException, "Malformed packet"):
+                self.marionette._handle_error(t)
+
+    def test_known_error_code(self):
+        with self.assertRaises(errors.NoSuchElementException):
+            self.marionette._handle_error(
+                {"error": {"status": errors.ErrorCodes.NO_SUCH_ELEMENT}})
+
+    def test_unknown_error_code(self):
+        with self.assertRaises(errors.MarionetteException):
+            self.marionette._handle_error({"error": {"status": 123456}})
