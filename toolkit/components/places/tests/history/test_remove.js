d623bc073e7acd5607a3cc13d37000eb41a26b7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:45:23 +0000

    Backed out changeset c426c7eb3b43 (bug 1076775) for XPC Shell Test failures
    UltraBlame original commit: 3bc058c3acfeeb1ff51b08f45c234ea5ccf323a2

--- toolkit/components/places/tests/history/test_remove.js
+++ toolkit/components/places/tests/history/test_remove.js
@@ -3,6 +3,9 @@
 
 
 
+
+
+
 "use strict";
 
 Cu.importGlobalProperties(["URL"]);
@@ -10,10 +13,6 @@ Cu.importGlobalProperties(["URL"]);
 
 
 add_task(function* test_remove_single() {
-  yield PlacesTestUtils.clearHistory();
-  yield PlacesUtils.bookmarks.eraseEverything();
-
-
   let WITNESS_URI = NetUtil.newURI("http://mozilla.com/test_browserhistory/test_remove/" + Math.random());
   yield PlacesTestUtils.addVisits(WITNESS_URI);
   Assert.ok(page_in_database(WITNESS_URI));
@@ -90,15 +89,13 @@ add_task(function* test_remove_single() {
     let removed = false;
     if (options.useCallback) {
       let onRowCalled = false;
-      let guid = do_get_guid_for_uri(uri);
-      let frecency = frecencyForUrl(uri);
       removed = yield PlacesUtils.history.remove(removeArg, page => {
         Assert.equal(onRowCalled, false, "Callback has not been called yet");
         onRowCalled = true;
         Assert.equal(page.url.href, uri.spec, "Callback provides the correct url");
-        Assert.equal(page.guid, guid, "Callback provides the correct guid");
+        Assert.equal(page.guid, do_get_guid_for_uri(uri), "Callback provides the correct guid");
         Assert.equal(page.title, title, "Callback provides the correct title");
-        Assert.equal(page.frecency, frecency, "Callback provides the correct frecency");
+        Assert.equal(page.frecency, frecencyForUrl(uri), "Callback provides the correct frecency");
       });
       Assert.ok(onRowCalled, "Callback has been called");
     } else {
@@ -141,9 +138,6 @@ add_task(function* test_remove_single() {
 
 add_task(function* test_remove_many() {
   const SIZE = 10;
-
-  yield PlacesTestUtils.clearHistory();
-  yield PlacesUtils.bookmarks.eraseEverything();
 
   do_print("Adding a witness page");
   let WITNESS_URI = NetUtil.newURI("http://mozilla.com/test_browserhistory/test_remove/" + Math.random());;
@@ -272,11 +266,10 @@ add_task(function* test_remove_many() {
 
   Assert.notEqual(visits_in_database(WITNESS_URI), 0, "Witness URI still has visits");
   Assert.notEqual(page_in_database(WITNESS_URI), 0, "Witness URI is still here");
-});
 
-add_task(function* cleanup() {
+  do_print("Cleaning up");
   yield PlacesTestUtils.clearHistory();
-  yield PlacesUtils.bookmarks.eraseEverything();  
+
 });
 
 
