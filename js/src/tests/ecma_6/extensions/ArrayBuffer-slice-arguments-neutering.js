805c8abfea34d86d930066a68a8c09fe0a37c141
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:00:23 +0000

    Bug 991981 - Test for ArrayBuffer.prototype.slice.  r=sfink
    UltraBlame original commit: 17e0e70a8b5458d39156aa1c475a5054bfd7719b

---
+++ js/src/tests/ecma_6/extensions/ArrayBuffer-slice-arguments-neutering.js
@@ -0,0 +1,83 @@
+
+
+
+
+
+
+var gTestfile = "ArrayBuffer-slice-arguments-neutering.js";
+
+var BUGNUMBER = 991981;
+var summary =
+  "ArrayBuffer.prototype.slice shouldn't misbehave horribly if " +
+  "index-argument conversion neuters the ArrayBuffer being sliced";
+
+print(BUGNUMBER + ": " + summary);
+
+
+
+
+
+function testStart(dataType)
+{
+  var ab = new ArrayBuffer(0x1000);
+
+  var start =
+    {
+      valueOf: function()
+      {
+        neuter(ab, dataType);
+        gc();
+        return 0x800;
+      }
+    };
+
+  var ok = false;
+  try
+  {
+    ab.slice(start);
+  }
+  catch (e)
+  {
+    ok = true;
+  }
+  assertEq(ok, true, "start weirdness should have thrown");
+  assertEq(ab.byteLength, 0, "neutering should work for start weirdness");
+}
+testStart("change-data");
+testStart("same-data");
+
+function testEnd(dataType)
+{
+  var ab = new ArrayBuffer(0x1000);
+
+  var end =
+    {
+      valueOf: function()
+      {
+        neuter(ab, dataType);
+        gc();
+        return 0x1000;
+      }
+    };
+
+  var ok = false;
+  try
+  {
+    ab.slice(0x800, end);
+  }
+  catch (e)
+  {
+    ok = true;
+  }
+  assertEq(ok, true, "byteLength weirdness should have thrown");
+  assertEq(ab.byteLength, 0, "neutering should work for byteLength weirdness");
+}
+testEnd("change-data");
+testEnd("same-data");
+
+
+
+if (typeof reportCompare === "function")
+  reportCompare(true, true);
+
+print("Tests complete");
