6340a18eee2d1b8c2935b009257e13e25ea71770
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:57:16 +0000

    Bug 994133 - Make sure allocated memory has the upper 17 bits clear on NetBSD/sparc64. r=luke
    UltraBlame original commit: 9ea1e520692ea3edb9f85265920e088eaf47bf45

--- js/src/gc/Memory.cpp
+++ js/src/gc/Memory.cpp
@@ -379,7 +379,7 @@ static inline void *
 MapMemoryAt(void *desired, size_t length, int prot = PROT_READ | PROT_WRITE,
             int flags = MAP_PRIVATE | MAP_ANON, int fd = -1, off_t offset = 0)
 {
-#if defined(__ia64__)
+#if defined(__ia64__) || (defined(__sparc64__) && defined(__NetBSD__))
     MOZ_ASSERT(0xffff800000000000ULL & (uintptr_t(desired) + length - 1) == 0);
 #endif
     void *region = mmap(desired, length, prot, flags, fd, offset);
@@ -402,7 +402,7 @@ static inline void *
 MapMemory(size_t length, int prot = PROT_READ | PROT_WRITE,
           int flags = MAP_PRIVATE | MAP_ANON, int fd = -1, off_t offset = 0)
 {
-#if defined(__ia64__)
+#if defined(__ia64__) || (defined(__sparc64__) && defined(__NetBSD__))
     
 
 
