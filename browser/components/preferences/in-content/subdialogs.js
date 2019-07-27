0f5edd0dc19ed18adf340eac4149e4eb9e89dea1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:04:35 +0000

    Bug 1044597 - Limit the minimum size of subdialogs in the in-content preferences. r=Gijs
    UltraBlame original commit: c6aca5f18fdf9cdb065470f6250acbf4255c03a6

--- browser/components/preferences/in-content/subdialogs.js
+++ browser/components/preferences/in-content/subdialogs.js
@@ -92,6 +92,8 @@ let gSubDialog = {
     
     this._box.removeAttribute("width");
     this._box.removeAttribute("height");
+    this._box.style.removeProperty("min-height");
+    this._box.style.removeProperty("min-width");
 
     setTimeout(() => {
       
@@ -146,10 +148,17 @@ let gSubDialog = {
 
     
     
-    let paddingBottom = parseFloat(this._frame.contentWindow.getComputedStyle(docEl).paddingBottom);
+    let frameVerticalPadding = 2 * parseFloat(this._frame.contentWindow.getComputedStyle(docEl).paddingTop);
+    let frameHorizontalPadding = 2 * parseFloat(this._frame.contentWindow.getComputedStyle(docEl).paddingLeft);
+    let frameWidth = parseFloat(docEl.style.width) || docEl.scrollWidth;
+    let frameHeight = parseFloat(docEl.style.height) || (docEl.scrollHeight + frameVerticalPadding);
+    let boxVerticalBorder = 2 * parseFloat(getComputedStyle(this._box).borderTopWidth);
+    let boxHorizontalBorder = 2 * parseFloat(getComputedStyle(this._box).borderLeftWidth);
 
-    this._frame.style.width = docEl.style.width || docEl.scrollWidth + "px";
-    this._frame.style.height = docEl.style.height || (docEl.scrollHeight + paddingBottom) + "px";
+    this._frame.style.width = frameWidth + "px";
+    this._frame.style.height = frameHeight + "px";
+    this._box.style.minHeight = (boxVerticalBorder + this._box.clientHeight) + "px";
+    this._box.style.minWidth = (boxHorizontalBorder + frameHorizontalPadding + this._box.clientWidth) + "px";
 
     this._overlay.style.visibility = "visible";
     this._frame.focus();
