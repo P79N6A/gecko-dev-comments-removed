c5b710fd0bae01e0752169d6f20767f0b9149b66
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:32:18 +0000

    Bug 996796 patch 16 - Add comment about potential performance impromevent to RuleNodeWithReplacement.  r=heycam
    UltraBlame original commit: 06ffae59cea913a6d36f5e99bdd1755583e3dc30

--- layout/style/nsStyleSet.cpp
+++ layout/style/nsStyleSet.cpp
@@ -1341,6 +1341,11 @@ nsStyleSet::RuleNodeWithReplacement(Element* aElement,
                     nsPrintfCString("unexpected replacement bits 0x%lX",
                                     uint32_t(aReplacements)).get());
 
+  
+  
+  
+  
+
   nsTArray<RuleNodeInfo> rules;
   for (nsRuleNode* ruleNode = aOldRuleNode; !ruleNode->IsRoot();
        ruleNode = ruleNode->GetParent()) {
