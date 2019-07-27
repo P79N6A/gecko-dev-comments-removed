3cac864fbe3df3ae6d2033808f5a5d87fd12029b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:12:34 +0000

    Bug 963358 - [e10s] Crashed tab page's Try Again button doesn't always work. r=billm.
    Instead of attaching / listening for click events in BrowserOnClick for the
    Try Again button, we instead bubble up an event when the Try Again button
    is clicked. That event has a listener set up for it that is established during
    delayedStartup, so it has a very high probability of being set. This bypasses
    some error cases where we hadn't successfully attached BrowserOnClick in the past.
    
    UltraBlame original commit: 16a220a579a67420a0f57ab20c8ca41a0d30a65a

--- browser/base/content/aboutTabCrashed.js
+++ browser/base/content/aboutTabCrashed.js
@@ -12,6 +12,22 @@ function parseQueryString() {
 
 document.title = parseQueryString();
 
+addEventListener("DOMContentLoaded", () => {
+  let tryAgain = document.getElementById("tryAgain");
+  let sendCrashReport = document.getElementById("checkSendReport");
+
+  tryAgain.addEventListener("click", () => {
+    let event = new CustomEvent("AboutTabCrashedTryAgain", {
+      bubbles: true,
+      detail: {
+        sendCrashReport: sendCrashReport.checked,
+      },
+    });
+
+    document.dispatchEvent(event);
+  });
+});
+
 
 var event = new CustomEvent("AboutTabCrashedLoad", {bubbles:true});
 document.dispatchEvent(event);
