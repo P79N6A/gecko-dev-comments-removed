e43dbd3eee1b5ba9b748f1cb34d1c4d26cabd7cd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:08:32 +0000

    Bug 1125713 - Browser API: Add event for page download progress, r=fabrice
    UltraBlame original commit: c7c68c4389d666bfbfe9d18281457eba669896ef

--- dom/browser-element/mochitest/browserElement_LoadEvents.js
+++ dom/browser-element/mochitest/browserElement_LoadEvents.js
@@ -14,6 +14,7 @@ function runTest() {
   
   
   
+  
 
   var seenLoadEnd = false;
   var seenLoadStart = false;
@@ -74,6 +75,7 @@ function runTest2() {
   var seenLoadStart = false;
   var seenLoadEnd = false;
   var seenLocationChange = false;
+  var seenLoadProgressChanged = false;
 
   
   document.addEventListener('mozbrowserloadstart', function(e) {
@@ -101,6 +103,14 @@ function runTest2() {
     ok(seenLoadStart, 'Load end after load start.');
     ok(seenLocationChange, 'Load end after location change.');
     is(e.detail.backgroundColor, 'transparent', 'Expected background color reported')
+  });
+
+  iframe.addEventListener('mozbrowserloadprogresschanged', function(e) {
+    ok(e.isTrusted, 'Event should be trusted.');
+    seenLoadProgressChanged = true;
+    ok(seenLoadStart, 'Load end after load start.');
+    ok(seenLocationChange, 'Load end after location change.');
+    ok(!seenLoadEnd, 'Load end after load progress.');
   });
 
   iframe.src = browserElementTestHelpers.emptyPage2;
