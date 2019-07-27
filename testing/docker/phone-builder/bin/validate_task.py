d8a0f23d57f3e54087c90aecf59cab0c07e67d67
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:52:28 +0000

    Bug 1098467: Validate the task before build the phone image r=lightsofapollo.
    Phone builds are special because they may contain vendor blobs that we
    are not allowed to distribute. So, we have to make sure the task is not
    leaking anything.
    
    We check that the gecko repository is a mozilla oficial repository and
    that we are not copying the built stuff to public folders.
    
    UltraBlame original commit: ea5d67b9f456032bf009de5441a1ee58d5240e27

---
+++ testing/docker/phone-builder/bin/validate_task.py
@@ -0,0 +1,48 @@
+
+
+from __future__ import print_function
+import os
+import os.path
+import json
+import urllib2
+import sys
+import re
+
+def get_task(taskid):
+    return json.load(urllib2.urlopen('https://queue.taskcluster.net/v1/task/' + taskid))
+
+def check_task(task):
+    payload = task['payload']
+
+    if 'REPOSITORY' not in payload['env']:
+        print('Task has no gecko repository', file=sys.stderr)
+        return -1
+
+    repo = payload['env']['REPOSITORY']
+    
+    if not re.match(r'[a-z]+://hg\.mozilla\.org', repo):
+        print('Invalid repository', repo, file=sys.stderr)
+        return -1
+
+    if 'artifacts' in payload:
+        artifacts = payload['artifacts']
+        
+        
+        if any(map(lambda a: 'public' in a, artifacts)):
+            print('Cannot upload to public', file=sys.stderr)
+            return -1
+
+    return 0
+
+def main():
+    taskid = os.getenv('TASK_ID')
+
+    
+    if taskid is None:
+        sys.exit(0)
+
+    task = get_task(taskid)
+    sys.exit(check_task(task))
+
+if __name__ == '__main__':
+    main()
