12dbac23eb2568b4d4e642053ba1ec625a4ffb9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:50:50 +0000

    Bug 1031198: Renamed SIMD functions X4.toY4 and X4.bitsToY4 into Y4.fromX4 and Y4.fromX4Bits; r=bbouvier
    UltraBlame original commit: 9abc7829bde3dff951955959491f27307e714af5

--- js/src/tests/ecma_6/TypedObject/simd/float32x4fromint32x4bits.js
+++ js/src/tests/ecma_6/TypedObject/simd/float32x4fromint32x4bits.js
@@ -3,7 +3,7 @@ var BUGNUMBER = 946042;
 var float32x4 = SIMD.float32x4;
 var int32x4 = SIMD.int32x4;
 
-var summary = 'int32x4 bitsToFloat32x4';
+var summary = 'float32x4 fromInt32x4Bits';
 
 function test() {
   print(BUGNUMBER + ": " + summary);
@@ -11,7 +11,7 @@ function test() {
   
 
   var a = int32x4(100, 200, 300, 400);
-  var c = SIMD.int32x4.bitsToFloat32x4(a);
+  var c = SIMD.float32x4.fromInt32x4Bits(a);
   assertEq(c.x, 1.401298464324817e-43);
   assertEq(c.y, 2.802596928649634e-43);
   assertEq(c.z, 4.203895392974451e-43);
