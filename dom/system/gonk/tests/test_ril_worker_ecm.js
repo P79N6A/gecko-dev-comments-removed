182e54bb9f261a43f14fad7d33864e7effdbbeec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:54:48 +0000

    Bug 1023141 - query ril.ecclist per dial request - part 3 - fix xpcshell. r=aknow
    UltraBlame original commit: 3073df95a85980b21f2a9f2c643f9315f4b6eca2

--- dom/system/gonk/tests/test_ril_worker_ecm.js
+++ dom/system/gonk/tests/test_ril_worker_ecm.js
@@ -122,8 +122,9 @@ add_test(function test_request_exit_emergencyCbMode_when_dial() {
   };
 
   
-  context.RIL.dial({number: "0912345678",
-                  isDialEmergency: false});
+  context.RIL.dialNonEmergencyNumber({number: "0912345678",
+                                      isEmergency: false,
+                                      isDialEmergency: false});
 
   
   do_check_eq(context.RIL._exitEmergencyCbModeTimeoutID, null);
