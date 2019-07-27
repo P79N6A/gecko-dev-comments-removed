f271b8205349d5c00b28fe0ec7935fe804258b93
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:33:09 +0000

    Bug 1163467. Part 3 - move AddOutputStream code below MediaDecoder::Load(). r=roc.
    UltraBlame original commit: 092ce739460d0d86f0cfe68d4ffa0ebadd8aa183

--- dom/media/MediaDecoder.cpp
+++ dom/media/MediaDecoder.cpp
@@ -343,26 +343,14 @@ void MediaDecoder::AddOutputStream(ProcessedMediaStream* aStream,
 {
   MOZ_ASSERT(NS_IsMainThread());
   DECODER_LOG("AddOutputStream aStream=%p!", aStream);
+  MOZ_ASSERT(mDecoderStateMachine, "Must be called after Load().");
 
-  {
-    ReentrantMonitorAutoEnter mon(GetReentrantMonitor());
-    if (mDecoderStateMachine) {
-      mDecoderStateMachine->DispatchAudioCaptured();
-    }
-    if (!GetDecodedStream()) {
-      RecreateDecodedStream(mLogicalPosition, aStream->Graph());
-    }
-    mDecodedStream.Connect(aStream, aFinishWhenEnded);
+  ReentrantMonitorAutoEnter mon(GetReentrantMonitor());
+  if (!GetDecodedStream()) {
+    RecreateDecodedStream(mLogicalPosition, aStream->Graph());
   }
-
-  
-  
-  
-  if (mDecoderStateMachine) {
-    
-    
-    ScheduleStateMachine();
-  }
+  mDecodedStream.Connect(aStream, aFinishWhenEnded);
+  mDecoderStateMachine->DispatchAudioCaptured();
 }
 
 double MediaDecoder::GetDuration()
@@ -564,9 +552,6 @@ void MediaDecoder::SetStateMachineParameters()
 {
   ReentrantMonitorAutoEnter mon(GetReentrantMonitor());
   mDecoderStateMachine->SetDuration(mDuration);
-  if (GetDecodedStream()) {
-    mDecoderStateMachine->DispatchAudioCaptured();
-  }
   if (mMinimizePreroll) {
     mDecoderStateMachine->DispatchMinimizePrerollUntilPlaybackStarts();
   }
