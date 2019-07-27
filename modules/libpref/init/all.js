9e08c238103927cc1e55e5970280de349502bb34
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:54:16 +0000

    Bug 952139 - Enable MessagePort/MessageChannel by default, r=smaug
    UltraBlame original commit: bd11384b3241596c4ab1b4bb95a0fecbbaaf811d

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4797,6 +4797,9 @@ pref("camera.control.low_memory_thresholdMB", 404);
 pref("dom.udpsocket.enabled", false);
 
 
+pref("dom.messageChannel.enabled", true);
+
+
 pref("dom.beforeAfterKeyboardEvent.enabled", false);
 
 
