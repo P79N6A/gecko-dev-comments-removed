10e2347986fb3987449481327ec5bcc2d8989380
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:47:31 +0000

    Bug 1153498 - Use RelocatablePtr in RegExpShared since these can be destroyed outside of the GC r=terrence
    UltraBlame original commit: 607cfbb06eb83175f7546ff585e191359511b52d

---
+++ js/src/jit-test/tests/gc/bug-1153498.js
@@ -0,0 +1,10 @@
+
+if (!("oomAfterAllocations" in this && "gczeal" in this))
+    quit();
+gczeal(0);
+"".search(function() {});
+r = /x/;
+x = [/y/];
+oomAfterAllocations(5);
+x.v = function() {};
+a = /z/;
