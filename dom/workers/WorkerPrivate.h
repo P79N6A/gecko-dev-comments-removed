52014b2cdde80bf2a89bea8d1ca511349ef79da2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:05:10 +0000

    Bug 1175138 P3 Expose the devtools SW testing flag on workers. r=ehsan
    UltraBlame original commit: 4e4a0a6900410910e75f710786c1b540dbcfcd33

--- dom/workers/WorkerPrivate.h
+++ dom/workers/WorkerPrivate.h
@@ -759,6 +759,13 @@ public:
     return mLoadInfo.mPrivateBrowsing;
   }
 
+  
+  bool
+  ServiceWorkersTestingInWindow() const
+  {
+    return mLoadInfo.mServiceWorkersTestingInWindow;
+  }
+
   void
   GetAllSharedWorkers(nsTArray<nsRefPtr<SharedWorker>>& aSharedWorkers);
 
@@ -1245,6 +1252,7 @@ public:
     return mPreferences[WORKERPREF_SERVICEWORKERS];
   }
 
+  
   bool
   ServiceWorkersTestingEnabled() const
   {
