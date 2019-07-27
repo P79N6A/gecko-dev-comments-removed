f33ec35c772af60098747cdeadfddabb42d18384
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:07:55 +0000

    Bug 1096185 - Part 3 - Fix function redefinition in test_selectioncarets.py. r=automatedtester
    UltraBlame original commit: 3563d3cfba5cae6e32b0b2b68adaae61cef6668a

--- layout/base/tests/marionette/test_selectioncarets.py
+++ layout/base/tests/marionette/test_selectioncarets.py
@@ -388,8 +388,6 @@ class SelectionCaretsTest(MarionetteTestCase):
     
     
     
-    def test_contenteditable_minimum_select_one_character(self):
+    def test_contenteditable2_minimum_select_one_character(self):
         self.openTestHtml(enabled=True)
         self._test_minimum_select_one_character(self._contenteditable2, self.assertEqual)
-
-
