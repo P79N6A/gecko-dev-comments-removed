085e3be3ba5b1bfd61b7f58b129f3ba9d0d0d91f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:45:42 +0000

    Bug 1068468 - Truncate ssid if its length of encoded to utf8 is longer than 32. r=chulee
    UltraBlame original commit: ef25dc770b58336e369cb1c6d465d1dba0450a0e

--- dom/wifi/WifiWorker.js
+++ dom/wifi/WifiWorker.js
@@ -3077,6 +3077,12 @@ WifiWorker.prototype = {
       debug("Invalid SSID value.");
       return null;
     }
+    
+    while (unescape(encodeURIComponent(ssid)).length > 32)
+    {
+      ssid = ssid.substring(0, ssid.length-1);
+    }
+
     if (securityType != WIFI_SECURITY_TYPE_NONE &&
         securityType != WIFI_SECURITY_TYPE_WPA_PSK &&
         securityType != WIFI_SECURITY_TYPE_WPA2_PSK) {
