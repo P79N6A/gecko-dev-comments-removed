3311afcacb9c65f3ffb2d41c311cae301d0518f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:20:23 +0000

    Backed out changeset 144276fd40a1 (bug 1179111)
    UltraBlame original commit: 252d5e965cd448fe63ad0d6c3e7643d026d4ce20

--- layout/style/nsAnimationManager.h
+++ layout/style/nsAnimationManager.h
@@ -69,10 +69,6 @@ public:
 
   virtual CSSAnimation* AsCSSAnimation() override { return this; }
 
-  
-  void GetAnimationName(nsString& aRetVal) const { aRetVal = Name(); }
-
-  
   virtual Promise* GetReady(ErrorResult& aRv) override;
   virtual void Play(ErrorResult& aRv, LimitBehavior aLimitBehavior) override;
   virtual void Pause(ErrorResult& aRv) override;
