27ad6a482c4fcaa688cfffd6763ed0d580e408f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:18:32 +0000

    Bug 1132771 - Add a test for reading all moz.build files in filesystem traversal mode; r=glandium
    moz.build files should execute in filesystem traversal mode. Add a test
    that verifies this is true.
    
    This test performs a brute force filesystem scan to find relevant
    moz.build files. This can be a little slow. That's unfortunate. But it's
    a price we need to pay in order to ensure metadata extraction mode
    continues to work.
    
    UltraBlame original commit: a6b72bee55a8b7a2a0f7e4e6e8b8a3c5da7946b7

---
+++ config/tests/test_mozbuild_reading.py
@@ -0,0 +1,45 @@
+
+
+
+
+from __future__ import unicode_literals
+
+import os
+import unittest
+
+from mozunit import main
+
+from mozbuild.base import MozbuildObject
+from mozbuild.frontend.reader import BuildReader
+
+
+class TestMozbuildReading(unittest.TestCase):
+    
+    def setUp(self):
+        self._old_env = dict(os.environ)
+        os.environ.pop('MOZCONFIG', None)
+        os.environ.pop('MOZ_OBJDIR', None)
+
+    def tearDown(self):
+        os.environ.clear()
+        os.environ.update(self._old_env)
+
+    @unittest.skip('failing in SpiderMonkey builds')
+    def test_filesystem_traversal_reading(self):
+        """Reading moz.build according to filesystem traversal works.
+
+        We attempt to read every known moz.build file via filesystem traversal.
+
+        If this test fails, it means that metadata extraction will fail.
+        """
+        mb = MozbuildObject.from_environment(detect_virtualenv_mozinfo=False)
+        config = mb.config_environment
+        reader = BuildReader(config)
+        all_paths = set(reader.all_mozbuild_paths())
+        paths, contexts = reader.read_relevant_mozbuilds(all_paths)
+        self.assertEqual(set(paths), all_paths)
+        self.assertGreaterEqual(len(contexts), len(paths))
+
+
+if __name__ == '__main__':
+    main()
