367a0847c6f295ded522feaa9c03131064d6c693
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:54:46 +0000

    Bug 1148893 - Part 13 - Clean up browser_styleeditor_import_rule.js. r=bgrinstead
    UltraBlame original commit: eaee1c55f99c7e4910719aab51097ab29b7addfe

--- browser/devtools/styleeditor/test/browser_styleeditor_import_rule.js
+++ browser/devtools/styleeditor/test/browser_styleeditor_import_rule.js
@@ -1,37 +1,25 @@
 
 
 
+"use strict";
+
+
 
 
 const TESTCASE_URI = TEST_BASE_HTTP + "import.html";
 
-let gUI;
+add_task(function* () {
+  let { ui } = yield openStyleEditorForURL(TESTCASE_URI);
 
-function test()
-{
-  waitForExplicitFinish();
-
-  addTabAndOpenStyleEditors(3, onEditorAdded);
-
-  content.location = TESTCASE_URI;
-}
-
-function onEditorAdded(panel)
-{
-  gUI = panel.UI;
-
-  is(gUI.editors.length, 3,
+  is(ui.editors.length, 3,
     "there are 3 stylesheets after loading @imports");
 
-  is(gUI.editors[0].styleSheet.href, TEST_BASE_HTTP + "simple.css",
+  is(ui.editors[0].styleSheet.href, TEST_BASE_HTTP + "simple.css",
     "stylesheet 1 is simple.css");
 
-  is(gUI.editors[1].styleSheet.href, TEST_BASE_HTTP + "import.css",
+  is(ui.editors[1].styleSheet.href, TEST_BASE_HTTP + "import.css",
     "stylesheet 2 is import.css");
 
-  is(gUI.editors[2].styleSheet.href, TEST_BASE_HTTP + "import2.css",
+  is(ui.editors[2].styleSheet.href, TEST_BASE_HTTP + "import2.css",
     "stylesheet 3 is import2.css");
-
-  gUI = null;
-  finish();
-}
+});
