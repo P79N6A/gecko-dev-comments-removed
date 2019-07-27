b781690898d84dea72a46ae26a6545a0e6a6cc61
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:14:02 +0000

    Bug 1052924 - Separate reflow code. r=dbaron
    UltraBlame original commit: 0ea4763c76f3e9e8509713082495bcc4487e8c18

--- layout/generic/nsRubyFrame.h
+++ layout/generic/nsRubyFrame.h
@@ -11,6 +11,8 @@
 
 #include "nsContainerFrame.h"
 
+class nsRubyBaseContainerFrame;
+
 
 
 
@@ -57,6 +59,12 @@ protected:
   friend nsContainerFrame* NS_NewRubyFrame(nsIPresShell* aPresShell,
                                            nsStyleContext* aContext);
   explicit nsRubyFrame(nsStyleContext* aContext) : nsContainerFrame(aContext) {}
+
+  void ReflowSegment(nsPresContext* aPresContext,
+                     const nsHTMLReflowState& aReflowState,
+                     nsRubyBaseContainerFrame* aBaseContainer,
+                     nsReflowStatus& aStatus);
+
   nscoord mBaseline;
 };
 
