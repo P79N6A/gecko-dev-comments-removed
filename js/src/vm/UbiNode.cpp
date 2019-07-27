137a9c2cc0a3f20d0e4cfb6ed87090a25d311e96
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:58:00 +0000

    Bug 1149294 - Part 1: Don't trace permanent atoms and well known symbols that are owned by a parent JSRuntime inside SimpeEdgeVectorTracer; r=terrence
    UltraBlame original commit: 38873310354d3e4c24eeda92d439cfb1bde4df66

--- js/src/vm/UbiNode.cpp
+++ js/src/vm/UbiNode.cpp
@@ -115,6 +115,13 @@ class SimpleEdgeVectorTracer : public JS::CallbackTracer {
         if (!okay)
             return;
 
+        
+        
+        if (kind == JS::TraceKind::String && static_cast<JSString*>(*thingp)->isPermanentAtom())
+            return;
+        if (kind == JS::TraceKind::Symbol && static_cast<JS::Symbol*>(*thingp)->isWellKnownSymbol())
+            return;
+
         char16_t* name16 = nullptr;
         if (wantNames) {
             
@@ -186,7 +193,7 @@ template<typename Referent>
 JS::Zone*
 TracerConcrete<Referent>::zone() const
 {
-    return get().zone();
+    return get().zoneFromAnyThread();
 }
 
 template<typename Referent>
