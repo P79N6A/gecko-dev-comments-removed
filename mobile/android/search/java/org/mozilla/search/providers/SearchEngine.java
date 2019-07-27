30b5e8ebc312cc0c21fba5d4453025f384b93b61
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:57:54 +0000

    Bug 1054303 - Use open search plugins for search engines. r=bnicholson
    UltraBlame original commit: 36bc6b13ac2c8344f7f8d97513d4fcb8460aa004

--- mobile/android/search/java/org/mozilla/search/providers/SearchEngine.java
+++ mobile/android/search/java/org/mozilla/search/providers/SearchEngine.java
@@ -5,12 +5,36 @@
 package org.mozilla.search.providers;
 
 import android.net.Uri;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Locale;
 
 
 
 
 
-public abstract class SearchEngine {
+public class SearchEngine {
+
+    private static final String URLTYPE_SUGGEST_JSON = "application/x-suggestions+json";
+    private static final String URLTYPE_SEARCH_HTML  = "text/html";
+
+    
+    private static final String MOZ_PARAM_LOCALE = "\\{moz:locale\\}";
+    private static final String MOZ_PARAM_DIST_ID = "\\{moz:distributionID\\}";
+    private static final String MOZ_PARAM_OFFICIAL = "\\{moz:official\\}";
+
+    
+    
+    private static final String OS_PARAM_USER_DEFINED = "\\{searchTerms\\??\\}";
+    private static final String OS_PARAM_INPUT_ENCODING = "\\{inputEncoding\\??\\}";
+    private static final String OS_PARAM_LANGUAGE = "\\{language\\??\\}";
+    private static final String OS_PARAM_OUTPUT_ENCODING = "\\{outputEncoding\\??\\}";
+    private static final String OS_PARAM_OPTIONAL = "\\{(?:\\w+:)?\\w+\\?\\}";
 
     
     
@@ -21,50 +45,137 @@ public abstract class SearchEngine {
                     "document.getElementsByTagName('head')[0].appendChild(tag);" +
                     "tag.innerText='%s'})();";
 
-    private String suggestionTemplate;
+    private String identifier;
+    private String shortName;
 
     
+    private Uri resultsUri;
+    private Uri suggestUri;
+
+    
+
+
+
+    public SearchEngine(String identifier, InputStream in) throws IOException, XmlPullParserException {
+        this.identifier = identifier;
+
+        final XmlPullParser parser = Xml.newPullParser();
+        parser.setInput(in, null);
+        parser.nextTag();
+        readSearchPlugin(parser);
+    }
+
+    private void readSearchPlugin(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, null, "SearchPlugin");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+
+            final String tag = parser.getName();
+            if (tag.equals("ShortName")) {
+                readShortName(parser);
+            } else if (tag.equals("Url")) {
+                readUrl(parser);
+            
+            
+            } else {
+                skip(parser);
+            }
+        }
+    }
+
+    private void readShortName(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, null, "ShortName");
+        if (parser.next() == XmlPullParser.TEXT) {
+            shortName = parser.getText();
+            parser.nextTag();
+        }
+    }
+
+    private void readUrl(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, null, "Url");
+
+        final String type = parser.getAttributeValue(null, "type");
+        final String template = parser.getAttributeValue(null, "template");
+
+        Uri uri = Uri.parse(template);
+
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+
+            final String tag = parser.getName();
+
+            if (tag.equals("Param")) {
+                final String name = parser.getAttributeValue(null, "name");
+                final String value = parser.getAttributeValue(null, "value");
+                uri = uri.buildUpon().appendQueryParameter(name, value).build();
+                parser.nextTag();
+            
+            
+            } else {
+                skip(parser);
+            }
+        }
+
+        if (type.equals(URLTYPE_SEARCH_HTML)) {
+            resultsUri = uri;
+        } else if (type.equals(URLTYPE_SUGGEST_JSON)) {
+            suggestUri = uri;
+        }
+    }
+
+    private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
+        if (parser.getEventType() != XmlPullParser.START_TAG) {
+            throw new IllegalStateException();
+        }
+        int depth = 1;
+        while (depth != 0) {
+            switch (parser.next()) {
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+            }
+        }
+    }
+
+    
+
+
 
 
 
     public String getInjectableJs() {
-        return String.format(STYLE_INJECTION_SCRIPT, getInjectableCss());
-    }
+        final String css;
 
-    
-
-
-
-    final public boolean isSearchResultsPage(String url) {
-        return getResultsUri().getAuthority().equalsIgnoreCase(Uri.parse(url).getAuthority());
-    }
-
-    
-
-
-
-
-    final public String suggestUriForQuery(String query) {
-        return getSuggestionUri().buildUpon().appendQueryParameter(getSuggestionQueryParam(), query).build().toString();
-    }
-
-    
-
-
-
-
-    final public String resultsUriForQuery(String query) {
-        return getResultsUri().buildUpon().appendQueryParameter(getResultsQueryParam(), query).build().toString();
-    }
-
-    
-
-
-    final public String getSuggestionTemplate(String placeholder) {
-        if (suggestionTemplate == null) {
-            suggestionTemplate = suggestUriForQuery(placeholder);
+        if (identifier.equals("bing")) {
+            css = "#mHeader{display:none}#contentWrapper{margin-top:0}";
+        } else if (identifier.equals("google")) {
+            css = "#sfcnt,#top_nav{display:none}";
+        } else if (identifier.equals("yahoo")) {
+            css = "#nav,#header{display:none}";
+        } else {
+            css = "";
         }
-        return suggestionTemplate;
+
+        return String.format(STYLE_INJECTION_SCRIPT, css);
+    }
+
+    public String getName() {
+        return shortName;
+    }
+
+    
+
+
+
+    public boolean isSearchResultsPage(String url) {
+        return resultsUri.getAuthority().equalsIgnoreCase(Uri.parse(url).getAuthority());
     }
 
     
@@ -72,31 +183,45 @@ public abstract class SearchEngine {
 
 
 
-    protected abstract String getInjectableCss();
+    public String resultsUriForQuery(String query) {
+        final String template = Uri.decode(resultsUri.toString());
+        return paramSubstitution(template, Uri.encode(query));
+    }
 
     
 
 
 
 
-    protected abstract Uri getResultsUri();
+    public String getSuggestionTemplate(String query) {
+        final String template = Uri.decode(suggestUri.toString());
+        return paramSubstitution(template, Uri.encode(query));
+    }
 
     
 
 
 
 
-    protected abstract Uri getSuggestionUri();
-
-    
 
 
 
-    protected abstract String getSuggestionQueryParam();
+    private String paramSubstitution(String template, String query) {
+        final String locale = Locale.getDefault().toString();
 
-    
+        template = template.replaceAll(MOZ_PARAM_LOCALE, locale);
+        template = template.replaceAll(MOZ_PARAM_DIST_ID, "");
+        template = template.replaceAll(MOZ_PARAM_OFFICIAL, "unofficial");
 
+        template = template.replaceAll(OS_PARAM_USER_DEFINED, query);
+        template = template.replaceAll(OS_PARAM_INPUT_ENCODING, "UTF-8");
 
+        template = template.replaceAll(OS_PARAM_LANGUAGE, locale);
+        template = template.replaceAll(OS_PARAM_OUTPUT_ENCODING, "UTF-8");
 
-    protected abstract String getResultsQueryParam();
+        
+        template = template.replaceAll(OS_PARAM_OPTIONAL, "");
+
+        return template;
+    }
 }
--- mobile/android/search/java/org/mozilla/search/providers/SearchEngineManager.java
+++ mobile/android/search/java/org/mozilla/search/providers/SearchEngineManager.java
@@ -10,9 +10,15 @@ import android.os.AsyncTask;
 import android.text.TextUtils;
 import android.util.Log;
 
+import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.GeckoSharedPrefs;
+import org.mozilla.gecko.util.GeckoJarReader;
 import org.mozilla.search.Constants;
 import org.mozilla.search.SearchPreferenceActivity;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SearchEngineManager implements SharedPreferences.OnSharedPreferenceChangeListener {
     private static final String LOG_TAG = "SearchEngineManager";
@@ -20,13 +26,6 @@ public class SearchEngineManager implements SharedPreferences.OnSharedPreference
     private Context context;
     private SearchEngineCallback changeCallback;
     private SearchEngine engine;
-
-    
-    public static enum Engine {
-        BING,
-        GOOGLE,
-        YAHOO
-    }
 
     public static interface SearchEngineCallback {
         public void execute(SearchEngine engine);
@@ -63,36 +62,90 @@ public class SearchEngineManager implements SharedPreferences.OnSharedPreference
         engine = null;
     }
 
+    @Override
+    public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, final String key) {
+        if (!TextUtils.equals(SearchPreferenceActivity.PREF_SEARCH_ENGINE_KEY, key)) {
+            return;
+        }
+        getEngineFromPrefs(changeCallback);
+    }
+
     
+
 
 
 
 
 
     private void getEngineFromPrefs(final SearchEngineCallback callback) {
-        final AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
+        final AsyncTask<Void, Void, SearchEngine> task = new AsyncTask<Void, Void, SearchEngine>() {
             @Override
-            protected String doInBackground(Void... params) {
-                return GeckoSharedPrefs.forApp(context).getString(SearchPreferenceActivity.PREF_SEARCH_ENGINE_KEY, null);
+            protected SearchEngine doInBackground(Void... params) {
+                final String identifier = GeckoSharedPrefs.forApp(context)
+                        .getString(SearchPreferenceActivity.PREF_SEARCH_ENGINE_KEY, Constants.DEFAULT_SEARCH_ENGINE)
+                        .toLowerCase();
+                return createEngine(identifier);
             }
 
             @Override
-            protected void onPostExecute(String engineName) {
-                updateEngine(engineName);
-                callback.execute(engine);
+            protected void onPostExecute(SearchEngine engine) {
+                
+                SearchEngineManager.this.engine = engine;
+                if (callback != null) {
+                    callback.execute(engine);
+                }
             }
         };
         task.execute();
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (TextUtils.equals(SearchPreferenceActivity.PREF_SEARCH_ENGINE_KEY, key)) {
-            updateEngine(sharedPreferences.getString(key, null));
+    
 
-            if (changeCallback != null) {
-                changeCallback.execute(engine);
+
+
+
+
+
+    private SearchEngine createEngine(String identifier) {
+        InputStream in = getEngineFromJar(identifier);
+
+        
+        if (in == null) {
+            in = getEngineFromAssets(identifier);
+        }
+
+        if (in == null) {
+            throw new IllegalArgumentException("Couldn't find search engine for identifier: " + identifier);
+        }
+
+        try {
+            try {
+                return new SearchEngine(identifier, in);
+            } finally {
+                in.close();
             }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Exception creating search engine", e);
+        } catch (XmlPullParserException e) {
+            Log.e(LOG_TAG, "Exception creating search engine", e);
+        }
+
+        return null;
+    }
+
+    
+
+
+
+
+
+
+    private InputStream getEngineFromAssets(String identifier) {
+        try {
+            return context.getResources().getAssets().open(identifier + ".xml");
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Exception getting search engine from assets", e);
+            return null;
         }
     }
 
@@ -103,33 +156,13 @@ public class SearchEngineManager implements SharedPreferences.OnSharedPreference
 
 
 
-
-    private void updateEngine(String engineName) {
-
-        if (TextUtils.isEmpty(engineName)) {
-            engineName = Constants.DEFAULT_SEARCH_ENGINE;
-        }
-
-        try {
-            engine = createEngine(engineName);
-        } catch (IllegalArgumentException e) {
-            Log.e(LOG_TAG, "Search engine not found for " + engineName + " reverting to default engine.", e);
-            engine = createEngine(Constants.DEFAULT_SEARCH_ENGINE);
-        }
-    }
-
-    private static SearchEngine createEngine(String engineName) {
-        switch (Engine.valueOf(engineName)) {
-            case BING:
-                return new BingSearchEngine();
-            case GOOGLE:
-                return new GoogleSearchEngine();
-            case YAHOO:
-                return new YahooSearchEngine();
-        }
-
+    private InputStream getEngineFromJar(String identifier) {
         
-        
-        return null;
+        final String locale = "en-US";
+
+        final String path = "!/chrome/" + locale + "/locale/" + locale + "/browser/searchplugins/" + identifier + ".xml";
+        final String url = "jar:jar:file://" + context.getPackageResourcePath() + "!/" + AppConstants.OMNIJAR_NAME + path;
+
+        return GeckoJarReader.getStream(url);
     }
 }
