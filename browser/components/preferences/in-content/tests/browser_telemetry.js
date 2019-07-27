1fc58fca4a14fa2963aee5c5daee4117794716f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:11:31 +0000

    Bug 1075055 - Adds mochitest for Telemetry options in Preferences UI. r=jaws
    UltraBlame original commit: ac7c99989aa56f04e181ed5fe63b8540cf9cd06f

---
+++ browser/components/preferences/in-content/tests/browser_telemetry.js
@@ -0,0 +1,45 @@
+
+
+
+"use strict";
+
+function runPaneTest(fn) {
+  open_preferences((win) => {
+    let doc = win.document;
+    win.gotoPref("paneAdvanced");
+    let advancedPrefs = doc.getElementById("advancedPrefs");
+    let tab = doc.getElementById("dataChoicesTab");
+    advancedPrefs.selectedTab = tab;
+    fn(win, doc);
+  });
+}
+
+function test() {
+  waitForExplicitFinish();
+  resetPreferences();
+  registerCleanupFunction(resetPreferences);
+  runPaneTest(testTelemetryState);
+}
+
+function testTelemetryState(win, doc) {
+  let fhrCheckbox = doc.getElementById("submitHealthReportBox");
+  Assert.ok(fhrCheckbox.checked, "Health Report checkbox is checked on app first run.");
+
+  let telmetryCheckbox = doc.getElementById("submitTelemetryBox");
+  Assert.ok(!telmetryCheckbox.disabled,
+            "Telemetry checkbox must be enabled if FHR is checked.");
+
+  
+  fhrCheckbox.click();
+
+  Assert.ok(telmetryCheckbox.disabled,
+            "Telemetry checkbox must be disabled if FHR is unchecked.");
+
+  win.close();
+  finish();
+}
+
+function resetPreferences() {
+  Services.prefs.clearUserPref("datareporting.healthreport.uploadEnabled");
+}
+
