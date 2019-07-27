15349ec55a8a58e42c1c849cd6e8c3f176c85a65
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:57:09 +0000

    Bug 1173378 - crash in mozilla::net::HttpBaseChannel::OverrideSecurityInfo(nsISupports*), r=ehsan
    UltraBlame original commit: 2e5641d6cd6f93934451d19b960401dbaac3a28e

--- netwerk/protocol/http/HttpBaseChannel.h
+++ netwerk/protocol/http/HttpBaseChannel.h
@@ -249,7 +249,7 @@ public:
     const NetAddr& GetPeerAddr() { return mPeerAddr; }
 
     nsresult OverrideSecurityInfo(nsISupports* aSecurityInfo);
-    void OverrideURI(nsIURI* aRedirectedURI);
+    nsresult OverrideURI(nsIURI* aRedirectedURI);
 
 public: 
     bool IsNavigation();
