ac43181f9341a24d28ac26c4c0aa774a1f3f347b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:48:42 +0000

    Bug 1151309 (Part 2) - Hide errors in multipart image parts both visually and internally. r=tn
    UltraBlame original commit: 414baffa50d2fa870670215855f59a2aa75abead

--- image/src/MultipartImage.cpp
+++ image/src/MultipartImage.cpp
@@ -151,6 +151,21 @@ void MultipartImage::FinishTransition()
   MOZ_ASSERT(NS_IsMainThread());
   MOZ_ASSERT(mNextPart, "Should have a next part here");
 
+  nsRefPtr<ProgressTracker> newCurrentPartTracker =
+    mNextPart->GetProgressTracker();
+  if (newCurrentPartTracker->GetProgress() & FLAG_HAS_ERROR) {
+    
+    mNextPart = nullptr;
+
+    
+    mTracker->ResetForNewRequest();
+    nsRefPtr<ProgressTracker> currentPartTracker =
+      InnerImage()->GetProgressTracker();
+    mTracker->SyncNotifyProgress(currentPartTracker->GetProgress());
+
+    return;
+  }
+
   
   {
     nsRefPtr<ProgressTracker> currentPartTracker =
@@ -162,8 +177,6 @@ void MultipartImage::FinishTransition()
   mTracker->ResetForNewRequest();
   SetInnerImage(mNextPart);
   mNextPart = nullptr;
-  nsRefPtr<ProgressTracker> newCurrentPartTracker =
-    InnerImage()->GetProgressTracker();
   newCurrentPartTracker->AddObserver(this);
 
   
@@ -210,12 +223,14 @@ MultipartImage::OnImageDataAvailable(nsIRequest* aRequest,
   
   nsRefPtr<Image> nextPart = mNextPart;
   if (nextPart) {
-    return nextPart->OnImageDataAvailable(aRequest, aContext, aInStr,
-                                          aSourceOffset, aCount);
+    nextPart->OnImageDataAvailable(aRequest, aContext, aInStr,
+                                   aSourceOffset, aCount);
+  } else {
+    InnerImage()->OnImageDataAvailable(aRequest, aContext, aInStr,
+                                       aSourceOffset, aCount);
   }
 
-  return InnerImage()->OnImageDataAvailable(aRequest, aContext, aInStr,
-                                            aSourceOffset, aCount);
+  return NS_OK;
 }
 
 nsresult
@@ -230,12 +245,12 @@ MultipartImage::OnImageDataComplete(nsIRequest* aRequest,
   
   nsRefPtr<Image> nextPart = mNextPart;
   if (nextPart) {
-    return nextPart->OnImageDataComplete(aRequest, aContext, aStatus,
-                                         aLastPart);
+    nextPart->OnImageDataComplete(aRequest, aContext, aStatus, aLastPart);
+  } else {
+    InnerImage()->OnImageDataComplete(aRequest, aContext, aStatus, aLastPart);
   }
 
-  return InnerImage()->OnImageDataComplete(aRequest, aContext, aStatus,
-                                           aLastPart);
+  return NS_OK;
 }
 
 void
