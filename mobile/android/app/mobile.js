dbb0f1d7e35a59431a2432a76aae5a3b06785ba4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:26:08 +0000

    Bug 1175218 - disable region defaults by making the geoSpecificDefaults.url pref empty to fix test bustage, a=KWierso on CLOSED TREE
    UltraBlame original commit: 912519e2d0df6c574874841311edc3676712b065

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -269,7 +269,7 @@ pref("browser.search.order.3", "chrome://browser/locale/region.properties");
 
 
 pref("browser.search.geoSpecificDefaults", false);
-pref("browser.search.geoSpecificDefaults.url", "https://search.services.mozilla.com/1/%APP%/%VERSION%/%CHANNEL%/%LOCALE%/%REGION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%");
+pref("browser.search.geoSpecificDefaults.url", "");
 
 
 pref("browser.search.defaultenginename.US", "chrome://browser/locale/region.properties");
