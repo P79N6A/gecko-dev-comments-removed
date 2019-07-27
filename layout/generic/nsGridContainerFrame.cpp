855a0618da8c39f29b2db7e716f99e786823a145
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:30:18 +0000

    Bug 1107786 - part 3, [css-grid] Implement layout and painting per the CSS 'order' property for absolute positioned grid items.  r=dholbert
    UltraBlame original commit: c2c20dfa58f642b911acbb4bdb81fa08c879503d

--- layout/generic/nsGridContainerFrame.cpp
+++ layout/generic/nsGridContainerFrame.cpp
@@ -1249,7 +1249,15 @@ nsGridContainerFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
   
   
   
-  nsDisplayListSet childLists(aLists, aLists.BlockBorderBackgrounds());
+  
+  
+  nsDisplayList positionedDescendants;
+  nsDisplayListSet childLists(aLists.BlockBorderBackgrounds(),
+                              aLists.BlockBorderBackgrounds(),
+                              aLists.Floats(),
+                              aLists.Content(),
+                              &positionedDescendants,
+                              aLists.Outlines());
   typedef GridItemCSSOrderIterator::OrderState OrderState;
   OrderState order = mIsNormalFlowInCSSOrder ? OrderState::eKnownOrdered
                                              : OrderState::eKnownUnordered;
@@ -1259,6 +1267,8 @@ nsGridContainerFrame::BuildDisplayList(nsDisplayListBuilder*   aBuilder,
     BuildDisplayListForChild(aBuilder, child, aDirtyRect, childLists,
                              ::GetDisplayFlagsForGridItem(child));
   }
+  positionedDescendants.SortByCSSOrder(aBuilder);
+  aLists.PositionedDescendants()->AppendToTop(&positionedDescendants);
 }
 
 #ifdef DEBUG_FRAME_DUMP
