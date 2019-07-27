ef96d51cfd85c43cad0addf0469868e191bada6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:11:55 +0000

    Bug 1038467 - Move content/media/plugins to content/media/android and rename files. r=cpearce
    UltraBlame original commit: eb7dda331d9941916537b4a0949eda94c35f5f5b

--- content/media/android/AndroidMediaReader.h
+++ content/media/android/AndroidMediaReader.h
@@ -1,8 +1,8 @@
-/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim:set ts=2 sw=2 sts=2 et cindent: */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+
+
+
+
 #if !defined(MediaPluginReader_h_)
 #define MediaPluginReader_h_
 
@@ -63,7 +63,7 @@ public:
 
   virtual bool IsMediaSeekable()
   {
-    // not used
+    
     return true;
   }
 
@@ -91,6 +91,6 @@ public:
 
 };
 
-} // namespace mozilla
+} 
 
 #endif
