1be92f92354dc5e5536ef0bf295180c01aa549e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:34:36 +0000

    Bug 1149397 - JS::ubi::Node::edges should return a mozilla::UniquePtr; r=jimb
    UltraBlame original commit: fff6dec11ff9d1a74e903c70b1f8403ab66fd64b

--- js/public/UbiNodeTraverse.h
+++ js/public/UbiNodeTraverse.h
@@ -126,7 +126,7 @@ struct BreadthFirst {
             pending.popFront();
 
             
-            js::ScopedJSDeletePtr<EdgeRange> range(origin.edges(cx, wantNames));
+            auto range = origin.edges(cx, wantNames);
             if (!range)
                 return false;
 
