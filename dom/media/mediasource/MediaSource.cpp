6ca3e4f324735d9d4d596ed167cd740b9ccde1b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:58:04 +0000

    Backed out changeset 221ca54f4df0 (bug 1092096) for m2 test failures
    UltraBlame original commit: a27e38c36cb0832a5d0e51ab6cf341bae5159032

--- dom/media/mediasource/MediaSource.cpp
+++ dom/media/mediasource/MediaSource.cpp
@@ -63,10 +63,21 @@ static const unsigned int MAX_SOURCE_BUFFERS = 16;
 namespace mozilla {
 
 static const char* const gMediaSourceTypes[6] = {
+
+
+
+
+
+#ifdef MOZ_GONK_MEDIACODEC
   "video/mp4",
   "audio/mp4",
+#else
   "video/webm",
   "audio/webm",
+#endif
+#if 0
+  "audio/mpeg",
+#endif
   nullptr
 };
 
@@ -76,6 +87,10 @@ IsTypeSupported(const nsAString& aType)
   if (aType.IsEmpty()) {
     return NS_ERROR_DOM_INVALID_ACCESS_ERR;
   }
+  if (Preferences::GetBool("media.mediasource.ignore_codecs", false)) {
+    return NS_OK;
+  }
+  
   nsContentTypeParser parser(aType);
   nsAutoString mimeType;
   nsresult rv = parser.GetType(mimeType);
@@ -85,16 +100,6 @@ IsTypeSupported(const nsAString& aType)
   bool found = false;
   for (uint32_t i = 0; gMediaSourceTypes[i]; ++i) {
     if (mimeType.EqualsASCII(gMediaSourceTypes[i])) {
-      if ((mimeType.EqualsASCII("video/mp4") ||
-           mimeType.EqualsASCII("audio/mp4")) &&
-          !Preferences::GetBool("media.mediasource.mp4.enabled", false)) {
-        break;
-      }
-      if ((mimeType.EqualsASCII("video/webm") ||
-           mimeType.EqualsASCII("audio/webm")) &&
-          !Preferences::GetBool("media.mediasource.webm.enabled", false)) {
-        break;
-      }
       found = true;
       break;
     }
