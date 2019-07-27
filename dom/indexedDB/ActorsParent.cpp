edff596446053156b9ab0a708ad4220274afd7ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:57:05 +0000

    Bug 1155634 - Move ConnectionPool creation closer to where we actually use it and at a point guaranteed to be after QuotaManager has been started. r=khuey relanding CLOSED TREE
    UltraBlame original commit: bf525e8a7de7763f08a49307628782a128fa2112

--- dom/indexedDB/ActorsParent.cpp
+++ dom/indexedDB/ActorsParent.cpp
@@ -11070,10 +11070,6 @@ Factory::Create(const LoggingInfo& aLoggingInfo)
 
   
   if (!sFactoryInstanceCount) {
-    if (!gConnectionPool) {
-      gConnectionPool = new ConnectionPool();
-    }
-
     MOZ_ASSERT(!gLiveDatabaseHashtable);
     gLiveDatabaseHashtable = new DatabaseActorHashtable();
 
@@ -11903,6 +11899,10 @@ Database::RecvPBackgroundIDBTransactionConstructor(
     
     
     return true;
+  }
+
+  if (!gConnectionPool) {
+    gConnectionPool = new ConnectionPool();
   }
 
   auto* transaction = static_cast<NormalTransaction*>(aActor);
@@ -17773,6 +17773,10 @@ OpenDatabaseOp::DispatchToWorkThread()
 
   if (NS_WARN_IF(!mDatabase->RegisterTransaction(mVersionChangeTransaction))) {
     return NS_ERROR_OUT_OF_MEMORY;
+  }
+
+  if (!gConnectionPool) {
+    gConnectionPool = new ConnectionPool();
   }
 
   nsRefPtr<VersionChangeOp> versionChangeOp = new VersionChangeOp(this);
