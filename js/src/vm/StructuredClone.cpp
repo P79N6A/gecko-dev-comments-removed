55ddf72e899ac2b6f5597addf4ff82553f583af7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:34:42 +0000

    Bug 1185752 - Part 1: use normal Rooted for CloneMemory; r=sfink
    UltraBlame original commit: 7b01ff214a2dad47100afb640af696ac46c22ec6

--- js/src/vm/StructuredClone.cpp
+++ js/src/vm/StructuredClone.cpp
@@ -41,6 +41,7 @@
 
 #include "builtin/MapObject.h"
 #include "js/Date.h"
+#include "js/TraceableHashTable.h"
 #include "vm/SharedArrayObject.h"
 #include "vm/TypedArrayObject.h"
 #include "vm/WrapperObject.h"
@@ -267,8 +268,9 @@ struct JSStructuredCloneWriter {
                                      Value tVal)
         : out(cx), objs(out.context()),
           counts(out.context()), entries(out.context()),
-          memory(out.context()), callbacks(cb), closure(cbClosure),
-          transferable(out.context(), tVal), transferableObjects(out.context()) { }
+          memory(out.context(), CloneMemory(out.context())), callbacks(cb),
+          closure(cbClosure), transferable(out.context(), tVal), transferableObjects(out.context())
+    {}
 
     ~JSStructuredCloneWriter();
 
@@ -283,6 +285,9 @@ struct JSStructuredCloneWriter {
     }
 
   private:
+    JSStructuredCloneWriter() = delete;
+    JSStructuredCloneWriter(const JSStructuredCloneWriter&) = delete;
+
     JSContext* context() { return out.context(); }
 
     bool writeTransferMap();
@@ -324,8 +329,8 @@ struct JSStructuredCloneWriter {
     
     
     
-    typedef AutoObjectUnsigned32HashMap CloneMemory;
-    CloneMemory memory;
+    using CloneMemory = TraceableHashMap<JSObject*, uint32_t>;
+    Rooted<CloneMemory> memory;
 
     
     const JSStructuredCloneCallbacks* callbacks;
