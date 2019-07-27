23079e4c124c04c7877c4eb6a67bfac684ad2416
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:50:10 +0000

    Bug 802808 - Tabs engine should explicitly skip its own record. r=rnewman
    UltraBlame original commit: 05f68fbdbcd9cf64e42c5ce604ead22d0c395b21

--- services/sync/modules/engines/tabs.js
+++ services/sync/modules/engines/tabs.js
@@ -83,6 +83,17 @@ TabEngine.prototype = {
       urls.add(entry.urlHistory[0]);
     }
     return urls;
+  },
+
+  _reconcile: function (item) {
+    
+    
+    if (this._store.itemExists(item.id)) {
+      this._log.trace("Ignoring incoming tab item because of its id: " + item.id);
+      return false;
+    }
+
+    return SyncEngine.prototype._reconcile.call(this, item);
   }
 };
 
