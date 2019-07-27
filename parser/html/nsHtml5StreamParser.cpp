a2e79558313a6facb7492d4fb776a0e46c0a06cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:56:23 +0000

    Backed out changeset dd73fa918f95 (bug 1055728) for linux build failures
    UltraBlame original commit: a79ab44016899b6c7cda68e7d7554a135d8292b0

--- parser/html/nsHtml5StreamParser.cpp
+++ parser/html/nsHtml5StreamParser.cpp
@@ -944,11 +944,7 @@ nsHtml5StreamParser::OnStartRequest(nsIRequest* aRequest, nsISupports* aContext)
   }
 
   if (NS_FAILED(rv)) {
-    
-    
-    if (XRE_GetProcessType() != GeckoProcessType_Content) {
-      NS_WARNING("Failed to retarget HTML data delivery to the parser thread.");
-    }
+    NS_WARNING("Failed to retarget HTML data delivery to the parser thread.");
   }
 
   if (mCharsetSource == kCharsetFromParentFrame) {
