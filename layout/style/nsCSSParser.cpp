905b90506e1b56d180cecde979e5ab7e98153f9f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:29:32 +0000

    Bug 1113811: Remove unnecessary null-check of infallible 'new', in ParseDeclarationBlock. r=heycam
    UltraBlame original commit: f6a3f25253234238e488018f28ae074422412985

--- layout/style/nsCSSParser.cpp
+++ layout/style/nsCSSParser.cpp
@@ -6039,21 +6039,19 @@ CSSParserImpl::ParseDeclarationBlock(uint32_t aFlags, nsCSSContextType aContext)
   }
   css::Declaration* declaration = new css::Declaration();
   mData.AssertInitialState();
-  if (declaration) {
-    for (;;) {
-      bool changed;
-      if (!ParseDeclaration(declaration, aFlags, true, &changed, aContext)) {
-        if (!SkipDeclaration(checkForBraces)) {
+  for (;;) {
+    bool changed;
+    if (!ParseDeclaration(declaration, aFlags, true, &changed, aContext)) {
+      if (!SkipDeclaration(checkForBraces)) {
+        break;
+      }
+      if (checkForBraces) {
+        if (ExpectSymbol('}', true)) {
           break;
         }
-        if (checkForBraces) {
-          if (ExpectSymbol('}', true)) {
-            break;
-          }
-        }
-        
-        
       }
+      
+      
     }
     declaration->CompressFrom(&mData);
   }
