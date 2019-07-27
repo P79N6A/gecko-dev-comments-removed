1658ad1f9991a1a003bf4ad8381e697ae98d9486
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:21:39 +0000

    Bug 1176300 - Move libsoundtouch to lgpllibs; r=glandium
    UltraBlame original commit: 65f6cb1dce37115517921b23651174c9351dd3cd

---
+++ media/libsoundtouch/src/soundtouch_perms.h
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+#ifndef MOZILLA_SOUNDTOUCH_PERMS_H
+#define MOZILLA_SOUNDTOUCH_PERMS_H
+
+#pragma GCC visibility push(default)
+#include "SoundTouch.h"
+#pragma GCC visibility pop
+
+#endif 
