2cd18e6038dd4f62d2a5e69a76fa30fe0b568238
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:16:03 +0000

    Bug 1088672 - Part 6. Rewrite Loop's incoming call handling in the flux style. Fix window titles for the incoming call view. r=mikedeboer
    UltraBlame original commit: ea81bbb5d288cf80edb126c6d75a45f4b19fa53c

--- browser/components/loop/content/js/conversationViews.js
+++ browser/components/loop/content/js/conversationViews.js
@@ -124,8 +124,6 @@ loop.conversationViews = (function(mozL10n) {
     render: function() {
       var contactName = _getContactDisplayName(this.props.contact);
 
-      document.title = contactName;
-
       return (
         React.createElement("div", {className: "call-window"}, 
           React.createElement(CallIdentifierView, {
@@ -966,6 +964,7 @@ loop.conversationViews = (function(mozL10n) {
   var CallControllerView = React.createClass({displayName: "CallControllerView",
     mixins: [
       sharedMixins.AudioMixin,
+      sharedMixins.DocumentTitleMixin,
       loop.store.StoreMixin("conversationStore"),
       Backbone.Events
     ],
@@ -995,7 +994,7 @@ loop.conversationViews = (function(mozL10n) {
 
 
     _renderFeedbackView: function() {
-      document.title = mozL10n.get("conversation_has_ended");
+      this.setTitle(mozL10n.get("conversation_has_ended"));
 
       return (
         React.createElement(sharedViews.FeedbackView, {
@@ -1033,6 +1032,14 @@ loop.conversationViews = (function(mozL10n) {
     },
 
     render: function() {
+      
+      
+      if (this.state.contact) {
+        this.setTitle(_getContactDisplayName(this.state.contact));
+      } else {
+        this.setTitle(this.state.callerId || "");
+      }
+
       switch (this.state.callState) {
         case CALL_STATES.CLOSE: {
           this._closeWindow();
