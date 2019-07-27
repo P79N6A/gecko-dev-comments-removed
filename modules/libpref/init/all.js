c898a222c10f992cd02b862bfea59f1a1e18e9e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:19:30 +0000

    Backed out changeset a970cb5c73f8 (bug 1116806) for frequent Linux e10s browser_tab_dragdrop.js. CLOSED TREE
    UltraBlame original commit: 8b1064f9e770701c5b50e19c772df9e7261a0e0a

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2422,7 +2422,7 @@ pref("dom.ipc.plugins.reportCrashURL", true);
 
 pref("dom.ipc.plugins.unloadTimeoutSecs", 30);
 
-pref("dom.ipc.plugins.asyncInit", true);
+pref("dom.ipc.plugins.asyncInit", false);
 
 pref("dom.ipc.processCount", 1);
 
