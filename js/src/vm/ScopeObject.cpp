507ee5e390e9a50eac2d2f1eefcdce41a1aec18f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:55:42 +0000

    Bug 1090491 part 2 - Remove loop in ClonedBlockObject::create that's no longer necessary. r=shu
    UltraBlame original commit: 301694366e2055d1ed27bdcd34d7fae3c8a4b4c8

--- js/src/vm/ScopeObject.cpp
+++ js/src/vm/ScopeObject.cpp
@@ -643,18 +643,6 @@ ClonedBlockObject::create(JSContext *cx, Handle<StaticBlockObject *> block, Abst
 
     obj->setReservedSlot(SCOPE_CHAIN_SLOT, ObjectValue(*frame.scopeChain()));
 
-    
-
-
-
-    unsigned nvars = block->numVariables();
-    for (unsigned i = 0; i < nvars; ++i) {
-        if (block->isAliased(i)) {
-            Value &val = frame.unaliasedLocal(block->blockIndexToLocalIndex(i));
-            obj->as<ClonedBlockObject>().setVar(i, val);
-        }
-    }
-
     MOZ_ASSERT(obj->isDelegate());
 
     return &obj->as<ClonedBlockObject>();
