83f9f335d5f67bec5e70dd784bcfc19a5a49c882
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:47:34 +0000

    merge mozilla-inbound to mozilla-central a=merge
    UltraBlame original commit: fe0afc101ad2760a8b891c1b91a875b077b1de80

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -670,11 +670,6 @@ private:
                           int32_t* aSliceRefCnt,
                           bool* aResult) MOZ_OVERRIDE;
 
-    virtual PDocAccessibleParent* AllocPDocAccessibleParent(PDocAccessibleParent*, const uint64_t&) MOZ_OVERRIDE;
-    virtual bool DeallocPDocAccessibleParent(PDocAccessibleParent*) MOZ_OVERRIDE;
-    virtual bool RecvPDocAccessibleConstructor(PDocAccessibleParent* aDoc,
-                                               PDocAccessibleParent* aParentDoc, const uint64_t& aParentID) MOZ_OVERRIDE;
-
     
     
     
