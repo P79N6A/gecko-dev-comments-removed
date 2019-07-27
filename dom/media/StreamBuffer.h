b1f953fd7bdfb9efe626c395d1d848c65841a6fb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:09:57 +0000

    Bug 1061046. Part 20: Remove Track::mGraphRate since it's not needed. r=karlt
    UltraBlame original commit: ee6c0bfb269ff48dbff8631b9ba23e89f89f3325

--- dom/media/StreamBuffer.h
+++ dom/media/StreamBuffer.h
@@ -64,10 +64,9 @@ public:
 
 
   class Track {
-    Track(TrackID aID, StreamTime aStart, MediaSegment* aSegment, TrackRate aGraphRate)
+    Track(TrackID aID, StreamTime aStart, MediaSegment* aSegment)
       : mStart(aStart),
         mSegment(aSegment),
-        mGraphRate(aGraphRate),
         mID(aID),
         mEnded(false)
     {
@@ -138,7 +137,6 @@ public:
     
     
     nsAutoPtr<MediaSegment> mSegment;
-    TrackRate mGraphRate; 
     
     TrackID mID;
     
@@ -206,7 +204,7 @@ public:
   {
     NS_ASSERTION(!FindTrack(aID), "Track with this ID already exists");
 
-    Track* track = new Track(aID, aStart, aSegment, GraphRate());
+    Track* track = new Track(aID, aStart, aSegment);
     mTracks.InsertElementSorted(track, CompareTracksByID());
 
     if (mTracksKnownTime == STREAM_TIME_MAX) {
