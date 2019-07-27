7d8619f8e789d1249e8dbd16e0ff7b9ab6589fa0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:03:19 +0000

    Bug 1082450 - Deny access to cross-origin callables on COWs. r=gabor
    UltraBlame original commit: 76dca963dd49a294791c37c4e544b6317b8ff829

--- js/xpconnect/tests/unit/test_bug780370.js
+++ js/xpconnect/tests/unit/test_bug780370.js
@@ -14,10 +14,10 @@ function run_test()
   var sb = Cu.Sandbox("http://www.example.com");
   sb.obj = { foo: 42, __exposedProps__: { hasOwnProperty: 'r' } };
   do_check_eq(Cu.evalInSandbox('typeof obj.foo', sb), 'undefined', "COW works as expected");
-  do_check_true(Cu.evalInSandbox('obj.hasOwnProperty === Object.prototype.hasOwnProperty', sb),
-                "Remapping happens even when the property is explicitly exposed");
-  
-  
-  
-  
+  try {
+    Cu.evalInSandbox('obj.hasOwnProperty', sb);
+    do_check_true(false);
+  } catch (e) {
+    do_check_true(/privileged or cross-origin callable/i.test(e));
+  }
 }
