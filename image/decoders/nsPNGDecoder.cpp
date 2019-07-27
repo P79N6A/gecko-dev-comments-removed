d73465162e841f667bfd7b7b0d487dc7e37a2d64
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:16:16 +0000

    Bug 1162751 - Part 2: Always disable pallete index checking. r=seth
    The original assumption was that PR_LOGGING wouldn't be enabled in release
    builds. Considering that this warning isn't actionable and PR_LOGGING is now
    always defined it's better to just always disable pallete index checking.
    
    UltraBlame original commit: 37f361e463b3ea5246eb313cb0a4c065a2a05937

--- image/decoders/nsPNGDecoder.cpp
+++ image/decoders/nsPNGDecoder.cpp
@@ -286,14 +286,12 @@ nsPNGDecoder::InitInternal()
 #endif
 
 #ifdef PNG_READ_CHECK_FOR_INVALID_INDEX_SUPPORTED
-#ifndef PR_LOGGING
   
   
   
   
   
   png_set_check_for_invalid_index(mPNG, 0);
-#endif
 #endif
 
 #if defined(PNG_SET_OPTION_SUPPORTED) && defined(PNG_sRGB_PROFILE_CHECKS) && \
