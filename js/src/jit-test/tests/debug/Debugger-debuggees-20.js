3934a27d18e07b2b8d191fdaf0cd62c5a26b83cc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:28:24 +0000

    Bug 1066733: Fix JS Debugger test to add debuggees as intended. f=tromey r=sfink
    UltraBlame original commit: cf7af109d888add6c381b30d974e3f252d34a6cf

--- js/src/jit-test/tests/debug/Debugger-debuggees-20.js
+++ js/src/jit-test/tests/debug/Debugger-debuggees-20.js
@@ -9,14 +9,17 @@ var g3 = newGlobal();
 var g4 = newGlobal();           
 
 var g1w = dbg.addDebuggee(g1);
+var g3w = dbg.addDebuggee(g3);
 assertEq(dbg.addAllGlobalsAsDebuggees(), undefined);
 
 
 
-var g1w = g1w.makeDebuggeeValue(g1).unwrap();
+assertEq(g1w, g3w.makeDebuggeeValue(g1).unwrap());
+assertEq(g3w, g1w.makeDebuggeeValue(g3).unwrap());
+
 var g2w = g1w.makeDebuggeeValue(g2).unwrap();
-var g3w = g1w.makeDebuggeeValue(g3).unwrap();
 var g4w = g1w.makeDebuggeeValue(g4).unwrap();
+
 var thisw = g1w.makeDebuggeeValue(this).unwrap();
 
 
