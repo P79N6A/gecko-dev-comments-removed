c263bc96ff28289342da6d485907b9901c78a6ab
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:56:54 +0000

    Bug 1079657 - DMD: Remove the "stress" mode. r=erahm.
    UltraBlame original commit: 33d851b1e655d727f2678f586417fb324945aff5

--- memory/replace/dmd/DMD.cpp
+++ memory/replace/dmd/DMD.cpp
@@ -291,8 +291,7 @@ class Options
 
   enum Mode {
     Normal,   
-    Test,     
-    Stress    
+    Test      
   };
 
   char* mDMDEnvVar;   
@@ -320,7 +319,6 @@ public:
   void SetSampleBelowSize(size_t aN) { mSampleBelowSize.mActual = aN; }
 
   bool IsTestMode()   const { return mMode == Test; }
-  bool IsStressMode() const { return mMode == Stress; }
 };
 
 static Options *gOptions;
@@ -1320,8 +1318,6 @@ Options::Options(const char* aDMDEnvVar)
         mMode = Options::Normal;
       } else if (strcmp(arg, "--mode=test")   == 0) {
         mMode = Options::Test;
-      } else if (strcmp(arg, "--mode=stress") == 0) {
-        mMode = Options::Stress;
 
       } else if (strcmp(arg, "") == 0) {
         
@@ -1358,7 +1354,7 @@ Options::BadArg(const char* aArg)
             int(mMaxFrames.mMax),
             int(mMaxFrames.mDefault));
   StatusMsg("  --show-dump-stats=<yes|no>   Show stats about dumps? [no]\n");
-  StatusMsg("  --mode=<normal|test|stress>  Mode of operation [normal]\n");
+  StatusMsg("  --mode=<normal|test>         Mode of operation [normal]\n");
   StatusMsg("\n");
   exit(1);
 }
@@ -1389,7 +1385,6 @@ OpenOutputFile(const char* aFilename)
 
 static void RunTestMode(UniquePtr<FpWriteFunc> aF1, UniquePtr<FpWriteFunc> aF2,
                         UniquePtr<FpWriteFunc> aF3, UniquePtr<FpWriteFunc> aF4);
-static void RunStressMode(UniquePtr<FpWriteFunc> aF);
 
 
 
@@ -1459,16 +1454,6 @@ Init(const malloc_table_t* aMallocTable)
     StatusMsg("running test mode...\n");
     RunTestMode(Move(f1), Move(f2), Move(f3), Move(f4));
     StatusMsg("finished test mode\n");
-    exit(0);
-  }
-
-  if (gOptions->IsStressMode()) {
-    auto f = MakeUnique<FpWriteFunc>(OpenOutputFile("stress.json"));
-    gIsDMDRunning = true;
-
-    StatusMsg("running stress mode...\n");
-    RunStressMode(Move(f));
-    StatusMsg("finished stress mode\n");
     exit(0);
   }
 
@@ -2086,82 +2071,6 @@ RunTestMode(UniquePtr<FpWriteFunc> aF1, UniquePtr<FpWriteFunc> aF2,
   
   JSONWriter writer4(Move(aF4));
   AnalyzeReports(writer4);
-}
-
-
-
-
-
-
-static void
-UseItOrLoseIt2(void* a)
-{
-  if (a == (void*)0x42) {
-    printf("UseItOrLoseIt2\n");
-  }
-}
-
-MOZ_NEVER_INLINE static void
-stress5()
-{
-  for (int i = 0; i < 10; i++) {
-    void* x = malloc(64);
-    UseItOrLoseIt2(x);
-    if (i & 1) {
-      free(x);
-    }
-  }
-}
-
-MOZ_NEVER_INLINE static void
-stress4()
-{
-  stress5(); stress5(); stress5(); stress5(); stress5();
-  stress5(); stress5(); stress5(); stress5(); stress5();
-}
-
-MOZ_NEVER_INLINE static void
-stress3()
-{
-  for (int i = 0; i < 10; i++) {
-    stress4();
-  }
-}
-
-MOZ_NEVER_INLINE static void
-stress2()
-{
-  stress3(); stress3(); stress3(); stress3(); stress3();
-  stress3(); stress3(); stress3(); stress3(); stress3();
-}
-
-MOZ_NEVER_INLINE static void
-stress1()
-{
-  for (int i = 0; i < 10; i++) {
-    stress2();
-  }
-}
-
-
-
-
-
-
-
-
-static void
-RunStressMode(UniquePtr<FpWriteFunc> aF)
-{
-  JSONWriter writer(Move(aF));
-
-  
-  gOptions->SetSampleBelowSize(1);
-
-  stress1(); stress1(); stress1(); stress1(); stress1();
-  stress1(); stress1(); stress1(); stress1(); stress1();
-
-  AnalyzeReports(writer);
 }
 
 }   
