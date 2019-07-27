faa6606746ef1143f59a351a07289e28e26292a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:03:19 +0000

    Bug 1173754 - Add a restrict token for search suggestions in the awesomebar. r=mak
    UltraBlame original commit: 38c71cf56b27f6928f36e0376fb0c23a2806fcfc

--- toolkit/components/places/tests/unifiedcomplete/test_searchSuggestions.js
+++ toolkit/components/places/tests/unifiedcomplete/test_searchSuggestions.js
@@ -3,6 +3,7 @@ Cu.import("resource://gre/modules/FormHistory.jsm");
 const ENGINE_NAME = "engine-suggestions.xml";
 const SERVER_PORT = 9000;
 const SUGGEST_PREF = "browser.urlbar.suggest.searches";
+const SUGGEST_RESTRICT_TOKEN = "$";
 
 
 
@@ -143,5 +144,100 @@ add_task(function* suffixMatch() {
   });
 
   suggestionsFromSearchString = oldFn;
+  yield cleanup();
+});
+
+add_task(function* restrictToken() {
+  Services.prefs.setBoolPref(SUGGEST_PREF, true);
+
+  
+  
+  
+  yield PlacesTestUtils.addVisits([
+    {
+      uri: NetUtil.newURI("http://example.com/hello-visit"),
+      title: "hello visit",
+    },
+    {
+      uri: NetUtil.newURI("http://example.com/hello-bookmark"),
+      title: "hello bookmark",
+    },
+  ]);
+
+  yield addBookmark({
+    uri: NetUtil.newURI("http://example.com/hello-bookmark"),
+    title: "hello bookmark",
+  });
+
+  
+  
+  let searchStr = "hello";
+  yield check_autocomplete({
+    search: searchStr,
+    matches: [
+      {
+        uri: NetUtil.newURI("http://example.com/hello-visit"),
+        title: "hello visit",
+      },
+      {
+        uri: NetUtil.newURI("http://example.com/hello-bookmark"),
+        title: "hello bookmark",
+        style: ["bookmark"],
+      },
+      {
+        uri: makeActionURI(("searchengine"), {
+          engineName: ENGINE_NAME,
+          input: searchStr,
+          searchQuery: searchStr,
+          searchSuggestion: "hello foo",
+        }),
+        title: ENGINE_NAME,
+        style: ["action", "searchengine"],
+        icon: "",
+      },
+      {
+        uri: makeActionURI(("searchengine"), {
+          engineName: ENGINE_NAME,
+          input: searchStr,
+          searchQuery: searchStr,
+          searchSuggestion: "hello bar",
+        }),
+        title: ENGINE_NAME,
+        style: ["action", "searchengine"],
+        icon: "",
+      },
+    ],
+  });
+
+  
+  searchStr = SUGGEST_RESTRICT_TOKEN + " hello";
+  yield check_autocomplete({
+    search: searchStr,
+    matches: [
+      {
+        uri: makeActionURI(("searchengine"), {
+          engineName: ENGINE_NAME,
+          input: searchStr,
+          searchQuery: searchStr,
+          searchSuggestion: "hello foo",
+        }),
+        title: ENGINE_NAME,
+        style: ["action", "searchengine"],
+        icon: "",
+      },
+      {
+        uri: makeActionURI(("searchengine"), {
+          engineName: ENGINE_NAME,
+          input: searchStr,
+          searchQuery: searchStr,
+          searchSuggestion: "hello bar",
+        }),
+        title: ENGINE_NAME,
+        style: ["action", "searchengine"],
+        icon: "",
+      }
+    ],
+  });
+
   yield cleanup();
 });
