4c548ecc1e02e822a0ac14586032495545a2a432
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:06:21 +0000

    Bug 1073093 - [mozlog] make sure formatter wasn't removed from defaults before adding commandline option for it, r=chmanchester
    UltraBlame original commit: c6ab97a41edba799edbdc3f50a1c4d1a4bc29380

--- testing/mozbase/mozlog/mozlog/structured/commandline.py
+++ testing/mozbase/mozlog/mozlog/structured/commandline.py
@@ -76,8 +76,10 @@ def add_logging_group(parser):
                              help=help_str)
         for optname, (cls, help_str, formatters, action) in fmt_options.iteritems():
             for fmt in formatters:
-                group.add_option("--log-%s-%s" % (fmt, optname), action=action,
-                                 help=help_str, default=None)
+                
+                if fmt in log_formatters:
+                    group.add_option("--log-%s-%s" % (fmt, optname), action=action,
+                                     help=help_str, default=None)
         parser.add_option_group(group)
     else:
         group = parser.add_argument_group(group_name,
@@ -88,8 +90,10 @@ def add_logging_group(parser):
 
         for optname, (cls, help_str, formatters, action) in fmt_options.iteritems():
             for fmt in formatters:
-                group.add_argument("--log-%s-%s" % (fmt, optname), action=action,
-                                   help=help_str, default=None)
+                
+                if fmt in log_formatters:
+                    group.add_argument("--log-%s-%s" % (fmt, optname), action=action,
+                                       help=help_str, default=None)
 
 
 def setup_handlers(logger, formatters, formatter_options):
