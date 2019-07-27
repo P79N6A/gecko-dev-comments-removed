5d8df9711078b8924a1032302689c190e0d1a364
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:57:05 +0000

    Bug 1033841: Ported Loop panel views to React. r=Standard8
    UltraBlame original commit: f6016481c7f277f999e08b990bb02c9518851be2

--- browser/components/loop/content/shared/js/router.js
+++ browser/components/loop/content/shared/js/router.js
@@ -51,12 +51,38 @@ loop.shared.router = (function(l10n) {
 
 
 
-    loadView : function(view) {
-      if (this._activeView) {
-        this._activeView.remove();
+    loadView: function(view) {
+      this.clearActiveView();
+      this._activeView = {type: "backbone", view: view.render().show()};
+      this.updateView(this._activeView.view.$el);
+    },
+
+    
+
+
+
+
+    loadReactComponent: function(reactComponent) {
+      this.clearActiveView();
+      this._activeView = {
+        type: "react",
+        view: React.renderComponent(reactComponent,
+                                    document.querySelector("#main"))
+      };
+    },
+
+    
+
+
+    clearActiveView: function() {
+      if (!this._activeView) {
+        return;
       }
-      this._activeView = view.render().show();
-      this.updateView(this._activeView.$el);
+      if (this._activeView.type === "react") {
+        React.unmountComponentAtNode(document.querySelector("#main"));
+      } else {
+        this._activeView.view.remove();
+      }
     },
 
     
