ed208c4c5b8d393328255e726af38407ac0e0144
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:11:19 +0000

    Bug 1102210 - Test for Android History (normal and redirected page loads) r=rnewman
    UltraBlame original commit: 65205034ed90102a8053119ca9c679bc2889f40c

---
+++ mobile/android/base/tests/testHistoryService.java
@@ -0,0 +1,12 @@
+
+
+
+
+package org.mozilla.gecko.tests;
+
+public class testHistoryService extends JavascriptTest {
+
+    public testHistoryService() {
+        super("testHistoryService.js");
+    }
+}
