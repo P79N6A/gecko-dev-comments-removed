8561bc33414186ffb51864a03e101f844f795ed0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:43:04 +0000

    Bug 1170966 - check quad's effective region before drawing. r=nical
    UltraBlame original commit: 89807785d4d47b8d49383fbb23d940ca0e8fd4df

--- gfx/layers/opengl/CompositorOGL.cpp
+++ gfx/layers/opengl/CompositorOGL.cpp
@@ -607,6 +607,8 @@ CompositorOGL::BeginFrame(const nsIntRegion& aInvalidRegion,
     *aRenderBoundsOut = rect;
   }
 
+  mRenderBoundsOut = rect;
+
   GLint width = rect.width;
   GLint height = rect.height;
 
@@ -950,6 +952,17 @@ CompositorOGL::DrawQuad(const Rect& aRect,
     DrawVRDistortion(aRect, aClipRect, aEffectChain, aOpacity, aTransform);
     return;
   }
+
+  
+  
+  const Rect destRect = aTransform.TransformBounds(aRect);
+
+  if (!mRenderBoundsOut.Intersects(destRect)) {
+    return;
+  }
+
+  mPixelsFilled += destRect.width * destRect.height;
+
   LayerScope::DrawBegin();
 
   Rect clipRect = aClipRect;
@@ -997,13 +1010,6 @@ CompositorOGL::DrawQuad(const Rect& aRect,
                  : MaskType::Mask2d;
   } else {
     maskType = MaskType::MaskNone;
-  }
-
-  {
-    
-    
-    const Rect destRect = aTransform.TransformBounds(aRect);
-    mPixelsFilled += destRect.width * destRect.height;
   }
 
   
