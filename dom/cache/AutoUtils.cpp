db048e58e3b65047317df4c9f58d30e10d5c4a81
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:56:54 +0000

    Bug 1173439 P4 Remove Request url field from Cache API database. r=ehsan
    UltraBlame original commit: 12ccbd45dcb5d403481fa5f9441eb6e4f2a41361

--- dom/cache/AutoUtils.cpp
+++ dom/cache/AutoUtils.cpp
@@ -292,8 +292,11 @@ MatchInPutList(InternalRequest* aRequest,
     nsAutoCString url;
     aRequest->GetURL(url);
 
+    nsAutoCString requestUrl(cachedRequest.urlWithoutQuery());
+    requestUrl.Append(cachedRequest.urlQuery());
+
     
-    if (url != cachedRequest.url()) {
+    if (url != requestUrl) {
       continue;
     }
 
