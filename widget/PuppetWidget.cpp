95e4567ad7f036751d4f60db21e463c669830f6e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:11:39 +0000

    Backed out changeset 2bf5b03a15da (bug 1068626) for build bustage
    UltraBlame original commit: d1c2b9e959e06458b39534a0677a369c0843547d

--- widget/PuppetWidget.cpp
+++ widget/PuppetWidget.cpp
@@ -761,14 +761,6 @@ PuppetWidget::PaintTask::Run()
 bool
 PuppetWidget::NeedsPaint()
 {
-  
-  if (XRE_GetProcessType() == GeckoProcessType_Content &&
-      Preferences::GetBool("browser.tabs.remote.desktopbehavior", false) &&
-      mWindowType == eWindowType_popup) {
-    NS_WARNING("Trying to paint an e10s popup in the child process!");
-    return false;
-  }
-
   return mVisible;
 }
 
