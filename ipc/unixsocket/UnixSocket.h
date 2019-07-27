7930843a2f0130f297d8706b0e554555e545c3fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:29:53 +0000

    Backed out changeset 56a5acbceadb (bug 1046109) for bustage on a CLOSED TREE
    UltraBlame original commit: 24b6b4da789387004a2f0f2fac79eeba05a05a48

--- ipc/unixsocket/UnixSocket.h
+++ ipc/unixsocket/UnixSocket.h
@@ -7,16 +7,49 @@
 #ifndef mozilla_ipc_UnixSocket_h
 #define mozilla_ipc_UnixSocket_h
 
+
 #include <stdlib.h>
 #include "nsAutoPtr.h"
 #include "nsString.h"
 #include "nsThreadUtils.h"
 #include "mozilla/ipc/UnixSocketWatcher.h"
-#include "mozilla/ipc/SocketBase.h"
 #include "mozilla/RefPtr.h"
 
 namespace mozilla {
 namespace ipc {
+
+class UnixSocketRawData
+{
+public:
+  
+  size_t mSize;
+  size_t mCurrentWriteOffset;
+  nsAutoArrayPtr<uint8_t> mData;
+
+  
+
+
+
+  UnixSocketRawData(size_t aSize) :
+    mSize(aSize),
+    mCurrentWriteOffset(0)
+  {
+    mData = new uint8_t[mSize];
+  }
+
+  
+
+
+
+  UnixSocketRawData(const void* aData, size_t aSize)
+    : mSize(aSize),
+      mCurrentWriteOffset(0)
+  {
+    MOZ_ASSERT(aData || !mSize);
+    mData = new uint8_t[mSize];
+    memcpy(mData, aData, mSize);
+  }
+};
 
 class UnixSocketImpl;
 
@@ -95,13 +128,42 @@ public:
 
 };
 
-class UnixSocketConsumer : public SocketConsumerBase
+enum SocketConnectionStatus {
+  SOCKET_DISCONNECTED = 0,
+  SOCKET_LISTENING = 1,
+  SOCKET_CONNECTING = 2,
+  SOCKET_CONNECTED = 3
+};
+
+class UnixSocketConsumer
 {
 protected:
   virtual ~UnixSocketConsumer();
 
 public:
+  NS_INLINE_DECL_THREADSAFE_REFCOUNTING(UnixSocketConsumer)
+
   UnixSocketConsumer();
+
+  SocketConnectionStatus GetConnectionStatus() const
+  {
+    MOZ_ASSERT(NS_IsMainThread());
+    return mConnectionStatus;
+  }
+
+  int GetSuggestedConnectDelayMs() const
+  {
+    MOZ_ASSERT(NS_IsMainThread());
+    return mConnectDelayMs;
+  }
+
+  
+
+
+
+
+
+  virtual void ReceiveSocketData(nsAutoPtr<UnixSocketRawData>& aMessage) = 0;
 
   
 
@@ -157,10 +219,46 @@ public:
   
 
 
+
+  virtual void OnConnectSuccess() = 0;
+
+  
+
+
+  virtual void OnConnectError() = 0;
+
+  
+
+
+  virtual void OnDisconnect() = 0;
+
+  
+
+
+  void NotifySuccess();
+
+  
+
+
+  void NotifyError();
+
+  
+
+
+  void NotifyDisconnect();
+
+  
+
+
   void GetSocketAddr(nsAString& aAddrStr);
 
 private:
+  uint32_t CalculateConnectDelayMs() const;
+
   UnixSocketImpl* mImpl;
+  SocketConnectionStatus mConnectionStatus;
+  PRIntervalTime mConnectTimestamp;
+  uint32_t mConnectDelayMs;
 };
 
 } 
