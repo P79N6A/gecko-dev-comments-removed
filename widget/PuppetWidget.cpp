5a1051c26774e9b90048abc4f897e28d6d9d5411
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:09:19 +0000

    Bug 1121313 - Part 3. Don't commit composition on IME blur from child process's widget. r=masayuki
    UltraBlame original commit: e726777aad6f667684321ca2ae175e13f6c3fd74

--- widget/PuppetWidget.cpp
+++ widget/PuppetWidget.cpp
@@ -502,9 +502,6 @@ PuppetWidget::NotifyIMEOfFocusChange(bool aFocus)
     if (queryEvent.mSucceeded) {
       mTabChild->SendNotifyIMETextHint(queryEvent.mReply.mString);
     }
-  } else {
-    
-    IMEEndComposition(false);
   }
 
   uint32_t chromeSeqno;
