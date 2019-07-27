4e001abc6f3c5bd3d939c22f63f5509f45df3648
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:01:24 +0000

    Bug 1175636 - Do not spawn a watchdog for each test on Windows; r=sfink
    UltraBlame original commit: cf426328aee818b5a1756123243631dd55193f34

--- js/src/tests/lib/tasks_win.py
+++ js/src/tests/lib/tasks_win.py
@@ -3,7 +3,11 @@
 
 from __future__ import print_function, unicode_literals, division
 
+import subprocess
 import sys
+from datetime import datetime, timedelta
+from progressbar import ProgressBar
+from results import TestOutput
 from threading import Thread
 from Queue import Queue, Empty
 
@@ -12,13 +16,52 @@ class EndMarker:
     pass
 
 
-def _do_work(qTasks, qResults, prefix, timeout):
+class TaskFinishedMarker:
+    pass
+
+
+def _do_work(qTasks, qResults, qWatch, prefix, timeout):
     while True:
         test = qTasks.get(block=True, timeout=sys.maxint)
         if test is EndMarker:
+            qWatch.put(EndMarker)
             qResults.put(EndMarker)
             return
-        qResults.put(test.run(prefix, timeout))
+
+        
+        cmd = test.get_command(prefix)
+        tStart = datetime.now()
+        proc = subprocess.Popen(cmd,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+
+        
+        
+        
+        qWatch.put(proc)
+        out, err = proc.communicate()
+        qWatch.put(TaskFinishedMarker)
+
+        
+        dt = datetime.now() - tStart
+        result = TestOutput(test, cmd, out, err, proc.returncode, dt.total_seconds(),
+                            dt > timedelta(seconds=timeout))
+        qResults.put(result)
+
+
+def _do_watch(qWatch, timeout):
+    while True:
+        proc = qWatch.get(True)
+        if proc == EndMarker:
+            return
+        try:
+            fin = qWatch.get(block=True, timeout=timeout)
+            assert fin is TaskFinishedMarker, "invalid finish marker"
+        except Empty:
+            
+            proc.terminate()
+            fin = qWatch.get(block=True, timeout=sys.maxint)
+            assert fin is TaskFinishedMarker, "invalid finish marker"
 
 
 def run_all_tests_gen(tests, prefix, results, options):
@@ -28,9 +71,15 @@ def run_all_tests_gen(tests, prefix, results, options):
     qTasks, qResults = Queue(), Queue()
 
     workers = []
+    watchdogs = []
     for _ in range(options.worker_count):
-        worker = Thread(target=_do_work, args=(qTasks, qResults, prefix,
-                                               options.timeout))
+        qWatch = Queue()
+        watcher = Thread(target=_do_watch, args=(qWatch, options.timeout))
+        watcher.setDaemon(True)
+        watcher.start()
+        watchdogs.append(watcher)
+        worker = Thread(target=_do_work, args=(qTasks, qResults, qWatch,
+                                               prefix, options.timeout))
         worker.setDaemon(True)
         worker.start()
         workers.append(worker)
@@ -47,16 +96,22 @@ def run_all_tests_gen(tests, prefix, results, options):
 
     
     ended = 0
+    delay = ProgressBar.update_granularity().total_seconds()
     while ended < len(workers):
-        result = qResults.get(block=True, timeout=sys.maxint)
-        if result is EndMarker:
-            ended += 1
-        else:
-            yield result
+        try:
+            result = qResults.get(block=True, timeout=delay)
+            if result is EndMarker:
+                ended += 1
+            else:
+                yield result
+        except Empty:
+            results.pb.poke()
 
     
     for worker in workers:
         worker.join()
+    for watcher in watchdogs:
+        watcher.join()
     assert qTasks.empty(), "Send queue not drained"
     assert qResults.empty(), "Result queue not drained"
 
