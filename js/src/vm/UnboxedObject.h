02abc565153c6937fe90771c3cb7dd9ac0d3dc3f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:00:18 +0000

    Bug 1157809 - Optimizing scanning of unboxed plain objects and typed objects during nursery collections, r=terrence.
    UltraBlame original commit: 2551d444a29b826ed93321ab3ecd9e536d2b1252

--- js/src/vm/UnboxedObject.h
+++ js/src/vm/UnboxedObject.h
@@ -233,6 +233,11 @@ class UnboxedPlainObject : public JSObject
         expando_ = nullptr;
     }
 
+    
+    JSObject** addressOfExpando() {
+        return reinterpret_cast<JSObject**>(&expando_);
+    }
+
     bool containsUnboxedOrExpandoProperty(ExclusiveContext* cx, jsid id) const;
 
     static UnboxedExpandoObject* ensureExpando(JSContext* cx, Handle<UnboxedPlainObject*> obj);
