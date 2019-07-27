cca4ed8df27c8c036d40e5289893144d86e62c2c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:23:01 +0000

    Bug 1143903 - Display username and password as separate fields in the password doorhanger. r=MattN
    UltraBlame original commit: aa2b8bc620db9e80b6c8254e654c2f7d2153f9af

--- toolkit/components/passwordmgr/test/browser/browser_notifications.js
+++ toolkit/components/passwordmgr/test/browser/browser_notifications.js
@@ -1,33 +1,83 @@
 
 
 
-add_task(function* test_save() {
-  let tab = gBrowser.addTab("https://example.com/browser/toolkit/components/" +
-                            "passwordmgr/test/browser/form_basic.html");
-  let browser = tab.linkedBrowser;
-  yield BrowserTestUtils.browserLoaded(browser);
-  gBrowser.selectedTab = tab;
+Cu.import("resource://testing-common/LoginTestUtils.jsm", this);
 
-  let promiseShown = BrowserTestUtils.waitForEvent(PopupNotifications.panel,
-                                                   "Shown");
-  yield ContentTask.spawn(browser, null, function* () {
-    content.document.getElementById("form-basic-username").value = "username";
-    content.document.getElementById("form-basic-password").value = "password";
-    content.document.getElementById("form-basic").submit();
-  });
-  yield promiseShown;
-  let notificationElement = PopupNotifications.panel.childNodes[0];
 
-  let promiseLogin = TestUtils.topicObserved("passwordmgr-storage-changed",
-                                             (_, data) => data == "addLogin");
-  notificationElement.button.doCommand();
-  let [login] = yield promiseLogin;
-  login.QueryInterface(Ci.nsILoginInfo);
 
-  Assert.equal(login.username, "username");
-  Assert.equal(login.password, "password");
 
-  
-  Services.logins.removeAllLogins();
-  gBrowser.removeTab(tab);
+
+add_task(function* test_save_change() {
+  let testCases = [{
+    username: "username",
+    password: "password",
+  }, {
+    username: "",
+    password: "password",
+  }, {
+    username: "username",
+    oldPassword: "password",
+    password: "newPassword",
+  }, {
+    username: "",
+    oldPassword: "password",
+    password: "newPassword",
+  }];
+
+  for (let { username, oldPassword, password } of testCases) {
+    
+    if (oldPassword) {
+      Services.logins.addLogin(LoginTestUtils.testData.formLogin({
+        hostname: "https://example.com",
+        formSubmitURL: "https://example.com",
+        username,
+        password: oldPassword,
+      }));
+    }
+
+    yield BrowserTestUtils.withNewTab({
+      gBrowser,
+      url: "https://example.com/browser/toolkit/components/" +
+           "passwordmgr/test/browser/form_basic.html",
+    }, function* (browser) {
+      
+      
+      let promiseShown = BrowserTestUtils.waitForEvent(PopupNotifications.panel,
+                                                       "Shown");
+      yield ContentTask.spawn(browser, { username, password },
+        function* ({ username, password }) {
+          let doc = content.document;
+          doc.getElementById("form-basic-username").value = username;
+          doc.getElementById("form-basic-password").value = password;
+          doc.getElementById("form-basic").submit();
+        });
+      yield promiseShown;
+
+      
+      Assert.equal(document.getElementById("password-notification-username")
+                           .getAttribute("value"), username);
+      Assert.equal(document.getElementById("password-notification-password")
+                           .getAttribute("value"), password);
+
+      
+      
+      
+      let expectedNotification = oldPassword ? "modifyLogin" : "addLogin";
+      let promiseLogin = TestUtils.topicObserved("passwordmgr-storage-changed",
+                         (_, data) => data == expectedNotification);
+      let notificationElement = PopupNotifications.panel.childNodes[0];
+      notificationElement.button.doCommand();
+      let [result] = yield promiseLogin;
+
+      
+      let login = oldPassword ? result.QueryInterface(Ci.nsIArray)
+                                      .queryElementAt(1, Ci.nsILoginInfo)
+                              : result.QueryInterface(Ci.nsILoginInfo);
+      Assert.equal(login.username, username);
+      Assert.equal(login.password, password);
+    });
+
+    
+    Services.logins.removeAllLogins();
+  }
 });
