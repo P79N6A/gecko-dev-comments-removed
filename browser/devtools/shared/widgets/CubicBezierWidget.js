d32be0aff4f79356e495fa7f0c49af8ef0236df8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:23:44 +0000

    Bug 1154809 - rewrite CubicBezierWidget to use CSSLexer; r=pbrosset
    UltraBlame original commit: 555aa2087f62c8e5bd0c16bc8124450f4cf90b78

--- browser/devtools/shared/widgets/CubicBezierWidget.js
+++ browser/devtools/shared/widgets/CubicBezierWidget.js
@@ -28,6 +28,10 @@
 const EventEmitter = require("devtools/toolkit/event-emitter");
 const {setTimeout, clearTimeout} = require("sdk/timers");
 const {PREDEFINED, PRESETS, DEFAULT_PRESET_CATEGORY} = require("devtools/shared/widgets/CubicBezierPresets");
+const {Cc, Ci} = require('chrome');
+loader.lazyGetter(this, "DOMUtils", () => {
+  return Cc["@mozilla.org/inspector/dom-utils;1"].getService(Ci.inIDOMUtils);
+});
 
 
 
@@ -439,12 +443,7 @@ CubicBezierWidget.prototype = {
     value = value.trim();
 
     
-    let coordinates = PREDEFINED[value];
-
-    
-    if (!coordinates && value.startsWith("cubic-bezier")) {
-      coordinates = value.replace(/cubic-bezier|\(|\)/g, "").split(",").map(parseFloat);
-    }
+    let coordinates = parseTimingFunction(value);
 
     this.presets.refreshMenu(coordinates);
     this.coordinates = coordinates;
@@ -760,7 +759,7 @@ TimingFunctionPreviewWidget.prototype = {
 
     clearTimeout(this.autoRestartAnimation);
 
-    if (isValidTimingFunction(value)) {
+    if (parseTimingFunction(value)) {
       this.dot.style.animationTimingFunction = value;
       this.restartAnimation();
     }
@@ -815,19 +814,49 @@ function distance(x1, y1, x2, y2) {
 
 
 
-function isValidTimingFunction(value) {
-  
+
+
+function parseTimingFunction(value) {
   if (value in PREDEFINED) {
-    return true;
+    return PREDEFINED[value];
   }
 
-  
-  if (value.match(/^cubic-bezier\(([0-9.\- ]+,){3}[0-9.\- ]+\)/)) {
-    return true;
+  let tokenStream = DOMUtils.getCSSLexer(value);
+  let getNextToken = () => {
+    while (true) {
+      let token = tokenStream.nextToken();
+      if (!token || (token.tokenType !== "whitespace" &&
+                     token.tokenType !== "comment")) {
+        return token;
+      }
+    }
+  };
+
+  let token = getNextToken();
+  if (token.tokenType !== "function" || token.text !== "cubic-bezier") {
+    return undefined;
   }
 
-  return false;
+  let result = [];
+  for (let i = 0; i < 4; ++i) {
+    token = getNextToken();
+    if (!token || token.tokenType !== "number") {
+      return undefined;
+    }
+    result.push(token.number);
+
+    token = getNextToken();
+    if (!token || token.tokenType !== "symbol" ||
+        token.text !== (i == 3 ? ")" : ",")) {
+      return undefined;
+    }
+  }
+
+  return result;
 }
+
+
+exports._parseTimingFunction = parseTimingFunction;
 
 
 
