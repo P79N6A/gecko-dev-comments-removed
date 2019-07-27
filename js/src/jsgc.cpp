a7f46acebfb84515593ddbfbf7942de93f33681a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:26:44 +0000

    Bug 1141563 - Don't update Shapes in parallel after compacting GC r=terrence
    UltraBlame original commit: 5850aa2851fe3706a75327b1495f2d3decb6af37

--- js/src/jsgc.cpp
+++ js/src/jsgc.cpp
@@ -2312,6 +2312,8 @@ struct ArenasToUpdate
 bool ArenasToUpdate::shouldProcessKind(AllocKind kind)
 {
     MOZ_ASSERT(kind < AllocKind::LIMIT);
+
+    
     if (kind == AllocKind::FAT_INLINE_STRING ||
         kind == AllocKind::STRING ||
         kind == AllocKind::EXTERNAL_STRING ||
@@ -2320,10 +2322,19 @@ bool ArenasToUpdate::shouldProcessKind(AllocKind kind)
         return false;
     }
 
-    if (js::gc::IsBackgroundFinalized(kind))
+    
+    
+    
+    
+    
+    if (js::gc::IsBackgroundFinalized(kind) &&
+        kind != AllocKind::SHAPE &&
+        kind != AllocKind::ACCESSOR_SHAPE)
+    {
         return (kinds & BACKGROUND) != 0;
-    else
+    } else {
         return (kinds & FOREGROUND) != 0;
+    }
 }
 
 ArenasToUpdate::ArenasToUpdate(Zone *zone, KindsToUpdate kinds)
