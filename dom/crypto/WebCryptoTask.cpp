49aaa61ea3b309fcbc8467211c42b9989dbe153c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:47:22 +0000

    Bug 1064320 - NSC_Encrypt returns uninitialised garbage which is handed onwards to realloc
    UltraBlame original commit: b87e4ed4ffc6b6845fa52a0e73009c8f0fc16cb6

--- dom/crypto/WebCryptoTask.cpp
+++ dom/crypto/WebCryptoTask.cpp
@@ -824,7 +824,7 @@ private:
     param.data = (unsigned char*) &oaepParams;
     param.len = sizeof(oaepParams);
 
-    uint32_t outLen;
+    uint32_t outLen = 0;
     if (mEncrypt) {
       
       
@@ -841,9 +841,9 @@ private:
              mResult.Elements(), &outLen, mResult.Length(),
              mData.Elements(), mData.Length()));
     }
-    mResult.SetLength(outLen);
-
     NS_ENSURE_SUCCESS(rv, NS_ERROR_DOM_OPERATION_ERR);
+
+    mResult.SetLength(outLen);
     return NS_OK;
   }
 };
