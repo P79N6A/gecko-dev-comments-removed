bb8ba7ada838131898ca5f03d93f97f592f8e510
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:37:30 +0000

    Bug 1102703 - Porting bluetooth interface of bluez 5.26 (core). r=btian, f=tzimmermann
    UltraBlame original commit: f62155e4fb9c32f91e21395bab8b870b992f0388

--- dom/bluetooth/bluedroid/BluetoothDaemonHelpers.h
+++ dom/bluetooth/bluedroid/BluetoothDaemonHelpers.h
@@ -419,6 +419,9 @@ nsresult
 PackPDU(BluetoothScanMode aIn, BluetoothDaemonPDU& aPDU);
 
 nsresult
+PackPDU(BluetoothTransport aIn, BluetoothDaemonPDU& aPDU);
+
+nsresult
 PackPDU(ControlPlayStatus aIn, BluetoothDaemonPDU& aPDU);
 
 
