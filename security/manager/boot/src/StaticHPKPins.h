c26bd4ec278ee7b75cb86946e90af39c7bda5690
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:48:02 +0000

    Merge inbound to m-c. a=merge
    UltraBlame original commit: 94ba78a423050deda23c6382fc3aa004d5e89913

--- security/manager/boot/src/StaticHPKPins.h
+++ security/manager/boot/src/StaticHPKPins.h
@@ -1070,4 +1070,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1416397232719000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1416650879721000);
