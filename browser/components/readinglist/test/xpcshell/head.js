834b9d8a787cffcc9cf05059d5945d68e44e55e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:15:40 +0000

    Backed out changeset a226fd427f01 (bug 1131412)
    UltraBlame original commit: 981635797452f5c98a7976e9dfe179b5ac42d533

--- browser/components/readinglist/test/xpcshell/head.js
+++
@@ -1,7 +0,0 @@
-
-
-
-const {classes: Cc, interfaces: Ci, utils: Cu, results: Cr} = Components;
-
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-Cu.import("resource://gre/modules/Services.jsm");
