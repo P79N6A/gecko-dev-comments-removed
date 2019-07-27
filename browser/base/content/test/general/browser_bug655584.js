fc7c4c83aec7be7f19f20b3cf5bcf4bd87013626
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:56:11 +0000

    Bug 1155348, e10s, rewrite and reenable browser_bug655584, r=felipe
    UltraBlame original commit: 1011f5a173904e3d6b88316fd50355f51b936333

--- browser/base/content/test/general/browser_bug655584.js
+++ browser/base/content/test/general/browser_bug655584.js
@@ -4,20 +4,20 @@
 
 
 
-function test() {
+add_task(function* () {
   var tab1 = gBrowser.addTab();
   var tab2 = gBrowser.addTab();
 
   
   
-  gBrowser.selectedTab = tab2;
+  yield BrowserTestUtils.switchTab(gBrowser, tab2);
   gURLBar.focus(); 
-  gBrowser.selectedTab = tab1;
+  yield BrowserTestUtils.switchTab(gBrowser, tab1);
   gURLBar.openPopup();
-  gBrowser.selectedTab = tab2;
+  yield BrowserTestUtils.switchTab(gBrowser, tab2);
   ok(!gURLBar.popupOpen, "urlbar focused in tab to switch to, close popup");
   
   
   gBrowser.removeCurrentTab();
   gBrowser.removeCurrentTab();
-}
+});
