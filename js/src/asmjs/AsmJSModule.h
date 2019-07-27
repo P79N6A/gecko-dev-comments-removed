0800795ba10a41760d59a92b767012ceeb2d7842
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:19:09 +0000

    Bug 1064668 - OdinMonkey: Maintain correct offsets for src line info (r=bbouvier)
    UltraBlame original commit: 526c183e9cd3dea3a8d6448346cb7bd629dafb5b

--- js/src/asmjs/AsmJSModule.h
+++ js/src/asmjs/AsmJSModule.h
@@ -1127,18 +1127,21 @@ class AsmJSModule
     
 
     bool addExportedFunction(PropertyName *name,
-                             uint32_t srcStart,
-                             uint32_t srcEnd,
+                             uint32_t funcSrcBegin,
+                             uint32_t funcSrcEnd,
                              PropertyName *maybeFieldName,
                              ArgCoercionVector &&argCoercions,
                              ReturnType returnType)
     {
+        
+        
+        
         JS_ASSERT(isFinishedWithFunctionBodies() && !isFinished());
-        ExportedFunction func(name, srcStart, srcEnd, maybeFieldName,
-                              mozilla::Move(argCoercions), returnType);
-        if (exports_.length() >= UINT32_MAX)
-            return false;
-        return exports_.append(mozilla::Move(func));
+        JS_ASSERT(srcStart_ < funcSrcBegin);
+        JS_ASSERT(funcSrcBegin < funcSrcEnd);
+        ExportedFunction func(name, funcSrcBegin - srcStart_, funcSrcEnd - srcStart_,
+                              maybeFieldName, mozilla::Move(argCoercions), returnType);
+        return exports_.length() < UINT32_MAX && exports_.append(mozilla::Move(func));
     }
     unsigned numExportedFunctions() const {
         JS_ASSERT(isFinishedWithFunctionBodies());
