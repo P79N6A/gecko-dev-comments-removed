d76e1b41b02dba19139fe9508151a5e2bd7b67c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:49:37 +0000

    Bug 1061469 - part 8: Encoding for Imports. r=mrbkap
    UltraBlame original commit: 7c3db413f9da1fa18f1818603a257fa2a1441cf4

--- content/base/src/nsDocument.cpp
+++ content/base/src/nsDocument.cpp
@@ -3520,6 +3520,10 @@ nsDocument::SetDocumentCharacterSet(const nsACString& aCharSetID)
   
   
   if (!mCharacterSet.Equals(aCharSetID)) {
+    if (mMasterDocument && !aCharSetID.EqualsLiteral("UTF-8")) {
+      
+      return;
+    }
     mCharacterSet = aCharSetID;
 
     int32_t n = mCharSetObservers.Length();
