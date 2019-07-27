defb178901777c7ba6251d32f49c1f639f4972d9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:02:04 +0000

    Bug 1158545 - Remove nsFilePickerProxy::mParent in favor of nsBaseFilePicker::mParent; r=roc
    There is no need to have two separate variables for the same thing
    in the same object.
    
    UltraBlame original commit: 62091556e23a553817040c6a73dde37b33ade22c

--- widget/nsFilePickerProxy.h
+++ widget/nsFilePickerProxy.h
@@ -59,8 +59,6 @@ private:
     ~nsFilePickerProxy();
     void InitNative(nsIWidget*, const nsAString&) override;
 
-    
-    nsCOMPtr<nsPIDOMWindow> mParent;
     nsCOMArray<nsIDOMFile> mDomfiles;
     nsCOMPtr<nsIFilePickerShownCallback> mCallback;
 
