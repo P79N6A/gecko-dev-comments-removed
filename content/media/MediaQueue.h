e0e5d783890b916549372a8c0e1eb1b7e3e44f93
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:45:57 +0000

    Bug 1054149 - Fix anFoo coding style violation in content/media.  r=cpearce
    UltraBlame original commit: 5cbae203cad255b105a172482b1a977b80eb7f48

--- content/media/MediaQueue.h
+++ content/media/MediaQueue.h
@@ -17,8 +17,8 @@ namespace mozilla {
 
 template <class T>
 class MediaQueueDeallocator : public nsDequeFunctor {
-  virtual void* operator() (void* anObject) {
-    delete static_cast<T*>(anObject);
+  virtual void* operator() (void* aObject) {
+    delete static_cast<T*>(aObject);
     return nullptr;
   }
 };
