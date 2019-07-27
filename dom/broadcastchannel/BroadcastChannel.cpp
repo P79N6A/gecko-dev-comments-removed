73da5f547b14c0545fa703d00e466bbc2b9ea9be
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:43:30 +0000

    Bug 966439 - BroadcastChannel API - patch 2 - close() method, r=smaug
    UltraBlame original commit: 2ebe98dfcc9d07d1af49a6f58fad40b8534bcbca

--- dom/broadcastchannel/BroadcastChannel.cpp
+++ dom/broadcastchannel/BroadcastChannel.cpp
@@ -225,6 +225,7 @@ BroadcastChannel::BroadcastChannel(nsPIDOMWindow* aWindow,
   , mChannel(aChannel)
   , mIsKeptAlive(false)
   , mInnerID(0)
+  , mState(StateActive)
 {
   
 }
@@ -328,7 +329,18 @@ BroadcastChannel::Constructor(const GlobalObject& aGlobal,
 }
 
 void
-BroadcastChannel::PostMessage(const nsAString& aMessage)
+BroadcastChannel::PostMessage(const nsAString& aMessage, ErrorResult& aRv)
+{
+  if (mState != StateActive) {
+    aRv.Throw(NS_ERROR_FAILURE);
+    return;
+  }
+
+  PostMessageInternal(aMessage);
+}
+
+void
+BroadcastChannel::PostMessageInternal(const nsAString& aMessage)
 {
   if (mActor) {
     nsRefPtr<PostMessageRunnable> runnable =
@@ -345,6 +357,21 @@ BroadcastChannel::PostMessage(const nsAString& aMessage)
 }
 
 void
+BroadcastChannel::Close()
+{
+  if (mState != StateActive) {
+    return;
+  }
+
+  if (mPendingMessages.IsEmpty()) {
+    Shutdown();
+  } else {
+    MOZ_ASSERT(!mActor);
+    mState = StateClosing;
+  }
+}
+
+void
 BroadcastChannel::ActorFailed()
 {
   MOZ_CRASH("Failed to create a PBackgroundChild actor!");
@@ -354,6 +381,10 @@ void
 BroadcastChannel::ActorCreated(ipc::PBackgroundChild* aActor)
 {
   MOZ_ASSERT(aActor);
+
+  if (mState == StateClosed) {
+    return;
+  }
 
   PBroadcastChannelChild* actor =
     aActor->SendPBroadcastChannelConstructor(mPrincipalInfo, mOrigin, mChannel);
@@ -365,15 +396,21 @@ BroadcastChannel::ActorCreated(ipc::PBackgroundChild* aActor)
 
   
   for (uint32_t i = 0; i < mPendingMessages.Length(); ++i) {
-    PostMessage(mPendingMessages[i]);
+    PostMessageInternal(mPendingMessages[i]);
   }
 
   mPendingMessages.Clear();
+
+  if (mState == StateClosing) {
+    Shutdown();
+  }
 }
 
 void
 BroadcastChannel::Shutdown()
 {
+  mState = StateClosed;
+
   
   
   if (mIsKeptAlive) {
