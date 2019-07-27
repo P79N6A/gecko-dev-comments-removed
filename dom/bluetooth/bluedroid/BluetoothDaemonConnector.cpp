f1d6cbdb0da2b54bc111ee279fe4008708cca4b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:23:27 +0000

    Bug 1161020: Implement new socket-connector interface for Bluetooth daemon socket, r=shuang
    This patch moves |BluetoothDaemonSocketConnector| into its own file,
    renames it to |BluetoothDaemonConnector| and implements the new socket-
    connector interface.
    
    UltraBlame original commit: b8c39f01f5f4780b3b77cc2cc0c3572b555c4794

---
+++ dom/bluetooth/bluedroid/BluetoothDaemonConnector.cpp
@@ -0,0 +1,237 @@
+
+
+
+
+
+
+
+#include "BluetoothDaemonConnector.h"
+#include <fcntl.h>
+#include "nsThreadUtils.h"
+
+BEGIN_BLUETOOTH_NAMESPACE
+
+BluetoothDaemonConnector::BluetoothDaemonConnector(
+  const nsACString& aSocketName)
+  : mSocketName(aSocketName)
+{ }
+
+BluetoothDaemonConnector::~BluetoothDaemonConnector()
+{ }
+
+nsresult
+BluetoothDaemonConnector::CreateSocket(int& aFd) const
+{
+  aFd = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+  if (aFd < 0) {
+    BT_WARNING("Could not open Bluetooth daemon socket!");
+    return NS_ERROR_FAILURE;
+  }
+
+  return NS_OK;
+}
+
+nsresult
+BluetoothDaemonConnector::SetSocketFlags(int aFd) const
+{
+  static const int sReuseAddress = 1;
+
+  
+  int flags = TEMP_FAILURE_RETRY(fcntl(aFd, F_GETFD));
+  if (flags < 0) {
+    return NS_ERROR_FAILURE;
+  }
+  flags |= FD_CLOEXEC;
+  int res = TEMP_FAILURE_RETRY(fcntl(aFd, F_SETFD, flags));
+  if (res < 0) {
+    return NS_ERROR_FAILURE;
+  }
+
+  
+  flags = TEMP_FAILURE_RETRY(fcntl(aFd, F_GETFL));
+  if (flags < 0) {
+    return NS_ERROR_FAILURE;
+  }
+  flags |= O_NONBLOCK;
+  res = TEMP_FAILURE_RETRY(fcntl(aFd, F_SETFL, flags));
+  if (res < 0) {
+    return NS_ERROR_FAILURE;
+  }
+
+  
+  res = setsockopt(aFd, SOL_SOCKET, SO_REUSEADDR, &sReuseAddress,
+                   sizeof(sReuseAddress));
+  if (res < 0) {
+    return NS_ERROR_FAILURE;
+  }
+
+  return NS_OK;
+}
+
+nsresult
+BluetoothDaemonConnector::CreateAddress(struct sockaddr& aAddress,
+                                        socklen_t& aAddressLength) const
+{
+  static const size_t sNameOffset = 1;
+
+  struct sockaddr_un* address =
+    reinterpret_cast<struct sockaddr_un*>(&aAddress);
+
+  size_t namesiz = mSocketName.Length() + 1; 
+
+  if (NS_WARN_IF((sNameOffset + namesiz) > sizeof(address->sun_path))) {
+    return NS_ERROR_FAILURE;
+  }
+
+  address->sun_family = AF_UNIX;
+  memset(address->sun_path, '\0', sNameOffset); 
+  memcpy(address->sun_path + sNameOffset, mSocketName.get(), namesiz);
+
+  aAddressLength =
+    offsetof(struct sockaddr_un, sun_path) + sNameOffset + namesiz;
+
+  return NS_OK;
+}
+
+
+
+nsresult
+BluetoothDaemonConnector::ConvertAddressToString(
+  const struct sockaddr& aAddress, socklen_t aAddressLength,
+  nsACString& aAddressString)
+{
+  MOZ_ASSERT(aAddress.sa_family == AF_UNIX);
+
+  const struct sockaddr_un* un =
+    reinterpret_cast<const struct sockaddr_un*>(&aAddress);
+
+  size_t len = aAddressLength - offsetof(struct sockaddr_un, sun_path);
+
+  aAddressString.Assign(un->sun_path, len);
+
+  return NS_OK;
+}
+
+nsresult
+BluetoothDaemonConnector::CreateListenSocket(struct sockaddr* aAddress,
+                                             socklen_t* aAddressLength,
+                                             int& aListenFd)
+{
+  ScopedClose fd;
+
+  nsresult rv = CreateSocket(fd.rwget());
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+  rv = SetSocketFlags(fd);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+  if (aAddress && aAddressLength) {
+    rv = CreateAddress(*aAddress, *aAddressLength);
+    if (NS_FAILED(rv)) {
+      return rv;
+    }
+  }
+
+  aListenFd = fd.forget();
+
+  return NS_OK;
+}
+
+nsresult
+BluetoothDaemonConnector::AcceptStreamSocket(int aListenFd,
+                                             struct sockaddr* aAddress,
+                                             socklen_t* aAddressLength,
+                                             int& aStreamFd)
+{
+  ScopedClose fd(
+    TEMP_FAILURE_RETRY(accept(aListenFd, aAddress, aAddressLength)));
+  if (fd < 0) {
+    NS_WARNING("Cannot accept file descriptor!");
+    return NS_ERROR_FAILURE;
+  }
+  nsresult rv = SetSocketFlags(fd);
+  if (NS_FAILED(rv)) {
+    return rv;
+  }
+
+  aStreamFd = fd.forget();
+
+  return NS_OK;
+}
+
+nsresult
+BluetoothDaemonConnector::CreateStreamSocket(struct sockaddr* aAddress,
+                                             socklen_t* aAddressLength,
+                                             int& aStreamFd)
+{
+  MOZ_CRASH("|BluetoothDaemonConnector| does not support "
+            "creating stream sockets.");
+  return NS_ERROR_ABORT;
+}
+
+
+
+int
+BluetoothDaemonConnector::Create()
+{
+  MOZ_ASSERT(!NS_IsMainThread());
+
+  int fd = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+  if (fd < 0) {
+    BT_WARNING("Could not open socket!");
+    return -1;
+  }
+  return fd;
+}
+
+bool
+BluetoothDaemonConnector::CreateAddr(bool aIsServer,
+                                     socklen_t& aAddrSize,
+                                     mozilla::ipc::sockaddr_any& aAddr,
+                                     const char* aAddress)
+{
+  static const size_t sNameOffset = 1;
+
+  size_t namesiz = mSocketName.Length() + 1; 
+
+  if ((sNameOffset + namesiz) > sizeof(aAddr.un.sun_path)) {
+    BT_WARNING("Address too long for socket struct!");
+    return false;
+  }
+
+  memset(aAddr.un.sun_path, '\0', sNameOffset); 
+  memcpy(aAddr.un.sun_path + sNameOffset, mSocketName.get(), namesiz);
+  aAddr.un.sun_family = AF_UNIX;
+
+  aAddrSize = offsetof(struct sockaddr_un, sun_path) + sNameOffset + namesiz;
+
+  return true;
+}
+
+bool
+BluetoothDaemonConnector::SetUp(int aFd)
+{
+  if (TEMP_FAILURE_RETRY(fcntl(aFd, F_SETFL, O_NONBLOCK)) < 0) {
+    BT_WARNING("Failed to set non-blocking I/O.");
+    return false;
+  }
+  return true;
+}
+
+bool
+BluetoothDaemonConnector::SetUpListenSocket(int aFd)
+{
+  return true;
+}
+
+void
+BluetoothDaemonConnector::GetSocketAddr(
+  const mozilla::ipc::sockaddr_any& aAddr, nsAString& aAddrStr)
+{
+  
+  MOZ_CRASH("This should never be called!");
+}
+
+END_BLUETOOTH_NAMESPACE
