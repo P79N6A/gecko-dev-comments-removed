900d277a5d6037d50a57bc63de6d79116935ed00
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:21:57 +0000

    Bug 1102209 - Remove use of CodeGen::JoinInstructions in the Linux sandboxing code. r=kang
    This reorganizes SandboxAssembler to stack up the policy rules and
    traverse them in reverse order to build the filter DAG from tail to head
    (i.e., starting with "deny all" and prepending allow and return-errno
    rules).  Thus, this code will continue to work (perhaps with minor
    changes, such as to the NodePtr typedef) with future versions of the
    Chromium sandbox code that don't allow mutating the filter program with
    the JoinInstructions method.
    
    UltraBlame original commit: a2ae4c0a26fe0ec5ddac93ec0c6c9642c4a3d321

--- security/sandbox/linux/SandboxAssembler.h
+++ security/sandbox/linux/SandboxAssembler.h
@@ -7,20 +7,16 @@
 #ifndef mozilla_SandboxAssembler_h
 #define mozilla_SandboxAssembler_h
 
-#include "sandbox/linux/seccomp-bpf/codegen.h"
-
 #include <vector>
+
 #include "mozilla/Assertions.h"
 
-using namespace sandbox;
+struct sock_filter;
 
 namespace mozilla {
 
 class SandboxAssembler {
 public:
-  SandboxAssembler();
-  ~SandboxAssembler();
-
   class Condition {
     friend class SandboxAssembler;
     uint32_t mSyscallNr;
@@ -49,45 +45,29 @@ public:
     {
       MOZ_ASSERT(aArgChecked < sNumArgs);
     }
+    
+    Condition(const Condition& aOther)
+    : mSyscallNr(aOther.mSyscallNr)
+    , mCheckingArg(aOther.mCheckingArg)
+    , mArgChecked(aOther.mArgChecked)
+    , mArgValues(aOther.mArgValues)
+    { }
   };
 
   
-  void Allow(const Condition &aCond) {
-    Handle(aCond, RetAllow());
+  void Allow(const Condition& aCond) {
+    mRuleStack.push_back(std::make_pair(0, aCond));
   }
   
   
-  void Deny(int aErrno, const Condition &aCond) {
-    Handle(aCond, RetDeny(aErrno));
+  void Deny(int aErrno, const Condition& aCond) {
+    MOZ_ASSERT(aErrno != 0);
+    mRuleStack.push_back(std::make_pair(aErrno, aCond));
   }
 
-  void Finish();
-  void Compile(std::vector<struct sock_filter> *aProgram,
-               bool aPrint = false);
+  void Compile(std::vector<sock_filter>* aProgram, bool aPrint = false);
 private:
-  CodeGen mCode;
-  
-  Instruction *mHead;
-  
-  
-  Instruction *mTail;
-  
-  
-  
-  Instruction *mTailAlt;
-
-  Instruction *RetAllow();
-  Instruction *RetDeny(int aErrno);
-  Instruction *RetKill();
-  Instruction *LoadArch(Instruction *aNext);
-  Instruction *LoadSyscall(Instruction *aNext);
-  Instruction *LoadArgHi(int aArg, Instruction *aNext);
-  Instruction *LoadArgLo(int aArg, Instruction *aNext);
-  Instruction *JmpEq(uint32_t aValue, Instruction *aThen, Instruction *aElse);
-  void AppendCheck(Instruction *aCheck,
-                   Instruction *aNewTail,
-                   Instruction *aNewTailAlt);
-  void Handle(const Condition &aCond, Instruction* aResult);
+  std::vector<std::pair<int, Condition>> mRuleStack;
 
   static const uint8_t sNumArgs = 6;
 };
