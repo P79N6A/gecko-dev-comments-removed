d9222ad04b349830a2a9fddfcf808d48ae22bb42
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:55:43 +0000

    Bug 1083989 - Adapting Devtools tests that expect Promise.jsm in the scope. r=paul
    UltraBlame original commit: c2df2cbac90fd36b0bf160b975f6439763a38203

--- toolkit/devtools/server/tests/browser/browser_storage_dynamic_windows.js
+++ toolkit/devtools/server/tests/browser/browser_storage_dynamic_windows.js
@@ -128,177 +128,177 @@ function testReload() {
 
   let shouldBeEmptyFirst = Cu.cloneInto(beforeReload,  {});
   let shouldBeEmptyLast = Cu.cloneInto(beforeReload,  {});
-  let reloaded = Promise.defer();
+  return new Promise(resolve => {
 
-  let onStoresUpdate = data => {
-    info("in stores update of testReload");
-    
-    
-    
-    
-    if (data.deleted) {
-      markOutMatched(shouldBeEmptyFirst, data.deleted, true);
-    }
+    let onStoresUpdate = data => {
+      info("in stores update of testReload");
+      
+      
+      
+      
+      if (data.deleted) {
+        markOutMatched(shouldBeEmptyFirst, data.deleted, true);
+      }
 
-    if (!Object.keys(shouldBeEmptyFirst).length) {
-      info("shouldBeEmptyFirst is empty now");
-    }
+      if (!Object.keys(shouldBeEmptyFirst).length) {
+        info("shouldBeEmptyFirst is empty now");
+      }
 
-    
-    
-    if (data.added) {
-      markOutMatched(shouldBeEmptyLast, data.added);
-    }
+      
+      
+      if (data.added) {
+        markOutMatched(shouldBeEmptyLast, data.added);
+      }
 
-    if (!Object.keys(shouldBeEmptyLast).length) {
-      info("Everything to be received is received.");
-      endTestReloaded();
-    }
-  };
+      if (!Object.keys(shouldBeEmptyLast).length) {
+        info("Everything to be received is received.");
+        endTestReloaded();
+      }
+    };
 
-  let endTestReloaded = () => {
-    gFront.off("stores-update", onStoresUpdate);
-    reloaded.resolve();
-  };
+    let endTestReloaded = () => {
+      gFront.off("stores-update", onStoresUpdate);
+      resolve();
+    };
 
-  gFront.on("stores-update", onStoresUpdate);
+    gFront.on("stores-update", onStoresUpdate);
 
-  content.location.reload();
-  return reloaded.promise;
+    content.location.reload();
+  });
 }
 
 function testAddIframe() {
   info("Testing if new iframe addition works properly");
-  let reloaded = Promise.defer();
+  return new Promise(resolve => {
 
-  let shouldBeEmpty = {
-    localStorage: {
-      "https://sectest1.example.org": ["iframe-s-ls1"]
-    },
-    sessionStorage: {
-      "https://sectest1.example.org": ["iframe-s-ss1"]
-    },
-    cookies: {
-      "sectest1.example.org": ["sc1"]
-    },
-    indexedDB: {
-      
-      
-      "https://sectest1.example.org": []
-    }
-  };
+    let shouldBeEmpty = {
+      localStorage: {
+        "https://sectest1.example.org": ["iframe-s-ls1"]
+      },
+      sessionStorage: {
+        "https://sectest1.example.org": ["iframe-s-ss1"]
+      },
+      cookies: {
+        "sectest1.example.org": ["sc1"]
+      },
+      indexedDB: {
+        
+        
+        "https://sectest1.example.org": []
+      }
+    };
 
-  let onStoresUpdate = data => {
-    info("checking if the hosts list is correct for this iframe addition");
+    let onStoresUpdate = data => {
+      info("checking if the hosts list is correct for this iframe addition");
 
-    markOutMatched(shouldBeEmpty, data.added);
+      markOutMatched(shouldBeEmpty, data.added);
 
-    ok(!data.changed || !data.changed.cookies ||
-       !data.changed.cookies["https://sectest1.example.org"],
-       "Nothing got changed for cookies");
-    ok(!data.changed || !data.changed.localStorage ||
-       !data.changed.localStorage["https://sectest1.example.org"],
-       "Nothing got changed for local storage");
-    ok(!data.changed || !data.changed.sessionStorage ||
-       !data.changed.sessionStorage["https://sectest1.example.org"],
-       "Nothing got changed for session storage");
-    ok(!data.changed || !data.changed.indexedDB ||
-       !data.changed.indexedDB["https://sectest1.example.org"],
-       "Nothing got changed for indexed db");
+      ok(!data.changed || !data.changed.cookies ||
+         !data.changed.cookies["https://sectest1.example.org"],
+         "Nothing got changed for cookies");
+      ok(!data.changed || !data.changed.localStorage ||
+         !data.changed.localStorage["https://sectest1.example.org"],
+         "Nothing got changed for local storage");
+      ok(!data.changed || !data.changed.sessionStorage ||
+         !data.changed.sessionStorage["https://sectest1.example.org"],
+         "Nothing got changed for session storage");
+      ok(!data.changed || !data.changed.indexedDB ||
+         !data.changed.indexedDB["https://sectest1.example.org"],
+         "Nothing got changed for indexed db");
 
-    ok(!data.deleted || !data.deleted.cookies ||
-       !data.deleted.cookies["https://sectest1.example.org"],
-       "Nothing got deleted for cookies");
-    ok(!data.deleted || !data.deleted.localStorage ||
-       !data.deleted.localStorage["https://sectest1.example.org"],
-       "Nothing got deleted for local storage");
-    ok(!data.deleted || !data.deleted.sessionStorage ||
-       !data.deleted.sessionStorage["https://sectest1.example.org"],
-       "Nothing got deleted for session storage");
-    ok(!data.deleted || !data.deleted.indexedDB ||
-       !data.deleted.indexedDB["https://sectest1.example.org"],
-       "Nothing got deleted for indexed db");
+      ok(!data.deleted || !data.deleted.cookies ||
+         !data.deleted.cookies["https://sectest1.example.org"],
+         "Nothing got deleted for cookies");
+      ok(!data.deleted || !data.deleted.localStorage ||
+         !data.deleted.localStorage["https://sectest1.example.org"],
+         "Nothing got deleted for local storage");
+      ok(!data.deleted || !data.deleted.sessionStorage ||
+         !data.deleted.sessionStorage["https://sectest1.example.org"],
+         "Nothing got deleted for session storage");
+      ok(!data.deleted || !data.deleted.indexedDB ||
+         !data.deleted.indexedDB["https://sectest1.example.org"],
+         "Nothing got deleted for indexed db");
 
-    if (!Object.keys(shouldBeEmpty).length) {
-      info("Everything to be received is received.");
-      endTestReloaded();
-    }
-  };
+      if (!Object.keys(shouldBeEmpty).length) {
+        info("Everything to be received is received.");
+        endTestReloaded();
+      }
+    };
 
-  let endTestReloaded = () => {
-    gFront.off("stores-update", onStoresUpdate);
-    reloaded.resolve();
-  };
+    let endTestReloaded = () => {
+      gFront.off("stores-update", onStoresUpdate);
+      resolve();
+    };
 
-  gFront.on("stores-update", onStoresUpdate);
+    gFront.on("stores-update", onStoresUpdate);
 
-  let iframe = content.document.createElement("iframe");
-  iframe.src = ALT_DOMAIN_SECURED + "storage-secured-iframe.html";
-  content.document.querySelector("body").appendChild(iframe);
-  return reloaded.promise;
+    let iframe = content.document.createElement("iframe");
+    iframe.src = ALT_DOMAIN_SECURED + "storage-secured-iframe.html";
+    content.document.querySelector("body").appendChild(iframe);
+  });
 }
 
 function testRemoveIframe() {
   info("Testing if iframe removal works properly");
-  let reloaded = Promise.defer();
+  return new Promise(resolve => {
 
-  let shouldBeEmpty = {
-    localStorage: {
-      "http://sectest1.example.org": []
-    },
-    sessionStorage: {
-      "http://sectest1.example.org": []
+    let shouldBeEmpty = {
+      localStorage: {
+        "http://sectest1.example.org": []
+      },
+      sessionStorage: {
+        "http://sectest1.example.org": []
+      }
+    };
+
+    let onStoresUpdate = data => {
+      info("checking if the hosts list is correct for this iframe deletion");
+
+      markOutMatched(shouldBeEmpty, data.deleted, true);
+
+      ok(!data.deleted.cookies || !data.deleted.cookies["sectest1.example.org"],
+        "Nothing got deleted for Cookies as the same hostname is still present");
+
+      ok(!data.changed || !data.changed.cookies ||
+         !data.changed.cookies["http://sectest1.example.org"],
+         "Nothing got changed for cookies");
+      ok(!data.changed || !data.changed.localStorage ||
+         !data.changed.localStorage["http://sectest1.example.org"],
+         "Nothing got changed for local storage");
+      ok(!data.changed || !data.changed.sessionStorage ||
+         !data.changed.sessionStorage["http://sectest1.example.org"],
+         "Nothing got changed for session storage");
+
+      ok(!data.added || !data.added.cookies ||
+         !data.added.cookies["http://sectest1.example.org"],
+         "Nothing got added for cookies");
+      ok(!data.added || !data.added.localStorage ||
+         !data.added.localStorage["http://sectest1.example.org"],
+         "Nothing got added for local storage");
+      ok(!data.added || !data.added.sessionStorage ||
+         !data.added.sessionStorage["http://sectest1.example.org"],
+         "Nothing got added for session storage");
+
+      if (!Object.keys(shouldBeEmpty).length) {
+        info("Everything to be received is received.");
+        endTestReloaded();
+      }
+    };
+
+    let endTestReloaded = () => {
+      gFront.off("stores-update", onStoresUpdate);
+      resolve();
+    };
+
+    gFront.on("stores-update", onStoresUpdate);
+
+    for (let iframe of content.document.querySelectorAll("iframe")) {
+      if (iframe.src.startsWith("http:")) {
+        iframe.remove();
+        break;
+      }
     }
-  };
-
-  let onStoresUpdate = data => {
-    info("checking if the hosts list is correct for this iframe deletion");
-
-    markOutMatched(shouldBeEmpty, data.deleted, true);
-
-    ok(!data.deleted.cookies || !data.deleted.cookies["sectest1.example.org"],
-       "Nothing got deleted for Cookies as the same hostname is still present");
-
-    ok(!data.changed || !data.changed.cookies ||
-       !data.changed.cookies["http://sectest1.example.org"],
-       "Nothing got changed for cookies");
-    ok(!data.changed || !data.changed.localStorage ||
-       !data.changed.localStorage["http://sectest1.example.org"],
-       "Nothing got changed for local storage");
-    ok(!data.changed || !data.changed.sessionStorage ||
-       !data.changed.sessionStorage["http://sectest1.example.org"],
-       "Nothing got changed for session storage");
-
-    ok(!data.added || !data.added.cookies ||
-       !data.added.cookies["http://sectest1.example.org"],
-       "Nothing got added for cookies");
-    ok(!data.added || !data.added.localStorage ||
-       !data.added.localStorage["http://sectest1.example.org"],
-       "Nothing got added for local storage");
-    ok(!data.added || !data.added.sessionStorage ||
-       !data.added.sessionStorage["http://sectest1.example.org"],
-       "Nothing got added for session storage");
-
-    if (!Object.keys(shouldBeEmpty).length) {
-      info("Everything to be received is received.");
-      endTestReloaded();
-    }
-  };
-
-  let endTestReloaded = () => {
-    gFront.off("stores-update", onStoresUpdate);
-    reloaded.resolve();
-  };
-
-  gFront.on("stores-update", onStoresUpdate);
-
-  for (let iframe of content.document.querySelectorAll("iframe")) {
-    if (iframe.src.startsWith("http:")) {
-      iframe.remove();
-      break;
-    }
-  }
-  return reloaded.promise;
+  });
 }
 
 function test() {
