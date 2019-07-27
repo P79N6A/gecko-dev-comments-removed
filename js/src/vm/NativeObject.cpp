3cc174433ce3bfc4f4d6bae789e1890618a135b4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:23:31 +0000

    Bug 1090636, part 7 - Assigning to an array element via a proxy should trigger the "shadowing" code. r=efaust.
    UltraBlame original commit: f2e89dd69118c5e9b568d5670293d748c4f31a8c

--- js/src/vm/NativeObject.cpp
+++ js/src/vm/NativeObject.cpp
@@ -2118,7 +2118,7 @@ SetExistingProperty(typename ExecutionModeTraits<mode>::ContextType cxArg,
     RootedShape shape(cxArg, foundShape);
     if (IsImplicitDenseOrTypedArrayElement(shape)) {
         
-        if (pobj != obj)
+        if (pobj != receiver)
             shape = nullptr;
     } else {
         
