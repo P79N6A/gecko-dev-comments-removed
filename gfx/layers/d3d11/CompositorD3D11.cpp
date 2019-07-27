d89e88eb1136f3885d50b313052bb042f8dc2b38
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:48:08 +0000

    Bug 1117925: Make sure to invalidate the entire window when the size changes. r=jrmuizel
    UltraBlame original commit: 68537ac70f77d26102dc428a4fe9a5b6501e2f96

--- gfx/layers/d3d11/CompositorD3D11.cpp
+++ gfx/layers/d3d11/CompositorD3D11.cpp
@@ -1050,6 +1050,7 @@ CompositorD3D11::BeginFrame(const nsIntRegion& aInvalidRegion,
     return;
   }
 
+  nsIntSize oldSize = mSize;
   UpdateRenderTarget();
 
   
@@ -1069,7 +1070,13 @@ CompositorD3D11::BeginFrame(const nsIntRegion& aInvalidRegion,
   nsIntRect intRect = nsIntRect(nsIntPoint(0, 0), mSize);
   
   nsIntRegion invalidRegionSafe;
-  invalidRegionSafe.And(aInvalidRegion, intRect);
+
+  if (mSize != oldSize) {
+    invalidRegionSafe = intRect;
+  } else {
+    invalidRegionSafe.And(aInvalidRegion, intRect);
+  }
+
   nsIntRect invalidRect = invalidRegionSafe.GetBounds();
   mInvalidRect = IntRect(invalidRect.x, invalidRect.y, invalidRect.width, invalidRect.height);
   mInvalidRegion = invalidRegionSafe;
