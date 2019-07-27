1ae2a3290038636f67a8639fc79bdb467d3a5060
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:26:01 +0000

    Bug 1111528 - onpeerfound shouldn't be called when receiving NDEF from another peer. r=dimi
    UltraBlame original commit: 7d696e87efb126d03e92ad5b84371b2773a345b9

--- dom/nfc/gonk/Nfc.js
+++ dom/nfc/gonk/Nfc.js
@@ -471,7 +471,11 @@ Nfc.prototype = {
         delete message.sessionId;
 
         if (SessionHelper.isP2PSession(sessionId)) {
-          gMessageManager.onPeerEvent(NFC.PEER_EVENT_FOUND, message.sessionToken);
+          if (message.records) {
+            
+          } else {
+            gMessageManager.onPeerEvent(NFC.PEER_EVENT_FOUND, message.sessionToken);
+          }
         } else {
           gMessageManager.onTagFound(message);
         }
