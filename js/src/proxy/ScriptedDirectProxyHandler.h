516e9e5451b4b02bd3c516120d49b77212cf27e6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:19:21 +0000

    Bug 1031092 - Part 4: Factor out ScriptedDirectProxyHandler. (r=bholley)
    UltraBlame original commit: 72dad42e391234244a62536d61b7705de69504ef

---
+++ js/src/proxy/ScriptedDirectProxyHandler.h
@@ -0,0 +1,78 @@
+
+
+
+
+
+
+#ifndef proxy_ScriptedDirectProxyHandler_h
+#define proxy_ScriptedDirectProxyHandler_h
+
+#include "jsproxy.h"
+
+namespace js {
+
+
+class ScriptedDirectProxyHandler : public DirectProxyHandler {
+  public:
+    MOZ_CONSTEXPR ScriptedDirectProxyHandler()
+      : DirectProxyHandler(&family)
+    { }
+
+    
+    virtual bool preventExtensions(JSContext *cx, HandleObject proxy) const MOZ_OVERRIDE;
+    virtual bool getPropertyDescriptor(JSContext *cx, HandleObject proxy, HandleId id,
+                                       MutableHandle<JSPropertyDescriptor> desc) const MOZ_OVERRIDE;
+    virtual bool getOwnPropertyDescriptor(JSContext *cx, HandleObject proxy, HandleId id,
+                                          MutableHandle<JSPropertyDescriptor> desc) const MOZ_OVERRIDE;
+    virtual bool defineProperty(JSContext *cx, HandleObject proxy, HandleId id,
+                                MutableHandle<JSPropertyDescriptor> desc) const MOZ_OVERRIDE;
+    virtual bool getOwnPropertyNames(JSContext *cx, HandleObject proxy,
+                                     AutoIdVector &props) const MOZ_OVERRIDE;
+    virtual bool delete_(JSContext *cx, HandleObject proxy, HandleId id, bool *bp) const MOZ_OVERRIDE;
+    virtual bool enumerate(JSContext *cx, HandleObject proxy, AutoIdVector &props) const MOZ_OVERRIDE;
+
+    
+    virtual bool has(JSContext *cx, HandleObject proxy, HandleId id, bool *bp) const MOZ_OVERRIDE;
+    virtual bool hasOwn(JSContext *cx, HandleObject proxy, HandleId id, bool *bp) const MOZ_OVERRIDE {
+        return BaseProxyHandler::hasOwn(cx, proxy, id, bp);
+    }
+    virtual bool get(JSContext *cx, HandleObject proxy, HandleObject receiver, HandleId id,
+                     MutableHandleValue vp) const MOZ_OVERRIDE;
+    virtual bool set(JSContext *cx, HandleObject proxy, HandleObject receiver, HandleId id,
+                     bool strict, MutableHandleValue vp) const MOZ_OVERRIDE;
+    
+    
+    virtual bool keys(JSContext *cx, HandleObject proxy, AutoIdVector &props) const MOZ_OVERRIDE {
+        return BaseProxyHandler::keys(cx, proxy, props);
+    }
+    virtual bool iterate(JSContext *cx, HandleObject proxy, unsigned flags,
+                         MutableHandleValue vp) const MOZ_OVERRIDE;
+
+    
+    virtual bool isExtensible(JSContext *cx, HandleObject proxy, bool *extensible) const MOZ_OVERRIDE;
+
+    
+    virtual bool call(JSContext *cx, HandleObject proxy, const CallArgs &args) const MOZ_OVERRIDE;
+    virtual bool construct(JSContext *cx, HandleObject proxy, const CallArgs &args) const MOZ_OVERRIDE;
+    virtual bool isScripted() const MOZ_OVERRIDE { return true; }
+
+    static const char family;
+    static const ScriptedDirectProxyHandler singleton;
+
+    
+    
+    static const int HANDLER_EXTRA = 0;
+    
+    
+    static const int REVOKE_SLOT = 0;
+};
+
+bool
+proxy(JSContext *cx, unsigned argc, jsval *vp);
+
+bool
+proxy_revocable(JSContext *cx, unsigned argc, jsval *vp);
+
+} 
+
+#endif 
