6448a0d3655690bcb9f6e38b2a7b03c568b5f4eb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:10:54 +0000

    Bug 1159267: Share Bluedroid's HFP manager between Bluetooth v1 and v2, r=btian
    The implementation of the Bluedroid HFP manager in Bluetoth v1 and v2
    differs by a number of interface changes. This patch merges both variants
    into a single one, were possible.
    
    UltraBlame original commit: 3698cc94f038209804463850c6e273ce2046df8c

--- dom/bluetooth/bluedroid/hfp/BluetoothHfpManager.h
+++ dom/bluetooth/bluedroid/hfp/BluetoothHfpManager.h
@@ -172,11 +172,7 @@ private:
   BluetoothHfpManager();
   bool Init();
 
-#ifdef MOZ_B2G_BT_API_V2
-  
-#else
   void Cleanup();
-#endif
 
   void HandleShutdown();
   void HandleVolumeChanged(nsISupports* aSubject);
