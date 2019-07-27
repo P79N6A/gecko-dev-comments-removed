cf69b50969e6be3855da6dd2de9d20db0b287c38
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:54:58 +0000

    Bug 1040017: Prevent ContentParent from sending broadcast message to Nuwa. r=khuey
    UltraBlame original commit: a9c2b0b7adacc9ad8d24460f94799dca5c65fba7

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -78,6 +78,11 @@ class ContentParent MOZ_FINAL : public PContentParent
     typedef mozilla::dom::ClonedMessageData ClonedMessageData;
 
 public:
+#ifdef MOZ_NUWA_PROCESS
+    static bool IsNuwaReady() {
+        return sNuwaReady;
+    }
+#endif
     virtual bool IsContentParent() MOZ_OVERRIDE { return true; }
     
 
@@ -700,6 +705,12 @@ private:
     
     
     ScopedClose mChildXSocketFdDup;
+#endif
+
+#ifdef MOZ_NUWA_PROCESS
+    static bool sNuwaReady;
+    struct NuwaReinitializeData;
+    nsAutoPtr<NuwaReinitializeData> mReinitializeData;
 #endif
 };
 
