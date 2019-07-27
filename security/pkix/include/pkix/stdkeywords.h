9de1d0976eb382a17d7afc772c88382d6557a66a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:42:55 +0000

    Bug 1115910: Remove now-unneeded nullptr polyfill for old versions of GCC, r=keeler
    UltraBlame original commit: 081ec95c09338f945de1b1ce307bd6d0b6019fbb

--- security/pkix/include/pkix/stdkeywords.h
+++ security/pkix/include/pkix/stdkeywords.h
@@ -28,11 +28,6 @@
 #if defined(__GNUC__) && !defined(__clang__)
 
 
-#if __GNUC__ * 100 + __GNUC_MINOR__ < 406
-#define nullptr __null
-#endif
-
-
 #if __GNUC__ * 100 + __GNUC_MINOR__ < 407
 #define final
 #define override
