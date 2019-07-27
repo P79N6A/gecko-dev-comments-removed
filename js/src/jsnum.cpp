0c653e871da072d4bfbb42f72bf2827e7f55cf57
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:01:40 +0000

    Bug 1079120 - Make ToNumber(string) support binary and octal literals. r=till
    UltraBlame original commit: 9e3582d234c13280a5fb6677319eb5e5b6bf0f45

--- js/src/jsnum.cpp
+++ js/src/jsnum.cpp
@@ -1453,23 +1453,32 @@ CharsToNumber(ThreadSafeContext *cx, const CharT *chars, size_t length, double *
     const CharT *bp = SkipSpace(chars, end);
 
     
-    if (end - bp >= 2 && bp[0] == '0' && (bp[1] == 'x' || bp[1] == 'X')) {
-        
+    if (end - bp >= 2 && bp[0] == '0') {
+        int radix = 0;
+        if (bp[1] == 'b' || bp[1] == 'B')
+            radix = 2;
+        else if (bp[1] == 'o' || bp[1] == 'O')
+            radix = 8;
+        else if (bp[1] == 'x' || bp[1] == 'X')
+            radix = 16;
+
+        if (radix != 0) {
+            
 
 
 
-
-        const CharT *endptr;
-        double d;
-        if (!GetPrefixInteger(cx, bp + 2, end, 16, &endptr, &d) ||
-            endptr == bp + 2 ||
-            SkipSpace(endptr, end) != end)
-        {
-            *result = GenericNaN();
-        } else {
-            *result = d;
+            const CharT *endptr;
+            double d;
+            if (!GetPrefixInteger(cx, bp + 2, end, radix, &endptr, &d) ||
+                endptr == bp + 2 ||
+                SkipSpace(endptr, end) != end)
+            {
+                *result = GenericNaN();
+            } else {
+                *result = d;
+            }
+            return true;
         }
-        return true;
     }
 
     
