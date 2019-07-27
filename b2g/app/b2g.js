cb8e055068331b9f94b53289d3cdda3c40b936a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:43:28 +0000

    Bug 1151489. Enable software vsync on all b2g versions. r=kats
    UltraBlame original commit: b987bc66a933b3fb5ccc2d35a4491cb432b653ef

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1127,22 +1127,7 @@ pref("dom.mozSettings.allowForceReadOnly", false);
 pref("dom.requestSync.enabled", true);
 
 
-
-
-#if ANDROID_VERSION == 19 || ANDROID_VERSION == 21 || ANDROID_VERSION == 15
 pref("gfx.vsync.hw-vsync.enabled", true);
 pref("gfx.vsync.compositor", true);
 pref("gfx.touch.resample", true);
-#else
-pref("gfx.vsync.hw-vsync.enabled", false);
-pref("gfx.vsync.compositor", false);
-pref("gfx.touch.resample", false);
-#endif
-
-
-
-#if ANDROID_VERSION == 19 || ANDROID_VERSION == 15
 pref("gfx.vsync.refreshdriver", true);
-#else
-pref("gfx.vsync.refreshdriver", false);
-#endif
