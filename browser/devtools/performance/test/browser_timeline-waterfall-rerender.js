dfeb0403cb976bd9ce5de7753587569dc6ec9cfb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:51:33 +0000

    Bug 1169352 - Reenable assertion, r=me
    UltraBlame original commit: fb65503fe7b61418a474ca50358d426bd8b4d3ce

--- browser/devtools/performance/test/browser_timeline-waterfall-rerender.js
+++ browser/devtools/performance/test/browser_timeline-waterfall-rerender.js
@@ -65,10 +65,8 @@ function* spawnTest() {
   is(afterResizeBarsCount, beforeResizeBarsCount,
     "The same subset of the total markers remained visible.");
 
-  
-  
-  
-  
+  is(Array.indexOf($$(".waterfall-tree-item"), $(".waterfall-tree-item:focus")), 2,
+    "The correct item is still focused in the tree.");
   ok(!$("#waterfall-details").hidden,
     "The waterfall sidebar is still visible.");
 
