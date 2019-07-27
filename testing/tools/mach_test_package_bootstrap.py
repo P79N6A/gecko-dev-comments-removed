dc92f500a62f708dd3aba71a1e08f48c71a4ebaf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:24:13 +0000

    Bug 1058923 - Package mach in tests.zip; create bootstrap script for test package, r=gps
    UltraBlame original commit: fec6ff864e3c636b9c6c72b9f3b0026338a66d8e

---
+++ testing/tools/mach_test_package_bootstrap.py
@@ -0,0 +1,75 @@
+
+
+
+
+from __future__ import print_function, unicode_literals
+
+import os
+import platform
+import sys
+import time
+
+
+SEARCH_PATHS = [
+    'tools/mach',
+]
+
+
+MACH_MODULES = [
+    'tools/mach/mach/commands/commandinfo.py',
+]
+
+
+CATEGORIES = {
+    'testing': {
+        'short': 'Testing',
+        'long': 'Run tests.',
+        'priority': 30,
+    },
+    'devenv': {
+        'short': 'Development Environment',
+        'long': 'Set up and configure your development environment.',
+        'priority': 20,
+    },
+    'misc': {
+        'short': 'Potpourri',
+        'long': 'Potent potables and assorted snacks.',
+        'priority': 10,
+    },
+    'disabled': {
+        'short': 'Disabled',
+        'long': 'The disabled commands are hidden by default. Use -v to display them. These commands are unavailable for your current context, run "mach <command>" to see why.',
+        'priority': 0,
+    }
+}
+
+
+def bootstrap(test_package_root):
+    
+    
+    
+    if sys.version_info[0] != 2 or sys.version_info[1] < 7:
+        print('Python 2.7 or above (but not Python 3) is required to run mach.')
+        print('You are running Python', platform.python_version())
+        sys.exit(1)
+
+    try:
+        import mach.main
+    except ImportError:
+        sys.path[0:0] = [os.path.join(test_package_root, path) for path in SEARCH_PATHS]
+        import mach.main
+
+    def populate_context(context, key=None):
+        return context
+
+    mach = mach.main.Mach(os.getcwd())
+    mach.populate_context_handler = populate_context
+
+    for category, meta in CATEGORIES.items():
+        mach.define_category(category, meta['short'], meta['long'],
+            meta['priority'])
+
+    for path in MACH_MODULES:
+        mach.load_commands_from_file(os.path.join(test_package_root, path))
+
+    return mach
