df0c1851b3d65e22571beac44c4dc695aa8a139b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:58:30 +0000

    Bug 1126739 - Add locking to SurfaceCache entry points that bypass the public API. r=tn
    UltraBlame original commit: f274f6a5616fd050cf593e2bb6d7adc3bc302d72

--- image/test/browser/browser_bug666317.js
+++ image/test/browser/browser_bug666317.js
@@ -27,18 +27,6 @@ function currentRequest() {
   return img.getRequest(Ci.nsIImageLoadingContent.CURRENT_REQUEST);
 }
 
-function attachDiscardObserver(result) {
-  
-  let observer = new ImageDiscardObserver(result);
-  let scriptedObserver = Cc["@mozilla.org/image/tools;1"]
-                           .getService(Ci.imgITools)
-                           .createScriptedObserver(observer);
-
-  
-  let request = currentRequest();
-  return request.clone(scriptedObserver);
-}
-
 function isImgDecoded() {
   let request = currentRequest();
   return request.imageStatus & Ci.imgIRequest.STATUS_FRAME_COMPLETE ? true : false;
@@ -71,7 +59,16 @@ function test() {
 function step2() {
   
   var result = { wasDiscarded: false };
-  var clonedRequest = attachDiscardObserver(result);
+
+  
+  var observer = new ImageDiscardObserver(result);
+  var scriptedObserver = Cc["@mozilla.org/image/tools;1"]
+                           .getService(Ci.imgITools)
+                           .createScriptedObserver(observer);
+
+  
+  var request = currentRequest();
+  var clonedRequest = request.clone(scriptedObserver);
 
   
   forceDecodeImg();
@@ -83,6 +80,19 @@ function step2() {
   var os = Cc["@mozilla.org/observer-service;1"]
              .getService(Ci.nsIObserverService);
   os.notifyObservers(null, 'memory-pressure', 'heap-minimize');
+
+  
+  
+  window.addEventListener('message', function (event) {
+    if (event.data == 'step3') {
+      step3(result, scriptedObserver, clonedRequest);
+    }
+  }, false);
+
+  window.postMessage('step3', '*');
+}
+
+function step3(result, scriptedObserver, clonedRequest) {
   ok(result.wasDiscarded, 'Image should be discarded.');
 
   
