328441667729dd0f65df0fce5997bf80d31061af
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:28:41 +0000

    Bug 1114383 - Unlock the demuxer monitor before calling out via MP4Stream. r=k17e
    UltraBlame original commit: 73a8a6927bdecc164059b5e7b428a7e16defabfc

--- dom/media/fmp4/MP4Stream.cpp
+++ dom/media/fmp4/MP4Stream.cpp
@@ -9,7 +9,9 @@
 
 namespace mozilla {
 
-MP4Stream::MP4Stream(MediaResource* aResource) : mResource(aResource)
+MP4Stream::MP4Stream(MediaResource* aResource, Monitor* aDemuxerMonitor)
+  : mResource(aResource)
+  , mDemuxerMonitor(aDemuxerMonitor)
 {
   MOZ_COUNT_CTOR(MP4Stream);
   MOZ_ASSERT(aResource);
@@ -24,6 +26,12 @@ bool
 MP4Stream::ReadAt(int64_t aOffset, void* aBuffer, size_t aCount,
                   size_t* aBytesRead)
 {
+  
+  
+  
+  mDemuxerMonitor->AssertCurrentThreadOwns();
+  MonitorAutoUnlock unlock(*mDemuxerMonitor);
+
   uint32_t sum = 0;
   uint32_t bytesRead = 0;
   do {
@@ -44,6 +52,12 @@ bool
 MP4Stream::CachedReadAt(int64_t aOffset, void* aBuffer, size_t aCount,
                         size_t* aBytesRead)
 {
+  
+  
+  
+  mDemuxerMonitor->AssertCurrentThreadOwns();
+  MonitorAutoUnlock unlock(*mDemuxerMonitor);
+
   nsresult rv = mResource->ReadFromCache(reinterpret_cast<char*>(aBuffer),
                                          aOffset, aCount);
   if (NS_FAILED(rv)) {
