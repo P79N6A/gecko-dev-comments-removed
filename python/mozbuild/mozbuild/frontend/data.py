48892c92d272d109c97a0189ba7c00a0e7ae2697
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:09:39 +0000

    Bug 1122812 - part 1 - move unified file grouping to UnifiedSources's constructor; r=mshal
    UnifiedSources, not the recursivemake backend, should be responsible for
    figuring out what unified files to generate, and what those unified
    files should contain.
    
    UltraBlame original commit: 308e00e6f6e86fa36f1821fa64b82fbb766513ec

--- python/mozbuild/mozbuild/frontend/data.py
+++ python/mozbuild/mozbuild/frontend/data.py
@@ -27,6 +27,10 @@ from mozbuild.util import (
 import mozpack.path as mozpath
 from .context import FinalTargetValue
 
+from ..util import (
+    group_unified_files,
+)
+
 
 class TreeMetadata(object):
     """Base class for all data being captured."""
@@ -778,13 +782,32 @@ class UnifiedSources(BaseSources):
     """Represents files to be compiled in a unified fashion during the build."""
 
     __slots__ = (
-        'files_per_unified_file',
+        'have_unified_mapping',
+        'unified_source_mapping'
     )
 
     def __init__(self, context, files, canonical_suffix, files_per_unified_file=16):
         BaseSources.__init__(self, context, files, canonical_suffix)
 
-        self.files_per_unified_file = files_per_unified_file
+        self.have_unified_mapping = files_per_unified_file > 1
+
+        if self.have_unified_mapping:
+            
+            source_files = list(sorted(self.files))
+
+            
+            
+            unified_prefix = context.relsrcdir
+            if len(unified_prefix) > 20:
+                unified_prefix = unified_prefix[-20:].split('/', 1)[-1]
+            unified_prefix = unified_prefix.replace('/', '_')
+
+            suffix = self.canonical_suffix[1:]
+            unified_prefix='Unified_%s_%s' % (suffix, unified_prefix)
+            self.unified_source_mapping = list(group_unified_files(source_files,
+                                                                   unified_prefix=unified_prefix,
+                                                                   unified_suffix=suffix,
+                                                                   files_per_unified_file=files_per_unified_file))
 
 
 class InstallationTarget(ContextDerived):
