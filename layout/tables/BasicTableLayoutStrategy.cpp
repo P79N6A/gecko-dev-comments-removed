cf3a0350bbd2ff0eadd542f29262b68d278b731c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:34:47 +0000

    Backed out changeset dd96df1da2b5 (bug 1166728) for making test_bug320799.html permafail on Mulet. CLOSED TREE
    UltraBlame original commit: 703ace2e04148859467b4d2276d0ffbdbe86e7e0

--- layout/tables/BasicTableLayoutStrategy.cpp
+++ layout/tables/BasicTableLayoutStrategy.cpp
@@ -113,13 +113,25 @@ GetWidthInfo(nsRenderingContext *aRenderingContext,
         
         
         
-        if (isQuirks || stylePos->mBoxSizing == NS_STYLE_BOX_SIZING_CONTENT) {
+        if (isQuirks) {
             boxSizingToBorderEdge = offsets.hPadding + offsets.hBorder;
         }
         else {
-            
-            minCoord += offsets.hPadding + offsets.hBorder;
-            prefCoord += offsets.hPadding + offsets.hBorder;
+            switch (stylePos->mBoxSizing) {
+                case NS_STYLE_BOX_SIZING_CONTENT:
+                    boxSizingToBorderEdge = offsets.hPadding + offsets.hBorder;
+                    break;
+                case NS_STYLE_BOX_SIZING_PADDING:
+                    minCoord += offsets.hPadding;
+                    prefCoord += offsets.hPadding;
+                    boxSizingToBorderEdge = offsets.hBorder;
+                    break;
+                default:
+                    
+                    minCoord += offsets.hPadding + offsets.hBorder;
+                    prefCoord += offsets.hPadding + offsets.hBorder;
+                    break;
+            }
         }
     } else {
         minCoord = 0;
