2ac82ba99846fdcd48714b5dcd04a803557e6318
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:48:56 +0000

    bug 1045739 - (1/2) mozilla::pkix: stop checking revocation for expired certificates r=keeler
    UltraBlame original commit: f7d98f2e35fd713eb2b2fc015ddd9acac13bd8b2

--- security/pkix/lib/pkixbuild.cpp
+++ security/pkix/lib/pkixbuild.cpp
@@ -50,7 +50,7 @@ public:
                    Time time, KeyPurposeId requiredEKUIfPresent,
                    const CertPolicyId& requiredPolicy,
                     const Input* stapledOCSPResponse,
-                   unsigned int subCACount)
+                   unsigned int subCACount, Result deferredSubjectError)
     : trustDomain(trustDomain)
     , subject(subject)
     , time(time)
@@ -58,6 +58,7 @@ public:
     , requiredPolicy(requiredPolicy)
     , stapledOCSPResponse(stapledOCSPResponse)
     , subCACount(subCACount)
+    , deferredSubjectError(deferredSubjectError)
     , result(Result::FATAL_ERROR_LIBRARY_FAILURE)
     , resultWasSet(false)
   {
@@ -77,6 +78,7 @@ private:
   const CertPolicyId& requiredPolicy;
    Input const* const stapledOCSPResponse;
   const unsigned int subCACount;
+  const Result deferredSubjectError;
 
   Result RecordResult(Result currentResult,  bool& keepGoing);
   Result result;
@@ -185,13 +187,19 @@ PathBuildingStep::Check(Input potentialIssuerDER,
     return RecordResult(rv, keepGoing);
   }
 
-  CertID certID(subject.GetIssuer(), potentialIssuer.GetSubjectPublicKeyInfo(),
-                subject.GetSerialNumber());
-  rv = trustDomain.CheckRevocation(subject.endEntityOrCA, certID, time,
-                                   stapledOCSPResponse,
-                                   subject.GetAuthorityInfoAccess());
-  if (rv != Success) {
-    return RecordResult(rv, keepGoing);
+  
+  
+  
+  
+  if (deferredSubjectError != Result::ERROR_EXPIRED_CERTIFICATE) {
+    CertID certID(subject.GetIssuer(), potentialIssuer.GetSubjectPublicKeyInfo(),
+                  subject.GetSerialNumber());
+    rv = trustDomain.CheckRevocation(subject.endEntityOrCA, certID, time,
+                                     stapledOCSPResponse,
+                                     subject.GetAuthorityInfoAccess());
+    if (rv != Success) {
+      return RecordResult(rv, keepGoing);
+    }
   }
 
   return RecordResult(Success, keepGoing);
@@ -268,7 +276,8 @@ BuildForward(TrustDomain& trustDomain,
 
   PathBuildingStep pathBuilder(trustDomain, subject, time,
                                requiredEKUIfPresent, requiredPolicy,
-                               stapledOCSPResponse, subCACount);
+                               stapledOCSPResponse, subCACount,
+                               deferredEndEntityError);
 
   
   rv = trustDomain.FindIssuer(subject.GetIssuer(), pathBuilder, time);
