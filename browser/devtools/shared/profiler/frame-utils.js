b479606c59b63c589fc6e9fa65268e9b03c48225
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:16:20 +0000

    Bug 1160691 - Optimize FrameUtils.isContent and FrameUtils.parseLocation. (r=jsantell)
    UltraBlame original commit: 0049538641a2aac08175474834c2978b18c8a5f6

--- browser/devtools/shared/profiler/frame-utils.js
+++ browser/devtools/shared/profiler/frame-utils.js
@@ -10,36 +10,121 @@ loader.lazyRequireGetter(this, "CATEGORY_OTHER",
   "devtools/shared/profiler/global", true);
 
 
+const CHAR_CODE_A = "a".charCodeAt(0);
+const CHAR_CODE_C = "c".charCodeAt(0);
+const CHAR_CODE_E = "e".charCodeAt(0);
+const CHAR_CODE_F = "f".charCodeAt(0);
+const CHAR_CODE_H = "h".charCodeAt(0);
+const CHAR_CODE_I = "i".charCodeAt(0);
+const CHAR_CODE_J = "j".charCodeAt(0);
+const CHAR_CODE_L = "l".charCodeAt(0);
+const CHAR_CODE_M = "m".charCodeAt(0);
+const CHAR_CODE_O = "o".charCodeAt(0);
+const CHAR_CODE_P = "p".charCodeAt(0);
+const CHAR_CODE_R = "r".charCodeAt(0);
+const CHAR_CODE_S = "s".charCodeAt(0);
+const CHAR_CODE_T = "t".charCodeAt(0);
+const CHAR_CODE_U = "u".charCodeAt(0);
+const CHAR_CODE_0 = "0".charCodeAt(0);
+const CHAR_CODE_9 = "9".charCodeAt(0);
+
+const CHAR_CODE_LPAREN = "(".charCodeAt(0);
+const CHAR_CODE_COLON = ":".charCodeAt(0);
+const CHAR_CODE_SLASH = "/".charCodeAt(0);
+
+
 const gNSURLStore = new Map();
-const CHROME_SCHEMES = ["chrome://", "resource://", "jar:file://"];
-const CONTENT_SCHEMES = ["http://", "https://", "file://", "app://"];
 
 
 
 
 
-exports.parseLocation = function parseLocation (frame) {
+exports.parseLocation = function parseLocation(location, fallbackLine, fallbackColumn) {
   
-  let lineAndColumn = frame.location.match(/((:\d+)*)\)?$/)[1];
-  let [, line, column] = lineAndColumn.split(":");
-  line = line || frame.line;
-  column = column || frame.column;
 
-  let firstParenIndex = frame.location.indexOf("(");
-  let lineAndColumnIndex = frame.location.indexOf(lineAndColumn);
-  let resource = frame.location.substring(firstParenIndex + 1, lineAndColumnIndex);
+  let line, column, url;
 
-  let url = resource.split(" -> ").pop();
-  let uri = nsIURL(url);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  let firstParenIndex = -1;
+  let lineAndColumnIndex = -1;
+
+  
+  
+  for (let i = 0; i < location.length; i++) {
+    let c = location.charCodeAt(i);
+
+    
+    if (c === CHAR_CODE_LPAREN) {
+      if (firstParenIndex < 0) {
+        firstParenIndex = i;
+      }
+      continue;
+    }
+
+    
+    
+    if (c === CHAR_CODE_COLON) {
+      if (isNumeric(location.charCodeAt(i + 1))) {
+        
+        if (lineAndColumnIndex < 0) {
+          lineAndColumnIndex = i;
+        }
+
+        let start = ++i;
+        let length = 1;
+        while (isNumeric(location.charCodeAt(++i))) {
+          length++;
+        }
+
+        if (!line) {
+          line = location.substr(start, length);
+        } else if (!column) {
+          column = location.substr(start, length);
+        }
+
+        break;
+      }
+    }
+  }
+
+  let uri;
+  if (lineAndColumnIndex > 0) {
+    let resource = location.substring(firstParenIndex + 1, lineAndColumnIndex);
+    url = resource.split(" -> ").pop();
+    if (url) {
+      uri = nsIURL(url);
+    }
+  }
+
   let functionName, fileName, hostName;
 
   
   if (uri) {
-    functionName = frame.location.substring(0, firstParenIndex - 1);
+    functionName = location.substring(0, firstParenIndex - 1);
     fileName = (uri.fileName + (uri.ref ? "#" + uri.ref : "")) || "/";
     hostName = getHost(url, uri.host);
   } else {
-    functionName = frame.location;
+    functionName = location;
     url = null;
   }
 
@@ -51,16 +136,6 @@ exports.parseLocation = function parseLocation (frame) {
     line: line,
     column: column
   };
-},
-
-
-
-
-
-
-
-exports.isRoot = function isRoot({ location }) {
-  return location === "(root)";
 };
 
 
@@ -71,17 +146,28 @@ exports.isRoot = function isRoot({ location }) {
 
 
 
-exports.isContent = function isContent (frame) {
-  if (exports.isRoot(frame)) {
-    return true;
+
+
+function isContent({ location, category }) {
+  
+  if (category) {
+    return false;
   }
 
   
-  const { category, location } = frame;
-  return !!(!category &&
-    !CHROME_SCHEMES.find(e => location.includes(e)) &&
-    CONTENT_SCHEMES.find(e => location.includes(e)));
+  
+  
+  
+  for (let i = 0; i < location.length; i++) {
+    if (location.charCodeAt(i) === CHAR_CODE_LPAREN) {
+      return isContentScheme(location, i + 1);
+    }
+  }
+
+  
+  return isContentScheme(location, 0);
 }
+exports.isContent = isContent;
 
 
 
@@ -149,15 +235,106 @@ function nsIURL(url) {
   }
   gNSURLStore.set(url, uri);
   return uri;
-}
+};
 
 
 
 
 
 function getHost (url, hostName) {
-  if (CHROME_SCHEMES.find(e => url.indexOf(e) === 0)) {
-    return null;
+  return isChromeScheme(url) ? null : hostName;
+}
+
+
+
+
+
+
+
+function isColonSlashSlash(location, i) {
+  return location.charCodeAt(++i) === CHAR_CODE_COLON &&
+         location.charCodeAt(++i) === CHAR_CODE_SLASH &&
+         location.charCodeAt(++i) === CHAR_CODE_SLASH;
+}
+
+function isContentScheme(location, i) {
+  let firstChar = location.charCodeAt(i);
+
+  switch (firstChar) {
+  case CHAR_CODE_H: 
+    if (location.charCodeAt(++i) === CHAR_CODE_T &&
+        location.charCodeAt(++i) === CHAR_CODE_T &&
+        location.charCodeAt(++i) === CHAR_CODE_P) {
+      if (location.charCodeAt(i) === CHAR_CODE_S) {
+        ++i;
+      }
+      return isColonSlashSlash(location, i);
+    }
+    return false;
+
+  case CHAR_CODE_F: 
+    if (location.charCodeAt(++i) === CHAR_CODE_I &&
+        location.charCodeAt(++i) === CHAR_CODE_L &&
+        location.charCodeAt(++i) === CHAR_CODE_E) {
+      return isColonSlashSlash(location, i);
+    }
+    return false;
+
+  case CHAR_CODE_A: 
+    if (location.charCodeAt(++i) == CHAR_CODE_P &&
+        location.charCodeAt(++i) == CHAR_CODE_P) {
+      return isColonSlashSlash(location, i);
+    }
+    return false;
+
+  default:
+    return false;
   }
-  return hostName;
+}
+
+function isChromeScheme(location, i) {
+  let firstChar = location.charCodeAt(i);
+
+  switch (firstChar) {
+  case CHAR_CODE_C: 
+    if (location.charCodeAt(++i) === CHAR_CODE_H &&
+        location.charCodeAt(++i) === CHAR_CODE_R &&
+        location.charCodeAt(++i) === CHAR_CODE_O &&
+        location.charCodeAt(++i) === CHAR_CODE_M &&
+        location.charCodeAt(++i) === CHAR_CODE_E) {
+      return isColonSlashSlash(location, i);
+    }
+    return false;
+
+  case CHAR_CODE_R: 
+    if (location.charCodeAt(++i) === CHAR_CODE_E &&
+        location.charCodeAt(++i) === CHAR_CODE_S &&
+        location.charCodeAt(++i) === CHAR_CODE_O &&
+        location.charCodeAt(++i) === CHAR_CODE_U &&
+        location.charCodeAt(++i) === CHAR_CODE_R &&
+        location.charCodeAt(++i) === CHAR_CODE_C &&
+        location.charCodeAt(++i) === CHAR_CODE_E) {
+      return isColonSlashSlash(location, i);
+    }
+    return false;
+
+  case CHAR_CODE_J: 
+    if (location.charCodeAt(++i) === CHAR_CODE_A &&
+        location.charCodeAt(++i) === CHAR_CODE_R &&
+        location.charCodeAt(++i) === CHAR_CODE_COLON &&
+        location.charCodeAt(++i) === CHAR_CODE_F &&
+        location.charCodeAt(++i) === CHAR_CODE_I &&
+        location.charCodeAt(++i) === CHAR_CODE_L &&
+        location.charCodeAt(++i) === CHAR_CODE_E) {
+      return isColonSlashSlash(location, i);
+    }
+    return false;
+
+  default:
+    return false;
+  }
+}
+
+function isNumeric(c) {
+  return c >= CHAR_CODE_0 && c <= CHAR_CODE_9;
 }
