ad6a9db64bfa12bf385888e3994d7a44b984a3f2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:47:29 +0000

    Bug 1027763, part 4 - Add a new DrawTarget::GetType() method and three generic type enum values for it to return. r=Bas, r=jrmuizel
    UltraBlame original commit: 14fe3ddc2e9cbbdc7eda2a89a22e750fe945426b

--- gfx/2d/2D.h
+++ gfx/2d/2D.h
@@ -620,6 +620,8 @@ public:
   DrawTarget() : mTransformDirty(false), mPermitSubpixelAA(false) {}
   virtual ~DrawTarget() {}
 
+  virtual DrawTargetType GetType() const = 0;
+
   virtual BackendType GetBackendType() const = 0;
   
 
