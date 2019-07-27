84e8a2fcc6606e0bf7b146e6bf87dd2bc4b35794
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:21:48 +0000

    Bug 1143740 - Re-enable browser_491168.js r=smacleod
    UltraBlame original commit: f9617ec35258ca8fcb57f8c6f1bb3872ef0d1c63

--- browser/components/sessionstore/test/browser_491168.js
+++ browser/components/sessionstore/test/browser_491168.js
@@ -1,41 +1,42 @@
+"use strict";
 
+const REFERRER1 = "http://example.org/?" + Date.now();
+const REFERRER2 = "http://example.org/?" + Math.random();
 
-
-
-function test() {
+add_task(function* () {
   
-
-  waitForExplicitFinish();
-
-  const REFERRER1 = "http://example.org/?" + Date.now();
-  const REFERRER2 = "http://example.org/?" + Math.random();
-
-  let tab = gBrowser.addTab();
-  gBrowser.selectedTab = tab;
-
+  let tab = gBrowser.selectedTab = gBrowser.addTab("about:blank");
   let browser = tab.linkedBrowser;
-  promiseBrowserLoaded(browser).then(() => {
-    let tabState = JSON.parse(ss.getTabState(tab));
-    is(tabState.entries[0].referrer,  REFERRER1,
-       "Referrer retrieved via getTabState matches referrer set via loadURI.");
+  yield promiseBrowserLoaded(browser);
 
-    tabState.entries[0].referrer = REFERRER2;
-
-    promiseTabState(tab, tabState).then(() => {
-      is(window.content.document.referrer, REFERRER2, "document.referrer matches referrer set via setTabState.");
-
-      gBrowser.removeTab(tab);
-
-      let newTab = ss.undoCloseTab(window, 0);
-      promiseTabRestored(newTab).then(() => {
-        is(window.content.document.referrer, REFERRER2, "document.referrer is still correct after closing and reopening the tab.");
-        gBrowser.removeTab(newTab);
-
-        finish();
-      });
-    });
-  });
-
+  
   let referrerURI = Services.io.newURI(REFERRER1, null, null);
   browser.loadURI("http://example.org", referrerURI, null);
+  yield promiseBrowserLoaded(browser);
+
+  TabState.flush(browser);
+  let tabState = JSON.parse(ss.getTabState(tab));
+  is(tabState.entries[0].referrer,  REFERRER1,
+     "Referrer retrieved via getTabState matches referrer set via loadURI.");
+
+  tabState.entries[0].referrer = REFERRER2;
+  yield promiseTabState(tab, tabState);
+
+  is((yield promiseDocumentReferrer()), REFERRER2,
+     "document.referrer matches referrer set via setTabState.");
+  gBrowser.removeCurrentTab();
+
+  
+  tab = ss.undoCloseTab(window, 0);
+  yield promiseTabRestored(tab);
+
+  is((yield promiseDocumentReferrer()), REFERRER2,
+     "document.referrer is still correct after closing and reopening the tab.");
+  gBrowser.removeCurrentTab();
+});
+
+function promiseDocumentReferrer() {
+  return ContentTask.spawn(gBrowser.selectedBrowser, null, function* () {
+    return content.document.referrer;
+  });
 }
