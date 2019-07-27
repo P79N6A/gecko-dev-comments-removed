4a8dae650baaef37cb1b5191ee41658c1aa16cb7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:19:49 +0000

    Bug 1143537 - Part 3: Serialize unicode-range without leading zeroes. r=jdaggett
    UltraBlame original commit: 2fcd63998e56c9256c07f1d582445893d19ad6dd

--- layout/style/nsStyleUtil.cpp
+++ layout/style/nsStyleUtil.cpp
@@ -463,11 +463,10 @@ nsStyleUtil::ComputeFunctionalAlternates(const nsCSSValueList* aList,
   }
 }
 
-
 static void
 AppendSerializedUnicodePoint(uint32_t aCode, nsACString& aBuf)
 {
-  aBuf.Append(nsPrintfCString("%04X", aCode));
+  aBuf.Append(nsPrintfCString("%0X", aCode));
 }
 
 
