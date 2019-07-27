409d525c39298ef131a7583ee5d430ef5aba126f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:51:18 +0000

    Backed out changeset 5555ee7490fb (bug 1113631) for asan mochitest-4 bustage on a CLOSED TREE
    UltraBlame original commit: e9d37cd95335336532e6e5d1b32207e31ccc7a51

--- dom/workers/ServiceWorkerManager.cpp
+++ dom/workers/ServiceWorkerManager.cpp
@@ -504,7 +504,8 @@ public:
   {
     MOZ_ASSERT(mCallback);
     mCallback->UpdateFailed(aError);
-    FailCommon(NS_ERROR_DOM_JS_EXCEPTION);
+    mCallback = nullptr;
+    Done(NS_ERROR_DOM_JS_EXCEPTION);
   }
 
   
@@ -633,39 +634,16 @@ private:
     mCallback = nullptr;
   }
 
-  void
-  FailCommon(nsresult aRv)
-  {
-    mCallback = nullptr;
-    MaybeRemoveRegistration();
-    
-    mRegistration = nullptr;
-    Done(aRv);
-  }
-
   
   
   
   void
-  Fail(nsresult aRv)
+  Fail(nsresult rv)
   {
     MOZ_ASSERT(mCallback);
-    mCallback->UpdateFailed(aRv);
-    FailCommon(aRv);
-  }
-
-  void
-  MaybeRemoveRegistration()
-  {
-    MOZ_ASSERT(mRegistration);
-    nsRefPtr<ServiceWorkerInfo> newest = mRegistration->Newest();
-    if (!newest) {
-      nsRefPtr<ServiceWorkerManager> swm = ServiceWorkerManager::GetInstance();
-      nsRefPtr<ServiceWorkerManager::ServiceWorkerDomainInfo> domainInfo =
-        swm->GetDomainInfo(mRegistration->mScope);
-      MOZ_ASSERT(domainInfo);
-      domainInfo->RemoveRegistration(mRegistration);
-    }
+    mCallback->UpdateFailed(rv);
+    mCallback = nullptr;
+    Done(rv);
   }
 
   void
@@ -688,7 +666,6 @@ private:
       mRegistration->mInstallingWorker = nullptr;
       swm->InvalidateServiceWorkerRegistrationWorker(mRegistration,
                                                      WhichServiceWorker::INSTALLING_WORKER);
-      MaybeRemoveRegistration();
       return Done(NS_ERROR_DOM_ABORT_ERR);
     }
 
