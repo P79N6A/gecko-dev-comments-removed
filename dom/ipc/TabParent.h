700844b1c952346a043d67c53e2a00df2e297156
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:44:35 +0000

    Bug 567058 - Stop using intr messages for window.open (r=bent)
    UltraBlame original commit: 8798cd000e6b47354d634e080f6b6026630c451b

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -134,16 +134,17 @@ public:
                                             const nsString& aName,
                                             const nsString& aFeatures,
                                             bool* aOutWindowOpened) MOZ_OVERRIDE;
-    virtual bool AnswerCreateWindow(const uint32_t& aChromeFlags,
-                                    const bool& aCalledFromJS,
-                                    const bool& aPositionSpecified,
-                                    const bool& aSizeSpecified,
-                                    const nsString& aURI,
-                                    const nsString& aName,
-                                    const nsString& aFeatures,
-                                    const nsString& aBaseURI,
-                                    bool* aWindowIsNew,
-                                    PBrowserParent** aRetVal) MOZ_OVERRIDE;
+    virtual bool RecvCreateWindow(PBrowserParent* aOpener,
+                                  const uint32_t& aChromeFlags,
+                                  const bool& aCalledFromJS,
+                                  const bool& aPositionSpecified,
+                                  const bool& aSizeSpecified,
+                                  const nsString& aURI,
+                                  const nsString& aName,
+                                  const nsString& aFeatures,
+                                  const nsString& aBaseURI,
+                                  bool* aWindowIsNew,
+                                  InfallibleTArray<FrameScriptInfo>* aFrameScripts) MOZ_OVERRIDE;
     virtual bool RecvSyncMessage(const nsString& aMessage,
                                  const ClonedMessageData& aData,
                                  const InfallibleTArray<CpowEntry>& aCpows,
@@ -352,6 +353,11 @@ public:
     void SetInitedByParent() { mInitedByParent = true; }
     bool IsInitedByParent() const { return mInitedByParent; }
 
+    static TabParent* GetNextTabParent();
+
+    bool SendLoadRemoteScript(const nsString& aURL,
+                              const bool& aRunInGlobalScope);
+
 protected:
     bool ReceiveMessage(const nsString& aMessage,
                         bool aSync,
@@ -467,6 +473,35 @@ private:
     nsCOMPtr<nsILoadContext> mLoadContext;
 
     TabId mTabId;
+
+    
+    struct AutoUseNewTab;
+
+    
+    
+    
+    
+    
+    static TabParent* sNextTabParent;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    bool mSkipLoad;
+
+    
+    
+    
+    
+    
+    
+    
+    nsTArray<FrameScriptInfo> mDelayedFrameScripts;
 
 private:
     
