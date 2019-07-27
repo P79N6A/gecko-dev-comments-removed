40795b4b66689f32501c3974ad6cb18b03807004
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:09:50 +0000

    Bug 1032573 part 2 - Add a timeline member to ElementAnimations; r=dbaron
    When we expose ElementAnimation objects to script they need to have a parent
    object so they can be associated with a Window.
    
    This patch adds a pointer from an ElementAnimation to its AnimationTimeline.
    
    UltraBlame original commit: 5a606567031850658300f470ffa1a9a26561e8ca

--- layout/style/AnimationCommon.cpp
+++ layout/style/AnimationCommon.cpp
@@ -12,6 +12,7 @@
 #include "nsRuleData.h"
 #include "nsCSSPropertySet.h"
 #include "nsCSSValue.h"
+#include "nsCycleCollectionParticipant.h"
 #include "nsStyleContext.h"
 #include "nsIFrame.h"
 #include "nsLayoutUtils.h"
@@ -405,6 +406,10 @@ ComputedTimingFunction::GetValue(double aPortion) const
 
 const double ComputedTiming::kNullTimeFraction =
   mozilla::PositiveInfinity<double>();
+
+NS_IMPL_CYCLE_COLLECTION(ElementAnimation, mTimeline)
+NS_IMPL_CYCLE_COLLECTION_ROOT_NATIVE(ElementAnimation, AddRef)
+NS_IMPL_CYCLE_COLLECTION_UNROOT_NATIVE(ElementAnimation, Release)
 
 bool
 ElementAnimation::IsRunningAt(TimeStamp aTime) const
