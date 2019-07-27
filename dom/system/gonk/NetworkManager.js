ec64b994feaf535536934cceb9ce89de5520cecb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:59:43 +0000

    Bug 1120843 - Part 1: clear http proxy when active network is disconnected. r=echen
    UltraBlame original commit: 17d151a8dee1ae5fbbfcd6962298f00f50107ca9

--- dom/system/gonk/NetworkManager.js
+++ dom/system/gonk/NetworkManager.js
@@ -398,6 +398,10 @@ NetworkManager.prototype = {
         } else if (network.type == Ci.nsINetworkInterface.NETWORK_TYPE_MOBILE) {
           gNetworkService.removeDefaultRoute(network);
         }
+        
+        if (this.active && network.type == this.active.type) {
+          gNetworkService.clearNetworkProxy();
+        }
 
         
         CaptivePortalDetectionHelper
