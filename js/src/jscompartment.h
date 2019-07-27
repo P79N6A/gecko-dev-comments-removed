a6475957e20c59f11140ee014b7bb16a34c4f05f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:15:05 +0000

    Bug 1100173, bug 1102510 - Add baseline caches for typed object scalar element accesses and scalar/reference property accesses, r=jandem.
    UltraBlame original commit: 9439c9dbd36efa79d5a3215f7529111aa0c5c0ce

--- js/src/jscompartment.h
+++ js/src/jscompartment.h
@@ -241,6 +241,9 @@ struct JSCompartment
 
     bool                         globalWriteBarriered;
 
+    
+    int32_t                      neuteredTypedObjects;
+
   public:
     void addSizeOfIncludingThis(mozilla::MallocSizeOf mallocSizeOf,
                                 size_t *tiAllocationSiteTables,
