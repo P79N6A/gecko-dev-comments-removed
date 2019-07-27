e6e590bc46965a87c343761b9712666706549f96
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:08:27 +0000

    Bug 1129871 - Favor passing tiles by ref rather than copy part 1. r=BenWa
    UltraBlame original commit: 6952d9fe4f36b90405ed21d4e1d548f913119ed8

--- gfx/layers/TiledLayerBuffer.h
+++ gfx/layers/TiledLayerBuffer.h
@@ -544,8 +544,8 @@ TiledLayerBuffer<Derived, Tile>::Update(const nsIntRegion& newValidRegion,
       
       
       nsIntPoint tileOrigin(tileStartX, tileStartY);
-      newTile = AsDerived().ValidateTile(newTile, nsIntPoint(tileStartX, tileStartY),
-                                         tileDrawRegion);
+      AsDerived().ValidateTile(newTile, nsIntPoint(tileStartX, tileStartY),
+                               tileDrawRegion);
       NS_ASSERTION(!newTile.IsPlaceholderTile(), "Unexpected placeholder tile - failed to allocate?");
 #ifdef GFX_TILEDLAYER_PREF_WARNINGS
       printf_stderr("Store Validate tile %i, %i -> %i\n", tileStartX, tileStartY, index);
