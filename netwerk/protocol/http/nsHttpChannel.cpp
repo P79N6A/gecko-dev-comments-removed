3f32d0b89f5a74aaae7d967995c29716915eb833
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:13:43 +0000

    bug 1069724 - all conn reuse after conn pool flush on shift-reload r=hurley
    UltraBlame original commit: 6a917dc8e60cd06161a2657361cacbcc6cd48cf9

--- netwerk/protocol/http/nsHttpChannel.cpp
+++ netwerk/protocol/http/nsHttpChannel.cpp
@@ -4897,8 +4897,7 @@ nsHttpChannel::BeginConnect()
         if (mLoadFlags & LOAD_INITIAL_DOCUMENT_URI) {
             gHttpHandler->ConnMgr()->DoShiftReloadConnectionCleanup(mConnectionInfo);
         }
-        
-        mCaps &= ~(NS_HTTP_ALLOW_KEEPALIVE | NS_HTTP_ALLOW_PIPELINING);
+        mCaps &= ~NS_HTTP_ALLOW_PIPELINING;
     }
 
     
