64e7b2508c7bd751c04172e83b6f22625c45b0c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:10:26 +0000

    Bug 1131455 - about:sync-log assertion failure: newly created channel must have a loadinfo attached (r=rnewman)
    UltraBlame original commit: 087732d18dc12cacc3c2fc0900fe7f7336fc1d6b

--- services/sync/Weave.js
+++ services/sync/Weave.js
@@ -177,15 +177,11 @@ AboutWeaveLog.prototype = {
     return 0;
   },
 
-  newChannel: function(aURI) {
+  newChannel: function(aURI, aLoadInfo) {
     let dir = FileUtils.getDir("ProfD", ["weave", "logs"], true);
     let uri = Services.io.newFileURI(dir);
-    let channel = Services.io.newChannelFromURI2(uri,
-                                                 null,      
-                                                 Services.scriptSecurityManager.getSystemPrincipal(),
-                                                 null,      
-                                                 Ci.nsILoadInfo.SEC_NORMAL,
-                                                 Ci.nsIContentPolicy.TYPE_OTHER);
+    let channel = Services.io.newChannelFromURIWithLoadInfo(uri, aLoadInfo);
+
     channel.originalURI = aURI;
 
     
