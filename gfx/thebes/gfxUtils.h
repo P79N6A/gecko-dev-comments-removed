5279781d3bfe004e9cbb8441e6fb8cc59d507166
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:18:57 +0000

    Bug 1105834 - Part 3: Add lz4 support. r=jrmuizel
    UltraBlame original commit: 33592fd41f1f77c7917f7e8bac5c03dd37e26e79

--- gfx/thebes/gfxUtils.h
+++ gfx/thebes/gfxUtils.h
@@ -283,6 +283,7 @@ public:
     }
     static nsCString GetAsDataURI(SourceSurface* aSourceSurface);
     static nsCString GetAsDataURI(DrawTarget* aDT);
+    static nsCString GetAsLZ4Base64Str(DataSourceSurface* aSourceSurface);
 
     
 
