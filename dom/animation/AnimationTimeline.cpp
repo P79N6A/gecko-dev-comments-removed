bb819607350069d7ce876f5f2d7d76cc81d0d0d6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:26:42 +0000

    Bug 927349 part 7 - Factor out AnimationTimeline::GetRefreshDriver into a separate method; r=jwatt
    This is in preparation for adding AnimationTimeline::FastForward in the next
    patch which will reuse this code.
    
    UltraBlame original commit: 1e4ce2a592b820029a7b864f3af6aceeeb9c0160

--- dom/animation/AnimationTimeline.cpp
+++ dom/animation/AnimationTimeline.cpp
@@ -55,17 +55,12 @@ AnimationTimeline::GetCurrentTimeStamp() const
     result = timing->GetNavigationStartTimeStamp();
   }
 
-  nsIPresShell* presShell = mDocument->GetShell();
-  if (MOZ_UNLIKELY(!presShell)) {
+  nsRefreshDriver* refreshDriver = GetRefreshDriver();
+  if (!refreshDriver) {
     return result;
   }
 
-  nsPresContext* presContext = presShell->GetPresContext();
-  if (MOZ_UNLIKELY(!presContext)) {
-    return result;
-  }
-
-  result = presContext->RefreshDriver()->MostRecentRefresh();
+  result = refreshDriver->MostRecentRefresh();
   
   
   
@@ -102,6 +97,22 @@ AnimationTimeline::ToTimeStamp(const TimeDuration& aTimeDuration) const
 
   result = timing->GetNavigationStartTimeStamp() + aTimeDuration;
   return result;
+}
+
+nsRefreshDriver*
+AnimationTimeline::GetRefreshDriver() const
+{
+  nsIPresShell* presShell = mDocument->GetShell();
+  if (MOZ_UNLIKELY(!presShell)) {
+    return nullptr;
+  }
+
+  nsPresContext* presContext = presShell->GetPresContext();
+  if (MOZ_UNLIKELY(!presContext)) {
+    return nullptr;
+  }
+
+  return presContext->RefreshDriver();
 }
 
 } 
