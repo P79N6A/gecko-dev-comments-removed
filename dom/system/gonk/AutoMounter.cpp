4c7239be4a1429593335cc3a2a3b01e4a0526d31
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:40:00 +0000

    Bug 1091544 - Set volume as STATE_MOUNT_FAIL while mounting the volume failed, r=dhylands.
    UltraBlame original commit: f3f06c6800fc73f0cd6bf26195529b61b69378f7

--- dom/system/gonk/AutoMounter.cpp
+++ dom/system/gonk/AutoMounter.cpp
@@ -1059,15 +1059,16 @@ AutoMounter::UpdateState()
           vol->StartUnmount(mResponseCallback);
           return; 
         }
-        case nsIVolume::STATE_IDLE: {
-          LOG("UpdateState: Volume %s is nsIVolume::STATE_IDLE", vol->NameStr());
+        case nsIVolume::STATE_IDLE:
+        case nsIVolume::STATE_MOUNT_FAIL: {
+          LOG("UpdateState: Volume %s is %s", vol->NameStr(), vol->StateStr());
           if (vol->IsFormatting() && !vol->IsFormatRequested()) {
             vol->SetFormatRequested(false);
             LOG("UpdateState: Mounting %s", vol->NameStr());
             vol->StartMount(mResponseCallback);
             break;
           }
-          if (tryToShare && vol->IsSharingEnabled()) {
+          if (tryToShare && vol->IsSharingEnabled() && volState == nsIVolume::STATE_IDLE) {
             
             LOG("UpdateState: Sharing %s", vol->NameStr());
             vol->StartShare(mResponseCallback);
