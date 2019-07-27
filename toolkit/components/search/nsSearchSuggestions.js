b7cbb38c7421bf9c8cd0aa33ee08ddf21449591c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:27:10 +0000

    Bug 1007979 - Refactor nsSearchSuggestions into a reusable JSM. r=adw
    Original JSM by mconnor.
    
    UltraBlame original commit: bc27aaabefa89b118b2b0bec16f10cd95147a0f0

--- toolkit/components/search/nsSearchSuggestions.js
+++ toolkit/components/search/nsSearchSuggestions.js
@@ -2,25 +2,17 @@
 
 
 
-const SEARCH_RESPONSE_SUGGESTION_JSON = "application/x-suggestions+json";
-
 const BROWSER_SUGGEST_PREF = "browser.search.suggest.enabled";
 const XPCOM_SHUTDOWN_TOPIC              = "xpcom-shutdown";
 const NS_PREFBRANCH_PREFCHANGE_TOPIC_ID = "nsPref:changed";
 
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-const Cr = Components.results;
-const Cu = Components.utils;
-
-const HTTP_OK                    = 200;
-const HTTP_INTERNAL_SERVER_ERROR = 500;
-const HTTP_BAD_GATEWAY           = 502;
-const HTTP_SERVICE_UNAVAILABLE   = 503;
+const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/nsFormAutoCompleteResult.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "SearchSuggestionController",
+                                  "resource://gre/modules/SearchSuggestionController.jsm");
 
 
 
@@ -38,6 +30,7 @@ SuggestAutoComplete.prototype = {
   _init: function() {
     this._addObservers();
     this._suggestEnabled = Services.prefs.getBoolPref(BROWSER_SUGGEST_PREF);
+    this._suggestionController = new SearchSuggestionController(obj => this.onResultsReturned(obj));
   },
 
   get _suggestionLabel() {
@@ -56,135 +49,7 @@ SuggestAutoComplete.prototype = {
 
 
 
-  
-
-
-
-  _serverErrorLog: [],
-
-  
-
-
-
-  _maxErrorsBeforeBackoff: 3,
-
-  
-
-
-
-
-  _serverErrorPeriod: 600000,  
-
-  
-
-
-
-  _serverErrorTimeoutIncrement: 600000,  
-
-  
-
-
-
-  _serverErrorTimeout: 0,
-
-  
-
-
-  _nextRequestTime: 0,
-
-  
-
-
-
-  _serverErrorEngine: null,
-
-  
-
-
-
-  _request: null,
-
-  
-
-
-
-
   _listener: null,
-
-  
-
-
-
-  _includeFormHistory: true,
-
-  
-
-
-
-
-
-  _sentSuggestRequest: false,
-
-  
-
-
-  notify: function SAC_notify(timer) {
-    
-    
-    this._formHistoryTimer = null;
-
-    
-    
-    if (!this._listener)
-      return;
-
-    
-    
-    this._listener.onSearchResult(this, this._formHistoryResult);
-    this._reset();
-  },
-
-  
-
-
-
-  _suggestionTimeout: 500,
-
-  
-
-
-
-  onSearchResult: function SAC_onSearchResult(search, result) {
-    this._formHistoryResult = result;
-
-    if (this._request) {
-      
-      
-      this._formHistoryTimer = Cc["@mozilla.org/timer;1"].
-                               createInstance(Ci.nsITimer);
-      this._formHistoryTimer.initWithCallback(this, this._suggestionTimeout,
-                                              Ci.nsITimer.TYPE_ONE_SHOT);
-    } else if (!this._sentSuggestRequest) {
-      
-      this._listener.onSearchResult(this, this._formHistoryResult);
-      this._reset();
-    }
-  },
-
-  
-
-
-  _suggestURI: null,
-
-  
-
-
-  _formHistoryResult: null,
-
-  
-
-
-  _formHistoryTimer: null,
 
   
 
@@ -197,174 +62,30 @@ SuggestAutoComplete.prototype = {
   
 
 
-  _reset: function SAC_reset() {
+
+  onResultsReturned: function(results) {
+    let finalResults = [];
+    let finalComments = [];
+
     
-    
-    if (!this._formHistoryTimer) {
-      this._listener = null;
-      this._formHistoryResult = null;
+    let maxHistoryItems = Math.min(results.local.length, this._historyLimit);
+    for (let i = 0; i < maxHistoryItems; ++i) {
+      finalResults.push(results.local[i]);
+      finalComments.push("");
     }
-    this._request = null;
-  },
 
-  
-
-
-
-  _startHistorySearch: function SAC_SHSearch(searchString, searchParam) {
-    var formHistory =
-      Cc["@mozilla.org/autocomplete/search;1?name=form-history"].
-      createInstance(Ci.nsIAutoCompleteSearch);
-    formHistory.startSearch(searchString, searchParam, this._formHistoryResult, this);
-  },
-
-  
-
-
-
-  _noteServerError: function SAC__noteServeError() {
-    var currentTime = Date.now();
-
-    this._serverErrorLog.push(currentTime);
-    if (this._serverErrorLog.length > this._maxErrorsBeforeBackoff)
-      this._serverErrorLog.shift();
-
-    if ((this._serverErrorLog.length == this._maxErrorsBeforeBackoff) &&
-        ((currentTime - this._serverErrorLog[0]) < this._serverErrorPeriod)) {
+    
+    if (results.remote.length) {
       
-      this._serverErrorTimeout = (this._serverErrorTimeout * 2) +
-                                 this._serverErrorTimeoutIncrement;
-      this._nextRequestTime = currentTime + this._serverErrorTimeout;
-    }
-  },
-
-  
-
-
-  _clearServerErrors: function SAC__clearServerErrors() {
-    this._serverErrorLog = [];
-    this._serverErrorTimeout = 0;
-    this._nextRequestTime = 0;
-  },
-
-  
-
-
-
-
-
-  _okToRequest: function SAC__okToRequest() {
-    return Date.now() > this._nextRequestTime;
-  },
-
-  
-
-
-
-
-
-
-
-  _checkForEngineSwitch: function SAC__checkForEngineSwitch(engine) {
-    if (engine == this._serverErrorEngine)
-      return;
-
-    
-    this._serverErrorEngine = engine;
-    this._clearServerErrors();
-  },
-
-  
-
-
-
-
-
-
-  _isBackoffError: function SAC__isBackoffError(status) {
-    return ((status == HTTP_INTERNAL_SERVER_ERROR) ||
-            (status == HTTP_BAD_GATEWAY) ||
-            (status == HTTP_SERVICE_UNAVAILABLE));
-  },
-
-  
-
-
-
-
-  onReadyStateChange: function() {
-    
-    if (!this._request || this._request.readyState != 4)
-      return;
-
-    try {
-      var status = this._request.status;
-    } catch (e) {
-      
-      return;
-    }
-
-    if (this._isBackoffError(status)) {
-      this._noteServerError();
-      return;
-    }
-
-    var responseText = this._request.responseText;
-    if (status != HTTP_OK || responseText == "")
-      return;
-
-    this._clearServerErrors();
-
-    try {
-      var serverResults = JSON.parse(responseText);
-    } catch(ex) {
-      Components.utils.reportError("Failed to parse JSON from " + this._suggestURI.spec + ": " + ex);
-      return;
-    }
-
-    var searchString = serverResults[0] || "";
-    var results = serverResults[1] || [];
-
-    var comments = [];  
-    var historyResults = [];
-    var historyComments = [];
-
-    
-    if (this._includeFormHistory && this._formHistoryResult &&
-        (this._formHistoryResult.searchResult ==
-         Ci.nsIAutoCompleteResult.RESULT_SUCCESS)) {
-      var maxHistoryItems = Math.min(this._formHistoryResult.matchCount, this._historyLimit);
-      for (var i = 0; i < maxHistoryItems; ++i) {
-        var term = this._formHistoryResult.getValueAt(i);
-
-        
-        var dupIndex = results.indexOf(term);
-        if (dupIndex != -1)
-          results.splice(dupIndex, 1);
-
-        historyResults.push(term);
-        historyComments.push("");
-      }
-    }
-
-    
-    for (var i = 0; i < results.length; ++i)
-      comments.push("");
-
-    
-    if (comments.length > 0)
+      let comments = new Array(results.remote.length).fill("", 1);
       comments[0] = this._suggestionLabel;
+      
+      finalResults = finalResults.concat(results.remote);
+      finalComments = finalComments.concat(comments);
+    }
 
     
-    var finalResults = historyResults.concat(results);
-    var finalComments = historyComments.concat(comments);
-
-    
-    this.onResultsReady(searchString, finalResults, finalComments,
-                        this._formHistoryResult);
-
-    
-    this._reset();
+    this.onResultsReady(results.term, finalResults, finalComments, results.formHistoryResult);
   },
 
   
@@ -374,10 +95,9 @@ SuggestAutoComplete.prototype = {
 
 
 
-  onResultsReady: function(searchString, results, comments,
-                           formHistoryResult) {
+  onResultsReady: function(searchString, results, comments, formHistoryResult) {
     if (this._listener) {
-      var result = new FormAutoCompleteResult(
+      let result = new FormAutoCompleteResult(
           searchString,
           Ci.nsIAutoCompleteResult.RESULT_SUCCESS,
           0,
@@ -389,7 +109,6 @@ SuggestAutoComplete.prototype = {
 
       this._listener.onSearchResult(this, result);
 
-      
       
       this._listener = null;
     }
@@ -445,56 +164,10 @@ SuggestAutoComplete.prototype = {
 
 
   _triggerSearch: function(searchString, searchParam, listener, privacyMode) {
-    
-    
-    
-    
-    
-    this.stopSearch();
-
     this._listener = listener;
-
-    var engine = Services.search.currentEngine;
-
-    this._checkForEngineSwitch(engine);
-
-    if (!searchString ||
-        !this._suggestEnabled ||
-        !engine.supportsResponseType(SEARCH_RESPONSE_SUGGESTION_JSON) ||
-        !this._okToRequest()) {
-      
-      
-      
-      
-      this._sentSuggestRequest = false;
-      this._startHistorySearch(searchString, searchParam);
-      return;
-    }
-
-    
-    this._request = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].
-                    createInstance(Ci.nsIXMLHttpRequest);
-    var submission = engine.getSubmission(searchString,
-                                          SEARCH_RESPONSE_SUGGESTION_JSON);
-    this._suggestURI = submission.uri;
-    var method = (submission.postData ? "POST" : "GET");
-    this._request.open(method, this._suggestURI.spec, true);
-    this._request.channel.notificationCallbacks = new AuthPromptOverride();
-    if (this._request.channel instanceof Ci.nsIPrivateBrowsingChannel) {
-      this._request.channel.setPrivate(privacyMode);
-    }
-
-    var self = this;
-    function onReadyStateChange() {
-      self.onReadyStateChange();
-    }
-    this._request.onreadystatechange = onReadyStateChange;
-    this._request.send(submission.postData);
-
-    if (this._includeFormHistory) {
-      this._sentSuggestRequest = true;
-      this._startHistorySearch(searchString, searchParam);
-    }
+    this._suggestionController.fetch(searchString,
+                                     privacyMode,
+                                     Services.search.currentEngine);
   },
 
   
@@ -502,10 +175,7 @@ SuggestAutoComplete.prototype = {
 
 
   stopSearch: function() {
-    if (this._request) {
-      this._request.abort();
-      this._reset();
-    }
+    this._suggestionController.stop();
   },
 
   
@@ -539,31 +209,6 @@ SuggestAutoComplete.prototype = {
                                          Ci.nsIAutoCompleteObserver])
 };
 
-function AuthPromptOverride() {
-}
-AuthPromptOverride.prototype = {
-  
-  getAuthPrompt: function (reason, iid) {
-    
-    return {
-      promptAuth: function () {
-        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
-      },
-      asyncPromptAuth: function () {
-        throw Cr.NS_ERROR_NOT_IMPLEMENTED;
-      }
-    };
-  },
-
-  
-  getInterface: function SSLL_getInterface(iid) {
-    return this.QueryInterface(iid);
-  },
-
-  
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAuthPromptProvider,
-                                         Ci.nsIInterfaceRequestor])
-};
 
 
 
