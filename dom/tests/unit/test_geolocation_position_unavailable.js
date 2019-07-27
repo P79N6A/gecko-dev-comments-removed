a86ccff3cba9230fe00e17821a97ea7a5ce5128a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:57:32 +0000

    Bug 811470 - Send geolocation error codes to the content process. r=jdm
    UltraBlame original commit: d0c7bece66e33cdb636fa2acb2d844c588a6c200

---
+++ dom/tests/unit/test_geolocation_position_unavailable.js
@@ -0,0 +1,36 @@
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+const Cr = Components.results;
+
+
+function successCallback() {
+  do_check_true(false);
+  do_test_finished();
+}
+
+function errorCallback(err) {
+  do_check_eq(Ci.nsIDOMGeoPositionError.POSITION_UNAVAILABLE, err.code);
+  do_test_finished();
+}
+
+function run_test()
+{
+  do_test_pending();
+
+  if (Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULRuntime)
+        .processType == Ci.nsIXULRuntime.PROCESS_TYPE_DEFAULT) {
+    
+    
+    
+    do_get_profile();
+
+    var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefBranch);
+    prefs.setBoolPref("geo.wifi.scan", false);
+    prefs.setCharPref("geo.wifi.uri", "UrlNotUsedHere:");
+    prefs.setBoolPref("dom.testing.ignore_ipc_principal", true);
+  }
+
+  geolocation = Cc["@mozilla.org/geolocation;1"].getService(Ci.nsISupports);
+  geolocation.getCurrentPosition(successCallback, errorCallback);
+}
