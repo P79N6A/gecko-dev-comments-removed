f26f6709af92548b02749ea70ef67dc506bb159e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:47:05 +0000

    Bug 975591 - Part 2: Show DevTools devices in WebIDE. r=paul
    UltraBlame original commit: 82a75a2df1c249ad088e4498b5c55dac4111cd43

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -641,6 +641,8 @@ pref("devtools.dump.emit", false);
 
 pref("devtools.discovery.log", false);
 
+pref("devtools.remote.wifi.scan", false);
+
 
 pref("view_source.syntax_highlight", true);
 pref("view_source.wrap_long_lines", false);
