47156e62f01f017e974d125ec32c11ca79ae8de7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:56:41 +0000

    Bug 1156135. Add runtime testing of graphics features. r=mattwoodrow,mossop
    UltraBlame original commit: a53b5cee6b6f89f5c987b00060ab76f47614da08

---
+++ toolkit/components/gfx/SanityTest.js
@@ -0,0 +1,150 @@
+
+
+
+
+"use strict";
+
+const { utils: Cu, interfaces: Ci, classes: Cc, results: Cr } = Components;
+
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import('resource://gre/modules/Preferences.jsm');
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+
+const PAGE_WIDTH=72;
+const PAGE_HEIGHT=136;
+const DRIVER_PREF="sanity-test.driver-version";
+const DEVICE_PREF="sanity-test.device-id";
+const VERSION_PREF="sanity-test.version";
+const DISABLE_VIDEO_PREF="media.hardware-video-decoding.failed";
+const RUNNING_PREF="sanity-test.running";
+
+
+const TEST_PASSED=0;
+const TEST_FAILED_RENDER=1;
+const TEST_FAILED_VIDEO=2;
+const TEST_CRASHED=3;
+
+function testPixel(ctx, x, y, r, g, b, a, fuzz) {
+  var data = ctx.getImageData(x, y, 1, 1);
+
+  if (Math.abs(data.data[0] - r) <= fuzz &&
+      Math.abs(data.data[1] - g) <= fuzz &&
+      Math.abs(data.data[2] - b) <= fuzz &&
+      Math.abs(data.data[3] - a) <= fuzz) {
+    return true;
+  }
+  return false;
+}
+
+function reportResult(val) {
+  try {
+    let histogram = Services.telemetry.getHistogramById("GRAPHICS_SANITY_TEST");
+    histogram.add(val);
+  } catch (e) {}
+
+  Preferences.set(RUNNING_PREF, false);
+  Services.prefs.savePrefFile(null);
+}
+
+function takeWindowSnapshot(win) {
+  
+  var canvas = win.createElementNS("http://www.w3.org/1999/xhtml", "canvas");
+  canvas.setAttribute("width", PAGE_WIDTH);
+  canvas.setAttribute("height", PAGE_HEIGHT);
+
+  
+  
+  
+  var ctx = canvas.getContext("2d");
+  var flags = ctx.DRAWWINDOW_DRAW_CARET | ctx.DRAWWINDOW_DRAW_VIEW | ctx.DRAWWINDOW_USE_WIDGET_LAYERS;
+  ctx.drawWindow(win.ownerGlobal, 0, 0, PAGE_WIDTH, PAGE_HEIGHT, "rgb(255,255,255)", flags);
+
+  win.ownerGlobal.close();
+  return ctx;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function verifyVideoRendering(ctx) {
+  return testPixel(ctx, 18, 82, 255, 255, 255, 255, 64) &&
+    testPixel(ctx, 50, 82, 0, 255, 0, 255, 64) &&
+    testPixel(ctx, 18, 114, 0, 0, 255, 255, 64) &&
+    testPixel(ctx, 50, 114, 255, 0, 0, 255, 64);
+}
+
+function testGfxFeatures(event) {
+  var win = event.target;
+  var canvas = takeWindowSnapshot(win);
+
+  if (!verifyVideoRendering(canvas)) {
+    reportResult(TEST_FAILED_VIDEO);
+    Preferences.set(DISABLE_VIDEO_PREF, true);
+  } else {
+    reportResult(TEST_PASSED);
+  }
+}
+
+function SanityTest() {}
+SanityTest.prototype = {
+  classID: Components.ID("{f3a8ca4d-4c83-456b-aee2-6a2cbf11e9bd}"),
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver,
+                                         Ci.nsISupportsWeakReference]),
+
+  shouldRunTest: function() {
+    
+    
+    var appInfo = Cc["@mozilla.org/xre/app-info;1"].getService(Ci.nsIXULAppInfo);
+    var xulVersion = appInfo.version;
+    var gfxinfo = Cc["@mozilla.org/gfx/info;1"].getService(Ci.nsIGfxInfo);
+
+    if (Preferences.get(RUNNING_PREF, false)) {
+      Preferences.set(DISABLE_VIDEO_PREF, true);
+      reportResult(TEST_CRASHED);
+      return false;
+    }
+
+    
+    if (Preferences.get(DRIVER_PREF, "") == gfxinfo.adapterDriverVersion &&
+        Preferences.get(DEVICE_PREF, "") == gfxinfo.adapterDeviceID &&
+        Preferences.get(VERSION_PREF, "") == xulVersion) {
+      return false;
+    }
+
+    
+    
+    Preferences.set(DISABLE_VIDEO_PREF, false);
+    Preferences.set(DRIVER_PREF, gfxinfo.adapterDriverVersion);
+    Preferences.set(DEVICE_PREF, gfxinfo.adapterDeviceID);
+    Preferences.set(VERSION_PREF, xulVersion);
+
+    
+    Preferences.set(RUNNING_PREF, true);
+    Services.prefs.savePrefFile(null);
+    return true;
+  },
+
+  observe: function(subject, topic, data) {
+    if (topic != "profile-after-change") return;
+    if (!this.shouldRunTest()) return;
+
+    
+    var sanityTest = Services.ww.openWindow(null,
+        "chrome://gfxsanity/content/sanitytest.html",
+        "Test Page",
+        "width=" + PAGE_WIDTH + ",height=" + PAGE_HEIGHT + ",chrome,titlebar=0,scrollbars=0",
+        null);
+    sanityTest.onload = testGfxFeatures;
+  },
+};
+
+this.NSGetFactory = XPCOMUtils.generateNSGetFactory([SanityTest]);
