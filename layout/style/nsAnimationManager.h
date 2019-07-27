e124674501eec0d74f2af627e9b9202592aae084
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:22:42 +0000

    Bug 1171817 part 4 - Add const version of AsCSSAnimation/AsCSSTransition methods; r=dbaron
    These will be needed for sorting animations and transitions in a const-correct
    fashion.
    
    UltraBlame original commit: c1aa5430f58680cc21c3e4cb282bc5ecbfdfdb08

--- layout/style/nsAnimationManager.h
+++ layout/style/nsAnimationManager.h
@@ -75,7 +75,8 @@ public:
   JSObject* WrapObject(JSContext* aCx,
                        JS::Handle<JSObject*> aGivenProto) override;
 
-  virtual CSSAnimation* AsCSSAnimation() override { return this; }
+  CSSAnimation* AsCSSAnimation() override { return this; }
+  const CSSAnimation* AsCSSAnimation() const override { return this; }
 
   
   void GetAnimationName(nsString& aRetVal) const { aRetVal = mAnimationName; }
