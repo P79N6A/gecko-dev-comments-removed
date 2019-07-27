15cafc642dfc241f789952e849acd456da894ea9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:44:26 +0000

    Bug 1149677 - Force minimum QR code size. r=past
    UltraBlame original commit: 76a06f930f3b790f69e6b3d13e0e6edd4d5bc856

--- browser/devtools/webide/modules/runtimes.js
+++ browser/devtools/webide/modules/runtimes.js
@@ -529,9 +529,12 @@ WiFiRuntime.prototype = {
 
     
     DevToolsUtils.executeSoon(() => {
+      
+      
+      const MIN_HEIGHT = 600;
       let win = Services.wm.getMostRecentWindow("devtools:webide");
       let width = win.outerWidth * 0.8;
-      let height = win.outerHeight * 0.5;
+      let height = Math.max(win.outerHeight * 0.5, MIN_HEIGHT);
       win.openDialog("chrome://webide/content/wifi-auth.xhtml",
                      WINDOW_ID,
                      "modal=yes,width=" + width + ",height=" + height, session);
