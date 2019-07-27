fb54d7f02174fa039a75ac40a8fb29a9df5092c9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:47:59 +0000

    Bug 1052923 - Fix the volume conversion in millibels. r=padenot
    UltraBlame original commit: 5cdce85e23289d904baf0604c6910184c635dbb6

--- media/libcubeb/src/cubeb_opensl.c
+++ media/libcubeb/src/cubeb_opensl.c
@@ -745,6 +745,7 @@ opensl_stream_set_volume(cubeb_stream * stm, float volume)
 {
   SLresult res;
   SLmillibel max_level, millibels;
+  float unclamped_millibels;
 
   res = (*stm->volume)->GetMaxVolumeLevel(stm->volume, &max_level);
 
@@ -752,12 +753,15 @@ opensl_stream_set_volume(cubeb_stream * stm, float volume)
     return CUBEB_ERROR;
   }
 
-  millibels = SL_MILLIBEL_MIN + (float)(max_level - SL_MILLIBEL_MIN) * volume;
-
   
-  if (millibels > max_level) {
-   millibels = max_level;
-  }
+
+
+
+  unclamped_millibels = 100.0f * 20.0f * log10f(fmaxf(volume, 0.0f));
+  unclamped_millibels = fmaxf(unclamped_millibels, SL_MILLIBEL_MIN);
+  unclamped_millibels = fminf(unclamped_millibels, max_level);
+
+  millibels = lroundf(unclamped_millibels);
 
   res = (*stm->volume)->SetVolumeLevel(stm->volume, millibels);
 
