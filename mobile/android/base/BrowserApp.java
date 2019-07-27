f866a9290929f77de6e2256cdee9c025d79709dc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:33:29 +0000

    Bug 1059792 - Launching Firefox for the first time through an external intent should not display First Run experience. r=margaret
    UltraBlame original commit: 297008b7e1f6e0d3d340bb568d3d26cca99c02d5

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -82,7 +82,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -153,7 +152,7 @@ public class BrowserApp extends GeckoApp
 
     
     private static final int ACTIVITY_REQUEST_PREFERENCES = 1001;
-    public static final String ACTION_NEW_PROFILE = "org.mozilla.gecko.NEW_PROFILE";
+    private static final String PREF_FIRSTRUN_STARTPANE = "firstrun_startpane";
 
     private BrowserSearch mBrowserSearch;
     private View mBrowserSearchContainer;
@@ -611,8 +610,6 @@ public class BrowserApp extends GeckoApp
             "Updater:Launch",
             "BrowserToolbar:Visibility");
 
-        registerOnboardingReceiver(this);
-
         Distribution distribution = Distribution.init(this);
 
         
@@ -673,24 +670,34 @@ public class BrowserApp extends GeckoApp
         }
     }
 
-    private void registerOnboardingReceiver(Context context) {
-        final LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(this);
+    
 
-        
-        mOnboardingReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                launchStartPane(BrowserApp.this);
+
+
+
+
+
+    private void checkStartPane(Context context, String intentAction) {
+        final StrictMode.ThreadPolicy savedPolicy = StrictMode.allowThreadDiskReads();
+
+        try {
+            final SharedPreferences prefs = GeckoSharedPrefs.forProfile(this);
+
+            if (prefs.contains(PREF_FIRSTRUN_STARTPANE)) {
+                return;
+             }
+
+            if (!Intent.ACTION_VIEW.equals(intentAction)) {
+                final Intent startIntent = new Intent(this, StartPane.class);
+                context.startActivity(startIntent);
             }
-        };
+            
+            prefs.edit().putBoolean(PREF_FIRSTRUN_STARTPANE, true).apply();
 
-        lbm.registerReceiver(mOnboardingReceiver, new IntentFilter(ACTION_NEW_PROFILE));
-    }
-
-    private void launchStartPane(Context context) {
-         final Intent startIntent = new Intent(context, StartPane.class);
-         context.startActivity(startIntent);
-     }
+        } finally {
+            StrictMode.setThreadPolicy(savedPolicy);
+        }
+      }
 
     private Class<?> getMediaPlayerManager() {
         if (AppConstants.MOZ_MEDIA_PLAYER) {
@@ -1092,6 +1099,8 @@ public class BrowserApp extends GeckoApp
 
     @Override
     protected void initializeChrome() {
+        checkStartPane(this, getIntent().getAction());
+
         super.initializeChrome();
 
         mDoorHangerPopup.setAnchor(mBrowserToolbar.getDoorHangerAnchor());
@@ -2568,7 +2577,6 @@ public class BrowserApp extends GeckoApp
         
         
         
-        final SharedPreferences prefs = GeckoSharedPrefs.forProfile(this);
         final boolean visible = Versions.preICS ||
                                 HardwareUtils.isTelevision() ||
                                 !PrefUtils.getStringSet(GeckoSharedPrefs.forProfile(this),
@@ -2931,6 +2939,8 @@ public class BrowserApp extends GeckoApp
             String uri = intent.getDataString();
             GeckoAppShell.sendEventToGecko(GeckoEvent.createURILoadEvent(uri));
         }
+
+        checkStartPane(this, action);
 
         if (!mInitialized) {
             return;
