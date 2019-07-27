ed57993c2f2bb0444c129f50d932c5244ad2a000
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:57:03 +0000

    Backed out changeset 822901f56c1f (bug 1170216)
    UltraBlame original commit: ed587a5aaecf2afd6313a1c6aa230210811e7603

--- js/src/jsobj.cpp
+++ js/src/jsobj.cpp
@@ -548,19 +548,6 @@ js::SetIntegrityLevel(JSContext* cx, HandleObject obj, IntegrityLevel level)
 
         MOZ_ASSERT(nobj->lastProperty()->slotSpan() == last->slotSpan());
         JS_ALWAYS_TRUE(nobj->setLastProperty(cx, last));
-
-        
-        
-        
-        
-        
-        
-        
-        if (level == IntegrityLevel::Frozen && obj->is<ArrayObject>()) {
-            if (!obj->as<ArrayObject>().maybeCopyElementsForWrite(cx))
-                return false;
-            obj->as<ArrayObject>().getElementsHeader()->setNonwritableArrayLength();
-        }
     } else {
         RootedId id(cx);
         Rooted<PropertyDescriptor> desc(cx);
@@ -597,6 +584,21 @@ js::SetIntegrityLevel(JSContext* cx, HandleObject obj, IntegrityLevel level)
             if (!DefineProperty(cx, obj, id, desc))
                 return false;
         }
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if (level == IntegrityLevel::Frozen && obj->is<ArrayObject>()) {
+        if (!obj->as<ArrayObject>().maybeCopyElementsForWrite(cx))
+            return false;
+        obj->as<ArrayObject>().getElementsHeader()->setNonwritableArrayLength();
     }
 
     return true;
