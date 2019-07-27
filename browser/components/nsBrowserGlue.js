e2c4f58bb25777a56e0c2d27d6e9a5d1ab110dbb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:14:21 +0000

    Bug 1151507 - Warn users when unsigned add-ons have been disabled during startup. r=dtownsend
    UltraBlame original commit: dbec5281f6255715621a258a15bd2b6baee25df8

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -955,12 +955,14 @@ BrowserGlue.prototype = {
     
     
     aWindow.messageManager.loadFrameScript("resource://pdf.js/pdfjschildbootstrap.js", true);
+
 #ifdef NIGHTLY_BUILD
     
     aWindow.messageManager.loadFrameScript("chrome://shumway/content/bootstrap-content.js", true);
     
     ShumwayUtils.init();
 #endif
+
 #ifdef XP_WIN
     
     const WINTASKBAR_CONTRACTID = "@mozilla.org/windows-taskbar;1";
@@ -1004,6 +1006,14 @@ BrowserGlue.prototype = {
     }
 
     this._checkForOldBuildUpdates();
+
+    let disabledAddons = AddonManager.getStartupChanges(AddonManager.STARTUP_CHANGE_DISABLED);
+    for (let id of disabledAddons) {
+      if (AddonManager.getAddonByID(id).signedState <= AddonManager.SIGNEDSTATE_MISSING) {
+        this._notifyUnsignedAddonsDisabled();
+        break;
+      }
+    }
 
     this._firstWindowTelemetry(aWindow);
   },
