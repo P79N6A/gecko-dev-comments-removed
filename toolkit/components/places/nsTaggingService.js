778b5ee3097bae9ac5ae0089eae242f58e5ad252
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:30:31 +0000

    Bug 407821 - Discard bookmark tags exceeding maximum length of 100 characters. r=paolo
    UltraBlame original commit: 232b2eadb49addb7dabaa1cfbc23f690575fb0ec

--- toolkit/components/places/nsTaggingService.js
+++ toolkit/components/places/nsTaggingService.js
@@ -115,7 +115,7 @@ TaggingService.prototype = {
         
         tag.__defineGetter__("name", function () this._self._tagFolders[this.id]);
       }
-      else if (typeof(val) == "string" && val.length > 0) {
+      else if (typeof(val) == "string" && val.length > 0 && val.length <= Ci.nsITaggingService.MAX_TAG_LENGTH) {
         
         tag.name = val;
         
