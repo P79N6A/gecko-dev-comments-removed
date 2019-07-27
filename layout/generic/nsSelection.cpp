8f2d1e074a27e67b74d06a7c77da39ccc599362a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:29:39 +0000

    Bug 1140625 - Part 1: Add recursive call in GetFrameForNodeOffset. r=roc
    UltraBlame original commit: 1b76bac87d825887ca19e7eab0a8455f740486c5

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -1855,29 +1855,21 @@ nsFrameSelection::GetFrameForNodeOffset(nsIContent*        aNode,
       theNode = childNode;
     }
 
-#ifdef DONT_DO_THIS_YET
     
     
-
-    
-    
-
-    if (theNode->IsElement())
+    if (theNode->IsElement() &&
+        theNode->GetChildCount() &&
+        !theNode->HasIndependentSelection())
     {
       int32_t newOffset = 0;
 
-      if (aOffset > childIndex)
-      {
+      if (aOffset > childIndex) {
         numChildren = theNode->GetChildCount();
-
         newOffset = numChildren;
       }
 
       return GetFrameForNodeOffset(theNode, newOffset, aHint, aReturnOffset);
-    }
-    else
-#endif 
-    {
+    } else {
       
       
 
