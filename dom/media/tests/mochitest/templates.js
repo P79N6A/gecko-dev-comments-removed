6ad76c6485ce80657c04636d2736a0848127193b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:11:00 +0000

    Bug 1089798 - MediaStream ID tests. r=bwc
    UltraBlame original commit: a6ccfddbdac032abc47085f85b3e8747cd9b65d8

--- dom/media/tests/mochitest/templates.js
+++ dom/media/tests/mochitest/templates.js
@@ -486,14 +486,6 @@ var commandsPeerConnectionOfferAnswer = [
   function PC_REMOTE_CHECK_MEDIA_FLOW_PRESENT(test) {
     return test.pcRemote.checkMediaFlowPresent();
   },
-
-
-
-
-
-
-
-
   function PC_LOCAL_CHECK_STATS(test) {
     return test.pcLocal.getStats(null).then(stats => {
       test.pcLocal.checkStats(stats, test.steeplechase);
