ae64e01b4fd165c5671df534e1d42dd5d4eacb4a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:10:50 +0000

    Bug 1037447 - Introduce nsICacheEntry nsICacheStorage.openTruncate(URL, idEnhance), r=michal
    UltraBlame original commit: 72a245d3e41544b6657c7dc18550e9ad46ecf8c5

--- netwerk/cache2/CacheStorage.cpp
+++ netwerk/cache2/CacheStorage.cpp
@@ -105,6 +105,33 @@ NS_IMETHODIMP CacheStorage::AsyncOpenURI(nsIURI *aURI,
 }
 
 
+NS_IMETHODIMP CacheStorage::OpenTruncate(nsIURI *aURI, const nsACString & aIdExtension,
+                                         nsICacheEntry **aCacheEntry)
+{
+  if (!CacheStorageService::Self())
+    return NS_ERROR_NOT_INITIALIZED;
+
+  nsresult rv;
+
+  nsCOMPtr<nsIURI> noRefURI;
+  rv = aURI->CloneIgnoringRef(getter_AddRefs(noRefURI));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  nsRefPtr<CacheEntryHandle> entry;
+  rv = CacheStorageService::Self()->AddStorageEntry(
+    this, noRefURI, aIdExtension,
+    true, 
+    true, 
+    getter_AddRefs(entry));
+  NS_ENSURE_SUCCESS(rv, rv);
+
+  
+  entry->Entry()->AsyncOpen(nullptr, OPEN_TRUNCATE);
+  entry.forget(aCacheEntry);
+
+  return NS_OK;
+}
+
 NS_IMETHODIMP CacheStorage::Exists(nsIURI *aURI, const nsACString & aIdExtension,
                                    bool *aResult)
 {
