680228acdb277ea68e88ba249f8be275c9edc405
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:15:09 +0000

    bug 1174704 - Implement GetUserDataDirectoryHome for iOS. r=froydnj
    UltraBlame original commit: 783903e5da3b3e33a52458be4d44d5359e3d013e

---
+++ toolkit/xre/UIKitDirProvider.h
@@ -0,0 +1,13 @@
+
+
+
+
+
+#ifndef TOOLKIT_XRE_UIKITDIRPROVIDER_H_
+#define TOOLKIT_XRE_UIKITDIRPROVIDER_H_
+
+#include "nsString.h"
+
+bool GetUIKitDirectory(bool aLocal, nsACString& aUserDir);
+
+#endif 
