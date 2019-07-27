3c818ec117cb8be22f1b803d3cbff0fb372380a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:40:34 +0000

    Bug 1146304 - Touch slider bar or tap forward button, the video got stuck 1s then resume playing. r=cpearce
    UltraBlame original commit: 2bb03bbdbd79e5419ede588103e7e123c7bf9f5f

--- dom/media/MediaDecoderStateMachine.cpp
+++ dom/media/MediaDecoderStateMachine.cpp
@@ -2389,8 +2389,7 @@ MediaDecoderStateMachine::SeekCompleted()
     
     int64_t videoStart = video ? video->mTime : seekTime;
     int64_t audioStart = audio ? audio->mTime : seekTime;
-    newCurrentTime = mAudioStartTime =
-        std::min(std::min(audioStart, videoStart), seekTime);
+    newCurrentTime = mAudioStartTime = std::min(audioStart, videoStart);
   } else {
     newCurrentTime = video ? video->mTime : seekTime;
   }
