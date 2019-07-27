44a747b7636d4ddf8110e46d0e430b35571e6305
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:26:48 +0000

    Bug 1109457 - Add persistent session to our ClearKey CDM. r=edwin
    UltraBlame original commit: a0ba2f134695239e129589268bdb619be8b8fee4

---
+++ media/gmp-clearkey/0.1/ClearKeyStorage.h
@@ -0,0 +1,36 @@
+
+
+
+
+#ifndef __ClearKeyStorage_h__
+#define __ClearKeyStorage_h__
+
+#include "gmp-errors.h"
+#include "gmp-platform.h"
+#include <string>
+#include <vector>
+#include <stdint.h>
+
+class GMPTask;
+
+
+void StoreData(const std::string& aRecordName,
+               const std::vector<uint8_t>& aData,
+               GMPTask* aOnSuccess,
+               GMPTask* aOnFailure);
+
+class ReadContinuation {
+public:
+  virtual void ReadComplete(GMPErr aStatus,
+                            const uint8_t* aData,
+                            uint32_t aLength) = 0;
+  virtual ~ReadContinuation() {}
+};
+
+
+void ReadData(const std::string& aSessionId,
+              ReadContinuation* aContinuation);
+
+GMPErr EnumRecordNames(RecvGMPRecordIteratorPtr aRecvIteratorFunc);
+
+#endif 
