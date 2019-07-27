2a5b953a1ebbea0f4429bf79711e8649ae05eb31
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:27:16 +0000

    Bug 1143653 - Backout part of the initial patch because of crashes.
    UltraBlame original commit: 4460210c41b57e7c15c2a93dab6639f09217f81c

--- gfx/layers/d3d9/CompositorD3D9.cpp
+++ gfx/layers/d3d9/CompositorD3D9.cpp
@@ -590,7 +590,6 @@ CompositorD3D9::Ready()
       CheckResetCount();
       return true;
     }
-    FailedToResetDevice();
     return false;
   }
 
@@ -614,9 +613,6 @@ CompositorD3D9::Ready()
 void
 CompositorD3D9::FailedToResetDevice() {
   mFailedResetAttempts += 1;
-  auto withoutAssertion = CriticalLog::DefaultOptions(false);
-  gfxCriticalError(withoutAssertion) << "[D3D9] Failed to re-create a D3D9 device, attempt "
-                                     << mFailedResetAttempts;
   
   
   if (mFailedResetAttempts > 10) {
