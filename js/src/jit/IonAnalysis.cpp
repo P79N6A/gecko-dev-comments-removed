3c11ae0befd62b600505ca270ef08eb8b3439754
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:38:34 +0000

    Bug 1169594 - Don't add pointless KeepAlive instructions after constants. r=nbp
    UltraBlame original commit: 61e7c3734c53dda2d284bd8cbbd1b2e20aa8c762

--- js/src/jit/IonAnalysis.cpp
+++ js/src/jit/IonAnalysis.cpp
@@ -2805,6 +2805,12 @@ jit::AddKeepAliveInstructions(MIRGraph& graph)
 
             MOZ_ASSERT(ownerObject->type() == MIRType_Object);
 
+            if (ownerObject->isConstant()) {
+                
+                
+                continue;
+            }
+
             for (MUseDefIterator uses(ins); uses; uses++) {
                 MInstruction* use = uses.def()->toInstruction();
 
