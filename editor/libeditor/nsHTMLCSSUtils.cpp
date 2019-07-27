ca284652e60b2e8c44372defd0d4adaf8513c7c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:35:03 +0000

    Bug 1141017 - resurrect serif and monospace. r=ehsan
    UltraBlame original commit: a474e9f2c5cd52b83aadd7f5c0948eefeab375bc

--- editor/libeditor/nsHTMLCSSUtils.cpp
+++ editor/libeditor/nsHTMLCSSUtils.cpp
@@ -1166,11 +1166,7 @@ nsHTMLCSSUtils::IsCSSEquivalentToHTMLInlineStyleSet(nsIDOMNode *aNode,
         aIsSet = htmlValueString.Equals(valueStringNorm,
                                         nsCaseInsensitiveStringComparator());
       } else {
-        
-        nsAutoString valueStringLower;
-        ToLowerCase(valueString, valueStringLower);
-        aIsSet = !valueStringLower.EqualsLiteral("monospace") &&
-                 !valueStringLower.EqualsLiteral("serif");
+        aIsSet = true;
       }
       return NS_OK;
     } else if (aHTMLAttribute && aHTMLAttribute->EqualsLiteral("align")) {
