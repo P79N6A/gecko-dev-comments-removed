8677904cc867c364652ed6318ff90eb72c95792e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:26:34 +0000

    Bug 1113086 - AudioChannel policy in Browser API - patch 7 - Support non-app iframe, r=alwu
    UltraBlame original commit: 200c185e4d71047906be3386074158e4309ca23b

--- dom/html/nsBrowserElement.cpp
+++ dom/html/nsBrowserElement.cpp
@@ -520,14 +520,21 @@ nsBrowserElement::GetAllowedAudioChannels(
 {
   aAudioChannels.Clear();
 
-  NS_ENSURE_TRUE_VOID(IsBrowserElementOrThrow(aRv));
-  NS_ENSURE_TRUE_VOID(IsNotWidgetOrThrow(aRv));
-
   
   if (mBrowserElementAudioChannels.IsEmpty()) {
     nsCOMPtr<nsIFrameLoader> frameLoader = GetFrameLoader();
     if (!frameLoader) {
       aRv.Throw(NS_ERROR_FAILURE);
+      return;
+    }
+
+    bool isBrowserOrApp;
+    aRv = frameLoader->GetOwnerIsBrowserOrAppFrame(&isBrowserOrApp);
+    if (NS_WARN_IF(aRv.Failed())) {
+      return;
+    }
+
+    if (!isBrowserOrApp) {
       return;
     }
 
@@ -587,14 +594,6 @@ nsBrowserElement::GetAllowedAudioChannels(
       return;
     }
 
-    bool noapp = false;
-    Preferences::GetBool("dom.testing.browserElementAudioChannel.noapp", &noapp);
-
-    if (!noapp && !app) {
-      aRv.Throw(NS_ERROR_DOM_SECURITY_ERR);
-      return;
-    }
-
     
     nsTArray<nsRefPtr<BrowserElementAudioChannel>> channels;
 
@@ -609,7 +608,6 @@ nsBrowserElement::GetAllowedAudioChannels(
 
     channels.AppendElement(ac);
 
-    
     if (app) {
       const nsAttrValue::EnumTable* audioChannelTable =
         AudioChannelService::GetAudioChannelTable();
