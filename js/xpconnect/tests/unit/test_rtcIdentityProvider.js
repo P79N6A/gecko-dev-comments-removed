3308b5e6945dbacc224f3960131171a55905deff
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:13:12 +0000

    Bug 975144 - Tests for rtcIdentityProvider property, r=gabor
    UltraBlame original commit: 8fc35500a37b8b6ca2c2f4e62fd1ff645f80fa89

---
+++ js/xpconnect/tests/unit/test_rtcIdentityProvider.js
@@ -0,0 +1,35 @@
+
+
+
+function run_test() {
+  let Cu = Components.utils;
+  let sb = new Cu.Sandbox('https://www.example.com',
+                          { wantGlobalProperties: ['rtcIdentityProvider'] });
+
+  function exerciseInterface() {
+    equal(typeof rtcIdentityProvider, 'object');
+    equal(typeof rtcIdentityProvider.register, 'function');
+    rtcIdentityProvider.register({
+      generateAssertion: function(a, b, c) {
+        return Promise.resolve({
+          idp: { domain: 'example.com' },
+          assertion: JSON.stringify([a, b, c])
+        });
+      },
+      validateAssertion: function(d, e) {
+        return Promise.resolve({
+          identity: 'user@example.com',
+          contents: JSON.stringify([d, e])
+        });
+      }
+    });
+  }
+
+  sb.equal = equal;
+  Cu.evalInSandbox('(' + exerciseInterface.toSource() + ')();', sb);
+  ok(sb.rtcIdentityProvider.idp);
+
+  Cu.importGlobalProperties(['rtcIdentityProvider']);
+  exerciseInterface();
+  ok(rtcIdentityProvider.idp);
+}
