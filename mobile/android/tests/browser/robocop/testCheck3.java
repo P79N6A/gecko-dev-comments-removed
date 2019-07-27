930337c877fd86bde602d8269c35bb6725b1e450
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:34:22 +0000

    Bug 1176579 - Create a clone of testCheck2.java that points to an autophone friendly url. r=bc
    UltraBlame original commit: 2f28e9e0009ad62462063ee73c3ba43732ede460

---
+++ mobile/android/tests/browser/robocop/testCheck3.java
@@ -0,0 +1,69 @@
+
+
+
+
+package org.mozilla.gecko.tests;
+
+import org.json.JSONObject;
+
+public class testCheck3 extends PixelTest {
+    @Override
+    protected Type getTestType() {
+        return Type.TALOS;
+    }
+
+    public void testCheck3() {
+        String url = getAbsoluteUrl("/facebook.com/www.facebook.com/barackobama.html");
+
+        
+        JSONObject jsonPref = new JSONObject();
+        try {
+            jsonPref.put("name", "browser.ui.zoom.force-user-scalable");
+            jsonPref.put("type", "bool");
+            jsonPref.put("value", true);
+            setPreferenceAndWaitForChange(jsonPref);
+        } catch (Exception ex) {
+            mAsserter.ok(false, "exception in testCheck3", ex.toString());
+        }
+
+        blockForGeckoReady();
+        loadAndPaint(url);
+
+        mDriver.setupScrollHandling();
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        MotionEventReplayer mer = new MotionEventReplayer(getInstrumentation(), mDriver.getGeckoLeft(), mDriver.getGeckoTop(),
+                mDriver.getGeckoWidth(), mDriver.getGeckoHeight());
+
+        float completeness = 0.0f;
+        mDriver.startCheckerboardRecording();
+        
+        try {
+            mer.replayEvents(getAsset("testcheck2-motionevents"));
+            
+            Thread.sleep(1000);
+            completeness = mDriver.stopCheckerboardRecording();
+        } catch (Exception e) {
+            e.printStackTrace();
+            mAsserter.ok(false, "Exception while replaying events", e.toString());
+        }
+
+        mAsserter.dumpLog("__start_report" + completeness + "__end_report");
+        System.out.println("Completeness score: " + completeness);
+        long msecs = System.currentTimeMillis();
+        mAsserter.dumpLog("__startTimestamp" + msecs + "__endTimestamp");
+    }
+}
