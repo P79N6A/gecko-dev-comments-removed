141163f82b9450a60a5b79a459948aafe0d31877
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:36:03 +0000

    Bug 1109390 part 23 - Add aborted pause behavior; r=jwatt
    When a pending pause operation is interrupted by a play operation we should
    preserve the original start time of the animation so that it appears to continue
    moving uninterrupted. At the same time, however, for consistency with other
    calls to play(), the operation should complete asynchronously.
    
    UltraBlame original commit: 2273676edfc27e63cd8ae21e8e527a5d148b00a3

--- dom/animation/AnimationPlayer.cpp
+++ dom/animation/AnimationPlayer.cpp
@@ -429,6 +429,8 @@ AnimationPlayer::ComposeStyle(nsRefPtr<css::AnimValuesStyleRule>& aStyleRule,
 void
 AnimationPlayer::DoPlay(LimitBehavior aLimitBehavior)
 {
+  bool abortedPause = mPendingState == PendingState::PausePending;
+
   bool reuseReadyPromise = false;
   if (mPendingState != PendingState::NotPending) {
     CancelPendingTasks();
@@ -452,12 +454,20 @@ AnimationPlayer::DoPlay(LimitBehavior aLimitBehavior)
     mHoldTime.SetValue(TimeDuration(0));
   }
 
-  if (mHoldTime.IsNull()) {
+  
+  
+  
+  
+  if (mHoldTime.IsNull() && !abortedPause) {
     return;
   }
 
   
-  mStartTime.SetNull();
+  
+  
+  if (!abortedPause) {
+    mStartTime.SetNull();
+  }
 
   if (!reuseReadyPromise) {
     
@@ -513,15 +523,19 @@ AnimationPlayer::ResumeAt(const TimeDuration& aReadyTime)
   
   MOZ_ASSERT(mPendingState == PendingState::PlayPending,
              "Expected to resume a play-pending player");
-  MOZ_ASSERT(!mHoldTime.IsNull(),
-             "A player in the play-pending state should have a resolved"
-             " hold time");
+  MOZ_ASSERT(mHoldTime.IsNull() != mStartTime.IsNull(),
+             "A player in the play-pending state should have either a"
+             " resolved hold time or resolved start time (but not both)");
 
-  if (mPlaybackRate != 0) {
-    mStartTime.SetValue(aReadyTime - (mHoldTime.Value() / mPlaybackRate));
-    mHoldTime.SetNull();
-  } else {
-    mStartTime.SetValue(aReadyTime);
+  
+  
+  if (mStartTime.IsNull()) {
+    if (mPlaybackRate != 0) {
+      mStartTime.SetValue(aReadyTime - (mHoldTime.Value() / mPlaybackRate));
+      mHoldTime.SetNull();
+    } else {
+      mStartTime.SetValue(aReadyTime);
+    }
   }
   mPendingState = PendingState::NotPending;
 
