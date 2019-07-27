c55f185558b74bc7a6b12a25b5c0d3f958d5dadf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:02:39 +0000

    Backed out changeset 2b027c3e1736 (bug 1075531) for Android S4 Test Failures
    UltraBlame original commit: 8a110b34d05b920c0b687455969777f534a720f4

--- mobile/android/base/tests/components/ToolbarComponent.java
+++ mobile/android/base/tests/components/ToolbarComponent.java
@@ -91,7 +91,7 @@ public class ToolbarComponent extends BaseComponent {
         return (ImageButton) getToolbarView().findViewById(R.id.edit_cancel);
     }
 
-    private String getTitle() {
+    private CharSequence getTitle() {
         return getTitleHelper(true);
     }
 
@@ -100,16 +100,16 @@ public class ToolbarComponent extends BaseComponent {
 
 
 
-    public String getPotentiallyInconsistentTitle() {
+    public CharSequence getPotentiallyInconsistentTitle() {
         return getTitleHelper(false);
     }
 
-    private String getTitleHelper(final boolean shouldAssertNotEditing) {
+    private CharSequence getTitleHelper(final boolean shouldAssertNotEditing) {
         if (shouldAssertNotEditing) {
             assertIsNotEditing();
         }
 
-        return getUrlTitleText().getText().toString();
+        return getUrlTitleText().getText();
     }
 
     private boolean isEditing() {
