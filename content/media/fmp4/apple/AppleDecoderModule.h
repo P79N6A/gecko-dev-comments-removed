17b203b3ffe7f2c34f6ef9187682831a4eb02fde
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:48:35 +0000

    Bug 1022501 - Add MP3 support to MP4 demuxer. r=edwin
    UltraBlame original commit: 945fe09a569ac2d6dc77d8830be19f3fba29a63e

--- content/media/fmp4/apple/AppleDecoderModule.h
+++ content/media/fmp4/apple/AppleDecoderModule.h
@@ -34,9 +34,11 @@ public:
 
   
   virtual already_AddRefed<MediaDataDecoder>
-  CreateAACDecoder(const mp4_demuxer::AudioDecoderConfig& aConfig,
-                   MediaTaskQueue* aAudioTaskQueue,
-                   MediaDataDecoderCallback* aCallback) MOZ_OVERRIDE;
+  CreateAudioDecoder(const mp4_demuxer::AudioDecoderConfig& aConfig,
+                     MediaTaskQueue* aAudioTaskQueue,
+                     MediaDataDecoderCallback* aCallback) MOZ_OVERRIDE;
+
+  virtual bool SupportsAudioMimeType(const char* aMimeType) MOZ_OVERRIDE;
 
   static void Init();
 private:
