a0bb2ebe2b02e71802c572352e60bfc30eade900
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:56:13 +0000

    Bug 1161183: Don't show the add-on version in the list view. r=dao
    UltraBlame original commit: fe87e9025dc9885f30ad31c106a26ff305ab3204

--- toolkit/mozapps/extensions/content/extensions.js
+++ toolkit/mozapps/extensions/content/extensions.js
@@ -460,6 +460,31 @@ var gEventManager = {
       menuSep.hidden = (countMenuItemsBeforeSep == 0);
 
     }, false);
+
+    let addonTooltip = document.getElementById("addonitem-tooltip");
+    addonTooltip.addEventListener("popupshowing", function() {
+      let addonItem = document.tooltipNode;
+      
+      
+      
+      if (addonItem.localName != "richlistitem")
+        addonItem = document.getBindingParent(addonItem);
+
+      let tiptext = addonItem.getAttribute("name");
+
+      if (addonItem.mAddon) {
+        if (shouldShowVersionNumber(addonItem.mAddon)) {
+          tiptext += " " + (addonItem.hasAttribute("upgrade") ? addonItem.mManualUpdate.version
+                                                              : addonItem.mAddon.version);
+        }
+      }
+      else {
+        if (shouldShowVersionNumber(addonItem.mInstall))
+          tiptext += " " + addonItem.mInstall.version;
+      }
+
+      addonTooltip.label = tiptext;
+    }, false);
   },
 
   shutdown: function gEM_shutdown() {
@@ -1446,6 +1471,10 @@ function isInState(aInstall, aState) {
 
 function shouldShowVersionNumber(aAddon) {
   if (!aAddon.version)
+    return false;
+
+  
+  if (aAddon.type == "experiment")
     return false;
 
   
