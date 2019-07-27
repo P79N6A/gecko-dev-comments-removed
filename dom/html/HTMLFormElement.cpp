87eeda4934a7d2f6417a0b3811eeba7db1390d59
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:08:11 +0000

    Bug 529697 - (CSP 1.1) Implement form-action directive [2/4], r=smaug
    Check CSP when submitting HTML forms.
    
    UltraBlame original commit: 5a9dc311e76f5b6dcbcd4e141a6dd205bfbee2bb

--- dom/html/HTMLFormElement.cpp
+++ dom/html/HTMLFormElement.cpp
@@ -61,6 +61,8 @@
 #include "nsIDOMHTMLButtonElement.h"
 #include "nsSandboxFlags.h"
 
+#include "nsIContentSecurityPolicy.h"
+
 
 #include "mozilla/dom/HTMLImageElement.h"
 
@@ -1624,6 +1626,19 @@ HTMLFormElement::GetActionURL(nsIURI** aActionURL,
     CheckLoadURIWithPrincipal(NodePrincipal(), actionURL,
                               nsIScriptSecurityManager::STANDARD);
   NS_ENSURE_SUCCESS(rv, rv);
+
+  
+  nsCOMPtr<nsIContentSecurityPolicy> csp;
+  rv = NodePrincipal()->GetCsp(getter_AddRefs(csp));
+  NS_ENSURE_SUCCESS(rv, rv);
+  if (csp) {
+    bool permitsFormAction = true;
+    rv = csp->PermitsFormAction(actionURL, &permitsFormAction);
+    NS_ENSURE_SUCCESS(rv, rv);
+    if (!permitsFormAction) {
+      rv = NS_ERROR_CSP_FORM_ACTION_VIOLATION;
+    }
+  }
 
   
   
