e2eb2d51955c34213e9fa5e94666440a8b544020
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:20:42 +0000

    Bug 1140723 - Enable vsync compositor on Windows. r=kats
    UltraBlame original commit: d3380179efc16e5f2c50b566b8323221938f2329

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -4628,13 +4628,9 @@ pref("media.gmp.insecure.allow", false);
 
 
 
-#if defined(XP_MACOSX)
+#if defined(XP_MACOSX) || defined(XP_WIN)
 pref("gfx.vsync.hw-vsync.enabled", true);
 pref("gfx.vsync.compositor", true);
-pref("gfx.vsync.refreshdriver", false);
-#elif defined(XP_WIN)
-pref("gfx.vsync.hw-vsync.enabled", false);
-pref("gfx.vsync.compositor", false);
 pref("gfx.vsync.refreshdriver", false);
 #endif
 
