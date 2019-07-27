a74b8d20a120e25d27aa297c2b9896503464a763
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:03:45 +0000

    Bug 1074817 - Handle content preference downgrades for the timestamp column migration. r=MattN f=mak
    UltraBlame original commit: 42a182f0431fb873eeba2bde9ec240db78680cbe

--- toolkit/components/contentprefs/nsContentPrefService.js
+++ toolkit/components/contentprefs/nsContentPrefService.js
@@ -1167,6 +1167,13 @@ ContentPrefService.prototype = {
   },
 
   _dbMigrate: function ContentPrefService__dbMigrate(aDBConnection, aOldVersion, aNewVersion) {
+    
+
+
+
+
+
+
     aDBConnection.beginTransaction();
 
     try {
@@ -1213,7 +1220,14 @@ ContentPrefService.prototype = {
   },
 
   _dbMigrate3To4: function ContentPrefService__dbMigrate3To4(aDBConnection) {
-    aDBConnection.executeSimpleSQL("ALTER TABLE prefs ADD COLUMN timestamp INTEGER NOT NULL DEFAULT 0");
+    
+    try {
+      let stmt = aDBConnection.createStatement("SELECT timestamp FROM prefs");
+      stmt.finalize();
+    } catch (e) {
+      aDBConnection.executeSimpleSQL("ALTER TABLE prefs ADD COLUMN timestamp INTEGER NOT NULL DEFAULT 0");
+    }
+
     
     aDBConnection.executeSimpleSQL("DROP INDEX IF EXISTS prefs_idx");
     this._dbCreateIndices(aDBConnection);
