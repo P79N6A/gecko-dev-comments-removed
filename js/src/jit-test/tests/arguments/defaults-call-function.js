292ae9116ff159401f4dc10dfd755fedbd7d23b5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:12:44 +0000

    Bug 1022962 - Part 2: Add test for function call inside default parameter. r=jorendorff
    UltraBlame original commit: cecd74a436449ec6a43eb0bb9e211335d93813e3

---
+++ js/src/jit-test/tests/arguments/defaults-call-function.js
@@ -0,0 +1,16 @@
+load(libdir + "asserts.js");
+
+function f1(a=g()) {
+  function g() {
+  }
+}
+
+assertThrowsInstanceOf(f1, TypeError);
+
+function f2(a=g()) {
+  function g() {
+    return 43;
+  }
+  assertEq(a, 42);
+}
+f2(42);
