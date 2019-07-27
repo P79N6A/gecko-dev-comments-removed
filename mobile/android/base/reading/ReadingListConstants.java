e220730535e32af541ee6197ac187696b9963d54
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:21:26 +0000

    Bug 1140241 - Add ReadingListConstants.
    UltraBlame original commit: 4663931493f9d360b59d44be5e882b094995f296

---
+++ mobile/android/base/reading/ReadingListConstants.java
@@ -0,0 +1,14 @@
+
+
+
+
+package org.mozilla.gecko.reading;
+
+import org.mozilla.gecko.AppConstants;
+
+public class ReadingListConstants {
+  public static final String GLOBAL_LOG_TAG = "FxReadingList";
+  public static final String USER_AGENT = "Firefox-Android-FxReader/" + AppConstants.MOZ_APP_VERSION + " (" + AppConstants.MOZ_APP_DISPLAYNAME + ")";
+  public static final String DEFAULT_DEV_ENDPOINT = "https://readinglist.dev.mozaws.net/v1/";
+  public static final String DEFAULT_PROD_ENDPOINT = null;     
+}
