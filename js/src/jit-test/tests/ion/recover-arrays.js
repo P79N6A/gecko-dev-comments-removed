646ef2ff3213aa39ad2615b7ee8b0902f28c914d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:32:48 +0000

    Bug 1040027 part 1 - Detect when we can replace an Array by an ArrayState. r=jandem
    UltraBlame original commit: eb591b79d451aaaefa4e3ca4272c045684601e5e

---
+++ js/src/jit-test/tests/ion/recover-arrays.js
@@ -0,0 +1,247 @@
+
+
+
+
+
+var resumeHere = function (i) { if (i >= 99) bailout(); };
+
+
+
+
+var uceFault = function (i) {
+    if (i > 98)
+        uceFault = function (i) { return true; };
+    return false;
+};
+
+
+
+var global_arr;
+function escape(arr) { global_arr = arr; }
+
+
+function array0Length(i) {
+    var a = [];
+    return a.length;
+}
+
+function array0LengthBail(i) {
+    var a = [];
+    resumeHere(i);
+    return a.length;
+}
+
+function array1Length(i) {
+    var a = [ i ];
+    return a.length;
+}
+
+function array1LengthBail(i) {
+    var a = [ i ];
+    resumeHere(i);
+    return a.length;
+}
+
+function array2Length(i) {
+    var a = [ i, i ];
+    return a.length;
+}
+
+function array2LengthBail(i) {
+    var a = [ i, i ];
+    resumeHere(i);
+    return a.length;
+}
+
+
+
+function arrayWithGCInit(i) {
+    var a = [ gc(), gc() ];
+    gc();
+    return a.length;
+}
+
+
+function array1Content(i) {
+    var a = [ i ];
+    assertEq(a[0], i);
+    return a.length;
+}
+function array1ContentBail0(i) {
+    var a = [ i ];
+    resumeHere(i);
+    assertEq(a[0], i);
+    return a.length;
+}
+function array1ContentBail1(i) {
+    var a = [ i ];
+    assertEq(a[0], i);
+    resumeHere(i);
+    return a.length;
+}
+
+function array2Content(i) {
+    var a = [ i, i ];
+    assertEq(a[0], i);
+    assertEq(a[1], i);
+    return a.length;
+}
+
+function array2ContentBail0(i) {
+    var a = [ i, i ];
+    resumeHere(i);
+    assertEq(a[0], i);
+    assertEq(a[1], i);
+    return a.length;
+}
+
+function array2ContentBail1(i) {
+    var a = [ i, i ];
+    assertEq(a[0], i);
+    resumeHere(i);
+    assertEq(a[1], i);
+    return a.length;
+}
+
+function array2ContentBail2(i) {
+    var a = [ i, i ];
+    assertEq(a[0], i);
+    assertEq(a[1], i);
+    resumeHere(i);
+    return a.length;
+}
+
+
+function arrayInitBail0(i) {
+    var a = [ resumeHere(i), i ];
+    return a.length;
+}
+
+function arrayInitBail1(i) {
+    var a = [ i, resumeHere(i) ];
+    return a.length;
+}
+
+
+function arrayLarge0(i) {
+    var a = new Array(10000000);
+    resumeHere(); bailout(); 
+    return a.length;
+}
+
+function arrayLarge1(i) {
+    var a = new Array(10000000);
+    a[0] = i;
+    assertEq(a[0], i);
+    return a.length;
+}
+
+function arrayLarge2(i) {
+    var a = new Array(10000000);
+    a[0] = i;
+    a[100] = i;
+    assertEq(a[0], i);
+    assertEq(a[100], i);
+    return a.length;
+}
+
+
+function arrayHole0(i) {
+    var a = [i,,i];
+    if (i != 99)
+        a[1] = i;
+    assertEq(a[0], i);
+    assertEq(a[1], i != 99 ? i : undefined);
+    assertEq(a[2], i);
+    return a.length;
+}
+
+
+
+function arrayHole1(i) {
+    var a = [i,,i];
+    if (i != 99)
+        a[1] = i;
+    assertEq(a[0], i);
+    assertEq(a[1], i != 99 ? i : 100);
+    assertEq(a[2], i);
+    return a.length;
+}
+
+
+var uceFault_arrayAlloc0 = eval(uneval(uceFault).replace('uceFault', 'uceFault_arrayAlloc0'));
+function arrayAlloc0(i) {
+    var a = new Array(10000000);
+    if (uceFault_arrayAlloc0(i) || uceFault_arrayAlloc0(i)) {
+        return a.length;
+    }
+    return 0;
+}
+
+var uceFault_arrayAlloc1 = eval(uneval(uceFault).replace('uceFault', 'uceFault_arrayAlloc1'));
+function arrayAlloc1(i) {
+    var a = new Array(10000000);
+    if (uceFault_arrayAlloc0(i) || uceFault_arrayAlloc0(i)) {
+        a[0] = i;
+        a[1] = i;
+        assertEq(a[0], i);
+        assertEq(a[1], i);
+        assertEq(a[2], undefined);
+        return a.length;
+    }
+    return 0;
+}
+
+var uceFault_arrayAlloc2 = eval(uneval(uceFault).replace('uceFault', 'uceFault_arrayAlloc2'));
+function arrayAlloc2(i) {
+    var a = new Array(10000000);
+    if (uceFault_arrayAlloc0(i) || uceFault_arrayAlloc0(i)) {
+        a[4096] = i;
+        assertEq(a[0], undefined);
+        assertEq(a[4096], i);
+        return a.length;
+    }
+    return 0;
+}
+
+
+eval(uneval(resumeHere));
+
+for (var i = 0; i < 100; i++) {
+    array0Length(i);
+    array0LengthBail(i);
+    array1Length(i);
+    array1LengthBail(i);
+    array2Length(i);
+    array2LengthBail(i);
+    arrayWithGCInit(i);
+    array1Content(i);
+    array1ContentBail0(i);
+    array1ContentBail1(i);
+    array2Content(i);
+    array2ContentBail0(i);
+    array2ContentBail1(i);
+    array2ContentBail2(i);
+    arrayInitBail0(i);
+    arrayInitBail1(i);
+    arrayLarge0(i);
+    arrayLarge1(i);
+    arrayLarge2(i);
+    arrayHole0(i);
+    arrayAlloc0(i);
+    arrayAlloc1(i);
+    arrayAlloc2(i);
+}
+
+
+
+Object.defineProperty(Array.prototype, 1, {
+  value: 100,
+  configurable: true,
+  enumerable: true,
+  writable: true
+});
+
+for (var i = 0; i < 100; i++) {
+    arrayHole1(i);
+}
--- js/src/jit/ScalarReplacement.cpp
+++ js/src/jit/ScalarReplacement.cpp
@@ -116,8 +116,10 @@ IsObjectEscaped(MInstruction *ins)
     return false;
 }
 
-typedef MObjectState BlockState;
-typedef Vector<BlockState *, 8, SystemAllocPolicy> GraphState;
+struct ObjectTrait {
+    typedef MObjectState BlockState;
+    typedef Vector<BlockState *, 8, SystemAllocPolicy> GraphState;
+};
 
 
 
@@ -126,9 +128,12 @@ typedef Vector<BlockState *, 8, SystemAllocPolicy> GraphState;
 
 
 static bool
-ScalarReplacementOfObject(MIRGenerator *mir, MIRGraph &graph, GraphState &states,
+ScalarReplacementOfObject(MIRGenerator *mir, MIRGraph &graph,
+                          ObjectTrait::GraphState &states,
                           MInstruction *obj)
 {
+    typedef ObjectTrait::BlockState BlockState;
+
     
     
     if (!states.appendN(nullptr, graph.numBlocks()))
@@ -339,10 +344,406 @@ ScalarReplacementOfObject(MIRGenerator *mir, MIRGraph &graph, GraphState &states
     return true;
 }
 
+static bool
+IndexOf(MDefinition *ins, int32_t *res)
+{
+    MOZ_ASSERT(ins->isLoadElement() || ins->isStoreElement());
+    MDefinition *indexDef = ins->getOperand(1); 
+    if (indexDef->isBoundsCheck())
+        indexDef = indexDef->toBoundsCheck()->index();
+    if (indexDef->isToInt32())
+        indexDef = indexDef->toToInt32()->getOperand(0);
+    if (!indexDef->isConstant())
+        return false;
+
+    Value index = indexDef->toConstant()->value();
+    if (!index.isInt32())
+        return false;
+    *res = index.toInt32();
+    return true;
+}
+
+
+
+
+
+
+
+static bool
+IsArrayEscaped(MInstruction *ins)
+{
+    MOZ_ASSERT(ins->type() == MIRType_Object);
+    MOZ_ASSERT(ins->isNewArray());
+    uint32_t count = ins->toNewArray()->count();
+
+    
+    
+    if (!ins->toNewArray()->isAllocating() || count >= 16)
+        return true;
+
+    
+    
+    
+    for (MUseIterator i(ins->usesBegin()); i != ins->usesEnd(); i++) {
+        MNode *consumer = (*i)->consumer();
+        if (!consumer->isDefinition()) {
+            
+            if (consumer->toResumePoint()->isObservableOperand(*i))
+                return true;
+            continue;
+        }
+
+        MDefinition *def = consumer->toDefinition();
+        switch (def->op()) {
+          case MDefinition::Op_Elements: {
+            MOZ_ASSERT(def->toElements()->object() == ins);
+            for (MUseIterator i(def->usesBegin()); i != def->usesEnd(); i++) {
+                
+                
+                MDefinition *access = (*i)->consumer()->toDefinition();
+
+                switch (access->op()) {
+                  case MDefinition::Op_LoadElement: {
+                    MOZ_ASSERT(access->toLoadElement()->elements() == def);
+
+                    
+                    
+                    
+                    
+                    
+                    if (access->toLoadElement()->needsHoleCheck())
+                        return true;
+
+                    
+                    
+                    int32_t index;
+                    if (!IndexOf(access, &index))
+                        return true;
+                    if (index < 0 || count <= index)
+                        return true;
+                    break;
+                  }
+
+                  case MDefinition::Op_StoreElement: {
+                    MOZ_ASSERT(access->toStoreElement()->elements() == def);
+
+                    
+                    
+                    
+                    
+                    
+                    if (access->toStoreElement()->needsHoleCheck())
+                        return true;
+
+                    
+                    
+                    int32_t index;
+                    if (!IndexOf(access, &index))
+                        return true;
+                    if (index < 0 || count <= index)
+                        return true;
+
+                    
+                    if (access->toStoreElement()->value()->type() == MIRType_MagicHole)
+                        return true;
+                    break;
+                  }
+
+                  case MDefinition::Op_SetInitializedLength:
+                    MOZ_ASSERT(access->toSetInitializedLength()->elements() == def);
+                    break;
+
+                  case MDefinition::Op_InitializedLength:
+                    MOZ_ASSERT(access->toInitializedLength()->elements() == def);
+                    break;
+
+                  case MDefinition::Op_ArrayLength:
+                    MOZ_ASSERT(access->toArrayLength()->elements() == def);
+                    break;
+
+                  default:
+                    return true;
+                }
+            }
+
+            break;
+          }
+
+          default:
+            return true;
+        }
+    }
+
+    return false;
+}
+
+struct ArrayTrait {
+    typedef MArrayState BlockState;
+    typedef Vector<BlockState *, 8, SystemAllocPolicy> GraphState;
+};
+
+
+
+
+
+
+
+static bool
+ScalarReplacementOfArray(MIRGenerator *mir, MIRGraph &graph,
+                         ArrayTrait::GraphState &states,
+                         MInstruction *arr)
+{
+    typedef ArrayTrait::BlockState BlockState;
+
+    
+    
+    if (!states.appendN(nullptr, graph.numBlocks()))
+        return false;
+
+    
+    MBasicBlock *arrBlock = arr->block();
+    MConstant *undefinedVal = MConstant::New(graph.alloc(), UndefinedValue());
+    MConstant *initLength = MConstant::New(graph.alloc(), Int32Value(0));
+    MConstant *length = nullptr;
+    arrBlock->insertBefore(arr, undefinedVal);
+    arrBlock->insertBefore(arr, initLength);
+    states[arrBlock->id()] = BlockState::New(graph.alloc(), arr, undefinedVal, initLength);
+
+    
+    for (ReversePostorderIterator block = graph.rpoBegin(arr->block()); block != graph.rpoEnd(); block++) {
+        if (mir->shouldCancel("Scalar Replacement of Array"))
+            return false;
+
+        BlockState *state = states[block->id()];
+        if (!state) {
+            MOZ_ASSERT(!arrBlock->dominates(*block));
+            continue;
+        }
+
+        
+        
+        if (*block == arrBlock)
+            arrBlock->insertAfter(arr, state);
+        else if (block->numPredecessors() > 1)
+            block->insertBefore(*block->begin(), state);
+        else
+            MOZ_ASSERT(state->block()->dominates(*block));
+
+        
+        ReplaceResumePointOperands(block->entryResumePoint(), arr, state);
+
+        for (MDefinitionIterator ins(*block); ins; ) {
+            switch (ins->op()) {
+              case MDefinition::Op_ArrayState: {
+                ins++;
+                continue;
+              }
+
+              case MDefinition::Op_LoadElement: {
+                MLoadElement *def = ins->toLoadElement();
+
+                
+                MDefinition *elements = def->elements();
+                if (!elements->isElements())
+                    break;
+                if (elements->toElements()->object() != arr)
+                    break;
+
+                
+                int32_t index;
+                MOZ_ALWAYS_TRUE(IndexOf(def, &index));
+                ins->replaceAllUsesWith(state->getElement(index));
+
+                
+                ins = block->discardDefAt(ins);
+                if (!elements->hasLiveDefUses())
+                    elements->block()->discard(elements->toInstruction());
+                continue;
+              }
+
+              case MDefinition::Op_StoreElement: {
+                MStoreElement *def = ins->toStoreElement();
+
+                
+                MDefinition *elements = def->elements();
+                if (!elements->isElements())
+                    break;
+                if (elements->toElements()->object() != arr)
+                    break;
+
+                
+                int32_t index;
+                MOZ_ALWAYS_TRUE(IndexOf(def, &index));
+                state = BlockState::Copy(graph.alloc(), state);
+                state->setElement(index, def->value());
+                block->insertBefore(ins->toInstruction(), state);
+
+                
+                ins = block->discardDefAt(ins);
+                if (!elements->hasLiveDefUses())
+                    elements->block()->discard(elements->toInstruction());
+                continue;
+              }
+
+              case MDefinition::Op_SetInitializedLength: {
+                MSetInitializedLength *def = ins->toSetInitializedLength();
+
+                
+                MDefinition *elements = def->elements();
+                if (!elements->isElements())
+                    break;
+                if (elements->toElements()->object() != arr)
+                    break;
+
+                
+                
+                
+                
+                
+                state = BlockState::Copy(graph.alloc(), state);
+                int32_t initLengthValue = def->index()->toConstant()->value().toInt32() + 1;
+                MConstant *initLength = MConstant::New(graph.alloc(), Int32Value(initLengthValue));
+                block->insertBefore(ins->toInstruction(), initLength);
+                block->insertBefore(ins->toInstruction(), state);
+                state->setInitializedLength(initLength);
+
+                
+                ins = block->discardDefAt(ins);
+                if (!elements->hasLiveDefUses())
+                    elements->block()->discard(elements->toInstruction());
+                continue;
+              }
+
+              case MDefinition::Op_InitializedLength: {
+                MInitializedLength *def = ins->toInitializedLength();
+
+                
+                MDefinition *elements = def->elements();
+                if (!elements->isElements())
+                    break;
+                if (elements->toElements()->object() != arr)
+                    break;
+
+                
+                ins->replaceAllUsesWith(state->initializedLength());
+
+                
+                ins = block->discardDefAt(ins);
+                if (!elements->hasLiveDefUses())
+                    elements->block()->discard(elements->toInstruction());
+                continue;
+              }
+
+              case MDefinition::Op_ArrayLength: {
+                MArrayLength *def = ins->toArrayLength();
+
+                
+                MDefinition *elements = def->elements();
+                if (!elements->isElements())
+                    break;
+                if (elements->toElements()->object() != arr)
+                    break;
+
+                
+                if (!length) {
+                    length = MConstant::New(graph.alloc(), Int32Value(state->numElements()));
+                    arrBlock->insertBefore(arr, length);
+                }
+                ins->replaceAllUsesWith(length);
+
+                
+                ins = block->discardDefAt(ins);
+                if (!elements->hasLiveDefUses())
+                    elements->block()->discard(elements->toInstruction());
+                continue;
+              }
+
+              default:
+                break;
+            }
+
+            
+            if (ins->isInstruction())
+                ReplaceResumePointOperands(ins->toInstruction()->resumePoint(), arr, state);
+
+            ins++;
+        }
+
+        
+        
+        for (size_t s = 0; s < block->numSuccessors(); s++) {
+            MBasicBlock *succ = block->getSuccessor(s);
+            BlockState *succState = states[succ->id()];
+
+            
+            
+            if (!succState) {
+                
+                
+                
+                
+                
+                
+                if (!arrBlock->dominates(succ))
+                    continue;
+
+                if (succ->numPredecessors() > 1) {
+                    succState = states[succ->id()] = BlockState::Copy(graph.alloc(), state);
+                    size_t numPreds = succ->numPredecessors();
+                    for (size_t index = 0; index < state->numElements(); index++) {
+                        MPhi *phi = MPhi::New(graph.alloc());
+                        if (!phi->reserveLength(numPreds))
+                            return false;
+
+                        
+                        
+                        for (size_t p = 0; p < numPreds; p++)
+                            phi->addInput(undefinedVal);
+
+                        
+                        succ->addPhi(phi);
+                        succState->setElement(index, phi);
+                    }
+                } else {
+                    succState = states[succ->id()] = state;
+                }
+            }
+
+            if (succ->numPredecessors() > 1) {
+                
+                
+                
+                
+                size_t numPreds = succ->numPredecessors();
+                for (size_t p = 0; p < numPreds; p++) {
+                    if (succ->getPredecessor(p) != *block)
+                        continue;
+
+                    
+                    
+                    for (size_t index = 0; index < state->numElements(); index++) {
+                        MPhi *phi = succState->getElement(index)->toPhi();
+                        phi->replaceOperand(p, state->getElement(index));
+                    }
+                }
+            }
+        }
+    }
+
+    MOZ_ASSERT(!arr->hasLiveDefUses());
+    
+    
+    states.clear();
+    return true;
+}
+
+
 bool
 ScalarReplacement(MIRGenerator *mir, MIRGraph &graph)
 {
-    GraphState objectStates;
+    ObjectTrait::GraphState objectStates;
+    ArrayTrait::GraphState arrayStates;
+
     for (ReversePostorderIterator block = graph.rpoBegin(); block != graph.rpoEnd(); block++) {
         if (mir->shouldCancel("Scalar Replacement (main loop)"))
             return false;
@@ -351,6 +752,13 @@ ScalarReplacement(MIRGenerator *mir, MIRGraph &graph)
             if (ins->isNewObject() && !IsObjectEscaped(*ins)) {
                 if (!ScalarReplacementOfObject(mir, graph, objectStates, *ins))
                     return false;
+                continue;
+            }
+
+            if (ins->isNewArray() && !IsArrayEscaped(*ins)) {
+                if (!ScalarReplacementOfArray(mir, graph, arrayStates, *ins))
+                    return false;
+                continue;
             }
         }
     }
