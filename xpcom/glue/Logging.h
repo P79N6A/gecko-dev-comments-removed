5fa403a0e46b42bf3c116a7b39ded07d72e5819d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:37:36 +0000

    Bug 1165515 - Part 13-1: Add log level enum class. r=froydnj
    This adds the mozilla::LogLevel enum class. Additionaly a log_test function is
    added to use rather than a macro, this allows us to enforce only
    mozilla::LogLevel is passed into the function.
    
    UltraBlame original commit: 4e875a4883490698623427a180f37f243d840764

--- xpcom/glue/Logging.h
+++ xpcom/glue/Logging.h
@@ -9,20 +9,59 @@
 
 #include "prlog.h"
 
+#include "mozilla/Assertions.h"
 
 
 
 
 
-#define PR_LOG_INFO PR_LOG_DEBUG
-#define PR_LOG_VERBOSE (PR_LOG_DEBUG + 1)
 
-#define MOZ_LOG PR_LOG
+namespace mozilla {
 
 
 
-#define MOZ_LOG_TEST(_module, _level) \
-  ((_module) && (_module)->level >= (_level))
 
-#endif 
+
+
+
+
+
+
+
+
+
+
+
+
+
+enum class LogLevel {
+  Disabled = 0,
+  Error,
+  Warning,
+  Info,
+  Debug,
+  Verbose,
+};
+
+namespace detail {
+
+inline bool log_test(const PRLogModuleInfo* module, LogLevel level) {
+  MOZ_ASSERT(level != LogLevel::Disabled);
+  return module && module->level >= static_cast<int>(level);
+}
+
+}
+
+}
+
+#define MOZ_LOG_TEST(_module,_level) mozilla::detail::log_test(_module, _level)
+
+#define MOZ_LOG(_module,_level,_args)     \
+  PR_BEGIN_MACRO             \
+    if (MOZ_LOG_TEST(_module,_level)) { \
+      PR_LogPrint _args;         \
+    }                     \
+  PR_END_MACRO
+
+#endif
 
