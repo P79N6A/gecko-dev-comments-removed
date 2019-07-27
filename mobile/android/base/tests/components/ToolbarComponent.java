7e7421fe4a682a5e8d5bbbc26ae7863d07a31821
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:27:51 +0000

    Bug 1105795 - Avoid ClassCastException in testAboutHomeVisibility; r=mcomella
    UltraBlame original commit: 5c191da183d62d1d52275537b200b1d6435d9165

--- mobile/android/base/tests/components/ToolbarComponent.java
+++ mobile/android/base/tests/components/ToolbarComponent.java
@@ -122,8 +122,8 @@ public class ToolbarComponent extends BaseComponent {
     
 
 
-    private ImageButton getEditCancelButton() {
-        return (ImageButton) getToolbarView().findViewById(R.id.edit_cancel);
+    private View getEditCancelButton() {
+        return getToolbarView().findViewById(R.id.edit_cancel);
     }
 
     private String getTitle() {
