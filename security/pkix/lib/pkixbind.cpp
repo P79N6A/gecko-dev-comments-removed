7786bb2978632c03293cfa9628b0927e911e4a86
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:57:27 +0000

    Bug 1029247, Part 1: Add new overload to mozilla::pkix::bind, r=keeler
    UltraBlame original commit: 6afeb8211a659384a391bcbc348929c0d1dd738d

--- security/pkix/lib/pkixbind.cpp
+++ security/pkix/lib/pkixbind.cpp
@@ -22,13 +22,15 @@
 
 
 
-#ifndef MOZILLA_PKIX_USE_REAL_FUNCTIONAL
-
 #include "pkix/bind.h"
+
+#ifndef MOZILLA_PKIX_USE_REAL_FUNCTIONAL
 
 namespace mozilla { namespace pkix {
 
 Placeholder1 _1;
+Placeholder2 _2;
+Placeholder3 _3;
 
 } } 
 
