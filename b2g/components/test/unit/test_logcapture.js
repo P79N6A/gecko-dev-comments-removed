6a66eb943fb77e996d8479fb6422706252777acd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:38:43 +0000

    Bug 1103958 - Dumping Gecko AppInfos and about:memory in LogShake. r=gwagner
    UltraBlame original commit: a5adaf7a846cbf3065b96d9b4d39c9b6cd849c02

--- b2g/components/test/unit/test_logcapture.js
+++ b2g/components/test/unit/test_logcapture.js
@@ -1,30 +1,13 @@
 
 
 
-
-
-
-
-
-
 function run_test() {
   Components.utils.import("resource:///modules/LogCapture.jsm");
-
-  function verifyLog(log) {
-    
-    notEqual(log, null);
-    
-    ok(log.length >= 0);
-  }
-
-  let propertiesLog = LogCapture.readProperties();
-  notEqual(propertiesLog, null, "Properties should not be null");
-  notEqual(propertiesLog, undefined, "Properties should not be undefined");
-  equal(propertiesLog["ro.kernel.qemu"], "1", "QEMU property should be 1");
-
-  let mainLog = LogCapture.readLogFile("/dev/log/main");
-  verifyLog(mainLog);
-
-  let meminfoLog = LogCapture.readLogFile("/proc/meminfo");
-  verifyLog(meminfoLog);
+  run_next_test();
 }
+
+
+add_test(function test_logCapture_loads() {
+  ok(LogCapture, "LogCapture object exists");
+  run_next_test();
+});
