09aa6432f59c735d3ebb3816e6cedcbc1cd7aa7c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:24:24 +0000

    Backed out 2 changesets (bug 938845) for testSettingsMenuItems NullPointerExceptions
    Backed out changeset 8e8d8f82f9de (bug 938845)
    Backed out changeset 28b3258bc0dc (bug 938845)
    
    UltraBlame original commit: 910d4e8527067541094e22262071f7c7f6d6e549

--- mobile/android/base/tests/testDistribution.java
+++ mobile/android/base/tests/testDistribution.java
@@ -462,29 +462,29 @@ public class testDistribution extends ContentProviderTest {
 
     public void checkTilesReporting(String localeCode) throws JSONException {
         
-        inputAndLoadUrl(mStringHelper.ABOUT_BLANK_URL);
-        inputAndLoadUrl(mStringHelper.ABOUT_HOME_URL);
+        inputAndLoadUrl(StringHelper.ABOUT_BLANK_URL);
+        inputAndLoadUrl(StringHelper.ABOUT_HOME_URL);
 
         
-        JSONObject response = clickTrackingTile(mStringHelper.DISTRIBUTION1_LABEL);
+        JSONObject response = clickTrackingTile(StringHelper.DISTRIBUTION1_LABEL);
         mAsserter.is(response.getInt("click"), 0, "JSON click index matched");
         mAsserter.is(response.getString("locale"), localeCode, "JSON locale code matched");
         mAsserter.is(response.getString("tiles"), "[{\"id\":123},{\"id\":456},{\"id\":632},{\"id\":629},{\"id\":630},{\"id\":631}]", "JSON tiles data matched");
 
-        inputAndLoadUrl(mStringHelper.ABOUT_HOME_URL);
+        inputAndLoadUrl(StringHelper.ABOUT_HOME_URL);
 
         
-        pinTopSite(mStringHelper.DISTRIBUTION2_LABEL);
+        pinTopSite(StringHelper.DISTRIBUTION2_LABEL);
 
         
-        response = clickTrackingTile(mStringHelper.DISTRIBUTION2_LABEL);
+        response = clickTrackingTile(StringHelper.DISTRIBUTION2_LABEL);
         mAsserter.is(response.getInt("click"), 1, "JSON click index matched");
         mAsserter.is(response.getString("tiles"), "[{\"id\":123},{\"id\":456,\"pin\":true},{\"id\":632},{\"id\":629},{\"id\":630},{\"id\":631}]", "JSON tiles data matched");
 
-        inputAndLoadUrl(mStringHelper.ABOUT_HOME_URL);
+        inputAndLoadUrl(StringHelper.ABOUT_HOME_URL);
 
         
-        unpinTopSite(mStringHelper.DISTRIBUTION2_LABEL);
+        unpinTopSite(StringHelper.DISTRIBUTION2_LABEL);
     }
 
     private JSONObject clickTrackingTile(String text) throws JSONException {
