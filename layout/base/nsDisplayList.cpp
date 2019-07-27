8fc31f4232a7f741767756a1a62547825983cac7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:18:10 +0000

    Bug 1032880 - Disable 'transparent content with displayports' warning in tests. r=tn
    UltraBlame original commit: 6ab8ef0895ca40ff5dc313108c51a73dfacd4c07

--- layout/base/nsDisplayList.cpp
+++ layout/base/nsDisplayList.cpp
@@ -65,6 +65,20 @@ using namespace mozilla::dom;
 using namespace mozilla::layout;
 typedef FrameMetrics::ViewID ViewID;
 
+static bool
+SpammyLayoutWarningsEnabled()
+{
+  static bool sValue = false;
+  static bool sValueInitialized = false;
+
+  if (!sValueInitialized) {
+    Preferences::GetBool("layout.spammy_warnings.enabled", &sValue);
+    sValueInitialized = true;
+  }
+
+  return sValue;
+}
+
 static inline nsIFrame*
 GetTransformRootFrame(nsIFrame* aFrame)
 {
@@ -1267,11 +1281,17 @@ void nsDisplayList::PaintForFrame(nsDisplayListBuilder* aBuilder,
                      aBuilder->FindReferenceFrameFor(aForFrame),
                      root, viewport,
                      !isRoot, isRoot, containerParameters);
+
+  
+  
+#ifdef DEBUG
   if (usingDisplayport &&
-      !(root->GetContentFlags() & Layer::CONTENT_OPAQUE)) {
+      !(root->GetContentFlags() & Layer::CONTENT_OPAQUE) &&
+      SpammyLayoutWarningsEnabled()) {
     
     NS_WARNING("Transparent content with displayports can be expensive.");
   }
+#endif
 
   layerManager->SetRoot(root);
   layerBuilder->WillEndTransaction();
