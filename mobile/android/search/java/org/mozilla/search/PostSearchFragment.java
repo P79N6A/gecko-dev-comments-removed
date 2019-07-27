4fc3da602308a7b0c722ad527b619785f5d80b6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:20:33 +0000

    Bug 1038797 - Add network error page. r=bnicholson
    UltraBlame original commit: 79e600c682d8ffdce754e2dae9dcfaa43710fcf5

--- mobile/android/search/java/org/mozilla/search/PostSearchFragment.java
+++ mobile/android/search/java/org/mozilla/search/PostSearchFragment.java
@@ -9,15 +9,20 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.mozilla.gecko.AppConstants;
 import org.mozilla.gecko.Telemetry;
@@ -33,6 +38,7 @@ public class PostSearchFragment extends Fragment {
 
     private SearchEngineManager searchEngineManager;
     private WebView webview;
+    private View errorView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -43,7 +49,8 @@ public class PostSearchFragment extends Fragment {
 
         webview = (WebView) mainView.findViewById(R.id.webview);
         webview.setWebChromeClient(new ChromeClient());
-        webview.setWebViewClient(new LinkInterceptingClient());
+        webview.setWebViewClient(new ResultsWebViewClient());
+
         
         webview.getSettings().setJavaScriptEnabled(true);
 
@@ -77,11 +84,10 @@ public class PostSearchFragment extends Fragment {
             @Override
             public void execute(SearchEngine engine) {
                 final String url = engine.resultsUriForQuery(query);
+
                 
-                if (!TextUtils.equals(webview.getUrl(), url)) {
-                    webview.loadUrl(Constants.ABOUT_BLANK);
-                    webview.loadUrl(url);
-                }
+                webview.loadUrl(Constants.ABOUT_BLANK);
+                webview.loadUrl(url);
             }
         });
     }
@@ -92,10 +98,16 @@ public class PostSearchFragment extends Fragment {
 
 
 
-    private class LinkInterceptingClient extends WebViewClient {
+    private class ResultsWebViewClient extends WebViewClient {
+
+        
+        private boolean networkError;
 
         @Override
         public void onPageStarted(WebView view, final String url, Bitmap favicon) {
+            
+            networkError = false;
+
             searchEngineManager.getEngine(new SearchEngineManager.SearchEngineCallback() {
                 @Override
                 public void execute(SearchEngine engine) {
@@ -118,6 +130,40 @@ public class PostSearchFragment extends Fragment {
                     startActivity(i);
                 }
             });
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            Log.e(LOG_TAG, "Error loading search results: " + description);
+
+            networkError = true;
+
+            if (errorView == null) {
+                final ViewStub errorViewStub = (ViewStub) getView().findViewById(R.id.error_view_stub);
+                errorView = errorViewStub.inflate();
+
+                ((ImageView) errorView.findViewById(R.id.empty_image)).setImageResource(R.drawable.network_error);
+                ((TextView) errorView.findViewById(R.id.empty_title)).setText(R.string.network_error_title);
+
+                final TextView message = (TextView) errorView.findViewById(R.id.empty_message);
+                message.setText(R.string.network_error_message);
+                message.setTextColor(getResources().getColor(R.color.network_error_link));
+                message.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(Settings.ACTION_SETTINGS));
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            
+            if (errorView != null) {
+                errorView.setVisibility(networkError ? View.VISIBLE : View.GONE);
+                webview.setVisibility(networkError ? View.GONE : View.VISIBLE);
+            }
         }
     }
 
