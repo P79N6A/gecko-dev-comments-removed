82faa562fcb97614605b4f6b7c8af8623695fb2b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:32:43 +0000

    Backed out changeset 2bca09c404ad (bug 1068597) for breaking non-unified OS X Builds
    UltraBlame original commit: d201d679ac688eb21206dc0f0f0a526de2257e8a

--- content/media/fmp4/apple/AppleCMFunctions.h
+++ content/media/fmp4/apple/AppleCMFunctions.h
@@ -6,7 +6,7 @@
 
 
 
-LINK_FUNC(VideoFormatDescriptionCreate)
-LINK_FUNC(BlockBufferCreateWithMemoryBlock)
-LINK_FUNC(SampleBufferCreate)
-LINK_FUNC(TimeMake)
+LINK_FUNC(CMVideoFormatDescriptionCreate)
+LINK_FUNC(CMBlockBufferCreateWithMemoryBlock)
+LINK_FUNC(CMSampleBufferCreate)
+LINK_FUNC(CMTimeMake)
