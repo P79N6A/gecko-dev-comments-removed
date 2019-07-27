8183952e46492ab6ebb3decbaab9ddba8404b78c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:00:34 +0000

    Bug 1176266: In TimeStamp_posix.cpp, check for XP_LINUX instead of LINUX, and add missing #include, to allow strrchr usage. r=BenWa Landing on a CLOSED TREE with RyanVM's permission.
    UltraBlame original commit: bcf7d4b06871a7368083df5c2b082fefa61e3cec

--- mozglue/misc/TimeStamp_posix.cpp
+++ mozglue/misc/TimeStamp_posix.cpp
@@ -16,6 +16,7 @@
 #include <sys/syscall.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 
 #if defined(__DragonFly__) || defined(__FreeBSD__) \
     || defined(__NetBSD__) || defined(__OpenBSD__)
@@ -205,7 +206,7 @@ TimeStamp::Now(bool aHighResolution)
   return TimeStamp(ClockTimeNs());
 }
 
-#if defined(LINUX) || defined(ANDROID)
+#if defined(XP_LINUX) || defined(ANDROID)
 
 
 
