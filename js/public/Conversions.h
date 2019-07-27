fe3668e0b07ab4a68440948625c34c1ad7c8c82f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:15:08 +0000

    bug 1172657 - ToInt32's arm asm crashes Apple clang, so #ifdef around the assembly for now. r=waldo
    UltraBlame original commit: 1affb4e7d334c5411a6127aec0fada847b138bac

--- js/public/Conversions.h
+++ js/public/Conversions.h
@@ -349,7 +349,9 @@ ToIntWidth(double d)
 inline int32_t
 ToInt32(double d)
 {
-#if defined (__arm__) && defined (__GNUC__)
+    
+    
+#if defined (__arm__) && defined (__GNUC__) && !defined(__APPLE__)
     int32_t i;
     uint32_t    tmp0;
     uint32_t    tmp1;
