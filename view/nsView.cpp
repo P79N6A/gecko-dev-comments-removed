bd5a11bba4d889aae3c2015b140cb3aa1d424fd5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:02:25 +0000

    Bug 1129774. Part 6: Remove aContext parameter from nsIWidget::CreateChild. r=jmathies
    UltraBlame original commit: 23111a5d00fcf635dfccbc5642b34bad574fe38b

--- view/nsView.cpp
+++ view/nsView.cpp
@@ -566,8 +566,6 @@ nsresult nsView::CreateWidget(nsWidgetInitData *aWidgetInitData,
 
   nsIntRect trect = CalcWidgetBounds(aWidgetInitData->mWindowType);
 
-  nsRefPtr<nsDeviceContext> dx = mViewManager->GetDeviceContext();
-
   nsIWidget* parentWidget =
     GetParent() ? GetParent()->GetNearestWidget(nullptr) : nullptr;
   if (!parentWidget) {
@@ -577,8 +575,7 @@ nsresult nsView::CreateWidget(nsWidgetInitData *aWidgetInitData,
 
   
   
-  mWindow = parentWidget->CreateChild(trect, dx, aWidgetInitData,
-                                      true).take();
+  mWindow = parentWidget->CreateChild(trect, aWidgetInitData, true).take();
   if (!mWindow) {
     return NS_ERROR_FAILURE;
   }
@@ -604,10 +601,8 @@ nsresult nsView::CreateWidgetForParent(nsIWidget* aParentWidget,
 
   nsIntRect trect = CalcWidgetBounds(aWidgetInitData->mWindowType);
 
-  nsRefPtr<nsDeviceContext> dx = mViewManager->GetDeviceContext();
-
   mWindow =
-    aParentWidget->CreateChild(trect, dx, aWidgetInitData).take();
+    aParentWidget->CreateChild(trect, aWidgetInitData).take();
   if (!mWindow) {
     return NS_ERROR_FAILURE;
   }
@@ -629,8 +624,6 @@ nsresult nsView::CreateWidgetForPopup(nsWidgetInitData *aWidgetInitData,
 
   nsIntRect trect = CalcWidgetBounds(aWidgetInitData->mWindowType);
 
-  nsRefPtr<nsDeviceContext> dx = mViewManager->GetDeviceContext();
-
   
   
   
@@ -638,8 +631,7 @@ nsresult nsView::CreateWidgetForPopup(nsWidgetInitData *aWidgetInitData,
   if (aParentWidget) {
     
     
-    mWindow = aParentWidget->CreateChild(trect, dx, aWidgetInitData,
-                                         true).take();
+    mWindow = aParentWidget->CreateChild(trect, aWidgetInitData, true).take();
   }
   else {
     nsIWidget* nearestParent = GetParent() ? GetParent()->GetNearestWidget(nullptr)
@@ -650,8 +642,7 @@ nsresult nsView::CreateWidgetForPopup(nsWidgetInitData *aWidgetInitData,
       return NS_ERROR_FAILURE;
     }
 
-    mWindow =
-      nearestParent->CreateChild(trect, dx, aWidgetInitData).take();
+    mWindow = nearestParent->CreateChild(trect, aWidgetInitData).take();
   }
   if (!mWindow) {
     return NS_ERROR_FAILURE;
