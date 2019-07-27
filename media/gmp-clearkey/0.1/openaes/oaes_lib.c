f3a6ba60e830e406ba830821971f313330f96f40
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:12:48 +0000

    Bug 1092023: comment out oaes_get_seed() definition r=edwin
    It's unused (the sole caller is already commented out),
    and uses deprecated ftime() & sys/timeb.h which are not
    available on OpenBSD and Android NDK r10c.
    Dont build openaes/rand.c, OAES_HAVE_ISAAC is not defined.
    Remove now unused rand.c & rand.h.
    
    UltraBlame original commit: eaff9e176e42d0003f6e7869547b494cdee7ac19

--- media/gmp-clearkey/0.1/openaes/oaes_lib.c
+++ media/gmp-clearkey/0.1/openaes/oaes_lib.c
@@ -34,7 +34,6 @@ static const char _NR[] = {
 #include <stdlib.h>
 #include <stddef.h>
 #include <time.h> 
-#include <sys/timeb.h>
 #include <string.h>
 
 #ifdef WIN32
@@ -480,45 +479,47 @@ OAES_RET oaes_sprintf(
 	return OAES_RET_SUCCESS;
 }
 
-#ifdef OAES_HAVE_ISAAC
-static void oaes_get_seed( char buf[RANDSIZ + 1] )
-{
-	struct timeb timer;
-	struct tm *gmTimer;
-	char * _test = NULL;
-	
-	ftime (&timer);
-	gmTimer = gmtime( &timer.time );
-	_test = (char *) calloc( sizeof( char ), timer.millitm );
-	sprintf( buf, "%04d%02d%02d%02d%02d%02d%03d%p%d",
-		gmTimer->tm_year + 1900, gmTimer->tm_mon + 1, gmTimer->tm_mday,
-		gmTimer->tm_hour, gmTimer->tm_min, gmTimer->tm_sec, timer.millitm,
-		_test + timer.millitm, getpid() );
-	
-	if( _test )
-		free( _test );
-}
-#else
-static uint32_t oaes_get_seed()
-{
-	struct timeb timer;
-	struct tm *gmTimer;
-	char * _test = NULL;
-	uint32_t _ret = 0;
-	
-	ftime (&timer);
-	gmTimer = gmtime( &timer.time );
-	_test = (char *) calloc( sizeof( char ), timer.millitm );
-	_ret = gmTimer->tm_year + 1900 + gmTimer->tm_mon + 1 + gmTimer->tm_mday +
-			gmTimer->tm_hour + gmTimer->tm_min + gmTimer->tm_sec + timer.millitm +
-			(uint32_t) ( _test + timer.millitm ) + getpid();
 
-	if( _test )
-		free( _test );
-	
-	return _ret;
-}
-#endif 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 static OAES_RET oaes_key_destroy( oaes_key ** key )
 {
