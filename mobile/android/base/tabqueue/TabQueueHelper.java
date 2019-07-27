bb45b6b8e14671cb42fcebc70e29beeaf6fc3e75
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:21:53 +0000

    Bug 1134235 - Store Tab Queue urls in file (r=rnewman)
    UltraBlame original commit: a6445426b812b47df23d7b745b35b130a2417d74

---
+++ mobile/android/base/tabqueue/TabQueueHelper.java
@@ -0,0 +1,40 @@
+
+
+
+
+
+package org.mozilla.gecko.tabqueue;
+
+import org.mozilla.gecko.GeckoProfile;
+import org.mozilla.gecko.util.ThreadUtils;
+
+import android.text.TextUtils;
+import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+public class TabQueueHelper {
+    private static final String LOGTAG = "Gecko" + TabQueueHelper.class.getSimpleName();
+
+    public static final String FILE_NAME = "tab_queue_url_list.json";
+
+    
+
+
+
+
+
+
+
+    public static void queueURL(final GeckoProfile profile, final String url, final String filename) {
+        ThreadUtils.assertNotOnUiThread();
+
+        JSONArray jsonArray = profile.readJSONArrayFromFile(filename);
+
+        jsonArray.put(url);
+
+        profile.writeFile(filename, jsonArray.toString());
+    }
+}
