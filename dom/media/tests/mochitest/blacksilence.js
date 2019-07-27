17a716935d0d150a61648588dd1b2ad805e16212
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:47:48 +0000

    Bug 1030435 - Attempt to ease load on B2G emulator for tests. r=jib
    UltraBlame original commit: 247b2cbfadf65cac2a49a34ffda083845dfb75d2

--- dom/media/tests/mochitest/blacksilence.js
+++ dom/media/tests/mochitest/blacksilence.js
@@ -24,19 +24,31 @@
   }
 
   function periodicCheck(type, checkFunc, successMessage, done) {
-    var interval = setInterval(function periodic() {
+    var num = 0;
+    var timeout;
+    function periodic() {
       if (checkFunc()) {
         ok(true, type + ' is ' + successMessage);
-        clearInterval(interval);
-        interval = null;
         done();
+      } else {
+        setupNext();
       }
-    }, 200);
+    }
+    function setupNext() {
+      
+      
+      
+      timeout = setTimeout(periodic, 200 << num);
+      num++;
+    }
+
+    setupNext();
+
     return function cancel() {
-      if (interval) {
+      if (timeout) {
         ok(false, type + ' (' + successMessage + ')' +
            ' failed after waiting full duration');
-        clearInterval(interval);
+        clearTimeout(timeout);
         done();
       }
     };
@@ -59,16 +71,22 @@
       var silent = check(constraintApplied, isSilence(view), 'be silence for audio');
       return sampleCount > 0 && silent;
     }
+    function disconnect() {
+      source.disconnect();
+      analyser.disconnect();
+      done();
+    }
     return periodicCheck('audio', testAudio,
-                         (constraintApplied ? '' : 'not ') + 'silent', done);
+                         (constraintApplied ? '' : 'not ') + 'silent', disconnect);
   }
 
   function mkElement(type) {
-    var display = document.getElementById('display');
+    
+    
+    
     var e = document.createElement(type);
     e.width = 32;
     e.height = 24;
-    display.appendChild(e);
     return e;
   }
 
