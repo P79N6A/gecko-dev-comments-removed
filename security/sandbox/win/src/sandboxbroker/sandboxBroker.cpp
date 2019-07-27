63ba8473533f7097ac3d0e6775dd372fb66b65a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:23:28 +0000

    Bug 1145432: Add the policy for the client side of the crash server pipe to the GMP Windows sandbox. r=aklotz
    UltraBlame original commit: 1c6d4f4dc12fdeb82326ddf07fdaa903db29d428

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -272,6 +272,12 @@ SandboxBroker::SetSecurityLevelForGMPlugin()
                             L"\\??\\pipe\\chrome.*");
   ret = ret && (sandbox::SBOX_ALL_OK == result);
 
+  
+  result = mPolicy->AddRule(sandbox::TargetPolicy::SUBSYS_FILES,
+                            sandbox::TargetPolicy::FILES_ALLOW_ANY,
+                            L"\\??\\pipe\\gecko-crash-server-pipe.*");
+  ret = ret && (sandbox::SBOX_ALL_OK == result);
+
 #ifdef DEBUG
   
   
