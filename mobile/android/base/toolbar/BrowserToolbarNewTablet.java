3955442b56d883bdb0b77fda2898a5b4da8fe67b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:21:12 +0000

    Bug 1085771 - Part 3: Update LWT UI for new tablet browser toolbar. r=lucasr
    UltraBlame original commit: d18d4ecda9c3016fe51d3165e405f0b227f9b61a

--- mobile/android/base/toolbar/BrowserToolbarNewTablet.java
+++ mobile/android/base/toolbar/BrowserToolbarNewTablet.java
@@ -11,6 +11,7 @@ import org.mozilla.gecko.animation.ViewHelper;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 
 
@@ -203,5 +204,11 @@ class BrowserToolbarNewTablet extends BrowserToolbarTabletBase {
         
         
         setButtonEnabled(forwardButton, true);
+    }
+
+    @Override
+    protected Drawable getLWTDefaultStateSetDrawable() {
+        return BrowserToolbar.getLightweightThemeDrawable(this, getResources(), getTheme(),
+                R.color.background_normal);
     }
 }
