480181defa83cb6f642e24667be6750a77198794
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:13:32 +0000

    Bug 1135432 - Suppress line break after text run which ends in ruby. r=jfkthame
    UltraBlame original commit: f4b96c24feced2848c51a38c12fff2e317fb9bd4

--- layout/generic/nsTextFrame.cpp
+++ layout/generic/nsTextFrame.cpp
@@ -8524,6 +8524,7 @@ nsTextFrame::ReflowText(nsLineLayout& aLineLayout, nscoord aAvailableWidth,
   bool emptyTextAtStartOfLine = atStartOfLine && length == 0;
   if (!breakAfter && charsFit == length && !emptyTextAtStartOfLine &&
       transformedOffset + transformedLength == mTextRun->GetLength() &&
+      !StyleContext()->IsInlineDescendantOfRuby() &&
       (mTextRun->GetFlags() & nsTextFrameUtils::TEXT_HAS_TRAILING_BREAK)) {
     
     
