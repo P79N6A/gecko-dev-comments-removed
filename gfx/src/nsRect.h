907f0e4ee3da30b4b5bef878c6ae578caaef6be7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:48:44 +0000

    Bug 1171528 - Remove overflowed nscoord_MAX warnings from nsRect. r=dholbert
    UltraBlame original commit: b3f90ab360afe8da2916d5fac83e2f2f1f01f1cf

--- gfx/src/nsRect.h
+++ gfx/src/nsRect.h
@@ -10,7 +10,6 @@
 #include <stdio.h>                      
 #include <stdint.h>                     
 #include <algorithm>                    
-#include "nsDebug.h"                    
 #include "gfxCore.h"                    
 #include "mozilla/Likely.h"             
 #include "mozilla/gfx/Rect.h"
@@ -80,7 +79,6 @@ struct NS_GFX nsRect :
     result.x = std::min(aRect.x, x);
     int64_t w = std::max(int64_t(aRect.x) + aRect.width, int64_t(x) + width) - result.x;
     if (MOZ_UNLIKELY(w > nscoord_MAX)) {
-      NS_WARNING("Overflowed nscoord_MAX in conversion to nscoord width");
       
       result.x = std::max(result.x, nscoord_MIN / 2);
       w = std::max(int64_t(aRect.x) + aRect.width, int64_t(x) + width) - result.x;
@@ -93,7 +91,6 @@ struct NS_GFX nsRect :
     result.y = std::min(aRect.y, y);
     int64_t h = std::max(int64_t(aRect.y) + aRect.height, int64_t(y) + height) - result.y;
     if (MOZ_UNLIKELY(h > nscoord_MAX)) {
-      NS_WARNING("Overflowed nscoord_MAX in conversion to nscoord height");
       
       result.y = std::max(result.y, nscoord_MIN / 2);
       h = std::max(int64_t(aRect.y) + aRect.height, int64_t(y) + height) - result.y;
