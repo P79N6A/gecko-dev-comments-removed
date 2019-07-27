531d6bbdece7ab92fde76286309e3d5c74eab517
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:30:57 +0000

    Bug 1172473 - Fix intermittent browser_607016.js failures by properly waiting for tabs to be removed r=mconley
    UltraBlame original commit: 92fe8eaa78c13f7c47f36d778a82d105a9b1e9eb

--- browser/components/sessionstore/test/browser_607016.js
+++ browser/components/sessionstore/test/browser_607016.js
@@ -1,37 +1,14 @@
-
-
-
+"use strict";
 
 let stateBackup = ss.getBrowserState();
 
-function cleanup() {
+add_task(function* () {
   
-  try {
-    Services.prefs.clearUserPref("browser.sessionstore.restore_on_demand");
-  } catch (e) {}
-  ss.setBrowserState(stateBackup);
-  executeSoon(finish);
-}
-
-function test() {
-  
-  waitForExplicitFinish();
   ignoreAllUncaughtExceptions();
 
   
   
   Services.prefs.setBoolPref("browser.sessionstore.restore_on_demand", true);
-
-  
-  let progressListener = {
-    onStateChange: function (aBrowser, aWebProgress, aRequest, aStateFlags, aStatus) {
-      if (aBrowser.__SS_restoreState == TAB_STATE_RESTORING &&
-          aStateFlags & Ci.nsIWebProgressListener.STATE_STOP &&
-          aStateFlags & Ci.nsIWebProgressListener.STATE_IS_NETWORK &&
-          aStateFlags & Ci.nsIWebProgressListener.STATE_IS_WINDOW)
-        progressCallback(aBrowser);
-    }
-  }
 
   let state = { windows: [{ tabs: [
     { entries: [{ url: "http://example.org#1" }], extData: { "uniq": r() } },
@@ -42,11 +19,7 @@ function test() {
     { entries: [{ url: "http://example.org#6" }] } 
   ], selected: 1 }] };
 
-  function progressCallback(aBrowser) {
-    
-    
-    window.gBrowser.removeTabsProgressListener(progressListener);
-
+  function* progressCallback() {
     let curState = JSON.parse(ss.getBrowserState());
     for (let i = 0; i < curState.windows[0].tabs.length; i++) {
       let tabState = state.windows[0].tabs[i];
@@ -67,28 +40,28 @@ function test() {
     
     let newUniq = r();
     ss.setTabValue(gBrowser.tabs[1], "uniq", newUniq);
-    gBrowser.removeTab(gBrowser.tabs[1]);
+    yield promiseRemoveTab(gBrowser.tabs[1]);
     let closedTabData = (JSON.parse(ss.getClosedTabData(window)))[0];
     is(closedTabData.state.extData.uniq, newUniq,
        "(overwriting) new data is stored in extData");
 
     
     gBrowser.hideTab(gBrowser.tabs[1]);
-    gBrowser.removeTab(gBrowser.tabs[1]);
+    yield promiseRemoveTab(gBrowser.tabs[1]);
     closedTabData = (JSON.parse(ss.getClosedTabData(window)))[0];
     ok(closedTabData.state.hidden, "(hiding) tab data has hidden == true");
 
     
     let stillUniq = r();
     ss.setTabValue(gBrowser.tabs[1], "stillUniq", stillUniq);
-    gBrowser.removeTab(gBrowser.tabs[1]);
+    yield promiseRemoveTab(gBrowser.tabs[1]);
     closedTabData = (JSON.parse(ss.getClosedTabData(window)))[0];
     is(closedTabData.state.extData.stillUniq, stillUniq,
        "(adding) new data is stored in extData");
 
     
     ss.deleteTabValue(gBrowser.tabs[1], "uniq");
-    gBrowser.removeTab(gBrowser.tabs[1]);
+    yield promiseRemoveTab(gBrowser.tabs[1]);
     closedTabData = (JSON.parse(ss.getClosedTabData(window)))[0];
     
     
@@ -103,15 +76,24 @@ function test() {
     
     let newUniq2 = r();
     ss.setTabValue(gBrowser.tabs[1], "uniq", newUniq2);
-    gBrowser.removeTab(gBrowser.tabs[1]);
+    yield promiseRemoveTab(gBrowser.tabs[1]);
     closedTabData = (JSON.parse(ss.getClosedTabData(window)))[0];
     is(closedTabData.state.extData.uniq, newUniq2,
        "(creating) new data is stored in extData where there was none");
-
-    cleanup();
   }
 
-  window.gBrowser.addTabsProgressListener(progressListener);
+  
   ss.setBrowserState(JSON.stringify(state));
-}
 
+  
+  yield Promise.all(Array.map(gBrowser.tabs, tab => {
+    return (tab == gBrowser.selectedTab) ?
+      promiseTabRestored(tab) : promiseTabRestoring(tab)
+  }));
+
+  
+  yield progressCallback();
+
+  
+  yield promiseBrowserState(stateBackup);
+});
