13402d5142ef0b75fe9da2b25dfbb10747fc16c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:35:17 +0000

    Bug 941302 - Part 5: Add Gonk Decoder Module. r=cpearce
    UltraBlame original commit: 73cfc50bcd3797bacb32ddf7644efab8e1c2db9a

---
+++ content/media/fmp4/gonk/GonkDecoderModule.h
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+#if !defined(GonkPlatformDecoderModule_h_)
+#define GonkPlatformDecoderModule_h_
+
+#include "PlatformDecoderModule.h"
+
+namespace mozilla {
+
+class GonkDecoderModule : public PlatformDecoderModule {
+public:
+  GonkDecoderModule();
+  virtual ~GonkDecoderModule();
+
+  
+  virtual nsresult Shutdown() MOZ_OVERRIDE;
+
+  
+  virtual already_AddRefed<MediaDataDecoder>
+  CreateH264Decoder(const mp4_demuxer::VideoDecoderConfig& aConfig,
+                    mozilla::layers::LayersBackend aLayersBackend,
+                    mozilla::layers::ImageContainer* aImageContainer,
+                    MediaTaskQueue* aVideoTaskQueue,
+                    MediaDataDecoderCallback* aCallback) MOZ_OVERRIDE;
+
+  
+  virtual already_AddRefed<MediaDataDecoder>
+  CreateAACDecoder(const mp4_demuxer::AudioDecoderConfig& aConfig,
+                   MediaTaskQueue* aAudioTaskQueue,
+                   MediaDataDecoderCallback* aCallback) MOZ_OVERRIDE;
+
+  static void Init();
+};
+
+} 
+
+#endif
