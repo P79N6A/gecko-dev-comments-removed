99f9fd8864ce0a96e94458927f0cbb937aa94d3e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:59:08 +0000

    Bug 1058428 - Ensure all in-use decoders contain the target range - r=cajbir
    UltraBlame original commit: 36da0216faedd8d085b521784cf4eff0e4698363

--- content/media/mediasource/MediaSourceReader.cpp
+++ content/media/mediasource/MediaSourceReader.cpp
@@ -398,12 +398,18 @@ private:
 bool
 MediaSourceReader::DecodersContainTime(double aTime)
 {
+  bool found = false;
+
   for (uint32_t i = 0; i < mDecoders.Length(); ++i) {
-    if (!mDecoders[i]->IsDiscarded() && mDecoders[i]->ContainsTime(aTime)) {
-      return true;
+    if (!mDecoders[i]->IsDiscarded()) {
+      if (!mDecoders[i]->ContainsTime(aTime)) {
+        
+        return false;
+      }
+      found = true;
     }
   }
-  return false;
+  return found;
 }
 
 nsresult
