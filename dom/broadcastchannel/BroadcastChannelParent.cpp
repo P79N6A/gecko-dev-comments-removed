7021525e2433caacf4d87e40ed88d041816ca78f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:35:47 +0000

    Bug 1148032 - BroadcastChannel API should not bypass private browsing mode, r=ehsan
    UltraBlame original commit: 976f64497fbcc992130dc4b5af93a807d57cca99

--- dom/broadcastchannel/BroadcastChannelParent.cpp
+++ dom/broadcastchannel/BroadcastChannelParent.cpp
@@ -18,10 +18,12 @@ namespace dom {
 
 BroadcastChannelParent::BroadcastChannelParent(
                                             const nsAString& aOrigin,
-                                            const nsAString& aChannel)
+                                            const nsAString& aChannel,
+                                            bool aPrivateBrowsing)
   : mService(BroadcastChannelService::GetOrCreate())
   , mOrigin(aOrigin)
   , mChannel(aChannel)
+  , mPrivateBrowsing(aPrivateBrowsing)
 {
   AssertIsOnBackgroundThread();
   mService->RegisterActor(this);
@@ -41,7 +43,7 @@ BroadcastChannelParent::RecvPostMessage(const ClonedMessageData& aData)
     return false;
   }
 
-  mService->PostMessage(this, aData, mOrigin, mChannel);
+  mService->PostMessage(this, aData, mOrigin, mChannel, mPrivateBrowsing);
   return true;
 }
 
@@ -77,11 +79,14 @@ BroadcastChannelParent::ActorDestroy(ActorDestroyReason aWhy)
 void
 BroadcastChannelParent::CheckAndDeliver(const ClonedMessageData& aData,
                                         const nsString& aOrigin,
-                                        const nsString& aChannel)
+                                        const nsString& aChannel,
+                                        bool aPrivateBrowsing)
 {
   AssertIsOnBackgroundThread();
 
-  if (aOrigin == mOrigin && aChannel == mChannel) {
+  if (aOrigin == mOrigin &&
+      aChannel == mChannel &&
+      aPrivateBrowsing == mPrivateBrowsing) {
     
     
     if (aData.blobsParent().IsEmpty() ||
