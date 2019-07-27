b4c3e79294334f7a8f14635692df81046f7bbb08
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:13:49 +0000

    Bug 1134638: 2. Inline some float32x4 binary arithmetic and bitwise operations; r=nbp
    UltraBlame original commit: 9436a637f882b09a24ebc6eb50d8fe556c21918b

--- js/src/jit/IonBuilder.h
+++ js/src/jit/IonBuilder.h
@@ -805,10 +805,10 @@ class IonBuilder
 
     
     InliningStatus inlineConstructSimdObject(CallInfo &callInfo, SimdTypeDescr *target);
-    InliningStatus inlineSimdInt32x4BinaryArith(CallInfo &callInfo, JSNative native,
-                                                MSimdBinaryArith::Operation op);
-    InliningStatus inlineSimdInt32x4BinaryBitwise(CallInfo &callInfo, JSNative native,
-                                                  MSimdBinaryBitwise::Operation op);
+    InliningStatus inlineSimdBinaryArith(CallInfo &callInfo, JSNative native,
+                                         MSimdBinaryArith::Operation op, SimdTypeDescr::Type type);
+    InliningStatus inlineSimdBinaryBitwise(CallInfo &callInfo, JSNative native,
+                                           MSimdBinaryBitwise::Operation op, SimdTypeDescr::Type type);
 
     
     InliningStatus inlineIsCallable(CallInfo &callInfo);
