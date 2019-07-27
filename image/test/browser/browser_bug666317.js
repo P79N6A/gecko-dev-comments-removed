c99eaeb6aec935729392a4f4e44742ab57204ba3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:51:54 +0000

    Back out 2 changesets (bug 1125490) for Windows and Android reftest failures CLOSED TREE
    Backed out changeset 042523d55ed8 (bug 1125490)
    Backed out changeset fa3e911c2941 (bug 1125490)
    
    UltraBlame original commit: 1f0d37966eac18d30e78026f0a0ef2e6417713cf

--- image/test/browser/browser_bug666317.js
+++ image/test/browser/browser_bug666317.js
@@ -27,6 +27,18 @@ function currentRequest() {
   return img.getRequest(Ci.nsIImageLoadingContent.CURRENT_REQUEST);
 }
 
+function attachDiscardObserver(result) {
+  
+  let observer = new ImageDiscardObserver(result);
+  let scriptedObserver = Cc["@mozilla.org/image/tools;1"]
+                           .getService(Ci.imgITools)
+                           .createScriptedObserver(observer);
+
+  
+  let request = currentRequest();
+  return request.clone(scriptedObserver);
+}
+
 function isImgDecoded() {
   let request = currentRequest();
   return request.imageStatus & Ci.imgIRequest.STATUS_FRAME_COMPLETE ? true : false;
@@ -59,16 +71,7 @@ function test() {
 function step2() {
   
   var result = { wasDiscarded: false };
-
-  
-  var observer = new ImageDiscardObserver(result);
-  var scriptedObserver = Cc["@mozilla.org/image/tools;1"]
-                           .getService(Ci.imgITools)
-                           .createScriptedObserver(observer);
-
-  
-  var request = currentRequest();
-  var clonedRequest = request.clone(scriptedObserver);
+  var clonedRequest = attachDiscardObserver(result);
 
   
   forceDecodeImg();
