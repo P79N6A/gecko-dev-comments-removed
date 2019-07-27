4c5ba59ef751509293b354d2c5cb72097efbfcee
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:48:32 +0000

    Bug 1035999 - use JNI to determine if in webapp process for automatic update check; r=wesj
    UltraBlame original commit: fcf2f68864d6c5484577dbc3b8996a7da8d01674

--- mobile/android/chrome/content/WebappRT.js
+++ mobile/android/chrome/content/WebappRT.js
@@ -34,8 +34,6 @@ let WebappRT = {
     pref("toolkit.telemetry.notifiedOptOut", 999),
     pref("media.useAudioChannelService", true),
     pref("dom.mozTCPSocket.enabled", true),
-    
-    pref("browser.webapps.checkForUpdates", 0),
 
     
     pref("dom.sysmsg.enabled", true),
--- mobile/android/components/WebappsUpdateTimer.js
+++ mobile/android/components/WebappsUpdateTimer.js
@@ -12,17 +12,42 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
 
+Cu.import("resource://gre/modules/JNI.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/WebappManager.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-function log(message) {
-  
-  
-  
-  
-  
-  dump(message);
+const Log = Cu.import("resource://gre/modules/AndroidLog.jsm", {}).AndroidLog.bind("WebappsUpdateTimer");
+
+function getContextClassName() {
+  let jenv = null;
+  try {
+    jenv = JNI.GetForThread();
+
+    let GeckoAppShell = JNI.LoadClass(jenv, "org.mozilla.gecko.GeckoAppShell", {
+      static_methods: [
+        { name: "getContext", sig: "()Landroid/content/Context;" }
+      ],
+    });
+
+    JNI.LoadClass(jenv, "android.content.Context", {
+      methods: [
+        { name: "getClass", sig: "()Ljava/lang/Class;" },
+      ],
+    });
+
+    JNI.LoadClass(jenv, "java.lang.Class", {
+      methods: [
+        { name: "getName", sig: "()Ljava/lang/String;" },
+      ],
+    });
+
+    return JNI.ReadString(jenv, GeckoAppShell.getContext().getClass().getName());
+  } finally {
+    if (jenv) {
+      JNI.UnloadClasses(jenv);
+    }
+  }
 }
 
 function WebappsUpdateTimer() {}
@@ -33,20 +58,22 @@ WebappsUpdateTimer.prototype = {
   classID: Components.ID("{8f7002cb-e959-4f0a-a2e8-563232564385}"),
 
   notify: function(aTimer) {
-    if (Services.prefs.getIntPref("browser.webapps.checkForUpdates") == 0) {
-      
-      log("Webapps update timer invoked in webapp process; ignoring.");
+    
+    
+    
+    if (Services.prefs.getIntPref("browser.webapps.checkForUpdates") == 0 ||
+        getContextClassName().startsWith("org.mozilla.gecko.webapp")) {
       return;
     }
 
     
     if (Services.io.offline) {
-      log("network offline for webapp update check; waiting");
+      Log.i("network offline for webapp update check; waiting");
       Services.obs.addObserver(this, "network:offline-status-changed", true);
       return;
     }
 
-    log("periodic check for webapp updates");
+    Log.i("periodic check for webapp updates");
     WebappManager.checkForUpdates();
   },
 
@@ -55,7 +82,7 @@ WebappsUpdateTimer.prototype = {
       return;
     }
 
-    log("network back online for webapp update check; commencing");
+    Log.i("network back online for webapp update check; commencing");
     Services.obs.removeObserver(this, "network:offline-status-changed");
     WebappManager.checkForUpdates();
   }
