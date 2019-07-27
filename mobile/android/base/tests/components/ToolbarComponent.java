3255e4afca069c86e859ddc9c75af1a79bf876f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:21:10 +0000

    Bug 1142699 - ToolbarComponent: Wait up to 60 seconds for the reader mode button to appear. r=mcomella
    UltraBlame original commit: 382875ef67af2619624b77041aed0f894cb52a6f

--- mobile/android/base/tests/components/ToolbarComponent.java
+++ mobile/android/base/tests/components/ToolbarComponent.java
@@ -35,7 +35,8 @@ public class ToolbarComponent extends BaseComponent {
 
     
     
-    private static final int READER_MODE_WAIT_MS = 30000;
+    
+    private static final int READER_MODE_WAIT_MS = 60000;
 
     public ToolbarComponent(final UITestContext testContext) {
         super(testContext);
