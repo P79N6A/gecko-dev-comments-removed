c9b2e7d9167e07430e9a94cb2a113135a2b5622a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:49:28 +0000

    Bug 1137285 - Replace content.location = ... by a navigateTo request r=pbrosset
    UltraBlame original commit: 120d272971a6f3746e701744694d387b14ced429

--- browser/devtools/inspector/test/head.js
+++ browser/devtools/inspector/test/head.js
@@ -78,6 +78,11 @@ let addTab = Task.async(function* (url) {
   return tab;
 });
 
+let navigateTo = function (toolbox, url) {
+  let activeTab = toolbox.target.activeTab;
+  return activeTab.navigateTo(url);
+};
+
 
 
 
