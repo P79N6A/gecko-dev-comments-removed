0cd03239981df1f3050a63b87438a53a1ebb6f0a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:34:36 +0000

    Bug 1088437 - Enable the CSS Font Loading API pref in non-release builds. r=dbaron
    UltraBlame original commit: 8476f8a5e00b5a7258c49f3e333ae4c8dcd6bc43

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -2256,10 +2256,11 @@ pref("layout.css.scroll-behavior.damping-ratio", "1.0");
 pref("layout.css.scroll-snap.enabled", true);
 
 
-
-
-
+#ifdef RELEASE_BUILD
 pref("layout.css.font-loading-api.enabled", false);
+#else
+pref("layout.css.font-loading-api.enabled", true);
+#endif
 
 
 
