3364a0b4b79d86cae43d4b69f1553e6c74db3d16
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:30:29 +0000

    Bug 1004609, part 4 - Templatize LocationService over the string allocator. r=njn
    UltraBlame original commit: d8e95f35689131c74fd3826cfade57531953461f

--- memory/replace/dmd/DMD.cpp
+++ memory/replace/dmd/DMD.cpp
@@ -565,7 +565,11 @@ public:
 
 
 
+
+
+
 template <class StringTable,
+          class StringAlloc,
           class Writer>
 class LocationService
 {
@@ -608,8 +612,8 @@ class LocationService
     ~Entry()
     {
       
-      InfallibleAllocPolicy::free_(mFunction);
-      InfallibleAllocPolicy::free_(mFileName);
+      StringAlloc::free(mFunction);
+      StringAlloc::free(mFileName);
     }
 
     void Replace(const void* aPc, const char* aFunction,
@@ -619,12 +623,12 @@ class LocationService
       mPc = aPc;
 
       
-      InfallibleAllocPolicy::free_(mFunction);
+      StringAlloc::free(mFunction);
       mFunction =
-        !aFunction[0] ? nullptr : InfallibleAllocPolicy::strdup_(aFunction);
-      InfallibleAllocPolicy::free_(mFileName);
+        !aFunction[0] ? nullptr : StringAlloc::copy(aFunction);
+      StringAlloc::free(mFileName);
       mFileName =
-        !aFileName[0] ? nullptr : InfallibleAllocPolicy::strdup_(aFileName);
+        !aFileName[0] ? nullptr : StringAlloc::copy(aFileName);
 
 
       mLibrary = aLibrary;
@@ -800,7 +804,22 @@ private:
   StringHashSet mSet;
 };
 
-typedef LocationService<StringTable, Writer> DMDLocationService;
+class StringAlloc
+{
+public:
+  static char*
+  copy(const char* aString)
+  {
+    return InfallibleAllocPolicy::strdup_(aString);
+  }
+  static void
+  free(char* aString)
+  {
+    InfallibleAllocPolicy::free_(aString);
+  }
+};
+
+typedef LocationService<StringTable, StringAlloc, Writer> DMDLocationService;
 
 
 
