6d842d570c0784baef1890bf621c6229c0c6f52b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:35:15 +0000

    Bug 1113683 - Add dummy NullHttpChannel r=mcmanus
    UltraBlame original commit: dfea3cd26c291b45b1f09687294cf67288dc3c62

---
+++ netwerk/protocol/http/NullHttpChannel.h
@@ -0,0 +1,67 @@
+
+
+
+
+#ifndef mozilla_net_NullHttpChannel_h
+#define mozilla_net_NullHttpChannel_h
+
+#include "nsINullChannel.h"
+#include "nsIHttpChannel.h"
+#include "nsITimedChannel.h"
+#include "nsIURI.h"
+#include "nsCOMPtr.h"
+#include "mozilla/TimeStamp.h"
+#include "nsString.h"
+#include "prtime.h"
+
+class nsProxyInfo;
+class nsHttpChannel;
+
+namespace mozilla {
+namespace net {
+
+class NullHttpChannel MOZ_FINAL
+  : public nsINullChannel
+  , public nsIHttpChannel
+  , public nsITimedChannel
+{
+  NS_DECL_THREADSAFE_ISUPPORTS
+  NS_DECL_NSINULLCHANNEL
+  NS_DECL_NSIHTTPCHANNEL
+  NS_DECL_NSITIMEDCHANNEL
+  NS_DECL_NSIREQUEST
+  NS_DECL_NSICHANNEL
+
+  NullHttpChannel();
+
+  
+  
+  explicit NullHttpChannel(nsIHttpChannel * chan);
+
+  
+  nsresult Init(nsIURI *aURI, uint32_t aCaps, nsProxyInfo *aProxyInfo,
+                        uint32_t aProxyResolveFlags,
+                        nsIURI *aProxyURI);
+private:
+  ~NullHttpChannel() { }
+
+protected:
+  nsCOMPtr<nsIURI> mURI;
+  nsCOMPtr<nsIURI> mOriginalURI;
+
+  nsString  mInitiatorType;
+  PRTime    mChannelCreationTime;
+  TimeStamp mAsyncOpenTime;
+  TimeStamp mChannelCreationTimestamp;
+  nsCOMPtr<nsIPrincipal> mResourcePrincipal;
+  nsCString mTimingAllowOriginHeader;
+  bool      mAllRedirectsSameOrigin;
+  bool      mAllRedirectsPassTimingAllowCheck;
+};
+
+} 
+} 
+
+
+
+#endif 
