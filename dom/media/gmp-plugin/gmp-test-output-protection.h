944b4dcb9d74012ab2c5e8f13ec48b85845a71f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:57:09 +0000

    Bug 1088130: Add to TestGMPCrossOrigin.cpp gtest to test that the APIs required for Output Protection on Windows work within the sandbox. r=cpearce
    UltraBlame original commit: 8d56722cff6c3f38ddeece4356e4c115630fc955

---
+++ dom/media/gmp-plugin/gmp-test-output-protection.h
@@ -0,0 +1,130 @@
+
+
+
+
+
+
+#if defined(XP_WIN)
+#include <d3d9.h> 
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+#include "Opmapi.h"
+#endif
+
+namespace mozilla {
+namespace gmptest {
+
+#if defined(XP_WIN)
+typedef HRESULT(STDAPICALLTYPE * OPMGetVideoOutputsFromHMONITORProc)
+          (HMONITOR, OPM_VIDEO_OUTPUT_SEMANTICS, ULONG*, IOPMVideoOutput***);
+
+static OPMGetVideoOutputsFromHMONITORProc sOPMGetVideoOutputsFromHMONITORProc = nullptr;
+
+static BOOL CALLBACK EnumDisplayMonitorsCallback(HMONITOR hMonitor, HDC hdc,
+                                                 LPRECT lprc, LPARAM pData)
+{
+  std::vector<std::string>* failureMsgs = (std::vector<std::string>*)pData;
+
+  MONITORINFOEXA miex;
+  ZeroMemory(&miex, sizeof(miex));
+  miex.cbSize = sizeof(miex);
+  if (!GetMonitorInfoA(hMonitor, &miex)) {
+    failureMsgs->push_back("FAIL GetMonitorInfoA call failed");
+  }
+
+  DISPLAY_DEVICEA dd;
+  ZeroMemory(&dd, sizeof(dd));
+  dd.cb = sizeof(dd);
+  if (!EnumDisplayDevicesA(miex.szDevice, 0, &dd, 1)) {
+    failureMsgs->push_back("FAIL EnumDisplayDevicesA call failed");
+  }
+
+  ULONG numVideoOutputs = 0;
+  IOPMVideoOutput** opmVideoOutputArray = nullptr;
+  HRESULT hr = sOPMGetVideoOutputsFromHMONITORProc(hMonitor,
+                                                   OPM_VOS_OPM_SEMANTICS,
+                                                   &numVideoOutputs,
+                                                   &opmVideoOutputArray);
+  if (S_OK != hr) {
+    if (0x8007001f != hr && 0x80070032 != hr) {
+      char msg[100];
+      sprintf(msg, "FAIL OPMGetVideoOutputsFromHMONITOR call failed: HRESULT=0x%08x", hr);
+      failureMsgs->push_back(msg);
+    }
+    return true;
+  }
+
+  for (ULONG i = 0; i < numVideoOutputs; ++i) {
+    OPM_RANDOM_NUMBER opmRandomNumber;
+    BYTE* certificate = nullptr;
+    ULONG certificateLength = 0;
+    hr = opmVideoOutputArray[i]->StartInitialization(&opmRandomNumber,
+                                                     &certificate,
+                                                     &certificateLength);
+    if (S_OK != hr) {
+      char msg[100];
+      sprintf(msg, "FAIL StartInitialization call failed: HRESULT=0x%08x", hr);
+      failureMsgs->push_back(msg);
+    }
+
+    if (certificate) {
+      CoTaskMemFree(certificate);
+    }
+
+    opmVideoOutputArray[i]->Release();
+  }
+
+  if (opmVideoOutputArray) {
+    CoTaskMemFree(opmVideoOutputArray);
+  }
+
+  return true;
+}
+#endif
+
+static void
+RunOutputProtectionAPITests()
+{
+#if defined(XP_WIN)
+  
+  HMODULE hDvax2DLL = GetModuleHandleW(L"dxva2.dll");
+  if (!hDvax2DLL) {
+    FakeDecryptor::Message("FAIL GetModuleHandleW call failed for dxva2.dll");
+    return;
+  }
+
+  sOPMGetVideoOutputsFromHMONITORProc = (OPMGetVideoOutputsFromHMONITORProc)
+    GetProcAddress(hDvax2DLL, "OPMGetVideoOutputsFromHMONITOR");
+  if (!sOPMGetVideoOutputsFromHMONITORProc) {
+    FakeDecryptor::Message("FAIL GetProcAddress call failed for OPMGetVideoOutputsFromHMONITOR");
+    return;
+  }
+
+  
+  
+  std::vector<std::string> failureMsgs;
+  if (!EnumDisplayMonitors(NULL, NULL, EnumDisplayMonitorsCallback,
+                           (LPARAM) &failureMsgs)) {
+    FakeDecryptor::Message("FAIL EnumDisplayMonitors call failed");
+  }
+
+  
+  for (auto& msg : failureMsgs) {
+    FakeDecryptor::Message(msg);
+  }
+#endif
+}
+
+static void
+TestOuputProtectionAPIs()
+{
+  RunOutputProtectionAPITests();
+  FakeDecryptor::Message("OP tests completed");
+  return;
+}
+
+} 
+} 
