d2d4d7a1e5715fb0502bceb638d100baccd233e8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:18:55 +0000

    Bug 1132566 - Make test e10s compatible: browser_privatebrowsing_protocolhandler.js; r=mossop
    The modifications should make the test e10s compatible but I have left
    it disabled as it fails due to Bug 940206.
    
    UltraBlame original commit: 9da87b37243eadb0832b2eba58f8d2cc766da9e2

--- browser/components/privatebrowsing/test/browser/browser_privatebrowsing_protocolhandler.js
+++ browser/components/privatebrowsing/test/browser/browser_privatebrowsing_protocolhandler.js
@@ -5,61 +5,43 @@
 
 
 
-function test() {
-  
-  waitForExplicitFinish();
-  let windowsToClose = [];
+add_task(function* test() {
   let notificationValue = "Protocol Registration: testprotocol";
   let testURI = "http://example.com/browser/" +
     "browser/components/privatebrowsing/test/browser/browser_privatebrowsing_protocolhandler_page.html";
 
-  function doTest(aIsPrivateMode, aWindow, aCallback) {
-    aWindow.gBrowser.selectedBrowser.addEventListener("load", function onLoad() {
-      aWindow.gBrowser.selectedBrowser.removeEventListener("load", onLoad, true);
+  let doTest = Task.async(function* (aIsPrivateMode, aWindow) {
+    let tab = aWindow.gBrowser.selectedTab = aWindow.gBrowser.addTab(testURI);
+    yield BrowserTestUtils.browserLoaded(tab.linkedBrowser);
 
-      setTimeout(function() {
-        let notificationBox = aWindow.gBrowser.getNotificationBox();
-        let notification = notificationBox.getNotificationWithValue(notificationValue);
+    let promiseFinished = PromiseUtils.defer();
+    setTimeout(function() {
+      let notificationBox = aWindow.gBrowser.getNotificationBox();
+      let notification = notificationBox.getNotificationWithValue(notificationValue);
 
-        if (aIsPrivateMode) {
-          
-          ok(!notification, "Notification box should not be displayed inside of private browsing mode");
-        } else {
-          
-          ok(notification, "Notification box should be displaying outside of private browsing mode");
-        }
+      if (aIsPrivateMode) {
+        
+        ok(!notification, "Notification box should not be displayed inside of private browsing mode");
+      } else {
+        
+        ok(notification, "Notification box should be displaying outside of private browsing mode");
+      }
 
-        aCallback();
-      }, 100); 
-    }, true);
+      promiseFinished.resolve();
+    }, 100); 
 
-    aWindow.gBrowser.selectedBrowser.loadURI(testURI);
-  }
-
-  function testOnWindow(aOptions, aCallback) {
-    whenNewWindowLoaded(aOptions, function(aWin) {
-      windowsToClose.push(aWin);
-      
-      
-      
-      executeSoon(function() aCallback(aWin));
-    });
-  };
-
-   
-  registerCleanupFunction(function() {
-    windowsToClose.forEach(function(aWin) {
-      aWin.close();
-    });
+    yield promiseFinished.promise;
   });
 
   
-  testOnWindow({}, function(aWin) {
-    doTest(false, aWin, function() {
-      
-      testOnWindow({private: true}, function(aWin) {
-        doTest(true, aWin, finish);
-      });
-    });
-  });
-}
+  let win = yield BrowserTestUtils.openNewBrowserWindow();
+  yield doTest(false, win);
+
+  
+  let privateWin = yield BrowserTestUtils.openNewBrowserWindow({private: true});
+  yield doTest(true, privateWin);
+
+  
+  yield BrowserTestUtils.closeWindow(win);
+  yield BrowserTestUtils.closeWindow(privateWin);
+});
