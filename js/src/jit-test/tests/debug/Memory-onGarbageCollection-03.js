2ad99b064de9de799d601a8be3e96fd3e896c14c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:19:13 +0000

    Bug 1137844 - Part 4: Test the Debugger.Memory.prototype.onGarbageCollection hook; r=sfink
    UltraBlame original commit: 5cbdedc9203829f5560c29d2bcad57f1fea001ad

---
+++ js/src/jit-test/tests/debug/Memory-onGarbageCollection-03.js
@@ -0,0 +1,16 @@
+
+
+const root = newGlobal();
+const dbg = new Debugger();
+const wrappedRoot = dbg.addDebuggee(root)
+
+let timesFired = 0;
+
+dbg.memory.onGarbageCollection = _ => {
+  timesFired++;
+  root.eval(`gc()`);
+}
+
+root.eval(`gc()`);
+
+assertEq(timesFired, 1);
