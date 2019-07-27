7411156e862f78e3e8749ad917ac316c4f574013
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:04:18 +0000

    Bug 1020090: Proxy all GMP Shmem create/delete to parent and reduce allocation traffic. r=nical
    UltraBlame original commit: 96aa9d33a1f515363915c28ca0a3afc61c163747

--- content/media/gmp/GMPSharedMemManager.h
+++ content/media/gmp/GMPSharedMemManager.h
@@ -7,6 +7,7 @@
 #define GMPSharedMemManager_h_
 
 #include "mozilla/ipc/Shmem.h"
+#include "nsTArray.h"
 
 namespace mozilla {
 namespace gmp {
@@ -14,10 +15,40 @@ namespace gmp {
 class GMPSharedMemManager
 {
 public:
-  virtual bool MgrAllocShmem(size_t aSize,
+  typedef enum {
+    kGMPFrameData = 0,
+    kGMPEncodedData,
+    kGMPNumTypes
+  } GMPMemoryClasses;
+
+  
+  
+  
+  
+  
+  static const uint32_t kGMPBufLimit = 20;
+
+  GMPSharedMemManager() {}
+
+  virtual ~GMPSharedMemManager() {
+    
+  }
+
+  virtual bool MgrAllocShmem(GMPMemoryClasses aClass, size_t aSize,
                              ipc::Shmem::SharedMemory::SharedMemoryType aType,
-                             ipc::Shmem* aMem) = 0;
-  virtual bool MgrDeallocShmem(ipc::Shmem& aMem) = 0;
+                             ipc::Shmem* aMem);
+  virtual bool MgrDeallocShmem(GMPMemoryClasses aClass, ipc::Shmem& aMem);
+
+  
+  virtual uint32_t NumInUse(GMPMemoryClasses aClass);
+
+  
+  virtual void CheckThread() = 0;
+
+  
+  
+  virtual bool Alloc(size_t aSize, ipc::Shmem::SharedMemory::SharedMemoryType aType, ipc::Shmem* aMem) = 0;
+  virtual void Dealloc(ipc::Shmem& aMem) = 0;
 };
 
 } 
