a54c1afe7aff2e53b26d915068a1fda692117371
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:10:40 +0000

    Bug 1062552 - Use empty string as title if no title provided when adding a bookmark. r=ckitching
    UltraBlame original commit: ee1cfc2406f5be412a702195ba241c34a917ab3b

--- mobile/android/base/db/BrowserProvider.java
+++ mobile/android/base/db/BrowserProvider.java
@@ -978,6 +978,12 @@ public class BrowserProvider extends SharedBrowserDatabaseProvider {
                        Long.toString(BrowserContract.Bookmarks.DEFAULT_POSITION));
         }
 
+        if (!values.containsKey(Bookmarks.TITLE)) {
+            
+            
+            values.put(Bookmarks.TITLE, "");
+        }
+
         String url = values.getAsString(Bookmarks.URL);
 
         debug("Inserting bookmark in database with URL: " + url);
