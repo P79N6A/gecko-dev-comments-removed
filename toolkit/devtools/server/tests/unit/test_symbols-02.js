be7c28ee5e6ba10436da1d78c4f9188d43a8df92
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:57:28 +0000

    Bug 881480 - Part 1: Create RDP value forms for ES6 symbols. r=past
    UltraBlame original commit: 086723e296b0be3c79c49ee306328446a57b2713

---
+++ toolkit/devtools/server/tests/unit/test_symbols-02.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+const URL = "foo.js";
+
+function run_test() {
+  initTestDebuggerServer();
+  const debuggee = addTestGlobal("test-symbols");
+  const client = new DebuggerClient(DebuggerServer.connectPipe());
+
+  client.connect(function() {
+    attachTestTabAndResume(client, "test-symbols", function(response, tabClient, threadClient) {
+      add_task(testSymbols.bind(null, client, debuggee));
+      run_next_test();
+    });
+  });
+
+  do_test_pending();
+}
+
+function* testSymbols(client, debuggee) {
+  const evalCode = () => {
+    Components.utils.evalInSandbox(
+      "(" + function () {
+        Symbol.prototype.toString = () => {
+          throw new Error("lololol");
+        };
+        var sym = Symbol("le troll");
+        debugger;
+      } + "())",
+      debuggee,
+      "1.8",
+      URL,
+      1
+    );
+  };
+
+  const packet = yield executeOnNextTickAndWaitForPause(evalCode, client);
+  const { sym } = packet.frame.environment.bindings.variables;
+
+  equal(sym.value.type, "symbol");
+  equal(sym.value.name, "le troll");
+
+  finishClient(client);
+}
