4ac8b0b28effad87257b73eeaf0e71f12cd85df6
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:18:22 +0000

    Bug 1137601 - Fire onadapterremoved before onattributechanged in BluetoothManager, f=jocelyn, r=shuang
    UltraBlame original commit: 5f6ddd0ef2eb460b3146eac6b61732e13a7fff79

--- dom/bluetooth2/BluetoothManager.cpp
+++ dom/bluetooth2/BluetoothManager.cpp
@@ -196,16 +196,12 @@ BluetoothManager::HandleAdapterRemoved(const BluetoothValue& aValue)
   
   nsString addressToRemove = aValue.get_nsString();
 
-  uint32_t numAdapters = mAdapters.Length();
-  for (uint32_t i = 0; i < numAdapters; i++) {
+  uint32_t i;
+  for (i = 0; i < mAdapters.Length(); i++) {
     nsString address;
     mAdapters[i]->GetAddress(address);
     if (address.Equals(addressToRemove)) {
       mAdapters.RemoveElementAt(i);
-
-      if (mDefaultAdapterIndex == (int)i) {
-        ReselectDefaultAdapter();
-      }
       break;
     }
   }
@@ -214,6 +210,11 @@ BluetoothManager::HandleAdapterRemoved(const BluetoothValue& aValue)
   BluetoothAdapterEventInit init;
   init.mAddress = addressToRemove;
   DispatchAdapterEvent(NS_LITERAL_STRING("adapterremoved"), init);
+
+  
+  if (mDefaultAdapterIndex == (int)i) {
+    ReselectDefaultAdapter();
+  }
 }
 
 void
