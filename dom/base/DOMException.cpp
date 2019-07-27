9b3b07754b4e29a873f508bae0bc78b7696f2dd4
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:08:15 +0000

    Backed out changeset 8db12229549a (bug 1131894)
    UltraBlame original commit: 9f036ba0137e7ed9b87a8275a6993416e3f41afc

--- dom/base/DOMException.cpp
+++ dom/base/DOMException.cpp
@@ -692,7 +692,6 @@ DOMException::Constructor(GlobalObject& ,
       if (name.EqualsASCII(sDOMErrorMsgMap[idx].mName)) {
         exceptionResult = sDOMErrorMsgMap[idx].mNSResult;
         exceptionCode = sDOMErrorMsgMap[idx].mCode;
-        break;
       }
     }
   }
