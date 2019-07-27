666b04768b9f77ceedcb4b83c9b463b3db646c3c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:41:19 +0000

    Bug 1051985 - Move the scroll handoff parent id from ContainerLayer to Layer. r=mattwoodrow
    UltraBlame original commit: 9c32803344ef48b2af5e2321e57b0bb3ee586cbe

--- gfx/layers/apz/src/APZCTreeManager.cpp
+++ gfx/layers/apz/src/APZCTreeManager.cpp
@@ -233,7 +233,7 @@ APZCTreeManager::UpdatePanZoomControllerTree(CompositorParent* aCompositor,
 
         apzc->NotifyLayersUpdated(metrics,
                                   aIsFirstPaint && (aLayersId == aOriginatingLayersId));
-        apzc->SetScrollHandoffParentId(container->GetScrollHandoffParentId());
+        apzc->SetScrollHandoffParentId(aLayer->GetScrollHandoffParentId());
 
         
         
