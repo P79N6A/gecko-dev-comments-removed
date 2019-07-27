f18f1a09c55fa26891c95ddf03e0d13b8043d4a5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:21:39 +0000

    Backed out changeset c677f867a18a (bug 1101287) for making the test more failure-prone that it was before. CLOSED TREE
    UltraBlame original commit: 0d35326dfbae2d2dc62ebcd99d8696d8fedb9bbf

--- browser/components/customizableui/test/head.js
+++ browser/components/customizableui/test/head.js
@@ -461,27 +461,6 @@ function promiseTabHistoryNavigation(aDirection = -1, aConditionFn) {
   return deferred.promise;
 }
 
-
-
-
-
-
-
-
-function promiseAttributeMutation(aNode, aOptions) {
-  return new Promise((resolve, reject) => {
-    info("waiting for mutation: " + JSON.stringify(aOptions));
-    let obs = new MutationObserver((mutations) => {
-      if (mutations.length > 0) {
-        ok(true, "mutation occurred");
-        obs.disconnect();
-        resolve();
-      }
-    });
-    obs.observe(aNode, aOptions);
-  });
-}
-
 function popupShown(aPopup) {
   return promisePopupEvent(aPopup, "shown");
 }
