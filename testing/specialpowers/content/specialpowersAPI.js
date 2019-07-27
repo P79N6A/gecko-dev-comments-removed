b8db2981034b8ed050e0aa653f4a36eac774c12e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:24:36 +0000

    Bug 1141510.  Fix our detection of File in the global in specialpowersAPI.js to actually check the global.  r=bholley
    UltraBlame original commit: 322ea4cf5b8e9eb89125eeee7efa769b9d301399

--- testing/specialpowers/content/specialpowersAPI.js
+++ testing/specialpowers/content/specialpowersAPI.js
@@ -19,7 +19,12 @@ Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/PrivateBrowsingUtils.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-if (!this.File) {
+
+
+
+
+
+if (!(function() { var e = eval; return e("this"); })().File) {
     Cu.importGlobalProperties(["File"]);
 }
 
