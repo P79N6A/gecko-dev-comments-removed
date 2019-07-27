f20189fbd61522435c2884f53614000d427bdaef
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:21:56 +0000

    Bug 1042373 - Implement GMPDecryptor interface for use in GMPs. r=jesup
    UltraBlame original commit: 0616023bd207b41ed53cee2066d3128cda468bf2

---
+++ content/media/gmp/GMPEncryptedBufferDataImpl.h
@@ -0,0 +1,68 @@
+
+
+
+
+
+#ifndef GMPEncryptedBufferDataImpl_h_
+#define GMPEncryptedBufferDataImpl_h_
+
+#include "gmp-decryption.h"
+#include "mp4_demuxer/DecoderData.h"
+#include "nsTArray.h"
+#include "mozilla/gmp/GMPTypes.h"
+
+namespace mozilla {
+namespace gmp {
+
+class GMPEncryptedBufferDataImpl : public GMPEncryptedBufferMetadata {
+private:
+  typedef mp4_demuxer::CryptoSample CryptoSample;
+public:
+  GMPEncryptedBufferDataImpl(const CryptoSample& aCrypto);
+  GMPEncryptedBufferDataImpl(const GMPDecryptionData& aData);
+
+  void RelinquishData(GMPDecryptionData& aData);
+
+  virtual const uint8_t* KeyId() const MOZ_OVERRIDE;
+  virtual uint32_t KeyIdSize() const MOZ_OVERRIDE;
+  virtual const uint8_t* IV() const MOZ_OVERRIDE;
+  virtual uint32_t IVSize() const MOZ_OVERRIDE;
+  virtual uint32_t NumSubsamples() const MOZ_OVERRIDE;
+  virtual const uint16_t* ClearBytes() const MOZ_OVERRIDE;
+  virtual const uint32_t* CipherBytes() const MOZ_OVERRIDE;
+
+private:
+  nsTArray<uint8_t> mKeyId;
+  nsTArray<uint8_t> mIV;
+  nsTArray<uint16_t> mClearBytes;
+  nsTArray<uint32_t> mCipherBytes;
+};
+
+class GMPBufferImpl : public GMPBuffer {
+public:
+  GMPBufferImpl(uint32_t aId, const nsTArray<uint8_t>& aData)
+    : mId(aId)
+    , mData(aData)
+  {
+  }
+  virtual uint32_t Id() const {
+    return mId;
+  }
+  virtual uint8_t* Data() {
+    return mData.Elements();
+  }
+  virtual uint32_t Size() const {
+    return mData.Length();
+  }
+  virtual void Resize(uint32_t aSize) {
+    mData.SetLength(aSize);
+  }
+
+  uint32_t mId;
+  nsTArray<uint8_t> mData;
+};
+
+} 
+} 
+
+#endif 
