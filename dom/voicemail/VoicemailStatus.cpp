9821af4deed6fcebf020dd4b52cb951fbf39d57e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:42:14 +0000

    Bug 833229 - 3.c/4: implement MozVoicemailStatus in C++. r=smaug
    UltraBlame original commit: 6f906bc6cc5f624b9cb8350c729a2ad7f09b74ea

---
+++ dom/voicemail/VoicemailStatus.cpp
@@ -0,0 +1,83 @@
+
+
+
+
+
+
+#include "mozilla/dom/VoicemailStatus.h"
+
+#include "mozilla/dom/MozVoicemailStatusBinding.h"
+#include "nsIVoicemailService.h"
+#include "nsPIDOMWindow.h"
+
+namespace mozilla {
+namespace dom {
+
+
+NS_IMPL_CYCLE_COLLECTION_WRAPPERCACHE(VoicemailStatus, mParent)
+
+NS_IMPL_CYCLE_COLLECTING_ADDREF(VoicemailStatus)
+NS_IMPL_CYCLE_COLLECTING_RELEASE(VoicemailStatus)
+
+NS_INTERFACE_MAP_BEGIN_CYCLE_COLLECTION(VoicemailStatus)
+  NS_WRAPPERCACHE_INTERFACE_MAP_ENTRY
+  NS_INTERFACE_MAP_ENTRY(nsISupports)
+NS_INTERFACE_MAP_END
+
+VoicemailStatus::VoicemailStatus(nsISupports* aParent,
+                                 nsIVoicemailProvider* aProvider)
+  : mParent(aParent)
+  , mProvider(aProvider)
+{
+  MOZ_ASSERT(mParent);
+  MOZ_ASSERT(mProvider);
+
+  SetIsDOMBinding();
+}
+
+JSObject*
+VoicemailStatus::WrapObject(JSContext* aCx)
+{
+  return MozVoicemailStatusBinding::Wrap(aCx, this);
+}
+
+uint32_t
+VoicemailStatus::ServiceId() const
+{
+  uint32_t result = 0;
+  mProvider->GetServiceId(&result);
+  return result;
+}
+
+bool
+VoicemailStatus::HasMessages() const
+{
+  bool result = false;
+  mProvider->GetHasMessages(&result);
+  return result;
+}
+
+int32_t
+VoicemailStatus::MessageCount() const
+{
+  int32_t result = 0;
+  mProvider->GetMessageCount(&result);
+  return result;
+}
+
+void
+VoicemailStatus::GetReturnNumber(nsString& aReturnNumber) const
+{
+  aReturnNumber.SetIsVoid(true);
+  mProvider->GetReturnNumber(aReturnNumber);
+}
+
+void
+VoicemailStatus::GetReturnMessage(nsString& aReturnMessage) const
+{
+  aReturnMessage.SetIsVoid(true);
+  mProvider->GetReturnMessage(aReturnMessage);
+}
+
+} 
+} 
---
+++ dom/voicemail/VoicemailStatus.h
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_voicemail_VoicemailStatus_h__
+#define mozilla_dom_voicemail_VoicemailStatus_h__
+
+#include "mozilla/Attributes.h"
+#include "nsCOMPtr.h"
+#include "nsIVoicemailService.h" 
+#include "nsString.h"
+#include "nsWrapperCache.h"
+
+namespace mozilla {
+namespace dom {
+
+class VoicemailStatus MOZ_FINAL : public nsISupports
+                                , public nsWrapperCache
+{
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(VoicemailStatus)
+
+  VoicemailStatus(nsISupports* aParent,
+                  nsIVoicemailProvider* aProvider);
+
+  nsISupports*
+  GetParentObject() const { return mParent; }
+
+  virtual JSObject*
+  WrapObject(JSContext* aCx) MOZ_OVERRIDE;
+
+  
+
+  uint32_t
+  ServiceId() const;
+
+  bool
+  HasMessages() const;
+
+  int32_t
+  MessageCount() const;
+
+  void
+  GetReturnNumber(nsString& aReturnNumber) const;
+
+  void
+  GetReturnMessage(nsString& aReturnMessage) const;
+
+private:
+  
+  ~VoicemailStatus() {}
+
+private:
+  nsCOMPtr<nsISupports> mParent;
+  nsCOMPtr<nsIVoicemailProvider> mProvider;
+};
+
+} 
+} 
+
+#endif 
