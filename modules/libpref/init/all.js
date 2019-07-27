d2144b772525da9c76ea7f3f61c8836b71674083
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:45:40 +0000

    Bug 1070722 - Use the imagelib high quality downscaler on OSX instead of the quartz one. r=jrmuizel
    UltraBlame original commit: 6fb508e952b15f736bd9ad7b0ad2805b83ca8f4a

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -3672,13 +3672,7 @@ pref("image.cache.timeweight", 500);
 
 pref("image.http.accept", "image/png,image/*;q=0.8,*/*;q=0.5");
 
-
-
-#ifdef XP_MACOSX
-pref("image.high_quality_downscaling.enabled", false);
-#else
 pref("image.high_quality_downscaling.enabled", true);
-#endif
 
 
 
