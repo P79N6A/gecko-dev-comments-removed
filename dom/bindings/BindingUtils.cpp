8e74dbf6c71a6b1928a603fc3d046789b4e34b00
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:47:43 +0000

    Bug 1084001.  Set the .name of DOM interface objects to the name of the interface.  r=peterv
    UltraBlame original commit: 35582d2331a80665c951f3b360e1766e288c8140

--- dom/bindings/BindingUtils.cpp
+++ dom/bindings/BindingUtils.cpp
@@ -459,7 +459,19 @@ CreateInterfaceObject(JSContext* cx, JS::Handle<JSObject*> global,
     }
 
     if (!JS_DefineProperty(cx, constructor, "length", ctorNargs,
-                           JSPROP_READONLY | JSPROP_PERMANENT)) {
+                           JSPROP_READONLY)) {
+      return nullptr;
+    }
+
+    
+    
+    
+    JS::Rooted<JSString*> nameStr(cx, JS_InternString(cx, name));
+    if (!nameStr) {
+      return nullptr;
+    }
+
+    if (!JS_DefineProperty(cx, constructor, "name", nameStr, JSPROP_READONLY)) {
       return nullptr;
     }
   }
