250ba79e91135f6641e3d2b20ec239d079d2f4c1
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:52:50 +0000

    Bug 1068410 - Convert remote crash dump to use pipe instead of socketpair in the child. r=kang r=ted
    UltraBlame original commit: afeff2d265bdf1d9b27284de44b6185082691f91

--- toolkit/crashreporter/google-breakpad/src/client/linux/crash_generation/crash_generation_server.cc
+++ toolkit/crashreporter/google-breakpad/src/client/linux/crash_generation/crash_generation_server.cc
@@ -275,14 +275,7 @@ CrashGenerationServer::ClientEvent(short revents)
   }
 
   
-  memset(&msg, 0, sizeof(msg));
-  struct iovec done_iov;
-  done_iov.iov_base = const_cast<char*>("\x42");
-  done_iov.iov_len = 1;
-  msg.msg_iov = &done_iov;
-  msg.msg_iovlen = 1;
-
-  HANDLE_EINTR(sendmsg(signal_fd, &msg, MSG_DONTWAIT | MSG_NOSIGNAL));
+  
   HANDLE_EINTR(close(signal_fd));
 
   return true;
