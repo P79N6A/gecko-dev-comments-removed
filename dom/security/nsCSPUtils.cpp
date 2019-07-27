9e5d72296d26412d2c0734dd84045f4b4b6e4e1e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:26:20 +0000

    Bug 1139297 - Implement CSP upgrade-insecure-requests directive - devtool changes (r=sstamm,bholley)
    UltraBlame original commit: 29bdec3b3dcb601cf3d1e1ecf9ecabcca639aa3e

--- dom/security/nsCSPUtils.cpp
+++ dom/security/nsCSPUtils.cpp
@@ -892,6 +892,11 @@ nsCSPDirective::toDomCSPStruct(mozilla::dom::CSP& outCSP) const
       outCSP.mForm_action.Value() = mozilla::Move(srcs);
       return;
 
+    case nsIContentSecurityPolicy::UPGRADE_IF_INSECURE_DIRECTIVE:
+      outCSP.mUpgrade_insecure_requests.Construct();
+      
+      return;
+
     
 
     default:
