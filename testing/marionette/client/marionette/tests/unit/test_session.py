a7d3905edad6e4a0e87326ff4f3505dc762dfd08
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:02:06 +0000

    Bug 1174941: Update server capabilities that are set on startup; r=ato
    UltraBlame original commit: de8a383de160432bcc36947b092da9de92597570

--- testing/marionette/client/marionette/tests/unit/test_session.py
+++ testing/marionette/client/marionette/tests/unit/test_session.py
@@ -24,7 +24,6 @@ class TestSession(MarionetteTestCase):
 
         
         self.assertIn("device", caps)
-        self.assertIn("handlesAlerts", caps)
         self.assertIn("rotatable", caps)
         self.assertIn("takesScreenshot", caps)
         self.assertIn("version", caps)
