6b6c182dcab7369ce9fe1f5926f9d8019f95dae7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:48:18 +0000

    Bug 1172184 - Pull out logic from TimelineActor into a standalone module that can be consumed by other actors.
    UltraBlame original commit: 9cc8ad860e4b181a1019b27a5525b59acf2318aa

--- toolkit/devtools/server/actors/timeline.js
+++ toolkit/devtools/server/actors/timeline.js
@@ -16,24 +16,11 @@
 
 
 
-
-
-
-const {Ci, Cu} = require("chrome");
 const protocol = require("devtools/server/protocol");
-const {method, Arg, RetVal, Option} = protocol;
+const { method, Arg, RetVal, Option } = protocol;
 const events = require("sdk/event/core");
-const {setTimeout, clearTimeout} = require("sdk/timers");
-const {Task} = Cu.import("resource://gre/modules/Task.jsm", {});
-
-const {Memory} = require("devtools/toolkit/shared/memory");
-const {FramerateActor} = require("devtools/server/actors/framerate");
-const {StackFrameCache} = require("devtools/server/actors/utils/stack");
-
-
-
-
-const DEFAULT_TIMELINE_DATA_PULL_TIMEOUT = 200; 
+const { Timeline } = require("devtools/toolkit/shared/timeline");
+const { actorBridge } = require("devtools/server/actors/common");
 
 
 
@@ -104,18 +91,13 @@ let TimelineActor = exports.TimelineActor = protocol.ActorClass({
   
 
 
-  initialize: function(conn, tabActor) {
+  initialize: function (conn, tabActor) {
     protocol.Actor.prototype.initialize.call(this, conn);
     this.tabActor = tabActor;
+    this.bridge = new Timeline(tabActor);
 
-    this._isRecording = false;
-    this._stackFrames = null;
-    this._memoryBridge = null;
-
-    
-    this._onWindowReady = this._onWindowReady.bind(this);
-    this._onGarbageCollection = this._onGarbageCollection.bind(this);
-    events.on(this.tabActor, "window-ready", this._onWindowReady);
+    this._onTimelineEvent = this._onTimelineEvent.bind(this);
+    events.on(this.bridge, "*", this._onTimelineEvent);
   },
 
   
@@ -130,13 +112,11 @@ let TimelineActor = exports.TimelineActor = protocol.ActorClass({
   
 
 
-  destroy: function() {
-    this.stop();
-
-    events.off(this.tabActor, "window-ready", this._onWindowReady);
+  destroy: function () {
+    events.off(this.bridge, "*", this._onTimelineEvent);
+    this.bridge.destroy();
+    this.bridge = null;
     this.tabActor = null;
-    this._memoryBridge = null;
-
     protocol.Actor.prototype.destroy.call(this);
   },
 
@@ -144,139 +124,20 @@ let TimelineActor = exports.TimelineActor = protocol.ActorClass({
 
 
 
-
-
-
-
-
-  get docShells() {
-    let originalDocShell;
-
-    if (this.tabActor.isRootActor) {
-      originalDocShell = this.tabActor.docShell;
-    } else {
-      originalDocShell = this.tabActor.originalDocShell;
+  _onTimelineEvent: function (eventName, ...args) {
+    if (this.events[eventName]) {
+      events.emit(this, eventName, ...args);
     }
-
-    let docShellsEnum = originalDocShell.getDocShellEnumerator(
-      Ci.nsIDocShellTreeItem.typeAll,
-      Ci.nsIDocShell.ENUMERATE_FORWARDS
-    );
-
-    let docShells = [];
-    while (docShellsEnum.hasMoreElements()) {
-      let docShell = docShellsEnum.getNext();
-      docShells.push(docShell.QueryInterface(Ci.nsIDocShell));
-    }
-
-    return docShells;
   },
 
-  
-
-
-
-  _pullTimelineData: function() {
-    if (!this._isRecording || !this.docShells.length) {
-      return;
-    }
-
-    let endTime = this.docShells[0].now();
-    let markers = [];
-
-    for (let docShell of this.docShells) {
-      markers.push(...docShell.popProfileTimelineMarkers());
-    }
-
-    
-    
-    
-    
-    
-    
-    for (let marker of markers) {
-      if (marker.stack) {
-        marker.stack = this._stackFrames.addFrame(Cu.waiveXrays(marker.stack));
-      }
-      if (marker.endStack) {
-        marker.endStack = this._stackFrames.addFrame(Cu.waiveXrays(marker.endStack));
-      }
-    }
-
-    let frames = this._stackFrames.makeEvent();
-    if (frames) {
-      events.emit(this, "frames", endTime, frames);
-    }
-    if (markers.length > 0) {
-      events.emit(this, "markers", markers, endTime);
-    }
-    if (this._withMemory) {
-      events.emit(this, "memory", endTime, this._memoryBridge.measure());
-    }
-    if (this._withTicks) {
-      events.emit(this, "ticks", endTime, this._framerateActor.getPendingTicks());
-    }
-
-    this._dataPullTimeout = setTimeout(() => {
-      this._pullTimelineData();
-    }, DEFAULT_TIMELINE_DATA_PULL_TIMEOUT);
-  },
-
-  
-
-
-  isRecording: method(function() {
-    return this._isRecording;
-  }, {
+  isRecording: actorBridge("isRecording", {
     request: {},
     response: {
       value: RetVal("boolean")
     }
   }),
 
-  
-
-
-
-
-
-
-
-
-
-
-  start: method(Task.async(function *({ withMemory, withTicks }) {
-    var startTime = this._startTime = this.docShells[0].now();
-    
-    
-    this._unixStartTime = Date.now();
-
-    if (this._isRecording) {
-      return startTime;
-    }
-
-    this._isRecording = true;
-    this._stackFrames = new StackFrameCache();
-    this._stackFrames.initFrames();
-    this._withMemory = withMemory;
-    this._withTicks = withTicks;
-
-    for (let docShell of this.docShells) {
-      docShell.recordProfileTimelineMarkers = true;
-    }
-
-    this._memoryBridge = new Memory(this.tabActor, this._stackFrames);
-    this._memoryBridge.attach();
-    events.on(this._memoryBridge, "garbage-collection", this._onGarbageCollection);
-
-    if (withTicks) {
-      this._framerateActor = new FramerateActor(this.conn, this.tabActor);
-      this._framerateActor.startRecording();
-    }
-
-    this._pullTimelineData();
-    return startTime;
-  }), {
+  start: actorBridge("start", {
     request: {
       withMemory: Option(0, "boolean"),
       withTicks: Option(0, "boolean")
@@ -286,81 +147,13 @@ let TimelineActor = exports.TimelineActor = protocol.ActorClass({
     }
   }),
 
-  
-
-
-  stop: method(Task.async(function *() {
-    if (!this._isRecording) {
-      return;
-    }
-    this._isRecording = false;
-    this._stackFrames = null;
-
-    events.off(this._memoryBridge, "garbage-collection", this._onGarbageCollection);
-    this._memoryBridge.detach();
-
-    if (this._framerateActor) {
-      this._framerateActor.stopRecording();
-      this._framerateActor = null;
-    }
-
-    for (let docShell of this.docShells) {
-      docShell.recordProfileTimelineMarkers = false;
-    }
-
-    clearTimeout(this._dataPullTimeout);
-    return this.docShells[0].now();
-  }), {
+  stop: actorBridge("stop", {
     response: {
       
       
       value: RetVal("nullable:number")
     }
   }),
-
-  
-
-
-
-  _onWindowReady: function({window}) {
-    if (this._isRecording) {
-      let docShell = window.QueryInterface(Ci.nsIInterfaceRequestor)
-                           .getInterface(Ci.nsIWebNavigation)
-                           .QueryInterface(Ci.nsIDocShell);
-      docShell.recordProfileTimelineMarkers = true;
-    }
-  },
-
-  
-
-
-
-
-
-
-
-
-  _onGarbageCollection: function ({ collections, reason, nonincrementalReason }) {
-    if (!this._isRecording || !this.docShells.length) {
-      return;
-    }
-
-    
-    
-    let startTime = (this._unixStartTime - this._startTime) * 1000;
-    let endTime = this.docShells[0].now();
-
-    events.emit(this, "markers", collections.map(({ startTimestamp: start, endTimestamp: end }) => {
-      return {
-        name: "GarbageCollection",
-        causeName: reason,
-        nonincrementalReason: nonincrementalReason,
-        
-        start: (start - startTime) / 1000,
-        end: (end - startTime) / 1000
-      };
-    }), endTime);
-  },
 });
 
 exports.TimelineFront = protocol.FrontClass(TimelineActor, {
---
+++ toolkit/devtools/shared/timeline.js
@@ -0,0 +1,285 @@
+
+
+
+"use strict";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const { Ci, Cu } = require("chrome");
+const { Class } = require("sdk/core/heritage");
+
+
+loader.lazyRequireGetter(this, "events", "sdk/event/core");
+loader.lazyRequireGetter(this, "Timers", "sdk/timers");
+loader.lazyRequireGetter(this, "Task", "resource://gre/modules/Task.jsm", true);
+loader.lazyRequireGetter(this, "Memory", "devtools/toolkit/shared/memory", true);
+loader.lazyRequireGetter(this, "Framerate", "devtools/toolkit/shared/framerate", true);
+loader.lazyRequireGetter(this, "StackFrameCache", "devtools/server/actors/utils/stack", true);
+loader.lazyRequireGetter(this, "EventTarget", "sdk/event/target", true);
+
+
+
+
+const DEFAULT_TIMELINE_DATA_PULL_TIMEOUT = 200; 
+
+
+
+
+let Timeline = exports.Timeline = Class({
+  extends: EventTarget,
+
+  
+
+
+  initialize: function (tabActor) {
+    this.tabActor = tabActor;
+
+    this._isRecording = false;
+    this._stackFrames = null;
+    this._memory = null;
+    this._framerate = null;
+
+    
+    this._onWindowReady = this._onWindowReady.bind(this);
+    this._onGarbageCollection = this._onGarbageCollection.bind(this);
+    events.on(this.tabActor, "window-ready", this._onWindowReady);
+  },
+
+  
+
+
+  destroy: function() {
+    this.stop();
+
+    events.off(this.tabActor, "window-ready", this._onWindowReady);
+    this.tabActor = null;
+
+    if (this._memory) {
+      this._memory.destroy();
+      this._memory = null;
+    }
+
+    if (this._framerate) {
+      this._framerate.destroy();
+      this._framerate = null;
+    }
+  },
+
+  
+
+
+
+
+
+
+
+
+  get docShells() {
+    let originalDocShell;
+
+    if (this.tabActor.isRootActor) {
+      originalDocShell = this.tabActor.docShell;
+    } else {
+      originalDocShell = this.tabActor.originalDocShell;
+    }
+
+    let docShellsEnum = originalDocShell.getDocShellEnumerator(
+      Ci.nsIDocShellTreeItem.typeAll,
+      Ci.nsIDocShell.ENUMERATE_FORWARDS
+    );
+
+    let docShells = [];
+    while (docShellsEnum.hasMoreElements()) {
+      let docShell = docShellsEnum.getNext();
+      docShells.push(docShell.QueryInterface(Ci.nsIDocShell));
+    }
+
+    return docShells;
+  },
+
+  
+
+
+
+  _pullTimelineData: function() {
+    if (!this._isRecording || !this.docShells.length) {
+      return;
+    }
+
+    let endTime = this.docShells[0].now();
+    let markers = [];
+
+    for (let docShell of this.docShells) {
+      markers.push(...docShell.popProfileTimelineMarkers());
+    }
+
+    
+    
+    
+    
+    
+    
+    for (let marker of markers) {
+      if (marker.stack) {
+        marker.stack = this._stackFrames.addFrame(Cu.waiveXrays(marker.stack));
+      }
+      if (marker.endStack) {
+        marker.endStack = this._stackFrames.addFrame(Cu.waiveXrays(marker.endStack));
+      }
+    }
+
+    let frames = this._stackFrames.makeEvent();
+    if (frames) {
+      events.emit(this, "frames", endTime, frames);
+    }
+    if (markers.length > 0) {
+      events.emit(this, "markers", markers, endTime);
+    }
+    if (this._withMemory) {
+      events.emit(this, "memory", endTime, this._memory.measure());
+    }
+    if (this._withTicks) {
+      events.emit(this, "ticks", endTime, this._framerate.getPendingTicks());
+    }
+
+    this._dataPullTimeout = Timers.setTimeout(() => {
+      this._pullTimelineData();
+    }, DEFAULT_TIMELINE_DATA_PULL_TIMEOUT);
+  },
+
+  
+
+
+  isRecording: function () {
+    return this._isRecording;
+  },
+
+  
+
+
+
+
+
+
+
+
+
+
+  start: Task.async(function *({ withMemory, withTicks }) {
+    let startTime = this._startTime = this.docShells[0].now();
+    
+    
+    this._unixStartTime = Date.now();
+
+    if (this._isRecording) {
+      return startTime;
+    }
+
+    this._isRecording = true;
+    this._stackFrames = new StackFrameCache();
+    this._stackFrames.initFrames();
+    this._withMemory = withMemory;
+    this._withTicks = withTicks;
+
+    for (let docShell of this.docShells) {
+      docShell.recordProfileTimelineMarkers = true;
+    }
+
+    this._memory = new Memory(this.tabActor, this._stackFrames);
+    this._memory.attach();
+    events.on(this._memory, "garbage-collection", this._onGarbageCollection);
+
+    if (withTicks) {
+      this._framerate = new Framerate(this.tabActor);
+      this._framerate.startRecording();
+    }
+
+    this._pullTimelineData();
+    return startTime;
+  }),
+
+  
+
+
+  stop: Task.async(function *() {
+    if (!this._isRecording) {
+      return;
+    }
+    this._isRecording = false;
+    this._stackFrames = null;
+
+    events.off(this._memory, "garbage-collection", this._onGarbageCollection);
+    this._memory.detach();
+
+    if (this._framerate) {
+      this._framerate.stopRecording();
+      this._framerate = null;
+    }
+
+    for (let docShell of this.docShells) {
+      docShell.recordProfileTimelineMarkers = false;
+    }
+
+    Timers.clearTimeout(this._dataPullTimeout);
+    return this.docShells[0].now();
+  }),
+
+  
+
+
+
+  _onWindowReady: function({ window }) {
+    if (this._isRecording) {
+      let docShell = window.QueryInterface(Ci.nsIInterfaceRequestor)
+                           .getInterface(Ci.nsIWebNavigation)
+                           .QueryInterface(Ci.nsIDocShell);
+      docShell.recordProfileTimelineMarkers = true;
+    }
+  },
+
+  
+
+
+
+
+
+
+
+
+  _onGarbageCollection: function ({ collections, reason, nonincrementalReason }) {
+    if (!this._isRecording || !this.docShells.length) {
+      return;
+    }
+
+    
+    
+    let startTime = (this._unixStartTime - this._startTime) * 1000;
+    let endTime = this.docShells[0].now();
+
+    events.emit(this, "markers", collections.map(({ startTimestamp: start, endTimestamp: end }) => {
+      return {
+        name: "GarbageCollection",
+        causeName: reason,
+        nonincrementalReason: nonincrementalReason,
+        
+        start: (start - startTime) / 1000,
+        end: (end - startTime) / 1000
+      };
+    }), endTime);
+  },
+});
