c6f607cd5e7a88bd84b8e03211a481ba5c462cc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:00:33 +0000

    Backed out 13 changesets (bug 1165486) for ASAN jsreftest crashes.
    Backed out changeset 24966b67d232 (bug 1165486)
    Backed out changeset 4a65eeea4d9a (bug 1165486)
    Backed out changeset c5301aa7a2ae (bug 1165486)
    Backed out changeset fea908d8a836 (bug 1165486)
    Backed out changeset ca9561cbcd29 (bug 1165486)
    Backed out changeset 91a3217d4cd0 (bug 1165486)
    Backed out changeset e74163801eef (bug 1165486)
    Backed out changeset ec5df87d2ee5 (bug 1165486)
    Backed out changeset 17d21020a786 (bug 1165486)
    Backed out changeset 6318eba2d3fe (bug 1165486)
    Backed out changeset a103caa36183 (bug 1165486)
    Backed out changeset 18c025c16bed (bug 1165486)
    Backed out changeset b32fcdc115b6 (bug 1165486)
    
    CLOSED TREE
    
    UltraBlame original commit: bfc988bd3c77b961248c666a4edd83de2a102490

--- js/src/jsscript.h
+++ js/src/jsscript.h
@@ -55,15 +55,6 @@ namespace frontend {
     class UpvarCookie;
 }
 
-namespace detail {
-
-
-
-bool
-CopyScript(JSContext* cx, HandleObject scriptStaticScope, HandleScript src, HandleScript dst);
-
-} 
-
 }
 
 
@@ -139,10 +130,6 @@ struct BlockScopeArray {
 };
 
 class YieldOffsetArray {
-    friend bool
-    detail::CopyScript(JSContext* cx, HandleObject scriptStaticScope, HandleScript src,
-                       HandleScript dst);
-
     uint32_t*       vector_;   
     uint32_t        length_;    
 
@@ -768,6 +755,16 @@ bool
 XDRScript(XDRState<mode>* xdr, HandleObject enclosingScope, HandleScript enclosingScript,
           HandleFunction fun, MutableHandleScript scriptp);
 
+enum PollutedGlobalScopeOption {
+    HasPollutedGlobalScope,
+    HasCleanGlobalScope
+};
+
+JSScript*
+CloneScript(JSContext* cx, HandleObject enclosingScope, HandleFunction fun, HandleScript script,
+            PollutedGlobalScopeOption polluted = HasCleanGlobalScope,
+            NewObjectKind newKind = GenericObject);
+
 template<XDRMode mode>
 bool
 XDRLazyScript(XDRState<mode>* xdr, HandleObject enclosingScope, HandleScript enclosingScript,
@@ -787,13 +784,13 @@ class JSScript : public js::gc::TenuredCell
     template <js::XDRMode mode>
     friend
     bool
-    js::XDRScript(js::XDRState<mode>* xdr, js::HandleObject enclosingScope,
-                  js::HandleScript enclosingScript,
+    js::XDRScript(js::XDRState<mode>* xdr, js::HandleObject enclosingScope, js::HandleScript enclosingScript,
                   js::HandleFunction fun, js::MutableHandleScript scriptp);
 
-    friend bool
-    js::detail::CopyScript(JSContext* cx, js::HandleObject scriptStaticScope, js::HandleScript src,
-                           js::HandleScript dst);
+    friend JSScript*
+    js::CloneScript(JSContext* cx, js::HandleObject enclosingScope, js::HandleFunction fun,
+                    js::HandleScript src, js::PollutedGlobalScopeOption polluted,
+                    js::NewObjectKind newKind);
 
   public:
     
@@ -939,7 +936,7 @@ class JSScript : public js::gc::TenuredCell
     
     
     
-    bool hasNonSyntacticScope_:1;
+    bool hasPollutedGlobalScope_:1;
 
     
     bool selfHosted_:1;
@@ -1181,8 +1178,8 @@ class JSScript : public js::gc::TenuredCell
 
     bool explicitUseStrict() const { return explicitUseStrict_; }
 
-    bool hasNonSyntacticScope() const {
-        return hasNonSyntacticScope_;
+    bool hasPollutedGlobalScope() const {
+        return hasPollutedGlobalScope_;
     }
 
     bool selfHosted() const { return selfHosted_; }
@@ -2296,12 +2293,9 @@ DescribeScriptedCallerForCompilation(JSContext* cx, MutableHandleScript maybeScr
                                      uint32_t* pcOffset, bool* mutedErrors,
                                      LineOption opt = NOT_CALLED_FROM_JSOP_EVAL);
 
-JSScript*
-CloneScriptIntoFunction(JSContext* cx, HandleObject enclosingScope, HandleFunction fun,
-                        HandleScript src);
-
-JSScript*
-CloneGlobalScript(JSContext* cx, Handle<ScopeObject*> enclosingScope, HandleScript src);
+bool
+CloneFunctionScript(JSContext* cx, HandleFunction original, HandleFunction clone,
+                    PollutedGlobalScopeOption polluted, NewObjectKind newKind);
 
 } 
 
