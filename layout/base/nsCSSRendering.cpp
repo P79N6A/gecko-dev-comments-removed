7422a33e293efee380d3614b1f7825f7ea20e016
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:17:25 +0000

    Bug 1107256 - Correct the position of text decoration lines on vertical text. r=smontagu
    UltraBlame original commit: 15cff727b0d61fee04d277d74d8ab96689c87969

--- layout/base/nsCSSRendering.cpp
+++ layout/base/nsCSSRendering.cpp
@@ -4202,7 +4202,7 @@ nsCSSRendering::PaintDecorationLine(nsIFrame* aFrame,
 
   
   if (aVertical) {
-    rect.x -= lineThickness / 2;
+    rect.x += lineThickness / 2;
   } else {
     rect.y += lineThickness / 2;
   }
@@ -4404,7 +4404,7 @@ nsCSSRendering::DecorationLineToPath(nsIFrame* aFrame,
 
   
   if (aVertical) {
-    rect.x -= lineThickness / 2;
+    rect.x += lineThickness / 2;
     aGfxContext->Rectangle
       (gfxRect(gfxPoint(rect.TopLeft() - gfxPoint(lineThickness / 2, 0.0)),
                gfxSize(lineThickness, rect.Height())));
