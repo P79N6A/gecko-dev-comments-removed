e7f75c6548c95c505a8989da82e97352dd7213b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:30:43 +0000

    Bug 1012677 - Investigate delayed initialization of nsILoginManager in Firefox for Android r=margaret
    UltraBlame original commit: e21dd0c363f8b1d78147424422eda63def90f361

--- mobile/android/chrome/content/browser.js
+++ mobile/android/chrome/content/browser.js
@@ -304,6 +304,19 @@ var BrowserApp = {
         BrowserApp.deck.removeEventListener("DOMContentLoaded", BrowserApp_delayedStartup, false);
         Services.obs.notifyObservers(window, "browser-delayed-startup-finished", "");
         sendMessageToJava({ type: "Gecko:DelayedStartup" });
+
+        
+        Services.tm.mainThread.dispatch(function() {
+          
+          Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager);
+        }, Ci.nsIThread.DISPATCH_NORMAL);
+
+#ifdef MOZ_SAFE_BROWSING
+        Services.tm.mainThread.dispatch(function() {
+          
+          SafeBrowsing.init();
+        }, Ci.nsIThread.DISPATCH_NORMAL);
+#endif
       } catch(ex) { console.log(ex); }
     }, false);
 
@@ -400,9 +413,6 @@ var BrowserApp = {
     ShumwayUtils.init();
 #endif
 
-    
-    Cc["@mozilla.org/login-manager;1"].getService(Ci.nsILoginManager);
-
     let url = null;
     let pinned = false;
     if ("arguments" in window) {
@@ -441,11 +451,6 @@ var BrowserApp = {
 
     
     sendMessageToJava({ type: "Gecko:Ready" });
-
-#ifdef MOZ_SAFE_BROWSING
-    
-    setTimeout(function() { SafeBrowsing.init(); }, 5000);
-#endif
   },
 
   get _startupStatus() {
