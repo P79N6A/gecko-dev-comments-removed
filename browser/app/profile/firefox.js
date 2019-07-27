89d80ca5ff1285436ee122b516b51103e209fa57
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:20:59 +0000

    Bug 1140045 - Disable desktop reader view toolbar button on Nightly. r=me
    UltraBlame original commit: 2205ab3f21a6a2a92456c56457922fa2c47a420e

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1887,10 +1887,8 @@ pref("dom.ipc.reportProcessHangs", false);
 pref("dom.ipc.reportProcessHangs", true);
 #endif
 
-#ifndef NIGHTLY_BUILD
 
 pref("reader.parse-on-load.enabled", false);
-#endif
 
 
 pref("browser.readinglist.enabled", false);
