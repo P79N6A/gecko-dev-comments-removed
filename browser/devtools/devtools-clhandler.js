f92f361e1f19d2b9dd4625237d23bd573edaaca7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:40:17 +0000

    Bug 1119894 - Add start-debugger-server to Firefox. r=past
    UltraBlame original commit: 7536897c179d5ee387e16e38d786ecbea48c22b0

--- browser/devtools/devtools-clhandler.js
+++ browser/devtools/devtools-clhandler.js
@@ -23,6 +23,18 @@ devtoolsCommandlineHandler.prototype = {
     if (debuggerFlag) {
       this.handleDebuggerFlag(cmdLine);
     }
+    let debuggerServerFlag;
+    try {
+      debuggerServerFlag =
+        cmdLine.handleFlagWithParam("start-debugger-server", false);
+    } catch(e) {
+      
+      
+      debuggerServerFlag = cmdLine.handleFlag("start-debugger-server", false);
+    }
+    if (debuggerServerFlag) {
+      this.handleDebuggerServerFlag(cmdLine, debuggerServerFlag);
+    }
   },
 
   handleConsoleFlag: function(cmdLine) {
@@ -43,23 +55,68 @@ devtoolsCommandlineHandler.prototype = {
     }
   },
 
-  handleDebuggerFlag: function(cmdLine) {
+  _isRemoteDebuggingEnabled() {
     let remoteDebuggingEnabled = false;
     try {
       remoteDebuggingEnabled = kDebuggerPrefs.every((pref) => Services.prefs.getBoolPref(pref));
     } catch (ex) {
       Cu.reportError(ex);
-      return;
+      return false;
     }
-    if (remoteDebuggingEnabled) {
-      Cu.import("resource:///modules/devtools/ToolboxProcess.jsm");
-      BrowserToolboxProcess.init();
-    } else {
+    if (!remoteDebuggingEnabled) {
       let errorMsg = "Could not run chrome debugger! You need the following prefs " +
                      "to be set to true: " + kDebuggerPrefs.join(", ");
       Cu.reportError(errorMsg);
       
+      
       dump(errorMsg + "\n");
+    }
+    return remoteDebuggingEnabled;
+  },
+
+  handleDebuggerFlag: function(cmdLine) {
+    if (!this._isRemoteDebuggingEnabled()) {
+      return;
+    }
+    Cu.import("resource:///modules/devtools/ToolboxProcess.jsm");
+    BrowserToolboxProcess.init();
+
+    if (cmdLine.state == Ci.nsICommandLine.STATE_REMOTE_AUTO) {
+      cmdLine.preventDefault = true;
+    }
+  },
+
+  handleDebuggerServerFlag: function(cmdLine, portOrPath) {
+    if (!this._isRemoteDebuggingEnabled()) {
+      return;
+    }
+    if (portOrPath === true) {
+      
+      portOrPath = 6000;
+    }
+    let { DevToolsLoader } =
+      Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
+
+    try {
+      
+      
+      
+      
+      
+      
+      let serverLoader = new DevToolsLoader();
+      serverLoader.invisibleToDebugger = true;
+      serverLoader.main("devtools/server/main");
+      let debuggerServer = serverLoader.DebuggerServer;
+      debuggerServer.init();
+      debuggerServer.addBrowserActors();
+
+      let listener = debuggerServer.createListener();
+      listener.portOrPath = portOrPath;
+      listener.open();
+      dump("Started debugger server on " + portOrPath + "\n");
+    } catch(e) {
+      dump("Unable to start debugger server on " + portOrPath + ": " + e);
     }
 
     if (cmdLine.state == Ci.nsICommandLine.STATE_REMOTE_AUTO) {
@@ -68,7 +125,10 @@ devtoolsCommandlineHandler.prototype = {
   },
 
   helpInfo : "  --jsconsole        Open the Browser Console.\n" +
-             "  --jsdebugger       Open the Browser Toolbox.\n",
+             "  --jsdebugger       Open the Browser Toolbox.\n" +
+             "  --start-debugger-server [port|path] " +
+             "Start the debugger server on a TCP port or " +
+             "Unix domain socket path.  Defaults to TCP port 6000.\n",
 
   classID: Components.ID("{9e9a9283-0ce9-4e4a-8f1c-ba129a032c32}"),
   QueryInterface: XPCOMUtils.generateQI([Ci.nsICommandLineHandler]),
