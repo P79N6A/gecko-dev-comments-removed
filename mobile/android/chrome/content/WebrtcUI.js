0325672fb0b3aef75051b464b025ddb3b0147849
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:37:00 +0000

    backout 8ff6b2bf34cd (bug 989094) for regressions. r=backout
    UltraBlame original commit: a3ae1a0cf65f30674b13d01d37554c303be07579

--- mobile/android/chrome/content/WebrtcUI.js
+++ mobile/android/chrome/content/WebrtcUI.js
@@ -4,14 +4,9 @@
 "use strict";
 
 XPCOMUtils.defineLazyModuleGetter(this, "Notifications", "resource://gre/modules/Notifications.jsm");
-XPCOMUtils.defineLazyServiceGetter(this, "contentPrefs",
-                                   "@mozilla.org/content-pref/service;1",
-                                   "nsIContentPrefService2");
 
 var WebrtcUI = {
   _notificationId: null,
-  VIDEO_SOURCE: "videoSource",
-  AUDIO_SOURCE: "audioDevice",
 
   observe: function(aSubject, aTopic, aData) {
     if (aTopic === "getUserMedia:request") {
@@ -83,42 +78,39 @@ var WebrtcUI = {
 
     contentWindow.navigator.mozGetUserMediaDevices(
       constraints,
-      function (aDevices) {
-        WebrtcUI.prompt(contentWindow, aSubject.callID, constraints.audio, constraints.video, aDevices);
+      function (devices) {
+        WebrtcUI.prompt(contentWindow, aSubject.callID, constraints.audio,
+                        constraints.video, devices);
       },
-      Cu.reportError, aSubject.innerWindowID);
+      function (error) {
+        Cu.reportError(error);
+      },
+      aSubject.innerWindowID);
   },
 
-  getDeviceButtons: function(aAudioDevices, aVideoDevices, aCallID, aHost) {
+  getDeviceButtons: function(audioDevices, videoDevices, aCallID) {
     return [{
       label: Strings.browser.GetStringFromName("getUserMedia.denyRequest.label"),
-      callback: () => {
+      callback: function() {
         Services.obs.notifyObservers(null, "getUserMedia:response:deny", aCallID);
       }
-    }, {
+    },
+    {
       label: Strings.browser.GetStringFromName("getUserMedia.shareRequest.label"),
-      callback: (checked , inputs) => {
+      callback: function(checked , inputs) {
         let allowedDevices = Cc["@mozilla.org/supports-array;1"].createInstance(Ci.nsISupportsArray);
 
         let audioId = 0;
-        if (inputs && inputs[this.AUDIO_SOURCE] != undefined) {
-          audioId = inputs[this.AUDIO_SOURCE];
-        }
-
-        if (aAudioDevices[audioId]) {
-          allowedDevices.AppendElement(aAudioDevices[audioId]);
-          this.setDefaultDevice(this.AUDIO_SOURCE, aAudioDevices[audioId].name, aHost);
-        }
+        if (inputs && inputs.audioDevice != undefined)
+          audioId = inputs.audioDevice;
+        if (audioDevices[audioId])
+          allowedDevices.AppendElement(audioDevices[audioId]);
 
         let videoId = 0;
-        if (inputs && inputs[this.VIDEO_SOURCE] != undefined) {
-          videoId = inputs[this.VIDEO_SOURCE];
-        }
-
-        if (aVideoDevices[videoId]) {
-          allowedDevices.AppendElement(aVideoDevices[videoId]);
-          this.setDefaultDevice(this.VIDEO_SOURCE, aVideoDevices[videoId].name, aHost);
-        }
+        if (inputs && inputs.videoSource != undefined)
+          videoId = inputs.videoSource;
+        if (videoDevices[videoId])
+          allowedDevices.AppendElement(videoDevices[videoId]);
 
         Services.obs.notifyObservers(allowedDevices, "getUserMedia:response:allow", aCallID);
       }
@@ -129,34 +121,30 @@ var WebrtcUI = {
   _getList: function(aDevices, aType) {
     let defaultCount = 0;
     return aDevices.map(function(device) {
-      let name = device.name;
-      
-      let res = /Camera\ \d+,\ Facing (front|back)/.exec(name);
-      if (res) {
-        return Strings.browser.GetStringFromName("getUserMedia." + aType + "." + res[1] + "Camera");
-      }
+        
+        let res = /Camera\ \d+,\ Facing (front|back)/.exec(device.name);
+        if (res)
+          return Strings.browser.GetStringFromName("getUserMedia." + aType + "." + res[1] + "Camera");
 
-      if (name.startsWith("&") && name.endsWith(";")) {
-        return Strings.browser.GetStringFromName(name.substring(1, name.length -1));
-      }
+        if (device.name.startsWith("&") && device.name.endsWith(";"))
+          return Strings.browser.GetStringFromName(device.name.substring(1, device.name.length -1));
 
-      if (name.trim() == "") {
-        defaultCount++;
-        return Strings.browser.formatStringFromName("getUserMedia." + aType + ".default", [defaultCount], 1);
-      }
-
-      return name;
-    }, this);
+        if (device.name.trim() == "") {
+          defaultCount++;
+          return Strings.browser.formatStringFromName("getUserMedia." + aType + ".default", [defaultCount], 1);
+        }
+        return device.name
+      }, this);
   },
 
-  _addDevicesToOptions: function(aDevices, aType, aOptions, aHost, aContext) {
-    if (aDevices.length == 0) {
-      return Promise.resolve(aOptions);
-    }
+  _addDevicesToOptions: function(aDevices, aType, aOptions, extraOptions) {
+    if (aDevices.length) {
 
-    let updateOptions = () => {
       
       let list = this._getList(aDevices, aType);
+      if (extraOptions)
+        list = list.concat(extraOptions);
+
       if (list.length > 0) {
         aOptions.inputs.push({
           id: aType,
@@ -164,96 +152,40 @@ var WebrtcUI = {
           label: Strings.browser.GetStringFromName("getUserMedia." + aType + ".prompt"),
           values: list
         });
+
       }
-
-      return aOptions;
-    }
-
-    return this.getDefaultDevice(aType, aHost, aContext).then((defaultDevice) => {
-      aDevices.sort((a, b) => {
-        if (b.name === defaultDevice) return 1;
-        return 0;
-      });
-      return updateOptions();
-    }).catch(updateOptions);
-  },
-
-  
-  
-  setDefaultDevice: function(aType, aValue, aHost, aContext) {
-    if (aHost) {
-      contentPrefs.set(aHost, "webrtc." + aType, aValue, aContext);
-    } else {
-      contentPrefs.setGlobal("webrtc." + aType, aValue, aContext);
     }
   },
 
-  _checkContentPref(aHost, aType, aContext) {
-    return new Promise((resolve, reject) => {
-      let result = null;
-      let handler = {
-        handleResult: (aResult) => result = aResult,
-        handleCompletion: function(aReason) {
-          if (aReason == Components.interfaces.nsIContentPrefCallback2.COMPLETE_OK &&
-              result instanceof Components.interfaces.nsIContentPref) {
-            resolve(result.value);
-          } else {
-            reject(result);
-          }
-        }
-      };
-
-      if (aHost) {
-        contentPrefs.getByDomainAndName(aHost, "webrtc." + aType, aContext, handler);
-      } else {
-        contentPrefs.getGlobal("webrtc." + aType, aContext, handler);
-      }
-    });
-  },
-
-  
-  getDefaultDevice: function(aType, aHost, aContext) {
-    return this._checkContentPref(aHost, aType, aContext).catch(() => {
-      
-      return this._checkContentPref(null, aType, aContext);
-    });
-  },
-
-  prompt: function (aWindow, aCallID, aAudioRequested, aVideoRequested, aDevices) {
+  prompt: function prompt(aContentWindow, aCallID, aAudioRequested,
+                          aVideoRequested, aDevices) {
     let audioDevices = [];
     let videoDevices = [];
-
-    
     for (let device of aDevices) {
       device = device.QueryInterface(Ci.nsIMediaDevice);
       switch (device.type) {
       case "audio":
-        if (aAudioRequested) {
+        if (aAudioRequested)
           audioDevices.push(device);
-        }
         break;
       case "video":
-        if (aVideoRequested) {
+        if (aVideoRequested)
           videoDevices.push(device);
-        }
         break;
       }
     }
 
-    
     let requestType;
-    if (audioDevices.length && videoDevices.length) {
+    if (audioDevices.length && videoDevices.length)
       requestType = "CameraAndMicrophone";
-    } else if (audioDevices.length) {
+    else if (audioDevices.length)
       requestType = "Microphone";
-    } else if (videoDevices.length) {
+    else if (videoDevices.length)
       requestType = "Camera";
-    } else {
+    else
       return;
-    }
 
-    let host = aWindow.document.documentURIObject.host;
-    
+    let host = aContentWindow.document.documentURIObject.host;
     let requestor = BrowserApp.manifest ? "'" + BrowserApp.manifest.name  + "'" : host;
     let message = Strings.browser.formatStringFromName("getUserMedia.share" + requestType + ".message", [ requestor ], 1);
 
@@ -261,20 +193,24 @@ var WebrtcUI = {
     
     
     
-    if (videoDevices.length > 0 && audioDevices.length > 0) {
-      videoDevices.push({ name: Strings.browser.GetStringFromName("getUserMedia.videoSource.none") });
-      audioDevices.push({ name: Strings.browser.GetStringFromName("getUserMedia.audioDevice.none") });
+    var extraItems = null;
+    if (videoDevices.length > 1 || audioDevices.length > 0) {
+      
+      if (audioDevices.length > 0)
+        extraItems = [ Strings.browser.GetStringFromName("getUserMedia.videoSource.none") ];
+      
+      this._addDevicesToOptions(videoDevices, "videoSource", options, extraItems);
     }
 
-    let loadContext = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
-                             .getInterface(Ci.nsIWebNavigation)
-                             .QueryInterface(Ci.nsILoadContext);
-    
-    this._addDevicesToOptions(videoDevices, this.VIDEO_SOURCE, options, host, loadContext).then((aOptions) => {
-      return this._addDevicesToOptions(audioDevices, this.AUDIO_SOURCE, aOptions, host, loadContext);
-    }).catch(Cu.reportError).then((aOptions) => {
-      let buttons = this.getDeviceButtons(audioDevices, videoDevices, aCallID, host);
-      NativeWindow.doorhanger.show(message, "webrtc-request", buttons, BrowserApp.selectedTab.id, aOptions);
-    });
+    if (audioDevices.length > 1 || videoDevices.length > 0) {
+      
+      if (videoDevices.length > 0)
+        extraItems = [ Strings.browser.GetStringFromName("getUserMedia.audioDevice.none") ];
+      this._addDevicesToOptions(audioDevices, "audioDevice", options, extraItems);
+    }
+
+    let buttons = this.getDeviceButtons(audioDevices, videoDevices, aCallID);
+
+    NativeWindow.doorhanger.show(message, "webrtc-request", buttons, BrowserApp.selectedTab.id, options);
   }
 }
