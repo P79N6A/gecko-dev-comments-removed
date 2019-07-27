ddf6430b5c584f68a1f6dafd5f7d84259fafc664
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:21:03 +0000

    Bug 1140238 - Part 2: Adjust ranges after changing selection direction. r=mats
    When dragging the second caret (eDirNext) over non-selectable elements,
    the last range is not generated. Therefore when dragging the first
    caret, we will change the selection direction to eDirPrevious, the last
    range will be excluded by AutoPrepareFocusRange.
    
    We add a new function to adjust generated bit and anchor focus range
    after changing selection direction.
    
    UltraBlame original commit: 44438a4eef3e35c588bba02bf2126820763ef98f

--- layout/generic/Selection.h
+++ layout/generic/Selection.h
@@ -126,6 +126,7 @@ public:
   void         SetDirection(nsDirection aDir){mDirection = aDir;}
   nsresult     SetAnchorFocusToRange(nsRange *aRange);
   void         ReplaceAnchorFocusRange(nsRange *aRange);
+  void         AdjustAnchorFocusForMultiRange(nsDirection aDirection);
 
   
   NS_IMETHOD   GetPrimaryFrameForAnchorNode(nsIFrame **aResultFrame);
