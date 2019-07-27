e0292ba19bf29738a4fde76ab25ce5041e3fd0a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 03:00:36 +0000

    Bug 1074496 - "Disable import from Android in Guest mode" [r=liuche]
    UltraBlame original commit: c2e81e5b109296f3ab78e0afd342f2181d242e07

--- mobile/android/base/preferences/ClearOnShutdownPref.java
+++ mobile/android/base/preferences/ClearOnShutdownPref.java
@@ -19,12 +19,13 @@ public class ClearOnShutdownPref implements GeckoPreferences.PrefHandler {
     public static final String PREF = GeckoPreferences.NON_PREF_PREFIX + "history.clear_on_exit";
 
     @Override
-    public void setupPref(Context context, Preference pref) {
+    public boolean setupPref(Context context, Preference pref) {
         
         
         final SharedPreferences prefs = GeckoSharedPrefs.forProfile(context);
         final Set<String> clearItems = PrefUtils.getStringSet(prefs, PREF, new HashSet<String>());
         ((ListCheckboxPreference) pref).setChecked(clearItems.size() > 0);
+        return true;
     }
 
     @Override
