8bc8435d9a4ead6c657d5470fb07d011767a9230
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:11:32 +0000

    Bug 1137483 - Move search engine bar under keyboard when shown. r=liuche
    This requires the window to resize when the keyboard is shown, which required a
    revision in when we draw the window background.
    
    UltraBlame original commit: 3a1669b7df0f9e78b216e807962847023cd92dd5

--- mobile/android/base/BrowserApp.java
+++ mobile/android/base/BrowserApp.java
@@ -2632,6 +2632,16 @@ public class BrowserApp extends GeckoApp
 
         fm.beginTransaction().add(R.id.search_container, mBrowserSearch, BROWSER_SEARCH_TAG).commitAllowingStateLoss();
         mBrowserSearch.setUserVisibleHint(true);
+
+        
+        
+        
+        
+        
+        
+        
+        
+        getActivity().getWindow().setBackgroundDrawableResource(android.R.color.white);
     }
 
     private void hideBrowserSearch() {
@@ -2648,6 +2658,8 @@ public class BrowserApp extends GeckoApp
         getSupportFragmentManager().beginTransaction()
                 .remove(mBrowserSearch).commitAllowingStateLoss();
         mBrowserSearch.setUserVisibleHint(false);
+
+        getWindow().setBackgroundDrawable(null);
     }
 
     
