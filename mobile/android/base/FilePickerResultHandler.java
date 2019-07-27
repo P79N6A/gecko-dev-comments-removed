bdaf3f6857b151efada8c244ef1456ecc6b7950b
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:46:59 +0000

    Bug 1031429 - Fix NPE by adding a NULL check. r=bnicholson
    UltraBlame original commit: f526a385989aa1d31b4a677fd412b461389f1020

--- mobile/android/base/FilePickerResultHandler.java
+++ mobile/android/base/FilePickerResultHandler.java
@@ -250,7 +250,7 @@ class FilePickerResultHandler implements ActivityResultHandler {
         
         @Override
         public void onTabChanged(Tab tab, Tabs.TabEvents msg, Object data) {
-            if (tab.getId() != tabId) {
+            if ((tab == null) || (tab.getId() != tabId)) {
                 return;
             }
 
