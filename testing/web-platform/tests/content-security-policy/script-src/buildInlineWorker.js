95255d4c1d7d1bd59618b8f92626f71c3360e226
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:25:53 +0000

    Bug 1086999 - CSP: Asterisk (*) wildcard should not allow blob:, data:, or filesystem: when matching source expressions - web platform test update (r=sstamm,jgraham)
    UltraBlame original commit: 772945b1130d44087ad2009c3112cb69e307549e

--- testing/web-platform/tests/content-security-policy/script-src/buildInlineWorker.js
+++ testing/web-platform/tests/content-security-policy/script-src/buildInlineWorker.js
@@ -1,20 +1,20 @@
 (function ()
 {
- var test = new async_test("test inline worker");
  var workerSource = document.getElementById('inlineWorker');
-
  var blob = new Blob([workerSource.textContent]);
 
  
  var url = window.URL.createObjectURL(blob);
 
- var worker = new Worker(url);
+ try {
+   var worker = new Worker(url);
+ }
+ catch (e) {
+   done();
+ }
 
  worker.addEventListener('message', function(e) {
-    test.step(function () {
-        assert_not_equals(e.data, 'fail', 'inline script ran');
-        test.done();
-    })
+   assert_unreached("script ran");
  }, false);
 
  worker.postMessage('');
