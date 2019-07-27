727a67f92459a4ca1bbf0bd7fb2ab8551cf1148f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:52:00 +0000

    Bug 1146214 - Combine FenceHandle and FenceHandleFromChild. r=nical
    UltraBlame original commit: 0af24986e84357eda91521228114c6d77a83564e

--- gfx/layers/ipc/FenceUtilsGonk.cpp
+++ gfx/layers/ipc/FenceUtilsGonk.cpp
@@ -29,7 +29,6 @@ ParamTraits<FenceHandle>::Write(Message* aMsg,
 #endif
   size_t nbytes = flattenable->getFlattenedSize();
   size_t nfds = flattenable->getFdCount();
-
   char data[nbytes];
   int fds[nfds];
 
@@ -56,7 +55,7 @@ ParamTraits<FenceHandle>::Write(Message* aMsg,
     
     
     
-    aMsg->WriteFileDescriptor(FileDescriptor(fds[n], false));
+    aMsg->WriteFileDescriptor(FileDescriptor(dup(fds[n]), true));
   }
 }
 
@@ -91,106 +90,6 @@ ParamTraits<FenceHandle>::Read(const Message* aMsg,
     
     
     
-    bool sameProcess = (XRE_GetProcessType() == GeckoProcessType_Default);
-    int dupFd = sameProcess ? dup(fd.fd) : fd.fd;
-    fds[n] = dupFd;
-  }
-
-  sp<Fence> buffer(new Fence());
-#if ANDROID_VERSION >= 19
-  
-  void const *pdata = (void const *)data;
-  int const *pfds = fds;
-
-  if (NO_ERROR == buffer->unflatten(pdata, nbytes, pfds, nfds)) {
-#else
-  Flattenable *flattenable = buffer.get();
-
-  if (NO_ERROR == flattenable->unflatten(data, nbytes, fds, nfds)) {
-#endif
-    aResult->mFence = buffer;
-    return true;
-  }
-  return false;
-}
-
-void
-ParamTraits<FenceHandleFromChild>::Write(Message* aMsg,
-                                         const paramType& aParam)
-{
-#if ANDROID_VERSION >= 19
-  sp<Fence> flattenable = aParam.mFence;
-#else
-  Flattenable *flattenable = aParam.mFence.get();
-#endif
-  size_t nbytes = flattenable->getFlattenedSize();
-  size_t nfds = flattenable->getFdCount();
-
-  char data[nbytes];
-  int fds[nfds];
-
-#if ANDROID_VERSION >= 19
-  
-  void *pdata = (void *)data;
-  int *pfds = fds;
-
-  flattenable->flatten(pdata, nbytes, pfds, nfds);
-
-  
-  
-  
-  
-  nbytes = flattenable->getFlattenedSize();
-  nfds = flattenable->getFdCount();
-#else
-  flattenable->flatten(data, nbytes, fds, nfds);
-#endif
-  aMsg->WriteSize(nbytes);
-  aMsg->WriteSize(nfds);
-  aMsg->WriteBytes(data, nbytes);
-  for (size_t n = 0; n < nfds; ++n) {
-    
-    
-    
-    
-    
-    bool sameProcess = (XRE_GetProcessType() == GeckoProcessType_Default);
-    int dupFd = sameProcess ? dup(fds[n]) : fds[n];
-    
-
-    
-    
-    
-    aMsg->WriteFileDescriptor(FileDescriptor(dupFd, false));
-  }
-}
-
-bool
-ParamTraits<FenceHandleFromChild>::Read(const Message* aMsg,
-                                        void** aIter, paramType* aResult)
-{
-  size_t nbytes;
-  size_t nfds;
-  const char* data;
-
-  if (!aMsg->ReadSize(aIter, &nbytes) ||
-      !aMsg->ReadSize(aIter, &nfds) ||
-      !aMsg->ReadBytes(aIter, &data, nbytes)) {
-    return false;
-  }
-
-  
-  
-  if (nfds > aMsg->num_fds()) {
-    return false;
-  }
-  int fds[nfds];
-
-  for (size_t n = 0; n < nfds; ++n) {
-    FileDescriptor fd;
-    if (!aMsg->ReadFileDescriptor(aIter, &fd)) {
-      return false;
-    }
     fds[n] = fd.fd;
   }
 
@@ -217,13 +116,14 @@ ParamTraits<FenceHandleFromChild>::Read(const Message* aMsg,
 namespace mozilla {
 namespace layers {
 
-FenceHandle::FenceHandle(const sp<Fence>& aFence)
-  : mFence(aFence)
+FenceHandle::FenceHandle()
+  : mFence(android::Fence::NO_FENCE)
 {
 }
 
-FenceHandle::FenceHandle(const FenceHandleFromChild& aFenceHandle) {
-  mFence = aFenceHandle.mFence;
+FenceHandle::FenceHandle(const sp<Fence>& aFence)
+  : mFence(aFence)
+{
 }
 
 void
@@ -248,11 +148,6 @@ FenceHandle::Merge(const FenceHandle& aFenceHandle)
     }
     mFence = mergedFence;
   }
-}
-
-FenceHandleFromChild::FenceHandleFromChild(const sp<Fence>& aFence)
-  : mFence(aFence)
-{
 }
 
 } 
