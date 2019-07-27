bebc11e26de98ec8701e3def1fcc65127906079c
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:24:36 +0000

    Bug 1141986 - introduce a TruncateToInt32 policy. r=h4writer
    UltraBlame original commit: 1034b9b9d6a0d39a6ab71c272a713773bb728f77

--- js/src/jit/TypePolicy.h
+++ js/src/jit/TypePolicy.h
@@ -194,6 +194,18 @@ class ConvertToInt32Policy final : public TypePolicy
 
 
 template <unsigned Op>
+class TruncateToInt32Policy final : public TypePolicy
+{
+  public:
+    EMPTY_DATA_;
+    static bool staticAdjustInputs(TempAllocator& alloc, MInstruction* def);
+    virtual bool adjustInputs(TempAllocator& alloc, MInstruction* def) override {
+        return staticAdjustInputs(alloc, def);
+    }
+};
+
+
+template <unsigned Op>
 class DoublePolicy final : public TypePolicy
 {
   public:
