26c52fb695bf4fa335a9e2dc80214b14854271a2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:44:50 +0000

    Backed out changeset d4fca818c9a8 (bug 650161)
    UltraBlame original commit: 72ce360905d26daaa9f510ff8764d83025ae8efb

--- js/src/jit/mips/Assembler-mips.cpp
+++ js/src/jit/mips/Assembler-mips.cpp
@@ -262,7 +262,6 @@ TraceDataRelocations(JSTracer *trc, uint8_t *buffer, CompactBufferReader &reader
 
         
         gc::MarkGCThingUnbarriered(trc, reinterpret_cast<void **>(&ptr), "ion-masm-ptr");
-        Assembler::UpdateLuiOriValue(inst, inst->next(), uint32_t(ptr));
     }
 }
 
@@ -277,7 +276,6 @@ TraceDataRelocations(JSTracer *trc, MIPSBuffer *buffer, CompactBufferReader &rea
 
         
         gc::MarkGCThingUnbarriered(trc, reinterpret_cast<void **>(&ptr), "ion-masm-ptr");
-        Assembler::UpdateLuiOriValue(iter.cur(), iter.next(), uint32_t(ptr));
     }
 }
 
