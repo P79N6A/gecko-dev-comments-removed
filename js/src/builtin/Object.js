22e0699af6128bba5794216043f6facb2a945481
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:40:26 +0000

    Bug 937855 - Implement Object.assign.  Patch is a tag-team effort with till.  Also add tests: most courtesy of André Bargull, some from me, some suggested by jwalden.  r=jwalden
    UltraBlame original commit: 53769e48d35bcdb4b69c97f64815d20fe3fcdda6

---
+++ js/src/builtin/Object.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+function ObjectStaticAssign(target, firstSource) {
+    
+    var to = ToObject(target);
+
+    
+    if (arguments.length < 2)
+        return to;
+
+    
+    var i = 1;
+    do {
+        
+        var nextSource = arguments[i];
+        var from = ToObject(nextSource);
+
+        
+        var keysArray = std_Object_getOwnPropertyNames(from);
+
+        
+        var len = keysArray.length;
+
+        
+        var nextIndex = 0;
+
+        
+        
+        const MISSING = {};
+        var pendingException = MISSING;
+
+        
+        while (nextIndex < len) {
+            
+            var nextKey = keysArray[nextIndex];
+
+            
+            try {
+                
+                
+                
+                
+                var desc = std_Object_getOwnPropertyDescriptor(from, nextKey);
+                if (desc !== undefined && desc.enumerable)
+                    to[nextKey] = from[nextKey];
+            } catch (e) {
+                if (pendingException === MISSING)
+                    pendingException = e;
+            }
+
+            
+            nextIndex++;
+        }
+
+        
+        if (pendingException !== MISSING)
+            throw pendingException;
+        i++;
+    } while (i < arguments.length);
+
+    
+    return to;
+}
+
