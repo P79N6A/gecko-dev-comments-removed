399d4d92dc703ec4dd9b251419ef2330431b3893
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:01:18 +0000

    Bug 1068949 - Some tests to ensure warnings are displayed appropriately. r=past
    UltraBlame original commit: 96e2e929a9f2f46839ca57c52c9a8e04b5ba3444

---
+++ browser/devtools/webconsole/test/browser_webconsole_certificate_messages.js
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+const TEST_BAD_URI = "https://sha1ee.example.com/browser/browser/devtools/webconsole/test/test-certificate-messages.html";
+const TEST_GOOD_URI = "https://sha256ee.example.com/browser/browser/devtools/webconsole/test/test-certificate-messages.html";
+const TRIGGER_MSG = "If you haven't seen ssl warnings yet, you won't";
+
+let gHud = undefined;
+
+function test() {
+  registerCleanupFunction(function () {
+    gHud = null;
+  });
+
+  addTab("data:text/html;charset=utf8,Web Console SHA-1 warning test");
+  browser.addEventListener("load", function _onLoad() {
+    browser.removeEventListener("load", _onLoad, true);
+    openConsole(null, loadBadDocument);
+  }, true);
+}
+
+function loadBadDocument(theHud) {
+  gHud = theHud;
+  browser.addEventListener("load", onBadLoad, true);
+  content.location = TEST_BAD_URI;
+}
+
+function onBadLoad(aEvent) {
+  browser.removeEventListener("load", onBadLoad, true);
+  testForWarningMessage();
+}
+
+function loadGoodDocument(theHud) {
+  gHud.jsterm.clearOutput()
+  browser.addEventListener("load", onGoodLoad, true);
+  content.location = TEST_GOOD_URI;
+}
+
+function onGoodLoad(aEvent) {
+  browser.removeEventListener("load", onGoodLoad, true);
+  testForNoWarning();
+}
+
+function testForWarningMessage() {
+  let aOutputNode = gHud.outputNode;
+
+  waitForSuccess({
+      name: "SHA1 warning displayed successfully",
+      validatorFn: function() {
+        return gHud.outputNode.textContent.indexOf("SHA-1") > -1;
+      },
+      successFn: loadGoodDocument,
+      failureFn: finishTest,
+    });
+}
+
+function testForNoWarning() {
+  let aOutputNode = gHud.outputNode;
+
+  waitForSuccess({
+      name: "SHA1 warning appropriately missed",
+      validatorFn: function() {
+        if (gHud.outputNode.textContent.indexOf(TRIGGER_MSG) > -1) {
+          return gHud.outputNode.textContent.indexOf("SHA-1") == -1;
+        }
+      },
+      successFn: finishTest,
+      failureFn: finishTest,
+    });
+}
