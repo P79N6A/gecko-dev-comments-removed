853e7b25955457a6dfb8db32c74bbda497cb28fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:21:31 +0000

    Bug 945584: Part 2 - Add CSS scroll snapping attributes to ScrollbarStyles (v10 Patch),r=mstange
    - ScrollbarStyles now carries additional variables to support new
      CSS scroll snapping attributes:
      - scroll-snap-type / scroll-snap-type-x / scroll-snap-type-y
      - scroll-snap-points-x / scroll-snap-points-y
      - scroll-snap-destination
      - (scroll-snap-coordinate does not apply to the scroll container)
    - Simplified the constructor and operator== for ScrollbarStyles.
    
    UltraBlame original commit: c86b570fbb7815b075f72216a98e2b20871ebe2d

---
+++ layout/base/ScrollbarStyles.cpp
@@ -0,0 +1,32 @@
+
+
+
+
+
+#include "ScrollbarStyles.h"
+#include "nsStyleStruct.h" 
+
+namespace mozilla {
+
+  ScrollbarStyles::ScrollbarStyles(uint8_t aH, uint8_t aV,
+                                   const nsStyleDisplay* aDisplay)
+    : mHorizontal(aH), mVertical(aV),
+      mScrollBehavior(aDisplay->mScrollBehavior),
+      mScrollSnapTypeX(aDisplay->mScrollSnapTypeX),
+      mScrollSnapTypeY(aDisplay->mScrollSnapTypeY),
+      mScrollSnapPointsX(aDisplay->mScrollSnapPointsX),
+      mScrollSnapPointsY(aDisplay->mScrollSnapPointsY),
+      mScrollSnapDestinationX(aDisplay->mScrollSnapDestination.mXPosition),
+      mScrollSnapDestinationY(aDisplay->mScrollSnapDestination.mYPosition) {}
+
+  ScrollbarStyles::ScrollbarStyles(const nsStyleDisplay* aDisplay)
+    : mHorizontal(aDisplay->mOverflowX), mVertical(aDisplay->mOverflowY),
+      mScrollBehavior(aDisplay->mScrollBehavior),
+      mScrollSnapTypeX(aDisplay->mScrollSnapTypeX),
+      mScrollSnapTypeY(aDisplay->mScrollSnapTypeY),
+      mScrollSnapPointsX(aDisplay->mScrollSnapPointsX),
+      mScrollSnapPointsY(aDisplay->mScrollSnapPointsY),
+      mScrollSnapDestinationX(aDisplay->mScrollSnapDestination.mXPosition),
+      mScrollSnapDestinationY(aDisplay->mScrollSnapDestination.mYPosition) {}
+
+}
