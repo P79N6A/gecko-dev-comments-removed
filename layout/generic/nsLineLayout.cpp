4acbc589a808d52b3539a0d7c90bc369eef805e9
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:53:35 +0000

    Bug 1146117 - Downgrade the assertion in FindNearestRubyBaseAncestor to warning. r=dbaron
    UltraBlame original commit: c4d86ce81aab3652e7432de502fb6718c0c04320

--- layout/generic/nsLineLayout.cpp
+++ layout/generic/nsLineLayout.cpp
@@ -2910,7 +2910,7 @@ FindNearestRubyBaseAncestor(nsIFrame* aFrame)
   
   
   
-  NS_ASSERTION(aFrame, "No ruby base ancestor?");
+  NS_WARN_IF_FALSE(aFrame, "no ruby base ancestor?");
   return aFrame;
 }
 
