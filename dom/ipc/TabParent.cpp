0aa04b076981362034752262e4916d7ad7bd047d
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:21:18 +0000

    Bug 1105468 - OpenFileAndSendFDRunnable::OpenFile needs to ensure we don't release off main thread. r=khuey
    If any of the file operations fail, we need to ensure we still dispatch to the main thread.
    
    If the dispatch fails, leak the runnable (and the things it owns) rather than crash when we try to release them off main thread.
    
    UltraBlame original commit: ac697e1121348618643867a0a6196b7a6c6f2912

--- dom/ipc/TabParent.cpp
+++ dom/ipc/TabParent.cpp
@@ -118,6 +118,7 @@ public:
 private:
     ~OpenFileAndSendFDRunnable()
     {
+        MOZ_ASSERT(NS_IsMainThread());
         MOZ_ASSERT(!mFD);
     }
 
@@ -170,7 +171,8 @@ private:
         }
     }
 
-    void OpenFile()
+    
+    void OpenFileImpl()
     {
         MOZ_ASSERT(!NS_IsMainThread());
         MOZ_ASSERT(!mFD);
@@ -184,10 +186,21 @@ private:
         NS_ENSURE_SUCCESS_VOID(rv);
 
         mFD = fd;
+    }
+
+    void OpenFile()
+    {
+        MOZ_ASSERT(!NS_IsMainThread());
+
+        OpenFileImpl();
 
         if (NS_FAILED(NS_DispatchToMainThread(this))) {
             NS_WARNING("Failed to dispatch to main thread!");
 
+            
+            
+            
+            NS_ADDREF(this);
             CloseFile();
         }
     }
