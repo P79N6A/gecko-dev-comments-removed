29d0e5ae5f7aac625bc28d9ecec9f83dabd92da7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:21:40 +0000

    Bug 1088070 - Instantiate print settings from the content process instead of the parent. r=Mossop.
    UltraBlame original commit: e58f9c6913b7e1df3b3c9dfce47bd25f57dbfb96

--- toolkit/content/browser-content.js
+++ toolkit/content/browser-content.js
@@ -384,7 +384,7 @@ let Printing = {
     let data = message.data;
     switch(message.name) {
       case "Printing:Preview:Enter": {
-        this.enterPrintPreview(objects.printSettings, objects.contentWindow);
+        this.enterPrintPreview(objects.contentWindow);
         break;
       }
 
@@ -404,19 +404,37 @@ let Printing = {
       }
 
       case "Printing:Print": {
-        this.print(objects.printSettings, objects.contentWindow);
+        this.print(objects.contentWindow);
         break;
       }
     }
   },
 
-  enterPrintPreview(printSettings, contentWindow) {
-    
-    
-    if (Cu.isCrossProcessWrapper(printSettings)) {
-      printSettings = null;
+  getPrintSettings() {
+    try {
+      let PSSVC = Cc["@mozilla.org/gfx/printsettings-service;1"]
+                    .getService(Ci.nsIPrintSettingsService);
+
+      let printSettings = PSSVC.globalPrintSettings;
+      if (!printSettings.printerName) {
+        printSettings.printerName = PSSVC.defaultPrinterName;
+      }
+      
+      PSSVC.initPrintSettingsFromPrinter(printSettings.printerName,
+                                         printSettings);
+      
+      PSSVC.initPrintSettingsFromPrefs(printSettings, true,
+                                       printSettings.kInitSaveAll);
+
+      return printSettings;
+    } catch(e) {
+      Components.utils.reportError(e);
     }
 
+    return null;
+  },
+
+  enterPrintPreview(contentWindow) {
     
     
     
@@ -439,6 +457,7 @@ let Printing = {
     addEventListener("printPreviewUpdate", onPrintPreviewReady);
 
     try {
+      let printSettings = this.getPrintSettings();
       docShell.printPreview.printPreview(printSettings, contentWindow, this);
     } catch(error) {
       
@@ -452,13 +471,8 @@ let Printing = {
     docShell.printPreview.exitPrintPreview();
   },
 
-  print(printSettings, contentWindow) {
-    
-    
-    if (Cu.isCrossProcessWrapper(printSettings)) {
-      printSettings = null;
-    }
-
+  print(contentWindow) {
+    let printSettings = this.getPrintSettings();
     let rv = Cr.NS_OK;
     try {
       let print = contentWindow.QueryInterface(Ci.nsIInterfaceRequestor)
