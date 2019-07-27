333a2b5ad2d67576348bbab12ea557d63f5fc282
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:26:30 +0000

    Bug 1146472 part 2.  Use JSOP_NEWOBJECT as needed even if the script is not compile-and-go.  r=luke
    UltraBlame original commit: 8bccbbf668c63c160ff9e27f19ee95b70d30430b

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -2223,8 +2223,6 @@ BytecodeEmitter::emitNewInit(JSProtoKey key)
 static bool
 IteratorResultShape(ExclusiveContext *cx, BytecodeEmitter *bce, unsigned *shape)
 {
-    MOZ_ASSERT(bce->script->compileAndGo());
-
     RootedPlainObject obj(cx);
     
     
@@ -2258,14 +2256,10 @@ IteratorResultShape(ExclusiveContext *cx, BytecodeEmitter *bce, unsigned *shape)
 bool
 BytecodeEmitter::emitPrepareIteratorResult()
 {
-    if (script->compileAndGo()) {
-        unsigned shape;
-        if (!IteratorResultShape(cx, this, &shape))
-            return false;
-        return emitIndex32(JSOP_NEWOBJECT, shape);
-    }
-
-    return emitNewInit(JSProto_Object);
+    unsigned shape;
+    if (!IteratorResultShape(cx, this, &shape))
+        return false;
+    return emitIndex32(JSOP_NEWOBJECT, shape);
 }
 
 bool
@@ -6587,14 +6581,12 @@ BytecodeEmitter::emitObject(ParseNode *pn)
 
 
     RootedPlainObject obj(cx);
-    if (script->compileAndGo()) {
-        
-        
-        gc::AllocKind kind = gc::GetGCObjectKind(pn->pn_count);
-        obj = NewBuiltinClassInstance<PlainObject>(cx, kind, TenuredObject);
-        if (!obj)
-            return false;
-    }
+    
+    
+    gc::AllocKind kind = gc::GetGCObjectKind(pn->pn_count);
+    obj = NewBuiltinClassInstance<PlainObject>(cx, kind, TenuredObject);
+    if (!obj)
+        return false;
 
     if (!emitPropertyList(pn, &obj, ObjectLiteral))
         return false;
