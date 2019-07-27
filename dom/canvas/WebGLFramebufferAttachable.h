4ee02c5cf29f36feff7f7844753087bee73fe5ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:19:41 +0000

    Back out f7657171e034 (bug 1017865) for relanding after already being backed out because it causes orange CLOSED TREE
    UltraBlame original commit: c39e4d4e5ba1c7f003a502579bb7a73ca927f0c3

--- dom/canvas/WebGLFramebufferAttachable.h
+++ dom/canvas/WebGLFramebufferAttachable.h
@@ -3,12 +3,12 @@
 
 
 
-#ifndef WEBGL_FRAMEBUFFER_ATTACHABLE_H_
-#define WEBGL_FRAMEBUFFER_ATTACHABLE_H_
+#ifndef WEBGLFRAMEBUFFERATTACHABLE_H_
+#define WEBGLFRAMEBUFFERATTACHABLE_H_
 
 #include "GLDefs.h"
-#include "mozilla/WeakPtr.h"
 #include "nsTArray.h"
+#include "mozilla/WeakPtr.h"
 #include "WebGLFramebuffer.h"
 #include "WebGLStrongTypes.h"
 
@@ -16,13 +16,29 @@ namespace mozilla {
 
 class WebGLFramebufferAttachable
 {
-    nsTArray<const WebGLFramebuffer::AttachPoint*> mAttachmentPoints;
+    struct AttachmentPoint
+    {
+        AttachmentPoint(const WebGLFramebuffer* fb, FBAttachment attachment)
+            : mFB(fb)
+            , mAttachment(attachment)
+        {}
+
+        WeakPtr<const WebGLFramebuffer> mFB;
+        FBAttachment mAttachment;
+
+        bool operator==(const AttachmentPoint& o) const {
+          return mFB == o.mFB && mAttachment == o.mAttachment;
+        }
+    };
+
+    nsTArray<AttachmentPoint> mAttachmentPoints;
 
 public:
+
     
-    void MarkAttachment(const WebGLFramebuffer::AttachPoint& attachment);
-    void UnmarkAttachment(const WebGLFramebuffer::AttachPoint& attachment);
-    void InvalidateStatusOfAttachedFBs() const;
+    void AttachTo(WebGLFramebuffer* fb, FBAttachment attachment);
+    void DetachFrom(WebGLFramebuffer* fb, FBAttachment attachment);
+    void NotifyFBsStatusChanged();
 };
 
 } 
