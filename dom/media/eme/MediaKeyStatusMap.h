b51c4be39b7dac2c003677e8e0524508d4b93510
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:57:32 +0000

    Bug 1121332. Part 4 - implement MediaKeySession.keyStatuses and remove MediaKeySession.getUsableKeyIds. r=bz.
    UltraBlame original commit: 455407ddfe431d116391bcd15f97d573de82c5e4

---
+++ dom/media/eme/MediaKeyStatusMap.h
@@ -0,0 +1,78 @@
+
+
+
+
+
+
+#ifndef mozilla_dom_MediaKeyStatuses_h
+#define mozilla_dom_MediaKeyStatuses_h
+
+#include "mozilla/ErrorResult.h"
+#include "mozilla/Attributes.h"
+#include "nsCycleCollectionParticipant.h"
+#include "nsWrapperCache.h"
+
+#include "mozilla/dom/TypedArray.h"
+#include "mozilla/dom/MediaKeyStatusMapBinding.h"
+#include "mozilla/CDMCaps.h"
+
+class nsPIDOMWindow;
+class ArrayBufferViewOrArrayBuffer;
+
+namespace mozilla {
+namespace dom {
+
+class MediaKeyStatusMap MOZ_FINAL : public nsISupports,
+                                    public nsWrapperCache
+{
+public:
+  NS_DECL_CYCLE_COLLECTING_ISUPPORTS
+  NS_DECL_CYCLE_COLLECTION_SCRIPT_HOLDER_CLASS(MediaKeyStatusMap)
+
+public:
+  explicit MediaKeyStatusMap(nsPIDOMWindow* aParent);
+
+protected:
+  ~MediaKeyStatusMap();
+
+public:
+  nsPIDOMWindow* GetParentObject() const;
+
+  virtual JSObject* WrapObject(JSContext* aCx) MOZ_OVERRIDE;
+
+  MediaKeyStatus Get(JSContext* aCx,
+                     const ArrayBufferViewOrArrayBuffer& aKey,
+                     ErrorResult& aRv) const;
+
+  bool Has(JSContext* aCx,
+           const ArrayBufferViewOrArrayBuffer& aKey,
+           ErrorResult& aRv) const;
+
+  void Keys(JSContext* aCx,
+            JS::MutableHandle<JSObject*> aResult,
+            ErrorResult& aRv) const;
+
+  void Values(JSContext* aCx,
+              JS::MutableHandle<JSObject*> aResult,
+              ErrorResult& aRv) const;
+
+  void Entries(JSContext* aCx,
+               JS::MutableHandle<JSObject*> aResult,
+               ErrorResult& aRv) const;
+
+  uint32_t GetSize(JSContext* aCx, ErrorResult& aRv) const;
+
+  void Update(const nsTArray<CDMCaps::KeyStatus>& keys);
+
+private:
+  nsresult UpdateInternal(const nsTArray<CDMCaps::KeyStatus>& keys);
+
+  nsCOMPtr<nsPIDOMWindow> mParent;
+  JS::Heap<JSObject*> mMap;
+  nsresult mUpdateError;
+};
+
+} 
+} 
+
+#endif
