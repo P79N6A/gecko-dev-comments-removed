d65bef52537c63a80fe62e0359b3e8be227cfa2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:34:34 +0000

    Bug 1186174 - Enable region specific search defaults for all Fennec users, r=mfinkle.
    UltraBlame original commit: 9f71fd7e13f98429597976e7c851c46da6b7a09e

--- mobile/android/app/mobile.js
+++ mobile/android/app/mobile.js
@@ -266,9 +266,7 @@ pref("browser.search.order.2", "chrome://browser/locale/region.properties");
 pref("browser.search.order.3", "chrome://browser/locale/region.properties");
 
 
-
-
-pref("browser.search.geoSpecificDefaults", false);
+pref("browser.search.geoSpecificDefaults", true);
 pref("browser.search.geoSpecificDefaults.url", "https://search.services.mozilla.com/1/%APP%/%VERSION%/%CHANNEL%/%LOCALE%/%REGION%/%DISTRIBUTION%/%DISTRIBUTION_VERSION%");
 
 
