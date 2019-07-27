3860799a4a9fe44d9cbf24682402f396309060c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:02:33 +0000

    Bug 1076820 - Redraw on resize performance for content is pretty bad with e10s r=billm
    Modern OSs adjust process scheduling based on CPU-boundedness and Interface-boundedness.  A process performing a lot of UI tasks requires lower-latency than a CPU-bound process.  For this reason, at least on Windows (but probably also Linux and Mac), in e10s, resize events are sent at a much higher rate than non-e10s, where the main process has to do the work to respond to them.  This was supposed to be handled by the 'compress' option in IPDL but its duplicate-event test was very imprecise - it only tested the *oldest* message in the queue.  This patch searches the messagequeue/deque to remove *any* duplicate.  deque::erase is linear but this is a very uncommon case - currently only two compressed IPDL messages exist in the entire code base.  Also, these queues are small.
    
    UltraBlame original commit: a0a30f2ddb2d2c0cc8db7104366c60cbc7e335e0

--- ipc/glue/MessageChannel.cpp
+++ ipc/glue/MessageChannel.cpp
@@ -583,6 +583,19 @@ MessageChannel::ShouldDeferMessage(const Message& aMsg)
     return mSide == ParentSide && aMsg.transaction_id() != mCurrentTransaction;
 }
 
+
+class MatchingKinds {
+    typedef IPC::Message Message;
+    Message::msgid_t mType;
+    int32_t mRoutingId;
+public:
+    MatchingKinds(Message::msgid_t aType, int32_t aRoutingId) :
+        mType(aType), mRoutingId(aRoutingId) {}
+    bool operator()(const Message &msg) {
+        return msg.type() == mType && msg.routing_id() == mRoutingId;
+    }
+};
+
 void
 MessageChannel::OnMessageReceivedFromLink(const Message& aMsg)
 {
@@ -604,15 +617,22 @@ MessageChannel::OnMessageReceivedFromLink(const Message& aMsg)
     
     MOZ_ASSERT(!aMsg.compress() || aMsg.priority() == IPC::Message::PRIORITY_NORMAL);
 
-    bool compress = (aMsg.compress() && !mPending.empty() &&
-                     mPending.back().type() == aMsg.type() &&
-                     mPending.back().routing_id() == aMsg.routing_id());
+    bool compress = (aMsg.compress() && !mPending.empty());
     if (compress) {
         
-        
-        
-        MOZ_ASSERT(mPending.back().compress());
-        mPending.pop_back();
+        auto it = std::find_if(mPending.rbegin(), mPending.rend(),
+                               MatchingKinds(aMsg.type(), aMsg.routing_id()));
+        if (it != mPending.rend()) {
+            
+            
+            
+            
+            MOZ_ASSERT((*it).compress());
+            mPending.erase((++it).base());
+        } else {
+            
+            compress = false;
+        }
     }
 
     bool shouldWakeUp = AwaitingInterruptReply() ||
