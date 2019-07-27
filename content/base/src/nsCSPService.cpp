599dbf6b791f7660c90c98249e00e6363a2b90cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:10:51 +0000

    Bug 1005225 - CSP in C++: SImplify shouldProcess for CSP (r=sstamm)
    UltraBlame original commit: 8fd6bf6c57cbed415c08ab7c58c3b18e0bd54277

--- content/base/src/nsCSPService.cpp
+++ content/base/src/nsCSPService.cpp
@@ -218,56 +218,7 @@ CSPService::ShouldProcess(uint32_t         aContentType,
   if (!aContentLocation)
     return NS_ERROR_FAILURE;
 
-  
   *aDecision = nsIContentPolicy::ACCEPT;
-
-  
-  if (!sCSPEnabled)
-    return NS_OK;
-
-  
-  
-  nsCOMPtr<nsINode> node(do_QueryInterface(aRequestContext));
-  nsCOMPtr<nsIPrincipal> principal;
-  nsCOMPtr<nsIContentSecurityPolicy> csp;
-  if (node) {
-    principal = node->NodePrincipal();
-    principal->GetCsp(getter_AddRefs(csp));
-
-    if (csp) {
-#ifdef PR_LOGGING
-      {
-        uint32_t numPolicies = 0;
-        nsresult rv = csp->GetPolicyCount(&numPolicies);
-        if (NS_SUCCEEDED(rv)) {
-          for (uint32_t i=0; i<numPolicies; i++) {
-            nsAutoString policy;
-            csp->GetPolicy(i, policy);
-            PR_LOG(gCspPRLog, PR_LOG_DEBUG,
-                   ("shouldProcess - document has policy[%d]: %s", i,
-                   NS_ConvertUTF16toUTF8(policy).get()));
-          }
-        }
-      }
-#endif
-      
-      csp->ShouldProcess(aContentType,
-                         aContentLocation,
-                         aRequestOrigin,
-                         aRequestContext,
-                         aMimeTypeGuess,
-                         aExtra,
-                         aDecision);
-    }
-  }
-#ifdef PR_LOGGING
-  else {
-    nsAutoCString uriSpec;
-    aContentLocation->GetSpec(uriSpec);
-    PR_LOG(gCspPRLog, PR_LOG_DEBUG,
-           ("COULD NOT get nsINode for location: %s", uriSpec.get()));
-  }
-#endif
   return NS_OK;
 }
 
