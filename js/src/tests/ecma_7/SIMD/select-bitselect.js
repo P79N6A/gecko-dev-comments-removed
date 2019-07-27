e46e068314932c08c40fe2d378f8904556ba4697
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:01:59 +0000

    Bug 1129416: Make FuncConvertBits and SIMD ctors GC sane in the interpreter; r=jonco
    UltraBlame original commit: e7c18ad1b770e6b108bd25d135df4bbbdc03cfa2

--- js/src/tests/ecma_7/SIMD/select-bitselect.js
+++ js/src/tests/ecma_7/SIMD/select-bitselect.js
@@ -128,9 +128,9 @@ function test() {
     testSelect(float64x2, inputs);
     testBitSelectSimple(float64x2, inputs);
     testBitSelectComplex(float64x2, inputs);
+
+    if (typeof reportCompare === "function")
+        reportCompare(true, true);
 }
 
-
-
-if (typeof reportCompare === "function")
-    reportCompare(true, true);
+test();
