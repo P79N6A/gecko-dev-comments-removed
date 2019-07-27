8c896fcdad4d645b288dfdbd3ee1ade8307addbb
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:02:02 +0000

    Bug 1175564 - don't lock y-axis if pan displacement is 0. r=kats
    UltraBlame original commit: 738e3dcf5eed00cf3a64672258f51876c11e7a34

--- gfx/layers/apz/src/AsyncPanZoomController.cpp
+++ gfx/layers/apz/src/AsyncPanZoomController.cpp
@@ -1639,10 +1639,14 @@ nsEventStatus AsyncPanZoomController::OnPanBegin(const PanGestureInput& aEvent) 
   }
 
   float dx = aEvent.mPanDisplacement.x, dy = aEvent.mPanDisplacement.y;
-  double angle = atan2(dy, dx); 
-  angle = fabs(angle); 
 
-  HandlePanning(angle);
+  if (dx || dy) {
+    double angle = atan2(dy, dx); 
+    angle = fabs(angle); 
+    HandlePanning(angle);
+  } else {
+    SetState(PANNING);
+  }
 
   return nsEventStatus_eConsumeNoDefault;
 }
