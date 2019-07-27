b2885af0c00be6894c349db875222a9c33c3f7f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:57:30 +0000

    Bug 1154425 - Tapping the same link twice when Tab Queue is enabled should Open Now (r=mcomella)
    UltraBlame original commit: 1baa62b2cc57f4827b9ffbb82303884739436aca

--- mobile/android/base/tabqueue/TabQueueHelper.java
+++ mobile/android/base/tabqueue/TabQueueHelper.java
@@ -21,6 +21,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
 import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -102,6 +103,45 @@ public class TabQueueHelper {
         profile.writeFile(filename, jsonArray.toString());
 
         return jsonArray.length();
+    }
+
+    
+
+
+
+
+
+
+
+
+
+    public static int removeURLFromFile(final Context context, final String urlToRemove, final String filename) {
+        ThreadUtils.assertNotOnUiThread();
+
+        final GeckoProfile profile = GeckoProfile.get(context);
+
+        JSONArray jsonArray = profile.readJSONArrayFromFile(filename);
+        JSONArray newArray = new JSONArray();
+        String url;
+
+        
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                url = jsonArray.getString(i);
+            } catch (JSONException e) {
+                url = "";
+            }
+            if(!TextUtils.isEmpty(url) && !urlToRemove.equals(url)) {
+                newArray.put(url);
+            }
+        }
+
+        profile.writeFile(filename, newArray.toString());
+
+        final SharedPreferences prefs = GeckoSharedPrefs.forApp(context);
+        prefs.edit().putInt(PREF_TAB_QUEUE_COUNT, newArray.length()).apply();
+
+        return newArray.length();
     }
 
     
