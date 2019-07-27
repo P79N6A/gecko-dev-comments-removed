b9269287b3850985b1989b4a1ba8ebc7f8af3474
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:53:03 +0000

    Bug 1053804: Cleanup Bluetooth Handsfree manager, r=shuang
    This patch removes the unusued callback code from Bluetooth's
    Handsfree manager.
    
    UltraBlame original commit: c0243ec27fdef453718117e62685252dc5b72dd3

--- dom/bluetooth/bluedroid/hfp/BluetoothHfpManager.h
+++ dom/bluetooth/bluedroid/hfp/BluetoothHfpManager.h
@@ -104,22 +104,6 @@ public:
   void HandleVoiceConnectionChanged(uint32_t aClientId);
 
   
-  void ProcessConnectionState(bthf_connection_state_t aState, bt_bdaddr_t* aBdAddress);
-  void ProcessAudioState(bthf_audio_state_t aState, bt_bdaddr_t* aBdAddress);
-  void ProcessAnswerCall();
-  void ProcessHangupCall();
-  void ProcessVolumeControl(bthf_volume_type_t aType, int aVolume);
-  void ProcessDialCall(char *aNumber);
-  void ProcessDtmfCmd(char aDtmf);
-  void ProcessAtChld(bthf_chld_type_t aChld);
-  void ProcessAtCnum();
-  void ProcessAtCind();
-  void ProcessAtCops();
-  void ProcessAtClcc();
-  void ProcessUnknownAt(char *aAtString);
-  void ProcessKeyPressed();
-
-  
   void UpdateSecondNumber(const nsAString& aNumber);
   void AnswerWaitingCall();
   void IgnoreWaitingCall();
