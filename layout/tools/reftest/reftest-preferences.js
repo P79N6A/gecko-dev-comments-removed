2b3d83423ce341db9b55ab6b1db2a521016d74f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:21:05 +0000

    Bug 1039834 - Fix PNG suite reftest on mulet. r=dbaron
    UltraBlame original commit: 2a85317e8f7ee2aedaaa9958f8fd466915ba942f

--- layout/tools/reftest/reftest-preferences.js
+++ layout/tools/reftest/reftest-preferences.js
@@ -3,6 +3,7 @@
     
     
     branch.setBoolPref("dom.use_xbl_scopes_for_remote_xul", false);
+    branch.setIntPref("gfx.color_management.mode", 2);
     branch.setBoolPref("gfx.color_management.force_srgb", true);
     branch.setBoolPref("browser.dom.window.dump.enabled", true);
     branch.setIntPref("ui.caretBlinkTime", -1);
