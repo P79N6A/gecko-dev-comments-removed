8ecd33e0c282484496022478012a82d831c85216
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:22:53 +0000

    Bug 1090636, part 11 - Pass pobj (the object that contains the element) to SetDenseOrTypedArrayElement rather than obj. Reflect.set could trigger the bug being fixed here, so we leave a sleeper test for it. r=efaust.
    UltraBlame original commit: 14f7fecc080c1d8e86a38ecbd12135311c1fb273

---
+++ js/src/tests/ecma_6/Reflect/set.js
@@ -0,0 +1,16 @@
+if (this.Reflect && Reflect.set)
+    throw new Error("Congrats on implementing Reflect.set! Uncomment this test for 1 karma point.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+reportCompare(0, 0, 'ok');
