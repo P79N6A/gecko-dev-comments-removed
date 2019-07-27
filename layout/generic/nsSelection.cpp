47baf74bed18b188a61a75a2065c4387d2e0e07f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 01:34:06 +0000

    Bug 1046573 - Add an observer notification for newly created selection objects; r=ehsan
    UltraBlame original commit: 80d85e3fdf41554337e55b673d8032902369c8ca

--- layout/generic/nsSelection.cpp
+++ layout/generic/nsSelection.cpp
@@ -362,6 +362,12 @@ nsFrameSelection::nsFrameSelection()
   
   mDelayedMouseEventIsShift = false;
   mDelayedMouseEventClickCount = 0;
+
+  nsCOMPtr<nsIObserverService> observerService =
+    mozilla::services::GetObserverService();
+  if (observerService) {
+    observerService->NotifyObservers(this, "selection-object-created", nullptr);
+  }
 }
 
 
