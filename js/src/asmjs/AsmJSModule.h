8766802a7149a2813ca2bd9a84c2ee8c8711987f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 02:34:03 +0000

    Bug 1025100: SIMD: add support for with{X,Y,Z,W} in Odin; r=luke
    UltraBlame original commit: 4df302f6b71995dd1a9010dc1bfecf1639e6f810

--- js/src/asmjs/AsmJSModule.h
+++ js/src/asmjs/AsmJSModule.h
@@ -92,7 +92,11 @@ enum AsmJSSimdOperation
     AsmJSSimdOperation_or,
     AsmJSSimdOperation_xor,
     AsmJSSimdOperation_select,
-    AsmJSSimdOperation_splat
+    AsmJSSimdOperation_splat,
+    AsmJSSimdOperation_withX,
+    AsmJSSimdOperation_withY,
+    AsmJSSimdOperation_withZ,
+    AsmJSSimdOperation_withW
 };
 
 
