c2e7b03e4c067987aaec622f1639116447d5b3d7
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:44:32 +0000

    Bug 1171171 - Make sure the graphics preferences are initialized early enough. r=billm
    UltraBlame original commit: a318923da99bc9fbcf834dd555b393acba0802ca

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -100,6 +100,8 @@
 #include "cairo/cairo-features.h"
 #include "mozilla/WindowsVersion.h"
 
+#include "gfxPrefs.h"
+
 #ifndef PROCESS_DEP_ENABLE
 #define PROCESS_DEP_ENABLE 0x1
 #endif
@@ -2150,6 +2152,10 @@ ShowProfileManager(nsIToolkitProfileService* aProfileSvc,
     ScopedXPCOMStartup xpcom;
     rv = xpcom.Initialize();
     NS_ENSURE_SUCCESS(rv, rv);
+
+    
+    
+    gfxPrefs::GetSingleton();
 
     rv = xpcom.SetWindowCreator(aNative);
     NS_ENSURE_SUCCESS(rv, NS_ERROR_FAILURE);
