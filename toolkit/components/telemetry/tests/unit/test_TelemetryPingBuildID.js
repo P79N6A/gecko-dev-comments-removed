e1a760aae8f106eabebf4439acdc0c7ae77c2b17
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:48:05 +0000

    Merge fx-team to m-c. a=merge
    UltraBlame original commit: de27ac2ab94ff46ee5ef7c7bf708bde742086944

--- toolkit/components/telemetry/tests/unit/test_TelemetryPingBuildID.js
+++ toolkit/components/telemetry/tests/unit/test_TelemetryPingBuildID.js
@@ -16,7 +16,6 @@
 "use strict";
 
 Cu.import("resource://gre/modules/Services.jsm", this);
-Cu.import("resource://gre/modules/TelemetryPing.jsm", this);
 Cu.import("resource://gre/modules/TelemetrySession.jsm", this);
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
@@ -26,7 +25,7 @@ XPCOMUtils.defineLazyGetter(this, "gDatareportingService",
           .wrappedJSObject);
 
 
-Services.prefs.setBoolPref(TelemetryPing.Constants.PREF_ENABLED, true);
+Services.prefs.setBoolPref(TelemetrySession.Constants.PREF_ENABLED, true);
 
 
 Cu.import("resource://testing-common/AppInfo.jsm", this);
