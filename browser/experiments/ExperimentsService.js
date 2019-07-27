05ea2f79eeee773d6a1051ec5676950bc04a7a2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 14:14:50 +0000

    Bug 1178456 - Experiments service shouldn't use FHR prefs. r=gfritzsche
    UltraBlame original commit: b9b256c42a62ac7e23fbd421f9a1f6ee6a29e4ac

--- browser/experiments/ExperimentsService.js
+++ browser/experiments/ExperimentsService.js
@@ -21,7 +21,12 @@ const PREF_EXPERIMENTS_ENABLED  = "experiments.enabled";
 const PREF_ACTIVE_EXPERIMENT    = "experiments.activeExperiment"; 
 const PREF_HEALTHREPORT_ENABLED = "datareporting.healthreport.service.enabled";
 const PREF_TELEMETRY_ENABLED    = "toolkit.telemetry.enabled";
+const PREF_TELEMETRY_UNIFIED    = "toolkit.telemetry.unified";
 const DELAY_INIT_MS             = 30 * 1000;
+
+
+
+const IS_UNIFIED_TELEMETRY = Preferences.get(PREF_TELEMETRY_UNIFIED, false);
 
 XPCOMUtils.defineLazyGetter(
   this, "gPrefs", () => {
@@ -30,9 +35,11 @@ XPCOMUtils.defineLazyGetter(
 
 XPCOMUtils.defineLazyGetter(
   this, "gExperimentsEnabled", () => {
+    
+    
     return gPrefs.get(PREF_EXPERIMENTS_ENABLED, false) &&
-           gPrefs.get(PREF_TELEMETRY_ENABLED, false) &&
-           gPrefs.get(PREF_HEALTHREPORT_ENABLED, false);
+           (gPrefs.get(PREF_HEALTHREPORT_ENABLED, false) || IS_UNIFIED_TELEMETRY) &&
+           gPrefs.get(PREF_TELEMETRY_ENABLED, false);
   });
 
 XPCOMUtils.defineLazyGetter(
