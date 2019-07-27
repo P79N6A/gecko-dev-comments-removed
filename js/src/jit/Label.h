fec7ec20cf6270c7ecf3da847f3156813e7f2248
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 00:50:48 +0000

    Bug 1031482 - IonMonkey: Restore Label's asserts. r=jandem
    UltraBlame original commit: b5cf6f6dfa83f43f738158b051d1946758283967

--- js/src/jit/Label.h
+++ js/src/jit/Label.h
@@ -7,6 +7,8 @@
 #ifndef jit_Label_h
 #define jit_Label_h
 
+#include "jit/Ion.h"
+
 namespace js {
 namespace jit {
 
@@ -24,10 +26,6 @@ struct LabelBase
     static const int32_t INVALID_OFFSET = -1;
 
     LabelBase() : offset_(INVALID_OFFSET), bound_(false)
-    { }
-    LabelBase(const LabelBase &label)
-      : offset_(label.offset_),
-        bound_(label.bound_)
     { }
 
     
@@ -79,10 +77,20 @@ struct LabelBase
 class Label : public LabelBase
 {
   public:
-    Label()
-    { }
-    Label(const Label &label) : LabelBase(label)
-    { }
+    ~Label()
+    {
+#ifdef DEBUG
+        
+        if (OOM_counter > OOM_maxAllocations)
+            return;
+        if (IonContext *context = MaybeGetIonContext()) {
+            if (context->runtime->hadOutOfMemory())
+                return;
+        }
+
+        MOZ_ASSERT(!used());
+#endif
+    }
 };
 
 
@@ -90,6 +98,14 @@ class Label : public LabelBase
 
 class NonAssertingLabel : public Label
 {
+  public:
+    ~NonAssertingLabel()
+    {
+#ifdef DEBUG
+        if (used())
+            bind(0);
+#endif
+    }
 };
 
 } } 
