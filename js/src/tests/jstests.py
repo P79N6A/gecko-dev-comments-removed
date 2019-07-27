4b7ec9c56187d60f7640a4399c1e2fb45d640dce
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 07:53:34 +0000

    Bug 1089745 - Fix jstest failures outside PST timezone. r=terrence
    UltraBlame original commit: 99072302a3f4f9b3c4a65baba7a66e9bc39854c8

--- js/src/tests/jstests.py
+++ js/src/tests/jstests.py
@@ -306,6 +306,9 @@ def main():
     if test_dir not in ('', '.'):
         os.chdir(test_dir)
 
+    
+    os.environ['TZ'] = 'PST8PDT'
+
     results = None
     try:
         results = ResultsSink(options, len(skip_list) + len(test_list))
