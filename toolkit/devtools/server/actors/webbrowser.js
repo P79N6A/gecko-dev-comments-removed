dfeddc7ed63cf01b7e64df212cc83e8017d14dfc
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 13:29:39 +0000

    Bug 1067325 - DevTools test fixes for view source in tab. r=pbrosset
    UltraBlame original commit: 08dfcb03f55b6ad6ee40b6c6915447b3bc3177ab

--- toolkit/devtools/server/actors/webbrowser.js
+++ toolkit/devtools/server/actors/webbrowser.js
@@ -978,7 +978,6 @@ TabActor.prototype = {
     });
 
     this._extraActors = null;
-    this._styleSheetActors.clear();
 
     this._exited = true;
   },
@@ -1325,6 +1324,10 @@ TabActor.prototype = {
     this._popContext();
 
     
+    for (let sheetActor of this._styleSheetActors.values()) {
+      this._tabPool.removeActor(sheetActor);
+    }
+    this._styleSheetActors.clear();
     this.conn.removeActorPool(this._tabPool);
     this._tabPool = null;
     if (this._tabActorPool) {
@@ -1641,12 +1644,6 @@ TabActor.prototype = {
       
       threadActor.global = window;
     }
-
-    for (let sheetActor of this._styleSheetActors.values()) {
-      this._tabPool.removeActor(sheetActor);
-    }
-    this._styleSheetActors.clear();
-
 
     
     
