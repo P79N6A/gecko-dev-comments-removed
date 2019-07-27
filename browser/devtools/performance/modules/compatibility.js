da54fea8853387cb2170e7224c57747a0bcfa38f
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 09:04:46 +0000

    Bug 1130200 - Fix the mocked memory and timeline actors for older gecko targets (fx2.0, fx2.1). r=vp
    UltraBlame original commit: f97102233fba13f6176f958de17172aa402f4a13

---
+++ browser/devtools/performance/modules/compatibility.js
@@ -0,0 +1,109 @@
+
+
+
+"use strict";
+
+const { Task } = require("resource://gre/modules/Task.jsm");
+loader.lazyRequireGetter(this, "EventEmitter",
+  "devtools/toolkit/event-emitter");
+
+const REQUIRED_MEMORY_ACTOR_METHODS = [
+  "attach", "detach", "startRecordingAllocations", "stopRecordingAllocations", "getAllocations"
+];
+
+
+
+
+
+
+
+function MockFront (blueprint) {
+  EventEmitter.decorate(this);
+
+  for (let [funcName, retVal] of blueprint) {
+    this[funcName] = (x => typeof x === "function" ? x() : x).bind(this, retVal);
+  }
+}
+
+function MockMemoryFront () {
+  MockFront.call(this, [
+    ["attach"],
+    ["detach"],
+    ["initialize"],
+    ["destroy"],
+    ["startRecordingAllocations", 0],
+    ["stopRecordingAllocations", 0],
+    ["getAllocations", createMockAllocations],
+  ]);
+}
+exports.MockMemoryFront = MockMemoryFront;
+
+function MockTimelineFront () {
+  MockFront.call(this, [
+    ["start", 0],
+    ["stop", 0],
+    ["initialize"],
+    ["destroy"],
+  ]);
+}
+exports.MockTimelineFront = MockTimelineFront;
+
+
+
+
+
+
+
+function createMockAllocations () {
+  return {
+    allocations: [],
+    allocationsTimestamps: [],
+    frames: [],
+    counts: []
+  };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function* memoryActorSupported (target) {
+  
+  
+  if (target.TEST_MOCK_MEMORY_ACTOR) {
+    return false;
+  }
+
+  for (let method of REQUIRED_MEMORY_ACTOR_METHODS) {
+    if (!(yield target.actorHasMethod("memory", method))) {
+      return false;
+    }
+  }
+  return true;
+}
+exports.memoryActorSupported = Task.async(memoryActorSupported);
+
+
+
+
+
+
+
+
+function* timelineActorSupported(target) {
+  
+  
+  if (target.TEST_MOCK_TIMELINE_ACTOR) {
+    return false;
+  }
+
+  return yield target.hasActor("timeline");
+}
+exports.timelineActorSupported = Task.async(timelineActorSupported);
