ee05156d8dbb6f6e0824b64aba608346cc3ce6d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 08:30:15 +0000

    Bug 1108771 - Part 2: Implement |mach bootstrap| for mobile/android on Debian-like systems. r=gps
    This adds a generic android Python module that handles:
    
    * downloading and unpacking Google's Android SDK and NDK bundles;
    * using the |android| tool to install additional Android packages;
    * printing a mozconfig snippet suitable for mobile/android builds.
    
    UltraBlame original commit: 3ecd0c6da57ba01c64ad467781c45d82510e3791

---
+++ python/mozboot/mozboot/android.py
@@ -0,0 +1,208 @@
+
+
+
+
+
+from __future__ import print_function
+
+import errno
+import os
+import subprocess
+
+
+
+
+ANDROID_PLATFORM = 'android-21'
+ANDROID_BUILD_TOOLS_VERSION = '21.1.2'
+
+
+
+ANDROID_PACKAGES = [
+    'tools',
+    'platform-tools',
+    'build-tools-%s' % ANDROID_BUILD_TOOLS_VERSION,
+    ANDROID_PLATFORM,
+    'extra-android-support',
+    'extra-google-google_play_services',
+]
+
+ANDROID_NDK_EXISTS = '''
+Looks like you have the Android NDK installed at:
+%s
+'''
+
+ANDROID_SDK_EXISTS = '''
+Looks like you have the Android SDK installed at:
+%s
+We will install all required Android packages.
+'''
+
+NOT_INSTALLING_ANDROID_PACKAGES = '''
+It looks like you already have the following Android packages:
+%s
+No need to update!
+'''
+
+INSTALLING_ANDROID_PACKAGES = '''
+We are now installing the following Android packages:
+%s
+You may be prompted to agree to the Android license. You may see some of
+output as packages are downloaded and installed.
+'''
+
+MISSING_ANDROID_PACKAGES = '''
+We tried to install the following Android packages:
+%s
+But it looks like we couldn't install:
+%s
+Install these Android packages manually and run this bootstrapper again.
+'''
+
+MOBILE_ANDROID_MOZCONFIG_TEMPLATE = '''
+Paste the lines between the chevrons (>>> and <<<) into your mozconfig file:
+
+<<<
+# Build Firefox for Android:
+ac_add_options --enable-application=mobile/android
+ac_add_options --target=arm-linux-androideabi
+
+# With the following Android SDK and NDK:
+ac_add_options --with-android-sdk="%s"
+ac_add_options --with-android-ndk="%s"
+>>>
+'''
+
+
+def check_output(*args, **kwargs):
+    """Run subprocess.check_output even if Python doesn't provide it."""
+    from base import BaseBootstrapper
+    fn = getattr(subprocess, 'check_output', BaseBootstrapper._check_output)
+
+    return fn(*args, **kwargs)
+
+def list_missing_android_packages(android_tool, packages):
+    '''
+    Use the given |android| tool to return the sub-list of Android
+    |packages| given that are not installed.
+    '''
+    missing = []
+
+    
+    
+    
+    lines = check_output([android_tool,
+        'list', 'sdk', '--no-ui', '--extended']).splitlines()
+
+    
+    for line in lines:
+        is_id_line = False
+        try:
+            is_id_line = line.startswith("id:")
+        except:
+            
+            pass
+        if not is_id_line:
+            continue
+
+        for package in packages:
+            if '"%s"' % package in line:
+                
+                missing.append(package)
+
+    return missing
+
+def install_mobile_android_sdk_or_ndk(url, path):
+    '''
+    Fetch an Android SDK or NDK from |url| and unpack it into
+    the given |path|.
+
+    We expect wget to be installed and found on the system path.
+
+    We use, and wget respects, https.  We could also include SHAs for a
+    small improvement in the integrity guarantee we give. But this script is
+    bootstrapped over https anyway, so it's a really minor improvement.
+
+    We use |wget --continue| as a cheap cache of the downloaded artifacts,
+    writing into |path|/mozboot.  We don't yet clean the cache; it's better
+    to waste disk and not require a long re-download than to wipe the cache
+    prematurely.
+    '''
+
+    old_path = os.getcwd()
+    try:
+        download_path = os.path.join(path, 'mozboot')
+        try:
+            os.makedirs(download_path)
+        except OSError as e:
+            if e.errno == errno.EEXIST and os.path.isdir(download_path):
+                pass
+            else:
+                raise
+
+        os.chdir(download_path)
+        subprocess.check_call(['wget', '--continue', url])
+        file = url.split('/')[-1]
+
+        os.chdir(path)
+        if file.endswith('.tar.gz') or file.endswith('.tgz'):
+            cmd = ['tar', 'zvxf']
+        elif file.endswith('.tar.bz2'):
+            cmd = ['tar', 'jvxf']
+        elif file.endswitch('.zip'):
+            cmd = ['unzip']
+        else:
+            raise NotImplementedError("Don't know how to unpack file: %s" % file)
+        subprocess.check_call(cmd + [os.path.join(download_path, file)])
+    finally:
+        os.chdir(old_path)
+
+def ensure_android_sdk_and_ndk(path, sdk_path, sdk_url, ndk_path, ndk_url):
+    '''
+    Ensure the Android SDK and NDK are found at the given paths.  If not, fetch
+    and unpack the SDK and/or NDK from the given URLs into |path|.
+    '''
+
+    
+    
+    
+    if os.path.isdir(ndk_path):
+        print(ANDROID_NDK_EXISTS % ndk_path)
+    else:
+        install_mobile_android_sdk_or_ndk(ndk_url, path)
+
+    
+    
+    
+    if os.path.isdir(sdk_path):
+        print(ANDROID_SDK_EXISTS % sdk_path)
+    else:
+        install_mobile_android_sdk_or_ndk(sdk_url, path)
+
+def ensure_android_packages(android_tool, packages=None):
+    '''
+    Use the given android tool (like 'android') to install required Android
+    packages.
+    '''
+
+    if not packages:
+        packages = ANDROID_PACKAGES
+
+    missing = list_missing_android_packages(android_tool, packages=packages)
+    if not missing:
+        print(NOT_INSTALLING_ANDROID_PACKAGES % ', '.join(packages))
+        return
+
+    
+    
+    print(INSTALLING_ANDROID_PACKAGES % ', '.join(missing))
+    subprocess.check_call([android_tool,
+        'update', 'sdk', '--no-ui',
+        '--filter', ','.join(missing)])
+
+    
+    failing = list_missing_android_packages(android_tool, packages=packages)
+    if failing:
+        raise Exception(MISSING_ANDROID_PACKAGES % (', '.join(missing), ', '.join(failing)))
+
+def suggest_mozconfig(sdk_path=None, ndk_path=None):
+    print(MOBILE_ANDROID_MOZCONFIG_TEMPLATE % (sdk_path, ndk_path))
--- python/mozboot/mozboot/debian.py
+++ python/mozboot/mozboot/debian.py
@@ -2,6 +2,9 @@
 
 
 
+import os
+import sys
+
 from mozboot.base import BaseBootstrapper
 
 class DebianBootstrapper(BaseBootstrapper):
@@ -22,6 +25,8 @@ class DebianBootstrapper(BaseBootstrapper):
     
     DISTRO_PACKAGES = []
 
+    
+    
     BROWSER_COMMON_PACKAGES = [
         'libasound2-dev',
         'libcurl4-openssl-dev',
@@ -44,6 +49,21 @@ class DebianBootstrapper(BaseBootstrapper):
     
     BROWSER_DISTRO_PACKAGES = []
 
+    
+    
+    MOBILE_ANDROID_COMMON_PACKAGES = [
+        'zlib1g-dev', 
+        'openjdk-7-jdk',
+        'ant',
+        'wget', 
+        'libncurses5:i386', 
+        'libstdc++6:i386',
+        'zlib1g:i386',
+    ]
+
+    
+    MOBILE_ANDROID_DISTRO_PACKAGES = []
+
     def __init__(self, version, dist_id):
         BaseBootstrapper.__init__(self)
 
@@ -52,12 +72,57 @@ class DebianBootstrapper(BaseBootstrapper):
 
         self.packages = self.COMMON_PACKAGES + self.DISTRO_PACKAGES
         self.browser_packages = self.BROWSER_COMMON_PACKAGES + self.BROWSER_DISTRO_PACKAGES
+        self.mobile_android_packages = self.MOBILE_ANDROID_COMMON_PACKAGES + self.MOBILE_ANDROID_DISTRO_PACKAGES
 
     def install_system_packages(self):
         self.apt_install(*self.packages)
 
     def install_browser_packages(self):
         self.apt_install(*self.browser_packages)
+
+    def install_mobile_android_packages(self):
+        import android
+
+        
+        
+        
+        
+
+        
+        
+        
+        
+        
+        self.run_as_root(['dpkg', '--add-architecture', 'i386'])
+        
+        self.apt_install(*self.mobile_android_packages)
+
+        
+        
+        
+        mozbuild_path = os.environ.get('MOZBUILD_STATE_PATH', os.path.expanduser(os.path.join('~', '.mozbuild')))
+        self.sdk_path = os.environ.get('ANDROID_SDK_HOME', os.path.join(mozbuild_path, 'android-sdk-linux'))
+        self.ndk_path = os.environ.get('ANDROID_NDK_HOME', os.path.join(mozbuild_path, 'android-ndk-r8e'))
+        self.sdk_url = 'https://dl.google.com/android/android-sdk_r24.0.1-linux.tgz'
+        is_64bits = sys.maxsize > 2**32
+        if is_64bits:
+            self.ndk_url = 'https://dl.google.com/android/ndk/android-ndk-r8e-linux-x86_64.tar.bz2'
+        else:
+            self.ndk_url = 'https://dl.google.com/android/ndk/android-ndk-r8e-linux-x86.tar.bz2'
+        android.ensure_android_sdk_and_ndk(path=mozbuild_path,
+            sdk_path=self.sdk_path, sdk_url=self.sdk_url,
+            ndk_path=self.ndk_path, ndk_url=self.ndk_url)
+
+        
+        
+        android_tool = os.path.join(self.sdk_path, 'tools', 'android')
+        android.ensure_android_packages(android_tool=android_tool)
+
+    def suggest_mobile_android_mozconfig(self):
+        import android
+
+        android.suggest_mozconfig(sdk_path=self.sdk_path,
+            ndk_path=self.ndk_path)
 
     def _update_package_manager(self):
         self.run_as_root(['apt-get', 'update'])
