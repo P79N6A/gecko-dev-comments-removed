af560833a133ea02cc638d95c700055d96f8ada8
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-07-27 12:23:43 +0000

    Bug 1135428: Fix test to run with --no-asmjs as well on a CLOSED TREE; r=bustage
    UltraBlame original commit: 4113f4e7e3e3ee8f9dd079001f44dc9c9f72630f

--- js/src/jit-test/tests/asm.js/testBug893519.js
+++ js/src/jit-test/tests/asm.js/testBug893519.js
@@ -1,4 +1,10 @@
 
+
+if (!isAsmJSCompilationAvailable()) {
+    throw new Error('this test expects an error to be thrown, here it is');
+    quit();
+}
+
 var g = newGlobal();
 evaluate("function h() { function f() { 'use asm'; function g() { return 42 } return g } return f }", { compileAndGo:false, global:g});
 var h = clone(g.h);
